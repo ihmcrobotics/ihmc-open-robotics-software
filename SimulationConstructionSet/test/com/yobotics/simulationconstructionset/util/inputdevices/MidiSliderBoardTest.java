b5a72c19372f36435e9d5e48a95d0907f824361c
Peter Neuhaus <pneuhaus@ihmc.us>
2010-07-14 17:04:44 +0000

    Created junit test

---
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/util/inputdevices/MidiSliderBoardTest.java
@@ -0,0 +1,36 @@
+package com.yobotics.simulationconstructionset.util.inputdevices;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.yobotics.simulationconstructionset.Robot;
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.YoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class MidiSliderBoardTest
+{
+   @Before
+   public void setUp() throws Exception
+   {
+   }
+
+   @After
+   public void tearDown() throws Exception
+   {
+   }
+   
+   @Test
+   public void testWeirdCase()
+   {
+	   MidiSliderBoard midiSliderBoard = new MidiSliderBoard();
+	   YoVariableRegistry registry = new YoVariableRegistry("testRegistry");
+	   YoVariable yoVariable = new YoVariable("test", registry);
+	   midiSliderBoard.setChannel(3, yoVariable, 1.5, 2.5);
+   }
+
+}
