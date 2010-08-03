3fb3b62887f8f9ef3ff4f3dcdd5d2f8ae8c8c708
Twan Koolen <tkoolen@ihmc.us>
2010-08-03 20:50:38 +0000

    Moved DesiredHeadingControlModule over to CommonWalkingControlModules. Renamed R2Sim02CartesianTrajectoryGenerator --> TakeoffLandingCartesianTrajectoryGenerator.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/DesiredHeadingControlModule.java
@@ -0,0 +1,17 @@
+package us.ihmc.commonWalkingControlModules.controlModules;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+public interface DesiredHeadingControlModule
+{
+   public abstract void updateDesiredHeadingFrame();
+
+   public abstract ReferenceFrame getDesiredHeadingFrame();
+
+   public abstract FrameVector getDisplacementWithRespectToFoot(RobotSide robotSide, FramePoint position);
+
+   public abstract void setDesiredHeading(double desiredHeading);
+}
