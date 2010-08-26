a1b76d29959fd8e7ad22789bff1c16d461c6768e
Matt Johnson <mjohnson@ihmc.us>
2010-08-26 21:53:18 +0000

    PROJECTM-172 added velocity offset to guideline calculator

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/GuideLineCalculator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/GuideLineCalculator.java
@@ -3,13 +3,13 @@ package us.ihmc.commonWalkingControlModules.controlModules;
 
 import us.ihmc.commonWalkingControlModules.RobotSide;
 import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.BipedSupportPolygons;
-import us.ihmc.utilities.math.geometry.FrameLineSegment2d;
-import us.ihmc.utilities.math.geometry.FramePoint;
-import us.ihmc.utilities.math.geometry.FramePoint2d;
+import us.ihmc.utilities.math.geometry.*;
 
 public interface GuideLineCalculator
 {
-   public void update(RobotSide side, BipedSupportPolygons bipedSupportPolygons, FramePoint2d capturePointInSupportFootZUp, FramePoint finalDesiredSwingTarget);
+   public void update(RobotSide side, BipedSupportPolygons bipedSupportPolygons, FramePoint2d capturePointInSupportFootZUp,
+                      FramePoint finalDesiredSwingTarget, FrameVector2d desiredVelocityInSupportFootFrame,
+                      FrameVector2d actualCenterOfMassVelocityInSupportFootFrame);
 
    public void reset();
 
