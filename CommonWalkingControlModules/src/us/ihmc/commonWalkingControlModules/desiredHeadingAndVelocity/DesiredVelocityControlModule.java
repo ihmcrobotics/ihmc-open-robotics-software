9f835e346317aa1249d0dd46fc6155822c6fdaf2
Jerry Pratt <jpratt@ihmc.us>
2010-10-08 17:45:10 +0000

    PROJECTM-199: Moved DesiredVelocityControlModule to CommonWalkingControlModules

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredHeadingAndVelocity/DesiredVelocityControlModule.java
@@ -0,0 +1,14 @@
+package us.ihmc.commonWalkingControlModules.desiredHeadingAndVelocity;
+
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.FrameVector2d;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+public interface DesiredVelocityControlModule
+{
+   public abstract FrameVector2d getDesiredVelocity();
+
+   public abstract FrameVector2d getVelocityErrorInFrame(ReferenceFrame referenceFrame);
+
+   public abstract void setDesiredVelocity(FrameVector newDesiredVelocity);
+}
