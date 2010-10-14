f230f6ae88e2e4df20f3f90c07df450d18e74900
Peter Neuhaus <pneuhaus@ihmc.us>
2010-10-14 16:51:07 +0000

    PROJECTM-199 Moved SimpleCapturePointBasedDesiredStepLocationCalculator to CommonWalkingControlModules

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
@@ -1,12 +1,17 @@
 package us.ihmc.commonWalkingControlModules.sensors;
 
 import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
 
 public interface ProcessedSensorsInterface
 {
-
      public abstract double getTime();
      
      public abstract double getKneeAngle(RobotSide robotSide);
-   
+     
+     public abstract FrameVector getCenterOfMassVelocityInFrame(ReferenceFrame referenceFrame);
+     public abstract FramePoint getCenterOfMassPositionInFrame(ReferenceFrame referenceFrame);
+     public abstract FrameVector getGravityInWorldFrame();
 }
