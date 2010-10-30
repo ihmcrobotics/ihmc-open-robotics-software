45619c0ddf04029db14f62325f3144ba295c1bf3
Jerry Pratt <jpratt@ihmc.us>
2010-10-30 05:50:17 +0000

    PROJECTM-206: Enums are moved to CommonWalkingControlModules http://jira.ihmc.us/browse/PROJECTM-206 Moved a bunch more of the enums like ArmTorques, etc.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/partNamesAndTorques/LowerBodyTorques.java
@@ -0,0 +1,49 @@
+package us.ihmc.commonWalkingControlModules.partNamesAndTorques;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegTorques;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.RobotSpecificJointNames;
+
+public class LowerBodyTorques
+{
+   private LegTorques[] legTorquesArray = new LegTorques[RobotSide.values().length];
+
+   public LowerBodyTorques(RobotSpecificJointNames robotJointNames)
+   {
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         legTorquesArray[robotSide.ordinal()] = new LegTorques(robotJointNames, robotSide);
+      }
+   }
+
+// private LowerBodyTorques(LowerBodyTorques lowerBodyTorques)
+// {
+//    for (RobotSide robotSide : RobotSide.values())
+//    {
+//       legTorquesArray[robotSide.ordinal()] = lowerBodyTorques.getLegTorquesCopy(robotSide);
+//    }
+// }
+
+   public LegTorques getLegTorques(RobotSide robotSide)
+   {
+      return legTorquesArray[robotSide.ordinal()];
+   }
+
+   public LegTorques getLegTorquesCopy(RobotSide robotSide)
+   {
+      return legTorquesArray[robotSide.ordinal()].getLegTorquesCopy();
+   }
+
+   public void copyLegTorquesValuesToLowerBodyTorques(LegTorques legTorques)
+   {
+      legTorquesArray[legTorques.getRobotSide().ordinal()] = legTorques.getLegTorquesCopy();
+   }
+
+   public void setLowerBodyTorquesToZero()
+   {
+      for (int i = 0; i < legTorquesArray.length; i++)
+      {
+         legTorquesArray[i].setTorquesToZero();
+      }
+   }
+}
