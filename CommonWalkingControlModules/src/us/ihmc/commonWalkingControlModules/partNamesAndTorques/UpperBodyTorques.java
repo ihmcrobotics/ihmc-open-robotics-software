417527dabeb44d2b018ad320cea1db0a22e501cd
Jerry Pratt <jpratt@ihmc.us>
2010-10-30 07:33:57 +0000

    PROJECTM-206: Enums are moved to CommonWalkingControlModules http://jira.ihmc.us/browse/PROJECTM-206 Moved the R2 upper body enums.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/partNamesAndTorques/UpperBodyTorques.java
@@ -0,0 +1,81 @@
+package us.ihmc.commonWalkingControlModules.partNamesAndTorques;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmTorques;
+
+public class UpperBodyTorques
+{
+   private SpineTorques spineTorques = new SpineTorques();
+   private ArmTorques[] armTorquesArray = new ArmTorques[RobotSide.values().length];
+   private NeckTorques neckTorques = new NeckTorques();
+
+   public UpperBodyTorques()
+   {
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         armTorquesArray[robotSide.ordinal()] = new ArmTorques(robotSide);
+      }
+   }
+
+
+// private UpperBodyTorques(UpperBodyTorques upperBodyTorques)
+// {
+//    this.spineTorques = upperBodyTorques.getSpineTorquesCopy();
+//    this.headTorques = upperBodyTorques.getHeadTorquesCopy();
+//    this.armTorquesArray = upperBodyTorques.getArmTorquesArrayCopy();
+// }
+
+   public ArmTorques getArmTorquesCopy(RobotSide robotSide)
+   {
+      return armTorquesArray[robotSide.ordinal()].getArmTorquesCopy();
+   }
+
+   public ArmTorques[] getArmTorquesArrayCopy()
+   {
+      ArmTorques[] ret = new ArmTorques[armTorquesArray.length];
+
+      for (int i = 0; i < armTorquesArray.length; i++)
+      {
+         ret[i] = armTorquesArray[i].getArmTorquesCopy();
+      }
+
+      return ret;
+   }
+
+   public SpineTorques getSpineTorquesCopy()
+   {
+      return spineTorques.getSpineTorquesCopy();
+   }
+
+   public NeckTorques getNeckTorquesCopy()
+   {
+      return neckTorques.getNeckTorquesCopy();
+   }
+
+   public void setArmTorques(ArmTorques armTorques)
+   {
+      armTorquesArray[armTorques.getRobotSide().ordinal()] = armTorques.getArmTorquesCopy();
+   }
+
+   public void setSpineTorques(SpineTorques spineTorques)
+   {
+      this.spineTorques = spineTorques.getSpineTorquesCopy();
+   }
+
+
+   public void setNeckTorques(NeckTorques neckTorques)
+   {
+      this.neckTorques = neckTorques.getNeckTorquesCopy();
+   }
+
+   public void setTorquesToZero()
+   {
+      neckTorques.setTorquesToZero();
+      spineTorques.setTorquesToZero();
+
+      for (ArmTorques armTorques : armTorquesArray)
+      {
+         armTorques.setTorquesToZero();
+      }
+   }
+}
