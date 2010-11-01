f15c77d5d0d36423ccc88b21f920b136fdc937d4
Jerry Pratt <jpratt@ihmc.us>
2010-11-01 17:25:20 +0000

    PROJECTM-207: RegularWalkingGait and SubControllers are moved to CommonWalkingControlModules http://jira.ihmc.us/browse/PROJECTM-207 Starting to merge the RegularWalkingGait state machine stuff.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllers/regularWalkingGait/RegularWalkingGaitAbstractController.java
@@ -0,0 +1,12 @@
+package us.ihmc.commonWalkingControlModules.controllers.regularWalkingGait;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+
+import com.yobotics.simulationconstructionset.util.statemachines.State;
+
+public class RegularWalkingGaitAbstractController
+{
+
+   
+   
+}
