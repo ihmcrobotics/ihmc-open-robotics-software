9c9ccc9a5280ad8dd8815db6f910a7cd93032959
Peter Neuhaus <pneuhaus@ihmc.us>
2010-11-01 20:31:07 +0000

    Moved AxisAnglePelvisOrientationControlModule

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
@@ -4,6 +4,7 @@ import us.ihmc.commonWalkingControlModules.RobotSide;
 import us.ihmc.utilities.math.geometry.FramePoint;
 import us.ihmc.utilities.math.geometry.FrameVector;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.screwTheory.Twist;
 
 public interface ProcessedSensorsInterface
 {
@@ -14,4 +15,5 @@ public interface ProcessedSensorsInterface
      public abstract FrameVector getCenterOfMassVelocityInFrame(ReferenceFrame referenceFrame);
      public abstract FramePoint getCenterOfMassPositionInFrame(ReferenceFrame referenceFrame);
      public abstract FrameVector getGravityInWorldFrame();
+     public abstract Twist computeTwistOfPelvisWithRespectToWorld();
 }
