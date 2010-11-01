86b3f05eaa9442ebab1b501814dd2970059dd02a
Jerry Pratt <jpratt@ihmc.us>
2010-11-01 19:01:57 +0000

    PROJECTM-207: RegularWalkingGait and SubControllers are moved to CommonWalkingControlModules http://jira.ihmc.us/browse/PROJECTM-207 Added UpperBodySubController to the M2V2 side and use DoNothing version. Added more stuff to the RegularWalkingGaitAbstractController.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllers/regularWalkingGait/RegularWalkingGaitAbstractController.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllers/regularWalkingGait/RegularWalkingGaitAbstractController.java
@@ -6,7 +6,23 @@ import com.yobotics.simulationconstructionset.util.statemachines.State;
 
 public class RegularWalkingGaitAbstractController
 {
+   protected final DoEveryTickSubController doEveryTickSubController;
+   protected final StanceSubController stanceSubController;
+   protected final SwingSubController swingSubController;
+   protected final UpperBodySubController upperBodySubController;
 
    
+   public RegularWalkingGaitAbstractController(
+         DoEveryTickSubController doEveryTickSubController,
+         StanceSubController stanceSubController,
+         SwingSubController swingSubController,
+         UpperBodySubController upperBodySubController
    
+   )
+   {
+      this.doEveryTickSubController = doEveryTickSubController;
+      this.stanceSubController = stanceSubController;
+      this.swingSubController = swingSubController;
+      this.upperBodySubController = upperBodySubController;
+   }
 }
