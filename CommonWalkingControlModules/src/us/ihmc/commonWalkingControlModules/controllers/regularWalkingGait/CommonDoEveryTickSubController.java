b7ee499fa0e1b755117f109a6f49c3b419436142
Jerry Pratt <jpratt@ihmc.us>
2010-11-04 22:24:19 +0000

    PROJECTM-211: M2V2 and R2 DoEveryTickSubControllers are nearly identical. http://jira.ihmc.us/browse/PROJECTM-211 Done. There is now only one CommonDoEveryTickSubController that M2V2 and R2 use.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllers/regularWalkingGait/CommonDoEveryTickSubController.java
@@ -0,0 +1,105 @@
+package us.ihmc.commonWalkingControlModules.controllers.regularWalkingGait;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.BipedFeetUpdater;
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.BipedFootInterface;
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.BipedSupportPolygons;
+import us.ihmc.commonWalkingControlModules.captureRegion.CapturePointCalculatorInterface;
+import us.ihmc.commonWalkingControlModules.captureRegion.CaptureRegionCalculator;
+import us.ihmc.commonWalkingControlModules.controllers.regularWalkingGait.DoEveryTickSubController;
+import us.ihmc.commonWalkingControlModules.couplingRegistry.CouplingRegistry;
+import us.ihmc.commonWalkingControlModules.desiredHeadingAndVelocity.DesiredHeadingControlModule;
+import us.ihmc.commonWalkingControlModules.desiredHeadingAndVelocity.DesiredVelocityControlModule;
+import us.ihmc.commonWalkingControlModules.desiredStepLocation.DesiredStepLocationCalculator;
+import us.ihmc.commonWalkingControlModules.desiredStepLocation.Footstep;
+import us.ihmc.commonWalkingControlModules.referenceFrames.CommonWalkingReferenceFrames;
+import us.ihmc.utilities.math.geometry.FrameConvexPolygon2d;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.FrameVector2d;
+
+public class CommonDoEveryTickSubController implements DoEveryTickSubController
+{
+   private final CommonWalkingReferenceFrames referenceFrames;
+
+   private final BipedFootInterface leftFoot;
+   private final BipedFootInterface rightFoot;
+   private final BipedFeetUpdater bipedFeetUpdater;
+   
+   private final DesiredHeadingControlModule desiredHeadingControlModule;
+   private final DesiredVelocityControlModule desiredVelocityControlModule;
+
+   private final DesiredStepLocationCalculator desiredStepLocationCalculator;
+
+   private final CapturePointCalculatorInterface capturePointCalculator;
+   private final CaptureRegionCalculator captureRegionCalculator;
+   private final CouplingRegistry couplingRegistry;
+
+   public CommonDoEveryTickSubController(CommonWalkingReferenceFrames referenceFrames, 
+         BipedFootInterface leftFoot, BipedFootInterface rightFoot,
+         BipedFeetUpdater bipedFeetUpdater, 
+         DesiredHeadingControlModule desiredHeadingControlModule,
+         DesiredVelocityControlModule desiredVelocityControlModule, 
+         DesiredStepLocationCalculator desiredStepLocationCalculator,
+         CapturePointCalculatorInterface capturePointCalculator, 
+         CaptureRegionCalculator captureRegionCalculator, 
+         CouplingRegistry couplingRegistry)
+   {
+      this.referenceFrames = referenceFrames;
+
+      this.leftFoot = leftFoot;
+      this.rightFoot = rightFoot;
+      this.bipedFeetUpdater = bipedFeetUpdater;
+      
+      this.desiredHeadingControlModule = desiredHeadingControlModule;
+      this.desiredVelocityControlModule = desiredVelocityControlModule;
+      this.desiredStepLocationCalculator = desiredStepLocationCalculator;
+
+      this.capturePointCalculator = capturePointCalculator;
+      this.captureRegionCalculator = captureRegionCalculator;
+      this.couplingRegistry = couplingRegistry;
+   }
+
+
+   public void doEveryControlTick(RobotSide supportLeg)
+   {
+      FrameVector2d desiredVelocity = desiredVelocityControlModule.getDesiredVelocity();
+      couplingRegistry.setDesiredVelocity(desiredVelocity);
+      desiredHeadingControlModule.updateDesiredHeadingFrame();
+
+      // Compute the instantaneous capture point.
+      capturePointCalculator.computeCapturePoint(supportLeg);
+      FramePoint capturePointInMidfeetZUp = capturePointCalculator.getCapturePointInFrame(referenceFrames.getMidFeetZUpFrame());
+      couplingRegistry.setCapturePoint(capturePointInMidfeetZUp);
+
+      boolean forceHindOnToes = false;
+//    boolean forceHindOnToes = couplingRegistry.getForceHindOnToes();
+
+      bipedFeetUpdater.updateBipedFeet(leftFoot, rightFoot, supportLeg, capturePointInMidfeetZUp, forceHindOnToes);
+      
+      BipedSupportPolygons bipedSupportPolygons = couplingRegistry.getBipedSupportPolygons();
+      bipedSupportPolygons.update(leftFoot, rightFoot);
+
+      if (supportLeg != null)
+      {
+         // TODO: also compute capture regions in double support
+         
+         FrameConvexPolygon2d supportFoot = bipedSupportPolygons.getFootPolygonInAnkleZUp(supportLeg);
+         FrameConvexPolygon2d captureRegion = captureRegionCalculator.calculateCaptureRegion(supportLeg, supportFoot,
+                                                 couplingRegistry.getEstimatedSwingTimeRemaining());
+         couplingRegistry.setCaptureRegion(captureRegion);
+
+//       ReferenceFrame desiredHeadingFrame = desiredHeadingControlModule.getDesiredHeadingFrame();
+         Footstep desiredStepLocation = desiredStepLocationCalculator.computeDesiredStepLocation(supportLeg, bipedSupportPolygons, captureRegion);
+         
+//       Footstep desiredStepLocation = desiredStepLocationCalculator.getDesiredStepLocation(supportLeg.getOppositeSide(), couplingRegistry);
+         couplingRegistry.setDesiredStepLocation(desiredStepLocation);
+      }
+      else
+      {
+         captureRegionCalculator.hideCaptureRegion();
+         couplingRegistry.setCaptureRegion(null);
+         couplingRegistry.setDesiredStepLocation(null);
+      }
+   }
+}
+
