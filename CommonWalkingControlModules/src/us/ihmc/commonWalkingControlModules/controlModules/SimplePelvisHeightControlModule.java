d4b3bfb5e3c2754bb8f516399232ed038d5bafec
Twan Koolen <tkoolen@ihmc.us>
2010-11-09 20:42:24 +0000

    Moved SimplePelvisHeightControlModule and related to common.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/SimplePelvisHeightControlModule.java
@@ -0,0 +1,83 @@
+package us.ihmc.commonWalkingControlModules.controlModules;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.commonWalkingControlModules.controlModuleInterfaces.PelvisHeightControlModule;
+import us.ihmc.commonWalkingControlModules.controllers.PDController;
+import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class SimplePelvisHeightControlModule implements PelvisHeightControlModule
+{
+   private final boolean DO_STANCE_HEIGHT_CONTROL = false;
+   private final ProcessedSensorsInterface processedSensors;
+
+   private final YoVariableRegistry registry = new YoVariableRegistry("PelvisHeightContro");
+
+   private final DoubleYoVariable fZ = new DoubleYoVariable("fZ", "Total fZ on the pelvis from the Height Controller", registry);
+   private final DoubleYoVariable fZExtra = new DoubleYoVariable("fZExtra", "Extra fZ to make sure leg is straight during stance", registry);
+   private final DoubleYoVariable fZHeight = new DoubleYoVariable("fZHeight", "fZ for stance height control effort", registry);
+   private final DoubleYoVariable stanceHeightDes = new DoubleYoVariable("stanceHeightDes", registry);
+   private final PDController stanceHeightPDcontroller;
+
+   private final StanceHeightCalculator stanceHeightCalculator;
+
+
+   public SimplePelvisHeightControlModule(ProcessedSensorsInterface processedSensors, StanceHeightCalculator stanceHeightCalculator, YoVariableRegistry parentRegistry)
+   {
+      this.processedSensors = processedSensors;
+      this.stanceHeightCalculator = stanceHeightCalculator;
+      this.stanceHeightPDcontroller = new PDController("stanceHeight", registry);
+
+      parentRegistry.addChild(registry);
+
+   }
+
+   public double doPelvisHeightControl(double desiredPelvisHeightInWorld, RobotSide supportLeg)
+   {
+      if (DO_STANCE_HEIGHT_CONTROL)
+      {
+         if (supportLeg == null)
+         {
+            stanceHeightCalculator.getStanceHeightUsingBothFeet();
+            fZHeight.set(0.0);
+         }
+         else
+         {
+            double stanceHeight = stanceHeightCalculator.getStanceHeightUsingOneFoot(supportLeg);
+            fZHeight.set(stanceHeightPDcontroller.compute(stanceHeight, stanceHeightDes.getDoubleValue(), 0.0, 0.0));
+         }
+      }
+      else
+         fZHeight.set(0.0);
+
+      double gravityZ = processedSensors.getGravityInWorldFrame().getZ();
+      double totalMass = processedSensors.getTotalMass();
+
+      double fZDueToGravity = -gravityZ * totalMass;
+
+      fZ.set(fZDueToGravity + fZExtra.getDoubleValue() + fZHeight.getDoubleValue());
+
+      return fZ.getDoubleValue();
+   }
+   
+   public void setParametersForR2()
+   {
+      stanceHeightPDcontroller.setProportionalGain(1000.0);
+      stanceHeightPDcontroller.setDerivativeGain(10.0);
+
+      fZExtra.set(100.0);    // 80.0;
+      stanceHeightDes.set(1.03);
+   }
+   
+   public void setParametersForM2V2()
+   {
+      // TODO: tune
+      stanceHeightPDcontroller.setProportionalGain(1000.0);
+      stanceHeightPDcontroller.setDerivativeGain(10.0);
+
+      fZExtra.set(100.0);    // 80.0;
+      stanceHeightDes.set(1.03);
+   }
+}
--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
@@ -10,18 +10,25 @@ import us.ihmc.utilities.screwTheory.Twist;
 
 public interface ProcessedSensorsInterface
 {
-     public abstract double getTime();
-     
-     public abstract double getKneeAngle(RobotSide robotSide);
-     
-     public abstract FrameVector getCenterOfMassVelocityInFrame(ReferenceFrame referenceFrame);
-     public abstract FramePoint getCenterOfMassPositionInFrame(ReferenceFrame referenceFrame);
-     public abstract FrameVector getGravityInWorldFrame();
-     public abstract Twist computeTwistOfPelvisWithRespectToWorld();
-     public abstract Orientation getPelvisOrientationInFrame(ReferenceFrame referenceFrame);
-     public abstract FramePoint getCenterOfMassGroundProjectionInFrame(ReferenceFrame referenceFrame);
+   public abstract double getTime();
+
+   public abstract double getKneeAngle(RobotSide robotSide);
+
+   public abstract FrameVector getCenterOfMassVelocityInFrame(ReferenceFrame referenceFrame);
+
+   public abstract FramePoint getCenterOfMassPositionInFrame(ReferenceFrame referenceFrame);
+
+   public abstract FrameVector getGravityInWorldFrame();
+
+   public abstract Twist computeTwistOfPelvisWithRespectToWorld();
+
+   public abstract Orientation getPelvisOrientationInFrame(ReferenceFrame referenceFrame);
+
+   public abstract FramePoint getCenterOfMassGroundProjectionInFrame(ReferenceFrame referenceFrame);
 
    public abstract double getLegJointVelocity(RobotSide robotSide, LegJointName legJointName);
 
    public abstract double getLegJointPosition(RobotSide robotSide, LegJointName legJointName);
+
+   public abstract double getTotalMass();
 }
