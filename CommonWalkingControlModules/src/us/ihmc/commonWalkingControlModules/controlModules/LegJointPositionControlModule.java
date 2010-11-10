0663cb532f76561c71423a1848ad63a8e937ec2b
Jerry Pratt <jpratt@ihmc.us>
2010-11-10 15:52:39 +0000

    PROJECTM-214: Moved LegJointPositionControlModule to CommonWalking.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/LegJointPositionControlModule.java
@@ -0,0 +1,327 @@
+package us.ihmc.commonWalkingControlModules.controlModules;
+
+import java.util.EnumMap;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.commonWalkingControlModules.controllers.PIDController;
+import us.ihmc.commonWalkingControlModules.outputs.ProcessedOutputsInterface;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegJointName;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegJointPositions;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegJointVelocities;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegTorques;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.RobotSpecificJointNames;
+import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
+import us.ihmc.utilities.containers.ContainerTools;
+import us.ihmc.utilities.math.MathTools;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class LegJointPositionControlModule
+{
+   private final boolean useDesiredVelocities;
+   private boolean doDerivativeControlInThisModule = true;
+
+   private final RobotSide robotSide;
+   private final LegJointName[] legJointNames;
+
+   private final ProcessedSensorsInterface processedSensors;
+   private final YoVariableRegistry registry;
+
+   private final DoubleYoVariable jacobianDeterminant;
+   private final DoubleYoVariable desiredVelocityScale;
+
+   private final EnumMap<LegJointName, DoubleYoVariable> kpGains = ContainerTools.createEnumMap(LegJointName.class);
+   private final EnumMap<LegJointName, DoubleYoVariable> kdGains = ContainerTools.createEnumMap(LegJointName.class);
+   private final EnumMap<LegJointName, DoubleYoVariable> jointPositionErrors = ContainerTools.createEnumMap(LegJointName.class);
+   private final EnumMap<LegJointName, DoubleYoVariable> jointVelocityErrors = ContainerTools.createEnumMap(LegJointName.class);
+   private final EnumMap<LegJointName, DoubleYoVariable> desiredJointPositions = ContainerTools.createEnumMap(LegJointName.class);
+   private final EnumMap<LegJointName, DoubleYoVariable> desiredJointVelocities = ContainerTools.createEnumMap(LegJointName.class);
+
+   private final DoubleYoVariable legGainScale, ankleTorqueScale;
+
+   private static final String kpPrefix = "kp";
+   private static final String kdPrefix = "kd";
+   private static final String positionErrorPrefix = "jointPosErr";
+   private static final String velocityErrorPrefix = "jointVelErr";
+   private static final String desiredPositionPrefix = "desiredJointPos";
+   private static final String desiredVelocityPrefix = "desiredJointVel";
+
+   private final DoubleYoVariable softScaleFactor;
+
+   private final EnumMap<LegJointName, PIDController> controllers = ContainerTools.createEnumMap(LegJointName.class);
+
+   private final double controlDT;
+
+   private final ProcessedOutputsInterface processedOutputs;
+
+   public LegJointPositionControlModule(RobotSpecificJointNames robotJointNames, ProcessedSensorsInterface processedSensors,
+           ProcessedOutputsInterface processedOutputs, YoVariableRegistry yoVariableParentRegistry, RobotSide robotSide, double controlDT,
+           boolean useDesiredVelocities)
+   {
+      this.legJointNames = robotJointNames.getLegJointNames();
+      this.robotSide = robotSide;
+      this.processedSensors = processedSensors;
+      this.controlDT = controlDT;
+      this.useDesiredVelocities = useDesiredVelocities;
+      this.processedOutputs = processedOutputs;
+
+      registry = new YoVariableRegistry("LegJointPositionControlModule - " + robotSide.getCamelCaseNameForStartOfExpression());
+
+      String robotSideStartString = robotSide.getCamelCaseNameForStartOfExpression();
+      String robotSideMiddleString = robotSide.getCamelCaseNameForMiddleOfExpression();
+
+      jacobianDeterminant = new DoubleYoVariable(robotSideStartString + "JacobianDeterminant", registry);
+      desiredVelocityScale = new DoubleYoVariable(robotSideStartString + "DesiredVelocityScale", registry);
+
+      for (LegJointName legJointName : legJointNames)
+      {
+         kpGains.put(legJointName,
+                     new DoubleYoVariable(kpPrefix + robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(),
+                                          "Position control gain for joint " + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
+
+         kdGains.put(legJointName,
+                     new DoubleYoVariable(kdPrefix + robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(),
+                                          "Damping control gain for joint " + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
+
+         jointPositionErrors.put(legJointName,
+                                 new DoubleYoVariable(positionErrorPrefix + robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(),
+                                    "Position error for joint " + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
+
+         jointVelocityErrors.put(legJointName,
+                                 new DoubleYoVariable(velocityErrorPrefix + robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(),
+                                    "Velocity error for joint " + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
+
+         desiredJointPositions.put(legJointName,
+                                   new DoubleYoVariable(desiredPositionPrefix + robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(),
+                                      "Desired position for joint " + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
+
+         desiredJointVelocities.put(legJointName,
+                                    new DoubleYoVariable(desiredVelocityPrefix + robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(),
+                                       "Desired velocity for joint " + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
+
+         controllers.put(legJointName, new PIDController(robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
+      }
+
+      legGainScale = new DoubleYoVariable(robotSideStartString + "AnkleGainScale", "", registry);
+      ankleTorqueScale = new DoubleYoVariable(robotSideStartString + "AnkleTorqueScale", "", registry);
+      softScaleFactor = new DoubleYoVariable(robotSideStartString + "SoftScaleFactor", "This is the scale factor on ankle kp.", registry);
+
+      // Set default gains to NaN to ensure that the user calls one of the
+      // default gain setters. If not, should get NaN errors down the road.
+      setDefaultGainsToNaN();
+      resetScalesToDefault();
+
+      yoVariableParentRegistry.addChild(registry);
+   }
+
+   private void setDefaultGainsToNaN()
+   {
+      for (LegJointName legJointName : legJointNames)
+      {
+         kpGains.get(legJointName).set(Double.NaN);
+         kdGains.get(legJointName).set(Double.NaN);
+
+         softScaleFactor.set(Double.NaN);
+      }
+
+   }
+
+   public void setDefaultGainsForM2V2()
+   {
+      doDerivativeControlInThisModule = false;
+
+      kpGains.get(LegJointName.HIP_YAW).set(120.0);
+      kpGains.get(LegJointName.HIP_ROLL).set(200.0);
+      kpGains.get(LegJointName.HIP_PITCH).set(200.0);
+      kpGains.get(LegJointName.KNEE).set(120.0);
+      kpGains.get(LegJointName.ANKLE_ROLL).set(8.0);
+      kpGains.get(LegJointName.ANKLE_PITCH).set(8.0);
+
+      kdGains.get(LegJointName.HIP_YAW).set(2.0);
+      kdGains.get(LegJointName.HIP_ROLL).set(8.0);
+      kdGains.get(LegJointName.HIP_PITCH).set(8.0);
+      kdGains.get(LegJointName.KNEE).set(6.0);
+      kdGains.get(LegJointName.ANKLE_ROLL).set(1.0);
+      kdGains.get(LegJointName.ANKLE_PITCH).set(1.0);
+
+      softScaleFactor.set(0.25);
+   }
+
+   public void setDefaultGainsForR2()
+   {
+      doDerivativeControlInThisModule = true;
+
+      // kpGains.get(LegJointName.HIP_YAW).val = 5000.0;    // 1000.0;
+      // kpGains.get(LegJointName.HIP_ROLL).val = 5000.0;    // 1000.0;
+      // kpGains.get(LegJointName.HIP_PITCH).val = 5000.0;    // 1000.0;
+      // kpGains.get(LegJointName.KNEE).val = 2500.0;    // 1000.0;
+      // kpGains.get(LegJointName.ANKLE_PITCH).val = 50.0;    // 20.0;
+      // kpGains.get(LegJointName.ANKLE_ROLL).val = 50.0;    // 20.0;
+      //
+      // kdGains.get(LegJointName.HIP_YAW).val = 50.0;    // 100.0;
+      // kdGains.get(LegJointName.HIP_ROLL).val = 50.0;    // 100.0;
+      // kdGains.get(LegJointName.HIP_PITCH).val = 50;    // 100.0;
+      // kdGains.get(LegJointName.KNEE).val = 50.0;    // 100.0;
+      // kdGains.get(LegJointName.ANKLE_PITCH).val = 5.0;    // 2.0;//5.0;
+      // kdGains.get(LegJointName.ANKLE_ROLL).val = 5.0;    // 2.0;
+
+      kpGains.get(LegJointName.HIP_YAW).set(1400.0);    // 1000.0;
+      kpGains.get(LegJointName.HIP_ROLL).set(1400.0);    // 1000.0;
+      kpGains.get(LegJointName.HIP_PITCH).set(1400.0);    // 1000.0;
+      kpGains.get(LegJointName.KNEE).set(1000.0);    // 1000.0;    // 1000.0;
+      kpGains.get(LegJointName.ANKLE_ROLL).set(10.0);    // 100.0;    // 20.0;
+      kpGains.get(LegJointName.ANKLE_PITCH).set(10.0);    // 250.0;    // 20.0;
+
+      kdGains.get(LegJointName.HIP_YAW).set(50.0);    // 100.0;
+      kdGains.get(LegJointName.HIP_ROLL).set(50.0);    // 100.0;
+      kdGains.get(LegJointName.HIP_PITCH).set(50.0);    // 100.0;
+      kdGains.get(LegJointName.KNEE).set(50.0);    // 100.0;    // 100.0;
+      kdGains.get(LegJointName.ANKLE_ROLL).set(1.0);    // 3.0); // 10.0;    // 2.0;
+      kdGains.get(LegJointName.ANKLE_PITCH).set(3.0);    // 20.0;    // 2.0;//5.0;
+
+      softScaleFactor.set(0.25);    // (0.05);
+   }
+
+   public void packTorquesForLegJointsPositionControl(LegTorques legTorquesToPackForSwingLeg, LegJointPositions desiredLegJointPositions,
+           LegJointVelocities desiredLegJointVelocities, double jacobianDeterminant)
+   {
+      // RobotSide checks
+      legTorquesToPackForSwingLeg.getRobotSide().checkRobotSideMatch(desiredLegJointPositions.getRobotSide());
+      legTorquesToPackForSwingLeg.getRobotSide().checkRobotSideMatch(robotSide);
+
+      this.jacobianDeterminant.set(jacobianDeterminant);
+
+      if (desiredLegJointVelocities == null)
+      {
+         desiredLegJointVelocities = new LegJointVelocities(legJointNames, robotSide);
+         desiredLegJointVelocities.setJointVelocitiesToNAN();
+      }
+
+      for (LegJointName legJointName : legJointNames)
+      {
+         desiredJointPositions.get(legJointName).set(desiredLegJointPositions.getJointPosition(legJointName));
+         desiredJointVelocities.get(legJointName).set(desiredLegJointVelocities.getJointVelocity(legJointName));
+
+         double actualJointPosition = processedSensors.getLegJointPosition(robotSide, legJointName);
+
+         double minJacobianDeterminant = 0.03;
+         desiredVelocityScale.set((Math.abs(jacobianDeterminant) - minJacobianDeterminant) / minJacobianDeterminant);    // avoid singularity problem
+         desiredVelocityScale.set(MathTools.clipToMinMax(desiredVelocityScale.getDoubleValue(), 0.0, 1.0));
+
+         double kpGain = kpGains.get(legJointName).getDoubleValue() * legGainScale.getDoubleValue();
+         double kdGain = kdGains.get(legJointName).getDoubleValue() * legGainScale.getDoubleValue();    // * kdScale.getDoubleValue();
+
+         controllers.get(legJointName).setProportionalGain(kpGain);
+         controllers.get(legJointName).setDerivativeGain(kdGain);
+
+         double torque;
+         if (doDerivativeControlInThisModule)
+         {
+            double desiredJointVelocity = useDesiredVelocities
+                                          ? desiredJointVelocities.get(legJointName).getDoubleValue() * desiredVelocityScale.getDoubleValue() : 0.0;
+
+            double actualJointVelocity = processedSensors.getLegJointVelocity(robotSide, legJointName);
+
+            torque = controllers.get(legJointName).compute(actualJointPosition, desiredJointPositions.get(legJointName).getDoubleValue(), actualJointVelocity,
+                                     desiredJointVelocity, controlDT);
+
+         }
+         else
+         {
+            processedOutputs.setDamping(robotSide, legJointName, kdGains.get(legJointName).getDoubleValue());
+            double desiredJointVelocity = useDesiredVelocities
+                                          ? desiredJointVelocities.get(legJointName).getDoubleValue() * desiredVelocityScale.getDoubleValue() : 0.0;
+            processedOutputs.setDesiredJointVelocity(robotSide, legJointName, desiredJointVelocity);
+
+            // double torque = controllers.get(legJointName).compute(actualJointPosition, desiredJointPositions.get(legJointName).getDoubleValue(), actualJointVelocity,
+            // desiredJointVelocity, controlDT); // if this were called in the low level loop
+
+            torque = controllers.get(legJointName).compute(actualJointPosition, desiredJointPositions.get(legJointName).getDoubleValue(), 0.0, 0.0, controlDT);    // don't do derivative stuff here; that's for the low level loop
+
+         }
+
+         legTorquesToPackForSwingLeg.setTorque(legJointName, torque);
+      }
+
+      // For soft touchdowns reduce the ankle torques more...
+      legTorquesToPackForSwingLeg.setTorque(LegJointName.ANKLE_PITCH,
+              legTorquesToPackForSwingLeg.getTorque(LegJointName.ANKLE_PITCH) * ankleTorqueScale.getDoubleValue());
+
+      return;
+   }
+
+   public void setProportionalGain(LegJointName legJointName, double proportionalGain)
+   {
+      kpGains.get(legJointName).set(proportionalGain);
+   }
+
+   public void setDerivativeGain(LegJointName legJointName, double derivativeGain)
+   {
+      kdGains.get(legJointName).set(derivativeGain);
+   }
+
+   public void setAnkleGainsSoft()
+   {
+      ankleTorqueScale.set(softScaleFactor.getDoubleValue());
+   }
+
+   public void scaleLegGains(double scaleFactor)
+   {
+      scaleFactor = MathTools.clipToMinMax(scaleFactor, 0.0, 1.0);
+      legGainScale.set(scaleFactor);
+   }
+
+   public void resetScalesToDefault()
+   {
+      legGainScale.set(1.0);
+      ankleTorqueScale.set(1.0);
+   }
+
+
+   public void setupGUI(SimulationConstructionSet scs)
+   {
+      int numberOfLegJointNames = legJointNames.length;
+
+      String[][][] positionGraphGroupStrings = new String[numberOfLegJointNames][][];
+      String[][][] velocityGraphGroupStrings = new String[numberOfLegJointNames][][];
+
+      String[] entryBoxGroupStrings = new String[2 * numberOfLegJointNames];    // times two, because we want both kp and kd
+
+      for (int jointNameIndex = 0; jointNameIndex < numberOfLegJointNames; jointNameIndex++)
+      {
+         LegJointName jointName = legJointNames[jointNameIndex];
+
+         // positions
+         String desiredPositionName = desiredJointPositions.get(jointName).getName();
+         String actualPositionName = processedSensors.getLegJointPositionName(robotSide, jointName);
+
+         positionGraphGroupStrings[jointNameIndex] = new String[][]
+         {
+            {desiredPositionName, actualPositionName}, {"auto"}
+         };
+
+         // velocities
+         String desiredVelocityName = desiredJointVelocities.get(jointName).getName();
+         String actualVelocityName = processedSensors.getLegJointVelocityName(robotSide, jointName);
+
+         velocityGraphGroupStrings[jointNameIndex] = new String[][]
+         {
+            {desiredVelocityName, actualVelocityName}, {"auto"}
+         };
+
+         // kp, kd
+         entryBoxGroupStrings[jointNameIndex] = kpGains.get(jointName).getName();
+         entryBoxGroupStrings[numberOfLegJointNames + jointNameIndex] = kdGains.get(jointName).getName();
+      }
+
+      String sideString = "(" + robotSide.getCamelCaseNameForStartOfExpression() + ")";
+      scs.setupGraphGroup("LegJointPositionControlModule - positions " + sideString, positionGraphGroupStrings, 2);
+      scs.setupGraphGroup("LegJointPositionControlModule - velocities " + sideString, velocityGraphGroupStrings, 2);
+      scs.setupEntryBoxGroup("LegJointPositionControlModule", entryBoxGroupStrings);
+      scs.setupConfiguration("LegJointPositionControlModule", "all", "LegJointPositionControlModule - velocities", "LegJointPositionControlModule");
+   }
+
+}
+
