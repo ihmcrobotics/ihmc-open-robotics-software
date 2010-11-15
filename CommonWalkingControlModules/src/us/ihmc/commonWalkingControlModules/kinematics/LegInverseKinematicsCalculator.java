d468ade40fcec0022dae0a3f74654440f67eddcb
Twan Koolen <tkoolen@ihmc.us>
2010-11-15 18:41:44 +0000

    Made a common LegInverseKinematicsCalculator interface, which has implementations in both YoboticsBiped and R2Sim02. Now passing a LegInverseKinematicsCalculator into the ctors of the swing subcontrollers.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/kinematics/LegInverseKinematicsCalculator.java
@@ -0,0 +1,12 @@
+package us.ihmc.commonWalkingControlModules.kinematics;
+
+import javax.media.j3d.Transform3D;
+
+import us.ihmc.commonWalkingControlModules.RobotSide;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegJointPositions;
+
+public interface LegInverseKinematicsCalculator
+{
+   public abstract void solve(LegJointPositions legJointPositionsToPack, Transform3D footToPelvis, RobotSide robotSide, double desiredHipYaw);
+
+}
