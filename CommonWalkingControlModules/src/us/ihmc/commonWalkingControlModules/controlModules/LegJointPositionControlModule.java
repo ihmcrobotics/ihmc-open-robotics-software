78faf12177d47fd3dd8e0f2b1f46833d61afe413
Twan Koolen <tkoolen@ihmc.us>
2011-01-20 18:18:15 +0000

    Now computing swing foot position error

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/LegJointPositionControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/LegJointPositionControlModule.java
@@ -105,7 +105,7 @@ public class LegJointPositionControlModule
          controllers.put(legJointName, new PIDController(robotSideMiddleString + legJointName.getCamelCaseNameForMiddleOfExpression(), registry));
       }
 
-      legGainScale = new DoubleYoVariable(robotSideStartString + "AnkleGainScale", "", registry);
+      legGainScale = new DoubleYoVariable(robotSideStartString + "LegGainScale", "", registry);
       ankleTorqueScale = new DoubleYoVariable(robotSideStartString + "AnkleTorqueScale", "", registry);
       softScaleFactor = new DoubleYoVariable(robotSideStartString + "SoftScaleFactor", "This is the scale factor on ankle kp.", registry);
 
@@ -138,7 +138,7 @@ public class LegJointPositionControlModule
    {
       doDerivativeControlInThisModule = false;
 
-      kpGains.get(LegJointName.HIP_YAW).set(120.0);
+      kpGains.get(LegJointName.HIP_YAW).set(300.0); // 120.0);
       kpGains.get(LegJointName.HIP_ROLL).set(200.0);
       kpGains.get(LegJointName.HIP_PITCH).set(200.0);
       kpGains.get(LegJointName.KNEE).set(120.0);
