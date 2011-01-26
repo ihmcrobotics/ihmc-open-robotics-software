70aaf162b0279ca8937f006fe8ec0901fc8c36a1
Twan Koolen <tkoolen@ihmc.us>
2011-01-26 01:18:32 +0000

    Added filtering to fZ in order to prepare for one more try at doing the swing leg compensation properly.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/SimplePelvisHeightControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/SimplePelvisHeightControlModule.java
@@ -7,6 +7,7 @@ import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
 
 import com.yobotics.simulationconstructionset.DoubleYoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.math.filter.AlphaFilteredYoVariable;
 
 public class SimplePelvisHeightControlModule implements PelvisHeightControlModule
 {
@@ -15,21 +16,24 @@ public class SimplePelvisHeightControlModule implements PelvisHeightControlModul
 
    private final YoVariableRegistry registry = new YoVariableRegistry("PelvisHeightControlModule");
 
-   private final DoubleYoVariable fZ = new DoubleYoVariable("fZ", "Total fZ on the pelvis from the Height Controller", registry);
+   private final DoubleYoVariable alphaFz = new DoubleYoVariable("alphaFz", registry);
+   private final AlphaFilteredYoVariable fZ = new AlphaFilteredYoVariable("fZ", registry, alphaFz);
    private final DoubleYoVariable fZExtra = new DoubleYoVariable("fZExtra", "Extra fZ to make sure leg is straight during stance", registry);
    private final DoubleYoVariable fZHeight = new DoubleYoVariable("fZHeight", "fZ for stance height control effort", registry);
    private final DoubleYoVariable stanceHeightDes = new DoubleYoVariable("stanceHeightDes", registry);
    private final PDController stanceHeightPDcontroller;
 
    private final StanceHeightCalculator stanceHeightCalculator;
+   private double controlDT;
 
 
    public SimplePelvisHeightControlModule(ProcessedSensorsInterface processedSensors, StanceHeightCalculator stanceHeightCalculator,
-           YoVariableRegistry parentRegistry)
+           YoVariableRegistry parentRegistry, double dt)
    {
       this.processedSensors = processedSensors;
       this.stanceHeightCalculator = stanceHeightCalculator;
       this.stanceHeightPDcontroller = new PDController("stanceHeight", registry);
+      this.controlDT = dt;
 
       parentRegistry.addChild(registry);
 
@@ -72,7 +76,9 @@ public class SimplePelvisHeightControlModule implements PelvisHeightControlModul
       stanceHeightPDcontroller.setProportionalGain(1000.0);
       stanceHeightPDcontroller.setDerivativeGain(10.0);
 
-      fZExtra.set(50.0); // 0.0);
+      fZExtra.set(50.0);    // 0.0);
       stanceHeightDes.set(0.95);
+
+      alphaFz.set(AlphaFilteredYoVariable.computeAlphaGivenBreakFrequencyProperly(10.0, controlDT));
    }
 }
