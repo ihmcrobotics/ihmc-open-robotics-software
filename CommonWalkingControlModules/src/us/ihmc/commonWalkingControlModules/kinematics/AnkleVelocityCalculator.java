c528b95f2ca155fab20b8922cb7f9af3a5cb583d
Twan Koolen <tkoolen@ihmc.us>
2011-02-01 18:43:32 +0000

    Used regular expression ^[\s\w]*[^=]changeFrameCopy
    to find places where we call changeFrameCopy, but do nothing with the result. The regexp is conservative.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/kinematics/AnkleVelocityCalculator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/kinematics/AnkleVelocityCalculator.java
@@ -7,9 +7,7 @@ import us.ihmc.commonWalkingControlModules.RobotSide;
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegJointName;
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegJointVelocities;
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.RobotSpecificJointNames;
-import us.ihmc.commonWalkingControlModules.referenceFrames.CommonWalkingReferenceFrames;
 import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
-import us.ihmc.utilities.math.geometry.FramePoint;
 import us.ihmc.utilities.math.geometry.FrameVector;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.screwTheory.Twist;
@@ -17,27 +15,22 @@ import us.ihmc.utilities.screwTheory.Twist;
 public class AnkleVelocityCalculator
 {
    private final ProcessedSensorsInterface processedSensors;
-   private final CommonWalkingReferenceFrames referenceFrames;
    private final RobotSide robotSide;
    private final SwingFullLegJacobian swingLegJacobian;
    private final LegJointName[] legJointNames;
 
-   public AnkleVelocityCalculator(RobotSpecificJointNames robotJointNames, ProcessedSensorsInterface processedSensors, CommonWalkingReferenceFrames referenceFrames,
-                                  RobotSide robotSide, SwingFullLegJacobian swingLegJacobian)
+   public AnkleVelocityCalculator(RobotSpecificJointNames robotJointNames, ProcessedSensorsInterface processedSensors, RobotSide robotSide,
+                                  SwingFullLegJacobian swingLegJacobian)
    {
       this.legJointNames = robotJointNames.getLegJointNames();
 
       this.processedSensors = processedSensors;
-      this.referenceFrames = referenceFrames;
       this.robotSide = robotSide;
       this.swingLegJacobian = swingLegJacobian;
    }
 
    public FrameVector getAnkleVelocityInWorldFrame()
    {
-      FramePoint footOrigin = new FramePoint(referenceFrames.getFootFrame(robotSide));
-      footOrigin.changeFrameCopy(referenceFrames.getPelvisFrame());
-
       swingLegJacobian.computeJacobian();    // TODO: doing this twice, but ok.
       LegJointVelocities legJointVelocities = getLegJointVelocities();
       Twist twistOfFootWithRespectToBody = swingLegJacobian.getTwistOfFootWithRespectToPelvisInFootFrame(legJointVelocities);
