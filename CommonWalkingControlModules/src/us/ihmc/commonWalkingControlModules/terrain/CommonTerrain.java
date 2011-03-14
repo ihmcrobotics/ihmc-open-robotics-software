a077d2f13e94bed87e232fc80419364bba1b9a3b
Jerry Pratt <jpratt@ihmc.us>
2011-03-14 19:52:37 +0000

    Moving stepping stones and terrain stuff to common walking.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/terrain/CommonTerrain.java
@@ -0,0 +1,448 @@
+package us.ihmc.commonWalkingControlModules.terrain;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Transform3D;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.vecmath.Point2d;
+
+import us.ihmc.utilities.math.geometry.ConvexPolygon2d;
+
+import com.yobotics.simulationconstructionset.GroundProfile;
+import com.yobotics.simulationconstructionset.LinkGraphics;
+import com.yobotics.simulationconstructionset.TexturedGround;
+import com.yobotics.simulationconstructionset.YoAppearance;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.FlatGroundProfile;
+import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicObjectsListRegistry;
+import com.yobotics.simulationconstructionset.util.ground.AlternatingSlopesGroundProfile;
+import com.yobotics.simulationconstructionset.util.ground.BumpyGroundProfile;
+import com.yobotics.simulationconstructionset.util.ground.GroundProfileFromFile;
+import com.yobotics.simulationconstructionset.util.ground.ObstacleCourse;
+import com.yobotics.simulationconstructionset.util.ground.RandomRockyGroundProfile;
+import com.yobotics.simulationconstructionset.util.ground.RollingGroundProfile;
+import com.yobotics.simulationconstructionset.util.ground.steppingStones.SteppingStone;
+import com.yobotics.simulationconstructionset.util.ground.steppingStones.SteppingStones;
+import com.yobotics.simulationconstructionset.util.ground.steppingStones.SteppingStonesGroundProfile;
+
+public class CommonTerrain
+{
+   private SteppingStones steppingStones;
+   private GroundProfile groundProfile;
+   
+   public CommonTerrain(TerrainType terrainType, YoVariableRegistry registry)
+   {
+      setUpTerrain(terrainType, registry);
+      this.steppingStones = null;
+   }
+   
+   public CommonTerrain(ConvexPolygon2d footPolygon, SteppingStonePattern steppingStonePattern, boolean useSteppingStonesGroundModel, YoVariableRegistry registry, DynamicGraphicObjectsListRegistry dynamicGraphicObjectsListRegistry)
+   {
+      setUpSteppingStones(footPolygon, steppingStonePattern, useSteppingStonesGroundModel, registry);
+      
+      if (dynamicGraphicObjectsListRegistry != null)
+      {
+         System.out.println("Registering Stepping Stones Artifact");
+
+         SteppingStonesArtifact steppingStonesArtifact = new SteppingStonesArtifact("SteppingStones", steppingStones, Color.magenta, Color.GREEN);
+         dynamicGraphicObjectsListRegistry.registerArtifact("Stepping Stones", steppingStonesArtifact);
+      }   
+   }
+   
+   public GroundProfile getGroundProfile()
+   {
+      return groundProfile;
+   }
+   
+   public SteppingStones getSteppingStones()
+   {
+      return steppingStones;
+   }
+   
+   public BranchGroup createBranchGroup(boolean drawGroundBelow)
+   {
+      if (steppingStones != null)
+      {
+         BranchGroup branchGroup = steppingStones.createBranchGroup();
+
+         if (drawGroundBelow)
+         {
+            LinkGraphics groundBelow = new LinkGraphics();
+            groundBelow.translate(0.0, 0.0, -0.2);
+            groundBelow.addCube(10.0, 10.0, 0.1, YoAppearance.Green());
+
+            javax.media.j3d.Link link = new javax.media.j3d.Link(groundBelow.getSharedGroup());
+            branchGroup.addChild(link);
+         }
+
+         return branchGroup;
+      }
+
+      if (groundProfile != null)
+      {
+         BranchGroup branchGroup = new BranchGroup();
+         TexturedGround texturedGround = new TexturedGround(groundProfile, 300, YoAppearance.Green(), new Transform3D());
+         branchGroup.addChild(texturedGround.getBranchGroup());
+
+         return branchGroup;
+      }
+      
+      throw new RuntimeException("Both steppingStones and groundProfile are null!");
+      
+   }
+   
+   
+   public static final double FLAT_HEIGHT = 0.0;
+   
+   private void setUpSteppingStones(ConvexPolygon2d footPolygon, 
+         SteppingStonePattern steppingStonePattern,
+         boolean useSteppingStonesGroundModel,
+         YoVariableRegistry registry)
+   {
+      
+      switch (steppingStonePattern)   
+      {
+         case WHOLE_WORLD :
+         {
+            steppingStones = generateWholeWorldSteppingStones(footPolygon);
+
+            break;
+         }
+
+         case SIMPLE_RECTANGULAR_GRID :
+         {
+            steppingStones = generateSimpleRectangularGridSteppingStones(footPolygon);
+
+            break;
+         }
+
+         case GROUP_OF_THREE :
+         {
+            steppingStones = generateGroupsOfThreeSteppingStones(footPolygon);
+
+            break;
+         }
+
+         case SIMPLE_BEAM_BALANCE :
+         {
+            steppingStones = generateRectangularBeamBalance(footPolygon);
+
+            // steppingStones = generateSimpleRectangularBeamBalance();
+
+            break;
+         }
+
+         case STEP_DOWNS :
+         {
+            steppingStones = generateStepDowns(footPolygon);
+
+            break;
+         }
+
+         case CRISS_CROSS_BEAMS :
+         {
+            steppingStones = generateCrissCrossBeams(footPolygon);
+
+            break;
+         }
+
+         default :
+         {
+            throw new RuntimeException("Shouldn't get here!");
+         }
+      }
+      
+      if (useSteppingStonesGroundModel)
+      {
+         System.out.println("Using a stepping stone ground model");
+         groundProfile = new SteppingStonesGroundProfile(steppingStones);
+      }
+      else
+      {
+         System.out.println("Drawing SteppingStones but using a flat ground model.");
+         groundProfile = new FlatGroundProfile();
+      }
+
+   }
+   
+   
+   private static final SteppingStones generateGroupsOfThreeSteppingStones(ConvexPolygon2d footPolygon)
+   {
+      double startXPosition = 0.45;    // 0.65; //0.55; //-0.1;
+      double startYPosition = -0.4; //-0.6;    // -0.9; //-0.75;
+      double radius = 0.2;    // 0.25;
+      int numSides = 6;
+      double baseZ = -0.1;
+      double height = 0.0;
+      int numStones = 30; //60;
+      double xBetweenStoneGroups = 0.15;    // 0.2; //0.25;
+
+      // Please don't change these
+      boolean MATTS_PUSH_TEST = false;
+      if (MATTS_PUSH_TEST)
+      {
+         startXPosition = 0.45;
+         startYPosition = -0.6;
+         radius = 0.2;
+         numSides = 6;
+         baseZ = -0.1;
+         height = 0.0;
+         numStones = 60;
+         xBetweenStoneGroups = 0.15;
+      }
+      
+      boolean R2_TEST = true;
+      if (R2_TEST)
+      {
+         startXPosition = 0.25;
+         startYPosition = -0.2;
+         radius = 0.3; //0.25;
+         numSides = 6;
+         baseZ = -0.1;
+         height = 0.0;
+         numStones = 30;
+         xBetweenStoneGroups = 0.15;
+      }
+
+      SteppingStones steppingStones = SteppingStones.generateRandomPolygonalRandomPatternOneSteppingStones(startXPosition, startYPosition, radius, numSides,
+                                         xBetweenStoneGroups, baseZ, height, numStones, footPolygon, false);
+
+      return steppingStones;
+
+
+//    steppingStones = SteppingStones.createSampleSteppingStones();
+//    steppingStones = SteppingStones.generateRandomSteppingStones(random, 50);
+//    steppingStones.addSteppingStone(SteppingStone.generateRandomCicularStone("base", random, 0.5, -0.2, -0.2, 0.0, 1.5));
+
+//    steppingStones = SteppingStones.generateRandomPolygonalUniformSteppingStones(-0.0,-0.8,0.2,6,0.0,0.0,-0.1,0.0,3,20);
+//    steppingStones = SteppingStones.generateRandomPolygonalChessBoardSteppingStones(-0.1, -0.75, 0.25, 6, 0.0, -0.15, -0.1, 0.0, 4, 20);
+//    steppingStones = SteppingStones.generateRandomPolygonalUniformSteppingStones(-0.0,-0.8,0.2,6,0.0,0.0,-0.1,0.0,3, 1);
+
+//    steppingStones = SteppingStones.generateRandomPolygonalChessBoardSteppingStones(startXPosition, startYPosition, radius, numSides, spacingInX, spacingInY, baseZ, height, numRows, numColumns);
+//    steppingStones = SteppingStones.generateRandomPolygonalRandomPatternOneSteppingStones(startXPosition, startYPosition, radius, numSides, xBetweenStoneGroups, baseZ, height, numStones);
+//    steppingStones = SteppingStones.generateRandomPolygonalRandomPatternOneSteppingStones(startXPosition, startYPosition, radius, numSides, 0.2, baseZ, height, numStones);
+//    steppingStones = SteppingStones.generateRandomPolygonalRandomPatternTwoSteppingStones(startXPosition, startYPosition, radius, numSides, 0.0, baseZ, height, numStones);
+   }
+
+   private static final SteppingStones generateWholeWorldSteppingStones(ConvexPolygon2d footPolygon)
+   {
+      ArrayList<Point2d> points = new ArrayList<Point2d>();
+
+      points.add(new Point2d(-100.0, -100.0));
+      points.add(new Point2d(-100.0, 100.0));
+      points.add(new Point2d(100.0, -100.0));
+      points.add(new Point2d(100.0, 100.0));
+
+      SteppingStone steppingStone = new SteppingStone("wholeWorld", -0.02, 0.0, points, footPolygon);
+
+      SteppingStones steppingStones = new SteppingStones();
+      steppingStones.addSteppingStone(steppingStone);
+
+      return steppingStones;
+   }
+
+   private static final SteppingStones generateSimpleRectangularGridSteppingStones(ConvexPolygon2d footPolygon)
+   {
+      double startXPosition = 0.65;    // 0.65; //0.55; //-0.1;
+      double startYPosition = -0.92;    // -0.9; //-0.75;
+      double xDimension = 0.28;
+      double yDimension = 0.18;
+      double spacingInX = 0.16;    // 0.1;
+      double spacingInY = 0.05;    // -0.15;
+      double baseZ = -0.1;
+      double height = 0.0;
+      int numRows = 8;
+      int numColumns = 20;
+
+
+      SteppingStones steppingStones = SteppingStones.generateRectangularUniformSteppingStones(startXPosition, startYPosition, xDimension, yDimension,
+                                         spacingInX, spacingInY, baseZ, height, numRows, numColumns, footPolygon, false);
+
+      return steppingStones;
+   }
+
+   @SuppressWarnings("unused")
+   private static final SteppingStones generateSimpleRectangularBeamBalance(ConvexPolygon2d footPolygon)
+   {
+      double startXPosition = 0.65;    // 0.65; //0.55; //-0.1;
+      double startYPosition = -0.745;    // -0.9; //-0.75;
+      double xDimension = 0.28;
+      double yDimension = 0.14;    // 0.16; //0.18;
+      double spacingInX = -0.2;    // 0.0; //0.1;
+      double spacingInYSmall = 0.05;    // -0.15;
+      double spacingInYLarge = 0.1;
+      double baseZ = -0.1;
+      double height = 0.0;
+      int numRows = 4;
+      int numColumns = 40;    // 20;
+
+      SteppingStones steppingStones = SteppingStones.generateRectangularBeamBalance(startXPosition, startYPosition, xDimension, yDimension, spacingInX,
+                                         spacingInYSmall, spacingInYLarge, baseZ, height, numRows, numColumns, footPolygon);
+
+      return steppingStones;
+   }
+
+   private static final SteppingStones generateRectangularBeamBalance(ConvexPolygon2d footPolygon)
+   {
+      double startXPosition = 7.5;    // 0.65; //0.55; //-0.1;
+      double startYPosition = -0.72;    // -0.68;//-0.745; //-0.9; //-0.75;
+      double xDimension = 14.0;
+      double yDimension = 0.16;    // 0.14; //0.16; //0.18;
+      double spacingInX = -0.2;    // 0.0; //0.1;
+      double spacingInYSmall = 0.05;    // -0.15;
+      double spacingInYLarge = 0.1;
+      double baseZ = -0.1;
+      double height = 0.0;
+      int numRows = 4;
+      int numColumns = 1;    // 20;
+
+      SteppingStones steppingStones = SteppingStones.generateRectangularBeamBalance(startXPosition, startYPosition, xDimension, yDimension, spacingInX,
+                                         spacingInYSmall, spacingInYLarge, baseZ, height, numRows, numColumns, footPolygon);
+
+      return steppingStones;
+   }
+
+   private static final SteppingStones generateStepDowns(ConvexPolygon2d footPolygon)
+   {
+      double startXPosition = 0.65;    // 0.65; //0.55; //-0.1;
+      double startYPosition = -0.92;    // -0.9; //-0.75;
+      double xDimension = 0.28;
+      double yDimension = 0.18;
+      double spacingInX = 0.16;    // 0.1;
+      double spacingInY = 0.05;    // -0.15;
+      double baseZ = 0.0;
+      double height = 0.0;
+      int numRows = 8;
+      int numColumns = 20;
+
+      SteppingStones steppingStones = SteppingStones.generateRectangularUniformSteppingStones(startXPosition, startYPosition, xDimension, yDimension,
+                                         spacingInX, spacingInY, baseZ, height, numRows, numColumns, footPolygon, true);
+
+      return steppingStones;
+   }
+
+   private static final SteppingStones generateCrissCrossBeams(ConvexPolygon2d footPolygon)
+   {
+      SteppingStones steppingStones = new SteppingStones();
+
+      double startXPosition = 0.95;    // 0.65; //0.55; //-0.1;
+      double startYPosition = -2.65;    // -0.9; //-0.75;
+      double xDimension = 4.00;
+      double yDimension = 0.2;
+      double spacingInX = 3.00;    // 0.1;
+      double spacingInY = 2.0;    // -0.15;
+      double baseZ = -0.1;
+      double height = 0.0;
+      int numRows = 3;
+      int numColumns = 2;
+
+      steppingStones = SteppingStones.generateRectangularCrissCrossBeams(startXPosition, startYPosition, xDimension, yDimension, spacingInX, spacingInY, baseZ,
+              height, numRows, numColumns, footPolygon, false);
+
+      return steppingStones;
+   }
+
+
+   private GroundProfile setUpTerrain(TerrainType terrainType, YoVariableRegistry registry)
+   {
+      switch (terrainType)
+      {
+         case FLAT :
+            groundProfile = new FlatGroundProfile(FLAT_HEIGHT);
+
+            break;
+
+         case ROLLING_HILLS :
+            groundProfile = new RollingGroundProfile(0.1, 0.3, 0.0, -20.0, 20.0, -20.0, 20.0);
+
+            break;
+
+         case BUMPY :
+            groundProfile = new BumpyGroundProfile(0.04, 0.4, 0.1, 0.2, 0.02, 0.35, 0.0, 5.0, -20.0, 20.0, -20.0, 20.0);
+
+            break;
+
+         case ROCKY :
+            groundProfile = new RandomRockyGroundProfile(10);
+
+            break;
+
+         case STEP_UP :
+            groundProfile = new StepUpGroundProfile(registry);
+            break;
+            
+         case ALTERNATING_SLOPES :
+         {
+            double xMin = -1.0, xMax = 12.0;
+            double yMin = -2.0, yMax = 2.0;
+            double[][] xSlopePairs = new double[][]{{1.0, 0.05}, {3.0, 0.0}, {5.0, 0.1}, {7.0, 0.0}, {9.0, 0.1}, {11.0, 0.0}};
+            groundProfile = new AlternatingSlopesGroundProfile(xSlopePairs, xMin, xMax, yMin, yMax);
+         
+            break;
+         }
+            
+         case DOWNHILL :
+         {
+            double xMin = -1.0, xMax = 12.0;
+            double yMin = -2.0, yMax = 2.0;
+            double[][] xSlopePairs = new double[][]{{1.0, -0.05}, {3.0, 0.0}, {5.0, -0.1}, {7.0, -0.0}, {9.0, -0.1}, {11.0, 0.0}};
+            groundProfile = new AlternatingSlopesGroundProfile(xSlopePairs, xMin, xMax, yMin, yMax);
+         
+            break;
+         }
+         
+         case STEEP_UPSLOPE :
+         {
+            double xMin = -1.0, xMax = 10.0;
+            double yMin = -2.0, yMax = 2.0;
+            double[][] xSlopePairs = new double[][]{{1.0, 0.2}, {6.0, 0.0}, {10.0, 0.0}};
+            groundProfile = new AlternatingSlopesGroundProfile(xSlopePairs, xMin, xMax, yMin, yMax);
+            break;
+         }
+         
+         case COURSE :
+            groundProfile = new ObstacleCourse();
+
+            break;
+
+         case MOON :
+            groundProfile = new GroundProfileFromFile("GroundModels" + File.separator + "moonNew.asc", new Transform3D());
+
+            break;
+
+         case LOADED :
+            JFileChooser fc = new JFileChooser();
+            int returnVal = fc.showOpenDialog(new JFrame());
+
+            if (returnVal == JFileChooser.APPROVE_OPTION)
+            {
+               File file = fc.getSelectedFile();
+
+               // This is where a real application would open the file.
+               System.out.println("Opening: " + file.getName());
+               groundProfile = new GroundProfileFromFile(file.getAbsolutePath(), new Transform3D());
+            }
+            else
+            {
+               System.out.println("Open command cancelled by user.");
+               groundProfile = new FlatGroundProfile();
+            }
+
+
+
+            break;
+
+         default :
+            groundProfile = new FlatGroundProfile();
+      }
+
+      return groundProfile;
+   }
+
+   
+   
+}
