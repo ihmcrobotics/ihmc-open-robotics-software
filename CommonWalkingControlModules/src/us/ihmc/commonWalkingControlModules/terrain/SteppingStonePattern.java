bb1640b49e90d6222b13128f6a5d89684626f67d
Jerry Pratt <jpratt@ihmc.us>
2011-03-14 19:52:37 +0000

    Moving stepping stones and terrain stuff to common walking.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/terrain/SteppingStonePattern.java
@@ -0,0 +1,6 @@
+package us.ihmc.commonWalkingControlModules.terrain;
+
+public enum SteppingStonePattern
+{
+   WHOLE_WORLD, SIMPLE_RECTANGULAR_GRID, GROUP_OF_THREE, SIMPLE_BEAM_BALANCE, STEP_DOWNS, CRISS_CROSS_BEAMS;
+}
