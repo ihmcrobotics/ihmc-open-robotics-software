d39169039301a33fd3878c1902578a891bc51fd8
Matt Johnson <mjohnson@ihmc.us>
2011-03-18 20:04:14 +0000

    Vicon update

---
+++ Vicon/src/us/ihmc/vicon/PoseListener.java
@@ -0,0 +1,57 @@
+package us.ihmc.vicon;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * Last updated by: mjohnson
+ * On: 3/17/11 4:48 PM
+ */
+public class PoseListener implements Runnable
+{
+   private String modelName;
+   private long updateRateInMillis;
+   private ViconServer viconServer;
+   private String host;
+   private int port;
+   private boolean DONE = false;
+
+   public PoseListener(String host, Integer port, String modelName, Long updateRateInMillis, ViconServer viconServer)
+   {
+      this.host = host;
+      this.port = port;
+      this.modelName = modelName;
+      this.updateRateInMillis = updateRateInMillis;
+      this.viconServer = viconServer;
+   }
+
+   @Override
+   public void run()
+   {
+      try
+      {
+         Socket socket = new Socket(host, port);
+         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+         while (!DONE)
+         {
+            Pose pose = viconServer.getPose(modelName);
+            oos.writeObject(pose);
+            oos.flush();
+            oos.reset();
+            Thread.sleep(updateRateInMillis);
+         }
+         System.out.println("PoseListener: closing socket...");
+         socket.close();
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+   }
+
+   public void stopListening()
+   {
+      DONE = true;
+   }
+}
