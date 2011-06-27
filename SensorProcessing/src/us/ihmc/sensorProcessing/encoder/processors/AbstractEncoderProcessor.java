86d0621465518176a2e34d3b0f3a624389b6a7e1
M2V2 Computer <M2V2Computer@unknown>
2011-06-27 17:53:26 +0000

    * Added EncoderResolutionReducer * Now updating processed position and velocity variables inside the encoder processors. * Updated jardesc. * got rid of DM6816 debugging stuff

--- SensorProcessing/src/us/ihmc/sensorProcessing/encoder/processors/AbstractEncoderProcessor.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/encoder/processors/AbstractEncoderProcessor.java
@@ -1,7 +1,9 @@
 package us.ihmc.sensorProcessing.encoder.processors;
 
+import com.yobotics.simulationconstructionset.AbstractYoVariable;
 import com.yobotics.simulationconstructionset.DoubleYoVariable;
 import com.yobotics.simulationconstructionset.IntYoVariable;
+import com.yobotics.simulationconstructionset.VariableChangedListener;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
 public abstract class AbstractEncoderProcessor implements EncoderProcessor
@@ -13,7 +15,8 @@ public abstract class AbstractEncoderProcessor implements EncoderProcessor
    protected final DoubleYoVariable processedTicks;
    protected final DoubleYoVariable processedTickRate;
 
-   private final double distancePerTick;
+   private final DoubleYoVariable processedPosition;
+   private final DoubleYoVariable processedVelocity;
 
    public AbstractEncoderProcessor(String name, IntYoVariable rawTicks, DoubleYoVariable time, double distancePerTick, YoVariableRegistry registry)
    {
@@ -22,19 +25,41 @@ public abstract class AbstractEncoderProcessor implements EncoderProcessor
       this.time = time;
       this.processedTicks = new DoubleYoVariable(name + "ProcTicks", registry);
       this.processedTickRate = new DoubleYoVariable(name + "ProcTickRate", registry);
-      this.distancePerTick = distancePerTick;
+      this.processedPosition = new DoubleYoVariable(name + "ProcPos", registry);
+      this.processedVelocity = new DoubleYoVariable(name + "ProcVel", registry);
+
+      processedTicks.addVariableChangedListener(new MultiplicationVariableChangedListener(processedPosition, distancePerTick));
+      processedTickRate.addVariableChangedListener(new MultiplicationVariableChangedListener(processedVelocity, distancePerTick));
    }
 
    public final double getQ()
    {
-      return processedTicks.getDoubleValue() * distancePerTick;
+      return processedPosition.getDoubleValue();
    }
 
    public final double getQd()
    {
-      return processedTickRate.getDoubleValue() * distancePerTick;
+      return processedVelocity.getDoubleValue();
    }
-   
+
    public abstract void initialize();
+
    public abstract void update();
+
+   private static final class MultiplicationVariableChangedListener implements VariableChangedListener
+   {
+      private final AbstractYoVariable output;
+      private final double multiplicationFactor;
+
+      public MultiplicationVariableChangedListener(AbstractYoVariable output, double multiplicationFactor)
+      {
+         this.output = output;
+         this.multiplicationFactor = multiplicationFactor;
+      }
+
+      public void variableChanged(AbstractYoVariable variable)
+      {
+         output.setValueFromDouble(variable.getValueAsDouble() * multiplicationFactor);
+      }
+   }
 }
