ebbbbf9fa003a5f093b57d3eaf97479309d7b449
Daniel Duran <dduran@ihmc.us>
2011-08-09 21:39:57 +0000

    - Moved GroundProfileTest from FastRunner to SCS - Created shipCorridorGroundProfileTest - Fixed a bug when calculating the normal

---
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/ground/GroundProfileTest.java
@@ -0,0 +1,40 @@
+package com.yobotics.simulationconstructionset.ground;
+
+import javax.vecmath.Vector3d;
+import org.junit.Test;
+import us.ihmc.utilities.test.JUnitTools;
+import com.yobotics.simulationconstructionset.GroundProfile;
+
+public abstract class GroundProfileTest
+{
+   protected GroundProfile groundProfile;
+   private final double epsilon;
+   private final boolean debug = true;
+
+   public GroundProfileTest(double epsilon)
+   {
+      this.epsilon = epsilon;
+   }
+
+   @Test
+   public void testSurfaceNormalAlongXAxis()
+   {
+      int nSteps = 1000;
+      double xStep = (groundProfile.getXMax() - groundProfile.getXMin()) / nSteps;
+      double dx = 1e-8;
+      double y = (groundProfile.getYMax() - groundProfile.getYMin()) / 2.0;
+      double z = 0.0;
+
+      for (int i = 0; i < nSteps; i++)
+      {
+         double x = i * xStep;
+         double dzdx = (groundProfile.heightAt(x + dx, y, z) - groundProfile.heightAt(x, y, z)) / dx;
+         Vector3d numericalSurfaceNormal = new Vector3d(-dzdx, 0.0, 1.0);
+         numericalSurfaceNormal.normalize();
+
+         Vector3d surfaceNormalFromGroundProfile = new Vector3d();
+         groundProfile.surfaceNormalAt(x, y, z, surfaceNormalFromGroundProfile);
+         JUnitTools.assertVector3dEquals(numericalSurfaceNormal, surfaceNormalFromGroundProfile, epsilon);
+      }
+   }
+}
