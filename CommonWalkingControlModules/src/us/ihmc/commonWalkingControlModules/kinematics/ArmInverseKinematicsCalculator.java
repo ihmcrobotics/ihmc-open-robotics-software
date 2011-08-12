7fa772eadf4157e3b343bddcaeb97c09957a0d29
Stephen McCrory <smccrory@ihmc.us>
2011-08-12 23:04:22 +0000

    Unfinished calculator for R2 arm inverse kinematics

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/kinematics/ArmInverseKinematicsCalculator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/kinematics/ArmInverseKinematicsCalculator.java
@@ -1,6 +1,5 @@
 package us.ihmc.commonWalkingControlModules.kinematics;
 
-import javax.media.j3d.Transform3D;
 import javax.vecmath.Vector3d;
 
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmJointPositions;
@@ -8,5 +7,5 @@ import us.ihmc.robotSide.RobotSide;
 
 public interface ArmInverseKinematicsCalculator
 {
-   public abstract void solve(ArmJointPositions armJointPositionsToPack, Transform3D handToChest, RobotSide robotSide, Vector3d elbowPoint) throws InverseKinematicsException;
+   public abstract void solve(ArmJointPositions armJointPositionsToPack, Vector3d desiredElbowPointInShoulderFrame, Vector3d desiredOrientationOfUpperArmInShoulderFrame, RobotSide robotSide) throws InverseKinematicsException;
 }
