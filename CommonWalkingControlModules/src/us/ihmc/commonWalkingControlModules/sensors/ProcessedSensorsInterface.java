ead24f81e0cac77c2128f6b23746d920db905795
Twan Koolen <tkoolen@ihmc.us>
2011-09-17 17:15:25 +0000

    Got CoM velocity calculator to work.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/ProcessedSensorsInterface.java
@@ -57,5 +57,4 @@ public interface ProcessedSensorsInterface
    public abstract double getNeckJointPosition(NeckJointName neckJointName);
    public abstract double getNeckJointVelocity(NeckJointName neckJointName);
    public abstract FullRobotModel getFullRobotModel();
-   public abstract FrameVector getIMUAngularVelocity();
 }
