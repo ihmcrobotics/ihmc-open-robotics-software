5d78ced2e467bbf0ea1208b89f794a5d1df21c49
M2V2 Computer <M2V2Computer@unknown>
2011-09-22 20:58:24 +0000

    Create and add a SwapSimPAndQ Button on M2V2 simulation. Should be useful for comparing body position estimators and such.
    Created a SineWaveDesiredPelvisOrientationControlModule for making the pelvis do sine waves.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/desiredPelvisOrientation/SineWaveDesiredPelvisOrientationControlModule.java
@@ -0,0 +1,77 @@
+package us.ihmc.commonWalkingControlModules.controlModules.desiredPelvisOrientation;
+
+import us.ihmc.commonWalkingControlModules.controlModuleInterfaces.DesiredPelvisOrientationControlModule;
+import us.ihmc.commonWalkingControlModules.referenceFrames.CommonWalkingReferenceFrames;
+import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
+import us.ihmc.robotSide.RobotSide;
+import us.ihmc.utilities.math.geometry.Orientation;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.math.filter.AlphaFilteredYoFrameVector;
+import com.yobotics.simulationconstructionset.util.math.filter.AlphaFilteredYoVariable;
+
+public class SineWaveDesiredPelvisOrientationControlModule implements DesiredPelvisOrientationControlModule
+{
+   private final CommonWalkingReferenceFrames referenceFrames;
+   private final ProcessedSensorsInterface processedSensors;
+   
+   private final YoVariableRegistry registry = new YoVariableRegistry("SineWaveOrientation");
+   
+   private final DoubleYoVariable yawAmp = new DoubleYoVariable("yawAmp", registry);
+   private final DoubleYoVariable yawOffset = new DoubleYoVariable("yawOffset", registry);
+   private final DoubleYoVariable yawFreq = new DoubleYoVariable("yawFreq", registry);
+  
+   private final DoubleYoVariable pitchAmp = new DoubleYoVariable("pitchAmp", registry);
+   private final DoubleYoVariable pitchOffset = new DoubleYoVariable("pitchOffset", registry);
+   private final DoubleYoVariable pitchFreq = new DoubleYoVariable("pitchFreq", registry);
+   
+   private final DoubleYoVariable rollAmp = new DoubleYoVariable("rollAmp", registry);
+   private final DoubleYoVariable rollOffset = new DoubleYoVariable("rollOffset", registry);
+   private final DoubleYoVariable rollFreq = new DoubleYoVariable("rollFreq", registry);
+   private final DoubleYoVariable alphaDesiredOrientation;
+      
+   private final AlphaFilteredYoFrameVector orientationVector;
+   
+   public SineWaveDesiredPelvisOrientationControlModule(CommonWalkingReferenceFrames referenceFrames, ProcessedSensorsInterface processedSensors, YoVariableRegistry parentRegistry)
+   {
+      this.referenceFrames = referenceFrames;
+      this.processedSensors = processedSensors;
+      
+      alphaDesiredOrientation = new DoubleYoVariable("alphaDesiredOrientation", registry);
+      orientationVector = AlphaFilteredYoFrameVector.createAlphaFilteredYoFrameVector("filtDesOrientation", "", registry, alphaDesiredOrientation, referenceFrames.getMidFeetZUpFrame());
+      
+      parentRegistry.addChild(registry);
+   }
+   
+   public Orientation getDesiredPelvisOrientationSingleSupport(RobotSide robotSide)
+   {
+      return new Orientation(referenceFrames.getPelvisFrame());
+   }
+   
+   public void setAlphaForFilterFreqInHz(double filterFreqInHz, double dt)
+   {
+      alphaDesiredOrientation.set(AlphaFilteredYoVariable.computeAlphaGivenBreakFrequencyProperly(filterFreqInHz, dt));
+   }
+
+   public Orientation getDesiredPelvisOrientationDoubleSupport()
+   {
+      double time = processedSensors.getTime();
+      
+      double yaw = yawOffset.getDoubleValue() + yawAmp.getDoubleValue() * Math.sin(2.0 * Math.PI * yawFreq.getDoubleValue() * time);
+      double pitch = pitchOffset.getDoubleValue() + pitchAmp.getDoubleValue() * Math.sin(2.0 * Math.PI * pitchFreq.getDoubleValue() * time);
+      double roll = rollOffset.getDoubleValue() + rollAmp.getDoubleValue() * Math.sin(2.0 * Math.PI * rollFreq.getDoubleValue() * time);
+      
+      orientationVector.update(roll, pitch, yaw);
+     
+      return new Orientation(referenceFrames.getMidFeetZUpFrame(), orientationVector.getZ(), orientationVector.getY(), orientationVector.getX());
+   }
+
+   public void setParametersForM2V2()
+   {
+      yawFreq.set(0.5);
+      pitchFreq.set(0.5);
+      rollFreq.set(0.5);
+   }
+
+}
