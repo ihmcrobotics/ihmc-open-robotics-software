caf45483b55ce5daad4aa27b87c90b4bd121789e
Jerry Pratt <jpratt@ihmc.us>
2011-09-28 21:34:13 +0000

    Moved GeneticAlgorithm classes to ParameterOptimization and removed the GeneticAlgorithm project.

---
+++ ParameterOptimization/src/us/ihmc/utilities/parameterOptimization/geneticAlgorithm/GeneticAlgorithmChangedListener.java
@@ -0,0 +1,21 @@
+package us.ihmc.utilities.parameterOptimization.geneticAlgorithm;
+
+/**
+ * <p>Title: Genetic Algorithm Library </p>
+ *
+ * <p>Description: General Purpose Genetic Algorithm Library </p>
+ *
+ * <p>Copyright: Copyright (c) 2003-2005 Jerry Pratt, IHMC </p>
+ *
+ * <p>Company: Institute for Human and Machine Cognition.
+ * 40 South Alcaniz Street
+ * Pensacola, FL 32502 </p>
+ *
+ * @author Jerry Pratt and Jim Warrenfeltz, jpratt@ihmc.us
+ * @version 1.0
+ */
+
+public interface GeneticAlgorithmChangedListener
+{
+   public abstract void geneticAlgorithmChanged();
+}
---
+++ ParameterOptimization/src/us/ihmc/utilities/parameterOptimization/geneticAlgorithm/GeneticAlgorithmIndividualToEvaluate.java
@@ -0,0 +1,160 @@
+package us.ihmc.utilities.parameterOptimization.geneticAlgorithm;
+
+import us.ihmc.utilities.parameterOptimization.IndividualToEvaluate;
+import us.ihmc.utilities.parameterOptimization.ListOfParametersToOptimize;
+
+/**
+ * <p>Title: Genetic Algorithm Library </p>
+ *
+ * <p>Description: General Purpose Genetic Algorithm Library </p>
+ *
+ * <p>Copyright: Copyright (c) 2003-2005 Jerry Pratt, IHMC </p>
+ *
+ * <p>Company: Institute for Human and Machine Cognition.
+ * 40 South Alcaniz Street
+ * Pensacola, FL 32502 </p>
+ *
+ * @author Jerry Pratt and Jim Warrenfeltz, jpratt@ihmc.us
+ * @version 1.0
+ */
+
+public class GeneticAlgorithmIndividualToEvaluate implements Comparable<GeneticAlgorithmIndividualToEvaluate>
+{
+   private final IndividualToEvaluate individualToEvaluate;
+   
+   private Genotype genotype;
+
+   public GeneticAlgorithmIndividualToEvaluate(IndividualToEvaluate individualToEvaluate)
+   {
+      this.individualToEvaluate = individualToEvaluate;
+   }
+   
+   public IndividualToEvaluate getIndividualToEvaluate()
+   {
+      return individualToEvaluate;
+   }
+   
+//   public abstract GeneticAlgorithmIndividualToEvaluate createNewIndividual();
+
+//   public abstract GeneticAlgorithmIndividualToEvaluate readIn(BufferedReader bufferedReader) throws java.io.IOException;
+//
+//   public abstract int[] getBitsPerGene();
+//
+//   public abstract void writeOut(PrintWriter printWriter);
+
+//   public GeneticAlgorithmIndividualToEvaluate()
+//   {
+//      // genes = new Genotype(bitsPerGene);
+//   }
+   
+   public String getName()
+   {
+      return individualToEvaluate.getName();
+   }
+   
+   public void setName(String name)
+   {
+      individualToEvaluate.setName(name);
+   }
+   
+   public boolean isEvaluationDone()
+   {
+      return individualToEvaluate.isEvaluationDone();
+   }
+   
+   public void startEvaluation()
+   {
+      individualToEvaluate.startEvaluation();
+   }
+
+   public Genotype getGenotype()
+   {
+      return genotype;
+   }
+
+   public void setGenotype(Genotype genes)
+   {
+      this.genotype = genes;
+      double[] phenotype = genes.getDoublePhenotype();
+      
+      ListOfParametersToOptimize listOfParametersToOptimize = this.individualToEvaluate.getAllParametersToOptimize();
+      listOfParametersToOptimize.setCurrentValuesGivenZeroToOnes(phenotype);
+   }
+
+   public static GeneticAlgorithmIndividualToEvaluate[] mate(GeneticAlgorithmIndividualToEvaluate parent1, GeneticAlgorithmIndividualToEvaluate parent2, double mutate_rate)
+   {
+      GeneticAlgorithmIndividualToEvaluate children[] = new GeneticAlgorithmIndividualToEvaluate[2];
+
+      children[0] = parent1.makeNewIndividual();
+      children[1] = parent1.makeNewIndividual();
+
+      Genotype genotypeForTwoChildren[] = new Genotype[2];
+      genotypeForTwoChildren = parent1.getGenotype().crossover(parent2.getGenotype(), mutate_rate);
+
+      children[0].setGenotype(genotypeForTwoChildren[0]);
+      children[1].setGenotype(genotypeForTwoChildren[1]);
+
+      return children;
+   } 
+   
+   public double getFitness()
+   {
+      return individualToEvaluate.getFitness();
+   }
+   
+   public int compareTo(GeneticAlgorithmIndividualToEvaluate ind)
+   {
+      double fit1 = this.individualToEvaluate.getFitness();
+      double fit2 = ind.individualToEvaluate.getFitness();
+
+      if (fit1 > fit2)
+         return -1;
+      else if (fit1 < fit2)
+         return 1;
+      else
+         return 0;
+   }
+
+   public static GeneticAlgorithmIndividualToEvaluate makeRandomIndividual(IndividualToEvaluate individualToCopyFrom)
+   {
+      IndividualToEvaluate individualToEvaluate = individualToCopyFrom.createNewIndividual();
+      
+      ListOfParametersToOptimize listOfParametersToOptimize = individualToEvaluate.getAllParametersToOptimize();
+      
+      Genotype genotype = new Genotype(listOfParametersToOptimize.getBitsOfResolution());
+      genotype.setRandomGenes();
+      double[] doublePhenotype = genotype.getDoublePhenotype();
+      listOfParametersToOptimize.setCurrentValuesGivenZeroToOnes(doublePhenotype);
+      
+      GeneticAlgorithmIndividualToEvaluate ret = new GeneticAlgorithmIndividualToEvaluate(individualToEvaluate);
+      ret.setGenotype(genotype);
+      
+      return ret;
+   }
+   
+   public static GeneticAlgorithmIndividualToEvaluate makeNewIndividual(IndividualToEvaluate individualToCopyFrom)
+   {
+      IndividualToEvaluate individualToEvaluate = individualToCopyFrom.createNewIndividual();
+     
+      GeneticAlgorithmIndividualToEvaluate ret = new GeneticAlgorithmIndividualToEvaluate(individualToEvaluate);
+      
+      return ret;
+   }
+   
+   
+   public GeneticAlgorithmIndividualToEvaluate makeRandomIndividual()
+   {
+      return makeRandomIndividual(this.individualToEvaluate);
+   }
+   
+   public GeneticAlgorithmIndividualToEvaluate makeNewIndividual()
+   {
+      return makeRandomIndividual(this.individualToEvaluate);
+   }
+   
+   public String toString()
+   {
+      return individualToEvaluate.toString();
+   }
+
+}
