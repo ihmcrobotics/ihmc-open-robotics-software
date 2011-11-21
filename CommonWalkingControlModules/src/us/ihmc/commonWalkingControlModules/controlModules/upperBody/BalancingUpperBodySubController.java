3c95915e3fa60f00ff414afdbb42de36c57e0e34
Sander van Weperen <svanweperen@ihmc.us>
2011-11-21 19:45:30 +0000

    - Implemented first two (now empty) states of the BalacingUpperBodySubController - Created interface SpineLungingControlModule - UpperBodySubController has access to the couplingRegistery

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/upperBody/BalancingUpperBodySubController.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/upperBody/BalancingUpperBodySubController.java
@@ -1,13 +1,11 @@
 package us.ihmc.commonWalkingControlModules.controlModules.upperBody;
 
-import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.HashMap;
 
 import javax.vecmath.Vector3d;
 
 import us.ihmc.commonWalkingControlModules.controlModuleInterfaces.ArmControlModule;
-import us.ihmc.commonWalkingControlModules.controlModuleInterfaces.SpineControlModule;
+import us.ihmc.commonWalkingControlModules.controlModuleInterfaces.SpineLungingControlModule;
 import us.ihmc.commonWalkingControlModules.controllers.regularWalkingGait.UpperBodySubController;
 import us.ihmc.commonWalkingControlModules.couplingRegistry.CouplingRegistry;
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.NeckJointName;
@@ -15,67 +13,198 @@ import us.ihmc.commonWalkingControlModules.partNamesAndTorques.NeckTorques;
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.UpperBodyTorques;
 import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
 import us.ihmc.utilities.containers.ContainerTools;
-import us.ihmc.utilities.math.geometry.FramePoint;
-import us.ihmc.utilities.math.geometry.ReferenceFrame;
-import us.ihmc.utilities.screwTheory.InverseDynamicsCalculator;
-import us.ihmc.utilities.screwTheory.InverseDynamicsJoint;
-import us.ihmc.utilities.screwTheory.RigidBody;
-import us.ihmc.utilities.screwTheory.SpatialAccelerationVector;
-import us.ihmc.utilities.screwTheory.Wrench;
 
 import com.yobotics.simulationconstructionset.DoubleYoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 import com.yobotics.simulationconstructionset.util.PIDController;
+import com.yobotics.simulationconstructionset.util.statemachines.State;
+import com.yobotics.simulationconstructionset.util.statemachines.StateMachine;
+import com.yobotics.simulationconstructionset.util.statemachines.StateTransition;
+import com.yobotics.simulationconstructionset.util.statemachines.StateTransitionCondition;
 
 public class BalancingUpperBodySubController implements UpperBodySubController
 {
    private final CouplingRegistry couplingRegistry;
    private final ProcessedSensorsInterface processedSensors;
-   private final YoVariableRegistry registry = new YoVariableRegistry("SimpleUpperBodySubController");
+   private final YoVariableRegistry registry = new YoVariableRegistry("BalancingUpperBodySubController");
 
    private final ArmControlModule armControlModule;
-   private final SpineControlModule spineControlModule;
+   private final SpineLungingControlModule spineControlModule;
 
    private final EnumMap<NeckJointName, PIDController> neckControllers = ContainerTools.createEnumMap(NeckJointName.class);
    private final EnumMap<NeckJointName, DoubleYoVariable> desiredNeckPositions = ContainerTools.createEnumMap(NeckJointName.class);
 
    private final double controlDT;
-   
-   public BalancingUpperBodySubController(CouplingRegistry couplingRegistry, ProcessedSensorsInterface processedSensors, double controlDT, ArmControlModule armControlModule, SpineControlModule spineControlModule,
-           YoVariableRegistry parentRegistry)
+   private NeckTorques neckTorques = new NeckTorques();
+
+   private final StateMachine stateMachine;
+   private final String name = "BalancingUpperBodySubController";
+
+   public BalancingUpperBodySubController(CouplingRegistry couplingRegistry, ProcessedSensorsInterface processedSensors, double controlDT,
+         ArmControlModule armControlModule, SpineLungingControlModule spineControlModule, YoVariableRegistry parentRegistry)
    {
       this.couplingRegistry = couplingRegistry;
       this.processedSensors = processedSensors;
       this.controlDT = controlDT;
       this.armControlModule = armControlModule;
       this.spineControlModule = spineControlModule;
+      this.stateMachine = new StateMachine(name + "State", name + "SwitchTime", BalancingUpperBodySubControllerState.class, processedSensors.getYoTime(),
+            registry);
 
       populateYoVariables();
 
       populateControllers();
       parentRegistry.addChild(registry);
 
-
       setGains();
+
+      setUpStateMachine();
    }
 
-   
    public void doUpperBodyControl(UpperBodyTorques upperBodyTorquesToPack)
    {
-      armControlModule.doArmControl(upperBodyTorquesToPack.getArmTorques());
-
-      upperBodyTorquesToPack.setNeckTorques(doNeckControl());
-
-      FramePoint icpInWorldFrame = couplingRegistry.getCapturePointInFrame(ReferenceFrame.getWorldFrame());
+      stateMachine.doAction();
+      stateMachine.checkTransitionConditions();
       
-      spineControlModule.doSpineControl(upperBodyTorquesToPack.getSpineTorques());
-
+      armControlModule.doArmControl(upperBodyTorquesToPack.getArmTorques());
+      this.doNeckControl();
+      
+      // set torques
+      upperBodyTorquesToPack.setNeckTorques(neckTorques);
+      spineControlModule.getSpineTorques(upperBodyTorquesToPack.getSpineTorques());
    }
 
-   private NeckTorques doNeckControl()
+   private void setUpStateMachine()
    {
-      NeckTorques neckTorques = new NeckTorques();
+      State base = new BaseState();
+      State icpRecoverAccelerateState = new ICPRecoverAccelerateState();
+      State icpRecoverDecelerateState = new ICPRecoverDecelerateState();
 
+      StateTransitionCondition isICPOutsideLungeRadius = new IsICPOutsideLungeRadiusCondition();
+      StateTransitionCondition doWeNeedToDecelerate = new DoWeNeedToSlowDownDecelerate();
+      StateTransitionCondition isBodyAngularVelocityZero = new IsBodyAngularVelocityZeroCondition(1e-2);
+      
+      StateTransition toICPRecoverAccelerate = new StateTransition(icpRecoverAccelerateState.getStateEnum(), isICPOutsideLungeRadius);
+      StateTransition toICPRecoverDecelerate = new StateTransition(icpRecoverDecelerateState.getStateEnum(), doWeNeedToDecelerate);
+      StateTransition toBase = new StateTransition(base.getStateEnum(), isBodyAngularVelocityZero); //TODO now going back to base
+      
+      base.addStateTransition(toICPRecoverAccelerate);
+      icpRecoverAccelerateState.addStateTransition(toICPRecoverDecelerate);
+      icpRecoverDecelerateState.addStateTransition(toBase); //TODO now going back to base
+      
+      stateMachine.addState(base);
+      stateMachine.addState(icpRecoverAccelerateState);
+      stateMachine.addState(icpRecoverDecelerateState);
+      
+      stateMachine.setCurrentState(base.getStateEnum());
+   }
+
+   private enum BalancingUpperBodySubControllerState
+   {
+      BASE, ICP_REC_ACC, ICP_REC_DEC;
+   }
+
+   private class BaseState extends NoTransitionActionsState
+   {
+      public BaseState()
+      {
+         super(BalancingUpperBodySubControllerState.BASE);
+      }
+
+      @Override
+      public void doAction()
+      {
+         spineControlModule.doMaintainDesiredChestOrientation();
+      }
+   }
+
+   private class ICPRecoverAccelerateState extends NoTransitionActionsState
+   {
+      public ICPRecoverAccelerateState()
+      {
+         super(BalancingUpperBodySubControllerState.ICP_REC_ACC);
+      }
+
+      @Override
+      public void doAction()
+      {
+         // TODO Auto-generated method stub
+      }
+   }
+
+   private class ICPRecoverDecelerateState extends NoTransitionActionsState
+   {
+      public ICPRecoverDecelerateState()
+      {
+         super(BalancingUpperBodySubControllerState.ICP_REC_DEC);
+      }
+
+      @Override
+      public void doAction()
+      {
+         // TODO Auto-generated method stub
+      }
+   }
+
+   private class IsICPOutsideLungeRadiusCondition implements StateTransitionCondition
+   {
+      public boolean checkCondition()
+      {
+         //TODO implement
+         return false;
+      }
+   }
+
+   private class DoWeNeedToSlowDownDecelerate implements StateTransitionCondition
+   {
+      private double angularVelocity;
+      private double angularAcceleration;
+
+      public boolean checkCondition()
+      {
+         updateAngularVelocityAndAcceleration();
+         boolean doWeNeedToSlowDownBecauseOfAngleLimit = doWeNeedToSlowDownBecauseOfAngleLimit();
+         boolean willICPEndUpInsideStopLungingRadius = willICPEndUpFarEnoughBack();
+
+         return doWeNeedToSlowDownBecauseOfAngleLimit || willICPEndUpInsideStopLungingRadius;
+      }
+
+      private void updateAngularVelocityAndAcceleration()
+      {
+         //TODO implement
+      }
+
+      private boolean doWeNeedToSlowDownBecauseOfAngleLimit()
+      {
+         //TODO implement
+         return false;
+      }
+
+      private boolean willICPEndUpFarEnoughBack()
+      {
+         //TODO implement
+         return false;
+      }
+   }
+
+   private class IsBodyAngularVelocityZeroCondition implements StateTransitionCondition
+   {
+      private final double epsilon;
+
+      public IsBodyAngularVelocityZeroCondition(double epsilon)
+      {
+         this.epsilon = epsilon;
+      }
+
+      public boolean checkCondition()
+      {
+         // TODO implement
+         return false;
+      }
+   }
+
+   private void doNeckControl()
+   {
       for (NeckJointName neckJointName : NeckJointName.values())
       {
          PIDController pidController = neckControllers.get(neckJointName);
@@ -88,16 +217,14 @@ public class BalancingUpperBodySubController implements UpperBodySubController
          double torque = pidController.compute(actualPosition, desiredPosition, actualVelcoity, desiredVelocity, controlDT);
          neckTorques.setTorque(neckJointName, torque);
       }
-
-      return neckTorques;
    }
 
    private void populateYoVariables()
    {
       for (NeckJointName neckJointName : NeckJointName.values())
       {
-         String name = "desired" + neckJointName.getCamelCaseNameForMiddleOfExpression();
-         DoubleYoVariable variable = new DoubleYoVariable(name, registry);
+         String varName = "desired" + neckJointName.getCamelCaseNameForMiddleOfExpression();
+         DoubleYoVariable variable = new DoubleYoVariable(varName, registry);
 
          desiredNeckPositions.put(neckJointName, variable);
       }
@@ -122,5 +249,22 @@ public class BalancingUpperBodySubController implements UpperBodySubController
       neckControllers.get(NeckJointName.UPPER_NECK_PITCH).setDerivativeGain(5.0);
    }
 
-}
+   public abstract class NoTransitionActionsState extends State
+   {
+      public NoTransitionActionsState(Enum<?> stateEnum)
+      {
+         super(stateEnum);
+      }
 
+      @Override
+      public void doTransitionIntoAction()
+      {
+      }
+
+      @Override
+      public void doTransitionOutOfAction()
+      {
+      }
+   }
+
+}
