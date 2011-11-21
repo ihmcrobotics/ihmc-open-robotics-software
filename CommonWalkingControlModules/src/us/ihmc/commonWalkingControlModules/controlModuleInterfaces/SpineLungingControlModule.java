d38a925c018f5de45234669c438ab7bd572f147f
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2011-11-21 22:34:00 +0000

    Inverse Dynamics for the spine.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModuleInterfaces/SpineLungingControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModuleInterfaces/SpineLungingControlModule.java
@@ -1,12 +1,15 @@
 package us.ihmc.commonWalkingControlModules.controlModuleInterfaces;
 
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.SpineTorques;
+import us.ihmc.utilities.screwTheory.Wrench;
 
 public interface SpineLungingControlModule extends SpineControlModule
 {
    public abstract void doMaintainDesiredChestOrientation();
    
    public abstract void getSpineTorques(SpineTorques spineTorquesToPack);
+   
+   public abstract void setWrenchBetweenChestAndPelvis(Wrench wrench, SpineTorques spineTorquesToPack);
 
 }
 
