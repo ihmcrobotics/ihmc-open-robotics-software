17843f65bb43885be9bd66ec9495e85f96f8ddee
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2011-12-05 19:59:39 +0000

    Use inverse dynamics for arm control on R2 when balancing using upper body (lunging).

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/arm/ArmsSidewaysUsingIDArmControlModule.java
@@ -0,0 +1,49 @@
+package us.ihmc.commonWalkingControlModules.controlModules.arm;
+
+import java.util.EnumMap;
+
+import us.ihmc.commonWalkingControlModules.dynamics.FullRobotModel;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmJointName;
+import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
+import us.ihmc.robotSide.RobotSide;
+import us.ihmc.robotSide.SideDependentList;
+import us.ihmc.utilities.screwTheory.InverseDynamicsCalculator;
+import us.ihmc.utilities.screwTheory.RevoluteJoint;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class ArmsSidewaysUsingIDArmControlModule extends IDArmControlModule
+{
+   public ArmsSidewaysUsingIDArmControlModule(ProcessedSensorsInterface processedSensors, double controlDT, YoVariableRegistry parentRegistry, InverseDynamicsCalculator armsIDCalculator, SideDependentList<EnumMap<ArmJointName, RevoluteJoint>> armJointArrayLists)
+    {
+      super(processedSensors, controlDT, parentRegistry, armsIDCalculator, armJointArrayLists);
+   }
+
+   protected void computeDesireds()
+   {
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         desiredArmJointPositions.get(robotSide).get(ArmJointName.SHOULDER_ROLL).set(robotSide.negateIfRightSide(Math.PI / 1.2));
+         desiredArmJointPositions.get(robotSide).get(ArmJointName.SHOULDER_YAW).set(robotSide.negateIfRightSide(0.7));
+      }
+   }
+
+   protected void setGains()
+   {
+      
+      double gainScaling = 0.0;
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.SHOULDER_PITCH).setProportionalGain(gainScaling * 100.0);
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.SHOULDER_ROLL).setProportionalGain(gainScaling * 200.0);
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.SHOULDER_YAW).setProportionalGain(gainScaling * 100.0);
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.ELBOW).setProportionalGain(gainScaling * 100.0);
+
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.SHOULDER_PITCH).setDerivativeGain(gainScaling * 10.0);
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.SHOULDER_ROLL).setDerivativeGain(gainScaling * 20.0);
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.SHOULDER_YAW).setDerivativeGain(gainScaling * 10.0);
+         armQddReferenceControllers.get(robotSide).get(ArmJointName.ELBOW).setDerivativeGain(gainScaling * 10.0);
+      }
+   }
+}
+
---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/arm/IDArmControlModule.java
@@ -0,0 +1,121 @@
+package us.ihmc.commonWalkingControlModules.controlModules.arm;
+
+import java.util.EnumMap;
+
+import us.ihmc.commonWalkingControlModules.controlModuleInterfaces.ArmControlModule;
+import us.ihmc.commonWalkingControlModules.dynamics.FullRobotModel;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmJointName;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmTorques;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.LegJointName;
+import us.ihmc.commonWalkingControlModules.sensors.ProcessedSensorsInterface;
+import us.ihmc.robotSide.RobotSide;
+import us.ihmc.robotSide.SideDependentList;
+import us.ihmc.utilities.screwTheory.InverseDynamicsCalculator;
+import us.ihmc.utilities.screwTheory.RevoluteJoint;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.PIDController;
+
+public abstract class IDArmControlModule implements ArmControlModule
+{
+   protected final ProcessedSensorsInterface processedSensors;
+   protected final YoVariableRegistry registry = new YoVariableRegistry("IDControlModule");
+   
+   protected final InverseDynamicsCalculator armsIDCalculator;
+   protected final SideDependentList<EnumMap<ArmJointName, RevoluteJoint>> armJointArrayLists;
+   
+   protected final SideDependentList<EnumMap<ArmJointName, PIDController>> armQddReferenceControllers = SideDependentList.createListOfEnumMaps(ArmJointName.class);
+
+   protected final SideDependentList<EnumMap<ArmJointName, DoubleYoVariable>> desiredArmJointPositions = SideDependentList.createListOfEnumMaps(ArmJointName.class);
+   protected final SideDependentList<EnumMap<ArmJointName, DoubleYoVariable>> desiredArmJointVelocities = SideDependentList.createListOfEnumMaps(ArmJointName.class);
+
+//   protected final SideDependentList<EnumMap<ArmJointName, Double>> desiredArmJointAccelerations = SideDependentList.createListOfEnumMaps(ArmJointName.class);
+
+   private final double controlDT;
+   
+   public IDArmControlModule(ProcessedSensorsInterface processedSensors, double controlDT, YoVariableRegistry parentRegistry, InverseDynamicsCalculator armsIDCalculator, SideDependentList<EnumMap<ArmJointName, RevoluteJoint>> armJointArrayLists)
+   {
+      this.armJointArrayLists = armJointArrayLists;
+      this.armsIDCalculator = armsIDCalculator;
+      this.processedSensors = processedSensors;
+      this.controlDT = controlDT;
+      
+      populateYoVariables();
+      populateQddReferenceControllers();
+      setGains();
+      parentRegistry.addChild(registry);
+   }
+
+   protected abstract void computeDesireds();
+
+   protected abstract void setGains();
+
+   public void doArmControl(ArmTorques[] armTorquesToPack)
+   {
+      computeDesireds();
+
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         
+         EnumMap<ArmJointName, PIDController> armQddReferenceControllers = this.armQddReferenceControllers.get(robotSide);
+
+         for (ArmJointName armJointName : ArmJointName.values())
+         {
+            PIDController pidController = armQddReferenceControllers.get(armJointName);
+            double desiredPosition = desiredArmJointPositions.get(robotSide).get(armJointName).getDoubleValue();
+            double desiredVelocity = desiredArmJointVelocities.get(robotSide).get(armJointName).getDoubleValue();
+            
+            double desiredAcceleration = pidController.compute(processedSensors.getArmJointPosition(robotSide, armJointName), desiredPosition,
+                  processedSensors.getArmJointVelocity(robotSide, armJointName), desiredVelocity, controlDT);
+            
+//            desiredArmJointAccelerations.get(robotSide).put(armJointName, desiredAcceleration);
+            armJointArrayLists.get(robotSide).get(armJointName).setQddDesired(desiredAcceleration);
+         }
+      }
+      
+      armsIDCalculator.compute();
+      
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         ArmTorques armTorques = armTorquesToPack[robotSide.ordinal()];
+         for (ArmJointName armJointName : ArmJointName.values())
+         {
+            double torque = armJointArrayLists.get(robotSide).get(armJointName).getTau();
+            armTorques.setTorque(armJointName, torque);
+         }
+      }
+      
+
+   }
+
+   private void populateYoVariables()
+   {
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         for (ArmJointName armJointName : ArmJointName.values())
+         {
+            String name = "desired" + robotSide.getCamelCaseNameForMiddleOfExpression() + armJointName.getCamelCaseNameForMiddleOfExpression();
+
+            DoubleYoVariable positionVariable = new DoubleYoVariable(name + "Position", registry);
+            desiredArmJointPositions.get(robotSide).put(armJointName, positionVariable);
+
+            DoubleYoVariable velocityVariable = new DoubleYoVariable(name + "Velocity", registry);
+            desiredArmJointVelocities.get(robotSide).put(armJointName, velocityVariable);
+         }
+      }
+   }
+
+   
+   private void populateQddReferenceControllers()
+   {
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         for (ArmJointName armJointName : ArmJointName.values())
+         {
+            String name = robotSide.getCamelCaseNameForStartOfExpression() + armJointName.getCamelCaseNameForMiddleOfExpression();
+            armQddReferenceControllers.get(robotSide).put(armJointName, new PIDController(name, registry));
+         }
+      }
+   }
+}
