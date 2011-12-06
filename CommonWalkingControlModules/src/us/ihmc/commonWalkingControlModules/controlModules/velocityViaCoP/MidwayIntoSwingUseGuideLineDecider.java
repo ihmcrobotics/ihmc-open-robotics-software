2e73f39fe2d1e2ec9a909aa74ed73d6f2aaffbcb
Twan Koolen <tkoolen@ihmc.us>
2011-12-06 21:13:39 +0000

    YoVariablized the time to wait in early stance before using the guide line.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/velocityViaCoP/MidwayIntoSwingUseGuideLineDecider.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/velocityViaCoP/MidwayIntoSwingUseGuideLineDecider.java
@@ -1,17 +1,29 @@
 package us.ihmc.commonWalkingControlModules.controlModules.velocityViaCoP;
 
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
 import us.ihmc.commonWalkingControlModules.controllers.regularWalkingGait.SingleSupportCondition;
 import us.ihmc.utilities.math.geometry.FrameVector2d;
 
 public class MidwayIntoSwingUseGuideLineDecider implements UseGuideLineDecider
 {
+   private final String name = getClass().getSimpleName();
+   private final YoVariableRegistry registry = new YoVariableRegistry(name);
+   private final DoubleYoVariable earlyStanceWaitTime = new DoubleYoVariable("earlyStanceWaitTime", registry);
 
+   public MidwayIntoSwingUseGuideLineDecider(YoVariableRegistry parentRegistry)
+   {
+      parentRegistry.addChild(registry);
+      earlyStanceWaitTime.set(0.2);
+   }
+   
    public boolean useGuideLine(SingleSupportCondition singleSupportCondition, double timeInState, FrameVector2d desiredVelocity)
    {
       if (desiredVelocity.lengthSquared() == 0.0) return false;
       if (singleSupportCondition == SingleSupportCondition.StopWalking) return false;
       if (singleSupportCondition == SingleSupportCondition.Loading) return false;
-      if ((singleSupportCondition == SingleSupportCondition.EarlyStance) && (timeInState < 0.2))
+      if ((singleSupportCondition == SingleSupportCondition.EarlyStance) && (timeInState < earlyStanceWaitTime.getDoubleValue()))
       {
          return false;
       }
