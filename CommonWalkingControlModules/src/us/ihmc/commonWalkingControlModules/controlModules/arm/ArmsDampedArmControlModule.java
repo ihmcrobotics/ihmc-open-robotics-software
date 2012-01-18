c81102cf623cc3a81c74a686c003f923b5fa7c31
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2012-01-18 20:28:00 +0000

    Arm control modules for testing upper body balancing.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/arm/ArmsDampedArmControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/arm/ArmsDampedArmControlModule.java
@@ -6,37 +6,30 @@ import us.ihmc.robotSide.RobotSide;
 
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
-public class ArmsSidewaysArmControlModule extends PDArmControlModule
+public class ArmsDampedArmControlModule extends PDArmControlModule
 {
-   public ArmsSidewaysArmControlModule(ProcessedSensorsInterface processedSensors, double controlDT, YoVariableRegistry parentRegistry)
+   public ArmsDampedArmControlModule(ProcessedSensorsInterface processedSensors, double controlDT, YoVariableRegistry parentRegistry)
    {
       super(processedSensors, controlDT, parentRegistry);
    }
 
    protected void computeDesireds()
    {
-      for (RobotSide robotSide : RobotSide.values())
-      {
-         desiredArmJointPositions.get(robotSide).get(ArmJointName.SHOULDER_ROLL).set(robotSide.negateIfRightSide(Math.PI / 2.1));
-         desiredArmJointPositions.get(robotSide).get(ArmJointName.SHOULDER_YAW).set(robotSide.negateIfRightSide(0.7));
-      }
-      
-      RobotSide robotSide = RobotSide.LEFT;
-      desiredArmJointPositions.get(robotSide).get(ArmJointName.SHOULDER_ROLL).set(robotSide.negateIfRightSide(0.0));
-
+      // do nothing, keep at zero.
    }
 
    protected void setGains()
    {
       for (RobotSide robotSide : RobotSide.values())
       {
-         armControllers.get(robotSide).get(ArmJointName.SHOULDER_PITCH).setProportionalGain(100.0);
-         armControllers.get(robotSide).get(ArmJointName.SHOULDER_ROLL).setProportionalGain(200.0);
+         armControllers.get(robotSide).get(ArmJointName.SHOULDER_PITCH).setProportionalGain(0.0);
+         
+         armControllers.get(robotSide).get(ArmJointName.SHOULDER_ROLL).setProportionalGain(100.0);
          armControllers.get(robotSide).get(ArmJointName.SHOULDER_YAW).setProportionalGain(100.0);
          armControllers.get(robotSide).get(ArmJointName.ELBOW).setProportionalGain(100.0);
 
          armControllers.get(robotSide).get(ArmJointName.SHOULDER_PITCH).setDerivativeGain(10.0);
-         armControllers.get(robotSide).get(ArmJointName.SHOULDER_ROLL).setDerivativeGain(20.0);
+         armControllers.get(robotSide).get(ArmJointName.SHOULDER_ROLL).setDerivativeGain(10.0);
          armControllers.get(robotSide).get(ArmJointName.SHOULDER_YAW).setDerivativeGain(10.0);
          armControllers.get(robotSide).get(ArmJointName.ELBOW).setDerivativeGain(10.0);
       }
