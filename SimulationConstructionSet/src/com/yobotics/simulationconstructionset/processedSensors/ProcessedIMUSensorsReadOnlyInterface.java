b40ec63bbeba054dcc4774bb4165f017c7b12959
Peter Fankhauser <peterfa@student.ethz.ch>
2012-01-23 23:01:47 +0000

    *Added DynamicGraphic Class for VRML models *Added IMU Visualizer in ExoHomeUse *Extended IMU Processor Interface *Wrote PerfectIMUProcessor for ExoHomeUse

--- SimulationConstructionSet/src/com/yobotics/simulationconstructionset/processedSensors/ProcessedIMUSensorsReadOnlyInterface.java
+++ SimulationConstructionSet/src/com/yobotics/simulationconstructionset/processedSensors/ProcessedIMUSensorsReadOnlyInterface.java
@@ -9,9 +9,11 @@ import com.yobotics.simulationconstructionset.DoubleYoVariable;
 
 public interface ProcessedIMUSensorsReadOnlyInterface
 {
+   public abstract Quat4d getQuaternion(int imuIndex);
+   
    public abstract DoubleYoVariable[] getYawPitchRoll(int imuIndex);
 
-   public abstract Quat4d getQuaternion(int imuIndex);
-
    public abstract FrameVector getBodyAcceleration(int imuIndex);
+   
+   public abstract Vector3d getBodyAngularVelocity(int imuIndex);
 }
