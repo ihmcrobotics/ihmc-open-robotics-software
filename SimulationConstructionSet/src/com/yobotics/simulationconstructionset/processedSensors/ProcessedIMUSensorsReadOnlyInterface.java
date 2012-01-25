2737c8abfb471a3749051c6a2b013c05e3a4aa04
Twan Koolen <tkoolen@ihmc.us>
2012-01-25 15:12:59 +0000

    * removed dependency on BucknellHeadTwo (now BucknellHeadTwo depends on YoboticsBiped, but you don't need to check out BucknellHeadTwo to work on M2V2 software) * more work on getting the ProcessedIMUSensors interfaces to be the same; this required some tweaks in ExoHomeUse

--- SimulationConstructionSet/src/com/yobotics/simulationconstructionset/processedSensors/ProcessedIMUSensorsReadOnlyInterface.java
+++ SimulationConstructionSet/src/com/yobotics/simulationconstructionset/processedSensors/ProcessedIMUSensorsReadOnlyInterface.java
@@ -1,19 +1,22 @@
 package com.yobotics.simulationconstructionset.processedSensors;
 
 import javax.vecmath.Quat4d;
-import javax.vecmath.Vector3d;
 
 import us.ihmc.utilities.math.geometry.FrameVector;
 
-import com.yobotics.simulationconstructionset.DoubleYoVariable;
-
 public interface ProcessedIMUSensorsReadOnlyInterface
 {
-   public abstract Quat4d getQuaternion(int imuIndex);
+   public abstract FrameVector getAcceleration(int imuIndex);
    
-   public abstract DoubleYoVariable[] getYawPitchRoll(int imuIndex);
+   public abstract double getYaw(int imuIndex);
 
-   public abstract FrameVector getBodyAcceleration(int imuIndex);
+   public abstract double getPitch(int imuIndex);
+
+   public abstract double getRoll(int imuIndex);
    
-   public abstract Vector3d getBodyAngularVelocity(int imuIndex);
+   public abstract Quat4d getQuaternion(int imuIndex);
+
+   public abstract FrameVector getAngularVelocity(int imuIndex);
+   
+   public abstract FrameVector getAngularAcceleration();
 }
