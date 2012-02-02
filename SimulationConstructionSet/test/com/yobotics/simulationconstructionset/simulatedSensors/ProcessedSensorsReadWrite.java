2f8d1a291f969a48bf938636fcd48a458c8f6d33
Twan Koolen <tkoolen@ihmc.us>
2012-02-02 23:36:18 +0000

    * added method to set SpatialAccelerationVector based on origin acceleration * added another test for SimulatedIMURawSensorReader * simplified the inheritance diagram of SimulatedIMURawSensorReader by passing in the root body and the root acceleration, instead of having to subclass and create a TwistAndAccelerationCalculator.

---
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/simulatedSensors/ProcessedSensorsReadWrite.java
@@ -0,0 +1,115 @@
+package com.yobotics.simulationconstructionset.simulatedSensors;
+
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
+
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.math.geometry.RotationFunctions;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.processedSensors.ProcessedIMUSensorsReadOnlyInterface;
+import com.yobotics.simulationconstructionset.processedSensors.ProcessedIMUSensorsWriteOnlyInterface;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFrameVector;
+
+public class ProcessedSensorsReadWrite implements ProcessedIMUSensorsReadOnlyInterface, ProcessedIMUSensorsWriteOnlyInterface
+{
+   protected final YoVariableRegistry registry = new YoVariableRegistry("ProcessedSensors");
+   protected final DoubleYoVariable p_qs;
+   protected final DoubleYoVariable p_qx;
+   protected final DoubleYoVariable p_qy;
+   protected final DoubleYoVariable p_qz;
+   protected final DoubleYoVariable p_roll;
+   protected final DoubleYoVariable p_pitch;
+   protected final DoubleYoVariable p_yaw;
+   protected final YoFrameVector pd_w;
+   protected final YoFrameVector pdd_world;
+
+   public ProcessedSensorsReadWrite(ReferenceFrame imuReferenceFrame, YoVariableRegistry yoVariableRegistry)
+   {
+      p_qs = new DoubleYoVariable("p_q", registry);
+      p_qx = new DoubleYoVariable("p_qx", registry);
+      p_qy = new DoubleYoVariable("p_qy", registry);
+      p_qz = new DoubleYoVariable("p_qz", registry);
+
+      p_pitch = new DoubleYoVariable("p_pitch", registry);
+      p_roll = new DoubleYoVariable("p_roll", registry);
+      p_yaw = new DoubleYoVariable("p_yaw", registry);
+
+      pd_w = new YoFrameVector("pd_w", "", imuReferenceFrame, registry);
+      pdd_world = new YoFrameVector("pdd_", "_world", ReferenceFrame.getWorldFrame(), registry);
+
+      if (yoVariableRegistry != null)    // one of very few place where we can do this!
+      {
+         yoVariableRegistry.addChild(registry);
+      }
+   }
+
+   public void setRotation(Matrix3d rotationMatrix, int imuIndex)
+   {
+      Quat4d q = new Quat4d();
+      q.set(rotationMatrix);
+
+      p_qs.set(q.getW());
+      p_qx.set(q.getX());
+      p_qy.set(q.getY());
+      p_qz.set(q.getZ());
+
+      p_yaw.set(RotationFunctions.getYaw(rotationMatrix));
+      p_pitch.set(RotationFunctions.getPitch(rotationMatrix));
+      p_roll.set(RotationFunctions.getRoll(rotationMatrix));
+   }
+
+   public void setAcceleration(FrameVector accelerationInWorld, int imuIndex)
+   {
+      pdd_world.set(accelerationInWorld);
+   }
+
+   public void setAngularVelocityInBody(Vector3d angularVelocityInBody, int imuIndex)
+   {
+      pd_w.set(angularVelocityInBody);
+   }
+
+   public void setAngularAccelerationInBody(Vector3d angularAccelerationInBody, int imuIndex)
+   {
+      throw new RuntimeException("Not supported");
+   }
+
+   public Quat4d getQuaternion(int imuIndex)
+   {
+      return new Quat4d(p_qx.getDoubleValue(), p_qy.getDoubleValue(), p_qz.getDoubleValue(),
+                        p_qs.getDoubleValue());
+   }
+
+   public FrameVector getAcceleration(int imuIndex)
+   {
+      return pdd_world.getFrameVectorCopy();
+   }
+
+   public FrameVector getAngularVelocity(int imuIndex)
+   {
+      return pd_w.getFrameVectorCopy();
+   }
+
+   public double getYaw(int imuIndex)
+   {
+      return p_yaw.getDoubleValue();
+   }
+
+   public double getPitch(int imuIndex)
+   {
+      return p_pitch.getDoubleValue();
+   }
+
+   public double getRoll(int imuIndex)
+   {
+      return p_roll.getDoubleValue();
+   }
+
+   public FrameVector getAngularAcceleration(int imuIndex)
+   {
+      throw new RuntimeException("Not implemented");
+   }
+}
