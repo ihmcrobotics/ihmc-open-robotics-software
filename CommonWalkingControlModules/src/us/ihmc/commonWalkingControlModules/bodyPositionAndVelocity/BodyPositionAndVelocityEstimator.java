6cebb6fe77bced27fd26b0c9d34715015bba9f01
Twan Koolen <tkoolen@ihmc.us>
2012-02-09 16:36:48 +0000

    Moved BodyPositionAndVelocityEstimatorKalman to CommonWalkingControlModules.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/bodyPositionAndVelocity/BodyPositionAndVelocityEstimator.java
@@ -0,0 +1,7 @@
+package us.ihmc.commonWalkingControlModules.bodyPositionAndVelocity;
+
+import com.yobotics.simulationconstructionset.robotController.SensorProcessor;
+
+public interface BodyPositionAndVelocityEstimator extends SensorProcessor
+{
+}
