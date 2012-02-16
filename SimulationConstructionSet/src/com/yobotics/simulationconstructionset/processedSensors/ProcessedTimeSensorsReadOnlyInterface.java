26c3513037902fa2e6d4aa7b5f27f781ea66b51e
Peter Fankhauser <peterfa@student.ethz.ch>
2012-02-16 22:46:39 +0000

    * New MotionNodeServerCommunicator to pass in into MotionNodeIMUCommunicator for every IMU * Changed BodyPositionAndVelocityEstimatorKalman filter to be able to deal with variable time steps * SCS: New ProcessedTimeSensorReadOnlyInterface * ExoHomeUse: New RealRimeRawSensorReader * ExoHomeUse: Changes made for switch between real and simulated robot * ExoHomeUse: Added IMUCommunicators to IMUConfiguration

---
+++ SimulationConstructionSet/src/com/yobotics/simulationconstructionset/processedSensors/ProcessedTimeSensorsReadOnlyInterface.java
@@ -0,0 +1,9 @@
+package com.yobotics.simulationconstructionset.processedSensors;
+
+
+public interface ProcessedTimeSensorsReadOnlyInterface
+{
+   public abstract double getTime();
+   
+   public abstract double getDT();
+}
