1234f86e3adaf8d1d6e7ccb51f1da63a7a15a8b6
Matt Johnson <mjohnson@ihmc.us>
2012-06-07 14:27:47 +0000

    added Graphing utility

---
+++ Plotting/src/us/ihmc/plotting/GraphingUtility.java
@@ -0,0 +1,144 @@
+package us.ihmc.plotting;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.RangeType;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import javax.vecmath.Point2d;
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * User: mjohnson
+ * Date: 6/6/12
+ */
+public class GraphingUtility
+{
+   public static ChartPanel createGraph(String title, String xAxisLabel, String yAxisLabel, XYDataset dataset)
+   {
+      // create the chart
+      final JFreeChart chart = ChartFactory.createXYLineChart(title,   // chart title
+              xAxisLabel,                                              // x axis label
+              yAxisLabel,                                              // y axis label
+              dataset,                                                 // data
+              PlotOrientation.VERTICAL, true,                          // include legend
+              true,                                                    // tooltips
+              false                                                    // urls
+      );
+
+      // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+      chart.setBackgroundPaint(Color.white);
+
+      //        final StandardLegend legend = (StandardLegend) chart.getLegend();
+      //      legend.setDisplaySeriesShapes(true);
+
+      // get a reference to the plot for further customisation...
+      final XYPlot plot = chart.getXYPlot();
+      plot.setBackgroundPaint(Color.white);
+      //    plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+      plot.setDomainGridlinePaint(Color.lightGray);
+      plot.setRangeGridlinePaint(Color.lightGray);
+
+      final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+      //        renderer.setSeriesLinesVisible(0, false);
+      //        renderer.setSeriesShapesVisible(1, false);
+      plot.setRenderer(renderer);
+
+      // change the auto tick unit selection to integer units only...
+      final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+      rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+      ((NumberAxis) plot.getDomainAxis()).setAutoRange(false);
+
+      ChartPanel chartPanel = new ChartPanel(chart);
+      chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+      return chartPanel;
+   }
+
+   public static void displayGraph(String title, String xAxisLabel, String yAxisLabel, XYDataset dataset)
+   {
+      ChartPanel chartPanel = createGraph(title, xAxisLabel,yAxisLabel, dataset);
+      JFrame jFrame = new JFrame("Test Graph");
+      jFrame.getContentPane().add(chartPanel);
+      jFrame.pack();
+      jFrame.setVisible(true);
+   }
+
+   public static XYSeries createXYSeries(String seriesName, ArrayList<Point2d> data)
+   {
+      final XYSeries series1 = new XYSeries(seriesName);
+      for (int i = 0; i < data.size(); i++)
+      {
+         Point2d point2d = data.get(i);
+         series1.add(point2d.getX(), point2d.getY());
+      }
+
+      return series1;
+   }
+
+   public static XYSeriesCollection createXYSeriesCollection(ArrayList<XYSeries> dataSets)
+   {
+      final XYSeriesCollection dataSet = new XYSeriesCollection();
+      for (int i = 0; i < dataSets.size(); i++)
+      {
+         dataSet.addSeries(dataSets.get(i));
+      }
+      return dataSet;
+   }
+
+   public static void main(String[] args)
+   {
+      ArrayList<XYSeries> data = new ArrayList<XYSeries>();
+
+      ArrayList<Point2d> series1 = new ArrayList<Point2d>();
+      series1.add(new Point2d(3.0, 19.0));
+      series1.add(new Point2d(6.0, 22.0));
+      series1.add(new Point2d(9.0, 27.0));
+      series1.add(new Point2d(12.0, 31.0));
+      series1.add(new Point2d(15.0, 28.0));
+      series1.add(new Point2d(18.0, 22.0));
+      series1.add(new Point2d(21.0, 18.0));
+      XYSeries series1Set = GraphingUtility.createXYSeries("0 Acceleration", series1);
+      data.add(series1Set);
+
+      ArrayList<Point2d> series2 = new ArrayList<Point2d>();
+      series2.add(new Point2d(3.0, 18.0));
+      series2.add(new Point2d(6.0, 20.0));
+      series2.add(new Point2d(9.0, 22.0));
+      series2.add(new Point2d(12.0, 24.0));
+      series2.add(new Point2d(15.0, 31.0));
+      series2.add(new Point2d(18.0, 28.0));
+      series2.add(new Point2d(21.0, 21.0));
+      XYSeries series2Set = GraphingUtility.createXYSeries("-1 Acceleration", series2);
+      data.add(series2Set);
+
+      ArrayList<Point2d> series3 = new ArrayList<Point2d>();
+      series3.add(new Point2d(3.0, 20.0));
+      series3.add(new Point2d(6.0, 23.0));
+      series3.add(new Point2d(9.0, 31.0));
+      series3.add(new Point2d(12.0, 26.0));
+      series3.add(new Point2d(15.0, 21.0));
+      series3.add(new Point2d(18.0, 19.0));
+      series3.add(new Point2d(21.0, 17.0));
+      XYSeries series3Set = GraphingUtility.createXYSeries("+1 Acceleration", series3);
+      data.add(series3Set);
+
+      XYSeriesCollection xySeriesCollection = GraphingUtility.createXYSeriesCollection(data);
+
+      ChartPanel chartPanel = GraphingUtility.createGraph("BIN Noise vs. Flight Path Angle", "Flight Path Angle (degrees)", "BIN Noise", xySeriesCollection);
+
+      JFrame jFrame = new JFrame("Test Graph");
+      jFrame.getContentPane().add(chartPanel);
+      jFrame.pack();
+      jFrame.setVisible(true);
+   }
+}
