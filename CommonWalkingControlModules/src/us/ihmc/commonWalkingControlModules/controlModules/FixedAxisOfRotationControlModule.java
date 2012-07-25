b7458058689a896a1f83ac69e43653db6a124d41
Twan Koolen <tkoolen@ihmc.us>
2012-07-25 17:08:50 +0000

    More foot orientation control experiments on FixedBaseR2Leg. Can now pitch foot around toe while external force is being exerted at toe. Updates to FixedAxisOfRotationControlModule.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/FixedAxisOfRotationControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/FixedAxisOfRotationControlModule.java
@@ -1,50 +1,100 @@
 package us.ihmc.commonWalkingControlModules.controlModules;
 
+import javax.media.j3d.Transform3D;
 import javax.vecmath.AxisAngle4d;
-import javax.vecmath.Quat4d;
+import javax.vecmath.Matrix3d;
 import javax.vecmath.Vector3d;
 
 import us.ihmc.utilities.math.geometry.FramePoint;
 import us.ihmc.utilities.math.geometry.FramePose;
+import us.ihmc.utilities.math.geometry.FrameVector;
 import us.ihmc.utilities.math.geometry.Orientation;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.screwTheory.SpatialAccelerationVector;
 import us.ihmc.utilities.screwTheory.Twist;
 
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFrameOrientation;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFramePoint;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFrameVector;
+
 public class FixedAxisOfRotationControlModule
 {
+   private final YoVariableRegistry registry;
+
    private final ReferenceFrame bodyFrame;
    private final ReferenceFrame baseFrame;
+
+   private final YoFramePoint initialPosition;
+   private final YoFrameOrientation initialOrientation;
+   private final YoFrameVector axisOfRotation;
+   private final YoFramePoint offset;
 
    private final FramePose desiredPose;
    private final Twist desiredTwist = new Twist();
    private final SpatialAccelerationVector feedForward = new SpatialAccelerationVector();
 
-   public FixedAxisOfRotationControlModule(ReferenceFrame bodyFrame, ReferenceFrame baseFrame)
+   private final Vector3d zero = new Vector3d();
+
+   public FixedAxisOfRotationControlModule(String namePrefix, ReferenceFrame bodyFrame, ReferenceFrame baseFrame)
    {
+      this.registry = new YoVariableRegistry(namePrefix + getClass().getSimpleName());
       this.bodyFrame = bodyFrame;
       this.baseFrame = baseFrame;
       this.desiredPose = new FramePose(baseFrame);
+      this.initialPosition = new YoFramePoint(namePrefix + "InitialPosition", baseFrame, registry);
+      this.initialOrientation = new YoFrameOrientation(namePrefix + "InitialOrientation", "", baseFrame, registry);
+      this.axisOfRotation = new YoFrameVector(namePrefix + "Axis", baseFrame, registry);
+      this.offset = new YoFramePoint(namePrefix + "Offset", baseFrame, registry);
    }
 
-   public void compute(double q, double qd, double qdd, Vector3d axisOfRotation, Vector3d offset)
+   public void initialize(FrameVector axisOfRotation, FramePoint offset)
    {
-      Vector3d axisOfRotationDot = new Vector3d();
-      Vector3d offsetDot = new Vector3d();
+      Transform3D bodyToBase = bodyFrame.getTransformToDesiredFrame(baseFrame);
+      Vector3d translation = new Vector3d();
+      bodyToBase.get(translation);
+      this.initialPosition.set(translation);
+      this.initialOrientation.setRotation(bodyToBase);
+      
+      axisOfRotation.changeFrame(baseFrame);
+      offset.changeFrame(baseFrame);
+      
+      this.axisOfRotation.set(axisOfRotation);
+      this.offset.set(offset);
+   }
 
-      Vector3d positionVector3d = new Vector3d();
-      positionVector3d.cross(offset, axisOfRotation);
-      positionVector3d.scale(q);
-      desiredPose.setPosition(new FramePoint(desiredPose.getReferenceFrame(), positionVector3d));
+   public void compute(double q, double qd, double qdd)
+   {
+      FramePoint initial = initialPosition.getFramePointCopy();
+      FrameVector initialMinusOffset = new FrameVector(initial);
+      FramePoint offsetCopy = offset.getFramePointCopy();
+      initialMinusOffset.sub(offsetCopy);
 
-      AxisAngle4d axisAngle = new AxisAngle4d(axisOfRotation, q);
-      Quat4d quaternion = new Quat4d();
-      quaternion.set(axisAngle);
-      Orientation orientation = new Orientation(desiredPose.getReferenceFrame(), quaternion);
+      Vector3d axisOfRotationVector = axisOfRotation.getFrameVectorCopy().getVector();
+      AxisAngle4d axisAngle = new AxisAngle4d(axisOfRotationVector, q);
+      Matrix3d rotationMatrix = new Matrix3d();
+      rotationMatrix.set(axisAngle);
+
+      rotationMatrix.transform(initialMinusOffset.getVector());
+      FramePoint positionPoint = new FramePoint(initialMinusOffset);
+      positionPoint.add(offsetCopy);
+      desiredPose.setPosition(positionPoint);
+
+      Matrix3d initialRotation = new Matrix3d();
+      initialOrientation.getMatrix3d(initialRotation);
+      rotationMatrix.mul(initialRotation, rotationMatrix);
+
+      Orientation orientation = new Orientation(desiredPose.getReferenceFrame(), rotationMatrix);
       desiredPose.setOrientation(orientation);
 
-      desiredTwist.setScrew(bodyFrame, baseFrame, bodyFrame, qd, 0.0, axisOfRotation, offset);
-      feedForward.setScrew(bodyFrame, baseFrame, bodyFrame, qd, qdd, 0.0, 0.0, axisOfRotation, axisOfRotationDot, offset, offsetDot);
+      FrameVector axisOfRotation = this.axisOfRotation.getFrameVectorCopy();
+      FramePoint offset = this.offset.getFramePointCopy();
+      
+      axisOfRotation.changeFrame(bodyFrame);
+      offset.changeFrame(bodyFrame);
+      
+      desiredTwist.setScrew(bodyFrame, baseFrame, bodyFrame, qd, 0.0, axisOfRotation.getVector(), offset.getVectorCopy());
+      feedForward.setScrew(bodyFrame, baseFrame, bodyFrame, qd, qdd, 0.0, 0.0, axisOfRotation.getVector(), zero, offset.getVectorCopy(), zero);
    }
 
    public void pack(FramePose desiredPose, Twist desiredTwist, SpatialAccelerationVector feedForward)
