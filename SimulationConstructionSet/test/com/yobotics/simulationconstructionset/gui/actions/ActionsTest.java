6f6f61ab7f2685d454db3f856098bee950876226
Jerry Pratt <jpratt@ihmc.us>
2012-08-29 18:53:26 +0000

    Decided to use <command>CommandExecutor as the interface names for SCS commands such as simulate, play, etc. Added a few more. Continuing to abstract them all out.

--- SimulationConstructionSet/test/com/yobotics/simulationconstructionset/gui/actions/ActionsTest.java
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/gui/actions/ActionsTest.java
@@ -1,53 +1,72 @@
 package com.yobotics.simulationconstructionset.gui.actions;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.yobotics.simulationconstructionset.commands.AboutCommandExecutor;
+import com.yobotics.simulationconstructionset.commands.AddCameraKeyCommandExecutor;
+import com.yobotics.simulationconstructionset.commands.SimulateCommandExecutor;
 
 public class ActionsTest
 {
-   @BeforeClass
-   public static void setUpBeforeClass() throws Exception
-   {
-   }
-
-   @AfterClass
-   public static void tearDownAfterClass() throws Exception
-   {
-   }
-
-   @Before
-   public void setUp() throws Exception
-   {
-   }
-
-   @After
-   public void tearDown() throws Exception
-   {
-   }
-
    @Test
    public void testAboutAction()
    {
-      final boolean[] listenerCalled = new boolean[] {false};
+      final boolean[] executorCalled = new boolean[] {false};
 
-      AboutActionListener aboutActionListener = new AboutActionListener()
+      AboutCommandExecutor executor = new AboutCommandExecutor()
       {
          public void showAbout()
          {
-            listenerCalled[0] = true;
+            executorCalled[0] = true;
          }
       };
 
-      AboutAction aboutAction = new AboutAction(aboutActionListener);
+      AboutAction aboutAction = new AboutAction(executor);
       aboutAction.actionPerformed(null);
 
-      assertTrue(listenerCalled[0]);
-
+      assertTrue(executorCalled[0]);
    }
+
+   @Test
+   public void testAddCameraKeyAction()
+   {
+      final boolean[] executorCalled = new boolean[] {false};
+
+      AddCameraKeyCommandExecutor executor = new AddCameraKeyCommandExecutor()
+      {
+         public void addCameraKeyPoint()
+         {
+            executorCalled[0] = true;
+         }
+      };
+
+      AddCameraKeyAction action = new AddCameraKeyAction(executor);
+      action.actionPerformed(null);
+
+      assertTrue(executorCalled[0]);
+   }
+
+   @Test
+   public void testSimulateAction()
+   {
+      final boolean[] executorCalled = new boolean[] {false};
+
+      SimulateCommandExecutor executor = new SimulateCommandExecutor()
+      {
+         public void simulate()
+         {
+            executorCalled[0] = true;
+         }
+      };
+
+      SimulateAction action = new SimulateAction(executor);
+      action.actionPerformed(null);
+
+      assertTrue(executorCalled[0]);
+   }
+
+
 
 }
