8932139a494a8b23024fcd451c910743435af0ad
Jerry Pratt <jpratt@ihmc.us>
2012-10-04 22:06:38 +0000

    Added ContactController to DRCDemo0 so that the example simulation project didn't need to be in the classpath.

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ContactController.java
@@ -0,0 +1,80 @@
+package us.ihmc.darpaRoboticsChallenge;
+
+import java.util.ArrayList;
+
+import com.yobotics.simulationconstructionset.ExternalForcePoint;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.robotController.RobotController;
+import com.yobotics.simulationconstructionset.util.ground.Contactable;
+import com.yobotics.simulationconstructionset.util.ground.SimpleStickSlipContactModel;
+
+public class ContactController implements RobotController
+{
+   private static final long serialVersionUID = 1463656383530512645L;
+   
+   private final YoVariableRegistry registry = new YoVariableRegistry("ContactController");
+   
+   private final SimpleStickSlipContactModel contactModel;
+   
+   public ContactController()
+   {
+      contactModel = new SimpleStickSlipContactModel("simpleContact", registry);
+      initialize();
+   }
+   
+   public void addContactPoints(ArrayList<ExternalForcePoint> contactPoints)
+   {
+      for (ExternalForcePoint contactPoint : contactPoints)
+      {
+         addContactPoint(contactPoint);
+      }   
+   }
+   
+   public void addContactPoint(ExternalForcePoint contactPoint)
+   {
+      contactModel.addContactPoint(contactPoint);
+   }
+   
+   public void addContactables(ArrayList<Contactable> contactables)
+   {
+      for (Contactable contactable : contactables)
+      {
+         addContactable(contactable);
+      }
+   }
+   
+   public void addContactable(Contactable contactable)
+   {
+      contactModel.addContactable(contactable);
+   }
+   
+   public void initialize()
+   {
+      contactModel.setKContact(200.0);
+      contactModel.setBContact(10.0);
+      contactModel.setFrictionCoefficients(0.5, 0.3);
+   }
+  
+   
+   public void doControl()
+   {
+      contactModel.doContact();
+   }
+
+   public YoVariableRegistry getYoVariableRegistry()
+   {
+      return registry;
+   }
+
+   public String getName()
+   {
+      return registry.getName();
+   }
+
+   public String getDescription()
+   {
+     return getName();
+   }
+
+}
+
