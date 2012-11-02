e12f021855a2d21d3edf2912b030024b255be16e
Jerry Pratt <jpratt@ihmc.us>
2012-11-02 04:58:00 +0000

    Removed error messages from Synchronizers.

--- SimulationConstructionSet/src/com/yobotics/simulationconstructionset/gui/graphics/GraphicsSynchronizer.java
+++ SimulationConstructionSet/src/com/yobotics/simulationconstructionset/gui/graphics/GraphicsSynchronizer.java
@@ -8,8 +8,8 @@ public class GraphicsSynchronizer
    {
       if (createdOne)
       {
-         String errorMessage = "Should only ever create one Graphics Synchronizer in order for everything to stay synchronized!";
-         System.err.println(errorMessage);
+//         String errorMessage = "Should only ever create one Graphics Synchronizer in order for everything to stay synchronized!";
+//         System.err.println(errorMessage);
 //         throw new RuntimeException(errorMessage);
       }
       createdOne = true;
