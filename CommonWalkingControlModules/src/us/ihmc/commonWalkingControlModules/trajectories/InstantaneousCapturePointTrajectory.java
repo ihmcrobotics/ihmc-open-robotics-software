bbd75881be14b3217a70d5f1929a41775824ccf1
Jeff Van Egmond <jvanegmond@ihmc.us>
2012-11-02 02:55:50 +0000

    * Added Finishable as a super-interface for TrajectoryGenerator * Added the InstantaneousCapturePointTrajectory as an interface * Added ParabolicVelocityInstantaneousCapturePointTrajectory * Added InstantaneousCapturePointTrajectory to the BoxMovingState * Change getTrajectoryGenerators in BoxMovingState to getFinishables, which now incorporates the ICP trajectory. * Cleanup of the stateTransitionCondition is still required, as well as some left over of the use of icpTrajectoryGenerator as a field in BoxMovingHighLevelHumanoidController

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/trajectories/InstantaneousCapturePointTrajectory.java
@@ -0,0 +1,17 @@
+package us.ihmc.commonWalkingControlModules.trajectories;
+
+import us.ihmc.utilities.math.geometry.FramePoint2d;
+import us.ihmc.utilities.math.geometry.FrameVector2d;
+
+public interface InstantaneousCapturePointTrajectory extends Finishable
+{
+
+   public abstract void initialize(FramePoint2d initialDesiredICP, FramePoint2d finalDesiredICP, double moveTime, double omega0, double amountToBeInside);
+
+   public abstract void pack(FramePoint2d desiredPosition, FrameVector2d desiredVelocity, double omega0);
+
+   public abstract double getMoveTime();
+
+   public abstract void reset();
+
+}
