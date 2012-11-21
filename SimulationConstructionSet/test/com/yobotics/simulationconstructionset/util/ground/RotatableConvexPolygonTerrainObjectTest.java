1add97aefed6a9fbef41b84ce24d45bb6c512990
Jeff Van Egmond <jvanegmond@ihmc.us>
2012-11-21 21:46:44 +0000

    Added CombinedTerrainObjectTest and RotatableConvexPolygonTerrainObjectTest to the SCS test suite.

--- SimulationConstructionSet/test/com/yobotics/simulationconstructionset/util/ground/RotatableConvexPolygonTerrainObjectTest.java
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/util/ground/RotatableConvexPolygonTerrainObjectTest.java
@@ -3,7 +3,6 @@ package com.yobotics.simulationconstructionset.util.ground;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 
