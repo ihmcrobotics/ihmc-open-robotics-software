2f07ab15b7e3096bd284dbcffced35e5deba4dc9
Jerry Pratt <jpratt@ihmc.us>
2012-11-30 22:55:53 +0000

    Fixed combine in LinkGraphics. Fixed build path for a couple classes that weren't compiling.

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/LinkGraphics.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/LinkGraphics.java
@@ -102,6 +102,7 @@ public class LinkGraphics
     */
    public void combine(LinkGraphics linkGraphics)
    {
+      this.identity();
       this.linkGraphicsInstructions.addAll(linkGraphics.getLinkGraphicsInstructions());
    }
    
