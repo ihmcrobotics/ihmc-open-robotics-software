9b2881a2a26d9ec2f3920432cc64d4a3729f0f9d
Jesper Smith <jsmith@ihmc.us>
2012-12-05 21:49:28 +0000

    Added change-able link appearances to JME

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/instructions/LinkGraphicsInstruction.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/instructions/LinkGraphicsInstruction.java
@@ -8,7 +8,7 @@ public abstract class LinkGraphicsInstruction implements LinkGraphicsPrimitiveIn
    boolean hasChanged = false;
    protected YoAppearanceDefinition appearance = null;
 
-   public final boolean hasChangedSinceLastCalled()
+   public final boolean hasChanged()
    {
       return hasChanged;
    }
@@ -21,6 +21,7 @@ public abstract class LinkGraphicsInstruction implements LinkGraphicsPrimitiveIn
    public final void setAppearance(YoAppearanceDefinition appearance)
    {
       this.appearance = appearance;
+      hasChanged = true;
    }
 
    public final void reset()
