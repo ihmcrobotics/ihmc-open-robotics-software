a7f1cff5c8c4e857aa95a017fe0b93b33e47894c
Gray Thomas <gthomas@ihmc.us>
2012-12-06 20:50:41 +0000

    I made unit tests for the JMERenderer and the RayTraceLidarSensor. I also refactored the interface of Graphics3dAdapter to better represent the fuction of the pickingAdapter now known as the RayCollisionAdapter. Most of the unit tests pass, with the exception of the test which ensures the virtual objects are not hit by the Lidar. Jesper has volunteered to fix that one.

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/RayCollisionAdapter.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/RayCollisionAdapter.java
@@ -2,7 +2,7 @@ package us.ihmc.graphics3DAdapter;
 
 import us.ihmc.utilities.math.geometry.Ray;
 
-public interface PickingAdapter
+public interface RayCollisionAdapter
 {
 	public void setPickingGeometry(Ray ray);
 	public double getPickDistance();
