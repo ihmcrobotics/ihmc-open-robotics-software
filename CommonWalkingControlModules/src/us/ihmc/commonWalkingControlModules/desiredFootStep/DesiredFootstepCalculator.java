27772f0ac806e6f79b55f7a0f8ab15b91fb608fe
Twan Koolen <tkoolen@ihmc.us>
2012-12-06 21:23:31 +0000

    Got rid of setParameters methods in interface.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredFootStep/DesiredFootstepCalculator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredFootStep/DesiredFootstepCalculator.java
@@ -7,9 +7,4 @@ public interface DesiredFootstepCalculator
    public abstract Footstep updateAndGetDesiredFootstep(RobotSide supportLegSide);
 
    public abstract void initializeDesiredFootstep(RobotSide supportLegSide);
-
-   public abstract void setupParametersForM2V2();
-   
-   public abstract void setupParametersForR2();
-
 }
