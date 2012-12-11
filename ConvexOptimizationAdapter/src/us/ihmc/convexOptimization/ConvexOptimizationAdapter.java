1a26e092379a447715ee8dc4213c69745a6d4e74
Jerry Pratt <jpratt@ihmc.us>
2012-12-11 17:54:57 +0000

    Got newest JOptimizer, which supposedly handles SOCPs, but doesn't seem to quite yet. Added some more tests.

--- ConvexOptimizationAdapter/src/us/ihmc/convexOptimization/ConvexOptimizationAdapter.java
+++ ConvexOptimizationAdapter/src/us/ihmc/convexOptimization/ConvexOptimizationAdapter.java
@@ -2,7 +2,6 @@ package us.ihmc.convexOptimization;
 
 public interface ConvexOptimizationAdapter
 {
-
    public abstract void setLinearCostFunctionVector(double[] linearCostFunctionVector);
 
    public abstract void setQuadraticCostFunction(double[][] quadraticCostFunctionPMatrix, double[] quadraticCostFunctionQVector, double quadraticCostFunctionR);
@@ -12,6 +11,10 @@ public interface ConvexOptimizationAdapter
    public abstract void setLinearEqualityConstraintsBVector(double[] linearEqualityConstraintsBVector);
    
    public abstract void setLinearInequalityConstraints(double[][] linearInequalityConstraintCVectors, double[] linearInequalityConstraintBs);
+   
+   public abstract void addQuadraticInequalities(double[][] quadraticPMatrix, double[] quadraticQVector, double quadraticRScalar);
+
+   public abstract void addSecondOrderConeConstraints(double[][] secondOrderConeAMatrix, double[] secondOrderConeBVector, double[] secondOrderConeCVector, double secondOrderConeDScalar);
    
    public abstract double[] solve();
    
