ebae84d98d7320de224681a31d863292a2b7ec5e
Jesper Smith <jsmith@ihmc.us>
2012-12-11 22:41:39 +0000

    Implementing pickable objects for java 3d

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/SelectedListener.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/SelectedListener.java
@@ -1,10 +1,20 @@
 package us.ihmc.graphics3DAdapter;
 
 import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
 
 import us.ihmc.graphics3DAdapter.structure.Graphics3DNode;
 
 public interface SelectedListener
 {
-   public void selected(Graphics3DNode graphics3dNode, String modifierKey, Point3d location);
+   /**
+    * This function is called when a point on the screen is selected while holding the modifier key defined with getModifierKey()
+    * 
+    * @param graphics3dNode   The node that was selected, null if none selected
+    * @param modifierKeys TODO
+    * @param location location of the selected point
+    * @param cameraLocation camera position
+    * @param lookAtDirection camera view direction
+    */
+   public void selected(Graphics3DNode graphics3dNode, int[] modifierKeys, Point3d location, Point3d cameraLocation, Vector3d lookAtDirection);
 }
