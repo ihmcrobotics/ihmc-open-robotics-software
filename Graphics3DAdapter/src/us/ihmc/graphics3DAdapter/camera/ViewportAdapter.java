931af7d15bf0a309aedb3b205fa948bf77b0ad48
Jesper Smith <jsmith@ihmc.us>
2012-12-17 23:23:30 +0000

    Automatically register panels as key listeners.

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/ViewportAdapter.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/ViewportAdapter.java
@@ -17,7 +17,6 @@ public interface ViewportAdapter
    public abstract void setFieldOfView(double fieldOfView);
 
    public abstract void destroy();
-   public abstract void registerAsKeyListener();
    public abstract double getFieldOfView();
    public abstract double getPhysicalWidth();
    public abstract double getPhysicalHeight();
