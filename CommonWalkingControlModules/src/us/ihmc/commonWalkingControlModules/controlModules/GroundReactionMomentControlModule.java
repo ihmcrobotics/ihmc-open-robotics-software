1ab671ac21011ad5a5fe33c1e09d19c6c341068e
Twan Koolen <tkoolen@ihmc.us>
2012-12-19 22:53:49 +0000

    MomentumBasedController refactor part 2. Extracted out GroundReactionMomentControlModule.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/GroundReactionMomentControlModule.java
@@ -0,0 +1,50 @@
+package us.ihmc.commonWalkingControlModules.controlModules;
+
+import javax.vecmath.Matrix3d;
+
+import us.ihmc.utilities.math.geometry.AngleTools;
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.math.geometry.RotationFunctions;
+import us.ihmc.utilities.screwTheory.Momentum;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class GroundReactionMomentControlModule
+{
+   private final String name = getClass().getSimpleName();
+   private final YoVariableRegistry registry = new YoVariableRegistry(name);
+   private final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+   private final ReferenceFrame pelvisFrame;
+   private final DoubleYoVariable kAngularMomentumZ = new DoubleYoVariable("kAngularMomentumZ", registry);
+   private final DoubleYoVariable kPelvisYaw = new DoubleYoVariable("kPelvisYaw", registry);
+
+   public GroundReactionMomentControlModule(ReferenceFrame pelvisFrame, YoVariableRegistry parentRegistry)
+   {
+      this.pelvisFrame = pelvisFrame;
+      parentRegistry.addChild(registry);
+   }
+
+   public FrameVector determineGroundReactionMoment(Momentum momentum, double desiredPelvisYaw)
+   {
+      FrameVector ret = new FrameVector(worldFrame);
+      FrameVector angularMomentum = new FrameVector(momentum.getExpressedInFrame(), momentum.getAngularPartCopy());
+      angularMomentum.changeFrame(worldFrame);
+
+      Matrix3d pelvisToWorld = new Matrix3d();
+      pelvisFrame.getTransformToDesiredFrame(worldFrame).get(pelvisToWorld);
+      double pelvisYaw = RotationFunctions.getYaw(pelvisToWorld);
+
+      double error = AngleTools.computeAngleDifferenceMinusPiToPi(desiredPelvisYaw, pelvisYaw);
+      ret.setZ(-kAngularMomentumZ.getDoubleValue() * angularMomentum.getZ() + kPelvisYaw.getDoubleValue() * error);
+
+      return ret;
+   }
+
+   public void setGains(double kAngularMomentumZ, double kPelvisYaw)
+   {
+      this.kAngularMomentumZ.set(kAngularMomentumZ);
+      this.kPelvisYaw.set(kPelvisYaw);
+   }
+}
