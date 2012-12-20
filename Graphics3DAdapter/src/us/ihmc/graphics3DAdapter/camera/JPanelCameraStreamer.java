77b41e3fe420997e9e463e5c443e521cf58988ac
Jerry Pratt <jpratt@ihmc.us>
2012-12-20 21:33:12 +0000

    Got rid of VideoStreamerServer and VideoStreamerClient and replaced somewhat with Graphics3DStreamingVideoTCPServer. But right now it just creates a new Window. Someone needs to actually go and consume the frames better.

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/JPanelCameraStreamer.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/JPanelCameraStreamer.java
@@ -1,8 +1,11 @@
 package us.ihmc.graphics3DAdapter.camera;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.vecmath.Point3d;
 import javax.vecmath.Quat4d;
@@ -30,6 +33,23 @@ public class JPanelCameraStreamer extends JPanel implements CameraStreamer
       {
          g.drawImage(bufferedImage, 0, 0, this);
       }
+   }
+
+   public void createAndDisplayInNewWindow(String name, int xLocation, int yLocation)
+   {
+      JPanel panel = new JPanel(new BorderLayout());
+      panel.add("Center", this);
+
+      JFrame jFrame = new JFrame(name);
+      jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Container contentPane = jFrame.getContentPane();
+      contentPane.setLayout(new BorderLayout());
+      contentPane.add("Center", panel);
+
+      jFrame.pack();
+      jFrame.setVisible(true);
+      jFrame.setSize(800, 600);
+      jFrame.setLocation(xLocation, yLocation); 
    }
 
 }
