c0bbba7a182a65b15cb830636267599b5000d89c
Twan Koolen <tkoolen@ihmc.us>
2013-01-11 22:08:03 +0000

    Tuned parameters a little better for DRC robot.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCFlatGroundWalkingTrack.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCFlatGroundWalkingTrack.java
@@ -1,9 +1,5 @@
 package us.ihmc.darpaRoboticsChallenge;
 
-import java.awt.Component;
-
-import javax.vecmath.Vector3d;
-
 import us.ihmc.commonWalkingControlModules.automaticSimulationRunner.AutomaticSimulationRunner;
 import us.ihmc.commonWalkingControlModules.controllers.ControllerFactory;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.FlatGroundWalkingHighLevelHumanoidControllerFactory;
@@ -12,12 +8,10 @@ import us.ihmc.commonWalkingControlModules.terrain.TerrainType;
 import us.ihmc.darpaRoboticsChallenge.controllers.DRCRobotMomentumBasedControllerFactory;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.SquaredUpDRCRobotInitialSetup;
-import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
 
 import com.martiansoftware.jsap.JSAPException;
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
-import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicCheckBoxMenuItem;
 import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicObjectsListRegistry;
 import com.yobotics.simulationconstructionset.util.inputdevices.MidiSliderBoard;
 
@@ -27,7 +21,7 @@ public class DRCFlatGroundWalkingTrack
    private final DRCDemo01Environment environment;
 
    public DRCFlatGroundWalkingTrack(DRCGuiInitialSetup guiInitialSetup, AutomaticSimulationRunner automaticSimulationRunner, double timePerRecordTick,
-                   int simulationDataBufferSize, boolean doChestOrientationControl, boolean showInstructions)
+                   int simulationDataBufferSize, boolean doChestOrientationControl)
    {
       DRCSCSInitialSetup scsInitialSetup;
       DRCRobotInitialSetup drcRobotInitialSetup;
@@ -50,8 +44,14 @@ public class DRCFlatGroundWalkingTrack
       DynamicGraphicObjectsListRegistry dynamicGraphicObjectsListRegistry = new DynamicGraphicObjectsListRegistry();
       YoVariableRegistry registry = new YoVariableRegistry("adjustableParabolicTrajectoryDemoSimRegistry");
 
-      double desiredCoMHeight = 0.9;
-      HighLevelHumanoidControllerFactory highLevelHumanoidControllerFactory = new FlatGroundWalkingHighLevelHumanoidControllerFactory(desiredCoMHeight);
+      double desiredCoMHeight = 0.8;
+      double inPlaceWidth = 0.25;
+      double maxStepLength = 0.35;
+      double minStepWidth = 0.15;
+      double maxStepWidth = 0.4;
+      double stepPitch = 0.0;
+      HighLevelHumanoidControllerFactory highLevelHumanoidControllerFactory = new FlatGroundWalkingHighLevelHumanoidControllerFactory(desiredCoMHeight,
+                                                                                 inPlaceWidth, maxStepLength, minStepWidth, maxStepWidth, stepPitch);
       ControllerFactory controllerFactory = new DRCRobotMomentumBasedControllerFactory(highLevelHumanoidControllerFactory);
 
       environment = new DRCDemo01Environment();
@@ -79,9 +79,6 @@ public class DRCFlatGroundWalkingTrack
 
       simulationConstructionSet.setCameraPosition(6.0, -2.0, 4.5);
       simulationConstructionSet.setCameraFix(-0.44, -0.17, 0.75);
-      hideDynamicGraphicsForActualDemo(showInstructions);
-
-      setUpInstructionsBoard(simulationConstructionSet, showInstructions);
 
       if (automaticSimulationRunner != null)
       {
@@ -90,30 +87,6 @@ public class DRCFlatGroundWalkingTrack
       else
       {
          drcSimulation.start(null);
-      }
-   }
-
-   private void hideDynamicGraphicsForActualDemo(boolean showInstructions)
-   {
-      if (showInstructions)
-      {
-         Component[] components = drcSimulation.getSimulationConstructionSet().getDynamicGraphicMenuManager().getjMenu().getMenuComponents();
-         for (int j = 0; j < components.length; j++)
-         {
-            Component component = components[j];
-            if (component instanceof DynamicGraphicCheckBoxMenuItem)
-            {
-               DynamicGraphicCheckBoxMenuItem jCheckBox = (DynamicGraphicCheckBoxMenuItem) component;
-               if (jCheckBox.getText().equals("trajectoryEndPoint"))
-               {
-                  jCheckBox.setSelected(true);
-               }
-               else
-               {
-                  jCheckBox.setSelected(false);
-               }
-            }
-         }
       }
    }
 
@@ -131,17 +104,11 @@ public class DRCFlatGroundWalkingTrack
 
    public static void main(String[] args) throws JSAPException
    {
-      DRCDemo0ArgumentParser drcDemo0ArgumentParser = new DRCDemo0ArgumentParser(args);
-
-      boolean showInstructions = drcDemo0ArgumentParser.getShowInstructions();
-
       AutomaticSimulationRunner automaticSimulationRunner = null;
-
-      
 
       DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup();
 
-      new DRCFlatGroundWalkingTrack(guiInitialSetup, automaticSimulationRunner, 0.005, 16000, true, showInstructions);
+      new DRCFlatGroundWalkingTrack(guiInitialSetup, automaticSimulationRunner, 0.005, 16000, true);
    }
 
 
@@ -153,21 +120,5 @@ public class DRCFlatGroundWalkingTrack
    public DRCDemo01Environment getEnvironment()
    {
       return environment;
-   }
-
-   private void setUpInstructionsBoard(SimulationConstructionSet scs, boolean showInstructions)
-   {
-      if (showInstructions)
-      {
-         if (showInstructions)
-         {
-            Graphics3DObject linkGraphics = new Graphics3DObject();
-            linkGraphics.translate(new Vector3d(-1.0, -1.0, 0.0));
-            linkGraphics.rotate(Math.toRadians(90), Graphics3DObject.Z);
-            linkGraphics.scale(0.2);
-            linkGraphics.addModelFile(getClass().getResource("3ds/billboard.3DS"));
-            scs.addStaticLinkGraphics(linkGraphics);
-         }
-      }
    }
 }
