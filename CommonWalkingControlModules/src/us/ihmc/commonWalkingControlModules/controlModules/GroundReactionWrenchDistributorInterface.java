e3b485b3901d78ee95b7a414566951e128550c0c
Jerry Pratt <jpratt@ihmc.us>
2013-01-18 00:41:09 +0000

    Implemented more of GeometricFlatGroundReactionWrenchDistributor

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/GroundReactionWrenchDistributorInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/GroundReactionWrenchDistributorInterface.java
@@ -12,12 +12,18 @@ public interface GroundReactionWrenchDistributorInterface
 
    public abstract void addContact(PlaneContactState contactState, double coefficientOfFriction, double rotationalCoefficientOfFriction);
 
+   
+   //TODO: I think the desiredNetSpatialForceVector should be the total desired wrench on the body from the feet, and therefore include mg in it
+   // when this is called, instead of adding mg to it. The GroundReactionWrenchDistributor should not have to be created with m and g.
+   // But let's discuss more...
    public abstract void solve(SpatialForceVector desiredNetSpatialForceVector);
 
+   //TODO: Make these be pack methods instead of get methods.
+      
    public abstract FramePoint2d getCenterOfPressure(PlaneContactState contactState);
 
-   public abstract double getNormalTorque(PlaneContactState contactState);
-
    public abstract FrameVector getForce(PlaneContactState contactState);
+   
+   public abstract double getNormalTorque(PlaneContactState contactState);
 
 }
