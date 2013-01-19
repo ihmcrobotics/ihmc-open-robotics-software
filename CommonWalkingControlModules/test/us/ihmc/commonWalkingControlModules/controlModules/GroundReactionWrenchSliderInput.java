a7c0f4b0c5836fb34396f8676bfdcd09d6af7e2e
Jerry Pratt <jpratt@ihmc.us>
2013-01-19 19:55:02 +0000

    Improved WrenchDistributor Visualizer. Plus class for SliderInput.

---
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/controlModules/GroundReactionWrenchSliderInput.java
@@ -0,0 +1,139 @@
+package us.ihmc.commonWalkingControlModules.controlModules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.PlaneContactState;
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.YoPlaneContactState;
+import us.ihmc.utilities.ThreadTools;
+import us.ihmc.utilities.math.geometry.FramePoint2d;
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.math.geometry.TranslationReferenceFrame;
+import us.ihmc.utilities.screwTheory.SpatialForceVector;
+
+import com.yobotics.simulationconstructionset.Robot;
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicObjectsListRegistry;
+import com.yobotics.simulationconstructionset.util.inputdevices.MidiSliderBoard;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFramePoint;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFrameVector;
+
+public class GroundReactionWrenchSliderInput
+{
+   public static void main(String[] args)
+   {
+      DynamicGraphicObjectsListRegistry dynamicGraphicObjectsListRegistry = new DynamicGraphicObjectsListRegistry();
+
+      Robot nullRobot = new Robot("null");
+      SimulationConstructionSet scs = new SimulationConstructionSet(nullRobot);
+
+      int numberOfContacts = 2;
+      int maxNumberOfVertices = 10;
+      GroundReactionWrenchDistributorVisuzalizer visualizer = new GroundReactionWrenchDistributorVisuzalizer(numberOfContacts, maxNumberOfVertices, scs.getRootRegistry(), dynamicGraphicObjectsListRegistry);
+      
+      ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+      
+      TranslationReferenceFrame centerOfMassFrame = new TranslationReferenceFrame("centerOfMassFrame", worldFrame);
+      int nSupportVectors = 4;
+
+      YoVariableRegistry registry = new YoVariableRegistry("Wrench");
+      YoFramePoint centerOfMassPosition = new YoFramePoint("centerOfMass", "", worldFrame, registry);      
+      YoFrameVector forceOnCenterOfMass = new YoFrameVector("forceOnCenterOfMass", "", worldFrame, registry);      
+      YoFrameVector torqueOnCenterOfMass = new YoFrameVector("torqueOnCenterOfMass", "", worldFrame, registry);      
+      centerOfMassPosition.set(1.0, 0.5, 1.0);
+      forceOnCenterOfMass.set(10.0, 20.0, 1000.0);
+      torqueOnCenterOfMass.set(1.0, 2.0, 3.0);
+      
+      ArrayList<YoFrameVector> contactTranslations = new ArrayList<YoFrameVector>();
+      
+      GroundReactionWrenchDistributorInterface distributor = new GeometricFlatGroundReactionWrenchDistributor(registry, dynamicGraphicObjectsListRegistry);
+//    GroundReactionWrenchDistributorInterface distributor = new LeeGoswamiGroundReactionWrenchDistributor(centerOfMassFrame, nSupportVectors, parentRegistry);
+      
+      dynamicGraphicObjectsListRegistry.addDynamicGraphicsObjectListsToSimulationConstructionSet(scs);
+      
+      
+      ArrayList<PlaneContactState> contactStates = new ArrayList<PlaneContactState>();
+      double coefficientOfFriction = 0.7;
+      double rotationalCoefficientOfFriction = 0.02;
+      
+      double footLength = 0.3;
+      double footWidth = 0.15;
+      
+      for (int i=0; i<numberOfContacts; i++)
+      {
+         YoFrameVector contactTranslation = new YoFrameVector("contactTranslation"+i, worldFrame, registry);
+         
+         if (i==0)
+            contactTranslation.set(0.1, 0.5, 0.05);
+         if (i==1)
+            contactTranslation.set(0.0, -0.5, 0.02);
+
+         contactTranslations.add(contactTranslation );
+         
+         TranslationReferenceFrame planeFrame = new TranslationReferenceFrame("contact"+i, worldFrame);
+         planeFrame.updateTranslation(contactTranslation.getFrameVectorCopy());
+         planeFrame.update();
+         
+         YoPlaneContactState yoPlaneContactState = new YoPlaneContactState("contact" + i, planeFrame, registry);
+         
+         List<FramePoint2d> contactPoints = new ArrayList<FramePoint2d>();
+         contactPoints.add(new FramePoint2d(planeFrame, footLength/2.0, footWidth/2.0));
+         contactPoints.add(new FramePoint2d(planeFrame, footLength/2.0, -footWidth/2.0));
+         contactPoints.add(new FramePoint2d(planeFrame, -footLength/2.0, -footWidth/2.0));
+         contactPoints.add(new FramePoint2d(planeFrame, -footLength/2.0, footWidth/2.0));
+         
+         yoPlaneContactState.setContactPoints(contactPoints);
+         
+         contactStates.add(yoPlaneContactState);
+         distributor.addContact(yoPlaneContactState, coefficientOfFriction, rotationalCoefficientOfFriction);
+      }
+      
+      scs.addYoVariableRegistry(registry);
+      
+      MidiSliderBoard sliderBoard = new MidiSliderBoard(scs);
+      sliderBoard.setSlider(0, "forceOnCenterOfMassx", scs, -100.0, 100.0);
+      sliderBoard.setSlider(1, "forceOnCenterOfMassy", scs, -100.0, 100.0);
+      sliderBoard.setSlider(2, "forceOnCenterOfMassz", scs, 100.0, 2000.0);
+      
+      sliderBoard.setSlider(3, "torqueOnCenterOfMassx", scs, -20.0, 20.0);
+      sliderBoard.setSlider(4, "torqueOnCenterOfMassy", scs, -20.0, 20.0);
+      sliderBoard.setSlider(5, "torqueOnCenterOfMassz", scs, -20.0, 20.0);
+      
+      sliderBoard.setSlider(6, "contactTranslation0x", scs, -2.0, 2.0);
+      sliderBoard.setSlider(7, "contactTranslation0y", scs, -2.5, -0.3);
+      sliderBoard.setSlider(8, "contactTranslation0z", scs, -0.05, 0.3);
+      
+      sliderBoard.setSlider(9, "contactTranslation1x", scs, -2.0, 2.0);
+      sliderBoard.setSlider(10, "contactTranslation1y", scs, 0.3, 2.5);
+      sliderBoard.setSlider(11, "contactTranslation1z", scs, -0.05, 0.3);
+      
+      scs.startOnAThread();
+      
+      while(true)
+      {
+         centerOfMassFrame.updateTranslation(centerOfMassPosition.getFrameVectorCopy());
+         centerOfMassFrame.update();
+         
+         for (int i=0; i<numberOfContacts; i++)
+         {
+            PlaneContactState contactState = contactStates.get(i);
+            TranslationReferenceFrame planeFrame = (TranslationReferenceFrame) contactState.getPlaneFrame();
+            FrameVector contactTranslation = contactTranslations.get(i).getFrameVectorCopy();
+            planeFrame.updateTranslation(contactTranslation);
+            planeFrame.update();
+         }
+         
+         SpatialForceVector desiredNetSpatialForceVector = new SpatialForceVector(centerOfMassFrame);
+         desiredNetSpatialForceVector.setAngularPart(torqueOnCenterOfMass.getFrameVectorCopy().getVector());
+         desiredNetSpatialForceVector.setLinearPart(forceOnCenterOfMass.getFrameVectorCopy().getVector());
+         
+         distributor.solve(desiredNetSpatialForceVector);
+         
+         visualizer.update(scs, distributor, centerOfMassFrame, contactStates, desiredNetSpatialForceVector);
+         scs.tickAndUpdate();
+         ThreadTools.sleep(100L);
+      }
+   }
+}
