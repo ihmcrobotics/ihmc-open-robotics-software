44ef507625eaf546b009a8b880d20a1ee80a973d
Gray Thomas <gthomas@ihmc.us>
2013-01-21 21:39:32 +0000

    refactored the way the simulation is started, also some things in the footstep generator. Working towards a test of the footstep generator in isolation.

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotSDFLoader.java
@@ -0,0 +1,36 @@
+package us.ihmc.darpaRoboticsChallenge;
+
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
+import us.ihmc.SdfLoader.JaxbSDFLoader;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotJointMap;
+
+public class DRCRobotSDFLoader
+{
+   private static final String fileName = "Models/GFE/models/drc_robot/gfe.sdf";
+   private static final String modelName = "drc_robot";
+   private static final String resourceDirectory = "Models/GFE/models/";
+
+
+   public static JaxbSDFLoader loadDRCRobot()
+   {
+      JaxbSDFLoader jaxbSDFLoader;
+      try
+      {
+         jaxbSDFLoader = new JaxbSDFLoader(fileName, modelName, resourceDirectory, new DRCRobotJointMap());
+      }
+      catch (FileNotFoundException e)
+      {
+         throw new RuntimeException("Cannot find SDF file: " + e.getMessage());
+      }
+      catch (JAXBException e)
+      {
+         e.printStackTrace();
+
+         throw new RuntimeException("Invalid SDF file: " + e.getMessage());
+      }
+      return jaxbSDFLoader;
+   }
+}
