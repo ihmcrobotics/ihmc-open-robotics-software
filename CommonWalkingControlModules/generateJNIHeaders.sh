e6da59733d4c51b89b533417b43b1c06253502dc
Twan Koolen <tkoolen@ihmc.us>
2013-01-22 16:50:44 +0000

    Adding native code for ContactPointWrenchOptimizer.

--- CommonWalkingControlModules/generateJNIHeaders.sh
+++ CommonWalkingControlModules/generateJNIHeaders.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 javah -classpath ../IHMCUtilities/classes:./classes -o csrc/LeeGoswamiForceOptimizerNative.h us.ihmc.commonWalkingControlModules.controlModules.nativeOptimization.LeeGoswamiForceOptimizerNative
 javah -classpath ../IHMCUtilities/classes:./classes -o csrc/LeeGoswamiCoPAndNormalTorqueOptimizerNative.h us.ihmc.commonWalkingControlModules.controlModules.nativeOptimization.LeeGoswamiCoPAndNormalTorqueOptimizerNative
+javah -classpath ../IHMCUtilities/classes:./classes -o csrc/ContactPointWrenchOptimizerNative.h us.ihmc.commonWalkingControlModules.controlModules.nativeOptimization.ContactPointWrenchOptimizerNative
 
