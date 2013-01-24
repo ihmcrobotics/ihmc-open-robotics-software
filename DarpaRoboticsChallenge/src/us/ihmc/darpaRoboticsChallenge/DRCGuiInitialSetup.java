c7a3f405515070b74e641c9207ec4b03116a1f81
Twan Koolen <tkoolen@ihmc.us>
2013-01-24 21:44:06 +0000

    Fixing up flat ground walking track / walking course stuff part 3: generified GUIInitialSetup.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCGuiInitialSetup.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCGuiInitialSetup.java
@@ -1,27 +1,18 @@
 package us.ihmc.darpaRoboticsChallenge;
 
-import javax.swing.JButton;
-
-import us.ihmc.graphics3DAdapter.Graphics3DAdapter;
 import us.ihmc.graphics3DAdapter.camera.CameraConfiguration;
-import us.ihmc.graphics3DAdapter.jme.JMEGraphics3dAdapter;
-import us.ihmc.projectM.R2Sim02.DataExporter;
-import us.ihmc.projectM.R2Sim02.R2Robot;
+import us.ihmc.projectM.R2Sim02.initialSetup.GuiInitialSetup;
 
 import com.yobotics.simulationconstructionset.Robot;
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
 import com.yobotics.simulationconstructionset.SupportedGraphics3DAdapter;
 
-public class DRCGuiInitialSetup
+public class DRCGuiInitialSetup implements GuiInitialSetup<Robot>
 {
    private SupportedGraphics3DAdapter graphics3dAdapter = SupportedGraphics3DAdapter.JAVA_MONKEY_ENGINE;
 
    public void initializeGUI(SimulationConstructionSet scs, Robot robot)
    {
-      setUpGUI(scs);
-
-      setupSliderBoard(scs);
-
       CameraConfiguration behindPelvis = new CameraConfiguration("BehindPelvis");
       behindPelvis.setCameraTracking(false, true, true, false);
       behindPelvis.setCameraDolly(false, true, true, false);
@@ -38,24 +29,6 @@ public class DRCGuiInitialSetup
       scs.setupCamera(camera6);
    }
 
-   private void setupSliderBoard(SimulationConstructionSet scs)
-   {
-      //    EvolutionUC33ESetup setup = new EvolutionUC33ESetup(scs);
-      //    setup.setupEvolutionForDrivingUpperBodyDesiredPosition();
-   }
-
-   private void setUpGUI(SimulationConstructionSet scs)
-   {
-   }
-
-   public void initializeGUI(SimulationConstructionSet scs, R2Robot robot)
-   {
-      JButton exportTorqueAndSpeedButton = new JButton("Export Torque And Speed");
-      DataExporter dataExporter = new DataExporter(scs, robot);
-      exportTorqueAndSpeedButton.addActionListener(dataExporter);
-      scs.addButton(exportTorqueAndSpeedButton);
-   }
-
    public SupportedGraphics3DAdapter getGraphics3dAdapter()
    {
       return graphics3dAdapter;
@@ -64,5 +37,10 @@ public class DRCGuiInitialSetup
    public void setGraphics3dAdapter(SupportedGraphics3DAdapter graphics3dAdapter)
    {
       this.graphics3dAdapter = graphics3dAdapter;
+   }
+
+   public boolean isGuiShown()
+   {
+      return true;
    }
 }
