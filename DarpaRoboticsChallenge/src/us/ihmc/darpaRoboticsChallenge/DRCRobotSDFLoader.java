7852350bccb38248e0cafdac3ef88c9efad4c9d4
Matt Johnson <mjohnson@ihmc.us>
2013-01-29 21:33:18 +0000

    Added combo box in startup options dialog to select the GFE model to use (no hands, robot hands, sandia hands).

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotSDFLoader.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotSDFLoader.java
@@ -9,13 +9,42 @@ import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotJointMap;
 
 public class DRCRobotSDFLoader
 {
-   private static final String fileName = "Models/GFE/models/drc_robot/gfe.sdf";
-   private static final String modelName = "drc_robot";
-   private static final String resourceDirectory = "Models/GFE/models/";
+   public static RobotModel selectedModel = RobotModel.ATLAS_NO_HANDS;
 
+   private static String fileName = "Models/GFE/models/drc_robot/gfe.sdf";
+   private static String modelName = "drc_robot";
+   private static String resourceDirectory = "Models/GFE/models/";
+
+   public static enum RobotModel {ATLAS_NO_HANDS, ATLAS_IROBOT_HANDS, ATLAS_SANDIA_HANDS;}
 
    public static JaxbSDFLoader loadDRCRobot()
    {
+      switch (selectedModel)
+      {
+         case ATLAS_NO_HANDS :
+            fileName = "Models/GFE/models/drc_robot/gfe.sdf";
+            modelName = "drc_robot";
+            resourceDirectory = "Models/GFE/models/";
+            break;
+
+         case ATLAS_IROBOT_HANDS :
+            fileName = "Models/GFE/atlas_irobot_hands.sdf";
+            modelName = "atlas";
+            resourceDirectory = "Models/GFE/models/";
+            break;
+
+         case ATLAS_SANDIA_HANDS :
+            fileName = "Models/GFE/atlas_sandia_hands.sdf";
+            modelName = "atlas";
+            resourceDirectory = "Models/GFE/models/";
+            
+            //modelName = "atlas";
+            //fileName = "Models/GFE/models/atlas_sandia_hand/atlas.sdf";
+            //resourceDirectory = "Models/GFE/models/";
+
+            break;
+      }
+
       JaxbSDFLoader jaxbSDFLoader;
       try
       {
@@ -31,6 +60,7 @@ public class DRCRobotSDFLoader
 
          throw new RuntimeException("Invalid SDF file: " + e.getMessage());
       }
+
       return jaxbSDFLoader;
    }
 }
