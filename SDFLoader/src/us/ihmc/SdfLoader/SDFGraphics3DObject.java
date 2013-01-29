41ba96de1f8c64c2cd1974b6f0d6d4877ae1cd27
Matt Johnson <mjohnson@ihmc.us>
2013-01-29 15:58:32 +0000

    added handling of cylinder, sphere, and box geometries.

--- SDFLoader/src/us/ihmc/SdfLoader/SDFGraphics3DObject.java
+++ SDFLoader/src/us/ihmc/SdfLoader/SDFGraphics3DObject.java
@@ -26,41 +26,64 @@ public class SDFGraphics3DObject extends Graphics3DObject
       for(SDFVisual sdfVisual : sdfVisuals)
       {
          Transform3D visualPose = SDFConversionsHelper.poseToTransform(sdfVisual.getPose());
+         Vector3d modelOffset = new Vector3d();
+         Matrix3d modelRotation = new Matrix3d();
+         visualPose.get(modelRotation, modelOffset);
+         
+         if (SHOW_COORDINATE_SYSTEMS)
+         {
+            addCoordinateSystem(0.1);         
+         }
+         translate(modelOffset);
+         rotate(modelRotation);       
          
          if(sdfVisual.getGeometry().getMesh() != null)
          {
             String uri = convertToFullPath(resourceDirectory, sdfVisual.getGeometry().getMesh().getUri());
             addMesh(uri, visualPose);
          }
+         else if(sdfVisual.getGeometry().getCylinder() != null)
+         {
+            double length = Double.parseDouble(sdfVisual.getGeometry().getCylinder().getLength());
+            double radius = Double.parseDouble(sdfVisual.getGeometry().getCylinder().getRadius()); 
+            addCylinder(length, radius, YoAppearance.White());
+         }
+         else if(sdfVisual.getGeometry().getBox() != null)
+         {
+            String[] boxDimensions = sdfVisual.getGeometry().getBox().getSize().split(" ");            
+            double bx = Double.parseDouble(boxDimensions[0]);
+            double by = Double.parseDouble(boxDimensions[1]);
+            double bz = Double.parseDouble(boxDimensions[2]);
+            addCube(bx, by, bz, YoAppearance.AluminumMaterial());
+         }
+         else if(sdfVisual.getGeometry().getSphere() != null)
+         {
+            double radius = Double.parseDouble(sdfVisual.getGeometry().getSphere().getRadius());
+            addSphere(radius, YoAppearance.White());
+         }
          else
          {
             System.err.println("Visual for " + sdfVisual.getName() + " not implemented yet");
          }
+
+         identity();
       }
+      identity();
    }
    
    private void addMesh(String mesh, Transform3D visualPose)
    {
-      Vector3d offset = new Vector3d();
-      Matrix3d rotation = new Matrix3d();
-      visualPose.get(rotation, offset);
+      
 
       AppearanceDefinition appearance = null;
       if (ModelFileType.getFileType(mesh) == ModelFileType._STL)
       {
          appearance = YoAppearance.BlackMetalMaterial(); // Otherwise it becomes a white blob
       }
-      if (SHOW_COORDINATE_SYSTEMS)
-      {
-         addCoordinateSystem(0.1);         
-      }
-
-      translate(offset);
-      rotate(rotation);
+      
 
       addModelFile(mesh, appearance);
 
-      identity();
    }
    
    private String convertToFullPath(String resourceDirectory, String meshPath)
