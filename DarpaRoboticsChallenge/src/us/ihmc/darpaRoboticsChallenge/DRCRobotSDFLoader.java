95dd6526d639afb54dd4c0b274899b87abe56392
Gray Thomas <gthomas@ihmc.us>
2013-01-31 17:44:05 +0000

    moving the robot model directory. Now bamboo will be able to find the model files.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotSDFLoader.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotSDFLoader.java
@@ -1,6 +1,7 @@
 package us.ihmc.darpaRoboticsChallenge;
 
 import java.io.FileNotFoundException;
+import java.net.URL;
 
 import javax.xml.bind.JAXBException;
 
@@ -11,44 +12,44 @@ public class DRCRobotSDFLoader
 {
    public static RobotModel selectedModel = RobotModel.ATLAS_NO_HANDS;
 
-   private static String fileName = "Models/GFE/models/drc_robot/gfe.sdf";
-   private static String modelName = "drc_robot";
-   private static String resourceDirectory = "Models/GFE/models/";
+   
 
    public static enum RobotModel {ATLAS_NO_HANDS, ATLAS_IROBOT_HANDS, ATLAS_SANDIA_HANDS;}
 
    public static JaxbSDFLoader loadDRCRobot()
    {
+      URL fileURL;
+      String modelName;
+      URL resourceDirectory;
+      Class<DRCRobotSDFLoader> myClass = DRCRobotSDFLoader.class;
       switch (selectedModel)
       {
          case ATLAS_NO_HANDS :
-            fileName = "Models/GFE/models/drc_robot/gfe.sdf";
+            fileURL = myClass.getResource("models/GFE/models/drc_robot/gfe.sdf");
             modelName = "drc_robot";
-            resourceDirectory = "Models/GFE/models/";
+            resourceDirectory = myClass.getResource("models/GFE/models/");
             break;
 
          case ATLAS_IROBOT_HANDS :
-            fileName = "Models/GFE/atlas_irobot_hands.sdf";
+            fileURL = myClass.getResource("models/GFE/atlas_irobot_hands.sdf");
             modelName = "atlas";
-            resourceDirectory = "Models/GFE/models/";
+            resourceDirectory = myClass.getResource("models/GFE/models/");
             break;
 
          case ATLAS_SANDIA_HANDS :
-            fileName = "Models/GFE/atlas_sandia_hands.sdf";
+            fileURL = myClass.getResource("models/GFE/atlas_sandia_hands.sdf");
             modelName = "atlas";
-            resourceDirectory = "Models/GFE/models/";
-            
-            //modelName = "atlas";
-            //fileName = "Models/GFE/models/atlas_sandia_hand/atlas.sdf";
-            //resourceDirectory = "Models/GFE/models/";
+            resourceDirectory = myClass.getResource("models/GFE/models/");
 
             break;
+         default:
+            throw new RuntimeException("DRCRobotSDFLoader: Unimplemented enumeration case : " +selectedModel);
       }
 
       JaxbSDFLoader jaxbSDFLoader;
       try
       {
-         jaxbSDFLoader = new JaxbSDFLoader(fileName, modelName, resourceDirectory, new DRCRobotJointMap());
+         jaxbSDFLoader = new JaxbSDFLoader(fileURL, modelName, resourceDirectory, new DRCRobotJointMap());
       }
       catch (FileNotFoundException e)
       {
