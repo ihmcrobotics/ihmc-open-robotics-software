bb52ebb0e715037274bb8ffbc3a6a605e6a36a82
Jerry Pratt <jpratt@ihmc.us>
2013-02-05 18:13:55 +0000

    Cleaning up CenterOfMassHeightTrajectoryGenerators to be more like ControlFlowElements. Got rid of the compute method, realized we need the initialize method and will later need to figure out how to deal with things like this in ControlFlow.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/trajectories/CenterOfMassHeightInputData.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/trajectories/CenterOfMassHeightInputData.java
@@ -1,6 +1,23 @@
 package us.ihmc.commonWalkingControlModules.trajectories;
 
+import us.ihmc.robotSide.RobotSide;
+
 public class CenterOfMassHeightInputData
 {
+   private RobotSide supportLeg;
+   
+   public CenterOfMassHeightInputData()
+   {
+      
+   }
 
+   public RobotSide getSupportLeg()
+   {
+      return supportLeg;
+   }
+
+   public void setSupportLeg(RobotSide supportLeg)
+   {
+      this.supportLeg = supportLeg;
+   }
 }
