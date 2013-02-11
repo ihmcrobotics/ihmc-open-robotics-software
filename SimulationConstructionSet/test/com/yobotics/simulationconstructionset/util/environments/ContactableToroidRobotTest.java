5630d9ea576429a9a0e80631afd0e433ccb6b25c
Douglas Stephen <dstephen@ihmc.us>
2013-02-11 19:03:06 +0000

    Small fix in ContactableToroidRobot; added simple test for whether or not a point is in/on the torus.

---
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/util/environments/ContactableToroidRobotTest.java
@@ -0,0 +1,127 @@
+package com.yobotics.simulationconstructionset.util.environments;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import javax.media.j3d.Transform3D;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import org.junit.Test;
+
+import us.ihmc.utilities.RandomTools;
+
+public class ContactableToroidRobotTest
+{
+   @Test
+   public void testIsPointOnOrInsideAtOrigin()
+   {
+      double majorRadius = ContactableToroidRobot.DEFAULT_RADIUS;
+      double minorRadius = ContactableToroidRobot.DEFAULT_THICKNESS;
+      double delta = 1e-4;
+
+      ContactableToroidRobot bot = new ContactableToroidRobot("bot", new Transform3D(), new Vector3d(0, 0, 0));
+
+      Point3d pt = new Point3d();
+
+      for (double x = majorRadius - minorRadius; x < majorRadius + minorRadius; x += delta)
+      {
+         for (double y = majorRadius - minorRadius; y < majorRadius + minorRadius; y += delta)
+         {
+            for (double z = -minorRadius; z < minorRadius; z += delta)
+            {
+               double x_sq = x * x;
+               double y_sq = y * y;
+               double z_sq = z * z;
+               double rma_sq = (majorRadius + minorRadius) * (majorRadius + minorRadius);
+               double rmi_sq = (minorRadius * minorRadius);               
+
+               if ((x_sq + y_sq < rma_sq) &&  ((x_sq + y_sq) + z_sq < rmi_sq))
+               {
+                  pt.set(x, y, z);
+                  assertTrue("Nope: " + pt, bot.isPointOnOrInside(pt));
+               }
+            }
+         }
+      }
+   }
+   
+   @Test
+   public void testIsPointOnOrInsideNotAtOriginUsingTransform()
+   {
+      Random random = new Random(1972L);
+      
+      double majorRadius = ContactableToroidRobot.DEFAULT_RADIUS;
+      double minorRadius = ContactableToroidRobot.DEFAULT_THICKNESS;
+      double delta = 1e-4;
+      
+      Vector3d randomVector = RandomTools.generateRandomVector(random);
+      Transform3D transform3d = new Transform3D();
+      transform3d.setTranslation(randomVector);
+
+      ContactableToroidRobot bot = new ContactableToroidRobot("bot", transform3d, new Vector3d(0, 0, 0));
+
+      Point3d pt = new Point3d();
+      
+      for (double x = randomVector.x + majorRadius - minorRadius; x < randomVector.x + majorRadius + minorRadius; x += delta)
+      {
+         for (double y = randomVector.y + majorRadius - minorRadius; y < randomVector.y + majorRadius + minorRadius; y += delta)
+         {
+            for (double z = randomVector.z - minorRadius; z < randomVector.z + minorRadius; z += delta)
+            {
+               double x_sq = x * x;
+               double y_sq = y * y;
+               double z_sq = z * z;
+               double rma_sq = (randomVector.x + majorRadius + minorRadius) * (randomVector.x + majorRadius + minorRadius);
+               double rmi_sq = (minorRadius * minorRadius);               
+
+               if ((x_sq + y_sq < rma_sq) &&  ((x_sq + y_sq) + z_sq < rmi_sq))
+               {
+                  pt.set(x, y, z);
+                  assertTrue("Nope: " + pt, bot.isPointOnOrInside(pt));
+               }
+            }
+         }
+      }
+   }
+   
+   @Test
+   public void testPointIsntInsideWhenUsingComOffset()
+   {
+      Random random = new Random(1972L);
+      
+      double majorRadius = ContactableToroidRobot.DEFAULT_RADIUS;
+      double minorRadius = ContactableToroidRobot.DEFAULT_THICKNESS;
+      double delta = 1e-4;
+      
+      Vector3d randomVector = RandomTools.generateRandomVector(random);
+      Transform3D transform3d = new Transform3D();      
+
+      ContactableToroidRobot bot = new ContactableToroidRobot("bot", transform3d, randomVector);
+
+      Point3d pt = new Point3d();
+      
+      for (double x = randomVector.x + majorRadius - minorRadius; x < randomVector.x + majorRadius + minorRadius; x += delta)
+      {
+         for (double y = randomVector.y + majorRadius - minorRadius; y < randomVector.y + majorRadius + minorRadius; y += delta)
+         {
+            for (double z = randomVector.z - minorRadius; z < randomVector.z + minorRadius; z += delta)
+            {
+               double x_sq = x * x;
+               double y_sq = y * y;
+               double z_sq = z * z;
+               double rma_sq = (randomVector.x + majorRadius + minorRadius) * (randomVector.x + majorRadius + minorRadius);
+               double rmi_sq = (minorRadius * minorRadius);               
+
+               if ((x_sq + y_sq < rma_sq) &&  ((x_sq + y_sq) + z_sq < rmi_sq))
+               {
+                  pt.set(x, y, z);
+                  assertFalse("Nope: " + pt, bot.isPointOnOrInside(pt));
+               }
+            }
+         }
+      }
+   }
+
+}
