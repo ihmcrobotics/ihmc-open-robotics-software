3eb15104725029282bd288a0f0a2aed4380d5f20
Nathan Mertins <nmertins@ihmc.us>
2013-02-13 16:17:11 +0000

    factored out Ellipsoid3d class from Graphics3DStaticEllipsoidExample

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/examples/Graphics3DStaticEllipsoidExample.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/examples/Graphics3DStaticEllipsoidExample.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import javax.vecmath.Point3d;
-import javax.vecmath.Tuple3d;
 import javax.vecmath.Vector3d;
 
 import us.ihmc.graphics3DAdapter.Graphics3DAdapter;
@@ -13,10 +12,11 @@ import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
 import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
 import us.ihmc.graphics3DAdapter.structure.Graphics3DNode;
 import us.ihmc.graphics3DAdapter.structure.Graphics3DNodeType;
+import us.ihmc.utilities.math.geometry.Ellipsoid3d;
 
 public class Graphics3DStaticEllipsoidExample
 {
-   private final ArrayList<EllipsoidDefinition> ellipsoids = new ArrayList<EllipsoidDefinition>();
+   private final ArrayList<Ellipsoid3d> ellipsoids = new ArrayList<Ellipsoid3d>();
    
    public void createWorld(Graphics3DAdapter graphics3DAdapter, Random random, int numberOfEllipsoids)
    {
@@ -33,7 +33,7 @@ public class Graphics3DStaticEllipsoidExample
         double yRadius = generateRandomDoubleBetween(random, minRadius, maxRadius);
         double zRadius = generateRandomDoubleBetween(random, minRadius, maxRadius);
          
-         EllipsoidDefinition definition = new EllipsoidDefinition(center, xRadius, yRadius, zRadius);
+         Ellipsoid3d definition = new Ellipsoid3d(center, xRadius, yRadius, zRadius);
          
          ellipsoids.add(definition);
          
@@ -51,7 +51,7 @@ public class Graphics3DStaticEllipsoidExample
    
    public boolean isPointNearSurfaceOfAnEllipsoid(Point3d point, double epsilon)
    {
-      for (EllipsoidDefinition ellipsoid : ellipsoids)
+      for (Ellipsoid3d ellipsoid : ellipsoids)
       {
          if (ellipsoid.isPointNearSurfaceOfEllipsoid(point, epsilon)) return true;
       }
@@ -61,7 +61,7 @@ public class Graphics3DStaticEllipsoidExample
    
    public boolean isPointInsideAnEllipsoid(Point3d point, double epsilon)
    {
-      for (EllipsoidDefinition ellipsoid : ellipsoids)
+      for (Ellipsoid3d ellipsoid : ellipsoids)
       {
          if (ellipsoid.isPointInsideEllipsoid(point, epsilon)) return true;
       }
@@ -81,90 +81,6 @@ public class Graphics3DStaticEllipsoidExample
       double z = generateRandomDoubleBetween(random, minValues.getZ(), maxValues.getZ());
       
       return new Vector3d(x, y, z);
-   }
-   
-   private static class EllipsoidDefinition
-   {
-      private final Point3d center;
-      private final double xRadius, yRadius, zRadius;
-      
-      public EllipsoidDefinition(Tuple3d center, double xRadius, double yRadius, double zRadius)
-      {
-         this.center = new Point3d(center);
-         
-         this.xRadius = xRadius;
-         this.yRadius = yRadius;
-         this.zRadius = zRadius;
-      }
-      
-      public boolean isPointInsideEllipsoid(Point3d point, double epsilon)
-      {
-         double sumSquared = getSumSquared(point);
-         return (sumSquared < 1.0 - epsilon);
-      }
-
-      public boolean isPointNearSurfaceOfEllipsoid(Point3d point, double epsilon)
-      {
-         double sumSquared = getSumSquared(point);
-         return (Math.abs(sumSquared - 1.0) < epsilon);
-      }
-      
-      private double getSumSquared(Point3d point)
-      {
-         double ellipsoidX = point.getX() - center.getX();
-         double ellipsoidY = point.getY() - center.getY();
-         double ellipsoidZ = point.getZ() - center.getZ();
-         
-         double scaledX = ellipsoidX/xRadius;
-         double scaledY = ellipsoidY/yRadius;
-         double scaledZ = ellipsoidZ/zRadius;
-         
-         double sumSquared = scaledX*scaledX + scaledY*scaledY + scaledZ*scaledZ;
-         
-         return sumSquared;
-      }
-      
-      private void projectOntoEllipsoid(Point3d point)
-      {
-         double ellipsoidX = point.getX() - center.getX();
-         double ellipsoidY = point.getY() - center.getY();
-         double ellipsoidZ = point.getZ() - center.getZ();
-         
-         double scaledX = ellipsoidX/xRadius;
-         double scaledY = ellipsoidY/yRadius;
-         double scaledZ = ellipsoidZ/zRadius;
-         
-         Vector3d vector3d = new Vector3d(scaledX, scaledY, scaledZ);
-         vector3d.normalize();
-         
-         scaledX = vector3d.getX() * xRadius;
-         scaledY = vector3d.getY() * yRadius;
-         scaledZ = vector3d.getZ() * zRadius;
-         
-         point.setX(center.getX() + scaledX);
-         point.setY(center.getY() + scaledY);
-         point.setZ(center.getZ() + scaledZ);    
-      }
-      
-      public Point3d getCenterCopy()
-      {
-         return new Point3d(center);
-      }
-      
-      public double getXRadius()
-      {
-         return xRadius;
-      }
-      
-      public double getYRadius()
-      {
-         return yRadius;
-      }
-      
-      public double getZRadius()
-      {
-         return zRadius;
-      }
    }
    
 }
