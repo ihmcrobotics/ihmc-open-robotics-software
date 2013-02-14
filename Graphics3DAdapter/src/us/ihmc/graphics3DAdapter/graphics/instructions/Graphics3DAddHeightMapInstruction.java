6f46b748b018d0f5055db9bf2b48c5d9a95a8a26
Jerry Pratt <jpratt@ihmc.us>
2013-02-14 00:07:07 +0000

    Got Height Maps to work in JME. Can now see LittleDog Terrain!

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/instructions/Graphics3DAddHeightMapInstruction.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/instructions/Graphics3DAddHeightMapInstruction.java
@@ -1,7 +1,5 @@
 package us.ihmc.graphics3DAdapter.graphics.instructions;
 
-import javax.media.j3d.Transform3D;
-
 import us.ihmc.graphics3DAdapter.HeightMap;
 import us.ihmc.graphics3DAdapter.graphics.appearances.AppearanceDefinition;
 
@@ -9,17 +7,14 @@ public class Graphics3DAddHeightMapInstruction extends Graphics3DInstruction
 {
    private final HeightMap heightMap;
    private final int xPointsPerSide, yPointsPerSide;
-   private final Transform3D transform;
    
-   public Graphics3DAddHeightMapInstruction(HeightMap heightMap, int xPointsPerSide, int yPointsPerSide, AppearanceDefinition appearance, Transform3D transform)
+   public Graphics3DAddHeightMapInstruction(HeightMap heightMap, int xPointsPerSide, int yPointsPerSide, AppearanceDefinition appearance)
    {
       this.heightMap = heightMap;
       this.xPointsPerSide = xPointsPerSide;
       this.yPointsPerSide = yPointsPerSide;
       
       setAppearance(appearance);
-      
-      this.transform = transform;
    }
 
    public HeightMap getHeightMap()
@@ -35,11 +30,6 @@ public class Graphics3DAddHeightMapInstruction extends Graphics3DInstruction
    public int getYPointsPerSide()
    {
       return yPointsPerSide;
-   }
-
-   public Transform3D getTransform()
-   {
-      return transform;
    }
    
 }
