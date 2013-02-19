519eeb52f26bd6d0d58d5888dac0ff53d47a035e
Jerry Pratt <jpratt@ihmc.us>
2013-02-19 16:12:39 +0000

    Extracted FinalDesiredICPCalculator interface. Lookahead one visualization is working now.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/ShiftInsideFinalDesiredICPCalculator.java
@@ -0,0 +1,75 @@
+package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl;
+
+
+import javax.media.j3d.Transform3D;
+
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.ContactablePlaneBody;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.Footstep;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.FootstepUtils;
+import us.ihmc.robotSide.RobotSide;
+import us.ihmc.utilities.math.geometry.FrameConvexPolygon2d;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.FramePoint2d;
+import us.ihmc.utilities.math.geometry.FramePose;
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+/**
+ * Very simple FinalDesiredICPCalculator that just puts the final desired ICP 4cm or so inside the transfer to foot.
+ *
+ */
+public class ShiftInsideFinalDesiredICPCalculator implements FinalDesiredICPCalculator
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+   private final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+
+   public ShiftInsideFinalDesiredICPCalculator(YoVariableRegistry parentRegistry)
+   {
+      parentRegistry.addChild(registry);
+   }
+
+   public FramePoint2d getDoubleSupportFinalDesiredICPForWalking(TransferToAndNextFootstepsData transferToAndNextFootstepsData)
+   {
+      Footstep transferToFootstep = transferToAndNextFootstepsData.getTransferToFootstep();
+
+      FramePose transferToFootstepAnklePose = transferToFootstep.getPoseCopy();
+      ContactablePlaneBody transferToFootContactablePlaneBody = transferToAndNextFootstepsData.getTransferToFootContactablePlaneBody();
+      FrameConvexPolygon2d transferToFootPolygonInSoleFrame = transferToAndNextFootstepsData.getTransferToFootPolygonInSoleFrame();
+      RobotSide transferToSide = transferToAndNextFootstepsData.getTransferToSide();
+
+      Transform3D footstepAnkleToWorldTransform = new Transform3D();
+      getTransformFromPoseToWorld(footstepAnkleToWorldTransform, transferToFootstepAnklePose);
+
+      Transform3D ankleToSoleTransform = FootstepUtils.getAnkleToSoleTransform(transferToFootContactablePlaneBody);
+      ankleToSoleTransform.invert();
+
+      FramePoint2d centroid2d = transferToFootPolygonInSoleFrame.getCentroidCopy();
+      FramePoint centroid = centroid2d.toFramePoint();
+      centroid.changeFrameUsingTransform(null, ankleToSoleTransform);
+      centroid.changeFrameUsingTransform(worldFrame, footstepAnkleToWorldTransform);
+
+      FramePoint pointOffsetFromCentroid = new FramePoint(centroid);
+
+      double extraX = 0.0;    // 0.02
+      double extraY = transferToSide.negateIfLeftSide(0.04);
+      FrameVector offset = new FrameVector(null, extraX, extraY, 0.0);
+      offset.changeFrameUsingTransform(worldFrame, footstepAnkleToWorldTransform);
+
+      pointOffsetFromCentroid.changeFrame(offset.getReferenceFrame());
+      pointOffsetFromCentroid.add(offset);
+
+      FramePoint2d ret = pointOffsetFromCentroid.toFramePoint2d();
+
+      return ret;
+   }
+
+
+   private static void getTransformFromPoseToWorld(Transform3D poseToWorldTransformToPack, FramePose framePose)
+   {
+      ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+      framePose.changeFrame(worldFrame);
+      framePose.getTransformFromPoseToFrame(poseToWorldTransformToPack);
+   }
+}
