bd2f37b5d5287e233a345cf225bca0ae8b5056d3
Twan Koolen <tkoolen@ihmc.us>
2013-02-23 18:02:19 +0000

    checkOrbitalCondition --> checkOrbitalEnergyCondition

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotWalkingControllerParameters.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotWalkingControllerParameters.java
@@ -57,7 +57,7 @@ public class DRCRobotWalkingControllerParameters implements WalkingControllerPar
       };
    }
 
-   public boolean checkOrbitalCondition()
+   public boolean checkOrbitalEnergyCondition()
    {
       return false;
    }
