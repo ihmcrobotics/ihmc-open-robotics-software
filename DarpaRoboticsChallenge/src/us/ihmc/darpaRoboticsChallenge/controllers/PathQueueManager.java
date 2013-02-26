8f27aa0d870c5a97262e34e17f675725df5e3942
John Carff <jcarff@ihmc.us>
2013-02-26 17:13:16 +0000

    delete foot steps implemented and append footsteps added to singlestep

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/controllers/PathQueueManager.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/controllers/PathQueueManager.java
@@ -1,12 +1,16 @@
 package us.ihmc.darpaRoboticsChallenge.controllers;
 
+import java.util.ArrayList;
+
+import us.ihmc.commonWalkingControlModules.desiredFootStep.Footstep;
 import us.ihmc.commonWalkingControlModules.desiredFootStep.FootstepStatus;
 
 public interface PathQueueManager
 {
-
    public abstract void updateFootstepStatus(FootstepStatus footstepStatus);
 
    public abstract void setPathReplacer(Path pathReplacer);
+
+   public abstract ArrayList<Footstep> getFootStepQueue();
 
 }
