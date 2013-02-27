c11455ae33394e7f495d41c04181a325f4b9ee78
Twan Koolen <tkoolen@ihmc.us>
2013-02-27 15:56:27 +0000

    Now doing my own way of discretizing dynamics. Still need to check if it's exactly right.

--- CommonWalkingControlModules/matlab/StateEstimation/kalmanFilterQuaternion.m
+++ CommonWalkingControlModules/matlab/StateEstimation/kalmanFilterQuaternion.m
@@ -21,7 +21,7 @@ ROmega = covariances.ROmega;
 QContinuous = blkdiag(QPhi, QOmega, QB);
 RContinuous = blkdiag(RPhi, ROmega);
 BW = eye(9);
-[F, G, Q, R] = discretize(A, B, BW, QContinuous, RContinuous, dt);
+[F, G, Q, R] = discretize2(A, B, BW, QContinuous, RContinuous, dt);
 H = [eye(3), zeros(3, 6);
      zeros(3, 3), eye(3), eye(3)];
 
@@ -139,4 +139,37 @@ phi22 = phi(Nx+1:2*Nx,Nx+1:2*Nx);
 Qd = phi22'*phi12;
 Qd = (Qd+Qd')/2;        % Make sure Qd is symmetric
 Rd = rn/Ts;
+end
+
+% note: G not used.
+function [Ad, Bd, Qd, Rd] = discretize2(A, B, G, Q, R, deltaT)
+n = size(A, 1);
+m = size(B, 2);
+C = [-A', Q, zeros(n, m);
+     zeros(n, n), A, B;
+     zeros(m, n), zeros(m, n), zeros(m, m)];
+
+% expC = expm(C * deltaT);
+nApprox = 3;
+expC = expmApprox(C * deltaT, nApprox);
+F3 = expC(n + 1 : 2 * n, n + 1 : 2 * n);
+G2 = expC(1 : n, n + 1 : 2 * n);
+G3 = expC(n + 1 : 2 * n, 2 * n + 1 : end);
+
+Ad = F3;
+Bd = F3 * G3;
+Qd = F3' * G2;
+
+Rd = R / deltaT;
+end
+
+function A = expmApprox(M, n)
+A = zeros(size(M));
+factorial = 1;
+MPower = eye(size(M, 1));
+for i = 1 : n
+    A = A + 1 / factorial * MPower;
+    factorial = factorial * i;
+    MPower = MPower * M;
+end
 end
