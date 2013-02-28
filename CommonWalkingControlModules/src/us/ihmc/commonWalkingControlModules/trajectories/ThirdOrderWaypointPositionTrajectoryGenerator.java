f9abbd261e44e0d811698bcd2dad72bba5d2c178
Eric Morphis <emorphis@ihmc.us>
2013-02-28 00:50:52 +0000

    Cleaned up ThirdOrderWaypointPositionTrajectoryGenerator a bit.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/trajectories/ThirdOrderWaypointPositionTrajectoryGenerator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/trajectories/ThirdOrderWaypointPositionTrajectoryGenerator.java
@@ -18,15 +18,15 @@ import com.yobotics.simulationconstructionset.util.math.frames.YoFramePoint;
 import com.yobotics.simulationconstructionset.util.math.frames.YoFrameVector;
 import com.yobotics.simulationconstructionset.util.trajectory.DoubleProvider;
 import com.yobotics.simulationconstructionset.util.trajectory.PositionProvider;
+import com.yobotics.simulationconstructionset.util.trajectory.PositionTrajectoryGenerator;
 import com.yobotics.simulationconstructionset.util.trajectory.VectorProvider;
 import com.yobotics.simulationconstructionset.util.trajectory.YoPolynomial;
 
-public class ThirdOrderWaypointPositionTrajectoryGenerator
+public class ThirdOrderWaypointPositionTrajectoryGenerator implements PositionTrajectoryGenerator
 {
    private final String namePostFix = getClass().getSimpleName();
    private final YoVariableRegistry registry;
    private final List<EnumMap<Direction, YoPolynomial>> spaceSplines = new ArrayList<EnumMap<Direction, YoPolynomial>>();
-   private final YoPolynomial timeSpline;
 
    private final DoubleProvider stepTimeProvider;
    private final PositionProvider[] positionSource = new PositionProvider[4];
@@ -35,28 +35,21 @@ public class ThirdOrderWaypointPositionTrajectoryGenerator
    private final DoubleYoVariable stepTime;
    private final DoubleYoVariable timeIntoStep;
 
-   private final double obstacleWidth;
-   private final double obstacleLength;
-   private final double obstacleHeight;
-
    private final YoFramePoint desiredPosition;
    private final YoFrameVector desiredVelocity;
    private final YoFrameVector desiredAcceleration;
-
-   private final ReferenceFrame obstacleReferenceFrame;
    private final ReferenceFrame referenceFrame;
-   private final FrameVector tempVector = new FrameVector(ReferenceFrame.getWorldFrame());
-   
+
    private final int desiredNumberOfSplines;
-   private final double tolerance;
+   private final double allowedPercentDifferenceBetweenDesiredAndActualArcLengths;
+   private final int arcLengthPrecisionRating;
 
    private ConcatenatedSplines concatenatedSplines;
 
    public ThirdOrderWaypointPositionTrajectoryGenerator(String namePrefix, ReferenceFrame referenceFrame, DoubleProvider stepTimeProvider,
            PositionProvider initialPositionProvider, VectorProvider initalVelocityProvider, PositionProvider finalPositionProvider,
-           VectorProvider finalDesiredVelocityProvider, YoVariableRegistry parentRegistry, double obstacleWidth, double obstacleLength, double obstacleHeight,
-           ReferenceFrame obstacleReferenceFrame, PositionProvider firstIntermediatePosition, PositionProvider secondIntermediatePosition,
-           int desiredNumberOfSplines, int tolerance)
+           VectorProvider finalDesiredVelocityProvider, YoVariableRegistry parentRegistry, PositionProvider firstIntermediatePosition,
+           PositionProvider secondIntermediatePosition, int desiredNumberOfSplines, double allowedPercentDifferenceBetweenDesiredAndActualArcLengths, int arcLengthPrecisionRating)
    {
       this.registry = new YoVariableRegistry(namePrefix + namePostFix);
 
@@ -70,18 +63,17 @@ public class ThirdOrderWaypointPositionTrajectoryGenerator
          }
       }
 
-      timeSpline = new YoPolynomial(namePrefix + "Time", 3, registry);
       this.stepTimeProvider = stepTimeProvider;
 
       this.positionSource[0] = initialPositionProvider;
       this.positionSource[1] = firstIntermediatePosition;
       this.positionSource[2] = secondIntermediatePosition;
       this.positionSource[3] = finalPositionProvider;
-
+      
       this.velocitySource[0] = initalVelocityProvider;
-
-      // TODO make intermed velocity
       this.velocitySource[3] = finalDesiredVelocityProvider;
+      this.velocitySource[1] = getFirstIntermediateVelocityProvider();
+      this.velocitySource[2] = getSecondIntermediateVelocityProvider();
 
       this.stepTime = new DoubleYoVariable("stepTime", registry);
       this.timeIntoStep = new DoubleYoVariable("timeIntoStep", registry);
@@ -93,19 +85,56 @@ public class ThirdOrderWaypointPositionTrajectoryGenerator
       this.referenceFrame = referenceFrame;
       parentRegistry.addChild(registry);
 
-      this.obstacleHeight = obstacleHeight;
-      this.obstacleLength = obstacleLength;
-      this.obstacleWidth = obstacleWidth;
-
-      this.obstacleReferenceFrame = obstacleReferenceFrame;
-      
       this.desiredNumberOfSplines = desiredNumberOfSplines;
-      this.tolerance = tolerance;
+      this.allowedPercentDifferenceBetweenDesiredAndActualArcLengths = allowedPercentDifferenceBetweenDesiredAndActualArcLengths;
+      this.arcLengthPrecisionRating = arcLengthPrecisionRating;
+   }
+
+   //TODO smarter method
+   private VectorProvider getFirstIntermediateVelocityProvider()
+   {
+      FramePoint initialPosition = new FramePoint(referenceFrame);
+      FramePoint firstWayPoint = new FramePoint(referenceFrame);
+      FrameVector initialVelocity = new FrameVector(referenceFrame);
+      positionSource[0].get(initialPosition);
+      positionSource[1].get(firstWayPoint);
+      velocitySource[0].get(initialVelocity);
+      
+      FrameVector firstWayPointVelocity = new FrameVector(firstWayPoint);
+      firstWayPointVelocity.sub(initialPosition);
+      firstWayPointVelocity.normalize();
+      firstWayPointVelocity.scale(initialVelocity.length());
+      
+      return new ConstantVectorProvider(firstWayPointVelocity);
    }
    
-   public void compute(double t)
+   //TODO smarter method
+   private VectorProvider getSecondIntermediateVelocityProvider()
    {
-      concatenatedSplines.compute(t);
+      FramePoint secondWayPoint = new FramePoint(referenceFrame);
+      FramePoint finalPosition = new FramePoint(referenceFrame);
+      FrameVector finalVelocity = new FrameVector(referenceFrame);
+      positionSource[2].get(secondWayPoint);
+      positionSource[3].get(finalPosition);
+      velocitySource[3].get(finalVelocity);
+      
+      FrameVector secondWayPointVelocity = new FrameVector(finalPosition);
+      secondWayPointVelocity.sub(secondWayPoint);
+      secondWayPointVelocity.normalize();
+      secondWayPointVelocity.scale(finalVelocity.length());
+      
+      return new ConstantVectorProvider(secondWayPointVelocity);
+   }
+
+   public void compute(double time)
+   {
+      timeIntoStep.set(time);
+
+      double totalTime = this.stepTime.getDoubleValue();
+      if (time > totalTime)
+         time = totalTime;
+
+      concatenatedSplines.compute(time);
       desiredPosition.set(concatenatedSplines.getPosition());
       desiredVelocity.set(concatenatedSplines.getVelocity());
       desiredAcceleration.set(concatenatedSplines.getAcceleration());
@@ -125,7 +154,7 @@ public class ThirdOrderWaypointPositionTrajectoryGenerator
    {
       desiredAcceleration.getFrameVectorAndChangeFrameOfPackedVector(accelerationToPack);
    }
-   
+
    public void initialize()
    {
       double stepTime = stepTimeProvider.getValue();
@@ -181,13 +210,13 @@ public class ThirdOrderWaypointPositionTrajectoryGenerator
          splineMap.put(new Pair<Double, Double>(stepTime / 3.0, (2.0 * stepTime) / 3.0), spaceSplines.get(1));
          splineMap.put(new Pair<Double, Double>((2.0 * stepTime) / 3.0, stepTime), spaceSplines.get(2));
 
-         concatenatedSplines = new ConcatenatedSplines(splineMap, referenceFrame);
-         concatenatedSplines = new ConcatenatedSplines(concatenatedSplines, desiredNumberOfSplines, tolerance);
+         concatenatedSplines = new ConcatenatedSplines(splineMap, referenceFrame, arcLengthPrecisionRating);
+         concatenatedSplines = new ConcatenatedSplines(concatenatedSplines, desiredNumberOfSplines, allowedPercentDifferenceBetweenDesiredAndActualArcLengths, arcLengthPrecisionRating);
       }
+   }
 
-      // TODO get path lengths for each path, chop up total footstep time
-//    timeSpline.setCubicUsingIntermediatePoints(0, totalTime / pathLength1, totalTime / pathLength2, totalTime, z0, zIntermediate1, zIntermediate2, zFinal);
-//    timeSpline.setCubic(0.0, stepTime, 0.0, initialParameterd, 1.0, finalParameterd);
-
+   public boolean isDone()
+   {
+      return timeIntoStep.getDoubleValue() >= stepTime.getDoubleValue();
    }
 }
