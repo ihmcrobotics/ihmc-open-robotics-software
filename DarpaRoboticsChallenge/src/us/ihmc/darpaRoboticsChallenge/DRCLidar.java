4d30d07da0627e3abf93bfa9cecd5fbf6775afd1
Gray Thomas <gthomas@ihmc.us>
2013-03-04 16:43:25 +0000

    got rid of vanilla lidar. Polar lidar retained.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCLidar.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCLidar.java
@@ -5,19 +5,15 @@ import java.util.ArrayList;
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.graphics3DAdapter.Graphics3DAdapter;
 import us.ihmc.utilities.net.ObjectCommunicator;
-import us.ihmc.utilities.polarLidarGeometry.LIDARScanDefinition;
 import us.ihmc.utilities.polarLidarGeometry.PolarLidarScanDefinition;
 import us.ihmc.utilities.screwTheory.OneDoFJoint;
 
 import com.yobotics.simulationconstructionset.simulatedSensors.FastPolarRayCastLIDAR;
-import com.yobotics.simulationconstructionset.simulatedSensors.RayTraceLIDARSensor;
 import com.yobotics.simulationconstructionset.simulatedSensors.SimulatedLIDARSensorUpdateParameters;
 
 public class DRCLidar
 {
    static {
-      if(DRCConfigParameters.STREAM_VANILLA_LIDAR)
-         System.out.println("DRCLidar: Lidar is ON");
       if(DRCConfigParameters.STREAM_POLAR_LIDAR)
          System.out.println("DRCLidar: PolarLidar is ON. This lidar passes less data, and also includes the transforms from when the data was produced.");
    }
@@ -27,30 +23,6 @@ public class DRCLidar
       Graphics3DAdapter graphics3dAdapter = sdfRobotSimulation.getSimulationConstructionSet().getGraphics3dAdapter();
       SimulatedLIDARSensorUpdateParameters updateParameters = new SimulatedLIDARSensorUpdateParameters();
       updateParameters.setTransponderSettings(objectCommunicator);
-      if (DRCConfigParameters.STREAM_VANILLA_LIDAR)
-      {
-         System.out.println("Streaming Lidar");
-         ArrayList<RayTraceLIDARSensor> lidarSensors = sdfRobotSimulation.getRobot().getSensors(RayTraceLIDARSensor.class);
-         if (lidarSensors.size() == 0)
-            System.err.println("DRCLidar: No LIDAR units found on SDF Robot.");
-         else
-         {
-            if (lidarSensors.size() >=2)
-               System.err.println("DRCLidar: Only one LIDAR unit is supported at this time. Found "+lidarSensors.size() +" LIDAR units. Will use only the first.");
-            RayTraceLIDARSensor rayTraceLIDARSensor = lidarSensors.get(0); // the only one.
-            rayTraceLIDARSensor.setWorld(graphics3dAdapter);
-            if (DRCConfigParameters.OVERRIDE_DRC_LIDAR_CONFIG)
-            {
-               LIDARScanDefinition largeScan = LIDARScanDefinition.defineSimplifiedNoddingLIDARScan(DRCConfigParameters.LIDAR_VERTICAL_SCAN_ANGLE, DRCConfigParameters.LIDAR_SWEEPS_PER_SCAN, DRCConfigParameters.LIDAR_HORIZONTAL_SCAN_ANGLE,
-                     DRCConfigParameters.LIDAR_POINTS_PER_SWEEP, DRCConfigParameters.MIN_LIDAR_DISTANCE);
-               rayTraceLIDARSensor.setScan(largeScan);
-               updateParameters.setUpdateRate(DRCConfigParameters.LIDAR_UPDATE_RATE_OVERRIDE);
-            }
-            
-            rayTraceLIDARSensor.setLidarDaemonParameters(updateParameters);            
-            rayTraceLIDARSensor.startLidarDaemonThread();
-         }
-      }
       if (DRCConfigParameters.STREAM_POLAR_LIDAR)
       {
          System.out.println("Streaming Lidar");
@@ -70,12 +42,10 @@ public class DRCLidar
                      DRCConfigParameters.LIDAR_SWEEPS_PER_SCAN,
                      DRCConfigParameters.LIDAR_SWEEP_MAX_YAW,
                      DRCConfigParameters.LIDAR_SWEEP_MIN_YAW,
-                     DRCConfigParameters.LIDAR_SCAN_MAX_PITCH,
-                     DRCConfigParameters.LDIAR_SCAN_MIN_PITCH,
+                     DRCConfigParameters.LIDAR_SCAN_MAX_ROLL,
+                     DRCConfigParameters.LDIAR_SCAN_MIN_ROLL,
                      DRCConfigParameters.LIDAR_MIN_DISTANCE
                      );
-               LIDARScanDefinition.defineSimplifiedNoddingLIDARScan(DRCConfigParameters.LIDAR_VERTICAL_SCAN_ANGLE, DRCConfigParameters.LIDAR_SWEEPS_PER_SCAN, DRCConfigParameters.LIDAR_HORIZONTAL_SCAN_ANGLE,
-                     DRCConfigParameters.LIDAR_POINTS_PER_SWEEP, DRCConfigParameters.MIN_LIDAR_DISTANCE);
                polarLidar.setScan(largeScan);
                updateParameters.setUpdateRate(DRCConfigParameters.LIDAR_UPDATE_RATE_OVERRIDE);
             }
