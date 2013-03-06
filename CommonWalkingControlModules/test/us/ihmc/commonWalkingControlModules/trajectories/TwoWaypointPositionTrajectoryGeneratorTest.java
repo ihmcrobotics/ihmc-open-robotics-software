d1e956e2acbadf3764f52b2d50e843cd240cdcc7
Stephen McCrory <smccrory@ihmc.us>
2013-03-06 16:38:15 +0000

    Tweaked first test

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/TwoWaypointPositionTrajectoryGeneratorTest.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/TwoWaypointPositionTrajectoryGeneratorTest.java
@@ -1,5 +1,7 @@
 package us.ihmc.commonWalkingControlModules.trajectories;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import us.ihmc.utilities.math.geometry.FramePoint;
@@ -18,23 +20,36 @@ public class TwoWaypointPositionTrajectoryGeneratorTest {
 	private static ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
 
 	@Test
-	public void testSimpleLinearTrajectory()
+	public void testSimpleTrajectory()
 	{
 		DoubleProvider stepTimeProvider = new ConstantDoubleProvider(1.0);
 		PositionProvider initialPositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{0.0, 0.0, 0.0}));
-		VectorProvider initialVelocityProvider = new ConstantVectorProvider(new FrameVector(worldFrame, new double[]{0.0, 0.0, 0.0}));
+		VectorProvider initialVelocityProvider = new ConstantVectorProvider(new FrameVector(worldFrame, new double[]{1.0, 1.0, 1.0}));
 		
-		PositionProvider firstIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{0.0, 0.0, 0.0}));
-		PositionProvider secondIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{0.0, 0.0, 0.0}));
+		PositionProvider firstIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{-1.0, 1.0, 1.0}));
+		PositionProvider secondIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{2.0, 2.0, 2.0}));
+		
+		PositionProvider finalPositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{3.0, 3.0, 3.0}));
+		VectorProvider finalVelocityProvider = new ConstantVectorProvider(new FrameVector(worldFrame, new double[]{0.0, 0.0, 0.0}));
 		
 		int numDesiredSplines = 3;
 		int arcLengthCalculatorDivisions = 100;
-		
 		TwoWaypointPositionTrajectoryGenerator trajectory = new TwoWaypointPositionTrajectoryGenerator("", worldFrame, stepTimeProvider, 
-				initialPositionProvider, initialVelocityProvider, secondIntermediatePositionProvider, initialVelocityProvider, new YoVariableRegistry(""), firstIntermediatePositionProvider, 
+				initialPositionProvider, initialVelocityProvider, finalPositionProvider, finalVelocityProvider, new YoVariableRegistry(""), firstIntermediatePositionProvider, 
 				secondIntermediatePositionProvider, numDesiredSplines, arcLengthCalculatorDivisions);
 		
-		
+	//	trajectory.initialize();
+		trajectory.compute(0.0);
+		FramePoint actual = new FramePoint(worldFrame);
+		trajectory.get(actual);
+		System.out.println(actual.getX());
+		System.out.println(actual.getY());
+		System.out.println(actual.getZ());
+//		assertEquals(actual.getX(), 0.0, 1e-7);
+//		assertEquals(actual.getY(), 0.0, 1e-7);
+//		assertEquals(actual.getZ(), 0.0, 1e-7);
 	}
 	
+//	@Test
+//	public void 
 }
