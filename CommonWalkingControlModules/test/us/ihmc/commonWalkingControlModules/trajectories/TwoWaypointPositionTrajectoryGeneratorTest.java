22af4c2b319f207610d91bcf8caf78a3c43bea9c
Stephen McCrory <smccrory@ihmc.us>
2013-03-07 15:30:44 +0000

    Made a simple test case dependent on number of splines, and test for 3 and 4 splines

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/TwoWaypointPositionTrajectoryGeneratorTest.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/TwoWaypointPositionTrajectoryGeneratorTest.java
@@ -1,5 +1,7 @@
 package us.ihmc.commonWalkingControlModules.trajectories;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import us.ihmc.utilities.math.geometry.FramePoint;
@@ -18,19 +20,24 @@ public class TwoWaypointPositionTrajectoryGeneratorTest {
 	private static ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
 
 	@Test
-	public void testSimpleTrajectory()
+	public void testSimpleTrajectories()
 	{
-		DoubleProvider stepTimeProvider = new ConstantDoubleProvider(1.0);
-		PositionProvider initialPositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{0.0, 0.0, 0.0}));
-		VectorProvider initialVelocityProvider = new ConstantVectorProvider(new FrameVector(worldFrame, new double[]{1.0, 1.0, 1.0}));
+		testSimpleTrajectory(3);
+		testSimpleTrajectory(4);
+	}
+	
+	private void testSimpleTrajectory(int numDesiredSplines)
+	{
+		DoubleProvider stepTimeProvider = new ConstantDoubleProvider(0.8);
+		PositionProvider initialPositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{-0.1, 2.3, 0.0}));
+		VectorProvider initialVelocityProvider = new ConstantVectorProvider(new FrameVector(worldFrame, new double[]{0.2, 0.0, -0.05}));
 		
-		PositionProvider firstIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{1.0, 1.0, 1.0}));
-		PositionProvider secondIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{2.0, 2.0, 2.0}));
+		PositionProvider firstIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{0.12, 2.4, 0.2}));
+		PositionProvider secondIntermediatePositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{0.16, 2.3, 0.15}));
 		
-		PositionProvider finalPositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{3.0, 3.0, 3.0}));
-		VectorProvider finalVelocityProvider = new ConstantVectorProvider(new FrameVector(worldFrame, new double[]{0.0, 0.0, 0.0}));
+		PositionProvider finalPositionProvider = new ConstantPositionProvider(new FramePoint(worldFrame, new double[]{0.2, 2.35, 0.03}));
+		VectorProvider finalVelocityProvider = new ConstantVectorProvider(new FrameVector(worldFrame, new double[]{0.1, 0.01, -0.02}));
 		
-		int numDesiredSplines = 3;
 		int arcLengthCalculatorDivisions = 100;
 		TwoWaypointPositionTrajectoryGenerator trajectory = new TwoWaypointPositionTrajectoryGenerator("", worldFrame, stepTimeProvider, 
 				initialPositionProvider, initialVelocityProvider, finalPositionProvider, finalVelocityProvider, new YoVariableRegistry(""), firstIntermediatePositionProvider, 
@@ -39,15 +46,41 @@ public class TwoWaypointPositionTrajectoryGeneratorTest {
 		trajectory.initialize();
 		trajectory.compute(0.0);
 		FramePoint actual = new FramePoint(worldFrame);
+		FramePoint expected = new FramePoint(worldFrame);
+		initialPositionProvider.get(expected);
 		trajectory.get(actual);
-		System.out.println(actual.getX());
-		System.out.println(actual.getY());
-		System.out.println(actual.getZ());
-//		assertEquals(actual.getX(), 0.0, 1e-7);
-//		assertEquals(actual.getY(), 0.0, 1e-7);
-//		assertEquals(actual.getZ(), 0.0, 1e-7);
+		assertEquals(actual.getX(), expected.getX(), 1e-7);
+		assertEquals(actual.getY(), expected.getY(), 1e-7);
+		assertEquals(actual.getZ(), expected.getZ(), 1e-7);
+		assertFalse(trajectory.isDone());
+		
+		FrameVector actualVel = new FrameVector(worldFrame);
+		FrameVector expectedVel = new FrameVector(worldFrame);
+		trajectory.packVelocity(actualVel);
+		initialVelocityProvider.get(expectedVel);
+		assertEquals(actualVel.getX(), expectedVel.getX(), 1e-7);
+		assertEquals(actualVel.getY(), expectedVel.getY(), 1e-7);
+		assertEquals(actualVel.getZ(), expectedVel.getZ(), 1e-7);
+		
+		trajectory.compute(0.8);
+		finalPositionProvider.get(expected);
+		trajectory.get(actual);
+		assertEquals(actual.getX(), expected.getX(), 1e-7);
+		assertEquals(actual.getY(), expected.getY(), 1e-7);
+		assertEquals(actual.getZ(), expected.getZ(), 1e-7);
+		
+		trajectory.packVelocity(actualVel);
+		finalVelocityProvider.get(expectedVel);
+		assertEquals(actualVel.getX(), expectedVel.getX(), 1e-7);
+		assertEquals(actualVel.getY(), expectedVel.getY(), 1e-7);
+		assertEquals(actualVel.getZ(), expectedVel.getZ(), 1e-7);
+		assertTrue(trajectory.isDone());
 	}
 	
-//	@Test
-//	public void 
+	// make a test where you can know the arc lengths of the various portions in order to test intermediate points
+	
+	// take a look at other trajectory generator tests
+	
+	// incorporate tests of the two public voids
+	
 }
