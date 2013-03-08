87cc67d1f906a6129f4736401f4052c82e478f5e
Matt Johnson <mjohnson@ihmc.us>
2013-03-08 04:05:18 +0000

    Added Steering Input Connected steering to lane position

--- ImageProcessing/src/us/ihmc/imageProcessing/driving/VanishingPointDetector.java
+++ ImageProcessing/src/us/ihmc/imageProcessing/driving/VanishingPointDetector.java
@@ -48,12 +48,12 @@ public class VanishingPointDetector implements PostProcessor
       Stroke originalStroke = g2d.getStroke();
 
       // current aim point
-      g2d.setStroke(new BasicStroke(2));
-      graphics.setColor(Color.red);
-      int width = 20 + (int) screenDimension.getWidth() / 2;
-      int height = (int) screenDimension.getHeight() / 2;
-      graphics.drawLine(width - (markerDiameter / 2), height, width + (markerDiameter / 2), height);
-      graphics.drawLine(width, height - (markerDiameter / 2), width, height + (markerDiameter / 2));
+//      g2d.setStroke(new BasicStroke(2));
+//      graphics.setColor(Color.red);
+//      int width = 20 + (int) screenDimension.getWidth() / 2;
+//      int height = (int) screenDimension.getHeight() / 2;
+//      graphics.drawLine(width - (markerDiameter / 2), height, width + (markerDiameter / 2), height);
+//      graphics.drawLine(width, height - (markerDiameter / 2), width, height + (markerDiameter / 2));
 
       // vanishing point (desired aim point)
       graphics.setColor(color);
@@ -64,18 +64,18 @@ public class VanishingPointDetector implements PostProcessor
          graphics.drawLine((int) point.getX(), (int) point.getY() - markerDiameter / 2, (int) point.getX(), (int) point.getY() + markerDiameter / 2);
       }
 
-      // steering wheel
-      g2d.setStroke(new BasicStroke(4));
-      graphics.setColor(Color.red);
-      graphics.drawOval(195, (int) (screenDimension.getHeight() / 2) + 130, 220, 220);
-
-      // steering input
-      if (point != null)
-      {
-         graphics.setColor(Color.cyan);
-         double angle = -Math.atan2((point.getX() - width), height);
-         graphics.drawArc(195, (int) (screenDimension.getHeight() / 2) + 130, 220, 220, 90,  (int) Math.toDegrees(angle));
-      }
+//      // steering wheel
+//      g2d.setStroke(new BasicStroke(4));
+//      graphics.setColor(Color.red);
+//      graphics.drawOval(195, (int) (screenDimension.getHeight() / 2) + 130, 220, 220);
+//
+//      // steering input
+//      if (point != null)
+//      {
+//         graphics.setColor(Color.cyan);
+//         double angle = -Math.atan2((point.getX() - width), height);
+//         graphics.drawArc(195, (int) (screenDimension.getHeight() / 2) + 130, 220, 220, 90,  (int) Math.toDegrees(angle));
+//      }
 
       graphics.setColor(originalGraphicsColor);
       g2d.setStroke(originalStroke);
