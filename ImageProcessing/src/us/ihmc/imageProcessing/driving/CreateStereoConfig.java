b493a8d186cbe11f5d8a45847979d168d930f26d
Peter Abeles <peter.abeles@gmail.com>
2013-03-12 17:06:05 +0000

    - Fixed fy calculation

--- ImageProcessing/src/us/ihmc/imageProcessing/driving/CreateStereoConfig.java
+++ ImageProcessing/src/us/ihmc/imageProcessing/driving/CreateStereoConfig.java
@@ -14,7 +14,6 @@ public class CreateStereoConfig {
 	public static final int width = 1024;
 	public static final int height = 544;
 	public static final double hfov = 1.396263; // radians
-	public static final double vfov = (hfov/width)*height;
 	public static final double baseline = 0.07; // meters
 
 	private static IntrinsicParameters createIntrinsic() {
@@ -24,7 +23,7 @@ public class CreateStereoConfig {
 		param.cx = width/2;
 		param.cy = height/2;
 		param.fx =  (float)((width/2)/ Math.tan(hfov / 2.0));
-		param.fy =  (float)((height/2)/ Math.tan(vfov / 2.0));
+		param.fy =  param.fx;
 		param.skew = 0;
 		param.radial = new double[0];
 		param.flipY = false;
