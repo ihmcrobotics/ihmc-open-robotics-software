1b37baa2a3efbc34fe84e34540164ff88413d483
Peter Abeles <peter.abeles@gmail.com>
2013-03-12 16:51:03 +0000

    - Added code for creating stereo parameters - Modified ExampleStereoVisualOdometry to use those parameters - Added stereo_simulation.xml

---
+++ ImageProcessing/src/us/ihmc/imageProcessing/driving/CreateStereoConfig.java
@@ -0,0 +1,48 @@
+package us.ihmc.imageProcessing.driving;
+
+import boofcv.misc.BoofMiscOps;
+import boofcv.struct.calib.IntrinsicParameters;
+import boofcv.struct.calib.StereoParameters;
+import georegression.struct.se.Se3_F64;
+
+/**
+ * Creates a StereoParameters config file for DARPA robotics challenge simulation.
+ *
+ * @author Peter Abeles
+ */
+public class CreateStereoConfig {
+	public static final int width = 1024;
+	public static final int height = 544;
+	public static final double hfov = 1.396263; // radians
+	public static final double vfov = (hfov/width)*height;
+	public static final double baseline = 0.07; // meters
+
+	private static IntrinsicParameters createIntrinsic() {
+		IntrinsicParameters param = new IntrinsicParameters();
+		param.width = width;
+		param.height = height;
+		param.cx = width/2;
+		param.cy = height/2;
+		param.fx =  (float)((width/2)/ Math.tan(hfov / 2.0));
+		param.fy =  (float)((height/2)/ Math.tan(vfov / 2.0));
+		param.skew = 0;
+		param.radial = new double[0];
+		param.flipY = false;
+		return param;
+	}
+
+	public static void main( String args[] ) {
+
+		Se3_F64 rightToLeft = new Se3_F64();
+		rightToLeft.getT().x = baseline;
+
+		StereoParameters stereo = new StereoParameters();
+
+		stereo.left = createIntrinsic();
+		stereo.right = createIntrinsic();
+
+		stereo.setRightToLeft(rightToLeft);
+
+		BoofMiscOps.saveXML(stereo,"stereo_simulation.xml");
+	}
+}
