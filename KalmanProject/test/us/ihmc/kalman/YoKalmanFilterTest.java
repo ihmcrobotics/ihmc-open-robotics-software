20785c1cecbd29b68742a185713ce43af1f2b22b
Jerry Pratt <jpratt@ihmc.us>
2013-03-19 19:47:39 +0000

    Test YoKalmanFilter with a simulated system and check that the P matrices converge based on the Riccati Equation.

--- KalmanProject/test/us/ihmc/kalman/YoKalmanFilterTest.java
+++ KalmanProject/test/us/ihmc/kalman/YoKalmanFilterTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import java.util.Random;
 
 import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
 import org.ejml.ops.EjmlUnitTests;
 import org.ejml.ops.RandomMatrices;
 import org.junit.Before;
@@ -16,6 +17,8 @@ import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
 public class YoKalmanFilterTest
 {
+   private static final boolean DEBUG = false;
+   
    private Random random;
    private YoVariableRegistry parentRegistry;
 
@@ -32,7 +35,6 @@ public class YoKalmanFilterTest
    private DenseMatrix64F P;
    private DenseMatrix64F u;
    private DenseMatrix64F y;
-
 
    @Before
    public void setUp()
@@ -204,19 +206,19 @@ public class YoKalmanFilterTest
       kalmanFilter.setProcessNoiseCovariance(Q);
       kalmanFilter.setMeasurementNoiseCovariance(R);
       kalmanFilter.setState(x, P);
-      
+
       kalmanFilter.predict(u);
       DenseMatrix64F stateBeforeUpdate = new DenseMatrix64F(kalmanFilter.getState());
       DenseMatrix64F covarianceBeforeUpdate = new DenseMatrix64F(kalmanFilter.getCovariance());
-      
+
       kalmanFilter.update(y);
       DenseMatrix64F stateAfterUpdate = new DenseMatrix64F(kalmanFilter.getState());
       DenseMatrix64F covarianceAfterUpdate = new DenseMatrix64F(kalmanFilter.getCovariance());
-      
+
       EjmlUnitTests.assertEquals(stateBeforeUpdate, stateAfterUpdate, 1e-8);
       EjmlUnitTests.assertEquals(covarianceBeforeUpdate, covarianceAfterUpdate, 1e-8);
    }
-   
+
    @Test
    public void testTwoUpdatesVersusOne()
    {
@@ -238,7 +240,7 @@ public class YoKalmanFilterTest
          kalmanFilter.setState(x, P);
          kalmanFilter.predict(u);
       }
-      
+
       yoKalmanFilter0.update(y);
 
       for (int i = 0; i < nMeasurements; i++)
@@ -251,12 +253,233 @@ public class YoKalmanFilterTest
                RPrime.set(j, j, Double.POSITIVE_INFINITY);
             }
          }
+
          yoKalmanFilter1.setMeasurementNoiseCovariance(RPrime);
          yoKalmanFilter1.update(y);
       }
 
       EjmlUnitTests.assertEquals(kalmanFilters[0].getState(), kalmanFilters[1].getState(), 1e-8);
       EjmlUnitTests.assertEquals(kalmanFilters[0].getCovariance(), kalmanFilters[1].getCovariance(), 1e-8);
+   }
+
+
+   @Test
+   public void testViaSimulatingATrueSystemAndCheckingErrorDynamics()
+   {
+      nStates = 8;
+      nInputs = 3;
+      nMeasurements = 4;
+      createRandomParameters(nStates, nInputs, nMeasurements);
+
+      YoKalmanFilter kalmanFilter = new YoKalmanFilter("yo", nStates, nInputs, nMeasurements, parentRegistry);
+
+      kalmanFilter.configure(F, G, H);
+      kalmanFilter.setProcessNoiseCovariance(Q);
+      kalmanFilter.setMeasurementNoiseCovariance(R);
+      kalmanFilter.setState(x, P);
+
+      DenseMatrix64F trueState = new DenseMatrix64F(x);
+      DenseMatrix64F estimatedState = kalmanFilter.getState();
+
+      DenseMatrix64F stateError = new DenseMatrix64F(nStates, 1);
+      CommonOps.sub(trueState, estimatedState, stateError);
+
+      EjmlUnitTests.assertEquals(stateError, new DenseMatrix64F(nStates, 1), 1e-6);
+
+      int numberOfTicksToTest = 10;
+
+      for (int i = 0; i < numberOfTicksToTest; i++)
+      {
+         // Create random inputs, process noise, and sensor noise:
+         u = RandomMatrices.createRandom(nInputs, 1, random);    // new DenseMatrix64F(nInputs, 1); //
+         DenseMatrix64F wProcessNoise = RandomMatrices.createRandom(nStates, 1, random);    // new DenseMatrix64F(nStates, 1); //
+         DenseMatrix64F vSensorNoise = RandomMatrices.createRandom(nMeasurements, 1, random);    // new DenseMatrix64F(nMeasurements, 1); //
+
+         // Compute the true state dynamics:
+         // trueState = F * trueState + G * u + w
+         DenseMatrix64F nextTrueState = new DenseMatrix64F(nStates, 1);
+         CommonOps.multAdd(F, trueState, nextTrueState);
+         CommonOps.multAdd(G, u, nextTrueState);
+         CommonOps.add(nextTrueState, wProcessNoise, nextTrueState);
+
+         printIfDebug("nextTrueState = " + nextTrueState);
+
+         // Compute the true measurments:
+         // y = H * trueState + v
+         CommonOps.mult(H, nextTrueState, y);
+         CommonOps.add(y, vSensorNoise, y);
+
+         // Update the Kalman filter with the input and the measurement and get the estimated state:
+
+         Q = RandomMatrices.createSymmPosDef(nStates, random);
+         R = RandomMatrices.createSymmPosDef(nMeasurements, random);
+         kalmanFilter.setMeasurementNoiseCovariance(R);
+         kalmanFilter.setProcessNoiseCovariance(Q);
+
+         kalmanFilter.predict(u);
+         kalmanFilter.update(y);
+
+         estimatedState = kalmanFilter.getState();
+         printIfDebug("next estimated state = " + estimatedState);
+
+         // Compute the next true state error:
+         DenseMatrix64F nextTrueStateError = new DenseMatrix64F(nStates, 1);
+         CommonOps.sub(nextTrueState, estimatedState, nextTrueStateError);
+
+         printIfDebug("nextTrueStateError = " + nextTrueStateError);
+
+         // Check the error dynamics:
+         DenseMatrix64F kalmanKGain = kalmanFilter.getKGain();
+         printIfDebug("kalmanKGain = " + kalmanKGain);
+
+         DenseMatrix64F KH = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(kalmanKGain, H, KH);
+         DenseMatrix64F Identity = new DenseMatrix64F(nStates, nStates);
+         CommonOps.setIdentity(Identity);
+         DenseMatrix64F IMinusKH = new DenseMatrix64F(Identity);
+         CommonOps.sub(IMinusKH, KH, IMinusKH);
+
+         printIfDebug("IMinusKH = " + IMinusKH);
+
+         DenseMatrix64F IMinusKHTimesF = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(IMinusKH, F, IMinusKHTimesF);
+
+         DenseMatrix64F computedNextError = new DenseMatrix64F(nStates, 1);
+         CommonOps.multAdd(IMinusKHTimesF, stateError, computedNextError);
+         CommonOps.multAdd(IMinusKH, wProcessNoise, computedNextError);
+
+         DenseMatrix64F negativeVSensorNoise = new DenseMatrix64F(vSensorNoise);
+         CommonOps.scale(-1.0, negativeVSensorNoise);
+         CommonOps.multAdd(kalmanKGain, negativeVSensorNoise, computedNextError);
+
+         printIfDebug("computedNextError = " + computedNextError);
+
+         EjmlUnitTests.assertEquals(nextTrueStateError, computedNextError, 1e-6);
+
+         trueState = nextTrueState;
+         stateError = nextTrueStateError;
+      }
+
+   }
+
+
+
+   @Test
+   public void testViaKeepingCovariancesConstantAndCheckingKMatrixConvergence()
+   {
+      nStates = 6;
+      nInputs = 1;
+      nMeasurements = 8;
+      createRandomParameters(nStates, nInputs, nMeasurements);
+
+      YoKalmanFilter kalmanFilter = new YoKalmanFilter("yo", nStates, nInputs, nMeasurements, parentRegistry);
+
+      kalmanFilter.configure(F, G, H);
+      kalmanFilter.setProcessNoiseCovariance(Q);
+      kalmanFilter.setMeasurementNoiseCovariance(R);
+      kalmanFilter.setState(x, P);
+
+      int numberOfTicksToTest = 1000;
+
+      for (int i = 0; i < numberOfTicksToTest; i++)
+      {
+         // Update the Kalman filter with the input and the measurement and get the estimated state:
+         kalmanFilter.predict(u);
+         kalmanFilter.update(y);
+         DenseMatrix64F kalmanKGain = kalmanFilter.getKGain();
+         printIfDebug("kalmanKGain = " + kalmanKGain);
+         printIfDebug("Covariance = " + kalmanFilter.getCovariance());
+      }
+
+      {
+         // Check that, after prediction but before update, P = FPF' - FPH'(HPH'+R)^-1 HPF' + Q
+         kalmanFilter.predict(u);
+
+         DenseMatrix64F P = kalmanFilter.getCovariance();
+
+         DenseMatrix64F FTranspose = new DenseMatrix64F(F);
+         CommonOps.transpose(FTranspose);
+
+         DenseMatrix64F HTranspose = new DenseMatrix64F(H);
+         CommonOps.transpose(HTranspose);
+
+         DenseMatrix64F FP = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(F, P, FP);
+
+         DenseMatrix64F FPFTranspose = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(FP, FTranspose, FPFTranspose);
+
+         DenseMatrix64F FPHTranspose = new DenseMatrix64F(nStates, nMeasurements);
+         CommonOps.mult(FP, HTranspose, FPHTranspose);
+
+         DenseMatrix64F HP = new DenseMatrix64F(nMeasurements, nStates);
+         CommonOps.mult(H, P, HP);
+
+         DenseMatrix64F HPHTranspose = new DenseMatrix64F(nMeasurements, nMeasurements);
+         CommonOps.mult(HP, HTranspose, HPHTranspose);
+
+         DenseMatrix64F HPFTranspose = new DenseMatrix64F(nMeasurements, nStates);
+         CommonOps.mult(HP, FTranspose, HPFTranspose);
+
+         DenseMatrix64F HPHTransposePlusRInverse = new DenseMatrix64F(HPHTranspose);
+         CommonOps.add(HPHTransposePlusRInverse, R, HPHTransposePlusRInverse);
+         CommonOps.invert(HPHTransposePlusRInverse);
+
+         DenseMatrix64F leftMiddleBlock = new DenseMatrix64F(nStates, nMeasurements);
+         CommonOps.mult(FPHTranspose, HPHTransposePlusRInverse, leftMiddleBlock);
+         DenseMatrix64F middleBlock = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(leftMiddleBlock, HPFTranspose, middleBlock);
+         CommonOps.scale(-1.0, middleBlock);
+
+         DenseMatrix64F convergedP = new DenseMatrix64F(FPFTranspose);
+         CommonOps.add(convergedP, middleBlock, convergedP);
+         CommonOps.add(convergedP, Q, convergedP);
+
+         printIfDebug("P = " + P);
+         printIfDebug("convergedP = " + convergedP);
+
+         EjmlUnitTests.assertEquals(P, convergedP, 1e-6);
+      }
+
+      {
+         // Check that, after update, P = (I - KH) * (FPF' + Q)
+         kalmanFilter.update(y);
+
+         DenseMatrix64F P = kalmanFilter.getCovariance();
+
+         DenseMatrix64F FP = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(F, P, FP);
+
+         DenseMatrix64F FTranspose = new DenseMatrix64F(F);
+         CommonOps.transpose(FTranspose);
+
+         DenseMatrix64F FPFTranspose = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(FP, FTranspose, FPFTranspose);
+
+         DenseMatrix64F K = kalmanFilter.getKGain();
+         
+         printIfDebug("K = " + K);
+         printIfDebug("H = " + H);
+
+         
+         DenseMatrix64F KH = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(K, H, KH);
+
+         DenseMatrix64F IMinusKH = CommonOps.identity(nStates);
+         CommonOps.sub(IMinusKH, KH, IMinusKH);
+
+         DenseMatrix64F FPFTransposePlusQ = new DenseMatrix64F(nStates, nStates);
+         CommonOps.add(FPFTranspose, Q, FPFTransposePlusQ);
+
+         DenseMatrix64F convergedP = new DenseMatrix64F(nStates, nStates);
+         CommonOps.mult(IMinusKH, FPFTransposePlusQ, convergedP);
+
+         printIfDebug("P = " + P);
+         printIfDebug("convergedP = " + convergedP);
+
+         EjmlUnitTests.assertEquals(P, convergedP, 1e-6);
+      }
+
    }
 
    private void createRandomParameters(int nStates, int nInputs, int nMeasurements)
@@ -270,5 +493,13 @@ public class YoKalmanFilterTest
       P = RandomMatrices.createSymmPosDef(nStates, random);
       u = RandomMatrices.createRandom(nInputs, 1, random);
       y = RandomMatrices.createRandom(nMeasurements, 1, random);
+   }
+   
+   private void printIfDebug(String toPrint)
+   {
+      if (DEBUG)
+      {
+         System.out.println(toPrint);
+      }
    }
 }
