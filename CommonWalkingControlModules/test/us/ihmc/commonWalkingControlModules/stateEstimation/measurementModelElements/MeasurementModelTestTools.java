b6720d80125354a5557478531c9e5325aced836a
Twan Koolen <tkoolen@ihmc.us>
2013-03-25 21:57:45 +0000

    * bug fixes in PointVelocityMeasurementModelElement * expanded test for PointVelocityMeasurementModelElement. Not completely working yet.

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/stateEstimation/measurementModelElements/MeasurementModelTestTools.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/stateEstimation/measurementModelElements/MeasurementModelTestTools.java
@@ -1,14 +1,21 @@
 package us.ihmc.commonWalkingControlModules.stateEstimation.measurementModelElements;
 
+import javax.vecmath.AxisAngle4d;
+import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
+
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
 import org.ejml.ops.EjmlUnitTests;
 
 import us.ihmc.commonWalkingControlModules.stateEstimation.MeasurementModelElement;
 import us.ihmc.controlFlow.ControlFlowOutputPort;
+import us.ihmc.utilities.math.MathTools;
 import us.ihmc.utilities.math.MatrixTools;
 import us.ihmc.utilities.math.geometry.Direction;
+import us.ihmc.utilities.math.geometry.FrameOrientation;
 import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.RotationFunctions;
 
 public class MeasurementModelTestTools
 {
@@ -43,6 +50,36 @@ public class MeasurementModelTestTools
 
          FrameVector perturbedState = new FrameVector(nominalState);
          perturbedState.add(perturbationVector);
+         statePort.setData(perturbedState);
+
+         if (runnable != null)
+            runnable.run();
+
+         MeasurementModelTestTools.assertDeltaResidualCorrect(modelElement, outputMatrixBlock, perturbationEjmlVector, tolerance);
+      }
+
+      statePort.setData(nominalState);
+   }
+
+   public static void assertOutputMatrixCorrectUsingPerturbation(ControlFlowOutputPort<FrameOrientation> statePort, MeasurementModelElement modelElement,
+           FrameOrientation nominalState, double perturbationMagnitude, double tolerance, Runnable runnable)
+   {
+      DenseMatrix64F outputMatrixBlock = modelElement.getOutputMatrixBlock(statePort);
+      for (Direction direction : Direction.values())
+      {
+         Vector3d perturbationRotationVector = new Vector3d();
+         MathTools.set(perturbationRotationVector, direction, perturbationMagnitude);
+
+         DenseMatrix64F perturbationEjmlVector = new DenseMatrix64F(3, 1);
+         MatrixTools.setDenseMatrixFromTuple3d(perturbationEjmlVector, perturbationRotationVector, 0, 0);
+
+         Quat4d perturbedQuaternion = new Quat4d();
+         AxisAngle4d perturbationAxisAngle = new AxisAngle4d();
+         RotationFunctions.setAxisAngleBasedOnRotationVector(perturbationAxisAngle, perturbationRotationVector);
+         Quat4d perturbationQuaternion = new Quat4d();
+         perturbationQuaternion.set(perturbationAxisAngle);
+         perturbedQuaternion.mul(perturbationQuaternion);
+         FrameOrientation perturbedState = new FrameOrientation(nominalState.getReferenceFrame(), perturbedQuaternion);
          statePort.setData(perturbedState);
 
          if (runnable != null)
