229dd4ca7dd96296f40df4a1fc16901ff2eb7872
Stephen McCrory <smccrory@ihmc.us>
2013-04-04 16:48:49 +0000

    New simulation for testing more robust touchdown

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCSingleElevationChangeSimulation.java
@@ -0,0 +1,50 @@
+package us.ihmc.darpaRoboticsChallenge;
+
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.commonWalkingControlModules.automaticSimulationRunner.AutomaticSimulationRunner;
+
+import us.ihmc.darpaRoboticsChallenge.drcRobot.PlainDRCRobot;
+import us.ihmc.darpaRoboticsChallenge.initialSetup.SquaredUpDRCRobotInitialSetup;
+import us.ihmc.projectM.R2Sim02.initialSetup.RobotInitialSetup;
+
+import com.martiansoftware.jsap.JSAPException;
+import com.yobotics.simulationconstructionset.BooleanYoVariable;
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.util.SingleStepGroundProfile;
+
+
+public class DRCSingleElevationChangeSimulation
+{   
+   public static void main(String[] args) throws JSAPException
+   {
+      final double stepHeight = 0.05;
+      final boolean stepUp = false;
+      
+      AutomaticSimulationRunner automaticSimulationRunner = null;
+      DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false);
+
+      DRCRobotInterface robotInterface = new PlainDRCRobot(DRCRobotModel.getDefaultRobotModel());
+      final double groundHeight = 0.0;
+      double elevationChange = 0.0;
+      if(stepUp) elevationChange = Math.abs(stepHeight);
+      else elevationChange = - Math.abs(stepHeight);
+      DRCSCSInitialSetup scsInitialSetup = new DRCSCSInitialSetup(new SingleStepGroundProfile(0.0, 5.0, -2.0, 2.0, 0.53, elevationChange), robotInterface.getSimulateDT());    //(new FlatGroundProfile(groundHeight), robotInterface.getSimulateDT());
+      RobotInitialSetup<SDFRobot> robotInitialSetup = new SquaredUpDRCRobotInitialSetup(groundHeight);
+
+      boolean useVelocityAndHeadingScript = false;
+      boolean cheatWithGroundHeightAtForFootstep = false;
+      boolean doChestOrientationControl = false;
+
+      DRCRobotWalkingControllerParameters drcControlParameters = new DRCRobotWalkingControllerParameters();
+      
+      DRCFlatGroundWalkingTrack drcFlatGroundWalkingTrack = new DRCFlatGroundWalkingTrack(drcControlParameters, robotInterface, 
+            robotInitialSetup, guiInitialSetup, scsInitialSetup, 
+            useVelocityAndHeadingScript, automaticSimulationRunner, 0.005, 16000, 
+            doChestOrientationControl, cheatWithGroundHeightAtForFootstep);
+      
+      ((BooleanYoVariable) drcFlatGroundWalkingTrack.getSimulationConstructionSet().getVariable("walk")).set(true);
+      ((DoubleYoVariable) drcFlatGroundWalkingTrack.getSimulationConstructionSet().getVariable("desiredVelocityX")).set(1.0);
+   }
+
+   
+}
