ff81ecb88519d9f17284401d622c577f7522d96b
Jerry Pratt <jpratt@ihmc.us>
2013-04-05 21:03:56 +0000

    Started refactoring estimator evaluator classes in preparation for making one for DRC.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/stateEstimation/StateEstimatorEstimatorEvaluatorRobot.java
@@ -0,0 +1,241 @@
+package us.ihmc.commonWalkingControlModules.stateEstimation;
+
+import java.util.ArrayList;
+
+import javax.media.j3d.Transform3D;
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Point3d;
+import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
+
+import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
+import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
+import us.ihmc.utilities.Axis;
+
+import com.yobotics.simulationconstructionset.ExternalForcePoint;
+import com.yobotics.simulationconstructionset.FloatingJoint;
+import com.yobotics.simulationconstructionset.IMUMount;
+import com.yobotics.simulationconstructionset.KinematicPoint;
+import com.yobotics.simulationconstructionset.Link;
+import com.yobotics.simulationconstructionset.PinJoint;
+import com.yobotics.simulationconstructionset.Robot;
+
+public class StateEstimatorEstimatorEvaluatorRobot extends Robot
+{
+   private static final boolean ADD_ARM_LINKS = true;
+   private static final boolean OFFSET_IMU_FRAMES = true;
+   private static final boolean ROTATE_IMU_FRAMES = true;
+   
+   private static final long serialVersionUID = 2647791981594204134L;
+   private final Link bodyLink;
+   private final FloatingJoint rootJoint;
+   private final ArrayList<IMUMount> imuMounts = new ArrayList<IMUMount>();
+   private final ArrayList<KinematicPoint> velocityPoints = new ArrayList<KinematicPoint>();
+
+   private final Vector3d gravitationalAcceleration = new Vector3d(0.0, 0.0, -9.81);
+   
+   public StateEstimatorEstimatorEvaluatorRobot()
+   {
+      super(StateEstimatorEstimatorEvaluatorRobot.class.getSimpleName());
+
+      rootJoint = new FloatingJoint("root", new Vector3d(), this);
+
+      bodyLink = new Link("body");
+      bodyLink.setMassAndRadiiOfGyration(10.0, 0.1, 0.2, 0.3);
+
+      ExternalForcePoint externalForcePoint = new ExternalForcePoint("ef_rootJoint", this);
+      rootJoint.addExternalForcePoint(externalForcePoint);
+
+      Graphics3DObject bodyLinkGraphics = new Graphics3DObject();
+      bodyLinkGraphics.translate(0.0, 0.0, -0.15);
+      bodyLinkGraphics.addCube(0.1, 0.2, 0.3, YoAppearance.Red());
+      bodyLink.setLinkGraphics(bodyLinkGraphics);
+      rootJoint.setLink(bodyLink);
+
+      Transform3D imu0Offset = new Transform3D();
+      Vector3d offsetVector0 = new Vector3d();
+      imu0Offset.setTranslation(offsetVector0);
+
+      IMUMount imuMount0 = new IMUMount("imuMount0", imu0Offset, this);
+      KinematicPoint kinematicPoint0 = new KinematicPoint("kp0", offsetVector0, this);
+      rootJoint.addKinematicPoint(kinematicPoint0);
+      rootJoint.addIMUMount(imuMount0);
+
+      Vector3d velocityPointOffsetVector0 = new Vector3d(0.0, 0.0, 0.2);
+      KinematicPoint velocityPoint0 = new KinematicPoint("vp0", velocityPointOffsetVector0, this);
+      rootJoint.addKinematicPoint(velocityPoint0);
+      
+      this.addRootJoint(rootJoint);
+
+      if (ADD_ARM_LINKS)
+      {
+         PinJoint pinJoint1 = new PinJoint("pinJoint1", new Vector3d(), this, Axis.X);
+         Link armLink1 = new Link("armLink1");
+         armLink1.setMassAndRadiiOfGyration(0.3, 0.1, 0.1, 0.1);
+         armLink1.setComOffset(new Vector3d(0.0, 0.0, 0.5));
+
+         Graphics3DObject armLink1Graphics = new Graphics3DObject();
+
+         // armLink1Graphics.rotate(-Math.PI/2.0, Graphics3DObject.X);
+         armLink1Graphics.addCylinder(1.0, 0.02, YoAppearance.Green());
+         armLink1.setLinkGraphics(armLink1Graphics);
+         pinJoint1.setLink(armLink1);
+
+         Transform3D imu1Offset = new Transform3D();
+         if (ROTATE_IMU_FRAMES)
+         {
+            imu1Offset.rotX(Math.PI / 7.0);
+         }
+
+         Vector3d offsetVector1 = new Vector3d();
+         if (OFFSET_IMU_FRAMES)
+         {
+            offsetVector1.set(0.1, 0.2, 0.3);
+         }
+
+         imu1Offset.setTranslation(offsetVector1);
+
+         KinematicPoint kinematicPoint1 = new KinematicPoint("kp1", offsetVector1, this);
+         pinJoint1.addKinematicPoint(kinematicPoint1);
+         IMUMount imuMount1 = new IMUMount("imuMount1", imu1Offset, this);
+         pinJoint1.addIMUMount(imuMount1);
+
+         rootJoint.addJoint(pinJoint1);
+
+         PinJoint pinJoint2 = new PinJoint("pinJoint2", new Vector3d(0.0, 0.0, 1.0), this, Axis.Z);
+         Link armLink2 = new Link("armLink2");
+         armLink2.setMassAndRadiiOfGyration(0.2, 0.1, 0.1, 0.1);
+         armLink2.setComOffset(new Vector3d(0.2, 0.0, 0.0));
+
+         Graphics3DObject armLink2Graphics = new Graphics3DObject();
+         armLink2Graphics.rotate(Math.PI / 2.0, Graphics3DObject.Y);
+         armLink2Graphics.addCylinder(1.0, 0.02, YoAppearance.Blue());
+         armLink2.setLinkGraphics(armLink2Graphics);
+         pinJoint2.setLink(armLink2);
+
+         Transform3D imu2Offset = new Transform3D();
+         if (ROTATE_IMU_FRAMES)
+         {
+            imu2Offset.rotY(Math.PI / 8.0);
+         }
+
+         Vector3d offsetVector2 = new Vector3d();
+         if (OFFSET_IMU_FRAMES)
+         {
+            offsetVector2.set(0.1, 0.03, 0.007);
+         }
+
+         imu2Offset.setTranslation(offsetVector2);
+
+         KinematicPoint kinematicPoint2 = new KinematicPoint("kp2", offsetVector2, this);
+         pinJoint2.addKinematicPoint(kinematicPoint2);
+         IMUMount imuMount2 = new IMUMount("imuMount2", imu2Offset, this);
+         pinJoint2.addIMUMount(imuMount2);
+
+         Vector3d velocityPointOffsetVector2 = new Vector3d(0.1, 0.2, 0.3);
+         KinematicPoint velocityPoint2 = new KinematicPoint("vp2", velocityPointOffsetVector2, this);
+         pinJoint2.addKinematicPoint(velocityPoint2);
+         
+//         Vector3d velocityPointOffsetVector2A = new Vector3d(0.0, 0.3, 0.0);
+//         KinematicPoint velocityPoint2A = new KinematicPoint("vp2A", velocityPointOffsetVector2A, this);
+//         pinJoint2.addKinematicPoint(velocityPoint2A);
+
+         pinJoint1.addJoint(pinJoint2);
+
+         pinJoint1.setQ(1.2);
+         pinJoint2.setQ(0.8);
+
+//         pinJoint1.setQd(-0.5);
+//         pinJoint2.setQd(0.77);
+
+         imuMounts.add(imuMount0);
+         imuMounts.add(imuMount1);
+         imuMounts.add(imuMount2);
+
+         velocityPoints.add(velocityPoint0);
+         velocityPoints.add(velocityPoint2);
+      }
+
+      else
+      {
+         //TODO: Temp since get an exception if there are no pin joints right now...
+         PinJoint pinJoint1 = new PinJoint("pinJoint1", new Vector3d(), this, Axis.X);
+         Link armLink1 = new Link("armLink1");
+         armLink1.setMassAndRadiiOfGyration(0.001, 0.1, 0.1, 0.1);
+         armLink1.setComOffset(new Vector3d(0.0, 0.0, 0.0));
+         pinJoint1.setLink(armLink1);
+         rootJoint.addJoint(pinJoint1);
+         
+         imuMounts.add(imuMount0);
+         velocityPoints.add(velocityPoint0);
+      }
+
+      this.setGravity(gravitationalAcceleration);
+
+      if (ADD_ARM_LINKS)
+      {
+         rootJoint.setPosition(new Point3d(0.0, 0.0, 0.4));
+         Matrix3d rotationMatrix = new Matrix3d();
+         rotationMatrix.rotX(0.6);
+         rootJoint.setRotation(rotationMatrix);
+
+         rootJoint.setAngularVelocityInBody(new Vector3d(0.1, 0.2, 0.07));
+//         rootJoint.setAngularVelocityInBody(new Vector3d(0.2, 2.2, 0.3));
+      }
+
+      else
+      {
+         Transform3D rootJointPostionAndRotation = new Transform3D();
+         rootJointPostionAndRotation.rotY(Math.PI*0.9);
+         rootJointPostionAndRotation.setTranslation(new Vector3d(0.0, 0.0, 0.4));
+         rootJoint.setRotationAndTranslation(rootJointPostionAndRotation);
+         
+         rootJoint.setVelocity(new Vector3d());
+//         rootJoint.setAngularVelocityInBody(new Vector3d(0.2, 2.5, 0.0));
+      }
+
+      Vector3d externalForceVector = new Vector3d(gravitationalAcceleration);
+      Point3d comPoint = new Point3d();
+      double mass = this.computeCenterOfMass(comPoint);
+      externalForceVector.scale(-mass);
+      externalForcePoint.setForce(externalForceVector);
+
+      update();
+   }
+
+   // public Link getBodyLink()
+   // {
+   // return bodyLink;
+   // }
+
+   public FloatingJoint getRootJoint()
+   {
+      return rootJoint;
+   }
+
+   public ArrayList<IMUMount> getIMUMounts()
+   {
+      return imuMounts;
+   }
+
+   public ArrayList<KinematicPoint> getVelocityPoints()
+   {
+      return velocityPoints;
+   }
+
+   public Vector3d getActualAngularAccelerationInBodyFrame()
+   {
+      return rootJoint.getAngularAccelerationInBody();
+   }
+
+   public Quat4d getActualOrientation()
+   {
+      return rootJoint.getQuaternion();
+   }
+   
+   public Vector3d getActualAngularVelocity()
+   {
+      return rootJoint.getAngularVelocityInBody();
+   }
+}
+
