efaa07c9ba4955b289b1544688acc25b676c49f6
Jerry Pratt <jpratt@ihmc.us>
2013-04-09 02:59:33 +0000

    More SensorMap and SensorMapFromRobotFactory.

--- SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SensorMap.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SensorMap.java
@@ -1,6 +1,8 @@
 package us.ihmc.sensorProcessing.simulatedSensors;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.vecmath.Matrix3d;
 import javax.vecmath.Vector3d;
@@ -16,7 +18,7 @@ public class SensorMap
                                                                                                      ControlFlowOutputPort<Double>>();
 
    private final LinkedHashMap<IMUDefinition, ControlFlowOutputPort<Matrix3d>> orientationSensors = new LinkedHashMap<IMUDefinition,
-                                                                                                     ControlFlowOutputPort<Matrix3d>>();
+                                                                                                       ControlFlowOutputPort<Matrix3d>>();
    private final LinkedHashMap<IMUDefinition, ControlFlowOutputPort<Vector3d>> angularVelocitySensors = new LinkedHashMap<IMUDefinition,
                                                                                                            ControlFlowOutputPort<Vector3d>>();
    private final LinkedHashMap<IMUDefinition, ControlFlowOutputPort<Vector3d>> linearAccelerationSensors = new LinkedHashMap<IMUDefinition,
@@ -24,10 +26,39 @@ public class SensorMap
 
    private final LinkedHashMap<PointVelocitySensorDefinition, ControlFlowOutputPort<Vector3d>> pointVelocitySensors =
       new LinkedHashMap<PointVelocitySensorDefinition, ControlFlowOutputPort<Vector3d>>();
-
-
+   
    public SensorMap()
    {
+   }
+
+   public Map<OneDoFJoint, ControlFlowOutputPort<Double>> getJointPositionSensors()
+   {
+      return jointPositionSensors;
+   }
+
+   public Map<OneDoFJoint, ControlFlowOutputPort<Double>> getJointVelocitySensors()
+   {
+      return jointVelocitySensors;
+   }
+
+   public Map<IMUDefinition, ControlFlowOutputPort<Matrix3d>> getOrientationSensors()
+   {
+      return orientationSensors;
+   }
+
+   public Map<IMUDefinition, ControlFlowOutputPort<Vector3d>> getAngularVelocitySensors()
+   {
+      return angularVelocitySensors;
+   }
+
+   public Map<IMUDefinition, ControlFlowOutputPort<Vector3d>> getLinearAccelerationSensors()
+   {
+      return linearAccelerationSensors;
+   }
+   
+   public Map<PointVelocitySensorDefinition, ControlFlowOutputPort<Vector3d>> getPointVelocitySensors()
+   {
+      return pointVelocitySensors;
    }
 
    public ControlFlowOutputPort<Double> getJointPositionSensorPort(OneDoFJoint oneDoFJoint)
@@ -88,6 +119,26 @@ public class SensorMap
    public void addPointVelocitySensorPort(PointVelocitySensorDefinition pointVelocitySensorDefinition, ControlFlowOutputPort<Vector3d> pointVelocitySensorPort)
    {
       pointVelocitySensors.put(pointVelocitySensorDefinition, pointVelocitySensorPort);
+   }
+
+   public Collection<ControlFlowOutputPort<Matrix3d>> getOrientationOutputPorts()
+   {
+      return orientationSensors.values();
+   }
+
+   public Collection<ControlFlowOutputPort<Vector3d>> getAngularVelocityOutputPorts()
+   {
+      return angularVelocitySensors.values();
+   }
+
+   public Collection<ControlFlowOutputPort<Vector3d>> getLinearAccelerationOutputPorts()
+   {
+      return linearAccelerationSensors.values();
+   }
+
+   public Collection<ControlFlowOutputPort<Vector3d>> getPointVelocitySensorOutputPorts()
+   {
+      return pointVelocitySensors.values();
    }
 
 }
