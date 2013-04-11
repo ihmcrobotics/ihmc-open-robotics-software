6cac6bc40b4f230e4c9a73ea65c5bab5b52b91ba
Jerry Pratt <jpratt@ihmc.us>
2013-04-11 03:00:58 +0000

    Refactoring Estimator: Pulled out FullInverseDynamicsStructure. Got simulated robot out of where the estimating is done now.

--- SensorProcessing/src/us/ihmc/sensorProcessing/stateEstimation/evaluation/ComposableStateEstimatorEvaluator.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/stateEstimation/evaluation/ComposableStateEstimatorEvaluator.java
@@ -1,13 +1,17 @@
 package us.ihmc.sensorProcessing.stateEstimation.evaluation;
 
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Vector3d;
+
 import us.ihmc.sensorProcessing.simulatedSensors.InverseDynamicsJointsFromSCSRobotGenerator;
 import us.ihmc.sensorProcessing.simulatedSensors.SensorMap;
 import us.ihmc.sensorProcessing.stateEstimation.DesiredCoMAccelerationsFromRobotStealerController;
-import us.ihmc.sensorProcessing.stateEstimation.JointStateFullRobotModelUpdater;
+import us.ihmc.sensorProcessing.stateEstimation.OrientationEstimator;
 import us.ihmc.sensorProcessing.stateEstimation.SimulatedSensorController;
-import us.ihmc.utilities.math.geometry.ReferenceFrame;
-import us.ihmc.utilities.screwTheory.SpatialAccelerationCalculator;
-import us.ihmc.utilities.screwTheory.TwistCalculator;
+import us.ihmc.utilities.math.geometry.FrameOrientation;
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.screwTheory.RigidBody;
+import us.ihmc.utilities.screwTheory.SixDoFJoint;
 
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
@@ -24,6 +28,8 @@ public class ComposableStateEstimatorEvaluator
    private final String name = getClass().getSimpleName();
    private final YoVariableRegistry registry = new YoVariableRegistry(name);
 
+   private static final boolean INITIALIZE_ANGULAR_VELOCITY_ESTIMATE_TO_ACTUAL = true;    // false;
+
    public ComposableStateEstimatorEvaluator()
    {
       StateEstimatorEvaluatorRobot robot = new StateEstimatorEvaluatorRobot();
@@ -35,24 +41,47 @@ public class ComposableStateEstimatorEvaluator
       SimulatedSensorController simulatedSensorController = new SimulatedSensorController(perfectFullRobotModel, generator, robot, controlDT);
       SensorMap sensorMap = simulatedSensorController.getSensorMap();
 
-//      TwistCalculator twistCalculator = new TwistCalculator(ReferenceFrame.getWorldFrame(), perfectFullRobotModel.getElevator());
-//      SpatialAccelerationCalculator spatialAccelerationCalculator = new SpatialAccelerationCalculator(perfectFullRobotModel.getElevator(), twistCalculator,
-//                                                                       0.0, false);
-
-
       DesiredCoMAccelerationsFromRobotStealerController desiredCoMAccelerationsFromRobotStealerController =
          new DesiredCoMAccelerationsFromRobotStealerController(perfectFullRobotModel, generator, robot, controlDT);
 
       StateEstimatorEvaluatorFullRobotModel estimatedFullRobotModel = simulatedSensorController.getStateEstimatorEvaluatorFullRobotModel();
 
+      RigidBody estimationLink = estimatedFullRobotModel.getRootBody();
+      RigidBody elevator = estimatedFullRobotModel.getElevator();
+      SixDoFJoint rootInverseDynamicsJoint = estimatedFullRobotModel.getRootInverseDynamicsJoint();
+
+      FullInverseDynamicsStructure robotStuffToEstimate = new FullInverseDynamicsStructure(elevator, estimationLink, rootInverseDynamicsJoint);
 
       ComposableStateEstimatorEvaluatorController composableStateEstimatorEvaluatorController = new ComposableStateEstimatorEvaluatorController(robot,
-                                                                                                   estimatedFullRobotModel,
-                                                                                                   controlDT, sensorMap,
+                                                                                                   robotStuffToEstimate, controlDT, sensorMap,
                                                                                                    desiredCoMAccelerationsFromRobotStealerController);
       robot.setController(simulatedSensorController, simTicksPerControlDT);
       robot.setController(desiredCoMAccelerationsFromRobotStealerController, simTicksPerControlDT);
       robot.setController(composableStateEstimatorEvaluatorController, simTicksPerControlDT);
+
+      OrientationEstimator orientationEstimator = composableStateEstimatorEvaluatorController.getOrientationEstimator();
+
+      if (INITIALIZE_ANGULAR_VELOCITY_ESTIMATE_TO_ACTUAL)
+      {
+         estimatedFullRobotModel.updateBasedOnRobot(true);
+
+         Matrix3d rotationMatrix = new Matrix3d();
+         robot.getRootJoint().getRotationToWorld(rotationMatrix);
+         Vector3d angularVelocityInBody = robot.getRootJoint().getAngularVelocityInBody();
+
+         FrameOrientation estimatedOrientation = orientationEstimator.getEstimatedOrientation();
+         estimatedOrientation.set(rotationMatrix);
+         orientationEstimator.setEstimatedOrientation(estimatedOrientation);
+
+         FrameVector estimatedAngularVelocity = orientationEstimator.getEstimatedAngularVelocity();
+         estimatedAngularVelocity.set(angularVelocityInBody);
+         orientationEstimator.setEstimatedAngularVelocity(estimatedAngularVelocity);
+
+         // TODO: This wasn't doing anything.
+         // DenseMatrix64F x = orientationEstimator.getState();
+         // MatrixTools.insertTuple3dIntoEJMLVector(angularVelocityInBody, x, 3);
+         // orientationEstimator.setState(x, orientationEstimator.getCovariance());
+      }
 
       SimulationConstructionSet scs = new SimulationConstructionSet(robot, SHOW_GUI, 32000);
       scs.addYoVariableRegistry(registry);
