08a6cbcc073a930af68e3908086d07083061196c
Jerry Pratt <jpratt@ihmc.us>
2013-04-15 21:00:36 +0000

    Made a CommonWalkingReferenceFramesUpdater

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/CommonWalkingReferenceFramesUpdater.java
@@ -0,0 +1,43 @@
+package us.ihmc.commonWalkingControlModules.sensors;
+
+import us.ihmc.commonWalkingControlModules.referenceFrames.CommonWalkingReferenceFrames;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.robotController.SensorProcessor;
+
+public class CommonWalkingReferenceFramesUpdater implements SensorProcessor
+{
+   private final String name = getClass().getSimpleName();
+   private final YoVariableRegistry registry = new YoVariableRegistry(name);
+   private final CommonWalkingReferenceFrames refrenceFrames;
+
+   public CommonWalkingReferenceFramesUpdater(CommonWalkingReferenceFrames refrenceFrames)
+   {
+      this.refrenceFrames = refrenceFrames;
+   }
+
+   public void initialize()
+   {
+      update();
+   }
+
+   public YoVariableRegistry getYoVariableRegistry()
+   {
+      return registry;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public String getDescription()
+   {
+      return getName();
+   }
+
+   public void update()
+   {
+      refrenceFrames.updateFrames();
+   }
+}
