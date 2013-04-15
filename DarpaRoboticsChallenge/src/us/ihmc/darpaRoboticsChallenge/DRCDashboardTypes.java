8e9f356d234df540e5ee54ddeff45ae36a2d52a0
Douglas Stephen <dstephen@ihmc.us>
2013-04-15 17:14:46 +0000

    Small bugfixes. Still needs a lot more.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCDashboardTypes.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCDashboardTypes.java
@@ -2,38 +2,39 @@ package us.ihmc.darpaRoboticsChallenge;
 
 import java.util.HashMap;
 
-public class DRCDashboardTypes {
-	
-	public enum DRCTask
-	{
-		QUAL1, QUAL2, QUAL3, QUAL4, VRC1, VRC2, VRC3, DRIVING_TASK1, DRIVING_TASK1_WITH_EXTERNAL_CAMS, DRIVING_TEST_TRACK_WITH_EXTERNAL_CAMS;
-	}
-	
-	private static final HashMap<DRCTask, String> taskPath = new HashMap<DRCTask, String>()
-	{
-		private static final long serialVersionUID = 7295926598139266176L;
+public class DRCDashboardTypes
+{
 
-		{
-			put(DRCTask.QUAL1, "cd ~/workspace/GazeboStateCommunicator/launch/\n./qual1.sh\n");
-			put(DRCTask.QUAL2, "cd ~/workspace/GazeboStateCommunicator/launch/\n./qual2.sh\n");
-			put(DRCTask.QUAL3, "cd ~/workspace/GazeboStateCommunicator/launch/\n./qual3.sh\n");
-			put(DRCTask.QUAL4, "cd ~/workspace/GazeboStateCommunicator/launch/\n./qual4.sh\n");
-			put(DRCTask.VRC1, "cd ~/workspace/GazeboStateCommunicator/launch/\n./task1.sh\n");
-			put(DRCTask.VRC2, "cd ~/workspace/GazeboStateCommunicator/launch/\n./task2.sh\n");
-			put(DRCTask.VRC3, "cd ~/workspace/GazeboStateCommunicator/launch/\n./task3.sh\n");
-			put(DRCTask.DRIVING_TASK1, "roslaunch atlas_utils vrc_task_1.launch\n");
-			put(DRCTask.DRIVING_TASK1_WITH_EXTERNAL_CAMS, "cd ~/workspace/GazeboStateCommunicator/launch/\n./drivingTask1.sh\n");
-			put(DRCTask.DRIVING_TEST_TRACK_WITH_EXTERNAL_CAMS, "cd ~/workspace/GazeboStateCommunicator/launch/\n./drivingTestTrack.sh\n");
-		}
-	};
-	
-	public static String getDRCTaskLaunchScriptPath(DRCTask drcTask)
-	{
-		return taskPath.get(drcTask);
-	}
-	
-	public enum RobotModel
-	{
-		ATLAS_NO_HANDS, ATLAS_SANDIA_HANDS;
-	}
+   public enum DRCTask
+   {
+      QUAL1, QUAL2, QUAL3, QUAL4, VRC1, VRC2, VRC3, DRIVING_TASK1, DRIVING_TASK1_WITH_EXTERNAL_CAMS, DRIVING_TEST_TRACK_WITH_EXTERNAL_CAMS;
+   }
+
+   private static final HashMap<DRCTask, String> taskPath = new HashMap<DRCTask, String>()
+   {
+      private static final long serialVersionUID = 7295926598139266176L;
+
+      {
+         put(DRCTask.QUAL1, "cd ~/workspace/GazeboStateCommunicator/launch/; ./qual1.sh\n");
+         put(DRCTask.QUAL2, "cd ~/workspace/GazeboStateCommunicator/launch/; ./qual2.sh\n");
+         put(DRCTask.QUAL3, "cd ~/workspace/GazeboStateCommunicator/launch/; ./qual3.sh\n");
+         put(DRCTask.QUAL4, "cd ~/workspace/GazeboStateCommunicator/launch/; ./qual4.sh\n");
+         put(DRCTask.VRC1, "cd ~/workspace/GazeboStateCommunicator/launch/; ./task1.sh\n");
+         put(DRCTask.VRC2, "cd ~/workspace/GazeboStateCommunicator/launch/; ./task2.sh\n");
+         put(DRCTask.VRC3, "cd ~/workspace/GazeboStateCommunicator/launch/; ./task3.sh\n");
+         put(DRCTask.DRIVING_TASK1, "roslaunch atlas_utils vrc_task_1.launch\n");
+         put(DRCTask.DRIVING_TASK1_WITH_EXTERNAL_CAMS, "cd ~/workspace/GazeboStateCommunicator/launch/; ./drivingTask1.sh\n");
+         put(DRCTask.DRIVING_TEST_TRACK_WITH_EXTERNAL_CAMS, "cd ~/workspace/GazeboStateCommunicator/launch/; ./drivingTestTrack.sh\n");
+      }
+   };
+
+   public static String getDRCTaskLaunchScriptPath(DRCTask drcTask)
+   {
+      return taskPath.get(drcTask);
+   }
+
+   public enum RobotModel
+   {
+      ATLAS_NO_HANDS, ATLAS_SANDIA_HANDS;
+   }
 }
