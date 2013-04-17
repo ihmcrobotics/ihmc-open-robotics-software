a551cb6e80b018653f50011fa384788cf446ecf0
Eric Morphis <emorphis@ihmc.us>
2013-04-17 22:53:48 +0000

    Fixed R2StairClimbing.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/bipedSupportPolygons/BipedSupportPolygons.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/bipedSupportPolygons/BipedSupportPolygons.java
@@ -3,7 +3,6 @@ package us.ihmc.commonWalkingControlModules.bipedSupportPolygons;
 import java.awt.Color;
 import java.util.List;
 
-import us.ihmc.commonWalkingControlModules.desiredFootStep.DesiredFootstepCalculatorTools;
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.robotSide.SideDependentList;
 import us.ihmc.utilities.math.geometry.FrameConvexPolygon2d;
@@ -139,7 +138,6 @@ public class BipedSupportPolygons
       for (RobotSide robotSide : RobotSide.values())
       {
          List<FramePoint> contactPointsForSide = contactPoints.get(robotSide);
-         contactPointsForSide = DesiredFootstepCalculatorTools.fixTwoPointsAndCopy(contactPointsForSide); // TODO: terrible
          boolean isSupportFoot = contactPointsForSide.size() > 0;
          if (isSupportFoot)
          {
