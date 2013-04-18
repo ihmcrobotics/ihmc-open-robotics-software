57cd4a1e41afa3e3d130b99f26db15b27188c8ad
Jerry Pratt <jpratt@ihmc.us>
2013-04-18 22:59:44 +0000

    RunnableRunnerController.

---
+++ SensorProcessing/src/us/ihmc/sensorProcessing/stateEstimation/evaluation/RunnableRunnerController.java
@@ -0,0 +1,49 @@
+package us.ihmc.sensorProcessing.stateEstimation.evaluation;
+
+import java.util.ArrayList;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.robotController.RobotController;
+
+public class RunnableRunnerController implements RobotController
+{
+   private final String name = getClass().getSimpleName();
+   private final ArrayList<Runnable> runnables = new ArrayList<Runnable>();
+
+   public RunnableRunnerController()
+   {
+   }
+
+   public RunnableRunnerController(Runnable runnable)
+   {
+      this.runnables.add(runnable);
+   }
+
+
+   public void initialize()
+   {
+   }
+
+   public YoVariableRegistry getYoVariableRegistry()
+   {
+      return null;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public String getDescription()
+   {
+      return getName();
+   }
+
+   public void doControl()
+   {
+      for (Runnable runnable : runnables)
+      {
+         runnable.run();
+      }
+   }
+}
