78b6e4d2953ebdb209edc39d5b01ee6cd3c887c9
Twan Koolen <tkoolen@ihmc.us>
2013-04-23 03:26:29 +0000

    * got rid of test YoVariable in MomentumSolver. * commented out testing code in OriginalDynamicallyConsistentNullspaceCalculator.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/MomentumSolver.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/MomentumSolver.java
@@ -73,7 +73,6 @@ public class MomentumSolver
 
 
    private final List<InverseDynamicsJoint> unconstrainedJoints = new ArrayList<InverseDynamicsJoint>();
-   private final DoubleYoVariable aHatRootCondition = new DoubleYoVariable("aHatRootCondition", registry);
 
    public MomentumSolver(SixDoFJoint rootJoint, RigidBody elevator, ReferenceFrame centerOfMassFrame, TwistCalculator twistCalculator,
                          LinearSolver<DenseMatrix64F> jacobianSolver, double controlDT, YoVariableRegistry parentRegistry)
@@ -244,11 +243,6 @@ public class MomentumSolver
                      DenseMatrix64F momentumMultipliers)
    {
       DenseMatrix64F aHatRoot = aHats.get(rootJoint);
-
-      DenseMatrix64F aHatRootMomentum = new DenseMatrix64F(momentumSubspace.getNumCols(), aHatRoot.getNumCols());
-      CommonOps.multTransA(momentumSubspace, aHatRoot, aHatRootMomentum);
-      this.aHatRootCondition.set(NormOps.conditionP2(aHatRootMomentum));
-
       rootJointSolver.solveAndSetRootJointAcceleration(vdotRoot, aHatRoot, b, accelerationSubspace, accelerationMultipliers, momentumSubspace,
               momentumMultipliers, rootJoint, jointAccelerationValidMap);
       jointSpaceConstraintResolver.solveAndSetJointspaceAccelerations(jointSpaceAccelerations, jointAccelerationValidMap);
