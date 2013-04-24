58a96176a3656b7b15eaec40bbc67afd2425a010
Matt Johnson <mjohnson@ihmc.us>
2013-04-24 20:02:43 +0000

    verifying Gazebo LIDAR output

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networkProcessor/ros/RosLidarSubscriber.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networkProcessor/ros/RosLidarSubscriber.java
@@ -5,6 +5,8 @@ import us.ihmc.utilities.polarLidarGeometry.PolarLidarScanDefinition;
 
 public abstract class RosLidarSubscriber extends AbstractRosTopicSubscriber<sensor_msgs.LaserScan>
 {
+   PolarLidarScanDefinition initialPolarLidarScanDefinition = null;
+
    public RosLidarSubscriber()
    {
       super(sensor_msgs.LaserScan._TYPE);
@@ -14,14 +16,50 @@ public abstract class RosLidarSubscriber extends AbstractRosTopicSubscriber<sens
    {
       long timestamp = message.getHeader().getStamp().totalNsecs();
       float[] ranges = message.getRanges();
-      float maxRange = message.getRangeMax();
-      float minRange = message.getRangeMin();
-      PolarLidarScanDefinition lidarScanDefinition = new PolarLidarScanDefinition(ranges.length, 1, message.getAngleMax(), message.getAngleMin(), 0.0f, 0.0f, minRange);
-      
-      newScan(timestamp, ranges, lidarScanDefinition, minRange, maxRange, message.getTimeIncrement());
-      
+
+      PolarLidarScanDefinition polarLidarScanDefinition = new PolarLidarScanDefinition(ranges.length, 1, message.getAngleMax(), message.getAngleMin(),
+                                                             message.getAngleIncrement(), message.getTimeIncrement(), message.getScanTime(), 0.0f, 0.0f,
+                                                             message.getRangeMin(), message.getRangeMax());
+
+      verifyDataFromGazeboRemainsTheSame(polarLidarScanDefinition);
+
+      newScan(timestamp, ranges, polarLidarScanDefinition, message.getRangeMin(), message.getRangeMax(), message.getTimeIncrement());
+
    }
 
-   protected abstract void newScan(long timeStamp, float[] ranges, PolarLidarScanDefinition scanDefinition, double rangeMin, double rangeMax, double timeIncrement);
+   private void verifyDataFromGazeboRemainsTheSame(PolarLidarScanDefinition polarLidarScanDefinition)
+   {
+      verifyLidarScanDefinitionDoesNotChange(polarLidarScanDefinition);
+      verifyTimeIncrementRemainsZero(polarLidarScanDefinition);
+   }
+
+   private void verifyTimeIncrementRemainsZero(PolarLidarScanDefinition polarLidarScanDefinition)
+   {
+      if(polarLidarScanDefinition.timeIncrement != 0.0)
+      {
+         System.err.println("WARNING: Gazebo LIDAR time increment no longer zero: " + polarLidarScanDefinition.timeIncrement);
+      }
+   }
+
+   private void verifyLidarScanDefinitionDoesNotChange(PolarLidarScanDefinition polarLidarScanDefinition)
+   {
+      if(initialPolarLidarScanDefinition == null)
+      {
+         initialPolarLidarScanDefinition = polarLidarScanDefinition;
+      }
+      else
+      {
+         if(!polarLidarScanDefinition.equals(initialPolarLidarScanDefinition))
+         {
+            System.err.println("WARNING: your scan definition has changed");
+            System.err.println("old scan definition:\n" + initialPolarLidarScanDefinition);
+            System.err.println("new scan definition:\n" + polarLidarScanDefinition);
+            initialPolarLidarScanDefinition = polarLidarScanDefinition;
+         }
+      }
+   }
+
+   protected abstract void newScan(long timeStamp, float[] ranges, PolarLidarScanDefinition scanDefinition, double rangeMin, double rangeMax,
+                                   double timeIncrement);
 
 }
