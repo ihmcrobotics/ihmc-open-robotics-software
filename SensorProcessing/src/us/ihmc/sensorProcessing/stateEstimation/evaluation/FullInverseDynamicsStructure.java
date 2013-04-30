3bb6d6410f4f05e65f989eee33123c347f15dc32
Twan Koolen <tkoolen@ihmc.us>
2013-04-30 17:27:53 +0000

    * made it possible to change estimation link again * set estimation link to chest instead of pelvis.

--- SensorProcessing/src/us/ihmc/sensorProcessing/stateEstimation/evaluation/FullInverseDynamicsStructure.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/stateEstimation/evaluation/FullInverseDynamicsStructure.java
@@ -15,7 +15,6 @@ public class FullInverseDynamicsStructure
    private final TwistCalculator twistCalculator;
    private final SpatialAccelerationCalculator spatialAccelerationCalculator;
    private final RigidBody estimationLink;
-   private final ReferenceFrame estimationFrame;
    private final RigidBody elevator;
    private final SixDoFJoint rootJoint;
 
@@ -29,7 +28,6 @@ public class FullInverseDynamicsStructure
       spatialAccelerationCalculator = new SpatialAccelerationCalculator(elevator, twistCalculator, 0.0, false);
 
       this.estimationLink = estimationLink;
-      estimationFrame = estimationLink.getParentJoint().getFrameAfterJoint();
    }
 
    public SixDoFJoint getRootJoint()
@@ -54,7 +52,7 @@ public class FullInverseDynamicsStructure
 
    public ReferenceFrame getEstimationFrame()
    {
-      return estimationFrame;
+      return estimationLink.getParentJoint().getFrameAfterJoint();
    }
 
    public RigidBody getElevator()
@@ -67,33 +65,4 @@ public class FullInverseDynamicsStructure
       twistCalculator.compute();
       spatialAccelerationCalculator.compute();
    }
-
-   private final FrameOrientation estimatedOrientation = new FrameOrientation(ReferenceFrame.getWorldFrame());
-   private final FrameVector estimatedAngularVelocity = new FrameVector(ReferenceFrame.getWorldFrame());
-   
-   public void updateRootJointBasedOnEstimator(StateEstimator estimator)
-   {
-      estimator.getEstimatedOrientation(estimatedOrientation);
-      estimator.getEstimatedAngularVelocity(estimatedAngularVelocity);
-
-      updateRootJointBasedOnEstimator(estimatedOrientation, estimatedAngularVelocity);
-   }
-
-   public void updateRootJointBasedOnEstimator(FrameOrientation estimatedOrientation, FrameVector estimatedAngularVelocity)
-   {
-      rootJoint.setRotation(estimatedOrientation.getQuaternion());
-
-      elevator.updateFramesRecursively();
-
-      ReferenceFrame elevatorFrame = rootJoint.getFrameBeforeJoint();
-      ReferenceFrame bodyFrame = rootJoint.getFrameAfterJoint();
-
-      estimatedAngularVelocity.changeFrame(bodyFrame);
-
-      Twist bodyTwist = new Twist(bodyFrame, elevatorFrame, bodyFrame);
-      bodyTwist.setAngularPart(estimatedAngularVelocity.getVector());
-      rootJoint.setJointTwist(bodyTwist);
-   }
-
-
 }
