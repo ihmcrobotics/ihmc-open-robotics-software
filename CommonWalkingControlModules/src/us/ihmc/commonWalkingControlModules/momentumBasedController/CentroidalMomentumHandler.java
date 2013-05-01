e1da6f8b203fe6a4df552474f0847af162d9c1fd
Twan Koolen <tkoolen@ihmc.us>
2013-05-01 21:15:09 +0000

    Added CentroidalMomentumHandler.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/CentroidalMomentumHandler.java
@@ -0,0 +1,94 @@
+package us.ihmc.commonWalkingControlModules.momentumBasedController;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.MatrixYoVariableConversionTools;
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
+import us.ihmc.utilities.math.MatrixTools;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.screwTheory.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author twan
+ *         Date: 5/1/13
+ */
+public class CentroidalMomentumHandler
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+   private final CentroidalMomentumMatrix centroidalMomentumMatrix;
+   private final DenseMatrix64F adotV = new DenseMatrix64F(SpatialMotionVector.SIZE, 1);
+   private final DenseMatrix64F centroidalMomentumMatrixPart = new DenseMatrix64F(1, 1);
+
+   private final DenseMatrix64F centroidalMomentumMatrixDerivative;
+   private final DenseMatrix64F previousCentroidalMomentumMatrix;
+   private final DoubleYoVariable[][] yoPreviousCentroidalMomentumMatrix;    // to make numerical differentiation rewindable
+
+   private final double controlDT;
+   private final InverseDynamicsJoint[] jointsInOrder;
+   private final DenseMatrix64F v;
+   private final Map<InverseDynamicsJoint, int[]> columnsForJoints = new LinkedHashMap<InverseDynamicsJoint, int[]>();
+
+   public CentroidalMomentumHandler(InverseDynamicsJoint rootJoint, ReferenceFrame centerOfMassFrame, double controlDT, YoVariableRegistry parentRegistry)
+   {
+      this.jointsInOrder = ScrewTools.computeSupportAndSubtreeJoints(rootJoint.getSuccessor());
+
+      this.centroidalMomentumMatrix = new CentroidalMomentumMatrix(ScrewTools.getRootBody(rootJoint.getPredecessor()), centerOfMassFrame);
+      this.previousCentroidalMomentumMatrix = new DenseMatrix64F(centroidalMomentumMatrix.getMatrix().getNumRows(),
+              centroidalMomentumMatrix.getMatrix().getNumCols());
+      this.centroidalMomentumMatrixDerivative = new DenseMatrix64F(centroidalMomentumMatrix.getMatrix().getNumRows(),
+              centroidalMomentumMatrix.getMatrix().getNumCols());
+      yoPreviousCentroidalMomentumMatrix = new DoubleYoVariable[previousCentroidalMomentumMatrix.getNumRows()][previousCentroidalMomentumMatrix.getNumCols()];
+      MatrixYoVariableConversionTools.populateYoVariables(yoPreviousCentroidalMomentumMatrix, "previousCMMatrix", registry);
+
+      this.controlDT = controlDT;
+
+      int nDegreesOfFreedom = ScrewTools.computeDegreesOfFreedom(jointsInOrder);
+      this.v = new DenseMatrix64F(nDegreesOfFreedom, 1);
+
+      for (InverseDynamicsJoint joint : jointsInOrder)
+      {
+         columnsForJoints.put(joint, ScrewTools.computeIndicesForJoint(jointsInOrder, joint));
+      }
+
+      parentRegistry.addChild(registry);
+   }
+
+   public void compute()
+   {
+      centroidalMomentumMatrix.compute();
+      MatrixYoVariableConversionTools.getFromYoVariables(previousCentroidalMomentumMatrix, yoPreviousCentroidalMomentumMatrix);
+      MatrixTools.numericallyDifferentiate(centroidalMomentumMatrixDerivative, previousCentroidalMomentumMatrix, centroidalMomentumMatrix.getMatrix(),
+              controlDT);
+      MatrixYoVariableConversionTools.storeInYoVariables(previousCentroidalMomentumMatrix, yoPreviousCentroidalMomentumMatrix);
+
+      ScrewTools.packJointVelocitiesMatrix(jointsInOrder, v);
+      CommonOps.mult(centroidalMomentumMatrixDerivative, v, adotV);
+   }
+
+   public DenseMatrix64F getCentroidalMomentumMatrixPart(InverseDynamicsJoint[] joints)
+   {
+      int partDegreesOfFreedom = ScrewTools.computeDegreesOfFreedom(joints);
+      centroidalMomentumMatrixPart.reshape(Momentum.SIZE, partDegreesOfFreedom);
+      centroidalMomentumMatrixPart.zero();
+      for (InverseDynamicsJoint joint : joints)
+      {
+         int[] columnsForJoint = columnsForJoints.get(joint);
+         MatrixTools.extractColumns(centroidalMomentumMatrix.getMatrix(), centroidalMomentumMatrixPart, columnsForJoint);
+      }
+      return centroidalMomentumMatrixPart;
+   }
+
+   public DenseMatrix64F getCentroidalMomentumMatrix()
+   {
+      return centroidalMomentumMatrix.getMatrix();
+   }
+
+   public DenseMatrix64F getCentroidalMomentumConvectiveTerm()
+   {
+      return adotV;
+   }
+}
