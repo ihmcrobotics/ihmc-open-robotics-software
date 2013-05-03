e9aa229159912bfaf197c8652eeb70f22abae15f
Peter Neuhaus <pneuhaus@ihmc.us>
2013-05-03 19:19:32 +0000

    Some ROS stuff for the Sandia hands that Jesper asked me to write. This needs to checked if this is correct.

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/ROSSandiaJointMap.java
@@ -0,0 +1,39 @@
+package us.ihmc.darpaRoboticsChallenge.ros;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pneuhaus
+ * Date: 5/3/13
+ * Time: 1:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ROSSandiaJointMap
+{
+   public final static int left_f0_j0 = 0;
+   public final static int left_f0_j1 = 1;
+   public final static int left_f0_j2 = 2;
+   public final static int left_f1_j0 = 3;
+   public final static int left_f1_j1 = 4;
+   public final static int left_f1_j2 = 5;
+   public final static int left_f2_j0 = 6;
+   public final static int left_f2_j1 = 7;
+   public final static int left_f2_j2 = 8;
+   public final static int left_f3_j0 = 9;
+   public final static int left_f3_j1 = 10;
+   public final static int left_f3_j2 = 11;
+
+   public final static int right_f0_j0 = 0;
+   public final static int right_f0_j1 = 1;
+   public final static int right_f0_j2 = 2;
+   public final static int right_f1_j0 = 3;
+   public final static int right_f1_j1 = 4;
+   public final static int right_f1_j2 = 5;
+   public final static int right_f2_j0 = 6;
+   public final static int right_f2_j1 = 7;
+   public final static int right_f2_j2 = 8;
+   public final static int right_f3_j0 = 9;
+   public final static int right_f3_j1 = 10;
+   public final static int right_f3_j2 = 11;
+
+   public final static int numberOfJointsPerHand = left_f3_j2 + 1;
+}
