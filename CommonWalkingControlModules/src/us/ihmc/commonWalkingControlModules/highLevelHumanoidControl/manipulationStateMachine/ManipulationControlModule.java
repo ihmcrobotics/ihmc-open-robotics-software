fec4d7855ea0ec15604a46ada91f74283a0a0d8e
Twan Koolen <tkoolen@ihmc.us>
2013-05-13 18:17:58 +0000

    Added ManipulableToroidUpdater to ManipulationControlModule.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/manipulationStateMachine/ManipulationControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/manipulationStateMachine/ManipulationControlModule.java
@@ -7,8 +7,10 @@ import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParam
 import us.ihmc.commonWalkingControlModules.controllers.HandControllerInterface;
 import us.ihmc.commonWalkingControlModules.dynamics.FullRobotModel;
 import us.ihmc.commonWalkingControlModules.momentumBasedController.MomentumBasedController;
+import us.ihmc.commonWalkingControlModules.sensors.ManipulableToroidUpdater;
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.robotSide.SideDependentList;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.screwTheory.GeometricJacobian;
 import us.ihmc.utilities.screwTheory.OneDoFJoint;
 import us.ihmc.utilities.screwTheory.RigidBody;
@@ -27,6 +29,7 @@ public class ManipulationControlModule
    private final SideDependentList<IndividualHandControlStateMachine> individualHandControlStateMachines =
       new SideDependentList<IndividualHandControlStateMachine>();
    private final SideDependentList<GeometricJacobian> jacobians = new SideDependentList<GeometricJacobian>();
+   private final ManipulableToroidUpdater manipulableToroidUpdater;
 
    public ManipulationControlModule(DoubleYoVariable yoTime, FullRobotModel fullRobotModel, TwistCalculator twistCalculator,
                                     WalkingControllerParameters walkingControllerParameters, DesiredHandPoseProvider handPoseProvider,
@@ -34,15 +37,28 @@ public class ManipulationControlModule
                                     SideDependentList<HandControllerInterface> handControllers, MomentumBasedController momentumBasedController,
                                     YoVariableRegistry parentRegistry)
    {
+      double controlDT = momentumBasedController.getControlDT();
+      double gravityZ = momentumBasedController.getGravityZ();
+      SideDependentList<ReferenceFrame> handPositionControlFrames = new SideDependentList<ReferenceFrame>();
+
       for (RobotSide robotSide : RobotSide.values())
       {
          HandControllerInterface handControllerInterface = null;
          if (handControllers != null)
+         {
             handControllerInterface = handControllers.get(robotSide);
+         }
+
+         RigidBody endEffector = fullRobotModel.getHand(robotSide);
+
+         String frameName = endEffector.getName() + "PositionControlFrame";
+         final ReferenceFrame frameAfterJoint = endEffector.getParentJoint().getFrameAfterJoint();
+         ReferenceFrame handPositionControlFrame = ReferenceFrame.constructBodyFrameWithUnchangingTransformToParent(frameName, frameAfterJoint,
+               walkingControllerParameters.getHandControlFramesWithRespectToFrameAfterWrist().get(robotSide));
+         handPositionControlFrames.put(robotSide, handPositionControlFrame);
 
          // TODO: create manipulationControlParameters, have current walking parameters class implement it
 
-         RigidBody endEffector = fullRobotModel.getHand(robotSide);
          GeometricJacobian jacobian = new GeometricJacobian(fullRobotModel.getChest(), endEffector, endEffector.getBodyFixedFrame());
          jacobians.put(robotSide, jacobian);
 
@@ -51,10 +67,13 @@ public class ManipulationControlModule
          Map<OneDoFJoint, Double> maxTaskSpacePositions = walkingControllerParameters.getMaxTaskspaceArmJointPositions(fullRobotModel, robotSide);
 
          individualHandControlStateMachines.put(robotSide,
-               new IndividualHandControlStateMachine(yoTime, robotSide, fullRobotModel, twistCalculator, walkingControllerParameters, handPoseProvider,
-                     dynamicGraphicObjectsListRegistry, handControllerInterface, momentumBasedController.getGravityZ(), momentumBasedController.getControlDT(),
-                     momentumBasedController, jacobian, defaultArmJointPositions, minTaskSpacePositions, maxTaskSpacePositions, registry));
+                 new IndividualHandControlStateMachine(yoTime, robotSide, fullRobotModel, twistCalculator, handPositionControlFrame, handPoseProvider,
+                    dynamicGraphicObjectsListRegistry, handControllerInterface, gravityZ, controlDT, momentumBasedController, jacobian,
+                    defaultArmJointPositions, minTaskSpacePositions, maxTaskSpacePositions, registry));
       }
+
+      this.manipulableToroidUpdater = new ManipulableToroidUpdater(fullRobotModel, handPositionControlFrames, dynamicGraphicObjectsListRegistry, yoTime,
+              controlDT, registry);
 
       parentRegistry.addChild(registry);
    }
@@ -74,5 +93,6 @@ public class ManipulationControlModule
          jacobians.get(robotSide).compute();
          individualHandControlStateMachines.get(robotSide).doControl();
       }
+      manipulableToroidUpdater.update();
    }
 }
