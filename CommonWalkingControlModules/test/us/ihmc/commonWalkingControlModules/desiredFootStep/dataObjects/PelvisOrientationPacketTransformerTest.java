074645431f07836efcca647d337e2fa99c6a318b
Peter Neuhaus <pneuhaus@ihmc.us>
2013-05-14 01:43:01 +0000

    Implemented all of the transformer classes and their associated junit test classes. All tests pass. I guess these junit tests should be added to bamboo?

---
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/desiredFootStep/dataObjects/PelvisOrientationPacketTransformerTest.java
@@ -0,0 +1,141 @@
+package us.ihmc.commonWalkingControlModules.desiredFootStep.dataObjects;
+
+import org.junit.Test;
+import us.ihmc.commonWalkingControlModules.controlModules.head.HeadOrientationPacket;
+import us.ihmc.commonWalkingControlModules.controlModules.head.PelvisOrientationPacket;
+import us.ihmc.utilities.RandomTools;
+import us.ihmc.utilities.math.geometry.FrameOrientation;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+import javax.media.j3d.Transform3D;
+import javax.vecmath.AxisAngle4d;
+import javax.vecmath.Point3d;
+import javax.vecmath.Quat4d;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pneuhaus
+ * Date: 5/13/13
+ * Time: 7:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PelvisOrientationPacketTransformerTest
+{
+   @Test
+   public void testPelvisOrientationPacketTransformer()
+   {
+      int numberOfTests = 10;
+      Random random = new Random(100L);
+      Transform3D transform3D;
+      for (int i = 0; i < numberOfTests; i++)
+      {
+         AxisAngle4d axisAngle = RandomTools.generateRandomRotation(random);
+         Quat4d quat = new Quat4d();
+         quat.set(axisAngle);
+
+         PelvisOrientationPacket starting = new PelvisOrientationPacket(quat, "string1", "string2");
+
+         transform3D = RandomTools.generateRandomTransform(random);
+
+         PelvisOrientationPacket ending = PelvisOrientationPacketTransformer.transformPelvisOrientationPacket(starting, transform3D);
+
+         performEqualsTestForQuat(starting, transform3D, ending);
+      }
+
+      for (int i = 0; i < numberOfTests; i++)
+      {
+         Point3d point3d = RandomTools.generateRandomPoint(random, 10.0, 10.0, 10.0);
+
+         PelvisOrientationPacket starting = new PelvisOrientationPacket(point3d, "string1", "string2");
+         transform3D = RandomTools.generateRandomTransform(random);
+         PelvisOrientationPacket ending = PelvisOrientationPacketTransformer.transformPelvisOrientationPacket(starting, transform3D);
+         performEqualsTestForPoint(starting, transform3D, ending);
+      }
+   }
+
+   private static void performEqualsTestForQuat(PelvisOrientationPacket starting, Transform3D transform3D, PelvisOrientationPacket ending)
+   {
+//    public Quat4d quaternion;
+      Quat4d startQuat = starting.getQuaternion();
+      Quat4d endQuat = ending.getQuaternion();
+      assertTrue(areOrientationsEqualWithTransform(startQuat, transform3D, endQuat));
+
+//    public Point3d point;
+      assertTrue(starting.getPoint() == null);
+      assertTrue(ending.getPoint() == null);
+
+//    public String frameName;
+      assertTrue(starting.getFrameName().equals(ending.getFrameName()));
+
+//    public String baseName;
+      assertTrue(starting.getBaseName().equals(ending.getBaseName()));
+   }
+
+   private static void performEqualsTestForPoint(PelvisOrientationPacket starting, Transform3D transform3D, PelvisOrientationPacket ending)
+   {
+//    public Quat4d quaternion;
+      Point3d startPoint = starting.getPoint();
+      Point3d endPoint = ending.getPoint();
+      double distance = getDistanceBetweenPoints(startPoint, transform3D, endPoint);
+      assertEquals("not equal", 0.0, distance, 1e-6);
+
+//    public Point3d point;
+      assertTrue(starting.getQuaternion() == null);
+      assertTrue(ending.getQuaternion() == null);
+
+//    public String frameName;
+      assertTrue(starting.getFrameName().equals(ending.getFrameName()));
+
+//    public String baseName;
+      assertTrue(starting.getBaseName().equals(ending.getBaseName()));
+   }
+
+   private static double getDistanceBetweenPoints(Point3d startingPoint, Transform3D transform3D, Point3d endPoint)
+   {
+      ReferenceFrame ending = ReferenceFrame.constructARootFrame("ending", false, true, true);
+      ReferenceFrame starting = ReferenceFrame.constructFrameWithUnchangingTransformToParent("starting", ending, transform3D, false, true, true);
+
+      FramePoint start = new FramePoint(starting, startingPoint);
+      FramePoint end = new FramePoint(ending, endPoint);
+
+      end.changeFrame(starting);
+
+      return end.distance(start);
+   }
+
+   private static boolean areOrientationsEqualWithTransform(Quat4d orientationStart, Transform3D transform3D, Quat4d orientationEnd)
+   {
+      ReferenceFrame ending = ReferenceFrame.constructARootFrame("ending", false, true, true);
+      ReferenceFrame starting = ReferenceFrame.constructFrameWithUnchangingTransformToParent("starting", ending, transform3D, false, true, true);
+
+      FrameOrientation start = new FrameOrientation(starting, orientationStart);
+      FrameOrientation end = new FrameOrientation(ending, orientationEnd);
+
+      end.changeFrame(starting);
+
+      return equalsFrameOrientation(start, end);
+   }
+
+   private static boolean equalsFrameOrientation(FrameOrientation frameOrientation1, FrameOrientation frameOrientation2)
+   {
+      // Check reference frame first
+      if (frameOrientation1.getReferenceFrame() != frameOrientation2.getReferenceFrame())
+         return false;
+
+      double[] rpyThis = frameOrientation1.getYawPitchRoll();
+      double[] rpyThat = frameOrientation2.getYawPitchRoll();
+
+      for (int i = 0; i < rpyThat.length; i++)
+      {
+         if (Math.abs(rpyThis[i] - rpyThat[i]) > 1e-6)
+            return false;
+      }
+
+      return true;
+   }
+}
