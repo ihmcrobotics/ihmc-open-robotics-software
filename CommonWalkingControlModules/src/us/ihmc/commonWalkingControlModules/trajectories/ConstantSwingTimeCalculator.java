8c17f89aee7344691f8d7e05c7f251f0dcd8d908
Jerry Pratt <jpratt@ihmc.us>
2013-05-19 05:24:43 +0000

    Put a YoVariable in ConstantSwingTimeProvider so that we can take slow steps using the SCS GUI.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/trajectories/ConstantSwingTimeCalculator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/trajectories/ConstantSwingTimeCalculator.java
@@ -1,17 +1,26 @@
 package us.ihmc.commonWalkingControlModules.trajectories;
 
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
 public class ConstantSwingTimeCalculator implements SwingTimeCalculator
 {
-   private final double swingTime;
+   private final DoubleYoVariable swingTime;
    
-   public ConstantSwingTimeCalculator(double swingTime)
+   public ConstantSwingTimeCalculator(double swingTime, YoVariableRegistry registry)
    {
-      this.swingTime = swingTime;
+      this.swingTime = new DoubleYoVariable("swingTime", registry);
+      this.swingTime.set(swingTime);
    }
    
    public double getSwingTime(double stepLength)
    {
-      return swingTime;
+      return swingTime.getDoubleValue();
+   }
+   
+   public void setSwingTime(double swingTime)
+   {
+      this.swingTime.set(swingTime);
    }
 
 }
