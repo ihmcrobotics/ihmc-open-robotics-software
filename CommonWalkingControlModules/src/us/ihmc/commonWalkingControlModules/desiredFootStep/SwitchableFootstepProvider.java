12912c2005028e5be249cb023fdd3d5b743e8712
Jerry Pratt <jpratt@ihmc.us>
2013-05-19 22:05:43 +0000

    Added SwitchableFootstepProvider for blind walking...

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredFootStep/SwitchableFootstepProvider.java
@@ -0,0 +1,70 @@
+package us.ihmc.commonWalkingControlModules.desiredFootStep;
+
+import java.util.ArrayList;
+
+import com.yobotics.simulationconstructionset.IntegerYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class SwitchableFootstepProvider implements FootstepProvider
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+
+   private final IntegerYoVariable footstepProviderIndex = new IntegerYoVariable("footstepProviderIndex", registry);
+
+   private final ArrayList<FootstepProvider> footstepProviders = new ArrayList<FootstepProvider>();
+
+   public SwitchableFootstepProvider(FootstepProvider footstepProvider, YoVariableRegistry parentRegistry)
+   {
+      footstepProviders.add(footstepProvider);
+      parentRegistry.addChild(registry);
+   }
+
+   public void addFootstepProvider(FootstepProvider footstepProvider)
+   {
+      footstepProviders.add(footstepProvider);
+   }
+
+   private FootstepProvider getFootstepProviderInUse()
+   {
+      int index = footstepProviderIndex.getIntegerValue();
+      if (index < 0)
+         index = 0;
+      if (index >= footstepProviders.size())
+         index = 0;
+
+      FootstepProvider footstepProvider = footstepProviders.get(index);
+
+      return footstepProvider;
+   }
+
+   public Footstep poll()
+   {
+      return getFootstepProviderInUse().poll();
+   }
+
+   public Footstep peek()
+   {
+      return getFootstepProviderInUse().peek();
+   }
+
+   public Footstep peekPeek()
+   {
+      return getFootstepProviderInUse().peekPeek();
+   }
+
+   public boolean isEmpty()
+   {
+      return getFootstepProviderInUse().isEmpty();
+   }
+
+   public void notifyComplete()
+   {
+      getFootstepProviderInUse().notifyComplete();
+   }
+
+   public int getNumberOfFootstepsToProvide()
+   {
+      return getFootstepProviderInUse().getNumberOfFootstepsToProvide();
+   }
+
+}
