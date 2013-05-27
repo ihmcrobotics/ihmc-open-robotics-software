42b61d234844dfcdfea658d3068ddd65d2834374
Jesper Smith <jsmith@ihmc.us>
2013-05-27 23:10:09 +0000

    Added SDF vehicle loader

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCVehicleSDFLoader.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCVehicleSDFLoader.java
@@ -1,58 +1,103 @@
 package us.ihmc.darpaRoboticsChallenge;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
+import java.net.URL;
 
 import javax.xml.bind.JAXBException;
 
-import us.ihmc.SdfLoader.JaxbSDFLoader;
+import us.ihmc.SdfLoader.GeneralizedSDFRobotModel;
+import us.ihmc.SdfLoader.SDFFullRobotModelFactory;
+import us.ihmc.SdfLoader.SDFModelVisual;
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.SdfLoader.SDFWorldLoader;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotJointMap;
+import us.ihmc.darpaRoboticsChallenge.environment.DRCWorld;
+import us.ihmc.utilities.net.TimestampProvider;
 
-import com.yobotics.simulationconstructionset.Robot;
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.util.ground.TerrainObject;
 
-public class DRCVehicleSDFLoader
+public class DRCVehicleSDFLoader extends DRCWorld
 {
-   public static JaxbSDFLoader loadDRCVehicle()
+   
+   public DRCVehicleSDFLoader()
    {
-      ArrayList<String> resourceDirectories = new ArrayList<String>();
-      Class<DRCVehicleSDFLoader> myClass = DRCVehicleSDFLoader.class;
-      
-      InputStream fileInputStream = myClass.getResourceAsStream("models/polaris_ranger_ev/model-1_3.sdf");
-      resourceDirectories.add(myClass.getResource("models/").getFile());
-      
-      
-
-      JaxbSDFLoader jaxbSDFLoader;
+      super();
+   }
+   
+   public SDFModelVisual loadDRCVehicle()
+   {
+      URL fileURL = DRCVehicleSDFLoader.class.getResource("models/GFE/models/polaris_ranger_ev/model-1_3.sdf");
       try
       {
-         jaxbSDFLoader = new JaxbSDFLoader(fileInputStream, resourceDirectories);
+         SDFWorldLoader sdfWorldLoader = new SDFWorldLoader(new File(fileURL.getFile()), resourceDirectories);
+         GeneralizedSDFRobotModel generalizedSDFRobotModel = sdfWorldLoader.getGeneralizedRobotModelAndRemoveFromWorld("polaris_ranger_ev");
+         return new SDFModelVisual(generalizedSDFRobotModel);
       }
       catch (FileNotFoundException e)
       {
-         throw new RuntimeException("Cannot find SDF file: " + e.getMessage());
+         throw new RuntimeException(e);
       }
       catch (JAXBException e)
       {
-         e.printStackTrace();
-
-         throw new RuntimeException("Invalid SDF file: " + e.getMessage());
+         throw new RuntimeException(e);
       }
-
-      return jaxbSDFLoader;
    }
    
    public static void main(String argv[]) throws FileNotFoundException, JAXBException, MalformedURLException
    {
-      String modelName = "polaris_ranger_ev";
-      JaxbSDFLoader loader = loadDRCVehicle();
-      Robot robot = loader.createRobot(modelName);
+      SimulationConstructionSet scs = new SimulationConstructionSet();
+      
+      DRCVehicleSDFLoader drcVehicleSDFLoader = new DRCVehicleSDFLoader();
+      scs.addStaticLinkGraphics(drcVehicleSDFLoader.loadDRCVehicle());
       
       
-      SimulationConstructionSet scs = new SimulationConstructionSet(robot);
       Thread thread = new Thread(scs);
       thread.start();
+   }
+
+   public TerrainObject getTerrainObject()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public SDFRobot getRobot()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public SDFFullRobotModelFactory getFullRobotModelFactory()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public DRCRobotJointMap getJointMap()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   public double getSimulateDT()
+   {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+
+   public boolean simulateDelay()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   public TimestampProvider getTimeStampProvider()
+   {
+      // TODO Auto-generated method stub
+      return null;
    }
    
 }
