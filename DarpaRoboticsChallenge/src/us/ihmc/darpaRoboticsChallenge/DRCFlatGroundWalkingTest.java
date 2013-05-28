c76541dc93e77dffbb28508dbf05d0df82930ca6
Johannes Englsberger <jenglsberger@ihmc.us>
2013-05-28 17:34:04 +0000

    set Always_Show_Gui to false again (mistakenly checked in as true)

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCFlatGroundWalkingTest.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCFlatGroundWalkingTest.java
@@ -34,7 +34,7 @@ import com.yobotics.simulationconstructionset.util.simulationTesting.NothingChan
 
 public class DRCFlatGroundWalkingTest
 {
-   private static final boolean ALWAYS_SHOW_GUI = !false;
+   private static final boolean ALWAYS_SHOW_GUI = false;
    private static final boolean KEEP_SCS_UP = false;
 
    private static final boolean CREATE_MOVIE = BambooTools.doMovieCreation();
