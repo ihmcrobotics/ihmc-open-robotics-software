60061b9c34e79a9a4d5b84d9520cae4e36e0ca30
Twan Koolen <tkoolen@ihmc.us>
2013-05-29 02:38:27 +0000

    * stubbed out DrivingInterface. * now using ManipulationControlModule in DrivingHighLevelHumanoidController.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/driving/DrivingInterface.java
@@ -0,0 +1,25 @@
+package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.driving;
+
+/**
+ * @author twan
+ *         Date: 5/28/13
+ */
+
+// TODO: add callbacks
+public interface DrivingInterface
+{
+   public abstract void turnSteeringWheel(double angle);
+
+   public abstract void pressGasPedal(double angle);
+
+   public abstract void pressBrakePedal(double angle);
+
+   public abstract void setParkingBrake(boolean engaged);
+
+   public abstract void setGear(GearName gearName);
+
+   enum GearName
+   {
+      FORWARD, REVERSE
+   }
+}
