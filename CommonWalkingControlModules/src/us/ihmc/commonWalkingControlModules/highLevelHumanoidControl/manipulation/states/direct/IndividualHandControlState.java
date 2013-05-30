fcba83c49a88a7d5da35f6dbe6bc48c001088b61
Twan Koolen <tkoolen@ihmc.us>
2013-05-30 05:17:10 +0000

    Degenerified IndividualHandControlStates again... Added stub for DrivingManipulationState

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/manipulation/states/direct/IndividualHandControlState.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/manipulation/states/direct/IndividualHandControlState.java
@@ -4,5 +4,5 @@ package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.manipulatio
  * @author twan
  *         Date: 5/9/13
  */
-public enum DirectIndividualHandControlState
+public enum IndividualHandControlState
 {DEFAULT, SINGULARITY_ESCAPE, OBJECT_MANIPULATION, LOAD_BEARING_CYLINDRICAL, LOAD_BEARING_PLANE, OBJECT_MANIPULATION_CHEST, TWO_HANDED_TOROID_MANIPULATION}
