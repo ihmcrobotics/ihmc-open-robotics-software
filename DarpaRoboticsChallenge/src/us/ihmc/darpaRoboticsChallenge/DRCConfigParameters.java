d061875de2875e7f9687c3afd5f94536bca514f1
Sylvain Bertrand <sbertrand@ihmc.us>
2013-05-30 04:01:58 +0000

    Added DRCRobotMidiSliderBoardPositionManipulation to position robot using slider board.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
@@ -14,6 +14,9 @@ public class DRCConfigParameters
    
    // TODO: Temporary static variable for testing getting into the car
    public static final boolean TEST_GETTING_INTO_CAR = false; // true;
+   
+   public static final boolean USE_SLIDER_FOR_POSE_PLAYBACK = false; // true;
+   
    public static final boolean USE_SUPER_DUPER_HIGH_RESOLUTION_FOR_COMMS = false;
    
    public static final boolean USE_GAZEBO_PHYSICS = false;    // TODO: This one is needed just for FlatGroundWalkingTrack in Gazebo...
