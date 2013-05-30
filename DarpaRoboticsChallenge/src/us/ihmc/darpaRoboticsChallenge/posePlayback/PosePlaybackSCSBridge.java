696ec302bc50de239181469c5a80c95b1149d799
Jerry Pratt <jpratt@ihmc.us>
2013-05-30 05:57:21 +0000

    More PosePlayback stuff. Now can set up a pose on the slider and hit a button and it'll play it back in gazebo. Next up splining between them, saving and loading, and let people loose on getting back up.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/posePlayback/PosePlaybackSCSBridge.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/posePlayback/PosePlaybackSCSBridge.java
@@ -6,45 +6,95 @@ import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.environment.VRCTask;
 import us.ihmc.darpaRoboticsChallenge.environment.VRCTaskName;
-import us.ihmc.utilities.ThreadTools;
 
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.VariableChangedListener;
+import com.yobotics.simulationconstructionset.YoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
 public class PosePlaybackSCSBridge
 {
    private static final String ipAddress = DRCConfigParameters.CLOUD_MINION5_IP;
-   
-   public static void main(String[] args) throws IOException
+
+   private final PosePlaybackAllJointsController posePlaybackController;
+   private final PosePlaybackSender posePlaybackSender;
+
+   public PosePlaybackSCSBridge() throws IOException
    {
       YoVariableRegistry registry = new YoVariableRegistry("PlaybackPoseSCSBridge");
-      PosePlaybackAllJointsController posePlaybackController = new PosePlaybackAllJointsController(registry);
-      PosePlaybackSender posePlaybackSender = new PosePlaybackSender(posePlaybackController, ipAddress);
-      
-      posePlaybackSender.connect();
-      posePlaybackSender.waitUntilConnected();
-      
+      posePlaybackController = new PosePlaybackAllJointsController(registry);
+      posePlaybackSender = new PosePlaybackSender(posePlaybackController, ipAddress);
+
       VRCTask vrcTask = new VRCTask(VRCTaskName.ONLY_VEHICLE);
       SDFRobot sdfRobot = vrcTask.getRobot();
-      
+
       SimulationConstructionSet scs = new SimulationConstructionSet(sdfRobot);
       scs.addYoVariableRegistry(registry);
-      
-      
+
+
       DRCRobotMidiSliderBoardPositionManipulation sliderBoard = new DRCRobotMidiSliderBoardPositionManipulation(scs);
 
-      
+      CaptureSnapshotListener listener = new CaptureSnapshotListener(sdfRobot);
+      sliderBoard.addIsCaptureSnapshotListener(listener);
+
       scs.startOnAThread();
-      
-      while(true)
+
+      try
       {
+         posePlaybackSender.connect();
+         posePlaybackSender.waitUntilConnected();
+      }
+      catch (Exception e)
+      {
+         System.err.println("Didn't connect to posePlaybackSender!");
+      }
+
+//    while (true)
+//    {
+//       PosePlaybackRobotPose pose = new PosePlaybackRobotPose(sdfRobot);
+//       posePlaybackController.setPlaybackPose(pose);
+//
+//       posePlaybackSender.writeData();
+//
+//       ThreadTools.sleep(1000);
+//    }
+   }
+
+   private class CaptureSnapshotListener implements VariableChangedListener
+   {
+      private final SDFRobot sdfRobot;
+
+      public CaptureSnapshotListener(SDFRobot sdfRobot)
+      {
+         this.sdfRobot = sdfRobot;
+      }
+
+      public void variableChanged(YoVariable yoVariable)
+      {
+//         BooleanYoVariable captureSnapshot = (BooleanYoVariable) yoVariable;
+         //         if (captureSnapshot.getBooleanValue())
+         //         {
          PosePlaybackRobotPose pose = new PosePlaybackRobotPose(sdfRobot);
+         System.out.println(pose);
+
          posePlaybackController.setPlaybackPose(pose);
 
-         posePlaybackSender.writeData();
-         
-         ThreadTools.sleep(1000);
+         try
+         {
+            if (posePlaybackSender.isConnected())
+               posePlaybackSender.writeData();
+         }
+         catch (IOException e)
+         {
+         }
       }
+
+   }
+
+
+   public static void main(String[] args) throws IOException
+   {
+      new PosePlaybackSCSBridge();
    }
 
 }
