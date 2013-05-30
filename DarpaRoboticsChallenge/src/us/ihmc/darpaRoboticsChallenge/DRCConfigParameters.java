e41bfbe21995ab2839f4379215afe24ec9d99fb4
Jerry Pratt <jpratt@ihmc.us>
2013-05-30 04:59:19 +0000

    Got slider board working with Sylvain. Can now move joints in Gazebo using slider board in SCS. Next up: Record a pose, then do smooth playback.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
@@ -15,7 +15,7 @@ public class DRCConfigParameters
    // TODO: Temporary static variable for testing getting into the car
    public static final boolean TEST_GETTING_INTO_CAR = false; // true;
    
-   public static final boolean USE_SLIDER_FOR_POSE_PLAYBACK = false; // true;
+   public static final boolean USE_SLIDER_FOR_POSE_PLAYBACK = true; // true;
    
    public static final boolean USE_SUPER_DUPER_HIGH_RESOLUTION_FOR_COMMS = false;
    
