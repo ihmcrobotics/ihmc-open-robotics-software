34e6ecd2ccdb8a8aba99fd990f921f6040f79ad5
Twan Koolen <tkoolen@ihmc.us>
2013-05-30 00:11:25 +0000

    Made VehicleModelObjectVisualizer usable in driving controller.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/driving/VehicleModelObjectVisualizer.java
@@ -0,0 +1,54 @@
+package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.driving;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicObject;
+import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicObjectsList;
+import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicObjectsListRegistry;
+import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicReferenceFrame;
+import us.ihmc.utilities.FormattingTools;
+import us.ihmc.utilities.math.geometry.FramePose;
+import us.ihmc.utilities.math.geometry.PoseReferenceFrame;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+
+public class VehicleModelObjectVisualizer
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+   private final DynamicGraphicObjectsList dynamicGraphicObjectsList;
+   private final double scale = 0.1;
+
+   public VehicleModelObjectVisualizer(ReferenceFrame vehicleFrame, VehicleModelObjects vehicleModelObjects,
+                                DynamicGraphicObjectsListRegistry dynamicGraphicObjectsListRegistry, YoVariableRegistry parentRegistry)
+   {
+      dynamicGraphicObjectsList = new DynamicGraphicObjectsList("vehicleObjects");
+
+      for (VehicleObject vehicleObject : VehicleObject.values())
+      {
+
+         FramePose framePose = vehicleModelObjects.getFramePose(vehicleFrame, vehicleObject);
+
+         String objectName = FormattingTools.underscoredToCamelCase(vehicleObject.toString(), false);
+         ReferenceFrame objectFrame = new PoseReferenceFrame(objectName, framePose);
+         objectFrame.update();
+         DynamicGraphicReferenceFrame dynamicGraphicReferenceFrame = new DynamicGraphicReferenceFrame(objectFrame, registry, scale);
+
+         dynamicGraphicObjectsList.add(dynamicGraphicReferenceFrame);
+      }
+
+      dynamicGraphicObjectsListRegistry.registerDynamicGraphicObjectsList(dynamicGraphicObjectsList);
+      parentRegistry.addChild(registry);
+   }
+
+   public void update()
+   {
+      for (DynamicGraphicObject dynamicGraphicObject : dynamicGraphicObjectsList.getDynamicGraphicObjects())
+      {
+         dynamicGraphicObject.update();
+      }
+   }
+
+   public void setVisible(boolean visible)
+   {
+      dynamicGraphicObjectsList.setVisible(visible);
+   }
+}
