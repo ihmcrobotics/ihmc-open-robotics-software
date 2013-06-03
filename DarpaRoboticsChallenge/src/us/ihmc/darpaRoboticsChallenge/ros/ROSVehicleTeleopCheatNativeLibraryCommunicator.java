351aaf3b2d11d108e07dcaf25480c64acf21af16
Douglas Stephen <dstephen@ihmc.us>
2013-06-03 13:18:37 +0000

    Continuing to stub out the native vehicle tele-op stuff.  Almost ready to gen headers, committing so I can switch over to Ubuntu.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/ROSVehicleTeleopCheatNativeLibraryCommunicator.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/ROSVehicleTeleopCheatNativeLibraryCommunicator.java
@@ -1,12 +1,58 @@
 package us.ihmc.darpaRoboticsChallenge.ros;
 
+import us.ihmc.darpaRoboticsChallenge.networkProcessor.ros.RosTools;
+
+import java.net.InetAddress;
+import java.nio.*;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ROSVehicleTeleopCheatNativeLibraryCommunicator
 {
    private static ROSVehicleTeleopCheatNativeLibraryCommunicator instance = null;
    private static String rosMasterURI;
 
+   /*
+    * Subscribers
+    */
+   private final DoubleBuffer vehiclePoseBuffer;
+   private final ByteBuffer leftEyeImageBuffer, rightEyeImageBuffer;
+   private final LongBuffer clockBuffer;
+   private double steeringWheelState, handBrakeState, gasPedalState, brakePedalState;
+
+   /*
+    * Publishers
+    */
+   private byte directionCommandBuffer;
+   private double steeringWheelCommandBuffer, handBrakeCommandBuffer, gasPedalCommandBuffer, brakePedalCommandBuffer;
+   private final DoubleBuffer atlasTeleportPoseCommandBuffer;
+
+   /*
+    * TODO: Make classes for messages
+    *
+    * PoseMessage (vehiclePose, atlasPose)
+    * ImageMessage
+    * ClockMessage
+    * Float64Message
+    * Int8Message
+    *
+    */
+
+   private final ReentrantLock lock =  new ReentrantLock();
+
    private ROSVehicleTeleopCheatNativeLibraryCommunicator(String rosMasterURI)
    {
+      InetAddress myIP = RosTools.getMyIP(rosMasterURI);
+
+      System.loadLibrary("ROSVehicleTeleopCheatNativeLibraryCommunicator");
+      if (!register(rosMasterURI, myIP.getHostAddress()))
+      {
+         throw new RuntimeException("Cannot load native library");
+      }
+      vehiclePoseBuffer = null;
+      leftEyeImageBuffer = null;
+      rightEyeImageBuffer = null;
+      clockBuffer = null;
+      atlasTeleportPoseCommandBuffer = null;
    }
 
    public static ROSVehicleTeleopCheatNativeLibraryCommunicator getInstance(String rosMasterURI)
@@ -23,4 +69,37 @@ public class ROSVehicleTeleopCheatNativeLibraryCommunicator
 
       return instance;
    }
+
+   private static DoubleBuffer setupDoubleBuffer(ByteBuffer buffer)
+   {
+      buffer.order(ByteOrder.nativeOrder());
+
+      return buffer.asDoubleBuffer();
+   }
+
+   private static ByteBuffer setUpByteBuffer(ByteBuffer buffer)
+   {
+      buffer.order(ByteOrder.nativeOrder());
+
+      return buffer;
+   }
+
+   private static LongBuffer setUpLongBuffer(ByteBuffer buffer)
+   {
+      buffer.order(ByteOrder.nativeOrder());
+
+      return buffer.asLongBuffer();
+   }
+
+   private static FloatBuffer setupFloatBuffer(ByteBuffer buffer)
+   {
+      buffer.order(ByteOrder.nativeOrder());
+
+      return  buffer.asFloatBuffer();
+   }
+
+   private native boolean register(String rosMasterURI, String myIP);
+
+   private native void spin();
+
 }
