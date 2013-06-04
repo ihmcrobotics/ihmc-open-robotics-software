f9c9597bb84b476d3fd5f8654f979e500a3077a1
Twan Koolen <tkoolen@ihmc.us>
2013-06-04 22:29:07 +0000

    * got rid of DrivingHighLevelHumanoidControllerFactory. driving controller is now being created in PolyFact. * changed default hand positions for driving task so that robot doesn't push against seat.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotDrivingControllerParameters.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotDrivingControllerParameters.java
@@ -1,8 +1,32 @@
 package us.ihmc.darpaRoboticsChallenge;
 
 
+import us.ihmc.commonWalkingControlModules.dynamics.FullRobotModel;
+import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmJointName;
+import us.ihmc.robotSide.RobotSide;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class DRCRobotDrivingControllerParameters extends DRCRobotWalkingControllerParameters
 {
+   @Override
+   public Map<OneDoFJoint, Double> getDefaultArmJointPositions(FullRobotModel fullRobotModel, RobotSide robotSide)
+   {
+      Map<OneDoFJoint, Double> jointPositions = new LinkedHashMap<OneDoFJoint, Double>();
+
+      jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.SHOULDER_ROLL), robotSide.negateIfRightSide(-1.36));
+      jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.SHOULDER_PITCH), 0.34);
+      jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.ELBOW_ROLL), robotSide.negateIfRightSide(1.5));
+      jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.ELBOW_PITCH), 1.94);
+      jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.WRIST_PITCH), -0.19);
+      jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.WRIST_ROLL), robotSide.negateIfRightSide(-0.07));
+
+      return jointPositions;
+   }
+
+
    @Override
    public String[] getDefaultHeadOrientationControlJointNames()
    {
