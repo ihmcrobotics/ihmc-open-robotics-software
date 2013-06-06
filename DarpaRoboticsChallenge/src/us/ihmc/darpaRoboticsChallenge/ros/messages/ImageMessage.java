2b6bf8eefec59557267ccb4bc764fcb990c53e42
Douglas Stephen <dstephen@ihmc.us>
2013-06-06 16:07:28 +0000

    Vehicle teleop stuff compiles.  Committing.  Need to run tests to make sure it actually works..

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/messages/ImageMessage.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/messages/ImageMessage.java
@@ -6,18 +6,23 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.nio.ByteBuffer;
 
-public class CompressedImageMessage
+public class ImageMessage
 {
+   int height;
+   int width;
+   int step;
    private byte[] imageData;
 
    public void setFromBuffer(ByteBuffer buffer, int size)
    {
-      imageData = new byte[size];
+      height = buffer.getInt();
+      width = buffer.getInt();
+      step = buffer.getInt();
       buffer.get(this.imageData, 0, size);
    }
 
    public void packBufferedImage(BufferedImage image, ColorModel colorModel)
    {
-      image = RosTools.bufferedImageFromByteArrayJpeg(colorModel, this.imageData);
+      image = RosTools.bufferedImageFromByteArrayJpeg(colorModel, this.imageData, width, height);
    }
 }
