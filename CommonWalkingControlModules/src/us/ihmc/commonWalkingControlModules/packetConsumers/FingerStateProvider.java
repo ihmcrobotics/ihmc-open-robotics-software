076a38712974d2c4d4db406a0d6257ba43ea0fe2
Matt Johnson <mjohnson@ihmc.us>
2013-06-09 02:55:58 +0000

    Moved FingerState to CommonWalking so it can be used in packets Renamed FingerState back to HandState so things will work while FingerState serialization is incomplete

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/FingerStateProvider.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/FingerStateProvider.java
@@ -1,24 +1,24 @@
 package us.ihmc.commonWalkingControlModules.packetConsumers;
 
-import us.ihmc.commonWalkingControlModules.packets.FingerStatePacket;
+import us.ihmc.commonWalkingControlModules.packets.HandStatePacket;
 import us.ihmc.utilities.net.ObjectConsumer;
 
 /**
  * @author twan
  *         Date: 6/7/13
  */
-public class FingerStateProvider implements ObjectConsumer<FingerStatePacket>
+public class FingerStateProvider implements ObjectConsumer<HandStatePacket>
 {
-   private FingerStatePacket packet;
+   private HandStatePacket packet;
    private boolean isNewHandStateAvailable;
 
-   public synchronized void consumeObject(FingerStatePacket packet)
+   public synchronized void consumeObject(HandStatePacket packet)
    {
       this.packet = packet;
       isNewHandStateAvailable = true;
    }
 
-   public synchronized FingerStatePacket getPacket()
+   public synchronized HandStatePacket getPacket()
    {
       isNewHandStateAvailable = false;
       return packet;
