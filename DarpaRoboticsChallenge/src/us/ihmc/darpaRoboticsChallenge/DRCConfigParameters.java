88a3203aa1a240563683644ae8b61f844d513b64
Douglas Stephen <dstephen@ihmc.us>
2013-06-11 18:09:16 +0000

    Added a new networking related field to DRCConfigParameters for the Network Processor.  Up to this point, Network Processor and SCS were the same thing.  This will not necessarily be the case soon.  This commit doesn't change functionality, it just gets the working copy of this file to be clean on my machine so I'm not constantly merging stuff in while waiting for constellation testing to end to commit functionality changes.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
@@ -69,8 +69,9 @@ public class DRCConfigParameters
    public static final String CONSTELLATION_FIELD_COMPUTER_1_VPN_IP = "10.0.0.52";
    public static final String CONSTELLATION_FIELD_COMPUTER_2_VPN_IP = "10.0.0.53";
 
-   public static final String GAZEBO_HOST = CLOUD_MINION3_IP;
-   public static final String SCS_MACHINE_IP_ADDRESS = LOCALHOST;    // CLOUD_MONSTER_IP;
+   public static final String GAZEBO_HOST = LOCALHOST; //CONSTELLATION_SIMULATOR_COMPUTER_VPN_IP;
+   public static final String SCS_MACHINE_IP_ADDRESS = LOCALHOST; //CONSTELLATION_FIELD_COMPUTER_2_VPN_IP;    // CLOUD_MONSTER_IP;
+   public static final String NET_PROC_MACHINE_IP_ADDRESS = LOCALHOST; //CONSTELLATION_FIELD_COMPUTER_1_VPN_IP;    // SCS_MACHINE_IP_ADDRESS;
 
    public static final String OPERATOR_INTERFACE_IP_ADDRESS = LOCALHOST;
 
@@ -101,11 +102,7 @@ public class DRCConfigParameters
    public static final boolean STREAM_POLAR_LIDAR = true;
    public static final int LIDAR_UPDATE_RATE_OVERRIDE = 5;
    public static final int LIDAR_SWEEPS_PER_SCAN = 1;
-   public static final int LIDAR_POINTS_PER_SWEEP = 640;    // I assume this
-
-   // number will never
-   // be 720 in SCS
-   // -Gray.
+   public static final int LIDAR_POINTS_PER_SWEEP = 640; //I assume this number will never be 720 in SCS -Gray.
    public static final boolean OVERRIDE_DRC_LIDAR_CONFIG = true;
    public static final float LIDAR_MIN_DISTANCE = 0.2f;
    public static final float LIDAR_MAX_DISTANCE = 10.0f;
