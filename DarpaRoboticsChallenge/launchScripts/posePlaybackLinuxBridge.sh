66e0e32022fd529327d95dc184b3a1d97a4c061a
Nathan Mertins <nmertins@ihmc.us>
2013-06-11 21:39:47 +0000

    Updated the build process and launch scripts.  Build process now runs using parallel tasks for remote connections so it doesn't take 7 minutes.

--- DarpaRoboticsChallenge/launchScripts/posePlaybackLinuxBridge.sh
+++ DarpaRoboticsChallenge/launchScripts/posePlaybackLinuxBridge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source getClasspath.sh
+source getControllerClasspath.sh
 java -classpath "${CLASSPATH}" -Djava.library.path="${MYDIR}/lib"  us.ihmc.darpaRoboticsChallenge.posePlayback.PosePlaybackLinuxBridge --headless --host http://localhost:11311 $@
