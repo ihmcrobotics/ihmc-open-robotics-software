c911c8bab9f8e65debb0c504463666b5c3ceb0fe
Operator Computer <OperatorComputer@unknown>
2013-06-14 15:49:52 +0000

    updated for cloud stuff.

--- DarpaRoboticsChallenge/launchScripts/runControllerG1.sh
+++ DarpaRoboticsChallenge/launchScripts/runControllerG1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+source /home/ubuntu/cloudsim/ros.bash
+
+cd /home/ubuntu/vrc
+
 source getClasspath.sh
+
 java -Xms10240m -Xmx10240m -XX:+UseG1GC -XX:MaxGCPauseMillis=3 -XX:InitiatingHeapOccupancyPercent=65 -XX:ParallelGCThreads=8 -XX:ConcGCThreads=8 -XX:+AggressiveOpts -classpath "${CLASSPATH}" -Djava.library.path="${MYDIR}/lib" us.ihmc.darpaRoboticsChallenge.gazebo.AtlasROSControllerFactory --headless --host "$1" "${@:2}"
