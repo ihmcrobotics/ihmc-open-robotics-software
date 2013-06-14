8a081f8fc639e460a19e1d06433da1409e40d959
Operator Computer <OperatorComputer@unknown>
2013-06-14 15:50:02 +0000

    updated for cloud stuff.

--- DarpaRoboticsChallenge/launchScripts/runNetworkProcessor.sh
+++ DarpaRoboticsChallenge/launchScripts/runNetworkProcessor.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+source /home/ubuntu/cloudsim/ros.bash
+
+cd /home/ubuntu/vrc
+
 source getClasspath.sh
+
 java -classpath "${CLASSPATH}" -Djava.library.path="${MYDIR}/lib" us.ihmc.darpaRoboticsChallenge.networkProcessor.DRCNetworkProcessorEnterpriseCloudDispatcherBackend -h --ros-uri $1 --scs-ip $2
