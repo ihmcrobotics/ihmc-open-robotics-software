9d85b82bf5717e23630a993b6cfa066a50e2d18b
Sylvain Bertrand <sbertrand@ihmc.us>
2013-06-16 22:13:01 +0000

    Changed the initial setup for having the left grabbing the bar more forward.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/initialSetup/VRCTask1InVehicleHovering.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/initialSetup/VRCTask1InVehicleHovering.java
@@ -20,56 +20,56 @@ public class VRCTask1InVehicleHovering implements RobotInitialSetup<SDFRobot>
    public void initializeRobot(SDFRobot robot)
    {
       // Avoid singularities at startup
-      robot.getOneDoFJoint("l_arm_usy").setQ(-1.3363);
-      robot.getOneDoFJoint("l_arm_shx").setQ(-0.751);
-      robot.getOneDoFJoint("l_arm_ely").setQ(0.6108);
-      robot.getOneDoFJoint("l_arm_elx").setQ(2.0035);
-      robot.getOneDoFJoint("l_arm_uwy").setQ(-0.1013);
-      robot.getOneDoFJoint("l_arm_mwx").setQ(0.5174);
+      robot.getOneDoFJoint("l_arm_usy").setQ(-1.0518);
+      robot.getOneDoFJoint("l_arm_shx").setQ(-0.4177);
+      robot.getOneDoFJoint("l_arm_ely").setQ(0.8856);
+      robot.getOneDoFJoint("l_arm_elx").setQ(1.616);
+      robot.getOneDoFJoint("l_arm_uwy").setQ(-0.4238);
+      robot.getOneDoFJoint("l_arm_mwx").setQ(0.9266);
       
-      robot.getOneDoFJoint("r_arm_usy").setQ(-0.2486);
-      robot.getOneDoFJoint("r_arm_shx").setQ(1.1101);
-      robot.getOneDoFJoint("r_arm_ely").setQ(1.6088);
-      robot.getOneDoFJoint("r_arm_elx").setQ(-1.1552);
-      robot.getOneDoFJoint("r_arm_uwy").setQ(-0.5442);
-      robot.getOneDoFJoint("r_arm_mwx").setQ(-0.8353);
+      robot.getOneDoFJoint("r_arm_usy").setQ(-0.297);
+      robot.getOneDoFJoint("r_arm_shx").setQ(1.1054);
+      robot.getOneDoFJoint("r_arm_ely").setQ(1.5975);
+      robot.getOneDoFJoint("r_arm_elx").setQ(-1.1719);
+      robot.getOneDoFJoint("r_arm_uwy").setQ(-0.5188);
+      robot.getOneDoFJoint("r_arm_mwx").setQ(-0.8051);
       
-      robot.getOneDoFJoint("l_leg_uhz").setQ(0.0111);
-      robot.getOneDoFJoint("l_leg_mhx").setQ(-0.0207);
-      robot.getOneDoFJoint("l_leg_lhy").setQ(-1.1163);
-      robot.getOneDoFJoint("l_leg_kny").setQ(1.2062);
-      robot.getOneDoFJoint("l_leg_uay").setQ(0.0659);
-      robot.getOneDoFJoint("l_leg_lax").setQ(0.0126);
+      robot.getOneDoFJoint("l_leg_uhz").setQ(0.0405);
+      robot.getOneDoFJoint("l_leg_mhx").setQ(0.0214);
+      robot.getOneDoFJoint("l_leg_lhy").setQ(-1.1232);
+      robot.getOneDoFJoint("l_leg_kny").setQ(1.2086);
+      robot.getOneDoFJoint("l_leg_uay").setQ(0.0724);
+      robot.getOneDoFJoint("l_leg_lax").setQ(-0.0005);
 
-      robot.getOneDoFJoint("r_leg_uhz").setQ(-0.0804);
-      robot.getOneDoFJoint("r_leg_mhx").setQ(-0.1971);
-      robot.getOneDoFJoint("r_leg_lhy").setQ(-1.2435);
-      robot.getOneDoFJoint("r_leg_kny").setQ(1.3467);
-      robot.getOneDoFJoint("r_leg_uay").setQ(0.0605);
-      robot.getOneDoFJoint("r_leg_lax").setQ(0.1517);
+      robot.getOneDoFJoint("r_leg_uhz").setQ(-0.1527);
+      robot.getOneDoFJoint("r_leg_mhx").setQ(-0.1526);
+      robot.getOneDoFJoint("r_leg_lhy").setQ(-1.2337);
+      robot.getOneDoFJoint("r_leg_kny").setQ(1.3334);
+      robot.getOneDoFJoint("r_leg_uay").setQ(0.048);
+      robot.getOneDoFJoint("r_leg_lax").setQ(0.1072);
 
-      robot.getOneDoFJoint("back_lbz").setQ(-0.0739);
-      robot.getOneDoFJoint("back_mby").setQ(0.1544);
-      robot.getOneDoFJoint("back_ubx").setQ(-0.0588);
+      robot.getOneDoFJoint("back_lbz").setQ(-0.1519);
+      robot.getOneDoFJoint("back_mby").setQ(0.164);
+      robot.getOneDoFJoint("back_ubx").setQ(-0.0049);
 
-      robot.getOneDoFJoint("left_f0_j0").setQ(0.1233);
-      robot.getOneDoFJoint("left_f1_j0").setQ(0.1018);
-      robot.getOneDoFJoint("left_f2_j0").setQ(0.0894);
-      robot.getOneDoFJoint("left_f3_j0").setQ(0.0381);
+      robot.getOneDoFJoint("left_f0_j0").setQ(0.1098);
+      robot.getOneDoFJoint("left_f1_j0").setQ(0.1060);
+      robot.getOneDoFJoint("left_f2_j0").setQ(0.1040);
+      robot.getOneDoFJoint("left_f3_j0").setQ(-0.0376);
 
-      robot.getOneDoFJoint("left_f0_j1").setQ(1.1508);
-      robot.getOneDoFJoint("left_f1_j1").setQ(1.1687);
-      robot.getOneDoFJoint("left_f2_j1").setQ(1.1756);
-      robot.getOneDoFJoint("left_f3_j1").setQ(1.0582);
+      robot.getOneDoFJoint("left_f0_j1").setQ(1.1659);
+      robot.getOneDoFJoint("left_f1_j1").setQ(1.1627);
+      robot.getOneDoFJoint("left_f2_j1").setQ(1.1598);
+      robot.getOneDoFJoint("left_f3_j1").setQ(1.0352);
       
-      robot.getOneDoFJoint("left_f0_j2").setQ(1.0354);
-      robot.getOneDoFJoint("left_f1_j2").setQ(0.9934);
-      robot.getOneDoFJoint("left_f2_j2").setQ(0.9725);
-      robot.getOneDoFJoint("left_f3_j2").setQ(-0.0363);
+      robot.getOneDoFJoint("left_f0_j2").setQ(1.0046);
+      robot.getOneDoFJoint("left_f1_j2").setQ(1.0054);
+      robot.getOneDoFJoint("left_f2_j2").setQ(1.0060);
+      robot.getOneDoFJoint("left_f3_j2").setQ(0.0231);
 
       robot.getOneDoFJoint("right_f3_j1").setQ(-1.57);
 
-      robot.setPositionInWorld(new Vector3d(-0.0955, 0.3763, 1.1063));
-      robot.setOrientation(new Quat4d(0.0091, -0.0797, 0.053, 0.9954));
+      robot.setPositionInWorld(new Vector3d(-0.096, 0.3726, 1.1064));
+      robot.setOrientation(new Quat4d(-0.0076, -0.0814, 0.0747, 0.9938));
    }
 }
