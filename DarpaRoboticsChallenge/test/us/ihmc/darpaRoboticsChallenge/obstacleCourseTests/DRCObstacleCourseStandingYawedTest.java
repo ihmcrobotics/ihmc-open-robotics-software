a0f3aae0cfc1e14c87df8f09e918d1566c2a0213
Maarten Griffioen <mgriffioen@ihmc.us>
2013-09-11 18:10:04 +0000

    New test for checking orientation problems when have perfect imu.

---
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/obstacleCourseTests/DRCObstacleCourseStandingYawedTest.java
@@ -0,0 +1,91 @@
+package us.ihmc.darpaRoboticsChallenge.obstacleCourseTests;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.vecmath.Point3d;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.bambooTools.BambooTools;
+import us.ihmc.darpaRoboticsChallenge.DRCDemo01StartingLocation;
+import us.ihmc.darpaRoboticsChallenge.DRCEnvironmentModel;
+import us.ihmc.darpaRoboticsChallenge.testTools.DRCSimulationTestHelper;
+import us.ihmc.darpaRoboticsChallenge.testTools.ScriptedFootstepGenerator;
+import us.ihmc.utilities.MemoryTools;
+import us.ihmc.utilities.ThreadTools;
+
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+
+   public class DRCObstacleCourseStandingYawedTest
+   {
+      private static final boolean KEEP_SCS_UP = true;
+
+      private static final boolean createMovie = BambooTools.doMovieCreation();
+      private static final boolean checkNothingChanged = BambooTools.getCheckNothingChanged();
+
+      private DRCSimulationTestHelper drcSimulationTestHelper;
+
+      @Before
+      public void showMemoryUsageBeforeTest()
+      {
+         MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
+      }
+
+      @After
+      public void destroySimulationAndRecycleMemory()
+      {
+         if (KEEP_SCS_UP)
+         {
+            ThreadTools.sleepForever();
+         }
+
+         // Do this here in case a test fails. That way the memory will be recycled.
+         if (drcSimulationTestHelper != null)
+         {
+            drcSimulationTestHelper.destroySimulation();
+            drcSimulationTestHelper = null;
+         }
+
+         MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
+      }
+
+     
+      
+      @Test
+      public void testStandingYawed() throws SimulationExceededMaximumTimeException
+      {
+         BambooTools.reportTestStartedMessage();
+
+         DRCDemo01StartingLocation selectedLocation = DRCDemo01StartingLocation.ROCKS;
+         DRCEnvironmentModel selectedEnvironment = DRCEnvironmentModel.OBSTACLE_COURSE;
+         drcSimulationTestHelper = new DRCSimulationTestHelper("DRCWalkingOntoRocksTest", selectedLocation, selectedEnvironment, checkNothingChanged, createMovie);
+
+         SimulationConstructionSet simulationConstructionSet = drcSimulationTestHelper.getSimulationConstructionSet();
+         ScriptedFootstepGenerator scriptedFootstepGenerator = drcSimulationTestHelper.createScriptedFootstepGenerator();
+
+         setupCameraForWalkingOntoRocks(simulationConstructionSet);
+
+         ThreadTools.sleep(1000);
+         boolean success = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(2.0);
+         
+         drcSimulationTestHelper.createMovie(simulationConstructionSet, 1);
+         drcSimulationTestHelper.checkNothingChanged();
+
+         assertTrue(success);
+         
+         BambooTools.reportTestFinishedMessage();
+      }
+      
+      
+      private void setupCameraForWalkingOntoRocks(SimulationConstructionSet scs)
+      {
+         Point3d cameraFix = new Point3d(0.1, 3.2, 0.5);
+         Point3d cameraPosition = new Point3d(-2.8, 4.8, 1.5);
+
+         drcSimulationTestHelper.setupCameraForUnitTest(scs, cameraFix, cameraPosition);
+      }
+
+   }
