4926966900d69a5c4205d39035e744646fb1b454
Sylvain Bertrand <sbertrand@ihmc.us>
2013-09-16 21:49:42 +0000

    Wrote a few test cases for ControlFlowPorts

---
+++ SensorProcessing/test/us/ihmc/sensorProcessing/controlFlowPorts/YoFramePointControlFlowOutputPortTest.java
@@ -0,0 +1,50 @@
+package us.ihmc.sensorProcessing.controlFlowPorts;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import javax.vecmath.Vector3d;
+
+import org.junit.Test;
+
+import us.ihmc.controlFlow.ControlFlowElement;
+import us.ihmc.controlFlow.NullControlFlowElement;
+import us.ihmc.utilities.RandomTools;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class YoFramePointControlFlowOutputPortTest
+{
+
+   private static final double EPS = 1e-17;
+
+   @Test
+   public void simpleWritingReadingTest()
+   {
+      ControlFlowElement controlFlowElement = new NullControlFlowElement();
+      
+      String namePrefix = "test";
+      ReferenceFrame frame = ReferenceFrame.getWorldFrame();
+      YoVariableRegistry registry = new YoVariableRegistry("blop");
+      
+      YoFramePointControlFlowOutputPort controlFlowOutputPort = new YoFramePointControlFlowOutputPort(controlFlowElement, namePrefix, frame, registry);
+
+      assertEquals(namePrefix, controlFlowOutputPort.getName());
+      
+      Random rand = new Random(1567);
+      
+      for (int i = 0; i < 1000; i++)
+      {
+         Vector3d vector = RandomTools.generateRandomVector(rand, RandomTools.generateRandomDouble(rand, Double.MIN_VALUE, Double.MAX_VALUE));
+         FramePoint dataIn = new FramePoint(frame, vector);
+         controlFlowOutputPort.setData(dataIn);
+         FramePoint dataOut = controlFlowOutputPort.getData();
+
+         assertTrue("Expected: " + dataIn + ", but was: " + dataOut, dataIn.epsilonEquals(dataOut, EPS));
+      }
+   }
+
+}
