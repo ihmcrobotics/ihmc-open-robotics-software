c2ce28d760e522d78ae7d201105ed7e1c2bbbac6
Sylvain Bertrand <sbertrand@ihmc.us>
2013-09-16 21:49:42 +0000

    Wrote a few test cases for ControlFlowPorts

---
+++ SensorProcessing/test/us/ihmc/sensorProcessing/controlFlowPorts/YoFrameQuaternionControlFlowOutputPortTest.java
@@ -0,0 +1,48 @@
+package us.ihmc.sensorProcessing.controlFlowPorts;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import us.ihmc.controlFlow.ControlFlowElement;
+import us.ihmc.controlFlow.NullControlFlowElement;
+import us.ihmc.utilities.RandomTools;
+import us.ihmc.utilities.math.geometry.FrameOrientation;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class YoFrameQuaternionControlFlowOutputPortTest
+{
+
+   private static final double EPS = 1e-15;
+
+   @Test
+   public void simpleWritingReadingTest()
+   {
+      ControlFlowElement controlFlowElement = new NullControlFlowElement();
+      
+      String namePrefix = "test";
+      ReferenceFrame frame = ReferenceFrame.getWorldFrame();
+      YoVariableRegistry registry = new YoVariableRegistry("blop");
+      
+      YoFrameQuaternionControlFlowOutputPort controlFlowOutputPort = new YoFrameQuaternionControlFlowOutputPort(controlFlowElement, namePrefix, frame, registry);
+
+      assertEquals(namePrefix, controlFlowOutputPort.getName());
+      
+      Random rand = new Random(1567);
+      
+      for (int i = 0; i < 1000; i++)
+      {
+         FrameOrientation dataIn = new FrameOrientation(frame, RandomTools.generateRandomQuaternion(rand));
+         controlFlowOutputPort.setData(dataIn);
+         FrameOrientation dataOut = controlFlowOutputPort.getData();
+
+         assertTrue("Expected: " + dataIn + ", but was: " + dataOut, dataIn.getQuaternion().epsilonEquals(dataOut.getQuaternion(), EPS));
+      }
+   }
+
+}
---
+++ SensorProcessing/test/us/ihmc/sensorProcessing/controlFlowPorts/YoFrameVectorControlFlowOutputPortTest.java
@@ -0,0 +1,51 @@
+package us.ihmc.sensorProcessing.controlFlowPorts;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import javax.vecmath.Vector3d;
+
+import org.junit.Test;
+
+import us.ihmc.controlFlow.ControlFlowElement;
+import us.ihmc.controlFlow.NullControlFlowElement;
+import us.ihmc.utilities.RandomTools;
+import us.ihmc.utilities.math.geometry.FrameVector;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class YoFrameVectorControlFlowOutputPortTest
+{
+
+   private static final double EPS = 1e-17;
+
+   @Test
+   public void simpleWritingReadingTest()
+   {
+      ControlFlowElement controlFlowElement = new NullControlFlowElement();
+      
+      String namePrefix = "test";
+      ReferenceFrame frame = ReferenceFrame.getWorldFrame();
+      YoVariableRegistry registry = new YoVariableRegistry("blop");
+      
+      YoFrameVectorControlFlowOutputPort controlFlowOutputPort = new YoFrameVectorControlFlowOutputPort(controlFlowElement, namePrefix, frame, registry);
+
+      assertEquals(namePrefix, controlFlowOutputPort.getName());
+      
+      Random rand = new Random(1567);
+      
+      for (int i = 0; i < 1000; i++)
+      {
+         Vector3d vector = RandomTools.generateRandomVector(rand, RandomTools.generateRandomDouble(rand, Double.MIN_VALUE, Double.MAX_VALUE));
+         FrameVector dataIn = new FrameVector(frame, vector);
+         controlFlowOutputPort.setData(dataIn);
+         FrameVector dataOut = controlFlowOutputPort.getData();
+
+         assertTrue("Expected: " + dataIn + ", but was: " + dataOut, dataIn.epsilonEquals(dataOut, EPS));
+      }
+   }
+
+}
