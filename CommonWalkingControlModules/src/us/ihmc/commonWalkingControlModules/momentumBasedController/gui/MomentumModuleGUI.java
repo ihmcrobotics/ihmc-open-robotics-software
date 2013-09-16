04b254a21b6f16e9246b7bb8a56d5f63aac73257
Maarten Griffioen <mgriffioen@ihmc.us>
2013-09-16 23:30:52 +0000

    Always on top, and deleted some imports

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/gui/MomentumModuleGUI.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/gui/MomentumModuleGUI.java
@@ -1,7 +1,6 @@
 package us.ihmc.commonWalkingControlModules.momentumBasedController.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.util.ArrayList;
@@ -22,9 +21,9 @@ public class MomentumModuleGUI
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
 
    private JFrame jFrame;
-   private final DesiredJointAccelerationMultipleJPanel desiredJointAccelerationMultipleJPanel;  
-   private final DesiredSpatialAccelerationMultipleJPanel desiredSpatialAccelerationMultipleJPanel;  
-   private final JointAccelerationSolutionJPanel jointAccelerationSolutionJPanel;  
+   private final DesiredJointAccelerationMultipleJPanel desiredJointAccelerationMultipleJPanel;
+   private final DesiredSpatialAccelerationMultipleJPanel desiredSpatialAccelerationMultipleJPanel;
+   private final JointAccelerationSolutionJPanel jointAccelerationSolutionJPanel;
 
    public MomentumModuleGUI(YoVariableRegistry parentRegistry)
    {
@@ -32,66 +31,72 @@ public class MomentumModuleGUI
       desiredJointAccelerationMultipleJPanel = new DesiredJointAccelerationMultipleJPanel();
       desiredSpatialAccelerationMultipleJPanel = new DesiredSpatialAccelerationMultipleJPanel();
       jointAccelerationSolutionJPanel = new JointAccelerationSolutionJPanel();
-      
+
       jFrame.getContentPane().add(desiredJointAccelerationMultipleJPanel, BorderLayout.NORTH);
       jFrame.getContentPane().add(desiredSpatialAccelerationMultipleJPanel, BorderLayout.CENTER);
       jFrame.getContentPane().add(jointAccelerationSolutionJPanel, BorderLayout.SOUTH);
-      
+
       jFrame.setSize(1400, 942);
       jFrame.setVisible(true);
       jFrame.setLocationRelativeTo(null);
       jFrame.setResizable(false);
+      jFrame.setAlwaysOnTop(true);
+
       int showOnScreenNumber = 1;
       showOnScreen(showOnScreenNumber, jFrame);
-      
+
       parentRegistry.addChild(registry);
    }
 
-//   public void setDesiredsAndSolution(MomentumModuleDataObject momentumModuleDataObject, MomentumModuleSolution momentumModuleSolution)
-//   {
-//      ArrayList<DesiredJointAccelerationCommand> desiredJointAccelerationCommands = momentumModuleDataObject.getDesiredJointAccelerationCommands();
-//      ArrayList<DesiredSpatialAccelerationCommand> desiredSpatialAccelerationCommands = momentumModuleDataObject.getDesiredSpatialAccelerationCommands();
-//      
-//      desiredJointAccelerationMultipleJPanel.setDesiredJointAccelerations(desiredJointAccelerationCommands);
-//      desiredSpatialAccelerationMultipleJPanel.setDesiredSpatialAccelerations(desiredSpatialAccelerationCommands);
-//   }
-   
+   //   public void setDesiredsAndSolution(MomentumModuleDataObject momentumModuleDataObject, MomentumModuleSolution momentumModuleSolution)
+   //   {
+   //      ArrayList<DesiredJointAccelerationCommand> desiredJointAccelerationCommands = momentumModuleDataObject.getDesiredJointAccelerationCommands();
+   //      ArrayList<DesiredSpatialAccelerationCommand> desiredSpatialAccelerationCommands = momentumModuleDataObject.getDesiredSpatialAccelerationCommands();
+   //      
+   //      desiredJointAccelerationMultipleJPanel.setDesiredJointAccelerations(desiredJointAccelerationCommands);
+   //      desiredSpatialAccelerationMultipleJPanel.setDesiredSpatialAccelerations(desiredSpatialAccelerationCommands);
+   //   }
+
    public void update(AllMomentumModuleListener allMomentumModuleListener)
    {
-      ArrayList<DesiredJointAccelerationCommandAndMotionConstraint> desiredJointAccelerationCommandAndMotionConstraints = allMomentumModuleListener.getDesiredJointAccelerationCommandAndMotionConstraints();
+      ArrayList<DesiredJointAccelerationCommandAndMotionConstraint> desiredJointAccelerationCommandAndMotionConstraints = allMomentumModuleListener
+            .getDesiredJointAccelerationCommandAndMotionConstraints();
       desiredJointAccelerationMultipleJPanel.setDesiredJointAccelerations(desiredJointAccelerationCommandAndMotionConstraints);
 
-      ArrayList<DesiredSpatialAccelerationCommandAndMotionConstraint> desiredSpatialAccelerationCommandAndMotionConstraints = allMomentumModuleListener.getDesiredSpatialAccelerationCommandAndMotionConstraints();
+      ArrayList<DesiredSpatialAccelerationCommandAndMotionConstraint> desiredSpatialAccelerationCommandAndMotionConstraints = allMomentumModuleListener
+            .getDesiredSpatialAccelerationCommandAndMotionConstraints();
       desiredSpatialAccelerationMultipleJPanel.setDesiredSpatialAccelerations(desiredSpatialAccelerationCommandAndMotionConstraints);
-   
-      
+
       InverseDynamicsJoint[] jointsToOptimizeFor = allMomentumModuleListener.getJointsToOptimizeFor();
       DenseMatrix64F jointAccelerationsSolution = allMomentumModuleListener.getJointAccelerationsSolution();
       jointAccelerationSolutionJPanel.setJointAccelerationSolution(jointsToOptimizeFor, jointAccelerationsSolution);
-      System.out.println(" frame height = " + jFrame.getHeight());
    }
 
    public void reset()
    {
-      // TODO Auto-generated method stub
-      
+
    }
 
    public void initialize()
    {
-      // TODO Auto-generated method stub
-      
+
    }
-   
-   public static void showOnScreen( int screen, JFrame frame ) {
+
+   public static void showOnScreen(int screen, JFrame frame)
+   {
       GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
       GraphicsDevice[] gd = ge.getScreenDevices();
-      if( screen > -1 && screen < gd.length ) {
-          frame.setLocation(gd[screen].getDefaultConfiguration().getBounds().x + 10, frame.getY());
-      } else if( gd.length > 0 ) {
-          frame.setLocation(gd[0].getDefaultConfiguration().getBounds().x + 10, frame.getY());
-      } else {
-          throw new RuntimeException( "No Screens Found" );
+      if (screen > -1 && screen < gd.length)
+      {
+         frame.setLocation(gd[screen].getDefaultConfiguration().getBounds().x + 10, frame.getY());
       }
-  }
+      else if (gd.length > 0)
+      {
+         frame.setLocation(gd[0].getDefaultConfiguration().getBounds().x + 10, frame.getY());
+      }
+      else
+      {
+         throw new RuntimeException("No Screens Found");
+      }
+   }
 }
