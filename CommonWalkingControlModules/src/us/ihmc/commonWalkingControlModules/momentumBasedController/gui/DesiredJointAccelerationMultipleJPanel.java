15d5e5ed3d0d3f04e5f4365ce0f74a7f1f61359e
Maarten Griffioen <mgriffioen@ihmc.us>
2013-09-16 16:30:44 +0000

    Display the data in table's instead of using graphics.drawString methods

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/gui/DesiredJointAccelerationMultipleJPanel.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/gui/DesiredJointAccelerationMultipleJPanel.java
@@ -1,36 +1,64 @@
 package us.ihmc.commonWalkingControlModules.momentumBasedController.gui;
 
 import java.awt.Graphics;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import org.ejml.data.DenseMatrix64F;
+
 import us.ihmc.commonWalkingControlModules.momentumBasedController.dataObjects.DesiredJointAccelerationCommandAndMotionConstraint;
+import us.ihmc.utilities.screwTheory.InverseDynamicsJoint;
 
 public class DesiredJointAccelerationMultipleJPanel extends AbstractMultipleReusableJPanel<DesiredJointAccelerationJPanel>
 {
    private static final long serialVersionUID = -4892971095902174475L;
    
+   private DefaultTableModel model;
+   private JTable jTable;
+   private NumberFormat numberFormat;
+
+   private TableTools tableTools;
+   
    public DesiredJointAccelerationMultipleJPanel()
    {
-//    this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-//      this.setLayout(new GridLayout(39, 1));
-//      this.setLayout(new GridLayout(39, 1));
+      this.numberFormat = NumberFormat.getInstance();
+      this.numberFormat.setMaximumFractionDigits(5);
+      this.numberFormat.setMinimumFractionDigits(1);
+      this.numberFormat.setGroupingUsed(false);
+
+      tableTools = new TableTools();
+      ArrayList<String> header = new ArrayList<String>();
+      header.add("Joint Name"); header.add("Desired Joint Accaleration"); header.add("Achieved Joint Acceleration"); header.add("Error");
+      jTable = tableTools.createJTableModelForDesiredJointAcceleration(header, 14);
+      model = tableTools.getModel();
     
       this.repaint();
    }
 
    public synchronized void setDesiredJointAccelerations(ArrayList<DesiredJointAccelerationCommandAndMotionConstraint> desiredJointAccelerationCommandAndMotionConstraints)
    {
-      rearrangePanelsIfNecessary(desiredJointAccelerationCommandAndMotionConstraints.size());
 
       for (int i = 0; i < desiredJointAccelerationCommandAndMotionConstraints.size(); i++)
       {
          DesiredJointAccelerationCommandAndMotionConstraint desiredJointAccelerationCommandAndMotionConstraint = desiredJointAccelerationCommandAndMotionConstraints.get(i);
          
-         DesiredJointAccelerationJPanel desiredJointAccelerationJPanel = getJPanel(i);
+         DesiredJointAccelerationJPanel desiredJointAccelerationJPanel = new DesiredJointAccelerationJPanel();
          desiredJointAccelerationJPanel.setDesiredJointAccelerationCommand(desiredJointAccelerationCommandAndMotionConstraint);
+         writeDataToJointTable(i, desiredJointAccelerationJPanel);
       }
 
       this.repaint();
+   }
+   
+   private void writeDataToJointTable(int rowCount, DesiredJointAccelerationJPanel desiredJointAccelerationJPanel)
+   {
+      model.setValueAt(desiredJointAccelerationJPanel.getJointName(), rowCount + 1, 0);
+      model.setValueAt(desiredJointAccelerationJPanel.getDesiredAcceleration(), rowCount + 1, 1);
+      model.setValueAt(desiredJointAccelerationJPanel.getAchievedJointAcceleration(), rowCount + 1, 2);
+      model.setValueAt(desiredJointAccelerationJPanel.getErrorAcceleration(), rowCount + 1, 3);
    }
 
    public DesiredJointAccelerationJPanel constructNewJPanel()
@@ -41,6 +69,7 @@ public class DesiredJointAccelerationMultipleJPanel extends AbstractMultipleReus
    public synchronized void paintComponent(Graphics g)
    {
       super.paintComponent(g);
+      add(jTable);
    }
 
 }
