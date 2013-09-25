83e2e05e6dbd2cb0c5b010dae822e7ae49b9692b
Alex Lesman <alesman@ihmc.us>
2013-09-25 21:09:28 +0000

    Made JointConfigurationGathererAndProducer lockless using realtime data structures.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networking/dataProducers/JointConfigurationGathererAndProducer.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networking/dataProducers/JointConfigurationGathererAndProducer.java
@@ -1,46 +1,32 @@
 package us.ihmc.darpaRoboticsChallenge.networking.dataProducers;
 
 import java.util.HashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantLock;
 
 import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
 
 import us.ihmc.SdfLoader.SDFFullRobotModel;
-import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
+import us.ihmc.concurrent.Builder;
+import us.ihmc.concurrent.ConcurrentCopier;
+import us.ihmc.concurrent.ConcurrentRingBuffer;
 import us.ihmc.darpaRoboticsChallenge.handControl.FingerJoint;
 import us.ihmc.darpaRoboticsChallenge.handControl.SandiaHandModel;
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.robotSide.SideDependentList;
 import us.ihmc.utilities.ThreadTools;
-import us.ihmc.utilities.math.TimeTools;
 import us.ihmc.utilities.net.ObjectCommunicator;
 import us.ihmc.utilities.screwTheory.OneDoFJoint;
 import us.ihmc.utilities.screwTheory.SixDoFJoint;
 
-import com.yobotics.simulationconstructionset.BooleanYoVariable;
-import com.yobotics.simulationconstructionset.IntegerYoVariable;
-import com.yobotics.simulationconstructionset.LongYoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
 public class JointConfigurationGathererAndProducer
 {
+   private final int WORKER_SLEEP_TIME_MILLIS = 1;
+
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
-   private final LongYoVariable nextTimestamp = new LongYoVariable("nextTimestamp", registry);
-   private final BooleanYoVariable hasAtlasAngles = new BooleanYoVariable("hasAtlasAngles", registry);
-   private final BooleanYoVariable hasLeftHandAngles = new BooleanYoVariable("hasLeftHandAngles", registry);
-   private final BooleanYoVariable hasRightHandAngles = new BooleanYoVariable("hasRightHandAngles", registry);
-   private final SideDependentList<BooleanYoVariable> hasHandAngles = new SideDependentList<BooleanYoVariable>(hasLeftHandAngles, hasRightHandAngles);
-   private final IntegerYoVariable delay = new IntegerYoVariable("jointConfigurationDelayInMs", registry);
 
    private final ObjectCommunicator objectCommunicator;
-
-   private final ReentrantLock lock = new ReentrantLock(true);
-
-   private int totalNumberOfJoints;
-   private double[] allJointAngles;
 
    private final OneDoFJoint[] atlasJoints;
    private final SideDependentList<FingerJoint[]> handJoints = new SideDependentList<FingerJoint[]>();
@@ -49,32 +35,27 @@ public class JointConfigurationGathererAndProducer
    private final Vector3d rootTranslation = new Vector3d();
    private final Quat4d rootOrientation = new Quat4d();
 
-   private final double[] atlasJointAngles;
-   private final SideDependentList<double[]> handAngles = new SideDependentList<double[]>();
-
-   private final ExecutorService sendPool = Executors.newSingleThreadExecutor(ThreadTools
-         .getNamedThreadFactory("JointConfigurationGathererAndProducerSendPool"));
-   private final JointConfigurationWorker jointConfigurationWorker = new JointConfigurationWorker();
+   private final SideDependentList<ConcurrentCopier<double[]>> handAngles = new SideDependentList<>();
+   private final ConcurrentRingBuffer<DRCJointConfigurationData> stateRingBuffer;
 
    public JointConfigurationGathererAndProducer(ObjectCommunicator objectCommunicator, SDFFullRobotModel estimatorModel,
          SideDependentList<SandiaHandModel> handModels, YoVariableRegistry parentRegistry)
    {
 
+      parentRegistry.addChild(registry);
       this.objectCommunicator = objectCommunicator;
+      this.rootJoint = estimatorModel.getRootJoint();
 
+      // Setup Atlas Joints
       int numberOfAtlasJoints = DRCJointConfigurationData.atlasJointNames.length;
       this.atlasJoints = new OneDoFJoint[numberOfAtlasJoints];
-      this.atlasJointAngles = new double[numberOfAtlasJoints];
 
       for (int i = 0; i < numberOfAtlasJoints; ++i)
       {
          atlasJoints[i] = estimatorModel.getOneDoFJointByName(DRCJointConfigurationData.atlasJointNames[i]);
       }
 
-      this.rootJoint = estimatorModel.getRootJoint();
-
-      totalNumberOfJoints += numberOfAtlasJoints;
-
+      // Setup hand joints
       for (RobotSide robotSide : RobotSide.values)
       {
          HashMap<String, FingerJoint> jointsByName = new HashMap<String, FingerJoint>();
@@ -85,138 +66,104 @@ public class JointConfigurationGathererAndProducer
 
          String[] handNames = robotSide == RobotSide.LEFT ? DRCJointConfigurationData.leftHandNames : DRCJointConfigurationData.rightHandNames;
 
-         int numberOfHandJoints = handNames.length;
+         final int numberOfHandJoints = handNames.length;
          handJoints.put(robotSide, new FingerJoint[numberOfHandJoints]);
-         handAngles.put(robotSide, new double[numberOfHandJoints]);
          for (int i = 0; i < numberOfHandJoints; i++)
          {
             handJoints.get(robotSide)[i] = jointsByName.get(handNames[i]);
          }
 
-         totalNumberOfJoints += numberOfHandJoints;
+         final Builder<double[]> handAngleBuilder = new Builder<double[]>()
+         {
+            public double[] newInstance()
+            {
+               return new double[numberOfHandJoints];
+            }
+         };
+
+         handAngles.set(robotSide, new ConcurrentCopier<>(handAngleBuilder));
       }
 
-      allJointAngles = new double[totalNumberOfJoints];
+      // Setup ring buffer
+      Builder<DRCJointConfigurationData> jointConfigurationBuilder = new Builder<DRCJointConfigurationData>()
+      {
+         public DRCJointConfigurationData newInstance()
+         {
+            return new DRCJointConfigurationData();
+         }
+      };
+      stateRingBuffer = new ConcurrentRingBuffer<>(jointConfigurationBuilder, 8);
 
-      nextTimestamp.set(Long.MIN_VALUE);
-      hasAtlasAngles.set(false);
-      hasLeftHandAngles.set(false);
-      hasRightHandAngles.set(false);
-
-      delay.set(DRCConfigParameters.JOINT_CONFIGURATION_RATE_IN_MS);
-
-      parentRegistry.addChild(registry);
+      // Start thread
+      new Thread(new JointConfigurationWorker()).start();
    }
 
+   
+   // upon receiving new atlas joint data, pulls the latest hand states and fills a
+   // DRCJointConfigurationData object on the ConcurrentRingBuffer
    public void updateEstimatorJoints(long timestamp)
    {
-      lock.lock();
-      {
-         if (!(timestamp >= nextTimestamp.getLongValue() && !hasAtlasAngles.getBooleanValue()))
-         {
-            lock.unlock();
-            return;
-         }
-      }
-      lock.unlock();
+      rootJoint.packTranslation(rootTranslation);
+      rootJoint.packRotation(rootOrientation);
 
+      DRCJointConfigurationData jointConfigurationData = stateRingBuffer.next();
+      if (jointConfigurationData == null)
+      {
+         return;
+      }
+
+      double[] jointAngles = jointConfigurationData.getJointAngles();
       for (int i = 0; i < atlasJoints.length; i++)
       {
-         atlasJointAngles[i] = atlasJoints[i].getQ();
+         jointAngles[i] = atlasJoints[i].getQ();
       }
 
-      lock.lock();
+      double[] leftHandAngles = handAngles.get(RobotSide.LEFT).getCopyForReading();
+      double[] rightHandAngles = handAngles.get(RobotSide.LEFT).getCopyForReading();
+      if (leftHandAngles != null && rightHandAngles != null)
       {
-         hasAtlasAngles.set(true);
-         rootJoint.packTranslation(rootTranslation);
-         rootJoint.packRotation(rootOrientation);
-         System.arraycopy(atlasJointAngles, 0, allJointAngles, 0, atlasJointAngles.length);
-         sendIfNeeded(timestamp);
+         System.arraycopy(leftHandAngles, 0, jointAngles, atlasJoints.length, leftHandAngles.length);
+         System.arraycopy(rightHandAngles, 0, jointAngles, atlasJoints.length + leftHandAngles.length, rightHandAngles.length);
       }
-      lock.unlock();
 
+      jointConfigurationData.setRootTranslation(rootTranslation);
+      jointConfigurationData.setRootOrientation(rootOrientation);
+      jointConfigurationData.setSimTime(timestamp);
+
+      stateRingBuffer.commit();
    }
 
-   private void sendIfNeeded(long timestamp)
-   {
-      if (hasAtlasAngles.getBooleanValue() && hasLeftHandAngles.getBooleanValue() && hasRightHandAngles.getBooleanValue())
-      {
-         nextTimestamp.set(timestamp + TimeTools.milliSecondsToNanoSeconds(delay.getIntegerValue()));
-         sendPool.execute(jointConfigurationWorker);
-      }
-   }
-
+   // uses ConcurrentCopier to keep the latest hand joint state for each hand
    public void updateHandJoints(RobotSide robotSide, long timestamp)
    {
-      lock.lock();
-      {
-         if (!(timestamp >= nextTimestamp.getLongValue() && !hasHandAngles.get(robotSide).getBooleanValue()))
-         {
-            lock.unlock();
-            return;
-         }
-      }
-      lock.unlock();
-
       final FingerJoint[] sideHandJoints = handJoints.get(robotSide);
-      final double[] sideHandAngles = handAngles.get(robotSide);
+      final double[] sideHandAngles = handAngles.get(robotSide).getCopyForWriting();
       for (int i = 0; i < sideHandJoints.length; i++)
       {
          sideHandAngles[i] = sideHandJoints[i].getQ();
       }
-
-      int offset;
-      if (robotSide == RobotSide.LEFT)
-      {
-         offset = atlasJointAngles.length;
-      }
-      else
-      {
-         offset = atlasJointAngles.length + handAngles.get(RobotSide.LEFT).length;
-      }
-      lock.lock();
-      {
-         hasHandAngles.get(robotSide).set(true);
-         System.arraycopy(sideHandAngles, 0, allJointAngles, offset, sideHandAngles.length);
-         sendIfNeeded(timestamp);
-      }
-      lock.unlock();
-
+      handAngles.get(robotSide).commit();
    }
 
+   // feeds the RingBuffer into the objectCommunicator
    private class JointConfigurationWorker implements Runnable
    {
-      private final DRCJointConfigurationData jointConfigurationData = new DRCJointConfigurationData();
-      
       public void run()
       {
-         try
+         while (true)
          {
-            lock.lock();
-            jointConfigurationData.setJointAngles(allJointAngles);
-            jointConfigurationData.setRootTranslation(rootTranslation);
-            jointConfigurationData.setRootOrientation(rootOrientation);
-            jointConfigurationData.setSimTime(nextTimestamp.getLongValue() - TimeTools.milliSecondsToNanoSeconds(delay.getIntegerValue()));
-            lock.unlock();
-   
-            objectCommunicator.consumeObject(jointConfigurationData);
-              
-            lock.lock();
+            if (stateRingBuffer.poll())
             {
-               jointConfigurationData.setJointAngles(allJointAngles);
-               for (RobotSide robotSide : RobotSide.values)
+               DRCJointConfigurationData state;
+               while ((state = stateRingBuffer.read()) != null)
                {
-                  hasHandAngles.get(robotSide).set(false);
-                  hasAtlasAngles.set(false);
+                  objectCommunicator.consumeObject(state);
                }
+               stateRingBuffer.flush();
             }
-            lock.unlock();
-         }
-         catch(Exception e)
-         {
-            e.printStackTrace();
+
+            ThreadTools.sleep(WORKER_SLEEP_TIME_MILLIS);
          }
       }
-
    }
 }
