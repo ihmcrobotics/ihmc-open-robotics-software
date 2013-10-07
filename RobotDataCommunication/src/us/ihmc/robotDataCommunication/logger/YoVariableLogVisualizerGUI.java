adc0be3e82651b99fffdb0fe29c011c6f8338ad2
John Carff <jcarff@ihmc.us>
2013-10-07 19:52:43 +0000

    relocated the playback slider under the viewport window.

--- RobotDataCommunication/src/us/ihmc/robotDataCommunication/logger/YoVariableLogVisualizerGUI.java
+++ RobotDataCommunication/src/us/ihmc/robotDataCommunication/logger/YoVariableLogVisualizerGUI.java
@@ -1,13 +1,16 @@
 package us.ihmc.robotDataCommunication.logger;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
-import javax.swing.JFrame;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -18,10 +21,10 @@ import com.yobotics.simulationconstructionset.UnreasonableAccelerationException;
 import com.yobotics.simulationconstructionset.VariableChangedListener;
 import com.yobotics.simulationconstructionset.YoVariable;
 
-public class YoVariableLogVisualizerGUI extends JFrame
+public class YoVariableLogVisualizerGUI extends JPanel
 {
    private static final long serialVersionUID = 6414551517161321596L;
-   
+
    private final VideoDataPlayer player;
    private final YoVariableLogPlaybackRobot robot;
    private final SimulationConstructionSet scs;
@@ -29,35 +32,31 @@ public class YoVariableLogVisualizerGUI extends JFrame
    public YoVariableLogVisualizerGUI(VideoDataPlayer player, YoVariableLogPlaybackRobot robot, SimulationConstructionSet scs)
    {
       super();
-      
+
       this.player = player;
       this.robot = robot;
       this.scs = scs;
-      
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setTitle("YoVariable Log playback");
-      setSize(600, 100);
-      
-      setLocationByPlatform(true);
-      setLocationRelativeTo(null);
-      
-      getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-      
+
+
+
+      setLayout(new GridLayout(1, 2));
+
       addGUIElements();
-      
+
       setVisible(true);
    }
-   
+
    private void switchVideoUpdate(boolean update)
    {
-       player.updateVideo(update);
+      player.updateVideo(update);
    }
 
    private void seek(int newValue)
    {
-      if(!scs.isSimulating())
+      if (!scs.isSimulating())
       {
          robot.seek(newValue);
+
          try
          {
             scs.simulateOneRecordStepNow();
@@ -66,56 +65,62 @@ public class YoVariableLogVisualizerGUI extends JFrame
          {
             e.printStackTrace();
          }
+
          player.indexChanged(0, 0);
       }
    }
-   
+
    private void addGUIElements()
    {
       final JCheckBox showVideo = new JCheckBox("Update video", true);
       showVideo.addActionListener(new ActionListener()
       {
-         
          @Override
          public void actionPerformed(ActionEvent e)
          {
             switchVideoUpdate(showVideo.isSelected());
          }
       });
-      
-      getContentPane().add(showVideo);
-      
+      add(showVideo);
+
+      showVideo.setHorizontalAlignment(JLabel.CENTER);
+
+//    timePanel.add(new JSeparator(JSeparator.VERTICAL));
+
       JPanel timePanel = new JPanel();
-      timePanel.setLayout(new BoxLayout(timePanel, BoxLayout.X_AXIS));
-      timePanel.add(new JLabel("Position: "));
-      timePanel.add(new JLabel(String.valueOf(robot.getInitialTimestamp())));
+      timePanel.setLayout(new BorderLayout());
+      timePanel.add(new JLabel("Position: "), BorderLayout.WEST);
+
+//    timePanel.add(new JLabel(String.valueOf(robot.getInitialTimestamp())));
       final JSlider slider = new JSlider(0, robot.getNumberOfEntries(), 0);
+
+      final JLabel currentTime = new JLabel(String.valueOf(robot.getFinalTimestamp()));
+
       slider.addChangeListener(new ChangeListener()
       {
-         
          @Override
          public void stateChanged(ChangeEvent e)
          {
+            currentTime.setText(slider.getValue() + "");
             seek(slider.getValue());
          }
       });
-      
+
       robot.addCurrentRecordTickListener(new VariableChangedListener()
       {
-         
          @Override
          public void variableChanged(YoVariable v)
          {
-            if(scs.isSimulating())
+            if (scs.isSimulating())
             {
                slider.setValue((int) ((LongYoVariable) v).getLongValue());
             }
          }
       });
-      timePanel.add(slider);
-      timePanel.add(new JLabel(String.valueOf(robot.getFinalTimestamp())));
-      getContentPane().add(timePanel);
+      timePanel.add(slider, BorderLayout.CENTER);
+      timePanel.add(currentTime, BorderLayout.EAST);
+      add(timePanel);
    }
 
-   
+
 }
