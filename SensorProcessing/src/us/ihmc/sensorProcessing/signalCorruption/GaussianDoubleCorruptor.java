7b3bb8f162dad2a1aada81cf089d2add7537817f
Jerry Pratt <jpratt@ihmc.us>
2013-10-11 01:00:14 +0000

    GaussianDoubleCorruptor for adding noise to a sensor.

---
+++ SensorProcessing/src/us/ihmc/sensorProcessing/signalCorruption/GaussianDoubleCorruptor.java
@@ -0,0 +1,36 @@
+package us.ihmc.sensorProcessing.signalCorruption;
+
+import java.util.Random;
+
+import org.apache.commons.lang.mutable.MutableDouble;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+public class GaussianDoubleCorruptor implements SignalCorruptor<MutableDouble>
+{
+   private final YoVariableRegistry registry;
+   private final Random random;
+   private final DoubleYoVariable standardDeviation;
+
+   public GaussianDoubleCorruptor(long seed, String namePrefix, YoVariableRegistry parentRegistry)
+   {
+      this.random = new Random(seed);
+      this.registry = new YoVariableRegistry(namePrefix + getClass().getSimpleName());
+      this.standardDeviation = new DoubleYoVariable(namePrefix + "StdDev", parentRegistry);
+
+      parentRegistry.addChild(registry);
+   }
+
+   public void corrupt(MutableDouble signal)
+   {
+      double std = standardDeviation.getDoubleValue();
+      double noise = std * random.nextGaussian();
+      signal.add(noise);
+   }
+
+   public void setStandardDeviation(double standardDeviation)
+   {
+      this.standardDeviation.set(standardDeviation);
+   }
+}
