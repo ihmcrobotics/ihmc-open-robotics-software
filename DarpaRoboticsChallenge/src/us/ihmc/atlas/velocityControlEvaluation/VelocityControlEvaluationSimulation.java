1222c1e559d64c68d5e7568fb4cd03b7402ec8f5
Jerry Pratt <jpratt@ihmc.us>
2013-10-21 17:30:43 +0000

    Simple example sim that demonstrates that integrating the acceleration can be helpful.

---
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/velocityControlEvaluation/VelocityControlEvaluationSimulation.java
@@ -0,0 +1,32 @@
+package us.ihmc.atlas.velocityControlEvaluation;
+
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+
+public class VelocityControlEvaluationSimulation
+{
+   
+   public VelocityControlEvaluationSimulation()
+   {
+      double simDT = 0.001;
+      double controlDT = 0.006;
+      int ticksPerControl = (int) Math.round(controlDT/simDT);
+      controlDT = simDT * ticksPerControl;
+      
+      System.out.println("controlDT = " + controlDT);
+      
+      VelocityControlEvaluationRobot robot = new VelocityControlEvaluationRobot();
+      VelocityControlEvaluationController controller = new VelocityControlEvaluationController(robot, controlDT);
+      robot.setController(controller, ticksPerControl);
+      
+      SimulationConstructionSet scs = new SimulationConstructionSet(robot, 32768);
+      scs.setDT(simDT, 1);
+      scs.setSimulateDuration(1.5);
+      
+      scs.startOnAThread();
+   }
+   
+   public static void main(String[] args)
+   {
+      new VelocityControlEvaluationSimulation();
+   }
+}
