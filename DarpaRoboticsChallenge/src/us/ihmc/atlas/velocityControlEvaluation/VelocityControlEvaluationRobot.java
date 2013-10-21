229d5737a3cfd88154f67c03c8672529422ba524
Jerry Pratt <jpratt@ihmc.us>
2013-10-21 17:30:43 +0000

    Simple example sim that demonstrates that integrating the acceleration can be helpful.

---
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/velocityControlEvaluation/VelocityControlEvaluationRobot.java
@@ -0,0 +1,60 @@
+package us.ihmc.atlas.velocityControlEvaluation;
+
+import javax.vecmath.Vector3d;
+
+import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
+import us.ihmc.utilities.Axis;
+
+import com.yobotics.simulationconstructionset.ExternalForcePoint;
+import com.yobotics.simulationconstructionset.Link;
+import com.yobotics.simulationconstructionset.Robot;
+import com.yobotics.simulationconstructionset.SliderJoint;
+
+public class VelocityControlEvaluationRobot extends Robot
+{
+   private static final long serialVersionUID = -2717723504360444704L;
+   public static final double MASS = 10.0;
+   public static final double STICTION = 60.0; 
+   
+   private final SliderJoint rootJoint;
+
+   public VelocityControlEvaluationRobot()
+   {
+      super("VelocityControlEvaluationRobot");
+      
+      rootJoint = new SliderJoint("x", new Vector3d(), this, Axis.X);
+      rootJoint.setStiction(STICTION);
+      
+      Link pointMass = new Link("pointMass");
+      pointMass.setMass(MASS);
+      pointMass.setMomentOfInertia(0.0, 0.0, 0.0);
+      pointMass.setComOffset(new Vector3d());
+      
+      Graphics3DObject linkGraphics = new Graphics3DObject();
+      linkGraphics.addSphere(0.03);
+      pointMass.setLinkGraphics(linkGraphics);
+      
+      ExternalForcePoint externalForce = new ExternalForcePoint("externalForce", this);
+      rootJoint.addExternalForcePoint(externalForce);
+      
+      rootJoint.setLink(pointMass);
+      this.addRootJoint(rootJoint);
+   }
+
+   public void setTau(double tau)
+   {
+      rootJoint.setTau(tau);
+   }
+
+   public double getX()
+   {
+      return rootJoint.getQ().getDoubleValue();
+   }
+   
+   public double getXDot()
+   {
+      return rootJoint.getQD().getDoubleValue();
+   }
+
+  
+}
