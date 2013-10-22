d454998ee0a607cb2ab89709cdf86058f0355477
Atlas Control Computer <AtlasControlComputer@unknown>
2013-10-22 23:51:30 +0000

    Put upper body gains on the slider board.

--- DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/WalkControllerSliderBoard.java
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/WalkControllerSliderBoard.java
@@ -28,8 +28,11 @@ public class WalkControllerSliderBoard
       sliderBoardConfigurationManager.setSlider(3, "kpPelvisOrientation", registry, 0.0, 100.0);
       sliderBoardConfigurationManager.setKnob  (3, "zetaPelvisOrientation", registry, 0.0, 1.0);
 
-      sliderBoardConfigurationManager.setSlider(4, "walkingHeadOrientationKp", registry, 0.0, 40.0);
-      sliderBoardConfigurationManager.setKnob  (4, "walkingHeadOrientationZeta", registry, 0.0, 1.0);
+//      sliderBoardConfigurationManager.setSlider(4, "walkingHeadOrientationKp", registry, 0.0, 40.0);
+//      sliderBoardConfigurationManager.setKnob  (4, "walkingHeadOrientationZeta", registry, 0.0, 1.0);
+      
+      sliderBoardConfigurationManager.setSlider(4, "kpUpperBody", registry, 0.0, 200.0);
+      sliderBoardConfigurationManager.setKnob  (4, "zetaUpperBody", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(5, "kpAllArmJointsL", registry, 0.0, 120.0);
       sliderBoardConfigurationManager.setKnob  (5, "zetaAllArmJointsL", registry, 0.0, 1.0);
