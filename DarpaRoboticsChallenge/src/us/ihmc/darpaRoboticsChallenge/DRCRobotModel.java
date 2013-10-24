0149264ea00f54d07a85819416fa2b931b0109bf
Atlas Control Computer <AtlasControlComputer@unknown>
2013-10-24 14:27:57 +0000

    Timing fixes, added mass model

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotModel.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotModel.java
@@ -2,7 +2,7 @@ package us.ihmc.darpaRoboticsChallenge;
 
 public enum DRCRobotModel
 {
-   ATLAS_NO_HANDS, ATLAS_IROBOT_HANDS, ATLAS_SANDIA_HANDS, ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS, V1;
+   ATLAS_NO_HANDS, ATLAS_NO_HANDS_ADDED_MASS, ATLAS_IROBOT_HANDS, ATLAS_SANDIA_HANDS, ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS, V1;
    
    public static DRCRobotModel getDefaultRobotModel()
    {
@@ -14,6 +14,7 @@ public enum DRCRobotModel
       switch(this)
       {
       case ATLAS_NO_HANDS:
+      case ATLAS_NO_HANDS_ADDED_MASS:
       case ATLAS_IROBOT_HANDS:
       case ATLAS_SANDIA_HANDS:
       case ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS:
