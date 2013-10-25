968bf859c224baa5074b589a6b2c0a6ae34154c4
Jerry Pratt <jpratt@ihmc.us>
2013-10-25 05:58:45 +0000

    Create slider board for DRCFlatGroundWalkingTrack.

--- DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/WalkControllerSliderBoard.java
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/WalkControllerSliderBoard.java
@@ -2,7 +2,10 @@ package us.ihmc.atlas.visualization;
 
 import us.ihmc.SdfLoader.GeneralizedSDFRobotModel;
 
+import com.yobotics.simulationconstructionset.EnumYoVariable;
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.VariableChangedListener;
+import com.yobotics.simulationconstructionset.YoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 import com.yobotics.simulationconstructionset.util.inputdevices.SliderBoardConfigurationManager;
 
@@ -12,13 +15,9 @@ public class WalkControllerSliderBoard
 
    public WalkControllerSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
    {
-  
-      
+      final EnumYoVariable<SliderBoardMode> sliderBoardMode = new EnumYoVariable<SliderBoardMode>("sliderBoardMode", registry, SliderBoardMode.class);
       final SliderBoardConfigurationManager sliderBoardConfigurationManager = new SliderBoardConfigurationManager(scs);
       
-      
-
-
       sliderBoardConfigurationManager.setSlider(1, "captureKpParallel", registry, 0.0, 2.0);
       sliderBoardConfigurationManager.setKnob  (1, "captureKpOrthogonal", registry, 0.0, 2.0);
 
@@ -42,14 +41,54 @@ public class WalkControllerSliderBoard
 
       sliderBoardConfigurationManager.setSlider(7, "ll_transitionRatio", registry,  0.0, 1.0);
       sliderBoardConfigurationManager.setSlider(8, "offsetHeightAboveGround", registry, 0.0, 0.12);
+      
+      sliderBoardConfigurationManager.setKnob  (8, "sliderBoardMode", registry, 0.0, SliderBoardMode.values().length);
 
+      sliderBoardConfigurationManager.saveConfiguration(SliderBoardMode.WalkingGains.toString());
+      sliderBoardConfigurationManager.clearControls();
+      
+      sliderBoardConfigurationManager.setSlider(1, "offsetHeightAboveGround", registry, 0.0, 0.12);
+
+      sliderBoardConfigurationManager.setSlider(2, "desiredICPX", registry, -0.05, 0.05);
+      sliderBoardConfigurationManager.setKnob  (2, "desiredICPY", registry, -0.2, 0.2);
+      
+      sliderBoardConfigurationManager.setSlider(3, "userDesiredPelvisYaw", registry, -0.05, 0.05);
+      sliderBoardConfigurationManager.setKnob  (3, "userSetDesiredPelvis", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setSlider(4, "userDesiredPelvisPitch", registry, -0.05, 0.05);
+      sliderBoardConfigurationManager.setSlider(5, "userDesiredPelvisRoll", registry, -0.05, 0.05);
+      
+      sliderBoardConfigurationManager.setSlider(6, "userDesiredHeadPitch", registry, -0.05, 0.05);
+      sliderBoardConfigurationManager.setSlider(7, "userDesiredHeadYaw", registry, -0.05, 0.05);
+
+      sliderBoardConfigurationManager.setKnob  (8, "sliderBoardMode", registry, 0.0, SliderBoardMode.values().length);
+
+      sliderBoardConfigurationManager.saveConfiguration(SliderBoardMode.WalkingDesireds.toString());
+      sliderBoardConfigurationManager.clearControls();
+
+      
+      sliderBoardMode.set(SliderBoardMode.WalkingGains);
+      
+      sliderBoardMode.addVariableChangedListener(new VariableChangedListener()
+      {
+         public void variableChanged(YoVariable v)
+         {
+            sliderBoardConfigurationManager.loadConfiguration(sliderBoardMode.getEnumValue().toString());
+         }
+      });
+      
+   }
+   
+   private enum SliderBoardMode
+   {
+      WalkingGains, WalkingDesireds;
    }
    
    private static final SliderBoardFactory factory = new SliderBoardFactory() {
    
       @Override
-      public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel) {
-            new WalkControllerSliderBoard( scs,  registry,  generalizedSDFRobotModel);
+      public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel) 
+      {
+         new WalkControllerSliderBoard( scs,  registry,  generalizedSDFRobotModel);
       }
    };
    
