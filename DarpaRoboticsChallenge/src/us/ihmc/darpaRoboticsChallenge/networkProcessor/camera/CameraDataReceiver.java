76109569d6c3479892c5037321faa5fcf5535850
Douglas Stephen <dstephen@ihmc.us>
2013-11-13 21:28:57 +0000

    Another update to Camera stuff.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networkProcessor/camera/CameraDataReceiver.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networkProcessor/camera/CameraDataReceiver.java
@@ -9,6 +9,7 @@ import javax.vecmath.Point3d;
 import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
 
+import geometry_msgs.Transform;
 import us.ihmc.atlas.PPSTimestampOffsetProvider;
 import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.driving.DRCStereoListener;
@@ -27,11 +28,10 @@ import us.ihmc.utilities.net.ObjectConsumer;
 
 public abstract class CameraDataReceiver
 {
-   
    private final RobotPoseBuffer robotPoseBuffer;
    private final CompressedVideoDataServer compressedVideoDataServer;
    private final ArrayList<DRCStereoListener> stereoListeners = new ArrayList<DRCStereoListener>();
-   
+
    private final Point3d cameraPosition = new Point3d();
    private final Quat4d cameraOrientation = new Quat4d();
    private final Vector3d tempVector = new Vector3d();
@@ -39,50 +39,49 @@ public abstract class CameraDataReceiver
    private final PPSTimestampOffsetProvider ppsTimestampOffsetProvider;
    private final ROSNativeTransformTools rosTransformProvider;
    private Transform3D rosTransformFromHeadBaseToCamera = new Transform3D();
-   
+
    private final Transform3D cameraPose;
-   
-   public CameraDataReceiver(RobotPoseBuffer robotPoseBuffer, VideoSettings videoSettings, final DRCNetworkProcessorNetworkingManager networkingManager, PPSTimestampOffsetProvider ppsTimestampOffsetProvider)
+
+   public CameraDataReceiver(RobotPoseBuffer robotPoseBuffer, VideoSettings videoSettings, final DRCNetworkProcessorNetworkingManager networkingManager,
+                             PPSTimestampOffsetProvider ppsTimestampOffsetProvider)
    {
       this.robotPoseBuffer = robotPoseBuffer;
       this.ppsTimestampOffsetProvider = ppsTimestampOffsetProvider;
       this.cameraPose = new Transform3D();
       final DRCNetworkProcessorControllerStateHandler controllerStateHandler = networkingManager.getControllerStateHandler();
-      
+
       ObjectCommunicator videoConsumer = new ObjectCommunicator()
       {
-         
          @Override
          public void consumeObject(Object object)
          {
-            if(object instanceof VideoPacket)
+            if (object instanceof VideoPacket)
             {
                controllerStateHandler.sendVideoPacket((VideoPacket) object);
             }
          }
-         
+
          @Override
          public boolean isConnected()
          {
             return networkingManager.isConnected();
          }
-         
+
          @Override
          public void close()
          {
             networkingManager.close();
          }
-         
+
          @Override
          public void attachStateListener(NetStateListener stateListener)
          {
             networkingManager.attachStateListener(stateListener);
          }
-         
+
          @Override
          public <T> void attachListener(Class<T> clazz, ObjectConsumer<T> listener)
          {
-            
          }
 
          @Override
@@ -90,88 +89,100 @@ public abstract class CameraDataReceiver
          {
          }
       };
-      
+
       if (DRCConfigParameters.USE_ROS_FOR_MULTISENSE_TRANSFORMS)
       {
          rosTransformProvider = ROSNativeTransformTools.getInstance(DRCConfigParameters.ROS_MASTER_URI);
          rosTransformProvider.connect();
       }
-      else {
+      else
+      {
          rosTransformProvider = null;
       }
-      
+
       compressedVideoDataServer = new CompressedVideoDataServer(videoSettings, videoConsumer);
       networkingManager.getControllerCommandHandler().setVideoCommandListener(compressedVideoDataServer);
-      
+
    }
 
    protected void updateLeftEyeImage(BufferedImage bufferedImage, long timeStamp, double fov)
    {
-      for(int i = 0; i < stereoListeners.size(); i++)
+      for (int i = 0; i < stereoListeners.size(); i++)
       {
          stereoListeners.get(i).leftImage(bufferedImage, timeStamp, fov);
       }
+
       RobotPoseData robotPoseData = robotPoseBuffer.floorEntry(ppsTimestampOffsetProvider.ajustTimeStampToRobotClock(timeStamp));
-      if(robotPoseData == null)
+      if (robotPoseData == null)
       {
          return;
       }
-      
+
       if (DRCConfigParameters.USE_ROS_FOR_MULTISENSE_TRANSFORMS)
       {
-         if(rosTransformFromHeadBaseToCamera == null)
+         if ((rosTransformFromHeadBaseToCamera.getType() & Transform3D.IDENTITY) != 0)
          {
             getHeadToCameraTransform(timeStamp);
          }
-         
+
+         if ((rosTransformFromHeadBaseToCamera.getType() & Transform3D.ZERO) != 0)
+         {
+            rosTransformFromHeadBaseToCamera.setIdentity();
+         }
+
          cameraPose.mul(robotPoseData.getCameraPose(), rosTransformFromHeadBaseToCamera);
-         
-      } else {
+      }
+      else
+      {
          cameraPose.set(robotPoseData.getCameraPose());
       }
-      
+
       cameraPose.get(cameraOrientation, tempVector);
       cameraPosition.set(tempVector);
       compressedVideoDataServer.updateImage(bufferedImage, timeStamp, cameraPosition, cameraOrientation, fov, DRCConfigParameters.MULTISENSE_LEFT_CAMERA);
    }
-   
+
    protected void updateFishEyeImage(BufferedImage bufferedImage, long timeStamp, double fov, int sourceId)
    {
-	   RobotPoseData robotPoseData = robotPoseBuffer.floorEntry(ppsTimestampOffsetProvider.ajustTimeStampToRobotClock(timeStamp));
-	   
-	   if(robotPoseData == null)
-	   {
-		   return;
-	   }
-	   
-	   robotPoseData.getCameraPose().get(cameraOrientation, tempVector);
-	   cameraPosition.set(tempVector);
-	   compressedVideoDataServer.updateImage(bufferedImage, timeStamp, cameraPosition, cameraOrientation, fov, sourceId);
-	   
-	   
+      RobotPoseData robotPoseData = robotPoseBuffer.floorEntry(ppsTimestampOffsetProvider.ajustTimeStampToRobotClock(timeStamp));
+
+      if (robotPoseData == null)
+      {
+         return;
+      }
+
+      robotPoseData.getCameraPose().get(cameraOrientation, tempVector);
+      cameraPosition.set(tempVector);
+      compressedVideoDataServer.updateImage(bufferedImage, timeStamp, cameraPosition, cameraOrientation, fov, sourceId);
+
+
    }
-   
+
    protected void getHeadToCameraTransform(long rosTimestamp)
    {
-	   if (ppsTimestampOffsetProvider.offsetIsDetermined())
-	   {
-		   long robotTimestamp = ppsTimestampOffsetProvider.ajustTimeStampToRobotClock(rosTimestamp);
-		   TimeStampedTransform3D transformFromRos = rosTransformProvider.getTimeStampedTransform("/left_camera_optical_frame", "/head", rosTimestamp, robotTimestamp);
-		   rosTransformFromHeadBaseToCamera = transformFromRos.getTransform3D();
-	   }
+      if (ppsTimestampOffsetProvider.offsetIsDetermined())
+      {
+         long robotTimestamp = ppsTimestampOffsetProvider.ajustTimeStampToRobotClock(rosTimestamp);
+         TimeStampedTransform3D transformFromRos = rosTransformProvider.getTimeStampedTransform("/left_camera_optical_frame", "/head", rosTimestamp,
+                                                      robotTimestamp);
+         rosTransformFromHeadBaseToCamera = transformFromRos.getTransform3D();
+      }
    }
 
    protected void updateRightEyeImage(BufferedImage bufferedImage, long timeStamp, double fov)
    {
-      for(int i = 0; i < stereoListeners.size(); i++)
+      for (int i = 0; i < stereoListeners.size(); i++)
       {
          stereoListeners.get(i).rightImage(bufferedImage, timeStamp, fov);
       }
    }
-   
+
    public void registerCameraListener(DRCStereoListener drcStereoListener)
    {
       stereoListeners.add(drcStereoListener);
    }
 
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
