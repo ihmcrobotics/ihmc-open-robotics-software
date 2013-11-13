770659c6fd74ab212565ada9242e8348285ae93a
Tomas de Boer <tdeboer@ihmc.us>
2013-11-13 00:00:36 +0000

    reverted from gainscaling on "50" locations back to one location (okay...two) and cleaned up our code of this morning

--- DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/GainControllerSliderBoard.java
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/GainControllerSliderBoard.java
@@ -1,6 +1,7 @@
 package us.ihmc.atlas.visualization;
 
 import us.ihmc.SdfLoader.GeneralizedSDFRobotModel;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.CommonNames;
 
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
@@ -8,47 +9,46 @@ import com.yobotics.simulationconstructionset.util.inputdevices.SliderBoardConfi
 
 public class GainControllerSliderBoard
 {
-
    public GainControllerSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
    {
-  
-      
       final SliderBoardConfigurationManager sliderBoardConfigurationManager = new SliderBoardConfigurationManager(scs);
-      
-      
+
+
 
       sliderBoardConfigurationManager.setSlider(1, "carIngressPelvisPositionKp", registry, 0.0, 100.0);
-      sliderBoardConfigurationManager.setKnob  (1, "carIngressPelvisPositionZeta", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(1, "carIngressPelvisPositionZeta", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(2, "carIngressPelvisOrientationKp", registry, 0.0, 100.0);
-      sliderBoardConfigurationManager.setKnob  (2, "carIngressPelvisOrientationZeta", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(2, "carIngressPelvisOrientationZeta", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(3, "carIngressChestOrientationKp", registry, 0.0, 100.0);
-      sliderBoardConfigurationManager.setKnob  (3, "carIngressChestOrientationZeta", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(3, "carIngressChestOrientationZeta", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(4, "carIngressHeadOrientationKp", registry, 0.0, 100.0);
-      sliderBoardConfigurationManager.setKnob  (4, "carIngressHeadOrientationZeta", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(4, "carIngressHeadOrientationZeta", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(5, "kpAllArmJointsL", registry, 0.0, 100.0);
-      sliderBoardConfigurationManager.setKnob  (5, "zetaAllArmJointsL", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(5, "zetaAllArmJointsL", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(6, "kpAllArmJointsR", registry, 0.0, 100.0);
-      sliderBoardConfigurationManager.setKnob  (6, "zetaAllArmJointsR", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(6, "zetaAllArmJointsR", registry, 0.0, 1.0);
 
-      sliderBoardConfigurationManager.setSlider(7, "ll_transitionRatio", registry,  0.0, 1.0);
+      sliderBoardConfigurationManager.setSlider(7, "doIHMCControlRatio", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setSlider(7, CommonNames.doIHMCControlRatio.toString(), registry, 0.0, 1.0);
 
-      //sliderBoardConfigurationManager.saveConfiguration(this.getClass().getSimpleName());
-      //sliderBoardConfigurationManager.loadConfiguration(this.getClass().getSimpleName());
+      // sliderBoardConfigurationManager.saveConfiguration(this.getClass().getSimpleName());
+      // sliderBoardConfigurationManager.loadConfiguration(this.getClass().getSimpleName());
    }
-   
-   private static final SliderBoardFactory factory = new SliderBoardFactory() {
-	
-		@Override
-		public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel) {
-			   new GainControllerSliderBoard( scs,  registry,  generalizedSDFRobotModel);
-		}
+
+   private static final SliderBoardFactory factory = new SliderBoardFactory()
+   {
+      @Override
+      public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
+      {
+         new GainControllerSliderBoard(scs, registry, generalizedSDFRobotModel);
+      }
    };
-   
+
    public static SliderBoardFactory getFactory()
    {
       return factory;
--- DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/WalkControllerSliderBoard.java
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/visualization/WalkControllerSliderBoard.java
@@ -1,6 +1,7 @@
 package us.ihmc.atlas.visualization;
 
 import us.ihmc.SdfLoader.GeneralizedSDFRobotModel;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.CommonNames;
 
 import com.yobotics.simulationconstructionset.EnumYoVariable;
 import com.yobotics.simulationconstructionset.SimulationConstructionSet;
@@ -12,67 +13,67 @@ import com.yobotics.simulationconstructionset.util.inputdevices.SliderBoardConfi
 public class WalkControllerSliderBoard
 
 {
-
    public WalkControllerSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
    {
       final EnumYoVariable<SliderBoardMode> sliderBoardMode = new EnumYoVariable<SliderBoardMode>("sliderBoardMode", registry, SliderBoardMode.class);
       final SliderBoardConfigurationManager sliderBoardConfigurationManager = new SliderBoardConfigurationManager(scs);
-      
+
       sliderBoardConfigurationManager.setSlider(1, "captureKpParallel", registry, 0.0, 2.0);
-      sliderBoardConfigurationManager.setKnob  (1, "captureKpOrthogonal", registry, 0.0, 2.0);
+      sliderBoardConfigurationManager.setKnob(1, "captureKpOrthogonal", registry, 0.0, 2.0);
 
       sliderBoardConfigurationManager.setSlider(2, "kp_comHeight", registry, 0.0, 40.0);
-      sliderBoardConfigurationManager.setKnob  (2, "kd_comHeight", registry, 0.0, 13.0);
+      sliderBoardConfigurationManager.setKnob(2, "kd_comHeight", registry, 0.0, 13.0);
 
       sliderBoardConfigurationManager.setSlider(3, "kpPelvisOrientation", registry, 0.0, 100.0);
-      sliderBoardConfigurationManager.setKnob  (3, "zetaPelvisOrientation", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(3, "zetaPelvisOrientation", registry, 0.0, 1.0);
 
-//      sliderBoardConfigurationManager.setSlider(4, "walkingHeadOrientationKp", registry, 0.0, 40.0);
-//      sliderBoardConfigurationManager.setKnob  (4, "walkingHeadOrientationZeta", registry, 0.0, 1.0);
-      
+//    sliderBoardConfigurationManager.setSlider(4, "walkingHeadOrientationKp", registry, 0.0, 40.0);
+//    sliderBoardConfigurationManager.setKnob  (4, "walkingHeadOrientationZeta", registry, 0.0, 1.0);
+
       sliderBoardConfigurationManager.setSlider(4, "kpUpperBody", registry, 0.0, 200.0);
-      sliderBoardConfigurationManager.setKnob  (4, "zetaUpperBody", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(4, "zetaUpperBody", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(5, "kpAllArmJointsL", registry, 0.0, 120.0);
-      sliderBoardConfigurationManager.setKnob  (5, "zetaAllArmJointsL", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(5, "zetaAllArmJointsL", registry, 0.0, 1.0);
 
       sliderBoardConfigurationManager.setSlider(6, "kpAllArmJointsR", registry, 0.0, 120.0);
-      sliderBoardConfigurationManager.setKnob  (6, "zetaAllArmJointsR", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(6, "zetaAllArmJointsR", registry, 0.0, 1.0);
 
-//      sliderBoardConfigurationManager.setSlider(7, "ll_transitionRatio", registry,  0.0, 1.0);
-      sliderBoardConfigurationManager.setSlider(7, "ll_transitionRatio", registry,  0.0, 1.0, 3.5, 0.0);
+      sliderBoardConfigurationManager.setSlider(7, CommonNames.doIHMCControlRatio.toString(), registry, 0.0, 1.0);
+
+//    sliderBoardConfigurationManager.setSlider(7, CommonNames.doIHMCControlRatio.toString(), registry, 0.0, 1.0, 3.5, 0.0);
       sliderBoardConfigurationManager.setSlider(8, "offsetHeightAboveGround", registry, 0.0, 0.20);
-      
-//      sliderBoardConfigurationManager.setKnob  (8, "sliderBoardMode", registry, 0.0, SliderBoardMode.values().length);
-      sliderBoardConfigurationManager.setKnob(8, "gainScaleFactor", registry, 0.0, 1.0, 3.5, 0.0);
+
+//    sliderBoardConfigurationManager.setKnob  (8, "sliderBoardMode", registry, 0.0, SliderBoardMode.values().length);
+//      sliderBoardConfigurationManager.setKnob(8, "gainScaleFactor", registry, 0.0, 1.0, 3.5, 0.0);
 
       sliderBoardConfigurationManager.saveConfiguration(SliderBoardMode.WalkingGains.toString());
       sliderBoardConfigurationManager.clearControls();
-      
+
       sliderBoardConfigurationManager.setSlider(1, "captureKpParallel", registry, 0.0, 2.0);
-      sliderBoardConfigurationManager.setKnob  (1, "captureKpOrthogonal", registry, 0.0, 2.0);
+      sliderBoardConfigurationManager.setKnob(1, "captureKpOrthogonal", registry, 0.0, 2.0);
 
       sliderBoardConfigurationManager.setSlider(2, "desiredICPX", registry, -0.3, 0.3);
-      sliderBoardConfigurationManager.setKnob  (2, "desiredICPY", registry, -0.3, 0.3);
-      
+      sliderBoardConfigurationManager.setKnob(2, "desiredICPY", registry, -0.3, 0.3);
+
       sliderBoardConfigurationManager.setSlider(3, "userDesiredPelvisYaw", registry, -Math.PI, Math.PI);
-      sliderBoardConfigurationManager.setKnob  (3, "userSetDesiredPelvis", registry, 0.0, 1.0);
+      sliderBoardConfigurationManager.setKnob(3, "userSetDesiredPelvis", registry, 0.0, 1.0);
       sliderBoardConfigurationManager.setSlider(4, "userDesiredPelvisPitch", registry, -0.4, 0.4);
       sliderBoardConfigurationManager.setSlider(5, "userDesiredPelvisRoll", registry, -0.3, 0.3);
-      
+
       sliderBoardConfigurationManager.setSlider(6, "userDesiredHeadPitch", registry, -0.5, 0.5);
       sliderBoardConfigurationManager.setSlider(7, "userDesiredHeadYaw", registry, -0.8, 0.8);
       sliderBoardConfigurationManager.setSlider(8, "offsetHeightAboveGround", registry, 0.0, 0.20);
 
-//      sliderBoardConfigurationManager.setKnob  (8, "sliderBoardMode", registry, 0.0, SliderBoardMode.values().length);
+//    sliderBoardConfigurationManager.setKnob  (8, "sliderBoardMode", registry, 0.0, SliderBoardMode.values().length);
       sliderBoardConfigurationManager.setKnob(8, "gainScaleFactor", registry, 0.0, 1.0, 3.5, 0.0);
 
       sliderBoardConfigurationManager.saveConfiguration(SliderBoardMode.WalkingDesireds.toString());
       sliderBoardConfigurationManager.clearControls();
 
-      
+
       sliderBoardMode.set(SliderBoardMode.WalkingGains);
-      
+
       VariableChangedListener listener = new VariableChangedListener()
       {
          @Override
@@ -81,26 +82,23 @@ public class WalkControllerSliderBoard
             sliderBoardConfigurationManager.loadConfiguration(sliderBoardMode.getEnumValue().toString());
          }
       };
-      
+
       sliderBoardMode.addVariableChangedListener(listener);
       listener.variableChanged(null);
-      
+
    }
-   
-   private enum SliderBoardMode
+
+   private enum SliderBoardMode {WalkingGains, WalkingDesireds;}
+
+   private static final SliderBoardFactory factory = new SliderBoardFactory()
    {
-      WalkingGains, WalkingDesireds;
-   }
-   
-   private static final SliderBoardFactory factory = new SliderBoardFactory() {
-   
       @Override
-      public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel) 
+      public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
       {
-         new WalkControllerSliderBoard( scs,  registry,  generalizedSDFRobotModel);
+         new WalkControllerSliderBoard(scs, registry, generalizedSDFRobotModel);
       }
    };
-   
+
    public static SliderBoardFactory getFactory()
    {
       return factory;
