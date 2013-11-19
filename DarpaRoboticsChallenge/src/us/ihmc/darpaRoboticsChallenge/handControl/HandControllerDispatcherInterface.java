bce5c3bb8189eb2a9eba080e58c41945948e30a9
Atlas Control Computer <AtlasControlComputer@unknown>
2013-11-19 13:57:15 +0000

    Passed SDFFullRobotModel down the chain for the hand controllers. Changed [0] in the hands with _0_. Important: We need to either make [0] work, or we need to change to _0_ in all the .sdf files. - Jerry

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandControllerDispatcherInterface.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandControllerDispatcherInterface.java
@@ -2,13 +2,13 @@ package us.ihmc.darpaRoboticsChallenge.handControl;
 
 import java.util.ArrayList;
 
-import us.ihmc.commonWalkingControlModules.controllers.HandControllerInterface;
+import us.ihmc.SdfLoader.SDFFullRobotModel;
 import us.ihmc.darpaRoboticsChallenge.networking.dataProducers.JointConfigurationGatherer;
 import us.ihmc.robotSide.RobotSide;
 
 public interface HandControllerDispatcherInterface
 {
    public void setHandController(RobotSide robotSide, HandControllerWithYoVariables simulatedUnderactuatedSandiaHandController);
-   public void addHandJoints(RobotSide robotSide, ArrayList<FingerJoint> handJoints);
+   public void addHandJoints(SDFFullRobotModel fullRobotModel, RobotSide robotSide, ArrayList<FingerJoint> handJoints);
    public void setJointConfigurationGatherer(JointConfigurationGatherer jointConfigurationGathererAndProducer);
 }
