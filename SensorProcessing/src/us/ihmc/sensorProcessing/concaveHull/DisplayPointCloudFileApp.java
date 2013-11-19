f33adafa9617645eb6a9f4fc91ea04dbdf398c0c
Pushyami Kaveti <pkaveti@ihmc.us>
2013-11-19 00:01:05 +0000

    displaying point cloud with minimal changes

--- SensorProcessing/src/us/ihmc/sensorProcessing/concaveHull/DisplayPointCloudFileApp.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/concaveHull/DisplayPointCloudFileApp.java
@@ -35,17 +35,20 @@ public class DisplayPointCloudFileApp extends SimpleApplication
    Random rand = new Random(234);
 
    public String fileName;
+   public String fileName2;
 
    public static void main(String[] args)
    {
       ///home/unknownid/workspace/SensorProcessing/output.txt
-	   DisplayPointCloudFileApp test1 = new DisplayPointCloudFileApp("plane.txt");
+	   DisplayPointCloudFileApp test1 = new DisplayPointCloudFileApp("torus.txt","gen_torus.txt");
+	  
       test1.start();
    }
 
-   public DisplayPointCloudFileApp(String fileName)
+   public DisplayPointCloudFileApp(String fileName1,String fileName2)
    {
-      this.fileName = fileName;
+      this.fileName = fileName1;
+      this.fileName2 = fileName2;
    }
 
    private ColorRGBA generateColors(float value, float colorRange)
@@ -61,28 +64,50 @@ public class DisplayPointCloudFileApp extends SimpleApplication
 	   Node zUpNode = new Node();
 	      zUpNode.setLocalRotation(JMEGeometryUtils.getRotationFromJMEToZupCoordinates());
 	   
-      List<Point3D_F64> cloud = readPointCloud(1000000);
+      List<Point3D_F64> cloud = readPointCloud(1000000,fileName);
       
       
-      ShapeTranslator shapeTranslator = new ShapeTranslator(this);
-      PlaneGeneral3D_F64 plane = new PlaneGeneral3D_F64(0,1,0,2);
-     
-      shapeTranslator.createPolygon(plane,cloud, ColorRGBA.Cyan, zUpNode);
-      System.out.println("total points: " + cloud.size());
+     ShapeTranslator shapeTranslator = new ShapeTranslator(this);
+//      PlaneGeneral3D_F64 plane = new PlaneGeneral3D_F64(0,1,0,2);
+//     
+//      shapeTranslator.createPolygon(plane,cloud, ColorRGBA.Cyan, zUpNode);
+//      System.out.println("total points: " + cloud.size());
 
-      Vector3f[] points = new Vector3f[cloud.size()];
-      ColorRGBA[] colors = new ColorRGBA[cloud.size()];
+      Vector3f[] points = new Vector3f[cloud.size()*2];
+      ColorRGBA[] colors = new ColorRGBA[cloud.size()*2];
       int index = 0;
       for (Point3D_F64 p : cloud)
       {
-         ColorRGBA color = generateColors(new Float(p.z), 0.25f);
+         ColorRGBA color = generateColors(new Float(2.3), 0.25f);
          points[index] = new Vector3f((float) p.x, (float) p.y, (float) p.z);
          colors[index] = color;
          index++;
       }
-
-
-      
+      cloud = readPointCloud(1000000, fileName2);
+      for (Point3D_F64 p : cloud)
+      {
+         ColorRGBA color = generateColors(new Float(7.9), 0.25f);
+         points[index] = new Vector3f((float) p.x, (float) p.y, (float) p.z);
+         colors[index] = color;
+         index++;
+      }
+//      List<Point3D_F64> eigen = readPointCloud(3,fileName2);
+//      System.out.println(eigen.size());
+//      for (int i = 0; i < eigen.size(); i++)
+//      {
+//         Vector3f start = new Vector3f(0.0f,0.0f,0.0f);
+//         Vector3f end = new Vector3f((float) eigen.get(i).x, (float) eigen.get(i).y, (float) eigen.get(i).z);
+//         System.out.println(end.x+" "+end.y+" "+end.z);
+//         zUpNode.attachChild(shapeTranslator.drawLine(start, end));
+//     
+//      }
+//      Vector3f start = new Vector3f(0.0f,0.0f,0.0f);
+//      Vector3f end = new Vector3f(8.0f, 0.0f, 0.0f);
+//      zUpNode.attachChild(shapeTranslator.drawLine(start, end));
+//      end = new Vector3f(0.0f,8.0f,0.0f);
+//      zUpNode.attachChild(shapeTranslator.drawLine(start, end));
+//      end = new Vector3f(0.0f,0.0f,8.0f);
+//      zUpNode.attachChild(shapeTranslator.drawLine(start, end));
 
       PointCloud generator = new PointCloud(assetManager);
 
@@ -103,7 +128,7 @@ public class DisplayPointCloudFileApp extends SimpleApplication
       flyCam.setMoveSpeed(10);
    }
 
-   private List<Point3D_F64> readPointCloud(int maxLines)
+   private List<Point3D_F64> readPointCloud(int maxLines, String filename)
    {
       List<Point3D_F64> cloud = new ArrayList<Point3D_F64>();
       SerializationDefinitionManager manager = new SerializationDefinitionManager();
@@ -111,7 +136,7 @@ public class DisplayPointCloudFileApp extends SimpleApplication
 
       try
       {
-         FileInputStream in = new FileInputStream(fileName);
+         FileInputStream in = new FileInputStream(filename);
          ReadCsvObjectSmart<Point3D_F64> reader = new ReadCsvObjectSmart<Point3D_F64>(in, manager, "point3d");
 
 
