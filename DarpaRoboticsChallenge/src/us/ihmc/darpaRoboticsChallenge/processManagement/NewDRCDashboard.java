0ecaf2497e776104f817cb2c1ed9422beda18557
Nathan Mertins <nmertins@ihmc.us>
2013-11-19 16:35:14 +0000

    UI for the new Dashboard

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/processManagement/NewDRCDashboard.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/processManagement/NewDRCDashboard.java
@@ -11,52 +11,93 @@ import javax.swing.JPanel;
 public class NewDRCDashboard
 {
    private JFrame frame;
-   private JPanel panel;
+   private JPanel mainPanel, dropListPanel, taskLaunchPanel;
+      
+   private final GazeboRemoteSimulationAdapter sshSimLauncher = new GazeboRemoteSimulationAdapter();
+   private TaskSelectionPanel taskSelectionPanel;
+   private LocalCloudListPanel cloudListPanel;
+   private LaunchSCSAndUIPanel launchPanel;
+   private ImagePanel ihmcLogo;
    
-   private GridBagConstraints c;
+   private final GridBagConstraints c;
+
 
    public NewDRCDashboard()
    {
-      frame = new JFrame("Drag and Drop");
-      panel = new JPanel(new GridBagLayout());
+      frame = new JFrame("DRC Dashboard v2.0");
+      mainPanel = new JPanel(new GridBagLayout());
       
       c = new GridBagConstraints();
       
       c.insets = new Insets(5, 5, 5, 5);
       
-      GazeboRemoteSimulationAdapter sshSimLauncher = new GazeboRemoteSimulationAdapter();
+      setupLeftPanel();
+      setupCenterPanel();
+      setupRightPanel();
       
-      TaskSelectionPanel taskSelectionPanel = new TaskSelectionPanel();
-      LocalCloudListPanel cloudListPanel = new LocalCloudListPanel(sshSimLauncher);
-      JPanel dropListPanel = new JPanel(new GridBagLayout());
-      
-      c.gridx = 0;
-      c.gridy = 0;
-      dropListPanel.add(new DragAndDropTreePanel("Empty Panel:", sshSimLauncher), c);      
-      c.gridy++;
-      dropListPanel.add(new DragAndDropTreePanel("Empty Panel:", sshSimLauncher), c);
-      c.gridy++;
-      dropListPanel.add(new DragAndDropTreePanel("Empty Panel:", sshSimLauncher), c);
-      
-      c.fill = GridBagConstraints.HORIZONTAL;
-      c.weightx = 1.0;
-      c.gridx = 0;
-      c.gridy = 0;
-      panel.add(taskSelectionPanel, c);
-      
-      c.weightx = 0;
-//      c.weighty = 1;
-      c.gridy = 1;
-      panel.add(cloudListPanel, c);
-      
-      c.gridx = 1;
-      panel.add(dropListPanel, c);
-      
-      frame.add(panel);
+      frame.add(mainPanel);
       
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setMinimumSize(new Dimension(800, 650));
+      frame.setMinimumSize(new Dimension(820, 460));
+//      frame.setResizable(false);
       frame.setVisible(true);
+   }
+   
+   private void setupLeftPanel()
+   {
+      cloudListPanel = new LocalCloudListPanel(sshSimLauncher);
+      
+      c.gridx = 0;
+      c.gridy = 0;
+      c.weightx = 0;
+      c.fill = GridBagConstraints.VERTICAL;
+      mainPanel.add(cloudListPanel, c);
+   }
+   
+   private void setupCenterPanel()
+   {
+      dropListPanel = new JPanel(new GridBagLayout());
+      
+      GridBagConstraints c2 = new GridBagConstraints();
+      c2.gridx = 0;
+      c2.gridy = 0;
+      dropListPanel.add(new DragAndDropTreePanel("Controller", sshSimLauncher), c2);      
+      c2.gridy++;
+      dropListPanel.add(new DragAndDropTreePanel("Network Processor", sshSimLauncher), c2);
+      c2.gridy++;
+      dropListPanel.add(new DragAndDropTreePanel("Gazebo", sshSimLauncher), c2);
+      
+      c.gridx++;
+      mainPanel.add(dropListPanel, c);
+   }
+   
+   private void setupRightPanel()
+   {
+      taskLaunchPanel = new JPanel(new GridBagLayout());
+      
+      taskSelectionPanel = new TaskSelectionPanel();
+      launchPanel = new LaunchSCSAndUIPanel();
+      ihmcLogo = new ImagePanel("ihmcRoboticsBlue_cropped.png", 300, 90);
+      
+      GridBagConstraints c2 = new GridBagConstraints();
+      c2.fill = GridBagConstraints.HORIZONTAL;
+      c2.gridx = 0;
+      c2.gridy = 0;
+      taskLaunchPanel.add(taskSelectionPanel, c2);
+      
+      c2.gridy++;
+      taskLaunchPanel.add(launchPanel, c2);
+      
+      c2.gridy++;
+      c2.fill = GridBagConstraints.BOTH;
+      c2.insets = new Insets(0, 0, 0, 0);
+      c2.ipadx = 0;
+      c2.ipady = 0;
+      taskLaunchPanel.add(ihmcLogo, c2);
+      
+      c.weighty = 1.0;
+      c.gridx++;
+      mainPanel.add(taskLaunchPanel, c);
    }
 
    public static void main(String[] args)
--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/processManagement/TaskSelectionPanel.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/processManagement/TaskSelectionPanel.java
@@ -1,17 +1,15 @@
 package us.ihmc.darpaRoboticsChallenge.processManagement;
 
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import us.ihmc.darpaRoboticsChallenge.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.processManagement.DRCDashboardTypes.DRCROSTasks;
 
 public class TaskSelectionPanel extends JPanel
@@ -19,8 +17,8 @@ public class TaskSelectionPanel extends JPanel
 
    private static final long serialVersionUID = -8753653110398404717L;
 
-   private JLabel taskLabel;
-   private JComboBox taskCombo;
+   private JLabel taskLabel, robotModelLabel;
+   private JComboBox taskCombo, robotModelCombo;
    
    private GridBagConstraints c;
    
@@ -37,33 +35,19 @@ public class TaskSelectionPanel extends JPanel
 
       c.gridx = 0;
       c.gridy = 0;
-      c.gridwidth = 1;
-      c.gridheight = 1;
-      c.weighty = 0.3;
-      taskLabel = new JLabel("Select DRC Task:", JLabel.CENTER);
+      taskLabel = new JLabel("Select DRC Task", JLabel.CENTER);
       this.add(taskLabel, c);
 
-      c.gridy = 1;
-
-      DefaultComboBoxModel model = new DefaultComboBoxModel();
-      
-      for (DRCROSTasks task : DRCROSTasks.values())
-      {
-         model.addElement(task);
-      }
-      
-      taskCombo = new JComboBox(model);
+      c.gridy++;
+      taskCombo = new JComboBox(DRCROSTasks.values());
       this.add(taskCombo, c);
-   }
-
-   public static void main(String[] args)
-   {
-      JFrame frame = new JFrame("Task Selection");
-      TaskSelectionPanel taskPanel = new TaskSelectionPanel();
       
-      frame.add(taskPanel);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setMinimumSize(new Dimension(200, 100));
-      frame.setVisible(true);
+      c.gridy++;
+      robotModelLabel = new JLabel("Select Robot Model", JLabel.CENTER);
+      this.add(robotModelLabel, c);
+      
+      c.gridy++;
+      robotModelCombo = new JComboBox(DRCRobotModel.values());
+      this.add(robotModelCombo, c);
    }
 }
