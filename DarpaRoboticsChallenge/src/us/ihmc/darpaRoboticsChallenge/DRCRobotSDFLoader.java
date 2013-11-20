25d076d0e704a2a096dad8ccc2eb7777d361a576
Wessel Straatman <wstraatman@ihmc.us>
2013-11-20 00:26:11 +0000

    Added a new robot model, ATLAS_IHMC_PARAMETERS, which does not have the iRobot hands weighing 9kg each but only 2.6 kg each as they should be. Weight is distributed over hand (2.45 kg, including attachment disc) and fingers (~50g each). Also, for the finger joints the moments of inertia have been multiplied by a factor 100 per Jerry's request.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotSDFLoader.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotSDFLoader.java
@@ -11,20 +11,19 @@ import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotJointMap;
 
 public class DRCRobotSDFLoader
 {
-   
    public static JaxbSDFLoader loadDRCRobot(DRCRobotJointMap jointMap)
    {
-      return loadDRCRobot(jointMap ,false);
+      return loadDRCRobot(jointMap, false);
    }
-   
+
    public static JaxbSDFLoader loadDRCRobot(DRCRobotJointMap jointMap, boolean headless)
    {
       InputStream fileInputStream;
       ArrayList<String> resourceDirectories = new ArrayList<String>();
       Class<DRCRobotSDFLoader> myClass = DRCRobotSDFLoader.class;
       DRCRobotModel selectedModel = jointMap.getSelectedModel();
-      
-      if(!headless)
+
+      if (!headless)
       {
          resourceDirectories.add(myClass.getResource("models/GFE/gazebo").getFile());
          resourceDirectories.add(myClass.getResource("models/GFE/").getFile());
@@ -32,47 +31,71 @@ public class DRCRobotSDFLoader
          resourceDirectories.add(myClass.getResource("models/GFE/gazebo_models/multisense_sl_description").getFile());
          resourceDirectories.add(myClass.getResource("models").getFile());
       }
-      
+
       switch (selectedModel)
       {
          case ATLAS_NO_HANDS :
          case ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS :
             fileInputStream = myClass.getResourceAsStream("models/GFE/atlas.sdf");
-            
+
             break;
 
          case ATLAS_IROBOT_HANDS :
             fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_irobot_hands.sdf");
-            if(!headless)
+
+            if (!headless)
             {
                resourceDirectories.add(myClass.getResource("models/GFE/gazebo_models/irobot_hand_description").getFile());
             }
+
             break;
+
          case ATLAS_V3_IROBOT_HANDS :
-        	 fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_v3_irobot_hands.sdf");
-        	 if(!headless)
-        	 {
-        		 resourceDirectories.add(myClass.getResource("models/GFE/gazebo_models/irobot_hand_description").getFile());
-        	 }
-        	 break;
+            fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_v3_irobot_hands.sdf");
+
+            if (!headless)
+            {
+               resourceDirectories.add(myClass.getResource("models/GFE/gazebo_models/irobot_hand_description").getFile());
+            }
+
+            break;
+
          case ATLAS_V3_IROBOT_HANDS_ADDED_MASS :
             fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_v3_irobot_hands_addedmass.sdf");
-            if(!headless)
+
+            if (!headless)
             {
                resourceDirectories.add(myClass.getResource("models/GFE/gazebo_models/irobot_hand_description").getFile());
             }
+
+            break;
+
+         case ATLAS_IHMC_PARAMETERS :
+            fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_ihmc_parameters.sdf");
+
+            if (!headless)
+            {
+               resourceDirectories.add(myClass.getResource("models/GFE/gazebo_models/irobot_hand_description").getFile());
+            }
+
             break;
 
          case ATLAS_SANDIA_HANDS :
             fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_sandia_hands.sdf");
+
             break;
+
          case ATLAS_NO_HANDS_ADDED_MASS :
             fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_addedmass.sdf");
+
             break;
-         case ATLAS_CALIBRATION:
+
+         case ATLAS_CALIBRATION :
             fileInputStream = myClass.getResourceAsStream("models/GFE/atlas_calibration.sdf");
+
             break;
-         default:
+
+         default :
             throw new RuntimeException("DRCRobotSDFLoader: Unimplemented enumeration case : " + selectedModel);
       }
 
@@ -91,15 +114,16 @@ public class DRCRobotSDFLoader
 
          throw new RuntimeException("Invalid SDF file: " + e.getMessage());
       }
+
       return jaxbSDFLoader;
    }
-   
+
    public static void main(String[] args)
    {
       DRCRobotJointMap jointMap = new DRCRobotJointMap(DRCRobotModel.ATLAS_SANDIA_HANDS, false);
       JaxbSDFLoader loader = loadDRCRobot(jointMap, false);
       System.out.println(loader.createRobot(jointMap, true).getName());
-      
+
    }
-   
+
 }
