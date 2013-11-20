f7d3c8c9bab97cb55ad7ad3886843b490b340177
Jesper Smith <jsmith@ihmc.us>
2013-11-20 00:29:54 +0000

    Fisheye camera streaming.

---
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/CompressedVideoHandler.java
@@ -0,0 +1,16 @@
+package us.ihmc.graphics3DAdapter.camera;
+
+import javax.vecmath.Point3d;
+import javax.vecmath.Quat4d;
+
+import us.ihmc.graphics3DAdapter.camera.VideoSettings.VideoCompressionKey;
+
+public interface CompressedVideoHandler
+{
+   public abstract void newVideoPacketAvailable(long timeStamp, byte[] data, Point3d position, Quat4d orientation, double fieldOfView,
+         VideoCompressionKey videoCompressionKey);
+
+   public abstract void addNetStateListener(CompressedVideoDataServer compressedVideoDataServer);
+   
+   public abstract boolean isConnected();
+}
