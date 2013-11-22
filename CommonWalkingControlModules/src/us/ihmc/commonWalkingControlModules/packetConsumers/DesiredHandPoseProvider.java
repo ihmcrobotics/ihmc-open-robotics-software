164041a8abad610fbeeff244e0c3979acfb23a5d
Sylvain Bertrand <sbertrand@ihmc.us>
2013-11-22 09:50:11 +0000

    Created the BooleanYoVariable useChestFrameForArms in DesiredHandPoseProvider to switch between worldFrame and chestFrame when sending desired hand poses from the GUI.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/DesiredHandPoseProvider.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/DesiredHandPoseProvider.java
@@ -2,6 +2,9 @@ package us.ihmc.commonWalkingControlModules.packetConsumers;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.yobotics.simulationconstructionset.BooleanYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
 import us.ihmc.commonWalkingControlModules.dynamics.FullRobotModel;
 import us.ihmc.commonWalkingControlModules.packets.HandPosePacket;
@@ -13,17 +16,20 @@ import us.ihmc.utilities.net.ObjectConsumer;
 
 public class DesiredHandPoseProvider implements ObjectConsumer<HandPosePacket>
 {
-
    private final SideDependentList<AtomicReference<HandPosePacket>> packets = new SideDependentList<AtomicReference<HandPosePacket>>();
    
    private final SideDependentList<FramePose> homePositions = new SideDependentList<FramePose>();
    private final SideDependentList<FramePose> desiredHandPoses = new SideDependentList<FramePose>();
    private double trajectoryTime = 1.0;
 
+   private final BooleanYoVariable useChestFrameForArms;
+   
    private final ReferenceFrame chestFrame;
 
-   public DesiredHandPoseProvider(FullRobotModel fullRobotModel, WalkingControllerParameters walkingControllerParameters)
+   public DesiredHandPoseProvider(FullRobotModel fullRobotModel, WalkingControllerParameters walkingControllerParameters, YoVariableRegistry registry)
    {
+      useChestFrameForArms = new BooleanYoVariable("useChestFrameForArms", registry);
+      
       chestFrame = fullRobotModel.getChest().getBodyFixedFrame();
       for (RobotSide robotSide : RobotSide.values)
       {
@@ -82,6 +88,13 @@ public class DesiredHandPoseProvider implements ObjectConsumer<HandPosePacket>
             }
             
             FramePose pose = new FramePose(referenceFrame, object.getPosition(), object.getOrientation());
+            
+            if (useChestFrameForArms.getBooleanValue())
+            {
+               referenceFrame = chestFrame;
+               pose.changeFrame(referenceFrame);
+            }
+            
             desiredHandPoses.put(robotSide, pose);
          }
       }
