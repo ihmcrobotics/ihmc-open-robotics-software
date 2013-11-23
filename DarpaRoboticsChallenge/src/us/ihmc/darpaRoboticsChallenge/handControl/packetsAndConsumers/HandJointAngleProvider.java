c0c34aa64bd7330095595edcb087ab0c6fc53352
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2013-11-23 01:38:00 +0000

    setting up joint angle providers for sending the hand angles to the UI. not finished. COmmented code so I can work at home

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/packetsAndConsumers/HandJointAngleProvider.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/packetsAndConsumers/HandJointAngleProvider.java
@@ -1,89 +1,58 @@
 package us.ihmc.darpaRoboticsChallenge.handControl.packetsAndConsumers;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 import us.ihmc.SdfLoader.SDFFullRobotModel;
-import us.ihmc.commonWalkingControlModules.dynamics.FullRobotModel;
-import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmJointName;
-import us.ihmc.darpaRoboticsChallenge.handControl.iRobot.iRobotHandModel.iRobotFingerJointName;
-import us.ihmc.darpaRoboticsChallenge.handControl.iRobot.iRobotHandModel.iRobotFingerName;
+import us.ihmc.darpaRoboticsChallenge.handControl.iRobot.iRobotHandModel.iRobotHandJointNameMinimal;
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.robotSide.SideDependentList;
 import us.ihmc.utilities.net.ObjectConsumer;
 import us.ihmc.utilities.screwTheory.OneDoFJoint;
 
+import com.yobotics.simulationconstructionset.graphics.GraphicsUpdatable;
+
 public class HandJointAngleProvider implements ObjectConsumer<HandJointAnglePacket>
 {
-   private final SideDependentList<AtomicReference<HandJointAnglePacket>> packets = new SideDependentList<AtomicReference<HandJointAnglePacket>>();
-   
-   protected final SideDependentList<EnumMap<iRobotFingerName, double[]>> fingerJointPositions = new SideDependentList<EnumMap<iRobotFingerName, double[]>>();
+   private final SideDependentList<EnumMap<iRobotHandJointNameMinimal, OneDoFJoint>> handJoints = new SideDependentList<EnumMap<iRobotHandJointNameMinimal, OneDoFJoint>>();
 
-   public HandJointAngleProvider()
+   private final Object lock = new Object();
+   private ArrayList<GraphicsUpdatable> graphicsToUpdate = new ArrayList<GraphicsUpdatable>();
+
+   public HandJointAngleProvider(SDFFullRobotModel fullRobotModel)
    {
-//      for (RobotSide robotSide : RobotSide.values)
-//      {
-//         packets.put(robotSide, new AtomicReference<HandJointAnglePacket>());
-//         
-//         EnumMap<iRobotFingerJointName, Double> jointAngles = new EnumMap<iRobotFingerJointName, Double>(iRobotFingerJointName.class);
-//
-//         for(iRobotFingerJointName jointName : iRobotFingerJointName.values())
-//         {
-//            jointAngles.put(jointName, 0.0);
-//         }
-//
-//         fingerJointPositions.put(robotSide, jointAngles);
-//      }
+      for (RobotSide side : RobotSide.values())
+      {
+         final EnumMap<iRobotHandJointNameMinimal, OneDoFJoint> joints = new EnumMap<iRobotHandJointNameMinimal, OneDoFJoint>(iRobotHandJointNameMinimal.class);
+
+         for (iRobotHandJointNameMinimal jointName : iRobotHandJointNameMinimal.values())
+         {
+            joints.put(jointName, fullRobotModel.getOneDoFJointByName(jointName.toLowerCase()));
+         }
+         handJoints.put(side, joints);
+      }
    }
 
-   public boolean checkForNewArmJointPositions(RobotSide robotSide)
+   public void addGraphicsUpdateable(GraphicsUpdatable updateable)
    {
-//      HandJointAnglePacket object = packets.get(robotSide).getAndSet(null);
-//      if(object != null)
-//      {
-//         EnumMap<iRobotFingerJointName, Double> jointAngles = fingerJointPositions.get(robotSide);
-//
-//         jointAngles.put(iRobotFingerJointName.FLEXIBLE_JOINT_FLEX_FROM_1_TO_2, object.getIndexJoints()[0]);
-//         return true;
-//      }
-//      else
-//      {
-//         return false;
-//      }
-      return false;
-   }
-
-   public Map<OneDoFJoint, Double> getDesiredArmJointPositions(SDFFullRobotModel fullRobotModel, RobotSide robotSide)
-   {
-      Map<OneDoFJoint, Double> jointPositions = new LinkedHashMap<OneDoFJoint, Double>();
-//      EnumMap<ArmJointName, Double> jointAngles = armJointPositions.get(robotSide);
-//
-//      for (ArmJointName armJointName : jointAngles.keySet())
-//      {
-//         OneDoFJoint armJoint = fullRobotModel.getArmJoint(robotSide, armJointName);
-//         Double jointAngle = jointAngles.get(armJointName);
-//
-//         jointPositions.put(armJoint, jointAngle);
-//      }
-
-      return jointPositions;
+      graphicsToUpdate.add(updateable);
    }
 
    public void consumeObject(HandJointAnglePacket object)
    {
-      packets.get(object.getRobotSide()).set(object);
-   }
+      synchronized (lock)
+      {
+         EnumMap<iRobotHandJointNameMinimal, OneDoFJoint> joints = handJoints.get(object.getRobotSide());
 
-   public EnumMap<ArmJointName, Double> peekAtDesiredArmJointPostionEnumMap(RobotSide robotSide)
-   {
-      return null;// armJointPositions.get(robotSide);
-   }
+         for (iRobotHandJointNameMinimal jointName : iRobotHandJointNameMinimal.values())
+         {
+            joints.get(jointName).setQ(object.getJointAngle(jointName));
+         }
 
-   public EnumMap<ArmJointName, Double> getDesiredArmJointPostionEnumMap(RobotSide robotSide)
-   {
-      return null;//armJointPositions.get(robotSide);
+         for (GraphicsUpdatable graphicsUpdatable : graphicsToUpdate)
+         {
+            graphicsUpdatable.update();
+         }
+      }
    }
-
 }
