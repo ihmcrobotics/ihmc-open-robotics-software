8d2c5609fc1abaa6dd8bcdbe8ee545c53d2c96bb
Nathan Mertins <nmertins@ihmc.us>
2013-11-26 22:17:10 +0000

    DRC-789: FingerStatePackets weren't making it to the ROSiRobotCommunicator so I pulled the NodeMainExecutor into the CommandDispatcher

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/ROSiRobotCommandDispatcher.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/ROSiRobotCommandDispatcher.java
@@ -1,32 +1,53 @@
 package us.ihmc.darpaRoboticsChallenge.ros;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.ros.node.DefaultNodeMainExecutor;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+
 import us.ihmc.commonWalkingControlModules.packetConsumers.FingerStateProvider;
 import us.ihmc.commonWalkingControlModules.packets.FingerStatePacket;
 import us.ihmc.utilities.net.ObjectCommunicator;
+import us.ihmc.utilities.ros.RosTools;
 
 public class ROSiRobotCommandDispatcher implements Runnable
 {
-   private FingerStateProvider fingerStateProvider = new FingerStateProvider();
+   private final FingerStateProvider fingerStateProvider = new FingerStateProvider();
 
-   private ROSiRobotCommunicator rosHandCommunicator;
+   private final ROSiRobotCommunicator rosHandCommunicator;
 
    public ROSiRobotCommandDispatcher(ObjectCommunicator objectCommunicator, String rosHostIP)
    {
       objectCommunicator.attachListener(FingerStatePacket.class, fingerStateProvider);
-      rosHandCommunicator = new ROSiRobotCommunicator("http://" + rosHostIP + ":11311");
+      
+      String rosURI = "http://" + rosHostIP + ":11311";
+      
+      rosHandCommunicator = new ROSiRobotCommunicator(rosURI);
+      
+      try
+      {
+         NodeConfiguration nodeConfiguration = RosTools.createNodeConfiguration(new URI(rosURI));
+         NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+         nodeMainExecutor.execute(rosHandCommunicator, nodeConfiguration);
+      }
+      catch (URISyntaxException e)
+      {
+         e.printStackTrace();
+      }
    }
 
    @Override
    public void run()
    {
-      if (fingerStateProvider.isNewFingerStateAvailable())
+      while (true)
       {
-         FingerStatePacket packet = fingerStateProvider.pullPacket();
-         rosHandCommunicator.sendHandCommand(packet);
+         if (fingerStateProvider.isNewFingerStateAvailable())
+         {
+            FingerStatePacket packet = fingerStateProvider.pullPacket();
+            rosHandCommunicator.sendHandCommand(packet);
+         }
       }
-
    }
-
-
-
 }
