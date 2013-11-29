4b4e4be16fac38fff1df4f1fddc869ac970b4fd3
Matt Johnson <mjohnson@ihmc.us>
2013-11-29 21:18:15 +0000

    DRC-809 added watcher to see when we are approaching joint limits
    cleaned up some UI panels:
    Bandwidth: lined things up and arranged them consistently. Limited max to 25fps
    PositionControl: removed - it was for VRC

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/sensors/RobotJointLimitWatcherForUI.java
@@ -0,0 +1,77 @@
+package us.ihmc.darpaRoboticsChallenge.sensors;
+
+import com.yobotics.simulationconstructionset.YoVariableLimitChecker;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+
+import java.util.ArrayList;
+
+public class RobotJointLimitWatcherForUI extends RobotJointLimitWatcher
+{
+   private YoVariableLimitChecker[] closeToLimitCheckers;
+
+   public RobotJointLimitWatcherForUI(OneDoFJoint[] oneDoFJoints)
+   {
+      super(oneDoFJoints);
+
+      int numberOfJoints = oneDoFJoints.length;
+      closeToLimitCheckers = new YoVariableLimitChecker[numberOfJoints];
+
+      for (int i = 0; i < numberOfJoints; i++)
+      {
+         OneDoFJoint oneDoFJoint = oneDoFJoints[i];
+
+         double thresholdPercentage = 0.02;
+         double range = oneDoFJoint.getJointLimitUpper() - oneDoFJoint.getJointLimitLower();
+         double thresholdAmount = range * thresholdPercentage;
+         double lowerLimit = oneDoFJoint.getJointLimitLower() + thresholdAmount + (range * 0.1);
+         double upperLimit = oneDoFJoint.getJointLimitUpper() - thresholdAmount - (range * 0.1);
+
+         closeToLimitCheckers[i] = new YoVariableLimitChecker(variablesToTrack[i], "approaching", lowerLimit, upperLimit, registry);
+      }
+   }
+
+   public void doControl()
+   {
+      for (int i = 0; i < limitCheckers.length; i++)
+      {
+         variablesToTrack[i].set(oneDoFJoints[i].getQ());
+         limitCheckers[i].update();
+         closeToLimitCheckers[i].update();
+      }
+   }
+
+   public ArrayList<String> getJointsAtLimits()
+   {
+      ArrayList<String> jointsAtLimits = new ArrayList<String>();
+
+      for (int i = 0; i < limitCheckers.length; i++)
+      {
+         String jointName = oneDoFJoints[i].getName();
+         YoVariableLimitChecker.Status jointStatus = limitCheckers[i].getStatus();
+         if (!jointStatus.equals(YoVariableLimitChecker.Status.IN_RANGE))
+         {
+            jointsAtLimits.add(jointName);
+         }
+      }
+
+      return jointsAtLimits;
+   }
+
+   public ArrayList<String> getJointsApproachingLimits()
+   {
+      ArrayList<String> jointsApproachingLimits = new ArrayList<String>();
+
+      for (int i = 0; i < closeToLimitCheckers.length; i++)
+      {
+         String jointName = oneDoFJoints[i].getName();
+         YoVariableLimitChecker.Status jointStatus = closeToLimitCheckers[i].getStatus();
+         if (!jointStatus.equals(YoVariableLimitChecker.Status.IN_RANGE))
+         {
+            jointsApproachingLimits.add(jointName);
+         }
+      }
+
+      return jointsApproachingLimits;
+   }
+}
