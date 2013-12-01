5a782439a52dc4390a9c47d4bebfd813024094f0
Atlas Control Computer <AtlasControlComputer@unknown>
2013-12-01 06:02:52 +0000

    NONE: Set rhoMin to 4.0 (was 0.0).

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/HighLevelHumanoidControllerFactoryHelper.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/HighLevelHumanoidControllerFactoryHelper.java
@@ -183,7 +183,7 @@ public class HighLevelHumanoidControllerFactoryHelper
       momentumOptimizationSettings.setPhiCylinderContactRegularization(0.002);
       momentumOptimizationSettings.setRhoPlaneContactRegularization(0.001);
       momentumOptimizationSettings.setMomentumWeight(1.0, 1.0, 10.0, 10.0);
-      momentumOptimizationSettings.setRhoMin(0.0);
+      momentumOptimizationSettings.setRhoMin(4.0);
       momentumOptimizationSettings.setRateOfChangeOfRhoPlaneContactRegularization(0.01);
       momentumOptimizationSettings.setRhoPenalizerPlaneContactRegularization(0.02);
       momentumOptimizationSettings.setMomentumOptimizerToUse(MomentumOptimizer.GRF_SMOOTHER);
