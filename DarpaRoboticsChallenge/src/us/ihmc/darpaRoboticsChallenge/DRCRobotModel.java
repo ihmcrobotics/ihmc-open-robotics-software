4d29954708e9530a8025057246379d69a7c4dede
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2013-12-02 04:13:06 +0000

    DRC-807 created new sdf with hands rotated to match extension mounting bracket

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotModel.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotModel.java
@@ -5,13 +5,13 @@ import us.ihmc.darpaRoboticsChallenge.handControl.DRCHandModel;
 public enum DRCRobotModel
 {
    ATLAS_NO_HANDS, ATLAS_NO_HANDS_ADDED_MASS, ATLAS_SANDIA_HANDS, ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS, ATLAS_IROBOT_HANDS,
-   ATLAS_IROBOT_HANDS_ADDED_MASS,  ATLAS_IROBOT_HANDS_WITH_EXTENSION, ATLAS_IROBOT_HANDS_ADDED_MASS_COMXZ, ATLAS_IROBOT_HANDS_ADDED_MASS_COMXYZ,ATLAS_CALIBRATION,ATLAS_RHOOK_HAND, ATLAS_IHMC_PARAMETERS, ATLAS_;
+   ATLAS_IROBOT_HANDS_ADDED_MASS,  ATLAS_IROBOT_HANDS_WITH_EXTENSION, ATLAS_IROBOT_HANDS_WITH_EXTENSION_ROTATED, ATLAS_IROBOT_HANDS_ADDED_MASS_COMXZ, ATLAS_IROBOT_HANDS_ADDED_MASS_COMXYZ,ATLAS_CALIBRATION,ATLAS_RHOOK_HAND, ATLAS_IHMC_PARAMETERS, ATLAS_;
 
    public static DRCRobotModel getDefaultRobotModel()
    {
       return DRCLocalConfigParameters.robotModelToUse;
    }
-
+   
    public boolean hasIRobotHands()
    {
       switch (this)
@@ -24,6 +24,7 @@ public enum DRCRobotModel
          case ATLAS_IROBOT_HANDS_ADDED_MASS_COMXYZ:
          case ATLAS_CALIBRATION :
          case ATLAS_RHOOK_HAND:
+         case ATLAS_IROBOT_HANDS_WITH_EXTENSION_ROTATED:
             return true;
 
          default :
@@ -43,6 +44,7 @@ public enum DRCRobotModel
          case ATLAS_IROBOT_HANDS_ADDED_MASS_COMXYZ:
          case ATLAS_CALIBRATION :
          case ATLAS_RHOOK_HAND:
+         case ATLAS_IROBOT_HANDS_WITH_EXTENSION_ROTATED:
             return DRCHandModel.IROBOT;
 
          case ATLAS_SANDIA_HANDS :
@@ -72,6 +74,7 @@ public enum DRCRobotModel
          case ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS :
          case ATLAS_CALIBRATION :
          case ATLAS_RHOOK_HAND:
+         case ATLAS_IROBOT_HANDS_WITH_EXTENSION_ROTATED:
             return "atlas";
 
          default :
