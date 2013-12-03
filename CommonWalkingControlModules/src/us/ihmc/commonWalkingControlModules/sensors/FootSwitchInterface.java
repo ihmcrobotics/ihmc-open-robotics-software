b6c8dc1e5257b9643e1ad41717aca9c68a75eede
Sylvain Bertrand <sbertrand@ihmc.us>
2013-12-03 02:27:46 +0000

    NONE: Passed footSwitches in MomentumBasedController and added the method computeAndPackCoP() in FootSwitchInterface.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/FootSwitchInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/FootSwitchInterface.java
@@ -1,11 +1,14 @@
 package us.ihmc.commonWalkingControlModules.sensors;
 
+import us.ihmc.utilities.math.geometry.FramePoint2d;
+
 
 public interface FootSwitchInterface
 {
    public abstract boolean hasFootHitGround();
    
    public abstract double computeFootLoadPercentage();
+   public abstract void computeAndPackCoP(FramePoint2d copToPack);
 
    public void reset();
 }
