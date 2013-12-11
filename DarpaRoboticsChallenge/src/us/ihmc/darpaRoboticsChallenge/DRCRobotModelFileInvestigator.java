a7ef40038bb501b0d4f0d2376fdddc161ecb5b44
Jerry Pratt <jpratt@ihmc.us>
2013-12-11 06:55:30 +0000

    DRC-743: Cleaned up DRCRobotModelFileInvestigator.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotModelFileInvestigator.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCRobotModelFileInvestigator.java
@@ -1,7 +1,6 @@
 package us.ihmc.darpaRoboticsChallenge;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 import us.ihmc.SdfLoader.JaxbSDFLoader;
@@ -10,31 +9,41 @@ import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotJointMap;
 
 public class DRCRobotModelFileInvestigator
 {
-   public static void main(String[] args) throws FileNotFoundException
+   public void writeModelFile(DRCRobotModel model)
+   {
+      try
+      {
+         String filename = "scsRobotModelOutputs/scsRobot_" + model;
+         File file = new File(filename);
+
+         PrintWriter printWriter = new PrintWriter(file);
+
+         DRCRobotJointMap jointMap = new DRCRobotJointMap(model, false);
+         JaxbSDFLoader robotLoader = DRCRobotSDFLoader.loadDRCRobot(jointMap);
+         final SDFRobot robot = robotLoader.createRobot(jointMap, false);
+
+         printWriter.println(robot);
+         printWriter.close();
+      }
+      catch (Exception e)
+      {
+         System.err.println("Caught exception with model " + model);
+         e.printStackTrace();
+      }
+   }
+
+   public void writeAllModelFiles()
    {
       for (DRCRobotModel model : DRCRobotModel.values())
       {
-         try
-         {
-            String filename = "scsRobotModelOutputs/scsRobot_" + model;
-            File file = new File(filename);
-
-            PrintWriter printWriter = new PrintWriter(file);
-
-            DRCRobotJointMap jointMap = new DRCRobotJointMap(model, false);
-            JaxbSDFLoader robotLoader = DRCRobotSDFLoader.loadDRCRobot(jointMap);
-            final SDFRobot robot = robotLoader.createRobot(jointMap, false);
-
-            printWriter.println(robot);
-            printWriter.close();
-         }
-         catch(Exception e)
-         {
-            System.err.println("Caught exception with model " + model);
-            e.printStackTrace();
-         }
+         writeModelFile(model);
       }
-
-
    }
+
+   public static void main(String[] args)
+   {
+      DRCRobotModelFileInvestigator investigator = new DRCRobotModelFileInvestigator();
+      investigator.writeAllModelFiles();
+   }
+
 }
