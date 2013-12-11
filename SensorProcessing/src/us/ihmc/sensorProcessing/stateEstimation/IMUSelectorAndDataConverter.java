f30980d18dabc7ced1b1364f7f0dbbfd8b3075a9
Sylvain Bertrand <sbertrand@ihmc.us>
2013-12-11 17:22:06 +0000

    NONE: Added a simulated IMU drift.

--- SensorProcessing/src/us/ihmc/sensorProcessing/stateEstimation/IMUSelectorAndDataConverter.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/stateEstimation/IMUSelectorAndDataConverter.java
@@ -15,6 +15,7 @@ import us.ihmc.sensorProcessing.controlFlowPorts.YoFrameVectorControlFlowOutputP
 import us.ihmc.sensorProcessing.stateEstimation.evaluation.FullInverseDynamicsStructure;
 import us.ihmc.sensorProcessing.stateEstimation.sensorConfiguration.AngularVelocitySensorConfiguration;
 import us.ihmc.sensorProcessing.stateEstimation.sensorConfiguration.OrientationSensorConfiguration;
+import us.ihmc.utilities.math.geometry.AngleTools;
 import us.ihmc.utilities.math.geometry.FrameOrientation;
 import us.ihmc.utilities.math.geometry.FrameVector;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
@@ -22,6 +23,7 @@ import us.ihmc.utilities.screwTheory.RigidBody;
 import us.ihmc.utilities.screwTheory.Twist;
 import us.ihmc.utilities.screwTheory.TwistCalculator;
 
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
 public class IMUSelectorAndDataConverter extends AbstractControlFlowElement
@@ -42,15 +44,25 @@ public class IMUSelectorAndDataConverter extends AbstractControlFlowElement
    private final ReferenceFrame angularVelocityMeasurementFrame;
    
    private final YoVariableRegistry registry;
+   private final DoubleYoVariable imuSimulatedDriftYawAcceleration;
+   private final DoubleYoVariable imuSimulatedDriftYawVelocity;
+   private final DoubleYoVariable imuSimulatedDriftYawAngle;
+   
+   private final double estimatorDT;
 
    public IMUSelectorAndDataConverter(ControlFlowGraph controlFlowGraph, Collection<OrientationSensorConfiguration> orientationSensorConfigurations,
                                       Collection<AngularVelocitySensorConfiguration> angularVelocitySensorConfigurations,
-                                      ControlFlowOutputPort<FullInverseDynamicsStructure> inverseDynamicsStructureOutputPort, YoVariableRegistry registry)
+                                      ControlFlowOutputPort<FullInverseDynamicsStructure> inverseDynamicsStructureOutputPort, double estimatorDT, YoVariableRegistry registry)
    {
       OrientationSensorConfiguration selectedOrientationSensorConfiguration = null;
       if ((orientationSensorConfigurations.size() != 1) || (angularVelocitySensorConfigurations.size() != 1))
          throw new RuntimeException("We are assuming there is only 1 IMU for right now..");
 
+      imuSimulatedDriftYawAcceleration = new DoubleYoVariable("imuSimulatedDriftYawAcceleration", registry);
+      imuSimulatedDriftYawVelocity = new DoubleYoVariable("imuSimulatedDriftYawVelocity", registry);
+      imuSimulatedDriftYawAngle = new DoubleYoVariable("imuSimulatedDriftYawAngle", registry);
+      this.estimatorDT = estimatorDT;
+      
       for (OrientationSensorConfiguration orientationSensorConfiguration : orientationSensorConfigurations)
       {
          selectedOrientationSensorConfiguration = orientationSensorConfiguration;
@@ -98,6 +110,10 @@ public class IMUSelectorAndDataConverter extends AbstractControlFlowElement
 
    public void startComputation()
    {
+      imuSimulatedDriftYawVelocity.add(imuSimulatedDriftYawAcceleration.getDoubleValue() * estimatorDT);
+      imuSimulatedDriftYawAngle.add(imuSimulatedDriftYawVelocity.getDoubleValue() * estimatorDT);
+      imuSimulatedDriftYawAngle.set(AngleTools.trimAngleMinusPiToPi(imuSimulatedDriftYawAngle.getDoubleValue()));
+      
       convertOrientationAndSetOnOutputPort();      
       convertAngularVelocityAndSetOnOutputPort();   
    }
@@ -115,6 +131,8 @@ public class IMUSelectorAndDataConverter extends AbstractControlFlowElement
    private final Transform3D transformFromEstimationToWorld = new Transform3D();
    private final Matrix3d rotationFromEstimationToWorld = new Matrix3d();
    
+   private final double[] estimationFrameYawPitchRoll = new double[3];
+   
    private void convertOrientationAndSetOnOutputPort()
    {
       transformFromIMUToWorld.set(orientationInputPort.getData());
@@ -124,6 +142,11 @@ public class IMUSelectorAndDataConverter extends AbstractControlFlowElement
       transformFromEstimationToWorld.mul(transformFromIMUToWorld, transformFromEstimationFrameToIMUFrame);
       transformFromEstimationToWorld.get(rotationFromEstimationToWorld);
       tempOrientationEstimationFrame.set(ReferenceFrame.getWorldFrame(), rotationFromEstimationToWorld);
+      
+      // Introduce simulated IMU drift
+      tempOrientationEstimationFrame.getYawPitchRoll(estimationFrameYawPitchRoll);
+      estimationFrameYawPitchRoll[0] += imuSimulatedDriftYawAngle.getDoubleValue();
+      tempOrientationEstimationFrame.setYawPitchRoll(estimationFrameYawPitchRoll);
       
       orientationOutputPort.setData(tempOrientationEstimationFrame);
    }
@@ -142,6 +165,9 @@ public class IMUSelectorAndDataConverter extends AbstractControlFlowElement
       relativeAngularVelocity.add(tempAngularVelocityMeasurementLink);
 
       tempAngularVelocityEstimationLink.setAndChangeFrame(relativeAngularVelocity); // just a copy for clarity
+      
+      // Introduce simulated IMU drift
+      tempAngularVelocityEstimationLink.setZ(tempAngularVelocityEstimationLink.getZ() + imuSimulatedDriftYawVelocity.getDoubleValue());
 
       angularVelocityOutputPort.setData(tempAngularVelocityEstimationLink);
    }
