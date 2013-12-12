d8c0cf1292ff17426c3cc57772c55a895284ed8d
Jesper Smith <jsmith@ihmc.us>
2013-12-12 01:01:08 +0000

    NONE: Zoom/crop modus for camera. Already #fixbamboo

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/VideoControlSettings.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/VideoControlSettings.java
@@ -5,6 +5,7 @@ import us.ihmc.graphics3DAdapter.camera.VideoSettings.VideoCompressionKey;
 public interface VideoControlSettings
 {
    public boolean isSendVideo();
+   public boolean crop();
    public VideoCompressionKey getVideoQualitySetting();
    public int getFps();
 }
