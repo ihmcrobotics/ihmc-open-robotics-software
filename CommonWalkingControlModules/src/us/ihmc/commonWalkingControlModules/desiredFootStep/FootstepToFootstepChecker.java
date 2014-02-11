9a6e88f0f6671d56d9aa8a33499efb42333c4f96
Marshall Floyd <mfloyd@ihmc.us>
2014-02-11 01:44:51 +0000

    DRC-965: Changed FootstepToFootstep checker to use a circular radius metric instead of square area in world coordinates. RangeOfStepGraphics and RangeOfStepGraphicsManager changed to be consistent with this area.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredFootStep/FootstepToFootstepChecker.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredFootStep/FootstepToFootstepChecker.java
@@ -19,14 +19,14 @@ public class FootstepToFootstepChecker
    private static FramePose startingFramePose = new FramePose(worldFrame);
    private static FramePose endingFramePose = new FramePose(worldFrame);
 
-   private static double MAXIMUM_DELTA_X = 0.6;
-   private static double MAXIMUM_DELTA_Y = 0.6;
+   private static double MAXIMUM_DELTA_R = 0.6;
+   private static double MAXIMUM_DELTA_R_SQUARED = MAXIMUM_DELTA_R*MAXIMUM_DELTA_R;
    private static double MAXIMUM_DELTA_Z = 0.3;
 
    //TODO: The maximum delta pitch/roll are smaller than risk regions for a single raw step (see DRCOperatorInterfaceConfig)  
-   private static double MAXIMUM_ROLL = Math.toRadians(15.0);
+   private static double MAXIMUM_ROLL = Math.toRadians(15.0);//0.26
    private static double MAXIMUM_PITCH = Math.toRadians(15.0);
-   private static double MAXIMUM_YAW = Math.toRadians(90.0);
+   private static double MAXIMUM_YAW = Math.toRadians(90.0);//1.57
 
    public static boolean isFootstepToFootstepChangeLarge(Footstep startingFootstep, Footstep endingFootstep)
    {
@@ -36,25 +36,29 @@ public class FootstepToFootstepChecker
       FrameVector startToEnd = new FrameVector(endingFramePose.getPositionCopy());
       startToEnd.sub(startingFramePose.getPositionCopy());
 
-      //TODO: THIS is in global coordinates, but should be in the stance foot (startingFootstep) frame.
-      //TODO: This is a rectangular region, The rectangular region allows steps to be farther away at an angle.
-      //       A circular region would be the same in global or local coordinates. (added benefit) 
       //TODO: This has equal X and Y dimensions, but if allowable distance is direction dependent, then it needs to be a different shape (e.g. elliptical).
+      //      This is in global coordinates, but should be in the stance foot (startingFootstep) frame if changed to non-circular.
       //TODO: This function will not always notify of values that are "Outside allowable range" resulting in an 'OutsideSerializableValuesException' when sending footsteps.
-      //       Make these two checks consistent. (Can also make consistent with single step validity graphics)
+      //       us.ihmc.utilities.fixedPointRepresentation.OutsideSerializableValuesException
+      //     From FootstepDataListSerializer:
+      //      public static final double[] XYZ_MAX = { 1.0, 1.0, 0.5 };
+      //      public static final double[] XYZ_MIN = { -1.0, -1.0, -1.5 };
+      //     This range is based on subsequent footsteps, not stance/step pairs. So this is most likely violated when making large steps with the same side to opposite extremes
+      //      For example -0.6 to 0.6 is a 1.2 difference which is larger than the allowable serializable range. This is unlikely to be done in practice.
+
       double deltaX = Math.abs(startToEnd.getX());
       double deltaY = Math.abs(startToEnd.getY());
       double deltaZ = Math.abs(startToEnd.getZ());
 
+      double deltaRSquared = deltaX*deltaX + deltaY*deltaY;
+      
       double deltaYaw = Math.abs(AngleTools.computeAngleDifferenceMinusPiToPi(endingFramePose.getYaw(), startingFramePose.getYaw()));
       double deltaPitch = Math.abs(AngleTools.computeAngleDifferenceMinusPiToPi(endingFramePose.getPitch(), startingFramePose.getPitch()));
       double deltaRoll = Math.abs(AngleTools.computeAngleDifferenceMinusPiToPi(endingFramePose.getRoll(), startingFramePose.getRoll()));
       
       boolean ret;
 
-      if (deltaX > MAXIMUM_DELTA_X)
-         ret = true;
-      else if (deltaY > MAXIMUM_DELTA_Y)
+      if (deltaRSquared > MAXIMUM_DELTA_R_SQUARED)
          ret = true;
       else if (deltaZ > MAXIMUM_DELTA_Z)
          ret = true;
@@ -67,7 +71,7 @@ public class FootstepToFootstepChecker
       else
          ret = false;
 
-      printIfDebug(ret, deltaX, deltaY, deltaZ, deltaYaw, deltaPitch, deltaRoll);
+      printIfDebug(ret, deltaX, deltaY, deltaZ, deltaRSquared, deltaYaw, deltaPitch, deltaRoll);
 
       return ret;
    }
@@ -106,15 +110,15 @@ public class FootstepToFootstepChecker
       }
    }
    
-   private static void printIfDebug(boolean ret, double x, double y, double z, double yaw, double pitch, double roll)
+   private static void printIfDebug(boolean ret, double x, double y, double z, double r2, double yaw, double pitch, double roll)
    {
       if (!DEBUG)
          return;
 
       if (ret)
-         System.err.println("FootstepToFootstepChecker: " + ret + ", " + x + ", " + y + ", " + z + ", " + yaw + ", " + pitch + ", " + roll);
+         System.err.println("FootstepToFootstepChecker: " + ret + ", x" + x + ", y" + y + ", z" + z + ", r" + Math.sqrt(r2) + ", y" + yaw + ", p" + pitch + ", r" + roll);
       else
-         System.out.println("FootstepToFootstepChecker: " + ret + ", " + x + ", " + y + ", " + z + ", " + yaw + ", " + pitch + ", " + roll);
+         System.out.println("FootstepToFootstepChecker: " + ret + ", x" + x + ", y" + y + ", z" + z + ", r" + Math.sqrt(r2) + ", y" + yaw + ", p" + pitch + ", r" + roll);
    }
 
    
