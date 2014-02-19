5f468814c84a76f388c4ee1bcafd002dfd67b2e3
Sylvain Bertrand <sbertrand@ihmc.us>
2014-02-19 17:49:59 +0000

    DRC-999: Created JointStateUpdater that reads the joint position/velocity sensors and updates the robot internal state.

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/stateEstimation/kinematicsBasedStateEstimator/JointStateUpdater.java
@@ -0,0 +1,110 @@
+package us.ihmc.darpaRoboticsChallenge.stateEstimation.kinematicsBasedStateEstimator;
+
+import java.util.LinkedHashMap;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+
+import us.ihmc.sensorProcessing.simulatedSensors.JointAndIMUSensorMap;
+import us.ihmc.sensorProcessing.stateEstimation.JointAndIMUSensorDataSource;
+import us.ihmc.sensorProcessing.stateEstimation.evaluation.FullInverseDynamicsStructure;
+import us.ihmc.utilities.screwTheory.InverseDynamicsJoint;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+import us.ihmc.utilities.screwTheory.RigidBody;
+import us.ihmc.utilities.screwTheory.ScrewTools;
+import us.ihmc.utilities.screwTheory.SpatialAccelerationCalculator;
+import us.ihmc.utilities.screwTheory.TwistCalculator;
+
+/**
+ * JointStateUpdater simply reads the joint position/velocity sensors and updates the FullInverseDynamicsStructure.
+ * (Based on {@link us.ihmc.sensorProcessing.stateEstimation.JointStateFullRobotModelUpdater}.)
+ * @author Sylvain
+ *
+ */
+public class JointStateUpdater
+{
+   private final static boolean DEBUG = false;
+   
+   private final TwistCalculator twistCalculator;
+   private final SpatialAccelerationCalculator spatialAccelerationCalculator;
+   private final RigidBody rootBody;
+
+   private final OneDoFJoint[] oneDoFJoints;
+   private final JointAndIMUSensorDataSource jointAndIMUSensorDataSource;
+   private final JointAndIMUSensorMap sensorMap;
+   private final YoVariableRegistry registry;
+   private final LinkedHashMap<OneDoFJoint, DoubleYoVariable> estimatedJointPositions;
+
+   public JointStateUpdater(FullInverseDynamicsStructure inverseDynamicsStructure, JointAndIMUSensorDataSource jointAndIMUSensorDataSource, YoVariableRegistry parentRegistry)
+   {
+      twistCalculator = inverseDynamicsStructure.getTwistCalculator();
+      spatialAccelerationCalculator = inverseDynamicsStructure.getSpatialAccelerationCalculator();
+      rootBody = twistCalculator.getRootBody();
+      
+      this.jointAndIMUSensorDataSource = jointAndIMUSensorDataSource;
+      this.sensorMap = jointAndIMUSensorDataSource.getSensorMap();
+      
+      InverseDynamicsJoint[] joints = ScrewTools.computeSupportAndSubtreeJoints(inverseDynamicsStructure.getRootJoint().getSuccessor());
+      this.oneDoFJoints = ScrewTools.filterJoints(joints, OneDoFJoint.class);
+
+      for (int i = 0; i < oneDoFJoints.length; i++)
+      {
+         OneDoFJoint oneDoFJoint = oneDoFJoints[i];
+         if (sensorMap.getJointPositionSensorPort(oneDoFJoint) == null)
+         {
+            throw new RuntimeException("sensorMap.getJointPositionSensorPort(oneDoFJoint) == null. oneDoFJoint = " + oneDoFJoint);
+         }
+      }
+      
+      if (DEBUG)
+      {
+         estimatedJointPositions = new LinkedHashMap<OneDoFJoint, DoubleYoVariable>();
+         registry = new YoVariableRegistry(getClass().getSimpleName());
+         
+         for (int i = 0; i < oneDoFJoints.length; i++)
+         {
+            OneDoFJoint oneDoFJoint = oneDoFJoints[i];
+            DoubleYoVariable jointPosition = new DoubleYoVariable("estimated_q_" + oneDoFJoint.getName(), registry);
+            estimatedJointPositions.put(oneDoFJoint, jointPosition);
+         }
+         
+         parentRegistry.addChild(registry);
+      }
+      else
+      {
+         estimatedJointPositions = null;
+         registry = null;
+      }
+   }
+
+   public void initialize()
+   {
+      jointAndIMUSensorDataSource.initialize();
+      updateJointState();
+   }
+
+   public void updateJointState()
+   {
+      jointAndIMUSensorDataSource.startComputation();
+      jointAndIMUSensorDataSource.waitUntilComputationIsDone();
+      
+      for (int i = 0; i < oneDoFJoints.length; i++)
+      {
+         OneDoFJoint oneDoFJoint = oneDoFJoints[i];
+         
+         double positionSensorData = sensorMap.getJointPositionSensorPort(oneDoFJoint).getData()[0];
+         double velocitySensorData = sensorMap.getJointVelocitySensorPort(oneDoFJoint).getData()[0];
+
+         if (DEBUG)
+            estimatedJointPositions.get(oneDoFJoint).set(positionSensorData);
+         
+         oneDoFJoint.setQ(positionSensorData);
+         oneDoFJoint.setQd(velocitySensorData);
+         oneDoFJoint.setQdd(oneDoFJoint.getQddDesired());
+      }
+
+      rootBody.updateFramesRecursively();
+      twistCalculator.compute();
+      spatialAccelerationCalculator.compute();
+   }
+}
