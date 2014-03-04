8acbdef58aeb13790215748ec308be644d6a714d
Georg Wiedebach <gwiedebach@ihmc.us>
2014-03-04 16:22:35 +0000

    DRC-1036: removed a first bunch of static DRCRobotModel calls.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCDoubleElevationChangeSimulation.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCDoubleElevationChangeSimulation.java
@@ -1,5 +1,7 @@
 package us.ihmc.darpaRoboticsChallenge;
 
+import java.util.Arrays;
+
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.commonWalkingControlModules.automaticSimulationRunner.AutomaticSimulationRunner;
 import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
@@ -8,7 +10,10 @@ import us.ihmc.darpaRoboticsChallenge.drcRobot.PlainDRCRobot;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.SquaredUpDRCRobotInitialSetup;
 import us.ihmc.projectM.R2Sim02.initialSetup.RobotInitialSetup;
 
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
 import com.yobotics.simulationconstructionset.BooleanYoVariable;
 import com.yobotics.simulationconstructionset.DoubleYoVariable;
 import com.yobotics.simulationconstructionset.util.DoubleStepGroundProfile;
@@ -17,14 +22,41 @@ public class DRCDoubleElevationChangeSimulation
 {   
    public static void main(String[] args) throws JSAPException
    {
+      // Flag to set robot model
+      JSAP jsap = new JSAP();
+      FlaggedOption robotModel = new FlaggedOption("robotModel").setLongFlag("model").setShortFlag('m').setRequired(true).setStringParser(JSAP.STRING_PARSER);
+      robotModel.setHelp("Robot models: " + Arrays.toString(DRCRobotModel.values()));
+      
+      DRCRobotModel model;
+      try
+      {
+         jsap.registerParameter(robotModel);
+
+         JSAPResult config = jsap.parse(args);
+
+         if (config.success())
+         {
+            model = DRCRobotModel.valueOf(config.getString("robotModel"));
+         }
+         else
+         {
+            System.out.println("Enter a robot model.");
+            return;
+         }
+      }
+      catch (JSAPException e)
+      {
+         e.printStackTrace();
+         return;
+      }
+      
       final double stepHeight = 0.05;
       final boolean stepUp = false;
       
       AutomaticSimulationRunner automaticSimulationRunner = null;
       DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false);
 
-      DRCRobotModel robotModel= DRCRobotModel.getDefaultRobotModel();
-      DRCRobotInterface robotInterface = new PlainDRCRobot(robotModel, false);
+      DRCRobotInterface robotInterface = new PlainDRCRobot(model, false);
       final double groundHeight = 0.0;
       double elevationChange = 0.0;
       if(stepUp) elevationChange = Math.abs(stepHeight);
@@ -35,13 +67,13 @@ public class DRCDoubleElevationChangeSimulation
       boolean useVelocityAndHeadingScript = false;
       boolean cheatWithGroundHeightAtForFootstep = false;
 
-      WalkingControllerParameters drcControlParameters = robotModel.getWalkingControlParamaters();
-      ArmControllerParameters armControllerParameters = robotModel.getArmControllerParameters();
+      WalkingControllerParameters drcControlParameters = model.getWalkingControlParamaters();
+      ArmControllerParameters armControllerParameters = model.getArmControllerParameters();
       
       DRCFlatGroundWalkingTrack drcFlatGroundWalkingTrack = new DRCFlatGroundWalkingTrack(drcControlParameters, armControllerParameters, robotInterface, 
             robotInitialSetup, guiInitialSetup, scsInitialSetup, 
             useVelocityAndHeadingScript, automaticSimulationRunner, DRCConfigParameters.CONTROL_DT, 16000,
-            cheatWithGroundHeightAtForFootstep,robotModel);
+            cheatWithGroundHeightAtForFootstep,model);
       
       ((BooleanYoVariable) drcFlatGroundWalkingTrack.getSimulationConstructionSet().getVariable("walk")).set(true);
       ((DoubleYoVariable) drcFlatGroundWalkingTrack.getSimulationConstructionSet().getVariable("desiredVelocityX")).set(1.0);
