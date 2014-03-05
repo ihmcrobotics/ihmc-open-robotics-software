f9dc0a1fc53ada52a50b8e57829dacab1ca01582
Douglas Stephen <dstephen@ihmc.us>
2014-03-05 22:10:25 +0000

    DRC-1042: Setting up force control slider board stuff.  Probably won't be able to test any time soon.

--- src/us/ihmc/valkyrie/controllers/ValkyrieSliderBoard.java
+++ src/us/ihmc/valkyrie/controllers/ValkyrieSliderBoard.java
@@ -6,9 +6,6 @@ import us.ihmc.SdfLoader.GeneralizedSDFRobotModel;
 import us.ihmc.atlas.visualization.SliderBoardFactory;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.CommonNames;
 
-import java.util.EnumMap;
-import java.util.HashMap;
-
 /**
  * Created by dstephen on 2/28/14.
  */
@@ -17,7 +14,8 @@ public class ValkyrieSliderBoard
    private final EnumYoVariable<ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints> selectedJoint, remoteSelectedJoint;
 
    @SuppressWarnings("unchecked")
-   public ValkyrieSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
+   public ValkyrieSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel,
+                              boolean forceControl)
    {
       selectedJoint = new EnumYoVariable<>("selectedJoint", registry, ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints.class);
       selectedJoint.set(ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints.RightKneeExtensor);
@@ -26,7 +24,60 @@ public class ValkyrieSliderBoard
 
       final SliderBoardConfigurationManager sliderBoardConfigurationManager = new SliderBoardConfigurationManager(scs);
 
+      if (!forceControl)
+      {
+         setupSliderBoardForOnBoardPositionControl(registry, generalizedSDFRobotModel, sliderBoardConfigurationManager);
+      }
+      else
+      {
+         setupSliderBoardForForceControl(registry, generalizedSDFRobotModel, sliderBoardConfigurationManager);
+      }
 
+      sliderBoardConfigurationManager.loadConfiguration(selectedJoint.getEnumValue().toString());
+   }
+
+   private void setupSliderBoardForForceControl(YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel,
+           final SliderBoardConfigurationManager sliderBoardConfigurationManager)
+   {
+      for (ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints jointId :
+              ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints.values())
+      {
+         String jointName = jointId.toString();
+         String pdControllerBaseName = jointName + "ValkyrieJointPDController";
+
+         // knobs
+
+         sliderBoardConfigurationManager.setKnob(1, selectedJoint, 0, ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints.values().length - 1);
+
+         // sliders
+         sliderBoardConfigurationManager.setSlider(1, pdControllerBaseName + "_q_d", registry,
+                 generalizedSDFRobotModel.getJointHolder(jointName).getLowerLimit(), generalizedSDFRobotModel.getJointHolder(jointName).getUpperLimit());
+         sliderBoardConfigurationManager.setSlider(2, "kp_" + pdControllerBaseName, registry, 0.0, 200.0);
+         sliderBoardConfigurationManager.setSlider(3, "kd_" + pdControllerBaseName, registry, 0.0, 200.0);
+
+         sliderBoardConfigurationManager.saveConfiguration(jointId.toString());
+      }
+
+
+      selectedJoint.addVariableChangedListener(new VariableChangedListener()
+      {
+         @Override
+         public void variableChanged(YoVariable v)
+         {
+            System.out.println("loading configuration " + selectedJoint.getEnumValue());
+            sliderBoardConfigurationManager.loadConfiguration(selectedJoint.getEnumValue().toString());
+
+            if (remoteSelectedJoint != null)
+            {
+               remoteSelectedJoint.set(selectedJoint.getEnumValue());
+            }
+         }
+      });
+   }
+
+   private void setupSliderBoardForOnBoardPositionControl(YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel,
+           final SliderBoardConfigurationManager sliderBoardConfigurationManager)
+   {
       for (ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints jointId :
               ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints.values())
       {
@@ -37,7 +88,8 @@ public class ValkyrieSliderBoard
          sliderBoardConfigurationManager.setKnob(1, selectedJoint, 0, ValkyrieSliderBoardController.ValkyrieSliderBoardSelectableJoints.values().length - 1);
 
          // sliders
-         sliderBoardConfigurationManager.setSlider(1, jointName + CommonNames.q_d, registry, generalizedSDFRobotModel.getJointHolder(jointName).getLowerLimit(), generalizedSDFRobotModel.getJointHolder(jointName).getUpperLimit());
+         sliderBoardConfigurationManager.setSlider(1, jointName + CommonNames.q_d, registry,
+                 generalizedSDFRobotModel.getJointHolder(jointName).getLowerLimit(), generalizedSDFRobotModel.getJointHolder(jointName).getUpperLimit());
          sliderBoardConfigurationManager.setSlider(2, jointName + CommonNames.qd_d, registry, -9, 9);
 
          sliderBoardConfigurationManager.saveConfiguration(jointId.toString());
@@ -58,21 +110,33 @@ public class ValkyrieSliderBoard
             }
          }
       });
-
-      sliderBoardConfigurationManager.loadConfiguration(selectedJoint.getEnumValue().toString());
    }
 
-   private static final SliderBoardFactory factory = new SliderBoardFactory()
+   private static final SliderBoardFactory turboDriverPositionControlFactory = new SliderBoardFactory()
    {
       @Override
       public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
       {
-         new ValkyrieSliderBoard(scs, registry, generalizedSDFRobotModel);
+         new ValkyrieSliderBoard(scs, registry, generalizedSDFRobotModel, false);
       }
    };
 
-   public static SliderBoardFactory getFactory()
+   public static SliderBoardFactory getTurboDriverPositionControlFactory()
    {
-      return factory;
+      return turboDriverPositionControlFactory;
+   }
+
+   private static final SliderBoardFactory forceControlFactory = new SliderBoardFactory()
+   {
+      @Override
+      public void makeSliderBoard(SimulationConstructionSet scs, YoVariableRegistry registry, GeneralizedSDFRobotModel generalizedSDFRobotModel)
+      {
+         new ValkyrieSliderBoard(scs, registry, generalizedSDFRobotModel, true);
+      }
+   };
+
+   public static SliderBoardFactory getForceControlFactory()
+   {
+      return forceControlFactory;
    }
 }
--- src/us/ihmc/valkyrie/visualizer/RemoteValkyrieVisualizer.java
+++ src/us/ihmc/valkyrie/visualizer/RemoteValkyrieVisualizer.java
@@ -19,7 +19,9 @@ public class RemoteValkyrieVisualizer
 {
    public static final String defaultHost = ValkyrieNetworkParameters.CONTROL_COMPUTER_HOST;
    public static final int defaultPort = ValkyrieNetworkParameters.VARIABLE_SERVER_PORT;
-   
+
+   private static final boolean USE_FORCE_CONTROL = false;
+
    public RemoteValkyrieVisualizer(String host, int port, int bufferSize)
    {
       System.out.println("Connecting to host " + host);
@@ -27,11 +29,21 @@ public class RemoteValkyrieVisualizer
       JaxbSDFLoader robotLoader = ValkyrieSDFLoader.loadValkyrieRobot(false);
 
 
-      SliderBoardFactory sliderBoardFactory = ValkyrieSliderBoard.getFactory();
-      SliderBoardControllerListener scsYoVariablesUpdatedListener = new ValkyrieSliderBoardControllerListener(robotLoader, jointMap, bufferSize, sliderBoardFactory);
-      
-//      SCSYoVariablesUpdatedListener scsYoVariablesUpdatedListener = new SCSYoVariablesUpdatedListener(robot, 16000);
-      
+      SliderBoardFactory sliderBoardFactory;
+      if (USE_FORCE_CONTROL)
+      {
+         sliderBoardFactory = ValkyrieSliderBoard.getForceControlFactory();
+      }
+      else
+      {
+         sliderBoardFactory = ValkyrieSliderBoard.getTurboDriverPositionControlFactory();
+      }
+
+      SliderBoardControllerListener scsYoVariablesUpdatedListener = new ValkyrieSliderBoardControllerListener(robotLoader, jointMap, bufferSize,
+                                                                       sliderBoardFactory);
+
+//    SCSYoVariablesUpdatedListener scsYoVariablesUpdatedListener = new SCSYoVariablesUpdatedListener(robot, 16000);
+
       YoVariableClient client = new YoVariableClient(host, port, scsYoVariablesUpdatedListener, "remote", false);
       client.start();
    }
@@ -40,23 +52,23 @@ public class RemoteValkyrieVisualizer
    {
       int bufferSize = 16384;
       JSAP jsap = new JSAP();
-      
-      FlaggedOption hostOption = new FlaggedOption("host").setStringParser(JSAP.STRING_PARSER).setRequired(false).setLongFlag("host").setShortFlag('L').setDefault(
-            defaultHost);
-      FlaggedOption portOption = new FlaggedOption("port").setStringParser(JSAP.INTEGER_PARSER).setRequired(false).setLongFlag("port").setShortFlag('p')
-            .setDefault(String.valueOf(defaultPort));
-      
+
+      FlaggedOption hostOption =
+         new FlaggedOption("host").setStringParser(JSAP.STRING_PARSER).setRequired(false).setLongFlag("host").setShortFlag('L').setDefault(defaultHost);
+      FlaggedOption portOption = new FlaggedOption("port").setStringParser(JSAP.INTEGER_PARSER).setRequired(false).setLongFlag("port").setShortFlag(
+                                     'p').setDefault(String.valueOf(defaultPort));
+
       jsap.registerParameter(hostOption);
       jsap.registerParameter(portOption);
-      
+
       JSAPResult config = jsap.parse(args);
-      
+
       if (config.success())
       {
          String host = config.getString("host");
          int port = config.getInt("port");
-         
-         new RemoteValkyrieVisualizer(host, port, bufferSize);         
+
+         new RemoteValkyrieVisualizer(host, port, bufferSize);
       }
       else
       {
@@ -66,8 +78,8 @@ public class RemoteValkyrieVisualizer
          System.err.println();
          System.exit(1);
       }
-      
-      
-      
+
+
+
    }
 }
