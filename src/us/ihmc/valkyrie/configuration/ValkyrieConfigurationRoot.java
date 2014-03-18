ce428f2a29330ee6603dbca48ee3ca99c1543559
Douglas Stephen <dstephen@ihmc.us>
2014-03-18 19:28:16 +0000

    DRC-1079: Bench setup code implemented.  Can track loopback variable for timing tests.  Had to expand the APIBuilder to accomdate the bench versions of the actuators.

--- src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
+++ src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
@@ -5,10 +5,11 @@ import us.ihmc.robotSide.SideDependentList;
 public class ValkyrieConfigurationRoot
 {
    public static final String[] API_BUILDER_INPUT_FILES = new String[] {"TurbodriverAPI_DRCv3.xml", "TurbodriverAPI_DRCv4.xml", "TurbodriverAPI_DRCv4_linear.xml", "FingerAPI_DRCv1.xml",
-         "WristAPI_DRCv1.xml"};
+         "WristAPI_DRCv1.xml", "TurbodriverAPI_DRCv4_bench.xml"};
 
 
    public static final SideDependentList<String> FOOT_SENSOR_FILES_BASENAMES = new SideDependentList<>("FT14020", "FT14175");
 
-   public static final String SCHEDULE_FILE = "main_sim.yaml";
+   public static final String SCHEDULE_FILE = "main_bench.yaml";
+   public static final String URDF_FILE = "models/V1_hw_bench.urdf";
 }
