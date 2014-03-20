172fce88021c49b3fede4e309527f9e3fc6a0fbb
Jesper Smith <jsmith@ihmc.us>
2014-03-20 20:07:41 +0000

    NONE: Moved network parameters to configuration package.  #fixBamboo

--- src/us/ihmc/valkyrie/visualizer/RemoteValkyrieVisualizer.java
+++ src/us/ihmc/valkyrie/visualizer/RemoteValkyrieVisualizer.java
@@ -1,11 +1,6 @@
 package us.ihmc.valkyrie.visualizer;
 
 
-import com.martiansoftware.jsap.FlaggedOption;
-import com.martiansoftware.jsap.JSAP;
-import com.martiansoftware.jsap.JSAPException;
-import com.martiansoftware.jsap.JSAPResult;
-
 import us.ihmc.SdfLoader.JaxbSDFLoader;
 import us.ihmc.atlas.visualization.SliderBoardFactory;
 import us.ihmc.darpaRoboticsChallenge.valkyrie.ValkyrieJointMap;
@@ -13,7 +8,12 @@ import us.ihmc.darpaRoboticsChallenge.valkyrie.ValkyrieSDFLoader;
 import us.ihmc.robotDataCommunication.YoVariableClient;
 import us.ihmc.robotDataCommunication.visualizer.SCSYoVariablesUpdatedListener;
 import us.ihmc.valkyrie.configuration.ValkyrieNetworkParameters;
-import us.ihmc.valkyrie.controllers.ValkyrieSliderBoard;
+import us.ihmc.valkyrie.controllers.StandPrepSliderBoardFactory;
+
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
 
 public class RemoteValkyrieVisualizer
 {
@@ -26,7 +26,7 @@ public class RemoteValkyrieVisualizer
       ValkyrieJointMap jointMap = new ValkyrieJointMap();
       JaxbSDFLoader robotLoader = ValkyrieSDFLoader.loadValkyrieRobot(false);
 
-      SliderBoardFactory sliderBoardFactory = ValkyrieSliderBoard.getDefaultSliderBoardFactory();
+      SliderBoardFactory sliderBoardFactory = new StandPrepSliderBoardFactory();
 
       SCSYoVariablesUpdatedListener scsYoVariablesUpdatedListener = new ValkyrieSliderBoardControllerListener(robotLoader, jointMap, bufferSize, sliderBoardFactory);
 
