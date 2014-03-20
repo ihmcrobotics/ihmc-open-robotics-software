f89d9f76046ced1be1691881d328ca69766e0773
Stephen Hart <StephenHart@unknown>
2014-03-20 20:57:17 +0000

    NONE: added odometry (ros) pose publisher in ValkyrieDemo program

---
+++ IHMCROSTools/src/us/ihmc/utilities/ros/RosPoseStampedPublisher.java
@@ -0,0 +1,76 @@
+package us.ihmc.utilities.ros;
+
+import geometry_msgs.Pose;
+import geometry_msgs.PoseStamped;
+import org.ros.message.Time;
+import std_msgs.Header;
+
+import javax.vecmath.Vector3d;
+import javax.vecmath.Quat4d;
+
+
+public class RosPoseStampedPublisher extends RosTopicPublisher<PoseStamped>
+{
+   private final PoseStamped initialValue;
+   private int seq = 0;
+
+   public RosPoseStampedPublisher(boolean latched)
+   {
+      this(latched, null);
+   }
+
+   public RosPoseStampedPublisher(boolean latched, PoseStamped initialValue)
+   {
+      super(PoseStamped._TYPE, latched);
+      this.initialValue = initialValue;
+   }
+   
+   @Override
+   public void connected()
+   {
+      if(initialValue != null)
+      {
+         publish(initialValue);
+      }
+   }
+
+
+   public void publish(String frameID, Vector3d pos, Quat4d rot, double t)
+   {
+      PoseStamped message = getMessage();
+      Header header = message.getHeader();
+
+      header.setStamp(new Time(System.currentTimeMillis() / 1000.0 ));
+      System.out.println("time: " + System.currentTimeMillis() / 1000.0);
+
+      header.setFrameId(frameID);
+      header.setSeq(seq++);
+      message.setHeader(header);
+
+
+      if (pos != null) {
+          message.getPose().getPosition().setX(pos.getX());
+          message.getPose().getPosition().setY(pos.getY());
+          message.getPose().getPosition().setZ(pos.getZ());
+      } else {
+          message.getPose().getPosition().setX(0.0);
+          message.getPose().getPosition().setY(0.0);
+          message.getPose().getPosition().setZ(0.0);
+
+      }
+
+      if (rot != null) {
+          message.getPose().getOrientation().setX(rot.getX());
+          message.getPose().getOrientation().setY(rot.getY());
+          message.getPose().getOrientation().setZ(rot.getZ());
+          message.getPose().getOrientation().setW(rot.getW());
+      } else {
+          message.getPose().getOrientation().setX(0.0);
+          message.getPose().getOrientation().setY(0.0);
+          message.getPose().getOrientation().setZ(0.0);
+          message.getPose().getOrientation().setW(1.0);
+      }
+
+      publish(message);
+   }
+}
