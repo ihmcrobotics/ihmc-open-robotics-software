b5fda318951098d9cc5d169aac5a1078d490ab42
Sylvain Bertrand <sbertrand@ihmc.us>
2014-03-25 22:57:44 +0000

    DRC-1053: Big refactor for StateEstimatoParameters. RUN ATLAS BEFORE AND AFTER THIS COMMIT, please please please.

--- Acsell/src/us/ihmc/acsell/ACSELLStateEstimatorParameters.java
+++ Acsell/src/us/ihmc/acsell/ACSELLStateEstimatorParameters.java
@@ -1,44 +1,120 @@
 package us.ihmc.acsell;
 
-import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
-import us.ihmc.darpaRoboticsChallenge.stateEstimation.StateEstimatorParameters;
+import us.ihmc.darpaRoboticsChallenge.stateEstimation.DRCSimulatedSensorNoiseParameters;
 import us.ihmc.sensorProcessing.simulatedSensors.SensorFilterParameters;
+import us.ihmc.sensorProcessing.simulatedSensors.SensorNoiseParameters;
 import us.ihmc.sensorProcessing.stateEstimation.PointMeasurementNoiseParameters;
+import us.ihmc.sensorProcessing.stateEstimation.StateEstimatorParameters;
 
-public class ACSELLStateEstimatorParameters implements StateEstimatorParameters {
-	private final boolean runningOnRealRobot;
+public class ACSELLStateEstimatorParameters implements StateEstimatorParameters
+{
+   private final boolean runningOnRealRobot;
 
-	public ACSELLStateEstimatorParameters(boolean runningOnRealRobot) {
-		this.runningOnRealRobot = runningOnRealRobot;
-	}
+   private final double estimatorDT;
 
+   private final boolean useKinematicsBasedStateEstimator = true;
+   private final boolean assumePerfectIMU = true;
 
+   private final double jointVelocitySlopTimeForBacklashCompensation;
 
-	@Override
-	public SensorFilterParameters getSensorFilterParameters(double estimateDT) {
-		return new SensorFilterParameters(
-				DRCConfigParameters.JOINT_POSITION_FILTER_FREQ_HZ, DRCConfigParameters.JOINT_VELOCITY_FILTER_FREQ_HZ, 
-				DRCConfigParameters.ORIENTATION_FILTER_FREQ_HZ, DRCConfigParameters.ANGULAR_VELOCITY_FILTER_FREQ_HZ,
-				DRCConfigParameters.LINEAR_ACCELERATION_FILTER_FREQ_HZ, estimateDT);
-	}
+   private final double jointPositionFilterFrequencyHz;
+   private final double jointVelocityFilterFrequencyHz;
+   private final double orientationFilterFrequencyHz;
+   private final double angularVelocityFilterFrequencyHz;
+   private final double linearAccelerationFilterFrequencyHz;
 
-	@Override
-	public boolean getAssumePerfectImu() {
-		return true;
-	}
+   // State Estimator Filter Parameters
+   private final double pointVelocityXYMeasurementStandardDeviation;
+   private final double pointVelocityZMeasurementStandardDeviation;
 
-	@Override
-	public boolean getUseSimplePelvisPositionEstimator() {
-		return true;
-	}
+   private final double pointPositionXYMeasurementStandardDeviation;
+   private final double pointPositionZMeasurementStandardDeviation;
 
-	@Override
-	public PointMeasurementNoiseParameters getPointMeasurementNoiseParameters() {
-		return new  PointMeasurementNoiseParameters(
-				DRCConfigParameters.pointVelocityXYMeasurementStandardDeviation,
-				DRCConfigParameters.pointVelocityZMeasurementStandardDeviation,
-				DRCConfigParameters.pointPositionXYMeasurementStandardDeviation,
-				DRCConfigParameters.pointPositionZMeasurementStandardDeviation);		      
-	}
+   private final SensorFilterParameters sensorFilterParameters;
 
+   private final PointMeasurementNoiseParameters pointMeasurementNoiseParameters;
+
+   //      DRCSimulatedSensorNoiseParameters.createNoiseParametersForEstimatorJerryTuning();
+   private final SensorNoiseParameters sensorNoiseParameters = DRCSimulatedSensorNoiseParameters
+         .createNoiseParametersForEstimatorJerryTuningSeptember2013();
+
+   public ACSELLStateEstimatorParameters(boolean runningOnRealRobot, double estimatorDT)
+   {
+      this.runningOnRealRobot = runningOnRealRobot;
+
+      this.estimatorDT = estimatorDT;
+
+      final double defaultFilterBreakFrequency;
+
+      if (!runningOnRealRobot)
+      {
+         defaultFilterBreakFrequency = Double.POSITIVE_INFINITY;
+      }
+      else
+      {
+         defaultFilterBreakFrequency = 16.0;
+      }
+
+      jointPositionFilterFrequencyHz = defaultFilterBreakFrequency;
+      jointVelocityFilterFrequencyHz = defaultFilterBreakFrequency;
+      orientationFilterFrequencyHz = defaultFilterBreakFrequency;
+      angularVelocityFilterFrequencyHz = defaultFilterBreakFrequency;
+      linearAccelerationFilterFrequencyHz = defaultFilterBreakFrequency;
+
+      jointVelocitySlopTimeForBacklashCompensation = 0.03;
+
+      pointVelocityXYMeasurementStandardDeviation = 2.0;
+      pointVelocityZMeasurementStandardDeviation = 2.0;
+
+      pointPositionXYMeasurementStandardDeviation = 0.1;
+      pointPositionZMeasurementStandardDeviation = 0.1;
+
+      sensorFilterParameters = new SensorFilterParameters(jointPositionFilterFrequencyHz, jointVelocityFilterFrequencyHz, orientationFilterFrequencyHz,
+            angularVelocityFilterFrequencyHz, linearAccelerationFilterFrequencyHz, jointVelocitySlopTimeForBacklashCompensation, estimatorDT);
+
+      pointMeasurementNoiseParameters = new PointMeasurementNoiseParameters(pointVelocityXYMeasurementStandardDeviation,
+            pointVelocityZMeasurementStandardDeviation, pointPositionXYMeasurementStandardDeviation, pointPositionZMeasurementStandardDeviation);
+   }
+
+   @Override
+   public SensorFilterParameters getSensorFilterParameters()
+   {
+      return sensorFilterParameters;
+   }
+
+   @Override
+   public boolean getAssumePerfectIMU()
+   {
+      return assumePerfectIMU;
+   }
+
+   @Override
+   public boolean useKinematicsBasedStateEstimator()
+   {
+      return useKinematicsBasedStateEstimator;
+   }
+
+   @Override
+   public PointMeasurementNoiseParameters getPointMeasurementNoiseParameters()
+   {
+      return pointMeasurementNoiseParameters;
+   }
+
+   @Override
+   public SensorNoiseParameters getSensorNoiseParameters()
+   {
+      return sensorNoiseParameters;
+   }
+
+   @Override
+   public double getEstimatorDT()
+   {
+      return estimatorDT;
+   }
+
+   @Override
+   public boolean isRunningOnRealRobot()
+   {
+      return runningOnRealRobot;
+   }
 }
--- Acsell/src/us/ihmc/acsell/BonoRobotModel.java
+++ Acsell/src/us/ihmc/acsell/BonoRobotModel.java
@@ -5,117 +5,129 @@ import java.io.InputStream;
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
-import us.ihmc.darpaRoboticsChallenge.AtlasRobotVersion;
 import us.ihmc.darpaRoboticsChallenge.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotJointMap;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotPhysicalProperties;
 import us.ihmc.darpaRoboticsChallenge.handControl.DRCHandModel;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
-import us.ihmc.darpaRoboticsChallenge.stateEstimation.StateEstimatorParameters;
 import us.ihmc.robotSide.RobotSide;
+import us.ihmc.sensorProcessing.stateEstimation.StateEstimatorParameters;
 
 import com.jme3.math.Transform;
 
-public class BonoRobotModel implements DRCRobotModel{
+public class BonoRobotModel implements DRCRobotModel
+{
 
-	private static Class<BonoRobotModel> thisClass = BonoRobotModel.class;
-	private static String[] resourceDirectories;
-	
-	@Override
-	public ArmControllerParameters getArmControllerParameters() {
-		return new ACSELLArmControlParameters();
-	}
+   private static Class<BonoRobotModel> thisClass = BonoRobotModel.class;
+   private static String[] resourceDirectories;
 
-	@Override
-	public WalkingControllerParameters getWalkingControlParamaters() {
-		return new ACSELLWalkingControllerParameters();
-	}
+   @Override
+   public ArmControllerParameters getArmControllerParameters()
+   {
+      return new ACSELLArmControlParameters();
+   }
 
-	@Override
-	public StateEstimatorParameters getStateEstimatorParameters(
-			boolean runningOnRealRobot) {
-		return new ACSELLStateEstimatorParameters(runningOnRealRobot);
-	}
+   @Override
+   public WalkingControllerParameters getWalkingControlParamaters()
+   {
+      return new ACSELLWalkingControllerParameters();
+   }
 
-	@Override
-	public DRCRobotPhysicalProperties getPhysicalProperties() {
-		return new ACSELLPhysicalProperties();
-	}
+   @Override
+   public StateEstimatorParameters getStateEstimatorParameters(boolean runningOnRealRobot, double estimatorDT)
+   {
+      return new ACSELLStateEstimatorParameters(runningOnRealRobot, estimatorDT);
+   }
 
-	@Override
-	public DRCRobotJointMap getJointMap(boolean addLoadsOfContactPoints,
-			boolean addLoadsOfContactPointsToFeetOnly) {
-		return new BonoJointMap(this);
-	}
+   @Override
+   public DRCRobotPhysicalProperties getPhysicalProperties()
+   {
+      return new ACSELLPhysicalProperties();
+   }
 
-	@Override
-	public boolean hasIRobotHands() {
-		return false;
-	}
+   @Override
+   public DRCRobotJointMap getJointMap(boolean addLoadsOfContactPoints, boolean addLoadsOfContactPointsToFeetOnly)
+   {
+      return new BonoJointMap(this);
+   }
 
-	@Override
-	public boolean hasArmExtensions() {
-		return false;
-	}
+   @Override
+   public boolean hasIRobotHands()
+   {
+      return false;
+   }
 
-	@Override
-	public boolean hasHookHands() {
-		return false;
-	}
+   @Override
+   public boolean hasArmExtensions()
+   {
+      return false;
+   }
 
-	@Override
-	public DRCHandModel getHandModel() {
-		return DRCHandModel.NONE;
-	}
+   @Override
+   public boolean hasHookHands()
+   {
+      return false;
+   }
 
-	@Override
-	public String getModelName() {
-		return "bono";
-	}
+   @Override
+   public DRCHandModel getHandModel()
+   {
+      return DRCHandModel.NONE;
+   }
 
-	@Override
-	public Transform getOffsetHandFromWrist(RobotSide side) {
-		return new Transform();
-	}
+   @Override
+   public String getModelName()
+   {
+      return "bono";
+   }
 
-	@Override
-	public String getSdfFile() {
-		return "models/axl/axl_description/bono/robots/bono.sdf";
-	}
+   @Override
+   public Transform getOffsetHandFromWrist(RobotSide side)
+   {
+      return new Transform();
+   }
 
-	public String[] getResourceDirectories() {
+   @Override
+   public String getSdfFile()
+   {
+      return "models/axl/axl_description/bono/robots/bono.sdf";
+   }
 
-		if(resourceDirectories == null)
-		{
-			resourceDirectories = new String[] {
-			         thisClass.getResource("models/axl/axl_description").getFile()
-				};
-		}
-		return resourceDirectories;
-	}
+   public String[] getResourceDirectories()
+   {
 
-	public InputStream getSdfFileAsStream() {
-		return thisClass.getResourceAsStream(getSdfFile());
-	}
+      if (resourceDirectories == null)
+      {
+         resourceDirectories = new String[] { thisClass.getResource("models/axl/axl_description").getFile() };
+      }
+      return resourceDirectories;
+   }
 
-	@Override
-	public RobotType getType() {
-		return RobotType.ACSELL;
-	}
-	
-	public String toString()
-	{
-		return "BONO";
-	}
-	
-	public DRCRobotInitialSetup<SDFRobot> getDefaultRobotInitialSetup(double groundHeight, double initialYaw)
-	   {
-	         return new BonoInitialSetup(groundHeight, initialYaw);
-	   }
+   public InputStream getSdfFileAsStream()
+   {
+      return thisClass.getResourceAsStream(getSdfFile());
+   }
 
-	@Override
-	public WalkingControllerParameters getMultiContactControllerParameters() {
-		return new ACSELLWalkingControllerParameters();
-	}
+   @Override
+   public RobotType getType()
+   {
+      return RobotType.ACSELL;
+   }
+
+   public String toString()
+   {
+      return "BONO";
+   }
+
+   public DRCRobotInitialSetup<SDFRobot> getDefaultRobotInitialSetup(double groundHeight, double initialYaw)
+   {
+      return new BonoInitialSetup(groundHeight, initialYaw);
+   }
+
+   @Override
+   public WalkingControllerParameters getMultiContactControllerParameters()
+   {
+      return new ACSELLWalkingControllerParameters();
+   }
 
 }
