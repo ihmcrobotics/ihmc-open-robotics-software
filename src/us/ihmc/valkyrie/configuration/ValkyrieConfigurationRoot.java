3d7e71a0fda636760f821bc5809ef69c88204059
Douglas Stephen <dstephen@ihmc.us>
2014-03-25 20:20:16 +0000

    NONE: Adding new schedule files and API builder files that expose the new low-level force control tuning parameters to the high level.  Still need to implement the appropriate slider board controller so we can tune low-level force control.

--- src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
+++ src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
@@ -4,14 +4,14 @@ import us.ihmc.robotSide.SideDependentList;
 
 public class ValkyrieConfigurationRoot
 {
-   public static final String[] API_BUILDER_INPUT_FILES = new String[] {"TurbodriverAPI_DRCv3.xml", "TurbodriverAPI_DRCv4.xml", "TurbodriverAPI_DRCv4_linear.xml", "FingerAPI_DRCv1.xml",
+   public static final String[] API_BUILDER_INPUT_FILES = new String[] {"TurbodriverAPI_DRCv3.xml", "TurbodriverAPI_DRCv4_ihmc.xml", "TurbodriverAPI_DRCv4_linear_ihmc.xml", "TurbodriverAPI_DRCv4.xml", "TurbodriverAPI_DRCv4_linear.xml", "FingerAPI_DRCv1.xml",
          "WristAPI_DRCv1.xml", "TurbodriverAPI_DRCv4_bench.xml"};
 
 
    public static final SideDependentList<String> FOOT_SENSOR_FILES_BASENAMES = new SideDependentList<>("FT14020", "FT14175");
 
-   public static final String SCHEDULE_FILE = "main_bench.yaml";
-   public static final String URDF_FILE = "models/V1_hw.urdf";
+   public static final String SCHEDULE_FILE = "main_ihmc.yaml";
+   public static final String URDF_FILE = "models/V1_hw_ihmc.urdf";
 
    public static final String BENCH_SCHEDULE_FILE = "main_bench.yaml";
    public static final String BENCH_URDF_FILE = "models/V1_hw_bench.urdf";
