b518f1fdb1d80639aa4f4f92b48dd2dd0a3f7a0c
Stephen Hart <StephenHart@unknown>
2014-03-27 20:22:20 +0000

    NONE: added rus pub for system clock

---
+++ IHMCROSTools/src/us/ihmc/utilities/ros/RosClockPublisher.java
@@ -0,0 +1,32 @@
+package us.ihmc.utilities.ros;
+import org.ros.message.Time;
+import rosgraph_msgs.Clock;
+
+public class RosClockPublisher extends RosTopicPublisher<Clock>
+{
+   public RosClockPublisher()
+   {
+       super(Clock._TYPE, false);
+   }
+
+   @Override
+   public void connected()
+   {
+   }
+
+   public void publish(Time t)
+   {
+       Clock message = getMessage();
+       message.setClock(t);
+       try
+       {
+            publish(message);
+       }
+       catch (RuntimeException e)
+       {
+           System.out.println("cant publish clock?");
+           e.printStackTrace();
+       }
+   }
+
+}
