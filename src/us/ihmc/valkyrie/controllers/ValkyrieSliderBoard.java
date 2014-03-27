037346362ad5ea167ea33df8436fca92fa070f97
Atlas Control Computer <AtlasControlComputer@unknown>
2014-03-27 19:19:28 +0000

    NONE: Changed the ranges on one of the sliders.

--- src/us/ihmc/valkyrie/controllers/ValkyrieSliderBoard.java
+++ src/us/ihmc/valkyrie/controllers/ValkyrieSliderBoard.java
@@ -180,7 +180,7 @@ public class ValkyrieSliderBoard
                            sliderBoardConfigurationManager.setSlider(2, "kp_" + pdControllerBaseName, registry, 0.0, 2000.0);
                            sliderBoardConfigurationManager.setSlider(3, "kd_" + pdControllerBaseName, registry, 0.0, 600.0);
                            sliderBoardConfigurationManager.setSlider(4, pdControllerBaseName + "_transitionFactor", registry, 0.0, 1.0);
-                           sliderBoardConfigurationManager.setSlider(5, pdControllerBaseName + "_tauDesired", registry, 0.0, 100.0);
+                           sliderBoardConfigurationManager.setSlider(5, pdControllerBaseName + "_tauDesired", registry, -100.0, 100.0);
 
                            sliderBoardConfigurationManager.setButton(1, pdControllerBaseName + "_useFunctionGenerator", registry);
                            sliderBoardConfigurationManager.setSlider(6, pdControllerBaseName + "_functionGeneratorAmplitude", registry, 0, 200);
@@ -216,7 +216,7 @@ public class ValkyrieSliderBoard
                               sliderBoardConfigurationManager.setSlider(2, "kp_" + pdControllerBaseName, registry, 0.0, 2000.0);
                               sliderBoardConfigurationManager.setSlider(3, "kd_" + pdControllerBaseName, registry, 0.0, 600.0);
                               sliderBoardConfigurationManager.setSlider(4, pdControllerBaseName + "_transitionFactor", registry, 0.0, 1.0);
-                              sliderBoardConfigurationManager.setSlider(5, pdControllerBaseName + "_tauDesired", registry, 0.0, 100.0);
+                              sliderBoardConfigurationManager.setSlider(5, pdControllerBaseName + "_tauDesired", registry, -100.0, 100.0);
 
                               sliderBoardConfigurationManager.setButton(1, pdControllerBaseName + "_useFunctionGenerator", registry);
                               sliderBoardConfigurationManager.setSlider(6, pdControllerBaseName + "_functionGeneratorAmplitude", registry, 0, 200);
