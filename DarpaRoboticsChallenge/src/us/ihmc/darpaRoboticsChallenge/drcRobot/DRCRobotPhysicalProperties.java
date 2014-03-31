8481219296e1b0a23efc8b42f1c5e08f9ac2e179
Nathan Mertins <nmertins@ihmc.us>
2014-03-31 20:04:13 +0000

    DRC-1101: moved arm joint limits to DRCRobotPhysicalProperties

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/drcRobot/DRCRobotPhysicalProperties.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/drcRobot/DRCRobotPhysicalProperties.java
@@ -7,10 +7,10 @@ import us.ihmc.utilities.Pair;
 
 public abstract class DRCRobotPhysicalProperties
 {
-	public static final EnumMap<ArmJointName, Pair<Double, Double>> armJointLimits = new EnumMap<ArmJointName, Pair<Double, Double>>(ArmJointName.class);
+	public final EnumMap<ArmJointName, Pair<Double, Double>> armJointLimits = new EnumMap<ArmJointName, Pair<Double, Double>>(ArmJointName.class);
 	
 	public abstract double getAnkleHeight();
-	public static Pair<Double, Double> getArmJointMinAndMax(ArmJointName jointName)
+	public Pair<Double, Double> getArmJointMinAndMax(ArmJointName jointName)
 	{
 		return armJointLimits.get(jointName);
 	}
