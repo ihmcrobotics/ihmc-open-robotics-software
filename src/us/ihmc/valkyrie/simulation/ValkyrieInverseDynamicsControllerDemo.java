90b87fbf7eef56a888c11d0cadf945306d4be210
Sylvain Bertrand <sbertrand@ihmc.us>
2014-03-31 04:53:35 +0000

    NONE: Fixed a couple of bugs with the variable changed listeners in IverseDynamicsController.

---
+++ src/us/ihmc/valkyrie/simulation/ValkyrieInverseDynamicsControllerDemo.java
@@ -0,0 +1,34 @@
+package us.ihmc.valkyrie.simulation;
+
+import com.yobotics.simulationconstructionset.util.FlatGroundProfile;
+
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.commonWalkingControlModules.automaticSimulationRunner.AutomaticSimulationRunner;
+import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
+import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
+import us.ihmc.darpaRoboticsChallenge.DRCInverseDynamicsControllerDemo;
+import us.ihmc.darpaRoboticsChallenge.DRCRobotInterface;
+import us.ihmc.darpaRoboticsChallenge.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
+import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
+import us.ihmc.graphics3DAdapter.GroundProfile;
+import us.ihmc.valkyrie.ValkyrieRobotInterface;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+
+public class ValkyrieInverseDynamicsControllerDemo
+{
+   public static void main(String[] args)
+   {
+      DRCRobotModel model = new ValkyrieRobotModel();
+      DRCRobotInterface robotInterface = new ValkyrieRobotInterface();
+      double groundHeight = 0.0;
+      double initialYaw = 0.0;
+      DRCRobotInitialSetup<SDFRobot> robotInitialSetup = model.getDefaultRobotInitialSetup(groundHeight + 0.3, initialYaw);
+      DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, true);
+      GroundProfile groundProfile = new FlatGroundProfile(groundHeight);
+      DRCSCSInitialSetup scsInitialSetup = new DRCSCSInitialSetup(groundProfile, robotInterface.getSimulateDT());
+      AutomaticSimulationRunner automaticSimulationRunner = null;
+      
+      new DRCInverseDynamicsControllerDemo(robotInterface, robotInitialSetup, guiInitialSetup, scsInitialSetup, automaticSimulationRunner, DRCConfigParameters.CONTROL_DT, 16000, model);
+   }
+}
