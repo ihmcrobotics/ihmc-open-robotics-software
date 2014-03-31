9f071a00608d65eb248664cdd4107c5dc7654114
Peter Abeles <peter.abeles@gmail.com>
2014-03-31 22:43:33 +0000

    DRC-906:
    Added correct implementation of offset to collisions

--- DarpaRoboticsChallenge/src/us/ihmc/atlas/AtlasPhysicalProperties.java
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/AtlasPhysicalProperties.java
@@ -1,18 +1,21 @@
 package us.ihmc.atlas;
 
-import javax.media.j3d.Transform3D;
-import javax.vecmath.Vector3d;
-
 import us.ihmc.commonWalkingControlModules.partNamesAndTorques.ArmJointName;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotPhysicalProperties;
 import us.ihmc.utilities.Pair;
 import us.ihmc.utilities.math.geometry.TransformTools;
 
+import javax.media.j3d.Transform3D;
+import javax.vecmath.Vector3d;
+
 public class AtlasPhysicalProperties extends DRCRobotPhysicalProperties
 {
    public static final double ankleHeight = 0.084;
    public static final double pelvisToFoot = 0.887;
-   public static final Transform3D ankle_to_sole_frame_tranform = TransformTools.createTranslationTransform(new Vector3d(0.0, 0.0, -ankleHeight));;
+
+   // this transform is probably in the wrong direction.  The ankle is above the sole,  so the origin of the ankle should have a positive z value
+   // from the point of view of the sole.  With this transform it will have a negative value -- PA
+   public static final Transform3D ankle_to_sole_frame_tranform = TransformTools.createTranslationTransform(new Vector3d(0.0, 0.0, -ankleHeight));
    public static final double foot_width = 0.12;    // 0.08;   //0.124887;
    public static final double toe_width = 0.095;    // 0.07;   //0.05;   //
    public static final double foot_length = 0.255;
