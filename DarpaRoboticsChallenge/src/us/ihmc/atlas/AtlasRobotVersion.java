b222fa2207db743f11cd7357ed7950e5a4c0670e
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2014-04-02 21:20:40 +0000

    DRC-1035, Forgot to change the resource loading path to match file moving. Should #fixbamboo

--- DarpaRoboticsChallenge/src/us/ihmc/atlas/AtlasRobotVersion.java
+++ DarpaRoboticsChallenge/src/us/ihmc/atlas/AtlasRobotVersion.java
@@ -7,13 +7,14 @@ import com.jme3.math.Transform;
 import com.jme3.math.Vector3f;
 
 import us.ihmc.darpaRoboticsChallenge.handControl.DRCHandModel;
+import us.ihmc.darpaRoboticsChallenge.models.ModelRoot;
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.robotSide.SideDependentList;
 
 public enum AtlasRobotVersion {
 	ATLAS_NO_HANDS_ADDED_MASS, ATLAS_SANDIA_HANDS, ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS, DRC_NO_HANDS, DRC_HANDS, DRC_EXTENDED_HANDS, DRC_HOOKS, DRC_TASK_HOSE, DRC_EXTENDED_HOOKS;
 	
-	private static Class<AtlasRobotVersion> thisClass = AtlasRobotVersion.class;
+	private static Class<ModelRoot> modelRoot = ModelRoot.class;
 	private static String[] resourceDirectories;
 	private final SideDependentList<Transform> offsetHandFromWrist = new SideDependentList<Transform>(); 
 	
@@ -67,23 +68,23 @@ public enum AtlasRobotVersion {
 		switch (this)
 	      {
 	         case ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS :
-	            return "models/GFE/atlas.sdf";
+	            return "GFE/atlas.sdf";
 	         case ATLAS_SANDIA_HANDS :
-	            return "models/GFE/atlas_sandia_hands.sdf";
+	            return "GFE/atlas_sandia_hands.sdf";
 	         case ATLAS_NO_HANDS_ADDED_MASS :
-	            return "models/GFE/atlas_addedmass.sdf";
+	            return "GFE/atlas_addedmass.sdf";
 	         case DRC_NO_HANDS:
-	            return "models/GFE/drc_no_hands.sdf";
+	            return "GFE/drc_no_hands.sdf";
 	         case DRC_HANDS:
-	            return "models/GFE/drc_hands.sdf";
+	            return "GFE/drc_hands.sdf";
 	         case DRC_EXTENDED_HANDS:
-	            return "models/GFE/drc_extended_hands.sdf";
+	            return "GFE/drc_extended_hands.sdf";
 	         case DRC_HOOKS:
-	            return "models/GFE/drc_hooks.sdf";
+	            return "GFE/drc_hooks.sdf";
 	         case DRC_TASK_HOSE:
-	            return "models/GFE/drc_task_hose.sdf";
+	            return "GFE/drc_task_hose.sdf";
 	         case DRC_EXTENDED_HOOKS:
-	            return "models/GFE/drc_extended_hooks.sdf";
+	            return "GFE/drc_extended_hooks.sdf";
 	         default :
 	            throw new RuntimeException("AtlasRobotVersion: Unimplemented enumeration case : " + this);
 	      }
@@ -94,16 +95,16 @@ public enum AtlasRobotVersion {
 		if(resourceDirectories == null)
 		{
 			resourceDirectories = new String[] {
-			         thisClass.getResource("models/GFE/gazebo_models/atlas_description").getFile(),
-			         thisClass.getResource("models/GFE/gazebo_models/multisense_sl_description").getFile(),
-			         thisClass.getResource("models/GFE/gazebo_models/irobot_hand_description").getFile()
+			         modelRoot.getResource("GFE/gazebo_models/atlas_description").getFile(),
+			         modelRoot.getResource("GFE/gazebo_models/multisense_sl_description").getFile(),
+			         modelRoot.getResource("GFE/gazebo_models/irobot_hand_description").getFile()
 				};
 		}
 		return resourceDirectories;
 	}
 
 	public InputStream getSdfFileAsStream() {
-		return thisClass.getResourceAsStream(getSdfFile());
+		return modelRoot.getResourceAsStream(getSdfFile());
 	}
 
 	public Transform getOffsetFromWrist(RobotSide side) {
