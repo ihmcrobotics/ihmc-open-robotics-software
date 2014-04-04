61cab953355d6e5b33b28bf3170c5b955397c03e
Sylvain Bertrand <sbertrand@ihmc.us>
2014-04-04 02:25:47 +0000

    NONE: Created a test case for the pose playback controller. Started refactoring the PolyvalentHighLevelHumanoidControllerFactory.

---
+++ test/us/ihmc/valkyrie/simulation/ValkyriePosePlaybackDemoTest.java
@@ -0,0 +1,200 @@
+package us.ihmc.valkyrie.simulation;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.bambooTools.BambooTools;
+import us.ihmc.commonWalkingControlModules.automaticSimulationRunner.AutomaticSimulationRunner;
+import us.ihmc.commonWalkingControlModules.dynamics.FullRobotModel;
+import us.ihmc.commonWalkingControlModules.posePlayback.PosePlaybackPacket;
+import us.ihmc.commonWalkingControlModules.visualizer.RobotVisualizer;
+import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
+import us.ihmc.darpaRoboticsChallenge.DRCController;
+import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
+import us.ihmc.darpaRoboticsChallenge.DRCPosePlaybackDemo;
+import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
+import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
+import us.ihmc.graphics3DAdapter.GroundProfile;
+import us.ihmc.utilities.AsyncContinuousExecutor;
+import us.ihmc.utilities.MemoryTools;
+import us.ihmc.utilities.RandomTools;
+import us.ihmc.utilities.ThreadTools;
+import us.ihmc.utilities.TimerTaskScheduler;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+import us.ihmc.utilities.screwTheory.TotalMassCalculator;
+import us.ihmc.valkyrie.ValkyrieRobotInterface;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.time.GlobalTimer;
+import com.yobotics.simulationconstructionset.util.FlatGroundProfile;
+import com.yobotics.simulationconstructionset.util.GainCalculator;
+import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner;
+import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+
+public class ValkyriePosePlaybackDemoTest
+{
+   private static final boolean ALWAYS_SHOW_GUI = false;
+   private static final boolean KEEP_SCS_UP = false;
+
+   private static final boolean CREATE_MOVIE = BambooTools.doMovieCreation();
+   private static final boolean checkNothingChanged = BambooTools.getCheckNothingChanged();
+
+   private static final boolean SHOW_GUI = ALWAYS_SHOW_GUI || checkNothingChanged || CREATE_MOVIE;
+
+   private BlockingSimulationRunner blockingSimulationRunner;
+   private DRCController drcController;
+   private RobotVisualizer robotVisualizer;
+   
+   @Before
+   public void showMemoryUsageBeforeTest()
+   {
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
+   }
+   
+   @After
+   public void destroySimulationAndRecycleMemory()
+   {
+      if (KEEP_SCS_UP)
+      {
+         ThreadTools.sleepForever();
+      }
+
+      // Do this here in case a test fails. That way the memory will be recycled.
+      if (blockingSimulationRunner != null)
+      {
+         blockingSimulationRunner.destroySimulation();
+         blockingSimulationRunner = null;
+      }
+
+      if (drcController != null)
+      {
+         drcController.dispose();
+         drcController = null;
+      }
+
+      if (robotVisualizer != null)
+      {
+         robotVisualizer.close();
+         robotVisualizer = null;
+      }
+
+      GlobalTimer.clearTimers();
+      TimerTaskScheduler.cancelAndReset();
+      AsyncContinuousExecutor.cancelAndReset();
+      
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
+   }
+
+
+   @Test
+   public void testPosePlaybackControllerWithRandomPoses() throws SimulationExceededMaximumTimeException
+   {
+      ValkyrieRobotModel valkyrieRobotModel = new ValkyrieRobotModel();
+      ValkyrieRobotInterface valkyrieRobotInterface = new ValkyrieRobotInterface();
+
+      AutomaticSimulationRunner automaticSimulationRunner = null;
+
+      DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false, null);
+      double floatingHeight = 0.3;
+      double groundHeight = 0.0;
+      double initialYaw = 0.0;
+      DRCRobotInitialSetup<SDFRobot> robotInitialSetup = valkyrieRobotModel.getDefaultRobotInitialSetup(groundHeight + floatingHeight, initialYaw);
+      GroundProfile groundProfile = new FlatGroundProfile(groundHeight);
+      DRCSCSInitialSetup scsInitialSetup = new DRCSCSInitialSetup(groundProfile, valkyrieRobotInterface.getSimulateDT());
+      scsInitialSetup.setDrawGroundProfile(true);
+      scsInitialSetup.setInitializeEstimatorToActual(true);
+      
+      DRCPosePlaybackDemo drcPosePlaybackDemo = new DRCPosePlaybackDemo(valkyrieRobotInterface, robotInitialSetup, guiInitialSetup, scsInitialSetup, automaticSimulationRunner, DRCConfigParameters.CONTROL_DT, 16000, valkyrieRobotModel);
+      
+      drcController = drcPosePlaybackDemo.getDRCController();
+      SimulationConstructionSet scs = drcPosePlaybackDemo.getSimulationConstructionSet();
+      
+      int numberOfPoses = 5;
+      double trajectoryTime = 1.0;
+      PosePlaybackPacket posePlaybackPacket = createRandomPosePlaybackPacket(drcPosePlaybackDemo.getControllerModel(), numberOfPoses, trajectoryTime);
+      drcPosePlaybackDemo.setupPosePlaybackController(posePlaybackPacket, true);
+
+      blockingSimulationRunner = new BlockingSimulationRunner(scs, 1000.0);
+      
+      blockingSimulationRunner.simulateAndBlock(numberOfPoses * trajectoryTime + 0.5);
+
+      createMovie(scs);
+      BambooTools.reportTestFinishedMessage();
+   }
+
+   private void createMovie(SimulationConstructionSet scs)
+   {
+      if (CREATE_MOVIE)
+      {
+         BambooTools.createMovieAndDataWithDateTimeClassMethodAndShareOnSharedDriveIfAvailable(scs, 1);
+      }
+   }
+
+   private PosePlaybackPacket createRandomPosePlaybackPacket(final FullRobotModel fullRobotModel, final int numberOfPoses, final double trajectoryTime)
+   {
+      PosePlaybackPacket posePlaybackPacket = new PosePlaybackPacket()
+      {
+         private final Random random = new Random(16581L);
+         private final ArrayList<Double> trajectoryTimes = new ArrayList<>(numberOfPoses);
+         private final List<Map<OneDoFJoint, Double>> listOfPosesToPlayback = new ArrayList<>(numberOfPoses);
+         private final LinkedHashMap<OneDoFJoint, Double> jointKps = new LinkedHashMap<>(numberOfPoses);
+         private final LinkedHashMap<OneDoFJoint, Double> jointKds = new LinkedHashMap<>(numberOfPoses);
+         {
+            for (int i = 0; i < numberOfPoses; i ++)
+            {
+               trajectoryTimes.add(trajectoryTime);
+               
+               LinkedHashMap<OneDoFJoint, Double> pose = new LinkedHashMap<>();
+               for (OneDoFJoint joint : fullRobotModel.getOneDoFJoints())
+               {
+                  pose.put(joint, RandomTools.generateRandomDouble(random, joint.getJointLimitLower(), joint.getJointLimitUpper()));
+               }
+               listOfPosesToPlayback.add(pose);
+               
+               for (OneDoFJoint joint : fullRobotModel.getOneDoFJoints())
+               {
+                  double mass = TotalMassCalculator.computeSubTreeMass(joint.getSuccessor());
+                  jointKps.put(joint, 4.0 * mass);
+                  jointKds.put(joint, GainCalculator.computeDampingForSecondOrderSystem(mass, jointKps.get(joint), 0.05));
+               }
+            }
+         }
+         
+         @Override
+         public List<Double> getTrajectoryTimes()
+         {
+            return trajectoryTimes;
+         }
+         
+         @Override
+         public List<Map<OneDoFJoint, Double>> getListOfPosesToPlayback()
+         {
+            return listOfPosesToPlayback;
+         }
+         
+         @Override
+         public Map<OneDoFJoint, Double> getJointKps()
+         {
+            return jointKps;
+         }
+         
+         @Override
+         public Map<OneDoFJoint, Double> getJointKds()
+         {
+            return jointKds;
+         }
+      };
+      
+      return posePlaybackPacket;
+   }
+
+}
