e1746500f7084e4781221d30845e3e9db6ff56c6
Tingfan Wu <twu@ihmc.us>
2014-04-04 02:15:50 +0000

    NONE: add YoFunctionGenerator::setFrequencyWithContinuousOutput along with a unittest

--- SimulationConstructionSet/test/com/yobotics/simulationconstructionset/util/math/functionGenerator/YoFunctionGeneratorTest.java
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/util/math/functionGenerator/YoFunctionGeneratorTest.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import us.ihmc.utilities.test.JUnitTools;
+
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
 public class YoFunctionGeneratorTest
@@ -38,6 +40,42 @@ public class YoFunctionGeneratorTest
 		   assertEquals(amplitude, yoFunctionGenerator.getValue(time), 1e-10);
 
 	   }
+   }
+   
+   @Test
+   public void testOutputContinuityDuringFrequencyChange()
+   {
+	   double freq0=10,amp0=10;
+	   yoFunctionGenerator.setMode(YoFunctionGeneratorMode.SINE);
+	   yoFunctionGenerator.setAmplitude(amp0);
+	   yoFunctionGenerator.setPhase(Math.PI/2);
+	   
+	   double t0 = 0.134,dt=0.001;
+	   
+	   /*
+	   //regular setFrequency
+	   yoFunctionGenerator.setFrequency(freq0);
+	   System.out.println(yoFunctionGenerator.getValue(t0+2*dt));
+	   yoFunctionGenerator.setFrequency(freq0*3);
+	   System.out.println(yoFunctionGenerator.getValue(t0+3*dt));
+
+	   //PhaseSync setFrequency
+	   System.out.println("-");
+	   yoFunctionGenerator.setFrequency(freq0);
+	   System.out.println(yoFunctionGenerator.getValue(t0+2*dt));
+	   yoFunctionGenerator.setFrequencyPhaseSync(freq0*3);
+	   System.out.println(yoFunctionGenerator.getValue(t0+3*dt));
+		*/
+
+	   //Actual test
+	   double output0,output1;
+	   yoFunctionGenerator.setFrequency(freq0);
+	   output0 = yoFunctionGenerator.getValue(t0+2*dt);
+	   yoFunctionGenerator.setFrequencyWithContinuousOutput(freq0*3);
+	   output1 = yoFunctionGenerator.getValue(t0+3*dt);
+	   
+	   double tolerance=6*dt*amp0*freq0*3;
+	   assertEquals("|"+output0+"-"+output1+"|<" + tolerance,0,output1-output0, tolerance);
    }
 
    @Test
