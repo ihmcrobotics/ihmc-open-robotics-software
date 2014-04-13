680bab70df48daa36417671a8e094c0e5b78afc3
Sylvain Bertrand <sbertrand@ihmc.us>
2014-04-13 01:00:25 +0000

    NONE: Reduced code duplication.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/configurations/WalkingControllerParameters.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/configurations/WalkingControllerParameters.java
@@ -9,6 +9,7 @@ public interface WalkingControllerParameters extends HeadOrientationControllerPa
    public abstract SideDependentList<Transform3D> getDesiredHandPosesWithRespectToChestFrame();
 
    public abstract String[] getAllowableChestOrientationControlJointNames();
+
    public abstract String[] getDefaultChestOrientationControlJointNames();
 
    public abstract boolean checkOrbitalEnergyCondition();
@@ -16,27 +17,31 @@ public interface WalkingControllerParameters extends HeadOrientationControllerPa
    public abstract double getGroundReactionWrenchBreakFrequencyHertz();
 
    public abstract boolean resetDesiredICPToCurrentAtStartOfSwing();
-   
+
    public abstract double getFootForwardOffset();
-   
+
    public abstract double getFootBackwardOffset();
-   
+
    public abstract double getAnkleHeight();
 
    public abstract double getLegLength();
-   
+
    public abstract double getMinLegLengthBeforeCollapsingSingleSupport();
-   
+
    public abstract double minimumHeightAboveAnkle();
+
    public abstract double nominalHeightAboveAnkle();
+
    public abstract double maximumHeightAboveAnkle();
-   
+
    public abstract boolean finishSwingWhenTrajectoryDone();
 
    public abstract boolean stayOnToes();
-   
+
    public abstract boolean doToeOffIfPossible();
+
    public abstract boolean doToeTouchdownIfPossible();
+
    public abstract boolean doHeelTouchdownIfPossible();
 
    public abstract double getFinalToeOffPitchAngularVelocity();
@@ -54,48 +59,73 @@ public interface WalkingControllerParameters extends HeadOrientationControllerPa
    public abstract double getStepPitch();
 
    public abstract double getCaptureKpParallelToMotion();
+
    public abstract double getCaptureKpOrthogonalToMotion();
+
    public abstract double getCaptureKi();
+
    public abstract double getCaptureKiBleedoff();
 
    public abstract double getCaptureFilterBreakFrequencyInHz();
+
    public abstract double getCMPRateLimit();
+
    public abstract double getCMPAccelerationLimit();
 
    public abstract double getKpPelvisOrientation();
+
    public abstract double getZetaPelvisOrientation();
+
    public abstract double getMaxAccelerationPelvisOrientation();
+
    public abstract double getMaxJerkPelvisOrientation();
 
    public abstract double getKpCoMHeight();
+
    public abstract double getZetaCoMHeight();
 
    public abstract double getKpHeadOrientation();
+
    public abstract double getZetaHeadOrientation();
+
    public abstract double getTrajectoryTimeHeadOrientation();
 
    public abstract double getKpUpperBody();
+
    public abstract double getZetaUpperBody();
+
    public abstract double getMaxAccelerationUpperBody();
+
    public abstract double getMaxJerkUpperBody();
 
    public abstract double getSwingKpXY();
+
    public abstract double getSwingKpZ();
+
    public abstract double getSwingKpOrientation();
+
    public abstract double getSwingZetaXYZ();
+
    public abstract double getSwingZetaOrientation();
 
    public abstract double getHoldKpXY();
+
    public abstract double getHoldKpOrientation();
+
    public abstract double getHoldZeta();
 
    public abstract double getSwingMaxPositionAcceleration();
+
    public abstract double getSwingMaxPositionJerk();
+
    public abstract double getSwingMaxOrientationAcceleration();
+
    public abstract double getswingMaxOrientationJerk();
-   
+
    public abstract double getToeOffKpXY();
+
    public abstract double getToeOffKpOrientation();
+
    public abstract double getToeOffZeta();
 
    public abstract boolean doPrepareManipulationForLocomotion();
@@ -103,6 +133,7 @@ public interface WalkingControllerParameters extends HeadOrientationControllerPa
    public abstract boolean isRunningOnRealRobot();
 
    public abstract double getDefaultTransferTime();
+
    public abstract double getDefaultSwingTime();
 
    public abstract double getPelvisPitchUpperLimit();
@@ -110,11 +141,14 @@ public interface WalkingControllerParameters extends HeadOrientationControllerPa
    public abstract double getPelvisPitchLowerLimit();
 
    public abstract boolean isPelvisPitchReversed();
+
    public abstract double getFoot_width();
+
    public abstract double getToe_width();
+
    public abstract double getFoot_length();
-   public abstract double getFoot_back();
+
    public abstract double getFoot_start_toetaper_from_back();
-   public abstract double getFoot_forward();
+
    public abstract double getSideLengthOfBoundingBoxForFootstepHeight();
 }
