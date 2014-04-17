500d0edec0a4c63db0516852aa2a44f2c8936b73
Atlas Control Computer <AtlasControlComputer@unknown>
2014-04-17 19:11:38 +0000

    NONE: use AtlasOperatorUserInterface instead of DRC in frontend

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/processManagement/DRCEnterpriseCloudDispatcherFrontend.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/processManagement/DRCEnterpriseCloudDispatcherFrontend.java
@@ -1,15 +1,43 @@
 package us.ihmc.darpaRoboticsChallenge.processManagement;
 
-import com.martiansoftware.jsap.FlaggedOption;
-import com.martiansoftware.jsap.JSAP;
-import com.martiansoftware.jsap.JSAPException;
-import com.martiansoftware.jsap.JSAPResult;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
+import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+import us.ihmc.atlas.AtlasOperatorUserInterface;
 import us.ihmc.atlas.AtlasRobotModelFactory;
 import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.DRCLocalConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
-import us.ihmc.darpaRoboticsChallenge.userInterface.DRCOperatorUserInterface;
 import us.ihmc.utilities.fixedPointRepresentation.UnsignedByteTools;
 import us.ihmc.utilities.gui.IHMCSwingTools;
 import us.ihmc.utilities.net.NetStateListener;
@@ -18,21 +46,10 @@ import us.ihmc.utilities.net.tcpServer.ReconnectingTCPClient;
 import us.ihmc.utilities.processManagement.ExitListener;
 import us.ihmc.utilities.processManagement.JavaProcessSpawner;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.PlainDocument;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
 
 public class DRCEnterpriseCloudDispatcherFrontend implements Runnable
 {
@@ -403,7 +420,7 @@ public class DRCEnterpriseCloudDispatcherFrontend implements Runnable
                String[] javaArgs = new String[] {"-Xms4096m", "-Xmx4096m"};
                String[] programArgs = new String[] {"-m", selectRobotModelRadioButtonGroup.getSelection().getActionCommand()};
 
-               uiSpawner.spawn(DRCOperatorUserInterface.class, javaArgs, programArgs, new ExitListener()
+               uiSpawner.spawn(AtlasOperatorUserInterface.class, javaArgs, programArgs, new ExitListener()
                {
                   @Override
                   public void exited(int statusValue)
