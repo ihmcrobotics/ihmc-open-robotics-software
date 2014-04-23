d34b66c396bf5b7c0cd47aab4eddc2b440658396
Sylvain Bertrand <sbertrand@ihmc.us>
2014-04-23 16:28:28 +0000

    NONE: Added toString() to YoContactPoint and YoPlaneContactState.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/bipedSupportPolygons/YoContactPoint.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/bipedSupportPolygons/YoContactPoint.java
@@ -11,14 +11,18 @@ public class YoContactPoint extends ContactPoint
    private final YoVariableRegistry registry;
    private final YoFramePoint2d yoPosition2d;
    private final BooleanYoVariable isInContact;
+   private final String namePrefix;
 
    public YoContactPoint(String namePrefix, int index, FramePoint2d point2d, PlaneContactState parentContactState, YoVariableRegistry parentRegistry)
    {
       super(point2d, parentContactState);
       
+      this.namePrefix = namePrefix;
+      
       //TODO: Check if it is better to create an actual child registry
       registry = parentRegistry;
 
+      //TODO That one seems to be useless
       yoPosition2d = new YoFramePoint2d(namePrefix + "Contact" + index, point2d.getReferenceFrame(), registry);
       isInContact = new BooleanYoVariable(namePrefix + "InContact" + index, registry);
    }
@@ -37,5 +41,10 @@ public class YoContactPoint extends ContactPoint
    public YoFramePoint2d getYoPosition2d()
    {
       return yoPosition2d;
+   }
+   
+   public String toString()
+   {
+      return namePrefix + ", in contact: " + isInContact() + ", position: " + getPosition2d().toString();
    }
 }
