03799a8358759b4e129ec8040e3dcb89f73d501e
Nathan Mertins <nmertins@ihmc.us>
2014-04-28 22:36:59 +0000

    DRC-1123: removed a hack

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/DesiredHandPoseProvider.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/DesiredHandPoseProvider.java
@@ -106,9 +106,6 @@ public class DesiredHandPoseProvider implements ObjectConsumer<HandPosePacket>
          {
             if(object.getJointAngles() != null)
             	finalDesiredJointAngleMap.put(fullRobotModel.getArmJoint(robotSide, armJoint), object.getJointAngles()[++i]);
-            else
-            	//XXX: Nathan added this dirty hack to fixbamboo and should feel ashamed that he left it here.
-            	finalDesiredJointAngleMap.put(fullRobotModel.getArmJoint(robotSide, armJoint), 0.0);
          }
       }
 
