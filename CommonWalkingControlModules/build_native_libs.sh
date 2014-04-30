864e698fa0689f7a38e28bad2b39c62a5791a4d0
Atlas Control Computer <AtlasControlComputer@unknown>
2014-04-30 13:35:37 +0000

    NONE: fix buildscript, not fixbamboo

--- CommonWalkingControlModules/build_native_libs.sh
+++ CommonWalkingControlModules/build_native_libs.sh
@@ -5,9 +5,11 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make -j4 VERBOSE=1 install
+cd ..
 
 rm -rf build/*
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel ../
 make -j4 VERBOSE=1 install
+cd ..
