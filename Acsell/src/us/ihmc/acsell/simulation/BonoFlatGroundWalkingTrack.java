b14235444f8b9e1472c3b88f4e6a48feba3dd09a
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2014-04-30 23:40:59 +0000

    DRC-1091: Cleaning up the DRCRobotInterface, renamed to DRCSimulatedRobotInterface, removed the jointmap getter. Removed the implements classes on DRCWorld, re-added drc_vehicle.sdf to fix a main method.
    Fixed epsilon equals bug in handPosepacket, added armJointAngles headers to pose packet serializer and commented out the ArmJointAnglesPacket serializer. this should fixBamboo, need to get with nathan to make sure this was done correctly

--- Acsell/src/us/ihmc/acsell/simulation/BonoFlatGroundWalkingTrack.java
+++ Acsell/src/us/ihmc/acsell/simulation/BonoFlatGroundWalkingTrack.java
@@ -6,7 +6,7 @@ import us.ihmc.commonWalkingControlModules.automaticSimulationRunner.AutomaticSi
 import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
 import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
-import us.ihmc.darpaRoboticsChallenge.DRCRobotInterface;
+import us.ihmc.darpaRoboticsChallenge.DRCSimulatedRobotInterface;
 import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.PlainDRCRobot;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.SimulatedModelCorruptorDRCRobotInterface;
@@ -25,7 +25,7 @@ public class BonoFlatGroundWalkingTrack
       AutomaticSimulationRunner automaticSimulationRunner = null;
       SliderBoardFactory sliderBoardFactory = WalkControllerSliderBoard.getFactory();
       DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false, sliderBoardFactory);
-      DRCRobotInterface robotInterface = new PlainDRCRobot(robotModel);
+      DRCSimulatedRobotInterface robotInterface = new PlainDRCRobot(robotModel);
 
       if (DRCConfigParameters.CORRUPT_SIMULATION_MODEL)
       {
