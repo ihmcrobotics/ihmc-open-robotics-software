fad31740bd152fee0139a2b1eb0f0b8a8b4b459c
Atlas Control Computer <AtlasControlComputer@unknown>
2014-04-30 12:59:43 +0000

    DRC-1197 build linux libraries, should fixbamboo

--- CommonWalkingControlModules/build_native_libs.sh
+++ CommonWalkingControlModules/build_native_libs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 rm -rf build/*
+mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make -j4 VERBOSE=1 install
 
 rm -rf build/*
+mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelMinSize ../
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel ../
 make -j4 VERBOSE=1 install
