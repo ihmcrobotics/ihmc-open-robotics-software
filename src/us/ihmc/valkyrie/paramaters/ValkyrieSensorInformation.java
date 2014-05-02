0eef26906854d2e56067631597ba9bae9ece3134
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2014-05-02 22:13:54 +0000

    DRC-1091: Removed ValkyrieSensorNames, added urdf feetForceSensors and imu usb serial numbers to the ValkyrieSensorInformation

--- src/us/ihmc/valkyrie/paramaters/ValkyrieSensorInformation.java
+++ src/us/ihmc/valkyrie/paramaters/ValkyrieSensorInformation.java
@@ -1,27 +1,49 @@
 package us.ihmc.valkyrie.paramaters;
 
+import java.util.HashMap;
+
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotSensorInformation;
+import us.ihmc.robotSide.RobotSide;
 import us.ihmc.robotSide.SideDependentList;
 
 public class ValkyrieSensorInformation implements DRCRobotSensorInformation
 {
 
-   public static final String[] forceSensorNames = { "LeftAnkle", "RightAnkle", "LeftForearmSupinator", "RightForearmSupinator" };
-   public static final SideDependentList<String> feetForceSensorNames = new SideDependentList<String>("LeftAnkle", "RightAnkle");
-   public static final SideDependentList<String> wristForceSensorNames = new SideDependentList<String>("LeftForearmSupinator", "RightForearmSupinator");
-   public static final String lidarSensorName = "/v1/Ibeo_sensor";
-   public static final String lidarJointName = "";
-   public static final String leftCameraName = "/v1/LeftHazardCamera___default__";
-   public static final String rightCameraName ="/v1/RightHazardCamera___default__";
-   public static final String rightTrunkIMUSensor = "v1Trunk_RightIMU";
-   public static final String leftTrunkIMUSensor = "v1Trunk_LeftIMU";
-   public static final String leftPelvisIMUSensor = "v1Pelvis_LeftIMU";
-   public static final String rightPelvisIMUSensor = "v1Pelvis_RightIMU";
-   public static final String fakePelvisIMUSensor = "v1Pelvis_SimulatedIMU";
+   private static final String[] forceSensorNames = { "LeftAnkle", "RightAnkle", "LeftForearmSupinator", "RightForearmSupinator" };
+   private static final SideDependentList<String> feetForceSensorNames = new SideDependentList<String>("LeftAnkle", "RightAnkle");
+   private static final SideDependentList<String> urdfFeetForceSensorNames = new SideDependentList<>("/v1/LeftLeg6Axis_Offset", "/v1/RightLeg6Axis_Offset");
+   
+   private static final SideDependentList<String> wristForceSensorNames = new SideDependentList<String>("LeftForearmSupinator", "RightForearmSupinator");
+   private static final String lidarSensorName = "/v1/Ibeo_sensor";
+   private static final String lidarJointName = "";
+   private static final String leftCameraName = "/v1/LeftHazardCamera___default__";
+   private static final String rightCameraName ="/v1/RightHazardCamera___default__";
+   private static final String rightTrunkIMUSensor = "v1Trunk_RightIMU";
+   private static final String leftTrunkIMUSensor = "v1Trunk_LeftIMU";
+   private static final String leftPelvisIMUSensor = "v1Pelvis_LeftIMU";
+   private static final String rightPelvisIMUSensor = "v1Pelvis_RightIMU";
+   private static final String fakePelvisIMUSensor = "v1Pelvis_SimulatedIMU";
+   
+   private static final HashMap<String, Integer> imuUSBSerialIds = new HashMap<>();
+   static
+   {
+      imuUSBSerialIds.put("v1Pelvis_LeftIMU", 622730566);
+      imuUSBSerialIds.put("v1Pelvis_RightIMU", 622730571);
+   }
    
    // Use this until sim can handle multiple IMUs
    // public static final String[] imuSensorsToUse = {leftPelvisIMUSensor, rightPelvisIMUSensor};
    public static final String[] imuSensorsToUse = {leftPelvisIMUSensor};
+   
+   public String getUrdfFeetForceSensorName(RobotSide side)
+   {
+      return urdfFeetForceSensorNames.get(side);
+   }
+   
+   public HashMap<String, Integer> getImuUSBSerialIds()
+   {
+      return imuUSBSerialIds;
+   }
    
    @Override
    public String getLeftCameraName()
