a9a010fd928928888e43a91fbcd97904b995de00
Jesper Smith <jsmith@ihmc.us>
2014-05-05 22:41:18 +0000

    DRC-1203: Started writing multithreaded SCS controller.

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCControllerThread.java
@@ -0,0 +1,6 @@
+package us.ihmc.darpaRoboticsChallenge;
+
+public class DRCControllerThread
+{
+
+}
---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCEstimatorThread.java
@@ -0,0 +1,23 @@
+package us.ihmc.darpaRoboticsChallenge;
+
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.sensorProcessing.simulatedSensors.SensorReaderFactory;
+
+public class DRCEstimatorThread
+{
+   public DRCEstimatorThread(DRCRobotModel model, SensorReaderFactory sensorReaderFactory)
+   {
+      
+   }
+   
+   public void run()
+   {
+      
+   }
+   
+   public void possiblyWaitTillDone()
+   {
+      
+   }
+   
+}
