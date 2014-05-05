678f9145119adbe9d1482a58d51db23da024717f
Sylvain Bertrand <sbertrand@ihmc.us>
2014-05-05 20:06:19 +0000

    NONE: Some cleanup around force sensors, and sensor readers.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/stateEstimation/DRCStateEstimatorInterface.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/stateEstimation/DRCStateEstimatorInterface.java
@@ -3,14 +3,12 @@ package us.ihmc.darpaRoboticsChallenge.stateEstimation;
 import javax.vecmath.Point3d;
 import javax.vecmath.Quat4d;
 
-import us.ihmc.sensorProcessing.sensors.ForceSensorDataHolder;
 import us.ihmc.sensorProcessing.stateEstimation.StateEstimator;
 
 import com.yobotics.simulationconstructionset.robotController.RobotController;
 
 public interface DRCStateEstimatorInterface extends RobotController
 {
-
    public abstract StateEstimator getStateEstimator();
 
    public abstract void startIMUDriftEstimation();
@@ -18,7 +16,4 @@ public interface DRCStateEstimatorInterface extends RobotController
    public abstract void startIMUDriftCompensation();
 
    public abstract void initializeEstimatorToActual(Point3d initialCoMPosition, Quat4d initialEstimationLinkOrientation);
-
-   public abstract void setForceSensorDataHolder(ForceSensorDataHolder forceSensorDataHolderForEstimator);
-
 }
