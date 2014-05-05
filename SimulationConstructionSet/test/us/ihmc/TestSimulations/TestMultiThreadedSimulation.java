a2cf7f949f830f91f6c4b3c4d7882439e0553444
Jesper Smith <jsmith@ihmc.us>
2014-05-05 22:41:18 +0000

    DRC-1203: Started writing multithreaded SCS controller.

---
+++ SimulationConstructionSet/test/us/ihmc/TestSimulations/TestMultiThreadedSimulation.java
@@ -0,0 +1,31 @@
+package us.ihmc.TestSimulations;
+
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.robotController.MultiThreadedRobotController;
+
+public class TestMultiThreadedSimulation
+{
+   public TestMultiThreadedSimulation()
+   {
+      DoublePendulum doublePendulum = new DoublePendulum();
+      SimulationConstructionSet scs = new SimulationConstructionSet(doublePendulum);
+
+      DoublePendulumController controller = new DoublePendulumController(doublePendulum);
+      
+      MultiThreadedRobotController multiThreadedRobotController = new MultiThreadedRobotController("threadedController", scs);
+      multiThreadedRobotController.addController(controller, 10);
+      multiThreadedRobotController.initialize();
+      
+      
+      doublePendulum.setController(multiThreadedRobotController);
+      
+      scs.setDT(0.0001, 1);
+      
+      scs.startOnAThread();
+   }
+
+   public static void main(String[] args)
+   {
+      new TestMultiThreadedSimulation();
+   }
+}
