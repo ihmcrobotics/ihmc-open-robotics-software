18deeb25fe6f4c58af5f5a6748a2fee12b1eb6f4
Sylvain Bertrand <sbertrand@ihmc.us>
2014-05-06 20:24:49 +0000

    NONE: Created SensorProcessing and a read-only interface. SensorProcessing is for now doing the same as JointAndIMUDataSource.

---
+++ SensorProcessing/src/us/ihmc/sensorProcessing/sensorProcessors/SensorOutputMapReadOnly.java
@@ -0,0 +1,15 @@
+package us.ihmc.sensorProcessing.sensorProcessors;
+
+import java.util.List;
+
+import us.ihmc.sensorProcessing.stateEstimation.IMUSensorReadOnly;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+
+public interface SensorOutputMapReadOnly
+{
+   public double getJointPositionProcessedOutput(OneDoFJoint oneDoFJoint);
+
+   public double getJointVelocityProcessedOutput(OneDoFJoint oneDoFJoint);
+   
+   public List<? extends IMUSensorReadOnly> getIMUProcessedOutputs();
+}
