ced87b4a2cb77b97fb1a163bea25dd42cb298e94
Jordan Lack <jlack@ihmc.us>
2014-05-08 21:12:56 +0000

    NONE: Adding trajectory stuff to fix some broken nightly tests. Not going to fix anything just yet though.

---
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/EndPointConstrainedCubicTrajectoryTest.java
@@ -0,0 +1,36 @@
+package us.ihmc.commonWalkingControlModules.trajectories;
+
+import org.junit.Test;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import static org.junit.Assert.assertTrue;
+
+public class EndPointConstrainedCubicTrajectoryTest
+{
+   
+   @Test
+   public void TestEndPointConstrainedCubicTrajectory()
+   {
+      YoVariableRegistry registry = new YoVariableRegistry("Test");
+      String name = "TestName";
+      
+      EndPointConstrainedCubicTrajectory trajectory = new EndPointConstrainedCubicTrajectory(name, registry);
+      
+      double X0 = 0.2;
+      double Xf = 1;
+      double t0 = 0.2;
+      double tf = 1.2;
+      double Xdotf = -0.3;
+      double Xdot0 = 0.1;
+      
+      trajectory.setParams(t0, tf, X0, Xdot0, Xf, Xdotf);
+      
+      trajectory.computeTrajectory(t0);
+      assertTrue(Math.abs(trajectory.getPosition()-X0) < 0.000001);
+      assertTrue(Math.abs(trajectory.getVelocity()-Xdot0) < 0.000001);
+      
+      trajectory.computeTrajectory(tf);
+      assertTrue(Math.abs(trajectory.getPosition()-Xf) < 0.000001);
+      assertTrue(Math.abs(trajectory.getVelocity()-Xdotf) < 0.000001);
+   }
+
+}
