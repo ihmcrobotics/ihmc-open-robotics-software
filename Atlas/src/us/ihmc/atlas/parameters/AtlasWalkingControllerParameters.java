8150b0098c9b85cb3f2192a32205364ad8b24bbd
Jordan Lack <jlack@ihmc.us>
2014-05-08 22:27:47 +0000

    NONE: Setting things up for mods to swing trajectory generator.

--- Atlas/src/us/ihmc/atlas/parameters/AtlasWalkingControllerParameters.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasWalkingControllerParameters.java
@@ -408,7 +408,7 @@ public class AtlasWalkingControllerParameters implements WalkingControllerParame
    
    public double getSwingHeightMaxForPushRecoveryTrajectory()
    {
-      return 0.1;
+      return 0.12;
    }
    
    public double getSwingKpZ()
