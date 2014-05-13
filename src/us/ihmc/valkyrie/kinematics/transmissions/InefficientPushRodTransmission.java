33942a3d5b274c5f52176d18058591cd9d5daef7
Jerry Pratt <jpratt@ihmc.us>
2014-05-13 15:44:02 +0000

    DRC-1170: Abstracted out PushRodTransmissionInterface and made stubs for Efficient and Inefficient versions. Renamed Interpolated version. This will not fixBamboo.

---
+++ src/us/ihmc/valkyrie/kinematics/transmissions/InefficientPushRodTransmission.java
@@ -0,0 +1,51 @@
+package us.ihmc.valkyrie.kinematics.transmissions;
+
+import us.ihmc.valkyrie.kinematics.ValkyrieJoint;
+import us.ihmc.valkyrie.roboNet.TurboDriver;
+
+public class InefficientPushRodTransmission implements PushRodTransmissionInterface
+{
+
+   @Override
+   public void actuatorToJointEffort(TurboDriver[] act_data, ValkyrieJoint[] jnt_data)
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void actuatorToJointVelocity(TurboDriver[] act_data, ValkyrieJoint[] jnt_data)
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void actuatorToJointPosition(TurboDriver[] act_data, ValkyrieJoint[] jnt_data)
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void jointToActuatorEffort(TurboDriver[] act_data, ValkyrieJoint[] jnt_data)
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void jointToActuatorVelocity(TurboDriver[] act_data, ValkyrieJoint[] jnt_data)
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void jointToActuatorPosition(TurboDriver[] act_data, ValkyrieJoint[] jnt_data)
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+}
