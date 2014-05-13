0a140ea7196dba0dbec66dbd8b209a94391b41fa
Jerry Pratt <jpratt@ihmc.us>
2014-05-13 18:19:43 +0000

    DRC-1170: Added regression tests to InefficientPushrodTransmissionTest. Added YoVariable to PushrodTransmission so that you can select between the different PushrodTransmissionInterfaces. Need to test that we can switch like that on the real robot. Need to implement the efficient one and need to test them.

--- test/us/ihmc/valkyrie/kinematics/transmissions/InefficientPushRodTransmissionTest.java
+++ test/us/ihmc/valkyrie/kinematics/transmissions/InefficientPushRodTransmissionTest.java
@@ -19,7 +19,7 @@ public class InefficientPushRodTransmissionTest
    public void testForwardBackward()
    {
       Random random = new Random(1234L);
-      
+
       double reflect = 1.0;
       InefficientPushRodTransmission inefficientPushrodTransmission = new InefficientPushRodTransmission(reflect, null, null);
 
@@ -31,7 +31,7 @@ public class InefficientPushRodTransmissionTest
       jointData[0] = new ValkyrieJoint("joint0", null);
       jointData[1] = new ValkyrieJoint("joint1", null);
 
-      double increment = 0.02;
+      double increment = 0.05;
       for (double pitch = -Math.PI / 3.0; pitch < Math.PI / 3.0; pitch = pitch + increment)
       {
          for (double roll = -Math.PI / 3.0; roll < Math.PI / 3.0; roll = roll + increment)
@@ -67,17 +67,17 @@ public class InefficientPushRodTransmissionTest
 
             printIfDebug("actuatorForceReturn0 = " + actuatorForceReturn0 + ", actuatorForceReturn1 = " + actuatorForceReturn1);
             printIfDebug("");
-            
+
             assertEquals(actuatorForce0, actuatorForceReturn0, 1e-7);
             assertEquals(actuatorForce1, actuatorForceReturn1, 1e-7);
          }
       }
    }
-   
-   
+
+
    @Test
    public void testRegression()
-   {      
+   {
       double reflect = 1.0;
 
       InefficientPushRodTransmission inefficientPushrodTransmission = new InefficientPushRodTransmission(reflect, null, null);
@@ -92,52 +92,59 @@ public class InefficientPushRodTransmissionTest
 
       double pitch = 0.0;
       double roll = 0.0;
-     
-      
 
-            double actuatorForce0 = 1.0;
-            double actuatorForce1 = 0.0;
+      double actuatorForce0 = 1.0;
+      double actuatorForce1 = 0.0;
 
-            actuatorData[0].setEffortCommand(actuatorForce0);
-            actuatorData[1].setEffortCommand(actuatorForce1);
+      double regressionPitchTorque = -0.0366712094326246;
+      double regressionRollTorque = -0.034118686505983736;
+      verifyARegressionTest(inefficientPushrodTransmission, pitch, roll, actuatorData, jointData, actuatorForce0, actuatorForce1, regressionPitchTorque,
+                            regressionRollTorque);
 
-            jointData[0].setPosition(pitch);
-            jointData[1].setPosition(roll);
+      actuatorForce0 = 0.0;
+      actuatorForce1 = 1.0;
+      regressionPitchTorque = -0.0366712094326246;
+      regressionRollTorque = 0.034118686505983736;
 
-            inefficientPushrodTransmission.actuatorToJointEffort(actuatorData, jointData);
+      verifyARegressionTest(inefficientPushrodTransmission, pitch, roll, actuatorData, jointData, actuatorForce0, actuatorForce1, regressionPitchTorque,
+                            regressionRollTorque);
 
-            double pitchTorque = jointData[0].getEffort();
-            double rollTorque = jointData[1].getEffort();
 
-            assertEquals(pitchTorque, -0.0366712094326246, 1e-7);
-            assertEquals(pitchTorque, -0.0366712094326246, 1e-7);
-            
+      pitch = -0.2;
+      roll = 0.1;
+      actuatorForce0 = 1.0;
+      actuatorForce1 = 0.0;
+      regressionPitchTorque = -0.03695254741929382;
+      regressionRollTorque = -0.02988230913579041;
 
-//            computeAndPrint(inefficientPushrodTransmissionJacobian, pitch, roll, jacobian, scs);
-//            assertJacobianEquals(jacobian, -0.0366712094326246, -0.0366712094326246, 0.034118686505983736, -0.034118686505983736);
+      verifyARegressionTest(inefficientPushrodTransmission, pitch, roll, actuatorData, jointData, actuatorForce0, actuatorForce1, regressionPitchTorque,
+                            regressionRollTorque);
 
-            
-            printIfDebug("pitch = " + pitch + ", roll = " + roll);
+      actuatorForce0 = 0.0;
+      actuatorForce1 = 1.0;
+      regressionPitchTorque = -0.034740329545336665;
+      regressionRollTorque = 0.03440182578269918;
 
-            printIfDebug("actuatorForce0 = " + actuatorForce0 + ", actuatorForce1 = " + actuatorForce1 + ", pitchTorque = " + pitchTorque + ", rollTorque = "
-                         + rollTorque);
-            actuatorData[0].setEffortCommand(Double.NaN);
-            actuatorData[1].setEffortCommand(Double.NaN);
+      verifyARegressionTest(inefficientPushrodTransmission, pitch, roll, actuatorData, jointData, actuatorForce0, actuatorForce1, regressionPitchTorque,
+                            regressionRollTorque);
+   }
 
-            jointData[0].setDesiredEffort(pitchTorque);
-            jointData[1].setDesiredEffort(rollTorque);
+   private void verifyARegressionTest(InefficientPushRodTransmission inefficientPushrodTransmission, double pitch, double roll, TurboDriver[] actuatorData,
+                                      ValkyrieJoint[] jointData, double actuatorForce0, double actuatorForce1, double pitchTorque, double rollTorque)
+   {
+      actuatorData[0].setEffortCommand(actuatorForce0);
+      actuatorData[1].setEffortCommand(actuatorForce1);
 
-            inefficientPushrodTransmission.jointToActuatorEffort(actuatorData, jointData);
+      jointData[0].setPosition(pitch);
+      jointData[1].setPosition(roll);
 
-            double actuatorForceReturn0 = actuatorData[0].getEffort();
-            double actuatorForceReturn1 = actuatorData[1].getEffort();
+      inefficientPushrodTransmission.actuatorToJointEffort(actuatorData, jointData);
 
-            printIfDebug("actuatorForceReturn0 = " + actuatorForceReturn0 + ", actuatorForceReturn1 = " + actuatorForceReturn1);
-            printIfDebug("");
-            
-            assertEquals(actuatorForce0, actuatorForceReturn0, 1e-7);
-            assertEquals(actuatorForce1, actuatorForceReturn1, 1e-7);
+      double computedPitchTorque = jointData[0].getEffort();
+      double computedRollTorque = jointData[1].getEffort();
 
+      assertEquals(computedPitchTorque, pitchTorque, 1e-7);
+      assertEquals(computedRollTorque, rollTorque, 1e-7);
    }
 
    private void printIfDebug(String string)
