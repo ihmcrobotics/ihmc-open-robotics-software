c97a6a7a7d0ed7af506bcf2398b95a20d8996cde
Sylvain Bertrand <sbertrand@ihmc.us>
2014-05-16 19:33:39 +0000

    NONE: Added chest mass in FullRobotModelCorruptor.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/corruptors/FullRobotModelCorruptor.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/corruptors/FullRobotModelCorruptor.java
@@ -4,6 +4,7 @@ import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 import us.ihmc.utilities.math.geometry.FramePoint;
 import us.ihmc.utilities.screwTheory.RigidBody;
 
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
 import com.yobotics.simulationconstructionset.VariableChangedListener;
 import com.yobotics.simulationconstructionset.YoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
@@ -12,38 +13,46 @@ import com.yobotics.simulationconstructionset.util.math.frames.YoFramePoint;
 public class FullRobotModelCorruptor
 {
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
-      
+
    private final YoFramePoint chestCoMOffset;
+   private final DoubleYoVariable chestMass;
    private final FramePoint tempFramePoint = new FramePoint();
-   
-   private final FullRobotModel fullRobotModel;
-   
+
    public FullRobotModelCorruptor(final FullRobotModel fullRobotModel, YoVariableRegistry parentRegistry)
    {
-      this.fullRobotModel = fullRobotModel;
-      
-      
       FramePoint originalChestCoMOffset = new FramePoint();
       RigidBody chest = fullRobotModel.getChest();
       chest.packCoMOffset(originalChestCoMOffset);
-      
+
       chestCoMOffset = new YoFramePoint("chestCoMOffset", originalChestCoMOffset.getReferenceFrame(), registry);
       chestCoMOffset.set(originalChestCoMOffset);
-      
+
       chestCoMOffset.attachVariableChangedListener(new VariableChangedListener()
       {
-         
+
          @Override
          public void variableChanged(YoVariable<?> v)
          {
             RigidBody chest = fullRobotModel.getChest();
             chestCoMOffset.getFrameTuple(tempFramePoint);
-            
+
             chest.setCoMOffset(tempFramePoint);
          }
       });
-      
+
+      chestMass = new DoubleYoVariable("chestMass", registry);
+      chestMass.set(chest.getInertia().getMass());
+
+      chestMass.addVariableChangedListener(new VariableChangedListener()
+      {
+         @Override
+         public void variableChanged(YoVariable<?> v)
+         {
+            RigidBody chest = fullRobotModel.getChest();
+            chest.getInertia().setMass(chestMass.getDoubleValue());
+         }
+      });
+
       parentRegistry.addChild(registry);
    }
-   
 }
