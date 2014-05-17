934af6b1885a67ee5e4dc475eaacedfb9a5a1c53
Nathan Mertins <nmertins@ihmc.us>
2014-05-17 23:49:11 +0000

    DRC-1146: small change to the foot slip vector; should get this test to pass on Nightly; won't fixbamboo but should fix the last failing test in ValkyrieObstacleCourseFlatTest

--- test/us/ihmc/valkyrie/ValkyrieObstacleCourseFlatTest.java
+++ test/us/ihmc/valkyrie/ValkyrieObstacleCourseFlatTest.java
@@ -20,7 +20,7 @@ public class ValkyrieObstacleCourseFlatTest extends DRCObstacleCourseFlatTest
    @Override
    protected Vector3d getFootSlipVector()
    {
-      return new Vector3d(0.025, -0.025, 0.0);
+      return new Vector3d(0.02, -0.02, 0.0);
    }
    @Override
    protected DoubleYoVariable getPelvisOrientationErrorVariableName(SimulationConstructionSet scs)
