004c2fffc0e56bf0547e609bf676162212fe78cb
Douglas Stephen <dstephen@ihmc.us>
2014-05-21 16:35:25 +0000

    NONE: Modified the MultiRobot testing interface to require a contract for providing a simple name for the robot being tested.  Utilize this new functionality in the Bamboo video generation process to make the file names of the generated videos more clear.

--- Acsell/test/us/ihmc/acsell/BonoFlatGroundWalkingTest.java
+++ Acsell/test/us/ihmc/acsell/BonoFlatGroundWalkingTest.java
@@ -7,17 +7,33 @@ import us.ihmc.bambooTools.BambooTools;
 import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTest;
 
 import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 
 public class BonoFlatGroundWalkingTest extends DRCFlatGroundWalkingTest
 {
+
+   private BonoRobotModel robotModel;
+
    @Test
    public void testBONOFlatGroundWalking() throws SimulationExceededMaximumTimeException
    {
       BambooTools.reportTestStartedMessage();
 
       String runName = "BONOFlatGroundWalkingTest";
-      BonoRobotModel robotModel = new BonoRobotModel(false, false);
+      robotModel = new BonoRobotModel(false, false);
 
       setupAndTestFlatGroundSimulationTrack(robotModel, runName);
+   }
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.BONO);
    }
 }
