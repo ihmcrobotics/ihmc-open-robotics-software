ba3badbf180e273cead044b918f983e483eb0805
Jerry Pratt <jpratt@ihmc.us>
2014-05-21 22:45:40 +0000

    DCR-1058: Added hipYawOffset to the FullRobotModelCorruptor.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/corruptors/FullRobotModelCorruptor.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/corruptors/FullRobotModelCorruptor.java
@@ -1,7 +1,14 @@
 package us.ihmc.commonWalkingControlModules.corruptors;
 
+import javax.media.j3d.Transform3D;
+import javax.vecmath.Vector3d;
+
+import us.ihmc.robotSide.RobotSide;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
+import us.ihmc.utilities.humanoidRobot.partNames.LegJointName;
 import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
 import us.ihmc.utilities.screwTheory.RigidBody;
 
 import com.yobotics.simulationconstructionset.DoubleYoVariable;
@@ -9,6 +16,7 @@ import com.yobotics.simulationconstructionset.VariableChangedListener;
 import com.yobotics.simulationconstructionset.YoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
 import com.yobotics.simulationconstructionset.util.math.frames.YoFramePoint;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFrameVector;
 
 public class FullRobotModelCorruptor
 {
@@ -18,6 +26,9 @@ public class FullRobotModelCorruptor
    private final DoubleYoVariable chestMass;
    private final FramePoint tempFramePoint = new FramePoint();
 
+   private final YoFrameVector hipYawOffset;
+
+   
    public FullRobotModelCorruptor(final FullRobotModel fullRobotModel, YoVariableRegistry parentRegistry)
    {
       FramePoint originalChestCoMOffset = new FramePoint();
@@ -54,6 +65,30 @@ public class FullRobotModelCorruptor
             chest.getInertia().setMass(chestMass.getDoubleValue());
          }
       });
+      
+      
+      
+      hipYawOffset = new YoFrameVector("hipYawOffset", null, registry);
+      
+      hipYawOffset.attachVariableChangedListener(new VariableChangedListener()
+      {
+         @Override
+         public void variableChanged(YoVariable<?> v)
+         {
+            for (RobotSide robotSide : RobotSide.values)
+            { 
+               OneDoFJoint hipYawJoint = fullRobotModel.getLegJoint(robotSide, LegJointName.HIP_YAW);
+               ReferenceFrame frameBeforeJoint = hipYawJoint.getFrameBeforeJoint();
+
+               Transform3D postCorruptionTransform = new Transform3D();
+               Vector3d offsetVector = hipYawOffset.getVector3dCopy();
+               offsetVector.setY(robotSide.negateIfRightSide(offsetVector.getY()));
+               postCorruptionTransform.setTranslation(offsetVector);
+               frameBeforeJoint.corruptTransformToParentPostMultiply(postCorruptionTransform);
+            }
+         }
+      });
+
 
       parentRegistry.addChild(registry);
    }
