30e5899f2a0352880bfc62f21e6a470c1ffab510
Jerry Pratt <jpratt@ihmc.us>
2014-05-22 16:03:55 +0000

    NONE: Added Corruptors for Joint angle offsets. Won't fixBamboo.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/corruptors/FullRobotModelCorruptor.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/corruptors/FullRobotModelCorruptor.java
@@ -1,14 +1,17 @@
 package us.ihmc.commonWalkingControlModules.corruptors;
 
 import javax.media.j3d.Transform3D;
+import javax.vecmath.AxisAngle4d;
 import javax.vecmath.Vector3d;
 
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 import us.ihmc.utilities.humanoidRobot.partNames.LegJointName;
+import us.ihmc.utilities.humanoidRobot.partNames.RobotSpecificJointNames;
 import us.ihmc.utilities.math.geometry.FramePoint;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.screwTheory.OneDoFJoint;
+import us.ihmc.utilities.screwTheory.RevoluteJoint;
 import us.ihmc.utilities.screwTheory.RigidBody;
 
 import com.yobotics.simulationconstructionset.DoubleYoVariable;
@@ -24,11 +27,10 @@ public class FullRobotModelCorruptor
 
    private final YoFramePoint chestCoMOffset;
    private final DoubleYoVariable chestMass;
-   private final FramePoint tempFramePoint = new FramePoint();
-
    private final YoFrameVector hipYawOffset;
 
-   
+   private final FramePoint tempFramePoint = new FramePoint();
+
    public FullRobotModelCorruptor(final FullRobotModel fullRobotModel, YoVariableRegistry parentRegistry)
    {
       FramePoint originalChestCoMOffset = new FramePoint();
@@ -40,13 +42,12 @@ public class FullRobotModelCorruptor
 
       chestCoMOffset.attachVariableChangedListener(new VariableChangedListener()
       {
-
          @Override
          public void variableChanged(YoVariable<?> v)
          {
             RigidBody chest = fullRobotModel.getChest();
             tempFramePoint.setToZero(chestCoMOffset.getReferenceFrame());
-            
+
             chestCoMOffset.getFrameTuple(tempFramePoint);
 
             chest.setCoMOffset(tempFramePoint);
@@ -65,18 +66,18 @@ public class FullRobotModelCorruptor
             chest.getInertia().setMass(chestMass.getDoubleValue());
          }
       });
-      
-      
-      
+
+
+
       hipYawOffset = new YoFrameVector("hipYawOffset", null, registry);
-      
+
       hipYawOffset.attachVariableChangedListener(new VariableChangedListener()
       {
          @Override
          public void variableChanged(YoVariable<?> v)
          {
             for (RobotSide robotSide : RobotSide.values)
-            { 
+            {
                OneDoFJoint hipYawJoint = fullRobotModel.getLegJoint(robotSide, LegJointName.HIP_YAW);
                ReferenceFrame frameBeforeJoint = hipYawJoint.getFrameBeforeJoint();
 
@@ -89,6 +90,34 @@ public class FullRobotModelCorruptor
          }
       });
 
+
+      // Joint Calibration offset errors:
+      RobotSpecificJointNames robotSpecificJointNames = fullRobotModel.getRobotSpecificJointNames();
+      LegJointName[] legJointNames = robotSpecificJointNames.getLegJointNames();
+      
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         for (LegJointName legJointName : legJointNames)
+         {
+            RevoluteJoint oneDoFJoint = (RevoluteJoint) fullRobotModel.getLegJoint(robotSide, legJointName);
+            final ReferenceFrame frameBeforeJoint = oneDoFJoint.getFrameBeforeJoint();
+            final Vector3d jointAxis = oneDoFJoint.getJointAxis().getVectorCopy();
+            
+            final Transform3D preCorruptionTransform = new Transform3D();
+
+            final DoubleYoVariable offset = new DoubleYoVariable(robotSide + legJointName.getCamelCaseNameForMiddleOfExpression() + "Offset", registry);
+            
+            offset.addVariableChangedListener(new VariableChangedListener(){
+
+               @Override
+               public void variableChanged(YoVariable<?> v)
+               {
+                  AxisAngle4d axisAngle = new AxisAngle4d(jointAxis, offset.getDoubleValue());
+                  preCorruptionTransform.set(axisAngle);
+                  frameBeforeJoint.corruptTransformToParentPreMultiply(preCorruptionTransform);
+               }});
+         }
+      }
 
       parentRegistry.addChild(registry);
    }
