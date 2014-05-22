ff1d89a93938209270ef9f2e0a039a080cc95d40
Jordan Lack <jlack@ihmc.us>
2014-05-22 17:48:20 +0000

    DRC-1305: Added more single support push recovery tests. Extended Valkyrie max step length for simulation to increase push recovery capability. Separated double support test from single support tests for now, should probably be combined later.

--- test/us/ihmc/valkyrie/ValkyriePushInSingleSupportTest.java
+++ test/us/ihmc/valkyrie/ValkyriePushInSingleSupportTest.java
@@ -1,29 +1,21 @@
 package us.ihmc.valkyrie;
 
-import javax.vecmath.Vector3d;
-
-import org.junit.Test;
-
 import us.ihmc.bambooTools.BambooTools;
 import us.ihmc.darpaRoboticsChallenge.DRCPushRecoveryTest;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 
-import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
-
 public class ValkyriePushInSingleSupportTest extends DRCPushRecoveryTest
 {
-   private final double forceMagnitude = 1000.0;
-   private final double forceDuration = 0.05;
-   private final Vector3d forceDirection = new Vector3d(0.0, -1.0, 0.0);
-   
-   @Test
-   public void testValkyriePush() throws SimulationExceededMaximumTimeException, InterruptedException
-   {
-      BambooTools.reportTestStartedMessage();
 
-      String runName = "ValkyriePushDuringSwing";
-      DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
-      
-      setupAndTestPushInSingleSupport(robotModel, runName, forceMagnitude, forceDuration, forceDirection);
-   }
+	   @Override
+	   public DRCRobotModel getRobotModel()
+	   {
+	      return new ValkyrieRobotModel(false,false);
+	   }
+
+	   @Override
+	   public String getSimpleRobotName()
+	   {
+	      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.VALKYRIE);
+	   }
 }
