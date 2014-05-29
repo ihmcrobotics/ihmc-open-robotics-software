d99a394b210e2489ec3e0b597772eb980846cbc3
Atlas Logging Computer <AtlasLoggingComputer@unknown>
2014-05-29 16:45:18 +0000

    DRC-1058: Added userDesiredHeadRoll.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/UserDesiredHeadOrientationProvider.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/UserDesiredHeadOrientationProvider.java
@@ -13,65 +13,67 @@ import com.yobotics.simulationconstructionset.YoVariableRegistry;
 
 public class UserDesiredHeadOrientationProvider extends DesiredHeadOrientationProvider
 {
-   private final DoubleYoVariable userDesiredHeadPitch, userDesiredHeadYaw, userDesiredNeckPitch;
+   private final DoubleYoVariable userDesiredHeadPitch, userDesiredHeadYaw, userDesiredNeckPitch, userDesiredHeadRoll;
    private final ReferenceFrame headOrientationFrame;
-   
+
    private final AtomicBoolean isNewHeadOrientationInformationAvailable = new AtomicBoolean(true);
    private final AtomicDouble desiredJointForExtendedNeckPitchRangeAngle = new AtomicDouble(0.0);
    private final FrameOrientation desiredHeadOrientation = new FrameOrientation();
-   
+
    public UserDesiredHeadOrientationProvider(ReferenceFrame headOrientationFrame, YoVariableRegistry registry)
    {
       super(headOrientationFrame);
 
       this.headOrientationFrame = headOrientationFrame;
-      
+
       userDesiredHeadPitch = new DoubleYoVariable("userDesiredHeadPitch", registry);
       userDesiredHeadYaw = new DoubleYoVariable("userDesiredHeadYaw", registry);
       userDesiredNeckPitch = new DoubleYoVariable("userDesiredNeckPitch", registry);
+      userDesiredHeadRoll = new DoubleYoVariable("userDesiredHeadRoll", registry);
 
       userDesiredHeadPitch.addVariableChangedListener(new VariableChangedListener()
       {
-         
-         public void variableChanged(YoVariable v)
+         public void variableChanged(YoVariable<?> v)
          {
             desiredJointForExtendedNeckPitchRangeAngle.set(userDesiredHeadPitch.getDoubleValue());
          }
       });
-      
+
       setupListeners();
    }
-   
+
    private void setupListeners()
    {
       VariableChangedListener variableChangedListener = new VariableChangedListener()
       {
-         public void variableChanged(YoVariable v)
+         public void variableChanged(YoVariable<?> v)
          {
             isNewHeadOrientationInformationAvailable.set(true);
-            desiredHeadOrientation.set(headOrientationFrame, userDesiredHeadYaw.getDoubleValue(), userDesiredNeckPitch.getDoubleValue(), 0.0);
+            desiredHeadOrientation.set(headOrientationFrame, userDesiredHeadYaw.getDoubleValue(), userDesiredNeckPitch.getDoubleValue(),
+                  userDesiredHeadRoll.getDoubleValue());
          }
       };
 
       userDesiredHeadPitch.addVariableChangedListener(variableChangedListener);
       userDesiredHeadYaw.addVariableChangedListener(variableChangedListener);
       userDesiredNeckPitch.addVariableChangedListener(variableChangedListener);
-      
+      userDesiredHeadRoll.addVariableChangedListener(variableChangedListener);
+
       variableChangedListener.variableChanged(null);
    }
-   
+
    public double getDesiredExtendedNeckPitchJointAngle()
    {
       return desiredJointForExtendedNeckPitchRangeAngle.getAndSet(Double.NaN);
    }
-   
+
    public boolean isNewHeadOrientationInformationAvailable()
    {
       return isNewHeadOrientationInformationAvailable.getAndSet(false);
    }
 
    public FrameOrientation getDesiredHeadOrientation()
-   {      
+   {
       return desiredHeadOrientation;
    }
 }
