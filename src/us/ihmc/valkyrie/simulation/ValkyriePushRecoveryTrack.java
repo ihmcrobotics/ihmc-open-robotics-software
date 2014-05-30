ee4d6f1d2ca005631b4db95dc8cf5698aee5a851
Luca Fiorio <lfiorio@ihmc.us>
2014-05-30 01:33:01 +0000

    DRC-1216: adding Valkyrie push recovery track

---
+++ src/us/ihmc/valkyrie/simulation/ValkyriePushRecoveryTrack.java
@@ -0,0 +1,83 @@
+package us.ihmc.valkyrie.simulation;
+
+import javax.vecmath.Vector3d;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.commonWalkingControlModules.automaticSimulationRunner.AutomaticSimulationRunner;
+import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
+import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
+import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
+import us.ihmc.darpaRoboticsChallenge.controllers.DRCPushRobotController;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
+import us.ihmc.darpaRoboticsChallenge.validation.YoVariableThreadAccessValidator;
+import us.ihmc.darpaRoboticsChallenge.visualization.SliderBoardFactory;
+import us.ihmc.darpaRoboticsChallenge.visualization.WalkControllerSliderBoard;
+import us.ihmc.graphics3DAdapter.GroundProfile;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+
+import com.martiansoftware.jsap.JSAPException;
+import com.yobotics.simulationconstructionset.BooleanYoVariable;
+import com.yobotics.simulationconstructionset.SimulationConstructionSet;
+import com.yobotics.simulationconstructionset.util.FlatGroundProfile;
+
+public class ValkyriePushRecoveryTrack
+{
+   private final static boolean VISUALIZE_FORCE = true;
+   
+   public static void main(String[] args) throws JSAPException
+   {
+      DRCRobotModel model = new ValkyrieRobotModel(false, false);
+      final double groundHeight = 0.0;
+
+      GroundProfile groundProfile = new FlatGroundProfile(groundHeight);
+
+      YoVariableThreadAccessValidator.registerAccessValidator();
+      AutomaticSimulationRunner automaticSimulationRunner = null;
+      SliderBoardFactory sliderBoardFactory = WalkControllerSliderBoard.getFactory();
+      DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false, sliderBoardFactory);
+
+      DRCSCSInitialSetup scsInitialSetup = new DRCSCSInitialSetup(groundProfile, model.getSimulateDT());
+      scsInitialSetup.setDrawGroundProfile(true);
+      scsInitialSetup.setInitializeEstimatorToActual(true);
+
+      double initialYaw = 0.3;
+      DRCRobotInitialSetup<SDFRobot> robotInitialSetup = model.getDefaultRobotInitialSetup(groundHeight, initialYaw);
+
+      boolean useVelocityAndHeadingScript = true;
+      boolean cheatWithGroundHeightAtForFootstep = false;
+
+      DRCFlatGroundWalkingTrack track = new DRCFlatGroundWalkingTrack(robotInitialSetup, guiInitialSetup, scsInitialSetup, useVelocityAndHeadingScript,
+            automaticSimulationRunner, 16000, cheatWithGroundHeightAtForFootstep, model);
+
+      SDFRobot robot = track.getDrcSimulation().getRobot();
+      SDFFullRobotModel fullRobotModel = model.createFullRobotModel();
+      DRCPushRobotController pushRobotController = new DRCPushRobotController(robot, fullRobotModel);
+
+      pushRobotController.addPushButtonToSCS(track.getSimulationConstructionSet());
+      
+      double defaultForceDurationInSeconds = 0.15;
+      double defaultForceMagnitude = 300.0;
+      Vector3d defaultForceDirection = new Vector3d(1.0, 0.0, 0.0);
+      
+      SimulationConstructionSet scs = track.getSimulationConstructionSet();
+      
+      @SuppressWarnings("deprecation")
+      BooleanYoVariable enable = (BooleanYoVariable) scs.getVariable("enablePushRecovery");
+      @SuppressWarnings("deprecation")
+      BooleanYoVariable enableDS = (BooleanYoVariable) scs.getVariable("enablePushRecoveryFromDoubleSupport");
+      // enable push recovery
+      enable.set(true);
+      enableDS.set(true);
+      
+      if(VISUALIZE_FORCE)
+      {
+         scs.addDynamicGraphicObject(pushRobotController.getForceVisualizer());
+      }
+      
+      pushRobotController.setPushDuration(defaultForceDurationInSeconds);
+      pushRobotController.setPushForceMagnitude(defaultForceMagnitude);
+      pushRobotController.setPushForceDirection(defaultForceDirection);
+   }
+}
