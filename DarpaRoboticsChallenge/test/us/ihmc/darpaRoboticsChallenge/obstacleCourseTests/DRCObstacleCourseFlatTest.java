343afdb7d13cbc12434fdff73845f3f80443866b
Douglas Stephen <dstephen@ihmc.us>
2014-05-30 17:08:49 +0000

    NONE: Revert last commit.  SVN is just fine.

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/obstacleCourseTests/DRCObstacleCourseFlatTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/obstacleCourseTests/DRCObstacleCourseFlatTest.java
@@ -29,7 +29,6 @@ import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimu
 
 public abstract class DRCObstacleCourseFlatTest implements MultiRobotTestInterface
 {
-   //test commit comment.
    private static final boolean KEEP_SCS_UP = false;
 
    private static final boolean createMovie = BambooTools.doMovieCreation();
