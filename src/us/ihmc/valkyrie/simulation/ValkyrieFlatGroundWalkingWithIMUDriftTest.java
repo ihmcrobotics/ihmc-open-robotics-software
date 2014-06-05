63b6168fb9878f7612eee1e920149edfe7b0136e
Sylvain Bertrand <sbertrand@ihmc.us>
2014-06-05 17:19:11 +0000

    NONE: Precommit for FusedIMUSensor for PushPush.

---
+++ src/us/ihmc/valkyrie/simulation/ValkyrieFlatGroundWalkingWithIMUDriftTest.java
@@ -0,0 +1,65 @@
+package us.ihmc.valkyrie.simulation;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.YoVariable;
+import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+
+import us.ihmc.bambooTools.BambooTools;
+import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
+import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingWithIMUDriftTest;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.sensorProcessing.signalCorruption.OrientationConstantAcceleratingYawDriftCorruptor;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+
+public class ValkyrieFlatGroundWalkingWithIMUDriftTest extends DRCFlatGroundWalkingWithIMUDriftTest
+{
+   private DRCRobotModel robotModel;
+
+   @Test
+   public void testValkyrieFlatGroundWalking() throws SimulationExceededMaximumTimeException
+   {
+      BambooTools.reportTestStartedMessage();
+
+      String runName = "ValkyrieFlatGroundWalkingTest";
+      robotModel = new ValkyrieRobotModel(false, false);
+
+      DRCFlatGroundWalkingTrack track = setupFlatGroundSimulationTrack(robotModel);
+      YoVariable[] allVariables = track.getSimulationConstructionSet().getAllVariablesArray();
+
+      ArrayList<DoubleYoVariable> yawDriftAccelerationVariables = new ArrayList<>();
+
+      for (YoVariable<?> yoVariable : allVariables)
+      {
+         if (yoVariable.getName().endsWith(OrientationConstantAcceleratingYawDriftCorruptor.SIMULATED_YAW_DRIFT_ACCELERATION))
+            yawDriftAccelerationVariables.add((DoubleYoVariable) yoVariable);
+      }
+
+      double driftAccelerationMagnitude = 1.0;
+      double sign = 1.0;
+
+      for (int i = 0; i < yawDriftAccelerationVariables.size(); i++)
+      {
+         yawDriftAccelerationVariables.get(i).set(sign * driftAccelerationMagnitude);
+         sign *= -driftAccelerationMagnitude;
+      }
+      
+      simulateAndAssertGoodWalking(track, runName);
+   }
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.VALKYRIE);
+   }
+
+}
