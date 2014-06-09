9727349e84c0166ab8c5b4dbafbb3cd4b2504541
Sylvain Bertrand <sbertrand@ihmc.us>
2014-06-09 16:23:02 +0000

    NONE: Removed unused armJointLimits in DRCRobotPhysicalProperties.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/drcRobot/DRCRobotPhysicalProperties.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/drcRobot/DRCRobotPhysicalProperties.java
@@ -1,17 +1,7 @@
 package us.ihmc.darpaRoboticsChallenge.drcRobot;
 
-import java.util.EnumMap;
-
-import us.ihmc.utilities.Pair;
-import us.ihmc.utilities.humanoidRobot.partNames.ArmJointName;
 
 public abstract class DRCRobotPhysicalProperties
 {
-	public final EnumMap<ArmJointName, Pair<Double, Double>> armJointLimits = new EnumMap<ArmJointName, Pair<Double, Double>>(ArmJointName.class);
-	
-	public abstract double getAnkleHeight();
-	public Pair<Double, Double> getArmJointMinAndMax(ArmJointName jointName)
-	{
-		return armJointLimits.get(jointName);
-	}
+   public abstract double getAnkleHeight();
 }
