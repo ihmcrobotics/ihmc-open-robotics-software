228fb8c953ea04224b5a8850c6971fa3297a873d
Sylvain Bertrand <sbertrand@ihmc.us>
2014-06-09 23:07:27 +0000

    NONE: Renamed variables in AtlasPhysicalProperties to use camel case.

--- Atlas/src/us/ihmc/atlas/parameters/AtlasPhysicalProperties.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasPhysicalProperties.java
@@ -13,13 +13,13 @@ public class AtlasPhysicalProperties extends DRCRobotPhysicalProperties
 
    // this transform is probably in the wrong direction.  The ankle is above the sole,  so the origin of the ankle should have a positive z value
    // from the point of view of the sole.  With this transform it will have a negative value -- PA
-   public static final double foot_width = 0.12;    // 0.08;   //0.124887;
-   public static final double toe_width = 0.095;    // 0.07;   //0.05;   //
-   public static final double foot_length = 0.255;
-   public static final double foot_back = 0.09;    // 0.06;   //0.082;    // 0.07;
-   public static final double foot_start_toetaper_from_back = 0.195;
-   public static final double foot_forward = foot_length - foot_back;    // 0.16;   //0.178;    // 0.18;
-   public static final Transform3D ankle_to_sole_frame_tranform = TransformTools.createTranslationTransform(new Vector3d(foot_length / 2.0 - foot_back, 0.0, -ankleHeight));
+   public static final double footWidth = 0.12;    // 0.08;   //0.124887;
+   public static final double toeWidth = 0.095;    // 0.07;   //0.05;   //
+   public static final double footLength = 0.255;
+   public static final double footBack = 0.09;    // 0.06;   //0.082;    // 0.07;
+   public static final double footStartToetaperFromBack = 0.195;
+   public static final double footForward = footLength - footBack;    // 0.16;   //0.178;    // 0.18;
+   public static final Transform3D ankleToSoleFrameTranform = TransformTools.createTranslationTransform(new Vector3d(footLength / 2.0 - footBack, 0.0, -ankleHeight));
    public static final double shinLength = 0.374;
    public static final double thighLength = 0.422;
 
