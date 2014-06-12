7644a18c7e6527cc74e745bc2f3a9a374588e1af
Georg Wiedebach <gwiedebach@ihmc.us>
2014-06-12 22:55:36 +0000

    DRC-1392: rip some more private states out of FootControlModule

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/foot/TouchdownOnHeelState.java
@@ -0,0 +1,51 @@
+package us.ihmc.commonWalkingControlModules.controlModules.foot;
+
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.ContactablePlaneBody;
+import us.ihmc.commonWalkingControlModules.controlModules.RigidBodySpatialAccelerationControlModule;
+import us.ihmc.commonWalkingControlModules.controlModules.foot.FootControlModule.ConstraintType;
+import us.ihmc.commonWalkingControlModules.kinematics.SpatialAccelerationProjector;
+import us.ihmc.commonWalkingControlModules.momentumBasedController.MomentumBasedController;
+import us.ihmc.robotSide.RobotSide;
+
+import com.yobotics.simulationconstructionset.BooleanYoVariable;
+import com.yobotics.simulationconstructionset.DoubleYoVariable;
+import com.yobotics.simulationconstructionset.EnumYoVariable;
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFramePoint;
+import com.yobotics.simulationconstructionset.util.math.frames.YoFrameVector;
+import com.yobotics.simulationconstructionset.util.trajectory.DoubleTrajectoryGenerator;
+
+public class TouchdownOnHeelState extends AbstractOnEdgeState
+{
+   public TouchdownOnHeelState(DoubleTrajectoryGenerator pitchTouchdownTrajectoryGenerator,
+         
+         YoFramePoint yoDesiredPosition, YoFrameVector yoDesiredLinearVelocity, YoFrameVector yoDesiredLinearAcceleration,
+         RigidBodySpatialAccelerationControlModule accelerationControlModule,
+         MomentumBasedController momentumBasedController, ContactablePlaneBody contactableBody,
+         EnumYoVariable<ConstraintType> requestedState, int jacobianId, 
+         DoubleYoVariable nullspaceMultiplier, BooleanYoVariable jacobianDeterminantInRange,
+         BooleanYoVariable doSingularityEscape, BooleanYoVariable forceFootAccelerateIntoGround,
+         LegSingularityAndKneeCollapseAvoidanceControlModule legSingularityAndKneeCollapseAvoidanceControlModule,
+         RobotSide robotSide, YoVariableRegistry registry,
+         
+         SpatialAccelerationProjector spatialAccelerationProjector)
+   {
+      super(ConstraintType.HEEL_TOUCHDOWN, pitchTouchdownTrajectoryGenerator,
+            
+            yoDesiredPosition, yoDesiredLinearVelocity,
+            yoDesiredLinearAcceleration, accelerationControlModule, momentumBasedController,
+            contactableBody, requestedState, jacobianId, nullspaceMultiplier,
+            jacobianDeterminantInRange, doSingularityEscape,
+            forceFootAccelerateIntoGround, legSingularityAndKneeCollapseAvoidanceControlModule,
+            robotSide, registry,
+            
+            spatialAccelerationProjector);
+   }
+
+   @Override
+   public void doTransitionIntoAction()
+   {
+      super.doTransitionIntoAction();
+      setTouchdownOnEdgeGains();
+   }
+}
