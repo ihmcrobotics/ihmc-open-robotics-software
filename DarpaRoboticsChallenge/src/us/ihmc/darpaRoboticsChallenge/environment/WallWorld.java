1a5a87c3466e5215e9ab283fa327ebb6b86c2625
Jesper Smith <jsmith@ihmc.us>
2014-06-12 20:52:46 +0000

    DRC-1401: Simple wall world for multicontact. does not fixbamboo

---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/WallWorld.java
@@ -0,0 +1,18 @@
+package us.ihmc.darpaRoboticsChallenge.environment;
+
+import com.yobotics.simulationconstructionset.util.ground.CombinedTerrainObject;
+
+public class WallWorld extends CombinedTerrainObject
+{
+
+   public WallWorld()
+   {
+      super("WallWorld");
+
+      addBox(-5, 0.75, 5, 2, 4);
+      
+      addBox(-10, -10, 10, 10, 1);
+   }
+
+   
+}
