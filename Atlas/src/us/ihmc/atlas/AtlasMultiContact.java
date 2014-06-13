11599475d9a5a138a89673bf504f72ccf5c22126
Sylvain Bertrand <sbertrand@ihmc.us>
2014-06-13 18:26:17 +0000

    NONE: Cleaned up high level factories. Forgot a file, sorry! Might fixbamboo

--- Atlas/src/us/ihmc/atlas/AtlasMultiContact.java
+++ Atlas/src/us/ihmc/atlas/AtlasMultiContact.java
@@ -12,7 +12,9 @@ import us.ihmc.atlas.parameters.AtlasRobotMultiContactControllerParameters;
 import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ContactableBodiesFactory;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.MultiContactTestHumanoidControllerFactory;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.PolyvalentHighLevelHumanoidControllerFactory;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.HighLevelState;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.MultiContactTestHumanoidControllerFactory;
 import us.ihmc.darpaRoboticsChallenge.DRCConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
@@ -99,8 +101,10 @@ public class AtlasMultiContact
          }
       };
 
-      MultiContactTestHumanoidControllerFactory highLevelHumanoidControllerFactory = new MultiContactTestHumanoidControllerFactory(contactableBodiesFactory,
-            footContactSides, handContactSides, controllerParameters, armControllerParameters);
+      PolyvalentHighLevelHumanoidControllerFactory highLevelHumanoidControllerFactory = new PolyvalentHighLevelHumanoidControllerFactory(
+            contactableBodiesFactory, null, controllerParameters, armControllerParameters, false, false, HighLevelState.DO_NOTHING_BEHAVIOR);
+      highLevelHumanoidControllerFactory.addHighLevelBehaviorFactory(new MultiContactTestHumanoidControllerFactory(controllerParameters, footContactSides,
+            handContactSides, true));
       DRCRobotMomentumBasedControllerFactory controllerFactory = new DRCRobotMomentumBasedControllerFactory(contactableBodiesFactory, highLevelHumanoidControllerFactory,
             DRCConfigParameters.contactTresholdForceForSCS, sensorInformation.getFeetForceSensorNames());
 
