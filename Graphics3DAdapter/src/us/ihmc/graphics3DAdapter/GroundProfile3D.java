57f0c33ade77867b90728714c0999d8bca2e18d8
Jerry Pratt <jpratt@ihmc.us>
2014-06-26 23:11:25 +0000

    DRC-1423: Set up so that can now have CombinedTerrainObject3Ds. Got simple sliding block demo working.

---
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/GroundProfile3D.java
@@ -0,0 +1,13 @@
+package us.ihmc.graphics3DAdapter;
+
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import us.ihmc.utilities.math.geometry.BoundingBox3d;
+
+public interface GroundProfile3D
+{
+   public abstract BoundingBox3d getBoundingBox();
+   public abstract boolean isClose(double x, double y, double z);
+   public abstract boolean checkIfInside(double x, double y, double z, Point3d intersectionToPack, Vector3d normalToPack);
+}
