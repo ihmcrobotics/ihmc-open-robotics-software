9fc2cbc687d35064755f300e4301c1283469aa4f
Jerry Pratt <jpratt@ihmc.us>
2014-06-28 04:39:12 +0000

    DRC-1470: Reducing usages of TerrainObject in favor of TerrainObject3D. Got rid of TerrainObject!

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCDemoEnvironmentSimpleSteeringWheelContact.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCDemoEnvironmentSimpleSteeringWheelContact.java
@@ -28,7 +28,6 @@ import com.yobotics.simulationconstructionset.util.environments.SelectableObject
 import com.yobotics.simulationconstructionset.util.graphics.DynamicGraphicObjectsListRegistry;
 import com.yobotics.simulationconstructionset.util.ground.CombinedTerrainObject3D;
 import com.yobotics.simulationconstructionset.util.ground.Contactable;
-import com.yobotics.simulationconstructionset.util.ground.TerrainObject;
 import com.yobotics.simulationconstructionset.util.ground.TerrainObject3D;
 
 public class DRCDemoEnvironmentSimpleSteeringWheelContact implements CommonAvatarEnvironmentInterface
