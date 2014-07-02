52099a1cf4de9e1f06f1bcbc9cc34dc5868b323f
Jesper Smith <jsmith@ihmc.us>
2014-07-02 16:54:10 +0000

    NONE: Hopefully fixed lowlevel slider board controller. Removed older versions, leaving a single one that should do everything. Does not fixbamboo

---
+++ src/us/ihmc/valkyrie/controllers/ValkyrieSliderBoardSelectableJoints.java
@@ -0,0 +1,10 @@
+package us.ihmc.valkyrie.controllers;
+
+public enum ValkyrieSliderBoardSelectableJoints
+{
+   LeftHipRotator, LeftHipAdductor, LeftHipExtensor, LeftKneeExtensor, LeftAnkleExtensor, LeftAnkle, RightHipRotator, RightHipAdductor, RightHipExtensor,
+   RightKneeExtensor, RightAnkleExtensor, RightAnkle, WaistRotator, WaistExtensor, WaistLateralExtensor, LeftShoulderExtensor, LeftShoulderAdductor,
+   LeftShoulderSupinator, LeftElbowExtensor, LeftForearmSupinator, LeftWristExtensor, LeftWrist, LowerNeckExtensor, NeckRotator, UpperNeckExtensor,
+   RightShoulderExtensor, RightShoulderAdductor, RightShoulderSupinator, RightElbowExtensor, RightForearmSupinator, RightWristExtensor,
+   RightWrist
+}
