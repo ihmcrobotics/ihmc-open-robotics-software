486cac6726d22c9ad836762a7f5803df5c199ef9
Jesper Smith <jsmith@ihmc.us>
2014-07-02 20:17:44 +0000

    NONE: Removed super duper high resolution comms.

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/DRCConfigParametersTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/DRCConfigParametersTest.java
@@ -1,6 +1,7 @@
 package us.ihmc.darpaRoboticsChallenge;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -13,14 +14,12 @@ public class DRCConfigParametersTest
    public void test()
    {
       assertFalse("Do not check in DRCConfigParameters.USE_SLIDER_FOR_POSE_PLAYBACK = true!!", DRCConfigParameters.USE_SLIDER_FOR_POSE_PLAYBACK);
-      
-      assertFalse("Do not check in DRCConfigParameters.USE_SUPER_DUPER_HIGH_RESOLUTION_FOR_COMMS = true!!", DRCConfigParameters.USE_SUPER_DUPER_HIGH_RESOLUTION_FOR_COMMS);
-      
+            
       assertFalse("Do not check in DRCConfigParameters.SEND_HIGH_SPEED_CONFIGURATION_DATA < 100!!", DRCConfigParameters.UI_JOINT_CONFIGURATION_UPDATE_MILLIS < 100);
       
       assertFalse("Do not check in MomentumBasedController.SPY_ON_MOMENTUM_BASED_CONTROLLER = true!!", MomentumBasedController.SPY_ON_MOMENTUM_BASED_CONTROLLER);      
       
-      assertTrue("Do not add parameters to DRCConfigParameters.", DRCConfigParameters.class.getFields().length <= 27);
+      assertTrue("Do not add parameters to DRCConfigParameters.", DRCConfigParameters.class.getFields().length <= 26);
       assertTrue("Do not add parameters to DRCLocalConfigParameters.", DRCLocalConfigParameters.class.getFields().length <= 9);
    }
 
