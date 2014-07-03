bd2f8163ad388a2bf43e14121990372f51f99915
Pushyami Kaveti <pkaveti@ihmc.us>
2014-07-03 19:02:06 +0000

    DRC-1477: Commiited  code for visualizing the occupancy grid

--- SensorProcessing/src/us/ihmc/sensorProcessing/pointClouds/octree/OctreeOccupancyExample.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/pointClouds/octree/OctreeOccupancyExample.java
@@ -5,7 +5,10 @@ import georegression.struct.point.Point3D_F64;
 import georegression.struct.shapes.Cube3D_F64;
 import us.ihmc.sensorProcessing.pointClouds.shape.PointCloudTools;
 
+import java.awt.Color;
 import java.util.List;
+
+import com.jme3.math.ColorRGBA;
 
 /**
  * @author Peter Abeles
@@ -14,9 +17,10 @@ public class OctreeOccupancyExample {
 
     public static int NUMBER_OCCUPIED = 10;
     public static double CELL_SIZE = 0.05;
+    
 
-    public static void main(String[] args) {
-        List<Point3D_F64> cloud = PointCloudTools.readPointCloud("../SensorProcessing/data/kinectcloud.txt",-1);
+    public static OctreeOccupancyMap mainMethod(List<Point3D_F64> cloud) {
+        //List<Point3D_F64> cloud = PointCloudTools.readPointCloud("../SensorProcessing/data/kinectcloud.txt",-1);
 
 
         OctreeOccupancyMap map = new OctreeOccupancyMap(CELL_SIZE,new Cube3D_F64(-10,-10,-10,10,10,10));
@@ -30,16 +34,18 @@ public class OctreeOccupancyExample {
                 OccupancyCell c = n.getUserData();
                 if( n.points.size() > NUMBER_OCCUPIED ) {
                     totalOccupied++;
-                    c.probability = 1.0;
+                    c.probability = 0.8;
+                    
                 } else {
-                    c.probability = 0.0;
+                    c.probability = 0.4;
                 }
+                Color color= Color.getHSBColor((float)c.probability, 0.85f, 1.0f);
+                c.setColor(new ColorRGBA(color.getRed() / 255.0f, color.getGreen() / 255.0f, color.getBlue() / 255.0f, 1.0f));
             }
         }
         System.out.println("Leafs occupied = " + totalOccupied + "  out of " + map.getLeafs().size());
 
-        // Someone needs to add visualization code here
-
+       return map;
     }
 
 
