4e1ffda8a192dd5771193fe7b62aa1667dfda469
Nathan Mertins <nmertins@ihmc.us>
2014-07-09 22:52:17 +0000

    DRC-1434: small refactor getting ready to move hand control to a separate application and add Robotiq hand control

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandControlThreadManager.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandControlThreadManager.java
@@ -1,7 +1,22 @@
 package us.ihmc.darpaRoboticsChallenge.handControl;
 
-public interface HandControlThreadManager extends Runnable
-{
-	public void start();
+import us.ihmc.utilities.net.ObjectCommunicator;
 
+public abstract class HandControlThreadManager implements Runnable
+{
+	protected ObjectCommunicator objectCommunicator;
+	
+	public HandControlThreadManager(ObjectCommunicator objectCommunicator)
+	{
+		this.objectCommunicator = objectCommunicator;
+		
+		if (objectCommunicator != null)
+			attachObjectConsumers();
+	}
+	
+	public abstract void start();
+	
+	public abstract void connect();
+	
+	protected abstract void attachObjectConsumers();
 }
