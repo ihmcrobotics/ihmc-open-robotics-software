f6b1ae4760d2261bceeac3958378e3aee765a367
Duncan Calvert <dcalvert@ihmc.us>
2014-07-10 20:42:46 +0000

    DRC-1494: (fixBamboo) Added GPULidar to callbacks.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCLidar.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCLidar.java
@@ -24,34 +24,35 @@ import com.yobotics.simulationconstructionset.simulatedSensors.LidarMount;
 
 public class DRCLidar
 {
-
    public static LidarMount getLidarSensor(SDFRobot robot)
    {
       ArrayList<LidarMount> lidarSensors = robot.getSensors(LidarMount.class);
       if (lidarSensors.size() == 0)
       {
          System.err.println("DRCLidar: No LIDAR units found on SDF Robot.");
+
          return null;
       }
       else if (lidarSensors.size() < 3)
       {
-         System.err.println("DRCLidar: Only one LIDAR unit is supported at this time. Found " + lidarSensors.size()
-               + " LIDAR units. Will use only the first.");
+         System.err.println("DRCLidar: Only one LIDAR unit is supported at this time. Found " + lidarSensors.size() + " LIDAR units. Will use only the first.");
+
          return lidarSensors.get(0);
       }
       else
       {
-            System.err.println("DRCLidar: Only one LIDAR unit is supported at this time. Found " + lidarSensors.size()
-                  + " LIDAR units. Assuming this is Valkyrie and using the third.");
+         System.err.println("DRCLidar: Only one LIDAR unit is supported at this time. Found " + lidarSensors.size()
+                            + " LIDAR units. Assuming this is Valkyrie and using the third.");
+
          return lidarSensors.get(2);
       }
    }
 
    public static void setupDRCRobotLidar(DRCSimulationFactory drcSimulation, ObjectCommunicator objectCommunicator, DRCRobotJointMap jointMap,
-         TimestampProvider timestampProvider, boolean startLidar)
+           TimestampProvider timestampProvider, boolean startLidar)
    {
       Graphics3DAdapter graphics3dAdapter = drcSimulation.getSimulationConstructionSet().getGraphics3dAdapter();
-      if(graphics3dAdapter != null)
+      if (graphics3dAdapter != null)
       {
          LidarMount lidarMount = getLidarSensor(drcSimulation.getRobot());
 
@@ -67,94 +68,95 @@ public class DRCLidar
          lidarMount.setLidar(lidar);
       }
    }
-   
+
    public static void setupDRCRobotPointCloud(DRCSimulationFactory drcSimulation, ObjectCommunicator objectCommunicator, DRCRobotJointMap jointMap,
-         TimestampProvider timestampProvider, boolean startLidar)
+           TimestampProvider timestampProvider, boolean startLidar)
    {
       Graphics3DAdapter graphics3dAdapter = drcSimulation.getSimulationConstructionSet().getGraphics3dAdapter();
-      
+
       LidarMount lidarMount = getLidarSensor(drcSimulation.getRobot());
-      
+
       LidarScanParameters lidarScanParameters = lidarMount.getLidarScanParameters();
       int horizontalRays = lidarScanParameters.pointsPerSweep;
       float fov = lidarScanParameters.sweepYawMax - lidarScanParameters.sweepYawMin;
       float near = lidarScanParameters.minRange;
       float far = lidarScanParameters.maxRange;
-      
-      
+
+
       DRCLidarToPointCloudCallback callback = new DRCLidarToPointCloudCallback(objectCommunicator, lidarScanParameters);
       GPULidar lidar = graphics3dAdapter.createGPULidar(callback, horizontalRays, fov, near, far);
       lidarMount.setLidar(lidar);
    }
 
-   
+
    public static class DRCLidarToPointCloudCallback implements GPULidarCallback
    {
       private final Executor pool = Executors.newSingleThreadExecutor();
 
       private final ObjectCommunicator objectCommunicator;
       private final LidarScanParameters lidarScanParameters;
-      
+
       public DRCLidarToPointCloudCallback(ObjectCommunicator objectCommunicator, LidarScanParameters lidarScanParameters)
       {
          this.objectCommunicator = objectCommunicator;
          this.lidarScanParameters = lidarScanParameters;
       }
-      
+
       @Override
-      public void scan(float[] scan, Transform3D lidarTransform, double time)
+      public void scan(float[] scan, Transform3D lidarTransform, double time, GPULidar gpuLidar)
       {
          Transform3D transform = new Transform3D(lidarTransform);
          final LidarScan lidarScan = new LidarScan(new LidarScanParameters(lidarScanParameters, TimeTools.secondsToNanoSeconds(time)), transform, transform,
-               Arrays.copyOf(scan, scan.length));
-         
+                                        Arrays.copyOf(scan, scan.length));
+
          ArrayList<Point3d> points = lidarScan.getAllPoints();
          Point3d[] pointsArray = new Point3d[points.size()];
          points.toArray(pointsArray);
-         
-         final PointCloudPacket pointCloud = new PointCloudPacket(pointsArray,TimeTools.secondsToNanoSeconds(time));
-         
+
+         final PointCloudPacket pointCloud = new PointCloudPacket(pointsArray, TimeTools.secondsToNanoSeconds(time));
+
          pool.execute(new Runnable()
-         {            
+         {
             @Override
             public void run()
             {
-               objectCommunicator.consumeObject(pointCloud);     
+               objectCommunicator.consumeObject(pointCloud);
             }
          });
       }
-      
+
    }
-   
+
+
    public static class DRCLidarCallback implements GPULidarCallback
    {
       private final Executor pool = Executors.newSingleThreadExecutor();
 
       private final ObjectCommunicator objectCommunicator;
       private final LidarScanParameters lidarScanParameters;
-      
+
       public DRCLidarCallback(ObjectCommunicator objectCommunicator, LidarScanParameters lidarScanParameters)
       {
          this.objectCommunicator = objectCommunicator;
          this.lidarScanParameters = lidarScanParameters;
       }
-      
+
       @Override
-      public void scan(float[] scan, Transform3D lidarTransform, double time)
+      public void scan(float[] scan, Transform3D lidarTransform, double time, GPULidar gpuLidar)
       {
          Transform3D transform = new Transform3D(lidarTransform);
          final LidarScan lidarScan = new LidarScan(new LidarScanParameters(lidarScanParameters, TimeTools.secondsToNanoSeconds(time)), transform, transform,
-               Arrays.copyOf(scan, scan.length));
-         
+                                        Arrays.copyOf(scan, scan.length));
+
          pool.execute(new Runnable()
-         {            
+         {
             @Override
             public void run()
             {
-               objectCommunicator.consumeObject(lidarScan);     
+               objectCommunicator.consumeObject(lidarScan);
             }
          });
       }
    }
-   
+
 }
