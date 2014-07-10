a2b3edd777ac1835b500c0b635b7902592dea357
Luca Fiorio <lfiorio@ihmc.us>
2014-07-10 03:14:20 +0000

    NONE: refactoring all push recovery tests. Now we have tests during walking (swing and transfer states) and during standing (double support state)

--- test/us/ihmc/valkyrie/ValkyriePushRecoveryWalkingTest.java
+++ test/us/ihmc/valkyrie/ValkyriePushRecoveryWalkingTest.java
@@ -1,10 +1,10 @@
 package us.ihmc.valkyrie;
 
 import us.ihmc.bambooTools.BambooTools;
-import us.ihmc.darpaRoboticsChallenge.DRCPushRecoverySingleSupportTest;
+import us.ihmc.darpaRoboticsChallenge.DRCPushRecoveryWalkingTest;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 
-public class ValkyriePushInSingleSupportTest extends DRCPushRecoverySingleSupportTest
+public class ValkyriePushRecoveryWalkingTest extends DRCPushRecoveryWalkingTest
 {
    @Override
    public DRCRobotModel getRobotModel()
