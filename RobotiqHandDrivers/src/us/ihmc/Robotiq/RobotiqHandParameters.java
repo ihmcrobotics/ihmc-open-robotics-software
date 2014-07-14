96afe95b9ce323f3f1954de9d6d136f710ce5f1f
Nolan Manor <nmanor@ihmc.us>
2014-07-14 20:30:39 +0000

    DRC-1434: Refactor and parameter reduction.

--- RobotiqHandDrivers/src/us/ihmc/Robotiq/RobotiqHandParameters.java
+++ RobotiqHandDrivers/src/us/ihmc/Robotiq/RobotiqHandParameters.java
@@ -7,10 +7,4 @@ public final class RobotiqHandParameters
 	static final String RIGHT_HAND_ADDRESS = "192.168.1.13";
 	static final String LEFT_HAND_ADDRESS = "192.168.1.12"; //placeholder
 	static final byte UNIT_ID = 0x02;
-	static final byte MAX_SPEED = (byte) 0xFF;
-	static final byte MAX_FORCE = (byte) 0xFF;
-	static final byte FULLY_OPEN = 0x00;
-	static final byte MIN_SPEED = 0x00;
-	static final byte MIN_FORCE = 0x00;
-	static final byte FULLY_CLOSED = (byte)0xFF;
 }
