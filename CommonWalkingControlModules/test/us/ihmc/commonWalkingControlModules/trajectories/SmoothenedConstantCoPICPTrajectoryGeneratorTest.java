f05a0d9aa94de227358de66d8dfb7beafd57b4ea
Brett Rowberry <browberry@ihmc.us>
2014-07-14 22:23:56 +0000

    NONE: Fixed formatting in SmoothenedConstantCoPICPTrajectoryGeneratorTest.

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/SmoothenedConstantCoPICPTrajectoryGeneratorTest.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/SmoothenedConstantCoPICPTrajectoryGeneratorTest.java
@@ -36,25 +36,21 @@ public class SmoothenedConstantCoPICPTrajectoryGeneratorTest
    {
       Random random = new Random(12525L);
       Vector3d[] jointAxes = new Vector3d[] {X, Y, Z};
-      
-      
+
       RandomFloatingChain randomFloatingChain = new RandomFloatingChain(random, jointAxes);
-      
-      
+
       YoVariableRegistry parentRegistry = new YoVariableRegistry("test");
       int lookahead = 4;
       SmoothenedConstantCoPICPTrajectoryGenerator trajectoryGenerator = new SmoothenedConstantCoPICPTrajectoryGenerator(parentRegistry, lookahead);
-      
+
       int nFootsteps = 15;
       List<Footstep> footsteps = new ArrayList<Footstep>();
-      
+
       List<RevoluteJoint> revoluteJoints = randomFloatingChain.getRevoluteJoints();
       RigidBody rigidBody = revoluteJoints.get(revoluteJoints.size() - 1).getSuccessor();
       ReferenceFrame soleFrame = rigidBody.getBodyFixedFrame();
       ContactablePlaneBody endEffector = new RectangularContactableBody(rigidBody, soleFrame, 1.0, -1.0, 1.0, -1.0);
 
-      
-      
       // Generate footsteps
       double steplength = 0.3; 
       double halfstepwidth = 0.1; 
@@ -62,19 +58,18 @@ public class SmoothenedConstantCoPICPTrajectoryGeneratorTest
       for (int i = 0; i < nFootsteps; i++)
       {
          FramePose pose = new FramePose(ReferenceFrame.getWorldFrame());
-         
-//         pose.setPosition(new FramePoint(ReferenceFrame.getWorldFrame(), RandomTools.generateRandomVector(random)));
-         
+
+         //         pose.setPosition(new FramePoint(ReferenceFrame.getWorldFrame(), RandomTools.generateRandomVector(random)));
+
          FramePoint framepoint = new FramePoint(ReferenceFrame.getWorldFrame()); 
          framepoint.set(i*steplength, flip*halfstepwidth, 0); 
          pose.setPosition(framepoint);
-         
-         
+
          PoseReferenceFrame poseFrame = new PoseReferenceFrame("poseFrame" + i, pose);
          boolean trustHeight = true;
          Footstep footstep = new Footstep(endEffector, poseFrame, trustHeight);
          footsteps.add(footstep);       
-         
+
          flip = -flip; 
       }
 
@@ -83,43 +78,35 @@ public class SmoothenedConstantCoPICPTrajectoryGeneratorTest
       double doubleSupportTime = 0.2;
       double transferIntoWalkingTime = 0.4;
       double steppingTime = singleSupportTime + doubleSupportTime; 
-      
+
       double currentTime = 0.3; 
       boolean isSingeSupport = !true; 
-     
-      
+
       List<FramePoint2d> equivalentConstantCoPs = new ArrayList<FramePoint2d>();
       List<FramePoint2d> desiredICPWaypoints = new ArrayList<FramePoint2d>();
-      
-      
-   // compute lookahead (number) equivalent constant CoPs, desired ICP Waypoints from the equivalent constant CoPs
+
+      // compute lookahead (number) equivalent constant CoPs, desired ICP Waypoints from the equivalent constant CoPs
       trajectoryGenerator.compute(footsteps, w0, singleSupportTime, doubleSupportTime, transferIntoWalkingTime);
-      
-   // get equivalent constant CoPs
+
+      // get equivalent constant CoPs
       equivalentConstantCoPs = trajectoryGenerator.getEquivalentConstantCoPs(footsteps, lookahead); 
-      
+
       // get desired ICP Waypoints 
       desiredICPWaypoints = trajectoryGenerator.getDesiredICPWaypoints(equivalentConstantCoPs, w0, steppingTime); 
-      
+
       // compute the polynomial parameter matrix, which represents the smooth double-support trajectory
       DenseMatrix64F paramMatrix = trajectoryGenerator.computeDoubleSupportpolynomialParams(equivalentConstantCoPs, desiredICPWaypoints, w0, steppingTime, doubleSupportTime); 
-      
-      
+
       // initialize desired ICP, desired ICP velocity and desired equivalent CoP
       FramePoint2d desiredICP = new FramePoint2d(ReferenceFrame.getWorldFrame());
       FrameVector2d desiredICPVelocity = new FrameVector2d(ReferenceFrame.getWorldFrame()); 
       FramePoint2d desiredEquivalentConstantCOP = new FramePoint2d(ReferenceFrame.getWorldFrame());
-      
+
       // Compute desired ICP, desired ICP velocity and desired equivalent CoP (depending of the current time)
       trajectoryGenerator.calcDCMandECMPofTime(equivalentConstantCoPs, desiredICPWaypoints, w0, currentTime, steppingTime, doubleSupportTime, 
             isSingeSupport, desiredICP, desiredICPVelocity, desiredEquivalentConstantCOP, paramMatrix);
-      
+
       int a; 
-      
       a = 0; 
-
    }
-   
-
-
 }
