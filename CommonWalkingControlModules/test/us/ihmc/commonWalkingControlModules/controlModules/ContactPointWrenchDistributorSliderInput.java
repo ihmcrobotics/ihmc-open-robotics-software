2f283071ea45ee5633bddb8146a4a702a19325c6
Brett Rowberry <browberry@ihmc.us>
2014-07-17 15:41:07 +0000

    NONE: Fixed formatting, removed junk comments, cleared warnings in ContactPointWrenchDistributorSliderInput.

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/controlModules/ContactPointWrenchDistributorSliderInput.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/controlModules/ContactPointWrenchDistributorSliderInput.java
@@ -15,27 +15,25 @@ public class ContactPointWrenchDistributorSliderInput implements VariableChanged
    private static final boolean DEBUG = false;
    private final ContactPointGroundReactionWrenchDistributor distributor;
    private final YoVariableRegistry registry = new YoVariableRegistry("ContactPointWrenchDistributorSliderInput");
-   
+
    private final DoubleYoVariable forceXYWeight = new DoubleYoVariable("forceXYWeight", registry);
    private final DoubleYoVariable forceZWeight = new DoubleYoVariable("forceZWeight", registry);
-   
+
    private final DoubleYoVariable torqueXYWeight = new DoubleYoVariable("torqueXYWeight", registry);
    private final DoubleYoVariable torqueZWeight = new DoubleYoVariable("torqueZWeight", registry);
-   
+
    private final DoubleYoVariable epsilonRho = new DoubleYoVariable("epsilonRho", registry);
 
    private final DoubleYoVariable minimumNormalForce = new DoubleYoVariable("minimumNormalForce", registry);
-   
+
    public ContactPointWrenchDistributorSliderInput(SimulationConstructionSet scs, YoVariableRegistry parentRegistry, ReferenceFrame centerOfMassFrame)
    {
       distributor = new ContactPointGroundReactionWrenchDistributor(centerOfMassFrame, parentRegistry);
 
-//      setWeightsForEncouragingLunging();
-//      setWeightsForDiscouragingLunging();
       setTwansTunedWeights();
-      
+
       updateWeights();
-      
+
       MidiSliderBoard sliderBoard = new MidiSliderBoard(scs);
       sliderBoard.setSlider(0, "forceXYWeight", registry, 0.0, 100.0);
       sliderBoard.setSlider(1, "forceZWeight", registry, 0.0, 100.0);
@@ -43,49 +41,49 @@ public class ContactPointWrenchDistributorSliderInput implements VariableChanged
       sliderBoard.setSlider(3, "torqueZWeight", registry, 0.0, 100.0);
       sliderBoard.setSlider(4, "epsilonRho", registry, 0.0, 10.0);
       sliderBoard.setSlider(5, "minimumNormalForce", registry, 0.0, 100.0);
-      
+
       sliderBoard.attachVariableChangedListener(this);
       parentRegistry.addChild(registry);
    }
-   
+
    public void setTwansTunedWeights()
    {
       forceXYWeight.set(1.0);
       forceZWeight.set(1.0);
-      torqueXYWeight.set(1.0); 
-      torqueZWeight.set(1.0); 
-      
+      torqueXYWeight.set(1.0);
+      torqueZWeight.set(1.0);
+
       epsilonRho.set(0.001);
       minimumNormalForce.set(0.0);
    }
-   
+
    public void setWeightsForEncouragingLunging()
    {
       forceXYWeight.set(100.0);
       forceZWeight.set(100.0);
-      torqueXYWeight.set(20.0); 
-      torqueZWeight.set(20.0); 
-      
+      torqueXYWeight.set(20.0);
+      torqueZWeight.set(20.0);
+
       epsilonRho.set(0.2);
       minimumNormalForce.set(10.0);
    }
-   
+
    public void setWeightsForDiscouragingLunging()
    {
-      forceXYWeight.set(40.0); 
+      forceXYWeight.set(40.0);
       forceZWeight.set(100.0);
-      torqueXYWeight.set(100.0); 
-      torqueZWeight.set(40.0); 
-      
-      epsilonRho.set(0.2); 
+      torqueXYWeight.set(100.0);
+      torqueZWeight.set(40.0);
+
+      epsilonRho.set(0.2);
       minimumNormalForce.set(10.0);
    }
-   
+
    public ContactPointGroundReactionWrenchDistributor getDistributor()
    {
       return distributor;
    }
-   
+
    public void updateWeights()
    {
       printIfDebug("Updating Weights");
@@ -94,22 +92,19 @@ public class ContactPointWrenchDistributorSliderInput implements VariableChanged
       printIfDebug("forceXYWeight = " + forceXYWeight);
       printIfDebug("forceZWeight = " + forceZWeight);
 
-      double[] diagonalCWeights = new double[]{torqueXYWeight.getDoubleValue(), torqueXYWeight.getDoubleValue(), torqueZWeight.getDoubleValue(), 
-            forceXYWeight.getDoubleValue(), forceXYWeight.getDoubleValue(), forceZWeight.getDoubleValue()};
+      double[] diagonalCWeights = new double[] { torqueXYWeight.getDoubleValue(), torqueXYWeight.getDoubleValue(), torqueZWeight.getDoubleValue(),
+            forceXYWeight.getDoubleValue(), forceXYWeight.getDoubleValue(), forceZWeight.getDoubleValue() };
       distributor.setWeights(diagonalCWeights, 1.0, epsilonRho.getDoubleValue());
    }
 
    private void printIfDebug(String string)
    {
-      if (DEBUG) 
+      if (DEBUG)
          System.out.println(string);
-      
    }
 
-   public void variableChanged(YoVariable variable)
-   {      
+   public void variableChanged(YoVariable<?> variable)
+   {
       updateWeights();
    }
-   
-   
 }
