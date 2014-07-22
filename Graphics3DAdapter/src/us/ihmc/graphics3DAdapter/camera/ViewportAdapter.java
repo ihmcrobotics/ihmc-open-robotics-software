5eead04d777f7b46b5a9834003fc40b1866d0d4e
Duncan Calvert <dcalvert@ihmc.us>
2014-07-22 22:46:06 +0000

    DRC-1506: Added frame listeners to ViewportAdapter. !fixBamboo

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/ViewportAdapter.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/camera/ViewportAdapter.java
@@ -1,30 +1,40 @@
 package us.ihmc.graphics3DAdapter.camera;
 
 import java.awt.Canvas;
+import java.util.ArrayList;
 
 import javax.vecmath.Point3d;
 
 import us.ihmc.graphics3DAdapter.CameraAdapter;
 import us.ihmc.graphics3DAdapter.ContextSwitchedListener;
+import us.ihmc.graphics3DAdapter.Graphics3DFrameListener;
 
 
-public interface ViewportAdapter
+public abstract class ViewportAdapter
 {
+   ArrayList<Graphics3DFrameListener> frameListeners = new ArrayList<>();
 
    public abstract Canvas getCanvas();
+
    public abstract CaptureDevice getCaptureDevice();
 
    public abstract void setCameraController(CameraController cameraController);
+
    public abstract CameraController getCameraController();
-   
+
    public abstract CameraAdapter getCamera();
 
    public abstract double getFieldOfView();
+
    public abstract double getPhysicalWidth();
+
    public abstract double getPhysicalHeight();
+
    public abstract void setupOffscreenView(int width, int height);
+
    public abstract Point3d getWorldCoordinatesFromScreenCoordinates(float f, float g, double z);
+
    public abstract void addContextSwitchedListener(ContextSwitchedListener contextSwitchedListener);
 
-
+   public abstract void addFrameListener(Graphics3DFrameListener frameListener);
 }
