d4c3a78919d459b86f4a6edb8dc7c4d3874759d3
Duncan Calvert <dcalvert@ihmc.us>
2014-07-22 21:05:20 +0000

    DRC-1506: Added initialCameraPosition parameter. !fixBamboo

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/Graphics3DAdapterTools.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/Graphics3DAdapterTools.java
@@ -3,9 +3,11 @@ package us.ihmc.graphics3DAdapter;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Container;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.vecmath.Vector3d;
 
 import us.ihmc.graphics3DAdapter.camera.CameraTrackingAndDollyPositionHolder;
 import us.ihmc.graphics3DAdapter.camera.ClassicCameraController;
@@ -14,22 +16,34 @@ import us.ihmc.graphics3DAdapter.camera.ViewportAdapter;
 
 public class Graphics3DAdapterTools
 {
-   
-   public static void createNewWindow(Graphics3DAdapter graphics3DAdapter, String title, int width, int height)
+   public static ClassicCameraController createNewWindow(Graphics3DAdapter graphics3DAdapter, String title, int width, int height,
+           Vector3d initialCameraTranslation)
    {
       ViewportAdapter viewportAdapter = graphics3DAdapter.createNewViewport(null, false, false);
       CameraTrackingAndDollyPositionHolder cameraTrackingAndDollyPositionHolder = new SimpleCameraTrackingAndDollyPositionHolder();
-      ClassicCameraController classicCameraController = ClassicCameraController.createClassicCameraControllerAndAddListeners(viewportAdapter, cameraTrackingAndDollyPositionHolder, graphics3DAdapter);
+      ClassicCameraController classicCameraController = ClassicCameraController.createClassicCameraControllerAndAddListeners(viewportAdapter,
+                                                           cameraTrackingAndDollyPositionHolder, graphics3DAdapter);
+      classicCameraController.setCameraPosition(initialCameraTranslation.x, initialCameraTranslation.y, initialCameraTranslation.z);
       viewportAdapter.setCameraController(classicCameraController);
       Canvas canvas = viewportAdapter.getCanvas();
-      
+
       Graphics3DAdapterTools.createNewWindow(canvas, title, width, height);
+
+      return classicCameraController;
    }
-   
+
+   public static ClassicCameraController createNewWindow(Graphics3DAdapter graphics3DAdapter, String title, int width, int height)
+   {
+      return createNewWindow(graphics3DAdapter, title, width, height,
+                             new Vector3d(ClassicCameraController.CAMERA_START_X, ClassicCameraController.CAMERA_START_Y,
+                                ClassicCameraController.CAMERA_START_Z));
+   }
+
    public static void createNewWindow(Canvas canvas, String title, int width, int height)
    {
       JPanel panel = new JPanel(new BorderLayout());
       panel.add("Center", canvas);
+      panel.setPreferredSize(new Dimension(width, height));
 
       JFrame jFrame = new JFrame(title);
       jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,6 +53,5 @@ public class Graphics3DAdapterTools
 
       jFrame.pack();
       jFrame.setVisible(true);
-      jFrame.setSize(800, 600);
    }
 }
