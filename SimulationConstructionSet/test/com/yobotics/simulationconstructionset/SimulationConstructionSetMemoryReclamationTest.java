ff0d9324f2f0c0ef839fabbdddc79b26618a98ba
Brett Rowberry <browberry@ihmc.us>
2014-07-23 21:36:06 +0000

    NONE: Simple cleanup of SimulationConstructionSetMemoryReclamationTest.

--- SimulationConstructionSet/test/com/yobotics/simulationconstructionset/SimulationConstructionSetMemoryReclamationTest.java
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/SimulationConstructionSetMemoryReclamationTest.java
@@ -1,23 +1,34 @@
 package com.yobotics.simulationconstructionset;
 
 import static org.junit.Assert.assertTrue;
-
 import java.awt.Frame;
-
 import org.junit.Test;
-
+import com.yobotics.exampleSimulations.fallingBrick.FallingBrickRobot;
 import us.ihmc.utilities.MemoryTools;
 
 public class SimulationConstructionSetMemoryReclamationTest
 {
    @Test
+   public void testMemoryReclamationForSCSWithoutARobot()
+   {
+      boolean useRobot = false;
+      int numberOfTests = 3;
+      int usedMemoryMBAtStart = MemoryTools.getCurrentMemoryUsageInMB();
+      int usedMemoryMBAtEnd = testOneAndReturnUsedMemoryMB(useRobot, numberOfTests);
+      int usedMemoryMB = usedMemoryMBAtEnd - usedMemoryMBAtStart;
+
+      assertTrue("usedMemoryMB = " + usedMemoryMB, usedMemoryMB < 50);
+   }
+   
+   @Test
    public void testMemoryReclamationForSCSWithARobot()
    {
+      boolean useRobot = true;
+      int numberOfTests = 1;
       int usedMemoryMBAtStart = MemoryTools.getCurrentMemoryUsageInMB();
-      int usedMemoryMBAtEnd = testOneAndReturnUsedMemoryMB(true, 1);
-      
+      int usedMemoryMBAtEnd = testOneAndReturnUsedMemoryMB(useRobot, numberOfTests);
       int usedMemoryMB = usedMemoryMBAtEnd - usedMemoryMBAtStart;
-      
+
       Frame[] frames = Frame.getFrames();
       if (frames != null)
       {
@@ -25,34 +36,15 @@ public class SimulationConstructionSetMemoryReclamationTest
          for (Frame frame : frames)
          {
             System.out.println(frame.getTitle() + ": " + frame);
-            
-//            frame.setVisible(false);
-//            frame.dispose();
+
+            //            frame.setVisible(false);
+            //            frame.dispose();
          }
       }
-      
       frames = null;
-      
+
       assertTrue("usedMemoryMB = " + usedMemoryMB, usedMemoryMB < 50);
    }
-   
-   
-   @Test
-   public void testMemoryReclamationForSCSWithoutARobot()
-   {
-      int usedMemoryMBAtStart = MemoryTools.getCurrentMemoryUsageInMB();
-      int usedMemoryMBAtEnd = testOneAndReturnUsedMemoryMB(false, 2);
-      
-      int usedMemoryMB = usedMemoryMBAtEnd - usedMemoryMBAtStart;
-      
-      assertTrue("usedMemoryMB = " + usedMemoryMB, usedMemoryMB < 50);
-   }
-   
-//   @Test
-//   public void sleepForever()
-//   {
-//      sleep(3000000);
-//   }
 
    private int testOneAndReturnUsedMemoryMB(boolean useARobot, int numberOfTests)
    {
@@ -61,15 +53,10 @@ public class SimulationConstructionSetMemoryReclamationTest
       for (int i = 0; i < numberOfTests; i++)
       {
          SimulationConstructionSet scs = createAndStartSimulationConstructionSet(useARobot);
-
-         sleep(2000);
-
+         sleep(200000); //2000
          scs.closeAndDispose();
-
          MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB("testOneAndReturnUsedMemoryMB final: ");
-
       }
-
       System.gc();
 
       System.out.println("Created and disposed of " + numberOfTests + " SCSs. Should be garbage collected now...");
@@ -84,18 +71,17 @@ public class SimulationConstructionSetMemoryReclamationTest
 
       if (useARobot)
       {
-         Robot robot = new Robot("TestRobot");
-         
+         FallingBrickRobot robot = new FallingBrickRobot();
          YoVariableRegistry registry = new YoVariableRegistry("TestRegistry");
-         
-         for (int i=0; i<5000; i++)
+
+         for (int i = 0; i < 5000; i++)
          {
             new DoubleYoVariable("variable" + i, registry);
          }
-         
+
          robot.addYoVariableRegistry(registry);
          scs = new SimulationConstructionSet(robot, 5000);
-      } 
+      }
       else
       {
          scs = new SimulationConstructionSet(true, 5000);
@@ -103,24 +89,22 @@ public class SimulationConstructionSetMemoryReclamationTest
 
       Thread thread = new Thread(scs);
       thread.start();
-      
-      while(useARobot && !scs.isSimulationThreadUpAndRunning())
+
+      while (useARobot && !scs.isSimulationThreadUpAndRunning())
       {
-         sleep(100);
+         sleep(10000); //100
       }
-      
       return scs;
-   } 
+   }
 
    private void sleep(long sleepMillis)
    {
       try
       {
          Thread.sleep(sleepMillis);
-      } catch (InterruptedException e)
+      }
+      catch (InterruptedException e)
       {
       }
-
    }
-
 }
