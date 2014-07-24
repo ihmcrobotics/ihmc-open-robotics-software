4a91aa8629cc75bdd5cd99ce7174992e90f03bdb
Sylvain Bertrand <sbertrand@ihmc.us>
2014-07-24 21:12:57 +0000

    NONE: Moved ConstantPositionProvider from SCS to IHMCUtilities. Won't fixbamboo.

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/CirclePositionAndOrientationTrajectoryGeneratorTest.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/trajectories/CirclePositionAndOrientationTrajectoryGeneratorTest.java
@@ -1,7 +1,6 @@
 package us.ihmc.commonWalkingControlModules.trajectories;
 
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
-import com.yobotics.simulationconstructionset.util.trajectory.ConstantPositionProvider;
 import org.junit.Test;
 import us.ihmc.utilities.RandomTools;
 import us.ihmc.utilities.math.geometry.FrameOrientation;
@@ -9,6 +8,7 @@ import us.ihmc.utilities.math.geometry.FramePoint;
 import us.ihmc.utilities.math.geometry.FrameVector;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.math.trajectories.providers.ConstantDoubleProvider;
+import us.ihmc.utilities.math.trajectories.providers.ConstantPositionProvider;
 import us.ihmc.utilities.math.trajectories.providers.DoubleProvider;
 import us.ihmc.utilities.math.trajectories.providers.PositionProvider;
 import us.ihmc.utilities.test.JUnitTools;
