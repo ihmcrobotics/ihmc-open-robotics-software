a7f0d0b3e086dcf94663679cc72eef6e09e4da61
Atlas Control Computer <AtlasControlComputer@unknown>
2014-07-24 21:05:06 +0000

    DRC-1502: Fixing some exceptions in the RobotiqHandDriver and ThreadManager. -Nolan

--- RobotiqHandDrivers/src/us/ihmc/Robotiq/RobotiqHandParameters.java
+++ RobotiqHandDrivers/src/us/ihmc/Robotiq/RobotiqHandParameters.java
@@ -4,7 +4,7 @@ public final class RobotiqHandParameters
 {
 	//Parameters for use with the Robotiq 3-Finger Gripper with ModbusTCP interface
 	static final int PORT = 502;
-	static final String RIGHT_HAND_ADDRESS = "192.168.1.12"; //placeholder
+	static final String RIGHT_HAND_ADDRESS = "10.66.171.140"; //placeholder
 	static final String LEFT_HAND_ADDRESS = "10.66.171.135";
 	static final byte UNIT_ID = 0x02;
 }
