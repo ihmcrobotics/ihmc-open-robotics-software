b881d361282b23f403433140054f34471a7cc373
Sylvain Bertrand <sbertrand@ihmc.us>
2014-07-24 21:41:31 +0000

    NONE: Moved OrientationProvider from CommonWalkingControlModules to IHMCUtilities.

--- CommonWalkingControlModules/src/us/ihmc/graveYard/commonWalkingControlModules/vrc/highLevelHumanoidControl/manipulation/states/fingerToroidManipulation/states/CirclePositionControlState.java
+++ CommonWalkingControlModules/src/us/ihmc/graveYard/commonWalkingControlModules/vrc/highLevelHumanoidControl/manipulation/states/fingerToroidManipulation/states/CirclePositionControlState.java
@@ -4,7 +4,6 @@ import us.ihmc.commonWalkingControlModules.controlModules.SE3PDGains;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.manipulation.individual.IndividualHandControlModule;
 import us.ihmc.commonWalkingControlModules.trajectories.CirclePositionTrajectoryGenerator;
 import us.ihmc.commonWalkingControlModules.trajectories.OrientationInterpolationTrajectoryGenerator;
-import us.ihmc.commonWalkingControlModules.trajectories.OrientationProvider;
 import us.ihmc.commonWalkingControlModules.trajectories.OrientationTrajectoryGenerator;
 import us.ihmc.commonWalkingControlModules.trajectories.SE3ConfigurationProvider;
 import us.ihmc.graveYard.commonWalkingControlModules.vrc.highLevelHumanoidControl.manipulation.states.fingerToroidManipulation.FingerToroidManipulationState;
@@ -14,6 +13,7 @@ import us.ihmc.utilities.FormattingTools;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.math.trajectories.providers.ConstantDoubleProvider;
 import us.ihmc.utilities.math.trajectories.providers.DoubleProvider;
+import us.ihmc.utilities.math.trajectories.providers.OrientationProvider;
 import us.ihmc.utilities.screwTheory.RigidBody;
 
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
