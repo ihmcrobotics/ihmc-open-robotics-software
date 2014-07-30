1195896bb9a4b63cdf605099272fbbeea56741fb
Duncan Calvert <dcalvert@ihmc.us>
2014-07-30 15:30:05 +0000

    DRC-1572: Refactored GPULidarCallback to GPULidarListener.

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/GPULidarListener.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/GPULidarListener.java
@@ -2,7 +2,7 @@ package us.ihmc.graphics3DAdapter;
 
 import javax.media.j3d.Transform3D;
 
-public interface GPULidarCallback
+public interface GPULidarListener
 {
    public void scan(float[] scan, Transform3D currentTransform, double time);
 }
