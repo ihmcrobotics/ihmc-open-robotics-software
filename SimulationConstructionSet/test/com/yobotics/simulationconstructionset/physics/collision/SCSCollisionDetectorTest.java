2b21f05a5f163f0bb8c40ed020f8a00bb2c688ac
Jerry Pratt <jpratt@ihmc.us>
2014-08-02 21:17:02 +0000

    NONE: Refactoring some of the collision detection stuff to make it easier to navigate and read.

--- SimulationConstructionSet/test/com/yobotics/simulationconstructionset/physics/collision/SCSCollisionDetectorTest.java
+++ SimulationConstructionSet/test/com/yobotics/simulationconstructionset/physics/collision/SCSCollisionDetectorTest.java
@@ -14,6 +14,7 @@ import com.yobotics.simulationconstructionset.Link;
 import com.yobotics.simulationconstructionset.physics.CollisionHandler;
 import com.yobotics.simulationconstructionset.physics.CollisionShape;
 import com.yobotics.simulationconstructionset.physics.CollisionShapeDescription;
+import com.yobotics.simulationconstructionset.physics.Contacts;
 import com.yobotics.simulationconstructionset.physics.FactoryCollisionShape;
 import com.yobotics.simulationconstructionset.physics.ScsCollisionDetector;
 
