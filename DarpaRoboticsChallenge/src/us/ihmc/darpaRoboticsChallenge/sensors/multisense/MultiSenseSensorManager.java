59bfa8bb7f92ac6022f337e610372e48eb254d01
Sylvain Bertrand <sbertrand@ihmc.us>
2014-08-04 15:33:50 +0000

    NONE: Fixed NullPointerException -- Push

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/sensors/multisense/MultiSenseSensorManager.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/sensors/multisense/MultiSenseSensorManager.java
@@ -24,15 +24,15 @@ import us.ihmc.utilities.ros.RosMainNode;
 public class MultiSenseSensorManager
 {
    private RosCameraReceiver cameraReceiver;
-   
+
    private final RobotPoseBuffer sharedRobotPoseBuffer;
    private final RosMainNode rosMainNode;
    private final DRCNetworkProcessorNetworkingManager networkingManager;
    private final RosNativeNetworkProcessor rosNativeNetworkProcessor;
    private final PPSTimestampOffsetProvider ppsTimestampOffsetProvider;
-   
+
    private static final double lidarCRC = -.0010908f;
-   
+
    private final String sensorURI;
 
    private final DRCRobotCameraParameters cameraParamaters;
@@ -69,24 +69,25 @@ public class MultiSenseSensorManager
       networkingManager.getControllerCommandHandler().setMultiSenseSensorManager(this);
 
    }
-   
-   public void initializeParameterListeners(){
-   
+
+   public void initializeParameterListeners()
+   {
+
       System.out.println("initialise parameteres--------------------------------------------------------------------------------");
 
       rosMainNode.attachParameterListener("/multisense/motor_speed", new ParameterListener()
       {
-         
+
          @Override
          public void onNewValue(Object value)
          {
             System.out.println("new motor speed received");
-          send();
+            send();
          }
       });
       rosMainNode.attachParameterListener("/multisense/gain", new ParameterListener()
       {
-         
+
          @Override
          public void onNewValue(Object value)
          {
@@ -94,10 +95,10 @@ public class MultiSenseSensorManager
             send();
          }
       });
-      
+
       rosMainNode.attachParameterListener("/multisense/led_duty_cycle", new ParameterListener()
       {
-         
+
          @Override
          public void onNewValue(Object value)
          {
@@ -107,7 +108,7 @@ public class MultiSenseSensorManager
       });
       rosMainNode.attachParameterListener("/multisense/lighting", new ParameterListener()
       {
-         
+
          @Override
          public void onNewValue(Object value)
          {
@@ -117,7 +118,7 @@ public class MultiSenseSensorManager
       });
       rosMainNode.attachParameterListener("/multisense/flash", new ParameterListener()
       {
-         
+
          @Override
          public void onNewValue(Object value)
          {
@@ -125,10 +126,10 @@ public class MultiSenseSensorManager
             send();
          }
       });
-      
+
       rosMainNode.attachParameterListener("/multisense/auto_exposure", new ParameterListener()
       {
-         
+
          @Override
          public void onNewValue(Object value)
          {
@@ -136,10 +137,10 @@ public class MultiSenseSensorManager
             send();
          }
       });
-      
+
       rosMainNode.attachParameterListener("/multisense/resolution", new ParameterListener()
       {
-         
+
          @Override
          public void onNewValue(Object value)
          {
@@ -147,48 +148,43 @@ public class MultiSenseSensorManager
             send();
          }
       });
-      
+
    }
-   
+
    public void handleMultisenseParameters(MultisenseParameterPacket object)
    {
-      System.out.println("beacuse of this---------------------------------------------------------------");
-      if(object.isFromUI()){
-         if(rosMainNode.isStarted()){
+      if (object.isFromUI())
+      {
+         if (rosMainNode.isStarted())
+         {
             params = rosMainNode.getParameters();
             send();
-            
-            
+
          }
-            
-            
       }
       else
          MultiSenseParamaterSetter.setMultisenseParameters(object);
-         
-      
+
    }
 
    private void send()
    {
-      if(params == null){
+      if (params == null)
+      {
          System.out.println("params are null");
          return;
       }
-         
-      networkingManager.getControllerStateHandler().sendSerializableObject( new MultisenseParameterPacket(false,params.getDouble("/multisense/gain"),
-            params.getDouble("/multisense/motor_speed"),
-            params.getDouble("/multisense/led_duty_cycle"),
-            params.getString("/multisense/resolution"),
-            params.getBoolean("/multisense/lighting"),
-            params.getBoolean("/multisense/flash"),
-            params.getBoolean("multisense/auto_exposure")));
+
+      networkingManager.getControllerStateHandler().sendSerializableObject(
+            new MultisenseParameterPacket(false, params.getDouble("/multisense/gain"), params.getDouble("/multisense/motor_speed"), params
+                  .getDouble("/multisense/led_duty_cycle"), params.getString("/multisense/resolution"), params.getBoolean("/multisense/lighting"), params
+                  .getBoolean("/multisense/flash"), params.getBoolean("multisense/auto_exposure")));
    }
-   
+
    private void setMultiseSenseParams(double lidarSpindleVelocity)
    {
       MultiSenseParamaterSetter.setMultisenseResolution(rosMainNode);
-      
+
       if (RosNativeNetworkProcessor.hasNativeLibrary())
       {
          MultiSenseParamaterSetter.setupNativeROSCommunicator(rosNativeNetworkProcessor, lidarSpindleVelocity);
@@ -198,28 +194,26 @@ public class MultiSenseSensorManager
          MultiSenseParamaterSetter.setupMultisenseSpindleSpeedPublisher(rosMainNode, lidarSpindleVelocity);
       }
    }
-   
+
    private void registerLidarReceivers()
    {
-      new MultisenseLidarDataReceiver(depthDataProcessor, rosTransformProvider, ppsTimestampOffsetProvider, sharedRobotPoseBuffer, rosMainNode,
-            lidarParamaters);
+      new MultisenseLidarDataReceiver(depthDataProcessor, rosTransformProvider, ppsTimestampOffsetProvider, sharedRobotPoseBuffer, rosMainNode, lidarParamaters);
    }
-   
+
    private void registerCameraReceivers()
    {
       CameraLogger logger = DRCConfigParameters.LOG_PRIMARY_CAMERA_IMAGES ? new CameraLogger("left") : null;
       cameraReceiver = new RosCameraReceiver(cameraParamaters, sharedRobotPoseBuffer, cameraParamaters.getVideoSettings(), rosMainNode, networkingManager,
-            ppsTimestampOffsetProvider,logger, sensorURI);
+            ppsTimestampOffsetProvider, logger, sensorURI);
 
-      CameraInfoReceiver cameraInfoServer = new RosCameraInfoReciever(cameraParamaters, rosMainNode,
-            networkingManager.getControllerStateHandler(),logger);
-      
+      CameraInfoReceiver cameraInfoServer = new RosCameraInfoReciever(cameraParamaters, rosMainNode, networkingManager.getControllerStateHandler(), logger);
+
       networkingManager.getControllerCommandHandler().setIntrinsicServer(cameraInfoServer);
    }
 
    public void registerCameraListener(ArmCalibrationHelper armCalibrationHelper)
    {
       cameraReceiver.registerCameraListener(armCalibrationHelper);
-      
+
    }
 }
