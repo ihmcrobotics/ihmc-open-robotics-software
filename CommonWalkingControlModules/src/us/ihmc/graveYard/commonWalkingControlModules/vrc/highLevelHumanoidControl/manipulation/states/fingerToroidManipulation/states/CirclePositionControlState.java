65a5327053d29e9ca9ac02c91a22c2cfc20f9b46
Sylvain Bertrand <sbertrand@ihmc.us>
2014-08-07 18:05:56 +0000

    DRC-1600: Renamed SE3PDGains and SE3PDController into SE3PIDGains and SE3PIDController. Included maximum acceleration and maximum Jerk in SE3PIDGains.

--- CommonWalkingControlModules/src/us/ihmc/graveYard/commonWalkingControlModules/vrc/highLevelHumanoidControl/manipulation/states/fingerToroidManipulation/states/CirclePositionControlState.java
+++ CommonWalkingControlModules/src/us/ihmc/graveYard/commonWalkingControlModules/vrc/highLevelHumanoidControl/manipulation/states/fingerToroidManipulation/states/CirclePositionControlState.java
@@ -1,6 +1,6 @@
 package us.ihmc.graveYard.commonWalkingControlModules.vrc.highLevelHumanoidControl.manipulation.states.fingerToroidManipulation.states;
 
-import us.ihmc.commonWalkingControlModules.controlModules.SE3PDGains;
+import us.ihmc.commonWalkingControlModules.controlModules.SE3PIDGains;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.manipulation.individual.HandControlModule;
 import us.ihmc.graveYard.commonWalkingControlModules.vrc.highLevelHumanoidControl.manipulation.states.fingerToroidManipulation.FingerToroidManipulationState;
 import us.ihmc.robotSide.RobotSide;
@@ -29,14 +29,14 @@ public class CirclePositionControlState extends State<FingerToroidManipulationSt
    private final SideDependentList<HandControlModule> individualHandControlModules;
    private final SideDependentList<ReferenceFrame> fingerPositionControlFrames;
    private final RigidBody rootBody;
-   private final SE3PDGains gains;
+   private final SE3PIDGains gains;
 
    public CirclePositionControlState(FingerToroidManipulationState stateEnum, RigidBody rootBody, ReferenceFrame staticTorusFrame,
                                      SideDependentList<ReferenceFrame> handPositionControlFrames,
                                      SideDependentList<SE3ConfigurationProvider> initialConfigurationProviders,
                                      SideDependentList<? extends OrientationProvider> finalOrientationProviders, DoubleProvider desiredRotationAngleProvider,
                                      double trajectoryTime, SideDependentList<HandControlModule> individualHandControlModules,
-                                     SideDependentList<ReferenceFrame> fingerPositionControlFrames, SE3PDGains gains, YoVariableRegistry parentRegistry)
+                                     SideDependentList<ReferenceFrame> fingerPositionControlFrames, SE3PIDGains gains, YoVariableRegistry parentRegistry)
    {
       super(stateEnum);
       registry = new YoVariableRegistry(FormattingTools.lowerCaseFirstLetter(stateEnum.toString()));
