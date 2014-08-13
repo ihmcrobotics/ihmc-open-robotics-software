c608596e67fb1e5ba0297cf643d4004fd2255899
Jerry Pratt <jpratt@ihmc.us>
2014-08-13 19:10:28 +0000

    DRC-1656: Cleaning up MomentumBasedControllerFactory to not have to deal with VariousWalkingProviders.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/DataProducerVariousWalkingProviderFactory.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/DataProducerVariousWalkingProviderFactory.java
@@ -1,17 +1,56 @@
 package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.ContactablePlaneBody;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.commonWalkingControlModules.controlModules.spine.ChestOrientationPacket;
 import us.ihmc.commonWalkingControlModules.controllers.Updatable;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.BlindWalkingPacketConsumer;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.BlindWalkingToDestinationDesiredFootstepCalculator;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.Footstep;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.FootstepPathConsumer;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.FootstepPathCoordinator;
 import us.ihmc.commonWalkingControlModules.desiredFootStep.FootstepTimingParameters;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.PauseCommandConsumer;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.dataObjects.BlindWalkingPacket;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.dataObjects.FootstepDataList;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.dataObjects.PauseCommand;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredChestOrientationProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredComHeightProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredFootPoseProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredFootStateProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredHandLoadBearingProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredHandPoseProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredHeadOrientationProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredPelvisLoadBearingProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredPelvisPoseProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredThighLoadBearingProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.HandstepProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.ReinitializeWalkingControllerProvider;
+import us.ihmc.commonWalkingControlModules.packetProviders.ControlStatusProducer;
+import us.ihmc.commonWalkingControlModules.packetProviders.NetworkControlStatusProducer;
+import us.ihmc.commonWalkingControlModules.packets.BumStatePacket;
+import us.ihmc.commonWalkingControlModules.packets.ComHeightPacket;
+import us.ihmc.commonWalkingControlModules.packets.DesiredHighLevelStateProvider;
+import us.ihmc.commonWalkingControlModules.packets.FootPosePacket;
+import us.ihmc.commonWalkingControlModules.packets.FootStatePacket;
+import us.ihmc.commonWalkingControlModules.packets.HandLoadBearingPacket;
+import us.ihmc.commonWalkingControlModules.packets.HandPosePacket;
+import us.ihmc.commonWalkingControlModules.packets.HeadOrientationPacket;
+import us.ihmc.commonWalkingControlModules.packets.HighLevelStatePacket;
+import us.ihmc.commonWalkingControlModules.packets.LookAtPacket;
+import us.ihmc.commonWalkingControlModules.packets.PelvisOrientationPacket;
+import us.ihmc.commonWalkingControlModules.packets.ReinitializeWalkingControllerPacket;
+import us.ihmc.commonWalkingControlModules.packets.ThighStatePacket;
 import us.ihmc.commonWalkingControlModules.referenceFrames.CommonWalkingReferenceFrames;
 import us.ihmc.commonWalkingControlModules.trajectories.ConstantSwingTimeCalculator;
 import us.ihmc.commonWalkingControlModules.trajectories.ConstantTransferTimeCalculator;
 import us.ihmc.robotSide.SideDependentList;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 import us.ihmc.utilities.io.streamingData.GlobalDataProducer;
+import us.ihmc.utilities.math.trajectories.providers.TrajectoryParameters;
 
 import com.yobotics.simulationconstructionset.DoubleYoVariable;
 import com.yobotics.simulationconstructionset.YoVariableRegistry;
@@ -33,8 +72,63 @@ public class DataProducerVariousWalkingProviderFactory implements VariousWalking
            ConstantTransferTimeCalculator transferTimeCalculator, ConstantSwingTimeCalculator swingTimeCalculator, ArrayList<Updatable> updatables,
            YoVariableRegistry registry, DynamicGraphicObjectsListRegistry dynamicGraphicObjectsListRegistry)
    {
-      return VariousWalkingProviders.createUsingObjectCommunicator(footstepTimingParameters, objectCommunicator, fullRobotModel, walkingControllerParameters,
-              referenceFrames, feet, transferTimeCalculator, swingTimeCalculator, registry);
+      HandstepProvider handstepProvider = null;
+
+      DesiredHandPoseProvider handPoseProvider = new DesiredHandPoseProvider(fullRobotModel,
+                                                    walkingControllerParameters.getDesiredHandPosesWithRespectToChestFrame());
+
+      LinkedHashMap<Footstep, TrajectoryParameters> mapFromFootstepsToTrajectoryParameters = new LinkedHashMap<Footstep, TrajectoryParameters>();
+
+      BlindWalkingToDestinationDesiredFootstepCalculator desiredFootstepCalculator =
+         HighLevelHumanoidControllerFactoryHelper.getBlindWalkingToDestinationDesiredFootstepCalculator(walkingControllerParameters, referenceFrames, feet,
+            registry);
+
+      FootstepPathCoordinator footstepPathCoordinator = new FootstepPathCoordinator(footstepTimingParameters, objectCommunicator, desiredFootstepCalculator,
+                                                           swingTimeCalculator, transferTimeCalculator, registry);
+
+      FootstepPathConsumer footstepPathConsumer = new FootstepPathConsumer(feet, footstepPathCoordinator, mapFromFootstepsToTrajectoryParameters);
+      BlindWalkingPacketConsumer blindWalkingPacketConsumer = new BlindWalkingPacketConsumer(footstepPathCoordinator);
+      PauseCommandConsumer pauseCommandConsumer = new PauseCommandConsumer(footstepPathCoordinator);
+      DesiredHighLevelStateProvider highLevelStateProvider = new DesiredHighLevelStateProvider();
+      DesiredHeadOrientationProvider headOrientationProvider = new DesiredHeadOrientationProvider(referenceFrames.getPelvisZUpFrame());
+      DesiredComHeightProvider desiredComHeightProvider = new DesiredComHeightProvider();
+      DesiredPelvisPoseProvider pelvisPoseProvider = new DesiredPelvisPoseProvider();
+      DesiredChestOrientationProvider chestOrientationProvider = new DesiredChestOrientationProvider();
+      DesiredFootPoseProvider footPoseProvider = new DesiredFootPoseProvider();
+      ReinitializeWalkingControllerProvider reinitializeWalkingControllerProvider = new ReinitializeWalkingControllerProvider();
+
+      DesiredHandLoadBearingProvider handLoadBearingProvider = new DesiredHandLoadBearingProvider();
+      DesiredFootStateProvider footLoadBearingProvider = new DesiredFootStateProvider();
+      DesiredThighLoadBearingProvider thighLoadBearingProvider = new DesiredThighLoadBearingProvider();
+      DesiredPelvisLoadBearingProvider pelvisLoadBearingProvider = new DesiredPelvisLoadBearingProvider();
+
+      objectCommunicator.attachListener(FootstepDataList.class, footstepPathConsumer);
+      objectCommunicator.attachListener(BlindWalkingPacket.class, blindWalkingPacketConsumer);
+      objectCommunicator.attachListener(PauseCommand.class, pauseCommandConsumer);
+      objectCommunicator.attachListener(HighLevelStatePacket.class, highLevelStateProvider);
+      objectCommunicator.attachListener(HeadOrientationPacket.class, headOrientationProvider.getHeadOrientationPacketConsumer());
+      objectCommunicator.attachListener(ComHeightPacket.class, desiredComHeightProvider.getComHeightPacketConsumer());
+      objectCommunicator.attachListener(LookAtPacket.class, headOrientationProvider.getLookAtPacketConsumer());
+      objectCommunicator.attachListener(PelvisOrientationPacket.class, pelvisPoseProvider);
+      objectCommunicator.attachListener(HandPosePacket.class, handPoseProvider);
+      objectCommunicator.attachListener(FootPosePacket.class, footPoseProvider);
+      objectCommunicator.attachListener(ChestOrientationPacket.class, chestOrientationProvider);
+      objectCommunicator.attachListener(ReinitializeWalkingControllerPacket.class, reinitializeWalkingControllerProvider);
+
+      objectCommunicator.attachListener(HandLoadBearingPacket.class, handLoadBearingProvider);
+      objectCommunicator.attachListener(FootStatePacket.class, footLoadBearingProvider);
+      objectCommunicator.attachListener(ThighStatePacket.class, thighLoadBearingProvider);
+      objectCommunicator.attachListener(BumStatePacket.class, pelvisLoadBearingProvider);
+
+      ControlStatusProducer controlStatusProducer = new NetworkControlStatusProducer(objectCommunicator);
+
+      VariousWalkingProviders variousWalkingProviders = new VariousWalkingProviders(footstepPathCoordinator, handstepProvider,
+                                                           mapFromFootstepsToTrajectoryParameters, headOrientationProvider, desiredComHeightProvider,
+                                                           pelvisPoseProvider, handPoseProvider, handLoadBearingProvider, chestOrientationProvider,
+                                                           footPoseProvider, footLoadBearingProvider, highLevelStateProvider, thighLoadBearingProvider,
+                                                           pelvisLoadBearingProvider, reinitializeWalkingControllerProvider, controlStatusProducer);
+
+      return variousWalkingProviders;
    }
 
 }
