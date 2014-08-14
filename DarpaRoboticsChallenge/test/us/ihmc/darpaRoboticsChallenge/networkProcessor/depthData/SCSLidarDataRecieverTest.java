e1f4e7f31122bbcbaf7dc12aadcbc2fa8520ee62
Duncan Calvert <dcalvert@ihmc.us>
2014-08-14 17:22:03 +0000

    DRC-1563: Lidar data receiver test created.

---
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/networkProcessor/depthData/SCSLidarDataRecieverTest.java
@@ -0,0 +1,93 @@
+package us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import javax.vecmath.Point3d;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.bambooTools.BambooTools;
+import us.ihmc.darpaRoboticsChallenge.MultiRobotTestInterface;
+import us.ihmc.darpaRoboticsChallenge.networking.DRCUserInterfaceNetworkingManager;
+import us.ihmc.darpaRoboticsChallenge.testTools.DRCSimulationNetworkTestHelper;
+import us.ihmc.utilities.MemoryTools;
+import us.ihmc.utilities.lidar.polarLidar.SparseLidarScan;
+import us.ihmc.utilities.net.NetStateListener;
+import us.ihmc.utilities.net.ObjectConsumer;
+
+import com.yobotics.simulationconstructionset.simulatedSensors.DepthDataStateCommand;
+import com.yobotics.simulationconstructionset.simulatedSensors.DepthDataStateCommand.LidarState;
+import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+
+public abstract class SCSLidarDataRecieverTest implements MultiRobotTestInterface, NetStateListener
+{
+   @Before
+   public void showMemoryUsageBeforeTest()
+   {
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test. ");
+   }
+
+   @Test
+   public void testSCSLidarDataReceiver() throws SimulationExceededMaximumTimeException
+   {
+      BambooTools.reportTestStartedMessage();
+
+      DRCSimulationNetworkTestHelper drcSimulationTestHelper = new DRCSimulationNetworkTestHelper(getRobotModel());
+      drcSimulationTestHelper.getDRCSimulationTestHelper().setupCameraForUnitTest(new Point3d(1.8375, -0.16, 0.89), new Point3d(1.10, 8.30, 1.37));
+
+      // TODO Listen to tcp stream rather than local object communicator
+
+      DRCUserInterfaceNetworkingManager networkingManager = new DRCUserInterfaceNetworkingManager("localhost", getRobotModel());
+      networkingManager.attachStateListener(this);
+
+      try
+      {
+         networkingManager.connect();
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
+
+      networkingManager.getControllerHandler().send(new DepthDataStateCommand(LidarState.ENABLE));
+      networkingManager.getVisualizationHandler().attachListener(SparseLidarScan.class, new LidarConsumer());
+
+      boolean success = drcSimulationTestHelper.getDRCSimulationTestHelper().simulateAndBlockAndCatchExceptions(0.5);
+
+      // Put drc robot in world, bring up window
+
+      // TODO
+      // Put known object in world
+      // Get Object Communicator
+      // Receive Sparse Lidar Packet
+      // Check points
+
+      assertTrue(success);
+
+      BambooTools.reportTestFinishedMessage();
+   }
+
+   private class LidarConsumer implements ObjectConsumer<SparseLidarScan>
+   {
+      @Override
+      public void consumeObject(SparseLidarScan object)
+      {
+//       System.out.println("Sparse lidar scan received.");
+         System.out.println(object.getClass().getSimpleName() + " received!");
+      }
+   }
+
+
+   public void connected()
+   {
+      System.out.println("DRCUserInterfaceNetworkingManager: Connected!");
+   }
+
+   public void disconnected()
+   {
+      System.out.println("DRCUserInterfaceNetworkingManager: Disconnected.");
+   }
+}
