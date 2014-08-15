7f9c9f0a3f9f9aab38d07cea32c845ea9c3026c7
Sylvain Bertrand <sbertrand@ihmc.us>
2014-08-15 19:17:40 +0000

    NONE: Cleaned the high level gains in ArmControllerParameters.

--- Atlas/src/us/ihmc/atlas/parameters/AtlasArmControllerParameters.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasArmControllerParameters.java
@@ -3,6 +3,11 @@ package us.ihmc.atlas.parameters;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.controller.YoPIDGains;
+import com.yobotics.simulationconstructionset.util.controller.YoSE3PIDGains;
+import com.yobotics.simulationconstructionset.util.controller.YoSymmetricSE3PIDGains;
+
 import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
@@ -18,108 +23,87 @@ public class AtlasArmControllerParameters implements ArmControllerParameters
    private static final double[] kd = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }; //{ 11, 17, 39, 47, 26, 25 };
    private static final double[] ff_qd_ds = { 60.0, 80.0, 50.0, 50.0, 30.0, 30.0 }; //{ 35, 35, 35, 35, 35, 35 };
    private static final double[] qerr_maxs = { 0.1, 0.2, 0.015, 0.05, 0.1, 0.05 };
-   
+
    public AtlasArmControllerParameters()
    {
       this(false);
    }
-   
+
    public AtlasArmControllerParameters(boolean runningOnRealRobot)
    {
       this.runningOnRealRobot = runningOnRealRobot;
    }
-   
+
+   @Override
    public double[] getLowLevelArmJointspaceKp()
    {
-      return kp; 
+      return kp;
    }
+   @Override
    public double[] getLowLevelArmJointspaceKi()
    {
-      return ki; 
+      return ki;
    }
+   @Override
    public double[] getLowLevelArmJointspaceKd()
    {
-      return kd; 
+      return kd;
    }
+   @Override
    public double[] getLowLevelArmJointspaceFfqd_d()
    {
-      return ff_qd_ds; 
+      return ff_qd_ds;
    }
+   @Override
    public double[] getLowLevelArmJointspaceQerrMax()
    {
-      return qerr_maxs; 
+      return qerr_maxs;
    }
 
-   public double getArmJointspaceKp()
+   @Override
+   public YoPIDGains createJointspaceControlGains(YoVariableRegistry registry)
    {
-      if (!runningOnRealRobot)  return 80.0;
-      return 40.0; 
+      YoPIDGains jointspaceControlGains = new YoPIDGains("ArmJointspace", registry);
+
+      double kp = runningOnRealRobot ? 40.0 : 80.0;
+      double zeta = runningOnRealRobot ? 0.0 : 0.6;
+      double ki = 0.0;
+      double maxIntegralError = 0.0;
+      double maxAccel = runningOnRealRobot ? 20.0 : Double.POSITIVE_INFINITY;
+      double maxJerk = runningOnRealRobot ? 100.0 : Double.POSITIVE_INFINITY;
+
+      jointspaceControlGains.setKp(kp);
+      jointspaceControlGains.setZeta(zeta);
+      jointspaceControlGains.setKi(ki);
+      jointspaceControlGains.setMaximumIntegralError(maxIntegralError);
+      jointspaceControlGains.setMaximumAcceleration(maxAccel);
+      jointspaceControlGains.setMaximumJerk(maxJerk);
+      jointspaceControlGains.createDerivativeGainUpdater(true);
+      
+      return jointspaceControlGains;
    }
 
-   public double getArmJointspaceZeta()
+   @Override
+   public YoSE3PIDGains createTaskspaceControlGains(YoVariableRegistry registry)
    {
-      if (!runningOnRealRobot)  return 0.6;
-      return 0.0;  // Lots of natural damping in the arms. Don't need to damp the controllers.
-   }
+      YoSymmetricSE3PIDGains taskspaceControlGains = new YoSymmetricSE3PIDGains("ArmTaskspace", registry);
 
-   public double getArmJointspaceKi()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 0.0;
-   }
+      double kp = 100.0;
+      double zeta = runningOnRealRobot ? 0.6 : 1.0;
+      double ki = 0.0;
+      double maxIntegralError = 0.0;
+      double maxAccel = runningOnRealRobot ? 10.0 : Double.POSITIVE_INFINITY;
+      double maxJerk = runningOnRealRobot ? 100.0 : Double.POSITIVE_INFINITY;
 
-   public double getArmJointspaceMaxIntegralError()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 0.0;
-   }
+      taskspaceControlGains.setProportionalGain(kp);
+      taskspaceControlGains.setDampingRatio(zeta);
+      taskspaceControlGains.setIntegralGain(ki);
+      taskspaceControlGains.setMaximumIntegralError(maxIntegralError);
+      taskspaceControlGains.setMaximumAcceleration(maxAccel);
+      taskspaceControlGains.setMaximumJerk(maxJerk);
+      taskspaceControlGains.createDerivativeGainUpdater(true);
 
-   public double getArmJointspaceMaxAcceleration()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 20.0;
-   }
-
-   public double getArmJointspaceMaxJerk()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 100.0;
-   }
-
-   public double getArmTaskspaceKp()
-   {
-      if (!runningOnRealRobot)  return 100.0;
-      return 100.0; 
-   }
-
-   public double getArmTaskspaceZeta()
-   {
-      if (!runningOnRealRobot)  return 1.0;
-      return 0.6; 
-   }
-
-   public double getArmTaskspaceKi()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 0.0; 
-   }
-
-   public double getArmTaskspaceMaxIntegralError()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 0.0; 
-   }
-
-   public double getArmTaskspaceMaxAcceleration()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 10.0; 
-   }
-
-   public double getArmTaskspaceMaxJerk()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 100.0; 
+      return taskspaceControlGains;
    }
 
    @Override
