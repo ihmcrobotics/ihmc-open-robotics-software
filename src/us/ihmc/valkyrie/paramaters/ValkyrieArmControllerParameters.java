37a4be3acfbc53ed21d37729845b21f252102b3a
Sylvain Bertrand <sbertrand@ihmc.us>
2014-08-15 19:17:40 +0000

    NONE: Cleaned the high level gains in ArmControllerParameters.

--- src/us/ihmc/valkyrie/paramaters/ValkyrieArmControllerParameters.java
+++ src/us/ihmc/valkyrie/paramaters/ValkyrieArmControllerParameters.java
@@ -3,6 +3,11 @@ package us.ihmc.valkyrie.paramaters;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.yobotics.simulationconstructionset.YoVariableRegistry;
+import com.yobotics.simulationconstructionset.util.controller.YoPIDGains;
+import com.yobotics.simulationconstructionset.util.controller.YoSE3PIDGains;
+import com.yobotics.simulationconstructionset.util.controller.YoSymmetricSE3PIDGains;
+
 import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
 import us.ihmc.robotSide.RobotSide;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
@@ -12,109 +17,88 @@ import us.ihmc.utilities.screwTheory.OneDoFJoint;
 public class ValkyrieArmControllerParameters implements ArmControllerParameters
 {
    private final boolean runningOnRealRobot;
-   
+
    public ValkyrieArmControllerParameters(boolean runningOnRealRobot)
    {
       this.runningOnRealRobot = runningOnRealRobot;
    }
-   
+
    public static final double[] lowLevelArmJointSpace_kp = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
    public static final double[] lowLevelArmJointSpace_ki = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
    public static final double[] lowLevelArmJointSpace_kd = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
    public static final double[] lowLevelArmJointSpace_ff_qd_ds = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
    public static final double[] lowLevelArmJointSpace_qerr_maxs = { 10.0, 10.0, 10.0, 10.0, 10.0, 10.0 };
-   
+
+   @Override
    public double[] getLowLevelArmJointspaceKp()
    {
-      return lowLevelArmJointSpace_kp; 
+      return lowLevelArmJointSpace_kp;
    }
+   @Override
    public double[] getLowLevelArmJointspaceKi()
    {
-      return lowLevelArmJointSpace_ki; 
+      return lowLevelArmJointSpace_ki;
    }
+   @Override
    public double[] getLowLevelArmJointspaceKd()
    {
-      return lowLevelArmJointSpace_kd; 
+      return lowLevelArmJointSpace_kd;
    }
+   @Override
    public double[] getLowLevelArmJointspaceFfqd_d()
    {
-      return lowLevelArmJointSpace_ff_qd_ds; 
+      return lowLevelArmJointSpace_ff_qd_ds;
    }
+   @Override
    public double[] getLowLevelArmJointspaceQerrMax()
    {
-      return lowLevelArmJointSpace_qerr_maxs; 
+      return lowLevelArmJointSpace_qerr_maxs;
    }
 
-   public double getArmJointspaceKp()
+   @Override
+   public YoPIDGains createJointspaceControlGains(YoVariableRegistry registry)
    {
-      if (!runningOnRealRobot)  return 80.0;
-      return 80.0; 
+      YoPIDGains jointspaceControlGains = new YoPIDGains("ArmJointspace", registry);
+
+      double kp = runningOnRealRobot ? 80.0 : 80.0;
+      double zeta = runningOnRealRobot ? 0.2 : 0.6;
+      double ki = runningOnRealRobot ? 20.0 : 0.0;
+      double maxIntegralError = 0.0;
+      double maxAccel = runningOnRealRobot ? 5.0 : Double.POSITIVE_INFINITY;
+      double maxJerk = runningOnRealRobot ? 100.0 : Double.POSITIVE_INFINITY;
+
+      jointspaceControlGains.setKp(kp);
+      jointspaceControlGains.setZeta(zeta);
+      jointspaceControlGains.setKi(ki);
+      jointspaceControlGains.setMaximumIntegralError(maxIntegralError);
+      jointspaceControlGains.setMaximumAcceleration(maxAccel);
+      jointspaceControlGains.setMaximumJerk(maxJerk);
+      jointspaceControlGains.createDerivativeGainUpdater(true);
+      
+      return jointspaceControlGains;
    }
 
-   public double getArmJointspaceZeta()
+   @Override
+   public YoSE3PIDGains createTaskspaceControlGains(YoVariableRegistry registry)
    {
-      if (!runningOnRealRobot)  return 0.6;
-      return 0.2;  // Lots of natural damping in the arms. Don't need to damp the controllers.
-   }
+      YoSymmetricSE3PIDGains taskspaceControlGains = new YoSymmetricSE3PIDGains("ArmTaskspace", registry);
 
-   public double getArmJointspaceKi()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 20.0; //0.0 
-   }
+      double kp = 100.0;
+      double zeta = runningOnRealRobot ? 0.6 : 1.0;
+      double ki = 0.0;
+      double maxIntegralError = 0.0;
+      double maxAccel = runningOnRealRobot ? 10.0 : Double.POSITIVE_INFINITY;
+      double maxJerk = runningOnRealRobot ? 100.0 : Double.POSITIVE_INFINITY;
 
-   public double getArmJointspaceMaxIntegralError()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 0.2; //0.0;
-   }
+      taskspaceControlGains.setProportionalGain(kp);
+      taskspaceControlGains.setDampingRatio(zeta);
+      taskspaceControlGains.setIntegralGain(ki);
+      taskspaceControlGains.setMaximumIntegralError(maxIntegralError);
+      taskspaceControlGains.setMaximumAcceleration(maxAccel);
+      taskspaceControlGains.setMaximumJerk(maxJerk);
+      taskspaceControlGains.createDerivativeGainUpdater(true);
 
-   public double getArmJointspaceMaxAcceleration()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 5.0; //1.0; //20.0;
-   }
-
-   public double getArmJointspaceMaxJerk()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 100.0;
-   }
-
-   public double getArmTaskspaceKp()
-   {
-      if (!runningOnRealRobot)  return 100.0;
-      return 100.0; 
-   }
-
-   public double getArmTaskspaceZeta()
-   {
-      if (!runningOnRealRobot)  return 1.0;
-      return 0.6; 
-   }
-
-   public double getArmTaskspaceKi()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 0.0; 
-   }
-
-   public double getArmTaskspaceMaxIntegralError()
-   {
-      if (!runningOnRealRobot)  return 0.0;
-      return 0.0; 
-   }
-
-   public double getArmTaskspaceMaxAcceleration()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 10.0; 
-   }
-
-   public double getArmTaskspaceMaxJerk()
-   {
-      if (!runningOnRealRobot)  return Double.POSITIVE_INFINITY;
-      return 100.0; 
+      return taskspaceControlGains;
    }
 
    @Override
@@ -141,7 +125,7 @@ public class ValkyrieArmControllerParameters implements ArmControllerParameters
       jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.ELBOW_YAW), 0.0);
       jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.WRIST_PITCH), 0.0);
       jointPositions.put(fullRobotModel.getArmJoint(robotSide, ArmJointName.WRIST_ROLL), 0.0);
-      
+
       return jointPositions;
    }
 }
