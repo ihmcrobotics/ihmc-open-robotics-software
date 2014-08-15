f511667ecd2ffd43d5b975ee1c3f4784e4e04a32
Jerry Pratt <jpratt@ihmc.us>
2014-08-15 22:38:34 +0000

    DRC-1638: BigStepUpWithHandPlatform works if we fake the second step to be high enough to clear the ground. Need to fix so that they clear the ground anyway. Won't fixBamboo.

--- Atlas/src/us/ihmc/atlas/AtlasWallWorldDemo.java
+++ Atlas/src/us/ihmc/atlas/AtlasWallWorldDemo.java
@@ -3,8 +3,8 @@ package us.ihmc.atlas;
 import us.ihmc.atlas.parameters.AtlasContactPointParameters;
 import us.ihmc.commonAvatarInterfaces.CommonAvatarEnvironmentInterface;
 import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseDemoStarter;
-import us.ihmc.darpaRoboticsChallenge.DRCWallWorldEnvironment;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.environment.BigStepUpWithHandPlatformEnvironment;
 import us.ihmc.darpaRoboticsChallenge.visualization.SliderBoardFactory;
 import us.ihmc.darpaRoboticsChallenge.visualization.WalkControllerSliderBoard;
 import us.ihmc.utilities.processManagement.JavaProcessSpawner;
@@ -20,10 +20,12 @@ public class AtlasWallWorldDemo extends DRCObstacleCourseDemoStarter
       SliderBoardFactory sliderBoardFactory = WalkControllerSliderBoard.getFactory();
       AtlasWallWorldDemo atlasDemo = new AtlasWallWorldDemo();
       boolean automaticallyStartSimulation = true;
-      boolean startDRCNetworkProcessor = false;
+      boolean startDRCNetworkProcessor = true;
       boolean initializeEstimatorToActual = false;
 
-      CommonAvatarEnvironmentInterface environment = new DRCWallWorldEnvironment(-10.0, 10.0);
+      //      CommonAvatarEnvironmentInterface environment = new DRCWallWorldEnvironment(-10.0, 10.0);
+      double stepHeight = 0.2;
+      CommonAvatarEnvironmentInterface environment = new BigStepUpWithHandPlatformEnvironment(stepHeight);
       
       AtlasContactPointParameters contactPointParameters = robotModel.getContactPointParameters();
       contactPointParameters.createHandKnobContactPoints();
