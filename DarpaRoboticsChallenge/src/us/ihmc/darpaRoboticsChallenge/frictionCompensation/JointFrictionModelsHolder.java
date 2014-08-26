6b9deab2349a1d16aecd82cc12ff9bd2ccf1ec23
Luca Fiorio <lfiorio@ihmc.us>
2014-08-26 23:38:16 +0000

    NONE: clean up and loading the friction models

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/frictionCompensation/JointFrictionModelsHolder.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/frictionCompensation/JointFrictionModelsHolder.java
@@ -13,14 +13,14 @@ import com.yobotics.simulationconstructionset.YoVariableRegistry;
 public class JointFrictionModelsHolder
 {
    private final String name;
-   
+
    protected final DoubleYoVariable frictionForce;
    protected final EnumYoVariable<FrictionState> frictionCompensationState;
    protected final EnumYoVariable<FrictionModel> activeFrictionModel;
    protected final EnumMap<FrictionModel, JointFrictionModel> frictionModels;
-   
+
    private double stictionTransitionVelocity;
-   
+
    public JointFrictionModelsHolder(String name, YoVariableRegistry registry)
    {
       this.name = name;
@@ -36,56 +36,56 @@ public class JointFrictionModelsHolder
     * In case of velocity the equivalent velocity is the desired velocity. 
     * As current Joint velocity use the less noisy.
     */
-    protected Double selectFrictionStateAndFrictionVelocity(double requestedForce, double currentJointVelocity, double requestedJointVelocity)
-    {
-       double velocityForFrictionCalculation;
-       
-       if(activeFrictionModel.getEnumValue() == FrictionModel.OFF)
-       {
-          frictionCompensationState.set(FrictionState.NOT_COMPENSATING);
-          frictionForce.set(0.0);  
-          return null;
-       }
-       
-       if (requestedJointVelocity == 0.0 && requestedForce == 0.0)
-       {
-          frictionCompensationState.set(FrictionState.NOT_COMPENSATING);
-          frictionForce.set(0.0);
-          return null;
-       }
+   protected Double selectFrictionStateAndFrictionVelocity(double requestedForce, double currentJointVelocity, double requestedJointVelocity)
+   {
+      double velocityForFrictionCalculation;
 
-       if (Math.abs(currentJointVelocity) > stictionTransitionVelocity)
-       {
-          frictionCompensationState.set(FrictionState.OUT_STICTION);
-          velocityForFrictionCalculation = currentJointVelocity;
-       }
-       else
-       {
-          if (requestedJointVelocity == 0.0)
-          {
-             frictionCompensationState.set(FrictionState.IN_STICTION_FORCE_MODE);
-             velocityForFrictionCalculation = stictionTransitionVelocity * Math.signum(requestedForce);
-          }
-          else
-          {
-             frictionCompensationState.set(FrictionState.IN_STICTION_VELOCITY_MODE);
-             velocityForFrictionCalculation = requestedJointVelocity;
-          }
-       }
+      if (activeFrictionModel.getEnumValue() == FrictionModel.OFF)
+      {
+         frictionCompensationState.set(FrictionState.NOT_COMPENSATING);
+         frictionForce.set(0.0);
+         return null;
+      }
 
-       return velocityForFrictionCalculation;
-    }
+      if (requestedJointVelocity == 0.0 && requestedForce == 0.0)
+      {
+         frictionCompensationState.set(FrictionState.NOT_COMPENSATING);
+         frictionForce.set(0.0);
+         return null;
+      }
 
-    public void setActiveFrictionModel(FrictionModel requestedFrictionModel)
-    {
-       activeFrictionModel.set(requestedFrictionModel);
-    }
-    
+      if (Math.abs(currentJointVelocity) > stictionTransitionVelocity)
+      {
+         frictionCompensationState.set(FrictionState.OUT_STICTION);
+         velocityForFrictionCalculation = currentJointVelocity;
+      }
+      else
+      {
+         if (requestedJointVelocity == 0.0)
+         {
+            frictionCompensationState.set(FrictionState.IN_STICTION_FORCE_MODE);
+            velocityForFrictionCalculation = stictionTransitionVelocity * Math.signum(requestedForce);
+         }
+         else
+         {
+            frictionCompensationState.set(FrictionState.IN_STICTION_VELOCITY_MODE);
+            velocityForFrictionCalculation = requestedJointVelocity;
+         }
+      }
+
+      return velocityForFrictionCalculation;
+   }
+
+   public void setActiveFrictionModel(FrictionModel requestedFrictionModel)
+   {
+      activeFrictionModel.set(requestedFrictionModel);
+   }
+
    public FrictionModel getActiveFrictionModel()
    {
       return activeFrictionModel.getEnumValue();
    }
-   
+
    protected JointFrictionModel getActiveJointFrictionModel()
    {
       return frictionModels.get(activeFrictionModel.getEnumValue());
