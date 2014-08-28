a430ca25d10d70c15c0eb99423035425f8c672f4
Nathan Mertins <nmertins@ihmc.us>
2014-08-28 20:07:53 +0000

    DRC-1715: pulled network port constants out of DRCConfigParameters into new NetworkConfigParameters class in IHMCCommunication

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
@@ -10,14 +10,6 @@ public class DRCConfigParameters
    public static final boolean USE_HYDRA = false;
    public static final boolean USE_MINI_ATLAS = false;
 
-   // Networking
-   public static final int NETWORK_PROCESSOR_TO_CONTROLLER_TCP_PORT = 4895;
-   public static final int NETWORK_PROCESSOR_TO_UI_TCP_PORT = 4897;
-   public static final int NETWORK_PROCESSOR_TO_UI_RAW_PROTOCOL_TCP_PORT = 4898;
-   public static final int NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_TCP_PORT = 5000;
-   public static final int CONTROLLER_CLOUD_DISPATCHER_BACKEND_TCP_PORT = 5002;
-   public static final int DEFAULT_YOVARIABLE_SERVER_PORT = 5555;
-
    // Log images from the primary camera
    public static boolean LOG_PRIMARY_CAMERA_IMAGES = false;
 
---
+++ IHMCCommunication/src/us/ihmc/communication/networking/NetworkConfigParameters.java
@@ -0,0 +1,12 @@
+package us.ihmc.communication.networking;
+
+public class NetworkConfigParameters
+{
+   // Ports
+   public static final int NETWORK_PROCESSOR_TO_CONTROLLER_TCP_PORT = 4895;
+   public static final int NETWORK_PROCESSOR_TO_UI_TCP_PORT = 4897;
+   public static final int NETWORK_PROCESSOR_TO_UI_RAW_PROTOCOL_TCP_PORT = 4898;
+   public static final int NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_TCP_PORT = 5000;
+   public static final int CONTROLLER_CLOUD_DISPATCHER_BACKEND_TCP_PORT = 5002;
+   public static final int DEFAULT_YOVARIABLE_SERVER_PORT = 5555;
+}
