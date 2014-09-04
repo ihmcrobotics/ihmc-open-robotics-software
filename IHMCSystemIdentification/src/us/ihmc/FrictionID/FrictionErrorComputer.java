5357590dcf5c3fd823e3768b9e4e9608a0c19e48
Luca Fiorio <lfiorio@ihmc.us>
2014-09-04 01:47:02 +0000

    DRC-1713: adding method to equalize samples number for different velocity intervals

--- IHMCSystemIdentification/src/us/ihmc/FrictionID/FrictionErrorComputer.java
+++ IHMCSystemIdentification/src/us/ihmc/FrictionID/FrictionErrorComputer.java
@@ -8,13 +8,34 @@ import org.ddogleg.optimization.functions.FunctionNtoM;
 import us.ihmc.utilities.frictionModels.FrictionModel;
 import us.ihmc.utilities.frictionModels.JointFrictionModel;
 import us.ihmc.utilities.frictionModels.PressureBasedFrictionModel;
-import us.ihmc.utilities.frictionModels.PressureBasedFrictionModel;
+
+/**
+ * Use this class to compute the error between measured joint torque and predicted joint torque for a set of samples.
+ * The class can also uniformly equalize the number of samples per velocity intervals. Use the dedicated constructor. 
+ * If you are not interested in doing identification with the pressure based friction model, the positive and negative pressures can be all zeros.
+ * 
+ * @param model - instance of JointFrictionModel to identify.
+ * @param sampleMap - an HashMap of String and ArrayList<Double> containing all samples. In particular the following quantities are required:
+ * @param   "position"
+ * @param   "velocity"
+ * @param   "acceleration"
+ * @param   "negativePressure"
+ * @param   "positivePressure"
+ * @param   "measuredTorque"
+ * 
+ * @param uniformSamples - set to true if you want to uniformly equalize the samples.
+ * @param numberOfIntervals - specify the number of velocity intervals used to perform sample equalization.
+ * @param maxPointPerInterval - specify the maximum number of samples per each interval.
+ * 
+ */
 
 public class FrictionErrorComputer implements FunctionNtoM
 {
    private static final double OFFSET_LOWER_BOUND = -Math.PI;
    private static final double OFFSET_UPPER_BOUND = Math.PI;
    private static final int NUMBER_OF_ADDITIONAL_PARAMETERS = 3;
+   private static final String[] EXPECTED_INPUT_VARIABLES = {"position", "velocity", "acceleration", "negativePressure", "positivePressure", "measuredTorque"};
+   private static final int NUMBER_OF_REQUIRED_INPUTS = EXPECTED_INPUT_VARIABLES.length;
    
    private JointFrictionModel model;
    private final HashMap<String, ArrayList<Double>> sampleMap;
@@ -23,11 +44,13 @@ public class FrictionErrorComputer implements FunctionNtoM
    private double frictionTorque, inertiaTorque, gravityTorque;
    private double boundedOffset;
    private double[] frictionParameters;
-
+   
    /**
     * Use this class to compute the error between measured joint torque and predicted joint torque for a set of samples.
+    * The class can also uniformly equalize the number of samples per velocity intervals. Use the dedicated constructor. 
+    * If you are not interested in doing identification with the pressure based friction model, the positive and negative pressures can be all zeros.
     * 
-    * @param model - instance of JointFrictionModel to identify
+    * @param model - instance of JointFrictionModel to identify.
     * @param sampleMap - an HashMap of String and ArrayList<Double> containing all samples. In particular the following quantities are required:
     * @param   "position"
     * @param   "velocity"
@@ -37,13 +60,48 @@ public class FrictionErrorComputer implements FunctionNtoM
     * @param   "measuredTorque"
     * 
     */
-
    public FrictionErrorComputer(JointFrictionModel model, HashMap<String, ArrayList<Double>> sampleMap)
-   {
-      this.model = model;
-      this.sampleMap = sampleMap;
+   {      
+      this(model, sampleMap, false, 0, 0);
+   }
 
-      numberOfSamples = sampleMap.get("position").size();
+   /**
+    * Use this class to compute the error between measured joint torque and predicted joint torque for a set of samples.
+    * The class can also uniformly equalize the number of samples per velocity intervals.
+    * If you are not interested in doing identification with the pressure based friction model, the positive and negative pressures can be all zeros.
+    * 
+    * @param model - instance of JointFrictionModel to identify.
+    * @param sampleMap - an HashMap of String and ArrayList<Double> containing all samples. In particular the following quantities are required:
+    * @param   "position"
+    * @param   "velocity"
+    * @param   "acceleration"
+    * @param   "negativePressure"
+    * @param   "positivePressure"
+    * @param   "measuredTorque"
+    * 
+    * @param uniformSamples - set to true if you want to uniformly equalize the samples.
+    * @param numberOfIntervals - specify the number of velocity intervals used to perform sample equalization.
+    * @param maxPointPerInterval - specify the maximum number of samples per each interval.
+    * 
+    */
+   public FrictionErrorComputer(JointFrictionModel model, HashMap<String, ArrayList<Double>> sampleMap, boolean uniformSamples, int numberOfIntervals, int maxPointPerInterval)
+   {
+      if (sampleMap.size() != NUMBER_OF_REQUIRED_INPUTS || sampleMap == null)
+      {
+         throw new IllegalArgumentException("The input sample map is not valid. Check if is null or if number of inputs is not correct.");
+      }
+      
+      if(uniformSamples)
+      {
+         this.sampleMap = uniformSamplesByVelocity(sampleMap, numberOfIntervals, maxPointPerInterval);
+      }
+      else
+      {
+      this.sampleMap = sampleMap;
+      }
+      
+      this.model = model;
+      numberOfSamples = this.sampleMap.get("position").size();
    }
 
    @Override
@@ -101,6 +159,122 @@ public class FrictionErrorComputer implements FunctionNtoM
          gravityTorque = input[1] * Math.cos(sampleMap.get("position").get(i) + boundedOffset);
          output[i] = sampleMap.get("measuredTorque").get(i) - frictionTorque - inertiaTorque - gravityTorque;
       }
+   }
+   
+   /**
+    * This function can be used to uniform the samples creating velocity intervals between the max and min velocity.
+    * For each interval the number of samples is limited to the requested maximum number of samples.
+    * If the interval contains more samples than the maximum the algorithm collects randomly only the maximum number of samples.
+    * If the interval contains less samples that the maximum the algorithm takes all samples.
+    *  
+    * @param samples - an HashMap of String and ArrayList<Double> containing all samples. In particular the following quantities are required:
+    *                  "position", "velocity", "acceleration", "negativePressure", "positivePressure", "measuredTorque"
+    * @param numberOfIntervals - specify the number of intervals between max and min velocity
+    * @param maxPointPerInterval - specify the maximum number of samples that will be collected for each velocity interval
+    * @return - an HashMap of String and ArrayList<Double> containing the new samples.
+    */
+   private HashMap<String, ArrayList<Double>> uniformSamplesByVelocity(HashMap<String, ArrayList<Double>> samples, int numberOfIntervals, int maxPointPerInterval)
+   {
+      HashMap<String, ArrayList<Double>> ret = new HashMap<String, ArrayList<Double>>();
+      for (int i = 0; i < NUMBER_OF_REQUIRED_INPUTS; i++)
+      {
+         ret.put(EXPECTED_INPUT_VARIABLES[i], new ArrayList<Double>());
+      }
+
+      double maxVelocity = getMaxValue(samples.get("velocity"));
+      double minVelocity = getMinValue(samples.get("velocity"));
+      double delta = (maxVelocity - minVelocity)/numberOfIntervals;
+      
+      for(int i = 0; i < numberOfIntervals; i++)
+      {
+         double lowerBound = minVelocity + i * delta;
+         double upperBound = minVelocity + (i+1) * delta;
+         ArrayList<Integer> indexList = findIndexOfValuesBetweenBounds(samples.get("velocity"), upperBound, lowerBound);
+         
+         if(indexList.size() > maxPointPerInterval)
+         {
+            indexList = pickElementsRandomly(indexList, maxPointPerInterval);
+         }
+         
+         for(int j = 0; j < NUMBER_OF_REQUIRED_INPUTS; j++)
+         {
+            for(int k = 0; k < indexList.size(); k++)
+            {
+               ret.get(EXPECTED_INPUT_VARIABLES[j]).add(samples.get(EXPECTED_INPUT_VARIABLES[j]).get(indexList.get(k)));               
+            }   
+         }
+      }
+      
+      return ret;
+   }
+   
+   private ArrayList<Integer> pickElementsRandomly(ArrayList<Integer> list, int maxNumberOfElements)
+   {
+      ArrayList<Integer> newList = new ArrayList<Integer>();
+      boolean enoughElementsSelected = false;
+      
+      while(!enoughElementsSelected) // there might be a problem if the number of samples in the input list is close to the max number of Elements, it will take a while to collect the samples. the solution is to remove element already taken from the list
+      {
+         int index = (int) Math.round(Math.random() * (list.size()-1));
+         
+         if(!newList.contains(list.get(index)))
+         {
+            newList.add(list.get(index));
+            list.remove(index);
+         }
+         
+         if(newList.size() >= maxNumberOfElements)
+         {
+            enoughElementsSelected = true;
+         }
+      }
+      
+      return newList;
+   }
+   
+   private ArrayList<Integer> findIndexOfValuesBetweenBounds(ArrayList<Double> list, double max, double min)
+   {
+      ArrayList<Integer> indexList = new ArrayList<Integer>();
+      
+      for(int i = 0; i < list.size(); i++)
+      {
+         if(list.get(i) < max && list.get(i) > min)
+         {
+            indexList.add(i);
+         }
+      }
+      
+      return indexList;
+   }
+   
+   private double getMaxValue(ArrayList<Double> list)
+   {
+      double max = Double.NEGATIVE_INFINITY;
+      
+      for (int i = 0; i < list.size(); i++)
+      {
+         if (list.get(i) > max) 
+         {
+           max = list.get(i);
+         }
+      }
+      
+      return max;
+   }
+   
+   private double getMinValue(ArrayList<Double> list)
+   {
+      double min = Double.POSITIVE_INFINITY;
+      
+      for (int i = 0; i < list.size(); i++)
+      {
+         if (list.get(i) < min) 
+         {
+           min = list.get(i);
+         }
+      }
+      
+      return min;
    }
 
    static public synchronized double getBoundedOffset(double value)
