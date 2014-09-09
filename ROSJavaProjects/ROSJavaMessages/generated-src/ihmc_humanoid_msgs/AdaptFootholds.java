127573c53ba4ee00d1d42cefa1823218b92cc277
Peter Fankhauser <peterfa@student.ethz.ch>
2014-09-09 03:18:18 +0000

    DRC-1742: Added foothold adaptation messages and service for communication with ROS.

---
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_humanoid_msgs/AdaptFootholds.java
@@ -0,0 +1,6 @@
+package ihmc_humanoid_msgs;
+
+public interface AdaptFootholds extends org.ros.internal.message.Message {
+  static final java.lang.String _TYPE = "ihmc_humanoid_msgs/AdaptFootholds";
+  static final java.lang.String _DEFINITION = "# Takes an initial list of footholds and returns the footholds adapted to the terrain\r\n\r\nFootholdList initialFoodholds\r\n---\r\nFootholdList adaptedFoodholds";
+}
