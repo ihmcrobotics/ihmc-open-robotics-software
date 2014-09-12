68bbd3d3f0ffb77e5981aeb9f4c9fcdb2cb61f4a
Jesper Smith <jsmith@ihmc.us>
2014-09-12 16:06:34 +0000

    DRC-1781: Decreased Valkyrie control DT to 0.003ms, estimator DT to 0.0015ms

--- src/us/ihmc/valkyrie/configuration/ValkyriePriorityParameters.java
+++ src/us/ihmc/valkyrie/configuration/ValkyriePriorityParameters.java
@@ -4,15 +4,11 @@ import us.ihmc.realtime.PriorityParameters;
 import us.ihmc.valkyrie.roboNet.api.turbodriver_api.TurbodriverAPI_DRCv4_bench;
 import us.ihmc.valkyrie.roboNet.api.turbodriver_api.TurbodriverAPI_DRCv4_ihmc;
 
-public class ValkyrieTimingParameters
+public class ValkyriePriorityParameters
 {
    public static final PriorityParameters ESTIMATOR_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 1);
    public static final PriorityParameters CONTROLLER_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 5);
-   public static final PriorityParameters IMU_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 10);
-   
-   public static final long ESTIMATOR_DT_IN_NS = 2000000;
-   public static final long CONTROL_DT_IN_NS = 6000000;
-   
+   public static final PriorityParameters IMU_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 10);   
 
    // FQN of the turbodriver we use to test overall round trip time
    public static final Class<?>[] timedTurbodrivers = {TurbodriverAPI_DRCv4_ihmc.class, TurbodriverAPI_DRCv4_bench.class};
