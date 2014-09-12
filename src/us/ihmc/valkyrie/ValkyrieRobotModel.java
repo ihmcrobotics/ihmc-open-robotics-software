cf6880c36c3571eaab606268463719cf6ea4954c
Jesper Smith <jsmith@ihmc.us>
2014-09-12 16:06:34 +0000

    DRC-1781: Decreased Valkyrie control DT to 0.003ms, estimator DT to 0.0015ms

--- src/us/ihmc/valkyrie/ValkyrieRobotModel.java
+++ src/us/ihmc/valkyrie/ValkyrieRobotModel.java
@@ -273,13 +273,13 @@ public class ValkyrieRobotModel implements DRCRobotModel
    @Override
    public double getEstimatorDT()
    {
-      return 0.002;
+      return 0.0015;
    }
 
    @Override
    public double getControllerDT()
    {
-      return 0.004;
+      return 0.003;
    }
 
    @Override
