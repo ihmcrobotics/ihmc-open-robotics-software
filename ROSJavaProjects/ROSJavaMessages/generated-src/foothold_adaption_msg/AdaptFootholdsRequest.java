cc14dd62c834b1b548b149c984dd1e0a5fde858b
Peter Fankhauser <peterfa@student.ethz.ch>
2014-09-15 21:37:31 +0000

    DRC-1745: Bugfixing footstep adaption communication with ROS.

--- ROSJavaProjects/ROSJavaMessages/generated-src/foothold_adaption_msg/AdaptFootholdsRequest.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/foothold_adaption_msg/AdaptFootholdsRequest.java
@@ -1,8 +1,8 @@
-package ihmc_humanoid_msgs;
+package foothold_adaption_msg;
 
 public interface AdaptFootholdsRequest extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "ihmc_humanoid_msgs/AdaptFootholdsRequest";
+  static final java.lang.String _TYPE = "foothold_adaption_msg/AdaptFootholdsRequest";
   static final java.lang.String _DEFINITION = "# Takes an initial list of footholds and returns the footholds adapted to the terrain\r\n\r\nFootholdList initialFoodholds\r\n";
-  ihmc_humanoid_msgs.FootholdList getInitialFoodholds();
-  void setInitialFoodholds(ihmc_humanoid_msgs.FootholdList value);
+  foothold_adaption_msg.FootholdList getInitialFoodholds();
+  void setInitialFoodholds(foothold_adaption_msg.FootholdList value);
 }
