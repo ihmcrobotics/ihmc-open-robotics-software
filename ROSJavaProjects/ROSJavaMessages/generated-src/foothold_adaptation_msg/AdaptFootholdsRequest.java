686d6550c2aa723b8e42725c70862bd5aaecccf2
Peter Fankhauser <peterfa@student.ethz.ch>
2014-09-15 22:27:01 +0000

    DRC-1745: Renaming ROS interfaces. Wont fixBamboo.

--- ROSJavaProjects/ROSJavaMessages/generated-src/foothold_adaptation_msg/AdaptFootholdsRequest.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/foothold_adaptation_msg/AdaptFootholdsRequest.java
@@ -1,8 +1,8 @@
-package foothold_adaption_msg;
+package foothold_adaptation_msg;
 
 public interface AdaptFootholdsRequest extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "foothold_adaption_msg/AdaptFootholdsRequest";
+  static final java.lang.String _TYPE = "foothold_adaptation_msg/AdaptFootholdsRequest";
   static final java.lang.String _DEFINITION = "# Takes an initial list of footholds and returns the footholds adapted to the terrain\r\n\r\nFootholdList initialFoodholds\r\n";
-  foothold_adaption_msg.FootholdList getInitialFoodholds();
-  void setInitialFoodholds(foothold_adaption_msg.FootholdList value);
+  foothold_adaptation_msg.FootholdList getInitialFoodholds();
+  void setInitialFoodholds(foothold_adaptation_msg.FootholdList value);
 }
