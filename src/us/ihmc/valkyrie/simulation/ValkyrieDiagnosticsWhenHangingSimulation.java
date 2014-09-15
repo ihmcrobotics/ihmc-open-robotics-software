f1d356b5f205aa4c2202175c25d9ee95cbe85ea9
Jerry Pratt <jpratt@ihmc.us>
2014-09-15 19:10:28 +0000

    DRC-1782: Cleaned up and moved VirtualHoist. Setup ValkyrieDiagnosticsWhenHangingSimulation. Right now does nothing. Next we need to make a controller to do the diagnostics.

---
+++ src/us/ihmc/valkyrie/simulation/ValkyrieDiagnosticsWhenHangingSimulation.java
@@ -0,0 +1,69 @@
+package us.ihmc.valkyrie.simulation;
+
+import java.util.ArrayList;
+
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.darpaRoboticsChallenge.HumanoidDiagnosticsWhenHangingSimulation;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
+import us.ihmc.valkyrie.ValkyrieInitialSetup;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+
+import com.yobotics.simulationconstructionset.Joint;
+import com.yobotics.simulationconstructionset.util.virtualHoist.VirtualHoist;
+
+public class ValkyrieDiagnosticsWhenHangingSimulation
+{
+   public ValkyrieDiagnosticsWhenHangingSimulation()
+   {
+      DRCRobotModel robotModel = new ValkyrieRobotModelWithHoist(false, false);
+      double groundZ = 0.0;
+      double initialYaw = 0.0;
+      DRCRobotInitialSetup<SDFRobot> robotInitialSetup = new ValkyrieInitialSetup(groundZ, initialYaw);
+      
+      new HumanoidDiagnosticsWhenHangingSimulation(robotModel, robotInitialSetup);
+   }
+
+   public static void main(String[] args)
+   {
+      new ValkyrieDiagnosticsWhenHangingSimulation();
+   }
+   
+   
+   private class ValkyrieRobotModelWithHoist extends ValkyrieRobotModel
+   {
+
+      public ValkyrieRobotModelWithHoist(boolean runningOnRealRobot, boolean headless)
+      {
+         super(runningOnRealRobot, headless);
+      }
+      
+      @Override
+      public SDFRobot createSdfRobot(boolean createCollisionMeshes)
+      {
+         SDFRobot robot = super.createSdfRobot(createCollisionMeshes);
+         
+         Joint joint = robot.getJoint("WaistLateralExtensor");
+         
+         ArrayList<Vector3d> attachmentLocations = new ArrayList<Vector3d>();
+             
+         attachmentLocations.add(new Vector3d(0.0, 0.3, 0.5));
+         attachmentLocations.add(new Vector3d(0.0, -0.3, 0.5));
+         
+         double updateDT = 0.0001;
+         VirtualHoist virtualHoist = new VirtualHoist(joint, robot, attachmentLocations, updateDT);
+         robot.setController(virtualHoist, 1);
+          
+         virtualHoist.turnHoistOn();
+         virtualHoist.setTeepeeLocation(new Point3d(0.0, 0.0, 2.5));
+         virtualHoist.setHoistStiffness(20000.0);
+         virtualHoist.setHoistDamping(5000.0);
+         
+         return robot;
+      }
+
+   }
+}
