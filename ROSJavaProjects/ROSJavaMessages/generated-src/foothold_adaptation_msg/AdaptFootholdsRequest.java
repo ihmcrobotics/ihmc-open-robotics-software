8266b1c073314251437a014bab25376f1c649e0a
Matthijs Damen <mdamen@ihmc.us>
2014-09-18 16:20:47 +0000

    DRC-1745: Adjust ROS message structure. won't FixBamboo

--- ROSJavaProjects/ROSJavaMessages/generated-src/foothold_adaptation_msg/AdaptFootholdsRequest.java
+++
@@ -1,8 +0,0 @@
-package foothold_adaptation_msg;
-
-public interface AdaptFootholdsRequest extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "foothold_adaptation_msg/AdaptFootholdsRequest";
-  static final java.lang.String _DEFINITION = "# Takes an initial list of footholds and returns the footholds adapted to the terrain\r\n\r\nFootholdList initialFoodholds\r\n";
-  foothold_adaptation_msg.FootholdList getInitialFoodholds();
-  void setInitialFoodholds(foothold_adaptation_msg.FootholdList value);
-}
