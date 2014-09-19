8b003ed0c9e3141d90b23fe398c3e219a0165053
Jerry Pratt <jpratt@ihmc.us>
2014-09-19 22:17:44 +0000

    NONE: Cleaning up YoContactPoint so that it actually does someething.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/bipedSupportPolygons/YoContactPoint.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/bipedSupportPolygons/YoContactPoint.java
@@ -1,14 +1,17 @@
 package us.ihmc.commonWalkingControlModules.bipedSupportPolygons;
 
+import us.ihmc.utilities.math.geometry.FramePoint;
 import us.ihmc.utilities.math.geometry.FramePoint2d;
 import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
 import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
+import us.ihmc.yoUtilities.math.frames.YoFramePoint;
 import us.ihmc.yoUtilities.math.frames.YoFramePoint2d;
 
 
 public class YoContactPoint extends ContactPoint
 {
    private final YoVariableRegistry registry;
+   private final YoFramePoint yoPosition;
    private final YoFramePoint2d yoPosition2d;
    private final BooleanYoVariable isInContact;
    private final String namePrefix;
@@ -22,8 +25,8 @@ public class YoContactPoint extends ContactPoint
       //TODO: Check if it is better to create an actual child registry
       registry = parentRegistry;
 
-      //TODO That one seems to be useless
-      yoPosition2d = new YoFramePoint2d(namePrefix + "Contact" + index, point2d.getReferenceFrame(), registry);
+      yoPosition = new YoFramePoint(namePrefix + "Contact" + index, point2d.getReferenceFrame(), registry);
+      yoPosition2d = new YoFramePoint2d(namePrefix + "Contact2d" + index, point2d.getReferenceFrame(), registry);
       isInContact = new BooleanYoVariable(namePrefix + "InContact" + index, registry);
    }
 
@@ -37,10 +40,19 @@ public class YoContactPoint extends ContactPoint
       super.setInContact(inContact);
       isInContact.set(inContact);
    }
-
-   public YoFramePoint2d getYoPosition2d()
+   
+   public FramePoint2d getPosition2d()
    {
-      return yoPosition2d;
+      FramePoint2d position2d = super.getPosition2d();
+      yoPosition2d.set(position2d);
+      return position2d;
+   }
+
+   public FramePoint getPosition()
+   {
+      FramePoint position = super.getPosition();
+      yoPosition.set(position);
+      return position;
    }
    
    public String toString()
