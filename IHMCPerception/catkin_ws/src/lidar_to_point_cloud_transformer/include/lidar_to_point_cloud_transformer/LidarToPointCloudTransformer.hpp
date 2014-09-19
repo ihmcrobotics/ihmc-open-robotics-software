df8f63cf75accea66d10972e5a713979d8794c9a
Tingfan Wu <twu@ihmc.us>
2014-09-19 04:28:25 +0000

    DRC-1721: initial commit of Robot Self Filter, still have parameters hardcoded in

--- IHMCPerception/catkin_ws/src/lidar_to_point_cloud_transformer/include/lidar_to_point_cloud_transformer/LidarToPointCloudTransformer.hpp
+++ IHMCPerception/catkin_ws/src/lidar_to_point_cloud_transformer/include/lidar_to_point_cloud_transformer/LidarToPointCloudTransformer.hpp
@@ -16,11 +16,51 @@
 #include <sensor_msgs/LaserScan.h>
 #include <laser_geometry/laser_geometry.h>
 #include <filters/filter_chain.h>
-
+#include <moveit/point_containment_filter/shape_mask.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
 // PCL
 #include <pcl/pcl_base.h>
 #include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
+#include <string>
+#include <limits>
+
+
+using namespace point_containment_filter;
+
+class RobotSelfFilter
+{
+	  mutable boost::recursive_mutex shape_handles_lock_;
+public:
+	 typedef std::map<const robot_model::LinkModel*, std::vector<std::pair<ShapeHandle, std::size_t> > > LinkShapeHandles;
+	 typedef std::map<ShapeHandle, Eigen::Affine3d, std::less<ShapeHandle>,
+	                  Eigen::aligned_allocator<std::pair<const ShapeHandle, Eigen::Affine3d> > > ShapeTransformCache;
+
+	LinkShapeHandles link_shape_handles_;
+	boost::scoped_ptr<ShapeMask> shape_mask_;
+	boost::shared_ptr<robot_model_loader::RobotModelLoader> robot_model_loader;
+	ShapeTransformCache transformCache;
+	const double scale=1;
+	const double padding=0.1;
+	const double max_range_=std::numeric_limits<double>::max();
+	tf::TransformListener tfListener;
+
+	RobotSelfFilter();
+
+	void addAllLinksToShapeMask();
+
+	bool getShapeTransform(ShapeHandle h, Eigen::Affine3d &transform) const;
+
+	~RobotSelfFilter(){};
+
+//	inline void filterPointClould(sensor_msgs::PointCloud2::Ptr & cloud_msg)
+//	{
+//		ROS_ERROR("not implemented, supporting moveit-current (09/07/2014)");
+//	};
+
+	void filterPointClould(const pcl::PointCloud<pcl::PointXYZI> & source_cloud, pcl::PointCloud<pcl::PointXYZI>& filtered_cloud);
+	bool updateShapeTransformCache(const std::string &target_frame, const ros::Time &target_time);
+};
 
 namespace lidar_to_point_cloud_transformer {
 
@@ -93,6 +133,10 @@ class LidarToPointCloudTransformer
   //! Point cloud publisher for each scan.
   ros::Publisher pointCloudPublisher_;
 
+  //! Point cloud selfFilter
+  boost::shared_ptr<RobotSelfFilter> selfFilter;
+  ros::Publisher filteredPointCloudPublisher_;
+
   //! Assembled point cloud publisher.
   ros::Publisher assembledPointCloudPublisher_;
 
@@ -113,6 +157,8 @@ class LidarToPointCloudTransformer
 
   //! Projection from laser to point cloud.
   laser_geometry::LaserProjection laserProjector_;
+
+
 };
 
 } /* namespace */
