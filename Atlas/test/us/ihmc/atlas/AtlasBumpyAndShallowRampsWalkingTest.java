2c112abdcd3b3ff3de90ee25613d495d258b0563
Luca Fiorio <lfiorio@ihmc.us>
2014-09-22 16:22:48 +0000

    NONE: revert this test. going to add a dedicated test for foot exploration

--- Atlas/test/us/ihmc/atlas/AtlasBumpyAndShallowRampsWalkingTest.java
+++ Atlas/test/us/ihmc/atlas/AtlasBumpyAndShallowRampsWalkingTest.java
@@ -1,6 +1,5 @@
 package us.ihmc.atlas;
 
-import us.ihmc.atlas.parameters.AtlasContactPointParameters;
 import us.ihmc.bambooTools.BambooTools;
 import us.ihmc.darpaRoboticsChallenge.DRCBumpyAndShallowRampsWalkingTest;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
@@ -11,11 +10,7 @@ public class AtlasBumpyAndShallowRampsWalkingTest extends DRCBumpyAndShallowRamp
    @Override
    public DRCRobotModel getRobotModel()
    {
-      DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS, false, false);
-
-      AtlasContactPointParameters contactPointParameters = (AtlasContactPointParameters) robotModel.getContactPointParameters();
-      contactPointParameters.createHandKnobContactPoints();
-      return robotModel;
+      return new AtlasRobotModel(AtlasRobotVersion.DRC_NO_HANDS, false, false);
    }
 
    @Override
