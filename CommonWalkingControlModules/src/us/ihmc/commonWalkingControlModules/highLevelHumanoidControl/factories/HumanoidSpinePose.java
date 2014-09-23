9176201966ec7d837a0ea92c8e32ae91468c05df
Duncan Calvert <dcalvert@ihmc.us>
2014-09-23 22:34:05 +0000

    DRC-1786: Valkyrie diagnostics update. Found duplicate enum poses. Added descriptions of parities and joint angle mins and maxes in comments. Better names. More poses.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/HumanoidSpinePose.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/HumanoidSpinePose.java
@@ -2,34 +2,35 @@ package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories;
 
 public enum HumanoidSpinePose
 {
-   STAND_PREP, FORWARD, BACKWARD, LEFT, RIGHT, SPINE_ONE, SPINE_TWO, SPINE_THREE;
+   STAND_PREP, TURN_LEFT, TURN_RIGHT, LEAN_FORWARD_A_LOT, LEAN_BACKWARD_A_LOT, LEAN_LEFT, LEAN_RIGHT;
  
-   
+   /**
+    * Spine angles are as follows:
+    * 
+    * 1 - waist rotator (negative torso right / positive torso left) min: 0.0 max: 0.0 
+    * 2 - waist extensor (negative back / positive forward) min: -0.06 max: 0.1
+    * 3 - waist lateral extensor (negative lean left / positive lean right) min: -0.1  max: 0.1
+    * 
+    * @return armAngles
+    */
    public double[] getSpineJointAngles()
    {
       switch (this)
       {
          case STAND_PREP:
             return new double[]{0.0, -0.06, 0.0};
-         case FORWARD:
-            return new double[]{0.0, -0.06, 0.0};
-         case BACKWARD:
-            return new double[]{0.0, -0.06, 0.0};
-         case LEFT:
-            return new double[]{0.0, -0.06, 0.0};
-         case RIGHT:
-            return new double[]{0.0, -0.06, 0.0};
-            
-         case SPINE_ONE:
+         case TURN_LEFT:
+            return new double[]{0.1, -0.06, 0.0};
+         case TURN_RIGHT:
+            return new double[]{-0.1, -0.06, 0.0};
+         case LEAN_FORWARD_A_LOT:
             return new double[]{0.0, 0.1, 0.0};
-
-            
-         case SPINE_TWO:
+         case LEAN_BACKWARD_A_LOT:
+            return new double[]{0.0, -0.1, 0.0};
+         case LEAN_LEFT:
             return new double[]{0.0, -0.06, 0.1};
-            
-         case SPINE_THREE:
+         case LEAN_RIGHT:
             return new double[]{0.0, -0.06, -0.1};
-
 
          default:
             throw new RuntimeException("Shouldn't get here!");
