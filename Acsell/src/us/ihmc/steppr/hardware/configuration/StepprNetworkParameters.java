4882569c59c8851b5d6497bc52be7ad728b6c3f3
Jesper Smith <jsmith@ihmc.us>
2014-09-23 18:07:27 +0000

    DRC-1796: Wrote awesome publish script for Acsell using gradle!

--- Acsell/src/us/ihmc/steppr/hardware/configuration/StepprNetworkParameters.java
+++ Acsell/src/us/ihmc/steppr/hardware/configuration/StepprNetworkParameters.java
@@ -2,6 +2,6 @@ package us.ihmc.steppr.hardware.configuration;
 
 public class StepprNetworkParameters
 {
-   public static final String CONTROL_COMPUTER_HOST = "10.66.171.25";
+   public static final String CONTROL_COMPUTER_HOST = "localhost";
    public static final int VARIABLE_SERVER_PORT = 5555;
 }
