7df233d5e67ec186d97c29cef56255cb93d3a0c4
Jerry Pratt <jpratt@ihmc.us>
2014-09-24 16:42:42 +0000

    DRC-1782: A few more arm poses. Default to just arms for now...

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/HumanoidArmPose.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/HumanoidArmPose.java
@@ -3,7 +3,8 @@ package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories;
 public enum HumanoidArmPose
 {   
    STAND_PREP, SMALL_CHICKEN_WINGS, LARGE_CHICKEN_WINGS, STRAIGHTEN_ELBOWS, SUPPINATE_ARMS_IN_A_LITTLE, ARMS_BACK, LARGER_CHICKEN_WINGS,
-   ARMS_OUT_EXTENDED, SUPPINATE_ARMS_IN_MORE, SUPPINATE_ARMS_IN_A_LOT, SUPER_CHICKEN_WINGS, FLYING, FLYING_SUPPINATE_IN, FLYING_SUPPINATE_OUT;
+   ARMS_OUT_EXTENDED, SUPPINATE_ARMS_IN_MORE, SUPPINATE_ARMS_IN_A_LOT, SUPER_CHICKEN_WINGS, FLYING, FLYING_SUPPINATE_IN, FLYING_SUPPINATE_OUT,
+   REACH_BACK, REACH_WAY_BACK, ARMS_03, REACH_FORWARD, REACH_WAY_FORWARD;
  
    /**
     * Arm angles are as follows:
@@ -47,6 +48,17 @@ public enum HumanoidArmPose
             return new double[]{0.3, -0.4, 0.5, -1.7};
          case SUPER_CHICKEN_WINGS:
             return new double[]{0.3, -1.3, 0.05, -1.7};
+            
+         case REACH_BACK:
+            return new double[]{1.0, -0.4, 0.05, -1.7};
+         case REACH_WAY_BACK:
+            return new double[]{1.0, -0.4, 0.05, -0.4};
+         case ARMS_03:
+            return new double[]{0.3, -0.4, 0.3, -1.7};
+         case REACH_FORWARD:
+            return new double[]{-0.6, -0.4, 0.05, -1.7};
+         case REACH_WAY_FORWARD:
+            return new double[]{-0.6, -0.4, 0.05, -0.4};
             
          default:
             throw new RuntimeException("Shouldn't get here!");
