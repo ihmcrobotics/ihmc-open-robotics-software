b88a96f54a0b3760a6701d328d98ca8f011b0ab8
Jesper Smith <jsmith@ihmc.us>
2014-09-24 03:26:15 +0000

    DRC-1802: Finished up dashboard with logic/motor power commands. Implemented start streaming command in StepprSetup. does not fixbamboo

--- Acsell/src/us/ihmc/steppr/hardware/StepprSetup.java
+++ Acsell/src/us/ihmc/steppr/hardware/StepprSetup.java
@@ -9,9 +9,9 @@ import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.DatagramChannel;
-import java.nio.channels.MembershipKey;
 import java.util.EnumMap;
 
+import us.ihmc.commonWalkingControlModules.visualizer.RobotVisualizer;
 import us.ihmc.realtime.MonotonicTime;
 import us.ihmc.realtime.PeriodicParameters;
 import us.ihmc.realtime.PriorityParameters;
@@ -23,10 +23,21 @@ import us.ihmc.yoUtilities.dataStructure.variable.IntegerYoVariable;
 public class StepprSetup extends RealtimeThread
 {
    private static final int PARAMETER_INDEX_ENCODER_WRAP = 22;
+   private static final long POWER_BUS_ON_DELAY = 1000000000;
+   private static final long POWER_BUS_OFF_DELAY = 100000000;
 
    private enum NudgeState
    {
       IDLE, INCREASE, DECREASE
+   }
+   
+   private enum PowerState
+   {
+      IDLE,
+      DELAYED_RIGHT_LEG_OFF,
+      DELAYED_BACK_OFF,      
+      DELAYED_RIGHT_LEG_ON,
+      DELAYED_BACK_ON
    }
 
    private static final PriorityParameters priority = new PriorityParameters(50);
@@ -37,23 +48,37 @@ public class StepprSetup extends RealtimeThread
    private final EnumMap<StepprActuator, IntegerYoVariable> nudges = new EnumMap<>(StepprActuator.class);
    private final EnumMap<StepprActuator, NudgeState> nudgeStates = new EnumMap<>(StepprActuator.class);
    
+   private final IntegerYoVariable logicPowerStateRequest = new IntegerYoVariable("logicPowerStateRequest", registry);
+   private final IntegerYoVariable motorPowerStateRequest = new IntegerYoVariable("motorPowerStateRequest", registry);
+   
+   private long logicSwitchTime = -1;
+   private long motorSwitchTime = -1;
+   
+   private PowerState logicPowerState = PowerState.IDLE;
+   private PowerState motorPowerState = PowerState.IDLE;
+   
    private DatagramChannel channel;
    private ByteBuffer sendBuffer;
    private InetSocketAddress sendAddress;
+   private InetSocketAddress powerCommandAddress;
 
+   private final RobotVisualizer visualizer;
    
-   
-   public StepprSetup(YoVariableRegistry parentRegistry)
+   public StepprSetup(RobotVisualizer vizualizer)
    {
       super(priority, new PeriodicParameters(interval));
+      this.visualizer = vizualizer;
+      
       for (StepprActuator actuator : StepprActuator.values)
       {
          nudges.put(actuator, new IntegerYoVariable(actuator.getName() + "Nudge", registry));
          nudgeStates.put(actuator, NudgeState.IDLE);
       }
 
-      
-      parentRegistry.addChild(registry);
+      if(vizualizer != null)
+      {
+         vizualizer.addRegistry(registry, null);
+      }
    }
 
    @Override
@@ -66,6 +91,8 @@ public class StepprSetup extends RealtimeThread
 
          InetAddress group = InetAddress.getByName(StepprNetworkParameters.STEPPR_MULTICAST_GROUP);
          sendAddress = new InetSocketAddress(group, StepprNetworkParameters.UDP_MULTICAST_PARAMETER_REQUEST_PORT);
+         powerCommandAddress = new InetSocketAddress(group, StepprNetworkParameters.UDP_MULTICAST_POWER_BUS_PORT);
+         
          InetSocketAddress receiveAddress = new InetSocketAddress(StepprNetworkParameters.UDP_MULTICAST_PARAMETER_REPLY_PORT);
          channel = DatagramChannel.open(StandardProtocolFamily.INET).setOption(StandardSocketOptions.SO_REUSEADDR, true)
                .setOption(StandardSocketOptions.IP_MULTICAST_IF, iface).bind(receiveAddress);
@@ -114,7 +141,8 @@ public class StepprSetup extends RealtimeThread
                   requestParameterValue(actuator, PARAMETER_INDEX_ENCODER_WRAP);
                }
             }
-
+            handlePower();
+            visualizer.update(getCurrentMonotonicClockTime(), registry);
          }
       }
       catch (IOException e)
@@ -222,6 +250,196 @@ public class StepprSetup extends RealtimeThread
             }
          }
       }
+   }
+   
+   private void sendPowerState(int channel, int state) throws IOException   
+   {
+      sendBuffer.clear();
+      sendBuffer.put((byte) 0x01);
+      sendBuffer.put((byte) 0x01);
+      sendBuffer.put((byte) 0x08);
+      sendBuffer.put((byte) 0x07);
+      sendBuffer.put((byte) 0x42);
+      sendBuffer.put((byte) 0x03);
+      
+      byte id = (byte) 0x01;
+      byte channelB = (byte) channel;
+      byte stateB = (byte) state;
+      
+      short checksum = (short) (id & 0xFFFF);
+      checksum = (short) ((checksum  + (short) (channelB & 0xFFFF)) & 0xFFFF);
+      checksum = (short) ((checksum  + (short) (stateB & 0xFFFF)) & 0xFFFF);
+            
+      sendBuffer.put(id);
+      sendBuffer.put(channelB);
+      sendBuffer.put(stateB);
+      
+      // Screw you Morgan, curveball big-endian
+      sendBuffer.put((byte) (checksum >>> 8));
+      sendBuffer.put((byte) (checksum & 0xFF));
+      sendBuffer.flip();
+      this.channel.send(sendBuffer, powerCommandAddress);
+   }
+   
+   private boolean motorSwitchOnTime()
+   {
+      return getCurrentMonotonicClockTime() - motorSwitchTime > POWER_BUS_ON_DELAY;
+   }
+   
+   private boolean motorSwitchOffTime()
+   {
+      return getCurrentMonotonicClockTime() - motorSwitchTime > POWER_BUS_OFF_DELAY;
+   }
+   
+   private boolean logicSwitchOnTime()
+   {
+      return getCurrentMonotonicClockTime() - logicSwitchTime > POWER_BUS_ON_DELAY;
+   }
+   
+   private boolean logicSwitchOffTime()
+   {
+      return getCurrentMonotonicClockTime() - logicSwitchTime > POWER_BUS_OFF_DELAY;
+   }
+   
+   private void handlePower() throws IOException
+   {
+      switch(motorPowerState)
+      {
+      case IDLE:
+         if(motorPowerStateRequest.getIntegerValue() == 1)
+         {
+            motorPowerState = PowerState.DELAYED_RIGHT_LEG_ON;
+            sendPowerState(0, 2);
+            motorSwitchTime = getCurrentMonotonicClockTime();
+         }
+         else if (motorPowerStateRequest.getIntegerValue() == -1)
+         {
+            sendPowerState(0, 0);
+            motorSwitchTime = getCurrentMonotonicClockTime();
+            motorPowerState = PowerState.DELAYED_RIGHT_LEG_OFF;
+         }
+         
+         break;
+      case DELAYED_RIGHT_LEG_ON:
+         if(motorSwitchOnTime())
+         {
+            sendPowerState(1, 2);
+            motorSwitchTime = getCurrentMonotonicClockTime();
+            motorPowerState = PowerState.DELAYED_BACK_ON;
+         }
+         break;
+      case DELAYED_BACK_ON:
+         if(motorSwitchOnTime())
+         {
+           sendPowerState(2, 2);
+           motorSwitchTime = getCurrentMonotonicClockTime();
+           motorPowerState = PowerState.IDLE;
+         }
+         break;
+      case DELAYED_RIGHT_LEG_OFF:
+         if(motorSwitchOffTime())
+         {
+            sendPowerState(1, 0);
+            motorSwitchTime = getCurrentMonotonicClockTime();
+            motorPowerState = PowerState.DELAYED_BACK_OFF;
+         }
+         break;
+      case DELAYED_BACK_OFF:
+         if(motorSwitchOffTime())
+         {
+            sendPowerState(2, 0);
+            motorSwitchTime = getCurrentMonotonicClockTime();
+            motorPowerState = PowerState.IDLE;
+         }
+         break;
+      }
+      
+      switch(logicPowerState)
+      {
+      case IDLE:
+         if(logicPowerStateRequest.getIntegerValue() == 1)
+         {
+            logicPowerState = PowerState.DELAYED_RIGHT_LEG_ON;
+            sendPowerState(6, 2);
+            logicSwitchTime = getCurrentMonotonicClockTime();
+         }
+         else if (logicPowerStateRequest.getIntegerValue() == -1)
+         {
+            sendPowerState(6, 0);
+            logicSwitchTime = getCurrentMonotonicClockTime();
+            logicPowerState = PowerState.DELAYED_RIGHT_LEG_OFF;
+         }
+         
+         break;
+      case DELAYED_RIGHT_LEG_ON:
+         if(logicSwitchOnTime())
+         {
+            sendPowerState(7, 2);
+            logicSwitchTime = getCurrentMonotonicClockTime();
+            logicPowerState = PowerState.DELAYED_BACK_ON;
+         }
+         break;
+      case DELAYED_BACK_ON:
+         if(logicSwitchOnTime())
+         {
+            sendPowerState(8, 2);
+            logicSwitchTime = getCurrentMonotonicClockTime();
+            logicPowerState = PowerState.IDLE;
+         }
+         break;
+      case DELAYED_RIGHT_LEG_OFF:
+         if(logicSwitchOffTime())
+         {
+            sendPowerState(7, 0);
+            logicSwitchTime = getCurrentMonotonicClockTime();
+            logicPowerState = PowerState.DELAYED_BACK_OFF;
+         }
+         break;
+      case DELAYED_BACK_OFF:
+         if(logicSwitchOffTime())
+         {
+            sendPowerState(8, 0);
+            logicSwitchTime = getCurrentMonotonicClockTime();
+            logicPowerState = PowerState.IDLE;
+         }
+         break;
+      }
+      
+      
+      
+      motorPowerStateRequest.set(0);
+      logicPowerStateRequest.set(0);
+   }
+   
+   public static void startStreamingData()
+   {
+      try
+      {
+         NetworkInterface iface = NetworkInterface.getByInetAddress(InetAddress.getByName(StepprNetworkParameters.CONTROL_COMPUTER_HOST));
+         InetAddress group = InetAddress.getByName(StepprNetworkParameters.STEPPR_MULTICAST_GROUP);
+         InetSocketAddress streamAddress = new InetSocketAddress(group, StepprNetworkParameters.UDP_MULTICAST_STREAM_COMMAND_PORT);
+         
+        
+         DatagramChannel channel = DatagramChannel.open(StandardProtocolFamily.INET).setOption(StandardSocketOptions.SO_REUSEADDR, true)
+               .setOption(StandardSocketOptions.IP_MULTICAST_IF, iface);
+         channel.join(group, iface);
+   
+         ByteBuffer command = ByteBuffer.allocate(5);
+         command.put((byte) 1);
+         command.put((byte) 0);
+         command.put((byte) 1);
+         command.put((byte) 1);
+         command.put((byte) 0);
+         command.flip();
+         channel.send(command, streamAddress);
+         channel.close();
+         
+      }
+      catch(IOException e)
+      {
+         throw new RuntimeException(e);
+      }
+
    }
 
 }
