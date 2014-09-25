2128f9ccb712d804be8f03c132c97bd0f445b924
Douglas Stephen <dstephen@ihmc.us>
2014-09-25 21:47:28 +0000

    NONE: Checking in USE_BEHAVIORS_MANAGER as false by default, since it causes problems with the real robot so this should be the default. Working towards fixbamboo

--- IHMCCommunication/src/us/ihmc/communication/util/NetworkConfigParameters.java
+++ IHMCCommunication/src/us/ihmc/communication/util/NetworkConfigParameters.java
@@ -2,7 +2,7 @@ package us.ihmc.communication.util;
 
 public class NetworkConfigParameters
 {
-   public static final boolean USE_BEHAVIORS_MODULE = true;
+   public static final boolean USE_BEHAVIORS_MODULE = false;
    
    public static final int NETWORK_PROCESSOR_TO_CONTROLLER_TCP_PORT = 4895;
    public static final int NETWORK_PROCESSOR_TO_UI_TCP_PORT = 4897;
