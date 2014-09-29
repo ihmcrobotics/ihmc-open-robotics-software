b6eb6b03025fa5c8c14de9e5af57e2d23e7d0774
Sylvain Bertrand <sbertrand@ihmc.us>
2014-09-29 17:22:15 +0000

    NONE: New behavior architecture, the previous one is still working. Still need some cleanup.

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/workingArea/IHMCHumanoidBehaviorManager.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/workingArea/IHMCHumanoidBehaviorManager.java
@@ -1,20 +1,60 @@
 package us.ihmc.humanoidBehaviors.workingArea;
 
+import us.ihmc.communication.packets.behaviors.HumanoidBehaviorControlModePacket;
+import us.ihmc.communication.packets.behaviors.HumanoidBehaviorType;
 import us.ihmc.communication.packets.behaviors.HumanoidBehaviorTypePacket;
+import us.ihmc.communication.packets.dataobjects.RobotConfigurationData;
+import us.ihmc.communication.subscribers.RobotDataReceiver;
+import us.ihmc.humanoidBehaviors.workingArea.behaviors.SimpleDoNothingBehavior;
+import us.ihmc.humanoidBehaviors.workingArea.behaviors.SimpleForwardingBehavior;
+import us.ihmc.humanoidBehaviors.workingArea.behaviors.scripts.ScriptBehavior;
 import us.ihmc.humanoidBehaviors.workingArea.communication.BehaviorCommunicationBridge;
+import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 import us.ihmc.utilities.net.ObjectCommunicator;
 import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
 
-public class IHMCHumanoidBehaviorManager {
-	private final BehaviorCommunicationBridge communicationBridge;
-	private final BehaviorDisptacher dispatcher;
-	private final BehaviorFactory behaviorFactory;
-	
-	public IHMCHumanoidBehaviorManager(ObjectCommunicator networkProcessorCommunicator, ObjectCommunicator controllerCommunicator, YoVariableRegistry parentRegistry)
-	{
-		communicationBridge = new BehaviorCommunicationBridge(networkProcessorCommunicator, controllerCommunicator, parentRegistry);
-		dispatcher = new BehaviorDisptacher(communicationBridge, parentRegistry);
-		behaviorFactory = new BehaviorFactory(dispatcher, communicationBridge, parentRegistry);
-		networkProcessorCommunicator.attachListener(HumanoidBehaviorTypePacket.class, behaviorFactory);
-	}
+public class IHMCHumanoidBehaviorManager
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+   private final DoubleYoVariable yoTime = new DoubleYoVariable("yoTime", registry);
+
+   public IHMCHumanoidBehaviorManager(FullRobotModel fullRobotModel, ObjectCommunicator networkProcessorCommunicator, ObjectCommunicator controllerCommunicator)
+   {
+      BehaviorCommunicationBridge communicationBridge = new BehaviorCommunicationBridge(networkProcessorCommunicator, controllerCommunicator, registry);
+
+      RobotDataReceiver robotDataReceiver = new RobotDataReceiver(fullRobotModel);
+      HumanoidBehaviorDispatcherControlSubscriber desiredBehaviorControlSubscriber = new HumanoidBehaviorDispatcherControlSubscriber();
+      HumanoidBehaviorTypeSubscriber desiredBehaviorSubscriber = new HumanoidBehaviorTypeSubscriber();
+
+      BehaviorDisptacher dispatcher = createBehaviorDispatcher(fullRobotModel, communicationBridge, robotDataReceiver, desiredBehaviorControlSubscriber,
+            desiredBehaviorSubscriber);
+
+      controllerCommunicator.attachListener(RobotConfigurationData.class, robotDataReceiver);
+      networkProcessorCommunicator.attachListener(HumanoidBehaviorControlModePacket.class, desiredBehaviorControlSubscriber);
+      networkProcessorCommunicator.attachListener(HumanoidBehaviorTypePacket.class, desiredBehaviorSubscriber);
+
+      Thread dispatcherThread = new Thread(dispatcher, "BehaviorDispatcher");
+      dispatcherThread.start();
+   }
+
+   private BehaviorDisptacher createBehaviorDispatcher(FullRobotModel fullRobotModel, BehaviorCommunicationBridge communicationBridge,
+         RobotDataReceiver robotDataReceiver, HumanoidBehaviorDispatcherControlSubscriber desiredBehaviorControlSubscriber,
+         HumanoidBehaviorTypeSubscriber desiredBehaviorSubscriber)
+   {
+      BehaviorDisptacher dispatcher = new BehaviorDisptacher(yoTime, fullRobotModel, robotDataReceiver, desiredBehaviorControlSubscriber, desiredBehaviorSubscriber,
+            communicationBridge, registry);
+
+      SimpleForwardingBehavior simpleForwardingBehavior = new SimpleForwardingBehavior(communicationBridge);
+      simpleForwardingBehavior.attachCommunicationBridge(communicationBridge);
+
+      ScriptBehavior scriptBehavior = new ScriptBehavior(communicationBridge);
+      scriptBehavior.setFullRobotModel(fullRobotModel);
+
+      dispatcher.addHumanoidBehavior(HumanoidBehaviorType.STOP, simpleForwardingBehavior);
+      dispatcher.addHumanoidBehavior(HumanoidBehaviorType.DO_NOTHING, new SimpleDoNothingBehavior(communicationBridge));
+      dispatcher.addHumanoidBehavior(HumanoidBehaviorType.SCRIPT, scriptBehavior);
+
+      return dispatcher;
+   }
 }
