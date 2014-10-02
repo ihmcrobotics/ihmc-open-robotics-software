50f15730cfacde76199a14a575601400a75d1a98
Matthijs Damen <mdamen@ihmc.us>
2014-10-02 14:03:46 +0000

    DRC-1742: Unit test for snap footsteps. Won't fixBamboo

---
+++ Atlas/test/us/ihmc/atlas/ObstacleCourseTests/AtlasObstacleCourseRampSnapFootstepTest.java
@@ -0,0 +1,25 @@
+package us.ihmc.atlas.ObstacleCourseTests;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.bambooTools.BambooTools;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCObstacleCourseRampSnapFootstepTest;
+
+public class AtlasObstacleCourseRampSnapFootstepTest extends DRCObstacleCourseRampSnapFootstepTest
+{
+   private DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.DRC_NO_HANDS, false, false);
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+
+}
