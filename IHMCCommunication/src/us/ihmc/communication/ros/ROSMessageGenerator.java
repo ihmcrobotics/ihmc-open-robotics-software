bfb13f512231ace0e44a82dfa5a423dd26466290
Alex Graber-Tilton <agrabertilton@ihmc.us>
2014-10-03 00:15:08 +0000

    GFE-43: Updated the ROSMessageGenerater to handle more conversions, like a single layer List. No feasable way to handle multidimension lists though multidimension arrays should be fine.

--- IHMCCommunication/src/us/ihmc/communication/ros/ROSMessageGenerator.java
+++ IHMCCommunication/src/us/ihmc/communication/ros/ROSMessageGenerator.java
@@ -1,139 +1,173 @@
 package us.ihmc.communication.ros;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-import javax.swing.JFileChooser;
 import javax.vecmath.Point3d;
 import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
 
+import us.ihmc.communication.packets.dataobjects.RobotConfigurationData;
 import us.ihmc.communication.packets.manipulation.HandPosePacket;
+import us.ihmc.communication.packets.sensing.LookAtPacket;
+import us.ihmc.communication.packets.walking.ChestOrientationPacket;
+import us.ihmc.communication.packets.walking.ComHeightPacket;
+import us.ihmc.communication.packets.walking.FootPosePacket;
+import us.ihmc.communication.packets.walking.FootStatePacket;
+import us.ihmc.communication.packets.walking.FootstepData;
+import us.ihmc.communication.packets.walking.FootstepDataList;
+import us.ihmc.communication.packets.walking.FootstepStatus;
 import us.ihmc.communication.packets.walking.HeadOrientationPacket;
 import us.ihmc.communication.packets.walking.PauseCommand;
+import us.ihmc.communication.packets.walking.PelvisPosePacket;
 
 public class ROSMessageGenerator
 {
 
-   private String messageFolder;
-   boolean overwriteSubMessages;
+	private String messageFolder;
+	boolean overwriteSubMessages;
 
-   public ROSMessageGenerator(String messageFolder, Boolean overwriteSubMessages)
-   {
-      this.messageFolder = messageFolder;
-   }
+	public ROSMessageGenerator(String messageFolder, Boolean overwriteSubMessages)
+	{
+		this.messageFolder = messageFolder;
+		this.overwriteSubMessages = overwriteSubMessages;
+	}
 
-   public static void main(String... args)
-   {
-      List<Class> toConvert = new ArrayList<Class>();
-      toConvert.add(PauseCommand.class);
-      toConvert.add(HeadOrientationPacket.class);
-      toConvert.add(HandPosePacket.class);
+	public static void main(String... args)
+	{
+		List<Class> toConvert = new ArrayList<Class>();
+		toConvert.add(RobotConfigurationData.class);
+		toConvert.add(HandPosePacket.class);
+		toConvert.add(ComHeightPacket.class);
+		toConvert.add(FootPosePacket.class);
+		toConvert.add(FootStatePacket.class);
+		//toConvert.add(RobotPoseData.class); //RigidBodyTranspose conversion required
+		toConvert.add(FootstepData.class);
+		toConvert.add(FootstepDataList.class);
+		toConvert.add(FootstepStatus.class);
+		toConvert.add(ChestOrientationPacket.class);
+		toConvert.add(LookAtPacket.class);
+		toConvert.add(HeadOrientationPacket.class);
+		toConvert.add(PauseCommand.class);
+		toConvert.add(PelvisPosePacket.class);
 
-      
-      String destination = "us/ihmc/communication/msgs/";
-      String messageFolder = ("generated/" + destination).replace("/", File.separator);
-      File file = new File(messageFolder);
-      if (!file.exists())
-      {
-         file.mkdirs();
-      }
-      
-      System.out.println(messageFolder);
-      System.out.println();
-      ROSMessageGenerator messageGenerator = new ROSMessageGenerator(messageFolder, false);
-      for (Class clazz : toConvert)
-      {
-         messageGenerator.createNewRosMessage(clazz, true);
-      }
-   }
 
-   public String createNewRosMessage(Class clazz, boolean overwrite)
-   {
-      if (clazz == null)
-      {
-         return "";
-      }
-      String messageName = clazz.getSimpleName() + ".msg";
-      File messageFile = new File(messageFolder + "\\" + messageName);
 
-      if (overwrite || !messageFile.exists())
-      {
-         messageFile.delete();
-         try
-         {
-            messageFile.createNewFile();
-            System.out.println("Message Created: " + messageFile.getName());
-            PrintStream fileStream = new PrintStream(messageFile);
-            
-            String outBuffer = "// " + messageName + System.lineSeparator() + System.lineSeparator();
 
-            Field[] fields = clazz.getFields();
-            for (Field field : fields)
-            {
-               outBuffer += printType(field.getType());
-               outBuffer += " " + field.getName() + System.lineSeparator();
-            }
-            fileStream.println(outBuffer);
-         }
-         catch (IOException e)
-         {
-            e.printStackTrace();
-         }
-      }
-      return messageName;
-   }
+		String destination = "us/ihmc/communication/msgs/";
+		String messageFolder = ("generated/" + destination).replace("/", File.separator);
+		File file = new File(messageFolder);
+		if (!file.exists())
+		{
+			file.mkdirs();
+		}
 
-   private String printType(Class clazz)
-   {
-      String buffer = "";
-      if (clazz == null)
-      {
-         return buffer;
-      }
+		ROSMessageGenerator messageGenerator = new ROSMessageGenerator(messageFolder, false);
+		for (Class clazz : toConvert)
+		{
+			messageGenerator.createNewRosMessage(clazz, true);
+		}
+	}
 
-      if (clazz.isArray())
-      {
-         buffer += printType(clazz.getComponentType());
-         buffer += "[]";
-      }
-      else if (clazz.equals(boolean.class))
-      {
-         buffer += "bool";
-      }
-      else if (clazz.equals(double.class))
-      {
-         buffer += "float64";
-      }
-      else if (clazz.equals(Quat4d.class))
-      {
-         buffer += "geometry_msgs/Quaternion";
-      }
-      else if (clazz.equals(Point3d.class))
-      {
-         buffer += "geometry_msgs/Vector3";
-      }
-      else if (clazz.isEnum())
-      {
-         Object[] enumList = clazz.getEnumConstants();
-         for (int i = 0; i < enumList.length; i++)
-         {
-            buffer += "uint8";
-            buffer += " " + enumList[i];
-            buffer += " = " + i + System.lineSeparator();
-         }
-         buffer += "uint8";
-      }
-      else
-      {
-         buffer += createNewRosMessage(clazz, overwriteSubMessages);
-      }
-      return buffer;
-   }
+	public String createNewRosMessage(Class clazz, boolean overwrite)
+	{
+		if (clazz == null)
+		{
+			return "";
+		}
+		String messageName = clazz.getSimpleName();
+		File messageFile = new File(messageFolder + "\\" + messageName + ".msg");
+
+		if (overwrite || !messageFile.exists())
+		{
+			messageFile.delete();
+			try
+			{
+				messageFile.createNewFile();
+				System.out.println("Message Created: " + messageFile.getName());
+				PrintStream fileStream = new PrintStream(messageFile);
+
+				String outBuffer = "// " + messageName + System.lineSeparator() + System.lineSeparator();
+
+				Field[] fields = clazz.getFields();
+				for (Field field : fields)
+				{
+					outBuffer += printType(field);
+					outBuffer += " " + field.getName() + System.lineSeparator();
+				}
+				fileStream.println(outBuffer);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return messageName;
+	}
+	
+	private String printType(Field field){
+		String buffer = "";
+		if (List.class.isAssignableFrom(field.getType())){
+			Class genericType = ((Class) ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0]);
+			buffer += printType(genericType);
+			buffer += "[]";
+		}
+		else{
+			buffer += printType(field.getType());
+		}
+		
+		return buffer;
+	}
+
+	private String printType(Class clazz)
+	{
+		String buffer = "";
+		if (clazz == null)
+		{
+			return buffer;
+		}
+
+		if (clazz.isArray())
+		{
+			buffer += printType(clazz.getComponentType());
+			buffer += "[]";
+		}      
+		else if (clazz.isEnum())
+		{
+			Object[] enumList = clazz.getEnumConstants();
+			for (int i = 0; i < enumList.length; i++)
+			{
+				buffer += "uint8";
+				buffer += " " + enumList[i];
+				buffer += " = " + i + System.lineSeparator();
+			}
+			buffer += "uint8";
+		}
+		else if (clazz.equals(byte.class)){ buffer += "int8";}
+		else if (clazz.equals(short.class)){ buffer += "int16";}
+		else if (clazz.equals(int.class)){ buffer += "int32";	}
+		else if (clazz.equals(long.class)){	buffer += "int64";	}
+		else if (clazz.equals(float.class)){	buffer += "float32";	}
+		else if (clazz.equals(double.class)){	buffer += "float64";}
+		else if (clazz.equals(boolean.class)){ buffer += "bool";}
+		else if (clazz.equals(char.class)){ buffer += "uint8";}
+		else if (clazz.equals(String.class)){ buffer += "string";}
+		
+		else if (clazz.equals(Quat4d.class)){buffer += "geometry_msgs/Quaternion";}
+		else if (clazz.equals(Point3d.class) || (clazz.equals(Vector3d.class))){buffer += "geometry_msgs/Vector3";}
+		else{
+			buffer += createNewRosMessage(clazz, overwriteSubMessages);
+		}
+		return buffer;
+	}
 }
 
 
