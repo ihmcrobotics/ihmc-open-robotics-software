9de1b6995ebe017e6f86334963b2993e157e52af
Sylvain Bertrand <sbertrand@ihmc.us>
2014-10-08 22:53:11 +0000

    NONE: Fixed gains for support foot. Won't fixbamboo.

--- Atlas/src/us/ihmc/atlas/parameters/AtlasWalkingControllerParameters.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasWalkingControllerParameters.java
@@ -567,7 +567,7 @@ public class AtlasWalkingControllerParameters implements WalkingControllerParame
       double maxAngularAcceleration = runningOnRealRobot ? 100.0 : Double.POSITIVE_INFINITY;
       double maxAngularJerk = runningOnRealRobot ? 1500.0 : Double.POSITIVE_INFINITY;
       
-      gains.setPositionDerivativeGains(20.0, 0.0, 0.0);
+      gains.setOrientationDerivativeGains(20.0, 0.0, 0.0);
       gains.setOrientationMaxAccelerationAndJerk(maxAngularAcceleration, maxAngularJerk);
 
       return gains;
