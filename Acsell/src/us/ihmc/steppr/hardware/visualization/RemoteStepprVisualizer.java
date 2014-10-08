2c6a7263c7331efc389f0d973bb5724fef5197cb
Duncan Calvert <dcalvert@ihmc.us>
2014-10-08 20:02:24 +0000

    DRC-1835: Renamed SCSYoVariablesUpdatedListener to SCSVisualizer. Created SCSVisualizerStateListener to remove AtlasSCSYoVariablesUpdatedListener and ValkyrieSCSYoVariablesUpdatedListener. Cleaned up RemoteValkyrieVisualizer to remove duplicate code.

--- Acsell/src/us/ihmc/steppr/hardware/visualization/RemoteStepprVisualizer.java
+++ Acsell/src/us/ihmc/steppr/hardware/visualization/RemoteStepprVisualizer.java
@@ -3,13 +3,13 @@ package us.ihmc.steppr.hardware.visualization;
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.acsell.parameters.BonoRobotModel;
 import us.ihmc.robotDataCommunication.YoVariableClient;
-import us.ihmc.robotDataCommunication.visualizer.SCSYoVariablesUpdatedListener;
+import us.ihmc.robotDataCommunication.visualizer.SCSVisualizer;
 import us.ihmc.steppr.hardware.StepprDashboard;
 import us.ihmc.steppr.hardware.configuration.StepprNetworkParameters;
 
 import com.yobotics.simulationconstructionset.Robot;
 
-public class RemoteStepprVisualizer extends SCSYoVariablesUpdatedListener
+public class RemoteStepprVisualizer extends SCSVisualizer
 {
    public RemoteStepprVisualizer(Robot robot, int bufferSize)
    {
@@ -30,7 +30,7 @@ public class RemoteStepprVisualizer extends SCSYoVariablesUpdatedListener
       BonoRobotModel robotModel = new BonoRobotModel(true, false);
       SDFRobot robot = robotModel.createSdfRobot(false);
 
-      SCSYoVariablesUpdatedListener scsYoVariablesUpdatedListener = new RemoteStepprVisualizer(robot, 16384);
+      SCSVisualizer scsYoVariablesUpdatedListener = new RemoteStepprVisualizer(robot, 16384);
 
     
       
