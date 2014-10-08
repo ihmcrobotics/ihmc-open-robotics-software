5f1b8184bf3b0ac5279eb1aff66743d1c6c4723f
Sylvain Bertrand <sbertrand@ihmc.us>
2014-10-08 19:12:06 +0000

    DRC-1873: Added the foot sensors in the diagnostic controller. Won't fixbamboo

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/FootSwitchInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/FootSwitchInterface.java
@@ -1,14 +1,20 @@
 package us.ihmc.commonWalkingControlModules.sensors;
 
 import us.ihmc.utilities.math.geometry.FramePoint2d;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.screwTheory.Wrench;
 
-
+// TODO Nuke that interface
 public interface FootSwitchInterface
 {
    public abstract boolean hasFootHitGround();
    
    public abstract double computeFootLoadPercentage();
    public abstract void computeAndPackCoP(FramePoint2d copToPack);
-   
+
+   public abstract void computeAndPackFootWrench(Wrench footWrenchToPack);
+
+   public abstract ReferenceFrame getMeasurementFrame();
+
    public void reset();
 }
