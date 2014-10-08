f36397b10769d92abf88ddd2dd48152e120be8a3
Duncan Calvert <dcalvert@ihmc.us>
2014-10-08 21:53:05 +0000

    DRC-1835: Hooked a new SCSVisualizer to the BehaviorManager. Won't fixBamboo.

---
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/BehaviorVisualizer.java
@@ -0,0 +1,19 @@
+package us.ihmc.humanoidBehaviors;
+
+import us.ihmc.robotDataCommunication.YoVariableClient;
+import us.ihmc.robotDataCommunication.visualizer.SCSVisualizer;
+
+import com.yobotics.simulationconstructionset.Robot;
+
+public class BehaviorVisualizer extends SCSVisualizer
+{
+   private final boolean showOverheadView = true;
+   
+   public BehaviorVisualizer(String host, int bufferSize, Robot robot)
+   {
+      super(robot, bufferSize);
+      
+      YoVariableClient client = new YoVariableClient(host, IHMCHumanoidBehaviorManager.BEHAVIOR_YO_VARIABLE_SERVER_PORT, this, "behavior", showOverheadView);
+      client.start();
+   }
+}
