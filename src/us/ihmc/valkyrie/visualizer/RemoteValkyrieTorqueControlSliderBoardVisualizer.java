d29665cf3ba762c17a2fc709e1b582102e83253d
Duncan Calvert <dcalvert@ihmc.us>
2014-10-08 20:02:24 +0000

    DRC-1835: Renamed SCSYoVariablesUpdatedListener to SCSVisualizer. Created SCSVisualizerStateListener to remove AtlasSCSYoVariablesUpdatedListener and ValkyrieSCSYoVariablesUpdatedListener. Cleaned up RemoteValkyrieVisualizer to remove duplicate code.

--- src/us/ihmc/valkyrie/visualizer/RemoteValkyrieTorqueControlSliderBoardVisualizer.java
+++ src/us/ihmc/valkyrie/visualizer/RemoteValkyrieTorqueControlSliderBoardVisualizer.java
@@ -1,61 +1,16 @@
 package us.ihmc.valkyrie.visualizer;
 
-import us.ihmc.robotDataCommunication.YoVariableClient;
-import us.ihmc.robotDataCommunication.visualizer.SCSYoVariablesUpdatedListener;
-import us.ihmc.valkyrie.ValkyrieRobotModel;
-import us.ihmc.valkyrie.configuration.ValkyrieNetworkParameters;
 import us.ihmc.valkyrie.controllers.ValkyrieSliderBoard.ValkyrieSliderBoardType;
-
-import com.martiansoftware.jsap.FlaggedOption;
-import com.martiansoftware.jsap.JSAP;
-import com.martiansoftware.jsap.JSAPException;
-import com.martiansoftware.jsap.JSAPResult;
 
 public class RemoteValkyrieTorqueControlSliderBoardVisualizer
 {
-   public static final String defaultHost = ValkyrieNetworkParameters.CONTROL_COMPUTER_HOST;
-   public static final int defaultPort = ValkyrieNetworkParameters.VARIABLE_SERVER_PORT;
-
-   public RemoteValkyrieTorqueControlSliderBoardVisualizer(String host, int port, int bufferSize)
+   public RemoteValkyrieTorqueControlSliderBoardVisualizer(String[] networkArguments)
    {
-      System.out.println("Connecting to host " + host);
-      ValkyrieRobotModel robotModel = new ValkyrieRobotModel(true, false);
-
-      SCSYoVariablesUpdatedListener scsYoVariablesUpdatedListener = new ValkyrieSCSYoVariablesUpdatedListener(robotModel, bufferSize, ValkyrieSliderBoardType.TORQUE_PD_CONTROL);
-
-      YoVariableClient client = new YoVariableClient(host, port, scsYoVariablesUpdatedListener, "remote", false);
-      client.start();
+      new RemoteValkyrieVisualizer(networkArguments, ValkyrieSliderBoardType.TORQUE_PD_CONTROL);
    }
-
-   public static void main(String[] args) throws JSAPException
+   
+   public static void main(String[] args)
    {
-      int bufferSize = 16384;
-      JSAP jsap = new JSAP();
-
-      FlaggedOption hostOption =
-         new FlaggedOption("host").setStringParser(JSAP.STRING_PARSER).setRequired(false).setLongFlag("host").setShortFlag('L').setDefault(defaultHost);
-      FlaggedOption portOption = new FlaggedOption("port").setStringParser(JSAP.INTEGER_PARSER).setRequired(false).setLongFlag("port").setShortFlag(
-                                     'p').setDefault(String.valueOf(defaultPort));
-
-      jsap.registerParameter(hostOption);
-      jsap.registerParameter(portOption);
-
-      JSAPResult config = jsap.parse(args);
-
-      if (config.success())
-      {
-         String host = config.getString("host");
-         int port = config.getInt("port");
-
-         new RemoteValkyrieTorqueControlSliderBoardVisualizer(host, port, bufferSize);
-      }
-      else
-      {
-         System.err.println();
-         System.err.println("Usage: java " + RemoteValkyrieTorqueControlSliderBoardVisualizer.class.getName());
-         System.err.println("                " + jsap.getUsage());
-         System.err.println();
-         System.exit(1);
-      }
+      new RemoteValkyrieTorqueControlSliderBoardVisualizer(args);
    }
 }
--- src/us/ihmc/valkyrie/visualizer/RemoteValkyrieVisualizer.java
+++ src/us/ihmc/valkyrie/visualizer/RemoteValkyrieVisualizer.java
@@ -1,8 +1,16 @@
 package us.ihmc.valkyrie.visualizer;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 import us.ihmc.robotDataCommunication.YoVariableClient;
+import us.ihmc.robotDataCommunication.visualizer.SCSVisualizer;
+import us.ihmc.robotDataCommunication.visualizer.SCSVisualizerStateListener;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 import us.ihmc.valkyrie.configuration.ValkyrieNetworkParameters;
+import us.ihmc.valkyrie.controllers.ValkyrieSliderBoard;
 import us.ihmc.valkyrie.controllers.ValkyrieSliderBoard.ValkyrieSliderBoardType;
 
 import com.martiansoftware.jsap.FlaggedOption;
@@ -10,29 +18,40 @@ import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.JSAPResult;
 
-public class RemoteValkyrieVisualizer
+public class RemoteValkyrieVisualizer implements SCSVisualizerStateListener
 {
+   public static final int BUFFER_SIZE = 16384;
    public static final String defaultHost = ValkyrieNetworkParameters.CONTROL_COMPUTER_HOST;
    public static final int defaultPort = ValkyrieNetworkParameters.VARIABLE_SERVER_PORT;
+   
+   private ValkyrieSliderBoardType valkyrieSliderBoardType;
+   
+   private SCSVisualizer scsVisualizer;
+   private ValkyrieRobotModel valkyrieRobotModel;
+   private String host;
+   private int port;
 
-   public RemoteValkyrieVisualizer(String host, int port, int bufferSize)
+   public RemoteValkyrieVisualizer(String[] networkArguments, ValkyrieSliderBoardType valkyrieSliderBoardType)
    {
+      this.valkyrieSliderBoardType = valkyrieSliderBoardType;
+      
+      parseNetworkArguments(networkArguments);
+      
       System.out.println("Connecting to host " + host);
-      ValkyrieRobotModel robotModel = new ValkyrieRobotModel(true, false);
+      valkyrieRobotModel = new ValkyrieRobotModel(true, false);
 
-      ValkyrieSCSYoVariablesUpdatedListener scsYoVariablesUpdatedListener = new ValkyrieSCSYoVariablesUpdatedListener(robotModel, bufferSize,
-            ValkyrieSliderBoardType.WALKING);
+      scsVisualizer = new SCSVisualizer(valkyrieRobotModel.createSdfRobot(false), BUFFER_SIZE);
+      scsVisualizer.addSCSVisualizerStateListener(this);
 
       int numberOfTicksBeforeUpdatingGraphs = 30;
-      scsYoVariablesUpdatedListener.updateGraphsLessFrequently(true, numberOfTicksBeforeUpdatingGraphs);
+      scsVisualizer.updateGraphsLessFrequently(true, numberOfTicksBeforeUpdatingGraphs);
 
-      YoVariableClient client = new YoVariableClient(host, port, scsYoVariablesUpdatedListener, "remote", false);
+      YoVariableClient client = new YoVariableClient(host, port, scsVisualizer, "remote", false);
       client.start();
    }
-
-   public static void main(String[] args) throws JSAPException
+   
+   public void parseNetworkArguments(String[] networkArguments)
    {
-      int bufferSize = 16384;
       JSAP jsap = new JSAP();
 
       FlaggedOption hostOption = new FlaggedOption("host").setStringParser(JSAP.STRING_PARSER).setRequired(false).setLongFlag("host").setShortFlag('L')
@@ -40,19 +59,19 @@ public class RemoteValkyrieVisualizer
       FlaggedOption portOption = new FlaggedOption("port").setStringParser(JSAP.INTEGER_PARSER).setRequired(false).setLongFlag("port").setShortFlag('p')
             .setDefault(String.valueOf(defaultPort));
 
-      jsap.registerParameter(hostOption);
-      jsap.registerParameter(portOption);
-
-      JSAPResult config = jsap.parse(args);
-
-      if (config.success())
+      try
       {
-         String host = config.getString("host");
-         int port = config.getInt("port");
-
-         new RemoteValkyrieVisualizer(host, port, bufferSize);
+         jsap.registerParameter(hostOption);
+         jsap.registerParameter(portOption);
       }
-      else
+      catch (JSAPException e)
+      {
+         e.printStackTrace();
+      }
+
+      JSAPResult config = jsap.parse(networkArguments);
+
+      if (!config.success())
       {
          System.err.println();
          System.err.println("Usage: java " + RemoteValkyrieVisualizer.class.getName());
@@ -60,5 +79,52 @@ public class RemoteValkyrieVisualizer
          System.err.println();
          System.exit(1);
       }
+      
+      host = config.getString("host");
+      port = config.getInt("port");
+   }
+
+   @Override
+   public void starting()
+   {
+      RobonetRegisterPanel registerPanel = new RobonetRegisterPanel(scsVisualizer.getRegistry());
+      scsVisualizer.getSCS().addExtraJpanel(registerPanel, "Registers");
+      scsVisualizer.getSCS().attachPlaybackListener(registerPanel);
+
+      RobonetRegisterModifierPanel modifierPanel = new RobonetRegisterModifierPanel(scsVisualizer.getRegistry());
+      scsVisualizer.getSCS().addExtraJpanel(modifierPanel, "Change Control Modes");
+      scsVisualizer.getSCS().attachPlaybackListener(modifierPanel);
+
+      JButton showRegisterViewer = new JButton("Show registers");
+      showRegisterViewer.addActionListener(new ActionListener()
+      {
+
+         @Override
+         public void actionPerformed(ActionEvent e)
+         {
+            scsVisualizer.getSCS().getStandardSimulationGUI().selectPanel("Registers");
+         }
+      });
+
+      JButton showControlModePanel = new JButton("Change Control Modes");
+      showControlModePanel.addActionListener(new ActionListener()
+      {
+
+         @Override
+         public void actionPerformed(ActionEvent e)
+         {
+            scsVisualizer.getSCS().getStandardSimulationGUI().selectPanel("Change Control Modes");
+         }
+      });
+
+      scsVisualizer.getSCS().addButton(showRegisterViewer);
+      scsVisualizer.getSCS().addButton(showControlModePanel);
+      
+      new ValkyrieSliderBoard(scsVisualizer.getSCS(), scsVisualizer.getRegistry(), valkyrieRobotModel, valkyrieSliderBoardType);
+   }
+   
+   public static void main(String[] args)
+   {
+      new RemoteValkyrieWalkingVisualizer(args);
    }
 }
