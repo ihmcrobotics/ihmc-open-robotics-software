13b46b78c0c0cea11a87f62eb8f31d2e93bc5b13
Alex Graber-Tilton <agrabertilton@ihmc.us>
2014-10-09 19:50:40 +0000

    GFE-62: There is no way to create a guaranteed valid packet using reflection, so each packet should have it's own knowledge of how to create a random version of itself.

--- IHMCCommunication/src/us/ihmc/communication/packets/Packet.java
+++ IHMCCommunication/src/us/ihmc/communication/packets/Packet.java
@@ -2,7 +2,9 @@ package us.ihmc.communication.packets;
 
 import us.ihmc.utilities.net.ComparableDataObject;
 
+import java.util.Random;
+
 public interface Packet<T> extends ComparableDataObject<T>
 {
-
+    Packet<T> createRandomPacket(Random random);
 }
