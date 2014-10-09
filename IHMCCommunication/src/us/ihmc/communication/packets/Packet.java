1d7ba4173e5c66fc527db92371c4ec9fa0897903
Alex Graber-Tilton <agrabertilton@ihmc.us>
2014-10-09 22:09:45 +0000

    GFE-62: removed the createRandomPacket method from all packets. Added a constructor(Random) to all packets except lidar data. This is to allow generalized testing of a packet class. Also fixed the ROSMessageConverter to work with HandPosePackets.

--- IHMCCommunication/src/us/ihmc/communication/packets/Packet.java
+++ IHMCCommunication/src/us/ihmc/communication/packets/Packet.java
@@ -2,9 +2,6 @@ package us.ihmc.communication.packets;
 
 import us.ihmc.utilities.net.ComparableDataObject;
 
-import java.util.Random;
-
 public interface Packet<T> extends ComparableDataObject<T>
 {
-    Packet<T> createRandomPacket(Random random);
 }
