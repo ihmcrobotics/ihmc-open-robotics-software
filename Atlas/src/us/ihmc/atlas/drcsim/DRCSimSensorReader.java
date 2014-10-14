4cda9cd48bae5c820be203b8f2a98ab4853b8952
Jesper Smith <jsmith@ihmc.us>
2014-10-14 13:48:52 +0000

    NONE: Start of gazebo plugin for GFE. fixbamboo (really I'm remote, svn is slow, and don't want to check bamboo)

---
+++ Atlas/src/us/ihmc/atlas/drcsim/DRCSimSensorReader.java
@@ -0,0 +1,149 @@
+package us.ihmc.atlas.drcsim;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.List;
+
+import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
+
+import org.ejml.data.DenseMatrix64F;
+
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotSensorInformation;
+import us.ihmc.sensorProcessing.sensorProcessors.SensorOutputMapReadOnly;
+import us.ihmc.sensorProcessing.sensorProcessors.SensorProcessing;
+import us.ihmc.sensorProcessing.sensors.ForceSensorData;
+import us.ihmc.sensorProcessing.sensors.ForceSensorDataHolder;
+import us.ihmc.sensorProcessing.sensors.RawJointSensorDataHolderMap;
+import us.ihmc.sensorProcessing.simulatedSensors.SensorFilterParameters;
+import us.ihmc.sensorProcessing.simulatedSensors.SensorNoiseParameters;
+import us.ihmc.sensorProcessing.simulatedSensors.SensorReader;
+import us.ihmc.sensorProcessing.simulatedSensors.StateEstimatorSensorDefinitions;
+import us.ihmc.utilities.ForceSensorDefinition;
+import us.ihmc.utilities.IMUDefinition;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
+
+public class DRCSimSensorReader implements SensorReader
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry("DRCSimSensorReader");
+   
+   private final ForceSensorDataHolder forceSensorDataHolderForEstimator;
+   private final SensorProcessing sensorProcessing;
+   private final List<OneDoFJoint> jointList;
+   
+   private final IMUDefinition imu;
+
+   private final SocketChannel channel;
+   
+   private final int jointDataLength;
+   private final int imuDataLength;
+   private final int forceSensorDataLength;
+
+   public DRCSimSensorReader(StateEstimatorSensorDefinitions stateEstimatorSensorDefinitions, DRCRobotSensorInformation sensorInformation,
+         SensorFilterParameters sensorFilterParameters, SensorNoiseParameters sensorNoiseParameters, ForceSensorDataHolder forceSensorDataHolderForEstimator,
+         RawJointSensorDataHolderMap rawJointSensorDataHolderMap, YoVariableRegistry parentRegistry)
+   {
+      this.sensorProcessing = new SensorProcessing(stateEstimatorSensorDefinitions, sensorFilterParameters, sensorNoiseParameters, registry);
+      
+      this.jointList = stateEstimatorSensorDefinitions.getJointPositionSensorDefinitions();
+      this.imu = stateEstimatorSensorDefinitions.getAngularVelocitySensorDefinitions().get(0);
+      this.forceSensorDataHolderForEstimator = forceSensorDataHolderForEstimator;
+      
+      jointDataLength = jointList.size() * 8 * 2;
+      imuDataLength = 10 * 8;
+      forceSensorDataLength = forceSensorDataHolderForEstimator.getForceSensorDefinitions().size() * 6  * 8;
+      
+      try
+      {
+         channel = SocketChannel.open();
+         channel.configureBlocking(true);
+         channel.socket().setReceiveBufferSize(jointDataLength + imuDataLength + forceSensorDataLength);
+         channel.socket().setKeepAlive(true);
+         channel.socket().setReuseAddress(true);
+         channel.socket().setSoLinger(false, 0);
+         channel.socket().setTcpNoDelay(true);
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
+      
+      
+      
+      
+      parentRegistry.addChild(registry);
+      // TODO Auto-generated constructor stub
+   }
+
+   @Override
+   public void read()
+   {
+      ByteBuffer data = ByteBuffer.allocate(jointDataLength + imuDataLength + forceSensorDataLength);
+      try
+      {
+         channel.read(data);
+         data.flip();
+         for(int i = 0; i < jointList.size(); i++)
+         {
+            OneDoFJoint joint = jointList.get(i);
+            sensorProcessing.setJointPositionSensorValue(joint, data.getDouble());
+            sensorProcessing.setJointVelocitySensorValue(joint, data.getDouble());
+         }
+         
+         Quat4d orientation = new Quat4d();
+         Vector3d linearAcceleration = new Vector3d();
+         Vector3d angularVelocity = new Vector3d();
+         
+         orientation.setX(data.getDouble());
+         orientation.setY(data.getDouble());
+         orientation.setZ(data.getDouble());
+         orientation.setW(data.getDouble());
+
+         linearAcceleration.setX(data.getDouble());
+         linearAcceleration.setY(data.getDouble());
+         linearAcceleration.setZ(data.getDouble());
+         
+         angularVelocity.setX(data.getDouble());
+         angularVelocity.setY(data.getDouble());
+         angularVelocity.setZ(data.getDouble());
+         
+         for(int i = 0; i < forceSensorDataHolderForEstimator.getForceSensorDefinitions().size(); i++)
+         {
+            ForceSensorDefinition definition = forceSensorDataHolderForEstimator.getForceSensorDefinitions().get(i);
+            ForceSensorData dataHolder = forceSensorDataHolderForEstimator.get(definition);
+            
+            DenseMatrix64F wrench = new DenseMatrix64F(6,1);
+            
+            wrench.set(0, 0, data.getDouble());
+            wrench.set(1, 0, data.getDouble());
+            wrench.set(2, 0, data.getDouble());
+            wrench.set(3, 0, data.getDouble());
+            wrench.set(4, 0, data.getDouble());
+            wrench.set(5, 0, data.getDouble());
+            wrench.set(6, 0, data.getDouble());
+            dataHolder.setWrench(wrench);
+         }
+         
+         sensorProcessing.startComputation(); 
+         
+         
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
+      
+   }
+
+   @Override
+   public SensorOutputMapReadOnly getSensorOutputMapReadOnly()
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+}
