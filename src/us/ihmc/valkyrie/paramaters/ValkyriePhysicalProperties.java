87f400aa729f74d24b16a08575b28e8dbe8d1034
Atlas Logging Computer <AtlasLoggingComputer@unknown>
2014-10-14 20:44:31 +0000

    NONE: Some foot sizing stuff. Won't fixbamboo.

--- src/us/ihmc/valkyrie/paramaters/ValkyriePhysicalProperties.java
+++ src/us/ihmc/valkyrie/paramaters/ValkyriePhysicalProperties.java
@@ -9,16 +9,15 @@ import us.ihmc.robotSide.SideDependentList;
 
 public class ValkyriePhysicalProperties extends DRCRobotPhysicalProperties
 {
-   public static final double footsizeReduction = 0.0;
+   public static final double footsizeReduction = 0.04;
    
-   public static final double ankleHeight = 0.082;
+   public static final double ankleHeight = 0.09; // Should be 0.075 + 0.015 (sole thickness)
    public static final double footLength = 0.25 - footsizeReduction;
-   public static final double footBack = 0.058;
+   public static final double footBack = 0.058 - footsizeReduction/2.0;
    public static final double footForward = footLength - footBack;
    public static final double footWidth = 0.15 - footsizeReduction;
    public static final double thighLength = 0.431;
    public static final double shinLength = 0.406;
-   public static final double pelvisToFoot = 0.887 + 0.3;
    
    public static final double footChamferX = 0.2 * footLength;
    public static final double footChamferY = 0.2 * footWidth;
