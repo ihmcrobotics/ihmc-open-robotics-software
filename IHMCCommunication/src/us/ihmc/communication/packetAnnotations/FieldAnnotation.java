08027d74ba298482a99f4f54e1dc9f208579d084
Nathan Mertins <nmertins@ihmc.us>
2014-10-15 21:34:40 +0000

    GFE-46: more documentation

--- IHMCCommunication/src/us/ihmc/communication/packetAnnotations/FieldAnnotation.java
+++ IHMCCommunication/src/us/ihmc/communication/packetAnnotations/FieldAnnotation.java
@@ -1,4 +1,4 @@
-package us.ihmc.communication.ros;
+package us.ihmc.communication.packetAnnotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface ROSMessageFieldAnnotation
+public @interface FieldAnnotation
 {
 	String fieldDocumentation();
 }
