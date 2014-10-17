b9eae95441d332be0f3b6abdb112510c239e0e25
Alex Graber-Tilton <agrabertilton@ihmc.us>
2014-10-17 20:53:53 +0000

    GFE-72: DRCSimGazeboLauncher builds the catkin workspace and then launches Atlas with the IHMC Plugin in Gazebo.

--- Atlas/src/us/ihmc/atlas/drcsim/DRCSimGazeboLauncher.java
+++ Atlas/src/us/ihmc/atlas/drcsim/DRCSimGazeboLauncher.java
@@ -1,51 +1,55 @@
 package us.ihmc.atlas.drcsim;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 public class DRCSimGazeboLauncher
 {
-   public DRCSimGazeboLauncher()
-   {      
-      runProcess("ihmc_gazebo_catkin_ws", new String[] {"catkin_make"});
-   }
-   
-   private void runProcess(String directory, String[] args)
-   {
-      try
-      {
-         ProcessBuilder builder = new ProcessBuilder(args);
-         
-         builder.directory(new File(directory));
-         
-         builder.redirectErrorStream(true);
-         
-//         builder.environment().
-         
-         Map<String, String> environment = builder.environment();
-         
-         environment.put("PATH", environment.get("PATH") + ":/opt/ros/indigo/bin");
-         
-         for (String key : environment.keySet())
-         {
-            System.out.println(key + "=" + environment.get(key));
-         }
-         
-         builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-         
-         Process p = builder.start();
-         
-         p.waitFor();
-      }
-      catch (IOException | InterruptedException e)
-      {
-         e.printStackTrace();
-      }
-   }
-   
    public static void main(String[] args)
    {
       new DRCSimGazeboLauncher();
+   }
+
+   public DRCSimGazeboLauncher()
+   {      
+      runProcess();
+   }
+   
+   private void runProcess()
+   {
+      //Build plugin
+      String commandline1 = "source ~/.bashrc;" +
+            "source /opt/ros/indigo/setup.bash;" +
+            "cd ihmc_gazebo_catkin_ws;"+
+            "catkin_make";
+      runCommandLine(commandline1);
+
+      //Launch gazebo with ihmc_plugin
+      String commandline2 = "source ~/.bashrc;" +
+            "source /opt/ros/indigo/setup.bash;" +
+            "source /usr/share/drcsim/setup.sh;" +
+            "source $PWD/ihmc_gazebo_catkin_ws/devel/setup.bash;" +
+            "roslaunch ihmc_gazebo ihmc_atlas.launch";
+      runCommandLine(commandline2);
+   }
+
+   public static void runCommandLine(String commandline){
+      String[] commands = new String[]{"/bin/bash", "-l","-c", commandline};
+      ProcessBuilder processBuilder = new ProcessBuilder(commands);
+      processBuilder.inheritIO();
+      processBuilder.redirectErrorStream(true);
+
+      try {
+         Process p = processBuilder.start();
+         p.waitFor();
+         IOUtils.copy(p.getErrorStream(), System.out);
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
    }
 }
