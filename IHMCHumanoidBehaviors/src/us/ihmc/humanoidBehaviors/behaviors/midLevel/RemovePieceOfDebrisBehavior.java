2405f0ee64abd9af7184db9b1fb54f1ab6bd2af4
Nicolas Eyssette <neyssette@ihmc.us>
2014-10-24 16:46:51 +0000

    DRC-1837: improved the debrisBehavior so that the robot can move closer to the object. doesn't fixbamboo

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/midLevel/RemovePieceOfDebrisBehavior.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/midLevel/RemovePieceOfDebrisBehavior.java
@@ -6,12 +6,17 @@ import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
 import us.ihmc.humanoidBehaviors.behaviors.BehaviorInterface;
+import us.ihmc.humanoidBehaviors.behaviors.WalkToLocationBehavior;
 import us.ihmc.humanoidBehaviors.communication.OutgoingCommunicationBridgeInterface;
 import us.ihmc.utilities.humanoidRobot.frames.ReferenceFrames;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
+import us.ihmc.utilities.math.geometry.FrameOrientation;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
 import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+import us.ihmc.yoUtilities.math.frames.YoFrameOrientation;
 
 public class RemovePieceOfDebrisBehavior extends BehaviorInterface
 {
@@ -23,16 +28,31 @@ public class RemovePieceOfDebrisBehavior extends BehaviorInterface
    private final BooleanYoVariable haveInputsBeenSet;
    private BehaviorInterface currentBehavior;
 
+   private final WalkToLocationBehavior walkCloseToObject;
+
+   private final Point3d targetLocation;
+   private final YoFrameOrientation targetOrientation;
+
+   private final FullRobotModel fullRobotModel;
+
+   private double optimalDistanceToGrabObject = 0.3;
+
    public RemovePieceOfDebrisBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridge, FullRobotModel fullRobotModel,
          ReferenceFrames referenceFrame, DoubleYoVariable yoTime)
    {
       super(outgoingCommunicationBridge);
 
+      this.fullRobotModel = fullRobotModel;
+
       graspObject = new GraspObjectBehavior(outgoingCommunicationBridge, fullRobotModel, yoTime);
       dropMic = new DropDebrisBehavior(outgoingCommunicationBridge, fullRobotModel, yoTime);
+      walkCloseToObject = new WalkToLocationBehavior(outgoingCommunicationBridge, fullRobotModel, referenceFrame);
 
       isDone = new BooleanYoVariable("isDone", registry);
       haveInputsBeenSet = new BooleanYoVariable("hasInputsBeenSet", registry);
+
+      targetLocation = new Point3d();
+      targetOrientation = new YoFrameOrientation(getName() + "TargetOrientation", ReferenceFrame.getWorldFrame(), registry);
 
    }
 
@@ -71,7 +91,36 @@ public class RemovePieceOfDebrisBehavior extends BehaviorInterface
    public void setInputs(RigidBodyTransform graspTransform, Point3d graspPosition, Vector3d graspVector)
    {
       graspObject.setGraspPose(graspPosition, graspVector);
+      if (!isObjectCloseEnough(graspPosition))
+      {
+         calculateLocation(graspPosition);
+         walkCloseToObject.setTarget(targetLocation, targetOrientation);
+      }
+      else
+         currentBehavior = behaviors.remove(0);
       haveInputsBeenSet.set(true);
+   }
+
+   private void calculateLocation(Point3d graspPosition)
+   {
+      FramePoint target = new FramePoint(fullRobotModel.getChest().getBodyFixedFrame());
+      target.setToZero();
+      target.setX(graspPosition.getX()-optimalDistanceToGrabObject);
+      target.changeFrame(ReferenceFrame.getWorldFrame());
+      targetLocation.set(target.getPoint());
+      FrameOrientation frame = new FrameOrientation();
+      frame.setToZero(fullRobotModel.getChest().getBodyFixedFrame());
+      frame.changeFrame(ReferenceFrame.getWorldFrame());
+      targetOrientation.set(frame);
+   }
+
+   private boolean isObjectCloseEnough(Point3d graspPosition)
+   {
+      FramePoint target = new FramePoint(ReferenceFrame.getWorldFrame(),graspPosition);
+      target.changeFrame(fullRobotModel.getChest().getBodyFixedFrame());
+      if(target.getX()>=optimalDistanceToGrabObject )
+         return false;
+      return true;
    }
 
    @Override
@@ -132,8 +181,10 @@ public class RemovePieceOfDebrisBehavior extends BehaviorInterface
    {
       behaviors.clear();
       isDone.set(false);
+      walkCloseToObject.initialize();
       graspObject.initialize();
       dropMic.initialize();
+      behaviors.add(walkCloseToObject);
       behaviors.add(graspObject);
       behaviors.add(dropMic);
       currentBehavior = behaviors.remove(0);
