59d78a6135d2b6b56ebd3b0b4e3b2c0e8970a34c
Douglas Stephen <dstephen@ihmc.us>
2014-10-28 17:43:31 +0000

    NONE: Working on fixbamboo. Regenerated messages.

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ComHeightPacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ComHeightPacketMessage.java
@@ -2,7 +2,11 @@ package ihmc_msgs;
 
 public interface ComHeightPacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/ComHeightPacketMessage";
-  static final java.lang.String _DEFINITION = "## ComHeightPacketMessage\n# This message sets the robot\'s center of mass height.\n\n# heightOffset specifies CoM height relative to the default starting height\nfloat64 heightOffset\n\n";
+  static final java.lang.String _DEFINITION = "## ComHeightPacketMessage\n# This message sets the robot\'s center of mass height.\n\nfloat64 MIN_COM_HEIGHT\nfloat64 MAX_COM_HEIGHT\n# heightOffset specifies CoM height relative to the default starting height\nfloat64 heightOffset\n\n";
+  double getMINCOMHEIGHT();
+  void setMINCOMHEIGHT(double value);
+  double getMAXCOMHEIGHT();
+  void setMAXCOMHEIGHT(double value);
   double getHeightOffset();
   void setHeightOffset(double value);
 }
