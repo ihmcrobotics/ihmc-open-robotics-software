57e08516a5ae8231cef9b74c059520a0aef6dd6c
Atlas Control Computer <AtlasControlComputer@unknown>
2014-10-31 14:52:22 +0000

    NONE: Reduced the foot size for Atlas. Won't fixbamboo

--- Atlas/src/us/ihmc/atlas/parameters/AtlasPhysicalProperties.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasPhysicalProperties.java
@@ -11,9 +11,9 @@ public class AtlasPhysicalProperties extends DRCRobotPhysicalProperties
    public static final double ankleHeight = 0.084;
    public static final double pelvisToFoot = 0.887;
 
-   public static final double footWidth = 0.12; // 0.08;   //0.124887;
-   public static final double toeWidth = 0.095; // 0.07;   //0.05;   //
-   public static final double footLength = 0.255;
+   public static final double footWidth = 0.11; //0.12; // 0.08;   //0.124887;
+   public static final double toeWidth = 0.085; //0.095; // 0.07;   //0.05;   //
+   public static final double footLength = 0.22; //0.255;
    public static final double footBack = 0.09; // 0.06;   //0.082;    // 0.07;
    public static final double footStartToetaperFromBack = 0.195;
    public static final double footForward = footLength - footBack; // 0.16;   //0.178;    // 0.18;
