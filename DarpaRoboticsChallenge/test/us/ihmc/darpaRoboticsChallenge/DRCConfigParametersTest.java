18b0c6d2cd43e55a2cae8d10a152de7b4c161c18
Nicolas Eyssette <neyssette@ihmc.us>
2014-11-03 21:49:10 +0000

    NONE: Sylvain set DRCConfigParameters.SEND_ROBOT_DATA_TO_ROS to false. Wom't fixbamboo

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/DRCConfigParametersTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/DRCConfigParametersTest.java
@@ -18,6 +18,8 @@ public class DRCConfigParametersTest
       assertFalse("Do not check in MomentumBasedController.SPY_ON_MOMENTUM_BASED_CONTROLLER = true!!", MomentumBasedController.SPY_ON_MOMENTUM_BASED_CONTROLLER);      
       
       assertTrue("Do not add parameters to DRCConfigParameters.", DRCConfigParameters.class.getFields().length <= 14);
+
+      assertFalse("Do not check in SEND_ROBOT_DATA_TO_ROS = true!!", DRCConfigParameters.SEND_ROBOT_DATA_TO_ROS);      
    }
 
 }
