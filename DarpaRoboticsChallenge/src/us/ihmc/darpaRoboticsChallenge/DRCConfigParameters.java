6cfae59ebba243a3a0e2301be28b86e0ba3c79d3
Jesper Smith <jsmith@ihmc.us>
2014-11-03 18:07:02 +0000

    NONE: Removed shit from DRCConfigParameters.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCConfigParameters.java
@@ -28,4 +28,6 @@ public class DRCConfigParameters
    
    public static final boolean ALLOW_MODEL_CORRUPTION = false;
    public static final boolean SEND_ROBOT_DATA_TO_ROS = true;
+   
+   // Do not add anything here. I'll shout at you. Jesper
 }
