9498d0ce69420124ec0ee44e5fc4028b1574d31f
Jesper Smith <jsmith@ihmc.us>
2014-11-06 17:26:52 +0000

    DRC-1977: output encoders for StepprAnkleActuator

--- Acsell/src/us/ihmc/steppr/hardware/state/slowSensors/MotorTemperature.java
+++ Acsell/src/us/ihmc/steppr/hardware/state/slowSensors/MotorTemperature.java
@@ -15,7 +15,15 @@ public class MotorTemperature implements StepprSlowSensor
    @Override
    public void update(int value)
    {
-      motorTemperature.set(ADCTemperatureSensorConversion.toTemperature(value));
+      double therm_div_volt = value * 3.3f / 4096.0f;
+      double therm_r_meas = 10.0e3f / (3.3f / therm_div_volt - 1);
+      double THERM_B = 4288;
+      double THERM_R_25C = 50000;
+      double motor_temp_c = 0;
+      if (therm_div_volt > 0)
+         motor_temp_c = THERM_B / Math.log(therm_r_meas / (THERM_R_25C * Math.exp(-THERM_B / 298.15))) - 273.15;
+
+      motorTemperature.set(motor_temp_c);
    }
    
 }
