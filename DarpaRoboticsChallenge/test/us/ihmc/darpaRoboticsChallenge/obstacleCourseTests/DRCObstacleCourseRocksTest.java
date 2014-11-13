3dcddd4b722447414c0b494724b20dce194af6db
Duncan Calvert <dcalvert@ihmc.us>
2014-11-13 21:42:14 +0000

    NONE: Format. Won't fixBamboo.

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/obstacleCourseTests/DRCObstacleCourseRocksTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/obstacleCourseTests/DRCObstacleCourseRocksTest.java
@@ -25,8 +25,8 @@ import com.yobotics.simulationconstructionset.util.simulationRunner.BlockingSimu
 
 public abstract class DRCObstacleCourseRocksTest implements MultiRobotTestInterface
 {
-	   private static final boolean alwaysShowGUI = false;
-	   private static final boolean KEEP_SCS_UP = false;
+   private static final boolean alwaysShowGUI = false;
+   private static final boolean KEEP_SCS_UP = false;
 
    private static final boolean createMovie = BambooTools.doMovieCreation();
    private static final boolean checkNothingChanged = BambooTools.getCheckNothingChanged();
