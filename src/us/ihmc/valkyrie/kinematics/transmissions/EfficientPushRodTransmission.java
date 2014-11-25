346c55123b8a573aaa8cb88c47da1c679a7d501c
Will Rifenburgh <wrifenburgh@ihmc.us>
2014-11-25 17:39:47 +0000

    DRC-2027: Created a way to set joint qd with the inverse jacobian of the efficient pushrod tranmissions. wont fixbamboo

--- src/us/ihmc/valkyrie/kinematics/transmissions/EfficientPushRodTransmission.java
+++ src/us/ihmc/valkyrie/kinematics/transmissions/EfficientPushRodTransmission.java
@@ -8,209 +8,250 @@ import us.ihmc.valkyrie.roboNet.TurboDriver;
 import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
 import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
 
-public class EfficientPushRodTransmission implements PushRodTransmissionInterface {
-    private static final double INFINITY_THRESHOLD = 1e10;
+public class EfficientPushRodTransmission implements PushRodTransmissionInterface
+{
+   private static final double INFINITY_THRESHOLD = 1e10;
 
-    // Temporary matrix
-    private double[][]               jacobian                  = new double[2][2];
-    private final double[][]         jacobianTranspose         = new double[2][2];
-    private final double[][]         jacobianInverse           = new double[2][2];
-    private final double[][]         jacobianInvertedTranspose = new double[2][2];
-    private final ClosedFormJacobian efficientPushrodTransmissionJacobian;
-    private final double             reflect;
+   // Temporary matrix
+   private double[][] jacobian = new double[2][2];
+   private final double[][] jacobianTranspose = new double[2][2];
+   private final double[][] jacobianInverse = new double[2][2];
+   private final double[][] jacobianInvertedTranspose = new double[2][2];
+   private final ClosedFormJacobian efficientPushrodTransmissionJacobian;
+   private final double reflect;
+   private final PushRodTransmissionJoint pushRodTransmissionJoint;
 
-	private DoubleYoVariable pitchAngleOffset;
+   private DoubleYoVariable pitchAngleOffset;
 
-    public EfficientPushRodTransmission(PushRodTransmissionJoint pushRodTransmissionJoint, double reflect, boolean futekBoolean) {
-        if (Math.abs(Math.abs(reflect) - 1.0) > 1e-7) {
-            throw new RuntimeException("reflect must be 1.0 or -1.0");
-        }
+   public EfficientPushRodTransmission(PushRodTransmissionJoint pushRodTransmissionJoint, double reflect, boolean futekBoolean)
+   {
+      if (Math.abs(Math.abs(reflect) - 1.0) > 1e-7)
+      {
+         throw new RuntimeException("reflect must be 1.0 or -1.0");
+      }
 
-        this.reflect                         = reflect;
-        efficientPushrodTransmissionJacobian = new ClosedFormJacobian(pushRodTransmissionJoint);
-        efficientPushrodTransmissionJacobian.useFuteks(futekBoolean);
-    }
+      this.reflect = reflect;
+      efficientPushrodTransmissionJacobian = new ClosedFormJacobian(pushRodTransmissionJoint);
+      efficientPushrodTransmissionJacobian.useFuteks(futekBoolean);
+      this.pushRodTransmissionJoint = pushRodTransmissionJoint;
+   }
 
-    private int numActuators() {
-        return 2;
-    }
+   private int numActuators()
+   {
+      return 2;
+   }
 
-    private int numJoints() {
-        return 2;
-    }
+   private int numJoints()
+   {
+      return 2;
+   }
 
-    private void assertTrue(boolean test) {
-        if (!test) {
-            throw new RuntimeException();
-        }
-    }
+   private void assertTrue(boolean test)
+   {
+      if (!test)
+      {
+         throw new RuntimeException();
+      }
+   }
 
-    private void checkInfinity(double value) {
-        if (Math.abs(value) > INFINITY_THRESHOLD) {
-            throw new RuntimeException("checkInfinity: Infinity value detected in supplied data structure!");
-        }
-    }
+   private void checkInfinity(double value)
+   {
+      if (Math.abs(value) > INFINITY_THRESHOLD)
+      {
+         throw new RuntimeException("checkInfinity: Infinity value detected in supplied data structure!");
+      }
+   }
 
-    private boolean invertMatrix(double[][] matrix, double[][] inverseToPack) {
-        double det = (matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]);
+   private boolean invertMatrix(double[][] matrix, double[][] inverseToPack)
+   {
+      double det = (matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]);
 
-        if (det != 0.0) {
-            inverseToPack[0][0] = (1.0 / det) * matrix[1][1];
-            inverseToPack[0][1] = (1.0 / det) * (-matrix[0][1]);
-            inverseToPack[1][0] = (1.0 / det) * (-matrix[1][0]);
-            inverseToPack[1][1] = (1.0 / det) * matrix[0][0];
+      if (det != 0.0)
+      {
+         inverseToPack[0][0] = (1.0 / det) * matrix[1][1];
+         inverseToPack[0][1] = (1.0 / det) * (-matrix[0][1]);
+         inverseToPack[1][0] = (1.0 / det) * (-matrix[1][0]);
+         inverseToPack[1][1] = (1.0 / det) * matrix[0][0];
 
-            return true;
-        } else {
-            return false;
-        }
-    }
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   }
 
-    private void transposeMatrix(double[][] matrix, double[][] transposeToPack) {
-        transposeToPack[0][0] = matrix[0][0];
-        transposeToPack[0][1] = matrix[1][0];
-        transposeToPack[1][0] = matrix[0][1];
-        transposeToPack[1][1] = matrix[1][1];
-    }
+   private void transposeMatrix(double[][] matrix, double[][] transposeToPack)
+   {
+      transposeToPack[0][0] = matrix[0][0];
+      transposeToPack[0][1] = matrix[1][0];
+      transposeToPack[1][0] = matrix[0][1];
+      transposeToPack[1][1] = matrix[1][1];
+   }
 
-    @Override
-    public void actuatorToJointEffort(TurboDriver[] actuatorData, ValkyrieJointInterface[] jointData) {
-        assertTrue((numActuators() == actuatorData.length) && (numJoints() == jointData.length));
+   @Override
+   public void actuatorToJointEffort(TurboDriver[] actuatorData, ValkyrieJointInterface[] jointData)
+   {
+      assertTrue((numActuators() == actuatorData.length) && (numJoints() == jointData.length));
 
-        double actuatorForce0 = actuatorData[0].getEffort();
-        double actuatorForce1 = actuatorData[1].getEffort();
-        double pitchAngle     = jointData[0].getPosition();
-        if(pitchAngleOffset != null) pitchAngle += pitchAngleOffset.getDoubleValue();
-        double rollAngle      = reflect * jointData[1].getPosition();
+      double actuatorForce0 = actuatorData[0].getEffort();
+      double actuatorForce1 = actuatorData[1].getEffort();
+      double pitchAngle = jointData[0].getPosition();
+      if (pitchAngleOffset != null)
+         pitchAngle += pitchAngleOffset.getDoubleValue();
+      double rollAngle = reflect * jointData[1].getPosition();
 
-        jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
-        transposeMatrix(jacobian, jacobianTranspose);
+      jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
+      transposeMatrix(jacobian, jacobianTranspose);
 
-        // tau = (J^T) * F
-        double pitchTorque  = jacobianTranspose[0][0] * actuatorForce0 + jacobianTranspose[0][1] * actuatorForce1;
-        double rollTorque = jacobianTranspose[1][0] * actuatorForce0 + jacobianTranspose[1][1] * actuatorForce1;
+      // tau = (J^T) * F
+      double pitchTorque = jacobianTranspose[0][0] * actuatorForce0 + jacobianTranspose[0][1] * actuatorForce1;
+      double rollTorque = jacobianTranspose[1][0] * actuatorForce0 + jacobianTranspose[1][1] * actuatorForce1;
 
-        jointData[0].setEffort(pitchTorque);
-        jointData[1].setEffort(reflect * rollTorque);
-    }
+      jointData[0].setEffort(pitchTorque);
+      jointData[1].setEffort(reflect * rollTorque);
+   }
 
-    @Override
-    public void actuatorToJointVelocity(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data) {
-    	if(!efficientPushrodTransmissionJacobian.isUsingFuteks()){
-    		
-        // Validation Velocity is used to validate the correct order and sign of the jacobian matrix elements.
-        assertTrue((numActuators() == act_data.length) && (numJoints() == jnt_data.length));
+   @Override
+   public void actuatorToJointVelocity(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data)
+   {
+      if (!efficientPushrodTransmissionJacobian.isUsingFuteks())
+      {
 
-        double actuatorVelocity0 = act_data[0].getVelocity();
-        double actuatorVelocity1 = act_data[1].getVelocity();
-        double pitchAngle        = jnt_data[0].getPosition();
-        if(pitchAngleOffset != null) pitchAngle += pitchAngleOffset.getDoubleValue();
-        double rollAngle         = reflect * jnt_data[1].getPosition();
+         // Validation Velocity is used to validate the correct order and sign of the jacobian matrix elements.
+         assertTrue((numActuators() == act_data.length) && (numJoints() == jnt_data.length));
 
-        jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
-        invertMatrix(jacobian, jacobianInverse);
+         double actuatorVelocity0 = act_data[0].getVelocity();
+         double actuatorVelocity1 = act_data[1].getVelocity();
+         double pitchAngle = jnt_data[0].getPosition();
+         if (pitchAngleOffset != null)
+            pitchAngle += pitchAngleOffset.getDoubleValue();
+         double rollAngle = reflect * jnt_data[1].getPosition();
 
-        // theta_dot = J^-1 * x_dot
-        double pitchVelocity  = jacobianInverse[0][0] * actuatorVelocity0 + jacobianInverse[0][1] * actuatorVelocity1;
-        double rollVelocity = jacobianInverse[1][0] * actuatorVelocity0 + jacobianInverse[1][1] * actuatorVelocity1;
+         jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
+         invertMatrix(jacobian, jacobianInverse);
 
-        jnt_data[0].setValidationVelocity(pitchVelocity);
-        jnt_data[1].setValidationVelocity(reflect * rollVelocity);
-    	}
-    }
+         // theta_dot = J^-1 * x_dot
+         double pitchVelocity = jacobianInverse[0][0] * actuatorVelocity0 + jacobianInverse[0][1] * actuatorVelocity1;
+         double rollVelocity = jacobianInverse[1][0] * actuatorVelocity0 + jacobianInverse[1][1] * actuatorVelocity1;
 
-    @Override
-    public void actuatorToJointPosition(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data) {
+         if (pushRodTransmissionJoint == PushRodTransmissionJoint.WAIST)
+         {
+            jnt_data[0].setVelocity(-pitchVelocity);
+            jnt_data[1].setVelocity(reflect * -rollVelocity);
+         }
+         else
+         {
+            jnt_data[0].setVelocity(pitchVelocity);
+            jnt_data[1].setVelocity(reflect * rollVelocity);
+         }
+      }
+   }
 
-        // TODO Auto-generated method stub
-    }
+   @Override
+   public void actuatorToJointPosition(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data)
+   {
 
-    @Override
-    public void jointToActuatorEffort(TurboDriver[] actuatorData, ValkyrieJointInterface[] jointData) {
-        assertTrue((numActuators() == actuatorData.length) && (numJoints() == jointData.length));
+      // TODO Auto-generated method stub
+   }
 
-        double pitchAngle  = jointData[0].getPosition();
-        if(pitchAngleOffset != null) pitchAngle += pitchAngleOffset.getDoubleValue();
-        double rollAngle   = reflect * jointData[1].getPosition();
-        double pitchTorque = jointData[0].getDesiredEffort();
-        double rollTorque  = reflect * jointData[1].getDesiredEffort();
-        
+   @Override
+   public void jointToActuatorEffort(TurboDriver[] actuatorData, ValkyrieJointInterface[] jointData)
+   {
+      assertTrue((numActuators() == actuatorData.length) && (numJoints() == jointData.length));
 
-        if ((Math.abs(pitchAngle) > INFINITY_THRESHOLD) || (Math.abs(rollAngle) > INFINITY_THRESHOLD)) {
-            throw new RuntimeException("jointToActuatorEffort: pitchAngle or rollAngle is infinity!!\n");
-        }
+      double pitchAngle = jointData[0].getPosition();
+      if (pitchAngleOffset != null)
+         pitchAngle += pitchAngleOffset.getDoubleValue();
+      double rollAngle = reflect * jointData[1].getPosition();
+      double pitchTorque = jointData[0].getDesiredEffort();
+      double rollTorque = reflect * jointData[1].getDesiredEffort();
 
-        jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
-        transposeMatrix(jacobian, jacobianTranspose);
-        invertMatrix(jacobianTranspose, jacobianInvertedTranspose);
+      if ((Math.abs(pitchAngle) > INFINITY_THRESHOLD) || (Math.abs(rollAngle) > INFINITY_THRESHOLD))
+      {
+         throw new RuntimeException("jointToActuatorEffort: pitchAngle or rollAngle is infinity!!\n");
+      }
 
-        // F = (J^T)^-1 * tau
-        double actuatorForce0 = jacobianInvertedTranspose[0][0] * pitchTorque + jacobianInvertedTranspose[0][1] * rollTorque;
-        double actuatorForce1 = jacobianInvertedTranspose[1][0] * pitchTorque + jacobianInvertedTranspose[1][1] * rollTorque;
+      jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
+      transposeMatrix(jacobian, jacobianTranspose);
+      invertMatrix(jacobianTranspose, jacobianInvertedTranspose);
 
-        checkInfinity(actuatorForce0);
-        checkInfinity(actuatorForce1);
-        
-        actuatorData[0].setEffortCommand(actuatorForce0);
-        actuatorData[1].setEffortCommand(actuatorForce1);
-    }
+      // F = (J^T)^-1 * tau
+      double actuatorForce0 = jacobianInvertedTranspose[0][0] * pitchTorque + jacobianInvertedTranspose[0][1] * rollTorque;
+      double actuatorForce1 = jacobianInvertedTranspose[1][0] * pitchTorque + jacobianInvertedTranspose[1][1] * rollTorque;
 
-    @Override
-    public void jointToActuatorVelocity(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data) {
+      checkInfinity(actuatorForce0);
+      checkInfinity(actuatorForce1);
 
-        // TODO Auto-generated method stub
-    }
+      actuatorData[0].setEffortCommand(actuatorForce0);
+      actuatorData[1].setEffortCommand(actuatorForce1);
 
-    @Override
-    public void jointToActuatorPosition(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data) {
-    }
+   }
 
-    @Override
-    public void updateMotorCurrents(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data) {
-        assertTrue((numActuators() == act_data.length) && (numJoints() == jnt_data.length));
-        jnt_data[0].setMotorCurrent(act_data[0].getCurrentIq());
-        jnt_data[0].setCommandedMotorCurrent(act_data[0].getCurrentIqCmd());
-        jnt_data[1].setMotorCurrent(act_data[1].getCurrentIq());
-        jnt_data[1].setCommandedMotorCurrent(act_data[1].getCurrentIqCmd());
-    }
+   @Override
+   public void jointToActuatorVelocity(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data)
+   {
 
-	public void allowTopJointAngleOffset(String string, double topJointOffset, YoVariableRegistry parentRegistry) {
-	      pitchAngleOffset = new DoubleYoVariable(string + "pitchAngleOffset", parentRegistry);
-	      pitchAngleOffset.set(topJointOffset);
-	}
-	
-	public void renishawToFutekForce(TurboDriver[] actuatorData, ValkyrieJointInterface[] jointData, DoubleYoVariable renishawInFutekSpace0, DoubleYoVariable renishawInFutekSpace1){
-		if(!efficientPushrodTransmissionJacobian.isUsingFuteks()){
-			
-		double actuatorForce0 = actuatorData[0].getEffort();
-        double actuatorForce1 = actuatorData[1].getEffort();
-        double pitchAngle     = jointData[0].getPosition();
-        if(pitchAngleOffset != null) pitchAngle += pitchAngleOffset.getDoubleValue();
-        double rollAngle      = reflect * jointData[1].getPosition();
-        jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
-        transposeMatrix(jacobian, jacobianTranspose);
+      // TODO Auto-generated method stub
+   }
 
-        // tau = (J^T) * F
-        double pitchTorque  = jacobianTranspose[0][0] * actuatorForce0 + jacobianTranspose[0][1] * actuatorForce1;
-        double rollTorque = jacobianTranspose[1][0] * actuatorForce0 + jacobianTranspose[1][1] * actuatorForce1;
-        
-        efficientPushrodTransmissionJacobian.useFuteks(true);
-        jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
-        transposeMatrix(jacobian, jacobianTranspose);
-        invertMatrix(jacobianTranspose, jacobianInvertedTranspose);
+   @Override
+   public void jointToActuatorPosition(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data)
+   {
+   }
 
-        // F = (J^T)^-1 * tau
-        actuatorForce0 = jacobianInvertedTranspose[0][0] * pitchTorque + jacobianInvertedTranspose[0][1] * rollTorque;
-        actuatorForce1 = jacobianInvertedTranspose[1][0] * pitchTorque + jacobianInvertedTranspose[1][1] * rollTorque;
+   @Override
+   public void updateMotorCurrents(TurboDriver[] act_data, ValkyrieJointInterface[] jnt_data)
+   {
+      assertTrue((numActuators() == act_data.length) && (numJoints() == jnt_data.length));
+      jnt_data[0].setMotorCurrent(act_data[0].getCurrentIq());
+      jnt_data[0].setCommandedMotorCurrent(act_data[0].getCurrentIqCmd());
+      jnt_data[1].setMotorCurrent(act_data[1].getCurrentIq());
+      jnt_data[1].setCommandedMotorCurrent(act_data[1].getCurrentIqCmd());
+   }
 
-        checkInfinity(actuatorForce0);
-        checkInfinity(actuatorForce1);
-        
-        renishawInFutekSpace0.set(actuatorForce0);
-        renishawInFutekSpace1.set(actuatorForce1);
-        
-        efficientPushrodTransmissionJacobian.useFuteks(false);
-        
-		}
-	}
+   public void allowTopJointAngleOffset(String string, double topJointOffset, YoVariableRegistry parentRegistry)
+   {
+      pitchAngleOffset = new DoubleYoVariable(string + "pitchAngleOffset", parentRegistry);
+      pitchAngleOffset.set(topJointOffset);
+   }
+
+   public void renishawToFutekForce(TurboDriver[] actuatorData, ValkyrieJointInterface[] jointData, DoubleYoVariable renishawInFutekSpace0,
+         DoubleYoVariable renishawInFutekSpace1)
+   {
+      if (!efficientPushrodTransmissionJacobian.isUsingFuteks())
+      {
+
+         double actuatorForce0 = actuatorData[0].getEffort();
+         double actuatorForce1 = actuatorData[1].getEffort();
+         double pitchAngle = jointData[0].getPosition();
+         if (pitchAngleOffset != null)
+            pitchAngle += pitchAngleOffset.getDoubleValue();
+         double rollAngle = reflect * jointData[1].getPosition();
+         jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
+         transposeMatrix(jacobian, jacobianTranspose);
+
+         // tau = (J^T) * F
+         double pitchTorque = jacobianTranspose[0][0] * actuatorForce0 + jacobianTranspose[0][1] * actuatorForce1;
+         double rollTorque = jacobianTranspose[1][0] * actuatorForce0 + jacobianTranspose[1][1] * actuatorForce1;
+
+         efficientPushrodTransmissionJacobian.useFuteks(true);
+         jacobian = efficientPushrodTransmissionJacobian.getUpdatedTransform(rollAngle, pitchAngle);
+         transposeMatrix(jacobian, jacobianTranspose);
+         invertMatrix(jacobianTranspose, jacobianInvertedTranspose);
+
+         // F = (J^T)^-1 * tau
+         actuatorForce0 = jacobianInvertedTranspose[0][0] * pitchTorque + jacobianInvertedTranspose[0][1] * rollTorque;
+         actuatorForce1 = jacobianInvertedTranspose[1][0] * pitchTorque + jacobianInvertedTranspose[1][1] * rollTorque;
+
+         checkInfinity(actuatorForce0);
+         checkInfinity(actuatorForce1);
+
+         renishawInFutekSpace0.set(actuatorForce0);
+         renishawInFutekSpace1.set(actuatorForce1);
+
+         efficientPushrodTransmissionJacobian.useFuteks(false);
+
+      }
+   }
 }
