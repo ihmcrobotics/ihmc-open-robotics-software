b3947a3dae0ba8d842f7b48d91fd9644a6fc8358
Duncan Calvert <dcalvert@ihmc.us>
2014-12-02 15:56:51 +0000

    DRC-1868: Added destroy button toggles to process spawners. In JavaProcessSpawner, return Process from spawn(). fixBamboo.

--- Atlas/test/us/ihmc/atlas/ObstacleCourseTests/AtlasWallWorldTest.java
+++ Atlas/test/us/ihmc/atlas/ObstacleCourseTests/AtlasWallWorldTest.java
@@ -2,7 +2,6 @@ package us.ihmc.atlas.ObstacleCourseTests;
 
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
-import us.ihmc.atlas.parameters.AtlasContactPointParameters;
 import us.ihmc.bambooTools.BambooTools;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCWallWorldTest;
@@ -11,12 +10,9 @@ public class AtlasWallWorldTest extends DRCWallWorldTest
 {
    private final AtlasRobotModel robotModel;
    
-   public AtlasWallWorldTest()
    {
       robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_INVISIBLE_CONTACTABLE_PLANE_HANDS, AtlasRobotModel.AtlasTarget.SIM, false);
-      
-      AtlasContactPointParameters contactPointParameters = robotModel.getContactPointParameters();
-      contactPointParameters.createHandKnobContactPoints();
+      robotModel.getContactPointParameters().createHandKnobContactPoints();
    }
    
    @Override
