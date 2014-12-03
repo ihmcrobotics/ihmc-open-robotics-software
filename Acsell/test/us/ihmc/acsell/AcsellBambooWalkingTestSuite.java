042a32be1eb32dd3e12617ee54d826a360b00f35
Nathan Mertins <nmertins@ihmc.us>
2014-12-03 15:44:50 +0000

    GFE-110: added a walking test suite to Acsell project

---
+++ Acsell/test/us/ihmc/acsell/AcsellBambooWalkingTestSuite.java
@@ -0,0 +1,21 @@
+package us.ihmc.acsell;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+{
+	BonoFlatGroundWalkingTest.class,
+})
+
+public class AcsellBambooWalkingTestSuite
+{
+	public static void main(String[] args)
+	{
+		String packageName = "us.ihmc.acsell";
+		System.out.println(JUnitTestSuiteConstructor.createTestSuite("AcsellBambooWalkingTestSuite", packageName));
+	}
+}
