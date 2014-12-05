0c48434d1995f6720c20573641f3720409f8885e
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2014-12-05 01:08:17 +0000

    none: rosmsgs update  should fixbamboo

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ChestOrientationPacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ChestOrientationPacketMessage.java
@@ -2,7 +2,9 @@ package ihmc_msgs;
 
 public interface ChestOrientationPacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/ChestOrientationPacketMessage";
-  static final java.lang.String _DEFINITION = "## ChestOrientationPacketMessage\r\n# This message gives the orientation of the robot\'s chest in world frame.\r\n\r\ngeometry_msgs/Quaternion quaternion\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## ChestOrientationPacketMessage\n# This message gives the orientation of the robot\'s chest in world frame.\n\ngeometry_msgs/Quaternion quaternion\nfloat64 trajectoryTime\n\n";
   geometry_msgs.Quaternion getQuaternion();
   void setQuaternion(geometry_msgs.Quaternion value);
+  double getTrajectoryTime();
+  void setTrajectoryTime(double value);
 }
--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootPosePacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootPosePacketMessage";
-  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\r\n# This message gives the position and oreientation of one foot in world coordinates.\r\n\r\n#Options for enum\r\n# uint8 L = 0\r\n# uint8 R = 1\r\nuint8 robotSide\r\ngeometry_msgs/Vector3 position\r\ngeometry_msgs/Quaternion orientation\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\n# This message gives the position and oreientation of one foot in world coordinates.\n\n#Options for enum\n# uint8 L = 0\n# uint8 R = 1\nuint8 robotSide\ngeometry_msgs/Vector3 position\ngeometry_msgs/Quaternion orientation\n\n";
   byte getRobotSide();
   void setRobotSide(byte value);
   geometry_msgs.Vector3 getPosition();
