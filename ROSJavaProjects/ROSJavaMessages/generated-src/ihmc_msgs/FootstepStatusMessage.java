0e0f3bb54de8ccdc6f178c63cd19cbe4afb5a639
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2014-12-05 01:08:17 +0000

    none: rosmsgs update  should fixbamboo

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepStatusMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepStatusMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootstepStatusMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootstepStatusMessage";
-  static final java.lang.String _DEFINITION = "## FootstepStatusMessage\r\n# This message gives the status of the current footstep from the controller.\r\n\r\n#Options for enum\r\n# uint8 STARTED = 0\r\n# uint8 COMPLETED = 1\r\nuint8 status\r\n# footstepIndex monotonically increases with each completed footstep in a given\r\n# FootstepDataList and is then reset to 0 after all footsteps in the list are\r\n# completed.\r\nint32 footstepIndex\r\nint32 robotSide\r\ngeometry_msgs/Vector3 actualFootPositionInWorld\r\ngeometry_msgs/Quaternion actualFootOrientationInWorld\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## FootstepStatusMessage\n# This message gives the status of the current footstep from the controller.\n\n#Options for enum\n# uint8 STARTED = 0\n# uint8 COMPLETED = 1\nuint8 status\n# footstepIndex monotonically increases with each completed footstep in a given\n# FootstepDataList and is then reset to 0 after all footsteps in the list are\n# completed.\nint32 footstepIndex\nint32 robotSide\ngeometry_msgs/Vector3 actualFootPositionInWorld\ngeometry_msgs/Quaternion actualFootOrientationInWorld\n\n";
   byte getStatus();
   void setStatus(byte value);
   int getFootstepIndex();
--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/PauseCommandMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/PauseCommandMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface PauseCommandMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/PauseCommandMessage";
-  static final java.lang.String _DEFINITION = "## PauseCommandMessage\r\n# This message pauses the execution of a list of footsteps. If\r\n# sent in the middle of executing a footstep, the robot will finish\r\n# the step and pause when in double support.\r\n\r\nbool pause\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## PauseCommandMessage\n# This message pauses the execution of a list of footsteps. If\n# sent in the middle of executing a footstep, the robot will finish\n# the step and pause when in double support.\n\nbool pause\n\n";
   boolean getPause();
   void setPause(boolean value);
 }
