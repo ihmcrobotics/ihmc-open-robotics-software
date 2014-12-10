7db7ed643f79d06de5b2106014d0c77791d65492
Duncan Calvert <dcalvert@ihmc.us>
2014-12-10 23:17:28 +0000

    GFE-156: Refactoring and formatting test suites. Won't fixBamboo.

--- Graphics3DAdapter/test/us/ihmc/graphics3DAdapter/jme/JMEGraphics3dAdapterBambooJMETestSuite.java
+++ Graphics3DAdapter/test/us/ihmc/graphics3DAdapter/jme/JMEGraphics3dAdapterBambooJMETestSuite.java
@@ -6,20 +6,18 @@ import org.junit.runners.Suite;
 import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-   
+@Suite.SuiteClasses
+({
    us.ihmc.graphics3DAdapter.jme.lidar.JMEGPULidarTest.class,
    us.ihmc.graphics3DAdapter.jme.JMEGraphics3DWorldTest.class,
    us.ihmc.graphics3DAdapter.jme.JMERayCollisionAdapterTest.class,
    us.ihmc.graphics3DAdapter.jme.JMERendererTest.class
-                     
 })
 
 public class JMEGraphics3dAdapterBambooJMETestSuite
 {
    public static void main(String[] args)
    {
-      String packageName = "us.ihmc.graphics3DAdapter.jme";
-      System.out.println(JUnitTestSuiteConstructor.createTestSuite("JMEGraphics3dAdapterBambooTestSuite", packageName));
+      JUnitTestSuiteConstructor.generateTestSuite(JMEGraphics3dAdapterBambooJMETestSuite.class);
    }
 }
