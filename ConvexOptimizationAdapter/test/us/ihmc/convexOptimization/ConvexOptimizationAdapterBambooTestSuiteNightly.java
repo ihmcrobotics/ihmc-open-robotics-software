f0185456960fe01cd9a91542ccdb1f6ac6516753
Duncan Calvert <dcalvert@ihmc.us>
2014-12-10 22:52:16 +0000

    GFE-156: Refactoring and formatting test suites. Won't fixBamboo.

--- ConvexOptimizationAdapter/test/us/ihmc/convexOptimization/ConvexOptimizationAdapterBambooTestSuiteNightly.java
+++ ConvexOptimizationAdapter/test/us/ihmc/convexOptimization/ConvexOptimizationAdapterBambooTestSuiteNightly.java
@@ -1,22 +1,26 @@
 package us.ihmc.convexOptimization;
 
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
 import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-{
-   us.ihmc.convexOptimization.quadraticProgram.GenericActiveSetQPSolverTest.class
+@Suite.SuiteClasses
+({
+   us.ihmc.convexOptimization.experimental.ExperimentalSOCPSolverUsingJOptimizerTest.class,
+   us.ihmc.convexOptimization.jOptimizer.JOptimizerConvexOptimizationAdapterTest.class,
+   us.ihmc.convexOptimization.jOptimizer.SimpleJOptimizerTest.class,
+   us.ihmc.convexOptimization.quadraticProgram.GenericActiveSetQPSolverTest.class,
+   us.ihmc.convexOptimization.quadraticProgram.SimpleActiveSetQPSolverTest.class,
+   us.ihmc.convexOptimization.randomSearch.RandomSearchConvexOptimizationAdapterTest.class
 })
 
 public class ConvexOptimizationAdapterBambooTestSuiteNightly
 {
    public static void main(String[] args)
    {
-      String packageName = "us.ihmc.ConvexOptimizationAdapter";
-      System.out.println(JUnitTestSuiteConstructor.createTestSuite("ConvexOptimizationAdapterBambooTestSuite", packageName));
+      JUnitTestSuiteConstructor.generateTestSuite(ConvexOptimizationAdapterBambooTestSuiteNightly.class);
    }
 }
+
