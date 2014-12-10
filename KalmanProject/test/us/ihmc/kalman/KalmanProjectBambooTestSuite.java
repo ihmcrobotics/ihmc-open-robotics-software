492f97c2ed848729b4b1e095424f63777133109f
Duncan Calvert <dcalvert@ihmc.us>
2014-12-10 23:35:53 +0000

    GFE-156: Formatting test suites. Won't fixBamboo.

--- KalmanProject/test/us/ihmc/kalman/KalmanProjectBambooTestSuite.java
+++ KalmanProject/test/us/ihmc/kalman/KalmanProjectBambooTestSuite.java
@@ -1,23 +1,20 @@
 package us.ihmc.kalman;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
 import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-{
-us.ihmc.kalman.YoKalmanFilterTest.class
-}
-)
+@Suite.SuiteClasses
+({
+   us.ihmc.kalman.YoKalmanFilterTest.class
+})
 
 public class KalmanProjectBambooTestSuite
 {
-public static void main(String[] args)
-{
-   String packageName = "us.ihmc.kalman";
-   System.out.println(JUnitTestSuiteConstructor.createTestSuite("KalmanProjectBambooTestSuite", packageName));
+   public static void main(String[] args)
+   {
+      JUnitTestSuiteConstructor.generateTestSuite(KalmanProjectBambooTestSuite.class);
+   }
 }
-}
-
