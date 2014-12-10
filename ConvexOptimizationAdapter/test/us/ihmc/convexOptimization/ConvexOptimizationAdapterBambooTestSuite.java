9a8684bff88e6779913badc6de1bffc0171226bc
Duncan Calvert <dcalvert@ihmc.us>
2014-12-10 22:52:16 +0000

    GFE-156: Refactoring and formatting test suites. Won't fixBamboo.

--- ConvexOptimizationAdapter/test/us/ihmc/convexOptimization/ConvexOptimizationAdapterBambooTestSuite.java
+++ ConvexOptimizationAdapter/test/us/ihmc/convexOptimization/ConvexOptimizationAdapterBambooTestSuite.java
@@ -5,16 +5,15 @@ import org.junit.runners.Suite;
 import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-{
-//   us.ihmc.convexOptimization.quadraticProgram.GenericActiveSetQPSolverTest.class
+@Suite.SuiteClasses
+({
+   
 })
 
 public class ConvexOptimizationAdapterBambooTestSuite
 {
    public static void main(String[] args)
    {
-      String packageName = "us.ihmc.ConvexOptimizationAdapter";
-      System.out.println(JUnitTestSuiteConstructor.createTestSuite("ConvexOptimizationAdapterBambooTestSuite", packageName));
+      JUnitTestSuiteConstructor.generateTestSuite(ConvexOptimizationAdapterBambooTestSuite.class);
    }
 }
