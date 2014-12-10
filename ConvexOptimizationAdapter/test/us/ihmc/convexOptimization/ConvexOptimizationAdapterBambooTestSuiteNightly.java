f94be73139d1189f2e1e6cb403d44041459ca53f
Duncan Calvert <dcalvert@ihmc.us>
2014-12-10 23:49:53 +0000

    GFE-156: Refactoring and formatting test suites. Won't fixBamboo.

--- ConvexOptimizationAdapter/test/us/ihmc/convexOptimization/ConvexOptimizationAdapterBambooTestSuiteNightly.java
+++ ConvexOptimizationAdapter/test/us/ihmc/convexOptimization/ConvexOptimizationAdapterBambooTestSuiteNightly.java
@@ -8,12 +8,12 @@ import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.convexOptimization.experimental.ExperimentalSOCPSolverUsingJOptimizerTest.class,
-   us.ihmc.convexOptimization.jOptimizer.JOptimizerConvexOptimizationAdapterTest.class,
-   us.ihmc.convexOptimization.jOptimizer.SimpleJOptimizerTest.class,
+//   us.ihmc.convexOptimization.experimental.ExperimentalSOCPSolverUsingJOptimizerTest.class,
+//   us.ihmc.convexOptimization.jOptimizer.JOptimizerConvexOptimizationAdapterTest.class,
+//   us.ihmc.convexOptimization.jOptimizer.SimpleJOptimizerTest.class,
    us.ihmc.convexOptimization.quadraticProgram.GenericActiveSetQPSolverTest.class,
-   us.ihmc.convexOptimization.quadraticProgram.SimpleActiveSetQPSolverTest.class,
-   us.ihmc.convexOptimization.randomSearch.RandomSearchConvexOptimizationAdapterTest.class
+//   us.ihmc.convexOptimization.quadraticProgram.SimpleActiveSetQPSolverTest.class,
+//   us.ihmc.convexOptimization.randomSearch.RandomSearchConvexOptimizationAdapterTest.class
 })
 
 public class ConvexOptimizationAdapterBambooTestSuiteNightly
--- IHMCROSTools/test/us/ihmc/utilities/ros/IHMCRosToolsBambooTestSuite.java
+++ IHMCROSTools/test/us/ihmc/utilities/ros/IHMCRosToolsBambooTestSuite.java
@@ -1,24 +1,21 @@
 package us.ihmc.utilities.ros;
 
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
 import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-{
- us.ihmc.utilities.ros.ROSMessageConverterTest.class
-}
-)
+@Suite.SuiteClasses
+({
+   us.ihmc.utilities.ros.ROSMessageConverterTest.class,
+//   us.ihmc.utilities.ros.RosServiceClientTest.class
+})
 
 public class IHMCRosToolsBambooTestSuite
 {
-public static void main(String[] args)
-{
-      String packageName = "us.ihmc.utilities.ros";
-      System.out.println(JUnitTestSuiteConstructor.createTestSuite("IHMCRosToolsBambooTestSuite", packageName));
+   public static void main(String[] args)
+   {
+      JUnitTestSuiteConstructor.generateTestSuite(IHMCRosToolsBambooTestSuite.class);
+   }
 }
-}
-
