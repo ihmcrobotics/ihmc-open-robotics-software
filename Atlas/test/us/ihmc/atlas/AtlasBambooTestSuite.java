ec56d13baef251525ab2713470a463eb1011a914
Duncan Calvert <dcalvert@ihmc.us>
2014-12-10 22:33:26 +0000

    GFE-156: Refactoring and formatting test suites. Won't fixBamboo.

--- Atlas/test/us/ihmc/atlas/AtlasBambooTestSuite.java
+++ Atlas/test/us/ihmc/atlas/AtlasBambooTestSuite.java
@@ -3,34 +3,25 @@ package us.ihmc.atlas;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import us.ihmc.atlas.controllers.AtlasFootstepGeneratorTest;
-import us.ihmc.atlas.momentumBasedControl.AtlasOptimizationMomentumControlModuleTest;
-import us.ihmc.atlas.networkProcessor.depthData.AtlasDepthDataProcessorTest;
-import us.ihmc.atlas.networkProcessor.downlinkSerializers.AtlasJointConfigurationDataSerializerTest;
-import us.ihmc.atlas.networking.AtlasNetworkingCommandReaderTest;
-import us.ihmc.atlas.posePlayback.AtlasPlaybackPoseSequenceTest;
-import us.ihmc.atlas.utilities.kinematics.AtlasNumericalInverseKinematicsCalculatorWithRobotTest;
 import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   AtlasFootstepGeneratorTest.class, 
-   AtlasOptimizationMomentumControlModuleTest.class,
-   AtlasNetworkingCommandReaderTest.class,
-   AtlasNumericalInverseKinematicsCalculatorWithRobotTest.class,
-   AtlasMultiContactTest.class,
-   AtlasFlatGroundWalkingTest.class,
-   AtlasJointConfigurationDataSerializerTest.class,
-   AtlasPlaybackPoseSequenceTest.class,
-   AtlasDepthDataProcessorTest.class
+   us.ihmc.atlas.AtlasMultiContactTest.class,
+   us.ihmc.atlas.controllers.AtlasFootstepGeneratorTest.class,
+   us.ihmc.atlas.momentumBasedControl.AtlasOptimizationMomentumControlModuleTest.class,
+   us.ihmc.atlas.networking.AtlasNetworkingCommandReaderTest.class,
+   us.ihmc.atlas.networkProcessor.depthData.AtlasDepthDataProcessorTest.class,
+   us.ihmc.atlas.networkProcessor.downlinkSerializers.AtlasJointConfigurationDataSerializerTest.class,
+   us.ihmc.atlas.posePlayback.AtlasPlaybackPoseSequenceTest.class,
+   us.ihmc.atlas.utilities.kinematics.AtlasNumericalInverseKinematicsCalculatorWithRobotTest.class
 })
 
 public class AtlasBambooTestSuite
 {
    public static void main(String[] args)
    {
-      String packageName = "us.ihmc.atlas";
-      System.out.println(JUnitTestSuiteConstructor.createTestSuite("AtlasBambooTestSuite", packageName));
+      JUnitTestSuiteConstructor.generateTestSuite(AtlasBambooTestSuite.class);
    }
 }
