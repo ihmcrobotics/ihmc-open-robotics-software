52f1b0b3c2cc485deb574f76f9f01d62ae995c54
Duncan Calvert <dcalvert@ihmc.us>
2014-12-11 00:55:03 +0000

    GFE-156: Refactoring and formatting test suites. Won't fixBamboo.

--- test/us/ihmc/valkyrie/ValkyrieBambooTestSuite.java
+++ test/us/ihmc/valkyrie/ValkyrieBambooTestSuite.java
@@ -6,13 +6,14 @@ import org.junit.runners.Suite;
 import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-{
-   us.ihmc.valkyrie.ValkyrieFlatGroundWalkingTest.class,
+@Suite.SuiteClasses
+({
    us.ihmc.valkyrie.simulation.ValkyriePosePlaybackDemoTest.class,
    us.ihmc.valkyrie.kinematics.transmissions.InefficientPushrodTransmissionJacobianTest.class,
    us.ihmc.valkyrie.kinematics.transmissions.InefficientPushRodTransmissionTest.class,
+   us.ihmc.valkyrie.kinematics.PushrodTransmissionTest.class,
    us.ihmc.valkyrie.kinematics.ClosedFormJacobianTest.class,
+   us.ihmc.valkyrie.codeGenerators.APIBuilderTest.class,
    us.ihmc.valkyrie.kinematics.transmissions.ComparePushRodTransmissionsTest.class,
 //   us.ihmc.valkyrie.networkProcessor.depthData.ValkyrieDepthDataProcessorTest.class, // Feature not yet implemented @dcalvert
 })
@@ -21,7 +22,6 @@ public class ValkyrieBambooTestSuite
 {
    public static void main(String[] args)
    {
-      String packageName = "us.ihmc.valkyrie";
-      System.out.println(JUnitTestSuiteConstructor.createTestSuite("ValkyrieBambooTestSuite", packageName));
+      JUnitTestSuiteConstructor.generateTestSuite(ValkyrieBambooTestSuite.class);
    }
 }
