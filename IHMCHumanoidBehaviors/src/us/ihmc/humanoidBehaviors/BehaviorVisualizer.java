4ea940324fce7f18342bcaf1bc02ab00b0361768
Nicolas Eyssette <neyssette@ihmc.us>
2014-12-12 16:25:18 +0000

    NONE: added a main to the BehaviorVisualizer so that we can use it. does not fixbamboo

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/BehaviorVisualizer.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/BehaviorVisualizer.java
@@ -7,11 +7,22 @@ public class BehaviorVisualizer extends SCSVisualizer
 {
    private final boolean showOverheadView = false;
    
+   private final static boolean onRealRobot = false;
+   
    public BehaviorVisualizer(String host, int bufferSize)
    {
       super(bufferSize, true, false);
       
       YoVariableClient client = new YoVariableClient(host, this, "behavior", showOverheadView);
       client.start();
+   }
+   
+   public static void main(String[] args)
+   {
+      if (!onRealRobot)
+         new BehaviorVisualizer("127.0.0.1", 1024*32);
+      else
+         new BehaviorVisualizer("10.66.171.20", 1024*32);
+         
    }
 }
