331f435a267fdc1611c567fb8b487b86e6e6d36c
Atlas Control Computer <AtlasControlComputer@unknown>
2014-12-12 22:06:24 +0000

    NONE: format StepprOutputWriter, not fixbamboo

--- Acsell/src/us/ihmc/steppr/hardware/output/StepprOutputWriter.java
+++ Acsell/src/us/ihmc/steppr/hardware/output/StepprOutputWriter.java
@@ -33,13 +33,12 @@ public class StepprOutputWriter implements DRCOutputWriter
 
    private boolean outputEnabled;
    private final BooleanYoVariable enableOutput = new BooleanYoVariable("enableOutput", registry);
-   
-   private RawJointSensorDataHolderMap rawJointSensorDataHolderMap;
-   
-   private final UDPStepprOutputWriter outputWriter;
-   private final EnumMap<StepprJoint,DoubleYoVariable> yoTauSpring = new EnumMap<StepprJoint, DoubleYoVariable>(StepprJoint.class); 
 
-   
+   private RawJointSensorDataHolderMap rawJointSensorDataHolderMap;
+
+   private final UDPStepprOutputWriter outputWriter;
+   private final EnumMap<StepprJoint, DoubleYoVariable> yoTauSpring = new EnumMap<StepprJoint, DoubleYoVariable>(StepprJoint.class);
+
    public StepprOutputWriter(DRCRobotModel robotModel)
    {
 
@@ -47,16 +46,15 @@ public class StepprOutputWriter implements DRCOutputWriter
       standPrepJoints = StepprUtil.createJointMap(standPrepFullRobotModel.getOneDoFJoints());
 
       outputWriter = new UDPStepprOutputWriter(command);
-      
+
       standPrep.setFullRobotModel(standPrepFullRobotModel);
       registry.addChild(standPrep.getYoVariableRegistry());
-      
-      outputWriter.connect();
-      
 
-     yoTauSpring.put(StepprJoint.LEFT_HIP_X, new DoubleYoVariable(StepprJoint.LEFT_HIP_X.getSdfName()+"_tauSpringCorrection", registry));
-     yoTauSpring.put(StepprJoint.RIGHT_HIP_X, new DoubleYoVariable(StepprJoint.RIGHT_HIP_X.getSdfName()+"_tauSpringCorrection", registry));
-      
+      outputWriter.connect();
+
+      yoTauSpring.put(StepprJoint.LEFT_HIP_X, new DoubleYoVariable(StepprJoint.LEFT_HIP_X.getSdfName() + "_tauSpringCorrection", registry));
+      yoTauSpring.put(StepprJoint.RIGHT_HIP_X, new DoubleYoVariable(StepprJoint.RIGHT_HIP_X.getSdfName() + "_tauSpringCorrection", registry));
+
    }
 
    @Override
@@ -89,49 +87,47 @@ public class StepprOutputWriter implements DRCOutputWriter
             standPrepJoint.setQ(rawSensorData.getQ_raw());
             standPrepJoint.setQd(rawSensorData.getQd_raw());
          }
-         
+
          standPrep.doControl(timestamp);
-         
+
          for (StepprJoint joint : StepprJoint.values)
          {
             OneDoFJoint wholeBodyControlJoint = wholeBodyControlJoints.get(joint);
             OneDoFJoint standPrepJoint = standPrepJoints.get(joint);
             RawJointSensorDataHolder rawSensorData = rawJointSensorDataHolderMap.get(wholeBodyControlJoint);
-            
-            double tau = wholeBodyControlJoint.getTau() * controlRatio.getDoubleValue() + standPrepJoint.getTau()
-                  * (1.0 - controlRatio.getDoubleValue());
-            
-            
-            double kd =  wholeBodyControlJoint.getKd() * controlRatio.getDoubleValue() + standPrepJoint.getKd()
-                  * (1.0 - controlRatio.getDoubleValue());
-            
+
+            double tau = wholeBodyControlJoint.getTau() * controlRatio.getDoubleValue() + standPrepJoint.getTau() * (1.0 - controlRatio.getDoubleValue());
+
+            double kd = wholeBodyControlJoint.getKd() * controlRatio.getDoubleValue() + standPrepJoint.getKd() * (1.0 - controlRatio.getDoubleValue());
+
             StepprJointCommand jointCommand = command.getStepprJointCommand(joint);
-            
-            double tauSpring =0;
-            if (yoTauSpring.get(joint)!=null)
+
+            double tauSpring = 0;
+            if (yoTauSpring.get(joint) != null)
             {
                tauSpring = calcSpringTorque(joint, wholeBodyControlJoint.getQ());
-               yoTauSpring.get(joint).set(tauSpring);               
+               yoTauSpring.get(joint).set(tauSpring);
             }
-            
+
+//            jointCommand.setTauDesired(tau-tauSpring, rawSensorData); //use when Springs are installed
             jointCommand.setTauDesired(tau, rawSensorData);
             jointCommand.setDamping(kd);
 
          }
       }
-      
+
       outputWriter.write();
 
    }
-   
+
    private double calcSpringTorque(StepprJoint joint, double q)
    {
-      switch(joint)
+      switch (joint)
       {
       case LEFT_HIP_X:
-         return 550*Math.max(-0.05-q, 0);
+         return 550 * Math.max(-0.05 - q, 0);
       case RIGHT_HIP_X:
-         return -550*Math.max(q-0.05, 0);
+         return -550 * Math.max(q - 0.05, 0);
       default:
          return 0;
       }
@@ -147,7 +143,7 @@ public class StepprOutputWriter implements DRCOutputWriter
    @Override
    public void setForceSensorDataHolderForController(ForceSensorDataHolder forceSensorDataHolderForController)
    {
-      
+
    }
 
    @Override
