e08958255747ad0ffff913ae57d36bc4dc623718
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2014-12-13 01:07:12 +0000

    drc-2038, medium size refactor of moving network stuff into ihmc communication. Might fixbamboo as it was a flaky test

--- src/us/ihmc/valkyrie/sensors/ValkyrieSensorSuiteManager.java
+++ src/us/ihmc/valkyrie/sensors/ValkyrieSensorSuiteManager.java
@@ -4,6 +4,8 @@ import java.net.URI;
 
 import us.ihmc.SdfLoader.SDFFullRobotModel;
 import us.ihmc.communication.AbstractNetworkProcessorNetworkingManager;
+import us.ihmc.communication.net.LocalObjectCommunicator;
+import us.ihmc.communication.net.ObjectCommunicator;
 import us.ihmc.communication.packets.sensing.CameraInformationPacket;
 import us.ihmc.communication.producers.RobotPoseBuffer;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.camera.CameraInfoReceiver;
@@ -17,8 +19,6 @@ import us.ihmc.ihmcPerception.depthData.DepthDataFilter;
 import us.ihmc.ros.jni.wrapper.RosNativeNetworkProcessor;
 import us.ihmc.sensorProcessing.parameters.DRCRobotCameraParameters;
 import us.ihmc.sensorProcessing.parameters.DRCRobotSensorInformation;
-import us.ihmc.utilities.net.LocalObjectCommunicator;
-import us.ihmc.utilities.net.ObjectCommunicator;
 import us.ihmc.utilities.ros.PPSTimestampOffsetProvider;
 import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.valkyrie.parameters.ValkyrieSensorInformation;
