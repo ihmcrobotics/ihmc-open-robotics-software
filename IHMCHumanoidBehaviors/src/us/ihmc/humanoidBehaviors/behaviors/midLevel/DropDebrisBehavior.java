c15aa34f844c6bdb39337b82270d8398b4b95573
Nicolas Eyssette <neyssette@ihmc.us>
2014-12-16 22:47:44 +0000

    DRC-1902: update of the DropDebrisBehavior and GraspObjectBehavior so that they now use the taskExecutor. wont fixbamboo

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/midLevel/DropDebrisBehavior.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/midLevel/DropDebrisBehavior.java
@@ -1,14 +1,15 @@
 package us.ihmc.humanoidBehaviors.behaviors.midLevel;
 
-import java.util.ArrayList;
-
 import javax.vecmath.Quat4d;
 
+import us.ihmc.communication.packets.dataobjects.FingerState;
 import us.ihmc.communication.packets.manipulation.HandPosePacket.Frame;
 import us.ihmc.communication.util.PacketControllerTools;
 import us.ihmc.humanoidBehaviors.behaviors.BehaviorInterface;
 import us.ihmc.humanoidBehaviors.behaviors.primitives.HandPoseBehavior;
 import us.ihmc.humanoidBehaviors.communication.OutgoingCommunicationBridgeInterface;
+import us.ihmc.humanoidBehaviors.taskExecutor.FingerStateTask;
+import us.ihmc.humanoidBehaviors.taskExecutor.HandPoseTask;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 import us.ihmc.utilities.math.geometry.FramePose;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
@@ -16,6 +17,7 @@ import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 import us.ihmc.utilities.math.geometry.RotationFunctions;
 import us.ihmc.utilities.robotSide.RobotSide;
 import us.ihmc.utilities.robotSide.SideDependentList;
+import us.ihmc.utilities.taskExecutor.TaskExecutor;
 import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
 import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
 import us.ihmc.yoUtilities.dataStructure.variable.IntegerYoVariable;
@@ -23,14 +25,14 @@ import us.ihmc.yoUtilities.dataStructure.variable.IntegerYoVariable;
 public class DropDebrisBehavior extends BehaviorInterface
 {
    private final FullRobotModel fullRobotModel;
-   private BehaviorInterface currentBehavior;
-   private final ArrayList<BehaviorInterface> behaviors = new ArrayList<BehaviorInterface>();
+
+   private final TaskExecutor taskExecutor = new TaskExecutor();
+
    private final HandPoseBehavior handPoseBehavior;
-   private final OpenHandBehavior openHandBehavior;
+   private final FingerStateBehavior fingerStateBehavior;
 
    private final BooleanYoVariable isDone;
    private final BooleanYoVariable haveInputsBeenSet;
-   private final BooleanYoVariable reachedMidPoint;
    private final IntegerYoVariable behaviorIndex;
    private final DoubleYoVariable zHeightOffsetFromChestFrameForMidPoint;
 
@@ -41,29 +43,30 @@ public class DropDebrisBehavior extends BehaviorInterface
 
    private double trajectoryTime = 3.5;
 
-   private final RigidBodyTransform[] posesForHandPoseBehavior = new RigidBodyTransform[4];
-
    private final FramePose midPose = new FramePose();
    private final FramePose dropLocationPose = new FramePose();
+
+   private final DoubleYoVariable yoTime;
+
 
    public DropDebrisBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridge, FullRobotModel fullRobotModel, DoubleYoVariable yoTime)
    {
       super(outgoingCommunicationBridge);
       this.fullRobotModel = fullRobotModel;
+      this.yoTime = yoTime;
       handPoseBehavior = new HandPoseBehavior(outgoingCommunicationBridge, yoTime);
-      openHandBehavior = new OpenHandBehavior(outgoingCommunicationBridge);
-
+      fingerStateBehavior = new FingerStateBehavior(outgoingCommunicationBridge, yoTime);
+      
       zHeightOffsetFromChestFrameForMidPoint = new DoubleYoVariable("zHeightOffsetFromChestFrameForMidPoint", registry);
       zHeightOffsetFromChestFrameForMidPoint.set(0.27);
 
       haveInputsBeenSet = new BooleanYoVariable("haveInputsBeenSet", registry);
-      reachedMidPoint = new BooleanYoVariable("reachedMidPoint", registry);
       behaviorIndex = new IntegerYoVariable("behaviorIndex", registry);
       isDone = new BooleanYoVariable("isDone", registry);
 
       handReferenceFrames.put(RobotSide.LEFT, fullRobotModel.getHandControlFrame(RobotSide.LEFT));
       handReferenceFrames.put(RobotSide.RIGHT, fullRobotModel.getHandControlFrame(RobotSide.RIGHT));
-
+      
       chestFrame = fullRobotModel.getChest().getBodyFixedFrame();
    }
 
@@ -75,36 +78,39 @@ public class DropDebrisBehavior extends BehaviorInterface
 
    private void getDropLocation(RobotSide side)
    {
-      Quat4d leftDropRotation = new Quat4d();
+      Quat4d dropRotation = new Quat4d();
       if (side == RobotSide.LEFT)
       {
          dropLocationPose.translate(0.3, 0.7, -0.1);
-         RotationFunctions.setQuaternionBasedOnYawPitchRoll(leftDropRotation, -Math.PI / 2, 0, 0);
+         RotationFunctions.setQuaternionBasedOnYawPitchRoll(dropRotation, -Math.PI / 2, Math.PI/2, 0);
       }
       else
       {
          dropLocationPose.translate(0.3, -0.7, -0.1);
-         RotationFunctions.setQuaternionBasedOnYawPitchRoll(leftDropRotation, Math.PI / 2, 0, 0);
+         RotationFunctions.setQuaternionBasedOnYawPitchRoll(dropRotation, Math.PI / 2, Math.PI/2, 0);
       }
-      dropLocationPose.setOrientation(leftDropRotation);
+      dropLocationPose.setOrientation(dropRotation);
       dropLocationPose.changeFrame(ReferenceFrame.getWorldFrame());
    }
 
    private void setPoses(RobotSide side)
    {
+      RigidBodyTransform tempPose = new RigidBodyTransform();
+
       initializePoses(side);
+
       getMidPose();
-      posesForHandPoseBehavior[0] = new RigidBodyTransform();
-      midPose.getPose(posesForHandPoseBehavior[0]);
-
+      midPose.getPose(tempPose);
+      taskExecutor.submit(new HandPoseTask(side, yoTime, handPoseBehavior, Frame.WORLD, tempPose, trajectoryTime));
+                  
       getDropLocation(side);
-      posesForHandPoseBehavior[1] = new RigidBodyTransform();
-      dropLocationPose.getPose(posesForHandPoseBehavior[1]);
+      dropLocationPose.getPose(tempPose);
+      taskExecutor.submit(new HandPoseTask(side, yoTime, handPoseBehavior, Frame.WORLD, tempPose, trajectoryTime));
 
-      posesForHandPoseBehavior[2] = null;
-      posesForHandPoseBehavior[3] = null;
+      taskExecutor.submit(new FingerStateTask(robotSide, FingerState.OPEN, fingerStateBehavior));
+      
+      taskExecutor.submit(new HandPoseTask(PacketControllerTools.createGoToHomePacket(side, 3.0), handPoseBehavior, yoTime));
 
-      handPoseBehavior.setInput(Frame.CHEST, posesForHandPoseBehavior[0], robotSide, trajectoryTime);
    }
 
    private void initializePoses(RobotSide side)
@@ -119,11 +125,7 @@ public class DropDebrisBehavior extends BehaviorInterface
    @Override
    public void doControl()
    {
-      if (!isDone.getBooleanValue() && currentBehavior != null)
-      {
-         checkTransitionCondition();
-         currentBehavior.doControl();
-      }
+      taskExecutor.doControl();
    }
 
    public void setInputs(RobotSide side)
@@ -133,103 +135,61 @@ public class DropDebrisBehavior extends BehaviorInterface
       haveInputsBeenSet.set(true);
    }
 
-   private void checkTransitionCondition()
-   {
-      if (currentBehavior.isDone())
-      {
-         currentBehavior.finalize();
-
-         if (!behaviors.isEmpty())
-         {
-            currentBehavior = behaviors.remove(0);
-            behaviorIndex.add(1);
-            currentBehavior.initialize();
-
-            if (posesForHandPoseBehavior[behaviorIndex.getIntegerValue()] != null)
-            {
-               handPoseBehavior.setInput(Frame.CHEST, posesForHandPoseBehavior[behaviorIndex.getIntegerValue()], robotSide, trajectoryTime);
-            }
-
-            if (behaviors.size() == 1)
-            {
-               handPoseBehavior.setInput(PacketControllerTools.createGoToHomePacket(robotSide, trajectoryTime));
-            }
-         }
-         else
-         {
-            isDone.set(true);
-         }
-      }
-   }
-
    @Override
    protected void passReceivedNetworkProcessorObjectToChildBehaviors(Object object)
    {
-      if (currentBehavior != null)
-         currentBehavior.consumeObjectFromNetworkProcessor(object);
+      handPoseBehavior.consumeObjectFromNetworkProcessor(object);
    }
 
    @Override
    protected void passReceivedControllerObjectToChildBehaviors(Object object)
    {
-      if (currentBehavior != null)
-         currentBehavior.consumeObjectFromController(object);
+      handPoseBehavior.consumeObjectFromController(object);
    }
 
    @Override
    public void stop()
    {
-      currentBehavior.stop();
+      handPoseBehavior.stop();
    }
 
    @Override
    public void enableActions()
    {
-      currentBehavior.enableActions();
+      handPoseBehavior.enableActions();
    }
 
    @Override
    public void pause()
    {
-      currentBehavior.pause();
+      handPoseBehavior.pause();
    }
 
    @Override
    public void resume()
    {
-      currentBehavior.resume();
+      handPoseBehavior.resume();
    }
 
    @Override
    public boolean isDone()
    {
-      return isDone.getBooleanValue();
+      return taskExecutor.isDone();
    }
 
    @Override
    public void finalize()
    {
-      currentBehavior = null;
-      behaviors.clear();
       haveInputsBeenSet.set(false);
       isDone.set(false);
-      reachedMidPoint.set(false);
       behaviorIndex.set(-1);
    }
 
    @Override
    public void initialize()
    {
-      behaviors.clear();
-      behaviors.add(handPoseBehavior);
-      behaviors.add(handPoseBehavior);
-      behaviors.add(openHandBehavior);
-      behaviors.add(handPoseBehavior);
-
-      currentBehavior = behaviors.remove(0);
       haveInputsBeenSet.set(false);
       isDone.set(false);
-      reachedMidPoint.set(false);
       behaviorIndex.set(0);
    }
 
@@ -238,5 +198,4 @@ public class DropDebrisBehavior extends BehaviorInterface
    {
       return haveInputsBeenSet.getBooleanValue();
    }
-
 }
