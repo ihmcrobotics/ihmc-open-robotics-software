7904831ec801571ae231608345619fc5b9cf7817
Nicolas Eyssette <neyssette@ihmc.us>
2014-12-16 22:46:43 +0000

    DRC-1902: Removed CloseHandBehavior and OpenHandBehavior and created the FingerStateBehavior instead. also created the FingerStateTask. wont fixbamboo

---
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/midLevel/FingerStateBehavior.java
@@ -0,0 +1,160 @@
+package us.ihmc.humanoidBehaviors.behaviors.midLevel;
+
+import us.ihmc.communication.packets.PacketDestination;
+import us.ihmc.communication.packets.dataobjects.FingerState;
+import us.ihmc.communication.packets.manipulation.FingerStatePacket;
+import us.ihmc.humanoidBehaviors.behaviors.BehaviorInterface;
+import us.ihmc.humanoidBehaviors.communication.OutgoingCommunicationBridgeInterface;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+
+public class FingerStateBehavior extends BehaviorInterface
+{
+   private FingerStatePacket outgoingFingerStatePacket;
+   private final BooleanYoVariable hasInputBeenSet;
+   private final BooleanYoVariable hasPacketBeenSet;
+
+   private final DoubleYoVariable yoTime;
+   private final DoubleYoVariable startTime;
+   private final DoubleYoVariable trajectoryTime; // hardcoded, to be determined
+   private final BooleanYoVariable trajectoryTimeElapsed;
+
+   public FingerStateBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridgeInterface, DoubleYoVariable yoTime)
+   {
+      super(outgoingCommunicationBridgeInterface);
+      this.yoTime = yoTime;
+
+      hasInputBeenSet = new BooleanYoVariable(getName() + "hasInputBeenSet", registry);
+      hasPacketBeenSet = new BooleanYoVariable(getName() + "hasPacketBeenSet", registry);
+
+      startTime = new DoubleYoVariable(getName() + "StartTime", registry);
+      startTime.set(Double.NaN);
+      trajectoryTime = new DoubleYoVariable(getName() + "TrajectoryTime", registry);
+      trajectoryTime.set(Double.NaN);
+
+      trajectoryTimeElapsed = new BooleanYoVariable(getName() + "TrajectoryTimeElapsed", registry);
+   }
+
+   public void setInput(FingerStatePacket fingerStatePacket)
+   {
+      this.outgoingFingerStatePacket = fingerStatePacket;
+   }
+
+   @Override
+   public void doControl()
+   {
+      if (!hasPacketBeenSet.getBooleanValue() && outgoingFingerStatePacket != null)
+         sendFingerStateToController();
+   }
+
+   private void sendFingerStateToController()
+   {
+      if (!isPaused.getBooleanValue() && !isStopped.getBooleanValue())
+      {
+         outgoingFingerStatePacket.setDestination(PacketDestination.UI);
+
+         sendPacketToController(outgoingFingerStatePacket);
+         sendPacketToNetworkProcessor(outgoingFingerStatePacket);
+         hasPacketBeenSet.set(true);
+         startTime.set(yoTime.getDoubleValue());
+      }
+   }
+
+   @Override
+   protected void passReceivedNetworkProcessorObjectToChildBehaviors(Object object)
+   {
+   }
+
+   @Override
+   protected void passReceivedControllerObjectToChildBehaviors(Object object)
+   {
+   }
+
+   @Override
+   public void stop()
+   {
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         FingerStatePacket stopFingerStatePacket = new FingerStatePacket(robotSide, FingerState.STOP);
+         stopFingerStatePacket.setDestination(PacketDestination.UI);
+         sendPacketToController(stopFingerStatePacket);
+         sendPacketToNetworkProcessor(stopFingerStatePacket);
+      }
+      isStopped.set(true);
+   }
+
+   @Override
+   public void enableActions()
+   {
+   }
+
+   @Override
+   public void pause()
+   {
+      for (RobotSide robotSide : RobotSide.values())
+      {
+         FingerStatePacket stopFingerStatePacket = new FingerStatePacket(robotSide, FingerState.STOP);
+         stopFingerStatePacket.setDestination(PacketDestination.UI);
+         sendPacketToController(stopFingerStatePacket);
+         sendPacketToNetworkProcessor(stopFingerStatePacket);
+      }
+      isPaused.set(true);
+   }
+
+   @Override
+   public void resume()
+   {
+      isPaused.set(false);
+      hasPacketBeenSet.set(false);
+      if (hasInputBeenSet())
+      {
+         sendFingerStateToController();
+      }
+   }
+
+   @Override
+   public boolean isDone()
+   {
+      if (Double.isNaN(startTime.getDoubleValue()) || Double.isNaN(trajectoryTime.getDoubleValue()))
+         trajectoryTimeElapsed.set(false);
+      else
+         trajectoryTimeElapsed.set(yoTime.getDoubleValue() - startTime.getDoubleValue() > trajectoryTime.getDoubleValue());
+
+      return trajectoryTimeElapsed.getBooleanValue() && !isPaused.getBooleanValue();
+   }
+
+   @Override
+   public void initialize()
+   {
+      hasInputBeenSet.set(false);
+      hasPacketBeenSet.set(false);
+      outgoingFingerStatePacket = null;
+      isPaused.set(false);
+      isStopped.set(false);
+      trajectoryTime.set(2.5); //hardCoded to be determined
+      
+      
+      trajectoryTimeElapsed.set(false);
+   }
+
+   @Override
+   public void finalize()
+   {
+      hasInputBeenSet.set(false);
+      hasPacketBeenSet.set(false);
+      outgoingFingerStatePacket = null;
+      isPaused.set(false);
+      isStopped.set(false);
+
+      trajectoryTime.set(Double.NaN);
+      startTime.set(Double.NaN);
+      trajectoryTimeElapsed.set(false);
+   }
+
+   @Override
+   public boolean hasInputBeenSet()
+   {
+      return hasInputBeenSet.getBooleanValue();
+   }
+}
---
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/FingerStateTask.java
@@ -0,0 +1,47 @@
+package us.ihmc.humanoidBehaviors.taskExecutor;
+
+import us.ihmc.communication.packets.dataobjects.FingerState;
+import us.ihmc.communication.packets.manipulation.FingerStatePacket;
+import us.ihmc.humanoidBehaviors.behaviors.midLevel.FingerStateBehavior;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.utilities.taskExecutor.Task;
+
+public class FingerStateTask implements Task
+{
+   private final FingerStatePacket fingerStatePacket;
+   private final FingerStateBehavior fingerStateBehavior;
+
+   public FingerStateTask(RobotSide robotSide, FingerState fingerState,FingerStateBehavior fingerStateBehavior)
+   {
+      this.fingerStatePacket = new FingerStatePacket(robotSide, fingerState);
+
+      this.fingerStateBehavior = fingerStateBehavior;
+   }
+
+   @Override
+   public void doTransitionIntoAction()
+   {
+      System.out.println("started a finger Task");
+      fingerStateBehavior.initialize();
+      fingerStateBehavior.setInput(fingerStatePacket);
+   }
+
+   @Override
+   public void doAction()
+   {
+      fingerStateBehavior.doControl();
+   }
+
+   @Override
+   public void doTransitionOutOfAction()
+   {
+      System.out.println("finished a finger Task");
+      fingerStateBehavior.finalize();
+   }
+
+   @Override
+   public boolean isDone()
+   {
+      return fingerStateBehavior.isDone();
+   }
+}
