596d35e4ce000d8dd20ab6a7bc1dc019d5dc706e
Will Rifenburgh <wrifenburgh@ihmc.us>
2014-12-18 17:10:06 +0000

    DRC-1978: Updated setTarget functiongs to match Davides new definition for them in the IK solver. might fixbamboo

--- Atlas/src/us/ihmc/atlas/AtlasWholeBodyIKIngressEgressCtrlSim.java
+++ Atlas/src/us/ihmc/atlas/AtlasWholeBodyIKIngressEgressCtrlSim.java
@@ -9,13 +9,18 @@ import javax.vecmath.Point3d;
 import us.ihmc.SdfLoader.SDFFullRobotModel;
 import us.ihmc.atlas.AtlasRobotModel.AtlasTarget;
 import us.ihmc.communication.net.KryoLocalObjectCommunicator;
+import us.ihmc.communication.packets.dataobjects.HighLevelState;
 import us.ihmc.darpaRoboticsChallenge.WholeBodyIK.WholeBodyIKPacketCreator;
 import us.ihmc.darpaRoboticsChallenge.WholeBodyIK.WholeBodyIkSolver;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.utilities.RandomTools;
 import us.ihmc.utilities.ThreadTools;
+import us.ihmc.utilities.humanoidRobot.partNames.LimbName;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.yoUtilities.dataStructure.variable.EnumYoVariable;
+import us.ihmc.yoUtilities.dataStructure.variable.YoVariable;
 //import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
 //import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
 //import us.ihmc.yoUtilities.dataStructure.variable.YoVariable;
@@ -26,10 +31,10 @@ public class AtlasWholeBodyIKIngressEgressCtrlSim
 
    private final WholeBodyIkSolver wholeBodyIKSolver;
    private final WholeBodyIKPacketCreator wholeBodyIKPacketCreator;
-//   private final YoVariableRegistry registry;
-//   private DoubleYoVariable hik_arm_pos_x_d;
-//   private DoubleYoVariable hik_arm_pos_y_d;
-//   private DoubleYoVariable hik_arm_pos_z_d;
+   //   private final YoVariableRegistry registry;
+   //   private DoubleYoVariable hik_arm_pos_x_d;
+   //   private DoubleYoVariable hik_arm_pos_y_d;
+   //   private DoubleYoVariable hik_arm_pos_z_d;
    private final SDFFullRobotModel fullRobotModel;
    private final KryoLocalObjectCommunicator fieldObjectCommunicator;
    private ArrayList<Object> packetsToSend = new ArrayList<Object>();
@@ -40,17 +45,25 @@ public class AtlasWholeBodyIKIngressEgressCtrlSim
       DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasTarget.SIM, false);
 
       this.hikIngEgCtrlSim = new WholeBodyIKIngressEgressControllerSimulation(robotModel);
-      this.hikIngEgCtrlSim.getDRCSimulation().start();
+      hikIngEgCtrlSim.getDRCSimulation().start();
       this.fullRobotModel = robotModel.createFullRobotModel();
-//      this.registry = ikExample.getControllerFactory().getRegistry();
+      //      this.registry = ikExample.getControllerFactory().getRegistry();
       this.fieldObjectCommunicator = hikIngEgCtrlSim.getKryoLocalObjectCommunicator();
       this.wholeBodyIKSolver = new WholeBodyIkSolver(robotModel, fullRobotModel, "models/atlas_v4_dual.urdf");
       wholeBodyIKSolver.enableHandRotAndTranslation(RobotSide.LEFT, false);
       this.wholeBodyIKPacketCreator = new WholeBodyIKPacketCreator(robotModel, fullRobotModel);
       while (true)
       {
-         ThreadTools.sleep(1000);
-         doControl();
+         if (ingressEgressModeActivated())
+         {
+
+            ThreadTools.sleep(10000);
+            //         Random random = new Random();
+            //         ComHeightPacket chp = new ComHeightPacket(random);
+            //         System.out.println("Sending random ComHeightPacket.");
+            //         fieldObjectCommunicator.consumeObject(chp);
+            doControl();
+         }
       }
    }
 
@@ -62,19 +75,46 @@ public class AtlasWholeBodyIKIngressEgressCtrlSim
    private void doControl()
    {
       Random rand = new Random();
-      Point3d randomPoint = RandomTools.generateRandomPoint(rand, -0.5, -1.0, -1.0, 0.75, 1.0, 1.0);
-      RigidBodyTransform desiredToWorldTransform = new RigidBodyTransform();
-      desiredToWorldTransform.setTranslation(randomPoint.getX(), randomPoint.getY(), randomPoint.getZ());
-//      desiredToWorldTransform.setTranslation(rand.nextDouble(), rand.nextDouble(), rand.nextDouble());
+      Point3d randomPoint = RandomTools.generateRandomPoint(rand, -0.2, -0.2, 0.2, 0.2, 1.0, 1.5);
+      ReferenceFrame desiredReferenceFrame = fullRobotModel.getEndEffector(RobotSide.RIGHT, LimbName.ARM).getBodyFixedFrame();
+//      desiredToWorldTransform.setTranslation(randomPoint.getX(), randomPoint.getY(), randomPoint.getZ());
+      //      desiredToWorldTransform.setTranslation(0.0, 0.0, 1.0);
+      //      desiredToWorldTransform.setTranslation(rand.nextDouble(), rand.nextDouble(), rand.nextDouble());
       //      desiredToWorldTransform.setTranslation(hik_arm_pos_x_d.getDoubleValue(), hik_arm_pos_y_d.getDoubleValue(), hik_arm_pos_z_d.getDoubleValue());
       //      System.out.println(hik_arm_pos_x_d.getDoubleValue() + ", " + hik_arm_pos_y_d.getDoubleValue() + ", " + hik_arm_pos_z_d.getDoubleValue());
-      wholeBodyIKSolver.setTarget(RobotSide.RIGHT, desiredToWorldTransform);
+      wholeBodyIKSolver.setTarget(RobotSide.LEFT, desiredReferenceFrame);
       wholeBodyIKSolver.compute(fullRobotModel);
-      wholeBodyIKPacketCreator.createPackets(fullRobotModel, 1.0, packetsToSend);
+      wholeBodyIKPacketCreator.createPackets(fullRobotModel, 3.0, packetsToSend);
       for (int i = 0; i < packetsToSend.size(); i++)
       {
          fieldObjectCommunicator.consumeObject(packetsToSend.get(i));
       }
       packetsToSend.clear();
    }
+
+   private boolean ingressEgressModeActivated()
+   {
+      ArrayList<YoVariable<?>> yoVariables = hikIngEgCtrlSim.getDRCSimulation().getSimulationConstructionSet().getAllVariables();
+      boolean bool = false;
+      for (YoVariable<?> yoVariable : yoVariables)
+      {
+         if (yoVariable.getName() == "highLevelState")
+         {
+
+            @SuppressWarnings("unchecked")
+            EnumYoVariable<HighLevelState> enumYoVariable = (EnumYoVariable<HighLevelState>) yoVariable;
+            //            enumYoVariable.set(HighLevelState.INGRESS_EGRESS);
+            if (enumYoVariable.getEnumValue() == HighLevelState.INGRESS_EGRESS)
+            {
+               bool = true;
+            }
+            else
+            {
+               bool = false;
+            }
+         }
+      }
+      return bool;
+   }
+
 }
