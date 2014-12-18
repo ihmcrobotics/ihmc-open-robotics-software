b8353be1377727c6a432540344e109370133819f
Duncan Calvert <dcalvert@ihmc.us>
2014-12-18 01:47:09 +0000

    GFE-162: Moved unit test that hung into a new job called Flaky. fixBamboo.

--- test/us/ihmc/valkyrie/ValkyrieBambooValkyrieTestSuite.java
+++ test/us/ihmc/valkyrie/ValkyrieBambooValkyrieTestSuite.java
@@ -8,7 +8,7 @@ import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.valkyrie.simulation.ValkyriePosePlaybackDemoTest.class,
+//   us.ihmc.valkyrie.simulation.ValkyriePosePlaybackDemoTest.class, // Moved to flaky
    us.ihmc.valkyrie.kinematics.transmissions.InefficientPushrodTransmissionJacobianTest.class,
    us.ihmc.valkyrie.kinematics.transmissions.InefficientPushRodTransmissionTest.class,
    us.ihmc.valkyrie.kinematics.PushrodTransmissionTest.class,
