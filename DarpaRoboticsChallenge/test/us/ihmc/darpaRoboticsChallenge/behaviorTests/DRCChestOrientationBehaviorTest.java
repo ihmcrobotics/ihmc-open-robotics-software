3968ae9c4e018fce988651d3196ee72ef549a3e9
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-01-09 17:21:45 +0000

    NONE: Make sure that NetworkConfigParameters.USE_BEHAVIORS_MODULE = false.  Otherwise, controller doesn't receive behavior packets.  Is there a better fix for this...?  won't fixBamboo

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/behaviorTests/DRCChestOrientationBehaviorTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/behaviorTests/DRCChestOrientationBehaviorTest.java
@@ -21,6 +21,7 @@ import us.ihmc.communication.packetCommunicator.KryoLocalPacketCommunicator;
 import us.ihmc.communication.packets.dataobjects.RobotConfigurationData;
 import us.ihmc.communication.packets.walking.ChestOrientationPacket;
 import us.ihmc.communication.subscribers.RobotDataReceiver;
+import us.ihmc.communication.util.NetworkConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseStartingLocation;
 import us.ihmc.darpaRoboticsChallenge.MultiRobotTestInterface;
 import us.ihmc.darpaRoboticsChallenge.environment.DRCDemo01NavigationEnvironment;
@@ -71,6 +72,11 @@ public abstract class DRCChestOrientationBehaviorTest implements MultiRobotTestI
    @Before
    public void setUp()
    {
+      if (NetworkConfigParameters.USE_BEHAVIORS_MODULE)
+      {
+         throw new RuntimeException("Must set NetworkConfigParameters.USE_BEHAVIORS_MODULE = false in order to perform this test!");
+      }
+      
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
 
       drcSimulationTestHelper = new DRCSimulationTestHelper(testEnvironment, controllerCommunicator, getSimpleRobotName(), null,
