ae938f78d1376307111b43de637df1e82fb661ef
Jerry Pratt <jpratt@ihmc.us>
2015-01-09 16:31:58 +0000

    DRC-2161: Added ThinBuffer to DataBuffer menu in SCS.

---
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/commands/ThinBufferCommandExecutor.java
@@ -0,0 +1,8 @@
+package us.ihmc.simulationconstructionset.commands;
+
+public interface ThinBufferCommandExecutor
+{
+   public abstract void thinBuffer(int keepEveryNthPoint);
+}
+
+
---
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/gui/actions/ThinBufferAction.java
@@ -0,0 +1,30 @@
+package us.ihmc.simulationconstructionset.gui.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+import us.ihmc.simulationconstructionset.commands.ThinBufferCommandExecutor;
+
+public class ThinBufferAction extends AbstractAction
+{
+   private static final long serialVersionUID = 5502411154340525798L;
+   private ThinBufferCommandExecutor executor;
+
+   public ThinBufferAction(ThinBufferCommandExecutor executor)
+   {
+      super("Thin Buffer, Removing Every Other Tick");
+      this.executor = executor;
+
+      this.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_T));
+      this.putValue(Action.LONG_DESCRIPTION, "Long Description");
+      this.putValue(Action.SHORT_DESCRIPTION, "Short Description");
+   }
+
+   public void actionPerformed(ActionEvent actionEvent)
+   {
+      executor.thinBuffer(2);
+   }
+}
