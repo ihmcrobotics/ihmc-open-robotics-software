6b5a484efa7ecc99e49f98af578d1ea58898feb6
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-01-09 23:25:23 +0000

    DRC-2149: Tests for WalkToLocationBehavior().  FullRobotModel referenceFrames are not updating with actual robot for some reason.  Using robot ankle joints to get midFeet pose instead.  Won't fixBamboo

---
+++ Atlas/test/us/ihmc/atlas/behaviorTests/AtlasWalkToLocationBehaviorTest.java
@@ -0,0 +1,31 @@
+package us.ihmc.atlas.behaviorTests;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.bambooTools.BambooTools;
+import us.ihmc.darpaRoboticsChallenge.behaviorTests.DRCWalkToLocationBehaviorTest;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+
+public class AtlasWalkToLocationBehaviorTest extends DRCWalkToLocationBehaviorTest
+{
+   private final AtlasRobotModel robotModel;
+   
+   public AtlasWalkToLocationBehaviorTest()
+   {
+      robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false); 
+   }
+   
+   
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+
+}
