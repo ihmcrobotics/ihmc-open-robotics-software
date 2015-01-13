24d2785533e011801f3bdf263d650b25acd42c16
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-01-13 17:56:04 +0000

    DRC-2134: Added to nightly test suite, and noted things to do in TODO.  won't fixBamboo

--- Atlas/test/us/ihmc/atlas/AtlasBambooTestSuiteNightly.java
+++ Atlas/test/us/ihmc/atlas/AtlasBambooTestSuiteNightly.java
@@ -23,6 +23,7 @@ import us.ihmc.utilities.test.JUnitTestSuiteConstructor;
    us.ihmc.atlas.behaviorTests.AtlasDetectCollisionUsingWristSensorTest.class,
    us.ihmc.atlas.behaviorTests.AtlasHandPoseBehaviorTest.class,
    us.ihmc.atlas.behaviorTests.AtlasHandPoseListBehaviorTest.class,
+   us.ihmc.atlas.behaviorTests.AtlasHeadOrientationBehaviorTest.class,
    us.ihmc.atlas.behaviorTests.AtlasPelvisPoseBehaviorTest.class,
    us.ihmc.atlas.calib.KinematicCalibrationHeadLoopResidualTest.class,
    us.ihmc.atlas.controllers.AtlasFootstepGeneratorTest.class,
