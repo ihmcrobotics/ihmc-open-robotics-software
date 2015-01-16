6bcaf0aded54f97f3d4d2ee1c38a9c279cb6f7f5
Peter Neuhaus <pneuhaus@ihmc.us>
2015-01-16 23:19:06 +0000

    DRC-1916 created SCS with Atlas in it.wont fixbamboo.

---
+++ Atlas/src/us/ihmc/atlas/AtlasWholeBodyVisualizer.java
@@ -0,0 +1,14 @@
+package us.ihmc.atlas;
+
+import us.ihmc.atlas.AtlasRobotModel.AtlasTarget;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.wholeBodyInverseKinematicsSimulationController.WholeBodyVisualizier;
+
+public class AtlasWholeBodyVisualizer
+{
+   public static void main(String[] args)
+   {
+      DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasTarget.SIM, false);
+      WholeBodyVisualizier wholeBodyVisualizier = new WholeBodyVisualizier(robotModel);
+   }
+}
---
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/wholeBodyInverseKinematicsSimulationController/WholeBodyVisualizier.java
@@ -0,0 +1,24 @@
+package us.ihmc.darpaRoboticsChallenge.wholeBodyInverseKinematicsSimulationController;
+
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+
+public class WholeBodyVisualizier
+{
+   private final SimulationConstructionSet scs;
+   public WholeBodyVisualizier(DRCRobotModel robotModel)
+   {
+      //model = AtlasRobotModelFactory.selectSimulationModelFromFlag(args);
+   
+      SDFRobot simulatedRobot = robotModel.createSdfRobot(false);
+      
+    
+      scs = new SimulationConstructionSet(simulatedRobot);
+      
+      
+      scs.startOnAThread();
+   }
+   
+  
+}
