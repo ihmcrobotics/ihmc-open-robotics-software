8cc14bffd4907b5f9108244888d0ba0f2084fa36
Duncan Calvert <dcalvert@ihmc.us>
2015-01-21 01:27:19 +0000

    GFE-172: Created Docker test suites. Won't fixBamboo.

---
+++ KalmanProject/test/us/ihmc/kalman/KalmanFilterComparison.java
@@ -0,0 +1,68 @@
+package us.ihmc.kalman;
+
+import junit.framework.TestCase;
+import us.ihmc.IMUKalmanFilter.QuaternionBasedArrayFullIMUKalmanFilter;
+import us.ihmc.IMUKalmanFilter.QuaternionBasedJamaFullIMUKalmanFilter;
+
+public class KalmanFilterComparison extends TestCase
+{
+   @SuppressWarnings("unused")
+   private static final double DT = 0.001;
+
+   public KalmanFilterComparison(String name)
+   {
+      super(name);
+   }
+
+   protected void setUp() throws Exception
+   {
+      super.setUp();
+   }
+
+   public void testRepeatability()
+   {
+      @SuppressWarnings("unused") QuaternionBasedJamaFullIMUKalmanFilter quaternionBasedFullIMUKalmanFilter = null;
+      @SuppressWarnings("unused") QuaternionBasedArrayFullIMUKalmanFilter fastQuaternionBasedFullIMUKalmanFilter = null;
+
+   }
+
+// private QuaternionBasedFullIMUKalmanFilter createAndInitSlowKalman()
+// {
+//    QuaternionBasedFullIMUKalmanFilter ret = new QuaternionBasedFullIMUKalmanFilter(DT);
+//
+//    ret.initialize(accel, pqr, robot.yaw.val);
+//    ret.reset();
+//    return ret;
+// }
+//
+// private FastQuaternionBasedFullIMUKalmanFilter createAndInitFastKalman()
+//{
+// FastQuaternionBasedFullIMUKalmanFilter ret = new FastQuaternionBasedFullIMUKalmanFilter(DT);
+//    double[][] accel = new double[3][1];
+//    double[][] pqr = new double[3][1];
+//    accel[0][0] = -x_accel.val;
+//    accel[1][0] = -y_accel.val;
+//    accel[2][0] = -z_accel.val;
+//
+//    pqr[0][0] = 0.0;
+//    pqr[1][0] = 0.0;
+//    pqr[2][0] = 0.0;
+//    fastFullIMUKalmanFilter.initialize(accel, pqr, robot.yaw.val);
+//    fastFullIMUKalmanFilter.reset(fastFullIMUKalmanFilter.P);
+//
+// return ret;
+//}
+
+
+   protected void compareFilters(QuaternionBasedJamaFullIMUKalmanFilter filterOne, QuaternionBasedJamaFullIMUKalmanFilter filterTwo)
+   {
+   }
+
+
+
+   protected void tearDown() throws Exception
+   {
+      super.tearDown();
+   }
+
+}
