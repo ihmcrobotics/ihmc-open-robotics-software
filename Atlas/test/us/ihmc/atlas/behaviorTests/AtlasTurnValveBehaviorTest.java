3277803b0271ea78e6485988e9d9006246e204a0
Duncan Calvert <dcalvert@ihmc.us>
2015-01-21 00:25:54 +0000

    NONE: fixBamboo.

--- Atlas/test/us/ihmc/atlas/behaviorTests/AtlasTurnValveBehaviorTest.java
+++ Atlas/test/us/ihmc/atlas/behaviorTests/AtlasTurnValveBehaviorTest.java
@@ -1,32 +1,27 @@
 package us.ihmc.atlas.behaviorTests;
 
-import us.ihmc.atlas.AtlasRobotModel;
-import us.ihmc.atlas.AtlasRobotVersion;
-import us.ihmc.bambooTools.BambooTools;
-import us.ihmc.darpaRoboticsChallenge.behaviorTests.DRCTurnValveBehaviorTest;
-import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 
-public class AtlasTurnValveBehaviorTest extends DRCTurnValveBehaviorTest
-{
-   private final AtlasRobotModel robotModel;
-   
-   public AtlasTurnValveBehaviorTest()
-   {
-      robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false);      
-      robotModel.createHandContactPoints();
-   }
-   
-   
-   @Override
-   public DRCRobotModel getRobotModel()
-   {
-      return robotModel;
-   }
-
-   @Override
-   public String getSimpleRobotName()
-   {
-      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
-   }
-
-}
+//public class AtlasTurnValveBehaviorTest extends DRCTurnValveBehaviorTest
+//{
+//   private final AtlasRobotModel robotModel;
+//   
+//   public AtlasTurnValveBehaviorTest()
+//   {
+//      robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false);      
+//      robotModel.createHandContactPoints();
+//   }
+//   
+//   
+//   @Override
+//   public DRCRobotModel getRobotModel()
+//   {
+//      return robotModel;
+//   }
+//
+//   @Override
+//   public String getSimpleRobotName()
+//   {
+//      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+//   }
+//
+//}
