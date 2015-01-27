af1a0f4e770b1180f4b001c9806eeb70a66bd777
Nicolas Eyssette <neyssette@ihmc.us>
2015-01-27 21:07:53 +0000

    DRC-2112: changed the FullRobotModel into SDFFullRobotModel so that it can be used in the WholeBodyInverseKinematicBehavior. Added more code in the behavior. does not fixbamboo

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/primitives/WholeBodyInverseKinematicBehavior.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/primitives/WholeBodyInverseKinematicBehavior.java
@@ -1,8 +1,13 @@
 package us.ihmc.humanoidBehaviors.behaviors.primitives;
 
+import java.util.ArrayList;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.communication.packets.Packet;
 import us.ihmc.humanoidBehaviors.behaviors.BehaviorInterface;
 import us.ihmc.humanoidBehaviors.communication.OutgoingCommunicationBridgeInterface;
-import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
+import us.ihmc.utilities.math.geometry.FramePose;
+import us.ihmc.utilities.robotSide.RobotSide;
 import us.ihmc.wholeBodyController.WholeBodyControllerParameters;
 import us.ihmc.wholeBodyController.WholeBodyIKPacketCreator;
 import us.ihmc.wholeBodyController.WholeBodyIkSolver;
@@ -10,40 +15,81 @@ import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
 
 public class WholeBodyInverseKinematicBehavior extends BehaviorInterface
 {
+   private static final double trajectoryDuration = 2.0;
+   
    private final BooleanYoVariable packetHasBeenSent = new BooleanYoVariable("packetHasBeenSent" + behaviorName, registry);
+   private final BooleanYoVariable hasInputBeenSet = new BooleanYoVariable("hasInputBeenSet" + behaviorName, registry);
 
    private final WholeBodyIKPacketCreator wholeBodyNetworkModule;
    private final WholeBodyIkSolver wholeBodyIKSolver;
-   private final FullRobotModel actualFullRobotModel;
-   private final FullRobotModel desiredFullRobotModel;
+   private final SDFFullRobotModel actualFullRobotModel;
+   private final SDFFullRobotModel desiredFullRobotModel;
+   private final ArrayList<Packet> packetsToSend = new ArrayList<Packet>();
 
-   public WholeBodyInverseKinematicBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridge, WholeBodyControllerParameters wholeBodyControllerParameters, FullRobotModel actualFullRobotModel)
+   public WholeBodyInverseKinematicBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridge,
+         WholeBodyControllerParameters wholeBodyControllerParameters, SDFFullRobotModel actualFullRobotModel)
    {
       super(outgoingCommunicationBridge);
       wholeBodyNetworkModule = new WholeBodyIKPacketCreator(wholeBodyControllerParameters);
       wholeBodyIKSolver = wholeBodyControllerParameters.createWholeBodyIkSolver();
-      
+
       this.actualFullRobotModel = actualFullRobotModel;
       this.desiredFullRobotModel = wholeBodyControllerParameters.createFullRobotModel();
-      
    }
 
+   public void setInputs(RobotSide robotSide, FramePose endEffectorPose) throws Exception
+   {
+      wholeBodyIKSolver.setHandTarget(actualFullRobotModel, robotSide, endEffectorPose);
+      
+      wholeBodyIKSolver.compute(actualFullRobotModel, desiredFullRobotModel);
+      hasInputBeenSet.set(true);
+   }
+
+   private void sendSolutionToController(double trajectoryDuration)
+   {
+      packetsToSend.clear();
+      wholeBodyNetworkModule.createPackets(desiredFullRobotModel, trajectoryDuration, packetsToSend);
+      for (int i = 0; i < packetsToSend.size(); i++)
+      {
+         sendPacketToController(packetsToSend.get(i));
+      }
+   }
+   
+   
    @Override
    public void doControl()
    {
+
+      //TODO check all the status
+      
+      if (!packetHasBeenSent.getBooleanValue())
+      {
+         sendSolutionToController(trajectoryDuration);
+         //TODO send packets to controller
+         packetHasBeenSent.set(true);
+      }
+  
+      
    }
 
    @Override
    public void initialize()
    {
+      packetHasBeenSent.set(false);
+      hasInputBeenSet.set(false);
+
       wholeBodyIKSolver.setPreferedJointPose("r_leg_kny", 1.5);//- 0.81
-      
+
+      wholeBodyIKSolver.setVerbose(false);
+      wholeBodyIKSolver.getHierarchicalSolver().collisionAvoidance.setEnabled(false);
+
    }
 
    @Override
    public void finalize()
    {
       packetHasBeenSent.set(false);
+      hasInputBeenSet.set(false);
 
       isPaused.set(false);
       isStopped.set(false);
@@ -70,7 +116,7 @@ public class WholeBodyInverseKinematicBehavior extends BehaviorInterface
    @Override
    public boolean isDone()
    {
-      return packetHasBeenSent.getBooleanValue() &&!isPaused.getBooleanValue();
+      return false;
    }
 
    @Override
@@ -87,9 +133,10 @@ public class WholeBodyInverseKinematicBehavior extends BehaviorInterface
    protected void passReceivedControllerObjectToChildBehaviors(Object object)
    {
    }
-   
+
    @Override
-   public boolean hasInputBeenSet() {
-		   return false;
+   public boolean hasInputBeenSet()
+   {
+      return false;
    }
 }
