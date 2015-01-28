a27ade7ad3d6f98f2b851e9a82c4dad5c0f84e37
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-01-28 22:15:16 +0000

    DRC-2135: Unit test for HighLevelStateBehavior().  Only testing DO_NOTHING_BEHAVIOR, since the momentumControllerFactory doesn't add the other high level behaviors (multicontact, carIngressEgress).  won't fixBamboo.

---
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/behaviorTests/DRCHighLevelStateBehaviorTest.java
@@ -0,0 +1,221 @@
+package us.ihmc.darpaRoboticsChallenge.behaviorTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.communication.kryo.IHMCCommunicationKryoNetClassList;
+import us.ihmc.communication.net.PacketCommunicator;
+import us.ihmc.communication.packetCommunicator.KryoLocalPacketCommunicator;
+import us.ihmc.communication.packets.HighLevelStatePacket;
+import us.ihmc.communication.packets.dataobjects.HighLevelState;
+import us.ihmc.communication.packets.dataobjects.RobotConfigurationData;
+import us.ihmc.communication.packets.walking.CapturabilityBasedStatus;
+import us.ihmc.communication.subscribers.CapturabilityBasedStatusSubscriber;
+import us.ihmc.communication.subscribers.RobotDataReceiver;
+import us.ihmc.communication.util.NetworkConfigParameters;
+import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseStartingLocation;
+import us.ihmc.darpaRoboticsChallenge.MultiRobotTestInterface;
+import us.ihmc.darpaRoboticsChallenge.environment.DRCDemo01NavigationEnvironment;
+import us.ihmc.darpaRoboticsChallenge.testTools.DRCSimulationTestHelper;
+import us.ihmc.humanoidBehaviors.behaviors.BehaviorInterface;
+import us.ihmc.humanoidBehaviors.behaviors.primitives.HighLevelStateBehavior;
+import us.ihmc.humanoidBehaviors.communication.BehaviorCommunicationBridge;
+import us.ihmc.humanoidBehaviors.utilities.CapturePointUpdatable;
+import us.ihmc.simulationconstructionset.OneDegreeOfFreedomJoint;
+import us.ihmc.simulationconstructionset.Robot;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.utilities.MemoryTools;
+import us.ihmc.utilities.SysoutTool;
+import us.ihmc.utilities.humanoidRobot.frames.ReferenceFrames;
+import us.ihmc.utilities.humanoidRobot.model.ForceSensorDataHolder;
+import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
+import us.ihmc.utilities.math.geometry.FramePose;
+import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+import us.ihmc.yoUtilities.graphics.YoGraphicsListRegistry;
+
+public abstract class DRCHighLevelStateBehaviorTest implements MultiRobotTestInterface
+{
+   private static final boolean DEBUG = false;
+   private static final boolean createMovie = BambooTools.doMovieCreation();
+   private static final boolean checkNothingChanged = BambooTools.getCheckNothingChanged();
+   private static final boolean showGUI = false || createMovie;
+
+   private final double MAX_ANGLE_TO_TEST_RAD = 30.0 * Math.PI / 180.0;
+   private final double POSITION_THRESHOLD = 0.05;
+   private final double ORIENTATION_THRESHOLD = 0.007;
+   private final double EXTRA_SIM_TIME_FOR_SETTLING = 1.0;
+
+   private final DRCDemo01NavigationEnvironment testEnvironment = new DRCDemo01NavigationEnvironment();
+   private final PacketCommunicator controllerCommunicator = new KryoLocalPacketCommunicator(new IHMCCommunicationKryoNetClassList(), 10,
+         "DRCHandPoseBehaviorTestControllerCommunicator");
+
+   private DRCSimulationTestHelper drcSimulationTestHelper;
+
+   private DoubleYoVariable yoTime;
+
+   private RobotDataReceiver robotDataReceiver;
+   private ForceSensorDataHolder forceSensorDataHolder;
+
+   private BehaviorCommunicationBridge communicationBridge;
+
+   private SDFRobot robot;
+   private FullRobotModel fullRobotModel;
+   private ReferenceFrames referenceFrames;
+
+   private CapturePointUpdatable capturePointUpdatable;
+   private BooleanYoVariable yoDoubleSupport;
+
+   @Before
+   public void setUp()
+   {
+      if (NetworkConfigParameters.USE_BEHAVIORS_MODULE)
+      {
+         throw new RuntimeException("Must set NetworkConfigParameters.USE_BEHAVIORS_MODULE = false in order to perform this test!");
+      }
+
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
+
+      drcSimulationTestHelper = new DRCSimulationTestHelper(testEnvironment, controllerCommunicator, getSimpleRobotName(), null,
+            DRCObstacleCourseStartingLocation.DEFAULT, checkNothingChanged, showGUI, createMovie, false, getRobotModel());
+
+      Robot robotToTest = drcSimulationTestHelper.getRobot();
+      yoTime = robotToTest.getYoTime();
+
+      robot = drcSimulationTestHelper.getRobot();
+      fullRobotModel = getRobotModel().createFullRobotModel();
+
+      forceSensorDataHolder = new ForceSensorDataHolder(Arrays.asList(fullRobotModel.getForceSensorDefinitions()));
+
+      robotDataReceiver = new RobotDataReceiver(fullRobotModel, forceSensorDataHolder, true);
+      controllerCommunicator.attachListener(RobotConfigurationData.class, robotDataReceiver);
+      referenceFrames = robotDataReceiver.getReferenceFrames();
+
+      PacketCommunicator junkyObjectCommunicator = new KryoLocalPacketCommunicator(new IHMCCommunicationKryoNetClassList(), 10,
+            "DRCComHeightBehaviorTestJunkyCommunicator");
+
+      communicationBridge = new BehaviorCommunicationBridge(junkyObjectCommunicator, controllerCommunicator, robotToTest.getRobotsYoVariableRegistry());
+
+      CapturabilityBasedStatusSubscriber capturabilityBasedStatusSubsrciber = new CapturabilityBasedStatusSubscriber();
+      controllerCommunicator.attachListener(CapturabilityBasedStatus.class, capturabilityBasedStatusSubsrciber);
+      capturePointUpdatable = new CapturePointUpdatable(capturabilityBasedStatusSubsrciber, new YoGraphicsListRegistry(), robot.getRobotsYoVariableRegistry());
+      yoDoubleSupport = capturePointUpdatable.getYoDoubleSupport();
+   }
+
+   @After
+   public void destroySimulationAndRecycleMemory()
+   {
+      if (drcSimulationTestHelper != null)
+      {
+         drcSimulationTestHelper.destroySimulation();
+         drcSimulationTestHelper = null;
+      }
+
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
+   }
+  
+   
+   @Test(timeout = 300000)
+   public void testDoNothingBehavior() throws SimulationExceededMaximumTimeException
+   {
+      BambooTools.reportTestStartedMessage();
+
+      boolean success = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(1.0);
+      assertTrue(success);
+
+      double trajectoryTime = 2.0;
+
+      final HighLevelStateBehavior highLevelStateBehavior = new HighLevelStateBehavior(communicationBridge);
+      communicationBridge.attachGlobalListenerToController(highLevelStateBehavior.getControllerGlobalPacketConsumer());
+      HighLevelStatePacket highLevelStatePacket = new HighLevelStatePacket(HighLevelState.DO_NOTHING_BEHAVIOR);
+      highLevelStateBehavior.initialize();
+      highLevelStateBehavior.setInput(highLevelStatePacket);
+
+      success = executeBehavior(highLevelStateBehavior, trajectoryTime);
+      assertTrue(success);
+
+      assertTrue(highLevelStateBehavior.isDone());
+      
+      OneDegreeOfFreedomJoint[] oneDofJoints = robot.getOneDoFJoints();
+      
+      for (OneDegreeOfFreedomJoint joint : oneDofJoints)
+      {
+         String jointName = joint.getName();
+         double tau = joint.getTau().getDoubleValue();
+         SysoutTool.println(joint.getName() + " tau : " + tau, DEBUG);
+         
+         if ( !jointName.contains("hokuyo") )
+         {
+            assertTrue(tau == 0.0);           
+         }
+      }
+
+      BambooTools.reportTestFinishedMessage();
+   }
+   
+   private boolean executeBehavior(final BehaviorInterface behavior, double trajectoryTime) throws SimulationExceededMaximumTimeException
+   {
+      final double simulationRunTime = trajectoryTime + EXTRA_SIM_TIME_FOR_SETTLING;
+
+      SysoutTool.println("\n starting behavior: " + behavior.getName() + "   t = " + yoTime.getDoubleValue(), DEBUG);
+
+      Thread behaviorThread = new Thread()
+      {
+         public void run()
+         {
+            {
+               double startTime = Double.NaN;
+               boolean simStillRunning = true;
+               boolean initalized = false;
+
+               while (simStillRunning)
+               {
+                  if (!initalized)
+                  {
+                     startTime = yoTime.getDoubleValue();
+                     initalized = true;
+                  }
+
+                  double timeSpentSimulating = yoTime.getDoubleValue() - startTime;
+                  simStillRunning = timeSpentSimulating < simulationRunTime;
+
+                  behavior.doControl();
+
+                  capturePointUpdatable.update(yoTime.getDoubleValue());
+               }
+            }
+         }
+      };
+
+      behaviorThread.start();
+
+      boolean ret = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(simulationRunTime);
+
+      SysoutTool.println("done simulating behavior: " + behavior.getName() + "   t = " + yoTime.getDoubleValue(), DEBUG);
+
+      return ret;
+   }
+
+   private void assertPosesAreWithinThresholds(FramePose framePose1, FramePose framePose2)
+   {
+      double positionDistance = framePose1.getPositionDistance(framePose2);
+      double orientationDistance = framePose1.getOrientationDistance(framePose2);
+
+      if (DEBUG)
+      {
+         System.out.println("testSimpleHandPoseMove: positionDistance=" + positionDistance);
+         System.out.println("testSimpleHandPoseMove: orientationDistance=" + orientationDistance);
+      }
+
+      assertEquals(0.0, positionDistance, POSITION_THRESHOLD);
+      assertEquals(0.0, orientationDistance, ORIENTATION_THRESHOLD);
+   }
+}
