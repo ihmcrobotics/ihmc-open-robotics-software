fa6be52a386fb8f18aef9ada8e8ecc2dba26c441
Sylvain Bertrand <sbertrand@ihmc.us>
2015-01-28 17:17:00 +0000

    NONE: Commit my work on Steppr log processors. Won't fixbamboo.

--- src/us/ihmc/valkyrie/logProcessor/ValkyrieDiagnosticLogProcessor.java
+++ src/us/ihmc/valkyrie/logProcessor/ValkyrieDiagnosticLogProcessor.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.logProcessor.DRCLogProcessor;
 import us.ihmc.darpaRoboticsChallenge.logProcessor.LogDataProcessorFunction;
+import us.ihmc.darpaRoboticsChallenge.logProcessor.LogDataProcessorHelper;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 
 public class ValkyrieDiagnosticLogProcessor extends DRCLogProcessor
@@ -12,6 +13,7 @@ public class ValkyrieDiagnosticLogProcessor extends DRCLogProcessor
    public ValkyrieDiagnosticLogProcessor() throws IOException
    {
       super();
+      LogDataProcessorHelper logDataProcessorHelper = createLogDataProcessorHelper();
       LogDataProcessorFunction logDataProcessor = new DiagnosticLogProcessorFunction(logDataProcessorHelper);
       setLogDataProcessor(logDataProcessor);
       startLogger();
