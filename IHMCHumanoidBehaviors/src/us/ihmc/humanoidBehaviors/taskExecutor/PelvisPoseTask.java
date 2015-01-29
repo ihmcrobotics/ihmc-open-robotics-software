f927b03d24c4760fc110d07ea313ad304aeb5520
Nicolas Eyssette <neyssette@ihmc.us>
2015-01-29 16:58:55 +0000

    DRC-2112: Update of the handPoseTask and the PelvisPoseTask

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/PelvisPoseTask.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/PelvisPoseTask.java
@@ -1,11 +1,14 @@
 package us.ihmc.humanoidBehaviors.taskExecutor;
 
+import javax.vecmath.Point3d;
 import javax.vecmath.Quat4d;
 
 import us.ihmc.communication.packets.walking.PelvisPosePacket;
 import us.ihmc.communication.util.PacketControllerTools;
 import us.ihmc.humanoidBehaviors.behaviors.primitives.PelvisPoseBehavior;
 import us.ihmc.utilities.math.geometry.FrameOrientation;
+import us.ihmc.utilities.math.geometry.FramePoint;
+import us.ihmc.utilities.math.geometry.FramePose;
 import us.ihmc.utilities.taskExecutor.Task;
 import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
 
@@ -33,6 +36,23 @@ public class PelvisPoseTask implements Task
       Quat4d pelvisOrientation = new Quat4d();
       desiredPelvisOrientation.getQuaternion(pelvisOrientation);
       pelvisPosePacket = PacketControllerTools.createPelvisPosePacketForOrientationOnly(pelvisOrientation, trajectoryTime);
+   }
+
+   public PelvisPoseTask(FramePose desiredPelvisPose, DoubleYoVariable yoTime, PelvisPoseBehavior pelvisPoseBehavior, double trajectoryTime)
+   {
+      this(desiredPelvisPose, yoTime, pelvisPoseBehavior, trajectoryTime, 0.0);
+   }
+
+   public PelvisPoseTask(FramePose desiredPelvisPose, DoubleYoVariable yoTime, PelvisPoseBehavior pelvisPoseBehavior, double trajectoryTime, double sleepTime)
+   {
+      this.pelvisPoseBehavior = pelvisPoseBehavior;
+      this.yoTime = yoTime;
+      this.sleepTime = sleepTime;
+      Point3d pelvisPosition = new Point3d();
+      Quat4d pelvisOrientation = new Quat4d();
+      desiredPelvisPose.getOrientation(pelvisOrientation);
+      desiredPelvisPose.getPosition(pelvisPosition);
+      pelvisPosePacket = PacketControllerTools.createPelvisPosePacketForPositionAndOrientation(pelvisPosition, pelvisOrientation, trajectoryTime);
    }
 
    @Override
