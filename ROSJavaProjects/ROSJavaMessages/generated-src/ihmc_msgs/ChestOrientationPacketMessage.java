ebac5faab15160d4f566b00d95ce75b1b5f4c3d2
Nicolas Eyssette <neyssette@ihmc.us>
2015-02-01 22:50:28 +0000

    NONE: fixbamboo by updating ROS messages after modifying the pelvisPosePacket and ChestOrientationPacket

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ChestOrientationPacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ChestOrientationPacketMessage.java
@@ -2,9 +2,11 @@ package ihmc_msgs;
 
 public interface ChestOrientationPacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/ChestOrientationPacketMessage";
-  static final java.lang.String _DEFINITION = "## ChestOrientationPacketMessage\r\n# This message gives the orientation of the robot\'s chest in world frame.\r\n\r\ngeometry_msgs/Quaternion quaternion\r\nfloat64 trajectoryTime\r\n\r\n";
-  geometry_msgs.Quaternion getQuaternion();
-  void setQuaternion(geometry_msgs.Quaternion value);
+  static final java.lang.String _DEFINITION = "## ChestOrientationPacketMessage\r\n# This message gives the orientation of the robot\'s chest in world frame.\r\n\r\ngeometry_msgs/Quaternion orientation\r\nfloat64 trajectoryTime\r\n# toHomePosition can be used to move the Pelvis back to its starting position\r\nbool toHomeOrientation\r\n\r\n";
+  geometry_msgs.Quaternion getOrientation();
+  void setOrientation(geometry_msgs.Quaternion value);
   double getTrajectoryTime();
   void setTrajectoryTime(double value);
+  boolean getToHomeOrientation();
+  void setToHomeOrientation(boolean value);
 }
