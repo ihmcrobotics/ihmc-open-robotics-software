b3a8421535b33b6b65d71fc6256f68983204efcc
Tingfan Wu <twu@ihmc.us>
2015-02-01 19:18:22 +0000

    NONE: remove debug message from QPsolvers, update dependency to latest IHMCOptimizerWrappers not fixbamboo

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/nativeOptimization/QuadProgSolver.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/nativeOptimization/QuadProgSolver.java
@@ -1,5 +1,7 @@
 package us.ihmc.commonWalkingControlModules.controlModules.nativeOptimization;
 
+//~--- non-JDK imports --------------------------------------------------------
+
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
 
@@ -7,83 +9,62 @@ import us.ihmc.convexOptimization.QuadProgWrapper;
 import us.ihmc.utilities.exeptions.NoConvergenceException;
 import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
 
-
-public class QuadProgSolver extends ConstrainedQPSolver
-{
-   /*
-    * The problem is in the form:
-    *
-    * min 0.5 * x G x + g0 x
-    * s.t.
-    *   CE^T x + ce0 = 0
-    *   CI^T x + ci0 >= 0
-    */
-
-   private static final boolean DEBUG = false;
-
-   QuadProgWrapper qpWrapper;
-   DenseMatrix64F negAin, negAeq;
+//~--- JDK imports ------------------------------------------------------------
 
 
-   public QuadProgSolver(YoVariableRegistry parentRegistry)
-   {
-      this(1, 0, 0, parentRegistry);
-   }
+public class QuadProgSolver extends ConstrainedQPSolver {
 
-   public QuadProgSolver(int nvar, int neq, int nin, YoVariableRegistry parentRegistry)
-   {
-      qpWrapper = new QuadProgWrapper(nvar, neq, nin);
-      allocateTempraryMatrixOnDemand(nvar, neq, nin);
+    /*
+     * The problem is in the form:
+     *
+     * min 0.5 * x G x + g0 x
+     * s.t.
+     *   CE^T x + ce0 = 0
+     *   CI^T x + ci0 >= 0
+     */
+	
+	QuadProgWrapper qpWrapper;
+	DenseMatrix64F negAin=new DenseMatrix64F(0), negAeq=new DenseMatrix64F(0);
 
-   }
 
-   private void allocateTempraryMatrixOnDemand(int nvar, int neq, int nin)
-   {
-      if ((negAin == null) || (negAin.numRows != nvar) || (negAin.numCols != nin))
-      {
-         printWarningIfDebug(getClass().getSimpleName() + ": Re-allocated negAin");
-         negAin = new DenseMatrix64F(nvar, nin);
-      }
+    public QuadProgSolver(YoVariableRegistry parentRegistry) {
+    	this(1,0,0, parentRegistry);
+    }
+    public QuadProgSolver(int nvar, int neq, int nin, YoVariableRegistry parentRegistry) {
+        qpWrapper = new QuadProgWrapper(nvar, neq, nin);
+        allocateTempraryMatrixOnDemand(nvar, neq, nin);
 
-      if ((negAeq == null) || (negAeq.numRows != nvar) || (negAeq.numCols != neq))
-      {
-         printWarningIfDebug(getClass().getSimpleName() + ": Re-allocated negAeq");
-         negAeq = new DenseMatrix64F(nvar, neq);
-      }
-   }
+    }
+    
+    private void allocateTempraryMatrixOnDemand(int nvar, int neq, int nin)
+    {
+    		negAin.reshape(nvar, nin);
+         negAeq.reshape(nvar, neq);
+    }
 
-   private void printWarningIfDebug(String warning)
-   {
-      if (DEBUG)
-         System.err.println(warning);
-   }
+    @Override
+    public boolean supportBoxConstraints() {
+        return false;
+    }
 
-   @Override
-   public boolean supportBoxConstraints()
-   {
-      return false;
-   }
+    @Override
+    public int solve(DenseMatrix64F Q, DenseMatrix64F f, DenseMatrix64F Aeq, DenseMatrix64F beq, DenseMatrix64F Ain,
+                      DenseMatrix64F bin, DenseMatrix64F x, boolean initialize)
+            throws NoConvergenceException {
+        return solve(Q, f, Aeq, beq, Ain, bin, null, null, x, initialize);
+    }
 
-   @Override
-   public int solve(DenseMatrix64F Q, DenseMatrix64F f, DenseMatrix64F Aeq, DenseMatrix64F beq, DenseMatrix64F Ain, DenseMatrix64F bin, DenseMatrix64F x,
-                    boolean initialize)
-           throws NoConvergenceException
-   {
-      return solve(Q, f, Aeq, beq, Ain, bin, null, null, x, initialize);
-   }
+    @Override
+    public int solve(DenseMatrix64F Q, DenseMatrix64F f, DenseMatrix64F Aeq, DenseMatrix64F beq, DenseMatrix64F Ain,
+                      DenseMatrix64F bin, DenseMatrix64F lb, DenseMatrix64F ub, DenseMatrix64F x, boolean initialize)
+            throws NoConvergenceException {
 
-   @Override
-   public int solve(DenseMatrix64F Q, DenseMatrix64F f, DenseMatrix64F Aeq, DenseMatrix64F beq, DenseMatrix64F Ain, DenseMatrix64F bin, DenseMatrix64F lb,
-                    DenseMatrix64F ub, DenseMatrix64F x, boolean initialize)
-           throws NoConvergenceException
-   {
-      allocateTempraryMatrixOnDemand(Aeq.numCols, Aeq.numRows, Ain.numRows);
+    	allocateTempraryMatrixOnDemand(Aeq.numCols, Aeq.numRows, Ain.numRows);
 
-      CommonOps.transpose(Aeq, this.negAeq);
-      CommonOps.scale(-1, this.negAeq);
-      CommonOps.transpose(Ain, this.negAin);
-      CommonOps.scale(-1, this.negAin);
-
-      return qpWrapper.solve(Q, f, negAeq, beq, negAin, bin, x, initialize);
-   }
+    	CommonOps.transpose(Aeq, this.negAeq);
+    	CommonOps.scale(-1, this.negAeq);
+    	CommonOps.transpose(Ain, this.negAin);
+    	CommonOps.scale(-1, this.negAin);
+    	return qpWrapper.solve(Q, f, negAeq, beq, negAin, bin, x, initialize);
+    }
 }
