10e536897fad5696b59368e3af1ed5083744445f
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-02-03 01:16:08 +0000

    GFE-212: fixbamboo, ran bamboo test generator for atlas project

--- Atlas/test/us/ihmc/atlas/networkProcessor/depthData/AtlasDepthDataProcessorTest.java
+++ Atlas/test/us/ihmc/atlas/networkProcessor/depthData/AtlasDepthDataProcessorTest.java
@@ -5,10 +5,7 @@ import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData.DepthDataProcessorTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
-import us.ihmc.utilities.code.unitTesting.BambooAnnotations.CustomJob;
-import us.ihmc.utilities.code.unitTesting.BambooAnnotations.CustomJobType;
 
-@CustomJob(job = CustomJobType.Utilities)
 public class AtlasDepthDataProcessorTest extends DepthDataProcessorTest
 {
    private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.DRC_NO_HANDS, AtlasRobotModel.AtlasTarget.SIM, false);
