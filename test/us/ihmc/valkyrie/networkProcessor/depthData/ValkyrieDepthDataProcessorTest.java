048e0b93ead51945efbf7398a59a857bde4ba1e3
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-02-03 01:19:38 +0000

    GFE-212: ran bamboo test generator for val project

--- test/us/ihmc/valkyrie/networkProcessor/depthData/ValkyrieDepthDataProcessorTest.java
+++ test/us/ihmc/valkyrie/networkProcessor/depthData/ValkyrieDepthDataProcessorTest.java
@@ -3,11 +3,8 @@ package us.ihmc.valkyrie.networkProcessor.depthData;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData.DepthDataProcessorTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
-import us.ihmc.utilities.code.unitTesting.BambooAnnotations.CustomJob;
-import us.ihmc.utilities.code.unitTesting.BambooAnnotations.CustomJobType;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 
-@CustomJob(job = CustomJobType.Utilities)
 public class ValkyrieDepthDataProcessorTest extends DepthDataProcessorTest
 {
    private final DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
