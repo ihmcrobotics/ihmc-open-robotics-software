8d10ff1b306924040c6870a5ab266bb3d60f093b
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-02-03 21:14:23 +0000

    DRC-2134: fix memory leaks.

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/behaviorTests/DRCHeadOrientationBehaviorTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/behaviorTests/DRCHeadOrientationBehaviorTest.java
@@ -3,7 +3,6 @@ package us.ihmc.darpaRoboticsChallenge.behaviorTests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
 import java.util.Random;
 
 import javax.vecmath.AxisAngle4d;
@@ -11,44 +10,38 @@ import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
-import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.communication.kryo.IHMCCommunicationKryoNetClassList;
-import us.ihmc.communication.net.PacketCommunicator;
 import us.ihmc.communication.packetCommunicator.KryoLocalPacketCommunicator;
-import us.ihmc.communication.packets.dataobjects.RobotConfigurationData;
+import us.ihmc.communication.packetCommunicator.KryoPacketCommunicator;
 import us.ihmc.communication.packets.walking.HeadOrientationPacket;
-import us.ihmc.communication.subscribers.RobotDataReceiver;
 import us.ihmc.communication.util.NetworkConfigParameters;
 import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseStartingLocation;
 import us.ihmc.darpaRoboticsChallenge.MultiRobotTestInterface;
 import us.ihmc.darpaRoboticsChallenge.environment.DRCDemo01NavigationEnvironment;
-import us.ihmc.darpaRoboticsChallenge.testTools.DRCSimulationTestHelper;
-import us.ihmc.humanoidBehaviors.behaviors.BehaviorInterface;
+import us.ihmc.darpaRoboticsChallenge.testTools.DRCBehaviorTestHelper;
 import us.ihmc.humanoidBehaviors.behaviors.primitives.HeadOrientationBehavior;
-import us.ihmc.humanoidBehaviors.communication.BehaviorCommunicationBridge;
-import us.ihmc.simulationconstructionset.Robot;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
 import us.ihmc.simulationconstructionset.bambooTools.SimulationTestingParameters;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.utilities.AsyncContinuousExecutor;
 import us.ihmc.utilities.MemoryTools;
 import us.ihmc.utilities.RandomTools;
 import us.ihmc.utilities.SysoutTool;
 import us.ihmc.utilities.ThreadTools;
+import us.ihmc.utilities.TimerTaskScheduler;
 import us.ihmc.utilities.code.unitTesting.BambooAnnotations.AverageDuration;
-import us.ihmc.utilities.humanoidRobot.model.ForceSensorDataHolder;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 import us.ihmc.utilities.math.geometry.FramePose;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
-import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+import us.ihmc.yoUtilities.time.GlobalTimer;
 
 public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestInterface
 {
    private static final SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromEnvironmentVariables();
-
-   private DRCSimulationTestHelper drcSimulationTestHelper;
 
    @Before
    public void showMemoryUsageBeforeTest()
@@ -65,13 +58,23 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
       }
 
       // Do this here in case a test fails. That way the memory will be recycled.
-      if (drcSimulationTestHelper != null)
+      if (drcBehaviorTestHelper != null)
       {
-         drcSimulationTestHelper.destroySimulation();
-         drcSimulationTestHelper = null;
+         drcBehaviorTestHelper.closeAndDispose();
+         drcBehaviorTestHelper = null;
       }
 
+      GlobalTimer.clearTimers();
+      TimerTaskScheduler.cancelAndReset();
+      AsyncContinuousExecutor.cancelAndReset();
+
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
+   }
+
+   @AfterClass
+   public static void printMemoryUsageAfterClass()
+   {
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(DRCChestOrientationBehaviorTest.class + " after class.");
    }
 
    private static final boolean DEBUG = false;
@@ -81,19 +84,7 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
    private final double ORIENTATION_THRESHOLD = 0.007;
    private final double EXTRA_SIM_TIME_FOR_SETTLING = 1.0;
 
-   private final DRCDemo01NavigationEnvironment testEnvironment = new DRCDemo01NavigationEnvironment();
-   private final PacketCommunicator controllerCommunicator = new KryoLocalPacketCommunicator(new IHMCCommunicationKryoNetClassList(), 10,
-         "DRCHandPoseBehaviorTestControllerCommunicator");
-
-   private DoubleYoVariable yoTime;
-
-   private RobotDataReceiver robotDataReceiver;
-   private ForceSensorDataHolder forceSensorDataHolder;
-
-   private BehaviorCommunicationBridge communicationBridge;
-
-   private SDFRobot robot;
-   private FullRobotModel fullRobotModel;
+   private DRCBehaviorTestHelper drcBehaviorTestHelper;
 
    @Before
    public void setUp()
@@ -105,24 +96,13 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
 
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
 
-      drcSimulationTestHelper = new DRCSimulationTestHelper(testEnvironment, controllerCommunicator, getSimpleRobotName(), null,
-            DRCObstacleCourseStartingLocation.DEFAULT, simulationTestingParameters, false, getRobotModel());
+      DRCDemo01NavigationEnvironment testEnvironment = new DRCDemo01NavigationEnvironment();
 
-      Robot robotToTest = drcSimulationTestHelper.getRobot();
-      yoTime = robotToTest.getYoTime();
+      KryoPacketCommunicator controllerCommunicator = new KryoLocalPacketCommunicator(new IHMCCommunicationKryoNetClassList(), 10, "DRCControllerCommunicator");
+      KryoPacketCommunicator networkObjectCommunicator = new KryoLocalPacketCommunicator(new IHMCCommunicationKryoNetClassList(), 10, "DRCJunkyCommunicator");
 
-      robot = drcSimulationTestHelper.getRobot();
-      fullRobotModel = getRobotModel().createFullRobotModel();
-
-      forceSensorDataHolder = new ForceSensorDataHolder(Arrays.asList(fullRobotModel.getForceSensorDefinitions()));
-
-      robotDataReceiver = new RobotDataReceiver(fullRobotModel, forceSensorDataHolder, true);
-      controllerCommunicator.attachListener(RobotConfigurationData.class, robotDataReceiver);
-
-      PacketCommunicator junkyObjectCommunicator = new KryoLocalPacketCommunicator(new IHMCCommunicationKryoNetClassList(), 10,
-            "DRCComHeightBehaviorTestJunkyCommunicator");
-
-      communicationBridge = new BehaviorCommunicationBridge(junkyObjectCommunicator, controllerCommunicator, robotToTest.getRobotsYoVariableRegistry());
+      drcBehaviorTestHelper = new DRCBehaviorTestHelper(testEnvironment, networkObjectCommunicator, getSimpleRobotName(), null,
+            DRCObstacleCourseStartingLocation.DEFAULT, simulationTestingParameters, false, getRobotModel(), controllerCommunicator);
    }
 
    //TODO: Fix HeadOrienationManager() so that head actually tracks desired yaw and roll orientations.  Currently, only pitch orientation tracks properly.
@@ -138,11 +118,7 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
       double rotationAngle = MAX_ANGLE_TO_TEST_RAD * RandomTools.generateRandomDouble(new Random(), 0.3, 1.0);
 
       HeadOrientationPacket headOrientationPacket = createHeadOrientationPacket(axis, rotationAngle);
-
-      HeadOrientationBehavior headOrientationBehavior = testHeadOrientationBehavior(headOrientationPacket, trajectoryTime);
-
-      assertTrue(headOrientationBehavior.isDone());
-
+      testHeadOrientationBehavior(headOrientationPacket, trajectoryTime + EXTRA_SIM_TIME_FOR_SETTLING);
       BambooTools.reportTestFinishedMessage();
    }
 
@@ -156,11 +132,7 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
       double rotationAngle = MAX_ANGLE_TO_TEST_RAD * RandomTools.generateRandomDouble(new Random(), 0.3, 1.0);
 
       HeadOrientationPacket headOrientationPacket = createHeadOrientationPacket(axis, rotationAngle);
-
-      HeadOrientationBehavior headOrientationBehavior = testHeadOrientationBehavior(headOrientationPacket, trajectoryTime);
-
-      assertTrue(headOrientationBehavior.isDone());
-
+      testHeadOrientationBehavior(headOrientationPacket, trajectoryTime + EXTRA_SIM_TIME_FOR_SETTLING);
       BambooTools.reportTestFinishedMessage();
    }
 
@@ -174,11 +146,7 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
       double rotationAngle = MAX_ANGLE_TO_TEST_RAD * RandomTools.generateRandomDouble(new Random(), 0.3, 1.0);
 
       HeadOrientationPacket headOrientationPacket = createHeadOrientationPacket(axis, rotationAngle);
-
-      HeadOrientationBehavior headOrientationBehavior = testHeadOrientationBehavior(headOrientationPacket, trajectoryTime);
-
-      assertTrue(headOrientationBehavior.isDone());
-
+      testHeadOrientationBehavior(headOrientationPacket, trajectoryTime + EXTRA_SIM_TIME_FOR_SETTLING);
       BambooTools.reportTestFinishedMessage();
    }
 
@@ -191,10 +159,7 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
       Quat4d desiredHeadQuat = new Quat4d(RandomTools.generateRandomQuaternion(new Random(), MAX_ANGLE_TO_TEST_RAD));
       HeadOrientationPacket headOrientationPacket = new HeadOrientationPacket(desiredHeadQuat);
 
-      HeadOrientationBehavior headOrientationBehavior = testHeadOrientationBehavior(headOrientationPacket, trajectoryTime);
-
-      assertTrue(headOrientationBehavior.isDone());
-
+      testHeadOrientationBehavior(headOrientationPacket, trajectoryTime + EXTRA_SIM_TIME_FOR_SETTLING);
       BambooTools.reportTestFinishedMessage();
    }
 
@@ -209,34 +174,32 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
       return headOrientationPacket;
    }
 
-   private HeadOrientationBehavior testHeadOrientationBehavior(HeadOrientationPacket headOrientationPacket, double trajectoryTime)
-         throws SimulationExceededMaximumTimeException
+   private void testHeadOrientationBehavior(HeadOrientationPacket headOrientationPacket, double trajectoryTime) throws SimulationExceededMaximumTimeException
    {
-      boolean success = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(1.0);
+      boolean success = drcBehaviorTestHelper.simulateAndBlockAndCatchExceptions(1.0);
+      assertTrue(success);
 
-      final HeadOrientationBehavior headOrientBehavior = new HeadOrientationBehavior(communicationBridge, yoTime);
-      communicationBridge.attachGlobalListenerToController(headOrientBehavior.getControllerGlobalPacketConsumer());
+      final HeadOrientationBehavior headOrientBehavior = new HeadOrientationBehavior(drcBehaviorTestHelper.getBehaviorCommunicationBridge(),
+            drcBehaviorTestHelper.getYoTime());
 
       headOrientBehavior.initialize();
       headOrientBehavior.setInput(headOrientationPacket);
-      assertTrue( headOrientBehavior.hasInputBeenSet() );
+      assertTrue(headOrientBehavior.hasInputBeenSet());
 
-      FramePose initialHeadPose = getCurrentHeadPose(fullRobotModel);
-      success = success && executeBehavior(headOrientBehavior, trajectoryTime);
-      FramePose finalHeadPose = getCurrentHeadPose(fullRobotModel);
+      FramePose initialHeadPose = getCurrentHeadPose(drcBehaviorTestHelper.getFullRobotModel());
+      success = drcBehaviorTestHelper.executeBehaviorSimulateAndBlockAndCatchExceptions(headOrientBehavior, trajectoryTime + EXTRA_SIM_TIME_FOR_SETTLING);
+      assertTrue(success);
+      FramePose finalHeadPose = getCurrentHeadPose(drcBehaviorTestHelper.getFullRobotModel());
 
       if (DEBUG)
       {
          SysoutTool.println(" initial Head Pose :\n" + initialHeadPose);
       }
+
       FramePose desiredHeadPose = new FramePose();
       desiredHeadPose.setPose(initialHeadPose.getFramePointCopy().getPoint(), headOrientationPacket.quaternion);
-
       assertPosesAreWithinThresholds(desiredHeadPose, finalHeadPose);
-
-      assertTrue(success);
-
-      return headOrientBehavior;
+      assertTrue(headOrientBehavior.isDone());
    }
 
    private FramePose getCurrentHeadPose(FullRobotModel fullRobotModel)
@@ -248,54 +211,6 @@ public abstract class DRCHeadOrientationBehaviorTest implements MultiRobotTestIn
 
       ret.setToZero(headFrame);
       ret.changeFrame(ReferenceFrame.getWorldFrame());
-
-      return ret;
-   }
-
-   private boolean executeBehavior(final BehaviorInterface behavior, double trajectoryTime) throws SimulationExceededMaximumTimeException
-   {
-      final double simulationRunTime = trajectoryTime + EXTRA_SIM_TIME_FOR_SETTLING;
-
-      if (DEBUG)
-      {
-         System.out.println("\n");
-         SysoutTool.println("starting behavior: " + behavior.getName() + "   t = " + yoTime.getDoubleValue());
-      }
-
-      Thread behaviorThread = new Thread()
-      {
-         public void run()
-         {
-            {
-               double startTime = Double.NaN;
-               boolean simStillRunning = true;
-               boolean initalized = false;
-
-               while (simStillRunning)
-               {
-                  if (!initalized)
-                  {
-                     startTime = yoTime.getDoubleValue();
-                     initalized = true;
-                  }
-
-                  double timeSpentSimulating = yoTime.getDoubleValue() - startTime;
-                  simStillRunning = timeSpentSimulating < simulationRunTime;
-
-                  behavior.doControl();
-               }
-            }
-         }
-      };
-
-      behaviorThread.start();
-
-      boolean ret = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(simulationRunTime);
-
-      if (DEBUG)
-      {
-         SysoutTool.println("done with behavior: " + behavior.getName() + "   t = " + yoTime.getDoubleValue());
-      }
 
       return ret;
    }
