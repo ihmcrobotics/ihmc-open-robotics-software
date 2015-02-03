e5c0db073684e9b1f57a6d072f0a6f561f57ac24
Jerry Pratt <jpratt@ihmc.us>
2015-02-03 17:38:08 +0000

    GFE-27: Removed a bunch of SCS constructors. Won't fixBamboo.

--- src/us/ihmc/valkyrie/visualizer/ValkyrieSystemIdentificationSCSUI.java
+++ src/us/ihmc/valkyrie/visualizer/ValkyrieSystemIdentificationSCSUI.java
@@ -10,6 +10,7 @@ import us.ihmc.CoM.LinkComIDActionListener;
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 
 public class ValkyrieSystemIdentificationSCSUI
@@ -22,7 +23,10 @@ public class ValkyrieSystemIdentificationSCSUI
       // initialize SCS
      DRCRobotModel robotModel = new ValkyrieRobotModel(true,false);
       final SDFRobot robot =robotModel.createSdfRobot(false);
-      scs = new SimulationConstructionSet(robot, 65536);
+      
+      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
+      parameters.setDataBufferSize(65536);
+      scs = new SimulationConstructionSet(robot, parameters);
 
       // add sysid button
       JButton button = new JButton("findCOM");
