4864cb99da28fc895fe3e360162e882af602d78f
Duncan Calvert <dcalvert@ihmc.us>
2015-02-03 20:35:36 +0000

    GFE-172: Generated test suites now go in generatedTestSuites directory.

---
+++ test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversUtilitiesTestSuite.java
@@ -0,0 +1,28 @@
+package us.ihmc.valkyrie.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//import us.ihmc.utilities.code.unitTesting.runner.JUnitTestSuiteRunner;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.valkyrie.codeGenerators.APIBuilderTest.class,
+   us.ihmc.valkyrie.kinematics.ClosedFormJacobianTest.class,
+   us.ihmc.valkyrie.kinematics.PushrodTransmissionTest.class,
+   us.ihmc.valkyrie.kinematics.transmissions.ComparePushRodTransmissionsTest.class,
+   us.ihmc.valkyrie.kinematics.transmissions.InefficientPushrodTransmissionJacobianTest.class,
+   us.ihmc.valkyrie.kinematics.transmissions.InefficientPushRodTransmissionTest.class,
+   us.ihmc.valkyrie.kinematics.will.WillsPushrodTransmissionTest.class,
+   us.ihmc.valkyrie.simulation.ValkyriePosePlaybackDemoTest.class
+})
+
+public class ValkyrieHardwareDriversUtilitiesTestSuite
+{
+   public static void main(String[] args)
+   {
+      //new JUnitTestSuiteRunner(ValkyrieHardwareDriversUtilitiesTestSuite.class);
+   }
+}
+
