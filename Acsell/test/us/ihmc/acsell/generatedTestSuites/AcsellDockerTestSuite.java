e4d16bb6b8d057d210ff39013df61329df7cb8a0
Duncan Calvert <dcalvert@ihmc.us>
2015-02-03 20:35:36 +0000

    GFE-172: Generated test suites now go in generatedTestSuites directory.

---
+++ Acsell/test/us/ihmc/acsell/generatedTestSuites/AcsellDockerTestSuite.java
@@ -0,0 +1,22 @@
+package us.ihmc.acsell.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//import us.ihmc.utilities.code.unitTesting.runner.JUnitTestSuiteRunner;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.acsell.BonoFlatGroundWalkingKinematicFootSwitchTest.class,
+   us.ihmc.acsell.BonoFlatGroundWalkingTest.class
+})
+
+public class AcsellDockerTestSuite
+{
+   public static void main(String[] args)
+   {
+      //new JUnitTestSuiteRunner(AcsellDockerTestSuite.class);
+   }
+}
+
