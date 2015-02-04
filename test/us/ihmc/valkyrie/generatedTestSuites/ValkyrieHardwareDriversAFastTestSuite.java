4baae7ba258b354505e5b4dcc3e55495b1b2f43c
Duncan Calvert <dcalvert@ihmc.us>
2015-02-04 17:48:20 +0000

    GFE-217: Fast Slow UI Video implemented and test suites generated.

---
+++ test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversAFastTestSuite.java
@@ -0,0 +1,40 @@
+package us.ihmc.valkyrie.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//import us.ihmc.utilities.code.unitTesting.runner.JUnitTestSuiteRunner;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.valkyrie.codeGenerators.APIBuilderTest.class,
+   us.ihmc.valkyrie.kinematics.ClosedFormJacobianTest.class,
+   us.ihmc.valkyrie.kinematics.PushrodTransmissionTest.class,
+   us.ihmc.valkyrie.kinematics.transmissions.ComparePushRodTransmissionsTest.class,
+   us.ihmc.valkyrie.kinematics.transmissions.InefficientPushrodTransmissionJacobianTest.class,
+   us.ihmc.valkyrie.kinematics.transmissions.InefficientPushRodTransmissionTest.class,
+   us.ihmc.valkyrie.kinematics.will.WillsPushrodTransmissionTest.class,
+   us.ihmc.valkyrie.networkProcessor.depthData.ValkyrieDepthDataProcessorTest.class,
+   us.ihmc.valkyrie.simulation.ValkyrieFlatGroundWalkingWithIMUDriftTest.class,
+   us.ihmc.valkyrie.simulation.ValkyriePosePlaybackDemoTest.class,
+   us.ihmc.valkyrie.ValkyrieFlatGroundWalkingTest.class,
+   us.ihmc.valkyrie.ValkyrieObstacleCourseEveryBuildTest.class,
+   us.ihmc.valkyrie.ValkyrieObstacleCourseFlatTest.class,
+   us.ihmc.valkyrie.ValkyrieObstacleCourseRampsTest.class,
+   us.ihmc.valkyrie.ValkyrieObstacleCourseTrialsTerrainTest.class,
+   us.ihmc.valkyrie.ValkyriePushRecoveryMultiStepTest.class,
+   us.ihmc.valkyrie.ValkyriePushRecoveryStandingTest.class,
+   us.ihmc.valkyrie.ValkyriePushRecoveryTest.class,
+   us.ihmc.valkyrie.ValkyriePushRecoveryWalkingTest.class
+})
+
+public class ValkyrieHardwareDriversAFastTestSuite
+{
+   public static void main(String[] args)
+   {
+      //new JUnitTestSuiteRunner(ValkyrieHardwareDriversAFastTestSuite.class);
+   }
+}
+
