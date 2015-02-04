ee07b5f41362dc55a6d9c36bedfe6d9f549e92e0
Duncan Calvert <dcalvert@ihmc.us>
2015-02-04 18:17:31 +0000

    GFE-217: Renamed JUnitTestSuiteGenerator to BambooTestSuiteGenerator and removed references to ancient methods.

--- test/us/ihmc/valkyrie/ValkyrieBambooValkyrieTestSuite.java
+++ test/us/ihmc/valkyrie/ValkyrieBambooValkyrieTestSuite.java
@@ -3,8 +3,6 @@ package us.ihmc.valkyrie;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import us.ihmc.utilities.code.unitTesting.generator.JUnitTestSuiteGenerator;
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
@@ -22,6 +20,5 @@ public class ValkyrieBambooValkyrieTestSuite
 {
    public static void main(String[] args)
    {
-      JUnitTestSuiteGenerator.generateTestSuite(ValkyrieBambooValkyrieTestSuite.class);
    }
 }
