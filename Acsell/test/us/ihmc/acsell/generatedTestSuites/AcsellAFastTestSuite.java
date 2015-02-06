dd381a0d6f3c204bd524089aa200df078f5e5e4a
Duncan Calvert <dcalvert@ihmc.us>
2015-02-06 22:30:03 +0000

    GFE-217: Introduced local-to-minion speed modifier so load-balancing takes into account that minions are slower than developer PCs. Renamed JUnitTestSuiteRunner to BambooTestSuiteRunner. Will help fixBamboo.

--- Acsell/test/us/ihmc/acsell/generatedTestSuites/AcsellAFastTestSuite.java
+++ Acsell/test/us/ihmc/acsell/generatedTestSuites/AcsellAFastTestSuite.java
@@ -3,7 +3,7 @@ package us.ihmc.acsell.generatedTestSuites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import us.ihmc.utilities.code.unitTesting.runner.JUnitTestSuiteRunner;
+import us.ihmc.utilities.code.unitTesting.runner.BambooTestSuiteRunner;
 
 /** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
 @RunWith(Suite.class)
@@ -17,7 +17,7 @@ public class AcsellAFastTestSuite
 {
    public static void main(String[] args)
    {
-      new JUnitTestSuiteRunner(AcsellAFastTestSuite.class);
+      new BambooTestSuiteRunner(AcsellAFastTestSuite.class);
    }
 }
 
