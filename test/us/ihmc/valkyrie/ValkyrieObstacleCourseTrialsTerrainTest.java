d8f442b6e612d306f256ce99e7ac908e17a91950
Duncan Calvert <dcalvert@ihmc.us>
2015-02-08 20:35:12 +0000

    GFE-217: Added a bunch of Video annotations and generated test suites.

--- test/us/ihmc/valkyrie/ValkyrieObstacleCourseTrialsTerrainTest.java
+++ test/us/ihmc/valkyrie/ValkyrieObstacleCourseTrialsTerrainTest.java
@@ -3,7 +3,10 @@ package us.ihmc.valkyrie;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCObstacleCourseTrialsTerrainTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.utilities.code.unitTesting.BambooPlanType;
+import us.ihmc.utilities.code.unitTesting.BambooAnnotations.BambooPlan;
 
+@BambooPlan(planType = {BambooPlanType.Fast, BambooPlanType.Video})
 public class ValkyrieObstacleCourseTrialsTerrainTest extends DRCObstacleCourseTrialsTerrainTest
 {
 
