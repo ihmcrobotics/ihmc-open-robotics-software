a852854646e095cc0a53e08e0f02362cc98ca300
Duncan Calvert <dcalvert@ihmc.us>
2015-02-08 20:35:12 +0000

    GFE-217: Added a bunch of Video annotations and generated test suites.

---
+++ test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversAVideoTestSuite.java
@@ -0,0 +1,23 @@
+package us.ihmc.valkyrie.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import us.ihmc.utilities.code.unitTesting.runner.BambooTestSuiteRunner;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.valkyrie.ValkyrieFlatGroundWalkingTest.class,
+   us.ihmc.valkyrie.ValkyrieObstacleCourseFlatTest.class
+})
+
+public class ValkyrieHardwareDriversAVideoTestSuite
+{
+   public static void main(String[] args)
+   {
+      new BambooTestSuiteRunner(ValkyrieHardwareDriversAVideoTestSuite.class);
+   }
+}
+
