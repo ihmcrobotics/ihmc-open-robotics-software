ef0477853dd09744a9099bacf2198875f24a7474
Atlas Control Computer <AtlasControlComputer@unknown>
2015-02-09 15:58:59 +0000

    NONE: Added comments plus parameters for Steppr in StraightLinePathType. This needs to be refactored to be robot agnostic.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/controllers/StraightLinePathType.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/controllers/StraightLinePathType.java
@@ -1,19 +1,20 @@
 package us.ihmc.darpaRoboticsChallenge.controllers;
 
+// FIXME Extract these constant to be robot agnostic.
 public enum StraightLinePathType
 {
    STRAIGHT, REVERSE, LEFT_SHUFFLE, RIGHT_SHUFFLE;
 
    private static final String STRAIGHT_PATH_NAME = "Forward Path";
-   private static final double STRAIGHT_STEP_LENGTH = 0.4;
-   private static final double STRAIGHT_STEP_WIDTH = 0.25;
+   private static final double STRAIGHT_STEP_LENGTH = 0.4; // For Steppr: 0.30;
+   private static final double STRAIGHT_STEP_WIDTH = 0.25; // For Steppr: 0.35;
    private static final String REVERSE_PATH_NAME = "Reverse Path";
    private static final double REVERSE_ANGLE = Math.PI;
    private static final double REVERSE_STEP_LENGTH = 0.15;
-   private static final double REVERSE_STEP_WIDTH = 0.25;
+   private static final double REVERSE_STEP_WIDTH = 0.25; // For Steppr: 0.35;
    private static final String RIGHT_SHUFFLE_PATH_NAME = "Right Shuffle Path";
    private static final String LEFT_SHUFFLE_PATH_NAME = "Left Shuffle Path";
-   private static final double SHUFFLE_STEP_LENGTH = 0.25;
+   private static final double SHUFFLE_STEP_LENGTH = 0.25;  // For Steppr: 0.3;
    private static final double SHUFFLE_STEP_WIDTH = 0.21;
    private static final double LEFT_SHUFFLE_ANGLE = -Math.PI / 2;
 
