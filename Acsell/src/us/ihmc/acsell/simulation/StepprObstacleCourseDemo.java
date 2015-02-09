6cad9063b3ea904d7cc7cf61d66472899fd023dc
Sylvain Bertrand <sbertrand@ihmc.us>
2015-02-09 19:26:25 +0000

    NONE: Use the new DRCSimulationStarter for the obstacle course demos.

--- Acsell/src/us/ihmc/acsell/simulation/StepprObstacleCourseDemo.java
+++ Acsell/src/us/ihmc/acsell/simulation/StepprObstacleCourseDemo.java
@@ -1,30 +1,16 @@
 package us.ihmc.acsell.simulation;
 
-import us.ihmc.acsell.operatorInterface.StepprOperatorInterface;
 import us.ihmc.acsell.parameters.BonoRobotModel;
-import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseDemoStarter;
+import us.ihmc.darpaRoboticsChallenge.DRCSimulationStarter;
+import us.ihmc.darpaRoboticsChallenge.DRCSimulationTools;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
-import us.ihmc.utilities.processManagement.JavaProcessSpawner;
 
-public class StepprObstacleCourseDemo extends DRCObstacleCourseDemoStarter
+public class StepprObstacleCourseDemo
 {
-   private static final DRCRobotModel robotModel = new BonoRobotModel(false, false);
-
    public static void main(String[] args)
    {
-      StepprObstacleCourseDemo stepprObstacleCourseDemo = new StepprObstacleCourseDemo();
-      boolean automaticallyStartSimulation = true;
-      boolean startDRCNetworkProcessor = true; // set to false to use slider board instead.
-      boolean initializeEstimatorToActual = false;
-      
-      stepprObstacleCourseDemo.obstacleCourseStarter(robotModel, initializeEstimatorToActual, automaticallyStartSimulation, startDRCNetworkProcessor);
-   }
-
-   @Override
-   public void SpawnUI(DRCRobotModel robotModel)
-   {
-      JavaProcessSpawner spawner = new JavaProcessSpawner(true);
-      String[] args = {};
-      spawner.spawn(StepprOperatorInterface.class, args);
+      DRCRobotModel robotModel = new BonoRobotModel(false, false);
+      DRCSimulationStarter createDRCDemo01SimulationStarter = DRCSimulationTools.createObstacleCourseSimulationStarter(robotModel);
+      DRCSimulationTools.startSimulationWithGraphicSelector(createDRCDemo01SimulationStarter);
    }
 }
