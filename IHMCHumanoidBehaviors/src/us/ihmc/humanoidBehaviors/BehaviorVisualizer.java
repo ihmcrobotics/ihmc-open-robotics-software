2e20c18852fb290049965e5caead4c75b12d9ecc
Sylvain Bertrand <sbertrand@ihmc.us>
2015-02-09 00:50:04 +0000

    NONE: Made the BehaviorVisualizer use args to determine if running on the real robot.

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/BehaviorVisualizer.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/BehaviorVisualizer.java
@@ -3,26 +3,47 @@ package us.ihmc.humanoidBehaviors;
 import us.ihmc.robotDataCommunication.YoVariableClient;
 import us.ihmc.robotDataCommunication.visualizer.SCSVisualizer;
 
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.Switch;
+
 public class BehaviorVisualizer extends SCSVisualizer
 {
+   private static final String LOCAL_IP = "127.0.0.1";
+   private static final String CONTROLLER_IP = "192.168.130.112";
+
    private final boolean showOverheadView = false;
-   
-   private final static boolean onRealRobot = false;
-   
+
    public BehaviorVisualizer(String host, int bufferSize)
    {
       super(bufferSize, true, false);
-      
+
       YoVariableClient client = new YoVariableClient(host, this, "behavior", showOverheadView);
       client.start();
    }
-   
+
    public static void main(String[] args)
    {
-      if (!onRealRobot)
-         new BehaviorVisualizer("127.0.0.1", 1024*32);
-      else
-         new BehaviorVisualizer("192.168.130.112", 1024*32);
-         
+      JSAP jsap = new JSAP();
+      Switch runningOnRealRobotSwitch = new Switch("runningOnRealRobot").setLongFlag("realRobot");
+
+      try
+      {
+         jsap.registerParameter(runningOnRealRobotSwitch);
+
+         JSAPResult config = jsap.parse(args);
+
+         if (config.success())
+         {
+            boolean runningOnRealRobot = config.getBoolean(runningOnRealRobotSwitch.getID());
+            String ipAddressToUse = runningOnRealRobot ? CONTROLLER_IP : LOCAL_IP;
+            new BehaviorVisualizer(ipAddressToUse, 1024 * 32);
+         }
+      }
+      catch (JSAPException e)
+      {
+         e.printStackTrace();
+      }
    }
 }
