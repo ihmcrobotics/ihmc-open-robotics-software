870ba686c48e7e822af4f8b08e0872add217d9d0
Sylvain Bertrand <sbertrand@ihmc.us>
2015-02-10 22:54:04 +0000

    NONE: Set keep SCS up back to false. Don't commit that file in. Won't fixbamboo

--- SimulationConstructionSet/src/us/ihmc/simulationconstructionset/bambooTools/SimulationTestingParameters.java
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/bambooTools/SimulationTestingParameters.java
@@ -8,7 +8,7 @@ public class SimulationTestingParameters extends SimulationConstructionSetParame
 {
    private boolean createSCSMovies = false;
    private boolean checkNothingChangedInSimulation = false;
-   private boolean keepSCSUp = true;
+   private boolean keepSCSUp = false;
    
    public SimulationTestingParameters()
    {
