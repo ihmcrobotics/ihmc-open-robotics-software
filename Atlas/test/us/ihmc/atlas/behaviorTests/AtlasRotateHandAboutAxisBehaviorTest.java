34fd8d39cdabc539ebf5581b5d292740a6e55417
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-02-13 18:00:21 +0000

    NONE: created unit test for RotateHandAboutAxisBehavior.  Doesn't test anything yet.  won't fixBamboo.

---
+++ Atlas/test/us/ihmc/atlas/behaviorTests/AtlasRotateHandAboutAxisBehaviorTest.java
@@ -0,0 +1,35 @@
+package us.ihmc.atlas.behaviorTests;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.darpaRoboticsChallenge.behaviorTests.DRCRotateHandAboutAxisBehaviorTest;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.utilities.code.unitTesting.BambooAnnotations.BambooPlan;
+import us.ihmc.utilities.code.unitTesting.BambooPlanType;
+
+@BambooPlan(planType = {BambooPlanType.Fast, BambooPlanType.Video})
+public class AtlasRotateHandAboutAxisBehaviorTest extends DRCRotateHandAboutAxisBehaviorTest
+{
+   private final AtlasRobotModel robotModel;
+   
+   public AtlasRotateHandAboutAxisBehaviorTest()
+   {
+      robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false);      
+      robotModel.createHandContactPoints();
+   }
+   
+   
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+
+}
