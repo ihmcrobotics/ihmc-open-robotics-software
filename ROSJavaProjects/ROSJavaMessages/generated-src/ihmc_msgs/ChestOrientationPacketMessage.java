b7f93d8ab590d0da4c564a4a58e8aa19ab43d3c9
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-02-13 21:23:52 +0000

    none: fixBamboo, fixed ROSMessageConverterTest

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ChestOrientationPacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ChestOrientationPacketMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface ChestOrientationPacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/ChestOrientationPacketMessage";
-  static final java.lang.String _DEFINITION = "## ChestOrientationPacketMessage\r\n# This message gives the orientation of the robot\'s chest in world frame.\r\n\r\ngeometry_msgs/Quaternion orientation\r\nfloat64 trajectoryTime\r\n# toHomePosition can be used to move the Pelvis back to its starting position\r\nbool toHomeOrientation\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## ChestOrientationPacketMessage\r\n# This message sets the orientation of the robot\'s chest in world coordinates.\r\n\r\ngeometry_msgs/Quaternion orientation\r\n\r\n# trajectoryTime specifies how fast or how slow to move to the desired pose\r\nfloat64 trajectoryTime\r\n\r\n# toHomePosition can be used to move the chest back to its default starting position\r\nbool toHomeOrientation\r\n\r\n\r\n";
   geometry_msgs.Quaternion getOrientation();
   void setOrientation(geometry_msgs.Quaternion value);
   double getTrajectoryTime();
--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootPosePacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootPosePacketMessage";
-  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\r\n# This message gives the position and oreientation of one foot in world coordinates.\r\n\r\n#Options for enum\r\n# uint8 L = 0\r\n# uint8 R = 1\r\nuint8 robotSide\r\ngeometry_msgs/Vector3 position\r\ngeometry_msgs/Quaternion orientation\r\nfloat64 trajectoryTime\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\r\n# This message commands the controller to move the desired foot to a given location\r\n# and orientation in world coordinates. This message is not intended to specify footsteps\r\n# as the controller will use a straight-line trajectory when moving the foot from its\r\n# current to desired location/orientation\r\n\r\n#Options for robotSide\r\n# uint8 L = 0\r\n# uint8 R = 1\r\nuint8 robotSide\r\n\r\ngeometry_msgs/Vector3 position\r\n\r\ngeometry_msgs/Quaternion orientation\r\n\r\n# trajectoryTime specifies how fast or how slow to move to the desired pose\r\nfloat64 trajectoryTime\r\n\r\n\r\n";
   byte getRobotSide();
   void setRobotSide(byte value);
   geometry_msgs.Vector3 getPosition();
