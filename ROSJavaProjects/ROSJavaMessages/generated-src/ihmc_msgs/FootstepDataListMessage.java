f2970502a9be42fe113726374b944bea595371c4
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-02-14 02:03:05 +0000

    none: fixBamboo, fixed rosMsgs and null pointer in O Course FlatTest

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepDataListMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepDataListMessage.java
@@ -2,13 +2,9 @@ package ihmc_msgs;
 
 public interface FootstepDataListMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootstepDataListMessage";
-  static final java.lang.String _DEFINITION = "## FootstepDataListMessage\r\n# This message commands the controller to execute a list of footsteps. See FootstepDataMessage\r\n# for information about defining a footstep.\r\n\r\nFootstepDataMessage[] footstepDataList\r\n\r\n# DEFAULT gives the standard IHMC parabolic footstep trajectory\r\n# BY_BOX gives a square-wave type trajectory according to the parameters of trajectoryBoxData\r\n# STEP_ON_OR_OFF gives a higher-arched trajectory meant for stepping onto or off obstacles\r\n# NO_STEP results in a null footstep. This probably should not be used outside of IHMC source\r\n# LOW_HEIGHT gives a lower-arched trajectory\r\n#Options for trajectoryWaypointGenerationMethod\r\n# uint8 DEFAULT = 0\r\n# uint8 BY_BOX = 1\r\n# uint8 STEP_ON_OR_OFF = 2\r\n# uint8 NO_STEP = 3\r\n# uint8 LOW_HEIGHT = 4\r\nuint8 trajectoryWaypointGenerationMethod\r\n\r\n# trajectoryBoxData describes the position, orientation and dimension of\r\n# the box used to generate the trajectory waypoints for the BY_BOX method\r\nSquareDataMessage trajectoryBoxData\r\n\r\n# swingTime is the time spent in single-support when stepping\r\nfloat64 swingTime\r\n\r\n# transferTime is the time spent in double-support between steps\r\nfloat64 transferTime\r\n\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## FootstepDataListMessage\r\n# This message commands the controller to execute a list of footsteps. See FootstepDataMessage\r\n# for information about defining a footstep.\r\n\r\nFootstepDataMessage[] footstepDataList\r\n\r\n# swingTime is the time spent in single-support when stepping\r\nfloat64 swingTime\r\n\r\n# transferTime is the time spent in double-support between steps\r\nfloat64 transferTime\r\n\r\n\r\n";
   java.util.List<ihmc_msgs.FootstepDataMessage> getFootstepDataList();
   void setFootstepDataList(java.util.List<ihmc_msgs.FootstepDataMessage> value);
-  byte getTrajectoryWaypointGenerationMethod();
-  void setTrajectoryWaypointGenerationMethod(byte value);
-  ihmc_msgs.SquareDataMessage getTrajectoryBoxData();
-  void setTrajectoryBoxData(ihmc_msgs.SquareDataMessage value);
   double getSwingTime();
   void setSwingTime(double value);
   double getTransferTime();
--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepDataMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepDataMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootstepDataMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootstepDataMessage";
-  static final java.lang.String _DEFINITION = "## FootstepDataMessage\r\n# This message specifies the position, orientation and side (left or right) of a desired footstep in\r\n# world frame\r\n\r\n#Options for robotSide\r\n# uint8 L = 0\r\n# uint8 R = 1\r\nuint8 robotSide\r\n\r\ngeometry_msgs/Vector3 location\r\n\r\ngeometry_msgs/Quaternion orientation\r\n\r\n# predictedContactPoints gives the vertices of the expected contact polygon between the foot and\r\n# the world. A value of null will use the default controller contact points\r\nPoint2dMessage[] predictedContactPoints\r\n\r\n\r\n";
+  static final java.lang.String _DEFINITION = "## FootstepDataMessage\r\n# This message specifies the position, orientation and side (left or right) of a desired footstep in\r\n# world frame\r\n\r\n#Options for robotSide\r\n# uint8 L = 0\r\n# uint8 R = 1\r\nuint8 robotSide\r\n\r\ngeometry_msgs/Vector3 location\r\n\r\ngeometry_msgs/Quaternion orientation\r\n\r\n# predictedContactPoints gives the vertices of the expected contact polygon between the foot and\r\n# the world. A value of null will use the default controller contact points\r\nPoint2dMessage[] predictedContactPoints\r\n\r\n# This contains information on what the swing trajectory should be for each step\r\n# DEFAULT gives the standard IHMC parabolic footstep trajectory\r\n# LOW_HEIGHT gives a lower-arched trajectory\r\n# STEP_ON_OR_OFF gives a higher-arched trajectory meant for stepping onto or off obstacles\r\n# BY_BOX gives a square-wave type trajectory according to the parameters of trajectoryBoxData\r\n# NO_STEP results in a null footstep. This probably should not be used outside of IHMC source\r\n#Options for trajectoryGenerationMethod\r\n# uint8 DEFAULT = 0\r\n# uint8 LOW_HEIGHT = 1\r\n# uint8 STEP_ON_OR_OFF = 2\r\n# uint8 BY_BOX = 3\r\nuint8 trajectoryGenerationMethod\r\n\r\n# Contains information on how high the robot should step\r\nfloat64 swingHeight\r\n\r\n\r\n";
   byte getRobotSide();
   void setRobotSide(byte value);
   geometry_msgs.Vector3 getLocation();
@@ -11,4 +11,8 @@ public interface FootstepDataMessage extends org.ros.internal.message.Message {
   void setOrientation(geometry_msgs.Quaternion value);
   java.util.List<ihmc_msgs.Point2dMessage> getPredictedContactPoints();
   void setPredictedContactPoints(java.util.List<ihmc_msgs.Point2dMessage> value);
+  byte getTrajectoryGenerationMethod();
+  void setTrajectoryGenerationMethod(byte value);
+  double getSwingHeight();
+  void setSwingHeight(double value);
 }
