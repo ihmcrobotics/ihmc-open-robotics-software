6d250db11d661c73da02f79ac73281dc6a3d1eba
Sylvain Bertrand <sbertrand@ihmc.us>
2015-02-15 18:20:36 +0000

    NONE: Reverted changes on the pelvis trajectory with waypoints. Won't fixbamboo.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/PelvisPoseProvider.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/PelvisPoseProvider.java
@@ -3,14 +3,10 @@ package us.ihmc.commonWalkingControlModules.packetConsumers;
 import java.util.ArrayList;
 
 import javax.vecmath.Point3d;
-import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
 
 import us.ihmc.utilities.math.geometry.FrameOrientation;
-import us.ihmc.utilities.math.geometry.FrameOrientationWaypoint;
 import us.ihmc.utilities.math.geometry.FramePoint;
-import us.ihmc.utilities.math.geometry.FramePointWaypoint;
-import us.ihmc.utilities.math.geometry.FrameVector;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 
 public interface PelvisPoseProvider
@@ -23,10 +19,21 @@ public interface PelvisPoseProvider
 
    public abstract boolean checkForHomeOrientation();
    
-   public abstract Double getTrajectoryTimeToHome();
-      
-   public abstract FramePointWaypoint[] getDesiredPelvisPosition(ReferenceFrame desiredReferenceFrame);
+   public abstract boolean checkForNewTrajectory();
+   
+   public abstract void removeLastTrajectory();
 
-   public abstract FrameOrientationWaypoint[] getDesiredPelvisOrientation(ReferenceFrame desiredReferenceFrame);
+   public abstract FramePoint getDesiredPelvisPosition(ReferenceFrame supportFrame);
+   
+   public abstract ReferenceFrame getDesiredPelvisPositionTrajectory(
+         ArrayList<Double> time,
+         ArrayList<Point3d> position, 
+         ArrayList<Vector3d> velocity);
+
+   public abstract FrameOrientation getDesiredPelvisOrientation(ReferenceFrame desiredPelvisFrame);
+   
+   public abstract ReferenceFrame getDesiredPelvisOrientationTrajectory(double[] time, Point3d[] position, Vector3d[] velocity);
+
+   public abstract double getTrajectoryTime();
    
 }
