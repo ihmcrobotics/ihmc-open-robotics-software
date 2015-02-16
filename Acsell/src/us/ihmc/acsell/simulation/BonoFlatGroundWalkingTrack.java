65f3937f4572fdb317ba478c0995fbcc325d4e65
Tingfan Wu <twu@ihmc.us>
2015-02-16 13:49:08 +0000

    DRC-2235: add fix to joystick not found cases not fixbamboo

--- Acsell/src/us/ihmc/acsell/simulation/BonoFlatGroundWalkingTrack.java
+++ Acsell/src/us/ihmc/acsell/simulation/BonoFlatGroundWalkingTrack.java
@@ -2,12 +2,14 @@ package us.ihmc.acsell.simulation;
 
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.acsell.parameters.BonoRobotModel;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.WalkingProvider;
 import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
 import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
 import us.ihmc.graphics3DAdapter.GroundProfile3D;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.joystick.JoystickUpdater;
 import us.ihmc.simulationconstructionset.util.ground.FlatGroundProfile;
 import us.ihmc.steppr.hardware.visualization.StepprStandPrepSliderboard;
 
@@ -15,6 +17,8 @@ public class BonoFlatGroundWalkingTrack
 {
    public static void main(String[] args)
    {
+      boolean USE_JOYSTICK_CONTROLLER = JoystickUpdater.isJoyStickConnected();
+
       BonoRobotModel robotModel = new BonoRobotModel(false, false);
       DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false);
 
@@ -28,17 +32,22 @@ public class BonoFlatGroundWalkingTrack
 
       double initialYaw = 0.3;
       DRCRobotInitialSetup<SDFRobot> robotInitialSetup = robotModel.getDefaultRobotInitialSetup(groundHeight, initialYaw);
-      boolean useVelocityAndHeadingScript = false;
+      boolean useVelocityAndHeadingScript = !USE_JOYSTICK_CONTROLLER;
       boolean cheatWithGroundHeightAtForFootstep = false;
 
-      DRCFlatGroundWalkingTrack flatGroundWalkingTrack=new DRCFlatGroundWalkingTrack(robotInitialSetup, guiInitialSetup, scsInitialSetup,
-                                    useVelocityAndHeadingScript, cheatWithGroundHeightAtForFootstep, robotModel);
-      
-      
+      DRCFlatGroundWalkingTrack flatGroundWalkingTrack = new DRCFlatGroundWalkingTrack(robotInitialSetup, guiInitialSetup, scsInitialSetup,
+                                                            useVelocityAndHeadingScript, cheatWithGroundHeightAtForFootstep, robotModel,
+                                                            WalkingProvider.VELOCITY_HEADING_COMPONENT);
+
 
       SimulationConstructionSet scs = flatGroundWalkingTrack.getSimulationConstructionSet();
-      StepprStandPrepSliderboard.setupJoyStick(scs);
+      if (USE_JOYSTICK_CONTROLLER)
+      {
+         StepprStandPrepSliderboard.setupJoyStick(scs);
+         flatGroundWalkingTrack.getDrcSimulation().start();
+         flatGroundWalkingTrack.getDrcSimulation().simulate();
+      }
    }
-   
- 
+
+
 }
