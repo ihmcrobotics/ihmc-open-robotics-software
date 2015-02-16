c1ff6c6634b55cb6581242f707d7f182499ee84b
Duncan Calvert <dcalvert@ihmc.us>
2015-02-16 23:30:46 +0000

    GFE-246: Fixed generation of A and B test suites. Will help fixBamboo.

--- test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversCVideoATestSuite.java
+++ test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversCVideoATestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.valkyrie.ValkyrieObstacleCourseRampsTest.class,
+   us.ihmc.valkyrie.ValkyrieObstacleCourseTrialsTerrainTest.class,
 })
 
 public class ValkyrieHardwareDriversCVideoATestSuite
