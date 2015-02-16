d18d1ba66ebcbc12a9a511e8898646e58da92471
Jerry Pratt <jpratt@ihmc.us>
2015-02-16 00:07:07 +0000

    DRC-2258: Have DoNothingVariousWalkingProviderFactory actually do nothing.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/DoNothingVariousWalkingProviderFactory.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/DoNothingVariousWalkingProviderFactory.java
@@ -1,11 +1,82 @@
 package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
-public class DoNothingVariousWalkingProviderFactory extends ComponentBasedVariousWalkingProviderFactory
-{   
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.commonWalkingControlModules.controllers.Updatable;
+import us.ihmc.commonWalkingControlModules.desiredFootStep.DesiredFootstepCalculatorFootstepProviderWrapper;
+import us.ihmc.commonWalkingControlModules.packetConsumers.ChestOrientationProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredComHeightProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredFootPoseProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredFootStateProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredHandPoseProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredPelvisLoadBearingProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.DesiredThighLoadBearingProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.HandLoadBearingProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.HandstepProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.HeadOrientationProvider;
+import us.ihmc.commonWalkingControlModules.packetConsumers.PelvisPoseProvider;
+import us.ihmc.commonWalkingControlModules.packetProducers.CapturabilityBasedStatusProducer;
+import us.ihmc.commonWalkingControlModules.packetProducers.HandPoseStatusProducer;
+import us.ihmc.commonWalkingControlModules.packetProviders.ControlStatusProducer;
+import us.ihmc.commonWalkingControlModules.packetProviders.DesiredHighLevelStateProvider;
+import us.ihmc.commonWalkingControlModules.trajectories.ConstantSwingTimeCalculator;
+import us.ihmc.commonWalkingControlModules.trajectories.ConstantTransferTimeCalculator;
+import us.ihmc.utilities.humanoidRobot.bipedSupportPolygons.ContactablePlaneBody;
+import us.ihmc.utilities.humanoidRobot.footstep.Footstep;
+import us.ihmc.utilities.humanoidRobot.frames.CommonHumanoidReferenceFrames;
+import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
+import us.ihmc.utilities.math.trajectories.providers.TrajectoryParameters;
+import us.ihmc.utilities.robotSide.SideDependentList;
+import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+import us.ihmc.yoUtilities.graphics.YoGraphicsListRegistry;
+
+
+public class DoNothingVariousWalkingProviderFactory implements VariousWalkingProviderFactory    // extends ComponentBasedVariousWalkingProviderFactory
+{
    public DoNothingVariousWalkingProviderFactory(double controlDT)
    {
-      super(false, null, controlDT);
+   }
+
+
+   public VariousWalkingProviders createVariousWalkingProviders(DoubleYoVariable yoTime, FullRobotModel fullRobotModel,
+           WalkingControllerParameters walkingControllerParameters, CommonHumanoidReferenceFrames referenceFrames,
+           SideDependentList<ContactablePlaneBody> feet, ConstantTransferTimeCalculator transferTimeCalculator,
+           ConstantSwingTimeCalculator swingTimeCalculator, ArrayList<Updatable> updatables, YoVariableRegistry registry,
+           YoGraphicsListRegistry yoGraphicsListRegistry)
+   {
+      HandstepProvider handstepProvider = null;
+      DesiredFootstepCalculatorFootstepProviderWrapper footstepProvider = null;
+      LinkedHashMap<Footstep, TrajectoryParameters> mapFromFootstepsToTrajectoryParameters = new LinkedHashMap<Footstep, TrajectoryParameters>();
+
+      DesiredHighLevelStateProvider highLevelStateProvider = null;
+      HeadOrientationProvider headOrientationProvider = null;
+      DesiredComHeightProvider comHeightProvider = null;
+      PelvisPoseProvider pelvisPoseProvider = null;
+      ChestOrientationProvider chestOrientationProvider = null;
+      DesiredHandPoseProvider handPoseProvider = null;
+      DesiredFootPoseProvider footPoseProvider = null;
+
+
+      HandLoadBearingProvider handLoadBearingProvider = null;
+      DesiredFootStateProvider footLoadBearingProvider = null;
+      DesiredPelvisLoadBearingProvider pelvisLoadBearingProvider = null;
+      DesiredThighLoadBearingProvider thighLoadBearingProvider = null;
+
+      ControlStatusProducer controlStatusProducer = null;
+
+      CapturabilityBasedStatusProducer capturabilityBasedStatusProducer = null;
+
+      HandPoseStatusProducer handPoseStatusProducer = null;
+
+      VariousWalkingProviders variousWalkingProviders = new VariousWalkingProviders(footstepProvider, handstepProvider, mapFromFootstepsToTrajectoryParameters,
+                                                           headOrientationProvider, comHeightProvider, pelvisPoseProvider, handPoseProvider,
+                                                           handLoadBearingProvider, chestOrientationProvider, footPoseProvider, footLoadBearingProvider,
+                                                           highLevelStateProvider, thighLoadBearingProvider, pelvisLoadBearingProvider, controlStatusProducer,
+                                                           capturabilityBasedStatusProducer, handPoseStatusProducer);
+
+      return variousWalkingProviders;
    }
 }
-
