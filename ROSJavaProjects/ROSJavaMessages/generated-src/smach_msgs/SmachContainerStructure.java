4c89e109d3ae6c2c35962adce108b04e9edd9865
Tingfan Wu <twu@ihmc.us>
2015-02-16 12:15:15 +0000

    NONE: new ROS messages (from IHMC packets) were generated. not fixbamboo

--- ROSJavaProjects/ROSJavaMessages/generated-src/smach_msgs/SmachContainerStructure.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/smach_msgs/SmachContainerStructure.java
@@ -2,7 +2,7 @@ package smach_msgs;
 
 public interface SmachContainerStructure extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "smach_msgs/SmachContainerStructure";
-  static final java.lang.String _DEFINITION = "Header header\n\n# The path to this node in the server\nstring path\n\n# The children of this node\nstring[] children\n\n# The outcome edges\n# Each index across these arrays denote one edge\nstring[] internal_outcomes\nstring[] outcomes_from\nstring[] outcomes_to\n\n# The potential outcomes from this container\nstring[] container_outcomes\n";
+  static final java.lang.String _DEFINITION = "Header header\n\n# The path to this node in the server\nstring path\n\n# The children of this node\nstring[] children\n\n# The outcome edges\nstring[] internal_outcomes\nstring[] outcomes_from\nstring[] outcomes_to\n\n# The potential outcomes from this container\nstring[] container_outcomes\n";
   std_msgs.Header getHeader();
   void setHeader(std_msgs.Header value);
   java.lang.String getPath();
