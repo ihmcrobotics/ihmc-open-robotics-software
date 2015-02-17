51f04b01dd1ba349f750000e2dbe7d0955e8bae0
Duncan Calvert <dcalvert@ihmc.us>
2015-02-17 18:50:21 +0000

    GFE-249: Created Flaky and InDevelopment plans. Will help fixBamboo.

--- test/us/ihmc/valkyrie/ValkyrieObstacleCourseEveryBuildTest.java
+++ test/us/ihmc/valkyrie/ValkyrieObstacleCourseEveryBuildTest.java
@@ -3,9 +3,9 @@ package us.ihmc.valkyrie;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCObstacleCourseEveryBuildTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
-import us.ihmc.utilities.code.agileTesting.BambooAnnotations.UnfinishedTest;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.ExcludedTest;
 
-@UnfinishedTest() // Crashes Bamboo
+@ExcludedTest() // Crashes Bamboo
 public class ValkyrieObstacleCourseEveryBuildTest extends DRCObstacleCourseEveryBuildTest
 {
 
