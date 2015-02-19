66ee2561f0065ddc8a42bb76f1ccc5fa6584b4fd
Duncan Calvert <dcalvert@ihmc.us>
2015-02-19 05:43:12 +0000

    GFE-253: Generated test suites. Won't fixBamboo.

---
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasIInDevelopmentTestSuite.java
@@ -0,0 +1,24 @@
+package us.ihmc.atlas.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.atlas.behaviorTests.AtlasWholeBodyInverseKinematicBehaviorTest.class,
+   us.ihmc.atlas.hikSim.AtlasWholeBodyIkSolverTest.class,
+   us.ihmc.atlas.HumanoidBehaviorsICPFaultDetectionTest.class,
+   us.ihmc.atlas.ObstacleCourseTests.AtlasBigStepUpWithHandPlatformTest.class,
+   us.ihmc.atlas.ObstacleCourseTests.AtlasWallWorldTest.class,
+   us.ihmc.atlas.stateEstimation.AtlasPelvisPoseHistoryCorrectorTest.class
+})
+
+public class AtlasIInDevelopmentTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
