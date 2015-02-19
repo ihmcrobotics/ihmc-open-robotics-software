30805bce127f93b37348f75f51e6655402d71930
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-02-19 19:00:58 +0000

    NONE: code cleanup.  won't fixBamboo.

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/RotateHandAboutAxisTask.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/RotateHandAboutAxisTask.java
@@ -32,6 +32,6 @@ public class RotateHandAboutAxisTask extends BehaviorTask
    @Override
    protected void setBehaviorInput()
    {
-      rotateHandAboutAxisBehavior.setInput(robotSide, graspedObjectTransformToWorld, pinJointAxisInGraspedObjectFrame, turnAngleRad, trajectoryTime);
+      rotateHandAboutAxisBehavior.setInput(robotSide, pinJointAxisInGraspedObjectFrame, graspedObjectTransformToWorld, turnAngleRad, trajectoryTime);
    }
 }
