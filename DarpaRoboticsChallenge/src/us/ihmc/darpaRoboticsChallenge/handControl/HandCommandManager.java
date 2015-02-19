95e608790414810ca386477566880d17762aaeec
Nathan Mertins <nmertins@ihmc.us>
2015-02-19 18:10:01 +0000

    DRC-2326: RobotiqHandCommandManager now connects and calibrates the hand again; still not receiving commands after that for some reason; won't fixbamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandCommandManager.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandCommandManager.java
@@ -3,11 +3,10 @@ package us.ihmc.darpaRoboticsChallenge.handControl;
 import java.io.IOException;
 
 import us.ihmc.communication.kryo.IHMCCommunicationKryoNetClassList;
-import us.ihmc.communication.packetCommunicator.KryoPacketClient;
+import us.ihmc.communication.packetCommunicator.KryoPacketServer;
 import us.ihmc.communication.packetCommunicator.interfaces.PacketCommunicator;
 import us.ihmc.communication.packets.Packet;
 import us.ihmc.communication.packets.PacketDestination;
-import us.ihmc.communication.util.NetworkConfigParameters;
 import us.ihmc.utilities.processManagement.JavaProcessSpawner;
 //TODO: rename and create interface for sending hand command
 public abstract class HandCommandManager //implements HandCommandInterface
@@ -17,9 +16,7 @@ public abstract class HandCommandManager //implements HandCommandInterface
 	   
 	protected JavaProcessSpawner spawner = new JavaProcessSpawner(true);
 	
-	
-	protected KryoPacketClient packetCommunicator = new KryoPacketClient(SERVER_ADDRESS, NetworkConfigParameters.NETWORK_PROCESSOR_TO_CONTROLLER_TCP_PORT,
-         new IHMCCommunicationKryoNetClassList(),PacketDestination.HAND_MANAGER.ordinal(),PacketDestination.HAND_MANAGER.ordinal(),"AtlasROSAPINetworkProcessor");
+	protected KryoPacketServer packetCommunicator = new KryoPacketServer(4270, new IHMCCommunicationKryoNetClassList(), PacketDestination.HAND_MANAGER.ordinal(), "HandCommandManagerServerCommunicator");
 	
 	public HandCommandManager(Class<? extends Object> clazz)
 	{
