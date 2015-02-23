b3bb79b4e043f892325735bff07f044ca2d765ae
Jesper Smith <jsmith@ihmc.us>
2015-02-23 16:10:25 +0000

    NONE: Disabled hand simulation and lidar subtrees. Disable visualizations for proper lidar world. THis will speed up the simulation a lot.

--- IHMCWholeBodyController/src/us/ihmc/wholeBodyController/DRCHandType.java
+++ IHMCWholeBodyController/src/us/ihmc/wholeBodyController/DRCHandType.java
@@ -1,5 +1,17 @@
 package us.ihmc.wholeBodyController;
 
 public enum DRCHandType {
-	NONE, IROBOT, HOOK, ROBOTIQ, VALKYRIE
+	NONE(false), IROBOT(false), HOOK(false), ROBOTIQ(false), VALKYRIE(false);
+	
+	private final boolean simulateHand;
+	
+	private DRCHandType(boolean simulateHand)
+	{
+	   this.simulateHand = simulateHand;
+	}
+	
+	public boolean isHandSimulated()
+	{
+	   return simulateHand;
+	}
 }
