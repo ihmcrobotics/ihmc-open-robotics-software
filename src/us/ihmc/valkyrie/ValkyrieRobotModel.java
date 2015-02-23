1adef88a77fc40359b3bb0dc42e2926df5fe7ca4
Alex Graber-Tilton <agrabertilton@ihmc.us>
2015-02-23 16:54:21 +0000

    NONE:refactored parameters slightly, tests created for the new FootstepGenerator. won't fixBamboo

--- src/us/ihmc/valkyrie/ValkyrieRobotModel.java
+++ src/us/ihmc/valkyrie/ValkyrieRobotModel.java
@@ -19,7 +19,7 @@ import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.time.AlwaysZeroOffsetPPSTimestampOffsetProvider;
 import us.ihmc.darpaRoboticsChallenge.sensors.DRCSensorSuiteManager;
 import us.ihmc.graphics3DAdapter.jme.util.JMEGeometryUtils;
-import us.ihmc.pathGeneration.footstepPlanner.FootstepParameters;
+import us.ihmc.pathGeneration.footstepPlanner.FootstepPlanningParameterization;
 import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
 import us.ihmc.multicastLogDataProtocol.modelLoaders.SDFLogModelProvider;
 import us.ihmc.robotDataCommunication.logger.LogSettings;
@@ -342,7 +342,7 @@ public class ValkyrieRobotModel implements DRCRobotModel
    }
 
    @Override
-   public FootstepParameters getFootstepParameters()
+   public FootstepPlanningParameterization getFootstepParameters()
    {
       // TODO Auto-generated method stub
       return null;
