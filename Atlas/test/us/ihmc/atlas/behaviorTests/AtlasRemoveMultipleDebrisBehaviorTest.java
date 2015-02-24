e36bfe9a85a484680858c61815457bac7bc64c12
Nicolas Eyssette <neyssette@ihmc.us>
2015-02-24 23:00:10 +0000

    DRC-2146: Created the DRCRemoveMultipleDebrisBehaviorTest and the AtlasRemoveMultipleDebrisBehaviorTest

---
+++ Atlas/test/us/ihmc/atlas/behaviorTests/AtlasRemoveMultipleDebrisBehaviorTest.java
@@ -0,0 +1,32 @@
+package us.ihmc.atlas.behaviorTests;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.darpaRoboticsChallenge.behaviorTests.DRCRemoveMultipleDebrisBehaviorTest;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
+
+@BambooPlan(planType = {BambooPlanType.InDevelopment})
+public class AtlasRemoveMultipleDebrisBehaviorTest extends DRCRemoveMultipleDebrisBehaviorTest 
+{
+	private final AtlasRobotModel robotModel;
+	
+	public AtlasRemoveMultipleDebrisBehaviorTest() 
+	{
+		robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false);
+		robotModel.getContactPointParameters().createHandContactPoints(false);
+	}
+
+	@Override
+	public DRCRobotModel getRobotModel() {
+	      return robotModel;
+	}
+
+	@Override
+	public String getSimpleRobotName()
+	{
+		return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+	}
+}
