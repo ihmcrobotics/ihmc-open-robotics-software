d5eaa4df0d506bae32f3f56f7e5ad36c580738c8
Jesper Smith <jsmith@ihmc.us>
2015-02-25 01:27:30 +0000

    NONE: Created depth data store. does not fixbamboo

---
+++ IHMCPerception/src/us/ihmc/ihmcPerception/depthData/DepthDataStore.java
@@ -0,0 +1,102 @@
+package us.ihmc.ihmcPerception.depthData;
+
+import us.ihmc.communication.packets.sensing.DepthDataClearCommand.DepthDataTree;
+import us.ihmc.communication.packets.sensing.DepthDataFilterParameters;
+import us.ihmc.sensorProcessing.pointClouds.combinationQuadTreeOctTree.GroundOnlyQuadTree;
+import us.ihmc.sensorProcessing.pointClouds.combinationQuadTreeOctTree.QuadTreeForGroundHeightMap;
+import us.ihmc.sensorProcessing.pointClouds.combinationQuadTreeOctTree.QuadTreeHeightMapInterface;
+import us.ihmc.userInterface.util.DecayingResolutionFilter;
+import us.ihmc.utilities.dataStructures.quadTree.Box;
+import us.ihmc.utilities.dataStructures.quadTree.QuadTreeForGroundParameters;
+
+public class DepthDataStore
+{
+
+   private static final boolean USE_SIMPLIFIED_QUAD_TREE = true;
+   public static final double QUAD_TREE_EXTENT = 200;
+   protected final QuadTreeHeightMapInterface quadTree;
+   protected final DecayingResolutionFilter nearScan;
+   protected DepthDataFilterParameters parameters;
+
+   public DepthDataStore()
+   {
+      this(DepthDataFilterParameters.getDefaultParameters());
+   }
+   
+   public DepthDataStore(DepthDataFilterParameters parameters)
+   {
+      this.parameters = parameters;
+      nearScan = new DecayingResolutionFilter(parameters.nearScanResolution, parameters.nearScanDecayMillis, parameters.nearScanCapacity);
+      quadTree = setupGroundOnlyQuadTree(parameters);
+
+   }
+   
+   public static QuadTreeHeightMapInterface setupGroundOnlyQuadTree(DepthDataFilterParameters parameters)
+   {
+      // SphericalLinearResolutionProvider resolutionProvider = new SphericalLinearResolutionProvider(new FramePoint(headFrame,
+      // 0.0, 0.0, -2.0), DRCConfigParameters.LIDAR_RESOLUTION_SPHERE_INNER_RADIUS*3,
+      // DRCConfigParameters.LIDAR_RESOLUTION_SPHERE_INNER_RESOLUTION, DRCConfigParameters.LIDAR_RESOLUTION_SPHERE_OUTER_RADIUS*3,
+      // DRCConfigParameters.LIDAR_RESOLUTION_SPHERE_OUTER_RESOLUTION);
+   
+      if (USE_SIMPLIFIED_QUAD_TREE)
+      {
+         Box bounds = new Box(-QUAD_TREE_EXTENT, -QUAD_TREE_EXTENT, QUAD_TREE_EXTENT, QUAD_TREE_EXTENT);
+         QuadTreeForGroundParameters quadTreeParameters = new QuadTreeForGroundParameters(DepthDataFilterParameters.GRID_RESOLUTION,
+                                                             parameters.quadtreeHeightThreshold, parameters.quadTreeMaxMultiLevelZChangeToFilterNoise,
+                                                             parameters.maxSameHeightPointsPerNode,
+                                                             parameters.maxAllowableXYDistanceForAPointToBeConsideredClose);
+   
+         return new QuadTreeForGroundHeightMap(bounds, quadTreeParameters);
+      }
+   
+      return new GroundOnlyQuadTree(-QUAD_TREE_EXTENT, -QUAD_TREE_EXTENT, QUAD_TREE_EXTENT, QUAD_TREE_EXTENT, DepthDataFilterParameters.GRID_RESOLUTION,
+                                    parameters.quadtreeHeightThreshold, 100000);
+   }
+   public void clearLidarData(DepthDataTree lidarTree)
+   {
+      nearScan.clear();
+   
+      switch (lidarTree)
+      {
+         case DECAY_POINT_CLOUD :
+            nearScan.clear();
+            break;
+   
+         case QUADTREE :
+            quadTree.clearTree(Double.NaN); 
+   
+            break;
+   
+         default :
+            throw new RuntimeException("Unknown tree");
+      }
+   }
+
+   public QuadTreeHeightMapInterface getQuadTree()
+   {
+      return quadTree;
+   }
+
+   public DecayingResolutionFilter getNearScan()
+   {
+      return nearScan;
+   }
+
+   public void setParameters(DepthDataFilterParameters parameters)
+   {
+      this.parameters = parameters;
+   
+      nearScan.setResolution(parameters.nearScanResolution);
+      nearScan.setCapacity(parameters.nearScanCapacity);
+      nearScan.setDecay(parameters.nearScanDecayMillis);
+   
+      quadTree.setHeightThreshold(parameters.quadtreeHeightThreshold);
+   
+   }
+
+   public DepthDataFilterParameters getParameters()
+   {
+      return parameters;
+   }
+
+}
