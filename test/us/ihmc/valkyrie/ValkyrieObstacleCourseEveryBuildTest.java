101f0563a1f57362c34984e3a320ca600276887f
Jerry Pratt <jpratt@ihmc.us>
2015-02-26 21:27:16 +0000

    NONE: Bamboo babysitting. Quarantined a test that has been passing but shouldn't have been. Will help fixBamboo.

--- test/us/ihmc/valkyrie/ValkyrieObstacleCourseEveryBuildTest.java
+++ test/us/ihmc/valkyrie/ValkyrieObstacleCourseEveryBuildTest.java
@@ -1,14 +1,16 @@
 package us.ihmc.valkyrie;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCObstacleCourseEveryBuildTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
-import us.ihmc.utilities.code.agileTesting.BambooAnnotations.ExcludedTest;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.QuarantinedTest;
 
-@ExcludedTest() // Crashes Bamboo
 public class ValkyrieObstacleCourseEveryBuildTest extends DRCObstacleCourseEveryBuildTest
 {
-
    private final DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
    @Override
    public DRCRobotModel getRobotModel()
@@ -22,4 +24,12 @@ public class ValkyrieObstacleCourseEveryBuildTest extends DRCObstacleCourseEvery
       return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.VALKYRIE);
    }
 
+   @Ignore
+   @QuarantinedTest("")
+   @Override
+   @Test
+   public void testWalkingUpToRampWithLongSteps() throws SimulationExceededMaximumTimeException
+   {
+      super.testWalkingUpToRampWithLongSteps();
+   }
 }
