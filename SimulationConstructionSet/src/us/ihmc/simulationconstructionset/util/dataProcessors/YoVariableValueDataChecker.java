945ba6f6db30cb6b5f9651fb170c3da6284c4579
Peter Neuhaus <pneuhaus@ihmc.us>
2015-02-26 03:09:24 +0000

    DRC-2373 more development on data processor. wont fixbamboo.

--- SimulationConstructionSet/src/us/ihmc/simulationconstructionset/util/dataProcessors/YoVariableValueDataChecker.java
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/util/dataProcessors/YoVariableValueDataChecker.java
@@ -12,9 +12,8 @@ public class YoVariableValueDataChecker implements DataProcessingFunction
    private BooleanYoVariable minimumValueExceeded;
    private BooleanYoVariable maximumDerivativeExceeded;
    private BooleanYoVariable maximumSecondDerivativeExceeded;
-   
+
    private DoubleYoVariable calculatedDerivative;
-   private boolean hasBeenInitalized = false;
    private double previousValue;
    private double previousTime;
    private double previousDerivative = Double.NaN;
@@ -28,14 +27,15 @@ public class YoVariableValueDataChecker implements DataProcessingFunction
    private double minimumValue = Double.NEGATIVE_INFINITY;
 
    private boolean maxDerivativeExeeded = false;
-   
+
 
    private boolean maxSecondDerivativeExeeded = false;
    private boolean maxValueExeeded = false;
    private boolean minValueExeeded = false;
    private DoubleYoVariable calculatedSecondDerivative;
 
-   
+   private int counter;
+
    public YoVariableValueDataChecker(SimulationConstructionSet scs, DoubleYoVariable variableToCheck, DoubleYoVariable time)
    {
       YoVariableRegistry registry = scs.getRootRegistry();
@@ -47,94 +47,100 @@ public class YoVariableValueDataChecker implements DataProcessingFunction
       minimumValueExceeded = new BooleanYoVariable(variableToCheck.getName() + "_MinValueExceeded", registry);
       maximumDerivativeExceeded = new BooleanYoVariable(variableToCheck.getName() + "_MaxDervExceeded", registry);
       maximumSecondDerivativeExceeded = new BooleanYoVariable(variableToCheck.getName() + "_MaxSecDervExceeded", registry);
-      
+
       calculatedDerivative = new DoubleYoVariable(variableToCheck.getName() + "_CalcDerv", registry);
       calculatedSecondDerivative = new DoubleYoVariable(variableToCheck.getName() + "_CalcSecDerv", registry);
 
-      hasBeenInitalized = false;
+      counter = 0;
    }
 
    public void setMaximumDerivative(double maximumDerivative)
    {
-      this.maximumDerivative = Math.abs( maximumDerivative );
+      this.maximumDerivative = Math.abs(maximumDerivative);
    }
+
    public void setMaximumSecondDerivate(double maximumSecondDerivative)
    {
       this.maximumSecondDerivative = Math.abs(maximumSecondDerivative);
    }
-   
+
    public void setMaximumValue(double maximumValue)
    {
       if (maximumValue < minimumValue)
          throw new RuntimeException("maximumValue must be greater than maximumValue. maximumValue=" + maximumValue + ", maximumValue=" + maximumValue);
 
-      this.maximumValue = maximumValue ;
+      this.maximumValue = maximumValue;
    }
-   
+
    public void setMinimumValue(double minimumValue)
    {
       if (minimumValue > maximumValue)
          throw new RuntimeException("maximumValue must be greater than maximumValue. maximumValue=" + maximumValue + ", maximumValue=" + maximumValue);
-      
+
       this.minimumValue = minimumValue;
    }
 
    @Override
    public void initializeProcessing()
    {
-      hasBeenInitalized = false;
       maxDerivativeExeeded = false;
       maxSecondDerivativeExeeded = false;
       maxValueExeeded = false;
       minValueExeeded = false;
       previousDerivative = Double.NaN;
+      counter = 0;
    }
 
    @Override
    public void processData()
    {
-      if (hasBeenInitalized)
+      double currentValue = variableToCheck.getDoubleValue();
+      minimumValueExceeded.set(currentValue < minimumValue);
+      maximumValueExceeded.set(currentValue > maximumValue);
+
+      double currentTime = time.getDoubleValue();
+
+      double currentDerivative;
+      double currentSecondDerivative;
+
+      if (counter >= 1)
       {
-//       calculatedDerivative =  (variableToCheck.getvalue - variableToCheck.getpreviousvalue)/deltaT
-//       if abs(calculatedDerivative ) > max set true
-
-         double currentValue = variableToCheck.getDoubleValue();
-         double currentTime = time.getDoubleValue();
-         double currentDerivative = (currentValue - previousValue) / (currentTime - previousTime);
-        
-         double currentSecondDerivative = 0.0;
-         
-         if( Double.isNaN(previousDerivative) == false )
-           currentSecondDerivative = (currentDerivative - previousDerivative) / (currentTime - previousTime);
-
-         minimumValueExceeded.set( currentValue < minimumValue );
-         maximumValueExceeded.set( currentValue > maximumValue );
-     
-         calculatedDerivative.set(currentDerivative);
-         calculatedSecondDerivative.set(currentSecondDerivative);
-
-         maximumDerivativeExceeded.set(       Math.abs(currentDerivative)       > maximumDerivative );
-         maximumSecondDerivativeExceeded.set( Math.abs(currentSecondDerivative) > maximumSecondDerivative );
-
-         maxValueExeeded = maxValueExeeded || maximumValueExceeded.getBooleanValue();
-         minValueExeeded = minValueExeeded || minimumValueExceeded.getBooleanValue();
-         
-         maxDerivativeExeeded       = maxDerivativeExeeded       || maximumDerivativeExceeded.getBooleanValue();
-         maxSecondDerivativeExeeded = maxSecondDerivativeExeeded || maximumSecondDerivativeExceeded.getBooleanValue();
-         
-         previousTime = currentTime;
-         previousValue = currentValue;
-         previousDerivative = currentDerivative;
+         currentDerivative = (currentValue - previousValue) / (currentTime - previousTime);
       }
       else
       {
-         previousTime = time.getDoubleValue();
-         previousValue = variableToCheck.getDoubleValue();
-         previousDerivative = 0.0;
-         hasBeenInitalized = true;
+         currentDerivative = 0.0;
       }
+
+      if (counter >= 2)
+      {
+         currentSecondDerivative = (currentDerivative - previousDerivative) / (currentTime - previousTime);
+      }
+      else
+      {
+         currentSecondDerivative = 0.0;
+      }
+
+
+      calculatedDerivative.set(currentDerivative);
+      calculatedSecondDerivative.set(currentSecondDerivative);
+
+      maximumDerivativeExceeded.set(Math.abs(currentDerivative) > maximumDerivative);
+      maximumSecondDerivativeExceeded.set(Math.abs(currentSecondDerivative) > maximumSecondDerivative);
+
+      maxValueExeeded = maxValueExeeded || maximumValueExceeded.getBooleanValue();
+      minValueExeeded = minValueExeeded || minimumValueExceeded.getBooleanValue();
+
+      maxDerivativeExeeded = maxDerivativeExeeded || maximumDerivativeExceeded.getBooleanValue();
+      maxSecondDerivativeExeeded = maxSecondDerivativeExeeded || maximumSecondDerivativeExceeded.getBooleanValue();
+
+      previousTime = currentTime;
+      previousValue = currentValue;
+      previousDerivative = currentDerivative;
+
+      counter++;
    }
-   
+
    public boolean isMaxDerivativeExeeded()
    {
       return maxDerivativeExeeded;
