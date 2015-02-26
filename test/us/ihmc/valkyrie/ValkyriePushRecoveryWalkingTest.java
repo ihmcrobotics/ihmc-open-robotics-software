f18e0f2884959587f30aef3fa0d3ec7441a79177
Duncan Calvert <dcalvert@ihmc.us>
2015-02-26 03:47:45 +0000

    GFE-253: Moved PushRecovery tests to Slow, because it takes 6.2 min. Also put them in VideoB. Will make Bamboo faster.

--- test/us/ihmc/valkyrie/ValkyriePushRecoveryWalkingTest.java
+++ test/us/ihmc/valkyrie/ValkyriePushRecoveryWalkingTest.java
@@ -3,7 +3,10 @@ package us.ihmc.valkyrie;
 import us.ihmc.darpaRoboticsChallenge.DRCPushRecoveryWalkingTest;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
 
+@BambooPlan(planType = {BambooPlanType.Slow, BambooPlanType.VideoB})
 public class ValkyriePushRecoveryWalkingTest extends DRCPushRecoveryWalkingTest
 {
    @Override
