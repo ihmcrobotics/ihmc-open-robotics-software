31fc47b6f502bd1edcb90d00fbfc0db4c7827396
Duncan Calvert <dcalvert@ihmc.us>
2015-02-26 22:53:58 +0000

    NONE: fixBamboo.

--- Atlas/test/us/ihmc/communication/producers/AtlasRobotConfigurationDataBufferTest.java
+++ Atlas/test/us/ihmc/communication/producers/AtlasRobotConfigurationDataBufferTest.java
@@ -1,8 +1,8 @@
 package us.ihmc.communication.producers;
 
 import us.ihmc.atlas.AtlasRobotModel;
-import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.atlas.AtlasRobotModel.AtlasTarget;
+import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 
 public class AtlasRobotConfigurationDataBufferTest extends RobotConfigurationDataBufferTest
