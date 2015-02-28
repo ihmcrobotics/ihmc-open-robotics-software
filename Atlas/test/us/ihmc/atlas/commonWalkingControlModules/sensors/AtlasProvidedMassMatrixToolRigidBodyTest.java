bf06f2f396281ec756d9567f86f8478edbf75145
Georg Wiedebach <gwiedebach@ihmc.us>
2015-02-28 20:55:52 +0000

    NONE: unit test for providedMassMatrixToolRigidBody

---
+++ Atlas/test/us/ihmc/atlas/commonWalkingControlModules/sensors/AtlasProvidedMassMatrixToolRigidBodyTest.java
@@ -0,0 +1,37 @@
+package us.ihmc.atlas.commonWalkingControlModules.sensors;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.atlas.parameters.AtlasArmControllerParameters;
+import us.ihmc.atlas.parameters.AtlasDefaultArmConfigurations;
+import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
+import us.ihmc.commonWalkingControlModules.sensors.ProvidedMassMatrixToolRigidBodyTest;
+import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
+import us.ihmc.utilities.humanoidRobot.partNames.LimbName;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.wholeBodyController.parameters.DefaultArmConfigurations.ArmConfigurations;
+
+public class AtlasProvidedMassMatrixToolRigidBodyTest extends ProvidedMassMatrixToolRigidBodyTest
+{
+   AtlasRobotVersion version = AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ;
+   AtlasDefaultArmConfigurations config = new AtlasDefaultArmConfigurations();
+   RobotSide side = RobotSide.LEFT;
+   
+   @Override
+   public FullRobotModel getFullRobotModel()
+   {
+      SDFFullRobotModel fullRobotModel = new AtlasRobotModel(version, AtlasRobotModel.AtlasTarget.SIM, false).createFullRobotModel();
+      
+      fullRobotModel.setJointAngles(side, LimbName.ARM, config.getArmDefaultConfigurationJointAngles(ArmConfigurations.HOME, side));
+      fullRobotModel.updateFrames();
+      
+      return fullRobotModel;
+   }
+   
+   @Override
+   public ArmControllerParameters getArmControllerParameters()
+   {
+      return new AtlasArmControllerParameters(false, version.getDistanceAttachmentPlateHand());
+   }
+}
---
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/sensors/ProvidedMassMatrixToolRigidBodyTest.java
@@ -0,0 +1,49 @@
+package us.ihmc.commonWalkingControlModules.sensors;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.vecmath.Vector3d;
+
+import org.junit.Test;
+
+import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.AverageDuration;
+import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
+import us.ihmc.utilities.math.geometry.ReferenceFrame;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.utilities.screwTheory.SpatialAccelerationVector;
+import us.ihmc.utilities.screwTheory.Wrench;
+import us.ihmc.yoUtilities.dataStructure.registry.YoVariableRegistry;
+
+public abstract class ProvidedMassMatrixToolRigidBodyTest
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry("ProvidedMassMatrixToolRigidBodyTest");
+   private final RobotSide robotSide = RobotSide.LEFT;
+   private final double gravity = 9.81;
+   private final double mass = 2.0;
+   
+   @AverageDuration(duration = 30.0)
+   @Test(timeout = 120000)
+   public void testprovidedMassMatrixToolRigidBody()
+   {
+      FullRobotModel fullRobotModel = getFullRobotModel();
+      
+      ReferenceFrame elevatorFrame = fullRobotModel.getElevatorFrame();
+      
+      ProvidedMassMatrixToolRigidBody providedMassMatrixToolRigidBody = new ProvidedMassMatrixToolRigidBody(robotSide, fullRobotModel, gravity, getArmControllerParameters(), registry, null);
+      providedMassMatrixToolRigidBody.setMass(mass);
+      
+      SpatialAccelerationVector handSpatialAccelerationVector = new SpatialAccelerationVector(elevatorFrame, elevatorFrame, elevatorFrame);
+      Wrench toolWrench = new Wrench();
+      
+      providedMassMatrixToolRigidBody.control(handSpatialAccelerationVector, toolWrench);
+      
+      toolWrench.changeFrame(ReferenceFrame.getWorldFrame());
+      
+      assertTrue(toolWrench.getLinearPart().epsilonEquals(new Vector3d(0.0, 0.0, - mass * gravity), 10e-5));
+   }
+
+   public abstract FullRobotModel getFullRobotModel();
+   
+   public abstract ArmControllerParameters getArmControllerParameters();
+}
