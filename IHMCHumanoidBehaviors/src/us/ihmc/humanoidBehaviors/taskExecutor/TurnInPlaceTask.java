ecd4fe5780f71be8be2d29b404e6ee329806def6
Nicolas Eyssette <neyssette@ihmc.us>
2015-03-01 20:06:25 +0000

    DRC-2317: the TurnInPlaceTask now uses the swingTime and TransferTime provided by the diagnosticBehavior. does not fixbamboo

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/TurnInPlaceTask.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/TurnInPlaceTask.java
@@ -1,39 +1,36 @@
 package us.ihmc.humanoidBehaviors.taskExecutor;
 
 import us.ihmc.humanoidBehaviors.behaviors.TurnInPlaceBehavior;
-import us.ihmc.humanoidBehaviors.behaviors.WalkToLocationBehavior;
-import us.ihmc.humanoidBehaviors.behaviors.WalkToLocationBehavior.WalkingOrientation;
-import us.ihmc.utilities.math.geometry.FramePose2d;
 import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
 
 public class TurnInPlaceTask extends BehaviorTask
 {
-   
-   private double sleepTime;
    private double desiredYaw;
    private TurnInPlaceBehavior turnInPlaceBehavior;
 
    private double transferTime;
    private double swingTime;
-   
-   public TurnInPlaceTask(double desiredYaw, TurnInPlaceBehavior turnInPlaceBehavior, double transferTime, double swingTime ,DoubleYoVariable yoTime, double sleepTime)
+
+   public TurnInPlaceTask(double desiredYaw, TurnInPlaceBehavior turnInPlaceBehavior, double transferTime, double swingTime, DoubleYoVariable yoTime,
+         double sleepTime)
    {
-      super(turnInPlaceBehavior, yoTime,sleepTime);
+      super(turnInPlaceBehavior, yoTime, sleepTime);
       this.desiredYaw = desiredYaw;
       this.turnInPlaceBehavior = turnInPlaceBehavior;
+      this.transferTime = transferTime;
+      this.swingTime = swingTime;
    }
-   
-   public TurnInPlaceTask(double desiredYaw, TurnInPlaceBehavior turnInPlaceBehavior, double transferTime, double swingTime ,DoubleYoVariable yoTime)
+
+   public TurnInPlaceTask(double desiredYaw, TurnInPlaceBehavior turnInPlaceBehavior, double transferTime, double swingTime, DoubleYoVariable yoTime)
    {
       this(desiredYaw, turnInPlaceBehavior, transferTime, swingTime, yoTime, 0.0);
    }
-   
+
    public TurnInPlaceTask(double desiredYaw, TurnInPlaceBehavior turnInPlaceBehavior, DoubleYoVariable yoTime)
    {
-      this(desiredYaw, turnInPlaceBehavior, Double.NaN, Double.NaN ,yoTime, 0.0);
+      this(desiredYaw, turnInPlaceBehavior, Double.NaN, Double.NaN, yoTime, 0.0);
    }
-   
-   
+
    @Override
    protected void setBehaviorInput()
    {
