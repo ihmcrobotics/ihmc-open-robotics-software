96de5b2faafdcee0af682cd0971a88ff710913e1
Jordan Lack <jlack@ihmc.us>
2015-03-03 22:03:47 +0000

    NONE: ValkyrieFlatGroundWalkingTrack is joystick controllable. Won't fixbamboo.

--- src/us/ihmc/valkyrie/simulation/ValkyrieFlatGroundWalkingTrack.java
+++ src/us/ihmc/valkyrie/simulation/ValkyrieFlatGroundWalkingTrack.java
@@ -1,20 +1,26 @@
 package us.ihmc.valkyrie.simulation;
 
 import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.WalkingProvider;
 import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
 import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
 import us.ihmc.graphics3DAdapter.GroundProfile3D;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.joystick.JoystickUpdater;
 import us.ihmc.simulationconstructionset.util.ground.FlatGroundProfile;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
+import us.ihmc.valkyrie.controllers.ValkyrieSliderBoard;
 
 public class ValkyrieFlatGroundWalkingTrack
 {
 
    public static void main(String[] args)
    {
+      boolean USE_JOYSTICK_CONTROLLER = JoystickUpdater.isJoyStickConnected();
+      
       DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
       DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false);      
       
@@ -28,11 +34,23 @@ public class ValkyrieFlatGroundWalkingTrack
       double initialYaw = 0.0;
       DRCRobotInitialSetup<SDFRobot> robotInitialSetup = robotModel.getDefaultRobotInitialSetup(groundHeight, initialYaw);
 
-      boolean useVelocityAndHeadingScript = true;
+      boolean useVelocityAndHeadingScript = !USE_JOYSTICK_CONTROLLER;;
       boolean cheatWithGroundHeightAtForFootstep = false;
+      
+      DRCFlatGroundWalkingTrack flatGroundWalkingTrack = new DRCFlatGroundWalkingTrack(robotInitialSetup, guiInitialSetup, scsInitialSetup,
+            useVelocityAndHeadingScript, cheatWithGroundHeightAtForFootstep, robotModel,
+            WalkingProvider.VELOCITY_HEADING_COMPONENT);
+      
+      SimulationConstructionSet scs = flatGroundWalkingTrack.getSimulationConstructionSet();
+      if (USE_JOYSTICK_CONTROLLER)
+      {
+         ValkyrieSliderBoard.setupJoyStick(scs);
+         flatGroundWalkingTrack.getDrcSimulation().start();
+         flatGroundWalkingTrack.getDrcSimulation().simulate();
+      }
 
-      new DRCFlatGroundWalkingTrack(robotInitialSetup, guiInitialSetup, scsInitialSetup, useVelocityAndHeadingScript,
-                                    cheatWithGroundHeightAtForFootstep, robotModel);
+//      new DRCFlatGroundWalkingTrack(robotInitialSetup, guiInitialSetup, scsInitialSetup, useVelocityAndHeadingScript,
+//                                    cheatWithGroundHeightAtForFootstep, robotModel);
    }
 
 }
