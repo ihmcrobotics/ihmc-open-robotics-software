20d773600661423692d6801ff397a871bd5f5f4d
Georg Wiedebach <gwiedebach@ihmc.us>
2015-03-05 18:26:46 +0000

    DRC-2321: Drill behavior now sends weight information to controller (will not fixbamboo)

---
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/taskExecutor/ObjectWeightTask.java
@@ -0,0 +1,26 @@
+package us.ihmc.humanoidBehaviors.taskExecutor;
+
+import us.ihmc.communication.packets.manipulation.ObjectWeightPacket;
+import us.ihmc.humanoidBehaviors.behaviors.primitives.ObjectWeightBehavior;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+
+public class ObjectWeightTask extends BehaviorTask
+{
+   private final ObjectWeightPacket objectWeightPacket;
+   private final ObjectWeightBehavior objectWeightBehavior;
+   
+   public ObjectWeightTask(RobotSide robotSide, double weight, ObjectWeightBehavior objectWeightBehavior, DoubleYoVariable yoTime)
+   {
+      super(objectWeightBehavior, yoTime);
+      objectWeightPacket = new ObjectWeightPacket(robotSide, weight);
+      this.objectWeightBehavior = objectWeightBehavior;
+   }
+
+   @Override
+   protected void setBehaviorInput()
+   {
+      objectWeightBehavior.setInput(objectWeightPacket);
+   }
+
+}
