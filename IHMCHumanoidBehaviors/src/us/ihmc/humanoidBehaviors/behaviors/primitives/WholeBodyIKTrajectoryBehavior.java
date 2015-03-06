fda27c21e66584d2f9de76884741cd251687553d
Georg Wiedebach <gwiedebach@ihmc.us>
2015-03-06 18:36:40 +0000

    DRC-2396: create WholeBodyIKTrajectory-Behavior and -Task (will not fixbamboo)

---
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/primitives/WholeBodyIKTrajectoryBehavior.java
@@ -0,0 +1,192 @@
+package us.ihmc.humanoidBehaviors.behaviors.primitives;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.communication.packets.PacketDestination;
+import us.ihmc.communication.packets.wholebody.WholeBodyTrajectoryPacket;
+import us.ihmc.humanoidBehaviors.behaviors.BehaviorInterface;
+import us.ihmc.humanoidBehaviors.communication.OutgoingCommunicationBridgeInterface;
+import us.ihmc.utilities.math.geometry.FramePose;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.utilities.trajectory.TrajectoryND;
+import us.ihmc.wholeBodyController.WholeBodyControllerParameters;
+import us.ihmc.wholeBodyController.WholeBodyIkSolver;
+import us.ihmc.wholeBodyController.WholeBodyIkSolver.ComputeOption;
+import us.ihmc.wholeBodyController.WholeBodyIkSolver.ComputeResult;
+import us.ihmc.wholeBodyController.WholeBodyIkSolver.ControlledDoF;
+import us.ihmc.wholeBodyController.WholeBodyTrajectory;
+import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+
+public class WholeBodyIKTrajectoryBehavior extends BehaviorInterface
+{
+   private final BooleanYoVariable hasInputBeenSet = new BooleanYoVariable("hasInputBeenSet" + behaviorName, registry);
+   private final BooleanYoVariable hasComputationBeenDone = new BooleanYoVariable("hasComputationBeenDone" + behaviorName, registry);
+   private final BooleanYoVariable sendPacket = new BooleanYoVariable("solutionHaveBeenFound" + behaviorName, registry);
+   
+   private final WholeBodyIkSolver wholeBodyIKSolver;
+   private final WholeBodyTrajectory wholeBodyTrajectory;
+   private final SDFFullRobotModel actualFullRobotModel;
+   private final SDFFullRobotModel desiredFullRobotModel;
+   private final DoubleYoVariable yoTime;
+   
+   private WholeBodyTrajectoryPacket wholeBodyTrajectoryPacket;
+   private double trajectoryTime;
+   private double startTime;
+
+   public WholeBodyIKTrajectoryBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridge,
+         WholeBodyControllerParameters wholeBodyControllerParameters, SDFFullRobotModel actualFullRobotModel, DoubleYoVariable yoTime)
+   {
+      super(outgoingCommunicationBridge);
+      wholeBodyIKSolver = wholeBodyControllerParameters.createWholeBodyIkSolver();
+      wholeBodyTrajectory = new WholeBodyTrajectory(actualFullRobotModel, 1.5, 15, 0.1);
+      
+      this.actualFullRobotModel = actualFullRobotModel;
+      this.desiredFullRobotModel = wholeBodyControllerParameters.createFullRobotModel();
+      this.yoTime = yoTime;
+   }
+   
+   /**
+    * @param controlledDoFLeft
+    * @param palmTargetLeft set null if no left target
+    * @param controlledDoFRight
+    * @param palmTargetRight set null if no right target
+    */
+   public void setInput(ControlledDoF controlledDoFLeft, FramePose palmTargetLeft, ControlledDoF controlledDoFRight, FramePose palmTargetRight)
+   {
+      wholeBodyIKSolver.setNumberOfControlledDoF(RobotSide.LEFT, controlledDoFLeft);
+      if (palmTargetLeft != null)
+      {
+         wholeBodyIKSolver.setGripperPalmTarget(actualFullRobotModel, RobotSide.LEFT, palmTargetLeft);
+         hasInputBeenSet.set(true);
+      }
+      
+      wholeBodyIKSolver.setNumberOfControlledDoF(RobotSide.RIGHT, controlledDoFRight);
+      if (palmTargetRight != null)
+      {
+         wholeBodyIKSolver.setGripperPalmTarget(actualFullRobotModel, RobotSide.RIGHT, palmTargetRight);
+         hasInputBeenSet.set(true);
+      }
+   }
+
+   @Override
+   public void doControl()
+   {
+      if (isPaused())
+      {
+         return;
+      }
+      
+      if (hasInputBeenSet.getBooleanValue() && !hasComputationBeenDone.getBooleanValue())
+      {
+         try
+         {
+            ComputeResult result = wholeBodyIKSolver.compute(actualFullRobotModel, desiredFullRobotModel, ComputeOption.USE_ACTUAL_MODEL_JOINTS);
+            
+            TrajectoryND trajectory = wholeBodyTrajectory.createTaskSpaceTrajectory(wholeBodyIKSolver, actualFullRobotModel, desiredFullRobotModel);
+            wholeBodyTrajectoryPacket = wholeBodyTrajectory.convertTrajectoryToPacket(trajectory);
+            
+            hasComputationBeenDone.set(true);
+            if (result == ComputeResult.SUCCEEDED)
+            {
+               sendPacket.set(true);
+            }
+            else
+            {
+               sendPacket.set(false);
+            }
+            System.out.println("solution found = " + sendPacket.getBooleanValue());
+         }
+         catch (Exception e)
+         {
+            e.printStackTrace();
+         }
+      }
+      
+      if (sendPacket.getBooleanValue())
+      {
+         wholeBodyTrajectoryPacket.setDestination(PacketDestination.CONTROLLER);
+         sendPacketToController(wholeBodyTrajectoryPacket);
+         
+         wholeBodyTrajectoryPacket.setDestination(PacketDestination.UI);
+         sendPacketToNetworkProcessor(wholeBodyTrajectoryPacket);
+         
+         trajectoryTime = wholeBodyTrajectoryPacket.getTrajectoryTime();
+         startTime = yoTime.getDoubleValue();
+         sendPacket.set(false);
+      }
+   }
+
+   @Override
+   protected void passReceivedNetworkProcessorObjectToChildBehaviors(Object object)
+   {
+      
+   }
+
+   @Override
+   protected void passReceivedControllerObjectToChildBehaviors(Object object)
+   {
+      
+   }
+
+   @Override
+   public void stop()
+   {
+      defaultStop();
+   }
+
+   @Override
+   public void enableActions()
+   {
+      
+   }
+
+   @Override
+   public void pause()
+   {
+      defaultPause();
+   }
+
+   @Override
+   public void resume()
+   {
+      defaultResume();
+   }
+
+   @Override
+   public boolean isDone()
+   {
+      boolean trajectoryTimePassed = yoTime.getDoubleValue() - startTime > trajectoryTime;
+      return trajectoryTimePassed;
+   }
+
+   @Override
+   public void finalize()
+   {
+      defaultFinalize();
+      reset();
+   }
+
+   @Override
+   public void initialize()
+   {
+      defaultInitialize();
+      reset();
+   }
+   
+   private void reset()
+   {
+      hasInputBeenSet.set(false);
+      hasComputationBeenDone.set(false);
+      sendPacket.set(false);
+      
+      startTime = Double.POSITIVE_INFINITY;
+      trajectoryTime = Double.POSITIVE_INFINITY;
+   }
+
+   @Override
+   public boolean hasInputBeenSet()
+   {
+      return hasInputBeenSet.getBooleanValue();
+   }
+
+}
