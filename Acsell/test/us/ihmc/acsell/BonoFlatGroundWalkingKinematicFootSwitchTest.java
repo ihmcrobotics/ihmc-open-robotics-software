ec82fe9edb16359688135342593a4159c0a0ca4e
Duncan Calvert <dcalvert@ihmc.us>
2015-03-10 20:33:01 +0000

    GFE-253: To reduce confusion, AverageDuration has been renamed to EstimatedDuration.

--- Acsell/test/us/ihmc/acsell/BonoFlatGroundWalkingKinematicFootSwitchTest.java
+++ Acsell/test/us/ihmc/acsell/BonoFlatGroundWalkingKinematicFootSwitchTest.java
@@ -13,14 +13,14 @@ import us.ihmc.sensorProcessing.stateEstimation.StateEstimatorParameters;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.simulationconstructionset.util.simulationRunner.ControllerFailureException;
-import us.ihmc.utilities.code.agileTesting.BambooAnnotations.AverageDuration;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.EstimatedDuration;
 
 public class BonoFlatGroundWalkingKinematicFootSwitchTest extends DRCFlatGroundWalkingTest
 {
 
    private BonoRobotModel robotModel;
 
-	@AverageDuration
+	@EstimatedDuration
 	@Test(timeout=300000)
    public void testBONOFlatGroundWalking() throws SimulationExceededMaximumTimeException, ControllerFailureException
    {
