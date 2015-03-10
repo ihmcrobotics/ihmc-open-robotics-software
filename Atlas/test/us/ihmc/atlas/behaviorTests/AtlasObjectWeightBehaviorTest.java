cc636c785ebf1a16bf94418f5a8f0732e31aa898
Georg Wiedebach <gwiedebach@ihmc.us>
2015-03-10 22:16:41 +0000

    DRC-2394: junit tests for ObjectWeightBehavior (will not fixbamboo)

---
+++ Atlas/test/us/ihmc/atlas/behaviorTests/AtlasObjectWeightBehaviorTest.java
@@ -0,0 +1,29 @@
+package us.ihmc.atlas.behaviorTests;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.darpaRoboticsChallenge.behaviorTests.DRCObjectWeightBehaviorTest;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+
+public class AtlasObjectWeightBehaviorTest extends DRCObjectWeightBehaviorTest
+{
+   private final AtlasRobotModel robotModel;
+   
+   public AtlasObjectWeightBehaviorTest()
+   {
+      robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false); 
+   }
+   
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+}
