2d98722d7de6c1a23d5b1b32e3733ca653ec0753
Alex Graber-Tilton <agrabertilton@ihmc.us>
2015-03-10 17:18:03 +0000

    NONE: altered the swing height calculator to ignore some outliers, calculator parameters come from the robot model now.

--- src/us/ihmc/valkyrie/ValkyrieRobotModel.java
+++ src/us/ihmc/valkyrie/ValkyrieRobotModel.java
@@ -9,6 +9,7 @@ import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.commonWalkingControlModules.configurations.ArmControllerParameters;
 import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerParameters;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.commonWalkingControlModules.trajectories.HeightCalculatorParameters;
 import us.ihmc.communication.streamingData.GlobalDataProducer;
 import us.ihmc.darpaRoboticsChallenge.DRCRobotSDFLoader;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
@@ -392,6 +393,12 @@ public class ValkyrieRobotModel implements DRCRobotModel
    {
       
       
+      return null;
+   }
+
+   @Override
+   public HeightCalculatorParameters getHeightCalculatorParameters()
+   {
       return null;
    }
 }
