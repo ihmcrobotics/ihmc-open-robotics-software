cce4f44644039812a3921984a621ad7efafb3c33
Atlas Control Computer <AtlasControlComputer@unknown>
2015-03-13 21:18:48 +0000

    DRC-2406: fixed pinch and wide grip finger graphics in UI

--- RobotiqHandDrivers/src/us/ihmc/robotiq/data/RobotiqHandSensorData.java
+++ RobotiqHandDrivers/src/us/ihmc/robotiq/data/RobotiqHandSensorData.java
@@ -197,14 +197,13 @@ public class RobotiqHandSensorData implements HandSensorData
 	public double[][] getFingerJointAngles()
 	{
 		double[][] fingerJointAngles = new double[3][];
-		//TODO: verify SCISSOR sign
-		temp[0] = ((4.0/45) - (double)position[SCISSOR] * (8.0/45) / MAX_POSITION_VALUE) * Math.PI; //32 degrees
+		temp[0] = ((double)position[SCISSOR] * (8.0/45) / MAX_POSITION_VALUE - (4.0/45)) * Math.PI; //32 degrees
 		temp[1] = (double)position[FINGER_A] * (25.0/72) * Math.PI  / MAX_POSITION_VALUE; //62.5 degrees
 		temp[2] = (double)position[FINGER_A] * (0.5) * Math.PI / MAX_POSITION_VALUE; //90 degrees
 		temp[3] = 0.0;
 		fingerJointAngles[0] = Arrays.copyOf(temp, 4);
 		
-		temp[0] = -((4.0/45) - (double)position[SCISSOR] * (8.0/45) / MAX_POSITION_VALUE) * Math.PI; //32 degrees
+		temp[0] = -((double)position[SCISSOR] * (8.0/45) / MAX_POSITION_VALUE - (4.0/45)) * Math.PI; //32 degrees
 		temp[1] = (double)position[FINGER_B] * (25.0/72) * Math.PI  / MAX_POSITION_VALUE; //62.5 degrees
 		temp[2] = (double)position[FINGER_B] * (0.5) * Math.PI / MAX_POSITION_VALUE; //90 degrees
 		temp[3] = 0.0;
