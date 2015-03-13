2a49c961018f5b8c4f0ce1636256258d4c885cb3
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-03-13 00:28:17 +0000

    none: removed lookAtPacket, pelvisPosePacket, and JointAnglesPacket from the ros api (IHMCMessageMap) and regenerated the packets, should fixBamboo

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ComHeightPacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/ComHeightPacketMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface ComHeightPacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/ComHeightPacketMessage";
-  static final java.lang.String _DEFINITION = "## ComHeightPacketMessage\n# This message sets the robot\'s center of mass height.\n\nfloat64 MIN_COM_HEIGHT\n\nfloat64 MAX_COM_HEIGHT\n\n# heightOffset specifies CoM height relative to the default starting height, which is\n# about 78.9 cm off the ground e.g. heightOffset = -0.1 will put the CoM at about\n# 68.9 cm above ground level\nfloat64 heightOffset\n\n# trajectoryTime specifies how fast or how slow to move to the desired pose\nfloat64 trajectoryTime\n\nint8 destination\n\n\n";
+  static final java.lang.String _DEFINITION = "## ComHeightPacketMessage\r\n# This message sets the robot\'s center of mass height.\r\n\r\nfloat64 MIN_COM_HEIGHT\r\n\r\nfloat64 MAX_COM_HEIGHT\r\n\r\n# heightOffset specifies CoM height relative to the default starting height, which is\r\n# about 78.9 cm off the ground e.g. heightOffset = -0.1 will put the CoM at about\r\n# 68.9 cm above ground level\r\nfloat64 heightOffset\r\n\r\n# trajectoryTime specifies how fast or how slow to move to the desired pose\r\nfloat64 trajectoryTime\r\n\r\nint8 destination\r\n\r\n\r\n";
   double getMINCOMHEIGHT();
   void setMINCOMHEIGHT(double value);
   double getMAXCOMHEIGHT();
--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepStatusMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepStatusMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootstepStatusMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootstepStatusMessage";
-  static final java.lang.String _DEFINITION = "## FootstepStatusMessage\n# This message gives the status of the current footstep from the controller.\n\n#Options for status\n# uint8 STARTED = 0\n# uint8 COMPLETED = 1\nuint8 status\n\n# footstepIndex starts at 0 and monotonically increases with each completed footstep in a given\n# FootstepDataListMessage.\nint32 footstepIndex\n\nint32 robotSide\n\ngeometry_msgs/Vector3 actualFootPositionInWorld\n\ngeometry_msgs/Quaternion actualFootOrientationInWorld\n\nbool isDoneWalking\n\nint8 destination\n\n\n";
+  static final java.lang.String _DEFINITION = "## FootstepStatusMessage\r\n# This message gives the status of the current footstep from the controller.\r\n\r\n#Options for status\r\n# uint8 STARTED = 0\r\n# uint8 COMPLETED = 1\r\nuint8 status\r\n\r\n# footstepIndex starts at 0 and monotonically increases with each completed footstep in a given\r\n# FootstepDataListMessage.\r\nint32 footstepIndex\r\n\r\nint32 robotSide\r\n\r\ngeometry_msgs/Vector3 actualFootPositionInWorld\r\n\r\ngeometry_msgs/Quaternion actualFootOrientationInWorld\r\n\r\nbool isDoneWalking\r\n\r\nint8 destination\r\n\r\n\r\n";
   byte getStatus();
   void setStatus(byte value);
   int getFootstepIndex();
