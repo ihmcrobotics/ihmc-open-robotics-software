6787fdc8c1cdd8a2d8504c6acc0d18f993e9b79a
Sylvain Bertrand <sbertrand@ihmc.us>
2015-03-16 21:29:17 +0000

    NONE: Fixbamboo: Fixed the AtlasArmDefaultConfigurations for the new model.

--- Atlas/src/us/ihmc/atlas/parameters/AtlasDefaultArmConfigurations.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasDefaultArmConfigurations.java
@@ -14,31 +14,32 @@ public class AtlasDefaultArmConfigurations implements DefaultArmConfigurations
       {
       case HOME:
          armJointAngles = fillArmJointAnglesArray(robotSide.negateIfRightSide(0.1), robotSide.negateIfRightSide(-1.3), 1.94, robotSide.negateIfRightSide(1.18),
-               0.0, robotSide.negateIfRightSide(-0.07));
+               0.0, robotSide.negateIfRightSide(-0.07), 0.0);
          return armJointAngles;
       case WIDER_HOME:
          //TODO put the same angles as those in the UI
          armJointAngles = fillArmJointAnglesArray(robotSide.negateIfRightSide(0.1), robotSide.negateIfRightSide(-1.3), 1.94, robotSide.negateIfRightSide(1.18),
-               0.0, robotSide.negateIfRightSide(-0.07));
+               0.0, robotSide.negateIfRightSide(-0.07), 0.0);
          return armJointAngles;
       case COMPACT_HOME:
          armJointAngles = fillArmJointAnglesArray(robotSide.negateIfRightSide(0.1), robotSide.negateIfRightSide(-1.6), 1.9, robotSide.negateIfRightSide(2.1),
-               0.0, robotSide.negateIfRightSide(-0.55));
+               0.0, robotSide.negateIfRightSide(-0.55), 0.0);
          return armJointAngles;
       default:
          return null;
       }
    }
 
-   private double[] fillArmJointAnglesArray(double shoulderYaw, double shoulderRoll, double elbowPitch, double elbowRoll, double wristPitch, double wristRoll)
+   private double[] fillArmJointAnglesArray(double shoulderYaw, double shoulderRoll, double elbowPitch, double elbowRoll, double wristPitch, double wristRoll, double wristSecondPitch)
    {
-      double[] armJointAngles = new double[6];
+      double[] armJointAngles = new double[7];
       armJointAngles[0] = shoulderYaw;
       armJointAngles[1] = shoulderRoll;
       armJointAngles[2] = elbowPitch;
       armJointAngles[3] = elbowRoll;
       armJointAngles[4] = wristPitch;
       armJointAngles[5] = wristRoll;
+      armJointAngles[6] = wristSecondPitch;
 
       return armJointAngles;
 
