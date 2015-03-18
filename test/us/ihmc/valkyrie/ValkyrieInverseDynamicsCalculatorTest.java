165c1780974902968f38ee2cc9be42e7445fcffe
Jerry Pratt <jpratt@ihmc.us>
2015-03-18 16:22:34 +0000

    DRC-2421: Added ValkyrieInverseDynamicsCalculatorTest, but put in Exclude for now since torques are different for some reason. Probably integrate acceleration stuff or something. Won't fixBamboo.

---
+++ test/us/ihmc/valkyrie/ValkyrieInverseDynamicsCalculatorTest.java
@@ -0,0 +1,26 @@
+package us.ihmc.valkyrie;
+
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCInverseDynamicsCalculatorTest;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
+
+@BambooPlan(planType = {BambooPlanType.Exclude})
+public class ValkyrieInverseDynamicsCalculatorTest extends DRCInverseDynamicsCalculatorTest
+{
+   private final DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
+   
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      robotModel.setEnableJointDamping(false);
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+}
