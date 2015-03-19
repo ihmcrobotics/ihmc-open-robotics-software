e7cd96667fcd45ccf60caf790bb4a225cdc32435
Jerry Pratt <jpratt@ihmc.us>
2015-03-19 15:02:14 +0000

    DRC-2421: Generalized DRCPelvisLowGainsTest and added ValkyriePelvisLowGainsTest. Won't fixBamboo.

---
+++ test/us/ihmc/valkyrie/ValkyriePelvisLowGainsTest.java
@@ -0,0 +1,58 @@
+package us.ihmc.valkyrie;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCPelvisLowGainsTest;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
+import us.ihmc.utilities.screwTheory.InverseDynamicsCalculatorListener;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+
+
+@BambooPlan(planType = {BambooPlanType.Exclude})
+public class ValkyriePelvisLowGainsTest extends DRCPelvisLowGainsTest
+{
+   private final DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      robotModel.setEnableJointDamping(false);
+
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+   
+   @Override
+   protected DoubleYoVariable getPelvisOrientationErrorVariableName(SimulationConstructionSet scs)
+   {
+      return (DoubleYoVariable) scs.getVariable("MomentumBasedControllerFactory.PelvisOrientationManager.RootJointAngularAccelerationControlModule.v1PelvisAxisAngleOrientationController",
+                                                "v1PelvisOrientationErrorMagnitude");
+   }
+
+   @Override
+   public String getKpPelvisOrientationName()
+   {
+      return "kpXYAngularPelvisOrientation";
+   }
+
+   @Override
+   public String getZetaPelvisOrientationName()
+   {
+      return "zetaXYAngularPelvisOrientation";
+   }
+
+   @Override
+   public InverseDynamicsCalculatorListener getInverseDynamicsCalculatorListener(SDFFullRobotModel controllersFullRobotModel, SDFRobot robot)
+   {
+      return null;
+   }
+}
