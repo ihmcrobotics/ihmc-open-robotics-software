d0d7cd65a999348c885ed2289d8c9ba4f29e4147
Sylvain Bertrand <sbertrand@ihmc.us>
2015-03-20 02:48:36 +0000

    NONE: Committing my work on reachability map. Won't fixbamboo.

---
+++ Atlas/src/us/ihmc/atlas/reachabilityMap/AtlasReachabilitySphereMapSimulation.java
@@ -0,0 +1,64 @@
+package us.ihmc.atlas.reachabilityMap;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotModel.AtlasTarget;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.darpaRoboticsChallenge.reachabilityMapCalculator.ReachabilityMapListener;
+import us.ihmc.darpaRoboticsChallenge.reachabilityMapCalculator.ReachabilitySphereMapCalculator;
+import us.ihmc.darpaRoboticsChallenge.wholeBodyInverseKinematicsSimulationController.JointAnglesWriter;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
+import us.ihmc.utilities.FormattingTools;
+import us.ihmc.utilities.humanoidRobot.partNames.LimbName;
+import us.ihmc.utilities.math.geometry.FramePose;
+import us.ihmc.utilities.math.geometry.RigidBodyTransform;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+import us.ihmc.utilities.screwTheory.ScrewTools;
+
+public class AtlasReachabilitySphereMapSimulation
+{
+   public AtlasReachabilitySphereMapSimulation()
+   {
+      AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, AtlasTarget.SIM, false);
+      SDFFullRobotModel fullRobotModel = robotModel.createFullRobotModel();
+      SDFRobot sdfRobot = robotModel.createSdfRobot(false);
+      final JointAnglesWriter jointAnglesWriter = new JointAnglesWriter(sdfRobot, fullRobotModel);
+
+      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters(true, 16000);
+      SimulationConstructionSet scs = new SimulationConstructionSet(sdfRobot, parameters);
+      
+      OneDoFJoint[] armJoints = ScrewTools.filterJoints(ScrewTools.createJointPath(fullRobotModel.getChest(), fullRobotModel.getHand(RobotSide.LEFT)), OneDoFJoint.class);
+      ReachabilitySphereMapCalculator reachabilitySphereMapCalculator = new ReachabilitySphereMapCalculator(armJoints, scs);
+//      reachabilitySphereMapCalculator.setControlFrameFixedInEndEffector(fullRobotModel.getHandControlFrame(RobotSide.LEFT));
+      FramePose palmCenter = new FramePose(fullRobotModel.getHandControlFrame(RobotSide.LEFT));
+      palmCenter.setX(robotModel.getArmControllerParameters().getWristHandCenterOffset() - 0.02);
+      palmCenter.changeFrame(fullRobotModel.getEndEffector(RobotSide.LEFT, LimbName.ARM).getBodyFixedFrame());
+      RigidBodyTransform transformFromPalmCenterToHandBodyFixedFrame = new RigidBodyTransform();
+      palmCenter.getPose(transformFromPalmCenterToHandBodyFixedFrame);
+      reachabilitySphereMapCalculator.setTransformFromControlFrameToEndEffectorBodyFixedFrame(transformFromPalmCenterToHandBodyFixedFrame);
+      String robotName = FormattingTools.underscoredToCamelCase(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ.toString(), true);
+      reachabilitySphereMapCalculator.setupCalculatorToRecordInFile(robotName, getClass());
+
+      ReachabilityMapListener listener = new ReachabilityMapListener()
+      {
+         @Override
+         public void hasReachedNewConfiguration()
+         {
+            jointAnglesWriter.updateRobotConfigurationBasedOnFullRobotModel();
+         }
+      };
+
+      reachabilitySphereMapCalculator.attachReachabilityMapListener(listener);
+      scs.startOnAThread();
+      
+      reachabilitySphereMapCalculator.buildReachabilitySpace();
+   }
+
+   public static void main(String[] args)
+   {
+      new AtlasReachabilitySphereMapSimulation();
+   }
+}
