9fe94518c8d3894ddf758bba04bc2031f192662b
Sylvain Bertrand <sbertrand@ihmc.us>
2015-03-20 02:48:36 +0000

    NONE: Committing my work on reachability map. Won't fixbamboo.

---
+++ src/us/ihmc/valkyrie/reachabilityMap/ValkyrieReachabilitySphereMapSimulation.java
@@ -0,0 +1,51 @@
+package us.ihmc.valkyrie.reachabilityMap;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.darpaRoboticsChallenge.reachabilityMapCalculator.ReachabilityMapListener;
+import us.ihmc.darpaRoboticsChallenge.reachabilityMapCalculator.ReachabilitySphereMapCalculator;
+import us.ihmc.darpaRoboticsChallenge.wholeBodyInverseKinematicsSimulationController.JointAnglesWriter;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.utilities.screwTheory.OneDoFJoint;
+import us.ihmc.utilities.screwTheory.ScrewTools;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+
+public class ValkyrieReachabilitySphereMapSimulation
+{
+   public ValkyrieReachabilitySphereMapSimulation()
+   {
+      ValkyrieRobotModel robotModel = new ValkyrieRobotModel(false, false);
+      SDFFullRobotModel fullRobotModel = robotModel.createFullRobotModel();
+      SDFRobot sdfRobot = robotModel.createSdfRobot(false);
+      final JointAnglesWriter jointAnglesWriter = new JointAnglesWriter(sdfRobot, fullRobotModel);
+
+      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters(true, 16000);
+      SimulationConstructionSet scs = new SimulationConstructionSet(sdfRobot, parameters);
+
+      OneDoFJoint[] armJoints = ScrewTools.filterJoints(ScrewTools.createJointPath(fullRobotModel.getChest(), fullRobotModel.getHand(RobotSide.LEFT)),
+            OneDoFJoint.class);
+      ReachabilitySphereMapCalculator reachabilitySphereMapCalculator = new ReachabilitySphereMapCalculator(armJoints, scs);
+      reachabilitySphereMapCalculator.setupCalculatorToRecordInFile("Valkyrie", getClass());
+
+      ReachabilityMapListener listener = new ReachabilityMapListener()
+      {
+         @Override
+         public void hasReachedNewConfiguration()
+         {
+            jointAnglesWriter.updateRobotConfigurationBasedOnFullRobotModel();
+         }
+      };
+
+      reachabilitySphereMapCalculator.attachReachabilityMapListener(listener);
+      scs.startOnAThread();
+
+      reachabilitySphereMapCalculator.buildReachabilitySpace();
+   }
+
+   public static void main(String[] args)
+   {
+      new ValkyrieReachabilitySphereMapSimulation();
+   }
+}
