77773512835df8604d01f1d2215e082ab6acbf9a
Nathan Mertins <nmertins@ihmc.us>
2015-03-20 01:03:19 +0000

    GFE-310: finished infrastructure for Exception to ROS pipeline; need to test on Ubuntu; won't fixbamboo

---
+++ IHMCROSTools/src/us/ihmc/utilities/ros/publisher/PrintStreamToRosBridge.java
@@ -0,0 +1,77 @@
+package us.ihmc.utilities.ros.publisher;
+
+import java.io.PrintStream;
+import java.util.concurrent.TimeUnit;
+
+import us.ihmc.utilities.ThreadTools;
+import us.ihmc.utilities.io.TimerBasedOutputStream;
+import us.ihmc.utilities.ros.RosMainNode;
+
+public class PrintStreamToRosBridge extends PrintStream
+{
+   private static final int TIME_PERIOD = 1000;
+   private static final TimeUnit TIME_UNIT = TimeUnit.MILLISECONDS;
+   
+   private TimerBasedOutputStream tbos = (TimerBasedOutputStream) out;
+   private RosLogPublisher publisher;
+   private Thread currentThread = null;
+   
+   public PrintStreamToRosBridge(RosMainNode rosMainNode, String rosNamespace)
+   {
+      super(new TimerBasedOutputStream(TIME_PERIOD, TIME_UNIT), true);
+      
+      publisher = new RosLogPublisher(rosMainNode, true);
+      rosMainNode.attachPublisher(rosNamespace + "/ihmc_err", publisher);
+   }
+   
+   public PrintStreamToRosBridge()
+   {
+      super(new TimerBasedOutputStream(TIME_PERIOD, TIME_UNIT), true);
+   }
+   
+   public void start()
+   {
+      if(currentThread == null)
+      {
+         currentThread = new Thread(new OutputStreamMonitor());
+         currentThread.start();
+      }
+   }
+   
+   private class OutputStreamMonitor implements Runnable
+   {
+      @Override
+      public void run()
+      {
+         while(true)
+         {
+            if(tbos.hasNewOutput())
+            {
+               publisher.publish((byte) 8, tbos.getString());
+//               System.out.println(tbos.getString());
+            }
+         }
+      }
+   }
+   
+//   public static void main(String[] args)
+//   {
+//      PrintStreamToRosBridge bridge = new PrintStreamToRosBridge();
+//      System.setErr(bridge);
+//      bridge.start();
+//      
+//      while(true)
+//      {
+//         try
+//         {
+//            throw new Exception("Test");
+//         }
+//         catch(Exception e)
+//         {
+//            e.printStackTrace();
+//         }
+//         System.out.println("LAP");
+//         ThreadTools.sleep(1000);
+//      }
+//   }
+}
