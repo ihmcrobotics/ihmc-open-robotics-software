c56da1c8fd8d31be7d75dfc30d2b015428a85a8f
Nathan Mertins <nmertins@ihmc.us>
2015-03-23 15:49:25 +0000

    GFE-304: Added explicit annotation for ignoring fields when switching between Java and ROS messages; won't fixbamboo

--- IHMCCommunication/src/us/ihmc/communication/packets/Packet.java
+++ IHMCCommunication/src/us/ihmc/communication/packets/Packet.java
@@ -1,9 +1,11 @@
 package us.ihmc.communication.packets;
 
+import us.ihmc.communication.packetAnnotations.IgnoreField;
 import us.ihmc.utilities.ComparableDataObject;
 
 public abstract class Packet<T> implements ComparableDataObject<T>
 {
+   @IgnoreField
    public byte destination = (byte) PacketDestination.BROADCAST.ordinal();
    public void setDestination(PacketDestination destination)
    {
