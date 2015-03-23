3e0d9626a062b10f401e9ac9da7ed7eb0ee4720e
Georg Wiedebach <gwiedebach@ihmc.us>
2015-03-23 17:59:27 +0000

    NONE: updated generated messages and removed Point2dMessage

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootPosePacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootPosePacketMessage";
-  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\n# This message commands the controller to move the desired foot to a given location\n# and orientation in world coordinates. This message is not intended to specify footsteps\n# as the controller will use a straight-line trajectory when moving the foot from its\n# current to desired location/orientation\n\n#Options for robotSide\n# uint8 L = 0\n# uint8 R = 1\nuint8 robot_side\n\ngeometry_msgs/Vector3 position\n\ngeometry_msgs/Quaternion orientation\n\n# trajectoryTime specifies how fast or how slow to move to the desired pose\nfloat64 trajectory_time\n\n\n";
+  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\n# This message commands the controller to move the desired foot to a given location\n# and orientation in world coordinates. This message is not intended to specify footsteps\n# as the controller will use a straight-line trajectory when moving the foot from its\n# current to desired location/orientation\n\n# Options for robotSide\n# uint8 L = 0\n# uint8 R = 1\nuint8 robot_side\n\ngeometry_msgs/Vector3 position\n\ngeometry_msgs/Quaternion orientation\n\n# trajectoryTime specifies how fast or how slow to move to the desired pose\nfloat64 trajectory_time\n\n\n";
   byte getRobotSide();
   void setRobotSide(byte value);
   geometry_msgs.Vector3 getPosition();
