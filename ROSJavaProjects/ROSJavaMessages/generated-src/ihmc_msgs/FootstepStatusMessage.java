90f8c69212fa65bcf38676c93d7e12883fa5ed78
Georg Wiedebach <gwiedebach@ihmc.us>
2015-03-24 03:15:40 +0000

    GFE-319: Include documentation in enums used for ROS messages by making them implement DocumentedEnum. Update a bunch of message files. (won't fixbamboo)

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepStatusMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepStatusMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootstepStatusMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootstepStatusMessage";
-  static final java.lang.String _DEFINITION = "## FootstepStatusMessage\n# This message gives the status of the current footstep from the controller.\n\n# Options for status\n# uint8 STARTED = 0\n# uint8 COMPLETED = 1\nuint8 status\n\n# footstepIndex starts at 0 and monotonically increases with each completed footstep in a given\n# FootstepDataListMessage.\nint32 footstep_index\n\n# Options for robotSide\n# uint8 L = 0\n# uint8 R = 1\nuint8 robot_side\n\ngeometry_msgs/Vector3 actual_foot_position_in_world\n\ngeometry_msgs/Quaternion actual_foot_orientation_in_world\n\nbool is_done_walking\n\n\n";
+  static final java.lang.String _DEFINITION = "## FootstepStatusMessage\n# This message gives the status of the current footstep from the controller.\n\n# Options for status\n# STARTED = 0 - indicates that the execution of a footstep has begun\n# COMPLETED = 1 - a footstep is completed\nuint8 status\n\n# footstepIndex starts at 0 and monotonically increases with each completed footstep in a given\n# FootstepDataListMessage.\nint32 footstep_index\n\n# Options for robotSide\n# LEFT = 0 - refers to the LEFT side of a robot\n# RIGHT = 1 - refers to the RIGHT side of a robot\nuint8 robot_side\n\ngeometry_msgs/Vector3 actual_foot_position_in_world\n\ngeometry_msgs/Quaternion actual_foot_orientation_in_world\n\nbool is_done_walking\n\n\n";
   byte getStatus();
   void setStatus(byte value);
   int getFootstepIndex();
