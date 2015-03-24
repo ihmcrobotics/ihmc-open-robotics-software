d7a7966f32d69445e3ad6f9ca7fd4ed2d1dbff62
Nathan Mertins <nmertins@ihmc.us>
2015-03-24 00:18:08 +0000

    GFE-224: updated ROS messages and interfaces; moving ROSMessageConverterTest to InDevelopment Bamboo plan because it's going to be broken for a while; fixbamboo

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/HeadOrientationPacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/HeadOrientationPacketMessage.java
@@ -2,9 +2,9 @@ package ihmc_msgs;
 
 public interface HeadOrientationPacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/HeadOrientationPacketMessage";
-  static final java.lang.String _DEFINITION = "## HeadOrientationPacketMessage\n# This message gives the desired head orientation of the robot in world coordinates.\n\ngeometry_msgs/Quaternion quaternion\n\n# trajectoryTime specifies how fast or how slow to move to the desired pose\nfloat64 trajectory_time\n\n\n";
-  geometry_msgs.Quaternion getQuaternion();
-  void setQuaternion(geometry_msgs.Quaternion value);
+  static final java.lang.String _DEFINITION = "## HeadOrientationPacketMessage\n# This message gives the desired head orientation of the robot in world coordinates.\n\ngeometry_msgs/Quaternion orientation\n\n# trajectoryTime specifies how fast or how slow to move to the desired pose\nfloat64 trajectory_time\n\n\n";
+  geometry_msgs.Quaternion getOrientation();
+  void setOrientation(geometry_msgs.Quaternion value);
   double getTrajectoryTime();
   void setTrajectoryTime(double value);
 }
