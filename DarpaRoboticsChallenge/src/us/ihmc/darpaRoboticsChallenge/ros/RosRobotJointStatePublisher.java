a73bcaffc4917284e68131a9dd0b667233c62348
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-03-24 05:13:15 +0000

    none: threading the receive of robot states in gfe np. wont fixBamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/RosRobotJointStatePublisher.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/RosRobotJointStatePublisher.java
@@ -1,6 +1,7 @@
 package us.ihmc.darpaRoboticsChallenge.ros;
 
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.ros.message.Time;
 
@@ -9,6 +10,7 @@ import us.ihmc.SdfLoader.SDFFullRobotModelFactory;
 import us.ihmc.communication.net.PacketConsumer;
 import us.ihmc.communication.packetCommunicator.interfaces.PacketCommunicator;
 import us.ihmc.communication.packets.dataobjects.RobotConfigurationData;
+import us.ihmc.utilities.ThreadTools;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModelUtils;
 import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 import us.ihmc.utilities.ros.PPSTimestampOffsetProvider;
@@ -17,7 +19,7 @@ import us.ihmc.utilities.ros.msgToPacket.IHMCRosApiMessageMap;
 import us.ihmc.utilities.ros.publisher.RosJointStatePublisher;
 import us.ihmc.utilities.screwTheory.OneDoFJoint;
 
-public class RosRobotJointStatePublisher implements PacketConsumer<RobotConfigurationData>
+public class RosRobotJointStatePublisher implements PacketConsumer<RobotConfigurationData>, Runnable
 {
    public static final String WORLD_FRAME = "world";
 
@@ -27,6 +29,7 @@ public class RosRobotJointStatePublisher implements PacketConsumer<RobotConfigur
    private final ArrayList<String> nameList = new ArrayList<String>();
    private final RosMainNode rosMainNode;
    private final PPSTimestampOffsetProvider ppsTimestampOffsetProvider;
+   private final ConcurrentLinkedQueue<RobotConfigurationData> availableRobotConfigurationData = new ConcurrentLinkedQueue<RobotConfigurationData>();
 
    private final RigidBodyTransform transformFromHeadToMultisenseHeadRoot = new RigidBodyTransform();
 
@@ -51,27 +54,74 @@ public class RosRobotJointStatePublisher implements PacketConsumer<RobotConfigur
 
       rosMainNode.attachPublisher(rosNameSpace + IHMCRosApiMessageMap.PACKET_TO_TOPIC_MAP.get(RobotConfigurationData.class), jointStatePublisher);
       fieldComputer.attachListener(RobotConfigurationData.class, this);
+      
+      Thread t = new Thread(this, "RosRobotJointStatePublisher");
+      t.start();
    }
 
    @Override
    public void receivedPacket(RobotConfigurationData robotConfigurationData)
    {
-      if (rosMainNode.isStarted())
+      availableRobotConfigurationData.add(robotConfigurationData);
+      if(availableRobotConfigurationData.size() > 30)
       {
-         long timeStamp = ppsTimestampOffsetProvider.adjustRobotTimeStampToRosClock(robotConfigurationData.getTimestamp());
-         Time t = Time.fromNano(timeStamp);
+         availableRobotConfigurationData.clear();
+      }
+   }
 
-         if (robotConfigurationData.jointNameHash != jointNameHash)
+   @Override
+   public void run()
+   {
+      while (true)
+      {
+         RobotConfigurationData robotConfigurationData = availableRobotConfigurationData.poll();
+         if (robotConfigurationData != null)
          {
-            throw new RuntimeException("Joint names do not match for RobotConfigurationData");
+            if (rosMainNode.isStarted())
+            {
+               long timeStamp = ppsTimestampOffsetProvider.adjustRobotTimeStampToRosClock(robotConfigurationData.getTimestamp());
+               Time t = Time.fromNano(timeStamp);
+
+               if (robotConfigurationData.jointNameHash != jointNameHash)
+               {
+                  throw new RuntimeException("Joint names do not match for RobotConfigurationData");
+               }
+
+               jointStatePublisher.publish(nameList, robotConfigurationData.getJointAngles(), robotConfigurationData.getJointVelocities(),
+                     robotConfigurationData.getJointTorques(), t);
+
+               RigidBodyTransform pelvisTransform = new RigidBodyTransform(robotConfigurationData.getOrientation(), robotConfigurationData.getTranslation());
+               tfPublisher.publish(pelvisTransform, timeStamp, WORLD_FRAME, "pelvis");
+               tfPublisher.publish(transformFromHeadToMultisenseHeadRoot, timeStamp, "head", "multisense/head_root");
+
+            }
          }
-
-         jointStatePublisher.publish(nameList, robotConfigurationData.getJointAngles(), robotConfigurationData.getJointVelocities(), robotConfigurationData.getJointTorques(), t);
-
-         RigidBodyTransform pelvisTransform = new RigidBodyTransform(robotConfigurationData.getOrientation(), robotConfigurationData.getTranslation());
-         tfPublisher.publish(pelvisTransform, timeStamp, WORLD_FRAME, "pelvis");
-         tfPublisher.publish(transformFromHeadToMultisenseHeadRoot, timeStamp, "head", "multisense/head_root");
-
+         ThreadTools.sleep(1);
       }
    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
