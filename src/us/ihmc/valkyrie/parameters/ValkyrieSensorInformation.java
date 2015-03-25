b787bfbdd7b3a2279dfc63ead2219b5d4f31e792
Jordan Lack <jlack@ihmc.us>
2015-03-25 21:37:00 +0000

    NONE: Starting to write stuff to bring in Valkyrie's Tekscan sensor. Won't fixbamboo.

--- src/us/ihmc/valkyrie/parameters/ValkyrieSensorInformation.java
+++ src/us/ihmc/valkyrie/parameters/ValkyrieSensorInformation.java
@@ -28,48 +28,19 @@ public class ValkyrieSensorInformation implements DRCRobotSensorInformation
       else
          forceSensorNames = new String[]{ "LeftAnkle", "RightAnkle"};
    }
+   
+   private static final SideDependentList<String> tekscanSensorNames = new SideDependentList<String>("LeftLegTekscan", "RightLegTekscan");
    private static final SideDependentList<String> feetForceSensorNames = new SideDependentList<String>("LeftAnkle", "RightAnkle");
    private static final SideDependentList<String> urdfFeetForceSensorNames = new SideDependentList<>("/v1/LeftLeg6Axis_Offset", "/v1/RightLeg6Axis_Offset");
 
    public static final boolean USE_JSC_FOOT_MASS_TARING = false;
+   
+   public static final double tekscanContactRegionThreshold = .00000044;
+   public static final double tekscanContactThreshold = .00000044;
 
    private static final SideDependentList<SpatialForceVector> footForceSensorTareOffsets;
    static
    {
-//      SpatialForceVector leftFootForceSensorTareOffset_20140406 = new SpatialForceVector(null, new double[] { 7.1, -23.7, 3.8, 186.2, 319.7, 1067.7 });
-//      SpatialForceVector rightFootForceSensorTareOffset_20140406 = new SpatialForceVector(null, new double[] { -1.08, -2.79, -9.63, 38.05, 7.35, -109.3 });
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20140512 = new SpatialForceVector(null, new double[] { 7.02, -23.79, 3.09, 189.6, 322.5, 1081.0 });
-//      SpatialForceVector rightFootForceSensorTareOffset_20140512 = new SpatialForceVector(null, new double[] {-1.12, -2.46, -8.94, 27.54, 3.70, -101.7});
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20140903 = new SpatialForceVector(null, new double[] { 7.1, -24.2, 1.96, 187.9, 306.0, 1131.6 });
-//      SpatialForceVector rightFootForceSensorTareOffset_20140903 = new SpatialForceVector(null, new double[] { -1.0, -2.7, -10.1, 61.1, 16.3, -143.0 });
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20140909 = new SpatialForceVector(null, new double[] {6.6, -23.4, 2.2, 193.9, 298.5, 1142});
-//      SpatialForceVector rightFootForceSensorTareOffset_20140909 = new SpatialForceVector(null, new double[] {-1.5, -2.8, -10.8, 66.0, -2.0, -156.0});
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20140916 = new SpatialForceVector(null, new double[] {6.98, -24.8, 2.01, 175.0, 313.0, 1102.0});
-//      SpatialForceVector rightFootForceSensorTareOffset_20140916 = new SpatialForceVector(null, new double[] {-1.42, -2.89, -11.2, 83.0, -15.0, -138.2});
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20141004 = new SpatialForceVector(null, new double[] {6.14, -22.73, 2.42, 187.8, 303.5, 1083.4});
-//      SpatialForceVector rightFootForceSensorTareOffset_20141004 = new SpatialForceVector(null, new double[] {-1.8, -3.0, -11.6, 73.1, -12.5, -149.0});
-
-      // TODO The force sensor of the right foot seems to be drifting a lot when compared to the left foot force sensor.
-//      SpatialForceVector leftFootForceSensorTareOffset_20141007_1130am = new SpatialForceVector(null, new double[] {5.13, -21.52, 2.88, 189.1, 287.8, 1096.0});
-//      SpatialForceVector rightFootForceSensorTareOffset_20141007_1130am = new SpatialForceVector(null, new double[] {-1.77, -2.9, -9.82, 76.9, -12.8, -89.3});
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20141007_1225pm = new SpatialForceVector(null, new double[] {5.41, -22.25, 2.86, 187.6, 290.8, 1095.8});
-//      SpatialForceVector rightFootForceSensorTareOffset_20141007_1225pm = new SpatialForceVector(null, new double[] {-2.07, -2.96, -9.83, 78.0, -14.14, -113.2});
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20141008_151132 = new SpatialForceVector(null, new double[] { 5.61, -22.31,  2.78,  187.41,  292.99,  1103.93});
-//      SpatialForceVector rightFootForceSensorTareOffset_20141008_151132 = new SpatialForceVector(null, new double[] {-1.90, -2.83, -10.03,  77.03, -12.40, -106.21});
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20141008_154411 = new SpatialForceVector(null, new double[] { 5.96, -22.87,  3.13,  184.51,  296.45,  1086.79});
-//      SpatialForceVector rightFootForceSensorTareOffset_20141008_154411 = new SpatialForceVector(null, new double[] {-1.96, -2.91, -10.37,  82.31, -11.73, -134.80});
-
-//      SpatialForceVector leftFootForceSensorTareOffset_20141008_173646 = new SpatialForceVector(null, new double[] { 5.62, -22.47,  3.04,  186.05,  293.79,  1103.74});
-//      SpatialForceVector rightFootForceSensorTareOffset_20141008_173646 = new SpatialForceVector(null, new double[] {-1.94, -2.88, -10.29,  81.03, -14.43, -108.00});
-
       SpatialForceVector leftFootForceSensorTareOffset_20141012_135433 = new SpatialForceVector(null, new double[] { 6.72, -23.23,  3.28,  183.80,  306.97,  1065.85});
       SpatialForceVector rightFootForceSensorTareOffset_20141012_135433 = new SpatialForceVector(null, new double[] {-1.59, -3.04, -10.12,  82.64, -4.46, -122.53});
 
@@ -171,7 +142,6 @@ public class ValkyrieSensorInformation implements DRCRobotSensorInformation
    {
       return imuSensorsToUse;
    }
-
   
    @Override
    public String[] getForceSensorNames()
@@ -195,6 +165,16 @@ public class ValkyrieSensorInformation implements DRCRobotSensorInformation
    public String getPrimaryBodyImu()
    {
       return leftPelvisIMUSensor;
+   }
+   
+   public static double getTekscanContactRegionThreshold()
+   {
+	   return tekscanContactRegionThreshold;
+   }
+   
+   public static double getTekscanContactThreshold()
+   {
+	   return tekscanContactThreshold;
    }
 
    public static SpatialForceVector getFootForceSensorTareOffset(RobotSide robotSide)
