7b951d3abe8479daabdcf4dda51a68ec91e85533
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-03-25 21:24:14 +0000

    GFE-327 found the ros NaN bug and squished it. wont fixBamboo but does fix a gfe bug

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/RosSCSLidarPublisher.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/ros/RosSCSLidarPublisher.java
@@ -1,18 +1,12 @@
 package us.ihmc.darpaRoboticsChallenge.ros;
 
-import javax.vecmath.AxisAngle4d;
-
 import org.ros.message.Time;
 
 import us.ihmc.SdfLoader.SDFFullRobotModel;
 import us.ihmc.communication.net.PacketConsumer;
 import us.ihmc.communication.packetCommunicator.interfaces.PacketCommunicator;
 import us.ihmc.communication.packets.sensing.SimulatedLidarScanPacket;
-import us.ihmc.communication.packets.sensing.SpindleAnglePacket;
-import us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData.SpindleAngleReceiver;
 import us.ihmc.sensorProcessing.parameters.DRCRobotLidarParameters;
-import us.ihmc.utilities.math.geometry.FrameVector;
-import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 import us.ihmc.utilities.ros.PPSTimestampOffsetProvider;
 import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.utilities.ros.publisher.RosLidarPublisher;
@@ -22,10 +16,7 @@ public class RosSCSLidarPublisher implements PacketConsumer<SimulatedLidarScanPa
    private final RosLidarPublisher[] lidarPublisher;
    private final RosMainNode rosMainNode;
    private final PPSTimestampOffsetProvider ppsTimestampOffsetProvider;
-   private final SDFFullRobotModel fullRobotModel;
    private final DRCRobotLidarParameters[] lidarParameters;
-   private final SpindleAngleReceiver spindleAngleReceiver;
-   private final RosTfPublisher tfPublisher;
    private final int nSensors;
 
    public RosSCSLidarPublisher(PacketCommunicator scsCommunicator,
@@ -38,11 +29,7 @@ public class RosSCSLidarPublisher implements PacketConsumer<SimulatedLidarScanPa
       nSensors = lidarParameters.length;
       this.rosMainNode = rosMainNode;
       this.ppsTimestampOffsetProvider = ppsTimestampOffsetProvider;
-      this.fullRobotModel = fullRobotModel;
       this.lidarParameters = lidarParameters;
-      this.tfPublisher = tfPublisher;
-      
-      spindleAngleReceiver = new SpindleAngleReceiver(this.ppsTimestampOffsetProvider);
       
       lidarPublisher = new RosLidarPublisher[nSensors];
       for (int sensorId = 0; sensorId < nSensors; sensorId++)
@@ -53,50 +40,18 @@ public class RosSCSLidarPublisher implements PacketConsumer<SimulatedLidarScanPa
       }
 
       scsCommunicator.attachListener(SimulatedLidarScanPacket.class, this);
-      scsCommunicator.attachListener(SpindleAnglePacket.class, spindleAngleReceiver);
    }
    
    @Override
-   public void receivedPacket(SimulatedLidarScanPacket object)
+   public void receivedPacket(SimulatedLidarScanPacket simLidarScan)
    {
       if(rosMainNode.isStarted()){
-         int sensorId = object.getSensorId();
-         long timestamp = ppsTimestampOffsetProvider.adjustRobotTimeStampToRosClock(object.getLidarScanParameters().getTimestamp());
+         int sensorId = simLidarScan.getSensorId();
+         long timestamp = ppsTimestampOffsetProvider.adjustRobotTimeStampToRosClock(simLidarScan.getLidarScanParameters().getTimestamp());
          Time time = Time.fromNano(timestamp);
          String frameId = lidarParameters[sensorId].getEndFrameForRosTransform();
-         publishLidarScanFrame(sensorId, timestamp);
-         lidarPublisher[sensorId].publish(object, frameId, time);
+         lidarPublisher[sensorId].publish(simLidarScan, frameId, time);
       }
    }
    
-   private void publishLidarScanFrame(int sensorId, long timestamp)
-   {
-      RigidBodyTransform spindleRotationTransform = getTransformFromJointAngle(sensorId, timestamp);
-      if (spindleRotationTransform == null)
-      {
-         return;
-      }
-      
-      String targetFrame = lidarParameters[sensorId].getEndFrameForRosTransform();
-      String sourceFrame = lidarParameters[sensorId].getBaseFrameForRosTransform();
-      tfPublisher.publish(spindleRotationTransform, timestamp, sourceFrame, targetFrame);
-   }
-   
-   private RigidBodyTransform getTransformFromJointAngle(int sensorId, long timestamp)
-   {
-      String sensorNameInSdf = lidarParameters[sensorId].getSensorNameInSdf();
-      FrameVector spindleAxis = fullRobotModel.getLidarJointAxis(sensorNameInSdf);
-      RigidBodyTransform lidarBaseFrameTransform = fullRobotModel.getLidarBaseFrame(sensorNameInSdf).getTransformToParent();
-      double angle = spindleAngleReceiver.interpolate(timestamp);
-      if (angle == Double.NaN)
-      {
-         return null;
-      }
-      AxisAngle4d spindleRotation = new AxisAngle4d(spindleAxis.getVector(), angle);
-      RigidBodyTransform spindleRotationTransform = new RigidBodyTransform();
-      spindleRotationTransform.setRotationAndZeroTranslation(spindleRotation);
-      RigidBodyTransform transform = lidarBaseFrameTransform;
-      transform.multiply(spindleRotationTransform);
-      return transform;
-   }
 }
