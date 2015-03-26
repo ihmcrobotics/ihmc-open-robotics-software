b0c06a474fac73fccd6949384b3e51bc3c87df70
Steve Spencer <sjspenc@sandia.gov>
2015-03-26 19:28:14 +0000

    NONE: Updated Steppr properties to include spring ankle. wont fixbamboo

--- Acsell/src/us/ihmc/acsell/parameters/BonoPhysicalProperties.java
+++ Acsell/src/us/ihmc/acsell/parameters/BonoPhysicalProperties.java
@@ -10,6 +10,7 @@ import us.ihmc.utilities.robotSide.SideDependentList;
 
 public class BonoPhysicalProperties extends DRCRobotPhysicalProperties
 {
+   /* Original Ankle
    public static final double ankleHeight = 2.0 * 0.0254;
    public static final double shiftFootForward = 0.000;
    public static final double shortenFoot = 0.0;//0.025;
@@ -23,6 +24,23 @@ public class BonoPhysicalProperties extends DRCRobotPhysicalProperties
    public static final double shinLength = 0.42164;
    public static final double legLength = 1.01 * (thighLength + shinLength);
    public static final double pelvisToFoot = 0.887;
+   */
+   
+   /* Spring Ankle */
+   public static final double ankleHeight = 3.0 * 0.0254;
+   public static final double shiftFootForward = 0.001;
+   public static final double shortenFoot = 0.0;//0.025;
+   public static final double footForward = 0.202 + shiftFootForward - shortenFoot;
+   public static final double heelExtension = 2*0.0254;
+   public static final double footBack = 0.05 + heelExtension - shiftFootForward;
+   public static final double footLength = footForward + footBack;
+   public static final double toeWidth = 0.152;
+   public static final double footWidth = toeWidth - 0.022;
+   public static final double thighLength = 0.37694;
+   public static final double shinLength = 0.42164-0.6*0.0254;
+   public static final double legLength = 1.01 * (thighLength + shinLength);
+   public static final double pelvisToFoot = 0.869;
+   
 
    public static final SideDependentList<RigidBodyTransform> soleToAnkleFrameTransforms = new SideDependentList<RigidBodyTransform>();
    static
---
+++ Acsell/src/us/ihmc/acsell/springs/StepprLeftAnkleSpringProperties.java
@@ -0,0 +1,53 @@
+package us.ihmc.acsell.springs;
+
+public class StepprLeftAnkleSpringProperties implements HystereticSpringProperties {
+
+	public StepprLeftAnkleSpringProperties() {
+
+	}
+	
+	@Override
+	public double getLoadingSpringConstant() {
+		return 0.0;
+	}
+	
+	@Override
+	public double getLinearSpringConstant() {
+		return 300;
+	}
+	
+	@Override
+	public double getUnloadingSpringConstant() {
+		return 0.0;
+	}
+		
+	@Override
+	public double getLoadingRestLength() {
+		return 0.0;
+	}
+	
+	@Override
+	public double getLinearSpringRestLength() {
+		return -0.18;
+	}
+	
+	@Override
+	public double getUnloadingRestLength() {
+		return 0.0;
+	}
+	
+	@Override
+	public boolean isCompression() {
+		return true;
+	}
+
+	@Override
+	public boolean isExtension() {
+		return false;
+	}
+	
+	@Override
+	public double getDirectionallity() {
+		return 1.0; //Ankle Compresses at Negative Angles
+	}
+}
