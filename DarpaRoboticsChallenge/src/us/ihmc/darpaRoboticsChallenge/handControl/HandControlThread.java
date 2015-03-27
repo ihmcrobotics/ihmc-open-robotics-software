09d7d9f0c15ddea1207f4d8ed379fa43483a38df
Jesper Smith <jsmith@ihmc.us>
2015-03-27 16:44:37 +0000

    DRC-2491: Changed HandCommandManager to use new PacketCommunicator. fixbamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandControlThread.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/handControl/HandControlThread.java
@@ -1,22 +1,18 @@
 package us.ihmc.darpaRoboticsChallenge.handControl;
 
 import us.ihmc.communication.kryo.IHMCCommunicationKryoNetClassList;
-import us.ihmc.communication.packetCommunicator.KryoPacketCommunicator;
-import us.ihmc.communication.packetCommunicator.KryoPacketServer;
-import us.ihmc.communication.packets.PacketDestination;
+import us.ihmc.communication.packetCommunicator.PacketCommunicatorMock;
 import us.ihmc.communication.util.NetworkPorts;
 import us.ihmc.utilities.robotSide.RobotSide;
 
 public abstract class HandControlThread implements Runnable
 {
-   protected KryoPacketCommunicator packetCommunicator;
+   protected PacketCommunicatorMock packetCommunicator;
 	
    public HandControlThread(RobotSide robotSide)
    {
-      packetCommunicator = new KryoPacketServer(robotSide.equals(RobotSide.LEFT) ? NetworkPorts.LEFT_HAND_PORT.getPort() : NetworkPorts.RIGHT_HAND_PORT.getPort(),
-                                                new IHMCCommunicationKryoNetClassList(),
-                                                robotSide.equals(RobotSide.LEFT) ? PacketDestination.LEFT_HAND_MANAGER.ordinal() : PacketDestination.RIGHT_HAND_MANAGER.ordinal(),
-                                                robotSide.getCamelCaseNameForStartOfExpression() + "HandControlThreadServerCommunicator");
+      NetworkPorts port = robotSide.equals(RobotSide.LEFT) ? NetworkPorts.LEFT_HAND_PORT : NetworkPorts.RIGHT_HAND_PORT;
+      packetCommunicator = PacketCommunicatorMock.createTCPPacketCommunicatorServer(port, new IHMCCommunicationKryoNetClassList());
    }
    
    public abstract void connect();
