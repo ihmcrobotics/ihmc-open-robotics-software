e52dd0d7812d510b4afd264a92c4f1f84d16aab2
Sylvain Bertrand <sbertrand@ihmc.us>
2015-03-27 20:09:36 +0000

    NONE: Added boolean in CapturePointPlannerParameters to deactivate a terrible hack that consists in reducing the ICP desired velocity in the end of transfer state. Won't fixbamboo.

--- src/us/ihmc/valkyrie/parameters/ValkyrieCapturePointPlannerParameters.java
+++ src/us/ihmc/valkyrie/parameters/ValkyrieCapturePointPlannerParameters.java
@@ -4,67 +4,67 @@ import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerPar
 
 public class ValkyrieCapturePointPlannerParameters implements CapturePointPlannerParameters
 {
-	private boolean runningOnRealRobot;
+   private boolean runningOnRealRobot;
 
-	public ValkyrieCapturePointPlannerParameters(boolean runningOnRealRobot)
-	{
-		this.runningOnRealRobot = runningOnRealRobot;
-	}
-	
-	@Override
-	public double getDoubleSupportInitialTransferDuration()
-	{
-		return runningOnRealRobot ? 2.0 : 1.0; 
-	}
+   public ValkyrieCapturePointPlannerParameters(boolean runningOnRealRobot)
+   {
+      this.runningOnRealRobot = runningOnRealRobot;
+   }
 
-	@Override
-	public double getDoubleSupportDuration()
-	{
-		return runningOnRealRobot ? 1.5 : 0.25;
-	}
+   @Override
+   public double getDoubleSupportInitialTransferDuration()
+   {
+      return runningOnRealRobot ? 2.0 : 1.0;
+   }
 
-	@Override
-	public double getAdditionalTimeForSingleSupport()
-	{
-	   return 0.1;
-	}
+   @Override
+   public double getDoubleSupportDuration()
+   {
+      return runningOnRealRobot ? 1.5 : 0.25;
+   }
 
-	@Override
-	public double getSingleSupportDuration()
-	{
-		return runningOnRealRobot ? 1.5 : 0.7;
-	}
+   @Override
+   public double getAdditionalTimeForSingleSupport()
+   {
+      return 0.1;
+   }
 
-	@Override
-	public int getNumberOfFootstepsToConsider()
-	{
-		return 3;
-	}
+   @Override
+   public double getSingleSupportDuration()
+   {
+      return runningOnRealRobot ? 1.5 : 0.7;
+   }
 
-	@Override
-	public int getNumberOfCoefficientsForDoubleSupportPolynomialTrajectory()
-	{
-		return 5;
-	}
+   @Override
+   public int getNumberOfFootstepsToConsider()
+   {
+      return 3;
+   }
 
-	@Override
-	public int getNumberOfFootstepsToStop()
-	{
-		return 2;
-	}
+   @Override
+   public int getNumberOfCoefficientsForDoubleSupportPolynomialTrajectory()
+   {
+      return 5;
+   }
 
-	@Override
-	public double getIsDoneTimeThreshold()
-	{
-		return -1e-4;
-	}
+   @Override
+   public int getNumberOfFootstepsToStop()
+   {
+      return 2;
+   }
+
+   @Override
+   public double getIsDoneTimeThreshold()
+   {
+      return -1e-4;
+   }
 
    @Override
    public double getDoubleSupportSplitFraction()
    {
       return 0.5;
    }
-   
+
    @Override
    public double getFreezeTimeFactor()
    {
@@ -76,7 +76,7 @@ public class ValkyrieCapturePointPlannerParameters implements CapturePointPlanne
    {
       return 0.035;
    }
-   
+
    @Override
    public boolean getDoTimeFreezing()
    {
@@ -94,22 +94,28 @@ public class ValkyrieCapturePointPlannerParameters implements CapturePointPlanne
    {
       return 0.65;
    }
-   
-   @Override
-	public double getCapturePointInFromFootCenterDistance()
-	{
-      return runningOnRealRobot ? 0.026 : 0.006;
-	}
-   
-   @Override
-	public double getCapturePointForwardFromFootCenterDistance()
-	{
-		return 0.0;
-	}
 
-@Override
-public double getMaxAllowedErrorWithoutPartialTimeFreeze()
-{
-	return 0.03;
-}
+   @Override
+   public double getCapturePointInFromFootCenterDistance()
+   {
+      return runningOnRealRobot ? 0.026 : 0.006;
+   }
+
+   @Override
+   public double getCapturePointForwardFromFootCenterDistance()
+   {
+      return 0.0;
+   }
+
+   @Override
+   public double getMaxAllowedErrorWithoutPartialTimeFreeze()
+   {
+      return 0.03;
+   }
+
+   @Override
+   public boolean useTerribleHackToReduceICPVelocityAtTheEndOfTransfer()
+   {
+      return true;
+   }
 }
