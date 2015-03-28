1e986b2403a7fc75bf8758f033a32f2a5ef68f4c
Georg Wiedebach <gwiedebach@ihmc.us>
2015-03-28 23:25:41 +0000

    NONE: added BambooPlanTypes to tests (won't fixbamboo)

--- Atlas/test/us/ihmc/atlas/packets/wholebody/AtlasArmJointTrajectoryPacketTest.java
+++ Atlas/test/us/ihmc/atlas/packets/wholebody/AtlasArmJointTrajectoryPacketTest.java
@@ -4,7 +4,10 @@ import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.packets.ArmJointTrajectoryPacketTest;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
 
+@BambooPlan(planType = {BambooPlanType.Fast})
 public class AtlasArmJointTrajectoryPacketTest extends ArmJointTrajectoryPacketTest
 {
    private final AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false);
--- Atlas/test/us/ihmc/atlas/packets/wholebody/AtlasWholeBodyTrajectoryPacketTest.java
+++ Atlas/test/us/ihmc/atlas/packets/wholebody/AtlasWholeBodyTrajectoryPacketTest.java
@@ -4,7 +4,10 @@ import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.packets.WholeBodyTrajectoryPacketTest;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
 
+@BambooPlan(planType = {BambooPlanType.Fast})
 public class AtlasWholeBodyTrajectoryPacketTest extends WholeBodyTrajectoryPacketTest
 {
    private final AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, AtlasRobotModel.AtlasTarget.SIM, false);
