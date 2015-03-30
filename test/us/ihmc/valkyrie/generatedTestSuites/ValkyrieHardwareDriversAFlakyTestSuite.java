9736413e149251864d82ed091e43f465e122689d
Duncan Calvert <dcalvert@ihmc.us>
2015-03-30 15:21:38 +0000

    DRC-2484: Fixing lidar unit test to verify Jesper's non-cheating lidar fix. Won't fixBamboo.

---
+++ test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversAFlakyTestSuite.java
@@ -0,0 +1,19 @@
+package us.ihmc.valkyrie.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.valkyrie.networkProcessor.depthData.ValkyrieDepthDataProcessorTest.class
+})
+
+public class ValkyrieHardwareDriversAFlakyTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
--- test/us/ihmc/valkyrie/networkProcessor/depthData/ValkyrieDepthDataProcessorTest.java
+++ test/us/ihmc/valkyrie/networkProcessor/depthData/ValkyrieDepthDataProcessorTest.java
@@ -3,8 +3,11 @@ package us.ihmc.valkyrie.networkProcessor.depthData;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData.DepthDataProcessorTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 
+@BambooPlan(planType = BambooPlanType.Flaky)
 public class ValkyrieDepthDataProcessorTest extends DepthDataProcessorTest
 {
    private final DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
