53a02116274534541173aeee26e8abe2ccf91526
Jesper Smith <jsmith@ihmc.us>
2015-03-31 17:18:31 +0000

    DRC-2491: Switched to new packetcommunicator that uses server->client for local communicators. does not fixbamboo

--- src/us/ihmc/valkyrie/ValkyrieNetworkProcessor.java
+++ src/us/ihmc/valkyrie/ValkyrieNetworkProcessor.java
@@ -2,13 +2,6 @@ package us.ihmc.valkyrie;
 
 import java.net.URISyntaxException;
 
-import us.ihmc.communication.configuration.NetworkParameterKeys;
-import us.ihmc.communication.configuration.NetworkParameters;
-import us.ihmc.communication.kryo.IHMCCommunicationKryoNetClassList;
-import us.ihmc.communication.packetCommunicator.KryoPacketClientEndPointCommunicator;
-import us.ihmc.communication.packetCommunicator.KryoPacketCommunicator;
-import us.ihmc.communication.packets.PacketDestination;
-import us.ihmc.communication.util.NetworkPorts;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.DRCNetworkModuleParameters;
 import us.ihmc.darpaRoboticsChallenge.networkProcessor.DRCNetworkProcessor;
@@ -22,17 +15,9 @@ public class ValkyrieNetworkProcessor
    
    public static void main(String[] args) throws URISyntaxException, JSAPException
    {
-      IHMCCommunicationKryoNetClassList netClassList = new IHMCCommunicationKryoNetClassList();
-      String controllerKryoServerIp = NetworkParameters.getHost(NetworkParameterKeys.robotController);
-      int tcpPort = NetworkPorts.NETWORK_PROCESSOR_TO_CONTROLLER_TCP_PORT.getPort();
-      int communicatorId = PacketDestination.CONTROLLER.ordinal();
-      
-      KryoPacketCommunicator realRobotControllerConnection = new KryoPacketClientEndPointCommunicator(controllerKryoServerIp, tcpPort, netClassList,
-            communicatorId, "VAL_Controller_Endpoint");
-      
       DRCNetworkModuleParameters networkModuleParams = new DRCNetworkModuleParameters();
       
-      networkModuleParams.setControllerCommunicator(realRobotControllerConnection);
+      networkModuleParams.setUseLocalControllerCommunicator(false);
       networkModuleParams.setUseUiModule(true);
       networkModuleParams.setUseBehaviorModule(true);
       networkModuleParams.setUseBehaviorVisualizer(true);
