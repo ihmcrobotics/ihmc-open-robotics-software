cd1740671140b7dfa32b029c0a5b2c82b04736dc
Duncan Calvert <dcalvert@ihmc.us>
2015-03-31 17:07:12 +0000

    DRC-2484: Renamed DepthDataProcessorTest to PointCloudDataReceiverTest. Won't fixBamboo.

--- Atlas/test/us/ihmc/atlas/networkProcessor/depthData/AtlasPointCloudDataReceiverTest.java
+++ Atlas/test/us/ihmc/atlas/networkProcessor/depthData/AtlasPointCloudDataReceiverTest.java
@@ -3,13 +3,13 @@ package us.ihmc.atlas.networkProcessor.depthData;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
-import us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData.DepthDataProcessorTest;
+import us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData.PointCloudDataReceiverTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
 import us.ihmc.utilities.code.agileTesting.BambooPlanType;
 import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
 
 @BambooPlan(planType = BambooPlanType.Flaky)
-public class AtlasDepthDataProcessorTest extends DepthDataProcessorTest
+public class AtlasPointCloudDataReceiverTest extends PointCloudDataReceiverTest
 {
    private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, AtlasRobotModel.AtlasTarget.SIM, false);
 
