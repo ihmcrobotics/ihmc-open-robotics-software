57c80e81ad23afcdccfba043174076f1f9463dd2
Duncan Calvert <dcalvert@ihmc.us>
2015-04-01 17:53:45 +0000

    DRC-2422: Moved Tbot to Slow Bamboo plan. Regenerated test suites. Will help fixBamboo.

--- Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasRInDevelopmentTestSuite.java
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasRInDevelopmentTestSuite.java
@@ -7,8 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.atlas.stateEstimation.AtlasPelvisPoseHistoryCorrectorTest.class,
-   us.ihmc.atlas.ToBeMovedOrDeletedWholeBodyIkSolverTest.class
+   us.ihmc.atlas.roughTerrainWalking.AtlasFootExplorationTest.class
 })
 
 public class AtlasRInDevelopmentTestSuite
