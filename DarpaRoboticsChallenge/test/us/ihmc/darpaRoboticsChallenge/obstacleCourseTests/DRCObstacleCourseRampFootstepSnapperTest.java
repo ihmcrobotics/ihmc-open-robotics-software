1ec59d1ccbdd235cca1dcecd9778960ad4922ce3
Duncan Calvert <dcalvert@ihmc.us>
2015-04-01 20:19:42 +0000

    DRC-2422: Ran test suite runner on ValAFast. Regenerated test suites. Will help fixBamboo.

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/obstacleCourseTests/DRCObstacleCourseRampFootstepSnapperTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/obstacleCourseTests/DRCObstacleCourseRampFootstepSnapperTest.java
@@ -86,8 +86,8 @@ public abstract class DRCObstacleCourseRampFootstepSnapperTest implements MultiR
    private final ComHeightPacket comHeightPacket = new ComHeightPacket(0.05, 1.0);
    private final Random random = new Random(165163L);
 
-	@EstimatedDuration(duration = 62.3)
-   @Test(timeout = 311413)
+	@EstimatedDuration(duration = 68.7)
+   @Test(timeout = 343427)
    public void testWalkingUpRampUsingSnapFootsteps() throws SimulationExceededMaximumTimeException
    {
       BambooTools.reportTestStartedMessage();
