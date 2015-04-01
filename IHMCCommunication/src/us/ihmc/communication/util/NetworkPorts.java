6e2d3bcdf529073f8e2d45551ddd3ee840578d52
Jesper Smith <jsmith@ihmc.us>
2015-04-01 15:29:55 +0000

    DRC-2491: Switched from enum to class with statics for networkports, allowing creation of random test ports. might fixbamboo

--- IHMCCommunication/src/us/ihmc/communication/util/NetworkPorts.java
+++ IHMCCommunication/src/us/ihmc/communication/util/NetworkPorts.java
@@ -1,43 +1,40 @@
 package us.ihmc.communication.util;
 
-public enum NetworkPorts
+import java.util.Random;
+
+public class NetworkPorts
 {
    // Network processor <-> Controller communication
-   CONTROLLER_PORT(4895),
+   public static final NetworkPorts CONTROLLER_PORT = new NetworkPorts(4895);
    
    // Network processor <-> UI Communication
-   NETWORK_PROCESSOR_TO_UI_TCP_PORT(4898),
-   NETWORK_PROCESSOR_TO_UI_TEST_TCP_PORT(4899),
+   public static final NetworkPorts NETWORK_PROCESSOR_TO_UI_TCP_PORT = new NetworkPorts(4898);
+   public static final NetworkPorts NETWORK_PROCESSOR_TO_UI_TEST_TCP_PORT = new NetworkPorts(4899);
    
    // Cloud dispatcher ports
-   NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_TCP_PORT(5000),
-   NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_CONSOLE_TCP_PORT(5005),
-   CONTROLLER_CLOUD_DISPATCHER_BACKEND_TCP_PORT(5002),
-   CONTROLLER_CLOUD_DISPATCHER_BACKEND_CONSOLE_TCP_PORT(5007),
+   public static final NetworkPorts NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_TCP_PORT = new NetworkPorts(5000);
+   public static final NetworkPorts NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_CONSOLE_TCP_PORT = new NetworkPorts(5005);
+   public static final NetworkPorts CONTROLLER_CLOUD_DISPATCHER_BACKEND_TCP_PORT = new NetworkPorts(5002);
+   public static final NetworkPorts CONTROLLER_CLOUD_DISPATCHER_BACKEND_CONSOLE_TCP_PORT = new NetworkPorts(5007);
    
    // Hands
-   LEFT_HAND_PORT(5003),
-   RIGHT_HAND_PORT(5004),
+   public static final NetworkPorts LEFT_HAND_PORT = new NetworkPorts(5003);
+   public static final NetworkPorts RIGHT_HAND_PORT = new NetworkPorts(5004);
    
-   
-   // Test ports
-   TEST_PORT_A(21090),
-   TEST_PORT_B(21091),
-   TEST_PORT_C(21092),
-   TEST_PORT_D(21093),
-   TEST_PORT_E(21094),
-   TEST_PORT_F(21095), 
    
    // Network manager ports
-   BEHAVIOUR_MODULE_PORT(6001), 
-   UI_MODULE(6002), 
-   SENSOR_MANAGER(6003), 
-   LEFT_HAND_MANAGER_PORT(6004),
-   RIGHT_HAND_MANAGER_PORT(6005), 
-   ROS_MODULE(6006), 
-   GFE_COMMUNICATOR(6007),
+   public static final NetworkPorts BEHAVIOUR_MODULE_PORT = new NetworkPorts(6001); 
+   public static final NetworkPorts UI_MODULE = new NetworkPorts(6002);
+   public static final NetworkPorts SENSOR_MANAGER = new NetworkPorts(6003);
+   public static final NetworkPorts LEFT_HAND_MANAGER_PORT = new NetworkPorts(6004);
+   public static final NetworkPorts RIGHT_HAND_MANAGER_PORT = new NetworkPorts(6005); 
+   public static final NetworkPorts ROS_MODULE = new NetworkPorts(6006);
+   public static final NetworkPorts GFE_COMMUNICATOR = new NetworkPorts(6007);
    
-   ;
+   public static final NetworkPorts createRandomTestPort()
+   {
+      return new NetworkPorts(new Random().nextInt(65535 - 1025) + 1025);
+   }
    
    private final int port;
    private NetworkPorts(int port)
