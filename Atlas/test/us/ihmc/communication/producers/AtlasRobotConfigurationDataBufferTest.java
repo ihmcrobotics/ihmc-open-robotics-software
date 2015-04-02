7e983e7a30899747202508dc58a1d65ef5c534c3
Duncan Calvert <dcalvert@ihmc.us>
2015-04-02 20:46:57 +0000

    DRC-2422: Moved test to flaky. There is a race condition or something. Also put it in a loop so it is less flaky. Will help fixBamboo.

--- Atlas/test/us/ihmc/communication/producers/AtlasRobotConfigurationDataBufferTest.java
+++ Atlas/test/us/ihmc/communication/producers/AtlasRobotConfigurationDataBufferTest.java
@@ -3,8 +3,11 @@ package us.ihmc.communication.producers;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotModel.AtlasTarget;
 import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.BambooPlan;
+import us.ihmc.utilities.code.agileTesting.BambooPlanType;
 import us.ihmc.utilities.humanoidRobot.model.FullRobotModel;
 
+@BambooPlan(planType = BambooPlanType.Flaky)
 public class AtlasRobotConfigurationDataBufferTest extends RobotConfigurationDataBufferTest
 {
 
