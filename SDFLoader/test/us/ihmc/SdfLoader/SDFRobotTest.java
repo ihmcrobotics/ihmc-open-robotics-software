c26f17fe96baa98313a5570560a5518d494348a6
Jesper Smith <jsmith@ihmc.us>
2015-04-06 16:11:49 +0000

    NONE: Fixed nullpointerexception and made test to avoid this in the futures.

---
+++ SDFLoader/test/us/ihmc/SdfLoader/SDFRobotTest.java
@@ -0,0 +1,21 @@
+package us.ihmc.SdfLoader;
+
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Test;
+
+public class SDFRobotTest
+{
+
+   @Test
+   public void testNullJointMap() throws FileNotFoundException, JAXBException
+   {
+      JaxbSDFLoader loader = new JaxbSDFLoader(getClass().getClassLoader().getResourceAsStream("sdfRobotTest.sdf"), null);
+      
+      GeneralizedSDFRobotModel generalizedSDFRobotModel = loader.getGeneralizedSDFRobotModel("atlas");
+      new SDFRobot(generalizedSDFRobotModel, null, true);
+   }
+
+}
