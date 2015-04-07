b762d84f9650bbf87559b52273432975e4cd42a4
Jesper Smith <jsmith@ihmc.us>
2015-04-07 01:00:23 +0000

    DRC-2523: Connect/disconnect UI connection based on network processor connection state. does not fixbamboo

--- IHMCCommunication/src/us/ihmc/communication/net/NetworkedObjectCommunicator.java
+++ IHMCCommunication/src/us/ihmc/communication/net/NetworkedObjectCommunicator.java
@@ -9,4 +9,10 @@ public interface NetworkedObjectCommunicator extends ObjectCommunicator
     * @return bytes send
     */
    public int send(Object object);
+   
+   /**
+    * Disconnect the connection, but leave the executor listeners alive. This allows re-connecting at a later moment. 
+    * Use close() when you want to shutdown the connection completely. 
+    */
+   public void closeConnection();
 }
