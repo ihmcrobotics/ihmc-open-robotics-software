e72d0b5f12af1a8c1301019d752afc3146805577
Douglas Stephen <dstephen@ihmc.us>
2015-04-08 02:16:44 +0000

    NONE: Got the rest of the aux data going out over ROS. Won't fixbamboo.

--- Atlas/src/us/ihmc/atlas/ros/RosAtlasAuxiliaryRobotDataPublisher.java
+++ Atlas/src/us/ihmc/atlas/ros/RosAtlasAuxiliaryRobotDataPublisher.java
@@ -8,6 +8,7 @@ import us.ihmc.communication.packets.manipulation.AtlasElectricMotorPacketEnum;
 import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.utilities.ros.publisher.RosBoolPublisher;
 import us.ihmc.utilities.ros.publisher.RosDoublePublisher;
+import us.ihmc.utilities.ros.publisher.RosInt64Publisher;
 
 import java.util.LinkedHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -25,29 +26,74 @@ public class RosAtlasAuxiliaryRobotDataPublisher implements PacketConsumer<Robot
    private final LinkedHashMap<AtlasElectricMotorPacketEnum, RosDoublePublisher> electricForearmTemperaturePublishers = new LinkedHashMap<>();
    private final LinkedHashMap<AtlasElectricMotorPacketEnum, RosDoublePublisher> electricForearmCurrentPublishers = new LinkedHashMap<>();
 
+   private final RosDoublePublisher pumpInletPressurePublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher pumpSupplyPressurePublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher airSumpPressurePublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher pumpSupplyTemperaturePublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher pumpRPMPublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher motorTemperaturePublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher motorDriverTemperaturePublisher = new RosDoublePublisher(false);
+
+   private final RosBoolPublisher batteryChargingPublisher = new RosBoolPublisher(false);
+   private final RosDoublePublisher batteryVoltagePublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher batteryCurrentPublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher remainingBatteryTimePublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher remainingAmpHoursPublisher = new RosDoublePublisher(false);
+   private final RosDoublePublisher remainingChargePercentagePublisher = new RosDoublePublisher(false);
+   private final RosInt64Publisher cycleCountPublisher = new RosInt64Publisher(false);
+
    public RosAtlasAuxiliaryRobotDataPublisher(RosMainNode rosMainNode, String rosNameSpace)
    {
       this.rosMainNode = rosMainNode;
 
-      boolean latched = false;
+      setupElectricForearmPublishers(rosNameSpace);
 
-      for (AtlasElectricMotorPacketEnum value : AtlasElectricMotorPacketEnum.values)
-      {
-         RosBoolPublisher boolPublisher = new RosBoolPublisher(latched);
-         electricForearmEnabledPublishers.put(value, boolPublisher);
-         rosMainNode.attachPublisher(rosNameSpace + "/output/electric_forearms/" + value.toString().toLowerCase() + "/enabled", boolPublisher);
+      setupPumpPublishers(rosNameSpace);
 
-         RosDoublePublisher tempPublisher = new RosDoublePublisher(latched);
-         electricForearmTemperaturePublishers.put(value, tempPublisher);
-         rosMainNode.attachPublisher(rosNameSpace + "/output/electric_forearms/" + value.toString().toLowerCase() + "/temperature", tempPublisher);
-
-         RosDoublePublisher currentPublisher = new RosDoublePublisher(latched);
-         electricForearmCurrentPublishers.put(value, currentPublisher);
-         rosMainNode.attachPublisher(rosNameSpace + "/output/electric_forearms/" + value.toString().toLowerCase() + "/drive_current", currentPublisher);
-      }
+      setupBatteryPublishers(rosMainNode, rosNameSpace);
 
       Thread thread = new Thread(this, "RosAtlasAuxiliaryRobotDataPublisher");
       thread.start();
+   }
+
+   private void setupBatteryPublishers(RosMainNode rosMainNode, String rosNameSpace)
+   {
+      rosMainNode.attachPublisher(rosNameSpace + "/output/battery/charging", batteryChargingPublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/battery/voltage", batteryVoltagePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/battery/current", batteryCurrentPublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/battery/time_remaining", remainingBatteryTimePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/battery/amp_hours_remaining", remainingAmpHoursPublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/battery/charge_percentage_remaining", remainingChargePercentagePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/battery/cycle_count", cycleCountPublisher);
+   }
+
+   private void setupPumpPublishers(String rosNameSpace)
+   {
+      rosMainNode.attachPublisher(rosNameSpace + "/output/pump/pump_inlet_pressure", pumpInletPressurePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/pump/pump_supply_pressure", pumpSupplyPressurePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/pump/air_sump_pressure", airSumpPressurePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/pump/pump_supply_temperature", pumpSupplyTemperaturePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/pump/pump_rpm", pumpRPMPublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/pump/motor_temperature", motorTemperaturePublisher);
+      rosMainNode.attachPublisher(rosNameSpace + "/output/pump/motor_driver_temperature", motorDriverTemperaturePublisher);
+   }
+
+   private void setupElectricForearmPublishers(String rosNameSpace)
+   {
+      for (AtlasElectricMotorPacketEnum value : AtlasElectricMotorPacketEnum.values)
+      {
+         RosBoolPublisher boolPublisher = new RosBoolPublisher(false);
+         electricForearmEnabledPublishers.put(value, boolPublisher);
+         rosMainNode.attachPublisher(rosNameSpace + "/output/electric_forearms/" + value.toString().toLowerCase() + "/enabled", boolPublisher);
+
+         RosDoublePublisher tempPublisher = new RosDoublePublisher(false);
+         electricForearmTemperaturePublishers.put(value, tempPublisher);
+         rosMainNode.attachPublisher(rosNameSpace + "/output/electric_forearms/" + value.toString().toLowerCase() + "/temperature", tempPublisher);
+
+         RosDoublePublisher currentPublisher = new RosDoublePublisher(false);
+         electricForearmCurrentPublishers.put(value, currentPublisher);
+         rosMainNode.attachPublisher(rosNameSpace + "/output/electric_forearms/" + value.toString().toLowerCase() + "/drive_current", currentPublisher);
+      }
    }
 
    @Override public void run()
@@ -59,22 +105,53 @@ public class RosAtlasAuxiliaryRobotDataPublisher implements PacketConsumer<Robot
          {
             if(rosMainNode.isStarted())
             {
-               for (AtlasElectricMotorPacketEnum value : AtlasElectricMotorPacketEnum.values)
-               {
-                  RosBoolPublisher rosBoolPublisher = electricForearmEnabledPublishers.get(value);
-                  rosBoolPublisher.publish(auxiliaryRobotData.electricJointEnabledArray[value.getId()]);
+               publishElectricForearmData(auxiliaryRobotData);
 
-                  RosDoublePublisher temperaturePublisher = electricForearmTemperaturePublishers.get(value);
-                  temperaturePublisher.publish(auxiliaryRobotData.electricJointTemperatures[value.getId()]);
+               publishPumpData(auxiliaryRobotData);
 
-                  RosDoublePublisher currentPublisher = electricForearmCurrentPublishers.get(value);
-                  currentPublisher.publish(auxiliaryRobotData.electricJointCurrents[value.getId()]);
-               }
+               publishBatteryData(auxiliaryRobotData);
             }
          }
 
          Thread.yield();
       }
+   }
+
+   private void publishElectricForearmData(AtlasAuxiliaryRobotData auxiliaryRobotData)
+   {
+      for (AtlasElectricMotorPacketEnum value : AtlasElectricMotorPacketEnum.values)
+      {
+         RosBoolPublisher rosBoolPublisher = electricForearmEnabledPublishers.get(value);
+         rosBoolPublisher.publish(auxiliaryRobotData.electricJointEnabledArray[value.getId()]);
+
+         RosDoublePublisher temperaturePublisher = electricForearmTemperaturePublishers.get(value);
+         temperaturePublisher.publish(auxiliaryRobotData.electricJointTemperatures[value.getId()]);
+
+         RosDoublePublisher currentPublisher = electricForearmCurrentPublishers.get(value);
+         currentPublisher.publish(auxiliaryRobotData.electricJointCurrents[value.getId()]);
+      }
+   }
+
+   private void publishPumpData(AtlasAuxiliaryRobotData auxiliaryRobotData)
+   {
+      pumpInletPressurePublisher.publish(auxiliaryRobotData.pumpInletPressure);
+      pumpSupplyPressurePublisher.publish(auxiliaryRobotData.pumpSupplyPressure);
+      airSumpPressurePublisher.publish(auxiliaryRobotData.airSumpPressure);
+      pumpSupplyTemperaturePublisher.publish(auxiliaryRobotData.pumpSupplyTemperature);
+      pumpRPMPublisher.publish(auxiliaryRobotData.pumpRPM);
+      motorTemperaturePublisher.publish(auxiliaryRobotData.motorTemperature);
+      motorDriverTemperaturePublisher.publish(auxiliaryRobotData.motorDriverTemperature);
+   }
+
+   private void publishBatteryData(AtlasAuxiliaryRobotData auxiliaryRobotData)
+   {
+      batteryChargingPublisher.publish(auxiliaryRobotData.batteryCharging);
+      batteryVoltagePublisher.publish(auxiliaryRobotData.batteryVoltage);
+      batteryCurrentPublisher.publish(auxiliaryRobotData.batteryCurrent);
+      remainingBatteryTimePublisher.publish(auxiliaryRobotData.remainingBatteryTime);
+      remainingAmpHoursPublisher.publish(auxiliaryRobotData.remainingAmpHours);
+      remainingChargePercentagePublisher.publish(auxiliaryRobotData.remainingChargePercentage);
+      cycleCountPublisher.publish(auxiliaryRobotData.batteryCycleCount);
    }
 
    @Override public void receivedPacket(RobotConfigurationData packet)
