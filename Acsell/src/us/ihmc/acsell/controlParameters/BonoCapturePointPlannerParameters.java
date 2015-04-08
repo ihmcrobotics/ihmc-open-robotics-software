0d1e250badee32ae618bbad4adea0e086823af02
Sylvain Bertrand <sbertrand@ihmc.us>
2015-04-08 21:21:11 +0000

    NONE: Extended CapturePointPlannerParameters to include the parameters needed for the new ICP trajectory. Also renamed the methods getCapturePointInFromFootCenterDistance() and getCapturePointForwardFromFootCenterDistance() to getReferenceCMPInsideOffset() and getReferenceCMPForwardOffset(). Won't fixbamboo.

--- Acsell/src/us/ihmc/acsell/controlParameters/BonoCapturePointPlannerParameters.java
+++ Acsell/src/us/ihmc/acsell/controlParameters/BonoCapturePointPlannerParameters.java
@@ -5,24 +5,24 @@ import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerPar
 public class BonoCapturePointPlannerParameters implements CapturePointPlannerParameters
 {
 
-	private boolean runningOnRealRobot;
+   private boolean runningOnRealRobot;
 
-	public BonoCapturePointPlannerParameters(boolean runningOnRealRobot)
-	{
-		this.runningOnRealRobot = runningOnRealRobot;
-	}
-	
-	@Override
-	public double getDoubleSupportInitialTransferDuration()
-	{
-		return 1.0; 
-	}
+   public BonoCapturePointPlannerParameters(boolean runningOnRealRobot)
+   {
+      this.runningOnRealRobot = runningOnRealRobot;
+   }
 
-	@Override
-	public double getDoubleSupportDuration()
-	{
-		return runningOnRealRobot ? 0.5 : 0.25;
-	}
+   @Override
+   public double getDoubleSupportInitialTransferDuration()
+   {
+      return 1.0;
+   }
+
+   @Override
+   public double getDoubleSupportDuration()
+   {
+      return runningOnRealRobot ? 0.5 : 0.25;
+   }
 
    @Override
    public double getAdditionalTimeForSingleSupport()
@@ -30,42 +30,42 @@ public class BonoCapturePointPlannerParameters implements CapturePointPlannerPar
       return 0.0;
    }
 
-	@Override
-	public double getSingleSupportDuration()
-	{
-		return runningOnRealRobot ? 1.0 : 0.7;
-	}
+   @Override
+   public double getSingleSupportDuration()
+   {
+      return runningOnRealRobot ? 1.0 : 0.7;
+   }
 
-	@Override
-	public int getNumberOfFootstepsToConsider()
-	{
-		return 3;
-	}
+   @Override
+   public int getNumberOfFootstepsToConsider()
+   {
+      return 3;
+   }
 
-	@Override
-	public int getNumberOfCoefficientsForDoubleSupportPolynomialTrajectory()
-	{
-		return 5;
-	}
+   @Override
+   public int getNumberOfCoefficientsForDoubleSupportPolynomialTrajectory()
+   {
+      return 5;
+   }
 
-	@Override
-	public int getNumberOfFootstepsToStop()
-	{
-		return 2;
-	}
+   @Override
+   public int getNumberOfFootstepsToStop()
+   {
+      return 2;
+   }
 
-	@Override
-	public double getIsDoneTimeThreshold()
-	{
-		return -1e-4;
-	}
+   @Override
+   public double getIsDoneTimeThreshold()
+   {
+      return -1e-4;
+   }
 
    @Override
    public double getDoubleSupportSplitFraction()
    {
       return 0.5;
    }
-   
+
    @Override
    public double getFreezeTimeFactor()
    {
@@ -77,18 +77,18 @@ public class BonoCapturePointPlannerParameters implements CapturePointPlannerPar
    {
       return 0.02;
    }
-   
+
    @Override
    public boolean getDoTimeFreezing()
    {
       return false;
    }
-	
-	@Override
-	public double getMaxAllowedErrorWithoutPartialTimeFreeze()
-	{
-		return 0.03;
-	}
+
+   @Override
+   public double getMaxAllowedErrorWithoutPartialTimeFreeze()
+   {
+      return 0.03;
+   }
 
    @Override
    public boolean getDoFootSlipCompensation()
@@ -102,21 +102,57 @@ public class BonoCapturePointPlannerParameters implements CapturePointPlannerPar
       return 0.65;
    }
 
-	@Override
-	public double getCapturePointInFromFootCenterDistance()
-	{
-		return 0.005;
-	}
-	
-	@Override
-	public double getCapturePointForwardFromFootCenterDistance()
-	{
-		return 0.015;//0.00;//0.02-.025/2;
-	}
+   @Override
+   public double getReferenceCMPInsideOffset()
+   {
+      return 0.005;
+   }
+
+   @Override
+   public double getReferenceCMPForwardOffset()
+   {
+      return 0.015;//0.00;//0.02-.025/2;
+   }
 
    @Override
    public boolean useTerribleHackToReduceICPVelocityAtTheEndOfTransfer()
    {
       return true;
+   }
+
+   @Override
+   public boolean useNewICPPlanner()
+   {
+      return false;
+   }
+
+   @Override
+   public boolean useTwoCMPsPerSupport()
+   {
+      return false;
+   }
+
+   @Override
+   public double getTimeSpentOnExitCMPInPercentOfStepTime()
+   {
+      return 0.50;
+   }
+
+   @Override
+   public double getMaxReferenceCMPForwardOffset()
+   {
+      return 0.05;
+   }
+
+   @Override
+   public double getMinReferenceCMPForwardOffset()
+   {
+      return -0.02;
+   }
+
+   @Override
+   public double getSafeDistanceForSupportEdges()
+   {
+      return 0.03;
    }
 }
