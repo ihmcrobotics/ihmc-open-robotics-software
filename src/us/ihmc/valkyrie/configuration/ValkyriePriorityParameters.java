c4d65d56af0e8ca8aca04c4bef25cba03a9ed581
Jesper Smith <jsmith@ihmc.us>
2015-04-10 16:27:36 +0000

    DRC-2564: Switched logger over to an RT thread. does not fixbamboo

--- src/us/ihmc/valkyrie/configuration/ValkyriePriorityParameters.java
+++ src/us/ihmc/valkyrie/configuration/ValkyriePriorityParameters.java
@@ -8,7 +8,8 @@ public class ValkyriePriorityParameters
 {
    public static final PriorityParameters ESTIMATOR_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 1);
    public static final PriorityParameters CONTROLLER_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 5);
-   public static final PriorityParameters IMU_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 10);   
+   public static final PriorityParameters IMU_PRIORITY = new PriorityParameters(PriorityParameters.getMaximumPriority() - 10);
+   public static final PriorityParameters LOGGER_PRIORITY = new PriorityParameters(45);
 
    // FQN of the turbodriver we use to test overall round trip time
    public static final Class<?>[] timedTurbodrivers = {TurbodriverAPI_DRCv4_ihmc.class, TurbodriverAPI_DRCv4_bench.class};
