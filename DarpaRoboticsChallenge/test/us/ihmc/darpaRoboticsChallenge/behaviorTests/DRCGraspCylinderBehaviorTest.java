252e55fa5a5f9806ffe0348bf02130b384e00123
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-04-13 22:39:40 +0000

    NONE: Created grasp cylinder behavior and unit tests.  won't fixBamboo.

--- DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/behaviorTests/DRCGraspCylinderBehaviorTest.java
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/behaviorTests/DRCGraspCylinderBehaviorTest.java
@@ -17,12 +17,12 @@ import us.ihmc.communication.util.NetworkPorts;
 import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseStartingLocation;
 import us.ihmc.darpaRoboticsChallenge.MultiRobotTestInterface;
 import us.ihmc.darpaRoboticsChallenge.environment.CommonAvatarEnvironmentInterface;
-import us.ihmc.darpaRoboticsChallenge.environment.DRCSteeringWheelEnvironment;
+import us.ihmc.darpaRoboticsChallenge.environment.DRCGraspCylinderEnvironment;
 import us.ihmc.darpaRoboticsChallenge.testTools.DRCBehaviorTestHelper;
 import us.ihmc.humanoidBehaviors.behaviors.midLevel.GraspCylinderBehavior;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
 import us.ihmc.simulationconstructionset.bambooTools.SimulationTestingParameters;
-import us.ihmc.simulationconstructionset.util.environments.ContactableSteeringWheelRobot;
+import us.ihmc.simulationconstructionset.util.environments.ContactableStaticCylinderRobot;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.utilities.MemoryTools;
 import us.ihmc.utilities.ThreadTools;
@@ -86,22 +86,37 @@ public abstract class DRCGraspCylinderBehaviorTest implements MultiRobotTestInte
             simulationTestingParameters, getRobotModel());
    }
 
-   private static DRCSteeringWheelEnvironment createTestEnvironment()
+   private static DRCGraspCylinderEnvironment createTestEnvironment()
    {
-      ArrayList<Point3d> valveLocations = new ArrayList<Point3d>();
-      LinkedHashMap<Point3d, Double> valveYawAngles_degrees = new LinkedHashMap<Point3d, Double>();
-      LinkedHashMap<Point3d, Double> valvePitchAngles_degrees = new LinkedHashMap<Point3d, Double>();
+      ArrayList<Point3d> cylinderOrigins = new ArrayList<Point3d>();
+      LinkedHashMap<Point3d, Double> cylinderYawAngles_degrees = new LinkedHashMap<Point3d, Double>();
+      LinkedHashMap<Point3d, Double> cylinderPitchAngles_degrees = new LinkedHashMap<Point3d, Double>();
+      LinkedHashMap<Point3d, Double> cylinderRollAngles_degrees = new LinkedHashMap<Point3d, Double>();
 
-      valveLocations.add(new Point3d(0.25, 0.0, 0.6));
-      valveYawAngles_degrees.put(valveLocations.get(0), 0.0);
-      valvePitchAngles_degrees.put(valveLocations.get(0), 90.0);
+      // X-axis cylinder
+      cylinderOrigins.add(new Point3d(0.5, 0.0, 0.6));
+      cylinderYawAngles_degrees.put(cylinderOrigins.get(0), 0.0);
+      cylinderPitchAngles_degrees.put(cylinderOrigins.get(0), 90.0);
+      cylinderRollAngles_degrees.put(cylinderOrigins.get(0), 0.0);
 
-      return new DRCSteeringWheelEnvironment(valveLocations, valveYawAngles_degrees, valvePitchAngles_degrees);
+      // Y-axis cylinder
+      cylinderOrigins.add(new Point3d(0.7, 0.3, 0.6));
+      cylinderYawAngles_degrees.put(cylinderOrigins.get(1), 0.0);
+      cylinderPitchAngles_degrees.put(cylinderOrigins.get(1), 0.0);
+      cylinderRollAngles_degrees.put(cylinderOrigins.get(1), 90.0);
+
+      // Z-axis cylinder
+      cylinderOrigins.add(new Point3d(0.25, 0.0, 0.7));
+      cylinderYawAngles_degrees.put(cylinderOrigins.get(2), 0.0);
+      cylinderPitchAngles_degrees.put(cylinderOrigins.get(2), 0.0);
+      cylinderRollAngles_degrees.put(cylinderOrigins.get(2), 0.0);
+
+      return new DRCGraspCylinderEnvironment(cylinderOrigins, cylinderYawAngles_degrees, cylinderPitchAngles_degrees, cylinderRollAngles_degrees);
    }
 
    @EstimatedDuration(duration = 50.0)
    @Test(timeout = 300000)
-   public void testGraspSteeringWheelSpinner() throws FileNotFoundException, SimulationExceededMaximumTimeException
+   public void testGraspXAxisCylinder() throws FileNotFoundException, SimulationExceededMaximumTimeException
    {
       BambooTools.reportTestStartedMessage();
 
@@ -111,30 +126,103 @@ public abstract class DRCGraspCylinderBehaviorTest implements MultiRobotTestInte
       final GraspCylinderBehavior graspBehavior = new GraspCylinderBehavior(drcBehaviorTestHelper.getBehaviorCommunicationBridge(),
             drcBehaviorTestHelper.getSDFFullRobotModel(), getRobotModel(), drcBehaviorTestHelper.getYoTime());
 
-      ReferenceFrame chestFrame = drcBehaviorTestHelper.getReferenceFrames().getChestFrame();
-      drcBehaviorTestHelper.updateRobotModel();
-
       RobotSide robotSideOfGraspingHand = RobotSide.LEFT;
       
       CommonAvatarEnvironmentInterface testEnvironment = drcBehaviorTestHelper.getTestEnviroment();
-      ContactableSteeringWheelRobot steeringWheel = (ContactableSteeringWheelRobot) testEnvironment.getEnvironmentRobots().get(0);
-      RigidBodyTransform wheelTransformToWorld = new RigidBodyTransform();
-      steeringWheel.getBodyTransformToWorld(wheelTransformToWorld);     
-      TransformReferenceFrame steeringWheelFrame = new TransformReferenceFrame("steeringWheel", ReferenceFrame.getWorldFrame(), wheelTransformToWorld);
+      ContactableStaticCylinderRobot cylinder = (ContactableStaticCylinderRobot) testEnvironment.getEnvironmentRobots().get(0);
+      RigidBodyTransform cylinderTransformToWorld = new RigidBodyTransform();
+      cylinder.getBodyTransformToWorld(cylinderTransformToWorld);  
+      TransformReferenceFrame cylinderFrame = new TransformReferenceFrame("cylinder", ReferenceFrame.getWorldFrame(), cylinderTransformToWorld);
          
       
-      FramePoint graspPoint = new FramePoint(steeringWheelFrame, 0.0, 0.0, steeringWheel.getValveRadius());
-      FrameVector cylinderLongAxis = new FrameVector(chestFrame, 0.0, 0.0, 1.0);
+      FramePoint graspPoint = new FramePoint(cylinderFrame, 0.0, 0.0, 0.0);
+      FrameVector cylinderLongAxis = new FrameVector(cylinderFrame, 0.0, 0.0, 1.0);
+      double palmOffsetFromWrist = 0.1;
 
       graspBehavior.initialize();
-      graspBehavior.setGraspPose(robotSideOfGraspingHand, graspPoint, cylinderLongAxis, false);
+      graspBehavior.setGraspPose(robotSideOfGraspingHand, graspPoint, cylinderLongAxis, palmOffsetFromWrist, false);
 
       assertTrue(graspBehavior.hasInputBeenSet());
 
       success = drcBehaviorTestHelper.executeBehaviorUntilDone(graspBehavior);
       assertTrue(success);
 
-      //TODO: Figure out how to test this
+      assertTrue(graspBehavior.isDone());
+
+      BambooTools.reportTestFinishedMessage();
+   }
+   
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testGraspYAxisCylinder() throws FileNotFoundException, SimulationExceededMaximumTimeException
+   {
+      BambooTools.reportTestStartedMessage();
+
+      boolean success = drcBehaviorTestHelper.simulateAndBlockAndCatchExceptions(1.0);
+      assertTrue(success);
+
+      final GraspCylinderBehavior graspBehavior = new GraspCylinderBehavior(drcBehaviorTestHelper.getBehaviorCommunicationBridge(),
+            drcBehaviorTestHelper.getSDFFullRobotModel(), getRobotModel(), drcBehaviorTestHelper.getYoTime());
+
+      RobotSide robotSideOfGraspingHand = RobotSide.LEFT;
+      
+      CommonAvatarEnvironmentInterface testEnvironment = drcBehaviorTestHelper.getTestEnviroment();
+      ContactableStaticCylinderRobot cylinder = (ContactableStaticCylinderRobot) testEnvironment.getEnvironmentRobots().get(1);
+      RigidBodyTransform cylinderTransformToWorld = new RigidBodyTransform();
+      cylinder.getBodyTransformToWorld(cylinderTransformToWorld);     
+      TransformReferenceFrame cylinderFrame = new TransformReferenceFrame("cylinder", ReferenceFrame.getWorldFrame(), cylinderTransformToWorld);
+         
+      
+      FramePoint graspPoint = new FramePoint(cylinderFrame, 0.0, 0.0, 0.0);
+      FrameVector cylinderLongAxis = new FrameVector(cylinderFrame, 0.0, 0.0, 1.0);
+      double palmOffsetFromWrist = 0.1;
+
+      graspBehavior.initialize();
+      graspBehavior.setGraspPose(robotSideOfGraspingHand, graspPoint, cylinderLongAxis, palmOffsetFromWrist, false);
+
+      assertTrue(graspBehavior.hasInputBeenSet());
+
+      success = drcBehaviorTestHelper.executeBehaviorUntilDone(graspBehavior);
+      assertTrue(success);
+
+      assertTrue(graspBehavior.isDone());
+
+      BambooTools.reportTestFinishedMessage();
+   }
+   
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testGraspZAxisCylinder() throws FileNotFoundException, SimulationExceededMaximumTimeException
+   {
+      BambooTools.reportTestStartedMessage();
+
+      boolean success = drcBehaviorTestHelper.simulateAndBlockAndCatchExceptions(1.0);
+      assertTrue(success);
+
+      final GraspCylinderBehavior graspBehavior = new GraspCylinderBehavior(drcBehaviorTestHelper.getBehaviorCommunicationBridge(),
+            drcBehaviorTestHelper.getSDFFullRobotModel(), getRobotModel(), drcBehaviorTestHelper.getYoTime());
+
+      RobotSide robotSideOfGraspingHand = RobotSide.LEFT;
+      
+      CommonAvatarEnvironmentInterface testEnvironment = drcBehaviorTestHelper.getTestEnviroment();
+      ContactableStaticCylinderRobot cylinder = (ContactableStaticCylinderRobot) testEnvironment.getEnvironmentRobots().get(2);
+      RigidBodyTransform cylinderTransformToWorld = new RigidBodyTransform();
+      cylinder.getBodyTransformToWorld(cylinderTransformToWorld);     
+      TransformReferenceFrame cylinderFrame = new TransformReferenceFrame("cylinder", ReferenceFrame.getWorldFrame(), cylinderTransformToWorld);
+         
+      
+      FramePoint graspPoint = new FramePoint(cylinderFrame, 0.0, 0.0, 0.0);
+      FrameVector cylinderLongAxis = new FrameVector(cylinderFrame, 0.0, 0.0, 1.0);
+      double palmOffsetFromWrist = 0.1;
+
+      graspBehavior.initialize();
+      graspBehavior.setGraspPose(robotSideOfGraspingHand, graspPoint, cylinderLongAxis, palmOffsetFromWrist, false);
+
+      assertTrue(graspBehavior.hasInputBeenSet());
+
+      success = drcBehaviorTestHelper.executeBehaviorUntilDone(graspBehavior);
+      assertTrue(success);
+
       assertTrue(graspBehavior.isDone());
 
       BambooTools.reportTestFinishedMessage();
