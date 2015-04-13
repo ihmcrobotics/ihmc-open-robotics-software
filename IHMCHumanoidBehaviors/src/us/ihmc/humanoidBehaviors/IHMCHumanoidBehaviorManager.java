104a3e66dda223d48f23e221ba1f1749de9e41f3
Tobias Meier <tmeier@ihmc.us>
2015-04-13 21:04:23 +0000

    None: Registered PushButtonBehavior in the BehaviorManager

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/IHMCHumanoidBehaviorManager.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/IHMCHumanoidBehaviorManager.java
@@ -23,6 +23,7 @@ import us.ihmc.humanoidBehaviors.behaviors.RemoveMultipleDebrisBehavior;
 import us.ihmc.humanoidBehaviors.behaviors.TurnValveBehavior;
 import us.ihmc.humanoidBehaviors.behaviors.WalkToGoalBehavior;
 import us.ihmc.humanoidBehaviors.behaviors.diagnostic.DiagnosticBehavior;
+import us.ihmc.humanoidBehaviors.behaviors.midLevel.PushButtonBehavior;
 import us.ihmc.humanoidBehaviors.behaviors.scripts.ScriptBehavior;
 import us.ihmc.humanoidBehaviors.behaviors.simpleBehaviors.SimpleDoNothingBehavior;
 import us.ihmc.humanoidBehaviors.communication.BehaviorCommunicationBridge;
@@ -202,6 +203,8 @@ public class IHMCHumanoidBehaviorManager
       dispatcher.addHumanoidBehavior(HumanoidBehaviorType.RECEIVE_IMAGE, new ReceiveImageBehavior(outgoingCommunicationBridge));
       dispatcher.addHumanoidBehavior(HumanoidBehaviorType.LOCALIZE_DRILL, new LocalizeDrillBehavior(outgoingCommunicationBridge, referenceFrames));
       
+      PushButtonBehavior pushButtonBehavior = new PushButtonBehavior(outgoingCommunicationBridge, referenceFrames, yoTime, wristSensors);
+      dispatcher.addHumanoidBehavior(HumanoidBehaviorType.PUSH_BUTTON, pushButtonBehavior);
       
       if(wholeBodyControllerParameters.createWholeBodyIkSolver() != null)
       {
