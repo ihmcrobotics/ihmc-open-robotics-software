139d5a38a03f54f081640919ab47068a8995acc6
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-04-14 23:38:31 +0000

    none: extracting yoReferencePose

--- IHMCStateEstimation/src/us/ihmc/stateEstimation/humanoid/kinematicsBasedStateEstimation/PelvisPoseHistoryCorrection.java
+++ IHMCStateEstimation/src/us/ihmc/stateEstimation/humanoid/kinematicsBasedStateEstimation/PelvisPoseHistoryCorrection.java
@@ -10,7 +10,6 @@ import us.ihmc.communication.subscribers.PelvisPoseCorrectionCommunicatorInterfa
 import us.ihmc.communication.subscribers.TimeStampedTransformBuffer;
 import us.ihmc.sensorProcessing.stateEstimation.evaluation.FullInverseDynamicsStructure;
 import us.ihmc.utilities.kinematics.TimeStampedTransform3D;
-import us.ihmc.utilities.kinematics.TransformInterpolationCalculator;
 import us.ihmc.utilities.math.MathTools;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.math.geometry.RigidBodyTransform;
@@ -22,9 +21,8 @@ import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
 import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
 import us.ihmc.yoUtilities.dataStructure.variable.LongYoVariable;
 import us.ihmc.yoUtilities.dataStructure.variable.YoVariable;
+import us.ihmc.yoUtilities.math.YoReferencePose;
 import us.ihmc.yoUtilities.math.filters.AlphaFilteredYoVariable;
-import us.ihmc.yoUtilities.math.frames.YoFramePoint;
-import us.ihmc.yoUtilities.math.frames.YoFramePose;
 
 /**
  * Here ICP stands for Iterative Closest Point.
@@ -473,94 +471,5 @@ public class PelvisPoseHistoryCorrection implements PelvisPoseHistoryCorrectionI
    public void setExternalPelvisCorrectorSubscriber(PelvisPoseCorrectionCommunicatorInterface externalPelvisPoseSubscriber)
    {
       this.pelvisPoseCorrectionCommunicator = externalPelvisPoseSubscriber;
-   }
-
-   // TODO Extract that guy.
-   private class YoReferencePose extends ReferenceFrame
-   {
-      private static final long serialVersionUID = -7908261385108357220L;
-
-      private final YoFramePose yoFramePose;
-
-      //Below are used for interpolation only
-      private final TransformInterpolationCalculator transformInterpolationCalculator = new TransformInterpolationCalculator();
-      private final RigidBodyTransform interpolationStartingPosition = new RigidBodyTransform();
-      private final RigidBodyTransform interpolationGoalPosition = new RigidBodyTransform();
-      private final RigidBodyTransform output = new RigidBodyTransform();
-
-      //Below are used for updating YoFramePose only
-      private final Quat4d rotation = new Quat4d();
-      private final Vector3d translation = new Vector3d();
-      private final double[] yawPitchRoll = new double[3];
-
-      public YoReferencePose(String frameName, ReferenceFrame parentFrame, YoVariableRegistry registry)
-      {
-         super(frameName, parentFrame);
-         yoFramePose = new YoFramePose(frameName + "_", this, registry);
-      }
-
-      @Override
-      protected void updateTransformToParent(RigidBodyTransform transformToParent)
-      {
-         yoFramePose.getOrientation().getQuaternion(rotation);
-         transformToParent.setRotation(rotation);
-         YoFramePoint yoFramePoint = yoFramePose.getPosition();
-         transformToParent.setTranslation(yoFramePoint.getX(), yoFramePoint.getY(), yoFramePoint.getZ());
-      }
-
-      public void setAndUpdate(RigidBodyTransform transform)
-      {
-         transform.get(rotation, translation);
-         setAndUpdate(rotation, translation);
-      }
-
-      public void setAndUpdate(Vector3d newTranslation)
-      {
-         set(newTranslation);
-         update();
-      }
-
-      public void setAndUpdate(Quat4d newRotation)
-      {
-         set(newRotation);
-         update();
-      }
-
-      public void setAndUpdate(Quat4d newRotation, Vector3d newTranslation)
-      {
-         set(newRotation);
-         set(newTranslation);
-         update();
-      }
-
-      private void set(Quat4d newRotation)
-      {
-         RotationFunctions.setYawPitchRollBasedOnQuaternion(yawPitchRoll, newRotation);
-         yoFramePose.setYawPitchRoll(yawPitchRoll);
-      }
-
-      private void set(Vector3d newTranslation)
-      {
-         yoFramePose.setXYZ(newTranslation.getX(), newTranslation.getY(), newTranslation.getZ());
-      }
-
-      public void interpolate(YoReferencePose start, YoReferencePose goal, double alpha)
-      {
-         start.getTransformToDesiredFrame(interpolationStartingPosition, parentFrame);
-         goal.getTransformToDesiredFrame(interpolationGoalPosition, parentFrame);
-
-         transformInterpolationCalculator.computeInterpolation(interpolationStartingPosition, interpolationGoalPosition, output, alpha);
-         setAndUpdate(output);
-      }
-
-      public void get(Quat4d rotation)
-      {
-         yoFramePose.getOrientation().getQuaternion(rotation);
-      }
-
-      public void get(Vector3d translation)
-      {
-         yoFramePose.getPosition().get(translation);
-      }
    }
 }
