d2d0f72688277c8142fbeb0682d45dd64dacfd88
Atlas Control Computer <AtlasControlComputer@unknown>
2015-04-14 00:19:35 +0000

    none: logger shell scripts, changed ips. wont fixBamboo

--- RobotDataCommunication/publishLogger.sh
+++ RobotDataCommunication/publishLogger.sh
@@ -3,4 +3,4 @@
 gradle distTar -DmainClass=us.ihmc.robotDataCommunication.logger.YoVariableLoggerDispatcher
 
 scp build/distributions/RobotDataCommunication-1.0.tar unknownid@10.66.171.46:
-ssh unknownid@10.66.171.46 "tar xvf RobotDataCommunication-1.0.tar"
+ssh unknownid@10.7.4.46 "tar xvf RobotDataCommunication-1.0.tar"
