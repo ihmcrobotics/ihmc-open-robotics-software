f6ba22a434841753438e9a444d3f21c309caae14
Georg Wiedebach <gwiedebach@ihmc.us>
2015-04-14 21:13:27 +0000

    NONE: updated ROS messages with enums. (won't fixbamboo)

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/AtlasElectricMotorEnablePacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/AtlasElectricMotorEnablePacketMessage.java
@@ -2,7 +2,13 @@ package ihmc_msgs;
 
 public interface AtlasElectricMotorEnablePacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/AtlasElectricMotorEnablePacketMessage";
-  static final java.lang.String _DEFINITION = "## AtlasElectricMotorEnablePacketMessage\n# Specifies a specific electric motor in the Atlas forearm to power on or off.\n\n# Options for motorEnableEnum\n# L_ARM_WRY = 0 - left upper wrist pitch\n# L_ARM_WRX = 1 - left wrist roll\n# L_ARM_WRY2 = 2 - left lower wrist pitch\n# R_ARM_WRY = 3 - right upper wrist pitch\n# R_ARM_WRX = 4 - right wrist roll\n# R_ARM_WRY2 = 5 - right lower wrist pitch\nuint8 motor_enable_enum\n\nbool enable\n\n\n";
+  static final java.lang.String _DEFINITION = "## AtlasElectricMotorEnablePacketMessage\n# Specifies a specific electric motor in the Atlas forearm to power on or off.\n\n# Options for motorEnableEnum\nuint8 L_ARM_WRY=0 # left upper wrist pitch\nuint8 L_ARM_WRX=1 # left wrist roll\nuint8 L_ARM_WRY2=2 # left lower wrist pitch\nuint8 R_ARM_WRY=3 # right upper wrist pitch\nuint8 R_ARM_WRX=4 # right wrist roll\nuint8 R_ARM_WRY2=5 # right lower wrist pitch\nuint8 motor_enable_enum\n\nbool enable\n\n\n";
+  static final byte L_ARM_WRY = 0;
+  static final byte L_ARM_WRX = 1;
+  static final byte L_ARM_WRY2 = 2;
+  static final byte R_ARM_WRY = 3;
+  static final byte R_ARM_WRX = 4;
+  static final byte R_ARM_WRY2 = 5;
   byte getMotorEnableEnum();
   void setMotorEnableEnum(byte value);
   boolean getEnable();
--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootPosePacketMessage.java
@@ -2,7 +2,9 @@ package ihmc_msgs;
 
 public interface FootPosePacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootPosePacketMessage";
-  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\n# This message commands the controller to move the desired foot to a given location\n# and orientation in world coordinates. This message is not intended to specify footsteps\n# as the controller will use a straight-line trajectory when moving the foot from its\n# current to desired location/orientation\n\n# Options for robotSide\n# LEFT = 0 - refers to the LEFT side of a robot\n# RIGHT = 1 - refers to the RIGHT side of a robot\nuint8 robot_side\n\ngeometry_msgs/Vector3 position\n\ngeometry_msgs/Quaternion orientation\n\n# trajectoryTime specifies how fast or how slow to move to the desired pose\nfloat64 trajectory_time\n\n\n";
+  static final java.lang.String _DEFINITION = "## FootPosePacketMessage\n# This message commands the controller to move the desired foot to a given location\n# and orientation in world coordinates. This message is not intended to specify footsteps\n# as the controller will use a straight-line trajectory when moving the foot from its\n# current to desired location/orientation\n\n# Options for robotSide\nuint8 LEFT=0 # refers to the LEFT side of a robot\nuint8 RIGHT=1 # refers to the RIGHT side of a robot\nuint8 robot_side\n\ngeometry_msgs/Vector3 position\n\ngeometry_msgs/Quaternion orientation\n\n# trajectoryTime specifies how fast or how slow to move to the desired pose\nfloat64 trajectory_time\n\n\n";
+  static final byte LEFT = 0;
+  static final byte RIGHT = 1;
   byte getRobotSide();
   void setRobotSide(byte value);
   geometry_msgs.Vector3 getPosition();
