a818f5eb325118928951720cbcf1575bc7364e11
Sylvain Bertrand <sbertrand@ihmc.us>
2015-04-14 21:54:27 +0000

    NONE: Limited the spline duration in single support when using two CMPs per suppport. Added some JavaDoc in CapturePointPlannerParameters. Won't fixbamboo.

--- Acsell/src/us/ihmc/acsell/controlParameters/BonoCapturePointPlannerParameters.java
+++ Acsell/src/us/ihmc/acsell/controlParameters/BonoCapturePointPlannerParameters.java
@@ -2,6 +2,7 @@ package us.ihmc.acsell.controlParameters;
 
 import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerParameters;
 
+/** {@inheritDoc} */
 public class BonoCapturePointPlannerParameters implements CapturePointPlannerParameters
 {
 
@@ -12,171 +13,206 @@ public class BonoCapturePointPlannerParameters implements CapturePointPlannerPar
       this.runningOnRealRobot = runningOnRealRobot;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getDoubleSupportInitialTransferDuration()
    {
       return 1.0;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getDoubleSupportDuration()
    {
       return runningOnRealRobot ? 0.5 : 0.25;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getAdditionalTimeForSingleSupport()
    {
       return 0.0;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getSingleSupportDuration()
    {
       return runningOnRealRobot ? 1.0 : 0.7;
    }
 
+   /** {@inheritDoc} */
    @Override
    public int getNumberOfFootstepsToConsider()
    {
       return 3;
    }
 
+   /** {@inheritDoc} */
    @Override
    public int getNumberOfCoefficientsForDoubleSupportPolynomialTrajectory()
    {
       return 5;
    }
 
+   /** {@inheritDoc} */
    @Override
    public int getNumberOfFootstepsToStop()
    {
       return 2;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getIsDoneTimeThreshold()
    {
       return -1e-4;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getDoubleSupportSplitFraction()
    {
       return 0.5;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getFreezeTimeFactor()
    {
       return 0.9;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getMaxInstantaneousCapturePointErrorForStartingSwing()
    {
       return 0.02;
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean getDoTimeFreezing()
    {
       return false;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getMaxAllowedErrorWithoutPartialTimeFreeze()
    {
       return 0.03;
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean getDoFootSlipCompensation()
    {
       return true;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getAlphaDeltaFootPositionForFootslipCompensation()
    {
       return 0.65;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getEntryCMPInsideOffset()
    {
       return 0.005;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getExitCMPInsideOffset()
    {
       return 0.005;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getEntryCMPForwardOffset()
    {
       return 0.015;//0.00;//0.02-.025/2;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getExitCMPForwardOffset()
    {
       return 0.015;//0.00;//0.02-.025/2;
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean useTerribleHackToReduceICPVelocityAtTheEndOfTransfer()
    {
       return true;
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean useNewICPPlanner()
    {
       return false;
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean useTwoCMPsPerSupport()
    {
       return false;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getTimeSpentOnExitCMPInPercentOfStepTime()
    {
       return 0.50;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getMaxEntryCMPForwardOffset()
    {
       return 0.05;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getMaxExitCMPForwardOffset()
    {
       return 0.05;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getMinExitCMPForwardOffset()
    {
       return -0.02;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getMinEntryCMPForwardOffset()
    {
       return -0.02;
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getCMPSafeDistanceAwayFromSupportEdges()
    {
       return 0.03;
+   }
+
+   /** {@inheritDoc} */
+   @Override
+   public double getMaxDurationForSmoothingEntryToExitCMPSwitch()
+   {
+      return 0.5;
    }
 }
