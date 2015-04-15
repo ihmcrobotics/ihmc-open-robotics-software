2ac6fe4d4b173115c89ce2cd2d617b5090addd6e
Duncan Calvert <dcalvert@ihmc.us>
2015-04-15 18:21:56 +0000

    NONE: Updated DRCSimGazeboLauncher.

--- Atlas/src/us/ihmc/atlas/drcsimGazebo/DRCSimGazeboLauncher.java
+++ Atlas/src/us/ihmc/atlas/drcsimGazebo/DRCSimGazeboLauncher.java
@@ -5,6 +5,8 @@ import us.ihmc.utilities.ThreadTools;
 public class DRCSimGazeboLauncher
 {
    public static final boolean RUN_CATKIN_MAKE = true;
+   public enum InitialPose {STANDING, CAR_FRONT, CAR_LATER};
+   public static final InitialPose INITIAL_POSE = InitialPose.CAR_FRONT;
    
    public static void main(String[] args)
    {
@@ -33,7 +35,7 @@ public class DRCSimGazeboLauncher
             "source /opt/ros/indigo/setup.bash;" +
             "source /usr/share/drcsim/setup.sh;" +
             "source $PWD/ihmc_gazebo_catkin_ws/devel/setup.bash;" +
-            "roslaunch ihmc_gazebo ihmc_atlas_standing.launch";
+            "roslaunch ihmc_gazebo ihmc_atlas_" + INITIAL_POSE.name().toLowerCase() + ".launch";
       ThreadTools.runCommandLine(commandline2);
    }
 }
