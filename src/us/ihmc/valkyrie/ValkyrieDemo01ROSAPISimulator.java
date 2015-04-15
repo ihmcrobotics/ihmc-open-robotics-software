52b124ebf85210244f3f50a6fdba3fca90d150ba
Georg Wiedebach <gwiedebach@ihmc.us>
2015-04-15 21:23:06 +0000

    NONE: move ROSAPISimulator to DRC package and make ValkyrieROSAPISimulator use the ROSAPISimulator class. (won't fixbamboo)

---
+++ src/us/ihmc/valkyrie/ValkyrieDemo01ROSAPISimulator.java
@@ -0,0 +1,51 @@
+package us.ihmc.valkyrie;
+
+import java.io.IOException;
+
+import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseStartingLocation;
+import us.ihmc.darpaRoboticsChallenge.DRCStartingLocation;
+import us.ihmc.darpaRoboticsChallenge.ROSAPISimulator;
+import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.darpaRoboticsChallenge.environment.CommonAvatarEnvironmentInterface;
+import us.ihmc.darpaRoboticsChallenge.environment.DRCDemo01NavigationEnvironment;
+
+import com.martiansoftware.jsap.JSAPException;
+
+public class ValkyrieDemo01ROSAPISimulator extends ROSAPISimulator
+{
+   private static final String ROBOT_NAME = "valkyrie";
+   
+   public ValkyrieDemo01ROSAPISimulator(DRCRobotModel robotModel, DRCStartingLocation startingLocation, String nameSpace, String tfPrefix,
+         boolean runAutomaticDiagnosticRoutine, boolean disableViz) throws IOException
+   {
+      super(robotModel, startingLocation, nameSpace, tfPrefix, runAutomaticDiagnosticRoutine, disableViz);
+   }
+
+   @Override
+   protected CommonAvatarEnvironmentInterface createEnvironment()
+   {
+      return new DRCDemo01NavigationEnvironment();
+   }
+   
+   public static void main(String[] args) throws JSAPException, IOException
+   {
+      Options opt = parseArguments(args);
+      
+      DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
+      
+      DRCStartingLocation startingLocation;
+      try
+      {
+         startingLocation = DRCObstacleCourseStartingLocation.valueOf(opt.startingLocation);
+      }
+      catch (IllegalArgumentException e)
+      {
+         System.err.println("Incorrect starting location " + opt.startingLocation);
+         System.out.println("Starting locations: " + DRCObstacleCourseStartingLocation.optionsToString());
+         return;
+      }
+      
+      String nameSpace = opt.nameSpace + "/" + ROBOT_NAME;
+      new ValkyrieDemo01ROSAPISimulator(robotModel, startingLocation, nameSpace, opt.tfPrefix, opt.runAutomaticDiagnosticRoutine, opt.disableViz);
+   }
+}
