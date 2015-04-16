f3df7c75126dc6a41ca87c1453b0e763b2d5981e
Alex Graber-Tilton <agrabertilton@ihmc.us>
2015-04-16 16:24:39 +0000

    NONE: fix issue with stairs starting location, was starting with hand inside railing. won't fixBamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCSCStartingLocations.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/DRCSCStartingLocations.java
@@ -28,8 +28,8 @@ public enum DRCSCStartingLocations implements DRCStartingLocation
       addMapping(DRCSCStartingLocations.PLUG, new OffsetAndYawRobotInitialSetup(0,-8.5, 0,Math.toRadians(-180.0)));
       addMapping(DRCSCStartingLocations.WALKING_START, new OffsetAndYawRobotInitialSetup(0,-8.5, 0,Math.toRadians(-90.0)));
       addMapping(DRCSCStartingLocations.WALKING_END, new OffsetAndYawRobotInitialSetup(0.35,-13.5, 0,Math.toRadians(-90.0)));
-      addMapping(DRCSCStartingLocations.STAIRS_START, new OffsetAndYawRobotInitialSetup(0.85,-13.5, 0,Math.toRadians(-90.0)));
-      addMapping(DRCSCStartingLocations.STAIRS_END, new OffsetAndYawRobotInitialSetup(0.85,-16, 0.9144,Math.toRadians(-90.0)));
+      addMapping(DRCSCStartingLocations.STAIRS_START, new OffsetAndYawRobotInitialSetup(1.0,-13.5, 0,Math.toRadians(-90.0)));
+      addMapping(DRCSCStartingLocations.STAIRS_END, new OffsetAndYawRobotInitialSetup(1.0,-16, 0.9144,Math.toRadians(-90.0)));
    };
 
    private static void addMapping(DRCSCStartingLocations drcDemo01StartingLocation, OffsetAndYawRobotInitialSetup robotInitialSetup)
