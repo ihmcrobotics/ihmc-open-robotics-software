0bed15c6c027979c596731693e18b260c9435757
Chris Schmidt Wetekam <cschmidt@ihmc.us>
2015-04-16 19:21:00 +0000

    DRC-2586: Avoid numerical instability when simulating joint position control.  won't fixBamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/controllers/JointLowLevelPositionControlSimulator.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/controllers/JointLowLevelPositionControlSimulator.java
@@ -33,7 +33,7 @@ public class JointLowLevelPositionControlSimulator implements RobotController
          jointController.setProportionalGain(50.0 * subtreeMass);
          jointController.setIntegralGain(35.0 * subtreeMass);
          jointController.setMaxIntegralError(0.3);
-         jointController.setDerivativeGain(7.0 * subtreeMass);
+         jointController.setDerivativeGain(3.5 * subtreeMass);
       }
       else
       {
