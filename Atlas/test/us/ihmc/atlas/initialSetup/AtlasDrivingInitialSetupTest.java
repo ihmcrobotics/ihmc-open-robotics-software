096cdcaa01ab4243bb3f4f0ad5cfd46d94cc9f33
Georg Wiedebach <gwiedebach@ihmc.us>
2015-04-17 14:01:59 +0000

    DRC-2514: clean up starting conditions and write junit test for loading file with starting joint angles (won't fixbamboo)

---
+++ Atlas/test/us/ihmc/atlas/initialSetup/AtlasDrivingInitialSetupTest.java
@@ -0,0 +1,107 @@
+package us.ihmc.atlas.initialSetup;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import us.ihmc.SdfLoader.SDFRobot;
+import us.ihmc.atlas.AtlasJointMap;
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.utilities.code.agileTesting.BambooAnnotations.EstimatedDuration;
+import us.ihmc.utilities.humanoidRobot.partNames.ArmJointName;
+import us.ihmc.utilities.humanoidRobot.partNames.LegJointName;
+import us.ihmc.utilities.humanoidRobot.partNames.SpineJointName;
+import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.wholeBodyController.DRCRobotJointMap;
+
+public class AtlasDrivingInitialSetupTest
+{
+   private static final AtlasRobotVersion version = AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ;
+   
+   @EstimatedDuration(duration = 1.0)
+   @Test(timeout = 3000)
+   public void testLoadFile()
+   {
+      AtlasRobotModel robotModel = new AtlasRobotModel(version, AtlasRobotModel.AtlasTarget.SIM, false);
+      SDFRobot robot = robotModel.createSdfRobot(false);
+      DRCRobotJointMap jointMap = new AtlasJointMap(version);
+      
+      AtlasDrivingInitialSetup initialSetup = new AtlasDrivingInitialSetup();
+      initialSetup.initializeRobot(robot, jointMap);
+   }
+   
+   @EstimatedDuration(duration = 1.0)
+   @Test(timeout = 3000)
+   public void testFileContainsAllJoints()
+   {
+      boolean containsAllJoints = true;
+      
+      DRCRobotJointMap jointMap = new AtlasJointMap(version);
+      File file = new File(AtlasDrivingInitialSetup.INITIAL_CONDITIONS_FILE);
+      try
+      {
+         Properties properties = new Properties();
+         FileInputStream stream = new FileInputStream(file);
+         properties.load(stream);
+         
+         for (RobotSide robotSide : RobotSide.values())
+         {
+            for (LegJointName jointName : LegJointName.values())
+            {
+               String key = jointMap.getLegJointName(robotSide, jointName);
+               if (key == null)
+               {
+                  continue;
+               }
+               if (!properties.containsKey(key))
+               {
+                  System.out.println(file.getAbsolutePath() + " did not contain joint " + key);
+                  containsAllJoints = false;
+               }
+            }
+            
+            for (ArmJointName jointName : ArmJointName.values())
+            {
+               String key = jointMap.getArmJointName(robotSide, jointName);
+               if (key == null)
+               {
+                  continue;
+               }
+               if (!properties.containsKey(key))
+               {
+                  System.out.println(file.getAbsolutePath() + " did not contain joint " + key);
+                  containsAllJoints = false;
+               }
+            }
+         }
+         
+         for (SpineJointName jointName : SpineJointName.values())
+         {
+            String key = jointMap.getSpineJointName(jointName);
+            if (key == null)
+            {
+               continue;
+            }
+            if (!properties.containsKey(key))
+            {
+               System.out.println(file.getAbsolutePath() + " did not contain joint " + key);
+               containsAllJoints = false;
+            }
+         }
+         
+         stream.close();
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException("Atlas joint parameter file " + file.getAbsolutePath() + " cannot be loaded. ", e);
+      }
+      
+      assertTrue(containsAllJoints);
+   }
+}
