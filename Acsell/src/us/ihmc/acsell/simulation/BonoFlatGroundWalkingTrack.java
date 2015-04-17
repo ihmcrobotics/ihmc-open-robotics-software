ae67b0a09b96a4b0fa8e419a20358e9ce19fa174
Steve Spencer <sjspenc@sandia.gov>
2015-04-17 22:04:22 +0000

    NONE: Steppr Treadmill control added. wont fixbamboo

--- Acsell/src/us/ihmc/acsell/simulation/BonoFlatGroundWalkingTrack.java
+++ Acsell/src/us/ihmc/acsell/simulation/BonoFlatGroundWalkingTrack.java
@@ -1,7 +1,9 @@
 package us.ihmc.acsell.simulation;
 
+import net.java.games.input.Component;
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.acsell.parameters.BonoRobotModel;
+import us.ihmc.acsell.treadmill.TreadmillJoystickEventListener;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.WalkingProvider;
 import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
 import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
@@ -9,9 +11,17 @@ import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.initialSetup.DRCRobotInitialSetup;
 import us.ihmc.graphics3DAdapter.GroundProfile3D;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.joystick.BooleanYoVariableJoystickEventListener;
+import us.ihmc.simulationconstructionset.joystick.DoubleYoVariableJoystickEventListener;
+import us.ihmc.simulationconstructionset.joystick.JoyStickNotFoundException;
 import us.ihmc.simulationconstructionset.joystick.JoystickUpdater;
 import us.ihmc.simulationconstructionset.util.ground.FlatGroundProfile;
 import us.ihmc.steppr.hardware.visualization.StepprStandPrepSliderboard;
+import us.ihmc.yoUtilities.dataStructure.YoVariableHolder;
+import us.ihmc.yoUtilities.dataStructure.listener.VariableChangedListener;
+import us.ihmc.yoUtilities.dataStructure.variable.BooleanYoVariable;
+import us.ihmc.yoUtilities.dataStructure.variable.DoubleYoVariable;
+import us.ihmc.yoUtilities.dataStructure.variable.YoVariable;
 
 public class BonoFlatGroundWalkingTrack
 {
@@ -43,11 +53,70 @@ public class BonoFlatGroundWalkingTrack
       SimulationConstructionSet scs = flatGroundWalkingTrack.getSimulationConstructionSet();
       if (USE_JOYSTICK_CONTROLLER)
       {
-         StepprStandPrepSliderboard.setupJoyStick(scs);
+         setupJoyStick(scs);
          flatGroundWalkingTrack.getDrcSimulation().start();
          flatGroundWalkingTrack.getDrcSimulation().simulate();
       }
    }
 
 
+   public static void setupJoyStick(YoVariableHolder registry)
+   {
+	  
+	  final JoystickUpdater joystickUpdater;
+	  try
+	  {
+		   joystickUpdater = new JoystickUpdater();
+	  }
+      catch (JoyStickNotFoundException ex)
+      {
+    		  System.err.println("Joystick not found. Proceeding without joystick");
+    		  return;
+      }
+      Thread thread = new Thread(joystickUpdater);
+      thread.start();
+
+      
+      double deadZone = 0.02;
+      double desiredVelocityX_Bias = 0.0;
+      double desiredVelocityY_Bias = 0.0;
+      double desiredHeadingDot_Bias = 0.0;
+      final double maxDesiredVelocityX = 0.25;
+      final double minVelocityX = -0.10;
+      
+      
+      DoubleYoVariable desiredVelocityX = (DoubleYoVariable) registry.getVariable("ManualDesiredVelocityControlModule", "desiredVelocityX");
+      if(desiredVelocityX==null || joystickUpdater==null)
+         return;
+
+      desiredVelocityX.set(desiredVelocityX_Bias);
+      joystickUpdater.addListener(new DoubleYoVariableJoystickEventListener(desiredVelocityX, joystickUpdater.findComponent(Component.Identifier.Axis.Y),
+    		  -maxDesiredVelocityX+desiredVelocityX_Bias, maxDesiredVelocityX+desiredVelocityX_Bias, deadZone, true));
+      desiredVelocityX.addVariableChangedListener(new VariableChangedListener()
+      {
+         @Override
+         public void variableChanged(YoVariable<?> v)
+         {
+         	if (v.getValueAsDouble() < minVelocityX)
+            	 v.setValueFromDouble(minVelocityX, false);
+         }
+      });
+      
+      DoubleYoVariable desiredVelocityY = (DoubleYoVariable) registry.getVariable("ManualDesiredVelocityControlModule", "desiredVelocityY");
+      desiredVelocityY.set(desiredVelocityY_Bias);
+      joystickUpdater.addListener(new DoubleYoVariableJoystickEventListener(desiredVelocityY, joystickUpdater.findComponent(Component.Identifier.Axis.X),
+    		  -0.1+desiredVelocityY_Bias, 0.1+desiredVelocityY_Bias, deadZone, true));
+
+      DoubleYoVariable desiredHeadingDot = (DoubleYoVariable) registry.getVariable("RateBasedDesiredHeadingControlModule", "desiredHeadingDot");
+      desiredHeadingDot.set(desiredHeadingDot_Bias);
+      joystickUpdater.addListener(new DoubleYoVariableJoystickEventListener(desiredHeadingDot, joystickUpdater.findComponent(Component.Identifier.Axis.RZ),
+    		  -0.1+desiredHeadingDot_Bias, 0.1+desiredHeadingDot_Bias, deadZone/2.0, true));
+      
+      joystickUpdater.listComponents();
+      
+      BooleanYoVariable walk = (BooleanYoVariable) registry.getVariable("DesiredFootstepCalculatorFootstepProviderWrapper","walk");
+      joystickUpdater.addListener(new BooleanYoVariableJoystickEventListener(walk, joystickUpdater.findComponent(Component.Identifier.Button.TRIGGER), true));
+      
+   }
+   
 }
