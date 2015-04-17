a9f36f6c0ceed0803d841d865c0089ed15a76183
Jerry Pratt <jpratt@ihmc.us>
2015-04-17 18:55:11 +0000

    NONE: Bamboo babysitting. Fixed test suites. Will help fixBamboo.

--- Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasTInDevelopmentTestSuite.java
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasTInDevelopmentTestSuite.java
@@ -7,10 +7,10 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseFlatTest.class
+   us.ihmc.atlas.stateEstimation.AtlasPelvisPoseHistoryCorrectorTest.class
 })
 
-public class AtlasQInDevelopmentTestSuite
+public class AtlasTInDevelopmentTestSuite
 {
    public static void main(String[] args)
    {
