20be8e86e68bd09299834a9c91675da717d4510a
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-04-20 22:02:16 +0000

    none: tried to add ibeo data to the ui for valkyrie. wont fixBamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networkProcessor/depthData/RosPointCloudReceiver.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networkProcessor/depthData/RosPointCloudReceiver.java
@@ -7,26 +7,37 @@ import javax.vecmath.Point3d;
 
 import sensor_msgs.PointCloud2;
 import us.ihmc.sensorProcessing.parameters.DRCRobotLidarParameters;
+import us.ihmc.sensorProcessing.parameters.DRCRobotPointCloudParameters;
 import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.utilities.ros.subscriber.RosPointCloudSubscriber;
 
 public class RosPointCloudReceiver extends RosPointCloudSubscriber
 {
-   private final ReferenceFrame scanFrame;
+   private final ReferenceFrame cloudFrame;
    private final PointCloudDataReceiver pointCloudDataReceiver;
-   private final ReferenceFrame lidarFrame;
+   private final ReferenceFrame sensorframe;
 
    public RosPointCloudReceiver(DRCRobotLidarParameters lidarParameters, RosMainNode rosMainNode, ReferenceFrame scanFrame,
          PointCloudDataReceiver pointCloudDataReceiver)
    {
-      this.scanFrame = scanFrame;
+      this.cloudFrame = scanFrame;
       this.pointCloudDataReceiver = pointCloudDataReceiver;
-      this.lidarFrame = pointCloudDataReceiver.getLidarFrame(lidarParameters.getSensorNameInSdf());
+      this.sensorframe = pointCloudDataReceiver.getLidarFrame(lidarParameters.getSensorNameInSdf());
 
 
       rosMainNode.attachSubscriber(lidarParameters.getRosTopic(), this);
 
+   }
+   
+   public RosPointCloudReceiver(DRCRobotPointCloudParameters pointCloudParameters, RosMainNode rosMainNode, ReferenceFrame cloudFrame,
+         PointCloudDataReceiver pointCloudDataReceiver)
+   {
+      this.cloudFrame = cloudFrame;
+      this.pointCloudDataReceiver = pointCloudDataReceiver;
+      this.sensorframe = pointCloudDataReceiver.getLidarFrame(pointCloudParameters.getSensorNameInSdf());
+
+      rosMainNode.attachSubscriber(pointCloudParameters.getRosTopic(), this);
    }
 
 
@@ -39,6 +50,6 @@ public class RosPointCloudReceiver extends RosPointCloudSubscriber
 	   long[] timestamps = new long[points.length];
 	   long time = pointCloud.getHeader().getStamp().totalNsecs();
 	   Arrays.fill(timestamps, time);
-		RosPointCloudReceiver.this.pointCloudDataReceiver.receivedPointCloudData(RosPointCloudReceiver.this.scanFrame, lidarFrame, timestamps, pointsAsArrayList);
+		RosPointCloudReceiver.this.pointCloudDataReceiver.receivedPointCloudData(RosPointCloudReceiver.this.cloudFrame, sensorframe, timestamps, pointsAsArrayList);
 	}
 }
