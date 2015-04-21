eff0a8d760d1020e160a70df247c1a9da26e1123
Daniel Duran <dduran@ihmc.us>
2015-04-21 15:18:28 +0000

    None: fixed model loading (removed url and used string) Will not #FixBamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/PolarisRobot.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/PolarisRobot.java
@@ -1,11 +1,5 @@
 package us.ihmc.darpaRoboticsChallenge;
 
-import java.net.URL;
-
-import javax.vecmath.Matrix3d;
-import javax.vecmath.Vector3d;
-
-import us.ihmc.darpaRoboticsChallenge.environment.DRCCarEgressEnvironment;
 import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
 import us.ihmc.simulationconstructionset.FloatingJoint;
 import us.ihmc.simulationconstructionset.Link;
@@ -14,12 +8,15 @@ import us.ihmc.utilities.Axis;
 import us.ihmc.utilities.math.RotationalInertiaCalculator;
 import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Vector3d;
+
 /**
  * This is a non-contactable Polaris model only for visualizing in SCS
  */
 
 public class PolarisRobot extends Robot
-{   
+{
    private final String modelFile = "models/polarisModel.obj";
    private final FloatingJoint floatingJoint;
    private final Link link;
@@ -33,12 +30,12 @@ public class PolarisRobot extends Robot
       linkGraphics = new Graphics3DObject();
       linkGraphics.addModelFile(modelFile);
       link.setLinkGraphics(linkGraphics);
-      
+
       link.setMass(1.0);
       link.setComOffset(new Vector3d());
       Matrix3d inertia = RotationalInertiaCalculator.getRotationalInertiaMatrixOfSolidCylinder(1.0, 1.0, 1.0, Axis.Z);
       link.setMomentOfInertia(inertia);
-      
+
       floatingJoint = new FloatingJoint(name + "Base", name, new Vector3d(), this);
       floatingJoint.setRotationAndTranslation(rootJointTransform);
       floatingJoint.setLink(link);
