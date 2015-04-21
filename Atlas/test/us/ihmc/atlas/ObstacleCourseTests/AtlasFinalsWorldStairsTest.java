282a5b857985344c2ed758fb0bf6838110c9d7e2
Alex Graber-Tilton <agrabertilton@ihmc.us>
2015-04-21 18:08:12 +0000

    NONE:  foot not far enough on step. fixBamboo.

--- Atlas/test/us/ihmc/atlas/ObstacleCourseTests/AtlasFinalsWorldStairsTest.java
+++ Atlas/test/us/ihmc/atlas/ObstacleCourseTests/AtlasFinalsWorldStairsTest.java
@@ -224,7 +224,7 @@ public class AtlasFinalsWorldStairsTest
       footstepDataList.add(new FootstepData(RobotSide.RIGHT, new Point3d(startingLocation.x - 0.15, startingLocation.y - 1.18, startingLocation.z + 0.30),
               new Quat4d(orientation), rightFootPoint2ds));
 
-      footstepDataList.add(new FootstepData(RobotSide.LEFT, new Point3d(startingLocation.x + 0.15, startingLocation.y - 1.35, startingLocation.z + 0.55),
+      footstepDataList.add(new FootstepData(RobotSide.LEFT, new Point3d(startingLocation.x + 0.15, startingLocation.y - 1.36, startingLocation.z + 0.55),
               new Quat4d(orientation), leftFootPoint2ds));
 
       footstepDataList.add(new FootstepData(RobotSide.RIGHT, new Point3d(startingLocation.x - 0.15, startingLocation.y - 1.45, startingLocation.z + 0.55),
