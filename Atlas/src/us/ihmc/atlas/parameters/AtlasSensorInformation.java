035dc98dabd2d4388f38f2d95ba5eb4f0b25d687
Atlas Control Computer <AtlasControlComputer@unknown>
2015-04-24 00:43:26 +0000

    none: fix if else error. wont fixBamboo pair:twu

--- Atlas/src/us/ihmc/atlas/parameters/AtlasSensorInformation.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasSensorInformation.java
@@ -112,7 +112,7 @@ public class AtlasSensorInformation implements DRCRobotSensorInformation
          lidarParamaters[MULTISENSE_LIDAR_ID] = new DRCRobotLidarParameters(true, lidarSensorName, multisense_filtered_laser_as_point_cloud_topic_string, lidarJointName, lidarJointTopic, multisenseHandoffFrame, lidarBaseFrame, lidarEndFrame, lidar_spindle_velocity, MULTISENSE_LIDAR_ID);
          pointCloudParamaters[MULTISENSE_STEREO_ID] = new DRCRobotPointCloudParameters(stereoSensorName, stereoColorTopic, multisenseHandoffFrame, stereoBaseFrame, stereoEndFrame, MULTISENSE_STEREO_ID);
       }
-      if(target == AtlasTarget.HEAD_ON_A_STICK)
+      else if(target == AtlasTarget.HEAD_ON_A_STICK)
       {
          cameraParamaters[MULTISENSE_SL_LEFT_CAMERA_ID] = new DRCRobotCameraParameters(left_camera_name, left_camera_topic, left_info_camera_topic, multisenseHandoffFrame, baseTfName, left_frame_name, MULTISENSE_SL_LEFT_CAMERA_ID);
          cameraParamaters[MULTISENSE_SL_RIGHT_CAMERA_ID] = new DRCRobotCameraParameters(right_camera_name, right_camera_topic, right_info_camera_topic, multisenseHandoffFrame, baseTfName, right_frame_name, MULTISENSE_SL_RIGHT_CAMERA_ID);
