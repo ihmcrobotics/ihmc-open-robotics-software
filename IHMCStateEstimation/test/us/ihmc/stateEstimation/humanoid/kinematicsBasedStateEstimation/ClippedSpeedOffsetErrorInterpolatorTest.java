1926cfeba6bf4d40a52ff7b4379812fcd7a0493f
Sylvain Bertrand <sbertrand@ihmc.us>
2015-04-27 22:18:28 +0000

    NONE: Pair: Mm'Gros! Organized import. Won't fixbamboo

--- IHMCStateEstimation/test/us/ihmc/stateEstimation/humanoid/kinematicsBasedStateEstimation/ClippedSpeedOffsetErrorInterpolatorTest.java
+++ IHMCStateEstimation/test/us/ihmc/stateEstimation/humanoid/kinematicsBasedStateEstimation/ClippedSpeedOffsetErrorInterpolatorTest.java
@@ -13,7 +13,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import clover.org.jfree.util.Rotation;
 import us.ihmc.communication.subscribers.TimeStampedTransformBuffer;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
