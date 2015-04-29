e207dca3a159dad3463e74e0ec1b2d6de3b880a4
Nathan Mertins <nmertins@ihmc.us>
2015-04-29 03:46:22 +0000

    NONE: refactors in new Robotiq comms stack; won't fixbamboo; pair: an actual pear

--- RobotiqHandDrivers/src/us/ihmc/robotiq/communication/RobotiqWriteRequestFactory.java
+++ RobotiqHandDrivers/src/us/ihmc/robotiq/communication/RobotiqWriteRequestFactory.java
@@ -2,12 +2,18 @@ package us.ihmc.robotiq.communication;
 
 import net.wimpi.modbus.msg.WriteMultipleRegistersRequest;
 import net.wimpi.modbus.procimg.SimpleRegister;
+import us.ihmc.robotiq.communication.registers.ActionRequestRegister;
+import us.ihmc.robotiq.communication.registers.GripperOptionRegister;
+import us.ihmc.robotiq.communication.registers.RobotiqRegister;
 
 public class RobotiqWriteRequestFactory
 {
    private WriteMultipleRegistersRequest request;
    private SimpleRegister[] registers = new SimpleRegister[8];
-   private byte[] robotiqRegisters = new byte[16];
+   private RobotiqRegister actionRequestRegister, gripperOptionRegister, fingerAPostionRequestRegister, fingerASpeedRegister,
+                           fingerAForceRegister, fingerBPositionRequestRegister, fingerBSpeedRegister, fingerBForceRegister,
+                           fingerCPositionRequestRegister, fingerCSpeedRegister, fingerCForceRegister, scissorPositionRequestRegister,
+                           scissorSpeedRegister, scissorForceRegister;
    
    public RobotiqWriteRequestFactory()
    {
@@ -20,6 +26,12 @@ public class RobotiqWriteRequestFactory
       {
          registers[i] = new SimpleRegister(0);
       }
+   }
+   
+   private void initialize()
+   {
+      actionRequestRegister = new ActionRequestRegister();
+      gripperOptionRegister = new GripperOptionRegister();
    }
    
    public WriteMultipleRegistersRequest createActivationRequest()
