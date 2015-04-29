6398fd7e9033ef618ce258e5e9f1a762546456b0
Nathan Mertins <nmertins@ihmc.us>
2015-04-29 18:48:21 +0000

    DRC-2644: created class that will represent an outgoing Robotiq command; won't fixbamboo; pair: myself because nothing is using this yet

--- RobotiqHandDrivers/src/us/ihmc/robotiq/communication/RobotiqWriteRequestFactory.java
+++ RobotiqHandDrivers/src/us/ihmc/robotiq/communication/RobotiqWriteRequestFactory.java
@@ -2,18 +2,11 @@ package us.ihmc.robotiq.communication;
 
 import net.wimpi.modbus.msg.WriteMultipleRegistersRequest;
 import net.wimpi.modbus.procimg.SimpleRegister;
-import us.ihmc.robotiq.communication.registers.ActionRequestRegister;
-import us.ihmc.robotiq.communication.registers.GripperOptionRegister;
-import us.ihmc.robotiq.communication.registers.RobotiqRegister;
 
 public class RobotiqWriteRequestFactory
 {
    private WriteMultipleRegistersRequest request;
    private SimpleRegister[] registers = new SimpleRegister[8];
-   private RobotiqRegister actionRequestRegister, gripperOptionRegister, fingerAPostionRequestRegister, fingerASpeedRegister,
-                           fingerAForceRegister, fingerBPositionRequestRegister, fingerBSpeedRegister, fingerBForceRegister,
-                           fingerCPositionRequestRegister, fingerCSpeedRegister, fingerCForceRegister, scissorPositionRequestRegister,
-                           scissorSpeedRegister, scissorForceRegister;
    
    public RobotiqWriteRequestFactory()
    {
@@ -26,12 +19,6 @@ public class RobotiqWriteRequestFactory
       {
          registers[i] = new SimpleRegister(0);
       }
-   }
-   
-   private void initialize()
-   {
-      actionRequestRegister = new ActionRequestRegister();
-      gripperOptionRegister = new GripperOptionRegister();
    }
    
    public WriteMultipleRegistersRequest createActivationRequest()
