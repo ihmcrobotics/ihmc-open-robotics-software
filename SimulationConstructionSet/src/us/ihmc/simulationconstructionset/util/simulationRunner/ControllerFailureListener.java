3c0426af63e31fccd9e3e4dcc52db4a704836f9f
Alex Graber-Tilton <agrabertilton@ihmc.us>
2015-05-01 22:21:36 +0000

    NONE: Falling notifiers now receive a 2d fall direction vector in the world frame. won't FixBamboo. pair: McCrory

--- SimulationConstructionSet/src/us/ihmc/simulationconstructionset/util/simulationRunner/ControllerFailureListener.java
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/util/simulationRunner/ControllerFailureListener.java
@@ -1,6 +1,8 @@
 package us.ihmc.simulationconstructionset.util.simulationRunner;
 
+import us.ihmc.utilities.math.geometry.FrameVector2d;
+
 public interface ControllerFailureListener
 {
-   public void controllerFailed();
+   public void controllerFailed(FrameVector2d fallingDirection);
 }
