ab2c3e03152115bd42a99143fffff56ef3cab8c7
Nathan Mertins <nmertins@ihmc.us>
2015-05-02 23:23:58 +0000

    NONE: fun with generics; pair: a tall mug of 1845

--- RobotiqHandDrivers/test/us/ihmc/robotiq/communication/registers/RobotiqInputRegisterTest.java
+++ RobotiqHandDrivers/test/us/ihmc/robotiq/communication/registers/RobotiqInputRegisterTest.java
@@ -2,12 +2,16 @@ package us.ihmc.robotiq.communication.registers;
 
 import org.junit.Test;
 
-public class RobotiqInputRegisterTest
+import us.ihmc.utilities.test.AbstractUnitTest;
+
+public abstract class RobotiqInputRegisterTest implements AbstractUnitTest<RobotiqInputRegister>
 {
+   
+   
    @Test
    public void testSetRegister()
    {
-      
+      RobotiqInputRegister inputRegister = getImplementingType();
    }
 
 }
