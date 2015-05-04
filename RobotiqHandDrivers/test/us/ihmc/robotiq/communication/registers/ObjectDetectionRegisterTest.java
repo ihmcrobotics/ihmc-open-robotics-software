ddd35a2d633e13db0a079e5af0b376a29dbfe03e
Nathan Mertins <nmertins@ihmc.us>
2015-05-04 20:03:49 +0000

    NONE: unit tests that test setting a register's enums froma byte value; won't fixbamboo; pair: JUnit

---
+++ RobotiqHandDrivers/test/us/ihmc/robotiq/communication/registers/ObjectDetectionRegisterTest.java
@@ -0,0 +1,29 @@
+package us.ihmc.robotiq.communication.registers;
+
+import us.ihmc.robotiq.communication.registers.ObjectDetectionRegister.gDTA;
+import us.ihmc.robotiq.communication.registers.ObjectDetectionRegister.gDTB;
+import us.ihmc.robotiq.communication.registers.ObjectDetectionRegister.gDTC;
+import us.ihmc.robotiq.communication.registers.ObjectDetectionRegister.gDTS;
+
+public class ObjectDetectionRegisterTest extends RobotiqInputRegisterTest
+{
+
+   @Override
+   protected RobotiqInputRegister getInputRegister()
+   {
+      return new ObjectDetectionRegister(gDTA.FINGER_A_AT_REQUESTED_POSITION, gDTB.FINGER_B_AT_REQUESTED_POSITION, gDTC.FINGER_C_AT_REQUESTED_POSITION, gDTS.SCISSOR_AT_REQUESTED_POSITION);
+   }
+
+   @Override
+   protected RobotiqInputRegister getExpectedRegister()
+   {
+      return new ObjectDetectionRegister(gDTA.FINGER_A_CONTACT_WHILE_CLOSING, gDTB.FINGER_B_IN_MOTION, gDTC.FINGER_C_AT_REQUESTED_POSITION, gDTS.SCISSOR_CONTACT_WHILE_OPENING);
+   }
+
+   @Override
+   protected byte getValueToSet()
+   {
+      return 114;
+   }
+
+}
