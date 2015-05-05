02b3e491badf7099c7d3e80fcc9b7086b3765841
Stephen McCrory <smccrory@ihmc.us>
2015-05-05 20:33:00 +0000

    NONE: adding in auto-aligning of ModifiablePolarisModel with checkerboard. pair: Georg. won't fixBamboo. will saveTime.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/PolarisRobot.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/PolarisRobot.java
@@ -25,8 +25,17 @@ public class PolarisRobot extends Robot
    private final Link polarisLink;
    private final Graphics3DObject polarisLinkGraphics;
    
-   private static final RigidBodyTransform carToWheelTransform = new RigidBodyTransform();
-   private static final double carToWheelX = 0.39, carToWheelY = 0.39, carToWheelZ = 1.27, steeringWheelPitchInDegrees = -32.0;
+   /**
+    * Transforms between wheel, car, and checkerboard
+    * 
+    * Inverses are included so callers won't call invert() on the object
+    */
+   private static final RigidBodyTransform wheelToCarTransform = new RigidBodyTransform(), carToWheelTransform = new RigidBodyTransform();   
+   private static final double wheelToCarX = 0.39, wheelToCarY = 0.39, wheelToCarZ = 1.27, steeringWheelPitchInDegrees = -32.0;
+   
+   private static final Vector3d checkerBoardToWheelTranslation = new Vector3d(0.7418, -0.798, -0.0642);
+   private static final Matrix3d checkerBoardToWheelRotation = new Matrix3d(-0.8592, -0.0079, 0.5115, 0.0046, -1.0000, -0.0078, 0.5116, -0.0044, 0.8592);
+   private static final RigidBodyTransform wheelToCheckerBoard = new RigidBodyTransform(), checkerBoardToWheel = new RigidBodyTransform(checkerBoardToWheelRotation, checkerBoardToWheelTranslation);
 
    public PolarisRobot(String name, RigidBodyTransform rootJointTransform)
    {
@@ -65,14 +74,35 @@ public class PolarisRobot extends Robot
    
    static
    {
-      carToWheelTransform.setTranslation(carToWheelX, carToWheelY, carToWheelZ);
+      wheelToCarTransform.setTranslation(wheelToCarX, wheelToCarY, wheelToCarZ);
       Matrix3d rotation = new Matrix3d();
       rotation.rotY(Math.toRadians(steeringWheelPitchInDegrees));
-      carToWheelTransform.setRotation(rotation);
+      wheelToCarTransform.setRotation(rotation);
+      
+      carToWheelTransform.set(wheelToCarTransform);
+      carToWheelTransform.invert();
+      
+      wheelToCheckerBoard.set(checkerBoardToWheel);
+      wheelToCheckerBoard.invert();
    }
    
    public static RigidBodyTransform getCarToWheelTransform()
    {
+      return wheelToCarTransform;
+   }
+   
+   public static RigidBodyTransform getWheelToCarTransform()
+   {
       return carToWheelTransform;
+   }
+   
+   public static RigidBodyTransform getCheckerBoardToWheelTransform()
+   {
+      return checkerBoardToWheel;
+   }
+   
+   public static RigidBodyTransform getWheelToCheckerBoardTransform()
+   {
+      return wheelToCheckerBoard;
    }
 }
