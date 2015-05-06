992ed553852a8e4d82bb37c40857f726e87d4189
Sylvain Bertrand <sbertrand@ihmc.us>
2015-05-06 17:40:37 +0000

    NONE: Pair: Matt & John. Changed the CapturabilityBasedStatus to have access to the foot polygons from the UI. Won't fixbamboo.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetProducers/CapturabilityBasedStatusProducer.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetProducers/CapturabilityBasedStatusProducer.java
@@ -5,6 +5,7 @@ import us.ihmc.communication.streamingData.GlobalDataProducer;
 import us.ihmc.utilities.math.geometry.FrameConvexPolygon2d;
 import us.ihmc.utilities.math.geometry.FramePoint2d;
 import us.ihmc.utilities.robotSide.RobotSide;
+import us.ihmc.utilities.robotSide.SideDependentList;
 
 public class CapturabilityBasedStatusProducer
 {
@@ -15,9 +16,9 @@ public class CapturabilityBasedStatusProducer
       this.objectCommunicator = objectCommunicator;
    }
 
-   public void sendStatus(FramePoint2d capturePoint2d, FramePoint2d desiredCapturePoint2d, FrameConvexPolygon2d supportPolygon, RobotSide supportLeg)
+   public void sendStatus(FramePoint2d capturePoint2d, FramePoint2d desiredCapturePoint2d, SideDependentList<FrameConvexPolygon2d> footSupportPolygons)
    {
-      CapturabilityBasedStatus capturabilityBasedStatus = new CapturabilityBasedStatus(capturePoint2d, desiredCapturePoint2d, supportPolygon, supportLeg);
+      CapturabilityBasedStatus capturabilityBasedStatus = new CapturabilityBasedStatus(capturePoint2d, desiredCapturePoint2d, footSupportPolygons.get(RobotSide.LEFT), footSupportPolygons.get(RobotSide.RIGHT));
       objectCommunicator.queueDataToSend(capturabilityBasedStatus);
    }
 }
