65292c787975cc34ddb11519bb1deac85eff6ea7
Tingfan Wu <twu@ihmc.us>
2015-05-08 01:47:07 +0000

    NONE: chessboard detector based on BoofCV and OpenCV along with unit-test case pair: Giorg not fixbamboo

---
+++ IHMCPerception/test/us/ihmc/ihmcPerception/generatedTestSuites/IHMCPerceptionAFastTestSuite.java
@@ -0,0 +1,19 @@
+package us.ihmc.ihmcPerception.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.ihmcPerception.chessboardDetection.ChessboardPoseEstimatorTest.class
+})
+
+public class IHMCPerceptionAFastTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
