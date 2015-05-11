256b0de44380942691918c260fc40b348afa71a1
Igor Kalkov <ikalkov@ihmc.us>
2015-05-11 17:44:14 +0000

    DRC-2306: Immediate feedback to user actions in the UI. Pair: Doug. Won't fixBamboo.

--- IHMCCommunication/src/us/ihmc/communication/util/NetworkPorts.java
+++ IHMCCommunication/src/us/ihmc/communication/util/NetworkPorts.java
@@ -6,21 +6,21 @@ public class NetworkPorts
 {
    // Network processor <-> Controller communication
    public static final NetworkPorts CONTROLLER_PORT = new NetworkPorts(4895);
-   
+
    // Network processor <-> UI Communication
    public static final NetworkPorts NETWORK_PROCESSOR_TO_UI_TCP_PORT = new NetworkPorts(4898);
    public static final NetworkPorts NETWORK_PROCESSOR_TO_UI_TEST_TCP_PORT = new NetworkPorts(4899);
-   
+
    // Cloud dispatcher ports
    public static final NetworkPorts NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_TCP_PORT = new NetworkPorts(5000);
    public static final NetworkPorts NETWORK_PROCESSOR_CLOUD_DISPATCHER_BACKEND_CONSOLE_TCP_PORT = new NetworkPorts(5005);
    public static final NetworkPorts CONTROLLER_CLOUD_DISPATCHER_BACKEND_TCP_PORT = new NetworkPorts(5002);
    public static final NetworkPorts CONTROLLER_CLOUD_DISPATCHER_BACKEND_CONSOLE_TCP_PORT = new NetworkPorts(5007);
-   
+
    // Hands
    public static final NetworkPorts LEFT_HAND_PORT = new NetworkPorts(5003);
    public static final NetworkPorts RIGHT_HAND_PORT = new NetworkPorts(5004);
-   
+
    // Network manager ports
    public static final NetworkPorts BEHAVIOUR_MODULE_PORT = new NetworkPorts(6001); 
    public static final NetworkPorts UI_MODULE = new NetworkPorts(6002);
@@ -54,6 +54,7 @@ public class NetworkPorts
    }
    
    private final int port;
+
    private NetworkPorts(int port)
    {
       this.port = port;
