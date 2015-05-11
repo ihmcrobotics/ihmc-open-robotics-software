f9e5d7cce3c368f081731771a3e408c3840bef5b
Georg Wiedebach <gwiedebach@ihmc.us>
2015-05-11 16:13:29 +0000

    NONE: change sim and controller to use new steering wheel model Pair: Stephen won't fixbamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCSteeringWheelEnvironment.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCSteeringWheelEnvironment.java
@@ -11,104 +11,38 @@ import us.ihmc.darpaRoboticsChallenge.PolarisRobot;
 import us.ihmc.simulationconstructionset.ExternalForcePoint;
 import us.ihmc.simulationconstructionset.Robot;
 import us.ihmc.simulationconstructionset.robotController.ContactController;
-import us.ihmc.simulationconstructionset.util.environments.ContactableSteeringWheelRobot;
 import us.ihmc.simulationconstructionset.util.environments.SelectableObjectListener;
 import us.ihmc.simulationconstructionset.util.ground.CombinedTerrainObject3D;
 import us.ihmc.simulationconstructionset.util.ground.TerrainObject3D;
-import us.ihmc.utilities.math.geometry.FramePoint;
-import us.ihmc.utilities.math.geometry.FrameVector;
-import us.ihmc.utilities.math.geometry.ReferenceFrame;
 import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 
 public class DRCSteeringWheelEnvironment implements CommonAvatarEnvironmentInterface
 {
-   public enum SteeringWheelAttachment
-   {
-      SPINNER, CROSS_BAR, NONE, BOTH
-   }
-   
    private final List<Robot> robots = new ArrayList<Robot>();
-   private final ContactableSteeringWheelRobot wheelRobot;
    
    private final CombinedTerrainObject3D combinedTerrainObject;
 
    private final ArrayList<ExternalForcePoint> contactPoints = new ArrayList<ExternalForcePoint>();
 
-   public DRCSteeringWheelEnvironment(double percentOfSteeringWheelRadius, SteeringWheelAttachment wheelAttachment)
+   public DRCSteeringWheelEnvironment(double percentOfSteeringWheelRadius)
    {
-      this(0.39, 0.39, 1.27, 0.0, -32.0, percentOfSteeringWheelRadius, wheelAttachment);
+      this(0.39, 0.39, 1.27, 0.0, -32.0, percentOfSteeringWheelRadius);
    }
 
-   public DRCSteeringWheelEnvironment(Point3d wheelLocation, double valveYawInDegrees, double valvePitchInDegrees, double percentOfSteeringWheelRadius,
-         SteeringWheelAttachment wheelAttachment)
+   public DRCSteeringWheelEnvironment(Point3d wheelLocation, double valveYawInDegrees, double valvePitchInDegrees, double percentOfSteeringWheelRadius)
    {
-      this(wheelLocation.x, wheelLocation.y, wheelLocation.z, valveYawInDegrees, valvePitchInDegrees, percentOfSteeringWheelRadius, wheelAttachment);
+      this(wheelLocation.x, wheelLocation.y, wheelLocation.z, valveYawInDegrees, valvePitchInDegrees, percentOfSteeringWheelRadius);
    }
 
-   public DRCSteeringWheelEnvironment(double x, double y, double z, double yawInDegrees, double pitchInDegrees, double percentOfSteeringWheelRadius,
-         SteeringWheelAttachment wheelAttachment)
+   public DRCSteeringWheelEnvironment(double x, double y, double z, double yawInDegrees, double pitchInDegrees, double percentOfSteeringWheelRadius)
    {
-      double forceVectorScale = 1.0 / 50.0;
-
       combinedTerrainObject = new CombinedTerrainObject3D(getClass().getSimpleName());
       combinedTerrainObject.addTerrainObject(DRCDemo01NavigationEnvironment.setUpGround("Ground"));
 
-      wheelRobot = ContactableSteeringWheelRobot.createPolarisSteeringWheel(x, y, z, Math.toRadians(yawInDegrees), Math.toRadians(pitchInDegrees));
-      wheelRobot.createSteeringWheelRobot();
-      wheelRobot.createAvailableContactPoints(1, 30, forceVectorScale, true);
-      switch (wheelAttachment)
-      {
-         case SPINNER:
-            wheelRobot.addSpinnerHandle(percentOfSteeringWheelRadius);
-            break;
-         case CROSS_BAR:
-            wheelRobot.addCrossBar();
-            break;
-         case BOTH:
-            wheelRobot.addSpinnerHandle(0.0, 14.0/17.5, 0.15, 0.015, 0.1);
-            wheelRobot.addCrossBar();
-            break;
-         default:
-            break;
-      }
-      
-      robots.add(wheelRobot);
-      
+      // add Polaris to environment
       RigidBodyTransform polarisTransform = new RigidBodyTransform();
       polarisTransform.setRotation(new AxisAngle4d(new Vector3d(0.0, 0.0, 1.0), 0.0));
       robots.add(new PolarisRobot("polaris", polarisTransform));
-   }
-
-
-
-   public ReferenceFrame getSteeringWheelFrame()
-   {
-      return wheelRobot.getSteeringWheelFrame();
-   }
-
-   public double getSteeringWheelRadius()
-   {
-      return wheelRobot.getSteeringWheelRadius();
-   }
-
-   public FramePoint getSpinnerHandleCenter()
-   {
-      return wheelRobot.getSpinnerHandleCenter();
-   }
-
-   public FrameVector getSteeringWheelAxis()
-   {
-      return wheelRobot.getSteeringWheelAxis();
-   }
-
-   public FramePoint getSteeringWheelCenter()
-   {
-      return wheelRobot.getSteeringWheelCenter();
-   }
-
-   public double getSteeringWheelAngleAsAbsolutePercentageOfRangeOfMotion()
-   {
-      return wheelRobot.getSteeringWheelAngleAsAbsolutePercentageOfRangeOfMotion();
    }
 
    @Override
@@ -129,15 +63,12 @@ public class DRCSteeringWheelEnvironment implements CommonAvatarEnvironmentInter
       ContactController contactController = new ContactController("SteeringWheel");
       contactController.setContactParameters(1000.0, 100.0, 0.5, 0.3);
       contactController.addContactPoints(contactPoints);
-      contactController.addContactable(wheelRobot);
-      wheelRobot.setController(contactController);
    }
 
    @Override
    public void addContactPoints(List<? extends ExternalForcePoint> externalForcePoints)
    {
       this.contactPoints.addAll(externalForcePoints);
-
    }
 
    @Override
