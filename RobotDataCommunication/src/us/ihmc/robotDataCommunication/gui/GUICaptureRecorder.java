363f60d5bd88ae731887f663c15d0bde9a82befe
Jesper Smith <jsmith@ihmc.us>
2015-05-14 16:48:38 +0000

    NONE: does not fixbamboo. pair: Removed (most) swing stuff from GUICaptuureRecorder

--- RobotDataCommunication/src/us/ihmc/robotDataCommunication/gui/GUICaptureRecorder.java
+++ RobotDataCommunication/src/us/ihmc/robotDataCommunication/gui/GUICaptureRecorder.java
@@ -1,7 +1,5 @@
 package us.ihmc.robotDataCommunication.gui;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -11,10 +9,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import us.ihmc.communication.configuration.NetworkParameterKeys;
@@ -24,103 +18,47 @@ import us.ihmc.robotDataCommunication.logger.LogProperties;
 import us.ihmc.robotDataCommunication.logger.LogSettings;
 import us.ihmc.robotDataCommunication.logger.NetworkStreamVideoDataLogger;
 
-public class GUICaptureRecorder extends JFrame
+public class GUICaptureRecorder
 {
-   private static final long serialVersionUID = -5900895574333786847L;
-   public final JButton start = new JButton("start");
-   public final JButton stop = new JButton("stop");
-   private final JComboBox<LogSettings> gui = new JComboBox<>();
    public NetworkStreamVideoDataLogger currentLogger = null;
-   
    private File target;
 
    public GUICaptureRecorder()
    {
-      super("GUI Capture recorder");
-      BoxLayout layout = new BoxLayout(getContentPane(), BoxLayout.X_AXIS);
-      getContentPane().setLayout(layout);
 
-      for (LogSettings logSetting : LogSettings.values())
-      {
-         if (logSetting.getVideoStream() != null)
-         {
-            gui.addItem(logSetting);
-         }
-      }
-      
-      start.addActionListener(new ActionListener()
-      {
-         
-         @Override
-         public void actionPerformed(ActionEvent e)
-         {
-            try
-            {
-               startLog();
-            }
-            catch (UnknownHostException | SocketException e1)
-            {
-               e1.printStackTrace();
-            }
-         }
-      });
-      
-      stop.addActionListener(new ActionListener()
-      {
-         
-         @Override
-         public void actionPerformed(ActionEvent e)
-         {
-            stopLog();
-         }
-      });
-      
-      getContentPane().add(gui);
-      getContentPane().add(start);
-      getContentPane().add(stop);
-      
-      setLocationByPlatform(true);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-      pack();
-      setVisible(true);
    }
 
-   public void startLog() throws UnknownHostException, SocketException
+   public void startLog(LogSettings settings) throws UnknownHostException, SocketException
    {
-      if(currentLogger == null)
+      if (currentLogger == null)
       {
          DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
          Calendar calendar = Calendar.getInstance();
          String timestamp = dateFormat.format(calendar.getTime());
-         
-         LogSettings settings = (LogSettings) gui.getSelectedItem();
+
          target = new File(System.getProperty("user.home") + "/robotLogs/", timestamp + "_" + settings.toString() + "_UILog");
          target.mkdirs();
 
          LogProperties logProperties = new LogProperties()
          {
-            private static final long serialVersionUID = 1L; 
+            private static final long serialVersionUID = 1L;
          };
-         
-         InetAddress listen = InetAddress.getByName(NetworkParameters.getHost(NetworkParameterKeys.logger)); 
+
+         InetAddress listen = InetAddress.getByName(NetworkParameters.getHost(NetworkParameterKeys.logger));
          InetSocketAddress address = new InetSocketAddress(settings.getVideoStream(), LogDataProtocolSettings.UI_DATA_PORT);
          currentLogger = new NetworkStreamVideoDataLogger(listen.getAddress(), target, logProperties, address);
-         start.setEnabled(false);
-         stop.setEnabled(true);
       }
-      
+
    }
 
    public void stopLog()
    {
-      if(currentLogger != null)
+      if (currentLogger != null)
       {
          currentLogger.close();
-         JOptionPane.showMessageDialog(this, "Video saved to " + target.getAbsolutePath());
+         JOptionPane.showMessageDialog(null, "Video saved to " + target.getAbsolutePath());
          currentLogger = null;
       }
-      start.setEnabled(true);
 
    }
 
