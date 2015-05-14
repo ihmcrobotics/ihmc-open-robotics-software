80460b5f9fd552aa6dc2c2a6c760a3d4e4076785
Tingfan Wu <twu@ihmc.us>
2015-05-14 01:06:17 +0000

    NONE: Refactor, expose CameraData from CameraDataReceiver so objects can be passed around easily rather than tons of arguments. pair:none not fixbamboo

---
+++ SensorProcessing/src/us/ihmc/sensorProcessing/sensorData/CameraData.java
@@ -0,0 +1,23 @@
+package us.ihmc.sensorProcessing.sensorData;
+
+import java.awt.image.BufferedImage;
+
+import us.ihmc.utilities.robotSide.RobotSide;
+import boofcv.struct.calib.IntrinsicParameters;
+
+public class CameraData
+{
+   public final RobotSide robotSide;
+   public final BufferedImage image;
+   public final long timestamp;
+   public final IntrinsicParameters intrinsicParameters;
+
+   public CameraData(RobotSide robotSide, BufferedImage image, long timestamp, IntrinsicParameters intrinsicParameters)
+   {
+      this.robotSide = robotSide;
+      this.image = image;
+      this.timestamp = timestamp;
+      this.intrinsicParameters = intrinsicParameters;
+   }
+
+}
