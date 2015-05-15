4e2c40fbe8efe130e0bc7ed81288443b3077ba59
Tingfan Wu <twu@ihmc.us>
2015-05-15 16:21:29 +0000

    NONE: blackfly parameter setters, commit only awaiting to be tested on realRobot, not linked to competition code yet. pair: none (will be review with Brandon) not fixbamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/sensors/blackfly/BlackFlyParameterSetter.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/sensors/blackfly/BlackFlyParameterSetter.java
@@ -1,207 +1,68 @@
 package us.ihmc.darpaRoboticsChallenge.sensors.blackfly;
 
-import org.ros.exception.RemoteException;
-import org.ros.node.NodeConfiguration;
-import org.ros.node.parameter.ParameterListener;
-import org.ros.node.parameter.ParameterTree;
-import org.ros.node.service.ServiceResponseListener;
+import java.util.Map;
 
 import us.ihmc.communication.net.PacketConsumer;
 import us.ihmc.communication.packetCommunicator.PacketCommunicator;
 import us.ihmc.communication.packets.sensing.BlackFlyParameterPacket;
+import us.ihmc.sensorProcessing.parameters.DRCRobotCameraParameters;
+import us.ihmc.utilities.ros.RosDynamicReconfigure;
 import us.ihmc.utilities.ros.RosMainNode;
-import us.ihmc.utilities.ros.RosServiceClient;
-import dynamic_reconfigure.DoubleParameter;
-import dynamic_reconfigure.Reconfigure;
-import dynamic_reconfigure.ReconfigureRequest;
-import dynamic_reconfigure.ReconfigureResponse;
 
 public class BlackFlyParameterSetter implements PacketConsumer<BlackFlyParameterPacket>
 {
-   private static double gain;
-   private static double brightness;
-   private static double frameRate;
-   private static double Shutter;
-   private final RosServiceClient<ReconfigureRequest, ReconfigureResponse> blackFlyClient;
-   private final RosMainNode rosMainNode;
    private final PacketCommunicator packetCommunicator;
-   private ParameterTree params;
 
-   public BlackFlyParameterSetter(RosMainNode rosMainNode, PacketCommunicator packetCommunicator)
+   RosDynamicReconfigure dynamicReconfigureClient;
+
+   public BlackFlyParameterSetter(RosMainNode rosMainNode, DRCRobotCameraParameters cameraParameters, PacketCommunicator packetCommunicator)
    {
-      this.rosMainNode = rosMainNode;
       this.packetCommunicator = packetCommunicator;
-      blackFlyClient = new RosServiceClient<ReconfigureRequest, ReconfigureResponse>(Reconfigure._TYPE);
-      rosMainNode.attachServiceClient("blackfly/set_parameters", blackFlyClient);
-      
+      this.dynamicReconfigureClient = new RosDynamicReconfigure(cameraParameters.getRosCameraInfoTopicName().replace("/camera_info", ""), rosMainNode);
       packetCommunicator.attachListener(BlackFlyParameterPacket.class, this);
    }
 
-   public void handleBlackFlyParameters(BlackFlyParameterPacket object)
-   {
-      if (object.isFromUI())
-      {
-         if (rosMainNode.isStarted())
-         {
-            params = rosMainNode.getParameters();
-            send();
 
-         }
+
+   public void sendDeviceSettingToUI()
+   {
+      
+      Map<String, Object> params = dynamicReconfigureClient.getParameters();
+      packetCommunicator.send(new BlackFlyParameterPacket(false, 
+            (Double)params.get("gain"), (Double)params.get("brightness"), (Double)params.get("frame_rate"), (Double)params.get("shutter")));
+   }
+
+   public void setBlackFlyParameters(BlackFlyParameterPacket packet)
+   {
+
+      System.out.println("object received with gain " + packet.getGain() + " brightness " + packet.getBrightness() + " framerate" + packet.getFrameRate()
+            + " shutter" + packet.getShutter());
+
+      if (packet.getGain() < 0)
+      {
+         dynamicReconfigureClient.setBool("auto_gain", true);
+         dynamicReconfigureClient.setBool("auto_exposure", true);
+         dynamicReconfigureClient.setBool("auto_shutter", true);
       }
       else
-         setBlackFlyParameters(object);
-
-   }
-
-   private void send()
-   {
-      if (params == null)
       {
-         //System.out.println("params are null");
-         return;
+         dynamicReconfigureClient.setBool("auto_exposure", false);
+         dynamicReconfigureClient.setBool("auto_gain", false);
+         dynamicReconfigureClient.setDouble("gain", packet.getGain());
+         dynamicReconfigureClient.setDouble("brightness", packet.getBrightness());
+         dynamicReconfigureClient.setDouble("shutter_speed", packet.getShutter());
       }
 
-      packetCommunicator.send(
-            new BlackFlyParameterPacket(false, params.getDouble("/blackfly/prop_gain"), params.getDouble("/blackfly/prop_brightness"), params
-                  .getDouble("/blackfly/prop_frame_rate"), params.getDouble("/blackfly/prop_shutter")));
+      dynamicReconfigureClient.setDouble("frame_rate", packet.getFrameRate());
    }
 
-   public void initializeParameterListeners()
+
+   public void receivedPacket(BlackFlyParameterPacket packet)
    {
-
-    //  System.out.println("-----------initialising blackfly parameter listeners-------------------");
-
-      rosMainNode.attachParameterListener("/blackfly/prop_gain", new ParameterListener()
+      if (packet.isFromUI()) //avoid hearing my own packet
       {
-
-         @Override
-         public void onNewValue(Object value)
-         {
-           // System.out.println("new blackfly gain received");
-            send();
-         }
-      });
-      rosMainNode.attachParameterListener("/blackfly/prop_brightness", new ParameterListener()
-      {
-
-         @Override
-         public void onNewValue(Object value)
-         {
-           // System.out.println("new blackfly brightness received");
-            send();
-         }
-      });
-
-      rosMainNode.attachParameterListener("/blackfly/prop_frame_rate", new ParameterListener()
-      {
-
-         @Override
-         public void onNewValue(Object value)
-         {
-           // System.out.println("new blackfly fram rate received");
-            send();
-         }
-      });
-      rosMainNode.attachParameterListener("/blackfly/prop_shutter", new ParameterListener()
-      {
-
-         @Override
-         public void onNewValue(Object value)
-         {
-           // System.out.println("new blackfly shutter received");
-            send();
-         }
-      });
-
-   }
-   
-   
-   public void setBlackFlyParameters(BlackFlyParameterPacket object)
-   {
-     
-      //System.out.println("object received with gain "+ object.getGain()+" brightness "+ object.getBrightness()+" framerate"+object.getFrameRate()+" shutter"+ object.getShutter());
-     
-      
-      
-      blackFlyClient.waitTillConnected();
-      ReconfigureRequest request = blackFlyClient.getMessage();
-      if(object.getGain() != gain){
-      gain = object.getGain();
-      DoubleParameter gainParam = NodeConfiguration.newPrivate().getTopicMessageFactory().newFromType(DoubleParameter._TYPE);
-      gainParam.setName("prop_gain");
-      gainParam.setValue(gain);
-      request.getConfig().getDoubles().add(gainParam);
+         setBlackFlyParameters(packet);
+         sendDeviceSettingToUI();
       }
-      
-      if(object.getBrightness() != brightness){
-         brightness = object.getBrightness();
-      DoubleParameter brightnessParam = NodeConfiguration.newPrivate().getTopicMessageFactory().newFromType(DoubleParameter._TYPE);
-      brightnessParam.setName("prop_brightness");
-      brightnessParam.setValue(brightness);
-      request.getConfig().getDoubles().add(brightnessParam);
-      }
-      
-      if(object.getFrameRate() != frameRate){
-      frameRate = object.getFrameRate();
-      DoubleParameter frameRateParam = NodeConfiguration.newPrivate().getTopicMessageFactory().newFromType(DoubleParameter._TYPE);
-      frameRateParam.setName("prop_frame_rate");
-      frameRateParam.setValue(frameRate);
-      request.getConfig().getDoubles().add(frameRateParam);
-      }
-      
-      if(object.getShutter() != Shutter){
-         Shutter = object.getShutter();
-         DoubleParameter shutterParam = NodeConfiguration.newPrivate().getTopicMessageFactory().newFromType(DoubleParameter._TYPE);
-         shutterParam.setName("prop_shutter");
-         shutterParam.setValue(Shutter);
-         request.getConfig().getDoubles().add(shutterParam);
-         }
-         
-          
-//      blackFlyClient.call(request, new ServiceResponseListener<ReconfigureResponse>()
-//            {
-//
-//               public void onSuccess(ReconfigureResponse response)
-//               {
-//                  System.out.println("successful" + response.getConfig().getDoubles().get(0).getValue());
-//               }
-//
-//               public void onFailure(RemoteException e)
-//               {
-//                  e.printStackTrace();
-//               }
-//            });
-   }
-
-   
-   public void setFishEyeFrameRate(double frameRate)
-   {
-      
-      blackFlyClient.waitTillConnected();
-      System.out.println("setting the framer rate for fish eye");
-      ReconfigureRequest request = blackFlyClient.getMessage();
-      DoubleParameter frameRateDoubleParam = NodeConfiguration.newPrivate().getTopicMessageFactory().newFromType(DoubleParameter._TYPE);
-      frameRateDoubleParam.setName("prop_frame_rate");
-      frameRateDoubleParam.setValue(frameRate);
-      request.getConfig().getDoubles().add(frameRateDoubleParam);
-           
-      blackFlyClient.call(request, new ServiceResponseListener<ReconfigureResponse>()
-      {
-
-         public void onSuccess(ReconfigureResponse response)
-         {
-            System.out.println("success" + response.getConfig().getDoubles().get(1).getValue());
-         }
-
-         public void onFailure(RemoteException e)
-         {
-            throw new RuntimeException(e);
-         }
-      });
-   }
-
-   public void receivedPacket(BlackFlyParameterPacket object)
-   {
-      handleBlackFlyParameters(object);
    }
 }
