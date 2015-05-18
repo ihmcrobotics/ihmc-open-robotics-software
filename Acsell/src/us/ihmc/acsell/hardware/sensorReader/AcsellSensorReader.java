d8d361ffc46e9e55842c7d7c7b2103450cef13e8
Sylvain Bertrand <sbertrand@ihmc.us>
2015-05-18 01:31:14 +0000

    NONE: Pair: none. Update the force sensors of SensorProcessing. Won't fixbamboo.

--- Acsell/src/us/ihmc/acsell/hardware/sensorReader/AcsellSensorReader.java
+++ Acsell/src/us/ihmc/acsell/hardware/sensorReader/AcsellSensorReader.java
@@ -134,7 +134,7 @@ public class AcsellSensorReader<JOINT extends Enum<JOINT> & AcsellJoint> impleme
          for (RobotSide robotSide : RobotSide.values)
          {
             forceSensorDataHolderForEstimator.setForceSensorValue(forceSensorDefinitions.get(robotSide), state.getFootWrench(robotSide));
-
+            sensorProcessing.setForceSensorValue(forceSensorDefinitions.get(robotSide), state.getFootWrench(robotSide));
          }
 
          sensorProcessing.setOrientationSensorValue(pelvisIMU, quaternion);
