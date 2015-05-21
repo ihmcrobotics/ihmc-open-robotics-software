15bd31d794aa230b96fe2bed94d0461054653921
Igor Kalkov <ikalkov@ihmc.us>
2015-05-21 20:36:30 +0000

    DRC-2306: Limited the packet size from the field shaper to the ocu shaper to 500 bytes. Pair: Doug. Won't fixBamboo. It's codeFreeze!

--- IHMCCommunication/src/us/ihmc/communication/net/KryoObjectServer.java
+++ IHMCCommunication/src/us/ihmc/communication/net/KryoObjectServer.java
@@ -2,20 +2,23 @@ package us.ihmc.communication.net;
 
 import java.io.IOException;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.ByteBufferOutputStream;
+import com.esotericsoftware.kryo.io.Output;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.KryoSerialization;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Serialization;
 import com.esotericsoftware.kryonet.Server;
+import us.ihmc.utilities.io.printing.PrintTools;
 
 public class KryoObjectServer extends KryoObjectCommunicator
 {
    private final int writeBufferSize;
-   
    private final Server server;
-
    private final int tcpPort;
 
+   private int maximumObjectSize = 0;
    private int maximumNumberOfConnections = Integer.MAX_VALUE;
 
    public KryoObjectServer(int tcpPort, NetClassList netClassList)
@@ -31,7 +34,6 @@ public class KryoObjectServer extends KryoObjectCommunicator
 
    public KryoObjectServer(int tcpPort, NetClassList netClassList, int writeBufferSize, int receiveBufferSize, Serialization serialization)
    {
-      super();
       this.server = new Server(writeBufferSize, receiveBufferSize, serialization);
       this.writeBufferSize = writeBufferSize;
       this.tcpPort = tcpPort;
@@ -56,6 +58,11 @@ public class KryoObjectServer extends KryoObjectCommunicator
       });
    }
 
+   public void setMaximumObjectSize(int maximumObjectSize)
+   {
+      this.maximumObjectSize = maximumObjectSize;
+   }
+
    public void setMaximumNumberOfConnections(int maximumNumberOfConnections)
    {
       this.maximumNumberOfConnections = maximumNumberOfConnections;
@@ -75,19 +82,46 @@ public class KryoObjectServer extends KryoObjectCommunicator
       return bytesSend;
    }
 
+   /** This method produces a lot of garbage. Use it wisely! */
+   private int calculateObjectSize(Object object)
+   {
+      Kryo kryo = new Kryo();
+      ByteBufferOutputStream bbos = new ByteBufferOutputStream(writeBufferSize);
+      Output output = new Output(bbos, writeBufferSize);
+      kryo.writeClassAndObject(output, object);
+      output.flush();
+      output.close();
+      return bbos.getByteBuffer().position();
+   }
+
    @Override
    protected int sendTCP(Object object)
    {
       int bytesSend = 0;
+
+      // Do not send if the object is above the limit
+      if (maximumObjectSize > 0)
+      {
+         int size = calculateObjectSize(object);
+         if (size > maximumObjectSize)
+         {
+            PrintTools.error(this, "Dropping the object of " + object.getClass() + ", because it is too big " + size + " > " + maximumObjectSize);
+            return 0;
+         }
+      }
+
       Connection[] connections = server.getConnections();
       for (int i = 0, n = connections.length; i < n; i++)
       {
          Connection connection = connections[i];
+
          // Do not send if the write buffer > 90% full
          if (((double) connection.getTcpWriteBufferSize()) / ((double) writeBufferSize) > 0.9)
          {
+            PrintTools.error(this, "Dropping the object of " + object.getClass() + ", because the write buffer is > 90% full!");
             continue;
          }
+
          bytesSend = connection.sendTCP(object);
       }
 
