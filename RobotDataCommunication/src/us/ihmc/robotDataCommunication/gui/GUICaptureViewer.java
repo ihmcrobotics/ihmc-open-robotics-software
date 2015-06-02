e39772605354c951996522711a03f374fcd5fcbe
Atlas Control Computer <AtlasControlComputer@unknown>
2015-06-02 01:14:35 +0000

    NONE: codefreeze: pair: fixbamboo: Awesome viewer

--- RobotDataCommunication/src/us/ihmc/robotDataCommunication/gui/GUICaptureViewer.java
+++ RobotDataCommunication/src/us/ihmc/robotDataCommunication/gui/GUICaptureViewer.java
@@ -1,13 +1,16 @@
 package us.ihmc.robotDataCommunication.gui;
 
 import java.awt.Dimension;
+import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.NetworkInterface;
 import java.nio.ByteBuffer;
 import java.util.HashSet;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -32,19 +35,34 @@ public class GUICaptureViewer
    }
 
    private final JFrame main;
-   private final HashSet<JLabel> streams = new HashSet<JLabel>();
+//   private final HashSet<JLabel> streams = new HashSet<JLabel>();
    private final YUVPictureConverter converter = new YUVPictureConverter();
    private BufferedImage image;
+   
+   private final JLabel label = new JLabel();
+
+   
+   private final BufferedImage logo;
 
    public GUICaptureViewer()
    {
       main = new JFrame();
       main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       main.setLayout(new GridLayout(1, 1));
-      main.setMaximizedBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
-      main.setExtendedState(main.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+      main.add(label);
+      setFullScreen();
+      
+      try
+      {
+         logo = ImageIO.read(getClass().getClassLoader().getResourceAsStream("ihmcRoboticsBlue.png"));
+         ImageIcon icon = new ImageIcon(logo);
+         label.setIcon(icon);
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
 
-      main.setVisible(true);
 
       for (final LogSettings setting : LogSettings.values())
       {
@@ -58,26 +76,46 @@ public class GUICaptureViewer
 
    }
 
-   private void reLayout()
+   private void setFullScreen()
    {
-      int elements = streams.size();
-
-      int cols = 1;
-      int rows = 1;
-      if (elements > 1)
+      main.setMaximizedBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
+      main.setExtendedState(main.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+      main.setUndecorated(true);
+      main.setResizable(false);
+      GraphicsDevice device = GraphicsEnvironment
+            .getLocalGraphicsEnvironment().getDefaultScreenDevice();
+      if (device.isFullScreenSupported())
       {
-         cols = 2;
-         rows = elements % 2 == 0 ? elements / cols : elements / cols + 1;
+         device.setFullScreenWindow(main);
       }
+      else
+      {
 
-      GridLayout layout = new GridLayout(rows, cols);
-      main.setLayout(layout);
-      main.pack();
+         main.setVisible(true);
+      }
    }
+
+//   private void reLayout()
+//   {
+//      int elements = streams.size();
+//
+//      int cols = 1;
+//      int rows = 1;
+//      if (elements > 1)
+//      {
+//         cols = 2;
+//         rows = elements % 2 == 0 ? elements / cols : elements / cols + 1;
+//      }
+//
+//      GridLayout layout = new GridLayout(rows, cols);
+//      main.setLayout(layout);
+//      main.pack();
+//      setFullScreen();
+//      
+//   }
 
    private class Handler implements LogPacketHandler
    {
-      private final JLabel label = new JLabel();
       private final JPEGDecoder decoder = new JPEGDecoder();
 
       @Override
@@ -96,12 +134,12 @@ public class GUICaptureViewer
             @Override
             public void run()
             {
-               if (!streams.contains(label))
-               {
-                  streams.add(label);
-                  main.add(label);
-                  reLayout();
-               }
+//               if (!streams.contains(label))
+//               {
+//                  streams.add(label);
+//                  main.add(label);
+//                  reLayout();
+//               }
 
                Dimension labelSize = label.getSize();
                if (labelSize.getWidth() == 0 || labelSize.getHeight() == 0)
@@ -140,16 +178,17 @@ public class GUICaptureViewer
             @Override
             public void run()
             {
-               synchronized (streams)
-               {
-                  if (streams.contains(label))
-                  {
-                     streams.remove(label);
-                     main.remove(label);
-                     reLayout();
-                  }
-
-               }
+               label.setIcon(new ImageIcon(logo));
+//               synchronized (streams)
+//               {
+//                  if (streams.contains(label))
+//                  {
+//                     streams.remove(label);
+//                     main.remove(label);
+//                     reLayout();
+//                  }
+//
+//               }
             }
          });
       }
