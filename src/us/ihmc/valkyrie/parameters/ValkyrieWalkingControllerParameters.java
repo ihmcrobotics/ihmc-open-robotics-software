a4b7429ee8af25743a6c5fc1f6c044259070c15e
Duncan Calvert <dcalvert@ihmc.us>
2015-07-23 15:47:12 +0000

    ROSI-27: Rename ValkyrieSliderBoardControlledFingerJoints and split up into ValkyrieFingerJointLimits and ValkyrieFingerJointName. Won't fixBamboo.

--- src/us/ihmc/valkyrie/parameters/ValkyrieWalkingControllerParameters.java
+++ src/us/ihmc/valkyrie/parameters/ValkyrieWalkingControllerParameters.java
@@ -15,7 +15,8 @@ import us.ihmc.utilities.math.geometry.RigidBodyTransform;
 import us.ihmc.utilities.robotSide.RobotSide;
 import us.ihmc.utilities.robotSide.SideDependentList;
 import us.ihmc.valkyrie.configuration.ValkyrieSliderBoardControlledNeckJoints;
-import us.ihmc.valkyrie.hands.ValkyrieSliderBoardControlledFingerJoints;
+import us.ihmc.valkyrie.fingers.ValkyrieFingerJointLimits;
+import us.ihmc.valkyrie.fingers.ValkyrieFingerJointName;
 import us.ihmc.wholeBodyController.DRCRobotJointMap;
 import us.ihmc.yoUtilities.controllers.YoOrientationPIDGains;
 import us.ihmc.yoUtilities.controllers.YoPDGains;
@@ -64,10 +65,10 @@ public class ValkyrieWalkingControllerParameters implements WalkingControllerPar
       for(RobotSide side : RobotSide.values())
       {
          sliderBoardControlledFingerJointNamesWithLimits.put(side, new LinkedHashMap<String, Pair<Double,Double>>());
-         for(ValkyrieSliderBoardControlledFingerJoints fingerJoint : ValkyrieSliderBoardControlledFingerJoints.values())
+         for(ValkyrieFingerJointName fingerJoint : ValkyrieFingerJointName.values())
          {
             sliderBoardControlledFingerJointNamesWithLimits.get(side).put(side.getCamelCaseNameForMiddleOfExpression() + fingerJoint.toString(), 
-                  new Pair<Double,Double>(ValkyrieSliderBoardControlledFingerJoints.getFullyExtensonPositionLimit(fingerJoint),ValkyrieSliderBoardControlledFingerJoints.getFullyFlexedPositionLimit(fingerJoint)));
+                  new Pair<Double,Double>(ValkyrieFingerJointLimits.getFullyExtensonPositionLimit(fingerJoint), ValkyrieFingerJointLimits.getFullyFlexedPositionLimit(fingerJoint)));
          }
       }
       
