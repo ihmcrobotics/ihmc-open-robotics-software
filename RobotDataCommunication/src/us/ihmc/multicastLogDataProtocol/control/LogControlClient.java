5864c9ca97259cd5004e8c0803375d080bd77acf
Igor Kalkov <ikalkov@ihmc.us>
2015-08-02 20:33:05 +0000

    NONE: Reusable LogControlClient for Android. Won't fixBamboo.

--- RobotDataCommunication/src/us/ihmc/multicastLogDataProtocol/control/LogControlClient.java
+++ RobotDataCommunication/src/us/ihmc/multicastLogDataProtocol/control/LogControlClient.java
@@ -6,6 +6,7 @@ import java.net.UnknownHostException;
 import java.util.List;
 
 import us.ihmc.communication.net.KryoObjectClient;
+import us.ihmc.communication.net.NetClassList;
 import us.ihmc.communication.net.NetStateListener;
 import us.ihmc.communication.net.ObjectConsumer;
 import us.ihmc.robotDataCommunication.YoVariablesUpdatedListener;
@@ -15,19 +16,28 @@ import us.ihmc.yoUtilities.dataStructure.variable.YoVariable;
 public class LogControlClient implements NetStateListener
 {
    private final KryoObjectClient client;
-   private final YoVariablesUpdatedListener listener;
 
-   public LogControlClient(byte[] host, int port, YoVariablesUpdatedListener listener)
+   private YoVariablesUpdatedListener listener = null;
+
+   public LogControlClient(byte[] host, int port, int writeBufferSize, int readBufferSize)
    {
-      this.listener = listener;
       try
       {
-         client = new KryoObjectClient(InetAddress.getByAddress(host), port, new LogControlClassList(), 2097152, 2097152);
+         InetAddress address = InetAddress.getByAddress(host);
+         NetClassList list = new LogControlClassList();
+         client = new KryoObjectClient(address, port, list, writeBufferSize, readBufferSize);
       }
       catch (UnknownHostException e)
       {
          throw new RuntimeException(e);
       }
+   }
+
+   public LogControlClient(byte[] host, int port, YoVariablesUpdatedListener listener)
+   {
+      this(host, port, 2097152, 2097152);
+      this.listener = listener;
+
       client.attachStateListener(this);
       client.attachListener(ClearLogRequest.class, new ClearLogRequestListener());
    }
@@ -40,8 +50,6 @@ public class LogControlClient implements NetStateListener
    @Override
    public void disconnected()
    {
-      // TODO Auto-generated method stub
-
    }
 
    public void close()
@@ -64,49 +72,54 @@ public class LogControlClient implements NetStateListener
       }
    }
 
-   public void startVariableChangedProducers(List<YoVariable<?>> variables)
+   public void startVariableChangedProducers(List<YoVariable<?>> variables, boolean sendChangedValues)
    {
       for (int i = 0; i < variables.size(); i++)
       {
-         LogControlVariableChangeListener variableChangedListener = new LogControlVariableChangeListener(i);
+         LogControlVariableChangeListener variableChangedListener = new LogControlVariableChangeListener(i, sendChangedValues);
          variables.get(i).addVariableChangedListener(variableChangedListener);
       }
+   }
+
+   public void sendClearLogRequest()
+   {
+      client.consumeObject(new ClearLogRequest());
+   }
+
+   public void sendVariableChangeRequest(int id, double value)
+   {
+      VariableChangeRequest request = new VariableChangeRequest();
+      request.variableID = id;
+      request.requestedValue = value;
+      client.consumeObject(request);
    }
 
    public class LogControlVariableChangeListener implements VariableChangedListener
    {
       private final int id;
+      private final boolean sendChangedValue;
 
-      public LogControlVariableChangeListener(int id)
+      public LogControlVariableChangeListener(int id, boolean sendChangedValue)
       {
          this.id = id;
+         this.sendChangedValue = sendChangedValue;
       }
 
       public void variableChanged(YoVariable<?> v)
       {
-         if(listener.executeVariableChangedListeners())
+         if (sendChangedValue)
          {
-            VariableChangeRequest request = new VariableChangeRequest();
-            request.variableID = id;
-            request.requestedValue = v.getValueAsDouble();
-            client.consumeObject(request);
+            sendVariableChangeRequest(id, v.getValueAsDouble());
          }
       }
    }
 
    public class ClearLogRequestListener implements ObjectConsumer<ClearLogRequest>
    {
-
       @Override
       public void consumeObject(ClearLogRequest object)
       {
          listener.clearLog();
       }
-
-   }
-
-   public void sendClearLogRequest()
-   {
-      client.consumeObject(new ClearLogRequest());
    }
 }
