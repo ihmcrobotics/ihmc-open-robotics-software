650512b0c5423855614f85e37fbb9299b71ff5d0
Nathan Mertins <nmertins@ihmc.us>
2015-08-04 21:29:29 +0000

    ROSI-75: moved ReferenceFrames out of geometry into their own package; won't fixbamboo

--- Acsell/src/us/ihmc/steppr/logProcessor/StepprSensorPostProcessor.java
+++ Acsell/src/us/ihmc/steppr/logProcessor/StepprSensorPostProcessor.java
@@ -35,7 +35,7 @@ import us.ihmc.robotics.humanoidRobot.model.FullRobotModel;
 import us.ihmc.robotics.humanoidRobot.model.RobotMotionStatusHolder;
 import us.ihmc.robotics.geometry.FramePoint2d;
 import us.ihmc.robotics.geometry.FrameVector;
-import us.ihmc.robotics.geometry.ReferenceFrame;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.robotics.screwTheory.InverseDynamicsJoint;
