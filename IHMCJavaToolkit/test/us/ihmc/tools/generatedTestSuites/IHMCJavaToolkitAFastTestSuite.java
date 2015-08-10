13f08f3fb89c91599531fd8261a5c9f8ff9d7720
Nathan Mertins <nmertins@ihmc.us>
2015-08-10 20:02:02 +0000

    NONE: generated Bamboo test suite for new project; won't fixbamboo

---
+++ IHMCJavaToolkit/test/us/ihmc/tools/generatedTestSuites/IHMCJavaToolkitAFastTestSuite.java
@@ -0,0 +1,25 @@
+package us.ihmc.tools.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.tools.ArrayToolsTest.class,
+   us.ihmc.tools.containers.ContainerToolsTest.class,
+   us.ihmc.tools.FormattingToolsTest.class,
+   us.ihmc.tools.io.StreamGobblerTest.class,
+   us.ihmc.tools.random.RandomToolsTest.class,
+   us.ihmc.tools.test.JUnitToolsTest.class,
+   us.ihmc.tools.thread.ThreadToolsTest.class
+})
+
+public class IHMCJavaToolkitAFastTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
