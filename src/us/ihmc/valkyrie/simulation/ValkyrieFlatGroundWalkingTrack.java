02d5cce8c74eb2bc359fd821edc15f5c848de132
Jordan Lack <jlack@ihmc.us>
2015-08-11 20:58:33 +0000

    NONE: committer: Bilkit Githinji. Implemented speed and heading controller using visual tracker from multisense data. Pair: Jordan Lacke. Won't fixbamboo.

--- src/us/ihmc/valkyrie/simulation/ValkyrieFlatGroundWalkingTrack.java
+++ src/us/ihmc/valkyrie/simulation/ValkyrieFlatGroundWalkingTrack.java
@@ -2,6 +2,7 @@ package us.ihmc.valkyrie.simulation;
 
 import us.ihmc.SdfLoader.SDFRobot;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.WalkingProvider;
+import us.ihmc.communication.configuration.NetworkParameters;
 import us.ihmc.darpaRoboticsChallenge.DRCFlatGroundWalkingTrack;
 import us.ihmc.darpaRoboticsChallenge.DRCGuiInitialSetup;
 import us.ihmc.darpaRoboticsChallenge.DRCSCSInitialSetup;
@@ -11,19 +12,23 @@ import us.ihmc.graphics3DAdapter.GroundProfile3D;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.joystick.JoystickUpdater;
 import us.ihmc.simulationconstructionset.util.ground.FlatGroundProfile;
+import us.ihmc.tools.thread.ThreadTools;
+import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 import us.ihmc.valkyrie.controllers.ValkyrieSliderBoard;
+import us.ihmc.valkyrie.controllers.ValkyrieSliderBoard.MultisenseHeadingSubscriber;
+import java.net.URI;
+
 
 public class ValkyrieFlatGroundWalkingTrack
 {
-
    public static void main(String[] args)
    {
       boolean USE_JOYSTICK_CONTROLLER = JoystickUpdater.isJoyStickConnected();
       
       DRCRobotModel robotModel = new ValkyrieRobotModel(false, false);
       DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(true, false);      
-      
+
       final double groundHeight = 0.0;
       GroundProfile3D groundProfile = new FlatGroundProfile(groundHeight);
 
@@ -42,8 +47,23 @@ public class ValkyrieFlatGroundWalkingTrack
             WalkingProvider.VELOCITY_HEADING_COMPONENT);
       
       SimulationConstructionSet scs = flatGroundWalkingTrack.getSimulationConstructionSet();
+     
+		String namespace = "/multisense",nodeName = "/heading_listener", headingTopicName="/multisense/vector_to_target";
+
+		final RosMainNode rosMainNode;	
+		
       if (USE_JOYSTICK_CONTROLLER)
       {
+    	URI rosUri = NetworkParameters.getROSURI();
+    	rosMainNode = new RosMainNode(rosUri, namespace + nodeName);
+    	new MultisenseHeadingSubscriber(rosMainNode, headingTopicName,scs.getRootRegistry());      	  		
+  		rosMainNode.execute();  		
+  		while(!rosMainNode.isStarted())
+  		{
+  		   ThreadTools.sleep(100);
+  		}
+
+  		 
          ValkyrieSliderBoard.setupJoyStickAndTreadmill(scs.getRootRegistry());
          flatGroundWalkingTrack.getDrcSimulation().start();
          flatGroundWalkingTrack.getDrcSimulation().simulate();
