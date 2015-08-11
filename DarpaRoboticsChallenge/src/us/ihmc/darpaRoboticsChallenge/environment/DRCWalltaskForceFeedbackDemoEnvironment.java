b0bc9e8c07fef01de3ce636c3425d1ba9f4eca18
Tobias Meier <tmeier@ihmc.us>
2015-08-11 22:24:40 +0000

    NONE: removed unused code segments. does not fixbamboo

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCWalltaskForceFeedbackDemoEnvironment.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCWalltaskForceFeedbackDemoEnvironment.java
@@ -12,8 +12,6 @@ import us.ihmc.simulationconstructionset.util.ground.TerrainObject3D;
 
 public class DRCWalltaskForceFeedbackDemoEnvironment implements CommonAvatarEnvironmentInterface
 {
-   private final int NUMBER_OF_CONTACT_POINTS = 50;
-//   private final List<ContactableButtonRobot> buttonRobots = new ArrayList<ContactableButtonRobot>();
    private final CombinedTerrainObject3D combinedTerrainObject;
    private final ArrayList<ExternalForcePoint> contactPoints = new ArrayList<ExternalForcePoint>();
    
@@ -21,18 +19,12 @@ public class DRCWalltaskForceFeedbackDemoEnvironment implements CommonAvatarEnvi
    {
       combinedTerrainObject = new CombinedTerrainObject3D(getClass().getSimpleName());
       combinedTerrainObject.addTerrainObject(setUpGround("Ground"));
-      
-     
    }
-
-   
 
    private CombinedTerrainObject3D setUpGround(String name)
    {
       CombinedTerrainObject3D combinedTerrainObject = new CombinedTerrainObject3D(name);
-
       combinedTerrainObject.addBox(-10.0, -10.0, 10.0, 10.0, -0.05, 0.0, YoAppearance.DarkBlue());
-
       return combinedTerrainObject;
    }
 
@@ -46,33 +38,23 @@ public class DRCWalltaskForceFeedbackDemoEnvironment implements CommonAvatarEnvi
    public List<ContactableButtonRobot> getEnvironmentRobots()
    {
       return null;
-//      return buttonRobots;
    }
    
    @Override
-   public void createAndSetContactControllerToARobot() {
-      
-//      for(ContactableButtonRobot buttonRobot : this.buttonRobots)
-//      {
-//         ContactController contactController = new ContactController();
-//         contactController.setContactParameters(1000.0, 100.0, 0.5, 0.3);
-//         contactController.addContactPoints(contactPoints);
-//         contactController.addContactables(buttonRobots);
-//         buttonRobot.setController(contactController);
-//      }
+   public void createAndSetContactControllerToARobot()
+   {
+	   // TODO Auto-generated method stub
    };
 
    @Override
    public void addContactPoints(List<? extends ExternalForcePoint> externalForcePoints)
    {
       this.contactPoints.addAll(externalForcePoints);
-
    }
 
    @Override
    public void addSelectableListenerToSelectables(SelectableObjectListener selectedListener)
    {
       // TODO Auto-generated method stub
-
    }
 }
