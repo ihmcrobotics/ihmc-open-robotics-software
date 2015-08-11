176f625df7382b026514b9429e1cbbb6a029327e
Duncan Calvert <dcalvert@ihmc.us>
2015-08-11 21:08:10 +0000

    ROSI-94: Refactor affinity, concurrent, and util into realtime package. Won't fixBamboo.

--- Acsell/src/us/ihmc/acsell/hardware/AcsellAffinity.java
+++ Acsell/src/us/ihmc/acsell/hardware/AcsellAffinity.java
@@ -2,9 +2,8 @@ package us.ihmc.acsell.hardware;
 
 import java.util.logging.Logger;
 
-import us.ihmc.affinity.CPUTopology;
-import us.ihmc.affinity.Package;
-import us.ihmc.affinity.Processor;
+import us.ihmc.realtime.affinity.CPUTopology;
+import us.ihmc.realtime.affinity.Processor;
 
 public class AcsellAffinity
 {
@@ -26,7 +25,7 @@ public class AcsellAffinity
 
       log.config("Pinning control threads to processor 1 & 2.");
       setAffinity = true;
-      Package socket = topology.getPackage(0);
+      us.ihmc.realtime.affinity.Package socket = topology.getPackage(0);
       estimatorThreadProcessor = socket.getCore(1).getDefaultProcessor();
       controlThreadProcessor = socket.getCore(2).getDefaultProcessor();
 
