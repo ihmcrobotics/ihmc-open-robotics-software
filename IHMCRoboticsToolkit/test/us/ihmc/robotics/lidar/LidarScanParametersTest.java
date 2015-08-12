7d6d5eed6901091bb6760f7fa893220081f6d148
Daniel Duran <dduran@ihmc.us>
2015-08-12 15:36:36 +0000

    NONE: fixBamboo - no fix -  ROSI-100 : added unit tests for unit conversions, Lidar scans and lidar parameters

--- IHMCRoboticsToolkit/test/us/ihmc/robotics/lidar/LidarScanParametersTest.java
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/lidar/LidarScanParametersTest.java
@@ -235,5 +235,81 @@ public class LidarScanParametersTest
       }
 
    }
+   
+   
+   @EstimatedDuration(duration = 0.0)
+   @Test(timeout = 30000)
+   public void LidarScanParameters_9()
+   {
+      Random rand = new Random();
+
+      for (int i = 0; i < 1000; i++)
+      {
+         int pointsPerSweep = rand.nextInt();
+         int fov = (int) (rand.nextFloat() * Math.PI);
+         double minRange = rand.nextDouble();
+         double maxRange = rand.nextDouble();
+         double timeIncrement = rand.nextDouble();
+         double scanTime = rand.nextDouble();
+         long timeStamp = System.currentTimeMillis();
+         double minYaw = -fov / 2.0;
+         double maxYaw = fov / 2.0;
+         
+         LidarScanParameters lidarScanParameters = new LidarScanParameters(pointsPerSweep, (float)minYaw, (float)maxYaw, (float)minRange, (float)maxRange);
+
+         assertEquals(lidarScanParameters.getMaxRange(), maxRange, 1e-7);
+         assertEquals(lidarScanParameters.getMinRange(), minRange, 1e-7);
+         assertEquals(lidarScanParameters.getPointsPerSweep(), pointsPerSweep, 1e-7);
+         assertEquals(lidarScanParameters.getFieldOfView(), fov, 1e-7);
+         assertEquals(lidarScanParameters.getSweepYawMin(), -fov / 2.0, 1e-7);
+         assertEquals(lidarScanParameters.getSweepYawMax(), fov / 2.0, 1e-7);
+         assertEquals(lidarScanParameters.getTimeIncrement(), 0, 1e-7);
+         assertEquals(lidarScanParameters.getScanTime(), 0, 1e-7);
+         assertEquals(lidarScanParameters.getScanTimeNanos(), 0 * 1e9, 100);
+         assertEquals(lidarScanParameters.getTimestamp(), 0, 1e-7);
+         assertEquals(lidarScanParameters.getAngleIncrement(), (maxYaw - minYaw)/pointsPerSweep, 1e-3);
+      }
+
+   }
+   
+//   public LidarScanParameters(int pointsPerSweep, float sweepYawMin, float sweepYawMax, float timeIncrement, float minRange, float maxRange, float scanTime)
+//   {
+//      this(pointsPerSweep, sweepYawMin, sweepYawMax, timeIncrement, minRange, maxRange, scanTime, 0l);
+//   }
+   
+   @EstimatedDuration(duration = 0.0)
+   @Test(timeout = 30000)
+   public void LidarScanParameters_10()
+   {
+      Random rand = new Random();
+
+      for (int i = 0; i < 10000; i++)
+      {
+         int pointsPerSweep = rand.nextInt();
+         int fov = (int) (rand.nextFloat() * Math.PI);
+         double minRange = rand.nextDouble();
+         double maxRange = rand.nextDouble();
+         double timeIncrement = rand.nextDouble();
+         double scanTime = rand.nextDouble();
+         long timeStamp = System.currentTimeMillis();
+         double minYaw = -fov / 2.0;
+         double maxYaw = fov / 2.0;
+         
+         LidarScanParameters lidarScanParameters = new LidarScanParameters(pointsPerSweep, (float)minYaw, (float)maxYaw, (float)timeIncrement, (float)minRange, (float)maxRange, (float)scanTime);
+
+         assertEquals(lidarScanParameters.getMaxRange(), maxRange, 1e-7);
+         assertEquals(lidarScanParameters.getMinRange(), minRange, 1e-7);
+         assertEquals(lidarScanParameters.getPointsPerSweep(), pointsPerSweep, 1e-7);
+         assertEquals(lidarScanParameters.getFieldOfView(), fov, 1e-7);
+         assertEquals(lidarScanParameters.getSweepYawMin(), -fov / 2.0, 1e-7);
+         assertEquals(lidarScanParameters.getSweepYawMax(), fov / 2.0, 1e-7);
+         assertEquals(lidarScanParameters.getTimeIncrement(), timeIncrement, 1e-7);
+         assertEquals(lidarScanParameters.getScanTime(), scanTime, 1e-7);
+         assertEquals(lidarScanParameters.getScanTimeNanos(), scanTime * 1e9, 100);
+         assertEquals(lidarScanParameters.getTimestamp(), 0, 1e-7);
+         assertEquals(lidarScanParameters.getAngleIncrement(), (maxYaw - minYaw)/pointsPerSweep, 1e-3);
+      }
+
+   }
 
 }
