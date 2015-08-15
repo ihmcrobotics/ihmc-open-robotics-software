8b9940ebf065e4230cbf7472433c968083393c00
Duncan Calvert <dcalvert@ihmc.us>
2015-08-15 20:10:45 +0000

    ROSI-116: Generate ROS messages. fixBamboo.

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepDataListMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/FootstepDataListMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface FootstepDataListMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/FootstepDataListMessage";
-  static final java.lang.String _DEFINITION = "## FootstepDataListMessage\n# This message commands the controller to execute a list of footsteps. See FootstepDataMessage\n# for information about defining a footstep.\n\nFootstepDataMessage[] footstep_data_list\n\n# swingTime is the time spent in single-support when stepping\nfloat64 swing_time\n\n# transferTime is the time spent in double-support between steps\nfloat64 transfer_time\n\n\n";
+  static final java.lang.String _DEFINITION = "## FootstepDataListMessage\r\n# This message commands the controller to execute a list of footsteps. See FootstepDataMessage\r\n# for information about defining a footstep.\r\n\r\nFootstepDataMessage[] footstep_data_list\r\n\r\n# swingTime is the time spent in single-support when stepping\r\nfloat64 swing_time\r\n\r\n# transferTime is the time spent in double-support between steps\r\nfloat64 transfer_time\r\n\r\n\r\n";
   java.util.List<ihmc_msgs.FootstepDataMessage> getFootstepDataList();
   void setFootstepDataList(java.util.List<ihmc_msgs.FootstepDataMessage> value);
   double getSwingTime();
