8e733340828d0ea37e6538d35fe5a22daf77badd
Duncan Calvert <dcalvert@ihmc.us>
2015-08-18 18:59:33 +0000

    NONE: MINIBEAST: Remove DRC prefix from RobotContactPointParameters. Won't fixBamboo.

--- Acsell/src/us/ihmc/wanderer/parameters/WandererContactPointParameters.java
+++ Acsell/src/us/ihmc/wanderer/parameters/WandererContactPointParameters.java
@@ -12,17 +12,18 @@ import javax.vecmath.Point2d;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ContactableBodiesFactory;
-import us.ihmc.simulationconstructionset.util.LinearGroundContactModel;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ContactableBodiesFactory;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
 import us.ihmc.robotics.geometry.RotationFunctions;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
-import us.ihmc.wholeBodyController.DRCRobotContactPointParameters;
+import us.ihmc.simulationconstructionset.util.LinearGroundContactModel;
 import us.ihmc.wholeBodyController.DRCRobotJointMap;
+import us.ihmc.wholeBodyController.RobotContactPointParameters;
 
-public class WandererContactPointParameters extends DRCRobotContactPointParameters
+public class WandererContactPointParameters extends RobotContactPointParameters
 {
    private final ContactableBodiesFactory contactableBodiesFactory = new ContactableBodiesFactory();
 
