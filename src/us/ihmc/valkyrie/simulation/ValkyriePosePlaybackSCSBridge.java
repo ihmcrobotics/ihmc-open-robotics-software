f07ddafae5b654d7ca540971ba51f97c9a7df4f1
Brandon Shrewsbury <bshrewsbury@ihmc.us>
2015-08-18 17:17:35 +0000

    Ros-147 renamed FullRobotModel to FullHumanoidRobotModel for quadruped addition, wont fixBamboo

--- src/us/ihmc/valkyrie/simulation/ValkyriePosePlaybackSCSBridge.java
+++ src/us/ihmc/valkyrie/simulation/ValkyriePosePlaybackSCSBridge.java
@@ -6,7 +6,7 @@ import us.ihmc.SdfLoader.SDFFullRobotModel;
 import us.ihmc.SdfLoader.SDFHumanoidRobot;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.posePlayback.PosePlaybackSCSBridge;
-import us.ihmc.humanoidRobotics.model.FullRobotModel;
+import us.ihmc.humanoidRobotics.model.FullHumanoidRobotModel;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 import us.ihmc.wholeBodyController.DRCRobotJointMap;
 
@@ -19,7 +19,7 @@ public class ValkyriePosePlaybackSCSBridge
       
       DRCRobotJointMap jointMap = robotModel.getJointMap();
       SDFHumanoidRobot sdfRobot = robotModel.createSdfRobot(false);
-      FullRobotModel fullRobotModel = robotModel.createFullRobotModel();
+      FullHumanoidRobotModel fullRobotModel = robotModel.createFullRobotModel();
       SDFFullRobotModel fullRobotModelForSlider = robotModel.createFullRobotModel();
    
       new PosePlaybackSCSBridge(sdfRobot, fullRobotModel, fullRobotModelForSlider, robotModel.getControllerDT());
