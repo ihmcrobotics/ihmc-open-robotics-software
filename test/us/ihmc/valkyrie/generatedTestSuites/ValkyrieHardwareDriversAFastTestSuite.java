ed17ab1adbfcc3249a3f16d37cca14e1d4d79d48
Duncan Calvert <dcalvert@ihmc.us>
2015-08-19 20:45:24 +0000

    ROSI-132: Generate test suites. fixBamboo.

--- test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversAFastTestSuite.java
+++ test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversAFastTestSuite.java
@@ -14,7 +14,7 @@ import org.junit.runners.Suite;
    us.ihmc.valkyrie.kinematics.will.WillsPushrodTransmissionTest.class,
    us.ihmc.valkyrie.simulation.ValkyrieFlatGroundWalkingWithIMUDriftTest.class,
    us.ihmc.valkyrie.simulation.ValkyrieObstacleCourseRampFootstepSnapperTest.class,
-   us.ihmc.valkyrie.ValkyrieFlatGroundWalkingTest.class
+   us.ihmc.valkyrie.ValkyrieObstacleCourseEveryBuildTest.class
 })
 
 public class ValkyrieHardwareDriversAFastTestSuite
