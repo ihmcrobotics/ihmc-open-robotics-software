3ed7e61179ef449c11927afa025e100ede33dd2d
Duncan Calvert <dcalvert@ihmc.us>
2015-08-20 20:39:01 +0000

    NONE: Clean up package structure in IHMCSystemIdentification. Won't fixBamboo.

---
+++ IHMCSystemIdentification/test/us/ihmc/systemIdentification/generatedTestSuites/IHMCSystemIdentificationAFastTestSuite.java
@@ -0,0 +1,22 @@
+package us.ihmc.systemIdentification.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.systemIdentification.frictionId.frictionModels.AsymmetricCoulombViscousFrictionModelTest.class,
+   us.ihmc.systemIdentification.frictionId.frictionModels.AsymmetricCoulombViscousStribeckFrictionModelTest.class,
+   us.ihmc.systemIdentification.frictionId.frictionModels.NoCompensationFrictionModelTest.class,
+   us.ihmc.systemIdentification.frictionId.frictionModels.PressureBasedFrictionModelTest.class
+})
+
+public class IHMCSystemIdentificationAFastTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
