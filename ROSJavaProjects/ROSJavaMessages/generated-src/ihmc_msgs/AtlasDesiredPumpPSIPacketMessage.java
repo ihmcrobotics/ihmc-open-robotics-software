864eddd563b64f803038325d1307f74448b0c089
Jesper Smith <jsmith@ihmc.us>
2015-08-25 08:32:07 +0000

    NONE: PAIR: NOT FIXBAMBOO: Reverted commit 32404, removed RobotConfigurationData from messages

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/AtlasDesiredPumpPSIPacketMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/AtlasDesiredPumpPSIPacketMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface AtlasDesiredPumpPSIPacketMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/AtlasDesiredPumpPSIPacketMessage";
-  static final java.lang.String _DEFINITION = "## AtlasDesiredPumpPSIPacketMessage\n# Send a request to change the desired PSI of the Atlas hydraulic pump.\n\nint32 desired_pump_psi\n\n\n";
+  static final java.lang.String _DEFINITION = "## AtlasDesiredPumpPSIPacketMessage\r\n# Send a request to change the desired PSI of the Atlas hydraulic pump.\r\n\r\nint32 desired_pump_psi\r\n\r\n\r\n";
   int getDesiredPumpPsi();
   void setDesiredPumpPsi(int value);
 }
--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/PauseCommandMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/PauseCommandMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface PauseCommandMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/PauseCommandMessage";
-  static final java.lang.String _DEFINITION = "## PauseCommandMessage\n# This message pauses the execution of a list of footsteps. If this message is\n# sent in the middle of executing a footstep, the robot will finish the step and\n# pause when back in double support.\n\nbool pause\n\n\n";
+  static final java.lang.String _DEFINITION = "## PauseCommandMessage\r\n# This message pauses the execution of a list of footsteps. If this message is\r\n# sent in the middle of executing a footstep, the robot will finish the step and\r\n# pause when back in double support.\r\n\r\nbool pause\r\n\r\n\r\n";
   boolean getPause();
   void setPause(boolean value);
 }
