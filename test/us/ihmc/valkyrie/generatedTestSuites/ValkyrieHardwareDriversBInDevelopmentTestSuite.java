7c40f0b63b27ba6ce669b0057aeaaba0c505e6fd
Duncan Calvert <dcalvert@ihmc.us>
2015-08-26 02:14:42 +0000

    ROSI-132: Move tests around to different plans. fixBamboo.

--- test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversBInDevelopmentTestSuite.java
+++ test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieHardwareDriversBInDevelopmentTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.valkyrie.ValkyrieObstacleCourseRampsTest.class
+   us.ihmc.valkyrie.ValkyrieObstacleCourseFlatTest.class
 })
 
 public class ValkyrieHardwareDriversBInDevelopmentTestSuite
