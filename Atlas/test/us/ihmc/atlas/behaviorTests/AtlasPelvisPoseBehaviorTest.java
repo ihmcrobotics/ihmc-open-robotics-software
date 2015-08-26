629f4711d5106d2fcb1f2d25ca6bce3ccdf4eaff
Duncan Calvert <dcalvert@ihmc.us>
2015-08-26 20:04:15 +0000

    ROSI-160: Move Atlas failing test into Flaky. fixBamboo Slow.

--- Atlas/test/us/ihmc/atlas/behaviorTests/AtlasPelvisPoseBehaviorTest.java
+++ Atlas/test/us/ihmc/atlas/behaviorTests/AtlasPelvisPoseBehaviorTest.java
@@ -1,14 +1,18 @@
 package us.ihmc.atlas.behaviorTests;
 
+import org.junit.Test;
+
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.darpaRoboticsChallenge.behaviorTests.DRCPelvisPoseBehaviorTest;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.tools.agileTesting.BambooPlanType;
 import us.ihmc.tools.agileTesting.BambooAnnotations.BambooPlan;
+import us.ihmc.tools.agileTesting.BambooAnnotations.EstimatedDuration;
 
-@BambooPlan(planType = {BambooPlanType.Slow})
+@BambooPlan(planType = {BambooPlanType.Slow, BambooPlanType.Flaky})
 public class AtlasPelvisPoseBehaviorTest extends DRCPelvisPoseBehaviorTest
 {
    private final AtlasRobotModel robotModel;
@@ -28,5 +32,59 @@ public class AtlasPelvisPoseBehaviorTest extends DRCPelvisPoseBehaviorTest
    public String getSimpleRobotName()
    {
       return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testPelvisPitchRotationNoTranslation() throws SimulationExceededMaximumTimeException
+   {
+      BambooPlanType.assumeRunningOnPlanIfRunningOnBamboo(BambooPlanType.Slow);
+      super.testPelvisPitchRotationNoTranslation();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testPelvisYawRotationNoTranslation() throws SimulationExceededMaximumTimeException
+   {
+      BambooPlanType.assumeRunningOnPlanIfRunningOnBamboo(BambooPlanType.Slow);
+      super.testPelvisYawRotationNoTranslation();
+   }
+
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testPelvisRollRotationNoTranslation() throws SimulationExceededMaximumTimeException
+   {
+      BambooPlanType.assumeRunningOnPlanIfRunningOnBamboo(BambooPlanType.Slow);
+      super.testPelvisRollRotationNoTranslation();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testPelvisXTranslation() throws SimulationExceededMaximumTimeException
+   {
+      BambooPlanType.assumeRunningOnPlanIfRunningOnBamboo(BambooPlanType.Flaky);
+      super.testPelvisXTranslation();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testPelvisYTranslation() throws SimulationExceededMaximumTimeException
+   {
+      BambooPlanType.assumeRunningOnPlanIfRunningOnBamboo(BambooPlanType.Slow);
+      super.testPelvisYTranslation();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout = 300000)
+   public void testSingleRandomPelvisRotationNoTranslation() throws SimulationExceededMaximumTimeException
+   {
+      BambooPlanType.assumeRunningOnPlanIfRunningOnBamboo(BambooPlanType.Slow);
+      super.testSingleRandomPelvisRotationNoTranslation();
    }
 }
