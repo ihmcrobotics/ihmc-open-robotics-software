9293c65d475bbcbaaec64e9e8eafccfc69d87c89
Jesper Smith <jsmith@ihmc.us>
2015-08-26 14:54:54 +0000

    NONE: PAIR: NOT FIXBAMBOO: Updated documentation for messages. Changed camelcase to underscores for lastreceivedmessage

--- ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/LastReceivedMessage.java
+++ ROSJavaProjects/ROSJavaMessages/generated-src/ihmc_msgs/LastReceivedMessage.java
@@ -2,7 +2,7 @@ package ihmc_msgs;
 
 public interface LastReceivedMessage extends org.ros.internal.message.Message {
   static final java.lang.String _TYPE = "ihmc_msgs/LastReceivedMessage";
-  static final java.lang.String _DEFINITION = "##LastReceivedMessage\n\nstring type\nint64 uniqueId\nint64 receiveTimestamp\nfloat64 timeSinceLastReceived\n";
+  static final java.lang.String _DEFINITION = "##LastReceivedMessage\n\nstring type\nint64 unique_id\nint64 receive_timestamp\nfloat64 time_since_last_received\n";
   java.lang.String getType();
   void setType(java.lang.String value);
   long getUniqueId();
