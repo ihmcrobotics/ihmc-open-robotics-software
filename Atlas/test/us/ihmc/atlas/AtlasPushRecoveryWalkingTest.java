4475f548864f58783160d06a3a70192797a1993b
Duncan Calvert <dcalvert@ihmc.us>
2015-08-28 22:16:03 +0000

    ROSI-162: Move passing tests out of InDevelopment and into Slow. Won't fixBamboo.

--- Atlas/test/us/ihmc/atlas/AtlasPushRecoveryWalkingTest.java
+++ Atlas/test/us/ihmc/atlas/AtlasPushRecoveryWalkingTest.java
@@ -1,13 +1,17 @@
 package us.ihmc.atlas;
 
+import org.junit.Test;
 
 import us.ihmc.darpaRoboticsChallenge.DRCPushRecoveryWalkingTest;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
-import us.ihmc.tools.agileTesting.BambooPlanType;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.simulationconstructionset.util.simulationRunner.ControllerFailureException;
 import us.ihmc.tools.agileTesting.BambooAnnotations.BambooPlan;
+import us.ihmc.tools.agileTesting.BambooAnnotations.EstimatedDuration;
+import us.ihmc.tools.agileTesting.BambooPlanType;
 
-@BambooPlan(planType = {BambooPlanType.InDevelopment, BambooPlanType.VideoB})
+@BambooPlan(planType = {BambooPlanType.Slow, BambooPlanType.VideoB})
 public class AtlasPushRecoveryWalkingTest extends DRCPushRecoveryWalkingTest
 {  
    @Override
@@ -20,5 +24,77 @@ public class AtlasPushRecoveryWalkingTest extends DRCPushRecoveryWalkingTest
    public String getSimpleRobotName()
    {
       return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testForVideo() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testForVideo();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushLeftEarlySwing() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushLeftEarlySwing();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushLeftInitialTransferState() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushLeftInitialTransferState();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushRightInitialTransferState() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushRightInitialTransferState();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushRightLateSwing() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushRightLateSwing();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushRightThenLeftMidSwing() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushRightThenLeftMidSwing();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushRightTransferState() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushRightTransferState();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushTowardsTheBack() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushTowardsTheBack();
+   }
+   
+   @Override
+   @EstimatedDuration(duration = 50.0)
+   @Test(timeout=300000)
+   public void testPushTowardsTheFront() throws SimulationExceededMaximumTimeException, InterruptedException, ControllerFailureException
+   {
+      super.testPushTowardsTheFront();
    }
 }
