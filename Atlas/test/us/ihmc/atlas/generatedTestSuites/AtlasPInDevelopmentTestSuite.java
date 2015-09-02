e5a2fcb529b32b0f2aa8f9763455cccd796a4b5b
Duncan Calvert <dcalvert@ihmc.us>
2015-09-02 16:34:58 +0000

    ROSI-20: New Job duration test using REST API.

--- Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasPInDevelopmentTestSuite.java
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasPInDevelopmentTestSuite.java
@@ -7,10 +7,10 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseWholeBodyTrajectoryTest.class
+   us.ihmc.atlas.roughTerrainWalking.AtlasFootExplorationTest.class
 })
 
-public class AtlasOInDevelopmentTestSuite
+public class AtlasPInDevelopmentTestSuite
 {
    public static void main(String[] args)
    {
