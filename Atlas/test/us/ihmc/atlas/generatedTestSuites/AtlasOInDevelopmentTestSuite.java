9f97eaa5aed576d607ac2e2429b80b869a5b3f7f
Nathan Mertins <nmertins@ihmc.us>
2015-09-04 15:20:47 +0000

    NONE: fixed NullPointerException in DRCFootstepListBehaviorTest; moved AtlasFootstepListBehaviorTest to Fast and regenerated test suites; fixbamboo

--- Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasOInDevelopmentTestSuite.java
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasOInDevelopmentTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseWholeBodyTrajectoryTest.class
+   us.ihmc.atlas.roughTerrainWalking.AtlasFootExplorationTest.class
 })
 
 public class AtlasOInDevelopmentTestSuite
