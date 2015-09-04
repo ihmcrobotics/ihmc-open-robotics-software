5d515e34afcb4796061ef4388a236fabd29b8c55
Nathan Mertins <nmertins@ihmc.us>
2015-09-04 16:25:31 +0000

    NONE: moved AtlasScriptBehaviorTest from InDevelopment to Flaky; missed new generated test suite; fixbamboo

---
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasEFlakyTestSuite.java
@@ -0,0 +1,20 @@
+package us.ihmc.atlas.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseWobblyFootTest.class,
+   us.ihmc.communication.producers.AtlasRobotConfigurationDataBufferTest.class
+})
+
+public class AtlasEFlakyTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
