e9f9a2c7d4ee95912ebb447f7939c8d1a4193c1d
Duncan Calvert <dcalvert@ihmc.us>
2015-09-11 21:15:39 +0000

    NONE: Refactor workspace-wide test statistics out of TestSuiteGenerator and into ATWorkspace. In preparation for RunBeforeCheckInTestSuite.

--- IHMCUnitTesting/src/us/ihmc/tools/testing/TestPlanTarget.java
+++ IHMCUnitTesting/src/us/ihmc/tools/testing/TestPlanTarget.java
@@ -2,21 +2,55 @@ package us.ihmc.tools.testing;
 
 import static org.junit.Assume.assumeTrue;
 
+import java.util.ArrayList;
+
 public enum TestPlanTarget
 {
-   Exclude,
-   Manual,
-   Fast,
-   Slow,
-   VideoA,
-   VideoB,
-   UI,
-   CodeQuality,
-   Flaky,
-   InDevelopment;
+   Exclude(false, true),
+   Manual(false, false),
+   Fast(true, false),
+   Slow(true, false),
+   VideoA(true, false),
+   VideoB(true, false),
+   UI(true, false),
+   CodeQuality(false, false),
+   Flaky(true, false),
+   InDevelopment(true, false);
    
    public static final TestPlanTarget[] values = values();
+   public static final TestPlanTarget[] includedTargets;
+   public static final TestPlanTarget[] loadBalancedTargets;
+   static
+   {
+      includedTargets = new TestPlanTarget[values.length - 1];
+      int i = 0;
+      for (TestPlanTarget target : values)
+      {
+         if (target.isExcluded())
+            continue;
+
+         includedTargets[i++] = target;
+      }
+      
+      ArrayList<TestPlanTarget> balancedTargets = new ArrayList<>();
+      for (TestPlanTarget target : values)
+      {
+         if (target.isLoadBalanced())
+            balancedTargets.add(target);
+      }
+      loadBalancedTargets = balancedTargets.toArray(new TestPlanTarget[0]);
+   }
+   
    private static final String bambooJobName = System.getenv("BAMBOO_JOB_NAME");
+   
+   private final boolean loadBalanced;
+   private final boolean excluded;
+   
+   private TestPlanTarget(boolean loadBalanced, boolean excluded)
+   {
+      this.loadBalanced = loadBalanced;
+      this.excluded = excluded;
+   }
    
    public static boolean isRunningOnBamboo()
    {
@@ -44,5 +78,20 @@ public enum TestPlanTarget
          isRunningOnPlan |= isRunningOnPlan(planType);
          
       assumeTrue("Test only set to run on " + bambooPlanType, isRunningOnPlan);
+   }
+   
+   public boolean isIncludedAndNotLoadBalanced()
+   {
+      return !isExcluded() && !isLoadBalanced();
+   }
+
+   public boolean isLoadBalanced()
+   {
+      return loadBalanced;
+   }
+
+   public boolean isExcluded()
+   {
+      return excluded;
    }
 }
