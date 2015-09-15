3115641431c3bc2f1ca2d8c099d612a5aa91f5c5
Douglas Stephen <dstephen@ihmc.us>
2015-09-15 22:30:38 +0000

    ROSI-198: Added interface for tweaking sensors using the mutator and switched the mutator to be a single argument instead of a vararg since it's going to be a one-to-one relationship of mutator to file instead of mutator for model. Won't fixbamboo.

--- Acsell/src/us/ihmc/wanderer/parameters/WandererRobotModel.java
+++ Acsell/src/us/ihmc/wanderer/parameters/WandererRobotModel.java
@@ -91,11 +91,11 @@ public class WandererRobotModel implements DRCRobotModel
 
       if (headless)
       {
-         this.loader = DRCRobotSDFLoader.loadDRCRobot(new String[] {}, getSdfFileAsStream(), true);
+         this.loader = DRCRobotSDFLoader.loadDRCRobot(new String[] {}, getSdfFileAsStream(), true, null);
       }
       else
       {
-         this.loader = DRCRobotSDFLoader.loadDRCRobot(getResourceDirectories(), getSdfFileAsStream(), false);
+         this.loader = DRCRobotSDFLoader.loadDRCRobot(getResourceDirectories(), getSdfFileAsStream(), false, null);
       }
 
       for (String forceSensorNames : getSensorInformation().getForceSensorNames())
