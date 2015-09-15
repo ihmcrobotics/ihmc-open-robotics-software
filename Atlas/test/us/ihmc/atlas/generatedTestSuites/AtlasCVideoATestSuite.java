67a5ef9708799afbf523083ba5e9b7bdc70c3b47
Duncan Calvert <dcalvert@ihmc.us>
2015-09-15 19:04:47 +0000

    ROSI-77: Rewrite ATTestClass to parse tests and annotations using JavaParser with AST from Github. This is a rewrite of the core of test suite management and generation. Regenerate test suites.

--- Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasCVideoATestSuite.java
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasCVideoATestSuite.java
@@ -7,10 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
-   us.ihmc.atlas.AtlasPushRecoveryMultiStepTest.class,
-   us.ihmc.atlas.behaviorTests.AtlasChestOrientationBehaviorTest.class,
-   us.ihmc.atlas.behaviorTests.AtlasComHeightBehaviorTest.class,
-   us.ihmc.atlas.behaviorTests.AtlasWalkToGoalBehaviorTest.class
+   us.ihmc.atlas.AtlasPushRecoveryMultiStepTest.class
 })
 
 public class AtlasCVideoATestSuite
--- Graphics3DAdapter/test/us/ihmc/graphics3DAdapter/generatedTestSuites/Graphics3DAdapterAFastTestSuite.java
+++ Graphics3DAdapter/test/us/ihmc/graphics3DAdapter/generatedTestSuites/Graphics3DAdapterAFastTestSuite.java
@@ -8,6 +8,14 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses
 ({
    us.ihmc.graphics3DAdapter.graphics.Graphics3DObjectTest.class,
+   us.ihmc.graphics3DAdapter.jme.AWTPanelsTest.class,
+   us.ihmc.graphics3DAdapter.jme.JMERainbowTerrainTextureTest.class,
+   us.ihmc.graphics3DAdapter.jme.lidar.manual.JMELidar120FovTest.class,
+   us.ihmc.graphics3DAdapter.jme.lidar.manual.JMELidar180FovTest.class,
+   us.ihmc.graphics3DAdapter.jme.lidar.manual.JMELidar360FovTest.class,
+   us.ihmc.graphics3DAdapter.jme.lidar.manual.JMELidar60FovTest.class,
+   us.ihmc.graphics3DAdapter.jme.lidar.manual.JMELidar90FovTest.class,
+   us.ihmc.graphics3DAdapter.jme.lidar.manual.JMELidarSphere270FovTest.class,
    us.ihmc.graphics3DAdapter.jme.util.JMEDataTypeUtilsTest.class,
    us.ihmc.graphics3DAdapter.jme.util.JMEGeometryUtilsTest.class,
    us.ihmc.graphics3DAdapter.stlLoader.STLReaderTest.class
