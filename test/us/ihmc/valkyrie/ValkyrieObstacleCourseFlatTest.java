aed17105f770bb2c1dba183ce444a19586abedc7
Duncan Calvert <dcalvert@ihmc.us>
2015-09-16 19:42:57 +0000

    NONE: Load balanced some tests. Won't fixBamboo.

--- test/us/ihmc/valkyrie/ValkyrieObstacleCourseFlatTest.java
+++ test/us/ihmc/valkyrie/ValkyrieObstacleCourseFlatTest.java
@@ -1,10 +1,8 @@
 package us.ihmc.valkyrie;
 
 import javax.vecmath.Vector3d;
-
 import org.junit.Ignore;
 import org.junit.Test;
-
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCObstacleCourseFlatTest;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
@@ -19,7 +17,7 @@ import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
 public class ValkyrieObstacleCourseFlatTest extends DRCObstacleCourseFlatTest
 {
    private final DRCRobotModel robotModel = new ValkyrieRobotModel(DRCRobotModel.RobotTarget.SCS, false);
-   
+
    @Override
    public DRCRobotModel getRobotModel()
    {
@@ -37,12 +35,13 @@ public class ValkyrieObstacleCourseFlatTest extends DRCObstacleCourseFlatTest
    {
       return new Vector3d(0.02, -0.02, 0.0);
    }
-   
+
    @Override
    protected DoubleYoVariable getPelvisOrientationErrorVariableName(SimulationConstructionSet scs)
    {
-      return (DoubleYoVariable) scs.getVariable("MomentumBasedControllerFactory.PelvisOrientationManager.RootJointAngularAccelerationControlModule.v1PelvisAxisAngleOrientationController",
-                                                "v1PelvisOrientationErrorMagnitude");
+      return (DoubleYoVariable) scs.getVariable(
+          "MomentumBasedControllerFactory.PelvisOrientationManager.RootJointAngularAccelerationControlModule.v1PelvisAxisAngleOrientationController",
+          "v1PelvisOrientationErrorMagnitude");
    }
 
    @Override
@@ -50,7 +49,7 @@ public class ValkyrieObstacleCourseFlatTest extends DRCObstacleCourseFlatTest
    {
       return 0.1;
    }
-   
+
    /**
     * Doesn't work with Valkyrie yet. Need to get it working some day.
     */
@@ -63,127 +62,127 @@ public class ValkyrieObstacleCourseFlatTest extends DRCObstacleCourseFlatTest
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testWalkingUpToRampWithLongStepsAndOccasionallyStraightKnees();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 80.0)
-   @Test(timeout = 400000)
+   @DeployableTestMethod(estimatedDuration = 105.9)
+   @Test(timeout = 530000)
    public void testSimpleFlatGroundScriptWithOscillatingFeet() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testSimpleFlatGroundScriptWithOscillatingFeet();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 41.4)
+   @DeployableTestMethod(estimatedDuration = 41.4)
    @Test(timeout = 210000)
    public void testRotatedStepInTheAir() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment, TestPlanTarget.VideoB);
       super.testRotatedStepInTheAir();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 81.9)
-   @Test(timeout = 410000)
+   @DeployableTestMethod(estimatedDuration = 92.6)
+   @Test(timeout = 460000)
    public void testSimpleFlatGroundScriptWithRandomFootSlip() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testSimpleFlatGroundScriptWithRandomFootSlip();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 51.5)
-   @Test(timeout = 260000)
+   @DeployableTestMethod(estimatedDuration = 60.5)
+   @Test(timeout = 300000)
    public void testWalkingUpToRampWithShortSteps() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testWalkingUpToRampWithShortSteps();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 44.4)
-   @Test(timeout = 220000)
+   @DeployableTestMethod(estimatedDuration = 55.3)
+   @Test(timeout = 280000)
    public void testSideStepsWithSlipping() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testSideStepsWithSlipping();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 18.8)
-   @Test(timeout = 94000)
+   @DeployableTestMethod(estimatedDuration = 25.6)
+   @Test(timeout = 130000)
    public void testStandingTooHighToCheckIfSingularityStuffIsWorkingProperly() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testStandingTooHighToCheckIfSingularityStuffIsWorkingProperly();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 23.4)
-   @Test(timeout = 120000)
+   @DeployableTestMethod(estimatedDuration = 32.3)
+   @Test(timeout = 160000)
    public void testStandingWithOscillatingFeet() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testStandingWithOscillatingFeet();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 14.8)
-   @Test(timeout = 74000)
+   @DeployableTestMethod(estimatedDuration = 22.3)
+   @Test(timeout = 110000)
    public void testStandingForACoupleSeconds() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testStandingForACoupleSeconds();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 43.6)
-   @Test(timeout = 220000)
+   @DeployableTestMethod(estimatedDuration = 57.6)
+   @Test(timeout = 290000)
    public void testSideStepsWithRandomSlipping() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testSideStepsWithRandomSlipping();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 55.3)
-   @Test(timeout = 280000)
+   @DeployableTestMethod(estimatedDuration = 76.2)
+   @Test(timeout = 380000)
    public void testLongStepsMaxHeightPauseAndResume() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testLongStepsMaxHeightPauseAndResume();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 26.5)
+   @DeployableTestMethod(estimatedDuration = 26.5)
    @Test(timeout = 130000)
    public void testTurningInPlaceAndPassingPI() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testTurningInPlaceAndPassingPI();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 38.9)
-   @Test(timeout = 190000)
+   @DeployableTestMethod(estimatedDuration = 50.0)
+   @Test(timeout = 250000)
    public void testChestControlWithPackets() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testChestControlWithPackets();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 31.9)
-   @Test(timeout = 160000)
+   @DeployableTestMethod(estimatedDuration = 43.7)
+   @Test(timeout = 220000)
    public void testStandingOnUnevenTerrainForACoupleSeconds() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
       super.testStandingOnUnevenTerrainForACoupleSeconds();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 159.1)
-   @Test(timeout = 800000)
+   @DeployableTestMethod(estimatedDuration = 209.5)
+   @Test(timeout = 1000000)
    public void testForMemoryLeaks() throws Exception
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow, TestPlanTarget.VideoB);
--- test/us/ihmc/valkyrie/ValkyriePushRecoveryTest.java
+++ test/us/ihmc/valkyrie/ValkyriePushRecoveryTest.java
@@ -1,7 +1,6 @@
 package us.ihmc.valkyrie;
 
 import org.junit.Test;
-
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.pushRecovery.DRCPushRecoveryTest;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
@@ -16,7 +15,7 @@ public class ValkyriePushRecoveryTest extends DRCPushRecoveryTest
    {
       return new ValkyrieRobotModel(DRCRobotModel.RobotTarget.SCS, false);
    }
-   
+
    @Override
    @DeployableTestMethod(estimatedDuration = 25.0)
    @Test(timeout = 163619)
@@ -25,90 +24,90 @@ public class ValkyriePushRecoveryTest extends DRCPushRecoveryTest
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Fast);
       super.testControllerFailureKicksIn();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 26.0)
+   @DeployableTestMethod(estimatedDuration = 26.0)
    @Test(timeout = 130000)
    public void testLongBackwardPushWhileStanding() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testLongBackwardPushWhileStanding();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 16.4)
+   @DeployableTestMethod(estimatedDuration = 16.4)
    @Test(timeout = 82000)
    public void testLongBackwardPushWhileStandingAfterControllerFailureKickedIn() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testLongBackwardPushWhileStandingAfterControllerFailureKickedIn();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 15.7)
+   @DeployableTestMethod(estimatedDuration = 15.7)
    @Test(timeout = 78000)
    public void testLongForwardPushWhileStanding() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testLongForwardPushWhileStanding();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 17.2)
+   @DeployableTestMethod(estimatedDuration = 17.2)
    @Test(timeout = 86000)
    public void testLongForwardPushWhileStandingAfterControllerFailureKickedIn() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testLongForwardPushWhileStandingAfterControllerFailureKickedIn();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 35.7)
-   @Test(timeout = 180000)
+   @DeployableTestMethod(estimatedDuration = 46.3)
+   @Test(timeout = 230000)
    public void testPushWhileInSwing() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.Slow);
       super.testPushWhileInSwing();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 22.2)
+   @DeployableTestMethod(estimatedDuration = 22.2)
    @Test(timeout = 110000)
    public void testPushWhileInTransfer() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testPushWhileInTransfer();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 14.7)
+   @DeployableTestMethod(estimatedDuration = 14.7)
    @Test(timeout = 73000)
    public void testPushWhileStanding() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testPushWhileStanding();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 14.4)
+   @DeployableTestMethod(estimatedDuration = 14.4)
    @Test(timeout = 72000)
    public void testPushWhileStandingRecoveringAfterControllerFailureKickedIn() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testPushWhileStandingRecoveringAfterControllerFailureKickedIn();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 14.3)
+   @DeployableTestMethod(estimatedDuration = 14.3)
    @Test(timeout = 71000)
    public void testRecoveringWithSwingSpeedUpWhileInSwing() throws SimulationExceededMaximumTimeException
    {
       TestPlanTarget.assumeRunningOnPlanIfRunningOnBamboo(TestPlanTarget.InDevelopment);
       super.testRecoveringWithSwingSpeedUpWhileInSwing();
    }
-   
+
    @Override
-	@DeployableTestMethod(estimatedDuration = 25.2)
+   @DeployableTestMethod(estimatedDuration = 25.2)
    @Test(timeout = 130000)
    public void testRecoveryWhileInFlamingoStance() throws SimulationExceededMaximumTimeException
    {
