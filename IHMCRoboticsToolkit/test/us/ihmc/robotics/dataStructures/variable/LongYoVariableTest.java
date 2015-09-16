453d8c93fd6b1e0ffb59c70704bd7fc10b0bd744
Duncan Calvert <dcalvert@ihmc.us>
2015-09-16 16:59:40 +0000

    NONE: Move test classes to application class packages. Will fixBamboo.

---
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/dataStructures/variable/LongYoVariableTest.java
@@ -0,0 +1,203 @@
+package us.ihmc.robotics.dataStructures.variable;
+
+import java.util.Random;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.LongYoVariable;
+import us.ihmc.robotics.dataStructures.variable.YoVariableType;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LongYoVariableTest
+{
+   private YoVariableRegistry registry;
+   private Random random;
+   private LongYoVariable longYoVariable;
+   private static final double EPSILON = 1e-10;
+
+   @Before
+   public void setUp()
+   {
+      registry = new YoVariableRegistry("testRegistry");
+      random = new Random(1776L);
+      longYoVariable = new LongYoVariable("test", registry);
+   }
+
+   @After
+   public void tearDown()
+   {
+      registry = null;
+      longYoVariable = null;
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testSetAndGet()
+   {
+      for (int i = 0; i < 100; i++)
+      {
+         long value = random.nextLong();
+         boolean notify = random.nextBoolean();
+         longYoVariable.set(value, notify);
+         assertEquals(value, longYoVariable.getLongValue());
+      }
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testIncrementDecrementAddSubtract()
+   {
+      long value = random.nextLong();
+      longYoVariable.set(value);
+
+      longYoVariable.increment();
+      assertEquals(value + 1, longYoVariable.getLongValue());
+
+      longYoVariable.decrement();
+      assertEquals(value, longYoVariable.getLongValue());
+
+      longYoVariable.add(value);
+      assertEquals(value * 2, longYoVariable.getLongValue());
+
+      longYoVariable.subtract(value);
+      assertEquals(value, longYoVariable.getLongValue());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testLargeValue()
+   {
+      long value = Long.MAX_VALUE - 2;
+      longYoVariable.set(value);
+      assertEquals(value, longYoVariable.getLongValue());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testValueEquals()
+   {
+      assertTrue(longYoVariable.valueEquals(0));
+
+      long number = random.nextLong();
+      longYoVariable.set(number);
+      assertTrue(longYoVariable.valueEquals(number));
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testSetFinal()
+   {
+      assertEquals(0, longYoVariable.getLongValue());
+      longYoVariable.set(0);
+      assertEquals(0, longYoVariable.getLongValue());
+
+      int value = random.nextInt() + 1;
+      longYoVariable.set(value);
+      assertEquals(value, longYoVariable.getLongValue());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testSetValueFromDouble()
+   {
+      double doubleValue = random.nextDouble();
+      int intValue = (int) Math.round(doubleValue);
+      boolean notifyListeners = true;
+      longYoVariable.setValueFromDouble(doubleValue, notifyListeners);
+      assertEquals(intValue, longYoVariable.getLongValue());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testGetValueAsDouble()
+   {
+      assertEquals(0.0, longYoVariable.getValueAsDouble(), EPSILON);
+      long value = 15;
+      longYoVariable.set(value);
+      double result = (double) longYoVariable.getValueAsDouble();
+      assertEquals(15.0, result, EPSILON);
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testToString()
+   {
+      assertEquals(longYoVariable.getName() + ": " + longYoVariable.getLongValue(), longYoVariable.toString());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testGetValueString()
+   {
+      assertEquals(0, longYoVariable.getLongValue());
+      int value = random.nextInt();
+      longYoVariable.set(value);
+      StringBuffer stringBuffer = new StringBuffer();
+      longYoVariable.getValueString(stringBuffer);
+      assertEquals("" + value, stringBuffer.toString());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testGetValueStringFromDouble()
+   {
+      assertEquals(0, longYoVariable.getLongValue());
+      double doubleValue = random.nextDouble();
+      int value = (int) Math.round(doubleValue);
+      longYoVariable.setValueFromDouble(doubleValue);
+      StringBuffer stringBuffer = new StringBuffer();
+      longYoVariable.getValueStringFromDouble(stringBuffer, doubleValue);
+      assertEquals("" + value, stringBuffer.toString());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testGetYoVariableType()
+   {
+      assertEquals(YoVariableType.LONG, longYoVariable.getYoVariableType());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testGetAndSetValueAsLongBits()
+   {
+      int value = 57;
+      longYoVariable.set(value);
+      long result = longYoVariable.getValueAsLongBits();
+      assertEquals(value, result);
+
+      long longValue = 12345;
+      boolean notifyListeners = true;
+      longYoVariable.setValueFromLongBits(longValue, notifyListeners);
+      assertEquals(longValue, longYoVariable.getValueAsLongBits());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testDuplicate()
+   {
+      LongYoVariable longYoVariable2 = new LongYoVariable("var2", "descriptionTest", registry);
+      YoVariableRegistry newRegistry = new YoVariableRegistry("newRegistry");
+      LongYoVariable duplicate = longYoVariable2.duplicate(newRegistry);
+      assertEquals(longYoVariable2.getName(), duplicate.getName());
+      assertEquals(longYoVariable2.getDescription(), duplicate.getDescription());
+      assertEquals(longYoVariable2.getManualScalingMin(), duplicate.getManualScalingMin(), EPSILON);
+      assertEquals(longYoVariable2.getManualScalingMax(), duplicate.getManualScalingMax(), EPSILON);
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testSetValue()
+   {
+      LongYoVariable longYoVariable2 = new LongYoVariable("var2", "descriptionTest", registry);
+      boolean notifyListeners = true;
+      longYoVariable.setValue(longYoVariable2, notifyListeners);
+      assertEquals(longYoVariable2.getLongValue(), longYoVariable.getLongValue());
+   }
+}
---
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/math/trajectories/ConstantPositionTrajectoryGeneratorTest.java
@@ -0,0 +1,170 @@
+package us.ihmc.robotics.math.trajectories;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.geometry.FramePoint;
+import us.ihmc.robotics.geometry.FrameVector;
+import us.ihmc.robotics.math.trajectories.ConstantPositionTrajectoryGenerator;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+import us.ihmc.robotics.trajectories.providers.ConstantPositionProvider;
+import us.ihmc.robotics.trajectories.providers.PositionProvider;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
+
+public class ConstantPositionTrajectoryGeneratorTest
+{
+   private static final double EPSILON = 1e-10;
+
+   private ConstantPositionTrajectoryGenerator generator;
+   private String namePrefix = "namePrefixTEST";
+   private ReferenceFrame referenceFrame;
+   private PositionProvider positionProvider;
+   private FramePoint position;
+   private YoVariableRegistry parentRegistry;
+
+   private static double finalTime = 10.0;
+   private double xValue = Math.random();
+   private double yValue = Math.random();
+   private double zValue = Math.random();
+
+   @Before
+   public void setUp()
+   {
+      referenceFrame = ReferenceFrame.constructARootFrame("rootNameTEST");
+      position = new FramePoint(referenceFrame, xValue, yValue, zValue, "framePoint");
+      positionProvider = new ConstantPositionProvider(position);
+      parentRegistry = new YoVariableRegistry("registry");
+   }
+
+   @After
+   public void tearDown()
+   {
+
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testConstructor()
+   {
+      try
+      {
+         generator = new ConstantPositionTrajectoryGenerator(namePrefix, referenceFrame, positionProvider, finalTime, null);
+         fail();
+      }
+      catch (NullPointerException npe)
+      {
+      }
+
+      generator = new ConstantPositionTrajectoryGenerator(namePrefix, referenceFrame, positionProvider, finalTime, parentRegistry);
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testIsDone()
+   {
+      generator = new ConstantPositionTrajectoryGenerator(namePrefix, referenceFrame, positionProvider, finalTime, parentRegistry);
+      generator.initialize();
+      generator.compute(5.0);
+      assertFalse(generator.isDone());
+
+      generator.compute(finalTime + EPSILON);
+      assertTrue(generator.isDone());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testGet()
+   {
+      generator = new ConstantPositionTrajectoryGenerator(namePrefix, referenceFrame, positionProvider, finalTime, parentRegistry);
+      FramePoint positionToPack = new FramePoint();
+
+      generator.get(positionToPack);
+
+      assertEquals(referenceFrame, positionToPack.getReferenceFrame());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testPackVelocity()
+   {
+      generator = new ConstantPositionTrajectoryGenerator(namePrefix, referenceFrame, positionProvider, finalTime, parentRegistry);
+      FrameVector velocityToPack = new FrameVector(ReferenceFrame.getWorldFrame(), 10.0, 10.0, 10.0);
+
+      assertFalse(referenceFrame.equals(velocityToPack.getReferenceFrame()));
+
+      generator.packVelocity(velocityToPack);
+
+      assertEquals(0.0, velocityToPack.getX(), EPSILON);
+      assertEquals(0.0, velocityToPack.getY(), EPSILON);
+      assertEquals(0.0, velocityToPack.getZ(), EPSILON);
+      assertSame(referenceFrame, velocityToPack.getReferenceFrame());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testPackAcceleration()
+   {
+      generator = new ConstantPositionTrajectoryGenerator(namePrefix, referenceFrame, positionProvider, finalTime, parentRegistry);
+      FrameVector angularAccelerationToPack = new FrameVector(ReferenceFrame.getWorldFrame(), 10.0, 10.0, 10.0);
+
+      assertFalse(referenceFrame.equals(angularAccelerationToPack.getReferenceFrame()));
+
+      generator.packAcceleration(angularAccelerationToPack);
+
+      assertEquals(0.0, angularAccelerationToPack.getX(), EPSILON);
+      assertEquals(0.0, angularAccelerationToPack.getY(), EPSILON);
+      assertEquals(0.0, angularAccelerationToPack.getZ(), EPSILON);
+      assertSame(referenceFrame, angularAccelerationToPack.getReferenceFrame());
+   }
+
+	@DeployableTestMethod
+	@Test(timeout=300000)
+   public void testPackLinearData()
+   {
+      FramePoint positionToPack = new FramePoint(referenceFrame);
+      positionToPack.setIncludingFrame(referenceFrame, 4.4, 3.3, 1.4);
+
+      generator = new ConstantPositionTrajectoryGenerator(namePrefix, referenceFrame, positionProvider, finalTime, parentRegistry);
+      generator.get(positionToPack);
+
+      assertEquals(referenceFrame, positionToPack.getReferenceFrame());
+
+      generator.get(positionToPack);
+
+      assertEquals(referenceFrame, positionToPack.getReferenceFrame());
+
+      FrameVector velocityToPack = new FrameVector(ReferenceFrame.getWorldFrame(), 10.0, 10.0, 10.0);
+      FrameVector accelerationToPack = new FrameVector(ReferenceFrame.getWorldFrame(), 10.0, 10.0, 10.0);
+
+      assertFalse(referenceFrame.equals(velocityToPack.getReferenceFrame()));
+      assertTrue(ReferenceFrame.getWorldFrame().equals(velocityToPack.getReferenceFrame()));
+
+      assertFalse(referenceFrame.equals(accelerationToPack.getReferenceFrame()));
+      assertTrue(ReferenceFrame.getWorldFrame().equals(accelerationToPack.getReferenceFrame()));
+
+      generator.packLinearData(positionToPack, velocityToPack, accelerationToPack);
+
+      assertEquals(0.0, positionToPack.getX(), EPSILON);
+      assertEquals(0.0, positionToPack.getY(), EPSILON);
+      assertEquals(0.0, positionToPack.getZ(), EPSILON);
+      assertSame(referenceFrame, positionToPack.getReferenceFrame());
+
+      assertEquals(0.0, velocityToPack.getX(), EPSILON);
+      assertEquals(0.0, velocityToPack.getY(), EPSILON);
+      assertEquals(0.0, velocityToPack.getZ(), EPSILON);
+      assertSame(referenceFrame, velocityToPack.getReferenceFrame());
+
+      assertEquals(0.0, accelerationToPack.getX(), EPSILON);
+      assertEquals(0.0, accelerationToPack.getY(), EPSILON);
+      assertEquals(0.0, accelerationToPack.getZ(), EPSILON);
+      assertSame(referenceFrame, accelerationToPack.getReferenceFrame());
+   }
+}
