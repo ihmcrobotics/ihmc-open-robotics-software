89c614cc1216b46ccb88eb34f07a9c101d7b6b94
Duncan Calvert <dcalvert@ihmc.us>
2015-09-17 19:56:04 +0000

    NONE: Override test methods. Won't fixBamboo.

--- Atlas/test/us/ihmc/atlas/ObstacleCourseTests/AtlasObstacleCourseEveryBuildTest.java
+++ Atlas/test/us/ihmc/atlas/ObstacleCourseTests/AtlasObstacleCourseEveryBuildTest.java
@@ -1,18 +1,22 @@
 package us.ihmc.atlas.ObstacleCourseTests;
 
+import org.junit.Test;
+
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
 import us.ihmc.darpaRoboticsChallenge.obstacleCourseTests.DRCObstacleCourseEveryBuildTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestClass;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
 import us.ihmc.tools.testing.TestPlanTarget;
 
 @DeployableTestClass(targets = {TestPlanTarget.Fast, TestPlanTarget.VideoA})
 public class AtlasObstacleCourseEveryBuildTest extends DRCObstacleCourseEveryBuildTest
 {
-
    private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, DRCRobotModel.RobotTarget.SCS, false);
+
    @Override
    public DRCRobotModel getRobotModel()
    {
@@ -23,5 +27,21 @@ public class AtlasObstacleCourseEveryBuildTest extends DRCObstacleCourseEveryBui
    public String getSimpleRobotName()
    {
       return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+   
+   @Override
+   @DeployableTestMethod(estimatedDuration = 52.7)
+   @Test(timeout = 260000)
+   public void testSimpleFlatGroundScript() throws SimulationExceededMaximumTimeException
+   {
+      super.testSimpleFlatGroundScript();
+   }
+   
+   @Override
+   @DeployableTestMethod(estimatedDuration = 53.8)
+   @Test(timeout = 270000)
+   public void testWalkingUpToRampWithLongSteps() throws SimulationExceededMaximumTimeException
+   {
+      super.testWalkingUpToRampWithLongSteps();
    }
 }
