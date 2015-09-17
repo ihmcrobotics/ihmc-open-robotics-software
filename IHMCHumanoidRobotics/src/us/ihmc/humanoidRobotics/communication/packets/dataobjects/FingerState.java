e2a030d462311c7105fbc48e09f97a6626a26e00
Nathan Mertins <nmertins@ihmc.us>
2015-09-17 20:23:33 +0000

    ROSI-203: documented FingerState and FingerStatePacket for ROS message generation; won't fixbamboo

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/dataobjects/FingerState.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/dataobjects/FingerState.java
@@ -1,10 +1,12 @@
 package us.ihmc.humanoidRobotics.communication.packets.dataobjects;
 
+import us.ihmc.tools.DocumentedEnum;
+
 /**
 * @author twan
 *         Date: 6/8/13
 */
-public enum FingerState
+public enum FingerState implements DocumentedEnum<FingerState>
 {
    STOP,
    CONNECT,
@@ -36,5 +38,66 @@ public enum FingerState
    PARTIAL_CREEPY_GRASP,
    CREEPY_GRASPING,
    CREEPY_GRASPING_HARD,
-   SLOW_CLOSE
+   SLOW_CLOSE;
+
+   @Override public String getDocumentation(FingerState var)
+   {
+      switch (var)
+      {
+      case STOP:
+         return "stops the fingers at their current position";
+      case OPEN:
+         return "fully opens the fingers";
+      case CLOSE:
+         return "fully closes the fingers";
+      case CRUSH:
+         return "fully closes the fingers applying maximum force";
+      case HOOK:
+         return "closes all but one finger to create a hook";
+      case BASIC_GRIP:
+         return "sets gripper to use a standard grasp";
+      case PINCH_GRIP:
+         return "sets gripper to use a pinch grasp where the thumb and fingers come together when closed";
+      case WIDE_GRIP:
+         return "sets gripper to use a wide-spread finger grasp";
+      case SCISSOR_GRIP:
+         return "sets gripper to use a scissor grasp where the index and middle finger come together when closed";
+      case RESET:
+         return "";
+      case OPEN_FINGERS:
+         return "";
+      case OPEN_THUMB:
+         return "";
+      case CLOSE_FINGERS:
+         return "";
+      case CLOSE_THUMB:
+         return "";
+      case CRUSH_THUMB:
+         return "";
+      default:
+         return "no documentation available";
+      }
+   }
+
+   @Override public FingerState[] getDocumentedValues()
+   {
+      return new FingerState[]
+            {
+                  STOP,
+                  OPEN,
+                  CLOSE,
+                  CRUSH,
+                  HOOK,
+                  BASIC_GRIP,
+                  PINCH_GRIP,
+                  WIDE_GRIP,
+                  SCISSOR_GRIP,
+                  RESET,
+                  OPEN_FINGERS,
+                  OPEN_THUMB,
+                  CLOSE_FINGERS,
+                  CLOSE_THUMB,
+                  CRUSH_THUMB
+            };
+   }
 }
--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/manipulation/FingerStatePacket.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/manipulation/FingerStatePacket.java
@@ -2,13 +2,19 @@ package us.ihmc.humanoidRobotics.communication.packets.manipulation;
 
 import java.util.Random;
 
+import us.ihmc.communication.packetAnnotations.ClassDocumentation;
+import us.ihmc.communication.packetAnnotations.FieldDocumentation;
+import us.ihmc.communication.packets.IHMCRosApiPacket;
 import us.ihmc.communication.packets.Packet;
 import us.ihmc.humanoidRobotics.communication.packets.dataobjects.FingerState;
 import us.ihmc.robotics.robotSide.RobotSide;
 
-public class FingerStatePacket extends Packet<FingerStatePacket>
+@ClassDocumentation(value = "Packet for commanding the hands to perform various predefined grasps.")
+public class FingerStatePacket extends IHMCRosApiPacket<FingerStatePacket>
 {
+   @FieldDocumentation(value = "Specifies the side of the robot that will execute the trajectory")
    public RobotSide robotSide;
+   @FieldDocumentation(value = "Specifies the grasp to perform")
    public FingerState fingerState;
 
    public FingerStatePacket()
