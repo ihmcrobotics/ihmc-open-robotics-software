264aca21a38c490232865054d421ee6a0f38c1ac
Daniel Duran <dduran@ihmc.us>
2015-09-17 21:50:00 +0000

    NONE: fixBamboo - no fix - ROSI-100 : added unit tests for PDController.java (computeForAngles)

--- IHMCRoboticsToolkit/test/us/ihmc/robotics/controllers/PDControllerTest.java
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/controllers/PDControllerTest.java
@@ -35,13 +35,34 @@ public class PDControllerTest
          value = pdController.computeForAngles(current, desired, 0, 0);
          assertEquals(desired, value, 1e-6);
       }
-      
+
       for (int i = 0; i < 100; i++)
       {
          double current = Math.PI;
          double desired = random.nextDouble() * Math.PI + Math.PI;
          value = pdController.computeForAngles(current, desired, 0, 0);
          assertEquals(desired - Math.PI, value, 1e-6);
+      }
+   }
+
+   @DeployableTestMethod(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
+   public void testAngleBoundaries()
+   {
+      PDController pdController = new PDController("suffix", new YoVariableRegistry("testRegistry"));
+      pdController.setDerivativeGain(1.0);
+      pdController.setProportionalGain(1.0);
+      
+      Random random = new Random();
+      
+      for(int i = 0; i < 1000; i++)
+      {
+          double current = Math.PI - random.nextDouble()*Math.toRadians(10);
+          double desired = -(Math.PI - random.nextDouble()*Math.toRadians(10));
+          double value = pdController.computeForAngles(current, desired, 0, 0);
+          double error = (Math.PI - current) + (Math.PI - Math.abs(desired));
+          
+          assertEquals((Math.PI - current) + (Math.PI - Math.abs(desired)), value, 1e-6);
       }
    }
 }
