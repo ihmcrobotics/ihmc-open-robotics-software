f41754a9fe0cbb8cc98b8322309b7464489575d6
Elena Galbally <egalbally@ihmc.us>
2015-09-22 23:48:14 +0000

    NONE: Won't FixBamboo.

--- ExampleSimulations/src/us/ihmc/exampleSimulations/simpleDynamicWalkingExample/Step6Simulation.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/simpleDynamicWalkingExample/Step6Simulation.java
@@ -5,28 +5,32 @@ import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 public class Step6Simulation
 {
 
+   /**
+    *  Walking robot with an ICP visualizer
+    */
+   
    //Variables 
    private SimulationConstructionSet sim;
-   private double deltaT = 0.0001;
-   private int recordFrequency = 10;
+   private double deltaT = 1e-4;
+   private int recordFrequency = 80;
    
    public Step6Simulation()
    {
       //Robot
-      Step6IDandSCSRobot walkingRobot = new Step6IDandSCSRobot();
+      Step5IDandSCSRobot_pinKnee v5Robot = new Step5IDandSCSRobot_pinKnee();
       
       //Controller 
-      Step6WalkingController_SpringFlamInspired walkingController = new Step6WalkingController_SpringFlamInspired(walkingRobot, "v5Robot", deltaT);
-      walkingRobot.setController(walkingController);
+      Step5WalkingController v5Controller = new Step5WalkingController(v5Robot, "v5Robot", deltaT);
+      v5Robot.setController(v5Controller);
       
       //Simulation Object  
-      sim = new SimulationConstructionSet(walkingRobot);
+      sim = new SimulationConstructionSet(v5Robot);
       sim.setGroundVisible(true);
       sim.setDT(deltaT, recordFrequency);
       sim.setCameraFix(0.0, 0.025, 1.05);
-      sim.setCameraPosition(12.6, -15.4, 2.3);
+      sim.setCameraPosition(0.8643, -17.188, 2.72);
       sim.changeBufferSize(32000);
-      sim.selectConfiguration("Step5GUI.guiConf");
+      sim.selectConfiguration("step5Config.guiConf");
 
       Thread myThread = new Thread(sim);
       myThread.start();
