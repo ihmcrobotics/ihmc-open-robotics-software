3721fdc571b2a1f541fe585f2a3a638d9c73fbad
Nathan Mertins <nmertins@ihmc.us>
2015-09-23 14:54:45 +0000

    ROSI-139: small refactor to CalibrationProperties and added some more specific unit tests; won't fixbamboo

--- IHMCJavaToolkit/test/us/ihmc/tools/calibration/CalibrationPropertiesTest.java
+++ IHMCJavaToolkit/test/us/ihmc/tools/calibration/CalibrationPropertiesTest.java
@@ -3,6 +3,7 @@ package us.ihmc.tools.calibration;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.Before;
@@ -35,12 +36,41 @@ public class CalibrationPropertiesTest
 
 	@DeployableTestMethod(estimatedDuration = 0.0)
 	@Test(timeout = 30000)
-   public void testSimple()
+   public void testSetIntegerProperty()
    {
+      Random random = new Random(1984L);
+      int ITERATIONS = 100;
+
       String key = "abcd";
-      int value = 23;
-      calibrationProperties.setProperty(key, value);
-      assertEquals(value, calibrationProperties.getIntegerProperty(key));
+
+      for(int i = 0; i < ITERATIONS; i++)
+      {
+         int value = random.nextInt();
+         calibrationProperties.setProperty(key, value);
+         assertEquals(value, calibrationProperties.getIntegerProperty(key));
+      }
+
+      assertEquals(0, calibrationProperties.getIntegerProperty("notAKey"));
+   }
+
+   @DeployableTestMethod(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
+   public void testSetDoubleProperty()
+   {
+      final Random random = new Random(1984L);
+      final int ITERATIONS = 100;
+      final double EPSILON = 0.000001;
+
+      String key = "abcd";
+
+      for(int i = 0; i < ITERATIONS; i++)
+      {
+         double value = random.nextDouble();
+         calibrationProperties.setProperty(key, value);
+         assertEquals(value, calibrationProperties.getDoubleProperty(key), EPSILON);
+      }
+
+      assertEquals(0.0, calibrationProperties.getDoubleProperty("notAKey"), EPSILON);
    }
 
 	@DeployableTestMethod(estimatedDuration = 0.1)
@@ -51,7 +81,7 @@ public class CalibrationPropertiesTest
       int value = 23;
       calibrationProperties.setProperty(key, value);
       calibrationProperties.save();
-      
+
       CalibrationProperties calibrationProperties2 = new CalibrationProperties(".", fileName);
       assertEquals(value, calibrationProperties2.getIntegerProperty(key));
    }
