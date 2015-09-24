d8b00b16b49cc9ff0437278acf0e8d3f905e9e22
Duncan Calvert <dcalvert@ihmc.us>
2015-09-24 19:22:11 +0000

    ROSI-139: Code coverage for PairList. Won't fixBamboo.

--- IHMCJavaToolkit/test/us/ihmc/tools/generatedTestSuites/IHMCJavaToolkitAFastTestSuite.java
+++ IHMCJavaToolkit/test/us/ihmc/tools/generatedTestSuites/IHMCJavaToolkitAFastTestSuite.java
@@ -35,6 +35,7 @@ import us.ihmc.tools.testing.TestPlanTarget;
    us.ihmc.tools.io.logging.LogToolsTest.class,
    us.ihmc.tools.io.printing.PrintToolsTest.class,
    us.ihmc.tools.io.StreamGobblerTest.class,
+   us.ihmc.tools.lists.PairListTest.class,
    us.ihmc.tools.processManagement.ProcessSpawnerTest.class,
    us.ihmc.tools.processManagement.ProcessToolsTest.class,
    us.ihmc.tools.reflect.RecursiveObjectComparerTest.class,
