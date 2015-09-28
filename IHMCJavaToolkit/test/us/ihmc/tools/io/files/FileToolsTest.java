38d48e7e293a907d864f4645303fffe8c5fbf299
Duncan Calvert <dcalvert@ihmc.us>
2015-09-28 17:48:55 +0000

    ROSI-139: Update FileToolsTest to use contants for filenames. Add txt extension to generated files so they don't affect Clover. Won't fixBamboo.

--- IHMCJavaToolkit/test/us/ihmc/tools/io/files/FileToolsTest.java
+++ IHMCJavaToolkit/test/us/ihmc/tools/io/files/FileToolsTest.java
@@ -21,7 +21,6 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import us.ihmc.tools.io.files.FileTools;
 import us.ihmc.tools.io.printing.PrintTools;
 import us.ihmc.tools.testing.JUnitTools;
 import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
@@ -36,6 +35,13 @@ public class FileToolsTest
    private static final String EXAMPLE_FILE_1_TEXT_LINE_1 = "This is example File 1 !!&&#))(";
    private static final String EXAMPLE_FILE_2_TEXT_LINE_1 = "This is example File 2 *@&&%*@";
    private static final String EXAMPLE_FILE_2_TEXT_LINE_2 = "It has two lines";
+   private static final String EXAMPLE_JAVA_FILE2_JAVA = "ExampleJavaFile2.java";
+   private static final String EXAMPLE_JAVA_FILE1_JAVA = "ExampleJavaFile1.java";
+   private static final String TEST_FILE_BAD_TXT = "testFileBad.txt";
+   private static final String FILE_TOOLS_EXAMPLE_FILE_CAT_TXT = "FileToolsExampleFileCat.txt";
+   private static final String FILE_TOOLS_EXAMPLE_FILE2_TXT = "FileToolsExampleFile2.txt";
+   private static final String FILE_TOOLS_EXAMPLE_FILE1_TXT = "FileToolsExampleFile1.txt";
+   private static final String TEST_READ_ALL_LINES_TXT = "testReadAllLines.txt";
 
    @Before
    public void setUp()
@@ -57,7 +63,7 @@ public class FileToolsTest
    {
       String exampleFile = "line1\nline2\nline3\n";
       
-      Path testReadAllLinesPath = FILE_TOOLS_TEST_PATH.resolve("testReadAllLines.txt");
+      Path testReadAllLinesPath = FILE_TOOLS_TEST_PATH.resolve(TEST_READ_ALL_LINES_TXT);
       
       PrintWriter writer = FileTools.newPrintWriter(testReadAllLinesPath);
       writer.print(exampleFile);
@@ -83,9 +89,9 @@ public class FileToolsTest
    @Test(timeout = 30000)
    public void testConcatenateFilesTogether()
    {      
-      Path concatFile1 = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile1.txt");
-      Path concatFile2 = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile2.txt");
-      Path concatedFile = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFileCat.txt");
+      Path concatFile1 = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE1_TXT);
+      Path concatFile2 = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE2_TXT);
+      Path concatedFile = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE_CAT_TXT);
 
       List<Path> filesToConcat = new ArrayList<Path>();
       filesToConcat.add(concatFile1);
@@ -113,7 +119,7 @@ public class FileToolsTest
 
    private static void createTestFile1()
    {
-      PrintWriter writer = FileTools.newPrintWriter(TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile1.txt"));
+      PrintWriter writer = FileTools.newPrintWriter(TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE1_TXT));
       writer.println(EXAMPLE_FILE_1_TEXT_LINE_1);
       writer.flush();
       writer.close();
@@ -121,7 +127,7 @@ public class FileToolsTest
 
    private static void createTestFile2()
    {
-      PrintWriter writer = FileTools.newPrintWriter(TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile2.txt"));
+      PrintWriter writer = FileTools.newPrintWriter(TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE2_TXT));
       writer.println(EXAMPLE_FILE_2_TEXT_LINE_1);
       writer.println(EXAMPLE_FILE_2_TEXT_LINE_2);
       writer.flush();
@@ -130,7 +136,7 @@ public class FileToolsTest
 
    private static void createJavaFile1()
    {
-      PrintWriter writer = FileTools.newPrintWriter(JAVA_DIRECTORY_PATH.resolve("ExampleJavaFile1.java"));
+      PrintWriter writer = FileTools.newPrintWriter(JAVA_DIRECTORY_PATH.resolve(EXAMPLE_JAVA_FILE1_JAVA));
       writer.println("// This is a comment");
       writer.println("package us.ihmc.tools.io.files.fileToolsTest.exampleJavaFiles;");
       writer.println("public class ExampleJavaFile1");
@@ -147,7 +153,7 @@ public class FileToolsTest
 
    private static void createJavaFile2()
    {
-      PrintWriter writer = FileTools.newPrintWriter(JAVA_DIRECTORY_PATH.resolve("ExampleJavaFile2.java"));
+      PrintWriter writer = FileTools.newPrintWriter(JAVA_DIRECTORY_PATH.resolve(EXAMPLE_JAVA_FILE2_JAVA));
       writer.println("package us.ihmc.tools.io.files.fileToolsTest.exampleJavaFiles;");
       writer.println("public class ExampleJavaFile2");
       writer.println("{");
@@ -166,7 +172,7 @@ public class FileToolsTest
    @Test(timeout = 30000)
    public void testGetBufferedReader()
    {
-      File testFile1 = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile1.txt").toFile();
+      File testFile1 = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE1_TXT).toFile();
       if (!testFile1.exists())
          createTestFile1();
    
@@ -192,14 +198,14 @@ public class FileToolsTest
    @Test(timeout = 30000, expected = FileNotFoundException.class)
    public void testGetBufferedReaderWithFileNotFoundException() throws FileNotFoundException
    {
-      BufferedReader reader = FileTools.getFileReader("testFileBad.txt");
+      BufferedReader reader = FileTools.getFileReader(TEST_FILE_BAD_TXT);
    }
 
 	@DeployableTestMethod(estimatedDuration = 0.0)
    @Test(timeout = 30000)
    public void testGetFileWriter()
    {
-      File testFile1 = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile1.txt").toFile();
+      File testFile1 = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE1_TXT).toFile();
       if (!testFile1.exists())
          createTestFile1();
    
@@ -228,7 +234,7 @@ public class FileToolsTest
    @Test(timeout = 30000)
    public void testGetFileWriterWithAppend()
    {
-      File testFile1 = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile1.txt").toFile();
+      File testFile1 = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE1_TXT).toFile();
       if (!testFile1.exists())
          createTestFile1();
    
@@ -257,7 +263,7 @@ public class FileToolsTest
    @Test(timeout = 30000)
    public void testGetFileDataOutputStream()
    {
-      File testFile1 = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile1.txt").toFile();
+      File testFile1 = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE1_TXT).toFile();
       if (!testFile1.exists())
          createTestFile1();
    
@@ -286,7 +292,7 @@ public class FileToolsTest
    @Test(timeout = 30000)
    public void testGetFileDataInputStream()
    {
-      File testFile1 = TEXT_DIRECTORY_PATH.resolve("FileToolsExampleFile1.txt").toFile();
+      File testFile1 = TEXT_DIRECTORY_PATH.resolve(FILE_TOOLS_EXAMPLE_FILE1_TXT).toFile();
       if (!testFile1.exists())
          createTestFile1();
    
@@ -312,7 +318,7 @@ public class FileToolsTest
    @Test(timeout = 30000, expected = FileNotFoundException.class)
    public void testGetFileDataInputStreamWithFileNotFoundException() throws FileNotFoundException, IOException
    {
-      DataInputStream inStream = FileTools.getFileDataInputStream("testFileBad.txt");
+      DataInputStream inStream = FileTools.getFileDataInputStream(TEST_FILE_BAD_TXT);
    }
 
 	@DeployableTestMethod(estimatedDuration = 0.0)
@@ -329,13 +335,13 @@ public class FileToolsTest
          errorMessage = errorMessage + f.getName() + " ";
       }
    
-      assertTrue(listOfFileNames.contains("FileToolsExampleFile1.txt"));
-      assertTrue(listOfFileNames.contains("FileToolsExampleFile2.txt"));
-      assertTrue(listOfFileNames.contains("ExampleJavaFile1.java"));
-      assertTrue(listOfFileNames.contains("ExampleJavaFile2.java"));
+      assertTrue(listOfFileNames.contains(FILE_TOOLS_EXAMPLE_FILE1_TXT));
+      assertTrue(listOfFileNames.contains(FILE_TOOLS_EXAMPLE_FILE2_TXT));
+      assertTrue(listOfFileNames.contains(EXAMPLE_JAVA_FILE1_JAVA));
+      assertTrue(listOfFileNames.contains(EXAMPLE_JAVA_FILE2_JAVA));
    
       boolean thrown = false;
-      File notADirectoryExceptionTestFile = JAVA_DIRECTORY_PATH.resolve("ExampleJavaFile1.java").toFile();
+      File notADirectoryExceptionTestFile = JAVA_DIRECTORY_PATH.resolve(EXAMPLE_JAVA_FILE1_JAVA).toFile();
    
       try
       {
@@ -354,7 +360,7 @@ public class FileToolsTest
    public void testGetAllFilesInDirectoryWithSuffix()
    {
       File textFileDirectory = TEXT_DIRECTORY_PATH.toFile();
-      File notADirectoryExceptionTestFile = JAVA_DIRECTORY_PATH.resolve("ExampleJavaFile1.java").toFile();
+      File notADirectoryExceptionTestFile = JAVA_DIRECTORY_PATH.resolve(EXAMPLE_JAVA_FILE1_JAVA).toFile();
       File javaFileDirectory = JAVA_DIRECTORY_PATH.toFile();
       ArrayList<File> textFiles = FileTools.getAllFilesInDirectoryWithSuffix("txt", textFileDirectory);
       ArrayList<File> javaFiles = FileTools.getAllFilesInDirectoryWithSuffix("java", javaFileDirectory);
@@ -381,10 +387,10 @@ public class FileToolsTest
       assertEquals(errorMessage, 2, javaFiles.size());
    
    
-      assertTrue(listOfTextFileNames.contains("FileToolsExampleFile1.txt"));
-      assertTrue(listOfTextFileNames.contains("FileToolsExampleFile2.txt"));
-      assertTrue(listOfJavaFileNames.contains("ExampleJavaFile1.java"));
-      assertTrue(listOfJavaFileNames.contains("ExampleJavaFile2.java"));
+      assertTrue(listOfTextFileNames.contains(FILE_TOOLS_EXAMPLE_FILE1_TXT));
+      assertTrue(listOfTextFileNames.contains(FILE_TOOLS_EXAMPLE_FILE2_TXT));
+      assertTrue(listOfJavaFileNames.contains(EXAMPLE_JAVA_FILE1_JAVA));
+      assertTrue(listOfJavaFileNames.contains(EXAMPLE_JAVA_FILE2_JAVA));
    
       // test non directory search
       boolean thrown = false;
