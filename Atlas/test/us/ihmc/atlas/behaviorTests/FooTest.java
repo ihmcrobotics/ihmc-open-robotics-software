8522c4dde3e2531806f220e7882eb442c07fc86f
Duncan Calvert <dcalvert@ihmc.us>
2015-10-01 18:46:37 +0000

    ROSI-220: Exclude FooTest. fixBamboo.

--- Atlas/test/us/ihmc/atlas/behaviorTests/FooTest.java
+++ Atlas/test/us/ihmc/atlas/behaviorTests/FooTest.java
@@ -1,21 +1,24 @@
 package us.ihmc.atlas.behaviorTests;
 
+import static us.ihmc.tools.testing.TestPlanTarget.Exclude;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import us.ihmc.tools.MemoryTools;
-import us.ihmc.tools.testing.TestPlanAnnotations;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestClass;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
 
+@DeployableTestClass(targets = Exclude)
 public class FooTest
 {
-   
    @Before
    public void showMemoryUsageBeforeTest()
    {
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
    }
 
-   @TestPlanAnnotations.DeployableTestMethod(estimatedDuration = 0.0)
+   @DeployableTestMethod(estimatedDuration = 0.0)
    @Test(timeout = 5000)
    public void fooTest() throws InterruptedException
    {
