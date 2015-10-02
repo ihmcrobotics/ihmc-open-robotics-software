0f4603094f3072aab3e0346b958d1568c097b549
Nicolas Eyssette <neyssette@ihmc.us>
2015-10-02 20:31:12 +0000

    NONE: added the update method to the AlphaFilteredWrappingYoVariable. removed some methods that was only used for debug. does not fixbamboo

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/math/filters/AlphaFilteredWrappingYoVariable.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/math/filters/AlphaFilteredWrappingYoVariable.java
@@ -4,7 +4,6 @@ import us.ihmc.robotics.dataStructures.listener.VariableChangedListener;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
 import us.ihmc.robotics.dataStructures.variable.YoVariable;
-import us.ihmc.robotics.geometry.AngleTools;
 
 public class AlphaFilteredWrappingYoVariable extends AlphaFilteredYoVariable
 {
@@ -12,44 +11,48 @@ public class AlphaFilteredWrappingYoVariable extends AlphaFilteredYoVariable
 
    private final DoubleYoVariable unfilteredVariable;
    private final DoubleYoVariable alphaVariable;
-   
-   private final DoubleYoVariable shiftedVariable;
+
+   private final DoubleYoVariable temporaryOutputVariable;
    private final DoubleYoVariable error;
    private final double upperLimit;
    private final double lowerLimit;
-   
-   public AlphaFilteredWrappingYoVariable(String name, String description, YoVariableRegistry registry, final DoubleYoVariable unfilteredVariable, DoubleYoVariable alphaVariable, double lowerLimit, double upperLimit)
+   private final double range;
+
+   public AlphaFilteredWrappingYoVariable(String name, String description, YoVariableRegistry registry, final DoubleYoVariable unfilteredVariable,
+         DoubleYoVariable alphaVariable, double lowerLimit, double upperLimit)
    {
       super(name, description, registry, alphaVariable);
       this.alphaVariable = alphaVariable;
       this.upperLimit = upperLimit;
       this.lowerLimit = lowerLimit;
-      
+      this.range = upperLimit - lowerLimit;
+
       this.unfilteredVariable = unfilteredVariable;
       this.unfilteredVariable.addVariableChangedListener(new VariableChangedListener()
       {
          double range = AlphaFilteredWrappingYoVariable.this.upperLimit - AlphaFilteredWrappingYoVariable.this.lowerLimit;
-         
+
          @Override
          public void variableChanged(YoVariable<?> v)
          {
             //calculate the error
-            double tempError = unfilteredVariable.getDoubleValue() - getDoubleValue();
-            tempError = tempError % range;
-            tempError = shiftErrorToStartOfRange(tempError, AlphaFilteredWrappingYoVariable.this.lowerLimit);
-            error.set(tempError);
-            
-            //determine if wrapping and move the input if necessary
-            shiftedVariable.set(getDoubleValue());
-            if((getDoubleValue() + tempError) > AlphaFilteredWrappingYoVariable.this.upperLimit)
+            if (hasBeenCalled.getBooleanValue())
             {
-               shiftedVariable.set(getDoubleValue() - range);
-               System.out.println("over");
-            }
-            if((getDoubleValue() + tempError) <= AlphaFilteredWrappingYoVariable.this.lowerLimit)
-            {
-               shiftedVariable.set(getDoubleValue() + range);
-               System.out.println("under");
+               double tempError = unfilteredVariable.getDoubleValue() - getDoubleValue();
+               tempError = tempError % range;
+               tempError = shiftErrorToStartOfRange(tempError, AlphaFilteredWrappingYoVariable.this.lowerLimit);
+               error.set(tempError);
+
+               //determine if wrapping and move the input if necessary
+               temporaryOutputVariable.set(getDoubleValue());
+               if ((getDoubleValue() + tempError) > AlphaFilteredWrappingYoVariable.this.upperLimit)
+               {
+                  temporaryOutputVariable.set(getDoubleValue() - range);
+               }
+               if ((getDoubleValue() + tempError) <= AlphaFilteredWrappingYoVariable.this.lowerLimit)
+               {
+                  temporaryOutputVariable.set(getDoubleValue() + range);
+               }
             }
          }
 
@@ -71,32 +74,33 @@ public class AlphaFilteredWrappingYoVariable extends AlphaFilteredYoVariable
             return ret;
          }
       });
-      
-      shiftedVariable = new DoubleYoVariable(name + "shiftedVariable", registry);
+
+      temporaryOutputVariable = new DoubleYoVariable(name + "TemporaryOutputVariable", registry);
       error = new DoubleYoVariable(name + "Error", registry);
    }
 
    @Override
    public void update()
    {
-      
+      if (!hasBeenCalled.getBooleanValue())
+      {
+         hasBeenCalled.set(true);
+         temporaryOutputVariable.set(unfilteredVariable.getDoubleValue());
+         set(unfilteredVariable.getDoubleValue());
+      }
+      else
+      {
+         temporaryOutputVariable.set(alphaVariable.getDoubleValue() * temporaryOutputVariable.getDoubleValue() + (1.0 - alphaVariable.getDoubleValue()) * unfilteredVariable.getDoubleValue());
+
+         set(temporaryOutputVariable.getDoubleValue());
+         if (temporaryOutputVariable.getDoubleValue() > upperLimit)
+         {
+            set(temporaryOutputVariable.getDoubleValue() - range);
+         }
+         if (temporaryOutputVariable.getDoubleValue() <= lowerLimit)
+         {
+            set(temporaryOutputVariable.getDoubleValue() + range);
+         }
+      }
    }
-   
-   public double getError()
-   {
-      return error.getDoubleValue();
-   }
-   
-   public double getShiftedVariable()
-   {
-      return shiftedVariable.getDoubleValue();
-   }
-   
-   public static void main (String args[])
-   {
-      System.out.println(AngleTools.computeAngleDifferenceMinusPiToPi(Math.toRadians(45.0 + 90.0), Math.toRadians(-45.0 + 90.0)));
-      System.out.println(AngleTools.computeAngleDifferenceMinusPiToPi(Math.toRadians(-45.0 + 90.0), Math.toRadians(45.0 + 90.0)));
-   }
-   
-   //listener on the unfiltered to calculate the error, check if there will be a wrapping, and move the correctedUnfiltered to the proper spot
 }
