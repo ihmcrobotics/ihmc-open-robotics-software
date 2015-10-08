c6a682e90397c5dd1bc6a7bbd07ebfa8c73ad3d1
Nicolas Eyssette <neyssette@ihmc.us>
2015-10-08 16:35:37 +0000

    NONE: changed the way a new unfiltered value is handled. Added a modulo of the input. Made sure the setter of the filtered variable was only called once.

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/math/filters/AlphaFilteredWrappingYoVariable.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/math/filters/AlphaFilteredWrappingYoVariable.java
@@ -1,106 +1,134 @@
 package us.ihmc.robotics.math.filters;
 
-import us.ihmc.robotics.dataStructures.listener.VariableChangedListener;
+import us.ihmc.robotics.MathTools;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
-import us.ihmc.robotics.dataStructures.variable.YoVariable;
 
 public class AlphaFilteredWrappingYoVariable extends AlphaFilteredYoVariable
 {
    public static final double EPSILON = 1e-10;
 
+   private double previousUnfilteredVariable;
    private final DoubleYoVariable unfilteredVariable;
+   private final DoubleYoVariable unfilteredInRangeVariable;
    private final DoubleYoVariable alphaVariable;
-
+   
    private final DoubleYoVariable temporaryOutputVariable;
    private final DoubleYoVariable error;
    private final double upperLimit;
    private final double lowerLimit;
    private final double range;
-
-   public AlphaFilteredWrappingYoVariable(String name, String description, YoVariableRegistry registry, final DoubleYoVariable unfilteredVariable,
-         DoubleYoVariable alphaVariable, double lowerLimit, double upperLimit)
+   
+   //wrap the values in [lowerLimit ; upperLimit[
+   
+   public AlphaFilteredWrappingYoVariable(String name, String description, YoVariableRegistry registry, final DoubleYoVariable unfilteredVariable, DoubleYoVariable alphaVariable, double lowerLimit, double upperLimit)
    {
       super(name, description, registry, alphaVariable);
       this.alphaVariable = alphaVariable;
       this.upperLimit = upperLimit;
       this.lowerLimit = lowerLimit;
       this.range = upperLimit - lowerLimit;
-
       this.unfilteredVariable = unfilteredVariable;
-      this.unfilteredVariable.addVariableChangedListener(new VariableChangedListener()
-      {
-         double range = AlphaFilteredWrappingYoVariable.this.upperLimit - AlphaFilteredWrappingYoVariable.this.lowerLimit;
 
-         @Override
-         public void variableChanged(YoVariable<?> v)
-         {
-            //calculate the error
-            if (hasBeenCalled.getBooleanValue())
-            {
-               double tempError = unfilteredVariable.getDoubleValue() - getDoubleValue();
-               tempError = tempError % range;
-               tempError = shiftErrorToStartOfRange(tempError, AlphaFilteredWrappingYoVariable.this.lowerLimit);
-               error.set(tempError);
-
-               //determine if wrapping and move the input if necessary
-               temporaryOutputVariable.set(getDoubleValue());
-               if ((getDoubleValue() + tempError) > AlphaFilteredWrappingYoVariable.this.upperLimit)
-               {
-                  temporaryOutputVariable.set(getDoubleValue() - range);
-               }
-               if ((getDoubleValue() + tempError) <= AlphaFilteredWrappingYoVariable.this.lowerLimit)
-               {
-                  temporaryOutputVariable.set(getDoubleValue() + range);
-               }
-            }
-         }
-
-         private double shiftErrorToStartOfRange(double error, double startOfRange)
-         {
-            double ret = error;
-            startOfRange = startOfRange - EPSILON;
-
-            if (error < startOfRange)
-            {
-               ret = error + Math.ceil((startOfRange - error) / range) * range;
-            }
-
-            if (error >= (startOfRange + range))
-            {
-               ret = error - Math.floor((error - startOfRange) / range) * range;
-            }
-
-            return ret;
-         }
-      });
-
+      unfilteredInRangeVariable = new DoubleYoVariable(name + "UnfilteredInRangeVariable", registry);
       temporaryOutputVariable = new DoubleYoVariable(name + "TemporaryOutputVariable", registry);
       error = new DoubleYoVariable(name + "Error", registry);
+      
    }
-
+   
    @Override
    public void update()
    {
-      if (!hasBeenCalled.getBooleanValue())
+      if(!hasBeenCalled.getBooleanValue())
       {
          hasBeenCalled.set(true);
          temporaryOutputVariable.set(unfilteredVariable.getDoubleValue());
-         set(unfilteredVariable.getDoubleValue());
+         previousUnfilteredVariable = unfilteredVariable.getDoubleValue();
+         
+         unfilteredVariableModulo();
+         set(unfilteredInRangeVariable.getDoubleValue());
       }
       else
       {
-         temporaryOutputVariable.set(alphaVariable.getDoubleValue() * temporaryOutputVariable.getDoubleValue() + (1.0 - alphaVariable.getDoubleValue()) * unfilteredVariable.getDoubleValue());
+         if (!MathTools.epsilonEquals(unfilteredVariable.getDoubleValue(), previousUnfilteredVariable, EPSILON))
+         {
+            previousUnfilteredVariable = unfilteredVariable.getDoubleValue();
+            
+            unfilteredVariableModulo();
+            
+            //calculate the error
+            double tempError = unfilteredInRangeVariable.getDoubleValue() - getDoubleValue();
+            tempError = tempError % range;
+            tempError = shiftErrorToStartOfRange(tempError, lowerLimit);
+            error.set(tempError);
 
-         set(temporaryOutputVariable.getDoubleValue());
-         if (temporaryOutputVariable.getDoubleValue() > upperLimit)
+            //determine if wrapping and move the input if necessary
+            temporaryOutputVariable.set(getDoubleValue());
+            if ((getDoubleValue() + tempError) >= upperLimit)
+            {
+               temporaryOutputVariable.set(getDoubleValue() - range);
+            }
+            if ((getDoubleValue() + tempError) < lowerLimit)
+            {
+               temporaryOutputVariable.set(getDoubleValue() + range);
+            }
+         }
+         
+         temporaryOutputVariable.set(alphaVariable.getDoubleValue() * temporaryOutputVariable.getDoubleValue() + (1.0 - alphaVariable.getDoubleValue())
+               * unfilteredInRangeVariable.getDoubleValue());
+
+         if (temporaryOutputVariable.getDoubleValue() > upperLimit + EPSILON)
          {
             set(temporaryOutputVariable.getDoubleValue() - range);
          }
-         if (temporaryOutputVariable.getDoubleValue() <= lowerLimit)
+         else if (temporaryOutputVariable.getDoubleValue() <= lowerLimit - EPSILON)
          {
             set(temporaryOutputVariable.getDoubleValue() + range);
          }
+         else
+         {
+            set(temporaryOutputVariable.getDoubleValue());
+         }
       }
+   }
+
+   private void unfilteredVariableModulo()
+   {
+      //handle if the input is out of range
+      boolean rangeNeedsToBeChecked = true;
+      unfilteredInRangeVariable.set(unfilteredVariable.getDoubleValue());
+      
+      while(rangeNeedsToBeChecked)
+      {
+         rangeNeedsToBeChecked = false;
+         if (unfilteredInRangeVariable.getDoubleValue() >= upperLimit)
+         {
+            unfilteredInRangeVariable.set(unfilteredInRangeVariable.getDoubleValue() - range);
+            rangeNeedsToBeChecked = true;
+         }
+         if (unfilteredInRangeVariable.getDoubleValue() < lowerLimit)
+         {
+            unfilteredInRangeVariable.set(unfilteredInRangeVariable.getDoubleValue() + range);
+            rangeNeedsToBeChecked = true;
+         }
+      }
+   }
+   
+   private double shiftErrorToStartOfRange(double error, double startOfRange)
+   {
+      double ret = error;
+      startOfRange = startOfRange - EPSILON;
+
+      if (error < startOfRange)
+      {
+         ret = error + Math.ceil((startOfRange - error) / range) * range;
+      }
+
+      if (error >= (startOfRange + range))
+      {
+         ret = error - Math.floor((error - startOfRange) / range) * range;
+      }
+
+      return ret;
    }
 }
