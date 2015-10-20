35c0abc75bd2063d4a60b3f7e8fbe7c4851a3fbc
AtlasControlComputer <dcalvert@ihmc.us>
2015-10-20 18:03:29 -0500

    ROSI-223: Updated deploy script to deploy IMU scripts and executables

---
+++ launchScripts/runMicrostrainSensors.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd `dirname $0`
+pkill MicrostrainMip
+
+./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47092-if00
+./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47094-if00
+
+cp MicrostrainMipServer MicrostrainMipServer.run
+
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47092-if00 -h 127.0.0.1 -p 50092 2> /dev/null &
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47094-if00 -h 127.0.0.1 -p 50094 2> /dev/null &
