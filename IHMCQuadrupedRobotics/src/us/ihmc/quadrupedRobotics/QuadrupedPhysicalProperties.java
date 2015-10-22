b27928919dee47392ad30224ca4d8ceb94db3bb2
brandon shrewsbury <bshrewsbury@ihmc.us>
2015-10-22 17:13:53 -0500

    created a new IHMCQuadrupedRobotics project. It can't be merged into ihmcRoboticsToolkit because of it's depencies on sdfLoader (because of reference frames)

---
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/QuadrupedPhysicalProperties.java
@@ -0,0 +1,8 @@
+package us.ihmc.quadrupedRobotics;
+
+import javax.vecmath.Vector3d;
+
+public interface QuadrupedPhysicalProperties
+{
+   public Vector3d getOffsetFromKneeToFoot();
+}
---
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/QuadrupedRobotParameters.java
@@ -0,0 +1,21 @@
+package us.ihmc.quadrupedRobotics;
+
+import us.ihmc.SdfLoader.SDFFullRobotModel;
+import us.ihmc.SdfLoader.SDFParameters;
+import us.ihmc.SdfLoader.SDFRobot;
+
+public interface QuadrupedRobotParameters
+{
+   public SDFRobot createSdfRobot();
+
+   public QuadrupedJointNameMap getJointMap();
+
+   public SDFFullRobotModel createFullRobotModel();
+
+   public String getModelName();
+
+   public SDFParameters getSdfParameters();
+
+   public QuadrupedPhysicalProperties getPhysicalProperties();
+
+}
