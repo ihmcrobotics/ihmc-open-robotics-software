fddba348aaf68e74c564efd6161bf57b046acc4f
Jesper Smith <jsmith@ihmc.us>
2015-11-04 14:46:24 -0600

    BEAST-87:  Improving position esitmate. Adding velocity/yaw providers

--- IHMCCommunication/src/us/ihmc/communication/packetCommunicator/ConcurrentPacketQueue.java
+++ IHMCCommunication/src/us/ihmc/communication/packetCommunicator/ConcurrentPacketQueue.java
@@ -5,6 +5,13 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import us.ihmc.communication.net.PacketConsumer;
 import us.ihmc.communication.packets.Packet;
 
+/**
+ * 
+ * Make sure you need this. Do you want to keep all packets? Or do you just need the latest.
+ * 
+ * Remeber, this interface generates garbage
+ * 
+ */
 public class ConcurrentPacketQueue<T extends Packet> implements PacketConsumer<T>
 {
    private final ConcurrentLinkedQueue<T> packetQueue = new ConcurrentLinkedQueue<T>();
