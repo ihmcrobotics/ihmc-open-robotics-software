a24a59cc5543910eb67d648354588fb62e06af41
Jesper Smith <jsmith@ihmc.us>
2015-11-03 20:55:22 -0600

    BEAST-87: Added a DRCPoseCommunicator to wheelbot to send over RobotConfigurationData

--- SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SensorReader.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SensorReader.java
@@ -4,13 +4,13 @@ import us.ihmc.sensorProcessing.sensorProcessors.SensorRawOutputMapReadOnly;
 import us.ihmc.sensorProcessing.communication.packets.dataobjects.AuxiliaryRobotData;
 import us.ihmc.sensorProcessing.sensorProcessors.SensorOutputMapReadOnly;
 
-public interface SensorReader
+public interface SensorReader extends AuxiliaryRobotDataProvider
 {
    public abstract void read();
 
    public abstract SensorOutputMapReadOnly getSensorOutputMapReadOnly();
 
    public abstract SensorRawOutputMapReadOnly getSensorRawOutputMapReadOnly();
+   
 
-   AuxiliaryRobotData newAuxiliaryRobotDataInstance();
 }
