cb5a5e914da510515bfb7ca16d24d2408f289aea
AtlasControlComputer <dcalvert@ihmc.us>
2015-11-17 11:58:17 -0600

    New scripts to run the controller.

--- launchScripts/runMicrostrainSensors.sh
+++ launchScripts/runMicrostrainSensors.sh
@@ -8,5 +8,5 @@ pkill MicrostrainMip
 
 cp MicrostrainMipServer MicrostrainMipServer.run
 
-./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47092-if00 -h 127.0.0.1 -p 50092 2> /dev/null &
-./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47094-if00 -h 127.0.0.1 -p 50094 2> /dev/null &
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47092-if00 -h 10.7.4.221 -p 50092 2> /dev/null &
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47094-if00 -h 10.7.4.221 -p 50094 2> /dev/null &
