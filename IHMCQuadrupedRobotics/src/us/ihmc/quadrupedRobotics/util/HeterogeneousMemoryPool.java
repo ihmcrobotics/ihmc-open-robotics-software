a5865a81c5876366bd0ad1d6b3085c67cbc441b3
brandon shrewsbury <bshrewsbury@ihmc.us>
2015-11-18 19:12:31 -0600

    created an instance variable to limit garbage collection on reset

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/util/HeterogeneousMemoryPool.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/util/HeterogeneousMemoryPool.java
@@ -1,5 +1,6 @@
 package us.ihmc.quadrupedRobotics.util;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 public class HeterogeneousMemoryPool
 {
    private final Map<Class<?>, HomogeneousMemoryPool<?>> pools = new HashMap<>();
+   private final Collection<HomogeneousMemoryPool<?>> values = pools.values();
 
    @SuppressWarnings("unchecked")
    public <T> T grab(Class<T> type)
@@ -22,13 +24,15 @@ public class HeterogeneousMemoryPool
 
       HomogeneousMemoryPool<T> pool = new HomogeneousMemoryPool<>(type);
       pools.put(type, pool);
+      values.add(pool);
 
       return (T) pool.grab();
    }
 
    public void reset()
    {
-      for (HomogeneousMemoryPool<?> pool : pools.values())
+      
+      for (HomogeneousMemoryPool<?> pool : values)
       {
          pool.reset();
       }
