b54dd19a47179b9c1d6dce1d5914e9fe6cc23f29
Neyssette <neyssette@ihmc.us>
2015-11-20 12:14:27 -0600

    beast-96: first implementation of the FootContactStateBasedOnOtherFeet

--- IHMCStateEstimation/src/us/ihmc/stateEstimation/quadruped/kinematicsBasedStateEstimation/FootContactStateBasedOnOtherFeet.java
+++ IHMCStateEstimation/src/us/ihmc/stateEstimation/quadruped/kinematicsBasedStateEstimation/FootContactStateBasedOnOtherFeet.java
@@ -1,25 +1,39 @@
 package us.ihmc.stateEstimation.quadruped.kinematicsBasedStateEstimation;
 
-import us.ihmc.robotics.math.frames.YoFramePoint;
+import us.ihmc.robotics.MathTools;
+import us.ihmc.robotics.geometry.FramePoint;
 import us.ihmc.robotics.robotSide.QuadrantDependentList;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public class FootContactStateBasedOnOtherFeet implements FootContactStateInterface
 {
-
-   private QuadrantDependentList<YoFramePoint> footPositions = new QuadrantDependentList<YoFramePoint>();
+   private QuadrantDependentList<FramePoint> footPositions = new QuadrantDependentList<FramePoint>();
    
-   public FootContactStateBasedOnOtherFeet()
+   public FootContactStateBasedOnOtherFeet(QuadrantDependentList<FramePoint> footPositions)
    {
+      this.footPositions = footPositions;
    }
 
    @Override
    public boolean isFootInContactWithGround(RobotQuadrant footToBeChecked)
    {
+      double footToBeCheckedZ = footPositions.get(footToBeChecked).getZ(); 
+      double otherFeetZ = 0.0;
       
-      
+      for(RobotQuadrant robotQuadrant : RobotQuadrant.values())
+      {
+         if(robotQuadrant != footToBeChecked)
+         {
+            otherFeetZ += footPositions.get(robotQuadrant).getZ();
+         }
+         otherFeetZ /= 3.0;
+      }
+
+      if(MathTools.epsilonEquals(footToBeCheckedZ, otherFeetZ, 1e-5))
+         return true;
       
       return false;
    }
-
 }
+
+
