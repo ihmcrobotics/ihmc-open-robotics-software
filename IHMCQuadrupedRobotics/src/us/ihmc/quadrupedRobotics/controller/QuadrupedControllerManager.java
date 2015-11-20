dc559b050706db350ea37911768ebcfcdee3ba10
Mike Hopkins <michael.a.hopkins@disney.com>
2015-11-19 18:56:17 -0800

    revert initial quadruped control state to position-based crawl

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/controller/QuadrupedControllerManager.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/controller/QuadrupedControllerManager.java
@@ -44,7 +44,7 @@ public class QuadrupedControllerManager implements RobotController
       stateMachine.addState(vmcStandController);
       stateMachine.addState(positionBasedCrawlController);
 
-      stateMachine.setCurrentState(QuadrupedControllerState.VMC_STAND);
+      stateMachine.setCurrentState(QuadrupedControllerState.POSITION_CRAWL);
       requestedState.set(null);
    }
 
