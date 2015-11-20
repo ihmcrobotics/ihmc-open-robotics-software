2754aa0d852700db227f521a9913747741c90eec
Elena Galbally <egalbally@ihmc.us>
2015-11-20 15:38:08 -0600

    NONE: four bar link

--- ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageSimulation.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageSimulation.java
@@ -10,13 +10,10 @@ public class FourBarLinkageSimulation {
 	private static final double simDT = 1.0e-4;
 	private static final int recordFrequency = 10;
 
-	public FourBarLinkageSimulation()
+	public FourBarLinkageSimulation(FourBarLinkageParameters fourBarLinkageParameters)
 	{
-	// Parameters
-	FourBarLinkageParameters robotParameters = new FourBarLinkageParameters();
-
 	// Robot
-	FourBarLinkageRobot robot = new FourBarLinkageRobot("basicFourBar", robotParameters, offsetWorld);
+	FourBarLinkageRobot robot = new FourBarLinkageRobot("basicFourBar", fourBarLinkageParameters, offsetWorld);
 
 	// Controller
 
@@ -28,6 +25,13 @@ public class FourBarLinkageSimulation {
 	
 	public static void main(String[] args) 
 	{
-		new FourBarLinkageSimulation();
+		FourBarLinkageParameters fourBarLinkageParameters = new FourBarLinkageParameters();
+		fourBarLinkageParameters.linkageLength_1 = fourBarLinkageParameters.linkageLength_2 = fourBarLinkageParameters.linkageLength_3 = fourBarLinkageParameters.linkageLength_4 = 1.0;	
+		fourBarLinkageParameters.damping_1 = fourBarLinkageParameters.damping_2 = fourBarLinkageParameters.damping_3 = 0.05;
+		fourBarLinkageParameters.mass_1 = fourBarLinkageParameters.mass_2 = fourBarLinkageParameters.mass_3 = 0.3;
+		fourBarLinkageParameters.radius_1 = fourBarLinkageParameters.radius_2 = fourBarLinkageParameters.radius_3 = fourBarLinkageParameters.radius_4 = 0.03;
+		fourBarLinkageParameters.angle_1 = fourBarLinkageParameters.angle_2 = fourBarLinkageParameters.angle_3 = 0.5;
+		
+		new FourBarLinkageSimulation(fourBarLinkageParameters);
 	}
 }
