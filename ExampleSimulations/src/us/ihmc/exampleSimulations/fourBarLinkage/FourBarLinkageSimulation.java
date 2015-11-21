1ed2891c279d5e5888d2b250fd8a6ced37dd5e4d
stephen <stephen@stephen-MS-7850>
2015-11-21 17:30:25 -0600

    Added remaining joints, links and external force points to four bar linkage. Currently doesn't simulate due to referenceframe error.

--- ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageSimulation.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageSimulation.java
@@ -2,6 +2,7 @@ package us.ihmc.exampleSimulations.fourBarLinkage;
 
 import javax.vecmath.Vector3d;
 
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 
 public class FourBarLinkageSimulation
@@ -13,15 +14,21 @@ public class FourBarLinkageSimulation
 
    public FourBarLinkageSimulation(FourBarLinkageParameters fourBarLinkageParameters)
    {
+      YoVariableRegistry registry = new YoVariableRegistry("fourBarLinkageRegistry");
+      
       // Robot
-      FourBarLinkageRobot robot = new FourBarLinkageRobot("basicFourBar", fourBarLinkageParameters, offsetWorld);
+      FourBarLinkageRobot robot = new FourBarLinkageRobot("basicFourBar", fourBarLinkageParameters, offsetWorld, registry);
 
       // Controller
 
       // SCS
       SimulationConstructionSet scs = new SimulationConstructionSet(robot);
+      scs.setSimulateDuration(3.0);
       scs.setGroundVisible(false);
       scs.setDT(simDT, recordFrequency);
+      scs.addYoVariableRegistry(registry);
+      scs.setCameraFix(0.335, 0.0, -0.5);
+      scs.setCameraPosition(0.355, -11.2, -0.75);
       scs.startOnAThread();
    }
 
@@ -30,9 +37,9 @@ public class FourBarLinkageSimulation
       FourBarLinkageParameters fourBarLinkageParameters = new FourBarLinkageParameters();
       fourBarLinkageParameters.linkageLength_1 = fourBarLinkageParameters.linkageLength_2 = fourBarLinkageParameters.linkageLength_3 = fourBarLinkageParameters.linkageLength_4 = 1.0;
       fourBarLinkageParameters.damping_1 = fourBarLinkageParameters.damping_2 = fourBarLinkageParameters.damping_3 = 0.05;
-      fourBarLinkageParameters.mass_1 = fourBarLinkageParameters.mass_2 = fourBarLinkageParameters.mass_3 = 0.3;
+      fourBarLinkageParameters.mass_1 = fourBarLinkageParameters.mass_2 = fourBarLinkageParameters.mass_3 = fourBarLinkageParameters.mass_4 = 0.3;
       fourBarLinkageParameters.radius_1 = fourBarLinkageParameters.radius_2 = fourBarLinkageParameters.radius_3 = fourBarLinkageParameters.radius_4 = 0.03;
-      fourBarLinkageParameters.angle_1 = fourBarLinkageParameters.angle_2 = fourBarLinkageParameters.angle_3 = 0.5;
+      fourBarLinkageParameters.angle_1 = fourBarLinkageParameters.angle_2 = fourBarLinkageParameters.angle_3 = 0.5 * Math.PI;
 
       new FourBarLinkageSimulation(fourBarLinkageParameters);
    }
