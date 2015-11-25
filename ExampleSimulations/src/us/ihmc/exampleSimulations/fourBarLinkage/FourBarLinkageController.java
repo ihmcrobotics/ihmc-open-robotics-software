93c71e38def3fa33329a0b1953a9f66dc47c9ac6
stephen mccrory <smccrory@ihmc.us>
2015-11-25 17:04:22 -0600

    Refactoring the FourBarLinkage stuff to make general, changing joint name scheme to featherstone, and adding damping to loop closure joint. Adding getter for Moment vector in ExternalForcePoint.

--- ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageController.java
@@ -3,78 +3,46 @@ package us.ihmc.exampleSimulations.fourBarLinkage;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
-import us.ihmc.robotics.controllers.PIDController;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
 import us.ihmc.robotics.math.frames.YoFramePoint;
 import us.ihmc.robotics.math.frames.YoFrameVector;
-import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.simulationconstructionset.ExternalForcePoint;
 import us.ihmc.simulationconstructionset.robotController.RobotController;
 
 public class FourBarLinkageController implements RobotController
 {
-   private FourBarLinkageRobot robot;
    private String name;
    private YoVariableRegistry registry = new YoVariableRegistry("controllerRegistry");
-//   private final YoGraphicsList yoGraphicsList = new YoGraphicsList(name);
-   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
-   
-   private PIDController controllerActuator;
-   private DoubleYoVariable desiredDistance;
-   
-   private final YoFramePoint actuatorAnchor1Pos = new YoFramePoint("yoDesiredPositionSwingFoot", worldFrame, registry);
-   
+
    private final YoFramePoint yoPositionFix;
    private final YoFrameVector yoVelocityFix;
    private final YoFrameVector yoTauFix;
    private final DoubleYoVariable fix_kp = new DoubleYoVariable("fixGain", registry);
    private final DoubleYoVariable fix_kd = new DoubleYoVariable("fixDamping", registry);
    private final DoubleYoVariable positionErrorMagnitude = new DoubleYoVariable("positionErrorMagnitude", registry);
-   
+
+   // temp variables
    private Point3d posEfpFix = new Point3d();
    private Point3d posEfpFixDesired;
    private Point3d velEfpFix = new Point3d();
-   private Point3d velEfpFixDesired = new Point3d(0.0,0.0,0.0);
+   private Point3d velEfpFixDesired = new Point3d();
    private Vector3d pTau = new Vector3d();
    private Vector3d dTau = new Vector3d();
    private Vector3d totalTauFix = new Vector3d();
-   
+
    public FourBarLinkageController(FourBarLinkageRobot robot, String name, FourBarLinkageParameters fourBarLinkageParameters)
    {
-      this.robot = robot;
       this.name = name;
-      
-      ExternalForcePoint efpFix = robot.getEfpFixInWorld();
-      yoPositionFix = efpFix.getYoPosition();
-      posEfpFixDesired = new Point3d(fourBarLinkageParameters.linkageLength_2, 0.0, 0.0);
-      yoVelocityFix = efpFix.getYoVelocity();
-      yoTauFix = efpFix.getYoForce();  
-      
-      initializeControls();
-//      initializeVisualizers();
+
+      ExternalForcePoint efpToFixJoint1InWorld = new ExternalForcePoint(name + "Joint1_efp", new Vector3d(), robot);
+      robot.getJoint(1).addExternalForcePoint(efpToFixJoint1InWorld);
+      yoPositionFix = efpToFixJoint1InWorld.getYoPosition();
+      posEfpFixDesired = new Point3d(fourBarLinkageParameters.linkageLength_1, 0.0, 0.0);
+      yoVelocityFix = efpToFixJoint1InWorld.getYoVelocity();
+      yoTauFix = efpToFixJoint1InWorld.getYoForce();
    }
-   
-   public void initializeControls()
-   {
-      controllerActuator = new PIDController("actuatorController" + name, registry);
-      controllerActuator.setProportionalGain(100.0);
-      controllerActuator.setDerivativeGain(10.0);
-      
-      desiredDistance = new DoubleYoVariable("desiredPosition", registry);
-      desiredDistance.set(0.3);
-      
-      fix_kp.set(1000.0);
-      fix_kd.set(10.0);
-   }
-   
-//   public void initializeVisualizers()
-//   {
-//      YoGraphicPosition actuatorAnchor1Viz= new YoGraphicPosition("actuatorAnchor1", actuatorAnchor1Pos, 0.06, YoAppearance.CornflowerBlue());
-//      yoGraphicsList.add(actuatorAnchor1Viz);
-//   }
-   
-   
+
    @Override
    public void initialize()
    {
@@ -98,12 +66,22 @@ public class FourBarLinkageController implements RobotController
       return getName();
    }
 
+   public void setDamping(double damping)
+   {
+      fix_kd.set(damping);
+   }
+
+   public void setGain(double gain)
+   {
+      fix_kp.set(gain);
+   }
+
    @Override
    public void doControl()
    {
       // Fix Joint 2 with respect to world
-      
-      yoPositionFix.get(posEfpFix);   
+
+      yoPositionFix.get(posEfpFix);
       pTau.sub(posEfpFix, posEfpFixDesired);
       positionErrorMagnitude.set(pTau.length());
       pTau.scale(-fix_kp.getDoubleValue());
@@ -112,12 +90,7 @@ public class FourBarLinkageController implements RobotController
       dTau.sub(velEfpFix, velEfpFixDesired);
       dTau.scale(-fix_kd.getDoubleValue());
 
-      totalTauFix.add(pTau, dTau); 
+      totalTauFix.add(pTau, dTau);
       yoTauFix.set(totalTauFix);
    }
-   
-//   public YoGraphicsList getYoGraphicsList()
-//   {
-//      return yoGraphicsList;
-//   }
 }
--- ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageSimpleClosedLoopConstraintController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/fourBarLinkage/FourBarLinkageSimpleClosedLoopConstraintController.java
@@ -12,47 +12,63 @@ import us.ihmc.simulationconstructionset.robotController.RobotController;
 
 public class FourBarLinkageSimpleClosedLoopConstraintController implements RobotController
 {
-   private final YoFramePoint yoPositionA, yoPositionB;
-   private final YoFrameVector yoVelocityA, yoVelocityB;
-   private final YoFrameVector yoForceA, yoForceB;
-   
+   private final YoFramePoint yoPositionJoint0, yoPositionJoint4;
+   private final YoFrameVector yoVelocityJoint0, yoVelocityJoint4;
+   private final YoFrameVector yoForceJoint0, yoForceJoint4;
+   private final YoFrameVector yoMomentJoint0, yoMomentJoint4;
+
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
    private final DoubleYoVariable constraintGain = new DoubleYoVariable("constraintGain", registry);
    private final DoubleYoVariable constraintDamping = new DoubleYoVariable("constraintDamping", registry);
    private final DoubleYoVariable positionErrorMagnitude = new DoubleYoVariable("positionErrorMagnitude", registry);
 
+   private final DoubleYoVariable qdJoint1, qdJoint2, qdJoint3;
+   private final DoubleYoVariable closureJointDamping;
+
    // Temp variables:
-   private Point3d posA = new Point3d();
-   private Point3d posB = new Point3d();
-   private Vector3d velA = new Vector3d();
-   private Vector3d velB = new Vector3d();
-   private Vector3d springForceA = new Vector3d();
-   private Vector3d dampingForceA = new Vector3d();
-   private Vector3d newForceA = new Vector3d();
-   private Vector3d newForceB = new Vector3d();
-   
+   private Point3d posJoint0 = new Point3d();
+   private Point3d posJoint4 = new Point3d();
+   private Vector3d velJoint0 = new Vector3d();
+   private Vector3d velJoint4 = new Vector3d();
+   private Vector3d springForce0 = new Vector3d();
+   private Vector3d dampingForce4 = new Vector3d();
+   private Vector3d newForce0 = new Vector3d();
+   private Vector3d newForce4 = new Vector3d();
+   private Vector3d newMoment0 = new Vector3d();
+   private Vector3d newMoment4 = new Vector3d();
+   private double angleChangeAtLoopClosure;
+
    public FourBarLinkageSimpleClosedLoopConstraintController(FourBarLinkageRobot robot)
    {
-      ExternalForcePoint efpA = robot.getEfpJoint1to2();
-      ExternalForcePoint efpB = robot.getEfpJoint1to4();
-      
-      yoPositionA = efpA.getYoPosition();
-      yoPositionB = efpB.getYoPosition();
-      
-      yoVelocityA = efpA.getYoVelocity();
-      yoVelocityB = efpB.getYoVelocity();
-      
-      yoForceA = efpA.getYoForce();
-      yoForceB = efpB.getYoForce();
-      
+      ExternalForcePoint efpJoint0 = robot.getEfpJoint0();
+      ExternalForcePoint efpJoint4 = robot.getEfpJoint4();
+
+      yoPositionJoint0 = efpJoint0.getYoPosition();
+      yoPositionJoint4 = efpJoint4.getYoPosition();
+
+      yoVelocityJoint0 = efpJoint0.getYoVelocity();
+      yoVelocityJoint4 = efpJoint4.getYoVelocity();
+
+      yoForceJoint0 = efpJoint0.getYoForce();
+      yoForceJoint4 = efpJoint4.getYoForce();
+
+      qdJoint1 = robot.getJoint(1).getQD();
+      qdJoint2 = robot.getJoint(2).getQD();
+      qdJoint3 = robot.getJoint(3).getQD();
+
+      yoMomentJoint0 = efpJoint0.getYoMoment();
+      yoMomentJoint4 = efpJoint4.getYoMoment();
+
+      closureJointDamping = new DoubleYoVariable(robot.getName() + "ClosureJointDamping", registry);
+
       initialize();
    }
-   
+
    @Override
    public void initialize()
    {
       constraintGain.set(1000.0);
-      constraintDamping.set(10.0);
+      constraintDamping.set(100.0);
    }
 
    @Override
@@ -76,24 +92,35 @@ public class FourBarLinkageSimpleClosedLoopConstraintController implements Robot
    @Override
    public void doControl()
    {
-      yoPositionA.get(posA);
-      yoPositionB.get(posB);
-      
-      yoVelocityA.get(velA);
-      yoVelocityB.get(velB);
-      
-      springForceA.sub(posB, posA);
-      positionErrorMagnitude.set(springForceA.length());
-      springForceA.scale(constraintGain.getDoubleValue());
+      yoPositionJoint0.get(posJoint0);
+      yoPositionJoint4.get(posJoint4);
 
-      dampingForceA.sub(velB, velA);
-      dampingForceA.scale(constraintDamping.getDoubleValue());
+      yoVelocityJoint0.get(velJoint0);
+      yoVelocityJoint4.get(velJoint4);
 
-      newForceA.add(springForceA, dampingForceA);
-      newForceB.scale(-1.0, newForceA);
-      
-      yoForceA.set(newForceA);
-      yoForceB.set(newForceB);
+      springForce0.sub(posJoint4, posJoint0);
+      positionErrorMagnitude.set(springForce0.length());
+      springForce0.scale(constraintGain.getDoubleValue());
+
+      dampingForce4.sub(velJoint4, velJoint0);
+      dampingForce4.scale(constraintDamping.getDoubleValue());
+
+      newForce0.add(springForce0, dampingForce4);
+      newForce4.scale(-1.0, newForce0);
+
+      yoForceJoint0.set(newForce0);
+      yoForceJoint4.set(newForce4);
+
+      angleChangeAtLoopClosure = -qdJoint1.getDoubleValue() - qdJoint2.getDoubleValue() - qdJoint3.getDoubleValue();
+      newMoment0.setY(-angleChangeAtLoopClosure * closureJointDamping.getDoubleValue());
+      newMoment4.setY(angleChangeAtLoopClosure * closureJointDamping.getDoubleValue());
+
+      yoMomentJoint0.set(newMoment0);
+      yoMomentJoint4.set(newMoment4);
    }
 
+   public void setClosureJointDamping(double damping)
+   {
+      closureJointDamping.set(damping);
+   }
 }
