6dc0f5c15ed96fe5a9e7fc5041e47f38cfd94edf
Neyssette <neyssette@ihmc.us>
2015-11-25 15:09:02 -0600

    NONE: renamed a few classes so that their names are more consistent

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/sensorProcessing/sensorProcessors/FootSwitchUpdater.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/sensorProcessing/sensorProcessors/FootSwitchUpdater.java
@@ -2,7 +2,7 @@ package us.ihmc.quadrupedRobotics.sensorProcessing.sensorProcessors;
 
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
-public interface FootContactStateInterface
+public interface FootSwitchUpdater
 {
    public boolean isFootInContactWithGround(RobotQuadrant footToBeChecked);
 }
--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/sensorProcessing/simulatedSensors/FootSwitchUpdaterBasedOnGroundContactPoints.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/sensorProcessing/simulatedSensors/FootSwitchUpdaterBasedOnGroundContactPoints.java
@@ -1,11 +1,11 @@
 package us.ihmc.quadrupedRobotics.sensorProcessing.simulatedSensors;
 
-import us.ihmc.quadrupedRobotics.sensorProcessing.sensorProcessors.FootContactStateInterface;
+import us.ihmc.quadrupedRobotics.sensorProcessing.sensorProcessors.FootSwitchUpdater;
 import us.ihmc.quadrupedRobotics.sensorProcessing.sensorProcessors.FootSwitchOutputReadOnly;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
-public class FootSwitchUpdaterBasedOnGroundContactPoints implements FootContactStateInterface
+public class FootSwitchUpdaterBasedOnGroundContactPoints implements FootSwitchUpdater
 {
 
    private final FootSwitchOutputReadOnly footSwitchOutput;
