92189266432db6814d54ac1d71863a1273c13131
Doug Stephen <dstephen@ihmc.us>
2015-12-02 10:23:29 -0600

    ROSI-188 ROSI-290: Use the sim .sdf when not running on the real robot, as the real robot SDF doesn't seem to have the Multisense in it. Not sure if this is intentional, will investigate.

--- src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
+++ src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
@@ -2,7 +2,8 @@ package us.ihmc.valkyrie.configuration;
 
 public class ValkyrieConfigurationRoot
 {
-   public static final String SDF_FILE = "models/val_description/sdf/valkyrie_B_hw.sdf";
+   public static final String REAL_ROBOT_SDF_FILE = "models/val_description/sdf/valkyrie_B_hw.sdf";
+   public static final String SIM_SDF_FILE = "models/val_description/sdf/valkyrie_sim.sdf";
 
    public static final boolean VALKYRIE_WITH_ARMS = true;
 }
