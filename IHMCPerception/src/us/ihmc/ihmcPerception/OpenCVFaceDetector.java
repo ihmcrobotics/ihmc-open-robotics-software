c410d26c8dc67c833aa76ccfe76ee6490066f3ec
Nathan Mertins <nmertins@ihmc.us>
2015-12-03 12:22:30 -0600

    NONE: added convenience method to face detector

--- IHMCPerception/src/us/ihmc/ihmcPerception/OpenCVFaceDetector.java
+++ IHMCPerception/src/us/ihmc/ihmcPerception/OpenCVFaceDetector.java
@@ -1,35 +1,31 @@
 package us.ihmc.ihmcPerception;
 
-import java.awt.Graphics2D;
+import boofcv.gui.image.ImagePanel;
+import boofcv.gui.image.ShowImages;
+import org.opencv.core.*;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import us.ihmc.ihmcPerception.chessboardDetection.OpenCVChessboardPoseEstimator;
+import us.ihmc.tools.nativelibraries.NativeLibraryLoader;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-import javax.imageio.ImageIO;
-
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Rect;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
-import org.opencv.highgui.VideoCapture;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
-
-import boofcv.gui.image.ImagePanel;
-import boofcv.gui.image.ShowImages;
-import us.ihmc.ihmcPerception.chessboardDetection.OpenCVChessboardPoseEstimator;
-import us.ihmc.tools.nativelibraries.NativeLibraryLoader;
-
 public class OpenCVFaceDetector
 {
+   private final boolean DEBUG = true;
+
    static
    {
       NativeLibraryLoader.loadLibrary("org.opencv", "opencv_java2411");
    }
-   CascadeClassifier faceDetector = new CascadeClassifier(Paths.get("..", "IHMCPerception", "resources", "us", "ihmc", "ihmcPerception", "haarcascade_frontalface_alt.xml").toString());
+   CascadeClassifier faceDetector = new CascadeClassifier(Paths.get("..", "IHMCPerception", "resources", "faceDetection", "haarcascade_frontalface_alt.xml").toString());
    MatOfRect faces = new MatOfRect();
    Mat image= new Mat();
    final double scaleFactor;
@@ -59,6 +55,25 @@ public class OpenCVFaceDetector
       return faceArray;
    }
 
+   public Rect[] detectAndOutline(BufferedImage bufferedImage)
+   {
+      Rect[] faces = detect(bufferedImage);
+      Graphics2D g2 = bufferedImage.createGraphics();
+
+      for (int i = 0; i < faces.length; i++)
+      {
+         if(DEBUG)
+         {
+            System.out.println("Face " + i + ": " + faces[i]);
+         }
+         g2.drawRect(faces[i].x, faces[i].y, faces[i].width, faces[i].height);
+      }
+
+      g2.finalize();
+
+      return faces;
+   }
+
    
    public static void main(String[] arg) throws IOException
    {
@@ -73,13 +88,7 @@ public class OpenCVFaceDetector
          cap.read(image);
          Highgui.imencode(".bmp", image, mem);
          BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(mem.toArray()));
-         Rect[] faces = detector.detect(bufferedImage);
-         Graphics2D g2 = bufferedImage.createGraphics();
-         for (Rect face : faces)
-         {
-            System.out.println(face);
-            g2.drawRect(face.x, face.y, face.width, face.height);
-         }
+         detector.detectAndOutline(bufferedImage);
          if (panel == null)
          {
             panel = ShowImages.showWindow(bufferedImage, "faces");
@@ -88,8 +97,6 @@ public class OpenCVFaceDetector
          {
             panel.setBufferedImageSafe(bufferedImage);
          }
-
-         g2.finalize();
       }
 
    }
