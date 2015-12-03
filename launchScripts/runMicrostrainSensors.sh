bdd4a50cd9e0ab2271a1324645288fa9d3ef7243
Frank Mathis <frank.b.mathis@nasa.gov>
2015-12-03 08:01:26 -0600

    Changing addresses for unit C.
    Conflicts:
    	ValkyrieHardwareDrivers/src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java

--- launchScripts/runMicrostrainSensors.sh
+++ launchScripts/runMicrostrainSensors.sh
@@ -3,10 +3,10 @@
 cd `dirname $0`
 pkill MicrostrainMip
 
-./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47092-if00
-./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47094-if00
+./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47095-if00
+./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47093-if00
 
 cp MicrostrainMipServer MicrostrainMipServer.run
 
-./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47092-if00 -h 10.7.4.221 -p 50092 2> /dev/null &
-./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47094-if00 -h 10.7.4.221 -p 50094 2> /dev/null &
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47095-if00 -h 10.3.0.10 -p 50095 2> /dev/null &
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47093-if00 -h 10.3.0.10 -p 50093 2> /dev/null &
