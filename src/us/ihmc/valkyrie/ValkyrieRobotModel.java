7a468ec780e5fd11e8fdbfca265ef81953f9f518
Doug Stephen <dstephen@ihmc.us>
2015-12-09 17:11:10 -0600

    NONE: Added a no cameras log settings for Valkyrie.

--- src/us/ihmc/valkyrie/ValkyrieRobotModel.java
+++ src/us/ihmc/valkyrie/ValkyrieRobotModel.java
@@ -437,9 +437,17 @@ public class ValkyrieRobotModel implements DRCRobotModel, SDFDescriptionMutator
    @Override
    public LogSettings getLogSettings()
    {
+      return getLogSettings(true);
+   }
+
+   public LogSettings getLogSettings(boolean useCameras)
+   {
       if(target == RobotTarget.REAL_ROBOT)
       {
-         return LogSettings.VALKYRIE_IHMC;
+         if(useCameras)
+            return LogSettings.VALKYRIE_IHMC;
+         else
+            return LogSettings.VALKYRIE_NO_CAMERAS;
       }
       else
       {
