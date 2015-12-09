113def3676f585426f75feb9a20be6b3ea4ee62d
Doug Stephen <dstephen@ihmc.us>
2015-12-09 17:11:48 -0600

    NONE: Set up current Val controllers for logging at JSC; don't currently have working cameras.

--- src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
+++ src/us/ihmc/valkyrie/configuration/ValkyrieConfigurationRoot.java
@@ -6,4 +6,6 @@ public class ValkyrieConfigurationRoot
    public static final String SIM_SDF_FILE = "models/val_description/sdf/valkyrie_sim.sdf";
 
    public static final boolean VALKYRIE_WITH_ARMS = true;
+
+   public static final boolean USE_CAMERAS_FOR_LOGGING = false;
 }
