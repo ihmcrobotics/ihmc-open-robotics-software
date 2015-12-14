6ede1515011c575e40eef509869d0a06f42f9eb0
Valkyrie Operator Computer <OperatorComputer@unknown>
2015-12-14 13:02:17 -0600

    Got rid of trajectory in stand-prep.

--- src/us/ihmc/valkyrieRosControl/ValkyrieRosControlJointControlCommandCalculator.java
+++ src/us/ihmc/valkyrieRosControl/ValkyrieRosControlJointControlCommandCalculator.java
@@ -6,23 +6,17 @@ import us.ihmc.robotics.MathTools;
 import us.ihmc.robotics.controllers.PIDController;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
-import us.ihmc.robotics.math.trajectories.YoPolynomial;
 import us.ihmc.simulationconstructionset.util.math.functionGenerator.YoFunctionGenerator;
 import us.ihmc.simulationconstructionset.util.math.functionGenerator.YoFunctionGeneratorMode;
 import us.ihmc.valkyrieRosControl.dataHolders.YoJointHandleHolder;
 
 public class ValkyrieRosControlJointControlCommandCalculator
 {
-   private static final double standPrepTrajectoryTime = 5.0;
-
    private final YoJointHandleHolder yoJointHandleHolder;
-
-   private final YoPolynomial trajectory;
 
    private final PIDController pidController;
    private final DoubleYoVariable tauOff;
-   private final DoubleYoVariable standPrepAngle, trajectoryTime;
-   private final DoubleYoVariable desiredPosition, desiredVelocity;
+   private final DoubleYoVariable standPrepAngle;
    private final YoFunctionGenerator functionGenerator;
 
    private final double controlDT;
@@ -36,17 +30,12 @@ public class ValkyrieRosControlJointControlCommandCalculator
 
       String pdControllerBaseName = yoJointHandleHolder.getName();
       YoVariableRegistry registry = new YoVariableRegistry(pdControllerBaseName + "Command");
-      trajectory = new YoPolynomial(pdControllerBaseName + "StandPrepTrajectory", 4, registry);
 
       this.standPrepAngle = new DoubleYoVariable(pdControllerBaseName + "StandPrepAngle", registry);
-      this.trajectoryTime = new DoubleYoVariable(pdControllerBaseName + "StandPrepTrajectoryTime", registry);
 
       pidController = new PIDController(pdControllerBaseName + "StandPrep", registry);
       this.tauOff = new DoubleYoVariable("tau_offset_" + pdControllerBaseName, registry);
       this.functionGenerator = new YoFunctionGenerator(pdControllerBaseName + "StandPrep_", registry);
-
-      this.desiredPosition = new DoubleYoVariable(pdControllerBaseName + "StandPrep_q_d", registry);
-      this.desiredVelocity = new DoubleYoVariable(pdControllerBaseName + "StandPrep_qd_d", registry);
 
       pidController.setProportionalGain(gains.get("kp"));
       pidController.setDerivativeGain(gains.get("kd"));
@@ -57,42 +46,26 @@ public class ValkyrieRosControlJointControlCommandCalculator
       tauOff.set(torqueOffset);
 
       this.standPrepAngle.set(standPrepAngle);
-      this.trajectoryTime.set(standPrepTrajectoryTime);
 
       parentRegistry.addChild(registry);
    }
 
    public void initialize()
    {
-      double desiredAngle = standPrepAngle.getDoubleValue();
-      double desiredVelocity = 0.0;
-
-      double currentAngle = yoJointHandleHolder.getQ();
-      double currentVelocity = yoJointHandleHolder.getQd();
-
       functionGenerator.setMode(YoFunctionGeneratorMode.DC);
       pidController.setCumulativeError(0.0);
-      trajectory.setCubic(0.0, trajectoryTime.getDoubleValue(), currentAngle, currentVelocity, desiredAngle, desiredVelocity);
    }
 
    public void computeAndUpdateJointTorque(double inStateTime, double factor, double masterGain)
    {
-
       double standPrepFactor = 1.0 - factor;
-      double trajectoryTime = MathTools.clipToMinMax(inStateTime, 0.0, this.trajectoryTime.getDoubleValue());
-
-      trajectory.compute(trajectoryTime);
-
-      desiredPosition.set(trajectory.getPosition() + functionGenerator.getValue(inStateTime));
-
-      desiredVelocity.set(trajectory.getVelocity());
 
       factor = MathTools.clipToMinMax(factor, 0.0, 1.0);
 
       double q = yoJointHandleHolder.getQ();
-      double qDesired = desiredPosition.getDoubleValue();
+      double qDesired = standPrepAngle.getDoubleValue();
       double qd = yoJointHandleHolder.getQd();
-      double qdDesired = desiredVelocity.getDoubleValue();
+      double qdDesired = 0.0;
 
       double standPrepTau = standPrepFactor * masterGain * pidController.compute(q, qDesired, qd, qdDesired, controlDT);
       double controllerTau = factor * yoJointHandleHolder.getControllerTauDesired();
