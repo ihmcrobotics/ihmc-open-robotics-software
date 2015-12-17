0d52f0a222ed996aaf6df7c20264b2a2aed547d9
Kyle Cesare <kyle.cesare@disney.com>
2015-12-17 14:12:13 -0800

    Initial position documentation.

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/parameters/QuadrupedInitialPositionParameters.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/parameters/QuadrupedInitialPositionParameters.java
@@ -1,11 +1,18 @@
 package us.ihmc.quadrupedRobotics.parameters;
 
-import us.ihmc.SdfLoader.partNames.LegJointName;
-import us.ihmc.SdfLoader.partNames.NeckJointName;
-
 public interface QuadrupedInitialPositionParameters
 {
+   /**
+    * @return the height at which the root frame stands off the ground.
+    */
    public double getInitialHeight();
 
+   /**
+    * Maps joint names to initial positions.
+    * 
+    * @param joint
+    *           the joint name to lookup.
+    * @return the initial angle in radians.
+    */
    public double getInitialPosition(QuadrupedJointName joint);
 }
