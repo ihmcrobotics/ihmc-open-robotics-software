1aab21a9bd7ffce480034d0ef2fa76dd3df38f7d
Valkyrie Operator Computer <OperatorComputer@unknown>
2016-01-07 14:22:28 -0600

    Update serial numbers for Unit C UDP server usage

--- launchScripts/runMicrostrainSensors.sh
+++ launchScripts/runMicrostrainSensors.sh
@@ -3,10 +3,12 @@
 cd `dirname $0`
 pkill MicrostrainMip
 
-./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47099-if00
-./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47095-if00
+./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47099-if00 # Torso left IMU
+# ./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47093-if00 # Pelvis middle IMU
+./MicrostrainCalibrator -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47095-if00 # Pelvis rear IMU
 
 cp MicrostrainMipServer MicrostrainMipServer.run
 
-./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47099-if00 -h 10.185.0.30 -p 50099 2> /dev/null &
-./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47095-if00 -h 10.185.0.30 -p 50095 2> /dev/null &
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47099-if00 -h 10.185.0.30 -p 50099 2> /dev/null & # Torso left IMU
+# ./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47093-if00 -h 10.185.0.30 -p 50093 2> /dev/null & # Pelvis middle IMU
+./MicrostrainMipServer.run -d /dev/serial/by-id/usb-Lord_Microstrain_Lord_Inertial_Sensor_0000__6233.47095-if00 -h 10.185.0.30 -p 50095 2> /dev/null & # Pelvis rear IMU
