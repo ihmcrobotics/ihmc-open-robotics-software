b3b9cfd1d3dccaca5e806cbdfac675649ed31c57
Sylvain Bertrand <sbertrand@ihmc.us>
2016-01-07 18:18:52 -0600

    Implemented a delay estimator between two signals.

---
+++ SensorProcessing/src/us/ihmc/sensorProcessing/diagnostic/DelayEstimatorBetweenTwoSignals.java
@@ -0,0 +1,266 @@
+package us.ihmc.sensorProcessing.diagnostic;
+
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.ejml.data.DenseMatrix64F;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.dataStructures.variable.IntegerYoVariable;
+import us.ihmc.robotics.math.filters.AlphaFilteredYoVariable;
+
+/**
+ * This class aims to estimate the delay (lag or lead) between two given signals.
+ * It is achieved by finding the maximum cross-correlation between the two signals by shifting one with respect to the other.
+ * For each iteration, an initial guess for the delay is used as a seed to find the local maximum for the cross-correlation.
+ * However this estimated delay has a useful meaning only if there is sufficient correlation between the two signals.
+ * @author Sylvain
+ *
+ */
+public class DelayEstimatorBetweenTwoSignals
+{
+   private static final int DEFAULT_MAX_ABS_LEAD = 0;
+   private static final int DEFAULT_MAX_ABS_LAG = 25;
+   private static final int DEFAULT_NUMBER_OF_OBSERVATIONS = 100;
+
+   private final DoubleYoVariable referenceSignal;
+   private final DoubleYoVariable delayedSignal;
+   private final DoubleYoVariable estimatedDelay;
+
+   private final DenseMatrix64F referenceSignalBuffer;
+   private final DenseMatrix64F delayedSignalBuffer;
+   private final SimpleRegression correlationCalculator = new SimpleRegression();
+   private final DoubleYoVariable correlationForDelay;
+   private final DoubleYoVariable maxCorrelation;
+   private final IntegerYoVariable nTicksOfDelay;
+
+   private final IntegerYoVariable maxLeadInTicks;
+   private final IntegerYoVariable maxLagInTicks;
+   private final IntegerYoVariable numberOfObservations;
+
+   private final DoubleYoVariable correlationAlpha;
+   private final DenseMatrix64F correlationBuffer;
+   private final DenseMatrix64F filteredCorrelationBuffer;
+
+   private int bufferPosition = 0;
+   private boolean hasBufferBeenFilled = false;
+
+   private final double dt;
+
+   public DelayEstimatorBetweenTwoSignals(String namePrefix, double dt, YoVariableRegistry registry)
+   {
+      this(namePrefix, null, null, dt, registry);
+   }
+
+   /**
+    * @param referenceSignal is the signal used as ground truth to estimate the delay of {@code delayedSignal}.
+    * @param delayedSignal is the signal for which the delay is estimated.
+    */
+   public DelayEstimatorBetweenTwoSignals(String namePrefix, DoubleYoVariable referenceSignal, DoubleYoVariable delayedSignal, double dt, YoVariableRegistry registry)
+   {
+      this.dt = dt;
+      this.referenceSignal = referenceSignal;
+      this.delayedSignal = delayedSignal;
+
+      nTicksOfDelay = new IntegerYoVariable(namePrefix + "_estimatedNTicksOfDelay", registry);
+      estimatedDelay = new DoubleYoVariable(namePrefix + "_estimatedDelay", registry);
+      correlationForDelay = new DoubleYoVariable(namePrefix + "_correlationForDelay", registry);
+      maxCorrelation = new DoubleYoVariable(namePrefix + "_maxCorrelation", registry);
+
+      maxLeadInTicks = new IntegerYoVariable(namePrefix + "_maxPhaseLeadInTicks", registry);
+      maxLeadInTicks.set(DEFAULT_MAX_ABS_LEAD);
+      maxLagInTicks = new IntegerYoVariable(namePrefix + "_maxPhaseLagInTicks", registry);
+      maxLagInTicks.set(DEFAULT_MAX_ABS_LAG);
+      numberOfObservations = new IntegerYoVariable(namePrefix + "_nObservationsForDelayEstimation", registry);
+      numberOfObservations.set(DEFAULT_NUMBER_OF_OBSERVATIONS);
+
+      int bufferSize = numberOfObservations.getIntegerValue() + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue();
+      referenceSignalBuffer = new DenseMatrix64F(bufferSize, 1);
+      delayedSignalBuffer = new DenseMatrix64F(bufferSize, 1);
+
+      correlationAlpha = new DoubleYoVariable(namePrefix + "CorrelationApha", registry);
+      correlationAlpha.set(AlphaFilteredYoVariable.computeAlphaGivenBreakFrequencyProperly(0.16, dt));
+      correlationBuffer = new DenseMatrix64F(1 + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue(), 1);
+      filteredCorrelationBuffer = new DenseMatrix64F(1 + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue(), 1);
+   }
+
+   public void setAlphaFilterBreakFrequency(double breakFrequency)
+   {
+      correlationAlpha.set(AlphaFilteredYoVariable.computeAlphaGivenBreakFrequencyProperly(breakFrequency, dt));
+   }
+
+   public void setEstimationParameters(int maxAbsoluteLeadInTicks, int maxAbsoluteLagInTicks, int numberOfObservations)
+   {
+      maxLeadInTicks.set(maxAbsoluteLeadInTicks);
+      maxLagInTicks.set(maxAbsoluteLagInTicks);
+      this.numberOfObservations.set(numberOfObservations);
+
+      int bufferSize = this.numberOfObservations.getIntegerValue() + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue();
+      referenceSignalBuffer.reshape(bufferSize, 1);
+      delayedSignalBuffer.reshape(bufferSize, 1);
+
+      correlationBuffer.reshape(1 + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue(), 0);
+      filteredCorrelationBuffer.reshape(1 + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue(), 0);
+   }
+
+   public void setEstimationParameters(double maxAbsoluteLead, double maxAbsoluteLag, double observationWindow)
+   {
+      maxLeadInTicks.set((int) (maxAbsoluteLead / dt));
+      maxLagInTicks.set((int) (maxAbsoluteLag / dt));
+      this.numberOfObservations.set((int) (observationWindow / dt));
+
+      int bufferSize = this.numberOfObservations.getIntegerValue() + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue();
+      referenceSignalBuffer.reshape(bufferSize, 1);
+      delayedSignalBuffer.reshape(bufferSize, 1);
+
+      correlationBuffer.reshape(1 + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue(), 1);
+      filteredCorrelationBuffer.reshape(1 + maxLeadInTicks.getIntegerValue() + maxLagInTicks.getIntegerValue(), 1);
+   }
+
+   public void reset()
+   {
+      bufferPosition = 0;
+      hasBufferBeenFilled = false;
+   }
+
+   public void update()
+   {
+      update(referenceSignal.getDoubleValue(), delayedSignal.getDoubleValue());
+   }
+
+   public void update(double referenceSignalCurrentPosition, double delayedSignalCurrentPosition)
+   {
+      referenceSignalBuffer.set(bufferPosition, 0, referenceSignalCurrentPosition);
+      delayedSignalBuffer.set(bufferPosition, 0, delayedSignalCurrentPosition);
+
+      bufferPosition++;
+      if (bufferPosition >= referenceSignalBuffer.getNumRows())
+      {
+         bufferPosition = 0;
+         hasBufferBeenFilled = true;
+      }
+
+      if (!hasBufferBeenFilled)
+      {
+         estimatedDelay.set(0.0);
+         return;
+      }
+
+      updateCorrelationBuffer();
+      findDelayFromInitialGuess();
+
+      estimatedDelay.set(nTicksOfDelay.getIntegerValue() * dt);
+   }
+
+   private void updateCorrelationBuffer()
+   {
+      double maxCorr = Double.NEGATIVE_INFINITY;
+
+      for (int offset = -maxLagInTicks.getIntegerValue(); offset <= maxLeadInTicks.getIntegerValue(); offset++)
+      {
+         int index = offset + maxLagInTicks.getIntegerValue();
+         correlationCalculator.clear();
+
+         for (int i = 1; i <= numberOfObservations.getIntegerValue(); i++)
+         {
+            int indexX = (bufferPosition + i + offset + maxLagInTicks.getIntegerValue()) % referenceSignalBuffer.getNumRows();
+            double x = referenceSignalBuffer.get(indexX, 0);
+            int indexY = (bufferPosition + i + maxLagInTicks.getIntegerValue()) % delayedSignalBuffer.getNumRows();
+            double y = delayedSignalBuffer.get(indexY, 0);
+
+            correlationCalculator.addData(x, y);
+         }
+
+         double currentCorrelation = correlationCalculator.getR();
+         correlationBuffer.set(index, 0, currentCorrelation);
+         double previousFilteredCorrelation = filteredCorrelationBuffer.get(index, 0);
+         if (Double.isNaN(currentCorrelation))
+            currentCorrelation = 0.0;
+         double currentFilteredCorrelation = correlationAlpha.getDoubleValue() * previousFilteredCorrelation + (1.0 - correlationAlpha.getDoubleValue()) * currentCorrelation;
+         filteredCorrelationBuffer.set(index, 0, currentFilteredCorrelation);
+
+         maxCorr = Math.max(maxCorr, currentFilteredCorrelation);
+      }
+
+      maxCorrelation.set(maxCorr);
+   }
+
+   private void findDelayFromInitialGuess()
+   {
+      int initialOffsetGuess = -nTicksOfDelay.getIntegerValue();
+      double initialGuessCorrelation = filteredCorrelationBuffer.get(initialOffsetGuess + maxLagInTicks.getIntegerValue());
+
+      double maxCorrelationCoefficient = initialGuessCorrelation;
+      int maxCorrelationIndex = initialOffsetGuess;
+
+      double previousCorrelation = initialGuessCorrelation;
+
+      int counter = 0;
+      int maxBeforeBreak = 5;
+
+      for (int offset = initialOffsetGuess + 1; offset <= maxLeadInTicks.getIntegerValue(); offset++)
+      {
+         double correlation = filteredCorrelationBuffer.get(offset + maxLagInTicks.getIntegerValue());
+
+         if (correlation < previousCorrelation)
+            counter++;
+
+         if (counter == maxBeforeBreak)
+            break;
+
+         if (correlation > maxCorrelationCoefficient)
+         {
+            maxCorrelationCoefficient = correlation;
+            maxCorrelationIndex = offset;
+         }
+
+         previousCorrelation = correlation;
+      }
+
+      previousCorrelation = initialGuessCorrelation;
+
+      counter = 0;
+
+      for (int offset = initialOffsetGuess; offset >= -maxLagInTicks.getIntegerValue(); offset--)
+      {
+         double correlation = filteredCorrelationBuffer.get(offset + maxLagInTicks.getIntegerValue());
+
+         if (correlation < previousCorrelation)
+            counter++;
+
+         if (counter == maxBeforeBreak)
+            break;
+
+         if (correlation > maxCorrelationCoefficient)
+         {
+            maxCorrelationCoefficient = correlation;
+            maxCorrelationIndex = offset;
+         }
+
+         previousCorrelation = correlation;
+      }
+
+      nTicksOfDelay.set(-maxCorrelationIndex);
+      correlationForDelay.set(maxCorrelationCoefficient);
+   }
+
+   public boolean isEstimatingDelay()
+   {
+      return hasBufferBeenFilled;
+   }
+
+   /**
+    * @return The delay between the two signals in seconds. If {@link #delayedSignal} has a phase lag it is positive, it is negative otherwise.
+    */
+   public double getEstimatedDelay()
+   {
+      return estimatedDelay.getDoubleValue();
+   }
+
+   /**
+    * @return The best correlation value which can be used to control quality of the current estimation.
+    */
+   public double getCorrelationCoefficient()
+   {
+      return correlationForDelay.getDoubleValue();
+   }
+}
---
+++ SensorProcessing/test/us/ihmc/sensorProcessing/diagnostic/DelayEstimatorBetweenTwoSignalsTest.java
@@ -0,0 +1,134 @@
+package us.ihmc.sensorProcessing.diagnostic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.math.filters.DelayedDoubleYoVariable;
+import us.ihmc.simulationconstructionset.util.math.functionGenerator.YoFunctionGenerator;
+import us.ihmc.simulationconstructionset.util.math.functionGenerator.YoFunctionGeneratorMode;
+
+public class DelayEstimatorBetweenTwoSignalsTest
+{
+   @Test
+   public void testPerfectSignal() throws Exception
+   {
+      double epsilon = 1.0e-15;
+
+      YoVariableRegistry registry = new YoVariableRegistry("Blop");
+      double numberOfTicks = 10000;
+      double dt = 0.001;
+      DoubleYoVariable yoTime = new DoubleYoVariable("time", registry);
+
+      YoFunctionGenerator functionGenerator = new YoFunctionGenerator("foo", yoTime, registry);
+      functionGenerator.setChirpFrequencyMaxHz(40.0);
+      functionGenerator.setAmplitude(1.0);
+      functionGenerator.setResetTime(numberOfTicks * dt);
+      functionGenerator.setMode(YoFunctionGeneratorMode.CHIRP_LINEAR);
+
+      DoubleYoVariable referenceSignal = new DoubleYoVariable("referenceSignal", registry);
+      int expectedDelayInTicks = 12;
+      DelayedDoubleYoVariable delayedSignal = new DelayedDoubleYoVariable("delayedSignal", "", referenceSignal, expectedDelayInTicks, registry);
+
+      DelayEstimatorBetweenTwoSignals delayEstimatorBetweenTwoSignals = new DelayEstimatorBetweenTwoSignals("delayedSignal", referenceSignal, delayedSignal, dt, registry);
+
+      for (int i = 0; i < numberOfTicks; i++)
+      {
+         double sine = functionGenerator.getValue();
+         referenceSignal.set(sine);
+         delayedSignal.update();
+         delayEstimatorBetweenTwoSignals.update();
+
+         yoTime.add(dt);
+
+         if (delayEstimatorBetweenTwoSignals.isEstimatingDelay())
+         {
+            assertEquals(1.0, delayEstimatorBetweenTwoSignals.getCorrelationCoefficient(), epsilon);
+            assertEquals(expectedDelayInTicks * dt, delayEstimatorBetweenTwoSignals.getEstimatedDelay(), epsilon);
+         }
+      }
+   }
+
+   @Test
+   public void testWithShiftedSignal() throws Exception
+   {
+      double epsilon = 1.0e-15;
+
+      YoVariableRegistry registry = new YoVariableRegistry("Blop");
+      double numberOfTicks = 10000;
+      double dt = 0.001;
+      DoubleYoVariable yoTime = new DoubleYoVariable("time", registry);
+
+      YoFunctionGenerator functionGenerator = new YoFunctionGenerator("foo", yoTime, registry);
+      functionGenerator.setChirpFrequencyMaxHz(40.0);
+      functionGenerator.setAmplitude(1.0);
+      functionGenerator.setResetTime(numberOfTicks * dt);
+      functionGenerator.setMode(YoFunctionGeneratorMode.CHIRP_LINEAR);
+
+      DoubleYoVariable referenceSignal = new DoubleYoVariable("referenceSignal", registry);
+      int expectedDelayInTicks = 12;
+      DelayedDoubleYoVariable delayedSignal = new DelayedDoubleYoVariable("delayedSignal", "", referenceSignal, expectedDelayInTicks, registry);
+      DoubleYoVariable shiftedDelayedSignal = new DoubleYoVariable("shiftedDelayedSignal", registry);
+
+      DelayEstimatorBetweenTwoSignals delayEstimatorBetweenTwoSignals = new DelayEstimatorBetweenTwoSignals("delayedSignal", referenceSignal, shiftedDelayedSignal, dt, registry);
+
+      for (int i = 0; i < numberOfTicks; i++)
+      {
+         double sine = functionGenerator.getValue();
+         referenceSignal.set(sine);
+         delayedSignal.update();
+         shiftedDelayedSignal.set(delayedSignal.getDoubleValue() + 2.5);
+         delayEstimatorBetweenTwoSignals.update();
+
+         yoTime.add(dt);
+
+         if (delayEstimatorBetweenTwoSignals.isEstimatingDelay())
+         {
+            assertEquals(1.0, delayEstimatorBetweenTwoSignals.getCorrelationCoefficient(), epsilon);
+            assertEquals(expectedDelayInTicks * dt, delayEstimatorBetweenTwoSignals.getEstimatedDelay(), epsilon);
+         }
+      }
+   }
+
+   @Test
+   public void testNoisySignal() throws Exception
+   {
+      Random random = new Random(5616515L);
+
+      YoVariableRegistry registry = new YoVariableRegistry("Blop");
+      double numberOfTicks = 10000;
+      double dt = 0.001;
+      DoubleYoVariable yoTime = new DoubleYoVariable("time", registry);
+
+      DoubleYoVariable referenceSignal = new DoubleYoVariable("referenceSignal", registry);
+      int expectedDelayInTicks = 12;
+      DelayedDoubleYoVariable delayedSignal = new DelayedDoubleYoVariable("delayedSignal", "", referenceSignal, expectedDelayInTicks, registry);
+      DoubleYoVariable noisyDelayedSignal = new DoubleYoVariable("noisyDelayedSignal", registry);
+
+      DelayEstimatorBetweenTwoSignals delayEstimatorBetweenTwoSignals = new DelayEstimatorBetweenTwoSignals("delayedSignal", referenceSignal, noisyDelayedSignal, dt, registry);
+
+      for (int i = 0; i < numberOfTicks; i++)
+      {
+         double sine = 0.5 * Math.sin(2.0 * Math.PI * 16.0 * i * dt);
+         referenceSignal.set(sine);
+         delayedSignal.update();
+         noisyDelayedSignal.set(delayedSignal.getDoubleValue());
+         noisyDelayedSignal.add(0.5 * (random.nextDouble() - 0.5) + 1.5);
+         delayEstimatorBetweenTwoSignals.update();
+
+         yoTime.add(dt);
+
+         if (delayEstimatorBetweenTwoSignals.isEstimatingDelay())
+         {
+            assertTrue("Correlation seems abnormally low", delayEstimatorBetweenTwoSignals.getCorrelationCoefficient() > 0.85);
+            assertTrue("Bad delay estimation", (int)(delayEstimatorBetweenTwoSignals.getEstimatedDelay() / dt) <= expectedDelayInTicks + 2);
+            assertTrue("Bad delay estimation", (int)(delayEstimatorBetweenTwoSignals.getEstimatedDelay() / dt) >= expectedDelayInTicks - 2);
+         }
+      }
+   }
+}
