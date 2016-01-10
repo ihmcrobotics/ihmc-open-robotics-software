66728f961f898238d508933ab1eee762dbd4a836
Sylvain Bertrand <sbertrand@ihmc.us>
2016-01-10 13:19:10 -0600

    Created a joint force tracking delay estimator.

---
+++ SensorProcessing/src/us/ihmc/sensorProcessing/diagnostic/OneDoFJointForceTrackingDelayEstimator.java
@@ -0,0 +1,60 @@
+package us.ihmc.sensorProcessing.diagnostic;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.screwTheory.OneDoFJoint;
+
+public class OneDoFJointForceTrackingDelayEstimator implements DiagnosticUpdatable
+{
+   private final YoVariableRegistry registry;
+   private final OneDoFJoint joint;
+   private final DelayEstimatorBetweenTwoSignals delayEstimator;
+
+   public OneDoFJointForceTrackingDelayEstimator(OneDoFJoint joint, double dt, YoVariableRegistry parentRegistry)
+   {
+      this.joint = joint;
+      String jointName = joint.getName();
+      registry = new YoVariableRegistry(jointName + "ForceTrackingDelayEstimator");
+      delayEstimator = new DelayEstimatorBetweenTwoSignals(jointName + "ForceTracking", dt, registry);
+   }
+
+   public void enable()
+   {
+      delayEstimator.enable();
+   }
+
+   public void disable()
+   {
+      delayEstimator.disable();
+   }
+
+   @Override
+   public void update()
+   {
+      delayEstimator.update(joint.getTau(), joint.getTauMeasured());
+   }
+
+   public void setAlphaFilterBreakFrequency(double delayEstimatorFilterBreakFrequency)
+   {
+      delayEstimator.setAlphaFilterBreakFrequency(delayEstimatorFilterBreakFrequency);
+   }
+
+   public void setEstimationParameters(double maxAbsoluteLead, double maxAbsoluteLag, double observationWindow)
+   {
+      delayEstimator.setEstimationParameters(maxAbsoluteLead, maxAbsoluteLag, observationWindow);
+   }
+
+   public boolean isEstimatingDelay()
+   {
+      return delayEstimator.isEstimatingDelay();
+   }
+
+   public double getEstimatedDelay()
+   {
+      return delayEstimator.getEstimatedDelay();
+   }
+
+   public double getCorrelation()
+   {
+      return delayEstimator.getCorrelationCoefficient();
+   }
+}
