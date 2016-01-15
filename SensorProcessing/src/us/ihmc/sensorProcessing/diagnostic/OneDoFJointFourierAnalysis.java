01e33c1941f392bbad5d8f20264c2f4648b281ca
Sylvain Bertrand <sbertrand@ihmc.us>
2016-01-14 20:55:29 -0600

    Added the option to use DoubleYoVariables to get measurements.

--- SensorProcessing/src/us/ihmc/sensorProcessing/diagnostic/OneDoFJointFourierAnalysis.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/diagnostic/OneDoFJointFourierAnalysis.java
@@ -3,6 +3,7 @@ package us.ihmc.sensorProcessing.diagnostic;
 import org.ejml.data.DenseMatrix64F;
 
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
 import us.ihmc.robotics.screwTheory.OneDoFJoint;
 
 public class OneDoFJointFourierAnalysis implements DiagnosticUpdatable
@@ -15,7 +16,17 @@ public class OneDoFJointFourierAnalysis implements DiagnosticUpdatable
    private final Online1DSignalFourierAnalysis tauFourierAnalysis;
    private final Online1DSignalFourierAnalysis tauDesiredFourierAnalysis;
 
+   private final boolean hasYoVariables;
+   private final DoubleYoVariable velocity;
+   private final DoubleYoVariable tau;
+   private final DoubleYoVariable tauDesired;
+
    public OneDoFJointFourierAnalysis(OneDoFJoint joint, double estimationWindow, double dt, YoVariableRegistry parentRegistry)
+   {
+      this(joint, estimationWindow, dt, null, null, null, parentRegistry);
+   }
+
+   public OneDoFJointFourierAnalysis(OneDoFJoint joint, double estimationWindow, double dt, DoubleYoVariable velocity, DoubleYoVariable tau, DoubleYoVariable tauDesired, YoVariableRegistry parentRegistry)
    {
       this.joint = joint;
       String jointName = joint.getName();
@@ -26,6 +37,23 @@ public class OneDoFJointFourierAnalysis implements DiagnosticUpdatable
       velocityFourierAnalysis = new Online1DSignalFourierAnalysis(jointName + "Velocity", estimationWindow, dt, registry);
       tauFourierAnalysis = new Online1DSignalFourierAnalysis(jointName + "Tau", estimationWindow, dt, registry);
       tauDesiredFourierAnalysis = new Online1DSignalFourierAnalysis(jointName + "TauDesired", estimationWindow, dt, registry);
+
+      if (velocity == null || tau == null || tauDesired == null)
+      {
+         hasYoVariables = false;
+         this.velocity = null;
+         this.tau = null;
+         this.tauDesired = null;
+      }
+      else
+      {
+         hasYoVariables = true;
+         verifyYoVariableNames(jointName, velocity, tau, tauDesired);
+         this.velocity = velocity;
+         this.tau = tau;
+         this.tauDesired = tauDesired;
+      }
+
    }
 
    @Override
@@ -47,9 +75,18 @@ public class OneDoFJointFourierAnalysis implements DiagnosticUpdatable
    @Override
    public void update()
    {
-      velocityFourierAnalysis.update(joint.getQd());
-      tauFourierAnalysis.update(joint.getTauMeasured());
-      tauDesiredFourierAnalysis.update(joint.getTau());
+      if (hasYoVariables)
+      {
+         velocityFourierAnalysis.update(velocity.getDoubleValue());
+         tauFourierAnalysis.update(tau.getDoubleValue());
+         tauDesiredFourierAnalysis.update(tauDesired.getDoubleValue());
+      }
+      else
+      {
+         velocityFourierAnalysis.update(joint.getQd());
+         tauFourierAnalysis.update(joint.getTauMeasured());
+         tauDesiredFourierAnalysis.update(joint.getTau());
+      }
    }
 
    public int getOutputSize()
@@ -115,5 +152,15 @@ public class OneDoFJointFourierAnalysis implements DiagnosticUpdatable
    public boolean hasAnalysisStarted()
    {
       return velocityFourierAnalysis.hasAnalysisStarted();
+   }
+
+   private void verifyYoVariableNames(String jointName, DoubleYoVariable velocity, DoubleYoVariable tau, DoubleYoVariable tauDesired)
+   {
+      if (!velocity.getName().contains(jointName))
+         throw new RuntimeException("The velocity variable: " + velocity.getName() + " may not belong to the joint: " + jointName);
+      if (!tau.getName().contains(jointName))
+         throw new RuntimeException("The tau variable: " + tau.getName() + " may not belong to the joint: " + jointName);
+      if (!tauDesired.getName().contains(jointName))
+         throw new RuntimeException("The tauDesired variable: " + tauDesired.getName() + " may not belong to the joint: " + jointName);
    }
 }
