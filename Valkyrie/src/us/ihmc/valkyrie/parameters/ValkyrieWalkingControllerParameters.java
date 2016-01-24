c73ebdc3f4533cdb2d029b6517e6ffa07a322b3d
Valkyrie Operator Computer <OperatorComputer@unknown>
2016-01-24 11:48:25 -0600

    Cranked up the damping on chest and pelvis controllers.

--- Valkyrie/src/us/ihmc/valkyrie/parameters/ValkyrieWalkingControllerParameters.java
+++ Valkyrie/src/us/ihmc/valkyrie/parameters/ValkyrieWalkingControllerParameters.java
@@ -521,8 +521,8 @@ public class ValkyrieWalkingControllerParameters implements WalkingControllerPar
 
       double kpXY = runningOnRealRobot ? 100.0 : 100.0; // 160.0
       double kpZ = runningOnRealRobot ? 80.0 : 100.0; // 120.0
-      double zetaXY = runningOnRealRobot ? 0.5 : 0.8; // 0.7
-      double zetaZ = runningOnRealRobot ? 0.5 : 0.8; // 0.7
+      double zetaXY = runningOnRealRobot ? 0.8 : 0.8; // 0.7
+      double zetaZ = runningOnRealRobot ? 0.8 : 0.8; // 0.7
       double maxAccel = runningOnRealRobot ? 18.0 : 18.0;
       double maxJerk = runningOnRealRobot ? 270.0 : 270.0;
 
@@ -601,9 +601,9 @@ public class ValkyrieWalkingControllerParameters implements WalkingControllerPar
       boolean runningOnRealRobot = target == DRCRobotModel.RobotTarget.REAL_ROBOT;
 
       double kpXY = runningOnRealRobot ? 80.0 : 100.0;
-      double kpZ = runningOnRealRobot ? 40.0 : 100.0;
-      double zetaXY = runningOnRealRobot ? 0.7 : 0.8;
-      double zetaZ = runningOnRealRobot ? 0.5 : 0.8;
+      double kpZ = runningOnRealRobot ? 60.0 : 100.0;
+      double zetaXY = runningOnRealRobot ? 0.8 : 0.8;
+      double zetaZ = runningOnRealRobot ? 0.8 : 0.8;
       double maxAccel = runningOnRealRobot ? 12.0 : 18.0;
       double maxJerk = runningOnRealRobot ? 180.0 : 270.0;
 
