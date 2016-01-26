492a548a40da63f240fcd4983e42db1c93fc598a
Kyle Cesare <kyle.cesare@disney.com>
2016-01-26 15:50:12 -0800

    Allow user to trigger state machine events.

--- AwareQuadrupedRobotics/src/us/ihmc/aware/controller/QuadrupedControllerManager.java
+++ AwareQuadrupedRobotics/src/us/ihmc/aware/controller/QuadrupedControllerManager.java
@@ -3,31 +3,40 @@ package us.ihmc.aware.controller;
 import us.ihmc.aware.parameters.QuadrupedRuntimeEnvironment;
 import us.ihmc.aware.state.StateMachine;
 import us.ihmc.aware.state.StateMachineBuilder;
+import us.ihmc.aware.state.StateMachineYoVariableTrigger;
 import us.ihmc.quadrupedRobotics.parameters.QuadrupedRobotParameters;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
-import us.ihmc.robotics.dataStructures.variable.EnumYoVariable;
 import us.ihmc.sensorProcessing.model.RobotMotionStatusHolder;
 import us.ihmc.simulationconstructionset.robotController.RobotController;
 
+/**
+ * A {@link RobotController} for switching between other robot controllers according to an internal finite state
+ * machine.
+ *
+ * Users can manually fire events on the {@code userTrigger} YoVariable.
+ */
 public class QuadrupedControllerManager implements RobotController
 {
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
    private final RobotMotionStatusHolder motionStatusHolder = new RobotMotionStatusHolder();
 
    private final StateMachine<QuadrupedController, QuadrupedControllerEvent> stateMachine;
+   private final StateMachineYoVariableTrigger<QuadrupedControllerEvent> userEventTrigger;
 
-   private final EnumYoVariable<QuadrupedControllerEvent> userEvent = new EnumYoVariable<>("userEvent", registry, QuadrupedControllerEvent.class, true);
-
-   public QuadrupedControllerManager(QuadrupedRuntimeEnvironment runtimeEnvironment, QuadrupedRobotParameters parameters)
+   public QuadrupedControllerManager(QuadrupedRuntimeEnvironment runtimeEnvironment,
+         QuadrupedRobotParameters parameters)
    {
       QuadrupedDoNothingController doNothingController = new QuadrupedDoNothingController(runtimeEnvironment);
-      QuadrupedStandPrepController standPrepController = new QuadrupedStandPrepController(runtimeEnvironment, parameters);
+      QuadrupedStandPrepController standPrepController = new QuadrupedStandPrepController(runtimeEnvironment,
+            parameters);
 
       // TODO: Define more state transitions.
       StateMachineBuilder<QuadrupedController, QuadrupedControllerEvent> builder = new StateMachineBuilder<>();
       builder.addTransition(QuadrupedControllerEvent.JOINTS_INITIALIZED, doNothingController, standPrepController);
 
-      stateMachine = builder.build(doNothingController);
+      this.stateMachine = builder.build(doNothingController);
+      this.userEventTrigger = new StateMachineYoVariableTrigger<>(stateMachine, "userTrigger", registry,
+            QuadrupedControllerEvent.class);
    }
 
    @Override
@@ -39,12 +48,6 @@ public class QuadrupedControllerManager implements RobotController
    @Override
    public void doControl()
    {
-      if (userEvent.getEnumValue() != null)
-      {
-         stateMachine.trigger(userEvent.getEnumValue());
-         userEvent.set(null);
-      }
-
       stateMachine.process();
    }
 
