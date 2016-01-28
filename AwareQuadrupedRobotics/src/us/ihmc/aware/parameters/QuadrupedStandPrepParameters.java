319d5ce01c3d1f4f46fb5b090d3d898cb7560a3a
Mike Hopkins <michael.a.hopkins@disney.com>
2016-01-27 18:43:59 -0800

    add parameter map to quadruped step controller

--- AwareQuadrupedRobotics/src/us/ihmc/aware/parameters/QuadrupedStandPrepParameters.java
+++
@@ -1,9 +0,0 @@
-package us.ihmc.aware.parameters;
-
-public interface QuadrupedStandPrepParameters
-{
-   /**
-    * @return the allowed time for the preparation to complete in seconds.
-    */
-   public double getTrajectoryTime();
-}
