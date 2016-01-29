8683e4bad5a939c53362fa733eab56b1a9336cab
Sylvain Bertrand <sbertrand@ihmc.us>
2016-01-29 14:26:28 -0600

    Extracted SpiralBasedAlgorithm from SphereVoxelShape.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/reachabilityMapCalculator/voxelPrimitiveShapes/SphereVoxelShape.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/reachabilityMapCalculator/voxelPrimitiveShapes/SphereVoxelShape.java
@@ -2,8 +2,6 @@ package us.ihmc.darpaRoboticsChallenge.reachabilityMapCalculator.voxelPrimitiveS
 
 import java.awt.Color;
 
-import javax.vecmath.AxisAngle4d;
-import javax.vecmath.Matrix3d;
 import javax.vecmath.Point3d;
 import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
@@ -12,12 +10,10 @@ import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
 import us.ihmc.graphics3DAdapter.graphics.appearances.AppearanceDefinition;
 import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
 import us.ihmc.robotics.MathTools;
-import us.ihmc.robotics.geometry.AngleTools;
 import us.ihmc.robotics.geometry.FrameOrientation;
 import us.ihmc.robotics.geometry.FramePoint;
 import us.ihmc.robotics.geometry.FrameVector;
-import us.ihmc.robotics.geometry.GeometryTools;
-import us.ihmc.robotics.geometry.RotationTools;
+import us.ihmc.robotics.geometry.SpiralBasedAlgorithm;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 
 /**
@@ -51,42 +47,8 @@ public class SphereVoxelShape
       this.numberOfRays = numberOfRays;
       this.numberOfRotationsAroundRay = numberOfRotationsAroundRay;
 
-      pointsOnSphere = generatePointsOnSphereUsingSpiralBasedAlgorithm(sphereOrigin, voxelSize, numberOfRays);
-      rotations = new Quat4d[numberOfRays][numberOfRotationsAroundRay];
-      
-      Vector3d rayThroughSphere = new Vector3d();
-      
-      AxisAngle4d rotationForXAxisAlignedWithRay = new AxisAngle4d();
-      AxisAngle4d rotationAroundRay = new AxisAngle4d();
-      Matrix3d finalRotationMatrix = new Matrix3d();
-      Matrix3d rotationMatrixForXAxisAlignedWithRay = new Matrix3d();
-      Matrix3d rotationMatrixAroundRay = new Matrix3d();
-      
-      Vector3d xAxis = new Vector3d(1.0, 0.0, 0.0);
-      double stepSizeAngleArounRay = 2.0 * Math.PI / numberOfRotationsAroundRay;
-      
-      for (int rayIndex = 0; rayIndex < numberOfRays; rayIndex++)
-      {
-         // Ray that goes from the surface of the sphere to its origin
-         rayThroughSphere.sub(sphereOrigin, pointsOnSphere[rayIndex]);
-         rayThroughSphere.normalize();
-         
-         GeometryTools.getRotationBasedOnNormal(rotationForXAxisAlignedWithRay, rayThroughSphere, xAxis);
-         rotationMatrixForXAxisAlignedWithRay.set(rotationForXAxisAlignedWithRay);
-
-         for (int rotationAroundRayIndex = 0; rotationAroundRayIndex < numberOfRotationsAroundRay; rotationAroundRayIndex++)
-         {
-            double angle = rotationAroundRayIndex * stepSizeAngleArounRay;
-            rotationAroundRay.set(rayThroughSphere, angle);
-            rotationMatrixAroundRay.set(rotationAroundRay);
-
-            finalRotationMatrix.mul(rotationMatrixAroundRay, rotationMatrixForXAxisAlignedWithRay);
-
-            Quat4d rotation = new Quat4d();
-            RotationTools.convertMatrixToQuaternion(finalRotationMatrix, rotation);
-            rotations[rayIndex][rotationAroundRayIndex] = rotation;
-         }
-      }
+      pointsOnSphere = SpiralBasedAlgorithm.generatePointsOnSphere(sphereOrigin, voxelSize, numberOfRays);
+      rotations = SpiralBasedAlgorithm.generateOrientations(numberOfRays, numberOfRotationsAroundRay);
    }
 
    public int getNumberOfRays()
@@ -147,53 +109,5 @@ public class SphereVoxelShape
       voxelLocation.changeFrame(originalFrame);
 
       return voxelViz;
-   }
-
-   public static Point3d[] generatePointsOnSphereUsingSpiralBasedAlgorithm(double sphereRadius, int numberOfPointsToGenerate)
-   {
-      return generatePointsOnSphereUsingSpiralBasedAlgorithm(new Point3d(), sphereRadius, numberOfPointsToGenerate);
-   }
-
-   /**
-    * Generates a number of points uniformly distributed over the surface of a sphere using a spiral-based approach.
-    * This algorithm can be found in the paper: "Distributing Many Points on a Sphere" by E.B. Saff and B.J. Kuijlaars. (PDF version was on Google on the 02/13/2015).
-    */
-   public static Point3d[] generatePointsOnSphereUsingSpiralBasedAlgorithm(Point3d sphereOrigin, double sphereRadius, int numberOfPointsToGenerate)
-   {
-      Point3d[] pointsOnSphere = new Point3d[numberOfPointsToGenerate];
-
-      // Two solutions are suggested for deltaN:
-//      double deltaN = Math.sqrt(8.0 * Math.PI / Math.sqrt(3)) / Math.sqrt(numberOfPoints);
-      // From the paper it is said that the following can also be used. It seems to be a magic number at the end.
-      double deltaN = 3.6 / Math.sqrt(numberOfPointsToGenerate);
-      double phi;
-      double previousPhi = 0.0;
-
-      pointsOnSphere[0] = new Point3d();
-      pointsOnSphere[0].set(0.0, 0.0, -sphereRadius);
-      pointsOnSphere[0].add(sphereOrigin);
-
-      for (int planeIndex = 1; planeIndex < numberOfPointsToGenerate - 1; planeIndex++)
-      {
-         double unitHeight = -1.0 + 2.0 * planeIndex / (numberOfPointsToGenerate - 1.0);
-         double theta = Math.acos(unitHeight);
-         phi = previousPhi + deltaN / Math.sqrt(1 - unitHeight * unitHeight);
-         AngleTools.trimAngleMinusPiToPi(phi);
-
-         double rSinTheta = sphereRadius * Math.sin(theta);
-         pointsOnSphere[planeIndex] = new Point3d();
-         pointsOnSphere[planeIndex].x = rSinTheta * Math.cos(phi);
-         pointsOnSphere[planeIndex].y = rSinTheta * Math.sin(phi);
-         pointsOnSphere[planeIndex].z = sphereRadius * Math.cos(theta);
-         pointsOnSphere[planeIndex].add(sphereOrigin);
-
-         previousPhi = phi;
-      }
-
-      pointsOnSphere[numberOfPointsToGenerate - 1] = new Point3d();
-      pointsOnSphere[numberOfPointsToGenerate - 1].set(0.0, 0.0, sphereRadius);
-      pointsOnSphere[numberOfPointsToGenerate - 1].add(sphereOrigin);
-
-      return pointsOnSphere;
    }
 }
