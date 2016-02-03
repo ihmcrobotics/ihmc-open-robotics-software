40e3d1994e9f7d18ccc68c8fe16072acf2824705
Kyle Cesare <kyle.cesare@disney.com>
2016-02-03 13:54:58 -0800

    Add empty VMC stand controller.

---
+++ AwareQuadrupedRobotics/src/us/ihmc/aware/controller/QuadrupedVirtualModelBasedStandController.java
@@ -0,0 +1,22 @@
+package us.ihmc.aware.controller;
+
+public class QuadrupedVirtualModelBasedStandController implements QuadrupedController
+{
+   @Override
+   public void onEntry()
+   {
+      
+   }
+
+   @Override
+   public QuadrupedControllerEvent process()
+   {
+      return null;
+   }
+
+   @Override
+   public void onExit()
+   {
+
+   }
+}
