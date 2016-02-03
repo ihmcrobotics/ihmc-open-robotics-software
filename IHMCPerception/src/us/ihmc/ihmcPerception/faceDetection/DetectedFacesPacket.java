ab06f3b881e169b5110018ee3a758f288bacb770
nate_merts <eN6itF786e42De2dp3nuP46gt>
2016-02-03 08:30:11 -0600

    BEAST-195: added beginnings of packet communication between ROSJava face detection module and neck tracking behavior

--- IHMCPerception/src/us/ihmc/ihmcPerception/faceDetection/DetectedFacesPacket.java
+++ IHMCPerception/src/us/ihmc/ihmcPerception/faceDetection/DetectedFacesPacket.java
@@ -1,8 +1,10 @@
 package us.ihmc.ihmcPerception.faceDetection;
 
+import us.ihmc.communication.packets.Packet;
+
 import javax.vecmath.Point3d;
 
-public class DetectedFacesPacket
+public class DetectedFacesPacket extends Packet<DetectedFacesPacket>
 {
    public String[] ids;
    public Point3d[] positions;
@@ -26,5 +28,17 @@ public class DetectedFacesPacket
    public Point3d[] getPositions()
    {
       return positions;
+   }
+
+   @Override public boolean epsilonEquals(DetectedFacesPacket other, double epsilon)
+   {
+      boolean ret = true;
+
+      for(int i = 0; i < ids.length; i++)
+      {
+         ret &= ids[i].equals(other.getIds()[i]) && positions[i].epsilonEquals(other.getPositions()[i], epsilon);
+      }
+
+      return ret;
    }
 }
