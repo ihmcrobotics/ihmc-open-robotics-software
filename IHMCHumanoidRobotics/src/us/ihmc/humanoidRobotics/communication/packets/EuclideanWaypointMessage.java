9a413f3cb5b20e2c93ac327dc9ffeb53cb0b023e
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-10 11:37:48 -0600

    Changed the waypoints to be ROS messages.

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/EuclideanWaypointMessage.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/EuclideanWaypointMessage.java
@@ -5,12 +5,13 @@ import javax.vecmath.Vector3d;
 
 import us.ihmc.communication.packetAnnotations.ClassDocumentation;
 import us.ihmc.communication.packetAnnotations.FieldDocumentation;
+import us.ihmc.communication.packets.IHMCRosApiPacket;
 import us.ihmc.robotics.MathTools;
 import us.ihmc.robotics.math.trajectories.EuclideanWaypointInterface;
 
 @ClassDocumentation("This class is used to build trajectory messages in taskspace. It holds the only the translational information for one waypoint (position & linear velocity). "
       + "Feel free to look at SO3Waypoint (rotational) and SE3Waypoint (rotational AND translational)")
-public class EuclideanWaypoint implements EuclideanWaypointInterface
+public class EuclideanWaypointMessage extends IHMCRosApiPacket<EuclideanWaypointMessage> implements EuclideanWaypointInterface
 {
    @FieldDocumentation("Time at which the waypoint has to be reached. The time is relative to when the trajectory starts.")
    public double time;
@@ -19,20 +20,20 @@ public class EuclideanWaypoint implements EuclideanWaypointInterface
    @FieldDocumentation("Define the desired 3D linear velocity to be reached at this waypoint. It is expressed in world frame.")
    public Vector3d linearVelocity;
 
-   public EuclideanWaypoint()
+   public EuclideanWaypointMessage()
    {
    }
 
-   public EuclideanWaypoint(EuclideanWaypoint euclideanWaypoint)
+   public EuclideanWaypointMessage(EuclideanWaypointMessage euclideanWaypointMessage)
    {
-      if (euclideanWaypoint.position != null)
-         position = new Point3d(euclideanWaypoint.position);
-      if (euclideanWaypoint.linearVelocity != null)
-         linearVelocity = new Vector3d(euclideanWaypoint.linearVelocity);
-      time = euclideanWaypoint.time;
+      if (euclideanWaypointMessage.position != null)
+         position = new Point3d(euclideanWaypointMessage.position);
+      if (euclideanWaypointMessage.linearVelocity != null)
+         linearVelocity = new Vector3d(euclideanWaypointMessage.linearVelocity);
+      time = euclideanWaypointMessage.time;
    }
 
-   public EuclideanWaypoint(double time, Point3d position, Vector3d linearVelocity)
+   public EuclideanWaypointMessage(double time, Point3d position, Vector3d linearVelocity)
    {
       this.position = position;
       this.linearVelocity = linearVelocity;
@@ -72,7 +73,7 @@ public class EuclideanWaypoint implements EuclideanWaypointInterface
       this.linearVelocity = linearVelocity;
    }
 
-   public boolean epsilonEquals(EuclideanWaypoint other, double epsilon)
+   public boolean epsilonEquals(EuclideanWaypointMessage other, double epsilon)
    {
       if (position == null && other.position != null)
          return false;
