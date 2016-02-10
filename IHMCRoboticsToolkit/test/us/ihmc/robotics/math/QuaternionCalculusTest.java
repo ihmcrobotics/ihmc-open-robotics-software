8e3bad3da79c50cf35d96de76306df48daa89ede
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-09 20:03:01 -0600

    Added a test for the finite difference stuff.

--- IHMCRoboticsToolkit/test/us/ihmc/robotics/math/QuaternionCalculusTest.java
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/math/QuaternionCalculusTest.java
@@ -1,9 +1,11 @@
 package us.ihmc.robotics.math;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
 
+import javax.vecmath.AxisAngle4d;
 import javax.vecmath.Matrix3d;
 import javax.vecmath.Quat4d;
 import javax.vecmath.Vector3d;
@@ -11,6 +13,7 @@ import javax.vecmath.Vector3d;
 import org.junit.Test;
 
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.geometry.AngleTools;
 import us.ihmc.robotics.geometry.FrameOrientation;
 import us.ihmc.robotics.geometry.FrameVector;
 import us.ihmc.robotics.math.trajectories.SimpleOrientationTrajectoryGenerator;
@@ -130,6 +133,55 @@ public class QuaternionCalculusTest
          quaternionCalculus.computeAngularVelocity(q, qDot, actualAngularVelocity);
 
          assertTrue(expectedAngularVelocity.epsilonEquals(actualAngularVelocity, 1.0e-8));
+      }
+   }
+
+   @DeployableTestMethod(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
+   public void testFDSimpleCase() throws Exception
+   {
+      QuaternionCalculus quaternionCalculus = new QuaternionCalculus();
+      Random random = new Random(65265L);
+      double integrationTime = 1.0;
+      double angleVelocity = RandomTools.generateRandomDouble(random, 0.0, 2.0 * Math.PI) / integrationTime;
+      Vector3d expectedAngularVelocity = new Vector3d(angleVelocity, 0.0, 0.0);
+      Vector3d expectedAngularAcceleration = new Vector3d();
+      AxisAngle4d axisAnglePrevious = new AxisAngle4d(1.0, 0.0, 0.0, 0.0);
+      AxisAngle4d axisAngleCurrent = new AxisAngle4d(1.0, 0.0, 0.0, 0.0);
+      AxisAngle4d axisAngleNext = new AxisAngle4d(1.0, 0.0, 0.0, 0.0);
+      Quat4d qPrevious = new Quat4d();
+      Quat4d qCurrent = new Quat4d();
+      Quat4d qNext = new Quat4d();
+      Quat4d qDot = new Quat4d();
+      Quat4d qDDot = new Quat4d();
+      
+      Vector3d actualAngularVelocity = new Vector3d();
+      Vector3d actualAngularAcceleration = new Vector3d();
+
+      double dt = 1.0e-4;
+      for (double time = dt; time < integrationTime; time += dt)
+      {
+         axisAnglePrevious.setAngle(AngleTools.trimAngleMinusPiToPi(angleVelocity * (time - dt)) - Math.PI);
+         qPrevious.set(axisAnglePrevious);
+         axisAngleCurrent.setAngle(AngleTools.trimAngleMinusPiToPi(angleVelocity * time) - Math.PI);
+         qCurrent.set(axisAngleCurrent);
+         axisAngleNext.setAngle(AngleTools.trimAngleMinusPiToPi(angleVelocity * (time + dt)) - Math.PI);
+         qNext.set(axisAngleNext);
+
+         quaternionCalculus.computeQDotByFiniteDifferenceCentral(qPrevious, qNext, dt, qDot);
+         quaternionCalculus.computeAngularVelocity(qCurrent, qDot, actualAngularVelocity);
+         
+         quaternionCalculus.computeQDDotByFiniteDifferenceCentral(qPrevious, qCurrent, qNext, dt, qDDot);
+         quaternionCalculus.computeAngularAcceleration(qCurrent, qDot, qDDot, actualAngularAcceleration);
+
+         boolean sameVelocity = expectedAngularVelocity.epsilonEquals(actualAngularVelocity, 1.0e-7);
+         if (!sameVelocity)
+         {
+            System.out.println("Expected angular velocity: " + expectedAngularVelocity);
+            System.out.println("Actual   angular velocity: " + actualAngularVelocity);
+         }
+         assertTrue(sameVelocity);
+         assertTrue(expectedAngularAcceleration.epsilonEquals(actualAngularAcceleration, 1.0e-7));
       }
    }
 
