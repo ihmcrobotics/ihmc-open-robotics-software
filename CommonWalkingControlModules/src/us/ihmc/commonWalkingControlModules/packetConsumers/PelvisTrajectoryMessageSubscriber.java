ef15fe8bd47a640fc9e4795728748b78b63da72a
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-10 20:17:39 -0600

    Created the PelvisTrajectoryMessageSubscriber.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/PelvisTrajectoryMessageSubscriber.java
@@ -0,0 +1,43 @@
+package us.ihmc.commonWalkingControlModules.packetConsumers;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import us.ihmc.communication.net.PacketConsumer;
+import us.ihmc.humanoidRobotics.communication.packets.walking.PelvisTrajectoryMessage;
+import us.ihmc.humanoidRobotics.communication.streamingData.HumanoidGlobalDataProducer;
+
+public class PelvisTrajectoryMessageSubscriber implements PacketConsumer<PelvisTrajectoryMessage>
+{
+   private final HumanoidGlobalDataProducer globalDataProducer;
+
+   private final AtomicReference<PelvisTrajectoryMessage> latestMessageReference = new AtomicReference<PelvisTrajectoryMessage>(null);
+   
+   public PelvisTrajectoryMessageSubscriber(HumanoidGlobalDataProducer globalDataProducer)
+   {
+      this.globalDataProducer = globalDataProducer;
+   }
+
+   public boolean isNewTrajectoryMessageAvailable()
+   {
+      return latestMessageReference.get() != null;
+   }
+
+   public PelvisTrajectoryMessage pollMessage()
+   {
+      return latestMessageReference.getAndSet(null);
+   }
+
+   public void clearMessagesInQueue()
+   {
+      latestMessageReference.set(null);
+   }
+
+   @Override
+   public void receivedPacket(PelvisTrajectoryMessage pelvisTrajectoryMessage)
+   {
+      if (!PacketValidityChecker.validatePelvisTrajectoryMessage(pelvisTrajectoryMessage, globalDataProducer))
+         return;
+
+      latestMessageReference.set(pelvisTrajectoryMessage);
+   }
+}
