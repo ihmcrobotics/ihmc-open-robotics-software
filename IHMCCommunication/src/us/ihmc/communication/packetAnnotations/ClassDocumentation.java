f90da48d0e240d60c38c91d16bc92f33f5a895aa
Nathan Mertins <nmertins@ihmc.us>
2016-02-12 16:28:04 -0600

    ROSI-348: added documentation for generated ROS messages to javadoc

--- IHMCCommunication/src/us/ihmc/communication/packetAnnotations/ClassDocumentation.java
+++ IHMCCommunication/src/us/ihmc/communication/packetAnnotations/ClassDocumentation.java
@@ -1,17 +1,11 @@
 package us.ihmc.communication.packetAnnotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-/**
- * Created by agrabertilton on 10/15/14.
- */
-
-
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface ClassDocumentation {
+public @interface ClassDocumentation
+{
    String value() default "No Documentation For This Class Is Recorded.";
 }
--- IHMCCommunication/src/us/ihmc/communication/packetAnnotations/FieldDocumentation.java
+++ IHMCCommunication/src/us/ihmc/communication/packetAnnotations/FieldDocumentation.java
@@ -1,10 +1,8 @@
 package us.ihmc.communication.packetAnnotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface FieldDocumentation
