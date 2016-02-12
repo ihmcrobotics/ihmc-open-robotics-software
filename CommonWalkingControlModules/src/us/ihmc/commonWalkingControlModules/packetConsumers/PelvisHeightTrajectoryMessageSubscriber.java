5924f52e69c75f991b95a685b3b268b6b9646ed7
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-11 18:41:51 -0600

    Created the PelvisHeightTrajectorySubscriber.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/packetConsumers/PelvisHeightTrajectoryMessageSubscriber.java
@@ -0,0 +1,43 @@
+package us.ihmc.commonWalkingControlModules.packetConsumers;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import us.ihmc.communication.net.PacketConsumer;
+import us.ihmc.humanoidRobotics.communication.packets.walking.PelvisHeightTrajectoryMessage;
+import us.ihmc.humanoidRobotics.communication.streamingData.HumanoidGlobalDataProducer;
+
+public class PelvisHeightTrajectoryMessageSubscriber implements PacketConsumer<PelvisHeightTrajectoryMessage>
+{
+   private final HumanoidGlobalDataProducer globalDataProducer;
+
+   private final AtomicReference<PelvisHeightTrajectoryMessage> latestMessageReference = new AtomicReference<PelvisHeightTrajectoryMessage>(null);
+
+   public PelvisHeightTrajectoryMessageSubscriber(HumanoidGlobalDataProducer globalDataProducer)
+   {
+      this.globalDataProducer = globalDataProducer;
+   }
+
+   public boolean isNewTrajectoryMessageAvailable()
+   {
+      return latestMessageReference.get() != null;
+   }
+
+   public PelvisHeightTrajectoryMessage pollMessage()
+   {
+      return latestMessageReference.getAndSet(null);
+   }
+
+   public void clearMessagesInQueue()
+   {
+      latestMessageReference.set(null);
+   }
+
+   @Override
+   public void receivedPacket(PelvisHeightTrajectoryMessage pelvisHeightTrajectoryMessage)
+   {
+      if (!PacketValidityChecker.validatePelvisHeightTrajectoryMessage(pelvisHeightTrajectoryMessage, globalDataProducer))
+         return;
+
+      latestMessageReference.set(pelvisHeightTrajectoryMessage);
+   }
+}
