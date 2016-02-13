f712b28b8abce0c7b1f624d3305f133c83cdd9b8
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-13 17:22:25 -0600

    Revert "NONE: Created visualiser for Hermit based orientation trajectory generator based on the visualisation for velocity constrained orientation trajectory"
    This reverts commit e8ecad4b4e7850c8ed305166eee5b1980c22a722.

--- IHMCRoboticsToolkit/test/us/ihmc/robotics/math/trajectories/HermiteCurveBasedOrientationTrajectoryGeneratorTest.java
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/math/trajectories/HermiteCurveBasedOrientationTrajectoryGeneratorTest.java
@@ -88,7 +88,7 @@ public class HermiteCurveBasedOrientationTrajectoryGeneratorTest
       ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
       Random random = new Random(5165165161L);
       HermiteCurveBasedOrientationTrajectoryGenerator traj = new HermiteCurveBasedOrientationTrajectoryGenerator("traj", worldFrame, new YoVariableRegistry("null"));
-      double trajectoryTime = 5.0;
+      double trajectoryTime = 3.0;
       traj.setTrajectoryTime(trajectoryTime);
 
       for (int i = 0; i < 10000; i++)
@@ -112,11 +112,10 @@ public class HermiteCurveBasedOrientationTrajectoryGeneratorTest
          traj.compute(0.0 + dt);
          traj.packAngularData(currentOrientation, currentAngularVelocity, currentAngularAcceleration);
 
-         double orientationEpsilon = 5.0e-5;
-         double velocityEpsilon = 5.0e-4;
+         double epsilon = 5.0e-5;
          double accelerationEpsilon = 5.0e-4;
-         assertTrue(initialOrientation.epsilonEquals(currentOrientation, orientationEpsilon));
-         boolean goodInitialVelocity = initialAngularVelocity.epsilonEquals(currentAngularVelocity, velocityEpsilon);
+         assertTrue(initialOrientation.epsilonEquals(currentOrientation, epsilon));
+         boolean goodInitialVelocity = initialAngularVelocity.epsilonEquals(currentAngularVelocity, epsilon);
          if (DEBUG && !goodInitialVelocity)
          {
             FrameVector error = new FrameVector();
@@ -125,45 +124,30 @@ public class HermiteCurveBasedOrientationTrajectoryGeneratorTest
             printLimitConditions(initialOrientation, initialAngularVelocity, finalOrientation, finalAngularVelocity);
          }
          assertTrue(goodInitialVelocity);
-
-         //         boolean goodInitialAngularAcceleration = zeroAngularAcceleration.epsilonEquals(currentAngularAcceleration, accelerationEpsilon);
-         //         if (DEBUG && !goodInitialAngularAcceleration)
-         //         {
-         //            FrameVector error = new FrameVector();
-         //            error.sub(zeroAngularAcceleration, currentAngularAcceleration);
-         //            System.out.println("Bad initial acceleration, error: " + error);
+         boolean goodInitialAngularAcceleration = zeroAngularAcceleration.epsilonEquals(currentAngularAcceleration, accelerationEpsilon);
+         
+         if (DEBUG && !goodInitialAngularAcceleration)
+         {
+            FrameVector error = new FrameVector();
+            error.sub(zeroAngularAcceleration, currentAngularAcceleration);
+            System.out.println("Bad initial acceleration, error: " + error);
 //            printLimitConditions(initialOrientation, zeroAngularAcceleration, finalOrientation, zeroAngularAcceleration);
-         //         }
-         //
-         //         assertTrue(goodInitialAngularAcceleration);
+         }
+         
+         assertTrue(goodInitialAngularAcceleration);
 
          traj.compute(trajectoryTime - dt);
          traj.packAngularData(currentOrientation, currentAngularVelocity, currentAngularAcceleration);
 
-         assertTrue(finalOrientation.epsilonEquals(currentOrientation, orientationEpsilon));
-
-         boolean goodFinalAngularVelocity = finalAngularVelocity.epsilonEquals(currentAngularVelocity, velocityEpsilon);
-         if (DEBUG && !goodFinalAngularVelocity)
+         assertTrue(finalOrientation.epsilonEquals(currentOrientation, epsilon));
+         assertTrue(finalAngularVelocity.epsilonEquals(currentAngularVelocity, epsilon));
+         boolean goodFinalAngularAcceleration = zeroAngularAcceleration.epsilonEquals(currentAngularAcceleration, accelerationEpsilon);
+         if (DEBUG && !goodFinalAngularAcceleration)
          {
-            FrameVector error = new FrameVector();
-            error.sub(finalAngularVelocity, currentAngularVelocity);
-            System.out.println("Bad final velocity, error: " + error);
-            System.out.println("final X angular velocity " + currentAngularVelocity.getX());
-            System.out.println("final Y angular velocity " + currentAngularVelocity.getY());
-            System.out.println("final Z angular velocity " + currentAngularVelocity.getZ());
-
+            System.out.println("Bad final acceleration: " + currentAngularAcceleration);
             printLimitConditions(initialOrientation, initialAngularVelocity, finalOrientation, finalAngularVelocity);
          }
-
-         assertTrue(goodFinalAngularVelocity);
-
-         //         boolean goodFinalAngularAcceleration = zeroAngularAcceleration.epsilonEquals(currentAngularAcceleration, accelerationEpsilon);
-         //         if (DEBUG && !goodFinalAngularAcceleration)
-         //         {
-         //            System.out.println("Bad final acceleration: " + currentAngularAcceleration);
-         //            printLimitConditions(initialOrientation, initialAngularVelocity, finalOrientation, finalAngularVelocity);
-         //         }
-         //         assertTrue(goodFinalAngularAcceleration);
+         assertTrue(goodFinalAngularAcceleration);
       }
    }
 
