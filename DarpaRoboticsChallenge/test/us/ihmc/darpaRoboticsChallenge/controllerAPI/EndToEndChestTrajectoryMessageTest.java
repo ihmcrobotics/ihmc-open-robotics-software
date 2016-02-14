11c51dc3159e2b54680cc6c05fd17613a678b2d6
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-13 20:46:21 -0600

    Created end-to-end test for ChestTrajectoryMessage. Cleanup names.

---
+++ DarpaRoboticsChallenge/test/us/ihmc/darpaRoboticsChallenge/controllerAPI/EndToEndChestTrajectoryMessageTest.java
@@ -0,0 +1,126 @@
+package us.ihmc.darpaRoboticsChallenge.controllerAPI;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import javax.vecmath.Quat4d;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.SdfLoader.SDFFullHumanoidRobotModel;
+import us.ihmc.darpaRoboticsChallenge.DRCObstacleCourseStartingLocation;
+import us.ihmc.darpaRoboticsChallenge.MultiRobotTestInterface;
+import us.ihmc.darpaRoboticsChallenge.testTools.DRCSimulationTestHelper;
+import us.ihmc.humanoidRobotics.communication.packets.walking.ChestTrajectoryMessage;
+import us.ihmc.humanoidRobotics.frames.HumanoidReferenceFrames;
+import us.ihmc.robotics.geometry.FrameOrientation;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+import us.ihmc.robotics.screwTheory.OneDoFJoint;
+import us.ihmc.robotics.screwTheory.RigidBody;
+import us.ihmc.robotics.screwTheory.ScrewTestTools;
+import us.ihmc.robotics.screwTheory.ScrewTools;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.bambooTools.SimulationTestingParameters;
+import us.ihmc.tools.MemoryTools;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
+import us.ihmc.tools.thread.ThreadTools;
+
+public abstract class EndToEndChestTrajectoryMessageTest implements MultiRobotTestInterface
+{
+   private static final SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromEnvironmentVariables();
+
+   private DRCSimulationTestHelper drcSimulationTestHelper;
+
+   @DeployableTestMethod(estimatedDuration = 50.0)
+   @Test (timeout = 300000)
+   public void testSingleWaypoint() throws Exception
+   {
+      BambooTools.reportTestStartedMessage();
+
+      Random random = new Random(564574L);
+      double epsilon = 1.0e-5;
+
+      DRCObstacleCourseStartingLocation selectedLocation = DRCObstacleCourseStartingLocation.DEFAULT;
+
+      drcSimulationTestHelper = new DRCSimulationTestHelper(getClass().getSimpleName(), "", selectedLocation, simulationTestingParameters, getRobotModel());
+
+      ThreadTools.sleep(1000);
+      boolean success = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(0.5);
+      assertTrue(success);
+
+      SDFFullHumanoidRobotModel fullRobotModel = drcSimulationTestHelper.getControllerFullRobotModel();
+
+      double trajectoryTime = 1.0;
+      RigidBody pelvis = fullRobotModel.getPelvis();
+      RigidBody chest = fullRobotModel.getChest();
+
+      OneDoFJoint[] spineClone = ScrewTools.cloneOneDoFJointPath(pelvis, chest);
+
+      ScrewTestTools.setRandomPositionsWithinJointLimits(spineClone, random);
+
+      RigidBody chestClone = spineClone[spineClone.length - 1].getSuccessor();
+      FrameOrientation desiredRandomChestOrientation = new FrameOrientation(chestClone.getBodyFixedFrame());
+      desiredRandomChestOrientation.changeFrame(ReferenceFrame.getWorldFrame());
+
+      Quat4d desiredOrientation = new Quat4d();
+      desiredRandomChestOrientation.getQuaternion(desiredOrientation);
+      ChestTrajectoryMessage chestTrajectoryMessage = new ChestTrajectoryMessage(trajectoryTime, desiredOrientation);
+
+      HumanoidReferenceFrames humanoidReferenceFrames = new HumanoidReferenceFrames(fullRobotModel);
+      humanoidReferenceFrames.updateFrames();
+      desiredRandomChestOrientation.changeFrame(humanoidReferenceFrames.getPelvisZUpFrame());
+
+      drcSimulationTestHelper.send(chestTrajectoryMessage);
+
+      success = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(1.0 + trajectoryTime);
+      assertTrue(success);
+
+      SimulationConstructionSet scs = drcSimulationTestHelper.getSimulationConstructionSet();
+
+      String chestPrefix = "chest";
+      String orientationTrajectoryName = chestPrefix + "MultipleWaypointsOrientationTrajectoryGenerator";
+      String numberOfWaypointsVarName = chestPrefix + "NumberOfWaypoints";
+      String subTrajectoryName = chestPrefix + "SubTrajectory";
+      String currentOrientationVarNamePrefix = subTrajectoryName + "CurrentOrientation";
+
+      double numberOfWaypoints = scs.getVariable(orientationTrajectoryName, numberOfWaypointsVarName).getValueAsDouble();
+      assertEquals(2.0, numberOfWaypoints, 0.1);
+
+      double trajOutput = scs.getVariable(subTrajectoryName, currentOrientationVarNamePrefix + "Qx").getValueAsDouble();
+      assertEquals(desiredRandomChestOrientation.getQx(), trajOutput, epsilon);
+      trajOutput = scs.getVariable(subTrajectoryName, currentOrientationVarNamePrefix + "Qy").getValueAsDouble();
+      assertEquals(desiredRandomChestOrientation.getQy(), trajOutput, epsilon);
+      trajOutput = scs.getVariable(subTrajectoryName, currentOrientationVarNamePrefix + "Qz").getValueAsDouble();
+      assertEquals(desiredRandomChestOrientation.getQz(), trajOutput, epsilon);
+      trajOutput = scs.getVariable(subTrajectoryName, currentOrientationVarNamePrefix + "Qs").getValueAsDouble();
+      assertEquals(desiredRandomChestOrientation.getQs(), trajOutput, epsilon);
+   }
+
+   @Before
+   public void showMemoryUsageBeforeTest()
+   {
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
+   }
+
+   @After
+   public void destroySimulationAndRecycleMemory()
+   {
+      if (simulationTestingParameters.getKeepSCSUp())
+      {
+         ThreadTools.sleepForever();
+      }
+
+      // Do this here in case a test fails. That way the memory will be recycled.
+      if (drcSimulationTestHelper != null)
+      {
+         drcSimulationTestHelper.destroySimulation();
+         drcSimulationTestHelper = null;
+      }
+
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
+   }
+}
