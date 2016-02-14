dfec4bfd3c2fab7c83dcf8882a1a523ac0f39dbc
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-14 16:18:44 -0600

    Emptying the grave yard.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCDemoEnvironmentWithBoxAndSteeringWheel.java
+++ DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/environment/DRCDemoEnvironmentWithBoxAndSteeringWheel.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import javax.vecmath.Vector3d;
 
-import us.ihmc.darpaRoboticsChallenge.DRCVehicleModelObjects;
+import us.ihmc.darpaRoboticsChallenge.VehicleModelObjects;
 import us.ihmc.darpaRoboticsChallenge.controllers.SteeringWheelDisturbanceController;
 import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
 import us.ihmc.graveYard.commonWalkingControlModules.vrc.highLevelHumanoidControl.driving.VehicleObject;
@@ -35,7 +35,7 @@ public class DRCDemoEnvironmentWithBoxAndSteeringWheel implements CommonAvatarEn
 
    public DRCDemoEnvironmentWithBoxAndSteeringWheel(YoGraphicsListRegistry yoGraphicsListRegistry)
    {
-      DRCVehicleModelObjects drcVehicleModelObjects = new DRCVehicleModelObjects();
+      VehicleModelObjects drcVehicleModelObjects = new VehicleModelObjects();
 
 
 
@@ -48,7 +48,7 @@ public class DRCDemoEnvironmentWithBoxAndSteeringWheel implements CommonAvatarEn
       createBars(environmentRobots, contactables);
    }
 
-   private static ContactableToroidRobot createSteeringWheel(DRCVehicleModelObjects drcVehicleModelObjects)
+   private static ContactableToroidRobot createSteeringWheel(VehicleModelObjects drcVehicleModelObjects)
    {
       RigidBodyTransform steeringWheelTransform = drcVehicleModelObjects.getTransform(VehicleObject.STEERING_WHEEL);
 
