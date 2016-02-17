682feaab51c832f210f1a4a0d29a04a694d5ec8b
Duncan Calvert <dcalvert@ihmc.us>
2016-02-17 13:35:56 -0600

    NONE: Remove some ignores and generate Atlas test suites.

--- Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasFVideoATestSuite.java
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasFVideoATestSuite.java
@@ -12,7 +12,8 @@ import us.ihmc.tools.testing.TestPlanTarget;
 @TestSuiteTarget(TestPlanTarget.VideoA)
 @SuiteClasses
 ({
-   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseEveryBuildTest.class
+   us.ihmc.atlas.ObstacleCourseTests.AtlasFinalsWorldStairsTest.class,
+   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseDoNothingTest.class
 })
 
 public class AtlasFVideoATestSuite
