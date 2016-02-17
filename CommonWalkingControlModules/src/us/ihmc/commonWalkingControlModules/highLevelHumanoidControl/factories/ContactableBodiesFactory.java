9c01b6c71194d95e3e60cef4e4027074e117d02f
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-17 15:32:16 -0600

    Removed the obsolete contactable bodies.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/ContactableBodiesFactory.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/ContactableBodiesFactory.java
@@ -22,38 +22,7 @@ public class ContactableBodiesFactory
    private SideDependentList<List<Point2d>> handContactPoints = null;
    private SideDependentList<RigidBodyTransform> handContactPointTransforms = null;
 
-   private SideDependentList<String> namesOfJointsBeforeThighs = null;
-   private SideDependentList<List<Point2d>> thighContactPoints = null;
-   private SideDependentList<RigidBodyTransform> thighContactPointTransforms = null;
-
-   private List<Point2d> pelvisContactPoints = null;
-   private RigidBodyTransform pelvisContactPointTransform = null;
-
-   private List<Point2d> pelvisBackContactPoints = null;
-   private RigidBodyTransform pelvisBackContactPointTransform = null;
-
-   private List<Point2d> chestBackContactPoints = null;
-   private RigidBodyTransform chestBackContactPointTransform = null;
-
    private SideDependentList<? extends List<Point2d>> footContactPoints = null;
-
-   public void addPelvisContactParameters(List<Point2d> pelvisContactPoints, RigidBodyTransform pelvisContactPointTransform)
-   {
-      this.pelvisContactPointTransform = pelvisContactPointTransform;
-      this.pelvisContactPoints = pelvisContactPoints;
-   }
-
-   public void addPelvisBackContactParameters(List<Point2d> pelvisBackContactPoints, RigidBodyTransform pelvisBackContactPointTransform)
-   {
-      this.pelvisBackContactPoints = pelvisBackContactPoints;
-      this.pelvisBackContactPointTransform = pelvisBackContactPointTransform;
-   }
-
-   public void addChestBackContactParameters(List<Point2d> chestBackContactPoints, RigidBodyTransform chestBackContactPointTransform)
-   {
-      this.chestBackContactPoints = chestBackContactPoints;
-      this.chestBackContactPointTransform = chestBackContactPointTransform;
-   }
 
    public void addHandContactParameters(SideDependentList<String> namesOfJointsBeforeHands, SideDependentList<List<Point2d>> handContactPoints,
          SideDependentList<RigidBodyTransform> handContactPointTransforms)
@@ -63,41 +32,9 @@ public class ContactableBodiesFactory
       this.handContactPointTransforms = handContactPointTransforms;
    }
 
-   public void addThighContactParameters(SideDependentList<String> namesOfJointsBeforeThighs, SideDependentList<List<Point2d>> thighContactPoints,
-         SideDependentList<RigidBodyTransform> thighContactPointTransforms)
-   {
-      this.namesOfJointsBeforeThighs = namesOfJointsBeforeThighs;
-      this.thighContactPoints = thighContactPoints;
-      this.thighContactPointTransforms = thighContactPointTransforms;
-   }
-
    public void addFootContactParameters(SideDependentList<? extends List<Point2d>> footContactPoints)
    {
       this.footContactPoints = footContactPoints;
-   }
-
-   public ContactablePlaneBody createPelvisContactableBody(RigidBody pelvis)
-   {
-      if (pelvisContactPointTransform == null)
-         return null;
-
-      return createListOfPointsContactablePlaneBody("PelvisContact", pelvis, pelvisContactPointTransform, pelvisContactPoints);
-   }
-
-   public ContactablePlaneBody createPelvisBackContactableBody(RigidBody pelvis)
-   {
-      if (pelvisBackContactPointTransform == null)
-         return null;
-
-      return createListOfPointsContactablePlaneBody("PelvisBackContact", pelvis, pelvisBackContactPointTransform, pelvisBackContactPoints);
-   }
-
-   public ContactablePlaneBody createChestBackContactableBody(RigidBody chest)
-   {
-      if (chestBackContactPointTransform == null)
-         return null;
-
-      return createListOfPointsContactablePlaneBody("ChestBackContact", chest, chestBackContactPointTransform, chestBackContactPoints);
    }
 
    public SideDependentList<ContactablePlaneBody> createHandContactableBodies(RigidBody rootBody)
@@ -122,30 +59,6 @@ public class ContactableBodiesFactory
          handContactableBodies.put(robotSide, handContactableBody);
       }
       return handContactableBodies;
-   }
-
-   public SideDependentList<ContactablePlaneBody> createThighContactableBodies(RigidBody rootBody)
-   {
-      if (namesOfJointsBeforeThighs == null)
-         return null;
-
-      InverseDynamicsJoint[] allJoints = ScrewTools.computeSupportAndSubtreeJoints(rootBody);
-
-      SideDependentList<ContactablePlaneBody> thighContactableBodies = new SideDependentList<>();
-
-      for (RobotSide robotSide : RobotSide.values)
-      {
-         InverseDynamicsJoint[] jointBeforeThighArray = ScrewTools.findJointsWithNames(allJoints, namesOfJointsBeforeThighs.get(robotSide));
-         if (jointBeforeThighArray.length != 1)
-            throw new RuntimeException("Incorrect number of joints before thigh found: " + jointBeforeThighArray.length);
-
-         RigidBody thigh = jointBeforeThighArray[0].getSuccessor();
-         String name = robotSide.getCamelCaseNameForStartOfExpression() + "ThighContact";
-         ListOfPointsContactablePlaneBody thighContactableBody = createListOfPointsContactablePlaneBody(name, thigh, thighContactPointTransforms.get(robotSide),
-               thighContactPoints.get(robotSide));
-         thighContactableBodies.put(robotSide, thighContactableBody);
-      }
-      return thighContactableBodies;
    }
 
    public SideDependentList<ContactablePlaneBody> createFootContactableBodies(FullHumanoidRobotModel fullRobotModel,
--- IHMCWholeBodyController/src/us/ihmc/wholeBodyController/ExoskeletonContactPointParameters.java
+++ IHMCWholeBodyController/src/us/ihmc/wholeBodyController/ExoskeletonContactPointParameters.java
@@ -9,24 +9,11 @@ import javax.vecmath.Vector3d;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ContactableBodiesFactory;
-import us.ihmc.robotics.geometry.RigidBodyTransform;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.simulationconstructionset.util.LinearGroundContactModel;
 
 public abstract class ExoskeletonContactPointParameters
 {
-   public abstract RigidBodyTransform getPelvisContactPointTransform();
-
-   public abstract List<Point2d> getPelvisContactPoints();
-
-   public abstract RigidBodyTransform getPelvisBackContactPointTransform();
-
-   public abstract List<Point2d> getPelvisBackContactPoints();
-
-   public abstract SideDependentList<RigidBodyTransform> getThighContactPointTransforms();
-
-   public abstract SideDependentList<List<Point2d>> getThighContactPoints();
-
    public abstract List<ImmutablePair<String, Vector3d>> getJointNameGroundContactPointMap();
 
    public abstract SideDependentList<ArrayList<Point2d>> getFootContactPoints();
