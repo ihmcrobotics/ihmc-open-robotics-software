55fd2a078837393d843d6b8c0c2c3f56942e43fc
Elena Galbally <egalbally@ihmc.us>
2016-02-18 14:32:20 -0600

    Added some comments to the test. Changed the number of iterations. Got rid of some unnecessary parameters.

--- IHMCRoboticsToolkit/test/us/ihmc/robotics/kinematics/fourbar/FourBarCalculatorsComparisonTest.java
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/kinematics/fourbar/FourBarCalculatorsComparisonTest.java
@@ -16,6 +16,29 @@ import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
  */
 public class FourBarCalculatorsComparisonTest
 {
+   /*
+    * Notation used:
+    * 
+    *    D--------A
+    *    |\      /|
+    *    | \e   / |
+    *    |  \  /  |
+    *    |   \/   |
+    *    |   /\   |
+    *    |  /  \  |
+    *    | /f   \ |
+    *    |/      \|
+    *    C--------B
+    *    
+    *    For the calculator that is not from fastRunner:
+    *    AD = outputLink
+    *    AB = groundLink
+    *    BC = inputLink
+    *    CD = floatingLink
+    *    
+    *    Additionally it takes as an input the supplementary of angle BAD
+    *        
+    */
    private static final double eps = 1e-7;
 
    @DeployableTestMethod
@@ -23,7 +46,7 @@ public class FourBarCalculatorsComparisonTest
    public void equalOuputAnglesForRandomQuadrilatteralTest()
    {
       Random rand = new Random(1986L);
-      for (int j = 0; j < 10000; j++)
+      for (int j = 0; j < 150000; j++) // set the number of quadrilaterals such that the tests runs in about 0.5 seconds
       {
          // Generate quadrilatteral
          double e = 100 * (rand.nextDouble() + 0.001);
@@ -31,13 +54,16 @@ public class FourBarCalculatorsComparisonTest
          double k2 = rand.nextDouble();
          double d1 = e * abs(rand.nextGaussian());
          double d2 = e * abs(rand.nextGaussian());
-         double DE = e * k1, DF = e * k2, BE = e * (1 - k1), BF = e * (1 - k2);
+         
          double AE = d1, CF = d2;
-         double AD = sqrt(DE * DE + AE * AE), DAE = atan2(DE, AE), ADE = atan2(AE, DE);
-         double AB = sqrt(AE * AE + BE * BE), BAE = atan2(BE, AE), ABE = atan2(AE, BE);
-         double CD = sqrt(CF * CF + DF * DF), CDF = atan2(CF, DF), DCF = atan2(DF, CF);
-         double BC = sqrt(BF * BF + CF * CF), CBF = atan2(CF, BF), BCF = atan2(BF, CF);
-         double BAD = DAE + BAE, ABC = ABE + CBF, BCD = BCF + DCF, ADC = ADE + CDF;
+         double DE = e * k1, BE = e * (1 - k1); 
+         double DF = e * k2, BF = e * (1 - k2);
+         
+         double AD = sqrt(DE * DE + AE * AE), DAE = atan2(DE, AE);
+         double AB = sqrt(AE * AE + BE * BE), BAE = atan2(BE, AE);
+         double CD = sqrt(CF * CF + DF * DF);
+         double BC = sqrt(BF * BF + CF * CF);
+         double BAD = DAE + BAE;
 
          // Test
          testCalculators(AD, AB, BC, CD, BAD);
