0c82e2ab55b88dbfa640d2ee34dd4c0b85980e56
Kyle Cesare <kyle.cesare@disney.com>
2016-02-18 11:42:12 -0800

    Rename pack -> get.

--- AwareQuadrupedRobotics/src/us/ihmc/aware/controller/force/QuadrupedVirtualModelBasedStandController.java
+++ AwareQuadrupedRobotics/src/us/ihmc/aware/controller/force/QuadrupedVirtualModelBasedStandController.java
@@ -389,10 +389,10 @@ public class QuadrupedVirtualModelBasedStandController implements QuadrupedForce
       // compute sole poses and twists
       for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
       {
-         twistCalculator.packTwistOfBody(twistStorage, footRigidBody.get(robotQuadrant));
+         twistCalculator.getTwistOfBody(twistStorage, footRigidBody.get(robotQuadrant));
          twistStorage.changeFrame(soleFrame.get(robotQuadrant));
-         twistStorage.packAngularPart(soleAngularVelocityEstimate.get(robotQuadrant));
-         twistStorage.packLinearPart(soleLinearVelocityEstimate.get(robotQuadrant));
+         twistStorage.getAngularPart(soleAngularVelocityEstimate.get(robotQuadrant));
+         twistStorage.getLinearPart(soleLinearVelocityEstimate.get(robotQuadrant));
          soleOrientationEstimate.get(robotQuadrant).setToZero(soleFrame.get(robotQuadrant));
          solePositionEstimate.get(robotQuadrant).setToZero(soleFrame.get(robotQuadrant));
       }
@@ -412,16 +412,16 @@ public class QuadrupedVirtualModelBasedStandController implements QuadrupedForce
       supportFrame.setPoseAndUpdate(supportCentroidEstimate, supportOrientationEstimate);
 
       // compute body pose and twist
-      twistCalculator.packTwistOfBody(twistStorage, fullRobotModel.getPelvis());
+      twistCalculator.getTwistOfBody(twistStorage, fullRobotModel.getPelvis());
       twistStorage.changeFrame(bodyFrame);
-      twistStorage.packAngularPart(bodyAngularVelocityEstimate);
-      twistStorage.packLinearPart(bodyLinearVelocityEstimate);
+      twistStorage.getAngularPart(bodyAngularVelocityEstimate);
+      twistStorage.getLinearPart(bodyLinearVelocityEstimate);
       bodyOrientationEstimate.setToZero(bodyFrame);
       bodyPositionEstimate.setToZero(bodyFrame);
 
       // compute center of mass position and velocity
       comPositionEstimate.setToZero(comFrame);
-      comJacobian.packCenterOfMassVelocity(comVelocityEstimate);
+      comJacobian.getCenterOfMassVelocity(comVelocityEstimate);
 
       // compute divergent component of motion and instantaneous capture point
       comPositionEstimate.changeFrame(worldFrame);
