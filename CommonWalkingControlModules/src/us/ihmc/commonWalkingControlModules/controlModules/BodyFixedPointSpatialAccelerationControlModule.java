0259289f4c3b684b6b3d55b7d123f60e7e059d8c
Sylvain Bertrand <sbertrand@ihmc.us>
2016-02-21 11:32:09 -0600

    Cleaned up the spatial control modules.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/BodyFixedPointSpatialAccelerationControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/BodyFixedPointSpatialAccelerationControlModule.java
@@ -1,5 +1,6 @@
 package us.ihmc.commonWalkingControlModules.controlModules;
 
+import us.ihmc.robotics.controllers.SE3PIDGainsInterface;
 import us.ihmc.robotics.controllers.YoSE3PIDGainsInterface;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.geometry.FrameOrientation;
@@ -18,21 +19,21 @@ public class BodyFixedPointSpatialAccelerationControlModule
    private final YoTranslationFrame bodyFixedPointFrame;
    private final RigidBodySpatialAccelerationControlModule accelerationControlModule;
 
-   public BodyFixedPointSpatialAccelerationControlModule(String namePrefix, TwistCalculator twistCalculator, RigidBody endEffector,
-         double dt, YoVariableRegistry parentRegistry)
+   public BodyFixedPointSpatialAccelerationControlModule(String namePrefix, TwistCalculator twistCalculator, RigidBody endEffector, double dt,
+         YoVariableRegistry parentRegistry)
    {
       this(namePrefix, twistCalculator, endEffector, dt, null, parentRegistry);
    }
 
-   public BodyFixedPointSpatialAccelerationControlModule(String namePrefix, TwistCalculator twistCalculator, RigidBody endEffector,
-         double dt, YoSE3PIDGainsInterface taskspaceGains, YoVariableRegistry parentRegistry)
+   public BodyFixedPointSpatialAccelerationControlModule(String namePrefix, TwistCalculator twistCalculator, RigidBody endEffector, double dt,
+         YoSE3PIDGainsInterface taskspaceGains, YoVariableRegistry parentRegistry)
    {
       registry = new YoVariableRegistry(namePrefix + getClass().getSimpleName());
 
       bodyFixedPointFrame = new YoTranslationFrame("bodyFixedPointFrame", endEffector.getBodyFixedFrame(), registry);
-      
-      accelerationControlModule = new RigidBodySpatialAccelerationControlModule(namePrefix, twistCalculator,
-            endEffector, bodyFixedPointFrame, dt, taskspaceGains, registry);
+
+      accelerationControlModule = new RigidBodySpatialAccelerationControlModule(namePrefix, twistCalculator, endEffector, bodyFixedPointFrame, dt,
+            taskspaceGains, registry);
 
       parentRegistry.addChild(registry);
    }
@@ -51,12 +52,18 @@ public class BodyFixedPointSpatialAccelerationControlModule
    public void doPositionControl(FramePoint desiredPosition, FrameOrientation desiredOrientation, FrameVector desiredLinearVelocityOfOrigin,
          FrameVector desiredAngularVelocity, FrameVector desiredLinearAccelerationOfOrigin, FrameVector desiredAngularAcceleration, RigidBody base)
    {
-      accelerationControlModule.doPositionControl(desiredPosition, desiredOrientation, desiredLinearVelocityOfOrigin, desiredAngularVelocity, desiredLinearAccelerationOfOrigin, desiredAngularAcceleration, base);
+      accelerationControlModule.doPositionControl(desiredPosition, desiredOrientation, desiredLinearVelocityOfOrigin, desiredAngularVelocity,
+            desiredLinearAccelerationOfOrigin, desiredAngularAcceleration, base);
    }
 
    public void setPointToControl(FramePoint bodyFixedPointInEndEffectorFrame)
    {
       bodyFixedPointFrame.setTranslationToParent(bodyFixedPointInEndEffectorFrame);
+   }
+
+   public void setGains(SE3PIDGainsInterface gains)
+   {
+      accelerationControlModule.setGains(gains);
    }
 
    public void setToControlBodyFixedFrame()
@@ -72,6 +79,16 @@ public class BodyFixedPointSpatialAccelerationControlModule
    public RigidBody getEndEffector()
    {
       return accelerationControlModule.getEndEffector();
+   }
+
+   public void getEndEffectorCurrentLinearVelocity(FrameVector endEffectorLinearVelocityToPack)
+   {
+      accelerationControlModule.getEndEffectorCurrentLinearVelocity(endEffectorLinearVelocityToPack);
+   }
+
+   public void getEndEffectorCurrentAngularVelocity(FrameVector endEffectorAngularVelocityToPack)
+   {
+      accelerationControlModule.getEndEffectorCurrentLinearVelocity(endEffectorAngularVelocityToPack);
    }
 
    public void getAcceleration(SpatialAccelerationVector accelerationToPack)
