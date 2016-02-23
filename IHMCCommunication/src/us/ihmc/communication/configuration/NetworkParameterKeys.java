1ba653093679985fecc094585904a4d06e95f3e6
Doug Stephen <dstephen@ihmc.us>
2016-02-23 15:52:28 -0600

    Make Network Parameters settable from Environment Variables, and make the file itself optional. Only crash if a required parameter doesn't exist.

--- IHMCCommunication/src/us/ihmc/communication/configuration/NetworkParameterKeys.java
+++ IHMCCommunication/src/us/ihmc/communication/configuration/NetworkParameterKeys.java
@@ -2,27 +2,29 @@ package us.ihmc.communication.configuration;
 
 public enum NetworkParameterKeys
 {
-   robotController(true, "Hostname/IP of the robot controller.", "127.0.0.1"),
-   logger(true, "Hostname/IP of the logger or visualizer network.", "127.0.0.1"),
-   networkManager(false, "Hostname/IP of the network manager, as seen from the communication shaper.", "127.0.0.1"),
+   robotController(true, "Hostname/IP of the robot controller.", "127.0.0.1", true),
+   logger(true, "Hostname/IP of the logger or visualizer network.", "127.0.0.1", true),
+   networkManager(false, "Hostname/IP of the network manager, as seen from the communication shaper.", "127.0.0.1", true),
    
-   rosURI(false, "Fully qualified ROS master URI.", "http://127.0.0.1:11311"),
-   head(false, "Hostname/IP of the head.", ""),
-   leftHand(false, "Hostname/IP of the left hand.", ""),
-   rightHand(false, "Hostname/IP of the right hand.", ""), 
+   rosURI(false, "Fully qualified ROS master URI.", "http://127.0.0.1:11311", true),
+   head(false, "Hostname/IP of the head.", "", true),
+   leftHand(false, "Hostname/IP of the left hand.", "", true),
+   rightHand(false, "Hostname/IP of the right hand.", "", true),
    
-   loggedCameras(false, "Cameras to be recorded by the logger. Should be a comma seperated list", "");
+   loggedCameras(false, "Cameras to be recorded by the logger. Should be a comma seperated list", "", false);
    
    
    private final boolean required;
    private final String description;
    private final String defaultValue;
-   
-   private NetworkParameterKeys(boolean required, String description, String defaultValue)
+   private final boolean isIPAddress;
+
+   private NetworkParameterKeys(boolean required, String description, String defaultValue, boolean isIPAddress)
    {
       this.required = required;
       this.description = description; 
       this.defaultValue = defaultValue;
+      this.isIPAddress = isIPAddress;
    }
    
    
@@ -35,7 +37,12 @@ public enum NetworkParameterKeys
    {
       return required;
    }
-   
+
+   public boolean isIPAddress()
+   {
+      return isIPAddress;
+   }
+
    public String getDefaultValue()
    {
       return defaultValue;
