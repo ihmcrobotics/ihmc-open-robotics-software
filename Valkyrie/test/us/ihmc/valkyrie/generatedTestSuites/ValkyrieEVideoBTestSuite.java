6f4f18b5d3e1e047dba724a611fdce2a95f6e31d
Duncan Calvert <dcalvert@ihmc.us>
2016-03-01 15:13:12 -0600

    NONE: Valkyrie is much faster now so re-load balance.

--- Valkyrie/test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieEVideoBTestSuite.java
+++ Valkyrie/test/us/ihmc/valkyrie/generatedTestSuites/ValkyrieEVideoBTestSuite.java
@@ -12,7 +12,7 @@ import us.ihmc.tools.testing.TestPlanTarget;
 @TestSuiteTarget(TestPlanTarget.VideoB)
 @SuiteClasses
 ({
-   us.ihmc.valkyrie.ValkyrieObstacleCourseTrialsTerrainTest.class
+   us.ihmc.valkyrie.ValkyriePushRecoveryWalkingTest.class
 })
 
 public class ValkyrieEVideoBTestSuite
