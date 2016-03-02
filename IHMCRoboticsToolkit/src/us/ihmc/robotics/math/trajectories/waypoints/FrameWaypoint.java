21426506af39c00bb36efc6757049d1baa0a5ca7
JerryPratt <jpratt@ihmc.us>
2016-03-02 16:03:04 -0600

    ROSI-379: Removed WaypointInterface.
    GeometryObject covers it now.

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/math/trajectories/waypoints/FrameWaypoint.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/math/trajectories/waypoints/FrameWaypoint.java
@@ -2,12 +2,12 @@ package us.ihmc.robotics.math.trajectories.waypoints;
 
 import us.ihmc.robotics.geometry.AbstractReferenceFrameHolder;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
+import us.ihmc.robotics.geometry.interfaces.GeometryObject;
 import us.ihmc.robotics.geometry.transformables.Transformable;
-import us.ihmc.robotics.math.trajectories.waypoints.interfaces.WaypointInterface;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 
-public abstract class FrameWaypoint<S extends Transformable & WaypointInterface<S>, F extends FrameWaypoint<S, F>>
-      extends AbstractReferenceFrameHolder implements Transformable, WaypointInterface<F>
+public abstract class FrameWaypoint<S extends Transformable & GeometryObject<S>, F extends FrameWaypoint<S, F>>
+      extends AbstractReferenceFrameHolder implements Transformable, GeometryObject<F>
 {
    private ReferenceFrame referenceFrame = ReferenceFrame.getWorldFrame();
    protected final S simpleWaypoint;
