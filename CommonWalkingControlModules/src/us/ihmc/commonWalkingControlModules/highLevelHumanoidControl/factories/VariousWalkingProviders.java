20cd78a5a6a83991a0e203a796f05ef0ca440755
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-02 16:18:26 -0600

    High level state now goes through the API.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/VariousWalkingProviders.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/VariousWalkingProviders.java
@@ -3,23 +3,18 @@ package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories;
 import us.ihmc.commonWalkingControlModules.desiredFootStep.AbortWalkingMessageSubscriber;
 import us.ihmc.commonWalkingControlModules.desiredFootStep.FootstepProvider;
 import us.ihmc.commonWalkingControlModules.packetProducers.CapturabilityBasedStatusProducer;
-import us.ihmc.commonWalkingControlModules.packetProviders.HighLevelStateMessageSubscriber;
 
 public class VariousWalkingProviders
 {
    private final FootstepProvider footstepProvider;
    private final AbortWalkingMessageSubscriber abortWalkingMessageSubscriber;
 
-   private final HighLevelStateMessageSubscriber highLevelStateMessageSubscriber;
-
    // TODO: Shouldn't really be in providers but this class is the easiest to access
    private final CapturabilityBasedStatusProducer capturabilityBasedStatusProducer;
 
    public VariousWalkingProviders(FootstepProvider footstepProvider,
-         HighLevelStateMessageSubscriber highLevelStateMessageSubscriber, CapturabilityBasedStatusProducer capturabilityBasedStatusProducer,
-         AbortWalkingMessageSubscriber abortWalkingMessageSubscriber)
+         CapturabilityBasedStatusProducer capturabilityBasedStatusProducer, AbortWalkingMessageSubscriber abortWalkingMessageSubscriber)
    {
-      this.highLevelStateMessageSubscriber = highLevelStateMessageSubscriber;
       this.footstepProvider = footstepProvider;
 
       this.capturabilityBasedStatusProducer = capturabilityBasedStatusProducer;
@@ -32,11 +27,6 @@ public class VariousWalkingProviders
       {
          this.abortWalkingMessageSubscriber = abortWalkingMessageSubscriber;
       }
-   }
-
-   public HighLevelStateMessageSubscriber getHighLevelStateMessageSubscriber()
-   {
-      return highLevelStateMessageSubscriber;
    }
 
    public FootstepProvider getFootstepProvider()
