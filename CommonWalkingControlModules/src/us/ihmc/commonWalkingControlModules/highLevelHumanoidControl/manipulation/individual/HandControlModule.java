ec1a600b0950b64cc7dce8df1765d5513a89dd07
Wolfgang Merkt <w.merkt@gmail.com>
2016-03-03 17:45:27 +0000

    Potential fix for #17 - please verify safety!! (applied again after Doug's fix)

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/manipulation/individual/HandControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/manipulation/individual/HandControlModule.java
@@ -912,7 +912,7 @@ public class HandControlModule
             if (!MathTools.isInsideBoundsInclusive(position, oneDoFJoints[jointIdx].getJointLimitLower(), oneDoFJoints[jointIdx].getJointLimitUpper()))
             {
                PrintTools.warn(this, "Waypoint " + i + " is out of the joint position limits for joint " + oneDoFJoints[jointIdx].getName() + ", cancelling action.");
-               trajectoryGenerator.clear();
+               // trajectoryGenerator.clear();
                return;
             }
 
