1455444bd9091db02ea335a960cae15fea104a60
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-02 23:50:18 -0600

    Organized imports.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerAPI/output/ControllerStatusOutputManager.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerAPI/output/ControllerStatusOutputManager.java
@@ -1,7 +1,6 @@
 package us.ihmc.commonWalkingControlModules.controllerAPI.output;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
