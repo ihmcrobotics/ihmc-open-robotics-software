4bc5be05d76fd2adfb704a8da38e96c4b4331da7
Duncan Calvert <dcalvert@ihmc.us>
2016-03-07 16:33:13 -0600

    BEAST-200: First hints of trotting. Remove slipping force prevention and clean up some.

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/sliderboard/QuadrupedTrotWalkSliderBoardConfiguration.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/sliderboard/QuadrupedTrotWalkSliderBoardConfiguration.java
@@ -28,7 +28,7 @@ public class QuadrupedTrotWalkSliderBoardConfiguration implements EnumDependentS
       
       String namespace = "root.babyBeastSimple.QuadrupedSimulationController.QuadrupedControllerManager.TrotWalkController.";
       sliderBoardConfigurationManager.setSlider(1, namespace + "q_d_z", scs, 0.4, 1.0);
-      sliderBoardConfigurationManager.setSlider(2, namespace + "quadAlpha", scs, -0.2, 1.2);
+      // sliderBoardConfigurationManager.setSlider(2, namespace + "quadAlpha", scs, -0.2, 1.2);
       sliderBoardConfigurationManager.setSlider(3, namespace + "q_d_y", scs, -0.1, 0.1);
       sliderBoardConfigurationManager.setSlider(4, namespace + "q_d_roll", scs, -0.2, 0.2);
       sliderBoardConfigurationManager.setSlider(5, namespace + "q_d_pitch", scs, -0.1, 0.1);
