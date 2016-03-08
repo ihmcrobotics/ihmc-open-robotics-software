aa79cd1600b03db107a1d9cc1a86dd0b0fbf328a
Stephen McCrory <smccrory@ihmc.us>
2016-03-08 11:59:36 -0600

    adding test to FourBarKinematicLoopTest.java for rectangle with out of plane joint

--- IHMCRoboticsToolkit/test/us/ihmc/robotics/screwTheory/FourBarKinematicLoopTest.java
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/screwTheory/FourBarKinematicLoopTest.java
@@ -53,16 +53,8 @@ public class FourBarKinematicLoopTest
       passiveJointD = ScrewTools.addPassiveRevoluteJoint("jointD", rigidBodyCD, new Vector3d(sideLength, 0.0, 0.0), jointAxis, true);
       rigidBodyDA = createAndAttachCylinderRB("rigidBodyDA", 10.0, 0.1, sideLength, passiveJointD);
       
-      boolean recomputeJointLimits = false;
-      
-      masterJointA.setJointLimitLower(0.0);
-      masterJointA.setJointLimitUpper(Math.PI);
-      passiveJointB.setJointLimitLower(0.0);
-      passiveJointB.setJointLimitUpper(Math.PI);
-      passiveJointC.setJointLimitLower(0.0);
-      passiveJointC.setJointLimitUpper(Math.PI);
-      passiveJointD.setJointLimitLower(0.0);
-      passiveJointD.setJointLimitUpper(Math.PI);
+      boolean recomputeJointLimits = false;      
+      initializeAllJointsToSameLimits(0.0, Math.PI);
       
       closurePointFromLastPassiveJoint = new Vector3d(1.0, 0.0, 0.0);
       
@@ -89,6 +81,70 @@ public class FourBarKinematicLoopTest
       assertEquals(passiveJointB.getQd(), -1.0, eps);
       assertEquals(passiveJointC.getQd(), 1.0, eps);
       assertEquals(passiveJointD.getQd(), -1.0, eps);
+   }
+   
+   @DeployableTestMethod(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
+   public void testRectangleWithJointOutOfPlane()
+   {
+      elevatorFrame = ReferenceFrame.constructFrameWithUnchangingTransformToParent("elevatorFrame", worldFrame, new RigidBodyTransform());
+      elevator = new RigidBody("elevator", elevatorFrame);
+      jointAxis = new Vector3d(0.0, 0.0, 1.0);
+      double sideLength1 = 1.0;
+      double sideLength2 = 1.5;
+      double zOffset = 0.2;
+      
+      masterJointA = ScrewTools.addRevoluteJoint("jointA", elevator, new RigidBodyTransform(), jointAxis);
+      rigidBodyAB = createAndAttachCylinderRB("rigidBodyAB", 10.0, 0.1, sideLength1, masterJointA);
+      passiveJointB = ScrewTools.addPassiveRevoluteJoint("jointB", rigidBodyAB, new Vector3d(sideLength1, 0.0, 0.0), jointAxis, true);
+      rigidBodyBC = createAndAttachCylinderRB("rigidBodyBC", 10.0, 0.1, sideLength2, passiveJointB);
+      passiveJointC = ScrewTools.addPassiveRevoluteJoint("jointC", rigidBodyBC, new Vector3d(sideLength2, 0.0, zOffset), jointAxis, true);
+      rigidBodyCD = createAndAttachCylinderRB("rigidBodyCD", 10.0, 0.1, sideLength1, passiveJointC);
+      passiveJointD = ScrewTools.addPassiveRevoluteJoint("jointD", rigidBodyCD, new Vector3d(sideLength1, 0.0, -zOffset), jointAxis, true);
+      rigidBodyDA = createAndAttachCylinderRB("rigidBodyDA", 10.0, 0.1, sideLength2, passiveJointD);
+
+      boolean recomputeJointLimits = false;
+      initializeAllJointsToSameLimits(0.0, Math.PI);
+
+      closurePointFromLastPassiveJoint = new Vector3d(sideLength2, 0.0, 0.0);
+      
+      fourBarKinematicLoop = new FourBarKinematicLoop("simpleSquare", masterJointA, passiveJointB, passiveJointC, passiveJointD, closurePointFromLastPassiveJoint, recomputeJointLimits);
+
+      // master joint is 90 degrees, 0 velocity
+      masterJointA.setQ(0.5 * Math.PI);
+      masterJointA.setQd(0.0);
+      fourBarKinematicLoop.updateAnglesAndVelocities();
+      assertEquals(passiveJointB.getQ(), -0.5 * Math.PI, eps);
+      assertEquals(passiveJointC.getQ(), -0.5 * Math.PI, eps);
+      assertEquals(passiveJointD.getQ(), -0.5 * Math.PI, eps);
+      assertEquals(passiveJointB.getQd(), 0.0, eps);
+      assertEquals(passiveJointC.getQd(), 0.0, eps);
+      assertEquals(passiveJointD.getQd(), 0.0, eps);
+      
+      // master joint is near maximum angle, non-zero velocity
+      double masterJointVelocity = 0.2;
+      double angleEpsilon = 1e-4;
+      masterJointA.setQ(Math.PI - angleEpsilon);
+      masterJointA.setQd(masterJointVelocity);
+      fourBarKinematicLoop.updateAnglesAndVelocities();
+      assertEquals(passiveJointB.getQ(), - Math.PI + angleEpsilon, eps);
+      assertEquals(passiveJointC.getQ(), - angleEpsilon, eps);
+      assertEquals(passiveJointD.getQ(), - Math.PI + angleEpsilon, eps);      
+      assertEquals(passiveJointB.getQd(), - masterJointVelocity, eps);
+      assertEquals(passiveJointC.getQd(), masterJointVelocity, eps);
+      assertEquals(passiveJointD.getQd(), - masterJointVelocity, eps);
+      
+      // master joint in near minimum angle, non-zero velocity
+      masterJointA.setQ(angleEpsilon);
+      masterJointA.setQd(masterJointVelocity);
+      fourBarKinematicLoop.updateAnglesAndVelocities();
+      assertEquals(passiveJointB.getQ(), - angleEpsilon, eps);
+      assertEquals(passiveJointC.getQ(), - Math.PI + angleEpsilon, eps);
+      assertEquals(passiveJointD.getQ(), - angleEpsilon, eps);      
+      assertEquals(passiveJointB.getQd(), - masterJointVelocity, eps);
+      assertEquals(passiveJointC.getQd(), masterJointVelocity, eps);
+      assertEquals(passiveJointD.getQd(), - masterJointVelocity, eps);
+      
    }
    
    private FourBarKinematicLoop createRandomFourBarKinematicLoop(Random random)
@@ -118,5 +174,18 @@ public class FourBarKinematicLoopTest
       Matrix3d inertiaCylinder = RotationalInertiaCalculator.getRotationalInertiaMatrixOfSolidCylinder(mass, radius, length, Axis.Z);
       Vector3d comOffsetCylinder = new Vector3d(0.0, 0.0, -length / 2.0);
       return ScrewTools.addRigidBody(name, parentJoint, inertiaCylinder, mass, comOffsetCylinder);
+   }
+   
+   private void initializeAllJointsToSameLimits(double lowerLimit, double upperLimit)
+   {
+      masterJointA.setJointLimitLower(lowerLimit);
+      masterJointA.setJointLimitUpper(upperLimit);
+      passiveJointB.setJointLimitLower(lowerLimit);
+      passiveJointB.setJointLimitUpper(upperLimit);
+      passiveJointC.setJointLimitLower(lowerLimit);
+      passiveJointC.setJointLimitUpper(upperLimit);
+      passiveJointD.setJointLimitLower(lowerLimit);
+      passiveJointD.setJointLimitUpper(upperLimit);
+
    }
 }
