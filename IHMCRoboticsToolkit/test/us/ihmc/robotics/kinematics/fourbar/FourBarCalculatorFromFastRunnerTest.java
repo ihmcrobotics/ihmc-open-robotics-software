e1a88338e6d6d77b747dfc5a91c0fa59c3e0847a
Elena Galbally <egalbally@ihmc.us>
2016-03-09 17:17:56 -0600

    Fixed methods that calculate the master joint angle given one of the other three and wrote junit test

--- IHMCRoboticsToolkit/test/us/ihmc/robotics/kinematics/fourbar/FourBarCalculatorFromFastRunnerTest.java
+++ IHMCRoboticsToolkit/test/us/ihmc/robotics/kinematics/fourbar/FourBarCalculatorFromFastRunnerTest.java
@@ -347,4 +347,38 @@ public class FourBarCalculatorFromFastRunnerTest
          assertEquals(BCD_numerical_tf, BCD_fourbar_tf, eps);
       }
    }
+	
+	@DeployableTestMethod
+   @Test(timeout = 300000)
+   public void testMasterAngleComputations()
+   {
+	   Random rand = new Random(1986L);
+      for (int i = 0; i < 10000; i++)
+      {
+         double e = 100 * (rand.nextDouble() + 0.001);
+         double k1 = rand.nextDouble();
+         double k2 = rand.nextDouble();
+         double d1 = e * abs(rand.nextGaussian());
+         double d2 = e * abs(rand.nextGaussian());
+         double DE = e * k1, DF = e * k2, BE = e * (1 - k1), BF = e * (1 - k2);
+         double AE = d1, CF = d2;
+         double AD = sqrt(DE * DE + AE * AE), DAE = atan2(DE, AE), ADE = atan2(AE, DE);
+         double AB = sqrt(AE * AE + BE * BE), BAE = atan2(BE, AE), ABE = atan2(AE, BE);
+         double CD = sqrt(CF * CF + DF * DF), CDF = atan2(CF, DF), DCF = atan2(DF, CF);
+         double BC = sqrt(BF * BF + CF * CF), CBF = atan2(CF, BF), BCF = atan2(BF, CF);
+         double BAD = DAE + BAE, ABC = ABE + CBF, BCD = BCF + DCF, ADC = ADE + CDF;
+         
+         FourBarCalculatorFromFastRunner calculator = new FourBarCalculatorFromFastRunner(AD, AB, BC, CD);
+         
+         calculator.computeMasterJointAngleGivenAngleABC(ABC);    
+         assertEquals(BAD, calculator.getAngleDAB(), eps); 
+         
+         calculator.computeMasterJointAngleGivenAngleBCD(BCD);
+         assertEquals(BAD, calculator.getAngleDAB(), eps); 
+         
+         calculator.computeMasterJointAngleGivenAngleCDA(ADC);
+         assertEquals(BAD, calculator.getAngleDAB(), eps); 
+      }
+      
+   }
 }
