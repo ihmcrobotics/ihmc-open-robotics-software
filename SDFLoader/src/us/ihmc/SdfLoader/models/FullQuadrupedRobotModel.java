3a4c3f75302e1cf0705721de2e73ac2369ab0e6a
brandon shrewsbury <bshrewsbury@ihmc.us>
2016-03-09 12:29:16 -0600

    SDFFullRobotModel switch from SDFHumanoidJointNameMap to SDFJointNameMap

--- SDFLoader/src/us/ihmc/SdfLoader/models/FullQuadrupedRobotModel.java
+++ SDFLoader/src/us/ihmc/SdfLoader/models/FullQuadrupedRobotModel.java
@@ -1,6 +1,5 @@
 package us.ihmc.SdfLoader.models;
 
-import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 import us.ihmc.robotics.screwTheory.RigidBody;
 
@@ -15,19 +14,4 @@ public interface FullQuadrupedRobotModel extends FullRobotModel
     */
    public abstract RigidBody getFoot(RobotQuadrant robotQuadrant);
 
-   /**
-    * This methods returns the frame located right after the parent joint of this end-effector.
-    */
-   public abstract ReferenceFrame getEndEffectorFrame(RobotQuadrant robotQuadrant);
-
-   /**
-    * Returns the sole reference frame.
-    * A sole frame is attached to a foot and is generally used to put the foot contact points.
-    * Its origin is right in the middle of the bottom of the foot.
-    * @param robotSide
-    * @return
-    */
-   public abstract ReferenceFrame getSoleFrame(RobotQuadrant robotQuadrant);
-   
-   
 }
