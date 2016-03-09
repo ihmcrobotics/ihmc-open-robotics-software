3e731e72964dc054ca2400bf3121e102596c3a89
brandon shrewsbury <bshrewsbury@ihmc.us>
2016-03-09 12:29:16 -0600

    SDFFullRobotModel switch from SDFHumanoidJointNameMap to SDFJointNameMap

--- SDFLoader/src/us/ihmc/SdfLoader/SDFFullQuadrupedRobotModel.java
+++ SDFLoader/src/us/ihmc/SdfLoader/SDFFullQuadrupedRobotModel.java
@@ -1,61 +1,44 @@
 package us.ihmc.SdfLoader;
 
-import org.apache.commons.lang3.tuple.ImmutablePair;
-
 import us.ihmc.SdfLoader.models.FullQuadrupedRobotModel;
-import us.ihmc.SdfLoader.partNames.ArmJointName;
-import us.ihmc.SdfLoader.partNames.JointRole;
-import us.ihmc.SdfLoader.partNames.LegJointName;
-import us.ihmc.SdfLoader.partNames.LimbName;
-import us.ihmc.SdfLoader.partNames.NeckJointName;
-import us.ihmc.SdfLoader.partNames.SpineJointName;
 import us.ihmc.robotics.robotSide.QuadrantDependentList;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
-import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.screwTheory.OneDoFJoint;
 import us.ihmc.robotics.screwTheory.RigidBody;
 
 public class SDFFullQuadrupedRobotModel extends SDFFullRobotModel implements FullQuadrupedRobotModel
 {
-   private final QuadrantDependentList<RigidBody> feet = new QuadrantDependentList<RigidBody>();
+   private QuadrantDependentList<RigidBody> feet;
+   private SDFQuadrupedJointNameMap quadrupedJointNameMap;
 
    public SDFFullQuadrupedRobotModel(SDFFullQuadrupedRobotModel modelToCopy)
    {
-      this( modelToCopy.rootLink, modelToCopy.sdfJointNameMap, modelToCopy.sensorLinksToTrack );
+      this(modelToCopy.rootLink, modelToCopy.quadrupedJointNameMap, modelToCopy.sensorLinksToTrack );
    }
    
-   public SDFFullQuadrupedRobotModel(SDFLinkHolder rootLink, SDFJointNameMap sdfJointNameMap, String[] sensorLinksToTrack)
+   public SDFFullQuadrupedRobotModel(SDFLinkHolder rootLink, SDFQuadrupedJointNameMap sdfJointNameMap, String[] sensorLinksToTrack)
    {
       super(rootLink, sdfJointNameMap, sensorLinksToTrack);
-      
-      for(RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-      }
    }
    
    @Override
    protected void mapRigidBody(SDFJointHolder joint, OneDoFJoint inverseDynamicsJoint, RigidBody rigidBody)
    {
+      if(feet == null)
+      {
+         feet = new QuadrantDependentList<RigidBody>();
+      }
+      
       super.mapRigidBody(joint, inverseDynamicsJoint, rigidBody);
       
-      SDFLinkHolder childLink = joint.getChildLinkHolder();
-      String linkName = childLink.getName();
-//      sdfJointNameMap.get
-      
-      
-      ImmutablePair<RobotSide, LimbName> limbSideAndName = sdfJointNameMap.getLimbName(linkName);
-      if(limbSideAndName != null)
+      SDFQuadrupedJointNameMap jointMap = (SDFQuadrupedJointNameMap) sdfJointNameMap;
+      for(RobotQuadrant robotQuadrant : RobotQuadrant.values)
       {
-         RobotSide limbSide = limbSideAndName.getLeft();
-         LimbName limbName = limbSideAndName.getRight();
-         switch (limbName)
+         String jointBeforeFootName = jointMap.getJointBeforeFootName(robotQuadrant);
+         
+         if(jointBeforeFootName.equals(joint.getName()))
          {
-         case ARM:
-            hands.put(limbSide, rigidBody);
-            break;
-         case LEG:
-            feet.put(limbSide, rigidBody);
-            break;
+            feet.set(robotQuadrant, rigidBody);
          }
       }
    }
