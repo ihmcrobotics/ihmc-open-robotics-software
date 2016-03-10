554df73093e461e27e7f69931619335868f961a5
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-10 10:18:34 -0600

    Creating the IK module in the controller core.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/inverseKinematics/InverseKinematicsQPSolver.java
@@ -0,0 +1,201 @@
+package us.ihmc.commonWalkingControlModules.inverseKinematics;
+
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
+
+import us.ihmc.commonWalkingControlModules.controlModules.nativeOptimization.OASESConstrainedQPSolver;
+import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.IntegerYoVariable;
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.BooleanYoVariable;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.functionApproximation.DampedLeastSquaresSolver;
+import us.ihmc.robotics.linearAlgebra.MatrixTools;
+import us.ihmc.tools.exceptions.NoConvergenceException;
+
+public class InverseKinematicsQPSolver
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+
+   private final BooleanYoVariable seedFromPreviousSolution = new BooleanYoVariable("seedFromPreviousSolution", registry);
+   private final OASESConstrainedQPSolver qpSolver = new OASESConstrainedQPSolver(registry);
+
+   private final DenseMatrix64F solverInput_H;
+   private final DenseMatrix64F solverInput_f;
+
+   private final DenseMatrix64F solverInput_Aeq;
+   private final DenseMatrix64F solverInput_beq;
+   private final DenseMatrix64F solverInput_Ain;
+   private final DenseMatrix64F solverInput_bin;
+
+   private final DenseMatrix64F solverInput_lb;
+   private final DenseMatrix64F solverInput_ub;
+
+   private final DenseMatrix64F desiredJointVelocities;
+
+   private final IntegerYoVariable numberOfIterations = new IntegerYoVariable("numberOfIterations", registry);
+   private final IntegerYoVariable numberOfConstraints = new IntegerYoVariable("numberOfConstraints", registry);
+   private final DoubleYoVariable jointVelocityRegularization = new DoubleYoVariable("jointVelocityRegularization", registry);
+
+   private final int numberOfDoFs;
+
+   public InverseKinematicsQPSolver(int numberOfDoFs, YoVariableRegistry parentRegistry)
+   {
+      this.numberOfDoFs = numberOfDoFs;
+
+      solverInput_H = new DenseMatrix64F(numberOfDoFs, numberOfDoFs);
+      solverInput_f = new DenseMatrix64F(numberOfDoFs, 1);
+      solverInput_lb = new DenseMatrix64F(numberOfDoFs, 1);
+      solverInput_ub = new DenseMatrix64F(numberOfDoFs, 1);
+
+      solverInput_Aeq = new DenseMatrix64F(0, numberOfDoFs);
+      solverInput_beq = new DenseMatrix64F(0, 0);
+      solverInput_Ain = new DenseMatrix64F(0, numberOfDoFs);
+      solverInput_bin = new DenseMatrix64F(0, 0);
+
+      CommonOps.fill(solverInput_lb, Double.NEGATIVE_INFINITY);
+      CommonOps.fill(solverInput_ub, Double.POSITIVE_INFINITY);
+
+      jointVelocityRegularization.set(0.1);
+
+      desiredJointVelocities = new DenseMatrix64F(numberOfDoFs, 1);
+
+      pseudoInverseSolver = new DampedLeastSquaresSolver(numberOfDoFs, jointVelocityRegularization.getDoubleValue());
+
+      parentRegistry.addChild(registry);
+   }
+
+   public void reset()
+   {
+      solverInput_H.zero();
+      for (int i = 0; i < numberOfDoFs; i++)
+         solverInput_H.set(i, i, jointVelocityRegularization.getDoubleValue());
+      solverInput_f.reshape(numberOfDoFs, 1);
+      solverInput_f.zero();
+
+      solverInput_Aeq.reshape(0, numberOfDoFs);
+      solverInput_beq.reshape(0, 0);
+
+      jAugmented.reshape(0, numberOfDoFs);
+   }
+
+   private final DenseMatrix64F tempJtW = new DenseMatrix64F(1, 1);
+   private final DenseMatrix64F tempTask_H = new DenseMatrix64F(1, 1);
+   private final DenseMatrix64F tempTask_f = new DenseMatrix64F(1, 1);
+
+   public void addMotionInput(InverseKinematicsMotionQPInput input)
+   {
+      if (input.isMotionConstraint())
+         addMotionConstraint(input.taskJacobian, input.taskObjective);
+      else if (input.useWeightScalar())
+         addMotionTask(input.taskJacobian, input.taskObjective, input.getWeightScalar());
+      else
+         addMotionTask(input.taskJacobian, input.taskObjective, input.taskWeightMatrix);
+   }
+
+   public void addMotionTask(DenseMatrix64F taskJ, DenseMatrix64F taskObjective, double taskWeight)
+   {
+      int taskSize = taskJ.getNumRows();
+
+      // J^T W
+      tempJtW.reshape(numberOfDoFs, taskSize);
+      MatrixTools.scaleTranspose(taskWeight, taskJ, tempJtW);
+      
+      addMotionTaskInternal(tempJtW, taskJ, taskObjective);
+   }
+
+   public void addMotionTask(DenseMatrix64F taskJ, DenseMatrix64F taskXDot, DenseMatrix64F taskWeight)
+   {
+      int taskSize = taskJ.getNumRows();
+      jAugmented.reshape(jAugmented.getNumRows() + taskSize, numberOfDoFs);
+      CommonOps.insert(taskJ, jAugmented, jAugmented.getNumRows() - taskSize, 0);
+
+      // J^T W
+      tempJtW.reshape(numberOfDoFs, taskSize);
+      CommonOps.multTransA(taskJ, taskWeight, tempJtW);
+      
+      addMotionTaskInternal(tempJtW, taskJ, taskXDot);
+   }
+
+   private void addMotionTaskInternal(DenseMatrix64F taskJtW, DenseMatrix64F taskJacobian, DenseMatrix64F taskObjective)
+   {
+      // Compute: H += J^T W J
+      tempTask_H.reshape(numberOfDoFs, numberOfDoFs);
+      CommonOps.mult(taskJtW, taskJacobian, tempTask_H);
+      CommonOps.addEquals(solverInput_H, tempTask_H);
+
+      // Compute: f += - J^T W xDot
+      tempTask_f.reshape(numberOfDoFs, 1);
+      CommonOps.mult(taskJtW, taskObjective, tempTask_f);
+      CommonOps.subtractEquals(solverInput_f, tempTask_f);
+   }
+
+   public void addMotionConstraint(DenseMatrix64F taskJacobian, DenseMatrix64F taskObjective)
+   {
+      int taskSize = taskJacobian.getNumRows();
+      int previousSize = solverInput_beq.getNumRows();
+
+      // Careful on that one, it works as long as matrices are row major and that the number of columns is not changed.
+      solverInput_Aeq.reshape(previousSize + taskSize, numberOfDoFs, true);
+      solverInput_beq.reshape(previousSize + taskSize, 1, true);
+
+      CommonOps.insert(taskJacobian, solverInput_Aeq, previousSize, 0);
+      CommonOps.insert(taskObjective, solverInput_beq, previousSize, 0);
+   }
+
+   private final DenseMatrix64F jAugmented = new DenseMatrix64F(1, 1);
+   private final DenseMatrix64F jInverseAugmented = new DenseMatrix64F(1, 1);
+   private final DenseMatrix64F nullspaceProjector = new DenseMatrix64F(1, 1);
+   
+   private final DenseMatrix64F jacobianForPrivilegedJointVelocities = new DenseMatrix64F(1, 1);
+   
+   private final DampedLeastSquaresSolver pseudoInverseSolver;
+
+   public void finalizeWithPrivilegedJointVelocities(DenseMatrix64F selectionMatrix, DenseMatrix64F privilegedJointVelocities, DenseMatrix64F weight)
+   {
+      jInverseAugmented.reshape(numberOfDoFs, jAugmented.getNumRows());
+      pseudoInverseSolver.setA(jAugmented);
+      pseudoInverseSolver.invert(jInverseAugmented);
+
+      nullspaceProjector.reshape(numberOfDoFs, numberOfDoFs);
+      // I - J^* J
+      CommonOps.mult(-1.0, jInverseAugmented, jAugmented, nullspaceProjector);
+      for (int i = 0; i < numberOfDoFs; i++)
+         nullspaceProjector.add(i, i, 1.0);
+
+      jacobianForPrivilegedJointVelocities.reshape(privilegedJointVelocities.getNumRows(), numberOfDoFs);
+      CommonOps.mult(selectionMatrix, nullspaceProjector, jacobianForPrivilegedJointVelocities);
+
+      addMotionTask(jacobianForPrivilegedJointVelocities, privilegedJointVelocities, weight);
+   }
+
+   public void solve() throws NoConvergenceException
+   {
+      boolean firstCall = !seedFromPreviousSolution.getBooleanValue();
+
+      numberOfConstraints.set(Aeq.getNumRows() + Ain.getNumRows());
+
+      numberOfIterations.set(qpSolver.solve(solverInput_H, solverInput_f, solverInput_Aeq, solverInput_beq, solverInput_Ain, solverInput_bin, solverInput_lb, solverInput_ub, desiredJointVelocities, firstCall));
+      
+      seedFromPreviousSolution.set(true);
+   }
+
+   public void getJointVelocities(DenseMatrix64F jointVelocities)
+   {
+      jointVelocities.set(desiredJointVelocities);
+   }
+
+   public void setRegularizationWeight(double weight)
+   {
+      jointVelocityRegularization.set(weight);
+   }
+
+   public void setMinJointVelocities(DenseMatrix64F qDotMin)
+   {
+      solverInput_lb.set(qDotMin);
+   }
+
+   public void setMaxJointVelocities(DenseMatrix64F qDotMax)
+   {
+      solverInput_ub.set(qDotMax);
+   }
+}
