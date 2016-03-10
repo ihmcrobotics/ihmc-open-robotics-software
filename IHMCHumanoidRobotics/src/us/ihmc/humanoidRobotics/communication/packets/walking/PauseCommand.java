4568aa07b0b96f6e0862be3f5fbcf785c7424887
JerryPratt <jpratt@ihmc.us>
2016-03-09 22:08:39 -0600

    ROSI-386: Cleaning up scripts for tests.

---
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/walking/PauseCommand.java
@@ -0,0 +1,8 @@
+package us.ihmc.humanoidRobotics.communication.packets.walking;
+
+// To be deleted after the controller rewrite is done. 
+// Keeping for right now in order to transform old script files.
+public class PauseCommand
+{
+
+}
