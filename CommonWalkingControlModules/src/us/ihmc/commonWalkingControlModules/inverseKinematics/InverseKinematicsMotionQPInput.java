f631641fe9269022449e5532b0fb56775bdb7895
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-10 10:18:34 -0600

    Creating the IK module in the controller core.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/inverseKinematics/InverseKinematicsMotionQPInput.java
@@ -0,0 +1,59 @@
+package us.ihmc.commonWalkingControlModules.inverseKinematics;
+
+import org.ejml.data.DenseMatrix64F;
+
+public class InverseKinematicsMotionQPInput
+{
+   final DenseMatrix64F taskJacobian;
+   final DenseMatrix64F taskObjective;
+   final DenseMatrix64F taskWeightMatrix;
+   private double taskWeightScalar;
+   private boolean useWeightScalar = false;
+   private boolean isMotionConstraint = false;
+   private final int numberOfDoFs;
+
+   public InverseKinematicsMotionQPInput(int numberOfDoFs)
+      {
+         this.numberOfDoFs = numberOfDoFs;
+         taskJacobian = new DenseMatrix64F(numberOfDoFs, numberOfDoFs);
+         taskObjective = new DenseMatrix64F(numberOfDoFs, 1);
+         taskWeightMatrix = new DenseMatrix64F(numberOfDoFs, numberOfDoFs);
+      }
+
+   public void reshape(int taskSize)
+   {
+      taskJacobian.reshape(taskSize, numberOfDoFs);
+      taskObjective.reshape(taskSize, 1);
+      taskWeightMatrix.reshape(taskSize, taskSize);
+   }
+
+   public void setIsMotionConstraint(boolean isMotionConstraint)
+   {
+      this.isMotionConstraint = isMotionConstraint;
+   }
+
+   public void setUseWeightScalar(boolean useWeightScalar)
+   {
+      this.useWeightScalar = useWeightScalar;
+   }
+
+   public void setWeight(double weight)
+   {
+      this.taskWeightScalar = weight;
+   }
+
+   public double getWeightScalar()
+   {
+      return taskWeightScalar;
+   }
+
+   public boolean useWeightScalar()
+   {
+      return useWeightScalar;
+   }
+
+   public boolean isMotionConstraint()
+   {
+      return isMotionConstraint;
+   }
+}
---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/inverseKinematics/InverseKinematicsOptimizationControlModule.java
@@ -0,0 +1,12 @@
+package us.ihmc.commonWalkingControlModules.inverseKinematics;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+
+public class InverseKinematicsOptimizationControlModule
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+
+   public InverseKinematicsOptimizationControlModule()
+   {
+   }
+}
