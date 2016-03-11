0270992025d1d1b888da3de88155f501fa1f95cf
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-11 00:19:46 -0600

    Added weight level enum.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/inverseKinematics/dataObjects/InverseKinematicsCommand.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/inverseKinematics/dataObjects/InverseKinematicsCommand.java
@@ -2,6 +2,27 @@ package us.ihmc.commonWalkingControlModules.inverseKinematics.dataObjects;
 
 public abstract class InverseKinematicsCommand<T extends InverseKinematicsCommand<T>>
 {
+   public enum InverseKinematicsCommandWeightLevels
+   {
+      HIGH, MEDIUM, LOW, HARD_CONSTRAINT;
+
+      public double getWeightValue()
+      {
+         switch (this)
+         {
+         case HARD_CONSTRAINT:
+            return Double.POSITIVE_INFINITY;
+         case HIGH:
+            return 10.0;
+         case LOW:
+            return 0.10;
+         case MEDIUM:
+         default:
+            return 1.00;
+         }
+      }
+   }
+
    public enum InverseKinematicsCommandType
    {
       JOINTSPACE, TASKSPACE, MOMENTUM,
