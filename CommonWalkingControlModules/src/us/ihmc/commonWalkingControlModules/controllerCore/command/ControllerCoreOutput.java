6799d7d70d65e856b9a77b1b35bc9a266a593270
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-18 14:06:53 -0500

    Added the linear momentum rate in the ControllerCoreOutput.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/command/ControllerCoreOutput.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/command/ControllerCoreOutput.java
@@ -2,15 +2,19 @@ package us.ihmc.commonWalkingControlModules.controllerCore.command;
 
 import us.ihmc.humanoidRobotics.model.CenterOfPressureDataHolder;
 import us.ihmc.robotics.geometry.FramePoint2d;
+import us.ihmc.robotics.geometry.FrameVector;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.screwTheory.RigidBody;
 
 public class ControllerCoreOutput
 {
    private final CenterOfPressureDataHolder centerOfPressureDataHolder;
+   private final FrameVector linearMomentumRate = new FrameVector(ReferenceFrame.getWorldFrame());
 
    public ControllerCoreOutput(CenterOfPressureDataHolder centerOfPressureDataHolder)
    {
       this.centerOfPressureDataHolder = centerOfPressureDataHolder;
+      linearMomentumRate.setToNaN(ReferenceFrame.getWorldFrame());
    }
 
    public void setDesiredCenterOfPressure(FramePoint2d cop, RigidBody rigidBody)
@@ -21,5 +25,21 @@ public class ControllerCoreOutput
    public void getDesiredCenterOfPressure(FramePoint2d copToPack, RigidBody rigidBody)
    {
       centerOfPressureDataHolder.getCenterOfPressure(copToPack, rigidBody);
+   }
+
+   public void setLinearMomentumRate(FrameVector linearMomentumRate)
+   {
+      this.linearMomentumRate.set(linearMomentumRate);
+   }
+
+   public void setAndMatchFrameLinearMomentumRate(FrameVector linearMomentumRate)
+   {
+      this.linearMomentumRate.setIncludingFrame(linearMomentumRate);
+      this.linearMomentumRate.changeFrame(ReferenceFrame.getWorldFrame());
+   }
+
+   public void getLinearMomentumRate(FrameVector linearMomentumRateToPack)
+   {
+      linearMomentumRateToPack.setIncludingFrame(linearMomentumRate);
    }
 }
