b7d57d9a5932379d0697f25d4bb6c928d1295236
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-18 22:27:37 -0500

    Added the low-level desired joint data to the ControllerCoreOutput.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/command/ControllerCoreOutputReadOnly.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/command/ControllerCoreOutputReadOnly.java
@@ -1,14 +1,15 @@
 package us.ihmc.commonWalkingControlModules.controllerCore.command;
 
+import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.LowLevelOneDoFJointDesiredDataHolderReadOnly;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.RootJointDesiredConfigurationDataReadOnly;
 import us.ihmc.robotics.geometry.FramePoint2d;
 import us.ihmc.robotics.geometry.FrameVector;
 import us.ihmc.robotics.screwTheory.RigidBody;
 
 public interface ControllerCoreOutputReadOnly
 {
-
-   void getDesiredCenterOfPressure(FramePoint2d copToPack, RigidBody rigidBody);
-
-   void getLinearMomentumRate(FrameVector linearMomentumRateToPack);
-
+   public abstract void getDesiredCenterOfPressure(FramePoint2d copToPack, RigidBody rigidBody);
+   public abstract void getLinearMomentumRate(FrameVector linearMomentumRateToPack);
+   public abstract RootJointDesiredConfigurationDataReadOnly getRootJointDesiredConfigurationData();
+   public abstract LowLevelOneDoFJointDesiredDataHolderReadOnly getLowLevelOneDoFJointDesiredDataHolder();
 }
