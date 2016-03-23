1ca1dfa6f72dbfc90566888606eb9ad1f764ce29
Sylvain Bertrand <sbertrand@ihmc.us>
2016-03-23 14:16:56 -0500

    Documenting.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerAPI/input/command/MultipleCommandHolder.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerAPI/input/command/MultipleCommandHolder.java
@@ -2,7 +2,19 @@ package us.ihmc.commonWalkingControlModules.controllerAPI.input.command;
 
 import java.util.List;
 
-public interface MultipleCommandHolder
+import us.ihmc.communication.packets.Packet;
+
+/**
+ * Extension of {@link Command} with the possibility of wrapping a set of {@link Command}.
+ * @author Sylvain
+ *
+ * @param <C> Type of the final implementation of this MultipleCommandHolder. It is used for the copy method {@link #set(MultipleCommandHolder)}.
+ * @param <M> Type of the corresponding {@link Packet} that this MultipleCommandHolder can copy.
+ */
+public interface MultipleCommandHolder<C extends MultipleCommandHolder<C, M>, M extends Packet<M>> extends Command<C, M>
 {
+   /**
+    * @return the list of commands held by this MultipleCommandHolder.
+    */
    public abstract List<Command<?, ?>> getControllerCommands();
 }
