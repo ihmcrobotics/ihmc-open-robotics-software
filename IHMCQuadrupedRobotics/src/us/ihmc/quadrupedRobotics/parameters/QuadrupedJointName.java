18eb55f08df2f2aa9609ef494c6a1c0831834891
brandon shrewsbury <bshrewsbury@ihmc.us>
2016-03-29 16:48:47 -0500

    Changing quadrupedJointNameEnums, breaks stuff. Fixing soon

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/parameters/QuadrupedJointName.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/parameters/QuadrupedJointName.java
@@ -2,26 +2,14 @@ package us.ihmc.quadrupedRobotics.parameters;
 
 public enum QuadrupedJointName
 {
-   // TODO: Use these new neck joint names:
-//   /* Neck joints */
-//   NEAR_NECK_YAW,
-//   NEAR_NECK_PITCH,
-//   NEAR_NECK_ROLL,
-//
-//   /* Head joints */
-//   FAR_NECK_YAW,
-//   FAR_NECK_PITCH,
-//   FAR_NECK_ROLL,
-
    /* Neck joints */
-   HEAD_YAW,
-   HEAD_PITCH,
-   HEAD_ROLL,
+   PROXIMAL_NECK_YAW,
+   PROXIMAL_NECK_PITCH,
+   PROXIMAL_NECK_ROLL,
+   DISTAL_NECK_YAW,
+   DISTAL_NECK_PITCH,
+   DISTAL_NECK_ROLL,
 
-   /* Head joints */
-   NECK_ROOT_YAW,
-   NECK_ROOT_PITCH,
-   NECK_ROOT_ROLL,
 
    /* Leg joints */
    FRONT_LEFT_HIP_ROLL,
