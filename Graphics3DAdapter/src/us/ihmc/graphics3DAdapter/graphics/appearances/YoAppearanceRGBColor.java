b3b2ee5472dfdf3f4c208eb79b3352097e3a99fb
Sylvain Bertrand <sbertrand@ihmc.us>
2016-04-01 17:25:16 -0500

    Fixed YoAppearanceRGBColor

--- Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/appearances/YoAppearanceRGBColor.java
+++ Graphics3DAdapter/src/us/ihmc/graphics3DAdapter/graphics/appearances/YoAppearanceRGBColor.java
@@ -12,28 +12,28 @@ public class YoAppearanceRGBColor extends YoAppearanceTransparency
    public YoAppearanceRGBColor(Color3f color, double transparency)
    {
       this.color = new Color3f(color);
-      this.awtColor = new Color(this.color.x, this.color.y, this.color.z, (float) transparency);
+      awtColor = new Color(this.color.x, this.color.y, this.color.z, (float) transparency);
       setTransparency(transparency);
    }
    
    public YoAppearanceRGBColor(Color color, double transparency)
    {
       this.color = new Color3f(color);
-      this.awtColor = new Color(color.getRed(), color.getGreen(), color.getBlue(), (float) transparency);
+      awtColor = new Color(color.getRed() / 255.0f, color.getGreen() / 255.0f, color.getBlue() / 255.0f, (float) transparency);
       setTransparency(transparency);
    }
    
    public YoAppearanceRGBColor(float red, float green, float blue, double transparency)
    {
-      this.color = new Color3f(red, green, blue);
-      this.awtColor = new Color(red / 255.0f, green / 255.0f, blue / 255.0f, (float) transparency);
+      color = new Color3f(red, green, blue);
+      awtColor = new Color(red, green, blue, (float) transparency);
       setTransparency(transparency);
    }
    
    public YoAppearanceRGBColor(double red, double green, double blue, double transparency)
    {
-      this.color = new Color3f((float) red, (float) green, (float) blue);
-      this.awtColor = new Color((float) (red / 255.0), (int) (green / 255.0), (float) (blue / 255.0), (float) transparency);
+      color = new Color3f((float) red, (float) green, (float) blue);
+      awtColor = new Color((float) red, (float) green, (float) blue, (float) transparency);
       setTransparency(transparency);
    }
 
