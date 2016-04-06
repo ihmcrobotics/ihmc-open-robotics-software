0a22a35f6330dcca80dda228e6a32787473d6519
JerryPratt <jpratt@ihmc.us>
2016-04-06 14:43:15 -0500

    fixBamboo. Made field public.
    Also added @FieldDocumentation.

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/walking/WalkingStatusMessage.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/walking/WalkingStatusMessage.java
@@ -1,12 +1,16 @@
 package us.ihmc.humanoidRobotics.communication.packets.walking;
 
+import us.ihmc.communication.packetAnnotations.ClassDocumentation;
+import us.ihmc.communication.packetAnnotations.FieldDocumentation;
 import us.ihmc.communication.packets.StatusPacket;
 
+@ClassDocumentation("This class is used to report the status of walking.")
 public class WalkingStatusMessage extends StatusPacket<WalkingStatusMessage>
 {
    public enum Status {STARTED, COMPLETED, ABORT_REQUESTED}
 
-   private Status status;
+   @FieldDocumentation("Status of walking. Either STARTED, COMPLETED, or ABORT_REQUESTED.")
+   public Status status;
 
    public WalkingStatusMessage()
    {
