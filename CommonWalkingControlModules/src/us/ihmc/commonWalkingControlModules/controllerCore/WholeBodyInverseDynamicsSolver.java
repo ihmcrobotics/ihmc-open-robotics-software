dd77330d851e15c830797bb45b444506fcd6fea5
Sylvain Bertrand <sbertrand@ihmc.us>
2016-04-11 16:13:18 -0500

    Created JointAccelerationIncegrationCalculator.
    Extracted all the integration stuff out of
    WholeBodyInverseDynamicsSolver and made it possible to have gains on a
    per joint basis.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/WholeBodyInverseDynamicsSolver.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/WholeBodyInverseDynamicsSolver.java
@@ -1,6 +1,5 @@
 package us.ihmc.commonWalkingControlModules.controllerCore;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,18 +18,17 @@ import us.ihmc.commonWalkingControlModules.controllerCore.command.inverseDynamic
 import us.ihmc.commonWalkingControlModules.controllerCore.command.inverseDynamics.SpatialAccelerationCommand;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.inverseKinematics.PrivilegedConfigurationCommand;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.LowLevelJointControlMode;
-import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.LowLevelJointData;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.LowLevelOneDoFJointDesiredDataHolder;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.RootJointDesiredConfigurationData;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.RootJointDesiredConfigurationDataReadOnly;
 import us.ihmc.commonWalkingControlModules.momentumBasedController.PlaneContactWrenchProcessor;
 import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.InverseDynamicsOptimizationControlModule;
+import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.JointAccelerationIntegrationCalculator;
 import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.JointIndexHandler;
 import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.MomentumControlModuleException;
 import us.ihmc.commonWalkingControlModules.visualizer.WrenchVisualizer;
 import us.ihmc.humanoidRobotics.bipedSupportPolygons.ContactablePlaneBody;
 import us.ihmc.humanoidRobotics.model.CenterOfPressureDataHolder;
-import us.ihmc.robotics.MathTools;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
 import us.ihmc.robotics.geometry.FrameVector;
@@ -63,13 +61,10 @@ public class WholeBodyInverseDynamicsSolver
 
    private final PlaneContactWrenchProcessor planeContactWrenchProcessor;
    private final WrenchVisualizer wrenchVisualizer;
+   private final JointAccelerationIntegrationCalculator jointAccelerationIntegrationCalculator;
 
    private final OneDoFJoint[] controlledOneDoFJoints;
    private final InverseDynamicsJoint[] jointsToOptimizeFor;
-   private final List<OneDoFJoint> jointsToComputeDesiredPositionFor = new ArrayList<>();
-   private final DoubleYoVariable alphaPositionIntegration = new DoubleYoVariable("alphaPositionIntegration", registry);
-   private final DoubleYoVariable alphaVelocityIntegration = new DoubleYoVariable("alphaVelocityIntegration", registry);
-   private final DoubleYoVariable integrationMaxVelocity = new DoubleYoVariable("integrationMaxVelocity", registry);
 
    private final YoFrameVector yoDesiredMomentumRateLinear;
    private final YoFrameVector yoAchievedMomentumRateLinear;
@@ -111,13 +106,11 @@ public class WholeBodyInverseDynamicsSolver
       wrenchVisualizer = WrenchVisualizer.createWrenchVisualizerWithContactableBodies("DesiredExternalWrench", contactablePlaneBodies, 1.0,
             yoGraphicsListRegistry, registry);
 
+      jointAccelerationIntegrationCalculator = new JointAccelerationIntegrationCalculator(controlDT, registry);
+
       ReferenceFrame centerOfMassFrame = toolbox.getCenterOfMassFrame();
       yoDesiredMomentumRateLinear = new YoFrameVector("desiredMomentumRateLinear", centerOfMassFrame, registry);
       yoAchievedMomentumRateLinear = new YoFrameVector("achievedMomentumRateLinear", centerOfMassFrame, registry);
-
-      alphaPositionIntegration.set(0.9996);
-      alphaVelocityIntegration.set(0.95);
-      integrationMaxVelocity.set(2.0);
 
       parentRegistry.addChild(registry);
    }
@@ -193,33 +186,7 @@ public class WholeBodyInverseDynamicsSolver
       lowLevelOneDoFJointDesiredDataHolder.setDesiredTorqueFromJoints(controlledOneDoFJoints);
       lowLevelOneDoFJointDesiredDataHolder.setDesiredAccelerationFromJoints(controlledOneDoFJoints);
 
-      for (int i = 0; i < jointsToComputeDesiredPositionFor.size(); i++)
-      {
-         OneDoFJoint joint = jointsToComputeDesiredPositionFor.get(i);
-         LowLevelJointData lowLevelJointData = lowLevelOneDoFJointDesiredDataHolder.getLowLevelJointData(joint);
-         if (!lowLevelJointData.hasDesiredVelocity())
-            lowLevelJointData.setDesiredVelocity(joint.getQd());
-         if (!lowLevelJointData.hasDesiredPosition())
-            lowLevelJointData.setDesiredPosition(joint.getQ());
-
-         double desiredAcceleration = lowLevelJointData.getDesiredAcceleration();
-         double desiredVelocity = lowLevelJointData.getDesiredVelocity();
-         double desiredPosition = lowLevelJointData.getDesiredPosition();
-
-         desiredVelocity *= alphaVelocityIntegration.getDoubleValue();
-         desiredVelocity += desiredAcceleration * controlDT;
-         desiredVelocity = MathTools.clipToMinMax(desiredVelocity, integrationMaxVelocity.getDoubleValue());
-         desiredPosition += desiredVelocity * controlDT;
-
-         double errorPosition = MathTools.clipToMinMax(desiredPosition - joint.getQ(), 0.2);
-         desiredPosition = joint.getQ() + errorPosition;
-         desiredPosition = MathTools.clipToMinMax(desiredPosition, joint.getJointLimitLower(), joint.getJointLimitUpper());
-         desiredPosition = alphaPositionIntegration.getDoubleValue() * desiredPosition + (1.0 - alphaPositionIntegration.getDoubleValue()) * joint.getQ();
-         desiredVelocity = (desiredPosition - lowLevelJointData.getDesiredPosition()) / controlDT;
-
-         lowLevelJointData.setDesiredVelocity(desiredVelocity);
-         lowLevelJointData.setDesiredPosition(desiredPosition);
-      }
+      jointAccelerationIntegrationCalculator.computeAndUpdateDataHolder(lowLevelOneDoFJointDesiredDataHolder);
    }
 
    public void submitInverseDynamicsCommandList(InverseDynamicsCommandList inverseDynamicsCommandList)
@@ -252,7 +219,7 @@ public class WholeBodyInverseDynamicsSolver
             optimizationControlModule.submitPlaneContactStateCommand((PlaneContactStateCommand) command);
             break;
          case JOINT_ACCELERATION_INTEGRATION:
-            submitJointAccelerationIntegrationCommand((JointAccelerationIntegrationCommand) command);
+            jointAccelerationIntegrationCalculator.submitJointAccelerationIntegrationCommand((JointAccelerationIntegrationCommand) command);
             break;
          case COMMAND_LIST:
             submitInverseDynamicsCommandList((InverseDynamicsCommandList) command);
@@ -260,16 +227,6 @@ public class WholeBodyInverseDynamicsSolver
          default:
             throw new RuntimeException("The command type: " + command.getCommandType() + " is not handled.");
          }
-      }
-   }
-
-   private void submitJointAccelerationIntegrationCommand(JointAccelerationIntegrationCommand command)
-   {
-      for (int i = 0; i < command.getNumberOfJointsToComputeDesiedPositionFor(); i++)
-      {
-         OneDoFJoint jointToComputeDesiedPositionFor = command.getJointToComputeDesiedPositionFor(i);
-         if (!jointsToComputeDesiredPositionFor.contains(jointToComputeDesiedPositionFor))
-            jointsToComputeDesiredPositionFor.add(jointToComputeDesiedPositionFor);
       }
    }
 
---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/optimization/JointAccelerationIntegrationCalculator.java
@@ -0,0 +1,121 @@
+package us.ihmc.commonWalkingControlModules.momentumBasedController.optimization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gnu.trove.list.array.TDoubleArrayList;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.inverseDynamics.JointAccelerationIntegrationCommand;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.LowLevelJointData;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.LowLevelOneDoFJointDesiredDataHolder;
+import us.ihmc.robotics.MathTools;
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.screwTheory.OneDoFJoint;
+
+public class JointAccelerationIntegrationCalculator
+{
+   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
+
+   private final List<OneDoFJoint> jointsToComputeDesiredPositionFor = new ArrayList<>();
+   private final TDoubleArrayList jointSpecificAlphaPosition = new TDoubleArrayList();
+   private final TDoubleArrayList jointSpecificAlphaVelocity = new TDoubleArrayList();
+   private final TDoubleArrayList jointSpecificMaxPositionError = new TDoubleArrayList();
+   private final TDoubleArrayList jointSpecificMaxVelocity = new TDoubleArrayList();
+
+   private final DoubleYoVariable defaultAlphaPositionIntegration = new DoubleYoVariable("defaultAlphaPositionIntegration", registry);
+   private final DoubleYoVariable defaultAlphaVelocityIntegration = new DoubleYoVariable("defaultAlphaVelocityIntegration", registry);
+   private final DoubleYoVariable defaultIntegrationMaxVelocity = new DoubleYoVariable("defaultIntegrationMaxVelocity", registry);
+   private final DoubleYoVariable defaultIntegrationMaxPositionError = new DoubleYoVariable("defaultIntegrationMaxPositionError", registry);
+
+   private final double controlDT;
+
+   public JointAccelerationIntegrationCalculator(double controlDT, YoVariableRegistry parentRegistry)
+   {
+      this.controlDT = controlDT;
+      defaultAlphaPositionIntegration.set(0.9996);
+      defaultAlphaVelocityIntegration.set(0.95);
+      defaultIntegrationMaxPositionError.set(0.2);
+      defaultIntegrationMaxVelocity.set(2.0);
+
+      parentRegistry.addChild(registry);
+   }
+
+   public void submitJointAccelerationIntegrationCommand(JointAccelerationIntegrationCommand command)
+   {
+      for (int commandJointIndex = 0; commandJointIndex < command.getNumberOfJointsToComputeDesiredPositionFor(); commandJointIndex++)
+      {
+         OneDoFJoint jointToComputeDesierdPositionFor = command.getJointToComputeDesiredPositionFor(commandJointIndex);
+         int localJointIndex = jointsToComputeDesiredPositionFor.indexOf(jointToComputeDesierdPositionFor);
+
+         double newAlphaPosition = command.getJointAlphaPosition(commandJointIndex);
+         if (Double.isNaN(newAlphaPosition) || !MathTools.isInsideBoundsInclusive(newAlphaPosition, 0.0, 1.0))
+            newAlphaPosition = defaultAlphaPositionIntegration.getDoubleValue();
+
+         double newAlphaVelocity = command.getJointAlphaVelocity(commandJointIndex);
+         if (Double.isNaN(newAlphaVelocity) || !MathTools.isInsideBoundsInclusive(newAlphaVelocity, 0.0, 1.0))
+            newAlphaVelocity = defaultAlphaVelocityIntegration.getDoubleValue();
+
+         double newMaxPositionError = command.getJointMaxPositionError(commandJointIndex);
+         if (Double.isNaN(newMaxPositionError) || newMaxPositionError < 0.0)
+            newMaxPositionError = defaultIntegrationMaxPositionError.getDoubleValue();
+
+         double newMaxVelocity = command.getJointMaxVelocity(commandJointIndex);
+         if (Double.isNaN(newMaxVelocity) || newMaxVelocity < 0.0)
+            newMaxVelocity = defaultIntegrationMaxVelocity.getDoubleValue();
+
+         if (localJointIndex < 0)
+         {
+            jointsToComputeDesiredPositionFor.add(jointToComputeDesierdPositionFor);
+            jointSpecificAlphaPosition.add(newAlphaPosition);
+            jointSpecificAlphaVelocity.add(newAlphaVelocity);
+            jointSpecificMaxPositionError.add(newMaxVelocity);
+            jointSpecificMaxVelocity.add(newMaxVelocity);
+         }
+         else
+         {
+            jointSpecificAlphaPosition.set(localJointIndex, newAlphaPosition);
+            jointSpecificAlphaVelocity.set(localJointIndex, newAlphaVelocity);
+            jointSpecificMaxPositionError.set(localJointIndex, newMaxVelocity);
+            jointSpecificMaxVelocity.set(localJointIndex, newMaxVelocity);
+         }
+      }
+   }
+
+   public void computeAndUpdateDataHolder(LowLevelOneDoFJointDesiredDataHolder lowLevelJointDataHolderToUpdate)
+   {
+      for (int jointIndex = 0; jointIndex < jointsToComputeDesiredPositionFor.size(); jointIndex++)
+      {
+         OneDoFJoint joint = jointsToComputeDesiredPositionFor.get(jointIndex);
+
+         LowLevelJointData lowLevelJointData = lowLevelJointDataHolderToUpdate.getLowLevelJointData(joint);
+         if (!lowLevelJointData.hasDesiredVelocity())
+            lowLevelJointData.setDesiredVelocity(joint.getQd());
+         if (!lowLevelJointData.hasDesiredPosition())
+            lowLevelJointData.setDesiredPosition(joint.getQ());
+
+         double desiredAcceleration = lowLevelJointData.getDesiredAcceleration();
+         double desiredVelocity = lowLevelJointData.getDesiredVelocity();
+         double desiredPosition = lowLevelJointData.getDesiredPosition();
+
+         double alphaPosition = jointSpecificAlphaPosition.get(jointIndex);
+         double alphaVelocity = jointSpecificAlphaVelocity.get(jointIndex);
+         double maxPositionError = jointSpecificMaxPositionError.get(jointIndex);
+         double maxVelocity = jointSpecificMaxVelocity.get(jointIndex);
+
+         desiredVelocity *= alphaVelocity;
+         desiredVelocity += desiredAcceleration * controlDT;
+         desiredVelocity = MathTools.clipToMinMax(desiredVelocity, maxVelocity);
+         desiredPosition += desiredVelocity * controlDT;
+
+         double errorPosition = MathTools.clipToMinMax(desiredPosition - joint.getQ(), maxPositionError);
+         desiredPosition = joint.getQ() + errorPosition;
+         desiredPosition = MathTools.clipToMinMax(desiredPosition, joint.getJointLimitLower(), joint.getJointLimitUpper());
+         desiredPosition = alphaPosition * desiredPosition
+               + (1.0 - alphaPosition) * joint.getQ();
+         desiredVelocity = (desiredPosition - lowLevelJointData.getDesiredPosition()) / controlDT;
+
+         lowLevelJointData.setDesiredVelocity(desiredVelocity);
+         lowLevelJointData.setDesiredPosition(desiredPosition);
+      }
+   }
+}
