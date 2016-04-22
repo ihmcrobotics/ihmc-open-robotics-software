9a1872064cc4c20a853bdd5fa693f7ec1fcc1502
Kyle Cesare <kyle.cesare@disney.com>
2016-04-22 11:19:31 -0700

    Add rest of "set parameter" packets and handlers.

--- IHMCQuadrupedRobotics/src/us/ihmc/aware/params/ParameterPacketListener.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/aware/params/ParameterPacketListener.java
@@ -8,29 +8,80 @@ public class ParameterPacketListener
    // TODO: Make this a packet communicator, not a global data producer?
    public ParameterPacketListener(GlobalDataProducer communicator)
    {
+      communicator.attachListener(SetBooleanParameterPacket.class, new PacketConsumer<SetBooleanParameterPacket>()
+      {
+         @Override
+         public void receivedPacket(SetBooleanParameterPacket packet)
+         {
+            onSetBooleanPacket(packet);
+         }
+      });
+      communicator.attachListener(SetDoubleArrayParameterPacket.class, new PacketConsumer<SetDoubleArrayParameterPacket>()
+      {
+         @Override
+         public void receivedPacket(SetDoubleArrayParameterPacket packet)
+         {
+            onSetDoubleArrayPacket(packet);
+         }
+      });
       communicator.attachListener(SetDoubleParameterPacket.class, new PacketConsumer<SetDoubleParameterPacket>()
       {
          @Override
          public void receivedPacket(SetDoubleParameterPacket packet)
          {
-            Parameter parameter = ParameterRegistry.getInstance().getParameter(packet.getParameterName());
-
-            if (parameter == null)
-            {
-               // TODO: Error -- tried to set nonexistent parameter
-               System.err.println("Packet tried to set nonexistent parameter: " + packet.getParameterName());
-            }
-            else if (!(parameter instanceof DoubleParameter))
-            {
-               // TODO: Error -- tried to set wrong parameter type
-               System.err.println("Packet tried setting parameter of wrong type: " + packet.getParameterName());
-            }
-            else
-            {
-               ((DoubleParameter) parameter).set(packet.getParameterValue());
-               System.out.println("Setting " + parameter.getPath() + " to " + packet.getParameterValue());
-            }
+            onSetDoublePacket(packet);
          }
       });
+      communicator.attachListener(SetStringParameterPacket.class, new PacketConsumer<SetStringParameterPacket>()
+      {
+         @Override
+         public void receivedPacket(SetStringParameterPacket packet)
+         {
+            onStringPacket(packet);
+         }
+      });
+   }
+
+   public void onSetBooleanPacket(SetBooleanParameterPacket packet)
+   {
+      BooleanParameter parameter = lookup(packet.getParameterName(), BooleanParameter.class);
+      parameter.set(packet.getParameterValue());
+   }
+
+   public void onSetDoubleArrayPacket(SetDoubleArrayParameterPacket packet)
+   {
+      DoubleArrayParameter parameter = lookup(packet.getParameterName(), DoubleArrayParameter.class);
+      parameter.set(packet.getParameterValue());
+   }
+
+   public void onSetDoublePacket(SetDoubleParameterPacket packet)
+   {
+      DoubleParameter parameter = lookup(packet.getParameterName(), DoubleParameter.class);
+      parameter.set(packet.getParameterValue());
+   }
+
+   public void onStringPacket(SetStringParameterPacket packet)
+   {
+      StringParameter parameter = lookup(packet.getParameterName(), StringParameter.class);
+      parameter.set(packet.getParameterValue());
+   }
+
+   @SuppressWarnings("unchecked")
+   private <T extends Parameter> T lookup(String name, Class<T> type)
+   {
+      Parameter parameter = ParameterRegistry.getInstance().getParameter(name);
+
+      if (parameter == null)
+      {
+         System.err.println("Packet tried to set nonexistent parameter: " + name);
+      }
+      else if (!(type.isInstance(parameter)))
+      {
+         System.err.println(
+               "Packet tried setting parameter of wrong type (expected: " + type.getSimpleName() + ", got: " + parameter.getClass().getSimpleName() + "): "
+                     + name);
+      }
+
+      return (T) parameter;
    }
 }
