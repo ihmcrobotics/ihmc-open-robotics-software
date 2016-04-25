cbfe1783c294334aa721185ea6ccc51e29db9b06
Robert <rgriffin@ihmc.us>
2016-04-24 22:24:03 -0400

    got a test working that confirms the underlying theory of the jacobian transpose stuff

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/VirtualModelController.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/VirtualModelController.java
@@ -16,13 +16,12 @@ public class VirtualModelController
    private final GeometricJacobianHolder geometricJacobianHolder;
    private final RigidBody defaultRootBody;
 
-   private OneDoFJoint[] jointsToCompute;
    private Map<InverseDynamicsJoint, Double> jointTorques = new HashMap<>();
 
    private final ArrayList<RigidBody> endEffectors = new ArrayList<>();
+   private final HashMap<RigidBody, RigidBody> baseOfEndEffector = new HashMap<>();
    private final HashMap<RigidBody, Long> endEffectorJacobians = new HashMap<>();
-   private final HashMap<RigidBody, OneDoFJoint[]> limbJoints = new HashMap<>();
-   private final HashMap<RigidBody, DenseMatrix64F> endEffectorWrenchMatrices = new HashMap<>();
+   private final HashMap<RigidBody, Wrench> endEffectorWrenches = new HashMap<>();
    private final HashMap<RigidBody, DenseMatrix64F> endEffectorSelectionMatrices = new HashMap<>();
    private final HashMap<RigidBody, DenseMatrix64F> jointEffortMatrices = new HashMap<>();
 
@@ -46,10 +45,9 @@ public class VirtualModelController
       if (!endEffectors.contains(endEffector) && endEffector != null)
       {
          endEffectors.add(endEffector);
+         baseOfEndEffector.put(endEffector, base);
 
          OneDoFJoint[] joints = ScrewTools.createOneDoFJointPath(base, endEffector);
-         limbJoints.put(endEffector, joints);
-         jointsToCompute = appendJoints(jointsToCompute, joints);
 
          long jacobianID = geometricJacobianHolder.getOrCreateGeometricJacobian(joints, base.getBodyFixedFrame());
          endEffectorJacobians.put(endEffector, jacobianID);
@@ -60,54 +58,51 @@ public class VirtualModelController
       }
    }
 
+   private final Wrench dummyWrench = new Wrench();
+   private final DenseMatrix64F forceSelectionMatrix = new DenseMatrix64F(3, Wrench.SIZE);
    public void submitEndEffectorVirtualForce(RigidBody endEffector, FrameVector force)
    {
-      tmpWrenchMatrix.reshape(3, 1);
-      tmpWrenchMatrix.set(0, 0, force.getX());
-      tmpWrenchMatrix.set(1, 0, force.getY());
-      tmpWrenchMatrix.set(2, 0, force.getZ());
+      dummyWrench.setToZero();
+      dummyWrench.setToZero(baseOfEndEffector.get(endEffector).getBodyFixedFrame(), force.getReferenceFrame());
+      dummyWrench.setLinearPart(force);
 
-      endEffectorWrenchMatrices.put(endEffector, tmpWrenchMatrix);
-      endEffectorSelectionMatrices.put(endEffector, CommonOps.identity(3));
+      forceSelectionMatrix.set(0, 3, 1);
+      forceSelectionMatrix.set(1, 4, 1);
+      forceSelectionMatrix.set(2, 5, 1);
+
+      endEffectorWrenches.put(endEffector, dummyWrench);
+      endEffectorSelectionMatrices.put(endEffector, forceSelectionMatrix);
    }
 
    public void submitEndEffectorVirtualForce(RigidBody endEffector, FrameVector force, DenseMatrix64F selectionMatrix)
    {
-      tmpWrenchMatrix.reshape(3, 1);
-      tmpWrenchMatrix.set(0, 0, force.getX());
-      tmpWrenchMatrix.set(1, 0, force.getY());
-      tmpWrenchMatrix.set(2, 0, force.getZ());
+      dummyWrench.setToZero();
+      dummyWrench.setToZero(baseOfEndEffector.get(endEffector).getBodyFixedFrame(), force.getReferenceFrame());
+      dummyWrench.setLinearPart(force);
 
-      endEffectorWrenchMatrices.put(endEffector, tmpWrenchMatrix);
-      endEffectorSelectionMatrices.put(endEffector, selectionMatrix);
+      forceSelectionMatrix.set(0, 3, 1);
+      forceSelectionMatrix.set(1, 4, 1);
+      forceSelectionMatrix.set(2, 5, 1);
+
+      endEffectorWrenches.put(endEffector, dummyWrench);
+      endEffectorSelectionMatrices.put(endEffector, forceSelectionMatrix);
    }
 
    public void submitEndEffectorVirtualWrench(RigidBody endEffector, Wrench wrench)
    {
+      wrench.changeFrame(baseOfEndEffector.get(endEffector).getBodyFixedFrame());
+      wrench.changeBodyFrameAttachedToSameBody(endEffector.getBodyFixedFrame());
 
-      tmpWrenchMatrix.reshape(SpatialAccelerationVector.SIZE, 1);
-      tmpWrenchMatrix.set(0, 0, wrench.getAngularPartX());
-      tmpWrenchMatrix.set(1, 0, wrench.getAngularPartY());
-      tmpWrenchMatrix.set(2, 0, wrench.getAngularPartZ());
-      tmpWrenchMatrix.set(3, 0, wrench.getLinearPartX());
-      tmpWrenchMatrix.set(4, 0, wrench.getLinearPartY());
-      tmpWrenchMatrix.set(5, 0, wrench.getLinearPartZ());
-
-      endEffectorWrenchMatrices.put(endEffector, tmpWrenchMatrix);
-      endEffectorSelectionMatrices.put(endEffector, CommonOps.identity(SpatialAccelerationVector.SIZE));
+      endEffectorWrenches.put(endEffector, wrench);
+      endEffectorSelectionMatrices.put(endEffector, CommonOps.identity(Wrench.SIZE));
    }
 
    public void submitEndEffectorVirtualWrench(RigidBody endEffector, Wrench wrench, DenseMatrix64F selectionMatrix)
    {
-      tmpWrenchMatrix.reshape(SpatialAccelerationVector.SIZE, 1);
-      tmpWrenchMatrix.set(0, 0, wrench.getAngularPartX());
-      tmpWrenchMatrix.set(1, 0, wrench.getAngularPartY());
-      tmpWrenchMatrix.set(2, 0, wrench.getAngularPartZ());
-      tmpWrenchMatrix.set(3, 0, wrench.getLinearPartX());
-      tmpWrenchMatrix.set(4, 0, wrench.getLinearPartY());
-      tmpWrenchMatrix.set(5, 0, wrench.getLinearPartZ());
+      //wrench.changeFrame(baseOfEndEffector.get(endEffector).getBodyFixedFrame());
+      //wrench.changeBodyFrameAttachedToSameBody(endEffector.getBodyFixedFrame());
 
-      endEffectorWrenchMatrices.put(endEffector, tmpWrenchMatrix);
+      endEffectorWrenches.put(endEffector, wrench);
       endEffectorSelectionMatrices.put(endEffector, selectionMatrix);
    }
 
@@ -118,39 +113,54 @@ public class VirtualModelController
 
    public void reset()
    {
-      endEffectorWrenchMatrices.clear();
+      endEffectorWrenches.clear();
       endEffectorSelectionMatrices.clear();
       jointTorques.clear();
    }
 
 
    public final DenseMatrix64F tmpEndEffectorWrench = new DenseMatrix64F(1, 1);
+   public final DenseMatrix64F tmpJacobianMatrix = new DenseMatrix64F(1, 1);
+   public final DenseMatrix64F wrenchMatrix = new DenseMatrix64F(Wrench.SIZE, 1);
+
    public void compute(VirtualModelControlSolution virtualModelControlSolutionToPack)
    {
       for (RigidBody endEffector : endEffectors)
       {
-         if (!endEffectorWrenchMatrices.containsKey(endEffector) || !endEffectorSelectionMatrices.containsKey(endEffector))
+         if (!endEffectorWrenches.containsKey(endEffector) || !endEffectorSelectionMatrices.containsKey(endEffector))
             throw new RuntimeException("Not all registered end effectors have required forces to compute desired joint torques.");
       }
 
       for (RigidBody endEffector : endEffectors)
       {
-         DenseMatrix64F jacobianMatrix = geometricJacobianHolder.getJacobian(endEffectorJacobians.get(endEffector)).getJacobianMatrix();
-         DenseMatrix64F endEffectorWrenchMatrix = endEffectorWrenchMatrices.get(endEffector);
          DenseMatrix64F endEffectorSelectionMatrix = endEffectorSelectionMatrices.get(endEffector);
+         int taskSize = endEffectorSelectionMatrix.numRows;
+
+         // check and set frames
+         GeometricJacobian jacobian = geometricJacobianHolder.getJacobian(endEffectorJacobians.get(endEffector));
+         Wrench endEffectorWrench = endEffectorWrenches.get(endEffector);
+         endEffectorWrench.changeFrame(jacobian.getBaseFrame());
+         endEffectorWrench.changeBodyFrameAttachedToSameBody(jacobian.getJacobianFrame());
+
+         DenseMatrix64F jacobianMatrix = jacobian.getJacobianMatrix();
+         endEffectorWrench.getMatrix(wrenchMatrix);
          DenseMatrix64F jointEffortMatrix = jointEffortMatrices.get(endEffector);
          jointEffortMatrix.zero();
 
-         // Fix dimensionality of desired force to be achievable with the limb joints.
-         tmpEndEffectorWrench.reshape(Wrench.SIZE, 1);
-         CommonOps.mult(endEffectorSelectionMatrix, endEffectorWrenchMatrix, tmpEndEffectorWrench);
+         // Apply selection matrix
+         tmpEndEffectorWrench.reshape(taskSize, 1);
+         tmpJacobianMatrix.reshape(taskSize, jacobianMatrix.numCols);
+         CommonOps.mult(endEffectorSelectionMatrix, wrenchMatrix, tmpEndEffectorWrench);
+         CommonOps.mult(endEffectorSelectionMatrix, jacobianMatrix, tmpJacobianMatrix);
 
          // Compute desired joint torques
-         CommonOps.multTransA(jacobianMatrix, tmpEndEffectorWrench, jointEffortMatrix);
+         DenseMatrix64F altJointEffortMatrix = new DenseMatrix64F(Wrench.SIZE, 1);
+         altJointEffortMatrix = jacobian.computeJointTorques(endEffectorWrench);
+         CommonOps.multTransA(tmpJacobianMatrix, tmpEndEffectorWrench, jointEffortMatrix);
 
          // Write torques to map
          int index = 0;
-         for (OneDoFJoint joint : limbJoints.get(endEffector))
+         for (InverseDynamicsJoint joint : jacobian.getJointsInOrder())
          {
             jointTorques.put(joint, jointEffortMatrix.get(index));
             index++;
