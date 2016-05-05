deaa7580f9e51272a52c63b4e30fb0d4827cdaea
Nathan Mertins <nmertins@ihmc.us>
2016-05-05 13:51:46 -0500

    adding FPS to debug message

--- IHMCPerception/src/us/ihmc/ihmcPerception/depthData/RosPointCloudReceiver.java
+++ IHMCPerception/src/us/ihmc/ihmcPerception/depthData/RosPointCloudReceiver.java
@@ -3,6 +3,7 @@ package us.ihmc.ihmcPerception.depthData;
 import sensor_msgs.PointCloud2;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.sensorProcessing.parameters.DRCRobotPointCloudParameters;
+import us.ihmc.tools.time.Timer;
 import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.utilities.ros.subscriber.RosPointCloudSubscriber;
 
@@ -13,7 +14,13 @@ import java.util.Arrays;
 public class RosPointCloudReceiver extends RosPointCloudSubscriber
 {
    private final boolean DEBUG = false;
+   private Timer timer;
+   {
+      if(DEBUG)
+         timer = new Timer().start();
+   }
 
+   private final String rosTopic;
    private final ReferenceFrame cloudFrame;
    private final PointCloudDataReceiverInterface pointCloudDataReceiver;
    private final ReferenceFrame sensorframe;
@@ -34,6 +41,7 @@ public class RosPointCloudReceiver extends RosPointCloudSubscriber
    public RosPointCloudReceiver(String rosTopic, RosMainNode rosMainNode, ReferenceFrame cloudFrame, ReferenceFrame sensorframe,
          PointCloudDataReceiverInterface pointCloudDataReceiver, PointCloudSource... pointCloudSource)
    {
+      this.rosTopic = rosTopic;
       this.cloudFrame = cloudFrame;
       this.pointCloudDataReceiver = pointCloudDataReceiver;
       this.sensorframe = sensorframe;
@@ -46,7 +54,10 @@ public class RosPointCloudReceiver extends RosPointCloudSubscriber
 	public void onNewMessage(PointCloud2 pointCloud) 
 	{
       if(DEBUG)
-         System.out.println(getClass().getSimpleName() + ": Received point cloud from ROS");
+      {
+         System.out.println(getClass().getSimpleName() + ": Received point cloud from " + rosTopic + " at rate " + 1.0 / timer.averageLap() + " FPS");
+         timer.lap();
+      }
 
       UnpackedPointCloud pointCloudData = unpackPointsAndIntensities(pointCloud);
 		Point3d[] points = pointCloudData.getPoints();
