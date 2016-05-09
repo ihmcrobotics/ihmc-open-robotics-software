ab1d49f2aa70e3b0c977a6d67637cb2015b64600
Stephen McCrory <smccrory@ihmc.us>
2016-05-09 17:29:24 -0500

    Adding execution mode to footstep data list. generating new ros message

--- IHMCCommunication/src/us/ihmc/humanoidRobotics/communication/packets/ExecutionMode.java
+++ IHMCCommunication/src/us/ihmc/humanoidRobotics/communication/packets/ExecutionMode.java
@@ -7,5 +7,7 @@ public enum ExecutionMode
    @RosEnumValueDocumentation(documentation = "This message will override the previous.")
    OVERRIDE,
    @RosEnumValueDocumentation(documentation = "The previous message will first be executed before executing this message. When sending a series of queued messages, the very first has to be declared as OVERRIDE.")
-   QUEUE
+   QUEUE;
+
+   public static final ExecutionMode[] values = values();
 }
