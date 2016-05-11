7e083b0949e07b86bd0df3f007fc4510fd5cf0d6
Doug Stephen <dstephen@ihmc.us>
2016-05-11 12:15:37 -0500

    Don't propagate UniqeID to fields in MultiPacket messages

--- IHMCCommunication/src/us/ihmc/communication/controllerAPI/CommandInputManager.java
+++ IHMCCommunication/src/us/ihmc/communication/controllerAPI/CommandInputManager.java
@@ -118,7 +118,6 @@ public class CommandInputManager
 
       if (message instanceof MultiplePacketHolder)
       {
-         ((MultiplePacketHolder) message).parseMessageIdToChildren();
          submitMessages(((MultiplePacketHolder) message).getPackets());
          return;
       }
--- IHMCCommunication/src/us/ihmc/communication/packets/MultiplePacketHolder.java
+++ IHMCCommunication/src/us/ihmc/communication/packets/MultiplePacketHolder.java
@@ -4,7 +4,5 @@ import java.util.List;
 
 public interface MultiplePacketHolder
 {
-   public abstract void parseMessageIdToChildren();
-
    public abstract List<Packet<?>> getPackets();
 }
