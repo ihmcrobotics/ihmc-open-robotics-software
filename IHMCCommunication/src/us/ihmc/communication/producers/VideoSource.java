bf7b2368689cac3a482e94311430ddeacb914454
Duncan Calvert <dcalvert@ihmc.us>
2016-05-20 11:33:18 -0500

    NRI-80: Add VideoSource enum to VideoPacket. Send OpenCV filtered images to UI from behavior.

---
+++ IHMCCommunication/src/us/ihmc/communication/producers/VideoSource.java
@@ -0,0 +1,16 @@
+package us.ihmc.communication.producers;
+
+/**
+ * Make a new enum when you need a new video source.
+ * 
+ * @author Duncan Calvert
+ */
+public enum VideoSource
+{
+   MULTISENSE,
+   MULTISENSE_FISHEYE,
+   CV_THRESHOLD,
+   IMAGE_PROCESSING_BEHAVIOR,
+   AWARE_FACE_TRACKER,
+   ;
+}
