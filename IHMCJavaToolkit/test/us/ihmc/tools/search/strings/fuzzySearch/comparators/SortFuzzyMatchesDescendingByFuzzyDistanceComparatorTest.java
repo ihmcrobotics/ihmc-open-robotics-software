a03501c0d2c32693581a3cd1618e9d5ea103e1ac
Doug Stephen <dstephen@ihmc.us>
2016-06-15 12:49:28 -0500

    ROSI-457: Porting over the search engine from the old demo to Java Toolkit. Wrote tests for the comparators.

---
+++ IHMCJavaToolkit/test/us/ihmc/tools/search/strings/fuzzySearch/comparators/SortFuzzyMatchesDescendingByFuzzyDistanceComparatorTest.java
@@ -0,0 +1,50 @@
+package us.ihmc.tools.search.strings.fuzzySearch.comparators;
+
+import org.junit.Test;
+import us.ihmc.tools.search.strings.fuzzySearch.CombinedFuzzySearchResult;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
+ */
+public class SortFuzzyMatchesDescendingByFuzzyDistanceComparatorTest
+{
+
+   @Test
+   public void testCompare()
+   {
+      Random random = new Random(1976L);
+      for (int i = 0; i < 10000; i++)
+      {
+         int firstRandomDistance = random.nextInt(300) + 1;
+         int secondRandomDistance = random.nextInt(300) + 1;
+
+         CombinedFuzzySearchResult aardvark = new CombinedFuzzySearchResult("Aardvark", CombinedFuzzySearchResult.CombinedFuzzySearchResultType.FUZZY,
+               firstRandomDistance, null);
+
+         CombinedFuzzySearchResult sebastopol = new CombinedFuzzySearchResult("Sebastopol", CombinedFuzzySearchResult.CombinedFuzzySearchResultType.FUZZY,
+               secondRandomDistance, null);
+
+         SortFuzzyMatchesDescendingByFuzzyDistanceComparator comparator = new SortFuzzyMatchesDescendingByFuzzyDistanceComparator();
+
+         int compare = comparator.compare(aardvark, sebastopol);
+
+         if(firstRandomDistance > secondRandomDistance)
+         {
+            assertTrue(compare < 0);
+         }
+         else if (firstRandomDistance < secondRandomDistance)
+         {
+            assertTrue(compare > 0);
+         }
+         else if(firstRandomDistance == secondRandomDistance)
+         {
+            assertTrue(compare == 0);
+         }
+      }
+   }
+}
