8bb328ba2043213dfab6ad446750e463892dae3f
Doug Stephen <dstephen@ihmc.us>
2016-06-15 13:23:37 -0500

    ROSI-457: Added JavaDoc to search engine entry point.

--- IHMCJavaToolkit/src/us/ihmc/tools/search/strings/fuzzySearch/CombinedFuzzySearcher.java
+++ IHMCJavaToolkit/src/us/ihmc/tools/search/strings/fuzzySearch/CombinedFuzzySearcher.java
@@ -19,6 +19,41 @@ public class CombinedFuzzySearcher
    {
    }
 
+   /**
+    * This method uses several search techniques simultaneously to heuristically build up a list
+    * of possible matches based on the passed in search string. It searches the supplied collection
+    * for all possible matches.
+    *
+    * Three searches will be performed: Matching on the exact substring that is passed in, matching on
+    * regular expression, and using a "fuzzy" search similar to many popular text editors and IDEs that
+    * support fuzzy searching w/out the need for wildcards (Atom, Sublime Text, IntelliJ IDEA, etc.) provided
+    * by Apache Commons Lang.
+    *
+    * The returned List is sorted and has element uniqueness based on the following set of criteria:
+    *
+    * 1.) Any given String, for its fully qualified name, will only show up once, and
+    * 2.) The matching information for that String will correspond to search "priority"
+    *
+    * In this case, priority simply means that if the same String is identified by multiple
+    * search techniques (e.g. both Exact Substring and Fuzzy Search find the same String for the same query), then only
+    * the {@link CombinedFuzzySearchResult} with {@link us.ihmc.tools.search.strings.fuzzySearch.CombinedFuzzySearchResult.CombinedFuzzySearchResultType} corresponding to the highest
+    * priority will be in the result set. The priorities are:
+    *
+    * 1.) Exact Substring has highest priority
+    * 2.) Regular Expression is next
+    * 3.) Fuzzy results are last
+    *
+    * @see CombinedFuzzySearchResult
+    * @see us.ihmc.tools.search.strings.fuzzySearch.CombinedFuzzySearchResult.CombinedFuzzySearchResultType
+    * @see StringUtils#getFuzzyDistance(CharSequence, CharSequence, Locale)
+    * @see Pattern
+    * @see Matcher
+    *
+    * @param stringsToSearch The registry to search. Searches will be recursive and will still utilize the fully qualified name even if this is not the root registry.
+    * @param searchString The query to search for.
+    *
+    * @return A list of unique {@link CombinedFuzzySearchResult}s sorted according to their {@link us.ihmc.tools.search.strings.fuzzySearch.CombinedFuzzySearchResult.CombinedFuzzySearchResultType} priority.
+    */
    public static List<CombinedFuzzySearchResult> getAllSearchResultsForSearchString(Collection<String> stringsToSearch, String searchString)
    {
       Set<CombinedFuzzySearchResult> resultsOfFuzzyDistanceSearch;
