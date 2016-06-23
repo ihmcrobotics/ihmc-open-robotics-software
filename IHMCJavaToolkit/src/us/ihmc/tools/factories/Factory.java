5f13c85c695b2a79209942e940c36a817bcb53f1
Duncan Calvert <dcalvert@ihmc.us>
2016-06-23 16:45:56 -0500

    BEAST-281: Remove uneccesary boilerplate after learning setAccessible was a thing.

--- IHMCJavaToolkit/src/us/ihmc/tools/factories/Factory.java
+++
@@ -1,9 +0,0 @@
-package us.ihmc.tools.factories;
-
-import java.lang.reflect.Field;
-
-public interface Factory extends FactoryFieldAccessor
-{
-   @Override
-   Object accessFieldValue(Field field, Object object) throws IllegalArgumentException, IllegalAccessException;
-}
--- IHMCJavaToolkit/src/us/ihmc/tools/factories/FactoryFieldAccessor.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.tools.factories;
-
-import java.lang.reflect.Field;
-
-public interface FactoryFieldAccessor
-{
-   public Object accessFieldValue(Field field, Object object) throws IllegalArgumentException, IllegalAccessException;
-}
