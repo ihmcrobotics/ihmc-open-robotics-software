133047d5b95c936b672ea405318923e5d8e8a6e2
Sylvain Bertrand <sbertrand@ihmc.us>
2016-06-29 11:52:45 -0500

    Changed to depend on JointTorqueOffsetEstimator.

--- IHMCWholeBodyController/src/us/ihmc/wholeBodyController/diagnostics/TorqueOffsetPrinter.java
+++ IHMCWholeBodyController/src/us/ihmc/wholeBodyController/diagnostics/TorqueOffsetPrinter.java
@@ -1,5 +1,6 @@
 package us.ihmc.wholeBodyController.diagnostics;
 
-public interface TorqueOffsetPrinter {
-	public abstract void printTorqueOffsets(DiagnosticsWhenHangingController diagnosticsWhenHangingController);
+public interface TorqueOffsetPrinter
+{
+   public abstract void printTorqueOffsets(JointTorqueOffsetEstimator jointTorqueOffsetEstimator);
 }
