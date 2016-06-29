57b7c3bae851dd2876b6a1c3f4e2d55a5472f30a
Sylvain Bertrand <sbertrand@ihmc.us>
2016-06-29 08:52:41 -0500

    Simplified the Task interface.

--- IHMCJavaToolkit/src/us/ihmc/tools/taskExecutor/NullTask.java
+++ IHMCJavaToolkit/src/us/ihmc/tools/taskExecutor/NullTask.java
@@ -3,41 +3,24 @@ package us.ihmc.tools.taskExecutor;
 
 public class NullTask implements Task
 {
+   @Override
    public void doTransitionIntoAction()
    {
    }
 
+   @Override
    public void doAction()
    {
    }
 
+   @Override
    public void doTransitionOutOfAction()
    {
    }
 
+   @Override
    public boolean isDone()
    {
       return true;
-   }
-
-   @Override
-   public void pause()
-   {
-      // TODO Auto-generated method stub
-      
-   }
-
-   @Override
-   public void resume()
-   {
-      // TODO Auto-generated method stub
-      
-   }
-
-   @Override
-   public void stop()
-   {
-      // TODO Auto-generated method stub
-      
    }
 }
