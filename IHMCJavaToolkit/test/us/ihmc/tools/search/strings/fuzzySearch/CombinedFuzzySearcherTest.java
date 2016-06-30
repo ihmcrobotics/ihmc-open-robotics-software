d9152b4393bd7ab7e7cefeb91609475336cd5aea
Doug Stephen <dstephen@ihmc.us>
2016-06-30 16:34:59 -0500

    Removed old messages that were in the wrong package.

---
+++ IHMCJavaToolkit/test/us/ihmc/tools/search/strings/fuzzySearch/CombinedFuzzySearcherTest.java
@@ -0,0 +1,18 @@
+package us.ihmc.tools.search.strings.fuzzySearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
+ */
+public class CombinedFuzzySearcherTest
+{
+
+   @Test
+   public void testGetAllSearchResultsForSearchString()
+   {
+
+   }
+}
