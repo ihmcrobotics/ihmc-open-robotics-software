30fff3d404ee06bf5d2de9bd0ebe851bb5ffc14c
Duncan Calvert <dcalvert@ihmc.us>
2016-07-01 11:28:59 -0500

    NONE: Exclude empty test.

--- IHMCJavaToolkit/test/us/ihmc/tools/search/strings/fuzzySearch/CombinedFuzzySearcherTest.java
+++ IHMCJavaToolkit/test/us/ihmc/tools/search/strings/fuzzySearch/CombinedFuzzySearcherTest.java
@@ -2,15 +2,18 @@ package us.ihmc.tools.search.strings.fuzzySearch;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestClass;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
+import us.ihmc.tools.testing.TestPlanTarget;
 
 /**
  * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
  */
+@DeployableTestClass(targets = TestPlanTarget.Exclude)
 public class CombinedFuzzySearcherTest
 {
-
-   @Test
+   @DeployableTestMethod(estimatedDuration = 0.0)
+   @Test(timeout = 1000)
    public void testGetAllSearchResultsForSearchString()
    {
 
