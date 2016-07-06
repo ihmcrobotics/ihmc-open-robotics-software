529f5f769a1bf38cc0e4ed44abfee0b8d1633927
Nathan Mertins <nmertins@ihmc.us>
2016-07-06 11:55:35 -0500

    converted to abstract class and pulled out methods that would likely have nontrivial implementations

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/footSwitch/TouchdownDetectorBasedFootswitch.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/sensors/footSwitch/TouchdownDetectorBasedFootswitch.java
@@ -1,78 +1,37 @@
 package us.ihmc.commonWalkingControlModules.sensors.footSwitch;
 
-import us.ihmc.SdfLoader.SDFFullRobotModel;
 import us.ihmc.commonWalkingControlModules.touchdownDetector.TouchdownDetector;
-import us.ihmc.humanoidRobotics.bipedSupportPolygons.ContactablePlaneBody;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.BooleanYoVariable;
-import us.ihmc.robotics.geometry.FramePoint2d;
-import us.ihmc.robotics.math.frames.YoFramePoint2d;
-import us.ihmc.robotics.referenceFrames.ReferenceFrame;
-import us.ihmc.robotics.screwTheory.Wrench;
 
 import java.util.ArrayList;
 
-public class TouchdownDetectorBasedFootswitch implements FootSwitchInterface
+public abstract class TouchdownDetectorBasedFootswitch implements FootSwitchInterface
 {
-   private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName() + "Registry");
-   private final ArrayList<TouchdownDetector> touchdownDetectors = new ArrayList<>();
+   protected final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName() + "Registry");
+   protected final ArrayList<TouchdownDetector> touchdownDetectors = new ArrayList<>();
 
-   private final ContactablePlaneBody foot;
-   private final double totalRobotWeight;
+   protected final BooleanYoVariable hasTouchedDown;
 
-   private final BooleanYoVariable hasTouchedDown;
-   private final YoFramePoint2d yoResolvedCoP;
-
-   public TouchdownDetectorBasedFootswitch(ContactablePlaneBody foot, SDFFullRobotModel fullRobotModel, double totalRobotWeight, YoVariableRegistry parentRegistry)
+   public TouchdownDetectorBasedFootswitch()
    {
-      this.foot = foot;
-      this.totalRobotWeight = totalRobotWeight;
-
       hasTouchedDown = new BooleanYoVariable("hasTouchedDown", registry);
-      yoResolvedCoP = new YoFramePoint2d(foot.getName() + "ResolvedCoP", "", foot.getSoleFrame(), registry);
-
-      parentRegistry.addChild(registry);
+      setupTouchdownDetectors(touchdownDetectors);
    }
+
+   abstract protected void setupTouchdownDetectors(ArrayList<TouchdownDetector> touchdownDetectors);
 
    @Override
    public boolean hasFootHitGround()
    {
+      boolean ret = true;
+
       for(TouchdownDetector touchdownDetector : touchdownDetectors)
-         touchdownDetector.hasTouchedDown();
+         ret &= touchdownDetector.hasTouchedDown();
 
-      return this.hasTouchedDown.getBooleanValue();
-   }
+      hasTouchedDown.set(ret);
 
-   @Override
-   public double computeFootLoadPercentage()
-   {
-      return Double.NaN;
-   }
-
-   @Override
-   public void computeAndPackCoP(FramePoint2d copToPack)
-   {
-      copToPack.setToNaN(getMeasurementFrame());
-   }
-
-   @Override
-   public void updateCoP()
-   {
-      yoResolvedCoP.setToZero();
-   }
-
-   @Override
-   public void computeAndPackFootWrench(Wrench footWrenchToPack)
-   {
-      footWrenchToPack.setToZero();
-      if (hasFootHitGround())
-         footWrenchToPack.setLinearPartZ(totalRobotWeight / 4.0);
-   }
-
-   @Override
-   public ReferenceFrame getMeasurementFrame()
-   {
-      return foot.getSoleFrame();
+      return hasTouchedDown.getBooleanValue();
    }
 
    @Override
