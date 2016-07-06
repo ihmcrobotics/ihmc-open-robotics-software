d13f7adf276e4593fe5c851d93d3da6d8863c0f7
bshrewsbury <bshrewsbury@ihmc.us>
2016-07-06 14:27:54 -0500

    fix a typo in the testSuite

--- IHMCQuadrupedRobotics/test/us/ihmc/quadrupedRobotics/generatedTestSuites/IHMCQuadrupedRoboticsAFastTestSuite.java
+++ IHMCQuadrupedRobotics/test/us/ihmc/quadrupedRobotics/generatedTestSuites/IHMCQuadrupedRoboticsAFastTestSuite.java
@@ -17,7 +17,7 @@ import us.ihmc.tools.testing.TestPlanTarget;
    us.ihmc.quadrupedRobotics.planning.QuadrupedXGaitPlannerTest.class,
    us.ihmc.quadrupedRobotics.supportPolygon.QuadrupedSupportPolygonTest.class,
    us.ihmc.quadrupedRobotics.providers.QuadrupedControllerInputProviderTest.class,
-   us.ihmc.quadrupedRobotics.controller.force.toolbox.LinearInvertedPendulumModel.class
+   us.ihmc.quadrupedRobotics.controller.force.toolbox.LinearInvertedPendulumModelTest.class
 })
 
 public class IHMCQuadrupedRoboticsAFastTestSuite
