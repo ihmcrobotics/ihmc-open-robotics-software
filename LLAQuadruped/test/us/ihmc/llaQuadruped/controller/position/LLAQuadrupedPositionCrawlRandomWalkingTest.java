25237cbc2b76a7b5c259fdfa12437233a564a3db
Duncan Calvert <dcalvert@ihmc.us>
2016-07-07 17:31:54 -0500

    BEAST-330: Abstract quadruped position tests.

--- LLAQuadruped/test/us/ihmc/llaQuadruped/controller/position/LLAQuadrupedPositionCrawlRandomWalkingTest.java
+++ LLAQuadruped/test/us/ihmc/llaQuadruped/controller/position/LLAQuadrupedPositionCrawlRandomWalkingTest.java
@@ -1,4 +1,4 @@
-package us.ihmc.llaQuadruped.controller.force;
+package us.ihmc.llaQuadruped.controller.position;
 
 import java.io.IOException;
 
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import us.ihmc.llaQuadruped.LLAQuadrupedTestFactory;
 import us.ihmc.quadrupedRobotics.QuadrupedTestFactory;
-import us.ihmc.quadrupedRobotics.controller.force.QuadrupedXGaitRandomWalkingTest;
+import us.ihmc.quadrupedRobotics.controller.position.QuadrupedPositionCrawlRandomWalkingTest;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.simulationconstructionset.util.simulationRunner.ControllerFailureException;
 import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestClass;
@@ -14,41 +14,33 @@ import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
 import us.ihmc.tools.testing.TestPlanTarget;
 
 @DeployableTestClass(targets = TestPlanTarget.InDevelopment)
-public class LLAQuadrupedXGaitRandomWalkingTest extends QuadrupedXGaitRandomWalkingTest
+public class LLAQuadrupedPositionCrawlRandomWalkingTest extends QuadrupedPositionCrawlRandomWalkingTest
 {
    @Override
    public QuadrupedTestFactory createQuadrupedTestFactory()
    {
       return new LLAQuadrupedTestFactory();
    }
-
+   
    @Override
-   @DeployableTestMethod(estimatedDuration = 100.0)
-   @Test(timeout = 500000)
-   public void testExtremeRandomWalking() throws SimulationExceededMaximumTimeException, ControllerFailureException, IOException
-   {
-      super.testExtremeRandomWalking();
-   }
-
-   @Override
-   @DeployableTestMethod(estimatedDuration = 100.0)
-   @Test(timeout = 500000)
+   @DeployableTestMethod(estimatedDuration = 300.0)
+   @Test(timeout = 1000000)
    public void testWalkingRandomly() throws SimulationExceededMaximumTimeException, ControllerFailureException, IOException
    {
       super.testWalkingRandomly();
    }
 
    @Override
-   @DeployableTestMethod(estimatedDuration = 75.0)
-   @Test(timeout = 600000)
+   @DeployableTestMethod(estimatedDuration = 200.0)
+   @Test(timeout = 1000000)
    public void testWalkingAtRandomSpeedsWithStops() throws SimulationExceededMaximumTimeException, ControllerFailureException, IOException
    {
       super.testWalkingAtRandomSpeedsWithStops();
    }
 
    @Override
-   @DeployableTestMethod(estimatedDuration = 100.0)
-   @Test(timeout = 600000)
+   @DeployableTestMethod(estimatedDuration = 300.0)
+   @Test(timeout = 1000000)
    public void testWalkingRandomVelocitiesStoppingAndTurning() throws SimulationExceededMaximumTimeException, ControllerFailureException, IOException
    {
       super.testWalkingRandomVelocitiesStoppingAndTurning();
---
+++ LLAQuadruped/test/us/ihmc/llaQuadruped/controller/position/LLAQuadrupedPositionCrawlVelocityTest.java
@@ -0,0 +1,40 @@
+package us.ihmc.llaQuadruped.controller.position;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import us.ihmc.llaQuadruped.LLAQuadrupedTestFactory;
+import us.ihmc.quadrupedRobotics.QuadrupedTestFactory;
+import us.ihmc.quadrupedRobotics.controller.position.QuadrupedPositionCrawlVelocityTest;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.simulationconstructionset.util.simulationRunner.ControllerFailureException;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestClass;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
+import us.ihmc.tools.testing.TestPlanTarget;
+
+@DeployableTestClass(targets = TestPlanTarget.InDevelopment)
+public class LLAQuadrupedPositionCrawlVelocityTest extends QuadrupedPositionCrawlVelocityTest
+{
+   @Override
+   public QuadrupedTestFactory createQuadrupedTestFactory()
+   {
+      return new LLAQuadrupedTestFactory();
+   }
+   
+   @Override
+   @DeployableTestMethod(estimatedDuration = 90.0)
+   @Test(timeout = 600000)
+   public void testWalkingForward() throws SimulationExceededMaximumTimeException, ControllerFailureException, IOException
+   {
+      super.testWalkingForward();
+   }
+   
+   @Override
+   @DeployableTestMethod(estimatedDuration = 30.0)
+   @Test(timeout = 600000)
+   public void testWalkingBackward() throws SimulationExceededMaximumTimeException, ControllerFailureException, IOException
+   {
+      super.testWalkingBackward();
+   }
+}
