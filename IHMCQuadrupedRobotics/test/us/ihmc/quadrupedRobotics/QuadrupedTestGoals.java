a2043c49076a794c2dd6651c6014ef27c2df5fbe
Duncan Calvert <dcalvert@ihmc.us>
2016-07-11 12:36:04 -0500

    NONE: Fix notFallen null pointer bug.

--- IHMCQuadrupedRobotics/test/us/ihmc/quadrupedRobotics/QuadrupedTestGoals.java
+++ IHMCQuadrupedRobotics/test/us/ihmc/quadrupedRobotics/QuadrupedTestGoals.java
@@ -4,8 +4,13 @@ import us.ihmc.robotics.testing.YoVariableTestGoal;
 
 public class QuadrupedTestGoals
 {
-   public static YoVariableTestGoal notFallen(QuadrupedTestYoVariables variables)
+   public static YoVariableTestGoal notFallen(QuadrupedForceTestYoVariables variables)
    {
       return YoVariableTestGoal.doubleGreaterThan(variables.getRobotBodyZ(), variables.getStanceHeight().getDoubleValue() / 2.0);
+   }
+   
+   public static YoVariableTestGoal notFallen(QuadrupedPositionTestYoVariables variables)
+   {
+      return YoVariableTestGoal.doubleGreaterThan(variables.getRobotBodyZ(), variables.getDesiredCoMPositionZ().getDoubleValue() / 2.0);
    }
 }
