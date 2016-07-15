3f9399d29484f08fb884382585ec8c5d4f65e21a
Duncan Calvert <dcalvert@ihmc.us>
2016-07-15 11:56:55 -0500

    BEAST-333: Add XGait trot tests.

--- LLAQuadruped/test/us/ihmc/llaQuadruped/generatedTestSuites/LLAQuadrupedBInDevelopmentTestSuite.java
+++ LLAQuadruped/test/us/ihmc/llaQuadruped/generatedTestSuites/LLAQuadrupedBInDevelopmentTestSuite.java
@@ -12,7 +12,8 @@ import us.ihmc.tools.testing.TestPlanTarget;
 @TestSuiteTarget(TestPlanTarget.InDevelopment)
 @SuiteClasses
 ({
-   us.ihmc.llaQuadruped.controller.force.LLAQuadrupedXGaitFlatGroundWalkingTest.class
+   us.ihmc.llaQuadruped.controller.force.LLAQuadrupedXGaitBumpyTerrainWalkingTest.class,
+   us.ihmc.llaQuadruped.controller.force.LLAQuadrupedXGaitFlatGroundTrotTest.class
 })
 
 public class LLAQuadrupedBInDevelopmentTestSuite
