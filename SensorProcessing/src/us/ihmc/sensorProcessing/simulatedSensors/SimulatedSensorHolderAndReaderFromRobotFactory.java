9f73518c5e2b47ba32f5293fdd132021ae59a0b9
Aimee Gerold <agerold@ihmc.us>
2016-07-18 18:08:20 -0500

    Replaced sensorNoiseParameters boolean with BooleanYoVariable for corrupting orientation sensors

--- SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SimulatedSensorHolderAndReaderFromRobotFactory.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SimulatedSensorHolderAndReaderFromRobotFactory.java
@@ -41,7 +41,7 @@ public class SimulatedSensorHolderAndReaderFromRobotFactory implements SensorRea
    private final ArrayList<WrenchCalculatorInterface> groundContactPointBasedWrenchCalculators = new ArrayList<WrenchCalculatorInterface>();
 
    private SimulatedSensorHolderAndReader simulatedSensorHolderAndReader;
-   private BooleanYoVariable yawDriftCorruptor;
+   private BooleanYoVariable corruptYawDrift;
 
    private StateEstimatorSensorDefinitions stateEstimatorSensorDefinitions;
    private final SensorProcessingConfiguration sensorProcessingConfiguration;
@@ -79,9 +79,9 @@ public class SimulatedSensorHolderAndReaderFromRobotFactory implements SensorRea
       Map<IMUMount, IMUDefinition> imuDefinitions = stateEstimatorSensorDefinitionsFromRobotFactory.getIMUDefinitions();
       Map<WrenchCalculatorInterface, ForceSensorDefinition> forceSensors = stateEstimatorSensorDefinitionsFromRobotFactory.getForceSensorDefinitions();
 
-      yawDriftCorruptor.set(false);
+      corruptYawDrift.set(false);
 
-      if (yawDriftCorruptor.getBooleanValue())
+      if (corruptYawDrift.getBooleanValue())
          this.simulatedSensorHolderAndReader = new SimulatedSensorHolderAndReaderWithYawDriftCorruptor(stateEstimatorSensorDefinitions, sensorProcessingConfiguration, robot.getYoTime(), registry);
       else
          this.simulatedSensorHolderAndReader = new SimulatedSensorHolderAndReader(stateEstimatorSensorDefinitions, sensorProcessingConfiguration, robot.getYoTime(), registry);
@@ -162,7 +162,7 @@ public class SimulatedSensorHolderAndReaderFromRobotFactory implements SensorRea
 
          SimulatedOrientationSensorFromRobot orientationSensor = new SimulatedOrientationSensorFromRobot(sensorName, imuMount, registry);
 
-         if (sensorNoiseParameters != null)
+         if (corruptYawDrift.getBooleanValue())
          {
             double yawDriftAcceleration = sensorNoiseParameters.getIMUYawDriftAcceleration();
             OrientationConstantAcceleratingYawDriftCorruptor yawDriftCorruptor = new OrientationConstantAcceleratingYawDriftCorruptor(sensorName, estimateDT, registry);
--- SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SimulatedSensorHolderAndReaderWithYawDriftCorruptor.java
+++ SensorProcessing/src/us/ihmc/sensorProcessing/simulatedSensors/SimulatedSensorHolderAndReaderWithYawDriftCorruptor.java
@@ -2,13 +2,7 @@ package us.ihmc.sensorProcessing.simulatedSensors;
 
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
-import us.ihmc.robotics.screwTheory.OneDoFJoint;
-import us.ihmc.robotics.time.TimeTools;
 import us.ihmc.sensorProcessing.stateEstimation.SensorProcessingConfiguration;
-import us.ihmc.simulationconstructionset.simulatedSensors.WrenchCalculatorInterface;
-
-import javax.vecmath.Matrix3d;
-import javax.vecmath.Vector3d;
 
 public class SimulatedSensorHolderAndReaderWithYawDriftCorruptor extends SimulatedSensorHolderAndReader
 {
@@ -16,78 +10,5 @@ public class SimulatedSensorHolderAndReaderWithYawDriftCorruptor extends Simulat
          SensorProcessingConfiguration sensorProcessingConfiguration, DoubleYoVariable yoTime, YoVariableRegistry parentRegistry)
    {
       super(stateEstimatorSensorDefinitions, sensorProcessingConfiguration, yoTime, parentRegistry);
-   }
-
-   @Override
-   public void read()
-   {
-      for (int i = 0; i < jointPositionSensors.size(); i++)
-      {
-         OneDoFJoint oneDoFJoint = jointPositionSensors.get(i).getLeft();
-         SimulatedOneDoFJointPositionSensor simulatedOneDoFJointPositionSensor = jointPositionSensors.get(i).getRight();
-         simulatedOneDoFJointPositionSensor.startComputation();
-         simulatedOneDoFJointPositionSensor.waitUntilComputationIsDone();
-         double q = simulatedOneDoFJointPositionSensor.getJointPositionOutputPort().getData().doubleValue();
-         sensorProcessing.setJointPositionSensorValue(oneDoFJoint, q);
-      }
-
-      for (int i = 0; i < jointTorqueSensors.size(); i++)
-      {
-         OneDoFJoint oneDoFJoint = jointTorqueSensors.get(i).getLeft();
-         SimulatedOneDoFJointTorqueSensor simulatedOneDoFJointTorqueSensor = jointTorqueSensors.get(i).getRight();
-         simulatedOneDoFJointTorqueSensor.startComputation();
-         simulatedOneDoFJointTorqueSensor.waitUntilComputationIsDone();
-         double tau = simulatedOneDoFJointTorqueSensor.getJointTorqueOutputPort().getData().doubleValue();
-         sensorProcessing.setJointTauSensorValue(oneDoFJoint, tau);
-      }
-
-      for (int i = 0; i < jointVelocitySensors.size(); i++)
-      {
-         SimulatedOneDoFJointVelocitySensor simulatedOneDoFJointVelocitySensor = jointVelocitySensors.get(i).getRight();
-         simulatedOneDoFJointVelocitySensor.startComputation();
-         simulatedOneDoFJointVelocitySensor.waitUntilComputationIsDone();
-         double value = simulatedOneDoFJointVelocitySensor.getJointVelocityOutputPort().getData().doubleValue();
-         sensorProcessing.setJointVelocitySensorValue(jointVelocitySensors.get(i).getLeft(), value);
-      }
-
-      for (int i = 0; i < orientationSensors.size(); i++)
-      {
-         SimulatedOrientationSensorFromRobot orientationSensor = orientationSensors.get(i).getRight();
-         orientationSensor.startComputation();
-         orientationSensor.waitUntilComputationIsDone();
-         Matrix3d value = orientationSensor.getOrientationOutputPort().getData();
-         sensorProcessing.setOrientationSensorValue(orientationSensors.get(i).getLeft(), value);
-      }
-
-      for (int i = 0; i < angularVelocitySensors.size(); i++)
-      {
-         SimulatedAngularVelocitySensorFromRobot angularVelocitySensor = angularVelocitySensors.get(i).getRight();
-         angularVelocitySensor.startComputation();
-         angularVelocitySensor.waitUntilComputationIsDone();
-         Vector3d value = angularVelocitySensor.getAngularVelocityOutputPort().getData();
-         sensorProcessing.setAngularVelocitySensorValue(angularVelocitySensors.get(i).getLeft(), value);
-      }
-
-      for (int i = 0; i < linearAccelerationSensors.size(); i++)
-      {
-
-         SimulatedLinearAccelerationSensorFromRobot linearAccelerationSensor = linearAccelerationSensors.get(i).getRight();
-         linearAccelerationSensor.startComputation();
-         linearAccelerationSensor.waitUntilComputationIsDone();
-         Vector3d value = linearAccelerationSensor.getLinearAccelerationOutputPort().getData();
-         sensorProcessing.setLinearAccelerationSensorValue(linearAccelerationSensors.get(i).getLeft(), value);
-      }
-
-      for (int i = 0; i < forceTorqueSensors.size(); i++)
-      {
-         final WrenchCalculatorInterface forceTorqueSensor = forceTorqueSensors.get(i).getRight();
-         forceTorqueSensor.calculate();
-         sensorProcessing.setForceSensorValue(forceTorqueSensors.get(i).getLeft(), forceTorqueSensor.getWrench());
-      }
-
-      long timestamp = TimeTools.secondsToNanoSeconds(yoTime.getDoubleValue());
-      sensorProcessing.startComputation(timestamp, timestamp, -1);
-
-      step.increment();
    }
 }
