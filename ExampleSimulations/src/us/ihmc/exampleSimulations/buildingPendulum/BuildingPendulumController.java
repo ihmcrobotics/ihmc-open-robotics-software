db6be2ebe9f54643133ef6460b61ef165ebc2e1c
Stephen Tamba <stamba@ihmc.us>
2016-07-21 11:26:01 -0500

    I was able to put the rod in the simulation.

--- ExampleSimulations/src/us/ihmc/exampleSimulations/buildingPendulum/BuildingPendulumController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/buildingPendulum/BuildingPendulumController.java
@@ -3,6 +3,7 @@ package us.ihmc.exampleSimulations.buildingPendulum;
 import com.vividsolutions.jts.index.bintree.Root;
 import us.ihmc.robotics.dataStructures.variable.BooleanYoVariable;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.dataStructures.variable.EnumYoVariable;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.simulationconstructionset.robotController.RobotController;
 import us.ihmc.simulationconstructionset.robotController.SimpleRobotController;
@@ -13,11 +14,12 @@ public class BuildingPendulumController extends SimpleRobotController
    private double pendulumAngle;
    private double pendulumAngleSwitch;
 
-
+   private final EnumYoVariable<RobotSide> yoActiveSide = new EnumYoVariable<>("ActiveSide", registry, RobotSide.class);
 
    public BuildingPendulumController(BuildingPendulumRobot robot)
    {
       this.robot = robot;
+      activeSide = RobotSide.RIGHT; // figure out which side to start with
    }
 
    RobotSide activeSide;
@@ -30,33 +32,53 @@ public class BuildingPendulumController extends SimpleRobotController
    {
 
       // compute torques, decide which pendulum active ...
-
+     // robot.pendulumJoint1.
       setPendulumAngles();
 
-     boolean atCenter;
-      if(pendulumAngle>pendulumAngleSwitch)
-         atCenter = true;
-      else
-        atCenter = false;
+     boolean atCenter= pendulumAngle>pendulumAngleSwitch;
+//      if(pendulumAngle>pendulumAngleSwitch)
+//         atCenter = true;
+//      else
+//        atCenter = false;
 
      boolean shouldSwitchToLeft = activeSide == RobotSide.RIGHT && atCenter;
+      System.out.println("left: "+shouldSwitchToLeft);
+
      boolean shouldSwitchToRight = activeSide == RobotSide.LEFT && atCenter;
+      System.out.println("right: "+shouldSwitchToRight);
+      double mass = BuildingPendulumRobot.mass;
 
       if (shouldSwitchToLeft)
       {
            robot.getPendulumAngle(RobotSide.RIGHT);
+         double value = robot.pendulumJoint1.getQ().getDoubleValue();
+
 
 //         readRightPendulum;
+
+         double value2 = robot.pendulumJoint1.getQ().getDoubleValue();
+         robot.pendulumJoint2.getQ().set(value+value2);
+
 //         computeLeft;
 //         setLeftPensulum;
+
+
 //         hideRightPensulum;
 //         showLeftPensdulum;
          activeSide = RobotSide.LEFT;
       }
       else if (shouldSwitchToRight)
       {
-        // ...;
+         robot.getPendulumAngle(RobotSide.LEFT);
+
+         //         readRightPendulum;
+         //         computeLeft;
+         //         setLeftPensulum;
+         //         hideRightPensulum;
+         //         showLeftPensdulum;
+         activeSide = RobotSide.RIGHT;
       }
 
+      yoActiveSide.set(activeSide);
    }
 }
