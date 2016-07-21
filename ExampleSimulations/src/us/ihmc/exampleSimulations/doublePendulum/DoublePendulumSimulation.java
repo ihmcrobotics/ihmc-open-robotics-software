5791fcb48dfd1ba2df110f119a6908e190d5d903
Aimee Gerold <agerold@ihmc.us>
2016-07-21 12:28:43 -0500

    Refactored doublePendulum package to resolve compile errors.

--- ExampleSimulations/src/us/ihmc/exampleSimulations/doublePendulum/DoublePendulumSimulation.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/doublePendulum/DoublePendulumSimulation.java
@@ -5,31 +5,29 @@ import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 public class DoublePendulumSimulation
 {
    private SimulationConstructionSet sim;
-
    public DoublePendulumSimulation()
    {
-      DoublePendulumRobot doublePendulum = new DoublePendulumRobot();
-      doublePendulum.setController(new DoublePendulumController(doublePendulum,"doublePendulumController"));
-//      doublePendulum.setController(new ExternalControlServer(doublePendulum, "externalControlServer"));
+//      DoublePendulumRobot doublePendulum = new DoublePendulumRobot[1];
 
+      DoublePendulumRobot doublePendulum = new DoublePendulumRobot("doublePendulum1");
+     // doublePendulum[1] = new DoublePendulumRobot("doublePendulum2");
+      //doublePendulum[1].addYoVariableRegistry();
+     // doublePendulum[1].setLocation(5.0, 00.0, 0.0);
+     // doublePendulum[0].pin1.setLimitStops(3,5,6,10);
+//      doublePendulum[0].pin1.setLimitStops(1,15,15,15);
+
+     doublePendulum.setController(new DoublePendulumController(doublePendulum, "doublePendulumController"));
+    //  doublePendulum[1].setController(new DoublePendulumController(doublePendulum[1],"doublePendulumController"));
+     // sim = new SimulationConstructionSet(doublePendulum);
       sim = new SimulationConstructionSet(doublePendulum);
+      sim.setDT(0.0001, 100);
       sim.setGroundVisible(false);
       sim.setCameraPosition(0, -40.0, 2.0);
-      
-      sim.setSimulateDuration(3.0);
+
       Thread myThread = new Thread(sim);
       myThread.start();
    }
 
-   /*
-    * When your simulation is run, first the main method will be called. It creates a new
-    * DoublePendulumSimulation. In creating a DoublePendulumSimulation, a
-    * DoublePendulumRobot is first created, and then a SimulationConstructionSet object is
-    * created with that robot. A Thread is then created using the SimulationConstructionSet
-    * object. Finally the Thread is started, thereby starting your simulation. This simple template
-    * can be used for creating any simulation using the Simulation Construction Set.
-    */
-   
    public static void main(String[] args)
    {
       new DoublePendulumSimulation();
