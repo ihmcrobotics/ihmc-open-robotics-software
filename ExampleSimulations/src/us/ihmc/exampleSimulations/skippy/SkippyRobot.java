971c5e7ba227bae237ce35cd6cc40c0bd924ad8f
Nathaniel Choe <nchoe@ihmc.us>
2016-07-25 12:21:37 -0500

    Fixed up moments of inertia, rotation axes

--- ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippyRobot.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippyRobot.java
@@ -50,7 +50,7 @@ public class SkippyRobot extends Robot
       // Create joints and assign links. Each joint should be placed L* distance away from its previous joint.
       foot = new UniversalJoint("foot_X", "foot_Y", new Vector3d(0.0, 0.0, 0.0), this, Axis.X, Axis.Y);
       foot.changeOffsetVector(new Vector3d(0.0, 0.0, 0.0)); // initial Cartesian position
-      foot.setInitialState(0.05, 0.0); // initial angle
+      foot.setInitialState(0.05, 0.0, 0.0, 0.0); // initial angle
       Link leg = createLeg();
       foot.setLink(leg);
       this.addRootJoint(foot);
@@ -59,12 +59,15 @@ public class SkippyRobot extends Robot
       hip = new PinJoint("hip", new Vector3d(0.0, 0.0, L1), this, Axis.X);
       Link torso = createTorso();
       hip.setLink(torso);
+      hip.setInitialState(0.5,0.0);
       this.foot.addJoint(hip);
       hip.addGroundContactPoint(hipContact);
 
       shoulder = new PinJoint("shoulder", new Vector3d(0.0, 0.0, L2), this, Axis.Y);
       Link arms = createArms();
       shoulder.setLink(arms);
+      shoulder.setInitialState(0.5,0.0);
+      shoulder.setDamping(0.3);
       this.hip.addJoint(shoulder);
       shoulder.addGroundContactPoint(shoulderContact);
 
@@ -80,7 +83,7 @@ public class SkippyRobot extends Robot
       Link leg = new Link("leg");
       leg.setMass(M1);
       leg.setComOffset(0.0, 0.0, L1 / 2.0);
-      leg.setMomentOfInertia(0.0, Iyy1, 0.0);
+      leg.setMomentOfInertia(Iyy1, Iyy1, 0.0001);
 
       // create a LinkGraphics object to manipulate the visual representation of the link
       Graphics3DObject linkGraphics = new Graphics3DObject();
@@ -97,7 +100,7 @@ public class SkippyRobot extends Robot
       Link torso = new Link("torso");
       torso.setMass(M2);
       torso.setComOffset(0.0, 0.0, L2 / 2.0);
-      torso.setMomentOfInertia(0.0, Iyy2, 0.0);
+      torso.setMomentOfInertia(Iyy2, 0.0001, 0.0001);
 
       Graphics3DObject linkGraphics = new Graphics3DObject();
       linkGraphics.addCylinder(L2, R2, YoAppearance.LightSkyBlue());
@@ -113,7 +116,7 @@ public class SkippyRobot extends Robot
       Link arms = new Link("arms");
       arms.setMass(M3);
       arms.setComOffset(0.0, 0.0, L3 / 2.0);
-      arms.setMomentOfInertia(Iyy3, 0.0, 0.0);
+      arms.setMomentOfInertia(0.0001, Iyy3, 0.0001);
 
       Graphics3DObject linkGraphics = new Graphics3DObject();
       linkGraphics.rotate(Math.toRadians(90), Axis.Y);
