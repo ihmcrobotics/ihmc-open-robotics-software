2f91282c072fcdfa50a642bc7d3e4ed52edb75d9
Sean Mason <Sean.A.Mason@disney.com>
2016-08-04 15:37:54 -0700

    Removed fstand controller failure mode.

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/controller/force/states/QuadrupedDcmBasedStandController.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/controller/force/states/QuadrupedDcmBasedStandController.java
@@ -70,7 +70,6 @@ public class QuadrupedDcmBasedStandController implements QuadrupedController
    private final QuadrupedTaskSpaceController.Commands taskSpaceControllerCommands;
    private final QuadrupedTaskSpaceController.Settings taskSpaceControllerSettings;
    private final QuadrupedTaskSpaceController taskSpaceController;
-   private final QuadrupedSoleForceEstimator soleForceEstimator;
 
    // planning
    private final GroundPlaneEstimator groundPlaneEstimator;
@@ -107,7 +106,6 @@ public class QuadrupedDcmBasedStandController implements QuadrupedController
       taskSpaceControllerCommands = new QuadrupedTaskSpaceController.Commands();
       taskSpaceControllerSettings = new QuadrupedTaskSpaceController.Settings();
       taskSpaceController = controllerToolbox.getTaskSpaceController();
-      soleForceEstimator = controllerToolbox.getSoleForceEstimator();
 
       // planning
       groundPlaneEstimator = controllerToolbox.getGroundPlaneEstimator();
@@ -147,9 +145,6 @@ public class QuadrupedDcmBasedStandController implements QuadrupedController
 
       // update ground plane estimate
       groundPlaneEstimator.compute(taskSpaceEstimates.getSolePosition());
-
-      // update sole force estimate
-      soleForceEstimator.compute();
    }
 
    private void updateSetpoints()
@@ -195,7 +190,7 @@ public class QuadrupedDcmBasedStandController implements QuadrupedController
       updateGains();
       updateEstimates();
       updateSetpoints();
-      return detectFall() ? null : ControllerEvent.FAIL;
+      return null;
    }
 
    @Override
@@ -212,7 +207,6 @@ public class QuadrupedDcmBasedStandController implements QuadrupedController
       comPositionController.reset();
       bodyOrientationControllerSetpoints.initialize(taskSpaceEstimates);
       bodyOrientationController.reset();
-      soleForceEstimator.reset();
 
       // initialize task space controller
       taskSpaceControllerSettings.initialize();
@@ -229,19 +223,5 @@ public class QuadrupedDcmBasedStandController implements QuadrupedController
    @Override
    public void onExit()
    {
-   }
-
-   private boolean detectFall()
-   {
-      int numContacts = 0;
-      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-         if (soleForceEstimator.getSoleContactForce(robotQuadrant).getZ() > contactPressureLowerLimitParameter.get())
-         {
-            numContacts += 1;
-         }
-      }
-      if (comPositionController.)
-         return (numContacts > 2);
    }
 }
