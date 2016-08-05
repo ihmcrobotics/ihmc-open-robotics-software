f9669d1ace7bdedd0e070b4547ff21e384c14eec
Stephen Tamba <stamba@ihmc.us>
2016-08-05 08:16:36 -0500

    I made it look nice

--- ExampleSimulations/src/us/ihmc/exampleSimulations/RobotArm/RobotArm.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/RobotArm/RobotArm.java
@@ -48,7 +48,7 @@ public class RobotArm extends Robot
             stick1.setLink(link); // associate link1 with the joint pin1
             circle.addJoint(stick1);
 
-      PinJoint stick2 = new PinJoint("stick2", new Vector3d(0.0, 0.03, 1.5), this, Axis.Y);
+      PinJoint stick2 = new PinJoint("stick2", new Vector3d(0.0, 0.0, 1.5), this, Axis.Y);
             stick2.setInitialState(0.05, 0.0);
 
             stick2.setDamping(damping);
@@ -63,7 +63,7 @@ public class RobotArm extends Robot
             circle2.setLink(link2);
             stick2.addJoint(circle2);
 
-      PinJoint stick3 = new PinJoint("stick3", new Vector3d(0.2, 0.0, 0.0), this, Axis.Y);
+      PinJoint stick3 = new PinJoint("stick3", new Vector3d(0.0, 0.0, 0.0), this, Axis.Y);
              stick2.setInitialState(0.05, 0.0);
              stick2.setDamping(damping);
            // stick2.setLimitStops(-1.3,2.0,0,0);
@@ -71,7 +71,7 @@ public class RobotArm extends Robot
             stick3.setLink(link4); // associate link1 with the joint pin1
            circle2.addJoint(stick3);
 
-      PinJoint circle3 = new PinJoint("circle3", new Vector3d(0.2, 0.0, 0.0), this, Axis.X);
+      PinJoint circle3 = new PinJoint("circle3", new Vector3d(0.5, 0.0, 0.0), this, Axis.X);
             Link link5 = linkCircle3();
             //circle.setLimitStops(-1.7,2.1,0,0);
             circle3.setDamping(damping);
@@ -84,58 +84,6 @@ public class RobotArm extends Robot
             stick4.setLink(link6);
             circle3.addJoint(stick4);
 
-
-
-
-  //////////////////////////////////////////////////////
-
-//
-//      PinJoint stick1 = new PinJoint("stick1", new Vector3d(0.2, 0.0, 0.5), this, Axis.Y);
-//      stick1.setInitialState(1.5, 0.0);
-//      stick1.setDamping(damping);
-//      circle.setLimitStops(-1.15,2.35,0,0);
-//      Link link = link1();
-//      stick1.setLink(link); // associate link1 with the joint pin1
-//      circle.addJoint(stick1);
-//      /*
-//       *  The second joint is initiated with the offset vector (0.0,0.0,L1) since
-//       *  it should be placed a distance of L1 in the Z direction from the previous joint.
-//       */
-//      PinJoint circle2 = new PinJoint("circle2", new Vector3d(0.0, 0.03, 1.5), this, Axis.X);
-//      circle2.setDamping(damping);
-//      Link link2 = linkCircle2();
-//      circle2.setLink(link2);
-//      stick1.addJoint(circle2);
-////
-//      PinJoint stick2 = new PinJoint("stick2", new Vector3d(0.0, 0.0, 0.0), this, Axis.Y);
-//      stick2.setInitialState(0.05, 0.0);
-//
-//      stick2.setDamping(damping);
-//     circle.setLimitStops(-1.3,2.0,0,0);
-//      Link link3 = link2();
-//      stick2.setLink(link3); // associate link1 with the joint pin1
-//      circle2.addJoint(stick2);
-//
-//      PinJoint stick3 = new PinJoint("stick3", new Vector3d(1.5, 0.0, 0.0), this, Axis.Y);
-//       stick2.setInitialState(0.05, 0.0);
-//       stick2.setDamping(damping);
-//     // stick2.setLimitStops(-1.3,2.0,0,0);
-//      Link link4 = link3();
-//      stick3.setLink(link4); // associate link1 with the joint pin1
-//     stick2.addJoint(stick3);
-//
-//      PinJoint circle3 = new PinJoint("circle3", new Vector3d(0.2, 0.0, 0.0), this, Axis.X);
-//      Link link5 = linkCircle3();
-//      //circle.setLimitStops(-1.7,2.1,0,0);
-//      circle3.setDamping(damping);
-//      circle3.setLink(link5);
-//      stick3.addJoint(circle3);
-////
-//      PinJoint stick4 = new PinJoint("stick4", new Vector3d(0.0, 0.0, 0.0), this, Axis.Z);
-//      Link link6 = link4();
-//      stick4.setDamping(damping);
-//      stick4.setLink(link6);
-//      circle3.addJoint(stick4);
 
 
       this.addRootJoint(circle);
@@ -188,7 +136,7 @@ public class RobotArm extends Robot
       Link ret = new Link("link3");
 
       double m = 150.0;
-      double h = 0.2;
+      double h = 0.5;
       double r = 0.05;
 
       ret.setMass(m);
@@ -259,25 +207,6 @@ public class RobotArm extends Robot
       linkGraphics.addCylinder(h, r, YoAppearance.White());
       linkCircle2.setLinkGraphics(linkGraphics);
       return linkCircle2;
-
-//
-//      double m = 1.0;
-//      double h = 0.2;
-//      double r = 0.2;
-//
-//
-//
-//      Link linkCircle2 = new Link("linkCircle3");
-//      linkCircle2.setMass(m);
-//      linkCircle2.setComOffset(0.0, 0.0, 0.0);
-//      linkCircle2.setMomentOfInertia(m*r*r/2.0, m*(3*r*r + h*h)/12.0, m*(3*r*r + h*h)/12.0);
-//      Graphics3DObject linkGraphics = new Graphics3DObject();
-//
-//      linkGraphics.rotate(Math.PI/2.0, Axis.Y);
-//      linkGraphics.translate(0.0, 0.0, -0.1);
-//      linkGraphics.addCylinder(h, r, YoAppearance.White());
-//      linkCircle2.setLinkGraphics(linkGraphics);
-//      return linkCircle2;
    }
    private Link linkCircle3()
    {
