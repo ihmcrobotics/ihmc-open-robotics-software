43764db01d9c24c0ac927c7616d1e7b347249544
Aimee Gerold <agerold@ihmc.us>
2016-08-08 11:17:46 -0500

    Cleaning up code & refactoring

--- ExampleSimulations/src/us/ihmc/exampleSimulations/RobotArm/RobotArmController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/RobotArm/RobotArmController.java
@@ -8,26 +8,26 @@ import us.ihmc.simulationconstructionset.robotController.RobotController;
 public class RobotArmController implements RobotController
 {
 
-   public static String variableName = "Turning1";
-   public static String variableName2 = "longstick1";
-   public static String variableName3 = "longstick2";
-   public static String variableName4 = "Turning2";
-   public static String variableName5 = "longstick3";
-   public static String variableName6 = "Turning3";
-   public static String variableName7 = "longstick4";
+   public static String variableName = "axis1";
+   public static String variableName2 = "axis2";
+   public static String variableName3 = "axis3";
+   public static String variableName4 = "axis4";
+   public static String variableName5 = "axis5";
+   public static String variableName6 = "axis6";
+   public static String variableName7 = "axis7";
 
    // tau_* is torque, q_* is position, qd_* is velocity for joint *
-   private DoubleYoVariable q_circle, q_stick1, q_circle2, q_stick2, q_stick3, q_circle3, q_stick4;
+   private DoubleYoVariable q_axis1, q_axis2, q_axis4, q_axis3, q_axis5, q_axis6, q_axis7;
    private DoubleYoVariable k1, k2, k3, k4; // these are the controller gain parameters
    private final YoVariableRegistry registry = new YoVariableRegistry("RobotArmController");
-   private final DoubleYoVariable circle1 = new DoubleYoVariable(variableName, registry);
-   private final DoubleYoVariable stick1 = new DoubleYoVariable(variableName2, registry);
 
-   private final DoubleYoVariable stick2 = new DoubleYoVariable(variableName3, registry);
-   private final DoubleYoVariable circle2 = new DoubleYoVariable(variableName4, registry);
-   private final DoubleYoVariable stick3 = new DoubleYoVariable(variableName5, registry);
-   private final DoubleYoVariable circle3 = new DoubleYoVariable(variableName6, registry);
-   private final DoubleYoVariable stick4 = new DoubleYoVariable(variableName7, registry);
+   private final DoubleYoVariable axis1 = new DoubleYoVariable(variableName, registry);
+   private final DoubleYoVariable axis2 = new DoubleYoVariable(variableName2, registry);
+   private final DoubleYoVariable axis3 = new DoubleYoVariable(variableName3, registry);
+   private final DoubleYoVariable axis4 = new DoubleYoVariable(variableName4, registry);
+   private final DoubleYoVariable axis5 = new DoubleYoVariable(variableName5, registry);
+   private final DoubleYoVariable axis6 = new DoubleYoVariable(variableName6, registry);
+   private final DoubleYoVariable axis7 = new DoubleYoVariable(variableName7, registry);
    private String robotName;
 
    public RobotArmController(RobotArm robot, String robotName)
@@ -35,28 +35,24 @@ public class RobotArmController implements RobotController
       this.robotName = robotName;
 
       // get variable references from the robot
-      q_circle = (DoubleYoVariable) robot.getVariable("q_circle");
-      q_stick1 = (DoubleYoVariable) robot.getVariable("q_stick1");
-      q_circle2 = (DoubleYoVariable) robot.getVariable("q_circle2");
-      q_stick2 = (DoubleYoVariable) robot.getVariable("q_stick2");
-      q_circle3 = (DoubleYoVariable) robot.getVariable("q_circle3");
-      q_stick3 = (DoubleYoVariable) robot.getVariable("q_stick3");
-//      q_circle4 = (DoubleYoVariable) rob.getVariable("q_circle4");
-      q_stick4 = (DoubleYoVariable) robot.getVariable("q_stick4");
-
+      q_axis1 = (DoubleYoVariable) robot.getVariable("q_axis1");
+      q_axis2 = (DoubleYoVariable) robot.getVariable("q_axis2");
+      q_axis3 = (DoubleYoVariable) robot.getVariable("q_axis3");
+      q_axis4 = (DoubleYoVariable) robot.getVariable("q_axis4");
+      q_axis5 = (DoubleYoVariable) robot.getVariable("q_axis5");
+      q_axis6 = (DoubleYoVariable) robot.getVariable("q_axis6");
+      q_axis7 = (DoubleYoVariable) robot.getVariable("q_axis7");
    }
 
    public void doControl()
    {
-      q_circle.set(circle1.getDoubleValue());
-      //  System.out.println(circle1.getDoubleValue());
-      q_stick1.set(stick1.getDoubleValue());
-      q_circle2.set(circle2.getDoubleValue());
-      q_stick2.set(stick2.getDoubleValue());
-      q_circle3.set(circle3.getDoubleValue());
-      q_stick3.set(stick3.getDoubleValue());
-//      q_circle4.set(circle4.getDoubleValue());
-      q_stick4.set(stick4.getDoubleValue());
+      q_axis1.set(axis1.getDoubleValue());
+      q_axis2.set(axis2.getDoubleValue());
+      q_axis3.set(axis3.getDoubleValue());
+      q_axis4.set(axis4.getDoubleValue());
+      q_axis5.set(axis5.getDoubleValue());
+      q_axis6.set(axis6.getDoubleValue());
+      q_axis7.set(axis7.getDoubleValue());
       // tau_joint1.set(0.0); // free bearing
       // set the torque at the controlled second joint
       // tau_joint2.set(-k1.getDoubleValue() * q_joint1.getDoubleValue() - k2.getDoubleValue() * q_joint2.getDoubleValue() - k3.getDoubleValue() * qd_joint1.getDoubleValue() - k4.getDoubleValue() * qd_joint2.getDoubleValue());
