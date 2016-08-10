07f0d5c976e1e239b8cc343040f2333a6f735ba0
Nathaniel Choe <nchoe@ihmc.us>
2016-08-10 18:09:05 -0500

    changed constants - quicker stabilization

--- ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippyController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippyController.java
@@ -76,9 +76,9 @@ public class SkippyController implements RobotController
       k4.set(-50.0); //-15);
 
       k5 = new DoubleYoVariable("k5", registry);
-      k5.set(-2385.0);
+      k5.set(-1900);
       k6 = new DoubleYoVariable("k6", registry);
-      k6.set(-412.0);
+      k6.set(-500.0);
       k7 = new DoubleYoVariable("k7", registry);
       k7.set(-86.0);
       k8 = new DoubleYoVariable("k8", registry);
