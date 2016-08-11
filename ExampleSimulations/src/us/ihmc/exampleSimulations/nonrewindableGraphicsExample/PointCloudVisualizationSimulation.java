794c117f252919d758ac59c713c2e21e435b7440
Nathan Mertins <nmertins@ihmc.us>
2016-08-11 11:56:31 -0500

    moved visualizer to SCS module and played more with adding/removing graphics

--- ExampleSimulations/src/us/ihmc/exampleSimulations/nonrewindableGraphicsExample/PointCloudVisualizationSimulation.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/nonrewindableGraphicsExample/PointCloudVisualizationSimulation.java
@@ -1,10 +1,10 @@
 package us.ihmc.exampleSimulations.nonrewindableGraphicsExample;
 
-import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
-import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
-import us.ihmc.graphics3DAdapter.structure.Graphics3DNode;
-import us.ihmc.graphics3DAdapter.structure.Graphics3DNodeType;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.util.visualizers.SCSPointCloudVisualizer;
+
+import javax.vecmath.Point3d;
+import java.util.ArrayList;
 
 public class PointCloudVisualizationSimulation
 {
@@ -13,12 +13,42 @@ public class PointCloudVisualizationSimulation
       PointCloudRobot pointCloudRobot = new PointCloudRobot("PointCloudRobot");
       SimulationConstructionSet scs = new SimulationConstructionSet(pointCloudRobot);
 
-      Graphics3DObject testGraphicObject = new Graphics3DObject();
-      testGraphicObject.addCube(0.1, 0.1, 0.1, YoAppearance.Aqua());
-      Graphics3DNode node = scs.addStaticLinkGraphics(testGraphicObject, Graphics3DNodeType.VISUALIZATION);
-      node.translate(1.0, 0, 0);
+      SCSPointCloudVisualizer pointCloudVisualizer = new SCSPointCloudVisualizer(scs);
+
+      ArrayList<Point3d> points = new ArrayList<>();
+      for(double i = 0; i < 1.0; i += 0.1)
+      {
+         for(double j = 0; j < 1.0; j += 0.1)
+         {
+            points.add(new Point3d(1.0, i, j));
+         }
+      }
+
+      pointCloudVisualizer.addPoints(points);
 
       scs.startOnAThread();
+
+      try
+      {
+         Thread.sleep(5000);
+      }
+      catch(InterruptedException e)
+      {
+         e.printStackTrace();
+      }
+
+      pointCloudVisualizer.clear();
+
+      try
+      {
+         Thread.sleep(5000);
+      }
+      catch(InterruptedException e)
+      {
+         e.printStackTrace();
+      }
+
+      pointCloudVisualizer.addPoints(points);
    }
 
    public static void main(String[] args)
