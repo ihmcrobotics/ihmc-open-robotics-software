a5424747e4bb5c4713428d54f3d3d850f70ee20a
Nathan Mertins <nmertins@ihmc.us>
2016-08-11 11:56:31 -0500

    moved visualizer to SCS module and played more with adding/removing graphics

---
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/util/visualizers/SCSPointCloudVisualizer.java
@@ -0,0 +1,60 @@
+package us.ihmc.simulationconstructionset.util.visualizers;
+
+import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
+import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
+import us.ihmc.graphics3DAdapter.structure.Graphics3DNode;
+import us.ihmc.graphics3DAdapter.structure.Graphics3DNodeType;
+import us.ihmc.robotics.dataStructures.listener.RewoundListener;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+
+import javax.vecmath.Point3d;
+import java.util.ArrayList;
+
+public class SCSPointCloudVisualizer implements RewoundListener
+{
+   private final double VOXEL_SIZE = 0.01;
+   private final ArrayList<Graphics3DNode> nodes = new ArrayList<>();
+
+   private final SimulationConstructionSet scs;
+
+   private boolean disableGraphics = false;
+
+   public SCSPointCloudVisualizer(SimulationConstructionSet scs)
+   {
+      this.scs = scs;
+
+   }
+
+   public void addPoints(ArrayList<Point3d> points)
+   {
+      if(!disableGraphics)
+      {
+         for(Point3d point : points)
+         {
+            Graphics3DObject testCubeGraphicObject = new Graphics3DObject();
+            testCubeGraphicObject.addCube(VOXEL_SIZE, VOXEL_SIZE, VOXEL_SIZE, YoAppearance.Aqua());
+            Graphics3DNode cubeNode = scs.addStaticLinkGraphics(testCubeGraphicObject, Graphics3DNodeType.VISUALIZATION);
+            cubeNode.translate(point.getX(), point.getY(), point.getZ());
+
+            nodes.add(cubeNode);
+         }
+      }
+   }
+
+   public void clear()
+   {
+      for(Graphics3DNode node : nodes)
+      {
+         scs.removeGraphics3dNode(node);
+      }
+
+      nodes.clear();
+   }
+
+   @Override
+   public void wasRewound()
+   {
+      disableGraphics = true;
+      clear();
+   }
+}
