266a4d92bec242447026ff80ed80baa67ebd0021
Sean Mason <Sean.A.Mason@disney.com>
2016-08-11 15:20:19 -0700

    Added DoNothing request from freeze and soleWaypoint.

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/controller/force/QuadrupedForceControllerRequestedEvent.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/controller/force/QuadrupedForceControllerRequestedEvent.java
@@ -2,6 +2,7 @@ package us.ihmc.quadrupedRobotics.controller.force;
 
 public enum QuadrupedForceControllerRequestedEvent
 {
+   REQUEST_DO_NOTHING,
    REQUEST_STAND_PREP,
    REQUEST_FREEZE,
    REQUEST_STAND,
