73dec87f80a26f1372ab2bc20c2bdb9b27f752a2
Operator Computer <OperatorComputer@unknown>
2016-08-12 17:39:19 -0500

    NRI-127: Lots of refactoring to get centerOfMassVelocity set.
    Communicate from estimator to controller comVelocity.
    Jerry and Georg.

--- Valkyrie/src/us/ihmc/valkyrie/parameters/ValkyrieStateEstimatorParameters.java
+++ Valkyrie/src/us/ihmc/valkyrie/parameters/ValkyrieStateEstimatorParameters.java
@@ -389,4 +389,16 @@ public class ValkyrieStateEstimatorParameters implements StateEstimatorParameter
    {
       return false;
    }
+   
+   @Override
+   public double getCenterOfMassVelocityFusingFrequency()
+   {
+      return 0.4261;
+   }
+
+   @Override
+   public boolean useGroundReactionForcesToComputeCenterOfMassVelocity()
+   {
+      return false;
+   }
 }
