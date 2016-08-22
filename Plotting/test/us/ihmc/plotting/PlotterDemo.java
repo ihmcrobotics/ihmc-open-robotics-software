04e971199333ae58cd4a84bbf1c334b2621cfae0
Duncan Calvert <dcalvert@ihmc.us>
2016-08-21 19:03:02 -0500

    BEAST-351: Remove duplicate plotter demo and unite tests. Generate test suites.

--- Plotting/test/us/ihmc/plotting/PlotterDemo.java
+++ Plotting/test/us/ihmc/plotting/PlotterDemo.java
@@ -12,6 +12,7 @@ public class PlotterDemo
       plotter.setPreferredSize(800, 600);
       
       plotter.setViewRange(10.0);
+      plotter.setViewRange(1.0);
       plotter.setShowLabels(true);
       
       plotter.addArtifact(new LineArtifact("01", new Point2d(0, 0), new Point2d(1, 1)));
@@ -20,6 +21,7 @@ public class PlotterDemo
       
       plotter.showInNewWindow();
       
+//      plotter.setScale(40.0, 20.0);
       plotter.setFocusPointX(2.5);
       plotter.setFocusPointY(-3.0);
    }
