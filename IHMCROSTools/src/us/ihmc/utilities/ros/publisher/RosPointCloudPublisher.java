c774040bd8c96856624ed775f07b56b87b1bfe55
Nathan Mertins <nmertins@ihmc.us>
2016-08-29 09:57:56 -0700

    created general JME visualizer for ROS point clouds

--- IHMCROSTools/src/us/ihmc/utilities/ros/publisher/RosPointCloudPublisher.java
+++ IHMCROSTools/src/us/ihmc/utilities/ros/publisher/RosPointCloudPublisher.java
@@ -84,4 +84,34 @@ public class RosPointCloudPublisher extends RosTopicPublisher<PointCloud2>
       
    }
 
+   public void publish(Point3d[] points, Color3f color, String frameId)
+   {
+      PointCloud2 message = getMessage();
+      message.getHeader().setFrameId(frameId);
+      message.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
+      message.setHeight(1);
+      message.setWidth(points.length);
+      message.setPointStep(pointType.getPointStep());
+      int dataLength = pointType.getPointStep() * points.length;
+      message.setRowStep(dataLength);
+      message.setIsBigendian(false);
+      message.setIsDense(true);
+      message.setFields(pointType.getPointField());
+
+      ChannelBuffer buffer = new LittleEndianHeapChannelBuffer(dataLength);
+      for(int i=0;i<points.length;i++)
+      {
+         buffer.writeFloat((float)points[i].getX());
+         buffer.writeFloat((float)points[i].getY());
+         buffer.writeFloat((float)points[i].getZ());
+         buffer.writeByte((int)color.getZ());
+         buffer.writeByte((int)color.getY());
+         buffer.writeByte((int)color.getX());
+         buffer.writeByte(0); //dummy;
+      }
+      message.setData(buffer);
+
+      publish(message);
+   }
+
 }
