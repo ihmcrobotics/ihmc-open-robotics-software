1622e6fa2fa5c24aa5166e5de8873eadf87ee06a
Nathan Mertins <nmertins@ihmc.us>
2016-08-29 15:06:15 -0700

    Consolidated classes with duplicated functionality

--- IHMCPerception/src/us/ihmc/ihmcPerception/RosPointCloudVisualizer.java
+++ IHMCPerception/src/us/ihmc/ihmcPerception/RosPointCloudVisualizer.java
@@ -8,35 +8,32 @@ import us.ihmc.ihmcPerception.depthData.PointCloudDataReceiverInterface;
 import us.ihmc.ihmcPerception.depthData.PointCloudSource;
 import us.ihmc.ihmcPerception.depthData.RosPointCloudReceiver;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
-import us.ihmc.robotics.geometry.RotationTools;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.utilities.ros.RosMainNode;
 
 import javax.vecmath.Point3d;
 import javax.vecmath.Point3f;
-import javax.vecmath.Quat4d;
-import javax.vecmath.Vector3d;
-import java.net.URI;
 import java.util.ArrayList;
 
 public class RosPointCloudVisualizer
 {
    private final JMELidarSpriteGenerator lidarSpriteGenerator;
-
-   private final RigidBodyTransform magicTransform = new RigidBodyTransform();
-   {
-      // This is to correct the differences between default reference frames in ROS and JME
-      Quat4d magicRotation = new Quat4d();
-      RotationTools.convertYawPitchRollToQuaternion(-Math.PI/2.0, 0.0, -Math.PI/2.0, magicRotation);
-      magicTransform.set(magicRotation, new Vector3d());
-   }
+   private final RigidBodyTransform sensorTransform;
+   private final RosPointCloudReceiver pointCloudReceiver;
+   private final RosMainNode rosMainNode;
 
    public RosPointCloudVisualizer(String topicName, RosMainNode rosMainNode)
    {
       this(topicName, rosMainNode, null);
    }
 
+
    public RosPointCloudVisualizer(String topicName, RosMainNode rosMainNode, JMERenderer renderer)
+   {
+      this(topicName, rosMainNode, renderer, null);
+   }
+
+   public RosPointCloudVisualizer(String topicName, RosMainNode rosMainNode, JMERenderer renderer, RigidBodyTransform sensorTransform)
    {
       if(renderer == null)
       {
@@ -46,12 +43,21 @@ public class RosPointCloudVisualizer
          renderer = pointCloudVisualizer.getGraphics3DAdapter().getRenderer();
       }
 
+      this.rosMainNode = rosMainNode;
+      this.sensorTransform = sensorTransform;
+
       lidarSpriteGenerator = new JMELidarSpriteGenerator(renderer);
       renderer.getZUpNode().attachChild(lidarSpriteGenerator);
       renderer.registerUpdatable(lidarSpriteGenerator);
 
-      new RosPointCloudReceiver(topicName, rosMainNode, ReferenceFrame.getWorldFrame(), ReferenceFrame.getWorldFrame(), new PointCloudReceiver(),
-            PointCloudSource.NEARSCAN);
+      pointCloudReceiver = new RosPointCloudReceiver(topicName, rosMainNode, ReferenceFrame.getWorldFrame(),
+            ReferenceFrame.getWorldFrame(), new PointCloudReceiver(), PointCloudSource.NEARSCAN);
+   }
+
+   public void detach()
+   {
+      lidarSpriteGenerator.clear();
+      rosMainNode.removeSubscriber(pointCloudReceiver);
    }
 
    class PointCloudReceiver implements PointCloudDataReceiverInterface
@@ -64,7 +70,8 @@ public class RosPointCloudVisualizer
          for(int i = 0; i < points.size(); i++)
          {
             Point3d point = points.get(i);
-            magicTransform.transform(point);
+            if(sensorTransform != null)
+               sensorTransform.transform(point);
             point3fs[i] = new Point3f(point);
          }
 
