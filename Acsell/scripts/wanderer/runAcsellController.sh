8ebce6d655b1c48061b892d45ed7a8a9022484b1
Doug Stephen <dstephen@ihmc.us>
2016-09-02 16:45:40 -0500

    Fix bad VM flag in launchers and build scripts (-verbosegc should be -verbose:gc)

--- Acsell/scripts/wanderer/runAcsellController.sh
+++ Acsell/scripts/wanderer/runAcsellController.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -XX:+UseSerialGC -Xmx14g -Xms14g -XX:NewSize=12g -XX:MaxNewSize=12g -XX:CompileThreshold=1000 -verbosegc -Djava.library.path=lib/ -cp AcsellController.jar $@
+java -XX:+UseSerialGC -Xmx14g -Xms14g -XX:NewSize=12g -XX:MaxNewSize=12g -XX:CompileThreshold=1000 -verbose:gc -Djava.library.path=lib/ -cp AcsellController.jar $@
