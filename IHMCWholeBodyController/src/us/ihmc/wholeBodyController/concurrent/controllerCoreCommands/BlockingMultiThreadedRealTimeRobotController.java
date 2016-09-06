c5cf407ca64d4ceca379e10430e3716abbc47639
Doug Stephen <dstephen@ihmc.us>
2016-09-06 12:06:53 -0500

    ROSI-488: Stubbing out the blocking version of the real-time thread stuff for Gazebo.

---
+++ IHMCWholeBodyController/src/us/ihmc/wholeBodyController/concurrent/controllerCoreCommands/BlockingMultiThreadedRealTimeRobotController.java
@@ -0,0 +1,44 @@
+package us.ihmc.wholeBodyController.concurrent.controllerCoreCommands;
+
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import us.ihmc.affinity.Processor;
+import us.ihmc.realtime.PriorityParameters;
+import us.ihmc.realtime.RealtimeThread;
+import us.ihmc.simulationconstructionset.robotController.MultiThreadedRobotControlElement;
+
+import java.util.ArrayList;
+
+/**
+ * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
+ */
+public class BlockingMultiThreadedRealTimeRobotController
+{
+   private final MultiThreadedRobotControlElement sensorReader;
+   private final ArrayList<ImmutableTriple<MultiThreadedRobotControlElement, PriorityParameters, Processor>> robotControllers = new ArrayList<>();
+   //   private final MonotonicTime triggerTime = new MonotonicTime();
+   //   private final MonotonicTime monotonicTime = new MonotonicTime();
+
+   public BlockingMultiThreadedRealTimeRobotController(MultiThreadedRobotControlElement sensorReader)
+   {
+      this.sensorReader = sensorReader;
+   }
+
+   public void addController(MultiThreadedRobotControlElement robotController, PriorityParameters priorityParameters, Processor processor)
+   {
+      robotControllers
+            .add(new ImmutableTriple<MultiThreadedRobotControlElement, PriorityParameters, Processor>(robotController, priorityParameters, processor));
+   }
+
+   public void read()
+   {
+      long timestamp = RealtimeThread.getCurrentMonotonicClockTime();
+      sensorReader.read(timestamp);
+      sensorReader.run();
+      sensorReader.write(RealtimeThread.getCurrentMonotonicClockTime());
+   }
+
+   public void start()
+   {
+      //TODO this
+   }
+}
