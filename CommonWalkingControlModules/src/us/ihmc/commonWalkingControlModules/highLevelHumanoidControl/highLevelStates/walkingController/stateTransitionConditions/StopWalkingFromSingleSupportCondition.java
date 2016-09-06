804946a14a11ae0c755d5fd73226a703798273e1
Doug Stephen <dstephen@ihmc.us>
2016-09-06 18:20:13 -0500

    Restored StopWalkingFromSingleSupportCondition, got deleted by mistake.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/walkingController/stateTransitionConditions/StopWalkingFromSingleSupportCondition.java
@@ -0,0 +1,26 @@
+package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.walkingController.stateTransitionConditions;
+
+import us.ihmc.commonWalkingControlModules.desiredFootStep.WalkingMessageHandler;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.walkingController.states.SingleSupportState;
+import us.ihmc.robotics.stateMachines.StateTransitionCondition;
+
+public class StopWalkingFromSingleSupportCondition implements StateTransitionCondition
+{
+   private final SingleSupportState singleSupportState;
+   private final WalkingMessageHandler walkingMessageHandler;
+
+   public StopWalkingFromSingleSupportCondition(SingleSupportState singleSupportState, WalkingMessageHandler walkingMessageHandler)
+   {
+      this.singleSupportState = singleSupportState;
+      this.walkingMessageHandler = walkingMessageHandler;
+   }
+
+   @Override
+   public boolean checkCondition()
+   {
+      if (!singleSupportState.isDone())
+         return false;
+
+      return !walkingMessageHandler.hasUpcomingFootsteps();
+   }
+}
