a43049c9eb7ad9d259560abc1533bdfb585522d8
Mike Hopkins <michael.a.hopkins@disney.com>
2016-09-05 18:20:50 -0700

    Add TimeInterval tools and tests.

---
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/util/TimeIntervalTools.java
@@ -0,0 +1,115 @@
+package us.ihmc.quadrupedRobotics.util;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class TimeIntervalTools
+{
+   static public void sortByStartTime(PreallocatedList<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      PreallocatedListSorter.sort((PreallocatedList<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByStartTime);
+   }
+
+   static public void sortByReverseStartTime(PreallocatedList<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      PreallocatedListSorter.sort((PreallocatedList<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByStartTime.reversed());
+   }
+
+   static public void sortByEndTime(PreallocatedList<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      PreallocatedListSorter.sort((PreallocatedList<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByEndTime);
+   }
+
+   static public void sortByReverseEndTime(PreallocatedList<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      PreallocatedListSorter.sort((PreallocatedList<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByEndTime.reversed());
+   }
+
+   static public void sortByStartTime(List<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      ListSorter.sort((List<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByStartTime);
+   }
+
+   static public void sortByReverseStartTime(List<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      ListSorter.sort((List<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByStartTime.reversed());
+   }
+
+   static public void sortByEndTime(List<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      ListSorter.sort((List<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByEndTime);
+   }
+
+   static public void sortByReverseEndTime(List<? extends TimeIntervalProvider> timeIntervalProviders)
+   {
+      ListSorter.sort((List<TimeIntervalProvider>)timeIntervalProviders, compareTimeIntervalProvidersByEndTime.reversed());
+   }
+
+   static public void removeAllStartingBeforeTime(List<? extends TimeIntervalProvider> timeIntervalProviders, double time)
+   {
+      for (int i = 0; i < timeIntervalProviders.size(); i++)
+      {
+         if (timeIntervalProviders.get(i).getTimeInterval().getStartTime() < time)
+         {
+            timeIntervalProviders.remove(i);
+            i--;
+         }
+      }
+   }
+
+   static public void removeAllStartingAfterTime(List<? extends TimeIntervalProvider> timeIntervalProviders, double time)
+   {
+      for (int i = timeIntervalProviders.size() - 1; i >= 0; i--)
+      {
+         if (timeIntervalProviders.get(i).getTimeInterval().getStartTime() > time)
+         {
+            timeIntervalProviders.remove(i);
+         }
+      }
+   }
+
+   static public void removeAllEndingBeforeTime(List<? extends TimeIntervalProvider> timeIntervalProviders, double time)
+   {
+      for (int i = 0; i < timeIntervalProviders.size(); i++)
+      {
+         if (timeIntervalProviders.get(i).getTimeInterval().getEndTime() < time)
+         {
+            timeIntervalProviders.remove(i);
+            i--;
+         }
+      }
+   }
+
+   static public void removeAllEndingAfterTime(List<? extends TimeIntervalProvider> timeIntervalProviders, double time)
+   {
+      for (int i = timeIntervalProviders.size() - 1; i >= 0; i--)
+      {
+         if (timeIntervalProviders.get(i).getTimeInterval().getEndTime() > time)
+         {
+            timeIntervalProviders.remove(i);
+         }
+      }
+   }
+
+   static private Comparator<TimeIntervalProvider> compareTimeIntervalProvidersByStartTime = new Comparator<TimeIntervalProvider>()
+   {
+      @Override
+      public int compare(TimeIntervalProvider a, TimeIntervalProvider b)
+      {
+         double startTimeA = a.getTimeInterval().getStartTime();
+         double startTimeB = b.getTimeInterval().getStartTime();
+         return Double.compare(startTimeA, startTimeB);
+      }
+   };
+
+   static private Comparator<TimeIntervalProvider> compareTimeIntervalProvidersByEndTime = new Comparator<TimeIntervalProvider>()
+   {
+      @Override
+      public int compare(TimeIntervalProvider a, TimeIntervalProvider b)
+      {
+         double endTimeA = a.getTimeInterval().getEndTime();
+         double endTimeB = b.getTimeInterval().getEndTime();
+         return Double.compare(endTimeA, endTimeB);
+      }
+   };
+}
