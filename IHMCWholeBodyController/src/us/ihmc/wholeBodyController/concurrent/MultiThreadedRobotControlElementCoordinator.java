87ff4ef0ffc999aeed72c8c4ba054054b0a56783
Doug Stephen <dstephen@ihmc.us>
2016-09-07 13:19:53 -0500

    ROSI-488: Created a small interface to allow for generalizing the control element coordinator.
    Hopefully this'll make the branching logic in the controller factory suck less.

---
+++ IHMCWholeBodyController/src/us/ihmc/wholeBodyController/concurrent/MultiThreadedRobotControlElementCoordinator.java
@@ -0,0 +1,11 @@
+package us.ihmc.wholeBodyController.concurrent;
+
+/**
+ * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
+ */
+public interface MultiThreadedRobotControlElementCoordinator
+{
+   void start();
+
+   void read();
+}
