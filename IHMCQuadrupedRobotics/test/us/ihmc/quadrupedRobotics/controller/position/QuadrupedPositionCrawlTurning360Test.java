cca5b401ba965fd202d9247ced58724b2e0d0ccf
bshrewsbury <bshrewsbury@ihmc.us>
2016-09-08 18:06:30 -0500

    slowed yaw rate

--- IHMCQuadrupedRobotics/test/us/ihmc/quadrupedRobotics/controller/position/QuadrupedPositionCrawlTurning360Test.java
+++ IHMCQuadrupedRobotics/test/us/ihmc/quadrupedRobotics/controller/position/QuadrupedPositionCrawlTurning360Test.java
@@ -59,7 +59,7 @@ public abstract class QuadrupedPositionCrawlTurning360Test implements QuadrupedM
    {
       QuadrupedTestBehaviors.standUp(conductor, variables);
       
-      variables.getYoPlanarVelocityInputZ().set(-0.2);
+      variables.getYoPlanarVelocityInputZ().set(-0.1);
       
       int numSpins = 1;
       for (int i = 0; i < numSpins; i++)
@@ -86,7 +86,7 @@ public abstract class QuadrupedPositionCrawlTurning360Test implements QuadrupedM
    {
       QuadrupedTestBehaviors.standUp(conductor, variables);
       
-      variables.getYoPlanarVelocityInputZ().set(0.2);
+      variables.getYoPlanarVelocityInputZ().set(0.1);
       
       int numSpins = 1;
       for (int i = 0; i < numSpins; i++)
