492f7fd5dc75952db904c1d10c5894d86dd103a0
Karel Petranek <karel.petranek@gmail.com>
2016-09-08 15:40:41 -0500

    Basic 3D view for IHMCRobotBuilder

--- IHMCRobotBuilder/src/us/ihmc/robotbuilder/MainWindow.java
+++ IHMCRobotBuilder/src/us/ihmc/robotbuilder/MainWindow.java
@@ -1,22 +1,29 @@
 package us.ihmc.robotbuilder;
 
+import com.jme3.math.Vector3f;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Translate;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javaslang.control.Option;
+import us.ihmc.javaFXToolkit.cameraControllers.SimpleCameraMouseEventHandler;
 import us.ihmc.robotbuilder.model.Loader;
 import us.ihmc.robotbuilder.util.Util;
 import us.ihmc.robotics.robotDescription.JointDescription;
 import us.ihmc.robotics.robotDescription.RobotDescription;
+import us.ihmc.robotics.robotDescription.RobotDescriptionNode;
 
+import javax.vecmath.Vector3d;
 import java.io.File;
 
 /**
@@ -69,7 +76,10 @@ public class MainWindow extends Application {
             dialog.setContentText("Model:");
             return Option.ofOptional(dialog.showAndWait());
         })).flatMap(robotDescription -> Util.runLaterInUI(() -> {
-            robotDescription.peek(this::constructTreeView);
+            robotDescription.peek(description -> {
+                treeView.setRoot(constructTreeView(description));
+                populate3DView(description);
+            });
             return null;
         })).onFailure(err -> Util.runLaterInUI(() -> {
             Alert alert = new Alert(AlertType.ERROR, "Error loading file: " + err.getMessage(), ButtonType.OK);
@@ -78,27 +88,48 @@ public class MainWindow extends Application {
         }));
     }
 
-    private void constructTreeView(RobotDescription description) {
+    private TreeItem<String> constructTreeView(RobotDescriptionNode description) {
         TreeItem<String> rootItem = new TreeItem<>(description.getName());
-        description.getRootJoints().stream()
-                .map(this::constructTreeView)
-                .forEach(rootItem.getChildren()::add);
-        treeView.setRoot(rootItem);
-    }
-
-    private TreeItem<String> constructTreeView(JointDescription description) {
-        TreeItem<String> node = new TreeItem<>(description.getName());
         description.getChildrenJoints().stream()
                 .map(this::constructTreeView)
-                .forEach(node.getChildren()::add);
-        return node;
+                .forEach(rootItem.getChildren()::add);
+        return rootItem;
     }
 
     private void populate3DView(RobotDescription description) {
+        Group group = new Group();
+        description.getChildrenJoints().stream().map(this::create3DNodes).forEach(group.getChildren()::add);
+        AmbientLight light = new AmbientLight(Color.WHITE);
+        group.getChildren().add(light);
 
+        SubScene scene3d = new SubScene(group, view3D.getWidth(), view3D.getHeight(), true, SceneAntialiasing.DISABLED);
+        scene3d.setFill(Color.BLACK);
+        PerspectiveCamera camera = Util.lookAtNodeFromDirection((int) view3D.getWidth(), (int) view3D.getHeight(), new Vector3f(1, 0, 0), group, new Vector3f(0, 1, 0));
+        scene3d.setCamera(camera);
+
+        //FocusBasedCameraMouseEventHandler cameraController = new FocusBasedCameraMouseEventHandler(scene3d.widthProperty(), scene3d.heightProperty(), camera, new Vector3d(0, 1, 0));
+        //scene3d.addEventHandler(Event.ANY, cameraController);
+        SimpleCameraMouseEventHandler cameraController = new SimpleCameraMouseEventHandler(camera);
+        scene3d.addEventHandler(MouseEvent.ANY, cameraController);
+
+        view3D.getChildren().add(scene3d);
     }
 
-    private void populate3DView(JointDescription description) {
+    private Node create3DNodes(JointDescription description) {
+        Group jointGroup = new Group();
+        Vector3d offset = new Vector3d();
+        description.getOffsetFromParentJoint(offset);
+        jointGroup.getTransforms().add(new Translate(offset.x, offset.y, offset.z));
 
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        Sphere graphics = new Sphere(0.01);
+        graphics.setMaterial(redMaterial);
+        jointGroup.getChildren().add(graphics);
+
+        description.getChildrenJoints().stream().map(this::create3DNodes).forEach(jointGroup.getChildren()::add);
+        return jointGroup;
     }
 }
