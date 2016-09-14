f70e53eaa4b54ae24cf0d04d374f79f6a4ab856d
JerryPratt <jpratt@ihmc.us>
2016-09-14 13:03:08 -0500

    fixBamboo. Put in development.

--- SimulationConstructionSet/test/us/ihmc/simulationconstructionset/physics/collision/gdx/GdxCollisionDetectorTest.java
+++ SimulationConstructionSet/test/us/ihmc/simulationconstructionset/physics/collision/gdx/GdxCollisionDetectorTest.java
@@ -2,7 +2,10 @@ package us.ihmc.simulationconstructionset.physics.collision.gdx;
 
 import us.ihmc.simulationconstructionset.physics.ScsCollisionDetector;
 import us.ihmc.simulationconstructionset.physics.collision.SCSCollisionDetectorTest;
+import us.ihmc.tools.testing.TestPlanTarget;
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestClass;
 
+@DeployableTestClass(targets = TestPlanTarget.InDevelopment)
 public class GdxCollisionDetectorTest extends SCSCollisionDetectorTest
 {
    @Override
