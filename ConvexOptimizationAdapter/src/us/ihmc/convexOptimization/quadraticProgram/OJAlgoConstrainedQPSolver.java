372feab143df8b4c54c80bad311d581aafc77947
JerryPratt <jpratt@ihmc.us>
2016-09-14 09:44:55 -0500

    Starting in on OJAlgoConstrainedQPSolver.

--- ConvexOptimizationAdapter/src/us/ihmc/convexOptimization/quadraticProgram/OJAlgoConstrainedQPSolver.java
+++ ConvexOptimizationAdapter/src/us/ihmc/convexOptimization/quadraticProgram/OJAlgoConstrainedQPSolver.java
@@ -1,24 +1,31 @@
 package us.ihmc.convexOptimization.quadraticProgram;
 
 import org.ejml.data.DenseMatrix64F;
+import org.ojalgo.matrix.store.PrimitiveDenseStore;
 import org.ojalgo.optimisation.ExpressionsBasedModel;
+import org.ojalgo.optimisation.convex.ConvexSolver;
 
 import us.ihmc.tools.exceptions.NoConvergenceException;
 
 public class OJAlgoConstrainedQPSolver extends ConstrainedQPSolver
 {
- 
+
    public OJAlgoConstrainedQPSolver()
    {
+
       ExpressionsBasedModel foo;
    }
-   
+
    @Override
    public int solve(DenseMatrix64F Q, DenseMatrix64F f, DenseMatrix64F Aeq, DenseMatrix64F beq, DenseMatrix64F Ain, DenseMatrix64F bin, DenseMatrix64F x,
          boolean initialize) throws NoConvergenceException
    {
-      // TODO Auto-generated method stub
-      return 0;
+      PrimitiveDenseStore QDenseStore = PrimitiveDenseStore.FACTORY.columns(Q.data);
+      PrimitiveDenseStore CDenseStore = PrimitiveDenseStore.FACTORY.columns(f.data);
+
+      ConvexSolver.Builder builder = new ConvexSolver.Builder(QDenseStore, CDenseStore);
+
+      return 2;
    }
 
    @Override
