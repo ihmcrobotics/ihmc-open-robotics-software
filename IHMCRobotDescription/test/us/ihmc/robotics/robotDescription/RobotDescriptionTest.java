81cc7a92aec95fe8e76f92dc9e10a355b406d982
JerryPratt <jpratt@ihmc.us>
2016-09-14 10:15:46 -0500

    Added RobotDescriptionTest.
    Need to fill it in.

--- IHMCRobotDescription/test/us/ihmc/robotics/robotDescription/RobotDescriptionTest.java
+++ IHMCRobotDescription/test/us/ihmc/robotics/robotDescription/RobotDescriptionTest.java
@@ -4,15 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import us.ihmc.tools.testing.TestPlanAnnotations.DeployableTestMethod;
+
 public class RobotDescriptionTest
 {
 
-   @Test
+   @DeployableTestMethod(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
    public void testOne()
    {
       RobotDescription description = new RobotDescription("Test");
-
       assertEquals("Test", description.getName());
+
+      description.setName("TestTwo");
+      assertEquals("TestTwo", description.getName());
 
 
    }
---
+++ IHMCRobotDescription/test/us/ihmc/robotics/robotDescription/generatedTestSuites/IHMCRobotDescriptionAFastTestSuite.java
@@ -0,0 +1,25 @@
+package us.ihmc.robotics.robotDescription.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+import us.ihmc.tools.testing.TestPlanSuite;
+import us.ihmc.tools.testing.TestPlanSuite.TestSuiteTarget;
+import us.ihmc.tools.testing.TestPlanTarget;
+
+/** WARNING: AUTO-GENERATED FILE. DO NOT MAKE MANUAL CHANGES TO THIS FILE. **/
+@RunWith(TestPlanSuite.class)
+@TestSuiteTarget(TestPlanTarget.Fast)
+@SuiteClasses
+({
+   us.ihmc.robotics.robotDescription.RobotDescriptionTest.class,
+   us.ihmc.robotics.robotDescription.RobotDescriptionUsingSpringFlamingoTest.class
+})
+
+public class IHMCRobotDescriptionAFastTestSuite
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
