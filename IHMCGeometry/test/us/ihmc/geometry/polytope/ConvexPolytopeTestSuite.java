b3bcb13f1cee811ef14709fc14a748ee8b010c60
JerryPratt <jpratt@ihmc.us>
2016-09-15 18:21:16 -0500

    SimplexPolytope start.

--- IHMCGeometry/test/us/ihmc/geometry/polytope/ConvexPolytopeTestSuite.java
+++ IHMCGeometry/test/us/ihmc/geometry/polytope/ConvexPolytopeTestSuite.java
@@ -10,7 +10,7 @@ import us.ihmc.tools.testing.TestPlanTarget;
 
 @RunWith(TestPlanSuite.class)
 @TestSuiteTarget(TestPlanTarget.Fast)
-@SuiteClasses({ ConvexPolytopeTest.class, GilbertJohnsonKeerthiCollisionDetectorTest.class })
+@SuiteClasses({ ConvexPolytopeTest.class, SimplexPolytopeTest.class, GilbertJohnsonKeerthiCollisionDetectorTest.class })
 
 public class ConvexPolytopeTestSuite
 {
