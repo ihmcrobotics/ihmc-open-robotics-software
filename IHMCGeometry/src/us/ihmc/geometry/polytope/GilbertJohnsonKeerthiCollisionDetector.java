3f0a61cdc847c8f3a653385894512254cbd27750
JerryPratt <jpratt@ihmc.us>
2016-09-16 17:57:40 -0500

    Use barycentric coordinates to compute closest points.
    Still need to determine points of intersection when the two Polytopes
    are actually intersecting...

--- IHMCGeometry/src/us/ihmc/geometry/polytope/GilbertJohnsonKeerthiCollisionDetector.java
+++ IHMCGeometry/src/us/ihmc/geometry/polytope/GilbertJohnsonKeerthiCollisionDetector.java
@@ -5,7 +5,7 @@ import javax.vecmath.Vector3d;
 
 /**
  * GilbertJohnsonKeerthi (GJK) algorithm for doing collision detection
- * 
+ *
  * For more information see book, papers, and presentations at http://realtimecollisiondetection.net/pubs/
  */
 public class GilbertJohnsonKeerthiCollisionDetector
@@ -30,19 +30,19 @@ public class GilbertJohnsonKeerthiCollisionDetector
       supportPoint.sub(supportingVertexTwo.getPosition());
    }
 
-   public boolean arePolytopesColliding(ConvexPolytope polytopeA, ConvexPolytope polytopeB)
+   public boolean arePolytopesColliding(ConvexPolytope polytopeA, ConvexPolytope polytopeB, Point3d pointOnAToPack, Point3d pointOnBToPack)
    {
       simplex.clearPoints();
-      
+
       // Step 1) Initialize Simplex Q to a single point in A minkowskiDifference B. Here we'll just use A.vertex0 and B.vertex0
       PolytopeVertex vertexOne = polytopeA.getVertex(0);
       PolytopeVertex vertexTwo = polytopeB.getVertex(0);
-      
+
       Point3d minkowskiDifferenceVertex = new Point3d();
       minkowskiDifferenceVertex.sub(vertexOne.getPosition(), vertexTwo.getPosition());
-      
-      simplex.addPoint(minkowskiDifferenceVertex);
-            
+
+      simplex.addPoint(minkowskiDifferenceVertex, vertexOne.getPosition(), vertexTwo.getPosition());
+
       while (true)
       {
          // Step 2) Compute closest point to origin in the simplex. 4) Reduce points of Q not used in determining P.
@@ -51,39 +51,45 @@ public class GilbertJohnsonKeerthiCollisionDetector
 
          // Step 3) If P is origin, done.
          double distanceSquared = closestPointToOrigin.distanceSquared(new Point3d());
-         if (distanceSquared < 1e-10) return true;
-         
+         if (distanceSquared < 1e-10)
+         {
+            simplex.getClosestPointsOnAAndB(pointOnAToPack, pointOnBToPack);
+            return true;
+         }
+
          // Step 5) v = support vector in negative P direction on A minkowskiDifference B.
          // In other words, it is suppport vector on A in P direction minus support vector on B in negative P direction.
-         
+
          Vector3d supportDirection = new Vector3d(closestPointToOrigin);
          supportDirection.negate();
          PolytopeVertex supportingVertexA = polytopeA.getSupportingVertex(supportDirection);
-         
+
          supportDirection.negate();
          PolytopeVertex supportingVertexB = polytopeB.getSupportingVertex(supportDirection);
-         
+
          Vector3d v = new Vector3d();
          v.sub(supportingVertexA.getPosition(), supportingVertexB.getPosition());
-         
+
          // Step 6) If v is no more extremal in direction -P than P itself, then not intersecting. magnitude(v) is distance. Use v to determine closest points.
-         
+
          Vector3d P = new Vector3d(closestPointToOrigin);
-         
+
          double vDotP = v.dot(P);
          double foo = vDotP - distanceSquared;
 //         System.out.println("vDotP - PSquared = " + foo);
-         
-         // TODO: Do we need this epsilon here. Seems when to abort is tricky. Maybe look that the extremal points in the 
+
+         // TODO: Do we need this epsilon here. Seems when to abort is tricky. Maybe look that the extremal points in the
          // simplex stop changing?
          if (vDotP >= distanceSquared - 1e-12)
          {
+            simplex.getClosestPointsOnAAndB(pointOnAToPack, pointOnBToPack);
+
             return false;
          }
-         
+
          // Step 7) Add v to Q and got to step 2.
-         simplex.addPoint(new Point3d(v));
-         
+         simplex.addPoint(new Point3d(v), supportingVertexA.getPosition(), supportingVertexB.getPosition());
+
       }
 
    }
