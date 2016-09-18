09c7fb642b7f493f5ba40179365c030eb731bd50
Robert <rgriffin@ihmc.us>
2016-09-18 14:22:44 -0500

    added a pretty detailed java doc for the icp optimization parameters

--- Atlas/src/us/ihmc/atlas/parameters/AtlasICPOptimizationParameters.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasICPOptimizationParameters.java
@@ -2,144 +2,194 @@ package us.ihmc.atlas.parameters;
 
 import us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.icpOptimization.ICPOptimizationParameters;
 
-public class AtlasICPOptimizationParameters implements ICPOptimizationParameters
+/** {@inheritDoc} */
+public class AtlasICPOptimizationParameters extends ICPOptimizationParameters
 {
-   @Override public int getMaximumNumberOfFootstepsToConsider()
-   {
-      return 5;
-   }
-
-   @Override public int numberOfFootstepsToConsider()
+   /** {@inheritDoc} */
+   @Override
+   public int numberOfFootstepsToConsider()
    {
       return 1;
    }
 
-   @Override public double getFootstepWeight()
+   /** {@inheritDoc} */
+   @Override
+   public double getFootstepWeight()
    {
       return 15.0;
    }
 
-   @Override public double getFootstepRegularizationWeight()
+   /** {@inheritDoc} */
+   @Override
+   public double getFootstepRegularizationWeight()
    {
       return 0.0001;
    }
 
-   @Override public double getFeedbackWeight()
+   /** {@inheritDoc} */
+   @Override
+   public double getFeedbackWeight()
    {
       return 0.1;
    }
 
-   @Override public double getFeedbackRegularizationWeight()
+   /** {@inheritDoc} */
+   @Override
+   public double getFeedbackRegularizationWeight()
    {
       return 0.001;
    }
 
-   @Override public double getFeedbackParallelGain()
+   /** {@inheritDoc} */
+   @Override
+   public double getFeedbackParallelGain()
    {
       return 5.0;
    }
 
-   @Override public double getFeedbackOrthogonalGain()
+   /** {@inheritDoc} */
+   @Override
+   public double getFeedbackOrthogonalGain()
    {
       return 5.0;
    }
 
-   @Override public double getDynamicRelaxationWeight()
+   /** {@inheritDoc} */
+   @Override
+   public double getDynamicRelaxationWeight()
    {
       return 5000.0;
    }
 
-   @Override public double getDynamicRelaxationDoubleSupportWeightModifier()
+   /** {@inheritDoc} */
+   @Override
+   public double getDynamicRelaxationDoubleSupportWeightModifier()
    {
       return 5.0;
    }
 
-   @Override public boolean scaleStepRegularizationWeightWithTime()
+   /** {@inheritDoc} */
+   @Override
+   public boolean scaleStepRegularizationWeightWithTime()
    {
       return true;
    }
 
-   @Override public boolean scaleFeedbackWeightWithGain()
+   /** {@inheritDoc} */
+   @Override
+   public boolean scaleFeedbackWeightWithGain()
    {
       return true;
    }
 
-   @Override public boolean scaleUpcomingStepWeights()
+   /** {@inheritDoc} */
+   @Override
+   public boolean scaleUpcomingStepWeights()
    {
       return true;
    }
 
-   @Override public boolean useFeedback()
+   /** {@inheritDoc} */
+   @Override
+   public boolean useFeedback()
    {
       return true;
    }
 
-   @Override public boolean useFeedbackRegularization()
+   /** {@inheritDoc} */
+   @Override
+   public boolean useFeedbackRegularization()
    {
       return true;
    }
 
-   @Override public boolean useStepAdjustment()
+   /** {@inheritDoc} */
+   @Override
+   public boolean useStepAdjustment()
    {
       return true;
    }
 
-   @Override public boolean useFootstepRegularization()
+   /** {@inheritDoc} */
+   @Override
+   public boolean useFootstepRegularization()
    {
       return true;
    }
 
-   @Override public boolean useFeedbackWeightHardening()
+   /** {@inheritDoc} */
+   @Override
+   public boolean useFeedbackWeightHardening()
    {
       return false;
    }
 
-   @Override public boolean useICPFromBeginningOfState()
+   /** {@inheritDoc} */
+   @Override
+   public boolean useICPFromBeginningOfState()
    {
       return true;
    }
 
-   @Override public double getMinimumFootstepWeight()
+   /** {@inheritDoc} */
+   @Override
+   public double getMinimumFootstepWeight()
    {
       return 0.0001;
    }
 
-   @Override public double getMinimumFeedbackWeight()
+   /** {@inheritDoc} */
+   @Override
+   public double getMinimumFeedbackWeight()
    {
       return 0.0001;
    }
 
-   @Override public double getMinimumTimeRemaining()
+   /** {@inheritDoc} */
+   @Override
+   public double getMinimumTimeRemaining()
    {
       return 0.001;
    }
 
-   @Override public double getFeedbackWeightHardeningMultiplier()
+   /** {@inheritDoc} */
+   @Override
+   public double getFeedbackWeightHardeningMultiplier()
    {
       return 20.0;
    }
 
-   @Override public double getMaxCMPForwardExit()
+   /** {@inheritDoc} */
+   @Override
+   public double getMaxCMPForwardExit()
    {
       return 0.01;
    }
 
-   @Override public double getMaxCMPLateralExit()
+   /** {@inheritDoc} */
+   @Override
+   public double getMaxCMPLateralExit()
    {
       return 0.015;
    }
 
-   @Override public double getForwardAdjustmentDeadband()
+   /** {@inheritDoc} */
+   @Override
+   public double getForwardAdjustmentDeadband()
    {
       return 0.00;
    }
 
-   @Override public double getLateralAdjustmentDeadband()
+   /** {@inheritDoc} */
+   @Override
+   public double getLateralAdjustmentDeadband()
    {
       return 0.00;
    }
 
-   @Override public double getRemainingTimeToStopAdjusting()
+   /** {@inheritDoc} */
+   @Override
+   public double getRemainingTimeToStopAdjusting()
    {
       return 0.05;
    }
