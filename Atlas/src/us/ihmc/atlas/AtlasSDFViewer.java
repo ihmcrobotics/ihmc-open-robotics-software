b12e05b964255f205b2fb6069b5c702fe8e59190
bshrewsbury <bshrewsbury@ihmc.us>
2016-09-19 15:38:16 -0500

    added some graphics to the atlas sdf viewer, has issues

--- Atlas/src/us/ihmc/atlas/AtlasSDFViewer.java
+++ Atlas/src/us/ihmc/atlas/AtlasSDFViewer.java
@@ -1,14 +1,89 @@
 package us.ihmc.atlas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import us.ihmc.SdfLoader.FloatingRootJointRobot;
+import us.ihmc.SdfLoader.HumanoidFloatingRootJointRobot;
 import us.ihmc.darpaRoboticsChallenge.drcRobot.DRCRobotModel;
+import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
+import us.ihmc.graphics3DAdapter.graphics.appearances.AppearanceDefinition;
+import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
+import us.ihmc.simulationconstructionset.Joint;
+import us.ihmc.simulationconstructionset.Link;
+import us.ihmc.simulationconstructionset.OneDegreeOfFreedomJoint;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 
 public class AtlasSDFViewer
 {
+   private static final boolean SHOW_ELLIPSOIDS = true;
+   private static final boolean SHOW_COORDINATES_AT_JOINT_ORIGIN = false;
+
    public static void main(String[] args)
    {
       DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, DRCRobotModel.RobotTarget.SCS, false);
-      SimulationConstructionSet scs = new SimulationConstructionSet(robotModel.createSdfRobot(false));
+      HumanoidFloatingRootJointRobot sdfRobot = robotModel.createSdfRobot(false);
+      SimulationConstructionSet scs = new SimulationConstructionSet(sdfRobot);
+      if (SHOW_ELLIPSOIDS)
+      {
+         addIntertialEllipsoidsToVisualizer(sdfRobot);
+      }
+
+      if (SHOW_COORDINATES_AT_JOINT_ORIGIN)
+      {
+         addJointAxis(sdfRobot);
+      }
+
       scs.startOnAThread();
+   }
+
+
+   private static void addIntertialEllipsoidsToVisualizer(FloatingRootJointRobot sdfRobot)
+   {
+      ArrayList<Joint> joints = new ArrayList<>();
+      joints.add(sdfRobot.getRootJoint());
+
+      HashSet<Link> links = getAllLinks(joints, new HashSet<Link>());
+
+      for (Link link : links)
+      {
+         if(link.getLinkGraphics() != null)
+         {
+            AppearanceDefinition appearance = YoAppearance.Green();
+            appearance.setTransparency(0.6);
+//            link.addEllipsoidFromMassProperties(appearance);
+            link.addCoordinateSystemToCOM(0.1);
+//         l.addBoxFromMassProperties(appearance);
+         }
+      }
+   }
+
+   private static HashSet<Link> getAllLinks(ArrayList<Joint> joints, HashSet<Link> links)
+   {
+      for (Joint joint : joints)
+      {
+         links.add(joint.getLink());
+
+         if (!joint.getChildrenJoints().isEmpty())
+         {
+            links.addAll(getAllLinks(joint.getChildrenJoints(), links));
+         }
+      }
+
+      return links;
+   }
+
+   public static void addJointAxis(FloatingRootJointRobot sdfRobot)
+   {
+      ArrayList<OneDegreeOfFreedomJoint> joints = new ArrayList<>(Arrays.asList(sdfRobot.getOneDegreeOfFreedomJoints()));
+
+      for (OneDegreeOfFreedomJoint joint : joints)
+      {
+         Graphics3DObject linkGraphics = new Graphics3DObject();
+         linkGraphics.addCoordinateSystem(0.1);
+         linkGraphics.combine(joint.getLink().getLinkGraphics());
+         joint.getLink().setLinkGraphics(linkGraphics);
+      }
    }
 }
