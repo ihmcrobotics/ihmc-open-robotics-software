49b4c9b8c277abe4704659326dd9302882f00f05
Robert <rgriffin@ihmc.us>
2016-09-21 12:11:15 -0500

    regenerated the test suites

--- Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasKFastTestSuite.java
+++ Atlas/test/us/ihmc/atlas/generatedTestSuites/AtlasKFastTestSuite.java
@@ -12,7 +12,8 @@ import us.ihmc.tools.testing.TestPlanTarget;
 @TestSuiteTarget(TestPlanTarget.Fast)
 @SuiteClasses
 ({
-   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseRampsTest.class
+   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCoursePlatformTest.class,
+   us.ihmc.atlas.ObstacleCourseTests.AtlasObstacleCourseRampFootstepSnapperTest.class
 })
 
 public class AtlasKFastTestSuite
