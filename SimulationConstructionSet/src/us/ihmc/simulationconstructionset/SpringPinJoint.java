17a94d4f59c8e108e565d8a21f529a6c64f2d78a
brandon shrewsbury <bshrewsbury@ihmc.us>
2016-09-27 11:32:16 -0500

    Created a springPinJoint and a description

---
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/SpringPinJoint.java
@@ -0,0 +1,92 @@
+package us.ihmc.simulationconstructionset;
+
+import javax.vecmath.Vector3d;
+
+import us.ihmc.robotics.Axis;
+import us.ihmc.robotics.dataStructures.listener.VariableChangedListener;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.dataStructures.variable.YoVariable;
+
+/**
+ * creates a pin joint with a pd controller built in.
+ * This calls compute on the pd controller every time getTau is called.
+ * @author steel
+ *
+ */
+public class SpringPinJoint extends PinJoint
+{
+   
+   private final DoubleYoVariable kp;
+   private final DoubleYoVariable kd;
+   
+   public SpringPinJoint(String jname, Vector3d offset, Robot rob, Vector3d u_hat)
+   {
+      super(jname, offset, rob, u_hat);
+      kp = new DoubleYoVariable(jname + "_kp", registry);
+      kd = new DoubleYoVariable(jname + "_kd", registry);
+      setChangeListeners();
+      kp.set(40000.0);
+      kd.set(200.0);
+   }
+
+   public SpringPinJoint(String jname, Vector3d offset, Robot rob, Axis jaxis)
+   {
+      super(jname, offset, rob, jaxis);
+      kp = new DoubleYoVariable(jname + "_kp", registry);
+      kd = new DoubleYoVariable(jname + "_kd", registry);
+      setChangeListeners();
+      kp.set(40000.0);
+      kd.set(200.0);
+   }
+   
+   private void setChangeListeners()
+   {
+      kp.addVariableChangedListener(new VariableChangedListener()
+      {
+         @Override
+         public void variableChanged(YoVariable<?> v)
+         {
+            setKp(kp.getDoubleValue());
+         }
+      });
+      kd.addVariableChangedListener(new VariableChangedListener()
+      {
+         @Override
+         public void variableChanged(YoVariable<?> v)
+         {
+            setKd(kd.getDoubleValue());
+         }
+      });
+   }
+   
+   public void setKp(double kp)
+   {
+      super.setKp(kp);
+      this.kp.set(kp, false);
+   }
+   
+   public void setKd(double kd)
+   {
+      super.setKd(kd);
+      this.kd.set(kd, false);
+   }
+   
+   @Override
+   public double getTau()
+   {
+      compute();
+      return tau.getDoubleValue();
+   }
+
+   public void compute()
+   {
+      tau.set(doPDControl());
+   }
+
+   @Override
+   public DoubleYoVariable getTauYoVariable()
+   {
+      return tau;
+   }
+
+}
