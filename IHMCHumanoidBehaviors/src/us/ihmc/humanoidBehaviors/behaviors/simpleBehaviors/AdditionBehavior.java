67d5aa323eb8ececda001dc9478f87ef2480a179
John Carff <jcarff@ihmc.us>
2016-09-28 14:37:43 -0500

    Testing WalkToBehavior

---
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/simpleBehaviors/AdditionBehavior.java
@@ -0,0 +1,70 @@
+package us.ihmc.humanoidBehaviors.behaviors.simpleBehaviors;
+
+import us.ihmc.humanoidBehaviors.behaviors.AbstractBehavior;
+import us.ihmc.humanoidBehaviors.communication.OutgoingCommunicationBridgeInterface;
+
+public class AdditionBehavior extends AbstractBehavior
+{
+
+   boolean numbersSet = false;
+   boolean mathComplete = false;
+   double value1;
+   double value2;
+   double result;
+
+   public AdditionBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridge)
+   {
+      super(outgoingCommunicationBridge);
+   }
+
+   public AdditionBehavior(OutgoingCommunicationBridgeInterface outgoingCommunicationBridge, double value1, double value2)
+   {
+      super(outgoingCommunicationBridge);
+
+      setInput(value1, value2);
+   }
+
+   public void setInput(double value1, double value2)
+   {
+      this.value1 = value1;
+      this.value2 = value2;
+      numbersSet = true;
+   }
+
+   @Override
+   public void doControl()
+   {
+      if (hasInputBeenSet() && !mathComplete)
+      {
+         result = value1 + value2;
+         mathComplete = true;
+      }
+   }
+
+   public double getResult()
+   {
+      if (mathComplete)
+         return result;
+      else
+         return Double.MIN_VALUE;
+   }
+
+   @Override
+   public boolean isDone()
+   {
+      return mathComplete;
+   }
+
+   @Override
+   public void doPostBehaviorCleanup()
+   {
+      super.doPostBehaviorCleanup();
+      numbersSet = false;
+      mathComplete = false;
+   }
+
+   public boolean hasInputBeenSet()
+   {
+      return numbersSet;
+   }
+}
