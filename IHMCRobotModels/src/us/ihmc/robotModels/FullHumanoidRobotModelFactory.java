9f96d473094f739f01f9d29f7dc050b1bb4241d7
Nathan Mertins <nmertins@ihmc.us>
2016-09-29 09:37:18 -0700

    updating imports and dependencies

--- IHMCRobotModels/src/us/ihmc/robotModels/FullHumanoidRobotModelFactory.java
+++ IHMCRobotModels/src/us/ihmc/robotModels/FullHumanoidRobotModelFactory.java
@@ -1,8 +1,5 @@
 package us.ihmc.robotModels;
 
-import us.ihmc.SdfLoader.models.FullHumanoidRobotModel;
-import us.ihmc.SdfLoader.models.FullRobotModelFactory;
-
 public interface FullHumanoidRobotModelFactory extends FullRobotModelFactory
 {
    @Override
