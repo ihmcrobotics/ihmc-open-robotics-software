6a5a3b4766839d1a019f91e6f96cb4bfda88b7ac
Sylvain Bertrand <sbertrand@ihmc.us>
2016-10-14 17:04:29 -0500

    Changed extruded polygon to output triangles and normals.

--- IHMCJavaFXToolkit/visualizers/us/ihmc/javaFXToolkit/shapes/MeshDataGeneratorVisualizer.java
+++ IHMCJavaFXToolkit/visualizers/us/ihmc/javaFXToolkit/shapes/MeshDataGeneratorVisualizer.java
@@ -1,5 +1,8 @@
 package us.ihmc.javaFXToolkit.shapes;
 
+import java.util.Random;
+
+import javax.vecmath.Point2d;
 import javax.vecmath.Vector3d;
 
 import javafx.application.Application;
@@ -15,6 +18,8 @@ import javafx.stage.Stage;
 import us.ihmc.graphics3DAdapter.graphics.MeshDataGenerator;
 import us.ihmc.javaFXToolkit.cameraControllers.FocusBasedCameraMouseEventHandler;
 import us.ihmc.javaFXToolkit.graphics.JavaFXMeshDataInterpreter;
+import us.ihmc.robotics.geometry.ConvexPolygon2d;
+import us.ihmc.robotics.random.RandomTools;
 
 public class MeshDataGeneratorVisualizer extends Application
 {
@@ -32,7 +37,7 @@ public class MeshDataGeneratorVisualizer extends Application
 
       MeshView torus = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.ArcTorus(0.0, 2.0 * Math.PI, 0.3, 0.1, 64)));
       torus.setMaterial(defaultMaterial);
-      torus.setTranslateY(1.0);
+      torus.setTranslateY(1.5);
       rootNode.getChildren().add(torus);
 
       MeshView cylinder = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Cylinder(0.1, 0.3, 64)));
@@ -50,8 +55,37 @@ public class MeshDataGeneratorVisualizer extends Application
       sphere.setTranslateX(0.5);
       rootNode.getChildren().add(sphere);
 
+      Point2d[] polygonVertices = createPolygon();
+      MeshView extrudedPolygon = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.ExtrudedPolygon(polygonVertices, 0.1)));
+      extrudedPolygon.setMaterial(defaultMaterial);
+      extrudedPolygon.setTranslateX(2.0);
+      extrudedPolygon.setTranslateY(0.5);
+      rootNode.getChildren().add(extrudedPolygon);
+
       primaryStage.setScene(scene);
       primaryStage.show();
+   }
+
+   private Point2d[] createPolygon()
+   {
+      int numberOfPoints = 20;
+
+      ConvexPolygon2d polygon = new ConvexPolygon2d();
+
+      Point2d randomVertex = new Point2d();
+      Random random = new Random(234523L);
+
+      while (polygon.getNumberOfVertices() < numberOfPoints)
+      {
+         randomVertex.add(RandomTools.generateRandomPoint2d(random, 0.03, 0.03));
+         polygon.addVertex(randomVertex);
+         polygon.update();
+      }
+
+      Point2d[] vertices = new Point2d[numberOfPoints];
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         vertices[i] = polygon.getVertex(i);
+      return vertices;
    }
 
    private void setupCamera(Group root, Scene scene)
