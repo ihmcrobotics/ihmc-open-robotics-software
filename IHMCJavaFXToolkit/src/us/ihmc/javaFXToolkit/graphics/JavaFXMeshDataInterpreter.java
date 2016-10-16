4862a352a515bd30bcd4784e9d990a180ba1ad36
Sylvain Bertrand <sbertrand@ihmc.us>
2016-10-15 19:17:00 -0500

    Implemented filters to remove duplicates.

--- IHMCJavaFXToolkit/src/us/ihmc/javaFXToolkit/graphics/JavaFXMeshDataInterpreter.java
+++ IHMCJavaFXToolkit/src/us/ihmc/javaFXToolkit/graphics/JavaFXMeshDataInterpreter.java
@@ -1,10 +1,16 @@
 package us.ihmc.javaFXToolkit.graphics;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.vecmath.Point3f;
 import javax.vecmath.TexCoord2f;
 import javax.vecmath.Tuple2f;
 import javax.vecmath.Tuple3f;
 import javax.vecmath.Vector3f;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import gnu.trove.list.array.TIntArrayList;
 import javafx.scene.shape.TriangleMesh;
@@ -21,17 +27,21 @@ public class JavaFXMeshDataInterpreter
       Point3f[] vertices = meshData.getVertices();
       TexCoord2f[] texturePoints = meshData.getTexturePoints();
       int[] triangleIndices = meshData.getTriangleIndices();
-      Vector3f[] polygonNormals = meshData.getVertexNormals();
-
-      TIntArrayList facesVertexOnlyIndices = new TIntArrayList();
+      Vector3f[] normals = meshData.getVertexNormals();
       TIntArrayList facesIndices = new TIntArrayList();
 
-      facesVertexOnlyIndices.addAll(triangleIndices);
-      for (int triangleVertexIndex : triangleIndices)
+      Pair<int[], Point3f[]> filterDuplicateVertices = filterDuplicates(triangleIndices, vertices);
+      Pair<int[], Vector3f[]> filterDuplicateNormals = filterDuplicates(triangleIndices, normals);
+      Pair<int[], TexCoord2f[]> filterDuplicateTex = filterDuplicates(triangleIndices, texturePoints);
+      vertices = filterDuplicateVertices.getRight();
+      normals = filterDuplicateNormals.getRight();
+      texturePoints = filterDuplicateTex.getRight();
+
+      for (int pos = 0; pos < triangleIndices.length; pos++)
       {
-         facesIndices.add(triangleVertexIndex); // vertex index
-         facesIndices.add(triangleVertexIndex); // normal index
-         facesIndices.add(triangleVertexIndex); // texture index
+         facesIndices.add(filterDuplicateVertices.getLeft()[pos]); // vertex index
+         facesIndices.add(filterDuplicateNormals.getLeft()[pos]); // normal index
+         facesIndices.add(filterDuplicateTex.getLeft()[pos]); // texture index
       }
 
       int[] indices = facesIndices.toArray();
@@ -41,9 +51,36 @@ public class JavaFXMeshDataInterpreter
       triangleMesh.getTexCoords().addAll(convertToFloatArray(texturePoints));
       triangleMesh.getFaces().addAll(indices);
       triangleMesh.getFaceSmoothingGroups().addAll(new int[indices.length / triangleMesh.getFaceElementSize()]);
-      triangleMesh.getNormals().addAll(convertToFloatArray(polygonNormals));
+      triangleMesh.getNormals().addAll(convertToFloatArray(normals));
 
       return triangleMesh;
+   }
+
+   private static <T> Pair<int[], T[]> filterDuplicates(int[] originalIndices, T[] valuesWithDuplicates)
+   {
+      Map<T, Integer> uniqueValueIndices = new HashMap<>();
+
+      for (int valueIndex = valuesWithDuplicates.length - 1; valueIndex >= 0; valueIndex--)
+         uniqueValueIndices.put(valuesWithDuplicates[valueIndex], valueIndex);
+
+      @SuppressWarnings("unchecked")
+      T[] filteredValue = (T[]) Array.newInstance(valuesWithDuplicates[0].getClass(), uniqueValueIndices.size());
+      int pos = 0;
+      
+      for (T value : uniqueValueIndices.keySet())
+      {
+         uniqueValueIndices.put(value, pos);
+         filteredValue[pos] = value;
+         pos++;
+      }
+
+      int[] filteredIndices = new int[originalIndices.length];
+      pos = 0;
+
+      for (int triangleIndex : originalIndices)
+         filteredIndices[pos++] = uniqueValueIndices.get(valuesWithDuplicates[triangleIndex]);
+
+      return Pair.of(filteredIndices, filteredValue);
    }
 
    private static float[] convertToFloatArray(Tuple3f[] tuple3fs)
