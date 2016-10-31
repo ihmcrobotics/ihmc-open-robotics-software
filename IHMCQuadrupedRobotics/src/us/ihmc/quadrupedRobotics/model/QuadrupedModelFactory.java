e1110e84616629048a6a452dd18670714c016171
Jesper Smith <jsmith@ihmc.us>
2016-10-31 16:35:38 -0500

    BEAST-414: Refactored IHMCQuadrupedRobotics to not depend on SCS anymore

--- IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/model/QuadrupedModelFactory.java
+++ IHMCQuadrupedRobotics/src/us/ihmc/quadrupedRobotics/model/QuadrupedModelFactory.java
@@ -2,14 +2,14 @@ package us.ihmc.quadrupedRobotics.model;
 
 import java.util.Collection;
 
-import us.ihmc.robotModels.FullQuadrupedRobotModelFactory;
-import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
 import us.ihmc.robotModels.FullQuadrupedRobotModel;
+import us.ihmc.robotModels.FullQuadrupedRobotModelFactory;
 import us.ihmc.robotics.partNames.QuadrupedJointName;
+import us.ihmc.robotics.robotDescription.RobotDescription;
 
 public abstract class QuadrupedModelFactory implements FullQuadrupedRobotModelFactory
 {
-   public abstract FloatingRootJointRobot createSdfRobot();
+   public abstract RobotDescription createSdfRobot();
 
    @Override
    public abstract FullQuadrupedRobotModel createFullRobotModel();
