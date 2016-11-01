bd63146319c5b91450627c6580d223d7212b53ba
Duncan Calvert <dcalvert@ihmc.us>
2016-11-01 17:21:31 -0500

    NRI-152: Delete georegression transform listener and lidar feature localizer.

--- DarpaRoboticsChallenge/src/us/ihmc/darpaRoboticsChallenge/networkProcessor/depthData/GeoregressionTransformListener.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.darpaRoboticsChallenge.networkProcessor.depthData;
-
-import georegression.struct.se.Se3_F64;
-
-public interface GeoregressionTransformListener
-{
-   public void handleTransform(Se3_F64 transform);
-}
