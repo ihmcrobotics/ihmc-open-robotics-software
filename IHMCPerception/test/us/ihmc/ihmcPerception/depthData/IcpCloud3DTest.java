b0b8346550c54006ef07cfd9c56cc8f931c2c727
Duncan Calvert <dcalvert@ihmc.us>
2016-11-02 16:43:48 -0500

    NRI-153: Move IcpCloud3d to IHMCPerception.

--- IHMCPerception/test/us/ihmc/ihmcPerception/depthData/IcpCloud3DTest.java
+++ IHMCPerception/test/us/ihmc/ihmcPerception/depthData/IcpCloud3DTest.java
@@ -1,4 +1,4 @@
-package us.ihmc.darpaRoboticsChallenge.odometry;
+package us.ihmc.ihmcPerception.depthData;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -17,6 +17,7 @@ import georegression.geometry.RotationMatrixGenerator;
 import georegression.struct.point.Point3D_F64;
 import georegression.struct.se.Se3_F64;
 import georegression.transform.se.SePointOps_F64;
+import us.ihmc.ihmcPerception.depthData.IcpCloud3D;
 import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 
 /**
