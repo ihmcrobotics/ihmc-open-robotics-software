00429fa8907b16949cdda4332194c35eed74e1a4
Nick van Dijk <nvandijk@ihmc.us>
2016-11-05 16:51:36 -0500

    NRI-188: Added more test cases, adjusted stepwidth in planner.

--- IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/simplePlanners/TurnWalkTurnPlannerTest.java
+++ IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/simplePlanners/TurnWalkTurnPlannerTest.java
@@ -3,7 +3,6 @@ package us.ihmc.footstepPlanning.simplePlanners;
 import org.junit.Test;
 import us.ihmc.footstepPlanning.FootstepPlanner;
 import us.ihmc.robotics.geometry.ConvexPolygon2d;
-import us.ihmc.robotics.geometry.FramePoint2d;
 import us.ihmc.robotics.geometry.FramePose2d;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
@@ -21,11 +20,54 @@ public class TurnWalkTurnPlannerTest
 {
    private final Random random = new Random(727434726273L);
    private boolean succes = true;
+   private boolean showSimulation = true;
+
+   private double stepWidth = 0.3;
 
 
    @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
    @Test(timeout = 300000)
-   public void testIsGoalWithinFeet()
+   public void testJustStraightLine()
+   {
+      double xGoal = 5.0;
+      double yGoal = 0.0;
+      double yawGoal = 0.0;
+      Point2d goalPosition = new Point2d(xGoal, yGoal);
+      FramePose2d goalPose = new FramePose2d(ReferenceFrame.getWorldFrame(), goalPosition, yawGoal);
+
+      double xInitialStanceFoot = 0.0;
+      double yInitialStanceFoot = stepWidth/2.0;
+      double yawInitial = 0.0;
+      Point2d initialStanceFootPosition = new Point2d(xInitialStanceFoot, yInitialStanceFoot);
+      FramePose2d initialStanceFootPose = new FramePose2d(ReferenceFrame.getWorldFrame(), initialStanceFootPosition, yawInitial);
+      RobotSide initialStanceFootSide = RobotSide.RIGHT;
+
+      assertTrue(isGoalWithinFeet(goalPose, initialStanceFootPose, initialStanceFootSide));
+   }
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
+   public void testJustTurnInPlace()
+   {
+      double xGoal = 0.0;
+      double yGoal = 0.0;
+      double yawGoal = 0.0;
+      Point2d goalPosition = new Point2d(xGoal, yGoal);
+      FramePose2d goalPose = new FramePose2d(ReferenceFrame.getWorldFrame(), goalPosition, yawGoal);
+
+      double xInitialStanceFoot = 0.0;
+      double yInitialStanceFoot = stepWidth/2.0;
+      double yawInitial = 20;
+      Point2d initialStanceFootPosition = new Point2d(xInitialStanceFoot, yInitialStanceFoot);
+      FramePose2d initialStanceFootPose = new FramePose2d(ReferenceFrame.getWorldFrame(), initialStanceFootPosition, yawInitial);
+      RobotSide initialStanceFootSide = RobotSide.RIGHT;
+
+      assertTrue(isGoalWithinFeet(goalPose, initialStanceFootPose, initialStanceFootSide));
+   }
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
+   public void testRandomPoses()
    {
       double xGoal = random.nextDouble();
       double yGoal = random.nextDouble();
@@ -39,28 +81,33 @@ public class TurnWalkTurnPlannerTest
       Point2d initialStanceFootPosition = new Point2d(xInitialStanceFoot, yInitialStanceFoot);
       FramePose2d initialStanceFootPose = new FramePose2d(ReferenceFrame.getWorldFrame(), initialStanceFootPosition, yawInitial);
       RobotSide initialStanceFootSide = RobotSide.generateRandomRobotSide(random);
+
+      assertTrue(isGoalWithinFeet(goalPose, initialStanceFootPose, initialStanceFootSide));
+   }
+
+   public boolean isGoalWithinFeet(FramePose2d goalPose, FramePose2d initialStanceFootPose, RobotSide initialStanceFootSide)
+   {
+
       FootstepPlanner turnWalkTurnPlanner = new TurnWalkTurnPlanner();
       turnWalkTurnPlanner.setGoalPose(goalPose);
       turnWalkTurnPlanner.setInitialStanceFoot(initialStanceFootPose, initialStanceFootSide);
 
       List<FramePose2d> footstepPlan = turnWalkTurnPlanner.plan();
 
-      FramePose2d lastFoostep = footstepPlan.get(footstepPlan.size()-1);
+      FramePose2d lastFoostep = footstepPlan.get(footstepPlan.size() - 1);
       FramePose2d secondLastFoostep = footstepPlan.get(footstepPlan.size()-2);
 
       FramePose2d achievedGoal = new FramePose2d();
 
-      double distanceBetweenFootsteps = lastFoostep.getPositionDistance(secondLastFoostep);
-      double distanceFromLastStepToGoal = lastFoostep.getPositionDistance(goalPose);
       achievedGoal.interpolate(lastFoostep, secondLastFoostep, 0.5);
 
-      System.out.println("achievedgoal: " + achievedGoal.getX() + " " + achievedGoal.getY() + " " + achievedGoal.getYaw() + " " + achievedGoal.getReferenceFrame() );
-      System.out.println("goalpose: " + goalPose.getX() + " " + goalPose.getY()+ " " + goalPose.getYaw() + " " + goalPose.getReferenceFrame());
-      System.out.println("lastfootstep: " + lastFoostep.getX() + " " + lastFoostep.getY()+ " " + lastFoostep.getYaw() + " " + lastFoostep.getReferenceFrame());
-      System.out.println("secondlastfootstep: " + secondLastFoostep.getX() + " " + secondLastFoostep.getY()+ " " + secondLastFoostep.getYaw() + " " + secondLastFoostep.getReferenceFrame());
+      if(showSimulation)
+         new FlatFootstepPlanVisualizer(goalPose, initialStanceFootPose, initialStanceFootSide);
+      if(achievedGoal.epsilonEquals(goalPose, 10E-2))
+         succes = true;
+      else succes = false;
 
-      new FlatFootstepPlanVisualizer(goalPose, initialStanceFootPose, initialStanceFootSide);
-      assertTrue("Achieved pose is not equal to goal pose", achievedGoal.epsilonEquals(goalPose, 10E-2));
+      return succes;
    }
 
    private ConvexPolygon2d makeFootPolygon()
