73aff1888ea076ade156ebe5a7355a8bef5e2094
Georg Wiedebach <gwiedebach@ihmc.us>
2016-11-08 09:29:47 -0600

    move the footstep planning result message and have it use the FootstepDataListMessage

---
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/packets/walking/FootstepPlanningToolboxOutputStatus.java
@@ -0,0 +1,49 @@
+package us.ihmc.humanoidRobotics.communication.packets.walking;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import us.ihmc.communication.packets.StatusPacket;
+import us.ihmc.footstepPlanning.FootstepPlanningResult;
+
+public class FootstepPlanningToolboxOutputStatus extends StatusPacket<FootstepPlanningToolboxOutputStatus>
+{
+   public FootstepDataListMessage footstepDataList = new FootstepDataListMessage();
+   public FootstepPlanningResult planningResult;
+
+   public FootstepPlanningToolboxOutputStatus()
+   {
+      // empty constructor for serialization
+   }
+
+   public FootstepPlanningToolboxOutputStatus(Random random)
+   {
+      footstepDataList = new FootstepDataListMessage(random);
+      int result = random.nextInt(FootstepPlanningResult.values.length);
+      planningResult = FootstepPlanningResult.values[result];
+   }
+
+   @Override
+   public boolean epsilonEquals(FootstepPlanningToolboxOutputStatus other, double epsilon)
+   {
+      if (!planningResult.equals(other.planningResult))
+         return false;
+      return footstepDataList.epsilonEquals(other.footstepDataList, epsilon);
+   }
+
+   @Override
+   public void set(FootstepPlanningToolboxOutputStatus other)
+   {
+      planningResult = other.planningResult;
+      footstepDataList.destination = other.footstepDataList.destination;
+      footstepDataList.executionMode = other.footstepDataList.executionMode;
+      footstepDataList.footstepDataList = new ArrayList<>();
+      for (FootstepDataMessage footstepData : other.footstepDataList)
+         footstepDataList.footstepDataList.add(new FootstepDataMessage(footstepData));
+      footstepDataList.notes = new String(other.notes);
+      footstepDataList.swingTime = other.footstepDataList.swingTime;
+      footstepDataList.transferTime = other.footstepDataList.transferTime;
+      footstepDataList.uniqueId = other.footstepDataList.uniqueId;
+   }
+
+}
