de4adaeb19596eafcd73820828ac023ac5961029
JerryPratt <jpratt@ihmc.us>
2016-11-09 15:36:44 -0600

    Test cases for PlanarRegionBipedalFootstepPlanner

---
+++ IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/roughTerrainPlanning/PlanarRegionBipedalFootstepPlannerOnFlatTest.java
@@ -0,0 +1,65 @@
+package us.ihmc.footstepPlanning.roughTerrainPlanning;
+
+import org.junit.Test;
+
+import us.ihmc.footstepPlanning.FootstepPlanner;
+import us.ihmc.footstepPlanning.flatGroundPlanning.FootstepPlannerOnFlatGroundTest;
+import us.ihmc.footstepPlanning.graphSearch.PlanarRegionBipedalFootstepPlanner;
+import us.ihmc.footstepPlanning.testTools.PlanningTestTools;
+import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations;
+import us.ihmc.tools.continuousIntegration.IntegrationCategory;
+
+@ContinuousIntegrationAnnotations.ContinuousIntegrationPlan(categories = IntegrationCategory.IN_DEVELOPMENT)
+public class PlanarRegionBipedalFootstepPlannerOnFlatTest extends FootstepPlannerOnFlatGroundTest
+{
+   private static final boolean visualize = true;
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
+   public void testJustStraightLine()
+   {
+      super.testJustStraightLine();
+   }
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
+   public void testStraightLineWithInitialTurn()
+   {
+      super.testStraightLineWithInitialTurn();
+   }
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
+   public void testJustTurnInPlace()
+   {
+      super.testJustTurnInPlace();
+   }
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
+   public void testRandomPoses()
+   {
+      super.testRandomPoses();
+   }
+
+   @Override
+   public FootstepPlanner getPlanner()
+   {
+      PlanarRegionBipedalFootstepPlanner planner = new PlanarRegionBipedalFootstepPlanner();
+
+      planner.setMaxStepReach(1.0);
+
+      double idealFootstepLength = 0.2;
+      double idealFootstepWidth = 0.3;
+      planner.setIdealFootstep(idealFootstepLength, idealFootstepWidth);
+
+      planner.setFeetPolygons(PlanningTestTools.createDefaultFootPolygons());
+      return planner;
+   }
+
+   @Override
+   public boolean visualize()
+   {
+      return visualize;
+   }
+}
