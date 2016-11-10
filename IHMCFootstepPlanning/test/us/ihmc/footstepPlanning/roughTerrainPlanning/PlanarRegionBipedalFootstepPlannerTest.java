d8c9e0bc5a4ebdbf4d23d2b53cbdaba8bdef3501
JerryPratt <jpratt@ihmc.us>
2016-11-10 11:53:35 -0600

    Visualize planner. Clean some stuff up. Still need to actually plan.

--- IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/roughTerrainPlanning/PlanarRegionBipedalFootstepPlannerTest.java
+++ IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/roughTerrainPlanning/PlanarRegionBipedalFootstepPlannerTest.java
@@ -1,10 +1,24 @@
 package us.ihmc.footstepPlanning.roughTerrainPlanning;
 
+import javax.vecmath.Vector3d;
+
 import org.junit.Test;
 
 import us.ihmc.footstepPlanning.FootstepPlanner;
+import us.ihmc.footstepPlanning.graphSearch.BipedalFootstepPlannerListener;
+import us.ihmc.footstepPlanning.graphSearch.BipedalFootstepPlannerNode;
 import us.ihmc.footstepPlanning.graphSearch.PlanarRegionBipedalFootstepPlanner;
+import us.ihmc.footstepPlanning.graphSearch.PlanarRegionBipedalFootstepPlannerVisualizer;
 import us.ihmc.footstepPlanning.testTools.PlanningTestTools;
+import us.ihmc.graphics3DAdapter.graphics.Graphics3DObject;
+import us.ihmc.graphics3DAdapter.graphics.appearances.YoAppearance;
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.geometry.ConvexPolygon2d;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.robotics.robotSide.SideDependentList;
+import us.ihmc.simulationconstructionset.Robot;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.yoUtilities.graphics.YoGraphicsListRegistry;
 import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations;
 import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.tools.continuousIntegration.IntegrationCategory;
@@ -13,6 +27,13 @@ import us.ihmc.tools.continuousIntegration.IntegrationCategory;
 public class PlanarRegionBipedalFootstepPlannerTest extends FootstepPlannerOnRoughTerrainTest
 {
    private static final boolean visualize = true;
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 300000)
+   public void testOnStairCase()
+   {
+      super.testOnStaircase(new Vector3d(0.1, 0.1, -0.1), true);
+   }
 
    @ContinuousIntegrationTest(estimatedDuration = 0.0)
    @Test(timeout = 300000)
@@ -33,13 +54,18 @@ public class PlanarRegionBipedalFootstepPlannerTest extends FootstepPlannerOnRou
    {
       PlanarRegionBipedalFootstepPlanner planner = new PlanarRegionBipedalFootstepPlanner();
 
-      planner.setMaxStepReach(1.0);
+      planner.setMaxStepReach(0.4);
 
-      double idealFootstepLength = 0.2;
-      double idealFootstepWidth = 0.3;
+      double idealFootstepLength = 0.1;
+      double idealFootstepWidth = 0.2;
       planner.setIdealFootstep(idealFootstepLength, idealFootstepWidth);
 
-      planner.setFeetPolygons(PlanningTestTools.createDefaultFootPolygons());
+      SideDependentList<ConvexPolygon2d> footPolygonsInSoleFrame = PlanningTestTools.createDefaultFootPolygons();
+      planner.setFeetPolygons(footPolygonsInSoleFrame);
+
+      SCSPlanarRegionBipedalFootstepPlannerVisualizer visualizer = new SCSPlanarRegionBipedalFootstepPlannerVisualizer(footPolygonsInSoleFrame);
+      planner.setBipedalFootstepPlannerListener(visualizer);
+
       return planner;
    }
 
@@ -47,6 +73,77 @@ public class PlanarRegionBipedalFootstepPlannerTest extends FootstepPlannerOnRou
    public boolean visualize()
    {
       return visualize;
+   }
+
+   public class SCSPlanarRegionBipedalFootstepPlannerVisualizer implements BipedalFootstepPlannerListener
+   {
+      private final SimulationConstructionSet scs;
+      private final PlanarRegionBipedalFootstepPlannerVisualizer footstepPlannerVisualizer;
+
+      public SCSPlanarRegionBipedalFootstepPlannerVisualizer(SideDependentList<ConvexPolygon2d> footPolygonsInSoleFrame)
+      {
+         scs = new SimulationConstructionSet(new Robot("Test"));
+         YoVariableRegistry parentRegistry = scs.getRootRegistry();
+         YoGraphicsListRegistry graphicsListRegistry = new YoGraphicsListRegistry();
+
+         footstepPlannerVisualizer = new PlanarRegionBipedalFootstepPlannerVisualizer(footPolygonsInSoleFrame, parentRegistry, graphicsListRegistry);
+
+         scs.addYoGraphicsListRegistry(graphicsListRegistry);
+         scs.setDT(0.01, 1);
+         scs.startOnAThread();
+      }
+
+      @Override
+      public void nodeSelectedForExpansion(BipedalFootstepPlannerNode nodeToExpand)
+      {
+         footstepPlannerVisualizer.nodeSelectedForExpansion(nodeToExpand);
+         tickAndUpdateSCS();
+      }
+
+      @Override
+      public void nodeForExpansionWasAccepted(BipedalFootstepPlannerNode acceptedNode)
+      {
+         footstepPlannerVisualizer.nodeForExpansionWasAccepted(acceptedNode);
+         tickAndUpdateSCS();
+      }
+
+      @Override
+      public void nodeForExpansionWasRejected(BipedalFootstepPlannerNode rejectedNode)
+      {
+         footstepPlannerVisualizer.nodeForExpansionWasRejected(rejectedNode);
+         tickAndUpdateSCS();
+      }
+
+      @Override
+      public void notifyListenerSolutionWasFound()
+      {
+         scs.cropBuffer();
+         tickAndUpdateSCS();
+      }
+
+      @Override
+      public void notifyListenerSolutionWasNotFound()
+      {
+         scs.cropBuffer();
+         tickAndUpdateSCS();
+      }
+
+      @Override
+      public void planarRegionsListSet(PlanarRegionsList planarRegionsList)
+      {
+         Graphics3DObject graphics3dObject = new Graphics3DObject();
+         graphics3dObject.addPlanarRegionsList(planarRegionsList, YoAppearance.Gold(), YoAppearance.Purple(), YoAppearance.Pink());
+         scs.addStaticLinkGraphics(graphics3dObject);
+
+         tickAndUpdateSCS();
+      }
+
+
+      private void tickAndUpdateSCS()
+      {
+         scs.setTime(scs.getTime() + 1.0);
+         scs.tickAndUpdate();
+      }
    }
 
 }
