ae0bf3fd2f04de406d3931ff80b0eba62526d0d6
Georg Wiedebach <gwiedebach@ihmc.us>
2016-11-10 17:53:34 -0600

    add wiggeling to planner

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/simplePlanners/PlanThenSnapPlanner.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/simplePlanners/PlanThenSnapPlanner.java
@@ -1,26 +1,28 @@
 package us.ihmc.footstepPlanning.simplePlanners;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import us.ihmc.footstepPlanning.FootstepPlan;
 import us.ihmc.footstepPlanning.FootstepPlanner;
 import us.ihmc.footstepPlanning.FootstepPlannerGoal;
 import us.ihmc.footstepPlanning.FootstepPlanningResult;
 import us.ihmc.footstepPlanning.SimpleFootstep;
 import us.ihmc.footstepPlanning.polygonSnapping.PlanarRegionsListPolygonSnapper;
+import us.ihmc.footstepPlanning.polygonWiggling.PolygonWiggler;
 import us.ihmc.robotics.geometry.ConvexPolygon2d;
 import us.ihmc.robotics.geometry.FrameConvexPolygon2d;
 import us.ihmc.robotics.geometry.FramePose;
+import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
 import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
+import us.ihmc.tools.io.printing.PrintTools;
 
 public class PlanThenSnapPlanner implements FootstepPlanner
 {
+   private static final double maxWiggleYaw = Math.toRadians(10.0);
+
    private final FootstepPlanner internalPlanner;
    private final SideDependentList<ConvexPolygon2d> footPolygons;
    private PlanarRegionsList planarRegionsList;
@@ -70,12 +72,38 @@ public class PlanThenSnapPlanner implements FootstepPlanner
 
          PoseReferenceFrame soleFrameBeforeSnapping = new PoseReferenceFrame("SoleFrameBeforeSnapping", solePose);
          FrameConvexPolygon2d footPolygon = new FrameConvexPolygon2d(soleFrameBeforeSnapping, footPolygons.get(footstep.getRobotSide()));
-         footPolygon.changeFrameAndProjectToXYPlane(ReferenceFrame.getWorldFrame());
+         footPolygon.changeFrameAndProjectToXYPlane(ReferenceFrame.getWorldFrame()); // this works if the soleFrames are z up.
+
+         PlanarRegion regionToMoveTo = new PlanarRegion();
          RigidBodyTransform snapTransform =
-               PlanarRegionsListPolygonSnapper.snapPolygonToPlanarRegionsList(footPolygon.getConvexPolygon2d(), planarRegionsList);
+               PlanarRegionsListPolygonSnapper.snapPolygonToPlanarRegionsList(footPolygon.getConvexPolygon2d(), planarRegionsList, regionToMoveTo);
          if (snapTransform == null)
             return FootstepPlanningResult.SNAPPING_FAILED;
          solePose.applyTransform(snapTransform);
+
+         RigidBodyTransform regionToWorld = new RigidBodyTransform();
+         regionToMoveTo.getTransformToWorld(regionToWorld);
+         PoseReferenceFrame regionFrame = new PoseReferenceFrame("RegionFrame", ReferenceFrame.getWorldFrame());
+         regionFrame.setPoseAndUpdate(regionToWorld);
+         PoseReferenceFrame soleFrameBeforeWiggle = new PoseReferenceFrame("SoleFrameBeforeWiggle", solePose);
+
+         RigidBodyTransform soleToRegion = soleFrameBeforeWiggle.getTransformToDesiredFrame(regionFrame);
+         ConvexPolygon2d footPolygonInRegion = new ConvexPolygon2d(footPolygons.get(footstep.getRobotSide()));
+         footPolygonInRegion.applyTransformAndProjectToXYPlane(soleToRegion);
+
+         RigidBodyTransform wiggleTransform = PolygonWiggler.wigglePolygonIntoRegion(footPolygonInRegion, regionToMoveTo, maxWiggleYaw, maxWiggleYaw);
+         if (wiggleTransform == null)
+         {
+            PrintTools.info("Did not find a wiggle transform");
+            solePose.setToNaN();
+         }
+         else
+         {
+            solePose.changeFrame(regionFrame);
+            solePose.applyTransform(wiggleTransform);
+            solePose.changeFrame(ReferenceFrame.getWorldFrame());
+         }
+
          footstep.setSoleFramePose(solePose);
       }
       return result;
