60029f7bae0ff9083ad8c476df97bb66f13fc1b0
JerryPratt <jpratt@ihmc.us>
2016-11-11 11:37:07 -0600

    Remove Single Footstep goal type. Not needed.

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/FootstepPlannerGoalType.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/FootstepPlannerGoalType.java
@@ -2,5 +2,5 @@ package us.ihmc.footstepPlanning;
 
 public enum FootstepPlannerGoalType
 {
-   POSE_BETWEEN_FEET, POSITION_BETWEEN_FEET, SINGLE_FOOTSTEP, DOUBLE_FOOTSTEP;
+   POSE_BETWEEN_FEET, POSITION_BETWEEN_FEET, DOUBLE_FOOTSTEP;
 }
