adfc521751b1725d68208fffbcd8a0a6134cd0d6
Duncan Calvert <dcalvert@ihmc.us>
2016-11-14 09:51:35 -0600

    NONE: Improve Atlas test.

--- Atlas/test/us/ihmc/atlas/roughTerrainWalking/AtlasEndToEndCinderBlockFieldTest.java
+++ Atlas/test/us/ihmc/atlas/roughTerrainWalking/AtlasEndToEndCinderBlockFieldTest.java
@@ -1,5 +1,7 @@
 package us.ihmc.atlas.roughTerrainWalking;
 
+import org.junit.Test;
+
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
@@ -7,11 +9,11 @@ import us.ihmc.avatar.roughTerrainWalking.EndToEndCinderBlockFieldTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
 import us.ihmc.tools.continuousIntegration.IntegrationCategory;
 import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
+import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 
 @ContinuousIntegrationPlan(categories = {IntegrationCategory.FAST, IntegrationCategory.VIDEO})
 public class AtlasEndToEndCinderBlockFieldTest extends EndToEndCinderBlockFieldTest
 {
-
    private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, DRCRobotModel.RobotTarget.SCS, false);
 
    @Override
@@ -30,5 +32,13 @@ public class AtlasEndToEndCinderBlockFieldTest extends EndToEndCinderBlockFieldT
    public String getSimpleRobotName()
    {
       return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+   
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 167.7)
+   @Test(timeout = 840000)
+   public void testWalkingOverCinderBlockField() throws Exception
+   {
+      super.testWalkingOverCinderBlockField();
    }
 }
