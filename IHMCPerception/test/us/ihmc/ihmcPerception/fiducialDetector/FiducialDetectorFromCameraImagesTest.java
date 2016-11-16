27182bad5c0054bcc117e782f9179694b8fd947f
JerryPratt <jpratt@ihmc.us>
2016-11-16 11:45:45 -0600

    Added good asserts to FiducialDetectorFromCameraImagesTest.
    Not sure how this will run in Bamboo since it needs to generate images.
    Not sure if it runs without a GUI or not...

--- IHMCPerception/test/us/ihmc/ihmcPerception/fiducialDetector/FiducialDetectorFromCameraImagesTest.java
+++ IHMCPerception/test/us/ihmc/ihmcPerception/fiducialDetector/FiducialDetectorFromCameraImagesTest.java
@@ -40,8 +40,8 @@ public class FiducialDetectorFromCameraImagesTest
 {
    private static final SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromEnvironmentVariables();
 
-   @ContinuousIntegrationTest(estimatedDuration = 5.0)
-   @Test//(timeout = 30000)
+   @ContinuousIntegrationTest(estimatedDuration = 10.0)
+   @Test(timeout = 30000)
    public void testUsingSimulationConstructionSet()
    {
       double fieldOfView = 0.81;
@@ -52,23 +52,25 @@ public class FiducialDetectorFromCameraImagesTest
       YoGraphicsListRegistry yoGraphicsListRegistry = new YoGraphicsListRegistry();
 
       RigidBodyTransform transformFromReportedToFiducialFrame = new RigidBodyTransform();
-      transformFromReportedToFiducialFrame.setRotationEulerAndZeroTranslation(0.0, 0.0, Math.PI/2.0);
+      transformFromReportedToFiducialFrame.setRotationEulerAndZeroTranslation(0.0, 0.0, Math.PI / 2.0);
 
       final FiducialDetectorFromCameraImages detector = new FiducialDetectorFromCameraImages(transformFromReportedToFiducialFrame, simpleRobotWithCamera.getRobotsYoVariableRegistry(), yoGraphicsListRegistry);
       detector.setTargetIDToLocate(50L);
 
       detector.setFieldOfView(fieldOfView, fieldOfView);
 
-      SimulationConstructionSet scsForDetecting = new SimulationConstructionSet(new Robot[] { simpleRobotWithCamera, simpleBoxRobotWithQRCode });
+      SimulationConstructionSet scsForDetecting = new SimulationConstructionSet(new Robot[] { simpleRobotWithCamera, simpleBoxRobotWithQRCode});
       scsForDetecting.addYoGraphicsListRegistry(yoGraphicsListRegistry);
 
       CameraConfiguration cameraConfiguration = new CameraConfiguration("cameraMount");
       cameraConfiguration.setCameraMount("cameraMount");
-//      cameraConfiguration.setCameraFieldOfView(fieldOfView);
+      //      cameraConfiguration.setCameraFieldOfView(fieldOfView);
       scsForDetecting.setupCamera(cameraConfiguration);
 
       scsForDetecting.setDT(0.001, 10);
       scsForDetecting.setSimulateNoFasterThanRealTime(true);
+
+      scsForDetecting.setSynchronizeGraphicsAndCamerasWhileSimulating(true);
 
       int width = 800;
       int height = 800;
@@ -85,19 +87,19 @@ public class FiducialDetectorFromCameraImagesTest
             cameraJoint.getPosition(cameraPositionInWorld);
             cameraJoint.getRotationToWorld(cameraOrientationInWorldXForward);
 
-//            System.out.println("Received image.");
-//            System.out.println("intrinsicParameters.width = " + intrinsicParameters.width);
-//            System.out.println("intrinsicParameters.height = " + intrinsicParameters.height);
-//            System.out.println("intrinsicParameters.fx = " + intrinsicParameters.fx);
-//            System.out.println("intrinsicParameters.fy = " + intrinsicParameters.fy);
-//            System.out.println("intrinsicParameters.skew = " + intrinsicParameters.skew);
-//            System.out.println("intrinsicParameters.cx = " + intrinsicParameters.cx);
-//            System.out.println("intrinsicParameters.cy = " + intrinsicParameters.cy);
-//            System.out.println("intrinsicParameters.t1 = " + intrinsicParameters.t1);
-//            System.out.println("intrinsicParameters.t2 = " + intrinsicParameters.t2);
-//            System.out.println("intrinsicParameters.radial.length = " + intrinsicParameters.radial.length);
-//            System.out.println("intrinsicParameters.radial[0] = " + intrinsicParameters.radial[0]);
-//            System.out.println("intrinsicParameters.radial[1] = " + intrinsicParameters.radial[1]);
+            //            System.out.println("Received image.");
+            //            System.out.println("intrinsicParameters.width = " + intrinsicParameters.width);
+            //            System.out.println("intrinsicParameters.height = " + intrinsicParameters.height);
+            //            System.out.println("intrinsicParameters.fx = " + intrinsicParameters.fx);
+            //            System.out.println("intrinsicParameters.fy = " + intrinsicParameters.fy);
+            //            System.out.println("intrinsicParameters.skew = " + intrinsicParameters.skew);
+            //            System.out.println("intrinsicParameters.cx = " + intrinsicParameters.cx);
+            //            System.out.println("intrinsicParameters.cy = " + intrinsicParameters.cy);
+            //            System.out.println("intrinsicParameters.t1 = " + intrinsicParameters.t1);
+            //            System.out.println("intrinsicParameters.t2 = " + intrinsicParameters.t2);
+            //            System.out.println("intrinsicParameters.radial.length = " + intrinsicParameters.radial.length);
+            //            System.out.println("intrinsicParameters.radial[0] = " + intrinsicParameters.radial[0]);
+            //            System.out.println("intrinsicParameters.radial[1] = " + intrinsicParameters.radial[1]);
 
             detector.setNewVideoPacket(bufferedImage, cameraPositionInWorld, cameraOrientationInWorldXForward);
          }
@@ -115,12 +117,11 @@ public class FiducialDetectorFromCameraImagesTest
       };
 
       TimestampProvider timestampProvider = new AtomicSettableTimestampProvider();
-      int framesPerSecond = 10;
+      int framesPerSecond = 20;
 
       scsForDetecting.startStreamingVideoData(cameraConfiguration, width, height, videoDataServer, timestampProvider, framesPerSecond);
 
       GoalOrientedTestConductor testConductor = new GoalOrientedTestConductor(scsForDetecting, simulationTestingParameters);
-
 
       BooleanYoVariable fiducialTargetIDHasBeenLocated = (BooleanYoVariable) scsForDetecting.getVariable("fiducialTargetIDHasBeenLocated");
 
@@ -132,7 +133,6 @@ public class FiducialDetectorFromCameraImagesTest
       DoubleYoVariable fiducialReportedPoseWorldFrameQX = (DoubleYoVariable) scsForDetecting.getVariable("fiducialReportedPoseWorldFrameQX");
       DoubleYoVariable fiducialReportedPoseWorldFrameQY = (DoubleYoVariable) scsForDetecting.getVariable("fiducialReportedPoseWorldFrameQY");
       DoubleYoVariable fiducialReportedPoseWorldFrameQZ = (DoubleYoVariable) scsForDetecting.getVariable("fiducialReportedPoseWorldFrameQZ");
-
 
       DoubleYoVariable q_qrCode_x = (DoubleYoVariable) scsForDetecting.getVariable("q_qrCode_x");
       DoubleYoVariable q_qrCode_y = (DoubleYoVariable) scsForDetecting.getVariable("q_qrCode_y");
@@ -151,30 +151,41 @@ public class FiducialDetectorFromCameraImagesTest
          public void variableChanged(YoVariable<?> v)
          {
             double t = time.getDoubleValue();
-            double amp = 0.1;
-            double freqX = 1.0;
-            double freqY = 1.27;
-            double freqZ = 1.92;
+            double ampX = 0.05;
+            double ampY = 0.07;
+            double ampZ = 0.13;
 
-            double wX = amp * Math.sin(2.0 * Math.PI * freqX * t);
-            double wY = amp * Math.sin(2.0 * Math.PI * freqY * t);
-            double wZ = amp * Math.sin(2.0 * Math.PI * freqZ * t);
+            double freqX = 0.23;
+            double freqY = 0.19;
+            double freqZ = 0.37;
+
+            double wX = ampX * Math.sin(2.0 * Math.PI * freqX * t);
+            double wY = ampY * Math.sin(2.0 * Math.PI * freqY * t);
+            double wZ = ampZ * Math.sin(2.0 * Math.PI * freqZ * t);
+
+            double vX = ampX * Math.sin(2.0 * Math.PI * freqX * t);
+            double vY = ampY * Math.sin(2.0 * Math.PI * freqY * t);
+            double vZ = ampZ * Math.sin(2.0 * Math.PI * freqZ * t);
+
+            Vector3d linearVelocityInWorld = new Vector3d(vX, vY, vZ);
+            simpleBoxRobotWithQRCode.setLinearVelocity(linearVelocityInWorld);
 
             Vector3d angularVelocityInBody = new Vector3d(wX, wY, wZ);
-            simpleBoxRobotWithQRCode.setAngularVelocity(angularVelocityInBody );
+            simpleBoxRobotWithQRCode.setAngularVelocity(angularVelocityInBody);
          }
       });
 
-      testConductor.addTerminalGoal(YoVariableTestGoal.doubleGreaterThan(time, 3.0));
+      testConductor.addTerminalGoal(YoVariableTestGoal.doubleGreaterThan(time, 6.0));
 
       testConductor.addSustainGoal(YoVariableTestGoal.booleanEquals(fiducialTargetIDHasBeenLocated, true));
 
-      double okTrackingDeltaPosition = 0.15;
-      testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameX, q_qrCode_x, okTrackingDeltaPosition));
-      testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameY, q_qrCode_y, okTrackingDeltaPosition));
-      testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameZ, q_qrCode_z, okTrackingDeltaPosition));
+      double okTrackingDeltaPositionX = 0.05;
+      double okTrackingDeltaPositionYZ = 0.025;
+      testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameX, q_qrCode_x, okTrackingDeltaPositionX));
+      testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameY, q_qrCode_y, okTrackingDeltaPositionYZ));
+      testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameZ, q_qrCode_z, okTrackingDeltaPositionYZ));
 
-      double okTrackingDeltaQuaternion = 0.1;
+      double okTrackingDeltaQuaternion = 0.08;
       testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameQS, q_qrCode_qs, okTrackingDeltaQuaternion));
       testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameQX, q_qrCode_qx, okTrackingDeltaQuaternion));
       testConductor.addSustainGoal(YoVariableTestGoal.variablesEqual(fiducialReportedPoseWorldFrameQY, q_qrCode_qy, okTrackingDeltaQuaternion));
@@ -231,11 +242,11 @@ public class FiducialDetectorFromCameraImagesTest
          Graphics3DObject qrCodeLinkGraphics = new Graphics3DObject();
          //      qrCodeLinkGraphics.addCoordinateSystem(2.0);
          double cubeLength = 1.0;
-         qrCodeLinkGraphics.translate(0.0, 0.0, -0.99*cubeLength);
+         qrCodeLinkGraphics.translate(0.0, 0.0, -0.99 * cubeLength);
          AppearanceDefinition cubeAppearance = YoAppearance.Texture(Fiducial.FIDUCIAL50.getPathString());
          qrCodeLinkGraphics.addCube(cubeLength * 0.98, cubeLength * 1.01, cubeLength * 0.98, YoAppearance.Yellow());
 
-         boolean[] textureFaces = new boolean[]{true, true, false, false, true, true};
+         boolean[] textureFaces = new boolean[] { true, true, false, false, false, false };
          qrCodeLinkGraphics.translate(0.0, 0.0, -0.01 * cubeLength);
          qrCodeLinkGraphics.addCube(cubeLength, cubeLength, cubeLength, cubeAppearance, textureFaces);
 
@@ -245,13 +256,18 @@ public class FiducialDetectorFromCameraImagesTest
          setGravity(0.0);
 
          qrCodeJoint.setPosition(6.0, 0.0, 2.0);
-         qrCodeJoint.setYawPitchRoll(0.0, -Math.PI/2.0, 0.0);
+         qrCodeJoint.setYawPitchRoll(0.0, -Math.PI / 2.0, 0.0);
          //      qrCodeJoint.setAngularVelocityInBody(new Vector3d(0.0, 0.0, 1.0));
       }
 
       public void setAngularVelocity(Vector3d angularVelocityInBody)
       {
          qrCodeJoint.setAngularVelocityInBody(angularVelocityInBody);
+      }
+
+      public void setLinearVelocity(Vector3d linearVelocityInWorld)
+      {
+         qrCodeJoint.setVelocity(linearVelocityInWorld);
       }
    }
 }
