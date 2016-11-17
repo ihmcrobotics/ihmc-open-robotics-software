8f48f96501849523e87e2348d281cddcb0b16beb
John Carff <jcarff@ihmc.us>
2016-11-17 14:22:02 -0600

    added valid/invalid to the validate behavior and also working on turn valve improvments

--- SimulationConstructionSet/src/us/ihmc/simulationconstructionset/util/environments/DefaultCommonAvatarEnvironment.java
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/util/environments/DefaultCommonAvatarEnvironment.java
@@ -74,7 +74,7 @@ public class DefaultCommonAvatarEnvironment implements CommonAvatarEnvironmentIn
    private static final boolean ADD_CEILING = false;
 
    private static final boolean ADD_FIDUCIAL_ABOVE_VALVE = true;
-   private static final boolean ADD_SOCCER_BALL = false;
+   private static final boolean ADD_SOCCER_BALL = true;
    public static final double SOCCER_BALL_RADIUS = 0.0762;
 
    // private static final double FLOOR_THICKNESS = 0.001;
