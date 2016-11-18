488b2d72198bf647ab8952b252f9c2b14d11489a
Mike Hopkins <michael.a.hopkins@disney.com>
2016-11-18 10:25:31 -0800

    Predict velocity based on heading rate in footstep calculator.

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredFootStep/DesiredFootstepCalculator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredFootStep/DesiredFootstepCalculator.java
@@ -8,9 +8,10 @@ public interface DesiredFootstepCalculator
 {
    public abstract FootstepDataMessage updateAndGetDesiredFootstep(RobotSide supportLegSide);
 
-   public abstract void initializeDesiredFootstep(RobotSide supportLegSide, double timeFromNow);
+   public abstract void initializeDesiredFootstep(RobotSide supportLegSide, double stepDuration);
 
-   public abstract FootstepDataMessage predictFootstepAfterDesiredFootstep(RobotSide supportLegSide, FootstepDataMessage desiredFootstep, double timeFromNow);
+   public abstract FootstepDataMessage predictFootstepAfterDesiredFootstep(RobotSide supportLegSide, FootstepDataMessage desiredFootstep, double timeFromNow,
+         double stepDuration);
 
    public abstract void initialize();
 
--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredHeadingAndVelocity/DesiredVelocityControlModule.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/desiredHeadingAndVelocity/DesiredVelocityControlModule.java
@@ -1,9 +1,12 @@
 package us.ihmc.commonWalkingControlModules.desiredHeadingAndVelocity;
 
 import us.ihmc.robotics.geometry.FrameVector2d;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 
 public interface DesiredVelocityControlModule
 {
+   public abstract ReferenceFrame getReferenceFrame();
+
    public abstract void getDesiredVelocity(FrameVector2d desiredVelocityToPack);
 
    public abstract void updateDesiredVelocity();
