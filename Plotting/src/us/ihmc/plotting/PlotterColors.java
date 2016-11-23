415611d1b1ab58ff92ade38ea8755eab7ba52e5c
Duncan Calvert <dcalvert@ihmc.us>
2016-11-23 17:45:01 -0600

    NRI-175: Fix Plotter grid colors.

--- Plotting/src/us/ihmc/plotting/PlotterColors.java
+++ Plotting/src/us/ihmc/plotting/PlotterColors.java
@@ -8,7 +8,7 @@ public class PlotterColors
    public static final Color BACKGROUND = new Color(180, 220, 240);
    public static final Color GRID_AXIS = Color.GRAY;
    public static final Color GRID_EVERY_1 = new Color(230, 240, 250);
-   public static final Color GRID_EVERY_5 = new Color(180, 210, 230);
+   public static final Color GRID_EVERY_5 = new Color(180, 190, 210);
    public static final Color GRID_EVERY_10 = new Color(180, 190, 210);
    public static final Color SELECTION = Color.RED;
 }
