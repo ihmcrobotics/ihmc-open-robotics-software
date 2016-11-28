a3d8c685b2c263124d7d2892cbccf945444742c5
JerryPratt <jpratt@ihmc.us>
2016-11-28 16:05:22 -0600

    Rename Factory. Add getter for TickAndUpdatable

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/roughTerrain/PlanarRegionBipedalFootstepPlannerVisualizerFactory.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/roughTerrain/PlanarRegionBipedalFootstepPlannerVisualizerFactory.java
@@ -14,37 +14,39 @@ import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.util.PeriodicNonRealtimeThreadScheduler;
 import us.ihmc.util.PeriodicThreadScheduler;
 
-public class YoVariableServerPlanarRegionBipedalFootstepPlannerVisualizer
-{   
+public class PlanarRegionBipedalFootstepPlannerVisualizerFactory
+{
    public static PlanarRegionBipedalFootstepPlannerVisualizer createWithYoVariableServer(double dtForViz, FullRobotModel fullRobotModel,
                                                                                          LogModelProvider logModelProvider,
                                                                                          SideDependentList<ConvexPolygon2d> footPolygonsInSoleFrame)
    {
-      YoVariableRegistry registry = new YoVariableRegistry(YoVariableServerPlanarRegionBipedalFootstepPlannerVisualizer.class.getSimpleName());
+      YoVariableRegistry registry = new YoVariableRegistry(PlanarRegionBipedalFootstepPlannerVisualizerFactory.class.getSimpleName());
       YoGraphicsListRegistry graphicsListRegistry = new YoGraphicsListRegistry();
 
-      PlanarRegionBipedalFootstepPlannerVisualizer footstepPlannerVisualizer = new PlanarRegionBipedalFootstepPlannerVisualizer(footPolygonsInSoleFrame, registry, graphicsListRegistry);
-      
-      
+      PlanarRegionBipedalFootstepPlannerVisualizer footstepPlannerVisualizer = new PlanarRegionBipedalFootstepPlannerVisualizer(footPolygonsInSoleFrame,
+                                                                                                                                registry, graphicsListRegistry);
+
       PeriodicThreadScheduler scheduler = new PeriodicNonRealtimeThreadScheduler("PlannerScheduler");
-      YoVariableServer yoVariableServer = new YoVariableServer(YoVariableServerPlanarRegionBipedalFootstepPlannerVisualizer.class, scheduler, logModelProvider, LogSettings.FOOTSTEP_PLANNER,
-                                                               dtForViz);
+      YoVariableServer yoVariableServer = new YoVariableServer(PlanarRegionBipedalFootstepPlannerVisualizerFactory.class, scheduler, logModelProvider,
+                                                               LogSettings.FOOTSTEP_PLANNER, dtForViz);
       footstepPlannerVisualizer.setTickAndUpdatable(yoVariableServer);
 
       yoVariableServer.setMainRegistry(registry, fullRobotModel, graphicsListRegistry);
       yoVariableServer.start();
-            
+
       return footstepPlannerVisualizer;
    }
 
-   public static PlanarRegionBipedalFootstepPlannerVisualizer createWithSimulationConstructionSet(double dtForViz, SideDependentList<ConvexPolygon2d> footPolygonsInSoleFrame)
+   public static PlanarRegionBipedalFootstepPlannerVisualizer createWithSimulationConstructionSet(double dtForViz,
+                                                                                                  SideDependentList<ConvexPolygon2d> footPolygonsInSoleFrame)
    {
-      YoVariableRegistry registry = new YoVariableRegistry(YoVariableServerPlanarRegionBipedalFootstepPlannerVisualizer.class.getSimpleName());
+      YoVariableRegistry registry = new YoVariableRegistry(PlanarRegionBipedalFootstepPlannerVisualizerFactory.class.getSimpleName());
       YoGraphicsListRegistry graphicsListRegistry = new YoGraphicsListRegistry();
 
-      PlanarRegionBipedalFootstepPlannerVisualizer footstepPlannerVisualizer = new PlanarRegionBipedalFootstepPlannerVisualizer(footPolygonsInSoleFrame, registry, graphicsListRegistry);
+      PlanarRegionBipedalFootstepPlannerVisualizer footstepPlannerVisualizer = new PlanarRegionBipedalFootstepPlannerVisualizer(footPolygonsInSoleFrame,
+                                                                                                                                registry, graphicsListRegistry);
       SimulationConstructionSet scs = new SimulationConstructionSet(new Robot("Test"));
-      
+
       footstepPlannerVisualizer.setTickAndUpdatable(scs);
 
       scs.changeBufferSize(32000);
@@ -54,8 +56,6 @@ public class YoVariableServerPlanarRegionBipedalFootstepPlannerVisualizer
 
       scs.setDT(dtForViz, 1);
 
-      scs.setCameraFix(-6.0, 0.0, 0.0);
-      scs.setCameraPosition(-11.0, 0.0, 8.0);
       scs.setGroundVisible(false);
       scs.startOnAThread();
 
