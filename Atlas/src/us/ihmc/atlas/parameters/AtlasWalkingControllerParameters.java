3bf9cdb669c5dede408431a6a05c0d9259dbba24
Georg Wiedebach <gwiedebach@ihmc.us>
2016-11-29 14:49:16 -0600

    move the boolean that decided whether the toe point is controlled during swing to the walking controller parameters activate toe control for Atlas by default

--- Atlas/src/us/ihmc/atlas/parameters/AtlasWalkingControllerParameters.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasWalkingControllerParameters.java
@@ -7,8 +7,6 @@ import javax.vecmath.Vector3d;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
-import us.ihmc.robotics.partNames.NeckJointName;
-import us.ihmc.robotics.partNames.SpineJointName;
 import us.ihmc.atlas.AtlasJointMap;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
@@ -26,6 +24,8 @@ import us.ihmc.robotics.controllers.YoSymmetricSE3PIDGains;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
 import us.ihmc.robotics.geometry.RotationTools;
+import us.ihmc.robotics.partNames.NeckJointName;
+import us.ihmc.robotics.partNames.SpineJointName;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.sensorProcessing.stateEstimation.FootSwitchType;
@@ -1052,5 +1052,12 @@ public class AtlasWalkingControllerParameters extends WalkingControllerParameter
    {
       boolean realRobot = target == DRCRobotModel.RobotTarget.REAL_ROBOT;
       return realRobot ? 0.8 : 0.5; // Robert: 0.8
+   }
+
+   /** {@inheritDoc} */
+   @Override
+   public boolean controlToeDuringSwing()
+   {
+      return true;
    }
 }
