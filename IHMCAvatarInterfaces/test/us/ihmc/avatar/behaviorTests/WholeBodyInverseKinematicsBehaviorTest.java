17532974c80adc65529b1fca323576ff045cb312
Edoardo Amadei <eamadei@ihmc.us>
2016-11-29 10:00:48 -0600

    Added check for hand position and implemented @After method

--- IHMCAvatarInterfaces/test/us/ihmc/avatar/behaviorTests/WholeBodyInverseKinematicsBehaviorTest.java
+++ IHMCAvatarInterfaces/test/us/ihmc/avatar/behaviorTests/WholeBodyInverseKinematicsBehaviorTest.java
@@ -9,7 +9,6 @@ import javax.vecmath.Quat4d;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import us.ihmc.avatar.MultiRobotTestInterface;
@@ -17,6 +16,7 @@ import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.networkProcessor.kinematicsToolboxModule.KinematicsToolboxModule;
 import us.ihmc.avatar.testTools.DRCBehaviorTestHelper;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.manipulation.individual.HandControlModule;
+import us.ihmc.communication.packetCommunicator.PacketCommunicator;
 import us.ihmc.communication.packets.PacketDestination;
 import us.ihmc.communication.util.NetworkPorts;
 import us.ihmc.humanoidBehaviors.behaviors.primitives.WholeBodyInverseKinematicsBehavior;
@@ -41,6 +41,8 @@ public abstract class WholeBodyInverseKinematicsBehaviorTest implements MultiRob
    private static final SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromEnvironmentVariables();
    private boolean isKinematicsToolboxVisualizerEnabled = false;
    private DRCBehaviorTestHelper drcBehaviorTestHelper;
+   private KinematicsToolboxModule kinematicsToolboxModule;
+   private PacketCommunicator toolboxCommunicator;
 
    @Before
    public void showMemoryUsageBeforeTest()
@@ -62,21 +64,45 @@ public abstract class WholeBodyInverseKinematicsBehaviorTest implements MultiRob
          drcBehaviorTestHelper.closeAndDispose();
          drcBehaviorTestHelper = null;
       }
+      
+      if (kinematicsToolboxModule != null)
+      {
+         kinematicsToolboxModule.destroy();
+         kinematicsToolboxModule = null;
+      }
+
+      if (toolboxCommunicator != null)
+      {
+         toolboxCommunicator.close();
+         toolboxCommunicator.closeConnection();
+         toolboxCommunicator = null;
+      }
+      ThreadTools.sleep(1000);
 
       GlobalTimer.clearTimers();
-      
+
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
    }
-   @Ignore
-   @ContinuousIntegrationTest(estimatedDuration = 27.0)
+
+   @Before
+   public void setUp() throws IOException
+   {
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
+
+      CommonAvatarEnvironmentInterface envrionment = new FlatGroundEnvironment();
+
+      drcBehaviorTestHelper = new DRCBehaviorTestHelper(envrionment, getSimpleRobotName(), null, simulationTestingParameters, getRobotModel());
+
+      setupKinematicsToolboxModule();
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 30.0)
    @Test(timeout = 160000)
    public void testSolvingForAHandPose() throws SimulationExceededMaximumTimeException, IOException
    {
       BambooTools.reportTestStartedMessage(simulationTestingParameters.getShowWindows());
 
-      CommonAvatarEnvironmentInterface envrionment = new FlatGroundEnvironment();
-      drcBehaviorTestHelper = new DRCBehaviorTestHelper(envrionment, getSimpleRobotName(), null, simulationTestingParameters, getRobotModel());
-      setupKinematicsToolboxModule();
+      //setupKinematicsToolboxModule();
       boolean success = drcBehaviorTestHelper.simulateAndBlockAndCatchExceptions(1.0);
       assertTrue(success);
 
@@ -115,7 +141,7 @@ public abstract class WholeBodyInverseKinematicsBehaviorTest implements MultiRob
          ThreadTools.sleep(100);
       }
 
-      assertFalse(ik.hasSolverFailed());
+      assertFalse("Bad solution: " + ik.getSolutionQuality(), ik.hasSolverFailed());
 
       success = drcBehaviorTestHelper.simulateAndBlockAndCatchExceptions(1.0);
       assertTrue(success);
@@ -132,28 +158,27 @@ public abstract class WholeBodyInverseKinematicsBehaviorTest implements MultiRob
 
       FrameOrientation finalPelvisOrientation = new FrameOrientation(pelvisControlFrame);
       finalPelvisOrientation.changeFrame(ReferenceFrame.getWorldFrame());
-      
+
       double angleEpsilon = Math.toRadians(10);
 
       assertTrue(isOrientationEqual(initialChestOrientation.getQuaternion(), finalChestOrientation.getQuaternion(), angleEpsilon));
       assertTrue(isOrientationEqual(initialPelvisOrientation.getQuaternion(), finalPelvisOrientation.getQuaternion(), angleEpsilon));
 
       assertTrue("Expect: " + desiredHandPose + "\nActual: " + currentHandPose, currentHandPose.epsilonEquals(desiredHandPose, 1.0e-2));
-   }
-   
 
-   @ContinuousIntegrationTest(estimatedDuration = 27.0)
+      BambooTools.reportTestFinishedMessage(simulationTestingParameters.getShowWindows());
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 30.0)
    @Test(timeout = 160000)
    public void testSolvingForBothHandPoses() throws SimulationExceededMaximumTimeException, IOException
    {
       BambooTools.reportTestStartedMessage(simulationTestingParameters.getShowWindows());
 
-      CommonAvatarEnvironmentInterface envrionment = new FlatGroundEnvironment();
-      drcBehaviorTestHelper = new DRCBehaviorTestHelper(envrionment, getSimpleRobotName(), null, simulationTestingParameters, getRobotModel());
-      setupKinematicsToolboxModule();
+      //setupKinematicsToolboxModule();
       boolean success = drcBehaviorTestHelper.simulateAndBlockAndCatchExceptions(1.0);
       assertTrue(success);
-      
+
       drcBehaviorTestHelper.updateRobotModel();
 
       WholeBodyInverseKinematicsBehavior ik = new WholeBodyInverseKinematicsBehavior(getRobotModel(), drcBehaviorTestHelper.getYoTime(),
@@ -180,7 +205,7 @@ public abstract class WholeBodyInverseKinematicsBehaviorTest implements MultiRob
          ThreadTools.sleep(100);
       }
 
-      assertFalse(ik.hasSolverFailed());
+      assertFalse("Bad solution: " + ik.getSolutionQuality(), ik.hasSolverFailed());
 
       success = drcBehaviorTestHelper.simulateAndBlockAndCatchExceptions(1.0);
       assertTrue(success);
@@ -189,25 +214,27 @@ public abstract class WholeBodyInverseKinematicsBehaviorTest implements MultiRob
       currentHandPoseR.changeFrame(ReferenceFrame.getWorldFrame());
       FramePose currentHandPoseL = new FramePose(handControlFrameL);
       currentHandPoseL.changeFrame(ReferenceFrame.getWorldFrame());
-      
+
       AxisAngle4d leftAngle = new AxisAngle4d();
       AxisAngle4d rightAngle = new AxisAngle4d();
-      
+
       desiredHandPoseL.getAxisAngleRotationToOtherPose(currentHandPoseL, leftAngle);
       desiredHandPoseR.getAxisAngleRotationToOtherPose(currentHandPoseR, rightAngle);
-      
+
       double angleEpsilon = Math.toRadians(2);
-      
+
       assertTrue(Math.abs(leftAngle.angle) < angleEpsilon);
       assertTrue(Math.abs(rightAngle.angle) < angleEpsilon);
-      
-//      double leftPosition = desiredHandPoseL.getPositionDistance(currentHandPoseL);
-//      double rightPosition = desiredHandPoseR.getPositionDistance(currentHandPoseR);
-//      
-//      double positionEpsilon = 1.0e-2;
-//      System.out.println(leftPosition);
-//      assertTrue(Math.abs(leftPosition) < positionEpsilon);
-//      assertTrue(Math.abs(rightPosition) < positionEpsilon);
+
+      double leftPosition = desiredHandPoseL.getPositionDistance(currentHandPoseL);
+      double rightPosition = desiredHandPoseR.getPositionDistance(currentHandPoseR);
+
+      double positionEpsilon = 1.0e-2;
+      System.out.println(leftPosition);
+      assertTrue(Math.abs(leftPosition) < positionEpsilon);
+      assertTrue(Math.abs(rightPosition) < positionEpsilon);
+
+      BambooTools.reportTestFinishedMessage(simulationTestingParameters.getShowWindows());
    }
 
    private boolean isOrientationEqual(Quat4d initialQuat, Quat4d finalQuat, double angleEpsilon)
@@ -218,14 +245,14 @@ public abstract class WholeBodyInverseKinematicsBehaviorTest implements MultiRob
       AxisAngle4d angleDifference = new AxisAngle4d();
       angleDifference.set(quatDifference);
       AngleTools.trimAngleMinusPiToPi(angleDifference.getAngle());
-      
+
       return Math.abs(angleDifference.getAngle()) < angleEpsilon;
    }
 
    private void setupKinematicsToolboxModule() throws IOException
    {
       DRCRobotModel robotModel = getRobotModel();
-      new KinematicsToolboxModule(robotModel.createFullRobotModel(), robotModel.getLogModelProvider(), isKinematicsToolboxVisualizerEnabled);
-      drcBehaviorTestHelper.createAndStartPacketCommunicator(NetworkPorts.KINEMATICS_TOOLBOX_MODULE_PORT, PacketDestination.KINEMATICS_TOOLBOX_MODULE);
+      kinematicsToolboxModule = new KinematicsToolboxModule(robotModel.createFullRobotModel(), robotModel.getLogModelProvider(), isKinematicsToolboxVisualizerEnabled);
+      toolboxCommunicator = drcBehaviorTestHelper.createAndStartPacketCommunicator(NetworkPorts.KINEMATICS_TOOLBOX_MODULE_PORT, PacketDestination.KINEMATICS_TOOLBOX_MODULE);
    }
 }
