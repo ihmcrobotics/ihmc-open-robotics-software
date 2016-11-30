3766d89f4657f2db8fb8207bcb539884b7324f33
Operator Computer <OperatorComputer@unknown>
2016-11-30 11:48:07 -0600

    Update planner and behaviors for walking over cinders with Atlas.

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/graphSearch/BipedalFootstepPlannerListener.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/graphSearch/BipedalFootstepPlannerListener.java
@@ -1,5 +1,6 @@
 package us.ihmc.footstepPlanning.graphSearch;
 
+import us.ihmc.footstepPlanning.FootstepPlan;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
 
@@ -8,7 +9,7 @@ public interface BipedalFootstepPlannerListener
    public abstract void nodeSelectedForExpansion(BipedalFootstepPlannerNode nodeToExpand);
    public abstract void nodeForExpansionWasAccepted(BipedalFootstepPlannerNode acceptedNode);
    public abstract void nodeForExpansionWasRejected(BipedalFootstepPlannerNode rejectedNode, BipedalFootstepPlannerNodeRejectionReason reason);
-   public abstract void notifyListenerSolutionWasFound();
+   public abstract void notifyListenerSolutionWasFound(FootstepPlan footstepPlan);
    public abstract void notifyListenerSolutionWasNotFound();
    public abstract void planarRegionsListSet(PlanarRegionsList planarRegionsList);
    public abstract void goalWasSet(RigidBodyTransform goalLeftFootPose, RigidBodyTransform goalRightFootPose);
