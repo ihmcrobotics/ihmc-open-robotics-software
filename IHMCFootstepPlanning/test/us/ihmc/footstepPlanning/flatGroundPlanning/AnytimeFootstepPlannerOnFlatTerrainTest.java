34a07dbcad536ac75107e1edcdb3c1b499d94c69
Stephen McCrory <smccrory@ihmc.us>
2016-11-29 22:54:40 -0600

    Adding test for executing footsteps from an initial plan

--- IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/flatGroundPlanning/AnytimeFootstepPlannerOnFlatTerrainTest.java
+++ IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/flatGroundPlanning/AnytimeFootstepPlannerOnFlatTerrainTest.java
@@ -44,7 +44,7 @@ public class AnytimeFootstepPlannerOnFlatTerrainTest implements PlanningTest
 
       SideDependentList<FramePose> goalPoses = new SideDependentList<>();
 
-      for(RobotSide robotSide : RobotSide.values)
+      for (RobotSide robotSide : RobotSide.values)
       {
          FramePose footstepGoalPose = new FramePose(goalPose);
          footstepGoalPose.translate(0.0, robotSide.negateIfRightSide(0.2), 0.0);
@@ -55,19 +55,19 @@ public class AnytimeFootstepPlannerOnFlatTerrainTest implements PlanningTest
       PlanningTestTools.configureAnytimePlannerRunnable(anytimePlanner, initialStanceFootPose, initialStanceSide, goalPose, flatTerrain);
 
       new Thread(anytimePlanner).start();
-      
+
       double closestFootstepToGoal = Double.MAX_VALUE;
-      int numIterations = 100;
+      int numIterations = 10;
       FootstepPlan bestPlanYet = anytimePlanner.getBestPlanYet();
       FramePose lastFootstepPose = new FramePose();
 
-      for(int i = 0; i < numIterations; i++)
+      for (int i = 0; i < numIterations; i++)
       {
-         ThreadTools.sleep(10);
+         ThreadTools.sleep(100);
          bestPlanYet = anytimePlanner.getBestPlanYet();
 
          int numberOfFootsteps = bestPlanYet.getNumberOfSteps();
-         if(numberOfFootsteps < 2)
+         if (numberOfFootsteps < 2)
             continue;
 
          SimpleFootstep lastFootstep = bestPlanYet.getFootstep(numberOfFootsteps - 1);
@@ -88,6 +88,53 @@ public class AnytimeFootstepPlannerOnFlatTerrainTest implements PlanningTest
       {
          PlanningTestTools.visualizeAndSleep(flatTerrain, bestPlanYet, goalPose);
       }
+   }
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
+   public void testPlannerAdjustsPlanAfterExecutingFootstep()
+   {
+      PlanarRegionsList flatTerrain = PlanarRegionsListExamples.generateFlatGround(20.0, 20.0);
+
+      FramePose initialStanceFootPose = new FramePose(worldFrame);
+      initialStanceFootPose.setPosition(0.0, -0.7, 0.0);
+      RobotSide initialStanceSide = RobotSide.RIGHT;
+
+      FramePose goalPose = new FramePose(worldFrame);
+      goalPose.setPosition(7.0, 0.7, 0.0);
+
+      SideDependentList<FramePose> goalPoses = new SideDependentList<>();
+
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         FramePose footstepGoalPose = new FramePose(goalPose);
+         footstepGoalPose.translate(0.0, robotSide.negateIfRightSide(0.2), 0.0);
+         goalPoses.put(robotSide, footstepGoalPose);
+      }
+
+      SimplePlanarRegionBipedalAnytimeFootstepPlanner anytimePlanner = getPlanner();
+      PlanningTestTools.configureAnytimePlannerRunnable(anytimePlanner, initialStanceFootPose, initialStanceSide, goalPose, flatTerrain);
+
+      new Thread(anytimePlanner).start();
+      ThreadTools.sleep(500);
+
+      FootstepPlan bestPlanYet = anytimePlanner.getBestPlanYet();
+      assertTrue(bestPlanYet.getNumberOfSteps() > 3);
+
+      SimpleFootstep initialFootstepFirstPlan = bestPlanYet.getFootstep(0);
+      SimpleFootstep firstFootstepFirstPlan = bestPlanYet.getFootstep(1);
+      SimpleFootstep secondFootstepFirstPlan = bestPlanYet.getFootstep(2);
+
+      anytimePlanner.executingFootstep(initialFootstepFirstPlan);
+      anytimePlanner.executingFootstep(firstFootstepFirstPlan);
+      anytimePlanner.executingFootstep(secondFootstepFirstPlan);
+
+      ThreadTools.sleep(500);
+      bestPlanYet = anytimePlanner.getBestPlanYet();
+      assertTrue(bestPlanYet.getNumberOfSteps() > 2);
+
+      SimpleFootstep initialFootstepSecondPlan = bestPlanYet.getFootstep(0);
+      assertTrue(secondFootstepFirstPlan.epsilonEquals(initialFootstepSecondPlan, 1e-12));
    }
 
    @Override
@@ -113,7 +160,8 @@ public class AnytimeFootstepPlannerOnFlatTerrainTest implements PlanningTest
 
       if (visualize)
       {
-         PlanarRegionBipedalFootstepPlannerVisualizer visualizer = SCSPlanarRegionBipedalFootstepPlannerVisualizer.createWithSimulationConstructionSet(1.0, footPolygonsInSoleFrame);
+         PlanarRegionBipedalFootstepPlannerVisualizer visualizer = SCSPlanarRegionBipedalFootstepPlannerVisualizer
+               .createWithSimulationConstructionSet(1.0, footPolygonsInSoleFrame);
          planner.setBipedalFootstepPlannerListener(visualizer);
       }
 
