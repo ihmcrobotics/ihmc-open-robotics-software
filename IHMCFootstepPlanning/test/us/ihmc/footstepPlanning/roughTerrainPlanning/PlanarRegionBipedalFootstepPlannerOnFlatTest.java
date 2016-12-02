eb62e58d4f64015f9d6045871fb28a9c3fd547dd
JerryPratt <jpratt@ihmc.us>
2016-12-01 18:02:13 -0600

    Failing test case. Now we fix...

--- IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/roughTerrainPlanning/PlanarRegionBipedalFootstepPlannerOnFlatTest.java
+++ IHMCFootstepPlanning/test/us/ihmc/footstepPlanning/roughTerrainPlanning/PlanarRegionBipedalFootstepPlannerOnFlatTest.java
@@ -30,6 +30,14 @@ public class PlanarRegionBipedalFootstepPlannerOnFlatTest extends FootstepPlanne
    @Override
    @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
    @Test(timeout = 300000)
+   public void testATightTurn()
+   {
+      super.testATightTurn(true);
+   }
+
+   @Override
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 300000)
    public void testStraightLineWithInitialTurn()
    {
       super.testStraightLineWithInitialTurn(true);
@@ -63,7 +71,7 @@ public class PlanarRegionBipedalFootstepPlannerOnFlatTest extends FootstepPlanne
       parameters.setMaximumStepZ(0.25);
       parameters.setMaximumStepXWhenForwardAndDown(0.25);
       parameters.setMaximumStepZWhenForwardAndDown(0.25);
-      parameters.setMaximumStepYaw(0.25);
+      parameters.setMaximumStepYaw(0.05);
       parameters.setMinimumStepWidth(0.15);
       parameters.setMinimumFootholdPercent(0.8);
 
