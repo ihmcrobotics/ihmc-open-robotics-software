99e2228679a10a0566b8d72e02f58fcb8eb33f2e
Antonio Bento <abento@ihmc.us>
2016-12-05 11:55:50 -0600

    New controller doing ICP based balancing OK: added angular momentum feedback (knowledge about need to slow down)

--- ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippyICPBasedController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippyICPBasedController.java
@@ -18,20 +18,18 @@ import us.ihmc.simulationconstructionset.robotController.SimpleRobotController;
 public class SkippyICPBasedController extends SimpleRobotController
 {
    private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
-   private static final double Z0 = 1.216;
    private final SkippyRobot skippy;
 
    private final double dt;
-   private final PIDController hipController = new PIDController("hipController", registry);
 
-   private final DoubleYoVariable omega0 = new DoubleYoVariable("omega0", registry);
    private final DoubleYoVariable kCapture = new DoubleYoVariable("kCapture", registry);
-   private final DoubleYoVariable hipSetpoint = new DoubleYoVariable("hipSetpoint", registry);
+   private final DoubleYoVariable kMomentum = new DoubleYoVariable("kMomentum", registry);
 
    private final IntegerYoVariable tickCounter = new IntegerYoVariable("tickCounter", registry);
    private final IntegerYoVariable ticksForDesiredForce = new IntegerYoVariable("ticksForDesiredForce", registry);
    private final DoubleYoVariable angleBetweenDesiredAndActual = new DoubleYoVariable("angleBetweenDesiredAndActual", registry);
    private final PIDController angleController = new PIDController("angleController", registry);
+   private final FrameVector angularMomentum = new FrameVector(worldFrame);
 
    private final FramePoint com = new FramePoint(worldFrame);
    private final FrameVector comVelocity = new FrameVector(worldFrame);
@@ -53,18 +51,15 @@ public class SkippyICPBasedController extends SimpleRobotController
    private final YoFrameVector groundReachtionViz = new YoFrameVector("GroundReaction", ReferenceFrame.getWorldFrame(), registry);
    private final YoFrameVector hipToFootDirectionViz = new YoFrameVector("HipToFootDirection", ReferenceFrame.getWorldFrame(), registry);
    private final YoFrameVector hipAxisViz = new YoFrameVector("HipAxis", ReferenceFrame.getWorldFrame(), registry);
+   private final YoFrameVector angularMomentumViz = new YoFrameVector("AngularMomentum", ReferenceFrame.getWorldFrame(), registry);
 
    public SkippyICPBasedController(SkippyRobot skippy, double dt, YoGraphicsListRegistry yoGraphicsListRegistries)
    {
       this.skippy = skippy;
       this.dt = dt;
 
-      omega0.set(Math.sqrt(Z0 / Math.abs(skippy.getGravity())));
-      kCapture.set(1.5);
-
-      hipSetpoint.set(skippy.getHipJoint().getQ());
-      hipController.setProportionalGain(0.0);
-      hipController.setDerivativeGain(0.0);
+      kCapture.set(7.5);
+      kMomentum.set(0.3);
 
       ticksForDesiredForce.set(100);
       angleController.setProportionalGain(20.0);
@@ -110,18 +105,19 @@ public class SkippyICPBasedController extends SimpleRobotController
    @Override
    public void doControl()
    {
-      skippy.computeComAndICP(omega0.getDoubleValue(), com, comVelocity, icp);
+      skippy.computeComAndICP(com, comVelocity, icp, angularMomentum);
       skippy.computeFootContactForce(groundReaction.getVector());
       footLocation.set(skippy.computeFootLocation());
       cmpFromIcpDynamics(icp, footLocation, desiredCMP);
 
       if (tickCounter.getIntegerValue() > ticksForDesiredForce.getIntegerValue())
       {
+         desiredCMP.setY(desiredCMP.getY() - kMomentum.getDoubleValue() * angularMomentum.getX());
+         
          desiredGroundReaction.sub(com, desiredCMP);
          desiredGroundReaction.normalize();
          double reactionModulus = Math.abs(skippy.getGravity()) * skippy.getMass() / desiredGroundReaction.getZ();
          desiredGroundReaction.scale(reactionModulus);
-
          tickCounter.set(0);
       }
       tickCounter.increment();
@@ -132,10 +128,6 @@ public class SkippyICPBasedController extends SimpleRobotController
 
       skippy.getHipJointAxis(hipAxis);
       double balanceTorque = computeJointTorque(desiredGroundReaction, hipToFootDirection, hipAxis);
-//      double setpointTorque = hipController.compute(skippy.getQ_hip().getDoubleValue(), hipSetpoint.getDoubleValue(), skippy.getQd_hip().getDoubleValue(), 0.0, dt);
-//      double weightAboveTorque = torqueOnHipFromTheWeightAboveIt();
-//      skippy.getHipJoint().setTau(balanceTorque + setpointTorque + weightAboveTorque);
-
       double angleFeedback = computeAngleFeedback();
       skippy.getHipJoint().setTau(angleFeedback + balanceTorque);
 
@@ -172,40 +164,7 @@ public class SkippyICPBasedController extends SimpleRobotController
       hipLocationViz.set(worldToHip);
       hipToFootDirectionViz.set(hipToFootDirection);
       hipAxisViz.set(hipAxis);
-   }
-
-   private double torqueOnHipFromTheWeightAboveIt()
-   {
-      FrameVector torqueFromHip = new FrameVector(worldFrame);
-      FrameVector torqueFromShoulder = new FrameVector(worldFrame);
-      FrameVector torqueFromHipAndShoulder = new FrameVector(worldFrame);
-      FrameVector shoulderWeightVector = new FrameVector(worldFrame);
-      FrameVector hipWeightVector = new FrameVector(worldFrame);
-      FrameVector worldToHipCom = new FrameVector(worldFrame);
-      FrameVector hipCom = new FrameVector(worldFrame);
-      FrameVector worldToShoulder = new FrameVector(worldFrame);
-      /*
-       * Shoulder
-       */
-      shoulderWeightVector.set(0.0,0.0,9.81*skippy.getShoulderMass());
-      skippy.getShoulderJoint().getTranslationToWorld(worldToShoulder .getVector());
-      worldToShoulder.sub(footLocation);
-      torqueFromShoulder.cross(worldToShoulder,shoulderWeightVector);
-      /*
-       * Hip
-       */
-      hipWeightVector.set(0.0,0.0,9.81*skippy.getHipMass());
-      hipCom.set(worldToShoulder);
-      hipCom.sub(worldToHipCom);
-      hipCom.scale(1.0/2.0);
-      skippy.getHipJoint().getTranslationToWorld(worldToHipCom.getVector());
-      worldToHipCom.add(hipCom);
-      worldToHipCom.sub(footLocation);
-      torqueFromHip.cross(worldToHipCom,hipWeightVector);
-      torqueFromHipAndShoulder.set(torqueFromShoulder);
-      torqueFromHipAndShoulder.add(torqueFromHip);
-      double torqueOnHipFromWeightAbove = torqueFromHipAndShoulder.dot(hipAxis);
-      return torqueOnHipFromWeightAbove;
+      angularMomentumViz.set(angularMomentum);
    }
 
    /**
