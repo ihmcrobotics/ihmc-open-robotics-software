32b88bee267e589a4a05913859fa7c07676abda2
Georg Wiedebach <gwiedebach@ihmc.us>
2016-12-07 17:53:51 -0600

    clean up controller modes for skippy and add pushing unit test

--- ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippySimulation.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/skippy/SkippySimulation.java
@@ -3,7 +3,6 @@ package us.ihmc.exampleSimulations.skippy;
 import us.ihmc.exampleSimulations.skippy.SkippyRobot.RobotType;
 import us.ihmc.graphics3DDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.robotics.controllers.ControllerFailureListener;
-import us.ihmc.robotics.robotController.RobotController;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.gui.tools.VisualizerUtils;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner;
@@ -19,15 +18,20 @@ public class SkippySimulation
 
    private final SimulationConstructionSet sim;
    private final BlockingSimulationRunner blockingSimulationRunner;
+   private final SkippyRobot skippy;
 
-   private static final boolean USE_ICP_BASED_CONTROLLER = true;
+   public enum SkippyControllerMode
+   {
+      ICP_BASED,
+      STATE_FEEDBACK,
+      NONE
+   }
 
-   public SkippySimulation()
+   public SkippySimulation(SkippyControllerMode controllerMode)
    {
       // 0 - acrobot, 1 - skippy
       RobotType robotType = RobotType.SKIPPY;
-
-      SkippyRobot skippy = new SkippyRobot(robotType);
+      skippy = new SkippyRobot(robotType);
 
       sim = new SimulationConstructionSet(skippy);
       sim.setGroundVisible(true);
@@ -38,12 +42,20 @@ public class SkippySimulation
       boolean showOverheadView = true;
       YoGraphicsListRegistry yoGraphicsListRegistry = new YoGraphicsListRegistry();
 
-      RobotController controller = null;
-      if (USE_ICP_BASED_CONTROLLER)
-         controller = new SkippyICPBasedController(skippy, controlDT, yoGraphicsListRegistry);
-      else
-         controller = new SkippyController(skippy, robotType, "skippyController", controlDT, yoGraphicsListRegistry);
-      skippy.setController(controller);
+      switch (controllerMode)
+      {
+      case ICP_BASED:
+         SkippyICPBasedController icpBasedController = new SkippyICPBasedController(skippy, controlDT, yoGraphicsListRegistry);
+         skippy.setController(icpBasedController);
+         break;
+      case STATE_FEEDBACK:
+         SkippyController stateFeedbackController = new SkippyController(skippy, robotType, "skippyController", controlDT, yoGraphicsListRegistry);
+         skippy.setController(stateFeedbackController);
+         break;
+      case NONE:
+      default:
+         break;
+      }
 
       VisualizerUtils.createOverheadPlotter(sim, showOverheadView, yoGraphicsListRegistry);
       sim.addYoGraphicsListRegistry(yoGraphicsListRegistry);
@@ -66,6 +78,11 @@ public class SkippySimulation
       blockingSimulationRunner.destroySimulation();
    }
 
+   public SkippyRobot getSkippy()
+   {
+      return skippy;
+   }
+
    /*
     * When your simulation is run, first the main method will be called. In
     * creating a Skippy Simulation, a SkippyRobot is first created, and then a
@@ -73,10 +90,9 @@ public class SkippySimulation
     * then created using the SimulationConstructionSet object. Finally the
     * Thread is started, thereby starting your simulation.
     */
-
    public static void main(String[] args) throws SimulationExceededMaximumTimeException
    {
-      SkippySimulation skippySimulation = new SkippySimulation();
+      SkippySimulation skippySimulation = new SkippySimulation(SkippyControllerMode.ICP_BASED);
       skippySimulation.run(TIME);
       ThreadTools.sleepForever();
    }
--- ExampleSimulations/test/us/ihmc/exampleSimulations/skippy/SkippyTest.java
+++ ExampleSimulations/test/us/ihmc/exampleSimulations/skippy/SkippyTest.java
@@ -2,10 +2,13 @@ package us.ihmc.exampleSimulations.skippy;
 
 import static org.junit.Assert.assertTrue;
 
+import javax.vecmath.Vector3d;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import us.ihmc.exampleSimulations.skippy.SkippySimulation.SkippyControllerMode;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
 import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
@@ -15,7 +18,9 @@ import us.ihmc.tools.thread.ThreadTools;
 @ContinuousIntegrationPlan(categories = {IntegrationCategory.FAST})
 public class SkippyTest
 {
+   private static final SkippyControllerMode controllerMode = SkippyControllerMode.ICP_BASED;
    private static final boolean sleepAfterTest = false;
+
    private SkippySimulation skippySimulation;
 
    @ContinuousIntegrationTest(estimatedDuration = 10.0)
@@ -25,10 +30,30 @@ public class SkippyTest
       assertTrue(skippySimulation.run(10.0));
    }
 
+   @ContinuousIntegrationTest(estimatedDuration = 10.0)
+   @Test(timeout = 100000)
+   public void testRecoveringFromPush() throws SimulationExceededMaximumTimeException
+   {
+      double pushDuration = 0.03;
+      Vector3d pushForce = new Vector3d(0.0, 10.0, 0.0);
+
+      assertTrue(skippySimulation.run(1.0));
+      pushRobot(pushDuration, pushForce);
+      assertTrue(skippySimulation.run(5.0));
+   }
+
+   private void pushRobot(double time, Vector3d force) throws SimulationExceededMaximumTimeException
+   {
+      SkippyRobot skippy = skippySimulation.getSkippy();
+      skippy.setRootJointForce(force.x, force.y, force.z);
+      assertTrue(skippySimulation.run(time));
+      skippy.setRootJointForce(0.0, 0.0, 0.0);
+   }
+
    @Before
    public void setupTest()
    {
-      skippySimulation = new SkippySimulation();
+      skippySimulation = new SkippySimulation(controllerMode);
    }
 
    @After
