8170d24cce5d5449463eaef44af1cd630fb88b03
Georg Wiedebach <gwiedebach@ihmc.us>
2016-12-07 17:03:58 -0600

    unit tests for skippy

---
+++ ExampleSimulations/test/us/ihmc/exampleSimulations/skippy/SkippyTest.java
@@ -0,0 +1,41 @@
+package us.ihmc.exampleSimulations.skippy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
+import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.tools.continuousIntegration.IntegrationCategory;
+import us.ihmc.tools.thread.ThreadTools;
+
+@ContinuousIntegrationPlan(categories = {IntegrationCategory.FAST})
+public class SkippyTest
+{
+   private static final boolean sleepAfterTest = false;
+   private SkippySimulation skippySimulation;
+
+   @ContinuousIntegrationTest(estimatedDuration = 10.0)
+   @Test(timeout = 100000)
+   public void testStanding() throws SimulationExceededMaximumTimeException
+   {
+      assertTrue(skippySimulation.run(2.0));
+   }
+
+   @Before
+   public void setupTest()
+   {
+      skippySimulation = new SkippySimulation();
+   }
+
+   @After
+   public void afterTest()
+   {
+      if (sleepAfterTest)
+         ThreadTools.sleepForever();
+      skippySimulation.destroy();
+   }
+}
