a1fc09162240a14e912719f3579aac5854af5423
Georg Wiedebach <gwiedebach@ihmc.us>
2016-12-13 09:45:25 -0600

    cleaning up the trajectory types

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/trajectories/TrajectoryType.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/trajectories/TrajectoryType.java
@@ -2,7 +2,7 @@ package us.ihmc.robotics.trajectories;
 
 public enum TrajectoryType
 {
-   DEFAULT, BASIC, PUSH_RECOVERY, OBSTACLE_CLEARANCE;
+   DEFAULT, OBSTACLE_CLEARANCE;
 
    public static String getDocumentation(TrajectoryType var)
    {
@@ -10,13 +10,9 @@ public enum TrajectoryType
       {
       case DEFAULT:
          return "is a default trajectory";
-      case BASIC:
-         return "will do a basic swing with the specified swing height";
-      case PUSH_RECOVERY:
-         return "uses a low swing height for fast steps";
       case OBSTACLE_CLEARANCE:
          return "will attempt to step over an obstacle";
-         
+
       default:
          return "no documentation available";
       }
