ed4dff248dcd961967d5f37cd076803ad55d79b0
bshrewsbury <bshrewsbury@ihmc.us>
2016-12-14 17:38:39 -0600

    extracted StraightLinePathParameters and StraightLineTranslationalPathParameters into separate classes and moved params for the ui footstep path generator to a robotModel parameter class. Removed lots of public accessors.

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/footstep/footstepGenerator/FootstepPlanningParameterization.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/footstep/footstepGenerator/FootstepPlanningParameterization.java
@@ -4,30 +4,24 @@ import java.util.List;
 
 public abstract class FootstepPlanningParameterization
 {
-
-   public class FootstepOffset
-   {
-      public double dx;
-      public double dy;
-      public double dtheta;
-
-      public FootstepOffset(double dx, double dy, double dtheta)
-      {
-         this.dx = dx;
-         this.dy = dy;
-         this.dtheta = dtheta;
-      }
-   }
-
    protected List<FootstepOffset> offsetList;
-   public double footWidth;
-   public double footLength;
-   public double cropWidth;
-   public double cropLength;
-   public double maxSupportPolygonArea;
-   public double minSupportPolygonArea;
-   public double xAnkleOffset;
-   public double yAnkleOffset;
+   protected double footWidth;
+   protected double footLength;
+   protected double cropWidth;
+   protected double cropLength;
+   protected double maxSupportPolygonArea;
+   protected double minSupportPolygonArea;
+   protected double xAnkleOffset;
+   protected double yAnkleOffset;
+   
+   //used for straight line footstep path generation in the UI (clicking in the window)
+   //defaults work with atlas and val
+   protected double straightstepLength = 0.4; 
+   protected double straightStepWidth = 0.25;
+   protected double reverseStepLength = 0.15;
+   protected double reverseStepWidth = 0.25;
+   protected double shuffleStepLength = 0.25;
+   protected double shuffleStepWidth = 0.21;
 
    public List<FootstepOffset> getOffsets(FootstepPlanState currentState)
    {
@@ -45,4 +39,93 @@ public abstract class FootstepPlanningParameterization
    public abstract double getMaxStepDistance();
 
    public abstract double getDangerDistance();
+   
+   public double getMaxSupportPolygonArea()
+   {
+      return maxSupportPolygonArea;
+   }
+
+   public class FootstepOffset
+   {
+      public double dx;
+      public double dy;
+      public double dtheta;
+      
+      public FootstepOffset(double dx, double dy, double dtheta)
+      {
+         this.dx = dx;
+         this.dy = dy;
+         this.dtheta = dtheta;
+      }
+   }
+
+   public double getStraightstepLength()
+   {
+      return straightstepLength;
+   }
+
+   public double getStraightStepWidth()
+   {
+      return straightStepWidth;
+   }
+
+   public double getReverseStepLength()
+   {
+      return reverseStepLength;
+   }
+
+   public double getReverseStepWidth()
+   {
+      return reverseStepWidth;
+   }
+
+   public double getShuffleStepLength()
+   {
+      return shuffleStepLength;
+   }
+
+   public double getShuffleStepWidth()
+   {
+      return shuffleStepWidth;
+   }
+
+   public double getCropLength()
+   {
+      return cropLength;
+   }
+
+   public double getCropWidth()
+   {
+      return cropWidth;
+   }
+
+   public double getFootLength()
+   {
+      return footLength;
+   }
+
+   public void setFootLength(double footLength)
+   {
+      this.footLength = footLength;
+   }
+
+   public double getFootWidth()
+   {
+      return footWidth;
+   }
+
+   public void setFootWidth(double footWidth)
+   {
+      this.footWidth = footWidth;
+   }
+
+   public double getMinSupportPolygonArea()
+   {
+      return minSupportPolygonArea;
+   }
+
+   public void setMinSupportPolygonArea(double minSupportPolygonArea)
+   {
+      this.minSupportPolygonArea = minSupportPolygonArea;
+   }
 }
