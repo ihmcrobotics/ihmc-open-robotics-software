7857df693be6fce83dc6e74af85c272d83326118
Sylvain Bertrand <sbertrand@ihmc.us>
2016-12-15 21:28:26 -0600

    Added the option to not clear automatically the palette.

--- IHMCJavaFXToolkit/src/us/ihmc/javaFXToolkit/shapes/TextureColorAdaptivePalette.java
+++ IHMCJavaFXToolkit/src/us/ihmc/javaFXToolkit/shapes/TextureColorAdaptivePalette.java
@@ -26,27 +26,44 @@ public class TextureColorAdaptivePalette implements TextureColorPalette
 
    private final List<Color> colorList = new ArrayList<>();
    private final Map<Color, Integer> registeredColors = new HashMap<>();
+   private final boolean autoClear;
 
    public TextureColorAdaptivePalette()
    {
-      this(DEFAULT_PALETTE_SIZE, 1);
+      this(DEFAULT_PALETTE_SIZE, 1, true);
+   }
+
+   public TextureColorAdaptivePalette(boolean autoClear)
+   {
+      this(DEFAULT_PALETTE_SIZE, 1, autoClear);
    }
 
    public TextureColorAdaptivePalette(int paletteSize)
    {
-      this(paletteSize, 1);
+      this(paletteSize, 1, true);
+   }
+
+   public TextureColorAdaptivePalette(int paletteSize, boolean autoClear)
+   {
+      this(paletteSize, 1, autoClear);
    }
 
    public TextureColorAdaptivePalette(int paletteSize, int colorPixelSize)
    {
+      this(paletteSize, colorPixelSize, true);
+   }
+
+   public TextureColorAdaptivePalette(int paletteSize, int colorPixelSize, boolean autoClear)
+   {
       if (colorPixelSize >= paletteSize)
          throw new RuntimeException("The size of pixels has to me smaller than the size of the palette.");
 
+      this.autoClear = autoClear;
       this.paletteSize = paletteSize;
       this.pixelSize = colorPixelSize;
    }
 
-   private void clearPalette()
+   public void clearPalette()
    {
       colorPalette = null;
       colorList.clear();
@@ -84,7 +101,7 @@ public class TextureColorAdaptivePalette implements TextureColorPalette
          else
          {
             PrintTools.error(this, "Reached maximum capacity of the palette. Next colors will be wrong.");
-            return new float[]{0.0f, 0.0f};
+            return new float[] {0.0f, 0.0f};
          }
       }
    }
@@ -130,7 +147,8 @@ public class TextureColorAdaptivePalette implements TextureColorPalette
             ex.printStackTrace();
          }
       }
-      clearPalette();
+      if (autoClear)
+         clearPalette();
       return ret;
    }
 }
