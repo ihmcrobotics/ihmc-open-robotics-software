d39e22feaf4f390ce51f883e353d2cf3859f8b8d
Operator Computer <dragon_ryderz@maillist.ihmc.us>
2016-12-15 23:59:08 -0600

    Use ReferenceFrames to fix footstep checks.

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/graphSearch/PlanarRegionPotentialNextStepCalculator.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/graphSearch/PlanarRegionPotentialNextStepCalculator.java
@@ -22,12 +22,14 @@ import us.ihmc.robotics.dataStructures.variable.BooleanYoVariable;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
 import us.ihmc.robotics.geometry.AngleTools;
 import us.ihmc.robotics.geometry.ConvexPolygon2d;
+import us.ihmc.robotics.geometry.FramePoint;
 import us.ihmc.robotics.geometry.FramePose;
 import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
-import us.ihmc.robotics.geometry.RotationTools;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+import us.ihmc.robotics.referenceFrames.TransformReferenceFrame;
+import us.ihmc.robotics.referenceFrames.ZUpFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
@@ -555,69 +557,80 @@ public class PlanarRegionPotentialNextStepCalculator
       return true;
    }
 
+   private final RigidBodyTransform transform = new RigidBodyTransform();
+   private final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+   private final TransformReferenceFrame parentSoleFrame = new TransformReferenceFrame("parentSole", ReferenceFrame.getWorldFrame());
+   private final ZUpFrame parentSoleZupFrame = new ZUpFrame(worldFrame, parentSoleFrame, "parentSoleZupFrame");
+   private final TransformReferenceFrame nodeSoleFrame = new TransformReferenceFrame("nodeSole", ReferenceFrame.getWorldFrame());
+   private final FramePoint solePositionInParentZUpFrame = new FramePoint(parentSoleZupFrame);
+
    private boolean checkIfGoodFootstep(BipedalFootstepPlannerNode nodeToExpand)
    {
+      BipedalFootstepPlannerNode parentNode = nodeToExpand.getParentNode();
+      if (parentNode == null)
+         return true;
+
+      parentNode.getSoleTransform(transform);
+
+      parentSoleFrame.setTransformAndUpdate(transform);
+      parentSoleZupFrame.update();
+
+      nodeToExpand.getSoleTransform(transform);
+      nodeSoleFrame.setTransformAndUpdate(transform);
+
+      solePositionInParentZUpFrame.setToZero(nodeSoleFrame);
+      solePositionInParentZUpFrame.changeFrame(parentSoleZupFrame);
+
+      double minimumStepWidth = parameters.getMinimumStepWidth();
+
       RobotSide robotSide = nodeToExpand.getRobotSide();
-
-      RigidBodyTransform transformToParent = nodeToExpand.getTransformToParentCopy();
-
-      if (transformToParent != null)
+      if (((robotSide == RobotSide.LEFT) && (solePositionInParentZUpFrame.getY() < minimumStepWidth))
+            || ((robotSide == RobotSide.RIGHT) && (solePositionInParentZUpFrame.getY() > -minimumStepWidth)))
       {
-         RotationTools.removePitchAndRollFromTransform(transformToParent);
+         notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_NOT_WIDE_ENOUGH);
+         return false;
+      }
 
-         Point3d stepFromParentInSoleZUpFrame = new Point3d();
-         transformToParent.transform(stepFromParentInSoleZUpFrame);
+      double maximumStepWidth = parameters.getMaximumStepWidth();
 
-         double minimumStepWidth = parameters.getMinimumStepWidth();
+      if (((robotSide == RobotSide.LEFT) && (solePositionInParentZUpFrame.getY() > maximumStepWidth))
+            || ((robotSide == RobotSide.RIGHT) && (solePositionInParentZUpFrame.getY() < -maximumStepWidth)))
+      {
+         notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_WIDE);
+         return false;
+      }
 
-         if (((robotSide == RobotSide.LEFT) && (stepFromParentInSoleZUpFrame.getY() < minimumStepWidth))
-               || ((robotSide == RobotSide.RIGHT) && (stepFromParentInSoleZUpFrame.getY() > -minimumStepWidth)))
-         {
-            notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_NOT_WIDE_ENOUGH);
-            return false;
-         }
+      double minimumStepLength = parameters.getMinimumStepLength();
+      if (solePositionInParentZUpFrame.getX() < minimumStepLength)
+      {
+         notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_NOT_LONG_ENOUGH);
+         return false;
+      }
 
-         double maximumStepWidth = parameters.getMaximumStepWidth();
+      if (Math.abs(solePositionInParentZUpFrame.getZ()) > parameters.getMaximumStepZ())
+      {
+         notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_HIGH_OR_LOW);
+         return false;
+      }
 
-         if (((robotSide == RobotSide.LEFT) && (stepFromParentInSoleZUpFrame.getY() > maximumStepWidth))
-               || ((robotSide == RobotSide.RIGHT) && (stepFromParentInSoleZUpFrame.getY() < -maximumStepWidth)))
-         {
-            notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_WIDE);
-            return false;
-         }
+      if ((solePositionInParentZUpFrame.getX() > parameters.getMaximumStepXWhenForwardAndDown())
+            && (solePositionInParentZUpFrame.getZ() < -Math.abs(parameters.getMaximumStepZWhenForwardAndDown())))
+      {
+         notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_FORWARD_AND_DOWN);
+         return false;
+      }
 
-         double minimumStepLength = parameters.getMinimumStepLength();
-         if (stepFromParentInSoleZUpFrame.getX() < minimumStepLength)
-         {
-            notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_NOT_LONG_ENOUGH);
-            return false;
-         }
-
-         if (Math.abs(stepFromParentInSoleZUpFrame.getZ()) > parameters.getMaximumStepZ())
-         {
-            notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_HIGH_OR_LOW);
-            return false;
-         }
-
-         if ((stepFromParentInSoleZUpFrame.getX() > parameters.getMaximumStepXWhenForwardAndDown())
-               && (stepFromParentInSoleZUpFrame.getZ() < -Math.abs(parameters.getMaximumStepZWhenForwardAndDown())))
-         {
-            notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_FORWARD_AND_DOWN);
-            return false;
-         }
-
-         stepReach.set(getXYLength(stepFromParentInSoleZUpFrame));
-         if (stepReach.getDoubleValue() > parameters.getMaximumStepReach())
-         {
-            notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_FAR);
-            return false;
-         }
+      stepReach.set(getXYLength(solePositionInParentZUpFrame));
+      if (stepReach.getDoubleValue() > parameters.getMaximumStepReach())
+      {
+         notifyListenerNodeUnderConsiderationWasRejected(nodeToExpand, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_FAR);
+         return false;
       }
 
       return true;
    }
 
-   private double getXYLength(Point3d point)
+   private double getXYLength(FramePoint point)
    {
       return Math.sqrt(point.getX() * point.getX() + point.getY() * point.getY());
    }
