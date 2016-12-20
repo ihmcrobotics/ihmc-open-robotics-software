a0fa40d13e364189807c554db53405eaf5d0494a
Georg Wiedebach <gwiedebach@ihmc.us>
2016-12-20 12:37:42 -0600

    added optional base cost per step

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/EuclidianBasedCost.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/EuclidianBasedCost.java
@@ -5,9 +5,21 @@ import us.ihmc.footstepPlanning.aStar.FootstepNode;
 
 public class EuclidianBasedCost implements FootstepCost
 {
+   private final double stepBaseCost;
+
+   public EuclidianBasedCost()
+   {
+      this(0.0);
+   }
+
+   public EuclidianBasedCost(double stepBaseCost)
+   {
+      this.stepBaseCost = stepBaseCost;
+   }
+
    @Override
    public double compute(FootstepNode startNode, FootstepNode endNode)
    {
-      return startNode.euclideanDistance(endNode);
+      return startNode.euclideanDistance(endNode) + stepBaseCost;
    }
 }
