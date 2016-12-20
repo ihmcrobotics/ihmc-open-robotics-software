bcbd06a2242cb7a6bfa7d33cbe2ca02f987de253
Georg Wiedebach <gwiedebach@ihmc.us>
2016-12-20 12:40:44 -0600

    better node expansion for actual walking

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/SimpleSideBasedExpansion.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/SimpleSideBasedExpansion.java
@@ -2,42 +2,66 @@ package us.ihmc.footstepPlanning.aStar.implementations;
 
 import java.util.HashSet;
 
-import javax.vecmath.Vector3d;
-
 import us.ihmc.footstepPlanning.aStar.FootstepNode;
 import us.ihmc.footstepPlanning.aStar.FootstepNodeExpansion;
-import us.ihmc.robotics.geometry.RigidBodyTransform;
+import us.ihmc.robotics.geometry.FramePose;
+import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 
 public class SimpleSideBasedExpansion implements FootstepNodeExpansion
 {
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+
+   private double[] stepLengths = new double[] {0.0, FootstepNode.gridSizeX, 0.1, 0.2, 0.4};
+   private double[] stepWidths = new double[] {0.15, 0.3};
+   private double[] stepYaws = new double[] {0.0, FootstepNode.gridSizeYaw, Math.PI / 9.0};
+
+   private double defaultStepWidth = 0.25;
+
    @Override
    public HashSet<FootstepNode> expandNode(FootstepNode node)
    {
-      RobotSide stepSide = node.getRobotSide().getOppositeSide();
-      double yOffset = stepSide.negateIfRightSide(FootstepNode.gridSizeY);
-      double stanceYaw = node.getYaw();
-
       HashSet<FootstepNode> neighbors = new HashSet<>();
-      double yawGrid = FootstepNode.gridSizeYaw;
-      double[] neighborYaws = new double[] {stanceYaw - yawGrid, stanceYaw, stanceYaw + yawGrid};
 
-      for (int i = 0; i < neighborYaws.length; i++)
+      FramePose stanceFootPose = new FramePose(worldFrame);
+      stanceFootPose.setYawPitchRoll(node.getYaw(), 0.0, 0.0);
+      stanceFootPose.setX(node.getX());
+      stanceFootPose.setY(node.getY());
+      ReferenceFrame stanceFrame = new PoseReferenceFrame("stanceFrame", stanceFootPose);
+
+      RobotSide stepSide = node.getRobotSide().getOppositeSide();
+      double ySign = stepSide.negateIfRightSide(1.0);
+
+      for (int i = 0; i < stepLengths.length; i++)
       {
-         double neighborYaw = neighborYaws[i];
-         Vector3d offset1 = new Vector3d(FootstepNode.gridSizeX, yOffset, 0.0);
-         Vector3d offset2 = new Vector3d(-FootstepNode.gridSizeX, yOffset, 0.0);
-         Vector3d offset3 = new Vector3d(0.0, yOffset, 0.0);
+         double stepLength = stepLengths[i];
+         for (int j = 0; j < stepYaws.length; j++)
+         {
+            double yaw = stepYaws[j];
 
-         RigidBodyTransform transform = new RigidBodyTransform();
-         transform.setRotationYawAndZeroTranslation(neighborYaw);
-         transform.transform(offset1);
-         transform.transform(offset2);
-         transform.transform(offset3);
+            FramePose forwardStep = new FramePose(stanceFrame);
+            forwardStep.setX(stepLength);
+            forwardStep.setY(ySign * defaultStepWidth);
+            forwardStep.changeFrame(worldFrame);
+            neighbors.add(new FootstepNode(forwardStep.getX(), forwardStep.getY(), node.getYaw() + ySign * yaw, stepSide));
 
-         neighbors.add(new FootstepNode(node.getX() + offset1.getX(), node.getY() + offset1.getY(), neighborYaw, stepSide));
-         neighbors.add(new FootstepNode(node.getX() + offset2.getX(), node.getY() + offset2.getY(), neighborYaw, stepSide));
-         neighbors.add(new FootstepNode(node.getX() + offset3.getX(), node.getY() + offset3.getY(), neighborYaw, stepSide));
+            FramePose backwardStep = new FramePose(stanceFrame);
+            backwardStep.setX(-stepLength);
+            backwardStep.setY(ySign * defaultStepWidth);
+            backwardStep.changeFrame(worldFrame);
+            neighbors.add(new FootstepNode(backwardStep.getX(), backwardStep.getY(), node.getYaw() + ySign * yaw, stepSide));
+         }
+      }
+
+      for (int i = 0; i < stepWidths.length; i++)
+      {
+         double stepWidth = stepWidths[i];
+         FramePose sideStep = new FramePose(stanceFrame);
+         sideStep.setY(ySign * stepWidth);
+         sideStep.changeFrame(worldFrame);
+         neighbors.add(new FootstepNode(sideStep.getX(), sideStep.getY(), node.getYaw(), stepSide));
+
       }
 
       return neighbors;
