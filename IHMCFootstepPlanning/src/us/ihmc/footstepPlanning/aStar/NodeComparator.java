26e629eca5a491178eba62c0fce340857c097d26
Georg Wiedebach <gwiedebach@ihmc.us>
2016-12-20 14:14:47 -0600

    support reaching the goal with wither foot and square up

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/NodeComparator.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/NodeComparator.java
@@ -2,31 +2,26 @@ package us.ihmc.footstepPlanning.aStar;
 
 import java.util.Comparator;
 
-import us.ihmc.footstepPlanning.aStar.implementations.ZeroHeuristics;
+import us.ihmc.robotics.robotSide.SideDependentList;
 
 public class NodeComparator implements Comparator<FootstepNode>
 {
    private final FootstepGraph graph;
-   private final FootstepNode goalNode;
+   private final SideDependentList<FootstepNode> goalNodes;
    private final CostToGoHeuristics heuristics;
 
-   public NodeComparator(FootstepGraph graph, FootstepNode goalNode)
-   {
-      this(graph, goalNode, new ZeroHeuristics());
-   }
-
-   public NodeComparator(FootstepGraph graph, FootstepNode goalNode, CostToGoHeuristics heuristics)
+   public NodeComparator(FootstepGraph graph, SideDependentList<FootstepNode> goalNodes, CostToGoHeuristics heuristics)
    {
       this.graph = graph;
-      this.goalNode = goalNode;
+      this.goalNodes = goalNodes;
       this.heuristics = heuristics;
    }
 
    @Override
    public int compare(FootstepNode o1, FootstepNode o2)
    {
-      double cost1 = graph.getCostFromStart(o1) + heuristics.compute(o1, goalNode);
-      double cost2 = graph.getCostFromStart(o2) + heuristics.compute(o2, goalNode);
+      double cost1 = graph.getCostFromStart(o1) + heuristics.compute(o1, goalNodes.get(o1.getRobotSide()));
+      double cost2 = graph.getCostFromStart(o2) + heuristics.compute(o2, goalNodes.get(o2.getRobotSide()));
       if (cost1 == cost2) return 0;
       return cost1 < cost2 ? -1 : 1;
    }
