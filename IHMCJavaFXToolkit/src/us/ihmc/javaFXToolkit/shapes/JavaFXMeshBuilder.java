09fbacef0c13ad522553f8d89ec4b1ee305f1419
Sylvain Bertrand <sbertrand@ihmc.us>
2017-01-09 13:23:02 +0100

    Documented the JavaFX mesh builders.

--- IHMCJavaFXToolkit/src/us/ihmc/javaFXToolkit/shapes/JavaFXMeshBuilder.java
+++ IHMCJavaFXToolkit/src/us/ihmc/javaFXToolkit/shapes/JavaFXMeshBuilder.java
@@ -4,6 +4,9 @@ import javafx.scene.shape.Mesh;
 import us.ihmc.graphics3DDescription.MeshDataBuilder;
 import us.ihmc.javaFXToolkit.graphics.JavaFXMeshDataInterpreter;
 
+/**
+ * Extension of {@link MeshDataBuilder} that can generate JavaFX {@link Mesh}.
+ */
 public class JavaFXMeshBuilder extends MeshDataBuilder
 {
    public Mesh generateMesh()
