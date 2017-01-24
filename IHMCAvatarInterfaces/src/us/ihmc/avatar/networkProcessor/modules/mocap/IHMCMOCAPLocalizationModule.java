2210650d8c8be36796220a87f7437837f7c42baf
Edoardo Amadei <eamadei@ihmc.us>
2017-01-24 16:11:00 -0600

    Added server and registry for trial with MOCAP only

--- IHMCAvatarInterfaces/src/us/ihmc/avatar/networkProcessor/modules/mocap/IHMCMOCAPLocalizationModule.java
+++ IHMCAvatarInterfaces/src/us/ihmc/avatar/networkProcessor/modules/mocap/IHMCMOCAPLocalizationModule.java
@@ -10,22 +10,58 @@ import us.ihmc.communication.packets.PacketDestination;
 import us.ihmc.communication.util.NetworkPorts;
 import us.ihmc.humanoidRobotics.communication.packets.StampedPosePacket;
 import us.ihmc.humanoidRobotics.kryo.IHMCCommunicationKryoNetClassList;
+import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
+import us.ihmc.robotDataLogger.YoVariableServer;
+import us.ihmc.robotDataLogger.logger.LogSettings;
+import us.ihmc.robotModels.FullHumanoidRobotModel;
+import us.ihmc.robotModels.FullRobotModel;
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
 import us.ihmc.robotics.geometry.RigidBodyTransform;
 import us.ihmc.robotics.kinematics.TimeStampedTransform3D;
+import us.ihmc.robotics.math.frames.YoFramePose;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+import us.ihmc.util.PeriodicNonRealtimeThreadScheduler;
+import us.ihmc.util.PeriodicThreadScheduler;
 
 import java.util.ArrayList;
 
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
+
 public class IHMCMOCAPLocalizationModule implements MocapRigidbodiesListener
 {
+   private static final double MOCAP_SERVER_DT = 0.002;
+   private static final int PELVIS_ID = 1;
+   
    private final MocapToPelvisFrameConverter mocapToPelvisFrameConverter;
    private final PacketCommunicator packetCommunicator = PacketCommunicator.createIntraprocessPacketCommunicator(NetworkPorts.MOCAP_MODULE, new IHMCCommunicationKryoNetClassList());
-
+   private final YoVariableRegistry yoVariableRegistry = new YoVariableRegistry(getClass().getSimpleName());
+   private final YoVariableServer yoVariableServer;
+   private final FullHumanoidRobotModel fullRobotModel;
+   
+   private final DoubleYoVariable pelvisPositionX = new DoubleYoVariable("pelvisPositionX", yoVariableRegistry);
+   private final DoubleYoVariable pelvisPositionY = new DoubleYoVariable("pelvisPositionY", yoVariableRegistry);
+   private final DoubleYoVariable pelvisPositionZ = new DoubleYoVariable("pelvisPositionZ", yoVariableRegistry);
+   
+   private final Vector3d tempPosition = new Vector3d();
+   private final Matrix3d tempOrientation = new Matrix3d();
+   
    public IHMCMOCAPLocalizationModule(DRCRobotModel drcRobotModel)
    {
       MocapDataClient mocapDataClient = new MocapDataClient();
       mocapDataClient.registerRigidBodiesListener(this);
 
       mocapToPelvisFrameConverter = new MocapToPelvisFrameConverter(drcRobotModel, packetCommunicator);
+      
+      PeriodicThreadScheduler scheduler = new PeriodicNonRealtimeThreadScheduler("MocapModuleScheduler");
+      LogModelProvider logModelProvider = drcRobotModel.getLogModelProvider();
+      
+      yoVariableServer = new YoVariableServer(getClass(), scheduler, logModelProvider, LogSettings.ATLAS_IAN, MOCAP_SERVER_DT);
+      fullRobotModel = drcRobotModel.createFullRobotModel();
+      yoVariableServer.setMainRegistry(yoVariableRegistry, fullRobotModel, null);
+      yoVariableServer.start();
    }
 
    public PacketCommunicator getPacketCommunicator()
@@ -39,18 +75,36 @@ public class IHMCMOCAPLocalizationModule implements MocapRigidbodiesListener
       for (int i = 0; i < listOfRigidbodies.size(); i++)
       {
          MocapRigidBody mocapRigidBody = listOfRigidbodies.get(i);
-         RigidBodyTransform pelvisTransform = mocapToPelvisFrameConverter.convertMocapPoseToRobotFrame(mocapRigidBody);
-         sendPelvisTransformToController(mocapRigidBody.getId(), pelvisTransform);
+         
+         if(mocapRigidBody.getId() == PELVIS_ID)
+         {
+            RigidBodyTransform pelvisTransform = new RigidBodyTransform();
+            // mocapToPelvisFrameConverter.convertMocapPoseToRobotFrame(mocapRigidBody);
+            mocapRigidBody.packPose(pelvisTransform);
+            sendPelvisTransformToController(pelvisTransform);            
+         }
       }
+      
+      yoVariableServer.update(System.currentTimeMillis());
    }
 
-   private void sendPelvisTransformToController(int frameID, RigidBodyTransform pelvisTransform)
+   private void sendPelvisTransformToController(RigidBodyTransform pelvisTransform)
    {
       TimeStampedTransform3D timestampedTransform = createTimestampedTransform(pelvisTransform);
-      StampedPosePacket stampedPosePacket = new StampedPosePacket(Integer.toString(frameID), timestampedTransform, 1.0);
+      StampedPosePacket stampedPosePacket = new StampedPosePacket(Integer.toString(PELVIS_ID), timestampedTransform, 1.0);
+      
       stampedPosePacket.setDestination(PacketDestination.CONTROLLER.ordinal());
-
       packetCommunicator.send(stampedPosePacket);
+      
+      pelvisTransform.getTranslation(tempPosition);
+      pelvisTransform.getRotation(tempOrientation);
+            
+      fullRobotModel.getRootJoint().setPosition(tempPosition);
+      fullRobotModel.getRootJoint().setRotation(tempOrientation);
+      
+      pelvisPositionX.set(tempPosition.getX());
+      pelvisPositionY.set(tempPosition.getY());
+      pelvisPositionZ.set(tempPosition.getZ());
    }
 
    private TimeStampedTransform3D createTimestampedTransform(RigidBodyTransform rigidBodyTransform)
