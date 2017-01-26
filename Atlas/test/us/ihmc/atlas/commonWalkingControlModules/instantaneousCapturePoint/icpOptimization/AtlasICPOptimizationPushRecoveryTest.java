5e13e031eb59ab64095e80435748f9820fdaae8f
Robert <rjgriffin42@gmail.com>
2017-01-26 10:43:32 -0500

    fixed the main call in the icp optimization push test

--- Atlas/test/us/ihmc/atlas/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/AtlasICPOptimizationPushRecoveryTest.java
+++ Atlas/test/us/ihmc/atlas/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/AtlasICPOptimizationPushRecoveryTest.java
@@ -39,7 +39,7 @@ public class AtlasICPOptimizationPushRecoveryTest extends ICPOptimizationPushRec
       AtlasICPOptimizationPushRecoveryTest test = new AtlasICPOptimizationPushRecoveryTest();
       try
       {
-         test.testPushICPOptimizationOutwardPushInSlowSwing();
+         test.testPushICPOptimizationBackwardPushInSwing();
       }
       catch(SimulationExceededMaximumTimeException e)
       {
