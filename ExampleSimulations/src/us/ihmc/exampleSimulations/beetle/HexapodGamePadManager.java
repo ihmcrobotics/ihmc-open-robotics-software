517e9a248fc1cafc507aa08c82223c0c29ec0b8f
bshrewsbury <bshrewsbury@ihmc.us>
2017-01-26 11:24:28 -0600

    creating rhino beetle example sim

---
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/beetle/HexapodGamePadManager.java
@@ -0,0 +1,115 @@
+package us.ihmc.exampleSimulations.beetle;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+
+import net.java.games.input.Event;
+import us.ihmc.commonWalkingControlModules.controllerCore.WholeBodyControllerCoreMode;
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
+import us.ihmc.robotics.dataStructures.variable.EnumYoVariable;
+import us.ihmc.tools.inputDevices.joystick.Joystick;
+import us.ihmc.tools.inputDevices.joystick.JoystickComponentFilter;
+import us.ihmc.tools.inputDevices.joystick.JoystickEventListener;
+import us.ihmc.tools.inputDevices.joystick.mapping.XBoxOneMapping;
+
+public class HexapodGamePadManager implements JoystickEventListener
+{
+   private final Map<XBoxOneMapping, Double> channels = Collections.synchronizedMap(new EnumMap<XBoxOneMapping, Double>(XBoxOneMapping.class));
+   
+   private final EnumYoVariable<WholeBodyControllerCoreMode> controllerCoreMode;
+   private final DoubleYoVariable desiredLinearVelocityX; 
+   private final DoubleYoVariable desiredLinearVelocityY; 
+   private final DoubleYoVariable desiredLinearVelocityZ; 
+   private final DoubleYoVariable currentOrientationPitch;
+   private final DoubleYoVariable currentOrientationRoll;
+   private final DoubleYoVariable desiredAngularVelocityZ;
+   private final DoubleYoVariable desiredBodyHeight;
+   
+   public HexapodGamePadManager(Joystick device, YoVariableRegistry registry)
+   {
+      device.setComponentFilter(new JoystickComponentFilter(XBoxOneMapping.LEFT_TRIGGER, false, 0.05, 1, 1.0));
+      device.setComponentFilter(new JoystickComponentFilter(XBoxOneMapping.RIGHT_TRIGGER, false, 0.05, 1, 1.0));
+      device.setComponentFilter(new JoystickComponentFilter(XBoxOneMapping.LEFT_STICK_X, true, 0.1, 1));
+      device.setComponentFilter(new JoystickComponentFilter(XBoxOneMapping.LEFT_STICK_Y, true, 0.1, 1));
+      device.setComponentFilter(new JoystickComponentFilter(XBoxOneMapping.RIGHT_STICK_X, true, 0.1, 1));
+      device.setComponentFilter(new JoystickComponentFilter(XBoxOneMapping.RIGHT_STICK_Y, true, 0.1, 1));
+      
+      device.setPollInterval(10);
+      
+     desiredLinearVelocityX = (DoubleYoVariable) registry.getVariable("BODYdesiredLinearVelocityX");
+     desiredLinearVelocityY = (DoubleYoVariable) registry.getVariable("BODYdesiredLinearVelocityY");
+     desiredLinearVelocityZ = (DoubleYoVariable) registry.getVariable("BODYdesiredLinearVelocityZ");
+     desiredBodyHeight = (DoubleYoVariable) registry.getVariable("BODYdesiredBodyHeight");
+     
+     currentOrientationPitch = (DoubleYoVariable) registry.getVariable("BODYdesiredOrientationPitch");
+     currentOrientationRoll = (DoubleYoVariable) registry.getVariable("BODYdesiredOrientationRoll");
+     desiredAngularVelocityZ = (DoubleYoVariable) registry.getVariable("BODYdesiredAngularVelocityZ");
+     controllerCoreMode = (EnumYoVariable<WholeBodyControllerCoreMode>) registry.getVariable("controllerCoreMode");
+     
+     
+      
+     device.addJoystickEventListener(this);
+   }
+   
+   @Override
+   public void processEvent(Event event)
+   {
+      // Store updated value in a cache so historical values for all channels can be used.
+      channels.put(XBoxOneMapping.getMapping(event), (double) event.getValue());
+
+      // Handle events that should trigger once immediately after the event is triggered.
+      switch (XBoxOneMapping.getMapping(event))
+      {
+      case LEFT_BUMPER:
+         break;
+      case RIGHT_BUMPER:
+         break;
+      case LEFT_TRIGGER:
+         double value = channels.get(XBoxOneMapping.LEFT_TRIGGER) - 1.0;
+         currentOrientationRoll.set(value * -0.4);
+         break;
+      case RIGHT_TRIGGER:
+         double value2 = channels.get(XBoxOneMapping.RIGHT_TRIGGER) - 1.0;
+         currentOrientationRoll.set(value2 * -0.4);
+         break;
+      case LEFT_STICK_X:
+         desiredLinearVelocityY.set(channels.get(XBoxOneMapping.LEFT_STICK_X) * 0.2);
+         break;
+      case LEFT_STICK_Y:
+         desiredLinearVelocityX.set(channels.get(XBoxOneMapping.LEFT_STICK_Y) * 0.2);
+         break;
+      case RIGHT_STICK_X:
+         desiredAngularVelocityZ.set(channels.get(XBoxOneMapping.RIGHT_STICK_X) * 0.2);
+         break;
+      case RIGHT_STICK_Y:
+         currentOrientationPitch.set(channels.get(XBoxOneMapping.RIGHT_STICK_Y) * 0.1);
+         break;
+      case SELECT:
+         controllerCoreMode.set(WholeBodyControllerCoreMode.INVERSE_DYNAMICS);
+         break;
+      case START:
+         controllerCoreMode.set(WholeBodyControllerCoreMode.VIRTUAL_MODEL);
+         break;
+      case DPAD_DOWN:
+         desiredBodyHeight.add(-0.01);
+         break;
+      case DPAD_UP:
+         desiredBodyHeight.add(0.01);
+         break;
+      case DPAD:
+         if(event.getValue() == 0.25)
+         {
+            desiredBodyHeight.add(0.01);
+         }
+         else if(event.getValue() == 0.75)
+         {
+            desiredBodyHeight.add(-0.01);
+         }
+      default:
+         break;
+      }
+   }
+
+}
---
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/beetle/parameters/RhinoBeetleModelFactory.java
@@ -0,0 +1,84 @@
+package us.ihmc.exampleSimulations.beetle.parameters;
+
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
+import us.ihmc.robotModels.FullRobotModelFromDescription;
+import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
+import us.ihmc.SdfLoader.GeneralizedSDFRobotModel;
+import us.ihmc.SdfLoader.JaxbSDFLoader;
+import us.ihmc.SdfLoader.RobotDescriptionFromSDFLoader;
+import us.ihmc.robotics.partNames.JointNameMap;
+import us.ihmc.robotModels.FullRobotModel;
+import us.ihmc.robotModels.FullRobotModelFactory;
+import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
+import us.ihmc.multicastLogDataProtocol.modelLoaders.SDFLogModelProvider;
+import us.ihmc.robotics.robotDescription.RobotDescription;
+
+public class RhinoBeetleModelFactory implements FullRobotModelFactory
+{
+   private final RhinoBeetleSDFParameters sdfParameters;
+   private final RhinoBeetleJointNameMap jointNameMap;
+
+   private final GeneralizedSDFRobotModel generalizedSDFRobotModel;
+
+   private final RobotDescription robotDescription;
+
+   public RhinoBeetleModelFactory()
+   {
+      sdfParameters = new RhinoBeetleSDFParameters();
+      jointNameMap = new RhinoBeetleJointNameMap();
+
+      JaxbSDFLoader loader;
+      try
+      {
+         loader = new JaxbSDFLoader(sdfParameters.getSdfAsInputStream(), sdfParameters.getResourceDirectories(), null);
+         generalizedSDFRobotModel = loader.getGeneralizedSDFRobotModel(sdfParameters.getSdfModelName());
+      }
+      catch (FileNotFoundException | JAXBException e)
+      {
+         throw new RuntimeException("Cannot load model", e);
+      }
+
+      boolean useCollisionMeshes = false;
+
+      GeneralizedSDFRobotModel generalizedSDFRobotModel = getGeneralizedRobotModel();
+      RobotDescriptionFromSDFLoader descriptionLoader = new RobotDescriptionFromSDFLoader();
+      robotDescription = descriptionLoader.loadRobotDescriptionFromSDF(generalizedSDFRobotModel, getJointNameMap(), useCollisionMeshes);
+   }
+
+   private GeneralizedSDFRobotModel getGeneralizedRobotModel()
+   {
+      return generalizedSDFRobotModel;
+   }
+
+   public FloatingRootJointRobot createSdfRobot()
+   {
+
+      return new FloatingRootJointRobot(robotDescription);
+   }
+
+   public FullRobotModel createFullRobotModel()
+   {
+      String[] sensorLinksToTrack = new String[] {};
+      return new FullRobotModelFromDescription(robotDescription, getJointNameMap(), sensorLinksToTrack);
+   }
+
+   public LogModelProvider createLogModelProvider()
+   {
+      return new SDFLogModelProvider(sdfParameters);
+   }
+
+   @Override
+   public RobotDescription getRobotDescription()
+   {
+      return robotDescription;
+   }
+
+   public RhinoBeetleJointNameMap getJointNameMap()
+   {
+      return jointNameMap;
+   }
+   
+}
