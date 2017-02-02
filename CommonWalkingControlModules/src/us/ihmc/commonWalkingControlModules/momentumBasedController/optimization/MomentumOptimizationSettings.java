d0dcbabb564c0afb0e6fa7e129f3d14953192a04
Georg Wiedebach <gwiedebach@ihmc.us>
2017-02-02 13:33:27 -0600

    cleaned up the MomentumOptimizationSettings

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/optimization/MomentumOptimizationSettings.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/momentumBasedController/optimization/MomentumOptimizationSettings.java
@@ -5,58 +5,6 @@ import javax.vecmath.Vector3d;
 
 public abstract class MomentumOptimizationSettings
 {
-   public abstract void scaleForceWeights(double scale);
-
-   public abstract void setHeadWeights(double jointspace, double taskspace, double userMode);
-
-   public abstract void setBodyWeights(double chestWeight, double pelvisWeight);
-
-   public abstract void setBodyWeights(Vector3d chestAngularWeight, Vector3d pelvisAngularWeight);
-
-   public abstract void setHandTaskspaceControlWeights(Vector3d angular, Vector3d linear);
-
-   public abstract void setManipulationWeights(double jointspace, double taskspace, double userMode);
-
-   public abstract void setFootWeights(double support, double swing);
-
-   public abstract void setFootWeights(Vector3d supportAngular, Vector3d supportLinear, Vector3d swingAngular, Vector3d swingLinear);
-
-   public abstract void setPelvisWeight(double pelvisAngularWeight);
-
-   public abstract void setPelvisWeights(Vector3d pelvisAngularWeight);
-
-   public abstract void setMomentumWeight(double linearMomentumWeightX, double linearMomentumWeightY, double linearMomentumWeightZ, double angularMomentumWeightXY, double angularMomentumWeightZ);
-
-   public abstract void setMomentumWeight(double linearMomentumWeightXY, double linearMomentumWeightZ, double angularMomentumWeightXY, double angularMomentumWeightZ);
-
-   public abstract void setAngularMomentumWeight(double angularMomentumWeightXY, double angularMomentumWeightZ);
-
-   public abstract void setRhoPlaneContactRegularization(double rhoWeight);
-
-   public abstract void setRhoRateWeight(double defaultWeight, double highWeight);
-
-   public abstract void setJointWeight(double jointAccelerationWeight, double jointJerkWeight);
-
-   public abstract void setRhoMin(double rhoMin);
-
-   public abstract void setCoPWeight(double weightX, double weightY);
-
-   public abstract void setCoPWeight(Vector2d weight);
-
-   public abstract void setCoPRateDefaultWeight(double weightX, double weightY);
-
-   public abstract void setCoPRateDefaultWeight(Vector2d weight);
-
-   public abstract void setCoPRateHighWeight(double weightX, double weightY);
-
-   public abstract void setCoPRateHighWeight(Vector2d weight);
-
-   public abstract void setNumberOfBasisVectorsPerContactPoint(int nBasisVectorsPerContactPoint);
-
-   public abstract void setNumberOfContactPointsPerContactableBody(int nContactPointsPerContactableBody);
-
-   public abstract void setNumberOfContactableBodies(int nContactableBodies);
-
    public abstract Vector3d getLinearMomentumWeight();
 
    public abstract Vector3d getHighLinearMomentumWeightForRecovery();
