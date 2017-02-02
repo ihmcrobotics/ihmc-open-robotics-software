005dfd08b8360ed49ed27067a899580c9ea738d4
Edoardo Amadei <eamadei@ihmc.us>
2017-02-02 11:30:46 -0600

    Fixed time variables use in the SwingState class

--- ExampleSimulations/src/us/ihmc/exampleSimulations/planarWalker/WDIPeterPlanarWalkerController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/planarWalker/WDIPeterPlanarWalkerController.java
@@ -1,5 +1,6 @@
 package us.ihmc.exampleSimulations.planarWalker;
 
+import us.ihmc.quadrupedRobotics.controller.force.toolbox.QuadrupedFootStateMachine.FootEvent;
 import us.ihmc.robotics.MathTools;
 import us.ihmc.robotics.controllers.PIDController;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
@@ -13,8 +14,9 @@ import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.robotics.stateMachines.eventBasedStateMachine.FiniteStateMachine;
 import us.ihmc.robotics.stateMachines.eventBasedStateMachine.FiniteStateMachineBuilder;
+import us.ihmc.robotics.stateMachines.eventBasedStateMachine.FiniteStateMachineState;
 
-public class PeterPlanarWalkerControllerWDIStateMachine implements RobotController
+public class WDIPeterPlanarWalkerController implements RobotController
 {
    private final double MAX_HIP_ANGLE = 0.8;
    private double HIP_DEFUALT_P_GAIN = 100.0;
@@ -22,7 +24,7 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
 
    private double KNEE_DEFUALT_P_GAIN = 10000.0;
    private double KNEE_DEFUALT_D_GAIN = 1000.0;
-   
+
    private double deltaT;
 
    private PeterPlanarWalkerRobot robot;
@@ -50,26 +52,30 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
 
    private DoubleYoVariable kneeMoveStartTime = new DoubleYoVariable("kneeMoveStartTime", registry);
    private DoubleYoVariable startingHipAngle = new DoubleYoVariable("startingHipAngle", registry);
-   private DoubleYoVariable timeInState = new DoubleYoVariable("timeInState", registry);
    private DoubleYoVariable maxVelocityErrorAngle = new DoubleYoVariable("maxVelocityErrorAngle", registry);
 
    private DoubleYoVariable desiredBodyVelocity = new DoubleYoVariable("desiredBodyVelocity", registry);
    private DoubleYoVariable alphaFilterVariable = new DoubleYoVariable("alphaFilterVariable", registry);
-   private AlphaFilteredYoVariable filteredDesiredVelocity = new AlphaFilteredYoVariable("filteredDesiredVelocity", registry, alphaFilterVariable, desiredBodyVelocity);
-   
-   
+   private AlphaFilteredYoVariable filteredDesiredVelocity = new AlphaFilteredYoVariable("filteredDesiredVelocity", registry, alphaFilterVariable,
+                                                                                         desiredBodyVelocity);
+
    private YoMinimumJerkTrajectory trajectorySwingHip;
    private YoMinimumJerkTrajectory trajectorySwingKnee;
-    
+
    private EnumYoVariable<RobotSide> swingLeg = new EnumYoVariable<RobotSide>("swingLeg", registry, RobotSide.class);
 
-   private FiniteStateMachineBuilder<WalkerLegsControllerState, ControllerEvent> stateMachineBuilder;
-   private FiniteStateMachine<WalkerLegsControllerState, ControllerEvent> stateMachine;
+   private FiniteStateMachineBuilder<ControllerState, ControllerEvent> stateMachineBuilder;
+   private FiniteStateMachine<ControllerState, ControllerEvent> stateMachine;
+   private final DoubleYoVariable timestamp;
+   private final YoPlanarWalkerTimedStep stepCommand;
 
-   public PeterPlanarWalkerControllerWDIStateMachine(PeterPlanarWalkerRobot robot, double deltaT)
+   public WDIPeterPlanarWalkerController(PeterPlanarWalkerRobot robot, double deltaT, RobotSide side, DoubleYoVariable timestamp)
    {
+      String prefix = side.getLowerCaseName();
       this.robot = robot;
       this.deltaT = deltaT;
+      this.timestamp = timestamp;
+      this.stepCommand = new YoPlanarWalkerTimedStep(prefix + "StepCommand", registry);
 
       for (RobotSide robotSide : RobotSide.values)
       {
@@ -87,121 +93,143 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
       trajectorySwingHip = new YoMinimumJerkTrajectory("trajectorySwingHip", registry);
       trajectorySwingKnee = new YoMinimumJerkTrajectory("trajectorySwingKnee", registry);
       desiredHeight.set(robot.nominalHeight);
-      desiredKneeStance.set(robot.lowerLinkLength/2.0);
+      desiredKneeStance.set(robot.lowerLinkLength / 2.0);
       swingTime.set(0.3);
       scaleForVelToAngle.set(0.9);
       feedForwardGain.set(0.05);
       stepToStepHipAngleDelta.set(0.3);
       maxVelocityErrorAngle.set(0.3);
       alphaFilterVariable.set(0.9999);
-      
-      //initializeStateMachine();
+
+      initializeStateMachine(side);
    }
 
    @Override
    public void initialize()
    {
       // TODO Auto-generated method stub
-
    }
 
-//   private void initializeStateMachine()
-//   {
-//
-//      stateMachineBuilder = new FiniteStateMachineBuilder(WalkerLegsControllerState.class, ControllerEvent.class, "legState", registry);
-//        
-//      stateMachineBuilder.addState(WalkerLegsControllerState.LEFT_SUPPORT, new SingleSupportState(RobotSide.LEFT));
-//      stateMachineBuilder.addState(WalkerLegsControllerState.RIGHT_SUPPORT, new SingleSupportState(RobotSide.RIGHT));
-//      stateMachineBuilder.addTransition(ControllerEvent.DONE, WalkerLegsControllerState.RIGHT_SUPPORT, WalkerLegsControllerState.LEFT_SUPPORT);
-//      stateMachineBuilder.addTransition(ControllerEvent.DONE, WalkerLegsControllerState.LEFT_SUPPORT, WalkerLegsControllerState.RIGHT_SUPPORT);
-//      
-//      stateMachine = stateMachineBuilder.build(WalkerLegsControllerState.RIGHT_SUPPORT);
-//   }
-   
-//   private class SingleSupportState implements FiniteStateMachineState<ControllerEvent>
-//   {
-//      private RobotSide supportLeg;
-//     
-//      public SingleSupportState(RobotSide robotSide)
-//      {
-//            supportLeg = robotSide;
-//      }
-//
-//      public void doAction()
-//      {
-//         timeInState.set(this.getTimeInCurrentState());
-//         
-//       
-//
-//         //Swing Leg
-//         if ((this.getTimeInCurrentState() > swingTimeForThisStep.getDoubleValue()/2.0) && !initalizedKneeExtension.getBooleanValue())
-//         {
-//            double currentKneePosition = robot.getKneePosition(swingLeg.getEnumValue());
-//            trajectorySwingKnee.setParams(currentKneePosition, 0.0, 0.0, desiredKneeStance.getDoubleValue(), 0.0, 0.0, 0.0, swingTimeForThisStep.getDoubleValue()/2.0);
-//            initalizedKneeExtension.set(true);
-//            kneeMoveStartTime.set(this.getTimeInCurrentState());
-//         }
-//         else if((this.getTimeInCurrentState() > swingTimeForThisStep.getDoubleValue() && !initalizedKneeDoubleExtension.getBooleanValue()))
-//         {
-//            double currentKneePosition = robot.getKneePosition(swingLeg.getEnumValue());
-//            trajectorySwingKnee.setParams(currentKneePosition, 0.0, 0.0, desiredKneeStance.getDoubleValue() + 0.5, 0.0, 0.0, 0.0, 0.125);
-//            initalizedKneeDoubleExtension.set(true);
-//            kneeMoveStartTime.set(this.getTimeInCurrentState());
-//         }
-//         
-//         trajectorySwingKnee.computeTrajectory(this.getTimeInCurrentState() - kneeMoveStartTime.getDoubleValue());
-//         double desiredKneePositon = trajectorySwingKnee.getPosition();
-//         double desiredKneeVelocity = trajectorySwingKnee.getVelocity();
-//         controlKneeToPosition(swingLeg.getEnumValue(), desiredKneePositon, desiredKneeVelocity);
-//
-//         desiredSwingLegHipAngle.set(getDesireHipAngle());
-//         trajectorySwingHip.setParams(startingHipAngle.getDoubleValue(), 0.0, 0.0, desiredSwingLegHipAngle.getDoubleValue(), 0.0, 0.0, 0.0, swingTimeForThisStep.getDoubleValue());
-//         
-//         trajectorySwingHip.computeTrajectory(this.getTimeInCurrentState());
-//         double desiredHipAngle = trajectorySwingHip.getPosition();
-//         double currentHipAngle = robot.getHipPosition(swingLeg.getEnumValue());
-//         double currentHipAngleRate = robot.getHipVelocity(swingLeg.getEnumValue());
-//         
-//         PIDController pidController = hipControllers.get(swingLeg.getEnumValue());
-//         double controlEffort = pidController.compute(currentHipAngle, desiredHipAngle, currentHipAngleRate, 0.0, deltaT);
-//         robot.setHipTorque(swingLeg.getEnumValue(), controlEffort);
-//         
-//         
-//         //Stance leg
-//         controlHipToMaintainPitch(supportLeg);
-//         
-//         //add swing leg torque to stand leg
-//         addOppositeLegHipTorque(supportLeg);
-//         
-//         //controlKneeToMaintainBodyHeight(supportLeg);
-//         controlKneeToPosition(supportLeg, desiredKneeStance.getDoubleValue(), 0.0);
-//         
-//         if (initalizedKneeExtension.getBooleanValue() && robot.isFootOnGround(swingLeg.getEnumValue()))
-//            this.transitionToDefaultNextState();
-//      }
-//
-//      @Override
-//      public void onEntry()
-//      {
-//         // TODO Auto-generated method stub
-//         
-//      }
-//
-//      @Override
-//      public ControllerEvent process()
-//      {
-//         // TODO Auto-generated method stub
-//         return null;
-//      }
-//
-//      @Override
-//      public void onExit()
-//      {
-//         // TODO Auto-generated method stub
-//         
-//      }
-//   }
-   
+   private void initializeStateMachine(RobotSide supportLegSide)
+   {
+      stateMachineBuilder = new FiniteStateMachineBuilder(ControllerState.class, ControllerEvent.class, "stateMachineBuilder", registry);
+
+      stateMachineBuilder.addState(ControllerState.SUPPORT, new SupportState(supportLegSide));
+      stateMachineBuilder.addState(ControllerState.SWING, new SwingState(supportLegSide));
+      stateMachineBuilder.addTransition(ControllerEvent.TIMEOUT, ControllerState.SUPPORT, ControllerState.SWING);
+      stateMachineBuilder.addTransition(ControllerEvent.TIMEOUT, ControllerState.SWING, ControllerState.SUPPORT);
+
+      stateMachine = stateMachineBuilder.build(ControllerState.SUPPORT);
+   }
+
+   private class SupportState implements FiniteStateMachineState<ControllerEvent>
+   {
+      private RobotSide supportLeg;
+
+      public SupportState(RobotSide robotSide)
+      {
+         supportLeg = robotSide;
+      }
+
+      @Override
+      public void onEntry()
+      {
+      }
+
+      @Override
+      public ControllerEvent process()
+      {
+         controlHipToMaintainPitch(supportLeg);
+
+         //add swing leg torque to stand leg
+         addOppositeLegHipTorque(supportLeg);
+
+         //controlKneeToMaintainBodyHeight(supportLeg);
+         controlKneeToPosition(supportLeg, desiredKneeStance.getDoubleValue(), 0.0);
+
+         if (initalizedKneeExtension.getBooleanValue() && robot.isFootOnGround(swingLeg.getEnumValue()))
+         {
+            return ControllerEvent.TIMEOUT;
+         }
+         return null;
+      }
+
+      @Override
+      public void onExit()
+      {
+      }
+   }
+
+   private class SwingState implements FiniteStateMachineState<ControllerEvent>
+   {
+      private RobotSide swingLeg;
+
+      public SwingState(RobotSide robotSide)
+      {
+         swingLeg = robotSide;
+      }
+
+      @Override
+      public void onEntry()
+      {
+      }
+
+      @Override
+      public ControllerEvent process()
+      {
+         double currentTime = timestamp.getDoubleValue();
+         double liftOffTime = stepCommand.getTimeInterval().getStartTime();
+         double touchDownTime = stepCommand.getTimeInterval().getEndTime();
+         double timeInCurrentState = currentTime - liftOffTime;
+
+         if ((timeInCurrentState > swingTimeForThisStep.getDoubleValue() / 2.0) && !initalizedKneeExtension.getBooleanValue())
+         {
+            double currentKneePosition = robot.getKneePosition(swingLeg);
+            trajectorySwingKnee.setParams(currentKneePosition, 0.0, 0.0, desiredKneeStance.getDoubleValue(), 0.0, 0.0, 0.0,
+                                          swingTimeForThisStep.getDoubleValue() / 2.0);
+            initalizedKneeExtension.set(true);
+            kneeMoveStartTime.set(timeInCurrentState);
+         }
+         else if ((timeInCurrentState > swingTimeForThisStep.getDoubleValue() && !initalizedKneeDoubleExtension.getBooleanValue()))
+         {
+            double currentKneePosition = robot.getKneePosition(swingLeg);
+            trajectorySwingKnee.setParams(currentKneePosition, 0.0, 0.0, desiredKneeStance.getDoubleValue() + 0.5, 0.0, 0.0, 0.0, 0.125);
+            initalizedKneeDoubleExtension.set(true);
+            kneeMoveStartTime.set(timeInCurrentState);
+         }
+
+         trajectorySwingKnee.computeTrajectory(timeInCurrentState - kneeMoveStartTime.getDoubleValue());
+         double desiredKneePositon = trajectorySwingKnee.getPosition();
+         double desiredKneeVelocity = trajectorySwingKnee.getVelocity();
+         controlKneeToPosition(swingLeg, desiredKneePositon, desiredKneeVelocity);
+
+         desiredSwingLegHipAngle.set(getDesireHipAngle());
+         trajectorySwingHip.setParams(startingHipAngle.getDoubleValue(), 0.0, 0.0, desiredSwingLegHipAngle.getDoubleValue(), 0.0, 0.0, 0.0,
+                                      swingTimeForThisStep.getDoubleValue());
+
+         trajectorySwingHip.computeTrajectory(timeInCurrentState);
+         double desiredHipAngle = trajectorySwingHip.getPosition();
+         double currentHipAngle = robot.getHipPosition(swingLeg);
+         double currentHipAngleRate = robot.getHipVelocity(swingLeg);
+
+         PIDController pidController = hipControllers.get(swingLeg);
+         double controlEffort = pidController.compute(currentHipAngle, desiredHipAngle, currentHipAngleRate, 0.0, deltaT);
+         robot.setHipTorque(swingLeg, controlEffort);
+         
+         if (currentTime >= touchDownTime)
+         {
+            return ControllerEvent.TIMEOUT;
+         }
+         else
+            return null;
+      }
+
+      @Override
+      public void onExit()
+      {
+      }
+   }
+
    private double getDesireHipAngle()
    {
       double legLength = robot.upperLinkLength + desiredKneeStance.getDoubleValue();
@@ -210,25 +238,22 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
 
       //only use a fraction of it
       //angleForCapture.set(0.8 *angleForCapture.getDoubleValue());
-      
+
       //limit this angle
       angleForCapture.set(MathTools.clipToMinMax(angleForCapture.getDoubleValue(), MAX_HIP_ANGLE));
-      
+
       //angle is opposite sign of desired velocity
       double velocityError = (filteredDesiredVelocity.getDoubleValue() - robot.getBodyVelocity());
       velocityErrorAngle.set(velocityError * scaleForVelToAngle.getDoubleValue());
       velocityErrorAngle.set(MathTools.clipToMinMax(velocityErrorAngle.getDoubleValue(), maxVelocityErrorAngle.getDoubleValue()));
 
-      
-      
       feedForwardAngle.set(filteredDesiredVelocity.getDoubleValue() * feedForwardGain.getDoubleValue());
       double angle = angleForCapture.getDoubleValue() + feedForwardAngle.getDoubleValue() + velocityErrorAngle.getDoubleValue();
-    
-      angle = MathTools.clipToMinMax(angle, lastStepHipAngle.getDoubleValue() - stepToStepHipAngleDelta.getDoubleValue(), lastStepHipAngle.getDoubleValue() + stepToStepHipAngleDelta.getDoubleValue());
+
+      angle = MathTools.clipToMinMax(angle, lastStepHipAngle.getDoubleValue() - stepToStepHipAngleDelta.getDoubleValue(),
+                                     lastStepHipAngle.getDoubleValue() + stepToStepHipAngleDelta.getDoubleValue());
       return angle;
    }
-   
-  
 
    private void controlHipToMaintainPitch(RobotSide robotSide)
    {
@@ -238,7 +263,7 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
       double controlEffort = -hipControllers.get(robotSide).compute(currentPitch, desiredPitch.getDoubleValue(), currentPitchRate, 0.0, deltaT);
       robot.setHipTorque(robotSide, controlEffort);
    }
-   
+
    private void addOppositeLegHipTorque(RobotSide legToAddTorque)
    {
       double oppositeLegTorque = robot.getHipTorque(legToAddTorque.getOppositeSide());
@@ -254,7 +279,7 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
       double controlEffort = kneeControllers.get(robotSide).compute(currentHeight, desiredHeight.getDoubleValue(), currentHeightRate, 0.0, deltaT);
       robot.setKneeTorque(robotSide, controlEffort);
    }
-   
+
    private void controlKneeToPosition(RobotSide robotSide, double desiredPosition, double desiredVelocity)
    {
       double kneePosition = robot.getKneePosition(robotSide);
@@ -263,7 +288,6 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
       double controlEffort = kneeControllers.get(robotSide).compute(kneePosition, desiredPosition, kneePositionRate, desiredVelocity, deltaT);
       robot.setKneeTorque(robotSide, controlEffort);
    }
-
 
    @Override
    public YoVariableRegistry getYoVariableRegistry()
@@ -274,44 +298,39 @@ public class PeterPlanarWalkerControllerWDIStateMachine implements RobotControll
    @Override
    public String getName()
    {
-      // TODO Auto-generated method stub
       return null;
    }
 
    @Override
    public String getDescription()
    {
-      // TODO Auto-generated method stub
       return null;
    }
 
    @Override
    public void doControl()
    {
-//      for (RobotSide robotSide : RobotSide.values)
-//      {
-//         double currentPosition = robot.getKneePosition(robotSide);
-//         double currentVelocity = robot.getKneeVelocity(robotSide);
-//
-//         double effort = kneeControllers.get(robotSide).compute(currentPosition, desiredKneeExtension.getDoubleValue(), currentVelocity, 0.0, deltaT);
-//         robot.setKneeTorque(robotSide, effort);
-//      }
+      //      for (RobotSide robotSide : RobotSide.values)
+      //      {
+      //         double currentPosition = robot.getKneePosition(robotSide);
+      //         double currentVelocity = robot.getKneeVelocity(robotSide);
+      //
+      //         double effort = kneeControllers.get(robotSide).compute(currentPosition, desiredKneeExtension.getDoubleValue(), currentVelocity, 0.0, deltaT);
+      //         robot.setKneeTorque(robotSide, effort);
+      //      }
 
-//      filteredDesiredVelocity.update();
-//      stateMachine.doAction();
-//
-//      stateMachine.checkTransitionConditions();
-
+      filteredDesiredVelocity.update();
+      stateMachine.process();
    }
 
-   public enum WalkerLegsControllerState
+   public enum ControllerState
    {
-      LEFT_SUPPORT, RIGHT_SUPPORT;
+      SUPPORT, SWING;
    }
-   
+
    public enum ControllerEvent
    {
-      DONE, FAIL;
+      TIMEOUT;
    }
 
 }
