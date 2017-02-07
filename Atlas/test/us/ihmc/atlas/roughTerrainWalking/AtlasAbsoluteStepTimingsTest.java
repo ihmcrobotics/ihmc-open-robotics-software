1bb2ebc889436160c877d78350690f71e8dd121f
Georg Wiedebach <gwiedebach@ihmc.us>
2017-02-07 16:09:25 -0600

    add unit test for absolute step timing making sure the swing starts at the correct time

---
+++ Atlas/test/us/ihmc/atlas/roughTerrainWalking/AtlasAbsoluteStepTimingsTest.java
@@ -0,0 +1,37 @@
+package us.ihmc.atlas.roughTerrainWalking;
+
+import org.junit.Test;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.roughTerrainWalking.AvatarAbsoluteStepTimingsTest;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
+import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.tools.continuousIntegration.IntegrationCategory;
+
+@ContinuousIntegrationPlan(categories = {IntegrationCategory.SLOW})
+public class AtlasAbsoluteStepTimingsTest extends AvatarAbsoluteStepTimingsTest
+{
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 75.0)
+   @Test(timeout = 500000)
+   public void testTakingStepsWithAbsoluteTimings() throws SimulationExceededMaximumTimeException
+   {
+      super.testTakingStepsWithAbsoluteTimings();
+   }
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, DRCRobotModel.RobotTarget.SCS, false);
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+}
