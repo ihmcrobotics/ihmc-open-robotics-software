9ab1ddab0e33b754b37182c20bc1d256c1ca37aa
Robert <rgriffin@ihmc.us>
2017-02-09 14:30:36 -0500

    fixed the step adjustment demo test

--- Atlas/test/us/ihmc/atlas/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/AtlasStepAdjustmentDemoTest.java
+++ Atlas/test/us/ihmc/atlas/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/AtlasStepAdjustmentDemoTest.java
@@ -12,9 +12,9 @@ import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
 import us.ihmc.simulationconstructionset.bambooTools.SimulationTestingParameters;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.tools.MemoryTools;
-import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
-import us.ihmc.tools.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
-import us.ihmc.tools.continuousIntegration.IntegrationCategory;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.continuousIntegration.IntegrationCategory;
 import us.ihmc.tools.thread.ThreadTools;
 
 @ContinuousIntegrationPlan(categories = {IntegrationCategory.SLOW, IntegrationCategory.VIDEO})
