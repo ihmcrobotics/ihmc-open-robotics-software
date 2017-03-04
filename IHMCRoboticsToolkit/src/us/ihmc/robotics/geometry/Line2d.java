141bc2627822a3c45eda7c0cca575e9d61856aa2
Sylvain Bertrand <sbertrand@ihmc.us>
2017-03-03 18:55:02 -0600

    Deleted the interface Geometry2d.

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/geometry/Line2d.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/geometry/Line2d.java
@@ -1,5 +1,6 @@
 package us.ihmc.robotics.geometry;
 
+import us.ihmc.euclid.interfaces.GeometryObject;
 import us.ihmc.euclid.transform.interfaces.Transform;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.Vector2D;
@@ -14,7 +15,7 @@ import us.ihmc.robotics.robotSide.RobotSide;
 /**
  * @author Twan Koolen
  */
-public class Line2d implements Geometry2d<Line2d>
+public class Line2d implements GeometryObject<Line2d>
 {
    // TODO: think about usage of epsilons in the methods.
 
@@ -339,7 +340,6 @@ public class Line2d implements Geometry2d<Line2d>
       return new Line2d(point, perpendicularVector());
    }
 
-   @Override
    public Point2D intersectionWith(LineSegment2d lineSegment)
    {
       return lineSegment.intersectionWith(this);
@@ -351,7 +351,6 @@ public class Line2d implements Geometry2d<Line2d>
       return normalizedVector.dot(line.getNormalizedVector()) < 1e-7;
    }
 
-   @Override
    public Point2D intersectionWith(Line2d secondLine)
    {
       return GeometryTools.getIntersectionBetweenTwoLines(point, normalizedVector, secondLine.point, secondLine.normalizedVector);
@@ -370,13 +369,11 @@ public class Line2d implements Geometry2d<Line2d>
       return GeometryTools.getIntersectionBetweenTwoLines(point, normalizedVector, secondLine.point, secondLine.normalizedVector, intersectionToPack);
    }
 
-   @Override
    public Point2D[] intersectionWith(ConvexPolygon2d convexPolygon)
    {
       return convexPolygon.intersectionWith(this);
    }
 
-   @Override
    public double distance(Point2DReadOnly point)
    {
       tempPoint2d.set(this.point);
@@ -392,19 +389,16 @@ public class Line2d implements Geometry2d<Line2d>
       return distance * distance;
    }
 
-   @Override
    public double distance(Line2d line)
    {
       throw new RuntimeException("Not yet implemented");
    }
 
-   @Override
    public double distance(LineSegment2d lineSegment)
    {
       throw new RuntimeException("Not yet implemented");
    }
 
-   @Override
    public double distance(ConvexPolygon2d convexPolygon)
    {
       throw new RuntimeException("Not yet implemented");
@@ -427,14 +421,12 @@ public class Line2d implements Geometry2d<Line2d>
       normalizedVector.applyTransform(transform);
    }
 
-   @Override
    public void applyTransformAndProjectToXYPlane(Transform transform)
    {
       point.applyTransform(transform, false);
       normalizedVector.applyTransform(transform, false);
    }
 
-   @Override
    public Line2d applyTransformCopy(Transform transform)
    {
       Line2d copy = new Line2d(this);
@@ -442,7 +434,6 @@ public class Line2d implements Geometry2d<Line2d>
       return copy;
    }
 
-   @Override
    public Line2d applyTransformAndProjectToXYPlaneCopy(Transform transform)
    {
       Line2d copy = new Line2d(this);
@@ -565,7 +556,6 @@ public class Line2d implements Geometry2d<Line2d>
     * 
     * @param point2d the point to project on this line. Modified.
     */
-   @Override
    public void orthogonalProjection(Point2DBasics point2d)
    {
       GeometryTools.getOrthogonalProjectionOnLine(point2d, point, normalizedVector, point2d);
@@ -580,7 +570,6 @@ public class Line2d implements Geometry2d<Line2d>
     * @param point2d the point to compute the projection of. Not modified.
     * @return the projection of the point onto the line or {@code null} if the method failed.
     */
-   @Override
    public Point2D orthogonalProjectionCopy(Point2DReadOnly point2d)
    {
       Point2D projection = new Point2D();
