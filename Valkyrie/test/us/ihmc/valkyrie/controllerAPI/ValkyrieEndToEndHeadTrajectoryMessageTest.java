193b952a60c5210fc4f1b66d678fda4271b56437
Georg Wiedebach <gwiedebach@ihmc.us>
2017-03-06 13:47:00 -0600

    start making better tests for head trajectories

--- Valkyrie/test/us/ihmc/valkyrie/controllerAPI/ValkyrieEndToEndHeadTrajectoryMessageTest.java
+++ Valkyrie/test/us/ihmc/valkyrie/controllerAPI/ValkyrieEndToEndHeadTrajectoryMessageTest.java
@@ -1,14 +1,25 @@
 package us.ihmc.valkyrie.controllerAPI;
 
+import org.junit.Test;
+
 import us.ihmc.avatar.controllerAPI.EndToEndHeadTrajectoryMessageTest;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.DRCRobotModel.RobotTarget;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 
 public class ValkyrieEndToEndHeadTrajectoryMessageTest extends EndToEndHeadTrajectoryMessageTest
 {
    private final ValkyrieRobotModel robotModel = new ValkyrieRobotModel(RobotTarget.SCS, false);
+
+   @ContinuousIntegrationTest(estimatedDuration = 17.9)
+   @Test(timeout = 89000)
+   public void testSingleWaypoint() throws SimulationExceededMaximumTimeException
+   {
+      super.testSingleWaypoint();
+   }
 
    @Override
    public DRCRobotModel getRobotModel()
