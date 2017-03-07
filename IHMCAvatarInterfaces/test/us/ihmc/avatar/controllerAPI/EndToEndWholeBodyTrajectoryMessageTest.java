4a37a56d02233c754459a295d334081d1b9700fd
bshrewsbury <bshrewsbury@ihmc.us>
2017-03-07 14:30:53 -0600

    fix compile errors

--- IHMCAvatarInterfaces/test/us/ihmc/avatar/controllerAPI/EndToEndWholeBodyTrajectoryMessageTest.java
+++ IHMCAvatarInterfaces/test/us/ihmc/avatar/controllerAPI/EndToEndWholeBodyTrajectoryMessageTest.java
@@ -145,8 +145,9 @@ public abstract class EndToEndWholeBodyTrajectoryMessageTest implements MultiRob
       assertTrue(success);
 
       SimulationConstructionSet scs = drcSimulationTestHelper.getSimulationConstructionSet();
-
-      EndToEndChestTrajectoryMessageTest.assertSingleWaypointExecuted(desiredChestOrientation, scs);
+      RigidBody chest = fullRobotModel.getChest();
+      
+      EndToEndChestTrajectoryMessageTest.assertSingleWaypointExecuted(desiredChestOrientation, scs, chest);
 //      EndToEndPelvisTrajectoryMessageTest.assertSingleWaypointExecuted(desiredPosition, desiredOrientation, scs);
       EndToEndFootTrajectoryMessageTest.assertSingleWaypointExecuted(footSide, desiredFootPose.getFramePointCopy().getPoint(), desiredFootPose.getFrameOrientationCopy().getQuaternion(), scs);
       for (RobotSide robotSide : RobotSide.values)
