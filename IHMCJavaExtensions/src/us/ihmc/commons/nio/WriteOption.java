6bb9f03f4a467af7eb929de39e173e2e6dc13288
Duncan Calvert <dcalvert@ihmc.us>
2017-03-09 17:09:15 -0600

    Document DefaultExceptionHandler and WriteOption.

--- IHMCJavaExtensions/src/us/ihmc/commons/nio/WriteOption.java
+++ IHMCJavaExtensions/src/us/ihmc/commons/nio/WriteOption.java
@@ -3,9 +3,16 @@ package us.ihmc.commons.nio;
 import java.nio.file.OpenOption;
 import java.nio.file.StandardOpenOption;
 
+/**
+ * Write options for files. Subset of {@link StandardOpenOption} for convenience.
+ */
 public enum WriteOption
 {
-   APPEND(StandardOpenOption.CREATE, StandardOpenOption.APPEND), TRUNCATE(StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+   /** Append to the end of the file. */
+   APPEND(StandardOpenOption.CREATE, StandardOpenOption.APPEND),
+   
+   /** Start overwriting at the beginning of the file. */
+   TRUNCATE(StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
 
    private final StandardOpenOption[] openOptions;
 
