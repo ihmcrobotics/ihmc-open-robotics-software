a4bcf13af86dc1c33062177b7893c851688e9326
Georg Wiedebach <gwiedebach@ihmc.us>
2017-03-09 14:03:03 -0600

    create helper for position controlled joints

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/command/inverseDynamics/JointAccelerationIntegrationSettings.java
@@ -0,0 +1,10 @@
+package us.ihmc.commonWalkingControlModules.controllerCore.command.inverseDynamics;
+
+public class JointAccelerationIntegrationSettings
+{
+   private double alphaPosition = Double.NaN;
+   private double alphaVelocity = Double.NaN;
+   private double maxPositionError = Double.NaN;
+   private double maxVelocity = Double.NaN;
+
+}
