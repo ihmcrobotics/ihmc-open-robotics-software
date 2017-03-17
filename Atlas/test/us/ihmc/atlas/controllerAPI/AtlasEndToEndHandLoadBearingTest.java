340e102ec97c238b241b198742db4852c28c7d85
Georg Wiedebach <gwiedebach@ihmc.us>
2017-03-17 10:54:07 -0500

    added an empty test

---
+++ Atlas/test/us/ihmc/atlas/controllerAPI/AtlasEndToEndHandLoadBearingTest.java
@@ -0,0 +1,25 @@
+package us.ihmc.atlas.controllerAPI;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.avatar.controllerAPI.EndToEndHandLoadBearingTest;
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.simulationconstructionset.bambooTools.BambooTools;
+
+public class AtlasEndToEndHandLoadBearingTest extends EndToEndHandLoadBearingTest
+{
+   private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, DRCRobotModel.RobotTarget.SCS, false);
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+
+}
