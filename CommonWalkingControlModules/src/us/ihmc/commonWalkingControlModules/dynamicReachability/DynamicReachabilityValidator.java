617ec229bc73ef8deb30497e17c3bfba0603d1f7
Robert <rgriffin@ihmc.us>
2017-03-20 14:01:59 -0400

    got the dynamic reachability prediction working as a pretty good first cut

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/dynamicReachability/DynamicReachabilityValidator.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/dynamicReachability/DynamicReachabilityValidator.java
@@ -1,6 +1,11 @@
 package us.ihmc.commonWalkingControlModules.dynamicReachability;
 
 import us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.ICPPlanner;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.graphicsDescription.appearance.YoAppearance;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicPosition;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.humanoidRobotics.footstep.Footstep;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.MathTools;
@@ -8,14 +13,22 @@ import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.dataStructures.variable.BooleanYoVariable;
 import us.ihmc.robotics.dataStructures.variable.DoubleYoVariable;
 import us.ihmc.robotics.geometry.*;
+import us.ihmc.robotics.math.frames.YoFramePoint;
+import us.ihmc.robotics.math.frames.YoFramePoint2d;
 import us.ihmc.robotics.partNames.LegJointName;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+import us.ihmc.robotics.referenceFrames.TranslationReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.robotics.screwTheory.OneDoFJoint;
+import us.ihmc.robotics.screwTheory.ScrewTools;
+
+import javax.naming.Reference;
 
 public class DynamicReachabilityValidator
 {
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
 
    private final DoubleYoVariable minimumLegLength = new DoubleYoVariable("minimumLegLength", registry);
@@ -23,22 +36,27 @@ public class DynamicReachabilityValidator
 
    private final DoubleYoVariable maximumDesiredKneeBend = new DoubleYoVariable("maximumDesiredKneeBend", registry);
 
+   private final DoubleYoVariable stanceLegMinimumHeight = new DoubleYoVariable("stanceLegMinimumHeight", registry);
+   private final DoubleYoVariable stanceLegMaximumHeight = new DoubleYoVariable("stanceLegMaximumHeight", registry);
+   private final DoubleYoVariable swingLegMinimumHeight = new DoubleYoVariable("swingLegMinimumHeight", registry);
+   private final DoubleYoVariable swingLegMaximumHeight = new DoubleYoVariable("swingLegMaximumHeight", registry);
+
    private final BooleanYoVariable reachableWRTStanceFoot = new BooleanYoVariable("reachableWRTStanceFoot", registry);
    private final BooleanYoVariable reachableWRTFootstep = new BooleanYoVariable("reachableWRTFootstep", registry);
    private final BooleanYoVariable isStepReachable = new BooleanYoVariable("isStepReachable", registry);
 
+   private final SideDependentList<YoFramePoint> ankleLocations = new SideDependentList<>();
+   private final SideDependentList<YoFramePoint2d> hipLocations = new SideDependentList<>();
+   private final SideDependentList<YoFramePoint> hipMinimumLocations = new SideDependentList<>();
+   private final SideDependentList<YoFramePoint> hipMaximumLocations = new SideDependentList<>();
 
-   private final SideDependentList<FrameVector> centerOfMassOffsetFromHips = new SideDependentList<>();
-   private final SideDependentList<FrameVector> soleFrameOffsetFromAnkles = new SideDependentList<>();
+   private final YoFramePoint finalCoM;
 
    private final ICPPlanner icpPlanner;
    private final FullHumanoidRobotModel fullRobotModel;
 
    private Footstep nextFootstep;
 
-   private final ReferenceFrame centerOfMassFrame;
-
-   private final FramePoint centerOfMassPosition = new FramePoint();
    private final FramePoint tempHipPoint = new FramePoint();
    private final FramePoint tempAnklePoint = new FramePoint();
 
@@ -51,20 +69,52 @@ public class DynamicReachabilityValidator
    private final LineSegment1d stanceHeightLine = new LineSegment1d();
    private final LineSegment1d stepHeightLine = new LineSegment1d();
 
+   private final ReferenceFrame predictedCoMFrame;
+   private final TranslationReferenceFrame predictedPelvisFrame;
+   private final SideDependentList<ReferenceFrame> predictedHipFrames = new SideDependentList<>();
+
+   private final FramePose predictedPelvisPose = new FramePose();
+
+   private final FramePoint predictedCoMPosition = new FramePoint();
+   private final FrameOrientation predictedPelvisOrientation = new FrameOrientation();
+
    public DynamicReachabilityValidator(ICPPlanner icpPlanner, FullHumanoidRobotModel fullRobotModel, ReferenceFrame centerOfMassFrame,
-         YoVariableRegistry parentRegistry)
+         YoVariableRegistry parentRegistry, YoGraphicsListRegistry yoGraphicsListRegistry)
    {
       this.icpPlanner = icpPlanner;
       this.fullRobotModel = fullRobotModel;
-      this.centerOfMassFrame = centerOfMassFrame;
 
-      maximumDesiredKneeBend.set(0.4);
+      maximumDesiredKneeBend.set(1.0);
 
+      String listName = "Dynamic Reachability";
       for (RobotSide side : RobotSide.values)
       {
-         centerOfMassOffsetFromHips.put(side, new FrameVector());
-         soleFrameOffsetFromAnkles.put(side, new FrameVector());
+         YoFramePoint ankleLocation = new YoFramePoint(side.getShortLowerCaseName() + "PredictedAnklePoint", worldFrame, registry);
+         YoFramePoint2d hipLocation = new YoFramePoint2d(side.getShortLowerCaseName() + "PredictedHipPoint", worldFrame, registry);
+         YoFramePoint hipMaximumLocation = new YoFramePoint(side.getShortLowerCaseName() + "PredictedHipMaximumPoint", worldFrame, registry);
+         YoFramePoint hipMinimumLocation = new YoFramePoint(side.getShortLowerCaseName() + "PredictedHipMinimumPoint", worldFrame, registry);
+         ankleLocations.put(side, ankleLocation);
+         hipLocations.put(side, hipLocation);
+         hipMaximumLocations.put(side, hipMaximumLocation);
+         hipMinimumLocations.put(side, hipMinimumLocation);
+
+         YoGraphicPosition ankleLocationViz = new YoGraphicPosition(side.getSideNameFirstLetter() + "Predicted Ankle Point", ankleLocation, 0.05, YoAppearance.AliceBlue());
+         YoGraphicPosition hipLocationViz = new YoGraphicPosition(side.getSideNameFirstLetter() + "Predicted Hip Point", hipLocation, 0.01, YoAppearance.ForestGreen());
+         YoGraphicPosition hipMaximumLocationViz = new YoGraphicPosition(side.getSideNameFirstLetter() + "Predicted Maximum Hip Point", hipMaximumLocation, 0.05, YoAppearance.ForestGreen());
+         YoGraphicPosition hipMinimumLocationViz = new YoGraphicPosition(side.getSideNameFirstLetter() + "Predicted Minimum Hip Point", hipMinimumLocation, 0.05, YoAppearance.Blue());
+
+         yoGraphicsListRegistry.registerYoGraphic(listName, ankleLocationViz);
+         yoGraphicsListRegistry.registerYoGraphic(listName, hipMaximumLocationViz);
+         yoGraphicsListRegistry.registerYoGraphic(listName, hipMinimumLocationViz);
+
+         yoGraphicsListRegistry.registerArtifact(listName, ankleLocationViz.createArtifact());
+         yoGraphicsListRegistry.registerArtifact(listName, hipLocationViz.createArtifact());
       }
+
+      finalCoM = new YoFramePoint("PredictedFinalCoM", worldFrame, registry);
+      YoGraphicPosition finalCoMViz = new YoGraphicPosition("Predicted Final CoM", finalCoM, 0.01, YoAppearance.Black(), YoGraphicPosition.GraphicType.BALL_WITH_ROTATED_CROSS);
+      yoGraphicsListRegistry.registerYoGraphic(listName, finalCoMViz);
+      yoGraphicsListRegistry.registerArtifact(listName, finalCoMViz.createArtifact());
 
       // compute leg lengths
       ReferenceFrame hipPitchFrame = fullRobotModel.getLegJoint(RobotSide.LEFT, LegJointName.HIP_PITCH).getFrameAfterJoint();
@@ -80,6 +130,43 @@ public class DynamicReachabilityValidator
       tempAnklePoint.changeFrame(kneePitchFrame);
 
       shinLength = tempKneePoint.distance(tempAnklePoint);
+
+      ReferenceFrame pelvisFrame = fullRobotModel.getPelvis().getBodyFixedFrame();
+      FramePoint pelvis = new FramePoint(pelvisFrame);
+      FramePoint com = new FramePoint(centerOfMassFrame);
+      pelvis.changeFrame(centerOfMassFrame);
+      FrameVector translationToCoM = new FrameVector(centerOfMassFrame);
+      translationToCoM.set(com);
+      translationToCoM.sub(pelvis);
+      translationToCoM.changeFrame(pelvisFrame);
+
+      predictedCoMFrame = new ReferenceFrame("Predicted CoM Position", worldFrame, false, false, false)
+      {
+         @Override
+         protected void updateTransformToParent(RigidBodyTransform transformToParent)
+         {
+            predictedCoMPosition.changeFrame(worldFrame);
+            predictedPelvisOrientation.changeFrame(worldFrame);
+            transformToParent.setTranslation(predictedCoMPosition.getPoint());
+            transformToParent.setRotation(predictedPelvisOrientation.getQuaternion());
+         }
+      };
+
+      predictedPelvisFrame = new TranslationReferenceFrame("Predicted Pelvis Frame", predictedCoMFrame);
+      predictedPelvisFrame.updateTranslation(translationToCoM.getVector());
+
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         FrameVector translationToPelvis = new FrameVector(pelvisFrame);
+         FramePoint pelvisCenter = new FramePoint(pelvisFrame);
+         FramePoint hipJoint = new FramePoint(fullRobotModel.getLegJoint(robotSide, LegJointName.HIP_PITCH).getFrameAfterJoint());
+         hipJoint.changeFrame(pelvisFrame);
+         translationToPelvis.set(hipJoint);
+         translationToPelvis.sub(pelvisCenter);
+         TranslationReferenceFrame predictedHipFrame = new TranslationReferenceFrame(robotSide.getShortLowerCaseName() + " Predicted Hip Frame", predictedPelvisFrame);
+         predictedHipFrame.updateTranslation(translationToPelvis.getVector());
+         predictedHipFrames.put(robotSide, predictedHipFrame);
+      }
 
       parentRegistry.addChild(registry);
    }
@@ -98,7 +185,7 @@ public class DynamicReachabilityValidator
    {
       RobotSide supportSide = nextFootstep.getRobotSide().getOppositeSide();
 
-      updateOffsets();
+      updateFrames(nextFootstep);
       updateLegLengthLimits();
 
       computeHeightLineFromStance(supportSide);
@@ -111,30 +198,17 @@ public class DynamicReachabilityValidator
       return isStepReachable.getBooleanValue();
    }
 
-   private void updateOffsets()
+   private void updateFrames(Footstep nextFootstep)
    {
-      centerOfMassPosition.setToZero(centerOfMassFrame);
+      icpPlanner.getFinalDesiredCenterOfMassPosition(tempFinalCoM);
+      finalCoM.setXY(tempFinalCoM);
+      predictedCoMPosition.setXY(tempFinalCoM);
+      predictedPelvisOrientation.setToZero(fullRobotModel.getPelvis().getBodyFixedFrame()); // // TODO: 3/20/17  make more advanced
 
+      predictedCoMFrame.update();
+      predictedPelvisFrame.update();
       for (RobotSide robotSide : RobotSide.values)
-      {
-         OneDoFJoint hipPitchJoint = fullRobotModel.getLegJoint(robotSide, LegJointName.HIP_PITCH);
-         tempHipPoint.setToZero(hipPitchJoint.getFrameBeforeJoint());
-         tempHipPoint.changeFrame(centerOfMassFrame);
-
-         FrameVector centerOfMassOffsetFromHip = centerOfMassOffsetFromHips.get(robotSide);
-         centerOfMassOffsetFromHip.setToZero(centerOfMassFrame);
-         centerOfMassOffsetFromHip.set(centerOfMassPosition);
-         centerOfMassOffsetFromHip.sub(tempHipPoint);
-
-         ReferenceFrame soleFrame = fullRobotModel.getSoleFrame(robotSide);
-         OneDoFJoint anklePitchJoint = fullRobotModel.getLegJoint(robotSide, LegJointName.ANKLE_PITCH);
-         tempAnklePoint.setToZero(anklePitchJoint.getFrameAfterJoint());
-         tempAnklePoint.changeFrame(soleFrame);
-
-         FrameVector soleFrameOffsetFromAnkle = soleFrameOffsetFromAnkles.get(robotSide);
-         soleFrameOffsetFromAnkle.setToZero(soleFrame);
-         soleFrameOffsetFromAnkle.sub(tempAnklePoint);
-      }
+         predictedHipFrames.get(robotSide).update();
    }
 
    private void updateLegLengthLimits()
@@ -147,31 +221,54 @@ public class DynamicReachabilityValidator
       this.minimumLegLength.set(minimumLegLength);
    }
 
+   private final FramePoint tempFinalCoM3D = new FramePoint();
    private void computeHeightLineFromStance(RobotSide supportSide)
    {
       OneDoFJoint supportAnklePitchJoint = fullRobotModel.getLegJoint(supportSide, LegJointName.ANKLE_PITCH);
       tempAnklePoint.setToZero(supportAnklePitchJoint.getFrameAfterJoint());
-      tempAnklePoint.changeFrame(centerOfMassFrame);
+      tempAnklePoint.changeFrame(worldFrame);
       tempAnklePoint.getFrameTuple2d(tempPoint2d);
 
       // get the hip location in XY
-      FrameVector offsetFromHips = centerOfMassOffsetFromHips.get(supportSide);
-      icpPlanner.getFinalDesiredCenterOfMassPosition(tempFinalCoM);
-      tempFinalCoM.changeFrame(offsetFromHips.getReferenceFrame());
-      tempFinalCoM.sub(offsetFromHips.getX(), offsetFromHips.getY());
+      tempFinalCoM3D.setToZero(predictedHipFrames.get(supportSide));
+      tempFinalCoM3D.changeFrame(worldFrame);
+      tempFinalCoM.setByProjectionOntoXYPlaneIncludingFrame(tempFinalCoM3D);
+
+      hipLocations.get(supportSide).set(tempFinalCoM);
+      ankleLocations.get(supportSide).set(tempAnklePoint);
+
+      hipMaximumLocations.get(supportSide).setXY(tempFinalCoM);
+      hipMinimumLocations.get(supportSide).setXY(tempFinalCoM);
 
       double planarDistance = tempFinalCoM.distance(tempPoint2d);
 
       double minimumHeight, maximumHeight;
       if (planarDistance >= minimumLegLength.getDoubleValue())
+      {
          minimumHeight = 0.0;
+      }
       else
+      {
          minimumHeight = Math.sqrt(Math.pow(minimumLegLength.getDoubleValue(), 2.0) - Math.pow(planarDistance, 2.0));
+         minimumHeight += tempAnklePoint.getZ();
+      }
       if (planarDistance >= maximumLegLength.getDoubleValue())
+      {
          maximumHeight = 0.0;
+      }
       else
+      {
          maximumHeight = Math.sqrt(Math.pow(maximumLegLength.getDoubleValue(), 2.0) - Math.pow(planarDistance, 2.0));
+         maximumHeight += tempAnklePoint.getZ();
+      }
 
+
+      finalCoM.setZ(0.5 * (minimumHeight + maximumHeight));
+      hipMaximumLocations.get(supportSide).setZ(maximumHeight);
+      hipMinimumLocations.get(supportSide).setZ(minimumHeight);
+
+      stanceLegMinimumHeight.set(minimumHeight);
+      stanceLegMaximumHeight.set(maximumHeight);
       stanceHeightLine.set(minimumHeight, maximumHeight);
    }
 
@@ -179,31 +276,49 @@ public class DynamicReachabilityValidator
    {
       RobotSide swingSide = nextFootstep.getRobotSide();
 
-      ReferenceFrame upcomingSoleFrame = nextFootstep.getSoleReferenceFrame();
-      tempAnklePoint.setToZero(upcomingSoleFrame);
-      tempAnklePoint.changeFrame(fullRobotModel.getSoleFrame(swingSide));
-      tempAnklePoint.sub(soleFrameOffsetFromAnkles.get(swingSide));
-      tempAnklePoint.changeFrame(centerOfMassFrame);
+      nextFootstep.getPositionIncludingFrame(tempAnklePoint);
+      tempAnklePoint.changeFrame(worldFrame);
       tempAnklePoint.getFrameTuple2d(tempPoint2d);
 
-      // get the hip location in XY
-      FrameVector offsetFromHips = centerOfMassOffsetFromHips.get(swingSide);
-      icpPlanner.getFinalDesiredCenterOfMassPosition(tempFinalCoM);
-      tempFinalCoM.changeFrame(offsetFromHips.getReferenceFrame());
-      tempFinalCoM.sub(offsetFromHips.getX(), offsetFromHips.getY());
+      tempFinalCoM3D.setToZero(predictedHipFrames.get(swingSide));
+      tempFinalCoM3D.changeFrame(worldFrame);
+      tempFinalCoM.setByProjectionOntoXYPlaneIncludingFrame(tempFinalCoM3D);
 
       double planarDistance = tempFinalCoM.distance(tempPoint2d);
 
+      tempFinalCoM.changeFrame(worldFrame);
+      tempAnklePoint.changeFrame(worldFrame);
+      hipLocations.get(swingSide).set(tempFinalCoM);
+      ankleLocations.get(swingSide).set(tempAnklePoint);
+
+      hipMaximumLocations.get(swingSide).setXY(tempFinalCoM);
+      hipMinimumLocations.get(swingSide).setXY(tempFinalCoM);
+
       double minimumHeight, maximumHeight;
       if (planarDistance >= minimumLegLength.getDoubleValue())
+      {
          minimumHeight = 0.0;
+      }
       else
+      {
          minimumHeight = Math.sqrt(Math.pow(minimumLegLength.getDoubleValue(), 2.0) - Math.pow(planarDistance, 2.0));
+         minimumHeight += tempAnklePoint.getZ();
+      }
       if (planarDistance >= maximumLegLength.getDoubleValue())
+      {
          maximumHeight = 0.0;
+      }
       else
+      {
          maximumHeight = Math.sqrt(Math.pow(maximumLegLength.getDoubleValue(), 2.0) - Math.pow(planarDistance, 2.0));
+         maximumHeight += tempAnklePoint.getZ();
+      }
 
+      hipMaximumLocations.get(swingSide).setZ(maximumHeight);
+      hipMinimumLocations.get(swingSide).setZ(minimumHeight);
+
+      swingLegMinimumHeight.set(minimumHeight);
+      swingLegMaximumHeight.set(maximumHeight);
       stepHeightLine.set(minimumHeight, maximumHeight);
    }
 }
