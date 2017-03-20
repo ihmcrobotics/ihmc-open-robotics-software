45fd5608376efbbe52d6cb6e011dd3d8fa4d984f
InhoLee <ilee@ihmc.us>
2017-03-20 15:31:55 -0500

    SolarPanel, SolarPanelCleaningPose added

--- IHMCManipulationPlanning/test/us/ihmc/manipulation/planning/manipulation/solarpanelmotion/SolarPanelStraightTrajectory.java
+++ IHMCManipulationPlanning/test/us/ihmc/manipulation/planning/manipulation/solarpanelmotion/SolarPanelStraightTrajectory.java
@@ -7,34 +7,13 @@ import us.ihmc.euclid.tuple3D.Point3D;
 
 public class SolarPanelStraightTrajectory
 {
-   private SolarPanelCleaningPose startPose;
-   private SolarPanelCleaningPose endPose;
+   private SolarPanel solarPanel;
    private int numberOfWayPoints;
-   private ArrayList<SolarPanelCleaningPose> wayPointPoses = new ArrayList<SolarPanelCleaningPose>();
    
-   public SolarPanelStraightTrajectory(SolarPanelCleaningPose startPose, SolarPanelCleaningPose endPose, int numberOfWayPoints)
+   public SolarPanelStraightTrajectory(SolarPanel solarPanel, int numberOfWayPoints)
    {
-      this.startPose = startPose;
-      this.endPose = endPose;
-      this.numberOfWayPoints = numberOfWayPoints;
-      initialize();
+      this.solarPanel = solarPanel;
+      this.numberOfWayPoints = numberOfWayPoints;    
    }
    
-   private void initialize()
-   {  
-      for(int i=0;i<numberOfWayPoints;i++)
-      {         
-         Point3D startLocation = startPose.getLocation();
-         Point3D endLocation = endPose.getLocation();
-         Point3D wayPointLocation = new Point3D((endLocation.getX() - startLocation.getX())/(numberOfWayPoints-1)*i+startLocation.getX(), (endLocation.getY() - startLocation.getY())/(numberOfWayPoints-1)*i+startLocation.getY(), (endLocation.getZ() - startLocation.getZ())/(numberOfWayPoints-1)*i+startLocation.getZ());
-         SolarPanelCleaningPose wayPointPose = new SolarPanelCleaningPose(wayPointLocation, 0.0, -Math.PI/0.25, 0.0);
-      
-         wayPointPoses.add(wayPointPose);
-      }
-   }
-   
-   public ArrayList<SolarPanelCleaningPose> getPoses()
-   {
-      return wayPointPoses;
-   }
 }
