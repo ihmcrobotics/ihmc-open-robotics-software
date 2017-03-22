e6f2890980ff34301f4a21c2f3230e7b20bfa101
bshrewsbury <bshrewsbury@ihmc.us>
2017-03-22 10:13:13 -0500

    variable rename

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/controllerAPI/converter/FrameBasedCommand.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/controllerAPI/converter/FrameBasedCommand.java
@@ -5,5 +5,5 @@ import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 
 public interface FrameBasedCommand<M extends Packet<M>>
 {
-   void set(ReferenceFrame expressedInFrame, ReferenceFrame trajectoryFrame, M message);
+   void set(ReferenceFrame dataFrame, ReferenceFrame trajectoryFrame, M message);
 }
