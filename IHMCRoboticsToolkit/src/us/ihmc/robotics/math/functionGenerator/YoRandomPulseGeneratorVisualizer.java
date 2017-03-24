77ad803b3b284e2f83932dcc2b3e2c4123e05ce3
Stephen McCrory <smccrory@ihmc.us>
2017-03-24 16:10:07 -0500

    Moving CMAC to IHMCMachineLearning

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/math/functionGenerator/YoRandomPulseGeneratorVisualizer.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/math/functionGenerator/YoRandomPulseGeneratorVisualizer.java
@@ -3,8 +3,6 @@ package us.ihmc.simulationconstructionset.util.math.functionGenerator;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
 import us.ihmc.robotics.math.functionGenerator.YoRandomPulseGenerator;
 import us.ihmc.robotics.robotController.RobotController;
-import us.ihmc.simulationconstructionset.Robot;
-import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 
 public class YoRandomPulseGeneratorVisualizer implements RobotController
 {
@@ -59,38 +57,6 @@ public class YoRandomPulseGeneratorVisualizer implements RobotController
          e.printStackTrace();
       }
    }
-
-   public static void main(String[] args)
-   {
-      Robot robot = new Robot("Robot");
-
-      YoVariableRegistry registry = robot.getRobotsYoVariableRegistry();
-
-      YoRandomPulseGenerator yoRandomPulseGenerator = new YoRandomPulseGenerator("PGen", robot.getYoTime(), registry);
-      YoRandomPulseGeneratorVisualizer yoRandomPulseGeneratorVisualizer = new YoRandomPulseGeneratorVisualizer(yoRandomPulseGenerator);
-
-      robot.setController(yoRandomPulseGeneratorVisualizer);
-
-      SimulationConstructionSet scs = new SimulationConstructionSet(robot);
-
-      scs.setDT(0.01, 1);
-
-      Thread thread = new Thread(scs);
-
-      thread.start();
-
-//      try
-//      {
-//         Thread.sleep(1000);
-//      }
-//      catch (InterruptedException e)
-//      {
-//         e.printStackTrace();
-//      }
-//
-      scs.hideViewport();
-   }
-
 }
 
 
