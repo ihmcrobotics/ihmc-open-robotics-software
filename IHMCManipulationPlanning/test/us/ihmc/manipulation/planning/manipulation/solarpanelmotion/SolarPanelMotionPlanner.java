82148142389d4b37fafec3225346fe6f55398aeb
InhoLee <ilee@ihmc.us>
2017-03-24 15:16:41 -0500

    pelvisHeightControlTest

--- IHMCManipulationPlanning/test/us/ihmc/manipulation/planning/manipulation/solarpanelmotion/SolarPanelMotionPlanner.java
+++ IHMCManipulationPlanning/test/us/ihmc/manipulation/planning/manipulation/solarpanelmotion/SolarPanelMotionPlanner.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import us.ihmc.commons.PrintTools;
 import us.ihmc.humanoidRobotics.communication.packets.wholebody.WholeBodyTrajectoryMessage;
 import us.ihmc.robotics.geometry.transformables.Pose;
+import us.ihmc.robotics.robotSide.RobotSide;
 
 public class SolarPanelMotionPlanner
 {
    private SolarPanel solarPanel;
    
    WholeBodyTrajectoryMessage wholeBodyTrajectoryMessage = new WholeBodyTrajectoryMessage();   
-   SolarPanelCleaningPose readyPose;
    
    // debug
    public Pose debugPoseOne;
@@ -36,89 +36,91 @@ public class SolarPanelMotionPlanner
       return this.wholeBodyTrajectoryMessage;
    }
    
-   public void setWholeBodyTrajectoryMessage(CleaningMotion motion, double motionTime)
-   {
-      readyPose = new SolarPanelCleaningPose(solarPanel, 0.5, 0.1, -0.1, -Math.PI*0.2);
-      
-      this.wholeBodyTrajectoryMessage.clear();
-           
-      switch(motion)
-      {
-      case ReadyPose:
-         PrintTools.info("setTrajectoryMessage -> "+CleaningMotion.ReadyPose);
-         
-         wholeBodyTrajectoryMessage.setHandTrajectoryMessage(readyPose.getHandTrajectoryMessage(motionTime));
-         debugPoseOne = readyPose.getPose();
-         
-         break;
-      case LinearCleaningMotion:
-         PrintTools.info("setTrajectoryMessage -> "+CleaningMotion.LinearCleaningMotion);
-         
-         SolarPanelCleaningPose lineStart = new SolarPanelCleaningPose(readyPose);
-         SolarPanelCleaningPose lineEnd = new SolarPanelCleaningPose(solarPanel, 0.1, 0.1, -0.1);
-         SolarPanelStraightPath linePath = new SolarPanelStraightPath(solarPanel, motionTime, lineStart, lineEnd, 10);
-         SolarPanelStraightPathPlanner cleaningMotionPlanner = new SolarPanelStraightPathPlanner(linePath);
-         
-         cleaningMotionPlanner.temporarySolution();
-         wholeBodyTrajectoryMessage.setHandTrajectoryMessage(cleaningMotionPlanner.getHandTrajectoryTemporary());         
-         wholeBodyTrajectoryMessage.setChestTrajectoryMessage(cleaningMotionPlanner.getChestTrajectoryTemporary());
-         
-         
-         debugPoseOne = linePath.getStartPose().getPose();
-         debugPoseTwo = linePath.getEndPose().getPose();
-         
-         break;
-         
-      case LinearCleaningMotionWhole:
-         
-         ArrayList<SolarPanelCleaningPose> tempPoses = new ArrayList<SolarPanelCleaningPose>();
-         
-         // ten
-         tempPoses.add(new SolarPanelCleaningPose(readyPose));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.1, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.2, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.2, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.3, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.3, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.4, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.4, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.5, -0.1));
-         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.5, -0.1));
-         
-         ArrayList<SolarPanelStraightPath> paths = new ArrayList<SolarPanelStraightPath>();
-         
-         for(int i=0;i<tempPoses.size()-1;i++)
-         {
-            paths.add(new SolarPanelStraightPath(solarPanel, motionTime, tempPoses.get(i), tempPoses.get(i+1), 3));            
-         }
-         SolarPanelStraightPathPlanner cleaningMotionPlanner1 = new SolarPanelStraightPathPlanner(paths);
-         cleaningMotionPlanner1.temporarySolution();
-         
-         for(int i=0;i<tempPoses.size();i++)
-         {
-            debugPose.add(tempPoses.get(i).getPose());            
-         }
-                  
-         wholeBodyTrajectoryMessage.setHandTrajectoryMessage(cleaningMotionPlanner1.getHandTrajectoryTemporaryWhole());         
-         wholeBodyTrajectoryMessage.setChestTrajectoryMessage(cleaningMotionPlanner1.getChestTrajectoryTemporaryWhole());
-         
-         
-         
-         break;
-         
-      default:
-         PrintTools.info("setTrajectoryMessage -> NONE");
-         
-         break;
-         
-      }
-      
-   }
+//   public void setWholeBodyTrajectoryMessage(CleaningMotion motion, double motionTime)
+//   {
+//      readyPose = new SolarPanelCleaningPose(solarPanel, 0.5, 0.1, -0.1, -Math.PI*0.2);
+//      
+//      this.wholeBodyTrajectoryMessage.clear();
+//           
+//      switch(motion)
+//      {
+//      case ReadyPose:
+//         PrintTools.info("setTrajectoryMessage -> "+CleaningMotion.ReadyPose);
+//         
+//         wholeBodyTrajectoryMessage.setHandTrajectoryMessage(readyPose.getHandTrajectoryMessage(motionTime));
+//         debugPoseOne = readyPose.getPose();
+//         
+//         break;
+//      case LinearCleaningMotion:
+//         PrintTools.info("setTrajectoryMessage -> "+CleaningMotion.LinearCleaningMotion);
+//         
+//         SolarPanelCleaningPose lineStart = new SolarPanelCleaningPose(readyPose);
+//         SolarPanelCleaningPose lineEnd = new SolarPanelCleaningPose(solarPanel, 0.1, 0.1, -0.1);
+//         SolarPanelStraightPath linePath = new SolarPanelStraightPath(solarPanel, motionTime, lineStart, lineEnd, 10);
+//         SolarPanelStraightPathPlanner cleaningMotionPlanner = new SolarPanelStraightPathPlanner(linePath);
+//         
+//         cleaningMotionPlanner.temporarySolution();
+//         wholeBodyTrajectoryMessage.setHandTrajectoryMessage(cleaningMotionPlanner.getHandTrajectoryTemporary());         
+//         wholeBodyTrajectoryMessage.setChestTrajectoryMessage(cleaningMotionPlanner.getChestTrajectoryTemporary());
+//         
+//         
+//         debugPoseOne = linePath.getStartPose().getPose();
+//         debugPoseTwo = linePath.getEndPose().getPose();
+//         
+//         break;
+//         
+//      case LinearCleaningMotionWhole:
+//         
+//         ArrayList<SolarPanelCleaningPose> tempPoses = new ArrayList<SolarPanelCleaningPose>();
+//         
+//         // ten
+//         tempPoses.add(new SolarPanelCleaningPose(readyPose));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.1, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.2, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.2, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.3, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.3, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.4, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.4, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.5, 0.5, -0.1));
+//         tempPoses.add(new SolarPanelCleaningPose(solarPanel, 0.1, 0.5, -0.1));
+//         
+//         ArrayList<SolarPanelStraightPath> paths = new ArrayList<SolarPanelStraightPath>();
+//         
+//         for(int i=0;i<tempPoses.size()-1;i++)
+//         {
+//            paths.add(new SolarPanelStraightPath(solarPanel, motionTime, tempPoses.get(i), tempPoses.get(i+1), 3));            
+//         }
+//         SolarPanelStraightPathPlanner cleaningMotionPlanner1 = new SolarPanelStraightPathPlanner(paths);
+//         cleaningMotionPlanner1.temporarySolution();
+//         
+//         for(int i=0;i<tempPoses.size();i++)
+//         {
+//            debugPose.add(tempPoses.get(i).getPose());            
+//         }
+//                  
+//         wholeBodyTrajectoryMessage.setHandTrajectoryMessage(cleaningMotionPlanner1.getHandTrajectoryTemporaryWhole());         
+//         wholeBodyTrajectoryMessage.setChestTrajectoryMessage(cleaningMotionPlanner1.getChestTrajectoryTemporaryWhole());
+//         
+//         
+//         
+//         break;
+//         
+//      default:
+//         PrintTools.info("setTrajectoryMessage -> NONE");
+//         
+//         break;
+//         
+//      }
+//      
+//   }
    
-   public void setWholeBodyTrajectoryMessage(CleaningMotion motion, WholeBodyTrajectoryMessage wholeBodyTrajectoryMessage)
+   public void setWholeBodyTrajectoryMessage(CleaningMotion motion)
    {
-      readyPose = new SolarPanelCleaningPose(solarPanel, 0.5, 0.1, -0.1, -Math.PI*0.2);
-      this.wholeBodyTrajectoryMessage = wholeBodyTrajectoryMessage;
+      WholeBodyTrajectoryMessage wholeBodyTrajectoryMessage = new WholeBodyTrajectoryMessage();
+      
+      SolarPanelCleaningPose readyPose = new SolarPanelCleaningPose(solarPanel, 0.5, 0.1, -0.1, -Math.PI*0.2);
+      
       
            
       switch(motion)
@@ -138,8 +140,6 @@ public class SolarPanelMotionPlanner
          
       case LinearCleaningMotionWhole:
          
-         ArrayList<SolarPanelCleaningPose> tempPoses = new ArrayList<SolarPanelCleaningPose>();
-        
          
          
          
@@ -151,6 +151,13 @@ public class SolarPanelMotionPlanner
          break;
          
       }
+      
+      this.wholeBodyTrajectoryMessage = wholeBodyTrajectoryMessage;
+   }
+   
+   public double getMotionTime()
+   {
+      return this.wholeBodyTrajectoryMessage.getHandTrajectoryMessage(RobotSide.RIGHT).getTrajectoryTime();
    }
    
    
--- IHMCManipulationPlanning/test/us/ihmc/manipulation/planning/manipulation/solarpanelmotion/SolarPanelWholeBodyPose.java
+++ IHMCManipulationPlanning/test/us/ihmc/manipulation/planning/manipulation/solarpanelmotion/SolarPanelWholeBodyPose.java
@@ -1,6 +1,12 @@
 package us.ihmc.manipulation.planning.manipulation.solarpanelmotion;
 
+import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.humanoidRobotics.communication.packets.manipulation.HandTrajectoryMessage;
+import us.ihmc.humanoidRobotics.communication.packets.walking.ChestTrajectoryMessage;
+import us.ihmc.humanoidRobotics.communication.packets.walking.PelvisHeightTrajectoryMessage;
 import us.ihmc.humanoidRobotics.communication.packets.wholebody.WholeBodyTrajectoryMessage;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+import us.ihmc.robotics.robotSide.RobotSide;
 
 public class SolarPanelWholeBodyPose
 {
@@ -13,10 +19,20 @@ public class SolarPanelWholeBodyPose
       this.cleaningPose = cleaningPose;      
       this.desiredPelvisYaw = pelvisYaw;
       this.deisredPelvisHeight = pelvisHeight;
+   
    }
    
-   public void getWholeBodyTrajectoryMessage(WholeBodyTrajectoryMessage wholeBodyMessage)
+   public void getWholeBodyTrajectoryMessage(WholeBodyTrajectoryMessage wholeBodyMessage, double motionTime)
    {
+      HandTrajectoryMessage handTrajectoryMessage = new HandTrajectoryMessage(RobotSide.RIGHT, motionTime, cleaningPose.getDesiredHandPosition(), cleaningPose.getDesiredHandOrientation(), ReferenceFrame.getWorldFrame(), ReferenceFrame.getWorldFrame());
+      PelvisHeightTrajectoryMessage pelvisHeightTrajectoryMessage = new PelvisHeightTrajectoryMessage(motionTime, deisredPelvisHeight);
+      Quaternion desiredChestOrientation = new Quaternion();
+      desiredChestOrientation.appendYawRotation(desiredPelvisYaw);
+      ChestTrajectoryMessage chestTrajectoryMessage = new ChestTrajectoryMessage(motionTime, desiredChestOrientation, ReferenceFrame.getWorldFrame(), ReferenceFrame.getWorldFrame());
+      
+      wholeBodyMessage.setHandTrajectoryMessage(handTrajectoryMessage);
+      wholeBodyMessage.setChestTrajectoryMessage(chestTrajectoryMessage);
+      wholeBodyMessage.setPelvisTrajectoryMessage(pelvisHeightTrajectoryMessage);
       
    }
 }
