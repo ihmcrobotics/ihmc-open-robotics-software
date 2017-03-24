36275e6ae3adb5a4ac15c00414f9ae219aa28d31
Stephen McCrory <smccrory@ihmc.us>
2017-03-24 12:19:47 -0500

    Extracting external controller package into SCSTools

--- SimulationConstructionSetTools/src/us/ihmc/simulationconstructionsettools/externalcontroller/ExternalControllerTCPConnection.java
+++ SimulationConstructionSetTools/src/us/ihmc/simulationconstructionsettools/externalcontroller/ExternalControllerTCPConnection.java
@@ -1,4 +1,4 @@
-package us.ihmc.simulationconstructionset.externalcontroller;
+package us.ihmc.simulationconstructionsettools.externalcontroller;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
