06b0c4e2d8fd0394b71c9c4770c73d73e8a7d7c0
bshrewsbury <bshrewsbury@ihmc.us>
2017-04-04 13:04:17 -0500

    add HandHybridJointspaceTaskspaceTrajectoryMessage, ChestHybridJointspaceTaskspaceTrajectoryCommand, HeadHybridJointspaceTaskspaceMessage to kryoNetClassList

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/kryo/IHMCCommunicationKryoNetClassList.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/kryo/IHMCCommunicationKryoNetClassList.java
@@ -37,6 +37,7 @@ import us.ihmc.euclid.tuple3D.Vector3D32;
 import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.euclid.tuple4D.Quaternion32;
 import us.ihmc.footstepPlanning.FootstepPlanningResult;
+import us.ihmc.humanoidRobotics.communication.controllerAPI.command.ChestHybridJointspaceTaskspaceTrajectoryCommand;
 import us.ihmc.humanoidRobotics.communication.packets.DetectedObjectPacket;
 import us.ihmc.humanoidRobotics.communication.packets.EuclideanTrajectoryPointMessage;
 import us.ihmc.humanoidRobotics.communication.packets.ExecutionMode;
@@ -175,6 +176,8 @@ import us.ihmc.humanoidRobotics.communication.packets.walking.SpineTrajectoryMes
 import us.ihmc.humanoidRobotics.communication.packets.walking.TrackingWeightsMessage;
 import us.ihmc.humanoidRobotics.communication.packets.walking.WalkingControllerFailureStatusMessage;
 import us.ihmc.humanoidRobotics.communication.packets.walking.WalkingStatusMessage;
+import us.ihmc.humanoidRobotics.communication.packets.walking.hybridRigidBodyManager.HandHybridJointspaceTaskspaceTrajectoryMessage;
+import us.ihmc.humanoidRobotics.communication.packets.walking.hybridRigidBodyManager.HeadHybridJointspaceTaskspaceMessage;
 import us.ihmc.humanoidRobotics.communication.packets.wholebody.JointAnglesPacket;
 import us.ihmc.humanoidRobotics.communication.packets.wholebody.MultiJointAnglePacket;
 import us.ihmc.humanoidRobotics.communication.packets.wholebody.SingleJointAnglePacket;
@@ -269,6 +272,9 @@ public class IHMCCommunicationKryoNetClassList extends NetClassList
       registerPacketClass(TrackingWeightsMessage.class);
       registerPacketClass(StopAllTrajectoryMessage.class);
       registerPacketClass(GoHomeMessage.class);
+      registerPacketClass(HandHybridJointspaceTaskspaceTrajectoryMessage.class);
+      registerPacketClass(ChestHybridJointspaceTaskspaceTrajectoryCommand.class);
+      registerPacketClass(HeadHybridJointspaceTaskspaceMessage.class);
 
       // Trajectory message fields
       registerPacketClass(ExecutionMode.class);
