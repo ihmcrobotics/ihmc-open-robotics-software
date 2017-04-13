6bd92384551a7ffe579865ab4947e0d5b0af1ec8
Sylvain Bertrand <sbertrand@ihmc.us>
2017-04-13 15:46:20 -0500

    Renamed some methods of MomentumRateCommand.

--- ExampleSimulations/src/us/ihmc/exampleSimulations/beetle/controller/HexapodMomentumController.java
+++ ExampleSimulations/src/us/ihmc/exampleSimulations/beetle/controller/HexapodMomentumController.java
@@ -102,8 +102,8 @@ public class HexapodMomentumController
       yoLinearMomentumRateOfChange.set(linearMomentumRateOfChange);
       yoAngularMomentumRateOfChange.set(angularMomentumRateOfChange);
       
-      momentumRateCommand.setLinearMomentumRateOfChange(linearMomentumRateOfChange);
-      momentumRateCommand.setAngularMomentumRateOfChange(angularMomentumRateOfChange);
+      momentumRateCommand.setLinearMomentumRate(linearMomentumRateOfChange);
+      momentumRateCommand.setAngularMomentumRate(angularMomentumRateOfChange);
       momentumRateCommand.setSelectionMatrix(selectionMatrix);
       momentumRateCommand.setWeights(angularMomentumWeight.getX(), angularMomentumWeight.getY(), angularMomentumWeight.getZ(), linearMomentumWeight.getX(),
             linearMomentumWeight.getY(), linearMomentumWeight.getZ());
