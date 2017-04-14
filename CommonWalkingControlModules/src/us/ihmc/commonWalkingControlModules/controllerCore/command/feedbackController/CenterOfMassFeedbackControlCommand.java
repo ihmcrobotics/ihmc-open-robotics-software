1ab47e746d97dbad0ed8a275d73fc10b88753407
Sylvain Bertrand <sbertrand@ihmc.us>
2017-04-13 19:39:04 -0500

    Created the CenterOfMassFeedbackControlCommand.

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controllerCore/command/feedbackController/CenterOfMassFeedbackControlCommand.java
@@ -0,0 +1,218 @@
+package us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController;
+
+import org.ejml.data.DenseMatrix64F;
+
+import us.ihmc.commonWalkingControlModules.controllerCore.WholeBodyControllerCore;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.ControllerCoreCommand;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.ControllerCoreCommandType;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.inverseDynamics.MomentumRateCommand;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.robotics.controllers.PositionPIDGains;
+import us.ihmc.robotics.controllers.PositionPIDGainsInterface;
+import us.ihmc.robotics.geometry.FramePoint;
+import us.ihmc.robotics.geometry.FrameVector;
+import us.ihmc.robotics.geometry.ReferenceFrameMismatchException;
+import us.ihmc.robotics.linearAlgebra.MatrixTools;
+import us.ihmc.robotics.referenceFrames.ReferenceFrame;
+
+/**
+ * {@link CenterOfMassFeedbackControlCommand} is a command meant to be submit to the
+ * {@link WholeBodyControllerCore} via the {@link ControllerCoreCommand}.
+ * <p>
+ * The objective of a {@link CenterOfMassFeedbackControlCommand} is to notify the center of mass feedback
+ * controller that it is requested to run during the next control tick.
+ * </p>
+ * <p>
+ * From control tick to control tick each feedback controller can be entirely configured or
+ * reconfigured, and enabled (by submitting a command) or disabled (by NOT submitting a command).
+ * </p>
+ * <p>
+ * All the data contained in this command is expressed in world to ensure that the feedback
+ * controller can properly interpret it.
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ *
+ */
+public class CenterOfMassFeedbackControlCommand implements FeedbackControlCommand<CenterOfMassFeedbackControlCommand>
+{
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+
+   private final Point3D desiredPositionInWorld = new Point3D();
+   private final Vector3D desiredLinearVelocityInWorld = new Vector3D();
+   private final Vector3D feedForwardLinearAccelerationInWorld = new Vector3D();
+
+   /** The 3D gains used in the PD controller for the next control tick. */
+   private final PositionPIDGains gains = new PositionPIDGains();
+
+   /**
+    * Momentum rate command used to save different control properties such as the weight to be used
+    * in the QP optimization.
+    * <p>
+    * Should not be accessed from the user side.
+    * </p>
+    */
+   private final MomentumRateCommand momentumRateCommand = new MomentumRateCommand();
+
+   /**
+    * Creates an empty command. It needs to be configured before being submitted to the controller
+    * core.
+    */
+   public CenterOfMassFeedbackControlCommand()
+   {
+      momentumRateCommand.setSelectionMatrixForLinearControl();
+   }
+
+   /**
+    * Performs a full-depth copy of the data contained in the other command.
+    */
+   @Override
+   public void set(CenterOfMassFeedbackControlCommand other)
+   {
+      desiredPositionInWorld.set(other.desiredPositionInWorld);
+      desiredLinearVelocityInWorld.set(other.desiredLinearVelocityInWorld);
+      feedForwardLinearAccelerationInWorld.set(other.feedForwardLinearAccelerationInWorld);
+      setGains(other.gains);
+
+      momentumRateCommand.set(other.momentumRateCommand);
+   }
+
+   /**
+    * Sets the gains to use during the next control tick.
+    * 
+    * @param gains the new set of gains to use. Not modified.
+    */
+   public void setGains(PositionPIDGainsInterface gains)
+   {
+      this.gains.set(gains);
+   }
+
+   /**
+    * Sets the desired data expressed in world frame to be used during the next control tick.
+    * 
+    * @param desiredPosition describes the position that the center of mass should reach. Not
+    *           modified.
+    * @param desiredLinearVelocity describes the desired center of mass linear velocity with respect
+    *           to world. Not modified.
+    * @param feedForwardLinearAcceleration describes the desired center of mass linear acceleration
+    *           with respect to the world. Not modified.
+    * @throws ReferenceFrameMismatchException if any of the three arguments is not expressed in
+    *            {@link ReferenceFrame#getWorldFrame()}.
+    */
+   public void set(FramePoint desiredPosition, FrameVector desiredLinearVelocity, FrameVector feedForwardLinearAcceleration)
+   {
+      desiredPosition.checkReferenceFrameMatch(worldFrame);
+      desiredLinearVelocity.checkReferenceFrameMatch(worldFrame);
+      feedForwardLinearAcceleration.checkReferenceFrameMatch(worldFrame);
+
+      desiredPosition.get(desiredPositionInWorld);
+      desiredLinearVelocity.get(desiredLinearVelocityInWorld);
+      feedForwardLinearAcceleration.get(feedForwardLinearAccelerationInWorld);
+   }
+
+   /**
+    * Sets the selection matrix to be used for the next control tick to the following 3-by-6 matrix:
+    * 
+    * <pre>
+    *     / 0 0 0 1 0 0 \
+    * S = | 0 0 0 0 1 0 |
+    *     \ 0 0 0 0 0 1 /
+    * </pre>
+    * <p>
+    * This specifies that the 3 translational degrees of freedom of the end-effector are to be
+    * controlled.
+    * </p>
+    */
+   public void setSelectionMatrixToIdentity()
+   {
+      momentumRateCommand.setSelectionMatrixForLinearControl();
+   }
+
+   /**
+    * Sets the selection matrix to be used for the next control tick.
+    * <p>
+    * The selection matrix is used to describe the DoFs (Degrees Of Freedom) of the end-effector
+    * that are to be controlled. Using the following 3-by-6 matrix will request the control of all
+    * the 3 translational degrees of freedom:
+    * 
+    * <pre>
+    *     / 0 0 0 1 0 0 \
+    * S = | 0 0 0 0 1 0 |
+    *     \ 0 0 0 0 0 1 /
+    * </pre>
+    * </p>
+    * <p>
+    * Removing a row to the selection matrix using for instance
+    * {@link MatrixTools#removeRow(DenseMatrix64F, int)} is the quickest way to ignore a specific
+    * DoF of the end-effector.
+    * </p>
+    * <p>
+    * 
+    * @param selectionMatrix the new selection matrix to be used. Not modified.
+    * @throws RuntimeException if the selection matrix has a number of rows greater than 3 or has a
+    *            number of columns different to 6.
+    */
+   public void setSelectionMatrix(DenseMatrix64F selectionMatrix)
+   {
+      if (selectionMatrix.getNumRows() > 3)
+         throw new RuntimeException("Unexpected number of rows: " + selectionMatrix.getNumRows());
+
+      momentumRateCommand.setSelectionMatrix(selectionMatrix);
+   }
+
+   /**
+    * Sets the weight to use in the optimization problem.
+    * <p>
+    * WARNING: It is not the value of each individual command's weight that is relevant to how the
+    * optimization will behave but the ratio between them. A command with a higher weight than other
+    * commands value will be treated as more important than the other commands.
+    * </p>
+    * 
+    * @param weight the weight value to use for this command.
+    */
+   public void setWeightForSolver(double weight)
+   {
+      momentumRateCommand.setWeight(weight);
+   }
+
+   /**
+    * Sets the weights to use in the optimization problem for each individual degree of freedom.
+    * <p>
+    * WARNING: It is not the value of each individual command's weight that is relevant to how the
+    * optimization will behave but the ratio between them. A command with a higher weight than other
+    * commands value will be treated as more important than the other commands.
+    * </p>
+    * 
+    * @param weight the weight to use for each direction. Not modified.
+    */
+   public void setWeightsForSolver(Vector3D weight)
+   {
+      momentumRateCommand.setLinearWeights(weight);
+      momentumRateCommand.setAngularWeightsToZero();
+   }
+
+   public void getIncludingFrame(FramePoint desiredPositionToPack, FrameVector desiredLinearVelocityToPack, FrameVector feedForwardLinearAccelerationToPack)
+   {
+      desiredPositionToPack.setIncludingFrame(worldFrame, desiredPositionInWorld);
+      desiredLinearVelocityToPack.setIncludingFrame(worldFrame, desiredLinearVelocityInWorld);
+      feedForwardLinearAccelerationToPack.setIncludingFrame(worldFrame, feedForwardLinearAccelerationInWorld);
+   }
+
+   public MomentumRateCommand getMomentumRateCommand()
+   {
+      return momentumRateCommand;
+   }
+
+   public PositionPIDGainsInterface getGains()
+   {
+      return gains;
+   }
+
+   @Override
+   public ControllerCoreCommandType getCommandType()
+   {
+      return ControllerCoreCommandType.MOMENTUM;
+   }
+
+}
