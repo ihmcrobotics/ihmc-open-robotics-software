6b45d451b71156450a48fa039c62e13864df3c72
Robert <rgriffin@ihmc.us>
2017-04-18 17:01:59 -0400

    setup a wrapper for multiple toe off calculators

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/controlModules/foot/toeOffCalculator/WrapperForMultipleToeOffCalculators.java
@@ -0,0 +1,48 @@
+package us.ihmc.commonWalkingControlModules.controlModules.foot.toeOffCalculator;
+
+import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
+import us.ihmc.robotics.dataStructures.variable.IntegerYoVariable;
+import us.ihmc.robotics.geometry.FramePoint;
+import us.ihmc.robotics.geometry.FramePoint2d;
+import us.ihmc.robotics.robotSide.RobotSide;
+
+import java.util.ArrayList;
+
+public class WrapperForMultipleToeOffCalculators implements ToeOffCalculator
+{
+   private final String namePostfix = getClass().getSimpleName();
+
+   private final IntegerYoVariable toeOffCalculatorIndex;
+   private final ArrayList<ToeOffCalculator> toeOffCalculators;
+
+   public WrapperForMultipleToeOffCalculators(ArrayList<ToeOffCalculator> toeOffCalculators, YoVariableRegistry registry)
+   {
+      this.toeOffCalculators = toeOffCalculators;
+
+      toeOffCalculatorIndex = new IntegerYoVariable("ToeOffCalculatorIndex", registry);
+   }
+
+   public void clear()
+   {
+      ToeOffCalculator currentCalculator = toeOffCalculators.get(toeOffCalculatorIndex.getIntegerValue());
+      currentCalculator.clear();
+   }
+
+   public void setExitCMP(FramePoint exitCMP, RobotSide trailingLeg)
+   {
+      ToeOffCalculator currentCalculator = toeOffCalculators.get(toeOffCalculatorIndex.getIntegerValue());
+      currentCalculator.setExitCMP(exitCMP, trailingLeg);
+   }
+
+   public void computeToeOffContactPoint(FramePoint2d desiredCMP, RobotSide trailingLeg)
+   {
+      ToeOffCalculator currentCalculator = toeOffCalculators.get(toeOffCalculatorIndex.getIntegerValue());
+      currentCalculator.computeToeOffContactPoint(desiredCMP, trailingLeg);
+   }
+
+   public void getToeOffContactPoint(FramePoint2d contactPointToPack, RobotSide trailingLeg)
+   {
+      ToeOffCalculator currentCalculator = toeOffCalculators.get(toeOffCalculatorIndex.getIntegerValue());
+      currentCalculator.getToeOffContactPoint(contactPointToPack, trailingLeg);
+   }
+}
