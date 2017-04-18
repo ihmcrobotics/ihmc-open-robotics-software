cff1feb5874f92a959abd894b2e948554f6e0e04
Robert <rgriffin@ihmc.us>
2017-04-18 00:02:07 -0400

    got the different privileged gains for the legs to avoid singularities working properly

--- Atlas/src/us/ihmc/atlas/parameters/AtlasStraightLegWalkingParameters.java
+++ Atlas/src/us/ihmc/atlas/parameters/AtlasStraightLegWalkingParameters.java
@@ -4,9 +4,22 @@ import us.ihmc.commonWalkingControlModules.configurations.StraightLegWalkingPara
 
 public class AtlasStraightLegWalkingParameters extends StraightLegWalkingParameters
 {
+   private final boolean runningOnRealRobot;
+
+   public AtlasStraightLegWalkingParameters(boolean runningOnRealRobot)
+   {
+      this.runningOnRealRobot = runningOnRealRobot;
+   }
+
    /** {@inheritDoc} */
    public boolean attemptToStraightenLegs()
    {
       return false;
+   }
+
+   /** {@inheritDoc} */
+   public double getStraightLegPrivilegedConfigurationGain()
+   {
+      return runningOnRealRobot ? 40.0 : 125.0;
    }
 }
