777b26817232c143e1ef73f7582299def4f77b69
Sylvain Bertrand <sbertrand@ihmc.us>
2017-04-19 22:45:19 -0500

    More doc.

--- IHMCAvatarInterfaces/src/us/ihmc/avatar/networkProcessor/kinematicsToolboxModule/KinematicsToolboxCommandConverter.java
+++ IHMCAvatarInterfaces/src/us/ihmc/avatar/networkProcessor/kinematicsToolboxModule/KinematicsToolboxCommandConverter.java
@@ -11,6 +11,13 @@ import us.ihmc.communication.packets.Packet;
 import us.ihmc.robotics.screwTheory.RigidBody;
 import us.ihmc.robotics.screwTheory.ScrewTools;
 
+/**
+ * This class allows the retrieve the rigid-body from its hash code when converting a
+ * {@link KinematicsToolboxRigidBodyMessage} into a {@link KinematicsToolboxRigidBodyCommand}.
+ * 
+ * @author Sylvain Bertrand
+ *
+ */
 public class KinematicsToolboxCommandConverter implements CommandConversionInterface
 {
    private final Map<Long, RigidBody> rigidBodyNamedBasedHashMap = new HashMap<>();
@@ -23,12 +30,18 @@ public class KinematicsToolboxCommandConverter implements CommandConversionInter
          rigidBodyNamedBasedHashMap.put(rigidBody.getNameBasedHashCode(), rigidBody);
    }
 
+   /**
+    * Only converting {@link KinematicsToolboxRigidBodyMessage}.
+    */
    @Override
    public <C extends Command<?, M>, M extends Packet<M>> boolean isConvertible(C command, M message)
    {
       return message instanceof KinematicsToolboxRigidBodyMessage;
    }
 
+   /**
+    * Retrieves the end-effector and convert the message into its command counterpart.
+    */
    @Override
    public <C extends Command<?, M>, M extends Packet<M>> void process(C command, M message)
    {
