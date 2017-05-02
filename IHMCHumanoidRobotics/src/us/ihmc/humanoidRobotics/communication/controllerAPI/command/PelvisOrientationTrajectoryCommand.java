570e90a000cb0ee1b306315bd4504d61a53b4043
gwiedebach <gwiedebach@ihmc.us>
2017-05-02 13:12:50 -0500

    added option to disable pelvis orientation control from the walking controller and do a custom trajectory

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/controllerAPI/command/PelvisOrientationTrajectoryCommand.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/controllerAPI/command/PelvisOrientationTrajectoryCommand.java
@@ -1,9 +1,43 @@
 package us.ihmc.humanoidRobotics.communication.controllerAPI.command;
 
 import us.ihmc.humanoidRobotics.communication.packets.walking.PelvisOrientationTrajectoryMessage;
+import us.ihmc.sensorProcessing.frames.ReferenceFrameHashCodeResolver;
 
 public class PelvisOrientationTrajectoryCommand extends SO3TrajectoryControllerCommand<PelvisOrientationTrajectoryCommand, PelvisOrientationTrajectoryMessage>
 {
+   private boolean enableUserPelvisControlDuringWalking = false;
+
+   @Override
+   public void set(PelvisOrientationTrajectoryCommand other)
+   {
+      setEnableUserPelvisControlDuringWalking(other.isEnableUserPelvisControlDuringWalking());
+      super.set(other);
+   }
+
+   @Override
+   public void set(ReferenceFrameHashCodeResolver resolver, PelvisOrientationTrajectoryMessage message)
+   {
+      setEnableUserPelvisControlDuringWalking(message.isEnableUserPelvisControlDuringWalking());
+      super.set(resolver, message);
+   }
+
+   @Override
+   public void set(PelvisOrientationTrajectoryMessage message)
+   {
+      setEnableUserPelvisControlDuringWalking(message.isEnableUserPelvisControlDuringWalking());
+      super.set(message);
+   }
+
+   public boolean isEnableUserPelvisControlDuringWalking()
+   {
+      return enableUserPelvisControlDuringWalking;
+   }
+
+   public void setEnableUserPelvisControlDuringWalking(boolean enableUserPelvisControlDuringWalking)
+   {
+      this.enableUserPelvisControlDuringWalking = enableUserPelvisControlDuringWalking;
+   }
+
    public PelvisOrientationTrajectoryCommand()
    {
    }
