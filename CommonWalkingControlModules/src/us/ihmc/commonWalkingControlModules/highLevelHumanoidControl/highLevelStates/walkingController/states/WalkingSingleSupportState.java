827df115f3160eafaaeb3217e55dcc2c6f289649
Robert <rgriffin@ihmc.us>
2017-05-03 23:35:10 -0400

    started adding an option to collapse the stance leg during swing

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/walkingController/states/WalkingSingleSupportState.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/walkingController/states/WalkingSingleSupportState.java
@@ -44,7 +44,8 @@ public class WalkingSingleSupportState extends SingleSupportState
    private final FeetManager feetManager;
    private final KneeAngleManager kneeAngleManager;
 
-   private final DoubleYoVariable percentOfSwingToStraightenLeg = new DoubleYoVariable("percentOfSwingToStraightenLeg", registry);
+   private final DoubleYoVariable fractionOfSwingToStraightenSwingLeg = new DoubleYoVariable("fractionOfSwingToStraightenSwingLeg", registry);
+   private final DoubleYoVariable fractionOfSwingToCollapseStanceLeg = new DoubleYoVariable("fractionOfSwingToCollapseStanceLeg", registry);
 
    private final DoubleYoVariable remainingSwingTimeAccordingToPlan = new DoubleYoVariable("remainingSwingTimeAccordingToPlan", registry);
    private final DoubleYoVariable estimatedRemainingSwingTimeUnderDisturbance = new DoubleYoVariable("estimatedRemainingSwingTimeUnderDisturbance", registry);
@@ -68,7 +69,8 @@ public class WalkingSingleSupportState extends SingleSupportState
       feetManager = managerFactory.getOrCreateFeetManager();
       kneeAngleManager = managerFactory.getOrCreateKneeAngleManager();
 
-      percentOfSwingToStraightenLeg.set(walkingControllerParameters.getStraightLegWalkingParameters().getFractionOfSwingToStraightenLeg());
+      fractionOfSwingToStraightenSwingLeg.set(walkingControllerParameters.getStraightLegWalkingParameters().getFractionOfSwingToStraightenLeg());
+      fractionOfSwingToCollapseStanceLeg.set(walkingControllerParameters.getStraightLegWalkingParameters().getFractionOfSwingToCollapseStanceLeg());
 
       icpErrorThresholdToSpeedUpSwing.set(walkingControllerParameters.getICPErrorThresholdToSpeedUpSwing());
       finishSingleSupportWhenICPPlannerIsDone.set(walkingControllerParameters.finishSingleSupportWhenICPPlannerIsDone());
@@ -144,9 +146,13 @@ public class WalkingSingleSupportState extends SingleSupportState
          balanceManager.updateSwingTimeRemaining(swingTimeRemaining);
       }
 
-      if (getTimeInCurrentState() > percentOfSwingToStraightenLeg.getDoubleValue() * swingTime)
+      if (getTimeInCurrentState() > fractionOfSwingToStraightenSwingLeg.getDoubleValue() * swingTime)
       {
          kneeAngleManager.straightenLegDuringSwing(swingSide);
+      }
+      if (getTimeInCurrentState() > fractionOfSwingToCollapseStanceLeg.getDoubleValue() * swingTime)
+      {
+         kneeAngleManager.collapseStanceLegDuringSwing(swingSide.getOppositeSide());
       }
 
       walkingMessageHandler.clearFootTrajectory();
