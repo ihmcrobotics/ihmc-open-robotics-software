8890e56e35b20397ee2f0c402c256f41b8fde26d
Robert <rgriffin@ihmc.us>
2017-05-11 09:49:14 -0400

    got the dynamicm reachability solver able to actually edit the timings through the balance manager to make things reachable

--- Atlas/test/us/ihmc/atlas/commonWalkingControlModules/dynamicReachability/AtlasDynamicReachabilityCalculatorTest.java
+++ Atlas/test/us/ihmc/atlas/commonWalkingControlModules/dynamicReachability/AtlasDynamicReachabilityCalculatorTest.java
@@ -5,6 +5,7 @@ import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.atlas.parameters.AtlasWalkingControllerParameters;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.commonWalkingControlModules.AvatarDynamicReachabilityCalculatorTest;
+import us.ihmc.commonWalkingControlModules.configurations.DynamicReachabilityParameters;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 
@@ -24,8 +25,40 @@ public class AtlasDynamicReachabilityCalculatorTest extends AvatarDynamicReachab
                   {
                      return true;
                   }
+
+                  @Override
+                  public double nominalHeightAboveAnkle()
+                  {
+                     return 0.84; // height for straight leg steps
+                  }
+
+                  @Override
+                  public double maximumHeightAboveAnkle()
+                  {
+                     return 0.95; // height for straight leg steps
+                  }
+
+                  @Override
+                  public boolean useOptimizationBasedICPController()
+                  {
+                     return true;
+                  }
+
+                  @Override
+                  public DynamicReachabilityParameters getDynamicReachabilityParameters()
+                  {
+                     return new DynamicReachabilityParameters()
+                     {
+                        @Override
+                        public double getMaximumDesiredKneeBend()
+                        {
+                           return 0.8;
+                        }
+                     };
+                  }
                };
             }
+
          };
 
       return atlasRobotModel;
