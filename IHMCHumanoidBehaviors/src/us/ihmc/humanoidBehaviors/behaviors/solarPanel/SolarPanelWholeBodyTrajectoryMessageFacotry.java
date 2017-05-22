35b49bf3a6415ab3ca21645de4a2ff4b545a1c6e
InhoLee <ilee@ihmc.us>
2017-05-22 16:23:49 -0500

    Enable WB trajectory generate under three dimensional redundancy.

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/solarPanel/SolarPanelWholeBodyTrajectoryMessageFacotry.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/solarPanel/SolarPanelWholeBodyTrajectoryMessageFacotry.java
@@ -6,6 +6,7 @@ import us.ihmc.commons.PrintTools;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.humanoidBehaviors.behaviors.rrtPlanner.SolarPanelCleaningInfo;
 import us.ihmc.humanoidRobotics.communication.packets.manipulation.HandTrajectoryMessage;
 import us.ihmc.humanoidRobotics.communication.packets.walking.ChestTrajectoryMessage;
 import us.ihmc.humanoidRobotics.communication.packets.walking.PelvisTrajectoryMessage;
@@ -86,6 +87,24 @@ public class SolarPanelWholeBodyTrajectoryMessageFacotry
       wholebodyTrajectoryMessage.setPelvisTrajectoryMessage(pelvisTrajectoryMessage);
    }
    
+   public void setMessage(ArrayList<RRTNode> rrtPath)
+   {
+      switch(SolarPanelCleaningInfo.getDegreesOfRedundancy())
+      {
+         case ONE:
+         {
+            setMessage1D(rrtPath);
+            break;
+         }
+         case THREE:
+         {
+            PrintTools.info("set three ");
+            setMessage3D(rrtPath);
+            break;
+         }
+      }
+   }
+   
    public void setMessage1D(ArrayList<RRTNode> rrtPath)
    {
       if(cleaningPath == null)
@@ -136,37 +155,51 @@ public class SolarPanelWholeBodyTrajectoryMessageFacotry
       }
       else
       {
-//         clearWholeBodyTrajectoryMessage();
-//         int numberOfWayPoints = rrtPath.size()-1;
-//         
-//         PrintTools.info("numberOfWayPoints "+numberOfWayPoints);
-//         
-//         HandTrajectoryMessage handTrajectoryMessage;
-//         ChestTrajectoryMessage chestTrajectoryMessage;
-//         
-//         handTrajectoryMessage = new HandTrajectoryMessage(RobotSide.RIGHT, numberOfWayPoints);
-//         chestTrajectoryMessage = new ChestTrajectoryMessage(numberOfWayPoints);
-//         
-//         handTrajectoryMessage.getFrameInformation().setTrajectoryReferenceFrame(midFeetFrame);
-//         handTrajectoryMessage.getFrameInformation().setDataReferenceFrame(midFeetFrame);
-//         
-//         chestTrajectoryMessage.getFrameInformation().setTrajectoryReferenceFrame(midFeetFrame);
-//         chestTrajectoryMessage.getFrameInformation().setDataReferenceFrame(midFeetFrame);
-//         
-//         for(int i=1;i<rrtPath.size();i++)
-//         {
-//            double time = rrtPath.get(i).getNodeData(0);
-//            SolarPanelCleaningPose cleaningPose = cleaningPath.getCleaningPose(time);
-//            
-//            handTrajectoryMessage.setTrajectoryPoint(i-1, time, cleaningPose.getDesiredHandPosition(), cleaningPose.getDesiredHandOrientation(), new Vector3D(), new Vector3D(), midFeetFrame);            
-//            
-//            Quaternion desiredChestOrientation = new Quaternion();
-//            desiredChestOrientation.appendYawRotation(rrtPath.get(i).getNodeData(1));
-//            chestTrajectoryMessage.setTrajectoryPoint(i-1, time, desiredChestOrientation, new Vector3D(), midFeetFrame);
-//         }
-//         
-//         wholebodyTrajectoryMessage.setHandTrajectoryMessage(handTrajectoryMessage);
-//         wholebodyTrajectoryMessage.setChestTrajectoryMessage(chestTrajectoryMessage);     
+         clearWholeBodyTrajectoryMessage();
+         int numberOfWayPoints = rrtPath.size()-1;
+         
+         PrintTools.info("numberOfWayPoints "+numberOfWayPoints);
+         
+         HandTrajectoryMessage handTrajectoryMessage;
+         ChestTrajectoryMessage chestTrajectoryMessage;
+         PelvisTrajectoryMessage pelvisTrajectoryMessage;
+         
+         handTrajectoryMessage = new HandTrajectoryMessage(RobotSide.RIGHT, numberOfWayPoints);
+         chestTrajectoryMessage = new ChestTrajectoryMessage(numberOfWayPoints);
+         pelvisTrajectoryMessage = new PelvisTrajectoryMessage(numberOfWayPoints);
+         
+         handTrajectoryMessage.getFrameInformation().setTrajectoryReferenceFrame(midFeetFrame);
+         handTrajectoryMessage.getFrameInformation().setDataReferenceFrame(midFeetFrame);
+         
+         chestTrajectoryMessage.getFrameInformation().setTrajectoryReferenceFrame(midFeetFrame);
+         chestTrajectoryMessage.getFrameInformation().setDataReferenceFrame(midFeetFrame);
+         
+         pelvisTrajectoryMessage.getFrameInformation().setTrajectoryReferenceFrame(midFeetFrame);
+         pelvisTrajectoryMessage.getFrameInformation().setDataReferenceFrame(midFeetFrame);
+         
+         SelectionMatrix6D pelvisTrajectorySelectionMatrix = new SelectionMatrix6D();
+         pelvisTrajectorySelectionMatrix.clearSelection();
+         pelvisTrajectorySelectionMatrix.selectLinearZ(true);
+         pelvisTrajectoryMessage.setSelectionMatrix(pelvisTrajectorySelectionMatrix);
+         
+         for(int i=1;i<rrtPath.size();i++)
+         {
+            double time = rrtPath.get(i).getNodeData(0);
+            SolarPanelCleaningPose cleaningPose = cleaningPath.getCleaningPose(time);
+            
+            handTrajectoryMessage.setTrajectoryPoint(i-1, time, cleaningPose.getDesiredHandPosition(), cleaningPose.getDesiredHandOrientation(), new Vector3D(), new Vector3D(), midFeetFrame);            
+            
+            Quaternion desiredChestOrientation = new Quaternion();
+            desiredChestOrientation.appendYawRotation(rrtPath.get(i).getNodeData(2));
+            desiredChestOrientation.appendPitchRotation(rrtPath.get(i).getNodeData(3));
+            chestTrajectoryMessage.setTrajectoryPoint(i-1, time, desiredChestOrientation, new Vector3D(), midFeetFrame);
+            
+            pelvisTrajectoryMessage.setTrajectoryPoint(i-1, time, new Point3D(0,0, rrtPath.get(i).getNodeData(1)), new Quaternion(), new Vector3D(), new Vector3D(), midFeetFrame);
+         }
+         
+         wholebodyTrajectoryMessage.setHandTrajectoryMessage(handTrajectoryMessage);
+         wholebodyTrajectoryMessage.setChestTrajectoryMessage(chestTrajectoryMessage);
+         wholebodyTrajectoryMessage.setPelvisTrajectoryMessage(pelvisTrajectoryMessage);
       }
    }
 }
