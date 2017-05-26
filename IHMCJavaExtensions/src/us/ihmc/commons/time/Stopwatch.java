5817ffda0a98a3400ea2804c9838794efc81b0b0
Duncan Calvert <dcalvert@ihmc.us>
2017-05-26 18:33:10 -0500

    Add suspend() and resume() to Stopwatch with 100% mutation coverage. Document. Ready for ihmc-commons.

--- IHMCJavaExtensions/src/us/ihmc/commons/time/Stopwatch.java
+++ IHMCJavaExtensions/src/us/ihmc/commons/time/Stopwatch.java
@@ -1,73 +1,111 @@
 package us.ihmc.commons.time;
 
+import java.util.function.DoubleSupplier;
+
+import us.ihmc.commons.Conversions;
+
 /**
- * <p>Different from {@link org.apache.commons.lang3.time.StopWatch StopWatch} in
- * Apache Commons Lang in the following ways:</p>
+ * <p>Minimal stopwatch with a friendly API. Measures durations of time.
+ * Ability to suspend and resume measurement of the current lap.</p>
+ * 
+ * <p>Alternative to {@link org.apache.commons.lang3.time.StopWatch StopWatch} in
+ * Apache Commons Lang with the following functional differences:</p>
  * 
  * <li>Provides a more fluent interface</li>
  * <li>Keeps track of average lap</li>
- * 
- * <p>TODO: Add suspend and resume.</p>
  */
 public class Stopwatch
 {
-   private double currentTime;
-   private double lastTime;
-   private double deltaTime;
-   private double startTime;
-   private long numLaps;
-   private double deltaSum;
-   
+   private final DoubleSupplier nowSupplier;
+
+   private double lapStart;
+   private long lapCount;
+   private double recordedLapTotal;
+
+   private boolean suspended;
+   private double suspendStart;
+   private double resumedSuspensionTotal;
+
    /**
-    * Start the clock. Functionally the same as reset.
+    * <p>Construct a new Stopwatch.</p>
+    * 
+    * <p>All methods will return NaN until {@link #start()} or {@link #reset()} is called.</p>
+    */
+   public Stopwatch()
+   {
+      nowSupplier = () -> {
+         return Conversions.nanosecondsToSeconds(System.nanoTime());
+      };
+      lapStart = Double.NaN;
+   }
+
+   /**
+    * Start the clock. Functionally the same as {@link #reset()}.
+    * 
+    * <p>NOTE: Will reset any suspensions of measurement from previous calls
+    * to {@link #suspend()} and {@link #resume()}.</p>
     * 
     * @return <code>this</code> for convenience.
     */
    public Stopwatch start()
    {
       reset();
-      
+
       return this;
    }
-   
+
    /**
     * Reset the current lap. Does not get averaged.
+    * 
+    * <p>NOTE: Will reset any suspensions of measurement from previous calls
+    * to {@link #suspend()} and {@link #resume()}.</p>
     */
    public void resetLap()
    {
-      lastTime = now();
+      lapStart = now();
+
+      resetSuspension();
    }
 
    /**
     * Reset the stopwatch. Clears average, resets lap, starts the clock.
+    * 
+    * <p>NOTE: Will reset any suspensions of measurement from previous calls
+    * to {@link #suspend()} and {@link #resume()}.</p>
     */
    public void reset()
    {
-      startTime = now();
-      lastTime = startTime;
-      
-      numLaps = 0;
-      deltaSum = 0.0;
+      lapStart = now();
+
+      resetSuspension();
+
+      lapCount = 0;
+      recordedLapTotal = 0.0;
    }
-   
+
    /**
     * Record a lap. Shortest time since last lap, start, or reset is returned and averaged.
+    * 
+    * <p>NOTE: Will omit the sum of all suspend durations and
+    * automatically resume measurement for the next lap.</p>
     * 
     * @return Lap time.
     */
    public double lap()
    {
-      currentTime = now();
-      deltaTime = currentTime - lastTime;
-      lastTime = currentTime;
+      double now = now();
+      double lapDuration = lapElapsed(now);
+      lapStart = now;
+
+      resetSuspension();
 
       // for average lap
-      numLaps++;
-      deltaSum += deltaTime;
-      
-      return deltaTime;
+      lapCount++;
+      recordedLapTotal += lapDuration;
+
+      return lapDuration;
    }
-   
+
    /**
     * Get the average lap duration.
     * 
@@ -75,9 +113,9 @@ public class Stopwatch
     */
    public double averageLap()
    {
-      return deltaSum / numLaps;
+      return recordedLapTotal / lapCount;
    }
-   
+
    /**
     * Get the total elapsed time. Since the last reset or start.
     * 
@@ -85,9 +123,9 @@ public class Stopwatch
     */
    public double totalElapsed()
    {
-      return now() - startTime;
+      return recordedLapTotal + lapElapsed(now());
    }
-   
+
    /**
     * Get the elapsed time in the current lap. Shortest time since last lap, reset, or start.
     * 
@@ -95,11 +133,73 @@ public class Stopwatch
     */
    public double lapElapsed()
    {
-      return now() - lastTime;
+      return lapElapsed(now());
    }
-   
+
+   /**
+    * <p>Suspend the measurement of a lap.</p>
+    * 
+    * <p>Use with {@link #resume()} to omit durations of time from the measurement of a lap.</p>
+    * 
+    * <p>WARNING: {@link #lap()}, {@link #reset()}, {@link #lapReset()}, and {@link #start()}
+    * will reset any suspensions of measurement from previous calls to {@link #suspend()} and
+    * {@link #resume()}.</p>
+    */
+   public void suspend()
+   {
+      if (!suspended)
+      {
+         suspended = true;
+         suspendStart = now();
+      }
+   }
+
+   /**
+    * <p>Resume the measurement of a lap.</p>
+    * 
+    * <p>Use with {@link #suspend()} to omit durations of time from the measurement of a lap.</p>
+    * 
+    * <p>WARNING: {@link #lap()}, {@link #reset()}, {@link #lapReset()}, and {@link #start()}
+    * will reset any suspensions of measurement from previous calls to {@link #suspend()} and
+    * {@link #resume()}.</p>
+    */
+   public void resume()
+   {
+      if (suspended)
+      {
+         suspended = false;
+         resumedSuspensionTotal += now() - suspendStart;
+      }
+   }
+
+   private double lapElapsed(double now)
+   {
+      double lapElapsed = now - lapStart;
+      lapElapsed -= resumedSuspensionTotal;
+      if (suspended)
+      {
+         lapElapsed -= (now - suspendStart);
+      }
+      return lapElapsed;
+   }
+
    private double now()
    {
-      return System.nanoTime() / 1e9;
+      return nowSupplier.getAsDouble();
+   }
+
+   private void resetSuspension()
+   {
+      suspended = false;
+      resumedSuspensionTotal = 0.0;
+   }
+
+   /**
+    * WARNING: Exclusively for unit testing. Do not use.
+    */
+   /** package-private */ Stopwatch(DoubleSupplier nowSupplier)
+   {
+      this.nowSupplier = nowSupplier;
+      lapStart = Double.NaN;
    }
 }
--- IHMCJavaExtensions/test/us/ihmc/commons/time/StopwatchTest.java
+++ IHMCJavaExtensions/test/us/ihmc/commons/time/StopwatchTest.java
@@ -2,84 +2,336 @@ package us.ihmc.commons.time;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Random;
+import java.util.function.DoubleSupplier;
+
 import org.junit.Test;
 
+import us.ihmc.commons.Conversions;
+import us.ihmc.commons.Epsilons;
 import us.ihmc.commons.MutationTestFacilitator;
 import us.ihmc.commons.PrintTools;
-import us.ihmc.commons.time.Stopwatch;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.tools.thread.ThreadTools;
 
+@SuppressWarnings(value = "unused")
 public class StopwatchTest
 {
+   @ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 30000)
+   public void testConstructor()
+   {
+      Stopwatch stopwatch = new Stopwatch();
+
+      assertEquals("didnt NaN", Double.NaN, stopwatch.averageLap(), 0.0);
+      assertEquals("didnt NaN", Double.NaN, stopwatch.lapElapsed(), 0.0);
+      assertEquals("didnt NaN", Double.NaN, stopwatch.totalElapsed(), 0.0);
+      assertEquals("didnt NaN", Double.NaN, stopwatch.lap(), 0.0);
+      
+      stopwatch = new Stopwatch(new FakeTimeProvider());
+
+      assertEquals("didnt NaN", Double.NaN, stopwatch.averageLap(), 0.0);
+      assertEquals("didnt NaN", Double.NaN, stopwatch.lapElapsed(), 0.0);
+      assertEquals("didnt NaN", Double.NaN, stopwatch.totalElapsed(), 0.0);
+      assertEquals("didnt NaN", Double.NaN, stopwatch.lap(), 0.0);
+   }
+
+   public class FakeTimeProvider implements DoubleSupplier
+   {
+      public double clock = 0.0;
+
+      public void incrementClock(double amount)
+      {
+         clock += amount;
+      }
+
+      @Override
+      public double getAsDouble()
+      {
+         return clock;
+      }
+   }
+
    @ContinuousIntegrationTest(estimatedDuration = 1.8)
    @Test(timeout = 30000)
-   public void testStopwatch()
+   public void testStopwatchWithRealTime()
    {
       Stopwatch stopwatch = new Stopwatch();
       
       double averageLap = stopwatch.averageLap();
       PrintTools.debug(this, "Lap: " + stopwatch.lap());
-      assertEquals("averageLap incorrect", Double.NaN, averageLap, 1e-2);
+      assertEquals("averageLap incorrect", Double.NaN, averageLap, Epsilons.ONE_HUNDREDTH);
       
-      stopwatch.start();
+      assertEquals("return ref not equal", stopwatch, stopwatch.start());
       
       double lapElapsed = stopwatch.lapElapsed();
       double totalElapsed = stopwatch.totalElapsed();
       averageLap = stopwatch.averageLap();
-      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, 1e-2);
-      assertEquals("totalElapsed incorrect", 0.0, totalElapsed, 1e-2);
-      assertEquals("averageLap incorrect", Double.NaN, averageLap, 1e-2);
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_HUNDREDTH);
+      assertEquals("totalElapsed incorrect", 0.0, totalElapsed, Epsilons.ONE_HUNDREDTH);
+      assertEquals("averageLap incorrect", Double.NaN, averageLap, Epsilons.ONE_HUNDREDTH);
       
       double sleepTime1 = 0.5;
       ThreadTools.sleepSeconds(sleepTime1);
       
       double lap = stopwatch.lap();
       averageLap = stopwatch.averageLap();
-      assertEquals("lap incorrect", sleepTime1, lap, 1e-2);
-      assertEquals("averageLap incorrect", sleepTime1, averageLap, 1e-2);
+      assertEquals("lap incorrect", sleepTime1, lap, Epsilons.ONE_HUNDREDTH);
+      assertEquals("averageLap incorrect", sleepTime1, averageLap, Epsilons.ONE_HUNDREDTH);
       
       double sleepTime2 = 1.0;
       ThreadTools.sleepSeconds(sleepTime2);
       
       lap = stopwatch.lap();
       averageLap = stopwatch.averageLap();
-      assertEquals("lap incorrect", sleepTime2, lap, 1e-2);
-      assertEquals("averageLap incorrect", (sleepTime1 + sleepTime2) / 2.0, averageLap, 1e-2);
+      assertEquals("lap incorrect", sleepTime2, lap, Epsilons.ONE_HUNDREDTH);
+      assertEquals("averageLap incorrect", (sleepTime1 + sleepTime2) / 2.0, averageLap, Epsilons.ONE_HUNDREDTH);
       
       stopwatch.resetLap();
       lapElapsed = stopwatch.lapElapsed();
-      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, 1e-2);
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_HUNDREDTH);
       
       lap = stopwatch.lap();
       averageLap = stopwatch.averageLap();
-      assertEquals("lap incorrect", 0.0, lap, 1e-2);
-      assertEquals("averageLap incorrect", (sleepTime1 + sleepTime2) / 3.0, averageLap, 1e-2);
+      assertEquals("lap incorrect", 0.0, lap, Epsilons.ONE_HUNDREDTH);
+      assertEquals("averageLap incorrect", (sleepTime1 + sleepTime2) / 3.0, averageLap, Epsilons.ONE_HUNDREDTH);
       
       double sleepTime3 = 0.3;
       ThreadTools.sleepSeconds(sleepTime3);
       
       lapElapsed = stopwatch.lapElapsed();
       totalElapsed = stopwatch.totalElapsed();
-      assertEquals("lapElapsed incorrect", sleepTime3, lapElapsed, 1e-2);
-      assertEquals("totalElapsed incorrect", sleepTime1 + sleepTime2 + sleepTime3, totalElapsed, 1e-2);
+      assertEquals("lapElapsed incorrect", sleepTime3, lapElapsed, Epsilons.ONE_HUNDREDTH);
+      assertEquals("totalElapsed incorrect", sleepTime1 + sleepTime2 + sleepTime3, totalElapsed, Epsilons.ONE_HUNDREDTH);
       
       stopwatch.reset();
       lapElapsed = stopwatch.lapElapsed();
       totalElapsed = stopwatch.totalElapsed();
       averageLap = stopwatch.averageLap();
-      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, 1e-2);
-      assertEquals("totalElapsed incorrect", 0.0, totalElapsed, 1e-2);
-      assertEquals("averageLap incorrect", Double.NaN, averageLap, 1e-2);
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_HUNDREDTH);
+      assertEquals("totalElapsed incorrect", 0.0, totalElapsed, Epsilons.ONE_HUNDREDTH);
+      assertEquals("averageLap incorrect", Double.NaN, averageLap, Epsilons.ONE_HUNDREDTH);
       
       double sleepTime4 = 0.3;
       ThreadTools.sleepSeconds(sleepTime4);
       
       stopwatch.resetLap();
       lapElapsed = stopwatch.lapElapsed();
-      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, 1e-2);
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_HUNDREDTH);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 1.8)
+   @Test(timeout = 30000)
+   public void testStopwatch()
+   {
+      FakeTimeProvider fakeTimeProvider = new FakeTimeProvider();
+      Stopwatch stopwatch = new Stopwatch(fakeTimeProvider);
+      
+      double averageLap = stopwatch.averageLap();
+      PrintTools.debug(this, "Lap: " + stopwatch.lap());
+      assertEquals("averageLap incorrect", Double.NaN, averageLap, Epsilons.ONE_TEN_BILLIONTH);
+      
+      assertEquals("return ref not equal", stopwatch, stopwatch.start());
+      
+      double lapElapsed = stopwatch.lapElapsed();
+      double totalElapsed = stopwatch.totalElapsed();
+      averageLap = stopwatch.averageLap();
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("totalElapsed incorrect", 0.0, totalElapsed, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("averageLap incorrect", Double.NaN, averageLap, Epsilons.ONE_TEN_BILLIONTH);
+      
+      double sleepTime1 = 0.5;
+      fakeTimeProvider.incrementClock(sleepTime1);
+      
+      double lap = stopwatch.lap();
+      averageLap = stopwatch.averageLap();
+      assertEquals("lap incorrect", sleepTime1, lap, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("averageLap incorrect", sleepTime1, averageLap, Epsilons.ONE_TEN_BILLIONTH);
+      
+      double sleepTime2 = 1.0;
+      fakeTimeProvider.incrementClock(sleepTime2);
+      
+      lap = stopwatch.lap();
+      averageLap = stopwatch.averageLap();
+      assertEquals("lap incorrect", sleepTime2, lap, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("averageLap incorrect", (sleepTime1 + sleepTime2) / 2.0, averageLap, Epsilons.ONE_TEN_BILLIONTH);
+      
+      stopwatch.resetLap();
+      lapElapsed = stopwatch.lapElapsed();
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_TEN_BILLIONTH);
+      
+      lap = stopwatch.lap();
+      averageLap = stopwatch.averageLap();
+      assertEquals("lap incorrect", 0.0, lap, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("averageLap incorrect", (sleepTime1 + sleepTime2) / 3.0, averageLap, Epsilons.ONE_TEN_BILLIONTH);
+      
+      double sleepTime3 = 0.3;
+      fakeTimeProvider.incrementClock(sleepTime3);
+      
+      lapElapsed = stopwatch.lapElapsed();
+      totalElapsed = stopwatch.totalElapsed();
+      assertEquals("lapElapsed incorrect", sleepTime3, lapElapsed, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("totalElapsed incorrect", sleepTime1 + sleepTime2 + sleepTime3, totalElapsed, Epsilons.ONE_TEN_BILLIONTH);
+      
+      stopwatch.reset();
+      lapElapsed = stopwatch.lapElapsed();
+      totalElapsed = stopwatch.totalElapsed();
+      averageLap = stopwatch.averageLap();
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("totalElapsed incorrect", 0.0, totalElapsed, Epsilons.ONE_TEN_BILLIONTH);
+      assertEquals("averageLap incorrect", Double.NaN, averageLap, Epsilons.ONE_TEN_BILLIONTH);
+      
+      double sleepTime4 = 0.3;
+      fakeTimeProvider.incrementClock(sleepTime4);
+      
+      stopwatch.resetLap();
+      lapElapsed = stopwatch.lapElapsed();
+      assertEquals("lapElapsed incorrect", 0.0, lapElapsed, Epsilons.ONE_TEN_BILLIONTH);
    }
    
+   @ContinuousIntegrationTest(estimatedDuration = 0.1)
+   @Test(timeout = 30000)
+   public void testSuspendAndResume()
+   {
+      Random random = new Random(12389L);
+      FakeTimeProvider fakeTimeProvider = new FakeTimeProvider();
+      Stopwatch stopwatch = new Stopwatch(fakeTimeProvider);
+
+      for (int i = 0; i < 10; i++)
+      {
+         stopwatch.start();
+         double randomDuration1 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration2 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.resume();
+         double randomDuration3 = sleep(fakeTimeProvider, randomTime(random));
+         assertTimeEquals(randomDuration1 + randomDuration3, stopwatch.lapElapsed());
+         assertTimeEquals(randomDuration1 + randomDuration3, stopwatch.totalElapsed());
+         assertTimeEquals(randomDuration1 + randomDuration3, stopwatch.lap());
+      }
+
+      for (int i = 0; i < 10; i++)
+      {
+         stopwatch.start();
+         double randomDuration1 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.resume();
+         double randomDuration2 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration3 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.resume();
+         double randomDuration4 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration5 = sleep(fakeTimeProvider, randomTime(random));
+         double expectedElapsed = randomDuration1 + randomDuration2 + randomDuration4;
+         assertTimeEquals(expectedElapsed, stopwatch.lapElapsed());
+         assertTimeEquals(expectedElapsed, stopwatch.totalElapsed());
+         assertTimeEquals(expectedElapsed, stopwatch.lap());
+      }
+
+      for (int i = 0; i < 10; i++)
+      {
+         stopwatch.reset();
+         double randomDuration1 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration2 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.resume();
+         double randomDuration3 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.lap();
+         double randomDuration4 = sleep(fakeTimeProvider, randomTime(random));
+         assertTimeEquals(randomDuration4, stopwatch.lapElapsed());
+         assertTimeEquals(randomDuration1 + randomDuration3 + randomDuration4, stopwatch.totalElapsed());
+         assertTimeEquals(randomDuration4, stopwatch.lap());
+      }
+
+      for (int i = 0; i < 10; i++)
+      {
+         stopwatch.reset();
+         double randomDuration1 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration2 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.resume();
+         double randomDuration3 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.lap();
+         double randomDuration4 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration5 = sleep(fakeTimeProvider, randomTime(random));
+         assertTimeEquals(randomDuration4, stopwatch.lapElapsed());
+         assertTimeEquals(randomDuration1 + randomDuration3 + randomDuration4, stopwatch.totalElapsed());
+         assertTimeEquals(randomDuration4, stopwatch.lap());
+      }
+
+      for (int i = 0; i < 10; i++)
+      {
+         stopwatch.reset();
+         double randomDuration1 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration2 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.resume();
+         double randomDuration3 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.lap();
+         double randomDuration4 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.suspend();
+         double randomDuration5 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.reset();
+         double randomDuration6 = sleep(fakeTimeProvider, randomTime(random));
+         assertTimeEquals(randomDuration6, stopwatch.lapElapsed());
+         assertTimeEquals(randomDuration6, stopwatch.totalElapsed());
+         assertTimeEquals(randomDuration6, stopwatch.lap());
+         stopwatch.suspend();
+         double randomDuration7 = sleep(fakeTimeProvider, randomTime(random));
+         stopwatch.resetLap();
+         double randomDuration8 = sleep(fakeTimeProvider, randomTime(random));
+         assertTimeEquals(randomDuration8, stopwatch.lapElapsed());
+         assertTimeEquals(randomDuration8 + randomDuration6, stopwatch.totalElapsed());
+         assertTimeEquals(randomDuration8, stopwatch.lap());
+      }
+
+      stopwatch.reset();
+      {
+         double randomDuration1 = randomTime(random);
+         double randomDuration2 = randomTime(random);
+         double randomDuration3 = randomTime(random);
+         for (int i = 0; i < 10; i++)
+         {
+            sleep(fakeTimeProvider, randomDuration1);
+            stopwatch.suspend();
+            sleep(fakeTimeProvider, randomDuration2);
+            stopwatch.resume();
+            sleep(fakeTimeProvider, randomDuration3);
+            stopwatch.resetLap();
+            sleep(fakeTimeProvider, randomDuration1);
+            stopwatch.suspend();
+            sleep(fakeTimeProvider, randomDuration1);
+            stopwatch.suspend();
+            sleep(fakeTimeProvider, randomDuration2);
+            stopwatch.resume();
+            stopwatch.resume();
+            sleep(fakeTimeProvider, randomDuration3);
+            assertTimeEquals(randomDuration1 + randomDuration3, stopwatch.lapElapsed());
+            stopwatch.lap();
+            assertTimeEquals((randomDuration1 + randomDuration3) * (i + 1), stopwatch.totalElapsed());
+         }
+      }
+   }
+
+   private void assertTimeEquals(double expected, double actual)
+   {
+      PrintTools.info("Expected: " + expected + " (s)  Actual: " + actual + " (s)");
+      assertEquals("Expected time incorrect", expected, actual, Epsilons.ONE_TEN_BILLIONTH);
+      PrintTools.info("Assertions passed!");
+   }
+
+   private double randomTime(Random random)
+   {
+      return Conversions.millisecondsToSeconds(Math.abs(random.nextInt()) % 100);
+   }
+
+   private double sleep(FakeTimeProvider fakeTimeProvider, double duration)
+   {
+      fakeTimeProvider.incrementClock(duration);
+      return duration;
+   }
+
    public static void main(String[] args)
    {
       MutationTestFacilitator.facilitateMutationTestForClass(Stopwatch.class, StopwatchTest.class);
