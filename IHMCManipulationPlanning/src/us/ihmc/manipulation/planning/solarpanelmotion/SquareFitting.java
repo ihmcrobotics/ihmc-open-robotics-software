bd0b00802c362541d9b2faa5d849635ea7ad8575
InhoLee <ilee@ihmc.us>
2017-05-31 15:22:28 -0500

    Square fitting is created.

---
+++ IHMCManipulationPlanning/src/us/ihmc/manipulation/planning/solarpanelmotion/SquareFitting.java
@@ -0,0 +1,13 @@
+package us.ihmc.manipulation.planning.solarpanelmotion;
+
+import us.ihmc.robotics.geometry.PlanarRegion;
+
+public class SquareFitting
+{
+   private PlanarRegion planarRegion;
+   
+   public SquareFitting()
+   {
+      
+   }
+}
