cfed4fb01e040bf5ff2e71894b6c574a2ff0fc78
bshrewsbury <bshrewsbury@ihmc.us>
2017-06-01 16:37:51 -0500

    added comments

--- IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/controllerAPI/command/PelvisHeightTrajectoryCommand.java
+++ IHMCHumanoidRobotics/src/us/ihmc/humanoidRobotics/communication/controllerAPI/command/PelvisHeightTrajectoryCommand.java
@@ -9,7 +9,13 @@ import us.ihmc.robotics.weightMatrices.WeightMatrix3D;
 
 public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryControllerCommand<PelvisHeightTrajectoryCommand, PelvisHeightTrajectoryMessage>
 {
+   /** Execute this trajectory in user mode. User mode tries to achieve the desired regardless of the leg kinematics **/
    private boolean enableUserPelvisControl = false;
+   
+   /** 
+    * If {@code enableUserPelvisControl} is true then {@code enableUserPelvisControlDuringWalking} will keep the height manager in user mode while walking. If this is false the height
+    * manager will switch to controller mode when walking
+    **/
    private boolean enableUserPelvisControlDuringWalking = false;
    
    private Point3D tempPoint = new Point3D();
@@ -20,6 +26,10 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
       super(ReferenceFrame.getWorldFrame(), ReferenceFrame.getWorldFrame());
    }
 
+   /**
+    * clears the points and sets the frame to the reference frame supplied. 
+    * Sets enableUserPelvisControl and enableUserPelvisControlDuringWalking to false;
+    */
    @Override
    public void clear()
    {
@@ -28,6 +38,11 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
       enableUserPelvisControlDuringWalking = false;
    }
    
+   /**
+    * clears the points and sets the frame to the reference frame supplied. 
+    * Sets enableUserPelvisControl and enableUserPelvisControlDuringWalking to false;
+    * @param referenceFrame the reference frame the trajectory points will be expressed in
+    */
    public void clear(ReferenceFrame referenceFrame)
    {
       super.clear(referenceFrame);
@@ -35,6 +50,10 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
       enableUserPelvisControlDuringWalking = false;
    }
 
+   /**
+    * copy setter, copies all the data from the other command to this command
+    * @param command the other command
+    */
    @Override
    public void set(PelvisHeightTrajectoryCommand command)
    {
@@ -43,6 +62,10 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
       enableUserPelvisControlDuringWalking = command.enableUserPelvisControlDuringWalking;
    }
    
+   /**
+    * set this command to the contents of the message
+    * @param message the message that has trajectory data
+    */
    @Override
    public void set(PelvisHeightTrajectoryMessage message)
    {
@@ -51,6 +74,12 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
       enableUserPelvisControlDuringWalking = message.isEnableUserPelvisControlDuringWalking();
    }
    
+   /**
+    * set this command to the contents of the message
+    * @param dataFrame the frame the data is expressed in
+    * @param trajectoryFrame the frame the trajectory will be executed in
+    * @param message the message that has trajectory data
+    */
    public void set(ReferenceFrame dataFrame, ReferenceFrame trajectoryFrame, PelvisHeightTrajectoryMessage message)
    {
       clear(dataFrame);
@@ -59,6 +88,11 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
       enableUserPelvisControlDuringWalking = message.isEnableUserPelvisControlDuringWalking();
    }
 
+   /**
+    * Set this command to the contents of the z height of the pelvis trajectory command 
+    * Copies the z points, velocities, and the linear z weight and frame
+    * @param command the other command
+    */
    public void set(PelvisTrajectoryCommand command)
    {
       clear(command.getDataFrame());
@@ -87,21 +121,41 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
       enableUserPelvisControlDuringWalking = command.isEnableUserPelvisControlDuringWalking();
    }
    
+   /** 
+    * If {@code enableUserPelvisControl} is true then {@code enableUserPelvisControlDuringWalking} will keep the height manager in user mode while walking. If this is false the height
+    * manager will switch to controller mode when walking
+    * @return whether or not user mode is enabled while walking
+    **/
    public boolean isEnableUserPelvisControlDuringWalking()
    {
       return enableUserPelvisControlDuringWalking;
    }
 
+   /** 
+    * If {@code enableUserPelvisControl} is true then {@code enableUserPelvisControlDuringWalking} will keep the height manager in user mode while walking. If this is false the height
+    * manager will switch to controller mode when walking
+    * @param enableUserPelvisControlDuringWalking sets whether or not user mode is enabled while walking
+    **/
    public void setEnableUserPelvisControlDuringWalking(boolean enableUserPelvisControlDuringWalking)
    {
       this.enableUserPelvisControlDuringWalking = enableUserPelvisControlDuringWalking;
    }
 
+   /**
+    * Returns whether or not user mode is enabled.
+    * If enabled the controller will execute the trajectory in user mode. 
+    * User mode will try to achieve the desireds regardless of the leg kinematics
+    * @return  whether or not user mode is enabled.
+    */
    public boolean isEnableUserPelvisControl()
    {
       return enableUserPelvisControl;
    }
 
+   /**
+    * If enabled the controller will execute the trajectory in user mode. 
+    * User mode will try to achieve the desireds regardless of the leg kinematics
+    */
    public void setEnableUserPelvisControl(boolean enableUserPelvisControl)
    {
       this.enableUserPelvisControl = enableUserPelvisControl;
@@ -114,8 +168,7 @@ public class PelvisHeightTrajectoryCommand extends EuclideanTrajectoryController
    }
 
    /**
-    * Convenience method for accessing {@link #trajectoryPointList}. To get the list use
-    * {@link #getTrajectoryPointList()}.
+    * add a trajectory point to this message
     */
    public void addTrajectoryPoint(double time, double zHeight, double velocity)
    {
