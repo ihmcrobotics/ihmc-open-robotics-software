da171b1fddd7d6c935f9c7a470f199df77868f25
InhoLee <ilee@ihmc.us>
2017-06-05 16:44:28 -0500

    Test various pose

--- Atlas/test/us/ihmc/atlas/behaviorTests/AtlasWholeBodyPoseValidityTesterTest.java
+++ Atlas/test/us/ihmc/atlas/behaviorTests/AtlasWholeBodyPoseValidityTesterTest.java
@@ -8,7 +8,8 @@ import us.ihmc.avatar.drcRobot.DRCRobotModel.RobotTarget;
 
 public class AtlasWholeBodyPoseValidityTesterTest extends WholeBodyPoseValidityTesterTest
 {
-   private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false);
+   private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, RobotTarget.SCS, false);
+   //private final DRCRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false);
 
    @Override
    public DRCRobotModel getRobotModel()
