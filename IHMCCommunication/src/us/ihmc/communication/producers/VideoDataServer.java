7287bd7222a47574ecccf3431924be0bc5e62304
Duncan Calvert <dcalvert@ihmc.us>
2017-06-06 16:02:50 -0500

    Create VideoCallback to unify VideoDataServer and VideoStreamer.

--- IHMCCommunication/src/us/ihmc/communication/producers/VideoDataServer.java
+++ IHMCCommunication/src/us/ihmc/communication/producers/VideoDataServer.java
@@ -1,13 +1,9 @@
 package us.ihmc.communication.producers;
 
-import java.awt.image.BufferedImage;
-
-import boofcv.struct.calib.IntrinsicParameters;
 import us.ihmc.communication.interfaces.Connectable;
-import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
-import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
+import us.ihmc.communication.video.VideoCallback;
 
-public interface VideoDataServer extends Connectable
+public interface VideoDataServer extends Connectable, VideoCallback
 {
-   public void updateImage(VideoSource videoSource, BufferedImage bufferedImage, long timeStamp, Point3DReadOnly cameraPosition, QuaternionReadOnly cameraOrientation, IntrinsicParameters intrinsicParameters);
+   
 }
