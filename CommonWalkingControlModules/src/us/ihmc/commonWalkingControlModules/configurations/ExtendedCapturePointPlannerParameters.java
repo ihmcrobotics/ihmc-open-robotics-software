8c909d1f40440cdb636912301bfc1d5df8937884
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-06-14 17:02:31 -0500

    Made new extended ICP planner parameters interface

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/configurations/ExtendedCapturePointPlannerParameters.java
@@ -0,0 +1,15 @@
+package us.ihmc.commonWalkingControlModules.configurations;
+
+import java.util.List;
+
+import us.ihmc.robotics.geometry.FrameVector2d;
+
+public abstract class ExtendedCapturePointPlannerParameters extends CapturePointPlannerParameters
+{
+   public abstract List<FrameVector2d> getCoPOffsets();
+   
+   public int getNumberOfPointsPerFoot()
+   {
+      return 2;
+   }
+}
