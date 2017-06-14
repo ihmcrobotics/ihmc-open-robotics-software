88531c3fc456245973116ce3c3315f56fe190a3b
Robert <rgriffin@ihmc.us>
2017-06-13 23:20:50 -0500

    added mass scaling into the downward force

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/configurations/LeapOfFaithParameters.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/configurations/LeapOfFaithParameters.java
@@ -2,14 +2,31 @@ package us.ihmc.commonWalkingControlModules.configurations;
 
 public class LeapOfFaithParameters
 {
+   public final double massScale;
+
+   public LeapOfFaithParameters()
+   {
+      this(1.0);
+   }
+
+   public LeapOfFaithParameters(double massScale)
+   {
+      this.massScale = massScale;
+   }
+
    public boolean useFootForce()
    {
       return false;
    }
 
+   /**
+    * This is the gain used to determine an additional downward force to be applied to the swing foot to force it to contact the ground.
+    * It is multiplied by the time
+    * @return
+    */
    public double getFootForceGain()
    {
-      return 100.0;
+      return 100.0 * massScale;
    }
 
    public boolean usePelvisRelaxation()
