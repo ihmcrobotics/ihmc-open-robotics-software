f4797d5686c3a136aecdd864dbc464170ef30d8f
Tim Seyde <tseyde@ihmc.us>
2017-06-15 15:03:55 -0500

    Defined interfaces and enum for new ICP planner

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/CMPComponentPolynomialTrajectoryPlannerInterface.java
@@ -0,0 +1,16 @@
+package us.ihmc.commonWalkingControlModules.instantaneousCapturePoint;
+
+import java.util.List;
+
+import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerParameters;
+import us.ihmc.humanoidRobotics.footstep.Footstep;
+import us.ihmc.humanoidRobotics.footstep.FootstepTiming;
+import us.ihmc.robotics.math.trajectories.YoPolynomial;
+
+public interface CMPComponentPolynomialTrajectoryPlannerInterface
+{
+   public CMPComponentType getComponentType();
+   public void initializeParameters(CapturePointPlannerParameters icpPlannerParameters);
+   public void addFootstepToPlan(Footstep footstep, FootstepTiming timing);
+   public List<YoPolynomial> getPolynomialTrajectory();
+}
---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/CMPComponentType.java
@@ -0,0 +1,7 @@
+package us.ihmc.commonWalkingControlModules.instantaneousCapturePoint;
+
+public enum CMPComponentType
+{
+   CoP,
+   AngularMomentum;
+}
