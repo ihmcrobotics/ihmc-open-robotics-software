8312ba1852ed04926d23397672e288588c5e6ec7
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-06-15 16:19:21 -0500

    Modified interface to use extended icp planning parameters

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/CMPComponentPolynomialTrajectoryPlannerInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/CMPComponentPolynomialTrajectoryPlannerInterface.java
@@ -3,6 +3,7 @@ package us.ihmc.commonWalkingControlModules.instantaneousCapturePoint;
 import java.util.List;
 
 import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerParameters;
+import us.ihmc.commonWalkingControlModules.configurations.ExtendedCapturePointPlannerParameters;
 import us.ihmc.humanoidRobotics.footstep.Footstep;
 import us.ihmc.humanoidRobotics.footstep.FootstepTiming;
 import us.ihmc.robotics.math.trajectories.YoPolynomial;
@@ -10,7 +11,7 @@ import us.ihmc.robotics.math.trajectories.YoPolynomial;
 public interface CMPComponentPolynomialTrajectoryPlannerInterface
 {
    public CMPComponentType getComponentType();
-   public void initializeParameters(CapturePointPlannerParameters icpPlannerParameters);
+   public void initializeParameters(ExtendedCapturePointPlannerParameters icpPlannerParameters);
    public void addFootstepToPlan(Footstep footstep, FootstepTiming timing);
    public List<YoPolynomial> getPolynomialTrajectory();
 }
