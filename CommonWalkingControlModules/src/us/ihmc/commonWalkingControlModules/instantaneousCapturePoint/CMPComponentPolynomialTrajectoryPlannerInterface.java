ae0d04c13910bf14cd6fb337b6ccbcb6d6703679
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-06-15 17:22:46 -0500

    Added spline interpolation degree to ICP planner parameters. Modified trajectory planner interface to use YoPolynomial3D

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/CMPComponentPolynomialTrajectoryPlannerInterface.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/CMPComponentPolynomialTrajectoryPlannerInterface.java
@@ -3,13 +3,12 @@ package us.ihmc.commonWalkingControlModules.instantaneousCapturePoint;
 import java.util.List;
 
 import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.BipedSupportPolygons;
-import us.ihmc.commonWalkingControlModules.configurations.CapturePointPlannerParameters;
 import us.ihmc.commonWalkingControlModules.configurations.ExtendedCapturePointPlannerParameters;
 import us.ihmc.humanoidRobotics.bipedSupportPolygons.ContactablePlaneBody;
 import us.ihmc.humanoidRobotics.footstep.Footstep;
 import us.ihmc.humanoidRobotics.footstep.FootstepTiming;
 import us.ihmc.robotics.dataStructures.registry.YoVariableRegistry;
-import us.ihmc.robotics.math.trajectories.YoPolynomial;
+import us.ihmc.robotics.math.trajectories.YoPolynomial3D;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
 public interface CMPComponentPolynomialTrajectoryPlannerInterface
@@ -18,5 +17,5 @@ public interface CMPComponentPolynomialTrajectoryPlannerInterface
    public void initializeParameters(ExtendedCapturePointPlannerParameters icpPlannerParameters, BipedSupportPolygons bipedSupportPolygons,
                                     SideDependentList<? extends ContactablePlaneBody> contactableFeet, YoVariableRegistry parentRegistry);
    public void addFootstepToPlan(Footstep footstep, FootstepTiming timing);
-   public List<YoPolynomial> getPolynomialTrajectory();
+   public List<YoPolynomial3D> getPolynomialTrajectory();
 }
