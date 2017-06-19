92348b08d672e05948df5790f3e0fba09e788fff
Tim Seyde <tseyde@ihmc.us>
2017-06-18 20:12:03 -0500

    Refactored YoPolynomial derivative methods

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/math/trajectories/YoPolynomial.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/math/trajectories/YoPolynomial.java
@@ -13,7 +13,7 @@ import us.ihmc.yoVariables.variable.YoInteger;
 public class YoPolynomial
 {
    private final int maximumNumberOfCoefficients;
-   private double pos, vel, acc, dnPos;
+   private double pos, vel, acc, dPos;
    private final YoDouble[] a;
    private final YoInteger numberOfCoefficients;
    private final DenseMatrix64F constraintMatrix;
@@ -21,9 +21,8 @@ public class YoPolynomial
    private final DenseMatrix64F coefficientVector;
    private final double[] xPowers;
    
-   private final DenseMatrix64F xPowersNthDerivativeVector;
-   
-   // stores the N-th derivative of the xPowers vector
+   // Stores the (n-th order) derivative of the xPowers vector
+   private final DenseMatrix64F xPowersDerivativeVector;
 
    private final LinearSolver<DenseMatrix64F> solver;
 
@@ -39,7 +38,7 @@ public class YoPolynomial
       coefficientVector = new DenseMatrix64F(maximumNumberOfCoefficients, 1);
       xPowers = new double[maximumNumberOfCoefficients];
       
-      xPowersNthDerivativeVector = new DenseMatrix64F(maximumNumberOfCoefficients, 1);
+      xPowersDerivativeVector = new DenseMatrix64F(maximumNumberOfCoefficients, 1);
 
       numberOfCoefficients = new YoInteger(name + "_nCoeffs", registry);
 
@@ -63,7 +62,7 @@ public class YoPolynomial
       coefficientVector = new DenseMatrix64F(maximumNumberOfCoefficients, 1);
       xPowers = new double[maximumNumberOfCoefficients];
       
-      xPowersNthDerivativeVector = new DenseMatrix64F(maximumNumberOfCoefficients, 1);
+      xPowersDerivativeVector = new DenseMatrix64F(maximumNumberOfCoefficients, 1);
    }
 
    public double getPosition()
@@ -556,18 +555,19 @@ public class YoPolynomial
       }
    }
    
+   // Returns the order-th derivative of the polynomial at x
    public double getDerivative(int order, double x)
    {
       setXPowers(xPowers, x);
       
-      dnPos = 0.0;
+      dPos = 0.0;
       int derivativeCoefficient = 0;
       for (int i = order; i < numberOfCoefficients.getIntegerValue(); i++)
       {
          derivativeCoefficient = getDerivativeCoefficient(order, i);
-         dnPos += derivativeCoefficient * a[i].getDoubleValue() * xPowers[i - order];
+         dPos += derivativeCoefficient * a[i].getDoubleValue() * xPowers[i - order];
       }
-      return dnPos;
+      return dPos;
    }
 
    // Returns the order-th derivative of the xPowers vector at value x (Note: does NOT return the YoPolynomials order-th derivative at x)
@@ -579,10 +579,10 @@ public class YoPolynomial
       for(int i = order; i < xPowers.length; i++)
       {
          derivativeCoefficient = getDerivativeCoefficient(order, i);
-         xPowersNthDerivativeVector.set(i, derivativeCoefficient*xPowers[i - order]);
+         xPowersDerivativeVector.set(i, derivativeCoefficient*xPowers[i - order]);
       }
       
-      return xPowersNthDerivativeVector;
+      return xPowersDerivativeVector;
    }
    
    // Returns the constant coefficient at the exponent-th entry of the order-th derivative vector
