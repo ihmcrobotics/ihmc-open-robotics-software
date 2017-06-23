de74ff58f11a5395b996007845a170ed641b841f
Robert <rgriffin@ihmc.us>
2017-06-23 00:28:47 -0500

    fixed some issues with not-doable linear equations

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/smoothCMP/TransferCoPTrajectory.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/smoothCMP/TransferCoPTrajectory.java
@@ -6,29 +6,44 @@ import us.ihmc.robotics.math.trajectories.YoFramePolynomial3D;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
+import us.ihmc.yoVariables.variable.YoInteger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransferCoPTrajectory
 {
    private final YoDouble transferDuration;
    private final YoDouble transferSplitFraction;
 
-   private final YoFramePolynomial3D transferSegment;
+   private final YoInteger numberOfSegments;
+
+   private final YoFramePolynomial3D transferInitialSegment;
+   private final YoFramePolynomial3D transferEndSegment;
 
    private final FramePoint intermediatePoint = new FramePoint();
 
    private ReferenceFrame referenceFrame = ReferenceFrame.getWorldFrame();
+
+   private final List<YoFramePolynomial3D> transferSegments = new ArrayList<>();
 
    public TransferCoPTrajectory(String namePrefix, int stepNumber, YoDouble transferDuration, YoDouble transferSplitFraction, YoVariableRegistry registry)
    {
       this.transferDuration = transferDuration;
       this.transferSplitFraction = transferSplitFraction;
 
-      transferSegment = new YoFramePolynomial3D(namePrefix + stepNumber + "TransferSegment", 5, referenceFrame, registry);
+      numberOfSegments = new YoInteger(namePrefix + stepNumber + "TransferNumberOfSegments", registry);
+
+      transferInitialSegment = new YoFramePolynomial3D(namePrefix + stepNumber + "TransferInitialSegment", 5, referenceFrame, registry);
+      transferEndSegment = new YoFramePolynomial3D(namePrefix + stepNumber + "TransferEndSegment", 5, referenceFrame, registry);
    }
 
    public void reset()
    {
-      transferSegment.reset();
+      transferInitialSegment.reset();
+      transferEndSegment.reset();
+
+      transferSegments.clear();
    }
 
    public void update(CoPSplineType splineType, FramePoint initialCoP, FramePoint finalCoP)
@@ -39,10 +54,20 @@ public class TransferCoPTrajectory
       switch (splineType)
       {
       case CUBIC:
-         transferSegment.setCubicUsingIntermediatePoint(0.0, intermediateDuration, transferDuration.getDoubleValue(), initialCoP, intermediatePoint, finalCoP);
+         transferInitialSegment.setCubicUsingIntermediatePoint(0.0, intermediateDuration, transferDuration.getDoubleValue(), initialCoP, intermediatePoint, finalCoP);
+
+         transferSegments.add(transferInitialSegment);
+
+         numberOfSegments.set(1);
          break;
       default:
-         transferSegment.setLinearWithIntermediatePoint(0.0, intermediateDuration, transferDuration.getDoubleValue(), initialCoP, intermediatePoint, finalCoP);
+         transferInitialSegment.setLinear(0.0, intermediateDuration, initialCoP, intermediatePoint);
+         transferEndSegment.setLinear(intermediateDuration, transferDuration.getDoubleValue(), intermediatePoint, finalCoP);
+
+         transferSegments.add(transferInitialSegment);
+         transferSegments.add(transferEndSegment);
+
+         numberOfSegments.set(2);
          break;
       }
    }
