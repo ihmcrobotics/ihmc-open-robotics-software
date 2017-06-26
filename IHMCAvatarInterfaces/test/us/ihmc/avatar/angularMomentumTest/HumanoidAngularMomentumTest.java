16b8f4eb89cd6747a36c161ee40a6d608e4c37b1
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-06-26 17:46:18 -0500

    Testing various formulations for parameterized swing trajectory

--- IHMCAvatarInterfaces/test/us/ihmc/avatar/angularMomentumTest/HumanoidAngularMomentumTest.java
+++ IHMCAvatarInterfaces/test/us/ihmc/avatar/angularMomentumTest/HumanoidAngularMomentumTest.java
@@ -1,11 +1,12 @@
 package us.ihmc.avatar.angularMomentumTest;
 
+import java.util.ArrayList;
+
 import us.ihmc.avatar.DRCStartingLocation;
 import us.ihmc.avatar.MultiRobotTestInterface;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.initialSetup.OffsetAndYawRobotInitialSetup;
 import us.ihmc.avatar.testTools.DRCSimulationTestHelper;
-import us.ihmc.commons.PrintTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.Vector3D;
@@ -14,12 +15,14 @@ import us.ihmc.humanoidRobotics.communication.packets.walking.FootstepDataListMe
 import us.ihmc.humanoidRobotics.communication.packets.walking.FootstepDataMessage;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.math.frames.YoFrameVector;
+import us.ihmc.robotics.math.trajectories.YoPolynomial3D;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.simulationConstructionSetTools.robotController.SimpleRobotController;
 import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
 import us.ihmc.simulationconstructionset.FloatingJoint;
 import us.ihmc.simulationconstructionset.HumanoidFloatingRootJointRobot;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.simulationconstructionset.util.simulationTesting.SimulationTestingParameters;
 import us.ihmc.tools.thread.ThreadTools;
@@ -33,6 +36,9 @@ public abstract class HumanoidAngularMomentumTest implements MultiRobotTestInter
    private DRCRobotModel robotModel;
    private FullHumanoidRobotModel fullRobotModel;
    private DRCSimulationTestHelper drcSimulationTestHelper;
+   private double defaultSwingDuration = 0.5;
+   private double defaultTransferDuration = 0.1;
+   private double defaultFinalTransferDuration = 0.1;
 
    public void walk() throws SimulationExceededMaximumTimeException
    {
@@ -50,11 +56,13 @@ public abstract class HumanoidAngularMomentumTest implements MultiRobotTestInter
       {
          x += stepLength;
          footLocation.set(x, side.negateIfRightSide(stepWidth) / 2, 0.0);
-         addFootstep(footLocation, footOrientation, side, footMessage);
+         addFootstep(footLocation, footOrientation, side, defaultSwingDuration, defaultTransferDuration, footMessage);
          side = side.getOppositeSide();
       }
       footLocation.set(x, side.negateIfRightSide(stepWidth / 2), 0.0);
-      addFootstep(footLocation, footOrientation, side, footMessage);
+      addFootstep(footLocation, footOrientation, side, defaultSwingDuration, defaultTransferDuration, footMessage);
+      footMessage.setFinalTransferDuration(defaultFinalTransferDuration);
+      angularMomentumSpy.setFootstepList(footMessage);
       double testTime = numberOfFootstepsToTake * 2.0 + 1.0;
       drcSimulationTestHelper.send(footMessage);
       drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(testTime);
@@ -98,12 +106,15 @@ public abstract class HumanoidAngularMomentumTest implements MultiRobotTestInter
       simulationTestingParameters = null;
    }
 
-   private void addFootstep(Point3D stepLocation, Quaternion orient, RobotSide robotSide, FootstepDataListMessage message)
+   private void addFootstep(Point3D stepLocation, Quaternion orient, RobotSide robotSide, double swingTime, double transferTime,
+                            FootstepDataListMessage message)
    {
       FootstepDataMessage footstepData = new FootstepDataMessage();
       footstepData.setLocation(stepLocation);
       footstepData.setOrientation(orient);
       footstepData.setRobotSide(robotSide);
+      footstepData.setSwingDuration(swingTime);
+      footstepData.setTransferDuration(transferTime);
       message.add(footstepData);
    }
 
@@ -129,9 +140,20 @@ public abstract class HumanoidAngularMomentumTest implements MultiRobotTestInter
       Vector3D comLinMom = new Vector3D();
       Point3D comPoint = new Point3D();
       YoFrameVector comAngularMomentum;
+      YoFrameVector comEstimatedAngularMomentum;
       FloatingJoint rootJoint;
       Quaternion rootOrientation = new Quaternion();
       RigidBodyTransform rootJointTransform = new RigidBodyTransform();
+      FootstepDataListMessage footstepListMessage;
+      SimulationConstructionSet scs;
+      double t, temp, phaseTime;
+      ArrayList<FootstepDataMessage> footstepList;
+      Point3D fromPoint = new Point3D(), toPoint = new Point3D(), nextToPoint = new Point3D(), supportPoint = new Point3D(), approxCoM = new Point3D(), approxSwingFootLoc = new Point3D();
+      double swFootMass = 5.0;
+      Vector3D swFootVelo = new Vector3D(), swFootVeloIni = new Vector3D(), swFootVeloFi = new Vector3D();      
+      Vector3D swVectorAngMom = new Vector3D();
+      boolean transferPhase;
+      YoPolynomial3D swTraj;
       
       public AngularMomentumSpy(DRCSimulationTestHelper simulationTestHelper)
       {
@@ -140,17 +162,78 @@ public abstract class HumanoidAngularMomentumTest implements MultiRobotTestInter
          floatingRootJointModel = drcSimulationTestHelper.getRobot();
          rootJoint = floatingRootJointModel.getRootJoint();
          comAngularMomentum = new YoFrameVector("CoMAngularMomentum", worldFrame, scsRegistry);
+         comEstimatedAngularMomentum = new YoFrameVector("CoMEstiamtedAngularMomentum", worldFrame, scsRegistry);
+         scs = drcSimulationTestHelper.getSimulationConstructionSet();
+         swTraj = new YoPolynomial3D("SwingFootTraj", 4, scsRegistry);
       }
 
       @Override
       public void doControl()
-      {
+      {         
          floatingRootJointModel.computeCOMMomentum(comPoint, comLinMom, comAngMom);
          rootJoint.getRotationToWorld(rootOrientation);
          rootJointTransform.setRotationAndZeroTranslation(rootOrientation);
          rootJointTransform.inverseTransform(comAngMom);
          comAngularMomentum.set(comAngMom);
-         PrintTools.info(rootOrientation.toString());
+         t = scs.getTime(); temp = 0;
+         int index; 
+         
+         for(index = 0; index < footstepList.size() ; index++)
+         {
+            temp += footstepList.get(index).getTransferDuration();
+            if(temp > t)
+            {
+               phaseTime = footstepList.get(index).getTransferDuration();
+               transferPhase = true;
+               break;
+            }
+            temp += footstepList.get(index).getSwingDuration();
+            if(temp > t)
+            {
+               phaseTime = 1; //should be actial footstepList.get(index).getSwingDuration();
+               transferPhase = false;
+               break;
+            }            
+         }
+         
+         if(index > 1 && index < footstepList.size()-1)
+         {
+            fromPoint = footstepList.get(index-2).getLocation();
+            toPoint = footstepList.get(index).getLocation();
+            nextToPoint = footstepList.get(index + 1).getLocation();
+            supportPoint = footstepList.get(index-1).getLocation();
+            
+            swFootVeloFi.sub(toPoint, fromPoint);
+            swFootVeloFi.scale(-1.0/phaseTime);
+            swFootVeloIni.set(1, 0.0, 0.0);
+            swTraj.setCubic(0.0, phaseTime, fromPoint, swFootVeloIni, toPoint, swFootVeloFi);
+            double tsome = (t - temp + phaseTime)/(phaseTime);
+            swTraj.compute(tsome);
+            approxSwingFootLoc.set(swTraj.getPosition());
+            approxSwingFootLoc.add(0.0, 0.0, 4*tsome*(1.0-tsome));
+
+            if(transferPhase)
+               swFootVelo.setToZero();
+            else
+               swFootVelo.set(swTraj.getVelocity());
+
+            
+            approxCoM.set(approxSwingFootLoc);
+            approxCoM.add(supportPoint);
+            approxCoM.scale(0.5);               
+            approxCoM.add(0.0, 0.0, 0.33);
+            swVectorAngMom.set(approxSwingFootLoc);
+            swVectorAngMom.sub(approxCoM);
+            swVectorAngMom.cross(swVectorAngMom, swFootVelo);
+            swVectorAngMom.scale(swFootMass);
+            comEstimatedAngularMomentum.set(swVectorAngMom);
+         }
+      }
+
+      public void setFootstepList(FootstepDataListMessage footMessage)
+      {
+         this.footstepListMessage = footMessage;
+         this.footstepList = this.footstepListMessage.getDataList();
       }
    }
 }
