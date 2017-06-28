d2ef4024d394f83c8149ba9794aeeef9455206c4
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-06-28 10:46:33 -0500

    Interface for angular momentum planner and abstraction of trajectories defined

---
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/angularMomentumTrajectoryGenerator/AngularMomentumTrajectoryGeneratorInterface.java
@@ -0,0 +1,49 @@
+package us.ihmc.commonWalkingControlModules.angularMomentumTrajectoryGenerator;
+
+import java.util.List;
+
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsList;
+import us.ihmc.graphicsDescription.yoGraphics.plotting.ArtifactList;
+import us.ihmc.humanoidRobotics.footstep.Footstep;
+import us.ihmc.humanoidRobotics.footstep.FootstepTiming;
+import us.ihmc.robotics.geometry.FramePoint;
+import us.ihmc.robotics.geometry.FrameVector;
+import us.ihmc.robotics.math.frames.YoFrameVector;
+import us.ihmc.robotics.robotSide.RobotSide;
+
+public interface AngularMomentumTrajectoryGeneratorInterface 
+{
+   void updateListeners();
+
+   void createVisualizerForConstantAngularMomentum(YoGraphicsList yoGraphicsList, ArtifactList artifactList);
+
+   void clear();
+
+   void addFootstepToPlan(Footstep footstep, FootstepTiming timing);
+
+   int getNumberOfFootstepsRegistered();
+
+   void update(double currentTime);
+
+   void getDesiredAngularMomentum(FramePoint desiredAngMomToPack);
+
+   void getDesiredAngularMomentum(FramePoint desiredAngMomToPack, FrameVector desiredTorqueToPack);
+
+   void getDesiredAngularMomentum(YoFrameVector desiredAngMomToPack);
+
+   void getDesiredAngularMomentum(YoFrameVector desiredAngMomToPack, YoFrameVector desiredTorqueToPack);
+
+   void initializeForTransfer(double currentTime);
+
+   void initializeForSwing(double currentTime);
+
+   void computeReferenceAngularMomentumStartingFromDoubleSupport(boolean atAStop, RobotSide transferToSide);
+
+   void computeReferenceAngularMomentumStartingFromSingleSupport(RobotSide supportSide);
+
+   List<FrameVector> getWaypoints();
+
+   List<? extends AngularMomentumTrajectory> getTransferCoPTrajectories();
+
+   List<? extends AngularMomentumTrajectory> getSwingCoPTrajectories();
+}
