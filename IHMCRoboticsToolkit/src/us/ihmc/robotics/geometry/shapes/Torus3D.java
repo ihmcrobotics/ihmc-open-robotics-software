07fd68522f2d7f616a181602def1aed50fb53559
Sylvain Bertrand <sbertrand@ihmc.us>
2017-06-29 16:33:02 -0500

    Documented torus.

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/geometry/shapes/Torus3D.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/geometry/shapes/Torus3D.java
@@ -11,104 +11,188 @@ import us.ihmc.euclid.tuple3D.interfaces.Vector3DBasics;
 import us.ihmc.robotics.MathTools;
 
 /**
- * 
- * The torus is built in the XY plane, around the z axis
+ * {@code Torus3D} represents a torus in the XY-plane.
+ * <p>
+ * Shape description:
+ * <ul>
+ * <li>The origin is located at the centroid or center of the torus.
+ * <li>The axis of revolution is the z-axis.
+ * <li>The torus is defined by two radii: {@link #tubeRadius} that represents the radius of the
+ * tube, and {@link #radius} that is the radius for the center of the torus to the center of the
+ * tube.
+ * </ul>
+ * </p>
  */
 public class Torus3D extends Shape3D<Torus3D>
 {
+   private static final double MIN_TUBE_RADIUS = 1.0e-4;
+   private static final double MIN_INNER_RADIUS = 1.0e-4;
+
+   /** It is the radius for the center of the torus to the center of the tube. */
    private double radius;
+   /** Represents the radius of the tube */
    private double tubeRadius;
 
-   private static final double SMALLEST_ALLOWABLE_THICKNESS = 5e-3;
-   private static final double SMALLEST_ALLOWABLE_RADIUS_MINUS_THICKNESS = 1e-4;
-
-   public Torus3D(Torus3D torus3d)
-   {
-      setPose(torus3d);
-      this.radius = torus3d.radius;
-      this.tubeRadius = torus3d.tubeRadius;
-
-      checkRadiusAndThickness();
-   }
-
+   /**
+    * Creates a new torus 3D with a radius of {@code 1}, and tube radius of {@code 0.1}.
+    */
    public Torus3D()
    {
-      radius = 1.0;
-      tubeRadius = 0.1;
-
-      checkRadiusAndThickness();
+      this(1.0, 0.1);
    }
 
-   public Torus3D(double radius, double thickness)
+   /**
+    * Creates a new torus 3D identical to {@code other}.
+    * 
+    * @param other the other torus to copy. Not modified.
+    */
+   public Torus3D(Torus3D other)
    {
-      this.radius = radius;
-      this.tubeRadius = thickness;
-
-      checkRadiusAndThickness();
+      set(other);
    }
 
-   public Torus3D(RigidBodyTransform transform, double radius, double thickness)
+   /**
+    * Creates a new torus 3D and initializes its radii.
+    * 
+    * @param radius radius from the torus center to the tube center.
+    * @param tubeRadius radius of the torus' tube.
+    * @throws IllegalArgumentException if {@code tubeRadius} is less than {@value #MIN_TUBE_RADIUS}
+    *            or if the resulting inner radius is less than {@value #MIN_INNER_RADIUS}.
+    */
+   public Torus3D(double radius, double tubeRadius)
    {
-      setPose(transform);
-      this.radius = radius;
-      this.tubeRadius = thickness;
-
-      checkRadiusAndThickness();
+      setRadii(radius, tubeRadius);
    }
 
+   /**
+    * Creates a new torus 3D and initializes its pose and radii.
+    * 
+    * @param pose the position and orientation of this torus. Not modified.
+    * @param radius radius from the torus center to the tube center.
+    * @param tubeRadius radius of the torus' tube.
+    * @throws IllegalArgumentException if {@code tubeRadius} is less than {@value #MIN_TUBE_RADIUS}
+    *            or if the resulting inner radius is less than {@value #MIN_INNER_RADIUS}.
+    */
+   public Torus3D(RigidBodyTransform pose, double radius, double tubeRadius)
+   {
+      set(pose, radius, tubeRadius);
+   }
+
+   /**
+    * Creates a new torus 3D and initializes its pose and radii.
+    * 
+    * @param pose the position and orientation of this torus. Not modified.
+    * @param radius radius from the torus center to the tube center.
+    * @param tubeRadius radius of the torus' tube.
+    * @throws IllegalArgumentException if {@code tubeRadius} is less than {@value #MIN_TUBE_RADIUS}
+    *            or if the resulting inner radius is less than {@value #MIN_INNER_RADIUS}.
+    */
    public Torus3D(Pose3D pose, double radius, double thickness)
    {
-      setPose(pose);
-      this.radius = radius;
-      this.tubeRadius = thickness;
-
-      checkRadiusAndThickness();
+      set(pose, radius, tubeRadius);
    }
 
+   /**
+    * Copies the {@code other} torus data into {@code this}.
+    * 
+    * @param other the other torus to copy. Not modified.
+    */
    @Override
-   public void set(Torus3D torus3d)
+   public void set(Torus3D other)
    {
-      setPose(torus3d);
-      this.radius = torus3d.radius;
-      this.tubeRadius = torus3d.tubeRadius;
+      setPose(other);
+      this.radius = other.radius;
+      this.tubeRadius = other.tubeRadius;
    }
 
-   private void checkRadiusAndThickness()
+   /**
+    * Sets the pose and radii of this torus 3D.
+    * 
+    * @param pose the position and orientation of this torus. Not modified.
+    * @param radius radius from the torus center to the tube center.
+    * @param tubeRadius radius of the torus' tube.
+    * @throws IllegalArgumentException if {@code tubeRadius} is less than {@value #MIN_TUBE_RADIUS}
+    *            or if the resulting inner radius is less than {@value #MIN_INNER_RADIUS}.
+    */
+   public void set(Pose3D pose, double radius, double tubeRadius)
    {
-      if (radius - tubeRadius < SMALLEST_ALLOWABLE_RADIUS_MINUS_THICKNESS)
-         throw new RuntimeException("Torus3d: checkRadiusAndThickness(): "
-               + "Invalid dimensions: Difference between radius and thickness is too small. Enter new dimensions.");
-
-      if (tubeRadius < SMALLEST_ALLOWABLE_THICKNESS)
-         throw new RuntimeException("Torus3d: checkRadiusAndThickness(): " + "Invalid thickness: Torus is too thin. Enter new dimensions.");
+      setPose(pose);
+      setRadii(radius, tubeRadius);
    }
 
+   /**
+    * Sets the pose and radii of this torus 3D.
+    * 
+    * @param pose the position and orientation of this torus. Not modified.
+    * @param radius radius from the torus center to the tube center.
+    * @param tubeRadius radius of the torus' tube.
+    * @throws IllegalArgumentException if {@code tubeRadius} is less than {@value #MIN_TUBE_RADIUS}
+    *            or if the resulting inner radius is less than {@value #MIN_INNER_RADIUS}.
+    */
+   public void set(RigidBodyTransform pose, double radius, double tubeRadius)
+   {
+      setPose(pose);
+      setRadii(radius, tubeRadius);
+   }
+
+   /**
+    * Sets the radii of this torus 3D.
+    * 
+    * @param radius radius from the torus center to the tube center.
+    * @param tubeRadius radius of the torus' tube.
+    * @throws IllegalArgumentException if {@code tubeRadius} is less than {@value #MIN_TUBE_RADIUS}
+    *            or if the resulting inner radius is less than {@value #MIN_INNER_RADIUS}.
+    */
+   public void setRadii(double radius, double tubeRadius)
+   {
+      this.radius = radius;
+      this.tubeRadius = tubeRadius;
+
+      if (radius - tubeRadius < MIN_INNER_RADIUS)
+         throw new IllegalArgumentException("Invalid dimensions: Difference between radius and tube radius is too small, difference: " + (radius - tubeRadius)
+               + ", minimum allowed: " + MIN_INNER_RADIUS);
+
+      if (tubeRadius < MIN_TUBE_RADIUS)
+         throw new IllegalArgumentException("Invalid tube radius: Torus is too thin: " + tubeRadius + ", minimum allowed: " + MIN_TUBE_RADIUS);
+   }
+
+   /**
+    * Gets the radius from the torus center to the tube center.
+    * 
+    * @return this torus main radius.
+    */
    public double getRadius()
    {
       return radius;
    }
 
-   public void setRadius(double radius)
-   {
-      this.radius = radius;
-   }
-
-   public double getThickness()
+   /**
+    * Gets the radius of the tube of this torus.
+    * 
+    * @return the radius of the tube.
+    */
+   public double getTubeRadius()
    {
       return tubeRadius;
    }
 
-   public void setThickness(double thickness)
-   {
-      this.tubeRadius = thickness;
-   }
-
+   /**
+    * Tests separately and on a per component basis if the pose and the radii of this torus and
+    * {@code other}'s pose and radii are equal to an {@code epsilon}.
+    * 
+    * @param other the other torus which pose and radii is to be compared against this torus pose
+    *           and radii. Not modified.
+    * @param epsilon tolerance to use when comparing each component.
+    * @return {@code true} if the two toruses are equal component-wise, {@code false} otherwise.
+    */
    @Override
    public boolean epsilonEquals(Torus3D other, double epsilon)
    {
-      return MathTools.epsilonEquals(radius, other.radius, epsilon) && MathTools.epsilonEquals(tubeRadius, other.tubeRadius, epsilon);
+      return MathTools.epsilonEquals(radius, other.radius, epsilon) && MathTools.epsilonEquals(tubeRadius, other.tubeRadius, epsilon)
+            && super.epsilonEqualsPose(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setToZero()
    {
@@ -117,6 +201,7 @@ public class Torus3D extends Shape3D<Torus3D>
       tubeRadius = 0.0;
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setToNaN()
    {
@@ -125,18 +210,29 @@ public class Torus3D extends Shape3D<Torus3D>
       tubeRadius = Double.NaN;
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean containsNaN()
    {
       return super.containsNaN() || Double.isNaN(radius) || Double.isNaN(tubeRadius);
    }
 
+   /**
+    * Provides a {@code String} representation of this torus 3D as follows:<br>
+    * Torus 3D: radius = R, tube radius = r, pose = <br>
+    * m00, m01, m02 | m03 <br>
+    * m10, m11, m12 | m13 <br>
+    * m20, m21, m22 | m23
+    *
+    * @return the {@code String} representing this torus 3D.
+    */
    @Override
    public String toString()
    {
-      return "radius = " + radius + ", thickness = " + tubeRadius + ", pose = " + getPoseString() + "\n";
+      return "Torus: radius = " + radius + ", tube radius = " + tubeRadius + ", pose =\n" + getPoseString();
    }
 
+   /** {@inheritDoc} */
    @Override
    protected boolean isInsideEpsilonShapeFrame(double x, double y, double z, double epsilon)
    {
@@ -156,6 +252,7 @@ public class Torus3D extends Shape3D<Torus3D>
       return EuclidGeometryTools.distanceBetweenPoint3Ds(x, y, z, closestTubeCenterX, closestTubeCenterY, 0.0) <= tubeRadius + epsilon;
    }
 
+   /** {@inheritDoc} */
    @Override
    protected double evaluateQuery(double x, double y, double z, Point3DBasics closestPointToPack, Vector3DBasics normalToPack)
    {
