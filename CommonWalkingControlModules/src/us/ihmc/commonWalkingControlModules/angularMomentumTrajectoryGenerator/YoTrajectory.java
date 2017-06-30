1bfc6cbd424a6f085980ed289887ab1e5f7f871c
Tim Seyde <tseyde@ihmc.us>
2017-06-30 10:08:29 -0500

    Workaround for NaN times being set by YoTrajectory.reset() in ICP initializations for Swing/Transfer; needs structural change for fix

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/angularMomentumTrajectoryGenerator/YoTrajectory.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/angularMomentumTrajectoryGenerator/YoTrajectory.java
@@ -78,8 +78,8 @@ public class YoTrajectory
    public void reset()
    {
       polynomial.reset();
-      tInitial.setToNaN();
-      tFinal.setToNaN();
+      tInitial.set(0.0);
+      tFinal.set(0.0);
    }
 
    public double getCoefficient(int i)
@@ -343,14 +343,15 @@ public class YoTrajectory
 
    public void compute(double x)
    {
-      if (x >= tInitial.getDoubleValue() && x <= tFinal.getDoubleValue())
+      //FIXME if (x >= tInitial.getDoubleValue() && x <= tFinal.getDoubleValue())
          polynomial.compute(x);
    }
 
    public double getIntegral(double from, double to)
    {
-      if (from < tInitial.getDoubleValue() || to > tFinal.getDoubleValue())
-         return Double.NaN;
+      // FIXME
+      //if (from < tInitial.getDoubleValue() || to > tFinal.getDoubleValue())
+      //   return Double.NaN;
 
       return polynomial.getIntegral(from, to);
    }
