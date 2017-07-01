2561b49fe777d7a404698016005c7504cd395443
Sylvain Bertrand <sbertrand@ihmc.us>
2017-07-01 16:04:26 -0500

    Documented ReferenceFrameMismatchException.

--- IHMCRoboticsToolkit/src/us/ihmc/robotics/geometry/ReferenceFrameMismatchException.java
+++ IHMCRoboticsToolkit/src/us/ihmc/robotics/geometry/ReferenceFrameMismatchException.java
@@ -1,9 +1,18 @@
 package us.ihmc.robotics.geometry;
 
+/**
+ * {@code ReferenceFrameMismatchException} is a {@code RuntimeException} thrown when attempting a
+ * forbidden operation because of mismatching {@code ReferenceFrame}s.
+ */
 public class ReferenceFrameMismatchException extends RuntimeException
 {
    private static final long serialVersionUID = -2379052247493923182L;
 
+   /**
+    * Creates a new exception with a specified message.
+    * 
+    * @param message the message detailing the cause of this exception.
+    */
    public ReferenceFrameMismatchException(String message)
    {
       super(message);
