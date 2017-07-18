2eb07b77abd0935bbea81d71808b60ca18a60007
InhoLee <ilee@ihmc.us>
2017-07-18 12:23:23 -0500

    Packet added on NetworkProcessor.

--- IHMCAvatarInterfaces/src/us/ihmc/avatar/networkProcessor/rrtToolboxModule/RRTPlanningToolboxController.java
+++ IHMCAvatarInterfaces/src/us/ihmc/avatar/networkProcessor/rrtToolboxModule/RRTPlanningToolboxController.java
@@ -1,13 +1,17 @@
 package us.ihmc.avatar.networkProcessor.rrtToolboxModule;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import us.ihmc.avatar.networkProcessor.modules.ToolboxController;
 import us.ihmc.communication.controllerAPI.StatusMessageOutputManager;
 import us.ihmc.communication.net.PacketConsumer;
+import us.ihmc.humanoidRobotics.communication.packets.manipulation.RRTPlanningRequestPacket;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
 public class RRTPlanningToolboxController extends ToolboxController
 {
-
+   private final AtomicReference<RRTPlanningRequestPacket> latestRequestReference = new AtomicReference<RRTPlanningRequestPacket>(null);
+   
    public RRTPlanningToolboxController(StatusMessageOutputManager statusOutputManager, YoVariableRegistry parentRegistry)
    {
       super(statusOutputManager, parentRegistry);
@@ -24,21 +28,32 @@ public class RRTPlanningToolboxController extends ToolboxController
    @Override
    protected boolean initialize()
    {
-      // TODO Auto-generated method stub
-      return false;
+      RRTPlanningRequestPacket request = latestRequestReference.getAndSet(null);
+      if (request == null)
+         return false;
+      
+      
+      return true;
    }
 
    @Override
    protected boolean isDone()
    {
-      // TODO Auto-generated method stub
       return false;
    }
 
-   public PacketConsumer createRequestConsumer()
+   public PacketConsumer<RRTPlanningRequestPacket> createRequestConsumer()
    {
-      // TODO Auto-generated method stub
-      return null;
+      return new PacketConsumer<RRTPlanningRequestPacket>()
+      {
+         @Override
+         public void receivedPacket(RRTPlanningRequestPacket packet)
+         {
+            if (packet == null)
+               return;
+            latestRequestReference.set(packet);
+         }
+      };
    }
 
 }
