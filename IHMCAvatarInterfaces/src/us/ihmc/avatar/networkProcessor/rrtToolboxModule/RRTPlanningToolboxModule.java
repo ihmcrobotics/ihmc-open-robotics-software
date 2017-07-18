8d6f5289b4466f98ed736a40943320f1673c99b1
InhoLee <ilee@ihmc.us>
2017-07-18 11:54:34 -0500

    RRT Planning Toolbox is created.

---
+++ IHMCAvatarInterfaces/src/us/ihmc/avatar/networkProcessor/rrtToolboxModule/RRTPlanningToolboxModule.java
@@ -0,0 +1,58 @@
+package us.ihmc.avatar.networkProcessor.rrtToolboxModule;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.networkProcessor.modules.ToolboxController;
+import us.ihmc.avatar.networkProcessor.modules.ToolboxModule;
+import us.ihmc.communication.controllerAPI.command.Command;
+import us.ihmc.communication.packets.PacketDestination;
+import us.ihmc.communication.packets.StatusPacket;
+import us.ihmc.communication.util.NetworkPorts;
+import us.ihmc.humanoidRobotics.communication.packets.manipulation.RRTPlanningRequestPacket;
+import us.ihmc.humanoidRobotics.communication.packets.manipulation.RRTPlanningToolboxOutputStatus;
+import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
+import us.ihmc.robotModels.FullHumanoidRobotModel;
+
+public class RRTPlanningToolboxModule extends ToolboxModule
+{
+   private static final PacketDestination PACKET_DESTINATION = PacketDestination.RRTPLANNING_TOOLBOX_MODULE;
+   private static final NetworkPorts NETWORK_PORT = NetworkPorts.RRTPLANNING_TOOLBOX_MODULE_PORT;
+
+   private final RRTPlanningToolboxController rrtToolboxController;
+   
+   public RRTPlanningToolboxModule(DRCRobotModel drcRobotModel, FullHumanoidRobotModel fullHumanoidRobotModel, LogModelProvider modelProvider,
+                           boolean startYoVariableServer)
+         throws IOException
+   {
+      super(fullHumanoidRobotModel, modelProvider, startYoVariableServer, PACKET_DESTINATION, NETWORK_PORT);
+      setTimeWithoutInputsBeforeGoingToSleep(Double.POSITIVE_INFINITY);
+      rrtToolboxController = new RRTPlanningToolboxController(statusOutputManager, registry);
+      packetCommunicator.attachListener(RRTPlanningRequestPacket.class, rrtToolboxController.createRequestConsumer());
+      startYoVariableServer();
+   }
+
+   @Override
+   public ToolboxController getToolboxController()
+   {
+      return rrtToolboxController;
+   }
+
+   @Override
+   public List<Class<? extends Command<?, ?>>> createListOfSupportedCommands()
+   {
+      List<Class<? extends Command<?, ?>>> commands = new ArrayList<>();
+      return commands;
+   }
+
+   @Override
+   public List<Class<? extends StatusPacket<?>>> createListOfSupportedStatus()
+   {
+      List<Class<? extends StatusPacket<?>>> status = new ArrayList<>();
+      status.add(RRTPlanningToolboxOutputStatus.class);
+      return status;
+   }
+
+}
