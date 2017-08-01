7cc10e584563d711bc84d1cb52f4162b3cf7a2bd
Robert <rgriffin@ihmc.us>
2017-07-31 22:11:28 -0500

    reverted a lot of the parameters in favor of the previous ones that worked

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/walkingController/states/WalkingSingleSupportState.java
+++ CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/walkingController/states/WalkingSingleSupportState.java
@@ -127,7 +127,6 @@ public class WalkingSingleSupportState extends SingleSupportState
 
             balanceManager.updateCurrentICPPlan();
             legConfigurationManager.prepareForLegBracing(swingSide);
-            legConfigurationManager.useLowWeight(swingSide);
          }
       }
       else if (balanceManager.isPushRecoveryEnabled())
