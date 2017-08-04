9f2b0c73d5f0eaaec61e89d10972f272b34c29f6
Stephen McCrory <smccrory@ihmc.us>
2017-08-03 19:29:17 -0500

    adding in foothold area check to snapper

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/SimplePlanarRegionFootstepNodeSnapper.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/SimplePlanarRegionFootstepNodeSnapper.java
@@ -5,12 +5,16 @@ import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.footstepPlanning.aStar.FootstepNode;
 import us.ihmc.footstepPlanning.aStar.FootstepNodeSnapper;
 import us.ihmc.footstepPlanning.polygonSnapping.PlanarRegionsListPolygonSnapper;
+import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.robotSide.SideDependentList;
+
+import java.util.ArrayList;
 
 public class SimplePlanarRegionFootstepNodeSnapper implements FootstepNodeSnapper
 {
    private PlanarRegionsList planarRegionsList = null;
+   private static final double minPercentageOfFoothold = 0.95;
 
    private final SideDependentList<ConvexPolygon2D> footPolygons;
    private final RigidBodyTransform footstepPose = new RigidBodyTransform();
@@ -32,18 +36,25 @@ public class SimplePlanarRegionFootstepNodeSnapper implements FootstepNodeSnappe
       if (planarRegionsList == null)
          return false;
 
-      footstepPose.setRotationYawAndZeroTranslation(footstepNode.getYaw());
-      footstepPose.setTranslationX(footstepNode.getX());
-      footstepPose.setTranslationY(footstepNode.getY());
+      PlanarRegion region = new PlanarRegion();
 
       ConvexPolygon2D footPolygon = new ConvexPolygon2D(footPolygons.get(footstepNode.getRobotSide()));
-      RigidBodyTransform worldToSole = new RigidBodyTransform();
-      worldToSole.setRotationYawAndZeroTranslation(footstepNode.getYaw());
-      worldToSole.setTranslation(footstepNode.getX(), footstepNode.getY(), 0.0);
-      footPolygon.applyTransformAndProjectToXYPlane(worldToSole);
-      RigidBodyTransform snapTransform = PlanarRegionsListPolygonSnapper.snapPolygonToPlanarRegionsList(footPolygon, planarRegionsList);
+      footstepPose.setRotationYawAndZeroTranslation(footstepNode.getYaw());
+      footstepPose.setTranslation(footstepNode.getX(), footstepNode.getY(), 0.0);
+      footPolygon.applyTransformAndProjectToXYPlane(footstepPose);
+      RigidBodyTransform snapTransform = PlanarRegionsListPolygonSnapper.snapPolygonToPlanarRegionsList(footPolygon, planarRegionsList, region);
 
-      if(snapTransform == null)
+      if (snapTransform == null)
+         return false;
+
+      ArrayList<ConvexPolygon2D> intersections = new ArrayList<>();
+      region.getPolygonIntersectionsWhenProjectedVertically(footPolygon, intersections);
+      if (intersections.size() != 1)
+         return false;
+
+      double area = intersections.get(0).getArea();
+      double footArea = footPolygon.getArea();
+      if (area < minPercentageOfFoothold * footArea)
          return false;
 
       snapTransform.transform(footstepPose);
