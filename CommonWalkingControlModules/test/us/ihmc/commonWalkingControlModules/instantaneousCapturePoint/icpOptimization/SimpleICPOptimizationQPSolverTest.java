153f290213d211eaab673d2862a14d00a0fc48d1
Robert <rgriffin@ihmc.us>
2017-08-05 14:03:04 -0500

    continued expanding the tests

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/SimpleICPOptimizationQPSolverTest.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/SimpleICPOptimizationQPSolverTest.java
@@ -1,5 +1,8 @@
 package us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.icpOptimization;
 
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.factory.LinearSolverFactory;
+import org.ejml.interfaces.linsol.LinearSolver;
 import org.jcodec.common.Assert;
 import org.junit.Test;
 import us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.icpOptimization.simpleController.SimpleICPOptimizationQPSolver;
@@ -276,6 +279,248 @@ public class SimpleICPOptimizationQPSolverTest
       footstepLocationExpected.set(desiredFootstepLocation);
       icpError.scale(1.0 / footstepMultiplier);
       footstepLocationExpected.add(icpError);
+
+      Assert.assertTrue(copFeedback.eplilonEquals(copFeedbackExpected, epsilon));
+      Assert.assertTrue(cmpCoPDifference.eplilonEquals(cmpCoPDifferenceExpected, epsilon));
+      Assert.assertTrue(footstepLocation.epsilonEquals(footstepLocationExpected, epsilon));
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 1.0)
+   @Test(timeout = 21000)
+   public void testSteppingUnconstrainedFootstepAdjustmentPreferredWithAngularMomentum() throws Exception
+   {
+      SimpleICPOptimizationQPSolver solver = new SimpleICPOptimizationQPSolver(0.0, 0.0, 10, false);
+      solver.setMaxNumberOfIterations(10);
+
+      double feedbackWeight = 1000.0;
+      double feedbackGain = 3.0;
+      double footstepWeight = 0.1;
+
+      solver.setFeedbackConditions(feedbackWeight, feedbackGain, 10000.0);
+      solver.setAngularMomentumConditions(10.0 * feedbackWeight, true);
+
+      double timeRemainingInState = 1.0;
+      double omega = 1.0;
+      double footstepMultiplier = Math.exp(-omega * timeRemainingInState);
+      FramePoint2d desiredFootstepLocation = new FramePoint2d(worldFrame, 0.3, 0.1);
+      solver.setFootstepAdjustmentConditions(footstepMultiplier, footstepWeight, desiredFootstepLocation);
+
+      FrameVector2d icpError = new FrameVector2d(worldFrame, 0.03, 0.04);
+      FramePoint2d perfectCMP = new FramePoint2d(worldFrame, 0.02, 0.01);
+
+      solver.compute(icpError, perfectCMP);
+
+      FrameVector2d cmpCoPDifference = new FrameVector2d();
+      FrameVector2d copFeedback = new FrameVector2d();
+      FramePoint2d footstepLocation = new FramePoint2d();
+
+      solver.getCMPDifferenceFromCoP(cmpCoPDifference);
+      solver.getCoPFeedbackDifference(copFeedback);
+      solver.getFootstepSolutionLocation(0, footstepLocation);
+
+      FrameVector2d cmpCoPDifferenceExpected = new FrameVector2d();
+      FrameVector2d copFeedbackExpected = new FrameVector2d();
+      FramePoint2d footstepLocationExpected = new FramePoint2d();
+
+      footstepLocationExpected.set(desiredFootstepLocation);
+      icpError.scale(1.0 / footstepMultiplier);
+      footstepLocationExpected.add(icpError);
+
+      Assert.assertTrue(copFeedback.eplilonEquals(copFeedbackExpected, epsilon));
+      Assert.assertTrue(cmpCoPDifference.eplilonEquals(cmpCoPDifferenceExpected, epsilon));
+      Assert.assertTrue(footstepLocation.epsilonEquals(footstepLocationExpected, epsilon));
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 1.0)
+   @Test(timeout = 21000)
+   public void testSteppingUnconstrainedWithAdjustment() throws Exception
+   {
+      SimpleICPOptimizationQPSolver solver = new SimpleICPOptimizationQPSolver(0.0, 0.0, 10, false);
+      solver.setMaxNumberOfIterations(10);
+
+      double feedbackWeight = 1.0;
+      double feedbackGain = 3.0;
+      double footstepWeight = 1.0;
+
+      solver.setFeedbackConditions(feedbackWeight, feedbackGain, 10000.0);
+
+      double timeRemainingInState = 1.0;
+      double omega = 1.0;
+      double footstepMultiplier = Math.exp(-omega * timeRemainingInState);
+      FramePoint2d desiredFootstepLocation = new FramePoint2d(worldFrame, 0.3, 0.1);
+      solver.setFootstepAdjustmentConditions(footstepMultiplier, footstepWeight, desiredFootstepLocation);
+
+      FrameVector2d icpError = new FrameVector2d(worldFrame, 0.06, 0.10);
+      FramePoint2d perfectCMP = new FramePoint2d(worldFrame, 0.02, 0.01);
+
+      solver.compute(icpError, perfectCMP);
+
+      FrameVector2d cmpCoPDifference = new FrameVector2d();
+      FrameVector2d copFeedback = new FrameVector2d();
+      FramePoint2d footstepLocation = new FramePoint2d();
+
+      solver.getCMPDifferenceFromCoP(cmpCoPDifference);
+      solver.getCoPFeedbackDifference(copFeedback);
+      solver.getFootstepSolutionLocation(0, footstepLocation);
+
+      FrameVector2d cmpCoPDifferenceExpected = new FrameVector2d();
+      FrameVector2d copFeedbackExpected = new FrameVector2d();
+      FramePoint2d footstepLocationExpected = new FramePoint2d();
+
+      LinearSolver<DenseMatrix64F> simpleSolver = LinearSolverFactory.linear(6);
+      DenseMatrix64F quadratic = new DenseMatrix64F(6, 6);
+      DenseMatrix64F equals = new DenseMatrix64F(6, 1);
+      DenseMatrix64F solution = new DenseMatrix64F(6, 1);
+      quadratic.set(0, 0, feedbackWeight);
+      quadratic.set(1, 1, feedbackWeight);
+      quadratic.set(0, 4, 1.0);
+      quadratic.set(1, 5, 1.0);
+      quadratic.set(2, 2, footstepWeight);
+      quadratic.set(3, 3, footstepWeight);
+      quadratic.set(2, 4, feedbackGain * footstepMultiplier);
+      quadratic.set(3, 5, feedbackGain * footstepMultiplier);
+      quadratic.set(4, 0, 1.0);
+      quadratic.set(5, 1, 1.0);
+      quadratic.set(4, 2, feedbackGain * footstepMultiplier);
+      quadratic.set(5, 3, feedbackGain * footstepMultiplier);
+
+      equals.set(4, 0, feedbackGain * icpError.getX());
+      equals.set(5, 0, feedbackGain * icpError.getY());
+
+      simpleSolver.setA(quadratic);
+      simpleSolver.solve(equals, solution);
+
+      copFeedbackExpected.setX(solution.get(0));
+      copFeedbackExpected.setY(solution.get(1));
+
+      footstepLocationExpected.setX(desiredFootstepLocation.getX() + solution.get(2));
+      footstepLocationExpected.setY(desiredFootstepLocation.getY() + solution.get(3));
+
+
+      Assert.assertTrue(copFeedback.eplilonEquals(copFeedbackExpected, epsilon));
+      Assert.assertTrue(cmpCoPDifference.eplilonEquals(cmpCoPDifferenceExpected, epsilon));
+      Assert.assertTrue(footstepLocation.epsilonEquals(footstepLocationExpected, epsilon));
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 1.0)
+   @Test(timeout = 21000)
+   public void testSteppingUnconstrainedWithAdjustmentAndAngularMomentum() throws Exception
+   {
+      SimpleICPOptimizationQPSolver solver = new SimpleICPOptimizationQPSolver(0.0, 0.0, 10, false);
+      solver.setMaxNumberOfIterations(10);
+
+      double feedbackWeight = 1.0;
+      double feedbackGain = 3.0;
+      double footstepWeight = 0.1;
+
+      solver.setFeedbackConditions(feedbackWeight, feedbackGain, 10000.0);
+      solver.setAngularMomentumConditions(100.0, true);
+
+      double timeRemainingInState = 1.0;
+      double omega = 1.0;
+      double footstepMultiplier = Math.exp(-omega * timeRemainingInState);
+      FramePoint2d desiredFootstepLocation = new FramePoint2d(worldFrame, 0.3, 0.1);
+      solver.setFootstepAdjustmentConditions(footstepMultiplier, footstepWeight, desiredFootstepLocation);
+
+      FrameVector2d icpError = new FrameVector2d(worldFrame, 0.06, 0.10);
+      FramePoint2d perfectCMP = new FramePoint2d(worldFrame, 0.02, 0.01);
+
+      solver.compute(icpError, perfectCMP);
+
+      FrameVector2d cmpCoPDifference = new FrameVector2d();
+      FrameVector2d copFeedback = new FrameVector2d();
+      FramePoint2d footstepLocation = new FramePoint2d();
+
+      solver.getCMPDifferenceFromCoP(cmpCoPDifference);
+      solver.getCoPFeedbackDifference(copFeedback);
+      solver.getFootstepSolutionLocation(0, footstepLocation);
+
+      FrameVector2d cmpCoPDifferenceExpected = new FrameVector2d();
+      FrameVector2d copFeedbackExpected = new FrameVector2d();
+      FramePoint2d footstepLocationExpected = new FramePoint2d();
+
+      LinearSolver<DenseMatrix64F> simpleSolver = LinearSolverFactory.linear(6);
+      DenseMatrix64F quadratic = new DenseMatrix64F(6, 6);
+      DenseMatrix64F equals = new DenseMatrix64F(6, 1);
+      DenseMatrix64F solution = new DenseMatrix64F(6, 1);
+      quadratic.set(0, 0, feedbackWeight);
+      quadratic.set(1, 1, feedbackWeight);
+      quadratic.set(0, 4, 1.0);
+      quadratic.set(1, 5, 1.0);
+      quadratic.set(2, 2, footstepWeight);
+      quadratic.set(3, 3, footstepWeight);
+      quadratic.set(2, 4, feedbackGain * footstepMultiplier);
+      quadratic.set(3, 5, feedbackGain * footstepMultiplier);
+      quadratic.set(4, 0, 1.0);
+      quadratic.set(5, 1, 1.0);
+      quadratic.set(4, 2, feedbackGain * footstepMultiplier);
+      quadratic.set(5, 3, feedbackGain * footstepMultiplier);
+
+      equals.set(4, 0, feedbackGain * icpError.getX());
+      equals.set(5, 0, feedbackGain * icpError.getY());
+
+      simpleSolver.setA(quadratic);
+      simpleSolver.solve(equals, solution);
+
+      copFeedbackExpected.setX(solution.get(0));
+      copFeedbackExpected.setY(solution.get(1));
+
+      footstepLocationExpected.setX(desiredFootstepLocation.getX() + solution.get(2));
+      footstepLocationExpected.setY(desiredFootstepLocation.getY() + solution.get(3));
+
+      Assert.assertTrue(copFeedback.eplilonEquals(copFeedbackExpected, epsilon));
+      Assert.assertTrue(cmpCoPDifference.eplilonEquals(cmpCoPDifferenceExpected, epsilon));
+      Assert.assertTrue(footstepLocation.epsilonEquals(footstepLocationExpected, epsilon));
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 1.0)
+   @Test(timeout = 21000)
+   public void testSteppingConstrainedWithAdjustment() throws Exception
+   {
+      SimpleICPOptimizationQPSolver solver = new SimpleICPOptimizationQPSolver(0.0, 0.0, 10, false);
+      solver.setMaxNumberOfIterations(10);
+
+      double feedbackWeight = 0.1;
+      double feedbackGain = 3.0;
+      double footstepWeight = 50.0;
+
+      solver.setFeedbackConditions(feedbackWeight, feedbackGain, 100000.0);
+
+      double sideLength = 0.1;
+      FrameConvexPolygon2d supportPolygon = createSupportPolygon(sideLength);
+      solver.addSupportPolygon(supportPolygon);
+
+      double timeRemainingInState = 1.0;
+      double omega = 1.0;
+      double footstepMultiplier = Math.exp(-omega * timeRemainingInState);
+      FramePoint2d desiredFootstepLocation = new FramePoint2d(worldFrame, 0.3, 0.1);
+      solver.setFootstepAdjustmentConditions(footstepMultiplier, footstepWeight, desiredFootstepLocation);
+
+      FrameVector2d icpError = new FrameVector2d(worldFrame, 0.04, 0.06);
+      FramePoint2d perfectCMP = new FramePoint2d(worldFrame, 0.02, 0.04);
+
+      solver.compute(icpError, perfectCMP);
+
+      FrameVector2d cmpCoPDifference = new FrameVector2d();
+      FrameVector2d copFeedback = new FrameVector2d();
+      FramePoint2d footstepLocation = new FramePoint2d();
+
+      solver.getCMPDifferenceFromCoP(cmpCoPDifference);
+      solver.getCoPFeedbackDifference(copFeedback);
+      solver.getFootstepSolutionLocation(0, footstepLocation);
+
+      FrameVector2d cmpCoPDifferenceExpected = new FrameVector2d();
+      FrameVector2d copFeedbackExpected = new FrameVector2d();
+      FramePoint2d footstepLocationExpected = new FramePoint2d();
+
+      copFeedbackExpected.set(0.08, 0.06);
+
+      footstepLocationExpected.set(icpError);
+      footstepLocationExpected.scale(feedbackGain);
+      footstepLocationExpected.sub(copFeedbackExpected);
+      footstepLocationExpected.scale(1.0 / (feedbackGain * footstepMultiplier));
+
+      footstepLocationExpected.add(desiredFootstepLocation);
 
       Assert.assertTrue(copFeedback.eplilonEquals(copFeedbackExpected, epsilon));
       Assert.assertTrue(cmpCoPDifference.eplilonEquals(cmpCoPDifferenceExpected, epsilon));
