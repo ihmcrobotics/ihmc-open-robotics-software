ff86dd9887dacf8a21245caa7734ef917cdf5c9f
Robert <rgriffin@ihmc.us>
2017-08-06 09:06:17 -0500

    setup the capture point planner to be able to not update the upcoming reference cmp locations and only those in the current foot so that reference ICP does not change with footstep adjustment

--- CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/ICPAdjustmentOptimizationControllerTest.java
+++ CommonWalkingControlModules/test/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/ICPAdjustmentOptimizationControllerTest.java
@@ -104,7 +104,7 @@ public class ICPAdjustmentOptimizationControllerTest
       icpPlanner.initializeForSingleSupport(0.0);
       icpOptimizationController.initializeForSingleSupport(0.0, supportSide, omega0);
 
-      icpPlanner.updateCurrentPlan();
+      icpPlanner.updateCurrentPlan(true);
       double currentTime = 0.5;
       FramePoint2d desiredICP = new FramePoint2d();
       FrameVector2d desiredICPVelocity = new FrameVector2d();
