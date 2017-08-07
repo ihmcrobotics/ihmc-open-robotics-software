c24754bc40c701b95a811bee1f5e7c79d00394c2
Duncan Calvert <dcalvert@ihmc.us>
2017-08-07 16:50:58 -0500

    Copy tests for now.

---
+++ IHMCRobotDataLogger/test/src/us/ihmc/robotDataCommunication/RobotDataCommunicationBambooTestSuite.java
@@ -0,0 +1,17 @@
+package us.ihmc.robotDataCommunication;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+//    us.ihmc.robotDataLogger.YoVariableConnectionBurstTest.class
+})
+
+public class RobotDataCommunicationBambooTestSuite
+{
+   public static void main(String[] args)
+   {
+   }
+}
