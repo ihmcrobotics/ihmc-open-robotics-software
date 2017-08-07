f43c40fb37cac0797b0fdd2a9915893b504aae8e
Stephen McCrory <smccrory@ihmc.us>
2017-08-07 16:45:21 -0500

    fixing bug in flat ground snapper

--- IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/FlatGroundFootstepNodeSnapper.java
+++ IHMCFootstepPlanning/src/us/ihmc/footstepPlanning/aStar/implementations/FlatGroundFootstepNodeSnapper.java
@@ -1,11 +1,9 @@
 package us.ihmc.footstepPlanning.aStar.implementations;
 
-import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.footstepPlanning.aStar.FootstepNode;
 import us.ihmc.footstepPlanning.aStar.FootstepNodeSnapper;
-import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
@@ -26,10 +24,8 @@ public class FlatGroundFootstepNodeSnapper implements FootstepNodeSnapper
    @Override
    public RigidBodyTransform snapFootstepNode(FootstepNode footstepNode, ConvexPolygon2D footholdIntersectionToPack)
    {
-      RigidBodyTransform soleTransform = new RigidBodyTransform();
-      soleTransform.setTranslation(footstepNode.getX(), footstepNode.getY(), 0.0);
-      soleTransform.setRotation(new AxisAngle(0.0, 0.0, 1.0, footstepNode.getYaw()));
-      footholdIntersectionToPack.set(footPolygons.get(footstepNode.getRobotSide()));
-      return soleTransform;
+      if(footholdIntersectionToPack != null)
+         footholdIntersectionToPack.set(footPolygons.get(footstepNode.getRobotSide()));
+      return new RigidBodyTransform();
    }
 }
