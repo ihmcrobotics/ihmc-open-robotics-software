ac20be3f6d84f0f36014ade3051311d42c03148b
Duncan Calvert <dcalvert@ihmc.us>
2017-08-08 10:20:20 -0500

    Remove copied tests from VCS.

--- IHMCFootstepPlanning/test/src/us/ihmc/footstepPlanning/testTools/PlanningTest.java
+++
@@ -1,9 +0,0 @@
-package us.ihmc.footstepPlanning.testTools;
-
-import us.ihmc.footstepPlanning.FootstepPlanner;
-
-public interface PlanningTest
-{
-   abstract public FootstepPlanner getPlanner();
-   abstract public boolean visualize();
-}
