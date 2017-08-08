95abb254d63e40fa8557305ef88361068b76637d
Duncan Calvert <dcalvert@ihmc.us>
2017-08-08 10:33:09 -0500

    Remove copied tests from VCS.

--- SimulationConstructionSetTools/test/src/us/ihmc/simulationConstructionSetTools/simulationTesting/SimulationComparer.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.simulationConstructionSetTools.simulationTesting;
-
-import us.ihmc.simulationconstructionset.SimulationConstructionSet;
-
-public interface SimulationComparer
-{
-   public abstract boolean compare(SimulationConstructionSet scs0, SimulationConstructionSet scs1);
-}
--- SimulationConstructionSetTools/test/src/us/ihmc/simulationConstructionSetTools/simulationTesting/SimulationComparisonScript.java
+++
@@ -1,9 +0,0 @@
-package us.ihmc.simulationConstructionSetTools.simulationTesting;
-
-import us.ihmc.simulationconstructionset.SimulationConstructionSet;
-
-public interface SimulationComparisonScript
-{
-   public abstract void doInitialAction(SimulationConstructionSet scs0, SimulationConstructionSet scs1);
-   public abstract void doFinalAction(SimulationConstructionSet scs0, SimulationConstructionSet scs1);
-}
