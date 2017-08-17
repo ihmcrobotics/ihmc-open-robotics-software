117d1b3f02c4f8da1f81912fb6f3efa6f046f796
Jesper Smith <jsmith@ihmc.us>
2017-08-17 10:56:02 -0500

    Cleanup, generate MLInt64 for Boolean values by default instead of MLInt32 and converting later

--- IHMCRobotDataVisualizer/src/us/ihmc/robotDataVisualizer/logger/YoVariableExporter.java
+++ IHMCRobotDataVisualizer/src/us/ihmc/robotDataVisualizer/logger/YoVariableExporter.java
@@ -121,7 +121,7 @@ public class YoVariableExporter extends YoVariableLogReader
             
             timestamp.setReal(entryTimestamp, i - startPosition);
             robotTime.setReal(Conversions.nanosecondsToSeconds(entryTimestamp - firstTimestamp), i - startPosition);
-
+            
             for (int dh = 0; dh < dataHolders.size(); dh++)
             {
                DataHolder<?> dataHolder = dataHolders.get(dh);
@@ -136,26 +136,7 @@ public class YoVariableExporter extends YoVariableLogReader
          matlabData.add(robotTime);
          for (int dh = 0; dh < dataHolders.size(); dh++)
          {
-            if (dataHolders.get(dh).getData() instanceof MLInt32)
-            {
-               MLInt32 tempdata = (MLInt32) dataHolders.get(dh).getData();
-               MLInt64 newData = new MLInt64(tempdata.getName(), tempdata.getDimensions());
-               int m = tempdata.getM();
-               int n = tempdata.getN();
-               for (int i = 0; i < m; i++)
-               {
-                  for (int j = 0; j < n; j++)
-                  {
-                     Long value = new Long(tempdata.get(i, j));
-                     newData.set(value, i, j);
-                  }
-               }
-               matlabData.add(newData);
-            }
-            else
-            {
-               matlabData.add(dataHolders.get(dh).getData());
-            }
+            matlabData.add(dataHolders.get(dh).getData());
          }
          
          new MatFileWriter(file, matlabData);
@@ -227,13 +208,13 @@ public class YoVariableExporter extends YoVariableLogReader
       }
       else if (variable instanceof YoBoolean)
       {
-         return new DataHolder<Integer>(offset, new MLInt32(name, dims))
+         return new DataHolder<Long>(offset, new MLInt64(name, dims))
          {
 
             @Override
             public void set(long entryAsLong)
             {
-               set((Integer) (entryAsLong == 1L ? 1 : 0));
+               set((Long) (entryAsLong == 0L ? 0L : 1L));   // Force true to equal 1L in all cases
             }
 
          };
