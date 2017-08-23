8e8c016b6e0f1e44d0d2a3d70b55a6380325aa75
Jesper Smith <jsmith@ihmc.us>
2017-08-23 15:22:01 -0500

    Added menu items to save and load parameters

---
+++ SimulationConstructionSet/src/us/ihmc/simulationconstructionset/gui/dialogConstructors/LoadParametersGenerator.java
@@ -0,0 +1,26 @@
+package us.ihmc.simulationconstructionset.gui.dialogConstructors;
+
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+
+public class LoadParametersGenerator implements LoadParametersConstructor
+{
+
+   public LoadParametersGenerator(SimulationConstructionSet scs)
+   {
+      // TODO Auto-generated constructor stub
+   }
+   
+   @Override
+   public void constructDialog()
+   {
+      System.out.println("Constructing Load dialog");
+   }
+
+   @Override
+   public void closeAndDispose()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+}
