7da79cb62ee9b1cf8d3fbaf3cd3e18ac7eba5d27
InhoLee <ilee@ihmc.us>
2017-08-29 11:18:05 -0500

    Extended GenericTaskNode.

--- IHMCManipulationPlanning/src/us/ihmc/manipulation/planning/rrt/constrainedplanning/configurationAndTimeSpace/GenericTaskNode.java
+++ IHMCManipulationPlanning/src/us/ihmc/manipulation/planning/rrt/constrainedplanning/configurationAndTimeSpace/GenericTaskNode.java
@@ -1,15 +1,34 @@
 package us.ihmc.manipulation.planning.rrt.constrainedplanning.configurationAndTimeSpace;
 
-import us.ihmc.humanoidRobotics.communication.packets.manipulation.constrainedWholeBodyPlanning.ConfigurationSpace;
-
 public class GenericTaskNode extends CTTaskNode
 {
    public static double handCoordinateOffsetX = -0.2;
-   public static int nodeDimension = 11;
+ 
+   /**
+    * 0  time
+    * 1  pelvis height
+    * 2  chest yaw
+    * 3  chest pitch
+    * 4  chest roll
+    * 5  left x
+    * 6  left y
+    * 7  left z
+    * 8  left roll
+    * 9  left pitch
+    * 10 left yaw
+    * 11 right x
+    * 12 right y
+    * 13 right z
+    * 14 right roll
+    * 15 right pitch
+    * 16 right yaw
+    * 
+    */
+   public static int nodeDimension = 17;  // 11
 
    public GenericTaskNode()
    {
-      super(11);
+      super(nodeDimension);
    }
    
    public GenericTaskNode(CTTaskNode node)
@@ -17,54 +36,55 @@ public class GenericTaskNode extends CTTaskNode
       super(node);
    }
 
-   public GenericTaskNode(double time, double pelvisHeight, double chestYaw, double chestPitch, double chestRoll, double eeX, double eeY, double eeZ,
-                          double eeRoll, double eePitch, double eeYaw)
-   {
-      super(11);
-      setNodeData(0, time);
-      setNodeData(1, pelvisHeight);
-      setNodeData(2, chestYaw);
-      setNodeData(3, chestPitch);
-      setNodeData(4, chestRoll);
-      setNodeData(5, eeX);
-      setNodeData(6, eeY);
-      setNodeData(7, eeZ);
-      setNodeData(8, eeRoll);
-      setNodeData(9, eePitch);
-      setNodeData(10, eeYaw);
-   }
-
-   public GenericTaskNode(double time, double pelvisHeight, double chestYaw, double chestPitch, double chestRoll, ConfigurationSpace eeConfigurationSpace)
-   {
-      super(11);
-      setNodeData(0, time);
-      setNodeData(1, pelvisHeight);
-      setNodeData(2, chestYaw);
-      setNodeData(3, chestPitch);
-      setNodeData(4, chestRoll);
-      setNodeData(5, eeConfigurationSpace.getTranslationX());
-      setNodeData(6, eeConfigurationSpace.getTranslationY());
-      setNodeData(7, eeConfigurationSpace.getTranslationZ());
-      setNodeData(8, eeConfigurationSpace.getRotationRoll());
-      setNodeData(9, eeConfigurationSpace.getRotationPitch());
-      setNodeData(10, eeConfigurationSpace.getRotationYaw());
-   }
-
-   public GenericTaskNode(double time, double pelvisHeight, double chestYaw, double chestPitch, double chestRoll)
-   {
-      super(11);
-      setNodeData(0, time);
-      setNodeData(1, pelvisHeight);
-      setNodeData(2, chestYaw);
-      setNodeData(3, chestPitch);
-      setNodeData(4, chestRoll);
-      ConfigurationSpace eeConfigurationSpace = new ConfigurationSpace();
-      setNodeData(5, eeConfigurationSpace.getTranslationX());
-      setNodeData(6, eeConfigurationSpace.getTranslationY());
-      setNodeData(7, eeConfigurationSpace.getTranslationZ());
-      setNodeData(8, eeConfigurationSpace.getRotationRoll());
-      setNodeData(9, eeConfigurationSpace.getRotationPitch());
-      setNodeData(10, eeConfigurationSpace.getRotationYaw());
-   }
+//   public GenericTaskNode(double time, double pelvisHeight, double chestYaw, double chestPitch, double chestRoll, double eeX, double eeY, double eeZ,
+//                          double eeRoll, double eePitch, double eeYaw)
+//   {
+//      super(nodeDimension);
+//      setNodeData(0, time);
+//      setNodeData(1, pelvisHeight);
+//      setNodeData(2, chestYaw);
+//      setNodeData(3, chestPitch);
+//      setNodeData(4, chestRoll);
+//      setNodeData(5, eeX);
+//      setNodeData(6, eeY);
+//      setNodeData(7, eeZ);
+//      setNodeData(8, eeRoll);
+//      setNodeData(9, eePitch);
+//      setNodeData(10, eeYaw);
+//   }
+//
+//   public GenericTaskNode(double time, double pelvisHeight, double chestYaw, double chestPitch, double chestRoll, ConfigurationSpace eeConfigurationSpace)
+//   {
+//      super(nodeDimension);
+//      setNodeData(0, time);
+//      setNodeData(1, pelvisHeight);
+//      setNodeData(2, chestYaw);
+//      setNodeData(3, chestPitch);
+//      setNodeData(4, chestRoll);
+//      setNodeData(5, eeConfigurationSpace.getTranslationX());
+//      setNodeData(6, eeConfigurationSpace.getTranslationY());
+//      setNodeData(7, eeConfigurationSpace.getTranslationZ());
+//      setNodeData(8, eeConfigurationSpace.getRotationRoll());
+//      setNodeData(9, eeConfigurationSpace.getRotationPitch());
+//      setNodeData(10, eeConfigurationSpace.getRotationYaw());
+//   }
+//
+//   public GenericTaskNode(double time, double pelvisHeight, double chestYaw, double chestPitch, double chestRoll)
+//   {
+//      super(nodeDimension);
+//      setNodeData(0, time);
+//      setNodeData(1, pelvisHeight);
+//      setNodeData(2, chestYaw);
+//      setNodeData(3, chestPitch);
+//      setNodeData(4, chestRoll);
+//      
+//      ConfigurationSpace eeConfigurationSpace = new ConfigurationSpace();
+//      setNodeData(5, eeConfigurationSpace.getTranslationX());
+//      setNodeData(6, eeConfigurationSpace.getTranslationY());
+//      setNodeData(7, eeConfigurationSpace.getTranslationZ());
+//      setNodeData(8, eeConfigurationSpace.getRotationRoll());
+//      setNodeData(9, eeConfigurationSpace.getRotationPitch());
+//      setNodeData(10, eeConfigurationSpace.getRotationYaw());
+//   }
 
 }
