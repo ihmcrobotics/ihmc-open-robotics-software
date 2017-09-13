344b944b10dd46303909e1d06b7f3edde40f5822
Robert <rgriffin@ihmc.us>
2017-09-12 23:49:12 -0500

    redid the new walking track syntax to allow customizable state machines

--- ihmc-simulation-toolkit/src/main/java/us/ihmc/simulationToolkit/controllers/JointLowLevelJointControlSimulator.java
+++ ihmc-simulation-toolkit/src/main/java/us/ihmc/simulationToolkit/controllers/JointLowLevelJointControlSimulator.java
@@ -83,19 +83,12 @@ public class JointLowLevelJointControlSimulator implements RobotController
       }
       else
       {
-         /*
          jointPositionController.setProportionalGain(63.0 * totalMass);
          jointPositionController.setIntegralGain(6.0 * 50.0 * totalMass);
          jointPositionController.setMaxIntegralError(0.2);
          jointPositionController.setIntegralLeakRatio(integralLeakRatio);
-         jointPositionController.setDerivativeGain(5.0 * totalMass);
-         jointPositionController.setMaximumOutputLimit(2.5 * totalMass);
-         */
-         jointPositionController.setProportionalGain(20.0 * totalMass);
-         jointPositionController.setIntegralGain(6.0 * 50.0 * totalMass);
-         jointPositionController.setMaxIntegralError(0.2);
-         jointPositionController.setIntegralLeakRatio(integralLeakRatio);
-         jointPositionController.setDerivativeGain(0.5 * totalMass);
+         //jointPositionController.setDerivativeGain(5.0 * totalMass);
+         jointPositionController.setDerivativeGain(0.1 * totalMass);
          jointPositionController.setMaximumOutputLimit(2.5 * totalMass);
 
          jointVelocityController.setProportionalGain(3.2 * totalMass);
