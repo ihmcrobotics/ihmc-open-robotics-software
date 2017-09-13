a267db84cbd8edcd9de418f4ec9baf89c0ae84c6
InhoLee <ilee@ihmc.us>
2017-09-13 16:21:24 -0500

    Tree Expansion test file is created.

---
+++ ihmc-manipulation-planning/src/main/java/us/ihmc/manipulation/planning/rrt/constrainedplanning/configurationAndTimeSpace/CTTaskNodeTreeExploringTest.java
@@ -0,0 +1,20 @@
+package us.ihmc.manipulation.planning.rrt.constrainedplanning.configurationAndTimeSpace;
+
+import us.ihmc.commons.PrintTools;
+
+public class CTTaskNodeTreeExploringTest
+{
+
+   CTTaskNode node;
+   
+   CTTaskNodeTree tree;
+   
+   
+   
+   
+   
+   public static void main(String[] args)
+   {
+      PrintTools.info("Exploring test for CTNodeTree");
+   }
+}
