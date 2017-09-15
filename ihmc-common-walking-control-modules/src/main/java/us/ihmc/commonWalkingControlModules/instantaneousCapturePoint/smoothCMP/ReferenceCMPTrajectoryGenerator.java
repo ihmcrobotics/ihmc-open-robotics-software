40d1e072401be33ca9da38c3198aa85272edf737
Robert <rgriffin@ihmc.us>
2017-09-15 13:33:49 -0500

    turned off the new planner by default for atlas

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/smoothCMP/ReferenceCMPTrajectoryGenerator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/smoothCMP/ReferenceCMPTrajectoryGenerator.java
@@ -10,7 +10,6 @@ import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.robotics.math.frames.YoFramePoint;
 import us.ihmc.robotics.math.frames.YoFrameVector;
-import us.ihmc.robotics.math.trajectories.Trajectory;
 import us.ihmc.robotics.math.trajectories.TrajectoryMathTools;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
