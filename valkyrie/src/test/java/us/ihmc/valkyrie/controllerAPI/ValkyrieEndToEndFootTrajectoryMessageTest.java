581ddc992aa9c4b26ad00ee580fbed0627a1527a
gwiedebach <gwiedebach@ihmc.us>
2017-09-20 11:58:56 -0400

    Fix some tests and their annotations to get run on bamboo.

--- valkyrie/src/test/java/us/ihmc/valkyrie/controllerAPI/ValkyrieEndToEndFootTrajectoryMessageTest.java
+++ valkyrie/src/test/java/us/ihmc/valkyrie/controllerAPI/ValkyrieEndToEndFootTrajectoryMessageTest.java
@@ -1,14 +1,66 @@
 package us.ihmc.valkyrie.controllerAPI;
 
+import org.junit.Test;
+
 import us.ihmc.avatar.controllerAPI.EndToEndFootTrajectoryMessageTest;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.continuousIntegration.IntegrationCategory;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 
 public class ValkyrieEndToEndFootTrajectoryMessageTest extends EndToEndFootTrajectoryMessageTest
 {
    private final ValkyrieRobotModel robotModel = new ValkyrieRobotModel(RobotTarget.SCS, false);
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 41.5, categoriesOverride = {IntegrationCategory.FAST, IntegrationCategory.VIDEO})
+   @Test
+   public void testSingleWaypoint() throws Exception
+   {
+      super.testSingleWaypoint();
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 41.5)
+   @Test
+   public void testCustomControlPoint() throws Exception
+   {
+      super.testCustomControlPoint();
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 46.8)
+   @Test
+   public void testMultipleTrajectoryPoints() throws Exception
+   {
+      super.testMultipleTrajectoryPoints();
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 108.9)
+   @Test
+   public void testQueuedMessages() throws Exception
+   {
+      super.testQueuedMessages();
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 32.1)
+   @Test
+   public void testQueueWithWrongPreviousId() throws Exception
+   {
+      super.testQueueWithWrongPreviousId();
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 38.0)
+   @Test
+   public void testQueueStoppedWithOverrideMessage() throws Exception
+   {
+      super.testQueueStoppedWithOverrideMessage();
+   }
 
    @Override
    public DRCRobotModel getRobotModel()
