85c0365ef0e144a09cd80f5968142b40c77bd274
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-10-05 17:06:59 -0500

    Nuked some code from earlier versions of the planner to increase coverage

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/smoothCMPBasedICPPlanner/WalkingTrajectoryType.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/smoothCMPBasedICPPlanner/WalkingTrajectoryType.java
@@ -3,14 +3,4 @@ package us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.smoothCMPB
 public enum WalkingTrajectoryType
 {
    SWING, TRANSFER;
-   
-   @Override
-   public String toString()
-   {
-      switch (this)
-      {
-      case SWING: return "Swing";
-      default: return "Transfer";
-      }
-   }
 }
