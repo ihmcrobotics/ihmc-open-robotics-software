4e9528d0403afe79439fa6f71851dfe676596566
Robert <rgriffin@ihmc.us>
2017-10-05 21:51:43 -0500

    added documentation to the standard controller transitions

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/ControllerStateTransitionFactory.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/ControllerStateTransitionFactory.java
@@ -10,8 +10,14 @@ import java.util.EnumMap;
 
 public interface ControllerStateTransitionFactory<E extends Enum<E>>
 {
+   /**
+    * Gets the new transition. If it has not been created previously, this creates it, and then returns it.
+    */
    StateTransition<E> getOrCreateStateTransition(EnumMap<E, ? extends FinishableState<E>> stateMap, HighLevelControllerFactoryHelper controllerFactoryHelper,
                                                  YoVariableRegistry parentRegistry);
 
+   /**
+    * The state in which this transition is to be checked.
+    */
    E getStateToAttachEnum();
 }
