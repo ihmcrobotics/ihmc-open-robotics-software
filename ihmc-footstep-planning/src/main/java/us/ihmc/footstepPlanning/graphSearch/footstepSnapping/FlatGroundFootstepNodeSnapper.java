09a7f552f1739a3ee9146ba3f98703a0b8c4e1d4
Stephen McCrory <smccrory@ihmc.us>
2017-10-07 14:11:19 -0500

    moving set planar regions list implementation to FootstepNodeSnapper and checking for planar regions before attempting to snap

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/footstepSnapping/FlatGroundFootstepNodeSnapper.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/footstepSnapping/FlatGroundFootstepNodeSnapper.java
@@ -15,11 +15,6 @@ public class FlatGroundFootstepNodeSnapper extends FootstepNodeSnapper
    }
 
    @Override
-   public void setPlanarRegions(PlanarRegionsList planarRegionsList)
-   {
-   }
-
-   @Override
    public FootstepNodeSnapData snapInternal(FootstepNode footstepNode)
    {
       return FootstepNodeSnapData.emptyData();
