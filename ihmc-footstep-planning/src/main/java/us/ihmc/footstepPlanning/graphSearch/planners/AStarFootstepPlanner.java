9125e4e4e99f606fa955c24cfc5bd35020e4ca1b
gwiedebach <gwiedebach@ihmc.us>
2017-10-14 16:36:36 -0500

    Added some statistics.

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/planners/AStarFootstepPlanner.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/planners/AStarFootstepPlanner.java
@@ -40,6 +40,7 @@ import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
+import us.ihmc.yoVariables.variable.YoLong;
 
 public class AStarFootstepPlanner implements FootstepPlanner
 {
@@ -63,6 +64,10 @@ public class AStarFootstepPlanner implements FootstepPlanner
    private final FootstepNodeSnapper snapper;
 
    private final YoDouble timeout;
+
+   private final YoLong numberOfExpandedNodes = new YoLong("NumberOfExpandedNodex", registry);
+   private final YoDouble percentRejectedNodes = new YoDouble("PercentRejectedNodes", registry);
+   private final YoLong itarationCount = new YoLong("ItarationCount", registry);
 
    public AStarFootstepPlanner(FootstepPlannerParameters parameters, FootstepNodeChecker nodeChecker, CostToGoHeuristics heuristics,
                                FootstepNodeExpansion expansion, FootstepCost stepCostCalculator, FootstepNodeSnapper snapper, YoVariableRegistry parentRegistry)
@@ -221,8 +226,14 @@ public class AStarFootstepPlanner implements FootstepPlanner
    {
       long planningStartTime = System.nanoTime();
 
+      long rejectedNodesCount = 0;
+      long expandedNodesCount = 0;
+      long iterations = 0;
+
       while (!stack.isEmpty())
       {
+         iterations++;
+
          FootstepNode nodeToExpand = stack.poll();
          if (expandedNodes.contains(nodeToExpand))
             continue;
@@ -243,11 +254,15 @@ public class AStarFootstepPlanner implements FootstepPlanner
          }
 
          HashSet<FootstepNode> neighbors = nodeExpansion.expandNode(nodeToExpand);
+         expandedNodesCount += neighbors.size();
          for (FootstepNode neighbor : neighbors)
          {
             /** Checks if the footstep (center of the foot) is on a planar region*/
             if (!nodeChecker.isNodeValid(neighbor, nodeToExpand))
+            {
+               rejectedNodesCount++;
                continue;
+            }
 
             double cost = stepCostCalculator.compute(nodeToExpand, neighbor);
             graph.checkAndSetEdge(nodeToExpand, neighbor, cost);
@@ -258,6 +273,10 @@ public class AStarFootstepPlanner implements FootstepPlanner
          if (Conversions.nanosecondsToSeconds(timeInNano - planningStartTime) > timeout.getDoubleValue())
             break;
       }
+
+      percentRejectedNodes.set(100.0 * rejectedNodesCount / expandedNodesCount);
+      itarationCount.set(iterations);
+      numberOfExpandedNodes.set(expandedNodesCount / iterations);
    }
 
    private FootstepPlanningResult checkResult()
