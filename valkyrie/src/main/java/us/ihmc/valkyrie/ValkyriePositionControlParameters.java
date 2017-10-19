35eb104fc6b729476176527fe8a9fcd806b92cbb
ValkyrieOCU <operatorcomputer@ihmc.us>
2017-10-19 15:50:00 -0500

    Cleaned up the position control parameters.

--- valkyrie/src/main/java/us/ihmc/valkyrie/ValkyriePositionControlParameters.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyriePositionControlParameters.java
@@ -1,27 +1,37 @@
 package us.ihmc.valkyrie;
 
-import org.yaml.snakeyaml.Yaml;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.PositionControlParameters;
-import us.ihmc.valkyrie.parameters.ValkyrieOrderedJointMap;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.yaml.snakeyaml.Yaml;
+
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.PositionControlParameters;
+import us.ihmc.robotics.partNames.ArmJointName;
+import us.ihmc.robotics.partNames.LegJointName;
+import us.ihmc.robotics.partNames.NeckJointName;
+import us.ihmc.robotics.partNames.SpineJointName;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.valkyrie.parameters.ValkyrieJointMap;
+import us.ihmc.valkyrie.parameters.ValkyrieOrderedJointMap;
+
 public class ValkyriePositionControlParameters implements PositionControlParameters
 {
-   private static HashMap<String, Double> proportionalGain = new HashMap<>();
-   private static HashMap<String, Double> derivativeGain = new HashMap<>();
-   private static HashMap<String, Double> integralGain = new HashMap<>();
+   private final HashMap<String, Double> proportionalGain = new HashMap<>();
+   private final HashMap<String, Double> derivativeGain = new HashMap<>();
+   private final HashMap<String, Double> integralGain = new HashMap<>();
+   private final ValkyrieJointMap jointMap;
 
-   static
+   public ValkyriePositionControlParameters(ValkyrieJointMap jointMap)
    {
+      this.jointMap = jointMap;
       useDefaultGains();
       loadCustomGains();
    }
 
-   private static void loadCustomGains()
+   @SuppressWarnings("unchecked")
+   private void loadCustomGains()
    {
       Yaml yaml = new Yaml();
       InputStream gainStream = ValkyriePositionControlParameters.class.getClassLoader().getResourceAsStream("standPrep/gains.yaml");
@@ -34,207 +44,133 @@ public class ValkyriePositionControlParameters implements PositionControlParamet
       }
       catch (IOException e)
       {
+         return;
+      }
+
+      for (String jointName : ValkyrieOrderedJointMap.jointNames)
+      {
+         Map<String, Double> jointGains = gainMap.get(jointName);
+         if (jointGains == null)
+         {
+            setJointGains(jointName, 0.0, 0.0, 0.0);
+         }
+         else
+         {
+            Double kp = jointGains.get("kp");
+            Double ki = jointGains.get("ki");
+            Double kd = jointGains.get("kd");
+            setProportionalGain(jointName, kp == null ? 0.0 : kp);
+            setIntegralGain(jointName, ki == null ? 0.0 : ki);
+            setDerivativeGain(jointName, kd == null ? 0.0 : kd);
+         }
       }
    }
 
-   private static void useDefaultGains()
+   private void useDefaultGains()
    {
-      setProportionalGain(ValkyrieOrderedJointMap.LeftHipYaw,          100.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftHipRoll,         500.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftHipPitch,        550.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftKneePitch,       259.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftAnklePitch,       68.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftAnkleRoll,        56.0);
+      setJointGains(jointMap.getNeckJointName(NeckJointName.PROXIMAL_NECK_PITCH), 0.0, 0.0, 0.0);
+      setJointGains(jointMap.getNeckJointName(NeckJointName.DISTAL_NECK_YAW), 0.0, 0.0, 0.0);
+      setJointGains(jointMap.getNeckJointName(NeckJointName.DISTAL_NECK_PITCH), 0.0, 0.0, 0.0);
 
-      setProportionalGain(ValkyrieOrderedJointMap.RightHipYaw,         100.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightHipRoll,        500.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightHipPitch,       550.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightKneePitch,      259.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightAnklePitch,      68.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightAnkleRoll,       56.0);
+      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_YAW), 100.0, 0.0, 6.2);
+      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_PITCH), 600.0, 0.0, 40.0);
+      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_ROLL), 600.0, 0.0, 40.0);
 
-      setProportionalGain(ValkyrieOrderedJointMap.TorsoYaw,            100.0);
-      setProportionalGain(ValkyrieOrderedJointMap.TorsoPitch,          600.0);
-      setProportionalGain(ValkyrieOrderedJointMap.TorsoRoll,           600.0);
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), 50.0, 0.0, 5.0);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), 50.0, 0.0, 5.0);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), 40.0, 0.0, 4.0);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), 40.0, 0.0, 10.0);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_ROLL), 0.0, 0.0, 0.0);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.WRIST_ROLL), 0.0, 0.0, 0.0);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.FIRST_WRIST_PITCH), 0.0, 0.0, 0.0);
 
-      setProportionalGain(ValkyrieOrderedJointMap.LeftShoulderPitch,    50.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftShoulderRoll,     50.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftShoulderYaw,      40.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftElbowPitch,       40.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftForearmYaw,        0.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftWristRoll,         0.0);
-      setProportionalGain(ValkyrieOrderedJointMap.LeftWristPitch,        0.0);
-
-      setProportionalGain(ValkyrieOrderedJointMap.LowerNeckPitch,        0.0);
-      setProportionalGain(ValkyrieOrderedJointMap.NeckYaw,               0.0);
-      setProportionalGain(ValkyrieOrderedJointMap.UpperNeckPitch,        0.0);
-
-      setProportionalGain(ValkyrieOrderedJointMap.RightShoulderPitch,   50.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightShoulderRoll,    50.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightShoulderYaw,     40.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightElbowPitch,      40.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightForearmYaw,       0.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightWristRoll,        0.0);
-      setProportionalGain(ValkyrieOrderedJointMap.RightWristPitch,       0.0);
-
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftHipYaw,            6.2);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftHipRoll,          25.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftHipPitch,         21.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftKneePitch,         9.4);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftAnklePitch,        6.29);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftAnkleRoll,         3.14);
-
-      setDerivativeGain(ValkyrieOrderedJointMap.RightHipYaw,           6.2);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightHipRoll,         25.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightHipPitch,        21.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightKneePitch,        9.4);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightAnklePitch,       6.29);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightAnkleRoll,        3.14);
-
-      setDerivativeGain(ValkyrieOrderedJointMap.TorsoYaw,              6.2);
-      setDerivativeGain(ValkyrieOrderedJointMap.TorsoPitch,           40.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.TorsoRoll,            40.0);
-
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftShoulderPitch,     5.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftShoulderRoll,      5.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftShoulderYaw,       4.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftElbowPitch,       10.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftForearmYaw,        0.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftWristRoll,         0.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.LeftWristPitch,        0.0);
-
-      setDerivativeGain(ValkyrieOrderedJointMap.LowerNeckPitch,        0.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.NeckYaw,               0.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.UpperNeckPitch,        0.0);
-
-      setDerivativeGain(ValkyrieOrderedJointMap.RightShoulderPitch,    5.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightShoulderRoll,     5.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightShoulderYaw,      4.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightElbowPitch,       4.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightForearmYaw,       0.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightWristRoll,        0.0);
-      setDerivativeGain(ValkyrieOrderedJointMap.RightWristPitch,       0.0);
-
-      setIntegralGain(ValkyrieOrderedJointMap.LeftHipYaw,          0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftHipRoll,         0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftHipPitch,        0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftKneePitch,       0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftAnklePitch,      0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftAnkleRoll,       0.0);
-
-      setIntegralGain(ValkyrieOrderedJointMap.RightHipYaw,         0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightHipRoll,        0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightHipPitch,       0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightKneePitch,      0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightAnklePitch,     0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightAnkleRoll,      0.0);
-
-      setIntegralGain(ValkyrieOrderedJointMap.TorsoYaw,            0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.TorsoPitch,          0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.TorsoRoll,           0.0);
-
-      setIntegralGain(ValkyrieOrderedJointMap.LeftShoulderPitch,     0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftShoulderRoll,      0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftShoulderYaw,       0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftElbowPitch,        0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftForearmYaw,        0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftWristRoll,         0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.LeftWristPitch,        0.0);
-
-      setIntegralGain(ValkyrieOrderedJointMap.LowerNeckPitch,        0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.NeckYaw,               0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.UpperNeckPitch,        0.0);
-
-      setIntegralGain(ValkyrieOrderedJointMap.RightShoulderPitch,     0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightShoulderRoll,      0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightShoulderYaw,       0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightElbowPitch,        0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightForearmYaw,        0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightWristRoll,         0.0);
-      setIntegralGain(ValkyrieOrderedJointMap.RightWristPitch,        0.0);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_YAW), 100.0, 0.0, 6.2);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_ROLL), 500.0, 0.0, 25.0);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_PITCH), 550.0, 0.0, 21.0);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.KNEE_PITCH), 259.0, 0.0, 9.4);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_PITCH), 68.0, 0.0, 6.29);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_ROLL), 56.0, 0.0, 3.14);
+      }
    }
 
-   
-   private static void setProportionalGain(int jointIndex, double value)
+   private void setJointGains(String jointName, double kp, double ki, double kd)
    {
-      String jointName = ValkyrieOrderedJointMap.jointNames[jointIndex];
-      setProportionalGain(jointName, value);
+      setProportionalGain(jointName, kp);
+      setIntegralGain(jointName, ki);
+      setDerivativeGain(jointName, kd);
    }
 
-   private static void setProportionalGain(String jointName, double value)
+   private void setProportionalGain(String jointName, double value)
    {
-      if(proportionalGain.containsKey(jointName))
+      if (proportionalGain.containsKey(jointName))
       {
          proportionalGain.remove(jointName);
       }
       proportionalGain.put(jointName, value);
    }
 
-   private static void setDerivativeGain(int jointIndex, double value)
+   private void setDerivativeGain(String jointName, double value)
    {
-      String jointName = ValkyrieOrderedJointMap.jointNames[jointIndex];
-      setDerivativeGain(jointName, value);
-   }
-
-   private static void setDerivativeGain(String jointName, double value)
-   {
-      if(derivativeGain.containsKey(jointName))
+      if (derivativeGain.containsKey(jointName))
       {
          derivativeGain.remove(jointName);
       }
       derivativeGain.put(jointName, value);
    }
 
-   private static void setIntegralGain(int jointIndex, double value)
+   private void setIntegralGain(String jointName, double value)
    {
-      String jointName = ValkyrieOrderedJointMap.jointNames[jointIndex];
-      setIntegralGain(jointName, value);
-   }
-
-   private static void setIntegralGain(String jointName, double value)
-   {
-      if(integralGain.containsKey(jointName))
+      if (integralGain.containsKey(jointName))
       {
          integralGain.remove(jointName);
       }
       integralGain.put(jointName, value);
    }
 
+   @Override
    public double getProportionalGain(int jointIndex)
    {
       String jointName = ValkyrieOrderedJointMap.jointNames[jointIndex];
       return getProportionalGain(jointName);
    }
 
+   @Override
    public double getProportionalGain(String jointName)
    {
       return proportionalGain.get(jointName);
    }
 
+   @Override
    public double getDerivativeGain(int jointIndex)
    {
       String jointName = ValkyrieOrderedJointMap.jointNames[jointIndex];
       return getDerivativeGain(jointName);
    }
 
+   @Override
    public double getDerivativeGain(String jointName)
    {
       return derivativeGain.get(jointName);
    }
 
+   @Override
    public double getIntegralGain(int jointIndex)
    {
       String jointName = ValkyrieOrderedJointMap.jointNames[jointIndex];
       return getIntegralGain(jointName);
    }
 
+   @Override
    public double getIntegralGain(String jointName)
    {
       return integralGain.get(jointName);
    }
 
+   @Override
    public double getPositionControlMasterGain()
    {
-      return 3.0;
+      return 1.0;
    }
 }
