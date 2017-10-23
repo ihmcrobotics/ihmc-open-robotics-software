aac35b9e0cca187db4722c93d89765eb5bc34d6e
Valkyrie Laptop <val@con01>
2017-10-23 18:58:17 -0500

    Removed the file loading.

--- valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieStandPrepParameters.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieStandPrepParameters.java
@@ -1,11 +1,6 @@
 package us.ihmc.valkyrie;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
-import java.util.Map;
-
-import org.yaml.snakeyaml.Yaml;
 
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.PositionControlParameters;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.WholeBodySetpointParameters;
@@ -29,31 +24,7 @@ public class ValkyrieStandPrepParameters implements WholeBodySetpointParameters,
    {
       this.jointMap = jointMap;
       useDefaultAngles();
-      loadCustomSetPoints();
       useDefaultGains();
-      loadCustomGains();
-   }
-
-   private void loadCustomSetPoints()
-   {
-      Yaml yaml = new Yaml();
-      InputStream setpointsStream = getClass().getClassLoader().getResourceAsStream("standPrep/setpoints.yaml");
-      @SuppressWarnings("unchecked")
-      Map<String, Double> setPointMap = (Map<String, Double>) yaml.load(setpointsStream);
-
-      for (String jointName : ValkyrieOrderedJointMap.jointNames)
-      {
-         if (setPointMap.containsKey(jointName))
-            setSetpoint(jointName, setPointMap.get(jointName));
-      }
-
-      try
-      {
-         setpointsStream.close();
-      }
-      catch (IOException e)
-      {
-      }
    }
 
    private void useDefaultAngles()
@@ -85,68 +56,29 @@ public class ValkyrieStandPrepParameters implements WholeBodySetpointParameters,
       }
    }
 
-   @SuppressWarnings("unchecked")
-   private void loadCustomGains()
-   {
-      Yaml yaml = new Yaml();
-      InputStream gainStream = getClass().getClassLoader().getResourceAsStream("standPrep/gains.yaml");
-
-      Map<String, Map<String, Double>> gainMap = (Map<String, Map<String, Double>>) yaml.load(gainStream);
-
-      try
-      {
-         gainStream.close();
-      }
-      catch (IOException e)
-      {
-         return;
-      }
-
-      for (String jointName : ValkyrieOrderedJointMap.jointNames)
-      {
-         Map<String, Double> jointGains = gainMap.get(jointName);
-         if (jointGains == null)
-         {
-            setJointGains(jointName, 0.0, 0.0, 0.0);
-         }
-         else
-         {
-            Double kp = jointGains.get("kp");
-            Double ki = jointGains.get("ki");
-            Double kd = jointGains.get("kd");
-            setProportionalGain(jointName, kp == null ? 0.0 : kp);
-            setIntegralGain(jointName, ki == null ? 0.0 : ki);
-            setDerivativeGain(jointName, kd == null ? 0.0 : kd);
-         }
-      }
-   }
-
    private void useDefaultGains()
    {
       setJointGains(jointMap.getNeckJointName(NeckJointName.PROXIMAL_NECK_PITCH), 0.0, 0.0, 0.0);
       setJointGains(jointMap.getNeckJointName(NeckJointName.DISTAL_NECK_YAW), 0.0, 0.0, 0.0);
       setJointGains(jointMap.getNeckJointName(NeckJointName.DISTAL_NECK_PITCH), 0.0, 0.0, 0.0);
 
-      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_YAW), 100.0, 0.0, 6.2);
-      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_PITCH), 600.0, 0.0, 40.0);
-      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_ROLL), 600.0, 0.0, 40.0);
+      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_YAW), 30.0, 0.0, 3.0);
+      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_PITCH), 180.0, 0.0, 12.0);
+      setJointGains(jointMap.getSpineJointName(SpineJointName.SPINE_ROLL), 180.0, 0.0, 12.0);
 
       for (RobotSide robotSide : RobotSide.values)
       {
-         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), 50.0, 0.0, 5.0);
-         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), 50.0, 0.0, 5.0);
-         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), 40.0, 0.0, 4.0);
-         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), 40.0, 0.0, 10.0);
-         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_ROLL), 0.0, 0.0, 0.0);
-         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.WRIST_ROLL), 0.0, 0.0, 0.0);
-         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.FIRST_WRIST_PITCH), 0.0, 0.0, 0.0);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), 15.0, 0.0, 1.5);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), 15.0, 0.0, 1.5);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), 12.0, 0.0, 1.2);
+         setJointGains(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), 12.0, 0.0, 3.0);
 
-         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_YAW), 100.0, 0.0, 6.2);
-         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_ROLL), 500.0, 0.0, 25.0);
-         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_PITCH), 550.0, 0.0, 21.0);
-         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.KNEE_PITCH), 259.0, 0.0, 9.4);
-         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_PITCH), 68.0, 0.0, 6.29);
-         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_ROLL), 56.0, 0.0, 3.14);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_YAW), 30.0, 0.0, 6.2);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_ROLL), 150.0, 0.0, 7.5);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.HIP_PITCH), 160.0, 0.0, 7.0);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.KNEE_PITCH), 75.0, 0.0, 3.0);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_PITCH), 25.0, 0.0, 2.0);
+         setJointGains(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_ROLL), 17.0, 0.0, 1.0);
       }
    }
 
