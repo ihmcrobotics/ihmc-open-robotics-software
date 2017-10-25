abf60210efa93f4619ad778e30f7ff84bc0cdd42
Robert <rgriffin@ihmc.us>
2017-10-24 21:21:15 -0500

    setup a bunch of tests for the simple efficient solver with equality and inequality consraints and inactive variables

--- ihmc-convex-optimization-adapter/src/main/java/us/ihmc/convexOptimization/quadraticProgram/SimpleActiveSetQPSolverInterface.java
+++ ihmc-convex-optimization-adapter/src/main/java/us/ihmc/convexOptimization/quadraticProgram/SimpleActiveSetQPSolverInterface.java
@@ -1,6 +1,7 @@
 package us.ihmc.convexOptimization.quadraticProgram;
 
 import org.ejml.data.DenseMatrix64F;
+import us.ihmc.robotics.linearAlgebra.MatrixTools;
 
 public interface SimpleActiveSetQPSolverInterface extends ActiveSetQPSolver
 {
@@ -25,6 +26,11 @@ public interface SimpleActiveSetQPSolverInterface extends ActiveSetQPSolver
 
    public abstract int solve(DenseMatrix64F solutionToPack, DenseMatrix64F lagrangeEqualityConstraintMultipliersToPack,
                              DenseMatrix64F lagrangeInequalityConstraintMultipliersToPack);
+
+   default void setActiveVariables(double[] activeVariables)
+   {
+      setActiveVariables(MatrixTools.createVector(activeVariables));
+   }
 
    @Override
    public abstract int solve(DenseMatrix64F solutionToPack);
