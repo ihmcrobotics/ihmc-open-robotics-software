f2e6d28525a8b5e374981d6171098b1aa5d3d305
InhoLee <ilee@ihmc.us>
2017-10-25 15:03:07 -0500

    WayPointsPacket is created.

---
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/behaviors/WayPointsPacket.java
@@ -0,0 +1,71 @@
+package us.ihmc.humanoidRobotics.communication.packets.behaviors;
+
+import us.ihmc.communication.packets.Packet;
+import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.robotics.MathTools;
+
+public class WayPointsPacket extends Packet<WayPointsPacket>
+{
+   public int numberOfWayPoints;
+   
+   public Point3D[] positionOfWayPoints;
+   public Quaternion[] orientationOfWayPoints;
+   
+   public double trajectoryTime;
+   
+   // VRUI mode should be able to select RobotSide.
+   // 
+   
+   public WayPointsPacket()
+   {
+      
+   }
+   
+   public void setWayPoints(Pose3D[] poseOfWayPoints, double trajectoryTime)
+   {
+      this.trajectoryTime = trajectoryTime;
+      
+      this.numberOfWayPoints = poseOfWayPoints.length;
+      
+      this.positionOfWayPoints = new Point3D[this.numberOfWayPoints];
+      this.orientationOfWayPoints = new Quaternion[this.numberOfWayPoints];
+      
+      for(int i=0;i<this.numberOfWayPoints;i++)
+      {
+         this.positionOfWayPoints[i] = new Point3D(poseOfWayPoints[i].getPosition());
+         this.orientationOfWayPoints[i] = new Quaternion(poseOfWayPoints[i].getOrientation());
+      }
+   }
+   
+   @Override
+   public boolean epsilonEquals(WayPointsPacket other, double epsilon)
+   {
+      if(this.numberOfWayPoints != other.numberOfWayPoints)
+         return false;
+      
+      if(this.positionOfWayPoints.length != other.positionOfWayPoints.length)
+         return false;
+      if(this.orientationOfWayPoints.length != other.orientationOfWayPoints.length)
+         return false;
+      
+      for(int i=0;i<this.positionOfWayPoints.length;i++)
+      {
+         if(!this.positionOfWayPoints[i].epsilonEquals(other.positionOfWayPoints[i], epsilon))
+            return false;
+      }
+      
+      for(int i=0;i<this.orientationOfWayPoints.length;i++)
+      {
+         if(!this.orientationOfWayPoints[i].epsilonEquals(other.orientationOfWayPoints[i], epsilon))
+            return false;
+      }
+      
+      if(!MathTools.epsilonEquals(trajectoryTime, other.trajectoryTime, epsilon))
+         return false;
+            
+      return true;
+   }
+
+}
