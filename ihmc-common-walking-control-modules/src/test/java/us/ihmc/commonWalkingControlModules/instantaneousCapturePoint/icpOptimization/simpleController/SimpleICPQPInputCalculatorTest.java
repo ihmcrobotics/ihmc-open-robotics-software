8f80597dcfe4108c8080449dd204c6cd85fdd05b
Robert <rgriffin@ihmc.us>
2017-10-26 17:15:35 -0500

    added a bunch more tests, most of which are yet left to do

--- ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/simpleController/SimpleICPQPInputCalculatorTest.java
+++ ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/instantaneousCapturePoint/icpOptimization/simpleController/SimpleICPQPInputCalculatorTest.java
@@ -2,6 +2,7 @@ package us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.icpOptimiz
 
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.ops.CommonOps;
+import org.ejml.ops.MatrixFeatures;
 import org.jcodec.common.Assert;
 import org.junit.Test;
 import us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.icpOptimization.qpInput.ICPQPInput;
@@ -166,4 +167,163 @@ public class SimpleICPQPInputCalculatorTest
 
       Assert.assertTrue(icpQPInputExpected.equals(icpQPInputToTest));
    }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testFootstepRegularizationTask()
+   {
+      SimpleICPQPIndexHandler indexHandler = new SimpleICPQPIndexHandler();
+      SimpleICPQPInputCalculator inputCalculator = new SimpleICPQPInputCalculator(indexHandler);
+
+      ICPQPInput icpQPInputToTest = new ICPQPInput(2);
+      ICPQPInput icpQPInputExpected = new ICPQPInput(2);
+
+      DenseMatrix64F footstepWeight = new DenseMatrix64F(2, 2);
+      CommonOps.setIdentity(footstepWeight);
+      CommonOps.scale(2.0, footstepWeight);
+
+      DenseMatrix64F footstepObjective = new DenseMatrix64F(2, 1);
+      footstepObjective.set(0, 0, 0.5);
+      footstepObjective.set(0, 0, 0.1);
+
+      icpQPInputExpected.quadraticTerm.set(footstepWeight);
+
+      DenseMatrix64F Qx_p = new DenseMatrix64F(2, 1);
+      CommonOps.mult(footstepWeight, footstepObjective, Qx_p);
+
+      icpQPInputExpected.linearTerm.set(Qx_p);
+
+      CommonOps.multTransA(footstepObjective, Qx_p, icpQPInputExpected.residualCost);
+      CommonOps.scale(0.5, icpQPInputExpected.residualCost);
+
+      inputCalculator.computeFootstepRegularizationTask(0, icpQPInputToTest, footstepWeight, footstepObjective);
+
+      Assert.assertTrue(icpQPInputExpected.equals(icpQPInputToTest));
+
+
+
+      DenseMatrix64F footstepObjective1 = new DenseMatrix64F(2, 1);
+      DenseMatrix64F footstepObjective2 = new DenseMatrix64F(2, 1);
+      DenseMatrix64F footstepObjective3 = new DenseMatrix64F(2, 1);
+      DenseMatrix64F footstepObjective4 = new DenseMatrix64F(2, 1);
+      footstepObjective1.set(0, 0, 0.5);
+      footstepObjective1.set(1, 0, 0.1);
+      footstepObjective2.set(0, 0, 1.0);
+      footstepObjective2.set(1, 0, -0.1);
+      footstepObjective3.set(0, 0, 1.5);
+      footstepObjective3.set(1, 0, 0.1);
+      footstepObjective4.set(0, 0, 2.0);
+      footstepObjective4.set(1, 0, -0.1);
+
+      footstepObjective = new DenseMatrix64F(8, 1);
+      footstepObjective.set(0, 0, 0.5);
+      footstepObjective.set(1, 0, 0.1);
+      footstepObjective.set(2, 0, 1.0);
+      footstepObjective.set(3, 0, -0.1);
+      footstepObjective.set(4, 0, 1.5);
+      footstepObjective.set(5, 0, 0.1);
+      footstepObjective.set(6, 0, 2.0);
+      footstepObjective.set(7, 0, -0.1);
+
+      DenseMatrix64F bigFootstepWeight = new DenseMatrix64F(8, 8);
+      CommonOps.setIdentity(bigFootstepWeight);
+      CommonOps.scale(2.0, bigFootstepWeight);
+
+      icpQPInputExpected.reshape(8);
+      icpQPInputExpected.reset();
+      icpQPInputToTest.reshape(8);
+      icpQPInputToTest.reset();
+
+      inputCalculator.computeFootstepRegularizationTask(0, icpQPInputToTest, footstepWeight, footstepObjective1);
+      inputCalculator.computeFootstepRegularizationTask(1, icpQPInputToTest, footstepWeight, footstepObjective2);
+      inputCalculator.computeFootstepRegularizationTask(2, icpQPInputToTest, footstepWeight, footstepObjective3);
+      inputCalculator.computeFootstepRegularizationTask(3, icpQPInputToTest, footstepWeight, footstepObjective4);
+
+      icpQPInputExpected.quadraticTerm.set(bigFootstepWeight);
+
+      Qx_p = new DenseMatrix64F(8, 1);
+      CommonOps.mult(bigFootstepWeight, footstepObjective, Qx_p);
+
+      icpQPInputExpected.linearTerm.set(Qx_p);
+
+      CommonOps.multTransA(footstepObjective, Qx_p, icpQPInputExpected.residualCost);
+      CommonOps.scale(0.5, icpQPInputExpected.residualCost);
+
+      Assert.assertTrue(icpQPInputExpected.equals(icpQPInputToTest));
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testComputeDynamicsTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitFeedbackTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitDynamicsTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitAngularMomentumMinimizationTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitFootstepTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitFeedbackAndAngularMomentumTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitFeedbackAndDynamicsTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitFeedbackAndFootstepTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitFeedbackAndFootstepAndDynamicsTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 21000)
+   public void testSubmitFeedbackAndFootstepAndDynamicsAndAngularMomentumTask()
+   {
+      Assert.assertTrue(false);
+   }
+
+
+
+
+
 }
