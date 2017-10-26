7e823b922a5612ea71b7de732fc52b09fa6c759f
Robert <rgriffin@ihmc.us>
2017-10-26 15:37:01 -0500

    added some indicators for the desired test plan

--- ihmc-convex-optimization-adapter/src/test/java/us/ihmc/convexOptimization/quadraticProgram/SimpleActiveSetQPSolverTest.java
+++ ihmc-convex-optimization-adapter/src/test/java/us/ihmc/convexOptimization/quadraticProgram/SimpleActiveSetQPSolverTest.java
@@ -5,9 +5,11 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.continuousIntegration.IntegrationCategory;
 
+@ContinuousIntegrationAnnotations.ContinuousIntegrationPlan(categories = {IntegrationCategory.FAST})
 public class SimpleActiveSetQPSolverTest
 {
 
