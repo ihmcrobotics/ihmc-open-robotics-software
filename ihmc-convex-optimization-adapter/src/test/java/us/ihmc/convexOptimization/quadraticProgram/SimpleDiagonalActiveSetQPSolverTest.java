699cc1e7817e99c813f548516f4004876fc73901
Robert <rgriffin@ihmc.us>
2017-10-27 15:37:16 -0500

    fixed simple diagonal active set qp solver test

--- ihmc-convex-optimization-adapter/src/test/java/us/ihmc/convexOptimization/quadraticProgram/SimpleDiagonalActiveSetQPSolverTest.java
+++ ihmc-convex-optimization-adapter/src/test/java/us/ihmc/convexOptimization/quadraticProgram/SimpleDiagonalActiveSetQPSolverTest.java
@@ -1,10 +1,20 @@
 package us.ihmc.convexOptimization.quadraticProgram;
 
+import org.junit.Test;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+
 public class SimpleDiagonalActiveSetQPSolverTest extends AbstractSimpleActiveSetQPSolverTest
 {
    @Override
    public SimpleActiveSetQPSolverInterface createSolverToTest()
    {
       return new SimpleDiagonalActiveSetQPSolver();
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
+   public void testSimpleCasesWithBoundsConstraints()
+   {
+      testSimpleCasesWithBoundsConstraints(1, 3, 2, 3, false);
    }
 }
