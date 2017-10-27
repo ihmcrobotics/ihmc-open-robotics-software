227ca05f3448e84a19c205e0c9a8e6af50f88b5e
Sylvain Bertrand <sbertrand@ihmc.us>
2017-10-27 11:20:45 -0500

    switched to JavaQuadProg.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsToolboxModule/KinematicsToolboxOptimizationSettings.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsToolboxModule/KinematicsToolboxOptimizationSettings.java
@@ -1,6 +1,8 @@
 package us.ihmc.avatar.networkProcessor.kinematicsToolboxModule;
 
 import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.ControllerCoreOptimizationSettings;
+import us.ihmc.convexOptimization.quadraticProgram.JavaQuadProgSolver;
+import us.ihmc.convexOptimization.quadraticProgram.SimpleActiveSetQPSolverInterface;
 import us.ihmc.euclid.tuple2D.Vector2D;
 
 public class KinematicsToolboxOptimizationSettings implements ControllerCoreOptimizationSettings
@@ -107,5 +109,11 @@ public class KinematicsToolboxOptimizationSettings implements ControllerCoreOpti
    public int getRhoSize()
    {
       return 0;
+   }
+
+   @Override
+   public SimpleActiveSetQPSolverInterface getActiveSetQPSolver()
+   {
+      return new JavaQuadProgSolver();
    }
 }
