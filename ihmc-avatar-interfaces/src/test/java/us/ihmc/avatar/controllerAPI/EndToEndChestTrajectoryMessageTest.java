0dd2e54c01ee549f0af864ef8af27a4963b06660
Robert <rgriffin@ihmc.us>
2017-11-01 10:08:56 -0500

    fixed an end to end chest trajectory test by slightly increasing the allowable epsilon

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/controllerAPI/EndToEndChestTrajectoryMessageTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/controllerAPI/EndToEndChestTrajectoryMessageTest.java
@@ -52,7 +52,7 @@ import us.ihmc.yoVariables.variable.YoInteger;
 public abstract class EndToEndChestTrajectoryMessageTest implements MultiRobotTestInterface
 {
    private static final SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromEnvironmentVariables();
-   private static final double EPSILON_FOR_DESIREDS = 1.0e-4;
+   private static final double EPSILON_FOR_DESIREDS = 5.0e-4;
 
    private DRCSimulationTestHelper drcSimulationTestHelper;
 
