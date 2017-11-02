e840ceaaea3fc2e20ec960c710b7447a87af01bd
Robert <rgriffin@ihmc.us>
2017-11-02 18:28:32 -0500

    created a push recovery test for stepping over some stones with planar regions

---
+++ atlas/src/test/java/us/ihmc/atlas/ObstacleCourseTests/AtlasPushRecoveryOverSteppingStonesTest.java
@@ -0,0 +1,88 @@
+package us.ihmc.atlas.ObstacleCourseTests;
+
+import org.junit.Test;
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.atlas.parameters.AtlasContinuousCMPPlannerParameters;
+import us.ihmc.atlas.parameters.AtlasPhysicalProperties;
+import us.ihmc.atlas.parameters.AtlasSimpleICPOptimizationParameters;
+import us.ihmc.atlas.parameters.AtlasWalkingControllerParameters;
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.obstacleCourseTests.DRCObstacleCourseSteppingStonesTest;
+import us.ihmc.avatar.obstacleCourseTests.DRCPushRecoveryOverSteppingStonesTest;
+import us.ihmc.commonWalkingControlModules.configurations.ICPWithTimeFreezingPlannerParameters;
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.icpOptimization.ICPOptimizationParameters;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.continuousIntegration.IntegrationCategory;
+import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+
+// this test is flaky because toe off is not triggered properly - the toe off condition needs to be fixed
+@ContinuousIntegrationPlan(categories = {IntegrationCategory.FAST, IntegrationCategory.VIDEO})
+public class AtlasPushRecoveryOverSteppingStonesTest extends DRCPushRecoveryOverSteppingStonesTest
+{
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      AtlasRobotModel atlasRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false)
+      {
+         @Override
+         public WalkingControllerParameters getWalkingControllerParameters()
+         {
+            return new AtlasWalkingControllerParameters(RobotTarget.SCS, getJointMap(), getContactPointParameters())
+            {
+               @Override
+               public boolean useOptimizationBasedICPController()
+               {
+                  return true;
+               }
+
+               @Override
+               public ICPOptimizationParameters getICPOptimizationParameters()
+               {
+                  return new AtlasSimpleICPOptimizationParameters(false)
+                  {
+                     @Override
+                     public boolean useAngularMomentum()
+                     {
+                        return true;
+                     }
+
+                     @Override
+                     public boolean useStepAdjustment()
+                     {
+                        return true;
+                     }
+                  };
+               }
+            };
+
+         }
+
+         @Override
+         public ICPWithTimeFreezingPlannerParameters getCapturePointPlannerParameters()
+         {
+            return new AtlasContinuousCMPPlannerParameters(new AtlasPhysicalProperties());
+         }
+      };
+
+      return atlasRobotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 48.9)
+   @Test(timeout = 950000)
+   public void testWalkingOverSteppingStonesForwardPush() throws SimulationExceededMaximumTimeException
+   {
+      super.testWalkingOverSteppingStonesForwardPush();
+   }
+}
