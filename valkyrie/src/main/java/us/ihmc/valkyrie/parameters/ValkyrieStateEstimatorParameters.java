8b47f5ac822d587bc786a8f57aafef7d5707c07c
ValkyrieOCU <operatorcomputer@ihmc.us>
2017-11-02 20:08:20 -0500

    Added the option to load the coeffs from a YAML file.

--- valkyrie/src/main/java/us/ihmc/valkyrie/parameters/ValkyrieStateEstimatorParameters.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/parameters/ValkyrieStateEstimatorParameters.java
@@ -20,10 +20,19 @@ import static us.ihmc.valkyrie.fingers.ValkyrieHandJointName.ThumbPitch2;
 import static us.ihmc.valkyrie.fingers.ValkyrieHandJointName.ThumbPitch3;
 import static us.ihmc.valkyrie.fingers.ValkyrieHandJointName.ThumbRoll;
 
-import java.util.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.yaml.snakeyaml.Yaml;
 
+import us.ihmc.commons.PrintTools;
 import us.ihmc.robotics.partNames.ArmJointName;
 import us.ihmc.robotics.partNames.LegJointName;
 import us.ihmc.robotics.robotSide.RobotSide;
@@ -40,6 +49,7 @@ import us.ihmc.yoVariables.variable.YoDouble;
 public class ValkyrieStateEstimatorParameters extends StateEstimatorParameters
 {
    private static final boolean DEBUG_VELOCITY_WITH_FD = false;
+   private static final String FINGER_TRANSMISSION_FILE = System.getProperty("user.home") + File.separator + "valkyrie/ValkyrieFingerJointTransmissionCoeffs.yaml";
 
    private final boolean runningOnRealRobot;
 
@@ -187,114 +197,159 @@ public class ValkyrieStateEstimatorParameters extends StateEstimatorParameters
    private void configureFingerProcessing(SensorProcessing sensorProcessing)
    {
       YoVariableRegistry registry = sensorProcessing.getYoVariableRegistry();
-
+      
+      SideDependentList<EnumMap<ValkyrieHandJointName, YoDouble>> sideDependentScales = SideDependentList.createListOfEnumMaps(ValkyrieHandJointName.class);
+      SideDependentList<EnumMap<ValkyrieHandJointName, YoDouble>> sideDependentBiases = SideDependentList.createListOfEnumMaps(ValkyrieHandJointName.class);
+      
+      for (RobotSide robotSide : RobotSide.values)
       {
-         SideDependentList<EnumMap<ValkyrieHandJointName, YoDouble>> sideDependentScales = SideDependentList.createListOfEnumMaps(ValkyrieHandJointName.class);
-         SideDependentList<EnumMap<ValkyrieHandJointName, YoDouble>> sideDependentBiases = SideDependentList.createListOfEnumMaps(ValkyrieHandJointName.class);
+         EnumMap<ValkyrieHandJointName, YoDouble> scales = sideDependentScales.get(robotSide);
+         EnumMap<ValkyrieHandJointName, YoDouble> biases = sideDependentBiases.get(robotSide);
+         
+         for (ValkyrieHandJointName fingerJoint : ValkyrieHandJointName.values)
+         {
+            YoDouble scale = new YoDouble("scale" + fingerJoint.getPascalCaseJointName(robotSide), registry);
+            YoDouble bias = new YoDouble("bias" + fingerJoint.getPascalCaseJointName(robotSide), registry);
+            scales.put(fingerJoint, scale);
+            biases.put(fingerJoint, bias);
+         }
+      }
 
+      Yaml yaml = new Yaml();
+      File coeffFile = new File(FINGER_TRANSMISSION_FILE);
+
+      boolean areCoeffsLoaded = false;
+
+      if (coeffFile.exists())
+      {
+         try
+         {
+            FileInputStream fileInputStream = new FileInputStream(coeffFile);
+            @SuppressWarnings("unchecked")
+            Map<String, Map<String, Double>> coeffs = (Map<String, Map<String, Double>>) yaml.load(fileInputStream);
+
+            for (RobotSide robotSide : RobotSide.values)
+            {
+               EnumMap<ValkyrieHandJointName, YoDouble> scales = sideDependentScales.get(robotSide);
+               EnumMap<ValkyrieHandJointName, YoDouble> biases = sideDependentBiases.get(robotSide);
+
+               for (ValkyrieHandJointName jointEnum : ValkyrieHandJointName.values)
+               {
+                  Map<String, Double> jointCoeffs = coeffs.get(jointEnum.getJointName(robotSide));
+
+                  scales.get(jointEnum).set(jointCoeffs.get("scale"));
+                  biases.get(jointEnum).set(jointCoeffs.get("bias"));
+               }
+            }
+            areCoeffsLoaded = true;
+         }
+         catch (FileNotFoundException | NullPointerException e)
+         {
+            e.printStackTrace();
+            System.err.println("Setting to default coeffs.");
+         }
+      }
+      else
+      {
+         PrintTools.info(this, "Did not find: \"" + FINGER_TRANSMISSION_FILE + "\", setting coeffs to default.");
+      }
+
+      if (!areCoeffsLoaded)
+      {
          for (RobotSide robotSide : RobotSide.values)
          {
             EnumMap<ValkyrieHandJointName, YoDouble> scales = sideDependentScales.get(robotSide);
             EnumMap<ValkyrieHandJointName, YoDouble> biases = sideDependentBiases.get(robotSide);
-
-            for (ValkyrieHandJointName fingerJoint : ValkyrieHandJointName.values)
-            {
-               YoDouble scale = new YoDouble("scale" + fingerJoint.getPascalCaseJointName(robotSide), registry);
-               YoDouble bias = new YoDouble("bias" + fingerJoint.getPascalCaseJointName(robotSide), registry);
-               scales.put(fingerJoint, scale);
-               biases.put(fingerJoint, bias);
-            }
-
+            
             boolean isLeftSide = robotSide == RobotSide.LEFT;
-
-            scales.get(ThumbRoll         ).set(isLeftSide ? -1.00 : 0.00);
+            
+            scales.get(ThumbRoll         ).set(isLeftSide ? -1.00 : 1.00);
             scales.get(ThumbPitch1       ).set(isLeftSide ? -1.00 : 1.00);
             scales.get(IndexFingerPitch1 ).set(isLeftSide ? -0.75 : 0.60);
             scales.get(MiddleFingerPitch1).set(isLeftSide ? -0.75 : 0.75);
             scales.get(PinkyPitch1       ).set(isLeftSide ? -0.70 : 0.60);
-
+            
             scales.get(ThumbPitch2       ).set(isLeftSide ?-0.50 : 0.40);
             scales.get(IndexFingerPitch2 ).set(isLeftSide ? 0.62 : 0.70);
             scales.get(MiddleFingerPitch2).set(isLeftSide ? 0.42 : 0.60);
             scales.get(PinkyPitch2       ).set(isLeftSide ? 0.50 : 0.65);
-
+            
             scales.get(ThumbPitch3       ).set(isLeftSide ? 0.30 : 0.30);
             scales.get(IndexFingerPitch3 ).set(isLeftSide ? 0.30 : 0.40);
             scales.get(MiddleFingerPitch3).set(isLeftSide ? 0.30 : 0.30);
             scales.get(PinkyPitch3       ).set(isLeftSide ? 0.30 : 0.30);
-
+            
             biases.get(ThumbRoll         ).set(1.57); // TODO at the same I added these, the thumb roll did not work.
             biases.get(ThumbPitch1       ).set(isLeftSide ?-0.20 :-1.10);
             biases.get(IndexFingerPitch1 ).set(isLeftSide ? 0.82 :-0.60);
             biases.get(MiddleFingerPitch1).set(isLeftSide ? 1.30 : 0.00);
             biases.get(PinkyPitch1       ).set(isLeftSide ? 1.40 :-0.50);
-
+            
             biases.get(ThumbPitch2       ).set(isLeftSide ? 0.00 : 0.40);
             biases.get(IndexFingerPitch2 ).set(isLeftSide ?-0.60 : 0.40);
             biases.get(MiddleFingerPitch2).set(isLeftSide ?-0.80 :-0.20);
             biases.get(PinkyPitch2       ).set(isLeftSide ?-0.63 : 0.30);
-
+            
             biases.get(ThumbPitch3       ).set(isLeftSide ? 0.00 : 0.00);
             biases.get(IndexFingerPitch3 ).set(isLeftSide ?-0.40 : 0.20);
             biases.get(MiddleFingerPitch3).set(isLeftSide ?-0.40 : 0.30);
             biases.get(PinkyPitch3       ).set(isLeftSide ?-0.40 : 0.10);
          }
+      }
 
-
-         for (RobotSide robotSide : RobotSide.values)
-         {
-            { // Doing the thumb separately
-               ValkyrieHandJointName[] thumbBaseJoints = {ThumbRoll, ThumbPitch1, ThumbPitch2};
-               for (ValkyrieHandJointName joint : thumbBaseJoints)
-               {
-                  YoDouble scale = sideDependentScales.get(robotSide).get(joint);
-                  YoDouble bias = sideDependentBiases.get(robotSide).get(joint);
-                  String jointName = joint.getCamelCaseJointName(robotSide);
-                  sensorProcessing.addJointPositionAffineTransformOnlyForSpecifiedJoints(scale, bias, false, jointName);
-               }
-               {
-                  ValkyrieHandJointName slaveJoint = ThumbPitch3;
-                  YoDouble scale = sideDependentScales.get(robotSide).get(slaveJoint);
-                  YoDouble bias = sideDependentBiases.get(robotSide).get(slaveJoint);
-                  String masterJointName = ThumbPitch2.getCamelCaseJointName(robotSide);
-                  String slaveJointName = slaveJoint.getCamelCaseJointName(robotSide);
-                  sensorProcessing.computeJointPositionUsingCoupling(masterJointName, slaveJointName, scale, bias, false);
-               }
-            }
-            
-            ValkyrieHandJointName[] masterJoints = {IndexFingerPitch1, MiddleFingerPitch1, PinkyPitch1};
-            ValkyrieHandJointName[] slaveJoints2 = {IndexFingerPitch2, MiddleFingerPitch2, PinkyPitch2};
-            ValkyrieHandJointName[] slaveJoints3 = {IndexFingerPitch3, MiddleFingerPitch3, PinkyPitch3};
-
-            for (ValkyrieHandJointName joint : masterJoints)
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         { // Doing the thumb separately
+            ValkyrieHandJointName[] thumbBaseJoints = {ThumbRoll, ThumbPitch1, ThumbPitch2};
+            for (ValkyrieHandJointName joint : thumbBaseJoints)
             {
                YoDouble scale = sideDependentScales.get(robotSide).get(joint);
                YoDouble bias = sideDependentBiases.get(robotSide).get(joint);
                String jointName = joint.getCamelCaseJointName(robotSide);
                sensorProcessing.addJointPositionAffineTransformOnlyForSpecifiedJoints(scale, bias, false, jointName);
             }
-
-            for (int i = 0; i < 3; i++)
             {
-               ValkyrieHandJointName masterJoint = masterJoints[i];
+               ValkyrieHandJointName slaveJoint = ThumbPitch3;
+               YoDouble scale = sideDependentScales.get(robotSide).get(slaveJoint);
+               YoDouble bias = sideDependentBiases.get(robotSide).get(slaveJoint);
+               String masterJointName = ThumbPitch2.getCamelCaseJointName(robotSide);
+               String slaveJointName = slaveJoint.getCamelCaseJointName(robotSide);
+               sensorProcessing.computeJointPositionUsingCoupling(masterJointName, slaveJointName, scale, bias, false);
+            }
+         }
 
-               {
-                  ValkyrieHandJointName slaveJoint = slaveJoints2[i];
-                  YoDouble scale = sideDependentScales.get(robotSide).get(slaveJoint);
-                  YoDouble bias = sideDependentBiases.get(robotSide).get(slaveJoint);
-                  String masterJointName = masterJoint.getCamelCaseJointName(robotSide);
-                  String slaveJointName = slaveJoint.getCamelCaseJointName(robotSide);
-                  sensorProcessing.computeJointPositionUsingCoupling(masterJointName, slaveJointName, scale, bias, false);
-               }
+         ValkyrieHandJointName[] masterJoints = {IndexFingerPitch1, MiddleFingerPitch1, PinkyPitch1};
+         ValkyrieHandJointName[] slaveJoints2 = {IndexFingerPitch2, MiddleFingerPitch2, PinkyPitch2};
+         ValkyrieHandJointName[] slaveJoints3 = {IndexFingerPitch3, MiddleFingerPitch3, PinkyPitch3};
 
-               {
-                  ValkyrieHandJointName slaveJoint = slaveJoints3[i];
-                  YoDouble scale = sideDependentScales.get(robotSide).get(slaveJoint);
-                  YoDouble bias = sideDependentBiases.get(robotSide).get(slaveJoint);
-                  String masterJointName = masterJoint.getCamelCaseJointName(robotSide);
-                  String slaveJointName = slaveJoint.getCamelCaseJointName(robotSide);
-                  sensorProcessing.computeJointPositionUsingCoupling(masterJointName, slaveJointName, scale, bias, false);
-               }
+         for (ValkyrieHandJointName joint : masterJoints)
+         {
+            YoDouble scale = sideDependentScales.get(robotSide).get(joint);
+            YoDouble bias = sideDependentBiases.get(robotSide).get(joint);
+            String jointName = joint.getCamelCaseJointName(robotSide);
+            sensorProcessing.addJointPositionAffineTransformOnlyForSpecifiedJoints(scale, bias, false, jointName);
+         }
+
+         for (int i = 0; i < 3; i++)
+         {
+            ValkyrieHandJointName masterJoint = masterJoints[i];
+
+            {
+               ValkyrieHandJointName slaveJoint = slaveJoints2[i];
+               YoDouble scale = sideDependentScales.get(robotSide).get(slaveJoint);
+               YoDouble bias = sideDependentBiases.get(robotSide).get(slaveJoint);
+               String masterJointName = masterJoint.getCamelCaseJointName(robotSide);
+               String slaveJointName = slaveJoint.getCamelCaseJointName(robotSide);
+               sensorProcessing.computeJointPositionUsingCoupling(masterJointName, slaveJointName, scale, bias, false);
+            }
+
+            {
+               ValkyrieHandJointName slaveJoint = slaveJoints3[i];
+               YoDouble scale = sideDependentScales.get(robotSide).get(slaveJoint);
+               YoDouble bias = sideDependentBiases.get(robotSide).get(slaveJoint);
+               String masterJointName = masterJoint.getCamelCaseJointName(robotSide);
+               String slaveJointName = slaveJoint.getCamelCaseJointName(robotSide);
+               sensorProcessing.computeJointPositionUsingCoupling(masterJointName, slaveJointName, scale, bias, false);
             }
          }
       }
