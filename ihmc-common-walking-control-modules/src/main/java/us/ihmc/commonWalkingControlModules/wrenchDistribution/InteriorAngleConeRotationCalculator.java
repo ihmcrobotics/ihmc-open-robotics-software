63030fcc479425070a47c902fb6d28885c5aa795
gwiedebach <gwiedebach@ihmc.us>
2017-11-07 17:24:32 -0600

    Added option to add offset to friction cones.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/wrenchDistribution/InteriorAngleConeRotationCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/wrenchDistribution/InteriorAngleConeRotationCalculator.java
@@ -4,13 +4,19 @@ import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.YoPlaneContactSt
 
 public class InteriorAngleConeRotationCalculator implements FrictionConeRotationCalculator
 {
+   private final double offset;
+
+   public InteriorAngleConeRotationCalculator(double offset)
+   {
+      this.offset = offset;
+   }
 
    @Override
    public double computeConeRotation(YoPlaneContactState yoPlaneContactState, int contactPointIndex)
    {
       double angleOfEdge = yoPlaneContactState.getAngleOfEdgeAfterPoint(contactPointIndex);
       double interiorAngle = yoPlaneContactState.getInteriorAngle(contactPointIndex);
-      return angleOfEdge - 0.5 * interiorAngle;
+      return angleOfEdge - 0.5 * interiorAngle + offset;
    }
 
 }
