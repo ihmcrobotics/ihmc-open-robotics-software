42afab1ab57e5758d54a92bec6e906b851d00c68
Sylvain Bertrand <sbertrand@ihmc.us>
2017-11-08 13:52:53 -0600

    Moved the source up to Packet.

--- ihmc-communication/src/main/java/us/ihmc/communication/packets/TrackablePacket.java
+++ ihmc-communication/src/main/java/us/ihmc/communication/packets/TrackablePacket.java
@@ -1,19 +1,5 @@
 package us.ihmc.communication.packets;
 
-import us.ihmc.communication.ros.generators.RosIgnoredField;
-
 public abstract class TrackablePacket<T extends TrackablePacket<T>> extends Packet<T>
 {
-   @RosIgnoredField
-   public byte source = (byte) PacketDestination.BROADCAST.ordinal();
-
-   public void setSource(int source)
-   {
-      this.source = (byte) source;
-   }
-
-   public int getSource()
-   {
-      return source;
-   }
 }
