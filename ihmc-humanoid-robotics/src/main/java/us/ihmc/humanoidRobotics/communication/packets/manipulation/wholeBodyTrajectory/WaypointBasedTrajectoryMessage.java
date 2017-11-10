ecf2893891e703402a5f91562a41d40c5ac4b361
Sylvain Bertrand <sbertrand@ihmc.us>
2017-11-10 12:40:18 -0600

    Added control frame pose to trajectory message.

--- ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/manipulation/wholeBodyTrajectory/WaypointBasedTrajectoryMessage.java
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/manipulation/wholeBodyTrajectory/WaypointBasedTrajectoryMessage.java
@@ -4,7 +4,14 @@ import java.util.Arrays;
 
 import us.ihmc.communication.packets.Packet;
 import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.euclid.matrix.interfaces.RotationMatrixReadOnly;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.tuple3D.Point3D32;
+import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+import us.ihmc.euclid.tuple4D.Quaternion32;
+import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
 import us.ihmc.humanoidRobotics.communication.packets.manipulation.wholeBodyTrajectory.ConfigurationBuildOrder.ConfigurationSpaceName;
+import us.ihmc.robotics.geometry.FramePose;
 import us.ihmc.robotics.screwTheory.RigidBody;
 import us.ihmc.tools.ArrayTools;
 
@@ -18,6 +25,20 @@ public class WaypointBasedTrajectoryMessage extends Packet<WaypointBasedTrajecto
    public double[] waypointTimes;
    public Pose3D[] waypoints;
    public ConfigurationSpaceName[] unconstrainedDegreesOfFreedom;
+   /**
+    * This is the position of the control frame's origin expressed in
+    * {@code endEffector.getBodyFixedFrame()}. By default the control frame is coincident to
+    * {@code endEffector.getBodyFixedFrame()}. The control frame is assumed to be attached to the
+    * end-effector.
+    */
+   public Point3D32 controlFramePositionInEndEffector;
+   /**
+    * This is the orientation of the control frame expressed in
+    * {@code endEffector.getBodyFixedFrame()}. By default the control frame is coincident to
+    * {@code endEffector.getBodyFixedFrame()}. The control frame is assumed to be attached to the
+    * end-effector.
+    */
+   public Quaternion32 controlFrameOrientationInEndEffector;
 
    public WaypointBasedTrajectoryMessage()
    {
@@ -51,6 +72,105 @@ public class WaypointBasedTrajectoryMessage extends Packet<WaypointBasedTrajecto
    public void setUnconstrainedDegreesOfFreedom(ConfigurationSpaceName[] unconstrainedDegreesOfFreedom)
    {
       this.unconstrainedDegreesOfFreedom = unconstrainedDegreesOfFreedom;
+   }
+
+   /**
+    * Resets the control frame so it is coincident with {@code endEffector.getBodyFixedFrame()}.
+    */
+   public void resetControlFrame()
+   {
+      if (controlFramePositionInEndEffector != null)
+         controlFramePositionInEndEffector.setToZero();
+      if (controlFrameOrientationInEndEffector != null)
+         controlFrameOrientationInEndEffector.setToZero();
+   }
+
+   /**
+    * Specifies the position of the control frame's origin. The given point is assumed to be
+    * expressed in {@code endEffector.getBodyFixedFrame()}.
+    * 
+    * @param controlFramePosition the position of the control frame's origin expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    */
+   public void setControlFramePosition(Tuple3DReadOnly controlFramePosition)
+   {
+      if (controlFramePositionInEndEffector == null)
+         controlFramePositionInEndEffector = new Point3D32(controlFramePosition);
+      else
+         controlFramePositionInEndEffector.set(controlFramePosition);
+   }
+
+   /**
+    * Specifies the orientation of the control frame. The given quaternion is assumed to be
+    * expressed in {@code endEffector.getBodyFixedFrame()}.
+    * 
+    * @param controlFrameOrientation the orientation of the control frame expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    */
+   public void setControlFrameOrientation(QuaternionReadOnly controlFrameOrientation)
+   {
+      if (controlFrameOrientationInEndEffector == null)
+         controlFrameOrientationInEndEffector = new Quaternion32(controlFrameOrientation);
+      else
+         controlFrameOrientationInEndEffector.set(controlFrameOrientation);
+   }
+
+   /**
+    * Specifies the orientation of the control frame. The given quaternion is assumed to be
+    * expressed in {@code endEffector.getBodyFixedFrame()}.
+    * 
+    * @param controlFrameOrientation the orientation of the control frame expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    */
+   public void setControlFrameOrientation(RotationMatrixReadOnly controlFrameOrientation)
+   {
+      if (controlFrameOrientationInEndEffector == null)
+         controlFrameOrientationInEndEffector = new Quaternion32(controlFrameOrientation);
+      else
+         controlFrameOrientationInEndEffector.set(controlFrameOrientation);
+   }
+
+   /**
+    * Specifies the pose of the control frame. The given point and quaternion are assumed to be
+    * expressed in {@code endEffector.getBodyFixedFrame()}.
+    * 
+    * @param controlFramePosition the position of the control frame's origin expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    * @param controlFrameOrientation the orientation of the control frame expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    */
+   public void setControlFramePose(Tuple3DReadOnly controlFramePosition, QuaternionReadOnly controlFrameOrientation)
+   {
+      setControlFramePosition(controlFramePosition);
+      setControlFrameOrientation(controlFrameOrientation);
+   }
+
+   /**
+    * Specifies the pose of the control frame. The given point and quaternion are assumed to be
+    * expressed in {@code endEffector.getBodyFixedFrame()}.
+    * 
+    * @param controlFramePosition the position of the control frame's origin expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    * @param controlFrameOrientation the orientation of the control frame expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    */
+   public void setControlFramePose(Tuple3DReadOnly controlFramePosition, RotationMatrixReadOnly controlFrameOrientation)
+   {
+      setControlFramePosition(controlFramePosition);
+      setControlFrameOrientation(controlFrameOrientation);
+   }
+
+   /**
+    * Specifies the pose of the control frame. The given pose is assumed to be expressed in
+    * {@code endEffector.getBodyFixedFrame()}.
+    * 
+    * @param controlFramePose the pose of the control frame expressed in
+    *           {@code endEffector.getBodyFixedFrame()}. Not modified.
+    */
+   public void setControlFramePose(Pose3D controlFramePose)
+   {
+      setControlFramePosition(controlFramePose.getPosition());
+      setControlFrameOrientation(controlFramePose.getOrientation());
    }
 
    public long getEndEffectorNameBasedHashCode()
@@ -96,6 +216,17 @@ public class WaypointBasedTrajectoryMessage extends Packet<WaypointBasedTrajecto
    public int getNumberOfUnconstrainedDegreesOfFreedom()
    {
       return unconstrainedDegreesOfFreedom == null ? 0 : unconstrainedDegreesOfFreedom.length;
+   }
+
+   public void getControlFramePose(RigidBody endEffector, FramePose controlFramePoseToPack)
+   {
+      ReferenceFrame referenceFrame = endEffector == null ? null : endEffector.getBodyFixedFrame();
+      controlFramePoseToPack.setToZero(referenceFrame);
+
+      if (controlFramePositionInEndEffector != null)
+         controlFramePoseToPack.setPosition(controlFramePositionInEndEffector);
+      if (controlFrameOrientationInEndEffector != null)
+         controlFramePoseToPack.setOrientation(controlFrameOrientationInEndEffector);
    }
 
    @Override
