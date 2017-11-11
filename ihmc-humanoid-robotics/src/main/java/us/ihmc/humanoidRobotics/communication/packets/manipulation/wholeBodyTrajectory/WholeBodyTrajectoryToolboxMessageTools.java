b409c3d953a6e09fbf4625caf2fb82aebe55ba52
Sylvain Bertrand <sbertrand@ihmc.us>
2017-11-11 17:27:00 -0600

    Added method to create a FunctionTrajectory from a message.

--- ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/manipulation/wholeBodyTrajectory/WholeBodyTrajectoryToolboxMessageTools.java
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/manipulation/wholeBodyTrajectory/WholeBodyTrajectoryToolboxMessageTools.java
@@ -1,5 +1,6 @@
 package us.ihmc.humanoidRobotics.communication.packets.manipulation.wholeBodyTrajectory;
 
+import us.ihmc.commons.MathTools;
 import us.ihmc.euclid.geometry.Pose3D;
 import us.ihmc.humanoidRobotics.communication.packets.manipulation.wholeBodyTrajectory.ConfigurationBuildOrder.ConfigurationSpaceName;
 import us.ihmc.robotics.screwTheory.RigidBody;
@@ -9,6 +10,39 @@ public class WholeBodyTrajectoryToolboxMessageTools
    public static interface FunctionTrajectory
    {
       public Pose3D compute(double time);
+   }
+
+   public static FunctionTrajectory createFunctionTrajectory(WaypointBasedTrajectoryMessage message)
+   {
+      return new FunctionTrajectory()
+      {
+         @Override
+         public Pose3D compute(double time)
+         {
+            Pose3D current = new Pose3D();
+
+            Pose3D previous = null;
+            Pose3D next = null;
+            double t0 = Double.NaN;
+            double tf = Double.NaN;
+
+            for (int i = 1; i < message.getNumberOfWaypoints(); i++)
+            {
+               t0 = message.getWaypointTime(i - 1);
+               tf = message.getWaypointTime(i);
+               previous = message.getWaypoint(i - 1);
+               next = message.getWaypoint(i);
+               if (time < message.getWaypointTime(i))
+                  break;
+            }
+
+            double alpha = (time - t0) / (tf - t0);
+            alpha = MathTools.clamp(alpha, 0.0, 1.0);
+            current.interpolate(previous, next, alpha);
+
+            return current;
+         }
+      };
    }
 
    public static WaypointBasedTrajectoryMessage createTrajectoryMessage(RigidBody endEffector, double t0, double tf, double timeResolution,
