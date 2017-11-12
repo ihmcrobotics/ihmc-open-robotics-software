f0011fc9d461167d5ae9aaefd736c7b6378e425e
Robert <rgriffin@ihmc.us>
2017-11-12 17:41:17 -0500

    started setting up a push over gaps test

---
+++ atlas/src/test/java/us/ihmc/atlas/roughTerrainWalking/AtlasPushRecoveryOverGapTest.java
@@ -0,0 +1,78 @@
+package us.ihmc.atlas.roughTerrainWalking;
+
+import org.junit.Test;
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.atlas.parameters.AtlasSimpleICPOptimizationParameters;
+import us.ihmc.atlas.parameters.AtlasWalkingControllerParameters;
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.roughTerrainWalking.AvatarPushRecoveryOverGapTest;
+import us.ihmc.avatar.roughTerrainWalking.AvatarSwingOverPlanarRegionsTest;
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.commonWalkingControlModules.instantaneousCapturePoint.icpOptimization.ICPOptimizationParameters;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.continuousIntegration.IntegrationCategory;
+import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+
+@ContinuousIntegrationPlan(categories = {IntegrationCategory.FAST, IntegrationCategory.VIDEO})
+public class AtlasPushRecoveryOverGapTest extends AvatarPushRecoveryOverGapTest
+{
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 30.0)
+   @Test(timeout = 70000)
+   public void testNoPush() throws SimulationExceededMaximumTimeException
+   {
+      super.testNoPush();
+   }
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      AtlasRobotModel atlasRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false)
+      {
+         @Override
+         public WalkingControllerParameters getWalkingControllerParameters()
+         {
+            return new AtlasWalkingControllerParameters(RobotTarget.SCS, getJointMap(), getContactPointParameters())
+            {
+               @Override
+               public boolean useOptimizationBasedICPController()
+               {
+                  return true;
+               }
+
+               @Override
+               public ICPOptimizationParameters getICPOptimizationParameters()
+               {
+                  return new AtlasSimpleICPOptimizationParameters(false)
+                  {
+                     @Override
+                     public boolean useAngularMomentum()
+                     {
+                        return true;
+                     }
+
+                     @Override
+                     public boolean useStepAdjustment()
+                     {
+                        return true;
+                     }
+                  };
+               }
+            };
+
+         }
+      };
+
+      return atlasRobotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.ATLAS);
+   }
+}
