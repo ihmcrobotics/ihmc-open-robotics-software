fcee9fd53d794c1f6310aa1189d790b896995733
InhoLee <ilee@ihmc.us>
2017-11-13 17:00:54 -0600

    Paper about joint limit score is added.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/rrtToolboxModule/WholeBodyTrajectoryToolboxHelper.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/rrtToolboxModule/WholeBodyTrajectoryToolboxHelper.java
@@ -24,6 +24,16 @@ public class WholeBodyTrajectoryToolboxHelper
       return Arrays.stream(jointsToScore).mapToDouble(j -> jointLimitScore(j)).sum();
    }
 
+   /**
+    * This joint limit score is defined in the following paper (proposed by Yoshikawa).
+    * <p>
+    * Nelson, Khosla, "Strategies for increasing the tracking region of an eye-in-hand system by singularity and joint limit avoidance.", The International journal of robotics research 14.3 (1995): 255-269.
+    * <p>
+    * See {@link <p> http://repository.cmu.edu/cgi/viewcontent.cgi?article=1581&context=isr}
+    * <p>
+    * See equation 24, 26. Joint limit score is stated on the head of natural exponential.
+    * 
+    */
    public static double jointLimitScore(OneDoFJoint jointToScore)
    {
       double q = jointToScore.getQ();
@@ -35,7 +45,7 @@ public class WholeBodyTrajectoryToolboxHelper
       double diffUpper = upperLimit - q;
       double diffLower = q - lowerLimit;
 
-      // Yoshikawa. TODO Add the reference of the paper
+      // Yoshikawa's definition.
       return diffUpper * diffLower / (motionRange * motionRange);
    }
 }
