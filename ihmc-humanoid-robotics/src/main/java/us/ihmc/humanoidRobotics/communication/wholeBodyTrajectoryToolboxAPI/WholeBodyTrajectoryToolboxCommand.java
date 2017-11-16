1d457dcccfdd085e9f33394258620ab52e3340b0
InhoLee <ilee@ihmc.us>
2017-11-16 09:42:33 -0600

    Add set method to be used in CommandInputManager.

--- ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/wholeBodyTrajectoryToolboxAPI/WholeBodyTrajectoryToolboxCommand.java
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/wholeBodyTrajectoryToolboxAPI/WholeBodyTrajectoryToolboxCommand.java
@@ -2,13 +2,18 @@ package us.ihmc.humanoidRobotics.communication.wholeBodyTrajectoryToolboxAPI;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import us.ihmc.commons.PrintTools;
 import us.ihmc.communication.controllerAPI.command.Command;
 import us.ihmc.communication.controllerAPI.command.MultipleCommandHolder;
+import us.ihmc.humanoidRobotics.communication.packets.manipulation.wholeBodyTrajectory.WaypointBasedTrajectoryMessage;
 import us.ihmc.humanoidRobotics.communication.packets.manipulation.wholeBodyTrajectory.WholeBodyTrajectoryToolboxMessage;
 import us.ihmc.robotics.lists.RecyclingArrayList;
+import us.ihmc.robotics.screwTheory.RigidBody;
+import us.ihmc.sensorProcessing.frames.ReferenceFrameHashCodeResolver;
 
-public class WholeBodyTrajectoryToolboxCommand implements MultipleCommandHolder<WholeBodyTrajectoryToolboxCommand, WholeBodyTrajectoryToolboxMessage>
+public class WholeBodyTrajectoryToolboxCommand implements MultipleCommandHolder<WholeBodyTrajectoryToolboxCommand, WholeBodyTrajectoryToolboxMessage>, WholeBodyTrajectoryToolboxAPI<WholeBodyTrajectoryToolboxMessage>
 {
    private boolean hasConfiguration = false;
    private final WholeBodyTrajectoryToolboxConfigurationCommand configuration = new WholeBodyTrajectoryToolboxConfigurationCommand();
@@ -81,8 +86,37 @@ public class WholeBodyTrajectoryToolboxCommand implements MultipleCommandHolder<
    }
 
    @Override
+   public void set(WholeBodyTrajectoryToolboxMessage message, Map<Long, RigidBody> rigidBodyNamedBasedHashMap,
+                   ReferenceFrameHashCodeResolver referenceFrameResolver)
+   {
+      clear();
+      
+      hasConfiguration = message.getConfiguration() != null;
+      if (hasConfiguration)
+      {
+         configuration.set(message.getConfiguration());
+         commands.add(configuration);
+      }
+
+      for (int i = 0; i < message.getEndEffectorTrajectories().size(); i++)
+      {
+         WaypointBasedTrajectoryCommand endEffectorTrajectory = endEffectorTrajectories.add();
+         endEffectorTrajectory.set(message.getEndEffectorTrajectories().get(i), rigidBodyNamedBasedHashMap, referenceFrameResolver);
+         commands.add(endEffectorTrajectory);
+      }
+      
+      for (int i = 0; i < message.getExplorationConfigurations().size(); i++)
+      {
+         RigidBodyExplorationConfigurationCommand rigidBodyExplorationConfiguration = rigidBodyExplorationConfigurations.add();
+         rigidBodyExplorationConfiguration.set(message.getExplorationConfigurations().get(i), rigidBodyNamedBasedHashMap, referenceFrameResolver);
+         commands.add(rigidBodyExplorationConfiguration);
+      }
+   }
+      
+   @Override
    public void parseCommandIdToChildren()
    {
+      
    }
 
    @Override
@@ -122,4 +156,5 @@ public class WholeBodyTrajectoryToolboxCommand implements MultipleCommandHolder<
    {
       return rigidBodyExplorationConfigurations;
    }
+
 }
