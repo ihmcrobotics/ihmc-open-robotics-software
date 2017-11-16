8d34aade59807c86c06f5f19a61d41e76173b6b8
InhoLee <ilee@ihmc.us>
2017-11-16 13:41:19 -0600

    Methods to get random configuration and time space are added.

--- ihmc-manipulation-planning/src/main/java/us/ihmc/manipulation/planning/rrt/constrainedplanning/configurationAndTimeSpace/CTTreeTools.java
+++ ihmc-manipulation-planning/src/main/java/us/ihmc/manipulation/planning/rrt/constrainedplanning/configurationAndTimeSpace/CTTreeTools.java
@@ -7,11 +7,10 @@ import us.ihmc.robotics.robotSide.RobotSide;
 public class CTTreeTools
 {
    /*
-    * 50 okay . why 1 has problem?
-    * angular velocity problem on chest.
+    * 50 okay . why 1 has problem? angular velocity problem on chest.
     */
    static Random randomManager = new Random(3);
-   
+
    public static void setRandomNormalizedNodeData(CTTaskNode node, boolean isUniform, double treeReachingTime)
    {
       for (int i = 0; i < node.getDimensionOfNodeData(); i++)
@@ -19,16 +18,16 @@ public class CTTreeTools
    }
 
    public static void setRandomNormalizedNodeData(CTTaskNode node, int index, boolean isUniform, double treeReachingTime)
-   {  
+   {
       setRandomNormalizedNodeData(node, randomManager, index, isUniform, treeReachingTime);
    }
 
    public static void setRandomNormalizedNodeData(CTTaskNode node, Random randomManager, int index, boolean isUniform, double treeReachingTime)
    {
       //double exceedIntentionalTimeRatio = 3.0;
-       double exceedIntentionalTimeRatio = 3.0 * treeReachingTime;
+      double exceedIntentionalTimeRatio = 3.0 * treeReachingTime;
       // double exceedIntentionalTimeRatio = 3.0 * treeReachingTime;
-      
+
       double exceedIntentionalRatio = 0.5;
 
       if (isUniform)
@@ -54,10 +53,46 @@ public class CTTreeTools
       node.setNormalizedNodeData(index, value);
    }
 
+   public static void updateRandomConfigurationData(double randomConfigurationData, double lowerLimit, double upperLimit, boolean isUniform)
+   {
+      double exceedIntentionalRatio = 0.5;
+
+      if (isUniform)
+         exceedIntentionalRatio = 0.0;
+      else
+         exceedIntentionalRatio = 1.0;
+
+      double value;
+
+      value = randomManager.nextDouble() * (1.0 + exceedIntentionalRatio);
+      value = value - 0.5 * exceedIntentionalRatio;
+
+      if (value >= 1)
+         value = 1;
+      if (value <= 0)
+         value = 0;
+
+      randomConfigurationData = lowerLimit + value*(upperLimit-lowerLimit);
+   }
+
+   public static void updateRandomTimeData(double randomTimeData, double trajectoryTime, double treeReachingTime)
+   {
+      double exceedIntentionalTimeRatio = 3.0 * treeReachingTime;
+
+      double value;
+
+      value = randomManager.nextDouble() * (1.0 + exceedIntentionalTimeRatio);
+
+      if (value > 1)
+         value = 1;
+
+      randomTimeData = value * trajectoryTime;
+   }
+
    public static ConfigurationSpace getConfigurationSpace(CTTaskNode node, RobotSide robotSide)
    {
       ConfigurationSpace configurationSpace = new ConfigurationSpace();
-      if(robotSide == RobotSide.LEFT)
+      if (robotSide == RobotSide.LEFT)
       {
          configurationSpace.setTranslation(node.getNodeData(5), node.getNodeData(6), node.getNodeData(7));
          configurationSpace.setRotation(node.getNodeData(8), node.getNodeData(9), node.getNodeData(10));
@@ -67,7 +102,7 @@ public class CTTreeTools
          configurationSpace.setTranslation(node.getNodeData(11), node.getNodeData(12), node.getNodeData(13));
          configurationSpace.setRotation(node.getNodeData(14), node.getNodeData(15), node.getNodeData(16));
       }
-      
+
       return configurationSpace;
    }
 }
