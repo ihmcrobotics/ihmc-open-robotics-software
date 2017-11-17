76aab791c2affbf4153854b873f1df22140bcd9c
Stephen McCrory <smccrory@ihmc.us>
2017-11-17 12:38:14 -0600

    Adding two parameters to enable best-effort planning when the goal node isn't achievable

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/FootstepPlannerParameters.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/FootstepPlannerParameters.java
@@ -289,22 +289,24 @@ public interface FootstepPlannerParameters
    }
 
    /**
-    * The maximum number of steps that the planner can return in its {@link FootstepPlan}
-    */
-   public default int getMaximumStepsInPlan()
-   {
-      return Integer.MAX_VALUE;
-   }
-
-   /**
     * When the planner is done planning and cannot find a path to the goal, this flag indicates whether the
     * planner should return the best plan that it found. If this value is false, the planner will return
     * a {@link FootstepPlan} of type {@link FootstepPlanningResult#NO_PATH_EXISTS}. Otherwise it will return
-    * the "best" effort plan, where "best" is determined by the planner.
+    * the "best" effort plan, where the plan is at least {@link #getMinimumStepsForBestEffortPlan()} steps long
+    * "best" is determined by the planner.
     */
    public default boolean getReturnBestEffortPlan()
    {
       return false;
+   }
+
+   /**
+    * When {@link #getReturnBestEffortPlan()} is true, the planner will return the best effort plan if the plan
+    * contains at least this many footsteps.
+    */
+   public default int getMinimumStepsForBestEffortPlan()
+   {
+      return 0;
    }
 
    /**
