0ee10539f71ed94ced8912f02b981498d4f15c2e
ValkyrieOCU <operatorcomputer@ihmc.us>
2017-11-18 16:28:27 -0600

    Updated the gains for the acceleration integration.

--- valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieWalkingPositionControlParameters.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieWalkingPositionControlParameters.java
@@ -6,33 +6,65 @@ import java.util.Map;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.PositionControlParameters;
 import us.ihmc.robotics.partNames.ArmJointName;
 import us.ihmc.robotics.partNames.LegJointName;
+import us.ihmc.robotics.partNames.SpineJointName;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.valkyrie.parameters.ValkyrieJointMap;
 import us.ihmc.valkyrie.parameters.ValkyrieOrderedJointMap;
 
 public class ValkyrieWalkingPositionControlParameters implements PositionControlParameters
 {
+   private final Map<String, Double> positionGains = new HashMap<>();
    private final Map<String, Double> derivativeGains = new HashMap<>();
+   private final ValkyrieJointMap jointMap;
 
    public ValkyrieWalkingPositionControlParameters(ValkyrieJointMap jointMap)
    {
-      double legKd = 4.75;
-      double armKd = 6.00;
+      this.jointMap = jointMap;
 
       for (RobotSide robotSide : RobotSide.values)
       {
-         derivativeGains.put(jointMap.getLegJointName(robotSide, LegJointName.HIP_YAW), legKd);
-         derivativeGains.put(jointMap.getLegJointName(robotSide, LegJointName.HIP_ROLL), legKd);
-         derivativeGains.put(jointMap.getLegJointName(robotSide, LegJointName.HIP_PITCH), legKd);
-         derivativeGains.put(jointMap.getLegJointName(robotSide, LegJointName.KNEE_PITCH), legKd);
-         derivativeGains.put(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_PITCH), legKd);
-         derivativeGains.put(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_ROLL), legKd);
+         setGains(robotSide, LegJointName.HIP_YAW, 30.0, 3.0);
+         setGains(robotSide, LegJointName.HIP_ROLL, 30.0, 3.0);
+         setGains(robotSide, LegJointName.HIP_PITCH, 30.0, 3.0);
+         setGains(robotSide, LegJointName.KNEE_PITCH, 30.0, 4.0);
+         setGains(robotSide, LegJointName.ANKLE_PITCH, 60.0, 6.0);
+         setGains(robotSide, LegJointName.ANKLE_ROLL, 60.0, 6.0);
 
-         derivativeGains.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), armKd);
-         derivativeGains.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), armKd);
-         derivativeGains.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), armKd);
-         derivativeGains.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), armKd);
+         setGains(robotSide, ArmJointName.SHOULDER_PITCH, 30.0, 2.0);
+         setGains(robotSide, ArmJointName.SHOULDER_ROLL, 30.0, 2.0);
+         setGains(robotSide, ArmJointName.SHOULDER_YAW, 30.0, 2.0);
+         setGains(robotSide, ArmJointName.ELBOW_PITCH, 30.0, 2.0);
+         setGains(robotSide, ArmJointName.ELBOW_ROLL, 7.0, 0.0);
+         setGains(robotSide, ArmJointName.WRIST_ROLL, 20.0, 0.5);
+         setGains(robotSide, ArmJointName.FIRST_WRIST_PITCH, 20.0, 0.5);
       }
+      setGains(SpineJointName.SPINE_YAW, 50.0, 1.0);
+      setGains(SpineJointName.SPINE_PITCH, 50.0, 1.0);
+      setGains(SpineJointName.SPINE_ROLL, 50.0, 1.0);
+   }
+   
+   private void setGains(SpineJointName spineJointName, double kp, double kd)
+   {
+      String jointName = jointMap.getSpineJointName(spineJointName);
+      setGains(jointName, kp, kd);
+   }
+
+   private void setGains(RobotSide robotSide, LegJointName legJointName, double kp, double kd)
+   {
+      String jointName = jointMap.getLegJointName(robotSide, legJointName);
+      setGains(jointName, kp, kd);
+   }
+
+   private void setGains(RobotSide robotSide, ArmJointName armJointName, double kp, double kd)
+   {
+      String jointName = jointMap.getArmJointName(robotSide, armJointName);
+      setGains(jointName, kp, kd);
+   }
+
+   private void setGains(String jointName, double kp, double kd)
+   {
+      positionGains.put(jointName, kp);
+      derivativeGains.put(jointName, kd);
    }
 
    @Override
@@ -59,7 +91,10 @@ public class ValkyrieWalkingPositionControlParameters implements PositionControl
    @Override
    public double getProportionalGain(String jointName)
    {
-      return 0;
+      if (positionGains.containsKey(jointName))
+         return positionGains.get(jointName);
+      else
+         return 0;
    }
 
    @Override
