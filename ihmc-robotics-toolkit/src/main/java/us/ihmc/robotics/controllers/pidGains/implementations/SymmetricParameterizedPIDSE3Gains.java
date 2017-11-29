54d57b38b3e628452e77a4200af1ae32336769bf
Jesper Smith <jsmith@ihmc.us>
2017-11-29 09:14:57 -0600

    Fixed symetric gains to match YoVariable version

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/controllers/pidGains/implementations/SymmetricParameterizedPIDSE3Gains.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/controllers/pidGains/implementations/SymmetricParameterizedPIDSE3Gains.java
@@ -1,13 +1,27 @@
 package us.ihmc.robotics.controllers.pidGains.implementations;
 
 import us.ihmc.robotics.controllers.pidGains.GainCoupling;
+import us.ihmc.robotics.controllers.pidGains.PID3DGainsReadOnly;
+import us.ihmc.robotics.controllers.pidGains.PIDSE3GainsReadOnly;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
-public class SymmetricParameterizedPIDSE3Gains extends ParameterizedPIDSE3Gains
+public class SymmetricParameterizedPIDSE3Gains extends ParameterizedPID3DGains implements PIDSE3GainsReadOnly
 {
    public SymmetricParameterizedPIDSE3Gains(String suffix, boolean useIntegrator, YoVariableRegistry registry)
    {
-      super(suffix, GainCoupling.XYZ, GainCoupling.XYZ, useIntegrator, useIntegrator, registry);
+      super(suffix, GainCoupling.XYZ, useIntegrator, registry);
+   }
+
+   @Override
+   public PID3DGainsReadOnly getPositionGains()
+   {
+      return this;
+   }
+
+   @Override
+   public PID3DGainsReadOnly getOrientationGains()
+   {
+      return this;
    }
 
 }
