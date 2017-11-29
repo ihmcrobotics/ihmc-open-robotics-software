758ed1583fa75f3a79d3a74fa1d6457afd4ed49b
Jesper Smith <jsmith@ihmc.us>
2017-11-29 09:05:15 -0600

    Added function to SCS to pre-select the parameter file that gets loaded, making development way faster.

--- simulation-construction-set/src/main/java/us/ihmc/simulationconstructionset/ParameterRootNamespaceHolder.java
+++ simulation-construction-set/src/main/java/us/ihmc/simulationconstructionset/ParameterRootNamespaceHolder.java
@@ -1,8 +1,11 @@
 package us.ihmc.simulationconstructionset;
 
+import java.net.URL;
+
 import us.ihmc.yoVariables.registry.NameSpace;
 
 public interface ParameterRootNamespaceHolder
 {
    public NameSpace getParameterRootPath();
+   public URL getDefaultParameterFile();
 }
