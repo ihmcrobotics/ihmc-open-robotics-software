22038ffd94f26ff6869951027bbfadba9d1c3abb
gwiedebach <gwiedebach@ihmc.us>
2017-12-05 12:18:45 -0600

    Added the parameters to the data structure.

--- ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/outputData/JointDesiredOutputReadOnly.java
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/outputData/JointDesiredOutputReadOnly.java
@@ -5,7 +5,7 @@ package us.ihmc.sensorProcessing.outputData;
  * (setpoints and controller properties) from a whole body controller to joint level
  * controllers.
  */
-public interface JointDesiredOutputReadOnly
+public abstract class JointDesiredOutputReadOnly
 {
    public abstract boolean hasDesiredTorque();
    public abstract boolean hasDesiredPosition();
@@ -78,4 +78,86 @@ public interface JointDesiredOutputReadOnly
     * from the whole body controller to the joint control level.
     */
    public abstract double getMasterGain();
+
+   /**
+    * Returns true if a velocity scaling was set for this joint.
+    * @see #getVelocityScaling()
+    */
+   public abstract boolean hasVelocityScaling();
+
+   /**
+    * <p>
+    * This allows to specify a desired velocity scaling for the joint level controller. In the
+    * simplest for the joint control law contains a damping / velocity term that looks like this:</br>
+    * damping * (velocityScaling * qd_d - qd)
+    * </p>
+    * By default this parameter should be set to 1.0 but can be set to a value between 0.0 and 1.0.
+    * If set to zero the velocity tracking of this joint will be deactivated and the joint damping
+    * will simulate viscous friction. If set to one the damping term will attempt to track the desired
+    * velocity.
+    */
+   public abstract double getVelocityScaling();
+
+   /**
+    * Returns true if a leak rate for the integration of the desired acceleration to a desired
+    * velocity was set for this joint.
+    * @see #getVelocityIntegrationLeakRate()
+    */
+   public abstract boolean hasVelocityIntegrationLeakRate();
+
+   /**
+    * <p>
+    * If the integration of desired accelerations is handled on the joint level this value allows
+    * to specify a leak rate for the integration of the desired acceleration to a desired velocity.
+    * In its simplest form the integration equation might look like this:</br>
+    * qd_desired(T+dt) = velocityLeak * qd(T) + dt * qdd_desired
+    * </p>
+    * The value of this parameter needs to be between 0.0 and 1.0.
+    */
+   public abstract double getVelocityIntegrationLeakRate();
+
+   /**
+    * Returns true if a leak rate for the integration of the desired acceleration to a desired
+    * position was set for this joint.
+    * @see #getPositionIntegrationLeakRate()
+    */
+   public abstract boolean hasPositionIntegrationLeakRate();
+
+   /**
+    * <p>
+    * If the integration of desired accelerations is handled on the joint level this value allows
+    * to specify a leak rate for the integration of the desired acceleration and the desired
+    * velocity to a desired position. In its simplest form the integration equation might look like
+    * this:</br>
+    * q_desired(T+dt) = leakPosition * (q_desired(T) + dt * qd_desired(T)) + (1.0 - leakPosition) * q(T)</br>
+    * Here, the desired position is leaking towards the actual (measured) joint position.
+    * </p>
+    * The value of this parameter needs to be between 0.0 and 1.0.
+    */
+   public abstract double getPositionIntegrationLeakRate();
+
+   @Override
+   public String toString()
+   {
+      String ret = "Joint Desired Output:\n";
+      if (hasControlMode())
+         ret += "controlMode = " + getControlMode() + "\n";
+      if (hasDesiredTorque())
+         ret += "desiredTorque = " + getDesiredTorque() + "\n";
+      if (hasDesiredPosition())
+         ret += "desiredPosition = " + getDesiredPosition() + "\n";
+      if (hasDesiredVelocity())
+         ret += "desiredVelocity = " + getDesiredVelocity() + "\n";
+      if (hasDesiredAcceleration())
+         ret += "desiredAcceleration = " + getDesiredAcceleration() + "\n";
+      if (hasMasterGain())
+         ret += "masterGain = " + getMasterGain() + "\n";
+      if (hasVelocityScaling())
+         ret += "velocityScaling = " + getVelocityScaling() + "\n";
+      if (hasVelocityIntegrationLeakRate())
+         ret += "velocityIntegrationLeakRate = " + getVelocityIntegrationLeakRate() + "\n";
+      if (hasPositionIntegrationLeakRate())
+         ret += "positionIntegrationLeakRate = " + getPositionIntegrationLeakRate() + "\n";
+      return ret;
+   }
 }
