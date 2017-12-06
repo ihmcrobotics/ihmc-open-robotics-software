74180b984387824f10597e4517e8d7feb7e773d8
gwiedebach <gwiedebach@ihmc.us>
2017-12-06 17:43:02 -0600

    Some helper methods in the DRCRobotJointMap.

--- ihmc-whole-body-controller/src/main/java/us/ihmc/wholeBodyController/DRCRobotJointMap.java
+++ ihmc-whole-body-controller/src/main/java/us/ihmc/wholeBodyController/DRCRobotJointMap.java
@@ -1,11 +1,11 @@
 package us.ihmc.wholeBodyController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import us.ihmc.robotics.controllers.YoPDGains;
-import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.robotics.partNames.ArmJointName;
 import us.ihmc.robotics.partNames.HumanoidJointNameMap;
 import us.ihmc.robotics.partNames.LegJointName;
@@ -13,6 +13,7 @@ import us.ihmc.robotics.partNames.NeckJointName;
 import us.ihmc.robotics.partNames.SpineJointName;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
 public interface DRCRobotJointMap extends HumanoidJointNameMap
 {
@@ -35,4 +36,64 @@ public interface DRCRobotJointMap extends HumanoidJointNameMap
    public abstract String[] getPositionControlledJointsForSimulation();
 
    public List<ImmutablePair<String, YoPDGains>> getPassiveJointNameWithGains(YoVariableRegistry registry);
+
+   public default List<String> getNeckJointNamesAsStrings()
+   {
+      List<String> neckJointNames = new ArrayList<>();
+      for (NeckJointName jointName : getNeckJointNames())
+      {
+         neckJointNames.add(getNeckJointName(jointName));
+      }
+      return neckJointNames;
+   }
+
+   public default List<String> getSpineJointNamesAsStrings()
+   {
+      List<String> spineJointNames = new ArrayList<>();
+      for (SpineJointName jointName : getSpineJointNames())
+      {
+         spineJointNames.add(getSpineJointName(jointName));
+      }
+      return spineJointNames;
+   }
+
+   public default List<String> getArmJointNamesAsStrings()
+   {
+      List<String> armJointNames = new ArrayList<>();
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         armJointNames.addAll(getArmJointNamesAsStrings(robotSide));
+      }
+      return armJointNames;
+   }
+
+   public default List<String> getArmJointNamesAsStrings(RobotSide robotSide)
+   {
+      List<String> armJointNames = new ArrayList<>();
+      for (ArmJointName jointName : getArmJointNames())
+      {
+         armJointNames.add(getArmJointName(robotSide, jointName));
+      }
+      return armJointNames;
+   }
+
+   public default List<String> getLegJointNamesAsStrings()
+   {
+      List<String> legJointNames = new ArrayList<>();
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         legJointNames.addAll(getLegJointNamesAsStrings(robotSide));
+      }
+      return legJointNames;
+   }
+
+   public default List<String> getLegJointNamesAsStrings(RobotSide robotSide)
+   {
+      List<String> legJointNames = new ArrayList<>();
+      for (LegJointName jointName : getLegJointNames())
+      {
+         legJointNames.add(getLegJointName(robotSide, jointName));
+      }
+      return legJointNames;
+   }
 }
