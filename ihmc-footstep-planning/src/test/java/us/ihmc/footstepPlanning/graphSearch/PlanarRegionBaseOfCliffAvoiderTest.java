dbdef99ef834d1dd3a78de609579aa81b768bb78
Stephen McCrory <smccrory@ihmc.us>
2017-12-06 17:31:34 -0600

    adding more complex unit test for cliff avoidance

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/graphSearch/PlanarRegionBaseOfCliffAvoiderTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/graphSearch/PlanarRegionBaseOfCliffAvoiderTest.java
@@ -10,19 +10,24 @@ import us.ihmc.commons.MutationTestFacilitator;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.continuousIntegration.IntegrationCategory;
+import us.ihmc.euclid.Axis;
+import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.tools.AxisAngleTools;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.footstepPlanning.DefaultFootstepPlanningParameters;
 import us.ihmc.footstepPlanning.graphSearch.footstepSnapping.PlanarRegionBaseOfCliffAvoider;
 import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
+import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNodeTools;
 import us.ihmc.footstepPlanning.testTools.PlanningTestTools;
 import us.ihmc.graphicsDescription.Graphics3DObject;
 import us.ihmc.graphicsDescription.appearance.YoAppearance;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
-import us.ihmc.robotics.geometry.PlanarRegion;
-import us.ihmc.robotics.geometry.PlanarRegionsList;
-import us.ihmc.robotics.geometry.PlanarRegionsListGenerator;
+import us.ihmc.robotics.geometry.*;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.simulationconstructionset.Robot;
@@ -30,14 +35,17 @@ import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
+import java.util.Random;
+
 @ContinuousIntegrationAnnotations.ContinuousIntegrationPlan(categories = IntegrationCategory.IN_DEVELOPMENT)
 public class PlanarRegionBaseOfCliffAvoiderTest
 {
    private final boolean visualize = false;
    private final boolean doAsserts = true;
+   private final Random random = new Random(4587L);
 
    @ContinuousIntegrationTest(estimatedDuration = 0.0)
-   @Test(timeout = 300000000)
+   @Test(timeout = 30000)
    public void testBaseOfCliffAvoiderWithSimpleQueriesOnABlock()
    {
       double stepHeight = 0.2;
@@ -108,7 +116,77 @@ public class PlanarRegionBaseOfCliffAvoiderTest
       assertTrue(avoider.isNodeValid(node, null));
    }
 
-   public static void main(String[] args)
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 3000000)
+   public void testAvoidingRotatedAndElevatedCliff()
+   {
+      YoVariableRegistry registry = new YoVariableRegistry("TestRegistry");
+      double cliffHeightToAvoid = 0.2;
+      double minimumDistanceFromCliffBottom = 0.1;
+      double footLength = 0.2;
+      double footWidth = 0.1;
+
+      YoFootstepPlannerParameters parameters = new YoFootstepPlannerParameters(registry, new DefaultFootstepPlanningParameters()
+      {
+         @Override
+         public double getCliffHeightToAvoid()
+         {
+            return cliffHeightToAvoid;
+         }
+
+         @Override
+         public double getMinimumDistanceFromCliffBottoms()
+         {
+            return minimumDistanceFromCliffBottom;
+         }
+      });
+
+      SideDependentList<ConvexPolygon2D> footPolygons = PlanningTestTools.createFootPolygons(footLength, footWidth);
+      PlanarRegionBaseOfCliffAvoider cliffAvoider = new PlanarRegionBaseOfCliffAvoider(parameters, footPolygons);
+
+      double centerX = 1.0;
+      double centerY = 1.0;
+      double groundHeight = 0.5;
+      double cliffHeight = cliffHeightToAvoid + 0.1;
+      double rotation = 0.25 * Math.PI;
+
+      PlanarRegionsListGenerator generator = new PlanarRegionsListGenerator();
+      generator.addRectangle(5.0, 5.0);
+      generator.translate(centerX, centerY, groundHeight + cliffHeight);
+      generator.rotate(rotation, Axis.Z);
+      double boxWidth = 0.5;
+      generator.addRectangle(boxWidth, boxWidth);
+
+      PlanarRegionsList planarRegionsList = generator.getPlanarRegionsList();
+      cliffAvoider.setPlanarRegions(planarRegionsList);
+
+      Vector2D frontNearNodeOffset = new Vector2D(0.5 * boxWidth + minimumDistanceFromCliffBottom + 0.5 * footLength - FootstepNode.gridSizeXY, 0.0);
+      Vector2D frontFarNodeOffset = new Vector2D(0.5 * boxWidth + minimumDistanceFromCliffBottom + 0.5 * footLength + FootstepNode.gridSizeXY, 0.0);
+      Vector2D sideNearNodeOffset = new Vector2D(0.0, 0.5 * boxWidth + 0.5 * footWidth + minimumDistanceFromCliffBottom - FootstepNode.gridSizeXY);
+      Vector2D sideFarNodeOffset = new Vector2D(0.0, 0.5 * boxWidth + 0.5 * footWidth + minimumDistanceFromCliffBottom + FootstepNode.gridSizeXY);
+
+      new AxisAngle(rotation, 0.0, 0.0).transform(frontNearNodeOffset);
+      new AxisAngle(rotation, 0.0, 0.0).transform(frontFarNodeOffset);
+      new AxisAngle(rotation, 0.0, 0.0).transform(sideNearNodeOffset);
+      new AxisAngle(rotation, 0.0, 0.0).transform(sideFarNodeOffset);
+
+      frontNearNodeOffset.add(centerX, centerY);
+      frontFarNodeOffset.add(centerX, centerY);
+      sideNearNodeOffset.add(centerX, centerY);
+      sideFarNodeOffset.add(centerX, centerY);
+
+      FootstepNode frontNearNode = new FootstepNode(frontNearNodeOffset.getX(), frontNearNodeOffset.getY(), rotation, RobotSide.generateRandomRobotSide(random));
+      FootstepNode frontFarNode = new FootstepNode(frontFarNodeOffset.getX(), frontFarNodeOffset.getY(), rotation, RobotSide.generateRandomRobotSide(random));
+      FootstepNode sideNearNode = new FootstepNode(sideNearNodeOffset.getX(), sideNearNodeOffset.getY(), rotation, RobotSide.generateRandomRobotSide(random));
+      FootstepNode sideFarNode = new FootstepNode(sideFarNodeOffset.getX(), sideFarNodeOffset.getY(), rotation, RobotSide.generateRandomRobotSide(random));
+
+      assertFalse(cliffAvoider.isNodeValid(frontNearNode, null));
+      assertFalse(cliffAvoider.isNodeValid(sideNearNode, null));
+      assertTrue(cliffAvoider.isNodeValid(frontFarNode, null));
+      assertTrue(cliffAvoider.isNodeValid(sideFarNode, null));
+   }
+
+      public static void main(String[] args)
    {
       MutationTestFacilitator.facilitateMutationTestForClass(PlanarRegionBaseOfCliffAvoider.class, PlanarRegionBaseOfCliffAvoiderTest.class);
    }
