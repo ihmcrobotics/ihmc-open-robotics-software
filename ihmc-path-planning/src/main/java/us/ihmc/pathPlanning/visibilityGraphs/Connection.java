51dc2f3814a23bb1bdc5d68b6e6aefc4b0f89bce
Sylvain Bertrand <sbertrand@ihmc.us>
2017-12-12 12:46:41 -0600

    Switched to use Point3DReadOnly.

--- ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/Connection.java
+++ ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/Connection.java
@@ -1,16 +1,17 @@
 package us.ihmc.pathPlanning.visibilityGraphs;
 
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 
 public class Connection
 {
    private Point3D point1;
    private Point3D point2;
 
-   public Connection(Point3D point1, Point3D point2)
+   public Connection(Point3DReadOnly point1, Point3DReadOnly point2)
    {
-      this.point1 = point1;
-      this.point2 = point2;
+      this.point1 = new Point3D(point1);
+      this.point2 = new Point3D(point2);
    }
 
    public Point3D getSourcePoint()
