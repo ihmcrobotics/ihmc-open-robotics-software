bb6306a465703c61fc12a3a245d8861a1b3d8a25
Stephen McCrory <smccrory@ihmc.us>
2017-12-13 11:45:45 -0600

    Adding new topics to footstep planner ui api

--- ihmc-footstep-planning/src/visualizers/java/us/ihmc/footstepPlanning/ui/FootstepPlannerUserInterfaceAPI.java
+++ ihmc-footstep-planning/src/visualizers/java/us/ihmc/footstepPlanning/ui/FootstepPlannerUserInterfaceAPI.java
@@ -2,6 +2,8 @@ package us.ihmc.footstepPlanning.ui;
 
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.footstepPlanning.FootstepPlannerType;
+import us.ihmc.footstepPlanning.graphSearch.FootstepPlannerParameters;
+import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
 import us.ihmc.robotEnvironmentAwareness.communication.APIFactory;
 import us.ihmc.robotEnvironmentAwareness.communication.APIFactory.*;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
@@ -23,20 +25,23 @@ public class FootstepPlannerUserInterfaceAPI
    private static final TypedTopicTheme<Point3D> Position = apiFactory.createTypedTopicTheme("Position");
    private static final TypedTopicTheme<Boolean> ComputePath = apiFactory.createTypedTopicTheme("ComputePath");
    private static final TypedTopicTheme<FootstepPlannerType> FootstepPlannerType = apiFactory.createTypedTopicTheme("FootstepPlannerType");
+   private static final TypedTopicTheme<FootstepPlannerParameters> FootstepPlannerParameters = apiFactory.createTypedTopicTheme("FootstepPlannerParameters");
 
    private static final TopicTheme Data = apiFactory.createTopicTheme("Data");
 
    private static final Category Root = apiFactory.getRootCategory(apiFactory.createCategoryTheme("FootstepPlanning"));
 
-   public static final Topic<PlanarRegionsList> PlanarRegionData = Root.child(PlanarRegion).topic(Data);
-   public static final Topic<FootstepPlannerType> SelectedPlannerType = Root.child(PlanarRegion).topic(Data);
+   public static final Topic<PlanarRegionsList> PlanarRegionDataTopic = Root.child(PlanarRegion).topic(Data);
 
-   public static final Topic<Boolean> StartEditModeEnabled = Root.child(Start).child(EditMode).topic(Enable);
-   public static final Topic<Boolean> GoalEditModeEnabled = Root.child(Goal).child(EditMode).topic(Enable);
-   public static final Topic<Point3D> StartPosition = Root.child(Start).topic(Position);
-   public static final Topic<Point3D> GoalPosition = Root.child(Goal).topic(Position);
+   public static final Topic<Boolean> ComputePathTopic = Root.child(FootstepPlan).topic(ComputePath);
+   public static final Topic<FootstepPlannerParameters> PlannerParametersTopic = Root.child(FootstepPlan).topic(FootstepPlannerParameters);
+   public static final Topic<FootstepPlannerType> PlannerTypeTopic = Root.child(PlanarRegion).topic(Data);
+   public static final Topic<Boolean> StartEditModeEnabledTopic = Root.child(Start).child(EditMode).topic(Enable);
+   public static final Topic<Boolean> GoalEditModeEnabledTopic = Root.child(Goal).child(EditMode).topic(Enable);
+   public static final Topic<Point3D> StartPositionTopic = Root.child(Start).topic(Position);
+   public static final Topic<Point3D> GoalPositionTopic = Root.child(Goal).topic(Position);
 
-   public static final Topic<Boolean> GlobalReset = Root.topic(Reset);
+   public static final Topic<Boolean> GlobalResetTopic = Root.topic(Reset);
 
    public static final API API = apiFactory.getAPIAndCloseFactory();
 }
