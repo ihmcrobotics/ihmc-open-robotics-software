f6706df70dcb47e22f86d2bf7751bd57d2f47b6d
Stephen McCrory <smccrory@ihmc.us>
2017-12-13 13:32:06 -0600

    Adding fxml files

---
+++ ihmc-footstep-planning/src/visualizers/java/us/ihmc/footstepPlanning/ui/FootstepPlannerUI.java
@@ -0,0 +1,56 @@
+package us.ihmc.footstepPlanning.ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import us.ihmc.javaFXToolkit.scenes.View3DFactory;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class FootstepPlannerUI
+{
+   private final Stage primaryStage;
+   private final BorderPane mainPane;
+
+   public FootstepPlannerUI(Stage primaryStage) throws IOException
+   {
+      this.primaryStage = primaryStage;
+
+      FXMLLoader loader = new FXMLLoader();
+      loader.setController(this);
+
+      String name = getClass().getSimpleName() + ".fxml";
+      System.out.println("Trying to load url: " + name);
+      URL resource = getClass().getResource(name);
+      System.out.println("Loading resource: " + resource);
+      loader.setLocation(resource);
+
+      mainPane = loader.load();
+
+      View3DFactory view3dFactory = View3DFactory.createSubscene();
+      view3dFactory.addCameraController(true);
+      view3dFactory.addWorldCoordinateSystem(0.3);
+      Pane subScene = view3dFactory.getSubSceneWrappedInsidePane();
+      mainPane.setCenter(subScene);
+
+      primaryStage.setTitle(getClass().getSimpleName());
+      primaryStage.setMaximized(true);
+      Scene mainScene = new Scene(mainPane, 600, 400);
+
+      primaryStage.setScene(mainScene);
+      primaryStage.setOnCloseRequest(event -> stop());
+   }
+
+   public void show() throws IOException
+   {
+      primaryStage.show();
+   }
+
+   public void stop()
+   {
+      // TODO
+   }
+}
