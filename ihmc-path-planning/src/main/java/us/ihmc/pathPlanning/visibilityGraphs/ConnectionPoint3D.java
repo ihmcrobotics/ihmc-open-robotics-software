fbd1dc91e8a0c96a5b871e072bc3b1e2569afa9a
Sylvain Bertrand <sbertrand@ihmc.us>
2017-12-12 18:59:14 -0600

    Performance - Precision.round(...) seems very expensive.

--- ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/ConnectionPoint3D.java
+++ ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/ConnectionPoint3D.java
@@ -1,8 +1,5 @@
 package us.ihmc.pathPlanning.visibilityGraphs;
 
-import org.apache.commons.math3.util.Precision;
-
-import us.ihmc.commons.MathTools;
 import us.ihmc.euclid.tools.EuclidCoreIOTools;
 import us.ihmc.euclid.tuple2D.interfaces.Tuple2DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DBasics;
@@ -10,9 +7,8 @@ import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
 
 public class ConnectionPoint3D implements Point3DBasics
 {
-   private static final double EPSILON = 1.0e-12;
-   private static final int SCALE = 3;
-   public static final double PRECISION = Math.pow(10.0, -SCALE); // Do not change PRECISION, change SCALE if needed.
+   public static final double PRECISION = 0.001;
+   public static final double INV_PRECISION = 1.0 / PRECISION;
 
    private double x, y, z;
 
@@ -106,17 +102,17 @@ public class ConnectionPoint3D implements Point3DBasics
 
    public double getRoundedX()
    {
-      return Precision.round(x, SCALE);
+      return round(x);
    }
 
    public double getRoundedY()
    {
-      return Precision.round(y, SCALE);
+      return round(y);
    }
 
    public double getRoundedZ()
    {
-      return Precision.round(z, SCALE);
+      return round(z);
    }
 
    @Override
@@ -134,53 +130,9 @@ public class ConnectionPoint3D implements Point3DBasics
    {
       try
       {
-         return equals((ConnectionPoint3D) obj);
+         return epsilonEquals((Tuple3DReadOnly) obj, PRECISION);
       }
-      catch (ClassCastException e)
-      {
-         try
-         {
-            return equals((Tuple3DReadOnly) obj);
-         }
-         catch (ClassCastException e1)
-         {
-            return false;
-         }
-      }
-   }
-
-   public boolean equals(ConnectionPoint3D other)
-   {
-      try
-      {
-         if (!MathTools.epsilonEquals(getRoundedX(), other.getRoundedX(), EPSILON))
-            return false;
-         if (!MathTools.epsilonEquals(getRoundedY(), other.getRoundedY(), EPSILON))
-            return false;
-         if (!MathTools.epsilonEquals(getRoundedZ(), other.getRoundedZ(), EPSILON))
-            return false;
-         return true;
-      }
-      catch (NullPointerException e)
-      {
-         return false;
-      }
-   }
-
-   @Override
-   public boolean equals(Tuple3DReadOnly point3D)
-   {
-      try
-      {
-         if (!MathTools.epsilonEquals(getRoundedX(), Precision.round(point3D.getX(), SCALE), EPSILON))
-            return false;
-         if (!MathTools.epsilonEquals(getRoundedY(), Precision.round(point3D.getY(), SCALE), EPSILON))
-            return false;
-         if (!MathTools.epsilonEquals(getRoundedZ(), Precision.round(point3D.getZ(), SCALE), EPSILON))
-            return false;
-         return true;
-      }
-      catch (NullPointerException e)
+      catch (ClassCastException | NullPointerException e)
       {
          return false;
       }
@@ -190,5 +142,10 @@ public class ConnectionPoint3D implements Point3DBasics
    public String toString()
    {
       return "ConnectionPoint3D: " + EuclidCoreIOTools.getTuple3DString(this);
+   }
+
+   private static double round(double value)
+   {
+      return ((int) value * INV_PRECISION) * PRECISION;
    }
 }
