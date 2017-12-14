48731c8303c0dd0994ca520b2426e4fed24939d2
Sylvain Bertrand <sbertrand@ihmc.us>
2017-12-14 10:13:04 -0600

    Cleaned up the test framework.

--- ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/VisibilityGraphsFrameworkTest.java
+++ ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/VisibilityGraphsFrameworkTest.java
@@ -2,25 +2,17 @@ package us.ihmc.pathPlanning.visibilityGraphs;
 
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import javafx.application.Platform;
 import us.ihmc.commons.PrintTools;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations;
 import us.ihmc.continuousIntegration.ContinuousIntegrationTools;
 import us.ihmc.continuousIntegration.IntegrationCategory;
-import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
-import us.ihmc.robotEnvironmentAwareness.ui.io.PlanarRegionDataImporter;
-import us.ihmc.robotics.geometry.PlanarRegion;
-import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.pathPlanning.visibilityGraphs.VisibilityGraphsIOTools.VisibilityGraphsUnitTestDataset;
 
 @ContinuousIntegrationAnnotations.ContinuousIntegrationPlan(categories = IntegrationCategory.IN_DEVELOPMENT)
 public class VisibilityGraphsFrameworkTest
@@ -36,73 +28,35 @@ public class VisibilityGraphsFrameworkTest
    @Test(timeout = 30000)
    public void testASolutionExists() throws Exception
    {
-      String pathString = getClass().getClassLoader().getResource("Data").getPath();
-      if (VisibilityGraphsIOTools.isWindows())
-         pathString = pathString.substring(1, pathString.length());
-
-      Path filePath = Paths.get(pathString);
-      File fileLocationsForAllData = filePath.toFile();
+      List<VisibilityGraphsUnitTestDataset> allDatasets = VisibilityGraphsIOTools.loadAllDatasets();
 
       if (debug)
       {
-         PrintTools.info("Unit test files found: " + fileLocationsForAllData.listFiles().length);
+         PrintTools.info("Unit test files found: " + allDatasets.size());
       }
 
-      File[] files = fileLocationsForAllData.listFiles();
-      for (int i = 0; i < files.length; i++)
+      for (VisibilityGraphsUnitTestDataset dataset : allDatasets)
       {
-         File fileFolder = files[i];
-         String fileName = fileFolder.getName();
-
-         if (debug)
-         {
-            PrintTools.info("Processing file: " + fileName);
-         }
-
-         if (!fileName.contains("UnitTest"))
-            continue;
-
-         String simpleFileName = fileName.replace("_UnitTest", "");
-         File fileLocationForPlanarRegions = new File(fileFolder.getPath(), simpleFileName);
-         File fileLocationForStartGoalParameters = new File(fileFolder.getPath(), "UnitTestParameters.txt");
-
-         PlanarRegionsList planarRegionData = null;
-
-         if (fileLocationForPlanarRegions != null)
-            planarRegionData = PlanarRegionDataImporter.importPlanRegionData(fileLocationForPlanarRegions);
-
-         if (planarRegionData == null)
-            Platform.exit();
-
-         if (debug)
-         {
-            PrintTools.info("Running test for : " + fileFolder.getName());
-         }
-
-         Point3D start = new Point3D();
-         Point3D goal = new Point3D();
-         VisibilityGraphsIOTools.readStartGoalParameters(fileLocationForStartGoalParameters, start, goal);
-         int expectedPathSize = VisibilityGraphsIOTools.parsePathSize(fileLocationForStartGoalParameters);
-
-         List<PlanarRegion> regions = planarRegionData.getPlanarRegionsAsList();
-         ArrayList<PlanarRegion> filteredRegions = new ArrayList<>();
-
-         for (PlanarRegion region : regions)
-         {
-            if (region.getConcaveHullSize() > 2)
-            {
-               filteredRegions.add(region);
-            }
-         }
-
-         NavigableRegionsManager manager = new NavigableRegionsManager(new DefaultVisibilityGraphParameters(), filteredRegions);
-
-         List<Point3DReadOnly> path = manager.calculateBodyPath(start, goal);
-
-         assertTrue("Path is null!", path != null);
-         assertTrue("Path does not contain any waypoints", path.size() > 0);
-         assertTrue("Path size is not equal", path.size() == expectedPathSize);
-
+         testFile(dataset);
       }
+   }
+
+   private void testFile(VisibilityGraphsUnitTestDataset dataset)
+   {
+      if (debug)
+      {
+         PrintTools.info("Processing file: " + dataset.getDatasetName());
+      }
+
+      NavigableRegionsManager manager = new NavigableRegionsManager();
+      manager.setPlanarRegions(dataset.getPlanarRegionsList().getPlanarRegionsAsList());
+
+      List<Point3DReadOnly> path = manager.calculateBodyPath(dataset.getStart(), dataset.getGoal());
+
+      assertTrue("Path is null!", path != null);
+      assertTrue("Path does not contain any waypoints", path.size() > 0);
+
+      if (dataset.hasExpectedPathSize())
+         assertTrue("Path size is not equal", path.size() == dataset.getExpectedPathSize());
    }
 }
