e0a134f0fbbf1020eb36b903b04a2de22e750ba6
Robert <rgriffin@ihmc.us>
2017-12-13 22:33:44 -0600

    setup a new discrete trajectory class to make the new discrete dynamic planning classes way simpler

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/DDPSolverInterface.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/DDPSolverInterface.java
@@ -8,19 +8,14 @@ import java.util.List;
 
 public interface DDPSolverInterface<E extends Enum>
 {
-   void initializeFromLQRSolution(E dynamicsState, RecyclingArrayList<DenseMatrix64F> stateTrajectory, RecyclingArrayList<DenseMatrix64F> controlTrajectory,
-                                  RecyclingArrayList<DenseMatrix64F> desiredStateTrajectory, RecyclingArrayList<DenseMatrix64F> desiredControlTrajectory,
+   void initializeFromLQRSolution(E dynamicsState, DiscreteOptimizationTrajectory optimalTrajectory, DiscreteOptimizationTrajectory desiredTrajectory,
                                   RecyclingArrayList<DenseMatrix64F> feedbackGainTrajectory, RecyclingArrayList<DenseMatrix64F> feedForwardTrajectory);
-   void initializeTrajectoriesFromDesireds(DenseMatrix64F initialCoM, RecyclingArrayList<DenseMatrix64F> desiredStateTrajectory,
-                                          RecyclingArrayList<DenseMatrix64F> desiredControlTrajectory);
+   void initializeTrajectoriesFromDesireds(DenseMatrix64F initialCoM, DiscreteOptimizationTrajectory desiredTrajectory);
 
-   RecyclingArrayList<DenseMatrix64F> getControlTrajectory();
-   RecyclingArrayList<DenseMatrix64F> getStateTrajectory();
+   DiscreteOptimizationTrajectory getOptimalTrajectory();
 
    int computeTrajectory(E dynamicsState);
    int computeTrajectory(List<E> dynamicsStates, TIntArrayList startIndices, TIntArrayList endIndices);
-   double forwardPass(E dynamicsState, int startIndex, int endIndex, DenseMatrix64F initialCoM, RecyclingArrayList<DenseMatrix64F> updatedStateTrajectory,
-                      RecyclingArrayList<DenseMatrix64F> updatedControlTrajectory);
-   boolean backwardPass(E dynamicsState, int startIndex, int endIndex, RecyclingArrayList<DenseMatrix64F> stateTrajectory,
-                        RecyclingArrayList<DenseMatrix64F> controlTrajectory);
+   double forwardPass(E dynamicsState, int startIndex, int endIndex, DenseMatrix64F initialCoM, DiscreteOptimizationTrajectory updatedTrajectory);
+   boolean backwardPass(E dynamicsState, int startIndex, int endIndex, DiscreteOptimizationTrajectory trajectory);
 }
