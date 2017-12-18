d20c6e4b8275a59a3fac64044236642d50472ec5
Sylvain Bertrand <sbertrand@ihmc.us>
2017-12-17 20:16:33 -0600

    Improved the loading action.

--- ihmc-path-planning/src/visualizers/java/us/ihmc/pathPlanning/visibilityGraphs/ui/controllers/SimpleUIMenuController.java
+++ ihmc-path-planning/src/visualizers/java/us/ihmc/pathPlanning/visibilityGraphs/ui/controllers/SimpleUIMenuController.java
@@ -1,17 +1,24 @@
 package us.ihmc.pathPlanning.visibilityGraphs.ui.controllers;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Window;
 import us.ihmc.commons.PrintTools;
+import us.ihmc.pathPlanning.visibilityGraphs.tools.VisibilityGraphsIOTools;
 import us.ihmc.pathPlanning.visibilityGraphs.ui.messager.UIVisibilityGraphsTopics;
 import us.ihmc.robotEnvironmentAwareness.communication.REAMessager;
-import us.ihmc.robotEnvironmentAwareness.ui.io.PlanarRegionDataImporter;
+import us.ihmc.robotics.PlanarRegionFileTools;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 
 public class SimpleUIMenuController
 {
    private static final boolean VERBOSE = true;
+
+   private final DirectoryChooser directoryChooser = new DirectoryChooser();
 
    @FXML
    private MenuItem reloadMenuItem;
@@ -20,6 +27,14 @@ public class SimpleUIMenuController
    private Window ownerWindow;
 
    private PlanarRegionsList loadedPlanarRegions = null;
+
+   public SimpleUIMenuController()
+   {
+      File defaultDataFolder = new File("..\\visualizers\\resources\\Data");
+      if (!defaultDataFolder.exists())
+         defaultDataFolder = new File(".");
+      directoryChooser.setInitialDirectory(defaultDataFolder);
+   }
 
    public void attachMessager(REAMessager messager)
    {
@@ -35,7 +50,16 @@ public class SimpleUIMenuController
    @FXML
    public void loadPlanarRegion()
    {
-      loadedPlanarRegions = PlanarRegionDataImporter.importUsingFileChooser(ownerWindow);
+      File result = directoryChooser.showDialog(ownerWindow);
+      if (result == null)
+         return;
+
+      File[] vizGraphsParameterFile = result.listFiles((FilenameFilter) (dir, name) -> name.equals(VisibilityGraphsIOTools.INPUTS_PARAMETERS_FILENAME));
+      if (vizGraphsParameterFile != null && vizGraphsParameterFile.length == 1)
+         loadedPlanarRegions = PlanarRegionFileTools.importPlanRegionData(result.listFiles(File::isDirectory)[0]);
+      else
+         loadedPlanarRegions = PlanarRegionFileTools.importPlanRegionData(result);
+      directoryChooser.setInitialDirectory(result.getParentFile());
 
       if (loadedPlanarRegions != null)
       {
