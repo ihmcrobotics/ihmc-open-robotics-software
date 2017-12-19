fe7bd26f5fb4fbbb67354795110202847c8a9100
Stephen McCrory <smccrory@ihmc.us>
2017-12-19 16:37:48 -0600

    adding actual arrows to planner ui

--- ihmc-footstep-planning/src/visualizers/java/us/ihmc/footstepPlanning/ui/StartGoalOrientationViewer.java
+++ ihmc-footstep-planning/src/visualizers/java/us/ihmc/footstepPlanning/ui/StartGoalOrientationViewer.java
@@ -3,27 +3,27 @@ package us.ihmc.footstepPlanning.ui;
 import javafx.animation.AnimationTimer;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.shape.Cylinder;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.MeshView;
+import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.javaFXToolkit.shapes.JavaFXMultiColorMeshBuilder;
+import us.ihmc.javaFXToolkit.shapes.TextureColorPalette1D;
 import us.ihmc.robotEnvironmentAwareness.communication.REAMessager;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 import static us.ihmc.footstepPlanning.ui.FootstepPlannerUserInterfaceAPI.*;
-
-import static us.ihmc.pathPlanning.visibilityGraphs.ui.viewers.StartGoalPositionViewer.RADIUS;
-import static us.ihmc.pathPlanning.visibilityGraphs.ui.viewers.StartGoalPositionViewer.startOpaqueMaterial;
-import static us.ihmc.pathPlanning.visibilityGraphs.ui.viewers.StartGoalPositionViewer.startTransparentMaterial;
-import static us.ihmc.pathPlanning.visibilityGraphs.ui.viewers.StartGoalPositionViewer.goalOpaqueMaterial;
-import static us.ihmc.pathPlanning.visibilityGraphs.ui.viewers.StartGoalPositionViewer.goalTransparentMaterial;
+import static us.ihmc.pathPlanning.visibilityGraphs.ui.viewers.StartGoalPositionViewer.*;
 
 public class StartGoalOrientationViewer extends AnimationTimer
 {
    private final Group root = new Group();
 
    private static final double cylinderLength = 5.0 * RADIUS;
-   private final Cylinder startArrow = new Cylinder(0.2 * RADIUS, cylinderLength);
-   private final Cylinder goalArrow = new Cylinder(0.2 * RADIUS, cylinderLength);
+   private final ArrowGraphic startArrow = new ArrowGraphic(0.2 * RADIUS, cylinderLength, Color.GREEN);
+   private final ArrowGraphic goalArrow = new ArrowGraphic(0.2 * RADIUS, cylinderLength, Color.RED);
 
    private final AtomicReference<Boolean> startRotationEditModeEnabled;
    private final AtomicReference<Boolean> goalRotationEditModeEnabled;
@@ -78,12 +78,39 @@ public class StartGoalOrientationViewer extends AnimationTimer
       }
    }
 
-   private static void setArrowPose(Cylinder arrow, Point3D position, double orientationRadians)
+   private static void setArrowPose(ArrowGraphic arrow, Point3D position, double orientationRadians)
    {
-      arrow.setTranslateX(position.getX() + 0.5 * cylinderLength * Math.cos(orientationRadians));
+      arrow.setTranslateX(position.getX() + 0.5 * cylinderLength * (Math.cos(orientationRadians) - 1.0));
       arrow.setTranslateY(position.getY() + 0.5 * cylinderLength * Math.sin(orientationRadians));
       arrow.setTranslateZ(position.getZ());
-      arrow.setRotate(Math.toDegrees(0.5 * Math.PI + orientationRadians));
+      arrow.setRotate(Math.toDegrees(orientationRadians));
+   }
+
+   private class ArrowGraphic extends Group
+   {
+      private final MeshView arrow;
+
+      public ArrowGraphic(double radius, double length, Color color)
+      {
+         TextureColorPalette1D colorPalette = new TextureColorPalette1D();
+         colorPalette.setHueBased(1.0, 1.0);
+         JavaFXMultiColorMeshBuilder meshBuilder = new JavaFXMultiColorMeshBuilder(colorPalette);
+
+         double coneHeight = 0.10 * length;
+         double coneRadius = 1.5 * radius;
+
+         meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
+         meshBuilder.addCone(coneHeight, coneRadius, new Point3D(length, 0.0, 0.0), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
+
+         this.arrow = new MeshView(meshBuilder.generateMesh());
+         arrow.setMaterial(meshBuilder.generateMaterial());
+         getChildren().add(arrow);
+      }
+
+      public void setMaterial(PhongMaterial material)
+      {
+         arrow.setMaterial(material);
+      }
    }
 
    public Node getRoot()
