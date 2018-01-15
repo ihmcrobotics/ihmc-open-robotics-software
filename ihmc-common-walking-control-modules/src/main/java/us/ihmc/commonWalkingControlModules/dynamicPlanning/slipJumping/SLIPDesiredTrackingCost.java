51934827a77a1f773affd1b120820d4cf14f0038
Robert <rgriffin@ihmc.us>
2018-01-15 12:13:04 -0600

    made the desired tracking cost hybrid switching

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/slipJumping/SLIPDesiredTrackingCost.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/slipJumping/SLIPDesiredTrackingCost.java
@@ -9,56 +9,104 @@ import static us.ihmc.commonWalkingControlModules.dynamicPlanning.slipJumping.SL
 
 public class SLIPDesiredTrackingCost implements LQTrackingCostFunction<SLIPState>
 {
-   static double qX = 1e-1;
-   static double qY = 1e-1;
-   static double qZ = 1e-1;
-   static double qThetaX = 1.0;
-   static double qThetaY = 1.0;
-   static double qThetaZ = 1.0;
-   static double qXDot = 0.0;
-   static double qYDot = 0.0;
-   static double qZDot = 0.0;
-   static double qThetaDotX = 0.0;
-   static double qThetaDotY = 0.0;
-   static double qThetaDotZ = 0.0;
+   static double qXFlight = 1e-1;
+   static double qYFlight = 1e-1;
+   static double qZFlight = 1e-1;
+   static double qThetaXFlight = 1.0;
+   static double qThetaYFlight = 1.0;
+   static double qThetaZFlight = 1.0;
+   static double qXDotFlight = 0.0;
+   static double qYDotFlight = 0.0;
+   static double qZDotFlight = 0.0;
+   static double qThetaDotXFlight = 0.0;
+   static double qThetaDotYFlight = 0.0;
+   static double qThetaDotZFlight = 0.0;
 
-   static double rFx = 0.0;
-   static double rFy = 0.0;
-   static double rFz = 1.0e2;
-   static double rTauX = 0.0;
-   static double rTauY = 0.0;
-   static double rTauZ = 0.0;
-   static double rXf = 1.0e2;
-   static double rYf = 1.0e2;
-   static double rK = 1.0e5;
+   static double rFxFlight = 0.0;
+   static double rFyFlight = 0.0;
+   static double rFzFlight = 0.0;
+   static double rTauXFlight = 0.0;
+   static double rTauYFlight = 0.0;
+   static double rTauZFlight = 0.0;
+   static double rXfFlight = 0.0;
+   static double rYfFlight = 0.0;
+   static double rKFlight = 0.0;
 
-   private final DenseMatrix64F Q = new DenseMatrix64F(stateVectorSize, stateVectorSize);
-   private final DenseMatrix64F R = new DenseMatrix64F(controlVectorSize, controlVectorSize);
+   static double qXStance = 1e-1;
+   static double qYStance = 1e-1;
+   static double qZStance = 1e-1;
+   static double qThetaXStance = 1.0;
+   static double qThetaYStance = 1.0;
+   static double qThetaZStance = 1.0;
+   static double qXDotStance = 0.0;
+   static double qYDotStance = 0.0;
+   static double qZDotStance = 0.0;
+   static double qThetaDotXStance = 0.0;
+   static double qThetaDotYStance = 0.0;
+   static double qThetaDotZStance = 0.0;
+
+   static double rFxStance = 0.0;
+   static double rFyStance = 0.0;
+   static double rFzStance = 1.0e2;
+   static double rTauXStance = 0.0;
+   static double rTauYStance = 0.0;
+   static double rTauZStance = 0.0;
+   static double rXfStance = 1.0e2;
+   static double rYfStance = 1.0e2;
+   static double rKStance = 1.0e5;
+
+   private final DenseMatrix64F QFlight = new DenseMatrix64F(stateVectorSize, stateVectorSize);
+   private final DenseMatrix64F RFlight = new DenseMatrix64F(controlVectorSize, controlVectorSize);
+   private final DenseMatrix64F QStance = new DenseMatrix64F(stateVectorSize, stateVectorSize);
+   private final DenseMatrix64F RStance = new DenseMatrix64F(controlVectorSize, controlVectorSize);
 
    public SLIPDesiredTrackingCost()
    {
-      Q.set(x, x, qX);
-      Q.set(y, y, qY);
-      Q.set(z, z, qZ);
-      Q.set(thetaX, thetaX, qThetaX);
-      Q.set(thetaY, thetaY, qThetaY);
-      Q.set(thetaZ, thetaZ, qThetaZ);
-      Q.set(xDot, xDot, qXDot);
-      Q.set(yDot, yDot, qYDot);
-      Q.set(zDot, zDot, qZDot);
-      Q.set(thetaXDot, thetaXDot, qThetaDotX);
-      Q.set(thetaYDot, thetaYDot, qThetaDotY);
-      Q.set(thetaZDot, thetaZDot, qThetaDotZ);
+      QStance.set(x, x, qXStance);
+      QStance.set(y, y, qYStance);
+      QStance.set(z, z, qZStance);
+      QStance.set(thetaX, thetaX, qThetaXStance);
+      QStance.set(thetaY, thetaY, qThetaYStance);
+      QStance.set(thetaZ, thetaZ, qThetaZStance);
+      QStance.set(xDot, xDot, qXDotStance);
+      QStance.set(yDot, yDot, qYDotStance);
+      QStance.set(zDot, zDot, qZDotStance);
+      QStance.set(thetaXDot, thetaXDot, qThetaDotXStance);
+      QStance.set(thetaYDot, thetaYDot, qThetaDotYStance);
+      QStance.set(thetaZDot, thetaZDot, qThetaDotZStance);
 
-      R.set(fx, fx, rFx);
-      R.set(fy, fy, rFy);
-      R.set(fz, fz, rFz);
-      R.set(tauX, tauX, rTauX);
-      R.set(tauY, tauY, rTauY);
-      R.set(tauZ, tauZ, rTauZ);
-      R.set(xF, xF, rXf);
-      R.set(yF, yF, rYf);
-      R.set(k, k, rK);
+      RStance.set(fx, fx, rFxStance);
+      RStance.set(fy, fy, rFyStance);
+      RStance.set(fz, fz, rFzStance);
+      RStance.set(tauX, tauX, rTauXStance);
+      RStance.set(tauY, tauY, rTauYStance);
+      RStance.set(tauZ, tauZ, rTauZStance);
+      RStance.set(xF, xF, rXfStance);
+      RStance.set(yF, yF, rYfStance);
+      RStance.set(k, k, rKStance);
+
+      QFlight.set(x, x, qXFlight);
+      QFlight.set(y, y, qYFlight);
+      QFlight.set(z, z, qZFlight);
+      QFlight.set(thetaX, thetaX, qThetaXFlight);
+      QFlight.set(thetaY, thetaY, qThetaYFlight);
+      QFlight.set(thetaZ, thetaZ, qThetaZFlight);
+      QFlight.set(xDot, xDot, qXDotFlight);
+      QFlight.set(yDot, yDot, qYDotFlight);
+      QFlight.set(zDot, zDot, qZDotFlight);
+      QFlight.set(thetaXDot, thetaXDot, qThetaDotXFlight);
+      QFlight.set(thetaYDot, thetaYDot, qThetaDotYFlight);
+      QFlight.set(thetaZDot, thetaZDot, qThetaDotZFlight);
+
+      RFlight.set(fx, fx, rFxFlight);
+      RFlight.set(fy, fy, rFyFlight);
+      RFlight.set(fz, fz, rFzFlight);
+      RFlight.set(tauX, tauX, rTauXFlight);
+      RFlight.set(tauY, tauY, rTauYFlight);
+      RFlight.set(tauZ, tauZ, rTauZFlight);
+      RFlight.set(xF, xF, rXfFlight);
+      RFlight.set(yF, yF, rYfFlight);
+      RFlight.set(k, k, rKFlight);
    }
 
    private DenseMatrix64F tempStateMatrix = new DenseMatrix64F(stateVectorSize, 1);
@@ -73,8 +121,17 @@ public class SLIPDesiredTrackingCost implements LQTrackingCostFunction<SLIPState
       CommonOps.subtract(controlVector, desiredControlVector, tempControlMatrix);
       CommonOps.subtract(stateVector, desiredStateVector, tempStateMatrix);
 
-      DiagonalMatrixTools.preMult(Q, tempStateMatrix, tempWX);
-      DiagonalMatrixTools.preMult(R, tempControlMatrix, tempWU);
+      switch (state)
+      {
+      case FLIGHT:
+         DiagonalMatrixTools.preMult(QFlight, tempStateMatrix, tempWX);
+         DiagonalMatrixTools.preMult(RFlight, tempControlMatrix, tempWU);
+         break;
+      case STANCE:
+         DiagonalMatrixTools.preMult(QStance, tempStateMatrix, tempWX);
+         DiagonalMatrixTools.preMult(RStance, tempControlMatrix, tempWU);
+         break;
+      }
 
       return CommonOps.dot(tempControlMatrix, tempWU) + CommonOps.dot(tempStateMatrix, tempWX);
    }
@@ -85,7 +142,15 @@ public class SLIPDesiredTrackingCost implements LQTrackingCostFunction<SLIPState
                                     DenseMatrix64F desiredStateVector, DenseMatrix64F matrixToPack)
    {
       CommonOps.subtract(stateVector, desiredStateVector, tempStateMatrix);
-      DiagonalMatrixTools.preMult(Q, tempStateMatrix, matrixToPack);
+      switch (state)
+      {
+      case FLIGHT:
+         DiagonalMatrixTools.preMult(QFlight, tempStateMatrix, matrixToPack);
+         break;
+      case STANCE:
+         DiagonalMatrixTools.preMult(QStance, tempStateMatrix, matrixToPack);
+         break;
+      }
       CommonOps.scale(2.0, matrixToPack);
    }
 
@@ -95,7 +160,15 @@ public class SLIPDesiredTrackingCost implements LQTrackingCostFunction<SLIPState
                                       DenseMatrix64F desiredStateVector, DenseMatrix64F matrixToPack)
    {
       CommonOps.subtract(controlVector, desiredControlVecotr, tempControlMatrix);
-      DiagonalMatrixTools.preMult(R, tempControlMatrix, matrixToPack);
+      switch (state)
+      {
+      case FLIGHT:
+         DiagonalMatrixTools.preMult(RFlight, tempControlMatrix, matrixToPack);
+         break;
+      case STANCE:
+         DiagonalMatrixTools.preMult(RStance, tempControlMatrix, matrixToPack);
+         break;
+      }
       CommonOps.scale(2.0, matrixToPack);
    }
 
@@ -103,14 +176,30 @@ public class SLIPDesiredTrackingCost implements LQTrackingCostFunction<SLIPState
    @Override
    public void getCostStateHessian(SLIPState state, DenseMatrix64F controlVector, DenseMatrix64F stateVector, DenseMatrix64F matrixToPack)
    {
-      CommonOps.scale(2.0, Q, matrixToPack);
+      switch (state)
+      {
+      case FLIGHT:
+         CommonOps.scale(2.0, QFlight, matrixToPack);
+         break;
+      case STANCE:
+         CommonOps.scale(2.0, QStance, matrixToPack);
+         break;
+      }
    }
 
    /** L_uu(X_k, U_k) */
    @Override
    public void getCostControlHessian(SLIPState state, DenseMatrix64F controlVector, DenseMatrix64F stateVector, DenseMatrix64F matrixToPack)
    {
-      CommonOps.scale(2.0, R, matrixToPack);
+      switch (state)
+      {
+      case FLIGHT:
+         CommonOps.scale(2.0, RFlight, matrixToPack);
+         break;
+      case STANCE:
+         CommonOps.scale(2.0, RStance, matrixToPack);
+         break;
+      }
    }
 
    /** L_ux(X_k, U_k) */
--- ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/slipJumping/SLIPDesiredTrackingCostTest.java
+++ ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/slipJumping/SLIPDesiredTrackingCostTest.java
@@ -58,36 +58,72 @@ public class SLIPDesiredTrackingCostTest extends TrackingCostFunctionTest<SLIPSt
       DenseMatrix64F desiredState = RandomGeometry.nextDenseMatrix64F(random, getStateVectorSize(), 1);
       DenseMatrix64F desiredControl = RandomGeometry.nextDenseMatrix64F(random, getControlVectorSize(), 1);
 
-      double cost = costFunction.getCost(SLIPState.STANCE, currentControl, currentState, desiredControl, desiredState);
+      double cost = costFunction.getCost(SLIPState.FLIGHT, currentControl, currentState, desiredControl, desiredState);
 
-      double expectedCost = SLIPDesiredTrackingCost.qX * (currentState.get(x) - desiredState.get(x)) * (currentState.get(x) - desiredState.get(x));
-      expectedCost += SLIPDesiredTrackingCost.qY * (currentState.get(y) - desiredState.get(y)) * (currentState.get(y) - desiredState.get(y));
-      expectedCost += SLIPDesiredTrackingCost.qZ * (currentState.get(z) - desiredState.get(z)) * (currentState.get(z) - desiredState.get(z));
+      double expectedCost = SLIPDesiredTrackingCost.qXFlight * (currentState.get(x) - desiredState.get(x)) * (currentState.get(x) - desiredState.get(x));
+      expectedCost += SLIPDesiredTrackingCost.qYFlight * (currentState.get(y) - desiredState.get(y)) * (currentState.get(y) - desiredState.get(y));
+      expectedCost += SLIPDesiredTrackingCost.qZFlight * (currentState.get(z) - desiredState.get(z)) * (currentState.get(z) - desiredState.get(z));
 
-      expectedCost += SLIPDesiredTrackingCost.qThetaX * (currentState.get(thetaX) - desiredState.get(thetaX)) * (currentState.get(thetaX) - desiredState.get(thetaX));
-      expectedCost += SLIPDesiredTrackingCost.qThetaY * (currentState.get(thetaY) - desiredState.get(thetaY)) * (currentState.get(thetaY) - desiredState.get(thetaY));
-      expectedCost += SLIPDesiredTrackingCost.qThetaZ * (currentState.get(thetaZ) - desiredState.get(thetaZ)) * (currentState.get(thetaZ) - desiredState.get(thetaZ));
+      expectedCost += SLIPDesiredTrackingCost.qThetaXFlight * (currentState.get(thetaX) - desiredState.get(thetaX)) * (currentState.get(thetaX) - desiredState.get(thetaX));
+      expectedCost += SLIPDesiredTrackingCost.qThetaYFlight * (currentState.get(thetaY) - desiredState.get(thetaY)) * (currentState.get(thetaY) - desiredState.get(thetaY));
+      expectedCost += SLIPDesiredTrackingCost.qThetaZFlight * (currentState.get(thetaZ) - desiredState.get(thetaZ)) * (currentState.get(thetaZ) - desiredState.get(thetaZ));
 
-      expectedCost += SLIPDesiredTrackingCost.qXDot * (currentState.get(xDot) - desiredState.get(xDot)) * (currentState.get(xDot) - desiredState.get(xDot));
-      expectedCost += SLIPDesiredTrackingCost.qYDot * (currentState.get(yDot) - desiredState.get(yDot)) * (currentState.get(yDot) - desiredState.get(yDot));
-      expectedCost += SLIPDesiredTrackingCost.qZDot * (currentState.get(zDot) - desiredState.get(zDot)) * (currentState.get(zDot) - desiredState.get(zDot));
+      expectedCost += SLIPDesiredTrackingCost.qXDotFlight * (currentState.get(xDot) - desiredState.get(xDot)) * (currentState.get(xDot) - desiredState.get(xDot));
+      expectedCost += SLIPDesiredTrackingCost.qYDotFlight * (currentState.get(yDot) - desiredState.get(yDot)) * (currentState.get(yDot) - desiredState.get(yDot));
+      expectedCost += SLIPDesiredTrackingCost.qZDotFlight * (currentState.get(zDot) - desiredState.get(zDot)) * (currentState.get(zDot) - desiredState.get(zDot));
 
-      expectedCost += SLIPDesiredTrackingCost.qThetaDotX * (currentState.get(thetaXDot) - desiredState.get(thetaXDot)) * (currentState.get(thetaXDot) - desiredState.get(thetaXDot));
-      expectedCost += SLIPDesiredTrackingCost.qThetaDotY * (currentState.get(thetaYDot) - desiredState.get(thetaYDot)) * (currentState.get(thetaYDot) - desiredState.get(thetaYDot));
-      expectedCost += SLIPDesiredTrackingCost.qThetaDotZ * (currentState.get(thetaZDot) - desiredState.get(thetaZDot)) * (currentState.get(thetaZDot) - desiredState.get(thetaZDot));
+      expectedCost += SLIPDesiredTrackingCost.qThetaDotXFlight * (currentState.get(thetaXDot) - desiredState.get(thetaXDot)) * (currentState.get(thetaXDot) - desiredState.get(thetaXDot));
+      expectedCost += SLIPDesiredTrackingCost.qThetaDotYFlight * (currentState.get(thetaYDot) - desiredState.get(thetaYDot)) * (currentState.get(thetaYDot) - desiredState.get(thetaYDot));
+      expectedCost += SLIPDesiredTrackingCost.qThetaDotZFlight * (currentState.get(thetaZDot) - desiredState.get(thetaZDot)) * (currentState.get(thetaZDot) - desiredState.get(thetaZDot));
 
-      expectedCost += SLIPDesiredTrackingCost.rFx * (currentControl.get(fx) - desiredControl.get(fx)) * (currentControl.get(fx) - desiredControl.get(fx));
-      expectedCost += SLIPDesiredTrackingCost.rFy * (currentControl.get(fy) - desiredControl.get(fy)) * (currentControl.get(fy) - desiredControl.get(fy));
-      expectedCost += SLIPDesiredTrackingCost.rFz * (currentControl.get(fz) - desiredControl.get(fz)) * (currentControl.get(fz) - desiredControl.get(fz));
+      expectedCost += SLIPDesiredTrackingCost.rFxFlight * (currentControl.get(fx) - desiredControl.get(fx)) * (currentControl.get(fx) - desiredControl.get(fx));
+      expectedCost += SLIPDesiredTrackingCost.rFyFlight * (currentControl.get(fy) - desiredControl.get(fy)) * (currentControl.get(fy) - desiredControl.get(fy));
+      expectedCost += SLIPDesiredTrackingCost.rFzFlight * (currentControl.get(fz) - desiredControl.get(fz)) * (currentControl.get(fz) - desiredControl.get(fz));
 
-      expectedCost += SLIPDesiredTrackingCost.rTauX * (currentControl.get(tauX) - desiredControl.get(tauX)) * (currentControl.get(tauX) - desiredControl.get(tauX));
-      expectedCost += SLIPDesiredTrackingCost.rTauY * (currentControl.get(tauY) - desiredControl.get(tauY)) * (currentControl.get(tauY) - desiredControl.get(tauY));
-      expectedCost += SLIPDesiredTrackingCost.rTauZ * (currentControl.get(tauZ) - desiredControl.get(tauZ)) * (currentControl.get(tauZ) - desiredControl.get(tauZ));
+      expectedCost += SLIPDesiredTrackingCost.rTauXFlight * (currentControl.get(tauX) - desiredControl.get(tauX)) * (currentControl.get(tauX) - desiredControl.get(tauX));
+      expectedCost += SLIPDesiredTrackingCost.rTauYFlight * (currentControl.get(tauY) - desiredControl.get(tauY)) * (currentControl.get(tauY) - desiredControl.get(tauY));
+      expectedCost += SLIPDesiredTrackingCost.rTauZFlight * (currentControl.get(tauZ) - desiredControl.get(tauZ)) * (currentControl.get(tauZ) - desiredControl.get(tauZ));
 
-      expectedCost += SLIPDesiredTrackingCost.rXf * (currentControl.get(xF) - desiredControl.get(xF)) * (currentControl.get(xF) - desiredControl.get(xF));
-      expectedCost += SLIPDesiredTrackingCost.rYf * (currentControl.get(yF) - desiredControl.get(yF)) * (currentControl.get(yF) - desiredControl.get(yF));
+      expectedCost += SLIPDesiredTrackingCost.rXfFlight * (currentControl.get(xF) - desiredControl.get(xF)) * (currentControl.get(xF) - desiredControl.get(xF));
+      expectedCost += SLIPDesiredTrackingCost.rYfFlight * (currentControl.get(yF) - desiredControl.get(yF)) * (currentControl.get(yF) - desiredControl.get(yF));
 
-      expectedCost += SLIPDesiredTrackingCost.rK * (currentControl.get(k) - desiredControl.get(k)) * (currentControl.get(k) - desiredControl.get(k));
+      expectedCost += SLIPDesiredTrackingCost.rKFlight * (currentControl.get(k) - desiredControl.get(k)) * (currentControl.get(k) - desiredControl.get(k));
+
+      Assert.assertEquals(expectedCost, cost, 1e-7);
+
+
+
+
+      cost = costFunction.getCost(SLIPState.STANCE, currentControl, currentState, desiredControl, desiredState);
+
+      expectedCost = SLIPDesiredTrackingCost.qXStance * (currentState.get(x) - desiredState.get(x)) * (currentState.get(x) - desiredState.get(x));
+      expectedCost += SLIPDesiredTrackingCost.qYStance * (currentState.get(y) - desiredState.get(y)) * (currentState.get(y) - desiredState.get(y));
+      expectedCost += SLIPDesiredTrackingCost.qZStance * (currentState.get(z) - desiredState.get(z)) * (currentState.get(z) - desiredState.get(z));
+
+      expectedCost += SLIPDesiredTrackingCost.qThetaXStance * (currentState.get(thetaX) - desiredState.get(thetaX)) * (currentState.get(thetaX) - desiredState.get(thetaX));
+      expectedCost += SLIPDesiredTrackingCost.qThetaYStance * (currentState.get(thetaY) - desiredState.get(thetaY)) * (currentState.get(thetaY) - desiredState.get(thetaY));
+      expectedCost += SLIPDesiredTrackingCost.qThetaZStance * (currentState.get(thetaZ) - desiredState.get(thetaZ)) * (currentState.get(thetaZ) - desiredState.get(thetaZ));
+
+      expectedCost += SLIPDesiredTrackingCost.qXDotStance * (currentState.get(xDot) - desiredState.get(xDot)) * (currentState.get(xDot) - desiredState.get(xDot));
+      expectedCost += SLIPDesiredTrackingCost.qYDotStance * (currentState.get(yDot) - desiredState.get(yDot)) * (currentState.get(yDot) - desiredState.get(yDot));
+      expectedCost += SLIPDesiredTrackingCost.qZDotStance * (currentState.get(zDot) - desiredState.get(zDot)) * (currentState.get(zDot) - desiredState.get(zDot));
+
+      expectedCost += SLIPDesiredTrackingCost.qThetaDotXStance * (currentState.get(thetaXDot) - desiredState.get(thetaXDot)) * (currentState.get(thetaXDot) - desiredState.get(thetaXDot));
+      expectedCost += SLIPDesiredTrackingCost.qThetaDotYStance * (currentState.get(thetaYDot) - desiredState.get(thetaYDot)) * (currentState.get(thetaYDot) - desiredState.get(thetaYDot));
+      expectedCost += SLIPDesiredTrackingCost.qThetaDotZStance * (currentState.get(thetaZDot) - desiredState.get(thetaZDot)) * (currentState.get(thetaZDot) - desiredState.get(thetaZDot));
+
+      expectedCost += SLIPDesiredTrackingCost.rFxStance * (currentControl.get(fx) - desiredControl.get(fx)) * (currentControl.get(fx) - desiredControl.get(fx));
+      expectedCost += SLIPDesiredTrackingCost.rFyStance * (currentControl.get(fy) - desiredControl.get(fy)) * (currentControl.get(fy) - desiredControl.get(fy));
+      expectedCost += SLIPDesiredTrackingCost.rFzStance * (currentControl.get(fz) - desiredControl.get(fz)) * (currentControl.get(fz) - desiredControl.get(fz));
+
+      expectedCost += SLIPDesiredTrackingCost.rTauXStance * (currentControl.get(tauX) - desiredControl.get(tauX)) * (currentControl.get(tauX) - desiredControl.get(tauX));
+      expectedCost += SLIPDesiredTrackingCost.rTauYStance * (currentControl.get(tauY) - desiredControl.get(tauY)) * (currentControl.get(tauY) - desiredControl.get(tauY));
+      expectedCost += SLIPDesiredTrackingCost.rTauZStance * (currentControl.get(tauZ) - desiredControl.get(tauZ)) * (currentControl.get(tauZ) - desiredControl.get(tauZ));
+
+      expectedCost += SLIPDesiredTrackingCost.rXfStance * (currentControl.get(xF) - desiredControl.get(xF)) * (currentControl.get(xF) - desiredControl.get(xF));
+      expectedCost += SLIPDesiredTrackingCost.rYfStance * (currentControl.get(yF) - desiredControl.get(yF)) * (currentControl.get(yF) - desiredControl.get(yF));
+
+      expectedCost += SLIPDesiredTrackingCost.rKStance * (currentControl.get(k) - desiredControl.get(k)) * (currentControl.get(k) - desiredControl.get(k));
 
       Assert.assertEquals(expectedCost, cost, 1e-7);
    }
