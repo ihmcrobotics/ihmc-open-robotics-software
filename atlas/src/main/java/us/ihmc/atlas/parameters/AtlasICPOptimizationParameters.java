0652af55e627c628b7dea58699c1b0543c9ba275
Robert <rgriffin@ihmc.us>
2018-01-22 16:45:40 -0600

    turned on cop rate limiting for atlas in sim

--- atlas/src/main/java/us/ihmc/atlas/parameters/AtlasICPOptimizationParameters.java
+++ atlas/src/main/java/us/ihmc/atlas/parameters/AtlasICPOptimizationParameters.java
@@ -122,7 +122,7 @@ public class AtlasICPOptimizationParameters extends ICPOptimizationParameters
    @Override
    public boolean useFeedbackRate()
    {
-      return false;
+      return !runningOnRealRobot;
    }
 
    /** {@inheritDoc} */
