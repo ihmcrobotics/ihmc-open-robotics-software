9a95b6722d3de813fef2618ba7d1846356d79481
Sylvain Bertrand <sbertrand@ihmc.us>
2018-01-23 10:09:02 -0600

    Extracted documentation from Carson's PR.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/frames/YoFramePoint2d.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/frames/YoFramePoint2d.java
@@ -12,16 +12,41 @@ public class YoFramePoint2d extends YoFrameTuple2d implements FixedFramePoint2DB
    /** Only for some garbage-free operations and reducing number of operations on the YoDoubles. */
    private final FramePoint2D framePoint2D = new FramePoint2D();
 
+   /**
+    * Creates a new yo frame point using the given yo variables and sets its reference frame to
+    * {@code referenceFrame}.
+    *
+    * @param referenceFrame the reference frame for this yo frame point.
+    * @param xVariable an existing variable representing the x value of this yo frame point.
+    * @param yVariable an existing variable representing the y value of this yo frame point.
+    */
    public YoFramePoint2d(YoDouble xVariable, YoDouble yVariable, ReferenceFrame referenceFrame)
    {
       super(xVariable, yVariable, referenceFrame);
    }
 
+   /**
+    * Creates a new yo frame point, initializes its coordinates to zero and its reference frame to
+    * {@code referenceFrame}, and registers variables to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param referenceFrame the reference frame for this yo frame point.
+    * @param registry the registry to register child variables to.
+    */
    public YoFramePoint2d(String namePrefix, ReferenceFrame referenceFrame, YoVariableRegistry registry)
    {
       super(namePrefix, "", referenceFrame, registry);
    }
 
+   /**
+    * Creates a new yo frame point, initializes its coordinates to zero and its reference frame to
+    * {@code referenceFrame}, and registers variables to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param nameSuffix a string to use as the suffix for child variable names.
+    * @param referenceFrame the reference frame for this yo frame point.
+    * @param registry the registry to register child variables to.
+    */
    public YoFramePoint2d(String namePrefix, String nameSuffix, ReferenceFrame referenceFrame, YoVariableRegistry registry)
    {
       super(namePrefix, nameSuffix, referenceFrame, registry);
--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/frames/YoFrameVector.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/frames/YoFrameVector.java
@@ -12,16 +12,42 @@ public class YoFrameVector extends YoFrameTuple implements FixedFrameVector3DBas
    /** Only for some garbage-free operations and reducing number of operations on the YoDoubles. */
    private final FrameVector3D frameVector3D = new FrameVector3D();
 
+   /**
+    * Creates a new yo frame vector using the given yo variables and sets its reference frame to
+    * {@code referenceFrame}.
+    *
+    * @param xVariable an existing variable representing the x value of this yo frame vector.
+    * @param yVariable an existing variable representing the y value of this yo frame vector.
+    * @param zVariable an existing variable representing the z value of this yo frame vector.
+    * @param referenceFrame the reference frame for this yo frame vector.
+    */
    public YoFrameVector(YoDouble xVariable, YoDouble yVariable, YoDouble zVariable, ReferenceFrame referenceFrame)
    {
       super(xVariable, yVariable, zVariable, referenceFrame);
    }
 
+   /**
+    * Creates a new yo frame vector, initializes its coordinates to zero and its reference frame to
+    * {@code referenceFrame}, and registers variables to {@code registry}.
+    *
+    * @param referenceFrame the reference frame for this yo frame vector.
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoFrameVector(String namePrefix, ReferenceFrame referenceFrame, YoVariableRegistry registry)
    {
       super(namePrefix, "", referenceFrame, registry);
    }
 
+   /**
+    * Creates a new yo frame vector using the given yo variables and sets its reference frame to
+    * {@code referenceFrame}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param nameSuffix a string to use as the suffix for child variable names.
+    * @param referenceFrame the reference frame for this yo frame vector.
+    * @param registry the registry to register child variables to.
+    */
    public YoFrameVector(String namePrefix, String nameSuffix, ReferenceFrame referenceFrame, YoVariableRegistry registry)
    {
       super(namePrefix, nameSuffix, referenceFrame, registry);
