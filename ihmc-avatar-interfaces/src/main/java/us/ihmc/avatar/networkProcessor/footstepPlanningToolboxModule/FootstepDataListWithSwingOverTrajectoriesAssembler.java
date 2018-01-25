c721019429579484d890a288448d04950e2c8dfe
Robert <rgriffin@ihmc.us>
2018-01-25 16:22:44 -0600

    tied the footstep is adjustable boolean into the data message

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/FootstepDataListWithSwingOverTrajectoriesAssembler.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/FootstepDataListWithSwingOverTrajectoriesAssembler.java
@@ -121,7 +121,7 @@ public class FootstepDataListWithSwingOverTrajectoriesAssembler
       return footstepDataListMessage;
    }
 
-   public FootstepDataListMessage assemble(List<Footstep> footstepList, double swingTime, double transferTime, ExecutionMode executionMode,
+   public FootstepDataListMessage assemble(List<Footstep> footstepList, double swingTime, double transferTime, boolean footstepIsAdjustable, ExecutionMode executionMode,
                                            PlanarRegionsList planarRegionsList)
    {
       FootstepDataListMessage footstepDataListMessage = new FootstepDataListMessage();
@@ -148,6 +148,7 @@ public class FootstepDataListWithSwingOverTrajectoriesAssembler
          waypoints[0].set(swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().get(0));
          waypoints[1].set(swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().get(1));
          footstepDataMessage.setCustomPositionWaypoints(waypoints);
+         footstepDataMessage.setIsAdjustable(footstepIsAdjustable);
 
          if (footstep.getFootstepType() == FootstepType.PARTIAL_FOOTSTEP)
          {
