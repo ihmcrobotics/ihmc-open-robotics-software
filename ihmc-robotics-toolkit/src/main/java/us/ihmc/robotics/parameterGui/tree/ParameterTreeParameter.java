dbea51da93c20f648eef0c611101119319101203
gwiedebach <gwiedebach@ihmc.us>
2018-01-25 11:51:33 -0600

    Added parameter values to the tree view.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/parameterGui/tree/ParameterTreeParameter.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/parameterGui/tree/ParameterTreeParameter.java
@@ -1,10 +1,19 @@
 package us.ihmc.robotics.parameterGui.tree;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import us.ihmc.robotics.parameterGui.GuiParameter;
 
 public class ParameterTreeParameter implements ParameterTreeValue
 {
    private final GuiParameter parameter;
+   private ParameterNode node;
 
    public ParameterTreeParameter(GuiParameter parameter)
    {
@@ -26,5 +35,44 @@ public class ParameterTreeParameter implements ParameterTreeValue
    public GuiParameter getParameter()
    {
       return parameter;
+   }
+
+   @Override
+   public Node getOrCreateNode()
+   {
+      if (node == null)
+      {
+         node = new ParameterNode(parameter);
+      }
+      return node;
+   }
+
+   private class ParameterNode extends HBox
+   {
+      private static final String FXML_PATH = "parameter_node.fxml";
+
+      @FXML
+      private Text name;
+      @FXML
+      private Label value;
+
+      public ParameterNode(GuiParameter parameter)
+      {
+         FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_PATH));
+         loader.setRoot(this);
+         loader.setController(this);
+         try
+         {
+            loader.load();
+         }
+         catch (IOException e)
+         {
+            e.printStackTrace();
+         }
+
+         parameter.addChangedListener(p -> value.setText(parameter.getCurrentValue()));
+         name.setText(parameter.getName());
+         value.setText(parameter.getCurrentValue());
+      }
    }
 }
--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/parameterGui/tree/ParameterTreeValue.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/parameterGui/tree/ParameterTreeValue.java
@@ -1,8 +1,12 @@
 package us.ihmc.robotics.parameterGui.tree;
 
+import javafx.scene.Node;
+
 public interface ParameterTreeValue
 {
    public boolean isRegistry();
 
    public String getName();
+
+   public Node getOrCreateNode();
 }
