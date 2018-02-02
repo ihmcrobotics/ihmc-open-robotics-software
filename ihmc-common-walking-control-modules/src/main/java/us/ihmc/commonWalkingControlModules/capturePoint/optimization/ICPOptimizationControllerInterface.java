3edb74bf56b3ac7b81cdc23f113b45ce5b3706e7
Robert <rgriffin@ihmc.us>
2018-02-02 17:07:39 -0600

    added the external methods to pass in a desired cmp offset to the icp controller

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/optimization/ICPOptimizationControllerInterface.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/optimization/ICPOptimizationControllerInterface.java
@@ -35,7 +35,11 @@ public interface ICPOptimizationControllerInterface
    boolean useAngularMomentum();
    boolean useStepAdjustment();
 
-   void compute(double currentTime, FramePoint2DReadOnly desiredICP, FrameVector2DReadOnly desiredICPVelocity, FramePoint2DReadOnly perfectCMP,
+   void compute(double currentTime, FramePoint2DReadOnly desiredICP, FrameVector2DReadOnly desiredICPVelocity,
+                FramePoint2DReadOnly desiredCoP, FramePoint2DReadOnly currentICP, double omega0);
+
+   void compute(double currentTime, FramePoint2DReadOnly desiredICP, FrameVector2DReadOnly desiredICPVelocity,
+                FramePoint2DReadOnly desiredCoP, FrameVector2DReadOnly desiredCMPOffset,
                 FramePoint2DReadOnly currentICP, double omega0);
 
    void submitRemainingTimeInSwingUnderDisturbance(double remainingTimeForSwing);
