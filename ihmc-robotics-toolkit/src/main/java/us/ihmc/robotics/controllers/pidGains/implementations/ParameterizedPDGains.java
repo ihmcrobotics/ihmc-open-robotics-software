b1aae6dcf4a3eab69434f45995393d181c58c6f1
gwiedebach <gwiedebach@ihmc.us>
2018-02-05 13:09:51 -0600

    Made parameterized PID safe for null defaults in the constructor.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/controllers/pidGains/implementations/ParameterizedPDGains.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/controllers/pidGains/implementations/ParameterizedPDGains.java
@@ -24,20 +24,27 @@ public class ParameterizedPDGains implements PDGainsReadOnly
 
    public ParameterizedPDGains(String suffix, YoVariableRegistry registry)
    {
-      kp = new DoubleParameter("kp" + suffix, registry);
-      zeta = new DoubleParameter("zeta" + suffix, registry);
-      maximumFeedback = new DoubleParameter("maximumFeedback" + suffix, registry, Double.POSITIVE_INFINITY);
-      maximumFeedbackRate = new DoubleParameter("maximumFeedbackRate" + suffix, registry, Double.POSITIVE_INFINITY);
-      positionDeadband = new DoubleParameter("positionDeadband" + suffix, registry, 0.0);
+      this(suffix, null, registry);
    }
 
    public ParameterizedPDGains(String suffix, PDGainsReadOnly defaults, YoVariableRegistry registry)
    {
-      kp = new DoubleParameter("kp" + suffix, registry, defaults.getKp());
-      zeta = new DoubleParameter("zeta" + suffix, registry, GainCalculator.computeDampingRatio(defaults.getKp(), defaults.getKd()));
-      maximumFeedback = new DoubleParameter("maximumFeedback" + suffix, registry, defaults.getMaximumFeedback());
-      maximumFeedbackRate = new DoubleParameter("maximumFeedbackRate" + suffix, registry, defaults.getMaximumFeedbackRate());
-      positionDeadband = new DoubleParameter("positionDeadband" + suffix, registry, defaults.getPositionDeadband());
+      if (defaults == null)
+      {
+         kp = new DoubleParameter("kp" + suffix, registry);
+         zeta = new DoubleParameter("zeta" + suffix, registry);
+         maximumFeedback = new DoubleParameter("maximumFeedback" + suffix, registry, Double.POSITIVE_INFINITY);
+         maximumFeedbackRate = new DoubleParameter("maximumFeedbackRate" + suffix, registry, Double.POSITIVE_INFINITY);
+         positionDeadband = new DoubleParameter("positionDeadband" + suffix, registry, 0.0);
+      }
+      else
+      {
+         kp = new DoubleParameter("kp" + suffix, registry, defaults.getKp());
+         zeta = new DoubleParameter("zeta" + suffix, registry, GainCalculator.computeDampingRatio(defaults.getKp(), defaults.getKd()));
+         maximumFeedback = new DoubleParameter("maximumFeedback" + suffix, registry, defaults.getMaximumFeedback());
+         maximumFeedbackRate = new DoubleParameter("maximumFeedbackRate" + suffix, registry, defaults.getMaximumFeedbackRate());
+         positionDeadband = new DoubleParameter("positionDeadband" + suffix, registry, defaults.getPositionDeadband());
+      }
    }
 
    @Override
