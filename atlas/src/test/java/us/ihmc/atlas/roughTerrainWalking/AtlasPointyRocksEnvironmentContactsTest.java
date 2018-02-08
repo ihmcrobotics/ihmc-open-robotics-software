45d00c8791e5db8a04e80a3ba87b5cc39086b054
gwiedebach <gwiedebach@ihmc.us>
2018-02-08 13:46:35 -0600

    Fix up some line contact tests to run again. Still need to fix the actual tests.

--- atlas/src/test/java/us/ihmc/atlas/roughTerrainWalking/AtlasPointyRocksEnvironmentContactsTest.java
+++ atlas/src/test/java/us/ihmc/atlas/roughTerrainWalking/AtlasPointyRocksEnvironmentContactsTest.java
@@ -2,11 +2,19 @@ package us.ihmc.atlas.roughTerrainWalking;
 
 import org.junit.Test;
 
+import us.ihmc.atlas.AtlasJointMap;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.atlas.parameters.AtlasContactPointParameters;
+import us.ihmc.atlas.parameters.AtlasICPOptimizationParameters;
+import us.ihmc.atlas.parameters.AtlasToeOffParameters;
+import us.ihmc.atlas.parameters.AtlasWalkingControllerParameters;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.roughTerrainWalking.HumanoidPointyRocksEnvironmentContactsTest;
+import us.ihmc.commonWalkingControlModules.capturePoint.optimization.ICPOptimizationParameters;
+import us.ihmc.commonWalkingControlModules.configurations.ToeOffParameters;
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.continuousIntegration.IntegrationCategory;
@@ -18,12 +26,10 @@ import us.ihmc.wholeBodyController.FootContactPoints;
 @ContinuousIntegrationPlan(categories = {IntegrationCategory.SLOW, IntegrationCategory.VIDEO})
 public class AtlasPointyRocksEnvironmentContactsTest extends HumanoidPointyRocksEnvironmentContactsTest
 {
-   private final AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false);
-
    @Override
    public DRCRobotModel getRobotModel()
    {
-      return robotModel;
+      return null;
    }
 
    @Override
@@ -36,7 +42,7 @@ public class AtlasPointyRocksEnvironmentContactsTest extends HumanoidPointyRocks
    protected DRCRobotModel getRobotModel(int xContactPoints, int yContactPoints, boolean createOnlyEdgePoints)
    {
       FootContactPoints simulationContactPoints = new AdditionalSimulationContactPoints(xContactPoints, yContactPoints, createOnlyEdgePoints, true);
-      AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false, simulationContactPoints);
+      AtlasRobotModel robotModel = new TestModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false, simulationContactPoints);
       return robotModel;
    }
 
@@ -45,7 +51,20 @@ public class AtlasPointyRocksEnvironmentContactsTest extends HumanoidPointyRocks
    @Test(timeout = 690000)
    public void testWalkingOnLinesInEnvironment() throws SimulationExceededMaximumTimeException
    {
-      super.testWalkingOnLinesInEnvironment();
+      try
+      {
+         super.testWalkingOnLinesInEnvironment();
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+         throw e;
+      }
+      catch (AssertionError e)
+      {
+         e.printStackTrace();
+         throw e;
+      }
    }
 
    @Override
@@ -54,5 +73,81 @@ public class AtlasPointyRocksEnvironmentContactsTest extends HumanoidPointyRocks
    public void testWalkingOnPointInEnvironment() throws SimulationExceededMaximumTimeException
    {
       super.testWalkingOnPointInEnvironment();
+   }
+
+   private class TestModel extends AtlasRobotModel
+   {
+      private final TestWalkingParameters walkingParameters;
+
+      public TestModel(AtlasRobotVersion atlasVersion, RobotTarget target, boolean headless, FootContactPoints simulationContactPoints)
+      {
+         super(atlasVersion, target, headless, simulationContactPoints);
+         walkingParameters = new TestWalkingParameters(target, getJointMap(), getContactPointParameters());
+      }
+
+      @Override
+      public WalkingControllerParameters getWalkingControllerParameters()
+      {
+         return walkingParameters;
+      }
+   }
+
+   private class TestWalkingParameters extends AtlasWalkingControllerParameters
+   {
+      private final TestICPOptimizationParameters icpOptimizationParameters;
+      private final TestToeOffParameters toeOffParameters;
+
+      public TestWalkingParameters(RobotTarget target, AtlasJointMap jointMap, AtlasContactPointParameters contactPointParameters)
+      {
+         super(target, jointMap, contactPointParameters);
+         icpOptimizationParameters = new TestICPOptimizationParameters();
+         toeOffParameters = new TestToeOffParameters(jointMap);
+      }
+
+      @Override
+      public boolean createFootholdExplorationTools()
+      {
+         return true;
+      }
+
+      @Override
+      public ICPOptimizationParameters getICPOptimizationParameters()
+      {
+         return icpOptimizationParameters;
+      }
+
+      @Override
+      public ToeOffParameters getToeOffParameters()
+      {
+         return toeOffParameters;
+      }
+   }
+
+   private class TestICPOptimizationParameters extends AtlasICPOptimizationParameters
+   {
+      public TestICPOptimizationParameters()
+      {
+         super(false);
+      }
+
+      @Override
+      public boolean useAngularMomentum()
+      {
+         return true;
+      }
+   }
+
+   private class TestToeOffParameters extends AtlasToeOffParameters
+   {
+      public TestToeOffParameters(AtlasJointMap jointMap)
+      {
+         super(jointMap);
+      }
+
+      @Override
+      public boolean doToeOffIfPossible()
+      {
+         return false;
+      }
    }
 }
