fb4273fbdfd4e57c792f570c59afd6318a3edfc2
Stephen McCrory <smccrory@ihmc.us>
2018-02-08 18:06:09 -0600

    Adding a check for the end to end test finishing

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/behaviorTests/AvatarWalkOverTerrainBehaviorTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/behaviorTests/AvatarWalkOverTerrainBehaviorTest.java
@@ -19,6 +19,8 @@ import us.ihmc.humanoidRobotics.kryo.IHMCCommunicationKryoNetClassList;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.simulationConstructionSetTools.util.environments.CommonAvatarEnvironmentInterface;
 import us.ihmc.simulationConstructionSetTools.util.environments.PlanarRegionsListDefinedEnvironment;
+import us.ihmc.simulationconstructionset.FloatingJoint;
+import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
 
 import java.io.IOException;
 
@@ -34,7 +36,7 @@ public abstract class AvatarWalkOverTerrainBehaviorTest implements MultiRobotTes
    {
       cinderBlockField = PlanarRegionsListExamples.generateCinderBlockField(CINDER_BLOCK_START_X, CINDER_BLOCK_START_Y, CINDER_BLOCK_SIZE, CINDER_BLOCK_HEIGHT,
                                                                             CINDER_BLOCK_COURSE_WIDTH_X_IN_NUMBER_OF_BLOCKS,
-                                                                            CINDER_BLOCK_COURSE_LENGTH_Y_IN_NUMBER_OF_BLOCKS, CINDER_BLOCK_HEIGHT_VARIATION, 0.03);
+                                                                            CINDER_BLOCK_COURSE_LENGTH_Y_IN_NUMBER_OF_BLOCKS, CINDER_BLOCK_HEIGHT_VARIATION, - 0.03);
 
 
       simulationStarter = new DRCSimulationStarter(getRobotModel(), createCommonAvatarInterface(cinderBlockField));
@@ -61,10 +63,19 @@ public abstract class AvatarWalkOverTerrainBehaviorTest implements MultiRobotTes
       PacketCommunicator controllerCommunicator = PacketCommunicator.createIntraprocessPacketCommunicator(NetworkPorts.CONTROLLER_PORT, new IHMCCommunicationKryoNetClassList());
       controllerCommunicator.connect();
 
+      double courseLength = CINDER_BLOCK_COURSE_WIDTH_X_IN_NUMBER_OF_BLOCKS * CINDER_BLOCK_SIZE + CINDER_BLOCK_FIELD_PLATFORM_LENGTH;
+      Point3D goalPosition = new Point3D(courseLength, 0.0, 0.0);
+
       behaviorCommunicator.send(PlanarRegionMessageConverter.convertToPlanarRegionsListMessage(cinderBlockField));
       behaviorCommunicator.send(new HumanoidBehaviorTypePacket(HumanoidBehaviorType.WAlK_OVER_TERRAIN));
-      behaviorCommunicator.send(new WalkOverTerrainGoalPacket(new Point3D(5.0, 0.0, 0.0), new Quaternion()));
+      behaviorCommunicator.send(new WalkOverTerrainGoalPacket(goalPosition, new Quaternion()));
 
-      ThreadTools.sleep(20 * 1000);
+      FloatingJoint pelvisJoint = simulationStarter.getSDFRobot().getRootJoint();
+      Point3D pelvisPosition = new Point3D();
+      while(pelvisPosition.distanceXY(goalPosition) > 0.1)
+      {
+         ThreadTools.sleep(2000);
+         pelvisJoint.getPosition(pelvisPosition);
+      }
    }
 }
