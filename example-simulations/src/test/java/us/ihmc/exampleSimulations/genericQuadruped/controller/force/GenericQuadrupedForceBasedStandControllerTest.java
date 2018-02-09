448da01916aec6e11e5445176f064b95ec61bbd8
gwiedebach <gwiedebach@ihmc.us>
2018-02-09 12:52:27 -0600

    Increase some timeouts.

--- example-simulations/src/test/java/us/ihmc/exampleSimulations/genericQuadruped/controller/force/GenericQuadrupedForceBasedStandControllerTest.java
+++ example-simulations/src/test/java/us/ihmc/exampleSimulations/genericQuadruped/controller/force/GenericQuadrupedForceBasedStandControllerTest.java
@@ -1,14 +1,15 @@
 package us.ihmc.exampleSimulations.genericQuadruped.controller.force;
 
+import java.io.IOException;
+
 import org.junit.Test;
+
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.continuousIntegration.IntegrationCategory;
 import us.ihmc.exampleSimulations.genericQuadruped.GenericQuadrupedTestFactory;
 import us.ihmc.quadrupedRobotics.QuadrupedTestFactory;
 import us.ihmc.quadrupedRobotics.controller.force.QuadrupedForceBasedStandControllerTest;
-
-import java.io.IOException;
 
 @ContinuousIntegrationPlan(categories = IntegrationCategory.FAST)
 public class GenericQuadrupedForceBasedStandControllerTest extends QuadrupedForceBasedStandControllerTest
@@ -21,47 +22,47 @@ public class GenericQuadrupedForceBasedStandControllerTest extends QuadrupedForc
 
    @Override
    @ContinuousIntegrationTest(estimatedDuration = 20.0)
-   @Test(timeout = 100000)
+   @Test(timeout = 200000)
    public void testStandingUpAndAdjustingCoM() throws IOException
    {
       super.testStandingUpAndAdjustingCoM();
    }
-   
+
    @Override
    @ContinuousIntegrationTest(estimatedDuration = 20.0)
-   @Test(timeout = 150000)
+   @Test(timeout = 250000)
    public void testStandingAndResistingPushesOnBody() throws IOException
    {
       super.testStandingAndResistingPushesOnBody();
    }
-   
+
    @Override
    @ContinuousIntegrationTest(estimatedDuration = 20.0)
-   @Test(timeout = 100000)
+   @Test(timeout = 200000)
    public void testStandingAndResistingPushesOnFrontLeftHipRoll() throws IOException
    {
       super.testStandingAndResistingPushesOnFrontLeftHipRoll();
    }
-   
+
    @Override
    @ContinuousIntegrationTest(estimatedDuration = 20.0)
-   @Test(timeout = 100000)
+   @Test(timeout = 200000)
    public void testStandingAndResistingPushesOnFrontRightHipRoll() throws IOException
    {
       super.testStandingAndResistingPushesOnFrontRightHipRoll();
    }
-   
+
    @Override
    @ContinuousIntegrationTest(estimatedDuration = 20.0)
-   @Test(timeout = 100000)
+   @Test(timeout = 200000)
    public void testStandingAndResistingPushesOnHindLeftHipRoll() throws IOException
    {
       super.testStandingAndResistingPushesOnHindLeftHipRoll();
    }
-   
+
    @Override
    @ContinuousIntegrationTest(estimatedDuration = 20.0)
-   @Test(timeout = 100000)
+   @Test(timeout = 200000)
    public void testStandingAndResistingPushesOnHindRightHipRoll() throws IOException
    {
       super.testStandingAndResistingPushesOnHindRightHipRoll();
