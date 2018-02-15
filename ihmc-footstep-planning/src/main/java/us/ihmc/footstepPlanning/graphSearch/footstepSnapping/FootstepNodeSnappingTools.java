d07ac121771690e5e4fef6cdeb50662c2f69c340
Stephen McCrory <smccrory@ihmc.us>
2018-02-15 10:15:15 -0600

    adding a helper method to add a ground plane near the robot's feet

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/footstepSnapping/FootstepNodeSnappingTools.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/footstepSnapping/FootstepNodeSnappingTools.java
@@ -3,13 +3,18 @@ package us.ihmc.footstepPlanning.graphSearch.footstepSnapping;
 import java.util.ArrayList;
 
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.euclid.geometry.LineSegment2D;
 import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.euclid.tuple4D.Vector4D;
 import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
 import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNodeTools;
 import us.ihmc.robotics.geometry.PlanarRegion;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.robotics.robotSide.SideDependentList;
 
 public class FootstepNodeSnappingTools
 {
@@ -105,5 +110,57 @@ public class FootstepNodeSnappingTools
       snapTransform.preMultiplyInvertThis(stepTransform);
 
       return snapTransform;
+   }
+
+   /**
+    * Adds a planar region near midFootZUp if there are no planar regions in the vicinity
+    */
+   public static void constructGroundPlaneAroundFeet(PlanarRegionsList planarRegionsList, FootstepNode stanceFootNode, RigidBodyTransform snapTransform, double idealStepWidth, double searchWidth,
+                                                             double minSearchLength, double maxSearchLength)
+   {
+      Point2D midFootPoint = stanceFootNode.getOrComputeMidFootPoint(idealStepWidth);
+      double yaw = stanceFootNode.getYaw();
+
+      RigidBodyTransform footstepPose = new RigidBodyTransform();
+      footstepPose.setRotationYawAndZeroTranslation(yaw);
+      footstepPose.setTranslationX(midFootPoint.getX());
+      footstepPose.setTranslationY(midFootPoint.getY());
+      snapTransform.transform(footstepPose);
+
+      int numberOfChecks = 5;
+      double lengthIncrement = (maxSearchLength - minSearchLength) / ((double) numberOfChecks - 1);
+
+      ArrayList<PlanarRegion> intersectingRegions = new ArrayList<>();
+
+      for (int i = 0; i < numberOfChecks; i++)
+      {
+         double boxLength = minSearchLength + i * lengthIncrement;
+         LineSegment2D lineSegment = new LineSegment2D(boxLength, -0.5 * searchWidth, boxLength, 0.5 * searchWidth);
+         lineSegment.applyTransform(footstepPose);
+
+         intersectingRegions.clear();
+         planarRegionsList.findPlanarRegionsIntersectingLineSegment(lineSegment, intersectingRegions);
+
+         if(intersectingRegions.size() > 0)
+         {
+            if(i == 0)
+               return;
+            planarRegionsList.addPlanarRegion(constructPlanarRegion(footstepPose, searchWidth, boxLength - lengthIncrement));
+         }
+      }
+
+      planarRegionsList.addPlanarRegion(constructPlanarRegion(footstepPose, searchWidth, maxSearchLength));
+   }
+
+   private static PlanarRegion constructPlanarRegion(RigidBodyTransform transformToWorld, double width, double length)
+   {
+      ConvexPolygon2D polygon = new ConvexPolygon2D();
+      polygon.addVertex(0.0, -0.5 * length);
+      polygon.addVertex(0.0, 0.5 * length);
+      polygon.addVertex(width, -0.5 * length);
+      polygon.addVertex(width, 0.5 * length);
+      polygon.update();
+
+      return new PlanarRegion(transformToWorld, polygon);
    }
 }
