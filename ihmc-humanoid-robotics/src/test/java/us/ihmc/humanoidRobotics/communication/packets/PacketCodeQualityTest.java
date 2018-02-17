b0c806b5183099c8402551469957bd0e1672c461
Sylvain Bertrand <sbertrand@ihmc.us>
2018-02-17 13:35:50 -0600

    Added test to assert byte field naming convention.
    The convention asserted is: All byte field should contain the name of
    the enum it refers to.

--- ihmc-humanoid-robotics/src/test/java/us/ihmc/humanoidRobotics/communication/packets/PacketCodeQualityTest.java
+++ ihmc-humanoid-robotics/src/test/java/us/ihmc/humanoidRobotics/communication/packets/PacketCodeQualityTest.java
@@ -12,8 +12,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.Timeout;
 import org.reflections.Reflections;
 
 import us.ihmc.commons.PrintTools;
@@ -21,14 +26,19 @@ import us.ihmc.communication.packets.Packet;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.continuousIntegration.IntegrationCategory;
+import us.ihmc.humanoidRobotics.communication.packets.sensing.VideoPacket;
+import us.ihmc.humanoidRobotics.communication.packets.walking.SnapFootstepPacket;
 import us.ihmc.humanoidRobotics.kryo.IHMCCommunicationKryoNetClassList;
 
 @ContinuousIntegrationPlan(categories = IntegrationCategory.HEALTH)
 public class PacketCodeQualityTest
 {
+   @Rule
+   public DisableOnDebug disableOnDebug = new DisableOnDebug(new Timeout(30, TimeUnit.SECONDS));
+   
    @SuppressWarnings("rawtypes")
    @ContinuousIntegrationTest(estimatedDuration = 4.0, categoriesOverride = IntegrationCategory.FAST)
-   @Test(timeout = 30000)
+   @Test
    public void testPacketHaveNoEnum()
    {
       boolean verbose = true;
@@ -66,7 +76,7 @@ public class PacketCodeQualityTest
          {
             System.out.println();
             System.out.println();
-            PrintTools.error("List of packet null fields after creation using empty constructor:");
+            PrintTools.error("List of packet with enum fields:");
             packetTypesWithEnumFields.forEach(type -> PrintTools.error(type.getSimpleName()));
          }
       }
@@ -76,7 +86,75 @@ public class PacketCodeQualityTest
 
    @SuppressWarnings("rawtypes")
    @ContinuousIntegrationTest(estimatedDuration = 4.0, categoriesOverride = IntegrationCategory.FAST)
-   @Test(timeout = 30000)
+   @Test
+   public void testPacketByteFieldNameRefersToEnumType() throws NoSuchFieldException, SecurityException
+   {
+      boolean verbose = true;
+
+      Reflections reflections = new Reflections("us.ihmc");
+      Set<Class<? extends Packet>> allPacketTypes = reflections.getSubTypesOf(Packet.class);
+      Set<String> enumLowerCaseNames = reflections.getSubTypesOf(Enum.class).stream().map(Class::getSimpleName).map(name -> name.toLowerCase()).collect(Collectors.toSet());
+
+      Set<Class<? extends Packet>> packetTypesWithByteFieldNameNotMatchingEnum = new HashSet<>();
+
+      Set<Field> fieldsToIngore = new HashSet<>();
+      fieldsToIngore.add(VideoPacket.class.getField("data"));
+      fieldsToIngore.add(SnapFootstepPacket.class.getField("flag"));
+
+      for (Class<? extends Packet> packetType : allPacketTypes)
+      {
+         try
+         {
+            Field[] fields = packetType.getDeclaredFields();
+            for (Field field : fields)
+            {
+               if (fieldsToIngore.contains(field))
+                  continue;
+               if (Modifier.isStatic(field.getModifiers()))
+                  continue; // Ignore the static fields.
+
+               Class<?> typeToCheck = field.getType();
+               while (typeToCheck.isArray())
+                  typeToCheck = typeToCheck.getComponentType();
+
+               boolean isEnum = byte.class == typeToCheck;
+
+               if (isEnum)
+               {
+                  String expectedToContainEnumName = field.getName().toLowerCase();
+                  boolean foundMatchingEnum = enumLowerCaseNames.stream().filter(enumName -> expectedToContainEnumName.contains(enumName)).findFirst().isPresent();
+                  if (!foundMatchingEnum)
+                  {
+                     packetTypesWithByteFieldNameNotMatchingEnum.add(packetType);
+                     break;
+                  }
+               }
+            }
+         }
+         catch (Exception e)
+         {
+            PrintTools.error("Problem with packet: " + packetType.getSimpleName());
+            e.printStackTrace();
+         }
+      }
+
+      if (verbose)
+      {
+         if (!packetTypesWithByteFieldNameNotMatchingEnum.isEmpty())
+         {
+            System.out.println();
+            System.out.println();
+            PrintTools.error("List of packet byte fields not matching enums:");
+            packetTypesWithByteFieldNameNotMatchingEnum.forEach(type -> PrintTools.error(type.getSimpleName()));
+         }
+      }
+
+      assertTrue("Packet sub-types byte fields' name should contain the name of the enum they referring to.", packetTypesWithByteFieldNameNotMatchingEnum.isEmpty());
+   }
+
+   @SuppressWarnings("rawtypes")
+   @ContinuousIntegrationTest(estimatedDuration = 4.0, categoriesOverride = IntegrationCategory.FAST)
+   @Test
    public void testOnlyEmptyAndCopyConstructor()
    {
       boolean verbose = false;
@@ -137,7 +215,7 @@ public class PacketCodeQualityTest
 
    @SuppressWarnings("rawtypes")
    @ContinuousIntegrationTest(estimatedDuration = 1.0, categoriesOverride = IntegrationCategory.FAST)
-   @Test(timeout = 30000)
+   @Test
    public void testNoRandomConstructor()
    {
       boolean printPacketTypesWithRandomConstructor = false;
@@ -167,7 +245,7 @@ public class PacketCodeQualityTest
 
 
 	@ContinuousIntegrationTest(estimatedDuration = 0.1)
-   @Test(timeout = 30000)
+   @Test
    public void testAllPacketFieldsArePublic()
    {
       IHMCCommunicationKryoNetClassList classList = new IHMCCommunicationKryoNetClassList();
