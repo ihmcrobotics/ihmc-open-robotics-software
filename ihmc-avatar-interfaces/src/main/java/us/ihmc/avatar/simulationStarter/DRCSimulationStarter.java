aa2eedc5384cbf68934dfec8516a250e365629d4
gwiedebach <gwiedebach@ihmc.us>
2018-02-19 14:33:22 -0800

    Default to not creating the server after a lot of people were too scared.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/simulationStarter/DRCSimulationStarter.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/simulationStarter/DRCSimulationStarter.java
@@ -88,7 +88,7 @@ public class DRCSimulationStarter implements SimulationStarterInterface
    private boolean useHeadingAndVelocityScript = false;
    private boolean cheatWithGroundHeightAtForFootstep = false;
 
-   private boolean createYoVariableServer = true;
+   private boolean createYoVariableServer = false;
 
    private PelvisPoseCorrectionCommunicatorInterface externalPelvisCorrectorSubscriber;
    private HeadingAndVelocityEvaluationScriptParameters walkingScriptParameters;
