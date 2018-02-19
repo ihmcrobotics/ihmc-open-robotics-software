85accdb05ef55d1b0d798a8f8f6adfcfe7049b90
gwiedebach <gwiedebach@ihmc.us>
2018-02-19 15:18:26 -0800

    Fix up loading so the real robot parameter file behaves as an overwrite file.

--- atlas/src/main/java/us/ihmc/atlas/parameters/AtlasParameterFileTuner.java
+++ atlas/src/main/java/us/ihmc/atlas/parameters/AtlasParameterFileTuner.java
@@ -6,19 +6,16 @@ import java.nio.file.Paths;
 import org.apache.commons.io.FilenameUtils;
 
 import us.ihmc.atlas.AtlasRobotModel;
-import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.parameterTuner.guiElements.main.ParameterGuiInterface;
 import us.ihmc.parameterTuner.guiElements.main.ParameterTuningApplication;
 import us.ihmc.parameterTuner.offline.FileInputManager;
 
 public class AtlasParameterFileTuner extends ParameterTuningApplication
 {
-   private static final RobotTarget target = RobotTarget.SCS;
-
    @Override
    protected ParameterGuiInterface createInputManager()
    {
-      String relativeFilePath = FilenameUtils.separatorsToSystem(AtlasRobotModel.getParameterResourceName(target));
+      String relativeFilePath = FilenameUtils.separatorsToSystem(AtlasRobotModel.getParameterResourceName());
       Path filePath = Paths.get("src", "main", "resources", relativeFilePath);
       return new FileInputManager(filePath.toFile());
    }
