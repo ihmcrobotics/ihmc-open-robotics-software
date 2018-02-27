db9924ea5bcbf27c36152a77f5da0df51e54aac2
Robert <rgriffin@ihmc.us>
2018-02-27 16:44:07 -0600

    made quadrant dependent list backed by segment dependent list

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/robotSide/RobotSextant.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/robotSide/RobotSextant.java
@@ -1,9 +1,12 @@
 package us.ihmc.robotics.robotSide;
 
-public enum RobotSextant
+import java.util.EnumSet;
+
+public enum RobotSextant implements RobotSegment<RobotSextant>
 {
    FRONT_LEFT(RobotSide.LEFT), FRONT_RIGHT(RobotSide.RIGHT), MIDDLE_LEFT(RobotSide.LEFT), MIDDLE_RIGHT(RobotSide.RIGHT), HIND_LEFT(RobotSide.LEFT), HIND_RIGHT(RobotSide.RIGHT);
-   
+
+   public static final EnumSet<RobotSextant> enumSet = EnumSet.allOf(RobotSextant.class);
    public static RobotSextant[] values = values();
    private final RobotSide robotSide;
    
@@ -17,5 +20,16 @@ public enum RobotSextant
    {
       return robotSide;
    }
-   
+
+   @Override
+   public EnumSet<RobotSextant> getEnumSet()
+   {
+      return enumSet;
+   }
+
+   @Override
+   public RobotSextant[] getValues()
+   {
+      return values;
+   }
 }
