6f7723b2a7515fa6d612d824f90c1b6a6b09752e
Stephen McCrory <smccrory@ihmc.us>
2018-03-02 12:06:17 -0600

    Converting QuadrupedBodyOrientationManager parameters

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controlModules/QuadrupedBodyOrientationManager.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controlModules/QuadrupedBodyOrientationManager.java
@@ -1,5 +1,6 @@
 package us.ihmc.quadrupedRobotics.controlModules;
 
+import us.ihmc.euclid.Axis;
 import us.ihmc.euclid.referenceFrame.FrameQuaternion;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
@@ -9,10 +10,8 @@ import us.ihmc.quadrupedRobotics.controller.force.toolbox.QuadrupedBodyOrientati
 import us.ihmc.quadrupedRobotics.estimator.GroundPlaneEstimator;
 import us.ihmc.quadrupedRobotics.providers.QuadrupedPostureInputProviderInterface;
 import us.ihmc.robotics.controllers.pidGains.YoPID3DGains;
-import us.ihmc.robotics.dataStructures.parameter.DoubleArrayParameter;
-import us.ihmc.robotics.dataStructures.parameter.DoubleParameter;
-import us.ihmc.robotics.dataStructures.parameter.ParameterFactory;
 import us.ihmc.robotics.referenceFrames.OrientationFrame;
+import us.ihmc.yoVariables.parameters.DoubleParameter;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
 public class QuadrupedBodyOrientationManager
@@ -24,11 +23,14 @@ public class QuadrupedBodyOrientationManager
    private final QuadrupedBodyOrientationController controller;
    private final YoPID3DGains gains;
 
-   private final ParameterFactory parameterFactory = ParameterFactory.createWithRegistry(getClass(), registry);
-   private final DoubleArrayParameter bodyOrientationProportionalGainsParameter = parameterFactory.createDoubleArray("bodyOrientationProportionalGains", 5000, 5000, 5000);
-   private final DoubleArrayParameter bodyOrientationDerivativeGainsParameter = parameterFactory.createDoubleArray("bodyOrientationDerivativeGains", 750, 750, 750);
-   private final DoubleArrayParameter bodyOrientationIntegralGainsParameter = parameterFactory.createDoubleArray("bodyOrientationIntegralGains", 0, 0, 0);
-   private final DoubleParameter bodyOrientationMaxIntegralErrorParameter = parameterFactory.createDouble("bodyOrientationMaxIntegralError", 0);
+   private final DoubleParameter[] bodyOrientationProportionalGainsParameter = new DoubleParameter[3];
+   private final DoubleParameter[] bodyOrientationDerivativeGainsParameter = new DoubleParameter[3];
+   private final DoubleParameter[] bodyOrientationIntegralGainsParameter = new DoubleParameter[3];
+   private final DoubleParameter bodyOrientationMaxIntegralErrorParameter = new DoubleParameter("bodyOrientationMaxIntegralError", registry,  0);
+
+   private final double[] bodyOrientationProportionalGains = new double[3];
+   private final double[] bodyOrientationDerivativeGains = new double[3];
+   private final double[] bodyOrientationIntegralGains = new double[3];
 
    private final QuadrupedPostureInputProviderInterface postureProvider;
    private final GroundPlaneEstimator groundPlaneEstimator;
@@ -44,6 +46,13 @@ public class QuadrupedBodyOrientationManager
       this.controllerToolbox = controllerToolbox;
       this.postureProvider = postureProvider;
 
+      for (int i = 0; i < 3; i++)
+      {
+         bodyOrientationProportionalGainsParameter[i] = new DoubleParameter("bodyOrientationProportionalGain" + Axis.values[i], registry, 5000.0);
+         bodyOrientationDerivativeGainsParameter[i] = new DoubleParameter("bodyOrientationDerivativeGain" + Axis.values[i], registry, 750.0);
+         bodyOrientationIntegralGainsParameter[i] = new DoubleParameter("bodyOrientationIntegralGain" + Axis.values[i], registry, 0.0);
+      }
+
       controller = new QuadrupedBodyOrientationController(controllerToolbox, registry);
       groundPlaneEstimator = controllerToolbox.getGroundPlaneEstimator();
       gains = controller.getGains();
@@ -56,9 +65,16 @@ public class QuadrupedBodyOrientationManager
 
    private void updateGains()
    {
-      gains.setProportionalGains(bodyOrientationProportionalGainsParameter.get());
-      gains.setIntegralGains(bodyOrientationIntegralGainsParameter.get(), bodyOrientationMaxIntegralErrorParameter.get());
-      gains.setDerivativeGains(bodyOrientationDerivativeGainsParameter.get());
+      for (int i = 0; i < 3; i++)
+      {
+         bodyOrientationProportionalGains[i] = bodyOrientationProportionalGainsParameter[i].getValue();
+         bodyOrientationDerivativeGains[i] = bodyOrientationDerivativeGainsParameter[i].getValue();
+         bodyOrientationIntegralGains[i] = bodyOrientationIntegralGainsParameter[i].getValue();
+      }
+
+      gains.setProportionalGains(bodyOrientationProportionalGains);
+      gains.setIntegralGains(bodyOrientationIntegralGains, bodyOrientationMaxIntegralErrorParameter.getValue());
+      gains.setDerivativeGains(bodyOrientationDerivativeGains);
    }
 
    public void initialize(FrameQuaternionReadOnly bodyOrientationEstimate)
