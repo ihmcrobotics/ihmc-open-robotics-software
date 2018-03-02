805a0c2b8bda243b0a30efa1cf80c456fee8ce45
Stephen McCrory <smccrory@ihmc.us>
2018-03-02 15:19:36 -0600

    Converting QuadrupedStepTeleopMode parameters and making a teleop parameters file

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/simulation/GenericQuadrupedTeleopNode.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/simulation/GenericQuadrupedTeleopNode.java
@@ -10,14 +10,19 @@ import us.ihmc.robotModels.FullQuadrupedRobotModel;
 import us.ihmc.tools.inputDevices.joystick.Joystick;
 import us.ihmc.tools.inputDevices.joystick.JoystickModel;
 import us.ihmc.exampleSimulations.genericQuadruped.GenericQuadrupedSimulationFactory;
+import us.ihmc.wholeBodyController.parameters.ParameterLoaderHelper;
+import us.ihmc.yoVariables.parameters.YoParameter;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * To run with simulation, make sure {@link GenericQuadrupedSimulationFactory#USE_NETWORKING} is true
  */
 public class GenericQuadrupedTeleopNode
 {
+   private static final String parameterResourcePath = "/parameters/teleop.xml";
+
    private GenericQuadrupedTeleopNode() throws IOException, InterruptedException
    {
       String host = "localhost";
@@ -29,6 +34,10 @@ public class GenericQuadrupedTeleopNode
 
       Joystick joystick = new Joystick(JoystickModel.XBOX_ONE, 0);
       QuadrupedBodyTeleopNode eventListener = new QuadrupedBodyTeleopNode(host, port, netClassList, joystick, fullRobotModel, physicalProperties);
+
+      InputStream parameterFile = getClass().getResourceAsStream(parameterResourcePath);
+      ParameterLoaderHelper.loadParameters(this, parameterFile, eventListener.getRegistry());
+
       eventListener.start();
       joystick.addJoystickEventListener(eventListener);
    }
