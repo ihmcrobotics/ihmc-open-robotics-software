50a5b4aec9c54b12340d5f7abe9422d4ee328422
Robert <rgriffin@ihmc.us>
2018-03-09 09:21:16 -0600

    added some static lists to the controller core command type

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/ControllerCoreCommandType.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/ControllerCoreCommandType.java
@@ -8,5 +8,37 @@ public enum ControllerCoreCommandType
    EXTERNAL_WRENCH, PLANE_CONTACT_STATE, CENTER_OF_PRESSURE,
    JOINT_ACCELERATION_INTEGRATION,
    COMMAND_LIST,
-   VIRTUAL_WRENCH, VIRTUAL_FORCE, VIRTUAL_TORQUE, CONTROLLED_BODIES
+   VIRTUAL_WRENCH, VIRTUAL_FORCE, VIRTUAL_TORQUE, CONTROLLED_BODIES;
+
+   private static final ControllerCoreCommandType[] inverseKinematicsCommands = { TASKSPACE, JOINTSPACE, MOMENTUM, PRIVILEGED_CONFIGURATION, PRIVILEGED_VELOCITY,
+         LIMIT_REDUCTION, COMMAND_LIST };
+
+   private static final ControllerCoreCommandType[] inverseDynamicsCommands = { TASKSPACE, JOINTSPACE, MOMENTUM, PRIVILEGED_CONFIGURATION, PRIVILEGED_ACCELERATION,
+         LIMIT_REDUCTION, JOINT_LIMIT_ENFORCEMENT, EXTERNAL_WRENCH, PLANE_CONTACT_STATE, CENTER_OF_PRESSURE, JOINT_ACCELERATION_INTEGRATION, COMMAND_LIST };
+
+   private static final ControllerCoreCommandType[] virtualModelControlCommands = { MOMENTUM, EXTERNAL_WRENCH, PLANE_CONTACT_STATE, VIRTUAL_WRENCH, VIRTUAL_FORCE,
+         VIRTUAL_TORQUE, JOINTSPACE, CONTROLLED_BODIES, COMMAND_LIST };
+
+   private static final ControllerCoreCommandType[] feedbackControlCommands = { TASKSPACE, POINT, ORIENTATION, JOINTSPACE, MOMENTUM, COMMAND_LIST };
+
+   public static ControllerCoreCommandType[] getInverseKinematicsCommands()
+   {
+      return inverseKinematicsCommands;
+   }
+
+   public static ControllerCoreCommandType[] getInverseDynamicsCommands()
+   {
+      return inverseDynamicsCommands;
+   }
+
+   public static ControllerCoreCommandType[] getVirtualModelControlCommands()
+   {
+      return virtualModelControlCommands;
+   }
+
+   public static ControllerCoreCommandType[] getFeedbackControlCommands()
+   {
+      return feedbackControlCommands;
+   }
+
 }
