135b5baf6435b02faf371323ae705a67ad2866d5
Robert <rgriffin@ihmc.us>
2018-03-19 11:20:52 -0500

    added the tools to setup the controller core toolbox

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controller/force/states/QuadrupedSteppingState.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controller/force/states/QuadrupedSteppingState.java
@@ -1,5 +1,6 @@
 package us.ihmc.quadrupedRobotics.controller.force.states;
 
+import us.ihmc.commonWalkingControlModules.controllerCore.WholeBodyControlCoreToolbox;
 import us.ihmc.communication.net.PacketConsumer;
 import us.ihmc.communication.streamingData.GlobalDataProducer;
 import us.ihmc.quadrupedRobotics.communication.packets.QuadrupedSteppingEventPacket;
@@ -18,6 +19,7 @@ import us.ihmc.quadrupedRobotics.providers.QuadrupedPlanarVelocityInputProvider;
 import us.ihmc.quadrupedRobotics.providers.QuadrupedPreplannedStepInputProvider;
 import us.ihmc.quadrupedRobotics.providers.QuadrupedSoleWaypointInputProvider;
 import us.ihmc.quadrupedRobotics.providers.YoQuadrupedXGaitSettings;
+import us.ihmc.robotModels.FullQuadrupedRobotModel;
 import us.ihmc.robotics.stateMachine.old.eventBasedStateMachine.FiniteStateMachine;
 import us.ihmc.robotics.stateMachine.old.eventBasedStateMachine.FiniteStateMachineBuilder;
 import us.ihmc.robotics.stateMachine.old.eventBasedStateMachine.FiniteStateMachineYoVariableTrigger;
@@ -56,6 +58,14 @@ public class QuadrupedSteppingState implements QuadrupedController
       this.runtimeEnvironment = runtimeEnvironment;
       this.controllerToolbox = controllerToolbox;
       this.controlManagerFactory = controlManagerFactory;
+
+      FullQuadrupedRobotModel fullRobotModel = runtimeEnvironment.getFullRobotModel();
+      WholeBodyControlCoreToolbox controlCoreToolbox = new WholeBodyControlCoreToolbox(runtimeEnvironment.getControlDT(), runtimeEnvironment.getGravity(),
+                                                                                       fullRobotModel.getRootJoint(), fullRobotModel.getControllableOneDoFJoints(),
+                                                                                       controllerToolbox.getReferenceFrames().getCenterOfMassFrame(),
+                                                                                       runtimeEnvironment.getControllerCoreOptimizationSettings(),
+                                                                                       runtimeEnvironment.getGraphicsListRegistry(), registry);
+      controlCoreToolbox.setupForVirtualModelControlSolver(fullRobotModel.getBody(), controllerToolbox.getContactablePlaneBodies());
 
       // Initialize input providers.
       xGaitSettingsProvider = new YoQuadrupedXGaitSettings(runtimeEnvironment.getXGaitSettings(), runtimeEnvironment.getGlobalDataProducer(), registry);
