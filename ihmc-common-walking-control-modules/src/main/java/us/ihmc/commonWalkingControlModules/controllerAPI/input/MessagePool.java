07f66aaabf8701baf92f09c768ee46d1c9dc3bc7
Sylvain Bertrand <sbertrand@ihmc.us>
2018-03-30 20:14:12 -0500

    Documented the message collection framework.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerAPI/input/MessagePool.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerAPI/input/MessagePool.java
@@ -8,11 +8,23 @@ import java.util.Map;
 import us.ihmc.communication.packets.Packet;
 import us.ihmc.robotics.lists.RecyclingArrayList;
 
+/**
+ * This class provides a simple way of creating a pool of messages that is garbage free.
+ * 
+ * @author Sylvain Bertrand
+ */
 public class MessagePool
 {
+   /** The list of pools, one for each message. */
    private final List<RecyclingArrayList<? extends Packet<?>>> messagePoolList = new ArrayList<>();
+   /** The map for retrieving a message pool given its class. */
    private final Map<Class<?>, RecyclingArrayList<? extends Packet<?>>> messagePoolMap = new HashMap<>();
 
+   /**
+    * Creates a new message pool only for the given message types.
+    * 
+    * @param supportedMessages the list of the message types to be supported.
+    */
    public MessagePool(List<Class<? extends Packet<?>>> supportedMessages)
    {
       for (int i = 0; i < supportedMessages.size(); i++)
@@ -24,6 +36,12 @@ public class MessagePool
       }
    }
 
+   /**
+    * Requests an available instance of the given type.
+    * 
+    * @param messageClass the type to get an instance of.
+    * @return the instance.
+    */
    public <T extends Packet<T>> T requestMessage(Class<T> messageClass)
    {
       @SuppressWarnings("unchecked")
@@ -31,6 +49,12 @@ public class MessagePool
       return messagePool.add();
    }
 
+   /**
+    * Marks all the internal memory as available for use.
+    * <p>
+    * Once this method has been called, messages previously requested will be recycled.
+    * </p>
+    */
    public void reset()
    {
       for (int i = 0; i < messagePoolList.size(); i++)
