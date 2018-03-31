fb1db1ea4e9d5d071af5f87d8607320189c9e6e7
Sylvain Bertrand <sbertrand@ihmc.us>
2018-03-30 20:14:12 -0500

    Documented the message collection framework.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerAPI/input/MessageCollectionMessenger.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerAPI/input/MessageCollectionMessenger.java
@@ -16,11 +16,18 @@ import us.ihmc.communication.packetCommunicator.PacketCommunicator;
 import us.ihmc.communication.packets.Packet;
 
 /**
- * MessageOfMessages provides a generic way to send a collection of messages to the controller.
+ * Tool for setting up and sending collections of messages that are to be processed by the
+ * controller at the same time.
  */
 @SuppressWarnings("rawtypes")
 public class MessageCollectionMessenger
 {
+   /**
+    * Maximum duration in milliseconds to wait for a {@link MessageCollectionNotification} once a
+    * {@link MessageCollection} has been sent.
+    */
+   private static final int NOTIFICATION_TIMEOUT = 5000;
+
    private long sequenceID;
 
    private PublishingTask publishingTask = null;
@@ -29,11 +36,22 @@ public class MessageCollectionMessenger
    private List<Packet<?>> packets = new ArrayList<>();
    private final Map<Class, Method> sequenceIDSetterMap = new HashMap<>();
 
+   /**
+    * Creates a new messenger ready to register a new collection of messages to be sent to the
+    * controller.
+    */
    public MessageCollectionMessenger()
    {
       this(new Random(1651).nextLong());
    }
 
+   /**
+    * Creates a new messenger ready to register a new collection of messages to be sent to the
+    * controller.
+    * 
+    * @param startSequenceID the ID of the collection message and start ID for all the messages of
+    *           the collection.
+    */
    public MessageCollectionMessenger(long startSequenceID)
    {
       sequenceID = startSequenceID;
@@ -41,22 +59,58 @@ public class MessageCollectionMessenger
       incrementSequenceID();
    }
 
-   public boolean addMessage(Packet<?> packet)
+   /**
+    * Convenience method for adding several messages.
+    * 
+    * @param messages the messages to add.
+    * @see #addMessage(Packet)
+    */
+   public void addMessages(Iterable<Packet<?>> messages)
    {
-      Class<? extends Packet> packetType = packet.getClass();
+      messages.forEach(this::addMessage);
+   }
 
-      Method sequenceIDSetter = sequenceIDSetterMap.get(packetType);
+   /**
+    * Convenience method for adding several messages.
+    * 
+    * @param messages the messages to add.
+    * @see #addMessage(Packet)
+    */
+   public void addMessages(Packet<?>... messages)
+   {
+      for (Packet<?> message : messages)
+      {
+         addMessage(message);
+      }
+   }
+
+   /**
+    * Add a message to the current collection.
+    * <p>
+    * Only messages with a field "{@code long sequence_id_}" and a setter
+    * "{@code setUniqueId(long)}" can be added to a collection.
+    * </p>
+    * 
+    * @param message the message to add.
+    * @return {@code true} if the message was properly added to the collection, {@code false}
+    *         otherwise.
+    */
+   public boolean addMessage(Packet<?> message)
+   {
+      Class<? extends Packet> messageType = message.getClass();
+
+      Method sequenceIDSetter = sequenceIDSetterMap.get(messageType);
 
       if (sequenceIDSetter == null)
       {
          try
          {
-            sequenceIDSetter = packetType.getMethod("setSequenceId", long.class);
-            sequenceIDSetterMap.put(packetType, sequenceIDSetter);
+            sequenceIDSetter = messageType.getMethod("setSequenceId", long.class);
+            sequenceIDSetterMap.put(messageType, sequenceIDSetter);
          }
          catch (NoSuchMethodException | SecurityException e)
          {
-            PrintTools.error("The message type: " + packetType.getSimpleName()
+            PrintTools.error("The message type: " + messageType.getSimpleName()
                   + " cannot be sent with a MessageCollection, it needs to declare a sequenceID field.");
             return false;
          }
@@ -64,14 +118,14 @@ public class MessageCollectionMessenger
 
       try
       {
-         sequenceIDSetter.invoke(packet, sequenceID);
+         sequenceIDSetter.invoke(message, sequenceID);
       }
       catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
       {
-         throw new RuntimeException("Something went wrong when setting the sequenceID for the message " + packetType.getSimpleName(), e);
+         throw new RuntimeException("Something went wrong when setting the sequenceID for the message " + messageType.getSimpleName(), e);
       }
 
-      packets.add(packet);
+      packets.add(message);
       collection.getSequences().add(sequenceID);
 
       incrementSequenceID();
@@ -87,22 +141,31 @@ public class MessageCollectionMessenger
          sequenceID++;
    }
 
-   public void addMessages(Iterable<Packet<?>> messages)
-   {
-      for (Packet<?> packet : messages)
-      {
-         addMessage(packet);
-      }
-   }
-
-   public void addMessages(Packet<?>... messages)
-   {
-      for (Packet<?> packet : messages)
-      {
-         addMessage(packet);
-      }
-   }
-
+   /**
+    * Sends, in a safe manner, first a {@code MessageCollection} to indicate that the following
+    * messages should be gathered and processed at the same time, and then the collection of
+    * messages that have been added via {@link #addMessage(Packet)}.
+    * <p>
+    * This method is safer than {@link #sendMessageCollectionUnsafe(Messenger)} as before actually
+    * sending the messages, it will wait for first receiving a
+    * {@link MessageCollectionNotification}. The notification indicates that the controller is ready
+    * to collect the collection of messages.
+    * </p>
+    * <p>
+    * If the controller does not send a notification within a given duration, the task is cancelled
+    * and the internal memory of {@code this} is cleared.
+    * </p>
+    * <p>
+    * If this method is executed on the local thread, i.e. {@code runOnThread = false}, the
+    * execution will be temporarily paused to wait for a notification. If this is undesirable,
+    * running the task on a different thread, i.e. {@code runOnThread = true} will prevent this
+    * effect.
+    * </p>
+    * 
+    * @param packetCommunicator used to communicate with the controller.
+    * @param runOnThread whether this method should be executed on a separate thread.
+    * @see #NOTIFICATION_TIMEOUT
+    */
    public void sendMessageCollectionSafe(PacketCommunicator packetCommunicator, boolean runOnThread)
    {
       PacketConsumer<MessageCollectionNotification> listener = this::receivedNotification;
@@ -111,12 +174,26 @@ public class MessageCollectionMessenger
       sendMessageCollectionSafe(packetCommunicator::send, runOnThread, () -> packetCommunicator.detachListener(MessageCollectionNotification.class, listener));
    }
 
+   /**
+    * Sends, in a safe manner, first a {@code MessageCollection} to indicate that the following
+    * messages should be gathered and processed at the same time, and then the collection of
+    * messages that have been added via {@link #addMessage(Packet)}.
+    * <p>
+    * When using this method, the user should register the method
+    * {@link #receivedNotification(MessageCollectionNotification)} as a listener to the
+    * communication, so {@code this} can receive the notification from the controller.
+    * </p>
+    * 
+    * @param messenger the protocol for sending messages to the controller.
+    * @param runOnThread whether this method should be executed on a separate thread.
+    * @see #sendMessageCollectionSafe(PacketCommunicator, boolean)
+    */
    public void sendMessageCollectionSafe(Messenger messenger, boolean runOnThread)
    {
       sendMessageCollectionSafe(messenger, runOnThread, null);
    }
 
-   public void sendMessageCollectionSafe(Messenger messenger, boolean runOnThread, Runnable postProcess)
+   private void sendMessageCollectionSafe(Messenger messenger, boolean runOnThread, Runnable postProcess)
    {
       publishingTask = new PublishingTask(messenger, collection, packets, postProcess);
 
@@ -132,6 +209,17 @@ public class MessageCollectionMessenger
       }
    }
 
+   /**
+    * Sends immediately a {@code MessageCollection} directly followed by the collection of messages
+    * to the controller.
+    * <p>
+    * This method strongly rely on the reliability of the communication and expect the
+    * {@code MessageCollection} to arrive first. Prefer using
+    * {@link #sendMessageCollectionSafe(PacketCommunicator, boolean)}.
+    * </p>
+    * 
+    * @param messenger the protocol for sending messages to the controller.
+    */
    public void sendMessageCollectionUnsafe(Messenger messenger)
    {
       messenger.sendMessage(collection);
@@ -140,10 +228,18 @@ public class MessageCollectionMessenger
       {
          messenger.sendMessage(packets.get(i));
       }
-      collection.getSequences().reset();
-      packets.clear();
+      reset();
    }
 
+   /**
+    * Notifies {@code this} that the controller just received a {@link MessageCollection}.
+    * <p>
+    * This method triggers the publishing of the collection of messages when using
+    * {@link #sendMessageCollectionSafe(Messenger, boolean)}.
+    * </p>
+    * 
+    * @param notification the notification.
+    */
    public void receivedNotification(MessageCollectionNotification notification)
    {
       if (collection == null)
@@ -159,8 +255,27 @@ public class MessageCollectionMessenger
       }
    }
 
+   private void reset()
+   {
+      collection.getSequences().reset();
+      incrementSequenceID();
+      collection.setSequenceId(sequenceID);
+      incrementSequenceID();
+      packets.clear();
+   }
+
+   /**
+    * Implements this interface to provide a protocol for sending messages.
+    * 
+    * @author Sylvain Bertrand
+    */
    public static interface Messenger
    {
+      /**
+       * Request for sending the given message.
+       * 
+       * @param message the message to be sent.
+       */
       void sendMessage(Packet<?> message);
    }
 
@@ -190,7 +305,7 @@ public class MessageCollectionMessenger
 
             try
             {
-               notificationSync.wait(5000);
+               notificationSync.wait(NOTIFICATION_TIMEOUT);
             }
             catch (InterruptedException e)
             {
@@ -214,8 +329,7 @@ public class MessageCollectionMessenger
             }
 
             hasReceivedNotification = false;
-            collection.getSequences().reset();
-            packets.clear();
+            reset();
             if (postProcess != null)
                postProcess.run();
          }
--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerAPI/input/MessageCollector.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerAPI/input/MessageCollector.java
@@ -11,17 +11,51 @@ import us.ihmc.commons.PrintTools;
 import us.ihmc.communication.packets.Packet;
 import us.ihmc.idl.IDLSequence;
 
+/**
+ * A {@code MessageCollector} can be added to a {@link ControllerNetworkSubscriber}.
+ * <p>
+ * It works with {@link MessageCollection} and allows the user to ensure that messages sent in group
+ * are also processed in group.
+ * </p>
+ * <p>
+ * See {@link MessageCollectionMessenger} for setting up and sending a collection of messages that
+ * are to be processed together.
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ */
 public class MessageCollector
 {
+   /** Whether this collector is currently waiting for additional messages of a collection. */
    private boolean isCollecting = false;
-
+   /**
+    * The ID set of the messages that are part of a collection. This set is empty when this
+    * collector is not active, when active the set contains the ID of the messages that are still to
+    * be collected.
+    */
    private final TLongHashSet expectedMessageIDs;
+   /** The list of the collection messages that have already been collected. */
    private final List<Packet<?>> interceptedMessages;
+   /** A view list of {@link #interceptedMessages}, it is for read-only purposes. */
    private final List<Packet<?>> interceptedMessagesView;
+   /**
+    * The internal buffer used to save a copy of the intercepted messages without holding onto the
+    * reference of a message that was created outside this collector.
+    */
    private final MessagePool messagePool;
+   /** Extractor used to get the ID of the messages. */
    private final MessageIDExtractor messageIDExtractor;
+   /**
+    * Notification that sent to notify the user that this collector has just received a
+    * {@code MessageCollection} and is now active and waiting to collect the messages.
+    */
    private final MessageCollectionNotification notification;
 
+   /**
+    * Dummy collector that is never active, for convenience purposes.
+    * 
+    * @return the dummy collector.
+    */
    public static MessageCollector createDummyCollector()
    {
       return new MessageCollector();
@@ -37,6 +71,15 @@ public class MessageCollector
       notification = null;
    }
 
+   /**
+    * Creates a message collector.
+    * <p>
+    * A message collector is necessary to support {@code MessageCollection}.
+    * </p>
+    * 
+    * @param messageIDExtractor provides the method for getting the IDs from the messages.
+    * @param supportedMessages the list of messages that this collector can handle.
+    */
    public MessageCollector(MessageIDExtractor messageIDExtractor, List<Class<? extends Packet<?>>> supportedMessages)
    {
       this.messageIDExtractor = messageIDExtractor;
@@ -47,6 +90,12 @@ public class MessageCollector
       notification = new MessageCollectionNotification();
    }
 
+   /**
+    * Disables this collector and marks its internal memory as available.
+    * <p>
+    * Should be called only once the intercepted messages have been processed.
+    * </p>
+    */
    public void reset()
    {
       if (messagePool == null)
@@ -58,6 +107,16 @@ public class MessageCollector
       messagePool.reset();
    }
 
+   /**
+    * Enables this collector.
+    * <p>
+    * This collector is now waiting for the messages referred by the IDs contained in the given
+    * {@code collection}.
+    * </p>
+    * 
+    * @param collection the message holding onto the IDs of the messages to collect.
+    * @return a notification ready to be sent back to the user.
+    */
    public MessageCollectionNotification startCollecting(MessageCollection collection)
    {
       if (messagePool == null)
@@ -77,6 +136,19 @@ public class MessageCollector
       return notification;
    }
 
+   /**
+    * Tests the given message and hold onto it if this collector is active and was waiting for the
+    * given message.
+    * <p>
+    * If this collector is inactive or not waiting for the given message, the message is not saved
+    * and this method returns {@code false}.
+    * </p>
+    * 
+    * @param message the message to possibly intercept.
+    * @return {@code true} if the message is part of a collection that this collector is expecting,
+    *         {@code false} if this collector is inactive or if the message is not part of the
+    *         current collection gathered by this collector.
+    */
    @SuppressWarnings({"unchecked", "rawtypes"})
    public boolean interceptMessage(Packet<?> message)
    {
@@ -103,6 +175,11 @@ public class MessageCollector
       return intercept;
    }
 
+   /**
+    * Whether this collector is currently gathering messages.
+    * 
+    * @return {@code true} if collecting, {@code false} otherwise.
+    */
    public boolean isCollecting()
    {
       if (messagePool == null)
@@ -111,20 +188,39 @@ public class MessageCollector
       return isCollecting;
    }
 
-   public boolean hasCollectedMessages()
-   {
-      return !interceptedMessagesView.isEmpty();
-   }
-
+   /**
+    * Gets a view of the collected messages.
+    * <p>
+    * These messages should only be processed once this collector is done collecting, i.e.
+    * {@link #isCollecting()} returns {@code false}.
+    * </p>
+    * 
+    * @return the collected messages.
+    */
    public List<Packet<?>> getCollectedMessages()
    {
       return interceptedMessagesView;
    }
 
+   /**
+    * Implement this interface to provide a method for getting the ID of messages.
+    * 
+    * @author Sylvain Bertrand
+    */
    public static interface MessageIDExtractor
    {
+      /**
+       * Default value for marking a message as without an ID. A message without an ID can never be
+       * part of a collection.
+       */
       static final long NO_ID = 0;
 
+      /**
+       * Gets the ID of the given message.
+       * 
+       * @param message the message to get the ID of.
+       * @return the ID or {@link #NO_ID} if the message should not be processed.
+       */
       public long getMessageID(Packet<?> message);
    }
 }
