29b32fd7b72ecbe5b3e6dda63eab5b792ca3528b
gwiedebach <gwiedebach@ihmc.us>
2018-04-04 09:59:05 -0500

    Some more cleanup.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/sliderboard/boards/BCF2000ControlMap.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/sliderboard/boards/BCF2000ControlMap.java
@@ -5,28 +5,6 @@ import us.ihmc.robotics.sliderboard.MidiControlMap;
 public class BCF2000ControlMap implements MidiControlMap
 {
    @Override
-   public int getKnobChannel(int knobIndex)
-   {
-      if (!MidiControlMap.isInRange(knobIndex, 0, 8))
-      {
-         return -1;
-      }
-
-      return knobIndex;
-   }
-
-   @Override
-   public int getKnobIndex(int knobChannel)
-   {
-      if (!MidiControlMap.isInRange(knobChannel, 0, 8))
-      {
-         return -1;
-      }
-
-      return knobChannel;
-   }
-
-   @Override
    public int getSliderChannel(int sliderIndex)
    {
       if (!MidiControlMap.isInRange(sliderIndex, 0, 8))
