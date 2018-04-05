40239838763e322de35e2d689c98cbdad4872000
Robert <rgriffin@ihmc.us>
2018-04-05 09:09:12 -0500

    removed some now unused code

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/messageHandling/QuadrupedStepMessageHandler.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/messageHandling/QuadrupedStepMessageHandler.java
@@ -2,28 +2,18 @@ package us.ihmc.quadrupedRobotics.messageHandling;
 
 import us.ihmc.commons.MathTools;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
-import us.ihmc.euclid.referenceFrame.FrameQuaternion;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.referenceFrame.interfaces.FrameQuaternionReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
-import us.ihmc.humanoidRobotics.bipedSupportPolygons.ContactablePlaneBody;
-import us.ihmc.humanoidRobotics.communication.controllerAPI.command.FootTrajectoryCommand;
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.QuadrupedTimedStepCommand;
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.QuadrupedTimedStepListCommand;
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.SoleTrajectoryCommand;
-import us.ihmc.humanoidRobotics.footstep.Footstep;
-import us.ihmc.quadrupedRobotics.estimator.referenceFrames.QuadrupedReferenceFrames;
 import us.ihmc.quadrupedRobotics.planning.YoQuadrupedTimedStep;
-import us.ihmc.quadrupedRobotics.util.TimeInterval;
 import us.ihmc.quadrupedRobotics.util.TimeIntervalTools;
 import us.ihmc.quadrupedRobotics.util.YoPreallocatedList;
 import us.ihmc.robotics.lists.RecyclingArrayDeque;
 import us.ihmc.robotics.lists.RecyclingArrayList;
-import us.ihmc.robotics.math.frames.YoFrameOrientation;
 import us.ihmc.robotics.robotSide.QuadrantDependentList;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
-import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.yoVariables.parameters.DoubleParameter;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
@@ -41,8 +31,6 @@ public class QuadrupedStepMessageHandler
 
    private final QuadrantDependentList<RecyclingArrayDeque<SoleTrajectoryCommand>> upcomingFootTrajectoryCommandList = new QuadrantDependentList<>();
 
-   private final QuadrupedReferenceFrames referenceFrames;
-   private final YoFrameOrientation bodyOrientation;
    private final ArrayList<YoQuadrupedTimedStep> activeSteps = new ArrayList<>();
    private final YoDouble robotTimestamp;
    private final DoubleParameter haltTransitionDurationParameter = new DoubleParameter("haltTransitionDuration", registry, 0.0);
@@ -54,13 +42,11 @@ public class QuadrupedStepMessageHandler
 
    private final FramePoint3D tempPoint = new FramePoint3D();
 
-   public QuadrupedStepMessageHandler(YoDouble robotTimestamp, QuadrupedReferenceFrames referenceFrames, YoVariableRegistry parentRegistry)
+   public QuadrupedStepMessageHandler(YoDouble robotTimestamp, YoVariableRegistry parentRegistry)
    {
       this.robotTimestamp = robotTimestamp;
       this.receivedStepSequence = new YoPreallocatedList<>("receivedStepSequence", registry, STEP_QUEUE_SIZE, YoQuadrupedTimedStep::new);
       this.adjustedStepSequence = new YoPreallocatedList<>("adjustedStepSequence", registry, STEP_QUEUE_SIZE, YoQuadrupedTimedStep::new);
-      this.referenceFrames = referenceFrames;
-      this.bodyOrientation = new YoFrameOrientation("bodyOrientation", ReferenceFrame.getWorldFrame(), registry);
 
       for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
          upcomingFootTrajectoryCommandList.put(robotQuadrant, new RecyclingArrayDeque<>(SoleTrajectoryCommand.class));
@@ -71,16 +57,6 @@ public class QuadrupedStepMessageHandler
    public boolean isStepPlanAvailable()
    {
       return receivedStepSequence.size() > 0;
-   }
-
-   public void getBodyOrientation(FrameQuaternion bodyOrientation)
-   {
-      bodyOrientation.setIncludingFrame(this.bodyOrientation.getFrameOrientation());
-   }
-
-   public FrameQuaternionReadOnly getBodyOrientation()
-   {
-      return bodyOrientation.getFrameOrientation();
    }
 
    /**
@@ -117,7 +93,6 @@ public class QuadrupedStepMessageHandler
       }
 
       TimeIntervalTools.sortByEndTime(receivedStepSequence);
-      bodyOrientation.setFromReferenceFrame(referenceFrames.getCenterOfFeetZUpFrameAveragingLowestZHeightsAcrossEnds());
    }
 
    public void clearSteps()
