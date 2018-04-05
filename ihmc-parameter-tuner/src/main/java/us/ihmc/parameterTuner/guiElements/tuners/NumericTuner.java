94c98bd3404a02d7602352b1bb112ba90630ab94
gwiedebach <gwiedebach@ihmc.us>
2018-04-05 17:06:03 -0500

    Fix rounding issue that would cause parameters with high precision to be rounded and marked as modified by the slider in the tuning GUI.

--- ihmc-parameter-tuner/src/main/java/us/ihmc/parameterTuner/guiElements/tuners/NumericTuner.java
+++ ihmc-parameter-tuner/src/main/java/us/ihmc/parameterTuner/guiElements/tuners/NumericTuner.java
@@ -33,9 +33,14 @@ public abstract class NumericTuner<T extends Number> extends HBox implements Inp
          Platform.runLater(() -> parameter.setMax(max.getValueAsText()));
       });
 
-      slider.valueProperty().addListener((observable, oldValue, newValue) -> {
-         value.setValue(slider.getNumber());
+      // Use the slider value when it gets clicked or dragged and released. Otherwise unwanted rounding will happen.
+      slider.valueChangingProperty().addListener((observable, oldValue, newValue) -> {
+         if (!newValue && oldValue)
+         {
+            value.setValue(slider.getNumber());
+         }
       });
+      slider.setOnMouseClicked(mouseEvent -> value.setValue(slider.getNumber()));
 
       parameter.addChangedListener(p -> {
          // This listener will be triggered by an external change and is called from the animation timer.
