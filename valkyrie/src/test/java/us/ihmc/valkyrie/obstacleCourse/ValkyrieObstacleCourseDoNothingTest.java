8f4427bf38404939383fb8ac22787555c09db769
InhoLee <ilee@ihmc.us>
2018-04-06 13:50:13 -0500

    Do nothing test for valkyrie.

---
+++ valkyrie/src/test/java/us/ihmc/valkyrie/obstacleCourse/ValkyrieObstacleCourseDoNothingTest.java
@@ -0,0 +1,36 @@
+package us.ihmc.valkyrie.obstacleCourse;
+
+import org.junit.Test;
+
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.obstacleCourseTests.DRCObstacleCourseDoNothingTest;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+
+public class ValkyrieObstacleCourseDoNothingTest extends DRCObstacleCourseDoNothingTest
+{
+   private final ValkyrieRobotModel robotModel = new ValkyrieRobotModel(RobotTarget.SCS, false, true);
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return robotModel;
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.VALKYRIE);
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 20.3)
+   @Test(timeout = 100000)
+   public void testDoNothing1() throws SimulationExceededMaximumTimeException
+   {
+      super.testDoNothing1();
+   }
+}
