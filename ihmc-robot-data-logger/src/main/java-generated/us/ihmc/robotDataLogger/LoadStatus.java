efd691bb95f7e013ebfa4bfd268d24fa50958c1c
Duncan Calvert <dcalvert@ihmc.us>
2018-04-10 10:21:58 -0500

    Generate logger messages.

--- ihmc-robot-data-logger/src/main/java-generated/us/ihmc/robotDataLogger/LoadStatus.java
+++ ihmc-robot-data-logger/src/main/java-generated/us/ihmc/robotDataLogger/LoadStatus.java
@@ -1,21 +1,30 @@
 package us.ihmc.robotDataLogger;
 
 /**
- * Definition of the enum "LoadStatus" defined in Handshake.idl.
- *
- * This file was automatically generated from Handshake.idl by us.ihmc.idl.generator.IDLGenerator.
- * Do not update this file directly, edit Handshake.idl instead.
- */
+* 
+* Definition of the enum "LoadStatus" defined in Handshake.idl. 
+*
+* This file was automatically generated from Handshake.idl by us.ihmc.idl.generator.IDLGenerator. 
+* Do not update this file directly, edit Handshake.idl instead.
+*
+*/
+import us.ihmc.idl.IDLTools;
+
 public enum LoadStatus
 {
-   NoParameter,
-
-   Unloaded,
-
-   Default,
-
-   Loaded,;
-
+         NoParameter,
+      
+         Unloaded,
+      
+         Default,
+      
+         Loaded,
+      
+   ;
    public static LoadStatus[] values = values();
 
+   public boolean epsilonEquals(LoadStatus other, double epsilon)
+   {
+      return IDLTools.epsilonEqualsEnum(this, other, epsilon);
+   }
 }
--- ihmc-robot-data-logger/src/main/java-generated/us/ihmc/robotDataLogger/VariableChangeRequestPubSubType.java
+++ ihmc-robot-data-logger/src/main/java-generated/us/ihmc/robotDataLogger/VariableChangeRequestPubSubType.java
@@ -1,20 +1,34 @@
 package us.ihmc.robotDataLogger;
 
 /**
- * Topic data type of the struct "VariableChangeRequest" defined in "VariableChangeRequest.idl". Use this class to provide the TopicDataType to a Participant.
- *
- * This file was automatically generated from VariableChangeRequest.idl by us.ihmc.idl.generator.IDLGenerator.
- * Do not update this file directly, edit VariableChangeRequest.idl instead.
- */
+* 
+* Topic data type of the struct "VariableChangeRequest" defined in "VariableChangeRequest.idl". Use this class to provide the TopicDataType to a Participant. 
+*
+* This file was automatically generated from VariableChangeRequest.idl by us.ihmc.idl.generator.IDLGenerator. 
+* Do not update this file directly, edit VariableChangeRequest.idl instead.
+*
+*/
 public class VariableChangeRequestPubSubType implements us.ihmc.pubsub.TopicDataType<us.ihmc.robotDataLogger.VariableChangeRequest>
 {
    public static final java.lang.String name = "us::ihmc::robotDataLogger::VariableChangeRequest";
+
    private final us.ihmc.idl.CDR serializeCDR = new us.ihmc.idl.CDR();
    private final us.ihmc.idl.CDR deserializeCDR = new us.ihmc.idl.CDR();
 
-   public VariableChangeRequestPubSubType()
+   @Override
+   public void serialize(us.ihmc.robotDataLogger.VariableChangeRequest data, us.ihmc.pubsub.common.SerializedPayload serializedPayload) throws java.io.IOException
    {
+      serializeCDR.serialize(serializedPayload);
+      write(data, serializeCDR);
+      serializeCDR.finishSerialize();
+   }
 
+   @Override
+   public void deserialize(us.ihmc.pubsub.common.SerializedPayload serializedPayload, us.ihmc.robotDataLogger.VariableChangeRequest data) throws java.io.IOException
+   {
+      deserializeCDR.deserialize(serializedPayload);
+      read(data, deserializeCDR);
+      deserializeCDR.finishDeserialize();
    }
 
    public static int getMaxCdrSerializedSize()
@@ -30,6 +44,7 @@ public class VariableChangeRequestPubSubType implements us.ihmc.pubsub.TopicData
 
       current_alignment += 8 + us.ihmc.idl.CDR.alignment(current_alignment, 8);
 
+
       return current_alignment - initial_alignment;
    }
 
@@ -44,25 +59,43 @@ public class VariableChangeRequestPubSubType implements us.ihmc.pubsub.TopicData
 
       current_alignment += 4 + us.ihmc.idl.CDR.alignment(current_alignment, 4);
 
+
       current_alignment += 8 + us.ihmc.idl.CDR.alignment(current_alignment, 8);
+
+
 
       return current_alignment - initial_alignment;
    }
 
    public static void write(us.ihmc.robotDataLogger.VariableChangeRequest data, us.ihmc.idl.CDR cdr)
    {
-
       cdr.write_type_2(data.getVariableID());
 
       cdr.write_type_6(data.getRequestedValue());
+
    }
 
    public static void read(us.ihmc.robotDataLogger.VariableChangeRequest data, us.ihmc.idl.CDR cdr)
    {
-
       data.setVariableID(cdr.read_type_2());
-
+      	
       data.setRequestedValue(cdr.read_type_6());
+      	
+
+   }
+
+   @Override
+   public final void serialize(us.ihmc.robotDataLogger.VariableChangeRequest data, us.ihmc.idl.InterchangeSerializer ser)
+   {
+      ser.write_type_2("variableID", data.getVariableID());
+      ser.write_type_6("requestedValue", data.getRequestedValue());
+   }
+
+   @Override
+   public final void deserialize(us.ihmc.idl.InterchangeSerializer ser, us.ihmc.robotDataLogger.VariableChangeRequest data)
+   {
+      data.setVariableID(ser.read_type_2("variableID"));
+      data.setRequestedValue(ser.read_type_6("requestedValue"));
    }
 
    public static void staticCopy(us.ihmc.robotDataLogger.VariableChangeRequest src, us.ihmc.robotDataLogger.VariableChangeRequest dest)
@@ -71,45 +104,10 @@ public class VariableChangeRequestPubSubType implements us.ihmc.pubsub.TopicData
    }
 
    @Override
-   public void serialize(us.ihmc.robotDataLogger.VariableChangeRequest data, us.ihmc.pubsub.common.SerializedPayload serializedPayload)
-         throws java.io.IOException
-   {
-      serializeCDR.serialize(serializedPayload);
-      write(data, serializeCDR);
-      serializeCDR.finishSerialize();
-   }
-
-   @Override
-   public void deserialize(us.ihmc.pubsub.common.SerializedPayload serializedPayload, us.ihmc.robotDataLogger.VariableChangeRequest data)
-         throws java.io.IOException
-   {
-      deserializeCDR.deserialize(serializedPayload);
-      read(data, deserializeCDR);
-      deserializeCDR.finishDeserialize();
-   }
-
-   @Override
-   public final void serialize(us.ihmc.robotDataLogger.VariableChangeRequest data, us.ihmc.idl.InterchangeSerializer ser)
-   {
-      ser.write_type_2("variableID", data.getVariableID());
-
-      ser.write_type_6("requestedValue", data.getRequestedValue());
-   }
-
-   @Override
-   public final void deserialize(us.ihmc.idl.InterchangeSerializer ser, us.ihmc.robotDataLogger.VariableChangeRequest data)
-   {
-      data.setVariableID(ser.read_type_2("variableID"));
-
-      data.setRequestedValue(ser.read_type_6("requestedValue"));
-   }
-
-   @Override
    public us.ihmc.robotDataLogger.VariableChangeRequest createData()
    {
       return new us.ihmc.robotDataLogger.VariableChangeRequest();
    }
-
    @Override
    public int getTypeSize()
    {
@@ -121,7 +119,7 @@ public class VariableChangeRequestPubSubType implements us.ihmc.pubsub.TopicData
    {
       return name;
    }
-
+   
    public void serialize(us.ihmc.robotDataLogger.VariableChangeRequest data, us.ihmc.idl.CDR cdr)
    {
       write(data, cdr);
@@ -131,7 +129,7 @@ public class VariableChangeRequestPubSubType implements us.ihmc.pubsub.TopicData
    {
       read(data, cdr);
    }
-
+   
    public void copy(us.ihmc.robotDataLogger.VariableChangeRequest src, us.ihmc.robotDataLogger.VariableChangeRequest dest)
    {
       staticCopy(src, dest);
