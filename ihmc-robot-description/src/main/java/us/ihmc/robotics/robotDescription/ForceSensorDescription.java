7d0c0cbef418efa6fce88ecddbdd20a5533bd96d
InhoLee <ilee@ihmc.us>
2018-04-11 15:27:33 -0500

    Force sensor description can hold useshapecollision.

--- ihmc-robot-description/src/main/java/us/ihmc/robotics/robotDescription/ForceSensorDescription.java
+++ ihmc-robot-description/src/main/java/us/ihmc/robotics/robotDescription/ForceSensorDescription.java
@@ -5,10 +5,22 @@ import us.ihmc.euclid.transform.RigidBodyTransform;
 public class ForceSensorDescription extends SensorDescription
 {
    private boolean useGroundContactPoints = true;
+   
+   private boolean useShapeCollision = false;
 
    public ForceSensorDescription(String name, RigidBodyTransform transformToJoint)
    {
       super(name, transformToJoint);
+   }
+   
+   public boolean useShapeCollision()
+   {
+      return useShapeCollision;
+   }
+   
+   public void setUseShapeCollision(boolean useShapeCollision)
+   {
+      this.useShapeCollision = useShapeCollision;
    }
 
    public boolean useGroundContactPoints()
