a5a2c7df9fcc4276a1804b07b23fabcf3f441703
Stephen McCrory <smccrory@ihmc.us>
2018-04-11 14:07:22 -0500

    adding a step data structure that holds onto a desired body yaw

---
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/QuadrupedTimedOrientedStep.java
@@ -0,0 +1,8 @@
+package us.ihmc.quadrupedRobotics.planning;
+
+/**
+ * Created by stephen on 4/11/18.
+ */
+public class QuadrupedTimedOrientedStep
+{
+}
---
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/stepStream/QuadrupedPlanarFootstepPlan.java
@@ -0,0 +1,8 @@
+package us.ihmc.quadrupedRobotics.planning.stepStream;
+
+/**
+ * Created by stephen on 4/11/18.
+ */
+public class QuadrupedPlanarFootstepPlan
+{
+}
