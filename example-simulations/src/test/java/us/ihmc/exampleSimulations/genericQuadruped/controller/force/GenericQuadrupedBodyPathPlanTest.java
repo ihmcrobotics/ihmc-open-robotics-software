c8e3c14a57e5d6a5f293c5b1220918b882a93d97
Stephen McCrory <smccrory@ihmc.us>
2018-04-19 17:30:49 -0500

    Simple body path planner unit test

---
+++ example-simulations/src/test/java/us/ihmc/exampleSimulations/genericQuadruped/controller/force/GenericQuadrupedBodyPathPlanTest.java
@@ -0,0 +1,22 @@
+package us.ihmc.exampleSimulations.genericQuadruped.controller.force;
+
+import org.junit.Test;
+import us.ihmc.exampleSimulations.genericQuadruped.GenericQuadrupedTestFactory;
+import us.ihmc.quadrupedRobotics.QuadrupedTestFactory;
+import us.ihmc.quadrupedRobotics.planning.QuadrupedBodyPathPlanTest;
+
+public class GenericQuadrupedBodyPathPlanTest extends QuadrupedBodyPathPlanTest
+{
+   @Override
+   public QuadrupedTestFactory createQuadrupedTestFactory()
+   {
+      return new GenericQuadrupedTestFactory();
+   }
+
+   @Test(timeout = 200000)
+   @Override
+   public void testSimpleBodyPathPlan()
+   {
+      super.testSimpleBodyPathPlan();
+   }
+}
