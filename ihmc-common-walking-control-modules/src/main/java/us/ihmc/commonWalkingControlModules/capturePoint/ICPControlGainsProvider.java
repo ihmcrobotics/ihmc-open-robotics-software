3c0e71708f7b9374716ee50e0ebe9883aebdc369
gwiedebach <gwiedebach@ihmc.us>
2018-04-23 12:40:51 -0500

    Added constructor with default values to the parameterized icp control gains and make them extend the read only icp control gains interface.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/ICPControlGainsProvider.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/ICPControlGainsProvider.java
@@ -2,9 +2,8 @@ package us.ihmc.commonWalkingControlModules.capturePoint;
 
 import us.ihmc.yoVariables.providers.DoubleProvider;
 
-public interface ICPControlGainsProvider
+public interface ICPControlGainsProvider extends ICPControlGainsReadOnly
 {
-
    DoubleProvider getYoKpParallelToMotion();
 
    DoubleProvider getYoKpOrthogonalToMotion();
@@ -17,4 +16,39 @@ public interface ICPControlGainsProvider
 
    DoubleProvider getYoFeedbackPartMaxRate();
 
+   @Override
+   default double getKpParallelToMotion()
+   {
+      return getYoKpParallelToMotion().getValue();
+   }
+
+   @Override
+   default double getKpOrthogonalToMotion()
+   {
+      return getYoKpOrthogonalToMotion().getValue();
+   }
+
+   @Override
+   default double getKi()
+   {
+      return getYoKi().getValue();
+   }
+
+   @Override
+   default double getIntegralLeakRatio()
+   {
+      return getYoIntegralLeakRatio().getValue();
+   }
+
+   @Override
+   default double getMaxIntegralError()
+   {
+      return getYoMaxIntegralError().getValue();
+   }
+
+   @Override
+   default double getFeedbackPartMaxRate()
+   {
+      return getYoFeedbackPartMaxRate().getValue();
+   }
 }
