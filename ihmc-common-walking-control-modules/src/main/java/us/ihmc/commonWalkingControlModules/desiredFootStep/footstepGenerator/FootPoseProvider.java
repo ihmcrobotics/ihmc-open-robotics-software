7128f0d38e89388a6a5c39cd0861b17db5d3efc0
Sylvain Bertrand <sbertrand@ihmc.us>
2018-05-02 10:27:41 -0500

    More documentation.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/FootPoseProvider.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/FootPoseProvider.java
@@ -3,7 +3,21 @@ package us.ihmc.commonWalkingControlModules.desiredFootStep.footstepGenerator;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
 import us.ihmc.robotics.robotSide.RobotSide;
 
+/**
+ * This interface is part of the API of {@link ContinuousStepGenerator}.
+ * <p>
+ * It is used to provide a protocol for obtaining the current pose of the robot feet.
+ * </p>
+ * 
+ * @author Sylvain Bertrand
+ */
 public interface FootPoseProvider
 {
+   /**
+    * Gets the current pose of a robot sole frame.
+    * 
+    * @param robotSide the side of interest.
+    * @return the current pose of the foot sole.
+    */
    FramePose3DReadOnly getCurrentFootPose(RobotSide robotSide);
 }
