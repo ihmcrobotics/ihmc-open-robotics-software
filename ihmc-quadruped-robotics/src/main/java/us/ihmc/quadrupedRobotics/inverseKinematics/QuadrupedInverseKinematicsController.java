18baee788af230da307ba10136d3dba4aa198e36
Robert <rgriffin@ihmc.us>
2018-05-09 12:25:46 -0500

    got the ik simulation working

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/inverseKinematics/QuadrupedInverseKinematicsController.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/inverseKinematics/QuadrupedInverseKinematicsController.java
@@ -6,6 +6,7 @@ import us.ihmc.commonWalkingControlModules.controllerCore.WholeBodyControllerCor
 import us.ihmc.commonWalkingControlModules.controllerCore.command.ControllerCoreCommand;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController.FeedbackControlCommandList;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController.PointFeedbackControlCommand;
+import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.ControllerCoreOptimizationSettings;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
@@ -16,12 +17,15 @@ import us.ihmc.robotics.controllers.pidGains.GainCoupling;
 import us.ihmc.robotics.controllers.pidGains.YoPID3DGains;
 import us.ihmc.robotics.controllers.pidGains.implementations.DefaultYoPID3DGains;
 import us.ihmc.robotics.controllers.pidGains.implementations.PID3DConfiguration;
+import us.ihmc.robotics.math.filters.RateLimitedYoFramePoint;
 import us.ihmc.robotics.robotController.RobotController;
 import us.ihmc.robotics.robotSide.QuadrantDependentList;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 import us.ihmc.robotics.screwTheory.RigidBody;
 import us.ihmc.sensorProcessing.outputData.JointDesiredOutputList;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
+import us.ihmc.yoVariables.variable.YoBoolean;
+import us.ihmc.yoVariables.variable.YoDouble;
 import us.ihmc.yoVariables.variable.YoFramePoint3D;
 
 public class QuadrupedInverseKinematicsController implements RobotController
@@ -31,29 +35,32 @@ public class QuadrupedInverseKinematicsController implements RobotController
    private final WholeBodyControlCoreToolbox controlCoreToolbox;
    private final WholeBodyControllerCore controllerCore;
 
-   private final ControllerCoreCommand controllerCoreCommand ;
+   private final ControllerCoreCommand controllerCoreCommand;
 
    private final RobotQuadrant[] quadrants;
 
    private final QuadrantDependentList<PointFeedbackControlCommand> feedbackControlCommands = new QuadrantDependentList<>();
    private final QuadrantDependentList<YoFramePoint3D> desiredSolePositions = new QuadrantDependentList<>();
+   private final QuadrantDependentList<RateLimitedYoFramePoint> limitedDesiredSolePositions = new QuadrantDependentList<>();
+   private final YoDouble maxRate = new YoDouble("maxRate", registry);
+
+   private final QuadrupedReferenceFrames referenceFrames;
 
    private final FramePoint3D desiredPosition = new FramePoint3D();
    private final FrameVector3D desiredVelocity = new FrameVector3D();
 
    private final YoPID3DGains gains;
+   private final YoDouble weight = new YoDouble("footWeight", registry);
+   private final YoBoolean setToCurrent = new YoBoolean("setToCurrent", registry);
+
+   private boolean initialized = false;
 
    public QuadrupedInverseKinematicsController(FullQuadrupedRobotModel fullRobotModel, RobotQuadrant[] quadrants, QuadrupedReferenceFrames referenceFrames,
-                                               JointDesiredOutputList lowLevelJointOutputList, double controlDT, double gravityZ,
-                                               YoGraphicsListRegistry graphicsListRegistry)
+                                               JointDesiredOutputList lowLevelJointOutputList, ControllerCoreOptimizationSettings optimizationSettings,
+                                               double controlDT, double gravityZ, YoGraphicsListRegistry graphicsListRegistry)
    {
       this.quadrants = quadrants;
-
-      controlCoreToolbox = new WholeBodyControlCoreToolbox(controlDT, gravityZ, null, fullRobotModel.getControllableOneDoFJoints(),
-                                                           referenceFrames.getCenterOfMassFrame(), null, graphicsListRegistry, registry);
-      controlCoreToolbox.setupForInverseKinematicsSolver();
-      controllerCore = new WholeBodyControllerCore(controlCoreToolbox, getFeedbackCommandTemplate(), lowLevelJointOutputList, registry);
-      controllerCoreCommand= new ControllerCoreCommand(WholeBodyControllerCoreMode.INVERSE_KINEMATICS);
+      this.referenceFrames = referenceFrames;
 
       for (RobotQuadrant robotQuadrant : quadrants)
       {
@@ -73,30 +80,54 @@ public class QuadrupedInverseKinematicsController implements RobotController
          while (rigidBody.getParentJoint().getPredecessor() != body)
             rigidBody = rigidBody.getParentJoint().getPredecessor();
          ReferenceFrame shoulderBaseFrame = rigidBody.getParentJoint().getFrameBeforeJoint();
-         YoFramePoint3D desiredPosition = new YoFramePoint3D(foot.getName() + "_DesiredPosition", shoulderBaseFrame, registry);
-         desiredPosition.setZ(-0.3);
+         YoFramePoint3D desiredPosition = new YoFramePoint3D(robotQuadrant.getCamelCaseName() + "_Foot_DesiredPosition", shoulderBaseFrame, registry);
+         RateLimitedYoFramePoint limitedDesiredPosition = new RateLimitedYoFramePoint(robotQuadrant.getCamelCaseName() + "_LimitedFoot_DesiredPosition", "",
+                                                                                      registry, maxRate, controlDT, desiredPosition);
          desiredSolePositions.put(robotQuadrant, desiredPosition);
+         limitedDesiredSolePositions.put(robotQuadrant, limitedDesiredPosition);
       }
 
       PID3DConfiguration gainConfiguration = new PID3DConfiguration(GainCoupling.XY, false);
       gains = new DefaultYoPID3DGains("footGains", gainConfiguration, registry);
       gains.setProportionalGains(10.0);
       gains.setDerivativeGains(1.0);
+
+      weight.set(1.0);
+      maxRate.set(0.1);
+
+      controlCoreToolbox = new WholeBodyControlCoreToolbox(controlDT, gravityZ, null, fullRobotModel.getControllableOneDoFJoints(),
+                                                           referenceFrames.getCenterOfMassFrame(), optimizationSettings, graphicsListRegistry, registry);
+      controlCoreToolbox.setupForInverseKinematicsSolver();
+      controllerCore = new WholeBodyControllerCore(controlCoreToolbox, getFeedbackCommandTemplate(), lowLevelJointOutputList, registry);
+      controllerCoreCommand = new ControllerCoreCommand(WholeBodyControllerCoreMode.INVERSE_KINEMATICS);
    }
 
    @Override
    public void doControl()
    {
+      referenceFrames.updateFrames();
+
+      if (!initialized)
+      {
+         initialize();
+         initialized = true;
+      }
+
+      if (setToCurrent.getBooleanValue())
+         setToCurrent();
+
       controllerCoreCommand.clear();
 
       for (RobotQuadrant robotQuadrant : quadrants)
       {
-         desiredPosition.setIncludingFrame(desiredSolePositions.get(robotQuadrant));
+         limitedDesiredSolePositions.get(robotQuadrant).update();
+         desiredPosition.setIncludingFrame(limitedDesiredSolePositions.get(robotQuadrant));
          desiredPosition.changeFrame(ReferenceFrame.getWorldFrame());
 
          PointFeedbackControlCommand feedbackControlCommand = feedbackControlCommands.get(robotQuadrant);
          feedbackControlCommand.set(desiredPosition, desiredVelocity);
          feedbackControlCommand.setGains(gains);
+         feedbackControlCommand.setWeightForSolver(weight.getDoubleValue());
 
          controllerCoreCommand.addFeedbackControlCommand(feedbackControlCommand);
       }
@@ -108,7 +139,21 @@ public class QuadrupedInverseKinematicsController implements RobotController
    @Override
    public void initialize()
    {
+      controllerCore.initialize();
 
+      setToCurrent();
+   }
+
+   private void setToCurrent()
+   {
+      setToCurrent.set(false);
+
+      for (RobotQuadrant robotQuadrant : quadrants)
+      {
+         desiredPosition.setToZero(referenceFrames.getSoleFrame(robotQuadrant));
+         desiredSolePositions.get(robotQuadrant).setMatchingFrame(desiredPosition);
+         limitedDesiredSolePositions.get(robotQuadrant).setMatchingFrame(desiredPosition);
+      }
    }
 
    @Override
--- ihmc-simulation-toolkit/src/main/java/us/ihmc/simulationToolkit/outputWriters/PerfectPositionControlSimulatedOutputWriter.java
+++ ihmc-simulation-toolkit/src/main/java/us/ihmc/simulationToolkit/outputWriters/PerfectPositionControlSimulatedOutputWriter.java
@@ -8,6 +8,9 @@ import us.ihmc.robotModels.FullRobotModel;
 import us.ihmc.robotModels.OutputWriter;
 import us.ihmc.robotics.screwTheory.FloatingInverseDynamicsJoint;
 import us.ihmc.robotics.screwTheory.OneDoFJoint;
+import us.ihmc.sensorProcessing.outputData.JointDesiredOutputList;
+import us.ihmc.sensorProcessing.outputData.JointDesiredOutputListReadOnly;
+import us.ihmc.sensorProcessing.outputData.JointDesiredOutputReadOnly;
 import us.ihmc.simulationconstructionset.FloatingJoint;
 import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
 import us.ihmc.simulationconstructionset.OneDegreeOfFreedomJoint;
@@ -17,18 +20,15 @@ public class PerfectPositionControlSimulatedOutputWriter implements OutputWriter
    private final String name;
    protected final FloatingRootJointRobot robot;
    protected ImmutablePair<FloatingJoint, FloatingInverseDynamicsJoint> rootJointPair;
-   protected final ArrayList<ImmutablePair<OneDegreeOfFreedomJoint,OneDoFJoint>> revoluteJoints = new ArrayList<ImmutablePair<OneDegreeOfFreedomJoint, OneDoFJoint>>();
-   
-   public PerfectPositionControlSimulatedOutputWriter(FloatingRootJointRobot robot)
+   protected final ArrayList<ImmutablePair<OneDegreeOfFreedomJoint, OneDoFJoint>> revoluteJoints = new ArrayList<ImmutablePair<OneDegreeOfFreedomJoint, OneDoFJoint>>();
+   private final JointDesiredOutputListReadOnly jointDesiredOutputListReadOnly;
+
+   public PerfectPositionControlSimulatedOutputWriter(FloatingRootJointRobot robot, FullRobotModel fullRobotModel,
+                                                      JointDesiredOutputListReadOnly jointDesiredOutputList)
    {
       this.name = robot.getName() + "SimulatedSensorReader";
       this.robot = robot;
-   }
-   
-   public PerfectPositionControlSimulatedOutputWriter(FloatingRootJointRobot robot, FullRobotModel fullRobotModel)
-   {
-      this.name = robot.getName() + "SimulatedSensorReader";
-      this.robot = robot;
+      this.jointDesiredOutputListReadOnly = jointDesiredOutputList;
 
       setFullRobotModel(fullRobotModel);
    }
@@ -37,22 +37,22 @@ public class PerfectPositionControlSimulatedOutputWriter implements OutputWriter
    public void initialize()
    {
    }
-   
+
    @Override
    public void setFullRobotModel(FullRobotModel fullRobotModel)
    {
       revoluteJoints.clear();
       OneDoFJoint[] revoluteJointsArray = fullRobotModel.getOneDoFJoints();
-      
+
       for (OneDoFJoint revoluteJoint : revoluteJointsArray)
       {
          String name = revoluteJoint.getName();
          OneDegreeOfFreedomJoint oneDoFJoint = robot.getOneDegreeOfFreedomJoint(name);
-         
-         ImmutablePair<OneDegreeOfFreedomJoint,OneDoFJoint> jointPair = new ImmutablePair<OneDegreeOfFreedomJoint, OneDoFJoint>(oneDoFJoint, revoluteJoint);
+
+         ImmutablePair<OneDegreeOfFreedomJoint, OneDoFJoint> jointPair = new ImmutablePair<OneDegreeOfFreedomJoint, OneDoFJoint>(oneDoFJoint, revoluteJoint);
          this.revoluteJoints.add(jointPair);
       }
-      
+
       rootJointPair = new ImmutablePair<FloatingJoint, FloatingInverseDynamicsJoint>(robot.getRootJoint(), fullRobotModel.getRootJoint());
    }
 
@@ -75,9 +75,12 @@ public class PerfectPositionControlSimulatedOutputWriter implements OutputWriter
          OneDegreeOfFreedomJoint pinJoint = jointPair.getLeft();
          OneDoFJoint revoluteJoint = jointPair.getRight();
 
-         pinJoint.setQ(revoluteJoint.getqDesired());
-         pinJoint.setQd(revoluteJoint.getQdDesired());
-         pinJoint.setQdd(revoluteJoint.getQddDesired());
+         JointDesiredOutputReadOnly jointOutput = jointDesiredOutputListReadOnly.getJointDesiredOutput(revoluteJoint);
+
+         if (jointOutput.hasDesiredPosition())
+            pinJoint.setQ(jointOutput.getDesiredPosition());
+         if (jointOutput.hasDesiredVelocity())
+            pinJoint.setQd(jointOutput.getDesiredVelocity());
       }
    }
 }
