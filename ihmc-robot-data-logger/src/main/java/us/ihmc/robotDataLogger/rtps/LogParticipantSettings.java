4727239c6fdbe5c0714e1f050d13ebd5a71852d9
Operator Computer <dragon_ryderz@maillist.ihmc.us>
2018-05-14 11:46:18 -0500

    Switched the kind of pair class used.

--- ihmc-robot-data-logger/src/main/java/us/ihmc/robotDataLogger/rtps/LogParticipantSettings.java
+++ ihmc-robot-data-logger/src/main/java/us/ihmc/robotDataLogger/rtps/LogParticipantSettings.java
@@ -1,6 +1,8 @@
 package us.ihmc.robotDataLogger.rtps;
 
-import javafx.util.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
 import us.ihmc.pubsub.attributes.ReliabilityKind;
 
 public class LogParticipantSettings
@@ -10,14 +12,14 @@ public class LogParticipantSettings
    public static final String namespaceSeperator = "/";
    public static final String partition = namespaceSeperator + "us" + namespaceSeperator + "ihmc" + namespaceSeperator + "robotDataLogger";
    
-   public static final Pair<String, ReliabilityKind> annoucement = new Pair<>("sessions", ReliabilityKind.RELIABLE);
-   public static final Pair<String, ReliabilityKind> handshake = new Pair<>("handshake", ReliabilityKind.RELIABLE);
-   public static final Pair<String, ReliabilityKind> modelFile = new Pair<>("modelFile", ReliabilityKind.RELIABLE);
-   public static final Pair<String, ReliabilityKind> resourceBundle = new Pair<>("resourceBundle", ReliabilityKind.RELIABLE);
-   public static final Pair<String, ReliabilityKind> variableChange = new Pair<>("changeVariable", ReliabilityKind.RELIABLE);
-   public static final Pair<String, ReliabilityKind> clearLog = new Pair<>("clearLog", ReliabilityKind.RELIABLE);
-   public static final Pair<String, ReliabilityKind> timestamp = new Pair<>("timestamps", ReliabilityKind.BEST_EFFORT);
-   public static final Pair<String, ReliabilityKind> data = new Pair<>("data", ReliabilityKind.BEST_EFFORT);
+   public static final Pair<String, ReliabilityKind> annoucement = new ImmutablePair<>("sessions", ReliabilityKind.RELIABLE);
+   public static final Pair<String, ReliabilityKind> handshake = new ImmutablePair<>("handshake", ReliabilityKind.RELIABLE);
+   public static final Pair<String, ReliabilityKind> modelFile = new ImmutablePair<>("modelFile", ReliabilityKind.RELIABLE);
+   public static final Pair<String, ReliabilityKind> resourceBundle = new ImmutablePair<>("resourceBundle", ReliabilityKind.RELIABLE);
+   public static final Pair<String, ReliabilityKind> variableChange = new ImmutablePair<>("changeVariable", ReliabilityKind.RELIABLE);
+   public static final Pair<String, ReliabilityKind> clearLog = new ImmutablePair<>("clearLog", ReliabilityKind.RELIABLE);
+   public static final Pair<String, ReliabilityKind> timestamp = new ImmutablePair<>("timestamps", ReliabilityKind.BEST_EFFORT);
+   public static final Pair<String, ReliabilityKind> data = new ImmutablePair<>("data", ReliabilityKind.BEST_EFFORT);
    
    public static final String modelFileTypeName = "us::ihmc::robotDataLogger::modelFile";
    public static final String resourceBundleTypeName = "us::ihmc::robotDataLogger::resourceBundle";
