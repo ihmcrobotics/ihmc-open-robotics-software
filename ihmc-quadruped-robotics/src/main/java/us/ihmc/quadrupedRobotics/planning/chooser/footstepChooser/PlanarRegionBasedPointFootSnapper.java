37ae3640641d128ca26ab7ac37f196716a94ceac
Robert <rgriffin@ihmc.us>
2018-05-20 10:41:23 -0500

    added the ability to snap the footsteps to the closest planar region

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/chooser/footstepChooser/PlanarRegionBasedPointFootSnapper.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/chooser/footstepChooser/PlanarRegionBasedPointFootSnapper.java
@@ -1,40 +1,45 @@
 package us.ihmc.quadrupedRobotics.planning.chooser.footstepChooser;
 
-import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.referenceFrame.FrameConvexPolygon2D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.robotics.geometry.ConvexPolygonScaler;
-import us.ihmc.robotics.geometry.ConvexPolygonTools;
 import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 
-import java.sql.Ref;
 import java.util.List;
 
 public class PlanarRegionBasedPointFootSnapper implements PointFootSnapper
 {
    private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
    private static final double distanceInside = 0.05;
+   private static final double maxNormalAngleFromVertical = 0.3;
+
 
    private final Point2D unsnappedPoint = new Point2D();
    private final Point2D snappedPoint2D = new Point2D();
    private final Point3D snappedPoint = new Point3D();
    private final ReferenceFrame planarRegionFrame;
 
-   private PlanarRegionsList planarRegionsList;
+   private final PlanarRegionsList planarRegionsList;
+   private final Vector3D planeNormal = new Vector3D();
+   private final Vector3D verticalAxis = new Vector3D(0.0, 0.0, 1.0);
+
 
    private final RigidBodyTransform planarRegionTransformToWorld = new RigidBodyTransform();
+   private final RigidBodyTransform planarRegionTransformToLocal = new RigidBodyTransform();
    private final FrameConvexPolygon2D planarRegionPolygon = new FrameConvexPolygon2D();
    private final FrameConvexPolygon2D shrunkPolygon = new FrameConvexPolygon2D();
    private final ConvexPolygonScaler scaler = new ConvexPolygonScaler();
 
    public PlanarRegionBasedPointFootSnapper()
    {
+      planarRegionsList = new PlanarRegionsList();
       planarRegionFrame = new ReferenceFrame("planarRegionFrame", worldFrame)
       {
          @Override
@@ -47,7 +52,19 @@ public class PlanarRegionBasedPointFootSnapper implements PointFootSnapper
 
    public void setPlanarRegionsList(PlanarRegionsList planarRegionsList)
    {
-      this.planarRegionsList = planarRegionsList;
+      this.planarRegionsList.clear();
+      for (int i = 0; i < planarRegionsList.getNumberOfPlanarRegions(); i++)
+      {
+         PlanarRegion planarRegion = planarRegionsList.getPlanarRegion(i);
+         planarRegion.getNormal(planeNormal);
+
+         double angle = planeNormal.angle(verticalAxis);
+
+         if (angle < maxNormalAngleFromVertical)
+         {
+            this.planarRegionsList.addPlanarRegion(planarRegion);
+         }
+      }
    }
 
    @Override
@@ -57,13 +74,16 @@ public class PlanarRegionBasedPointFootSnapper implements PointFootSnapper
       return snapStep(unsnappedPoint);
    }
 
-   public Point3DReadOnly snapStep(Point2D unsnappedPoint)
+   public Point3DReadOnly snapStep(Point2DReadOnly unsnappedPoint)
    {
       List<PlanarRegion> intersectingRegions = planarRegionsList.findPlanarRegionsContainingPointByProjectionOntoXYPlane(unsnappedPoint);
       if(intersectingRegions == null)
       {
-         // TODO do something smarter here
-         snappedPoint.set(unsnappedPoint, 0.0);
+         PlanarRegion closestRegion = planarRegionsList.findClosestPlanarRegionToPointByProjectionOntoXYPlane(unsnappedPoint);
+         Point2DReadOnly snappedPoint2D = projectPointIntoPlanarRegion(unsnappedPoint, closestRegion, distanceInside);
+         double zHeight = closestRegion.getPlaneZGivenXY(snappedPoint2D.getX(), snappedPoint2D.getY());
+
+         snappedPoint.set(snappedPoint2D, zHeight);
       }
       else
       {
@@ -79,25 +99,31 @@ public class PlanarRegionBasedPointFootSnapper implements PointFootSnapper
             }
          }
 
-         planarRegion.getTransformToWorld(planarRegionTransformToWorld);
-         planarRegionFrame.update();
-         planarRegionPolygon.setReferenceFrame(planarRegionFrame);
-         planarRegionPolygon.set(planarRegion.getConvexHull());
-         planarRegionPolygon.changeFrameAndProjectToXYPlane(worldFrame);
-
-         if (planarRegionPolygon.signedDistance(unsnappedPoint) > -distanceInside)
-         {
-            scaler.scaleConvexPolygon(planarRegionPolygon, distanceInside, shrunkPolygon);
-            shrunkPolygon.orthogonalProjection(unsnappedPoint, snappedPoint2D);
-         }
-         else
-         {
-            snappedPoint2D.set(unsnappedPoint);
-         }
-
+         Point2DReadOnly snappedPoint2D = projectPointIntoPlanarRegion(unsnappedPoint, planarRegion, distanceInside);
          snappedPoint.set(snappedPoint2D, maxZ);
       }
 
       return snappedPoint;
+   }
+
+   private Point2DReadOnly projectPointIntoPlanarRegion(Point2DReadOnly unsnappedPoint, PlanarRegion planarRegion, double distanceInside)
+   {
+      planarRegion.getTransformToWorld(planarRegionTransformToWorld);
+      planarRegionFrame.update();
+      planarRegionPolygon.setReferenceFrame(planarRegionFrame);
+      planarRegionPolygon.set(planarRegion.getConvexHull());
+      planarRegionPolygon.changeFrameAndProjectToXYPlane(worldFrame);
+
+      if (planarRegionPolygon.signedDistance(unsnappedPoint) > -distanceInside)
+      {
+         scaler.scaleConvexPolygon(planarRegionPolygon, distanceInside, shrunkPolygon);
+         shrunkPolygon.orthogonalProjection(unsnappedPoint, snappedPoint2D);
+      }
+      else
+      {
+         snappedPoint2D.set(unsnappedPoint);
+      }
+
+      return snappedPoint2D;
    }
 }
