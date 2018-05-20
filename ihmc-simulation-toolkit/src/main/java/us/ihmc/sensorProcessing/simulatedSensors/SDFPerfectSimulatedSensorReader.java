83901489a5fbc50feacd5dbdfeace1e4af59d357
Robert <rgriffin@ihmc.us>
2018-05-20 09:42:59 -0500

    added the measured torque to the perfect sensor reader

--- ihmc-simulation-toolkit/src/main/java/us/ihmc/sensorProcessing/simulatedSensors/SDFPerfectSimulatedSensorReader.java
+++ ihmc-simulation-toolkit/src/main/java/us/ihmc/sensorProcessing/simulatedSensors/SDFPerfectSimulatedSensorReader.java
@@ -195,6 +195,7 @@ public class SDFPerfectSimulatedSensorReader implements RawSensorReader, SensorO
          revoluteJoint.setQ(pinJoint.getQYoVariable().getDoubleValue());
          revoluteJoint.setQd(pinJoint.getQDYoVariable().getDoubleValue());
          revoluteJoint.setQdd(pinJoint.getQDDYoVariable().getDoubleValue());
+         revoluteJoint.setTauMeasured(pinJoint.getTau());
       }
    }
 
