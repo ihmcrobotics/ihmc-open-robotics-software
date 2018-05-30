f6518fd9cf715b57f3f640f292d950420d662053
Stephen McCrory <smccrory@ihmc.us>
2018-05-30 14:27:35 -0500

    adding a heightmap based point foot snapper

---
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/chooser/footstepChooser/HeightMapFootSnapper.java
@@ -0,0 +1,25 @@
+package us.ihmc.quadrupedRobotics.planning.chooser.footstepChooser;
+
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.graphicsDescription.HeightMap;
+
+public class HeightMapFootSnapper implements PointFootSnapper
+{
+   private final Point3D snappedStep = new Point3D();
+   private final HeightMap heightMap;
+
+   public HeightMapFootSnapper(HeightMap heightMap)
+   {
+      this.heightMap = heightMap;
+   }
+
+   @Override
+   public Point3DReadOnly snapStep(double xPosition, double yPosition)
+   {
+      snappedStep.setX(xPosition);
+      snappedStep.setY(yPosition);
+      snappedStep.setZ(heightMap.heightAt(xPosition, yPosition, 0.0));
+      return snappedStep;
+   }
+}
