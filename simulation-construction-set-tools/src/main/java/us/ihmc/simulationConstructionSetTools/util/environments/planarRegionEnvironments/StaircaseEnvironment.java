da590d63263dd92a8659c712ae66b855d9c732fb
Robert <rgriffin@ihmc.us>
2018-06-01 10:22:07 -0500

    added some stairs to the 2018 RCTA demo

--- simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/planarRegionEnvironments/StaircaseEnvironment.java
+++ simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/planarRegionEnvironments/StaircaseEnvironment.java
@@ -4,6 +4,11 @@ public class StaircaseEnvironment extends PlanarRegionEnvironmentInterface
 {
    public StaircaseEnvironment(int numberOfSteps, double stepHeight, double stepLength)
    {
+      this(numberOfSteps, stepHeight, stepLength, false);
+   }
+
+   public StaircaseEnvironment(int numberOfSteps, double stepHeight, double stepLength, boolean includeDown)
+   {
       double startingBlockLength = 1.2;
       generator.identity();
       generator.addRectangle(startingBlockLength, startingBlockLength);
@@ -15,7 +20,21 @@ public class StaircaseEnvironment extends PlanarRegionEnvironmentInterface
          generator.addCubeReferencedAtBottomMiddle(stepLength, 1.0, (i + 1) * stepHeight);
       }
 
-      generator.translate(-0.5 * stepLength + 0.5, 0.0, 0.0);
+      generator.translate(0.5 * (startingBlockLength + stepLength), 0.0, 0.0);
       generator.addCubeReferencedAtBottomMiddle(startingBlockLength, startingBlockLength, numberOfSteps * stepHeight);
+
+      if (includeDown)
+      {
+         generator.translate(0.5 * (startingBlockLength - stepLength), 0.0, 0.0);
+
+         for (int i = 0; i < numberOfSteps; i++)
+         {
+            generator.translate(stepLength, 0.0, 0.0);
+            generator.addCubeReferencedAtBottomMiddle(stepLength, 1.0, (numberOfSteps - i) * stepHeight);
+         }
+      }
+
+      generator.translate(0.5 * (startingBlockLength + stepLength), 0.0, 0.0);
+      generator.addRectangle(startingBlockLength, startingBlockLength);
    }
 }
