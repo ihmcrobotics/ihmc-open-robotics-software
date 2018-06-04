5adc766d2c549b5a0cfe58df10ec307efc1556bc
Stephen McCrory <smccrory@ihmc.us>
2018-06-03 21:46:43 -0500

    removing ground plane from beneath cinders

--- simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/planarRegionEnvironments/ObstacleCoursePlanarRegionEnvironment.java
+++ simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/planarRegionEnvironments/ObstacleCoursePlanarRegionEnvironment.java
@@ -17,13 +17,26 @@ public class ObstacleCoursePlanarRegionEnvironment extends PlanarRegionEnvironme
    public ObstacleCoursePlanarRegionEnvironment()
    {
       // ground plane
-      generator.translate(0.0, 0.0, -0.01);
-      generator.addRectangle(17.0, 15.0);
+      generator.translate(3.25, 0.0, -0.01);
+      generator.addRectangle(9.5, 16.0);
+      generator.translate(-3.25, 0.0, 0.0);
+
+      generator.translate(-4.0, -4.6, 0.0);
+      generator.addRectangle(8.0, 6.8);
+      generator.translate(4.0, 4.6, 0.0);
+
+      generator.translate(-4.0, 4.6, 0.0);
+      generator.addRectangle(8.0, 6.8);
+      generator.translate(4.0, -4.6, 0.0);
+
+      generator.translate(-6.7, 0.0, 0.0);
+      generator.addRectangle(2.6, 2.4);
+      generator.translate(6.7, 0.0, 0.0);
       addPlanarRegionsToTerrain(YoAppearance.RGBColor(110 / 256.0, 121 / 256.0, 121 / 256.0));
 
       // staircase and ramps
       generator.identity();
-      generator.translate(2.0, 0.0, 0.0);
+      generator.translate(1.5, 0.0, 0.0);
       double stepHeight = 0.1;
       double stepLength = 0.30;
       int numberOfSteps = 6;
@@ -47,24 +60,24 @@ public class ObstacleCoursePlanarRegionEnvironment extends PlanarRegionEnvironme
 
       // ramps
       generator.identity();
-      generator.translate(2.0, 1.0, 0.0);
-      generator.addRampReferencedAtBottomMiddle((numberOfSteps - 0.5) * stepLength, 1.0, stepHeight * numberOfSteps);
+      generator.translate(1.5, 1.0, 0.0);
+      generator.addRampReferencedAtBottomMiddle((numberOfSteps - 0.5) * stepLength, 1.4, stepHeight * numberOfSteps);
       generator.translate(numberOfSteps * stepLength + 0.5 * startingBlockLength, 0.0, stepHeight * numberOfSteps);
       generator.addRectangle(startingBlockLength + stepLength, startingBlockLength);
       generator.translate(numberOfSteps * stepLength + 0.5 * startingBlockLength, 0.0, -stepHeight * numberOfSteps);
       generator.rotate(Math.PI, Axis.Z);
-      generator.addRampReferencedAtBottomMiddle((numberOfSteps - 0.5) * stepLength, 1.0, stepHeight * numberOfSteps);
+      generator.addRampReferencedAtBottomMiddle((numberOfSteps - 0.5) * stepLength, 1., stepHeight * numberOfSteps);
 
       // cinder blocks
       generator.identity();
-      generator.translate(-2.0, 0.0, -0.05);
+      generator.translate(-1.5, 0.0, -0.05);
       generator.rotate(Math.PI, Axis.Z);
       PlanarRegionsListExamples.generateCinderBlockField(generator, 0.4, 0.1, 9, 6, 0.05);
 
       // stepping stones
       generator.identity();
       double steppingStoneHeight = 0.3;
-      generator.translate(0.0, 2.0, 0.0);
+      generator.translate(0.0, 1.5, 0.0);
       generator.rotate(0.5 * Math.PI, Axis.Z);
       generator.addRampReferencedAtBottomMiddle(1.0, 1.0, steppingStoneHeight);
       generator.translate(1.4, 0.0, 0.0);
@@ -80,31 +93,31 @@ public class ObstacleCoursePlanarRegionEnvironment extends PlanarRegionEnvironme
       generator.addCubeReferencedAtBottomMiddle(0.6, 0.25, steppingStoneHeight);
       generator.translate(-1.1, 0.375, 0.0);
 
-      generator.translate(1.35, 0.05, 0.0);
-      generator.addCubeReferencedAtBottomMiddle(0.2, 0.2, steppingStoneHeight);
-      generator.translate(-1.35, -0.05, 0.0);
+      generator.translate(1.25, 0.05, 0.0);
+      generator.addCubeReferencedAtBottomMiddle(0.35, 0.2, steppingStoneHeight);
+      generator.translate(-1.25, -0.05, 0.0);
 
       generator.translate(1.65, 0.2, 0.0);
       generator.addCubeReferencedAtBottomMiddle(0.3, 0.3, steppingStoneHeight);
       generator.translate(-1.65, -0.2, 0.0);
 
-      generator.translate(1.65, -0.175, 0.0);
+      generator.translate(1.65, -0.275, 0.0);
       generator.addCubeReferencedAtBottomMiddle(0.2, 0.2, steppingStoneHeight);
-      generator.translate(-1.65, 0.175, 0.0);
+      generator.translate(-1.65, 0.275, 0.0);
 
-      generator.translate(1.975, 0.175, 0.0);
-      generator.addCubeReferencedAtBottomMiddle(0.25, 0.25, steppingStoneHeight);
-      generator.translate(-1.975, -0.175, 0.0);
+      generator.translate(1.975, 0.075, 0.0);
+      generator.addCubeReferencedAtBottomMiddle(0.25, 0.35, steppingStoneHeight);
+      generator.translate(-1.975, -0.075, 0.0);
 
-      generator.translate(2.0, -0.2, 0.0);
-      generator.addCubeReferencedAtBottomMiddle(0.3, 0.25, steppingStoneHeight);
-      generator.translate(-2.0, 0.2, 0.0);
+      generator.translate(2.0, -0.35, 0.0);
+      generator.addCubeReferencedAtBottomMiddle(0.35, 0.35, steppingStoneHeight);
+      generator.translate(-2.0, 0.35, 0.0);
 
-      generator.translate(2.0 + 0.5 * 0.3 + 0.1 + 0.4, 0.0, 0.0);
-      generator.addCubeReferencedAtBottomMiddle(0.8, 1.0, steppingStoneHeight);
+      generator.translate(1.5 + 0.5 * 0.3 + 0.1 + 0.6, 0.0, 0.0);
+      generator.addCubeReferencedAtBottomMiddle(1.2, 1.0, steppingStoneHeight);
       generator.translate(0.0, -(0.5 + 1.0), 0.0);
       generator.rotate(0.5 * Math.PI, Axis.Z);
-      generator.addRampReferencedAtBottomMiddle(1.0, 0.8, steppingStoneHeight);
+      generator.addRampReferencedAtBottomMiddle(1.0, 1.2, steppingStoneHeight);
 
       addPlanarRegionsToTerrain(YoAppearance.Gray());
    }
