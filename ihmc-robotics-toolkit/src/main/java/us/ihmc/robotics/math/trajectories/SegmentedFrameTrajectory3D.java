ea4fe6663652f519931aab75eb5d4a62a10dcef1
Stephen McCrory <smccrory@ihmc.us>
2018-06-15 15:28:30 -0500

    fixing array initialization bug

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/trajectories/SegmentedFrameTrajectory3D.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/trajectories/SegmentedFrameTrajectory3D.java
@@ -46,6 +46,13 @@ public class SegmentedFrameTrajectory3D implements SegmentedFrameTrajectory3DInt
       currentSegmentIndex = -1;
       segments = new RecyclingArrayList<>(maxNumberOfSegments, new FrameTrajectory3DBuilder());
       nodeTime = new double[maxNumberOfSegments + 1];
+
+      /*
+       * FIXME Workaround to deal with a change of RecyclingArrayList that used to start with size()
+       * == initialCapacity. This change causes the test ReferenceCMPTrajectoryGeneratorTest.
+       */
+      while (segments.size() < maxNumberOfSegments)
+         segments.add();
    }
 
    public void reset()
