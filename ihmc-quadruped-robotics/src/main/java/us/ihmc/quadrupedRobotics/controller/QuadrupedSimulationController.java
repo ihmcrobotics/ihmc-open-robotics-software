321c257f808ae4f96d40ac1bfd4f393682905316
Stephen McCrory <smccrory@ihmc.us>
2018-06-21 10:47:00 -0500

    null check for state estimator in QuadrupedSimulationController

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controller/QuadrupedSimulationController.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controller/QuadrupedSimulationController.java
@@ -36,7 +36,8 @@ public class QuadrupedSimulationController implements RobotController
       this.stateEstimator = stateEstimator;
       this.headController = headController;
       registry.addChild(gaitControlManager.getYoVariableRegistry());
-      registry.addChild(stateEstimator.getYoVariableRegistry());
+      if(stateEstimator != null)
+         registry.addChild(stateEstimator.getYoVariableRegistry());
       if (headController != null)
          registry.addChild(headController.getYoVariableRegistry());
    }
