7d054f73875c8f364532c2742aa35643b72b43a6
ValkyrieOCU <operatorcomputer@ihmc.us>
2018-06-26 15:15:29 -0500

    Added the foot-side info to the FootstepAdjustment.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/FootstepAdjustment.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/FootstepAdjustment.java
@@ -2,6 +2,7 @@ package us.ihmc.commonWalkingControlModules.desiredFootStep.footstepGenerator;
 
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
+import us.ihmc.robotics.robotSide.RobotSide;
 
 /**
  * This interface is part of the API of {@link ContinuousStepGenerator}.
@@ -18,7 +19,8 @@ public interface FootstepAdjustment
     * 
     * @param footstepPose the generated footstep from the {@code ContinuousStepGenerator}. Height,
     *           pitch, and roll have to be computed.
+    * @param footSide indicates for which foot the footstep is meant to be.
     * @return the adjusted footstep.
     */
-   FramePose3DReadOnly adjustFootstep(FramePose2DReadOnly footstepPose);
+   FramePose3DReadOnly adjustFootstep(FramePose2DReadOnly footstepPose, RobotSide footSide);
 }
