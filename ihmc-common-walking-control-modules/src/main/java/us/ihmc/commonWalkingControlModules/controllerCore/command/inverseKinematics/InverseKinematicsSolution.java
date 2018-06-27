fbe2337f37fcc0303b1de56242bdab98741984f6
Robert <rgriffin@ihmc.us>
2018-06-27 14:56:02 -0500

    got the achieved and desired momentums added to controller core toolbox

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/inverseKinematics/InverseKinematicsSolution.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/inverseKinematics/InverseKinematicsSolution.java
@@ -3,11 +3,15 @@ package us.ihmc.commonWalkingControlModules.controllerCore.command.inverseKinema
 import org.ejml.data.DenseMatrix64F;
 
 import us.ihmc.robotics.screwTheory.InverseDynamicsJoint;
+import us.ihmc.robotics.screwTheory.SpatialForceVector;
+import us.ihmc.robotics.screwTheory.SpatialMotionVector;
 
 public class InverseKinematicsSolution
 {
    private final InverseDynamicsJoint[] jointsToOptimizeFor;
    private final DenseMatrix64F jointVelocities;
+
+   private SpatialForceVector centroidalMomentumSolution;
 
    public InverseKinematicsSolution(InverseDynamicsJoint[] jointsToOptimizeFor, DenseMatrix64F jointVelocities)
    {
@@ -23,5 +27,15 @@ public class InverseKinematicsSolution
    public DenseMatrix64F getJointVelocities()
    {
       return jointVelocities;
+   }
+
+   public void setCentroidalMomentumSolution(SpatialForceVector centroidalMomentumSolution)
+   {
+      this.centroidalMomentumSolution = centroidalMomentumSolution;
+   }
+
+   public SpatialForceVector getCentroidalMomentumSolution()
+   {
+      return centroidalMomentumSolution;
    }
 }
