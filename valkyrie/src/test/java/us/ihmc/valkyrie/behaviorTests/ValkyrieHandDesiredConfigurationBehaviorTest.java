fb618c9635a5636792b402cb6dd9b1249693eb4c
InhoLee <ilee@ihmc.us>
2018-07-06 11:24:36 -0500

    All tests of HandDesiredConfigurationBehavior are tested for valkyrie.

--- valkyrie/src/test/java/us/ihmc/valkyrie/behaviorTests/ValkyrieHandDesiredConfigurationBehaviorTest.java
+++ valkyrie/src/test/java/us/ihmc/valkyrie/behaviorTests/ValkyrieHandDesiredConfigurationBehaviorTest.java
@@ -33,4 +33,20 @@ public class ValkyrieHandDesiredConfigurationBehaviorTest extends HumanoidHandDe
    {
       super.testCloseHand();
    }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 51.9)
+   @Test(timeout = 260000)
+   public void testPauseAndResumeCloseHand() throws SimulationExceededMaximumTimeException
+   {
+      super.testPauseAndResumeCloseHand();
+   }
+
+   @Override
+   @ContinuousIntegrationTest(estimatedDuration = 39.8)
+   @Test(timeout = 200000)
+   public void testStopCloseHand() throws SimulationExceededMaximumTimeException
+   {
+      super.testStopCloseHand();
+   }
 }
