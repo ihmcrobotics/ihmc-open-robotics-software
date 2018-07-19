0c87fe5d2f2e69cabf8a172a1bfb276be84c9893
Doug Stephen <dstephen@ihmc.us>
2018-07-19 11:23:20 -0500

    Update runtime entrypoint.

--- ihmc-interfaces/runtime_entrypoint.sh
+++ ihmc-interfaces/runtime_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "sourcing /opt/ros/bouncy/setup.bash"
+echo "sourcing /opt/ros/ardent/setup.bash"
 source /opt/ros/bouncy/setup.bash
 
 #echo "sourcing /root/ros2_ws/install/local_setup.bash"
@@ -8,4 +8,10 @@ source /opt/ros/bouncy/setup.bash
 
 echo "sourcing /root/ihmc_ros1_bridge/install/local_setup.bash"
 source /root/ihmc_ros1_bridge/install/local_setup.bash
+
+echo "sourcing /opt/ros/kinetic/setup.bash"
+source /opt/ros/kinetic/setup.bash
+
+echo "sourcing /root/ihmc_catkin_ws/install/setup.bash"
+source /root/ihmc_catkin_ws/install/setup.bash
 
