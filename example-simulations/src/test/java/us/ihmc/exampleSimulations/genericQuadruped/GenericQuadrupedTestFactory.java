6188bec3ccc9a83fa1774c11601e6ddf26721594
Robert <rgriffin@ihmc.us>
2018-07-31 13:28:53 -0500

    turned on yo variable server for generic quadruped

--- example-simulations/src/test/java/us/ihmc/exampleSimulations/genericQuadruped/GenericQuadrupedTestFactory.java
+++ example-simulations/src/test/java/us/ihmc/exampleSimulations/genericQuadruped/GenericQuadrupedTestFactory.java
@@ -152,6 +152,7 @@ public class GenericQuadrupedTestFactory implements QuadrupedTestFactory
       simulationFactory.setControlMode(controlMode.get());
       simulationFactory.setInitialForceControlState(QuadrupedControllerEnum.DO_NOTHING);
       simulationFactory.setUseLocalCommunicator(useNetworking.get());
+      simulationFactory.setCreateYoVariableServer(true);
 
       if (groundContactModelType.hasValue())
       {
