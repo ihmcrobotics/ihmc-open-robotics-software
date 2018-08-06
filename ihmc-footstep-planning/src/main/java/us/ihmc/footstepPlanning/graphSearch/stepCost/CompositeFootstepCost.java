fd0bda58527afd6b96ea1c719108797f23e679a8
Robert <rgriffin@ihmc.us>
2018-08-05 21:21:12 -0500

    added a composite footstep cost field

---
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepCost/CompositeFootstepCost.java
@@ -0,0 +1,36 @@
+package us.ihmc.footstepPlanning.graphSearch.stepCost;
+
+import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeFootstepCost implements FootstepCost
+{
+   private final List<FootstepCost> footstepCosts;
+
+   public CompositeFootstepCost()
+   {
+      this.footstepCosts = new ArrayList<>();
+   }
+
+   public CompositeFootstepCost(List<FootstepCost> footstepCosts)
+   {
+      this.footstepCosts = footstepCosts;
+   }
+   
+   public void addFootstepCost(FootstepCost footstepCost)
+   {
+      footstepCosts.add(footstepCost);
+   }
+
+   @Override
+   public double compute(FootstepNode startNode, FootstepNode endNode)
+   {
+      double cost = 0.0;
+      for (FootstepCost footstepCost : footstepCosts)
+         cost += footstepCost.compute(startNode, endNode);
+
+      return cost;
+   }
+}
