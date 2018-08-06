3261b9826edc8c8ac56d3c416762607b60b13494
Robert <rgriffin@ihmc.us>
2018-08-06 13:07:00 -0500

    setup a configurable cost builder for the footstep planner

---
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepCost/PerStepCost.java
@@ -0,0 +1,20 @@
+package us.ihmc.footstepPlanning.graphSearch.stepCost;
+
+import us.ihmc.footstepPlanning.graphSearch.FootstepPlannerParameters;
+import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
+
+public class PerStepCost implements FootstepCost
+{
+   private final FootstepPlannerParameters parameters;
+
+   public PerStepCost(FootstepPlannerParameters parameters)
+   {
+      this.parameters = parameters;
+   }
+
+   @Override
+   public double compute(FootstepNode startNode, FootstepNode endNode)
+   {
+      return parameters.getCostPerStep();
+   }
+}
