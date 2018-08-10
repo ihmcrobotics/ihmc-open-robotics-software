dd2430a0799de4dc77954962e81eb52c46812654
Robert <rgriffin@ihmc.us>
2018-08-10 12:00:17 -0500

    upped the bounds on the ECMP proximity for valkyrie hardware

--- valkyrie/src/main/java/us/ihmc/valkyrie/parameters/ValkyrieToeOffParameters.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/parameters/ValkyrieToeOffParameters.java
@@ -35,7 +35,7 @@ public class ValkyrieToeOffParameters extends ToeOffParameters
    @Override
    public double getECMPProximityForToeOff()
    {
-      return runningOnRealRobot ? 0.0 : 0.04;
+      return runningOnRealRobot ? 0.02 : 0.04;
    }
 
 
