32dbaae1bacc14c7a6122fcd8c055c17bcece1dc
Robert <rgriffin@ihmc.us>
2018-09-04 20:37:33 -0500

    added some random enum generators

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/FootstepPlannerType.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/FootstepPlannerType.java
@@ -1,13 +1,23 @@
 package us.ihmc.footstepPlanning;
 
+import us.ihmc.commons.RandomNumbers;
+
+import java.util.Random;
+
 public enum FootstepPlannerType
 {
    PLANAR_REGION_BIPEDAL, PLAN_THEN_SNAP, A_STAR, SIMPLE_BODY_PATH, VIS_GRAPH_WITH_A_STAR;
 
    public static final FootstepPlannerType[] values = values();
+
    public byte toByte()
    {
       return (byte) ordinal();
+   }
+
+   public static FootstepPlannerType generateRandomPlannerType(Random random)
+   {
+      return values[RandomNumbers.nextInt(random, 0, values.length - 1)];
    }
 
    public static FootstepPlannerType fromByte(byte enumAsByte)
