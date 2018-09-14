4bd2613b624300edb97898fb37ead93bc82b912f
Robert <rgriffin@ihmc.us>
2018-09-14 15:21:24 -0500

    made it so that, when adjusting the CoP plan to maintain ICP continuity, the segment is broken to be much smaller, to make the adjustment smaller

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/smoothCMPBasedICPPlanner/ICPGeneration/SmoothCapturePointAdjustmentToolbox.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/smoothCMPBasedICPPlanner/ICPGeneration/SmoothCapturePointAdjustmentToolbox.java
@@ -37,6 +37,12 @@ public class SmoothCapturePointAdjustmentToolbox
 
    private final LinearSolver<DenseMatrix64F> pseudoInverseSolver = new ConfigurableSolvePseudoInverseSVD(defaultSize, defaultSize, 1.0e-10);
 
+   private double maxAdjustedSegmentDuration = 0.1;
+
+   public void setMaxAdjustedSegmentDuration(double maxAdjustedSegmentDuration)
+   {
+      this.maxAdjustedSegmentDuration = maxAdjustedSegmentDuration;
+   }
 
    public void adjustDesiredTrajectoriesForInitialSmoothing3D(double omega0, List<FrameTrajectory3D> copPolynomials3D,
                                                               List<? extends FrameTuple3DReadOnly> icpQuantityInitialConditionList,
@@ -44,6 +50,8 @@ public class SmoothCapturePointAdjustmentToolbox
    {
       FrameTrajectory3D cmpPolynomial3DSegment1 = copPolynomials3D.get(0);
       FrameTrajectory3D cmpPolynomial3DSegment2 = copPolynomials3D.get(1);
+
+      editPolynomialDurations(cmpPolynomial3DSegment1, cmpPolynomial3DSegment2);
 
       for (Axis axis : Axis.values)
       {
@@ -62,6 +70,15 @@ public class SmoothCapturePointAdjustmentToolbox
          computeAdjustedPolynomialCoefficientVectors1D(numberOfCoefficients);
          adjustCMPPolynomials(cmpPolynomialSegment1, cmpPolynomialSegment2);
       }
+   }
+
+   private void editPolynomialDurations(FrameTrajectory3D cmpPolynomialSegment1, FrameTrajectory3D cmpPolynomialSegment2)
+   {
+      double segment1Duration = cmpPolynomialSegment1.getFinalTime() - cmpPolynomialSegment1.getInitialTime();
+      double timeToRemoveFromSegment1 = Math.max(0.0, segment1Duration - maxAdjustedSegmentDuration);
+
+      cmpPolynomialSegment1.setFinalTime(cmpPolynomialSegment1.getFinalTime() - timeToRemoveFromSegment1);
+      cmpPolynomialSegment2.setInitialTime(cmpPolynomialSegment2.getInitialTime() - timeToRemoveFromSegment1);
    }
 
    private void adjustCMPPolynomials(Trajectory cmpPolynomialSegment1, Trajectory cmpPolynomialSegment2)
