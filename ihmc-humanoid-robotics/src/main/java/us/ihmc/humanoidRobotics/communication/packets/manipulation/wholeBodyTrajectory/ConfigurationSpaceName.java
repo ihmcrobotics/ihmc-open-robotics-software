dab9ddce62477d525942e0defbef39b121d65030
InhoLee <ilee@ihmc.us>
2018-09-21 14:28:10 -0500

    Transform generator in ConfigurationSpaceName is moved.

--- ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/manipulation/wholeBodyTrajectory/ConfigurationSpaceName.java
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/manipulation/wholeBodyTrajectory/ConfigurationSpaceName.java
@@ -1,15 +1,10 @@
 package us.ihmc.humanoidRobotics.communication.packets.manipulation.wholeBodyTrajectory;
 
 import gnu.trove.list.TByteList;
-import us.ihmc.euclid.matrix.RotationMatrix;
-import us.ihmc.euclid.transform.RigidBodyTransform;
-import us.ihmc.euclid.tuple4D.Quaternion;
 
 public enum ConfigurationSpaceName
 {
    X, Y, Z, ROLL, PITCH, YAW, SO3;
-
-   private static final boolean generateSE3ByQuaternion = true;
 
    public static final ConfigurationSpaceName[] values = values();
 
@@ -68,71 +63,6 @@ public enum ConfigurationSpaceName
       default:
          throw new RuntimeException("Unexpected value: " + this);
       }
-   }
-
-   public RigidBodyTransform getLocalRigidBodyTransform(double... configuration)
-   {
-      RigidBodyTransform ret = new RigidBodyTransform();
-
-      switch (this)
-      {
-      case X:
-         ret.appendTranslation(configuration[0], 0, 0);
-         break;
-      case Y:
-         ret.appendTranslation(0, configuration[0], 0);
-         break;
-      case Z:
-         ret.appendTranslation(0, 0, configuration[0]);
-         break;
-      case ROLL:
-         ret.appendRollRotation(configuration[0]);
-         break;
-      case PITCH:
-         ret.appendPitchRotation(configuration[0]);
-         break;
-      case YAW:
-         ret.appendYawRotation(configuration[0]);
-         break;
-      case SO3:
-         // uniform quat         
-         if (generateSE3ByQuaternion)
-         {
-            Quaternion quat = new Quaternion();
-
-            double s = configuration[0];
-            double s1 = Math.sqrt(1 - s);
-            double s2 = Math.sqrt(s);
-
-            double theta1 = Math.PI * 2 * configuration[1];
-            double theta2 = Math.PI * 2 * configuration[2];
-
-            quat.set(Math.sin(theta1) * s1, Math.cos(theta1) * s1, Math.sin(theta2) * s2, Math.cos(theta2) * s2);
-            quat.normalize();
-
-            RotationMatrix rotationMatrix = new RotationMatrix(quat);
-            ret.setRotation(rotationMatrix);
-         }
-         // uniform roll pitch yaw
-         else
-         {
-            double theta1 = Math.PI * 2 * configuration[0];
-            double theta2 = Math.acos(1 - 2 * configuration[1]) + Math.PI * 0.5;
-            if (configuration[1] < 0.5)
-               if (theta2 < Math.PI)
-                  theta2 = theta2 + Math.PI;
-               else
-                  theta2 = theta2 - Math.PI;
-            double theta3 = Math.PI * 2 * configuration[2] - Math.PI;
-
-            ret.appendRollRotation(theta1);
-            ret.appendPitchRotation(theta2);
-            ret.appendYawRotation(theta3);
-         }
-         break;
-      }
-
-      return ret;
    }
 
    public byte toByte()
