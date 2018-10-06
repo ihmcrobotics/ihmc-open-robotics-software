71613462fb0bdac9cf4d7939410fd365b28fc6a7
Georg Wiedebach <gwiedebach@ihmc.us>
2018-10-06 15:14:11 -0700

    Fix an exception.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/smoothCMPBasedICPPlanner/AMGeneration/CommandBasedAngularMomentumTrajectoryGenerator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/smoothCMPBasedICPPlanner/AMGeneration/CommandBasedAngularMomentumTrajectoryGenerator.java
@@ -291,26 +291,6 @@ public class CommandBasedAngularMomentumTrajectoryGenerator implements AngularMo
       {
          computeTrajectories(WalkingTrajectoryType.TRANSFER);
       }
-
-      if (initialTransfer && standing)
-      {
-         for (int i = 0; i < transferTrajectories.size(); i++)
-         {
-            transferTrajectories.get(i).reset();
-            for (int j = 0; j < waypoints.size() - 1; j++)
-            {
-               transferTrajectories.get(i).add().setConstant(0.0, Double.POSITIVE_INFINITY, zeroPoint);
-            }
-         }
-         for (int i = 0; i < swingTrajectories.size(); i++)
-         {
-            swingTrajectories.get(i).reset();
-            for (int j = 0; j < waypoints.size() - 1; j++)
-            {
-               swingTrajectories.get(i).add().setConstant(0.0, Double.POSITIVE_INFINITY, zeroPoint);
-            }
-         }
-      }
    }
 
    @Override
