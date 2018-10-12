33d94d03588232c6a3765bd31ae80f01b6ed156f
Robert <rgriffin@ihmc.us>
2018-10-12 11:24:11 -0500

    fixed a bunch of the test categories

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/VisGraphAStarPlannerToolboxTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/VisGraphAStarPlannerToolboxTest.java
@@ -1,8 +1,10 @@
 package us.ihmc.avatar.networkProcessor.footstepPlanningToolboxModule;
 
+import org.junit.Test;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations;
 import us.ihmc.continuousIntegration.IntegrationCategory;
 import us.ihmc.footstepPlanning.FootstepPlannerType;
+import us.ihmc.pubsub.DomainFactory;
 
 @ContinuousIntegrationAnnotations.ContinuousIntegrationPlan(categories = IntegrationCategory.FAST)
 public class VisGraphAStarPlannerToolboxTest extends FootstepPlannerToolboxTest
@@ -11,5 +13,35 @@ public class VisGraphAStarPlannerToolboxTest extends FootstepPlannerToolboxTest
    public FootstepPlannerType getPlannerType()
    {
       return FootstepPlannerType.VIS_GRAPH_WITH_A_STAR;
+   }
+
+   @Override
+   @Test(timeout = 500000)
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 4.2)
+   public void testDatasetsWithoutOcclusion()
+   {
+      pubSubImplementation = DomainFactory.PubSubImplementation.INTRAPROCESS;
+      setup();
+      runAssertionsOnAllDatasetsWithoutOcclusions(dataset -> runAssertions(dataset));
+   }
+
+   @Override
+   @Test(timeout = 500000)
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 13.0, categoriesOverride = IntegrationCategory.IN_DEVELOPMENT)
+   public void testDatasetsWithoutOcclusionInDevelopment()
+   {
+      pubSubImplementation = DomainFactory.PubSubImplementation.INTRAPROCESS;
+      setup();
+      runAssertionsOnAllDatasetsWithoutOcclusionsInDevelopment(dataset -> runAssertions(dataset));
+   }
+
+   @Override
+   @Test(timeout = 500000)
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 4.2, categoriesOverride = IntegrationCategory.IN_DEVELOPMENT)
+   public void testDatasetsWithoutOcclusionRTPS()
+   {
+      pubSubImplementation = DomainFactory.PubSubImplementation.FAST_RTPS;
+      setup();
+      runAssertionsOnAllDatasetsWithoutOcclusions(dataset -> runAssertions(dataset));
    }
 }
