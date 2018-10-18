edcefaa58b656323cdf31d669c7022a095a382dc
Robert <rgriffin@ihmc.us>
2018-10-18 11:41:18 -0400

    got a binding working for the different footstpe planner heuristics costs

--- ihmc-footstep-planning/src/visualizers/java/us/ihmc/footstepPlanning/ui/components/FootstepPlannerCostsUIController.java
+++ ihmc-footstep-planning/src/visualizers/java/us/ihmc/footstepPlanning/ui/components/FootstepPlannerCostsUIController.java
@@ -7,6 +7,7 @@ import us.ihmc.footstepPlanning.communication.FootstepPlannerMessagerAPI;
 import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParameters;
 import us.ihmc.javaFXToolkit.messager.JavaFXMessager;
 import us.ihmc.javaFXToolkit.messager.MessageBidirectionalBinding.PropertyToMessageTypeConverter;
+import us.ihmc.javaFXToolkit.messager.TopicListener;
 
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -24,6 +25,7 @@ public class FootstepPlannerCostsUIController
    @FXML
    private Slider heuristicsWeight;
 
+
    public void attachMessager(JavaFXMessager messager)
    {
       this.messager = messager;
@@ -31,10 +33,15 @@ public class FootstepPlannerCostsUIController
 
    public void bindControls()
    {
+      AtomicReference<FootstepPlannerType> plannerType = messager.createInput(FootstepPlannerMessagerAPI.PlannerTypeTopic);
+      AtomicReference<FootstepPlannerParameters> plannerParameters = messager.createInput(FootstepPlannerMessagerAPI.PlannerParametersTopic);
+
+      messager.registerTopicListener(FootstepPlannerMessagerAPI.PlannerTypeTopic, createPlannerTypeChangeListener(plannerType, plannerParameters));
+
       property.bidirectionalBindYawWeight(yawWeight.valueProperty());
       property.bidirectionalBindCostPerStep(costPerStep.valueProperty());
 
-      AtomicReference<FootstepPlannerType> plannerType = messager.createInput(FootstepPlannerMessagerAPI.PlannerTypeTopic);
+      messager.registerJavaFXSyncedTopicListener(FootstepPlannerMessagerAPI.PlannerTypeTopic, createPlannerTypeChangeListener(plannerType, plannerParameters));
       property.bidirectionalBindHeuristicsWeight(plannerType, heuristicsWeight.valueProperty());
 
       messager.bindBidirectional(FootstepPlannerMessagerAPI.PlannerParametersTopic, property, createConverter(), true);
@@ -54,6 +61,42 @@ public class FootstepPlannerCostsUIController
          public SettableFootstepPlannerParameters interpret(FootstepPlannerParameters messageContent)
          {
             return new SettableFootstepPlannerParameters(messageContent);
+         }
+      };
+   }
+
+   private TopicListener<FootstepPlannerType> createPlannerTypeChangeListener(AtomicReference<FootstepPlannerType> plannerType, AtomicReference<FootstepPlannerParameters> plannerParameters)
+   {
+      return new TopicListener<FootstepPlannerType>()
+      {
+         @Override
+         public void receivedMessageForTopic(FootstepPlannerType messageContent)
+         {
+            FootstepPlannerType footstepPlannerType = plannerType.get();
+            if (footstepPlannerType == null)
+               return;
+
+            double weight = 0.0;
+            switch (footstepPlannerType)
+            {
+            case A_STAR:
+               weight = plannerParameters.get().getCostParameters().getAStarHeuristicsWeight().getValue();
+               break;
+            case VIS_GRAPH_WITH_A_STAR:
+               weight = plannerParameters.get().getCostParameters().getVisGraphWithAStarHeuristicsWeight().getValue();
+               break;
+            case PLANAR_REGION_BIPEDAL:
+               weight = plannerParameters.get().getCostParameters().getDepthFirstHeuristicsWeight().getValue();
+               break;
+            case SIMPLE_BODY_PATH:
+               weight = plannerParameters.get().getCostParameters().getBodyPathBasedHeuristicsWeight().getValue();
+               break;
+            default:
+               weight = 0.0;
+               break;
+            }
+
+            heuristicsWeight.setValue(weight);
          }
       };
    }
