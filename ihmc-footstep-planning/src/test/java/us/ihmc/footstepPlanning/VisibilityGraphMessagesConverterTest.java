317794c5c03f2f7d4d6de50aeeebcde215208819
Robert <rgriffin@ihmc.us>
2018-10-21 21:20:02 -0500

    added a statistics request message and wired it into the footstep planner toolbox

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/VisibilityGraphMessagesConverterTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/VisibilityGraphMessagesConverterTest.java
@@ -11,7 +11,6 @@ import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.footstepPlanning.ui.VisibilityGraphMessagesConverter;
 import us.ihmc.pathPlanning.statistics.VisibilityGraphStatistics;
 import us.ihmc.pathPlanning.visibilityGraphs.VisibilityGraphRandomTools;
 import us.ihmc.pathPlanning.visibilityGraphs.VisibilityGraphTestTools;
