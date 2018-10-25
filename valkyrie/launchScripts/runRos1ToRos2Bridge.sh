c769d8ea6771c5fc5108ebd6c84839bb1f631b95
Mark Paterson <mark.paterson@nasa.gov>
2018-10-25 14:57:58 -0500

    Modify bridge to run in a loop

--- valkyrie/launchScripts/runRos1ToRos2Bridge.sh
+++ valkyrie/launchScripts/runRos1ToRos2Bridge.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source /home/val/.val_config
-source /home/val/.ihmc_bridge_config
-/usr/bin/python3 /opt/ros/ardent/bin/ros2 run ros1_bridge dynamic_bridge --bridge-all-topics &
-BRIDGE_PID=$!
-trap "kill $BRIDGE_PID && source /home/val/.val_config && rosnode kill ros_bridge" INT TERM
-wait
-
+source $HOME/.val_config
+source $HOME/.ihmc_bridge_config
+while [[ 1 ]]; do 
+    /usr/bin/python3 /opt/ros/ardent/bin/ros2 run ros1_bridge dynamic_bridge --bridge-all-topics &
+    BRIDGE_PID=$!
+    trap "kill $BRIDGE_PID && source $HOME/.val_config && rosnode kill ros_bridge; exit 0" INT TERM
+    wait
+done
