957b503c0c7bffb7bebf2d845902312172c04593
InhoLee <ilee@ihmc.us>
2018-10-25 11:56:07 -0500

    Commands are converted into list of kinematics tool box messages.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsPlanningToolboxModule/KinematicsPlanningToolboxController.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsPlanningToolboxModule/KinematicsPlanningToolboxController.java
@@ -1,15 +1,26 @@
 package us.ihmc.avatar.networkProcessor.kinematicsPlanningToolboxModule;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
+import controller_msgs.msg.dds.CapturabilityBasedStatus;
 import controller_msgs.msg.dds.KinematicsPlanningToolboxOutputStatus;
+import controller_msgs.msg.dds.KinematicsToolboxCenterOfMassMessage;
+import controller_msgs.msg.dds.KinematicsToolboxConfigurationMessage;
+import controller_msgs.msg.dds.KinematicsToolboxRigidBodyMessage;
+import controller_msgs.msg.dds.RobotConfigurationData;
+import gnu.trove.list.array.TDoubleArrayList;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.networkProcessor.modules.ToolboxController;
 import us.ihmc.commons.PrintTools;
 import us.ihmc.communication.controllerAPI.CommandInputManager;
 import us.ihmc.communication.controllerAPI.StatusMessageOutputManager;
+import us.ihmc.communication.packets.MessageTools;
+import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.euclid.tools.EuclidCoreTools;
 import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
 import us.ihmc.graphicsDescription.instructions.Graphics3DInstruction;
 import us.ihmc.graphicsDescription.instructions.Graphics3DPrimitiveInstruction;
@@ -17,15 +28,21 @@ import us.ihmc.humanoidRobotics.communication.kinematicsPlanningToolboxAPI.Kinem
 import us.ihmc.humanoidRobotics.communication.kinematicsPlanningToolboxAPI.KinematicsPlanningToolboxRigidBodyCommand;
 import us.ihmc.humanoidRobotics.communication.kinematicsToolboxAPI.KinematicsToolboxConfigurationCommand;
 import us.ihmc.humanoidRobotics.communication.packets.HumanoidMessageTools;
+import us.ihmc.humanoidRobotics.communication.packets.KinematicsToolboxMessageFactory;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.robotDescription.JointDescription;
 import us.ihmc.robotics.robotDescription.LinkDescription;
 import us.ihmc.robotics.robotDescription.LinkGraphicsDescription;
+import us.ihmc.robotics.screwTheory.RigidBody;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
 
 public class KinematicsPlanningToolboxController extends ToolboxController
 {
+   private final AtomicReference<RobotConfigurationData> latestRobotConfigurationDataReference = new AtomicReference<>(null);
+
+   private final AtomicReference<CapturabilityBasedStatus> latestCapturabilityBasedStatusReference = new AtomicReference<>(null);
+
    private final FullHumanoidRobotModel desiredFullRobotModel;
    private final CommandInputManager commandInputManager;
 
@@ -36,6 +53,14 @@ public class KinematicsPlanningToolboxController extends ToolboxController
    private final List<KinematicsPlanningToolboxRigidBodyCommand> rigidBodyCommands;
    private final AtomicReference<KinematicsPlanningToolboxCenterOfMassCommand> centerOfMassCommand;
    private final AtomicReference<KinematicsToolboxConfigurationCommand> configurationCommand;
+
+   private final List<RigidBody> ikRigidBodies;
+   private final Map<RigidBody, List<KinematicsToolboxRigidBodyMessage>> ikRigidBodyMessageMap;
+   private final List<KinematicsToolboxCenterOfMassMessage> ikCenterOfMassMessages;
+   private final AtomicReference<KinematicsToolboxConfigurationMessage> ikConfigurationMessage;
+
+   private final static double keyFrameTimeEpsilon = 0.01;
+   private final TDoubleArrayList keyFrameTimes;
 
    public KinematicsPlanningToolboxController(DRCRobotModel drcRobotModel, FullHumanoidRobotModel fullRobotModel, CommandInputManager commandInputManager,
                                               StatusMessageOutputManager statusOutputManager, YoVariableRegistry parentRegistry)
@@ -52,6 +77,13 @@ public class KinematicsPlanningToolboxController extends ToolboxController
       rigidBodyCommands = new ArrayList<KinematicsPlanningToolboxRigidBodyCommand>();
       centerOfMassCommand = new AtomicReference<>(null);
       configurationCommand = new AtomicReference<>(null);
+
+      ikRigidBodies = new ArrayList<RigidBody>();
+      ikRigidBodyMessageMap = new HashMap<>();
+      ikCenterOfMassMessages = new ArrayList<KinematicsToolboxCenterOfMassMessage>();
+      ikConfigurationMessage = new AtomicReference<>(null);
+
+      keyFrameTimes = new TDoubleArrayList();
 
       isDone = new YoBoolean("isDone", parentRegistry);
    }
@@ -70,8 +102,6 @@ public class KinematicsPlanningToolboxController extends ToolboxController
          List<KinematicsPlanningToolboxRigidBodyCommand> commands = commandInputManager.pollNewCommands(KinematicsPlanningToolboxRigidBodyCommand.class);
          for (int i = 0; i < commands.size(); i++)
             rigidBodyCommands.add(commands.get(i));
-         for (int i = 0; i < commands.size(); i++)
-            PrintTools.info(""+commands.get(i).getEndEffector());
       }
 
       if (commandInputManager.isNewCommandAvailable(KinematicsPlanningToolboxCenterOfMassCommand.class))
@@ -107,38 +137,111 @@ public class KinematicsPlanningToolboxController extends ToolboxController
 
    private boolean updateToolboxConfiguration()
    {
-      // TODO : check number of key frames between each rigidbody commands.
-      PrintTools.info(""+rigidBodyCommands.size());
-      for(int i=0;i<rigidBodyCommands.size();i++)
+      if (rigidBodyCommands.size() == 0)
+         new Exception(this.getClass().getSimpleName() + " needs at least one KinematicsPlanningToolboxRigidBodyMessage.");
+
+      // clear
+      ikRigidBodies.clear();
+      ikRigidBodyMessageMap.clear();
+      ikCenterOfMassMessages.clear();
+      keyFrameTimes.clear();
+
+      for (int i = 0; i < rigidBodyCommands.size(); i++)
       {
-         PrintTools.info(""+rigidBodyCommands.get(i).getEndEffector());
-         int numberOfWayPoints = rigidBodyCommands.get(i).getNumberOfWayPoints();
-         for(int j=0;j<numberOfWayPoints;j++)
+         KinematicsPlanningToolboxRigidBodyCommand command = rigidBodyCommands.get(i);
+
+         RigidBody endEffector = command.getEndEffector();
+         ikRigidBodies.add(endEffector);
+
+         List<KinematicsToolboxRigidBodyMessage> rigidBodyMessages = new ArrayList<KinematicsToolboxRigidBodyMessage>();
+         for (int j = 0; j < command.getNumberOfWayPoints(); j++)
          {
-            PrintTools.info(""+rigidBodyCommands.get(i).getWayPointTime(j));
-            PrintTools.info(""+rigidBodyCommands.get(i).getWayPoint(j));
+            Pose3D wayPoint = command.getWayPoint(j);
+            KinematicsToolboxRigidBodyMessage rigidBodyMessage = MessageTools.createKinematicsToolboxRigidBodyMessage(endEffector, wayPoint.getPosition(),
+                                                                                                                      wayPoint.getOrientation());
+            rigidBodyMessage.getControlFramePositionInEndEffector().set(command.getControlFramePose().getPosition());
+            rigidBodyMessage.getControlFrameOrientationInEndEffector().set(command.getControlFramePose().getOrientation());
+            rigidBodyMessage.getLinearSelectionMatrix().set(MessageTools.createSelectionMatrix3DMessage(command.getSelectionMatrix().getLinearPart()));
+            rigidBodyMessage.getAngularSelectionMatrix().set(MessageTools.createSelectionMatrix3DMessage(command.getSelectionMatrix().getAngularPart()));
+            rigidBodyMessage.getLinearWeightMatrix().set(MessageTools.createWeightMatrix3DMessage(command.getWeightMatrix().getLinearPart()));
+            rigidBodyMessage.getAngularWeightMatrix().set(MessageTools.createWeightMatrix3DMessage(command.getWeightMatrix().getAngularPart()));
+            // TODO : allowable displacement.
+
+            rigidBodyMessages.add(rigidBodyMessage);
+         }
+         ikRigidBodyMessageMap.put(endEffector, rigidBodyMessages);
+
+         if (i == 0) // save key frame times for the first rigid body message.
+            for (int j = 0; j < command.getNumberOfWayPoints(); j++)
+               keyFrameTimes.add(command.getWayPointTime(j));
+         else if (!checkKeyFrameTimes(command))
+            new Exception(command.getClass().getSimpleName() + " must have same key frame times with the first KinematicsPlanningToolboxRigidBodyMessage.");
+      }
+
+      if (centerOfMassCommand.get() != null)
+      {
+         if (!checkKeyFrameTimes(centerOfMassCommand.get()))
+            new Exception(centerOfMassCommand.get().getClass().getSimpleName()
+                  + " must have same key frame times with KinematicsPlanningToolboxRigidBodyMessage.");
+
+         for (int i = 0; i < centerOfMassCommand.get().getNumberOfWayPoints(); i++)
+         {
+            KinematicsToolboxCenterOfMassMessage comMessage = MessageTools.createKinematicsToolboxCenterOfMassMessage(centerOfMassCommand.get().getWayPoint(i));
+            comMessage.getWeights().set(MessageTools.createWeightMatrix3DMessage(centerOfMassCommand.get().getWeightMatrix()));
+            comMessage.getSelectionMatrix().set(MessageTools.createSelectionMatrix3DMessage(centerOfMassCommand.get().getSelectionMatrix()));
+
+            ikCenterOfMassMessages.add(comMessage);
          }
       }
-      
-      if(centerOfMassCommand.get() != null)
+      else
       {
-         PrintTools.info(""+centerOfMassCommand.get().getNumberOfWayPoints());   
+         for (int i = 0; i < getNumberOfKeyFrames(); i++)
+         {
+            KinematicsToolboxCenterOfMassMessage comMessage = KinematicsToolboxMessageFactory.holdCenterOfMassCurrentPosition(getDesiredFullRobotModel().getRootBody(),
+                                                                                                                              true, true, true);
+
+            ikCenterOfMassMessages.add(comMessage);
+         }
+      }
+
+      if (configurationCommand.get() != null)
+      {
+         
+         //ikConfigurationMessage.set(configurationCommand.get());
       }
       else
       {
-         // create default com trajectory
+         ikConfigurationMessage.set(KinematicsToolboxMessageFactory.privilegedConfigurationFromFullRobotModel(getDesiredFullRobotModel(), false));
       }
-      
-      if(configurationCommand.get() != null)
-      {
-         PrintTools.info(""+configurationCommand.get().getPrivilegedJointAngles());   
-      }
-      else
-      {
-         // create default configuration
-      }
-      
+
       return true;
+   }
+
+   private boolean checkKeyFrameTimes(KinematicsPlanningToolboxRigidBodyCommand command)
+   {
+      if (command.getNumberOfWayPoints() != keyFrameTimes.size())
+         return false;
+      for (int i = 0; i < keyFrameTimes.size(); i++)
+         if (!EuclidCoreTools.epsilonEquals(command.getWayPointTime(i), keyFrameTimes.size(), keyFrameTimeEpsilon))
+            return false;
+
+      return true;
+   }
+
+   private boolean checkKeyFrameTimes(KinematicsPlanningToolboxCenterOfMassCommand command)
+   {
+      if (command.getNumberOfWayPoints() != keyFrameTimes.size())
+         return false;
+      for (int i = 0; i < keyFrameTimes.size(); i++)
+         if (!EuclidCoreTools.epsilonEquals(command.getWayPointTime(i), keyFrameTimes.size(), keyFrameTimeEpsilon))
+            return false;
+
+      return true;
+   }
+
+   private int getNumberOfKeyFrames()
+   {
+      return keyFrameTimes.size();
    }
 
    public KinematicsPlanningToolboxOutputStatus getSolution()
@@ -150,7 +253,7 @@ public class KinematicsPlanningToolboxController extends ToolboxController
    {
       return desiredFullRobotModel;
    }
-   
+
    public static void recursivelyModifyGraphics(JointDescription joint, AppearanceDefinition ghostApperance)
    {
       if (joint == null)
@@ -184,5 +287,17 @@ public class KinematicsPlanningToolboxController extends ToolboxController
       {
          recursivelyModifyGraphics(child, ghostApperance);
       }
+   }
+
+   // TODO : check this method and variable.
+   public void updateRobotConfigurationData(RobotConfigurationData newConfigurationData)
+   {
+      latestRobotConfigurationDataReference.set(newConfigurationData);
+   }
+
+   // TODO : check this method and variable.
+   public void updateCapturabilityBasedStatus(CapturabilityBasedStatus newStatus)
+   {
+      latestCapturabilityBasedStatusReference.set(newStatus);
    }
 }
