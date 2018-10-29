47d2f3eba3910d2f2616deffd63c03a94df43efb
AwareOperator <AwareOperator@unknown>
2018-10-29 10:32:03 -0700

    Disable angular momentum adjustment in final transfer phase.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/smoothCMPBasedICPPlanner/SmoothCMPBasedICPPlanner.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/smoothCMPBasedICPPlanner/SmoothCMPBasedICPPlanner.java
@@ -443,7 +443,8 @@ public class SmoothCMPBasedICPPlanner extends AbstractICPPlanner
       angularMomentumTrajectoryGenerator.computeReferenceAngularMomentumStartingFromDoubleSupport(isInitialTransfer.getValue(), isStanding.getValue());
       angularMomentumTrajectoryGenerator.initializeForDoubleSupport(ZERO_TIME, isStanding.getBooleanValue());
 
-      if (isInitialTransfer.getValue() && isStanding.getValue())
+      // If standing still or in final transfer do not use angular momentum.
+      if (isStanding.getValue())
       {
          referenceCMPGenerator.initializeForTransfer(ZERO_TIME, referenceCoPGenerator.getTransferCoPTrajectories(),
                                                      referenceCoPGenerator.getSwingCoPTrajectories(), null, null);
@@ -493,7 +494,7 @@ public class SmoothCMPBasedICPPlanner extends AbstractICPPlanner
          referenceCoMGenerator.computeTrajectoryStartingFromSingleSupport(referenceCoPGenerator.getTransferCoPTrajectories(), referenceCoPGenerator.getSwingCoPTrajectories(),
                                                   referenceICPGenerator.getICPPositonFromCoPDesiredFinalList());
       }
-      
+
       angularMomentumTrajectoryGenerator.addCopAndComSetpointsToPlan(referenceCoPGenerator.getWaypoints(), referenceCoMGenerator.getCoMPositionDesiredInitialList(),
                                                            referenceCoMGenerator.getCoMPositionDesiredFinalList(),
                                                            referenceCoMGenerator.getCoMVelocityDesiredInitialList(),
@@ -557,7 +558,8 @@ public class SmoothCMPBasedICPPlanner extends AbstractICPPlanner
          referenceICPGenerator.getLinearData(desiredICPPosition, desiredICPVelocity, desiredICPAcceleration);
          referenceCoMGenerator.getLinearData(desiredCoMPosition, desiredCoMVelocity, desiredCoMAcceleration);
 
-         if (isInitialTransfer.getValue() && isStanding.getValue())
+         // If standing still or in final transfer do not use angular momentum.
+         if (isStanding.getValue())
          {
             desiredCentroidalAngularMomentum.setToZero();
             desiredCentroidalTorque.setToZero();
