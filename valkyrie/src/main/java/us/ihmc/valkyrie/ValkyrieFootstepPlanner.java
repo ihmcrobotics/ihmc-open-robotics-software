7aa9e46653867ed23d2170b6f9159b3eb7c6df16
Operator Computer <OperatorComputer@unknown>
2018-11-01 17:28:15 -0500

    created a footstep planner for val that can run outside the network processor

---
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieFootstepPlanner.java
@@ -0,0 +1,54 @@
+package us.ihmc.valkyrie;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.martiansoftware.jsap.JSAPException;
+
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.networkProcessor.DRCNetworkModuleParameters;
+import us.ihmc.avatar.networkProcessor.DRCNetworkProcessor;
+import us.ihmc.avatar.networkProcessor.footstepPlanningToolboxModule.FootstepPlanningToolboxModule;
+import us.ihmc.commons.PrintTools;
+import us.ihmc.communication.configuration.NetworkParameters;
+
+public class ValkyrieFootstepPlanner
+{
+   public ValkyrieFootstepPlanner()
+   {
+      ValkyrieRobotModel robotModel = new ValkyrieRobotModel(RobotTarget.REAL_ROBOT, false);
+      
+      
+      tryToStartModule(() -> startFootstepModule(robotModel)); 
+   }
+   
+   private void startFootstepModule(DRCRobotModel robotModel) throws IOException
+   {
+      new FootstepPlanningToolboxModule(robotModel, null, true);
+   }
+   
+   private void tryToStartModule(ModuleStarter runnable)
+   {
+      try
+      {
+         runnable.startModule();
+      }
+      catch (RuntimeException | IOException e)
+      {
+         PrintTools.error(this, "Failed to start a module in the network processor, stack trace:");
+         e.printStackTrace();
+      }
+   }
+   
+   private interface ModuleStarter
+   {
+      void startModule() throws IOException;
+   }
+
+   public static void main(String[] args) throws URISyntaxException, JSAPException
+   {
+      ValkyrieFootstepPlanner footstepPlanner = new ValkyrieFootstepPlanner();
+   }
+}
