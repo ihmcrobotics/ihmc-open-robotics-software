c981948e6b16e510df8aa60c799b8e60cb6fb28f
Robert <rgriffin@ihmc.us>
2018-11-01 16:02:53 -0400

    fixed a null pointer exception and a minimum timeout bug

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/roughTerrainPlanning/MessagerAStarOnRoughTerrainTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/roughTerrainPlanning/MessagerAStarOnRoughTerrainTest.java
@@ -1,8 +1,16 @@
 package us.ihmc.footstepPlanning.roughTerrainPlanning;
 
+import org.junit.Test;
+import us.ihmc.commons.PrintTools;
+import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.continuousIntegration.IntegrationCategory;
 import us.ihmc.footstepPlanning.FootstepPlannerType;
+
+import java.util.List;
+
+import static us.ihmc.footstepPlanning.testTools.PlannerTestEnvironments.getTestData;
 
 @ContinuousIntegrationPlan(categories = IntegrationCategory.FAST)
 public class MessagerAStarOnRoughTerrainTest extends MessagerFootstepPlannerOnRoughTerrainTest
@@ -13,4 +21,10 @@ public class MessagerAStarOnRoughTerrainTest extends MessagerFootstepPlannerOnRo
       return FootstepPlannerType.A_STAR;
    }
 
+   @ContinuousIntegrationTest(estimatedDuration = 20)
+   @Test(timeout = 30000000)
+   public void test()
+   {
+      super.test();
+   }
 }
