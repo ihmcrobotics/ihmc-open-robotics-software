034734818a4732fbd9e62b230bf34b633b3b3f47
Mark Paterson <mark.paterson@nasa.gov>
2018-11-19 16:21:50 -0600

    Revert "Permit arm motions in WalkingState to workaround stability bug"
    This workaround is no longer needed.
    
    This reverts commit 488eb8ad25c55aa69ac1168d49373c3853986be7.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/walkingController/states/WalkingState.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/walkingController/states/WalkingState.java
@@ -51,7 +51,7 @@ public abstract class WalkingState implements State
 
    public boolean isStateSafeToConsumeManipulationCommands()
    {
-      return true;
+      return false;
    }
 
    public WalkingStateEnum getStateEnum()
