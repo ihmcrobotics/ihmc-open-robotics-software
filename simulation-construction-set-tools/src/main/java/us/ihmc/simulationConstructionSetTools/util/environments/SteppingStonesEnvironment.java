0de978454fab9fb6fa2a07cdb65e6f21f69bb61c
Stephen McCrory <smccrory@ihmc.us>
2018-11-26 14:36:34 -0600

    Fixing sim transition time checks and stepping stones test initial position

--- simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/SteppingStonesEnvironment.java
+++ simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/SteppingStonesEnvironment.java
@@ -99,7 +99,7 @@ public class SteppingStonesEnvironment implements CommonAvatarEnvironmentInterfa
 
    public Point3D getStartPosition()
    {
-      return new Point3D(0.5, 0.5, 0.4);
+      return new Point3D(0.5, 0.5, 0.25);
    }
 
    public double getStartYaw()
