24f9be1e3814f07f76930481e1747a96de5cc873
Georg Wiedebach <gwiedebach@ihmc.us>
2018-11-29 12:24:13 -0600

    Made new native solvers and test them.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/commonOps/NativeCommonOpsWrapper.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/commonOps/NativeCommonOpsWrapper.java
@@ -3,4 +3,10 @@ package us.ihmc.robotics.linearAlgebra.commonOps;
 public class NativeCommonOpsWrapper
 {
    public native void computeAB(double[] result, double[] aData, double[] bData, int aRows, int aCols, int bCols);
+
+   public native void solve(double[] result, double[] aData, double[] bData, int aRows);
+
+   public native void solveRobust(double[] result, double[] aData, double[] bData, int aRows, int aCols);
+
+   public native void solveDamped(double[] result, double[] aData, double[] bData, int aRows, int aCols, double alpha);
 }
