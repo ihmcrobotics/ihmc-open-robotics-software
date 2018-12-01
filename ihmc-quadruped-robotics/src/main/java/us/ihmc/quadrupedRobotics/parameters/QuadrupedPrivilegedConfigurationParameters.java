166b4a4ccf82402d8e732ee0909ca869b5db2d16
Robert <rgriffin@ihmc.us>
2018-12-01 10:00:52 -0600

    modified a little bit how the privileged configuration works for quadrupeds

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/parameters/QuadrupedPrivilegedConfigurationParameters.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/parameters/QuadrupedPrivilegedConfigurationParameters.java
@@ -6,4 +6,10 @@ import us.ihmc.robotics.robotSide.RobotQuadrant;
 public abstract class QuadrupedPrivilegedConfigurationParameters extends JointPrivilegedConfigurationParameters
 {
    public abstract double getPrivilegedConfiguration(RobotQuadrant quadrant);
+
+   public abstract double getKneeConfigurationGain();
+
+   public abstract double getKneeConfigurationVelocityGain();
+
+   public abstract double getKneeWeight();
 }
