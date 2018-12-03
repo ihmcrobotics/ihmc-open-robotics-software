d4890002a5de7bf6facac939213130d584f33fc4
Robert <rgriffin@ihmc.us>
2018-12-02 22:41:53 -0600

    created an improved quadruped support polygon that hopefully wont shift as much

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controller/QuadrupedControllerToolbox.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controller/QuadrupedControllerToolbox.java
@@ -5,6 +5,7 @@ import static us.ihmc.humanoidRobotics.footstep.FootstepUtils.worldFrame;
 import java.awt.Color;
 import java.util.List;
 
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.QuadrupedSupportPolygons;
 import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.YoPlaneContactState;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
@@ -22,6 +23,7 @@ import us.ihmc.quadrupedRobotics.controller.toolbox.QuadrupedSoleForceEstimator;
 import us.ihmc.quadrupedRobotics.estimator.GroundPlaneEstimator;
 import us.ihmc.quadrupedRobotics.estimator.YoGroundPlaneEstimator;
 import us.ihmc.quadrupedRobotics.estimator.referenceFrames.QuadrupedReferenceFrames;
+import us.ihmc.quadrupedRobotics.geometry.supportPolygon.QuadrupedSupportPolygon;
 import us.ihmc.quadrupedRobotics.model.QuadrupedPhysicalProperties;
 import us.ihmc.quadrupedRobotics.model.QuadrupedRuntimeEnvironment;
 import us.ihmc.quadrupedRobotics.planning.ContactState;
@@ -46,7 +48,6 @@ public class QuadrupedControllerToolbox
    private final QuadrantDependentList<YoFramePoint3D> groundPlanePositions;
    private final QuadrantDependentList<YoFramePoint3D> upcomingGroundPlanePositions;
 
-
    private final QuadrupedSoleForceEstimator soleForceEstimator;
    private final QuadrupedFallDetector fallDetector;
 
@@ -61,10 +62,8 @@ public class QuadrupedControllerToolbox
    private final QuadrantDependentList<YoPlaneContactState> footContactStates = new QuadrantDependentList<>();
    private final List<ContactablePlaneBody> contactablePlaneBodies;
 
-   private final YoFrameConvexPolygon2D supportPolygon;
-   private final YoArtifactPolygon supportPolygonVisualizer;
+   private final QuadrupedSupportPolygons supportPolygon;
 
-   private final FramePoint3D tempPoint = new FramePoint3D();
    private final FrameVector3D comVelocityEstimate = new FrameVector3D();
 
    private final YoFrameVector3D yoCoMVelocityEstimate;
@@ -84,9 +83,6 @@ public class QuadrupedControllerToolbox
       footControlModuleParameters = new QuadrupedFootControlModuleParameters();
       registry.addChild(footControlModuleParameters.getYoVariableRegistry());
 
-      supportPolygon = new YoFrameConvexPolygon2D("supportPolygon", ReferenceFrame.getWorldFrame(), 4, registry);
-      supportPolygonVisualizer = new YoArtifactPolygon("supportPolygonVisualizer", supportPolygon, Color.black, false, 1);
-      yoGraphicsListRegistry.registerArtifact("supportPolygon", supportPolygonVisualizer);
 
 
       // create controllers and estimators
@@ -128,6 +124,11 @@ public class QuadrupedControllerToolbox
          contactStates.put(robotQuadrant, ContactState.IN_CONTACT);
       }
 
+
+      supportPolygon = new QuadrupedSupportPolygons(referenceFrames.getCenterOfFeetZUpFrameAveragingLowestZHeightsAcrossEnds(), footContactStates,
+                                                    referenceFrames.getSoleZUpFrames(), registry, yoGraphicsListRegistry);
+
+
       update(); 
    }
 
@@ -135,7 +136,7 @@ public class QuadrupedControllerToolbox
    {
       referenceFrames.updateFrames();
       soleForceEstimator.compute();
-      updateSupportPolygon();
+      supportPolygon.updateUsingContactStates(footContactStates);
 
       if(centerOfMassDataHolder == null)
       {
@@ -149,22 +150,6 @@ public class QuadrupedControllerToolbox
 
       yoCoMVelocityEstimate.setMatchingFrame(comVelocityEstimate);
       dcmPositionEstimator.compute(comVelocityEstimate);
-   }
-
-   private void updateSupportPolygon()
-   {
-      supportPolygon.clear();
-
-      for(RobotQuadrant quadrant : RobotQuadrant.values)
-      {
-         if(contactStates.get(quadrant) == ContactState.IN_CONTACT)
-         {
-            tempPoint.setToZero(referenceFrames.getSoleFrame(quadrant));
-            supportPolygon.addVertexMatchingFrame(tempPoint);
-         }
-      }
-
-      supportPolygon.update();
    }
 
    public FullQuadrupedRobotModel getFullRobotModel()
