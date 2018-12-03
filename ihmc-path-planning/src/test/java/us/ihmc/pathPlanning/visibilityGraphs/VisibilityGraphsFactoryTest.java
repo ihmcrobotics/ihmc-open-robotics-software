f17d86f5a71cf95c3882a0d5a8577bf29b233358
JerryPratt <jpratt@ihmc.us>
2018-12-03 11:07:37 -0600

    Some more tests for VisibilityGraphsFactory

--- ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/VisibilityGraphsFactoryTest.java
+++ ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/VisibilityGraphsFactoryTest.java
@@ -15,7 +15,10 @@ import us.ihmc.euclid.geometry.interfaces.Vertex2DSupplier;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
+import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster;
 import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.Connection;
+import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.ConnectionPoint3D;
+import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.InterRegionVisibilityMap;
 import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.NavigableRegion;
 import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.VisibilityMap;
 import us.ihmc.pathPlanning.visibilityGraphs.interfaces.NavigableExtrusionDistanceCalculator;
@@ -33,7 +36,7 @@ public class VisibilityGraphsFactoryTest
 
    @Test(timeout = 30000)
    @ContinuousIntegrationTest(estimatedDuration = 0.0)
-   public void testCreateNavigableRegion()
+   public void testCreateNavigableRegionSingleSquare()
    {
       RigidBodyTransform transformToWorld = new RigidBodyTransform();
 
@@ -56,6 +59,29 @@ public class VisibilityGraphsFactoryTest
       NavigableRegion navigableRegion = VisibilityGraphsFactory.createNavigableRegion(planarRegion, otherRegions, orthogonalAngle, clusterResolution,
                                                                                       obstacleRegionFilter, filter, navigableCalculator, obstacleCalculator);
 
+      List<Cluster> obstacleClusters = navigableRegion.getObstacleClusters();
+      assertTrue(obstacleClusters.isEmpty());
+
+      List<Cluster> clusters = navigableRegion.getAllClusters();
+      assertEquals(1, clusters.size());
+
+      Cluster cluster = clusters.get(0);
+
+      List<Point2DReadOnly> navigableExtrusionsInLocal = cluster.getNavigableExtrusionsInLocal();
+      assertEquals(8, navigableExtrusionsInLocal.size());
+
+      assertTrue(containsPoint(new Point2D(1.0, 1.0), navigableExtrusionsInLocal));
+      assertTrue(containsPoint(new Point2D(1.0, 2.0), navigableExtrusionsInLocal));
+      assertTrue(containsPoint(new Point2D(2.0, 2.0), navigableExtrusionsInLocal));
+      assertTrue(containsPoint(new Point2D(2.0, 1.0), navigableExtrusionsInLocal));
+      assertTrue(containsPoint(new Point2D(1.5, 1.0), navigableExtrusionsInLocal));
+      assertTrue(containsPoint(new Point2D(1.0, 1.5), navigableExtrusionsInLocal));
+      assertTrue(containsPoint(new Point2D(2.0, 1.5), navigableExtrusionsInLocal));
+      assertTrue(containsPoint(new Point2D(1.5, 2.0), navigableExtrusionsInLocal));
+
+      List<Point2DReadOnly> nonNavigableExtrusionsInLocal = cluster.getNonNavigableExtrusionsInLocal();
+      assertEquals(4, nonNavigableExtrusionsInLocal.size());
+
       VisibilityMap visibilityMapInLocal = navigableRegion.getVisibilityMapInLocal();
       Set<Connection> connections = visibilityMapInLocal.getConnections();
 
@@ -72,6 +98,17 @@ public class VisibilityGraphsFactoryTest
       Point2D pointHIn = new Point2D(1.5, 1.0);
 
       assertTrue(containsAllConnectionPairs(connections, pointAIn, pointBIn, pointCIn, pointDIn, pointEIn, pointFIn, pointGIn, pointHIn));
+   }
+
+   private boolean containsPoint(Point2D testPoint, List<Point2DReadOnly> points)
+   {
+      for (Point2DReadOnly point : points)
+      {
+         if (point.epsilonEquals(testPoint, EPSILON))
+            return true;
+      }
+
+      return false;
    }
 
    private ObstacleExtrusionDistanceCalculator createConstantObstacleExtrusionDistanceCalculator(double extrusionDistance)
@@ -128,7 +165,121 @@ public class VisibilityGraphsFactoryTest
 
    @Test(timeout = 30000)
    @ContinuousIntegrationTest(estimatedDuration = 0.0)
-   public void testCreateNavigableRegions()
+   public void testCreateInterRegionVisibilityMapTwoSquareToStepAcross()
+   {
+      VisibilityGraphsParameters parameters = createVisibilityGraphParametersForTests();
+
+      List<PlanarRegion> allRegions = new ArrayList<>();
+
+      RigidBodyTransform transformToWorld = new RigidBodyTransform();
+
+      Point2D pointA = new Point2D(-0.01, -0.01);
+      Point2D pointB = new Point2D(-0.01, 1.01);
+      Point2D pointC = new Point2D(1.01, 1.01);
+      Point2D pointD = new Point2D(1.01, -0.01);
+
+      PlanarRegion planarRegionOne = createPlanarRegion(transformToWorld, pointA, pointB, pointC, pointD);
+      planarRegionOne.setRegionId(1);
+      allRegions.add(planarRegionOne);
+
+      Point2D pointE = new Point2D(1.09, -0.01);
+      Point2D pointF = new Point2D(1.09, 1.01);
+      Point2D pointG = new Point2D(2.11, 1.01);
+      Point2D pointH = new Point2D(2.11, -0.01);
+
+      PlanarRegion planarRegionTwo = createPlanarRegion(transformToWorld, pointE, pointF, pointG, pointH);
+      planarRegionTwo.setRegionId(2);
+      allRegions.add(planarRegionTwo);
+
+      List<NavigableRegion> navigableRegions = VisibilityGraphsFactory.createNavigableRegionButNotVisibilityMaps(allRegions, parameters);
+      //      VisibilityGraphsFactory.createStaticVisibilityMapsForNavigableRegions(navigableRegions);
+      InterRegionVisibilityMap interRegionVisibilityMap = VisibilityGraphsFactory.createInterRegionVisibilityMap(navigableRegions,
+                                                                                                                 parameters.getInterRegionConnectionFilter());
+
+      VisibilityMap visibilityMap = interRegionVisibilityMap.getVisibilityMapInWorld();
+
+      Set<Connection> interConnections = visibilityMap.getConnections();
+      Set<ConnectionPoint3D> vertices = visibilityMap.getVertices();
+
+      assertEquals(7, interConnections.size());
+
+      Point2D pointC2 = new Point2D(1.0, 1.0);
+      Point2D pointCD2 = new Point2D(1.0, 0.5);
+      Point2D pointD2 = new Point2D(1.0, 0.0);
+
+      Point2D pointE2 = new Point2D(1.1, 0.0);
+      Point2D pointEF2 = new Point2D(1.1, 0.5);
+      Point2D pointF2 = new Point2D(1.1, 1.0);
+
+      assertTrue(doesConnectionsContain(interConnections, pointC2, pointF2));
+      assertTrue(doesConnectionsContain(interConnections, pointC2, pointEF2));
+      assertTrue(doesConnectionsContain(interConnections, pointCD2, pointF2));
+      assertTrue(doesConnectionsContain(interConnections, pointCD2, pointEF2));
+      assertTrue(doesConnectionsContain(interConnections, pointCD2, pointE2));
+      assertTrue(doesConnectionsContain(interConnections, pointD2, pointEF2));
+      assertTrue(doesConnectionsContain(interConnections, pointD2, pointE2));
+
+      //TODO: Why are the connections there, but the vertices are not?
+      assertEquals(0, vertices.size());
+   }
+
+   @Test(timeout = 30000)
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   public void testCreateInterRegionVisibilityMapTwoSquareOneObstacle()
+   {
+      VisibilityGraphsParameters parameters = createVisibilityGraphParametersForTests();
+
+      List<PlanarRegion> allRegions = new ArrayList<>();
+
+      RigidBodyTransform transformToWorld = new RigidBodyTransform();
+
+      Point2D pointA = new Point2D(-0.01, -0.01);
+      Point2D pointB = new Point2D(-0.01, 1.01);
+      Point2D pointC = new Point2D(1.01, 1.01);
+      Point2D pointD = new Point2D(1.01, -0.01);
+
+      PlanarRegion planarRegionOne = createPlanarRegion(transformToWorld, pointA, pointB, pointC, pointD);
+      planarRegionOne.setRegionId(1);
+      allRegions.add(planarRegionOne);
+
+      Point2D pointE = new Point2D(1.09, -0.01);
+      Point2D pointF = new Point2D(1.09, 1.01);
+      Point2D pointG = new Point2D(2.11, 1.01);
+      Point2D pointH = new Point2D(2.11, -0.01);
+
+      PlanarRegion planarRegionTwo = createPlanarRegion(transformToWorld, pointE, pointF, pointG, pointH);
+      planarRegionTwo.setRegionId(2);
+      allRegions.add(planarRegionTwo);
+
+      Point2D pointI = new Point2D(0.8, 0.51);
+      Point2D pointJ = new Point2D(0.8, 0.81);
+      Point2D pointK = new Point2D(1.2, 0.81);
+      Point2D pointL = new Point2D(1.2, 0.51);
+
+      transformToWorld.setTranslation(0.0, 0.0, 1.0);
+      PlanarRegion planarRegionThree = createPlanarRegion(transformToWorld, pointI, pointJ, pointK, pointL);
+      planarRegionThree.setRegionId(3);
+      allRegions.add(planarRegionThree);
+
+      List<NavigableRegion> navigableRegions = VisibilityGraphsFactory.createNavigableRegionButNotVisibilityMaps(allRegions, parameters);
+      //      VisibilityGraphsFactory.createStaticVisibilityMapsForNavigableRegions(navigableRegions);
+      InterRegionVisibilityMap interRegionVisibilityMap = VisibilityGraphsFactory.createInterRegionVisibilityMap(navigableRegions,
+                                                                                                                 parameters.getInterRegionConnectionFilter());
+
+      VisibilityMap visibilityMap = interRegionVisibilityMap.getVisibilityMapInWorld();
+
+      Set<Connection> connections = visibilityMap.getConnections();
+      Set<ConnectionPoint3D> vertices = visibilityMap.getVertices();
+
+      assertEquals(5, connections.size());
+
+      //TODO: Why are the connections there, but the vertices are not?
+      assertEquals(0, vertices.size());
+   }
+
+   @Test(timeout = 30000)
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   public void testCreateNavigableRegionsTwoSquaresTooFarToStepAcross()
    {
       VisibilityGraphsParameters parameters = createVisibilityGraphParametersForTests();
       List<PlanarRegion> allRegions = new ArrayList<>();
@@ -153,12 +304,21 @@ public class VisibilityGraphsFactoryTest
 
       allRegions = PlanarRegionTools.ensureClockwiseOrder(allRegions);
 
-      List<NavigableRegion> navigableRegions = VisibilityGraphsFactory.createNavigableRegions(allRegions, parameters);
+      //      List<NavigableRegion> navigableRegions = VisibilityGraphsFactory.createNavigableRegions(allRegions, parameters);
+      List<NavigableRegion> navigableRegions = VisibilityGraphsFactory.createNavigableRegionButNotVisibilityMaps(allRegions, parameters);
+      VisibilityGraphsFactory.createStaticVisibilityMapsForNavigableRegions(navigableRegions);
+      InterRegionVisibilityMap interRegionVisibilityMap = VisibilityGraphsFactory.createInterRegionVisibilityMap(navigableRegions,
+                                                                                                                 parameters.getInterRegionConnectionFilter());
+
+      assertTrue(interRegionVisibilityMap.getVisibilityMapInLocal().getConnections().isEmpty());
 
       assertEquals(2, navigableRegions.size());
 
       NavigableRegion navigableRegionOne = navigableRegions.get(0);
       NavigableRegion navigableRegionTwo = navigableRegions.get(1);
+
+      assertTrue(navigableRegionOne.getObstacleClusters().isEmpty());
+      assertTrue(navigableRegionTwo.getObstacleClusters().isEmpty());
 
       VisibilityMap visibilityMapOne = navigableRegionOne.getVisibilityMapInWorld();
       VisibilityMap visibilityMapTwo = navigableRegionTwo.getVisibilityMapInWorld();
@@ -166,7 +326,8 @@ public class VisibilityGraphsFactoryTest
       Set<Connection> connectionsOne = visibilityMapOne.getConnections();
       Set<Connection> connectionsTwo = visibilityMapTwo.getConnections();
 
-      printConnections(connectionsOne);
+      assertEquals(28, connectionsOne.size());
+      assertEquals(28, connectionsTwo.size());
    }
 
    private VisibilityGraphsParameters createVisibilityGraphParametersForTests()
