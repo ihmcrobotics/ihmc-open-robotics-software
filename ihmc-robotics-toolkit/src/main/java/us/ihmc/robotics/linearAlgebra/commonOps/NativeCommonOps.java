a083aff9533ad00a5f74f4250c8adc6f0799213f
Georg Wiedebach <gwiedebach@ihmc.us>
2018-12-05 14:26:09 -0600

    Some javadoc.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/commonOps/NativeCommonOps.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/commonOps/NativeCommonOps.java
@@ -14,6 +14,14 @@ public class NativeCommonOps
       return new NativeCommonOpsWrapper();
    }
 
+   /**
+    * Computes the matrix multiplication</br>
+    * c = a * b
+    * @param a matrix in multiplication
+    * @param b matrix in multiplication
+    * @param c where the result is stored (modified)
+    * @throws RuntimeException if the matrix dimensions are incompatible.
+    */
    public static void mult(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F c)
    {
       if (a.getNumCols() != b.getNumRows())
@@ -24,6 +32,14 @@ public class NativeCommonOps
       nativeCommonOpsWrapper.computeAB(c.data, a.data, b.data, a.getNumRows(), a.getNumCols(), b.getNumCols());
    }
 
+   /**
+    * Computes the quadratic form</br>
+    * c = a' * b * a
+    * @param a matrix in multiplication
+    * @param b matrix in multiplication
+    * @param c where the result is stored (modified)
+    * @throws RuntimeException if the matrix dimensions are incompatible.
+    */
    public static void multQuad(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F c)
    {
       if (a.getNumRows() != b.getNumCols() || b.getNumCols() != b.getNumRows())
@@ -34,6 +50,15 @@ public class NativeCommonOps
       nativeCommonOpsWrapper.computeAtBA(c.data, a.data, b.data, a.getNumRows(), a.getNumCols());
    }
 
+   /**
+    * Computes the solution to the linear equation</br>
+    * a * x == b</br>
+    * This method requires that the matrix a is square and invertible and uses a LU decomposition.
+    * @param a matrix in equation
+    * @param b matrix in equation
+    * @param x where the result is stored (modified)
+    * @throws RuntimeException if the matrix dimensions are incompatible.
+    */
    public static void solve(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F x)
    {
       if (a.getNumRows() != b.getNumRows() || b.getNumCols() != 1 || a.getNumCols() != a.getNumRows())
@@ -44,6 +69,15 @@ public class NativeCommonOps
       nativeCommonOpsWrapper.solve(x.data, a.data, b.data, a.getNumRows());
    }
 
+   /**
+    * Computes the solution to the linear equation</br>
+    * a * x == b</br>
+    * This method does not require that the matrix a is square and invertible and can be used to solve a least square problem. It uses a householder QR decomposition.
+    * @param a matrix in equation
+    * @param b matrix in equation
+    * @param x where the result is stored (modified)
+    * @throws RuntimeException if the matrix dimensions are incompatible.
+    */
    public static void solveRobust(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F x)
    {
       if (a.getNumRows() != b.getNumRows() || b.getNumCols() != 1)
@@ -54,6 +88,17 @@ public class NativeCommonOps
       nativeCommonOpsWrapper.solveRobust(x.data, a.data, b.data, a.getNumRows(), a.getNumCols());
    }
 
+   /**
+    * Computes the solution to the linear equation</br>
+    * a * x == b</br>
+    * This method uses a damped least square approach and a Cholesky decomposition of</br>
+    * a * a' + diag(alpha * alpha)
+    * @param a matrix in equation
+    * @param b matrix in equation
+    * @param alpha damping value
+    * @param x where the result is stored (modified)
+    * @throws RuntimeException if the matrix dimensions are incompatible.
+    */
    public static void solveDamped(RowD1Matrix64F a, RowD1Matrix64F b, double alpha, RowD1Matrix64F x)
    {
       if (a.getNumRows() != b.getNumRows() || b.getNumCols() != 1)
