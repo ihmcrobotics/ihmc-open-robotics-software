ee73685c666d11fb81bca5defe3785e0b1d0eef2
Georg Wiedebach <gwiedebach@ihmc.us>
2018-12-06 10:40:43 -0600

    Changed exception from runtime to illegal argument.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/commonOps/NativeCommonOps.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/commonOps/NativeCommonOps.java
@@ -20,13 +20,13 @@ public class NativeCommonOps
     * @param a matrix in multiplication
     * @param b matrix in multiplication
     * @param c where the result is stored (modified)
-    * @throws RuntimeException if the matrix dimensions are incompatible.
+    * @throws IllegalArgumentException if the matrix dimensions are incompatible.
     */
    public static void mult(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F c)
    {
       if (a.getNumCols() != b.getNumRows())
       {
-         throw new RuntimeException("Incompatible Matrix Dimensions.");
+         throw new IllegalArgumentException("Incompatible Matrix Dimensions.");
       }
       c.reshape(a.getNumRows(), b.getNumCols());
       nativeCommonOpsWrapper.computeAB(c.data, a.data, b.data, a.getNumRows(), a.getNumCols(), b.getNumCols());
@@ -38,13 +38,13 @@ public class NativeCommonOps
     * @param a matrix in multiplication
     * @param b matrix in multiplication
     * @param c where the result is stored (modified)
-    * @throws RuntimeException if the matrix dimensions are incompatible.
+    * @throws IllegalArgumentException if the matrix dimensions are incompatible.
     */
    public static void multQuad(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F c)
    {
       if (a.getNumRows() != b.getNumCols() || b.getNumCols() != b.getNumRows())
       {
-         throw new RuntimeException("Incompatible Matrix Dimensions.");
+         throw new IllegalArgumentException("Incompatible Matrix Dimensions.");
       }
       c.reshape(a.getNumCols(), a.getNumCols());
       nativeCommonOpsWrapper.computeAtBA(c.data, a.data, b.data, a.getNumRows(), a.getNumCols());
@@ -57,13 +57,13 @@ public class NativeCommonOps
     * @param a matrix in equation
     * @param b matrix in equation
     * @param x where the result is stored (modified)
-    * @throws RuntimeException if the matrix dimensions are incompatible.
+    * @throws IllegalArgumentException if the matrix dimensions are incompatible.
     */
    public static void solve(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F x)
    {
       if (a.getNumRows() != b.getNumRows() || b.getNumCols() != 1 || a.getNumCols() != a.getNumRows())
       {
-         throw new RuntimeException("Incompatible Matrix Dimensions.");
+         throw new IllegalArgumentException("Incompatible Matrix Dimensions.");
       }
       x.reshape(a.getNumCols(), 1);
       nativeCommonOpsWrapper.solve(x.data, a.data, b.data, a.getNumRows());
@@ -76,13 +76,13 @@ public class NativeCommonOps
     * @param a matrix in equation
     * @param b matrix in equation
     * @param x where the result is stored (modified)
-    * @throws RuntimeException if the matrix dimensions are incompatible.
+    * @throws IllegalArgumentException if the matrix dimensions are incompatible.
     */
    public static void solveRobust(RowD1Matrix64F a, RowD1Matrix64F b, RowD1Matrix64F x)
    {
       if (a.getNumRows() != b.getNumRows() || b.getNumCols() != 1)
       {
-         throw new RuntimeException("Incompatible Matrix Dimensions.");
+         throw new IllegalArgumentException("Incompatible Matrix Dimensions.");
       }
       x.reshape(a.getNumCols(), 1);
       nativeCommonOpsWrapper.solveRobust(x.data, a.data, b.data, a.getNumRows(), a.getNumCols());
@@ -97,13 +97,13 @@ public class NativeCommonOps
     * @param b matrix in equation
     * @param alpha damping value
     * @param x where the result is stored (modified)
-    * @throws RuntimeException if the matrix dimensions are incompatible.
+    * @throws IllegalArgumentException if the matrix dimensions are incompatible.
     */
    public static void solveDamped(RowD1Matrix64F a, RowD1Matrix64F b, double alpha, RowD1Matrix64F x)
    {
       if (a.getNumRows() != b.getNumRows() || b.getNumCols() != 1)
       {
-         throw new RuntimeException("Incompatible Matrix Dimensions.");
+         throw new IllegalArgumentException("Incompatible Matrix Dimensions.");
       }
       x.reshape(a.getNumCols(), 1);
       nativeCommonOpsWrapper.solveDamped(x.data, a.data, b.data, a.getNumRows(), a.getNumCols(), alpha);
