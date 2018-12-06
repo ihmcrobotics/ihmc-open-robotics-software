6638ac65bee0bfa2a34460b94a76a7ec34b978dc
InhoLee <ilee@ihmc.us>
2018-12-06 15:57:27 -0600

    Added toolbox api interface to convert rigid body hash code.

---
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/kinematicsPlanningToolboxAPI/KinematicsPlanningToolboxInputCommand.java
@@ -0,0 +1,80 @@
+package us.ihmc.humanoidRobotics.communication.kinematicsPlanningToolboxAPI;
+
+import java.util.Map;
+
+import controller_msgs.msg.dds.KinematicsPlanningToolboxInputMessage;
+import us.ihmc.commons.lists.RecyclingArrayList;
+import us.ihmc.communication.controllerAPI.command.Command;
+import us.ihmc.humanoidRobotics.communication.kinematicsToolboxAPI.KinematicsToolboxConfigurationCommand;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
+import us.ihmc.sensorProcessing.frames.ReferenceFrameHashCodeResolver;
+
+public class KinematicsPlanningToolboxInputCommand implements Command<KinematicsPlanningToolboxInputCommand, KinematicsPlanningToolboxInputMessage>,
+      KinematicsPlanningToolboxAPI<KinematicsPlanningToolboxInputMessage>
+{
+   private final RecyclingArrayList<KinematicsPlanningToolboxRigidBodyCommand> rigidBodyCommands = new RecyclingArrayList<>(KinematicsPlanningToolboxRigidBodyCommand.class);
+   private final KinematicsPlanningToolboxCenterOfMassCommand centerOfMassCommand = new KinematicsPlanningToolboxCenterOfMassCommand();
+   private final KinematicsToolboxConfigurationCommand kinematicsConfigurationCommand = new KinematicsToolboxConfigurationCommand();
+
+   @Override
+   public void set(KinematicsPlanningToolboxInputCommand other)
+   {
+      clear();
+      for (int i = 0; i < other.getRigidBodyCommands().size(); i++)
+         rigidBodyCommands.add().set(other.getRigidBodyCommands().get(i));
+      centerOfMassCommand.set(other.getCenterOfMassCommand());
+      kinematicsConfigurationCommand.set(other.getKinematicsConfigurationCommand());
+   }
+
+   @Override
+   public void clear()
+   {
+      rigidBodyCommands.clear();
+      centerOfMassCommand.clear();
+      kinematicsConfigurationCommand.clear();
+   }
+
+   @Override
+   public void setFromMessage(KinematicsPlanningToolboxInputMessage message)
+   {
+      clear();
+      set(message, null, null);
+   }
+
+   public void set(KinematicsPlanningToolboxInputMessage message, Map<Integer, RigidBodyBasics> rigidBodyHashMap,
+                   ReferenceFrameHashCodeResolver referenceFrameResolver)
+   {
+      for (int i = 0; i < message.getRigidBodyMessages().size(); i++)
+         rigidBodyCommands.add().set(message.getRigidBodyMessages().get(i), rigidBodyHashMap, referenceFrameResolver);
+      centerOfMassCommand.setFromMessage(message.getCenterOfMassMessage());
+      kinematicsConfigurationCommand.setFromMessage(message.getKinematicsConfigurationMessage());
+   }
+
+   @Override
+   public Class<KinematicsPlanningToolboxInputMessage> getMessageClass()
+   {
+      return KinematicsPlanningToolboxInputMessage.class;
+   }
+
+   @Override
+   public boolean isCommandValid()
+   {
+      return true;
+   }
+
+   public RecyclingArrayList<KinematicsPlanningToolboxRigidBodyCommand> getRigidBodyCommands()
+   {
+      return rigidBodyCommands;
+   }
+
+   public KinematicsPlanningToolboxCenterOfMassCommand getCenterOfMassCommand()
+   {
+      return centerOfMassCommand;
+   }
+
+   public KinematicsToolboxConfigurationCommand getKinematicsConfigurationCommand()
+   {
+      return kinematicsConfigurationCommand;
+   }
+
+}
