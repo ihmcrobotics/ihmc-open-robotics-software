5208bd0d599ec66e1f56796d7e6475e3f0eb2f68
Robert <rgriffin@ihmc.us>
2018-12-06 09:14:12 -0600

    started setting up custom privileged configurations for each leg joint on the quadruped

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/parameters/GenericQuadrupedPrivilegedConfigurationParameters.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/parameters/GenericQuadrupedPrivilegedConfigurationParameters.java
@@ -1,17 +1,30 @@
 package us.ihmc.exampleSimulations.genericQuadruped.parameters;
 
 import us.ihmc.quadrupedRobotics.parameters.QuadrupedPrivilegedConfigurationParameters;
+import us.ihmc.robotics.partNames.LegJointName;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public class GenericQuadrupedPrivilegedConfigurationParameters extends QuadrupedPrivilegedConfigurationParameters
 {
-   public double getPrivilegedConfiguration(RobotQuadrant quadrant)
+   public double getPrivilegedConfiguration(LegJointName jointName, RobotQuadrant quadrant)
    {
-      switch (quadrant)
+      switch (jointName)
       {
-         default:
-            return 0.0;
+      case HIP_ROLL:
+         return 0.0;
+      case HIP_PITCH:
+         if (quadrant.isQuadrantInFront())
+            return 0.9;
+         else
+            return -0.9;
+      case KNEE_PITCH:
+         if (quadrant.isQuadrantInFront())
+            return -2.0;
+         else
+            return 2.0;
       }
+
+      throw new IllegalArgumentException("Joint " + jointName + " is not implemented for privileged configurations.");
    }
 
    public double getDefaultConfigurationGain()
--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/parameters/QuadrupedPrivilegedConfigurationParameters.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/parameters/QuadrupedPrivilegedConfigurationParameters.java
@@ -1,11 +1,12 @@
 package us.ihmc.quadrupedRobotics.parameters;
 
 import us.ihmc.commonWalkingControlModules.configurations.JointPrivilegedConfigurationParameters;
+import us.ihmc.robotics.partNames.LegJointName;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public abstract class QuadrupedPrivilegedConfigurationParameters extends JointPrivilegedConfigurationParameters
 {
-   public abstract double getPrivilegedConfiguration(RobotQuadrant quadrant);
+   public abstract double getPrivilegedConfiguration(LegJointName jointName, RobotQuadrant quadrant);
 
    public abstract double getKneeConfigurationGain();
 
