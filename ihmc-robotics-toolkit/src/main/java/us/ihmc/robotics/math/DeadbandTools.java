0162bc0dc4ac44ee3cc33b0a044a9a12b71584d7
Robert <rgriffin@ihmc.us>
2018-12-06 22:01:27 -0600

    set up some deadband tools for frame points and frame vectors

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/DeadbandTools.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/DeadbandTools.java
@@ -1,5 +1,10 @@
 package us.ihmc.robotics.math;
 
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameVector2DBasics;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DBasics;
+
 public class DeadbandTools
 {
    public static double applyDeadband(double deadbandSize, double value)
@@ -13,5 +18,47 @@ public class DeadbandTools
          return Math.max(deadbandCenter, value - deadbandSize);
       else
          return Math.min(deadbandCenter, value + deadbandSize);
+   }
+
+   public static void applyDeadband(FrameVector2DBasics vectorToPack, double deadband)
+   {
+      double length = vectorToPack.length();
+      if (length < deadband)
+      {
+         vectorToPack.setToZero();
+      }
+      else
+      {
+         double newLength = length - deadband;
+         vectorToPack.scale(newLength / length);
+      }
+   }
+
+   public static void applyDeadband(FrameVector3DBasics vectorToPack, double deadband)
+   {
+      double length = vectorToPack.length();
+      if (length < deadband)
+      {
+         vectorToPack.setToZero();
+      }
+      else
+      {
+         double newLength = length - deadband;
+         vectorToPack.scale(newLength / length);
+      }
+   }
+
+   public static void applyDeadband(FramePoint2DBasics pointToPack, FramePoint2DReadOnly centerPoint, double deadband)
+   {
+      double distance = pointToPack.distance(centerPoint);
+      if (distance < deadband)
+      {
+         pointToPack.set(centerPoint);
+      }
+      else
+      {
+         double newDistance = distance - deadband;
+         pointToPack.interpolate(centerPoint, 1.0 - newDistance / distance);
+      }
    }
 }
