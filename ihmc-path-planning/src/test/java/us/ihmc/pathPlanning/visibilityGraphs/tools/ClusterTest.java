73642ac3642dabfe579e9b656f3260775bd6e569
JerryPratt <jpratt@ihmc.us>
2018-12-10 16:21:05 -0600

    Fixed BoundingBox bug in Cluster and added test case.

--- ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/tools/ClusterTest.java
+++ ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/tools/ClusterTest.java
@@ -1,5 +1,6 @@
 package us.ihmc.pathPlanning.visibilityGraphs.tools;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -7,11 +8,13 @@ import org.junit.Test;
 
 import us.ihmc.commons.MutationTestFacilitator;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.euclid.geometry.BoundingBox2D;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster;
 
 public class ClusterTest
 {
+   private static final double EPSILON = 1e-10;
 
    @Test(timeout = 30000)
    @ContinuousIntegrationTest(estimatedDuration = 0.0)
@@ -33,6 +36,24 @@ public class ClusterTest
       assertTrue(obstacleCluster.isInsideNonNavigableZone(pointB));
       assertTrue(obstacleCluster.isInsideNonNavigableZone(pointC));
       assertFalse(obstacleCluster.isInsideNonNavigableZone(pointD));
+   }
+
+   @Test(timeout = 30000)
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   public void testBoundingBox()
+   {
+      Cluster obstacleCluster = new Cluster();
+
+      obstacleCluster.addNonNavigableExtrusionInLocal(new Point2D(0.1, -0.1));
+      obstacleCluster.addNonNavigableExtrusionInLocal(new Point2D(1.1, -0.1));
+      obstacleCluster.addNonNavigableExtrusionInLocal(new Point2D(1.1, 1.6));
+      obstacleCluster.addNonNavigableExtrusionInLocal(new Point2D(0.3, 1.1));
+
+      BoundingBox2D boundingBox = obstacleCluster.getNonNavigableExtrusionsBoundingBox();
+      assertEquals(0.1, boundingBox.getMinX(), EPSILON);
+      assertEquals(-0.1, boundingBox.getMinY(), EPSILON);
+      assertEquals(1.1, boundingBox.getMaxX(), EPSILON);
+      assertEquals(1.6, boundingBox.getMaxY(), EPSILON);
    }
 
    public static void main(String[] args)
