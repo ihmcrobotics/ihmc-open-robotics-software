b233cfd619ca9e59e4e5c6be70b712e810d5c0a7
Robert <rgriffin@ihmc.us>
2018-12-11 15:05:50 -0600

    fixed an index overrun issue

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/icp/CoMTrajectoryPlanner.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/icp/CoMTrajectoryPlanner.java
@@ -205,7 +205,7 @@ public class CoMTrajectoryPlanner
 
    private void updateCornerPoints(int size)
    {
-      for (int i = 0; i < size; i++)
+      for (int i = 0; i < Math.min(size, maxCapacity + 1); i++)
       {
          ContactState contactState = contactSequence.get(i).getContactState();
          double firstCoefficientPositionMultiplier = CoMTrajectoryPlannerTools.getFirstCoefficientPositionMultiplier(contactState, 0.0, omega.getValue());
