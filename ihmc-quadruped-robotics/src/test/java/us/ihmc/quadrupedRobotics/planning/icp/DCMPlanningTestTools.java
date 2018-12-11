9f9cf40798d8a258b5394b8e5e6cffa877de0b78
Robert <rgriffin@ihmc.us>
2018-12-11 09:58:16 -0600

    got a test for computing step transitions

--- ihmc-quadruped-robotics/src/test/java/us/ihmc/quadrupedRobotics/planning/icp/DCMPlanningTestTools.java
+++ ihmc-quadruped-robotics/src/test/java/us/ihmc/quadrupedRobotics/planning/icp/DCMPlanningTestTools.java
@@ -4,6 +4,8 @@ import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.quadrupedBasics.gait.TimeInterval;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
+import java.util.List;
+
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,6 +22,29 @@ public class DCMPlanningTestTools
          EuclidCoreTestTools.assertPoint3DGeometricallyEquals(expected.getSolePosition(robotQuadrant), actual.getSolePosition(robotQuadrant), epsilon);
       }
    }
+
+   public static void assertQuadrupedStepTransitionsEqual(QuadrupedStepTransition expected, QuadrupedStepTransition actual, double epsilon)
+   {
+      assertEquals(expected.getTransitionTime(), actual.getTransitionTime(), epsilon);
+      assertEquals(expected.getNumberOfFeetInTransition(), actual.getNumberOfFeetInTransition());
+
+      for (int i = 0; i < expected.getNumberOfFeetInTransition(); i++)
+      {
+         RobotQuadrant quadrant = expected.getTransitionQuadrant(i);
+         assertTrue(actual.getTransitionQuadrants().contains(quadrant));
+         EuclidCoreTestTools.assertPoint3DGeometricallyEquals(expected.getTransitionPosition(quadrant), actual.getTransitionPosition(quadrant), epsilon);
+         assertEquals(expected.getTransitionType(i), actual.getTransitionType(i));
+      }
+   }
+
+   public static void assertQuadrupedStepTransitionsListEqual(List<QuadrupedStepTransition> expected, List<QuadrupedStepTransition> actual, double epsilon)
+   {
+      assertEquals(expected.size(), actual.size());
+
+      for (int i = 0; i < expected.size(); i++)
+         assertQuadrupedStepTransitionsEqual(expected.get(i), actual.get(i), epsilon);
+   }
+
 
    public static void assertTimeIntervalsEqual(TimeInterval expected, TimeInterval actual, double epsilon)
    {
