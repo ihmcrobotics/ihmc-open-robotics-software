6e732bb94316d1046feff7dd4eb60c25d8db31f5
Robert <rgriffin@ihmc.us>
2018-12-28 09:37:16 -0600

    setup an xbox adapter node

---
+++ ihmc-quadruped-planning/src/main/java/us/ihmc/quadrupedPlanning/networkProcessing/xBox/QuadrupedXBoxController.java
@@ -0,0 +1,237 @@
+package us.ihmc.quadrupedPlanning.networkProcessing.xBox;
+
+import controller_msgs.msg.dds.*;
+import net.java.games.input.Event;
+import us.ihmc.commons.Conversions;
+import us.ihmc.commons.PrintTools;
+import us.ihmc.communication.controllerAPI.CommandInputManager;
+import us.ihmc.communication.controllerAPI.StatusMessageOutputManager;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
+import us.ihmc.quadrupedPlanning.QuadrupedXGaitSettingsReadOnly;
+import us.ihmc.quadrupedPlanning.input.InputValueIntegrator;
+import us.ihmc.quadrupedPlanning.networkProcessing.QuadrupedRobotModelProviderNode;
+import us.ihmc.quadrupedPlanning.networkProcessing.QuadrupedToolboxController;
+import us.ihmc.quadrupedPlanning.networkProcessing.bodyTeleop.QuadrupedBodyTeleopModule;
+import us.ihmc.quadrupedPlanning.networkProcessing.heightTeleop.QuadrupedBodyHeightTeleopModule;
+import us.ihmc.quadrupedPlanning.networkProcessing.stepTeleop.QuadrupedStepTeleopManager;
+import us.ihmc.quadrupedPlanning.networkProcessing.stepTeleop.QuadrupedStepTeleopModule;
+import us.ihmc.tools.inputDevices.joystick.Joystick;
+import us.ihmc.tools.inputDevices.joystick.JoystickCustomizationFilter;
+import us.ihmc.tools.inputDevices.joystick.JoystickEventListener;
+import us.ihmc.tools.inputDevices.joystick.JoystickModel;
+import us.ihmc.tools.inputDevices.joystick.mapping.XBoxOneMapping;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
+import us.ihmc.yoVariables.variable.YoBoolean;
+import us.ihmc.yoVariables.variable.YoDouble;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class QuadrupedXBoxController extends QuadrupedToolboxController implements JoystickEventListener
+{
+   private final AtomicReference<HighLevelStateChangeStatusMessage> controllerStateChangeMessage = new AtomicReference<>();
+   private final AtomicReference<QuadrupedSteppingStateChangeMessage> steppingStateChangeMessage = new AtomicReference<>();
+
+   private final AtomicBoolean receivedInput = new AtomicBoolean();
+
+   private final Map<XBoxOneMapping, Double> channels = Collections.synchronizedMap(new EnumMap<>(XBoxOneMapping.class));
+
+   private QuadrupedStepTeleopModule stepTeleopModule = null;
+   private QuadrupedBodyTeleopModule bodyTeleopModule = null;
+   private QuadrupedBodyHeightTeleopModule heightTeleopModule = null;
+
+   private final YoBoolean isPaused = new YoBoolean("xBoxIsPaused", registry);
+   private final YoDouble maxBodyYaw = new YoDouble("xBoxMaxBodyYaw", registry);
+   private final YoDouble maxBodyPitch = new YoDouble("xBoxMaxBodyPitch", registry);
+   private final YoDouble maxBodyHeightVelocity = new YoDouble("xBoxMaxBodyHeightVelocity", registry);
+   private final YoDouble maxVelocityX = new YoDouble("xBoxMaxVelocityX", registry);
+   private final YoDouble maxVelocityY = new YoDouble("xBoxMaxVelocityY", registry);
+   private final YoDouble maxVelocityYaw = new YoDouble("xBoxMaxVelocityYaw", registry);
+   private final YoDouble bodyOrientationShiftTime = new YoDouble("xBoxBodyOrientationShiftTime", registry);
+
+   private InputValueIntegrator bodyHeight;
+   private double endPhaseShift;
+
+   public QuadrupedXBoxController(QuadrupedXGaitSettingsReadOnly defaultXGaitSettings, double nominalBodyHeight, CommandInputManager commandInputManager,
+                                  StatusMessageOutputManager statusOutputManager, YoVariableRegistry parentRegistry, int updateTimeInMs) throws IOException
+   {
+      super(statusOutputManager, parentRegistry);
+
+
+      boolean joystickIsConnected = Joystick.isAJoystickConnectedToSystem();
+      if (!joystickIsConnected)
+      {
+         throw new RuntimeException("No joystick detected, can't run without it.");
+      }
+      Joystick joystick = new Joystick(JoystickModel.XBOX_ONE, 0);
+      configureJoystickFilters(joystick);
+      joystick.addJoystickEventListener(this);
+      joystick.setPollInterval(10);
+
+      maxBodyYaw.set(0.15);
+      maxBodyPitch.set(0.15);
+      maxBodyHeightVelocity.set(0.1);
+      maxVelocityX.set(0.5);
+      maxVelocityY.set(0.25);
+      maxVelocityYaw.set(0.4);
+      bodyOrientationShiftTime.set(0.1);
+      this.bodyHeight = new InputValueIntegrator(Conversions.millisecondsToSeconds(updateTimeInMs), nominalBodyHeight);
+      this.endPhaseShift = defaultXGaitSettings.getEndPhaseShift();
+
+      // Initialize all channels to zero.
+      for (XBoxOneMapping channel : XBoxOneMapping.values)
+      {
+         channels.put(channel, 0.0);
+      }
+
+      commandInputManager.registerHasReceivedInputListener(command -> receivedInput.set(true));
+   }
+
+   public void processHighLevelStateChangeMessage(HighLevelStateChangeStatusMessage message)
+   {
+      controllerStateChangeMessage.set(message);
+   }
+
+   public void processSteppingStateChangeMessage(QuadrupedSteppingStateChangeMessage message)
+   {
+      steppingStateChangeMessage.set(message);
+   }
+
+   public void setPaused(boolean paused)
+   {
+      isPaused.set(paused);
+   }
+
+   public void setStepTeleopModule(QuadrupedStepTeleopModule stepTeleopModule)
+   {
+      this.stepTeleopModule = stepTeleopModule;
+   }
+
+   public void setBodyTeleopModule(QuadrupedBodyTeleopModule bodyTeleopModule)
+   {
+      this.bodyTeleopModule = bodyTeleopModule;
+   }
+
+   public void setHeightTeleopModule(QuadrupedBodyHeightTeleopModule heightTeleopModule)
+   {
+      this.heightTeleopModule = heightTeleopModule;
+   }
+
+   @Override
+   public boolean initialize()
+   {
+      return true;
+   }
+
+   @Override
+   public void updateInternal()
+   {
+      if (heightTeleopModule != null)
+         processJoystickHeightCommands();
+      if (bodyTeleopModule != null)
+         processJoystickBodyCommands();
+      if (stepTeleopModule != null)
+         processJoystickStepCommands();
+
+//      reportMessage(teleopManager.getStepListMessage());
+//      reportMessage(teleopManager.getBodyOrientationMessage());
+   }
+
+   @Override
+   public boolean isDone()
+   {
+      return controllerStateChangeMessage.get().getEndHighLevelControllerName() != HighLevelStateChangeStatusMessage.WALKING;
+   }
+
+   private void configureJoystickFilters(Joystick device)
+   {
+      device.setCustomizationFilter(new JoystickCustomizationFilter(XBoxOneMapping.LEFT_TRIGGER, false, 0.05, 1, 1.0));
+      device.setCustomizationFilter(new JoystickCustomizationFilter(XBoxOneMapping.RIGHT_TRIGGER, false, 0.05, 1, 1.0));
+      device.setCustomizationFilter(new JoystickCustomizationFilter(XBoxOneMapping.LEFT_STICK_X, true, 0.1, 1));
+      device.setCustomizationFilter(new JoystickCustomizationFilter(XBoxOneMapping.LEFT_STICK_Y, true, 0.1, 1));
+      device.setCustomizationFilter(new JoystickCustomizationFilter(XBoxOneMapping.RIGHT_STICK_X, true, 0.1, 1));
+      device.setCustomizationFilter(new JoystickCustomizationFilter(XBoxOneMapping.RIGHT_STICK_Y, true, 0.1, 1));
+   }
+
+   private void processJoystickHeightCommands()
+   {
+      if (channels.get(XBoxOneMapping.DPAD) == 0.25)
+      {
+         double bodyHeightVelocity = maxBodyHeightVelocity.getValue();
+         bodyHeight.update(bodyHeightVelocity);
+      }
+      else if (channels.get(XBoxOneMapping.DPAD) == 0.75)
+      {
+         double bodyHeightVelocity = -maxBodyHeightVelocity.getValue();
+         bodyHeight.update(bodyHeightVelocity);
+      }
+      heightTeleopModule.setDesiredBodyHeight(bodyHeight.value());
+   }
+
+   private void processJoystickBodyCommands()
+   {
+      double bodyRoll = 0.0;
+      double bodyPitch = channels.get(XBoxOneMapping.RIGHT_STICK_Y) * maxBodyPitch.getValue();
+      double bodyYaw = channels.get(XBoxOneMapping.RIGHT_STICK_X) * maxBodyYaw.getValue();
+      bodyTeleopModule.setDesiredBodyPose(0.0, 0.0, bodyYaw, bodyPitch, bodyRoll, bodyOrientationShiftTime.getValue());
+   }
+
+   private void processJoystickStepCommands()
+   {
+      double xVelocity = channels.get(XBoxOneMapping.LEFT_STICK_Y) * maxVelocityX.getDoubleValue();
+      double yVelocity = channels.get(XBoxOneMapping.LEFT_STICK_X) * maxVelocityY.getDoubleValue();
+      double yawRate = channels.get(XBoxOneMapping.RIGHT_STICK_X) * maxVelocityYaw.getDoubleValue();
+      stepTeleopModule.setDesiredVelocity(xVelocity, yVelocity, yawRate);
+   }
+
+   private void processStateChangeRequests(Event event)
+   {
+      if (event.getValue() < 0.5)
+         return;
+
+      XBoxOneMapping mapping = XBoxOneMapping.getMapping(event);
+
+      //      if (mapping == XBoxOneMapping.START)
+      //      {
+      //         stepTeleopManager.requestStandPrep();
+      //      }
+      //      else if (mapping == XBoxOneMapping.A)
+      //      {
+      //         stepTeleopManager.requestWalkingState();
+      //         if (stepTeleopManager.isWalking())
+      //            stepTeleopManager.requestStanding();
+      //      }
+      //      else if (mapping == XBoxOneMapping.X)
+      //      {
+      //         stepTeleopManager.requestXGait();
+      //      }
+      if (mapping == XBoxOneMapping.LEFT_BUMPER && channels.get(mapping) < 0.5 && stepTeleopModule != null) // the bumpers were firing twice for one click
+      {
+         endPhaseShift -= 90.0;
+         stepTeleopModule.setEndPhaseShift(endPhaseShift);
+      }
+      else if (mapping == XBoxOneMapping.RIGHT_BUMPER && channels.get(mapping) < 0.5 && stepTeleopModule != null)
+      {
+         endPhaseShift += 90.0;
+         stepTeleopModule.setEndPhaseShift(endPhaseShift);
+      }
+      //      else if(mapping == XBoxOneMapping.XBOX_BUTTON && channels.get(mapping) < 0.5)
+      //      {
+      //         stepTeleopManager.setPaused(!stepTeleopManager.isPaused());
+      //      }
+   }
+
+   @Override
+   public void processEvent(Event event)
+   {
+      // Handle events that should trigger once immediately after the event is triggered.
+      processStateChangeRequests(event);
+
+      // Store updated value in a cache so historical values for all channels can be used.
+      channels.put(XBoxOneMapping.getMapping(event), (double) event.getValue());
+   }
+}
---
+++ ihmc-quadruped-planning/src/main/java/us/ihmc/quadrupedPlanning/networkProcessing/xBox/QuadrupedXBoxModule.java
@@ -0,0 +1,110 @@
+package us.ihmc.quadrupedPlanning.networkProcessing.xBox;
+
+import controller_msgs.msg.dds.*;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.communication.controllerAPI.command.Command;
+import us.ihmc.euclid.interfaces.Settable;
+import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
+import us.ihmc.pubsub.DomainFactory;
+import us.ihmc.quadrupedPlanning.QuadrupedXGaitSettings;
+import us.ihmc.quadrupedPlanning.networkProcessing.QuadrupedRobotModelProviderNode;
+import us.ihmc.quadrupedPlanning.networkProcessing.QuadrupedToolboxController;
+import us.ihmc.quadrupedPlanning.networkProcessing.QuadrupedToolboxModule;
+import us.ihmc.quadrupedPlanning.networkProcessing.bodyTeleop.QuadrupedBodyTeleopModule;
+import us.ihmc.quadrupedPlanning.networkProcessing.heightTeleop.QuadrupedBodyHeightTeleopModule;
+import us.ihmc.quadrupedPlanning.networkProcessing.stepTeleop.QuadrupedStepTeleopController;
+import us.ihmc.quadrupedPlanning.networkProcessing.stepTeleop.QuadrupedStepTeleopModule;
+import us.ihmc.robotModels.FullQuadrupedRobotModelFactory;
+import us.ihmc.ros2.RealtimeRos2Node;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static us.ihmc.communication.ROS2Tools.getTopicNameGenerator;
+
+public class QuadrupedXBoxModule extends QuadrupedToolboxModule
+{
+   private static final int updatePeriodMilliseconds = 1;
+
+   private final QuadrupedXBoxController xBoxController;
+
+   public QuadrupedXBoxModule(FullQuadrupedRobotModelFactory modelFactory, QuadrupedXGaitSettings defaultXGaitSettings, double nominalBodyHeight,
+                              LogModelProvider modelProvider, boolean startYoVariableServer, DomainFactory.PubSubImplementation pubSubImplementation)
+         throws IOException
+   {
+      super(modelFactory.getRobotDescription().getName(), modelFactory.createFullRobotModel(), modelProvider, startYoVariableServer, updatePeriodMilliseconds,
+            pubSubImplementation);
+
+      xBoxController = new QuadrupedXBoxController(defaultXGaitSettings, nominalBodyHeight, commandInputManager, statusOutputManager, registry,
+                                                   updatePeriodMilliseconds);
+   }
+
+   @Override
+   public void registerExtraPuSubs(RealtimeRos2Node realtimeRos2Node)
+   {
+      ROS2Tools
+            .createCallbackSubscription(realtimeRos2Node, HighLevelStateMessage.class, getPublisherTopicNameGenerator(), s -> xBoxController.setPaused(true));
+      ROS2Tools.createCallbackSubscription(realtimeRos2Node, HighLevelStateChangeStatusMessage.class, getPublisherTopicNameGenerator(),
+                                           s -> xBoxController.processHighLevelStateChangeMessage(s.takeNextData()));
+      ROS2Tools.createCallbackSubscription(realtimeRos2Node, QuadrupedSteppingStateChangeMessage.class, getPublisherTopicNameGenerator(),
+                                           s -> xBoxController.processSteppingStateChangeMessage(s.takeNextData()));
+   }
+
+   @Override
+   public QuadrupedToolboxController getToolboxController()
+   {
+      return xBoxController;
+   }
+
+   @Override
+   public List<Class<? extends Command<?, ?>>> createListOfSupportedCommands()
+   {
+      return null;
+   }
+
+   @Override
+   public List<Class<? extends Settable<?>>> createListOfSupportedStatus()
+   {
+      List<Class<? extends Settable<?>>> statusMessages = new ArrayList<>();
+      statusMessages.add(HighLevelStateMessage.class);
+      statusMessages.add(HighLevelStateChangeStatusMessage.class);
+      statusMessages.add(QuadrupedSteppingStateChangeMessage.class);
+
+      return statusMessages;
+   }
+
+   @Override
+   public ROS2Tools.MessageTopicNameGenerator getPublisherTopicNameGenerator()
+   {
+      return getTopicNameGenerator(robotName, ROS2Tools.STEP_TELEOP_TOOLBOX, ROS2Tools.ROS2TopicQualifier.OUTPUT);
+   }
+
+   @Override
+   public ROS2Tools.MessageTopicNameGenerator getSubscriberTopicNameGenerator()
+   {
+      return getTopicNameGenerator(robotName, ROS2Tools.STEP_TELEOP_TOOLBOX, ROS2Tools.ROS2TopicQualifier.INPUT);
+   }
+
+   @Override
+   public void sleep()
+   {
+      super.sleep();
+   }
+
+   public void setStepTeleopModule(QuadrupedStepTeleopModule stepTeleopModule)
+   {
+      xBoxController.setStepTeleopModule(stepTeleopModule);
+   }
+
+   public void setBodyTeleopModule(QuadrupedBodyTeleopModule bodyTeleopModule)
+   {
+      xBoxController.setBodyTeleopModule(bodyTeleopModule);
+   }
+
+   public void setHeightTeleopModule(QuadrupedBodyHeightTeleopModule heightTeleopModule)
+   {
+      xBoxController.setHeightTeleopModule(heightTeleopModule);
+   }
+
+}
