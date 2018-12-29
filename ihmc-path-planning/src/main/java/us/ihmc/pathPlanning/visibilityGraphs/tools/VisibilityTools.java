4d07633b52c1f15243737c37bca64ef83b3244a8
Robert <rgriffin@ihmc.us>
2018-12-28 21:56:23 -0600

    added a method to compute the distance of an edge to a cluster

--- ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/tools/VisibilityTools.java
+++ ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/tools/VisibilityTools.java
@@ -4,11 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import us.ihmc.euclid.geometry.BoundingBox2D;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryPolygonTools;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.tuple2D.interfaces.Point2DBasics;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DBasics;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster;
 import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster.ExtrusionSide;
 import us.ihmc.robotics.geometry.PlanarRegion;
+
+import static us.ihmc.euclid.geometry.tools.EuclidGeometryTools.*;
+import static us.ihmc.euclid.geometry.tools.EuclidGeometryTools.distanceSquaredFromPoint2DToLineSegment2D;
+import static us.ihmc.euclid.tools.EuclidCoreTools.normSquared;
 
 public class VisibilityTools
 {
@@ -36,6 +44,220 @@ public class VisibilityTools
          }
       }
       return true;
+   }
+
+   public static double distanceToCluster(Point2DReadOnly firstPoint, Point2DReadOnly secondPoint, List<? extends Point2DReadOnly> listOfPointsInCluster,
+                                          boolean closed)
+   {
+      int numberOfVertices = listOfPointsInCluster.size();
+
+      if (numberOfVertices == 0)
+         return Double.NaN;
+
+      if (numberOfVertices == 1)
+         return distanceFromPoint2DToLineSegment2D(listOfPointsInCluster.get(0), firstPoint, secondPoint);
+
+      if (numberOfVertices == 2)
+         return distanceBetweenTwoLineSegment2Ds(firstPoint, secondPoint, listOfPointsInCluster.get(0), listOfPointsInCluster.get(1));
+
+      boolean pointIsVisibile = isPointVisible(firstPoint, secondPoint, listOfPointsInCluster, closed);
+
+      double minDistance = Double.POSITIVE_INFINITY;
+
+      for (int index = 0; index < numberOfVertices; index++)
+      {
+         Point2DReadOnly edgeStart = listOfPointsInCluster.get(index);
+         Point2DReadOnly edgeEnd = listOfPointsInCluster.get(next(index, numberOfVertices));
+
+         minDistance = Math.min(minDistance, distanceBetweenTwoLineSegment2Ds(firstPoint, secondPoint, edgeStart, edgeEnd));
+      }
+
+      minDistance = Math.sqrt(minDistance);
+
+      if (!pointIsVisibile)
+         minDistance = -minDistance;
+      return minDistance;
+   }
+
+   /**
+    * This methods computes the minimum distance between the two 2D line segments with finite length.
+    * <a href="http://geomalgorithms.com/a07-_distance.html"> Useful link</a>.
+    *
+    * @param lineSegmentStart1 the first endpoint of the first line segment. Not modified.
+    * @param lineSegmentEnd1 the second endpoint of the first line segment. Not modified.
+    * @param lineSegmentStart2 the first endpoint of the second line segment. Not modified.
+    * @param lineSegmentEnd2 the second endpoint of the second line segment. Not modified.
+    * @return the minimum distance between the two line segments.
+    */
+   public static double distanceBetweenTwoLineSegment2Ds(Point2DReadOnly lineSegmentStart1, Point2DReadOnly lineSegmentEnd1, Point2DReadOnly lineSegmentStart2,
+                                                         Point2DReadOnly lineSegmentEnd2)
+   {
+      return closestPoint2DsBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentStart2, lineSegmentEnd2, null, null);
+   }
+
+
+   /**
+    * Given two 2D line segments with finite length, this methods computes two points P &in;
+    * lineSegment1 and Q &in; lineSegment2 such that the distance || P - Q || is the minimum distance
+    * between the two 2D line segments. <a href="http://geomalgorithms.com/a07-_distance.html"> Useful
+    * link</a>.
+    *
+    * @param lineSegmentStart1 the first endpoint of the first line segment. Not modified.
+    * @param lineSegmentEnd1 the second endpoint of the first line segment. Not modified.
+    * @param lineSegmentStart2 the first endpoint of the second line segment. Not modified.
+    * @param lineSegmentEnd2 the second endpoint of the second line segment. Not modified.
+    * @param closestPointOnLineSegment1ToPack the 2D coordinates of the point P are packed in this 2D
+    *           point. Modified. Can be {@code null}.
+    * @param closestPointOnLineSegment2ToPack the 2D coordinates of the point Q are packed in this 2D
+    *           point. Modified. Can be {@code null}.
+    * @return the minimum distance between the two line segments.
+    */
+   public static double closestPoint2DsBetweenTwoLineSegment2Ds(Point2DReadOnly lineSegmentStart1, Point2DReadOnly lineSegmentEnd1,
+                                                                Point2DReadOnly lineSegmentStart2, Point2DReadOnly lineSegmentEnd2,
+                                                                Point2DBasics closestPointOnLineSegment1ToPack, Point2DBasics closestPointOnLineSegment2ToPack)
+   {
+      // Switching to the notation used in http://geomalgorithms.com/a07-_distance.html.
+      // The line1 is defined by (P0, u) and the line2 by (Q0, v).
+      Point2DReadOnly P0 = lineSegmentStart1;
+      double ux = lineSegmentEnd1.getX() - lineSegmentStart1.getX();
+      double uy = lineSegmentEnd1.getY() - lineSegmentStart1.getY();
+      Point2DReadOnly Q0 = lineSegmentStart2;
+      double vx = lineSegmentEnd2.getX() - lineSegmentStart2.getX();
+      double vy = lineSegmentEnd2.getY() - lineSegmentStart2.getY();
+
+      Point2DBasics Psc = closestPointOnLineSegment1ToPack;
+      Point2DBasics Qtc = closestPointOnLineSegment2ToPack;
+
+      double w0X = P0.getX() - Q0.getX();
+      double w0Y = P0.getY() - Q0.getY();
+
+      double a = ux * ux + uy * uy;
+      double b = ux * vx + uy * vy;
+      double c = vx * vx + vy * vy;
+      double d = ux * w0X + uy * w0Y;
+      double e = vx * w0X + vy * w0Y;
+
+      double delta = a * c - b * b;
+
+      double sc, sNumerator, sDenominator = delta;
+      double tc, tNumerator, tDenominator = delta;
+
+      // check to see if the lines are parallel
+      if (delta <= ONE_MILLIONTH)
+      {
+         /*
+          * The lines are parallel, there's an infinite number of pairs, but for one chosen point on one of
+          * the lines, there's only one closest point to it on the other line. So let's choose arbitrarily a
+          * point on the lineSegment1 and calculate the point that is closest to it on the lineSegment2.
+          */
+         sNumerator = 0.0;
+         sDenominator = 1.0;
+         tNumerator = e;
+         tDenominator = c;
+      }
+      else
+      {
+         sNumerator = b * e - c * d;
+         tNumerator = a * e - b * d;
+
+         if (sNumerator < 0.0)
+         {
+            sNumerator = 0.0;
+            tNumerator = e;
+            tDenominator = c;
+         }
+         else if (sNumerator > sDenominator)
+         {
+            sNumerator = sDenominator;
+            tNumerator = e + b;
+            tDenominator = c;
+         }
+      }
+
+      if (tNumerator < 0.0)
+      {
+         tNumerator = 0.0;
+         sNumerator = -d;
+         if (sNumerator < 0.0)
+            sNumerator = 0.0;
+         else if (sNumerator > a)
+            sNumerator = a;
+         sDenominator = a;
+      }
+      else if (tNumerator > tDenominator)
+      {
+         tNumerator = tDenominator;
+         sNumerator = -d + b;
+         if (sNumerator < 0.0)
+            sNumerator = 0.0;
+         else if (sNumerator > a)
+            sNumerator = a;
+         sDenominator = a;
+      }
+
+      sc = Math.abs(sNumerator) < ONE_MILLIONTH ? 0.0 : sNumerator / sDenominator;
+      tc = Math.abs(tNumerator) < ONE_MILLIONTH ? 0.0 : tNumerator / tDenominator;
+
+      double PscX = sc * ux + P0.getX();
+      double PscY = sc * uy + P0.getY();
+
+      double QtcX = tc * vx + Q0.getX();
+      double QtcY = tc * vy + Q0.getY();
+
+      if (Psc != null)
+         Psc.set(PscX, PscY);
+      if (Qtc != null)
+         Qtc.set(QtcX, QtcY);
+
+      double dx = PscX - QtcX;
+      double dy = PscY - QtcY;
+      return Math.sqrt(normSquared(dx, dy));
+   }
+
+   /**
+    * Increments then recomputes the given {@code index} such that it is &in; [0, {@code listSize}[.
+    * Examples:
+    * <ul>
+    * <li>{@code next(-1, 10)} returns 0.
+    * <li>{@code next(10, 10)} returns 1.
+    * <li>{@code next( 5, 10)} returns 6.
+    * <li>{@code next(15, 10)} returns 6.
+    * </ul>
+    * </p>
+    *
+    * @param index the index to be incremented and wrapped if necessary.
+    * @param listSize the size of the list around which the index is to be wrapped.
+    * @return the wrapped incremented index.
+    */
+   public static int next(int index, int listSize)
+   {
+      return wrap(index + 1, listSize);
+   }
+
+   /**
+    * Recomputes the given {@code index} such that it is &in; [0, {@code listSize}[.
+    * <p>
+    * The {@code index} remains unchanged if already &in; [0, {@code listSize}[.
+    * <p>
+    * Examples:
+    * <ul>
+    * <li>{@code wrap(-1, 10)} returns 9.
+    * <li>{@code wrap(10, 10)} returns 0.
+    * <li>{@code wrap( 5, 10)} returns 5.
+    * <li>{@code wrap(15, 10)} returns 5.
+    * </ul>
+    * </p>
+    *
+    * @param index the index to be wrapped if necessary.
+    * @param listSize the size of the list around which the index is to be wrapped.
+    * @return the wrapped index.
+    */
+   public static int wrap(int index, int listSize)
+   {
+      index %= listSize;
+      if (index < 0)
+         index += listSize;
+      return index;
    }
 
    private static boolean isNotInsideANonNavigableZone(Point2DReadOnly query, List<Cluster> clusters)
