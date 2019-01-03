6d18c4b0e6de672a9781fa2a6db28e3169415d8f
Stephen McCrory <smccrory@ihmc.us>
2019-01-03 14:04:13 -0600

    adding message to request planner parameters

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/communication/FootstepPlannerCommunicationProperties.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/communication/FootstepPlannerCommunicationProperties.java
@@ -1,11 +1,15 @@
 package us.ihmc.footstepPlanning.communication;
 
 import controller_msgs.msg.dds.*;
+import us.ihmc.commons.thread.ThreadTools;
+import us.ihmc.communication.IHMCRealtimeROS2Publisher;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.ROS2Tools.ROS2TopicQualifier;
 import us.ihmc.communication.ROS2Tools.MessageTopicNameGenerator;
 import us.ihmc.communication.controllerAPI.command.Command;
 import us.ihmc.euclid.interfaces.Settable;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
+import us.ihmc.ros2.RealtimeRos2Node;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -53,5 +57,18 @@ public class FootstepPlannerCommunicationProperties
    public static MessageTopicNameGenerator subscriberTopicNameGenerator(String robotName)
    {
       return getTopicNameGenerator(robotName, ROS2Tools.FOOTSTEP_PLANNER_TOOLBOX, ROS2TopicQualifier.INPUT);
+   }
+
+   public static void main(String[] args)
+   {
+      RealtimeRos2Node testNode = ROS2Tools.createRealtimeRos2Node(PubSubImplementation.INTRAPROCESS, "testNode");
+      IHMCRealtimeROS2Publisher<FootstepPlannerParametersPacket> n1 = ROS2Tools.createPublisher(testNode, FootstepPlannerParametersPacket.class, "n1");
+      ROS2Tools.createCallbackSubscription(testNode, FootstepPlannerParametersPacket.class, "n1", s -> System.out.println("received packet"));
+
+      while(true)
+      {
+         n1.publish(new FootstepPlannerParametersPacket());
+         ThreadTools.sleep(1000);
+      }
    }
 }
