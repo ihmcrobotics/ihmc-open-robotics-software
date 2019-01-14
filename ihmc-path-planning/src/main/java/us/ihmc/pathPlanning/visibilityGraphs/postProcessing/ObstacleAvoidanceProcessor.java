fdad9e562062ad3205591bbecefec38726646e62
Robert <rgriffin@ihmc.us>
2019-01-14 10:28:05 -0600

    added a cliff avoidance check

--- ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/postProcessing/ObstacleAvoidanceProcessor.java
+++ ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/postProcessing/ObstacleAvoidanceProcessor.java
@@ -1,19 +1,22 @@
 package us.ihmc.pathPlanning.visibilityGraphs.postProcessing;
 
-import org.apache.logging.log4j.util.PropertySource;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
+import us.ihmc.euclid.tuple2D.interfaces.Vector2DReadOnly;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.pathPlanning.visibilityGraphs.NavigableRegions;
 import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster;
 import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.NavigableRegion;
 import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.VisibilityGraphNode;
 import us.ihmc.pathPlanning.visibilityGraphs.dataStructure.VisibilityMapSolution;
 import us.ihmc.pathPlanning.visibilityGraphs.interfaces.VisibilityGraphsParameters;
+import us.ihmc.pathPlanning.visibilityGraphs.tools.PlanarRegionTools;
 import us.ihmc.pathPlanning.visibilityGraphs.tools.VisibilityTools;
-import us.ihmc.robotics.time.TimeIntervalProvider;
+import us.ihmc.robotics.geometry.PlanarRegion;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -24,6 +27,7 @@ import java.util.stream.Collectors;
 public class ObstacleAvoidanceProcessor
 {
    private static final double minDistanceToMove = 0.01;
+   private static final double cliffHeightToAvoid = 0.10;
    private final double realDistanceFromObstacle;
    private final IntermediateComparator comparator = new IntermediateComparator();
 
@@ -36,21 +40,21 @@ public class ObstacleAvoidanceProcessor
    {
       List<Point3D> newPath = nodePath.parallelStream().map(node -> new Point3D(node.getPointInWorld())).collect(Collectors.toList());
 
-      int nodeIndex = 0;
+      int pathNodeIndex = 0;
       // don't do the goal node
-      while (nodeIndex < nodePath.size() - 2)
+      while (pathNodeIndex < nodePath.size() - 2)
       {
-         int nextNodeIndex = nodeIndex + 1;
-         Point2D originPointInWorld2D = new Point2D(newPath.get(nodeIndex));
-         Point2D nextPointInWorld2D = new Point2D(newPath.get(nextNodeIndex));
+         int nextPathNodeIndex = pathNodeIndex + 1;
+         Point2D originPointInWorld2D = new Point2D(newPath.get(pathNodeIndex));
+         Point2D nextPointInWorld2D = new Point2D(newPath.get(nextPathNodeIndex));
 
-         boolean isGoalNode = nodeIndex > nodePath.size() - 2;
+         boolean isGoalNode = pathNodeIndex > nodePath.size() - 2;
 
          if (!isGoalNode)
          {
-            adjustGoalNodePositionToAvoidObstacles(newPath.get(nextNodeIndex), nodePath.get(nextNodeIndex));
+            adjustGoalNodePositionToAvoidObstacles(newPath.get(nextPathNodeIndex), nodePath.get(nextPathNodeIndex), visibilityMapSolution.getNavigableRegions());
             // run it again, in case moving it moved it to be within the range of another one.
-            adjustGoalNodePositionToAvoidObstacles(newPath.get(nextNodeIndex), nodePath.get(nextNodeIndex));
+            adjustGoalNodePositionToAvoidObstacles(newPath.get(nextPathNodeIndex), nodePath.get(nextPathNodeIndex), visibilityMapSolution.getNavigableRegions());
          }
 
          // add points
@@ -98,14 +102,36 @@ public class ObstacleAvoidanceProcessor
             intermediateWaypointsToAdd.sort(comparator);
 
             if (!nodeWasAdded)
-               nodeIndex++;
+               pathNodeIndex++;
          }
       }
 
       return newPath.parallelStream().map(Point3D::new).collect(Collectors.toList());
    }
 
-   private void adjustGoalNodePositionToAvoidObstacles(Point3D nodeLocationToPack, VisibilityGraphNode node)
+   private void adjustGoalNodePositionToAvoidObstacles(Point3D nodeLocationToPack, VisibilityGraphNode node, NavigableRegions navigableRegions)
+   {
+      Point2D nextPointInWorld2D = new Point2D(node.getPointInWorld());
+
+      NavigableRegion navigableRegion = node.getVisibilityGraphNavigableRegion().getNavigableRegion();
+
+      Vector2DReadOnly nodeShiftToAvoidObstacles = getDirectionAndDistanceToShiftNodeToAvoidObstacles(node);
+
+      if (nodeShiftToAvoidObstacles.length() > minDistanceToMove)
+      {
+         nextPointInWorld2D.add(nodeShiftToAvoidObstacles);
+
+         Vector2DReadOnly extraShiftToAvoidCliffs = getDirectionAndDistanceToShiftToAvoidCliffs(nextPointInWorld2D, node, navigableRegions, 0.1);
+
+         if (extraShiftToAvoidCliffs.length() > minDistanceToMove)
+            nextPointInWorld2D.add(extraShiftToAvoidCliffs);
+
+         double newHeight = navigableRegion.getPlaneZGivenXY(nextPointInWorld2D.getX(), nextPointInWorld2D.getY());
+         nodeLocationToPack.set(nextPointInWorld2D, newHeight);
+      }
+   }
+
+   private Vector2DReadOnly getDirectionAndDistanceToShiftNodeToAvoidObstacles(VisibilityGraphNode node)
    {
       Point2D nextPointInWorld2D = new Point2D(node.getPointInWorld());
       Vector2D nodeShift = new Vector2D();
@@ -118,7 +144,6 @@ public class ObstacleAvoidanceProcessor
          List<Point2DReadOnly> clusterPolygon = cluster.getNonNavigableExtrusionsInWorld2D();
 
          Point2D closestPointInCluster = new Point2D();
-
          double distanceToCluster = VisibilityTools.distanceToCluster(nextPointInWorld2D, clusterPolygon, closestPointInCluster, null);
          if (distanceToCluster < 1.1 * realDistanceFromObstacle)
          {
@@ -134,13 +159,55 @@ public class ObstacleAvoidanceProcessor
          }
       }
 
-      if (nodeShift.length() > minDistanceToMove)
+      nodeShift.scale(1.0 / numberOfShifts);
+
+      return nodeShift;
+   }
+
+   private static Vector2DReadOnly getDirectionAndDistanceToShiftToAvoidCliffs(Point2DReadOnly pointToCheck, VisibilityGraphNode originalNode,
+                                                                               NavigableRegions navigableRegions, double distanceInsideHomeRegionToAvoid)
+   {
+      double originalHeight = originalNode.getPointInWorld().getZ();
+      double adjustedHeight = getMaxHeightOfPointInWorld(pointToCheck, navigableRegions);
+
+      Vector2D nodeShiftToAvoidCliff = new Vector2D();
+
+      if (Math.abs(adjustedHeight - originalHeight) > cliffHeightToAvoid)
       {
-         nodeShift.scale(1.0 / numberOfShifts);
-         nextPointInWorld2D.add(nodeShift);
-         double newHeight = navigableRegion.getPlaneZGivenXY(nextPointInWorld2D.getX(), nextPointInWorld2D.getY());
-         nodeLocationToPack.set(nextPointInWorld2D, newHeight);
+         Cluster cluster = originalNode.getVisibilityGraphNavigableRegion().getNavigableRegion().getHomeRegionCluster();
+         List<Point2DReadOnly> clusterPolygon = cluster.getNonNavigableExtrusionsInWorld2D();
+
+         Point2D closestPointInCluster = new Point2D();
+         double distanceToCluster = VisibilityTools.distanceToCluster(pointToCheck, clusterPolygon, closestPointInCluster, null);
+
+         double distanceToMove = distanceToCluster + distanceInsideHomeRegionToAvoid;
+         nodeShiftToAvoidCliff.sub(closestPointInCluster, pointToCheck);
+         nodeShiftToAvoidCliff.normalize();
+         nodeShiftToAvoidCliff.scale(distanceToMove);
       }
+
+      return nodeShiftToAvoidCliff;
+   }
+
+   private static double getMaxHeightOfPointInWorld(Point2DReadOnly pointInWorldToCheck, NavigableRegions navigableRegions)
+   {
+      double maxHeight = Double.MIN_VALUE;
+      for (NavigableRegion navigableRegion : navigableRegions.getNaviableRegionsList())
+      {
+         PlanarRegion planarRegion = navigableRegion.getHomePlanarRegion();
+
+         RigidBodyTransform transformToWorld = new RigidBodyTransform();
+         planarRegion.getTransformToWorld(transformToWorld);
+         Point2D pointInLocalToCheck = new Point2D(pointInWorldToCheck);
+         pointInLocalToCheck.applyInverseTransform(transformToWorld, false);
+         double height = planarRegion.getPlaneZGivenXY(pointInWorldToCheck.getX(), pointInWorldToCheck.getY());
+         if (PlanarRegionTools.isPointInLocalInsidePlanarRegion(navigableRegion.getHomePlanarRegion(), pointInLocalToCheck) && height > maxHeight)
+         {
+            maxHeight = height;
+         }
+      }
+
+      return maxHeight;
    }
 
    private class IntermediateComparator implements Comparator<Point2DReadOnly>
