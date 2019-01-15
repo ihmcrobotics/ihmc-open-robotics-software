84342e922c51e6b192e33d2b5013c757c437e9e4
Sylvain Bertrand <sbertrand@ihmc.us>
2019-01-15 14:08:59 -0600

    Generated the messages.

---
+++ ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/WalkingControllerPreviewInputPubSubType.java
@@ -0,0 +1,142 @@
+package controller_msgs.msg.dds;
+
+/**
+* 
+* Topic data type of the struct "WalkingControllerPreviewInput" defined in "WalkingControllerPreviewInput_.idl". Use this class to provide the TopicDataType to a Participant. 
+*
+* This file was automatically generated from WalkingControllerPreviewInput_.idl by us.ihmc.idl.generator.IDLGenerator. 
+* Do not update this file directly, edit WalkingControllerPreviewInput_.idl instead.
+*
+*/
+public class WalkingControllerPreviewInputPubSubType implements us.ihmc.pubsub.TopicDataType<controller_msgs.msg.dds.WalkingControllerPreviewInput>
+{
+   public static final java.lang.String name = "controller_msgs::msg::dds_::WalkingControllerPreviewInput_";
+
+   private final us.ihmc.idl.CDR serializeCDR = new us.ihmc.idl.CDR();
+   private final us.ihmc.idl.CDR deserializeCDR = new us.ihmc.idl.CDR();
+
+   @Override
+   public void serialize(controller_msgs.msg.dds.WalkingControllerPreviewInput data, us.ihmc.pubsub.common.SerializedPayload serializedPayload) throws java.io.IOException
+   {
+      serializeCDR.serialize(serializedPayload);
+      write(data, serializeCDR);
+      serializeCDR.finishSerialize();
+   }
+
+   @Override
+   public void deserialize(us.ihmc.pubsub.common.SerializedPayload serializedPayload, controller_msgs.msg.dds.WalkingControllerPreviewInput data) throws java.io.IOException
+   {
+      deserializeCDR.deserialize(serializedPayload);
+      read(data, deserializeCDR);
+      deserializeCDR.finishDeserialize();
+   }
+
+   public static int getMaxCdrSerializedSize()
+   {
+      return getMaxCdrSerializedSize(0);
+   }
+
+   public static int getMaxCdrSerializedSize(int current_alignment)
+   {
+      int initial_alignment = current_alignment;
+
+      current_alignment += 4 + us.ihmc.idl.CDR.alignment(current_alignment, 4);
+
+      current_alignment += controller_msgs.msg.dds.FootstepDataListMessagePubSubType.getMaxCdrSerializedSize(current_alignment);
+
+
+      return current_alignment - initial_alignment;
+   }
+
+   public final static int getCdrSerializedSize(controller_msgs.msg.dds.WalkingControllerPreviewInput data)
+   {
+      return getCdrSerializedSize(data, 0);
+   }
+
+   public final static int getCdrSerializedSize(controller_msgs.msg.dds.WalkingControllerPreviewInput data, int current_alignment)
+   {
+      int initial_alignment = current_alignment;
+
+      current_alignment += 4 + us.ihmc.idl.CDR.alignment(current_alignment, 4);
+
+
+      current_alignment += controller_msgs.msg.dds.FootstepDataListMessagePubSubType.getCdrSerializedSize(data.getFootsteps(), current_alignment);
+
+
+      return current_alignment - initial_alignment;
+   }
+
+   public static void write(controller_msgs.msg.dds.WalkingControllerPreviewInput data, us.ihmc.idl.CDR cdr)
+   {
+      cdr.write_type_4(data.getSequenceId());
+
+      controller_msgs.msg.dds.FootstepDataListMessagePubSubType.write(data.getFootsteps(), cdr);
+   }
+
+   public static void read(controller_msgs.msg.dds.WalkingControllerPreviewInput data, us.ihmc.idl.CDR cdr)
+   {
+      data.setSequenceId(cdr.read_type_4());
+      	
+      controller_msgs.msg.dds.FootstepDataListMessagePubSubType.read(data.getFootsteps(), cdr);	
+
+   }
+
+   @Override
+   public final void serialize(controller_msgs.msg.dds.WalkingControllerPreviewInput data, us.ihmc.idl.InterchangeSerializer ser)
+   {
+      ser.write_type_4("sequence_id", data.getSequenceId());
+      ser.write_type_a("footsteps", new controller_msgs.msg.dds.FootstepDataListMessagePubSubType(), data.getFootsteps());
+
+   }
+
+   @Override
+   public final void deserialize(us.ihmc.idl.InterchangeSerializer ser, controller_msgs.msg.dds.WalkingControllerPreviewInput data)
+   {
+      data.setSequenceId(ser.read_type_4("sequence_id"));
+      ser.read_type_a("footsteps", new controller_msgs.msg.dds.FootstepDataListMessagePubSubType(), data.getFootsteps());
+
+   }
+
+   public static void staticCopy(controller_msgs.msg.dds.WalkingControllerPreviewInput src, controller_msgs.msg.dds.WalkingControllerPreviewInput dest)
+   {
+      dest.set(src);
+   }
+
+   @Override
+   public controller_msgs.msg.dds.WalkingControllerPreviewInput createData()
+   {
+      return new controller_msgs.msg.dds.WalkingControllerPreviewInput();
+   }
+   @Override
+   public int getTypeSize()
+   {
+      return us.ihmc.idl.CDR.getTypeSize(getMaxCdrSerializedSize());
+   }
+
+   @Override
+   public java.lang.String getName()
+   {
+      return name;
+   }
+   
+   public void serialize(controller_msgs.msg.dds.WalkingControllerPreviewInput data, us.ihmc.idl.CDR cdr)
+   {
+      write(data, cdr);
+   }
+
+   public void deserialize(controller_msgs.msg.dds.WalkingControllerPreviewInput data, us.ihmc.idl.CDR cdr)
+   {
+      read(data, cdr);
+   }
+   
+   public void copy(controller_msgs.msg.dds.WalkingControllerPreviewInput src, controller_msgs.msg.dds.WalkingControllerPreviewInput dest)
+   {
+      staticCopy(src, dest);
+   }
+
+   @Override
+   public WalkingControllerPreviewInputPubSubType newInstance()
+   {
+      return new WalkingControllerPreviewInputPubSubType();
+   }
+}
