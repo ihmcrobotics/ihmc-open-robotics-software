36f702856a9f037df5ac328ee7132e0822b06160
InhoLee <ilee@ihmc.us>
2019-01-17 14:50:27 -0600

    Replaced packet type to PointCloud2.

--- robot-environment-awareness/src/application/java/us/ihmc/robotEnvironmentAwareness/hardware/MultisenseStereoVisionPointCloudWithSourceReceiver.java
+++ robot-environment-awareness/src/application/java/us/ihmc/robotEnvironmentAwareness/hardware/MultisenseStereoVisionPointCloudWithSourceReceiver.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
 import gnu.trove.list.array.TFloatArrayList;
-import scan_to_cloud.PointCloud2WithSource;
+import sensor_msgs.PointCloud2;
 import us.ihmc.communication.IHMCROS2Publisher;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.packets.MessageTools;
@@ -19,39 +23,53 @@ import us.ihmc.utilities.ros.subscriber.AbstractRosTopicSubscriber;
 import us.ihmc.utilities.ros.subscriber.RosPointCloudSubscriber;
 import us.ihmc.utilities.ros.subscriber.RosPointCloudSubscriber.UnpackedPointCloud;
 
-public class MultisenseStereoVisionPointCloudWithSourceReceiver extends AbstractRosTopicSubscriber<PointCloud2WithSource>
+public class MultisenseStereoVisionPointCloudWithSourceReceiver extends AbstractRosTopicSubscriber<PointCloud2>
 {
+   private static final int MAX_NUMBER_OF_POINTS = 200000;
+
    private final Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, "stereoVisionPublisherNode");
 
    private final IHMCROS2Publisher<StereoVisionPointCloudMessage> stereoVisionPublisher;
 
    public MultisenseStereoVisionPointCloudWithSourceReceiver() throws URISyntaxException, IOException
    {
-      super(PointCloud2WithSource._TYPE);
+      super(PointCloud2._TYPE);
       URI masterURI = new URI("http://10.6.192.14:11311");
       RosMainNode rosMainNode = new RosMainNode(masterURI, "StereoVisionPublisher", true);
-      rosMainNode.attachSubscriber("/stereoVisionPointCloudWithSource", this);
+      rosMainNode.attachSubscriber("/multisense/image_points2_color", this);
+
       rosMainNode.execute();
 
       stereoVisionPublisher = ROS2Tools.createPublisher(ros2Node, StereoVisionPointCloudMessage.class, ROS2Tools.getDefaultTopicNameGenerator());
    }
 
    @Override
-   public void onNewMessage(PointCloud2WithSource cloudHolder)
+   public void onNewMessage(PointCloud2 cloudHolder)
    {
-      UnpackedPointCloud pointCloudData = RosPointCloudSubscriber.unpackPointsAndIntensities(cloudHolder.getCloud());
+      UnpackedPointCloud pointCloudData = RosPointCloudSubscriber.unpackPointsAndIntensities(cloudHolder);
 
-      int numberOfPoints = pointCloudData.getPoints().length;
       Point3D[] pointCloud = pointCloudData.getPoints();
       Color[] colors = pointCloudData.getPointColors();
 
-      long timestamp = cloudHolder.getCloud().getHeader().getStamp().totalNsecs();
+      List<Point3D> pointCloudToPublish = Arrays.stream(pointCloud).collect(Collectors.toList());
+      List<Color> colorsToPublish = Arrays.stream(colors).collect(Collectors.toList());
 
+      Random random = new Random();
+      while (pointCloudToPublish.size() > MAX_NUMBER_OF_POINTS)
+      {
+         int indexToRemove = random.nextInt(pointCloudToPublish.size());
+         pointCloudToPublish.remove(indexToRemove);
+         colorsToPublish.remove(indexToRemove);
+      }
+
+      int numberOfPoints = pointCloudToPublish.size();
+
+      long timestamp = cloudHolder.getHeader().getStamp().totalNsecs();
       TFloatArrayList pointCloudBuffer = new TFloatArrayList();
 
       for (int i = 0; i < numberOfPoints; i++)
       {
-         Point3D scanPoint = pointCloud[i];
+         Point3D scanPoint = pointCloudToPublish.get(i);
 
          pointCloudBuffer.add((float) scanPoint.getX());
          pointCloudBuffer.add((float) scanPoint.getY());
@@ -59,9 +77,8 @@ public class MultisenseStereoVisionPointCloudWithSourceReceiver extends Abstract
       }
 
       int[] colorsInteger = new int[numberOfPoints];
-
-      for (int i = 0; i < colors.length; i++)
-         colorsInteger[i] = colors[i].getRGB();
+      for (int i = 0; i < numberOfPoints; i++)
+         colorsInteger[i] = colorsToPublish.get(i).getRGB();
 
       StereoVisionPointCloudMessage stereoVisionMessage = MessageTools.createStereoVisionPointCloudMessage(timestamp, pointCloudBuffer.toArray(),
                                                                                                            colorsInteger);
