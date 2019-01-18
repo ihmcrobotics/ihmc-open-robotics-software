730767067745112e66194228110966442d1c6eb3
Robert <rgriffin@ihmc.us>
2019-01-17 22:32:53 -0600

    fixed the distance cost calculators to reflect the new node calculation

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/DistanceAndYawBasedHeuristics.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/DistanceAndYawBasedHeuristics.java
@@ -1,10 +1,8 @@
 package us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.heuristics;
 
-import us.ihmc.commons.MathTools;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph.FootstepNode;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.parameters.FootstepPlannerParameters;
 import us.ihmc.robotics.geometry.AngleTools;
-import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public class DistanceAndYawBasedHeuristics extends CostToGoHeuristics
 {
@@ -22,16 +20,6 @@ public class DistanceAndYawBasedHeuristics extends CostToGoHeuristics
       double minSteps = 4.0 * bodyDistance / parameters.getMaximumStepCycleDistance();
       double distanceWeight = 0.5 * (parameters.getForwardWeight() + parameters.getLateralWeight());
 
-      double totalCost = parameters.getYawWeight() * Math.abs(yaw) + parameters.getCostPerStep() * minSteps;
-
-      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-         double euclideanDistance = Math.sqrt(MathTools.square(node.getX(robotQuadrant) - goalNode.getX(robotQuadrant)) + MathTools
-               .square(node.getY(robotQuadrant) - goalNode.getY(robotQuadrant)));
-
-         totalCost += distanceWeight * euclideanDistance;
-      }
-
-      return totalCost;
+      return distanceWeight * node.euclideanDistance(goalNode) + parameters.getYawWeight() * Math.abs(yaw) + parameters.getCostPerStep() * minSteps;
    }
 }
