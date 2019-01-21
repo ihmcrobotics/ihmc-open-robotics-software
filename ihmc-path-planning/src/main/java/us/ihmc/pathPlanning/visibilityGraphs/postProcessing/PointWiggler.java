0a44920531edeef97530c75fa63c21fff294eaa7
Robert <rgriffin@ihmc.us>
2019-01-20 21:35:16 -0600

    started doing a fancier version of point wiggler

--- ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/postProcessing/PointWiggler.java
+++ ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/postProcessing/PointWiggler.java
@@ -1,9 +1,12 @@
 package us.ihmc.pathPlanning.visibilityGraphs.postProcessing;
 
+import org.ejml.data.DenseMatrix64F;
+import us.ihmc.convexOptimization.quadraticProgram.JavaQuadProgSolver;
 import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.euclid.tuple2D.interfaces.Vector2DReadOnly;
 
+import javax.vecmath.Vector2d;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -80,4 +83,127 @@ public class PointWiggler
 
       return averageShiftVector;
    }
+
+   static Vector2DReadOnly computeVectorToMaximizeAverageDistanceFromPointsFancily(Point2DReadOnly pointToShift, List<Point2DReadOnly> pointsToAvoidByDistanceA,
+                                                                            List<Point2DReadOnly> pointsToAvoidByDistanceB, double desiredDistanceA,
+                                                                                   double desiredDistanceB)
+   {
+      Vector2D averageShiftVector = new Vector2D();
+      int numberOfPointsWithinProximity = 0;
+
+      List<Point2DReadOnly> pointsToAvoidByDistanceACopy = new ArrayList<>(pointsToAvoidByDistanceA);
+      List<Point2DReadOnly> pointsToAvoidByDistanceBCopy = new ArrayList<>(pointsToAvoidByDistanceB);
+
+      // remove them if we know they can't be to close
+      double maxShift = Math.max(desiredDistanceA, desiredDistanceB);
+      int pointIndex = 0;
+      while (pointIndex < pointsToAvoidByDistanceACopy.size())
+      {
+         if (pointsToAvoidByDistanceACopy.get(pointIndex).distance(pointToShift) > desiredDistanceA + maxShift)
+            pointsToAvoidByDistanceACopy.remove(pointIndex);
+         else
+            pointIndex++;
+      }
+      pointIndex = 0;
+      while (pointIndex < pointsToAvoidByDistanceBCopy.size())
+      {
+         if (pointsToAvoidByDistanceBCopy.get(pointIndex).distance(pointToShift) > desiredDistanceB + maxShift)
+            pointsToAvoidByDistanceBCopy.remove(pointIndex);
+         else
+            pointIndex++;
+      }
+
+
+      // sort everything in ascending order
+      List<Point2DReadOnly> pointsToAvoid = new ArrayList<>(pointsToAvoidByDistanceACopy);
+      pointsToAvoid.addAll(pointsToAvoidByDistanceBCopy);
+      pointsToAvoid.sort((pointA, pointB) -> {
+         double distanceForPointA = pointsToAvoidByDistanceACopy.contains(pointA) ? desiredDistanceA : desiredDistanceB;
+         double distanceForPointB = pointsToAvoidByDistanceACopy.contains(pointB) ? desiredDistanceA : desiredDistanceB;
+
+         double aShiftDistance = pointToShift.distance(pointA) - distanceForPointA;
+         double bShiftDistance = pointToShift.distance(pointB) - distanceForPointB;
+
+         return Double.compare(aShiftDistance, bShiftDistance);
+      });
+
+
+      int pointToCheckIndex = 0;
+      while (pointToCheckIndex < pointsToAvoid.size())
+      {
+         int otherPointIndex = 0;
+         Vector2D currentVector = new Vector2D();
+         Point2DReadOnly pointToAvoid = pointsToAvoid.get(pointToCheckIndex);
+         currentVector.sub(pointToShift, pointToAvoid);
+         double currentDistance = currentVector.length();
+         double currentDesiredDistance = pointsToAvoidByDistanceACopy.contains(pointToAvoid) ? desiredDistanceA : desiredDistanceB;
+
+         boolean affectedByOtherShift = currentDistance < currentDesiredDistance;
+
+         while (otherPointIndex < pointsToAvoid.size() && !affectedByOtherShift)
+         {
+            if (otherPointIndex != pointToCheckIndex)
+            {
+               Point2DReadOnly otherPointToAvoid = pointsToAvoid.get(otherPointIndex);
+
+               double distance = pointsToAvoidByDistanceACopy.contains(otherPointToAvoid) ? desiredDistanceA : desiredDistanceB;
+
+               Vector2D otherVector = new Vector2D();
+               otherVector.sub(pointToShift, otherPointToAvoid);
+               otherVector.scale(distance, otherVector.length());
+
+               affectedByOtherShift = currentDistance + otherVector.dot(currentVector) < currentDesiredDistance;
+            }
+            otherPointIndex++;
+         }
+
+         if (!affectedByOtherShift)
+            pointsToAvoid.remove(pointToCheckIndex);
+         else
+            pointToCheckIndex++;
+      }
+
+
+      Vector2D vectorToPoint = new Vector2D();
+
+      DenseMatrix64F A = new DenseMatrix64F(2, 2);
+      DenseMatrix64F b = new DenseMatrix64F(2, 1);
+      double residualCost = 0.0;
+
+
+      for (Point2DReadOnly pointToShiftFrom : pointsToAvoid)
+      {
+         vectorToPoint.sub(pointToShift, pointToShiftFrom);
+         double distanceToPoint = vectorToPoint.length();
+         double desiredDistance = pointsToAvoidByDistanceACopy.contains(pointToShiftFrom) ? desiredDistanceA : desiredDistanceB;
+         double distanceToShift = desiredDistance - distanceToPoint;
+
+         if (distanceToShift > 0)
+         {
+            vectorToPoint.scale(distanceToShift / distanceToPoint);
+
+            b.add(0, 0, vectorToPoint.getX());
+            b.add(1, 0, vectorToPoint.getY());
+            residualCost += vectorToPoint.dot(vectorToPoint);
+
+            numberOfPointsWithinProximity++;
+         }
+      }
+
+      A.set(0, 0, numberOfPointsWithinProximity);
+      A.set(1, 1, numberOfPointsWithinProximity);
+
+      JavaQuadProgSolver solver = new JavaQuadProgSolver();
+      solver.setQuadraticCostFunction(A, b, residualCost);
+
+      DenseMatrix64F solutionVector = new DenseMatrix64F(2, 1);
+      solver.solve(solutionVector);
+
+      averageShiftVector.set(solutionVector);
+
+      return averageShiftVector;
+   }
+
+
+
 }
