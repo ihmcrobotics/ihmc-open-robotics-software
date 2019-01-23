c457b45a2251cfbe0cb33cbb42e30cee0b511d39
Mark <mark.paterson@nasa.gov>
2019-01-23 17:23:10 -0600

    Add simple non-GUI way to start REA

---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/LidarBasedNonGUIREAStandaloneLauncher.java
@@ -0,0 +1,19 @@
+package us.ihmc.robotEnvironmentAwareness;
+
+import us.ihmc.robotEnvironmentAwareness.updaters.LIDARBasedREAModule;
+
+public class LidarBasedNonGUIREAStandaloneLauncher {
+	private static final String MODULE_CONFIGURATION_FILE_NAME = "./Configurations/defaultREAModuleConfiguration.txt";
+
+	public static void main(String[] args) {
+		LIDARBasedREAModule module;
+
+		try {
+			module = LIDARBasedREAModule.createIntraprocessModule(MODULE_CONFIGURATION_FILE_NAME);
+			module.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
