e63a7ca5e3b58f4a0f657a3bc1222eaa8138e244
Robert <rgriffin@ihmc.us>
2019-01-22 22:13:25 -0600

    set up some more failing test cases

--- ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/tools/VisibilityToolsTest.java
+++ ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/tools/VisibilityToolsTest.java
@@ -8,16 +8,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 
 import us.ihmc.commons.MutationTestFacilitator;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
+import us.ihmc.euclid.tuple2D.interfaces.Vector2DReadOnly;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster;
 import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster.ClusterType;
 import us.ihmc.pathPlanning.visibilityGraphs.clusterManagement.Cluster.ExtrusionSide;
@@ -28,6 +37,7 @@ import us.ihmc.robotics.geometry.PlanarRegion;
 public class VisibilityToolsTest
 {
    private static final double EPSILON = 1.0e-12;
+   private static final int iters = 1000;
 
    @Test(timeout = 30000)
    @ContinuousIntegrationTest(estimatedDuration = 0.0)
@@ -147,8 +157,8 @@ public class VisibilityToolsTest
       Point2D closestPointOnAboveHorizontalLine = new Point2D();
       Point2D closestPointOnAboveHorizontalCluster = new Point2D();
 
-      Point2DReadOnly closestPointOnAboveHorizontalLineExpected = new Point2D(0.5, 0.2);
-      Point2DReadOnly closestPointOnAboveHorizontalClusterExpected = new Point2D(0.5, 0.1);
+      Point2DReadOnly closestPointOnAboveHorizontalLineExpected = new Point2D(0.0, 0.2);
+      Point2DReadOnly closestPointOnAboveHorizontalClusterExpected = new Point2D(0.0, 0.1);
 
       distance = VisibilityTools.distanceToCluster(firstPointAboveHorizontal, secondPointAboveHorizontal, pointsInCluster, closestPointOnAboveHorizontalLine,
                                                    closestPointOnAboveHorizontalCluster, null, true);
@@ -185,6 +195,206 @@ public class VisibilityToolsTest
       ArrayList<Cluster> multilineClusters = new ArrayList<>();
       multilineClusters.add(keepOutClusterMultiline);
       assertTrue(VisibilityTools.isPointVisibleForStaticMaps(multilineClusters, pointOutside, pointInside));
+   }
+
+   @Test(timeout = 30000)
+   @ContinuousIntegrationTest(estimatedDuration = 0.0)
+   public void testDistanceBetweenTwoLineSegment2Ds() throws Exception
+   {
+      Point2D closestPointOnLineSegment1 = new Point2D();
+      Point2D closestPointOnLineSegment2 = new Point2D();
+
+      Vector2D lineSegmentDirection1 = new Vector2D();
+      Vector2D lineSegmentDirection2 = new Vector2D();
+
+      Random random = new Random(11762L);
+
+      // Easy case, the closest points on inside each line segment bounds.
+      for (int i = 0; i < iters; i++)
+      {
+         Point2D lineSegmentStart1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentStart1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+         Point2D lineSegmentEnd1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentEnd1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+
+         lineSegmentDirection1.sub(lineSegmentEnd1, lineSegmentStart1);
+         lineSegmentDirection1.normalize();
+
+         // Put the first closest within bounds of line segment 1
+         closestPointOnLineSegment1.interpolate(lineSegmentStart1, lineSegmentEnd1, EuclidCoreRandomTools.nextDouble(random, 0.0, 1.0));
+
+         // Create the closest point of line segment 2
+         Vector2D orthogonalToLineSegment1 = nextOrthogonalVector2D(random, lineSegmentDirection1, true);
+         double expectedMinimumDistance = EuclidCoreRandomTools.nextDouble(random, 0.0, 10.0);
+         closestPointOnLineSegment2.scaleAdd(expectedMinimumDistance, orthogonalToLineSegment1, closestPointOnLineSegment1);
+
+         // Set the line direction 2 to be the rotation of 1 around the shift
+         // direction used to create the expectedPointOnLineSegment2
+         double rotationAngle = EuclidCoreRandomTools.nextDouble(random, 2.0 * Math.PI);
+//         AxisAngle rotationAroundShiftVector = new AxisAngle(orthogonalToLineSegment1, rotationAngle);
+         AxisAngle rotationAroundShiftVector = new AxisAngle();
+         rotationAroundShiftVector.setYawPitchRoll(rotationAngle, 0.0, 0.0);
+         rotationAroundShiftVector.transform(lineSegmentDirection1, lineSegmentDirection2);
+
+         // Set the end points of the line segment 2 around the expected
+         // closest point.
+         Point2D lineSegmentStart2 = new Point2D();
+         Point2D lineSegmentEnd2 = new Point2D();
+         lineSegmentStart2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, -10.0, 0.0), lineSegmentDirection2, closestPointOnLineSegment2);
+         lineSegmentEnd2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, 0.0, 10.0), lineSegmentDirection2, closestPointOnLineSegment2);
+
+         double actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentStart2,
+                                                                                             lineSegmentEnd2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+      }
+
+      // Parallel case, expecting expectedPointOnLineSegment1 =
+      // lineSegmentStart1
+      for (int i = 0; i < iters; i++)
+      {
+         Point2D lineSegmentStart1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentStart1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+         Point2D lineSegmentEnd1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentEnd1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+
+         lineSegmentDirection1.sub(lineSegmentEnd1, lineSegmentStart1);
+         lineSegmentDirection1.normalize();
+
+         // expectedPointOnLineSegment1 = lineSegmentStart1
+         closestPointOnLineSegment1.set(lineSegmentStart1);
+
+         // Create the closest point of line segment 2
+         Vector2D orthogonalToLineSegment1 = nextOrthogonalVector2D(random, lineSegmentDirection1, true);
+         double expectedMinimumDistance = EuclidCoreRandomTools.nextDouble(random, 0.0, 10.0);
+         closestPointOnLineSegment2.scaleAdd(expectedMinimumDistance, orthogonalToLineSegment1, closestPointOnLineSegment1);
+
+         // Set the lineSegmentDirection2 = lineSegmentDirection1
+         lineSegmentDirection2.set(lineSegmentDirection1);
+
+         // Set the end points of the line segment 2 around the expected
+         // closest point.
+         Point2D lineSegmentStart2 = new Point2D();
+         Point2D lineSegmentEnd2 = new Point2D();
+         lineSegmentStart2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, -10.0, 0.0), lineSegmentDirection2, closestPointOnLineSegment2);
+         lineSegmentEnd2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, 0.0, 10.0), lineSegmentDirection2, closestPointOnLineSegment2);
+
+         double actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentStart2,
+                                                                                             lineSegmentEnd2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+
+         // Set the end points of the line segment 2 before the expected
+         // closest point, so we have expectedClosestPointOnLineSegment2 =
+         // lineSegmentEnd2
+         double shiftStartFromExpected = EuclidCoreRandomTools.nextDouble(random, -20.0, -10.0);
+         double shiftEndFromExpected = EuclidCoreRandomTools.nextDouble(random, -10.0, 0.0);
+         lineSegmentStart2.scaleAdd(shiftStartFromExpected, lineSegmentDirection2, closestPointOnLineSegment2);
+         lineSegmentEnd2.scaleAdd(shiftEndFromExpected, lineSegmentDirection2, closestPointOnLineSegment2);
+         closestPointOnLineSegment2.set(lineSegmentEnd2);
+         expectedMinimumDistance = closestPointOnLineSegment1.distance(closestPointOnLineSegment2);
+
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentStart2, lineSegmentEnd2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentEnd2, lineSegmentStart2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+      }
+
+      // Case: on closest point on lineSegment1 outside end points.
+      for (int i = 0; i < iters; i++)
+      {
+         Point2D lineSegmentStart1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentStart1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+         Point2D lineSegmentEnd1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentEnd1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+
+         lineSegmentDirection1.sub(lineSegmentEnd1, lineSegmentStart1);
+         lineSegmentDirection1.normalize();
+
+         // Put the first closest to the start of line segment 1
+         closestPointOnLineSegment1.set(lineSegmentStart1);
+
+         // Create the closest point of line segment 2 such that it reaches
+         // out of line segment 1
+         Vector2D oppositeOflineSegmentDirection1 = new Vector2D();
+         oppositeOflineSegmentDirection1.setAndNegate(lineSegmentDirection1);
+         Vector2D orthogonalToLineSegment1 = nextOrthogonalVector2D(random, lineSegmentDirection1, true);
+         Vector2D shiftVector = new Vector2D();
+         shiftVector.interpolate(orthogonalToLineSegment1, oppositeOflineSegmentDirection1, EuclidCoreRandomTools.nextDouble(random, 0.0, 1.0));
+         closestPointOnLineSegment2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, 0.0, 10.0), shiftVector, closestPointOnLineSegment1);
+
+         // Set the line direction 2 to orthogonal to the shift vector
+         lineSegmentDirection2 = nextOrthogonalVector2D(random, shiftVector, true);
+
+         // Set the end points of the line segment 2 around the expected
+         // closest point.
+         Point2D lineSegmentStart2 = new Point2D();
+         Point2D lineSegmentEnd2 = new Point2D();
+         lineSegmentStart2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, -10.0, 0.0), lineSegmentDirection2, closestPointOnLineSegment2);
+         lineSegmentEnd2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, 0.0, 10.0), lineSegmentDirection2, closestPointOnLineSegment2);
+         double expectedMinimumDistance = closestPointOnLineSegment1.distance(closestPointOnLineSegment2);
+
+         double actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentStart2,
+                                                                                             lineSegmentEnd2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentEnd2, lineSegmentStart2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentEnd1, lineSegmentStart1, lineSegmentStart2, lineSegmentEnd2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentEnd1, lineSegmentStart1, lineSegmentEnd2, lineSegmentStart2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+      }
+
+      // Edge case: both closest points are outside bounds of each line
+      // segment
+      for (int i = 0; i < iters; i++)
+      {
+         Point2D lineSegmentStart1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentStart1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+         Point2D lineSegmentEnd1 = EuclidCoreRandomTools.nextPoint2D(random);
+         lineSegmentEnd1.scale(EuclidCoreRandomTools.nextDouble(random, 10.0));
+
+         lineSegmentDirection1.sub(lineSegmentEnd1, lineSegmentStart1);
+         lineSegmentDirection1.normalize();
+
+         // Put the first closest to the start of line segment 1
+         closestPointOnLineSegment1.set(lineSegmentStart1);
+
+         // Create the closest point of line segment 2 such that it reaches
+         // out of line segment 1
+         Vector2D oppositeOflineSegmentDirection1 = new Vector2D();
+         oppositeOflineSegmentDirection1.setAndNegate(lineSegmentDirection1);
+         Vector2D orthogonalToLineSegment1 = nextOrthogonalVector2D(random, lineSegmentDirection1, true);
+         Vector2D shiftVector = new Vector2D();
+         double alpha = EuclidCoreRandomTools.nextDouble(random, 0.0, 1.0);
+         shiftVector.interpolate(orthogonalToLineSegment1, oppositeOflineSegmentDirection1, alpha);
+         closestPointOnLineSegment2.scaleAdd(EuclidCoreRandomTools.nextDouble(random, 0.0, 10.0), shiftVector, closestPointOnLineSegment1);
+
+         // set the start of the second line segment to the expected closest
+         // point
+         Point2D lineSegmentStart2 = new Point2D(closestPointOnLineSegment2);
+
+         // Set the line direction 2 to point somewhat in the same direction
+         // as the shift vector
+         Vector2D orthogonalToShiftVector = nextOrthogonalVector2D(random, shiftVector, true);
+         lineSegmentDirection2.interpolate(shiftVector, orthogonalToShiftVector, EuclidCoreRandomTools.nextDouble(random, 0.0, 1.0));
+
+         // Set the end points of the line segment 2 around the expected
+         // closest point.
+         Point2D lineSegmentEnd2 = new Point2D();
+         double alpha2 = EuclidCoreRandomTools.nextDouble(random, 0.1, 10.0);
+         lineSegmentEnd2.scaleAdd(alpha2, lineSegmentDirection2, closestPointOnLineSegment2);
+
+         double expectedMinimumDistance = closestPointOnLineSegment1.distance(closestPointOnLineSegment2);
+         double actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentStart2,
+                                                                                             lineSegmentEnd2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentStart1, lineSegmentEnd1, lineSegmentEnd2, lineSegmentStart2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentEnd1, lineSegmentStart1, lineSegmentStart2, lineSegmentEnd2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+         actualMinimumDistance = VisibilityTools.distanceBetweenTwoLineSegment2Ds(lineSegmentEnd1, lineSegmentStart1, lineSegmentEnd2, lineSegmentStart2);
+         assertEquals(expectedMinimumDistance, actualMinimumDistance, EPSILON);
+      }
    }
 
    private PlanarRegion createAHomeRegionSquare(double xyMax)
@@ -228,6 +438,42 @@ public class VisibilityToolsTest
       //      System.out.println("}");
 
    }
+
+   /**
+    * Generates a random vector that is perpendicular to {@code vectorToBeOrthogonalTo}.
+    *
+    * @param random the random generator to use.
+    * @param vectorToBeOrthogonalTo the vector to be orthogonal to. Not modified.
+    * @param normalize whether to normalize the generated vector or not.
+    * @return the random vector.
+    */
+   public static Vector2D nextOrthogonalVector2D(Random random, Vector2DReadOnly vectorToBeOrthogonalTo, boolean normalize)
+   {
+      Vector2D v1 = new Vector2D(vectorToBeOrthogonalTo.getY(), -vectorToBeOrthogonalTo.getX());
+
+      Vector2D randomPerpendicular = new Vector2D();
+      double a = nextDouble(random, 1.0);
+      randomPerpendicular.scaleAdd(a, v1, randomPerpendicular);
+
+      if (normalize)
+         randomPerpendicular.normalize();
+
+      return randomPerpendicular;
+   }
+
+   public static double nextDouble(Random random, double minMaxValue)
+   {
+      return nextDouble(random, -minMaxValue, minMaxValue);
+   }
+
+   public static double nextDouble(Random random, double minValue, double maxValue)
+   {
+      if (minValue > maxValue)
+         throw new RuntimeException("Min is greater than max: min = " + minValue + ", max = " + maxValue);
+
+      return minValue + random.nextDouble() * (maxValue - minValue);
+   }
+
 
    public static void main(String[] args) throws IOException
    {
