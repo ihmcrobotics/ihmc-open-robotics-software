06a24086febe402d20123923d832c5676abd1466
Mark <mark.paterson@nasa.gov>
2019-01-24 13:54:17 -0600

    Revert "adding check to grandparent node for step downs"
    This reverts commit 022f1e3f1bfb5fc3fbbc45bdaf952a4355885b4f.

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/nodeChecking/SnapBasedNodeChecker.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/nodeChecking/SnapBasedNodeChecker.java
@@ -120,7 +120,6 @@ public class SnapBasedNodeChecker extends FootstepNodeChecker
             RigidBodyTransform grandparentSnapTransform = snapper.getSnapData(grandparentNode).getSnapTransform();
             RigidBodyTransform grandparentNodeTransform = new RigidBodyTransform();
             FootstepNodeTools.getSnappedNodeTransform(grandparentNode, grandparentSnapTransform, grandparentNodeTransform);
-            double grandparentTranslationScaleFactor = 1.6;
 
             double heightChangeFromGrandparentNode = nodePosition.getZ() - grandparentNodeTransform.getTranslationZ();
             double translationChangeFromGrandparentNode = EuclidCoreTools
@@ -128,15 +127,7 @@ public class SnapBasedNodeChecker extends FootstepNodeChecker
 
             boolean largeStepUp = heightChangeFromGrandparentNode > parameters.getMaximumStepZWhenSteppingUp();
 
-            if (largeStepUp && translationChangeFromGrandparentNode > grandparentTranslationScaleFactor * parameters.getMaximumStepReachWhenSteppingUp())
-            {
-               rejectNode(node, previousNode, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_FAR_AND_HIGH);
-               return false;
-            }
-
-            boolean largeStepDown = heightChangeFromGrandparentNode < parameters.getMaximumStepZWhenForwardAndDown();
-
-            if(largeStepDown && translationChangeFromGrandparentNode > grandparentTranslationScaleFactor * parameters.getMaximumStepXWhenForwardAndDown())
+            if (largeStepUp && translationChangeFromGrandparentNode > 1.7 * parameters.getMaximumStepReachWhenSteppingUp())
             {
                rejectNode(node, previousNode, BipedalFootstepPlannerNodeRejectionReason.STEP_TOO_FAR_AND_HIGH);
                return false;
