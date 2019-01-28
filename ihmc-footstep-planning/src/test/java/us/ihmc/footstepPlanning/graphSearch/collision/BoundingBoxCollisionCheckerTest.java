6bc4ebdb09f768417670f6f8766f08262a56d5a6
smccrory <smccrory@ihmc.us>
2019-01-28 15:30:43 -0600

    adding rotation and height tests

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/graphSearch/collision/BoundingBoxCollisionCheckerTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/graphSearch/collision/BoundingBoxCollisionCheckerTest.java
@@ -115,6 +115,125 @@ public class BoundingBoxCollisionCheckerTest
       Assert.assertTrue(collisionData.isCollisionDetected());
    }
 
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
+   public void testCollisionWithRotatedBody()
+   {
+      FootstepPlannerParameters plannerParameters = new DefaultFootstepPlanningParameters()
+      {
+         @Override
+         public double getBodyBoxDepth()
+         {
+            return 0.5;
+         }
+
+         @Override
+         public double getBodyBoxWidth()
+         {
+            return 1.0;
+         }
+
+         @Override
+         public double getBodyBoxHeight()
+         {
+            return 1.0;
+         }
+
+         @Override
+         public FootstepPlannerCostParameters getCostParameters()
+         {
+            return new DefaultFootstepPlannerCostParameters()
+            {
+               @Override
+               public double getMaximum2dDistanceFromBoundingBoxToPenalize()
+               {
+                  return 0.25;
+               }
+            };
+         }
+      };
+
+      BoundingBoxCollisionChecker collisionChecker = new BoundingBoxCollisionChecker(plannerParameters);
+      double distanceFromRegionAtZeroYaw = 0.025;
+      PlanarRegionsList planarRegionsList = getSquarePlanarRegionsList(0.75, 0.75, 0.5, 0.0, 0.5 - 2.0 * distanceFromRegionAtZeroYaw);
+      collisionChecker.setPlanarRegionsList(planarRegionsList);
+
+      // assert no collision at zero yaw
+      collisionChecker.setBoxPose(0.25, 0.25, 0.0, Math.toRadians(0.0));
+      BodyCollisionData collisionData = collisionChecker.checkForCollision();
+      Assert.assertFalse(collisionData.isCollisionDetected());
+      Assert.assertEquals(collisionData.getDistanceFromBoundingBox(), distanceFromRegionAtZeroYaw, 1e-8);
+
+      // assert no collision at small positive rotation
+      collisionChecker.setBoxPose(0.25, 0.25, 0.0, Math.toRadians(15.0));
+      collisionData = collisionChecker.checkForCollision();
+      Assert.assertFalse(collisionData.isCollisionDetected());
+
+      // assert collision at small negative rotation
+      collisionChecker.setBoxPose(0.25, 0.25, 0.0, Math.toRadians(-15.0));
+      collisionData = collisionChecker.checkForCollision();
+      Assert.assertTrue(collisionData.isCollisionDetected());
+   }
+
+   @ContinuousIntegrationAnnotations.ContinuousIntegrationTest(estimatedDuration = 0.0)
+   @Test(timeout = 30000)
+   public void testHeightDetection()
+   {
+      FootstepPlannerParameters plannerParameters = new DefaultFootstepPlanningParameters()
+      {
+         @Override
+         public double getBodyBoxDepth()
+         {
+            return 0.5;
+         }
+
+         @Override
+         public double getBodyBoxWidth()
+         {
+            return 1.0;
+         }
+
+         @Override
+         public double getBodyBoxHeight()
+         {
+            return 1.0;
+         }
+
+         @Override
+         public FootstepPlannerCostParameters getCostParameters()
+         {
+            return new DefaultFootstepPlannerCostParameters()
+            {
+               @Override
+               public double getMaximum2dDistanceFromBoundingBoxToPenalize()
+               {
+                  return 0.25;
+               }
+            };
+         }
+      };
+
+      BoundingBoxCollisionChecker collisionChecker = new BoundingBoxCollisionChecker(plannerParameters);
+      PlanarRegionsList planarRegionsList = getSquarePlanarRegionsList(0.25, 0.25, 0.5, 0.0, 0.5);
+      collisionChecker.setPlanarRegionsList(planarRegionsList);
+
+      // check when body is too high
+      collisionChecker.setBoxPose(0.0, 0.0, 0.51, 0.0);
+      Assert.assertFalse(collisionChecker.checkForCollision().isCollisionDetected());
+
+      // check when body is just below too high
+      collisionChecker.setBoxPose(0.0, 0.0, 0.49, 0.0);
+      Assert.assertTrue(collisionChecker.checkForCollision().isCollisionDetected());
+
+      // check when body is too low
+      collisionChecker.setBoxPose(0.0, 0.0, -0.51, 0.0);
+      Assert.assertFalse(collisionChecker.checkForCollision().isCollisionDetected());
+
+      // check when body is just above too low
+      collisionChecker.setBoxPose(0.0, 0.0, -0.49, 0.0);
+      Assert.assertTrue(collisionChecker.checkForCollision().isCollisionDetected());
+   }
+
    private static PlanarRegionsList getSquarePlanarRegionsList(double x, double y, double z, double yaw, double sideLength)
    {
       ConvexPolygon2D unitSquare = new ConvexPolygon2D();
