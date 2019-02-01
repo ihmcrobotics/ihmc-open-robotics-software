3c2e441ad7045cd08efd5ef41c7f4d043ca9a3f1
Robert <rgriffin@ihmc.us>
2019-01-31 19:11:43 -0600

    modified the heuristic functions to allow switching once the body has reached the goal

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/CostToGoHeuristics.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/CostToGoHeuristics.java
@@ -23,5 +23,7 @@ public abstract class CostToGoHeuristics
       return parameters.getHeuristicsWeight() * computeHeuristics(node, goalNode);
    }
 
+   public abstract void setBodyHasReachedGoal(boolean bodyHasReachedGoal);
+
    protected abstract double computeHeuristics(FootstepNode node, FootstepNode goalNode);
 }
