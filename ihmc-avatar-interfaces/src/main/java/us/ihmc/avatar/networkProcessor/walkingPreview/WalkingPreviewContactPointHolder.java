84eb12db96399809762feaedd23480aba6ed9435
Sylvain Bertrand <sbertrand@ihmc.us>
2019-02-01 15:15:22 -0600

    Fixed up the contact point holder.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/walkingPreview/WalkingPreviewContactPointHolder.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/walkingPreview/WalkingPreviewContactPointHolder.java
@@ -10,6 +10,7 @@ import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
 import us.ihmc.mecano.frames.MovingReferenceFrame;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.mecano.tools.MultiBodySystemTools;
 import us.ihmc.robotics.controllers.pidGains.GainCalculator;
 import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
@@ -22,7 +23,7 @@ public class WalkingPreviewContactPointHolder
    private final MovingReferenceFrame currentPlaneFrame;
    private final ReferenceFrame desiredPlaneFrame;
 
-   private final double kp = 100.0;
+   private final double kp = 500.0;
    private final double zeta = 1.0;
    private final double kd = GainCalculator.computeDerivativeGain(kp, zeta);
    private final double weight = 50.0;
@@ -52,9 +53,7 @@ public class WalkingPreviewContactPointHolder
       for (FramePoint3D currentContactPoint : contactStateToHold.getContactFramePointsInContactCopy())
       {
          currentContactPoint.changeFrame(currentPlaneFrame);
-         FrameVector3D toContactPoint = new FrameVector3D(currentContactPoint);
-         toContactPoint.changeFrame(desiredPlaneFrame);
-         FramePoint3D desiredContactPoint = new FramePoint3D(toContactPoint);
+         FramePoint3D desiredContactPoint = new FramePoint3D(desiredPlaneFrame, currentContactPoint);
          PoseReferenceFrame controlFrame = new PoseReferenceFrame("atContactPoint", currentPlaneFrame);
          controlFrame.setPositionAndUpdate(currentContactPoint);
 
@@ -68,10 +67,12 @@ public class WalkingPreviewContactPointHolder
          FrameVector3D desiredLinearAcceleration = new FrameVector3D();
          desiredLinearAcceleration.sub(desiredContactPoint, currentContactPoint);
          desiredLinearAcceleration.scale(kp);
-         desiredLinearAcceleration.scaleAdd(kd, currentLinearVelocity, desiredLinearAcceleration);
+         desiredLinearAcceleration.scaleAdd(-kd, currentLinearVelocity, desiredLinearAcceleration);
+         desiredLinearAcceleration.changeFrame(controlFrame);
 
          SpatialAccelerationCommand command = new SpatialAccelerationCommand();
-         command.set(MultiBodySystemTools.getRootBody(contactStateToHold.getRigidBody()), contactStateToHold.getRigidBody());
+         RigidBodyBasics contactingBody = contactStateToHold.getRigidBody();
+         command.set(MultiBodySystemTools.getRootBody(contactingBody), contactingBody);
          command.setLinearAcceleration(controlFrame, desiredLinearAcceleration);
          command.setWeight(0.0, weight);
          command.setSelectionMatrixForLinearControl();
