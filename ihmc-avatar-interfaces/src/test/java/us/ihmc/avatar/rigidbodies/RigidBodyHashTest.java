249fdd5812f26d195ac94a8b7f5ea0e4cf8d010c
InhoLee <ilee@ihmc.us>
2019-02-04 12:25:38 -0600

    Printer for hash codes of rigid bodies.

---
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/rigidbodies/RigidBodyHashTest.java
@@ -0,0 +1,94 @@
+package us.ihmc.avatar.rigidbodies;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.log.LogTools;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
+import us.ihmc.robotModels.FullHumanoidRobotModel;
+import us.ihmc.robotics.robotSide.RobotSide;
+
+public abstract class RigidBodyHashTest
+{
+   private static final int maximumNumberOfTimes = 30;
+
+   @ContinuousIntegrationTest(estimatedDuration = 2.0)
+   @Test(timeout = 30000)
+   public void testHandHashCode()
+   {
+      DRCRobotModel robotModel = getRobotModel();
+      FullHumanoidRobotModel fullRobotModel = robotModel.createFullRobotModel();
+
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         RigidBodyBasics hand = fullRobotModel.getHand(robotSide);
+
+         printNameIDAndHashCode(hand);
+      }
+
+      RigidBodyBasics chest = fullRobotModel.getChest();
+      printNameIDAndHashCode(chest);
+
+      RigidBodyBasics pelvis = fullRobotModel.getPelvis();
+      printNameIDAndHashCode(pelvis);
+      assertTrue(true);
+   }
+
+   @ContinuousIntegrationTest(estimatedDuration = 3.0)
+   @Test(timeout = 50000)
+   public void testAllRigidBodiesHashCode()
+   {
+      DRCRobotModel robotModel = getRobotModel();
+      FullHumanoidRobotModel fullRobotModel = robotModel.createFullRobotModel();
+
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         RigidBodyBasics hand = fullRobotModel.getHand(robotSide);
+
+         for (int i = 0; i < maximumNumberOfTimes; i++)
+         {
+            if (printNameIDAndHashCode(hand) == getElevatorHashCode())
+               break;
+
+            hand = hand.getParentJoint().getPredecessor();
+         }
+      }
+
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         RigidBodyBasics foot = fullRobotModel.getFoot(robotSide);
+
+         for (int i = 0; i < maximumNumberOfTimes; i++)
+         {
+            if (printNameIDAndHashCode(foot) == getElevatorHashCode())
+               break;
+
+            foot = foot.getParentJoint().getPredecessor();
+         }
+      }
+      assertTrue(true);
+   }
+
+   private int printNameIDAndHashCode(RigidBodyBasics rigidBody)
+   {
+      String rigidBodyName = rigidBody.getName();
+      String rigidBodyNameID = rigidBody.getNameId();
+      int hashCode = rigidBodyName.hashCode();
+      int hashCodeID = rigidBodyNameID.hashCode();
+      LogTools.info(rigidBodyName + " string ID is " + rigidBodyNameID);
+      LogTools.info(rigidBodyName + " hash code is " + hashCodeID);
+      return hashCode;
+   }
+
+   private int getElevatorHashCode()
+   {
+      DRCRobotModel robotModel = getRobotModel();
+
+      return robotModel.createFullRobotModel().getElevator().getNameId().hashCode();
+   }
+
+   public abstract DRCRobotModel getRobotModel();
+}
