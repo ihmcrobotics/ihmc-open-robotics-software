ff018a1e376001df22375070081d148a0b87ec04
Mark <mark.paterson@nasa.gov>
2019-02-05 18:53:54 -0600

    Improve ability to shutdown Network Processor

--- valkyrie/launchScripts/runNetworkProcessor.sh
+++ valkyrie/launchScripts/runNetworkProcessor.sh
@@ -1,4 +1,41 @@
-#!/bin/bash
+#!/bin/bash -e
 
-mkdir -p $HOME/.ihmc/logs/
-java -Djava.library.path=lib/ -cp ValkyrieController.jar us.ihmc.valkyrie.ValkyrieNetworkProcessor | tee $HOME/.ihmc/logs/ValkyrieNetworkProc_$(date '+%Y%m%d_%H%M%S').log
+
+NETWORK_PROCESSOR_CLASS=us.ihmc.valkyrie.ValkyrieNetworkProcessor
+
+# Little helper function to find Network Processor instances.
+function find_network_processor_pids {
+     old_processes=$(pgrep -a java | grep $NETWORK_PROCESSOR_CLASS | awk '{print $1}')
+}
+
+# Nuke any existing Network Processor processes, giving them time to die
+old_processes=""
+find_network_processor_pids
+
+if [[ ! -z "$old_processes" ]]; then
+    for pid in "$old_processes"; do
+        echo "Got Network Processor with PID $pid"
+        kill $old_processes && echo Waiting for old Network Processor processes to die && sleep 3
+        kill -9 $old_processes && echo Waiting for old Network Processor processes to die harder && sleep 3
+    done
+fi
+
+# Check whether there's something still not dead, in which case give up
+find_network_processor_pids
+if [[ ! -z "$old_processes" ]]; then
+    echo "Unable to kill old Network Processor PIDs $old_processes"
+    exit 1
+fi
+
+echo "Starting Network Processor"
+
+java -Djava.library.path=lib/ -cp ValkyrieController.jar $NETWORK_PROCESSOR_CLASS &
+NETWORK_PROCESSOR_PID=$!
+trap "rosnode kill /darpaRoboticsChallange/networkProcessor \
+      && rosnode kill /networkProcessor/rosModule \
+      && sleep 2 \
+      && kill $NETWORK_PROCESSOR_PID \
+      && sleep 5 \
+      && kill -9 $NETWORK_PROCESSOR_PID; exit 0" INT TERM
+wait
+
