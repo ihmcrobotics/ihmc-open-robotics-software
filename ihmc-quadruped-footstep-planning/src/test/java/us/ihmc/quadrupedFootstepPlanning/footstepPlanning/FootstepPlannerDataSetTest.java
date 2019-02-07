27d2fe8676e7147f3e9360ad82d9cd908832b970
Robert <rgriffin@ihmc.us>
2019-02-06 21:14:30 -0600

    got it so that the plans would be launched

--- ihmc-quadruped-footstep-planning/src/test/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/FootstepPlannerDataSetTest.java
+++ ihmc-quadruped-footstep-planning/src/test/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/FootstepPlannerDataSetTest.java
@@ -1,36 +1,40 @@
 package us.ihmc.quadrupedFootstepPlanning.footstepPlanning;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import us.ihmc.commons.Conversions;
+import us.ihmc.commons.MathTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import us.ihmc.continuousIntegration.ContinuousIntegrationTools;
 import us.ihmc.continuousIntegration.IntegrationCategory;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.footstepPlanning.communication.FootstepPlannerMessagerAPI;
-import us.ihmc.footstepPlanning.tools.FootstepPlannerDataExporter;
-import us.ihmc.footstepPlanning.ui.components.FootstepPathCalculatorModule;
-import us.ihmc.javaFXToolkit.messager.SharedMemoryJavaFXMessager;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DBasics;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.log.LogTools;
 import us.ihmc.messager.Messager;
-import us.ihmc.messager.SharedMemoryMessager;
+import us.ihmc.quadrupedBasics.gait.QuadrupedTimedStep;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph.FootstepNode;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.tools.FootstepPlannerIOTools;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.tools.FootstepPlannerIOTools.FootstepPlannerUnitTestDataset;
 import us.ihmc.robotics.Assert;
+import us.ihmc.robotics.robotSide.QuadrantDependentList;
+import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static org.junit.Assert.assertEquals;
 
 public abstract class FootstepPlannerDataSetTest
 {
    protected static final double bambooTimeScaling = 4.0;
+   private static final double epsilon = 1e-3;
+
 
    // Whether to start the UI or not.
    protected static boolean VISUALIZE = true;
@@ -38,25 +42,11 @@ public abstract class FootstepPlannerDataSetTest
    protected static boolean DEBUG = true;
    protected static boolean VERBOSE = true;
 
-   private FootstepPlannerUI ui = null;
-   private Messager messager = null;
-
-   private final AtomicReference<FootstepPlan> plannerPlanReference = new AtomicReference<>(null);
-   private final AtomicReference<FootstepPlanningResult> plannerResultReference = new AtomicReference<>(null);
-   private final AtomicReference<Boolean> plannerReceivedPlan = new AtomicReference<>(false);
-   private final AtomicReference<Boolean> plannerReceivedResult = new AtomicReference<>(false);
-
-   private final AtomicReference<Boolean> uiReceivedPlan = new AtomicReference<>(false);
-   private final AtomicReference<Boolean> uiReceivedResult = new AtomicReference<>(false);
-
-   private final AtomicReference<FootstepPlan> expectedPlan = new AtomicReference<>(null);
-   private final AtomicReference<FootstepPlan> actualPlan = new AtomicReference<>(null);
-   private final AtomicReference<FootstepPlanningResult> expectedResult = new AtomicReference<>(null);
-   private final AtomicReference<FootstepPlanningResult> actualResult = new AtomicReference<>(null);
-
    private QuadrupedBodyPathAndFootstepPlanner planner = null;
 
    protected abstract FootstepPlannerType getPlannerType();
+
+   protected abstract QuadrupedBodyPathAndFootstepPlanner createPlanner();
 
    @Before
    public void setup()
@@ -71,8 +61,6 @@ public abstract class FootstepPlannerDataSetTest
    {
       planner = null;
    }
-
-   protected abstract QuadrupedBodyPathAndFootstepPlanner createPlanner();
 
 
    @Test(timeout = 500000)
@@ -162,47 +150,86 @@ public abstract class FootstepPlannerDataSetTest
    protected String runAssertions(FootstepPlannerUnitTestDataset dataset)
    {
       ThreadTools.sleep(1000);
-      packPlanningRequest(dataset, messager);
+      packPlanningRequest(dataset);
       return findPlanAndAssertGoodResult(dataset);
    }
 
-   protected void packPlanningRequest(FootstepPlannerUnitTestDataset dataset, Messager messager)
+   protected void packPlanningRequest(FootstepPlannerUnitTestDataset dataset)
    {
-      messager.submitMessage(FootstepPlannerMessagerAPI.StartPositionTopic, dataset.getStart());
-      messager.submitMessage(FootstepPlannerMessagerAPI.GoalPositionTopic, dataset.getGoal());
-      messager.submitMessage(PlannerTypeTopic, getPlannerType());
-      messager.submitMessage(FootstepPlannerMessagerAPI.PlanarRegionDataTopic, dataset.getPlanarRegionsList());
+      FramePose3D startPose = new FramePose3D();
+      FramePose3D goalPose = new FramePose3D();
+      startPose.setPosition(dataset.getStart());
+      goalPose.setPosition(dataset.getGoal());
+      if (dataset.hasStartOrientation())
+         startPose.setOrientation(dataset.getStartOrientation());
+      if (dataset.hasGoalOrientation())
+         goalPose.setOrientation(dataset.getGoalOrientation());
 
       double timeMultiplier = ContinuousIntegrationTools.isRunningOnContinuousIntegrationServer() ? bambooTimeScaling : 1.0;
-      messager.submitMessage(FootstepPlannerMessagerAPI.PlannerTimeoutTopic, timeMultiplier * dataset.getTimeout(getPlannerType()));
+      double timeout = timeMultiplier * dataset.getTimeout(getPlannerType());
 
-      messager.submitMessage(FootstepPlannerMessagerAPI.PlannerHorizonLengthTopic, Double.MAX_VALUE);
+      QuadrupedFootstepPlannerStart start = new QuadrupedFootstepPlannerStart();
+      QuadrupedFootstepPlannerGoal goal = new QuadrupedFootstepPlannerGoal();
+      start.setStartPose(startPose);
+      goal.setGoalPose(goalPose);
 
-      if (dataset.hasGoalOrientation())
-         messager.submitMessage(FootstepPlannerMessagerAPI.GoalOrientationTopic, dataset.getGoalOrientation());
-      if (dataset.hasStartOrientation())
-         messager.submitMessage(FootstepPlannerMessagerAPI.StartOrientationTopic, dataset.getStartOrientation());
-
-      messager.submitMessage(FootstepPlannerMessagerAPI.ComputePathTopic, true);
+      planner.setStart(start);
+      planner.setGoal(goal);
+      planner.setPlanarRegionsList(dataset.getPlanarRegionsList());
+      planner.setTimeout(timeout);
+//      planner.setHorizonLengthTopic(Double.MAX_VALUE);
 
       if (DEBUG)
-         LogTools.info("Sending out planning request packet.");
+         LogTools.info("Set planner parameters.");
    }
 
 
 
-   protected String assertPlanIsValid(String datasetName, FootstepPlanningResult result, FootstepPlan plan, Point3D goal)
-   {
-      String errorMessage = "";
 
-      if(!result.validForExecution())
+
+
+
+   private String findPlanAndAssertGoodResult(FootstepPlannerUnitTestDataset dataset)
+   {
+      String datasetName = dataset.getDatasetName();
+
+      FootstepPlanningResult pathResult = planner.planPath();
+      if (!pathResult.validForExecution())
+         return "Path plan for " + datasetName + " is invalid.";
+
+      FootstepPlanningResult planResult = planner.plan();
+      if (!planResult.validForExecution())
+         return "Footstep plan for " + datasetName + " is invalid.";
+
+
+      String errorMessage = assertPlanIsValid(datasetName, planner.getSteps(), dataset.getGoal(), dataset.getGoalOrientation().getYaw());
+
+      ThreadTools.sleep(1000);
+      return errorMessage;
+   }
+
+   private static String assertPlanIsValid(String datasetName, List<? extends QuadrupedTimedStep> plannedSteps, Point3DReadOnly goalPosition, double goalYaw)
+   {
+      QuadrantDependentList<Point3DBasics> finalSteps = getFinalStepPositions(plannedSteps);
+
+      Point3D centerPoint = new Point3D();
+      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
       {
-         errorMessage = "Planning result for " + datasetName + " is invalid, result was " + result;
+         centerPoint.add(finalSteps.get(robotQuadrant));
       }
-      else if(!PlannerTools.isGoalNextToLastStep(goal, plan))
-      {
-         errorMessage = datasetName + " did not reach goal. Made it to " + PlannerTools.getEndPosition(plan) + ", trying to get to " + goal;
-      }
+
+      double nominalYaw = FootstepNode.computeNominalYaw(finalSteps.get(RobotQuadrant.FRONT_LEFT).getX(), finalSteps.get(RobotQuadrant.FRONT_LEFT).getY(),
+                                                         finalSteps.get(RobotQuadrant.FRONT_RIGHT).getX(), finalSteps.get(RobotQuadrant.FRONT_RIGHT).getY(),
+                                                         finalSteps.get(RobotQuadrant.HIND_LEFT).getX(), finalSteps.get(RobotQuadrant.HIND_LEFT).getY(),
+                                                         finalSteps.get(RobotQuadrant.HIND_RIGHT).getX(), finalSteps.get(RobotQuadrant.HIND_RIGHT).getY());
+
+      centerPoint.scale(0.25);
+
+      String errorMessage = "";
+      if (!goalPosition.epsilonEquals(centerPoint, epsilon))
+         errorMessage = datasetName + " did not reach goal position. Made it to " + centerPoint + ", trying to get to " + goalPosition;
+      if (!MathTools.epsilonEquals(goalYaw, nominalYaw, 0.02))
+         errorMessage = datasetName + " did not reach goal yaw. Made it to " + nominalYaw + ", trying to get to " + goalYaw;
 
       if((VISUALIZE || DEBUG) && !errorMessage.isEmpty())
          LogTools.error(errorMessage);
@@ -210,125 +237,19 @@ public abstract class FootstepPlannerDataSetTest
       return errorMessage;
    }
 
-
-   private double totalTimeTaken;
-
-   private String waitForResult(double maxTimeToWait, String prefix)
+   private static QuadrantDependentList<Point3DBasics> getFinalStepPositions(List<? extends QuadrupedTimedStep> plannedSteps)
    {
-      long waitTime = 10;
-
-      while (actualResult.get() == null)
+      QuadrantDependentList<Point3DBasics> finalSteps = new QuadrantDependentList<>();
+      for (int i = plannedSteps.size() - 1; i >= 0; i--)
       {
-         queryUIResults();
-         queryPlannerResults();
-
-         if (totalTimeTaken > maxTimeToWait)
-         {
-            return prefix + " timed out waiting for a result.\n";
-         }
-
-         ThreadTools.sleep(waitTime);
-         totalTimeTaken += Conversions.millisecondsToSeconds(waitTime);
+         QuadrupedTimedStep step = plannedSteps.get(i);
+         if (finalSteps.containsKey(step.getRobotQuadrant()))
+            continue;
+         else
+            finalSteps.put(step.getRobotQuadrant(), step.getGoalPosition());
       }
 
-      return "";
-   }
-
-   private String validateResult(String prefix)
-   {
-      if (!actualResult.get().validForExecution())
-      {
-         return prefix + " failed to find a valid result. Result : " + actualResult.get() + "\n";
-      }
-      else
-      {
-         return "";
-      }
-   }
-
-   private String waitForPlan(double maxTimeToWait, String prefix)
-   {
-      while (actualPlan.get() == null)
-      {
-         queryUIResults();
-         queryPlannerResults();
-         long waitTime = 10;
-
-         if (totalTimeTaken > maxTimeToWait)
-         {
-            return prefix + " timed out waiting on plan.\n";
-         }
-
-         ThreadTools.sleep(waitTime);
-         totalTimeTaken += Conversions.millisecondsToSeconds(waitTime);
-      }
-
-      return "";
-   }
-
-   private void queryUIResults()
-   {
-      if (uiReceivedPlan.get() && uiFootstepPlanReference.get() != null && actualPlan.get() == null)
-      {
-         if (DEBUG)
-            LogTools.info("Received a plan from the UI.");
-         actualPlan.set(uiFootstepPlanReference.getAndSet(null));
-         uiReceivedPlan.set(false);
-      }
-
-      if (uiReceivedResult.get() && uiPlanningResultReference.get() != null)
-      {
-         if (DEBUG)
-            LogTools.info("Received a result " + uiPlanningResultReference.get() + " from the UI.");
-         actualResult.set(uiPlanningResultReference.getAndSet(null));
-         uiReceivedResult.set(false);
-      }
-   }
-
-   private void queryPlannerResults()
-   {
-      if (plannerReceivedPlan.get() && plannerPlanReference.get() != null && expectedPlan.get() == null)
-      {
-         if (DEBUG)
-            LogTools.info("Received a plan from the planner.");
-         expectedPlan.set(plannerPlanReference.getAndSet(null));
-         plannerReceivedPlan.set(false);
-      }
-
-      if (plannerReceivedResult.get() && plannerResultReference.get() != null)
-      {
-         if (DEBUG)
-            LogTools.info("Received a result " + plannerResultReference.get() + " from the planner.");
-         expectedResult.set(plannerResultReference.getAndSet(null));
-         plannerReceivedResult.set(false);
-      }
-   }
-
-   private String findPlanAndAssertGoodResult(FootstepPlannerUnitTestDataset dataset)
-   {
-      totalTimeTaken = 0.0;
-      double timeoutMultiplier = ContinuousIntegrationTools.isRunningOnContinuousIntegrationServer() ? bambooTimeScaling : 1.0;
-      double maxTimeToWait = 2.0 * timeoutMultiplier * dataset.getTimeout(getPlannerType());
-      String datasetName = dataset.getDatasetName();
-
-      String errorMessage = waitForResult(maxTimeToWait, datasetName);
-      if (!errorMessage.isEmpty())
-         return errorMessage;
-
-      errorMessage = validateResult(datasetName);
-      if (!errorMessage.isEmpty())
-         return errorMessage;
-
-      errorMessage = waitForPlan(maxTimeToWait, datasetName);
-      if (!errorMessage.isEmpty())
-         return errorMessage;
-
-      FootstepPlanningResult result = actualResult.getAndSet(null);
-      FootstepPlan plan = actualPlan.getAndSet(null);
-      errorMessage = assertPlanIsValid(datasetName, result, plan, dataset.getGoal());
-
-      ThreadTools.sleep(1000);
-      return errorMessage;
+      return finalSteps;
    }
 
    protected interface DatasetTestRunner
