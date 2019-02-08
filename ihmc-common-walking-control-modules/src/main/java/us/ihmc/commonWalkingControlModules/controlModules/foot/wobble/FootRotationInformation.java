8cf38e65a0e9de026b9a8cdf66171ed195978148
Operator Computer <OperatorComputer@unknown>
2019-02-08 10:04:25 -0600

    Fix exception by switching MutableBoolean to AtomicBoolean.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/wobble/FootRotationInformation.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/wobble/FootRotationInformation.java
@@ -1,6 +1,6 @@
 package us.ihmc.commonWalkingControlModules.controlModules.foot.wobble;
 
-import org.apache.commons.lang3.mutable.MutableBoolean;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DBasics;
@@ -10,35 +10,35 @@ import us.ihmc.robotics.robotSide.SideDependentList;
 
 public class FootRotationInformation
 {
-   private final SideDependentList<MutableBoolean> isRotating = new SideDependentList<MutableBoolean>(new MutableBoolean(), new MutableBoolean());
-   private final SideDependentList<MutableBoolean> hasAdjusted = new SideDependentList<MutableBoolean>(new MutableBoolean(), new MutableBoolean());
+   private final SideDependentList<AtomicBoolean> isRotating = new SideDependentList<>(new AtomicBoolean(), new AtomicBoolean());
+   private final SideDependentList<AtomicBoolean> hasAdjusted = new SideDependentList<>(new AtomicBoolean(), new AtomicBoolean());
    private final SideDependentList<FramePoint3DBasics> desiredCoPs = new SideDependentList<FramePoint3DBasics>(new FramePoint3D(), new FramePoint3D());
 
    public void setRotating(RobotSide side, FramePoint3DReadOnly desiredCop)
    {
-      isRotating.get(side).setTrue();
+      isRotating.get(side).set(true);
       desiredCoPs.get(side).setIncludingFrame(desiredCop);
    }
 
    public void setAdjusted(RobotSide side)
    {
-      hasAdjusted.get(side).setTrue();
+      hasAdjusted.get(side).set(true);
    }
 
    public boolean hasAdjusted(RobotSide side)
    {
-      return hasAdjusted.get(side).booleanValue();
+      return hasAdjusted.get(side).get();
    }
 
    public void reset(RobotSide side)
    {
-      isRotating.get(side).setFalse();
-      hasAdjusted.get(side).setFalse();
+      isRotating.get(side).set(false);
+      hasAdjusted.get(side).set(false);
    }
 
    public boolean isRotating(RobotSide side)
    {
-      return isRotating.get(side).booleanValue();
+      return isRotating.get(side).get();
    }
 
    public FramePoint3DReadOnly getDesiredCoP(RobotSide side)
