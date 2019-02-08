279e434465b0bc458020ddb823039c32096dd39e
Georg Wiedebach <gwiedebach@ihmc.us>
2019-02-08 14:13:05 -0600

    Fix the messages.

--- atlas/src/main/java/us/ihmc/atlas/jfxvisualizer/AtlasRemoteFootstepPlannerUI.java
+++ atlas/src/main/java/us/ihmc/atlas/jfxvisualizer/AtlasRemoteFootstepPlannerUI.java
@@ -1,6 +1,6 @@
 package us.ihmc.atlas.jfxvisualizer;
 
-import controller_msgs.msg.dds.HighLevelStateMessage;
+import controller_msgs.msg.dds.AtlasLowLevelControlModeMessage;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
@@ -9,12 +9,17 @@ import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.footstepPlanning.MultiStageFootstepPlanningModule;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ControllerAPIDefinition;
+import us.ihmc.communication.IHMCRealtimeROS2Publisher;
+import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.controllerAPI.RobotLowLevelMessenger;
 import us.ihmc.footstepPlanning.communication.FootstepPlannerMessagerAPI;
 import us.ihmc.footstepPlanning.ui.FootstepPlannerUI;
 import us.ihmc.footstepPlanning.ui.RemoteUIMessageConverter;
+import us.ihmc.humanoidRobotics.communication.packets.atlas.AtlasLowLevelControlMode;
 import us.ihmc.javaFXToolkit.messager.SharedMemoryJavaFXMessager;
 import us.ihmc.pubsub.DomainFactory;
+import us.ihmc.ros2.RealtimeRos2Node;
 
 /**
  * This class provides a visualizer for the remote footstep planner found in the footstep planner
@@ -36,14 +41,17 @@ public class AtlasRemoteFootstepPlannerUI extends Application
       DRCRobotModel drcRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.REAL_ROBOT, false);
       DRCRobotModel previewRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.REAL_ROBOT, false);
       messager = new SharedMemoryJavaFXMessager(FootstepPlannerMessagerAPI.API);
-      messageConverter = RemoteUIMessageConverter.createConverter(messager, drcRobotModel.getSimpleRobotName(), DomainFactory.PubSubImplementation.FAST_RTPS);
+
+      RealtimeRos2Node ros2Node = ROS2Tools.createRealtimeRos2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "ihmc_footstep_planner_ui");
+      AtlasLowLevelMessenger robotLowLevelMessenger = new AtlasLowLevelMessenger(ros2Node, drcRobotModel.getSimpleRobotName());
+      messageConverter = new RemoteUIMessageConverter(ros2Node, messager, drcRobotModel.getSimpleRobotName());
 
       messager.startMessager();
 
       ui = FootstepPlannerUI.createMessagerUI(primaryStage, messager, drcRobotModel.getFootstepPlannerParameters(),
                                               drcRobotModel.getVisibilityGraphsParameters(), drcRobotModel, previewRobotModel,
                                               drcRobotModel.getContactPointParameters(), drcRobotModel.getWalkingControllerParameters());
-      ui.setRobotLowLevelMessenger(new AtlasLowLevelMessenger());
+      ui.setRobotLowLevelMessenger(robotLowLevelMessenger);
       ui.show();
 
       if (launchPlannerToolbox)
@@ -71,20 +79,28 @@ public class AtlasRemoteFootstepPlannerUI extends Application
 
    private class AtlasLowLevelMessenger implements RobotLowLevelMessenger
    {
+      private final IHMCRealtimeROS2Publisher<AtlasLowLevelControlModeMessage> lowLevelModePublisher;
+
+      public AtlasLowLevelMessenger(RealtimeRos2Node ros2Node, String robotName)
+      {
+         lowLevelModePublisher = ROS2Tools.createPublisher(ros2Node, AtlasLowLevelControlModeMessage.class,
+                                                           ControllerAPIDefinition.getSubscriberTopicNameGenerator(robotName));
+      }
+
       @Override
       public void sendFreezeRequest()
       {
-         HighLevelStateMessage message = new HighLevelStateMessage();
-         message.setHighLevelControllerName(HighLevelStateMessage.FREEZE_STATE);
-         messager.submitMessage(FootstepPlannerMessagerAPI.HighLevelStateTopic, message);
+         AtlasLowLevelControlModeMessage message = new AtlasLowLevelControlModeMessage();
+         message.setRequestedAtlasLowLevelControlMode(AtlasLowLevelControlMode.FREEZE.toByte());
+         lowLevelModePublisher.publish(message);
       }
 
       @Override
       public void sendStandRequest()
       {
-         HighLevelStateMessage message = new HighLevelStateMessage();
-         message.setHighLevelControllerName(HighLevelStateMessage.STAND_PREP_STATE);
-         messager.submitMessage(FootstepPlannerMessagerAPI.HighLevelStateTopic, message);
+         AtlasLowLevelControlModeMessage message = new AtlasLowLevelControlModeMessage();
+         message.setRequestedAtlasLowLevelControlMode(AtlasLowLevelControlMode.STAND_PREP.toByte());
+         lowLevelModePublisher.publish(message);
       }
    }
 }
