de3c853a9268b232da07f378a4335a9d1270e31f
Robert <rgriffin@ihmc.us>
2019-02-13 12:13:41 -0600

    did a bunch of cleanup

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/stepCost/EuclideanDistanceAndYawBasedCost.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/stepCost/EuclideanDistanceAndYawBasedCost.java
@@ -1,10 +1,8 @@
 package us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.stepCost;
 
-import us.ihmc.commons.MathTools;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph.FootstepNode;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.parameters.FootstepPlannerParameters;
 import us.ihmc.robotics.geometry.AngleTools;
-import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public class EuclideanDistanceAndYawBasedCost implements FootstepCost
 {
@@ -18,12 +16,8 @@ public class EuclideanDistanceAndYawBasedCost implements FootstepCost
    @Override
    public double compute(FootstepNode startNode, FootstepNode endNode)
    {
-      RobotQuadrant robotQuadrant = endNode.getMovingQuadrant();
-//      double euclideanDistance = Math.sqrt(MathTools.square(startNode.getX(robotQuadrant) - endNode.getX(robotQuadrant)) + MathTools
-//            .square(startNode.getY(robotQuadrant) - endNode.getY(robotQuadrant)));
       double euclideanDistance = startNode.getOrComputeXGaitCenterPoint().distance(endNode.getOrComputeXGaitCenterPoint());
 
-//      double yaw = AngleTools.computeAngleDifferenceMinusPiToPi(startNode.getYaw(), endNode.getYaw());
       double yaw = AngleTools.computeAngleDifferenceMinusPiToPi(startNode.getNominalYaw(), endNode.getNominalYaw());
       double distanceWeight = 0.5 * (parameters.getForwardWeight() + parameters.getLateralWeight());
       double totalCost = distanceWeight * euclideanDistance + parameters.getYawWeight() * Math.abs(yaw) + parameters.getCostPerStep();
