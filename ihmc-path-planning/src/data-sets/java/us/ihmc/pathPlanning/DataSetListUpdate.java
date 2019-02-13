0c338205d5e4508c7e196c9946055b0861223859
smccrory <smccrory@ihmc.us>
2019-02-13 11:00:44 -0600

    Adding file containing list of data set names and a main to generate name list

---
+++ ihmc-path-planning/src/data-sets/java/us/ihmc/pathPlanning/DataSetListUpdate.java
@@ -0,0 +1,31 @@
+package us.ihmc.pathPlanning;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * When adding/removing datasets, run this and copy output to dataSetList.txt
+ */
+public class DataSetListUpdate
+{
+   private DataSetListUpdate() throws IOException
+   {
+      InputStream dataSetStream = getClass().getResourceAsStream("/" + DataSetLoader.DATA_SET_DIRECTORY_PATH);
+      List<String> dataSetNames = IOUtils.readLines(dataSetStream, UTF_8);
+
+      for (int i = 0; i < dataSetNames.size(); i++)
+      {
+         System.out.println(dataSetNames.get(i));
+      }
+   }
+
+   public static void main(String[] args) throws IOException
+   {
+      new DataSetListUpdate();
+   }
+}
