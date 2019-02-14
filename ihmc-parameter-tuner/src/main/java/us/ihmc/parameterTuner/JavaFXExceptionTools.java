127d352aa394eddcae1d9d0abb20c1b018cc733a
Georg Wiedebach <gwiedebach@ihmc.us>
2019-02-14 10:06:53 -0600

    Add nicer exception handing to the JavaFX GUI to display an alert to the user.

---
+++ ihmc-parameter-tuner/src/main/java/us/ihmc/parameterTuner/JavaFXExceptionTools.java
@@ -0,0 +1,29 @@
+package us.ihmc.parameterTuner;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Region;
+
+public class JavaFXExceptionTools
+{
+   public static void setupExceptionHandling()
+   {
+      Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+         createExceptionDialog(e);
+      });
+   }
+
+   private static void createExceptionDialog(Throwable e)
+   {
+      e.printStackTrace();
+
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("Exception");
+      alert.setHeaderText("An exception was thrown.\nSee console for stacktrace.");
+      alert.setContentText(e.getMessage());
+      alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+      alert.getDialogPane().setMinWidth(Region.USE_PREF_SIZE);
+      alert.resizableProperty().set(true);
+      alert.showAndWait();
+   }
+}
