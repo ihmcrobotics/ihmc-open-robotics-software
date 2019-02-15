96518651341cf366e6e8043134f3799a65a521a0
Duncan Calvert <dcalvert@ihmc.us>
2019-02-15 13:14:32 -0600

    AtlasBehaviorUI launches with robot model and buttons showing.

---
+++ atlas/src/main/java/us/ihmc/atlas/jfxvisualizer/AtlasBehaviorUI.java
@@ -0,0 +1,64 @@
+package us.ihmc.atlas.jfxvisualizer;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.footstepPlanning.communication.FootstepPlannerMessagerAPI;
+import us.ihmc.humanoidBehaviors.ui.BehaviorUI;
+import us.ihmc.javaFXToolkit.messager.SharedMemoryJavaFXMessager;
+import us.ihmc.pubsub.DomainFactory;
+import us.ihmc.ros2.RealtimeRos2Node;
+
+public class AtlasBehaviorUI extends Application
+{
+   private static final boolean launchBehaviorModule = false;
+
+   private SharedMemoryJavaFXMessager messager;
+
+   private BehaviorUI ui;
+
+   @Override
+   public void start(Stage primaryStage) throws Exception
+   {
+      DRCRobotModel drcRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.REAL_ROBOT, false);
+      messager = new SharedMemoryJavaFXMessager(FootstepPlannerMessagerAPI.API);
+
+      RealtimeRos2Node ros2Node = ROS2Tools.createRealtimeRos2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "ihmc_footstep_planner_ui");
+
+      messager.startMessager();
+
+      ui = BehaviorUI.createMessagerUI(primaryStage, messager,
+                                       drcRobotModel.getFootstepPlannerParameters(),
+                                       drcRobotModel.getVisibilityGraphsParameters(),
+                                       drcRobotModel,
+                                       drcRobotModel.getContactPointParameters(),
+                                       drcRobotModel.getWalkingControllerParameters());
+      ui.show();
+
+      if (launchBehaviorModule)
+      {
+         // launch behavior module
+      }
+   }
+
+   @Override
+   public void stop() throws Exception
+   {
+      super.stop();
+
+      messager.closeMessager();
+      ui.stop();
+
+      Platform.exit();
+   }
+
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+}
---
+++ ihmc-humanoid-behaviors/src/fx-ui/java/us/ihmc/humanoidBehaviors/ui/BehaviorUI.java
@@ -0,0 +1,103 @@
+package us.ihmc.humanoidBehaviors.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParameters;
+import us.ihmc.humanoidBehaviors.ui.controllers.PatrolUIController;
+import us.ihmc.javaFXToolkit.messager.JavaFXMessager;
+import us.ihmc.javaFXToolkit.scenes.View3DFactory;
+import us.ihmc.javaFXVisualizers.JavaFXRobotVisualizer;
+import us.ihmc.pathPlanning.visibilityGraphs.interfaces.VisibilityGraphsParameters;
+import us.ihmc.pathPlanning.visibilityGraphs.ui.viewers.PlanarRegionViewer;
+import us.ihmc.robotModels.FullHumanoidRobotModelFactory;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.wholeBodyController.RobotContactPointParameters;
+
+import static us.ihmc.footstepPlanning.communication.FootstepPlannerMessagerAPI.*;
+
+/**
+ * This class constructs a UI for behavior operation.
+ */
+public class BehaviorUI
+{
+   private final JavaFXMessager messager;
+   private final Stage primaryStage;
+   private final BorderPane mainPane;
+
+   private final PlanarRegionViewer planarRegionViewer;
+   private final JavaFXRobotVisualizer robotVisualizer;
+
+   @FXML private PatrolUIController patrolUIController;
+
+   public BehaviorUI(Stage primaryStage, JavaFXMessager messager, FootstepPlannerParameters plannerParameters, VisibilityGraphsParameters visibilityGraphsParameters, FullHumanoidRobotModelFactory fullHumanoidRobotModelFactory, RobotContactPointParameters<RobotSide> contactPointParameters, WalkingControllerParameters walkingControllerParameters) throws Exception
+   {
+      this.primaryStage = primaryStage;
+      this.messager = messager;
+
+      FXMLLoader loader = new FXMLLoader();
+      loader.setController(this);
+      loader.setLocation(getClass().getResource(getClass().getSimpleName() + ".fxml"));
+
+      mainPane = loader.load();
+
+      patrolUIController.attachMessager(messager);
+
+      View3DFactory view3dFactory = View3DFactory.createSubscene();
+      view3dFactory.addCameraController(true);
+      view3dFactory.addWorldCoordinateSystem(0.3);
+      Pane subScene = view3dFactory.getSubSceneWrappedInsidePane();
+
+      this.planarRegionViewer = new PlanarRegionViewer(messager, PlanarRegionDataTopic, ShowPlanarRegionsTopic);
+
+      if(fullHumanoidRobotModelFactory == null)
+      {
+         robotVisualizer = null;
+      }
+      else
+      {
+         robotVisualizer = new JavaFXRobotVisualizer(fullHumanoidRobotModelFactory);
+         messager.registerTopicListener(RobotConfigurationDataTopic, robotVisualizer::submitNewConfiguration);
+         patrolUIController.setFullRobotModel(robotVisualizer.getFullRobotModel());
+         view3dFactory.addNodeToView(robotVisualizer.getRootNode());
+         robotVisualizer.start();
+      }
+
+      mainPane.setCenter(subScene);
+      primaryStage.setTitle(getClass().getSimpleName());
+      primaryStage.setMaximized(true);
+      Scene mainScene = new Scene(mainPane, 600, 400);
+
+      primaryStage.setScene(mainScene);
+      primaryStage.setOnCloseRequest(event -> stop());
+   }
+
+   public void show()
+   {
+      primaryStage.show();
+   }
+
+   public void stop()
+   {
+      planarRegionViewer.stop();
+
+      if(robotVisualizer != null)
+         robotVisualizer.stop();
+   }
+
+   public static BehaviorUI createMessagerUI(Stage primaryStage, JavaFXMessager messager,
+                                             FootstepPlannerParameters plannerParameters,
+                                             VisibilityGraphsParameters visibilityGraphsParameters,
+                                             FullHumanoidRobotModelFactory fullHumanoidRobotModelFactory,
+                                             RobotContactPointParameters<RobotSide> contactPointParameters,
+                                             WalkingControllerParameters walkingControllerParameters)
+         throws Exception
+   {
+      return new BehaviorUI(primaryStage, messager, plannerParameters, visibilityGraphsParameters, fullHumanoidRobotModelFactory,
+                                   contactPointParameters, walkingControllerParameters);
+   }
+}
