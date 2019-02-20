1ed3ad2e0940c6eac1ab8d8b3166e921d5d30674
smccrory <smccrory@ihmc.us>
2019-02-20 15:18:06 -0600

    adding bollards and corridor tests

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/AStarToolboxDataSetTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/AStarToolboxDataSetTest.java
@@ -6,6 +6,10 @@ import us.ihmc.commons.thread.ThreadTools;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Disabled;
 import us.ihmc.footstepPlanning.FootstepPlannerType;
+import us.ihmc.footstepPlanning.communication.FootstepPlannerMessagerAPI;
+import us.ihmc.footstepPlanning.graphSearch.parameters.DefaultFootstepPlanningParameters;
+import us.ihmc.pathPlanning.DataSet;
+import us.ihmc.pathPlanning.DataSetIOTools;
 import us.ihmc.pubsub.DomainFactory;
 
 public class AStarToolboxDataSetTest extends FootstepPlannerToolboxDataSetTest
@@ -21,6 +25,53 @@ public class AStarToolboxDataSetTest extends FootstepPlannerToolboxDataSetTest
    public void testDatasetsWithoutOcclusion()
    {
       super.testDatasetsWithoutOcclusion();
+   }
+
+   @Test
+   public void testCorridor()
+   {
+      DataSet dataSet = DataSetIOTools.loadDataSet("20190219_182005_Corridor");
+
+      messager.submitMessage(FootstepPlannerMessagerAPI.PlannerParametersTopic, new DefaultFootstepPlanningParameters()
+      {
+         @Override public boolean checkForBodyBoxCollisions()
+         {
+            return true;
+         }
+      });
+
+      runAssertions(dataSet);
+   }
+
+   @Test
+   public void testBetweenTwoBollards()
+   {
+      DataSet dataSet = DataSetIOTools.loadDataSet("20190219_182005_Bollards");
+
+      messager.submitMessage(FootstepPlannerMessagerAPI.PlannerParametersTopic, new DefaultFootstepPlanningParameters()
+      {
+         @Override public boolean checkForBodyBoxCollisions()
+         {
+            return true;
+         }
+
+         @Override public double getBodyBoxDepth()
+         {
+            return 0.45;
+         }
+
+         @Override public double getBodyBoxWidth()
+         {
+            return 0.9;
+         }
+
+         @Override public double getBodyBoxBaseX()
+         {
+            return 0.1;
+         }
+      });
+
+      runAssertions(dataSet);
    }
 
    public static void main(String[] args) throws Exception
