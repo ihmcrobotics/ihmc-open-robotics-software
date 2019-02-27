1a6d58f3720ea2fc4a285b4133b3ed492ef8602a
Sylvain Bertrand <sbertrand@ihmc.us>
2019-02-27 11:19:46 -0600

    Addressed a bunch of warnings in the controller core.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/virtualModelControl/VirtualModelControlCommand.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/virtualModelControl/VirtualModelControlCommand.java
@@ -2,7 +2,7 @@ package us.ihmc.commonWalkingControlModules.controllerCore.command.virtualModelC
 
 import us.ihmc.commonWalkingControlModules.controllerCore.command.ControllerCoreCommandType;
 
-public interface VirtualModelControlCommand<T extends VirtualModelControlCommand>
+public interface VirtualModelControlCommand<T extends VirtualModelControlCommand<T>>
 {
    void set(T other);
 
