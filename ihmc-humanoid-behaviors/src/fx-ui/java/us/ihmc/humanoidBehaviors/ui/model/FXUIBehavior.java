83ddf50c2fbacb7e3fbbc408f4a3b9f440d8babe
Duncan Calvert <dcalvert@ihmc.us>
2019-02-28 10:18:45 -0600

    Remove graphics ArrayList.

--- ihmc-humanoid-behaviors/src/fx-ui/java/us/ihmc/humanoidBehaviors/ui/model/FXUIBehavior.java
+++ ihmc-humanoid-behaviors/src/fx-ui/java/us/ihmc/humanoidBehaviors/ui/model/FXUIBehavior.java
@@ -4,25 +4,19 @@ import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Node;
 
-import java.util.ArrayList;
-
 public class FXUIBehavior
 {
-   private final ArrayList<FXUIGraphic> graphics = new ArrayList<>();
-
    private final Group root = new Group();
    protected final ObservableList<Node> rootChildren = root.getChildren();
 
    protected void registerGraphic(FXUIGraphic graphic)
    {
-      graphics.add(graphic);
       rootChildren.add(graphic.getRoot());
    }
 
    protected void removeGraphic(FXUIGraphic graphic)
    {
       rootChildren.remove(graphic.getRoot());
-      graphics.remove(graphic);
    }
 
    public Group getRoot()
