1e1f7d16d9c82b702dda8a7885320d21b729ce83
Robert <rgriffin@ihmc.us>
2019-03-02 09:51:24 -0600

    set up a test that has a flight phase and exposes a big in the biped contact sequence updater

--- ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/capturePoint/comBasedPlanner/BipedContactSequenceUpdaterTest.java
+++ ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/capturePoint/comBasedPlanner/BipedContactSequenceUpdaterTest.java
@@ -266,5 +266,96 @@ public class BipedContactSequenceUpdaterTest
       assertEquals(Double.POSITIVE_INFINITY, contactSequence.get(4).getTimeInterval().getEndTime(), epsilon);
    }
 
+   @Test
+   public void testTwoSimpleStepsWithFlight()
+   {
+      double stepLength = 0.2;
+      double nominalWidth = 0.5;
 
+      SideDependentList<MovingReferenceFrame> soleFrames = createSimpleSoleFrames(nominalWidth);
+      BipedContactSequenceUpdater contactSequenceUpdater = new BipedContactSequenceUpdater(soleFrames, null, null);
+
+      List<RobotSide> currentFeetInContact = new ArrayList<>();
+      for (RobotSide quadrant : RobotSide.values)
+         currentFeetInContact.add(quadrant);
+
+      Point3D leftFootFirstStep = new Point3D(stepLength, nominalWidth / 2.0, 0.0);
+
+      BipedTimedStep step1 = new BipedTimedStep();
+      step1.setGoalPose(new FramePoint3D(ReferenceFrame.getWorldFrame(), leftFootFirstStep), new FrameQuaternion());
+      step1.setRobotSide(RobotSide.LEFT);
+      step1.setTimeInterval(new TimeInterval(0.5, 1.0));
+
+      Point3D rightFootFirstStep = new Point3D(stepLength, -nominalWidth / 2.0, 0.0);
+
+      BipedTimedStep step2 = new BipedTimedStep();
+      step2.setGoalPose(new FramePoint3D(ReferenceFrame.getWorldFrame(), rightFootFirstStep), new FrameQuaternion());
+      step2.setRobotSide(RobotSide.RIGHT);
+      step2.setTimeInterval(new TimeInterval(0.8, 1.3));
+
+      List<BipedTimedStep> stepList = new ArrayList<>();
+      stepList.add(step1);
+      stepList.add(step2);
+
+      contactSequenceUpdater.update(stepList, currentFeetInContact, 0.0);
+
+      List<SimpleBipedContactPhase> contactSequence = contactSequenceUpdater.getContactSequence();
+
+      assertEquals(5, contactSequence.size());
+
+      Point3D rightFootFirstStance = new Point3D(0.0, -nominalWidth / 2.0, 0.0);
+      Point3D leftFootFirstStance = new Point3D(0.0, nominalWidth / 2.0, 0.0);
+      Point3D leftFootSecondStance = new Point3D(stepLength, nominalWidth / 2.0, 0.0);
+      Point3D rightFootSecondStance = new Point3D(stepLength, -nominalWidth / 2.0, 0.0);
+      Point3D initialStance = new Point3D();
+      Point3D finalStance = new Point3D();
+      initialStance.interpolate(leftFootFirstStance, rightFootFirstStance, 0.5);
+      finalStance.interpolate(leftFootSecondStance, rightFootSecondStance, 0.5);
+
+      // test first contact state, initial contact, center of feet
+      assertEquals(ContactState.IN_CONTACT, contactSequence.get(0).getContactState());
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(initialStance, contactSequence.get(0).getCopStartPosition(), epsilon);
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(rightFootFirstStance, contactSequence.get(0).getCopEndPosition(), epsilon);
+      assertEquals(2, contactSequence.get(0).getFeetInContact().size());
+      assertTrue(contactSequence.get(0).getFeetInContact().contains(RobotSide.LEFT));
+      assertTrue(contactSequence.get(0).getFeetInContact().contains(RobotSide.RIGHT));
+      assertEquals(0.0, contactSequence.get(0).getTimeInterval().getStartTime(), epsilon);
+      assertEquals(0.5, contactSequence.get(0).getTimeInterval().getEndTime(), epsilon);
+
+      // test second contact state, right foot stance
+      assertEquals(ContactState.IN_CONTACT, contactSequence.get(1).getContactState());
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(rightFootFirstStance, contactSequence.get(1).getCopStartPosition(), epsilon);
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(rightFootFirstStance, contactSequence.get(1).getCopEndPosition(), epsilon);
+      assertEquals(1, contactSequence.get(1).getFeetInContact().size());
+      assertTrue(contactSequence.get(1).getFeetInContact().contains(RobotSide.RIGHT));
+      assertEquals(0.5, contactSequence.get(1).getTimeInterval().getStartTime(), epsilon);
+      assertEquals(0.8, contactSequence.get(1).getTimeInterval().getEndTime(), epsilon);
+
+      // test third contact state, flight
+      assertEquals(ContactState.FLIGHT, contactSequence.get(2).getContactState());
+      assertEquals(0, contactSequence.get(2).getFeetInContact().size());
+//      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(leftFootSecondStance, contactSequence.get(2).getCopStartPosition(), epsilon);
+//      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(leftFootSecondStance, contactSequence.get(2).getCopEndPosition(), epsilon);
+      assertEquals(0.8, contactSequence.get(2).getTimeInterval().getStartTime(), epsilon);
+      assertEquals(1.0, contactSequence.get(2).getTimeInterval().getEndTime(), epsilon);
+
+      // test fourth contact state, left foot stance
+      assertEquals(ContactState.IN_CONTACT, contactSequence.get(3).getContactState());
+      assertEquals(1, contactSequence.get(3).getFeetInContact().size());
+      assertTrue(contactSequence.get(3).getFeetInContact().contains(RobotSide.LEFT));
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(leftFootSecondStance, contactSequence.get(3).getCopStartPosition(), epsilon);
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(leftFootSecondStance, contactSequence.get(3).getCopEndPosition(), epsilon);
+      assertEquals(1.0, contactSequence.get(3).getTimeInterval().getStartTime(), epsilon);
+      assertEquals(1.3, contactSequence.get(3).getTimeInterval().getEndTime(), epsilon);
+
+      // test fourth contact state, final transfer
+      assertEquals(ContactState.IN_CONTACT, contactSequence.get(4).getContactState());
+      assertEquals(2, contactSequence.get(4).getFeetInContact().size());
+      assertTrue(contactSequence.get(4).getFeetInContact().contains(RobotSide.LEFT));
+      assertTrue(contactSequence.get(4).getFeetInContact().contains(RobotSide.RIGHT));
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(leftFootSecondStance, contactSequence.get(4).getCopStartPosition(), epsilon);
+      EuclidCoreTestTools.assertPoint3DGeometricallyEquals(finalStance, contactSequence.get(4).getCopEndPosition(), epsilon);
+      assertEquals(1.3, contactSequence.get(4).getTimeInterval().getStartTime(), epsilon);
+      assertEquals(Double.POSITIVE_INFINITY, contactSequence.get(4).getTimeInterval().getEndTime(), epsilon);
+   }
 }
