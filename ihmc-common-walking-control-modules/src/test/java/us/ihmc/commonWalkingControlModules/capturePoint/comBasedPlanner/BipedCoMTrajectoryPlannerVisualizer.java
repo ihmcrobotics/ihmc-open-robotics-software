33aea30fa64dca1cacbe2f21155cede75f62e15a
Robert <rgriffin@ihmc.us>
2019-03-04 16:55:15 -0600

    got a cooler running demo

--- ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/capturePoint/comBasedPlanner/BipedCoMTrajectoryPlannerVisualizer.java
+++ ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/capturePoint/comBasedPlanner/BipedCoMTrajectoryPlannerVisualizer.java
@@ -4,17 +4,14 @@ import us.ihmc.commons.InterpolationTools;
 import us.ihmc.commons.MathTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
-import us.ihmc.euclid.referenceFrame.FrameConvexPolygon2D;
-import us.ihmc.euclid.referenceFrame.FramePoint2D;
-import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.*;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.graphicsDescription.Graphics3DObject;
+import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
 import us.ihmc.graphicsDescription.appearance.YoAppearance;
 import us.ihmc.graphicsDescription.yoGraphics.*;
 import us.ihmc.graphicsDescription.yoGraphics.plotting.YoArtifactPolygon;
-import us.ihmc.humanoidRobotics.footstep.FootSpoof;
 import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
@@ -108,6 +105,8 @@ public class BipedCoMTrajectoryPlannerVisualizer
 
    private final ConvexPolygon2D footPolygon = new ConvexPolygon2D();
 
+   private final Graphics3DObject worldGraphics = new Graphics3DObject();
+
    public BipedCoMTrajectoryPlannerVisualizer()
    {
       YoGraphicsListRegistry yoGraphicsListRegistry = new YoGraphicsListRegistry();
@@ -182,7 +181,8 @@ public class BipedCoMTrajectoryPlannerVisualizer
       nextFootstepPolygons.add(yoNextNextNextFootstepPolygon);
 
       planner = new BipedCoMTrajectoryPlanner(soleFrames, omega, gravity, nominalHeight, registry, yoGraphicsListRegistry);
-      steps = createSteps(soleFrames);
+//      steps = createSteps(soleFrames);
+      steps = createFancySteps(soleFrames);
 
       simDuration = steps.get(steps.size() - 1).getTimeInterval().getEndTime() + extraSimDuration;
 
@@ -196,12 +196,11 @@ public class BipedCoMTrajectoryPlannerVisualizer
       scs.setDT(simDt, 1);
       scs.addYoVariableRegistry(registry);
       scs.addYoGraphicsListRegistry(yoGraphicsListRegistry);
-      scs.setPlaybackRealTimeRate(0.25);
-      Graphics3DObject linkGraphics = new Graphics3DObject();
-      linkGraphics.addCoordinateSystem(0.3);
-      scs.addStaticLinkGraphics(linkGraphics);
-      scs.setCameraFix(0.0, 0.0, 0.5);
-      scs.setCameraPosition(-0.5, 0.0, 1.0);
+      scs.setPlaybackRealTimeRate(1.0);
+      worldGraphics.addCoordinateSystem(0.3);
+      scs.addStaticLinkGraphics(worldGraphics);
+      scs.setCameraFix(7.5, 0.0, 0.5);
+      scs.setCameraPosition(-2.0, 9.5, 6.0);
 
       SimulationOverheadPlotterFactory simulationOverheadPlotterFactory = scs.createSimulationOverheadPlotterFactory();
       simulationOverheadPlotterFactory.addYoGraphicsListRegistries(yoGraphicsListRegistry);
@@ -276,6 +275,238 @@ public class BipedCoMTrajectoryPlannerVisualizer
             stepStartTime += swingDuration - flightDuration;
          }
       }
+
+
+      return steps;
+   }
+
+   private List<BipedTimedStep> createFancySteps(SideDependentList<MovingReferenceFrame> soleFrames)
+   {
+      List<BipedTimedStep> steps = new ArrayList<>();
+
+      FramePose3D stepPose = new FramePose3D();
+      stepPose.setToZero(soleFrames.get(RobotSide.LEFT));
+      stepPose.changeFrame(worldFrame);
+
+      double stepStartTime = initialTransferTime;
+      double currentPosition = 0.0;
+      double currentHeight = 0.0;
+
+      worldGraphics.identity();
+      worldGraphics.translate(8.25, -0.3, 0.0);
+      worldGraphics.addCube(0.5, 0.6, 0.4, YoAppearance.Gray());
+
+      worldGraphics.identity();
+      worldGraphics.translate(8.75, 0.3, 0.0);
+      worldGraphics.addCube(0.5, 0.6, 0.8, YoAppearance.Gray());
+
+      worldGraphics.identity();
+      worldGraphics.translate(9.25 + 3.0, 0.0, 0.0);
+      worldGraphics.addCube(6.5, 1.5, 1.0, YoAppearance.Gray());
+
+
+      BipedTimedStep step1 = new BipedTimedStep();
+      step1.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step1.setRobotSide(RobotSide.LEFT);
+      step1.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.3, stanceWidth / 2.0, currentHeight), new FrameQuaternion());
+
+      steps.add(step1);
+
+      stepStartTime += swingDuration + stanceDuration;
+      currentPosition += 0.3;
+
+      BipedTimedStep step2 = new BipedTimedStep();
+      step2.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step2.setRobotSide(RobotSide.RIGHT);
+      step2.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.7, -stanceWidth / 2.0, currentHeight), new FrameQuaternion());
+
+      steps.add(step2);
+
+      stepStartTime += swingDuration + stanceDuration;
+      currentPosition += 0.7;
+
+      BipedTimedStep step3 = new BipedTimedStep();
+      step3.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step3.setRobotSide(RobotSide.LEFT);
+      step3.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.0, stanceWidth / 2.0, currentHeight), new FrameQuaternion());
+
+      steps.add(step3);
+
+      stepStartTime += swingDuration + stanceDuration;
+      currentPosition += 1.0;
+
+      BipedTimedStep step4 = new BipedTimedStep();
+      step4.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step4.setRobotSide(RobotSide.RIGHT);
+      step4.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.2, -stanceWidth / 2.0, currentHeight), new FrameQuaternion());
+
+      steps.add(step4);
+
+      stepStartTime += swingDuration - flightDuration;
+      currentPosition += 1.2;
+
+      BipedTimedStep step5 = new BipedTimedStep();
+      step5.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step5.setRobotSide(RobotSide.LEFT);
+      step5.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.5, stanceWidth / 2.0, currentHeight), new FrameQuaternion());
+
+      steps.add(step5);
+
+      stepStartTime += swingDuration - flightDuration;
+      currentPosition += 1.5;
+
+      BipedTimedStep step6 = new BipedTimedStep();
+      step6.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step6.setRobotSide(RobotSide.RIGHT);
+      step6.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.5, -stanceWidth / 2.0, currentHeight), new FrameQuaternion());
+
+      steps.add(step6);
+
+
+
+      stepStartTime += swingDuration - flightDuration;
+      currentPosition += 1.5;
+
+      BipedTimedStep step7 = new BipedTimedStep();
+      step7.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step7.setRobotSide(RobotSide.LEFT);
+      step7.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.5, stanceWidth / 2.0, currentHeight), new FrameQuaternion());
+
+      steps.add(step7);
+      stepStartTime += swingDuration - 1.5 * flightDuration;
+      currentPosition += 1.5;
+
+      BipedTimedStep step8 = new BipedTimedStep();
+      step8.getTimeInterval().setInterval(stepStartTime, stepStartTime + 0.2 + swingDuration);
+      step8.setRobotSide(RobotSide.RIGHT);
+      step8.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.5, -0.75 * stanceWidth, currentHeight + 0.4), new FrameQuaternion());
+
+      steps.add(step8);
+      stepStartTime += 0.2 + swingDuration - 1.5 * flightDuration;
+      currentPosition += 0.5;
+      currentHeight += 0.4;
+
+      BipedTimedStep step9 = new BipedTimedStep();
+      step9.getTimeInterval().setInterval(stepStartTime, stepStartTime + 0.2 + swingDuration);
+      step9.setRobotSide(RobotSide.LEFT);
+      step9.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.5, 0.75 * stanceWidth, currentHeight + 0.4), new FrameQuaternion());
+
+      steps.add(step9);
+      stepStartTime += 0.2 + swingDuration - 1.5 * flightDuration;
+      currentPosition += 0.5;
+      currentHeight += 0.4;
+
+      BipedTimedStep step10 = new BipedTimedStep();
+      step10.getTimeInterval().setInterval(stepStartTime, stepStartTime + 0.2 + swingDuration);
+      step10.setRobotSide(RobotSide.RIGHT);
+      step10.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.5, -0.5 * stanceWidth, currentHeight + 0.2), new FrameQuaternion());
+
+      steps.add(step10);
+
+      stepStartTime += 0.2 + swingDuration - 0.1;
+      currentPosition += 0.5;
+      currentHeight += 0.2;
+
+      BipedTimedStep step11 = new BipedTimedStep();
+      step11.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step11.setRobotSide(RobotSide.LEFT);
+      step11.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.75, 0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step11);
+
+      stepStartTime += swingDuration - 0.2; // slightly longer flight
+      currentPosition += 0.75;
+
+      // double jump
+      BipedTimedStep step12 = new BipedTimedStep();
+      step12.getTimeInterval().setInterval(stepStartTime, stepStartTime + (swingDuration + 0.1));
+      step12.setRobotSide(RobotSide.RIGHT);
+      step12.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.0, -0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step12);
+
+      // FIXME make these happen at the same time.
+      stepStartTime += 0.1;
+
+      BipedTimedStep step13 = new BipedTimedStep();
+      step13.getTimeInterval().setInterval(stepStartTime + 0.5 * swingDuration, stepStartTime + (swingDuration + 0.1)); // fixme remove that 0.1
+      step13.setRobotSide(RobotSide.LEFT);
+      step13.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.0, 0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step13);
+
+
+      stepStartTime += swingDuration + 0.1 + stanceDuration;
+      currentPosition += 1.0;
+
+
+      BipedTimedStep step14 = new BipedTimedStep();
+      step14.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration + 0.1 );
+      step14.setRobotSide(RobotSide.RIGHT);
+      step14.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.0, -0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step14);
+
+      stepStartTime += 0.1; // fixme remove
+
+      BipedTimedStep step15 = new BipedTimedStep();
+      step15.getTimeInterval().setInterval(stepStartTime + 0.1, stepStartTime + swingDuration + 0.15); // fixme remove 0.1
+      step15.setRobotSide(RobotSide.LEFT);
+      step15.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.0, 0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step15);
+
+
+      stepStartTime += swingDuration + 0.15 + stanceDuration;
+      currentPosition += 1.0;
+
+
+
+      BipedTimedStep step16 = new BipedTimedStep();
+      step16.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step16.setRobotSide(RobotSide.RIGHT);
+      step16.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.5, -0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step16);
+
+      stepStartTime += swingDuration + stanceDuration;
+      currentPosition += 0.5;
+
+      BipedTimedStep step17 = new BipedTimedStep();
+      step17.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step17.setRobotSide(RobotSide.LEFT);
+      step17.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.0, 0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step17);
+
+      stepStartTime += swingDuration - 0.1;
+      currentPosition += 1.0;
+
+      BipedTimedStep step18 = new BipedTimedStep();
+      step18.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step18.setRobotSide(RobotSide.RIGHT);
+      step18.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 1.0, -0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step18);
+
+      stepStartTime += swingDuration + stanceDuration;
+      currentPosition += 1.0;
+
+      BipedTimedStep step19 = new BipedTimedStep();
+      step19.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step19.setRobotSide(RobotSide.LEFT);
+      step19.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.7, 0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step19);
+
+      stepStartTime += swingDuration + stanceDuration;
+
+      BipedTimedStep step20 = new BipedTimedStep();
+      step20.getTimeInterval().setInterval(stepStartTime, stepStartTime + swingDuration);
+      step20.setRobotSide(RobotSide.RIGHT);
+      step20.setGoalPose(new FramePoint3D(worldFrame, currentPosition + 0.7, -0.5 * stanceWidth, currentHeight), new FrameQuaternion());
+
+      steps.add(step20);
 
 
       return steps;
