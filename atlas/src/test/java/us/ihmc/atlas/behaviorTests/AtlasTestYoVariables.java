edecb6a785286606d7218937eaa32ce19de6d79a
Duncan Calvert <dcalvert@ihmc.us>
2019-03-04 15:48:38 -0600

    Get a test working that uses GoalOrientedTestConductor to simulate for 1 second.

---
+++ atlas/src/test/java/us/ihmc/atlas/behaviorTests/AtlasTestYoVariables.java
@@ -0,0 +1,12 @@
+package us.ihmc.atlas.behaviorTests;
+
+import us.ihmc.avatar.AvatarTestYoVariables;
+import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+
+public class AtlasTestYoVariables extends AvatarTestYoVariables
+{
+   public AtlasTestYoVariables(SimulationConstructionSet scs)
+   {
+      super(scs);
+   }
+}
