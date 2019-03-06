78a01a6b6cc97c1153612ebd55f2319116eb99d7
Duncan Calvert <dcalvert@ihmc.us>
2019-03-06 11:16:41 -0600

    Improve/clean up Atlas behavior test.

--- atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasPatrolBehaviorTest.java
+++ atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasPatrolBehaviorTest.java
@@ -8,31 +8,22 @@ import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.factory.AvatarSimulation;
 import us.ihmc.avatar.factory.AvatarSimulationFactory;
 import us.ihmc.avatar.initialSetup.DRCGuiInitialSetup;
-import us.ihmc.avatar.initialSetup.DRCRobotInitialSetup;
 import us.ihmc.avatar.initialSetup.DRCSCSInitialSetup;
-import us.ihmc.commonWalkingControlModules.configurations.HighLevelControllerParameters;
-import us.ihmc.commonWalkingControlModules.configurations.ICPWithTimeFreezingPlannerParameters;
-import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ContactableBodiesFactory;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.HighLevelHumanoidControllerFactory;
 import us.ihmc.communication.ROS2Tools;
-import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName;
 import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.ros2.RealtimeRos2Node;
-import us.ihmc.sensorProcessing.parameters.DRCRobotSensorInformation;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
-import us.ihmc.simulationConstructionSetTools.util.HumanoidFloatingRootJointRobot;
+import us.ihmc.simulationConstructionSetTools.util.environments.CommonAvatarEnvironmentInterface;
 import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
 import us.ihmc.simulationConstructionSetTools.util.simulationrunner.GoalOrientedTestConductor;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.util.simulationTesting.SimulationTestingParameters;
 import us.ihmc.tools.MemoryTools;
 import us.ihmc.wholeBodyController.RobotContactPointParameters;
-
-import java.util.ArrayList;
 
 import static us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName.DO_NOTHING_BEHAVIOR;
 import static us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName.WALKING;
@@ -51,19 +42,11 @@ public class AtlasPatrolBehaviorTest
       conductor.simulate();
    }
 
-   @AfterAll
-   public static void printMemoryUsageAfterClass()
-   {
-      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(AtlasPatrolBehaviorTest.class + " after class.");
-   }
-
    @AfterEach
    public void destroySimulationAndRecycleMemory()
    {
       conductor.concludeTesting();
-
       BambooTools.reportTestFinishedMessage(simulationTestingParameters.getShowWindows());
-
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
    }
 
@@ -71,26 +54,21 @@ public class AtlasPatrolBehaviorTest
    public void setUp()
    {
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
-
-      SimulationConstructionSet scs = createAtlasSimulation();
+      SimulationConstructionSet scs =
+            createHumanoidSimulation(new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false),
+                                     new FlatGroundEnvironment());
       variables = new AtlasTestYoVariables(scs);
       conductor = new GoalOrientedTestConductor(scs, simulationTestingParameters);
       BambooTools.reportTestStartedMessage(simulationTestingParameters.getShowWindows());
    }
 
-   private SimulationConstructionSet createAtlasSimulation()
+   private SimulationConstructionSet createHumanoidSimulation(DRCRobotModel robotModel, CommonAvatarEnvironmentInterface environment)
    {
-      AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false);
-
-      FlatGroundEnvironment testEnvironment = new FlatGroundEnvironment();
-
       DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(false, false, simulationTestingParameters);
 
-      DRCSCSInitialSetup scsInitialSetup = new DRCSCSInitialSetup(testEnvironment, robotModel.getSimulateDT());
+      DRCSCSInitialSetup scsInitialSetup = new DRCSCSInitialSetup(environment, robotModel.getSimulateDT());
       scsInitialSetup.setInitializeEstimatorToActual(true);
-      int recordFrequency = (int) Math.round(robotModel.getControllerDT() / scsInitialSetup.getDT());
-      if (recordFrequency < 1) recordFrequency = 1;
-      scsInitialSetup.setRecordFrequency(recordFrequency);
+      scsInitialSetup.setTimePerRecordTick(robotModel.getControllerDT());
 
       RobotContactPointParameters<RobotSide> contactPointParameters = robotModel.getContactPointParameters();
       ContactableBodiesFactory<RobotSide> contactableBodiesFactory = new ContactableBodiesFactory<>();
@@ -119,17 +97,17 @@ public class AtlasPatrolBehaviorTest
       controllerFactory.addControllerFailureTransition(DO_NOTHING_BEHAVIOR, DO_NOTHING_BEHAVIOR);
       controllerFactory.addControllerFailureTransition(WALKING, DO_NOTHING_BEHAVIOR);
       controllerFactory.setInitialState(HighLevelControllerName.WALKING);
-      controllerFactory.createComponentBasedFootstepDataMessageGenerator(false, scsInitialSetup.getHeightMap());
+
+      RealtimeRos2Node realtimeRos2Node = ROS2Tools.createRealtimeRos2Node(PubSubImplementation.INTRAPROCESS, "humanoid_simulation_controller");
 
       AvatarSimulationFactory avatarSimulationFactory = new AvatarSimulationFactory();
       avatarSimulationFactory.setRobotModel(robotModel);
       avatarSimulationFactory.setShapeCollisionSettings(robotModel.getShapeCollisionSettings());
       avatarSimulationFactory.setHighLevelHumanoidControllerFactory(controllerFactory);
-      avatarSimulationFactory.setCommonAvatarEnvironment(testEnvironment);
+      avatarSimulationFactory.setCommonAvatarEnvironment(environment);
       avatarSimulationFactory.setRobotInitialSetup(robotModel.getDefaultRobotInitialSetup(0.0, 0.0));
       avatarSimulationFactory.setSCSInitialSetup(scsInitialSetup);
       avatarSimulationFactory.setGuiInitialSetup(guiInitialSetup);
-      RealtimeRos2Node realtimeRos2Node = ROS2Tools.createRealtimeRos2Node(PubSubImplementation.INTRAPROCESS, "humanoid_simulation_controller");
       avatarSimulationFactory.setRealtimeRos2Node(realtimeRos2Node);
       avatarSimulationFactory.setCreateYoVariableServer(true);
 
@@ -137,5 +115,11 @@ public class AtlasPatrolBehaviorTest
       SimulationConstructionSet scs = avatarSimulation.getSimulationConstructionSet();
       avatarSimulation.start();
       return scs;
+   }
+
+   @AfterAll
+   public static void printMemoryUsageAfterClass()
+   {
+      MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(AtlasPatrolBehaviorTest.class + " after class.");
    }
 }
