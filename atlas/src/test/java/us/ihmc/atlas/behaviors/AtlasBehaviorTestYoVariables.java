32b7b8fc9a5de6ca60b629b244bd1df46df0ef3a
Duncan Calvert <dcalvert@ihmc.us>
2019-03-06 16:52:17 -0600

    Atlas doNothing and step in place once.

--- atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasBehaviorTestYoVariables.java
+++ atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasBehaviorTestYoVariables.java
@@ -3,9 +3,9 @@ package us.ihmc.atlas.behaviors;
 import us.ihmc.avatar.AvatarTestYoVariables;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 
-public class AtlasTestYoVariables extends AvatarTestYoVariables
+public class AtlasBehaviorTestYoVariables extends AvatarTestYoVariables
 {
-   public AtlasTestYoVariables(SimulationConstructionSet scs)
+   public AtlasBehaviorTestYoVariables(SimulationConstructionSet scs)
    {
       super(scs);
    }
