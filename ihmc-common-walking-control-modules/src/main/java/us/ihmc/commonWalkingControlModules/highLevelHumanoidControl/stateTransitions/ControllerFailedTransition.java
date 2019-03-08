3359b054fefd2db0d58c59c6264dee35aa74930b
Robert <rgriffin@ihmc.us>
2019-03-07 20:02:52 -0600

    removed an unnecessary constructor

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/stateTransitions/ControllerFailedTransition.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/stateTransitions/ControllerFailedTransition.java
@@ -1,18 +1,11 @@
 package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.stateTransitions;
 
-import us.ihmc.commonWalkingControlModules.momentumBasedController.HighLevelHumanoidControllerToolbox;
 import us.ihmc.robotics.stateMachine.core.StateTransitionCondition;
-import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
 
 public class ControllerFailedTransition implements StateTransitionCondition
 {
    private final YoBoolean controllerFailed;
-
-   public ControllerFailedTransition(HighLevelHumanoidControllerToolbox controllerToolbox, YoVariableRegistry registry)
-   {
-      controllerFailed = controllerToolbox.getControllerFailedBoolean();
-   }
 
    public ControllerFailedTransition(YoBoolean controllerFailed)
    {
