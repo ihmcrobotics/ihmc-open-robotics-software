331429ad8cf9dbbcc7562d59c1d45aed3573ba9d
Duncan Calvert <dcalvert@ihmc.us>
2019-03-11 15:25:23 -0500

    Extract simulation construction to it's own class in main source set.

--- atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasPatrolBehaviorTest.java
+++ atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasPatrolBehaviorTest.java
@@ -5,33 +5,18 @@ import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.factory.AvatarSimulation;
-import us.ihmc.avatar.factory.AvatarSimulationFactory;
-import us.ihmc.avatar.initialSetup.DRCGuiInitialSetup;
-import us.ihmc.avatar.initialSetup.DRCSCSInitialSetup;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ContactableBodiesFactory;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.HighLevelHumanoidControllerFactory;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.exception.ExceptionTools;
-import us.ihmc.communication.ROS2Tools;
 import us.ihmc.humanoidBehaviors.BehaviorModule;
 import us.ihmc.humanoidBehaviors.BehaviorTeleop;
-import us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName;
 import us.ihmc.log.LogTools;
 import us.ihmc.messager.SharedMemoryMessager;
-import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
-import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.ros2.RealtimeRos2Node;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
-import us.ihmc.simulationConstructionSetTools.util.environments.CommonAvatarEnvironmentInterface;
 import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
 import us.ihmc.simulationConstructionSetTools.util.simulationrunner.GoalOrientedTestConductor;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.util.simulationTesting.SimulationTestingParameters;
 import us.ihmc.tools.MemoryTools;
-import us.ihmc.wholeBodyController.RobotContactPointParameters;
-
-import static us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName.DO_NOTHING_BEHAVIOR;
-import static us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName.WALKING;
 
 @Tag("humanoid-behaviors")
 public class AtlasPatrolBehaviorTest
@@ -100,70 +85,12 @@ public class AtlasPatrolBehaviorTest
    {
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
       robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false);
-      SimulationConstructionSet scs = createHumanoidSimulation(new FlatGroundEnvironment());
+      SimulationConstructionSet scs = AtlasBehaviorSimulation.create(robotModel, new FlatGroundEnvironment());
       variables = new AtlasBehaviorTestYoVariables(scs);
       conductor = new GoalOrientedTestConductor(scs, simulationTestingParameters);
       BambooTools.reportTestStartedMessage(simulationTestingParameters.getShowWindows());
 
       AtlasTestScripts.standUp(conductor, variables);
-   }
-
-   private SimulationConstructionSet createHumanoidSimulation(CommonAvatarEnvironmentInterface environment)
-   {
-      DRCGuiInitialSetup guiInitialSetup = new DRCGuiInitialSetup(false, false, simulationTestingParameters);
-
-      DRCSCSInitialSetup scsInitialSetup = new DRCSCSInitialSetup(environment, robotModel.getSimulateDT());
-      scsInitialSetup.setInitializeEstimatorToActual(true);
-      scsInitialSetup.setTimePerRecordTick(robotModel.getControllerDT());
-
-      RobotContactPointParameters<RobotSide> contactPointParameters = robotModel.getContactPointParameters();
-      ContactableBodiesFactory<RobotSide> contactableBodiesFactory = new ContactableBodiesFactory<>();
-      contactableBodiesFactory.setFootContactPoints(contactPointParameters.getFootContactPoints());
-      contactableBodiesFactory.setToeContactParameters(contactPointParameters.getControllerToeContactPoints(),
-                                                       contactPointParameters.getControllerToeContactLines());
-      for (int i = 0; i < contactPointParameters.getAdditionalContactNames().size(); i++)
-      {
-         contactableBodiesFactory.addAdditionalContactPoint(contactPointParameters.getAdditionalContactRigidBodyNames().get(i),
-                                                            contactPointParameters.getAdditionalContactNames().get(i),
-                                                            contactPointParameters.getAdditionalContactTransforms().get(i));
-      }
-
-      RealtimeRos2Node realtimeRos2Node = ROS2Tools.createRealtimeRos2Node(PubSubImplementation.INTRAPROCESS, "humanoid_simulation_controller");
-
-      HighLevelHumanoidControllerFactory controllerFactory =
-            new HighLevelHumanoidControllerFactory(contactableBodiesFactory,
-                                                   robotModel.getSensorInformation().getFeetForceSensorNames(),
-                                                   robotModel.getSensorInformation().getFeetContactSensorNames(),
-                                                   robotModel.getSensorInformation().getWristForceSensorNames(),
-                                                   robotModel.getHighLevelControllerParameters(),
-                                                   robotModel.getWalkingControllerParameters(),
-                                                   robotModel.getCapturePointPlannerParameters());
-      controllerFactory.useDefaultDoNothingControlState();
-      controllerFactory.useDefaultWalkingControlState();
-      controllerFactory.addRequestableTransition(DO_NOTHING_BEHAVIOR, WALKING);
-      controllerFactory.addRequestableTransition(WALKING, DO_NOTHING_BEHAVIOR);
-      controllerFactory.addControllerFailureTransition(DO_NOTHING_BEHAVIOR, DO_NOTHING_BEHAVIOR);
-      controllerFactory.addControllerFailureTransition(WALKING, DO_NOTHING_BEHAVIOR);
-      controllerFactory.setInitialState(HighLevelControllerName.WALKING);
-      controllerFactory.createControllerNetworkSubscriber(robotModel.getSimpleRobotName(), realtimeRos2Node);
-
-      AvatarSimulationFactory avatarSimulationFactory = new AvatarSimulationFactory();
-      avatarSimulationFactory.setRobotModel(robotModel);
-      avatarSimulationFactory.setShapeCollisionSettings(robotModel.getShapeCollisionSettings());
-      avatarSimulationFactory.setHighLevelHumanoidControllerFactory(controllerFactory);
-      avatarSimulationFactory.setCommonAvatarEnvironment(environment);
-      avatarSimulationFactory.setRobotInitialSetup(robotModel.getDefaultRobotInitialSetup(0.0, 0.0));
-      avatarSimulationFactory.setSCSInitialSetup(scsInitialSetup);
-      avatarSimulationFactory.setGuiInitialSetup(guiInitialSetup);
-      avatarSimulationFactory.setRealtimeRos2Node(realtimeRos2Node);
-      avatarSimulationFactory.setCreateYoVariableServer(true);
-
-      avatarSimulation = avatarSimulationFactory.createAvatarSimulation();
-
-      avatarSimulation.start();
-      realtimeRos2Node.spin();  // TODO Should probably happen in start()
-
-      return avatarSimulation.getSimulationConstructionSet();
    }
 
    @AfterAll
