12b15aad17dcdd0f2e430ec6718594a0ded9c1c9
Duncan Calvert <dcalvert@ihmc.us>
2019-03-13 17:40:48 -0500

    Use an existing joint name hash calculator instead of duplicating it.

--- ihmc-robot-models/src/main/java/us/ihmc/robotModels/FullRobotModelUtils.java
+++ ihmc-robot-models/src/main/java/us/ihmc/robotModels/FullRobotModelUtils.java
@@ -4,11 +4,8 @@ import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.mecano.tools.MultiBodySystemTools;
 import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.robotics.sensors.ForceSensorDefinition;
-import us.ihmc.robotics.sensors.IMUDefinition;
 
 import java.util.ArrayList;
-import java.util.zip.CRC32;
 
 public class FullRobotModelUtils
 {
@@ -34,26 +31,6 @@ public class FullRobotModelUtils
             }
          }
       }
-   }
-   public static int calculateJointNameHash(OneDoFJointBasics[] joints, ForceSensorDefinition[] forceSensorDefinitions, IMUDefinition[] imuDefinitions)
-   {
-      CRC32 crc = new CRC32();
-      for (OneDoFJointBasics joint : joints)
-      {
-         crc.update(joint.getName().getBytes());
-      }
-
-      for (ForceSensorDefinition forceSensorDefinition : forceSensorDefinitions)
-      {
-         crc.update(forceSensorDefinition.getSensorName().getBytes());
-      }
-
-      for (IMUDefinition imuDefinition : imuDefinitions)
-      {
-         crc.update(imuDefinition.getName().getBytes());
-      }
-
-      return (int) crc.getValue();
    }
 
    public static void checkJointNameHash(int expectedJointNameHash, int actualJointNameHash)
