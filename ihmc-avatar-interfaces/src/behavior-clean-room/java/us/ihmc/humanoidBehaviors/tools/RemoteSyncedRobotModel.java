82daea3ca0529c34b2aa915d96604e38f542322f
Duncan Calvert <dcalvert@ihmc.us>
2019-03-13 17:32:22 -0500

    Make RemoteSyncedRobotModel thread safe.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/RemoteSyncedRobotModel.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/RemoteSyncedRobotModel.java
@@ -4,82 +4,52 @@ import controller_msgs.msg.dds.RobotConfigurationData;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ControllerAPIDefinition;
 import us.ihmc.communication.ROS2Tools;
-import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.communication.Ros2QueuedSubscription;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotModels.FullRobotModelUtils;
-import us.ihmc.robotics.sensors.ForceSensorDefinition;
-import us.ihmc.robotics.sensors.IMUDefinition;
 import us.ihmc.ros2.Ros2Node;
-
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.zip.CRC32;
 
 public class RemoteSyncedRobotModel
 {
-   private final FullHumanoidRobotModel fullRobotModel;
+   protected final FullHumanoidRobotModel fullRobotModel;
    private final OneDoFJointBasics[] allJoints;
    private final int jointNameHash;
-   private final AtomicReference<RigidBodyTransform> newRootJointPoseReference = new AtomicReference<>(null);
-   private final AtomicReference<float[]> newJointConfigurationReference = new AtomicReference<>(null);
+   private final Ros2QueuedSubscription<RobotConfigurationData> robotConfigurationDataQueue;
+   private final RobotConfigurationData robotConfigurationData = new RobotConfigurationData();
 
    public RemoteSyncedRobotModel(DRCRobotModel robotModel, Ros2Node ros2Node)
    {
       fullRobotModel = robotModel.createFullRobotModel();
       allJoints = FullRobotModelUtils.getAllJointsExcludingHands(fullRobotModel);
-      jointNameHash = calculateJointNameHash(allJoints, fullRobotModel.getForceSensorDefinitions(),
-                                             fullRobotModel.getIMUDefinitions());
+      jointNameHash = FullRobotModelUtils.calculateJointNameHash(allJoints, fullRobotModel.getForceSensorDefinitions(), fullRobotModel.getIMUDefinitions());
 
-      ROS2Tools.createCallbackSubscription(ros2Node,
-                                           RobotConfigurationData.class,
-                                           ControllerAPIDefinition.getPublisherTopicNameGenerator(robotModel.getSimpleRobotName()),
-                                           s -> updateRobotModel(s.takeNextData()));
+      robotConfigurationDataQueue = ROS2Tools.createQueuedSubscription(ros2Node, RobotConfigurationData.class,
+                                                                       ControllerAPIDefinition.getPublisherTopicNameGenerator(robotModel.getSimpleRobotName()));
+   }
+
+   public FullHumanoidRobotModel pollFullRobotModel()
+   {
+      if (robotConfigurationDataQueue.flushAndGetLatest(robotConfigurationData))
+      {
+         FullRobotModelUtils.checkJointNameHash(jointNameHash, robotConfigurationData.getJointNameHash());
+
+         fullRobotModel.getRootJoint().setJointOrientation(robotConfigurationData.getRootOrientation());
+         fullRobotModel.getRootJoint().setJointPosition(robotConfigurationData.getRootTranslation());
+
+         for (int i = 0; i < robotConfigurationData.getJointAngles().size(); i++)
+         {
+            allJoints[i].setQ(robotConfigurationData.getJointAngles().get(i));
+         }
+
+         fullRobotModel.getElevator().updateFramesRecursively();
+      }
+
+      return fullRobotModel;
    }
 
    public FullHumanoidRobotModel getFullRobotModel()
    {
       return fullRobotModel;
-   }
-
-   private void updateRobotModel(RobotConfigurationData robotConfigurationData)
-   {
-      if (robotConfigurationData.getJointNameHash() != jointNameHash)
-         throw new RuntimeException("Joint names do not match for RobotConfigurationData");
-
-      newRootJointPoseReference.set(new RigidBodyTransform(robotConfigurationData.getRootOrientation(), robotConfigurationData.getRootTranslation()));
-      newJointConfigurationReference.set(robotConfigurationData.getJointAngles().toArray());
-
-      RigidBodyTransform newRootJointPose = newRootJointPoseReference.getAndSet(null);
-      if (newRootJointPose != null)
-         fullRobotModel.getRootJoint().setJointConfiguration(newRootJointPose);
-
-      float[] newJointConfiguration = newJointConfigurationReference.getAndSet(null);
-      if (newJointConfiguration != null)
-      {
-         for (int i = 0; i < allJoints.length; i++)
-            allJoints[i].setQ(newJointConfiguration[i]);
-      }
-      fullRobotModel.getElevator().updateFramesRecursively();
-   }
-
-   public static int calculateJointNameHash(OneDoFJointBasics[] joints, ForceSensorDefinition[] forceSensorDefinitions, IMUDefinition[] imuDefinitions)
-   {
-      CRC32 crc = new CRC32();
-      for (OneDoFJointBasics joint : joints)
-      {
-         crc.update(joint.getName().getBytes());
-      }
-
-      for (ForceSensorDefinition forceSensorDefinition : forceSensorDefinitions)
-      {
-         crc.update(forceSensorDefinition.getSensorName().getBytes());
-      }
-
-      for (IMUDefinition imuDefinition : imuDefinitions)
-      {
-         crc.update(imuDefinition.getName().getBytes());
-      }
-
-      return (int) crc.getValue();
    }
 }
