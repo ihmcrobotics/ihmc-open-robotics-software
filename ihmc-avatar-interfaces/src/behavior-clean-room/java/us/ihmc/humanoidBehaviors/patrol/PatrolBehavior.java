f4ae41a8aa77f71280256e591a8ff898eda80985
Duncan Calvert <dcalvert@ihmc.us>
2019-03-18 15:47:37 -0500

    First complete draft of PatrolBehavior.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/patrol/PatrolBehavior.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/patrol/PatrolBehavior.java
@@ -1,8 +1,6 @@
 package us.ihmc.humanoidBehaviors.patrol;
 
-import controller_msgs.msg.dds.AbortWalkingMessage;
-import controller_msgs.msg.dds.FootstepDataListMessage;
-import controller_msgs.msg.dds.FootstepPlanningToolboxOutputStatus;
+import controller_msgs.msg.dds.*;
 import org.apache.commons.lang3.tuple.Pair;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ControllerAPIDefinition;
@@ -11,26 +9,24 @@ import us.ihmc.communication.IHMCROS2Publisher;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.euclid.geometry.Pose3D;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.humanoidBehaviors.tools.Activator;
 import us.ihmc.humanoidBehaviors.tools.RemoteFootstepPlannerInterface;
 import us.ihmc.humanoidBehaviors.tools.RemoteSyncedHumanoidFrames;
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.AbortWalkingCommand;
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.FootstepDataListCommand;
 import us.ihmc.log.LogTools;
-import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.messager.Messager;
 import us.ihmc.messager.MessagerAPIFactory;
 import us.ihmc.messager.MessagerAPIFactory.Category;
 import us.ihmc.messager.MessagerAPIFactory.CategoryTheme;
 import us.ihmc.messager.MessagerAPIFactory.MessagerAPI;
 import us.ihmc.messager.MessagerAPIFactory.Topic;
+import us.ihmc.pubsub.subscriber.Subscriber;
 import us.ihmc.ros2.Ros2Node;
 import us.ihmc.util.PeriodicNonRealtimeThreadScheduler;
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -55,12 +51,13 @@ public class PatrolBehavior
    private final RemoteFootstepPlannerInterface remoteFootstepPlannerInterface;
 
    private final Notification stopNotification = new Notification();
-   private final Notification goToWaypointNotification = new Notification();
+   private final Notification overrideGoToWaypointNotification = new Notification();
 
    private final AtomicInteger goalWaypointIndex = new AtomicInteger();
 
    private final Notification readyToPlanNotification = new Notification();
    private final Notification footstepPlanCompleted = new Notification();
+   private final Notification walkingCompleted = new Notification();
 
    private FootstepPlanningToolboxOutputStatus footstepPlanningOutput;
 
@@ -81,6 +78,10 @@ public class PatrolBehavior
       abortPublisher = ROS2Tools.createPublisher(ros2Node,
                                                  ROS2Tools.newMessageInstance(AbortWalkingCommand.class).getMessageClass(),
                                                  ControllerAPIDefinition.getSubscriberTopicNameGenerator(robotModel.getSimpleRobotName()));
+      ROS2Tools.createCallbackSubscription(ros2Node,
+                                           WalkingStatusMessage.class,
+                                           ControllerAPIDefinition.getPublisherTopicNameGenerator(robotModel.getSimpleRobotName()),
+                                           this::updateWalkingCompletedStatus);
 
       remoteSyncedHumanoidFrames = new RemoteSyncedHumanoidFrames(robotModel, ros2Node);
 
@@ -90,7 +91,7 @@ public class PatrolBehavior
       messager.registerTopicListener(API.GoToWaypoint, goToWaypointIndex ->
       {
          goalWaypointIndex.set(goToWaypointIndex);
-         goToWaypointNotification.set();
+         overrideGoToWaypointNotification.set();
       });
 
       waypoints = messager.createInput(API.Waypoints);
@@ -133,6 +134,7 @@ public class PatrolBehavior
 
          if (walkable)
          {
+            walkingCompleted.poll(); // acting to clear the notification
             footstepDataListPublisher.publish(footstepPlanningOutput.getFootstepDataList());
             transitionTo(WALK_TO_GOAL_WAYPOINT);
          }
@@ -146,16 +148,23 @@ public class PatrolBehavior
    private void updateWalkToGoalWaypointState()
    {
       if (checkForInterruptions()) return;
-      // if walking is done
+
+      if (walkingCompleted.poll())  // in the future, may want to count steps and assert robot got to the waypoint bounding box
       {
-         //
+         ArrayList<Pose3D> latestWaypoints = waypoints.get();      // access and store these early
+         int nextGoalWaypointIndex = goalWaypointIndex.get() + 1;  // to make thread-safe
+         if (nextGoalWaypointIndex >= latestWaypoints.size())
+            nextGoalWaypointIndex = 0;
+         goalWaypointIndex.set(nextGoalWaypointIndex);
+         readyToPlanNotification.set();
+         transitionTo(RETRIEVE_FOOTSTEP_PLAN);
       }
    }
 
    private boolean checkForInterruptions()
    {
       stopNotification.poll();         // poll both at the same time to handle race condition
-      goToWaypointNotification.poll();
+      overrideGoToWaypointNotification.poll();
 
       if (stopNotification.read())  // favor stop if race condition
       {
@@ -163,7 +172,7 @@ public class PatrolBehavior
          transitionTo(STOP);
       }
 
-      if (goToWaypointNotification.read())
+      if (overrideGoToWaypointNotification.read())
       {
          sendStopWalking();
 
@@ -176,11 +185,11 @@ public class PatrolBehavior
          }
          else
          {
-            transitionTo(STOP);
+            transitionTo(STOP); // TODO submit message about incorrect index
          }
       }
 
-      return stopNotification.read() || goToWaypointNotification.read();
+      return stopNotification.read() || overrideGoToWaypointNotification.read();
    }
 
    private void sendStopWalking()
@@ -192,6 +201,18 @@ public class PatrolBehavior
    {
       currentState = stateToTransitionTo;
       messager.submitMessage(API.CurrentState, currentState.getKey());
+   }
+
+   private void updateWalkingCompletedStatus(Subscriber<WalkingStatusMessage> status)
+   {
+      WalkingStatusMessage walkingStatusMessage;
+      while ((walkingStatusMessage = status.takeNextData()) != null)
+      {
+         if (walkingStatusMessage.getWalkingStatus() == WalkingStatusMessage.COMPLETED)
+         {
+            walkingCompleted.set();
+         }
+      }
    }
 
    public static class API
