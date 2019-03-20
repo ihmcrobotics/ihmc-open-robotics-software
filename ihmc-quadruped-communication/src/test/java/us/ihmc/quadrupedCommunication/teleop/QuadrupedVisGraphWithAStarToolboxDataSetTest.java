e18cd905190c1e2d9b8a7dc21ceb33142085d335
Robert <rgriffin@ihmc.us>
2019-03-20 10:49:46 -0500

    fixed some other settings

--- ihmc-quadruped-communication/src/test/java/us/ihmc/quadrupedCommunication/teleop/QuadrupedVisGraphWithAStarToolboxDataSetTest.java
+++ ihmc-quadruped-communication/src/test/java/us/ihmc/quadrupedCommunication/teleop/QuadrupedVisGraphWithAStarToolboxDataSetTest.java
@@ -3,6 +3,8 @@ package us.ihmc.quadrupedCommunication.teleop;
 import org.junit.jupiter.api.Test;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.FootstepPlannerType;
+import us.ihmc.quadrupedPlanning.QuadrupedGait;
+import us.ihmc.quadrupedPlanning.QuadrupedSpeed;
 import us.ihmc.quadrupedPlanning.QuadrupedXGaitSettings;
 import us.ihmc.quadrupedPlanning.QuadrupedXGaitSettingsReadOnly;
 
@@ -18,11 +20,14 @@ public class QuadrupedVisGraphWithAStarToolboxDataSetTest extends FootstepPlanne
       QuadrupedXGaitSettings settings = new QuadrupedXGaitSettings();
       settings.setStanceLength(1.0);
       settings.setStanceWidth(0.5);
-      settings.setEndDoubleSupportDuration(0.15);
-//      settings.setEndDoubleSupportDuration(0.5 + 0.25);
-      settings.setStepDuration(0.33);
-//      settings.setEndPhaseShift(90);
-      settings.setEndPhaseShift(180.0);
+
+      settings.getTrotMediumTimings().setStepDuration(0.33);
+      settings.getTrotMediumTimings().setEndDoubleSupportDuration(0.15);
+      settings.getAmbleMediumTimings().setStepDuration(0.4);
+      settings.getAmbleMediumTimings().setEndDoubleSupportDuration(0.5 + 0.25);
+
+      settings.setQuadrupedSpeed(QuadrupedSpeed.MEDIUM);
+      settings.setEndPhaseShift(QuadrupedGait.TROT.getEndPhaseShift());
       return settings;
    }
 
