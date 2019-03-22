81449c8707eeae423a7a4c97dfe699beab6182b3
Duncan Calvert <dcalvert@ihmc.us>
2019-03-22 15:25:45 -0500

    Extract do nothing test into its own class to prevent already bound network error.

--- atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasBehaviorDoNothingTest.java
+++ atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasBehaviorDoNothingTest.java
@@ -4,13 +4,6 @@ import org.junit.jupiter.api.*;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.avatar.factory.AvatarSimulation;
-import us.ihmc.commons.exception.DefaultExceptionHandler;
-import us.ihmc.commons.exception.ExceptionTools;
-import us.ihmc.humanoidBehaviors.BehaviorBackpack;
-import us.ihmc.humanoidBehaviors.BehaviorTeleop;
-import us.ihmc.log.LogTools;
-import us.ihmc.messager.SharedMemoryMessager;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
 import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
 import us.ihmc.simulationConstructionSetTools.util.simulationrunner.GoalOrientedTestConductor;
@@ -19,51 +12,17 @@ import us.ihmc.simulationconstructionset.util.simulationTesting.SimulationTestin
 import us.ihmc.tools.MemoryTools;
 
 @Tag("humanoid-behaviors")
-public class AtlasStepInPlaceBehaviorTest
+public class AtlasBehaviorDoNothingTest
 {
    private static final SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromSystemProperties();
    private AtlasBehaviorTestYoVariables variables;
    private GoalOrientedTestConductor conductor;
-   private AvatarSimulation avatarSimulation;
    private AtlasRobotModel robotModel;
 
    @Test
-   public void testStepInPlaceBehavior()
+   public void testDoNothingBehavior()
    {
-      SharedMemoryMessager messager = new SharedMemoryMessager(BehaviorBackpack.getBehaviorAPI());
-      ExceptionTools.handle(() -> messager.startMessager(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
-
-      LogTools.info("Creating behavior module");
-      BehaviorBackpack.createForTest(robotModel, messager);
-
-      LogTools.info("Creating behavior teleop");
-      BehaviorTeleop behaviorTeleop = BehaviorTeleop.createForTest(robotModel, messager);
-
-      LogTools.info("Set stepping true");
-      behaviorTeleop.setStepping(true);
-
-      double initialTransfer = robotModel.getWalkingControllerParameters().getDefaultInitialTransferTime();
-      double transfer = robotModel.getWalkingControllerParameters().getDefaultTransferTime();
-      double swing = robotModel.getWalkingControllerParameters().getDefaultSwingTime();
-//      int steps = footMessage.getFootstepDataList().size();
-
-      LogTools.info("Awaiting touchdowns");
-      double timeLimit = 6.0;
-      LogTools.info("Waiting for touchdown 1");
-      AtlasTestScripts.nextTouchdown(conductor, variables, timeLimit);
-      LogTools.info("Waiting for touchdown 2");
-      AtlasTestScripts.nextTouchdown(conductor, variables, timeLimit);
-      LogTools.info("Waiting for touchdown 3");
-      AtlasTestScripts.nextTouchdown(conductor, variables, timeLimit);
-      LogTools.info("Waiting for touchdown 4");
-      AtlasTestScripts.nextTouchdown(conductor, variables, timeLimit);
-
-      behaviorTeleop.setStepping(false);
-      behaviorTeleop.abort();
-
-      AtlasTestScripts.wait(conductor, variables, 3.0);
-
-      AtlasTestScripts.holdDoubleSupport(conductor, variables, 3.0, timeLimit);
+      // do nothing!!!
    }
 
    @AfterEach
