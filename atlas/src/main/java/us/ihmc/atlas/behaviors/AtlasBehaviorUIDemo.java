5d50341c99d4e2e59cb02d7c6c0a8c1cfde78a8d
Duncan Calvert <dcalvert@ihmc.us>
2019-03-22 12:54:04 -0500

    Add toggle for flat ground/cinder block field

--- atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasBehaviorUIDemo.java
+++ atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasBehaviorUIDemo.java
@@ -21,6 +21,7 @@ import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.geometry.PlanarRegionsListGenerator;
 import us.ihmc.ros2.Ros2Node;
+import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
 import us.ihmc.simulationConstructionSetTools.util.environments.PlanarRegionsListDefinedEnvironment;
 import us.ihmc.simulationConstructionSetTools.util.planarRegions.PlanarRegionsListExamples;
 import us.ihmc.util.PeriodicNonRealtimeThreadScheduler;
@@ -32,8 +33,9 @@ import java.util.concurrent.TimeUnit;
  */
 public class AtlasBehaviorUIDemo extends Application
 {
-   public static final AtlasRobotVersion ATLAS_VERSION = AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS;
+   private static final AtlasRobotVersion ATLAS_VERSION = AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS;
    private static final RobotTarget ATLAS_TARGET = RobotTarget.SCS;
+   private static final boolean USE_FLAT_GROUND = true;
 
    private BehaviorUI ui;
 
@@ -42,27 +44,33 @@ public class AtlasBehaviorUIDemo extends Application
    {
       Log.DEBUG();
 
-      new Thread(() -> {
-         LogTools.info("Creating planar region publisher");
-         Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, "Fake_REA_module");
-         IHMCROS2Publisher<PlanarRegionsListMessage> planarRegionPublisher
-               = ROS2Tools.createPublisher(ros2Node,
-                                           PlanarRegionsListMessage.class,
-                                           ROS2Tools.getTopicNameGenerator(createRobotModel().getSimpleRobotName(),
-                                                                           ROS2Tools.REA_MODULE,
-                                                                           ROS2Tools.ROS2TopicQualifier.OUTPUT));
-         PlanarRegionsList planarRegions = createPlanarRegions();
-         PlanarRegionsListMessage planarRegionsListMessage = PlanarRegionMessageConverter.convertToPlanarRegionsListMessage(planarRegions);
-         PeriodicNonRealtimeThreadScheduler patrolThread = new PeriodicNonRealtimeThreadScheduler(getClass().getSimpleName());
-         patrolThread.schedule(() -> planarRegionPublisher.publish(planarRegionsListMessage), 500, TimeUnit.MILLISECONDS);
-      }).start();
+      if (!USE_FLAT_GROUND)
+      {
+         new Thread(() -> {
+            LogTools.info("Creating planar region publisher");
+            Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, "Fake_REA_module");
+            IHMCROS2Publisher<PlanarRegionsListMessage> planarRegionPublisher
+                  = ROS2Tools.createPublisher(ros2Node,
+                                              PlanarRegionsListMessage.class,
+                                              ROS2Tools.getTopicNameGenerator(createRobotModel().getSimpleRobotName(),
+                                                                              ROS2Tools.REA_MODULE,
+                                                                              ROS2Tools.ROS2TopicQualifier.OUTPUT));
+            PlanarRegionsList planarRegions = createPlanarRegions();
+            PlanarRegionsListMessage planarRegionsListMessage = PlanarRegionMessageConverter.convertToPlanarRegionsListMessage(planarRegions);
+            PeriodicNonRealtimeThreadScheduler patrolThread = new PeriodicNonRealtimeThreadScheduler(getClass().getSimpleName());
+            patrolThread.schedule(() -> planarRegionPublisher.publish(planarRegionsListMessage), 500, TimeUnit.MILLISECONDS);
+         }).start();
+      }
 
       new Thread(() -> {
          LogTools.info("Creating simulation");
          AtlasBehaviorSimulation.createForManualTest(createRobotModel(),
+                                                     USE_FLAT_GROUND ?
+                                                     new FlatGroundEnvironment() :
                                                      new PlanarRegionsListDefinedEnvironment(createPlanarRegions(),
                                                                                              0.02,
-                                                                                             false))
+                                                                                             false)
+         )
                                 .simulate();
       }
       ).start();
