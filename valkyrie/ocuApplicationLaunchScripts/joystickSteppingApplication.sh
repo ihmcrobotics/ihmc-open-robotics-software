948abd101b1c6ed1962c3f78045a12c7e13a4ed3
Sylvain Bertrand <sbertrand@ihmc.us>
2019-04-02 15:04:59 -0500

    Revert "Added Gradle script to deploy binaries for OCU applications"
    This reverts commit 34f8f40abd4edf59089590addcdadbaf903e6fdc.

--- valkyrie/ocuApplicationLaunchScripts/joystickSteppingApplication.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/.ihmc/logs/
-java -Djava.library.path=lib/ -cp ValkyrieController.jar us.ihmc.valkyrie.joystick.ValkyrieJoystickBasedSteppingApplication | tee $HOME/.ihmc/logs/ValkyrieJoystickBasedSteppingApplication_$(date '+%Y%m%d_%H%M%S').log
