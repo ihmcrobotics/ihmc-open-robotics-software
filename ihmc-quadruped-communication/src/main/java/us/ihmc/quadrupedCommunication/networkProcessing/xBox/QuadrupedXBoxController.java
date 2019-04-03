0e474935bb42f120a6f9d765b8d8a99ce9b1a2e1
Robert <rgriffin@ihmc.us>
2019-04-03 11:12:39 -0500

    yo variablized the planar velocity

--- ihmc-quadruped-communication/src/main/java/us/ihmc/quadrupedCommunication/networkProcessing/xBox/QuadrupedXBoxController.java
+++ ihmc-quadruped-communication/src/main/java/us/ihmc/quadrupedCommunication/networkProcessing/xBox/QuadrupedXBoxController.java
@@ -85,8 +85,8 @@ public class QuadrupedXBoxController extends QuadrupedToolboxController implemen
       maxTranslationX.set(0.25);
       maxTranslationY.set(0.15);
 
-      maxYSpeedFraction.set(0.35);
-      maxYawSpeedFraction.set(0.5);
+      maxYSpeedFraction.set(0.5);
+      maxYawSpeedFraction.set(0.9);
 
       bodyOrientationShiftTime.set(0.1);
       this.bodyHeight = new InputValueIntegrator(Conversions.millisecondsToSeconds(updateTimeInMs), nominalBodyHeight);
