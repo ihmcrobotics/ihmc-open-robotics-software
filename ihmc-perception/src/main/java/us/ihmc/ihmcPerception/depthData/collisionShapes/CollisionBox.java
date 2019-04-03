df458f2dd16905ef1fe0eef2cfbd2e51faafced3
Robert <rgriffin@ihmc.us>
2019-04-03 10:14:21 -0500

    added some missing java doc to the collision box

--- ihmc-perception/src/main/java/us/ihmc/ihmcPerception/depthData/collisionShapes/CollisionBox.java
+++ ihmc-perception/src/main/java/us/ihmc/ihmcPerception/depthData/collisionShapes/CollisionBox.java
@@ -13,6 +13,13 @@ public class CollisionBox extends CollisionShape
    private final double yExtent;
    private final double zExtent;
 
+   /**
+    * @param pose the pose of the center of the box in the parent frame. Used to define a reference frame with an origin
+    *             at the center of the box and orientation aligned with x-y-z
+    * @param xExtent half the length
+    * @param yExtent half the width
+    * @param zExtent half the height
+    */
    public CollisionBox(RigidBodyTransform pose, double xExtent, double yExtent, double zExtent)
    {
       super(pose);
@@ -36,6 +43,10 @@ public class CollisionBox extends CollisionShape
       return zExtent;
    }
 
+   /**
+    * Checks if the point is contained within the box. Note that this point is assumed to be in the box origin frame.
+    * @return whether or not the point is in the box
+    */
    @Override
    public boolean contains(Point3D point)
    {
@@ -47,7 +58,7 @@ public class CollisionBox extends CollisionShape
    {
       if (shape3D == null)
       {
-         shape3D = new Box3D(getPose(), xExtent, yExtent, zExtent);
+         shape3D = new Box3D(getPose(), 2.0 * xExtent, 2.0 * yExtent, 2.0 * zExtent);
       }
       return shape3D;
    }
