e3915713aac4834aa893c84ba65f270360e21ee4
Georg Wiedebach <gwiedebach@ihmc.us>
2019-04-05 11:28:11 -0500

    Added constructor to parameter tuner for server selection.

--- ihmc-parameter-tuner/src/main/java/us/ihmc/parameterTuner/remote/ParameterTuner.java
+++ ihmc-parameter-tuner/src/main/java/us/ihmc/parameterTuner/remote/ParameterTuner.java
@@ -5,7 +5,17 @@ import us.ihmc.parameterTuner.guiElements.main.ParameterTuningApplication;
 
 public class ParameterTuner extends ParameterTuningApplication
 {
-   private final ParameterGuiInterface inputManager = new RemoteInputManager();
+   private final ParameterGuiInterface inputManager;
+
+   public ParameterTuner()
+   {
+      inputManager = new RemoteInputManager();
+   }
+
+   public ParameterTuner(String serverAddress)
+   {
+      inputManager = new RemoteInputManager(serverAddress);
+   }
 
    @Override
    protected ParameterGuiInterface createInputManager()
