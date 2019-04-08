f30c5a0eefefb6bbabb45340e084d870da0455b6
Jcarff <jcarff@ihmc.us>
2019-04-08 09:18:06 -0500

    working on database connection

--- ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/diagnostic/SQLDatabaseManager.java
+++ ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/diagnostic/SQLDatabaseManager.java
@@ -1,17 +1,17 @@
 package us.ihmc.humanoidBehaviors.behaviors.diagnostic;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import us.ihmc.log.LogTools;
 
 public class SQLDatabaseManager
 {
-
    private final boolean DEBUG = true;
    private Connection databaseConnection;
 
@@ -28,13 +28,13 @@ public class SQLDatabaseManager
 
    ConcurrentLinkedQueue<String> statements = new ConcurrentLinkedQueue<String>();
 
-   public SQLDatabaseManager(String username, String password, String host, String port, String databaseName)
+   public SQLDatabaseManager()
    {
-      this.databaseUsername = username;
-      this.databasePassword = password;
-      this.database_host = host;
-      this.database_port = port;
-      this.database_name = databaseName;
+      this.databaseUsername = "shadylady";
+      this.databasePassword = "ShadyLady";
+      this.database_host = "10.7.4.48";
+      this.database_port = "32769";
+      this.database_name = "shadylady";
 
       databaseURL = "jdbc:postgresql://" + database_host + ":" + database_port + "/" + database_name;
 
@@ -49,7 +49,7 @@ public class SQLDatabaseManager
       {
          connection = null;
          connectionSuccessful = false;
-         LogTools.error("Could not connect to database! DB thread will not run");
+         //LogTools.error("Could not connect to database! DB thread will not run");
          e.printStackTrace();
       }
 
@@ -68,48 +68,22 @@ public class SQLDatabaseManager
 
    private void update()
    {
-      if(connected && !shutdown)
-      {
-         if()
-         try
-         {
-            isNewDataAvailable = true;
-
-            while (isNewDataAvailable)
-            {
-               updateInsertPreparedStatement();
-
-               timeSeriesInsertPreparedStatement.addBatch();
-
-               batchCount++;
-
-               if (batchCount >= BATCHES_BEFORE_EXECUTE)
-               {
-                  executionTimer.startMeasurement();
-
-                  timeSeriesInsertPreparedStatement.executeBatch();
-
-                  executionTimer.stopMeasurement();
-
-                  batchCount = 0;
-
-               }
-
-               isNewDataAvailable = true;
-            }
-         }
-         catch (SQLException e)
-         {
-            LogTools.error("Error executing database update!");
-            e.printStackTrace();
-         }
-      }
-      
-
+      /*
+       * if(connected && !shutdown) { if() try { isNewDataAvailable = true;
+       * while (isNewDataAvailable) { updateInsertPreparedStatement();
+       * timeSeriesInsertPreparedStatement.addBatch(); batchCount++; if
+       * (batchCount >= BATCHES_BEFORE_EXECUTE) {
+       * executionTimer.startMeasurement();
+       * timeSeriesInsertPreparedStatement.executeBatch();
+       * executionTimer.stopMeasurement(); batchCount = 0; } isNewDataAvailable
+       * = true; } } catch (SQLException e) {
+       * LogTools.error("Error executing database update!");
+       * e.printStackTrace(); } }
+       */
 
    }
 
-   public boolean insert(String statement)
+   private boolean sqlUpdate(String statement)
    {
       Statement stmt = null;
       try
@@ -123,81 +97,130 @@ public class SQLDatabaseManager
          //   + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
          stmt.executeUpdate(statement);
          stmt.close();
+         return true;
       }
       catch (Exception e)
       {
          System.err.println(e.getClass().getName() + ": " + e.getMessage());
       }
-   }
-
-   public void sendSQLStatement(String statement)
-   {
-      insertStatement = connection.prepareStatement(statement);
-      exerciseStatement = connection.prepareStatement(getExerciseSQLString);
-      connectionSuccessful = true;
-   }
-
-   public boolean getExerciseFromDatabase(long exerciseId)
-   {
-      try
-      {
-         getExerciseInnerJoinPreparedStatement.clearParameters();
-         getExerciseInnerJoinPreparedStatement.setLong(1, exerciseId);
-
-         ResultSet resultSet = getExerciseInnerJoinPreparedStatement.executeQuery();
-         resultSet.next();
-
-         String exerciseName = resultSet.getString(1);
-         boolean isLoaded = resultSet.getBoolean(2);
-
-         // exerciseToPack.setName(exerciseName);
-         // exerciseToPack.setLoaded(isLoaded);
-
-         return true;
-      }
-      catch (SQLException e)
-      {
-         LogTools.error("Could not execute prepared statement!");
-         e.printStackTrace();
-
-         return false;
-      }
+      return false;
    }
 
    private void shutdown()
    {
       try
       {
-         LogTools.info("Beginning database thread shutdown...");
-         finalizeRecording();
+         //LogTools.info("Beginning database thread shutdown...");
+         //   finalizeRecording();
 
-         LogTools.info("Closing database connection...");
+        // LogTools.info("Closing database connection...");
          databaseConnection.close();
       }
       catch (SQLException e)
       {
-         LogTools.error("Error closing database connections");
+         //LogTools.error("Error closing database connections");
          e.printStackTrace();
          System.exit(1);
       }
    }
 
-   private void finalizeRecording() throws SQLException
+   /*
+    * private void finalizeRecording() throws SQLException { shutdown = true;
+    * LogTools.info("Processing remaining buffered data..."); synchronized
+    * (timeSeriesInsertPreparedStatement) { while (isNewDataAvailable) {
+    * updateInsertPreparedStatement(); isNewDataAvailable = true; }
+    * timeSeriesInsertPreparedStatement.executeBatch(); }
+    * LogTools.info("All data processed, safe to finish shut down."); }
+    */
+
+   public Operator saveOperator(String operatorName)
    {
-      shutdown = true;
-      LogTools.info("Processing remaining buffered data...");
-      synchronized (timeSeriesInsertPreparedStatement)
+      sqlUpdate("INSERT INTO operators (name) VALUES (" + operatorName + ");");
+      //Operator returnOperator = getOperator(operatorName);
+
+     // if (returnOperator != null)
+      //   return returnOperator;
+      return null;
+   }
+
+   public boolean saveRun(Run run)
+   {
+      return sqlUpdate("INSERT INTO runs (id,operator,task,is_successful,notes,log_file,date,time) VALUES (" + run.runID + "," + run.operatorID + ","
+            + run.taskID + "," + run.successful + "," + run.notes + "," + run.logFile + "," + run.date + "," + run.time + ");");
+   }
+
+   public boolean saveRunEvent(RunEvent runEvent)
+   {
+      return sqlUpdate("INSERT INTO run_events (run_id,event_name,event_time_in_seconds,is_successful) VALUES (" + runEvent.runID + "," + runEvent.eventName
+            + "," + runEvent.runTime + "," + runEvent.successful + ");");
+   }
+
+   public boolean saveTask(Task run)
+   {
+      return sqlUpdate("INSERT INTO tasks (id,name) VALUES (" + run.taskID + "," + run.name + ");");
+   }
+
+   public Operator getOperator(String name)
+   {
+      Statement stmt = null;
+      Operator returnedOperator = new Operator();
+      try
       {
-         while (isNewDataAvailable)
+         stmt = databaseConnection.createStatement();
+
+         ResultSet rs = stmt.executeQuery("SELECT * FROM operators WHERE name = " + name + ";");
+         while (rs.next())
          {
-            updateInsertPreparedStatement();
-            isNewDataAvailable = true;
+            returnedOperator.operatorID = rs.getInt("id");
+            returnedOperator.name = rs.getString("name");
          }
-
-         timeSeriesInsertPreparedStatement.executeBatch();
+         rs.close();
+         stmt.close();
+         return returnedOperator;
       }
+      catch (Exception e)
+      {
+         System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      }
+      return null;
+   }
 
-      LogTools.info("All data processed, safe to finish shut down.");
+   public class Operator
+   {
+      public int operatorID;
+      public String name;
+   }
+
+   public class Run
+   {
+      public int runID;
+      public int operatorID;
+      public int taskID;
+      public boolean successful;
+      public String notes;
+      public String logFile;
+      public Date date;
+      public Time time;
+   }
+
+   public class RunEvent
+   {
+      public int runID;
+      public String eventName;
+      public float runTime;
+      public boolean successful;
+   }
+
+   public class Task
+   {
+      public int taskID;
+      public String name;
+   }
+
+   public static void main(String[] args)
+   {
+      SQLDatabaseManager test = new SQLDatabaseManager();
+      System.out.println("MY ID NUMBER IS "+test.saveOperator("john").operatorID);
    }
 
 }
