7fb3a408ab59c1d2f63b92640225711aed992c6a
Duncan Calvert <dcalvert@ihmc.us>
2019-04-15 16:13:13 -0500

    Add operator review state.

--- ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/behaviors/DirectRobotUIController.java
+++ ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/behaviors/DirectRobotUIController.java
@@ -3,6 +3,7 @@ package us.ihmc.humanoidBehaviors.ui.behaviors;
 import controller_msgs.msg.dds.BipedalSupportPlanarRegionParametersMessage;
 import controller_msgs.msg.dds.FlatGroundPlanarRegionParametersMessage;
 import controller_msgs.msg.dds.GoHomeMessage;
+import controller_msgs.msg.dds.REAStateRequestMessage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -18,6 +19,7 @@ import us.ihmc.humanoidBehaviors.tools.FlatGroundPlanarRegionPublisher;
 import us.ihmc.humanoidBehaviors.ui.tools.AtlasDirectRobotInterface;
 import us.ihmc.humanoidBehaviors.ui.tools.ValkyrieDirectRobotInterface;
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.GoHomeCommand;
+import us.ihmc.robotEnvironmentAwareness.updaters.LIDARBasedREAModule;
 import us.ihmc.ros2.Ros2Node;
 
 public class DirectRobotUIController
@@ -27,13 +29,15 @@ public class DirectRobotUIController
    @FXML private Button standPrep;
    @FXML private Button shutdown;
    @FXML private CheckBox enableSupportRegions;
-   @FXML private CheckBox enableFlatGroundRegion;
    @FXML private Spinner<Double> supportRegionScale;
+//   @FXML private CheckBox enableFlatGroundRegion;
+   @FXML private Button clearREA;
 
    private RobotLowLevelMessenger robotLowLevelMessenger;
    private IHMCROS2Publisher<GoHomeMessage> goHomePublisher;
    private IHMCROS2Publisher<BipedalSupportPlanarRegionParametersMessage> supportRegionsParametersPublisher;
    private IHMCROS2Publisher<FlatGroundPlanarRegionParametersMessage> flatGroundRegionParametersPublisher;
+   private IHMCROS2Publisher<REAStateRequestMessage> reaStateRequestPublisher;
 
    public void init(Ros2Node ros2Node, DRCRobotModel robotModel)
    {
@@ -64,6 +68,7 @@ public class DirectRobotUIController
                                                                     FlatGroundPlanarRegionParametersMessage.class,
                                                                     null,
                                                                     FlatGroundPlanarRegionPublisher.ROS2_ID);
+      reaStateRequestPublisher = new IHMCROS2Publisher<>(ros2Node, REAStateRequestMessage.class, null, LIDARBasedREAModule.ROS2_ID);
 
       supportRegionScale.setValueFactory(new DoubleSpinnerValueFactory(0.0, 10.0, 2.0, 0.1));
       enableSupportRegions.setSelected(true);
@@ -110,11 +115,19 @@ public class DirectRobotUIController
       supportRegionsParametersPublisher.publish(supportPlanarRegionParametersMessage);
    }
 
+//   @FXML
+//   public void sendFlatGroundRegionParameters()
+//   {
+//      FlatGroundPlanarRegionParametersMessage message = new FlatGroundPlanarRegionParametersMessage();
+//      message.setEnable(enableFlatGroundRegion.isSelected());
+//      flatGroundRegionParametersPublisher.publish(message);
+//   }
+
    @FXML
-   public void sendFlatGroundRegionParameters()
+   public void clearREA()
    {
-      FlatGroundPlanarRegionParametersMessage message = new FlatGroundPlanarRegionParametersMessage();
-      message.setEnable(enableFlatGroundRegion.isSelected());
-      flatGroundRegionParametersPublisher.publish(message);
+      REAStateRequestMessage clearMessage = new REAStateRequestMessage();
+      clearMessage.setRequestClear(true);
+      reaStateRequestPublisher.publish(clearMessage);
    }
 }
