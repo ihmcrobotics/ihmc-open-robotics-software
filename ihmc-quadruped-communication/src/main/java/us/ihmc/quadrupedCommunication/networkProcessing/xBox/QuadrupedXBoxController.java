b4226e3135b14c0f50a2da7c987354f3413717d6
Robert <rgriffin@ihmc.us>
2019-04-15 13:19:39 -0500

    tuned xbox for open house

--- ihmc-quadruped-communication/src/main/java/us/ihmc/quadrupedCommunication/networkProcessing/xBox/QuadrupedXBoxController.java
+++ ihmc-quadruped-communication/src/main/java/us/ihmc/quadrupedCommunication/networkProcessing/xBox/QuadrupedXBoxController.java
@@ -85,8 +85,8 @@ public class QuadrupedXBoxController extends QuadrupedToolboxController implemen
       maxTranslationX.set(0.25);
       maxTranslationY.set(0.15);
 
-      maxYSpeedFraction.set(0.25);
-      maxYawSpeedFraction.set(0.35);
+      maxYSpeedFraction.set(0.3);
+      maxYawSpeedFraction.set(0.4);
 
       bodyOrientationShiftTime.set(0.1);
       this.bodyHeight = new InputValueIntegrator(Conversions.millisecondsToSeconds(updateTimeInMs), nominalBodyHeight);
