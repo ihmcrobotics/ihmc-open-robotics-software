740189e3dd879d74e97134cada6191fd162b9b4b
Georg Wiedebach <gwiedebach@ihmc.us>
2019-04-17 12:35:31 -0500

    Made Atlas-specific context such that the raw joint data does not need to live in the general purpose robot context.

---
+++ atlas/src/main/java/us/ihmc/atlas/barrierScheduler/context/AtlasHumanoidRobotContextDataFactory.java
@@ -0,0 +1,26 @@
+package us.ihmc.atlas.barrierScheduler.context;
+
+import us.ihmc.realtime.barrierScheduler.context.HumanoidRobotContextDataFactory;
+import us.ihmc.sensorProcessing.sensors.RawJointSensorDataHolderMap;
+import us.ihmc.tools.factories.FactoryTools;
+import us.ihmc.tools.factories.RequiredFactoryField;
+
+public class AtlasHumanoidRobotContextDataFactory extends HumanoidRobotContextDataFactory
+{
+   private final RequiredFactoryField<RawJointSensorDataHolderMap> rawJointData = new RequiredFactoryField<>("rawJointData");
+
+   @Override
+   public AtlasHumanoidRobotContextData createHumanoidRobotContextData()
+   {
+      // TODO: this does not check fields of super factory.
+      FactoryTools.checkAllFactoryFieldsAreSet(this);
+
+      return new AtlasHumanoidRobotContextData(processedJointData.get(), forceSensorDataHolder.get(), centerOfPressureDataHolder.get(),
+                                               robotMotionStatusHolder.get(), jointDesiredOutputList.get(), rawJointData.get());
+   }
+
+   public void setRawJointData(RawJointSensorDataHolderMap value)
+   {
+      this.rawJointData.set(value);
+   }
+}
