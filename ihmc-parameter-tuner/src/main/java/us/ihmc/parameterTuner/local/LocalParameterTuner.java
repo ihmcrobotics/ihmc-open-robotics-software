5c9866a6476691468b676ef8d613306291c273bb
Duncan Calvert <dcalvert@ihmc.us>
2019-04-17 14:06:29 -0500

    First try on local parameter tuner, probably not even close, might have to restart.

---
+++ ihmc-parameter-tuner/src/main/java/us/ihmc/parameterTuner/local/LocalParameterTuner.java
@@ -0,0 +1,13 @@
+package us.ihmc.parameterTuner.local;
+
+import us.ihmc.parameterTuner.guiElements.main.ParameterGuiInterface;
+import us.ihmc.parameterTuner.guiElements.main.ParameterTuningApplication;
+
+public class LocalParameterTuner extends ParameterTuningApplication
+{
+   @Override
+   protected ParameterGuiInterface createInputManager()
+   {
+      return null;
+   }
+}
