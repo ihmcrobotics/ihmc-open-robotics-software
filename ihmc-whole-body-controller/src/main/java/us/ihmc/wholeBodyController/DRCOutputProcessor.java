2dc27110684500e2302e6fb4864820d3f2dd6019
Georg Wiedebach <gwiedebach@ihmc.us>
2019-04-17 10:48:03 -0500

    Remove `RawJointSensorDataHolderMap` from general purpose output writer.

--- ihmc-whole-body-controller/src/main/java/us/ihmc/wholeBodyController/DRCOutputProcessor.java
+++ ihmc-whole-body-controller/src/main/java/us/ihmc/wholeBodyController/DRCOutputProcessor.java
@@ -3,7 +3,6 @@ package us.ihmc.wholeBodyController;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.sensors.ForceSensorDataHolderReadOnly;
 import us.ihmc.sensorProcessing.outputData.JointDesiredOutputList;
-import us.ihmc.sensorProcessing.sensors.RawJointSensorDataHolderMap;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
 public interface DRCOutputProcessor
@@ -12,7 +11,7 @@ public interface DRCOutputProcessor
    
    public abstract void processAfterController(long timestamp);
 
-   public abstract void setLowLevelControllerCoreOutput(FullHumanoidRobotModel controllerRobotModel, JointDesiredOutputList lowLevelControllerCoreOutput, RawJointSensorDataHolderMap rawJointSensorDataHolderMap);
+   public abstract void setLowLevelControllerCoreOutput(FullHumanoidRobotModel controllerRobotModel, JointDesiredOutputList lowLevelControllerCoreOutput);
 
    public abstract void setForceSensorDataHolderForController(ForceSensorDataHolderReadOnly forceSensorDataHolderForController);
      
