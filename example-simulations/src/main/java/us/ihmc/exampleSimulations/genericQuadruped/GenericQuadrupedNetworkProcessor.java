37c0b6d94430d14cda9d736938c890750a950667
Robert <rgriffin@ihmc.us>
2019-04-22 16:33:48 -0500

    set up a quadruped planar region provider

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/GenericQuadrupedNetworkProcessor.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/GenericQuadrupedNetworkProcessor.java
@@ -1,5 +1,6 @@
 package us.ihmc.exampleSimulations.genericQuadruped;
 
+import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.exampleSimulations.genericQuadruped.model.GenericQuadrupedModelFactory;
 import us.ihmc.exampleSimulations.genericQuadruped.model.GenericQuadrupedPhysicalProperties;
 import us.ihmc.exampleSimulations.genericQuadruped.parameters.GenericQuadrupedPointFootSnapperParameters;
@@ -12,20 +13,25 @@ import us.ihmc.quadrupedPlanning.footstepChooser.PointFootSnapperParameters;
 import us.ihmc.quadrupedCommunication.networkProcessing.QuadrupedNetworkModuleParameters;
 import us.ihmc.quadrupedCommunication.networkProcessing.QuadrupedNetworkProcessor;
 import us.ihmc.robotModels.FullQuadrupedRobotModelFactory;
+import us.ihmc.robotics.robotSide.QuadrantDependentList;
+
+import java.util.ArrayList;
 
 public class GenericQuadrupedNetworkProcessor extends QuadrupedNetworkProcessor
 {
    public GenericQuadrupedNetworkProcessor(DomainFactory.PubSubImplementation pubSubImplementation, QuadrupedNetworkModuleParameters networkModuleParameters)
    {
-      this(new GenericQuadrupedModelFactory(), new GenericQuadrupedPhysicalProperties().getNominalBodyHeight(), new DefaultFootstepPlannerParameters(),
+      this(new GenericQuadrupedModelFactory(), new GenericQuadrupedPhysicalProperties().getNominalBodyHeight(),
+           new GenericQuadrupedPhysicalProperties().getFeetGroundContactPoints(), new DefaultFootstepPlannerParameters(),
            new GenericQuadrupedXGaitSettings(), new GenericQuadrupedPointFootSnapperParameters(), pubSubImplementation, networkModuleParameters);
    }
 
-   public GenericQuadrupedNetworkProcessor(FullQuadrupedRobotModelFactory robotModel, double nominalHeight, FootstepPlannerParameters footstepPlannerParameters,
+   public GenericQuadrupedNetworkProcessor(FullQuadrupedRobotModelFactory robotModel, double nominalHeight,
+                                           QuadrantDependentList<ArrayList<Point2D>> groundContactPoints, FootstepPlannerParameters footstepPlannerParameters,
                                            QuadrupedXGaitSettingsReadOnly xGaitSettings, PointFootSnapperParameters pointFootSnapperParameters,
                                            DomainFactory.PubSubImplementation pubSubImplementation, QuadrupedNetworkModuleParameters networkModuleParameters)
    {
-      super(robotModel, networkModuleParameters, nominalHeight, footstepPlannerParameters, xGaitSettings, pointFootSnapperParameters, pubSubImplementation);
+      super(robotModel, networkModuleParameters, nominalHeight, groundContactPoints, footstepPlannerParameters, xGaitSettings, pointFootSnapperParameters, pubSubImplementation);
    }
 
    public static void main(String[] args)
