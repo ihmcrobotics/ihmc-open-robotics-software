084fedf84d7d2c57aa8811822fc197418a778451
Robert <rgriffin@ihmc.us>
2019-04-23 11:04:58 -0500

    lowered the threshold for the vis graph incline filter to force the body path environment to take the far path

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/AStarToolboxDataSetTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/networkProcessor/footstepPlanningToolboxModule/AStarToolboxDataSetTest.java
@@ -89,7 +89,7 @@ public class AStarToolboxDataSetTest extends FootstepPlannerToolboxDataSetTest
       test.pubSubImplementation = DomainFactory.PubSubImplementation.INTRAPROCESS;
       VISUALIZE = true;
       test.setup();
-      test.runAssertionsOnDataset(test::runAssertions, DataSetName._20171215_214730_CinderBlockField);
+      test.runAssertionsOnDataset(test::runAssertions, DataSetName._20171218_205120_BodyPathPlannerEnvironment);
 
       ThreadTools.sleepForever();
       test.tearDown();
