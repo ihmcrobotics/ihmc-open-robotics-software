dd1ca6487322800cea13cc95aa3aa50c9533b70b
smccrory <smccrory@ihmc.us>
2019-04-24 18:03:44 -0500

    Adding a check that steps are snappable in StepGeneratorJavaFXController

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/FootstepValidityIndicator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/FootstepValidityIndicator.java
@@ -1,6 +1,7 @@
 package us.ihmc.commonWalkingControlModules.desiredFootStep.footstepGenerator;
 
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
+import us.ihmc.robotics.robotSide.RobotSide;
 
 /**
  * This interface is part of the API of {@link ContinuousStepGenerator}.
@@ -16,6 +17,7 @@ public interface FootstepValidityIndicator
     * Checks if the given pose is steppable.
     *
     * @param solePose footstep to check
+    * @param robotSide side of the given footstep
     */
-   boolean isFootstepValid(FramePose3DReadOnly solePose);
+   boolean isFootstepValid(FramePose3DReadOnly solePose, RobotSide robotSide);
 }
