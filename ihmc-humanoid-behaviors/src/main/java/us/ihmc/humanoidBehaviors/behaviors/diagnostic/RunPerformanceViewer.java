26bd1d6dbda1fa16f108cf3c1c6a31363650619d
MJ <mjohnson@ihmc.us>
2019-04-24 17:19:58 -0500

    set column properties

--- ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/diagnostic/RunPerformanceViewer.java
+++ ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/diagnostic/RunPerformanceViewer.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
@@ -15,25 +16,27 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class RunPerformanceViewer extends Application
+public class RunPerformanceViewer extends Application implements Initializable
 {
    public SQLBehaviorDatabaseManager dataBase = new SQLBehaviorDatabaseManager();
    private ObservableList<Run> runsList = FXCollections.observableArrayList();
 
    @FXML
-   private TableView<Run> runsTable = new TableView<>();
+   private TableView<Run> runsTable;
    @FXML
-   private TableColumn<Run, String> runTableColumn = new TableColumn<>();
+   private TableColumn<Run, String> runTableColumn;
    @FXML
-   private TableColumn<Run, String> operatorTableColumn = new TableColumn<>();;
+   private TableColumn<Run, String> operatorTableColumn;
    @FXML
-   private TableColumn<Run, String> taskTableColumn = new TableColumn<>();;
+   private TableColumn<Run, String> taskTableColumn;
    @FXML
-   private TableColumn<Run, String> successTableColumn = new TableColumn<>();;
+   private TableColumn<Run, String> successTableColumn;
    @FXML
-   private TableColumn<Run, String> notesTableColumn = new TableColumn<>();;
+   private TableColumn<Run, String> notesTableColumn;
    @FXML
    private TextField numberOfRunsTextField;
    @FXML
@@ -54,7 +57,6 @@ public class RunPerformanceViewer extends Application
       }
       runsList.addAll(allRuns);
       System.out.println("runsList = " + runsList.size());
-
    }
 
    @FXML
@@ -77,7 +79,11 @@ public class RunPerformanceViewer extends Application
       stage.setTitle(getClass().getSimpleName());
       stage.setScene(scene);
       stage.show();
+   }
 
+   @Override
+   public void initialize(URL location, ResourceBundle resources)
+   {
       runTableColumn.setCellValueFactory(new PropertyValueFactory<>("runID"));
       operatorTableColumn.setCellValueFactory(new PropertyValueFactory<>("operatorID"));
       taskTableColumn.setCellValueFactory(new PropertyValueFactory<>("taskID"));
