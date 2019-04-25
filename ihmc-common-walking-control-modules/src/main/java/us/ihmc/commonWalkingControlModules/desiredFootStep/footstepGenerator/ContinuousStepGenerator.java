0571d322dfd87f73c22569adfdc7d20f5c7ecf5f
smccrory <smccrory@ihmc.us>
2019-04-25 09:56:46 -0500

    adding docs

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/ContinuousStepGenerator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/ContinuousStepGenerator.java
@@ -90,6 +90,11 @@ import us.ihmc.yoVariables.variable.YoInteger;
  * behavior, see {@link #setFootstepAdjustment(FootstepAdjustment)},
  * {@link #setHeightMapBasedFootstepAdjustment(HeightMap)}, and
  * {@link #setSupportFootBasedFootstepAdjustment(boolean)}.
+ * <li><u>Method for indicating step validity and providing alternative steps for invalid ones:</u><br>
+ * See {@link #setFootstepValidityIndicator(FootstepValidityIndicator)} to set method for indicating invalid steps.
+ * By default, no steps are deemed invalid. See {@link #setAlternateStepChooser(AlternateStepChooser)} to set
+ * how an alternative step is chosen if a step is invalid. The default is to do a square-up step,
+ * see {@link #calculateSquareUpStep}.
  * <li><u>Frequency at which footsteps are sent to the controller:</u><br>
  * See {@link #setNumberOfTicksBeforeSubmittingFootsteps(int)}.
  * </ul>
