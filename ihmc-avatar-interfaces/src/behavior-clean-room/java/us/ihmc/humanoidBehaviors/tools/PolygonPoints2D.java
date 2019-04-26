c81a878d87c23c0c10e9b7eeaf7e04e4f6b7f498
Duncan Calvert <dcalvert@ihmc.us>
2019-04-26 14:06:24 -0500

    Initial implementation of down navigation to large level region.

---
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/PolygonPoints2D.java
@@ -0,0 +1,58 @@
+package us.ihmc.humanoidBehaviors.tools;
+
+import us.ihmc.euclid.referenceFrame.FramePoint2D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+
+import java.util.ArrayList;
+
+/**
+ * N corner points around one in the center in 2D, forming the points of a polygon plus a center one
+ *
+ * Like so:
+ *
+ * <pre>
+ *       *
+ *    *  *  *
+ *      *  *
+ * </pre>
+ *
+ * TODO add layers, like an onion
+ */
+public class PolygonPoints2D
+{
+   private final ArrayList<FramePoint2D> points = new ArrayList<>();
+   private final FramePoint2D centerPoint;
+
+   public PolygonPoints2D(int numberOfPoints, double radius, ReferenceFrame referenceFrame)
+   {
+      centerPoint = new FramePoint2D(referenceFrame);
+      getPoints().add(centerPoint); // center
+
+      double angleStepSize = Math.PI * 2.0 / (double) numberOfPoints;
+
+      for (int i = 0; i < numberOfPoints; i++)
+      {
+         double x = radius * Math.cos(i * angleStepSize);
+         double y = radius * Math.sin(i * angleStepSize);
+         getPoints().add(new FramePoint2D(referenceFrame, x, y));
+      }
+   }
+
+   public void add(double x, double y)
+   {
+      for (FramePoint2D point : points)
+      {
+         point.add(x, y);
+      }
+   }
+
+   public FramePoint2D getCenterPoint()
+   {
+      return centerPoint;
+   }
+
+   public ArrayList<FramePoint2D> getPoints()
+   {
+      return points;
+   }
+}
