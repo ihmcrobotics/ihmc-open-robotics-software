116ac487009f6e41ad9da2910e7dbcbd9da0eb6b
Robert <rgriffin@ihmc.us>
2019-05-01 12:03:41 -0500

    added the height change to the quadruped heuristic

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/SpeedAndYawBasedHeuristics.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/SpeedAndYawBasedHeuristics.java
@@ -1,22 +1,25 @@
 package us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.heuristics;
 
-import us.ihmc.euclid.axisAngle.AxisAngle;
-import us.ihmc.euclid.tuple2D.Vector2D;
-import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.footstepSnapping.FootstepNodeSnapData;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.footstepSnapping.FootstepNodeSnapper;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph.FootstepNode;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph.FootstepNodeTools;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.parameters.FootstepPlannerParameters;
 import us.ihmc.quadrupedPlanning.QuadrupedXGaitSettingsReadOnly;
 import us.ihmc.robotics.geometry.AngleTools;
+import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public class SpeedAndYawBasedHeuristics extends CostToGoHeuristics
 {
    private final QuadrupedXGaitSettingsReadOnly xGaitSettings;
-   private final static Vector2D forward = new Vector2D(1.0, 0.0);
+   private final FootstepNodeSnapper snapper;
 
-   public SpeedAndYawBasedHeuristics(FootstepPlannerParameters parameters, QuadrupedXGaitSettingsReadOnly xGaitSettings)
+   public SpeedAndYawBasedHeuristics(FootstepNodeSnapper snapper, FootstepPlannerParameters parameters, QuadrupedXGaitSettingsReadOnly xGaitSettings)
    {
       super(parameters);
 
+      this.snapper = snapper;
       this.xGaitSettings = xGaitSettings;
    }
 
@@ -27,16 +30,42 @@ public class SpeedAndYawBasedHeuristics extends CostToGoHeuristics
       double desiredSpeed = parameters.getMaxWalkingSpeedMultiplier() * xGaitSettings.getMaxSpeed();
       double minSteps = bodyDistance / desiredSpeed;
 
-      double yawHeuristicCost;
       double referenceYaw = computeReferenceYaw(node, goalNode);
-
-
       double angleDifference = AngleTools.computeAngleDifferenceMinusPiToPi(node.getNominalYaw(), referenceYaw);
-      yawHeuristicCost = parameters.getYawWeight() * Math.abs(angleDifference);
+      double yawHeuristicCost = parameters.getYawWeight() * Math.abs(angleDifference);
 
       double stepHeuristicCost = 4.0 * parameters.getCostPerStep() * minSteps;
 
-      return yawHeuristicCost + stepHeuristicCost + parameters.getDistanceHeuristicWeight() * bodyDistance;
+      RigidBodyTransform startNodeTransform = new RigidBodyTransform();
+      RigidBodyTransform endNodeTransform = new RigidBodyTransform();
+      
+      double heightCost = 0.0;
+
+      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
+      {
+         int endNodeXIndex = goalNode.getXIndex(robotQuadrant);
+         int endNodeYIndex = goalNode.getYIndex(robotQuadrant);
+         int startNodeXIndex = node.getXIndex(robotQuadrant);
+         int startNodeYIndex = node.getYIndex(robotQuadrant);
+
+         FootstepNodeSnapData endNodeData = snapper.getSnapData(endNodeXIndex, endNodeYIndex);
+         FootstepNodeSnapData startNodeData = snapper.getSnapData(startNodeXIndex, startNodeYIndex);
+
+         if (startNodeData == null || endNodeData == null)
+            return 0.0;
+
+         FootstepNodeTools.getSnappedNodeTransformToWorld(startNodeXIndex, startNodeYIndex, startNodeData.getSnapTransform(), startNodeTransform);
+         FootstepNodeTools.getSnappedNodeTransformToWorld(endNodeXIndex, endNodeYIndex, endNodeData.getSnapTransform(), endNodeTransform);
+
+         double heightChange = endNodeTransform.getTranslationVector().getZ() - startNodeTransform.getTranslationVector().getZ();
+
+         if (heightChange > 0.0)
+            heightCost += parameters.getStepUpWeight() * heightChange;
+         else
+            heightCost += -parameters.getStepDownWeight() * heightChange;
+      }
+
+      return yawHeuristicCost + stepHeuristicCost + heightCost + parameters.getDistanceHeuristicWeight() * bodyDistance;
    }
 
    private double computeReferenceYaw(FootstepNode node, FootstepNode goalNode)
