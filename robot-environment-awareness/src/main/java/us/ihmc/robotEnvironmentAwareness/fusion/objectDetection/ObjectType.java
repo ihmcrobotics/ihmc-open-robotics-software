803051f23c499c8f390705a6bda3edf899b998c7
inhol <ilee@ihmc.us>
2019-05-02 17:28:41 -0500

    Included color in object type for roi visualization.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/objectDetection/ObjectType.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/objectDetection/ObjectType.java
@@ -1,6 +1,24 @@
 package us.ihmc.robotEnvironmentAwareness.fusion.objectDetection;
 
+import java.awt.Color;
+
 public enum ObjectType
 {
-   Door, DoorHandle, Cup, Human
+   Door, DoorHandle, Cup, Human;
+   Color getROIColor()
+   {
+      switch (this)
+      {
+      case Cup:
+         return Color.WHITE;
+      case Door:
+         return Color.GREEN;
+      case DoorHandle:
+         return Color.BLUE;
+      case Human:
+         return Color.ORANGE;
+      default:
+         return null;
+      }
+   }
 }
