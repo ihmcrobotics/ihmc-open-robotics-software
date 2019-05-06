00cad9903004ac3622d56d8e8dd3369ef7bfb24c
Duncan Calvert <dcalvert@ihmc.us>
2019-05-06 17:16:07 -0500

    First stage write. About to change again so commit.

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/BipedFootstepPlannerParameterKeys.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/BipedFootstepPlannerParameterKeys.java
@@ -3,59 +3,54 @@ package us.ihmc.footstepPlanning.graphSearch.parameters;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BipedFootstepPlannerParameterKeys implements FootstepPlannerParameterKeys
+public class BipedFootstepPlannerParameterKeys
 {
    private static int idCount = 0;
 
    public static final List<FootstepPlannerParameterKey<?>> keys = new ArrayList<>();
-   public static final String saveFileName = "bipedFootstepPlannerParameters.ini";
+   public static final String saveFileName = "bipedFootstepPlannerParameters";
 
-   public static final FootstepPlannerParameterKey<Double > idealFootstepWidth                  = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > idealFootstepLength                 = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > wiggleInsideDelta                   = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Boolean> wiggleIntoConvexHullOfPlanarRegions = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Boolean> rejectIfCannotFullyWiggleInside     = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maximumXYWiggleDistance             = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maximumYawWiggle                    = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maxStepReach                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maxStepYaw                          = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minStepWidth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minStepLength                       = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minStepYaw                          = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maxStepZ                            = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minFootholdPercent                  = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minSurfaceIncline                   = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maxStepWidth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minXClearanceFromStance             = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minYClearanceFromStance             = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maximumStepReachWhenSteppingUp      = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maximumStepZWhenSteppingUp          = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maximumStepXWhenForwardAndDown      = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maximumStepZWhenForwardAndDown      = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > maximumZPenetrationOnValleyRegions  = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > cliffHeightToAvoid                  = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > minimumDistanceFromCliffBottoms     = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Boolean> returnBestEffortPlan                = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Integer> minimumStepsForBestEffortPlan       = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > bodyGroundClearance                 = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Boolean> checkForBodyBoxCollisions           = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Boolean> performHeuristicSearchPolicies      = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > bodyBoxWidth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > bodyBoxHeight                       = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > bodyBoxDepth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > bodyBoxBaseX                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > bodyBoxBaseY                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
-   public static final FootstepPlannerParameterKey<Double > bodyBoxBaseZ                        = new FootstepPlannerParameterKey<>(keys, idCount++, "");
+   public static final FootstepPlannerParameterKey<Double > idealFootstepWidth                  = new FootstepPlannerParameterKey<>(keys, idCount++, "Ideal footstep width"                      );
+   public static final FootstepPlannerParameterKey<Double > idealFootstepLength                 = new FootstepPlannerParameterKey<>(keys, idCount++, "Ideal footstep length"                     );
+   public static final FootstepPlannerParameterKey<Double > wiggleInsideDelta                   = new FootstepPlannerParameterKey<>(keys, idCount++, "Wiggle inside delta"                       );
+   public static final FootstepPlannerParameterKey<Boolean> wiggleIntoConvexHullOfPlanarRegions = new FootstepPlannerParameterKey<>(keys, idCount++, "Wiggle into convex hull of planar regions" );
+   public static final FootstepPlannerParameterKey<Boolean> rejectIfCannotFullyWiggleInside     = new FootstepPlannerParameterKey<>(keys, idCount++, "Reject if cannot fully wiggle inside"      );
+   public static final FootstepPlannerParameterKey<Double > maximumXYWiggleDistance             = new FootstepPlannerParameterKey<>(keys, idCount++, "Max XY wiggle distance"                    );
+   public static final FootstepPlannerParameterKey<Double > maximumYawWiggle                    = new FootstepPlannerParameterKey<>(keys, idCount++, "Max yaw wiggle"                            );
+   public static final FootstepPlannerParameterKey<Double > maxStepReach                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step reach"                            );
+   public static final FootstepPlannerParameterKey<Double > maxStepYaw                          = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step yaw"                              );
+   public static final FootstepPlannerParameterKey<Double > minStepWidth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Min step width"                            );
+   public static final FootstepPlannerParameterKey<Double > minStepLength                       = new FootstepPlannerParameterKey<>(keys, idCount++, "Min step length"                           );
+   public static final FootstepPlannerParameterKey<Double > minStepYaw                          = new FootstepPlannerParameterKey<>(keys, idCount++, "Min step yaw"                              );
+   public static final FootstepPlannerParameterKey<Double > maxStepZ                            = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step Z"                                );
+   public static final FootstepPlannerParameterKey<Double > minFootholdPercent                  = new FootstepPlannerParameterKey<>(keys, idCount++, "Min foothold percent"                      );
+   public static final FootstepPlannerParameterKey<Double > minSurfaceIncline                   = new FootstepPlannerParameterKey<>(keys, idCount++, "Min surface incline"                       );
+   public static final FootstepPlannerParameterKey<Double > maxStepWidth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step width"                            );
+   public static final FootstepPlannerParameterKey<Double > minXClearanceFromStance             = new FootstepPlannerParameterKey<>(keys, idCount++, "Min X clearance from stance"               );
+   public static final FootstepPlannerParameterKey<Double > minYClearanceFromStance             = new FootstepPlannerParameterKey<>(keys, idCount++, "Min Y clearance from stance"               );
+   public static final FootstepPlannerParameterKey<Double > maximumStepReachWhenSteppingUp      = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step reach when stepping up"           );
+   public static final FootstepPlannerParameterKey<Double > maximumStepZWhenSteppingUp          = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step Z when stepping up"               );
+   public static final FootstepPlannerParameterKey<Double > maximumStepXWhenForwardAndDown      = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step X when forward and down"          );
+   public static final FootstepPlannerParameterKey<Double > maximumStepZWhenForwardAndDown      = new FootstepPlannerParameterKey<>(keys, idCount++, "Max step Z when forward and down"          );
+   public static final FootstepPlannerParameterKey<Double > maximumZPenetrationOnValleyRegions  = new FootstepPlannerParameterKey<>(keys, idCount++, "Max Z penetration on valley regions"       );
+   public static final FootstepPlannerParameterKey<Double > cliffHeightToAvoid                  = new FootstepPlannerParameterKey<>(keys, idCount++, "Cliff height to avoid"                     );
+   public static final FootstepPlannerParameterKey<Double > minimumDistanceFromCliffBottoms     = new FootstepPlannerParameterKey<>(keys, idCount++, "Min distance from cliff bottoms"           );
+   public static final FootstepPlannerParameterKey<Boolean> returnBestEffortPlan                = new FootstepPlannerParameterKey<>(keys, idCount++, "Return best effort plan"                   );
+   public static final FootstepPlannerParameterKey<Integer> minimumStepsForBestEffortPlan       = new FootstepPlannerParameterKey<>(keys, idCount++, "Min steps for best effort plan"            );
+   public static final FootstepPlannerParameterKey<Double > bodyGroundClearance                 = new FootstepPlannerParameterKey<>(keys, idCount++, "Body ground clearance"                     );
+   public static final FootstepPlannerParameterKey<Boolean> checkForBodyBoxCollisions           = new FootstepPlannerParameterKey<>(keys, idCount++, "Check for body box collisions"             );
+   public static final FootstepPlannerParameterKey<Boolean> performHeuristicSearchPolicies      = new FootstepPlannerParameterKey<>(keys, idCount++, "Perform heuristic search policies"         );
+   public static final FootstepPlannerParameterKey<Double > bodyBoxWidth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Body box width"                            );
+   public static final FootstepPlannerParameterKey<Double > bodyBoxHeight                       = new FootstepPlannerParameterKey<>(keys, idCount++, "Body box height"                           );
+   public static final FootstepPlannerParameterKey<Double > bodyBoxDepth                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Body box depth"                            );
+   public static final FootstepPlannerParameterKey<Double > bodyBoxBaseX                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Body box base X"                           );
+   public static final FootstepPlannerParameterKey<Double > bodyBoxBaseY                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Body box base Y"                           );
+   public static final FootstepPlannerParameterKey<Double > bodyBoxBaseZ                        = new FootstepPlannerParameterKey<>(keys, idCount++, "Body box base Z"                           );
 
-   @Override
-   public List<FootstepPlannerParameterKey<?>> getKeys()
+   public static void main(String[] args)
    {
-      return keys;
-   }
+      FootstepPlannerParameterMap map = new FootstepPlannerParameterMap(BipedFootstepPlannerParameterKeys.keys, saveFileName);
+      map.printInitializedSaveFile();
 
-   @Override
-   public String getSaveFileName()
-   {
-      return saveFileName;
    }
 }
--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/FootstepPlannerParameterMap.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/FootstepPlannerParameterMap.java
@@ -1,16 +1,182 @@
 package us.ihmc.footstepPlanning.graphSearch.parameters;
 
-import java.util.HashMap;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ClassUtils;
+import org.ini4j.Ini;
+import org.ini4j.IniPreferences;
+import us.ihmc.commons.exception.DefaultExceptionHandler;
+import us.ihmc.commons.exception.ExceptionTools;
+import us.ihmc.log.LogTools;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Properties;
+import java.util.TreeMap;
+import java.util.prefs.BackingStoreException;
 
 public class FootstepPlannerParameterMap
 {
-   private final HashMap<String, FootstepPlannerParameterKey<?>> bySaveName = new HashMap<>();
-   private final HashMap<Integer, FootstepPlannerParameterKey<?>> byId = new HashMap<>();
-   private final HashMap<Integer, FootstepPlannerParameterKey<?>> byId = new HashMap<>();
+   private final List<FootstepPlannerParameterKey<?>> parameterKeys;
+   private final String saveFileName;
 
-   public FootstepPlannerParameterMap(FootstepPlannerParameterKeys parameterKeys)
+   private final TreeMap<Integer, FootstepPlannerParameter<?>> values = new TreeMap<>();
+
+   public FootstepPlannerParameterMap(List<FootstepPlannerParameterKey<?>> parameterKeys, String saveFileName)
    {
+      this.parameterKeys = parameterKeys;
+      this.saveFileName = saveFileName + ".ini";
 
+      reload();
+
+      for (FootstepPlannerParameterKey<?> key : parameterKeys)
+      {
+//         values.put(key.getId(), )
+      }
+   }
+
+   public <T> T getValue(FootstepPlannerParameterKey<T> key)
+   {
+      FootstepPlannerParameter<T> footstepPlannerParameter = (FootstepPlannerParameter<T>) values.get(key.getId());
+      return footstepPlannerParameter.getValue();
+   }
+
+   public void reload()
+   {
+      ExceptionTools.handle(() ->
+      {
+//         Ini ini = new Ini(accessStreamForLoading());
+//         IniPreferences preferences = new IniPreferences(ini);
+
+         Properties properties = new Properties();
+         properties.load(accessStreamForLoading());
+
+//         properties.containsKey()
+
+         for (FootstepPlannerParameterKey<?> key : parameterKeys)
+         {
+            if (!properties.containsKey(key.getSaveName()))
+            {
+               throw new RuntimeException(accessUrlForLoading() + " does not contain key: " + key.getSaveName());
+            }
+
+            Object stringValue = properties.get(key.getSaveName());
+
+            LogTools.info("{}: ({}) {}", key.getSaveName(), stringValue.getClass().getSimpleName(), stringValue);
+
+
+//            ClassUtils.isPrimitiveOrWrapper()
+//
+//
+//            if (Double.valueOf(stringValue))
+
+//            values.put(key.getId(), );
+         }
+
+//         for (String key : preferences.keys())
+//         {
+//            System.out.println(key);
+//
+//
+//
+//         }
+
+      }, DefaultExceptionHandler.PRINT_STACKTRACE);
+   }
+
+   private boolean preferencesContainKey(IniPreferences preferences, FootstepPlannerParameterKey<?> key) throws BackingStoreException
+   {
+      for (String foundKey : preferences.keys())
+      {
+         LogTools.info("{}:{}", foundKey, key.getSaveName());
+         if (foundKey.equals(key.getSaveName()))
+         {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   public void save()
+   {
+      ExceptionTools.handle(() ->
+      {
+         Ini ini = new Ini(accessStreamForLoading());
+         IniPreferences preferences = new IniPreferences(ini);
+
+         for (FootstepPlannerParameterKey<?> parameterKey : parameterKeys)
+         {
+            preferences.put(parameterKey.getSaveName(), "");
+         }
+
+         ini.store();
+      }, DefaultExceptionHandler.PRINT_STACKTRACE);
+   }
+
+   public void printInitializedSaveFile()
+   {
+      for (FootstepPlannerParameterKey<?> parameterKey : parameterKeys)
+      {
+         System.out.println(parameterKey.getSaveName() + "=");
+      }
+   }
+
+   private InputStream accessStreamForLoading()
+   {
+      return getClass().getResourceAsStream(saveFileName);
+   }
+
+   private URL accessUrlForLoading()
+   {
+      return getClass().getResource(saveFileName);
+   }
+
+   private File findFileForSaving()
+   {
+      return findSaveFileDirectory().resolve(saveFileName).toFile();
+   }
+
+   private Path findSaveFileDirectory()
+   {
+      // find ihmc-open-robotics-software/ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters
+      // of just save the file in the working directory
+
+      Path absoluteWorkingDirectory = Paths.get(".").toAbsolutePath().normalize();
+      LogTools.info(absoluteWorkingDirectory.toString());
+
+      Path reworkedPath = Paths.get("/").toAbsolutePath().normalize();
+      boolean openRoboticsFound = false;
+      for (Path path : absoluteWorkingDirectory)
+      {
+         LogTools.info("Part: {}", path.toString());
+
+         reworkedPath = reworkedPath.resolve(path); // building up the path
+
+         if (path.toString().equals("ihmc-open-robotics-software"))
+         {
+            openRoboticsFound = true;
+            break;
+         }
+      }
+
+      if (!openRoboticsFound)
+      {
+         LogTools.warn("Directory \"ihmc-open-robotics-software\" could not be found to save parameters. Using working directory {}",
+                       absoluteWorkingDirectory);
+         return absoluteWorkingDirectory;
+      }
+
+      LogTools.info("Reworked path: {}", reworkedPath);
+
+      Path subPath = Paths.get("ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters");
+
+      Path finalPath = reworkedPath.resolve(subPath);
+      LogTools.info("Final path: {}", finalPath);
+
+      return finalPath;
    }
 }
