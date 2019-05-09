3be5920e27ebcc3d17b00963997593f945ea4d1c
Georg Wiedebach <gwiedebach@ihmc.us>
2019-05-09 12:45:35 -0500

    Add some timing yo variables.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/EstimatorTask.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/EstimatorTask.java
@@ -3,9 +3,11 @@ package us.ihmc.avatar;
 import us.ihmc.avatar.factory.HumanoidRobotControlTask;
 import us.ihmc.commonWalkingControlModules.barrierScheduler.context.HumanoidRobotContextData;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.CrossRobotCommandResolver;
+import us.ihmc.commons.Conversions;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
+import us.ihmc.yoVariables.variable.YoDouble;
 import us.ihmc.yoVariables.variable.YoLong;
 
 public class EstimatorTask extends HumanoidRobotControlTask
@@ -15,6 +17,10 @@ public class EstimatorTask extends HumanoidRobotControlTask
 
    private final AvatarEstimatorThread estimatorThread;
    private final YoLong estimatorTick;
+   private final YoDouble estimatorDT;
+   private final YoDouble estimatorTimer;
+
+   private long lastStartTime;
 
    public EstimatorTask(AvatarEstimatorThread estimatorThread, long divisor, FullHumanoidRobotModel masterFullRobotModel)
    {
@@ -23,6 +29,8 @@ public class EstimatorTask extends HumanoidRobotControlTask
       estimatorResolver = new CrossRobotCommandResolver(estimatorThread.getFullRobotModel());
       masterResolver = new CrossRobotCommandResolver(masterFullRobotModel);
       estimatorTick = new YoLong("EstimatorTick", estimatorThread.getYoVariableRegistry());
+      estimatorDT = new YoDouble("EstimatorDT", estimatorThread.getYoVariableRegistry());
+      estimatorTimer = new YoDouble("EstimatorTimer", estimatorThread.getYoVariableRegistry());
    }
 
    @Override
@@ -35,10 +43,17 @@ public class EstimatorTask extends HumanoidRobotControlTask
    @Override
    protected void execute()
    {
+      long startTime = System.nanoTime();
+      if (lastStartTime != 0)
+         estimatorDT.set(Conversions.nanosecondsToMilliseconds((double) (startTime - lastStartTime)));
+      lastStartTime = startTime;
+
       estimatorTick.increment();
       estimatorThread.read(System.nanoTime());
       estimatorThread.run();
       estimatorThread.write();
+
+      estimatorTimer.set(Conversions.nanosecondsToMilliseconds((double) (System.nanoTime() - lastStartTime)));
    }
 
    @Override
