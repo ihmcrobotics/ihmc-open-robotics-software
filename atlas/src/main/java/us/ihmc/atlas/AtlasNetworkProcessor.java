6ee5c6b1f415ebffc25b93496f5780b20ed21277
Duncan Calvert <dcalvert@ihmc.us>
2019-05-16 15:12:50 -0500

    Format AtlasNetworkProcessor.
    # Conflicts:
    #	atlas/src/main/java/us/ihmc/atlas/AtlasNetworkProcessor.java

--- atlas/src/main/java/us/ihmc/atlas/AtlasNetworkProcessor.java
+++ atlas/src/main/java/us/ihmc/atlas/AtlasNetworkProcessor.java
@@ -22,13 +22,22 @@ public class AtlasNetworkProcessor
    {
       JSAP jsap = new JSAP();
 
-      FlaggedOption robotModel = new FlaggedOption("robotModel").setLongFlag("model").setShortFlag('m').setRequired(true).setStringParser(JSAP.STRING_PARSER);
+      FlaggedOption robotModel = new FlaggedOption("robotModel").setLongFlag("model")
+                                                                   .setShortFlag('m')
+                                                                   .setRequired(true)
+                                                                   .setStringParser(JSAP.STRING_PARSER);
 
       Switch runningOnRealRobot = new Switch("runningOnRealRobot").setLongFlag("realRobot");
       Switch runningOnGazebo = new Switch("runningOnGazebo").setLongFlag("gazebo");
 
-      FlaggedOption leftHandHost = new FlaggedOption("leftHandHost").setLongFlag("lefthand").setShortFlag('l').setRequired(false).setStringParser(JSAP.STRING_PARSER);
-      FlaggedOption rightHandHost = new FlaggedOption("rightHandHost").setLongFlag("righthand").setShortFlag('r').setRequired(false).setStringParser(JSAP.STRING_PARSER);
+      FlaggedOption leftHandHost = new FlaggedOption("leftHandHost").setLongFlag("lefthand")
+                                                                       .setShortFlag('l')
+                                                                       .setRequired(false)
+                                                                       .setStringParser(JSAP.STRING_PARSER);
+      FlaggedOption rightHandHost = new FlaggedOption("rightHandHost").setLongFlag("righthand")
+                                                                         .setShortFlag('r')
+                                                                         .setRequired(false)
+                                                                         .setStringParser(JSAP.STRING_PARSER);
 
       robotModel.setHelp("Robot models: " + AtlasRobotModelFactory.robotModelsToString());
       jsap.registerParameter(robotModel);
@@ -74,43 +83,42 @@ public class AtlasNetworkProcessor
            System.out.println("ROS_MASTER_URI="+rosuri);
 
             createAuxiliaryRobotDataRosPublisher(networkModuleParams, rosuri);
-        }
-    	  try
-    	  {
-    	     RobotTarget target;
-    	     if(config.getBoolean(runningOnRealRobot.getID()))
-    	     {
-    	        target = RobotTarget.REAL_ROBOT;
-    	     }
-    	     else if(config.getBoolean(runningOnGazebo.getID()))
-    	     {
-    	       target = RobotTarget.GAZEBO;
-    	     }
-    	     else
-    	     {
-    	        target = RobotTarget.SCS;
-    	     }
-    		  model = AtlasRobotModelFactory.createDRCRobotModel(config.getString("robotModel"), target, true);
-           if(model.getHandModel()!=null)
-              networkModuleParams.enableHandModule(true);
-    	  }
-    	  catch (IllegalArgumentException e)
-    	  {
-    		  System.err.println("Incorrect robot model " + config.getString("robotModel"));
-    		  System.out.println(jsap.getHelp());
+         }
+         try
+         {
+            RobotTarget target;
+            if (config.getBoolean(runningOnRealRobot.getID()))
+            {
+               target = RobotTarget.REAL_ROBOT;
+            }
+            else if (config.getBoolean(runningOnGazebo.getID()))
+            {
+               target = RobotTarget.GAZEBO;
+            }
+            else
+            {
+               target = RobotTarget.SCS;
+            }
+            model = AtlasRobotModelFactory.createDRCRobotModel(config.getString("robotModel"), target, true);
+            if (model.getHandModel() != null)
+               networkModuleParams.enableHandModule(true);
+         }
+         catch (IllegalArgumentException e)
+         {
+            System.err.println("Incorrect robot model " + config.getString("robotModel"));
+            System.out.println(jsap.getHelp());
 
-    		  return;
-    	  }
+            return;
+         }
 
-    	  System.out.println("Using the " + model + " model");
+         System.out.println("Using the " + model + " model");
 
-        URI rosMasterURI = NetworkParameters.getROSURI();
-        networkModuleParams.setRosUri(rosMasterURI);
+         URI rosMasterURI = NetworkParameters.getROSURI();
+         networkModuleParams.setRosUri(rosMasterURI);
 
+         networkModuleParams.enableLocalControllerCommunicator(false);
 
-    	  networkModuleParams.enableLocalControllerCommunicator(false);
-
-    	  new DRCNetworkProcessor(model, networkModuleParams);
+         new DRCNetworkProcessor(model, networkModuleParams);
       }
       else
       {
@@ -123,12 +131,12 @@ public class AtlasNetworkProcessor
    private static void createAuxiliaryRobotDataRosPublisher(DRCNetworkModuleParameters networkModuleParams, URI rosuri)
    {
       // FIXME Do we still need that?
-//      RosAtlasAuxiliaryRobotDataPublisher auxiliaryRobotDataPublisher = new RosAtlasAuxiliaryRobotDataPublisher(rosuri, defaultRosNameSpace);
-//      PacketCommunicator packetCommunicator = PacketCommunicator.createIntraprocessPacketCommunicator(NetworkPorts.ROS_AUXILIARY_ROBOT_DATA_PUBLISHER,
-//            new IHMCCommunicationKryoNetClassList());
-//      
-//      packetCommunicator.attachListener(AtlasAuxiliaryRobotData.class, auxiliaryRobotDataPublisher::receivedPacket);
-//
-//      networkModuleParams.addRobotSpecificModuleCommunicatorPort(NetworkPorts.ROS_AUXILIARY_ROBOT_DATA_PUBLISHER, PacketDestination.AUXILIARY_ROBOT_DATA_PUBLISHER);
+      //      RosAtlasAuxiliaryRobotDataPublisher auxiliaryRobotDataPublisher = new RosAtlasAuxiliaryRobotDataPublisher(rosuri, defaultRosNameSpace);
+      //      PacketCommunicator packetCommunicator = PacketCommunicator.createIntraprocessPacketCommunicator(NetworkPorts.ROS_AUXILIARY_ROBOT_DATA_PUBLISHER,
+      //            new IHMCCommunicationKryoNetClassList());
+      //
+      //      packetCommunicator.attachListener(AtlasAuxiliaryRobotData.class, auxiliaryRobotDataPublisher::receivedPacket);
+      //
+      //      networkModuleParams.addRobotSpecificModuleCommunicatorPort(NetworkPorts.ROS_AUXILIARY_ROBOT_DATA_PUBLISHER, PacketDestination.AUXILIARY_ROBOT_DATA_PUBLISHER);
    }
 }
