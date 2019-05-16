a2255ed025306802917f5ce1615cd46a9c998250
Duncan Calvert <dcalvert@ihmc.us>
2019-05-16 17:22:27 -0500

    Extract manual step generation.

--- ihmc-quadruped-ui/src/main/java/us/ihmc/quadrupedUI/uiControllers/ManualStepTabController.java
+++ ihmc-quadruped-ui/src/main/java/us/ihmc/quadrupedUI/uiControllers/ManualStepTabController.java
@@ -19,6 +19,7 @@ import us.ihmc.javaFXToolkit.messager.JavaFXMessager;
 import us.ihmc.log.LogTools;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.quadrupedBasics.referenceFrames.QuadrupedReferenceFrames;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.manual.QuadrupedStraightPawstepPlanGenerator;
 import us.ihmc.quadrupedPlanning.QuadrupedXGaitSettingsReadOnly;
 import us.ihmc.quadrupedUI.QuadrupedUIMessagerAPI;
 import us.ihmc.quadrupedUI.graphics.PositionGraphic;
@@ -44,6 +45,7 @@ public class ManualStepTabController extends Group
    private static final String NO_FLAMINGO_QUADRANT_SELECTED = "None";
 
    private final AtomicBoolean useTrotOverCrawl = new AtomicBoolean(false);
+   private final QuadrupedStraightPawstepPlanGenerator manualPlanGenerator = new QuadrupedStraightPawstepPlanGenerator();
 
    private JavaFXMessager messager;
    private AtomicReference<QuadrupedXGaitSettingsReadOnly> xGaitSettingsReference;
@@ -207,210 +209,36 @@ public class ManualStepTabController extends Group
       }
 
       if (useTrot.isSelected())
-         handleTrotRequest();
+      {
+         messager.submitMessage(QuadrupedUIMessagerAPI.ManualStepsListMessageTopic, manualPlanGenerator.generateTrotSteps(firstFoot.getValue(),
+                                                                                                                          swingHeight.getValue(),
+                                                                                                                          stepHeight.getValue(),
+                                                                                                                          stepLength.getValue(),
+                                                                                                                          stepWidth.getValue(),
+                                                                                                                          stepDuration.getValue(),
+                                                                                                                          dwellTime.getValue(),
+                                                                                                                          numberOfSteps.getValue(),
+                                                                                                                          xGaitSettingsReference.get(),
+                                                                                                                          referenceFrames));
+      }
       else
-         handleCrawlRequest();
+      {
+         messager.submitMessage(QuadrupedUIMessagerAPI.ManualStepsListMessageTopic, manualPlanGenerator.generateCrawlSteps(firstFoot.getValue(),
+                                                                                                                           swingHeight.getValue(),
+                                                                                                                           stepHeight.getValue(),
+                                                                                                                           stepLength.getValue(),
+                                                                                                                           stepWidth.getValue(),
+                                                                                                                           stepDuration.getValue(),
+                                                                                                                           dwellTime.getValue(),
+                                                                                                                           numberOfSteps.getValue(),
+                                                                                                                           xGaitSettingsReference.get(),
+                                                                                                                           referenceFrames));
+      }
    }
 
    public void setUseTrot()
    {
       useTrotOverCrawl.set(useTrot.isSelected());
-   }
-
-   private void handleTrotRequest()
-   {
-      RobotQuadrant firstFootQuadrant = firstFoot.getValue();
-      double swingHeight = this.swingHeight.getValue();
-      double stepHeight = this.stepHeight.getValue();
-      double stepLength = this.stepLength.getValue();
-      double stepWidth = this.stepWidth.getValue();
-      double stepDuration = this.stepDuration.getValue();
-      double dwellTime = this.dwellTime.getValue();
-      int numberOfSteps = this.numberOfSteps.getValue();
-
-      QuadrupedXGaitSettingsReadOnly xGaitSettings = xGaitSettingsReference.get();
-
-      QuadrupedTimedStepListMessage stepListMessage = new QuadrupedTimedStepListMessage();
-
-      stepListMessage.setIsExpressedInAbsoluteTime(false);
-      stepListMessage.getQuadrupedStepList().clear();
-
-      double stanceLength = xGaitSettings.getStanceLength();
-      double stanceWidth = xGaitSettings.getStanceWidth();
-
-      ReferenceFrame supportFrame = referenceFrames.getCenterOfFeetZUpFrameAveragingLowestZHeightsAcrossEnds();
-      FramePoint3D centerPoint = new FramePoint3D(supportFrame);
-      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-         FramePoint3D solePosition = new FramePoint3D(referenceFrames.getSoleFrame(robotQuadrant));
-         solePosition.changeFrame(supportFrame);
-         centerPoint.add(solePosition);
-      }
-      centerPoint.scale(0.25);
-
-      QuadrantDependentList<FramePoint3D> nominalPositions = new QuadrantDependentList<>();
-      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-         FramePoint3D nominalPosition = new FramePoint3D(centerPoint);
-         nominalPosition.addX(0.5 * robotQuadrant.getEnd().negateIfHindEnd(stanceLength));
-         nominalPosition.addY(0.5 * robotQuadrant.getSide().negateIfRightSide(stanceWidth));
-         nominalPositions.put(robotQuadrant, nominalPosition);
-      }
-
-      double timeDelay = 0.0;
-      double lengthOffset = 0.0;
-      double widthOffset = 0.0;
-
-      for (int i = 0; i < numberOfSteps; i++)
-      {
-         lengthOffset += stepLength;
-         widthOffset += stepWidth;
-
-         RobotQuadrant frontQuadrant = firstFootQuadrant;
-         RobotQuadrant rearQuadrant = firstFootQuadrant.getDiagonalOppositeQuadrant();
-
-         QuadrupedTimedStepMessage frontFootMessage = stepListMessage.getQuadrupedStepList().add();
-         QuadrupedTimedStepMessage hindFootMessage = stepListMessage.getQuadrupedStepList().add();
-
-         nominalPositions.get(frontQuadrant).changeFrame(supportFrame);
-         nominalPositions.get(rearQuadrant).changeFrame(supportFrame);
-
-         FramePoint3D frontPosition = new FramePoint3D(nominalPositions.get(frontQuadrant));
-         FramePoint3D rearPosition = new FramePoint3D(nominalPositions.get(rearQuadrant));
-         frontPosition.addX(lengthOffset);
-         frontPosition.addY(widthOffset);
-         rearPosition.addX(lengthOffset);
-         rearPosition.addY(widthOffset);
-
-         frontPosition.changeFrame(ReferenceFrame.getWorldFrame());
-         rearPosition.changeFrame(ReferenceFrame.getWorldFrame());
-         frontPosition.addZ(stepHeight);
-         rearPosition.addZ(stepHeight);
-
-         frontFootMessage.getTimeInterval().setStartTime(timeDelay);
-         frontFootMessage.getTimeInterval().setEndTime(timeDelay + stepDuration);
-         frontFootMessage.getQuadrupedStepMessage().setGroundClearance(swingHeight);
-         frontFootMessage.getQuadrupedStepMessage().setRobotQuadrant(frontQuadrant.toByte());
-         frontFootMessage.getQuadrupedStepMessage().getGoalPosition().set(frontPosition);
-
-         hindFootMessage.getTimeInterval().setStartTime(timeDelay);
-         hindFootMessage.getTimeInterval().setEndTime(timeDelay + stepDuration);
-         hindFootMessage.getQuadrupedStepMessage().setGroundClearance(swingHeight);
-         hindFootMessage.getQuadrupedStepMessage().setRobotQuadrant(rearQuadrant.toByte());
-         hindFootMessage.getQuadrupedStepMessage().getGoalPosition().set(rearPosition);
-
-
-         firstFootQuadrant = firstFootQuadrant.getAcrossBodyQuadrant();
-         timeDelay += stepDuration + dwellTime;
-      }
-
-      messager.submitMessage(QuadrupedUIMessagerAPI.ManualStepsListMessageTopic, stepListMessage);
-   }
-
-   private void handleCrawlRequest()
-   {
-      RobotQuadrant firstFootQuadrant = firstFoot.getValue();
-      double swingHeight = this.swingHeight.getValue();
-      double stepHeight = this.stepHeight.getValue();
-      double stepLength = this.stepLength.getValue();
-      double stepWidth = this.stepWidth.getValue();
-      double stepDuration = this.stepDuration.getValue();
-      double dwellTime = this.dwellTime.getValue();
-      int numberOfSteps = this.numberOfSteps.getValue();
-
-      QuadrupedXGaitSettingsReadOnly xGaitSettings = xGaitSettingsReference.get();
-
-      QuadrupedTimedStepListMessage stepListMessage = new QuadrupedTimedStepListMessage();
-
-      stepListMessage.setIsExpressedInAbsoluteTime(false);
-      stepListMessage.getQuadrupedStepList().clear();
-
-      double currentTime = 0.0;
-
-      double stanceLength = xGaitSettings.getStanceLength();
-      double stanceWidth = xGaitSettings.getStanceWidth();
-
-      ReferenceFrame supportFrame = referenceFrames.getCenterOfFeetZUpFrameAveragingLowestZHeightsAcrossEnds();
-      FramePoint3D centerPoint = new FramePoint3D(supportFrame);
-      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-         FramePoint3D solePosition = new FramePoint3D(referenceFrames.getSoleFrame(robotQuadrant));
-         solePosition.changeFrame(supportFrame);
-         centerPoint.add(solePosition);
-      }
-      centerPoint.scale(0.25);
-
-      QuadrantDependentList<FramePoint3D> nominalPositions = new QuadrantDependentList<>();
-      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-         FramePoint3D nominalPosition = new FramePoint3D(centerPoint);
-         nominalPosition.addX(0.5 * robotQuadrant.getEnd().negateIfHindEnd(stanceLength));
-         nominalPosition.addY(0.5 * robotQuadrant.getSide().negateIfRightSide(stanceWidth));
-         nominalPositions.put(robotQuadrant, nominalPosition);
-      }
-
-      double lengthOffset = 0.0;
-      double widthOffset = 0.0;
-
-      for (int i = 0; i < Math.ceil(numberOfSteps / 2.0); i++)
-      {
-         lengthOffset += 0.5 * stepLength;
-         widthOffset += 0.5 * stepWidth;
-
-         QuadrupedTimedStepMessage firstFootMessage = stepListMessage.getQuadrupedStepList().add();
-
-         nominalPositions.get(firstFootQuadrant).changeFrame(supportFrame);
-
-         FramePoint3D firstPosition = new FramePoint3D(nominalPositions.get(firstFootQuadrant));
-         firstPosition.addX(lengthOffset);
-         firstPosition.addY(widthOffset);
-
-         firstPosition.changeFrame(ReferenceFrame.getWorldFrame());
-         firstPosition.addZ(stepHeight);
-
-         firstFootMessage.getTimeInterval().setStartTime(currentTime);
-         firstFootMessage.getTimeInterval().setEndTime(currentTime + stepDuration);
-         firstFootMessage.getQuadrupedStepMessage().setGroundClearance(swingHeight);
-         firstFootMessage.getQuadrupedStepMessage().setRobotQuadrant(firstFootQuadrant.toByte());
-         firstFootMessage.getQuadrupedStepMessage().getGoalPosition().set(firstPosition);
-
-
-         if (2 * i + 1 < numberOfSteps)
-         {
-            lengthOffset += 0.5 * stepLength;
-            widthOffset += 0.5 * stepWidth;
-
-            QuadrupedTimedStepMessage oppositeFootMessage = stepListMessage.getQuadrupedStepList().add();
-
-            currentTime += 0.5 * stepDuration + dwellTime;
-            RobotQuadrant oppositeQuadrant = firstFootQuadrant.getDiagonalOppositeQuadrant();
-
-            nominalPositions.get(oppositeQuadrant).changeFrame(supportFrame);
-
-            FramePoint3D secondPosition = new FramePoint3D(nominalPositions.get(oppositeQuadrant));
-            secondPosition.addX(lengthOffset);
-            secondPosition.addY(widthOffset);
-
-            secondPosition.changeFrame(ReferenceFrame.getWorldFrame());
-            secondPosition.addZ(stepHeight);
-
-            oppositeFootMessage.getTimeInterval().setStartTime(currentTime);
-            oppositeFootMessage.getTimeInterval().setEndTime(currentTime + stepDuration);
-            oppositeFootMessage.getQuadrupedStepMessage().setGroundClearance(swingHeight);
-            oppositeFootMessage.getQuadrupedStepMessage().setRobotQuadrant(oppositeQuadrant.toByte());
-            oppositeFootMessage.getQuadrupedStepMessage().getGoalPosition().set(secondPosition);
-         }
-         else
-         {
-            break;
-         }
-
-         firstFootQuadrant = firstFootQuadrant.getAcrossBodyQuadrant();
-
-         currentTime += 0.5 * stepDuration + dwellTime;
-      }
-
-      messager.submitMessage(QuadrupedUIMessagerAPI.ManualStepsListMessageTopic, stepListMessage);
    }
 
    @FXML public void flamingoFoot() // add/remove virtual graphic
