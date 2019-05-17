3a4282e9d21099428d6f5a19bda307f29863f817
Doug Stephen <dstephen@ihmc.us>
2019-05-17 14:29:36 -0500

    Split out Vision and ROS specific stuff in to their own interfaces.
    Make AvatarHumanoidRobotSensorInformation.java extend the new interfaces.

--- ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/parameters/AvatarHumanoidRobotSensorInformation.java
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/parameters/AvatarHumanoidRobotSensorInformation.java
@@ -1,14 +1,8 @@
 package us.ihmc.sensorProcessing.parameters;
 
-import java.util.ArrayList;
-
-import org.apache.commons.lang3.tuple.ImmutableTriple;
-
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
-public interface AvatarHumanoidRobotSensorInformation
+public interface AvatarHumanoidRobotSensorInformation extends AvatarRobotVisionSensorInformation, AvatarRobotRosIntegratedSensorInformation
 {
    public String[] getIMUSensorsToUseInStateEstimator();
 
@@ -21,29 +15,4 @@ public interface AvatarHumanoidRobotSensorInformation
    public SideDependentList<String> getWristForceSensorNames();
 
    public String getPrimaryBodyImu();
-
-   public AvatarRobotCameraParameters[] getCameraParameters();
-
-   public AvatarRobotLidarParameters[] getLidarParameters();
-
-   public AvatarRobotPointCloudParameters[] getPointCloudParameters();
-
-   public AvatarRobotCameraParameters getCameraParameters(int cameraId);
-
-   public AvatarRobotLidarParameters getLidarParameters(int lidarId);
-
-   public AvatarRobotPointCloudParameters getPointCloudParameters(int pointCloudSensorId);
-
-   @Deprecated // Unused, remove.
-   public ReferenceFrame getHeadIMUFrameWhenLevel();
-
-   public String[] getSensorFramesToTrack();
-
-   public boolean setupROSLocationService();
-
-   public boolean setupROSParameterSetters();
-
-   public boolean isMultisenseHead();
-
-   public ArrayList<ImmutableTriple<String, String, RigidBodyTransform>> getStaticTransformsForRos();
 }
