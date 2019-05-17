2bdc475a806415a0b3996179aa533fff98e10b69
Robert <rgriffin@ihmc.us>
2019-05-16 21:30:34 -0500

    did some smarter splining for the initial trajectory

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/trajectory/DCMPlanner.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/trajectory/DCMPlanner.java
@@ -2,7 +2,6 @@ package us.ihmc.quadrupedRobotics.planning.trajectory;
 
 import us.ihmc.commonWalkingControlModules.capturePoint.CapturePointTools;
 import us.ihmc.commons.MathTools;
-import us.ihmc.euclid.referenceFrame.FramePoint2D;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
@@ -20,22 +19,18 @@ import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.quadrupedBasics.gait.QuadrupedTimedStep;
 import us.ihmc.quadrupedRobotics.planning.ContactState;
 import us.ihmc.quadrupedRobotics.planning.QuadrupedTimedContactSequence;
-import us.ihmc.quadrupedRobotics.planning.WeightDistributionCalculator;
-import us.ihmc.robotics.math.trajectories.FrameTrajectory3D;
 import us.ihmc.robotics.math.trajectories.YoFrameTrajectory3D;
 import us.ihmc.robotics.robotSide.QuadrantDependentList;
 import us.ihmc.yoVariables.parameters.DoubleParameter;
-import us.ihmc.yoVariables.providers.DoubleProvider;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static us.ihmc.humanoidRobotics.footstep.FootstepUtils.worldFrame;
-
 public class DCMPlanner implements DCMPlannerInterface
 {
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
 
    private static final boolean VISUALIZE = true;
@@ -47,7 +42,9 @@ public class DCMPlanner implements DCMPlannerInterface
    private final PiecewiseReverseDcmTrajectory dcmTrajectory;
    private final YoFrameTrajectory3D dcmTransitionTrajectory;
 
-   private final DoubleParameter initialTransitionDurationParameter = new DoubleParameter("initialTransitionDuration", registry, 0.5);
+   private final DoubleParameter initialTransitionDuration = new DoubleParameter("initialTransitionDuration", registry, 0.5);
+   private final DoubleParameter minimumSplineDuration = new DoubleParameter("minimumSplineDuration", registry, 0.1);
+   private final DoubleParameter splineSplitFraction = new DoubleParameter("splineSplitFraction", registry, 0.5);
 
    private final QuadrupedTimedContactSequence timedContactSequence = new QuadrupedTimedContactSequence(2 * STEP_SEQUENCE_CAPACITY);
    private final List<QuadrupedTimedStep> stepSequence = new ArrayList<>();
@@ -61,17 +58,29 @@ public class DCMPlanner implements DCMPlannerInterface
 
    private final YoBoolean isStanding = new YoBoolean("isStanding", registry);
    private final YoBoolean isUsingSpline = new YoBoolean("isUsingSpline", registry);
+   private final YoBoolean isInitialTransfer = new YoBoolean("isInitialTransfer", registry);
 
    private final ReferenceFrame supportFrame;
-   private final YoFramePoint3D dcmPositionAtStartOfState = new YoFramePoint3D("dcmPositionAtStartOfState", ReferenceFrame.getWorldFrame(), registry);
-   private final YoFrameVector3D dcmVelocityAtStartOfState = new YoFrameVector3D("dcmVelocityAtStartOfState", ReferenceFrame.getWorldFrame(), registry);
-   private final YoFramePoint3D dcmPositionAtEndOfTransition = new YoFramePoint3D("dcmPositionAtEndOfTransition", ReferenceFrame.getWorldFrame(), registry);
-   private final YoFrameVector3D dcmVelocityAtEndOfTransition = new YoFrameVector3D("dcmVelocityAtEndOfTransition", ReferenceFrame.getWorldFrame(), registry);
+   private final YoFramePoint3D dcmPositionAtStartOfState = new YoFramePoint3D("dcmPositionAtStartOfState", worldFrame, registry);
+   private final YoFrameVector3D dcmVelocityAtStartOfState = new YoFrameVector3D("dcmVelocityAtStartOfState", worldFrame, registry);
+   private final YoFramePoint3D dcmPositionAtEndOfTransition = new YoFramePoint3D("dcmPositionAtEndOfTransition", worldFrame, registry);
+   private final YoFrameVector3D dcmVelocityAtEndOfTransition = new YoFrameVector3D("dcmVelocityAtEndOfTransition", worldFrame, registry);
+   private final YoFramePoint3D dcmPositionAtStartOfSpline = new YoFramePoint3D("dcmPositionAtStartOfSpline", worldFrame, registry);
+   private final YoFrameVector3D dcmVelocityAtStartOfSpline = new YoFrameVector3D("dcmVelocityAtStartOfSpline", worldFrame, registry);
+   private final YoFramePoint3D dcmPositionAtEndOfSpline = new YoFramePoint3D("dcmPositionAtEndOfSpline", worldFrame, registry);
+   private final YoFrameVector3D dcmVelocityAtEndOfSpline = new YoFrameVector3D("dcmVelocityAtEndOfSpline", worldFrame, registry);
    private final YoDouble timeAtStartOfState = new YoDouble("timeAtStartOfState", registry);
+
    private final FramePoint3D initialTransitionDCMPosition = new FramePoint3D();
    private final FrameVector3D initialTransitionDCMVelocity = new FrameVector3D();
    private final FramePoint3D finalTransitionDCMPosition = new FramePoint3D();
    private final FrameVector3D finalTransitionDCMVelocity = new FrameVector3D();
+
+   private final FramePoint3D initialSplineDCMPosition = new FramePoint3D();
+   private final FrameVector3D initialSplineDCMVelocity = new FrameVector3D();
+   private final FramePoint3D finaSplineDCMPosition = new FramePoint3D();
+   private final FrameVector3D finalSplineDCMVelocity = new FrameVector3D();
+
    private final FramePoint3D finalDCM = new FramePoint3D();
 
    private final FramePoint3D tempPoint = new FramePoint3D();
@@ -155,6 +164,7 @@ public class DCMPlanner implements DCMPlannerInterface
                                      FrameVector3DReadOnly currentDCMVelocity)
    {
       isStanding.set(false);
+      isInitialTransfer.set(true);
 
       double currentTime = controllerTime.getDoubleValue();
       boolean isCurrentPlanValid = stepSequence.get(numberOfStepsInPlanner.getIntegerValue() - 1).getTimeInterval().getEndTime() > currentTime;
@@ -167,7 +177,7 @@ public class DCMPlanner implements DCMPlannerInterface
          dcmPositionAtStartOfState.setMatchingFrame(currentDCMPosition);
          dcmVelocityAtStartOfState.setMatchingFrame(currentDCMVelocity);
          timeAtStartOfState.set(controllerTime.getDoubleValue());
-         computeTransitionTrajectory();
+         computeInitialTransitionTrajectory();
       }
    }
 
@@ -183,6 +193,7 @@ public class DCMPlanner implements DCMPlannerInterface
       dcmPositionAtStartOfState.setMatchingFrame(desiredDCMPosition);
       dcmVelocityAtStartOfState.setMatchingFrame(desiredDCMVelocity);
       timeAtStartOfState.set(controllerTime.getDoubleValue());
+      isInitialTransfer.set(false);
    }
 
    private void computeDcmTrajectory(QuadrantDependentList<YoEnum<ContactState>> currentContactStates)
@@ -204,10 +215,49 @@ public class DCMPlanner implements DCMPlannerInterface
                                          piecewiseConstantCopTrajectory.getTimeAtStartOfInterval(numberOfIntervals - 1), tempPoint);
    }
 
+   private void computeInitialTransitionTrajectory()
+   {
+      boolean initialPhaseIsLongEnough = piecewiseConstantCopTrajectory.getIntervalDuration(0) > initialTransitionDuration.getValue();
+
+      double transitionStartTime = timeAtStartOfState.getDoubleValue();
+      double transitionEndTime;
+      if (initialPhaseIsLongEnough)
+      {
+         transitionEndTime = transitionStartTime + initialTransitionDuration.getValue();
+      }
+      else
+      {
+         double desiredTimeInNextPhase = initialTransitionDuration.getValue() - piecewiseConstantCopTrajectory.getIntervalDuration(0);
+         desiredTimeInNextPhase = MathTools.clamp(desiredTimeInNextPhase, minimumSplineDuration.getValue(), piecewiseConstantCopTrajectory.getIntervalDuration(1));
+         transitionEndTime = desiredTimeInNextPhase + piecewiseConstantCopTrajectory.getTimeAtStartOfInterval(1);
+      }
+
+      dcmTrajectory.computeTrajectory(transitionEndTime);
+      dcmTrajectory.getPosition(finalTransitionDCMPosition);
+      dcmTrajectory.getVelocity(finalTransitionDCMVelocity);
+
+      initialTransitionDCMPosition.setIncludingFrame(dcmPositionAtStartOfState);
+      initialTransitionDCMVelocity.setIncludingFrame(dcmVelocityAtStartOfState);
+      initialTransitionDCMPosition.changeFrame(dcmTransitionTrajectory.getReferenceFrame());
+      initialTransitionDCMVelocity.changeFrame(dcmTransitionTrajectory.getReferenceFrame());
+      finalTransitionDCMPosition.changeFrame(dcmTransitionTrajectory.getReferenceFrame());
+      finalTransitionDCMVelocity.changeFrame(dcmTransitionTrajectory.getReferenceFrame());
+
+      dcmTransitionTrajectory
+            .setCubic(transitionStartTime, transitionEndTime, initialTransitionDCMPosition, initialTransitionDCMVelocity, finalTransitionDCMPosition,
+                      finalTransitionDCMVelocity);
+
+      dcmPositionAtEndOfTransition.setMatchingFrame(finalTransitionDCMPosition);
+      dcmVelocityAtEndOfTransition.setMatchingFrame(finalTransitionDCMVelocity);
+
+      if (debug)
+         runTransitionDebugChecks(transitionStartTime, transitionEndTime);
+   }
+
    private void computeTransitionTrajectory()
    {
       double transitionStartTime = timeAtStartOfState.getDoubleValue();
-      double transitionEndTime = Math.min(piecewiseConstantCopTrajectory.getTimeAtStartOfInterval(1), transitionStartTime + initialTransitionDurationParameter.getValue());
+      double transitionEndTime = Math.min(piecewiseConstantCopTrajectory.getTimeAtStartOfInterval(1), transitionStartTime + initialTransitionDuration.getValue());
 
       dcmTrajectory.computeTrajectory(transitionEndTime);
       dcmTrajectory.getPosition(finalTransitionDCMPosition);
@@ -269,7 +319,11 @@ public class DCMPlanner implements DCMPlannerInterface
       else
       {
          computeDcmTrajectory(currentContactStates);
-         computeTransitionTrajectory();
+
+         if (isInitialTransfer.getBooleanValue())
+            computeInitialTransitionTrajectory();
+         else
+            computeTransitionTrajectory();
 
          double currentTime = controllerTime.getDoubleValue();
          dcmTrajectory.computeTrajectory(currentTime);
