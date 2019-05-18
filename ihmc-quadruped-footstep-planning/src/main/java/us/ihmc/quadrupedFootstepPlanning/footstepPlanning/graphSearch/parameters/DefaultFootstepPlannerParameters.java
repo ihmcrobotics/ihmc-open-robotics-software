141ef67009844ee5e44e1227ba2defe96ad39abe
Robert <rgriffin@ihmc.us>
2019-05-18 18:24:50 -0500

    pulled in the max yaw

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/parameters/DefaultFootstepPlannerParameters.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/parameters/DefaultFootstepPlannerParameters.java
@@ -41,14 +41,14 @@ public class DefaultFootstepPlannerParameters implements FootstepPlannerParamete
    @Override
    public double getMaximumStepYaw()
    {
-      return 0.5;
+      return 0.25;
    }
 
    /** {@inheritDoc} */
    @Override
    public double getMinimumStepYaw()
    {
-      return -0.5;
+      return -0.25;
    }
 
    /** {@inheritDoc} */
@@ -68,7 +68,7 @@ public class DefaultFootstepPlannerParameters implements FootstepPlannerParamete
    @Override
    public double getDistanceHeuristicWeight()
    {
-      return 0.2;
+      return 1.0;
    }
 
    @Override
@@ -80,7 +80,7 @@ public class DefaultFootstepPlannerParameters implements FootstepPlannerParamete
    @Override
    public double getYawWeight()
    {
-      return 0.1;
+      return 0.5;
    }
 
    @Override
@@ -104,7 +104,7 @@ public class DefaultFootstepPlannerParameters implements FootstepPlannerParamete
    @Override
    public double getHeuristicsInflationWeight()
    {
-      return 2.0;
+      return 1.0;
    }
 
    /** {@inheritDoc} */
