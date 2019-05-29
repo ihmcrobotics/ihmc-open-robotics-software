6eeb57eb633a5b9aa3e1370a284558a6abf27f54
Operator Computer <OperatorComputer@unknown>
2019-05-29 15:59:21 -0500

    Change remote atlas visualizer to use host selector again.

--- atlas/src/main/java/us/ihmc/atlas/RemoteAtlasVisualizer.java
+++ atlas/src/main/java/us/ihmc/atlas/RemoteAtlasVisualizer.java
@@ -7,19 +7,11 @@ import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.JSAPResult;
 
-import us.ihmc.commons.Conversions;
-import us.ihmc.log.LogTools;
 import us.ihmc.robotDataLogger.YoVariableClient;
-import us.ihmc.robotDataLogger.logger.DataServerSettings;
-import us.ihmc.robotDataLogger.websocket.client.discovery.HTTPDataServerConnection;
 import us.ihmc.robotDataVisualizer.visualizer.SCSVisualizer;
 
 public class RemoteAtlasVisualizer
 {
-   /** Timeout for starting the YoVariable client. If the server does not come online within this time the remote visualizer will crash. */
-   private static final int TIMEOUT = (int) Conversions.secondsToMilliseconds(600);
-   /** The IP address of the control computer. If you want to run this locally change this to "localhost". */
-   private static final String CONTROLLER_IP = "10.7.4.104";
    /** The update rate of the visualizer in milliseconds. If you experience delay build-up increase this value. */
    private static final int DEFAULT_UPDATE_RATE = 6;
 
@@ -31,15 +23,12 @@ public class RemoteAtlasVisualizer
       scsVisualizer.addButton("calibrateWristForceSensors", 1.0);
       scsVisualizer.setShowOverheadView(true);
 
-      HTTPDataServerConnection connection = HTTPDataServerConnection.connect(CONTROLLER_IP, DataServerSettings.DEFAULT_PORT);
       YoVariableClient client = new YoVariableClient(scsVisualizer);
-      client.start(TIMEOUT, connection);
+      client.startWithHostSelector();
    }
 
    public static void main(String[] args) throws JSAPException, IOException
    {
-      LogTools.info("Starting the remote visualizer for controller at " + CONTROLLER_IP);
-
       int bufferSize = 16384;
       JSAP jsap = new JSAP();
 
