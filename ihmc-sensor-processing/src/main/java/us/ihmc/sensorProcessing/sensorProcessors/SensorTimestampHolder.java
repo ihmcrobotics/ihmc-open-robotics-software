dcbed96bb7db8d47782227a4dd874368bc2f2eb1
Sylvain Bertrand <sbertrand@ihmc.us>
2019-06-03 17:27:05 -0500

    Added some doc.

--- ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/sensorProcessors/SensorTimestampHolder.java
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/sensorProcessors/SensorTimestampHolder.java
@@ -3,15 +3,32 @@ package us.ihmc.sensorProcessing.sensorProcessors;
 public interface SensorTimestampHolder
 {
    /**
-    * @return machine timestamp in nanoseconds.
+    * Gets the time in nanoseconds of the clock hanging on the wall. Takes into account leap
+    * seconds/years and is updated by the NTP server (thus can jump backwards). The wall time is
+    * usually used in ROS1 for synchronizing timestamps of different time sources (computers,
+    * sensors, etc.).
+    * 
+    * @return wall-time in nanoseconds.
     */
    public long getWallTime();
 
    /**
-    * @return timestamp in nanoseconds representing the controller up-time.
+    * Gets the time in nanoseconds that represents the absolute elapsed wall-clock time since some
+    * arbitrary, fixed point in the past. It is not affected by changes in the system time-of-day
+    * clock. This time is usually computed from a real-time process and can be used for reliably
+    * computing the time elapsed between two events.
+    * 
+    * @return monotonic time in nanoseconds.
     */
    public long getMonotonicTime();
 
-   // TODO Define me
+   /**
+    * Platform dependent.
+    * <p>
+    * Time signal in nanoseconds that can be used to synchronize two time sources.
+    * </p>
+    * 
+    * @return the timestamp for synchronization.
+    */
    public long getSyncTimestamp();
 }
