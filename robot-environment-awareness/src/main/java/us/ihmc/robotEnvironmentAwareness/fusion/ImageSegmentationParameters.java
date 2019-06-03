72f2f091bbfe3733a16375ee119c951a8db1a971
inhol <ilee@ihmc.us>
2019-06-03 15:45:31 -0500

    Defined parameters for image segmentation and propagation.

---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/ImageSegmentationParameters.java
@@ -0,0 +1,80 @@
+package us.ihmc.robotEnvironmentAwareness.fusion;
+
+public class ImageSegmentationParameters
+{
+   private static final int DEFAULT_PIXEL_SIZE = 30;
+   private static final double DEFAULT_PIXEL_RULER = 80.0;
+   private static final int DEFAULT_ITERATE = 6;
+   private static final int DEFAULT_MIN_ELEMENT_SIZE = 80;
+
+   private int pixelSize;
+   private double pixelRuler;
+   private int iterate;
+   private int minElementSize;
+
+   public ImageSegmentationParameters()
+   {
+      setDefaultParameters();
+   }
+
+   public ImageSegmentationParameters(ImageSegmentationParameters other)
+   {
+      set(other);
+   }
+
+   public void setDefaultParameters()
+   {
+      pixelSize = DEFAULT_PIXEL_SIZE;
+      pixelRuler = DEFAULT_PIXEL_RULER;
+      iterate = DEFAULT_ITERATE;
+      minElementSize = DEFAULT_MIN_ELEMENT_SIZE;
+   }
+
+   public void set(ImageSegmentationParameters other)
+   {
+      pixelSize = other.pixelSize;
+      pixelRuler = other.pixelRuler;
+      iterate = other.iterate;
+      minElementSize = other.minElementSize;
+   }
+
+   public int getPixelSize()
+   {
+      return pixelSize;
+   }
+
+   public double getPixelRuler()
+   {
+      return pixelRuler;
+   }
+
+   public int getIterate()
+   {
+      return iterate;
+   }
+
+   public int getMinElementSize()
+   {
+      return minElementSize;
+   }
+
+   public void setPixelSize(int pixelSize)
+   {
+      this.pixelSize = pixelSize;
+   }
+
+   public void setPixelRuler(double pixelRuler)
+   {
+      this.pixelRuler = pixelRuler;
+   }
+
+   public void setIterate(int iterate)
+   {
+      this.iterate = iterate;
+   }
+
+   public void setMinElementSize(int minElementSize)
+   {
+      this.minElementSize = minElementSize;
+   }
+}
---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/PlanarRegionPropagationParameters.java
@@ -0,0 +1,116 @@
+package us.ihmc.robotEnvironmentAwareness.fusion;
+
+public class PlanarRegionPropagationParameters
+{
+   private static final double DEFAULT_SPARSE_THRESHOLD = 0.01;
+   private static final double DEFAULT_PROXIMITY_THRESHOLD = 0.05;
+   private static final double DEFAULT_PLANARITY_THRESHOLD = Math.cos(Math.PI / 180 * 30);
+
+   private static final boolean DEFAULT_UPDATE_EXTENDED_DATA = false;
+   private static final double DEFAULT_EXTENDING_DISTANCE_THRESHOLD = 0.01;
+   private static final double DEFAULT_EXTENDING_RADIUS_THRESHOLD = 0.03;
+
+   private double sparseThreshold;
+   private double proximityThreshold;
+   private double planarityThreshold;
+   private boolean updateExtendedData;
+   private double extendingDistanceThreshold;
+   private double extendingRadiusThreshold;
+
+   public PlanarRegionPropagationParameters()
+   {
+      setDefaultParameters();
+   }
+
+   public PlanarRegionPropagationParameters(PlanarRegionPropagationParameters other)
+   {
+      set(other);
+   }
+
+   public double getSparseThreshold()
+   {
+      return sparseThreshold;
+   }
+
+   public double getProximityThreshold()
+   {
+      return proximityThreshold;
+   }
+
+   public double getPlanarityThreshold()
+   {
+      return planarityThreshold;
+   }
+
+   public boolean isUpdateExtendedData()
+   {
+      return updateExtendedData;
+   }
+
+   public double getExtendingDistanceThreshold()
+   {
+      return extendingDistanceThreshold;
+   }
+
+   public double getExtendingRadiusThreshold()
+   {
+      return extendingRadiusThreshold;
+   }
+
+   public void setSparseThreshold(double sparseThreshold)
+   {
+      this.sparseThreshold = sparseThreshold;
+   }
+
+   public void setProximityThreshold(double proximityThreshold)
+   {
+      this.proximityThreshold = proximityThreshold;
+   }
+
+   public void setPlanarityThreshold(double planarityThreshold)
+   {
+      this.planarityThreshold = planarityThreshold;
+   }
+   
+   public void setPlanarityThresholdInDegree(double planarityThresholdInDegree)
+   {
+      this.planarityThreshold = Math.cos(Math.PI / 180 * planarityThresholdInDegree);
+   }
+   
+   public void setUpdateExtendedData(boolean updateExtendedData)
+   {
+      this.updateExtendedData = updateExtendedData;
+   }
+
+   public void setExtendingDistanceThreshold(double extendingDistanceThreshold)
+   {
+      this.extendingDistanceThreshold = extendingDistanceThreshold;
+   }
+
+   public void setExtendingRadiusThreshold(double extendingRadiusThreshold)
+   {
+      this.extendingRadiusThreshold = extendingRadiusThreshold;
+   }
+
+   public void setDefaultParameters()
+   {
+      sparseThreshold = DEFAULT_SPARSE_THRESHOLD;
+      proximityThreshold = DEFAULT_PROXIMITY_THRESHOLD;
+      planarityThreshold = DEFAULT_PLANARITY_THRESHOLD;
+
+      updateExtendedData = DEFAULT_UPDATE_EXTENDED_DATA;
+      extendingDistanceThreshold = DEFAULT_EXTENDING_DISTANCE_THRESHOLD;
+      extendingRadiusThreshold = DEFAULT_EXTENDING_RADIUS_THRESHOLD;
+   }
+
+   public void set(PlanarRegionPropagationParameters other)
+   {
+      sparseThreshold = other.sparseThreshold;
+      proximityThreshold = other.proximityThreshold;
+      planarityThreshold = other.planarityThreshold;
+
+      updateExtendedData = other.updateExtendedData;
+      extendingDistanceThreshold = other.extendingDistanceThreshold;
+      extendingRadiusThreshold = other.extendingRadiusThreshold;
+   }
+}
