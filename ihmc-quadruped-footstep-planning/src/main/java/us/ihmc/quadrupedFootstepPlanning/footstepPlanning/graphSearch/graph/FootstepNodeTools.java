960cee9200ebcc6474815a884be804d1b77d4caa
Robert <rgriffin@ihmc.us>
2019-06-04 12:51:21 -0500

    improved the footstep node tools

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/graph/FootstepNodeTools.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/graph/FootstepNodeTools.java
@@ -3,7 +3,9 @@ package us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph;
 import us.ihmc.euclid.orientation.interfaces.Orientation3DReadOnly;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DBasics;
+import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.humanoidRobotics.footstep.Footstep;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public class FootstepNodeTools
@@ -23,7 +25,16 @@ public class FootstepNodeTools
     */
    public static void getNodeTransformToWorld(int xIndex, int yIndex, RigidBodyTransform nodeTransformToWorldToPack)
    {
-      nodeTransformToWorldToPack.setTranslation(FootstepNode.gridSizeXY * xIndex, FootstepNode.gridSizeXY * yIndex, 0.0);
+      getNodeTransformToWorld(FootstepNode.gridSizeXY * xIndex, FootstepNode.gridSizeXY * yIndex, nodeTransformToWorldToPack);
+   }
+
+   /**
+    * Computes a node-to-world RigidBodyTransform from the node's x and y position. This transform
+    * will always have no z translation..
+    */
+   public static void getNodeTransformToWorld(double xPosition, double yPosition, RigidBodyTransform nodeTransformToWorldToPack)
+   {
+      nodeTransformToWorldToPack.setTranslation(xPosition, yPosition, 0.0);
    }
 
 
@@ -62,14 +73,30 @@ public class FootstepNodeTools
    /**
     * Computes the snap transform which snaps the given node to the given position
     */
-   public static RigidBodyTransform computeSnapTransform(int xIndex, int yIndex, Point3DReadOnly footstepPosition, Orientation3DReadOnly footstepOrientation)
+   public static RigidBodyTransform computeSnapTransform(int xIndex, int yIndex, Point3DReadOnly snappedFootstepPosition, Orientation3DReadOnly footstepOrientation)
+   {
+      return computeSnapTransform(FootstepNode.gridSizeXY * xIndex, FootstepNode.gridSizeXY * yIndex, snappedFootstepPosition, footstepOrientation);
+   }
+
+   /**
+    * Computes the snap transform which snaps the given node to the given position
+    */
+   public static RigidBodyTransform computeSnapTransform(Point2DReadOnly stepToSnap, Point3DReadOnly snappedFootstepPosition, Orientation3DReadOnly footstepOrientation)
+   {
+      return computeSnapTransform(stepToSnap.getX(), stepToSnap.getY(), snappedFootstepPosition, footstepOrientation);
+   }
+
+   /**
+    * Computes the snap transform which snaps the given node to the given position
+    */
+   public static RigidBodyTransform computeSnapTransform(double xPosition, double yPosition, Point3DReadOnly snappedFootstepPosition, Orientation3DReadOnly footstepOrientation)
    {
       RigidBodyTransform snapTransform = new RigidBodyTransform();
       RigidBodyTransform stepTransform = new RigidBodyTransform();
-      stepTransform.setTranslation(footstepPosition);
+      stepTransform.setTranslation(snappedFootstepPosition);
       stepTransform.setRotation(footstepOrientation);
 
-      getNodeTransformToWorld(xIndex, yIndex, snapTransform);
+      getNodeTransformToWorld(xPosition, yPosition, snapTransform);
       snapTransform.preMultiplyInvertThis(stepTransform);
 
       return snapTransform;
