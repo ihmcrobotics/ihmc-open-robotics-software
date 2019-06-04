991753c6a58599107c33118dcb3180e29ebbb2c1
Robert <rgriffin@ihmc.us>
2019-06-04 00:06:44 -0500

    set up some generic quadruped footstep planner parameters

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/parameters/GenericQuadrupedXGaitSettings.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/genericQuadruped/parameters/GenericQuadrupedXGaitSettings.java
@@ -47,7 +47,7 @@ public class GenericQuadrupedXGaitSettings implements QuadrupedXGaitSettingsRead
       trotSlowTimings.setMaxSpeed(0.5);
 
       trotMediumTimings.setStepDuration(0.3);
-      trotMediumTimings.setEndDoubleSupportDuration(0.05);
+      trotMediumTimings.setEndDoubleSupportDuration(0.15);
       trotMediumTimings.setMaxSpeed(0.6);
 
       trotFastTimings.setStepDuration(0.25);
