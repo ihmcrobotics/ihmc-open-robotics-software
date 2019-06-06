51016ec55401f224c2d8ffab9cd60eaee4aa8420
Robert <rgriffin@ihmc.us>
2019-06-06 12:23:14 -0500

    fixed the vis graph quadruped planner

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/BodyPathHeuristics.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/heuristics/BodyPathHeuristics.java
@@ -2,24 +2,36 @@ package us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.heuristic
 
 import us.ihmc.commons.MathTools;
 import us.ihmc.euclid.geometry.Pose2D;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.pathPlanning.bodyPathPlanner.BodyPathPlanner;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.footstepSnapping.FootstepNodeSnapData;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.footstepSnapping.FootstepNodeSnapper;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph.FootstepNode;
+import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.graph.FootstepNodeTools;
 import us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.parameters.FootstepPlannerParameters;
+import us.ihmc.quadrupedPlanning.QuadrupedXGaitSettingsReadOnly;
 import us.ihmc.robotics.geometry.AngleTools;
+import us.ihmc.robotics.robotSide.RobotQuadrant;
 
 public class BodyPathHeuristics extends CostToGoHeuristics
 {
    private static final double pathViolationWeight = 30.0;
    private final BodyPathPlanner bodyPath;
+   private final FootstepNodeSnapper snapper;
+
+   private final QuadrupedXGaitSettingsReadOnly xGaitSettings;
 
    private double goalAlpha = 1.0;
 
-   public BodyPathHeuristics(FootstepPlannerParameters parameters, BodyPathPlanner bodyPath)
+   public BodyPathHeuristics(FootstepPlannerParameters parameters, BodyPathPlanner bodyPath, QuadrupedXGaitSettingsReadOnly xGaitSettings,
+                             FootstepNodeSnapper snapper)
    {
       super(parameters);
 
       this.bodyPath = bodyPath;
+      this.xGaitSettings = xGaitSettings;
+      this.snapper = snapper;
    }
 
    @Override
@@ -37,8 +49,50 @@ public class BodyPathHeuristics extends CostToGoHeuristics
       double remainingDistance = pathLength + pathViolationWeight * distanceToPath;
 
       double yaw = pathViolationWeight * AngleTools.computeAngleDifferenceMinusPiToPi(node.getNominalYaw(), closestPointOnPath.getYaw());
-      double minSteps = remainingDistance / parameters.getMaximumFrontStepReach();
-      return remainingDistance + parameters.getYawWeight() * Math.abs(yaw) + parameters.getCostPerStep() * minSteps;
+
+      double desiredSpeed = parameters.getMaxWalkingSpeedMultiplier() * xGaitSettings.getMaxSpeed();
+      double minSteps = 4.0 * remainingDistance / desiredSpeed;
+
+      double heightCost = 0.0;
+
+      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
+      {
+         int goalNodeXIndex = goalNode.getXIndex(robotQuadrant);
+         int goalNodeYIndex = goalNode.getYIndex(robotQuadrant);
+         int nodeXIndex = node.getXIndex(robotQuadrant);
+         int nodeYIndex = node.getYIndex(robotQuadrant);
+
+         FootstepNodeSnapData goalNodeData = snapper.snapFootstepNode(goalNodeXIndex, goalNodeYIndex);
+         FootstepNodeSnapData nodeData = snapper.snapFootstepNode(nodeXIndex, nodeYIndex);
+
+         if (nodeData == null || goalNodeData == null)
+         {
+            heightCost = 0.0;
+            break;
+         }
+
+         RigidBodyTransform nodeTransform = new RigidBodyTransform();
+         RigidBodyTransform goalNodeTransform = new RigidBodyTransform();
+
+         FootstepNodeTools.getSnappedNodeTransformToWorld(nodeXIndex, nodeYIndex, nodeData.getSnapTransform(), nodeTransform);
+         FootstepNodeTools.getSnappedNodeTransformToWorld(goalNodeXIndex, goalNodeYIndex, goalNodeData.getSnapTransform(), goalNodeTransform);
+
+         if (!nodeTransform.containsNaN() && !goalNodeTransform.containsNaN())
+         {
+            double heightChange = goalNodeTransform.getTranslationVector().getZ() - nodeTransform.getTranslationVector().getZ();
+
+            if (heightChange > 0.0)
+               heightCost += parameters.getStepUpWeight() * heightChange;
+            else
+               heightCost += Math.abs(parameters.getStepDownWeight() * heightChange);
+         }
+      }
+
+      double distanceCost = parameters.getDistanceHeuristicWeight() * remainingDistance;
+      double yawCost = parameters.getYawWeight() * Math.abs(yaw);
+      double stepCost = parameters.getCostPerStep() * minSteps;
+
+      return distanceCost + yawCost + stepCost + heightCost;
    }
 
    public void setGoalAlpha(double alpha)
