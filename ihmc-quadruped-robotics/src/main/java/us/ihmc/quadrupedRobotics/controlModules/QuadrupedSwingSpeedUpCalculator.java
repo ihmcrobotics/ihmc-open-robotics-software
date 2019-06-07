fac40a8556d2965d7a1f6b5eeb294b8e9d63082d
Robert <rgriffin@ihmc.us>
2019-06-07 14:50:08 -0500

    pushed the existing speed up calculation into the new speed up calculator

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controlModules/QuadrupedSwingSpeedUpCalculator.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/controlModules/QuadrupedSwingSpeedUpCalculator.java
@@ -1,17 +1,9 @@
 package us.ihmc.quadrupedRobotics.controlModules;
 
-import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.PlaneContactState;
 import us.ihmc.euclid.referenceFrame.*;
-import us.ihmc.euclid.referenceFrame.interfaces.FixedFramePoint2DBasics;
-import us.ihmc.euclid.referenceFrame.interfaces.FrameConvexPolygon2DBasics;
-import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
-import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.quadrupedBasics.gait.QuadrupedTimedStep;
 import us.ihmc.quadrupedRobotics.controller.QuadrupedControllerToolbox;
-import us.ihmc.robotics.robotSide.QuadrantDependentList;
-import us.ihmc.robotics.robotSide.RobotQuadrant;
-import us.ihmc.robotics.time.TimeIntervalBasics;
 import us.ihmc.yoVariables.parameters.BooleanParameter;
 import us.ihmc.yoVariables.parameters.DoubleParameter;
 import us.ihmc.yoVariables.providers.BooleanProvider;
@@ -30,56 +22,30 @@ public class QuadrupedSwingSpeedUpCalculator
    private final DoubleProvider controllerTime;
 
    private final BooleanProvider useSimpleSwingSpeedUpCalculation = new BooleanParameter("useSimpleSwingSpeedUpCalculation", registry, true);
-   private final BooleanProvider speedUpIfItWillHelpButIsntNeeded = new BooleanParameter("speedUpIfItWillHelpButIsntNeeded", registry, false);
    private final DoubleProvider durationForEmergencySwingSpeedUp = new DoubleParameter("durationForEmergencySwingSpeedUp", registry, 0.35);
-
-   private final DoubleProvider minDistanceInsideForSpeedUp = new DoubleParameter("minDistanceInsideForSpeedUp", registry, 0.0);
-
-   private final FramePoint2D tempFootPoint2D = new FramePoint2D();
-   private final FramePoint2D goalPosition = new FramePoint2D();
-
-   private final QuadrantDependentList<MovingReferenceFrame> soleFrames;
-   private final QuadrantDependentList<? extends PlaneContactState> contactStates;
-   private final FrameConvexPolygon2D supportPolygonInWorld = new FrameConvexPolygon2D();
-   private final FrameConvexPolygon2D supportPolygonInWorldAfterChange = new FrameConvexPolygon2D();
-
 
    public QuadrupedSwingSpeedUpCalculator(QuadrupedControllerToolbox controllerToolbox, YoVariableRegistry parentRegistry)
    {
-      this(controllerToolbox.getReferenceFrames().getSoleFrames(), controllerToolbox.getFootContactStates(),
-           controllerToolbox.getLinearInvertedPendulumModel().getYoNaturalFrequency(),
-           controllerToolbox.getRuntimeEnvironment().getRobotTimestamp(), parentRegistry);
+      this(controllerToolbox.getLinearInvertedPendulumModel().getYoNaturalFrequency(), controllerToolbox.getRuntimeEnvironment().getRobotTimestamp(), parentRegistry);
    }
 
-   public QuadrupedSwingSpeedUpCalculator(QuadrantDependentList<MovingReferenceFrame> soleFrames,
-                                          QuadrantDependentList<? extends PlaneContactState> contactStates, DoubleProvider omega, DoubleProvider controllerTime,
-                                          YoVariableRegistry parentRegistry)
+   public QuadrupedSwingSpeedUpCalculator(DoubleProvider omega, DoubleProvider controllerTime, YoVariableRegistry parentRegistry)
    {
-      this.soleFrames = soleFrames;
-      this.contactStates = contactStates;
       this.omega = omega;
       this.controllerTime = controllerTime;
 
       parentRegistry.addChild(registry);
    }
 
-   private final FramePoint2D desiredICP = new FramePoint2D();
-   private final FramePoint2D finalDesiredICP = new FramePoint2D();
-   private final FramePoint2D estimatedICP = new FramePoint2D();
-
    public double estimateSwingSpeedUpTimeUnderDisturbance(List<? extends QuadrupedTimedStep> activeSteps, double normalizedEllipticalDCMError,
-                                                          FramePoint3DReadOnly desiredDCM, FramePoint3DReadOnly finalDesiredDCM,
-                                                          FramePoint3DReadOnly estimatedDCM, FramePoint3DReadOnly perfectCMP)
+                                                          FramePoint3DReadOnly desiredICP, FramePoint3DReadOnly finalDesiredICP,
+                                                          FramePoint3DReadOnly estimatedICP, FramePoint3DReadOnly perfectCMP)
    {
       if (normalizedEllipticalDCMError < 1.0)
          return 0.0;
 
       if (activeSteps.isEmpty())
          return 0.0;
-
-      desiredICP.set(desiredDCM);
-      finalDesiredICP.set(finalDesiredDCM);
-      estimatedICP.set(estimatedDCM);
 
       double deltaTimeToBeAccounted;
       if (useSimpleSwingSpeedUpCalculation.getValue())
@@ -89,14 +55,8 @@ public class QuadrupedSwingSpeedUpCalculator
          for (int i = 0; i < activeSteps.size(); i++)
          {
             QuadrupedTimedStep activeStep = activeSteps.get(i);
-            goalPosition.setIncludingFrame(worldFrame, activeStep.getGoalPosition());
-
-            if (!wouldPuttingTheFootDownHelpWithErrorRejection(activeStep.getRobotQuadrant(), goalPosition))
-               continue;
-
-            TimeIntervalBasics timeInterval = activeStep.getTimeInterval();
-            double duration = timeInterval.getDuration();
-            double phaseThroughStep = (currentTime - timeInterval.getStartTime()) / duration;
+            double duration = activeStep.getTimeInterval().getDuration();
+            double phaseThroughStep = (currentTime - activeStep.getTimeInterval().getStartTime()) / duration;
             double nominalRemainingTime = durationForEmergencySwingSpeedUp.getValue() * (1.0 - phaseThroughStep);
             double actualRemainingTime = duration - nominalRemainingTime;
 
@@ -116,49 +76,8 @@ public class QuadrupedSwingSpeedUpCalculator
       return deltaTimeToBeAccounted;
    }
 
-
-   private boolean wouldPuttingTheFootDownHelpWithErrorRejection(RobotQuadrant robotQuadrant, FramePoint2DReadOnly goalPosition)
-   {
-      updateSupportPolygon(supportPolygonInWorld, contactStates, null, null);
-      updateSupportPolygon(supportPolygonInWorldAfterChange, contactStates, robotQuadrant, goalPosition);
-
-      boolean currentICPFarEnoughInsideSupport = supportPolygonInWorld.signedDistance(estimatedICP) < -minDistanceInsideForSpeedUp.getValue();
-
-      if (willPuttingTheFootDownMakeTrackingPossibleAgain(currentICPFarEnoughInsideSupport))
-         return true;
-
-      if (currentICPFarEnoughInsideSupport || supportPolygonInWorld.isPointInside(desiredICP))
-      {
-         if (speedUpIfItWillHelpButIsntNeeded.getValue())
-            return willPuttingTheFootDownHelpTracking();
-         else
-            return false;
-      }
-      else
-      {
-         // both the desired and the current values are outside the support polygon
-         return willPuttingTheFootDownHelpTracking();
-      }
-   }
-
-   private boolean willPuttingTheFootDownMakeTrackingPossibleAgain(boolean currentICPFarEnoughInsideSupport)
-   {
-      boolean trackingIsCurrentlyImpossible = supportPolygonInWorld.isPointInside(desiredICP) && !currentICPFarEnoughInsideSupport;
-      boolean trackingWillBePossible = supportPolygonInWorldAfterChange.isPointInside(desiredICP) && supportPolygonInWorldAfterChange.isPointInside(estimatedICP);
-
-      return trackingIsCurrentlyImpossible && trackingWillBePossible;
-   }
-
-   private boolean willPuttingTheFootDownHelpTracking()
-   {
-      double currentDesiredDistance = supportPolygonInWorld.signedDistance(desiredICP);
-      double currentEstimatedDistance = supportPolygonInWorld.signedDistance(estimatedICP);
-
-      double desiredDistanceAfterChange = supportPolygonInWorldAfterChange.signedDistance(desiredICP);
-      double estimatedDistanceAfterChange = supportPolygonInWorldAfterChange.signedDistance(estimatedICP);
-
-      return (desiredDistanceAfterChange < currentDesiredDistance) && (estimatedDistanceAfterChange < currentEstimatedDistance);
-   }
+   private final FrameLine2D desiredICPToFinalICPLine = new FrameLine2D();
+   private final FrameLineSegment2D desiredICPToFinalICPLineSegment = new FrameLineSegment2D();
 
    private final FrameVector2D icpError = new FrameVector2D();
    private final FrameLine2D adjustedICPDynamicsLine = new FrameLine2D();
@@ -166,9 +85,19 @@ public class QuadrupedSwingSpeedUpCalculator
    private final FramePoint2D projectedDesiredICP = new FramePoint2D();
    private final FramePoint2D projectedEstimatedICP = new FramePoint2D();
 
-   private double estimateDeltaTimeBetweenDesiredICPAndActualICP(FramePoint2DReadOnly desiredICP, FramePoint2DReadOnly finalDesiredICP,
-                                                                 FramePoint2DReadOnly estimatedICP, FramePoint3DReadOnly perfectCMP)
+   private final FramePoint2D desiredICP = new FramePoint2D();
+   private final FramePoint2D finalDesiredICP = new FramePoint2D();
+   private final FramePoint2D estimatedICP = new FramePoint2D();
+
+   private double estimateDeltaTimeBetweenDesiredICPAndActualICP(FramePoint3DReadOnly desiredDCM, FramePoint3DReadOnly finalDesiredDCM,
+                                                                 FramePoint3DReadOnly estimatedDCM, FramePoint3DReadOnly perfectCMP)
    {
+      perfectCMP.checkReferenceFrameMatch(worldFrame);
+
+      desiredICP.set(desiredDCM);
+      finalDesiredICP.set(finalDesiredDCM);
+      estimatedICP.set(estimatedDCM);
+
       /*
        * The ICP plan is not being updated with the step adjustment. We approximate the step adjustment here by offsetting the final ICP and then projecting
        * the desired ICP onto these dynamics. Without this, the foot will not be set down more quickly for lateral errors
@@ -190,7 +119,14 @@ public class QuadrupedSwingSpeedUpCalculator
          return Double.NaN;
       }
 
-      projectEstimatedICPOntoDynamics(projectedEstimatedICP, estimatedICP, desiredICP, finalDesiredICP);
+      desiredICPToFinalICPLineSegment.set(projectedDesiredICP, offsetFinalDesiredICP);
+      double percentAlongLineSegmentICP = desiredICPToFinalICPLineSegment.percentageAlongLineSegment(estimatedICP);
+      if (percentAlongLineSegmentICP < 0.0)
+      {
+         desiredICPToFinalICPLine.set(projectedDesiredICP, offsetFinalDesiredICP);
+         projectedDesiredICP.setIncludingFrame(estimatedDCM);
+         desiredICPToFinalICPLine.orthogonalProjection(projectedEstimatedICP);
+      }
 
       double actualDistanceDueToDisturbance = perfectCMP.distanceXY(projectedEstimatedICP);
       double expectedDistanceAccordingToPlan = perfectCMP.distanceXY(offsetFinalDesiredICP);
@@ -202,46 +138,4 @@ public class QuadrupedSwingSpeedUpCalculator
       else
          return Math.log(distanceRatio) / omega.getValue();
    }
-
-   private final FrameLineSegment2D desiredICPToFinalICPLineSegment = new FrameLineSegment2D();
-
-   private double projectEstimatedICPOntoDynamics(FixedFramePoint2DBasics projectedEstimatedICPToPack, FramePoint2DReadOnly estimatedICP,
-                                                  FramePoint2DReadOnly desiredICP, FramePoint2DReadOnly finalDesiredICP)
-   {
-      if (desiredICP.distance(finalDesiredICP) < 1e-5)
-      {
-         projectedEstimatedICPToPack.set(estimatedICP);
-         return desiredICP.distance(estimatedICP);
-      }
-
-      desiredICPToFinalICPLineSegment.set(desiredICP, finalDesiredICP);
-      desiredICPToFinalICPLineSegment.orthogonalProjection(estimatedICP, projectedEstimatedICPToPack);
-
-      return desiredICP.distance(projectedEstimatedICPToPack);
-   }
-
-
-
-   private void updateSupportPolygon(FrameConvexPolygon2DBasics polygonToPack, QuadrantDependentList<? extends PlaneContactState> contactStates,
-                                     RobotQuadrant quadrantToAdd, FramePoint2DReadOnly positionToAdd)
-   {
-      polygonToPack.clear();
-
-      for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
-      {
-         if (!contactStates.get(robotQuadrant).inContact())
-         {
-            if (quadrantToAdd == robotQuadrant)
-               polygonToPack.addVertex(positionToAdd);
-            else
-               continue;
-         }
-
-         tempFootPoint2D.setToZero(soleFrames.get(robotQuadrant));
-         tempFootPoint2D.changeFrameAndProjectToXYPlane(worldFrame);
-         polygonToPack.addVertex(tempFootPoint2D);
-      }
-      polygonToPack.update();
-   }
-
 }
