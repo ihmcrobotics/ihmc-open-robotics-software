6213907b3ea108b109cd82efec2c49de2d46b73f
Robert <rgriffin@ihmc.us>
2019-06-09 15:48:27 -0400

    modifeid the straight shot velocity provider to allow it to zero out the planar velocity when near a goal

--- ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/stepCost/StraightShotVelocityProvider.java
+++ ihmc-quadruped-footstep-planning/src/main/java/us/ihmc/quadrupedFootstepPlanning/footstepPlanning/graphSearch/stepCost/StraightShotVelocityProvider.java
@@ -1,5 +1,6 @@
 package us.ihmc.quadrupedFootstepPlanning.footstepPlanning.graphSearch.stepCost;
 
+import us.ihmc.euclid.tools.EuclidCoreTools;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
@@ -20,36 +21,46 @@ public class StraightShotVelocityProvider implements NominalVelocityProvider
    {
       Vector2D heading = new Vector2D();
 
+      Point2DReadOnly nodeCenter = node.getOrComputeXGaitCenterPoint();
       heading.set(goalNode.getOrComputeXGaitCenterPoint());
-      heading.sub(node.getOrComputeXGaitCenterPoint());
+      heading.sub(nodeCenter);
       heading.normalize();
+      
+      heading.scale(computeDistanceToGoalScalar(nodeCenter.getX(), nodeCenter.getY()));
 
       return heading;
    }
 
    public double computeNominalYaw(Point2DReadOnly node)
    {
-      double distanceToGoal = node.distance(goalNode.getOrComputeXGaitCenterPoint());
-      double finalTurnProximity = 1.0;
-
-      double minimumBlendDistance = 0.75 * finalTurnProximity;
-      double maximumBlendDistance = 1.25 * finalTurnProximity;
-
       double pathHeading = Math.atan2(goalNode.getOrComputeXGaitCenterPoint().getY() - node.getY(),
                                       goalNode.getOrComputeXGaitCenterPoint().getX() - node.getX());
       pathHeading = AngleTools.trimAngleMinusPiToPi(pathHeading);
 
-      double yawMultiplier;
-      if (distanceToGoal < minimumBlendDistance)
-         yawMultiplier = 0.0;
-      else if(distanceToGoal > maximumBlendDistance)
-         yawMultiplier = 1.0;
-      else
-         yawMultiplier = (distanceToGoal - minimumBlendDistance) / (maximumBlendDistance - minimumBlendDistance);
-
+      double yawMultiplier = computeDistanceToGoalScalar(node.getX(), node.getY());
       double referenceHeading = yawMultiplier * pathHeading;
       referenceHeading += (1.0 - yawMultiplier) * goalNode.getNominalYaw();
       return AngleTools.trimAngleMinusPiToPi(referenceHeading);
+   }
+   
+   private double computeDistanceToGoalScalar(double x, double y)
+   {
+      Point2DReadOnly goalCenter = goalNode.getOrComputeXGaitCenterPoint();
+      double distanceToGoal = EuclidCoreTools.norm(x - goalCenter.getX(), y - goalCenter.getY());
+      double finalTurnProximity = 1.0;
+
+      double minimumBlendDistance = 0.75 * finalTurnProximity;
+      double maximumBlendDistance = 1.25 * finalTurnProximity;
+      
+      double multiplier;
+      if (distanceToGoal < minimumBlendDistance)
+         multiplier = 0.0;
+      else if(distanceToGoal > maximumBlendDistance)
+         multiplier = 1.0;
+      else
+         multiplier = (distanceToGoal - minimumBlendDistance) / (maximumBlendDistance - minimumBlendDistance);
+      
+      return multiplier;
    }
 
 }
