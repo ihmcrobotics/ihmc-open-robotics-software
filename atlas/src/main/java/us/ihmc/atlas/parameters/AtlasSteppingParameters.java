1647ff8f5421c3c6ef7aa0af11d75c184efa06a7
Duncan Calvert <dcalvert@ihmc.us>
2019-06-24 10:37:53 -0500

    Revert parameter changes.

--- atlas/src/main/java/us/ihmc/atlas/parameters/AtlasSteppingParameters.java
+++ atlas/src/main/java/us/ihmc/atlas/parameters/AtlasSteppingParameters.java
@@ -15,15 +15,15 @@ public class AtlasSteppingParameters implements SteppingParameters
    @Override
    public double getMinSwingHeightFromStanceFoot()
    {
-      return 0.05 * jointMap.getModelScale();
+      return 0.10 * jointMap.getModelScale();
    }
 
    @Override
    public double getDefaultSwingHeightFromStanceFoot()
    {
-      return 0.08;
+      return getMinSwingHeightFromStanceFoot();
    }
-
+   
    @Override
    public double getMaxSwingHeightFromStanceFoot()
    {
