c3ad330e3365a7be2f2de2676b206941d60248a9
Duncan Calvert <dcalvert@ihmc.us>
2019-06-25 15:16:35 -0500

    Add ROS2 messaging example.

--- ihmc-communication/src/test/java/us/ihmc/communication/ROS2ToolsTest.java
+++ ihmc-communication/src/test/java/us/ihmc/communication/ROS2ToolsTest.java
@@ -5,9 +5,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.base.CaseFormat;
+import std_msgs.msg.dds.Int64;
+import std_msgs.msg.dds.Int8;
+import us.ihmc.commons.thread.ThreadTools;
+import us.ihmc.log.LogTools;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
+import us.ihmc.ros2.Ros2Node;
+import us.ihmc.tools.thread.ExceptionHandlingThreadScheduler;
 
 class ROS2ToolsTest
 {
+   public static void main(String[] args)
+   {
+      new ROS2ToolsTest().testROS2Communication();
+   }
+
+   public void testROS2Communication()
+   {
+      Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, getClass().getSimpleName());
+
+      ROS2ModuleIdentifier id = new ROS2ModuleIdentifier("ihmc_test", "/test");
+
+      String robotName = "skippy";
+      IHMCROS2Publisher intPublisher = new IHMCROS2Publisher(ros2Node, Int64.class, null, null, null);
+
+      new ROS2Callback<>(ros2Node, Int64.class, null, null, null, this::acceptMessage);
+
+      new ExceptionHandlingThreadScheduler(getClass().getSimpleName()).schedule(() ->
+      {
+         Int64 num = new Int64();
+         num.setData(System.nanoTime());
+         LogTools.info("Publishing: {}", num.getData());
+         intPublisher.publish(num);
+      }, 1.0);
+
+      try
+      {
+         Thread.currentThread().join();
+      }
+      catch (InterruptedException e)
+      {
+         e.printStackTrace();
+      }
+   }
+
+   private void acceptMessage(Int64 message)
+   {
+      LogTools.info("Received int: {}", message);
+   }
 
    @Test
    void testToROSTopicFormat()
