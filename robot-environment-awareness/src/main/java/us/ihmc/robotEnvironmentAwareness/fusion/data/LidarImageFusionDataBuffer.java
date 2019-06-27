e744a8aeff019dab54d91ebefee3e201b7b084f5
inhol <ilee@ihmc.us>
2019-06-27 16:34:45 -0500

    Redefined data factory.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/data/LidarImageFusionDataBuffer.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/data/LidarImageFusionDataBuffer.java
@@ -1,10 +1,12 @@
 package us.ihmc.robotEnvironmentAwareness.fusion.data;
 
 import java.awt.image.BufferedImage;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
 
 import boofcv.struct.calib.IntrinsicParameters;
 import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
+import us.ihmc.commons.Conversions;
 import us.ihmc.communication.packets.MessageTools;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple4D.Quaternion;
@@ -16,7 +18,7 @@ import us.ihmc.robotEnvironmentAwareness.fusion.tools.LidarImageFusionDataFactor
 
 public class LidarImageFusionDataBuffer
 {
-   //private final IntrinsicParameters intrinsicParameters;
+   private final LidarImageFusionDataFactory lidarImageFusionDataFactory = new LidarImageFusionDataFactory();
 
    private final AtomicReference<StereoVisionPointCloudMessage> latestStereoVisionPointCloudMessage = new AtomicReference<>(null);
    private final AtomicReference<BufferedImage> latestBufferedImage = new AtomicReference<>(null);
@@ -24,7 +26,7 @@ public class LidarImageFusionDataBuffer
    private final AtomicReference<Point3D> latestCameraPosition;
    private final AtomicReference<Quaternion> latestCameraOrientation;
    private final AtomicReference<IntrinsicParameters> latestCameraIntrinsicParameters;
-   
+
    private final AtomicReference<Integer> bufferSize;
 
    private final AtomicReference<SegmentationRawDataFilteringParameters> latestSegmentationRawDataFilteringParameters;
@@ -38,7 +40,7 @@ public class LidarImageFusionDataBuffer
       latestImageSegmentationParaeters = messager.createInput(LidarImageFusionAPI.ImageSegmentationParameters, new ImageSegmentationParameters());
       latestSegmentationRawDataFilteringParameters = messager.createInput(LidarImageFusionAPI.SegmentationRawDataFilteringParameters,
                                                                           new SegmentationRawDataFilteringParameters());
-      
+
       latestCameraPosition = messager.createInput(LidarImageFusionAPI.CameraPositionState, new Point3D());
       latestCameraOrientation = messager.createInput(LidarImageFusionAPI.CameraOrientationState, new Quaternion());
       latestCameraIntrinsicParameters = messager.createInput(LidarImageFusionAPI.CameraIntrinsicParametersState, new IntrinsicParameters());
@@ -51,20 +53,41 @@ public class LidarImageFusionDataBuffer
 
    public void updateNewBuffer()
    {
-      StereoVisionPointCloudMessage stereoVisionPointCloudMessage = latestStereoVisionPointCloudMessage.get();
-      ImageSegmentationParameters imageSegmentationParameters = latestImageSegmentationParaeters.get();
+      StereoVisionPointCloudMessage pointCloudMessage = latestStereoVisionPointCloudMessage.get();
 
-      SegmentationRawDataFilteringParameters segmentationRawDataFilteringParameters = latestSegmentationRawDataFilteringParameters.get();
-      LidarImageFusionData data = LidarImageFusionDataFactory.createLidarImageFusionData(MessageTools.unpackScanPoint3ds(stereoVisionPointCloudMessage),
-                                                                                         latestBufferedImage.get(), latestCameraIntrinsicParameters.get(), bufferSize.get(),
-                                                                                         latestCameraPosition.get(), latestCameraOrientation.get(),
-                                                                                         imageSegmentationParameters.getPixelSize(),
-                                                                                         imageSegmentationParameters.getPixelRuler(),
-                                                                                         true, imageSegmentationParameters.getMinElementSize(),
-                                                                                         imageSegmentationParameters.getIterate(),
-                                                                                         segmentationRawDataFilteringParameters.isEnableFilterFlyingPoint(),
-                                                                                         segmentationRawDataFilteringParameters.getFlyingPointThreshold(),
-                                                                                         segmentationRawDataFilteringParameters.getMinimumNumberOfFlyingPointNeighbors());
+      long pruningStart = System.nanoTime();
+      Point3D[] pointCloudBuffer = MessageTools.unpackScanPoint3ds(pointCloudMessage);
+      int[] colorBuffer = pointCloudMessage.getColors().toArray();
+      Random random = new Random();
+      int numberOfPoints = pointCloudBuffer.length;
+
+      while (numberOfPoints > bufferSize.get())
+      {
+         int indexToRemove = random.nextInt(numberOfPoints);
+         int lastIndex = numberOfPoints - 1;
+
+         pointCloudBuffer[indexToRemove] = pointCloudBuffer[lastIndex];
+         colorBuffer[indexToRemove] = colorBuffer[lastIndex];
+
+         numberOfPoints--;
+      }
+
+      Point3D[] pointCloud = new Point3D[numberOfPoints];
+      int[] colors = new int[numberOfPoints];
+      for (int i = 0; i < numberOfPoints; i++)
+      {
+         pointCloud[i] = pointCloudBuffer[i];
+         colors[i] = colorBuffer[i];
+      }
+      System.out.println("Pruning time " + Conversions.nanosecondsToSeconds(System.nanoTime() - pruningStart));
+
+      //      lidarImageFusionDataFactory.setIntrinsicParameters(latestCameraIntrinsicParameters.get());
+      lidarImageFusionDataFactory.setImageSegmentationParameters(latestImageSegmentationParaeters.get());
+      lidarImageFusionDataFactory.setSegmentationRawDataFilteringParameters(latestSegmentationRawDataFilteringParameters.get());
+      //      lidarImageFusionDataFactory.setCameraPose(latestCameraPosition.get(), latestCameraOrientation.get());
+
+      LidarImageFusionData data = lidarImageFusionDataFactory.createLidarImageFusionData(pointCloud, colors, latestBufferedImage.get());
+
       newBuffer.set(data);
    }
 
@@ -77,5 +100,4 @@ public class LidarImageFusionDataBuffer
    {
       latestBufferedImage.set(bufferedImage);
    }
-
 }
