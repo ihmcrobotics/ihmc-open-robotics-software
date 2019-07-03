6533635a3d8fadd9ee61275f306ac2c2f1d79bc7
Robert <rgriffin@ihmc.us>
2019-07-02 21:12:14 -0500

    implemented a foothold area cost

---
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepCost/FootholdAreaCost.java
@@ -0,0 +1,37 @@
+package us.ihmc.footstepPlanning.graphSearch.stepCost;
+
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DReadOnly;
+import us.ihmc.footstepPlanning.graphSearch.footstepSnapping.FootstepNodeSnapData;
+import us.ihmc.footstepPlanning.graphSearch.footstepSnapping.FootstepNodeSnapperReadOnly;
+import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
+import us.ihmc.robotics.robotSide.SideDependentList;
+
+public class FootholdAreaCost implements FootstepCost
+{
+   private static final double weight = 1.0;
+
+   private final SideDependentList<? extends ConvexPolygon2DReadOnly> footPolygons;
+   private final FootstepNodeSnapperReadOnly snapper;
+
+   public FootholdAreaCost(SideDependentList<? extends ConvexPolygon2DReadOnly> footPolygons, FootstepNodeSnapperReadOnly snapper)
+   {
+      this.footPolygons = footPolygons;
+      this.snapper = snapper;
+   }
+
+   @Override
+   public double compute(FootstepNode startNode, FootstepNode endNode)
+   {
+      FootstepNodeSnapData snapData = snapper.getSnapData(endNode);
+      ConvexPolygon2D footholdAfterSnap = snapData.getCroppedFoothold();
+      double area = footholdAfterSnap.getArea();
+      double footArea = footPolygons.get(endNode.getRobotSide()).getArea();
+
+      if (footholdAfterSnap.isEmpty())
+         return 0.0;
+
+      double percentAreaUnoccupied = 1.0 - area / footArea;
+      return percentAreaUnoccupied * weight;
+   }
+}
