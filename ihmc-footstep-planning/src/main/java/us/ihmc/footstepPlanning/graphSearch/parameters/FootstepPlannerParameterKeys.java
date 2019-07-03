503c6678a276f943ea2c7a611f9a1d8837d34e5b
Robert <rgriffin@ihmc.us>
2019-07-02 21:42:54 -0500

    added the new cost parameters to all the messages

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/FootstepPlannerParameterKeys.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/FootstepPlannerParameterKeys.java
@@ -66,4 +66,5 @@ public class FootstepPlannerParameterKeys
    public static final DoubleStoredPropertyKey  maximum2dDistanceFromBoundingBoxToPenalize = keys.addDoubleKey ("Maximum 2D distance from bounding box to penalize", 0.0);
    public static final DoubleStoredPropertyKey  boundingBoxCost                            = keys.addDoubleKey ("Bounding box cost", 0.0);
    public static final DoubleStoredPropertyKey  footholdAreaWeight                         = keys.addDoubleKey ("Foothold area weight", 1.0);
+   public static final DoubleStoredPropertyKey  longStepWeight                             = keys.addDoubleKey ("Long step weight", 1.0);
 }
--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/FootstepPlannerParametersReadOnly.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/parameters/FootstepPlannerParametersReadOnly.java
@@ -36,7 +36,6 @@ public interface FootstepPlannerParametersReadOnly extends StoredPropertySetRead
 
    /**
     * Returns the ideal step width for walking on flat ground.
-    * This is ONLY used when assumed to be walking on flat ground.
     */
    default double getIdealFootstepWidth()
    {
@@ -45,7 +44,6 @@ public interface FootstepPlannerParametersReadOnly extends StoredPropertySetRead
 
    /**
     * Returns the ideal step length for walking on flat ground.
-    * This is ONLY used when assumed to be walking on flat ground.
     */
    default double getIdealFootstepLength()
    {
@@ -667,6 +665,14 @@ public interface FootstepPlannerParametersReadOnly extends StoredPropertySetRead
    default double getFootholdAreaWeight()
    {
       return get(footholdAreaWeight);
+   }
+
+   /**
+    * When using a cost based planning approach this value defines the weight of the step distance of a step longer than {@link FootstepPlannerParameters#getIdealFootstepLength()} .
+    */
+   default double getLongStepWeight()
+   {
+      return get(longStepWeight);
    }
 
    /**
