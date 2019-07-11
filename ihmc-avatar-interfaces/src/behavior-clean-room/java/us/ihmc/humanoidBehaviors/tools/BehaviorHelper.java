efe6ff74b800f9a7474a65fac959d84e055f6579
JerryPratt <jpratt@ihmc.us>
2019-07-11 10:51:35 -0500

    Pulling out functionality and putting in BehaviorHelper

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/BehaviorHelper.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/BehaviorHelper.java
@@ -2,19 +2,28 @@ package us.ihmc.humanoidBehaviors.tools;
 
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import controller_msgs.msg.dds.FootstepDataListMessage;
 import controller_msgs.msg.dds.FootstepStatusMessage;
+import controller_msgs.msg.dds.PlanarRegionsListMessage;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.ControllerAPIDefinition;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.HighLevelHumanoidControllerFactory;
 import us.ihmc.communication.IHMCROS2Publisher;
 import us.ihmc.communication.ROS2Callback;
+import us.ihmc.communication.ROS2Input;
 import us.ihmc.communication.ROS2Tools;
+import us.ihmc.communication.packets.PlanarRegionMessageConverter;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.FootstepDataListCommand;
+import us.ihmc.humanoidRobotics.frames.HumanoidReferenceFrames;
 import us.ihmc.messager.Messager;
 import us.ihmc.messager.MessagerAPIFactory.Topic;
+import us.ihmc.robotEnvironmentAwareness.updaters.LIDARBasedREAModule;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.ros2.Ros2Node;
 import us.ihmc.tools.thread.ActivationReference;
 import us.ihmc.util.PeriodicNonRealtimeThreadScheduler;
@@ -26,7 +35,12 @@ public class BehaviorHelper
    private final Ros2Node ros2Node;
 
    private final RemoteSyncedRobotModel remoteSyncedRobotModel;
+   private final RemoteSyncedHumanoidFrames remoteSyncedHumanoidFrames;
+
    private final IHMCROS2Publisher<FootstepDataListMessage> footstepDataListPublisher;
+
+   private final ROS2Input<PlanarRegionsListMessage> planarRegionsList;
+
    private PeriodicNonRealtimeThreadScheduler threadScheduler;
 
    public BehaviorHelper(Messager messager, DRCRobotModel robotModel, Ros2Node ros2Node)
@@ -36,13 +50,27 @@ public class BehaviorHelper
       this.ros2Node = ros2Node;
 
       remoteSyncedRobotModel = new RemoteSyncedRobotModel(robotModel, ros2Node);
+      remoteSyncedHumanoidFrames = new RemoteSyncedHumanoidFrames(robotModel, ros2Node);
+
       footstepDataListPublisher = ROS2Tools.createPublisher(ros2Node, ROS2Tools.newMessageInstance(FootstepDataListCommand.class).getMessageClass(),
                                                             ControllerAPIDefinition.getSubscriberTopicNameGenerator(robotModel.getSimpleRobotName()));
+      
+      planarRegionsList = new ROS2Input<>(ros2Node, PlanarRegionsListMessage.class, null, LIDARBasedREAModule.ROS2_ID);
    }
 
    public FullHumanoidRobotModel pollFullRobotModel()
    {
       return remoteSyncedRobotModel.pollFullRobotModel();
+   }
+
+   public HumanoidReferenceFrames pollHumanoidReferenceFrames()
+   {
+      return remoteSyncedHumanoidFrames.pollHumanoidReferenceFrames();
+   }
+
+   public FramePose3DReadOnly quickPollPoseReadOnly(Function<HumanoidReferenceFrames, ReferenceFrame> frameSelector)
+   {
+      return remoteSyncedHumanoidFrames.quickPollPoseReadOnly(frameSelector);
    }
 
    public void publishFootstepList(FootstepDataListMessage footstepList)
@@ -72,6 +100,16 @@ public class BehaviorHelper
    public ActivationReference<Boolean> createBooleanActivationReference(Topic<Boolean> topic, boolean initialValue, boolean activationValue)
    {
       return new ActivationReference<>(messager.createInput(topic, initialValue), activationValue);
+   }
+
+   public PlanarRegionsList getLatestPlanarRegionList()
+   {
+      return PlanarRegionMessageConverter.convertToPlanarRegionsList(planarRegionsList.getLatest());
+   }
+
+   public PlanarRegionsListMessage getLatestPlanarRegionListMessage()
+   {
+      return planarRegionsList.getLatest();
    }
 
 }
