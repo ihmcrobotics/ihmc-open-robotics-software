86ad486f8693b4f986d449d6a135b014803db325
Duncan Calvert <dcalvert@ihmc.us>
2019-07-15 16:47:50 -0500

    Stubbed out PlanarRegionSLAMUI.

--- ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/slam/PlanarRegionSLAMUITabController.java
+++ ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/slam/PlanarRegionSLAMUITabController.java
@@ -2,13 +2,19 @@ package us.ihmc.humanoidBehaviors.ui.slam;
 
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.humanoidBehaviors.ui.graphics.PlanarRegionsGraphic;
+import us.ihmc.javaFXVisualizers.PrivateAnimationTimer;
 import us.ihmc.robotics.PlanarRegionFileTools;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlanarRegionSLAMUITabController extends Group
 {
@@ -18,27 +24,56 @@ public class PlanarRegionSLAMUITabController extends Group
    private static final String DATASET_2 = "20190710_174208_PlanarRegion";
    private static final String DATASET_3 = "20190710_174422_PlanarRegion";
 
-   @FXML private CheckBox regionSet1;
-   @FXML private CheckBox regionSet2;
-   @FXML private CheckBox regionSet3;
+   @FXML private CheckBox acceptNewRegionListsCheckbox;
+   @FXML private Button slamButton;
+   @FXML private Button exportMapButton;
+   @FXML private Button clearMapButton;
+   @FXML private Button exportIncomingButton;
 
-   private PlanarRegionsGraphic regionsGraphicOne;
-   private PlanarRegionsGraphic regionsGraphicTwo;
-   private PlanarRegionsGraphic regionsGraphicThree;
+   @FXML private CheckBox fakeREAPublisherCheckbox;
+   @FXML private RadioButton dataset1RadioButton;
+   @FXML private RadioButton dataset2RadioButton;
+   @FXML private RadioButton dataset3RadioButton;
+   @FXML private RadioButton loadFromFileRadioButton;
+
+   private List<RadioButton> datasetSelectionRadioButtons = new ArrayList<>();
+
+   private volatile PlanarRegionsList datasetToPublish = EMPTY_REGIONS_LIST;
+
+   private PlanarRegionsGraphic regionsGraphicOne = new PlanarRegionsGraphic();
+   private PlanarRegionsGraphic regionsGraphicTwo = new PlanarRegionsGraphic();
+   private PlanarRegionsGraphic regionsGraphicThree = new PlanarRegionsGraphic();
+
+   private PrivateAnimationTimer animationTimer = new PrivateAnimationTimer(this::fxUpdate);
 
    public void init()
    {
-      regionsGraphicOne = createPlanarRegionsGraphic(DATASET_1);
-      regionsGraphicTwo = createPlanarRegionsGraphic(DATASET_2);
-      regionsGraphicThree = createPlanarRegionsGraphic(DATASET_3);
+      datasetSelectionRadioButtons.add(dataset1RadioButton);
+      datasetSelectionRadioButtons.add(dataset2RadioButton);
+      datasetSelectionRadioButtons.add(dataset3RadioButton);
+      datasetSelectionRadioButtons.add(loadFromFileRadioButton);
+
+      getChildren().add(regionsGraphicOne);
+      getChildren().add(regionsGraphicTwo);
+      getChildren().add(regionsGraphicThree);
+
+      generateOnAThread(regionsGraphicOne, loadDataSet(DATASET_1));
+      generateOnAThread(regionsGraphicTwo, loadDataSet(DATASET_2));
+      generateOnAThread(regionsGraphicThree, loadDataSet(DATASET_3));
+
+      animationTimer.start();
    }
 
-   private PlanarRegionsGraphic createPlanarRegionsGraphic(String dataSetName)
+   private void fxUpdate(long now)
    {
-      PlanarRegionsGraphic regionsGraphic = new PlanarRegionsGraphic();
-      generateRegions(regionsGraphic, loadDataSet(dataSetName));
-      getChildren().add(regionsGraphic);
-      return regionsGraphic;
+      regionsGraphicOne.update();
+      regionsGraphicTwo.update();
+      regionsGraphicThree.update();
+   }
+
+   private void generateOnAThread(PlanarRegionsGraphic regionsGraphicOne, PlanarRegionsList planarRegionsList)
+   {
+      ThreadTools.startAThread(() -> regionsGraphicOne.generateMeshes(planarRegionsList), "MeshGeneration");
    }
 
    private PlanarRegionsList loadDataSet(String dataSetName)
@@ -48,45 +83,84 @@ public class PlanarRegionSLAMUITabController extends Group
       return PlanarRegionFileTools.importPlanarRegionData(path.toFile());
    }
 
-   private void generateRegions(PlanarRegionsGraphic graphic, PlanarRegionsList list)
+   private void setRadioButtonSelection(RadioButton selection)
    {
-      graphic.generateMeshes(list);
-      graphic.update();
+      datasetSelectionRadioButtons.forEach(it -> it.setSelected(false));
+      selection.setSelected(true);
    }
 
-   @FXML private void regionSet1()
+   @FXML private void acceptNewRegionListsCheckbox()
    {
-      if (regionSet1.isSelected())
-      {
-         generateRegions(regionsGraphicOne, loadDataSet(DATASET_1));
-      }
-      else
-      {
-         generateRegions(regionsGraphicOne, EMPTY_REGIONS_LIST);
-      }
+
    }
 
-   @FXML private void regionSet2()
+   @FXML private void slamButton()
    {
-      if (regionSet2.isSelected())
-      {
-         generateRegions(regionsGraphicTwo, loadDataSet(DATASET_2));
-      }
-      else
-      {
-         generateRegions(regionsGraphicTwo, EMPTY_REGIONS_LIST);
-      }
+
    }
 
-   @FXML private void regionSet3()
+   @FXML private void exportMapButton()
    {
-      if (regionSet3.isSelected())
-      {
-         generateRegions(regionsGraphicThree, loadDataSet(DATASET_3));
-      }
-      else
-      {
-         generateRegions(regionsGraphicThree, EMPTY_REGIONS_LIST);
-      }
+
+   }
+
+   @FXML private void clearMapButton()
+   {
+
+   }
+
+   @FXML private void exportIncomingButton()
+   {
+
+   }
+
+   @FXML private void fakeREAPublisherCheckbox()
+   {
+
+   }
+
+   @FXML private void dataset1RadioButton()
+   {
+      setRadioButtonSelection(dataset1RadioButton);
+//      if (regionSet1.isSelected())
+//      {
+//         generateOnAThread(regionsGraphicOne, loadDataSet(DATASET_1));
+//      }
+//      else
+//      {
+//         generateOnAThread(regionsGraphicOne, EMPTY_REGIONS_LIST);
+//      }
+   }
+
+   @FXML private void dataset2RadioButton()
+   {
+      setRadioButtonSelection(dataset2RadioButton);
+//      if (regionSet2.isSelected())
+//      {
+//         generateOnAThread(regionsGraphicTwo, loadDataSet(DATASET_2));
+//      }
+//      else
+//      {
+//         generateOnAThread(regionsGraphicTwo, EMPTY_REGIONS_LIST);
+//      }
+   }
+
+   @FXML private void dataset3RadioButton()
+   {
+      setRadioButtonSelection(dataset3RadioButton);
+//      if (regionSet3.isSelected())
+//      {
+//         generateOnAThread(regionsGraphicThree, loadDataSet(DATASET_3));
+//      }
+//      else
+//      {
+//         generateOnAThread(regionsGraphicThree, EMPTY_REGIONS_LIST);
+//      }
+   }
+
+   @FXML private void loadFromFileRadioButton()
+   {
+      setRadioButtonSelection(loadFromFileRadioButton);
+
    }
 }
