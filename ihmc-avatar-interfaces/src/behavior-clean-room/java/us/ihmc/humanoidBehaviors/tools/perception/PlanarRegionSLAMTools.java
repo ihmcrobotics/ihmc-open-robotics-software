aa055753682157b9585c63d684b73ede758ebd8b
Duncan Calvert <dcalvert@ihmc.us>
2019-07-17 17:12:18 -0500

    Implement detectBoundingBoxCollisions.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAMTools.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAMTools.java
@@ -5,12 +5,32 @@ import us.ihmc.euclid.shape.collision.gjk.GilbertJohnsonKeerthiCollisionDetector
 import us.ihmc.euclid.shape.primitives.Box3D;
 import us.ihmc.robotics.geometry.GeometryTools;
 import us.ihmc.robotics.geometry.PlanarRegion;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PlanarRegionSLAMTools
 {
-   public static void detectBoundingBoxCollisions()
+   public static Map<PlanarRegion, List<PlanarRegion>> detectBoundingBoxCollisions(PlanarRegionsList map, PlanarRegionsList newData)
    {
+      HashMap<PlanarRegion, List<PlanarRegion>> newDataCollisions = new HashMap<>();
+      for (PlanarRegion planarRegion : map.getPlanarRegionsAsList())
+      {
+         ArrayList<PlanarRegion> localCollisions = new ArrayList<>();
+         newDataCollisions.put(planarRegion, localCollisions);
 
+         for (PlanarRegion newRegion : newData.getPlanarRegionsAsList())
+         {
+            if (boundingBoxesIntersect(planarRegion, newRegion))
+            {
+               localCollisions.add(newRegion);
+            }
+         }
+      }
+      return newDataCollisions;
    }
 
    public static boolean boundingBoxesIntersect(PlanarRegion a, PlanarRegion b)
