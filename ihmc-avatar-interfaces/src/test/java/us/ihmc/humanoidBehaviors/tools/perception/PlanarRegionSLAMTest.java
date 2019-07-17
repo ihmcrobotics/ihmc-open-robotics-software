156d3f3f5995e22a364cd99c35d5a72046b90463
JerryPratt <jpratt@ihmc.us>
2019-07-17 12:19:55 -0500

    Simple tests for testFindHighConfidencePairsWithSomeSimpleSquares()

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAMTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAMTest.java
@@ -21,24 +21,110 @@ class PlanarRegionSLAMTest
 {
 
    @Test
-   void testFindHighConfidencePairs()
+   void testFindHighConfidencePairsWithSomeSimpleSquares()
+   {
+      // Two identical squares.
+      PlanarRegion squareOne = createASingleSquareCenteredAtOrigin(1.0, 1.0);
+      PlanarRegion squareTwo = createASingleSquareCenteredAtOrigin(1.0, 1.0);
+      assertHighConfidencePairingBothWays(squareOne, squareTwo);
+
+      // One small square inside a larger square.
+      squareOne = createASingleSquareCenteredAtOrigin(1.0, 1.0);
+      squareTwo = createASingleSquareCenteredAtOrigin(0.5, 0.5);
+      assertHighConfidencePairingBothWays(squareOne, squareTwo);
+
+      Vector3D translationOne = new Vector3D(10.1, -20.3, 7.6);
+
+      // One large, one small translated same amounts.
+      squareOne = createASingleTranslatedSquare(translationOne, 1.0, 1.0);
+      squareTwo = createASingleTranslatedSquare(translationOne, 0.5, 0.5);
+      assertHighConfidencePairingBothWays(squareOne, squareTwo);
+
+      // One large, one small, but translated a significant amount in z, so should not be a pair.
+      Vector3D largeZOffset = new Vector3D(0.0, 0.0, 0.2);
+      Vector3D translationTwo = new Vector3D(translationOne);
+      translationTwo.add(largeZOffset);
+      squareOne = createASingleTranslatedSquare(translationOne, 1.0, 1.0);
+      squareTwo = createASingleTranslatedSquare(translationTwo, 0.5, 0.5);
+      assertNotAHighConfidencePairing(squareOne, squareTwo);
+
+      // One large, one small, shifted a bit but still overlapping, with a small z delta.
+      double epsilon = 1e-3;
+      Vector3D smallZOffset = new Vector3D(0.0, 0.0, epsilon);
+      Vector3D overlappingHorizontalOffset = new Vector3D(0.1, 0.2, 0.0);
+
+      translationTwo = new Vector3D(translationOne);
+      translationTwo.add(smallZOffset);
+      translationTwo.add(overlappingHorizontalOffset);
+      squareOne = createASingleTranslatedSquare(translationOne, 1.0, 1.0);
+      squareTwo = createASingleTranslatedSquare(translationTwo, 0.5, 0.5);
+      assertHighConfidencePairingBothWays(squareOne, squareTwo);
+
+      translationTwo = new Vector3D(translationOne);
+      translationTwo.sub(smallZOffset);
+      translationTwo.sub(overlappingHorizontalOffset);
+      squareOne = createASingleTranslatedSquare(translationOne, 1.0, 1.0);
+      squareTwo = createASingleTranslatedSquare(translationTwo, 0.5, 0.5);
+      assertHighConfidencePairingBothWays(squareOne, squareTwo);
+
+      // One large, one small, both rotated differently in yaw should still overlap.
+      translationTwo = new Vector3D(translationOne);
+      squareOne = createASingleTranslatedAndYawedSquare(translationOne, Math.PI / 2.0, 1.0, 1.0);
+      squareTwo = createASingleTranslatedAndYawedSquare(translationTwo, -Math.PI / 3.0, 0.5, 0.5);
+      assertHighConfidencePairingBothWays(squareOne, squareTwo);
+
+   }
+
+   private void assertHighConfidencePairingBothWays(PlanarRegion regionOne, PlanarRegion regionTwo)
+   {
+      assertHighConfidencePairing(regionOne, regionTwo);
+      assertHighConfidencePairing(regionTwo, regionOne);
+   }
+
+   private void assertHighConfidencePairing(PlanarRegion regionOne, PlanarRegion regionTwo)
    {
       PlanarRegionsList map = new PlanarRegionsList();
       PlanarRegionsList newData = new PlanarRegionsList();
 
-      PlanarRegion unitSquare = createASingleSquareCenteredAtOrigin(1.0, 1.0);
-      PlanarRegion anotherUnitSquare = createASingleSquareCenteredAtOrigin(1.0, 1.0);
-
-      map.addPlanarRegion(unitSquare);
-      newData.addPlanarRegion(anotherUnitSquare);
+      map.addPlanarRegion(regionOne);
+      newData.addPlanarRegion(regionTwo);
 
       PairList<PlanarRegion, PlanarRegion> highConfidencePairs = PlanarRegionSLAM.findHighConfidencePairs(map, newData);
 
       assertEquals(1, highConfidencePairs.size());
       ImmutablePair<PlanarRegion, PlanarRegion> pair = highConfidencePairs.get(0);
 
-      assertTrue(unitSquare == pair.getLeft());
-      assertTrue(anotherUnitSquare == pair.getRight());
+      assertTrue(regionOne == pair.getLeft());
+      assertTrue(regionTwo == pair.getRight());
+   }
+
+   private void assertNotAHighConfidencePairing(PlanarRegion regionOne, PlanarRegion regionTwo)
+   {
+      PlanarRegionsList map = new PlanarRegionsList();
+      PlanarRegionsList newData = new PlanarRegionsList();
+
+      map.addPlanarRegion(regionOne);
+      newData.addPlanarRegion(regionTwo);
+
+      PairList<PlanarRegion, PlanarRegion> highConfidencePairs = PlanarRegionSLAM.findHighConfidencePairs(map, newData);
+
+      assertTrue(highConfidencePairs.isEmpty());
+   }
+
+   private PlanarRegion createASingleTranslatedAndYawedSquare(Vector3D translation, double yaw, double xSize, double ySize)
+   {
+      Point2D minimumPoint = new Point2D(-xSize / 2.0, -ySize / 2.0);
+      Point2D maximumPoint = new Point2D(xSize / 2.0, ySize / 2.0);
+
+      return createASingleSquare(translation, yaw, 0.0, 0.0, minimumPoint, maximumPoint);
+   }
+
+   private PlanarRegion createASingleTranslatedSquare(Vector3D translation, double xSize, double ySize)
+   {
+      Point2D minimumPoint = new Point2D(-xSize / 2.0, -ySize / 2.0);
+      Point2D maximumPoint = new Point2D(xSize / 2.0, ySize / 2.0);
+
+      return createASingleSquare(translation, 0.0, 0.0, 0.0, minimumPoint, maximumPoint);
    }
 
    private PlanarRegion createASingleSquareCenteredAtOrigin(double xSize, double ySize)
@@ -65,6 +151,49 @@ class PlanarRegionSLAMTest
       PlanarRegion square = new PlanarRegion(transformToWorld, convexPolygon);
       return square;
 
+      //      List<ConvexPolygon2D> polygonsRegion1 = new ArrayList<>();
+      //
+      //      ConvexPolygon2D polygon11 = new ConvexPolygon2D();
+      //      polygon11.addVertex(0.0, 0.0);
+      //      polygon11.addVertex(2.0, 0.0);
+      //      polygon11.addVertex(2.0, 0.5);
+      //      polygon11.addVertex(0.0, 0.5);
+      //      polygon11.update();
+      //      polygonsRegion1.add(polygon11);
+      //
+      //      ConvexPolygon2D polygon12 = new ConvexPolygon2D();
+      //      polygon12.addVertex(1.0, 0.0);
+      //      polygon12.addVertex(2.0, 0.0);
+      //      polygon12.addVertex(2.0, -1.5);
+      //      polygon12.addVertex(1.0, -1.5);
+      //      polygon12.update();
+      //      polygonsRegion1.add(polygon12);
+      //
+      //      RigidBodyTransform transform1 = new RigidBodyTransform();
+      //      PlanarRegion region1 = new PlanarRegion(transform1, polygonsRegion1);
+      //
+      //      List<ConvexPolygon2D> polygonsRegion2 = new ArrayList<>();
+      //      ConvexPolygon2D polygon21 = new ConvexPolygon2D();
+      //      polygon21.addVertex(-1.0, 0.1);
+      //      polygon21.addVertex(1.0, 0.1);
+      //      polygon21.addVertex(1.0, -0.1);
+      //      polygon21.addVertex(-1.0, -0.1);
+      //      polygon21.update();
+      //      polygonsRegion2.add(polygon21);
+      //
+      //      ConvexPolygon2D polygon22 = new ConvexPolygon2D();
+      //      polygon22.addVertex(1.5, 0.1);
+      //      polygon22.addVertex(2.0, 0.1);
+      //      polygon22.addVertex(2.0, -0.1);
+      //      polygon22.addVertex(1.5, -0.1);
+      //      polygon22.update();
+      //      polygonsRegion2.add(polygon22);
+      //
+      //      RigidBodyTransform transform2 = new RigidBodyTransform();
+      //      transform2.setTranslation(0.5, 0.0, 0.0);
+      //      transform2.appendYawRotation(-Math.PI / 4.0);
+      //      transform2.appendRollRotation(Math.PI / 2.0);
+      //      PlanarRegion region2 = new PlanarRegion(transform2, polygonsRegion2);
    }
 
 }
