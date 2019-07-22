adb52ef231fe084eaeb7b04b42ff4af54b84f3ab
JerryPratt <jpratt@ihmc.us>
2019-07-22 16:53:30 -0500

    Cleanup.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAM.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAM.java
@@ -1,6 +1,5 @@
 package us.ihmc.humanoidBehaviors.tools.perception;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +10,6 @@ import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple3D.Vector3D;
-import us.ihmc.log.LogTools;
 import us.ihmc.pathPlanning.visibilityGraphs.tools.ConcaveHullMerger;
 import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
@@ -19,8 +17,6 @@ import us.ihmc.tools.lists.PairList;
 
 public class PlanarRegionSLAM
 {
-   private static boolean verbose = false;
-
    public static PlanarRegionSLAMResult slam(PlanarRegionsList map, PlanarRegionsList newDataIn)
    {
       return slam(map, newDataIn, new PlanarRegionSLAMParameters());
@@ -45,43 +41,23 @@ public class PlanarRegionSLAM
                                                                                                                                            transformedNewData,
                                                                                                                                            parameters);
 
-         if (verbose)
-         {
-            LogTools.info("matchesWithReferencePoints.keys: {}", matchesWithReferencePoints.size());
-            LogTools.info("matchesWithReferencePoints.values: {}", matchesWithReferencePoints.values().size());
-         }
-
          RigidBodyTransform driftCorrectionTransform = PlanarRegionSLAMTools.findDriftCorrectionTransform(matchesWithReferencePoints, parameters);
-         //TODO: Multiply or post multiply???
          totalDriftCorrectionTransform.preMultiply(driftCorrectionTransform);
 
          transformedNewData = transformedNewData.copy();
          transformedNewData.transformByPreMultiply(driftCorrectionTransform);
-
-         if (verbose)
-         {
-            LogTools.info(driftCorrectionTransform.toString());
-         }
       }
 
-//      PlanarRegionsList mergedMap = generateMergedMapByJustAddingAllPlanarRegions(map, transformedNewData);
       PlanarRegionsList mergedMap = generateMergedMapByMergingAllPlanarRegionsMatches(map, transformedNewData, parameters);
       PlanarRegionSLAMResult result = new PlanarRegionSLAMResult(totalDriftCorrectionTransform, mergedMap);
       return result;
    }
 
-   private static PlanarRegionsList generateMergedMapByJustAddingAllPlanarRegions(PlanarRegionsList map, PlanarRegionsList transformedNewData)
-   {
-      PlanarRegionsList mergedMap = new PlanarRegionsList();
-      map.getPlanarRegionsAsList().forEach(region -> mergedMap.addPlanarRegion(region));
-      transformedNewData.getPlanarRegionsAsList().forEach(region -> mergedMap.addPlanarRegion(region));
-      return mergedMap;
-   }
-
    private static PlanarRegionsList generateMergedMapByMergingAllPlanarRegionsMatches(PlanarRegionsList map, PlanarRegionsList transformedNewData,
                                                                                       PlanarRegionSLAMParameters parameters)
    {
-      Map<PlanarRegion, PairList<PlanarRegion, Point2D>> matchesWithReferencePoints = findHighConfidenceRegionMatchesAndReferencePoints(map, transformedNewData,
+      Map<PlanarRegion, PairList<PlanarRegion, Point2D>> matchesWithReferencePoints = findHighConfidenceRegionMatchesAndReferencePoints(map,
+                                                                                                                                        transformedNewData,
                                                                                                                                         parameters);
 
       PlanarRegionsList mergedMap = new PlanarRegionsList();
@@ -102,19 +78,9 @@ public class PlanarRegionSLAM
                {
                   continue;
                }
-
                newRegionsConsidered.add(newRegion);
 
-               //          PlanarRegion copy = newRegion.copy();
-               //          copy.transformByPreMultiply(totalDriftCorrectionTransform);
-
-               LogTools.info("Merging.");
-               LogTools.info("mapPlanarRegion = \n" + mapPlanarRegion);
-               LogTools.info("newRegion = \n" + newRegion);
-
                mapPlanarRegion = ConcaveHullMerger.mergePlanarRegions(mapPlanarRegion, newRegion);
-               LogTools.info("DoneMerging!");
-
             }
          }
 
@@ -134,32 +100,24 @@ public class PlanarRegionSLAM
    }
 
    /**
-    * Looks through two PlanarRegionsLists and finds PlanarRegion pairs that are good potential matches.
-    * @param map The map that you are building.
+    * Looks through two PlanarRegionsLists and finds PlanarRegion pairs that are good potential
+    * matches.
+    * 
+    * @param map     The map that you are building.
     * @param newData The newData that you are adding to the map.
-    * @return A Map from PlanarRegions in the map to matching regions in the new data, and matching reference points in the new Data.
+    * @return A Map from PlanarRegions in the map to matching regions in the new data, and matching
+    *         reference points in the new Data.
     */
    public static Map<PlanarRegion, PairList<PlanarRegion, Point2D>> findHighConfidenceRegionMatchesAndReferencePoints(PlanarRegionsList map,
                                                                                                                       PlanarRegionsList newData,
                                                                                                                       PlanarRegionSLAMParameters parameters)
    {
-      Map<PlanarRegion, List<PlanarRegion>> boundingBox3DCollisions = PlanarRegionSLAMTools.detectLocalBoundingBox3DCollisions(map, newData,
+      Map<PlanarRegion, List<PlanarRegion>> boundingBox3DCollisions = PlanarRegionSLAMTools.detectLocalBoundingBox3DCollisions(map,
+                                                                                                                               newData,
                                                                                                                                parameters.getBoundingBoxHeight());
-
-      if (verbose)
-      {
-         LogTools.info("boundboxCollisions.keys: {}", boundingBox3DCollisions.size());
-         LogTools.info("boundboxCollisions.values: {}", boundingBox3DCollisions.values().size());
-      }
 
       Map<PlanarRegion, List<PlanarRegion>> normalSimilarityFiltered = PlanarRegionSLAMTools.filterMatchesBasedOnNormalSimilarity(boundingBox3DCollisions,
                                                                                                                                   parameters.getMinimumNormalDotProduct());
-
-      if (verbose)
-      {
-         LogTools.info("normalSimilarityFiltered.keys: {}", normalSimilarityFiltered.size());
-         LogTools.info("normalSimilarityFiltered.values: {}", normalSimilarityFiltered.values().size());
-      }
 
       Map<PlanarRegion, PairList<PlanarRegion, Point2D>> matchesWithReferencePoints = PlanarRegionSLAMTools.filterMatchesBasedOn2DBoundingBoxShadow(normalSimilarityFiltered);
       return matchesWithReferencePoints;
@@ -177,61 +135,4 @@ public class PlanarRegionSLAM
       return result;
    }
 
-   private static void mergeNewDataIntoMap(PlanarRegionsList map, PlanarRegionsList newData)
-   {
-      List<PlanarRegion> allRegionsInWorld = new ArrayList<>(); // worry about merging ids?
-      allRegionsInWorld.addAll(map.getPlanarRegionsAsList());
-      allRegionsInWorld.addAll(newData.getPlanarRegionsAsList());
-
-      // match regions by normality and coplanarity (this time including partially visible surfaces)
-      List<List<PlanarRegion>> regionsToMerge = matchRegionsToMerge(allRegionsInWorld);
-
-      List<PlanarRegion> mergedRegions = mergeRegions(regionsToMerge);
-
-      map.clear();
-      for (PlanarRegion mergedRegion : mergedRegions)
-      {
-         map.addPlanarRegion(mergedRegion); // set ids?
-      }
-   }
-
-   public static RigidBodyTransform snapToObviousGroundPlane(PlanarRegionsList map, PlanarRegionsList newData)
-   {
-      // if there exist two very large ground planes that intersect very near to the robot, then snap drifted to map's
-      // ground plane by rotating on ground planes' intersection axis
-
-      RigidBodyTransform drift = new RigidBodyTransform();
-      return drift;
-   }
-
-   public static RigidBodyTransform findDriftCorrectionTransform(PairList<PlanarRegion, PlanarRegion> highConfidencePairs)
-   {
-      RigidBodyTransform transform = new RigidBodyTransform();
-
-      // find average transform of all pairs
-
-      return transform;
-   }
-
-   public static List<List<PlanarRegion>> matchRegionsToMerge(List<PlanarRegion> regions)
-   {
-      ArrayList<List<PlanarRegion>> mappedRegions = new ArrayList<>();
-
-      // this is probably also n squared for now?
-      // need some data structure to sort them into a map
-
-      return mappedRegions;
-   }
-
-   private static List<PlanarRegion> mergeRegions(List<List<PlanarRegion>> regionsToMerge)
-   {
-      ArrayList<PlanarRegion> mergedRegions = new ArrayList<>();
-
-      for (List<PlanarRegion> touchingRegionsOnSamePlane : regionsToMerge)
-      {
-         // PlanarRegionTools.merge(region1, region2)
-      }
-
-      return mergedRegions;
-   }
 }
