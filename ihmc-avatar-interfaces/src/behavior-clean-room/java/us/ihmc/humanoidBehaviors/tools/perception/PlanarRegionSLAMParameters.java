02e4c98f580befecb8c83aee7b19499ac54a687e
Duncan Calvert <dcalvert@ihmc.us>
2019-07-23 14:24:43 -0500

    Add documentation to PlanarRegionSLAMParameters.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAMParameters.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PlanarRegionSLAMParameters.java
@@ -5,6 +5,21 @@ import us.ihmc.tools.property.IntegerStoredPropertyKey;
 import us.ihmc.tools.property.StoredPropertyKeyList;
 import us.ihmc.tools.property.StoredPropertySet;
 
+/**
+ * Parameters for the planar region SLAM algorithm.
+ *
+ * On construction, these properties are loaded from the INI file:
+ * ihmc-avatar-interfaces/src/behavior-clean-room/resources/us/ihmc/humanoidBehaviors/tools/perception/planarRegionSLAMParameters.ini
+ *
+ * To save/load properties, call the save() and load() methods.
+ *
+ * After adding new properties, call the main() method to add them to the INI file with null values. Then,
+ * set the initial value in the file.
+ *
+ * The named getters and setters are more of a "sugar" feature for this class and may go out of date as parameters are added.
+ *
+ * The set(Key, value) and get(Key) methods are always available.
+ */
 public class PlanarRegionSLAMParameters extends StoredPropertySet
 {
    public static final StoredPropertyKeyList keys = new StoredPropertyKeyList(PlanarRegionSLAMParameters.class);
