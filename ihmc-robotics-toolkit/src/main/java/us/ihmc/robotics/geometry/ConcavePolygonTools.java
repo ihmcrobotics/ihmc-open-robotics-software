f65fc394fdc08cf5d5f64ac7d0f8ae74f1f74db2
Duncan Calvert <dcalvert@ihmc.us>
2019-08-01 10:11:12 -0500

    Change concave cropper return type and fix compile error.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/geometry/ConcavePolygonTools.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/geometry/ConcavePolygonTools.java
@@ -1,30 +1,38 @@
 package us.ihmc.robotics.geometry;
 
-import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DBasics;
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DReadOnly;
 import us.ihmc.euclid.geometry.interfaces.Line2DReadOnly;
 import us.ihmc.euclid.tuple2D.interfaces.Vector2DReadOnly;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConcavePolygonTools
 {
 
-   public static ConcavePolygonCropResult cropPolygonToAboveLine(ConvexPolygon2DReadOnly polygonToCrop,
-                                                                 Line2DReadOnly cuttingLine,
-                                                                 Vector2DReadOnly upDirection,
-                                                                 ConvexPolygon2DBasics croppedPolygonToPack)
+   public static List<ConvexPolygon2D> cropPolygonToAboveLine(ConvexPolygon2DReadOnly polygonToCrop,
+                                                              Line2DReadOnly cuttingLine,
+                                                              Vector2DReadOnly upDirection)
    {
-      if (croppedPolygonToPack.getNumberOfVertices() < 5)
+      ArrayList<ConvexPolygon2D> croppedPolygons = new ArrayList<>();
+      if (polygonToCrop.getNumberOfVertices() < 5)
       {
          // must be convex, revert to convex polygon crop
+         ConvexPolygon2D croppedPolygonToPack = new ConvexPolygon2D();
          ConvexPolygonCropResult result = ConvexPolygonTools.cropPolygonToAboveLine(polygonToCrop,
                                                                                     cuttingLine,
                                                                                     upDirection,
                                                                                     croppedPolygonToPack);
-         return ConcavePolygonCropResult.fromConvexPolygonCropResult(result);
+         if (result != ConvexPolygonCropResult.REMOVE_ALL)
+         {
+            croppedPolygons.add(croppedPolygonToPack);
+         }
+         return croppedPolygons;
       }
 
 
 
-      return ConcavePolygonCropResult.CUT;
+      return croppedPolygons;
    }
 }
