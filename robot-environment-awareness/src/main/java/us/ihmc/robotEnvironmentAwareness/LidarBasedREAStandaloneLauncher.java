a4e89f673f85a220d5f3f62402d360f61a96f78e
inhol <ilee@ihmc.us>
2019-08-08 14:30:08 -0500

    Reverted.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/LidarBasedREAStandaloneLauncher.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/LidarBasedREAStandaloneLauncher.java
@@ -5,34 +5,29 @@ import javafx.application.Platform;
 import javafx.stage.Stage;
 import us.ihmc.robotEnvironmentAwareness.ui.LIDARBasedEnvironmentAwarenessUI;
 import us.ihmc.robotEnvironmentAwareness.updaters.LIDARBasedREAModule;
-import us.ihmc.robotEnvironmentAwareness.updaters.StereoBasedREAModule;
 
 public class LidarBasedREAStandaloneLauncher extends Application
 {
    private static final String MODULE_CONFIGURATION_FILE_NAME = "./Configurations/defaultREAModuleConfiguration.txt";
 
    private LIDARBasedEnvironmentAwarenessUI ui;
-   private LIDARBasedREAModule lidarBasedModule;
-   private StereoBasedREAModule stereoBasedModule;
+   private LIDARBasedREAModule module;
 
    @Override
    public void start(Stage primaryStage) throws Exception
    {
       ui = LIDARBasedEnvironmentAwarenessUI.creatIntraprocessUI(primaryStage);
-      lidarBasedModule = LIDARBasedREAModule.createIntraprocessModule(MODULE_CONFIGURATION_FILE_NAME);
-      stereoBasedModule = StereoBasedREAModule.createIntraprocessModule(MODULE_CONFIGURATION_FILE_NAME);
+      module = LIDARBasedREAModule.createIntraprocessModule(MODULE_CONFIGURATION_FILE_NAME);
 
       ui.show();
-      lidarBasedModule.start();
-      stereoBasedModule.start();
+      module.start();
    }
 
    @Override
    public void stop() throws Exception
    {
       ui.stop();
-      lidarBasedModule.stop();
-      stereoBasedModule.stop();
+      module.stop();
 
       Platform.exit();
    }
