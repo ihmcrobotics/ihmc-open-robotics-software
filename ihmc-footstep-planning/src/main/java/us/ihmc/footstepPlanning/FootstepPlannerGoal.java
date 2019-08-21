efbc356c4e4cb9a732e69698f17532a101b603c2
smccrory <smccrory@ihmc.us>
2019-08-21 14:40:27 -0500

    Removing unused fields in FootstepPlannerGoal

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/FootstepPlannerGoal.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/FootstepPlannerGoal.java
@@ -1,45 +1,41 @@
 package us.ihmc.footstepPlanning;
 
-import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.humanoidRobotics.footstep.SimpleFootstep;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
-
-
 public class FootstepPlannerGoal
 {
    private FramePose3D goalPoseBetweenFeet;
-   private FramePoint3D goalPositionBetweenFeet;
-   private SimpleFootstep singleFootstepGoal;
    private SideDependentList<SimpleFootstep> doubleFootstepGoal;
-   private Point2D xyGoal;
-   private double distanceFromXYGoal;
+
+   private double xyProximity;
+   private double yawProximity;
 
    private FootstepPlannerGoalType footstepPlannerGoalType;
 
    public void set(FootstepPlannerGoal other)
    {
       if (other.goalPoseBetweenFeet != null)
+      {
          goalPoseBetweenFeet = new FramePose3D(other.goalPoseBetweenFeet);
-      if (other.goalPositionBetweenFeet != null)
-         goalPositionBetweenFeet = new FramePoint3D(other.goalPositionBetweenFeet);
-      if (other.singleFootstepGoal != null)
-         singleFootstepGoal = new SimpleFootstep(singleFootstepGoal);
+      }
+
       if (other.doubleFootstepGoal != null)
       {
          doubleFootstepGoal = new SideDependentList<>();
          for (RobotSide robotSide : RobotSide.values)
             doubleFootstepGoal.put(robotSide, other.doubleFootstepGoal.get(robotSide));
       }
-      if (other.xyGoal != null)
-         xyGoal = new Point2D(other.xyGoal);
-      if (Double.isFinite(other.distanceFromXYGoal))
-         distanceFromXYGoal = other.distanceFromXYGoal;
+
+      this.xyProximity = other.xyProximity;
+      this.yawProximity = other.yawProximity;
+
       if (other.footstepPlannerGoalType != null)
+      {
          footstepPlannerGoalType = other.footstepPlannerGoalType;
+      }
    }
 
    public FramePose3D getGoalPoseBetweenFeet()
@@ -52,26 +48,6 @@ public class FootstepPlannerGoal
       this.goalPoseBetweenFeet = goalPoseBetweenFeet;
    }
 
-   public FramePoint3D getGoalPositionBetweenFeet()
-   {
-      return goalPositionBetweenFeet;
-   }
-
-   public void setGoalPositionBetweenFeet(FramePoint3D goalPositionBetweenFeet)
-   {
-      this.goalPositionBetweenFeet = goalPositionBetweenFeet;
-   }
-
-   public SimpleFootstep getSingleFootstepGoal()
-   {
-      return singleFootstepGoal;
-   }
-
-   public void setSingleFootstepGoal(SimpleFootstep singleFootstepGoal)
-   {
-      this.singleFootstepGoal = singleFootstepGoal;
-   }
-
    public SideDependentList<SimpleFootstep> getDoubleFootstepGoal()
    {
       return doubleFootstepGoal;
@@ -82,30 +58,33 @@ public class FootstepPlannerGoal
       this.doubleFootstepGoal = doubleFootstepGoal;
    }
 
-   public void setXYGoal(Point2D xyGoal, double distanceFromXYGoal)
-   {
-      this.xyGoal = new Point2D(xyGoal);
-      this.distanceFromXYGoal = distanceFromXYGoal;
-   }
-
-   public Point2D getXYGoal()
-   {
-      return xyGoal;
-   }
-
-   public double getDistanceFromXYGoal()
-   {
-      return distanceFromXYGoal;
-   }
-
    public FootstepPlannerGoalType getFootstepPlannerGoalType()
    {
       return footstepPlannerGoalType;
+   }
+
+   public double getXyProximity()
+   {
+      return xyProximity;
+   }
+
+   public double getYawProximity()
+   {
+      return yawProximity;
+   }
+
+   public void setXyProximity(double xyProximity)
+   {
+      this.xyProximity = xyProximity;
+   }
+
+   public void setYawProximity(double yawProximity)
+   {
+      this.yawProximity = yawProximity;
    }
 
    public void setFootstepPlannerGoalType(FootstepPlannerGoalType footstepPlannerGoalType)
    {
       this.footstepPlannerGoalType = footstepPlannerGoalType;
    }
-
 }
