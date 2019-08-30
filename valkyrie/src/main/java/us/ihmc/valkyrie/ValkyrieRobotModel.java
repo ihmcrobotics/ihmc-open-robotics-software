7a8dc50555f60af35c654a9dcb95128b6c62821a
Valkyrie <valkyrie@nasa.gov>
2019-08-30 14:23:51 -0500

    Saved the ROS clock calculator as a field.
    So the ROS module and the sensor suite share the same instance.

--- valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieRobotModel.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieRobotModel.java
@@ -17,6 +17,8 @@ import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.drcRobot.shapeContactSettings.DRCRobotModelShapeCollisionSettings;
 import us.ihmc.avatar.factory.SimulatedHandControlTask;
 import us.ihmc.avatar.initialSetup.DRCRobotInitialSetup;
+import us.ihmc.avatar.ros.RobotROSClockCalculator;
+import us.ihmc.avatar.ros.WallTimeBasedROSClockCalculator;
 import us.ihmc.avatar.sensors.DRCSensorSuiteManager;
 import us.ihmc.commonWalkingControlModules.configurations.HighLevelControllerParameters;
 import us.ihmc.commonWalkingControlModules.configurations.ICPWithTimeFreezingPlannerParameters;
@@ -84,6 +86,7 @@ public class ValkyrieRobotModel implements DRCRobotModel, SDFDescriptionMutator
    private final WalkingControllerParameters walkingControllerParameters;
    private final StateEstimatorParameters stateEstimatorParamaters;
    private final ValkyrieSensorInformation sensorInformation;
+   private final WallTimeBasedROSClockCalculator rosClockCalculator = new WallTimeBasedROSClockCalculator();
    private final ValkyrieJointMap jointMap;
    private final ValkyrieContactPointParameters contactPointParameters;
    private final ValkyrieCalibrationParameters calibrationParameters;
@@ -417,6 +420,12 @@ public class ValkyrieRobotModel implements DRCRobotModel, SDFDescriptionMutator
    public GeneralizedSDFRobotModel getGeneralizedRobotModel()
    {
       return loader.getGeneralizedSDFRobotModel(getJointMap().getModelName());
+   }
+
+   @Override
+   public RobotROSClockCalculator getROSClockCalculator()
+   {
+      return rosClockCalculator;
    }
 
    @Override
