1a97c487d7b5c98becdb7edb71e9722518d41501
Sylvain Bertrand <sbertrand@ihmc.us>
2019-09-04 15:44:12 -0500

    Extended duration before going back to sleep.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinemtaticsStreamingToolboxModule/KinematicsStreamingToolboxModule.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinemtaticsStreamingToolboxModule/KinematicsStreamingToolboxModule.java
@@ -41,6 +41,7 @@ public class KinematicsStreamingToolboxModule extends ToolboxModule
       super(robotModel.getSimpleRobotName(), robotModel.createFullRobotModel(), robotModel.getLogModelProvider(), startYoVariableServer,
             DEFAULT_UPDATE_PERIOD_MILLISECONDS, pubSubImplementation);
 
+      setTimeWithoutInputsBeforeGoingToSleep(30.0);
       controller = new KinematicsStreamingToolboxController(commandInputManager,
                                                             statusOutputManager,
                                                             fullRobotModel,
