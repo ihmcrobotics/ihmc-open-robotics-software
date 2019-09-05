8837e166f4449a7f4921b5ef21ac589a2a1ef1cb
smccrory <smccrory@ihmc.us>
2019-09-05 15:29:58 -0500

    Adding timing test

--- ihmc-quadruped-footstep-planning/src/test/java/us/ihmc/quadrupedFootstepPlanning/pawPlanning/graphSearch/pawSnapping/PlanarRegionPawConstraintDataTest.java
+++ ihmc-quadruped-footstep-planning/src/test/java/us/ihmc/quadrupedFootstepPlanning/pawPlanning/graphSearch/pawSnapping/PlanarRegionPawConstraintDataTest.java
@@ -1,11 +1,15 @@
 package us.ihmc.quadrupedFootstepPlanning.pawPlanning.graphSearch.pawSnapping;
 
 import org.junit.jupiter.api.Test;
+import us.ihmc.commons.Conversions;
 import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DReadOnly;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.robotics.geometry.ConvexPolygonScaler;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.geometry.PlanarRegionsListGenerator;
+
+import java.util.Random;
 
 import static us.ihmc.robotics.Assert.assertTrue;
 
@@ -43,9 +47,53 @@ public class PlanarRegionPawConstraintDataTest
       assertTrue(projectionPoint.epsilonEquals(new Point2D(0.0, 0.0), epsilon));
    }
 
-   @Test
-   public void testTiming()
+   private static void testTiming()
    {
-      
+      Random random = new Random(3290);
+
+      int numberOfRegions = 1000;
+      double[] successfulProjectionDistance = new double[numberOfRegions];
+      double[] unsuccessfulProjectionDistances = new double[numberOfRegions];
+
+      PlanarRegionsListGenerator generator = new PlanarRegionsListGenerator();
+
+      for (int i = 0; i < numberOfRegions; i++)
+      {
+         generator.translateThenRotate(EuclidCoreRandomTools.nextRigidBodyTransform(random));
+
+         double sideX = EuclidCoreRandomTools.nextDouble(random, 0.1, 2.0);
+         double sideY = EuclidCoreRandomTools.nextDouble(random, 0.1, 2.0);
+         generator.addRectangle(sideX, sideY);
+
+         successfulProjectionDistance[i] = EuclidCoreRandomTools.nextDouble(random, 1e-2, 0.5 * Math.min(sideX, sideY) - 1e-2);
+         unsuccessfulProjectionDistances[i] = EuclidCoreRandomTools.nextDouble(random, 0.5 * Math.min(sideX, sideY) + 1e-2, Math.max(sideX, sideY) - 1e-2);
+      }
+
+      PlanarRegionsList planarRegionsList = generator.getPlanarRegionsList();
+      ConvexPolygonScaler scaler = new ConvexPolygonScaler();
+
+      long start, stop;
+
+      start = System.nanoTime();
+      for (int i = 0; i < numberOfRegions; i++)
+      {
+         new PlanarRegionPawConstraintData(scaler, planarRegionsList.getPlanarRegion(i), true, successfulProjectionDistance[i]);
+      }
+      stop = System.nanoTime();
+      System.out.println("Average time taken not using fallback projection: " + (Conversions.nanosecondsToSeconds(stop - start) / numberOfRegions) + " sec");
+
+      start = System.nanoTime();
+      for (int i = 0; i < numberOfRegions; i++)
+      {
+         new PlanarRegionPawConstraintData(scaler, planarRegionsList.getPlanarRegion(i), true, unsuccessfulProjectionDistances[i]);
+      }
+      stop = System.nanoTime();
+      System.out.println("Average time taken using fallback projection: " + (Conversions.nanosecondsToSeconds(stop - start) / numberOfRegions) + " sec");
+
+   }
+
+   public static void main(String[] args)
+   {
+      testTiming();
    }
 }
