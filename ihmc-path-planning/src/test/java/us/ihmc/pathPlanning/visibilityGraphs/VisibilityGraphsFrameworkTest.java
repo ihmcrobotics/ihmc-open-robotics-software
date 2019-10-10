f7c687e9917c2042f1cab7f96a9d32a55089a412
Robert <rgriffin@ihmc.us>
2019-10-09 23:19:17 -0500

    started doing the inter region weight scaling

--- ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/VisibilityGraphsFrameworkTest.java
+++ ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/VisibilityGraphsFrameworkTest.java
@@ -96,6 +96,9 @@ public class VisibilityGraphsFrameworkTest
       parameters.setNormalZThresholdForAccessibleRegions(Math.cos(Math.toRadians(30.0)));
       parameters.setPerformPostProcessingNodeShifting(false);
 //      parameters.setIntroduceMidpointsInPostProcessing(true);
+      parameters.setObstacleExtrusionDistance(0.3);
+      parameters.setPreferredObstacleExtrusionDistance(0.6);
+      parameters.setNavigableExtrusionDistance(0.02);
 
       return parameters;
    }
@@ -828,13 +831,12 @@ public class VisibilityGraphsFrameworkTest
    public static void main(String[] args) throws Exception
    {
       VisibilityGraphsFrameworkTest test = new VisibilityGraphsFrameworkTest();
-//      String dataSetName = "20171218_205120_BodyPathPlannerEnvironment";
+      String dataSetName = "20171218_205120_BodyPathPlannerEnvironment";
 //      String dataSetName = "20171215_211034_DoorwayNoCeiling";
-//      String dataSetName = "20171218_205120_BodyPathPlannerEnvironment";
 //      String dataSetName = "20171215_220523_SteppingStones";
 //      String dataSetName = "20171218_204917_FlatGround";
 //      String dataSetName = "20171215_214730_CinderBlockField";
-      String dataSetName = "20001201_205050_TwoSquaresOneObstacle";
+//      String dataSetName = "20001201_205050_TwoSquaresOneObstacle";
 //      String dataSetName = "20171215_210811_DoorwayWithCeiling";
 
       VISUALIZE = true;
