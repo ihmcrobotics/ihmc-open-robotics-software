ff6bab0285fe09a9a4caca29317ce79ab4df884a
Sylvain Bertrand <sbertrand@ihmc.us>
2019-10-18 14:54:43 -0500

    Separated the controller-api between fast & slow.

--- valkyrie/src/test/java/us/ihmc/valkyrie/controllerAPI/ValkyrieEndToEndNeckDesiredAccelerationsMessageTest.java
+++ valkyrie/src/test/java/us/ihmc/valkyrie/controllerAPI/ValkyrieEndToEndNeckDesiredAccelerationsMessageTest.java
@@ -1,5 +1,6 @@
 package us.ihmc.valkyrie.controllerAPI;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import us.ihmc.avatar.controllerAPI.EndToEndNeckDesiredAccelerationsMessageTest;
@@ -24,6 +25,7 @@ public class ValkyrieEndToEndNeckDesiredAccelerationsMessageTest extends EndToEn
       return BambooTools.getSimpleRobotNameFor(BambooTools.SimpleRobotNameKeys.VALKYRIE);
    }
 
+   @Tag("controller-api-slow-2")
    @Override
    @Test
    public void testSimpleCommands() throws Exception
