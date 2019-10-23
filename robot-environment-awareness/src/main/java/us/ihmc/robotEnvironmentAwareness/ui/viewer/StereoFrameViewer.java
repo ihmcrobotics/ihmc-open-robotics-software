415d79e15186c6c5527da7b1777fbda3fbdd76e8
inhol <ilee@ihmc.us>
2019-10-23 15:26:36 -0500

    Defined abtract sensor viewer.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/viewer/StereoFrameViewer.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/viewer/StereoFrameViewer.java
@@ -1,26 +1,26 @@
 package us.ihmc.robotEnvironmentAwareness.ui.viewer;
 
-import controller_msgs.msg.dds.LidarScanMessage;
+import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.javaFXToolkit.JavaFXTools;
 import us.ihmc.messager.MessagerAPIFactory.Topic;
 import us.ihmc.robotEnvironmentAwareness.communication.REAUIMessager;
 
-public class LidarFrameViewer extends AbstractSensorFrameViewer<LidarScanMessage>
+public class StereoFrameViewer extends AbstractSensorFrameViewer<StereoVisionPointCloudMessage>
 {
-   public LidarFrameViewer(REAUIMessager uiMessager, Topic<LidarScanMessage> messageState)
+   public StereoFrameViewer(REAUIMessager uiMessager, Topic<StereoVisionPointCloudMessage> messageState)
    {
       super(uiMessager, messageState);
    }
 
    @Override
-   public void handleMessage(LidarScanMessage message)
+   public void handleMessage(StereoVisionPointCloudMessage message)
    {
       if (message == null)
          return;
-      Quaternion orientation = message.getLidarOrientation();
-      Point3D position = message.getLidarPosition();
+      Quaternion orientation = message.getSensorOrientation();
+      Point3D position = message.getSensorPosition();
       lastAffine.set(JavaFXTools.createAffineFromQuaternionAndTuple(orientation, position));
    }
 }
