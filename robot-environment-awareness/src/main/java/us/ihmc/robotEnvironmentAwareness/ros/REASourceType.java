e2727461a6d431278b7bad166d361b9449ee0c90
inhol <ilee@ihmc.us>
2019-11-12 14:27:33 -0600

    Added topic listener.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ros/REASourceType.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ros/REASourceType.java
@@ -1,5 +1,8 @@
 package us.ihmc.robotEnvironmentAwareness.ros;
 
+import us.ihmc.messager.MessagerAPIFactory.Topic;
+import us.ihmc.robotEnvironmentAwareness.communication.REAModuleAPI;
+
 public enum REASourceType
 {
    LIDAR_SCAN, STEREO_POINT_CLOUD, DEPTH_POINT_CLOUD;
@@ -14,6 +17,21 @@ public enum REASourceType
          return "/ihmc/stereo_vision_point_cloud";
       case DEPTH_POINT_CLOUD:
          return "/ihmc/stereo_vision_point_cloud_D435";
+      default:
+         throw new RuntimeException("Unexpected " + getClass().getSimpleName() + " value: " + this);
+      }
+   }
+
+   public Topic<Boolean> getEnableTopic()
+   {
+      switch (this)
+      {
+      case LIDAR_SCAN:
+         return REAModuleAPI.LidarBufferEnable;
+      case STEREO_POINT_CLOUD:
+         return REAModuleAPI.StereoVisionBufferEnable;
+      case DEPTH_POINT_CLOUD:
+         return REAModuleAPI.DepthCloudBufferEnable;
       default:
          throw new RuntimeException("Unexpected " + getClass().getSimpleName() + " value: " + this);
       }
