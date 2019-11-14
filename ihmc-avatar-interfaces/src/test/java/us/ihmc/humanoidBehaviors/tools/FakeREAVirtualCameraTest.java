784e6c4b175d0049edd8dcdad0063f5d1f1db40c
Duncan Calvert <dcalvert@ihmc.us>
2019-11-14 12:19:31 -0600

    Progress on tiled visual test cases.

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/humanoidBehaviors/tools/FakeREAVirtualCameraTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/humanoidBehaviors/tools/FakeREAVirtualCameraTest.java
@@ -2,77 +2,75 @@ package us.ihmc.humanoidBehaviors.tools;
 
 import javafx.application.Platform;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.javaFXToolkit.scenes.View3DFactory;
 import us.ihmc.javafx.applicationCreator.JavaFXApplicationCreator;
 import us.ihmc.pathPlanning.PlannerTestEnvironments;
 import us.ihmc.pathPlanning.visibilityGraphs.ui.graphics.PlanarRegionsGraphic;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
 
-public class FakeREAVirtualCameraTest //extends Application // TODO: Maybe extract JFX app
+public class FakeREAVirtualCameraTest
 {
    private static boolean VISUALIZE = Boolean.parseBoolean(System.getProperty("visualize")); // To visualize, pass -Dvisualize=true
+   private static int windowCount = 0; // for tiling
 
-//   @Override
-//   public void start(Stage primaryStage)
-//   {
-//      View3DFactory view3dFactory = new View3DFactory(1200, 800);
-//      view3dFactory.addCameraController(0.05, 2000.0,true);
-//      view3dFactory.addWorldCoordinateSystem(0.3);
-//      view3dFactory.addDefaultLighting();
-//
-//      PlanarRegionsGraphic regionsGraphic = new PlanarRegionsGraphic();
-//      regionsGraphic.generateMeshes(PlannerTestEnvironments.getTrickCorridor());
-//      regionsGraphic.update();
-//
-//      view3dFactory.addNodeToView(regionsGraphic);
-//
-//      primaryStage.setTitle(getClass().getSimpleName());
-//      primaryStage.setMaximized(false);
-//      primaryStage.setScene(view3dFactory.getScene());
-//
-//      primaryStage.show();
-//   }
-//
-//   @Override
-//   public void stop()
-//   {
-//   }
-
-   public void startOnAThread()
+   @BeforeAll
+   static public void beforeAll()
    {
-//      ThreadTools.startAThread(Application::launch, getClass().getSimpleName() + "JavaFXApp");
+      JavaFXApplicationCreator.createAJavaFXApplication();
+   }
+
+   @AfterEach
+   public void afterEach()
+   {
+      if (VISUALIZE) ThreadTools.sleepForever();
+   }
+
+   @Test
+   public void testFakeREA1()
+   {
+      Platform.runLater(() ->
+      {
+         createAndShowPlanarRegionWindow(PlannerTestEnvironments.getTrickCorridorWCutFloor(), windowCount++);
+      });
    }
 
    @Test
    public void testFakeREACutsComplexField()
    {
-      JavaFXApplicationCreator.createAJavaFXApplication();
-
       Platform.runLater(() ->
       {
-         View3DFactory view3dFactory = new View3DFactory(1200, 800);
-         view3dFactory.addCameraController(0.05, 2000.0, true);
-         view3dFactory.addWorldCoordinateSystem(0.3);
-         view3dFactory.addDefaultLighting();
-
-         PlanarRegionsGraphic regionsGraphic = new PlanarRegionsGraphic();
-         regionsGraphic.generateMeshes(PlannerTestEnvironments.getTrickCorridor());
-         regionsGraphic.update();
-
-         view3dFactory.addNodeToView(regionsGraphic);
-
-         Stage primaryStage = new Stage();
-         primaryStage.setTitle(getClass().getSimpleName());
-         primaryStage.setMaximized(false);
-         primaryStage.setScene(view3dFactory.getScene());
-
-         primaryStage.show();
+         createAndShowPlanarRegionWindow(PlannerTestEnvironments.getTrickCorridor(), windowCount++);
       });
+   }
 
-//      startOnAThread();
+   private void createAndShowPlanarRegionWindow(PlanarRegionsList trickCorridorWCutFloor, int windowCount)
+   {
+      int windowWidth = 800;
+      int windowHeight = 600;
+      View3DFactory view3dFactory = new View3DFactory(windowWidth, windowHeight);
+      view3dFactory.addCameraController(0.05, 2000.0, true);
+      view3dFactory.addWorldCoordinateSystem(0.3);
+      view3dFactory.addDefaultLighting();
 
-      ThreadTools.sleepForever();
-      if (VISUALIZE) ThreadTools.sleepForever();
+      PlanarRegionsGraphic regionsGraphic = new PlanarRegionsGraphic();
+      regionsGraphic.generateMeshes(trickCorridorWCutFloor);
+      regionsGraphic.update();
+
+      view3dFactory.addNodeToView(regionsGraphic);
+
+      Stage primaryStage = new Stage();
+      primaryStage.setTitle(getClass().getSimpleName());
+      primaryStage.setMaximized(false);
+      primaryStage.setScene(view3dFactory.getScene());
+
+      int numberOfColumns = 3;
+      primaryStage.setX((windowCount % numberOfColumns) * windowWidth);
+      primaryStage.setY((windowCount / numberOfColumns) * windowHeight);
+      primaryStage.show();
+      primaryStage.toFront();
    }
 }
