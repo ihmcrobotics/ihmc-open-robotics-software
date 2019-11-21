6b0d017a0a2ec0b61156d4709f17864015257c5e
inhol <ilee@ihmc.us>
2019-11-21 15:54:57 -0600

    Added handler for the single frame.

--- ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/mapping/StereoVisionPointCloudGraphic.java
+++ ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/mapping/StereoVisionPointCloudGraphic.java
@@ -20,9 +20,9 @@ public class StereoVisionPointCloudGraphic extends Group
    private static final float ORIGIN_POINT_SIZE = 0.05f;
    private static final float SCAN_POINT_SIZE = 0.0075f;
    private static final int NUMBER_OF_POINTS_PER_MESSAGE = 1000;
-   
+
    private static final int palleteSizeForMeshBuilder = 2048;
-   
+
    private volatile List<Node> messageNodes;
    private List<Node> lastNodes = null;
    private volatile List<Node> updatePointCloudMeshViews;
@@ -34,38 +34,87 @@ public class StereoVisionPointCloudGraphic extends Group
       meshBuilder = new JavaFXMultiColorMeshBuilder(new TextureColorAdaptivePalette(palleteSizeForMeshBuilder));
    }
 
+   private void addMesh(StereoVisionPointCloudMessage stereoVisionPointCloudMessage, JavaFXMultiColorMeshBuilder meshBuilder)
+   {
+      Point3D32 sensorPosition = new Point3D32();
+      Point3D32 scanPoint = new Point3D32();
+      sensorPosition.set(stereoVisionPointCloudMessage.getSensorPosition());
+      int redScaler = (int) (0xFF * (1 - (stereoVisionPointCloudMessage.getSensorPoseConfidence())));
+      int greenScaler = (int) (0xFF * (stereoVisionPointCloudMessage.getSensorPoseConfidence()));
+      Color confidenceColor = Color.rgb(redScaler, greenScaler, 0);
+      meshBuilder.addMesh(MeshDataGenerator.Tetrahedron(ORIGIN_POINT_SIZE), sensorPosition, confidenceColor);
+
+      int numberOfScanPoints = stereoVisionPointCloudMessage.getPointCloud().size() / 3;
+      int sizeOfPointCloudToVisualize = Math.min(numberOfScanPoints, NUMBER_OF_POINTS_PER_MESSAGE);
+      Random random = new Random();
+      for (int j = 0; j < sizeOfPointCloudToVisualize; j++)
+      {
+         int indexToVisualize;
+         if (numberOfScanPoints < NUMBER_OF_POINTS_PER_MESSAGE)
+            indexToVisualize = j;
+         else
+            indexToVisualize = random.nextInt(numberOfScanPoints);
+
+         Color color = Color.BLACK;
+         MessageTools.unpackScanPoint(stereoVisionPointCloudMessage, indexToVisualize, scanPoint);
+         meshBuilder.addMesh(MeshDataGenerator.Tetrahedron(SCAN_POINT_SIZE), scanPoint, color);
+      }
+
+   }
+
+   public void generateMeshes(StereoVisionPointCloudMessage stereoVisionPointCloudMessage)
+   {
+      updatePointCloudMeshViews = new ArrayList<>();
+
+      meshBuilder.clear();
+
+      addMesh(stereoVisionPointCloudMessage, meshBuilder);
+
+      MeshView scanMeshView = new MeshView(meshBuilder.generateMesh());
+      scanMeshView.setMaterial(meshBuilder.generateMaterial());
+
+      updatePointCloudMeshViews.add(scanMeshView);
+
+      meshBuilder.clear();
+      messageNodes = updatePointCloudMeshViews;
+   }
+
    public void generateMeshes(List<StereoVisionPointCloudMessage> stereoVisionPointCloudMessages)
    {
       updatePointCloudMeshViews = new ArrayList<>();
 
       meshBuilder.clear();
-      Point3D32 sensorPosition = new Point3D32();
-      Point3D32 scanPoint = new Point3D32();
       for (int i = 0; i < stereoVisionPointCloudMessages.size(); i++)
       {
-         StereoVisionPointCloudMessage message = stereoVisionPointCloudMessages.get(i);
-         sensorPosition.set(message.getSensorPosition());
-         int redScaler = (int) (0xFF * (1 - (message.getSensorPoseConfidence())));
-         int greenScaler = (int) (0xFF * (message.getSensorPoseConfidence()));
-         Color confidenceColor = Color.rgb(redScaler, greenScaler, 0);
-         meshBuilder.addMesh(MeshDataGenerator.Tetrahedron(ORIGIN_POINT_SIZE), sensorPosition, confidenceColor);
-         
-         int numberOfScanPoints = message.getPointCloud().size() / 3;
-         int sizeOfPointCloudToVisualize = Math.min(numberOfScanPoints, NUMBER_OF_POINTS_PER_MESSAGE);
-         Random random = new Random();
-         for (int j = 0; j < sizeOfPointCloudToVisualize; j++)
-         {
-            int indexToVisualize;
-            if (numberOfScanPoints < NUMBER_OF_POINTS_PER_MESSAGE)
-               indexToVisualize = j;
-            else
-               indexToVisualize = random.nextInt(numberOfScanPoints);
-
-            Color color = Color.BLACK;
-            MessageTools.unpackScanPoint(message, indexToVisualize, scanPoint);
-            meshBuilder.addMesh(MeshDataGenerator.Tetrahedron(SCAN_POINT_SIZE), scanPoint, color);
-         }
+         addMesh(stereoVisionPointCloudMessages.get(i), meshBuilder);
       }
+      //      Point3D32 sensorPosition = new Point3D32();
+      //      Point3D32 scanPoint = new Point3D32();
+      //      for (int i = 0; i < stereoVisionPointCloudMessages.size(); i++)
+      //      {
+      //         StereoVisionPointCloudMessage message = stereoVisionPointCloudMessages.get(i);
+      //         sensorPosition.set(message.getSensorPosition());
+      //         int redScaler = (int) (0xFF * (1 - (message.getSensorPoseConfidence())));
+      //         int greenScaler = (int) (0xFF * (message.getSensorPoseConfidence()));
+      //         Color confidenceColor = Color.rgb(redScaler, greenScaler, 0);
+      //         meshBuilder.addMesh(MeshDataGenerator.Tetrahedron(ORIGIN_POINT_SIZE), sensorPosition, confidenceColor);
+      //
+      //         int numberOfScanPoints = message.getPointCloud().size() / 3;
+      //         int sizeOfPointCloudToVisualize = Math.min(numberOfScanPoints, NUMBER_OF_POINTS_PER_MESSAGE);
+      //         Random random = new Random();
+      //         for (int j = 0; j < sizeOfPointCloudToVisualize; j++)
+      //         {
+      //            int indexToVisualize;
+      //            if (numberOfScanPoints < NUMBER_OF_POINTS_PER_MESSAGE)
+      //               indexToVisualize = j;
+      //            else
+      //               indexToVisualize = random.nextInt(numberOfScanPoints);
+      //
+      //            Color color = Color.BLACK;
+      //            MessageTools.unpackScanPoint(message, indexToVisualize, scanPoint);
+      //            meshBuilder.addMesh(MeshDataGenerator.Tetrahedron(SCAN_POINT_SIZE), scanPoint, color);
+      //         }
+      //      }
       MeshView scanMeshView = new MeshView(meshBuilder.generateMesh());
       scanMeshView.setMaterial(meshBuilder.generateMaterial());
 
