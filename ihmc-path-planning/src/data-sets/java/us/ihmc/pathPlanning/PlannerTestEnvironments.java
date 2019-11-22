f41438ff385f6cee5f02496c3457def17952d500
Robert <rgriffin@ihmc.us>
2019-11-22 11:23:59 -0600

    fixed adding clusters

--- ihmc-path-planning/src/data-sets/java/us/ihmc/pathPlanning/PlannerTestEnvironments.java
+++ ihmc-path-planning/src/data-sets/java/us/ihmc/pathPlanning/PlannerTestEnvironments.java
@@ -545,7 +545,7 @@ public class PlannerTestEnvironments
       return generator.getPlanarRegionsList();
    }
 
-   private static PlanarRegionsList drawTrickyCorridorWalls(PlanarRegionsListGenerator generator)
+   private static void drawTrickyCorridorWalls(PlanarRegionsListGenerator generator)
    {
       // left wall
       generator.translate(0.0, 1.5, 0.0);
@@ -601,8 +601,6 @@ public class PlannerTestEnvironments
       generator.translate(-10.0, 0.0, -1.0);
 
       generator.translate(3.0, 1.5, 0.0);
-
-      return generator.getPlanarRegionsList();
    }
 
    public static PlanarRegionsList getSimplePlatform()
