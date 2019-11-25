f77101f0302a95d35e55eb07657141cc1216997e
Sylvain Bertrand <sbertrand@ihmc.us>
2019-11-25 09:59:35 -0600

    Minor cleanup to DRCPoseCommunicator.

--- ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/communication/producers/DRCPoseCommunicator.java
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/communication/producers/DRCPoseCommunicator.java
@@ -7,21 +7,15 @@ import controller_msgs.msg.dds.RobotConfigurationData;
 import us.ihmc.communication.IHMCRealtimeROS2Publisher;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.ROS2Tools.MessageTopicNameGenerator;
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.tuple3D.Vector3D32;
-import us.ihmc.euclid.tuple4D.Quaternion32;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.robotModels.FullRobotModel;
 import us.ihmc.robotics.robotController.RawOutputWriter;
-import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.robotics.sensors.ForceSensorDefinition;
 import us.ihmc.robotics.sensors.IMUDefinition;
 import us.ihmc.ros2.RealtimeRos2Node;
 import us.ihmc.sensorProcessing.communication.packets.dataobjects.RobotConfigurationDataFactory;
 import us.ihmc.sensorProcessing.model.RobotMotionStatusHolder;
 import us.ihmc.sensorProcessing.parameters.HumanoidRobotSensorInformation;
-import us.ihmc.sensorProcessing.sensorData.ForceSensorDistalMassCompensator;
 import us.ihmc.sensorProcessing.sensorData.JointConfigurationGatherer;
 import us.ihmc.sensorProcessing.sensorProcessors.SensorRawOutputMapReadOnly;
 import us.ihmc.sensorProcessing.sensorProcessors.SensorTimestampHolder;
@@ -32,22 +26,11 @@ import us.ihmc.yoVariables.registry.YoVariableRegistry;
 // consumer
 public class DRCPoseCommunicator implements RawOutputWriter
 {
-   private final int WORKER_SLEEP_TIME_MILLIS = 1;
-
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
    private final JointConfigurationGatherer jointConfigurationGathererAndProducer;
    private final SensorTimestampHolder sensorTimestampHolder;
    private final SensorRawOutputMapReadOnly sensorRawOutputMapReadOnly;
-   //   private final SideDependentList<String> wristForceSensorNames;
    private final RobotMotionStatusHolder robotMotionStatusFromController;
-
-   // puts the state data into the ring buffer for the output thread
-   private final Vector3D32[] imuLinearAccelerations;
-   private final Vector3D32[] rawImuAngularVelocities;
-   private final Quaternion32[] imuOrientations;
-
-   private final SideDependentList<ReferenceFrame> wristForceSensorFrames = new SideDependentList<ReferenceFrame>();
-   private final SideDependentList<ForceSensorDistalMassCompensator> wristForceSensorDistalMassCompensators = new SideDependentList<ForceSensorDistalMassCompensator>();
 
    private final RobotConfigurationData robotConfigurationData;
 
@@ -63,56 +46,12 @@ public class DRCPoseCommunicator implements RawOutputWriter
       this.sensorRawOutputMapReadOnly = sensorRawOutputMapReadOnly;
       this.robotMotionStatusFromController = robotMotionStatusFromController;
 
-      if (sensorInformation != null)
-      {
-         SideDependentList<String> wristForceSensorNames = sensorInformation.getWristForceSensorNames();
-         if (wristForceSensorNames != null && !wristForceSensorNames.isEmpty())
-         {
-            setupForceSensorMassCompensators(estimatorModel, wristForceSensorNames);
-         }
-      }
-
       IMUDefinition[] imuDefinitions = estimatorModel.getIMUDefinitions();
-
-      int numberOfImuSensors = imuDefinitions.length;
-      imuLinearAccelerations = new Vector3D32[numberOfImuSensors];
-      rawImuAngularVelocities = new Vector3D32[numberOfImuSensors];
-      imuOrientations = new Quaternion32[numberOfImuSensors];
-
-      for (int imuSensorIndex = 0; imuSensorIndex < numberOfImuSensors; imuSensorIndex++)
-      {
-         imuLinearAccelerations[imuSensorIndex] = new Vector3D32();
-         rawImuAngularVelocities[imuSensorIndex] = new Vector3D32();
-         imuOrientations[imuSensorIndex] = new Quaternion32();
-      }
 
       ForceSensorDefinition[] forceSensorDefinitions = jointConfigurationGathererAndProducer.getForceSensorDefinitions();
       OneDoFJointBasics[] joints = jointConfigurationGathererAndProducer.getJoints();
       robotConfigurationData = RobotConfigurationDataFactory.create(joints, forceSensorDefinitions, imuDefinitions);
       robotConfigurationDataPublisher = ROS2Tools.createPublisher(realtimeRos2Node, RobotConfigurationData.class, publisherTopicNameGenerator);
-   }
-
-   private void setupForceSensorMassCompensators(FullRobotModel estimatorModel, SideDependentList<String> wristForceSensorNames)
-   {
-      ForceSensorDefinition[] forceSensorDefinitions = estimatorModel.getForceSensorDefinitions();
-
-      for (int i = 0; i < forceSensorDefinitions.length; i++)
-      {
-         ForceSensorDefinition sensorDef = forceSensorDefinitions[i];
-         String forceSensorName = sensorDef.getSensorName();
-
-         for (RobotSide robotSide : RobotSide.values)
-         {
-            if (forceSensorName.equals(wristForceSensorNames.get(robotSide)))
-            {
-               ForceSensorDistalMassCompensator massComp = new ForceSensorDistalMassCompensator(sensorDef, WORKER_SLEEP_TIME_MILLIS, registry);
-               wristForceSensorDistalMassCompensators.put(robotSide, massComp);
-
-               ReferenceFrame sensorFrame = sensorDef.getSensorFrame();
-               wristForceSensorFrames.put(robotSide, sensorFrame);
-            }
-         }
-      }
    }
 
    @Override
@@ -157,13 +96,9 @@ public class DRCPoseCommunicator implements RawOutputWriter
             IMUSensorReadOnly imuSensor = imuRawOutputs.get(sensorNumber);
             IMUPacket imuPacketToPack = robotConfigurationData.getImuSensorData().add();
 
-            imuLinearAccelerations[sensorNumber].set(imuSensor.getLinearAccelerationMeasurement());
-            imuOrientations[sensorNumber].set(imuSensor.getOrientationMeasurement());
-            rawImuAngularVelocities[sensorNumber].set(imuSensor.getAngularVelocityMeasurement());
-
-            imuPacketToPack.getLinearAcceleration().set(imuLinearAccelerations[sensorNumber]);
-            imuPacketToPack.getOrientation().set(imuOrientations[sensorNumber]);
-            imuPacketToPack.getAngularVelocity().set(rawImuAngularVelocities[sensorNumber]);
+            imuPacketToPack.getOrientation().set(imuSensor.getOrientationMeasurement());
+            imuPacketToPack.getAngularVelocity().set(imuSensor.getAngularVelocityMeasurement());
+            imuPacketToPack.getLinearAcceleration().set(imuSensor.getLinearAccelerationMeasurement());
          }
       }
 
