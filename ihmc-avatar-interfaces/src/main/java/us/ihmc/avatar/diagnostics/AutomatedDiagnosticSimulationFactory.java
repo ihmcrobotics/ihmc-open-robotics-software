cecf083e8b4053e066ec7d1e8832ad54e5aaef6f
Sylvain Bertrand <sbertrand@ihmc.us>
2019-11-25 15:10:12 -0600

    Renamed some variables to highlight processed vs raw sensor output.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/diagnostics/AutomatedDiagnosticSimulationFactory.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/diagnostics/AutomatedDiagnosticSimulationFactory.java
@@ -156,7 +156,7 @@ public class AutomatedDiagnosticSimulationFactory implements RobotController
       sensorReader = sensorReaderFactory.getSensorReader();
 
       FullInverseDynamicsStructure inverseDynamicsStructure = DRCControllerThread.createInverseDynamicsStructure(fullRobotModel);
-      SensorOutputMapReadOnly sensorOutputMapReadOnly = sensorReader.getProcessedSensorOutputMap();
+      SensorOutputMapReadOnly processedSensorOutputMap = sensorReader.getProcessedSensorOutputMap();
       HumanoidRobotSensorInformation sensorInformation = robotModel.getSensorInformation();
       String[] imuSensorsToUseInStateEstimator = sensorInformation.getIMUSensorsToUseInStateEstimator();
       double gravitationalAcceleration = 9.81;
@@ -195,13 +195,13 @@ public class AutomatedDiagnosticSimulationFactory implements RobotController
 
       RobotMotionStatusHolder robotMotionStatusHolder = new RobotMotionStatusHolder();
       robotMotionStatusHolder.setCurrentRobotMotionStatus(RobotMotionStatus.UNKNOWN);
-      stateEstimator = new DRCKinematicsBasedStateEstimator(inverseDynamicsStructure, stateEstimatorParameters, sensorOutputMapReadOnly,
+      stateEstimator = new DRCKinematicsBasedStateEstimator(inverseDynamicsStructure, stateEstimatorParameters, processedSensorOutputMap,
                                                             centerOfMassDataHolderToUpdate, imuSensorsToUseInStateEstimator, gravitationalAcceleration,
                                                             footSwitchMap, null, robotMotionStatusHolder, bipedFeetMap, null);
       simulationRegistry.addChild(stateEstimator.getYoVariableRegistry());
 
       forceSensorStateUpdater = new ForceSensorStateUpdater(fullRobotModel.getRootJoint(),
-                                                            sensorOutputMapReadOnly,
+                                                            processedSensorOutputMap,
                                                             forceSensorDataHolderToUpdate,
                                                             stateEstimatorParameters,
                                                             gravitationalAcceleration,
