66331da72caafcdc874039ef13be0482f71a6342
smccrory <smccrory@ihmc.us>
2019-11-26 16:46:14 -0600

    Making NodeComparator generic

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/heuristics/NodeComparator.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/heuristics/NodeComparator.java
@@ -1,26 +1,27 @@
 package us.ihmc.footstepPlanning.graphSearch.heuristics;
 
 import java.util.Comparator;
+import java.util.function.ToDoubleFunction;
 
 import us.ihmc.footstepPlanning.graphSearch.graph.DirectedGraph;
 import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
 
-public class NodeComparator implements Comparator<FootstepNode>
+public class NodeComparator<N> implements Comparator<N>
 {
-   private final DirectedGraph graph;
-   private final CostToGoHeuristics heuristics;
+   private final DirectedGraph<N> graph;
+   private final ToDoubleFunction<N> heuristics;
 
-   public NodeComparator(DirectedGraph graph, CostToGoHeuristics heuristics)
+   public NodeComparator(DirectedGraph<N> graph, ToDoubleFunction<N> heuristics)
    {
       this.graph = graph;
       this.heuristics = heuristics;
    }
 
    @Override
-   public int compare(FootstepNode o1, FootstepNode o2)
+   public int compare(N o1, N o2)
    {
-      double cost1 = graph.getCostFromStart(o1) + heuristics.compute(o1);
-      double cost2 = graph.getCostFromStart(o2) + heuristics.compute(o2);
+      double cost1 = graph.getCostFromStart(o1) + heuristics.applyAsDouble(o1);
+      double cost2 = graph.getCostFromStart(o2) + heuristics.applyAsDouble(o2);
       if (cost1 == cost2) return 0;
       return cost1 < cost2 ? -1 : 1;
    }
