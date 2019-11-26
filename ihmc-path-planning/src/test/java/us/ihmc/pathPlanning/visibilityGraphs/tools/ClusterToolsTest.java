5087e53250c100302a6e7b36056fd0c9049a859e
Robert <rgriffin@ihmc.us>
2019-11-26 16:16:00 -0600

    copied over the convex polygon shrinker tests to make sure the shrinking is done correctly

--- ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/tools/ClusterToolsTest.java
+++ ihmc-path-planning/src/test/java/us/ihmc/pathPlanning/visibilityGraphs/tools/ClusterToolsTest.java
@@ -4,11 +4,14 @@ import static us.ihmc.robotics.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import us.ihmc.commons.MutationTestFacilitator;
+import us.ihmc.commons.RandomNumbers;
 import us.ihmc.commons.lists.ListWrappingIndexTools;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.Line2D;
@@ -17,6 +20,8 @@ import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DReadOnly;
 import us.ihmc.euclid.geometry.interfaces.Vertex2DSupplier;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryTestTools;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.referenceFrame.FrameConvexPolygon2D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
@@ -33,6 +38,8 @@ import us.ihmc.pathPlanning.visibilityGraphs.interfaces.ObstacleExtrusionDistanc
 import us.ihmc.pathPlanning.visibilityGraphs.interfaces.ObstacleRegionFilter;
 import us.ihmc.robotEnvironmentAwareness.planarRegion.PlanarRegionFilter;
 import us.ihmc.robotEnvironmentAwareness.planarRegion.REAPlanarRegionTools;
+import us.ihmc.robotics.geometry.ConvexPolygon2dCalculator;
+import us.ihmc.robotics.geometry.ConvexPolygon2dTestHelpers;
 import us.ihmc.robotics.geometry.ConvexPolygonScaler;
 import us.ihmc.robotics.geometry.PlanarRegion;
 
@@ -775,6 +782,158 @@ public class ClusterToolsTest
       assertTrue(listContains(navigableExtrusionsInLocal, new Point2D(0.2, -0.4)));
       assertTrue(listContains(navigableExtrusionsInLocal, new Point2D(0.1, -0.4)));
    }
+
+   @Test
+   public void testSimpleSquareConvexPolygonShrinking()
+   {
+      ArrayList<Point2D> vertices = new ArrayList<Point2D>();
+
+      vertices.add(new Point2D(0.0, 0.0));
+      vertices.add(new Point2D(1.0, 0.0));
+      vertices.add(new Point2D(1.0, 1.0));
+      vertices.add(new Point2D(0.0, 1.0));
+
+      ConvexPolygon2D polygon = new ConvexPolygon2D(Vertex2DSupplier.asVertex2DSupplier(vertices));
+      ConvexPolygon2D shrunkenPolygon = new ConvexPolygon2D();
+
+      double[] distances = new double[polygon.getNumberOfVertices()];
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         distances[i] = 0.1;
+      ClusterTools.scaleConvexPolygonInward(polygon, distances, shrunkenPolygon);
+
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.1, 0.1), shrunkenPolygon.getVertexCCW(0), 1e-7);
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.9, 0.1), shrunkenPolygon.getVertexCCW(1), 1e-7);
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.9, 0.9), shrunkenPolygon.getVertexCCW(2), 1e-7);
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.1, 0.9), shrunkenPolygon.getVertexCCW(3), 1e-7);
+
+
+      polygon = new ConvexPolygon2D(Vertex2DSupplier.asVertex2DSupplier(vertices));
+
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         distances[i] = 1.1;
+
+      ClusterTools.scaleConvexPolygonInward(polygon, distances, shrunkenPolygon);
+
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.5, 0.5), shrunkenPolygon.getVertexCCW(0), 1e-7);
+      assertEquals(1, shrunkenPolygon.getNumberOfVertices());
+   }
+
+   @Test
+   public void testSimpleTriangleConvexPolygonShrinking()
+   {
+      ArrayList<Point2D> vertices = new ArrayList<Point2D>();
+
+      vertices.add(new Point2D(0.0, 0.5));
+      vertices.add(new Point2D(1.0, 0.0));
+      vertices.add(new Point2D(2.2, 1.0));
+
+      ConvexPolygon2D polygon = new ConvexPolygon2D(Vertex2DSupplier.asVertex2DSupplier(vertices));
+      ConvexPolygon2D shrunkenPolygon = new ConvexPolygon2D();
+
+      double[] distances = new double[polygon.getNumberOfVertices()];
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         distances[i] = 0.1;
+      ClusterTools.scaleConvexPolygonInward(polygon, distances, shrunkenPolygon);
+
+
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.986224428207409, 0.11869118477128504), shrunkenPolygon.getVertexCCW(0), 1e-7);
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(1.8160104213223893, 0.8101795123671021), shrunkenPolygon.getVertexCCW(1), 1e-7);
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.2947361006115917, 0.4644353485691937), shrunkenPolygon.getVertexCCW(2), 1e-7);
+
+      polygon = new ConvexPolygon2D(Vertex2DSupplier.asVertex2DSupplier(vertices));
+
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         distances[i] = 1.1;
+      ClusterTools.scaleConvexPolygonInward(polygon, distances, shrunkenPolygon);
+
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(1.0666666666, 0.5), shrunkenPolygon.getVertexCCW(0), 1e-7);
+      assertEquals(1, shrunkenPolygon.getNumberOfVertices());
+   }
+
+
+   @Test
+   public void testSimpleLineConvexPolygonShrinking()
+   {
+      ArrayList<Point2D> vertices = new ArrayList<Point2D>();
+
+      vertices.add(new Point2D(-1.0, 3.0));
+      vertices.add(new Point2D(1.0, 3.0));
+
+      ConvexPolygon2D polygon = new ConvexPolygon2D(Vertex2DSupplier.asVertex2DSupplier(vertices));
+      ConvexPolygon2D shrunkenPolygon = new ConvexPolygon2D();
+
+      double[] distances = new double[polygon.getNumberOfVertices()];
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         distances[i] = 0.1;
+      ClusterTools.scaleConvexPolygonInward(polygon, distances, shrunkenPolygon);
+
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.9, 3.0), shrunkenPolygon.getVertexCCW(0), 1e-7);
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(-0.9, 3.0), shrunkenPolygon.getVertexCCW(1), 1e-7);
+
+      polygon = new ConvexPolygon2D(Vertex2DSupplier.asVertex2DSupplier(vertices));
+
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         distances[i] = 1.1;
+      ClusterTools.scaleConvexPolygonInward(polygon, distances, shrunkenPolygon);
+
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(0.0, 3.0), shrunkenPolygon.getVertexCCW(0), 1e-7);
+      assertEquals(1, shrunkenPolygon.getNumberOfVertices());
+   }
+
+   @Test
+   public void testSimplePointConvexPolygonShrinking()
+   {
+      ArrayList<Point2D> vertices = new ArrayList<Point2D>();
+
+      vertices.add(new Point2D(-1.0, 3.0));
+
+      ConvexPolygon2D polygon = new ConvexPolygon2D(Vertex2DSupplier.asVertex2DSupplier(vertices));
+      ConvexPolygon2D shrunkenPolygon = new ConvexPolygon2D();
+
+      double[] distances = new double[polygon.getNumberOfVertices()];
+      for (int i = 0; i < polygon.getNumberOfVertices(); i++)
+         distances[i] = 0.1;
+      ClusterTools.scaleConvexPolygonInward(polygon, distances, shrunkenPolygon);
+
+      EuclidCoreTestTools.assertTuple2DEquals(new Point2D(-1.0, 3.0), shrunkenPolygon.getVertexCCW(0), 1e-7);
+   }
+
+   @Test
+   public void testShrinkingRandomPolygonsAreCompletelyInsideOriginalPolygons()
+   {
+      Random random = new Random(1984L);
+      ReferenceFrame zUpFrame = ReferenceFrame.getWorldFrame();
+
+      double xMin = -2.0;
+      double xMax = 2.0;
+      double yMin = -1.0;
+      double yMax = 4.0;
+      double widthMax = 2.2;
+      double heightMax = 1.3;
+      int numberOfPoints = random.nextInt(20);
+      int numberOfPolygons = 100;
+
+      ArrayList<FrameConvexPolygon2D> randomPolygons = ConvexPolygon2dTestHelpers
+            .generateRandomPolygons(random, zUpFrame, xMin, xMax, yMin, yMax, widthMax, heightMax, numberOfPoints, numberOfPolygons);
+
+      FrameConvexPolygon2D shrunkenPolygon = new FrameConvexPolygon2D();
+
+      for (FrameConvexPolygon2D randomPolygon : randomPolygons)
+      {
+         double distance = RandomNumbers.nextDouble(random, 0.001, 5.0);
+         double[] distances = new double[randomPolygon.getNumberOfVertices()];
+         for (int i = 0; i < randomPolygon.getNumberOfVertices(); i++)
+            distances[i] = distance;
+         ClusterTools.scaleConvexPolygonInward(randomPolygon, distances, shrunkenPolygon);
+
+         ConvexPolygon2DReadOnly bigPolygon = randomPolygon;
+         ConvexPolygon2DReadOnly smallPolygon = shrunkenPolygon;
+
+         boolean completelyInside = ConvexPolygon2dCalculator.isPolygonInside(smallPolygon, bigPolygon);
+         assertTrue(completelyInside);
+      }
+   }
+
 
    @Test
    public void testExtrudePolygonWithLoops()
