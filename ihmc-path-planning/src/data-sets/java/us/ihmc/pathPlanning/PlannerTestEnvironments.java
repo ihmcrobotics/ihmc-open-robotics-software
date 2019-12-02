f742833e6c03bb0a87ad1c2ac7bbc1d426d70fa8
Robert <rgriffin@ihmc.us>
2019-12-02 17:38:38 -0600

    fixed a dumb broken compile

--- ihmc-path-planning/src/data-sets/java/us/ihmc/pathPlanning/PlannerTestEnvironments.java
+++ ihmc-path-planning/src/data-sets/java/us/ihmc/pathPlanning/PlannerTestEnvironments.java
@@ -545,7 +545,7 @@ public class PlannerTestEnvironments
       return generator.getPlanarRegionsList();
    }
 
-   private static PlanarRegionsList drawTrickyCorridorWalls(PlanarRegionsListGenerator generator)
+   private static void drawTrickyCorridorWalls(PlanarRegionsListGenerator generator)
    {
       // left wall
       generator.translate(0.0, 1.5, 0.0);
