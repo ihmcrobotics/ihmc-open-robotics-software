bd66b8bc7a0f32af8754d7dada862e7f8590a0c2
Duncan Calvert <dcalvert@ihmc.us>
2019-12-03 19:13:27 -0600

    Temp

---
+++ ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/goalBasedTestRunner/GoalBasedTestRunner.java
@@ -0,0 +1,8 @@
+package us.ihmc.robotics.goalBasedTestRunner;
+
+/**
+ * Framework for running/pausing a thread while making assertions defined as goals.
+ */
+public class GoalBasedTestRunner
+{
+}
