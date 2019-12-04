21499418d2289a11f034d3b59e2236ec10961af9
Duncan Calvert <dcalvert@ihmc.us>
2019-12-04 10:55:46 -0600

    Start filling out structure of navigation path planning test.

--- atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasCorridorNavigationTest.java
+++ atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasCorridorNavigationTest.java
@@ -2,6 +2,7 @@ package us.ihmc.atlas.behaviors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import controller_msgs.msg.dds.PlanarRegionsListMessage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -10,19 +11,39 @@ import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.footstepPlanning.MultiStageFootstepPlanningModule;
 import us.ihmc.avatar.kinematicsSimulation.HumanoidKinematicsSimulation;
-import us.ihmc.commons.exception.DefaultExceptionHandler;
-import us.ihmc.commons.exception.ExceptionTools;
 import us.ihmc.commons.thread.ThreadTools;
-import us.ihmc.humanoidBehaviors.BehaviorModule;
+import us.ihmc.communication.ROS2Callback;
+import us.ihmc.communication.ROS2Input;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.communication.packets.PlanarRegionMessageConverter;
+import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.humanoidBehaviors.tools.RemoteSyncedHumanoidRobotState;
 import us.ihmc.humanoidBehaviors.tools.SimulatedREAModule;
+import us.ihmc.humanoidRobotics.communication.packets.HumanoidMessageTools;
 import us.ihmc.javafx.applicationCreator.JavaFXApplicationCreator;
 import us.ihmc.log.LogTools;
-import us.ihmc.messager.SharedMemoryMessager;
 import us.ihmc.pathPlanning.PlannerTestEnvironments;
-import us.ihmc.pubsub.DomainFactory;
+import us.ihmc.pathPlanning.visibilityGraphs.NavigableRegionsManager;
+import us.ihmc.pathPlanning.visibilityGraphs.OcclusionHandlingPathPlanner;
+import us.ihmc.pathPlanning.visibilityGraphs.parameters.DefaultVisibilityGraphParameters;
+import us.ihmc.pathPlanning.visibilityGraphs.parameters.VisibilityGraphsParametersBasics;
+import us.ihmc.pathPlanning.visibilityGraphs.postProcessing.ObstacleAvoidanceProcessor;
+import us.ihmc.pathPlanning.visibilityGraphs.postProcessing.PathOrientationCalculator;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
+import us.ihmc.robotics.geometry.PlanarRegionTools;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.ros2.Ros2Node;
 import us.ihmc.wholeBodyController.AdditionalSimulationContactPoints;
 import us.ihmc.wholeBodyController.FootContactPoints;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AtlasCorridorNavigationTest
 {
@@ -43,9 +64,17 @@ public class AtlasCorridorNavigationTest
    @Test
    public void testAtlasMakesItToGoalInTrickyCorridor()
    {
+      // start_position 0.0 0.0 0.0
+      // goal_position 6.0 0.0 0.0
+      // vis_graph_status test
+      // step_planner_status dev
+      // a_star_timeout 10.0
+      // vis_graph_with_a_star_timeout 45.0
+      // requires_occlusion false
+
       new Thread(() -> {
          LogTools.info("Creating simulate REA module");
-         new SimulatedREAModule(PlannerTestEnvironments.getTrickCorridor(), createRobotModel()).start();
+         new SimulatedREAModule(PlannerTestEnvironments.getTrickCorridor(), createRobotModel(), PubSubImplementation.INTRAPROCESS).start();
       }).start();
 
       new Thread(() -> {
@@ -56,16 +85,56 @@ public class AtlasCorridorNavigationTest
 
       new Thread(() -> {
          LogTools.info("Creating footstep toolbox");
-         new MultiStageFootstepPlanningModule(createRobotModel(), null, false, DomainFactory.PubSubImplementation.INTRAPROCESS);
+         new MultiStageFootstepPlanningModule(createRobotModel(), null, false, PubSubImplementation.INTRAPROCESS);
       }).start();
 
-      SharedMemoryMessager messager = new SharedMemoryMessager(BehaviorModule.MessagerAPI);
-      ExceptionTools.handle(() -> messager.startMessager(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
 
-      new Thread(() -> {
-         LogTools.info("Creating behavior backpack");
-         BehaviorModule.createForTest(createRobotModel(), messager);
-      }).start();
+      if (VISUALIZE)
+      {
+//         LogTools.info("Creating behavior user interface");
+//         new BehaviorUI(messager, createRobotModel(), DomainFactory.PubSubImplementation.INTRAPROCESS);
+
+
+      }
+
+      Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.INTRAPROCESS, "test_node");
+
+      // create map subscriber
+      ROS2Input<PlanarRegionsListMessage> mapRegionsInput = new ROS2Input<>(ros2Node, PlanarRegionsListMessage.class, null, ROS2Tools.REA);
+
+      // subscribe to robot pose
+      RemoteSyncedHumanoidRobotState humanoidRobotState = new RemoteSyncedHumanoidRobotState(createRobotModel(), ros2Node);
+
+      boolean fullyExpandVisibilityGraph = false;
+      Point3D start = new Point3D();
+      Point3D goal = new Point3D(6.0, 0.0, 0.0);
+      FramePose3D robotPose = new FramePose3D();
+      robotPose.setToZero(humanoidRobotState.pollHumanoidRobotState().getMidFeetZUpFrame());
+      robotPose.changeFrame(ReferenceFrame.getWorldFrame());
+      List<Point3DReadOnly> pathPoints = null;
+      List<? extends Pose3DReadOnly> path = null;
+
+      while (robotPose.getPosition().distance(goal) > 0.5)
+      {
+         VisibilityGraphsParametersBasics parameters = new DefaultVisibilityGraphParameters();
+         NavigableRegionsManager manager = new NavigableRegionsManager(parameters, null, new ObstacleAvoidanceProcessor(parameters));
+         OcclusionHandlingPathPlanner occlusionHandlingPathPlanner = new OcclusionHandlingPathPlanner(manager);
+         PathOrientationCalculator orientationCalculator = new PathOrientationCalculator(parameters);
+         manager.setPlanarRegions(PlanarRegionMessageConverter.convertToPlanarRegionsList(mapRegionsInput.getLatest()).getPlanarRegionsAsList());
+         robotPose.setToZero(humanoidRobotState.pollHumanoidRobotState().getMidFeetZUpFrame());
+         robotPose.changeFrame(ReferenceFrame.getWorldFrame());
+         pathPoints = occlusionHandlingPathPlanner.calculateBodyPath(robotPose.getPosition(), goal, fullyExpandVisibilityGraph);
+         path = orientationCalculator.computePosesFromPath(pathPoints, manager.getVisibilityMapSolution(), new Quaternion(), new Quaternion());
+
+         // make robot walk with path
+
+
+         // wait for robot to finish walking
+
+
+         robotPose.setToZero(humanoidRobotState.pollHumanoidRobotState().getMidFeetZUpFrame());
+         robotPose.changeFrame(ReferenceFrame.getWorldFrame());
+      }
    }
 
    private AtlasRobotModel createRobotModel()
