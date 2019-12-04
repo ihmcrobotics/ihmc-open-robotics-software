3468ae848cc598b0989df924cd77274bd11dba4d
Duncan Calvert <dcalvert@ihmc.us>
2019-12-03 19:01:16 -0600

    Extract SCSVideoDataROS2Bridge from sim w/ camera classes.

--- atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasKinematicSimWithCamera.java
+++ atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasKinematicSimWithCamera.java
@@ -7,14 +7,12 @@ import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.behaviors.scsSensorSimulation.SensorOnlySimulation;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.avatar.kinematicsSimulation.AvatarKinematicsSimulation;
+import us.ihmc.avatar.kinematicsSimulation.HumanoidKinematicsSimulation;
 import us.ihmc.codecs.generated.YUVPicture;
 import us.ihmc.codecs.generated.YUVPicture.YUVSubsamplingType;
 import us.ihmc.codecs.yuv.JPEGEncoder;
 import us.ihmc.codecs.yuv.YUVPictureConverter;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.HighLevelHumanoidControllerFactory;
 import us.ihmc.communication.IHMCROS2Publisher;
-import us.ihmc.communication.IHMCRealtimeROS2Publisher;
 import us.ihmc.communication.ROS2Input;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.producers.VideoDataServer;
@@ -22,7 +20,7 @@ import us.ihmc.communication.producers.VideoDataServerImageCallback;
 import us.ihmc.communication.producers.VideoSource;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
-import us.ihmc.humanoidBehaviors.tools.RemoteSyncedHumanoidFrames;
+import us.ihmc.humanoidBehaviors.tools.RemoteSyncedHumanoidRobotState;
 import us.ihmc.humanoidRobotics.communication.packets.HumanoidMessageTools;
 import us.ihmc.jMonkeyEngineToolkit.camera.CameraConfiguration;
 import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
@@ -42,13 +40,13 @@ public class AtlasKinematicSimWithCamera
 
    public AtlasKinematicSimWithCamera(DRCRobotModel robotModel, CommonAvatarEnvironmentInterface environment)
    {
-      AvatarKinematicsSimulation.createForManualTest(robotModel, false);
+      HumanoidKinematicsSimulation.createForManualTest(robotModel, false);
 
       Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, "kinematic_camera");
       scsCameraPublisher = new IHMCROS2Publisher<>(ros2Node, VideoPacket.class);
 
-      RemoteSyncedHumanoidFrames remoteSyncedHumanoidFrames = new RemoteSyncedHumanoidFrames(robotModel, ros2Node);
-      remoteSyncedHumanoidFrames.pollHumanoidReferenceFrames().getNeckFrame(NeckJointName.DISTAL_NECK_PITCH);
+      RemoteSyncedHumanoidRobotState remoteSyncedHumanoidFrames = new RemoteSyncedHumanoidRobotState(robotModel, ros2Node);
+      remoteSyncedHumanoidFrames.pollHumanoidRobotState().getNeckFrame(NeckJointName.DISTAL_NECK_PITCH);
 
       /// create scs
       SensorOnlySimulation sensorOnlySimulation = new SensorOnlySimulation();
@@ -59,7 +57,7 @@ public class AtlasKinematicSimWithCamera
       ROS2Input<RobotConfigurationData> robotConfigurationData = new ROS2Input<>(ros2Node,
                                                                                  RobotConfigurationData.class,
                                                                                  robotModel.getSimpleRobotName(),
-                                                                                 HighLevelHumanoidControllerFactory.ROS2_ID);
+                                                                                 ROS2Tools.HUMANOID_CONTROLLER);
 
       String cameraName = "camera";
 
@@ -72,57 +70,9 @@ public class AtlasKinematicSimWithCamera
       scs.startStreamingVideoData(cameraConfiguration,
                                   width,
                                   height,
-                                  new VideoDataServerImageCallback(new VideoPacketCallback()),
+                                  new VideoDataServerImageCallback(new SCSVideoDataROS2Bridge(scsCameraPublisher::publish)),
                                   () -> robotConfigurationData.getLatest().getSyncTimestamp(),
                                   framesPerSecond);
-   }
-
-   private static final Object hackyLockBecauseJPEGEncoderIsNotThreadsafe = new Object();
-
-   class VideoPacketCallback implements VideoDataServer
-   {
-      private final YUVPictureConverter converter = new YUVPictureConverter();
-      private final JPEGEncoder encoder = new JPEGEncoder();
-
-      @Override
-      public void onFrame(VideoSource videoSource,
-                          BufferedImage bufferedImage,
-                          long timeStamp,
-                          Point3DReadOnly cameraPosition,
-                          QuaternionReadOnly cameraOrientation,
-                          IntrinsicParameters intrinsicParameters)
-      {
-
-         YUVPicture picture = converter.fromBufferedImage(bufferedImage, YUVSubsamplingType.YUV420);
-         try
-         {
-            ByteBuffer buffer;
-            synchronized (hackyLockBecauseJPEGEncoderIsNotThreadsafe)
-            {
-               buffer = encoder.encode(picture, 75);
-            }
-            byte[] data = new byte[buffer.remaining()];
-            buffer.get(data);
-            VideoPacket videoPacket = HumanoidMessageTools.createVideoPacket(videoSource,
-                                                                             timeStamp,
-                                                                             data,
-                                                                             cameraPosition,
-                                                                             cameraOrientation,
-                                                                             intrinsicParameters);
-            scsCameraPublisher.publish(videoPacket);
-         }
-         catch (IOException e)
-         {
-            e.printStackTrace();
-         }
-         picture.delete();
-      }
-
-      @Override
-      public boolean isConnected()
-      {
-         return true; // do nothing
-      }
    }
 
    public static void main(String[] args)
