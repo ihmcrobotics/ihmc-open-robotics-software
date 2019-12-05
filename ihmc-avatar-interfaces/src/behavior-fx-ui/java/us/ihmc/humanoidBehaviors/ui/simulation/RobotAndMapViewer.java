b90733539a3fb525def8a025c672a943f7cfc5b1
Duncan Calvert <dcalvert@ihmc.us>
2019-12-04 19:56:12 -0600

    Add body path plan visualization. Working with flat ground snapper and always valid node checker.

--- ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/simulation/RobotAndMapViewer.java
+++ ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/simulation/RobotAndMapViewer.java
@@ -7,7 +7,10 @@ import org.apache.commons.lang3.tuple.Pair;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.euclid.geometry.Pose3D;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.footstepPlanning.FootstepPlan;
+import us.ihmc.humanoidBehaviors.ui.graphics.BodyPathPlanGraphic;
 import us.ihmc.humanoidBehaviors.ui.graphics.FootstepPlanGraphic;
 import us.ihmc.humanoidBehaviors.ui.graphics.live.LivePlanarRegionsGraphic;
 import us.ihmc.humanoidBehaviors.ui.tools.JavaFXRemoteRobotVisualizer;
@@ -17,10 +20,13 @@ import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.ros2.Ros2Node;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RobotAndMapViewer
 {
    private FootstepPlanGraphic footstepPlanGraphic;
+   private BodyPathPlanGraphic bodyPathPlanGraphic;
 
    public RobotAndMapViewer(DRCRobotModel robotModel, Ros2Node ros2Node)
    {
@@ -28,7 +34,7 @@ public class RobotAndMapViewer
       {
          View3DFactory view3dFactory = new View3DFactory(1200, 800);
          FocusBasedCameraMouseEventHandler camera = view3dFactory.addCameraController(0.05, 2000.0, true);
-         double isoZoomOut = 12.0;
+         double isoZoomOut = 10.0;
          camera.changeCameraPosition(-isoZoomOut, -isoZoomOut, isoZoomOut);
          view3dFactory.addWorldCoordinateSystem(0.3);
          view3dFactory.addDefaultLighting();
@@ -38,6 +44,9 @@ public class RobotAndMapViewer
 
          footstepPlanGraphic = new FootstepPlanGraphic(robotModel);
          view3dFactory.addNodeToView(footstepPlanGraphic);
+
+         bodyPathPlanGraphic = new BodyPathPlanGraphic();
+         view3dFactory.addNodeToView(bodyPathPlanGraphic);
 
          Stage primaryStage = new Stage();
          primaryStage.setTitle(getClass().getSimpleName());
@@ -59,5 +68,10 @@ public class RobotAndMapViewer
          footstepLocations.add(new MutablePair<>(footstepPlan.getFootstep(i).getRobotSide(), new Pose3D(soleFramePoseToPack)));
       }
       footstepPlanGraphic.generateMeshesAsynchronously(footstepLocations);
+   }
+
+   public void setBodyPathPlanToVisualize(List<Point3DReadOnly> bodyPath)
+   {
+      bodyPathPlanGraphic.generateMeshesAsynchronously(bodyPath.stream().map(Point3D::new).collect(Collectors.toList())); // deep copy
    }
 }
