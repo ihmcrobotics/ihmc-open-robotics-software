d3838ccb7c39430abdfa92573029bd860a8bbe0b
Robert <rgriffin@ihmc.us>
2019-12-09 17:42:10 -0600

    set up some tests that currently fail, because I have no idea how to guarantee the hessian has only real eigenvalues

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/careSolvers/HamiltonianCARESolver.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/careSolvers/HamiltonianCARESolver.java
@@ -16,30 +16,28 @@ import us.ihmc.matrixlib.MatrixTools;
 import us.ihmc.matrixlib.NativeCommonOps;
 
 /**
- * This solver computes the solution using the following approach:
+ * This solver computes the solution to the algebraic Riccati equation
  *
- * 1. Compute the Hamiltonian matrix 2. Extract its complex eigen vectors (not
- * the best solution, a better solution would be ordered Schur transformation)
- */
-
-/**
+ * A' P + P A - P B R^-1 B' P + Q = 0
+ * or
+ * A' P + P A - P M P + Q = 0
+ *
+ * where P is unknown.
+ *
+ * The solution using the following approach:
+ *
  * Compute P using the Hamiltonian and the ordered eigen values decomposition, as shown here:
  *
  * https://en.wikipedia.org/wiki/Algebraic_Riccati_equation
  *
- * @param A state transition matrix
- * @param B control multipliers matrix
- * @param Q state cost matrix
- * @param Rinv inverse of matrix R
- * @return initial solution
+ * A good set of notes to follow are
+ *
+ * https://stanford.edu/class/ee363/lectures/clqr.pdf
  */
 public class HamiltonianCARESolver implements CARESolver
 {
    /** The solution of the algebraic Riccati equation. */
    private final DenseMatrix64F P = new DenseMatrix64F(0, 0);
-
-   /** The computed K. */
-   private final DenseMatrix64F K = new DenseMatrix64F(0, 0);
 
    private final SingularValueDecomposition<DenseMatrix64F> svd = DecompositionFactory.svd(0, 0, false, false, false);
    private final EigenDecomposition<DenseMatrix64F> eigen = DecompositionFactory.eig(0, true);
@@ -48,9 +46,7 @@ public class HamiltonianCARESolver implements CARESolver
    private final DenseMatrix64F ATranspose = new DenseMatrix64F(0, 0);
 
    private final DenseMatrix64F A = new DenseMatrix64F(0, 0);
-   private final DenseMatrix64F B = new DenseMatrix64F(0, 0);
    private final DenseMatrix64F Q = new DenseMatrix64F(0, 0);
-   private final DenseMatrix64F R = new DenseMatrix64F(0, 0);
    private final DenseMatrix64F Rinv = new DenseMatrix64F(0, 0);
 
    /** Hamiltonian of the Riccati equation. */
@@ -62,10 +58,7 @@ public class HamiltonianCARESolver implements CARESolver
    private final DenseMatrix64F u2 = new DenseMatrix64F(0, 0);
    private final DenseMatrix64F u1Inv = new DenseMatrix64F(0, 0);
 
-   private int m;
    private int n;
-
-   private final DenseMatrix64F tempMatrix = new DenseMatrix64F(0, 0);
 
    /**
     * Constructor of the solver. A and B should be compatible. B and R must be
@@ -93,37 +86,52 @@ public class HamiltonianCARESolver implements CARESolver
             throw new IllegalArgumentException("R Matrix is singular.");
       }
 
-      this.A.set(A);
-      this.B.set(B);
-      this.Q.set(Q);
-      this.R.set(R);
+      this.n = A.getNumRows();
+
+      BTranspose.set(B);
+      CommonOps.transpose(BTranspose);
+
+      Rinv.reshape(n, n);
+      NativeCommonOps.invert(R, Rinv);
+      M.reshape(n, n);
+      NativeCommonOps.multQuad(BTranspose, Rinv, M);
+
+      setMatrices(A, Q, M, false);
    }
+
+   /**
+    * Constructor of the solver. A and B should be compatible. B and R must be
+    * multiplicative compatible. A and Q must be multiplicative compatible. R
+    * must be invertible.
+    *
+    * @param A state transition matrix
+    * @param B control multipliers matrix
+    * @param Q state cost matrix
+    * @param R control cost matrix
+    */
+   public void setMatrices(DenseMatrix64F A, DenseMatrix64F Q, DenseMatrix64F M, boolean checkMatrices)
+   {
+      if (checkMatrices)
+      {
+         // checking A
+         MatrixChecking.assertIsSquare(A);
+         MatrixChecking.assertMultiplicationCompatible(A, Q);
+
+      }
+
+      this.n = A.getNumRows();
+
+      this.A.set(A);
+      this.Q.set(Q);
+      this.M.set(M);
+   }
+
 
    /** {@inheritDoc} */
    public void computeP()
    {
-      this.n = A.getNumRows();
-      this.m = B.getNumCols();
-      this.Rinv.reshape(n, n);
-
-      NativeCommonOps.invert(R, Rinv);
-
-      ATranspose.reshape(n, n);
-      BTranspose.reshape(m, n);
-      CommonOps.transpose(A, ATranspose);
-      CommonOps.transpose(B, BTranspose);
-
-      // computing the Hamiltonian Matrix
-      M.reshape(n, n);
-      NativeCommonOps.multQuad(BTranspose, Rinv, M);
-
       // defining Hamiltonian
-      H.reshape(2 * n, 2 * n);
-
-      MatrixTools.setMatrixBlock(H, 0, 0, A, 0, 0, n, n, 1.0);
-      MatrixTools.setMatrixBlock(H, n, 0, Q, 0, 0, n, n, -1.0);
-      MatrixTools.setMatrixBlock(H, 0, n, M, 0, 0, n, n, -1.0);
-      MatrixTools.setMatrixBlock(H, n, n, ATranspose, 0, 0, n, n, -1.0);
+      assembleHamiltonian(H);
 
       // eigen decomposition
       // it must be ordered in order to work with submatrices
@@ -132,14 +140,18 @@ public class HamiltonianCARESolver implements CARESolver
       u1.reshape(n, n);
       u2.reshape(n, n);
       int valueIdx = 0;
-
       for (int i = 0; i < 2 * n; i++)
       {
+         if (eigen.getEigenvalue(i).getImaginary() != 0.0)
+            throw new RuntimeException("The imaginary part of the eigenvalue must be equal to zero.");
+
          if (eigen.getEigenvalue(i).getReal() < 0.0)
          {
-            MatrixTools.setMatrixBlock(u, 0, valueIdx, eigen.getEigenVector(i), 0, 0, 2 * n, 1, 1.0);
-            valueIdx++;
+            MatrixTools.setMatrixBlock(u, 0, valueIdx++, eigen.getEigenVector(i), 0, 0, 2 * n, 1, 1.0);
          }
+
+         if (valueIdx == n)
+            break;
       }
 
       // solving linear system
@@ -152,12 +164,19 @@ public class HamiltonianCARESolver implements CARESolver
 
       P.reshape(n, n);
       CommonOps.mult(u2, u1Inv, P);
+   }
 
-      // K = R^-1 B^T P
-      K.reshape(m, n);
-      tempMatrix.reshape(m, n);
-      CommonOps.mult(BTranspose, P, tempMatrix);
-      CommonOps.mult(Rinv, tempMatrix, K);
+   void assembleHamiltonian(DenseMatrix64F hamiltonianToPack)
+   {
+      ATranspose.reshape(n, n);
+      CommonOps.transpose(A, ATranspose);
+
+      hamiltonianToPack.reshape(2 * n, 2 * n);
+
+      MatrixTools.setMatrixBlock(hamiltonianToPack, 0, 0, A, 0, 0, n, n, 1.0);
+      MatrixTools.setMatrixBlock(hamiltonianToPack, n, 0, Q, 0, 0, n, n, -1.0);
+      MatrixTools.setMatrixBlock(hamiltonianToPack, 0, n, M, 0, 0, n, n, -1.0);
+      MatrixTools.setMatrixBlock(hamiltonianToPack, n, n, ATranspose, 0, 0, n, n, -1.0);
    }
 
    /** {inheritDoc} */
@@ -166,10 +185,5 @@ public class HamiltonianCARESolver implements CARESolver
       return P;
    }
 
-   /** {inheritDoc} */
-   public DenseMatrix64F getK()
-   {
-      return K;
-   }
 
 }
