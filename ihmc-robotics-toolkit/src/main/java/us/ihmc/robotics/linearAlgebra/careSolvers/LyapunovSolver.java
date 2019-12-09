3520cbb97731365efb04a14ef91b587ddd82d8ad
Robert <rgriffin@ihmc.us>
2019-12-08 23:10:25 -0600

    started working on extracting a lyapunov equation solver

---
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/careSolvers/LyapunovSolver.java
@@ -0,0 +1,90 @@
+package us.ihmc.robotics.linearAlgebra.careSolvers;
+
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.factory.LinearSolverFactory;
+import org.ejml.interfaces.linsol.LinearSolver;
+import org.ejml.ops.CommonOps;
+import us.ihmc.matrixlib.MatrixTools;
+
+/**
+ * Solves the Equation
+ *
+ * A X + X A' = Q
+ *
+ * for P
+ */
+public class LyapunovSolver
+{
+   private final DenseMatrix64F A = new DenseMatrix64F(0, 0);
+   private final DenseMatrix64F aTranspose = new DenseMatrix64F(0, 0);
+   private final DenseMatrix64F Q = new DenseMatrix64F(0, 0);
+
+   private final DenseMatrix64F tempMatrix = new DenseMatrix64F(0, 0);
+
+   private final DenseMatrix64F qVector = new DenseMatrix64F(0, 0);
+   private final DenseMatrix64F xVector = new DenseMatrix64F(0, 0);
+   private final DenseMatrix64F X = new DenseMatrix64F(0, 0);
+
+   private final DenseMatrix64F X1 = new  DenseMatrix64F(0, 0);
+   private final DenseMatrix64F eyeX = new DenseMatrix64F(0, 0);
+
+   private final LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.lu(0);
+
+   private int n;
+
+   public void setMatices(DenseMatrix64F A, DenseMatrix64F Q)
+   {
+      MatrixChecking.isSquare(A);
+
+      this.A.set(A);
+      this.Q.set(Q);
+
+      n = this.A.getNumRows();
+   }
+
+   public void solve()
+   {
+      aTranspose.reshape(n, n);
+      eyeX.reshape(n, n);
+      X1.reshape(n * n, n * n);
+      tempMatrix.reshape(n * n, n * n);
+
+      CommonOps.transpose(A, aTranspose);
+      CommonOps.setIdentity(eyeX);
+      CommonOps.kron(aTranspose, eyeX, X1);
+      CommonOps.kron(eyeX, aTranspose, tempMatrix);
+      CommonOps.addEquals(X1, tempMatrix);
+
+      // stack all of Q into a vector
+      stack(Q, qVector);
+
+      xVector.reshape(X1.getNumRows(), 1);
+      solver.setA(X1);
+      solver.solve(qVector, xVector);
+
+      toSquareMatrix(xVector, X);
+   }
+
+   public DenseMatrix64F getX()
+   {
+      return X;
+   }
+
+   static DenseMatrix64F stack(DenseMatrix64F QMatrix, DenseMatrix64F qVector)
+   {
+      int rows = QMatrix.getNumRows();
+      qVector.reshape(rows * rows, 1);
+      for (int i = 0; i < rows; i++)
+         MatrixTools.setMatrixBlock(qVector, i * rows, 0, QMatrix, 0, i, rows, 1, 1.0);
+
+      return qVector;
+   }
+
+   static void toSquareMatrix(DenseMatrix64F xVector, DenseMatrix64F xMatrix)
+   {
+      int rows = (int) Math.sqrt(xVector.getNumRows());
+      xMatrix.reshape(rows, rows);
+      for (int i = 0; i < rows; i++)
+         MatrixTools.setMatrixBlock(xMatrix, 0, i, xVector, i * rows, 0, rows, 1, 1.0);
+   }
+}
