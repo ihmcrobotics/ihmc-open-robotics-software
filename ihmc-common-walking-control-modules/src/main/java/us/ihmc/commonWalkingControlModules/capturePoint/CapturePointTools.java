466cac7f41dc1e9a8ce71568ff31e1ac82c4052a
Robert <rgriffin@ihmc.us>
2019-12-19 10:07:47 -0600

    set up the simple com trajectory planner to compute the desired DCM Trajectories

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/CapturePointTools.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/CapturePointTools.java
@@ -88,6 +88,27 @@ public class CapturePointTools
    }
 
    /**
+    * Computes the Instantaneous Capture Point velocity by
+    * <p>
+    *    x&#775;<sup>ICP</sup> = &omega; (x<sup>ICP</sup> - x<sup>CMP</sup>)
+    * </p>
+    *
+    * @param capturePointPosition
+    * @param cmpPosition
+    * @param omega0 the natural frequency &omega; =
+    *           &radic;<span style="text-decoration:overline;">&nbsp; g / z0&nbsp;</span> of the
+    *           biped.
+    * @param icpVelocityToPack
+    */
+   public static void computeCapturePointVelocity(FramePoint3DReadOnly capturePointPosition, FramePoint3DReadOnly cmpPosition,
+                                                  double omega0, FixedFrameVector3DBasics icpVelocityToPack)
+   {
+      icpVelocityToPack.sub(capturePointPosition, cmpPosition);
+      icpVelocityToPack.scale(omega0);
+   }
+
+
+   /**
     * FIXME this method is probably wrong.
     * Compute the capture point acceleration given the desired capture point velocity
     *
