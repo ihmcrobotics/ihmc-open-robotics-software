ae316308048de0d28a86ad9f3710c790090e18ff
Duncan Calvert <dcalvert@ihmc.us>
2019-12-19 13:46:52 -0600

    Saving model and resources.zip

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/kinematicsSimulation/IntraprocessYoVariableLogger.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/kinematicsSimulation/IntraprocessYoVariableLogger.java
@@ -2,6 +2,8 @@ package us.ihmc.avatar.kinematicsSimulation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import us.ihmc.commons.exception.DefaultExceptionHandler;
+import us.ihmc.commons.nio.FileTools;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.idl.serializers.extra.YAMLSerializer;
@@ -17,7 +19,9 @@ import us.ihmc.robotDataLogger.logger.LogPropertiesWriter;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -31,6 +35,8 @@ public class IntraprocessYoVariableLogger
    public static final String modelResourceBundle = "resources.zip";
    public static final String indexFilename = "robotData.dat";
    public static final String summaryFilename = "summary.csv";
+   private final String timestamp;
+   private String logFolder;
 
    public IntraprocessYoVariableLogger(String logName,
                                        LogModelProvider logModelProvider,
@@ -41,7 +47,8 @@ public class IntraprocessYoVariableLogger
    {
       DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
       Calendar calendar = Calendar.getInstance();
-      String timestamp = dateFormat.format(calendar.getTime());
+      timestamp = dateFormat.format(calendar.getTime());
+      logFolder = timestamp + "_Log";
 
       RegistrySendBufferBuilder registrySendBufferBuilder = new RegistrySendBufferBuilder(registry, rootBody, yoGraphicsListRegistry);
 
@@ -57,14 +64,14 @@ public class IntraprocessYoVariableLogger
       try
       {
          YAMLSerializer<Handshake> serializer = new YAMLSerializer<>(new HandshakePubSubType());
-         serializer.serialize(new File(handshakeFilename), handshake);
+         serializer.serialize(createFileInLogFolder(handshakeFilename), handshake);
       }
       catch (Exception e)
       {
          e.printStackTrace();
       }
 
-      LogPropertiesWriter logProperties = new LogPropertiesWriter(new File("robotData.log"));
+      LogPropertiesWriter logProperties = new LogPropertiesWriter(createFileInLogFolder("robotData.log"));
       logProperties.getVariables().setHandshake(handshakeFilename);
       logProperties.getVariables().setData(dataFilename);
       logProperties.getVariables().setCompressed(true);
@@ -74,6 +81,37 @@ public class IntraprocessYoVariableLogger
 
       logProperties.setName(logName);
       logProperties.setTimestamp(timestamp);
+
+      // create resource zip
+
+      logProperties.getModel().setLoader(logModelProvider.getLoader().getCanonicalName());
+      logProperties.getModel().setName(logModelProvider.getModelName());
+      for (String resourceDirectory : logModelProvider.getResourceDirectories())
+      {
+         logProperties.getModel().getResourceDirectoriesList().add(resourceDirectory);
+      }
+      logProperties.getModel().setPath(modelFilename);
+      logProperties.getModel().setResourceBundle(modelResourceBundle);
+
+      File modelFile = createFileInLogFolder(modelFilename);
+      File resourceFile = createFileInLogFolder(modelResourceBundle);
+      try
+      {
+         FileOutputStream modelStream = new FileOutputStream(modelFile, false);
+         modelStream.write(logModelProvider.getModel());
+         modelStream.getFD().sync();
+         modelStream.close();
+         FileOutputStream resourceStream = new FileOutputStream(resourceFile, false);
+         resourceStream.write(logModelProvider.getResourceZip());
+         resourceStream.getFD().sync();
+         resourceStream.close();
+
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
+
 
       try
       {
@@ -85,4 +123,9 @@ public class IntraprocessYoVariableLogger
       }
    }
 
+   private File createFileInLogFolder(String filename)
+   {
+      FileTools.ensureDirectoryExists(Paths.get(logFolder), DefaultExceptionHandler.RUNTIME_EXCEPTION);
+      return new File(logFolder + "/" + filename);
+   }
 }
