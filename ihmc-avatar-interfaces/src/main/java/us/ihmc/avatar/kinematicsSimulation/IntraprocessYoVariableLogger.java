b086ea398a017e3579058cc1238261e39c56549e
Duncan Calvert <dcalvert@ihmc.us>
2019-12-19 16:20:13 -0600

    Saving compressed YoVariable data in Intraprocess logger.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/kinematicsSimulation/IntraprocessYoVariableLogger.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/kinematicsSimulation/IntraprocessYoVariableLogger.java
@@ -4,39 +4,61 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.nio.FileTools;
+import us.ihmc.concurrent.ConcurrentRingBuffer;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.idl.serializers.extra.YAMLSerializer;
+import us.ihmc.log.LogTools;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
-import us.ihmc.robotDataLogger.Handshake;
-import us.ihmc.robotDataLogger.HandshakeFileType;
-import us.ihmc.robotDataLogger.HandshakePubSubType;
+import us.ihmc.robotDataLogger.*;
 import us.ihmc.robotDataLogger.dataBuffers.RegistrySendBufferBuilder;
 import us.ihmc.robotDataLogger.handshake.LogHandshake;
 import us.ihmc.robotDataLogger.handshake.YoVariableHandShakeBuilder;
+import us.ihmc.robotDataLogger.jointState.JointState;
 import us.ihmc.robotDataLogger.logger.LogPropertiesWriter;
+import us.ihmc.tools.compression.SnappyUtils;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
+import us.ihmc.yoVariables.variable.YoVariable;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class IntraprocessYoVariableLogger
 {
-   public static final String propertyFile = "robotData.log";
-   public static final String handshakeFilename = "handshake.yaml";
-   public static final String dataFilename = "robotData.bsz";
-   public static final String modelFilename = "model.sdf";
-   public static final String modelResourceBundle = "resources.zip";
-   public static final String indexFilename = "robotData.dat";
-   public static final String summaryFilename = "summary.csv";
+   public static final String PROPERTY_FILE = "robotData.log";
+   public static final String HANDSHAKE_FILENAME = "handshake.yaml";
+   public static final String DATA_FILENAME = "robotData.bsz";
+   public static final String MODEL_FILENAME = "model.sdf";
+   public static final String MODEL_RESOURCE_BUNDLE = "resources.zip";
+   public static final String INDEX_FILENAME = "robotData.dat";
+   public static final String SUMMARY_FILENAME = "summary.csv";
    private final String timestamp;
    private String logFolder;
+   private ByteBuffer compressedBuffer;
+   private ByteBuffer indexBuffer = ByteBuffer.allocate(16);
+   private List<YoVariable<?>> variables;
+   private List<JointState> jointStates;
+   private ByteBuffer dataBuffer;
+   private LongBuffer dataBufferAsLong;
+   private FileChannel dataChannel;
+   private FileChannel indexChannel;
+
+   private static final int CHANGED_BUFFER_CAPACITY = 128;
+   private ConcurrentRingBuffer<VariableChangedMessage> variableChanged = new ConcurrentRingBuffer<>(new VariableChangedMessage.Builder(),
+                                                                                                     CHANGED_BUFFER_CAPACITY);
 
    public IntraprocessYoVariableLogger(String logName,
                                        LogModelProvider logModelProvider,
@@ -64,19 +86,19 @@ public class IntraprocessYoVariableLogger
       try
       {
          YAMLSerializer<Handshake> serializer = new YAMLSerializer<>(new HandshakePubSubType());
-         serializer.serialize(createFileInLogFolder(handshakeFilename), handshake);
+         serializer.serialize(createFileInLogFolder(HANDSHAKE_FILENAME), handshake);
       }
       catch (Exception e)
       {
          e.printStackTrace();
       }
 
-      LogPropertiesWriter logProperties = new LogPropertiesWriter(createFileInLogFolder("robotData.log"));
-      logProperties.getVariables().setHandshake(handshakeFilename);
-      logProperties.getVariables().setData(dataFilename);
+      LogPropertiesWriter logProperties = new LogPropertiesWriter(createFileInLogFolder(PROPERTY_FILE));
+      logProperties.getVariables().setHandshake(HANDSHAKE_FILENAME);
+      logProperties.getVariables().setData(DATA_FILENAME);
       logProperties.getVariables().setCompressed(true);
       logProperties.getVariables().setTimestamped(true);
-      logProperties.getVariables().setIndex(indexFilename);
+      logProperties.getVariables().setIndex(INDEX_FILENAME);
       logProperties.getVariables().setHandshakeFileType(HandshakeFileType.IDL_YAML);
 
       logProperties.setName(logName);
@@ -90,11 +112,11 @@ public class IntraprocessYoVariableLogger
       {
          logProperties.getModel().getResourceDirectoriesList().add(resourceDirectory);
       }
-      logProperties.getModel().setPath(modelFilename);
-      logProperties.getModel().setResourceBundle(modelResourceBundle);
+      logProperties.getModel().setPath(MODEL_FILENAME);
+      logProperties.getModel().setResourceBundle(MODEL_RESOURCE_BUNDLE);
 
-      File modelFile = createFileInLogFolder(modelFilename);
-      File resourceFile = createFileInLogFolder(modelResourceBundle);
+      File modelFile = createFileInLogFolder(MODEL_FILENAME);
+      File resourceFile = createFileInLogFolder(MODEL_RESOURCE_BUNDLE);
       try
       {
          FileOutputStream modelStream = new FileOutputStream(modelFile, false);
@@ -112,10 +134,105 @@ public class IntraprocessYoVariableLogger
          throw new RuntimeException(e);
       }
 
-
       try
       {
          logProperties.store();
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+
+
+      int numberOfJointStates = 0;
+      for (int i = 0; i < handshake.getJoints().size(); i++)
+      {
+         JointDefinition joint = handshake.getJoints().get(i);
+         numberOfJointStates += JointState.getNumberOfVariables(joint.getType());
+      }
+
+      int stateVariables = 1 + 9876 + numberOfJointStates;
+      int bufferSize = stateVariables * 8;
+      LogTools.info("Buffer size: {}", bufferSize);
+      LogTools.info("Number of YoVariables: {}", registry.getNumberOfYoVariables());
+      LogTools.info("Number of joint states: {}", numberOfJointStates);
+
+      compressedBuffer = ByteBuffer.allocate(SnappyUtils.maxCompressedLength(bufferSize));
+      dataBuffer = ByteBuffer.allocate(bufferSize);
+      dataBufferAsLong = dataBuffer.asLongBuffer();
+      variables = registry.getAllVariables();
+      jointStates = new ArrayList<>();
+      for (int i = 0; i < handshake.getJoints().size(); i++)
+      {
+         JointDefinition joint = handshake.getJoints().get(i);
+         jointStates.add(JointState.createJointState(joint.getNameAsString(), joint.getType()));
+      }
+
+      try
+      {
+         dataChannel = new FileOutputStream(createFileInLogFolder(DATA_FILENAME), false).getChannel();
+         indexChannel = new FileOutputStream(createFileInLogFolder(INDEX_FILENAME), false).getChannel();
+         dataChannel.force(true);
+         indexChannel.force(true);
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+
+      Runtime.getRuntime().addShutdownHook(new Thread(() ->
+      {
+         try
+         {
+            dataChannel.close();
+         }
+         catch (IOException e)
+         {
+            e.printStackTrace();
+         }
+      }));
+   }
+
+   public void update(long timestamp)
+   {
+      dataBuffer.clear();
+      dataBufferAsLong.clear();
+
+      dataBufferAsLong.put(timestamp);
+      for (int i = 0; i < variables.size(); i++)
+      {
+         try
+         {
+            dataBufferAsLong.put(variables.get(i).getValueAsLongBits());
+         }
+         catch (BufferOverflowException e)
+         {
+            LogTools.error("yoVar # {}:  size: {}  {}", i, variables.size(), e.getMessage());
+         }
+      }
+
+      for (int i = 0; i < jointStates.size(); i++)
+      {
+         jointStates.get(i).get(dataBufferAsLong);
+      }
+
+      dataBufferAsLong.flip();
+      dataBuffer.clear();
+
+      try
+      {
+         dataBuffer.clear();
+         compressedBuffer.clear();
+         SnappyUtils.compress(dataBuffer, compressedBuffer);
+         compressedBuffer.flip();
+
+         indexBuffer.clear();
+         indexBuffer.putLong(timestamp);
+         indexBuffer.putLong(dataChannel.position());
+         indexBuffer.flip();
+
+         indexChannel.write(indexBuffer);
+         dataChannel.write(compressedBuffer);
       }
       catch (IOException e)
       {
