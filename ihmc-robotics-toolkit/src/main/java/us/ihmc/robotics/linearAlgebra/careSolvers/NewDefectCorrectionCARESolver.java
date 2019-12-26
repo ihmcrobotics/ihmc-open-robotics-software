a03952d03698c9f1c0b517c7b6bd6d302d16768b
Robert <rgriffin@ihmc.us>
2019-12-25 18:16:33 -0600

    wrote up a basic defect correction algorithm, according to that book

---
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/linearAlgebra/careSolvers/NewDefectCorrectionCARESolver.java
@@ -0,0 +1,90 @@
+package us.ihmc.robotics.linearAlgebra.careSolvers;
+
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
+import us.ihmc.matrixlib.NativeCommonOps;
+
+public class NewDefectCorrectionCARESolver extends AbstractCARESolver
+{
+  private static final int defaultMaxIterations = 1000;
+  private final int maxIterations;
+
+  private static final double defaultConvergenceEpsilon = 1e-8;
+  private final double convergenceEpsilon;
+
+  private final DenseMatrix64F P = new DenseMatrix64F(0, 0);
+  private final DenseMatrix64F X = new DenseMatrix64F(0, 0);
+
+  private final DenseMatrix64F PE = new DenseMatrix64F(0, 0);
+  private final DenseMatrix64F ASquiggle = new DenseMatrix64F(0, 0);
+  private final DenseMatrix64F QSquiggle = new DenseMatrix64F(0, 0);
+
+  private final NewCARESolver backendSolver;
+
+  private boolean isUpToDate = false;
+
+  public NewDefectCorrectionCARESolver(NewCARESolver backendSolver)
+  {
+     this.backendSolver = backendSolver;
+     this.maxIterations = defaultMaxIterations;
+     this.convergenceEpsilon = defaultConvergenceEpsilon;
+  }
+
+  /** {@inheritDoc} */
+  public DenseMatrix64F computeP()
+  {
+     backendSolver.setMatrices(A, E, M, Q);
+     P.set(backendSolver.getP());
+
+     int iterations = 0;
+     boolean converged = false;
+     while (!converged)
+     {
+        X.set(computeErrorEstimate(P));
+        CommonOps.addEquals(P, X);
+
+        converged = MatrixToolsLocal.isZero(X, convergenceEpsilon);
+
+        if (iterations > maxIterations)
+           throw new RuntimeException("Failed to converge.");
+
+        iterations++;
+     }
+
+
+     isUpToDate = true;
+     return P;
+  }
+
+  private DenseMatrix64F computeErrorEstimate(DenseMatrix64F currentValue)
+  {
+     if (!hasE)
+        PE.set(currentValue);
+     else
+        CommonOps.mult(currentValue, E, PE);
+
+     // QSquiggle = Q + A' P E + E' P A - E' P M P E
+     NativeCommonOps.multQuad(PE, M, QSquiggle);
+     CommonOps.scale(-1.0, QSquiggle);
+
+     CommonOps.multAddTransA(PE, A, QSquiggle);
+     CommonOps.multAddTransA(A, PE, QSquiggle);
+     CommonOps.addEquals(QSquiggle, Q);
+
+     // ASquiggle = A - M P E
+     ASquiggle.set(A);
+     CommonOps.multAdd(-1.0, M, PE, ASquiggle);
+
+     backendSolver.setMatrices(ASquiggle, E, M, QSquiggle);
+
+     return backendSolver.getP();
+  }
+
+
+  /** {inheritDoc} */
+  public DenseMatrix64F getP()
+  {
+     return isUpToDate ? P : computeP();
+  }
+
+}
