543b59a6fda52c06632ed6a2e6c6d00648bd9845
OperatorComputer <OperatorComputer@unknown>
2019-12-31 14:45:08 -0600

    Add computeVelo method with 2D params

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/CapturePointTools.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/CapturePointTools.java
@@ -69,6 +69,25 @@ public class CapturePointTools
    }
 
    /**
+    * Computes the Instantaneous Capture Point velocity by
+    * <p>
+    *    x&#775;<sup>ICP</sup> = x&#775;<sup>CoM</sup> + 1&frasl;&omega;  x&#776;<sup>CoM</sup>
+    * </p>
+    *
+    * @param centerOfMassVelocity
+    * @param centerOfMassAcceleration
+    * @param omega0 the natural frequency &omega; =
+    *           &radic;<span style="text-decoration:overline;">&nbsp; g / z0&nbsp;</span> of the
+    *           biped.
+    * @param icpVelocityToPack
+    */
+   public static void computeCapturePointVelocity(FrameVector2DReadOnly centerOfMassVelocity, FrameVector2DReadOnly centerOfMassAcceleration,
+                                                  double omega0, FixedFrameVector2DBasics icpVelocityToPack)
+   {
+      icpVelocityToPack.scaleAdd(1.0 / omega0, centerOfMassAcceleration, centerOfMassVelocity);
+   }
+
+   /**
     * FIXME this method is probably wrong.
     * Compute the capture point acceleration given the desired capture point velocity
     *
