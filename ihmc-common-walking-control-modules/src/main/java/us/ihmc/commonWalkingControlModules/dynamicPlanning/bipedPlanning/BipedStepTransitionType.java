d07e1eeeefbeb1887b0c66afb6b8dc05e06a9a5c
Robert <rgriffin@ihmc.us>
2020-01-09 11:58:06 -0600

    pulled in the relatively stable biped planning stuff

---
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/bipedPlanning/BipedStepTransitionType.java
@@ -0,0 +1,9 @@
+package us.ihmc.commonWalkingControlModules.dynamicPlanning.bipedPlanning;
+
+/**
+ * Defines the contact even transition type. This is useful for converting {@link BipedTimedStep} to {@link SimpleBipedContactPhase}.
+ */
+public enum BipedStepTransitionType
+{
+   LIFT_OFF, TOUCH_DOWN
+}
