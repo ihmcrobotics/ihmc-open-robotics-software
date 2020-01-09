8a15474108eb986cd0808597ec7b468e4e56fb26
Operator Computer <OperatorComputer@unknown>
2020-01-09 17:29:35 -0600

    Add AtlasSensorSuiteLauncher.

---
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSensorSuiteLauncher.java
@@ -0,0 +1,40 @@
+package us.ihmc.atlas.sensors;
+
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.networkProcessor.modules.RosModule;
+import us.ihmc.communication.configuration.NetworkParameters;
+
+import java.io.IOException;
+import java.net.URI;
+
+import static us.ihmc.pubsub.DomainFactory.PubSubImplementation.FAST_RTPS;
+
+public class AtlasSensorSuiteLauncher
+{
+   public AtlasSensorSuiteLauncher()
+   {
+      AtlasRobotModel atlasRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, RobotTarget.REAL_ROBOT);
+
+      AtlasSensorSuiteManager atlasSensorSuiteManager = (AtlasSensorSuiteManager) atlasRobotModel.getSensorSuiteManager();
+      URI rosCoreURI = NetworkParameters.getROSURI();
+      atlasSensorSuiteManager.initializePhysicalSensors(rosCoreURI);
+
+      new RosModule(atlasRobotModel, rosCoreURI, null, FAST_RTPS);
+
+      try
+      {
+         atlasSensorSuiteManager.connect();
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
+
+   public static void main(String[] args)
+   {
+      new AtlasSensorSuiteLauncher();
+   }
+}
