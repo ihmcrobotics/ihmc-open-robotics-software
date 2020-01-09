df8199f142f727ef020af8b5f9e56c688e248b70
inhol <ilee@ihmc.us>
2020-01-09 15:55:03 -0600

    Similarity computation is newly defined.

--- ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/mapping/EnvironmentMap.java
+++ ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/mapping/EnvironmentMap.java
@@ -6,8 +6,12 @@ import java.util.List;
 import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
 import gnu.trove.list.array.TDoubleArrayList;
 import us.ihmc.commons.Conversions;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.jOctoMap.ocTree.NormalOcTree;
 import us.ihmc.robotEnvironmentAwareness.geometry.ConcaveHullFactoryParameters;
 import us.ihmc.robotEnvironmentAwareness.planarRegion.PlanarRegionPolygonizer;
@@ -31,6 +35,9 @@ public class EnvironmentMap
 
    private static final double OCTREE_RESOLUTION = 0.02;
 
+   private static final boolean CONSIDER_TIME_DELAY = true;
+   private static final double ASSUMED_TIME_DELAY = 0.15;
+
    public EnvironmentMap(long firstTimeStamp, StereoVisionPointCloudMessage message)
    {
       initialTimeStamp = firstTimeStamp;
@@ -45,8 +52,39 @@ public class EnvironmentMap
    {
       double time = Conversions.nanosecondsToSeconds(newTimeStamp - initialTimeStamp);
       timeMap.add(time);
-      pointCloudMap.add(IhmcSLAMTools.extractPointsFromMessage(newPointCloudMessage));
-      sensorPoses.add(IhmcSLAMTools.extractSensorPoseFromMessage(newPointCloudMessage));
+
+      Point3D[] extractPointsFromMessage = IhmcSLAMTools.extractPointsFromMessage(newPointCloudMessage);
+      RigidBodyTransform extractSensorPoseFromMessage = IhmcSLAMTools.extractSensorPoseFromMessage(newPointCloudMessage);
+      if (CONSIDER_TIME_DELAY)
+      {
+         // TODO: handle if exceed previous time gap. (timeMap).
+         // TODO: implement time delayed sensor pose using ASSUMED_TIME_DELAY.
+         // TODO: linear approximation now. in future, cubic spline would be needed.
+         double timeGap = time - timeMap.get(timeMap.size() - 2);
+         double alphaFromPrevious = 1.0 - ASSUMED_TIME_DELAY / timeGap;
+
+         Point3D newPosition = new Point3D();
+         Quaternion newOrientation = new Quaternion();
+
+         RigidBodyTransformReadOnly previousSensorPose = sensorPoses.get(sensorPoses.size() - 1);
+         Tuple3DReadOnly previousSensorPosition = previousSensorPose.getTranslation();
+         Quaternion previousOrientation = new Quaternion(previousSensorPose.getRotation());
+
+         newPosition.interpolate(previousSensorPosition, extractSensorPoseFromMessage.getTranslation(), alphaFromPrevious);
+         newOrientation.interpolate(previousOrientation, new Quaternion(extractSensorPoseFromMessage.getRotation()), alphaFromPrevious);
+
+         RigidBodyTransform newSensorPose = new RigidBodyTransform(newOrientation, newPosition);
+         Point3D[] newPointCloud = IhmcSLAMTools.createConvertedPointsToWorld(extractSensorPoseFromMessage, extractPointsFromMessage, newSensorPose);
+
+         System.out.println(time + " " + alphaFromPrevious + " " + previousSensorPosition + " " + newPosition);
+         pointCloudMap.add(newPointCloud);
+         sensorPoses.add(newSensorPose);
+      }
+      else
+      {
+         pointCloudMap.add(extractPointsFromMessage);
+         sensorPoses.add(extractSensorPoseFromMessage);
+      }
 
       octreeMap.add(EnvironmentMappingTools.computeOctreeData(newPointCloudMessage, OCTREE_RESOLUTION));
    }
