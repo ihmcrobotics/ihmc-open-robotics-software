167bc57bef415e2ef6e421961f15abacb972bce7
Duncan Calvert <dcalvert@ihmc.us>
2020-01-27 16:42:31 -0600

    Navigation behavior kinda functions.

--- ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/behaviors/NavigationBehaviorUI.java
+++ ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/behaviors/NavigationBehaviorUI.java
@@ -6,16 +6,30 @@ import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.humanoidBehaviors.navigation.NavigationBehavior;
 import us.ihmc.humanoidBehaviors.ui.BehaviorUIDefinition;
 import us.ihmc.humanoidBehaviors.ui.BehaviorUIInterface;
+import us.ihmc.humanoidBehaviors.ui.graphics.BodyPathPlanGraphic;
+import us.ihmc.humanoidBehaviors.ui.graphics.FootstepPlanGraphic;
 import us.ihmc.messager.Messager;
+
+import static us.ihmc.humanoidBehaviors.navigation.NavigationBehavior.NavigationBehaviorAPI.BodyPathPlanForUI;
+import static us.ihmc.humanoidBehaviors.navigation.NavigationBehavior.NavigationBehaviorAPI.FootstepPlanForUI;
 
 public class NavigationBehaviorUI extends BehaviorUIInterface
 {
    public static final BehaviorUIDefinition DEFINITION = new BehaviorUIDefinition(NavigationBehavior.DEFINITION, NavigationBehaviorUI::new);
 
+   private FootstepPlanGraphic footstepPlanGraphic;
+   private BodyPathPlanGraphic bodyPathPlanGraphic;
+
    @Override
    public void init(SubScene sceneNode, Messager behaviorMessager, DRCRobotModel robotModel)
    {
+      footstepPlanGraphic = new FootstepPlanGraphic(robotModel);
+      getChildren().add(footstepPlanGraphic);
+      behaviorMessager.registerTopicListener(FootstepPlanForUI, footstepPlanGraphic::generateMeshesAsynchronously);
 
+      bodyPathPlanGraphic = new BodyPathPlanGraphic();
+      getChildren().add(bodyPathPlanGraphic);
+      behaviorMessager.registerTopicListener(BodyPathPlanForUI, bodyPathPlanGraphic::generateMeshesAsynchronously);
    }
 
    @FXML public void step()
