d779a136d31792a43b115fee24885058daf691f1
Duncan Calvert <dcalvert@ihmc.us>
2020-01-28 18:59:39 -0600

    Start to stub out single step snap and wiggler for look and step behavior.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBehavior.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBehavior.java
@@ -1,30 +1,58 @@
 package us.ihmc.humanoidBehaviors.lookAndStep;
 
+import org.apache.commons.lang3.tuple.Pair;
+import us.ihmc.commons.thread.Notification;
+import us.ihmc.communication.RemoteREAInterface;
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.footstepPlanning.FootstepDataMessageConverter;
+import us.ihmc.footstepPlanning.FootstepPlan;
+import us.ihmc.footstepPlanning.graphSearch.footstepSnapping.FootstepNodeSnapper;
+import us.ihmc.footstepPlanning.graphSearch.footstepSnapping.SimplePlanarRegionFootstepNodeSnapper;
+import us.ihmc.footstepPlanning.graphSearch.nodeChecking.FootstepNodeChecker;
+import us.ihmc.footstepPlanning.graphSearch.nodeChecking.FootstepNodeCheckerOfCheckers;
+import us.ihmc.footstepPlanning.graphSearch.nodeChecking.SnapBasedNodeChecker;
+import us.ihmc.footstepPlanning.graphSearch.parameters.DefaultFootstepPlannerParameters;
+import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParametersBasics;
 import us.ihmc.humanoidBehaviors.BehaviorDefinition;
 import us.ihmc.humanoidBehaviors.BehaviorInterface;
 import us.ihmc.humanoidBehaviors.tools.BehaviorHelper;
+import us.ihmc.humanoidBehaviors.tools.HumanoidRobotState;
+import us.ihmc.humanoidBehaviors.tools.RemoteHumanoidRobotInterface;
 import us.ihmc.log.LogTools;
 import us.ihmc.messager.MessagerAPIFactory;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.tools.thread.PausablePeriodicThread;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static us.ihmc.humanoidBehaviors.lookAndStep.LookAndStepBehavior.LookAndStepBehaviorAPI.*;
+import static us.ihmc.humanoidBehaviors.navigation.NavigationBehavior.NavigationBehaviorAPI.FootstepPlanForUI;
 
 public class LookAndStepBehavior implements BehaviorInterface
 {
    public static final BehaviorDefinition DEFINITION = new BehaviorDefinition("Look and Step", LookAndStepBehavior::new, create());
 
    private final BehaviorHelper helper;
-   private final AtomicReference<Object> takeStep;
+   private final SideDependentList<ConvexPolygon2D> footPolygons;
+   private final Notification takeStep;
    private final PausablePeriodicThread mainThread;
+   private final RemoteREAInterface rea;
+
+   private final FootstepPlannerParametersBasics footstepPlannerParameters = new DefaultFootstepPlannerParameters();
+   private final RemoteHumanoidRobotInterface robot;
 
    public LookAndStepBehavior(BehaviorHelper helper)
    {
       this.helper = helper;
-
-      takeStep = helper.createUIInput(TakeStep, null);
-
+      footPolygons = helper.createFootPolygons();
+      rea = helper.getOrCreateREAInterface();
+      robot = helper.getOrCreateRobotInterface();
+      takeStep = helper.createUINotification(TakeStep);
       mainThread = helper.createPausablePeriodicThread(getClass(), 0.1, this::lookAndStep);
    }
 
@@ -39,7 +67,57 @@ public class LookAndStepBehavior implements BehaviorInterface
 
    private void lookAndStep()
    {
+      HumanoidRobotState latestHumanoidRobotState = robot.pollHumanoidRobotState();
 
+      FramePose3D midFeetZUp = new FramePose3D();
+      midFeetZUp.setToZero(latestHumanoidRobotState.getMidFeetZUpFrame());
+      midFeetZUp.changeFrame(ReferenceFrame.getWorldFrame());
+
+      FramePose3D finalPose = new FramePose3D();
+      finalPose.setToZero(latestHumanoidRobotState.getMidFeetZUpFrame());
+      finalPose.appendTranslation(0.2, 0.0, 0.0);
+      finalPose.changeFrame(ReferenceFrame.getWorldFrame());
+
+      RobotSide initialStanceFootSide = null;
+      FramePose3D initialStanceFootPose = null;
+      FramePose3D leftSolePose = new FramePose3D();
+      leftSolePose.setToZero(latestHumanoidRobotState.getSoleZUpFrame(RobotSide.LEFT));
+      leftSolePose.changeFrame(ReferenceFrame.getWorldFrame());
+      FramePose3D rightSolePose = new FramePose3D();
+      rightSolePose.setToZero(latestHumanoidRobotState.getSoleZUpFrame(RobotSide.RIGHT));
+      rightSolePose.changeFrame(ReferenceFrame.getWorldFrame());
+
+      if (leftSolePose.getPosition().distance(finalPose.getPosition()) <= rightSolePose.getPosition().distance(finalPose.getPosition()))
+      {
+         initialStanceFootSide = RobotSide.LEFT;
+         initialStanceFootPose = leftSolePose;
+      }
+      else
+      {
+         initialStanceFootSide = RobotSide.RIGHT;
+         initialStanceFootPose = rightSolePose;
+      }
+
+      FootstepNodeSnapper snapper = new SimplePlanarRegionFootstepNodeSnapper(footPolygons);
+
+      FootstepNodeChecker snapBasedNodeChecker = new SnapBasedNodeChecker(footstepPlannerParameters, footPolygons, snapper);
+
+
+      // nodeChecker = new FootstepNodeCheckerOfCheckers(Arrays.asList(snapBasedNodeChecker, bodyCollisionNodeChecker, cliffAvoider));
+      FootstepNodeCheckerOfCheckers nodeChecker = new FootstepNodeCheckerOfCheckers(Arrays.asList(snapBasedNodeChecker));
+
+      FootstepPlan footstepPlan = new FootstepPlan();
+
+      // send footstep plan to UI
+      helper.publishToUI(FootstepPlanForUI, FootstepDataMessageConverter.reduceFootstepPlanForUIMessager(footstepPlan));
+
+
+      if (takeStep.poll())
+      {
+         // take step
+
+
+      }
    }
 
    public static class LookAndStepBehaviorAPI
@@ -49,6 +127,7 @@ public class LookAndStepBehavior implements BehaviorInterface
       private static final MessagerAPIFactory.CategoryTheme LookAndStepTheme = apiFactory.createCategoryTheme("LookAndStep");
 
       public static final MessagerAPIFactory.Topic<Object> TakeStep = topic("TakeStep");
+      public static final MessagerAPIFactory.Topic<ArrayList<Pair<RobotSide, Pose3D>>> FootstepPlanForUI = topic("FootstepPlan");
 
       private static final <T> MessagerAPIFactory.Topic<T> topic(String name)
       {
