dbb0753ba6c783f8fcdc0bf6c90435306abb1db3
Valkyrie <valkyrie@nasa.gov>
2020-02-10 11:47:53 -0600

    Added quality calculation for 1-DoF joint commands.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsToolboxModule/KinematicsSolutionQualityCalculator.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsToolboxModule/KinematicsSolutionQualityCalculator.java
@@ -10,6 +10,7 @@ import us.ihmc.commonWalkingControlModules.controllerCore.FeedbackControllerData
 import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController.CenterOfMassFeedbackControlCommand;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController.FeedbackControlCommand;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController.FeedbackControlCommandList;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController.OneDoFJointFeedbackControlCommand;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackController.SpatialFeedbackControlCommand;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.inverseDynamics.SpatialAccelerationCommand;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
@@ -63,6 +64,8 @@ public class KinematicsSolutionQualityCalculator
             case TASKSPACE:
                error += calculateCommandQuality((SpatialFeedbackControlCommand) command, feedbackControllerDataHolder);
                break;
+            case JOINTSPACE:
+               error += calculateCommandQuality((OneDoFJointFeedbackControlCommand) command, feedbackControllerDataHolder);
             default:
                throw new RuntimeException("The following command is not handled: " + command.getClass());
          }
@@ -154,5 +157,10 @@ public class KinematicsSolutionQualityCalculator
 
       // Returning the Euclidean norm of the error computed as the command quality
       return NormOps.normP2(errorSubspace);
+   }
+
+   private double calculateCommandQuality(OneDoFJointFeedbackControlCommand command, FeedbackControllerDataReadOnly feedbackControllerDataHolder)
+   {
+      return Math.abs(command.getJoint().getQ() - command.getReferencePosition()) * command.getWeightForSolver();
    }
 }
