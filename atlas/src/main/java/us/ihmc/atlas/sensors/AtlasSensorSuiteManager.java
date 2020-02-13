8a2ffce14e4cef749330a5fd78890adb55a8eac6
inhol <ilee@ihmc.us>
2020-02-13 14:53:33 -0600

    Implemented body bounding box filter.

--- atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSensorSuiteManager.java
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSensorSuiteManager.java
@@ -81,6 +81,7 @@ public class AtlasSensorSuiteManager implements DRCSensorSuiteManager
                                                                  rcdTopicName,
                                                                  rosClockCalculator,
                                                                  USE_DEPTH_FRAME_ESTIMATED_BY_TRACKING);
+      pointCloudSensorManager.setCollisionBoxProvider(collisionBoxProvider);
    }
 
    @Override
