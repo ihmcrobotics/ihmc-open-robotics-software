8d3f353abfb2a006949bfbda8d3175872e7ac2d2
Duncan Calvert <dcalvert@ihmc.us>
2020-02-24 16:58:16 -0600

    Fix up/restructure ROS tools

--- ihmc-ros-tools/build.gradle.kts
+++ ihmc-ros-tools/build.gradle.kts
@@ -9,10 +9,9 @@ ihmc {
    loadProductProperties("../product.properties")
    
    configureDependencyResolution()
+   javaDirectory("main", "generated-java")
    configurePublications()
 }
-
-project.sourceSets.main.java.srcDirs += ["generated-src"]
 
 mainDependencies {
    api("io.netty:netty:3.5.8.Final")
@@ -21,15 +20,20 @@ mainDependencies {
    api("org.apache.commons:commons-lang3:3.8.1")
    api("org.boofcv:geo:0.24.1")
    api("org.reflections:reflections:0.9.10")
-   api("org.ros.rosjava_core:rosjava:0.2.1")
+   api("org.ros.rosjava_core:rosjava:0.2.1") {
+      exclude("junit:junit")
+   }
    api("org.ros.rosjava_bootstrap:message_generation:0.2.1")
    api("org.ros.rosjava_messages:std_msgs:0.5.9")
+   api("org.ros.rosjava_messages:std_srvs:1.11.1")
+   api("org.ros.rosjava_messages:people_msgs:1.0.4")
    api("org.ros.rosjava_messages:test_rosmaster:1.11.10")
    api("org.ros.rosjava_messages:sensor_msgs:1.11.7")
    api("org.ros.rosjava_messages:dynamic_reconfigure:1.5.38")
    api("org.ros.rosjava_messages:multisense_ros:3.4.2")
    api("org.ros.rosjava_messages:rosgraph_msgs:1.11.1")
    api("org.ros.rosjava_messages:geometry_msgs:1.11.7")
+   api("org.ros.rosjava_messages:trajectory_msgs:1.11.7")
    api("org.ros.rosjava_messages:nav_msgs:1.11.7")
    api("org.ros.rosjava_messages:tf2_msgs:0.5.9")
    api("org.ros.rosjava_messages:tf:1.10.8")
@@ -42,7 +46,6 @@ mainDependencies {
 }
 
 testDependencies {
-
    api("us.ihmc:ihmc-commons-testing:0.26.6")
    api("us.ihmc:ihmc-robotics-toolkit-test:source")
 }
