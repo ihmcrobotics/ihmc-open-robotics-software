9477e690efe824703e693e4ecf44e6cb052bd00c
Duncan Calvert <dcalvert@ihmc.us>
2020-02-24 18:13:45 -0600

    Create a planar regions expiration condition.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBehaviorParameters.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBehaviorParameters.java
@@ -10,11 +10,17 @@ public class LookAndStepBehaviorParameters extends StoredPropertySet
 
    public static final DoubleStoredPropertyKey stepLength = keys.addDoubleKey("Step length");
    public static final DoubleStoredPropertyKey stepWidth = keys.addDoubleKey("Step width");
+   public static final DoubleStoredPropertyKey planarRegionsExpiration = keys.addDoubleKey("Planar regions expiration");
 
    public LookAndStepBehaviorParameters()
    {
       super(keys, LookAndStepBehaviorParameters.class, "ihmc-open-robotics-software", "ihmc-avatar-interfaces/src/behavior-clean-room/resources");
       load();
+   }
+
+   public double getPlanarRegionsExpiration()
+   {
+      return get(planarRegionsExpiration);
    }
 
    public static void main(String[] args)
