110fc9e50c5cf8af65429d39951a7355267ed860
Sylvain Bertrand <sbertrand@ihmc.us>
2020-02-26 17:06:44 -0600

    :arrow_up: ihmc-javafx-toolkit 0.14.0.

--- atlas/build.gradle.kts
+++ atlas/build.gradle.kts
@@ -38,7 +38,7 @@ mainDependencies {
    api("us.ihmc:simulation-construction-set:0.13.0")
    api("us.ihmc:ihmc-graphics-description:0.14.0")
    api("us.ihmc:ihmc-robot-description:0.13.0")
-   api("us.ihmc:ihmc-javafx-toolkit:0.13.0")
+   api("us.ihmc:ihmc-javafx-toolkit:0.14.0")
    api("us.ihmc:ihmc-humanoid-behaviors:source")
    api("us.ihmc:ihmc-mocap:source")
    api("us.ihmc:ihmc-common-walking-control-modules:source")
