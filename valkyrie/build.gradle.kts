3a8d0c915d581cb03dec5bd1a527f1dca793fcb5
Sylvain Bertrand <sbertrand@ihmc.us>
2020-02-26 17:57:48 -0600

    :arrow_up: simulation-construction-set 0.14.0.

--- valkyrie/build.gradle.kts
+++ valkyrie/build.gradle.kts
@@ -31,7 +31,7 @@ mainDependencies {
    }
    api("us.ihmc:ihmc-commons:0.26.6")
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.14.0")
-   api("us.ihmc:simulation-construction-set:0.13.0")
+   api("us.ihmc:simulation-construction-set:0.14.0")
    api("us.ihmc:ihmc-graphics-description:0.14.1")
    api("us.ihmc:ihmc-robot-description:0.14.1")
    api("us.ihmc:ihmc-communication:source")
