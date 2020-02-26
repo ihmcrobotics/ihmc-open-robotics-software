58c8aba45951801f240e40940d703c7393b2d395
Sylvain Bertrand <sbertrand@ihmc.us>
2020-02-26 17:57:48 -0600

    :arrow_up: simulation-construction-set 0.14.0.

--- ihmc-robot-models/build.gradle.kts
+++ ihmc-robot-models/build.gradle.kts
@@ -34,5 +34,5 @@ visualizersDependencies {
    api("us.ihmc:ihmc-interfaces:source")
    api("us.ihmc:ihmc-java-toolkit:source")
    api("us.ihmc:simulation-construction-set-tools:source")
-   api("us.ihmc:simulation-construction-set:0.13.0")
+   api("us.ihmc:simulation-construction-set:0.14.0")
 }
