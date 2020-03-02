2cdd8d99ef00246ef6eebb99a471b5d9af4e2642
Duncan Calvert <dcalvert@ihmc.us>
2020-03-02 17:23:20 -0600

    Add tool to null check behavior tree status.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/behaviorTree/BehaviorTreeNode.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/behaviorTree/BehaviorTreeNode.java
@@ -3,4 +3,12 @@ package us.ihmc.humanoidBehaviors.tools.behaviorTree;
 public interface BehaviorTreeNode
 {
    BehaviorTreeNodeStatus tick();
+
+   public static void checkStatusInNotNull(BehaviorTreeNodeStatus status)
+   {
+      if (status == null)
+      {
+         throw new RuntimeException("Behavior tree node status must not be null.");
+      }
+   }
 }
