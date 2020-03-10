c8fab507cb53f9f88c9db6f87c349298342b9656
Robert <rgriffin@ihmc.us>
2020-03-10 08:57:01 -0500

    started working on a simple occupancy grid for the cop history

---
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/occupancyGrid/OccupancyGrid.java
@@ -0,0 +1,117 @@
+package us.ihmc.robotics.occupancyGrid;
+
+import com.esotericsoftware.kryo.util.IntMap;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
+import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
+import us.ihmc.yoVariables.listener.VariableChangedListener;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
+import us.ihmc.yoVariables.variable.YoDouble;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class OccupancyGrid
+{
+   private final YoDouble decayRate;
+
+   private final YoDouble cellXSize;
+   private final YoDouble cellYSize;
+   private final YoDouble cellArea;
+
+   private final YoDouble thresholdForCellActivation;
+
+   private final ReferenceFrame gridFrame;
+
+   private final AtomicBoolean resetOccupancyGrid = new AtomicBoolean();
+
+   private final IntMap<OccupancyGridCell> occupancyCellMap = new IntMap<>();
+   private final List<OccupancyGridCell> allCellsPool = new ArrayList<>();
+   private final List<OccupancyGridCell> allActiveCells = new ArrayList<>();
+
+   public OccupancyGrid(String namePrefix, ReferenceFrame gridFrame, YoVariableRegistry parentRegistry)
+   {
+      this.gridFrame = gridFrame;
+
+      String name = getClass().getSimpleName();
+      YoVariableRegistry registry = new YoVariableRegistry(namePrefix + name);
+
+      cellXSize = new YoDouble(namePrefix + "CellXSize", registry);
+      cellYSize = new YoDouble(namePrefix + "CellYSize", registry);
+      cellArea = new YoDouble(namePrefix + "CellArea", registry);
+
+      thresholdForCellActivation = new YoDouble(namePrefix + "ThresholdForCellOccupancy", registry);
+      decayRate = new YoDouble(namePrefix + "CellOccupancyDecayRate", registry);
+
+      setupChangedGridParameterListeners();
+
+      parentRegistry.addChild(registry);
+   }
+
+   private void setupChangedGridParameterListeners()
+   {
+      VariableChangedListener changedGridSizeListener = (v) ->
+      {
+         cellArea.set(cellXSize.getDoubleValue() * cellYSize.getDoubleValue());
+      };
+      cellYSize.addVariableChangedListener(changedGridSizeListener);
+      cellXSize.addVariableChangedListener(changedGridSizeListener);
+      changedGridSizeListener.notifyOfVariableChange(null);
+   }
+
+   public void reset()
+   {
+      allActiveCells.clear();
+      for (int i = 0; i < allCellsPool.size(); i++)
+         allCellsPool.get(i).reset();
+   }
+
+   public void update()
+   {
+      if (resetOccupancyGrid.getAndSet(false))
+         reset();
+
+      for (int i = 0; i < allCellsPool.size(); i++)
+         allCellsPool.get(i).update();
+   }
+
+   public void registerPoint(FramePoint2DReadOnly point)
+   {
+      point.checkReferenceFrameMatch(gridFrame);
+      getOrCreateOccupancyGridCell(point).registerHit();
+   }
+
+   private int findXIndex(double x)
+   {
+      return (int) Math.floor(x / cellXSize.getDoubleValue());
+   }
+
+   private int findYIndex(double y)
+   {
+      return (int) Math.floor(y / cellYSize.getDoubleValue());
+   }
+
+   private OccupancyGridCell getOrCreateOccupancyGridCell(Point2DReadOnly pointInGrid)
+   {
+      int xIndex = findXIndex(pointInGrid.getX());
+      int yIndex = findYIndex(pointInGrid.getY());
+
+      return getOrCreateOccupancyGridCell(xIndex, yIndex);
+   }
+
+   private OccupancyGridCell getOrCreateOccupancyGridCell(int xIndex, int yIndex)
+   {
+      int hashCode = OccupancyGridCell.computeHashCode(xIndex, yIndex);
+      OccupancyGridCell cell = occupancyCellMap.get(hashCode);
+      if (cell == null)
+      {
+         cell = new OccupancyGridCell(xIndex, yIndex, thresholdForCellActivation, decayRate);
+         allCellsPool.add(cell);
+      }
+      if (!allActiveCells.contains(cell))
+         allActiveCells.add(cell);
+
+      return cell;
+   }
+}
---
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/occupancyGrid/OccupancyGridCell.java
@@ -0,0 +1,123 @@
+package us.ihmc.robotics.occupancyGrid;
+
+import us.ihmc.yoVariables.providers.DoubleProvider;
+import us.ihmc.yoVariables.providers.IntegerProvider;
+
+public class OccupancyGridCell
+{
+   private DoubleProvider occupancyThreshold = () -> 1;
+   private DoubleProvider occupancyDecayRate = () -> 1.0;
+   private boolean isOccupied = false;
+   private double numberOfHits = 0.0;
+
+   private final int xIndex;
+   private final int yIndex;
+
+   private final int hashCode;
+
+   public OccupancyGridCell(int xIndex, int yIndex)
+   {
+      this.xIndex = xIndex;
+      this.yIndex = yIndex;
+
+      hashCode = computeHashCode(this);
+   }
+
+   public OccupancyGridCell(int xIndex, int yIndex, DoubleProvider occupancyThreshold, DoubleProvider occupancyDecayRate)
+   {
+      this(xIndex, yIndex);
+      setOccupancyThreshold(occupancyThreshold);
+      setOccupancyDecayRate(occupancyDecayRate);
+   }
+
+   public boolean getIsOccupied()
+   {
+      return isOccupied;
+   }
+
+   public int getXIndex()
+   {
+      return xIndex;
+   }
+
+   public int getYIndex()
+   {
+      return yIndex;
+   }
+
+   public void reset()
+   {
+      isOccupied = false;
+      numberOfHits = 0.0;
+   }
+
+   public void setOccupancyThreshold(DoubleProvider occupancyThreshold)
+   {
+      this.occupancyThreshold = occupancyThreshold;
+   }
+
+   public void setOccupancyDecayRate(DoubleProvider occupancyDecayRate)
+   {
+      this.occupancyDecayRate = occupancyDecayRate;
+   }
+
+   public void update()
+   {
+      if (occupancyDecayRate.getValue() >= 1.0)
+         return;
+
+      numberOfHits *= occupancyDecayRate.getValue();
+
+      updateOccupancyEstimate();
+   }
+
+   public void registerHit()
+   {
+      numberOfHits++;
+      updateOccupancyEstimate();
+   }
+
+   private void updateOccupancyEstimate()
+   {
+      isOccupied = numberOfHits > occupancyThreshold.getValue();
+   }
+
+   public static int computeHashCode(OccupancyGridCell cell)
+   {
+      return computeHashCode(cell.xIndex, cell.yIndex);
+   }
+
+   public static int computeHashCode(int xIndex, int yIndex)
+   {
+      int result = 1;
+      int prime = 31;
+      result += prime * xIndex;
+      result += prime * yIndex;
+
+      return result;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      return hashCode;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (obj == null)
+         return false;
+      else if (obj == this)
+         return true;
+
+      if (getClass() != obj.getClass())
+         return false;
+      OccupancyGridCell other = (OccupancyGridCell) obj;
+      if (xIndex != other.xIndex)
+         return false;
+      if (yIndex != other.yIndex)
+         return false;
+      return true;
+   }
+}
