a549e1e3b6f7d4a20ad3ad7d5ebe90d8788ff4ec
Duncan Calvert <dcalvert@ihmc.us>
2020-03-16 12:41:41 -0500

    Refactor test wait methods to better names.

---
+++ atlas/src/test/java/us/ihmc/atlas/behaviors/AtlasLookAndStepBehaviorTest.java
@@ -0,0 +1,12 @@
+package us.ihmc.atlas.behaviors;
+
+import org.junit.jupiter.api.Test;
+
+public class AtlasLookAndStepBehaviorTest
+{
+   @Test
+   public void testLookAndStepOverStairs()
+   {
+
+   }
+}
