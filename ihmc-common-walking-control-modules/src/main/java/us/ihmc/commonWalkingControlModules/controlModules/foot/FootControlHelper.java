1ee0179d5ed5d4a1d70925c3fb81288fa5352e0c
Robert <rgriffin@ihmc.us>
2020-03-23 11:21:00 -0500

    added some of the confusing method back in because it apparently did something

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/FootControlHelper.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/FootControlHelper.java
@@ -78,9 +78,10 @@ public class FootControlHelper
 
       if (walkingControllerParameters.enableLegSingularityAndKneeCollapseAvoidanceModule())
       {
-         legSingularityAndKneeCollapseAvoidanceControlModule = new LegSingularityAndKneeCollapseAvoidanceControlModule(namePrefix, contactableFoot, robotSide,
-                                                                                                                       walkingControllerParameters,
-                                                                                                                       controllerToolbox, registry);
+//         legSingularityAndKneeCollapseAvoidanceControlModule = new LegSingularityAndKneeCollapseAvoidanceControlModule(namePrefix, contactableFoot, robotSide,
+//                                                                                                                       walkingControllerParameters,
+//                                                                                                                       controllerToolbox, registry);
+         legSingularityAndKneeCollapseAvoidanceControlModule = null;
          workspaceLimiterControlModule = new WorkspaceLimiterControlModule(namePrefix, contactableFoot, robotSide, walkingControllerParameters, controllerToolbox, registry);
       }
       else
