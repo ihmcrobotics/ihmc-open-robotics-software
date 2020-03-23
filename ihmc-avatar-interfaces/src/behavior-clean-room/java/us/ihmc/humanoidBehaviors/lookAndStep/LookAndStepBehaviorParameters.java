61de01a9f711f1638ed78bdc7de43dfeac950f97
Duncan Calvert <dcalvert@ihmc.us>
2020-03-23 17:52:37 -0500

    Add direction control.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBehaviorParameters.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBehaviorParameters.java
@@ -9,6 +9,7 @@ public class LookAndStepBehaviorParameters extends StoredPropertySet
 {
    public static final StoredPropertyKeyList keys = new StoredPropertyKeyList();
 
+   public static final DoubleStoredPropertyKey direction = keys.addDoubleKey("Direction");
    public static final DoubleStoredPropertyKey stepLength = keys.addDoubleKey("Step length");
    public static final DoubleStoredPropertyKey idealFootstepLengthOverride = keys.addDoubleKey("Ideal footstep length override");
    public static final DoubleStoredPropertyKey wiggleInsideDeltaOverride = keys.addDoubleKey("Wiggle inside delta override");
