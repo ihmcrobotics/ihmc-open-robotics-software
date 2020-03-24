424a36c7ec6d2dd62a4c130db7b5b782a0fd2421
smccrory <smccrory@ihmc.us>
2020-03-23 23:11:26 -0500

    Removing unused classes

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/listeners/StartAndGoalListener.java
+++
@@ -1,9 +0,0 @@
-package us.ihmc.footstepPlanning.graphSearch.listeners;
-
-import us.ihmc.euclid.referenceFrame.FramePose3D;
-
-public interface StartAndGoalListener
-{
-   void setInitialPose(FramePose3D startPose);
-   void setGoalPose(FramePose3D goalPose);
-}
