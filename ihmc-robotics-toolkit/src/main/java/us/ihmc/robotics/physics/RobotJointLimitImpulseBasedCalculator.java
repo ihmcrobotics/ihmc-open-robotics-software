95740dc1c8d05ae74dfd256460b48e12a05e8b71
Sylvain Bertrand <sbertrand@ihmc.us>
2020-03-24 11:37:20 -0500

    Reduced spring constant for joint limits.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/physics/RobotJointLimitImpulseBasedCalculator.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/physics/RobotJointLimitImpulseBasedCalculator.java
@@ -38,7 +38,7 @@ public class RobotJointLimitImpulseBasedCalculator implements ImpulseBasedConstr
       }
    };
 
-   private double springConstant = 5.0;
+   private double springConstant = 0.1;
    private final List<OneDoFJointBasics> jointsAtLimit = new ArrayList<>();
    private final List<ActiveLimit> activeLimits = new ArrayList<>();
 
