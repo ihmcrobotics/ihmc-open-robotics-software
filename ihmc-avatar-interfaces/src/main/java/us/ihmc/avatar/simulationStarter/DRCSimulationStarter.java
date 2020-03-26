8853fa9429ea9bb89be39bc3296ff9e54844fef8
Sylvain Bertrand <sbertrand@ihmc.us>
2020-03-26 12:03:03 -0500

    Got rid of the unnecessary flag.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/simulationStarter/DRCSimulationStarter.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/simulationStarter/DRCSimulationStarter.java
@@ -496,7 +496,6 @@ public class DRCSimulationStarter implements SimulationStarterInterface
       avatarSimulationFactory.setGuiInitialSetup(guiInitialSetup);
       avatarSimulationFactory.setRealtimeRos2Node(realtimeRos2Node);
       avatarSimulationFactory.setCreateYoVariableServer(createYoVariableServer);
-      avatarSimulationFactory.setUseExperimentalSimulationPhysicsEngine(scsInitialSetup.getUseExperimentalPhysicsEngine());
       if (externalPelvisCorrectorSubscriber != null)
          avatarSimulationFactory.setExternalPelvisCorrectorSubscriber(externalPelvisCorrectorSubscriber);
       AvatarSimulation avatarSimulation = avatarSimulationFactory.createAvatarSimulation();
