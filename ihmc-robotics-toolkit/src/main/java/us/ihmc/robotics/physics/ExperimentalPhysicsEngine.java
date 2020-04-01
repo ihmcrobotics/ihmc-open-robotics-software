a79150e81f9ff2b4612fe2d0a47230bb8cea2365
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-01 11:04:52 -0500

    Added visualization of the colliding shapes.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/physics/ExperimentalPhysicsEngine.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/physics/ExperimentalPhysicsEngine.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
+import us.ihmc.graphicsDescription.appearance.YoAppearance;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
@@ -29,11 +31,12 @@ import us.ihmc.yoVariables.registry.YoVariableRegistry;
  * Letters 3.2 (2018): 895-902.
  * </ul>
  * </p>
- * 
+ *
  * @author Sylvain Bertrand
  */
 public class ExperimentalPhysicsEngine
 {
+   private static final String collidableVisualizerGroupName = "Physics Engine - Active Collidable";
    private final ReferenceFrame rootFrame = ReferenceFrame.getWorldFrame();
 
    private final YoVariableRegistry physicsEngineRegistry = new YoVariableRegistry("PhysicsPlugins");
@@ -47,12 +50,21 @@ public class ExperimentalPhysicsEngine
    private final MultiRobotForwardDynamicsPlugin multiRobotPhysicsEnginePlugin;
    private final MultiRobotFirstOrderIntegrator integrationMethod = new MultiRobotFirstOrderIntegrator();
 
+   private final CollidableListVisualizer environmentCollidableVisualizers;
+   private final List<CollidableListVisualizer> robotCollidableVisualizers = new ArrayList<>();
+
    private boolean initialize = true;
 
    public ExperimentalPhysicsEngine()
    {
       collisionDetectionPlugin = new SimpleCollisionDetection(rootFrame);
       multiRobotPhysicsEnginePlugin = new MultiRobotForwardDynamicsPlugin(rootFrame, physicsEngineRegistry);
+      AppearanceDefinition environmentCollidableAppearance = YoAppearance.AluminumMaterial();
+      environmentCollidableAppearance.setTransparency(0.5);
+      environmentCollidableVisualizers = new CollidableListVisualizer(collidableVisualizerGroupName,
+                                                                      environmentCollidableAppearance,
+                                                                      physicsEngineRegistry,
+                                                                      physicsEngineGraphicsRegistry);
    }
 
    public void addRobot(String robotName, RigidBodyBasics rootBody, MultiBodySystemStateWriter controllerOutputWriter,
@@ -69,6 +81,14 @@ public class ExperimentalPhysicsEngine
       integrationMethod.addMultiBodySystem(robot.getMultiBodySystem());
       physicsOutputReader.setMultiBodySystem(robot.getMultiBodySystem());
       physicsOutputReaders.add(physicsOutputReader);
+      AppearanceDefinition robotCollidableAppearance = YoAppearance.DarkGreen();
+      robotCollidableAppearance.setTransparency(0.5);
+      CollidableListVisualizer collidableVisualizers = new CollidableListVisualizer(collidableVisualizerGroupName,
+                                                                                    robotCollidableAppearance,
+                                                                                    robot.getRobotRegistry(),
+                                                                                    physicsEngineGraphicsRegistry);
+      robot.getCollidables().forEach(collidableVisualizers::addCollidable);
+      robotCollidableVisualizers.add(collidableVisualizers);
       physicsEngineRegistry.addChild(robot.getRobotRegistry());
       robotList.add(robot);
    }
@@ -110,6 +130,9 @@ public class ExperimentalPhysicsEngine
       multiRobotPhysicsEnginePlugin.doScience(dt, gravity);
       integrationMethod.integrate(dt);
 
+      environmentCollidableVisualizers.update(collisionDetectionPlugin.getAllCollisions());
+      robotCollidableVisualizers.forEach(visualizer -> visualizer.update(collisionDetectionPlugin.getAllCollisions()));
+
       for (int i = 0; i < robotList.size(); i++)
       {
          PhysicsEngineRobotData robot = robotList.get(i);
@@ -121,6 +144,7 @@ public class ExperimentalPhysicsEngine
    public void addEnvironmentCollidables(Collection<? extends Collidable> collidables)
    {
       environmentCollidables.addAll(collidables);
+      collidables.forEach(environmentCollidableVisualizers::addCollidable);
    }
 
    public YoVariableRegistry getPhysicsEngineRegistry()
