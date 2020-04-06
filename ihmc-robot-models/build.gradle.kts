12e5cefe2e06b29a7251572b936e5ba1c43c50c0
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-06 17:34:27 -0500

    Added javafx toolkit to robot-models-visualizers.

--- ihmc-robot-models/build.gradle.kts
+++ ihmc-robot-models/build.gradle.kts
@@ -34,5 +34,6 @@ visualizersDependencies {
    api("us.ihmc:ihmc-interfaces:source")
    api("us.ihmc:ihmc-java-toolkit:source")
    api("us.ihmc:simulation-construction-set-tools:source")
+   api("us.ihmc:ihmc-javafx-toolkit:0.15.0")
    api("us.ihmc:simulation-construction-set:0.15.0")
 }
