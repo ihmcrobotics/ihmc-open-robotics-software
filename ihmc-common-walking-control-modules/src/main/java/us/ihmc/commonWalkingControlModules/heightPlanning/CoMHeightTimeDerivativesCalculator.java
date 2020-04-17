fa844b981f9de9a4ab7589fcc15de434216be1fb
Robert <rgriffin@ihmc.us>
2020-04-17 15:58:27 -0500

    set up the calculation of the com height jerk from the spline, which is probably zero, but added it as a feed forward value

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/heightPlanning/CoMHeightTimeDerivativesCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/heightPlanning/CoMHeightTimeDerivativesCalculator.java
@@ -30,6 +30,10 @@ public class CoMHeightTimeDerivativesCalculator
       double d2zDx2 = comPartialDerivatives.getPartialD2zDx2();
       double d2zDy2 = comPartialDerivatives.getPartialD2zDy2();
       double d2zDxDy = comPartialDerivatives.getPartialD2zDxDy();
+      double d3zDx3 = 0.0;
+      double d3zDy3 = 0.0;
+      double d3zDx2Dy = 0.0;
+      double d3zDxDy2 = 0.0;
 
       double xDot = comVelocity.getX();
       double yDot = comVelocity.getY();
@@ -37,11 +41,18 @@ public class CoMHeightTimeDerivativesCalculator
       double xDDot = comAcceleration.getX();
       double yDDot = comAcceleration.getY();
 
+      double xDDDot = 0.0;
+      double yDDDot = 0.0;
+
       double comHeightVelocity = dzDx * xDot + dzDy * yDot;
       double comHeightAcceleration = d2zDx2 * xDot * xDot + dzDx * xDDot + d2zDy2 * yDot * yDot + dzDy * yDDot + 2.0 * d2zDxDy * xDot * yDot;
+      double comHeightJerk =
+            d3zDx3 * (xDot * xDot * xDot) + d3zDy3 * (yDot * yDot * yDot) + dzDx * xDDDot + dzDy * yDDDot + 3.0 * d3zDx2Dy * (xDot * xDot * yDot)
+            + 3.0 * d3zDxDy2 * (xDot * yDot * yDot) + 3.0 * d2zDx2 * xDot * xDDot + 3.0 * d2zDy2 * yDot * yDDot + 3.0 * d2zDxDy * xDDot * yDot + xDot * yDDot;
 
       comHeightDataToPack.setComHeight(comPartialDerivatives.getFrameOfCoMHeight(), comPartialDerivatives.getComHeight());
       comHeightDataToPack.setComHeightVelocity(comHeightVelocity);
       comHeightDataToPack.setComHeightAcceleration(comHeightAcceleration);
+      comHeightDataToPack.setComHeightJerk(comHeightJerk);
    }
 }
