b3e942e9d17a7c0b082eff6458cca3caca2f93b5
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-24 12:29:58 -0500

    Added factory to create a state writer for a single joint.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/physics/MultiBodySystemStateWriter.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/physics/MultiBodySystemStateWriter.java
@@ -1,5 +1,8 @@
 package us.ihmc.robotics.physics;
 
+import java.util.function.Consumer;
+
+import us.ihmc.mecano.multiBodySystem.interfaces.JointBasics;
 import us.ihmc.mecano.multiBodySystem.interfaces.MultiBodySystemBasics;
 
 public interface MultiBodySystemStateWriter
@@ -7,4 +10,25 @@ public interface MultiBodySystemStateWriter
    void setMultiBodySystem(MultiBodySystemBasics multiBodySystem);
 
    void write();
+
+   public static <T extends JointBasics> MultiBodySystemStateWriter singleJointStateWriter(String jointName, Consumer<T> jointStateWriter)
+   {
+      return new MultiBodySystemStateWriter()
+      {
+         private T joint;
+         
+         @Override
+         public void write()
+         {
+            jointStateWriter.accept(joint);
+         }
+         
+         @SuppressWarnings("unchecked")
+         @Override
+         public void setMultiBodySystem(MultiBodySystemBasics multiBodySystem)
+         {
+            joint = (T) RobotCollisionModel.findJoint(jointName, multiBodySystem);
+         }
+      };
+   }
 }
