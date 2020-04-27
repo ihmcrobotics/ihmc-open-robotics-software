5b827721aadd22181f4c1aeae157c12987e974b2
Robert <rgriffin@ihmc.us>
2020-04-27 14:46:53 -0500

    cleaned up the icp control plane

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/ICPControlPlane.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/ICPControlPlane.java
@@ -2,13 +2,16 @@ package us.ihmc.commonWalkingControlModules.capturePoint;
 
 import us.ihmc.commons.MathTools;
 import us.ihmc.commons.lists.RecyclingArrayList;
-import us.ihmc.euclid.geometry.BoundingBox3D;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DBasics;
+import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DReadOnly;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.referenceFrame.*;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DBasics;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
+import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple2D.interfaces.Point2DBasics;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.robotics.geometry.ConvexPolygonScaler;
@@ -27,13 +30,16 @@ public class ICPControlPlane
    private final ReferenceFrame centerOfMassFrame;
 
    private final RecyclingArrayList<Point3D> convexHullInWorld = new RecyclingArrayList<>(Point3D::new);
+   private final RecyclingArrayList<Point2DBasics> pointsInPlane = new RecyclingArrayList<>(Point2D::new);
 
    private final FramePoint3D tempFramePoint = new FramePoint3D();
    private final FramePoint3D tempProjectedFramePoint = new FramePoint3D();
 
+   private final FramePoint3D planeOrigin = new FramePoint3D();
+   private final FrameVector3D planeNormal = new FrameVector3D();
+
    private final FramePoint3D centerOfMassPosition = new FramePoint3D();
    private final FrameVector3D rayDirection = new FrameVector3D();
-   private final FramePoint3D intersectionToThrowAway = new FramePoint3D();
 
    private final ConvexPolygonScaler scaler = new ConvexPolygonScaler();
 
@@ -63,15 +69,12 @@ public class ICPControlPlane
 
    public void projectPointOntoControlPlane(ReferenceFrame desiredReferenceFrame, FramePoint3DReadOnly pointToProject, FramePoint3D projectionToPack)
    {
-      tempFramePoint.setIncludingFrame(pointToProject);
-      tempFramePoint.changeFrame(centerOfMassFrame);
-
-      double heightOfUnprojectedPoint = tempFramePoint.getZ();
-      projectPointThroughReferenceFrame(centerOfMassFrame, tempFramePoint, projectionToPack, controlPlaneHeight.getDoubleValue(), heightOfUnprojectedPoint);
+      projectPointThroughReferenceFrame(centerOfMassFrame, pointToProject, projectionToPack, controlPlaneHeight.getDoubleValue());
 
       projectionToPack.changeFrame(desiredReferenceFrame);
    }
 
+   // FIXME clean up?
    public void projectPointFromControlPlaneOntoSurface(ReferenceFrame desiredReferenceFrame,
                                                        FramePoint2DReadOnly pointToProject,
                                                        FramePoint3DBasics projectionToPack,
@@ -79,15 +82,21 @@ public class ICPControlPlane
    {
       tempFramePoint.setIncludingFrame(pointToProject, 0.0);
       tempFramePoint.changeFrame(worldFrame);
+
       tempFramePoint.setZ(surfaceHeightInWorld);
       tempFramePoint.changeFrame(centerOfMassFrame);
 
       double surfaceHeightInCoMFrame = tempFramePoint.getZ();
-      projectPointThroughReferenceFrame(centerOfMassFrame, tempFramePoint, projectionToPack, surfaceHeightInCoMFrame, controlPlaneHeight.getDoubleValue());
+      tempFramePoint.setZ(controlPlaneHeight.getDoubleValue());
+
+      projectPointThroughReferenceFrame(centerOfMassFrame, tempFramePoint, projectionToPack, surfaceHeightInCoMFrame);
 
       projectionToPack.changeFrame(desiredReferenceFrame);
    }
 
+   /**
+    * Projects a point that is in the control plane to a point in the world that is on the planar region.
+    */
    public void projectPointFromControlPlaneOntoPlanarRegion(ReferenceFrame desiredReferenceFrame,
                                                             FramePoint2DReadOnly pointToProject,
                                                             FramePoint3D projectionToPack,
@@ -106,15 +115,17 @@ public class ICPControlPlane
 
       projectionToPack.setToZero(worldFrame);
 
-      BoundingBox3D boundingBoxInWorld = planarRegion.getBoundingBox3dInWorld();
-      boundingBoxInWorld.intersectionWithRay3D(centerOfMassPosition, rayDirection, projectionToPack, intersectionToThrowAway);
+      planarRegion.getOrigin(planeOrigin);
+      planarRegion.getNormal(planeNormal);
+
+      EuclidGeometryTools.intersectionBetweenLine3DAndPlane3D(planeOrigin, planeNormal, centerOfMassPosition, rayDirection, projectionToPack);
 
       projectionToPack.changeFrame(desiredReferenceFrame);
    }
 
    public void projectPlanarRegionConvexHullOntoControlPlane(PlanarRegion planarRegion, ConvexPolygon2D convexPolygonInControlPlaneToPack)
    {
-      ConvexPolygon2D convexHull = planarRegion.getConvexHull();
+      ConvexPolygon2DReadOnly convexHull = planarRegion.getConvexHull();
       convexPolygonInControlPlaneToPack.clear();
 
       convexHullInWorld.clear();
@@ -125,14 +136,19 @@ public class ICPControlPlane
          planarRegion.getTransformToWorld().transform(vertexInWorld);
       }
 
-      projectConvexPolygonOntoControlPlane(convexHullInWorld, convexPolygonInControlPlaneToPack);
+      projectPointsInWorldOntoControlPlane(convexHullInWorld, pointsInPlane);
+
+      convexPolygonInControlPlaneToPack.clear();
+      for (int i = 0; i < pointsInPlane.size(); i++)
+         convexPolygonInControlPlaneToPack.addVertex(pointsInPlane.get(i));
+      convexPolygonInControlPlaneToPack.update();
    }
 
    public void scaleAndProjectPlanarRegionConvexHullOntoControlPlane(PlanarRegion planarRegion,
                                                                      ConvexPolygon2D convexPolygonInControlPlaneToPack,
                                                                      double distanceInside)
    {
-      ConvexPolygon2D convexHull = planarRegion.getConvexHull();
+      ConvexPolygon2DReadOnly convexHull = planarRegion.getConvexHull();
       convexPolygonInControlPlaneToPack.clear();
 
       scaler.scaleConvexPolygon(convexHull, distanceInside, scaledConvexHull);
@@ -144,16 +160,20 @@ public class ICPControlPlane
          planarRegion.getTransformToWorld().transform(vertexInWorld);
       }
 
-      projectConvexPolygonOntoControlPlane(convexHullInWorld, convexPolygonInControlPlaneToPack);
+      projectPointsInWorldOntoControlPlane(convexHullInWorld, pointsInPlane);
+
+      convexPolygonInControlPlaneToPack.clear();
+      for (int i = 0; i < pointsInPlane.size(); i++)
+         convexPolygonInControlPlaneToPack.addVertex(pointsInPlane.get(i));
+      convexPolygonInControlPlaneToPack.update();
    }
 
    public void scaleAndProjectPlanarRegionConvexHullOntoControlPlane(PlanarRegion planarRegion,
-                                                                     ConvexPolygon2D footstepPolygon,
-                                                                     ConvexPolygon2D convexPolygonInControlPlaneToPack,
+                                                                     ConvexPolygon2DReadOnly footstepPolygon,
+                                                                     ConvexPolygon2DBasics convexPolygonInControlPlaneToPack,
                                                                      double distanceInside)
    {
-      ConvexPolygon2D convexHull = planarRegion.getConvexHull();
-      convexPolygonInControlPlaneToPack.clear();
+      ConvexPolygon2DReadOnly convexHull = planarRegion.getConvexHull();
 
       scaler.scaleConvexPolygonToContainInteriorPolygon(convexHull, footstepPolygon, distanceInside, scaledConvexHull);
 
@@ -164,35 +184,39 @@ public class ICPControlPlane
          planarRegion.getTransformToWorld().transform(vertexInWorld);
       }
 
-      projectConvexPolygonOntoControlPlane(convexHullInWorld, convexPolygonInControlPlaneToPack);
+      projectPointsInWorldOntoControlPlane(convexHullInWorld, pointsInPlane);
+
+      convexPolygonInControlPlaneToPack.clear();
+      for (int i = 0; i < pointsInPlane.size(); i++)
+         convexPolygonInControlPlaneToPack.addVertex(pointsInPlane.get(i));
+      convexPolygonInControlPlaneToPack.update();
    }
 
-   public void projectConvexPolygonOntoControlPlane(List<? extends Point3DReadOnly> convexHullInWorld, ConvexPolygon2DBasics convexPolygonInControlPlaneToPack)
+   private void projectPointsInWorldOntoControlPlane(List<? extends Point3DReadOnly> convexHullInWorld, RecyclingArrayList<? extends Point2DBasics> pointsInControlPlane)
    {
-      convexPolygonInControlPlaneToPack.clear();
+      pointsInControlPlane.clear();
 
       for (int vertexIndex = 0; vertexIndex < convexHullInWorld.size(); vertexIndex++)
       {
          tempFramePoint.setIncludingFrame(worldFrame, convexHullInWorld.get(vertexIndex));
-         tempFramePoint.changeFrame(centerOfMassFrame);
 
-         projectPointThroughReferenceFrame(centerOfMassFrame, tempFramePoint, tempProjectedFramePoint, controlPlaneHeight.getDoubleValue(), tempFramePoint.getZ());
+         projectPointThroughReferenceFrame(centerOfMassFrame, tempFramePoint, tempProjectedFramePoint, controlPlaneHeight.getDoubleValue());
 
          tempProjectedFramePoint.changeFrame(worldFrame);
 
-         convexPolygonInControlPlaneToPack.addVertex(tempProjectedFramePoint.getX(), tempProjectedFramePoint.getY());
+         pointsInControlPlane.add().set(tempProjectedFramePoint.getX(), tempProjectedFramePoint.getY());
       }
-      convexPolygonInControlPlaneToPack.update();
    }
 
    private static void projectPointThroughReferenceFrame(ReferenceFrame frameOfProjection,
                                                          FramePoint3DReadOnly pointToProject,
                                                          FramePoint3DBasics projectionToPack,
-                                                         double projectedHeight,
-                                                         double unprojectedHeight)
+                                                         double projectedHeight)
    {
       projectionToPack.setIncludingFrame(pointToProject);
       projectionToPack.changeFrame(frameOfProjection);
+
+      double unprojectedHeight = projectionToPack.getZ();
       projectionToPack.scale(projectedHeight / unprojectedHeight);
       projectionToPack.setZ(projectedHeight);
    }
