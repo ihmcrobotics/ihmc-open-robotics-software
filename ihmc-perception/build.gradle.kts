e9e84e9261fd74c7b4db40df937698da729e0756
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-30 12:42:04 -0500

    :arrow_up: euclid 0.14.0.

--- ihmc-perception/build.gradle.kts
+++ ihmc-perception/build.gradle.kts
@@ -53,7 +53,7 @@ mainDependencies {
    api("us.ihmc.ihmcPerception:cuda:7.5")
    api("org.ddogleg:ddogleg:0.7")
 
-   api("us.ihmc:euclid:0.13.1")
+   api("us.ihmc:euclid:0.14.0")
    api("us.ihmc:ihmc-yovariables:0.5.0")
    api("us.ihmc:simulation-construction-set:0.15.0")
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.15.0")
