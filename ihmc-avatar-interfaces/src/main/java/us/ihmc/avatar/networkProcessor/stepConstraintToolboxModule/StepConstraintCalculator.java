811ec7ca76d8fc3de510cb46f7718ee2170abdf4
Robert <rgriffin@ihmc.us>
2020-05-03 21:07:19 -0500

    modularized the step chooser to a much greater extent

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/stepConstraintToolboxModule/StepConstraintCalculator.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/stepConstraintToolboxModule/StepConstraintCalculator.java
@@ -1,24 +1,14 @@
 package us.ihmc.avatar.networkProcessor.stepConstraintToolboxModule;
 
-import controller_msgs.msg.dds.CapturabilityBasedStatus;
-import controller_msgs.msg.dds.FootstepStatusMessage;
-import controller_msgs.msg.dds.PlanarRegionsListMessage;
-import controller_msgs.msg.dds.RobotConfigurationData;
-import us.ihmc.avatar.networkProcessor.kinematicsToolboxModule.KinematicsToolboxHelper;
 import us.ihmc.commonWalkingControlModules.capturePoint.ICPControlPlane;
 import us.ihmc.commonWalkingControlModules.captureRegion.OneStepCaptureRegionCalculator;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
-import us.ihmc.communication.packets.PlanarRegionMessageConverter;
 import us.ihmc.euclid.geometry.interfaces.Vertex3DSupplier;
 import us.ihmc.euclid.referenceFrame.FrameConvexPolygon2D;
 import us.ihmc.euclid.referenceFrame.FramePoint2D;
-import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
-import us.ihmc.humanoidRobotics.communication.packets.walking.FootstepStatus;
-import us.ihmc.humanoidRobotics.frames.HumanoidReferenceFrames;
-import us.ihmc.mecano.multiBodySystem.interfaces.FloatingJointBasics;
-import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
-import us.ihmc.robotModels.FullHumanoidRobotModel;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameConvexPolygon2DBasics;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.robotSide.RobotSide;
@@ -26,128 +16,105 @@ import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.yoVariables.providers.DoubleProvider;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
-
-import static us.ihmc.robotModels.FullRobotModelUtils.getAllJointsExcludingHands;
 
 public class StepConstraintCalculator
 {
-   private final FloatingJointBasics rootJoint;
-   private final OneDoFJointBasics[] oneDoFJoints;
-   private final CapturabilityBasedStatus capturabilityBasedStatus = new CapturabilityBasedStatus();
    private final OneStepCaptureRegionCalculator captureRegionCalculator;
-   private final HumanoidReferenceFrames referenceFrames;
    private final ICPControlPlane icpControlPlane;
    private final YoVariableRegistry registry = new YoVariableRegistry(getClass().getSimpleName());
    private final DoubleProvider timeProvider;
    private final CapturabilityPlanarRegionDecider planarRegionDecider;
 
+   private final FramePoint2D capturePoint = new FramePoint2D();
+
+   private final SideDependentList<FrameConvexPolygon2D> supportPolygons = new SideDependentList<>(new FrameConvexPolygon2D(), new FrameConvexPolygon2D());
+
    private final AtomicReference<PlanarRegionsList> planarRegionsList = new AtomicReference<>();
 
-   private final SideDependentList<SimpleStep> currentSteps = new SideDependentList<>();
+   private SimpleStep currentStep;
 
-   public StepConstraintCalculator(WalkingControllerParameters walkingControllerParameters, FullHumanoidRobotModel fullRobotModel, DoubleProvider timeProvider,
+   private double omega = 3.0;
+
+   public StepConstraintCalculator(WalkingControllerParameters walkingControllerParameters,
+                                   SideDependentList<? extends ReferenceFrame> soleZUpFrames,
+                                   ReferenceFrame centerOfMassFrame,
+                                   DoubleProvider timeProvider,
                                    double gravityZ)
    {
       this.timeProvider = timeProvider;
-      this.referenceFrames = new HumanoidReferenceFrames(fullRobotModel);
-      this.captureRegionCalculator = new OneStepCaptureRegionCalculator(referenceFrames, walkingControllerParameters, registry, null);
-      this.icpControlPlane = new ICPControlPlane(referenceFrames.getCenterOfMassFrame(), gravityZ, registry);
+      this.captureRegionCalculator = new OneStepCaptureRegionCalculator(soleZUpFrames, walkingControllerParameters, registry, null);
+      this.icpControlPlane = new ICPControlPlane(centerOfMassFrame, gravityZ, registry);
       this.planarRegionDecider = new CapturabilityPlanarRegionDecider(captureRegionCalculator, icpControlPlane, registry, null);
-      this.rootJoint = fullRobotModel.getRootJoint();
-      this.oneDoFJoints = getAllJointsExcludingHands(fullRobotModel);
    }
 
-   // FIXME make this thread safe
-   public void updateCapturabilityBasedStatus(CapturabilityBasedStatus capturabilityBasedStatus)
+   public void setLeftFootSupportPolygon(List<? extends Point3DReadOnly> footVertices)
    {
-      this.capturabilityBasedStatus.set(capturabilityBasedStatus);
+      setFootSupportPolygon(RobotSide.LEFT, footVertices);
    }
 
-   // FIXME make this thread safe
-   public void updateRobotConfigurationData(RobotConfigurationData newConfigurationData)
+   public void setRightFootSupportPolygon(List<? extends Point3DReadOnly> footVertices)
    {
-      KinematicsToolboxHelper.setRobotStateFromRobotConfigurationData(newConfigurationData, rootJoint, oneDoFJoints);
+      setFootSupportPolygon(RobotSide.RIGHT, footVertices);
    }
 
-   // FIXME make this thread safe
-   public void updateFootstepStatus(FootstepStatusMessage statusMessage)
+   public void setFootSupportPolygon(RobotSide supportSide, List<? extends Point3DReadOnly> footVertices)
    {
-      RobotSide robotSide = RobotSide.fromByte(statusMessage.getRobotSide());
-      if (FootstepStatus.fromByte(statusMessage.getFootstepStatus()) == FootstepStatus.STARTED)
-      {
-         currentSteps.put(robotSide, new SimpleStep(statusMessage, timeProvider.getValue()));
-         planarRegionDecider.reset();
-      }
-      else
-      {
-         currentSteps.remove(robotSide);
-      }
+      FrameConvexPolygon2DBasics supportPolygon = supportPolygons.get(supportSide);
+      supportPolygon.clear();
+      supportPolygon.addVertices(Vertex3DSupplier.asVertex3DSupplier(footVertices));
+      supportPolygon.update();
    }
 
-   public void updatePlanarRegions(PlanarRegionsListMessage planarRegionsListMessage)
+   public void setOmega(double omega)
    {
-      planarRegionsList.set(PlanarRegionMessageConverter.convertToPlanarRegionsList(planarRegionsListMessage));
-      planarRegionDecider.setPlanarRegions(planarRegionsList.get().getPlanarRegionsAsList());
+      this.omega = omega;
+   }
+
+   public void setCapturePoint(Point3DReadOnly capturePointInWorld)
+   {
+      capturePoint.setIncludingFrame(ReferenceFrame.getWorldFrame(), capturePointInWorld);
+   }
+
+   public void setCurrentStep(SimpleStep simpleStep)
+   {
+      currentStep = simpleStep;
+   }
+
+   public void setPlanarRegions(PlanarRegionsList planarRegions)
+   {
+      planarRegionsList.set(planarRegions);
+      planarRegionDecider.setPlanarRegions(planarRegions.getPlanarRegionsAsList());
+   }
+
+   public void reset()
+   {
+      currentStep = null;
    }
 
    public void update()
    {
-      referenceFrames.updateFrames();
-      icpControlPlane.setOmega0(capturabilityBasedStatus.getOmega());
+      icpControlPlane.setOmega0(omega);
 
-      SimpleStep simpleStep;
-      if (currentSteps.containsKey(RobotSide.LEFT))
+      if (currentStep == null)
       {
-         simpleStep = currentSteps.get(RobotSide.LEFT);
-      }
-      else if (currentSteps.containsKey(RobotSide.RIGHT))
-      {
-         simpleStep = currentSteps.get(RobotSide.RIGHT);
+         captureRegionCalculator.hideCaptureRegion();
       }
       else
       {
-         simpleStep = null;
+         updateCaptureRegion(currentStep);
+         planarRegionDecider.updatePlanarRegionConstraintForStep(currentStep.getStepPose());
       }
-
-      updateCaptureRegion(simpleStep);
-
-      if (simpleStep != null)
-         planarRegionDecider.updatePlanarRegionConstraintForStep(simpleStep.getStepPose());
    }
-
-   private final FramePoint2D capturePoint = new FramePoint2D();
-   private final FrameConvexPolygon2D leftFootSupportPolygon = new FrameConvexPolygon2D();
-   private final FrameConvexPolygon2D rightFootSupportPolygon = new FrameConvexPolygon2D();
 
    private void updateCaptureRegion(SimpleStep simpleStep)
    {
-      capturePoint.set(capturabilityBasedStatus.getCapturePoint2d());
-      leftFootSupportPolygon.addVertices(Vertex3DSupplier.asVertex3DSupplier(capturabilityBasedStatus.getLeftFootSupportPolygon2d()));
-      rightFootSupportPolygon.addVertices(Vertex3DSupplier.asVertex3DSupplier(capturabilityBasedStatus.getRightFootSupportPolygon2d()));
-      leftFootSupportPolygon.update();
-      rightFootSupportPolygon.update();
-
-      if (simpleStep == null)
-      {
-         captureRegionCalculator.hideCaptureRegion();
-         return;
-      }
-
       double timeInState = timeProvider.getValue() - simpleStep.getStartTime();
       double timeRemaining = simpleStep.getSwingDuration() - timeInState;
       RobotSide swingSide = simpleStep.getSwingSide();
 
-      if (swingSide == RobotSide.LEFT)
-      {
-         captureRegionCalculator.calculateCaptureRegion(swingSide, timeRemaining, capturePoint, capturabilityBasedStatus.getOmega(),
-                                                        rightFootSupportPolygon);
-      }
-      else
-      {
-         captureRegionCalculator.calculateCaptureRegion(swingSide, timeRemaining, capturePoint, capturabilityBasedStatus.getOmega(),
-                                                        leftFootSupportPolygon);
-      }
+      captureRegionCalculator.calculateCaptureRegion(swingSide, timeRemaining, capturePoint, omega, supportPolygons.get(swingSide.getOppositeSide()));
    }
 
    public boolean constraintRegionChanged()
@@ -158,42 +125,5 @@ public class StepConstraintCalculator
    public PlanarRegion getConstraintRegion()
    {
       return planarRegionDecider.getConstraintRegion();
-   }
-
-   private class SimpleStep
-   {
-      private final RobotSide swingSide;
-      private final double swingDuration;
-      private final double startTime;
-      private final FramePose3D stepPose = new FramePose3D();
-
-      public SimpleStep(FootstepStatusMessage statusMessage, double startTime)
-      {
-         this.swingSide = RobotSide.fromByte(statusMessage.getRobotSide());
-         this.swingDuration = statusMessage.getSwingDuration();
-         this.startTime = startTime;
-         stepPose.getPosition().set(statusMessage.getDesiredFootPositionInWorld());
-         stepPose.getOrientation().set(statusMessage.getDesiredFootOrientationInWorld());
-      }
-
-      public double getSwingDuration()
-      {
-         return swingDuration;
-      }
-
-      public double getStartTime()
-      {
-         return startTime;
-      }
-
-      public RobotSide getSwingSide()
-      {
-         return swingSide;
-      }
-
-      public FramePose3DReadOnly getStepPose()
-      {
-         return stepPose;
-      }
    }
 }
