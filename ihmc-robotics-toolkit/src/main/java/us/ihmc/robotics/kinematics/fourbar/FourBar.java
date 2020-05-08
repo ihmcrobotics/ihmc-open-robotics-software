62ab3f6debd66d3ec183aeeb313042925052b319
Sylvain Bertrand <sbertrand@ihmc.us>
2020-05-07 19:06:14 -0500

    Extracted the nested classes.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/kinematics/fourbar/FourBar.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/kinematics/fourbar/FourBar.java
@@ -29,18 +29,18 @@ public class FourBar
     * @formatter:on
     */
 
-   private final Vertex A = new Vertex("A");
-   private final Vertex B = new Vertex("B");
-   private final Vertex C = new Vertex("C");
-   private final Vertex D = new Vertex("D");
+   private final FourBarVertex A = new FourBarVertex("A");
+   private final FourBarVertex B = new FourBarVertex("B");
+   private final FourBarVertex C = new FourBarVertex("C");
+   private final FourBarVertex D = new FourBarVertex("D");
 
-   private final Edge AB = new Edge("AB");
-   private final Edge BC = new Edge("BC");
-   private final Edge CD = new Edge("CD");
-   private final Edge DA = new Edge("DA");
+   private final FourBarEdge AB = new FourBarEdge("AB");
+   private final FourBarEdge BC = new FourBarEdge("BC");
+   private final FourBarEdge CD = new FourBarEdge("CD");
+   private final FourBarEdge DA = new FourBarEdge("DA");
 
-   private final Diagonal AC = new Diagonal("AC");
-   private final Diagonal BD = new Diagonal("AC");
+   private final FourBarDiagonal AC = new FourBarDiagonal("AC");
+   private final FourBarDiagonal BD = new FourBarDiagonal("AC");
 
    public enum Angle
    {
@@ -96,10 +96,10 @@ public class FourBar
    public void setSideLengths(double AB, double BC, double CD, double DA)
    {
       setToNaN();
-      this.AB.length = AB;
-      this.BC.length = BC;
-      this.CD.length = CD;
-      this.DA.length = DA;
+      this.AB.setLength(AB);
+      this.BC.setLength(BC);
+      this.CD.setLength(CD);
+      this.DA.setLength(DA);
    }
 
    /**
@@ -162,52 +162,52 @@ public class FourBar
       }
    }
 
-   public Vertex getVertexA()
+   public FourBarVertex getVertexA()
    {
       return A;
    }
 
-   public Vertex getVertexB()
+   public FourBarVertex getVertexB()
    {
       return B;
    }
 
-   public Vertex getVertexC()
+   public FourBarVertex getVertexC()
    {
       return C;
    }
 
-   public Vertex getVertexD()
+   public FourBarVertex getVertexD()
    {
       return D;
    }
 
-   public Edge getEdgeAB()
+   public FourBarEdge getEdgeAB()
    {
       return AB;
    }
 
-   public Edge getEdgeBC()
+   public FourBarEdge getEdgeBC()
    {
       return BC;
    }
 
-   public Edge getEdgeCD()
+   public FourBarEdge getEdgeCD()
    {
       return CD;
    }
 
-   public Edge getEdgeDA()
+   public FourBarEdge getEdgeDA()
    {
       return DA;
    }
 
-   public Diagonal getDiagonalAC()
+   public FourBarDiagonal getDiagonalAC()
    {
       return AC;
    }
 
-   public Diagonal getDiagonalBD()
+   public FourBarDiagonal getDiagonalBD()
    {
       return BD;
    }
@@ -330,314 +330,6 @@ public class FourBar
    public double getDA()
    {
       return getEdgeDA().getLength();
-   }
-
-   public static class Vertex
-   {
-      private final String name;
-
-      private double angle;
-      private double angleDot;
-      private double angleDDot;
-
-      private double minAngle;
-      private double maxAngle;
-
-      private Edge nextEdge, previousEdge;
-      private Diagonal diagonal;
-
-      private Vertex(String name)
-      {
-         this.name = name;
-      }
-
-      private void setup(Edge previousEdge, Edge nextEdge, Diagonal diagonal)
-      {
-         this.nextEdge = nextEdge;
-         this.previousEdge = previousEdge;
-         this.diagonal = diagonal;
-      }
-
-      public void setToNaN()
-      {
-         angle = Double.NaN;
-         angleDot = Double.NaN;
-         angleDDot = Double.NaN;
-         minAngle = Double.NaN;
-         maxAngle = Double.NaN;
-      }
-
-      public void setToMin()
-      {
-         angle = getMinAngle();
-         angle = 0.0;
-         angleDDot = 0.0;
-      }
-
-      public void setToMax()
-      {
-         angle = getMaxAngle();
-         angle = 0.0;
-         angleDDot = 0.0;
-      }
-
-      public void setAngle(double angle)
-      {
-         this.angle = angle;
-      }
-
-      public void setAngleDot(double angleDot)
-      {
-         this.angleDot = angleDot;
-      }
-
-      public void setAngleDDot(double angleDDot)
-      {
-         this.angleDDot = angleDDot;
-      }
-
-      public void setMinAngle(double minAngle)
-      {
-         this.minAngle = minAngle;
-      }
-
-      public void setMaxAngle(double maxAngle)
-      {
-         this.maxAngle = maxAngle;
-      }
-
-      public String getName()
-      {
-         return name;
-      }
-
-      public double getAngle()
-      {
-         return angle;
-      }
-
-      public double getAngleDot()
-      {
-         return angleDot;
-      }
-
-      public double getAngleDDot()
-      {
-         return angleDDot;
-      }
-
-      public double getMinAngle()
-      {
-         if (Double.isNaN(minAngle))
-            FourBarTools.updateLimits(this);
-         return minAngle;
-      }
-
-      public double getMaxAngle()
-      {
-         if (Double.isNaN(maxAngle))
-            FourBarTools.updateLimits(this);
-         return maxAngle;
-      }
-
-      public Edge getNextEdge()
-      {
-         return nextEdge;
-      }
-
-      public Edge getPreviousEdge()
-      {
-         return previousEdge;
-      }
-
-      public Diagonal getDiagonal()
-      {
-         return diagonal;
-      }
-
-      public Vertex getNextVertex()
-      {
-         return nextEdge.getEnd();
-      }
-
-      public Vertex getPreviousVertex()
-      {
-         return previousEdge.getStart();
-      }
-
-      public Vertex getOppositeVertex()
-      {
-         return nextEdge.getNext().getEnd();
-      }
-
-      @Override
-      public String toString()
-      {
-         return getName();
-      }
-   }
-
-   public static class Edge
-   {
-      private final String name;
-
-      private double length;
-      private Vertex start, end;
-      private Edge nextEdge, previous;
-
-      private Edge(String name)
-      {
-         this.name = name;
-      }
-
-      private void setup(Vertex start, Vertex end, Edge previous, Edge next)
-      {
-         this.start = start;
-         this.end = end;
-         this.previous = previous;
-         this.nextEdge = next;
-      }
-
-      public void setToNaN()
-      {
-         length = Double.NaN;
-      }
-
-      public String getName()
-      {
-         return name;
-      }
-
-      public double getLength()
-      {
-         return length;
-      }
-
-      public Vertex getStart()
-      {
-         return start;
-      }
-
-      public Vertex getEnd()
-      {
-         return end;
-      }
-
-      public Edge getNext()
-      {
-         return nextEdge;
-      }
-
-      public Edge getPrevious()
-      {
-         return previous;
-      }
-
-      @Override
-      public String toString()
-      {
-         return getName();
-      }
-   }
-
-   public static class Diagonal
-   {
-      private final String name;
-
-      private double length;
-      private double lengthDot;
-      private double lengthDDot;
-      private double maxLength;
-      private Vertex start, end;
-      private Diagonal other;
-
-      public Diagonal(String name)
-      {
-         this.name = name;
-      }
-
-      private void setup(Vertex start, Vertex end, Diagonal other)
-      {
-         this.start = start;
-         this.end = end;
-         this.other = other;
-      }
-
-      public void setToNaN()
-      {
-         length = Double.NaN;
-         lengthDot = Double.NaN;
-         lengthDDot = Double.NaN;
-         maxLength = Double.NaN;
-      }
-
-      public void setLength(double length)
-      {
-         this.length = length;
-      }
-
-      public void setLengthDot(double lengthDot)
-      {
-         this.lengthDot = lengthDot;
-      }
-
-      public void setLengthDDot(double lengthDDot)
-      {
-         this.lengthDDot = lengthDDot;
-      }
-
-      public void setMaxLength(double maxLength)
-      {
-         this.maxLength = maxLength;
-      }
-
-      public String getName()
-      {
-         return name;
-      }
-
-      public double getLength()
-      {
-         return length;
-      }
-
-      public double getLengthDot()
-      {
-         return lengthDot;
-      }
-
-      public double getLengthDDot()
-      {
-         return lengthDDot;
-      }
-
-      public double getMaxLength()
-      {
-         if (Double.isNaN(maxLength))
-            FourBarTools.updateDiagonalMaxLength(this);
-         return maxLength;
-      }
-
-      public Vertex getStart()
-      {
-         return start;
-      }
-
-      public Vertex getEnd()
-      {
-         return end;
-      }
-
-      public Diagonal getOther()
-      {
-         return other;
-      }
-
-      @Override
-      public String toString()
-      {
-         return getName();
-      }
    }
 
    @Override
