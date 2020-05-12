9a0bc2679b46596bb1f4f3a5b7de1785da197c26
Robert <rgriffin@ihmc.us>
2020-05-11 21:18:21 -0500

    started setting up tests for the constraint region calculator

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/stepConstraintToolboxModule/SimpleStep.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/stepConstraintToolboxModule/SimpleStep.java
@@ -1,25 +1,68 @@
 package us.ihmc.avatar.networkProcessor.stepConstraintToolboxModule;
 
 import controller_msgs.msg.dds.FootstepStatusMessage;
+import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
+import us.ihmc.euclid.orientation.interfaces.Orientation3DReadOnly;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.robotics.robotSide.RobotSide;
 
 class SimpleStep
 {
-   private final RobotSide swingSide;
-   private final double swingDuration;
-   private final double startTime;
-   private final FramePose3D stepPose = new FramePose3D();
+   private RobotSide swingSide;
+   private double swingDuration;
+   private double startTime;
+   private FramePose3D stepPose = new FramePose3D();
+
+   public SimpleStep()
+   {
+   }
 
    public SimpleStep(FootstepStatusMessage statusMessage, double startTime)
    {
-      this.swingSide = RobotSide.fromByte(statusMessage.getRobotSide());
-      this.swingDuration = statusMessage.getSwingDuration();
-      this.startTime = startTime;
-      stepPose.getPosition().set(statusMessage.getDesiredFootPositionInWorld());
-      stepPose.getOrientation().set(statusMessage.getDesiredFootOrientationInWorld());
+      setSwingSide(RobotSide.fromByte(statusMessage.getRobotSide()));
+      setSwingDuration(statusMessage.getSwingDuration());
+      setStartTime(startTime);
+      setStepPose(statusMessage.getDesiredFootPositionInWorld(), statusMessage.getDesiredFootOrientationInWorld());
    }
+
+   public void setSwingSide(RobotSide swingSide)
+   {
+      this.swingSide = swingSide;
+   }
+
+   public void setSwingDuration(double swingDuration)
+   {
+      this.swingDuration = swingDuration;
+   }
+
+   public void setStartTime(double startTime)
+   {
+      this.startTime = startTime;
+   }
+
+   public void setStepPose(Pose3DReadOnly stepPose)
+   {
+      setStepPose(stepPose.getPosition(), stepPose.getOrientation());
+   }
+
+   public void setStepPose(Point3DReadOnly stepPosition, Orientation3DReadOnly stepOrientation)
+   {
+      setStepPosition(stepPosition);
+      setStepOrientation(stepOrientation);
+   }
+
+   public void setStepPosition(Point3DReadOnly stepPosition)
+   {
+      stepPose.getPosition().set(stepPosition);
+   }
+
+   public void setStepOrientation(Orientation3DReadOnly stepOrientation)
+   {
+      stepPose.getOrientation().set(stepOrientation);
+   }
+
 
    public double getSwingDuration()
    {
