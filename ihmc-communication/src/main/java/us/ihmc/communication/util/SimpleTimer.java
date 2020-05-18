8f3cc7d01134254a2f9f39fad9559d9981d15b8a
Duncan Calvert <dcalvert@ihmc.us>
2020-05-17 21:19:32 -0500

    New SimpleTimer class.

---
+++ ihmc-communication/src/main/java/us/ihmc/communication/util/SimpleTimer.java
@@ -0,0 +1,23 @@
+package us.ihmc.communication.util;
+
+import us.ihmc.commons.time.Stopwatch;
+
+public class SimpleTimer
+{
+   private final Stopwatch stopwatch = new Stopwatch();
+
+   public void reset()
+   {
+      stopwatch.start();
+   }
+
+   public boolean isPastOrNaN(double time)
+   {
+      return Double.isNaN(timePassedSinceReset()) || timePassedSinceReset() > time;
+   }
+
+   private double timePassedSinceReset()
+   {
+      return stopwatch.totalElapsed();
+   }
+}
