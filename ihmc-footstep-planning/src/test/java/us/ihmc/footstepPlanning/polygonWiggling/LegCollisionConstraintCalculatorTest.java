c5c45d8b7f43ded41e50cdfeef70990211b5e47c
smccrory <smccrory@ihmc.us>
2020-05-26 19:17:26 -0500

    Adding visual test of shin collision in gradient descent solver

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/polygonWiggling/LegCollisionConstraintCalculatorTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/polygonWiggling/LegCollisionConstraintCalculatorTest.java
@@ -1,7 +1,10 @@
 package us.ihmc.footstepPlanning.polygonWiggling;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import us.ihmc.commonWalkingControlModules.polygonWiggling.LegCollisionConstraintCalculator;
+import us.ihmc.commons.ContinuousIntegrationTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.Axis3D;
 import us.ihmc.euclid.geometry.Pose2D;
@@ -13,12 +16,53 @@ import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.geometry.PlanarRegionsListGenerator;
 import us.ihmc.robotics.geometry.RigidBodyTransformGenerator;
+import us.ihmc.robotics.graphics.YoGraphicPlanarRegionsList;
 import us.ihmc.simulationconstructionset.Robot;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 
 public class LegCollisionConstraintCalculatorTest
 {
+   private boolean visualize = true;
+   private SimulationConstructionSet scs;
+   private YoGraphicsListRegistry graphicsListRegistry = new YoGraphicsListRegistry();
+   private YoGraphicPlanarRegionsList yoGraphicPlanarRegionsList;
+   private LegCollisionConstraintCalculator collisionConstraintCalculator;
+
+   @BeforeEach
+   public void setup()
+   {
+      visualize = visualize && !ContinuousIntegrationTools.isRunningOnContinuousIntegrationServer();
+
+      if (visualize)
+      {
+         scs = new SimulationConstructionSet(new Robot("dummy"));
+         scs.setGroundVisible(false);
+         collisionConstraintCalculator = new LegCollisionConstraintCalculator(graphicsListRegistry, scs.getRootRegistry());
+         yoGraphicPlanarRegionsList = new YoGraphicPlanarRegionsList("PlanarRegions", 150, 100, scs.getRootRegistry());
+         graphicsListRegistry.registerYoGraphic("PlanarRegionsGraphic", yoGraphicPlanarRegionsList);
+         scs.addYoGraphicsListRegistry(graphicsListRegistry);
+         scs.startOnAThread();
+      }
+      else
+      {
+         collisionConstraintCalculator = new LegCollisionConstraintCalculator();
+      }
+
+      double shinPitch = Math.toRadians(25.0);
+      double shinRadius = 0.05;
+      double shinLength = 0.4;
+      Cylinder3D legCylinder = new Cylinder3D(shinLength, shinRadius);
+
+      RigidBodyTransformGenerator transformGenerator = new RigidBodyTransformGenerator();
+      transformGenerator.translate(0.0, 0.0, 0.05);
+      transformGenerator.rotate(shinPitch, Axis3D.Y);
+      transformGenerator.translate(0.0, 0.0, 0.5 * shinLength);
+      RigidBodyTransform transformToSoleFrame = transformGenerator.getRigidBodyTransformCopy();
+      collisionConstraintCalculator.setLegCollisionShape(legCylinder, transformToSoleFrame);
+   }
+
    @Test
+   @Disabled
    public void testLegCollisionDetector()
    {
       double stepHeight = 0.3;
@@ -37,40 +81,31 @@ public class LegCollisionConstraintCalculatorTest
       }
 
       PlanarRegionsList stairCaseRegions = planarRegionsListGenerator.getPlanarRegionsList();
-      SimulationConstructionSet scs = new SimulationConstructionSet(new Robot("dummy"));
-      scs.setGroundVisible(false);
-
-      YoGraphicsListRegistry graphicsListRegistry = new YoGraphicsListRegistry();
-      LegCollisionConstraintCalculator collisionConstraintCalculator = new LegCollisionConstraintCalculator(graphicsListRegistry, scs.getRootRegistry());
-
-      double shinPitch = Math.toRadians(25.0);
-      double shinRadius = 0.05;
-      double shinLength = 0.4;
-
       PlanarRegion firstStep = stairCaseRegions.getPlanarRegion(1);
 
-      Cylinder3D legCylinder = new Cylinder3D(shinLength, shinRadius);
-
-      RigidBodyTransformGenerator transformGenerator = new RigidBodyTransformGenerator();
-      transformGenerator.translate(0.0, 0.0, 0.05);
-      transformGenerator.rotate(shinPitch, Axis3D.Y);
-      transformGenerator.translate(0.0, 0.0, 0.5 * shinLength);
-      RigidBodyTransform transformToSoleFrame = transformGenerator.getRigidBodyTransformCopy();
-      System.out.println(transformToSoleFrame);
-
-      collisionConstraintCalculator.setLegCollisionShape(legCylinder, transformToSoleFrame);
-
-      scs.addYoGraphicsListRegistry(graphicsListRegistry);
-      scs.startOnAThread();
-
+      visualizePlanarRegions(stairCaseRegions);
       collisionConstraintCalculator.calculateLegCollisionGradient(new Pose2D(), firstStep.getTransformToWorld(), stairCaseRegions, new Vector3D());
 
-      for (int i = 0; i < 100; i++)
+      if (visualize)
       {
-         collisionConstraintCalculator.update();
          scs.tickAndUpdate();
+         ThreadTools.sleepForever();
+      }
+   }
+
+   private void visualizePlanarRegions(PlanarRegionsList planarRegionsList)
+   {
+      if (!visualize)
+      {
+         return;
       }
 
-      ThreadTools.sleepForever();
+      yoGraphicPlanarRegionsList.clear();
+      yoGraphicPlanarRegionsList.submitPlanarRegionsListToRender(planarRegionsList);
+      for (int i = 0; i < 100; i++)
+      {
+         yoGraphicPlanarRegionsList.processPlanarRegionsListQueue();
+         scs.tickAndUpdate();
+      }
    }
 }
