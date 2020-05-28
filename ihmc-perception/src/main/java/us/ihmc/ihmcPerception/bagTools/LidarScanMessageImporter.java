95586cb0347ce62a07b5da5379a044ce9415ee63
smccrory <smccrory@ihmc.us>
2020-05-28 10:44:21 -0500

    Adding point cloud message convert, exporter and importer

---
+++ ihmc-perception/src/main/java/us/ihmc/ihmcPerception/bagTools/LidarScanMessageImporter.java
@@ -0,0 +1,8 @@
+package us.ihmc.ihmcPerception.bagTools;
+
+/**
+ * Loads and broadcasts to JSON file written by LidarScanMessageExporter, analogous to "rosbag play"
+ */
+public class LidarScanMessageImporter
+{
+}
