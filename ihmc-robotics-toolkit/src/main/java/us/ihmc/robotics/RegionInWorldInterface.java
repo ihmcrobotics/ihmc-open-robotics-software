3641abd0b9aa3ef648fb903fa7ccb218412b408c
Robert <rgriffin@ihmc.us>
2020-06-01 22:27:24 -0500

    added some java doc

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/RegionInWorldInterface.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/RegionInWorldInterface.java
@@ -7,12 +7,29 @@ import us.ihmc.euclid.tuple3D.interfaces.Vector3DBasics;
 
 public interface RegionInWorldInterface<T extends RegionInWorldInterface>
 {
+   /**
+    * Returns the transform from the world frame to the local frame.
+    */
    RigidBodyTransformReadOnly getTransformToLocal();
+
+   /**
+    * Returns the transform from the local frame to the world frame.
+    */
    RigidBodyTransformReadOnly getTransformToWorld();
 
+   /**
+    * Returns the origin of the region in the world.
+    */
    void getOrigin(Point3DBasics originToPack);
+
+   /**
+    * Returns the normal of the region relative to the world.
+    */
    void getNormal(Vector3DBasics normalToPack);
 
+   /**
+    * Checks to see if a point defined in the region frame is within the region bounds.
+    */
    boolean isPointInside(double xInLocal, double yInLocal);
 
    void set(T other);
