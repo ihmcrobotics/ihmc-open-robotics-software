a14227e39b3825e17e5ddc0cd0ea2e6b753f56f5
Duncan Calvert <dcalvert@ihmc.us>
2020-06-01 19:33:16 -0500

    Add a status to the SimpleTimer to be more thread safe.

--- ihmc-communication/src/main/java/us/ihmc/communication/util/SimpleTimer.java
+++ ihmc-communication/src/main/java/us/ihmc/communication/util/SimpleTimer.java
@@ -13,11 +13,44 @@ public class SimpleTimer
 
    public boolean isPastOrNaN(double time)
    {
-      return Double.isNaN(timePassedSinceReset()) || timePassedSinceReset() > time;
+      return isPastOrNaN(timePassedSinceReset(), time);
+   }
+
+   private boolean isPastOrNaN(double timePassedSinceReset, double time)
+   {
+      return Double.isNaN(timePassedSinceReset) || timePassedSinceReset > time;
    }
 
    public double timePassedSinceReset()
    {
       return stopwatch.totalElapsed();
+   }
+
+   public Status getStatus(double time)
+   {
+      double timePassedSinceReset = timePassedSinceReset();
+      return new Status(isPastOrNaN(timePassedSinceReset, time), timePassedSinceReset);
+   }
+
+   public class Status
+   {
+      private final boolean isPastOrNaN;
+      private final double timePassedSinceReset;
+
+      public Status(boolean isPastOrNaN, double timePassedSinceReset)
+      {
+         this.isPastOrNaN = isPastOrNaN;
+         this.timePassedSinceReset = timePassedSinceReset;
+      }
+
+      public boolean isPastOrNaN()
+      {
+         return isPastOrNaN;
+      }
+
+      public double getTimePassedSinceReset()
+      {
+         return timePassedSinceReset;
+      }
    }
 }
