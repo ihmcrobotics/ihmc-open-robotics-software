07ea0bc747c2b4eadbed1b79006ceeccfd060eec
Duncan Calvert <dcalvert@ihmc.us>
2020-06-01 21:34:14 -0500

    Extract body path task into a builder class and some high level wiring.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBodyPathPart.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBodyPathPart.java
@@ -1,39 +1,24 @@
 package us.ihmc.humanoidBehaviors.lookAndStep;
 
 import controller_msgs.msg.dds.PlanarRegionsListMessage;
-import us.ihmc.commons.time.Stopwatch;
 import us.ihmc.communication.packets.PlanarRegionMessageConverter;
 import us.ihmc.communication.util.SimpleTimer;
 import us.ihmc.euclid.geometry.Pose3D;
-import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
-import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.footstepPlanning.graphSearch.VisibilityGraphPathPlanner;
 import us.ihmc.humanoidBehaviors.tools.BehaviorHelper;
-import us.ihmc.humanoidBehaviors.tools.HumanoidRobotState;
 import us.ihmc.log.LogTools;
 import us.ihmc.messager.TopicListener;
 import us.ihmc.pathPlanning.visibilityGraphs.parameters.VisibilityGraphsParametersReadOnly;
-import us.ihmc.pathPlanning.visibilityGraphs.postProcessing.BodyPathPostProcessor;
-import us.ihmc.pathPlanning.visibilityGraphs.postProcessing.ObstacleAvoidanceProcessor;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
-import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.yoVariables.registry.YoVariableRegistry;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-
-import static us.ihmc.humanoidBehaviors.lookAndStep.LookAndStepBehaviorAPI.BodyPathPlanForUI;
-import static us.ihmc.humanoidBehaviors.lookAndStep.LookAndStepBehaviorAPI.MapRegionsForUI;
 
 public class LookAndStepBodyPathPart
 {
    private final BehaviorHelper helper;
    private final Supplier<Boolean> isBeingReviewed;
    private final Supplier<Boolean> operatorReviewEnabled;
-   private final Supplier<HumanoidRobotState> robotStateSupplier;
    private final LookAndStepBehaviorParameters lookAndStepParameters;
    private final VisibilityGraphsParametersReadOnly visibilityGraphParameters;
    private final Consumer<ArrayList<Pose3D>> successfulPlanConsumer;
@@ -46,11 +31,11 @@ public class LookAndStepBodyPathPart
    private final TypedInput<Pose3D> goalInput = new TypedInput<>();
 
    private SimpleTimer planningFailedTimer = new SimpleTimer();
+   private final LookAndStepBodyPathTask task;
 
    public LookAndStepBodyPathPart(BehaviorHelper helper,
                                   Supplier<Boolean> isBeingReviewed,
                                   Supplier<Boolean> operatorReviewEnabled,
-                                  Supplier<HumanoidRobotState> robotStateSupplier,
                                   LookAndStepBehaviorParameters lookAndStepParameters,
                                   VisibilityGraphsParametersReadOnly visibilityGraphParameters,
                                   Consumer<Consumer<PlanarRegionsListMessage>> mapRegionsCallbackRegistry,
@@ -62,7 +47,6 @@ public class LookAndStepBodyPathPart
       this.helper = helper;
       this.isBeingReviewed = isBeingReviewed;
       this.operatorReviewEnabled = operatorReviewEnabled;
-      this.robotStateSupplier = robotStateSupplier;
       this.lookAndStepParameters = lookAndStepParameters;
       this.visibilityGraphParameters = visibilityGraphParameters;
       this.successfulPlanConsumer = successfulPlanConsumer;
@@ -81,6 +65,7 @@ public class LookAndStepBodyPathPart
       goalInputCallbackRegistry.accept(this::acceptGoal);
       goalInput.addCallback(data -> executor.execute(this::evaluateAndRun));
 
+      task = new LookAndStepBodyPathTask(helper);
    }
 
    private void acceptMapRegions(PlanarRegionsListMessage planarRegionsListMessage)
@@ -97,140 +82,19 @@ public class LookAndStepBodyPathPart
 
    private void evaluateAndRun()
    {
-      // get snapshot of data to process
-      SimpleTimer.Status mapRegionsExpirationStatus = mapRegionsExpirationTimer.getStatus(lookAndStepParameters.getPlanarRegionsExpiration());
-      SimpleTimer.Status planningFailedTimerStatus
-            = planningFailedTimer.getStatus(lookAndStepParameters.get(LookAndStepBehaviorParameters.waitTimeAfterPlanFailed));
-      PlanarRegionsList mapRegions = mapRegionsInput.get();
-      Pose3D goal = goalInput.get();
-      HumanoidRobotState humanoidRobotState = helper.getOrCreateRobotInterface().pollHumanoidRobotState();
+      task.setGoal(goalInput.get());
+      task.setMapRegions(mapRegionsInput.get());
+      task.setHumanoidRobotState(helper.getOrCreateRobotInterface().pollHumanoidRobotState());
+      task.setIsBeingReviewed(isBeingReviewed);
+      task.setMapRegionsExpirationStatus(mapRegionsExpirationTimer.getStatus(lookAndStepParameters.getPlanarRegionsExpiration()));
+      task.setPlanningFailedTimerStatus(planningFailedTimer.getStatus(lookAndStepParameters.get(LookAndStepBehaviorParameters.waitTimeAfterPlanFailed)));
+      task.setResetPlanningFailedTimer(() -> planningFailedTimer.reset());
+      task.setReviewInitiator(review);
+      task.setSuccessfulPlanConsumer(successfulPlanConsumer);
+      task.setVisibilityGraphParameters(visibilityGraphParameters);
+      task.setNeedNewPlan(needNewPlan);
+      task.setOperatorReviewEnabled(operatorReviewEnabled);
 
-      if (evaluate(helper, mapRegions, goal, mapRegionsExpirationStatus, planningFailedTimerStatus, isBeingReviewed, needNewPlan))
-      {
-         run(helper,
-             mapRegions,
-             goal,
-             humanoidRobotState,
-             visibilityGraphParameters,
-             operatorReviewEnabled,
-             successfulPlanConsumer,
-             () -> planningFailedTimer.reset(),
-             review);
-      }
-   }
-
-   // static so it must use data snapshot
-   private static boolean evaluate(BehaviorHelper helper,
-                                   PlanarRegionsList mapRegions,
-                                   Pose3D goal,
-                                   SimpleTimer.Status mapRegionsExpirationStatus,
-                                   SimpleTimer.Status planningFailedTimerStatus,
-                                   Supplier<Boolean> isBeingReviewed,
-                                   Supplier<Boolean> needNewPlan)
-   {
-      boolean hasGoal = goal != null && !goal.containsNaN();
-      if (!hasGoal)
-      {
-         LogTools.warn("Body path: does not have goal");
-         LogTools.debug("Sending planar regions to UI: {}: {}", LocalDateTime.now(), mapRegions.hashCode());
-         helper.publishToUI(MapRegionsForUI, mapRegions);
-         return false;
-      }
-
-      boolean regionsOK = mapRegions != null
-                          && !mapRegionsExpirationStatus.isPastOrNaN()
-                          && !mapRegions.isEmpty();
-      if (!regionsOK)
-      {
-         LogTools.warn("Body path: Regions not OK: {}, timePassed: {}, isEmpty: {}",
-                       mapRegions,
-                       mapRegionsExpirationStatus.getTimePassedSinceReset(),
-                       mapRegions == null ? null : mapRegions.isEmpty());
-         return false;
-      }
-
-      if (!needNewPlan.get())
-      {
-         LogTools.warn("Body path: New plan not needed");
-         return false;
-      }
-
-      // TODO: This could be "run recently" instead of failed recently
-      boolean failedRecently = !planningFailedTimerStatus.isPastOrNaN();
-      if (failedRecently)
-      {
-         LogTools.warn("Body path: failedRecently = true");
-         return false;
-      }
-
-      if (isBeingReviewed.get())
-      {
-         LogTools.debug("Body path: bodyPathBeingReviewed = true");
-         return false;
-      }
-
-      return true;
-   }
-
-   private static void run(BehaviorHelper helper,
-                           PlanarRegionsList mapRegions,
-                           Pose3D goal,
-                           HumanoidRobotState humanoidRobotState,
-                           VisibilityGraphsParametersReadOnly visibilityGraphParameters,
-                           Supplier<Boolean> operatorReviewEnabled,
-                           Consumer<ArrayList<Pose3D>> successfulPlanConsumer,
-                           Runnable resetPlanningFailedTimer,
-                           Consumer<ArrayList<Pose3D>> review)
-   {
-      // TODO: Add robot standing still for 20s for real robot
-
-      helper.publishToUI(MapRegionsForUI, mapRegions);
-
-      LogTools.info("Planning body path...");
-
-      // calculate and send body path plan
-      BodyPathPostProcessor pathPostProcessor = new ObstacleAvoidanceProcessor(visibilityGraphParameters);
-      VisibilityGraphPathPlanner bodyPathPlanner = new VisibilityGraphPathPlanner(visibilityGraphParameters,
-                                                                                  pathPostProcessor,
-                                                                                  new YoVariableRegistry(LookAndStepBodyPathPart.class.getSimpleName()));
-
-      bodyPathPlanner.setGoal(goal);
-      bodyPathPlanner.setPlanarRegionsList(mapRegions);
-      FramePose3D leftFootPoseTemp = new FramePose3D();
-      leftFootPoseTemp.setToZero(humanoidRobotState.getSoleFrame(RobotSide.LEFT));
-      FramePose3D rightFootPoseTemp = new FramePose3D();
-      rightFootPoseTemp.setToZero(humanoidRobotState.getSoleFrame(RobotSide.RIGHT));
-      leftFootPoseTemp.changeFrame(ReferenceFrame.getWorldFrame());
-      rightFootPoseTemp.changeFrame(ReferenceFrame.getWorldFrame());
-      bodyPathPlanner.setStanceFootPoses(leftFootPoseTemp, rightFootPoseTemp);
-      Stopwatch stopwatch = new Stopwatch().start();
-      final ArrayList<Pose3D> bodyPathPlanForReview = new ArrayList<>(); // TODO Review making this final
-      bodyPathPlanner.planWaypoints();
-      LogTools.info("Body path planning took {}", stopwatch.totalElapsed()); // 0.1 s
-      //      bodyPathPlan = bodyPathPlanner.getWaypoints();
-      if (bodyPathPlanner.getWaypoints() != null)
-      {
-         for (Pose3DReadOnly poseWaypoint : bodyPathPlanner.getWaypoints())
-         {
-            bodyPathPlanForReview.add(new Pose3D(poseWaypoint));
-         }
-         helper.publishToUI(BodyPathPlanForUI, bodyPathPlanForReview);
-      }
-
-      if (bodyPathPlanForReview.size() >= 2)
-      {
-         if (operatorReviewEnabled.get())
-         {
-             review.accept(bodyPathPlanForReview);
-         }
-         else
-         {
-            successfulPlanConsumer.accept(bodyPathPlanForReview);
-         }
-      }
-      else
-      {
-         resetPlanningFailedTimer.run();
-      }
+      task.run();
    }
 }
---
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBodyPathTask.java
@@ -0,0 +1,227 @@
+package us.ihmc.humanoidBehaviors.lookAndStep;
+
+import us.ihmc.commons.time.Stopwatch;
+import us.ihmc.communication.util.SimpleTimer;
+import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.footstepPlanning.graphSearch.VisibilityGraphPathPlanner;
+import us.ihmc.humanoidBehaviors.tools.BehaviorHelper;
+import us.ihmc.humanoidBehaviors.tools.HumanoidRobotState;
+import us.ihmc.log.LogTools;
+import us.ihmc.pathPlanning.visibilityGraphs.parameters.VisibilityGraphsParametersReadOnly;
+import us.ihmc.pathPlanning.visibilityGraphs.postProcessing.BodyPathPostProcessor;
+import us.ihmc.pathPlanning.visibilityGraphs.postProcessing.ObstacleAvoidanceProcessor;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static us.ihmc.humanoidBehaviors.lookAndStep.LookAndStepBehaviorAPI.BodyPathPlanForUI;
+import static us.ihmc.humanoidBehaviors.lookAndStep.LookAndStepBehaviorAPI.MapRegionsForUI;
+
+public class LookAndStepBodyPathTask implements Builder
+{
+   private final BehaviorHelper helper;
+
+   private final Field<PlanarRegionsList> mapRegions = required();
+   private final Field<Pose3D> goal = required();
+   private final Field<HumanoidRobotState> humanoidRobotState = required();
+   private final Field<VisibilityGraphsParametersReadOnly> visibilityGraphParameters = required();
+   private final Field<Supplier<Boolean>> operatorReviewEnabled = required();
+   private final Field<Consumer<ArrayList<Pose3D>>> successfulPlanConsumer = required();
+   private final Field<Runnable> resetPlanningFailedTimer = required();
+   private final Field<Consumer<ArrayList<Pose3D>>> review = required();
+   private final Field<SimpleTimer.Status> mapRegionsExpirationStatus = required();
+   private final Field<SimpleTimer.Status> planningFailedTimerStatus = required();
+   private final Field<Supplier<Boolean>> isBeingReviewed = required();
+   private final Field<Supplier<Boolean>> needNewPlan = required();
+
+   public LookAndStepBodyPathTask(BehaviorHelper helper)
+   {
+      this.helper = helper;
+   }
+
+   private boolean evaluateEntry()
+   {
+      boolean proceed = true;
+
+      if (!hasGoal())
+      {
+         LogTools.warn("Body path: does not have goal");
+         LogTools.debug("Sending planar regions to UI: {}: {}", LocalDateTime.now(), mapRegions.hashCode());
+         helper.publishToUI(MapRegionsForUI, mapRegions.get());
+         proceed = false;
+      }
+      else if (!regionsOK())
+      {
+         LogTools.warn("Body path: Regions not OK: {}, timePassed: {}, isEmpty: {}",
+                       mapRegions,
+                       mapRegionsExpirationStatus.get().getTimePassedSinceReset(),
+                       mapRegions == null ? null : mapRegions.get().isEmpty());
+         proceed = false;
+      }
+      else if (!needNewPlan.get().get())
+      {
+         LogTools.warn("Body path: New plan not needed");
+         proceed = false;
+      }
+      else if (failedRecently()) // TODO: This could be "run recently" instead of failed recently
+      {
+         LogTools.warn("Body path: failedRecently = true");
+         proceed = false;
+      }
+      else if (isBeingReviewed.get().get())
+      {
+         LogTools.debug("Body path: bodyPathBeingReviewed = true");
+         proceed = false;
+      }
+
+      return proceed;
+   }
+
+   private boolean hasGoal()
+   {
+      return goal != null && !goal.get().containsNaN();
+   }
+
+   private boolean regionsOK()
+   {
+      return mapRegions != null && !mapRegionsExpirationStatus.get().isPastOrNaN() && !mapRegions.get().isEmpty();
+   }
+
+   private boolean failedRecently()
+   {
+      return !planningFailedTimerStatus.get().isPastOrNaN();
+   }
+
+   public void run()
+   {
+      validate();
+
+      if (evaluateEntry())
+      {
+         performTask();
+      }
+
+      invalidate();
+   }
+
+   private void performTask()
+   {
+      // TODO: Add robot standing still for 20s for real robot?
+      helper.publishToUI(MapRegionsForUI, mapRegions.get());
+
+      LogTools.info("Planning body path...");
+
+      // calculate and send body path plan
+      BodyPathPostProcessor pathPostProcessor = new ObstacleAvoidanceProcessor(visibilityGraphParameters.get());
+      VisibilityGraphPathPlanner bodyPathPlanner = new VisibilityGraphPathPlanner(visibilityGraphParameters.get(),
+                                                                                  pathPostProcessor,
+                                                                                  new YoVariableRegistry(LookAndStepBodyPathPart.class.getSimpleName()));
+
+      bodyPathPlanner.setGoal(goal.get());
+      bodyPathPlanner.setPlanarRegionsList(mapRegions.get());
+      FramePose3D leftFootPoseTemp = new FramePose3D();
+      leftFootPoseTemp.setToZero(humanoidRobotState.get().getSoleFrame(RobotSide.LEFT));
+      FramePose3D rightFootPoseTemp = new FramePose3D();
+      rightFootPoseTemp.setToZero(humanoidRobotState.get().getSoleFrame(RobotSide.RIGHT));
+      leftFootPoseTemp.changeFrame(ReferenceFrame.getWorldFrame());
+      rightFootPoseTemp.changeFrame(ReferenceFrame.getWorldFrame());
+      bodyPathPlanner.setStanceFootPoses(leftFootPoseTemp, rightFootPoseTemp);
+      Stopwatch stopwatch = new Stopwatch().start();
+      final ArrayList<Pose3D> bodyPathPlanForReview = new ArrayList<>(); // TODO Review making this final
+      bodyPathPlanner.planWaypoints();
+      LogTools.info("Body path planning took {}", stopwatch.totalElapsed()); // 0.1 s
+      //      bodyPathPlan = bodyPathPlanner.getWaypoints();
+      if (bodyPathPlanner.getWaypoints() != null)
+      {
+         for (Pose3DReadOnly poseWaypoint : bodyPathPlanner.getWaypoints())
+         {
+            bodyPathPlanForReview.add(new Pose3D(poseWaypoint));
+         }
+         helper.publishToUI(BodyPathPlanForUI, bodyPathPlanForReview);
+      }
+
+      if (bodyPathPlanForReview.size() >= 2)
+      {
+         if (operatorReviewEnabled.get().get())
+         {
+            review.get().accept(bodyPathPlanForReview);
+         }
+         else
+         {
+            successfulPlanConsumer.get().accept(bodyPathPlanForReview);
+         }
+      }
+      else
+      {
+         resetPlanningFailedTimer.get().run();
+      }
+   }
+
+   public void setMapRegions(PlanarRegionsList mapRegions)
+   {
+      this.mapRegions.set(mapRegions);
+   }
+
+   public void setGoal(Pose3D goal)
+   {
+      this.goal.set(goal);
+   }
+
+   public void setHumanoidRobotState(HumanoidRobotState humanoidRobotState)
+   {
+      this.humanoidRobotState.set(humanoidRobotState);
+   }
+
+   public void setVisibilityGraphParameters(VisibilityGraphsParametersReadOnly visibilityGraphParameters)
+   {
+      this.visibilityGraphParameters.set(visibilityGraphParameters);
+   }
+
+   public void setOperatorReviewEnabled(Supplier<Boolean> operatorReviewEnabled)
+   {
+      this.operatorReviewEnabled.set(operatorReviewEnabled);
+   }
+
+   public void setSuccessfulPlanConsumer(Consumer<ArrayList<Pose3D>> successfulPlanConsumer)
+   {
+      this.successfulPlanConsumer.set(successfulPlanConsumer);
+   }
+
+   public void setResetPlanningFailedTimer(Runnable resetPlanningFailedTimer)
+   {
+      this.resetPlanningFailedTimer.set(resetPlanningFailedTimer);
+   }
+
+   public void setReviewInitiator(Consumer<ArrayList<Pose3D>> reviewInitiation)
+   {
+      this.review.set(reviewInitiation);
+   }
+
+   public void setMapRegionsExpirationStatus(SimpleTimer.Status mapRegionsExpirationStatus)
+   {
+      this.mapRegionsExpirationStatus.set(mapRegionsExpirationStatus);
+   }
+
+   public void setPlanningFailedTimerStatus(SimpleTimer.Status planningFailedTimerStatus)
+   {
+      this.planningFailedTimerStatus.set(planningFailedTimerStatus);
+   }
+
+   public void setIsBeingReviewed(Supplier<Boolean> isBeingReviewed)
+   {
+      this.isBeingReviewed.set(isBeingReviewed);
+   }
+
+   public void setNeedNewPlan(Supplier<Boolean> needNewPlan)
+   {
+      this.needNewPlan.set(needNewPlan);
+   }
+}
