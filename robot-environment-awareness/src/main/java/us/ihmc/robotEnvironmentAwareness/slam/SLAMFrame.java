5b3e88ac8c6f9e489ac790e745f5dbcad561bc28
edile <edile@DESKTOP-1RF6LR7.attlocal.net>
2020-06-03 11:30:38 -0500

    Added surface normal and viz.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/slam/SLAMFrame.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/slam/SLAMFrame.java
@@ -1,11 +1,22 @@
 package us.ihmc.robotEnvironmentAwareness.slam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
 import us.ihmc.communication.packets.MessageTools;
+import us.ihmc.euclid.geometry.Plane3D;
+import us.ihmc.euclid.geometry.interfaces.Plane3DReadOnly;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.jOctoMap.iterators.OcTreeIterable;
+import us.ihmc.jOctoMap.iterators.OcTreeIteratorFactory;
+import us.ihmc.jOctoMap.node.NormalOcTreeNode;
+import us.ihmc.jOctoMap.normalEstimation.NormalEstimationParameters;
+import us.ihmc.jOctoMap.ocTree.NormalOcTree;
+import us.ihmc.jOctoMap.pointCloud.ScanCollection;
 import us.ihmc.robotEnvironmentAwareness.communication.converters.PointCloudCompression;
 import us.ihmc.robotEnvironmentAwareness.slam.tools.SLAMTools;
 
@@ -103,6 +114,67 @@ public class SLAMFrame
          optimizedPointCloudToWorld[i].set(pointCloudToSensorFrame[i]);
          optimizedSensorPoseToWorld.transform(optimizedPointCloudToWorld[i]);
       }
+      
+      for (int i = 0; i < surfaceElements.size(); i++)
+      {
+         Plane3D surfel = surfaceElements.get(i);
+         surfel.set(surfaceElementsToSensor.get(i));
+         getSensorPose().transform(surfel.getPoint());
+         getSensorPose().transform(surfel.getNormal());
+      }
+   }
+   
+   private final List<Plane3D> surfaceElements = new ArrayList<>();
+   private final List<Plane3DReadOnly> surfaceElementsToSensor = new ArrayList<>();
+   
+   public void registerSurfaceElements(NormalOcTree map, double windowMargin, double surfaceElementResolution, int minimumNumberOfHits)
+   {
+      surfaceElements.clear();
+      surfaceElementsToSensor.clear();
+      NormalOcTree frameMap = new NormalOcTree(surfaceElementResolution);
+
+      ScanCollection scanCollection = new ScanCollection();
+      int numberOfPoints = getOriginalPointCloud().length;
+
+      scanCollection.setSubSampleSize(numberOfPoints);
+      scanCollection.addScan(SLAMTools.toScan(getOriginalPointCloud(), getOriginalPointCloudToSensorPose(), getOriginalSensorPose(), map, windowMargin));
+
+      frameMap.insertScanCollection(scanCollection, false);
+      frameMap.enableParallelComputationForNormals(true);
+
+      NormalEstimationParameters normalEstimationParameters = new NormalEstimationParameters();
+      normalEstimationParameters.setNumberOfIterations(10);
+      normalEstimationParameters.setMaxDistanceFromPlane(surfaceElementResolution);
+      frameMap.setNormalEstimationParameters(normalEstimationParameters);
+      frameMap.updateNormals();
+
+      OcTreeIterable<NormalOcTreeNode> iterable = OcTreeIteratorFactory.createIterable(frameMap.getRoot());
+      for (NormalOcTreeNode node : iterable)
+      {
+         if (node.getNumberOfHits() >= minimumNumberOfHits)
+         {
+            //if (node.getNormalAverageDeviation() < 0.00005)
+            {
+               Plane3D surfaceElement = new Plane3D();
+               node.getNormal(surfaceElement.getNormal());
+               node.getHitLocation(surfaceElement.getPoint());
+
+               surfaceElements.add(surfaceElement);
+               surfaceElementsToSensor.add(SLAMTools.createConvertedSurfaceElementToSensorPose(getOriginalSensorPose(), surfaceElement));
+            }
+         }
+      }
+   }
+   
+
+   public List<Plane3D> getSurfaceElements()
+   {
+      return surfaceElements;
+   }
+
+   public List<Plane3DReadOnly> getSurfaceElementsToSensor()
+   {
+      return surfaceElementsToSensor;
    }
    
    public void setConfidenceFactor(double value)
