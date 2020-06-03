2189b3bc2281a28439da59c41f8d46947ffe11b5
edile <edile@DESKTOP-1RF6LR7.attlocal.net>
2020-06-03 11:30:38 -0500

    Added surface normal and viz.

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/slamTools/SLAMFrameYoGraphicsManager.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/slamTools/SLAMFrameYoGraphicsManager.java
@@ -1,19 +1,23 @@
 package us.ihmc.avatar.slamTools;
 
+import java.util.List;
 import java.util.Random;
 
 import gnu.trove.list.array.TIntArrayList;
+import us.ihmc.euclid.geometry.Plane3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicCoordinateSystem;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicPosition;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicVector;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsList;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.robotEnvironmentAwareness.slam.SLAMFrame;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoFramePoint3D;
 import us.ihmc.yoVariables.variable.YoFramePose3D;
+import us.ihmc.yoVariables.variable.YoFrameVector3D;
 
 public class SLAMFrameYoGraphicsManager
 {
@@ -24,10 +28,18 @@ public class SLAMFrameYoGraphicsManager
    private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
 
    private final YoFramePose3D yoFrameSensorPose;
-   private final YoFramePoint3D[] yoFramePointCloud;
-
    private final YoGraphicCoordinateSystem yoGraphicSensorPose;
+
+   private final YoFramePoint3D[] yoFramePointCloud;
    private final YoGraphicPosition[] yoGraphicPointCloud;
+
+   private final YoFramePoint3D[] yoFrameSurfelPoints;
+   private final YoGraphicPosition[] yoGraphicSurfelPoints;
+
+   private static final double NORMAL_VIZ_LENGTH = 0.05;
+
+   private final YoFrameVector3D[] yoFrameSurfelNormals;
+   private final YoGraphicVector[] yoGraphicSurfelNormals;
 
    public SLAMFrameYoGraphicsManager(String prefix, SLAMFrame frame, int size, AppearanceDefinition appearance, YoVariableRegistry registry,
                                      YoGraphicsListRegistry graphicsRegistry)
@@ -51,7 +63,7 @@ public class SLAMFrameYoGraphicsManager
       {
          Random selector = new Random(0612L);
          sizeOfPointCloud = size;
-         while(indicesArray.size() != sizeOfPointCloud)
+         while (indicesArray.size() != sizeOfPointCloud)
          {
             int selectedIndex = selector.nextInt(slamFrame.getPointCloud().length);
             if (!indicesArray.contains(selectedIndex))
@@ -70,6 +82,27 @@ public class SLAMFrameYoGraphicsManager
          yoGraphicListRegistry.add(yoGraphicPointCloud[i]);
       }
 
+      List<Plane3D> surfaceElements = frame.getSurfaceElements();
+      int numberOfSurfel = surfaceElements.size();
+
+      yoFrameSurfelPoints = new YoFramePoint3D[numberOfSurfel];
+      yoGraphicSurfelPoints = new YoGraphicPosition[numberOfSurfel];
+
+      yoFrameSurfelNormals = new YoFrameVector3D[numberOfSurfel];
+      yoGraphicSurfelNormals = new YoGraphicVector[numberOfSurfel];
+      for (int i = 0; i < numberOfSurfel; i++)
+      {
+         yoFrameSurfelPoints[i] = new YoFramePoint3D(prefix + "_SurfelPoint_" + i, worldFrame, registry);
+         yoGraphicSurfelPoints[i] = new YoGraphicPosition(prefix + "_SurfelPointViz_" + i, yoFrameSurfelPoints[i], 0.003, appearance);
+
+         yoFrameSurfelNormals[i] = new YoFrameVector3D(prefix + "_SurfelNormal_" + i, worldFrame, registry);
+         yoGraphicSurfelNormals[i] = new YoGraphicVector(prefix + "_SurfelNormalViz_"
+               + i, yoFrameSurfelPoints[i], yoFrameSurfelNormals[i], NORMAL_VIZ_LENGTH, appearance);
+
+         yoGraphicListRegistry.add(yoGraphicSurfelPoints[i]);
+         yoGraphicListRegistry.add(yoGraphicSurfelNormals[i]);
+      }
+
       graphicsRegistry.registerYoGraphicsList(yoGraphicListRegistry);
    }
 
@@ -86,6 +119,12 @@ public class SLAMFrameYoGraphicsManager
       for (int i = 0; i < sizeOfPointCloud; i++)
       {
          yoFramePointCloud[i].set(pointCloud[indicesArray.get(i)]);
+      }
+      List<Plane3D> surfaceElements = slamFrame.getSurfaceElements();
+      for (int i = 0; i < surfaceElements.size(); i++)
+      {
+         yoFrameSurfelPoints[i].set(surfaceElements.get(i).getPoint());
+         yoFrameSurfelNormals[i].set(surfaceElements.get(i).getNormal());
       }
    }
 }
