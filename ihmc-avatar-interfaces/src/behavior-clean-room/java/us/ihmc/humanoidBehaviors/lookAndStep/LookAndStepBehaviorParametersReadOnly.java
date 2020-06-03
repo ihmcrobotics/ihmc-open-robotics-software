b56e430791cfefe171adeaa6473ee09f51b03698
Duncan Calvert <dcalvert@ihmc.us>
2020-06-02 21:57:04 -0500

    Move look and step behavior parameters getters to default methods in a readonly interface.

---
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/lookAndStep/LookAndStepBehaviorParametersReadOnly.java
@@ -0,0 +1,81 @@
+package us.ihmc.humanoidBehaviors.lookAndStep;
+
+import us.ihmc.tools.property.StoredPropertySetReadOnly;
+
+public interface LookAndStepBehaviorParametersReadOnly extends StoredPropertySetReadOnly
+{
+   default double getMaxPlanStrayDistance()
+   {
+      return get(LookAndStepBehaviorParameters.maxPlanStrayDistance);
+   }
+
+   default double getGoalSatisfactionRadius()
+   {
+      return get(LookAndStepBehaviorParameters.goalSatisfactionRadius);
+   }
+
+   default double getPlanarRegionsExpiration()
+   {
+      return get(LookAndStepBehaviorParameters.planarRegionsExpiration);
+   }
+
+   default double getDirection()
+   {
+      return get(LookAndStepBehaviorParameters.direction);
+   }
+
+   default double getWiggleInsideDeltaOverride()
+   {
+      return get(LookAndStepBehaviorParameters.wiggleInsideDeltaOverride);
+   }
+
+   default double getPlanHorizon()
+   {
+      return get(LookAndStepBehaviorParameters.planHorizon);
+   }
+
+   default double getIdealFootstepLengthOverride()
+   {
+      return get(LookAndStepBehaviorParameters.idealFootstepLengthOverride);
+   }
+
+   default double getCliffBaseHeightToAvoidOverride()
+   {
+      return get(LookAndStepBehaviorParameters.cliffBaseHeightToAvoidOverride);
+   }
+
+   default boolean getEnableConcaveHullWigglerOverride()
+   {
+      return get(LookAndStepBehaviorParameters.enableConcaveHullWigglerOverride);
+   }
+
+   default double getFootstepPlannerTimeout()
+   {
+      return get(LookAndStepBehaviorParameters.footstepPlannerTimeout);
+   }
+
+   default double getSwingTime()
+   {
+      return get(LookAndStepBehaviorParameters.swingTime);
+   }
+
+   default double getTransferTime()
+   {
+      return get(LookAndStepBehaviorParameters.transferTime);
+   }
+
+   default double getWaitTimeAfterPlanFailed()
+   {
+      return get(LookAndStepBehaviorParameters.waitTimeAfterPlanFailed);
+   }
+
+   default boolean getReturnBestEffortPlanOverride()
+   {
+      return get(LookAndStepBehaviorParameters.returnBestEffortPlanOverride);
+   }
+
+   default double getPercentSwingToWait()
+   {
+      return get(LookAndStepBehaviorParameters.percentSwingToWait);
+   }
+}
