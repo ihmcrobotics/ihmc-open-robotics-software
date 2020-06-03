68d9a1e11ba2c0823a6108f2f3fbf23c04fac53b
Sylvain Bertrand <sbertrand@ihmc.us>
2020-06-02 19:35:19 -0500

    Forgot to rename some additional classes.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/FourBarKinematicLoopFunctionTools.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/FourBarKinematicLoopFunctionTools.java
@@ -20,7 +20,7 @@ import us.ihmc.robotics.kinematics.fourbar.FourBar;
 import us.ihmc.robotics.kinematics.fourbar.FourBarAngle;
 import us.ihmc.robotics.kinematics.fourbar.FourBarVertex;
 
-public class FourBarKinematicLoopTools
+public class FourBarKinematicLoopFunctionTools
 {
    private static final String A_NAME = "A";
    private static final String B_NAME = "B";
