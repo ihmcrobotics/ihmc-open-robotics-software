99cce2194f6416dae7d8934df765c656e4612af1
edile <edile@DESKTOP-1RF6LR7.ihmc.us>
2020-06-08 12:11:58 -0500

    Tested importer and exporter of raw data.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/SLAMDataManagerAnchorPaneController.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/SLAMDataManagerAnchorPaneController.java
@@ -1,23 +1,31 @@
 package us.ihmc.robotEnvironmentAwareness.ui.controller;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.Executor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import controller_msgs.msg.dds.PlanarRegionsListMessage;
+import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Window;
+import us.ihmc.communication.IHMCROS2Publisher;
+import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.packets.PlanarRegionMessageConverter;
+import us.ihmc.log.LogTools;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.robotEnvironmentAwareness.communication.SLAMModuleAPI;
 import us.ihmc.robotEnvironmentAwareness.tools.ExecutorServiceTools;
 import us.ihmc.robotEnvironmentAwareness.tools.ExecutorServiceTools.ExceptionHandling;
+import us.ihmc.robotEnvironmentAwareness.ui.io.StereoVisionPointCloudDataLoader;
 import us.ihmc.robotics.PlanarRegionFileTools;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.ros2.Ros2Node;
 
 public class SLAMDataManagerAnchorPaneController extends REABasicUIController
 {
@@ -45,9 +53,14 @@ public class SLAMDataManagerAnchorPaneController extends REABasicUIController
 
    private Window ownerWindow;
 
+   private static long DEFAULT_PUBLISHING_PERIOD_MS = 250;
+   private final ScheduledExecutorService executor = ExecutorServiceTools.newSingleThreadScheduledExecutor(getClass(), ExceptionHandling.CANCEL_AND_REPORT);
+   private final List<StereoVisionPointCloudMessage> stereoVisionPointCloudMessagesToPublish = new ArrayList<>();
+   private int indexToPublish = 0;
+   
    public SLAMDataManagerAnchorPaneController()
    {
-
+      executor.scheduleAtFixedRate(this::publish, 0, DEFAULT_PUBLISHING_PERIOD_MS, TimeUnit.MILLISECONDS);
    }
 
    public void setMainWindow(Window ownerWindow)
@@ -65,8 +78,6 @@ public class SLAMDataManagerAnchorPaneController extends REABasicUIController
       uiMessager.bindBidirectionalGlobal(SLAMModuleAPI.UIRawDataExportRequest, exportRawData.selectedProperty());
    }
 
-   private final Executor importExecutor = ExecutorServiceTools.newSingleThreadScheduledExecutor(getClass(), ExceptionHandling.CANCEL_AND_REPORT);
-
    @FXML
    private void exportSLAMData()
    {
@@ -76,7 +87,15 @@ public class SLAMDataManagerAnchorPaneController extends REABasicUIController
    @FXML
    private void importRawData()
    {
-      //TODO: implement
+      String rawDataFilePath = currentRawDataInputFolderTextField.getText();
+      File rawDataFile = new File(rawDataFilePath);
+      List<StereoVisionPointCloudMessage> messagesFromFile = StereoVisionPointCloudDataLoader.getMessagesFromFile(rawDataFile);
+      stereoVisionPointCloudMessagesToPublish.clear();
+      stereoVisionPointCloudMessagesToPublish.addAll(messagesFromFile);
+      indexToPublish = 0;
+
+      LogTools.info("Loading Data Is Done " + messagesFromFile.size() + " messages.");
+      LogTools.info("Publishing Is Started.");
    }
 
    @FXML
@@ -122,5 +141,32 @@ public class SLAMDataManagerAnchorPaneController extends REABasicUIController
       importPlanarRegionsDirectoryChooser.setInitialDirectory(defaultImportPlanarRegionsFile);
       String newPath = importPlanarRegionsDirectoryChooser.showDialog(ownerWindow).getAbsolutePath();
       Platform.runLater(() -> currentPlanarRegionsInputFolderTextField.setText(newPath));
+   }
+
+   private final Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS, "stereoVisionPublisherNode");
+   private final IHMCROS2Publisher<StereoVisionPointCloudMessage> stereoVisionPublisher = ROS2Tools.createPublisherTypeNamed(ros2Node,
+                                                                                                                             StereoVisionPointCloudMessage.class,
+                                                                                                                             ROS2Tools.IHMC_ROOT);
+   
+   private void publish()
+   {
+      if (stereoVisionPointCloudMessagesToPublish.size() == indexToPublish)
+      {
+         try
+         {
+            return;
+         }
+         catch (Exception e)
+         {
+            e.printStackTrace();
+         }
+      }
+      else
+      {
+         System.out.println("publish " + indexToPublish);
+         StereoVisionPointCloudMessage stereoVisionPointCloudMessage = stereoVisionPointCloudMessagesToPublish.get(indexToPublish);
+         stereoVisionPublisher.publish(stereoVisionPointCloudMessage);
+         indexToPublish++;
+      }
    }
 }
