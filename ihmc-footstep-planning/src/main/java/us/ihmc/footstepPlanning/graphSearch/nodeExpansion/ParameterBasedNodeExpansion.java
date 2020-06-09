5203ba46e392e7b40ca86f9305ebfbed6ca09dd5
smccrory <smccrory@ihmc.us>
2020-06-09 16:58:53 -0500

    Filtering footstep node expansion based on clearance parameter

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/nodeExpansion/ParameterBasedNodeExpansion.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/nodeExpansion/ParameterBasedNodeExpansion.java
@@ -5,12 +5,16 @@ import gnu.trove.list.array.TIntArrayList;
 import us.ihmc.commons.InterpolationTools;
 import us.ihmc.commons.MathTools;
 import us.ihmc.euclid.axisAngle.AxisAngle;
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.tools.EuclidCoreTools;
 import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
+import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNodeTools;
 import us.ihmc.footstepPlanning.graphSearch.graph.LatticeNode;
 import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParametersReadOnly;
+import us.ihmc.robotics.geometry.ConvexPolygonTools;
 import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.robotics.robotSide.SideDependentList;
 
 import java.util.*;
 import java.util.function.UnaryOperator;
@@ -23,6 +27,9 @@ public class ParameterBasedNodeExpansion implements FootstepNodeExpansion
    private final IdealStepProximityComparator idealStepProximityComparator = new IdealStepProximityComparator();
    private final HashMap<FootstepNode, PartialExpansionManager> expansionManagers = new HashMap<>();
 
+   private final ConvexPolygonTools convexPolygonTools = new ConvexPolygonTools();
+   private final SideDependentList<ConvexPolygon2D> footPolygons;
+
    private final TDoubleArrayList xOffsets = new TDoubleArrayList();
    private final TDoubleArrayList yOffsets = new TDoubleArrayList();
    private final TDoubleArrayList yawOffsets = new TDoubleArrayList();
@@ -33,10 +40,12 @@ public class ParameterBasedNodeExpansion implements FootstepNodeExpansion
    private final TIntArrayList xyExpansionMask = new TIntArrayList();
    private final TIntArrayList yawExpansionMask = new TIntArrayList();
 
-   public ParameterBasedNodeExpansion(FootstepPlannerParametersReadOnly parameters, UnaryOperator<FootstepNode> idealStepSupplier)
+   public ParameterBasedNodeExpansion(FootstepPlannerParametersReadOnly parameters, UnaryOperator<FootstepNode> idealStepSupplier, SideDependentList<ConvexPolygon2D> footPolygons)
    {
       this.parameters = parameters;
       this.idealStepSupplier = idealStepSupplier;
+      this.footPolygons = footPolygons;
+
       fillExpansionMask();
    }
 
@@ -70,10 +79,6 @@ public class ParameterBasedNodeExpansion implements FootstepNodeExpansion
             if (reachSquared > maxReachSquared)
                continue;
 
-            // TODO
-//            if (Math.abs(x) <= parameters.getMinXClearanceFromStance() && Math.abs(y) <= parameters.getMinYClearanceFromStance())
-//               continue;
-
             double reachFraction = EuclidCoreTools.fastSquareRoot(reachSquared) / parameters.getMaximumStepReach();
             double minYawAtFullExtension = (1.0 - parameters.getStepYawReductionFactorAtMaxReach()) * parameters.getMinimumStepYaw();
             double maxYawAtFullExtension = (1.0 - parameters.getStepYawReductionFactorAtMaxReach()) * parameters.getMaximumStepYaw();
@@ -83,6 +88,11 @@ public class ParameterBasedNodeExpansion implements FootstepNodeExpansion
 
             for (double yaw = minYaw; yaw <= maxYaw; yaw += LatticeNode.gridSizeYaw)
             {
+               FootstepNodeTools.computeDistanceBetweenFootPolygons(new FootstepNode(0.0, 0.0, 0.0, RobotSide.LEFT),
+                                                                    new FootstepNode(x, y, yaw, RobotSide.RIGHT),
+                                                                    footPolygons,
+                                                                    convexPolygonTools);
+
                xOffsets.add(x);
                yOffsets.add(y);
                yawOffsets.add(yaw);
