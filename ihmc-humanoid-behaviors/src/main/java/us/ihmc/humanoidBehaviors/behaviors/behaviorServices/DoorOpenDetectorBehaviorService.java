74847678904af2cb9f774971dfeac46f12469749
jcarff <jcarff@ihmc.us>
2020-06-10 11:58:07 -0500

    debugging walk through door behavior

--- ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/behaviorServices/DoorOpenDetectorBehaviorService.java
+++ ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/behaviorServices/DoorOpenDetectorBehaviorService.java
@@ -25,11 +25,13 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
 
    private DoorLocationPacket latestDoorLocationPacketRecieved;
    private boolean doorOpen = false;
-   private float openDistance = 0.17f;
+   private float openAngle = 0.17f;
+   private float closeAngle = 0.07f;
    private boolean run = false;
-   
-   private long timeOfLastUpdate =0;
+
+   private long timeOfLastUpdate = 0;
    private long timeToWait = 2000;
+
 
    protected final AtomicReference<DoorLocationPacket> doorLocationQueue = new AtomicReference<DoorLocationPacket>();
 
@@ -37,7 +39,7 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
    {
       super(robotName, ThreadName, ros2Node);//, yoGraphicsListRegistry);
 
-      createSubscriber(DoorLocationPacket.class, IHMCHumanoidBehaviorManager.getInputTopic(robotName), doorLocationQueue::set); 
+      createSubscriber(DoorLocationPacket.class, IHMCHumanoidBehaviorManager.getInputTopic(robotName), doorLocationQueue::set);
 
       initialize();
    }
@@ -76,14 +78,12 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
       averageOrigin = null;
       run = false;
       doorPoses.clear();
-
    }
 
    @Override
    public void doThreadAction()
    {
 
-	   
       //super.doThreadAction();
       if (run)
       {
@@ -130,35 +130,29 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
                   {
                      averageCurrentDoorLocation = averageFramePoses(doorPoses);
                   }
-                 // System.out.println("***************************");
-               //   System.out.println(averageOrigin.getYaw());
-                  System.out.println(averageCurrentDoorLocation.getYaw());
-                    System.out.println("##"+Math.abs(Math.abs(averageOrigin.getYaw()) -Math.abs(averageCurrentDoorLocation.getYaw())));
-                //    System.out.println(System.currentTimeMillis() - timeOfLastUpdate> timeToWait);
-               //   System.out.println("&"+averageOrigin.getPitch());
-                //  System.out.println("&"+averageCurrentDoorLocation.getPitch());
-                //  System.out.println("#"+averageOrigin.getRoll());
-                //  System.out.println("#"+averageCurrentDoorLocation.getRoll());
-                //  System.out.println(averageOrigin.getPositionDistance(averageCurrentDoorLocation));
-                  
-                  if(System.currentTimeMillis() - timeOfLastUpdate> timeToWait)
-                  {
-                     if (averageCurrentDoorLocation != null && Math.abs(averageOrigin.getYaw() -averageCurrentDoorLocation.getYaw()) > openDistance)
-                     {
-                    	 if(!doorOpen)
-                             timeOfLastUpdate = System.currentTimeMillis();
+                  // System.out.println("***************************");
+                  //   System.out.println(averageOrigin.getYaw());
+//                  System.out.println(Math.toDegrees(averageOrigin.getYaw())+" - "+Math.toDegrees(averageCurrentDoorLocation.getYaw()));
+//                  System.out.println("##" + Math.toDegrees(Math.abs(Math.abs(averageOrigin.getYaw()) - Math.abs(averageCurrentDoorLocation.getYaw()))));
+                  //    System.out.println(System.currentTimeMillis() - timeOfLastUpdate> timeToWait);
+                  //   System.out.println("&"+averageOrigin.getPitch());
+                  //  System.out.println("&"+averageCurrentDoorLocation.getPitch());
+                  //  System.out.println("#"+averageOrigin.getRoll());
+                  //  System.out.println("#"+averageCurrentDoorLocation.getRoll());
+                  //  System.out.println(averageOrigin.getPositionDistance(averageCurrentDoorLocation));
 
+                  if (averageCurrentDoorLocation != null)
+                  {
+                     if (!doorOpen && Math.abs(averageOrigin.getYaw() - averageCurrentDoorLocation.getYaw()) > openAngle)
+                     {
                         doorOpen = true;
                      }
-                     else
+                     else if(doorOpen && Math.abs(averageOrigin.getYaw() - averageCurrentDoorLocation.getYaw()) < closeAngle)
                      {
-                    	 if(doorOpen)
-                             timeOfLastUpdate = System.currentTimeMillis();
-
                         doorOpen = false;
                      }
                   }
-                  
+
                }
             }
 
@@ -167,40 +161,40 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
       }
    }
 
-   AverageQuaternionCalculator averageQuaternionCalculator = new AverageQuaternionCalculator();
 
-   
    private FramePose3D averageFramePoses(ArrayList<FramePose3D> poses)
    {
       if (poses.size() > 0)
       {
-    	  
-    	  
-    	  averageQuaternionCalculator.reset();
+         AverageQuaternionCalculator averageQuaternionCalculator = new AverageQuaternionCalculator();
 
-       
-    	  
+         averageQuaternionCalculator.reset();
+
          float numberOfPoses = poses.size();
          FramePose3D aveagedPose = new FramePose3D(ReferenceFrame.getWorldFrame());
+//         System.out.println("******************");
          for (FramePose3D pose : poses)
          {
+            
+//            System.out.print(Math.toDegrees(pose.getOrientation().getYaw())+",");
             aveagedPose.setX(aveagedPose.getX() + pose.getX());
             aveagedPose.setY(aveagedPose.getY() + pose.getY());
             aveagedPose.setZ(aveagedPose.getZ() + pose.getZ());
             averageQuaternionCalculator.queueQuaternion(new Quaternion(pose.getOrientation()));
 
          }
+         
+ //        System.out.println("\n******************");
+
          aveagedPose.setX(aveagedPose.getX() / numberOfPoses);
          aveagedPose.setY(aveagedPose.getY() / numberOfPoses);
          aveagedPose.setZ(aveagedPose.getZ() / numberOfPoses);
-         
-         
+
          averageQuaternionCalculator.compute();
          Quaternion actualAverageQuat = new Quaternion();
          averageQuaternionCalculator.getAverageQuaternion(actualAverageQuat);
          aveagedPose.setOrientation(actualAverageQuat);
-         
-         
+ //        System.out.println("***"+aveagedPose.getOrientation().getYaw());
          return aveagedPose;
       }
       else
