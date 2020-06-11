b53543269cf3d00a40f5850be219f0604152043c
Duncan Calvert <dcalvert@ihmc.us>
2020-06-11 14:48:34 -0500

    Test and fix ManagedROS2Node.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/ros2/ManagedROS2Publisher.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/ros2/ManagedROS2Publisher.java
@@ -3,26 +3,23 @@ package us.ihmc.humanoidBehaviors.tools.ros2;
 import us.ihmc.ros2.Ros2PublisherBasics;
 
 import java.io.IOException;
+import java.util.function.Supplier;
 
 public class ManagedROS2Publisher<T> implements Ros2PublisherBasics<T>
 {
    private final Ros2PublisherBasics<T> publisher;
-   private boolean enabled = true;
+   private final Supplier<Boolean> enabled;
 
-   public ManagedROS2Publisher(Ros2PublisherBasics<T> publisher)
+   public ManagedROS2Publisher(Ros2PublisherBasics<T> publisher, Supplier<Boolean> enabled)
    {
       this.publisher = publisher;
-   }
-
-   public void setEnabled(boolean enabled)
-   {
       this.enabled = enabled;
    }
 
    @Override
    public void publish(T data) throws IOException
    {
-      if (enabled)
+      if (enabled.get())
       {
          publisher.publish(data);
       }
