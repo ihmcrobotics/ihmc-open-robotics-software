daccf055467f83559195d31d26b5907ebb210786
Sylvain Bertrand <sbertrand@ihmc.us>
2020-06-18 12:38:17 -0500

    :arrow_up: ihmc-graphics-description 0.18.0

--- valkyrie/build.gradle.kts
+++ valkyrie/build.gradle.kts
@@ -37,7 +37,7 @@ mainDependencies {
    }
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.17.0-ejml-0.39-beta-2")
    api("us.ihmc:simulation-construction-set:0.18.0-ejml-0.39-beta-1")
-   api("us.ihmc:ihmc-graphics-description:0.17.0-ejml-0.39-beta-1")
+   api("us.ihmc:ihmc-graphics-description:0.18.0")
    api("us.ihmc:ihmc-robot-description:0.18.0-ejml-0.39-beta-1")
    api("us.ihmc:ihmc-communication:source")
    api("us.ihmc:ihmc-humanoid-robotics:source")
