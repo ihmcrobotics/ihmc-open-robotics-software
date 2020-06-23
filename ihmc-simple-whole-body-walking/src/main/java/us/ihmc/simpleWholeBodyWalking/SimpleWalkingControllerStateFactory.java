4a3027c12cffbdfc13e643b1dd82f5396acc800a
Connor Herron <cwh@vt.edu>
2020-06-23 14:35:41 -0500

    Added the set ICP parameters to SimpleWalkingControllerStateFactory.

--- ihmc-simple-whole-body-walking/src/main/java/us/ihmc/simpleWholeBodyWalking/SimpleWalkingControllerStateFactory.java
+++ ihmc-simple-whole-body-walking/src/main/java/us/ihmc/simpleWholeBodyWalking/SimpleWalkingControllerStateFactory.java
@@ -1,5 +1,6 @@
 package us.ihmc.simpleWholeBodyWalking;
 
+import us.ihmc.commonWalkingControlModules.configurations.ICPWithTimeFreezingPlannerParameters;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.HighLevelControllerFactoryHelper;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.HighLevelControllerStateFactory;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.HighLevelControllerState;
@@ -18,7 +19,7 @@ public class SimpleWalkingControllerStateFactory implements HighLevelControllerS
          SimpleControlManagerFactory managerFactory = new SimpleControlManagerFactory(controllerFactoryHelper.getHighLevelHumanoidControllerToolbox().getYoVariableRegistry());
          managerFactory.setHighLevelHumanoidControllerToolbox(controllerFactoryHelper.getHighLevelHumanoidControllerToolbox());
          managerFactory.setWalkingControllerParameters(controllerFactoryHelper.getWalkingControllerParameters());
-         
+         managerFactory.setCapturePointPlannerParameters((ICPWithTimeFreezingPlannerParameters) controllerFactoryHelper.getIcpPlannerParameters());
          walkingControllerState = new SimpleWalkingControllerState(controllerFactoryHelper.getCommandInputManager(), controllerFactoryHelper.getStatusMessageOutputManager(),
                                                                    managerFactory, controllerFactoryHelper.getHighLevelHumanoidControllerToolbox(),
                                                                    controllerFactoryHelper.getHighLevelControllerParameters(),
