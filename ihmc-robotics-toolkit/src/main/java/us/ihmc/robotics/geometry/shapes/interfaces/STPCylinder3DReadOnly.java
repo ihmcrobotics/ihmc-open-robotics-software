2148d77f7699e9cf7f88665dc2ae69625a8a9be4
Sylvain Bertrand <sbertrand@ihmc.us>
2020-07-02 12:06:10 -0500

    Implemented STPCylinder3D

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/geometry/shapes/interfaces/STPCylinder3DReadOnly.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/geometry/shapes/interfaces/STPCylinder3DReadOnly.java
@@ -1,20 +1,20 @@
 package us.ihmc.robotics.geometry.shapes.interfaces;
 
 import us.ihmc.euclid.geometry.interfaces.BoundingBox3DBasics;
-import us.ihmc.euclid.shape.primitives.interfaces.Ramp3DReadOnly;
+import us.ihmc.euclid.shape.primitives.interfaces.Cylinder3DReadOnly;
 import us.ihmc.euclid.tools.EuclidCoreTools;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DBasics;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DBasics;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 
-public interface STPRamp3DReadOnly extends STPShape3DReadOnly, Ramp3DReadOnly
+public interface STPCylinder3DReadOnly extends STPShape3DReadOnly, Cylinder3DReadOnly
 {
    @Override
    default void getBoundingBox(BoundingBox3DBasics boundingBoxToPack)
    {
       // TODO Naive implementation of the bounding box. It is guaranteed to contain the shape but it is not the tightest bounding box.
-      Ramp3DReadOnly.super.getBoundingBox(boundingBoxToPack);
+      Cylinder3DReadOnly.super.getBoundingBox(boundingBoxToPack);
       boundingBoxToPack.getMinPoint().sub(getMaximumMargin(), getMaximumMargin(), getMaximumMargin());
       boundingBoxToPack.getMaxPoint().add(getMaximumMargin(), getMaximumMargin(), getMaximumMargin());
    }
@@ -23,16 +23,16 @@ public interface STPRamp3DReadOnly extends STPShape3DReadOnly, Ramp3DReadOnly
    @Override
    boolean getSupportingVertex(Vector3DReadOnly supportDirection, Point3DBasics supportingVertexToPack);
 
-   default boolean equals(STPRamp3DReadOnly other)
+   default boolean equals(STPCylinder3DReadOnly other)
    {
-      if (!Ramp3DReadOnly.super.equals(other))
+      if (!Cylinder3DReadOnly.super.equals(other))
          return false;
       return getMinimumMargin() == other.getMinimumMargin() && getMaximumMargin() == other.getMaximumMargin();
    }
 
-   default boolean epsilonEquals(STPRamp3DReadOnly other, double epsilon)
+   default boolean epsilonEquals(STPCylinder3DReadOnly other, double epsilon)
    {
-      if (!Ramp3DReadOnly.super.epsilonEquals(other, epsilon))
+      if (!Cylinder3DReadOnly.super.epsilonEquals(other, epsilon))
          return false;
       if (!EuclidCoreTools.epsilonEquals(getMinimumMargin(), other.getMinimumMargin(), epsilon))
          return false;
@@ -41,9 +41,9 @@ public interface STPRamp3DReadOnly extends STPShape3DReadOnly, Ramp3DReadOnly
       return true;
    }
 
-   default boolean geometricallyEquals(STPRamp3DReadOnly other, double epsilon)
+   default boolean geometricallyEquals(STPCylinder3DReadOnly other, double epsilon)
    {
-      if (!Ramp3DReadOnly.super.geometricallyEquals(other, epsilon))
+      if (!Cylinder3DReadOnly.super.geometricallyEquals(other, epsilon))
          return false;
       if (!EuclidCoreTools.epsilonEquals(getMinimumMargin(), other.getMinimumMargin(), epsilon))
          return false;
@@ -52,7 +52,7 @@ public interface STPRamp3DReadOnly extends STPShape3DReadOnly, Ramp3DReadOnly
       return true;
    }
 
-   // The following part of the API has not been implemented for STP ramp yet, let's prevent their use for now.
+   // The following part of the API has not been implemented for STP cylinder yet, let's prevent their use for now.
 
    @Override
    default boolean evaluatePoint3DCollision(Point3DReadOnly pointToCheck, Point3DBasics closestPointOnSurfaceToPack, Vector3DBasics normalAtClosestPointToPack)
