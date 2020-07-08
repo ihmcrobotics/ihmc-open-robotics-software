bcdf576a893fed5b5bad43dcec4bb2cb05f64746
jcarff <jcarff@ihmc.us>
2020-07-08 11:04:22 -0500

    Adding a timer to the open door behavior so that is makes sure to have a result from the door detector before determining if the door was not open.

--- ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/behaviorServices/DoorOpenDetectorBehaviorService.java
+++ ihmc-humanoid-behaviors/src/main/java/us/ihmc/humanoidBehaviors/behaviors/behaviorServices/DoorOpenDetectorBehaviorService.java
@@ -19,7 +19,7 @@ import us.ihmc.ros2.Ros2Node;
 public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//FiducialDetectorBehaviorService
 {
 
-   private int numberToAverage = 10;
+   private int numberToAverage = 5;
    private ArrayList<FramePose3D> originPoses;
    private ArrayList<FramePose3D> doorPoses;
 
@@ -32,6 +32,8 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
    private float openAngle = 0.15f;
    private float closeAngle = 0.07f;
    private boolean run = false;
+   
+   private long lastUpdateTime = -1;
 
    private final IHMCROS2Publisher<TextToSpeechPacket> textToSpeechPublisher;
 
@@ -51,6 +53,7 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
    public void run(boolean run)
    {
       this.run = run;
+      lastUpdateTime = -1;
       LogTools.info(1, "Start door open detector service = "+run);
 
       textToSpeechPublisher.publish(MessageTools.createTextToSpeechPacket("Start door open detector service = "+run));
@@ -84,6 +87,7 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
       originPoses.clear();
       doorOpen = false;
       averageOrigin = null;
+      lastUpdateTime = -1;
       run = false;
       doorPoses.clear();
    }
@@ -91,6 +95,16 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
    @Override
    public void doThreadAction()
    {
+	//TODO, dont know why this has to be here... but without it, the loop freezes.. looking into it next iteration. 20/07/08
+      try
+      {
+         Thread.sleep(100);
+      }
+      catch (InterruptedException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
       if (run)
       {
          latestDoorLocationPacketRecieved = doorLocationLatest.getAndSet(null);
@@ -147,7 +161,10 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
                      {
                         doorOpen = false;
                      }
+                     lastUpdateTime = System.currentTimeMillis();
+
                   }
+                  
 
                }
             }
@@ -157,6 +174,11 @@ public class DoorOpenDetectorBehaviorService extends ThreadedBehaviorService//Fi
       }
    }
 
+   public long getLastupdateTime()
+   {
+      return lastUpdateTime;
+   }
+   
    private FramePose3D averageFramePoses(ArrayList<FramePose3D> poses)
    {
       if (poses.size() > 0)
