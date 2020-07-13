3614416c48aabd55387598eee249b9f5346b3ea7
Robert <rgriffin@ihmc.us>
2020-07-13 15:55:19 -0500

    started doing custom elements to allow more adaptive customization

---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/perceptionSuite/LiveMapPerceptionSuiteElement.java
@@ -0,0 +1,40 @@
+package us.ihmc.robotEnvironmentAwareness.perceptionSuite;
+
+import javafx.stage.Stage;
+import us.ihmc.robotEnvironmentAwareness.slam.SLAMModule;
+import us.ihmc.robotEnvironmentAwareness.ui.LiveMapUI;
+import us.ihmc.robotEnvironmentAwareness.ui.SLAMBasedEnvironmentAwarenessUI;
+import us.ihmc.robotEnvironmentAwareness.updaters.LiveMapModule;
+
+public class LiveMapPerceptionSuiteElement implements PerceptionSuiteElement<LiveMapModule, LiveMapUI>
+{
+   private final Stage stage;
+   private final LiveMapModule perceptionModule;
+   private final LiveMapUI uiModule;
+
+   public LiveMapPerceptionSuiteElement(ModuleProvider<LiveMapModule> moduleProvider, UIProvider<LiveMapUI> uiProvider) throws Exception
+   {
+      stage = new Stage();
+      perceptionModule = moduleProvider.createModule();
+      uiModule = uiProvider.createUI(stage);
+
+      stage.setOnCloseRequest((event) -> hide());
+   }
+
+   public LiveMapModule getPerceptionModule()
+   {
+      return perceptionModule;
+   }
+
+   @Override
+   public LiveMapUI getPerceptionUI()
+   {
+      return uiModule;
+   }
+
+   @Override
+   public Stage getStage()
+   {
+      return stage;
+   }
+}
---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/perceptionSuite/SegmentationPerceptionSuiteElement.java
@@ -0,0 +1,40 @@
+package us.ihmc.robotEnvironmentAwareness.perceptionSuite;
+
+import javafx.stage.Stage;
+import us.ihmc.robotEnvironmentAwareness.ui.LIDARBasedEnvironmentAwarenessUI;
+import us.ihmc.robotEnvironmentAwareness.ui.PlanarSegmentationUI;
+import us.ihmc.robotEnvironmentAwareness.updaters.LIDARBasedREAModule;
+import us.ihmc.robotEnvironmentAwareness.updaters.PlanarSegmentationModule;
+
+public class SegmentationPerceptionSuiteElement implements PerceptionSuiteElement<PlanarSegmentationModule, PlanarSegmentationUI>
+{
+   private final Stage stage;
+   private final PlanarSegmentationModule perceptionModule;
+   private final PlanarSegmentationUI uiModule;
+
+   public SegmentationPerceptionSuiteElement(ModuleProvider<PlanarSegmentationModule> moduleProvider, UIProvider<PlanarSegmentationUI> uiProvider) throws Exception
+   {
+      stage = new Stage();
+      perceptionModule = moduleProvider.createModule();
+      uiModule = uiProvider.createUI(stage);
+
+      stage.setOnCloseRequest((event) -> hide());
+   }
+
+   public PlanarSegmentationModule getPerceptionModule()
+   {
+      return perceptionModule;
+   }
+
+   @Override
+   public PlanarSegmentationUI getPerceptionUI()
+   {
+      return uiModule;
+   }
+
+   @Override
+   public Stage getStage()
+   {
+      return stage;
+   }
+}
