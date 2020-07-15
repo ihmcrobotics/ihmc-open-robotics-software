b43dc297e15a9c170ea08990cd823d14c04a982b
Ben Beiter <bencb93@gmail.com>
2020-07-15 18:29:18 -0500

    Temp Fix for Bug in ContactSequenceUpdater
    last contact state for the CSP output with time set to infinity caused problems in planner

--- ihmc-simple-whole-body-walking/src/main/java/us/ihmc/simpleWholeBodyWalking/SimpleBipedContactSequenceUpdater.java
+++ ihmc-simple-whole-body-walking/src/main/java/us/ihmc/simpleWholeBodyWalking/SimpleBipedContactSequenceUpdater.java
@@ -340,7 +340,8 @@ public class SimpleBipedContactSequenceUpdater implements ContactSequenceProvide
       }
 
       SimpleBipedContactPhase contactPhase = contactSequenceInAbsoluteTime.getLast();
-      contactPhase.getTimeInterval().setEndTime(Double.POSITIVE_INFINITY);
+      //contactPhase.getTimeInterval().setEndTime(Double.POSITIVE_INFINITY);
+      contactPhase.getTimeInterval().setEndTime(contactPhase.getTimeInterval().getStartTime()+1);//temporary
       for (int i = 0; i < feetInContact.size(); i++)
          contactPhase.addEndFoot(feetInContact.get(i), solePoses.get(feetInContact.get(i)));
       contactPhase.update();
