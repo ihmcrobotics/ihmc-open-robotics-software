7cd30f36b277d0f162c6bffe2b05023fce51b1f4
edile <edile@DESKTOP-1RF6LR7.attlocal.net>
2020-07-16 17:18:08 -0500

    Added slam history.

---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/slam/SLAMHistory.java
@@ -0,0 +1,106 @@
+package us.ihmc.robotEnvironmentAwareness.slam;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import gnu.trove.list.array.TLongArrayList;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+
+public class SLAMHistory
+{
+   private static final String SLAM_HISTORY_EXTENSION = ".csv";
+   private static final String FILE_NAME_HEADER = "slam_history_";
+   /**
+    * <p>
+    * frame id: time stamp
+    * <p>
+    * original sensor pose
+    * <p>
+    * final sensor pose
+    * <p>
+    * rigid body transformer
+    * <p>
+    * computation time
+    * <p>
+    * frame waiting time: (time period between arrived and start to compute)
+    * <p>
+    * number of iterations
+    * <p>
+    * number of surfel
+    * <p>
+    * initial distance
+    * <p>
+    * final distance
+    * <p>
+    * parameters
+    */
+   private final TLongArrayList timeStamps = new TLongArrayList();
+   private final List<RigidBodyTransformReadOnly> originalSensorFrames = new ArrayList<>();
+   private final List<RigidBodyTransformReadOnly> sensorFrames = new ArrayList<>();
+
+   private final List<DriftCorrectionResult> driftCorrectionResults = new ArrayList<DriftCorrectionResult>();
+
+   public void clearHistory()
+   {
+      timeStamps.clear();
+      originalSensorFrames.clear();
+      sensorFrames.clear();
+      driftCorrectionResults.clear();
+   }
+
+   public void addLatestFrameHistory(SLAMFrame latestFrame)
+   {
+      timeStamps.add(latestFrame.getTimeStamp());
+      originalSensorFrames.add(latestFrame.getOriginalSensorPose());
+      sensorFrames.add(latestFrame.getSensorPose());
+   }
+
+   public void addDriftCorrectionHistory(DriftCorrectionResult latestResult)
+   {
+      DriftCorrectionResult result = new DriftCorrectionResult();
+      result.set(latestResult);
+      driftCorrectionResults.add(result);
+   }
+
+   //TODO : add RigidBodyTransform.
+   public void export(Path path)
+   {
+      File sensorPoseFile = new File(path.toFile(), FILE_NAME_HEADER + System.currentTimeMillis() + SLAM_HISTORY_EXTENSION);
+      FileWriter sensorPoseFileWriter;
+      try
+      {
+         sensorPoseFileWriter = new FileWriter(sensorPoseFile);
+         StringBuilder builder = new StringBuilder("");
+         builder.append("timeStamps" + ",");
+         builder.append("success" + ",");
+         builder.append("computationTime (sec)" + ",");
+         builder.append("icpIterations" + ",");
+         builder.append("numberOfSurfels" + ",");
+         builder.append("initialDistance" + ",");
+         builder.append("finalDistance" + "\n");
+         for (int i = 0; i < timeStamps.size(); i++)
+         {
+            builder.append(timeStamps.get(i) + ",");
+            DriftCorrectionResult driftCorrectionResult = driftCorrectionResults.get(i);
+            builder.append(driftCorrectionResult.isSuccess() + ",");
+            builder.append(driftCorrectionResult.getComputationTime() + ",");
+            builder.append(driftCorrectionResult.getIcpIterations() + ",");
+
+            builder.append(driftCorrectionResult.getNumberOfSurfels() + ",");
+            builder.append(driftCorrectionResult.getInitialDistance() + ",");
+            builder.append(driftCorrectionResult.getFinalDistance() + "\n");
+
+         }
+         sensorPoseFileWriter.write(builder.toString());
+         sensorPoseFileWriter.close();
+      }
+      catch (IOException e1)
+      {
+         e1.printStackTrace();
+      }
+   }
+}
--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/slam/SurfaceElementICPSLAM.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/slam/SurfaceElementICPSLAM.java
@@ -7,9 +7,11 @@ import java.util.function.UnaryOperator;
 import org.ejml.data.DMatrixRMaj;
 
 import us.ihmc.euclid.geometry.Plane3D;
+import us.ihmc.euclid.matrix.RotationMatrix;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.log.LogTools;
 import us.ihmc.robotEnvironmentAwareness.slam.tools.SLAMTools;
 import us.ihmc.robotics.optimization.LevenbergMarquardtParameterOptimizer;
@@ -37,12 +39,11 @@ public class SurfaceElementICPSLAM extends SLAMBasics
    @Override
    public RigidBodyTransformReadOnly computeFrameCorrectionTransformer(SLAMFrame frame)
    {
-      SurfaceElementICPSLAMParameters parameters = this.parameters.getAndSet(null);
-      if (parameters == null)
-         return null;
+//      SurfaceElementICPSLAMParameters parameters = this.parameters.getAndSet(null);
+//      if (parameters == null)
+//         return null;
 
       // TODO: add diagnosing manager.
-
       double surfaceElementResolution = 0.04;
       double windowMargin = 0.0;
       int minimumNumberOfHits = 1;
@@ -94,21 +95,118 @@ public class SurfaceElementICPSLAM extends SLAMBasics
       purterbationVector.set(4, 0.00001);
       purterbationVector.set(5, 0.00001);
       optimizer.setPerturbationVector(purterbationVector);
-      optimizer.initialize();
+      boolean initialCondition = optimizer.initialize();
+      LogTools.info("initialCondition " + initialCondition);
       optimizer.setCorrespondenceThreshold(octree.getResolution() * 1.5);
+      driftCorrectionResult.setInitialDistance(optimizer.getQuality());
+      LogTools.info("initial quality " + optimizer.getQuality());
 
+      double qualitySteadyThreshold = 0.001;
+      double translationalSteadyThreshold = 0.001;
+      double rotationalSteadyThreshold = 0.005;
+
+      SteadyDetector qualitySteady = new SteadyDetector(0.0, qualitySteadyThreshold);
+      SteadyDetector translationalSteady = new SteadyDetector(0.0, translationalSteadyThreshold);
+      RotationalEffortSteadyDetector rotationalSteady = new RotationalEffortSteadyDetector(rotationalSteadyThreshold);
+
+      int numberOfSteadyIterations = 0;
+      int steadyIterationsThreshold = 3;
       // do ICP.
-      for (int i = 0; i < 20; i++)
+      double quality = 0.0;
+      double translationalEffort = 0.0;
+      RotationMatrix rotationalEffort = new RotationMatrix();
+      RigidBodyTransform icpTransformer = new RigidBodyTransform();
+      int iterations = -1;
+      for (int i = 0; i < 40; i++)
       {
          optimizer.iterate();
-         // TODO: add terminal condition. 
+         optimizer.convertInputToTransform(optimizer.getOptimalParameter(), icpTransformer);
 
+         quality = optimizer.getQuality();
+         translationalEffort = icpTransformer.getTranslation().lengthSquared();
+         rotationalEffort.set(icpTransformer.getRotation());
+
+         if (qualitySteady.isSteady(quality) && translationalSteady.isSteady(translationalEffort) && rotationalSteady.isSteady(rotationalEffort))
+         {
+            numberOfSteadyIterations++;
+         }
+         else
+         {
+            numberOfSteadyIterations = 0;
+         }
+
+         if (numberOfSteadyIterations >= steadyIterationsThreshold)
+         {
+            LogTools.info("################ " + i);
+            iterations = i;
+            break;
+         }
       }
 
+      LogTools.info("final quality " + optimizer.getQuality());
       // get parameter.
-      RigidBodyTransform icpTransformer = new RigidBodyTransform();
       optimizer.convertInputToTransform(optimizer.getOptimalParameter(), icpTransformer);
 
+      driftCorrectionResult.setFinalDistance(optimizer.getQuality());
+      driftCorrectionResult.setNumberOfSurfels(numberOfSurfel);
+      driftCorrectionResult.setDriftCorrectionTransformer(icpTransformer);
+      driftCorrectionResult.setIcpIterations(iterations);
+      if (iterations < 0)
+      {
+         driftCorrectionResult.setSuccess(false);
+      }
+      else
+      {
+         driftCorrectionResult.setSuccess(true);
+      }
+
       return icpTransformer;
+   }
+
+   private class SteadyDetector
+   {
+      private double previous;
+      private final double threshold;
+
+      SteadyDetector(double current, double threshold)
+      {
+         previous = current;
+         this.threshold = threshold;
+      }
+
+      boolean isSteady(double current)
+      {
+         boolean isSteady = distance(previous, current) < threshold;
+         previous = current;
+         return isSteady;
+      }
+
+      double distance(double value, double other)
+      {
+         return Math.abs(previous - other);
+      }
+   }
+
+   private class RotationalEffortSteadyDetector
+   {
+      private final RotationMatrix previous = new RotationMatrix();
+      private final double threshold;
+
+      RotationalEffortSteadyDetector(double threshold)
+      {
+         this.threshold = threshold;
+      }
+
+      boolean isSteady(RotationMatrix current)
+      {
+         boolean isSteady = distance(previous, current) < threshold;
+         previous.set(current);
+         return isSteady;
+      }
+
+      double distance(RotationMatrix value, RotationMatrix other)
+      {
+         return new Quaternion(value).distance(new Quaternion(other));
+      }
    }
 }
