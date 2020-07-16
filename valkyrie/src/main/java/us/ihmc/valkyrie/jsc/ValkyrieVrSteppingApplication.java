2abf665cdb9879a29742a0be37f37ad856de1c07
Mark Paterson <mark.paterson@nasa.gov>
2020-07-15 22:15:19 -0500

    WIP: Compileable version of the VR stepper

---
+++ valkyrie/src/main/java/us/ihmc/valkyrie/jsc/ValkyrieVrSteppingApplication.java
@@ -0,0 +1,75 @@
+package us.ihmc.valkyrie.jsc;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.joystickBasedJavaFXController.JoystickBasedSteppingMainUI;
+import us.ihmc.avatar.joystickBasedJavaFXController.StepGeneratorJavaFXController.SecondaryControlOption;
+import us.ihmc.commonWalkingControlModules.configurations.SteppingParameters;
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.log.LogTools;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
+import us.ihmc.robotics.robotSide.SideDependentList;
+import us.ihmc.ros2.Ros2Node;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+import us.ihmc.valkyrieRosControl.ValkyrieRosControlController;
+
+public class ValkyrieVrSteppingApplication extends Application {
+	private ValkyrieVrSteppingController ui;
+	private final Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.FAST_RTPS,
+			"ihmc_valkyrie_vr_joystick_control");
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		String robotTargetString = getParameters().getNamed().getOrDefault("robotTarget", "REAL_ROBOT");
+		String workingDirectory = getParameters().getNamed().get("workingDir");
+		RobotTarget robotTarget = RobotTarget.valueOf(robotTargetString);
+		LogTools.info("-------------------------------------------------------------------");
+		LogTools.info("  -------- Loading parameters for RobotTarget: " + robotTarget + "  -------");
+		LogTools.info("-------------------------------------------------------------------");
+		ValkyrieRobotModel robotModel = new ValkyrieRobotModel(robotTarget, ValkyrieRosControlController.VERSION);
+		String robotName = robotModel.getSimpleRobotName();
+
+		WalkingControllerParameters walkingControllerParameters = robotModel.getWalkingControllerParameters();
+		SteppingParameters steppingParameters = walkingControllerParameters.getSteppingParameters();
+		double footLength = steppingParameters.getFootLength();
+		double footWidth = steppingParameters.getFootWidth();
+		ConvexPolygon2D footPolygon = new ConvexPolygon2D();
+		footPolygon.addVertex(footLength / 2.0, footWidth / 2.0);
+		footPolygon.addVertex(footLength / 2.0, -footWidth / 2.0);
+		footPolygon.addVertex(-footLength / 2.0, -footWidth / 2.0);
+		footPolygon.addVertex(-footLength / 2.0, footWidth / 2.0);
+		footPolygon.update();
+
+		SideDependentList<ConvexPolygon2D> footPolygons = new SideDependentList<>(footPolygon, footPolygon);
+
+		ui = new ValkyrieVrSteppingController(robotModel, robotName, walkingControllerParameters, ros2Node, footPolygons);
+	}
+
+	@Override
+	public void stop() throws Exception {
+		super.stop();
+		ros2Node.destroy();
+
+		Platform.exit();
+	}
+
+	/**
+	 * Argument options:
+	 * <ul>
+	 * <li>Selecting the robot target: for sim: {@code --robotTarget=SCS}, for
+	 * hardware: {@code --robotTarget=REAL_ROBOT}.
+	 * <li>Selecting the working directory (where the profiles are saved):
+	 * {@code --workingDir=~/home/myWorkingDirectory}. If none provided the default
+	 * is set to {@code "~/.ihmc/joystick_step_app/"}.
+	 * </ul>
+	 * 
+	 * @param args the array of arguments to use for this run.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
