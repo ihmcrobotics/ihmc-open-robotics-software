462a6bfa6cea69e5e8a2c16299db2c5f8b0de1be
Sylvain Bertrand <sbertrand@ihmc.us>
2020-07-17 10:53:16 -0500

    Added some documentation for the new template class.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/FeedbackControllerTemplate.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/FeedbackControllerTemplate.java
@@ -15,6 +15,20 @@ import us.ihmc.commonWalkingControlModules.controllerCore.command.feedbackContro
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 
+/**
+ * A template should be used when creating a new {@code WholeBodyControllerCore}. It is used to
+ * specify the collection of feedback controllers that will be needed at runtime.
+ * <p>
+ * The feedback controllers creates a set of {@code YoVariable}s which need to be instantiated
+ * before starting a {@code YoVariableServer} to be accessible remotely.
+ * </p>
+ * <p>
+ * By default, if a feedback controller is missing at runtime, an exception will be thrown. This
+ * behavior can be changed to dynamically create any controller that is missing at runtime, but the
+ * new controllers' {@code YoVariable}s cannot be guaranteed to be accessible from a visualizer and
+ * the instantiation of a controller is not real-time safe.
+ * </p>
+ */
 public class FeedbackControllerTemplate
 {
    private boolean allowDynamicControllerConstruction = false;
@@ -24,15 +38,58 @@ public class FeedbackControllerTemplate
    private final Set<OneDoFJointBasics> oneDoFJointFeedbackControllerTemplate = new HashSet<>();
    private boolean enableCenterOfMassFeedbackController = false;
 
+   /**
+    * Creates an empty template.
+    */
    public FeedbackControllerTemplate()
    {
    }
 
+   /**
+    * Creates and configures a template for the given list of commands.
+    * <p>
+    * The command is unpacked and interpreted as follows:
+    * <ul>
+    * <li>For every end-effector found at least once in a {@code SpatialFeedbackControlCommand}, the
+    * request for a single spatial feedback controller is added to the template.
+    * <li>For every end-effector found at least once in a {@code PointFeedbackControlCommand}, the
+    * request for a single point feedback controller is added to the template.
+    * <li>For every end-effector found at least once in a {@code OrientationFeedbackControlCommand},
+    * the request for a single orientation feedback controller is added to the template.
+    * <li>For every joint found at least once in a {@code OneDoFJointFeedbackControlCommand}, the
+    * request for a 1-DoF joint feedback controller is added to the template.
+    * <li>If the command list contains at least one {@code CenterOfMassFeedbackControlCommand}, the
+    * request for a single center of mass feedback controller is added.
+    * </ul>
+    * </p>
+    * 
+    * @param commandTemplate the list of commands to be converted into a template. Not modified.
+    */
    public FeedbackControllerTemplate(FeedbackControlCommandList commandTemplate)
    {
       convert(commandTemplate);
    }
 
+   /**
+    * Configures a template for the given list of commands.
+    * <p>
+    * The command is unpacked and interpreted as follows:
+    * <ul>
+    * <li>For every end-effector found at least once in a {@code SpatialFeedbackControlCommand}, the
+    * request for a single spatial feedback controller is added to the template.
+    * <li>For every end-effector found at least once in a {@code PointFeedbackControlCommand}, the
+    * request for a single point feedback controller is added to the template.
+    * <li>For every end-effector found at least once in a {@code OrientationFeedbackControlCommand},
+    * the request for a single orientation feedback controller is added to the template.
+    * <li>For every joint found at least once in a {@code OneDoFJointFeedbackControlCommand}, the
+    * request for a 1-DoF joint feedback controller is added to the template.
+    * <li>If the command list contains at least one {@code CenterOfMassFeedbackControlCommand}, the
+    * request for a single center of mass feedback controller is added.
+    * </ul>
+    * </p>
+    * 
+    * @param commandTemplate the list of commands to be converted into a template. Not modified.
+    */
    public void convert(FeedbackControlCommandList commandTemplate)
    {
       for (int i = 0; i < commandTemplate.getNumberOfCommands(); i++)
@@ -65,46 +122,107 @@ public class FeedbackControllerTemplate
       }
    }
 
+   /**
+    * Sets the restriction for dynamic allocation of feedback controllers.
+    * <p>
+    * <strong>WARNING</strong>: This feature must not be enabled when the controller core is used in a
+    * real-time environment!
+    * </p>
+    * <p>
+    * (Default) When disabled: at runtime if a feedback controller is missing, the
+    * {@link WholeBodyFeedbackController} will throw a detailed exception which should result in the
+    * end of a run.
+    * </p>
+    * <p>
+    * When enabled: at runtime if a feedback controller is missing, the
+    * {@link WholeBodyFeedbackController} will instantiate a new one and use it.
+    * </p>
+    * 
+    * @param allowDynamicControllerConstruction {@code false} to prevent dynamic allocation (default,
+    *                                           real-time safe), {@code true} to allow dynamic
+    *                                           allocation (not real-time safe).
+    */
    public void setAllowDynamicControllerConstruction(boolean allowDynamicControllerConstruction)
    {
       this.allowDynamicControllerConstruction = allowDynamicControllerConstruction;
    }
 
+   /**
+    * Requests a single spatial feedback controller for the given end-effector.
+    * 
+    * @param endEffector the rigid-body to be controlled by the feedback controller.
+    */
    public void enableSpatialFeedbackController(RigidBodyBasics endEffector)
    {
       enableSpatialFeedbackController(endEffector, 1);
    }
 
+   /**
+    * Requests a number of spatial feedback controller(s) for the given end-effector.
+    * 
+    * @param endEffector         the rigid-body to be controlled by the feedback controller(s).
+    * @param numberOfControllers the number of controllers to be created for the given end-effector.
+    */
    public void enableSpatialFeedbackController(RigidBodyBasics endEffector, int numberOfControllers)
    {
       spatialFeedbackControllerTemplate.put(endEffector, numberOfControllers);
    }
 
+   /**
+    * Requests a single orientation feedback controller for the given end-effector.
+    * 
+    * @param endEffector the rigid-body to be controlled by the feedback controller.
+    */
    public void enableOrientationFeedbackController(RigidBodyBasics endEffector)
    {
       enableOrientationFeedbackController(endEffector, 1);
    }
 
+   /**
+    * Requests a number of orientation feedback controller(s) for the given end-effector.
+    * 
+    * @param endEffector         the rigid-body to be controlled by the feedback controller(s).
+    * @param numberOfControllers the number of controllers to be created for the given end-effector.
+    */
    public void enableOrientationFeedbackController(RigidBodyBasics endEffector, int numberOfControllers)
    {
       orientationFeedbackControllerTemplate.put(endEffector, numberOfControllers);
    }
 
+   /**
+    * Requests a single point feedback controller for the given end-effector.
+    * 
+    * @param endEffector the rigid-body to be controlled by the feedback controller.
+    */
    public void enablePointFeedbackController(RigidBodyBasics endEffector)
    {
       enablePointFeedbackController(endEffector, 1);
    }
 
+   /**
+    * Requests a number of point feedback controller(s) for the given end-effector.
+    * 
+    * @param endEffector         the rigid-body to be controlled by the feedback controller(s).
+    * @param numberOfControllers the number of controllers to be created for the given end-effector.
+    */
    public void enablePointFeedbackController(RigidBodyBasics endEffector, int numberOfControllers)
    {
       pointFeedbackControllerTemplate.put(endEffector, 1);
    }
 
+   /**
+    * Requests a single 1-DoF joint feedback controller for the given joint.
+    * 
+    * @param joint the joint to be controlled by the feedback controller.
+    */
    public void enableOneDoFJointFeedbackController(OneDoFJointBasics joint)
    {
       oneDoFJointFeedbackControllerTemplate.add(joint);
    }
 
+   /**
+    * Requests a single feedback controller to control the center of mass.
+    */
    public void enableCenterOfMassFeedbackController()
    {
       enableCenterOfMassFeedbackController = true;
