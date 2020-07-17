672a2a79cf4937db5b08e3ff67d59fb3b0116602
Robert <rgriffin@ihmc.us>
2020-07-16 21:28:48 -0500

    added segmentation to atlas slam

--- atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSLAMBasedREAStandaloneLauncher.java
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSLAMBasedREAStandaloneLauncher.java
@@ -9,16 +9,25 @@ import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.communication.ROS2Tools;
 import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.robotEnvironmentAwareness.communication.REACommunicationProperties;
+import us.ihmc.robotEnvironmentAwareness.ui.PlanarSegmentationUI;
 import us.ihmc.robotEnvironmentAwareness.ui.SLAMBasedEnvironmentAwarenessUI;
+import us.ihmc.robotEnvironmentAwareness.updaters.PlanarSegmentationModule;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.wholeBodyController.RobotContactPointParameters;
 
 public class AtlasSLAMBasedREAStandaloneLauncher extends Application
 {
+   private static final String MODULE_CONFIGURATION_FILE_NAME = "./Configurations/defaultSegmentationModuleConfiguration.txt";
+
    private SLAMBasedEnvironmentAwarenessUI ui;
    private AtlasSLAMModule module;
+
+   private PlanarSegmentationUI planarSegmentationUI;
+   private PlanarSegmentationModule segmentationModule;
 
    @Override
    public void start(Stage primaryStage) throws Exception
@@ -34,8 +43,18 @@ public class AtlasSLAMBasedREAStandaloneLauncher extends Application
       ui = SLAMBasedEnvironmentAwarenessUI.creatIntraprocessUI(primaryStage, defaultContactPoints);
       module = AtlasSLAMModule.createIntraprocessModule(drcRobotModel);
 
+      Stage secondStage = new Stage();
+      planarSegmentationUI = PlanarSegmentationUI.createIntraprocessUI(secondStage);
+      segmentationModule = PlanarSegmentationModule.createIntraprocessModule(REACommunicationProperties.inputTopic,
+                                                                             REACommunicationProperties.subscriberCustomRegionsTopicName,
+                                                                             ROS2Tools.REALSENSE_SLAM_MAP,
+                                                                             MODULE_CONFIGURATION_FILE_NAME);
+      module.attachOcTreeConsumer(segmentationModule);
+
       ui.show();
+      planarSegmentationUI.show();
       module.start();
+      segmentationModule.start();
    }
 
    @Override
@@ -43,6 +62,9 @@ public class AtlasSLAMBasedREAStandaloneLauncher extends Application
    {
       ui.stop();
       module.stop();
+
+      planarSegmentationUI.stop();
+      segmentationModule.stop();
 
       Platform.exit();
    }
