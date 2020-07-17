22565e056ba079d8f11318e880b21f0bc40e5910
Duncan Calvert <dcalvert@ihmc.us>
2020-07-17 14:33:39 -0500

    Fix up behavior launcher classes.

--- atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasBehaviorUIAndModule.java
+++ atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasBehaviorUIAndModule.java
@@ -4,19 +4,19 @@ import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.commons.thread.ThreadTools;
+import us.ihmc.humanoidBehaviors.BehaviorModule;
 import us.ihmc.humanoidBehaviors.ui.BehaviorUI;
 import us.ihmc.humanoidBehaviors.ui.BehaviorUIRegistry;
+import us.ihmc.humanoidBehaviors.ui.behaviors.LookAndStepBehaviorUI;
 
 public class AtlasBehaviorUIAndModule
 {
    public AtlasBehaviorUIAndModule()
    {
-      ThreadTools.startAThread(() -> new AtlasBehaviorModule(), AtlasBehaviorUIAndModule.class.getSimpleName());
-
       DRCRobotModel drcRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.REAL_ROBOT, false);
-
-      BehaviorUI.createInterprocess(BehaviorUIRegistry.DEFAULT_BEHAVIORS, drcRobotModel, "localhost");
+      BehaviorUIRegistry behaviorRegistry = BehaviorUIRegistry.of(LookAndStepBehaviorUI.DEFINITION);
+      BehaviorModule.createInterprocess(behaviorRegistry, drcRobotModel);
+      BehaviorUI.createInterprocess(behaviorRegistry, drcRobotModel, "localhost");
    }
 
    public static void main(String[] args)
