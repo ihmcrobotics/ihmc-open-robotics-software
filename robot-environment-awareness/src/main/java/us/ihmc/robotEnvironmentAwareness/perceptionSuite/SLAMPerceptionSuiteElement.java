ae09cace6597b15e5520fa067668c58bb59fdafa
Robert <rgriffin@ihmc.us>
2020-07-21 09:16:54 -0500

    did some fixes on the slam module to get it runnable

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/perceptionSuite/SLAMPerceptionSuiteElement.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/perceptionSuite/SLAMPerceptionSuiteElement.java
@@ -1,26 +1,47 @@
 package us.ihmc.robotEnvironmentAwareness.perceptionSuite;
 
 import javafx.stage.Stage;
+import us.ihmc.javaFXToolkit.messager.SharedMemoryJavaFXMessager;
+import us.ihmc.messager.Messager;
+import us.ihmc.robotEnvironmentAwareness.communication.SLAMModuleAPI;
 import us.ihmc.robotEnvironmentAwareness.slam.SLAMModule;
 import us.ihmc.robotEnvironmentAwareness.ui.SLAMBasedEnvironmentAwarenessUI;
 
 public class SLAMPerceptionSuiteElement implements PerceptionSuiteElement<SLAMModule, SLAMBasedEnvironmentAwarenessUI>
 {
+   private final Messager messager;
    private final Stage stage;
    private final SLAMModule perceptionModule;
    private final SLAMBasedEnvironmentAwarenessUI uiModule;
 
    public SLAMPerceptionSuiteElement(ModuleProvider<SLAMModule> moduleProvider, UIProvider<SLAMBasedEnvironmentAwarenessUI> uiProvider) throws Exception
    {
+      messager = new SharedMemoryJavaFXMessager(SLAMModuleAPI.API);
+      messager.startMessager();
+
       stage = new Stage();
-      perceptionModule = moduleProvider.createModule(null);
-      uiModule = uiProvider.createUI(null, stage);
+      perceptionModule = moduleProvider.createModule(messager);
+      uiModule = uiProvider.createUI(messager, stage);
 
       perceptionModule.start();
 
       stage.setOnCloseRequest((event) -> hide());
    }
+   
+   @Override
+   public void stopInternal()
+   {
+      try
+      {
+         messager.closeMessager();
+      }
+      catch (Exception e)
+      {
 
+      }
+   }
+
+   @Override
    public SLAMModule getPerceptionModule()
    {
       return perceptionModule;
