69dd9d190ea29a834fef4d58c04ff97e54259f7e
Duncan Calvert <dcalvert@ihmc.us>
2020-07-22 12:05:00 -0500

    Fix dengerous data oversharing issue.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PointCloudPolygonizer.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/perception/PointCloudPolygonizer.java
@@ -1,43 +1,44 @@
 package us.ihmc.humanoidBehaviors.tools.perception;
 
+import org.apache.commons.lang3.tuple.Pair;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.robotEnvironmentAwareness.geometry.ConcaveHullFactoryParameters;
 import us.ihmc.robotEnvironmentAwareness.planarRegion.PlanarRegionPolygonizer;
 import us.ihmc.robotEnvironmentAwareness.planarRegion.PlanarRegionSegmentationRawData;
 import us.ihmc.robotEnvironmentAwareness.planarRegion.PolygonizerParameters;
-import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PointCloudPolygonizer
 {
    private final ConcaveHullFactoryParameters concaveHullFactoryParameters = new ConcaveHullFactoryParameters();
    private final PolygonizerParameters polygonizerParameters = new PolygonizerParameters();
 
-   private volatile int id = 0;
+   private AtomicInteger id = new AtomicInteger(0);
 
    public PointCloudPolygonizer()
    {
    }
 
-   public PlanarRegionsList polygonize(HashMap<PlanarRegion, List<Point3D>> pointsInRegions)
+   public PlanarRegionsList polygonize(Map<Pair<Point3DReadOnly, Vector3DReadOnly>, List<Point3D>> pointsInRegions)
    {
       List<PlanarRegionSegmentationRawData> segmentationRawData = new ArrayList<>();
-      for (PlanarRegion originalRegion : pointsInRegions.keySet())
+      for (Pair<Point3DReadOnly, Vector3DReadOnly> centerNormal : pointsInRegions.keySet())
       {
-         List<Point3D> points = pointsInRegions.get(originalRegion);
+         List<Point3D> points = pointsInRegions.get(centerNormal);
 
          if (points.isEmpty())
             continue;
 
-         Point3D center = new Point3D();
-         originalRegion.getBoundingBox3dInWorld().getCenterPoint(center);
-         PlanarRegionSegmentationRawData rawData = new PlanarRegionSegmentationRawData(id++,
-                                                                                       originalRegion.getNormal(),
-                                                                                       center,
+         PlanarRegionSegmentationRawData rawData = new PlanarRegionSegmentationRawData(id.getAndIncrement(),
+                                                                                       centerNormal.getRight(),
+                                                                                       centerNormal.getLeft(),
                                                                                        points);
          segmentationRawData.add(rawData);
       }
