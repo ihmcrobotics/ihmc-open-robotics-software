9c329cfb4f661c08186c8e8e317d73b9fd11762c
Robert <rgriffin@ihmc.us>
2020-07-22 09:25:08 -0500

    set up the network providers for the realsense and lidar rea modules in the perception suite

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/updaters/REANetworkProvider.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/updaters/REANetworkProvider.java
@@ -9,25 +9,45 @@ public interface REANetworkProvider
 {
    void registerMessager(Messager messager);
 
-   void registerLidarScanHandler(NewMessageListener<LidarScanMessage> lidarScanHandler);
+   default void registerLidarScanHandler(NewMessageListener<LidarScanMessage> lidarScanHandler)
+   {
+   }
 
-   void registerStereoVisionPointCloudHandler(NewMessageListener<StereoVisionPointCloudMessage> stereoVisionPointCloudHandler);
+   default void registerStereoVisionPointCloudHandler(NewMessageListener<StereoVisionPointCloudMessage> stereoVisionPointCloudHandler)
+   {
+   }
 
-   void registerLidarScanHandler(Messager messager, NewMessageListener<LidarScanMessage> lidarScanHandler);
+   default void registerLidarScanHandler(Messager messager, NewMessageListener<LidarScanMessage> lidarScanHandler)
+   {
+   }
 
-   void registerDepthPointCloudHandler(Messager messager, NewMessageListener<StereoVisionPointCloudMessage> depthPointCloudHandler);
+   default void registerDepthPointCloudHandler(Messager messager, NewMessageListener<StereoVisionPointCloudMessage> depthPointCloudHandler)
+   {
+   }
 
-   void registerStereoVisionPointCloudHandler(Messager messager, NewMessageListener<StereoVisionPointCloudMessage> stereoVisionPointCloudHandler);
+   default void registerStereoVisionPointCloudHandler(Messager messager, NewMessageListener<StereoVisionPointCloudMessage> stereoVisionPointCloudHandler)
+   {
+   }
 
-   void registerStampedPosePacketHandler(Messager messager, NewMessageListener<StampedPosePacket> stampedPosePacketHandler);
+   default void registerStampedPosePacketHandler(Messager messager, NewMessageListener<StampedPosePacket> stampedPosePacketHandler)
+   {
+   }
 
-   void registerCustomRegionsHandler(NewMessageListener<PlanarRegionsListMessage> customRegionsHandler);
+   default void registerCustomRegionsHandler(NewMessageListener<PlanarRegionsListMessage> customRegionsHandler)
+   {
+   }
 
-   void registerPlanarRegionsListRequestHandler(NewMessageListener<RequestPlanarRegionsListMessage> requestHandler);
+   default void registerPlanarRegionsListRequestHandler(NewMessageListener<RequestPlanarRegionsListMessage> requestHandler)
+   {
+   }
 
-   void registerREAStateRequestHandler(NewMessageListener<REAStateRequestMessage> requestHandler);
+   default void registerREAStateRequestHandler(NewMessageListener<REAStateRequestMessage> requestHandler)
+   {
+   }
 
-   void registerREASensorDataFilterParametersHandler(NewMessageListener<REASensorDataFilterParametersMessage> parametersHandler);
+   default void registerREASensorDataFilterParametersHandler(NewMessageListener<REASensorDataFilterParametersMessage> parametersHandler)
+   {
+   }
 
    void update(RegionFeaturesProvider regionFeaturesProvider, boolean planarRegionsHaveBeenUpdated);
 
