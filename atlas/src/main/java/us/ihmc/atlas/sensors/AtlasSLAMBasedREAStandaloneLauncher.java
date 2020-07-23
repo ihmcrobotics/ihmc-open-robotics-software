6bc54ed493a81ecd8524636da32491208380e03d
Duncan Calvert <dcalvert@ihmc.us>
2020-07-22 21:53:20 -0500

    Allow to launch Realsense SLAM with or without UIs.

--- atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSLAMBasedREAStandaloneLauncher.java
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasSLAMBasedREAStandaloneLauncher.java
@@ -2,16 +2,19 @@ package us.ihmc.atlas.sensors;
 
 import java.util.List;
 
-import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.commons.exception.DefaultExceptionHandler;
+import us.ihmc.commons.exception.ExceptionTools;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.javaFXToolkit.messager.SharedMemoryJavaFXMessager;
+import us.ihmc.javafx.applicationCreator.JavaFXApplicationCreator;
+import us.ihmc.log.LogTools;
 import us.ihmc.messager.Messager;
 import us.ihmc.robotEnvironmentAwareness.communication.REACommunicationProperties;
 import us.ihmc.robotEnvironmentAwareness.communication.SLAMModuleAPI;
@@ -23,9 +26,10 @@ import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.wholeBodyController.RobotContactPointParameters;
 
-public class AtlasSLAMBasedREAStandaloneLauncher extends Application
+public class AtlasSLAMBasedREAStandaloneLauncher
 {
    private static final String MODULE_CONFIGURATION_FILE_NAME = "./Configurations/defaultSegmentationModuleConfiguration.txt";
+   private final boolean spawnUIs;
 
    private Messager slamMessager;
    private Messager segmentationMessager;
@@ -35,9 +39,19 @@ public class AtlasSLAMBasedREAStandaloneLauncher extends Application
    private PlanarSegmentationUI planarSegmentationUI;
    private PlanarSegmentationModule segmentationModule;
 
-   @Override
-   public void start(Stage primaryStage) throws Exception
+   public AtlasSLAMBasedREAStandaloneLauncher(boolean spawnUIs)
    {
+      this.spawnUIs = spawnUIs;
+
+      JavaFXApplicationCreator.createAJavaFXApplication();
+
+      Platform.runLater(() -> ExceptionTools.handle(this::setup, DefaultExceptionHandler.PRINT_STACKTRACE));
+   }
+
+   public void setup() throws Exception
+   {
+      Stage primaryStage = new Stage();
+
       DRCRobotModel drcRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, RobotTarget.REAL_ROBOT, false);
 
       RobotContactPointParameters<RobotSide> contactPointParameters = drcRobotModel.getContactPointParameters();
@@ -53,11 +67,18 @@ public class AtlasSLAMBasedREAStandaloneLauncher extends Application
       segmentationMessager = new SharedMemoryJavaFXMessager(SegmentationModuleAPI.API);
       segmentationMessager.startMessager();
 
-      ui = SLAMBasedEnvironmentAwarenessUI.creatIntraprocessUI(slamMessager, primaryStage, defaultContactPoints);
+      if (spawnUIs)
+      {
+         ui = SLAMBasedEnvironmentAwarenessUI.creatIntraprocessUI(slamMessager, primaryStage, defaultContactPoints);
+      }
       module = AtlasSLAMModule.createIntraprocessModule(drcRobotModel, slamMessager);
 
-      Stage secondStage = new Stage();
-      planarSegmentationUI = PlanarSegmentationUI.createIntraprocessUI(segmentationMessager, secondStage);
+      Stage secondStage = null;
+      if (spawnUIs)
+      {
+         secondStage = new Stage();
+         planarSegmentationUI = PlanarSegmentationUI.createIntraprocessUI(segmentationMessager, secondStage);
+      }
       segmentationModule = PlanarSegmentationModule.createIntraprocessModule(REACommunicationProperties.inputTopic,
                                                                              REACommunicationProperties.subscriberCustomRegionsTopicName,
                                                                              ROS2Tools.REALSENSE_SLAM_REGIONS,
@@ -65,13 +86,19 @@ public class AtlasSLAMBasedREAStandaloneLauncher extends Application
                                                                              segmentationMessager);
       module.attachOcTreeConsumer(segmentationModule);
 
-      ui.show();
-      planarSegmentationUI.show();
+      if (spawnUIs)
+      {
+         primaryStage.setOnCloseRequest(event -> ExceptionTools.handle(() -> stop(), DefaultExceptionHandler.PRINT_STACKTRACE));
+         secondStage.setOnCloseRequest(event -> ExceptionTools.handle(() -> stop(), DefaultExceptionHandler.PRINT_STACKTRACE));
+
+         ui.show();
+         planarSegmentationUI.show();
+      }
+
       module.start();
       segmentationModule.start();
    }
 
-   @Override
    public void stop() throws Exception
    {
       ui.stop();
@@ -88,6 +115,6 @@ public class AtlasSLAMBasedREAStandaloneLauncher extends Application
 
    public static void main(String[] args)
    {
-      launch(args);
+      new AtlasSLAMBasedREAStandaloneLauncher(true);
    }
 }
