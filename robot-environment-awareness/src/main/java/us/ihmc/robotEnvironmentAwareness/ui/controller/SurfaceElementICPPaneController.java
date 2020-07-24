dcd5e0aa0d0cfd90cc51fa34aa3ebfc99e382cbc
edile <edile@DESKTOP-1RF6LR7.attlocal.net>
2020-07-24 15:48:03 -0500

    Added new tab for surfel icp.

---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/SurfaceElementICPPaneController.java
@@ -0,0 +1,12 @@
+package us.ihmc.robotEnvironmentAwareness.ui.controller;
+
+public class SurfaceElementICPPaneController extends REABasicUIController
+{
+
+   @Override
+   public void bindControls()
+   {
+
+   }
+
+}
