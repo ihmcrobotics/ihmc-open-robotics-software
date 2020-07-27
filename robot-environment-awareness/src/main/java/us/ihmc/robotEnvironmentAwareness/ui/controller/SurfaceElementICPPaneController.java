2820603a741fafa91c230ab91c104d98771856a8
Robert <rgriffin@ihmc.us>
2020-07-27 15:14:38 -0500

    added the max queue size to the UI

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/SurfaceElementICPPaneController.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/SurfaceElementICPPaneController.java
@@ -40,6 +40,8 @@ public class SurfaceElementICPPaneController extends REABasicUIController
    
    @FXML
    private Spinner<Double> initialQualityThreshold;
+   @FXML
+   private Spinner<Integer> maxQueueSize;
 
    @FXML
    private Spinner<Integer> maxOptimizationIterations;
@@ -72,9 +74,10 @@ public class SurfaceElementICPPaneController extends REABasicUIController
       initialQualityThreshold.setValueFactory(createDoubleValueFactory(0.05, 0.3, 0.1, 0.05));
 
       maxOptimizationIterations.setValueFactory(createIntegerValueFactory(1, 100, 40, 1));
-
       translationPerturbation.setValueFactory(createDoubleValueFactory(0.0001, 0.01, 0.002, 0.0005));
       rotationPerturbation.setValueFactory(createDoubleValueFactory(0.00001, 0.0001, 0.00001, 0.00001));
+
+      maxQueueSize.setValueFactory(createIntegerValueFactory(1, Integer.MAX_VALUE, 100, 5));
    }
    
    @Override
@@ -93,6 +96,7 @@ public class SurfaceElementICPPaneController extends REABasicUIController
       
       surfaceElementICPSLAMParametersProperty.bindBidirectionalEnableInitialQualityFilter(enableInitialQualityFilter.selectedProperty());
       surfaceElementICPSLAMParametersProperty.bindBidirectionalInitialQualityThreshold(initialQualityThreshold.getValueFactory().valueProperty());
+      surfaceElementICPSLAMParametersProperty.bindBidirectionalMaxQueueSize(maxQueueSize.getValueFactory().valueProperty());
 
       surfaceElementICPSLAMParametersProperty.bindBidirectionalMaxOptimizationIterations(maxOptimizationIterations.getValueFactory().valueProperty());
       surfaceElementICPSLAMParametersProperty.bindBidirectionalComputeSurfaceNormalsInFrame(computeSurfaceNormalsInFrame.selectedProperty());
