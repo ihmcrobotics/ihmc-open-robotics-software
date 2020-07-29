3c00ed83d2f4c55eb177384f7c15dbf2758392e3
Mark Paterson <mark.paterson@nasa.gov>
2020-07-29 09:47:48 -0500

    Interim commit for Sylvain's inspection

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/directionalControlToolboxModule/DirectionalControlModule.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/directionalControlToolboxModule/DirectionalControlModule.java
@@ -3,7 +3,6 @@ package us.ihmc.avatar.networkProcessor.directionalControlToolboxModule;
 import java.util.ArrayList;
 import java.util.List;
 
-import controller_msgs.msg.dds.KinematicsToolboxOutputStatus;
 import controller_msgs.msg.dds.RobotConfigurationData;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.networkProcessor.modules.ToolboxController;
@@ -12,20 +11,50 @@ import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.Co
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.ROS2Tools.MessageTopicNameGenerator;
 import us.ihmc.communication.ROS2Tools.ROS2TopicQualifier;
+import us.ihmc.communication.controllerAPI.CommandInputManager.HasReceivedInputListener;
 import us.ihmc.communication.controllerAPI.command.Command;
 import us.ihmc.euclid.interfaces.Settable;
+import us.ihmc.humanoidRobotics.communication.directionalControlToolboxAPI.DirectionalControlConfigurationCommand;
+import us.ihmc.humanoidRobotics.communication.directionalControlToolboxAPI.DirectionalControlInputCommand;
+import us.ihmc.humanoidRobotics.communication.kinematicsToolboxAPI.KinematicsToolboxCenterOfMassCommand;
+import us.ihmc.log.LogTools;
 import us.ihmc.ros2.RealtimeRos2Node;
 
 public class DirectionalControlModule extends ToolboxModule {
 
 	private final DirectionalControlController steppingController;
+	private final static int update_period_in_ms = 1000;
 
 	public DirectionalControlModule(DRCRobotModel robotModel, boolean startYoVariableServer) {
 		super(robotModel.getSimpleRobotName(), robotModel.createFullRobotModel(), robotModel.getLogModelProvider(),
-				startYoVariableServer);
+				startYoVariableServer, update_period_in_ms);
 
-		steppingController = new DirectionalControlController(fullRobotModel, robotModel, statusOutputManager,
-				registry);
+		steppingController = new DirectionalControlController(fullRobotModel, robotModel, statusOutputManager, registry,
+				getSubscriberTopicNameGenerator(), getPublisherTopicNameGenerator());
+
+		commandInputManager.registerHasReceivedInputListener(new HasReceivedInputListener() {
+
+			@Override
+			public void hasReceivedInput(Class<? extends Command<?, ?>> commandClass) {
+				DirectionalControlConfigurationCommand configCommand = commandInputManager
+						.pollNewestCommand(DirectionalControlConfigurationCommand.class);
+				if (configCommand != null) {
+					LogTools.info("Got new config command");
+					steppingController.updateConfiguration(configCommand.getMessage());
+				}
+
+				DirectionalControlInputCommand inputCommand = commandInputManager
+						.pollNewestCommand(DirectionalControlInputCommand.class);
+				if (inputCommand != null) {
+					LogTools.info("Got new input command");
+					steppingController.updateInputs(inputCommand.getMessage());
+				}
+			}
+
+		});
+
+		// Must start the Yo Variable Server since it is used to track message timeouts
+		startYoVariableServer();
 	}
 
 	@Override
@@ -52,6 +81,8 @@ public class DirectionalControlModule extends ToolboxModule {
 
 	public static List<Class<? extends Command<?, ?>>> supportedCommands() {
 		List<Class<? extends Command<?, ?>>> commands = new ArrayList<>();
+		commands.add(DirectionalControlConfigurationCommand.class);
+		commands.add(DirectionalControlInputCommand.class);
 		return commands;
 	}
 
@@ -73,6 +104,18 @@ public class DirectionalControlModule extends ToolboxModule {
 	@Override
 	public MessageTopicNameGenerator getSubscriberTopicNameGenerator() {
 		return getSubscriberTopicNameGenerator(robotName);
+	}
+
+	@Override
+	public void sleep() {
+		LogTools.info("Directional toolbox told to sleep");
+		super.sleep();
+	}
+
+	@Override
+	public void wakeUp() {
+		LogTools.info("Directional toolbox told to wake up");
+		super.wakeUp();
 	}
 
 	public static MessageTopicNameGenerator getSubscriberTopicNameGenerator(String robotName) {
