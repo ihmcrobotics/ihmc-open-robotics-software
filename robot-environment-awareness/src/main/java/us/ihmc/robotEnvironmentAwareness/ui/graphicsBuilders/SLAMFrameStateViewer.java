471f853ddfc01bf1e9d17aafad8d0777d1a30a10
Robert <rgriffin@ihmc.us>
2020-07-30 22:08:52 -0500

    fixed the frame state viewer so that it doesnt throw exceptions

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/graphicsBuilders/SLAMFrameStateViewer.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/graphicsBuilders/SLAMFrameStateViewer.java
@@ -1,25 +1,17 @@
 package us.ihmc.robotEnvironmentAwareness.ui.graphicsBuilders;
 
 import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
-import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.MeshView;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.euclid.tuple3D.Point3D32;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
-import us.ihmc.graphicsDescription.MeshDataGenerator;
-import us.ihmc.javaFXToolkit.shapes.JavaFXMultiColorMeshBuilder;
-import us.ihmc.javaFXToolkit.shapes.TextureColorAdaptivePalette;
 import us.ihmc.messager.MessagerAPIFactory.Topic;
 import us.ihmc.robotEnvironmentAwareness.communication.REAUIMessager;
 import us.ihmc.robotEnvironmentAwareness.communication.SLAMModuleAPI;
 import us.ihmc.robotEnvironmentAwareness.communication.converters.PointCloudCompression;
 import us.ihmc.robotEnvironmentAwareness.slam.SLAMFrameState;
-import us.ihmc.robotEnvironmentAwareness.ui.controller.PointCloudAnchorPaneController;
 
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class SLAMFrameStateViewer implements Runnable
@@ -35,8 +27,6 @@ public class SLAMFrameStateViewer implements Runnable
    private final AtomicReference<Boolean> enable;
 
    private final StereoVisionPointCloudViewer uncorrectedPointCloudViewer;
-   private final StereoVisionPointCloudViewer correctedPointCloudViewer;
-   private final StereoVisionPointCloudViewer correspondingPointCloudViewer;
 
    private final REAUIMessager uiMessager;
 
@@ -48,15 +38,9 @@ public class SLAMFrameStateViewer implements Runnable
 
       enable = uiMessager.createInput(enableTopic, false);
 
-      uncorrectedPointCloudViewer = new StereoVisionPointCloudViewer(SLAMModuleAPI.UIUncorrectedPointCloud, uiMessager, enableTopic, clearTopic, sizeTopic);
-      correctedPointCloudViewer = new StereoVisionPointCloudViewer(SLAMModuleAPI.UICorrectedPointCloud, uiMessager, enableTopic, clearTopic, sizeTopic);
-      correspondingPointCloudViewer = new StereoVisionPointCloudViewer(SLAMModuleAPI.UICorrespondingPointsInWorld,
-                                                                       uiMessager,
-                                                                       enableTopic,
-                                                                       clearTopic,
-                                                                       sizeTopic);
+      uncorrectedPointCloudViewer = new StereoVisionPointCloudViewer(SLAMModuleAPI.UIStereoSLAMPointCloud, uiMessager, enableTopic, clearTopic, sizeTopic);
 
-      root.getChildren().addAll(uncorrectedPointCloudViewer.getRoot(), correctedPointCloudViewer.getRoot(), correspondingPointCloudViewer.getRoot());
+      root.getChildren().addAll(uncorrectedPointCloudViewer.getRoot());
    }
 
    public void run()
@@ -69,27 +53,47 @@ public class SLAMFrameStateViewer implements Runnable
 
       unpackPointCloud(newMessageToRender.getAndSet(null));
       uncorrectedPointCloudViewer.run();
-      correctedPointCloudViewer.run();
-      correspondingPointCloudViewer.run();
    }
 
    public void render()
    {
       uncorrectedPointCloudViewer.render();
-      correctedPointCloudViewer.render();
-      correspondingPointCloudViewer.render();
    }
 
    public void clear()
    {
       uncorrectedPointCloudViewer.clear();
-      correctedPointCloudViewer.clear();
-      correspondingPointCloudViewer.clear();
    }
 
    public Node getRoot()
    {
       return root;
+   }
+
+   private StereoVisionPointCloudMessage createLatestFrameStereoVisionPointCloudMessage(Point3DReadOnly[] uncorrectedPointCloudBuffer,
+                                                                                        Point3DReadOnly[] correspondingPointCloudBuffer,
+                                                                                        Point3DReadOnly[] correctedPointCloudBuffer)
+   {
+      int numberOfPointsToPack = uncorrectedPointCloudBuffer.length + correspondingPointCloudBuffer.length + correctedPointCloudBuffer.length;
+
+      Point3D[] pointCloudBuffer = new Point3D[numberOfPointsToPack];
+      int[] colorBuffer = new int[numberOfPointsToPack];
+      for (int i = 0; i < uncorrectedPointCloudBuffer.length; i++)
+      {
+         pointCloudBuffer[i] = new Point3D(uncorrectedPointCloudBuffer[i]);
+         colorBuffer[i] = StereoVisionPointCloudViewer.colorToInt(LATEST_ORIGINAL_POINT_CLOUD_COLOR);
+      }
+      for (int i = uncorrectedPointCloudBuffer.length; i < uncorrectedPointCloudBuffer.length + correspondingPointCloudBuffer.length; i++)
+      {
+         pointCloudBuffer[i] = new Point3D(correspondingPointCloudBuffer[i - uncorrectedPointCloudBuffer.length]);
+         colorBuffer[i] = StereoVisionPointCloudViewer.colorToInt(SOURCE_POINT_CLOUD_COLOR);
+      }
+      for (int i = uncorrectedPointCloudBuffer.length + correspondingPointCloudBuffer.length; i < numberOfPointsToPack; i++)
+      {
+         pointCloudBuffer[i] = new Point3D(correctedPointCloudBuffer[i - uncorrectedPointCloudBuffer.length - correspondingPointCloudBuffer.length]);
+         colorBuffer[i] = StereoVisionPointCloudViewer.colorToInt(LATEST_POINT_CLOUD_COLOR);
+      }
+      return PointCloudCompression.compressPointCloud(19870612L, pointCloudBuffer, colorBuffer, numberOfPointsToPack, 0.001, null);
    }
 
    private void unpackPointCloud(SLAMFrameState message)
@@ -98,42 +102,8 @@ public class SLAMFrameStateViewer implements Runnable
       Point3DReadOnly[] correctedPointCloudBuffer = message.getCorrectedPointCloudInWorld();
       Point3DReadOnly[] correspondingPointCloudBuffer = message.setCorrespondingPointsInWorld();
 
-      int uncorrectedSize = uncorrectedPointCloudBuffer.length;
-      int correctedSize = correctedPointCloudBuffer.length;
-      int correspondingSize = correspondingPointCloudBuffer.length;
+      StereoVisionPointCloudMessage convertedMessage = createLatestFrameStereoVisionPointCloudMessage(uncorrectedPointCloudBuffer, correspondingPointCloudBuffer, correctedPointCloudBuffer);
 
-      int[] uncorrectedColorBuffer = new int[uncorrectedSize];
-      int[] correctedColorBuffer = new int[correctedSize];
-      int[] correspondingColorBuffer = new int[correspondingSize];
-
-      for (int i = 0; i < uncorrectedSize; i++)
-         uncorrectedColorBuffer[i] = StereoVisionPointCloudViewer.colorToInt(LATEST_ORIGINAL_POINT_CLOUD_COLOR);
-      for (int i = 0; i < correctedSize; i++)
-         correctedColorBuffer[i] = StereoVisionPointCloudViewer.colorToInt(LATEST_POINT_CLOUD_COLOR);
-      for (int i = 0; i < correspondingSize; i++)
-         correspondingColorBuffer[i] = StereoVisionPointCloudViewer.colorToInt(SOURCE_POINT_CLOUD_COLOR);
-
-      StereoVisionPointCloudMessage uncorrectedMessage = PointCloudCompression.compressPointCloud(19870612L,
-                                                                                                  uncorrectedPointCloudBuffer,
-                                                                                                  uncorrectedColorBuffer,
-                                                                                                  uncorrectedSize,
-                                                                                                  0.001,
-                                                                                                  null);
-      StereoVisionPointCloudMessage correctedMessage = PointCloudCompression.compressPointCloud(19870612L,
-                                                                                                correctedPointCloudBuffer,
-                                                                                                correctedColorBuffer,
-                                                                                                correctedSize,
-                                                                                                0.001,
-                                                                                                null);
-      StereoVisionPointCloudMessage correspondingMessage = PointCloudCompression.compressPointCloud(19870612L,
-                                                                                                    correspondingPointCloudBuffer,
-                                                                                                    correspondingColorBuffer,
-                                                                                                    correspondingSize,
-                                                                                                    0.001,
-                                                                                                    null);
-
-      uiMessager.broadcastMessage(SLAMModuleAPI.UIUncorrectedPointCloud, uncorrectedMessage);
-      uiMessager.broadcastMessage(SLAMModuleAPI.UICorrectedPointCloud, correctedMessage);
-      uiMessager.broadcastMessage(SLAMModuleAPI.UICorrespondingPointsInWorld, correspondingMessage);
+      uiMessager.broadcastMessage(SLAMModuleAPI.UIStereoSLAMPointCloud, convertedMessage);
    }
 }
