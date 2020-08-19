87a2a0646ac1cf72d27e817f854cc2e6a661efaf
Mark Paterson <mark.paterson@nasa.gov>
2020-08-19 16:38:51 -0500

    Add command-line argument to ValkyrieObstacleCourseNoUI to support logging

--- valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieObstacleCourseNoUI.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyrieObstacleCourseNoUI.java
@@ -1,6 +1,10 @@
 package us.ihmc.valkyrie;
 
+import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.Switch;
+
 import us.ihmc.avatar.DRCObstacleCourseStartingLocation;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
@@ -12,15 +16,39 @@ import us.ihmc.avatar.ros.RobotROSClockCalculatorFromPPSOffset;
 import us.ihmc.avatar.simulationStarter.DRCSimulationStarter;
 import us.ihmc.avatar.simulationStarter.DRCSimulationTools;
 import us.ihmc.communication.net.LocalObjectCommunicator;
+import us.ihmc.robotDataLogger.logger.DataServerSettings;
 import us.ihmc.simulationConstructionSetTools.util.environments.DefaultCommonAvatarEnvironment;
 
 public class ValkyrieObstacleCourseNoUI {
+	public static JSAP getArgumentParser() {
+		JSAP argumentParser = new JSAP();
+        Switch logDataSwitch = new Switch("logData").setShortFlag('l').setLongFlag("logData").setDefault("false");
+        try {
+        	argumentParser.registerParameter(logDataSwitch);
+        } catch (JSAPException e1) {
+        	System.err.println("Unable to register option parameters");
+        	e1.printStackTrace();
+        	System.exit(1);
+        }
+        return argumentParser;
+	}
+	
 	public static void main(final String[] args) throws JSAPException {
+		/* 
+		 * Supported command-line arguments:
+		 * -l/--logData: create a YoVariableServer and set up the robot model for logging
+		 */
+        JSAP argumentParser = getArgumentParser();
+        JSAPResult arguments = argumentParser.parse(args);
+
+        boolean logData = arguments.getBoolean("logData");
+		
 		/* 
 		 * For this robot model and sim, we need to override the clock so that it provides sim time on the ROS1 /clock 
 		 * topic for use with ROS1 nodes. The RobotROSClockCalculatorFromPPSOffset class is already available to do this; 
 		 * we just need to make sure that the sim uses it.
 		 */
+		
 		DRCRobotModel robotModel = new ValkyrieRobotModel(RobotTarget.SCS) {
 			private RobotROSClockCalculatorFromPPSOffset rosSimClockCalculator = null;
 			
@@ -32,6 +60,11 @@ public class ValkyrieObstacleCourseNoUI {
 			       }
 			       return rosSimClockCalculator;
 		    }
+			
+			@Override
+			public DataServerSettings getLogSettings() {
+				return new DataServerSettings(logData, "SimulationGUI");
+			}
 		};
 		DRCSimulationStarter simulationStarter = new DRCSimulationStarter(robotModel,
 				new DefaultCommonAvatarEnvironment()) {
@@ -49,6 +82,7 @@ public class ValkyrieObstacleCourseNoUI {
 		};
 
 		simulationStarter.setRunMultiThreaded(true);
+		simulationStarter.setCreateYoVariableServer(logData);
 		DRCSimulationTools.startSimulationWithGraphicSelector(simulationStarter, null, null,
 				DRCObstacleCourseStartingLocation.values());
 
