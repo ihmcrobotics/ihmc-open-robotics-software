5cb09b13faa382efa391ca49a5ae9af0fb192c65
Operator Computer <OperatorComputer@unknown>
2020-08-26 12:06:27 -0500

    Enable Realsense SLAM in background process.

--- atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasLookAndStepBehaviorUIAndModule.java
+++ atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasLookAndStepBehaviorUIAndModule.java
@@ -1,7 +1,9 @@
 package us.ihmc.atlas.behaviors;
 
+import us.ihmc.atlas.sensors.AtlasSLAMBasedREAStandaloneLauncher;
 import us.ihmc.humanoidBehaviors.ui.BehaviorUIRegistry;
 import us.ihmc.humanoidBehaviors.ui.behaviors.LookAndStepBehaviorUI;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.tools.processManagement.JavaProcessManager;
 
 public class AtlasLookAndStepBehaviorUIAndModule
@@ -10,7 +12,7 @@ public class AtlasLookAndStepBehaviorUIAndModule
    {
       JavaProcessManager manager = new JavaProcessManager();
       manager.runOrRegister("AtlasBehaviorUIAndModule", () -> new AtlasBehaviorUIAndModule(BehaviorUIRegistry.of(LookAndStepBehaviorUI.DEFINITION)));
-//      manager.runOrRegister("RealsenseSLAM", () -> new AtlasSLAMBasedREAStandaloneLauncher(false, PubSubImplementation.FAST_RTPS));
+      manager.runOrRegister("RealsenseSLAM", () -> new AtlasSLAMBasedREAStandaloneLauncher(false, PubSubImplementation.FAST_RTPS));
 //      manager.runOrRegister("LidarREA", () -> new LidarBasedREAStandaloneLauncher());
 //      manager.runOrRegister("LidarREA", () -> new RemoteLidarBasedREAModuleLauncher());
       manager.spawnProcesses(AtlasLookAndStepBehaviorUIAndModule.class, args);
