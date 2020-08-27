d993135675ffa8a2fc8a251984f97067f305ac40
Robert <rgriffin@ihmc.us>
2020-08-26 22:29:49 -0500

    got the custom atlas files loading

--- atlas/src/main/java/us/ihmc/atlas/sensors/AtlasPerceptionSuite.java
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasPerceptionSuite.java
@@ -19,19 +19,25 @@ import java.nio.file.Paths;
 
 public class AtlasPerceptionSuite extends PerceptionSuite
 {
-   private static final String SLAM_MODULE_CONFIGURATION_FILE_NAME = "./Configurations/atlasSLAMModuleConfiguration.txt";
+   private static final Path rootPath = WorkspacePathTools.handleWorkingDirectoryFuzziness("ihmc-open-robotics-software/atlas");
+   private static final String directory = "/src/main/resources/";
+
+   private static final String SLAM_MODULE_CONFIGURATION_FILE_NAME = "atlasSLAMModuleConfiguration.txt";
+   private static final String SEGMENTATION_MODULE_CONFIGURATION_FILE_NAME = "atlasSegmentationModuleConfiguration.txt";
+   private static final String LIDAR_REA_MODULE_CONFIGURATION_FILE_NAME = "atlasREAModuleConfiguration.txt";
+   private static final String REALSENSE_REA_MODULE_CONFIGURATION_FILE_NAME = "atlasRealSenseREAModuleConfiguration.txt";
 
    private final Path slamConfigurationFilePath;
    private final DRCRobotModel robotModel;
 
    public AtlasPerceptionSuite(Messager messager)
    {
-      super(messager);
+      super(messager,
+            Paths.get(rootPath.toString(), directory + SEGMENTATION_MODULE_CONFIGURATION_FILE_NAME),
+            Paths.get(rootPath.toString(), directory + LIDAR_REA_MODULE_CONFIGURATION_FILE_NAME),
+            Paths.get(rootPath.toString(), directory + REALSENSE_REA_MODULE_CONFIGURATION_FILE_NAME));
 
-      Path slamConfigurationFilePath = WorkspacePathTools.handleWorkingDirectoryFuzziness("ihmc-open-robotics-software/atlas");
-      slamConfigurationFilePath = Paths.get(slamConfigurationFilePath.toString(), "/src/main/resources/" + SLAM_MODULE_CONFIGURATION_FILE_NAME);
-
-      this.slamConfigurationFilePath = slamConfigurationFilePath;
+      this.slamConfigurationFilePath = Paths.get(rootPath.toString(), directory + SLAM_MODULE_CONFIGURATION_FILE_NAME);
 
       robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ, RobotTarget.REAL_ROBOT, false);
    }
