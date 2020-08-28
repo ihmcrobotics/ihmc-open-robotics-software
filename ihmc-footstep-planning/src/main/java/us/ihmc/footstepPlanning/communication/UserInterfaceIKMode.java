bc911cb8e86ee285523fa7e9d880e673138f61d7
Stephen McCrory <smccrory@ihmc.us>
2020-08-28 15:58:22 -0500

    Adding leg ik

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/communication/UserInterfaceIKMode.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/communication/UserInterfaceIKMode.java
@@ -4,7 +4,7 @@ import us.ihmc.robotics.robotSide.RobotSide;
 
 public enum UserInterfaceIKMode
 {
-   LEFT_ARM, RIGHT_ARM, NECK, CHEST;
+   LEFT_ARM, RIGHT_ARM, NECK, CHEST, LEFT_LEG, RIGHT_LEG;
 
    public boolean isArmMode()
    {
@@ -18,13 +18,27 @@ public enum UserInterfaceIKMode
       }
    }
 
+   public boolean isLegMode()
+   {
+      switch (this)
+      {
+         case LEFT_LEG:
+         case RIGHT_LEG:
+            return true;
+         default:
+            return false;
+      }
+   }
+
    public RobotSide getSide()
    {
       switch (this)
       {
          case LEFT_ARM:
+         case LEFT_LEG:
             return RobotSide.LEFT;
          case RIGHT_ARM:
+         case RIGHT_LEG:
             return RobotSide.RIGHT;
          default:
             return null;
