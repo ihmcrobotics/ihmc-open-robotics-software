43846632c22d0adb6beb26730125197f67335c25
OperatorComputer <OperatorComputer@unknown>
2020-09-03 16:35:37 -0500

    Add Body frame method and explicitly name methods.

--- ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/math/QuaternionCalculusTest.java
+++ ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/math/QuaternionCalculusTest.java
@@ -77,7 +77,7 @@ public class QuaternionCalculusTest
          Vector3D actualAngularVelocity = new Vector3D();
          Vector4D qDot = new Vector4D();
 
-         quaternionCalculus.computeQDot(q, expectedAngularVelocity, qDot);
+         quaternionCalculus.computeQDotInWorldFrame(q, expectedAngularVelocity, qDot);
          quaternionCalculus.computeAngularVelocityInWorldFrame(q, qDot, actualAngularVelocity);
 
          assertTrue(expectedAngularVelocity.epsilonEquals(actualAngularVelocity, EPSILON));
@@ -104,22 +104,22 @@ public class QuaternionCalculusTest
          Vector4D qDot = new Vector4D();
          Vector4D qDDot = new Vector4D();
 
-         quaternionCalculus.computeQDot(q, angularVelocity, qDot);
+         quaternionCalculus.computeQDotInWorldFrame(q, angularVelocity, qDot);
 
-         quaternionCalculus.computeQDDot(q, qDot, expectedAngularAcceleration, qDDot);
+         quaternionCalculus.computeQDDotInWorldFrame(q, qDot, expectedAngularAcceleration, qDDot);
          quaternionCalculus.computeAngularAcceleration(q, qDot, qDDot, actualAngularAcceleration);
          assertTrue(expectedAngularAcceleration.epsilonEquals(actualAngularAcceleration, EPSILON));
 
-         quaternionCalculus.computeQDDot(q, angularVelocity, actualAngularAcceleration, qDDot);
+         quaternionCalculus.computeQDDotInWorldFrame(q, angularVelocity, actualAngularAcceleration, qDDot);
          quaternionCalculus.computeAngularAcceleration(q, qDot, qDDot, actualAngularAcceleration);
          assertTrue(expectedAngularAcceleration.epsilonEquals(actualAngularAcceleration, EPSILON));
 
-         quaternionCalculus.computeQDDot(q, qDot, angularVelocity, actualAngularAcceleration, qDDot);
+         quaternionCalculus.computeQDDotInWorldFrame(q, qDot, angularVelocity, actualAngularAcceleration, qDDot);
          quaternionCalculus.computeAngularAcceleration(q, qDot, qDDot, actualAngularAcceleration);
          assertTrue(expectedAngularAcceleration.epsilonEquals(actualAngularAcceleration, EPSILON));
 
-         quaternionCalculus.computeQDDot(q, qDot, expectedAngularAcceleration, qDDot);
-         quaternionCalculus.computeAngularAcceleration(q, qDDot, angularVelocity, actualAngularAcceleration);
+         quaternionCalculus.computeQDDotInWorldFrame(q, qDot, expectedAngularAcceleration, qDDot);
+         quaternionCalculus.computeAngularAccelerationInWorldFrame(q, qDDot, angularVelocity, actualAngularAcceleration);
          assertTrue(expectedAngularAcceleration.epsilonEquals(actualAngularAcceleration, EPSILON));
       }
    }
