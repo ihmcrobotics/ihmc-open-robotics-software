febd446defdfe9fa52fe3934470511018fabae87
Duncan Calvert <dcalvert@ihmc.us>
2020-09-04 15:55:28 -0500

    Launch RealsenseREA headless.

--- atlas/src/main/java/us/ihmc/atlas/sensors/AtlasRealsenseBasedREAStandaloneLauncher.java
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasRealsenseBasedREAStandaloneLauncher.java
@@ -1,17 +1,18 @@
 package us.ihmc.atlas.sensors;
 
-import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
+import us.ihmc.commons.exception.DefaultExceptionHandler;
+import us.ihmc.commons.exception.ExceptionTools;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.util.NetworkPorts;
+import us.ihmc.javafx.applicationCreator.JavaFXApplicationCreator;
 import us.ihmc.pubsub.DomainFactory;
 import us.ihmc.robotEnvironmentAwareness.io.FilePropertyHelper;
 import us.ihmc.robotEnvironmentAwareness.perceptionSuite.RealSenseREANetworkProvider;
 import us.ihmc.robotEnvironmentAwareness.ui.LIDARBasedEnvironmentAwarenessUI;
 import us.ihmc.robotEnvironmentAwareness.updaters.LIDARBasedREAModule;
 import us.ihmc.robotEnvironmentAwareness.updaters.REANetworkProvider;
-import us.ihmc.robotEnvironmentAwareness.updaters.REAPlanarRegionPublicNetworkProvider;
 import us.ihmc.ros2.ROS2Node;
 import us.ihmc.tools.io.WorkspacePathTools;
 
@@ -20,38 +21,53 @@ import java.nio.file.Paths;
 
 import static us.ihmc.robotEnvironmentAwareness.communication.REACommunicationProperties.*;
 
-public class AtlasRealsenseBasedREAStandaloneLauncher extends Application
+public class AtlasRealsenseBasedREAStandaloneLauncher
 {
    private static final Path rootPath = WorkspacePathTools.handleWorkingDirectoryFuzziness("ihmc-open-robotics-software");
    private static final String directory = "/atlas/src/main/resources/";
 
    private static final String REALSENSE_REA_MODULE_CONFIGURATION_FILE_NAME = "atlasRealSenseREAModuleConfiguration.txt";
 
+   private final boolean spawnUI;
+
    private LIDARBasedEnvironmentAwarenessUI ui;
    private LIDARBasedREAModule module;
 
-   @Override
-   public void start(Stage primaryStage) throws Exception
+   public AtlasRealsenseBasedREAStandaloneLauncher(boolean spawnUI)
    {
+      this.spawnUI = spawnUI;
+      JavaFXApplicationCreator.createAJavaFXApplication();
+      Platform.runLater(() -> ExceptionTools.handle(this::setup, DefaultExceptionHandler.PRINT_STACKTRACE));
+   }
+
+   private void setup() throws Exception
+   {
+      Stage primaryStage = null;
+      if (spawnUI)
+         primaryStage = new Stage();
       Path realsenseREAConfigurationFilePath = Paths.get(rootPath.toString(), directory + REALSENSE_REA_MODULE_CONFIGURATION_FILE_NAME);
 
       ROS2Node ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, ROS2Tools.REA_NODE_NAME);
       REANetworkProvider realSenseREANetworkProvider = new RealSenseREANetworkProvider(ros2Node, stereoOutputTopic);
 
-      ui = LIDARBasedEnvironmentAwarenessUI.creatIntraprocessUI(primaryStage,
-                                                                NetworkPorts.REA_MODULE2_UI_PORT);
+      if (spawnUI)
+         ui = LIDARBasedEnvironmentAwarenessUI.creatIntraprocessUI(primaryStage,
+                                                                   NetworkPorts.REA_MODULE2_UI_PORT);
       module = LIDARBasedREAModule.createIntraprocessModule(new FilePropertyHelper(realsenseREAConfigurationFilePath.toFile()),
                                                             realSenseREANetworkProvider,
                                                             NetworkPorts.REA_MODULE2_UI_PORT);
       module.setParametersForStereo();
       module.loadConfigurationsFromFile();
 
-      ui.show();
+      if (spawnUI)
+         ui.show();
       module.start();
+
+      if (spawnUI)
+         primaryStage.setOnCloseRequest(event -> stop());
    }
 
-   @Override
-   public void stop() throws Exception
+   public void stop()
    {
       ui.stop();
       module.stop();
@@ -61,6 +77,6 @@ public class AtlasRealsenseBasedREAStandaloneLauncher extends Application
 
    public static void main(String[] args)
    {
-      launch(args);
+      new AtlasRealsenseBasedREAStandaloneLauncher(true);
    }
 }
