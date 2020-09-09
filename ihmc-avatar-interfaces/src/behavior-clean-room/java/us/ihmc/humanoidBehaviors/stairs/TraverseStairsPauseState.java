082e4405c7700a34108240e7e42b3d9841ec7cc2
Stephen McCrory <smccrory@ihmc.us>
2020-09-09 11:54:39 -0500

    Initial state implementation

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/stairs/TraverseStairsPauseState.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/stairs/TraverseStairsPauseState.java
@@ -1,6 +1,7 @@
 package us.ihmc.humanoidBehaviors.stairs;
 
 import us.ihmc.humanoidBehaviors.tools.BehaviorHelper;
+import us.ihmc.log.LogTools;
 import us.ihmc.robotics.stateMachine.core.State;
 
 public class TraverseStairsPauseState implements State
@@ -17,12 +18,25 @@ public class TraverseStairsPauseState implements State
    @Override
    public void onEntry()
    {
-
+      LogTools.debug("Entering " + getClass().getSimpleName());
    }
 
    @Override
    public void doAction(double timeInState)
    {
+   }
 
+   @Override
+   public boolean isDone(double timeInState)
+   {
+      if (timeInState >= parameters.get(TraverseStairsBehaviorParameters.pauseTime))
+      {
+         LogTools.debug(getClass().getSimpleName() + " is done");
+         return true;
+      }
+      else
+      {
+         return false;
+      }
    }
 }
