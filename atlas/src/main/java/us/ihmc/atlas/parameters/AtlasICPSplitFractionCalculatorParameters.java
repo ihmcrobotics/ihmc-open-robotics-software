debece08c57abb1734ff8cc8d2f1062cf02e8e22
Operator Computer <OperatorComputer@unknown>
2020-09-10 18:26:19 -0500

    Tune step downs.

--- atlas/src/main/java/us/ihmc/atlas/parameters/AtlasICPSplitFractionCalculatorParameters.java
+++ atlas/src/main/java/us/ihmc/atlas/parameters/AtlasICPSplitFractionCalculatorParameters.java
@@ -30,7 +30,7 @@ public class AtlasICPSplitFractionCalculatorParameters implements SplitFractionC
    /** {@inheritDoc} */
    public double getLargestStepDownHeight()
    {
-      return 0.2;
+      return 0.15;
    }
 
    /** {@inheritDoc} */
