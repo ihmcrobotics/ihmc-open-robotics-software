902868207718102b33c159bb435ee237213638ed
Duncan Calvert <dcalvert@ihmc.us>
2020-09-11 13:31:50 -0500

    Fix bug with ROS2PublisherMap where two publishers of the same type could not be created.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/ros2/ROS2PublisherMap.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/ros2/ROS2PublisherMap.java
@@ -15,7 +15,7 @@ public class ROS2PublisherMap
 {
    private final ROS2NodeInterface ros2Node;
 
-   private HashMap<Class, IHMCROS2Publisher> map = new HashMap<>();
+   private HashMap<ROS2Topic, IHMCROS2Publisher> map = new HashMap<>();
 
    public ROS2PublisherMap(ROS2NodeInterface ros2Node)
    {
@@ -24,20 +24,20 @@ public class ROS2PublisherMap
 
    public <T> void publish(ROS2Topic<T> topic, T message)
    {
-      IHMCROS2Publisher publisher = map.computeIfAbsent(message.getClass(), key -> new IHMCROS2Publisher<>(ros2Node, topic));
+      IHMCROS2Publisher publisher = map.computeIfAbsent(topic, key -> new IHMCROS2Publisher<>(ros2Node, topic));
       publisher.publish(message);
    }
 
    public void publish(ROS2Topic<Pose3D> topic, Pose3D message)
    {
-      IHMCROS2Publisher publisher = map.computeIfAbsent(Pose3D.class, key -> IHMCROS2Publisher.newPose3DPublisher(ros2Node, topic));
+      IHMCROS2Publisher publisher = map.computeIfAbsent(topic, key -> IHMCROS2Publisher.newPose3DPublisher(ros2Node, topic));
       publisher.publish(message);
    }
 
    public void publish(ROS2Topic<Empty> topic)
    {
       Empty message = new Empty();
-      IHMCROS2Publisher publisher = map.computeIfAbsent(message.getClass(), key -> new IHMCROS2Publisher<>(ros2Node, topic));
+      IHMCROS2Publisher publisher = map.computeIfAbsent(topic, key -> new IHMCROS2Publisher<>(ros2Node, topic));
       publisher.publish(message);
    }
 }
