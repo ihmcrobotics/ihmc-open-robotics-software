4119310f955b4018ca01449f9fdaf7c11910ea15
Duncan Calvert <dcalvert@ihmc.us>
2020-09-20 18:04:27 -0500

    Add StringTools method for formatting doubles with 3 decimal places.

--- ihmc-java-toolkit/src/main/java/us/ihmc/tools/string/StringTools.java
+++ ihmc-java-toolkit/src/main/java/us/ihmc/tools/string/StringTools.java
@@ -32,11 +32,45 @@ public class StringTools
       return () -> ParameterizedMessageFactory.INSTANCE.newMessage(message, parameters).getFormattedMessage();
    }
 
+   public static Supplier<String> format3D(String message, Object... parameters)
+   {
+      Object[] formattedParameters = new Object[parameters.length];
+      for (int i = 0; i < parameters.length; i++)
+      {
+         if (parameters[i] instanceof Double)
+         {
+            formattedParameters[i] = new FormattedDouble((Double) parameters[i]);
+         }
+         else
+         {
+            formattedParameters[i] = parameters[i];
+         }
+      }
+
+      return format(message, formattedParameters);
+   }
+
    public static String zUpPoseString(Pose3DReadOnly pose)
    {
       return "(" + FormattingTools.getFormattedDecimal3D(pose.getX())
             + ", " + FormattingTools.getFormattedDecimal3D(pose.getY())
             + ", " + FormattingTools.getFormattedDecimal3D(pose.getZ())
             + ") yaw: " + FormattingTools.getFormattedDecimal3D(pose.getYaw());
+   }
+
+   public static class FormattedDouble
+   {
+      private final Double number;
+
+      public FormattedDouble(Double number)
+      {
+         this.number = number;
+      }
+
+      @Override
+      public String toString()
+      {
+         return FormattingTools.getFormattedDecimal3D(number);
+      }
    }
 }
