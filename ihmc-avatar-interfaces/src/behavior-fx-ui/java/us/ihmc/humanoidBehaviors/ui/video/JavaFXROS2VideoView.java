75a4692eb42a4887dd30231aafd66ca002285a04
Operator Computer <OperatorComputer@unknown>
2020-09-21 17:15:55 -0500

    Clean up D435 ROS 1 bridge.

--- ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/video/JavaFXROS2VideoView.java
+++ ihmc-avatar-interfaces/src/behavior-fx-ui/java/us/ihmc/humanoidBehaviors/ui/video/JavaFXROS2VideoView.java
@@ -1,12 +1,9 @@
 package us.ihmc.humanoidBehaviors.ui.video;
 
-import controller_msgs.msg.dds.Image32;
 import controller_msgs.msg.dds.VideoPacket;
-import rcl_interfaces.msg.dds.Log;
 import sensor_msgs.msg.dds.CompressedImage;
 import sensor_msgs.msg.dds.Image;
 import us.ihmc.communication.IHMCROS2Callback;
-import us.ihmc.idl.IDLSequence;
 import us.ihmc.log.LogTools;
 import us.ihmc.ros2.ROS2Topic;
 import us.ihmc.ros2.ROS2NodeInterface;
@@ -16,8 +13,6 @@ public class JavaFXROS2VideoView extends JavaFXVideoView
    private final ROS2NodeInterface ros2Node;
    private final ROS2Topic<?> topic;
    private IHMCROS2Callback<?> ros2Callback;
-
-   int i = 0;
 
    public JavaFXROS2VideoView(ROS2NodeInterface ros2Node, ROS2Topic<?> topic, int width, int height, boolean flipX, boolean flipY)
    {
@@ -39,25 +34,12 @@ public class JavaFXROS2VideoView extends JavaFXVideoView
          }
          else if (message instanceof Image)
          {
-//            if (i++ % 30 == 0)
-//            {
-//               LogTools.info("Received # {}", i);
-               acceptVideo(((Image) message).getData());
-//            }
+            acceptVideo(((Image) message).getData());
          }
          else if (message instanceof CompressedImage)
          {
-//            if (i++ % 30 == 0)
-//            {
-//               LogTools.info("Received compressed # {}", i);
-               acceptVideo(((CompressedImage) message).getData());
-//            }
+            acceptVideo(((CompressedImage) message).getData());
          }
-//         else if (message instanceof Image32)
-//         {
-//            IDLSequence.Byte();
-//            acceptVideo(((Image32) message).getRgbdata());
-//         }
       });
       super.start();
    }
--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sensors/RealsenseImageROS1Bridge.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sensors/RealsenseImageROS1Bridge.java
@@ -1,21 +1,17 @@
 package us.ihmc.avatar.sensors;
 
 import sensor_msgs.msg.dds.CompressedImage;
-import sensor_msgs.msg.dds.Image;
 import us.ihmc.communication.IHMCROS2Publisher;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.configuration.NetworkParameters;
 import us.ihmc.log.LogTools;
 import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.ros2.ROS2Node;
-import us.ihmc.tools.string.StringTools;
+import us.ihmc.ros2.ROS2Topic;
 import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.utilities.ros.subscriber.AbstractRosTopicSubscriber;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 public class RealsenseImageROS1Bridge extends AbstractRosTopicSubscriber<sensor_msgs.CompressedImage>
 {
@@ -32,80 +28,21 @@ public class RealsenseImageROS1Bridge extends AbstractRosTopicSubscriber<sensor_
       rosMainNode.attachSubscriber("/depthcam/color/image_raw/compressed", this);
       rosMainNode.execute();
 
-      LogTools.info(ROS2Tools.D435_VIDEO);
-      imagePublisher = ROS2Tools.createPublisher(ros2Node, ROS2Tools.D435_VIDEO);
+      ROS2Topic<CompressedImage> topic = ROS2Tools.D435_VIDEO;
+      LogTools.info("Publishing to {}", topic.getName());
+      imagePublisher = ROS2Tools.createPublisher(ros2Node, topic);
    }
-
-   int i = 0;
 
    @Override
    public void onNewMessage(sensor_msgs.CompressedImage ros1Image)
    {
       try
       {
-//         LogTools.info(StringTools.format("Format: {}", ros1Image.getFormat()));
          CompressedImage ros2Image = new CompressedImage();
-         //      ros2Image.setHeight(ros1Image.getHeight());
-         //      ros2Image.setWidth(ros1Image.getWidth());
          byte[] data = ros1Image.getData().array();
          int dataOffset = ros1Image.getData().arrayOffset();
          int length = data.length;
-                  ros2Image.getData().add(data, dataOffset, length - dataOffset);
-         //         ros2Image.getData().add(data);
-
-         //      Image ros2Image = new Image();
-         //      ros2Image.setHeight(ros1Image.getHeight());
-         //      ros2Image.setWidth(ros1Image.getWidth());
-         //      byte[] data = ros1Image.getData().array();
-         //      int dataOffset = ros1Image.getData().arrayOffset();
-         //      int length = data.length;
-         ////      LogTools.info(StringTools.format("Copying width {} height {} length {} offset: {}",
-         ////                                       ros1Image.getWidth(), ros1Image.getHeight(), length, dataOffset));
-         ////      ros2Image.getData().add(data, dataOffset, length - dataOffset);
-         //      ros2Image.getData().add(data);
-
-         //      Image32 ros2Image = new Image32();
-         //      ros2Image.setHeight(ros1Image.getHeight());
-         //      ros2Image.setWidth(ros1Image.getWidth());
-         //
-         //      ChannelBuffer data = ros1Image.getData();
-         //      byte[] array = data.array();
-         //      int dataIndex = data.arrayOffset();
-         //      for (int i = 0; i < ros1Image.getHeight(); i++)
-         //      {
-         //         for (int j = 0; j < ros1Image.getWidth(); j++)
-         //         {
-         //            int b = array[dataIndex];
-         //            dataIndex++;
-         //            int g = array[dataIndex];
-         //            dataIndex++;
-         //            int r = array[dataIndex];
-         //            dataIndex++;
-         //
-         //            int rgbColor = convertBGR2RGB(b, g, r);
-         //            ros2Image.getRgbdata().add(rgbColor);
-         //         }
-         //      }
-
-         //      ChannelBuffer data = ros1Image.getData();
-         //      byte[] array = data.array();
-         //      int dataIndex = data.arrayOffset();
-         //      for (int i = 0; i < ros1Image.getHeight(); i++)
-         //      {
-         //         for (int j = 0; j < ros1Image.getWidth(); j++)
-         //         {
-         //            byte b = array[dataIndex];
-         //            dataIndex++;
-         //            byte g = array[dataIndex];
-         //            dataIndex++;
-         //            byte r = array[dataIndex];
-         //            dataIndex++;
-         //
-         //            ros2Image.getData().add(r);
-         //            ros2Image.getData().add(g);
-         //            ros2Image.getData().add(b);
-         //         }
-         //      }
+         ros2Image.getData().add(data, dataOffset, length - dataOffset);
 
          imagePublisher.publish(ros2Image);
       }
@@ -116,13 +53,7 @@ public class RealsenseImageROS1Bridge extends AbstractRosTopicSubscriber<sensor_
       }
    }
 
-   private static int convertBGR2RGB(int b, int g, int r)
-   {
-      int rgb = ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
-      return rgb;
-   }
-
-   public static void main(String[] args) throws URISyntaxException, IOException
+   public static void main(String[] args)
    {
       new RealsenseImageROS1Bridge();
    }
