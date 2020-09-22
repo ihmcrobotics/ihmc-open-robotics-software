172bba0f9a85a1c286b37790f6a84209adee3317
Sylvain Bertrand <sbertrand@ihmc.us>
2020-09-22 11:37:24 -0500

    Renamed message for the ground plane.

--- ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/GroundPlaneMessagePubSubType.java
+++ ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/GroundPlaneMessagePubSubType.java
@@ -2,21 +2,21 @@ package controller_msgs.msg.dds;
 
 /**
 * 
-* Topic data type of the struct "QuadrupedGroundPlaneMessage" defined in "QuadrupedGroundPlaneMessage_.idl". Use this class to provide the TopicDataType to a Participant. 
+* Topic data type of the struct "GroundPlaneMessage" defined in "GroundPlaneMessage_.idl". Use this class to provide the TopicDataType to a Participant. 
 *
-* This file was automatically generated from QuadrupedGroundPlaneMessage_.idl by us.ihmc.idl.generator.IDLGenerator. 
-* Do not update this file directly, edit QuadrupedGroundPlaneMessage_.idl instead.
+* This file was automatically generated from GroundPlaneMessage_.idl by us.ihmc.idl.generator.IDLGenerator. 
+* Do not update this file directly, edit GroundPlaneMessage_.idl instead.
 *
 */
-public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.TopicDataType<controller_msgs.msg.dds.QuadrupedGroundPlaneMessage>
+public class GroundPlaneMessagePubSubType implements us.ihmc.pubsub.TopicDataType<controller_msgs.msg.dds.GroundPlaneMessage>
 {
-   public static final java.lang.String name = "controller_msgs::msg::dds_::QuadrupedGroundPlaneMessage_";
+   public static final java.lang.String name = "controller_msgs::msg::dds_::GroundPlaneMessage_";
 
    private final us.ihmc.idl.CDR serializeCDR = new us.ihmc.idl.CDR();
    private final us.ihmc.idl.CDR deserializeCDR = new us.ihmc.idl.CDR();
 
    @Override
-   public void serialize(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data, us.ihmc.pubsub.common.SerializedPayload serializedPayload) throws java.io.IOException
+   public void serialize(controller_msgs.msg.dds.GroundPlaneMessage data, us.ihmc.pubsub.common.SerializedPayload serializedPayload) throws java.io.IOException
    {
       serializeCDR.serialize(serializedPayload);
       write(data, serializeCDR);
@@ -24,7 +24,7 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
    }
 
    @Override
-   public void deserialize(us.ihmc.pubsub.common.SerializedPayload serializedPayload, controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data) throws java.io.IOException
+   public void deserialize(us.ihmc.pubsub.common.SerializedPayload serializedPayload, controller_msgs.msg.dds.GroundPlaneMessage data) throws java.io.IOException
    {
       deserializeCDR.deserialize(serializedPayload);
       read(data, deserializeCDR);
@@ -50,12 +50,12 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
       return current_alignment - initial_alignment;
    }
 
-   public final static int getCdrSerializedSize(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data)
+   public final static int getCdrSerializedSize(controller_msgs.msg.dds.GroundPlaneMessage data)
    {
       return getCdrSerializedSize(data, 0);
    }
 
-   public final static int getCdrSerializedSize(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data, int current_alignment)
+   public final static int getCdrSerializedSize(controller_msgs.msg.dds.GroundPlaneMessage data, int current_alignment)
    {
       int initial_alignment = current_alignment;
 
@@ -70,7 +70,7 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
       return current_alignment - initial_alignment;
    }
 
-   public static void write(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data, us.ihmc.idl.CDR cdr)
+   public static void write(controller_msgs.msg.dds.GroundPlaneMessage data, us.ihmc.idl.CDR cdr)
    {
       cdr.write_type_4(data.getSequenceId());
 
@@ -78,7 +78,7 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
       geometry_msgs.msg.dds.Vector3PubSubType.write(data.getRegionNormal(), cdr);
    }
 
-   public static void read(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data, us.ihmc.idl.CDR cdr)
+   public static void read(controller_msgs.msg.dds.GroundPlaneMessage data, us.ihmc.idl.CDR cdr)
    {
       data.setSequenceId(cdr.read_type_4());
       	
@@ -88,7 +88,7 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
    }
 
    @Override
-   public final void serialize(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data, us.ihmc.idl.InterchangeSerializer ser)
+   public final void serialize(controller_msgs.msg.dds.GroundPlaneMessage data, us.ihmc.idl.InterchangeSerializer ser)
    {
       ser.write_type_4("sequence_id", data.getSequenceId());
       ser.write_type_a("region_origin", new geometry_msgs.msg.dds.PointPubSubType(), data.getRegionOrigin());
@@ -98,7 +98,7 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
    }
 
    @Override
-   public final void deserialize(us.ihmc.idl.InterchangeSerializer ser, controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data)
+   public final void deserialize(us.ihmc.idl.InterchangeSerializer ser, controller_msgs.msg.dds.GroundPlaneMessage data)
    {
       data.setSequenceId(ser.read_type_4("sequence_id"));
       ser.read_type_a("region_origin", new geometry_msgs.msg.dds.PointPubSubType(), data.getRegionOrigin());
@@ -107,15 +107,15 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
 
    }
 
-   public static void staticCopy(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage src, controller_msgs.msg.dds.QuadrupedGroundPlaneMessage dest)
+   public static void staticCopy(controller_msgs.msg.dds.GroundPlaneMessage src, controller_msgs.msg.dds.GroundPlaneMessage dest)
    {
       dest.set(src);
    }
 
    @Override
-   public controller_msgs.msg.dds.QuadrupedGroundPlaneMessage createData()
+   public controller_msgs.msg.dds.GroundPlaneMessage createData()
    {
-      return new controller_msgs.msg.dds.QuadrupedGroundPlaneMessage();
+      return new controller_msgs.msg.dds.GroundPlaneMessage();
    }
    @Override
    public int getTypeSize()
@@ -129,24 +129,24 @@ public class QuadrupedGroundPlaneMessagePubSubType implements us.ihmc.pubsub.Top
       return name;
    }
    
-   public void serialize(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data, us.ihmc.idl.CDR cdr)
+   public void serialize(controller_msgs.msg.dds.GroundPlaneMessage data, us.ihmc.idl.CDR cdr)
    {
       write(data, cdr);
    }
 
-   public void deserialize(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage data, us.ihmc.idl.CDR cdr)
+   public void deserialize(controller_msgs.msg.dds.GroundPlaneMessage data, us.ihmc.idl.CDR cdr)
    {
       read(data, cdr);
    }
    
-   public void copy(controller_msgs.msg.dds.QuadrupedGroundPlaneMessage src, controller_msgs.msg.dds.QuadrupedGroundPlaneMessage dest)
+   public void copy(controller_msgs.msg.dds.GroundPlaneMessage src, controller_msgs.msg.dds.GroundPlaneMessage dest)
    {
       staticCopy(src, dest);
    }
 
    @Override
-   public QuadrupedGroundPlaneMessagePubSubType newInstance()
+   public GroundPlaneMessagePubSubType newInstance()
    {
-      return new QuadrupedGroundPlaneMessagePubSubType();
+      return new GroundPlaneMessagePubSubType();
    }
 }
