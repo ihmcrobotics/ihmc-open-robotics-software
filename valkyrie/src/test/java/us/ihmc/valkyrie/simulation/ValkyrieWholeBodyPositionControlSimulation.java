cea21fe9a7db1187e6dc51bf636e65ea66ae6e6d
Sylvain Bertrand <sbertrand@ihmc.us>
2020-09-24 14:59:48 -0500

    Tweaks of the simulation starter.

--- valkyrie/src/test/java/us/ihmc/valkyrie/simulation/ValkyrieWholeBodyPositionControlSimulation.java
+++ valkyrie/src/test/java/us/ihmc/valkyrie/simulation/ValkyrieWholeBodyPositionControlSimulation.java
@@ -2,6 +2,7 @@ package us.ihmc.valkyrie.simulation;
 
 import java.util.EnumMap;
 
+import controller_msgs.msg.dds.GroundPlaneMessage;
 import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.networkProcessor.HumanoidNetworkProcessorParameters;
 import us.ihmc.avatar.networkProcessor.kinematicsToolboxModule.KinematicsToolboxModule;
@@ -13,17 +14,25 @@ import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.Hi
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.HighLevelControllerState;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.JointspacePositionControllerState;
 import us.ihmc.commonWalkingControlModules.momentumBasedController.HighLevelHumanoidControllerToolbox;
+import us.ihmc.communication.IHMCROS2Publisher;
+import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.controllerAPI.CommandInputManager;
 import us.ihmc.communication.controllerAPI.StatusMessageOutputManager;
+import us.ihmc.euclid.Axis3D;
+import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DBasics;
 import us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.robotics.stateMachine.core.State;
 import us.ihmc.robotics.stateMachine.core.StateTransition;
+import us.ihmc.ros2.ROS2Node;
 import us.ihmc.sensorProcessing.outputData.JointDesiredOutputListReadOnly;
 import us.ihmc.simulationConstructionSetTools.util.environments.DefaultCommonAvatarEnvironment;
+import us.ihmc.valkyrie.ValkyrieInitialSetupFactories;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 import us.ihmc.valkyrie.configuration.ValkyrieRobotVersion;
+import us.ihmc.valkyrie.parameters.ValkyrieJointMap;
 import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class ValkyrieWholeBodyPositionControlSimulation
@@ -33,18 +42,35 @@ public class ValkyrieWholeBodyPositionControlSimulation
       new ValkyrieWholeBodyPositionControlSimulation();
    }
 
+   private final double dt = 8.0e-4;
    private final ValkyrieRobotModel robotModel;
+
+   private final ROS2Node ros2Node = ROS2Tools.createROS2Node(PubSubImplementation.FAST_RTPS, "ground_plane_node");
+   private final IHMCROS2Publisher<GroundPlaneMessage> groundPlanePublisher = ROS2Tools.createPublisher(ros2Node,
+                                                                                                        ROS2Tools.IHMC_ROOT.withTypeName(GroundPlaneMessage.class));
+   private final GroundPlaneMessage groundPlaneMessage = new GroundPlaneMessage();
 
    public ValkyrieWholeBodyPositionControlSimulation()
    {
+      groundPlaneMessage.getRegionNormal().set(Axis3D.Z);
+
       robotModel = new ValkyrieRobotModel(RobotTarget.SCS, ValkyrieRobotVersion.FINGERLESS);
+      ValkyrieJointMap jointMap = robotModel.getJointMap();
       robotModel.setHighLevelControllerParameters(new ValkyrieSimulationPositionControlParameters(robotModel.getHighLevelControllerParameters(),
-                                                                                                  robotModel.getJointMap(),
+                                                                                                  jointMap,
                                                                                                   HighLevelControllerName.CUSTOM1));
+      ValkyrieSimulationLowLevelControllerFactory simulationLowLevelControllerFactory = new ValkyrieSimulationLowLevelControllerFactory(jointMap, dt);
+      robotModel.setSimulationLowLevelControllerFactory(simulationLowLevelControllerFactory);
+
+      robotModel.setSimulateDT(dt);
+      robotModel.setControllerDT(dt);
+      robotModel.setEstimatorDT(dt);
 
       DRCSimulationStarter simulationStarter = new DRCSimulationStarter(robotModel, new DefaultCommonAvatarEnvironment());
       simulationStarter.setUsePerfectSensors(true);
+      simulationStarter.setRobotInitialSetup(ValkyrieInitialSetupFactories.newCrawl1(jointMap));
       simulationStarter.getSCSInitialSetup().setUseExperimentalPhysicsEngine(true);
+      simulationStarter.getSCSInitialSetup().setRecordFrequency(10);
       simulationStarter.registerHighLevelControllerState(new HighLevelControllerStateFactory()
       {
          @Override
@@ -62,6 +88,14 @@ public class ValkyrieWholeBodyPositionControlSimulation
             HighLevelHumanoidControllerToolbox controllerToolbox = controllerFactoryHelper.getHighLevelHumanoidControllerToolbox();
             HighLevelControllerParameters highLevelControllerParameters = controllerFactoryHelper.getHighLevelControllerParameters();
             JointDesiredOutputListReadOnly highLevelControllerOutput = controllerFactoryHelper.getLowLevelControllerOutput();
+
+            controllerToolbox.addUpdatable(time ->
+            {
+               Point3DBasics rootJointPosition = controllerToolbox.getFullRobotModel().getRootJoint().getJointPose().getPosition();
+               groundPlaneMessage.getRegionOrigin().set(new Point2D(rootJointPosition));
+               groundPlanePublisher.publish(groundPlaneMessage);
+            });
+
             return new JointspacePositionControllerState(getStateEnum(),
                                                          commandInputManager,
                                                          statusOutputManager,
@@ -92,6 +126,7 @@ public class ValkyrieWholeBodyPositionControlSimulation
       HumanoidNetworkProcessorParameters networkProcessorParameters = new HumanoidNetworkProcessorParameters();
       simulationStarter.createSimulation(networkProcessorParameters, true, false);
       simulationStarter.getAvatarSimulation().getHighLevelHumanoidControllerFactory().getRequestedControlStateEnum().set(null);
+      simulationStarter.getAvatarSimulation().getSimulationConstructionSet().setFastSimulate(true, 10);
 
       new KinematicsToolboxModule(robotModel, false, 10, false, PubSubImplementation.FAST_RTPS);
    }
