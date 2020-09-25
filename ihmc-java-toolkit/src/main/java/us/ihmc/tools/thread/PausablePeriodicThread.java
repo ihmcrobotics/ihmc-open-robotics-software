fdf0f36b216c710c5127e8769c6eff1922a52bc3
Operator Computer <OperatorComputer@unknown>
2020-09-25 13:17:56 -0500

    Add daemon option for ExceptionPrintingThreadScheduler and PausablePeriodicThread.

--- ihmc-java-toolkit/src/main/java/us/ihmc/tools/thread/PausablePeriodicThread.java
+++ ihmc-java-toolkit/src/main/java/us/ihmc/tools/thread/PausablePeriodicThread.java
@@ -12,15 +12,20 @@ public class PausablePeriodicThread
 
    public PausablePeriodicThread(String name, double period, Runnable runnable)
    {
-      this(name, period, 0, runnable);
+      this(name, period, 0, false, runnable);
    }
 
    public PausablePeriodicThread(String name, double period, int crashesBeforeGivingUp, Runnable runnable)
    {
+      this(name, period, crashesBeforeGivingUp, false, runnable);
+   }
+
+   public PausablePeriodicThread(String name, double period, int crashesBeforeGivingUp, boolean runAsDaemon, Runnable runnable)
+   {
       this.runnable = runnable;
       this.period = period;
 
-      scheduler = new ExceptionHandlingThreadScheduler(name, ExceptionHandlingThreadScheduler.DEFAULT_HANDLER, crashesBeforeGivingUp);
+      scheduler = new ExceptionHandlingThreadScheduler(name, ExceptionHandlingThreadScheduler.DEFAULT_HANDLER, crashesBeforeGivingUp, runAsDaemon);
    }
 
    public void start()
