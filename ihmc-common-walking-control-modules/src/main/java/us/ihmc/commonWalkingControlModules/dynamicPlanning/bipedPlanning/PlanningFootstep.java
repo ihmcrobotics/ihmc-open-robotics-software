83e2337466b6f1de8503f261fc4b80ece058ce14
Robert <rgriffin@ihmc.us>
2020-10-01 16:11:14 -0500

    started working on making a cop genreator state

---
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/bipedPlanning/PlanningFootstep.java
@@ -0,0 +1,11 @@
+package us.ihmc.commonWalkingControlModules.dynamicPlanning.bipedPlanning;
+
+import us.ihmc.yoVariables.registry.YoRegistry;
+
+public class PlanningFootstep
+{
+   public PlanningFootstep(String suffix, YoRegistry registry)
+   {
+
+   }
+}
