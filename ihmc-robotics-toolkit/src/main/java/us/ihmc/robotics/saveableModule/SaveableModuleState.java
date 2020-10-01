c2635adb4271b690e46604fe9115029ec7c8543b
Robert <rgriffin@ihmc.us>
2020-10-01 14:56:13 -0500

    set up more of the saveable module stuff

---
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/saveableModule/SaveableModuleState.java
@@ -0,0 +1,67 @@
+package us.ihmc.robotics.saveableModule;
+
+import us.ihmc.yoVariables.variable.YoBoolean;
+import us.ihmc.yoVariables.variable.YoDouble;
+import us.ihmc.yoVariables.variable.YoInteger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SaveableModuleState
+{
+   private final List<YoDouble> doubles = new ArrayList<>();
+   private final List<YoInteger> integers = new ArrayList<>();
+   private final List<YoBoolean> booleans = new ArrayList<>();
+
+   public void registerDoubleToSave(YoDouble yoDouble)
+   {
+      doubles.add(yoDouble);
+   }
+
+   public void registerIntegerToSave(YoInteger yoInteger)
+   {
+      integers.add(yoInteger);
+   }
+
+   public void registerBooleanToSave(YoBoolean yoBoolean)
+   {
+      booleans.add(yoBoolean);
+   }
+
+   @Override
+   public String toString()
+   {
+      String string = "";
+      for (int i = 0; i < doubles.size(); i++)
+      {
+         YoDouble yoDouble = doubles.get(i);
+         string += yoDouble.getFullNameString() + ": " + yoDouble.getValue() + ", ";
+      }
+      for (int i = 0; i < integers.size(); i++)
+      {
+         YoInteger yoInteger = integers.get(i);
+         string += yoInteger.getFullNameString() + ": " + yoInteger.getValue() + ", ";
+      }
+      for (int i = 0; i < booleans.size(); i++)
+      {
+         YoBoolean yoBoolean = booleans.get(i);
+         string += yoBoolean.getFullNameString() + ": " + yoBoolean.getValue() + ", ";
+      }
+
+      return string;
+   }
+
+   public void loadValues(String parametersAsString)
+   {
+      parametersAsString = parametersAsString.replace(",", "");
+      Scanner scanner = new Scanner(parametersAsString);
+      for (int i = 0; i < doubles.size(); i++)
+         doubles.get(i).set(SaveableModuleTools.readNextDouble(scanner));
+      for (int i = 0; i < integers.size(); i++)
+         integers.get(i).set(SaveableModuleTools.readNextInt(scanner));
+      for (int i = 0; i < booleans.size(); i++)
+         booleans.get(i).set(SaveableModuleTools.readNextBoolean(scanner));
+      scanner.close();
+   }
+}
