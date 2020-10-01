2ed585a353e5a29b7a50a014228bcbb8ef708f90
Stephen McCrory <smccrory@ihmc.us>
2020-10-01 12:13:23 -0500

    Adding ContactStateChangeMessage

--- ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/WholeBodyMultiContactTrajectoryMessage.java
+++ ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/WholeBodyMultiContactTrajectoryMessage.java
@@ -16,16 +16,34 @@ public class WholeBodyMultiContactTrajectoryMessage extends Packet<WholeBodyMult
             * Unique ID used to identify this message, should preferably be consecutively increasing.
             */
    public long sequence_id_;
+   /**
+            * Duration of trajectory in seconds
+            */
    public double trajectory_duration_;
+   /**
+            * Target joint angles of the trajectory
+            */
    public us.ihmc.idl.IDLSequence.Double  joint_angles_;
+   /**
+            * Terminal pelvis pose in world frame
+            */
    public us.ihmc.euclid.geometry.Pose3D pelvis_pose_;
+   /**
+            * Hash of joint array
+            */
    public int joint_name_hash_;
+   /**
+            * Contact state changes associated with this trajectory
+            */
+   public us.ihmc.idl.IDLSequence.Object<controller_msgs.msg.dds.ContactStateChangeMessage>  contact_state_changes_;
 
    public WholeBodyMultiContactTrajectoryMessage()
    {
       joint_angles_ = new us.ihmc.idl.IDLSequence.Double (50, "type_6");
 
       pelvis_pose_ = new us.ihmc.euclid.geometry.Pose3D();
+      contact_state_changes_ = new us.ihmc.idl.IDLSequence.Object<controller_msgs.msg.dds.ContactStateChangeMessage> (10, new controller_msgs.msg.dds.ContactStateChangeMessagePubSubType());
+
    }
 
    public WholeBodyMultiContactTrajectoryMessage(WholeBodyMultiContactTrajectoryMessage other)
@@ -44,6 +62,7 @@ public class WholeBodyMultiContactTrajectoryMessage extends Packet<WholeBodyMult
       geometry_msgs.msg.dds.PosePubSubType.staticCopy(other.pelvis_pose_, pelvis_pose_);
       joint_name_hash_ = other.joint_name_hash_;
 
+      contact_state_changes_.set(other.contact_state_changes_);
    }
 
    /**
@@ -61,34 +80,61 @@ public class WholeBodyMultiContactTrajectoryMessage extends Packet<WholeBodyMult
       return sequence_id_;
    }
 
+   /**
+            * Duration of trajectory in seconds
+            */
    public void setTrajectoryDuration(double trajectory_duration)
    {
       trajectory_duration_ = trajectory_duration;
    }
+   /**
+            * Duration of trajectory in seconds
+            */
    public double getTrajectoryDuration()
    {
       return trajectory_duration_;
    }
 
 
+   /**
+            * Target joint angles of the trajectory
+            */
    public us.ihmc.idl.IDLSequence.Double  getJointAngles()
    {
       return joint_angles_;
    }
 
 
+   /**
+            * Terminal pelvis pose in world frame
+            */
    public us.ihmc.euclid.geometry.Pose3D getPelvisPose()
    {
       return pelvis_pose_;
    }
 
+   /**
+            * Hash of joint array
+            */
    public void setJointNameHash(int joint_name_hash)
    {
       joint_name_hash_ = joint_name_hash;
    }
+   /**
+            * Hash of joint array
+            */
    public int getJointNameHash()
    {
       return joint_name_hash_;
+   }
+
+
+   /**
+            * Contact state changes associated with this trajectory
+            */
+   public us.ihmc.idl.IDLSequence.Object<controller_msgs.msg.dds.ContactStateChangeMessage>  getContactStateChanges()
+   {
+      return contact_state_changes_;
    }
 
 
@@ -118,6 +164,13 @@ public class WholeBodyMultiContactTrajectoryMessage extends Packet<WholeBodyMult
       if (!this.pelvis_pose_.epsilonEquals(other.pelvis_pose_, epsilon)) return false;
       if (!us.ihmc.idl.IDLTools.epsilonEqualsPrimitive(this.joint_name_hash_, other.joint_name_hash_, epsilon)) return false;
 
+      if (this.contact_state_changes_.size() != other.contact_state_changes_.size()) { return false; }
+      else
+      {
+         for (int i = 0; i < this.contact_state_changes_.size(); i++)
+         {  if (!this.contact_state_changes_.get(i).epsilonEquals(other.contact_state_changes_.get(i), epsilon)) return false; }
+      }
+
 
       return true;
    }
@@ -139,6 +192,7 @@ public class WholeBodyMultiContactTrajectoryMessage extends Packet<WholeBodyMult
       if (!this.pelvis_pose_.equals(otherMyClass.pelvis_pose_)) return false;
       if(this.joint_name_hash_ != otherMyClass.joint_name_hash_) return false;
 
+      if (!this.contact_state_changes_.equals(otherMyClass.contact_state_changes_)) return false;
 
       return true;
    }
@@ -158,7 +212,9 @@ public class WholeBodyMultiContactTrajectoryMessage extends Packet<WholeBodyMult
       builder.append("pelvis_pose=");
       builder.append(this.pelvis_pose_);      builder.append(", ");
       builder.append("joint_name_hash=");
-      builder.append(this.joint_name_hash_);
+      builder.append(this.joint_name_hash_);      builder.append(", ");
+      builder.append("contact_state_changes=");
+      builder.append(this.contact_state_changes_);
       builder.append("}");
       return builder.toString();
    }
