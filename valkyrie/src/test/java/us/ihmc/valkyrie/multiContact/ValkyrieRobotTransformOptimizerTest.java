c8fe06cfbf6e25c796268984b5a563ea662462f0
Sylvain Bertrand <sbertrand@ihmc.us>
2020-10-02 17:04:48 -0500

    Got first draft of robot configuration matcher.

---
+++ valkyrie/src/test/java/us/ihmc/valkyrie/multiContact/ValkyrieRobotTransformOptimizerTest.java
@@ -0,0 +1,66 @@
+package us.ihmc.valkyrie.multiContact;
+
+import java.util.Map.Entry;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.multiContact.HumanoidRobotTransformOptimizerTest;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
+import us.ihmc.valkyrie.ValkyrieInitialSetupFactories;
+import us.ihmc.valkyrie.ValkyrieMutableInitialSetup;
+import us.ihmc.valkyrie.ValkyrieRobotModel;
+import us.ihmc.valkyrie.configuration.ValkyrieRobotVersion;
+
+public class ValkyrieRobotTransformOptimizerTest extends HumanoidRobotTransformOptimizerTest
+{
+   @Override
+   public DRCRobotModel createNewRobotModel()
+   {
+      return new ValkyrieRobotModel(RobotTarget.SCS, ValkyrieRobotVersion.FINGERLESS);
+   }
+
+   @Test
+   public void testOnlyRootJointDiffer()
+   {
+      Random random = new Random(34656);
+
+      DRCRobotModel robotModel = createNewRobotModel();
+      ValkyrieMutableInitialSetup initialSetupA = ValkyrieInitialSetupFactories.newCrawl1(robotModel.getJointMap());
+
+      ValkyrieMutableInitialSetup initialSetupB = ValkyrieInitialSetupFactories.newCrawl1(robotModel.getJointMap());
+      initialSetupB.rootJointOrientation.preMultiply(EuclidCoreRandomTools.nextQuaternion(random));
+      initialSetupB.rootJointPosition.add(EuclidCoreRandomTools.nextPoint3D(random));
+
+      runTest(initialSetupA, initialSetupB);
+   }
+
+   @Test
+   public void testWithNoise()
+   {
+      Random random = new Random(34656);
+
+      DRCRobotModel robotModel = createNewRobotModel();
+      ValkyrieMutableInitialSetup initialSetupA = ValkyrieInitialSetupFactories.newCrawl1(robotModel.getJointMap());
+
+      ValkyrieMutableInitialSetup initialSetupB = ValkyrieInitialSetupFactories.newCrawl1(robotModel.getJointMap());
+      initialSetupB.rootJointOrientation.preMultiply(EuclidCoreRandomTools.nextQuaternion(random));
+      initialSetupB.rootJointPosition.add(EuclidCoreRandomTools.nextPoint3D(random));
+
+      double errorNorm = 0.0;
+
+      for (Entry<String, Double> entry : initialSetupB.jointPositions.entrySet())
+      {
+         double error = 0.05 * (0.5 - random.nextDouble());
+         errorNorm += error * error;
+         entry.setValue(entry.getValue() + error);
+      }
+
+      System.out.println("Error norm: " + Math.sqrt(errorNorm));
+
+      runTest(initialSetupA, initialSetupB);
+
+   }
+}
