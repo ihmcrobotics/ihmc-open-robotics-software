2f1014d2041265451c257d00d780395d233331db
Stephen McCrory <smccrory@ihmc.us>
2020-10-05 13:31:37 -0500

    Fixing bug with logging heuristic cost

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/FootstepPlannerHeuristicCalculator.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/FootstepPlannerHeuristicCalculator.java
@@ -26,7 +26,6 @@ public class FootstepPlannerHeuristicCalculator
    private final WaypointDefinedBodyPathPlanHolder bodyPathPlanHolder;
    private double desiredHeading;
 
-   private final YoDouble heuristicCost = new YoDouble("heuristicCost", registry);
    private final FramePose3D midFootPose = new FramePose3D();
    private final Point2D midFootPoint = new Point2D();
    private final Pose3D projectionPose = new Pose3D();
@@ -93,12 +92,6 @@ public class FootstepPlannerHeuristicCalculator
          finalTurnDistance = Math.abs(AngleTools.computeAngleDifferenceMinusPiToPi(pathHeading, goalPose.getYaw())) * 0.5 * Math.PI * parameters.getIdealFootstepWidth();
      }
 
-      heuristicCost.set(parameters.getAStarHeuristicsWeight().getValue() * (initialTurnDistance + walkDistance + finalTurnDistance));
-      return heuristicCost.getValue();
-   }
-
-   public void resetLoggedVariables()
-   {
-      heuristicCost.set(Double.NaN);
+      return parameters.getAStarHeuristicsWeight().getValue() * (initialTurnDistance + walkDistance + finalTurnDistance);
    }
 }
