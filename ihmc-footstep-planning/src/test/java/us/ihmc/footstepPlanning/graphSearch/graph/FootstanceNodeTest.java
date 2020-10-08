57486865dbf191ba4abe198283036c0299558ac7
Stephen McCrory <smccrory@ihmc.us>
2020-10-08 09:55:42 -0500

    Adding FootstanceNode, which is composed of two FootstepNodes

---
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/graphSearch/graph/FootstanceNodeTest.java
@@ -0,0 +1,58 @@
+package us.ihmc.footstepPlanning.graphSearch.graph;
+
+import org.junit.jupiter.api.Test;
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
+import us.ihmc.robotics.robotSide.RobotSide;
+
+import java.util.Random;
+
+import static us.ihmc.robotics.Assert.*;
+
+public class FootstanceNodeTest
+{
+   @Test
+   public void testEqualsAndHashcode()
+   {
+      Random random = new Random(3823L);
+      int numTrials = 100;
+      FootstanceNode nodeA, nodeB;
+
+      for (int i = 0; i < numTrials; i++)
+      {
+         RobotSide stanceSide = RobotSide.generateRandomRobotSide(random);
+         double xStance = EuclidCoreRandomTools.nextDouble(random, 1.0);
+         double yStance = EuclidCoreRandomTools.nextDouble(random, 1.0);
+         double yawStance = EuclidCoreRandomTools.nextDouble(random, 1.0);
+         double xSwing = EuclidCoreRandomTools.nextDouble(random, 1.0);
+         double ySwing = EuclidCoreRandomTools.nextDouble(random, 1.0);
+         double yawSwing = EuclidCoreRandomTools.nextDouble(random, 1.0);
+
+         nodeA = new FootstanceNode(new FootstepNode(xStance, yStance, yawStance, stanceSide), new FootstepNode(xSwing, ySwing, yawSwing, stanceSide.getOppositeSide()));
+         nodeB = new FootstanceNode(new FootstepNode(xStance, yStance, yawStance, stanceSide), new FootstepNode(xSwing, ySwing, yawSwing, stanceSide.getOppositeSide()));
+
+         assertTrue(nodeA.equals(nodeB));
+         assertTrue(nodeA.hashCode() == nodeB.hashCode());
+
+         nodeB = new FootstanceNode(new FootstepNode(xStance, yStance, yawStance, stanceSide), new FootstepNode(xSwing + 0.5, ySwing, yawSwing, stanceSide.getOppositeSide()));
+         assertFalse(nodeA.equals(nodeB));
+         nodeB = new FootstanceNode(new FootstepNode(xStance, yStance, yawStance, stanceSide), new FootstepNode(xSwing, ySwing + 0.5, yawSwing, stanceSide.getOppositeSide()));
+         assertFalse(nodeA.equals(nodeB));
+         nodeB = new FootstanceNode(new FootstepNode(xStance, yStance, yawStance, stanceSide), new FootstepNode(xSwing, ySwing, yawSwing + 0.5, stanceSide.getOppositeSide()));
+         assertFalse(nodeA.equals(nodeB));
+
+         // assert exception thrown
+         try
+         {
+            nodeB = new FootstanceNode(new FootstepNode(xStance, yStance, yawStance, stanceSide), new FootstepNode(xSwing, ySwing, yawSwing + 0.5, stanceSide));
+            fail();
+         }
+         catch (Exception e)
+         {
+         }
+
+         nodeB = new FootstanceNode(nodeA.getSwingNode(), nodeA.getStanceNode());
+         assertFalse(nodeA.equals(nodeB));
+      }
+   }
+
+}
