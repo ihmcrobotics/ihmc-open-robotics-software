575dc343ea3a576300397601171e2e0afec65f90
BhavyanshM <bhavyanshmishra@gmail.com>
2020-10-13 11:53:33 -0500

    Add the line-planerregion associator updates

--- ihmc-perception/src/main/java/us/ihmc/ihmcPerception/lineSegmentDetector/LineSegmentToPlanarRegionAssociator.java
+++ ihmc-perception/src/main/java/us/ihmc/ihmcPerception/lineSegmentDetector/LineSegmentToPlanarRegionAssociator.java
@@ -1,11 +1,9 @@
 package us.ihmc.ihmcPerception.lineSegmentDetector;
 
 import boofcv.struct.calib.CameraPinholeBrown;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
+import us.ihmc.euclid.geometry.Line2D;
 import us.ihmc.euclid.geometry.LineSegment2D;
 import us.ihmc.euclid.transform.QuaternionBasedTransform;
 import us.ihmc.euclid.transform.RigidBodyTransform;
@@ -16,7 +14,9 @@ import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class LineSegmentToPlanarRegionAssociator {
 
@@ -58,24 +58,37 @@ public class LineSegmentToPlanarRegionAssociator {
                 pointList.add(new Point(px, py));
             }
         }
+        regionMidPoint.scale(1/(double)regionSize);
         return pointList;
     }
 
-    public void drawProjectedRegion(Mat img, ArrayList<Point> pointList, Point2D regionMidPoint, int id){
+    public void drawPolygonOnImage(Mat img, ArrayList<Point> pointList, Point2D regionMidPoint, int id){
         if (pointList.size() > 2) {
 
             MatOfPoint points = new MatOfPoint();
             points.fromList(pointList);
+
+            MatOfInt hull = new MatOfInt();
+            Imgproc.convexHull(points, hull);
+
+            Point[] hullPoints = new Point[hull.rows()];
+
+            List<Integer> hullContourIdxList = hull.toList();
+            Point[] polygonArray = points.toArray();
+            for (int i = 0; i < hullContourIdxList.size(); i++) {
+                hullPoints[i] = polygonArray[hullContourIdxList.get(i)];
+            }
+
+            MatOfPoint convexPoly = new MatOfPoint(hullPoints);
             List<MatOfPoint> ppt = new ArrayList<>();
-            ppt.add(points);
-            regionMidPoint.scale(1 / (float) pointList.size());
+            ppt.add(convexPoly);
 
             Imgproc.fillPoly(img,ppt,new Scalar(id*123 % 255, id*321 % 255, id*135 % 255) );
             Imgproc.circle(img,new Point(regionMidPoint.getX(), regionMidPoint.getY()), 3, new Scalar(255,140,255),-1);
         }
     }
 
-    public void drawLineRegionAssociation(Mat img, ArrayList<Point> pointList, Point2D regionMidPoint){
+    public void drawLineRegionAssociation(Mat img, ArrayList<Point> pointList, Point2D regionMidPoint, int id){
         if (pointList.size() > 2) {
 
             if (regionMidPoint.getX() < camIntrinsics.getWidth() && regionMidPoint.getY() < camIntrinsics.getHeight() && regionMidPoint.getX() >= 0 && regionMidPoint.getY() >= 0) {
@@ -84,48 +97,79 @@ public class LineSegmentToPlanarRegionAssociator {
                 //         new Scalar(100 + region.getRegionId()*123 % 155, 100 + region.getRegionId()*321 % 155, 100 + region.getRegionId()*135 % 155),
                 //         new Rect(), new Scalar(4,4,4), new Scalar(4,4,4), 4);
 
-                ArrayList<Point2D> regionSegment = getSegmentFromLines(curLines, regionMidPoint);
+                ArrayList<Point> regionSegment = getSegmentFromLines(curLines, regionMidPoint);
 
-                for (Point2D segment : regionSegment) {
-                    Imgproc.line(img, new Point(segment.getX(), segment.getY()), new Point(regionMidPoint.getX(), regionMidPoint.getY()),
-                            new Scalar(255, 100, 10), 2);
-                    // Imgproc.line(img, new Point(segment.getSecondEndpointX(), segment.getSecondEndpointY()), new Point(regionMidPoint.getX(), regionMidPoint.getY()),
-                    //         new Scalar(255, 100, 10), 2);
-                    Imgproc.circle(img, new Point(segment.getX(), segment.getY()), 8, new Scalar(255, 0, 255), -1);
-                }
+                // for (Point2D segment : regionSegment) {
+                //     Imgproc.line(img, new Point(segment.getX(), segment.getY()), new Point(regionMidPoint.getX(), regionMidPoint.getY()),
+                //             new Scalar(255, 100, 10), 2);
+                //     // Imgproc.line(img, new Point(segment.getSecondEndpointX(), segment.getSecondEndpointY()), new Point(regionMidPoint.getX(), regionMidPoint.getY()),
+                //     //         new Scalar(255, 100, 10), 2);
+                //     Imgproc.circle(img, new Point(segment.getX(), segment.getY()), 8, new Scalar(255, 0, 255), -1);
+                // }
+                drawPolygonOnImage(img, regionSegment, regionMidPoint, id);
             }
             Imgproc.circle(img,new Point(regionMidPoint.getX(), regionMidPoint.getY()), 3, new Scalar(255,140,255),-1);
         }
     }
 
-    public ArrayList<Point2D> getSegmentFromLines(Mat curLines, Point2D centroid) {
-        ArrayList<Point2D> segments = new ArrayList<>();
+    public int compare(LineSegment2D a, LineSegment2D b, Point2D centroid){
+        double delta = a.distance(centroid) - b.distance(centroid);
+        if(delta > 0.00001) return 1;
+        if(delta < -0.00001) return -1;
+        return 0;
+    }
+
+    public ArrayList<Point> getSegmentFromLines(Mat curLines, Point2D centroid) {
+        ArrayList<Point> segments = new ArrayList<>();
+        PriorityQueue<LineSegment2D> queue = new PriorityQueue<>(10, (a,b) -> compare(a,b,centroid));
         for (int i = 0; i < curLines.rows(); i++) {
             double[] line = curLines.get(i, 0);
-            Point2D a = new Point2D(line[0], line[1]);
-            Point2D b = new Point2D(line[2], line[3]);
-            Point2D m = new Point2D();
-            m.add(a, b);
-            m.scale(0.5);
-            Point2D p = new Point2D();
-            p.sub(m, centroid);
-            double dist = p.distanceFromOriginSquared();
-            if (dist < 8000) {
-                LineSegment2D lineSeg = new LineSegment2D(a.getX(), a.getY(), b.getX(), b.getY());
-                Point2D proj = LineTools.getProjection(lineSeg, centroid);
-                segments.add(proj);
+            LineSegment2D ls = new LineSegment2D(line[0], line[1], line[2], line[3]);
+            if(ls.distanceSquared(centroid) < 8000 && ls.length() > 30){
+                queue.add(ls);
             }
         }
+
+        System.out.println("Queue Size:" + queue.size());
+
+        ArrayList<Line2D> polygon = new ArrayList<Line2D>();
+
+        for(LineSegment2D ls : queue){
+            System.out.println("Distance:" + ls.distance(centroid));
+
+            segments.add(new Point(ls.getFirstEndpointX(), ls.getFirstEndpointY()));
+            segments.add(new Point(ls.getSecondEndpointX(), ls.getSecondEndpointY()));
+
+            // Point2D closest = (Point2D) ls.orthogonalProjectionCopy(centroid);
+            // boolean present = false;
+            // ArrayList<Point2D> projections = new ArrayList<>();
+            // Line2D line = new Line2D(ls);
+            // Point2D proj = (Point2D) line.orthogonalProjectionCopy(centroid);
+            // for(Point2D p : projections){
+            //     if(proj.distanceSquared(p) < 10){
+            //         present = true;
+            //     }
+            // }
+            // if(!present){
+            //     projections.add(proj);
+            //     polygon.add(line);
+            // }
+            // if(polygon.size() >= 4)break;
+        }
+
+        // if(polygon.size() >= 4){
+        //     for(int i = 0; i<polygon.size()-1; i++){
+        //         System.out.println( (i+1) % polygon.size());
+        //         Point2D corner = (Point2D) polygon.get(i).intersectionWith(polygon.get( i+1 ));
+        //         segments.add(corner);
+        //     }
+        // }
         return segments;
     }
 
-    public void setCamIntrinsics(CameraPinholeBrown camIntrinsics) {
-        this.camIntrinsics = camIntrinsics;
-    }
+    public void setCamIntrinsics(CameraPinholeBrown camIntrinsics) { this.camIntrinsics = camIntrinsics; }
 
-    public void setCameraOrientation(Quaternion cameraOrientation) {
-        this.cameraOrientation = cameraOrientation;
-    }
+    public void setCameraOrientation(Quaternion cameraOrientation) { this.cameraOrientation = cameraOrientation;  }
 
     public void setCameraPosition(Point3D cameraPosition){ this.cameraPosition = cameraPosition; }
 
---
+++ ihmc-perception/src/main/java/us/ihmc/ihmcPerception/lineSegmentDetector/RealSenseL515DepthImageSubscriber.java
@@ -0,0 +1,66 @@
+package us.ihmc.ihmcPerception.lineSegmentDetector;
+
+import controller_msgs.msg.dds.VideoPacket;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgproc.Imgproc;
+import sensor_msgs.Image;
+import us.ihmc.communication.IHMCROS2Publisher;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.ihmcPerception.camera.RosCameraInfoSubscriber;
+import us.ihmc.ros2.Ros2Node;
+import us.ihmc.utilities.ros.RosMainNode;
+import us.ihmc.utilities.ros.subscriber.AbstractRosTopicSubscriber;
+
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
+
+public class RealSenseL515DepthImageSubscriber extends AbstractRosTopicSubscriber<Image> {
+
+    private final ColorModel colorModel;
+
+    private final String l515CompressedDepthTopic = "/camera/depth/image_rect_raw";
+    private String l515CameraInfoTopic = "/camera/depth/camera_info";
+
+    private BufferedImage latestBufferedDepth;
+
+    private final IHMCROS2Publisher<VideoPacket> depthPublisher;
+    private final RosCameraInfoSubscriber depthImageInfoSubscriber;
+
+    public RealSenseL515DepthImageSubscriber(RosMainNode rosNode, Ros2Node ros2Node) {
+        super(sensor_msgs.Image._TYPE);
+        depthPublisher = ROS2Tools.createPublisherTypeNamed(ros2Node, VideoPacket.class, ROS2Tools.IHMC_ROOT);
+        depthImageInfoSubscriber = new RosCameraInfoSubscriber(l515CameraInfoTopic);
+        rosNode.attachSubscriber(l515CompressedDepthTopic, this);
+        rosNode.attachSubscriber(l515CameraInfoTopic, depthImageInfoSubscriber);
+        ColorSpace colorSpace = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+        this.colorModel = new ComponentColorModel(colorSpace, false, false, Transparency.OPAQUE, DataBuffer.TYPE_SHORT);
+    }
+
+    public void onNewMessage(sensor_msgs.Image message) {
+        long timeStamp = message.getHeader().getStamp().totalNsecs();
+        System.out.println(message.getHeight() * message.getWidth() + " " + message.getData().array().length);
+
+        byte[] payload = message.getData().array();
+        Mat mat = new Mat(message.getHeight(), message.getWidth(), CvType.CV_8U);
+        mat.put(0, 0, payload);
+
+        Mat output = new Mat(mat.height(), mat.width(), CvType.CV_8U);
+        mat.convertTo(output, CvType.CV_8U);
+
+        Imgproc.resize(output, output, new Size(1200, 900));
+        HighGui.namedWindow("LineEstimator", HighGui.WINDOW_AUTOSIZE);
+        HighGui.resizeWindow("LineEstimator", 1200, 900);
+        HighGui.imshow("LineEstimator", output);
+        int code = HighGui.waitKey(32);
+
+        // imageReceived(timeStamp, RosTools.bufferedImageFromRosMessageRaw(colorModel, message));
+    }
+
+}
