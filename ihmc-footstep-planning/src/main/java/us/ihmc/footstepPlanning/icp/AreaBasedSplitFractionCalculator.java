ef1b7cb49ab2074dbfa9dc2fae3a9071b190b076
Robert <rgriffin@ihmc.us>
2020-10-13 23:26:32 -0500

    got the area based split fraction calculator converted over

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/icp/AreaBasedSplitFractionCalculator.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/icp/AreaBasedSplitFractionCalculator.java
@@ -1,15 +1,12 @@
 package us.ihmc.footstepPlanning.icp;
 
-import us.ihmc.commonWalkingControlModules.capturePoint.smoothCMPBasedICPPlanner.CoPGeneration.SplitFractionTools;
-import us.ihmc.commons.InterpolationTools;
-import us.ihmc.commons.MathTools;
+import us.ihmc.commonWalkingControlModules.capturePoint.smoothCMPBasedICPPlanner.CoPGeneration.SplitFractionFromAreaCalculator;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.footstepPlanning.FootstepPlan;
 import us.ihmc.footstepPlanning.FootstepPlannerRequest;
 import us.ihmc.footstepPlanning.PlannedFootstep;
-import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
@@ -21,13 +18,13 @@ import us.ihmc.robotics.robotSide.SideDependentList;
  * is some fraction of the transfer duration determined by the transfer split fraction {@link PlannedFootstep#setTransferSplitFraction}.
  *
  * <p>
- *    This module defines several parameters that allow the weight distribution and split fraction to be modified based on the comparative area and width of the
- *    leading and trailing footholds. That is, we can say that if the upcoming foothold has more area, we should shift our weight to be more on it, and do so
- *    quickly. The same is said for the width - if it has more width, we should shift out weight more and quickly.
+ * This module defines several parameters that allow the weight distribution and split fraction to be modified based on the comparative area and width of the
+ * leading and trailing footholds. That is, we can say that if the upcoming foothold has more area, we should shift our weight to be more on it, and do so
+ * quickly. The same is said for the width - if it has more width, we should shift out weight more and quickly.
  * </p>
  * <p>
- *    This is done by defining the desired weight distribution and split fraction if the upcoming foothold has ALL the area and width. That is, if the trailing
- *    foot has no support area, where the midpoint CoP should be located, and if the trailing foot is purely a line, where the midpoint CoP should be.
+ * This is done by defining the desired weight distribution and split fraction if the upcoming foothold has ALL the area and width. That is, if the trailing
+ * foot has no support area, where the midpoint CoP should be located, and if the trailing foot is purely a line, where the midpoint CoP should be.
  * </p>
  */
 public class AreaBasedSplitFractionCalculator
@@ -37,11 +34,14 @@ public class AreaBasedSplitFractionCalculator
    private final SplitFractionCalculatorParametersReadOnly parameters;
    private final SideDependentList<ConvexPolygon2D> footPolygons;
 
-   public AreaBasedSplitFractionCalculator(SplitFractionCalculatorParametersReadOnly parameters,
-                                           SideDependentList<ConvexPolygon2D> footPolygons)
+   private final SplitFractionFromAreaCalculator calculator;
+
+   public AreaBasedSplitFractionCalculator(SplitFractionCalculatorParametersReadOnly parameters, SideDependentList<ConvexPolygon2D> footPolygons)
    {
       this.parameters = parameters;
       this.footPolygons = footPolygons;
+
+      calculator = new SplitFractionFromAreaCalculator(parameters, footPolygons);
    }
 
    public void computeSplitFractions(FootstepPlannerRequest request, FootstepPlan footstepPlan)
@@ -53,6 +53,39 @@ public class AreaBasedSplitFractionCalculator
                                      SideDependentList<? extends Pose3DReadOnly> startFootPoses,
                                      SideDependentList<ConvexPolygon2D> startFootholds)
    {
+      calculator.setNumberOfStepsProvider(footstepPlan::getNumberOfSteps);
+
+      calculator.setFinalTransferSplitFractionProvider(footstepPlan::getFinalTransferSplitFraction);
+      calculator.setFinalTransferWeightDistributionProvider(footstepPlan::getFinalTransferWeightDistribution);
+
+      calculator.setTransferSplitFractionProvider((i) -> footstepPlan.getFootstep(i).getTransferSplitFraction());
+      calculator.setTransferWeightDistributionProvider((i) -> footstepPlan.getFootstep(i).getTransferWeightDistribution());
+
+      calculator.setFinalTransferSplitFractionConsumer(footstepPlan::setFinalTransferSplitFraction);
+      calculator.setFinalTransferWeightDistributionConsumer(footstepPlan::setFinalTransferWeightDistribution);
+
+      calculator.setTransferWeightDistributionConsumer((i) -> (d) -> footstepPlan.getFootstep(i).setTransferWeightDistribution(d));
+      calculator.setTransferSplitFractionConsumer((i) -> (d) -> footstepPlan.getFootstep(i).setTransferSplitFraction(d));
+
+      calculator.setFirstSupportPolygonProvider(() ->
+                                                {
+                                                   RobotSide stanceSide = footstepPlan.getFootstep(0).getRobotSide().getOppositeSide();
+                                                   ConvexPolygon2D initialStanceFoothold = startFootholds.get(stanceSide);
+                                                   if (initialStanceFoothold.isEmpty())
+                                                   {
+                                                      return footPolygons.get(stanceSide).getVertexBufferView();
+                                                   }
+                                                   else
+                                                   {
+                                                      return initialStanceFoothold.getVertexBufferView();
+                                                   }
+                                                });
+      calculator.setStepSideProvider((i) -> footstepPlan.getFootstep(i).getRobotSide());
+      calculator.setStepPolygonGetter((i) -> footstepPlan.getFootstep(i).getFoothold().getVertexBufferView());
+
+      calculator.computeSplitFractionsFromArea();
+
+      /*
       if (footstepPlan.getNumberOfSteps() == 0)
       {
          return;
@@ -118,16 +151,20 @@ public class AreaBasedSplitFractionCalculator
          if (MathTools.epsilonEquals(percentAreaOnCurrentFoot, 0.5, 1.0e-2) && MathTools.epsilonEquals(percentWidthOnCurrentFoot, 0.5, 2.0e-2))
             continue;
 
-         double transferWeightDistributionFromArea = InterpolationTools.linearInterpolate(defaultWeightDistribution, parameters.getFractionLoadIfFootHasFullSupport(),
+         double transferWeightDistributionFromArea = InterpolationTools.linearInterpolate(defaultWeightDistribution,
+                                                                                          parameters.getFractionLoadIfFootHasFullSupport(),
                                                                                           2.0 * percentAreaOnCurrentFoot - 1.0);
-         double transferWeightDistributionFromWidth = InterpolationTools.linearInterpolate(defaultWeightDistribution, parameters.getFractionLoadIfOtherFootHasNoWidth(),
+         double transferWeightDistributionFromWidth = InterpolationTools.linearInterpolate(defaultWeightDistribution,
+                                                                                           parameters.getFractionLoadIfOtherFootHasNoWidth(),
                                                                                            2.0 * percentWidthOnCurrentFoot - 1.0);
 
          // lower means it spends more time shifting to the center, higher means it spends less time shifting to the center
          // e.g., if we set the fraction to 0 and the trailing foot has no area, the split fraction should be 1 because we spend no time on the first segment
-         double transferSplitFractionFromArea = InterpolationTools.linearInterpolate(defaultTransferSplitFraction, 1.0 - parameters.getFractionTimeOnFootIfFootHasFullSupport(),
+         double transferSplitFractionFromArea = InterpolationTools.linearInterpolate(defaultTransferSplitFraction,
+                                                                                     1.0 - parameters.getFractionTimeOnFootIfFootHasFullSupport(),
                                                                                      2.0 * percentAreaOnCurrentFoot - 1.0);
-         double transferSplitFractionFromWidth = InterpolationTools.linearInterpolate(defaultTransferSplitFraction, 1.0 - parameters.getFractionTimeOnFootIfOtherFootHasNoWidth(),
+         double transferSplitFractionFromWidth = InterpolationTools.linearInterpolate(defaultTransferSplitFraction,
+                                                                                      1.0 - parameters.getFractionTimeOnFootIfOtherFootHasNoWidth(),
                                                                                       2.0 * percentWidthOnCurrentFoot - 1.0);
 
          double transferWeightDistribution = 0.5 * (transferWeightDistributionFromArea + transferWeightDistributionFromWidth);
@@ -143,7 +180,9 @@ public class AreaBasedSplitFractionCalculator
             double currentWeightDistribution = footstepPlan.getFinalTransferWeightDistribution();
 
             double splitFractionToSet = SplitFractionTools.appendSplitFraction(transferSplitFraction, currentSplitFraction, defaultTransferSplitFraction);
-            double weightDistributionToSet = SplitFractionTools.appendWeightDistribution(transferWeightDistribution, currentWeightDistribution, defaultWeightDistribution);
+            double weightDistributionToSet = SplitFractionTools.appendWeightDistribution(transferWeightDistribution,
+                                                                                         currentWeightDistribution,
+                                                                                         defaultWeightDistribution);
 
             footstepPlan.setFinalTransferSplitFraction(splitFractionToSet);
             footstepPlan.setFinalTransferWeightDistribution(weightDistributionToSet);
@@ -156,11 +195,15 @@ public class AreaBasedSplitFractionCalculator
             double currentWeightDistribution = nextFootstep.getTransferWeightDistribution();
 
             double splitFractionToSet = SplitFractionTools.appendSplitFraction(transferSplitFraction, currentSplitFraction, defaultTransferSplitFraction);
-            double weightDistributionToSet = SplitFractionTools.appendWeightDistribution(transferWeightDistribution, currentWeightDistribution, defaultWeightDistribution);
+            double weightDistributionToSet = SplitFractionTools.appendWeightDistribution(transferWeightDistribution,
+                                                                                         currentWeightDistribution,
+                                                                                         defaultWeightDistribution);
 
             nextFootstep.setTransferSplitFraction(splitFractionToSet);
             nextFootstep.setTransferWeightDistribution(weightDistributionToSet);
          }
       }
+
+       */
    }
 }
