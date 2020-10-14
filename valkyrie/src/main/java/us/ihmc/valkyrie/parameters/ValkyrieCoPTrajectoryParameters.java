47e422b51c13345f9f26776b042c0725dd82c654
Robert <rgriffin@ihmc.us>
2020-10-14 16:51:55 -0500

    values were all default for val

--- valkyrie/src/main/java/us/ihmc/valkyrie/parameters/ValkyrieCoPTrajectoryParameters.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/parameters/ValkyrieCoPTrajectoryParameters.java
@@ -16,23 +16,4 @@ import us.ihmc.yoVariables.variable.YoInteger;
 
 public class ValkyrieCoPTrajectoryParameters extends CoPTrajectoryParameters
 {
-   public ValkyrieCoPTrajectoryParameters()
-   {
-      entryCMPLengthOffsetFactor.set(1.0 / 3.0);
-      ballCMPLengthOffsetFactor.set(1.0 / 8.0);
-      exitCMPLengthOffsetFactor.set(1.0 / 3.0);
-
-      entryCMPOffset.set(0.0, -0.005);
-      ballCMPOffset.set(0.0, 0.01);
-      exitCMPOffset.set(0.0, 0.025);
-
-      entryCMPMinX.set(-0.04);
-      entryCMPMaxX.set(0.03);
-
-      ballCMPMinX.set(0.0);
-      ballCMPMaxX.set(0.055);
-
-      exitCMPMinX.set(0.0);
-      exitCMPMaxX.set(0.08);
-   }
 }
