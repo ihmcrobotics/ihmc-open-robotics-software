d2b67f390281ff76cbc447a61c74e5cfdc5b4e74
Operator Computer <OperatorComputer@unknown>
2020-10-15 18:06:50 -0500

    Initial try at converting OpenCv to JavaCV.

--- ihmc-perception/src/main/java/us/ihmc/ihmcPerception/lineSegmentDetector/LineDetectionTools.java
+++ ihmc-perception/src/main/java/us/ihmc/ihmcPerception/lineSegmentDetector/LineDetectionTools.java
@@ -1,10 +1,18 @@
 package us.ihmc.ihmcPerception.lineSegmentDetector;
 
-import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.ximgproc.FastLineDetector;
-import org.opencv.ximgproc.Ximgproc;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacpp.*;
+import org.bytedeco.javacpp.indexer.*;
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_imgproc.*;
+import org.bytedeco.opencv.opencv_calib3d.*;
+import org.bytedeco.opencv.opencv_objdetect.*;
+import static org.bytedeco.opencv.global.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_calib3d.*;
+import static org.bytedeco.opencv.global.opencv_objdetect.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,18 +20,23 @@ import java.util.List;
 
 public class LineDetectionTools
 {
-   public static Mat getFLDLinesFromImage(Mat image)
+   public static Vec4iVector getFLDLinesFromImage(Mat image)
    {
-      Mat lines = new Mat();
+      Vec4iVector lines = new Vec4iVector();
       Mat gray = new Mat();
-      Imgproc.cvtColor(image, gray, Imgproc.COLOR_BGR2GRAY);
+      cvtColor(image, gray, COLOR_BGR2GRAY);
 
-      FastLineDetector fld = Ximgproc.createFastLineDetector(20, 1.41421356f, 50, 50, 3, true);
-      fld.detect(gray, lines);
+      Mat cannyEdges = new Mat();
+
+      cannyEdges = getCannyEdges(gray);
+
+      lines = getHoughLinesFromEdges(cannyEdges);
+
+
       return lines;
    }
 
-   public static Mat getLinesFromImage(Mat image)
+   public static Vec4iVector getLinesFromImage(Mat image)
    {
       Mat frame = preProcessImage(image);
       Mat edges = getCannyEdges(frame);
@@ -34,49 +47,55 @@ public class LineDetectionTools
    {
       Mat gray = new Mat();
       //        Imgproc.pyrDown(frame,frame);
-      Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
-      Imgproc.medianBlur(gray, gray, 5);
-      Imgproc.GaussianBlur(gray, gray, new Size(3, 3), 20);
+      cvtColor(frame, gray, COLOR_BGR2GRAY);
+      medianBlur(gray, gray, 5);
+      GaussianBlur(gray, gray, new Size(3, 3), 20);
       return gray;
    }
 
    public static Mat getCannyEdges(Mat gray)
    {
       Mat edges = new Mat();
-      Imgproc.Canny(gray, edges, 60, 60 * 3, 3, true);
+      Canny(gray, edges, 60, 60 * 3, 3, true);
       Mat cannyColor = new Mat();
-      Imgproc.cvtColor(edges, cannyColor, Imgproc.COLOR_GRAY2BGR);
+      cvtColor(edges, cannyColor, COLOR_GRAY2BGR);
       return edges;
    }
 
-   public static Mat getHoughLinesFromEdges(Mat edges)
+   public static Vec4iVector getHoughLinesFromEdges(Mat edges)
    {
-      Mat lines = new Mat();
-      Imgproc.HoughLinesP(edges, lines, 1, Math.PI / 180, 70, 30, 10);
+      Vec4iVector lines = new Vec4iVector();
+      HoughLinesP(edges, lines, 1, Math.PI / 180, 70, 30, 10);
       return lines;
    }
 
    public static void displayLineMatches(Mat prevImg, Mat curImg, Mat dispImage, ArrayList<LineMatch> correspLines)
    {
-      List<Mat> src = Arrays.asList(prevImg, curImg);
-      Core.hconcat(src, dispImage);
+      MatVector src = new MatVector(prevImg, curImg);
+      hconcat(src, dispImage);
       for (LineMatch lm : correspLines)
       {
-         Imgproc.line(dispImage,
-                      new Point(lm.prevLine[0], lm.prevLine[1]),
-                      new Point(lm.curLine[0] + prevImg.cols(), lm.curLine[1]),
-                      new Scalar(255, 50, 50),
-                      2);
-         Imgproc.line(dispImage,
-                      new Point(lm.prevLine[2], lm.prevLine[3]),
-                      new Point(lm.curLine[2] + prevImg.cols(), lm.curLine[3]),
-                      new Scalar(255, 50, 50),
-                      2);
+         line(dispImage,
+                      new Point((int)lm.prevLine[0], (int)lm.prevLine[1]),
+                      new Point((int)lm.curLine[0] + prevImg.cols(), (int)lm.curLine[1]),
+                      Scalar.YELLOW,
+                      2,LINE_8, 0);
+         line(dispImage,
+                      new Point((int)lm.prevLine[2], (int)lm.prevLine[3]),
+                      new Point((int)lm.curLine[2] + prevImg.cols(), (int)lm.curLine[3]),
+                      Scalar.YELLOW,
+                      2, LINE_8, 0);
       }
    }
 
    public static Mat getImage(String path)
    {
-      return Imgcodecs.imread(path);
+      return imread(path);
+   }
+
+   public static void main(String[] args)
+   {
+//      System.loadLibrary("opencv_java310");
+//      FastLineDetector fld = Ximgproc.createFastLineDetector(20, 1.41421356f, 50, 50, 3, true);
    }
 }
