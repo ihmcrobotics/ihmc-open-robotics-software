6d1874ff86027cb00503c82ef6137b7f305b99dc
BhavyanshM <bhavyanshmishra@gmail.com>
2020-10-15 14:55:03 -0500

    Enable Subscription to ROS2 Message Types for Video when running Line Detection on the AtlasLookAndStepBehaviorDemo

--- atlas/src/main/java/us/ihmc/atlas/AtlasLineSegmentEstimator.java
+++ atlas/src/main/java/us/ihmc/atlas/AtlasLineSegmentEstimator.java
@@ -15,8 +15,10 @@ import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.configuration.NetworkParameters;
 import us.ihmc.communication.packets.PlanarRegionMessageConverter;
 import us.ihmc.communication.producers.JPEGDecompressor;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
-import us.ihmc.humanoidBehaviors.lookAndStep.BehaviorTaskSuppressor;
+import us.ihmc.humanoidRobotics.communication.packets.HumanoidMessageTools;
 import us.ihmc.ihmcPerception.lineSegmentDetector.LineDetectionTools;
 import us.ihmc.ihmcPerception.lineSegmentDetector.LineMatch;
 import us.ihmc.ihmcPerception.lineSegmentDetector.LineSegmentToPlanarRegionAssociator;
@@ -25,6 +27,7 @@ import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.pubsub.DomainFactory;
 import us.ihmc.robotics.geometry.PlanarRegion;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.robotics.partNames.NeckJointName;
 import us.ihmc.robotics.referenceFrames.TransformReferenceFrame;
 import us.ihmc.ros2.ROS2Callback;
 import us.ihmc.ros2.ROS2Node;
@@ -47,62 +50,86 @@ import static us.ihmc.ihmcPerception.lineSegmentDetector.LineMatchingTools.match
 
 public class AtlasLineSegmentEstimator
 {
+
+   private boolean USE_ROS1_PERCEPTION_TOPICS = false;
+
    private final AtlasRobotModel robotModel;
    private boolean prevImgFilled = false;
    private int code = 0;
 
-   private ConcurrentLinkedDeque<CompressedImage> images = new ConcurrentLinkedDeque<>();
+   private ConcurrentLinkedDeque<CompressedImage> compressedImagesRos1 = new ConcurrentLinkedDeque<>();
+   private ConcurrentLinkedDeque<VideoPacket> videoPacketsRos2 = new ConcurrentLinkedDeque<>();
+
    private volatile CameraInfo cameraInfo = null;
    private volatile PlanarRegionsListMessage currentPlanarRegionsListMessage = null;
+   private volatile CameraPinholeBrown cameraIntrinsics;
 
    private Sphere sensorNode;
 
    private Mat currentImage;
    private Mat previousImage;
-   private Mat curLines;
+   private Mat currentLines;
    private Mat previousLines;
-   private ArrayList<LineMatch> correspLines;
+   private ArrayList<LineMatch> correspondingLines;
 
-   private CameraPinholeBrown cameraIntrinsics;
 
-   private LineSegmentToPlanarRegionAssociator lineRegionAssoc;
+   private LineSegmentToPlanarRegionAssociator lineRegionAssociator;
+
    // private JavaFXPlanarRegionsViewer planarRegionsViewer;
+
    private RosMainNode ros1Node;
    private ROS2Node ros2Node;
+
    private JPEGDecompressor jpegDecompressor = new JPEGDecompressor();
    private ScheduledExecutorService executorService
          = ExecutorServiceTools.newScheduledThreadPool(3, getClass(), ExecutorServiceTools.ExceptionHandling.CATCH_AND_REPORT);
+
    private TransformReferenceFrame realsenseSensorFrame;
+   private TransformReferenceFrame multisenseSensorFrame;
+
    private RemoteSyncedRobotModel syncedRobot;
 
    public AtlasLineSegmentEstimator()
    {
       // System.out.println(System.getProperty("java.library.path"));
-//      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
       // NativeLibraryLoader.loadLibrary("org.opencv", OpenCVTools.OPEN_CV_LIBRARY_NAME);
-      this.ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "video_viewer");
+      ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "video_viewer");
+      new ROS2Callback<>(ros2Node, ROS2Tools.REALSENSE_SLAM_REGIONS, this::planarRegionsListCallback);
 
       robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS);
-
-      URI masterURI = NetworkParameters.getROSURI();
-      LogTools.info("Connecting to ROS 1 master URI: {}", masterURI);
-      RosMainNode ros1Node = new RosMainNode(masterURI, "video_viewer", true);
-
-      new ROS2Callback<>(ros2Node, ROS2Tools.REALSENSE_SLAM_REGIONS, this::planarRegionsListCallback);
-//      new ROS2Callback<>(ros2Node, ROS2Tools.VIDEO, this::videoPacketCallback);
-
-      ros1Node.attachSubscriber(RosTools.D435_VIDEO, CompressedImage.class, message -> images.addLast(message));
-      ros1Node.attachSubscriber(RosTools.D435_CAMERA_INFO, CameraInfo.class, info -> cameraInfo = info);
-
       syncedRobot = new RemoteSyncedRobotModel(robotModel, ros2Node);
-      MovingReferenceFrame pelvisFrame = syncedRobot.getReferenceFrames().getPelvisFrame();
-      // TODO: Check transform to color camera
-      realsenseSensorFrame = new TransformReferenceFrame("Realsense", pelvisFrame, AtlasSensorInformation.transformPelvisToDepthCamera);
 
-      ros1Node.execute();
+      if (USE_ROS1_PERCEPTION_TOPICS)
+      {
+         URI masterURI = NetworkParameters.getROSURI();
+         LogTools.info("Connecting to ROS 1 master URI: {}", masterURI);
+         RosMainNode ros1Node = new RosMainNode(masterURI, "video_viewer", true);
+         ros1Node.attachSubscriber(RosTools.D435_VIDEO, CompressedImage.class, message -> compressedImagesRos1.addLast(message));
+         ros1Node.attachSubscriber(RosTools.D435_CAMERA_INFO, CameraInfo.class, info -> cameraInfo = info);
+         ros1Node.execute();
 
-      executorService.scheduleAtFixedRate(this::mainUpdate, 0, 32, TimeUnit.MILLISECONDS);
-      lineRegionAssoc = new LineSegmentToPlanarRegionAssociator(realsenseSensorFrame);
+         MovingReferenceFrame pelvisFrame = syncedRobot.getReferenceFrames().getPelvisFrame();
+         // TODO: Check transform to color camera
+         realsenseSensorFrame = new TransformReferenceFrame("Realsense", pelvisFrame, AtlasSensorInformation.transformPelvisToDepthCamera);
+         executorService.scheduleAtFixedRate(this::mainUpdateUsingROS1Topics, 0, 32, TimeUnit.MILLISECONDS);
+         lineRegionAssociator = new LineSegmentToPlanarRegionAssociator(realsenseSensorFrame);
+      }
+      else
+      {
+         new ROS2Callback<>(ros2Node, ROS2Tools.VIDEO, message ->
+         {
+            videoPacketsRos2.addLast(message);
+            cameraIntrinsics = HumanoidMessageTools.toIntrinsicParameters(message.intrinsic_parameters_);
+         });
+
+         MovingReferenceFrame neckFrame = syncedRobot.getReferenceFrames().getNeckFrame(NeckJointName.PROXIMAL_NECK_PITCH);
+//         multisenseSensorFrame = new TransformReferenceFrame("Multisense", neckFrame, AtlasSensorInformation);
+
+         executorService.scheduleAtFixedRate(this::mainUpdateUsingROS2Topics, 0, 32, TimeUnit.MILLISECONDS);
+         lineRegionAssociator = new LineSegmentToPlanarRegionAssociator(neckFrame);
+      }
+
    }
 
    public void setSensorNode(Sphere sensorNode)
@@ -110,72 +137,94 @@ public class AtlasLineSegmentEstimator
       this.sensorNode = sensorNode;
    }
 
-//   public void videoPacketCallback(VideoPacket message)
-//   {
-//      currentVideoPacket = message;
-//   }
-
    public void planarRegionsListCallback(PlanarRegionsListMessage message)
    {
-//      currentPlanarRegionsListMessageFilled = true;
       currentPlanarRegionsListMessage = message;
    }
 
-   public void mainUpdate()
+   public void mainUpdateUsingROS1Topics()
    {
       LogTools.info(StringTools.format("{} {} {} {}", currentPlanarRegionsListMessage != null,
                                        syncedRobot.getDataReceptionTimerSnapshot().isRunning(1.0),
                                        cameraInfo != null,
-                                       images.size() > 1));
-      if (currentPlanarRegionsListMessage != null
-          && syncedRobot.getDataReceptionTimerSnapshot().isRunning(1.0)
-          && cameraInfo != null
-          && images.size() > 1)
+                                       compressedImagesRos1.size() > 1));
+      if (currentPlanarRegionsListMessage != null && syncedRobot.getDataReceptionTimerSnapshot().isRunning(1.0) && cameraInfo != null
+          && compressedImagesRos1.size() > 1)
       {
-         while (images.size() > 2)
+         while (compressedImagesRos1.size() > 2)
          {
-            images.removeFirst();
+            compressedImagesRos1.removeFirst();
          }
 
-         CompressedImage latest = images.removeFirst();
-         CompressedImage previous = images.getFirst();
+         CompressedImage latest = compressedImagesRos1.removeFirst();
+         CompressedImage previous = compressedImagesRos1.getFirst();
 
          BufferedImage latestBufferedImage = RosTools.bufferedImageFromRosMessageJpeg(latest);
          BufferedImage previousBufferedImage = RosTools.bufferedImageFromRosMessageJpeg(previous);
 
-         currentImage = new Mat(latestBufferedImage.getHeight(), latestBufferedImage.getWidth(), CvType.CV_8UC3);
-         byte[] data = ((DataBufferByte) latestBufferedImage.getRaster().getDataBuffer()).getData();
-         currentImage.put(0, 0, data);
-
-         previousImage = new Mat(previousBufferedImage.getHeight(), previousBufferedImage.getWidth(), CvType.CV_8UC3);
-         byte[] previousData = ((DataBufferByte) previousBufferedImage.getRaster().getDataBuffer()).getData();
-         previousImage.put(0, 0, previousData);
-
-         previousLines = LineDetectionTools.getFLDLinesFromImage(previousImage);
          cameraIntrinsics = RosTools.cameraIntrisicsFromCameraInfo(cameraInfo);
 
-         syncedRobot.update();
-
-         processImages();
+         processBufferedImagesAndPlanarRegions(latestBufferedImage, previousBufferedImage);
       }
+   }
 
-      if (code == 81)
-         System.exit(0);
+   public void mainUpdateUsingROS2Topics()
+   {
+      LogTools.info(StringTools.format("{} {} {} {}", currentPlanarRegionsListMessage != null,
+                                       syncedRobot.getDataReceptionTimerSnapshot().isRunning(1.0),
+                                       cameraIntrinsics != null,
+                                       videoPacketsRos2.size() > 1));
+      if (currentPlanarRegionsListMessage != null && syncedRobot.getDataReceptionTimerSnapshot().isRunning(1.0) && cameraIntrinsics != null
+          && videoPacketsRos2.size() > 1)
+      {
+         while (videoPacketsRos2.size() > 2)
+         {
+            videoPacketsRos2.removeFirst();
+         }
+
+         VideoPacket latest = videoPacketsRos2.removeFirst();
+         VideoPacket previous = videoPacketsRos2.getFirst();
+
+         BufferedImage latestBufferedImage = jpegDecompressor.decompressJPEGDataToBufferedImage(latest.getData().toArray());
+         BufferedImage previousBufferedImage = jpegDecompressor.decompressJPEGDataToBufferedImage(previous.getData().toArray());
+
+         processBufferedImagesAndPlanarRegions(latestBufferedImage, previousBufferedImage);
+      }
+   }
+
+   private void processBufferedImagesAndPlanarRegions(BufferedImage latestBufferedImage, BufferedImage previousBufferedImage)
+   {
+      currentImage = new Mat(latestBufferedImage.getHeight(), latestBufferedImage.getWidth(), CvType.CV_8UC3);
+      byte[] data = ((DataBufferByte) latestBufferedImage.getRaster().getDataBuffer()).getData();
+      currentImage.put(0, 0, data);
+
+      previousImage = new Mat(previousBufferedImage.getHeight(), previousBufferedImage.getWidth(), CvType.CV_8UC3);
+      byte[] previousData = ((DataBufferByte) previousBufferedImage.getRaster().getDataBuffer()).getData();
+      previousImage.put(0, 0, previousData);
+
+      previousLines = LineDetectionTools.getFLDLinesFromImage(previousImage);
+
+
+      syncedRobot.update();
+
+      processImages();
    }
 
    public void processImages()
    {
       float startTime = System.nanoTime();
 
+      System.out.println("Processing Images");
+
       Mat dispImage = new Mat();
-      this.curLines = LineDetectionTools.getFLDLinesFromImage(this.currentImage);
-      lineRegionAssoc.setCurLines(this.curLines);
+      this.currentLines = LineDetectionTools.getFLDLinesFromImage(this.currentImage);
+      lineRegionAssociator.setCurLines(this.currentLines);
 
-      this.correspLines = matchFLDLines(this.previousLines, this.curLines);
+      this.correspondingLines = matchFLDLines(this.previousLines, this.currentLines);
 
-      for (int i = 0; i < curLines.rows(); i++)
+      for (int i = 0; i < currentLines.rows(); i++)
       {
-         double[] val = curLines.get(i, 0);
+         double[] val = currentLines.get(i, 0);
          Imgproc.line(this.currentImage, new Point(val[0], val[1]), new Point(val[2], val[3]), new Scalar(0, 255, 255), 2);
       }
 
@@ -186,19 +235,19 @@ public class AtlasLineSegmentEstimator
       if (this.currentPlanarRegionsListMessage != null)
       {
          PlanarRegionsList newRegions = PlanarRegionMessageConverter.convertToPlanarRegionsList(this.currentPlanarRegionsListMessage);
-         lineRegionAssoc.loadParams(cameraIntrinsics);
+         lineRegionAssociator.loadParams(cameraIntrinsics);
+
 
          for (PlanarRegion region : newRegions.getPlanarRegionsAsList())
          {
-
             ArrayList<Point> pointList = new ArrayList<>();
             Point2D regionMidPoint = new Point2D(0, 0);
 
             if (region.getConvexHull().getArea() * 1000 > 50)
             {
-               pointList = lineRegionAssoc.projectPlanarRegion(region, regionMidPoint);
-               lineRegionAssoc.drawPolygonOnImage(currentImage, pointList, regionMidPoint, region.getRegionId());
-               lineRegionAssoc.drawLineRegionAssociation(curImgLeft, pointList, regionMidPoint, region.getRegionId());
+               pointList = lineRegionAssociator.projectPlanarRegion(region, regionMidPoint);
+               lineRegionAssociator.drawPolygonOnImage(currentImage, pointList, regionMidPoint, region.getRegionId());
+               lineRegionAssociator.drawLineRegionAssociation(curImgLeft, pointList, regionMidPoint, region.getRegionId());
             }
          }
       }
@@ -211,10 +260,9 @@ public class AtlasLineSegmentEstimator
       Imgproc.resize(dispImage, dispImage, new Size(2400, 1000));
       HighGui.namedWindow("LineEstimator", HighGui.WINDOW_AUTOSIZE);
       HighGui.imshow("LineEstimator", dispImage);
-      this.code = HighGui.waitKey(1);
+      HighGui.waitKey(1);
 
-      if (this.code == 81)
-         System.exit(0);
+      previousLines = currentLines;
    }
 
    public static void main(String[] args)
