1030679005bd20521654f76ca6972e962858b8f4
Sylvain Bertrand <sbertrand@ihmc.us>
2020-10-16 09:50:03 -0500

    :arrow_up: yovariables 0.9.6

--- ihmc-footstep-planning/build.gradle.kts
+++ ihmc-footstep-planning/build.gradle.kts
@@ -17,7 +17,7 @@ mainDependencies {
    api("org.ejml:ejml-ddense:0.39")
 
    api("us.ihmc:euclid:0.15.1")
-   api("us.ihmc:ihmc-yovariables:0.9.5")
+   api("us.ihmc:ihmc-yovariables:0.9.6")
    api("us.ihmc:ihmc-graphics-description:0.19.1")
    api("us.ihmc:ihmc-robotics-toolkit:source")
    api("us.ihmc:ihmc-convex-optimization:0.17.0")
