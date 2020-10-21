cb8508c7c4ad9ec7cd88340bf61b9a0ab68165b4
Duncan Calvert <dcalvert@ihmc.us>
2020-10-21 17:49:22 -0500

    A little more on SCSROS2Visualizer and ROS2Debugger.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/ros/visualizer/SCSROS2Visualizer.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/ros/visualizer/SCSROS2Visualizer.java
@@ -5,19 +5,22 @@ import us.ihmc.simulationconstructionset.Robot;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
 import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoDouble;
 
 public class SCSROS2Visualizer
 {
-   private final YoRegistry registry = new YoRegistry(getClass().getSimpleName());
+   private final YoRegistry yoRegistry = new YoRegistry(getClass().getSimpleName());
    private final SimulationConstructionSet scs = new SimulationConstructionSet(new Robot("Robot"),
                                                                                new NullGraphics3DAdapter(),
                                                                                new SimulationConstructionSetParameters());
+
+   private final YoDouble numberOfParticipants = new YoDouble("NumberOfParticipants", yoRegistry);
 
    public SCSROS2Visualizer()
    {
       ROS2Debugger ros2Debugger = new ROS2Debugger();
 
-      scs.addYoRegistry(registry);
+      scs.addYoRegistry(yoRegistry);
       scs.setDT(1.0, 1);
       scs.skipLoadingDefaultConfiguration();
       scs.hideViewport();
