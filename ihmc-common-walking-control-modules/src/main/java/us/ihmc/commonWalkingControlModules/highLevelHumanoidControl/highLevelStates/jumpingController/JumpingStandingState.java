238e1eea194aedbd55297e2c7287dc7696d9c7e2
Robert <rgriffin@ihmc.us>
2020-10-24 15:38:45 -0500

    got the jumping states all put into place and compiling

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/jumpingController/JumpingStandingState.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/jumpingController/JumpingStandingState.java
@@ -1,15 +1,11 @@
 package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.jumpingController;
 
-import us.ihmc.commonWalkingControlModules.capturePoint.BalanceManager;
-import us.ihmc.commonWalkingControlModules.capturePoint.stepAdjustment.JumpingBalanceManager;
-import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.commonWalkingControlModules.capturePoint.JumpingBalanceManager;
 import us.ihmc.commonWalkingControlModules.controlModules.WalkingFailureDetectionControlModule;
 import us.ihmc.commonWalkingControlModules.controlModules.pelvis.PelvisOrientationManager;
 import us.ihmc.commonWalkingControlModules.controlModules.rigidBody.RigidBodyControlManager;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories.HighLevelControlManagerFactory;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.walkingController.TouchdownErrorCompensator;
 import us.ihmc.commonWalkingControlModules.messageHandlers.WalkingMessageHandler;
-import us.ihmc.commonWalkingControlModules.momentumBasedController.HighLevelHumanoidControllerToolbox;
 import us.ihmc.communication.controllerAPI.CommandInputManager;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
@@ -20,24 +16,19 @@ import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class JumpingStandingState extends JumpingState
 {
-   private final CommandInputManager commandInputManager;
-   private final WalkingMessageHandler walkingMessageHandler;
-   private final HighLevelHumanoidControllerToolbox controllerToolbox;
+   private final JumpingControllerToolbox controllerToolbox;
    private final WalkingFailureDetectionControlModule failureDetectionControlModule;
 
    private final JumpingBalanceManager balanceManager;
    private final PelvisOrientationManager pelvisOrientationManager;
    private final SideDependentList<RigidBodyControlManager> handManagers = new SideDependentList<>();
 
-   public JumpingStandingState(CommandInputManager commandInputManager, WalkingMessageHandler walkingMessageHandler,
-                               HighLevelHumanoidControllerToolbox controllerToolbox, HighLevelControlManagerFactory managerFactory,
+   public JumpingStandingState(JumpingControllerToolbox controllerToolbox, HighLevelControlManagerFactory managerFactory,
                                JumpingBalanceManager balanceManager, WalkingFailureDetectionControlModule failureDetectionControlModule,
                                YoRegistry parentRegistry)
    {
       super(JumpingStateEnum.STANDING, parentRegistry);
 
-      this.commandInputManager = commandInputManager;
-      this.walkingMessageHandler = walkingMessageHandler;
       this.controllerToolbox = controllerToolbox;
       this.failureDetectionControlModule = failureDetectionControlModule;
       this.balanceManager = balanceManager;
@@ -71,15 +62,10 @@ public class JumpingStandingState extends JumpingState
    @Override
    public void onEntry()
    {
-      commandInputManager.clearAllCommands();
-
       // need to always update biped support polygons after a change to the contact states
       controllerToolbox.updateBipedSupportPolygons();
 
-      balanceManager.setICPPlanTransferFromSide(null);
       balanceManager.initializeICPPlanForStanding();
-
-      walkingMessageHandler.reportWalkingComplete();
 
       if (pelvisOrientationManager != null)
          pelvisOrientationManager.initializeStanding();
@@ -95,11 +81,6 @@ public class JumpingStandingState extends JumpingState
       {
          if (handManagers.get(robotSide) != null)
             handManagers.get(robotSide).prepareForLocomotion();
-      }
-
-      if (pelvisOrientationManager != null)
-      {
-         pelvisOrientationManager.prepareForLocomotion(walkingMessageHandler.getNextStepTime());
       }
 
       controllerToolbox.reportChangeOfRobotMotionStatus(RobotMotionStatus.IN_MOTION);
