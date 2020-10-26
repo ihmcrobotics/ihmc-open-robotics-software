1d2bcf739591a1126f57902d6adffcf484f3344a
Robert <rgriffin@ihmc.us>
2020-10-26 10:56:36 -0500

    started setting up the squat prep phase in the jumping standing state

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/JumpingBalanceManager.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/JumpingBalanceManager.java
@@ -1,24 +1,19 @@
 package us.ihmc.commonWalkingControlModules.capturePoint;
 
 import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.BipedSupportPolygons;
-import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.YoPlaneContactState;
-import us.ihmc.commonWalkingControlModules.controllerCore.command.inverseDynamics.PlaneContactStateCommand;
 import us.ihmc.commonWalkingControlModules.dynamicPlanning.bipedPlanning.*;
 import us.ihmc.commonWalkingControlModules.dynamicPlanning.comPlanning.CornerPointViewer;
 import us.ihmc.commonWalkingControlModules.dynamicPlanning.comPlanning.OptimizedCoMTrajectoryPlanner;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.jumpingController.JumpingControllerToolbox;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.jumpingController.StandingCoPTrajectoryGenerator;
 import us.ihmc.euclid.referenceFrame.*;
 import us.ihmc.euclid.referenceFrame.interfaces.*;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicPosition;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicPosition.GraphicType;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
-import us.ihmc.graphicsDescription.yoGraphics.plotting.YoArtifactPosition;
-import us.ihmc.robotics.geometry.ConvexPolygonScaler;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.robotics.time.ExecutionTimer;
-import us.ihmc.sensorProcessing.frames.CommonHumanoidReferenceFrames;
-import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePoint2D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePoint3D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
 import us.ihmc.yoVariables.registry.YoRegistry;
@@ -45,44 +40,28 @@ public class JumpingBalanceManager
    private final YoFrameVector3D yoDesiredCoMVelocity = new YoFrameVector3D("desiredCoMVelocity", worldFrame, registry);
    private final YoFramePoint3D yoPerfectVRP = new YoFramePoint3D("perfectVRP", worldFrame, registry);
 
-   private final ReferenceFrame centerOfMassFrame;
 
-   private final FramePoint2D centerOfMassPosition = new FramePoint2D();
-
-   private final ConvexPolygonScaler convexPolygonShrinker = new ConvexPolygonScaler();
-   private final FrameConvexPolygon2D shrunkSupportPolygon = new FrameConvexPolygon2D();
-
-   private final YoDouble distanceToShrinkSupportPolygonWhenHoldingCurrent = new YoDouble("distanceToShrinkSupportPolygonWhenHoldingCurrent", registry);
-
-   private final YoBoolean icpPlannerDone = new YoBoolean("ICPPlannerDone", registry);
+   private final YoBoolean comPlannerDone = new YoBoolean("ICPPlannerDone", registry);
    private final ExecutionTimer plannerTimer = new ExecutionTimer("icpPlannerTimer", registry);
 
-   private final SideDependentList<PlaneContactStateCommand> contactStateCommands = new SideDependentList<>(new PlaneContactStateCommand(),
-                                                                                                            new PlaneContactStateCommand());
    private final SideDependentList<? extends ReferenceFrame> soleFrames;
 
    private final YoDouble currentStateDuration = new YoDouble("CurrentStateDuration", registry);
    private final YoDouble totalStateDuration = new YoDouble("totalStateDuration", registry);
    private final YoDouble timeInSupportSequence = new YoDouble("TimeInSupportSequence", registry);
    private final CoPTrajectoryGeneratorState copTrajectoryState;
-   private final WalkingCoPTrajectoryGenerator copTrajectory;
+   private final StandingCoPTrajectoryGenerator copTrajectoryForStanding;
    private final OptimizedCoMTrajectoryPlanner comTrajectoryPlanner;
 
    public JumpingBalanceManager(JumpingControllerToolbox controllerToolbox,
                                 CoPTrajectoryParameters copTrajectoryParameters,
                                 YoRegistry parentRegistry)
    {
-      CommonHumanoidReferenceFrames referenceFrames = controllerToolbox.getReferenceFrames();
-
       YoGraphicsListRegistry yoGraphicsListRegistry = controllerToolbox.getYoGraphicsListRegistry();
 
       this.controllerToolbox = controllerToolbox;
 
-      centerOfMassFrame = referenceFrames.getCenterOfMassFrame();
-
       bipedSupportPolygons = controllerToolbox.getBipedSupportPolygons();
-
-      distanceToShrinkSupportPolygonWhenHoldingCurrent.set(0.08);
 
       FrameConvexPolygon2D defaultSupportPolygon = controllerToolbox.getDefaultFootPolygons().get(RobotSide.LEFT);
       soleFrames = controllerToolbox.getReferenceFrames().getSoleFrames();
@@ -90,8 +69,8 @@ public class JumpingBalanceManager
       comTrajectoryPlanner = new OptimizedCoMTrajectoryPlanner(controllerToolbox.getGravityZ(), controllerToolbox.getOmega0Provider(), registry);
       copTrajectoryState = new CoPTrajectoryGeneratorState(registry);
       copTrajectoryState.registerStateToSave(copTrajectoryParameters);
-      copTrajectory = new WalkingCoPTrajectoryGenerator(copTrajectoryParameters, defaultSupportPolygon, registry);
-      copTrajectory.registerState(copTrajectoryState);
+      copTrajectoryForStanding = new StandingCoPTrajectoryGenerator(copTrajectoryParameters, registry);
+      copTrajectoryForStanding.registerState(copTrajectoryState);
 
       String graphicListName = getClass().getSimpleName();
 
@@ -121,6 +100,11 @@ public class JumpingBalanceManager
       copTrajectoryState.clear();
    }
 
+   public void setDesiredCoMHeight(double height)
+   {
+      comTrajectoryPlanner.setNominalCoMHeight(height);
+   }
+
    public void compute()
    {
       yoDesiredDCM.set(comTrajectoryPlanner.getDesiredDCMPosition());
@@ -135,23 +119,13 @@ public class JumpingBalanceManager
 
       CapturePointTools.computeCentroidalMomentumPivot(yoDesiredDCM, yoDesiredDCMVelocity, omega0, yoPerfectVRP);
 
-      for (RobotSide robotSide : RobotSide.values)
-      {
-         YoPlaneContactState contactState = controllerToolbox.getFootContactState(robotSide);
-         contactState.getPlaneContactStateCommand(contactStateCommands.get(robotSide));
-      }
-
       jumpingMomentumRateControlModuleInput.setOmega0(omega0);
       jumpingMomentumRateControlModuleInput.setTimeInState(timeInSupportSequence.getDoubleValue());
       jumpingMomentumRateControlModuleInput.setVrpTrajectories(comTrajectoryPlanner.getVRPTrajectories());
    }
 
-   public void computeICPPlan()
-   {
-     computeICPPlanInternal(copTrajectory);
-   }
 
-   private void computeICPPlanInternal(CoPTrajectoryGenerator copTrajectory)
+   public void computeCoMPlanForStanding()
    {
       plannerTimer.startMeasurement();
 
@@ -161,18 +135,19 @@ public class JumpingBalanceManager
          if (controllerToolbox.getFootContactState(robotSide).inContact())
             copTrajectoryState.initializeStance(robotSide, bipedSupportPolygons.getFootPolygonsInSoleZUpFrame().get(robotSide), soleFrames.get(robotSide));
       }
-      copTrajectory.compute(copTrajectoryState);
+      copTrajectoryForStanding.compute(copTrajectoryState);
 
-      comTrajectoryPlanner.solveForTrajectory(copTrajectory.getContactStateProviders());
+      comTrajectoryPlanner.solveForTrajectory(copTrajectoryForStanding.getContactStateProviders());
       comTrajectoryPlanner.compute(timeInSupportSequence.getDoubleValue());
 
       // If this condition is false we are experiencing a late touchdown or a delayed liftoff. Do not advance the time in support sequence!
       timeInSupportSequence.add(controllerToolbox.getControlDT());
 
-      icpPlannerDone.set(timeInSupportSequence.getValue() >= currentStateDuration.getValue());
+      comPlannerDone.set(timeInSupportSequence.getValue() >= currentStateDuration.getValue());
 
       plannerTimer.stopMeasurement();
    }
+
 
    public FramePoint3DReadOnly getDesiredDCM()
    {
@@ -207,7 +182,7 @@ public class JumpingBalanceManager
       comTrajectoryPlanner.setMaintainInitialCoMVelocityContinuity(false);
    }
 
-   public void initializeICPPlanForStanding()
+   public void initializeCoMPlanForStanding()
    {
       copTrajectoryState.setInitialCoP(yoPerfectVRP);
       copTrajectoryState.initializeStance(bipedSupportPolygons.getFootPolygonsInSoleZUpFrame(), soleFrames);
@@ -219,10 +194,10 @@ public class JumpingBalanceManager
 
       comTrajectoryPlanner.setMaintainInitialCoMVelocityContinuity(true);
 
-      icpPlannerDone.set(false);
+      comPlannerDone.set(false);
    }
 
-   public void initializeICPPlanForTransferToStanding()
+   public void initializeCoMPlanForTransferToStanding()
    {
       copTrajectoryState.setInitialCoP(yoPerfectVRP);
       copTrajectoryState.initializeStance(bipedSupportPolygons.getFootPolygonsInSoleZUpFrame(), soleFrames);
@@ -234,28 +209,13 @@ public class JumpingBalanceManager
 
       comTrajectoryPlanner.setMaintainInitialCoMVelocityContinuity(true);
 
-      icpPlannerDone.set(false);
+      comPlannerDone.set(false);
    }
 
 
-   public boolean isICPPlanDone()
+   public boolean isCoMPlanDone()
    {
-      return icpPlannerDone.getValue();
-   }
-
-   public void requestICPPlannerToHoldCurrentCoM()
-   {
-      centerOfMassPosition.setToZero(centerOfMassFrame);
-
-      FrameConvexPolygon2DReadOnly supportPolygonInMidFeetZUp = bipedSupportPolygons.getSupportPolygonInMidFeetZUp();
-      convexPolygonShrinker.scaleConvexPolygon(supportPolygonInMidFeetZUp, distanceToShrinkSupportPolygonWhenHoldingCurrent.getDoubleValue(), shrunkSupportPolygon);
-
-      centerOfMassPosition.changeFrameAndProjectToXYPlane(shrunkSupportPolygon.getReferenceFrame());
-      shrunkSupportPolygon.orthogonalProjection(centerOfMassPosition);
-      centerOfMassPosition.changeFrameAndProjectToXYPlane(worldFrame);
-
-      // This tricks it to the current value.
-      copTrajectoryState.setInitialCoP(centerOfMassPosition);
+      return comPlannerDone.getValue();
    }
 
    public void setFinalTransferTime(double finalTransferDuration)
