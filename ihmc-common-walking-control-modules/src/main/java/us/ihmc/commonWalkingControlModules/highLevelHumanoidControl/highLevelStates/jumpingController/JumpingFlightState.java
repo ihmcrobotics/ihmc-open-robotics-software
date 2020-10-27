5c87a7dc2491f99d6aa337ea6e4f5a9918168e1c
Robert <rgriffin@ihmc.us>
2020-10-26 21:19:55 -0500

    started to get the jumping work

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/jumpingController/JumpingFlightState.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/jumpingController/JumpingFlightState.java
@@ -3,8 +3,10 @@ package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelSt
 import us.ihmc.commonWalkingControlModules.capturePoint.JumpingBalanceManager;
 import us.ihmc.commonWalkingControlModules.controlModules.WalkingFailureDetectionControlModule;
 import us.ihmc.commonWalkingControlModules.controlModules.rigidBody.RigidBodyControlManager;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
+import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.sensorProcessing.model.RobotMotionStatus;
@@ -12,13 +14,16 @@ import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class JumpingFlightState extends JumpingState
 {
+   private static final double swingHeight = 0.2;
+   private static final double defaultWidth = 0.2;
    private final JumpingControllerToolbox controllerToolbox;
    private final WalkingFailureDetectionControlModule failureDetectionControlModule;
 
    private final JumpingBalanceManager balanceManager;
-   private final JumpingPelvisOrientationManager pelvisOrientationManager;
+   private final JumpingFeetManager feetManager;
    private final SideDependentList<RigidBodyControlManager> handManagers = new SideDependentList<>();
 
+   private final JumpingCoPTrajectoryParameters jumpingCoPTrajectoryParameters;
    private final JumpingGoalHandler jumpingGoalHandler;
 
    private final JumpingGoal jumpingGoal = new JumpingGoal();
@@ -27,6 +32,7 @@ public class JumpingFlightState extends JumpingState
                              JumpingControllerToolbox controllerToolbox,
                              JumpingControlManagerFactory managerFactory,
                              WalkingFailureDetectionControlModule failureDetectionControlModule,
+                             JumpingCoPTrajectoryParameters jumpingCoPTrajectoryParameters,
                              YoRegistry parentRegistry)
    {
       super(JumpingStateEnum.FLIGHT, parentRegistry);
@@ -34,8 +40,10 @@ public class JumpingFlightState extends JumpingState
       this.jumpingGoalHandler = jumpingGoalHandler;
       this.controllerToolbox = controllerToolbox;
       this.failureDetectionControlModule = failureDetectionControlModule;
+      this.jumpingCoPTrajectoryParameters = jumpingCoPTrajectoryParameters;
 
       balanceManager = managerFactory.getOrCreateBalanceManager();
+      feetManager = managerFactory.getOrCreateFeetManager();
 
       RigidBodyBasics chest = controllerToolbox.getFullRobotModel().getChest();
       if (chest != null)
@@ -53,8 +61,6 @@ public class JumpingFlightState extends JumpingState
             }
          }
       }
-
-      pelvisOrientationManager = managerFactory.getOrCreatePelvisOrientationManager();
    }
 
    @Override
@@ -62,6 +68,11 @@ public class JumpingFlightState extends JumpingState
    {
       balanceManager.computeCoMPlanForJumping(jumpingGoal);
    }
+
+   private final FramePose3D footGoalPose = new FramePose3D();
+   private final FramePose3D midFootPose = new FramePose3D();
+   private final FramePose3D goalPose = new FramePose3D();
+   private final PoseReferenceFrame goalPoseFrame = new PoseReferenceFrame("goalPoseFrame", ReferenceFrame.getWorldFrame());
 
    @Override
    public void onEntry()
@@ -72,14 +83,44 @@ public class JumpingFlightState extends JumpingState
       controllerToolbox.updateBipedSupportPolygons();
 
       // TODO trigger the swing in the feet manager
-
       balanceManager.initializeCoMPlanForFlight(jumpingGoal);
 
+      midFootPose.setToZero(controllerToolbox.getReferenceFrames().getMidFeetUnderPelvisFrame());
+      goalPose.setIncludingFrame(midFootPose);
+      goalPose.setX(jumpingGoal.getGoalLength());
+      if (!Double.isNaN(jumpingGoal.getGoalHeight()))
+         goalPose.setZ(jumpingGoal.getGoalHeight());
+      if (!Double.isNaN(jumpingGoal.getGoalRotation()))
+         goalPose.getOrientation().setToYawOrientation(jumpingGoal.getGoalRotation());
+      goalPose.changeFrame(ReferenceFrame.getWorldFrame());
+      goalPoseFrame.setPoseAndUpdate(goalPose);
+
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         footGoalPose.setToZero(goalPoseFrame);
+         double width;
+         if (!Double.isNaN(jumpingGoal.getGoalFootWidth()))
+            width = robotSide.negateIfRightSide(0.5 * jumpingGoal.getGoalFootWidth());
+         else
+            width = robotSide.negateIfRightSide(0.5 * jumpingCoPTrajectoryParameters.getDefaultFootWidth());
+
+         footGoalPose.setY(width);
+         footGoalPose.changeFrame(ReferenceFrame.getWorldFrame());
+
+         double flightDuration;
+         if (!Double.isNaN(jumpingGoal.getFlightDuration()))
+            flightDuration = jumpingGoal.getFlightDuration();
+         else
+            flightDuration = jumpingCoPTrajectoryParameters.getDefaultFlightDuration();
+         feetManager.requestSwing(robotSide, footGoalPose, swingHeight, flightDuration);
+      }
 //      if (pelvisOrientationManager != null)
 //         pelvisOrientationManager.initializeStanding();
 
 //      failureDetectionControlModule.setNextFootstep(null);
    }
+
+
 
    @Override
    public void onExit()
