2a5193a09ddee7371f8e4f46d06050272a2e34d5
Robert <rgriffin@ihmc.us>
2020-10-29 15:34:54 -0500

    got most of the objectives converted over

--- ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/comPlanning/CoMTrajectoryPlannerToolsTest.java
+++ ihmc-common-walking-control-modules/src/test/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/comPlanning/CoMTrajectoryPlannerToolsTest.java
@@ -1,16 +1,21 @@
 package us.ihmc.commonWalkingControlModules.dynamicPlanning.comPlanning;
 
 import org.ejml.EjmlUnitTests;
+import org.ejml.data.DMatrix;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
+import org.ejml.dense.row.factory.LinearSolverFactory_DDRM;
+import org.ejml.interfaces.linsol.LinearSolverDense;
 import org.junit.jupiter.api.Test;
 import us.ihmc.commonWalkingControlModules.capturePoint.CapturePointTools;
 import us.ihmc.commons.RandomNumbers;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.euclid.referenceFrame.tools.EuclidFrameRandomTools;
 import us.ihmc.euclid.referenceFrame.tools.EuclidFrameTestTools;
+import us.ihmc.matrixlib.MatrixTools;
 
 import java.util.Random;
 
@@ -20,7 +25,6 @@ import static us.ihmc.robotics.Assert.assertTrue;
 public class CoMTrajectoryPlannerToolsTest
 {
    private static final double epsilon = 1e-4;
-
 
    @Test
    public void testTrajectoryConstruction()
@@ -46,7 +50,7 @@ public class CoMTrajectoryPlannerToolsTest
          FramePoint3D desiredVRPPosition2 = new FramePoint3D();
          FrameVector3D desiredCoMVelocity = new FrameVector3D();
          FrameVector3D desiredCoMAcceleration = new FrameVector3D();
-         FrameVector3D desiredDCMVelocity= new FrameVector3D();
+         FrameVector3D desiredDCMVelocity = new FrameVector3D();
 
          CoMTrajectoryPlannerTools.constructDesiredCoMPosition(desiredCoMPosition, c0, c1, c2, c3, c4, c5, time, omega);
          CoMTrajectoryPlannerTools.constructDesiredCoMVelocity(desiredCoMVelocity, c0, c1, c2, c3, c4, c5, time, omega);
@@ -240,6 +244,8 @@ public class CoMTrajectoryPlannerToolsTest
       }
    }
 
+      /*
+
    @Test
    public void testCoMPositionContinuityObjective()
    {
@@ -278,5 +284,412 @@ public class CoMTrajectoryPlannerToolsTest
       CommonOps_DDRM.multInner(jacobian, hessian1);
 
       EjmlUnitTests.assertEquals(hessian1, hessian2, 1e-5);
+   }
+
+    */
+
+   @Test
+   public void testCoMPositionObjective()
+   {
+      double omega = 3.0;
+      double time = 1.5;
+      double weight = 10.0;
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj xGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj yGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+      Random random = new Random(1738L);
+      for (int i = 0; i < 10; i++)
+      {
+         hessian.zero();
+         xGradient.zero();
+         yGradient.zero();
+         zGradient.zero();
+
+         MatrixTools.addDiagonal(hessian, 1e-6);
+
+         FramePoint3D desiredPosition = EuclidFrameRandomTools.nextFramePoint3D(random, ReferenceFrame.getWorldFrame(), -10.0, 10.0);
+
+         CoMTrajectoryPlannerTools.addCoMPositionObjective(weight, desiredPosition, omega, time, 0, hessian, xGradient, yGradient, zGradient);
+
+         DMatrixRMaj xSolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj ySolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+         solveProblem(hessian, xGradient, xSolution);
+         solveProblem(hessian, yGradient, ySolution);
+         solveProblem(hessian, zGradient, zSolution);
+
+         double xValue = 0.0, yValue = 0.0, zValue = 0.0;
+         for (int j = 0; j < 6; j++)
+         {
+            xValue += CoMTrajectoryPlannerTools.getCoMPositionCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+            yValue += CoMTrajectoryPlannerTools.getCoMPositionCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+            zValue += CoMTrajectoryPlannerTools.getCoMPositionCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+         }
+
+         assertEquals(desiredPosition.getX(), xValue, 1e-6);
+         assertEquals(desiredPosition.getY(), yValue, 1e-6);
+         assertEquals(desiredPosition.getZ(), zValue, 1e-6);
+      }
+   }
+
+   @Test
+   public void testCoMVelocityObjective()
+   {
+      double omega = 3.0;
+      double time = 1.5;
+      double weight = 10.0;
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj xGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj yGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+
+      MatrixTools.addDiagonal(hessian, 1e-6);
+
+      FrameVector3DReadOnly desiredVelocity = new FrameVector3D(ReferenceFrame.getWorldFrame(), 1.5, 2.5, 3.5);
+
+      CoMTrajectoryPlannerTools.addCoMVelocityObjective(weight, desiredVelocity, omega, time, 0, hessian, xGradient, yGradient, zGradient);
+
+      DMatrixRMaj xSolution = new DMatrixRMaj(6, 1);
+      DMatrixRMaj ySolution = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+      solveProblem(hessian, xGradient, xSolution);
+      solveProblem(hessian, yGradient, ySolution);
+      solveProblem(hessian, zGradient, zSolution);
+
+      double xValue = 0.0, yValue = 0.0, zValue = 0.0;
+      for (int j = 0; j < 6; j++)
+      {
+         xValue += CoMTrajectoryPlannerTools.getCoMVelocityCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+         yValue += CoMTrajectoryPlannerTools.getCoMVelocityCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+         zValue += CoMTrajectoryPlannerTools.getCoMVelocityCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+      }
+
+      assertEquals(1.5, xValue, 1e-6);
+      assertEquals(2.5, yValue, 1e-6);
+      assertEquals(3.5, zValue, 1e-6);
+
+
+      Random random = new Random(1738L);
+      for (int i = 0; i < 10; i++)
+      {
+         hessian.zero();
+         xGradient.zero();
+         yGradient.zero();
+         zGradient.zero();
+
+         MatrixTools.addDiagonal(hessian, 1e-6);
+
+         desiredVelocity = EuclidFrameRandomTools.nextFrameVector3D(random, ReferenceFrame.getWorldFrame(), -10.0, 10.0);
+
+         CoMTrajectoryPlannerTools.addCoMVelocityObjective(weight, desiredVelocity, omega, time, 0, hessian, xGradient, yGradient, zGradient);
+
+         xSolution = new DMatrixRMaj(6, 1);
+         ySolution = new DMatrixRMaj(6, 1);
+         zSolution = new DMatrixRMaj(6, 1);
+
+         solveProblem(hessian, xGradient, xSolution);
+         solveProblem(hessian, yGradient, ySolution);
+         solveProblem(hessian, zGradient, zSolution);
+
+         xValue = 0.0;
+         yValue = 0.0;
+         zValue = 0.0;
+         for (int j = 0; j < 6; j++)
+         {
+            xValue += CoMTrajectoryPlannerTools.getCoMVelocityCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+            yValue += CoMTrajectoryPlannerTools.getCoMVelocityCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+            zValue += CoMTrajectoryPlannerTools.getCoMVelocityCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+         }
+
+         assertEquals(desiredVelocity.getX(), xValue, 1e-6);
+         assertEquals(desiredVelocity.getY(), yValue, 1e-6);
+         assertEquals(desiredVelocity.getZ(), zValue, 1e-6);
+      }
+   }
+
+   @Test
+   public void testCoMAccelerationObjective()
+   {
+      double omega = 3.0;
+      double time = 0.5;
+      double weight = 1e2;
+
+      Random random = new Random(1738L);
+
+      FrameVector3D desiredAcceleration = EuclidFrameRandomTools.nextFrameVector3D(random, ReferenceFrame.getWorldFrame(), -10, 10.0);
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj xGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj yGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+      hessian.zero();
+      zGradient.zero();
+
+      MatrixTools.addDiagonal(hessian, 1e-10);
+
+      CoMTrajectoryPlannerTools.addCoMAccelerationObjective(weight, 0, omega, time, desiredAcceleration, hessian, xGradient, yGradient, zGradient);
+
+      DMatrixRMaj xSolution = new DMatrixRMaj(6, 1);
+      DMatrixRMaj ySolution = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+      solveProblem(hessian, xGradient, xSolution);
+      solveProblem(hessian, yGradient, ySolution);
+      solveProblem(hessian, zGradient, zSolution);
+
+      double xValue = 0.0;
+      double yValue = 0.0;
+      double zValue = 0.0;
+      for (int j = 0; j < 6; j++)
+      {
+         xValue += CoMTrajectoryPlannerTools.getCoMAccelerationCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+         yValue += CoMTrajectoryPlannerTools.getCoMAccelerationCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+         zValue += CoMTrajectoryPlannerTools.getCoMAccelerationCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+      }
+
+      assertEquals(desiredAcceleration.getX(), xValue, 1e-6);
+      assertEquals(desiredAcceleration.getY(), yValue, 1e-6);
+      assertEquals(desiredAcceleration.getZ(), zValue, 1e-6);
+   }
+
+   @Test
+   public void testCoMAccelerationIsGravityObjective()
+   {
+      double omega = 3.0;
+      double time = 1.5;
+      double weight = 1.0;
+      double gravity = -9.81;
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+         hessian.zero();
+         zGradient.zero();
+
+         MatrixTools.addDiagonal(hessian, 1e-6);
+
+         CoMTrajectoryPlannerTools.addCoMAccelerationIsGravityObjective(weight, 0, omega, time, gravity, hessian, zGradient);
+
+         DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+         solveProblem(hessian, zGradient, zSolution);
+
+         double zValue = 0.0;
+         for (int j = 0; j < 6; j++)
+         {
+            zValue += CoMTrajectoryPlannerTools.getCoMAccelerationCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+         }
+
+         assertEquals(gravity, zValue, 1e-6);
+   }
+
+   @Test
+   public void testCoMJerkObjective()
+   {
+      double omega = 3.0;
+      double time = 1.5;
+      double weight = 10.0;
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj xGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj yGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+      Random random = new Random(1738L);
+      for (int i = 0; i < 10; i++)
+      {
+         hessian.zero();
+         xGradient.zero();
+         yGradient.zero();
+         zGradient.zero();
+
+         MatrixTools.addDiagonal(hessian, 1e-6);
+
+         FrameVector3DReadOnly desiredJerk = EuclidFrameRandomTools.nextFrameVector3D(random, ReferenceFrame.getWorldFrame());
+
+         CoMTrajectoryPlannerTools.addCoMJerkObjective(weight, desiredJerk, omega, time, 0, hessian, xGradient, yGradient, zGradient);
+
+         DMatrixRMaj xSolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj ySolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+         solveProblem(hessian, xGradient, xSolution);
+         solveProblem(hessian, yGradient, ySolution);
+         solveProblem(hessian, zGradient, zSolution);
+
+         double xValue = 0.0, yValue = 0.0, zValue = 0.0;
+         for (int j = 0; j < 6; j++)
+         {
+            xValue += CoMTrajectoryPlannerTools.getCoMJerkCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+            yValue += CoMTrajectoryPlannerTools.getCoMJerkCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+            zValue += CoMTrajectoryPlannerTools.getCoMJerkCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+         }
+
+         assertEquals(desiredJerk.getX(), xValue, 1e-6);
+         assertEquals(desiredJerk.getY(), yValue, 1e-6);
+         assertEquals(desiredJerk.getZ(), zValue, 1e-6);
+      }
+   }
+
+   @Test
+   public void testDCMPositionObjective()
+   {
+      double omega = 3.0;
+      double time = 1.5;
+      double weight = 10.0;
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj xGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj yGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+      Random random = new Random(1738L);
+      for (int i = 0; i < 10; i++)
+      {
+         hessian.zero();
+         xGradient.zero();
+         yGradient.zero();
+         zGradient.zero();
+
+         MatrixTools.addDiagonal(hessian, 1e-6);
+
+         FramePoint3D desiredPosition = EuclidFrameRandomTools.nextFramePoint3D(random, ReferenceFrame.getWorldFrame());
+
+         CoMTrajectoryPlannerTools.addDCMPositionObjective(weight, desiredPosition, omega, time, 0, hessian, xGradient, yGradient, zGradient);
+
+         DMatrixRMaj xSolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj ySolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+         solveProblem(hessian, xGradient, xSolution);
+         solveProblem(hessian, yGradient, ySolution);
+         solveProblem(hessian, zGradient, zSolution);
+
+         double xValue = 0.0, yValue = 0.0, zValue = 0.0;
+         for (int j = 0; j < 6; j++)
+         {
+            xValue += CoMTrajectoryPlannerTools.getDCMPositionCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+            yValue += CoMTrajectoryPlannerTools.getDCMPositionCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+            zValue += CoMTrajectoryPlannerTools.getDCMPositionCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+         }
+
+         assertEquals(desiredPosition.getX(), xValue, 1e-6);
+         assertEquals(desiredPosition.getY(), yValue, 1e-6);
+         assertEquals(desiredPosition.getZ(), zValue, 1e-6);
+      }
+   }
+
+   @Test
+   public void testVRPPositionObjective()
+   {
+      double omega = 3.0;
+      double time = 1.5;
+      double weight = 10.0;
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj xGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj yGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+      Random random = new Random(1738L);
+      for (int i = 0; i < 10; i++)
+      {
+         hessian.zero();
+         xGradient.zero();
+         yGradient.zero();
+         zGradient.zero();
+
+         MatrixTools.addDiagonal(hessian, 1e-6);
+
+         FramePoint3D desiredPosition = EuclidFrameRandomTools.nextFramePoint3D(random, ReferenceFrame.getWorldFrame());
+
+         CoMTrajectoryPlannerTools.addVRPPositionObjective(weight, desiredPosition, omega, time, 0, hessian, xGradient, yGradient, zGradient);
+
+         DMatrixRMaj xSolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj ySolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+         solveProblem(hessian, xGradient, xSolution);
+         solveProblem(hessian, yGradient, ySolution);
+         solveProblem(hessian, zGradient, zSolution);
+
+         double xValue = 0.0, yValue = 0.0, zValue = 0.0;
+         for (int j = 0; j < 6; j++)
+         {
+            xValue += CoMTrajectoryPlannerTools.getVRPPositionCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+            yValue += CoMTrajectoryPlannerTools.getVRPPositionCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+            zValue += CoMTrajectoryPlannerTools.getVRPPositionCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+         }
+
+         assertEquals(desiredPosition.getX(), xValue, 1e-6);
+         assertEquals(desiredPosition.getY(), yValue, 1e-6);
+         assertEquals(desiredPosition.getZ(), zValue, 1e-6);
+      }
+   }
+
+   @Test
+   public void testVRPVelocityObjective()
+   {
+      double omega = 3.0;
+      double time = 1.5;
+      double weight = 10.0;
+
+      DMatrixRMaj hessian = new DMatrixRMaj(6, 6);
+      DMatrixRMaj xGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj yGradient = new DMatrixRMaj(6, 1);
+      DMatrixRMaj zGradient = new DMatrixRMaj(6, 1);
+
+      Random random = new Random(1738L);
+      for (int i = 0; i < 10; i++)
+      {
+         hessian.zero();
+         xGradient.zero();
+         yGradient.zero();
+         zGradient.zero();
+
+         MatrixTools.addDiagonal(hessian, 1e-6);
+
+         FrameVector3D desiredVelocity = EuclidFrameRandomTools.nextFrameVector3D(random, ReferenceFrame.getWorldFrame());
+
+         CoMTrajectoryPlannerTools.addVRPVelocityObjective(weight, desiredVelocity, omega, time, 0, hessian, xGradient, yGradient, zGradient);
+
+         DMatrixRMaj xSolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj ySolution = new DMatrixRMaj(6, 1);
+         DMatrixRMaj zSolution = new DMatrixRMaj(6, 1);
+
+         solveProblem(hessian, xGradient, xSolution);
+         solveProblem(hessian, yGradient, ySolution);
+         solveProblem(hessian, zGradient, zSolution);
+
+         double xValue = 0.0, yValue = 0.0, zValue = 0.0;
+         for (int j = 0; j < 6; j++)
+         {
+            xValue += CoMTrajectoryPlannerTools.getVRPVelocityCoefficientTimeFunction(j, omega, time) * xSolution.get(j);
+            yValue += CoMTrajectoryPlannerTools.getVRPVelocityCoefficientTimeFunction(j, omega, time) * ySolution.get(j);
+            zValue += CoMTrajectoryPlannerTools.getVRPVelocityCoefficientTimeFunction(j, omega, time) * zSolution.get(j);
+         }
+
+         assertEquals(desiredVelocity.getX(), xValue, 1e-6);
+         assertEquals(desiredVelocity.getY(), yValue, 1e-6);
+         assertEquals(desiredVelocity.getZ(), zValue, 1e-6);
+      }
+   }
+
+   private static void solveProblem(DMatrixRMaj hessian, DMatrixRMaj gradient, DMatrixRMaj coefficientSolutionToPack)
+   {
+      LinearSolverDense<DMatrixRMaj> solver = LinearSolverFactory_DDRM.lu(6);
+      solver.setA(hessian);
+
+      DMatrixRMaj objective = new DMatrixRMaj(gradient);
+      CommonOps_DDRM.scale(-0.5, objective);
+      solver.solve(objective, coefficientSolutionToPack);
    }
 }
