73244a50ea8c069d1bfe387f5a28eb334586654f
Stephen McCrory <smccrory@ihmc.us>
2020-10-29 15:53:39 -0500

    Adding ContactPointProjector to resolve surface points and normals given a robot collision model

---
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/externalForceEstimationToolboxModule/detector/ContactPointProjector.java
@@ -0,0 +1,112 @@
+package us.ihmc.avatar.networkProcessor.externalForceEstimationToolboxModule.detector;
+
+import us.ihmc.euclid.Axis3D;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.referenceFrame.FramePoint3D;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.FrameVector3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
+import us.ihmc.robotics.physics.Collidable;
+import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactPointProjector
+{
+   private final List<Collidable> allCollidables;
+   private final List<Collidable> collidablesToCheck = new ArrayList<>();
+
+   private final FramePoint3D surfacePoint = new FramePoint3D();
+   private final FrameVector3D surfaceNormal = new FrameVector3D();
+   private final FramePose3D surfacePose = new FramePose3D();
+   private final PoseReferenceFrame surfaceFrame = new PoseReferenceFrame("surfaceFrame", surfacePose);
+
+   public ContactPointProjector(List<Collidable> allCollidables)
+   {
+      this.allCollidables = allCollidables;
+   }
+
+   public void initialize(RigidBodyBasics rigidBody)
+   {
+      collidablesToCheck.clear();
+      allCollidables.stream().filter(collidable -> collidable.getRigidBody() == rigidBody).forEach(collidablesToCheck::add);
+
+      if (collidablesToCheck.isEmpty())
+      {
+         throw new RuntimeException("The rigid body " + rigidBody + " has no collidables");
+      }
+   }
+
+   public boolean isPointInside(FramePoint3DReadOnly queryPoint)
+   {
+      for (int i = 0; i < collidablesToCheck.size(); i++)
+      {
+         if (collidablesToCheck.get(i).getShape().isPointInside(queryPoint))
+         {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   public void computeProjection(FramePoint3DReadOnly pointToProject)
+   {
+      double minimumProjectionDistance = Double.MAX_VALUE;
+      int minimumDistanceIndex = -1;
+
+      for (int i = 0; i < collidablesToCheck.size(); i++)
+      {
+         collidablesToCheck.get(i).getShape().evaluatePoint3DCollision(pointToProject, surfacePoint, surfaceNormal);
+
+         boolean isInsideOthers = false;
+         for (int j = i + 1; j < collidablesToCheck.size(); j++)
+         {
+            if (collidablesToCheck.get(j).getShape().isPointInside(surfacePoint))
+            {
+               isInsideOthers = true;
+               break;
+            }
+         }
+
+         if (isInsideOthers)
+         {
+            continue;
+         }
+
+         double distance = surfacePoint.distance(pointToProject);
+         if (distance < minimumProjectionDistance)
+         {
+            minimumProjectionDistance = distance;
+            minimumDistanceIndex = i;
+         }
+      }
+
+      collidablesToCheck.get(minimumDistanceIndex).getShape().evaluatePoint3DCollision(pointToProject, surfacePoint, surfaceNormal);
+
+      surfacePoint.changeFrame(ReferenceFrame.getWorldFrame());
+      surfaceNormal.changeFrame(ReferenceFrame.getWorldFrame());
+
+      surfacePose.getPosition().set(surfacePoint);
+      EuclidGeometryTools.orientation3DFromFirstToSecondVector3D(Axis3D.Z, surfaceNormal, surfacePose.getOrientation());
+      surfaceFrame.update();
+   }
+
+   public FramePoint3D getSurfacePoint()
+   {
+      return surfacePoint;
+   }
+
+   public FrameVector3D getSurfaceNormal()
+   {
+      return surfaceNormal;
+   }
+
+   public PoseReferenceFrame getSurfaceFrame()
+   {
+      return surfaceFrame;
+   }
+}
