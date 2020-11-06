98a31891627092ddd8a0efb6abdf5d03ea055926
Stephen McCrory <smccrory@ihmc.us>
2020-11-06 14:56:02 -0600

    Implementing joint noise matrix and estimated point projection

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/externalForceEstimationToolboxModule/detector/ContactPointEvaluator.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/externalForceEstimationToolboxModule/detector/ContactPointEvaluator.java
@@ -9,6 +9,7 @@ import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.log.LogTools;
+import us.ihmc.matrixlib.NativeCommonOps;
 
 /**
  * QP-based force resolver for particle-filter-based contact estimator, based on the following:
@@ -20,13 +21,17 @@ public class ContactPointEvaluator
 
    private final double convergenceThreshold = 1e-10;
    private final int maximumNumberOfIterations = 500;
-   private final double dampingTerm = 1e-3;
+   private final double dampingTerm = 1e-4;
 
    private final Vector3D[] polyhedraBasisVectors = new Vector3D[4];
    private final FrameVector3D[] polyhedraFrameBasisVectors = new FrameVector3D[4];
 
    private final DMatrixRMaj basisVectorMatrix = new DMatrixRMaj(3, 4);
+   /** Shorthand for J^T * F */
    private final DMatrixRMaj JTF = new DMatrixRMaj(0);
+   /** Shorthand for sigma^-1 * J^T * F */
+   private final DMatrixRMaj SInvJTF = new DMatrixRMaj(0);
+   private final DMatrixRMaj sigmaInv = new DMatrixRMaj(0);
    private final DMatrixRMaj diagonalCost = new DMatrixRMaj(0);
    private final DMatrixRMaj quadraticCost = new DMatrixRMaj(0);
    private final DMatrixRMaj linearCost = new DMatrixRMaj(0);
@@ -54,7 +59,7 @@ public class ContactPointEvaluator
 
       CommonOps_DDRM.setIdentity(Ain);
       CommonOps_DDRM.scale(-1.0, Ain);
-      CommonOps_DDRM.fill(beq, 0.01);
+      CommonOps_DDRM.fill(beq, 0.0);
    }
 
    public void setCoefficientOfFriction(double coefficientOfFriction)
@@ -80,10 +85,11 @@ public class ContactPointEvaluator
     *
     * @param jointspaceResidual gamma in the above paper (eq 12)
     * @param contactPointJacobian J_rt in the above paper (eq 12)
+    * @param sigma modelled noise of the joint angles, i.e. relative expected joint torque std. dev (eq 10)
     * @param zInContactFrame reference frame of the contact point, with z pointing into the mesh
     * @return likelihood of the given contact point, QP in the above paper
     */
-   public double computeMaximumLikelihoodForce(DMatrixRMaj jointspaceResidual, DMatrixRMaj contactPointJacobian, ReferenceFrame zInContactFrame)
+   public double computeMaximumLikelihoodForce(DMatrixRMaj jointspaceResidual, DMatrixRMaj contactPointJacobian, DMatrixRMaj sigma, ReferenceFrame zInContactFrame)
    {
       if (polyhedraBasisVectors[0].containsNaN())
       {
@@ -98,18 +104,20 @@ public class ContactPointEvaluator
          polyhedraFrameBasisVectors[i].get(0, i, basisVectorMatrix);
       }
 
+      CommonOps_DDRM.invert(sigma, sigmaInv);
       CommonOps_DDRM.multTransA(contactPointJacobian, basisVectorMatrix, JTF);
-      CommonOps_DDRM.multInner(JTF, quadraticCost);
+      NativeCommonOps.multQuad(JTF, sigmaInv, quadraticCost);
 
       diagonalCost.reshape(quadraticCost.getNumRows(), quadraticCost.getNumCols());
       CommonOps_DDRM.setIdentity(diagonalCost);
       CommonOps_DDRM.scale(dampingTerm, diagonalCost);
       CommonOps_DDRM.addEquals(quadraticCost, diagonalCost);
 
-      CommonOps_DDRM.multTransA(JTF, jointspaceResidual, linearCost);
+      CommonOps_DDRM.mult(sigmaInv, JTF, SInvJTF);
+      CommonOps_DDRM.multTransA(SInvJTF, jointspaceResidual, linearCost);
       CommonOps_DDRM.scale(-1.0, linearCost);
 
-      CommonOps_DDRM.multInner(jointspaceResidual, scalarCost);
+      NativeCommonOps.multQuad(jointspaceResidual, sigmaInv, scalarCost);
 
       qpSolver.clear();
       qpSolver.resetActiveSet();
@@ -121,7 +129,6 @@ public class ContactPointEvaluator
       qpSolver.setLinearInequalityConstraints(Ain, bin);
 
       qpSolver.solve(rho);
-
       CommonOps_DDRM.mult(basisVectorMatrix, rho, estimatedForce);
 
       return qpSolver.getObjectiveCost(rho);
