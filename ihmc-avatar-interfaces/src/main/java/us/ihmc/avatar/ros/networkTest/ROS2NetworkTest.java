18a7cc0c673f3007c02da2e7bf6949f96d5b2fd5
Operator Computer <OperatorComputer@unknown>
2020-11-12 17:48:48 -0600

    Add more SCS and YoVariable setup.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/ros/networkTest/ROS2NetworkTest.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/ros/networkTest/ROS2NetworkTest.java
@@ -1,18 +1,23 @@
 package us.ihmc.avatar.ros.networkTest;
 
+import org.apache.commons.lang3.mutable.MutableLong;
+import us.ihmc.commons.Conversions;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.exception.ExceptionTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.jMonkeyEngineToolkit.NullGraphics3DAdapter;
 import us.ihmc.log.LogTools;
 import us.ihmc.robotDataLogger.YoVariableClient;
+import us.ihmc.robotDataLogger.YoVariableServer;
 import us.ihmc.robotDataLogger.logger.DataServerSettings;
 import us.ihmc.simulationconstructionset.Robot;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
 import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
+import us.ihmc.tools.UnitConversions;
 import us.ihmc.tools.thread.PausablePeriodicThread;
 import us.ihmc.yoVariables.registry.YoRegistry;
 
+import javax.swing.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -24,7 +29,9 @@ import java.util.List;
 public class ROS2NetworkTest
 {
    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+   public static final double UPDATE_PERIOD = UnitConversions.hertzToSeconds(100.0);
    private final YoRegistry yoRegistry = new YoRegistry(getClass().getSimpleName());
+   private boolean paused;
 
    private final HashMap<String, ROS2NetworkTestProfile> profiles = new HashMap<>();
    {
@@ -40,7 +47,7 @@ public class ROS2NetworkTest
       LocalDateTime now = LocalDateTime.now();
       LocalDateTime startTime;
 
-      if (args.contains("--host"))
+      if (!args.contains("--startTime"))
       {
          LogTools.info("Running as host. Starting SCS.");
 
@@ -49,6 +56,7 @@ public class ROS2NetworkTest
                                                                          new NullGraphics3DAdapter(),
                                                                          new SimulationConstructionSetParameters());
          scs.addYoRegistry(yoRegistry);
+         scs.setDT(UPDATE_PERIOD, 1);
 
          // decide time to start
          startTime = now.plusSeconds(15);
@@ -64,14 +72,14 @@ public class ROS2NetworkTest
             RemoteSSHTools.session(remoteMachine.getHostname(), remoteMachine.getUsername(), connection ->
             {
                // find deploy directory and start the script
-               connection.exec(remoteMachine.getDeployDirectory() + "ROS2NetworkTest"
+               connection.exec(remoteMachine.getDeployDirectory() + "/ROS2NetworkTest"
                                + " --profile " + profileName
                                + " --startTime " + formattedStartTime
                );
             });
 
             // start YoVariableClient
-            YoVariableClient yoVariableClient = new YoVariableClient(new ROS2NetworkTestYoVariablesUpdatedListener());
+            YoVariableClient yoVariableClient = new YoVariableClient(new ROS2NetworkTestYoVariablesUpdatedListener(yoRegistry));
             yoVariableClient.start(remoteMachine.getHostname(), DataServerSettings.DEFAULT_PORT);
             yoVariableClient.setVariableSynchronizer(variableSynchronizer);
             yoVariableClients.add(yoVariableClient);
@@ -80,13 +88,29 @@ public class ROS2NetworkTest
          // wait until they are all started
 
          // spin up SCS thread to tick it at some frequency
-         new PausablePeriodicThread("SCSUpdater", 0.01, () ->
+         scs.setupGraph("t");
+         scs.skipLoadingDefaultConfiguration();
+         scs.hideViewport();
+         scs.changeBufferSize(8096);
+         scs.setScrollGraphsEnabled(false);
+         JToggleButton pauseButton = new JToggleButton("Pause/End"); // TODO Fix up
+         pauseButton.addActionListener(e ->
+         {
+            paused = pauseButton.isSelected();
+            scs.setScrollGraphsEnabled(paused);
+         });
+         scs.addButton(pauseButton);
+         scs.startOnAThread();
+         while (!scs.hasSimulationThreadStarted())
+            ThreadTools.sleep(200);
+
+         new PausablePeriodicThread("SCSUpdater", UPDATE_PERIOD, () ->
          {
             synchronized (variableSynchronizer)
             {
                scs.tickAndUpdate();
             }
-         });
+         }).start();
       }
       else
       {
@@ -99,6 +123,15 @@ public class ROS2NetworkTest
          startTime = LocalDateTime.parse(startTimeString, dateTimeFormatter);
 
          // start YoVariableServer
+         YoVariableServer yoVariableServer = new YoVariableServer(profileName, null, new DataServerSettings(false), UPDATE_PERIOD);
+         yoVariableServer.setMainRegistry(profile.getYoRegistry(), null, null);
+         yoVariableServer.start();
+
+         MutableLong timestamp = new MutableLong();
+         new PausablePeriodicThread("YoServerUpdate", UPDATE_PERIOD, () ->
+         {
+            yoVariableServer.update(timestamp.getAndAdd(Conversions.secondsToNanoseconds(UPDATE_PERIOD)));
+         }).start();
       }
 
       if (!args.contains("--profile"))
