86893be785892fde261dbf410ea08f5eedf9ad31
Robert <rgriffin@ihmc.us>
2020-11-15 14:31:53 -0600

    started working on the coefficient objective calculators

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/CoMCoefficientJacobianCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/CoMCoefficientJacobianCalculator.java
@@ -5,7 +5,7 @@ import us.ihmc.commons.MathTools;
 
 public class CoMCoefficientJacobianCalculator
 {
-   public static void calculateJacobian(int segmentId, double time, DMatrix jacobianToPack, int derivative, double scale)
+   public static void calculateCoMJacobian(int segmentId, double time, DMatrix jacobianToPack, int derivative, double scale)
    {
       switch (derivative)
       {
@@ -18,57 +18,86 @@ public class CoMCoefficientJacobianCalculator
          case 2:
             calculateAccelerationJacobian(segmentId, time, jacobianToPack, scale);
             break;
+         case 3:
+            calculateJerkJacobian(segmentId, jacobianToPack, scale);
          default:
-            throw new IllegalArgumentException("Derivative order must be less than 3.");
+            throw new IllegalArgumentException("Derivative order must be less than 4.");
       }
    }
 
-   public static void calculatePositionJacobian(int segmentId, double time, DMatrix positionJacobian, double scale)
+   public static void calculateDCMJacobian(int segmentId, double omega, double time, DMatrix jacobianToPack, int derivative, double scale)
+   {
+      calculateCoMJacobian(segmentId, time, jacobianToPack, derivative, scale);
+      calculateCoMJacobian(segmentId, time, jacobianToPack, derivative + 1, -scale / omega);
+   }
+
+   public static void calculateVRPJacobian(int segmentId, double omega, double time, DMatrix jacobianToPack, int derivative, double scale)
+   {
+      calculateCoMJacobian(segmentId, time, jacobianToPack, derivative, scale);
+      calculateCoMJacobian(segmentId, time, jacobianToPack, derivative + 2, -scale / (omega * omega));
+   }
+
+   public static void calculatePositionJacobian(int segmentId, double time, DMatrix positionJacobianToPack, double scale)
    {
       if (!MathTools.epsilonEquals(time, 0.0, 1e-5))
       {
          int startIndex = MPCIndexHandler.comCoefficientsPerSegment * segmentId;
          double c1 = scale * time * time;
          double c0 = scale * time * c1;
-         positionJacobian.set(0, startIndex, c0);
-         positionJacobian.set(0, startIndex + 1, c1);
-         positionJacobian.set(1, startIndex + 2, c0);
-         positionJacobian.set(1, startIndex + 3, c1);
-         positionJacobian.set(2, startIndex + 4, c0);
-         positionJacobian.set(2, startIndex + 5, c1);
+         add(positionJacobianToPack, 0, startIndex, c0);
+         add(positionJacobianToPack, 0, startIndex + 1, c1);
+         add(positionJacobianToPack, 1, startIndex + 2, c0);
+         add(positionJacobianToPack, 1, startIndex + 3, c1);
+         add(positionJacobianToPack, 2, startIndex + 4, c0);
+         add(positionJacobianToPack, 2, startIndex + 5, c1);
       }
    }
 
-   public static void calculateVelocityJacobian(int segmentId, double time, DMatrix velocityJacobian, double scale)
+   public static void calculateVelocityJacobian(int segmentId, double time, DMatrix velocityJacobianToPack, double scale)
    {
       if (!MathTools.epsilonEquals(time, 0.0, 1e-5))
       {
          int startIndex = MPCIndexHandler.comCoefficientsPerSegment * segmentId;
          double c0 = 3.0 * scale * time * time;
          double c1 = 2.0 * scale * time;
-         velocityJacobian.set(0, startIndex, c0);
-         velocityJacobian.set(0, startIndex + 1, c1);
-         velocityJacobian.set(1, startIndex + 2, c0);
-         velocityJacobian.set(1, startIndex + 3, c1);
-         velocityJacobian.set(2, startIndex + 4, c0);
-         velocityJacobian.set(2, startIndex + 5, c1);
+         add(velocityJacobianToPack, 0, startIndex, c0);
+         add(velocityJacobianToPack, 0, startIndex + 1, c1);
+         add(velocityJacobianToPack, 1, startIndex + 2, c0);
+         add(velocityJacobianToPack, 1, startIndex + 3, c1);
+         add(velocityJacobianToPack, 2, startIndex + 4, c0);
+         add(velocityJacobianToPack, 2, startIndex + 5, c1);
       }
    }
 
-   public static void calculateAccelerationJacobian(int segmentId, double time, DMatrix accelerationJacobian, double scale)
+   public static void calculateAccelerationJacobian(int segmentId, double time, DMatrix accelerationJacobianToPack, double scale)
    {
       int startIndex = MPCIndexHandler.comCoefficientsPerSegment * segmentId;
       double c1 = 2.0 * scale;
-      accelerationJacobian.set(0, startIndex + 1, c1);
-      accelerationJacobian.set(1, startIndex + 3, c1);
-      accelerationJacobian.set(2, startIndex + 5, c1);
+      add(accelerationJacobianToPack, 0, startIndex + 1, c1);
+      add(accelerationJacobianToPack, 1, startIndex + 3, c1);
+      add(accelerationJacobianToPack, 2, startIndex + 5, c1);
 
       if (!MathTools.epsilonEquals(time, 0.0, 1e-5))
       {
          double c0 = 6.0 * time * scale;
-         accelerationJacobian.set(0, startIndex, c0);
-         accelerationJacobian.set(1, startIndex + 2, c0);
-         accelerationJacobian.set(2, startIndex + 4, c0);
+         add(accelerationJacobianToPack, 0, startIndex, c0);
+         add(accelerationJacobianToPack, 1, startIndex + 2, c0);
+         add(accelerationJacobianToPack, 2, startIndex + 4, c0);
       }
+   }
+
+   public static void calculateJerkJacobian(int segmentId, DMatrix jerkJacobianToPack, double scale)
+   {
+      int startIndex = MPCIndexHandler.comCoefficientsPerSegment * segmentId;
+
+      double c0 = 6.0 * scale;
+      add(jerkJacobianToPack, 0, startIndex, c0);
+      add(jerkJacobianToPack, 1, startIndex + 2, c0);
+      add(jerkJacobianToPack, 2, startIndex + 4, c0);
+   }
+
+   private static void add(DMatrix matrixToPack, int row, int col, double value)
+   {
+      matrixToPack.set(row, col, value + matrixToPack.get(row, col));
    }
 }
