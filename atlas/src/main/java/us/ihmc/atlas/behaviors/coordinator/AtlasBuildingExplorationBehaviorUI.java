df9580bdf2b07be42028f840084db566396b7eaf
Duncan Calvert <dcalvert@ihmc.us>
2020-11-19 12:43:03 -0600

    Decouple building exploration behavior from it's UI.

--- atlas/src/main/java/us/ihmc/atlas/behaviors/coordinator/AtlasBuildingExplorationBehaviorUI.java
+++ atlas/src/main/java/us/ihmc/atlas/behaviors/coordinator/AtlasBuildingExplorationBehaviorUI.java
@@ -8,18 +8,27 @@ import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.exception.ExceptionTools;
 import us.ihmc.communication.CommunicationMode;
 import us.ihmc.communication.ROS2Tools;
+import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.humanoidBehaviors.BehaviorModule;
 import us.ihmc.humanoidBehaviors.BehaviorRegistry;
 import us.ihmc.humanoidBehaviors.IHMCHumanoidBehaviorManager;
 import us.ihmc.humanoidBehaviors.RemoteBehaviorInterface;
 import us.ihmc.humanoidBehaviors.demo.BuildingExplorationBehaviorAPI;
+import us.ihmc.humanoidBehaviors.demo.BuildingExplorationBehaviorCoordinator;
+import us.ihmc.humanoidBehaviors.demo.BuildingExplorationStateName;
 import us.ihmc.humanoidBehaviors.ui.behaviors.coordinator.BuildingExplorationBehaviorUI;
 import us.ihmc.javaFXToolkit.messager.SharedMemoryJavaFXMessager;
 import us.ihmc.javafx.applicationCreator.JavaFXApplicationCreator;
+import us.ihmc.log.LogTools;
 import us.ihmc.messager.Messager;
 import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
 import us.ihmc.ros2.ROS2Node;
 import us.ihmc.sensorProcessing.parameters.HumanoidRobotSensorInformation;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static us.ihmc.humanoidBehaviors.demo.BuildingExplorationBehaviorAPI.*;
+import static us.ihmc.humanoidBehaviors.demo.BuildingExplorationBehaviorAPI.ConfirmDoor;
 
 public class AtlasBuildingExplorationBehaviorUI
 {
@@ -62,6 +71,28 @@ public class AtlasBuildingExplorationBehaviorUI
 
          ExceptionTools.handle(() ->
          {
+            BuildingExplorationBehaviorCoordinator behaviorCoordinator = new BuildingExplorationBehaviorCoordinator(robotModel,
+                                                                                                                    ros2Node,
+                                                                                                                    behaviorMessager);
+            AtomicReference<Point3D> goal = messager.createInput(Goal);
+
+            messager.registerTopicListener(RequestedState, behaviorCoordinator::requestState);
+            AtomicReference<BuildingExplorationStateName> requestedState = messager.createInput(RequestedState);
+
+            messager.registerTopicListener(Start, s ->
+            {
+               LogTools.debug("Start requested in UI... starting behavior coordinator");
+               behaviorCoordinator.setBombPosition(goal.get());
+               behaviorCoordinator.requestState(requestedState.get());
+               behaviorCoordinator.start();
+            });
+            messager.registerTopicListener(Stop, s -> behaviorCoordinator.stop());
+            behaviorCoordinator.setStateChangedCallback(newState -> messager.submitMessage(CurrentState, newState));
+            behaviorCoordinator.setDebrisDetectedCallback(() -> messager.submitMessage(DebrisDetected, true));
+            behaviorCoordinator.setStairsDetectedCallback(() -> messager.submitMessage(StairsDetected, true));
+            behaviorCoordinator.setDoorDetectedCallback(() -> messager.submitMessage(DoorDetected, true));
+            messager.registerTopicListener(IgnoreDebris, ignore -> behaviorCoordinator.ignoreDebris());
+            messager.registerTopicListener(ConfirmDoor, confirm -> behaviorCoordinator.proceedWithDoorBehavior());
             BuildingExplorationBehaviorUI ui = new BuildingExplorationBehaviorUI(stage, messager, robotModel, ros2Node, behaviorMessager);
             ui.show();
          }, DefaultExceptionHandler.RUNTIME_EXCEPTION);
