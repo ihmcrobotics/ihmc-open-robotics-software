639667dae5e0fe53b94495012493b68766e1df2d
Robert <rgriffin@ihmc.us>
2020-11-19 14:56:47 -0600

    started working on acceleration and jerk integration matrices

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/CoefficientJacobianMatrixHelper.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/CoefficientJacobianMatrixHelper.java
@@ -12,6 +12,8 @@ public class CoefficientJacobianMatrixHelper
    private final DMatrixRMaj positionJacobianMatrix;
    private final DMatrixRMaj velocityJacobianMatrix;
    private final DMatrixRMaj accelerationJacobianMatrix;
+   private final DMatrixRMaj accelerationIntegrationHessian;
+   private final DMatrixRMaj jerkIntegrationHessian;
 
    private int rhoSize;
    private int coefficientsSize;
@@ -29,6 +31,8 @@ public class CoefficientJacobianMatrixHelper
       positionJacobianMatrix = new DMatrixRMaj(rhoSize, coefficientsSize);
       velocityJacobianMatrix = new DMatrixRMaj(rhoSize, coefficientsSize);
       accelerationJacobianMatrix = new DMatrixRMaj(rhoSize, coefficientsSize);
+      accelerationIntegrationHessian = new DMatrixRMaj(coefficientsSize, coefficientsSize);
+      jerkIntegrationHessian = new DMatrixRMaj(coefficientsSize, coefficientsSize);
    }
 
    public void reshape(int numberOfContactPointsInContact)
@@ -38,6 +42,8 @@ public class CoefficientJacobianMatrixHelper
       positionJacobianMatrix.reshape(rhoSize, coefficientsSize);
       velocityJacobianMatrix.reshape(rhoSize, coefficientsSize);
       accelerationJacobianMatrix.reshape(rhoSize, coefficientsSize);
+      accelerationIntegrationHessian.reshape(coefficientsSize, coefficientsSize);
+      jerkIntegrationHessian.reshape(coefficientsSize, coefficientsSize);
    }
 
    public int getCoefficientSize()
@@ -62,7 +68,7 @@ public class CoefficientJacobianMatrixHelper
       double t2 = timeOfContact * timeOfContact;
       double t3 = timeOfContact * t2;
       double positiveExponential = Math.exp(omega * timeOfContact);
-      double negativeExponential = Math.exp(-omega * timeOfContact);
+      double negativeExponential = 1.0 / positiveExponential;
       double firstVelocityCoefficient = omega * positiveExponential;
       double secondVelocityCoefficient = -omega * negativeExponential;
       double firstAccelerationCoefficient = omega * firstVelocityCoefficient;
@@ -135,4 +141,97 @@ public class CoefficientJacobianMatrixHelper
    {
       return accelerationJacobianMatrix;
    }
+
+   public void computeAccelerationIntegrationMatrix(double duration, double omega)
+   {
+      int rhoIndex = 0;
+
+      double positiveExponential = Math.exp(omega * duration);
+      double positiveExponential2 = positiveExponential * positiveExponential;
+      double negativeExponential = 1.0 / positiveExponential;
+      double negativeExponential2 = negativeExponential * negativeExponential;
+      double duration2 = duration * duration;
+      double duration3 = duration * duration2;
+      double omega2 = omega * omega;
+      double omega3 = omega * omega2;
+      double omega4 = omega2 * omega2;
+      double c00 = omega3 / 2.0 * (positiveExponential2 - 1.0);
+      double c01 = omega4 * duration;
+      double c02 = 6.0 * (positiveExponential * (omega * duration - 1.0) + 1.0);
+      double c03 = 2.0 * omega * (positiveExponential - 1.0);
+      double c11 = -omega3 / 2.0 * (negativeExponential2 - 1.0);
+      double c12 = -6.0 * (negativeExponential * (omega * duration + 1.0) - 1.0);
+      double c13 = -2.0 * omega * (negativeExponential - 1.0);
+      double c22 = 12.0 * duration3;
+      double c23 = 6.0 * duration2;
+      double c33 = 4.0 * duration;
+
+      for (int contactPointIndex = 0; contactPointIndex < numberOfContactPointsInContact; contactPointIndex++)
+      {
+         for (int basisVectorIndex = 0; basisVectorIndex < numberOfBasisVectorsPerContactPoint; basisVectorIndex++)
+         {
+            int startColumn = rhoIndex * MPCIndexHandler.coefficientsPerRho;
+
+            accelerationIntegrationHessian.set(startColumn, startColumn, c00);
+            accelerationIntegrationHessian.set(startColumn, startColumn + 1, c01);
+            accelerationIntegrationHessian.set(startColumn, startColumn + 2, c02);
+            accelerationIntegrationHessian.set(startColumn, startColumn + 2, c03);
+            accelerationIntegrationHessian.set(startColumn + 1, startColumn, c01);
+            accelerationIntegrationHessian.set(startColumn + 1, startColumn + 1, c11);
+            accelerationIntegrationHessian.set(startColumn + 1, startColumn + 2, c12);
+            accelerationIntegrationHessian.set(startColumn + 1, startColumn + 3, c13);
+            accelerationIntegrationHessian.set(startColumn + 2, startColumn, c02);
+            accelerationIntegrationHessian.set(startColumn + 2, startColumn + 1, c12);
+            accelerationIntegrationHessian.set(startColumn + 2, startColumn + 2, c22);
+            accelerationIntegrationHessian.set(startColumn + 2, startColumn + 3, c23);
+            accelerationIntegrationHessian.set(startColumn + 3, startColumn, c03);
+            accelerationIntegrationHessian.set(startColumn + 3, startColumn + 1, c13);
+            accelerationIntegrationHessian.set(startColumn + 3, startColumn + 2, c23);
+            accelerationIntegrationHessian.set(startColumn + 3, startColumn + 3, c33);
+
+            rhoIndex++;
+         }
+      }
+   }
+
+   public void computeJerkIntegrationMatrix(double duration, double omega)
+   {
+      int rhoIndex = 0;
+
+      double positiveExponential = Math.exp(omega * duration);
+      double positiveExponential2 = positiveExponential * positiveExponential;
+      double negativeExponential = 1.0 / positiveExponential;
+      double negativeExponential2 = negativeExponential * negativeExponential;
+      double omega2 = omega * omega;
+      double omega3 = omega * omega2;
+      double omega5 = omega2 * omega3;
+      double omega6 = omega3 * omega3;
+      double c00 = omega5 / 2.0 * (positiveExponential2 - 1.0);
+      double c01 = omega6 * duration;
+      double c02 = 6.0 * omega2 * (positiveExponential - 1.0);
+      double c11 = -omega5 / 2.0 * (negativeExponential2 - 1.0);
+      double c12 = 6.0 * omega2 * (negativeExponential  - 1.0);
+      double c22 = 36.0 * duration;
+
+      for (int contactPointIndex = 0; contactPointIndex < numberOfContactPointsInContact; contactPointIndex++)
+      {
+         for (int basisVectorIndex = 0; basisVectorIndex < numberOfBasisVectorsPerContactPoint; basisVectorIndex++)
+         {
+            int startColumn = rhoIndex * MPCIndexHandler.coefficientsPerRho;
+
+            jerkIntegrationHessian.set(startColumn, startColumn, c00);
+            jerkIntegrationHessian.set(startColumn, startColumn + 1, c01);
+            jerkIntegrationHessian.set(startColumn, startColumn + 2, c02);
+            jerkIntegrationHessian.set(startColumn + 1, startColumn, c01);
+            jerkIntegrationHessian.set(startColumn + 1, startColumn + 1, c11);
+            jerkIntegrationHessian.set(startColumn + 1, startColumn + 2, c12);
+            jerkIntegrationHessian.set(startColumn + 2, startColumn, c02);
+            jerkIntegrationHessian.set(startColumn + 2, startColumn + 1, c12);
+            jerkIntegrationHessian.set(startColumn + 2, startColumn + 2, c22);
+
+            rhoIndex++;
+         }
+      }
+   }
+
 }
