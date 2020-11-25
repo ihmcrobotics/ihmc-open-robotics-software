359cad5c5adb8a90219c40caa96faa09f4b93464
Duncan Calvert <dcalvert@ihmc.us>
2020-11-25 13:49:32 -0600

    :arrow_up: ihmc-build 0.23.0, ihmc-ci 7.3, ihmc-cd 1.17, ihmc-commons 0.30.4

--- ihmc-robot-builder/build.gradle.kts
+++ ihmc-robot-builder/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-   id("us.ihmc.ihmc-build") version "0.22.0"
-   id("us.ihmc.ihmc-ci") version "6.8"
-   id("us.ihmc.ihmc-cd") version "1.14"
+   id("us.ihmc.ihmc-build") version "0.23.0"
+   id("us.ihmc.ihmc-ci") version "7.3"
+   id("us.ihmc.ihmc-cd") version "1.17"
    id("us.ihmc.log-tools-plugin") version "0.5.0"
 }
 
