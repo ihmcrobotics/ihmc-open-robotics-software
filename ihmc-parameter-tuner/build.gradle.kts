8606d43657270421684097d27ced9a642ef3125a
Duncan Calvert <dcalvert@ihmc.us>
2020-11-27 12:00:44 -0600

    Support for Gradle 6 while maintaining backwards compatibility with Gradle 5.

--- ihmc-parameter-tuner/build.gradle.kts
+++ ihmc-parameter-tuner/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-   id("us.ihmc.ihmc-build") version "0.23.0"
+   id("us.ihmc.ihmc-build")
    id("us.ihmc.ihmc-ci") version "7.3"
    id("us.ihmc.ihmc-cd") version "1.17"
    id("us.ihmc.log-tools-plugin") version "0.5.0"
