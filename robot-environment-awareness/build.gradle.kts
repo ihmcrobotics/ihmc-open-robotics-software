7d0e7bdb74f2d3b478fe5a35159dd7fceae7df35
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-03 10:32:23 -0600

    :arrow_up: ihmc-jmonkey-engine-toolkit 0.19.2

--- robot-environment-awareness/build.gradle.kts
+++ robot-environment-awareness/build.gradle.kts
@@ -64,7 +64,7 @@ applicationDependencies {
    api(ihmc.sourceSetProject("main"))
 
    api("us.ihmc:ihmc-ros-tools:source")
-   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.1")
+   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.2")
    api("us.ihmc:simulation-construction-set:0.21.1")
    api("us.ihmc:simulation-construction-set-tools:source")
 }
