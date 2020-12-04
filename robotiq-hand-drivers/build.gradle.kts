014809f232c11a6b59f0b84c82d4e79efae7e5a6
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-04 10:26:11 -0600

    :arrow_up: simulation-construction-set 0.21.4

--- robotiq-hand-drivers/build.gradle.kts
+++ robotiq-hand-drivers/build.gradle.kts
@@ -19,7 +19,7 @@ mainDependencies {
    api("commons-collections:commons-collections:3.2.1")
 
    api("us.ihmc:ihmc-yovariables:0.9.7")
-   api("us.ihmc:simulation-construction-set:0.21.3")
+   api("us.ihmc:simulation-construction-set:0.21.4")
    api("us.ihmc:ihmc-graphics-description:0.19.2")
    api("us.ihmc:ihmc-humanoid-robotics:source")
    api("us.ihmc:ihmc-avatar-interfaces:source")
