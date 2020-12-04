923b4365a6d361919f313f4f30f1c6835256c3a9
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-04 10:12:05 -0600

    :arrow_up: simulation-construction-set 0.21.3

--- ihmc-common-walking-control-modules/build.gradle.kts
+++ ihmc-common-walking-control-modules/build.gradle.kts
@@ -49,5 +49,5 @@ testDependencies {
 
 visualizersDependencies {
    api(ihmc.sourceSetProject("main"))
-   api("us.ihmc:simulation-construction-set:0.21.2")
+   api("us.ihmc:simulation-construction-set:0.21.3")
 }
--- valkyrie/build.gradle.kts
+++ valkyrie/build.gradle.kts
@@ -36,7 +36,7 @@ mainDependencies {
       setChanging(true)
    }
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.2")
-   api("us.ihmc:simulation-construction-set:0.21.2")
+   api("us.ihmc:simulation-construction-set:0.21.3")
    api("us.ihmc:ihmc-graphics-description:0.19.2")
    api("us.ihmc:ihmc-robot-description:0.20.2")
    api("us.ihmc:ihmc-communication:source")
