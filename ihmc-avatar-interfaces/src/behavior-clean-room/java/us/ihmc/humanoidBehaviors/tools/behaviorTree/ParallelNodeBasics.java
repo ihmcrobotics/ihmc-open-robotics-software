8663d302b344f9d9cf65a63bae91483a50b09bde
Duncan Calvert <dcalvert@ihmc.us>
2020-12-09 20:37:55 -0600

    Add doc to ParallelNodeBasics.

--- ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/behaviorTree/ParallelNodeBasics.java
+++ ihmc-avatar-interfaces/src/behavior-clean-room/java/us/ihmc/humanoidBehaviors/tools/behaviorTree/ParallelNodeBasics.java
@@ -6,6 +6,17 @@ import us.ihmc.log.LogTools;
 import static us.ihmc.humanoidBehaviors.tools.behaviorTree.BehaviorTreeNodeStatus.RUNNING;
 import static us.ihmc.humanoidBehaviors.tools.behaviorTree.BehaviorTreeNodeStatus.SUCCESS;
 
+/**
+ * A Technique for Parallelism with Behavior Trees
+ *
+ * A node starts on its task in a new Thread the first time it is ticked.
+ * The node returns running until the task is complete.
+ * When the task is complete, the node returns success or failure until it is reset.
+ * A node runs its task while it is being constantly ticked.
+ * An additional clock() method is used by a node to determine that the tree has gone elsewhere and that it should abort its task and reset.
+ *
+ * TODO: Kill the task as soon as we aren't getting ticked.
+ */
 public abstract class ParallelNodeBasics implements BehaviorTreeNode
 {
    private boolean hasStarted = false;
