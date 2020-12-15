4955991ca6e8414215aff58c1533582a97eb5f43
Valkyrie <valkyrie@nasa.gov>
2020-12-15 10:58:03 -0600

    Added param override for Atlas and added launchers to tuner.

--- atlas/src/test/java/us/ihmc/atlas/AtlasFlatGroundFastWalkingTest.java
+++ atlas/src/test/java/us/ihmc/atlas/AtlasFlatGroundFastWalkingTest.java
@@ -1,13 +1,25 @@
 package us.ihmc.atlas;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FilenameUtils;
 import org.junit.jupiter.api.Test;
 
+import javafx.application.Application;
 import us.ihmc.avatar.AvatarFlatGroundFastWalkingTest;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.parameterTuner.guiElements.main.ParameterGuiInterface;
+import us.ihmc.parameterTuner.guiElements.main.ParameterTuningApplication;
+import us.ihmc.parameterTuner.offline.FileInputManager;
 import us.ihmc.simulationConstructionSetTools.util.HumanoidFloatingRootJointRobot;
 
 public class AtlasFlatGroundFastWalkingTest extends AvatarFlatGroundFastWalkingTest
 {
+   private static final String FAST_WALKING_PARAMETERS_XML = "/us/ihmc/atlas/fast_walking_parameters.xml";
+
    @Override
    public DRCRobotModel getRobotModel()
    {
@@ -17,6 +29,12 @@ public class AtlasFlatGroundFastWalkingTest extends AvatarFlatGroundFastWalkingT
          public HumanoidFloatingRootJointRobot createHumanoidFloatingRootJointRobot(boolean createCollisionMeshes, boolean enableJointDamping)
          {
             return super.createHumanoidFloatingRootJointRobot(createCollisionMeshes, false);
+         }
+
+         @Override
+         public InputStream getParameterOverwrites()
+         {
+            return AtlasFlatGroundFastWalkingTest.class.getResourceAsStream(FAST_WALKING_PARAMETERS_XML);
          }
 
          @Override
@@ -50,5 +68,22 @@ public class AtlasFlatGroundFastWalkingTest extends AvatarFlatGroundFastWalkingT
    public void testForwardWalking() throws Exception
    {
       super.testForwardWalking();
+   }
+
+   public static void main(String[] args)
+   {
+      Application.launch(AtlasFastWalkingTunerOffline.class, args);
+   }
+
+   public static class AtlasFastWalkingTunerOffline extends ParameterTuningApplication
+   {
+      @Override
+      protected ParameterGuiInterface createInputManager()
+      {
+         System.out.println(new File("."));
+         String relativeFilePath = FilenameUtils.separatorsToSystem(FAST_WALKING_PARAMETERS_XML);
+         Path filePath = Paths.get("resources", relativeFilePath);
+         return new FileInputManager(filePath.toFile());
+      }
    }
 }
