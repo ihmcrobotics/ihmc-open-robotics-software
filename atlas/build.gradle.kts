9c5adddf8297f21b1412d9b42daa8f87247ef8ac
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-18 17:30:11 -0600

    :arrow_up: ihmc-robot-description 0.21.1

--- atlas/build.gradle.kts
+++ atlas/build.gradle.kts
@@ -41,7 +41,7 @@ mainDependencies {
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.4")
    api("us.ihmc:simulation-construction-set:0.21.4")
    api("us.ihmc:ihmc-graphics-description:0.19.3")
-   api("us.ihmc:ihmc-robot-description:0.21.0")
+   api("us.ihmc:ihmc-robot-description:0.21.1")
    api("us.ihmc:ihmc-javafx-toolkit:0.19.2")
    api("us.ihmc:ihmc-humanoid-behaviors:source")
    api("us.ihmc:ihmc-mocap:source")
