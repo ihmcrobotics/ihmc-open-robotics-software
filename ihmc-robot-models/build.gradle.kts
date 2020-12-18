89e58bfce9316cdf62b7c24690fc0873331900d8
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-18 17:30:11 -0600

    :arrow_up: ihmc-robot-description 0.21.1

--- ihmc-robot-models/build.gradle.kts
+++ ihmc-robot-models/build.gradle.kts
@@ -18,7 +18,7 @@ mainDependencies {
    api("com.google.guava:guava:18.0")
 
    api("us.ihmc:ihmc-yovariables:0.9.8")
-   api("us.ihmc:ihmc-robot-description:0.21.0")
+   api("us.ihmc:ihmc-robot-description:0.21.1")
    api("us.ihmc:ihmc-graphics-description:0.19.3")
    api("us.ihmc:ihmc-robotics-toolkit:source")
 }
