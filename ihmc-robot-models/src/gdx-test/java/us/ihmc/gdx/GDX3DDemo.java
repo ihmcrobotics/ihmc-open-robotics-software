4f68d765560b238a4355cfb0fd686ad274b76216
Duncan Calvert <dcalvert@ihmc.us>
2020-12-19 22:25:52 -0600

    Get some buttons on a panel.

--- ihmc-robot-models/src/gdx-test/java/us/ihmc/gdx/GDX3DDemo.java
+++ ihmc-robot-models/src/gdx-test/java/us/ihmc/gdx/GDX3DDemo.java
@@ -3,14 +3,22 @@ package us.ihmc.gdx;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.DirectionalLightsAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.profiling.GLProfiler;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
@@ -28,6 +36,10 @@ public class GDX3DDemo extends Lwjgl3ApplicationAdapter
    private ModelInstance coordinateFrame;
 
    private OrthographicCamera camera2D;
+   private int currentWindowWidth;
+   private int currentWindowHeight;
+   private Stage stage;
+   private Table table;
 
    public GDX3DDemo()
    {
@@ -54,19 +66,68 @@ public class GDX3DDemo extends Lwjgl3ApplicationAdapter
       modelBatch = new ModelBatch();
 
       camera3D = new FocusBasedGDXCamera();
-      viewport = new ExtendViewport(INITIAL_WIDTH, INITIAL_HEIGHT, camera3D);
+      viewport = new ScreenViewport(camera3D);
+
 
       coordinateFrame = new ModelInstance(GDXModelPrimitives.createCoordinateFrame(0.3));
       boxes = new BoxesDemoModel().newInstance();
 
-//      camera2D.position.set(camera3D.viewportWidth / 3f, camera3D.viewportHeight / 3f, 0);
-//      camera2D.update();
+      stage = new Stage(new ScreenViewport());
+//      Gdx.input.setInputProcessor(stage);
+
+      table = new Table();
+      table.setFillParent(true);
+      table.setDebug(true);
+      stage.addActor(table);
+
+      Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+//      Skin skin = new Skin();
+//
+//      // Generate a 1x1 white texture and store it in the skin named "white".
+//      Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+//      pixmap.setColor(Color.WHITE);
+//      pixmap.fill();
+//      skin.add("white", new Texture(pixmap));
+//
+//      // Store the default libgdx font under the name "default".
+//      skin.add("default", new BitmapFont());
+//
+//      TextButtonStyle style = new TextButtonStyle();
+//      style.up = skin.newDrawable("white", Color.DARK_GRAY);
+//      style.down = skin.newDrawable("white", Color.DARK_GRAY);
+//      style.checked = skin.newDrawable("white", Color.BLUE);
+//      style.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+//      style.font = skin.getFont("default");
+//      skin.add("default", style);
+
+//      TextureRegion upRegion = ...
+//      TextureRegion downRegion = ...
+//      BitmapFont buttonFont = ...
+//      TextButtonStyle style = new TextButtonStyle();
+//      style.up = new TextureRegionDrawable(upRegion);
+//      style.down = new TextureRegionDrawable(downRegion);
+//      style.font = buttonFont;
+
+      TextButton button1 = new TextButton("Button 1", skin);
+      table.add(button1);
+
+      TextButton button2 = new TextButton("Button 2", skin);
+      table.add(button2);
+
+//      table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
+
+//      table.setSize();
+
+      camera2D = new OrthographicCamera(INITIAL_WIDTH, INITIAL_HEIGHT / 4f);
+      camera2D.position.set(camera3D.viewportWidth / 3f, camera3D.viewportHeight / 3f, 0);
+      camera2D.update();
    }
 
    @Override
    public void resize(int width, int height)
    {
-      viewport.update(width, height);
+      this.currentWindowWidth = width;
+      this.currentWindowHeight = height;
    }
 
    @Override
@@ -74,8 +135,10 @@ public class GDX3DDemo extends Lwjgl3ApplicationAdapter
    {
       Gdx.gl.glClearColor(0.5019608f, 0.5019608f, 0.5019608f, 1.0f);
       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
       Gdx.gl.glEnable(GL20.GL_TEXTURE_2D);
+
+      viewport.update(currentWindowWidth, currentWindowHeight * 3 / 4);
+      Gdx.gl.glViewport(0, Gdx.graphics.getHeight()* 1 / 4, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()* 3 / 4);
 
       camera3D.render();
 
@@ -83,8 +146,13 @@ public class GDX3DDemo extends Lwjgl3ApplicationAdapter
       modelBatch.render(camera3D.getFocusPointSphere(), environment);
       modelBatch.render(boxes, environment);
       modelBatch.render(coordinateFrame, environment);
-      // TODO add more render calls here
       modelBatch.end();
+//
+      Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()* 1 / 4);
+      stage.getViewport().update(currentWindowWidth, currentWindowHeight * 1 / 4, true);
+
+      stage.act(Gdx.graphics.getDeltaTime());
+      stage.draw();
 
 //      camera2D.update();
    }
@@ -96,6 +164,7 @@ public class GDX3DDemo extends Lwjgl3ApplicationAdapter
       coordinateFrame.model.dispose();
       camera3D.dispose();
       modelBatch.dispose();
+      stage.dispose();
    }
 
    @Override
