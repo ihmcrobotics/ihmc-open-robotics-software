1d6c49f169446688ce9a5f12e79fa75e874366d2
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-20 21:16:41 -0600

    :arrow_up: simulation-construction-set 0.21.5

--- robot-environment-awareness/build.gradle.kts
+++ robot-environment-awareness/build.gradle.kts
@@ -60,7 +60,7 @@ applicationDependencies {
 
    api("us.ihmc:ihmc-ros-tools:source")
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.5")
-   api("us.ihmc:simulation-construction-set:0.21.4")
+   api("us.ihmc:simulation-construction-set:0.21.5")
    api("us.ihmc:simulation-construction-set-tools:source")
 }
 
