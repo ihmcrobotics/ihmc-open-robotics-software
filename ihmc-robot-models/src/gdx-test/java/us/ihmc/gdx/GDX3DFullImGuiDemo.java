3b5bcbee95efcda0978b64d6fc748cd9843dd16a
Duncan Calvert <dcalvert@ihmc.us>
2020-12-30 15:25:40 -0600

    Atlas rendering with ImGui window and docking system in the Behavior UI.

--- ihmc-robot-models/src/gdx-test/java/us/ihmc/gdx/GDX3DFullImGuiDemo.java
+++ ihmc-robot-models/src/gdx-test/java/us/ihmc/gdx/GDX3DFullImGuiDemo.java
@@ -11,9 +11,8 @@ public class GDX3DFullImGuiDemo
 {
    private final GDX3DApplication application = new GDX3DApplication();
    private final GDXImGuiWindowAndDockSystem imGui = new GDXImGuiWindowAndDockSystem();
-   private final GDXImGui3DView gdx3DWindow = new GDXImGui3DView();
+   private final ImGuiGDX3DWindow gdx3DWindow = new ImGuiGDX3DWindow();
 
-   private boolean isInitialized = false;
    private final Stopwatch stopwatch = new Stopwatch().start();
 
    public GDX3DFullImGuiDemo()
@@ -35,55 +34,45 @@ public class GDX3DFullImGuiDemo
          public void render()
          {
             application.glClearGrayscale(0.3f);
-
             imGui.beforeWindowManagement();
+            gdx3DWindow.renderBeforeOtherWindows(application);
 
-            gdx3DWindow.render(application);
-
-            ImGui.begin("Window");
-
-            if (ImGui.beginTabBar("main"))
             {
-               if (ImGui.beginTabItem("Window"))
+               ImGui.begin("Window");
+               if (ImGui.beginTabBar("main"))
                {
-                  ImGui.text("Tab bar detected!");
-                  ImGui.endTabItem();
+                  if (ImGui.beginTabItem("Window"))
+                  {
+                     ImGui.text("Tab bar detected!");
+                     ImGui.endTabItem();
+                  }
+                  ImGui.endTabBar();
                }
-               ImGui.endTabBar();
+               ImGui.text(StringTools.format3D("Time: {} s", stopwatch.totalElapsed()).get());
+               ImGui.button("I'm a Button!");
+               float[] values = new float[100];
+               for (int i = 0; i < 100; i++)
+               {
+                  values[i] = i;
+               }
+               ImGui.plotLines("Histogram", values, 100);
+               application.getCamera3D().addInputExclusionBox(ImGuiTools.windowBoundingBox());
+               ImGui.end();
             }
 
-            ImGui.text(StringTools.format3D("Time: {} s", stopwatch.totalElapsed()).get());
-            ImGui.button("I'm a Button!");
-
-            float[] values = new float[100];
-            for (int i = 0; i < 100; i++)
-            {
-               values[i] = i;
-            }
-
-            ImGui.plotLines("Histogram", values, 100);
-
-            application.getCamera3D().addInputExclusionBox(ImGuiTools.windowBoundingBox());
-
-            ImGui.end();
-
-            if (!isInitialized)
+            if (imGui.isFirstRenderCall())
             {
                ImGui.dockBuilderSetNodeSize(imGui.getCentralDockspaceId(), application.getCurrentWindowWidth(), application.getCurrentWindowHeight());
                ImInt outIdAtOppositeDir = new ImInt();
                int dockRightId = ImGui.dockBuilderSplitNode(imGui.getCentralDockspaceId(), ImGuiDir.Right, 0.20f, null, outIdAtOppositeDir);
-               ImGui.dockBuilderDockWindow("3D View", imGui.getCentralDockspaceId());
+               ImGui.dockBuilderDockWindow(gdx3DWindow.getWindowName(), imGui.getCentralDockspaceId());
                ImGui.dockBuilderDockWindow("Window", dockRightId);
                ImGui.dockBuilderFinish(imGui.getCentralDockspaceId());
             }
 
             imGui.afterWindowManagement();
-
             application.render();
-
             imGui.afterGDXRender();
-
-            isInitialized = true;
          }
 
          @Override
