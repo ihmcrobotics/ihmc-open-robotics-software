0dd8bd35f43eb8118818337438c77684482166b7
Duncan Calvert <dcalvert@ihmc.us>
2021-01-05 13:17:07 -0600

    Enable OpenGL 3.2

--- ihmc-robot-models/src/gdx/java/us/ihmc/gdx/GDXApplicationCreator.java
+++ ihmc-robot-models/src/gdx/java/us/ihmc/gdx/GDXApplicationCreator.java
@@ -18,6 +18,7 @@ public class GDXApplicationCreator
       applicationConfiguration.setWindowedMode((int) width, (int) height);
       applicationConfiguration.useVsync(true);
       applicationConfiguration.setBackBufferConfig(8, 8, 8, 8, 16, 0, 4);
+      applicationConfiguration.useOpenGL3(true, 3, 2);
 
       ThreadTools.startAThread(() -> new Lwjgl3Application(application, applicationConfiguration), title);
    }
