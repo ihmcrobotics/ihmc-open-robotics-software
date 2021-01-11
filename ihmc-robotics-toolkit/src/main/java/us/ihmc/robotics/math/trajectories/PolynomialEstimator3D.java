5540eab444b97b2b3a036b61e9636ecda1e6d38c
Robert <rgriffin@ihmc.us>
2021-01-11 16:43:24 -0600

    set up the polynomial estimator in 3D

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/trajectories/PolynomialEstimator3D.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/trajectories/PolynomialEstimator3D.java
@@ -1,5 +1,180 @@
 package us.ihmc.robotics.math.trajectories;
 
-public class PolynomialEstimator3D
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.factory.LinearSolverFactory_DDRM;
+import org.ejml.interfaces.linsol.LinearSolverDense;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.matrixlib.MatrixTools;
+import us.ihmc.robotics.time.TimeIntervalBasics;
+
+public class PolynomialEstimator3D implements Trajectory3DReadOnly, TimeIntervalBasics
 {
+   private static final double regularization = 1e-5;
+   private static final double defaultWeight = 1.0;
+
+   private final Point3DReadOnly position = new Point3DReadOnly()
+   {
+      @Override
+      public double getX()
+      {
+         return xEstimator.getPosition();
+      }
+
+      @Override
+      public double getY()
+      {
+         return yEstimator.getPosition();
+      }
+
+      @Override
+      public double getZ()
+      {
+         return zEstimator.getPosition();
+      }
+   };
+
+   private final Vector3DReadOnly velocity = new Vector3DReadOnly()
+   {
+      @Override
+      public double getX()
+      {
+         return xEstimator.getVelocity();
+      }
+
+      @Override
+      public double getY()
+      {
+         return yEstimator.getVelocity();
+      }
+
+      @Override
+      public double getZ()
+      {
+         return zEstimator.getVelocity();
+      }
+   };
+
+   private final Vector3DReadOnly acceleration = new Vector3DReadOnly()
+   {
+      @Override
+      public double getX()
+      {
+         return xEstimator.getAcceleration();
+      }
+
+      @Override
+      public double getY()
+      {
+         return yEstimator.getAcceleration();
+      }
+
+      @Override
+      public double getZ()
+      {
+         return zEstimator.getAcceleration();
+      }
+   };
+
+   private final PolynomialEstimator xEstimator = new PolynomialEstimator();
+   private final PolynomialEstimator yEstimator = new PolynomialEstimator();
+   private final PolynomialEstimator zEstimator = new PolynomialEstimator();
+
+   @Override
+   public void reset()
+   {
+      xEstimator.reset();
+      yEstimator.reset();
+      zEstimator.reset();
+   }
+
+   public void reshape(int order)
+   {
+      xEstimator.reshape(order);
+      yEstimator.reshape(order);
+      zEstimator.reshape(order);
+   }
+
+   @Override
+   public void setEndTime(double tFinal)
+   {
+      xEstimator.setEndTime(tFinal);
+      yEstimator.setEndTime(tFinal);
+      zEstimator.setEndTime(tFinal);
+   }
+
+   @Override
+   public void setStartTime(double t0)
+   {
+      xEstimator.setStartTime(t0);
+      yEstimator.setStartTime(t0);
+      zEstimator.setStartTime(t0);
+   }
+
+   @Override
+   public double getEndTime()
+   {
+      return xEstimator.getEndTime();
+   }
+
+   @Override
+   public double getStartTime()
+   {
+      return xEstimator.getStartTime();
+   }
+
+   public void addObjectivePosition(double time, Point3DReadOnly value)
+   {
+      addObjectivePosition(defaultWeight, time, value);
+   }
+
+   public void addObjectivePosition(double weight, double time, Point3DReadOnly value)
+   {
+      xEstimator.addObjectivePosition(weight, time, value.getX());
+      yEstimator.addObjectivePosition(weight, time, value.getY());
+      zEstimator.addObjectivePosition(weight, time, value.getZ());
+   }
+
+   public void addObjectiveVelocity(double time, Vector3DReadOnly value)
+   {
+      addObjectiveVelocity(defaultWeight, time, value);
+   }
+
+   public void addObjectiveVelocity(double weight, double time, Vector3DReadOnly value)
+   {
+      xEstimator.addObjectiveVelocity(weight, time, value.getX());
+      yEstimator.addObjectiveVelocity(weight, time, value.getY());
+      zEstimator.addObjectiveVelocity(weight, time, value.getZ());
+   }
+
+   public void solve()
+   {
+      xEstimator.solve();
+      yEstimator.solve();
+      zEstimator.solve();
+   }
+
+   public void compute(double time)
+   {
+      xEstimator.compute(time);
+      yEstimator.compute(time);
+      zEstimator.compute(time);
+   }
+
+   public Point3DReadOnly getPosition()
+   {
+      return position;
+   }
+
+   public Vector3DReadOnly getVelocity()
+   {
+      return velocity;
+   }
+
+   public Vector3DReadOnly getAcceleration()
+   {
+      return acceleration;
+   }
 }
--- ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/math/trajectories/PolynomialEstimatorTest.java
+++ ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/math/trajectories/PolynomialEstimatorTest.java
@@ -38,5 +38,225 @@ public class PolynomialEstimatorTest
       assertEquals(4.0, estimator.getPosition(), epsilon);
       assertEquals(1.0, estimator.getVelocity(), epsilon);
       assertEquals(0.0, estimator.getAcceleration(), epsilon);
+
+      // do the same test, but with a higher weight
+      estimator.reset();
+
+      estimator.addObjectivePosition(10.0, 0.0, 2.0);
+      estimator.addObjectivePosition(10.0, 2.0, 4.0);
+
+      estimator.solve();
+
+      assertEquals(2, estimator.getOrder());
+
+      coefficients = estimator.getCoefficients();
+      assertEquals(2, coefficients.getNumRows());
+      assertEquals(1, coefficients.getNumCols());
+
+      assertEquals(2.0, coefficients.get(0, 0), epsilon);
+      assertEquals(1.0, coefficients.get(1, 0), epsilon);
+
+      estimator.compute(0.0);
+      assertEquals(2.0, estimator.getPosition(), epsilon);
+      assertEquals(1.0, estimator.getVelocity(), epsilon);
+      assertEquals(0.0, estimator.getAcceleration(), epsilon);
+
+      estimator.compute(2.0);
+      assertEquals(4.0, estimator.getPosition(), epsilon);
+      assertEquals(1.0, estimator.getVelocity(), epsilon);
+      assertEquals(0.0, estimator.getAcceleration(), epsilon);
+
+      // do the same test, but with a velocity guess
+      estimator.reset();
+
+      estimator.addObjectivePosition(0.0, 2.0);
+      estimator.addObjectiveVelocity(0.0, 1.0);
+
+      estimator.solve();
+
+      assertEquals(2, estimator.getOrder());
+
+      coefficients = estimator.getCoefficients();
+      assertEquals(2, coefficients.getNumRows());
+      assertEquals(1, coefficients.getNumCols());
+
+      assertEquals(2.0, coefficients.get(0, 0), epsilon);
+      assertEquals(1.0, coefficients.get(1, 0), epsilon);
+
+      estimator.compute(0.0);
+      assertEquals(2.0, estimator.getPosition(), epsilon);
+      assertEquals(1.0, estimator.getVelocity(), epsilon);
+      assertEquals(0.0, estimator.getAcceleration(), epsilon);
+
+      estimator.compute(2.0);
+      assertEquals(4.0, estimator.getPosition(), epsilon);
+      assertEquals(1.0, estimator.getVelocity(), epsilon);
+      assertEquals(0.0, estimator.getAcceleration(), epsilon);
+   }
+
+   @Test
+   public void testCubic()
+   {
+      PolynomialEstimator estimator = new PolynomialEstimator();
+      estimator.reshape(4);
+
+      double duration = 2.0;
+      double x0 = 2.0;
+      double xf = 4.0;
+      double v0 = 2.0;
+      double vf = -2.0;
+      estimator.addObjectivePosition(0.0, x0);
+      estimator.addObjectiveVelocity(0.0, v0);
+      estimator.addObjectivePosition(duration, xf);
+      estimator.addObjectiveVelocity(duration, vf);
+
+      estimator.solve();
+
+      assertEquals(4, estimator.getOrder());
+
+      DMatrixRMaj coefficients = estimator.getCoefficients();
+      assertEquals(4, coefficients.getNumRows());
+      assertEquals(1, coefficients.getNumCols());
+
+      double c0 = x0;
+      double c1 = v0;
+      double c2 = 3.0 / (duration * duration) * (xf - x0) - 1.0 / duration * (2.0 * v0 + vf);
+      double c3 = 2.0 / (duration * duration * duration) * (x0 - xf) + 1.0 / (duration * duration) * (v0 + vf);
+      assertEquals(c0, coefficients.get(0, 0), epsilon);
+      assertEquals(c1, coefficients.get(1, 0), epsilon);
+      assertEquals(c2, coefficients.get(2, 0), epsilon);
+      assertEquals(c3, coefficients.get(3, 0), epsilon);
+
+      double a0 = 2.0 * c2;
+      double af = a0 + 6.0 * duration * c3;
+
+
+      estimator.compute(0.0);
+      assertEquals(x0, estimator.getPosition(), epsilon);
+      assertEquals(v0, estimator.getVelocity(), epsilon);
+      assertEquals(a0, estimator.getAcceleration(), epsilon);
+
+      estimator.compute(2.0);
+      assertEquals(xf, estimator.getPosition(), epsilon);
+      assertEquals(vf, estimator.getVelocity(), epsilon);
+      assertEquals(af, estimator.getAcceleration(), epsilon);
+
+      // do the same test, but with a higher weight
+      estimator.reset();
+
+      estimator.addObjectivePosition(10.0, 0.0, x0);
+      estimator.addObjectiveVelocity(10.0, 0.0, v0);
+      estimator.addObjectivePosition(10.0, duration, xf);
+      estimator.addObjectiveVelocity(10.0, duration, vf);
+
+      estimator.solve();
+
+      assertEquals(4, estimator.getOrder());
+
+      coefficients = estimator.getCoefficients();
+      assertEquals(c0, coefficients.get(0, 0), epsilon);
+      assertEquals(c1, coefficients.get(1, 0), epsilon);
+      assertEquals(c2, coefficients.get(2, 0), epsilon);
+      assertEquals(c3, coefficients.get(3, 0), epsilon);
+
+      estimator.compute(0.0);
+      assertEquals(x0, estimator.getPosition(), epsilon);
+      assertEquals(v0, estimator.getVelocity(), epsilon);
+      assertEquals(a0, estimator.getAcceleration(), epsilon);
+
+      estimator.compute(2.0);
+      assertEquals(xf, estimator.getPosition(), epsilon);
+      assertEquals(vf, estimator.getVelocity(), epsilon);
+      assertEquals(af, estimator.getAcceleration(), epsilon);
+
+      // do the same test, but with some acceleration at the beginning
+      estimator.reset();
+
+      estimator.addObjectivePosition(0.0, x0);
+      estimator.addObjectiveVelocity(0.0, v0);
+      estimator.addObjectiveAcceleration(0.0, a0);
+      estimator.addObjectivePosition(duration, xf);
+
+      estimator.solve();
+
+      assertEquals(4, estimator.getOrder());
+
+      coefficients = estimator.getCoefficients();
+      assertEquals(c0, coefficients.get(0, 0), epsilon);
+      assertEquals(c1, coefficients.get(1, 0), epsilon);
+      assertEquals(c2, coefficients.get(2, 0), epsilon);
+      assertEquals(c3, coefficients.get(3, 0), epsilon);
+
+      estimator.compute(0.0);
+      assertEquals(x0, estimator.getPosition(), epsilon);
+      assertEquals(v0, estimator.getVelocity(), epsilon);
+      assertEquals(a0, estimator.getAcceleration(), epsilon);
+
+      estimator.compute(2.0);
+      assertEquals(xf, estimator.getPosition(), epsilon);
+      assertEquals(vf, estimator.getVelocity(), epsilon);
+      assertEquals(af, estimator.getAcceleration(), epsilon);
+
+      // do the same test, but with some acceleration at the end
+      estimator.reset();
+
+      estimator.addObjectivePosition(0.0, x0);
+      estimator.addObjectiveVelocity(0.0, v0);
+      estimator.addObjectivePosition(duration, xf);
+      estimator.addObjectiveAcceleration(duration, af);
+
+      estimator.solve();
+
+      assertEquals(4, estimator.getOrder());
+
+      coefficients = estimator.getCoefficients();
+      assertEquals(c0, coefficients.get(0, 0), epsilon);
+      assertEquals(c1, coefficients.get(1, 0), epsilon);
+      assertEquals(c2, coefficients.get(2, 0), epsilon);
+      assertEquals(c3, coefficients.get(3, 0), epsilon);
+
+      estimator.compute(0.0);
+      assertEquals(x0, estimator.getPosition(), epsilon);
+      assertEquals(v0, estimator.getVelocity(), epsilon);
+      assertEquals(a0, estimator.getAcceleration(), epsilon);
+
+      estimator.compute(2.0);
+      assertEquals(xf, estimator.getPosition(), epsilon);
+      assertEquals(vf, estimator.getVelocity(), epsilon);
+      assertEquals(af, estimator.getAcceleration(), epsilon);
+   }
+
+   @Test
+   public void testQuartic()
+   {
+      PolynomialEstimator estimator = new PolynomialEstimator();
+      estimator.reshape(5);
+
+      double duration = 2.0;
+      double x0 = 2.0;
+      double xf = 4.0;
+      double v0 = 2.0;
+      double vf = -2.0;
+      estimator.addObjectivePosition(0.0, x0);
+      estimator.addObjectiveVelocity(0.0, v0);
+      estimator.addObjectivePosition(duration, xf);
+      estimator.addObjectiveVelocity(duration, vf);
+
+      estimator.solve();
+
+      assertEquals(5, estimator.getOrder());
+
+      DMatrixRMaj coefficients = estimator.getCoefficients();
+      assertEquals(5, coefficients.getNumRows());
+      assertEquals(1, coefficients.getNumCols());
+
+      estimator.compute(0.0);
+      assertEquals(x0, estimator.getPosition(), epsilon);
+      assertEquals(v0, estimator.getVelocity(), epsilon);
+
+      estimator.compute(2.0);
+      assertEquals(xf, estimator.getPosition(), epsilon);
+      assertEquals(vf, estimator.getVelocity(), epsilon);
+
    }
 }
