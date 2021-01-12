cf768095c889ae2fa593b2767e055bed51e24111
Duncan Calvert <dcalvert@ihmc.us>
2021-01-12 14:49:26 -0600

    Fix ros 1 dependencies.

--- ihmc-ros-tools/src/test/java/us/ihmc/utilities/ros/AddTwoIntsServer.java
+++ ihmc-ros-tools/src/test/java/us/ihmc/utilities/ros/AddTwoIntsServer.java
@@ -1,26 +1,21 @@
-package us.ihmc.utilities.ros.service;
+package us.ihmc.utilities.ros;
 
 import org.ros.exception.ServiceException;
 
 import test_rosmaster.AddTwoIntsResponse;
 import test_rosmaster.AddTwoInts;
 import test_rosmaster.AddTwoIntsRequest;
-import us.ihmc.utilities.ros.RosServiceServer;
 
 public class AddTwoIntsServer extends RosServiceServer<AddTwoIntsRequest, AddTwoIntsResponse>
 {
-
    public AddTwoIntsServer()
    {
       super(AddTwoInts._TYPE);
    }
-   
 
    @Override
    public void build(AddTwoIntsRequest request, AddTwoIntsResponse response) throws ServiceException
    {
       response.setSum(request.getA()+request.getB());
-      
    }
-
 }
