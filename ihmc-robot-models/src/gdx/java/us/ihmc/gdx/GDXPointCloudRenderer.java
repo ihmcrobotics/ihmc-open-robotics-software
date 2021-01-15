a4fb586e8e03820ca1e08734113f9078a89fbad8
Duncan Calvert <dcalvert@ihmc.us>
2021-01-14 18:53:43 -0600

    Make some progress on GPU depth sensor.

--- ihmc-robot-models/src/gdx/java/us/ihmc/gdx/GDXPointCloudRenderer.java
+++ ihmc-robot-models/src/gdx/java/us/ihmc/gdx/GDXPointCloudRenderer.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import us.ihmc.commons.lists.RecyclingArrayList;
 import us.ihmc.euclid.tuple3D.Point3D32;
 
 public class GDXPointCloudRenderer implements RenderableProvider
@@ -26,9 +27,9 @@ public class GDXPointCloudRenderer implements RenderableProvider
    private final int CPU_COLOR_OFFSET = (short) (CPU_ATTRIBUTES.findByUsage(VertexAttributes.Usage.ColorUnpacked).offset / 4);
    private final int CPU_SIZE_AND_ROTATION_OFFSET = (short) (CPU_ATTRIBUTES.findByUsage(sizeAndRotationUsage).offset / 4);
 
-   private Point3D32[] pointsToRender;
+   private RecyclingArrayList<Point3D32> pointsToRender;
 
-   public void create()
+   public void create(int size)
    {
       renderable = new Renderable();
       renderable.meshPart.primitiveType = GL20.GL_POINTS;
@@ -36,32 +37,32 @@ public class GDXPointCloudRenderer implements RenderableProvider
       renderable.material = new Material(ColorAttribute.createDiffuse(Color.WHITE));
 
       int initialCapacity = 10;
-      vertices = new float[initialCapacity * CPU_VERTEX_SIZE];
+      vertices = new float[size * CPU_VERTEX_SIZE];
       if (renderable.meshPart.mesh != null) renderable.meshPart.mesh.dispose();
-      renderable.meshPart.mesh = new Mesh(false, initialCapacity, 0, CPU_ATTRIBUTES);
+      renderable.meshPart.mesh = new Mesh(false, size, 0, CPU_ATTRIBUTES);
    }
 
-   public void setPointsToRender(Point3D32[] pointsToRender)
+   public void setPointsToRender(RecyclingArrayList<Point3D32> pointsToRender)
    {
       this.pointsToRender = pointsToRender;
-      if (pointsToRender.length > vertices.length)
-      {
-         vertices = new float[pointsToRender.length * CPU_VERTEX_SIZE];
-         renderable.meshPart.mesh = new Mesh(false, pointsToRender.length, 0, CPU_ATTRIBUTES);
-      }
+//      if (pointsToRender.size() > vertices.length)
+//      {
+//         vertices = new float[pointsToRender.size() * CPU_VERTEX_SIZE];
+//         renderable.meshPart.mesh = new Mesh(false, pointsToRender.size(), 0, CPU_ATTRIBUTES);
+//      }
    }
 
    public void render()
    {
-      if (pointsToRender != null)
+      if (pointsToRender != null && !pointsToRender.isEmpty())
       {
-         for (int i = 0; i < pointsToRender.length; i++)
+         for (int i = 0; i < pointsToRender.size(); i++)
          {
             int offset = i * CPU_VERTEX_SIZE;
 
-            vertices[offset + CPU_POSITION_OFFSET] = pointsToRender[i].getX32();
-            vertices[offset + CPU_POSITION_OFFSET + 1] = pointsToRender[i].getY32();
-            vertices[offset + CPU_POSITION_OFFSET + 2] = pointsToRender[i].getZ32();
+            vertices[offset + CPU_POSITION_OFFSET] = pointsToRender.get(i).getX32();
+            vertices[offset + CPU_POSITION_OFFSET + 1] = pointsToRender.get(i).getY32();
+            vertices[offset + CPU_POSITION_OFFSET + 2] = pointsToRender.get(i).getZ32();
             vertices[offset + CPU_COLOR_OFFSET] = Color.WHITE.r;
             vertices[offset + CPU_COLOR_OFFSET + 1] = Color.WHITE.g;
             vertices[offset + CPU_COLOR_OFFSET + 2] = Color.WHITE.b;
@@ -71,8 +72,8 @@ public class GDXPointCloudRenderer implements RenderableProvider
             vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 2] = 1;
          }
 
-         renderable.meshPart.size = pointsToRender.length;
-         renderable.meshPart.mesh.setVertices(vertices, 0, pointsToRender.length * CPU_VERTEX_SIZE);
+         renderable.meshPart.size = pointsToRender.size();
+         renderable.meshPart.mesh.setVertices(vertices, 0, pointsToRender.size() * CPU_VERTEX_SIZE);
          renderable.meshPart.update();
       }
    }
