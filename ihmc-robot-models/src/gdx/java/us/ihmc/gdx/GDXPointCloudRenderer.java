97dbf3b37d91e2101849d0e6557e661e44187da0
Duncan Calvert <dcalvert@ihmc.us>
2021-01-18 22:34:37 -0600

    Clean up point cloud renderer.

--- ihmc-robot-models/src/gdx/java/us/ihmc/gdx/GDXPointCloudRenderer.java
+++ ihmc-robot-models/src/gdx/java/us/ihmc/gdx/GDXPointCloudRenderer.java
@@ -16,16 +16,13 @@ public class GDXPointCloudRenderer implements RenderableProvider
    private Renderable renderable;
    private float[] vertices;
 
-   private final int sizeAndRotationUsage = 1 << 9;
-   private final VertexAttributes CPU_ATTRIBUTES = new VertexAttributes(
+   private final VertexAttributes vertexAttributes = new VertexAttributes(
          new VertexAttribute(VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE),
-         new VertexAttribute(VertexAttributes.Usage.ColorUnpacked, 4,ShaderProgram.COLOR_ATTRIBUTE),
-         new VertexAttribute(sizeAndRotationUsage, 3, "a_sizeAndRotation")
+         new VertexAttribute(VertexAttributes.Usage.ColorUnpacked, 4,ShaderProgram.COLOR_ATTRIBUTE)
    );
-   private final int CPU_VERTEX_SIZE = (short) (CPU_ATTRIBUTES.vertexSize / 4);
-   private final int CPU_POSITION_OFFSET = (short) (CPU_ATTRIBUTES.findByUsage(VertexAttributes.Usage.Position).offset / 4);
-   private final int CPU_COLOR_OFFSET = (short) (CPU_ATTRIBUTES.findByUsage(VertexAttributes.Usage.ColorUnpacked).offset / 4);
-   private final int CPU_SIZE_AND_ROTATION_OFFSET = (short) (CPU_ATTRIBUTES.findByUsage(sizeAndRotationUsage).offset / 4);
+   private final int vertexSize = (short) (vertexAttributes.vertexSize / 4);
+   private final int vertexPositionOffset = (short) (vertexAttributes.findByUsage(VertexAttributes.Usage.Position).offset / 4);
+   private final int vertexColorOffset = (short) (vertexAttributes.findByUsage(VertexAttributes.Usage.ColorUnpacked).offset / 4);
 
    private RecyclingArrayList<Point3D32> pointsToRender;
 
@@ -36,20 +33,15 @@ public class GDXPointCloudRenderer implements RenderableProvider
       renderable.meshPart.offset = 0;
       renderable.material = new Material(ColorAttribute.createDiffuse(Color.WHITE));
 
-      int initialCapacity = 10;
-      vertices = new float[size * CPU_VERTEX_SIZE];
-      if (renderable.meshPart.mesh != null) renderable.meshPart.mesh.dispose();
-      renderable.meshPart.mesh = new Mesh(false, size, 0, CPU_ATTRIBUTES);
+      vertices = new float[size * vertexSize];
+      if (renderable.meshPart.mesh != null)
+         renderable.meshPart.mesh.dispose();
+      renderable.meshPart.mesh = new Mesh(false, size, 0, vertexAttributes);
    }
 
    public void setPointsToRender(RecyclingArrayList<Point3D32> pointsToRender)
    {
       this.pointsToRender = pointsToRender;
-//      if (pointsToRender.size() > vertices.length)
-//      {
-//         vertices = new float[pointsToRender.size() * CPU_VERTEX_SIZE];
-//         renderable.meshPart.mesh = new Mesh(false, pointsToRender.size(), 0, CPU_ATTRIBUTES);
-//      }
    }
 
    public void render()
@@ -58,34 +50,35 @@ public class GDXPointCloudRenderer implements RenderableProvider
       {
          for (int i = 0; i < pointsToRender.size(); i++)
          {
-            int offset = i * CPU_VERTEX_SIZE;
+            int offset = i * vertexSize;
 
-            vertices[offset + CPU_POSITION_OFFSET] = pointsToRender.get(i).getX32();
-            vertices[offset + CPU_POSITION_OFFSET + 1] = pointsToRender.get(i).getY32();
-            vertices[offset + CPU_POSITION_OFFSET + 2] = pointsToRender.get(i).getZ32();
-            vertices[offset + CPU_COLOR_OFFSET] = Color.WHITE.r;
-            vertices[offset + CPU_COLOR_OFFSET + 1] = Color.WHITE.g;
-            vertices[offset + CPU_COLOR_OFFSET + 2] = Color.WHITE.b;
-            vertices[offset + CPU_COLOR_OFFSET + 3] = Color.WHITE.a;
-            vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET] = 1;
-            vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 1] = 0;
-            vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 2] = 1;
+            Point3D32 point = pointsToRender.get(i);
+            vertices[offset + vertexPositionOffset] = point.getX32();
+            vertices[offset + vertexPositionOffset + 1] = point.getY32();
+            vertices[offset + vertexPositionOffset + 2] = point.getZ32();
+
+            Color color = Color.WHITE;
+            vertices[offset + vertexColorOffset] = color.r;
+            vertices[offset + vertexColorOffset + 1] = color.g;
+            vertices[offset + vertexColorOffset + 2] = color.b;
+            vertices[offset + vertexColorOffset + 3] = color.a;
          }
 
          renderable.meshPart.size = pointsToRender.size();
-         renderable.meshPart.mesh.setVertices(vertices, 0, pointsToRender.size() * CPU_VERTEX_SIZE);
+         renderable.meshPart.mesh.setVertices(vertices, 0, pointsToRender.size() * vertexSize);
          renderable.meshPart.update();
       }
-   }
-
-   public Renderable getRenderable()
-   {
-      return renderable;
    }
 
    @Override
    public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
    {
       renderables.add(renderable);
+   }
+
+   public void dispose()
+   {
+      if (renderable.meshPart.mesh != null)
+         renderable.meshPart.mesh.dispose();
    }
 }
