9a6192b527c2cda6e072029e3f802d6c587070e9
Sylvain Bertrand <sbertrand@ihmc.us>
2021-01-21 12:16:05 -0600

    Added tests using the scale for Val2.

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/roughTerrainWalking/HumanoidEndToEndStairsTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/roughTerrainWalking/HumanoidEndToEndStairsTest.java
@@ -25,8 +25,10 @@ import us.ihmc.euclid.yawPitchRoll.YawPitchRoll;
 import us.ihmc.humanoidRobotics.communication.packets.HumanoidMessageTools;
 import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.simulationConstructionSetTools.util.environments.planarRegionEnvironments.StaircaseEnvironment;
+import us.ihmc.simulationConstructionSetTools.util.environments.CommonAvatarEnvironmentInterface;
+import us.ihmc.simulationConstructionSetTools.util.ground.CombinedTerrainObject3D;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
+import us.ihmc.simulationconstructionset.util.ground.TerrainObject3D;
 import us.ihmc.simulationconstructionset.util.simulationTesting.SimulationTestingParameters;
 import us.ihmc.tools.MemoryTools;
 
@@ -74,7 +76,7 @@ public abstract class HumanoidEndToEndStairsTest implements MultiRobotTestInterf
       double startX = up ? 0.0 : 1.2 + numberOfSteps * stepLength + 0.3;
       double startZ = up ? 0.0 : numberOfSteps * stepHeight;
 
-      StaircaseEnvironment environment = new StaircaseEnvironment(numberOfSteps, stepHeight, stepLength, true);
+      StairsEnvironment environment = new StairsEnvironment(numberOfSteps, stepHeight, stepLength, true);
       drcSimulationTestHelper = new DRCSimulationTestHelper(simulationTestingParameters, robotModel, environment);
       drcSimulationTestHelper.setStartingLocation(new OffsetAndYawRobotInitialSetup(startX, 0, startZ));
       drcSimulationTestHelper.createSimulation(testInfo.getTestClass().getClass().getSimpleName() + " " + testInfo.getTestMethod().get().getName());
@@ -225,5 +227,62 @@ public abstract class HumanoidEndToEndStairsTest implements MultiRobotTestInterf
             footstep.getOrientation().set(yawPitchRoll);
          }
       };
+   }
+
+   private static class StairsEnvironment implements CommonAvatarEnvironmentInterface
+   {
+      private final CombinedTerrainObject3D terrainObject = new CombinedTerrainObject3D("Stairs");
+
+      public StairsEnvironment(int numberOfSteps, double stepHeight, double stepLength, boolean includeDown)
+      {
+         this(numberOfSteps, stepHeight, stepLength, 1.75, includeDown);
+      }
+
+      public StairsEnvironment(int numberOfSteps, double stepHeight, double stepLength, double stairsWidth, boolean includeDown)
+      {
+         double startingBlockLength = 1.2;
+         double xStart = -0.5 * startingBlockLength;
+         double xEnd = 0.5 * startingBlockLength;
+         double yStart = -0.5 * stairsWidth;
+         double yEnd = 0.5 * stairsWidth;
+         double zStart = -0.10;
+         double zEnd = 0.0;
+         terrainObject.addBox(xStart, yStart, xEnd, yEnd, zStart, zEnd);
+
+         for (int i = 0; i < numberOfSteps - 1; i++)
+         {
+            xStart = xEnd;
+            xEnd += stepLength;
+            zEnd += stepHeight;
+            terrainObject.addBox(xStart, yStart, xEnd + 3.0e-2, yEnd, zStart, zEnd);
+         }
+
+         xStart = xEnd;
+         xEnd += startingBlockLength + 2 * stepLength;
+         zEnd = numberOfSteps * stepHeight;
+         terrainObject.addBox(xStart, yStart, xEnd, yEnd, zStart, zEnd);
+
+         if (includeDown)
+         {
+            for (int i = 0; i < numberOfSteps - 1; i++)
+            {
+               xStart = xEnd;
+               xEnd += stepLength;
+               zEnd -= stepHeight;
+               terrainObject.addBox(xStart - 3.0e-2, yStart, xEnd, yEnd, zStart, zEnd);
+            }
+
+            xStart = xEnd - 3.0e-2;
+            xEnd += startingBlockLength + stepLength;
+            zEnd = 0.0;
+            terrainObject.addBox(xStart, yStart, xEnd, yEnd, zStart, zEnd);
+         }
+      }
+
+      @Override
+      public TerrainObject3D getTerrainObject3D()
+      {
+         return terrainObject;
+      }
    }
 }
--- valkyrie/src/test/java/us/ihmc/valkyrie/roughTerrainWalking/ValkyrieEndToEndStairsTest.java
+++ valkyrie/src/test/java/us/ihmc/valkyrie/roughTerrainWalking/ValkyrieEndToEndStairsTest.java
@@ -13,15 +13,24 @@ import us.ihmc.valkyrie.configuration.ValkyrieRobotVersion;
 
 public class ValkyrieEndToEndStairsTest extends HumanoidEndToEndStairsTest
 {
+   private boolean useVal2Scale = false;
+
    @Override
    public DRCRobotModel getRobotModel()
    {
-      return new ValkyrieRobotModel(RobotTarget.SCS, ValkyrieRobotVersion.FINGERLESS);
+      ValkyrieRobotModel robotModel = new ValkyrieRobotModel(RobotTarget.SCS, ValkyrieRobotVersion.FINGERLESS);
+      if (useVal2Scale)
+      {
+         robotModel.setModelSizeScale(0.925170);
+         robotModel.setModelMassScale(0.925170);
+      }
+      return robotModel;
    }
 
    @Test
    public void testUpStairsSlow(TestInfo testInfo) throws Exception
    {
+      useVal2Scale = false;
       Random random = new Random(53415);
       testStairs(testInfo, true, true, 0.6, 0.25, 0.0, createFootstepCorruptor(random, 0.025, 0.10, 0.05, 0.2, 0.2, 0.2));
    }
@@ -29,6 +38,7 @@ public class ValkyrieEndToEndStairsTest extends HumanoidEndToEndStairsTest
    @Test
    public void testDownStairsSlow(TestInfo testInfo) throws Exception
    {
+      useVal2Scale = false;
       Random random = new Random(53415);
       testStairs(testInfo, true, false, 0.9, 0.25, 0.0, createFootstepCorruptor(random, 0.025, 0.10, 0.05, 0.2, 0.2, 0.2));
    }
@@ -36,13 +46,46 @@ public class ValkyrieEndToEndStairsTest extends HumanoidEndToEndStairsTest
    @Test
    public void testUpStairs(TestInfo testInfo) throws Exception
    {
-      testStairs(testInfo, false, true, 0.9, 0.25, 0.0);
+      useVal2Scale = false;
+      testStairs(testInfo, false, true, 1.0, 0.25, 0.0);
    }
 
    @Test
    public void testDownStairs(TestInfo testInfo) throws Exception
    {
+      useVal2Scale = false;
       Random random = new Random(53415);
       testStairs(testInfo, false, false, 1.0, 0.35, 0.0, createFootstepCorruptor(random, 0.025, 0.10, 0.05, 0.2, 0.2, 0.2));
+   }
+
+   @Test
+   public void testUpStairsSlowVal2Scale(TestInfo testInfo) throws Exception
+   {
+      useVal2Scale = true;
+      Random random = new Random(53415);
+      testStairs(testInfo, true, true, 0.6, 0.25, 0.0, createFootstepCorruptor(random, 0.02, 0.05, 0.05, 0.2, 0.2, 0.2));
+   }
+
+   @Test
+   public void testDownStairsSlowVal2Scale(TestInfo testInfo) throws Exception
+   {
+      useVal2Scale = true;
+      Random random = new Random(53415);
+      testStairs(testInfo, true, false, 1.0, 0.25, 0.0, createFootstepCorruptor(random, 0.02, 0.05, 0.05, 0.1, 0.2, 0.2));
+   }
+
+   @Test
+   public void testUpStairsVal2Scale(TestInfo testInfo) throws Exception
+   {
+      useVal2Scale = true;
+      testStairs(testInfo, false, true, 1.2, 0.25, 0.0);
+   }
+
+   @Test
+   public void testDownStairsVal2Scale(TestInfo testInfo) throws Exception
+   {
+      useVal2Scale = true;
+      Random random = new Random(53415);
+      testStairs(testInfo, false, false, 1.0, 0.35, 0.0, createFootstepCorruptor(random, 0.02, 0.05, 0.05, 0.2, 0.2, 0.2));
    }
 }
