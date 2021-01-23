6d9592cb9fd96ca42aa83df70fb6efdfe51f2526
Duncan Calvert <dcalvert@ihmc.us>
2021-01-22 19:29:14 -0600

    Remove unused import and avoid creating extra buffer.

--- ihmc-robot-models/src/gdx/java/com/badlogic/gdx/graphics/glutils/SensorFrameBuffer.java
+++ ihmc-robot-models/src/gdx/java/com/badlogic/gdx/graphics/glutils/SensorFrameBuffer.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.TextureData;
 
 public class SensorFrameBuffer extends GLFrameBuffer<Texture>
 {
@@ -32,6 +31,7 @@ public class SensorFrameBuffer extends GLFrameBuffer<Texture>
       }
       else
       {
+         attachmentSpec.isGpuOnly = true;
          depthTextureData = new FloatTextureData(bufferBuilder.width,
                                      bufferBuilder.height,
                                      attachmentSpec.internalFormat,
