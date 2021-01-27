db4cd21ce46bfd80e60a8e599d7eaa1951181d7b
Robert <rgriffin@ihmc.us>
2021-01-27 15:23:52 -0600

    got the standalone foot trajectory calculator kind of stood up

---
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/YoSwingTrajectoryParameters.java
@@ -0,0 +1,181 @@
+package us.ihmc.commonWalkingControlModules.configurations;
+
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+import us.ihmc.robotics.dataStructures.parameters.FrameParameterVector3D;
+import us.ihmc.robotics.dataStructures.parameters.ParameterVector3D;
+import us.ihmc.yoVariables.parameters.BooleanParameter;
+import us.ihmc.yoVariables.parameters.DoubleParameter;
+import us.ihmc.yoVariables.providers.DoubleProvider;
+import us.ihmc.yoVariables.registry.YoRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class YoSwingTrajectoryParameters
+{
+   private final BooleanParameter doHeelTouchdownIfPossible;
+   private final DoubleParameter heelTouchdownAngle;
+   private final DoubleParameter maximumHeightForHeelTouchdown;
+   private final DoubleParameter heelTouchdownLengthRatio;
+
+   private final BooleanParameter doToeTouchdownIfPossible;
+   private final DoubleParameter toeTouchdownAngle;
+   private final DoubleParameter stepDownHeightForToeTouchdown;
+   private final DoubleParameter toeTouchdownDepthRatio;
+
+   private final BooleanParameter addOrientationMidpointForClearance;
+   private final DoubleParameter midpointOrientationInterpolationForClearance;
+
+   private final DoubleParameter minHeightDifferenceForObstacleClearance;
+   private final DoubleParameter finalSwingHeightOffset;
+
+   private final List<DoubleProvider> defaultWaypointProportions = new ArrayList<>();
+   private final List<DoubleProvider> defaultObstacleClearanceWaypointProportions = new ArrayList<>();
+
+   private final ParameterVector3D touchdownVelocityWeight;
+   private final FrameParameterVector3D touchdownVelocity;
+   private final FrameParameterVector3D touchdownAcceleration;
+
+   public YoSwingTrajectoryParameters(String namePrefix, SwingTrajectoryParameters parameters, YoRegistry registry)
+   {
+      doHeelTouchdownIfPossible = new BooleanParameter(namePrefix + "DoHeelTouchdownIfPossible", registry, parameters.doHeelTouchdownIfPossible());
+      heelTouchdownAngle = new DoubleParameter(namePrefix + "HeelTouchdownAngle", registry, parameters.getHeelTouchdownAngle());
+      maximumHeightForHeelTouchdown = new DoubleParameter(namePrefix + "MaximumHeightForHeelTouchdown",
+                                                          registry,
+                                                          parameters.getMaximumHeightForHeelTouchdown());
+      heelTouchdownLengthRatio = new DoubleParameter(namePrefix + "HeelTouchdownLengthRatio", registry, parameters.getHeelTouchdownLengthRatio());
+
+      doToeTouchdownIfPossible = new BooleanParameter(namePrefix + "DoToeTouchdownIfPossible", registry, parameters.doToeTouchdownIfPossible());
+      toeTouchdownAngle = new DoubleParameter(namePrefix + "ToeTouchdownAngle", registry, parameters.getToeTouchdownAngle());
+      stepDownHeightForToeTouchdown = new DoubleParameter(namePrefix + "StepDownHeightForToeTouchdown",
+                                                          registry,
+                                                          parameters.getStepDownHeightForToeTouchdown());
+      toeTouchdownDepthRatio = new DoubleParameter(namePrefix + "ToeTouchdownDepthRatio", registry, parameters.getToeTouchdownDepthRatio());
+
+      addOrientationMidpointForClearance = new BooleanParameter(namePrefix + "AddOrientationMidpointForClearance",
+                                                                registry,
+                                                                parameters.addOrientationMidpointForObstacleClearance());
+      midpointOrientationInterpolationForClearance = new DoubleParameter(namePrefix + "MidpointOrientationInterpolationForClearance",
+                                                                         registry,
+                                                                         parameters.midpointOrientationInterpolationForObstacleClearance());
+
+      minHeightDifferenceForObstacleClearance = new DoubleParameter(namePrefix + "MinHeightDifferenceForObstacleClearance",
+                                                                    registry,
+                                                                    parameters.getMinHeightDifferenceForStepUpOrDown());
+
+      int numberWaypoints = 2;
+      double[] defaultWaypointProportions = parameters.getSwingWaypointProportions();
+      double[] defaultObstacleClearanceWaypointProportions = parameters.getObstacleClearanceProportions();
+
+      for (int i = 0; i < numberWaypoints; i++)
+      {
+         DoubleParameter waypointProportion = new DoubleParameter(namePrefix + "WaypointProportion" + i, registry, defaultWaypointProportions[i]);
+         DoubleParameter obstacleClearanceWaypointProportion = new DoubleParameter(namePrefix + "ObstacleClearanceWaypointProportion" + i,
+                                                                                   registry,
+                                                                                   defaultObstacleClearanceWaypointProportions[i]);
+         this.defaultWaypointProportions.add(waypointProportion);
+         this.defaultObstacleClearanceWaypointProportions.add(obstacleClearanceWaypointProportion);
+      }
+
+      this.touchdownVelocityWeight = new ParameterVector3D(namePrefix + "TouchdownVelocityWeight", parameters.getTouchdownVelocityWeight(), registry);
+
+      finalSwingHeightOffset = new DoubleParameter(namePrefix + "FinalHeightOffset", registry, parameters.getDesiredTouchdownHeightOffset());
+
+      Vector3D defaultTouchdownVelocity = new Vector3D(0.0, 0.0, parameters.getDesiredTouchdownVelocity());
+      touchdownVelocity = new FrameParameterVector3D(namePrefix + "TouchdownVelocity", ReferenceFrame.getWorldFrame(), defaultTouchdownVelocity, registry);
+
+      Vector3D defaultTouchdownAcceleration = new Vector3D(0.0, 0.0, parameters.getDesiredTouchdownAcceleration());
+      touchdownAcceleration = new FrameParameterVector3D(namePrefix + "TouchdownAcceleration",
+                                                                                ReferenceFrame.getWorldFrame(),
+                                                                                defaultTouchdownAcceleration,
+                                                                                registry);
+   }
+
+   public boolean doToeTouchdownIfPossible()
+   {
+      return doToeTouchdownIfPossible.getValue();
+   }
+
+   public double getToeTouchdownAngle()
+   {
+      return toeTouchdownAngle.getValue();
+   }
+
+   public double getToeTouchdownDepthRatio()
+   {
+      return toeTouchdownDepthRatio.getValue();
+   }
+
+   public double getStepDownHeightForToeTouchdown()
+   {
+      return stepDownHeightForToeTouchdown.getValue();
+   }
+
+   public boolean doHeelTouchdownIfPossible()
+   {
+      return doHeelTouchdownIfPossible.getValue();
+   }
+
+   public double getHeelTouchdownAngle()
+   {
+      return heelTouchdownAngle.getValue();
+   }
+
+   public double getHeelTouchdownLengthRatio()
+   {
+      return heelTouchdownLengthRatio.getValue();
+   }
+
+   public double getMaximumHeightForHeelTouchdown()
+   {
+      return maximumHeightForHeelTouchdown.getValue();
+   }
+
+   public boolean addOrientationMidpointForObstacleClearance()
+   {
+      return addOrientationMidpointForClearance.getValue();
+   }
+
+   public double getMidpointOrientationInterpolationForObstacleClearance()
+   {
+      return midpointOrientationInterpolationForClearance.getValue();
+   }
+
+   public double getMinHeightDifferenceForStepUpOrDown()
+   {
+      return minHeightDifferenceForObstacleClearance.getValue();
+   }
+
+   public List<DoubleProvider> getSwingWaypointProportions()
+   {
+      return defaultWaypointProportions;
+   }
+
+   public List<DoubleProvider> getObstacleClearanceProportions()
+   {
+      return defaultObstacleClearanceWaypointProportions;
+   }
+
+   public Tuple3DReadOnly getTouchdownVelocityWeight()
+   {
+      return touchdownVelocityWeight;
+   }
+
+   public double getDesiredTouchdownHeightOffset()
+   {
+      return finalSwingHeightOffset.getValue();
+   }
+
+   public FrameVector3DReadOnly getDesiredTouchdownVelocity()
+   {
+      return touchdownVelocity;
+   }
+
+   public FrameVector3DReadOnly getDesiredTouchdownAcceleration()
+   {
+      return touchdownAcceleration;
+   }
+}
