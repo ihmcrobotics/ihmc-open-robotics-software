ea232d1d0e54820408b7d2c87225eb434b9a0241
Duncan Calvert <dcalvert@ihmc.us>
2021-01-28 16:15:42 -0600

    Start behaviors and extract the lab environment into a class.

--- atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasLookAndStepBehaviorDemo.java
+++ atlas/src/main/java/us/ihmc/atlas/behaviors/AtlasLookAndStepBehaviorDemo.java
@@ -65,13 +65,12 @@ public class AtlasLookAndStepBehaviorDemo extends AtlasSimulationBasics
 
       BehaviorUIRegistry behaviorRegistry = BehaviorUIRegistry.of(LookAndStepBehaviorUI.DEFINITION);
 
-      behaviorModule = new BehaviorModule(behaviorRegistry, createRobotModel(), COMMUNICATION_MODE_ROS2, COMMUNICATION_MODE_KRYO);
+      behaviorModule = new BehaviorModule(behaviorRegistry, createRobotModel(), COMMUNICATION_MODE_ROS2, COMMUNICATION_MODE_MESSAGER);
 
       LogTools.info("Creating behavior user interface");
       behaviorUI = BehaviorUI.create(behaviorRegistry,
                                      createRobotModel(),
-                                     COMMUNICATION_MODE_ROS2,
-                                     COMMUNICATION_MODE_KRYO,
+                                     COMMUNICATION_MODE_ROS2, COMMUNICATION_MODE_MESSAGER,
                                      "localhost",
                                      behaviorModule.getMessager());
       behaviorUI.addOnCloseRequestListener(() -> ThreadTools.startAThread(() -> {
