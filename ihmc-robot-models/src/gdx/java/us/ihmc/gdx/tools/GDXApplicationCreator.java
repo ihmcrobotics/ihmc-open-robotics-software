6e76553445c6cde85d1e93fc0fb92094ed40506c
Duncan Calvert <dcalvert@ihmc.us>
2021-01-29 16:08:06 -0600

    Create VR terrain builder class and disable vsync to make it render at 90Hz to reduce dizziness.

--- ihmc-robot-models/src/gdx/java/us/ihmc/gdx/tools/GDXApplicationCreator.java
+++ ihmc-robot-models/src/gdx/java/us/ihmc/gdx/tools/GDXApplicationCreator.java
@@ -12,7 +12,12 @@ public class GDXApplicationCreator
       launchGDXApplication(application, clazz.getSimpleName(), 1100, 800);
    }
 
-   public static void launchGDXApplication(Lwjgl3ApplicationAdapter application, String title, double width, double height)
+   public static Lwjgl3ApplicationConfiguration getDefaultConfiguration(Class<?> clazz)
+   {
+      return getDefaultConfiguration(clazz.getSimpleName(), 1100, 800);
+   }
+
+   public static Lwjgl3ApplicationConfiguration getDefaultConfiguration(String title, double width, double height)
    {
       Lwjgl3ApplicationConfiguration applicationConfiguration = new Lwjgl3ApplicationConfiguration();
       applicationConfiguration.setTitle(title);
@@ -20,7 +25,18 @@ public class GDXApplicationCreator
       applicationConfiguration.useVsync(true);
       applicationConfiguration.setBackBufferConfig(8, 8, 8, 8, 16, 0, 4);
       applicationConfiguration.useOpenGL3(true, 3, 2);
+      return applicationConfiguration;
+   }
 
+   public static void launchGDXApplication(Lwjgl3ApplicationAdapter application, String title, double width, double height)
+   {
+      launchGDXApplication(getDefaultConfiguration(title, width, height), application, title);
+   }
+
+   public static void launchGDXApplication(Lwjgl3ApplicationConfiguration applicationConfiguration,
+                                           Lwjgl3ApplicationAdapter application,
+                                           String title)
+   {
       ThreadTools.startAThread(() -> new Lwjgl3Application(application, applicationConfiguration), title);
    }
 }
