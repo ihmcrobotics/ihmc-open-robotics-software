0005023de36734031d3f9a4c77e2eaf95263f422
Sylvain Bertrand <sbertrand@ihmc.us>
2021-02-04 13:57:29 -0600

    :arrow_up: ihmc-robot-data-logger 0.20.7

--- ihmc-parameter-tuner/build.gradle.kts
+++ ihmc-parameter-tuner/build.gradle.kts
@@ -21,7 +21,7 @@ mainDependencies {
 
    api("us.ihmc:ihmc-yovariables:0.9.8")
    api("us.ihmc:ihmc-javafx-toolkit:0.19.3")
-   api("us.ihmc:ihmc-robot-data-logger:0.20.6")
+   api("us.ihmc:ihmc-robot-data-logger:0.20.7")
    api("us.ihmc:ihmc-robotics-toolkit:source")
 }
 
