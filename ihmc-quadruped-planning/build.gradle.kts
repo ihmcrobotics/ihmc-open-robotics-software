58983917b7eddbdb250092bd991ab8af8a4a302b
Sylvain Bertrand <sbertrand@ihmc.us>
2021-02-04 13:57:29 -0600

    :arrow_up: ihmc-robot-data-logger 0.20.7

--- ihmc-quadruped-planning/build.gradle.kts
+++ ihmc-quadruped-planning/build.gradle.kts
@@ -26,7 +26,7 @@ mainDependencies {
     api("us.ihmc:ihmc-quadruped-basics:source")
     api("us.ihmc:robot-environment-awareness:source")
     api("us.ihmc:simulation-construction-set-tools-test:source")
-    api("us.ihmc:ihmc-robot-data-logger:0.20.6")
+    api("us.ihmc:ihmc-robot-data-logger:0.20.7")
     api("us.ihmc:ihmc-path-planning:source")
 }
 
