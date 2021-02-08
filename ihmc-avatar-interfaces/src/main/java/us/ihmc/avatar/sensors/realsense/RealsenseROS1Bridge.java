9b327c63f29f09de3a0bf64790813813808cdf5d
Operator Computer <OperatorComputer@unknown>
2021-02-08 16:46:36 -0600

    Remove D435 from name of point cloud bridge.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sensors/realsense/RealsenseROS1Bridge.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sensors/realsense/RealsenseROS1Bridge.java
@@ -16,8 +16,8 @@ public class RealsenseROS1Bridge
 {
    private final RealsenseVideoROS1Bridge d435VideoBridge;
    private final RealsenseVideoROS1Bridge l515VideoBridge;
-   private final RealsenseD435PointCloudROS1Bridge d435PointCloudBridge;
-   private final RealsenseD435PointCloudROS1Bridge l515PointCloudBridge;
+   private final RealsensePointCloudROS1Bridge d435PointCloudBridge;
+   private final RealsensePointCloudROS1Bridge l515PointCloudBridge;
    private final RealsensePlanarRegionROS1Bridge l515PlanarRegionBridge;
 
    public RealsenseROS1Bridge(DRCRobotModel robotModel, RigidBodyTransform d435PelvisToSensorTransform,
@@ -31,18 +31,18 @@ public class RealsenseROS1Bridge
 
       d435VideoBridge = new RealsenseVideoROS1Bridge(ros1Node, ros2Node, RosTools.D435_VIDEO, ROS2Tools.D435_VIDEO);
       l515VideoBridge = new RealsenseVideoROS1Bridge(ros1Node, ros2Node, RosTools.L515_VIDEO, ROS2Tools.L515_VIDEO);
-      d435PointCloudBridge = new RealsenseD435PointCloudROS1Bridge(robotModel,
-                                                                   ros1Node,
-                                                                   ros2Node,
-                                                                   d435PelvisToSensorTransform,
-                                                                   RosTools.D435_POINT_CLOUD,
-                                                                   ROS2Tools.D435_POINT_CLOUD);
-      l515PointCloudBridge = new RealsenseD435PointCloudROS1Bridge(robotModel,
-                                                                   ros1Node,
-                                                                   ros2Node,
-                                                                   l515PelvisToSensorTransform,
-                                                                   RosTools.L515_POINT_CLOUD,
-                                                                   ROS2Tools.L515_POINT_CLOUD);
+      d435PointCloudBridge = new RealsensePointCloudROS1Bridge(robotModel,
+                                                               ros1Node,
+                                                               ros2Node,
+                                                               d435PelvisToSensorTransform,
+                                                               RosTools.D435_POINT_CLOUD,
+                                                               ROS2Tools.D435_POINT_CLOUD);
+      l515PointCloudBridge = new RealsensePointCloudROS1Bridge(robotModel,
+                                                               ros1Node,
+                                                               ros2Node,
+                                                               l515PelvisToSensorTransform,
+                                                               RosTools.L515_POINT_CLOUD,
+                                                               ROS2Tools.L515_POINT_CLOUD);
       l515PlanarRegionBridge = new RealsensePlanarRegionROS1Bridge(robotModel,
                                                                    ros1Node,
                                                                    ros2Node,
