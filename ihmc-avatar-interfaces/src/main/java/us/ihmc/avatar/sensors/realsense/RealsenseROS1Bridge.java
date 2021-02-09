92a66fc02d9115ec64d72861d762a534bfbc23d1
BhavyanshM <bhavyanshmishra@gmail.com>
2021-02-09 17:48:01 -0600

    Add the GDXPerceptionVisualizer

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sensors/realsense/RealsenseROS1Bridge.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sensors/realsense/RealsenseROS1Bridge.java
@@ -16,9 +16,9 @@ public class RealsenseROS1Bridge
 {
    private final RealsenseVideoROS1Bridge d435VideoBridge;
    private final RealsenseVideoROS1Bridge l515VideoBridge;
-   private final RealsenseD435PointCloudROS1Bridge d435PointCloudBridge;
-   private final RealsenseD435PointCloudROS1Bridge l515PointCloudBridge;
-   private final RealsensePlanarRegionROS1Bridge l515PlanarRegionBridge;
+   private final RealsensePointCloudROS1Bridge d435PointCloudBridge;
+   private final RealsensePointCloudROS1Bridge l515PointCloudBridge;
+   private final MapsensePlanarRegionROS1Bridge l515PlanarRegionBridge;
 
    public RealsenseROS1Bridge(DRCRobotModel robotModel, RigidBodyTransform d435PelvisToSensorTransform,
                               RigidBodyTransform l515PelvisToSensorTransform)
@@ -31,24 +31,24 @@ public class RealsenseROS1Bridge
 
       d435VideoBridge = new RealsenseVideoROS1Bridge(ros1Node, ros2Node, RosTools.D435_VIDEO, ROS2Tools.D435_VIDEO);
       l515VideoBridge = new RealsenseVideoROS1Bridge(ros1Node, ros2Node, RosTools.L515_VIDEO, ROS2Tools.L515_VIDEO);
-      d435PointCloudBridge = new RealsenseD435PointCloudROS1Bridge(robotModel,
-                                                                   ros1Node,
-                                                                   ros2Node,
-                                                                   d435PelvisToSensorTransform,
-                                                                   RosTools.D435_POINT_CLOUD,
-                                                                   ROS2Tools.D435_POINT_CLOUD);
-      l515PointCloudBridge = new RealsenseD435PointCloudROS1Bridge(robotModel,
-                                                                   ros1Node,
-                                                                   ros2Node,
-                                                                   l515PelvisToSensorTransform,
-                                                                   RosTools.L515_POINT_CLOUD,
-                                                                   ROS2Tools.L515_POINT_CLOUD);
-      l515PlanarRegionBridge = new RealsensePlanarRegionROS1Bridge(robotModel,
-                                                                   ros1Node,
-                                                                   ros2Node,
-                                                                   RosTools.MAPSENSE_REGIONS,
-                                                                   ROS2Tools.MAPSENSE_REGIONS,
-                                                                   l515PelvisToSensorTransform);
+      d435PointCloudBridge = new RealsensePointCloudROS1Bridge(robotModel,
+                                                               ros1Node,
+                                                               ros2Node,
+                                                               d435PelvisToSensorTransform,
+                                                               RosTools.D435_POINT_CLOUD,
+                                                               ROS2Tools.D435_POINT_CLOUD);
+      l515PointCloudBridge = new RealsensePointCloudROS1Bridge(robotModel,
+                                                               ros1Node,
+                                                               ros2Node,
+                                                               l515PelvisToSensorTransform,
+                                                               RosTools.L515_POINT_CLOUD,
+                                                               ROS2Tools.L515_POINT_CLOUD);
+      l515PlanarRegionBridge = new MapsensePlanarRegionROS1Bridge(robotModel,
+                                                                  ros1Node,
+                                                                  ros2Node,
+                                                                  RosTools.MAPSENSE_REGIONS,
+                                                                  ROS2Tools.MAPSENSE_REGIONS,
+                                                                  l515PelvisToSensorTransform);
 
       ros1Node.execute();
    }
