ecf6c39ed4a0f7706123c42b7d742c8307c0e081
Carlos Gonzalez <cgonzalez@ihmc.us>
2021-02-20 12:44:19 -0600

    added tests taking one step down at different heights, checking ankle limits

--- atlas/src/test/java/us/ihmc/atlas/AtlasToeOffTest.java
+++ atlas/src/test/java/us/ihmc/atlas/AtlasToeOffTest.java
@@ -1,12 +1,36 @@
 package us.ihmc.atlas;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.avatar.obstacleCourseTests.AvatarToeOffTest;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
+import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner;
 
 public class AtlasToeOffTest extends AvatarToeOffTest
 {
+    @Test
+    public void testToeOffTakingShortStepDownCheckingAnkleLimits(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
+    {
+        setStepHeight(-0.1);
+        super.testToeOffTakingStepAndCheckingAnkleLimits(testInfo);
+    }
+
+    @Test
+    public void testToeOffTakingMediumStepDownCheckingAnkleLimits(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
+    {
+        setStepHeight(-0.2);
+        super.testToeOffTakingStepAndCheckingAnkleLimits(testInfo);
+    }
+
+    @Test
+    public void testToeOffTakingHighStepDownCheckingAnkleLimits(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
+    {
+        setStepHeight(-0.3);
+        super.testToeOffTakingStepAndCheckingAnkleLimits(testInfo);
+    }
+
     @Override
     public double getStepLength()
     {
@@ -29,7 +53,6 @@ public class AtlasToeOffTest extends AvatarToeOffTest
     public DRCRobotModel getRobotModel()
     {
         AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false);
-
         return robotModel;
     }
 
