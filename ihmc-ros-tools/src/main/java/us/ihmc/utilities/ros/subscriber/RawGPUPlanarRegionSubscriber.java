35548a165e573cde5db1145e76847de88985c478
Duncan Calvert <dcalvert@ihmc.us>
2021-02-27 12:19:14 -0600

    Add debug flag to RawGPUPlanarRegionSubscriber.

--- ihmc-ros-tools/src/main/java/us/ihmc/utilities/ros/subscriber/RawGPUPlanarRegionSubscriber.java
+++ ihmc-ros-tools/src/main/java/us/ihmc/utilities/ros/subscriber/RawGPUPlanarRegionSubscriber.java
@@ -1,14 +1,17 @@
 package us.ihmc.utilities.ros.subscriber;
 
+import map_sense.RawGPUPlanarRegion;
 import map_sense.RawGPUPlanarRegionList;
 import us.ihmc.utilities.ros.RosMainNode;
 import us.ihmc.utilities.ros.RosTools;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 public class RawGPUPlanarRegionSubscriber extends AbstractRosTopicSubscriber<map_sense.RawGPUPlanarRegionList>
 {
+   private boolean DEBUG = false;
 
    private RawGPUPlanarRegionList planarRegionList;
    private boolean regionListAvailable = false;
@@ -34,17 +37,24 @@ public class RawGPUPlanarRegionSubscriber extends AbstractRosTopicSubscriber<map
    {
       this.planarRegionList = message;
       this.regionListAvailable = true;
-      //        System.out.println("Received Message:" + message.getNumOfRegions());
-      //        for (int i = 0; i < message.getNumOfRegions(); i++) {
-      //            List<RawGPUPlanarRegion> regions = message.getRegions();
-      //            System.out.println("\tRegion:" + regions.get(i).getId());
-      //            System.out.println("\t\tNormal:" + "X:" + regions.get(i).getNormal().getX() + "\tY:" + regions.get(i).getNormal().getY() + "\tZ:" + regions.get(i).getNormal().getZ());
-      //            System.out.println("\t\tCentroid:" + "X:" + regions.get(i).getCentroid().getX() + "\tY:" + regions.get(i).getCentroid().getY() + "\tZ:" + regions.get(i).getCentroid().getZ());
-      //            for (int j = 0; j < regions.get(i).getVertices().size(); j++) {
-      //                geometry_msgs.Point point = regions.get(i).getVertices().get(j);
-      //                System.out.println("\t\tPoint:" + "X:" + point.getX() + "\tY:" + point.getY() + "\tZ:" + point.getZ());
-      //            }
-      //        }
+      if (DEBUG)
+      {
+         System.out.println("Received Message:" + message.getNumOfRegions());
+         for (int i = 0; i < message.getNumOfRegions(); i++)
+         {
+            List<RawGPUPlanarRegion> regions = message.getRegions();
+            System.out.println("\tRegion:" + regions.get(i).getId());
+            System.out.println("\t\tNormal:" + "X:" + regions.get(i).getNormal().getX()
+                               + "\tY:" + regions.get(i).getNormal().getY() + "\tZ:" + regions.get(i));
+            System.out.println("\t\tCentroid:" + "X:" + regions.get(i).getCentroid().getX()
+                               + "\tY:" + regions.get(i).getCentroid().getY() + "\tZ:" + regions.get(i));
+            for (int j = 0; j < regions.get(i).getVertices().size(); j++)
+            {
+               geometry_msgs.Point point = regions.get(i).getVertices().get(j);
+               System.out.println("\t\tPoint:" + "X:" + point.getX() + "\tY:" + point.getY() + "\tZ:" + point.getZ());
+            }
+         }
+      }
    }
 
    public static void main(String[] args) throws URISyntaxException
