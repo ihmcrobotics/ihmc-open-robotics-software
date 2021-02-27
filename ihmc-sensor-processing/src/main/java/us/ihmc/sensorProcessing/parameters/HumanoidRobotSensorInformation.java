00551f2b91e39d42f3ace21a1c36f43969566fee
Duncan Calvert <dcalvert@ihmc.us>
2021-02-27 12:21:44 -0600

    Add stepping camera transform getter to sensor information.

--- ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/parameters/HumanoidRobotSensorInformation.java
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/parameters/HumanoidRobotSensorInformation.java
@@ -1,5 +1,11 @@
 package us.ihmc.sensorProcessing.parameters;
 
+import us.ihmc.euclid.transform.RigidBodyTransform;
+
 public interface HumanoidRobotSensorInformation extends AvatarRobotRosVisionSensorInformation, HumanoidForceSensorInformation, IMUSensorInformation
 {
+   public default RigidBodyTransform getSteppingCameraTransform()
+   {
+      return new RigidBodyTransform();
+   }
 }
