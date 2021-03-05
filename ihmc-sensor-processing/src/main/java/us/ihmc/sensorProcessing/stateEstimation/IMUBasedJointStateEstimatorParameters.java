b3b099f52ce7358c3fe5fbca75c4a7cf5c178d5a
Sylvain Bertrand <sbertrand@ihmc.us>
2021-03-05 10:56:51 -0600

    Can now create 1+ estimator per robot. Same math.

---
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/stateEstimation/IMUBasedJointStateEstimatorParameters.java
@@ -0,0 +1,67 @@
+package us.ihmc.sensorProcessing.stateEstimation;
+
+public class IMUBasedJointStateEstimatorParameters
+{
+   private final String estimatorName;
+   private final boolean startEnabled;
+   private final String parentIMUName, childIMUName;
+   private final double breakFrequencyForVelocityEstimation;
+   private final double breakFrequencyForPositionEstimation;
+   private double velocityEstimationBacklashSlopTime = 0.0;
+
+   public IMUBasedJointStateEstimatorParameters(String estimatorName,
+                                                boolean startEnabled,
+                                                String parentIMUName,
+                                                String childIMUName,
+                                                double breakFrequencyForVelocityEstimation,
+                                                double breakFrequencyForPositionEstimation)
+   {
+      super();
+      this.estimatorName = estimatorName;
+      this.startEnabled = startEnabled;
+      this.parentIMUName = parentIMUName;
+      this.childIMUName = childIMUName;
+      this.breakFrequencyForVelocityEstimation = breakFrequencyForVelocityEstimation;
+      this.breakFrequencyForPositionEstimation = breakFrequencyForPositionEstimation;
+   }
+
+   public void setVelocityEstimationBacklashSlopTime(double velocityEstimationBacklashSlopTime)
+   {
+      this.velocityEstimationBacklashSlopTime = velocityEstimationBacklashSlopTime;
+   }
+
+   public String getEstimatorName()
+   {
+      return estimatorName;
+   }
+
+   public boolean isStartEnabled()
+   {
+      return startEnabled;
+   }
+
+   public String getParentIMUName()
+   {
+      return parentIMUName;
+   }
+
+   public String getChildIMUName()
+   {
+      return childIMUName;
+   }
+
+   public double getBreakFrequencyForVelocityEstimation()
+   {
+      return breakFrequencyForVelocityEstimation;
+   }
+
+   public double getBreakFrequencyForPositionEstimation()
+   {
+      return breakFrequencyForPositionEstimation;
+   }
+
+   public double getVelocityEstimationBacklashSlopTime()
+   {
+      return velocityEstimationBacklashSlopTime;
+   }
+}
