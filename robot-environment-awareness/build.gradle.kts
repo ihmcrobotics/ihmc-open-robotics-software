14437c639ca306f0139f3197e69a6f7c0df250ec
Duncan Calvert <dcalvert@ihmc.us>
2021-03-08 21:44:42 -0600

    Add ihmc-graphics dependencies so that everything compiles during the move.

--- robot-environment-awareness/build.gradle.kts
+++ robot-environment-awareness/build.gradle.kts
@@ -34,6 +34,8 @@ mainDependencies {
    api("us.ihmc:ihmc-communication:source")
    api("us.ihmc:ihmc-robotics-toolkit:source")
    api("us.ihmc:ihmc-robot-models-visualizers:source")
+   api("us.ihmc:ihmc-graphics-javafx:source")
+   api("us.ihmc:ihmc-graphics-jmonkeyengine:source")
    api("us.ihmc:ihmc-robot-data-logger:0.20.7")
 
    apiBytedecoNatives("javacpp", "1.5.4")
