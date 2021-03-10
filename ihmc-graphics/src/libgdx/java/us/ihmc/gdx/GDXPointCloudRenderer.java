89254af25e64c7e23d0d17fe158ef6199c8fb30b
Duncan Calvert <dcalvert@ihmc.us>
2021-03-09 19:23:30 -0600

    Figure out how to set the size of point sprites.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/GDXPointCloudRenderer.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/GDXPointCloudRenderer.java
@@ -1,10 +1,13 @@
 package us.ihmc.gdx;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.graphics.g3d.RenderableProvider;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.particles.ParticleShader;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
@@ -13,22 +16,29 @@ import us.ihmc.euclid.tuple3D.Point3D32;
 
 public class GDXPointCloudRenderer implements RenderableProvider
 {
+   private static final int SIZE_AND_ROTATION_USAGE = 1 << 9;
+   private static boolean POINT_SPRITES_ENABLED = false;
    private Renderable renderable;
    private float[] vertices;
 
    private final VertexAttributes vertexAttributes = new VertexAttributes(
          new VertexAttribute(VertexAttributes.Usage.Position, 3, ShaderProgram.POSITION_ATTRIBUTE),
-         new VertexAttribute(VertexAttributes.Usage.ColorUnpacked, 4,ShaderProgram.COLOR_ATTRIBUTE)
+         new VertexAttribute(VertexAttributes.Usage.ColorUnpacked, 4,ShaderProgram.COLOR_ATTRIBUTE),
+         new VertexAttribute(SIZE_AND_ROTATION_USAGE, 3, "a_sizeAndRotation")
    );
-   private final int vertexSize = (short) (vertexAttributes.vertexSize / 4);
+   private final int vertexSize = 10;
    private final int vertexPositionOffset = (short) (vertexAttributes.findByUsage(VertexAttributes.Usage.Position).offset / 4);
    private final int vertexColorOffset = (short) (vertexAttributes.findByUsage(VertexAttributes.Usage.ColorUnpacked).offset / 4);
+   private final int vertexSizeAndPositionOffset = (short) (vertexAttributes.findByUsage(SIZE_AND_ROTATION_USAGE).offset / 4);
 
    private RecyclingArrayList<Point3D32> pointsToRender;
-   private Color color = Color.WHITE;
+   private Color color = Color.RED;
 
    public void create(int size)
    {
+      if (!POINT_SPRITES_ENABLED)
+         enablePointSprites();
+
       renderable = new Renderable();
       renderable.meshPart.primitiveType = GL20.GL_POINTS;
       renderable.meshPart.offset = 0;
@@ -38,6 +48,10 @@ public class GDXPointCloudRenderer implements RenderableProvider
       if (renderable.meshPart.mesh != null)
          renderable.meshPart.mesh.dispose();
       renderable.meshPart.mesh = new Mesh(false, size, 0, vertexAttributes);
+      ParticleShader.Config config = new ParticleShader.Config(ParticleShader.ParticleType.Point);
+      renderable.shader = new ParticleShader(renderable, config);
+//      ((ParticleShader) renderable.shader).set(ShaderProgram.COLOR_ATTRIBUTE., Color.RED);
+      renderable.shader.init();
    }
 
    public void setColor(Color color)
@@ -59,14 +73,19 @@ public class GDXPointCloudRenderer implements RenderableProvider
             int offset = i * vertexSize;
 
             Point3D32 point = pointsToRender.get(i);
-            vertices[offset + vertexPositionOffset] = point.getX32();
-            vertices[offset + vertexPositionOffset + 1] = point.getY32();
-            vertices[offset + vertexPositionOffset + 2] = point.getZ32();
+            vertices[offset] = point.getX32();
+            vertices[offset + 1] = point.getY32();
+            vertices[offset + 2] = point.getZ32();
 
-            vertices[offset + vertexColorOffset] = color.r;
-            vertices[offset + vertexColorOffset + 1] = color.g;
-            vertices[offset + vertexColorOffset + 2] = color.b;
-            vertices[offset + vertexColorOffset + 3] = color.a;
+            // color [0.0f - 1.0f]
+            vertices[offset + 3] = 0.5f; // red (not working yet)
+            vertices[offset + 4] = 0.7f; // blue
+            vertices[offset + 5] = 0.5f; // green
+            vertices[offset + 6] = 0.0f; // alpha
+
+            vertices[offset + 7] = 0.01f; // size
+            vertices[offset + 8] = 1.0f; // cosine [0-1]
+            vertices[offset + 9] = 0.0f; // sine [0-1]
          }
 
          renderable.meshPart.size = pointsToRender.size();
@@ -85,5 +104,15 @@ public class GDXPointCloudRenderer implements RenderableProvider
    {
       if (renderable.meshPart.mesh != null)
          renderable.meshPart.mesh.dispose();
+   }
+
+   private static void enablePointSprites()
+   {
+      Gdx.gl.glEnable(GL20.GL_VERTEX_PROGRAM_POINT_SIZE);
+      if (Gdx.app.getType() == Application.ApplicationType.Desktop)
+      {
+         Gdx.gl.glEnable(0x8861); // GL_POINT_OES
+      }
+      POINT_SPRITES_ENABLED = true;
    }
 }
