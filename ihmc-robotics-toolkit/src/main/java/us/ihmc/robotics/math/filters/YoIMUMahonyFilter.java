4f17d28e5053d1fe54633a4356e18efe072a8cab
Sylvain Bertrand <sbertrand@ihmc.us>
2021-03-11 10:56:11 -0600

    Cleaned up the Mahony filter.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/filters/YoIMUMahonyFilter.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/filters/YoIMUMahonyFilter.java
@@ -1,15 +1,16 @@
 package us.ihmc.robotics.math.filters;
 
-import us.ihmc.euclid.axisAngle.AxisAngle;
-import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.Axis3D;
 import us.ihmc.euclid.matrix.RotationMatrix;
+import us.ihmc.euclid.matrix.interfaces.RotationMatrixBasics;
+import us.ihmc.euclid.orientation.interfaces.Orientation3DBasics;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.euclid.tools.EuclidCoreTools;
 import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DBasics;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.euclid.tuple4D.Quaternion;
-import us.ihmc.euclid.tuple4D.interfaces.QuaternionBasics;
 import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameQuaternion;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
@@ -34,47 +35,8 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
 {
    private static final double MIN_MAGNITUDE = 1.0e-5;
 
-   public static final Vector3DReadOnly ACCELERATION_REFERENCE = new Vector3DReadOnly()
-   {
-      @Override
-      public double getX()
-      {
-         return 0;
-      }
-
-      @Override
-      public double getY()
-      {
-         return 0;
-      }
-
-      @Override
-      public double getZ()
-      {
-         return 1.0;
-      }
-   };
-
-   public static final Vector3DReadOnly NORTH_REFERENCE = new Vector3DReadOnly()
-   {
-      @Override
-      public double getX()
-      {
-         return 1.0;
-      }
-
-      @Override
-      public double getY()
-      {
-         return 0;
-      }
-
-      @Override
-      public double getZ()
-      {
-         return 0.0;
-      }
-   };
+   public static final Vector3DReadOnly ACCELERATION_REFERENCE = Axis3D.Z;
+   public static final Vector3DReadOnly NORTH_REFERENCE = Axis3D.X;
 
    private YoFrameVector3D rawAngularVelocity;
    private YoFrameVector3D rawLinearAcceleration;
@@ -95,8 +57,13 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
 
    private final ReferenceFrame sensorFrame;
 
-   public YoIMUMahonyFilter(String imuName, String nameSuffix, double updateDT, ReferenceFrame sensorFrame, YoFrameQuaternion estimatedOrientation,
-                            YoFrameVector3D estimatedAngularVelocity, YoRegistry parentRegistry)
+   public YoIMUMahonyFilter(String imuName,
+                            String nameSuffix,
+                            double updateDT,
+                            ReferenceFrame sensorFrame,
+                            YoFrameQuaternion estimatedOrientation,
+                            YoFrameVector3D estimatedAngularVelocity,
+                            YoRegistry parentRegistry)
    {
       this.updateDT = updateDT;
       this.sensorFrame = sensorFrame;
@@ -120,8 +87,7 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
       hasBeenInitialized = new YoBoolean("HasBeenInitialized" + nameSuffix, registry);
    }
 
-   public YoIMUMahonyFilter(String imuName, String namePrefix, String nameSuffix, double updateDT, ReferenceFrame sensorFrame,
-                            YoRegistry parentRegistry)
+   public YoIMUMahonyFilter(String imuName, String namePrefix, String nameSuffix, double updateDT, ReferenceFrame sensorFrame, YoRegistry parentRegistry)
    {
       this.updateDT = updateDT;
       this.sensorFrame = sensorFrame;
@@ -144,10 +110,10 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
    /**
     * Sets the input variables to use when updating this filter.
     * 
-    * @param inputAngularVelocity
-    *           the variable holding the measurements from the gyroscope. Not modified.
-    * @param inputLinearAcceleration
-    *           the variable holding the measurements from the accelerometer. Not modified.
+    * @param inputAngularVelocity    the variable holding the measurements from the gyroscope. Not
+    *                                modified.
+    * @param inputLinearAcceleration the variable holding the measurements from the accelerometer. Not
+    *                                modified.
     */
    public void setInputs(YoFrameVector3D inputAngularVelocity, YoFrameVector3D inputLinearAcceleration)
    {
@@ -157,12 +123,12 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
    /**
     * Sets the input variables to use when updating this filter.
     * 
-    * @param inputAngularVelocity
-    *           the variable holding the measurements from the gyroscope. Not modified.
-    * @param inputLinearAcceleration
-    *           the variable holding the measurements from the accelerometer. Not modified.
-    * @param inputMagneticVector
-    *           the variable holding the measurements from the magnetometer. Not modified.
+    * @param inputAngularVelocity    the variable holding the measurements from the gyroscope. Not
+    *                                modified.
+    * @param inputLinearAcceleration the variable holding the measurements from the accelerometer. Not
+    *                                modified.
+    * @param inputMagneticVector     the variable holding the measurements from the magnetometer. Not
+    *                                modified.
     */
    public void setInputs(YoFrameVector3D inputAngularVelocity, YoFrameVector3D inputLinearAcceleration, YoFrameVector3D inputMagneticVector)
    {
@@ -181,10 +147,8 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
    /**
     * Sets the gains for this filter.
     * 
-    * @param proportionalGain
-    *           gain used to correct the orientation according to the estimated error.
-    * @param integralGain
-    *           gain used to update the gyroscope bias according to the estimated error.
+    * @param proportionalGain gain used to correct the orientation according to the estimated error.
+    * @param integralGain     gain used to update the gyroscope bias according to the estimated error.
     */
    public void setGains(double proportionalGain, double integralGain)
    {
@@ -196,8 +160,7 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
     * Mostly useful for test purpose, but can be used to reinitilize this filter by giving
     * {@code false}.
     * 
-    * @param value
-    *           whether this filter has been initialized or not.
+    * @param value whether this filter has been initialized or not.
     */
    public void setHasBeenInitialized(boolean value)
    {
@@ -206,9 +169,6 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
 
    private final Vector3D rotationUpdate = new Vector3D();
    private final Quaternion quaternionUpdate = new Quaternion();
-
-   private final Vector3D totalError = new Vector3D();
-   private final Vector3D integralTerm = new Vector3D();
    private final Vector3D angularVelocityTerm = new Vector3D();
 
    @Override
@@ -246,23 +206,26 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
    public void update(Vector3DReadOnly inputAngularVelocity, Vector3DReadOnly inputLinearAcceleration, Vector3DReadOnly inputMagneticVector)
    {
       if (!hasBeenInitialized.getValue())
-
       {
          initialize(inputLinearAcceleration, inputMagneticVector);
          return;
       }
 
-      boolean success = computeOrientationError((QuaternionReadOnly) estimatedOrientation, inputLinearAcceleration, inputMagneticVector, quaternionUpdate);
+      boolean success = computeOrientationError((QuaternionReadOnly) estimatedOrientation, inputLinearAcceleration, inputMagneticVector, yoErrorTerm);
+
       if (success)
       {
-         quaternionUpdate.getRotationVector(totalError);
-         yoErrorTerm.set(totalError);
+         angularVelocityTerm.scaleAdd(proportionalGain.getValue(), yoErrorTerm, inputAngularVelocity);
 
-         integralTerm.scaleAdd(integralGain.getValue() * updateDT, yoErrorTerm, yoIntegralTerm);
-         yoIntegralTerm.set(integralTerm);
-
-         angularVelocityTerm.scaleAdd(proportionalGain.getValue(), totalError, inputAngularVelocity);
-         angularVelocityTerm.add(integralTerm);
+         if (integralGain.getValue() > 0.0)
+         {
+            yoIntegralTerm.scaleAdd(integralGain.getValue() * updateDT, yoErrorTerm, yoIntegralTerm);
+            angularVelocityTerm.add(yoIntegralTerm);
+         }
+         else
+         {
+            yoIntegralTerm.setToZero();
+         }
       }
       else
       {
@@ -275,7 +238,7 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
       estimatedOrientation.multiply(quaternionUpdate);
 
       if (estimatedAngularVelocity != null)
-         estimatedAngularVelocity.add(inputAngularVelocity, integralTerm);
+         estimatedAngularVelocity.add(inputAngularVelocity, yoIntegralTerm);
    }
 
    @Override
@@ -286,26 +249,25 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
 
    private void initialize(Vector3DReadOnly acceleration, Vector3DReadOnly magneticVector)
    {
-      boolean success = computeOrientationError(estimatedOrientation, acceleration, magneticVector, quaternionUpdate);
+      boolean success = computeRotationMatrixFromXZAxes(magneticVector, acceleration, estimatedOrientation);
 
       if (!success)
-         return;
+         estimatedOrientation.setToZero();
+      else
+         estimatedOrientation.invert();
 
-      estimatedOrientation.multiply(quaternionUpdate);
       yoIntegralTerm.setToZero();
-
       hasBeenInitialized.set(true);
    }
 
-   private final Vector3D accelerationCurrentDirectionWorld = new Vector3D();
-   private final Vector3D accelerationDesiredDirectionWorld = new Vector3D();
-   private final Vector3D magneticCurrentDirectionWorld = new Vector3D();
-   private final Vector3D magneticDesiredDirectionWorld = new Vector3D();
+   // ------------------------------------------------ //
+   // Intermediate variables for computing the orientation error
+   private final Vector3D m = new Vector3D();
+   private final Vector3D mRef = new Vector3D();
 
-   private final AxisAngle axisAngleOffset = new AxisAngle();
-
-   private final RotationMatrix rotationMatrixCurrent = new RotationMatrix();
-   private final RotationMatrix rotationMatrixDesired = new RotationMatrix();
+   private final Vector3D a = new Vector3D();
+   private final Vector3D aRef = new Vector3D();
+   // ------------------------------------------------ //
 
    /**
     * Estimates the error in orientation based on the measurements from the accelerometer and
@@ -319,78 +281,60 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
     * the error is only computed from the accelerometer data.
     * </p>
     * 
-    * @param orientation
-    *           the current estimate of the sensor orientation with respect to world. Not modified.
-    * @param acceleration
-    *           the measurement from the accelerometer. Not modified.
-    * @param magneticVector
-    *           the measurement from the magnetometer. Can be {@code null}. Not modified.
-    * @param errorToPack
-    *           the error in orientation expressed in the local frame of {@code orientation}.
-    *           Modified.
+    * @param orientation    the current estimate of the sensor orientation with respect to world. Not
+    *                       modified.
+    * @param acceleration   the measurement from the accelerometer. Not modified.
+    * @param magneticVector the measurement from the magnetometer. Can be {@code null}. Not modified.
+    * @param errorToPack    the error in the local frame of {@code orientation} to add to the angular
+    *                       velocity. Modified.
     * @return whether the method was completed successfully or not.
     */
-   private boolean computeOrientationError(QuaternionReadOnly orientation, Vector3DReadOnly acceleration, Vector3DReadOnly magneticVector,
-                                           QuaternionBasics errorToPack)
+   private boolean computeOrientationError(QuaternionReadOnly orientation,
+                                           Vector3DReadOnly acceleration,
+                                           Vector3DReadOnly magneticVector,
+                                           Vector3DBasics errorToPack)
    {
-      boolean success;
-      double accelerationLength = acceleration.length();
-
-      if (accelerationLength < MIN_MAGNITUDE)
-         return false;
-
-      accelerationCurrentDirectionWorld.setAndScale(1.0 / accelerationLength, acceleration);
-      orientation.transform(accelerationCurrentDirectionWorld);
-      accelerationDesiredDirectionWorld.set(ACCELERATION_REFERENCE);
-
-      boolean useMagneticVector = false;
+      boolean success = false;
+      errorToPack.setToZero();
 
       if (magneticVector != null)
       {
-         double magneticLength = magneticVector.length();
-
-         if (magneticLength > MIN_MAGNITUDE)
+         double norm = magneticVector.length();
+         if (norm >= MIN_MAGNITUDE)
          {
-            useMagneticVector = true;
-            magneticCurrentDirectionWorld.setAndScale(1.0 / magneticLength, magneticVector);
-            orientation.transform(magneticCurrentDirectionWorld);
+            m.setAndScale(1.0 / norm, magneticVector);
+
+            orientation.transform(m, mRef);
+            // The magnetometer is only used to correct the heading in world. So we preserve the Z-component and XY magnitude to ensure that no pitch error is generated.
+            mRef.setX(EuclidCoreTools.norm(mRef.getX(), mRef.getY()));
+            mRef.setY(0.0);
+            orientation.inverseTransform(mRef);
+
+            errorToPack.cross(m, mRef);
+            success = true;
          }
       }
 
-      if (useMagneticVector)
+      if (acceleration != null)
       {
-         success = computeRotationMatrixFromXZAxes(magneticCurrentDirectionWorld, accelerationCurrentDirectionWorld, rotationMatrixCurrent);
-         if (success)
-         {
-            // Setting the desired direction to be x+ in the xz-plane
-            magneticDesiredDirectionWorld.setX(Math.sqrt(EuclidCoreTools.normSquared(magneticCurrentDirectionWorld.getX(),
-                                                                                     magneticCurrentDirectionWorld.getY())));
-            magneticDesiredDirectionWorld.setY(0.0);
-            magneticDesiredDirectionWorld.setZ(magneticCurrentDirectionWorld.getZ());
+         double norm = acceleration.length();
 
-            success = computeRotationMatrixFromXZAxes(magneticDesiredDirectionWorld, accelerationDesiredDirectionWorld, rotationMatrixDesired);
-            if (success)
-            {
-               rotationMatrixCurrent.multiplyTransposeThis(rotationMatrixDesired);
-               errorToPack.set(rotationMatrixCurrent);
-               /*
-                * Whether the current/desired directions are expressed in world or local frame, it
-                * seems that the rotation axis of the resulting error stored in
-                * rotationMatrixCurrent is expressed in world and needs to be transformed as
-                * follows. I don't really understand how is that.
-                */
-               errorToPack.multiply(orientation);
-               errorToPack.preMultiplyConjugateOther(orientation);
-               // It seems rather important to keep the rotation angle in [-pi,pi] to prevent jumps in the estimated orientation.
-               errorToPack.normalizeAndLimitToPi();
-               return true;
-            }
+         if (norm >= MIN_MAGNITUDE)
+         {
+            a.setAndScale(1.0 / norm, acceleration);
+
+            orientation.inverseTransform(ACCELERATION_REFERENCE, aRef);
+
+            double ex = errorToPack.getX();
+            double ey = errorToPack.getY();
+            double ez = errorToPack.getZ();
+            errorToPack.cross(a, aRef);
+            errorToPack.add(ex, ey, ez);
+            success = true;
          }
       }
-      EuclidGeometryTools.orientation3DFromFirstToSecondVector3D(accelerationCurrentDirectionWorld, accelerationDesiredDirectionWorld, axisAngleOffset);
-      errorToPack.set(axisAngleOffset);
 
-      return true;
+      return success;
    }
 
    public YoFrameQuaternion getEstimatedOrientation()
@@ -411,15 +355,12 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
    /**
     * Computes the rotation matrix describing the orientation of the given x and z axes.
     * 
-    * @param xAxis
-    *           the x-axis to use in the calculation. Not modified.
-    * @param zAxis
-    *           the z-axis to use in the calculation. Not modified.
-    * @param rotationToPack
-    *           the rotation matrix in which the result is stored. Modified.
+    * @param xAxis             the x-axis to use in the calculation. Not modified.
+    * @param zAxis             the z-axis to use in the calculation. Not modified.
+    * @param orientationToPack the rotation matrix in which the result is stored. Modified.
     * @return whether the method succeeded or not.
     */
-   private static boolean computeRotationMatrixFromXZAxes(Vector3DReadOnly xAxis, Vector3DReadOnly zAxis, RotationMatrix rotationToPack)
+   private static boolean computeRotationMatrixFromXZAxes(Vector3DReadOnly xAxis, Vector3DReadOnly zAxis, Orientation3DBasics orientationToPack)
    {
       double zAxisX = zAxis.getX();
       double zAxisY = zAxis.getY();
@@ -458,7 +399,10 @@ public class YoIMUMahonyFilter implements ProcessingYoVariable
       double xAxisY = yAxisZ * zAxisX - yAxisX * zAxisZ;
       double xAxisZ = yAxisX * zAxisY - yAxisY * zAxisX;
 
-      rotationToPack.setUnsafe(xAxisX, yAxisX, zAxisX, xAxisY, yAxisY, zAxisY, xAxisZ, yAxisZ, zAxisZ);
+      if (orientationToPack instanceof RotationMatrixBasics)
+         ((RotationMatrix) orientationToPack).setUnsafe(xAxisX, yAxisX, zAxisX, xAxisY, yAxisY, zAxisY, xAxisZ, yAxisZ, zAxisZ);
+      else
+         orientationToPack.setRotationMatrix(xAxisX, yAxisX, zAxisX, xAxisY, yAxisY, zAxisY, xAxisZ, yAxisZ, zAxisZ);
 
       return true;
    }
