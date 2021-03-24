beceecad83bcd517ccf3586e2483fe75002f1fca
Stephen McCrory <smccrory@ihmc.us>
2021-03-24 17:52:50 -0500

    adding footstep graphics to collision free viewer

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/swing/CollisionFreeSwingCalculator.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/swing/CollisionFreeSwingCalculator.java
@@ -1,49 +1,81 @@
 package us.ihmc.footstepPlanning.swing;
 
+import org.apache.commons.lang3.mutable.MutableInt;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.shape.collision.gjk.GilbertJohnsonKeerthiCollisionDetector;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
 import us.ihmc.footstepPlanning.FootstepPlan;
-import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParametersReadOnly;
+import us.ihmc.footstepPlanning.PlannedFootstep;
+import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
+import us.ihmc.graphicsDescription.appearance.YoAppearance;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicPolygon;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsList;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
+import us.ihmc.log.LogTools;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.simulationconstructionset.util.TickAndUpdatable;
+import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameConvexPolygon2D;
+import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePoseUsingYawPitchRoll;
 import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class CollisionFreeSwingCalculator
 {
    private final YoRegistry registry = new YoRegistry(getClass().getSimpleName());
+   private final boolean visualize;
 
    private final SwingPlannerParametersReadOnly swingPlannerParameters;
    private final WalkingControllerParameters walkingControllerParameters;
    private final TickAndUpdatable tickAndUpdatable;
 
-   private final GilbertJohnsonKeerthiCollisionDetector collisionDetector = new GilbertJohnsonKeerthiCollisionDetector();
+   private final SideDependentList<ConvexPolygon2D> footPolygons;
    private final FramePose3D startOfSwingPose = new FramePose3D();
    private final FramePose3D endOfSwingPose = new FramePose3D();
 
    private PlanarRegionsList planarRegionsList;
+   private final SideDependentList<FootstepVisualizer[]> footstepVisualizers = new SideDependentList<>();
+   private final SideDependentList<MutableInt> footstepVisualizerIndices = new SideDependentList<>(side -> new MutableInt());
 
    public CollisionFreeSwingCalculator(SwingPlannerParametersReadOnly swingPlannerParameters,
-                                       WalkingControllerParameters walkingControllerParameters)
+                                       WalkingControllerParameters walkingControllerParameters,
+                                       SideDependentList<ConvexPolygon2D> footPolygons)
    {
-      this(swingPlannerParameters, walkingControllerParameters, null, null, null);
+      this(swingPlannerParameters, walkingControllerParameters, footPolygons, null, null, null);
    }
 
    public CollisionFreeSwingCalculator(SwingPlannerParametersReadOnly swingPlannerParameters,
                                        WalkingControllerParameters walkingControllerParameters,
+                                       SideDependentList<ConvexPolygon2D> footPolygons,
                                        TickAndUpdatable tickAndUpdatable,
                                        YoGraphicsListRegistry graphicsListRegistry,
                                        YoRegistry parentRegistry)
    {
       this.swingPlannerParameters = swingPlannerParameters;
       this.walkingControllerParameters = walkingControllerParameters;
+      this.footPolygons = footPolygons;
       this.tickAndUpdatable = tickAndUpdatable;
 
-      if (parentRegistry != null)
+      visualize = parentRegistry != null;
+      if (visualize)
       {
+         YoGraphicsList graphicsList = new YoGraphicsList(getClass().getSimpleName());
+         for (RobotSide robotSide : RobotSide.values())
+         {
+            FootstepVisualizer[] footstepVisualizerArray = new FootstepVisualizer[50];
+            for (int i = 0; i < footstepVisualizerArray.length; i++)
+            {
+               footstepVisualizerArray[i] = new FootstepVisualizer(robotSide, footPolygons.get(robotSide), graphicsList);
+            }
+
+            footstepVisualizers.put(robotSide, footstepVisualizerArray);
+         }
+
+         graphicsListRegistry.registerYoGraphicsList(graphicsList);
+
          parentRegistry.addChild(registry);
       }
    }
@@ -58,6 +90,89 @@ public class CollisionFreeSwingCalculator
       if (planarRegionsList == null || planarRegionsList.isEmpty())
       {
          return;
+      }
+
+      updateFootstepGraphics(initialStanceFootPoses, footstepPlan);
+   }
+
+   private void updateFootstepGraphics(SideDependentList<? extends Pose3DReadOnly> initialStanceFootPoses, FootstepPlan footstepPlan)
+   {
+      if (!visualize)
+      {
+         return;
+      }
+
+      // hide all
+      for (RobotSide side : RobotSide.values())
+      {
+         FootstepVisualizer[] footstepVisualizers = this.footstepVisualizers.get(side);
+         for (int i = 0; i < footstepVisualizers.length; i++)
+         {
+            footstepVisualizers[i].hide();
+         }
+
+         footstepVisualizerIndices.get(side).setValue(0);
+      }
+
+      // render stance steps
+      for (RobotSide side : RobotSide.values())
+      {
+         FootstepVisualizer footstepVisualizer = getNextFootstepVisualizer(side);
+         footstepVisualizer.visualizeFootstep(new FramePose3D(initialStanceFootPoses.get(side)));
+      }
+
+      for (int i = 0; i < footstepPlan.getNumberOfSteps(); i++)
+      {
+         PlannedFootstep footstep = footstepPlan.getFootstep(i);
+         FootstepVisualizer footstepVisualizer = getNextFootstepVisualizer(footstep.getRobotSide());
+         footstepVisualizer.visualizeFootstep(footstep.getFootstepPose());
+      }
+
+      tickAndUpdatable.tickAndUpdate();
+   }
+
+   private FootstepVisualizer getNextFootstepVisualizer(RobotSide robotSide)
+   {
+      int indexToGet = footstepVisualizerIndices.get(robotSide).getAndIncrement();
+      FootstepVisualizer[] footstepVisualizer = this.footstepVisualizers.get(robotSide);
+
+      if (indexToGet >= footstepVisualizer.length)
+      {
+         throw new RuntimeException("Not enough footstep graphics allocated");
+      }
+
+      return footstepVisualizer[indexToGet];
+   }
+
+   private static final SideDependentList<AppearanceDefinition> footPolygonAppearances = new SideDependentList<>(YoAppearance.Purple(), YoAppearance.Green());
+   private static SideDependentList<MutableInt> footGraphicIndices = new SideDependentList<>(side -> new MutableInt());
+
+   private class FootstepVisualizer
+   {
+      private final YoFramePoseUsingYawPitchRoll soleFramePose;
+      private final YoFrameConvexPolygon2D yoFootPolygon;
+      private final YoGraphicPolygon footPolygonViz;
+
+      FootstepVisualizer(RobotSide robotSide, ConvexPolygon2D footPolygon, YoGraphicsList yoGraphicsList)
+      {
+         String namePrefix = robotSide.getLowerCaseName() + "Foot" + footGraphicIndices.get(robotSide).getAndIncrement();
+         this.soleFramePose = new YoFramePoseUsingYawPitchRoll(namePrefix + "graphicPolygon", ReferenceFrame.getWorldFrame(), registry);
+         this.yoFootPolygon = new YoFrameConvexPolygon2D(namePrefix + "yoPolygon", "", ReferenceFrame.getWorldFrame(), footPolygon.getNumberOfVertices(), registry);
+         yoFootPolygon.set(footPolygon);
+         footPolygonViz = new YoGraphicPolygon(namePrefix + "graphicPolygon", yoFootPolygon, soleFramePose, 1.0, footPolygonAppearances.get(robotSide));
+         yoGraphicsList.add(footPolygonViz);
+      }
+
+      void visualizeFootstep(FramePose3DReadOnly footstepPose)
+      {
+         soleFramePose.set(footstepPose);
+         footPolygonViz.update();
+      }
+
+      void hide()
+      {
+         soleFramePose.setToNaN();
+         footPolygonViz.update();
       }
    }
 }
