2a0f087673386a5da1c7d21336da48bc99dddd8b
Stephen McCrory <smccrory@ihmc.us>
2021-03-25 18:06:35 -0500

    adding stairs test

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/swing/CollisionFreeSwingCalculatorTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/swing/CollisionFreeSwingCalculatorTest.java
@@ -8,11 +8,18 @@ import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.Pose3D;
 import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.footstepPlanning.FootstepPlan;
+import us.ihmc.footstepPlanning.FootstepPlanningModule;
 import us.ihmc.graphicsDescription.Graphics3DObject;
 import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
 import us.ihmc.graphicsDescription.appearance.YoAppearance;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
+import us.ihmc.pathPlanning.DataSet;
+import us.ihmc.pathPlanning.DataSetIOTools;
+import us.ihmc.pathPlanning.DataSetName;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 import us.ihmc.robotics.geometry.PlanarRegionsListGenerator;
 import us.ihmc.robotics.graphics.Graphics3DObjectTools;
@@ -110,6 +117,32 @@ public class CollisionFreeSwingCalculatorTest
       footstepPlan.addFootstep(steppingSide, footstep);
 
       runTest(boxOnGround, initialStance, footstepPlan);
+   }
+
+   @Test
+   public void testUpStairs()
+   {
+      DataSet stairsDataSet = DataSetIOTools.loadDataSet(DataSetName._20200513_151318_StairsIHMC_Bottom);
+      PlanarRegionsList stairRegions = stairsDataSet.getPlanarRegionsList();
+
+      SideDependentList<Pose3D> stanceSteps = new SideDependentList<>();
+      stanceSteps.put(RobotSide.LEFT,  new Pose3D(new Point3D(-0.624,  0.365, -0.042 ), new Quaternion( 0.008, -0.000,  1.000,  0.002 )));
+      stanceSteps.put(RobotSide.RIGHT, new Pose3D(new Point3D(-0.623,  0.735, -0.042 ), new Quaternion( 0.008, -0.000,  1.000,  0.000 )));
+
+      FootstepPlan footstepPlan = new FootstepPlan();
+      footstepPlan.addFootstep(RobotSide.RIGHT, new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.051,  0.720,  0.108 ), new Quaternion( 0.007, -0.001,  0.997, -0.072 )));
+      footstepPlan.addFootstep(RobotSide.LEFT,  new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.024,  0.252,  0.106 ), new Quaternion( 0.007, -0.001,  0.996, -0.087 )));
+      footstepPlan.addFootstep(RobotSide.RIGHT, new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.345,  0.550,  0.279 ), new Quaternion( 0.005,  0.000,  0.985, -0.174 )));
+      footstepPlan.addFootstep(RobotSide.LEFT,  new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.317,  0.152,  0.279 ), new Quaternion( 0.005,  0.001,  1.000, -0.000 )));
+      footstepPlan.addFootstep(RobotSide.RIGHT, new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.607,  0.503,  0.460 ), new Quaternion( 0.002,  0.001,  0.996, -0.087 )));
+      footstepPlan.addFootstep(RobotSide.LEFT,  new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.588,  0.197,  0.460 ), new Quaternion( 0.002,  0.001,  0.996, -0.088 )));
+      footstepPlan.addFootstep(RobotSide.RIGHT, new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.879,  0.352,  0.621 ), new Quaternion(-0.008,  0.000,  0.985, -0.174 )));
+      footstepPlan.addFootstep(RobotSide.LEFT,  new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-1.863,  0.148,  0.621 ), new Quaternion(-0.008, -0.001,  1.000, -0.000 )));
+      footstepPlan.addFootstep(RobotSide.RIGHT, new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-2.100,  0.514,  0.782 ), new Quaternion(-0.004, -0.002,  0.996, -0.087 )));
+      footstepPlan.addFootstep(RobotSide.LEFT,  new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-2.076,  0.150,  0.781 ), new Quaternion(-0.004, -0.002,  0.996, -0.087 )));
+      footstepPlan.addFootstep(RobotSide.RIGHT, new FramePose3D(ReferenceFrame.getWorldFrame(), new Point3D(-2.550,  0.360,  0.965 ), new Quaternion( 0.000,  0.000,  1.000,  0.001 )));
+
+      runTest(stairRegions, stanceSteps, footstepPlan);
    }
 
    private PlanarRegionsList flatGround()
