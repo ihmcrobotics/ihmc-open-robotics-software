329d844a5a8f05a10cbcd4be7dfe14bcbd277b0a
Operator Computer <OperatorComputer@unknown>
2021-03-26 14:33:08 -0500

    put in a potential fix for height smoothness

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/heightPlanning/LookAheadCoMHeightTrajectoryGenerator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/heightPlanning/LookAheadCoMHeightTrajectoryGenerator.java
@@ -415,6 +415,8 @@ public class LookAheadCoMHeightTrajectoryGenerator
                                                  startWaypointX,
                                                  startWaypointY,
                                                  startGroundHeight);
+      startMinHeight = Math.min(startMinHeight, startCoMPosition.getZ() - heightOffsetHandler.getOffsetHeightAboveGround());
+      startMaxHeight = Math.max(startMaxHeight, startCoMPosition.getZ() - heightOffsetHandler.getOffsetHeightAboveGround());
       double firstMinHeight = findWaypointHeight(minimumHeight,
                                                  hipWidth.getDoubleValue(),
                                                  startAnkleX,
@@ -429,6 +431,8 @@ public class LookAheadCoMHeightTrajectoryGenerator
                                                  firstMidpointX,
                                                  firstMidpointY,
                                                  startGroundHeight);
+      firstMinHeight = Math.min(firstMinHeight, startMinHeight);
+      firstMaxHeight = Math.min(firstMaxHeight, startMaxHeight);
 
       double exchangeInFromMinHeight = findWaypointHeight(minimumHeight,
                                                         hipWidth.getDoubleValue(),
@@ -508,6 +512,9 @@ public class LookAheadCoMHeightTrajectoryGenerator
                                                  endGroundHeight);
       double endMinHeight = minimumHeight + endGroundHeight;
       double endMaxHeight = maximumHeight + endGroundHeight;
+      
+      fourthMinHeight = Math.min(fourthMinHeight, endMinHeight);
+      fourthMaxHeight = Math.min(fourthMaxHeight, endMaxHeight);
 
       startWaypoint.setMinMax(startMinHeight, startMaxHeight);
       firstMidpoint.setMinMax(firstMinHeight, firstMaxHeight);
