e4b3e444108850f7ec75549b9cf10905edd82cca
Robert <rgriffin@ihmc.us>
2021-04-01 14:01:12 -0500

    set up the cop planner to snap the cop to the toes if its far enough forward

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/bipedPlanning/PlanForToeOffCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/bipedPlanning/PlanForToeOffCalculator.java
@@ -1,6 +1,9 @@
 package us.ihmc.commonWalkingControlModules.dynamicPlanning.bipedPlanning;
 
+import us.ihmc.euclid.referenceFrame.interfaces.FrameConvexPolygon2DReadOnly;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
+
 public interface PlanForToeOffCalculator
 {
-   boolean shouldPutCMPOnToes(double stepLength, double stepHeight);
+   boolean shouldPutCMPOnToes(double stepHeight, FramePoint3DReadOnly currentCoPPositionInSole, FrameConvexPolygon2DReadOnly supportFootPolygon);
 }
