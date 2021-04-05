c34cb2560eea00a7121315ecac49d3a1c2904b89
Duncan Calvert <dcalvert@ihmc.us>
2021-04-04 19:44:08 -0500

    Move GDX tests from internal to open.

---
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDXImGuiVRDemo.java
@@ -0,0 +1,76 @@
+package us.ihmc.gdx;
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import imgui.flag.ImGuiDir;
+import imgui.internal.ImGui;
+import us.ihmc.commons.time.Stopwatch;
+import us.ihmc.gdx.tools.GDXApplicationCreator;
+import us.ihmc.gdx.tools.GDXModelPrimitives;
+import us.ihmc.gdx.ui.GDXImGuiBasedUI;
+import us.ihmc.tools.string.StringTools;
+
+public class GDXImGuiVRDemo
+{
+   private final GDXImGuiBasedUI baseUI = new GDXImGuiBasedUI();
+
+   private final Stopwatch stopwatch = new Stopwatch().start();
+
+   public GDXImGuiVRDemo()
+   {
+      GDXApplicationCreator.launchGDXApplication(new Lwjgl3ApplicationAdapter()
+      {
+         @Override
+         public void create()
+         {
+            baseUI.create();
+
+            baseUI.getSceneManager().addModelInstance(new ModelInstance(GDXModelPrimitives.createCoordinateFrame(0.3)));
+            baseUI.getSceneManager().addModelInstance(new BoxesDemoModel().newInstance());
+
+            baseUI.getImGuiDockingSetup().splitAdd("Window", ImGuiDir.Right, 0.20);
+         }
+
+         @Override
+         public void render()
+         {
+            baseUI.pollVREvents();
+
+            baseUI.renderBeforeOnScreenUI();
+
+            ImGui.begin("Window");
+            if (ImGui.beginTabBar("main"))
+            {
+               if (ImGui.beginTabItem("Window"))
+               {
+                  ImGui.text("Tab bar detected!");
+                  ImGui.endTabItem();
+               }
+               ImGui.endTabBar();
+            }
+            ImGui.text(StringTools.format3D("Time: {} s", stopwatch.totalElapsed()).get());
+            ImGui.button("I'm a Button!");
+            float[] values = new float[100];
+            for (int i = 0; i < 100; i++)
+            {
+               values[i] = i;
+            }
+            ImGui.plotLines("Histogram", values, 100);
+            ImGui.end();
+
+            baseUI.renderEnd();
+         }
+
+         @Override
+         public void dispose()
+         {
+            baseUI.dispose();
+         }
+      }, getClass());
+   }
+
+   public static void main(String[] args)
+   {
+      System.setProperty("enable.vr", "true");
+      new GDXImGuiVRDemo();
+   }
+}
