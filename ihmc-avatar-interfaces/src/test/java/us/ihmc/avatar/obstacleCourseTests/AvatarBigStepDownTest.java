4a0d4ee85e4ea238be39a7fb0821efcb124ea5d8
Carlos Gonzalez <cgonzalez@ihmc.us>
2021-04-06 16:15:25 -0500

    check split fraction parameters using listeners on YoVariables of interest

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/obstacleCourseTests/AvatarBigStepDownTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/obstacleCourseTests/AvatarBigStepDownTest.java
@@ -8,9 +8,12 @@ import org.junit.jupiter.api.Test;
 import us.ihmc.avatar.DRCObstacleCourseStartingLocation;
 import us.ihmc.avatar.MultiRobotTestInterface;
 import us.ihmc.avatar.testTools.DRCSimulationTestHelper;
+import us.ihmc.commonWalkingControlModules.capturePoint.splitFractionCalculation.DefaultSplitFractionCalculatorParameters;
+import us.ihmc.commonWalkingControlModules.capturePoint.splitFractionCalculation.SplitFractionCalculatorParametersReadOnly;
 import us.ihmc.commonWalkingControlModules.controlModules.foot.FootControlModule;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.walkingController.states.WalkingStateEnum;
 import us.ihmc.commons.ContinuousIntegrationTools;
+import us.ihmc.commons.MathTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.geometry.BoundingBox3D;
 import us.ihmc.euclid.tuple3D.Point3D;
@@ -34,13 +37,13 @@ public abstract class AvatarBigStepDownTest implements MultiRobotTestInterface
    private static final SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromSystemProperties();
 
    private DRCSimulationTestHelper drcSimulationTestHelper;
-   private int stepCounter = 0;
+   private static final double epsilon = 1.0e-6;
 
    @BeforeEach
    public void setup()
    {
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " before test.");
-
+      simulationTestingParameters.setKeepSCSUp(true);
       simulationTestingParameters.setKeepSCSUp(simulationTestingParameters.getKeepSCSUp() && !ContinuousIntegrationTools.isRunningOnContinuousIntegrationServer());
       BambooTools.reportTestStartedMessage(simulationTestingParameters.getShowWindows());
    }
@@ -137,23 +140,33 @@ public abstract class AvatarBigStepDownTest implements MultiRobotTestInterface
       ThreadTools.sleep(1000);
       boolean success = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(2.0);
 
+      YoEnum<FootControlModule.ConstraintType> rightFootState = ((YoEnum<FootControlModule.ConstraintType>) drcSimulationTestHelper.getYoVariable("rightFootCurrentState"));
+      YoEnum<WalkingStateEnum> currentWalkingState = (YoEnum<WalkingStateEnum>) drcSimulationTestHelper.getYoVariable("walkingCurrentState");
+
+      YoDouble processedSplitFraction = (YoDouble) drcSimulationTestHelper.getYoVariable("processedFinalTransferSplitFraction");
+      YoDouble processedWeightDistribution = (YoDouble) drcSimulationTestHelper.getYoVariable("processedFinalTransferWeightDistribution");
+      YoDouble percentageWeightOnLeftFoot = (YoDouble) drcSimulationTestHelper.getYoVariable("percentageStandingWeightDistributionOnLeftFoot");
+
+      // check that the split fraction parameters change in the correct walking states
+      SplitFractionCalculatorParametersReadOnly defaultSplitFractions = new DefaultSplitFractionCalculatorParameters();
+      processedSplitFraction.addListener(v -> checkSplitFractionParameters(currentWalkingState, rightFootState, processedSplitFraction, defaultSplitFractions.getTransferSplitFractionAtFullDepth()));
+      processedWeightDistribution.addListener(v -> checkSplitFractionParameters(currentWalkingState, rightFootState, processedWeightDistribution, defaultSplitFractions.getTransferWeightDistributionAtFullDepth()));
+      percentageWeightOnLeftFoot.addListener(v -> checkPercentageWeightParameters(currentWalkingState, rightFootState, percentageWeightOnLeftFoot, 1.0-defaultSplitFractions.getTransferWeightDistributionAtFullDepth()));
+
       Quaternion footRotation = new Quaternion();
       footRotation.setToYawOrientation(selectedLocation.getStartingLocationOffset().getYaw());
 
       FootstepDataMessage firstStep = HumanoidMessageTools.createFootstepDataMessage(RobotSide.LEFT,
-              new Point3D(-5.8 + 0.15, -7.471 - 0.15, 0.05),
+              new Point3D(-5.8 + 0.15, -7.471 - 0.15, 0.0),
               footRotation);
       FootstepDataMessage secondStep = HumanoidMessageTools.createFootstepDataMessage(RobotSide.RIGHT,
-              new Point3D(-5.8 - 0.15, -7.471 + 0.15, 0.05),
+              new Point3D(-5.8 - 0.15, -7.471 + 0.15, 0.0),
               footRotation);
 
       YoBoolean forceToeOffAtJointLimit = ((YoBoolean) drcSimulationTestHelper.getYoVariable("forceToeOffAtJointLimit"));
       forceToeOffAtJointLimit.set(true);
 
       drcSimulationTestHelper.publishToController(HumanoidMessageTools.createFootstepDataListMessage(firstStep));
-
-      YoEnum<WalkingStateEnum> currentWalkingState = (YoEnum<WalkingStateEnum>) drcSimulationTestHelper.getYoVariable("walkingCurrentState");
-      currentWalkingState.addListener(v -> checkSplitFractionParameters(currentWalkingState));
 
       success = success && drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(4.0);
 
@@ -171,31 +184,49 @@ public abstract class AvatarBigStepDownTest implements MultiRobotTestInterface
       BambooTools.reportTestFinishedMessage(simulationTestingParameters.getShowWindows());
    }
 
-   private void checkSplitFractionParameters(YoEnum<WalkingStateEnum> currentWalkingState)
+   private void checkSplitFractionParameters(YoEnum<WalkingStateEnum> currentWalkingState,
+                                             YoEnum<FootControlModule.ConstraintType> rightFootCurrentState,
+                                             YoDouble splitFraction,
+                                             double splitFractionValue)
    {
-      YoDouble processedSplitFraction = (YoDouble) drcSimulationTestHelper.getYoVariable("processedFinalTransferSplitFraction");
-      YoDouble processedWeightDistribution = (YoDouble) drcSimulationTestHelper.getYoVariable("processedFinalTransferWeightDistribution");
-
-      switch (currentWalkingState.getEnumValue())
-      {
-         case WALKING_RIGHT_SUPPORT:
-         case TO_STANDING:
-            if(stepCounter < 2)
-            { // first walking on single support or first transfer to standing
-               assertEquals(0.3, processedSplitFraction.getDoubleValue());
-               assertEquals(0.8, processedWeightDistribution.getDoubleValue());
-               stepCounter++;
-            }
-            else
-            { // it's taking "square up" step, and should be using default values
-               assertEquals(0.5, processedSplitFraction.getDoubleValue());
-               assertEquals(0.5, processedWeightDistribution.getDoubleValue());
-            }
-            break;
-         default:
-            break;
+      if(MathTools.epsilonEquals(splitFraction.getDoubleValue(), splitFractionValue, epsilon))
+      {  // period during which split fractions are not the default ones
+         boolean success = currentWalkingState.getEnumValue().equals(WalkingStateEnum.TO_WALKING_RIGHT_SUPPORT) ||
+                 currentWalkingState.getEnumValue().equals(WalkingStateEnum.WALKING_RIGHT_SUPPORT) ||
+                     (currentWalkingState.getEnumValue().equals(WalkingStateEnum.TO_STANDING) &&
+                             rightFootCurrentState.getEnumValue().equals(FootControlModule.ConstraintType.TOES));
+         assertTrue(success);
       }
+      else if(MathTools.epsilonEquals(splitFraction.getDoubleValue(), 0.5, epsilon))
+      {  // changing to default split fractions must not occur when in transfer to standing
+         boolean success = !currentWalkingState.getEnumValue().equals(WalkingStateEnum.TO_STANDING);
+         assertTrue(success);
+      }
+   }
 
+   private void checkPercentageWeightParameters(YoEnum<WalkingStateEnum> currentWalkingState,
+                                                YoEnum<FootControlModule.ConstraintType> rightFootCurrentState,
+                                                YoDouble percentageWeightDistribution,
+                                                double splitFractionValue)
+   {
+      if(MathTools.epsilonEquals(percentageWeightDistribution.getDoubleValue(), splitFractionValue, epsilon))
+      {  // period during which percent weight distribution is not the default
+         boolean success = currentWalkingState.getEnumValue().equals(WalkingStateEnum.TO_WALKING_RIGHT_SUPPORT) ||
+                 currentWalkingState.getEnumValue().equals(WalkingStateEnum.WALKING_RIGHT_SUPPORT) ||
+                 rightFootCurrentState.getEnumValue().equals(FootControlModule.ConstraintType.TOES) &&
+                         (currentWalkingState.getEnumValue().equals(WalkingStateEnum.TO_STANDING) ||
+                           currentWalkingState.getEnumValue().equals(WalkingStateEnum.STANDING));
+         assertTrue(success);
+      }
+      else if(MathTools.epsilonEquals(percentageWeightDistribution.getDoubleValue(), 0.5, epsilon))
+      {  // changing to default split fractions must not occur when transferring to take "square up" step
+         boolean success = currentWalkingState.getEnumValue().equals(WalkingStateEnum.TO_WALKING_LEFT_SUPPORT) ||
+                 currentWalkingState.getEnumValue().equals(WalkingStateEnum.WALKING_LEFT_SUPPORT) ||
+                     (rightFootCurrentState.getEnumValue().equals(FootControlModule.ConstraintType.FULL) &&
+                             currentWalkingState.getEnumValue().equals(WalkingStateEnum.TO_STANDING) ||
+                             currentWalkingState.getEnumValue().equals(WalkingStateEnum.STANDING));
+         assertTrue(success);
+      }
    }
 
 }
