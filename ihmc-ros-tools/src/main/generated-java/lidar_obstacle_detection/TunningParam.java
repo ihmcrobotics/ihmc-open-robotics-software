d2e2699914dd051e7368729968cb3abe5b8c9e77
Chao Chen <cchen@gsuite.ihmc.us>
2021-04-08 15:21:35 -0400

    publishing tunning parameter

---
+++ ihmc-ros-tools/src/main/generated-java/lidar_obstacle_detection/TunningParam.java
@@ -0,0 +1,41 @@
+package lidar_obstacle_detection;
+
+public interface TunningParam extends org.ros.internal.message.Message {
+   static final java.lang.String _TYPE = "lidar_obstacle_detection/TunningParam";
+   static final java.lang.String _DEFINITION =
+          "float64 tunningParam1\n"
+         +"float64 tunningParam2\n"
+         +"float64 tunningParam3\n"
+         +"float64 tunningParam4\n"
+         +"float64 tunningParam5\n"
+         +"float64 tunningParam6\n"
+         +"float64 tunningParam7\n"
+         +"float64 tunningParam8\n"
+         +"float64 tunningParam9\n";
+   double getTunningParam1();
+   void setTunningParam1(double tunningParam1);
+
+   double getTunningParam2();
+   void setTunningParam2(double tunningParam2);
+
+   double getTunningParam3();
+   void setTunningParam3(double tunningParam3);
+
+   double getTunningParam4();
+   void setTunningParam4(double tunningParam4);
+
+   double getTunningParam5();
+   void setTunningParam5(double tunningParam5);
+
+   double getTunningParam6();
+   void setTunningParam6(double tunningParam6);
+
+   double getTunningParam7();
+   void setTunningParam7(double tunningParam7);
+
+   double getTunningParam8();
+   void setTunningParam8(double tunningParam8);
+
+   double getTunningParam9();
+   void setTunningParam9(double tunningParam9);
+}
