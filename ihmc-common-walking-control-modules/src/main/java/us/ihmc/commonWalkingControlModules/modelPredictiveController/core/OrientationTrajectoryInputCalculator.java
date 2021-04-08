4356fd680aa14115c7c51e4512da314942b310f8
Robert <rgriffin@ihmc.us>
2021-04-07 22:38:53 -0500

    got the other input calculator to maybe be working

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/core/OrientationTrajectoryInputCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/core/OrientationTrajectoryInputCalculator.java
@@ -64,4 +64,40 @@ public class OrientationTrajectoryInputCalculator
 
       return true;
    }
+
+   public boolean computeErrorMinimizationCommand(int tick, QPInputTypeA inputToPack, OrientationTrajectoryCommand command)
+   {
+      int segmentNumber = command.getSegmentNumber();
+
+      int linearVariables = LinearMPCIndexHandler.comCoefficientsPerSegment + indexHandler.getRhoCoefficientsInSegment(segmentNumber);
+      int orientationVariables = segmentNumber > 0 ? OtherSE3MPCIndexHandler.variablesPerOrientationTick : 0;
+      inputToPack.setConstraintType(ConstraintType.OBJECTIVE);
+      inputToPack.setNumberOfVariables(linearVariables + orientationVariables);
+      inputToPack.reshape(6);
+
+      inputToPack.getTaskJacobian().zero();
+      inputToPack.getTaskObjective().zero();
+
+      MatrixTools.setMatrixBlock(inputToPack.getTaskJacobian(), 0, 0, command.getBMatrix(tick), 0, 0, 6, linearVariables, 1.0);
+      CommonOps_DDRM.scale(-1.0, command.getCMatrix(tick), inputToPack.getTaskObjective());
+
+      if (segmentNumber == 0)
+      {
+         CommonOps_DDRM.multAdd(-1.0, command.getAMatrix(tick), command.getInitialError(), inputToPack.getTaskObjective());
+      }
+      else
+      {
+         MatrixTools.setMatrixBlock(inputToPack.getTaskJacobian(),
+                                    0,
+                                    indexHandler.getOrientationStartIndices(segmentNumber),
+                                    command.getAMatrix(tick),
+                                    0,
+                                    0,
+                                    6,
+                                    6,
+                                    1.0);
+      }
+
+      return true;
+   }
 }
