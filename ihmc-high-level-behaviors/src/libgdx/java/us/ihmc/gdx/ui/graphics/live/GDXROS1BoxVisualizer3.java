2cd815c716755fda102947619bcc616f21d2b778
Duncan Calvert <dcalvert@ihmc.us>
2021-04-08 21:15:02 -0500

    Use euclid types to draw the boxes.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS1BoxVisualizer3.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS1BoxVisualizer3.java
@@ -9,16 +9,17 @@ import com.badlogic.gdx.graphics.g3d.model.MeshPart;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
-import lidar_obstacle_detection.Box3DO32;
+import lidar_obstacle_detection.GDXBoxMessage;
 import lidar_obstacle_detection.GDXBoxesMessage;
 import org.lwjgl.opengl.GL32;
-import us.ihmc.avatar.networkProcessor.stereoPointCloudPublisher.BoxData;
-import us.ihmc.euclid.referenceFrame.FramePoint3D;
+import us.ihmc.euclid.referenceFrame.FrameBoundingBox3D;
+import us.ihmc.euclid.referenceFrame.FrameBox3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
-import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.gdx.mesh.GDXMultiColorMeshBuilder;
 import us.ihmc.tools.thread.MissingThreadTools;
 import us.ihmc.tools.thread.ResettableExceptionHandlingExecutorService;
@@ -29,30 +30,46 @@ import java.util.ArrayList;
 
 public class GDXROS1BoxVisualizer3 implements RenderableProvider
 {
-   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
-   private static final int MAX_POINTS = 50000;
-
    private final ResettableExceptionHandlingExecutorService executorService = MissingThreadTools.newSingleThreadExecutor(getClass().getSimpleName(), true, 1);
    private final ModelBuilder modelBuilder = new ModelBuilder();
    private ModelInstance modelInstance;
    private Model lastModel;
    private volatile Runnable toRender = null;
 
-   private final ReferenceFrame baseBlockFrame;
-   private final FramePoint3D point1 = new FramePoint3D();
-   private final FramePoint3D point2 = new FramePoint3D();
-   private final FramePoint3D point3 = new FramePoint3D();
-   private final FramePoint3D point4 = new FramePoint3D();
-   private final FramePoint3D point5 = new FramePoint3D();
-   private final FramePoint3D point6 = new FramePoint3D();
-   private final FramePoint3D point7 = new FramePoint3D();
-   private final FramePoint3D point8 = new FramePoint3D();
+   private final ReferenceFrame sensorFrame;
+   private final FrameBoundingBox3D boundingBox = new FrameBoundingBox3D();
+   private final FrameBox3D box = new FrameBox3D();
+   private final Point3D center = new Point3D();
+   private final Quaternion zeroOrientation = new Quaternion();
+   private final Point3D[] vertices = new Point3D[8];
+   private final ArrayList<Point3DReadOnly> orderedVerticesForDrawing;
 
    public GDXROS1BoxVisualizer3(RosMainNode ros1Node, String ros1BoxTopic, ReferenceFrame sensorBaseFrame, RigidBodyTransformReadOnly baseToSensorTransform)
    {
-      baseBlockFrame = ReferenceFrameTools.constructFrameWithUnchangingTransformFromParent("baseFrame",
-                                                                                           ReferenceFrame.getWorldFrame(),
-                                                                                           baseToSensorTransform);
+      sensorFrame = ReferenceFrameTools.constructFrameWithUnchangingTransformFromParent("baseFrame",
+                                                                                        ReferenceFrame.getWorldFrame(),
+                                                                                        baseToSensorTransform);
+      for (int i = 0; i < vertices.length; i++)
+      {
+         vertices[i] = new Point3D();
+      }
+      orderedVerticesForDrawing = new ArrayList<>();
+      orderedVerticesForDrawing.add(vertices[0]); // x+y+z+  draw top
+      orderedVerticesForDrawing.add(vertices[1]); // x-y-z+
+      orderedVerticesForDrawing.add(vertices[3]); // x-y+z+
+      orderedVerticesForDrawing.add(vertices[2]); // x+y-z+
+      orderedVerticesForDrawing.add(vertices[0]); // x+y+z+
+      orderedVerticesForDrawing.add(vertices[4]); // x+y+z-  go down
+      orderedVerticesForDrawing.add(vertices[5]); // x-y-z-  leg 1
+      orderedVerticesForDrawing.add(vertices[1]); // x-y-z+
+      orderedVerticesForDrawing.add(vertices[5]); // x-y-z-
+      orderedVerticesForDrawing.add(vertices[7]); // x-y+z-  leg 2
+      orderedVerticesForDrawing.add(vertices[3]); // x-y+z+
+      orderedVerticesForDrawing.add(vertices[7]); // x-y+z-
+      orderedVerticesForDrawing.add(vertices[6]); // x+y-z-  leg 3
+      orderedVerticesForDrawing.add(vertices[2]); // x+y-z+
+      orderedVerticesForDrawing.add(vertices[6]); // x+y-z-
+      orderedVerticesForDrawing.add(vertices[4]); // x+y+z-  leg 4
 
       ros1Node.attachSubscriber(ros1BoxTopic, new AbstractRosTopicSubscriber<GDXBoxesMessage>(GDXBoxesMessage._TYPE)
       {
@@ -80,58 +97,32 @@ public class GDXROS1BoxVisualizer3 implements RenderableProvider
 
    public synchronized void generateMeshes(GDXBoxesMessage boxes)
    {
-      BoxData boxData = new BoxData(boxes, MAX_POINTS);
-      boxData.flipToZUp();
-
-      Box3DO32[] boxesData = boxData.applyTransform(new RigidBodyTransform());
+      double lineWidth = 0.03;
       GDXMultiColorMeshBuilder meshBuilder = new GDXMultiColorMeshBuilder();
-      for (Box3DO32 box : boxesData)
+      for (GDXBoxMessage message : boxes.getBoxes())
       {
-         point1.setIncludingFrame(baseBlockFrame, box.getPoint1());
-         point2.setIncludingFrame(baseBlockFrame, box.getPoint2());
-         point3.setIncludingFrame(baseBlockFrame, box.getPoint3());
-         point4.setIncludingFrame(baseBlockFrame, box.getPoint4());
-         point5.setIncludingFrame(baseBlockFrame, box.getPoint5());
-         point6.setIncludingFrame(baseBlockFrame, box.getPoint6());
-         point7.setIncludingFrame(baseBlockFrame, box.getPoint7());
-         point8.setIncludingFrame(baseBlockFrame, box.getPoint8());
+         boundingBox.setToZero(sensorFrame);
+         // Be robust to incorrect incoming data
+         double xMin = Math.min(message.getXMin(), message.getXMax());
+         double yMin = Math.min(message.getYMin(), message.getYMax());
+         double zMin = Math.min(message.getZMin(), message.getZMax());
+         double xMax = Math.max(message.getXMin(), message.getXMax());
+         double yMax = Math.max(message.getYMin(), message.getYMax());
+         double zMax = Math.max(message.getZMin(), message.getZMax());
+         boundingBox.set(xMin, yMin, zMin, xMax, yMax, zMax);
+         boundingBox.getCenterPoint(center);
+         box.setIncludingFrame(sensorFrame,
+                               center,
+                               zeroOrientation,
+                               boundingBox.getMaxX() - boundingBox.getMinX(),
+                               boundingBox.getMaxY() - boundingBox.getMinY(),
+                               boundingBox.getMaxZ() - boundingBox.getMinZ());
+         box.changeFrame(ReferenceFrame.getWorldFrame());
+         box.getVertices(vertices);
 
-         point1.changeFrame(worldFrame);
-         point2.changeFrame(worldFrame);
-         point3.changeFrame(worldFrame);
-         point4.changeFrame(worldFrame);
-         point5.changeFrame(worldFrame);
-         point6.changeFrame(worldFrame);
-         point7.changeFrame(worldFrame);
-         point8.changeFrame(worldFrame);
-
-         double lineWidth = 0.03;
-         ArrayList<Point3DReadOnly> orderedVertices = new ArrayList<>();
-
-         orderedVertices.add(point1); // x+y+z+  draw top
-         orderedVertices.add(point2); // x-y-z+
-         orderedVertices.add(point4); // x-y+z+
-         orderedVertices.add(point3); // x+y-z+
-         orderedVertices.add(point1); // x+y+z+
-
-         orderedVertices.add(point5); // x+y+z-  go down
-
-         orderedVertices.add(point6); // x-y-z-  leg 1
-         orderedVertices.add(point2); // x-y-z+
-         orderedVertices.add(point6); // x-y-z-
-
-         orderedVertices.add(point8); // x-y+z-  leg 2
-         orderedVertices.add(point4); // x-y+z+
-         orderedVertices.add(point8); // x-y+z-
-
-         orderedVertices.add(point7); // x+y-z-  leg 3
-         orderedVertices.add(point3); // x+y-z+
-         orderedVertices.add(point7); // x+y-z-
-
-         orderedVertices.add(point5); // x+y+z-  leg 4
-
-         meshBuilder.addMultiLine(orderedVertices, lineWidth, Color.RED, false);
+         meshBuilder.addMultiLine(orderedVerticesForDrawing, lineWidth, Color.RED, false);
       }
+
       toRender = () ->
       {
          modelBuilder.begin();
