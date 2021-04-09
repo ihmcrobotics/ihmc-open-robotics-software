aac6326fb4ec3a9f1b3cd6b766b867110e94aa2e
Carlos Gonzalez <cgonzalez@ihmc.us>
2021-04-09 12:05:52 -0500

    added some test cases for big step ups

--- atlas/src/test/java/us/ihmc/atlas/ObstacleCourseTests/AtlasCustomSteppingStonesTest.java
+++ atlas/src/test/java/us/ihmc/atlas/ObstacleCourseTests/AtlasCustomSteppingStonesTest.java
@@ -1,86 +1,46 @@
 package us.ihmc.atlas.ObstacleCourseTests;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
-import us.ihmc.atlas.parameters.AtlasToeOffParameters;
-import us.ihmc.atlas.parameters.AtlasWalkingControllerParameters;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.avatar.obstacleCourseTests.AvatarToeOffTest;
-import us.ihmc.commonWalkingControlModules.configurations.ToeOffParameters;
-import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.avatar.obstacleCourseTests.AvatarCustomSteppingStonesTest;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner;
 
-public class AtlasCustomSteppingStonesTest extends AvatarToeOffTest
+public class AtlasCustomSteppingStonesTest extends AvatarCustomSteppingStonesTest
 {
-    @Test
-    public void testToeOffWithDifferentStepLengths(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
-    {
-        super.testToeOffWithDifferentStepLengths(testInfo);
-    }
-
-    @Test
-    @Disabled
-    public void testToeOffTakingShortStepDownCheckingAnkleLimits(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
-    {
-        setCheckAnkleLimits(true);
-        super.testToeOffTakingStep(testInfo, -0.1);
-    }
-
-    @Test
-    @Disabled
-    public void testToeOffTakingShortStepDownCheckingAnkleLimitsWithExperimentalPhysicsEngine(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
-    {
-        setUseExperimentalPhysicsEngine(true);
-        setCheckAnkleLimits(true);
-        super.testToeOffTakingStep(testInfo, -0.1);
-    }
-
-    @Test
-    @Disabled
-    public void testToeOffTakingMediumStepDownCheckingAnkleLimits(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
-    {
-        setCheckAnkleLimits(true);
-        super.testToeOffTakingStep(testInfo, -0.2);
-    }
-
-    @Test
-    @Disabled
-    public void testToeOffTakingHighStepDownCheckingAnkleLimits(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
-    {
-        setCheckAnkleLimits(true);
-        super.testToeOffTakingStep(testInfo, 0.3);
-    }
-
     @Test
     public void testToeOffTakingBigStepsUp(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
     {
-        //TODO remove as it replicates {@link us.ihmc.atlas.roughTerrainWalking.AtlasEndToEndStairsTest#testUpStairs(TestInfo)}
+        /**
+         *  TODO remove as it assimilates {@link us.ihmc.atlas.roughTerrainWalking.AtlasEndToEndStairsTest#testUpStairs(TestInfo)}
+         */
+        changeWalkingParameters(0.3, 0.95);
         setNumberOfSteps(4);
-        super.testToeOffTakingStep(testInfo, 0.2);
+        super.testTakingStep(testInfo, 0.25);
     }
 
     @Test
-    public void testToeOffTakingBigStepUpAndStopping(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
+    public void testTakingBigStepUpAndStopping(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
     {   // NOTE this passes but violates knee joint limits
         setTakeSquareUpStep(false);
-        super.testToeOffTakingStep(testInfo, 0.4);
+        super.testTakingStep(testInfo, 0.4);
     }
 
     @Test
-    public void testToeOffTakingBigStepUpAndSquaringUp(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
+    public void testTakingBigStepUpAndSquaringUp(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
     {   // NOTE this passes but the shins collide with stair step
-        super.testToeOffTakingStep(testInfo, 0.4);
+        super.testTakingStep(testInfo, 0.4);
     }
 
     @Test
     public void testToeOffTakingBigSideStepUp(TestInfo testInfo) throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
     {
-        super.testToeOffTakingStep(testInfo, 0.4, -0.2);
+        changeWalkingParameters(0.25, 0.8);
+        super.testTakingStep(testInfo, 0.35, -0.25);
     }
 
     @Override
@@ -90,60 +50,9 @@ public class AtlasCustomSteppingStonesTest extends AvatarToeOffTest
     }
 
     @Override
-    public int getNumberOfSteps()
-    {
-        return 4;
-    }
-
-    @Override
-    public double getMaxStepLength()
-    {
-        return 1.1;
-    }
-
-    @Override
     public DRCRobotModel getRobotModel()
     {
-        AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false)
-        {
-            @Override
-            public WalkingControllerParameters getWalkingControllerParameters()
-            {
-                return new AtlasWalkingControllerParameters(getTarget(), getJointMap(), getContactPointParameters())
-                {
-                    @Override
-                    public ToeOffParameters getToeOffParameters()
-                    {
-                        return new AtlasToeOffParameters(getJointMap())
-                        {
-                            @Override
-                            public boolean doToeOffIfPossibleInSingleSupport()
-                            {
-                                return true;
-                            }
-
-                            @Override
-                            public double getECMPProximityForToeOff()
-                            {
-                                return 0.04;
-                            }
-
-                            @Override
-                            public double getICPProximityForToeOff()
-                            {
-                                return 0.0;
-                            }
-
-                            @Override
-                            public double getICPPercentOfStanceForSSToeOff()
-                            {
-                                return 0.25;
-                            }
-                        };
-                    }
-                };
-            }
-        };
+        AtlasRobotModel robotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS, false);
         return robotModel;
     }
 
