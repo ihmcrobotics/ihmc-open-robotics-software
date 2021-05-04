a136f9968080a8a569bb5b1801b6199302649aaf
Stephen McCrory <smccrory@ihmc.us>
2021-05-04 10:45:46 -0500

    Extracting multi-contact environments from position control simulation

--- valkyrie/src/main/java/us/ihmc/valkyrie/simulation/ValkyriePlanarRegionPositionControlSimulation.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/simulation/ValkyriePlanarRegionPositionControlSimulation.java
@@ -59,30 +59,17 @@ public class ValkyriePlanarRegionPositionControlSimulation
                                                                                                         ROS2Tools.IHMC_ROOT.withTypeName(GroundPlaneMessage.class));
    private final GroundPlaneMessage groundPlaneMessage = new GroundPlaneMessage();
 
-   public enum Environment
-   {
-      FLAT_GROUND,
-      FLAT_IN_FRONT,
-      _30DEG_SLOPE_IN_FRONT,
-      FLAT_ON_SIDE,
-      _30DEG_SLOPE_ON_SIDE,
-      GROUND_AND_WALLS,
-      TILTED_TILES,
-      BOXES_FOR_HELP_GETTING_DOWN
-   }
-
-   public static Environment environment = Environment.FLAT_ON_SIDE;
-
    public enum InitialPose
    {
       STANDING,
       DOWN_ON_ALL_FOURS
    }
+
    public static InitialPose initialPose = InitialPose.DOWN_ON_ALL_FOURS;
 
    public ValkyriePlanarRegionPositionControlSimulation(boolean headless)
    {
-      PlanarRegionsList planarRegionsList = createPlanarRegions();
+      PlanarRegionsList planarRegionsList = ValkyrieMultiContactEnvironment.createPlanarRegions();
       PlanarRegionsListDefinedEnvironment environment = new PlanarRegionsListDefinedEnvironment(planarRegionsList, 0.01, false);
 
       groundPlaneMessage.getRegionNormal().set(Axis3D.Z);
@@ -184,115 +171,5 @@ public class ValkyriePlanarRegionPositionControlSimulation
 
       KinematicsToolboxModule kinematicsToolboxModule = new KinematicsToolboxModule(robotModel, false, 10, false, DomainFactory.PubSubImplementation.FAST_RTPS);
       simulationStarter.getAvatarSimulation().getSimulationConstructionSet().addYoRegistry(kinematicsToolboxModule.getRegistry());
-   }
-
-   public static PlanarRegionsList createPlanarRegions()
-   {
-      PlanarRegionsListGenerator generator = new PlanarRegionsListGenerator();
-
-      switch (environment)
-      {
-         case GROUND_AND_WALLS:
-         {
-            generator.addRectangle(5.0, 5.0);
-
-            double wallHeight = 2.0;
-            double wallSpacing = 1.5;
-
-            generator.translate(0.0, 0.0, 0.5 * wallHeight);
-            for (RobotSide robotSide : RobotSide.values)
-            {
-               generator.translate(0.0, 0.5 * robotSide.negateIfRightSide(wallSpacing), 0.0);
-               generator.rotate(robotSide.negateIfRightSide(0.5 * Math.PI), Axis3D.X);
-               generator.addRectangle(1.0, wallHeight);
-               generator.rotate(robotSide.negateIfLeftSide(0.5 * Math.PI), Axis3D.X);
-               generator.translate(0.0, 0.5 * robotSide.negateIfLeftSide(wallSpacing), 0.0);
-            }
-            generator.translate(0.0, 0.0, -0.5 * wallHeight);
-
-            generator.translate(0.9, 0.0, 0.3);
-            generator.rotate(-0.25 * Math.PI, Axis3D.Y);
-            generator.addRectangle(0.7, 0.7);
-
-            return generator.getPlanarRegionsList();
-         }
-         case FLAT_IN_FRONT:
-         {
-            generator.addRectangle(5.0, 5.0);
-
-            double offsetX = 0.5;
-            double boxDepth = 0.5;
-            double boxWidth = 1.0;
-            double boxHeight = 0.25;
-
-            generator.translate(offsetX + 0.5 * boxDepth, 0.0, 0.0);
-            generator.addCubeReferencedAtBottomMiddle(boxDepth, boxWidth, boxHeight);
-            return generator.getPlanarRegionsList();
-         }
-         case _30DEG_SLOPE_IN_FRONT:
-         {
-            generator.addRectangle(5.0, 5.0);
-
-            double angle = Math.toRadians(30.0);
-            generator.translate(0.5, 0.0, 0.15);
-            generator.rotate(-angle, Axis3D.Y);
-
-            double lengthX = 0.7;
-            generator.translate(0.5 * lengthX, 0.0, 0.0);
-            generator.addRectangle(lengthX, 1.0);
-            return generator.getPlanarRegionsList();
-         }
-         case FLAT_ON_SIDE:
-         {
-            generator.addRectangle(5.0, 5.0);
-
-            double offsetX = 0.2;
-            double offsetY = 0.5;
-            double boxLength = 1.0;
-            double boxWidth = 0.5;
-            double boxHeight = 0.25;
-
-            for (RobotSide robotSide : RobotSide.values)
-            {
-               generator.identity();
-               generator.translate(offsetX, robotSide.negateIfRightSide(offsetY + 0.5 * boxWidth), 0.0);
-               generator.addCubeReferencedAtBottomMiddle(boxLength, boxWidth, boxHeight);
-            }
-
-            return generator.getPlanarRegionsList();
-         }
-         case BOXES_FOR_HELP_GETTING_DOWN:
-         {
-            generator.addRectangle(5.0, 5.0);
-
-            double offsetX = 0.3;
-            double offsetY = 0.35;
-            double boxLength = 0.3;
-            double boxWidth = 0.5;
-
-            for (RobotSide robotSide : RobotSide.values)
-            {
-               generator.identity();
-               generator.translate(offsetX, robotSide.negateIfRightSide(offsetY + 0.5 * boxWidth), 0.0);
-               generator.addCubeReferencedAtBottomMiddle(boxLength, boxWidth, 0.35);
-
-               generator.translate(boxLength, 0.0, 0.0);
-               generator.addCubeReferencedAtBottomMiddle(boxLength, boxWidth, 0.18);
-
-               generator.translate(boxLength, 0.0, 0.0);
-               generator.addCubeReferencedAtBottomMiddle(boxLength, boxWidth, 0.10);
-            }
-
-            return generator.getPlanarRegionsList();
-         }
-
-         case FLAT_GROUND:
-         default:
-         {
-            generator.translate(0.0, 0.0, 0.0);
-            generator.addRectangle(5.0, 5.0);
-            return generator.getPlanarRegionsList();
-         }
-      }
    }
 }
