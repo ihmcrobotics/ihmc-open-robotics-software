5229a9ee39801926b2e6ff0a8ea24ff4944a08da
Duncan Calvert <dcalvert@ihmc.us>
2021-05-10 17:53:41 -0500

    :arrow_up: ihmc-jmonkey-engine-toolkit 0.19.7

--- valkyrie/build.gradle.kts
+++ valkyrie/build.gradle.kts
@@ -32,7 +32,7 @@ mainDependencies {
    api("us.ihmc:ihmc-yovariables:0.9.9")
    api("us.ihmc:ihmc-realtime:1.3.1")
    api("us.ihmc:ihmc-ros-control:0.6.0")
-   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.5")
+   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.7")
    api("us.ihmc:simulation-construction-set:0.21.8")
    api("us.ihmc:ihmc-graphics-description:0.19.3")
    api("us.ihmc:ihmc-robot-description:0.21.2")
