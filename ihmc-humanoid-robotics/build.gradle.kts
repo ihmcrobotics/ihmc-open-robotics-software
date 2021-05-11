c3fd5ac9f48068c19943098b8bad4bccbc4dcea2
Sylvain Bertrand <sbertrand@ihmc.us>
2021-05-11 17:09:18 -0500

    :arrow-up: ihmc-graphics-description 0.19.4

--- ihmc-humanoid-robotics/build.gradle.kts
+++ ihmc-humanoid-robotics/build.gradle.kts
@@ -22,7 +22,7 @@ mainDependencies {
 
    api("us.ihmc:euclid:0.16.2")
    api("us.ihmc:ihmc-yovariables:0.9.9")
-   api("us.ihmc:ihmc-graphics-description:0.19.3")
+   api("us.ihmc:ihmc-graphics-description:0.19.4")
    api("us.ihmc:ihmc-java-toolkit:source")
    api("us.ihmc:ihmc-robotics-toolkit:source")
    api("us.ihmc:ihmc-communication:source")
--- ihmc-model-file-loader/build.gradle.kts
+++ ihmc-model-file-loader/build.gradle.kts
@@ -23,7 +23,7 @@ mainDependencies {
 
    api("us.ihmc:euclid:0.16.2")
    api("us.ihmc:ihmc-robot-description:0.21.2")
-   api("us.ihmc:ihmc-graphics-description:0.19.3")
+   api("us.ihmc:ihmc-graphics-description:0.19.4")
    api("us.ihmc:ihmc-javafx-toolkit:0.19.3")
    api("us.ihmc:ihmc-robotics-toolkit:source")
 }
