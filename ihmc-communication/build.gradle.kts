22e03d846628a1f89c5307d5b85ec98b394b17a8
Duncan Calvert <dcalvert@ihmc.us>
2021-05-11 15:49:03 -0500

    :arrow_up: ihmc-cd 1.20
    Conflicts:
    	ihmc-high-level-behaviors/build.gradle.kts

--- ihmc-communication/build.gradle.kts
+++ ihmc-communication/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.ihmc-ci") version "7.4"
-   id("us.ihmc.ihmc-cd") version "1.17"
+   id("us.ihmc.ihmc-cd") version "1.20"
    id("us.ihmc.log-tools-plugin") version "0.6.1"
 }
 
