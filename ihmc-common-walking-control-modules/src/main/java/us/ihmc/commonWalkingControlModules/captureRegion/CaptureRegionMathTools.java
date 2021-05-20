06276611dd95975df9427bd3ad2b8f33e2b46998
Robert <rgriffin@ihmc.us>
2021-05-20 15:59:21 -0500

    made the capture region viz more usable

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/CaptureRegionMathTools.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/CaptureRegionMathTools.java
@@ -1,55 +1,14 @@
 package us.ihmc.commonWalkingControlModules.captureRegion;
 
-import us.ihmc.euclid.axisAngle.AxisAngle;
-import us.ihmc.euclid.referenceFrame.FramePoint2D;
-import us.ihmc.euclid.referenceFrame.FrameVector2D;
+import us.ihmc.commons.MathTools;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameVector2DReadOnly;
 import us.ihmc.euclid.transform.RigidBodyTransform;
-import us.ihmc.euclid.tuple3D.Vector3D;
-import us.ihmc.commons.MathTools;
 
 public class CaptureRegionMathTools
 {
-   /**
-    * Takes a line and a circle and computes the intersection. If there is no intersection sets NaN.
-    */
-   public static void solveIntersectionOfRayAndCircle(FramePoint2D pointA, FramePoint2D pointB, FrameVector2D vector, double R, FramePoint2D pointToPack)
-   {
-      // Look at JPratt Notes February 18, 2009 for details on the following:
-
-      pointA.checkReferenceFrameMatch(pointB);
-      pointA.checkReferenceFrameMatch(vector);
-
-      double Ax = pointA.getX();
-      double Ay = pointA.getY();
-
-      double Bx = pointB.getX();
-      double By = pointB.getY();
-
-      double vx = vector.getX();
-      double vy = vector.getY();
-
-      double A = (vx * vx + vy * vy);
-      double B = (2.0 * vx * (Bx - Ax) + 2.0 * vy * (By - Ay));
-      double C = (Bx - Ax) * (Bx - Ax) + (By - Ay) * (By - Ay) - R * R;
-
-      double insideSqrt = B * B - 4 * A * C;
-
-      if (insideSqrt < 0.0)
-      {
-         pointToPack.set(Double.NaN, Double.NaN);
-         return;
-      }
-
-      double l2 = (-B + Math.sqrt(insideSqrt)) / (2.0 * A);
-
-      pointToPack.changeFrame(pointA.getReferenceFrame());
-      pointToPack.set(pointB.getX() + l2 * vector.getX(), pointB.getY() + l2 * vector.getY());
-   }
-
    private final RigidBodyTransform rotation = new RigidBodyTransform();
    private final FrameVector3D rotatedFromA = new FrameVector3D();
 
@@ -70,7 +29,7 @@ public class CaptureRegionMathTools
       alpha = MathTools.clamp(alpha, 0.0, 1.0);
 
       double angleBetweenDirections = Math.abs(directionA.angle(directionB));
-      double angleBetweenDirectionsToSetLine = angleBetweenDirections * alpha;
+      double angleBetweenDirectionsToSetLine = -angleBetweenDirections * alpha;
 
       rotatedFromA.setIncludingFrame(directionA, 0.0);
 
