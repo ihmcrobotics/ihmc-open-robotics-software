606725efc4b768b66e0944916d5ac05d5634543d
Robert <rgriffin@ihmc.us>
2021-05-20 14:39:40 -0500

    did some capture region math tools cleanup

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/CaptureRegionMathTools.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/CaptureRegionMathTools.java
@@ -4,29 +4,15 @@ import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.referenceFrame.FramePoint2D;
 import us.ihmc.euclid.referenceFrame.FrameVector2D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameVector2DReadOnly;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.commons.MathTools;
 
 public class CaptureRegionMathTools
 {
-   /**
-   * This function computes the position of the capture point after the time dt has passed given
-   * the current capture point and the Cop which is assumed to be at constant position over dt.
-   */
-   public static void predictCapturePoint(FramePoint2D ICP, FramePoint2D CoP, double dt, double omega0, FramePoint2D predictedICPtoPack)
-   {
-      // make sure everything is in the same frame:
-      ICP.checkReferenceFrameMatch(CoP);
-      ICP.checkReferenceFrameMatch(predictedICPtoPack);
-
-      // CP = (ICP - CoP) * exp(omega0*dt) + CoP
-      predictedICPtoPack.set(ICP);
-      predictedICPtoPack.sub(CoP);
-      predictedICPtoPack.scale(Math.exp(omega0 * dt));
-      predictedICPtoPack.add(CoP);
-   }
-
    /**
     * Takes a line and a circle and computes the intersection. If there is no intersection sets NaN.
     */
@@ -64,38 +50,36 @@ public class CaptureRegionMathTools
       pointToPack.set(pointB.getX() + l2 * vector.getX(), pointB.getY() + l2 * vector.getY());
    }
 
-   private final Vector3D negZRotationAxis = new Vector3D(0.0, 0.0, -1.0);
    private final RigidBodyTransform rotation = new RigidBodyTransform();
    private final FrameVector3D rotatedFromA = new FrameVector3D();
-   private final AxisAngle axisAngle = new AxisAngle();
 
    /**
    * Will return a point on a circle around the origin. The point will be in between the given directions and at
    * a position specified by the alpha value. E.g. an alpha value of 0.5 will result in the point being in the
    * middle of the given directions.
    */
-   public void getPointBetweenVectorsAtDistanceFromOriginCircular(FrameVector2D directionA, FrameVector2D directionB, double alpha, double radius,
-         FramePoint2D midpoint, FramePoint2D pointToPack)
+   public void getPointBetweenVectorsAtDistanceFromOriginCircular(FrameVector2DReadOnly directionA,
+                                                                  FrameVector2DReadOnly directionB,
+                                                                  double alpha,
+                                                                  double radius,
+                                                                  FramePoint2DReadOnly centerOfCircle,
+                                                                  FramePoint2DBasics pointToPack)
    {
       directionA.checkReferenceFrameMatch(directionB.getReferenceFrame());
-      directionA.checkReferenceFrameMatch(midpoint.getReferenceFrame());
+      directionA.checkReferenceFrameMatch(centerOfCircle.getReferenceFrame());
       alpha = MathTools.clamp(alpha, 0.0, 1.0);
 
       double angleBetweenDirections = Math.abs(directionA.angle(directionB));
       double angleBetweenDirectionsToSetLine = angleBetweenDirections * alpha;
 
-      rotatedFromA.setToZero(directionA.getReferenceFrame());
-      rotatedFromA.set(directionA.getX(), directionA.getY(), 0.0);
+      rotatedFromA.setIncludingFrame(directionA, 0.0);
 
-      axisAngle.set(negZRotationAxis, angleBetweenDirectionsToSetLine);
-      rotation.getRotation().set(axisAngle);
+      rotation.setRotationYawAndZeroTranslation(angleBetweenDirectionsToSetLine);
       rotatedFromA.applyTransform(rotation);
 
-      rotatedFromA.normalize();
-      rotatedFromA.scale(radius);
+      rotatedFromA.scale(radius / rotatedFromA.length());
 
-      pointToPack.changeFrame(rotatedFromA.getReferenceFrame());
-      pointToPack.set(rotatedFromA.getX(), rotatedFromA.getY());
-      pointToPack.add(midpoint);
+      pointToPack.setIncludingFrame(rotatedFromA);
+      pointToPack.add(centerOfCircle);
    }
 }
