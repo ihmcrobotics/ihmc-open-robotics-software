381fea31ee12656568fd7abc1e53c96bd56b5f8c
Robert <rgriffin@ihmc.us>
2021-05-21 14:15:27 -0500

    got the multi-step calculator working

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/MultiStepAchievableCaptureRegionCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/MultiStepAchievableCaptureRegionCalculator.java
@@ -1,22 +1,22 @@
 package us.ihmc.commonWalkingControlModules.captureRegion;
 
+import us.ihmc.commonWalkingControlModules.capturePoint.CapturePointTools;
 import us.ihmc.commons.lists.RecyclingArrayList;
-import us.ihmc.euclid.referenceFrame.FrameConvexPolygon2D;
-import us.ihmc.euclid.referenceFrame.FramePoint2D;
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.referenceFrame.interfaces.FrameConvexPolygon2DBasics;
-import us.ihmc.euclid.referenceFrame.interfaces.FrameConvexPolygon2DReadOnly;
-import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
-import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryPolygonTools;
+import us.ihmc.euclid.referenceFrame.*;
+import us.ihmc.euclid.referenceFrame.interfaces.*;
+import us.ihmc.graphicsDescription.appearance.YoAppearance;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicPosition;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.graphicsDescription.yoGraphics.plotting.YoArtifactPolygon;
+import us.ihmc.robotics.geometry.ConvexPolygonTools;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameConvexPolygon2D;
+import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePoint2D;
 import us.ihmc.yoVariables.registry.YoRegistry;
 
 import java.awt.*;
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,13 +27,23 @@ public class MultiStepAchievableCaptureRegionCalculator
    private static final boolean VISUALIZE = true;
    private final YoRegistry registry = new YoRegistry(getClass().getSimpleName());
 
+   private final SideDependentList<? extends ReferenceFrame> soleZUpFrames;
+
    private final ReachableFootholdsCalculator reachableFootholdsCalculator;
    private final AchievableCaptureRegionCalculatorWithDelay captureRegionCalculator;
 
-   private final RecyclingArrayList<FramePoint2DBasics> currentICPVertices = new RecyclingArrayList<>(FramePoint2D::new);
-   private final RecyclingArrayList<FrameConvexPolygon2DBasics> captureRegions = new RecyclingArrayList<>(FrameConvexPolygon2D::new);
-   private final List<YoFrameConvexPolygon2D> yoCaptureRegions = new ArrayList<>();
+   private final ConvexPolygonTools polygonTools = new ConvexPolygonTools();
 
+   private final FrameConvexPolygon2D reachableRegion = new FrameConvexPolygon2D();
+   private final FrameConvexPolygon2D captureRegion = new FrameConvexPolygon2D();
+
+   private final List<YoFramePoint2D> capturePointsAtTouchdown = new ArrayList<>();
+   private final List<YoFramePoint2D> recoveryStepLocations = new ArrayList<>();
+
+   private final List<YoFrameConvexPolygon2D> yoCaptureRegions = new ArrayList<>();
+   private final List<YoFrameConvexPolygon2D> yoReachableRegions = new ArrayList<>();
+
+   private static final int maxRegionDepth = 3;
 
    private final int depth = 3;
 
@@ -44,83 +54,137 @@ public class MultiStepAchievableCaptureRegionCalculator
                                                      YoRegistry parentRegistry,
                                                      YoGraphicsListRegistry graphicsListRegistry)
    {
-      reachableFootholdsCalculator = new ReachableFootholdsCalculator(kinematicsStepRange, kinematicsStepRange, 0.5 * footWidth, kinematicsStepRange,
-                                                                      soleZUpFrames, suffix, registry, graphicsListRegistry);
-      captureRegionCalculator = new AchievableCaptureRegionCalculatorWithDelay(footWidth, kinematicsStepRange, soleZUpFrames,
-                                                                               suffix, registry, null);
+      this.soleZUpFrames = soleZUpFrames;
+      reachableFootholdsCalculator = new ReachableFootholdsCalculator(kinematicsStepRange,
+                                                                      kinematicsStepRange,
+                                                                      0.5 * footWidth,
+                                                                      kinematicsStepRange,
+                                                                      soleZUpFrames,
+                                                                      registry);
+      captureRegionCalculator = new AchievableCaptureRegionCalculatorWithDelay(footWidth, kinematicsStepRange, soleZUpFrames, suffix, registry, null);
 
-      parentRegistry.addChild(registry);
+      for (int i = 0; i < maxRegionDepth; i++)
+      {
+         capturePointsAtTouchdown.add(new YoFramePoint2D("capturePointAtTouchdown" + i, worldFrame, registry));
+         recoveryStepLocations.add(new YoFramePoint2D("recoveryStepLocation" + i, worldFrame, registry));
+      }
+
       if (graphicsListRegistry != null && VISUALIZE)
       {
-         for (int i = 0; i < depth; i++)
+         String listName = getClass().getSimpleName();
+         for (int i = 0; i < maxRegionDepth; i++)
          {
-            String name = "captureRegion" + i;
+            String captureName = "captureRegion" + i;
 
-            YoFrameConvexPolygon2D yoCaptureRegionPolygon = new YoFrameConvexPolygon2D(name, suffix, worldFrame, 30, registry);
+            YoFrameConvexPolygon2D yoCaptureRegionPolygon = new YoFrameConvexPolygon2D(captureName, suffix, worldFrame, 30, registry);
             yoCaptureRegions.add(yoCaptureRegionPolygon);
 
-            YoArtifactPolygon polygonArtifact = new YoArtifactPolygon(name + suffix, yoCaptureRegionPolygon, Color.YELLOW, false);
-            graphicsListRegistry.registerArtifact(getClass().getSimpleName(), polygonArtifact);
+            YoArtifactPolygon capturePolygonArtifact = new YoArtifactPolygon(captureName + suffix, yoCaptureRegionPolygon, Color.YELLOW, false);
+
+            String reachableName = "reachableRegion" + i;
+
+            YoFrameConvexPolygon2D yoReachableRegionPolygon = new YoFrameConvexPolygon2D(reachableName, suffix, worldFrame, 30, registry);
+            yoReachableRegions.add(yoReachableRegionPolygon);
+
+            YoArtifactPolygon reachablePolygonArtifact = new YoArtifactPolygon(reachableName + suffix, yoReachableRegionPolygon, Color.BLUE, false);
+
+            YoGraphicPosition touchdownICPViz = new YoGraphicPosition("capturePointTouchdown" + i + suffix, capturePointsAtTouchdown.get(i), 0.01, YoAppearance.Yellow(),
+                                                                   YoGraphicPosition.GraphicType.BALL_WITH_ROTATED_CROSS);
+            YoGraphicPosition footstepViz = new YoGraphicPosition("recoveryStepLocation" + i + suffix, recoveryStepLocations.get(i), 0.01, YoAppearance.Blue(),
+                                                                      YoGraphicPosition.GraphicType.BALL_WITH_ROTATED_CROSS);
+
+            graphicsListRegistry.registerArtifact(listName, capturePolygonArtifact);
+            graphicsListRegistry.registerArtifact(listName, reachablePolygonArtifact);
+            graphicsListRegistry.registerArtifact(listName, touchdownICPViz.createArtifact());
+            graphicsListRegistry.registerArtifact(listName, footstepViz.createArtifact());
          }
       }
+
+      parentRegistry.addChild(registry);
+
    }
 
-   public void calculateCaptureRegions(RobotSide swingSide,
-                                       double swingTimeRemaining,
-                                       double nextTransferDuration,
-                                       FramePoint2DReadOnly currentICP,
-                                       double omega0,
-                                       FrameConvexPolygon2DReadOnly footPolygon)
-   {
-      reachableFootholdsCalculator.calculateReachableRegions(swingSide);
+   private final FramePose3D stancePose = new FramePose3D();
+   private final FramePoint2D stancePosition = new FramePoint2D();
 
-      currentICPVertices.clear();
-      currentICPVertices.add().setIncludingFrame(currentICP);
+   private final FramePoint2D icpAtStart = new FramePoint2D();
+   private final FrameConvexPolygon2DBasics stancePolygon = new FrameConvexPolygon2D();
+   private final FrameConvexPolygon2DBasics intersectingRegion = new FrameConvexPolygon2D();
+
+   public int calculateRecoveryStepLocations(RobotSide swingSide,
+                                              double swingTimeRemaining,
+                                              double nextTransferDuration,
+                                              FramePoint2DReadOnly currentICP,
+                                              double omega0,
+                                              FrameConvexPolygon2DReadOnly footPolygon)
+   {
+      icpAtStart.set(currentICP);
+      stancePose.setToZero(soleZUpFrames.get(swingSide.getOppositeSide()));
+      stancePose.changeFrame(worldFrame);
 
       int depthIdx = 0;
-      FrameConvexPolygon2DReadOnly polygon = footPolygon;
+      stancePolygon.setIncludingFrame(footPolygon);
+
+      int numberOfRecoverySteps = 0;
+
       for (; depthIdx < depth; depthIdx++)
       {
-         FrameConvexPolygon2DReadOnly captureRegion = computeCaptureRegionFromRegion(swingSide, swingTimeRemaining, nextTransferDuration, currentICPVertices, omega0, polygon);
+         reachableFootholdsCalculator.calculateReachableRegion(swingSide, stancePose.getPosition(), stancePose.getOrientation(), reachableRegion);
+
+         captureRegionCalculator.calculateCaptureRegion(swingSide, swingTimeRemaining, nextTransferDuration, icpAtStart, omega0, stancePolygon);
+         captureRegion.setIncludingFrame(captureRegionCalculator.getUnconstrainedCaptureRegion());
+         captureRegion.changeFrameAndProjectToXYPlane(worldFrame);
+
          if (VISUALIZE)
+         {
             yoCaptureRegions.get(depthIdx).set(captureRegion);
+            yoReachableRegions.get(depthIdx).set(reachableRegion);
+         }
+
+         stancePosition.set(stancePose.getPosition());
+         numberOfRecoverySteps++;
+
+         if (polygonTools.computeIntersectionOfPolygons(captureRegion, reachableRegion, intersectingRegion))
+         { // they do intersect
+            FramePoint2DReadOnly centerOfIntersection = intersectingRegion.getCentroid();
+
+            EuclidGeometryPolygonTools.intersectionBetweenLineSegment2DAndConvexPolygon2D(stancePosition,
+                                                                                          centerOfIntersection,
+                                                                                          intersectingRegion.getPolygonVerticesView(),
+                                                                                          intersectingRegion.getNumberOfVertices(),
+                                                                                          true,
+                                                                                          capturePointsAtTouchdown.get(depthIdx),
+                                                                                          null);
+
+            recoveryStepLocations.get(depthIdx).set(capturePointsAtTouchdown.get(depthIdx));
+            depthIdx++;
+            break;
+         }
+         else
+         {
+            polygonTools.computeMinimumDistancePoints(reachableRegion, captureRegion, recoveryStepLocations.get(depthIdx), capturePointsAtTouchdown.get(depthIdx));
+         }
 
          swingSide = swingSide.getOppositeSide();
-         polygon = reachableFootholdsCalculator.getReachableRegion(depthIdx);
+         stancePolygon.clear();
+         stancePolygon.addVertex(recoveryStepLocations.get(depthIdx));
+         stancePolygon.update();
 
-         currentICPVertices.clear();
-         for (int vertexIdx = 0; vertexIdx < captureRegion.getNumberOfVertices(); vertexIdx++)
-            currentICPVertices.add().set(captureRegion.getVertex(vertexIdx));
+         stancePose.getPosition().set(recoveryStepLocations.get(depthIdx));
+         icpAtStart.set(capturePointsAtTouchdown.get(depthIdx));
       }
 
       if (VISUALIZE)
       {
          for (; depthIdx < depth; depthIdx++)
-            yoCaptureRegions.get(depthIdx).clearAndUpdate();
-      }
-   }
-
-   private FrameConvexPolygon2DReadOnly computeCaptureRegionFromRegion(RobotSide swingSide,
-                                               double swingTimeRemaining,
-                                               double nextTransferDuration,
-                                               List<? extends FramePoint2DReadOnly> currentICPVertices,
-                                               double omega0,
-                                               FrameConvexPolygon2DReadOnly cmpVertices)
-   {
-      FrameConvexPolygon2DBasics captureRegion = captureRegions.add();
-      captureRegion.clear();
-      for (int i = 0; i < currentICPVertices.size(); i++)
-      {
-         captureRegionCalculator.calculateCaptureRegion(swingSide, swingTimeRemaining, nextTransferDuration, currentICPVertices.get(i), omega0, cmpVertices);
-         FrameConvexPolygon2DReadOnly unconstrainedRegion = captureRegionCalculator.getUnconstrainedCaptureRegion();
-         for (int regionVertexIdx = 0; regionVertexIdx < unconstrainedRegion.getNumberOfVertices(); regionVertexIdx++)
          {
-            captureRegion.addVertexMatchingFrame(unconstrainedRegion.getVertex(regionVertexIdx), false);
+            capturePointsAtTouchdown.get(depthIdx).setToNaN();
+            recoveryStepLocations.get(depthIdx).setToNaN();
+            yoReachableRegions.get(depthIdx).clearAndUpdate();
+            yoCaptureRegions.get(depthIdx).clearAndUpdate();
          }
       }
-      captureRegion.update();
 
-      return captureRegion;
+      return numberOfRecoverySteps;
    }
-
 }
