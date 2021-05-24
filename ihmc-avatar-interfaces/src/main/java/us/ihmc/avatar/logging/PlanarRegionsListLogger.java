c9a953bba592170a76fcff19005a973384050fda
Perry <pmacmurray@ihmc.org>
2021-05-24 17:03:24 -0500

    Fix code formatting

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logging/PlanarRegionsListLogger.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logging/PlanarRegionsListLogger.java
@@ -21,42 +21,55 @@ public class PlanarRegionsListLogger
    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
    private static final String logDirectory = System.getProperty("user.home") + File.separator + ".ihmc" + File.separator + "logs" + File.separator;
 
-   public PlanarRegionsListLogger(String logName, int maxTicksToRecord) {
+   public PlanarRegionsListLogger(String logName, int maxTicksToRecord)
+   {
       this.logName = logName;
       this.maxTicksToRecord = maxTicksToRecord;
    }
 
-   public void start() {
+   public void start()
+   {
       File file = new File(logDirectory + dateFormat.format(new Date()) + "_" + this.getClass().getSimpleName() + ".prllog");
-      try {
+      try
+      {
          out = new BufferedWriter(new FileWriter(file));
-      } catch (IOException ex) {
+      }
+      catch (IOException ex)
+      {
          LogTools.error("Could not start PlanarRegionsListLogger!");
          LogTools.error(ex.getMessage());
          for (StackTraceElement e : ex.getStackTrace())
             LogTools.error(e.toString());
       }
 
-      try {
+      try
+      {
          out.write("");
-      } catch (IOException ex) {
+      }
+      catch (IOException ex)
+      {
          LogTools.warn("Could not write to BufferedWriter - " + ex.getMessage());
       }
 
       started = true;
    }
 
-   public void update(long time) {
+   public void update(long time)
+   {
       if (!started)
          return;
    }
 
    @Override
-   public void finalize() {
-      try {
+   public void finalize()
+   {
+      try
+      {
          if (out != null)
             out.close();
       }
-      catch (IOException ignored) {}
+      catch (IOException ignored)
+      {
+      }
    }
 }
