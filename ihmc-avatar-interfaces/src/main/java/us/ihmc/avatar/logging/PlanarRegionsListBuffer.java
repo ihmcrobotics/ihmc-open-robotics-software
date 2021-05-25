89b578c9e6ae56f0228c013acabe8ce0576f1210
Perry <pmacmurray@ihmc.org>
2021-05-25 14:34:35 -0500

    Added log importing functionality to PlanarRegionsListBuffer

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logging/PlanarRegionsListBuffer.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logging/PlanarRegionsListBuffer.java
@@ -1,14 +1,45 @@
 package us.ihmc.avatar.logging;
 
+import org.apache.commons.io.IOUtils;
+import us.ihmc.robotics.PlanarRegionFileTools;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class PlanarRegionsListBuffer
 {
    private final long buffer_length;
    private final HashMap<Long, PlanarRegionsList> buffer;
    private long index = 0;
+
+   public PlanarRegionsListBuffer(File planarRegionListLog) throws IOException
+   {
+      buffer = new HashMap<>();
+      Scanner in = new Scanner(planarRegionListLog);
+      in.useDelimiter("##\n");
+
+      in.next(); //Skip metadata
+      while (in.hasNext()) {
+         in.nextLine(); //Skip past delimiter
+         long id = Long.parseLong(in.nextLine());
+
+         final File temp = File.createTempFile("prll", ".tmp");
+         temp.deleteOnExit();
+         try (FileOutputStream out = new FileOutputStream(temp)) {
+            IOUtils.copy(new StringReader(in.next()), out);
+         }
+         PlanarRegionsList list = PlanarRegionFileTools.importPlanarRegionData(temp);
+
+         buffer.put(id, list);
+      }
+
+      buffer_length = buffer.size();
+   }
 
    public PlanarRegionsListBuffer()
    {
