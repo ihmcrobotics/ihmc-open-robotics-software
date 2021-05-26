022f9a5e434c21a8ceeabd80b04909beb7762783
Melody Chiu <mchiu@ihmc.org>
2021-05-26 18:52:04 -0500

    Update File writing and loading functions

--- valkyrie/src/main/java/us/ihmc/valkyrie/stepReachability/StepReachabilityFileTools.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/stepReachability/StepReachabilityFileTools.java
@@ -17,7 +17,9 @@ public class StepReachabilityFileTools
    // FootstepPlannerLogger line 245
    // AtlasMultiDataExporter.writeSpreadsheetFormattedData
 
+   // Check path
    static String filePath = "ihmc-open-robotics-software/valkyrie/src/main/java/us/ihmc/valkyrie/stepReachability/";
+   private static final String logDirectory = System.getProperty("user.home") + File.separator + ".ihmc" + File.separator + "logs" + File.separator;
 
    public static void writeToFile(Map<FramePose3D, Boolean> feasibilityMap)
    {
@@ -33,8 +35,11 @@ public class StepReachabilityFileTools
 
          for (FramePose3D frame : feasibilityMap.keySet())
          {
-            fileWriter.write("Frame: " + frame.toString().substring(9));
-            fileWriter.write(", Feasiblility: " + feasibilityMap.get(frame));
+            String position = frame.toString().substring(21,44);
+            String orientation = frame.toString().substring(62,92);
+            fileWriter.write(position + ", ");
+            fileWriter.write(orientation + ", ");
+            fileWriter.write(String.valueOf(feasibilityMap.get(frame)));
             fileWriter.write("\n");
          }
 
@@ -53,114 +58,58 @@ public class StepReachabilityFileTools
    {
       for (FramePose3D frame : feasibilityMap.keySet())
       {
-         System.out.print("Frame: " + frame.toString().substring(9));
+         System.out.print("Frame: " + frame.toString());
          System.out.println(", Feasiblility: " + feasibilityMap.get(frame));
       }
    }
 
-//   public static Map<FramePose3D, Boolean> loadFromFile(String fileName)
-//   {
-//      Map<FramePose3D, Boolean> feasibilityMap = new HashMap<>();
-//
-//
-////         String reachabilityDataFileName = filePath + "StepReachabilityMap.txt";
-////         File file = new File(reachabilityDataFileName);
-//
-//      File file = new File("StepReachabilityMap.txt");
-//      try
-//      {
-//         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-//         String line;
-//         FramePose3D frame = new FramePose3D();
-//         while((line = bufferedReader.readLine())!=null)
-//         {
-//            // Parse to get frame and feasibility boolean, load into Hashmap
-//            String[] data = line.split(",");
-//
-//            double posX = Double.parseDouble(data[0]);
-//            double posY = Double.parseDouble(data[1]);
-//            double posZ = Double.parseDouble(data[2]);
-//            frame.getPosition().set(posX, posY, posZ);
-//
-//            double orX = Double.parseDouble(data[3]);
-//            double orY = Double.parseDouble(data[4]);
-//            double orZ = Double.parseDouble(data[5]);
-//            double orS = Double.parseDouble(data[6]);
-//            frame.getOrientation().set(orX, orY, orZ, orS);
-//
-//            if (line.contains("True"))
-//               feasibilityMap.put(frame, true);
-//            else if (line.contains("False"))
-//               feasibilityMap.put(frame, false);
-//            else
-//               throw new RuntimeException("Error in reachability file");
-//         }
-//      }
-//      catch (Exception e)
-//      {
-//         LogTools.error("Error loading reachability file");
-//         e.printStackTrace();
-//      }
-//      return null;
-//   }
-
-   public static void load(String filename)
+   public static Map<FramePose3D, Boolean> loadFromFile(String filename)
    {
       String reachabilityDataFileName = filePath + filename;
       File file = new File(reachabilityDataFileName);
-
-      load(file);
-   }
-
-   public static void load(File file)
-   {
       try
       {
          BufferedReader reader = new BufferedReader(new FileReader(file));
-         load(reader);
-      } catch (FileNotFoundException e)
-      {
-         e.printStackTrace();
-      }
-
-   }
-
-   private static Map<FramePose3D, Boolean> load(BufferedReader reader)
-   {
-      try
-      {
-         Map<FramePose3D, Boolean> feasibilityMap = new HashMap<>();
-         FramePose3D frame = new FramePose3D();
-         String line;
-         while((line = reader.readLine())!=null)
+         try
          {
-            // Parse to get frame and feasibility boolean, load into Hashmap
-            String[] data = line.split(",");
+            Map<FramePose3D, Boolean> feasibilityMap = new HashMap<>();
+            FramePose3D frame = new FramePose3D();
+            String line;
+            while((line = reader.readLine())!=null)
+            {
+               // Parse to get frame position, orientation and feasibility boolean
+               String[] data = line.split(",");
+               double posX = Double.parseDouble(data[0]);
+               double posY = Double.parseDouble(data[1]);
+               double posZ = Double.parseDouble(data[2]);
+               frame.getPosition().set(posX, posY, posZ);
 
-            // TODO: Fix parsing double
-            double posX = Double.parseDouble(data[0]);
-            double posY = Double.parseDouble(data[1]);
-            double posZ = Double.parseDouble(data[2]);
-            frame.getPosition().set(posX, posY, posZ);
+               double orX = Double.parseDouble(data[3]);
+               double orY = Double.parseDouble(data[4]);
+               double orZ = Double.parseDouble(data[5]);
+               double orS = Double.parseDouble(data[6]);
+               frame.getOrientation().set(orX, orY, orZ, orS);
 
-            double orX = Double.parseDouble(data[3]);
-            double orY = Double.parseDouble(data[4]);
-            double orZ = Double.parseDouble(data[5]);
-            double orS = Double.parseDouble(data[6]);
-            frame.getOrientation().set(orX, orY, orZ, orS);
+               // Load into hashmap
+               if (line.contains("true"))
+                  feasibilityMap.put(frame, true);
+               else if (line.contains("false"))
+                  feasibilityMap.put(frame, false);
+               else
+                  throw new RuntimeException("Error in reachability file");
+            }
+            System.out.println("Done loading from file");
+            return feasibilityMap;
 
-            if (line.contains("True"))
-               feasibilityMap.put(frame, true);
-            else if (line.contains("False"))
-               feasibilityMap.put(frame, false);
-            else
-               throw new RuntimeException("Error in reachability file");
          }
-         return feasibilityMap;
-
-      } catch (IOException e)
+         catch (IOException e)
+         {
+            LogTools.error("Error loading reachability file");
+            e.printStackTrace();
+         }
+      }
+      catch (FileNotFoundException e)
       {
-         LogTools.error("Error loading reachability file");
          e.printStackTrace();
       }
       return null;
