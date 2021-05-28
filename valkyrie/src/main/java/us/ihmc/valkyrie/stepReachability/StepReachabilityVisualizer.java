36f8e2db86fc19d3146ba68f3f1b6d572227d612
Stephen McCrory <smccrory@ihmc.us>
2021-05-28 11:26:03 -0500

    Stubbing out code for visualizer

---
+++ valkyrie/src/main/java/us/ihmc/valkyrie/stepReachability/StepReachabilityVisualizer.java
@@ -0,0 +1,20 @@
+package us.ihmc.valkyrie.stepReachability;
+
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+
+import java.util.Map;
+
+public class StepReachabilityVisualizer
+{
+   public StepReachabilityVisualizer(Map<FramePose3D, Boolean> reachabilityMap)
+   {
+
+//      voxelViz.identity();
+//      voxelViz.translate(1.0, 0.0, 0.0);
+//      voxelViz.addSphere()
+//      scs.addStaticLinkGraphics(voxelViz);
+
+      // ReachabilitySphereMapCalculator
+
+   }
+}
