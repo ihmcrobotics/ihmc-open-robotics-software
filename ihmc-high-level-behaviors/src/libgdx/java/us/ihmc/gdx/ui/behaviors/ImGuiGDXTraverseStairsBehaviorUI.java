8410a7b81cd47d757e75ed1beaef4ea641e02e3f
Duncan Calvert <dcalvert@ihmc.us>
2021-05-31 18:47:18 -0500

    Render the parts of the building exploration behavior as ImNodes. Listen to textToSpeech packets.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/behaviors/ImGuiGDXTraverseStairsBehaviorUI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/behaviors/ImGuiGDXTraverseStairsBehaviorUI.java
@@ -3,15 +3,20 @@ package us.ihmc.gdx.ui.behaviors;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import us.ihmc.behaviors.stairs.TraverseStairsBehavior;
 import us.ihmc.behaviors.stairs.TraverseStairsBehaviorAPI;
 import us.ihmc.behaviors.tools.BehaviorHelper;
 import us.ihmc.behaviors.tools.footstepPlanner.MinimalFootstep;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
+import us.ihmc.gdx.ui.behaviors.registry.GDXBehaviorUIDefinition;
 import us.ihmc.gdx.ui.behaviors.registry.GDXBehaviorUIInterface;
 import us.ihmc.gdx.ui.graphics.GDXFootstepPlanGraphic;
 
 public class ImGuiGDXTraverseStairsBehaviorUI extends GDXBehaviorUIInterface
 {
+   public static final GDXBehaviorUIDefinition DEFINITION = new GDXBehaviorUIDefinition(TraverseStairsBehavior.DEFINITION,
+                                                                                        ImGuiGDXTraverseStairsBehaviorUI::new);
+
    private final GDXFootstepPlanGraphic footstepPlanGraphic = new GDXFootstepPlanGraphic();
 
    public ImGuiGDXTraverseStairsBehaviorUI(BehaviorHelper helper)
@@ -27,6 +32,15 @@ public class ImGuiGDXTraverseStairsBehaviorUI extends GDXBehaviorUIInterface
    public void create(GDXImGuiBasedUI baseUI)
    {
 
+   }
+
+   @Override
+   public void renderNode(String nodeName)
+   {
+      if (nodeName.equals(DEFINITION.getName()))
+      {
+         render();
+      }
    }
 
    @Override
