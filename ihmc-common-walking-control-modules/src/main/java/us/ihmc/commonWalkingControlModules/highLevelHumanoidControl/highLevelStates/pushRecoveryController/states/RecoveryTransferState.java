0ce3d1702c9e4bfa67f195c1d77d2ee833f79bd0
Robert <rgriffin@ihmc.us>
2021-06-10 16:33:32 -0500

    removed things related to unloading from the transfer state

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/pushRecoveryController/states/RecoveryTransferState.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/pushRecoveryController/states/RecoveryTransferState.java
@@ -69,10 +69,6 @@ public class RecoveryTransferState extends PushRecoveryState
 
    private final Footstep nextFootstep = new Footstep();
 
-   private final YoBoolean isUnloading;
-   private final DoubleProvider unloadFraction;
-   private final DoubleProvider rhoMin;
-
    public RecoveryTransferState(PushRecoveryStateEnum stateEnum,
                                 WalkingMessageHandler walkingMessageHandler,
                                 HighLevelHumanoidControllerToolbox controllerToolbox,
@@ -81,8 +77,6 @@ public class RecoveryTransferState extends PushRecoveryState
                                 WalkingFailureDetectionControlModule failureDetectionControlModule,
                                 DoubleProvider minimumTransferTime,
                                 DoubleProvider minimumSwingTime,
-                                DoubleProvider unloadFraction,
-                                DoubleProvider rhoMin,
                                 YoRegistry parentRegistry)
    {
       super(stateEnum, parentRegistry);
@@ -91,22 +85,11 @@ public class RecoveryTransferState extends PushRecoveryState
       this.walkingMessageHandler = walkingMessageHandler;
       this.failureDetectionControlModule = failureDetectionControlModule;
       this.controllerToolbox = controllerToolbox;
-      this.unloadFraction = unloadFraction;
-      this.rhoMin = rhoMin;
       this.balanceManager = managerFactory.getOrCreateBalanceManager();
 
       comHeightManager = managerFactory.getOrCreateCenterOfMassHeightManager();
       pelvisOrientationManager = managerFactory.getOrCreatePelvisOrientationManager();
       feetManager = managerFactory.getOrCreateFeetManager();
-
-      if (unloadFraction != null)
-      {
-         isUnloading = new YoBoolean(transferToSide.getOppositeSide().getLowerCaseName() + "FootIsUnloading", registry);
-      }
-      else
-      {
-         isUnloading = null;
-      }
 
       this.minimumTransferTime = minimumTransferTime;
       this.minimumSwingTime = minimumSwingTime;
@@ -163,38 +146,14 @@ public class RecoveryTransferState extends PushRecoveryState
       feetManager.updateSwingTrajectoryPreview(swingSide);
       balanceManager.computeICPPlan();
 
-      if (!doManualLiftOff())
-      {
          if (switchToToeOffIfPossible())
             feetManager.initializeSwingTrajectoryPreview(swingSide, footsteps, footstepTimings.getSwingTime());
-      }
 
       feetManager.updateContactStatesInDoubleSupport(transferToSide);
 
       // Always do this so that when a foot slips or is loaded in the air, the height gets adjusted.
       //      comHeightManager.setSupportLeg(transferToSide.getOppositeSide());
 
-
-      double transferDuration = currentTransferDuration.getDoubleValue();
-
-      double toeOffDuration = footstepTimings.getLiftoffDuration();
-      if (doManualLiftOff() && transferDuration - timeInState < toeOffDuration)
-      {
-         Footstep upcomingFootstep = footsteps;
-         FrameSE3TrajectoryPoint firstWaypoint = upcomingFootstep.getSwingTrajectory().get(0);
-         MovingReferenceFrame soleZUpFrame = controllerToolbox.getReferenceFrames().getSoleZUpFrame(transferToSide.getOppositeSide());
-         tempOrientation.setIncludingFrame(firstWaypoint.getOrientation());
-         tempOrientation.changeFrame(soleZUpFrame);
-         tempAngularVelocity.setIncludingFrame(firstWaypoint.getAngularVelocity());
-         tempAngularVelocity.changeFrame(soleZUpFrame); // The y component is equivalent to the pitch rate since the yaw and roll rate are 0.0
-         feetManager.liftOff(transferToSide.getOppositeSide(), tempOrientation.getPitch(), tempAngularVelocity.getY(), toeOffDuration);
-      }
-   }
-
-   private boolean doManualLiftOff()
-   {
-      Footstep upcomingFootstep = footsteps;
-      return upcomingFootstep.getTrajectoryType() == TrajectoryType.WAYPOINTS && Precision.equals(upcomingFootstep.getSwingTrajectory().get(0).getTime(), 0.0);
    }
 
    @Override
@@ -248,47 +207,9 @@ public class RecoveryTransferState extends PushRecoveryState
    @Override
    public void onExit()
    {
-      if (isUnloading != null)
-      {
-         isUnloading.set(false);
-         feetManager.resetLoadConstraints(transferToSide.getOppositeSide());
-      }
       feetManager.reset();
 
       balanceManager.minimizeAngularMomentumRateZ(false);
-   }
-
-   /**
-    * This method checks if the upcoming step has a desired absolute start time. If that is the case
-    * the transfer time is adjusted such that the swing starts at the correct time.
-    */
-   private void adjustTiming(FootstepTiming stepTiming)
-   {
-      if (!stepTiming.hasAbsoluteTime())
-      {
-         originalTransferTime.setToNaN();
-         return;
-      }
-
-      double originalSwingTime = stepTiming.getSwingTime();
-      double originalTransferTime = stepTiming.getTransferTime();
-      this.originalTransferTime.set(originalTransferTime);
-
-      double currentTime = controllerToolbox.getYoTime().getDoubleValue();
-      double timeInFootstepPlan = currentTime - stepTiming.getExecutionStartTime();
-      double adjustedTransferTime = stepTiming.getSwingStartTime() - timeInFootstepPlan;
-
-      // make sure transfer does not get too short
-      adjustedTransferTime = Math.max(adjustedTransferTime, minimumTransferTime.getValue());
-
-      // GW TODO - possible improvement:
-      // If the adjustment is capped by the minimum transfer time adjust also the upcoming transfer times here. That
-      // would make the ICP plan for the upcoming steps more accurate. However, if the given original transfer times
-      // are correctly set this might be a minimal improvement that makes step timing more complicated and difficult
-      // to debug. If we have big adjustments a lot we should revisit this.
-
-      // keep swing times and only adjust transfers for now
-      stepTiming.setTimings(originalSwingTime, adjustedTransferTime);
    }
 
    /**
