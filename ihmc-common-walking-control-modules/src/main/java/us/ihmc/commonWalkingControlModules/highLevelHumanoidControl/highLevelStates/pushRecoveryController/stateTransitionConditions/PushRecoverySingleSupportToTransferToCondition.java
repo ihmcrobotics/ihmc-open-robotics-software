b51f35475d524eda37efd942e2705e3f4bf9ed3b
Robert <rgriffin@ihmc.us>
2021-06-09 22:00:57 -0500

    consolidated the push recovery single support state to one class

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/pushRecoveryController/stateTransitionConditions/PushRecoverySingleSupportToTransferToCondition.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/pushRecoveryController/stateTransitionConditions/PushRecoverySingleSupportToTransferToCondition.java
@@ -1,17 +1,17 @@
 package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.stateTransitionConditions;
 
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.states.SingleSupportPushRecoveryState;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.states.RecoveringSingleSupportState;
 import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.states.TransferToRecoveringSingleSupportState;
 import us.ihmc.commonWalkingControlModules.messageHandlers.WalkingMessageHandler;
 import us.ihmc.robotics.stateMachine.core.StateTransitionCondition;
 
 public class PushRecoverySingleSupportToTransferToCondition implements StateTransitionCondition
 {
-   private final SingleSupportPushRecoveryState singleSupportState;
+   private final RecoveringSingleSupportState singleSupportState;
    private final TransferToRecoveringSingleSupportState transferState;
    private final WalkingMessageHandler walkingMessageHandler;
 
-   public PushRecoverySingleSupportToTransferToCondition(SingleSupportPushRecoveryState singleSupportState, TransferToRecoveringSingleSupportState transferState, WalkingMessageHandler walkingMessageHandler)
+   public PushRecoverySingleSupportToTransferToCondition(RecoveringSingleSupportState singleSupportState, TransferToRecoveringSingleSupportState transferState, WalkingMessageHandler walkingMessageHandler)
    {
       this.singleSupportState = singleSupportState;
       this.transferState = transferState;
--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/pushRecoveryController/stateTransitionConditions/StopPushRecoveryFromSingleSupportCondition.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/pushRecoveryController/stateTransitionConditions/StopPushRecoveryFromSingleSupportCondition.java
@@ -1,15 +1,15 @@
 package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.stateTransitionConditions;
 
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.states.SingleSupportPushRecoveryState;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.states.RecoveringSingleSupportState;
 import us.ihmc.commonWalkingControlModules.messageHandlers.WalkingMessageHandler;
 import us.ihmc.robotics.stateMachine.core.StateTransitionCondition;
 
 public class StopPushRecoveryFromSingleSupportCondition implements StateTransitionCondition
 {
-   private final SingleSupportPushRecoveryState singleSupportState;
+   private final RecoveringSingleSupportState singleSupportState;
    private final WalkingMessageHandler walkingMessageHandler;
 
-   public StopPushRecoveryFromSingleSupportCondition(SingleSupportPushRecoveryState singleSupportState, WalkingMessageHandler walkingMessageHandler)
+   public StopPushRecoveryFromSingleSupportCondition(RecoveringSingleSupportState singleSupportState, WalkingMessageHandler walkingMessageHandler)
    {
       this.singleSupportState = singleSupportState;
       this.walkingMessageHandler = walkingMessageHandler;
