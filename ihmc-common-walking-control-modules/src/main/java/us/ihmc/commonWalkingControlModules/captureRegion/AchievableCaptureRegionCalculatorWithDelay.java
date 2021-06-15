2487a5dea0f6eadb2c89ab8acdf05c7ab7f4de48
rgriffin@ihmc.org <rgriffin@ihmc.us>
2021-06-15 12:54:00 -0500

    did a little cleanup

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/AchievableCaptureRegionCalculatorWithDelay.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/AchievableCaptureRegionCalculatorWithDelay.java
@@ -2,27 +2,16 @@ package us.ihmc.commonWalkingControlModules.captureRegion;
 
 import gnu.trove.list.array.TDoubleArrayList;
 import us.ihmc.commonWalkingControlModules.capturePoint.CapturePointTools;
-import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
-import us.ihmc.commons.MathTools;
 import us.ihmc.commons.lists.RecyclingArrayList;
 import us.ihmc.euclid.referenceFrame.FrameConvexPolygon2D;
 import us.ihmc.euclid.referenceFrame.FramePoint2D;
-import us.ihmc.euclid.referenceFrame.FrameVector2D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameConvexPolygon2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
-import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
-import us.ihmc.robotics.EuclidCoreMissingTools;
-import us.ihmc.robotics.geometry.ConvexPolygonTools;
 import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
 import us.ihmc.robotics.referenceFrames.ZUpFrame;
-import us.ihmc.robotics.robotSide.RobotSide;
-import us.ihmc.robotics.robotSide.SideDependentList;
-import us.ihmc.robotics.time.ExecutionTimer;
-import us.ihmc.sensorProcessing.frames.CommonHumanoidReferenceFrames;
-import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class AchievableCaptureRegionCalculatorWithDelay
 {
@@ -39,8 +28,7 @@ public class AchievableCaptureRegionCalculatorWithDelay
 
    private final FramePoint2D extremeCoP = new FramePoint2D();
    private final FramePoint2D capturePoint = new FramePoint2D(worldFrame);
-   private final FrameConvexPolygon2D unconstrainedCaptureRegion = new FrameConvexPolygon2D(worldFrame);
-   private final FrameConvexPolygon2D unconstrainedCaptureRegionAtTouchdown = new FrameConvexPolygon2D(worldFrame);
+   private final FrameConvexPolygon2D captureRegion = new FrameConvexPolygon2D(worldFrame);
 
    private final PoseReferenceFrame supportFrame = new PoseReferenceFrame("supportFrame", worldFrame);
    private final ZUpFrame supportSoleZUp = new ZUpFrame(worldFrame, supportFrame, "supportZUpFrame");
@@ -81,15 +69,13 @@ public class AchievableCaptureRegionCalculatorWithDelay
 
       predictedICPAtTouchdown.setToZero(supportSoleZUp);
 
-      unconstrainedCaptureRegion.clear(supportSoleZUp);
-      unconstrainedCaptureRegionAtTouchdown.clear(supportSoleZUp);
+      captureRegion.clear(supportSoleZUp);
 
       // 2. Get extreme CoP positions
       if (supportFootPolygon.isPointInside(capturePoint))
       {
          // If the ICP is in the support polygon return the whole reachable region.
-         unconstrainedCaptureRegion.setToNaN();
-         unconstrainedCaptureRegionAtTouchdown.setToNaN();
+         captureRegion.setToNaN();
          return true;
       }
 
@@ -105,25 +91,66 @@ public class AchievableCaptureRegionCalculatorWithDelay
             CapturePointTools.computeDesiredCapturePointPosition(omega0, candidateSwingTimes.get(timeIdx), capturePoint, extremeCoP, predictedICPAtTouchdown);
             computeCoPLocationToCapture(predictedICPAtTouchdown, extremeCoP, omega0, nextTransferDuration, predictedICPAfterTransfer);
 
-            unconstrainedCaptureRegionAtTouchdown.addVertexMatchingFrame(predictedICPAtTouchdown, false);
-            unconstrainedCaptureRegion.addVertexMatchingFrame(predictedICPAfterTransfer, false);
+            captureRegion.addVertexMatchingFrame(predictedICPAfterTransfer, false);
          }
       }
 
-      unconstrainedCaptureRegionAtTouchdown.update();
-      unconstrainedCaptureRegion.update();
+      captureRegion.update();
 
       return false;
    }
 
-   public FrameConvexPolygon2DReadOnly getUnconstrainedCaptureRegionAtTouchdown()
+   public boolean calculateCaptureRegionAtFixedTime(double nextTransferDuration,
+                                         double remainingSwingDuration,
+                                         FramePoint2DReadOnly currentICP,
+                                         double omega0,
+                                         FramePose3DReadOnly supportPose,
+                                         FrameConvexPolygon2DReadOnly footPolygon)
    {
-      return unconstrainedCaptureRegionAtTouchdown;
+      // 1. Set up all needed variables and reference frames for the calculation:
+      supportFrame.setPoseAndUpdate(supportPose);
+      supportSoleZUp.update();
+
+      this.supportFootPolygon.setIncludingFrame(footPolygon);
+      this.supportFootPolygon.changeFrameAndProjectToXYPlane(supportSoleZUp);
+
+      capturePoint.setIncludingFrame(currentICP);
+      capturePoint.changeFrame(supportSoleZUp);
+
+      footCentroid.setIncludingFrame(supportFootPolygon.getCentroid());
+
+      predictedICPAtTouchdown.setToZero(supportSoleZUp);
+
+      captureRegion.clear(supportSoleZUp);
+
+      // 2. Get extreme CoP positions
+      if (supportFootPolygon.isPointInside(capturePoint))
+      {
+         // If the ICP is in the support polygon return the whole reachable region.
+         captureRegion.setToNaN();
+         return true;
+      }
+
+      // 3. For every possible extreme CoP predict the corresponding ICP given the remaining swing time
+      for (int i = 0; i < supportFootPolygon.getNumberOfVertices(); i++)
+      {
+         extremeCoP.setIncludingFrame(supportFootPolygon.getVertex(i));
+         extremeCoP.changeFrame(supportSoleZUp);
+
+         CapturePointTools.computeDesiredCapturePointPosition(omega0, remainingSwingDuration, capturePoint, extremeCoP, predictedICPAtTouchdown);
+         computeCoPLocationToCapture(predictedICPAtTouchdown, extremeCoP, omega0, nextTransferDuration, predictedICPAfterTransfer);
+
+         captureRegion.addVertexMatchingFrame(predictedICPAfterTransfer, false);
+      }
+
+      captureRegion.update();
+
+      return false;
    }
 
-   public FrameConvexPolygon2DReadOnly getUnconstrainedCaptureRegion()
+   public FrameConvexPolygon2DReadOnly getCaptureRegion()
    {
-      return unconstrainedCaptureRegion;
+      return captureRegion;
    }
 
    public static void computeCoPLocationToCapture(FramePoint2DReadOnly initialICP,
