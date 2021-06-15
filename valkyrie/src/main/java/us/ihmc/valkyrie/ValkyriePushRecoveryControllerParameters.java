c20d692c8e5712fb00cf77f8708b3bd6e55265ef
rgriffin@ihmc.org <rgriffin@ihmc.us>
2021-06-15 13:05:14 -0500

    implemented valkyrie push recovery parameters

---
+++ valkyrie/src/main/java/us/ihmc/valkyrie/ValkyriePushRecoveryControllerParameters.java
@@ -0,0 +1,34 @@
+package us.ihmc.valkyrie;
+
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.pushRecoveryController.PushRecoveryControllerParameters;
+import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.MomentumOptimizationSettings;
+import us.ihmc.valkyrie.parameters.ValkyrieJointMap;
+import us.ihmc.valkyrie.parameters.ValkyrieMomentumOptimizationSettings;
+
+public class ValkyriePushRecoveryControllerParameters implements PushRecoveryControllerParameters
+{
+   private final ValkyrieMomentumOptimizationSettings momentumOptimizationSettings;
+
+   public ValkyriePushRecoveryControllerParameters(ValkyrieJointMap jointMap)
+   {
+      momentumOptimizationSettings = new ValkyrieMomentumOptimizationSettings(jointMap);
+   }
+
+   @Override
+   public MomentumOptimizationSettings getMomentumOptimizationSettings()
+   {
+      return momentumOptimizationSettings;
+   }
+
+   @Override
+   public double getMaxStepLength()
+   {
+      return 0.7;
+   }
+
+   @Override
+   public double getMinStepWidth()
+   {
+      return 0.15;
+   }
+}
