9c9f907968074afa13c5c2953d32fb16c7176baf
Melody Chiu <mchiu@ihmc.org>
2021-06-16 17:10:34 -0500

    Polish flat-ground reachability check based on comments on pull request. Add Z-axis queries.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/reachabilityMap/footstep/StepReachabilityVisualizer.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/reachabilityMap/footstep/StepReachabilityVisualizer.java
@@ -3,6 +3,7 @@ package us.ihmc.avatar.reachabilityMap.footstep;
 import us.ihmc.commonWalkingControlModules.staticReachability.StepReachabilityData;
 import us.ihmc.commonWalkingControlModules.staticReachability.StepReachabilityLatticePoint;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.graphicsDescription.Graphics3DObject;
 import us.ihmc.graphicsDescription.appearance.AppearanceDefinition;
 import us.ihmc.graphicsDescription.appearance.YoAppearance;
@@ -33,13 +34,15 @@ public class StepReachabilityVisualizer
       {
          // Represent foot position as sphere, yaw as z-axis translation
          Graphics3DObject validStep = new Graphics3DObject();
-         validStep.translate(latticePoint.getXIndex(), latticePoint.getYIndex(), latticePoint.getYawIndex());
+         validStep.translate(latticePoint.getXIndex(), latticePoint.getYIndex(), 0.0);
+         validStep.rotate(Math.toRadians(90), new Vector3D(0,1,0));
+         validStep.rotate(Math.toRadians(latticePoint.getYawIndex() * 10), new Vector3D(1,0,0));
 
          // Reachability for this foot position indicated by green/red color
          double reachabilityValue = stepReachabilityData.getLegReachabilityMap().get(latticePoint);
          if (reachabilityValue > 40) reachabilityValue = 40;
          AppearanceDefinition appearance = YoAppearance.RGBColor(reachabilityValue/40, (40-reachabilityValue)/40, 0);
-         validStep.addSphere(0.5/yawDivisions, appearance);
+         validStep.addArrow(0.3, appearance, appearance);
 
          scs.addStaticLinkGraphics(validStep);
       }
