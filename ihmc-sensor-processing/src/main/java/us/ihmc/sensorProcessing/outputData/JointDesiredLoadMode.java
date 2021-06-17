abb5d3ebd16a69809e2f7be7910c21e7e9f8cd2a
nadiaocu <nadiaocu@NADIA-OCU>
2021-06-17 10:02:49 -0500

    Added field to indicate whether the joint is loaded or not.

---
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/outputData/JointDesiredLoadMode.java
@@ -0,0 +1,9 @@
+package us.ihmc.sensorProcessing.outputData;
+
+public enum JointDesiredLoadMode
+{
+   NOT_LOADED,
+   LOADED;
+
+   public static final JointDesiredLoadMode[] values = values();
+}
