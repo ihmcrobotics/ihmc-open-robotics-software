3563d0f14d18e557587ab6acd1b5bc89ab5ed080
Duncan Calvert <dcalvert@ihmc.us>
2021-06-18 16:50:57 -0500

    Remove old API from ImGuiPanelManager.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/imgui/ImGuiGlfwWindow.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/imgui/ImGuiGlfwWindow.java
@@ -1,5 +1,6 @@
 package us.ihmc.gdx.imgui;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
@@ -8,13 +9,17 @@ public class ImGuiGlfwWindow
 {
    private final String windowTitle;
    private final GlfwWindowForImGui glfwWindowForImGui;
-   private final GDXImGuiWindowAndDockSystem imGuiDockSystem = new GDXImGuiWindowAndDockSystem(getClass(),
-                                                                                               "ihmc-open-robotics-software",
-                                                                                               "ihmc-graphics/src/libgdx/resources",
-                                                                                               Paths.get(System.getProperty("user.home"),
-                                                                                                         ".ihmc/" + "GLFWDemo"
-                                                                                                         + "ImGuiSettings.ini").toAbsolutePath().normalize());
-   private final ImGuiPanelManager panelManager = new ImGuiPanelManager();
+   private final Class<? extends ImGuiGlfwWindow> classForLoading = getClass();
+   private final String directoryNameToAssumePresent = "ihmc-open-robotics-software";
+   private final String subsequentPathToResourceFolder = "ihmc-graphics/src/libgdx/resources";
+   private final Path imGuiUserSettingsPath = Paths.get(System.getProperty("user.home"), ".ihmc/" + "GLFWDemo" + "ImGuiSettings.ini")
+                                                   .toAbsolutePath()
+                                                   .normalize();
+   private final GDXImGuiWindowAndDockSystem imGuiDockSystem = new GDXImGuiWindowAndDockSystem(classForLoading,
+                                                                                               directoryNameToAssumePresent,
+                                                                                               subsequentPathToResourceFolder,
+                                                                                               imGuiUserSettingsPath);
+   private final ImGuiPanelManager panelManager = new ImGuiPanelManager(classForLoading, directoryNameToAssumePresent, subsequentPathToResourceFolder);
 
    public ImGuiGlfwWindow(String windowTitle, int windowWidth, int windowHeight)
    {
