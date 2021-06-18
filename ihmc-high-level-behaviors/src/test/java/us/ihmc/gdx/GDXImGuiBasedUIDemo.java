5b1ae204769889c2beec8f20733569a1ea589afa
Duncan Calvert <dcalvert@ihmc.us>
2021-06-18 16:44:32 -0500

    Remove old ImGui panel API where dock construction was in code.

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDXImGuiBasedUIDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDXImGuiBasedUIDemo.java
@@ -2,10 +2,8 @@ package us.ihmc.gdx;
 
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import imgui.internal.ImGui;
-import imgui.flag.*;
 import us.ihmc.commons.time.Stopwatch;
 import us.ihmc.gdx.tools.BoxesDemoModel;
-import us.ihmc.gdx.tools.GDXApplicationCreator;
 import us.ihmc.gdx.tools.GDXModelPrimitives;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
 import us.ihmc.tools.string.StringTools;
@@ -21,7 +19,7 @@ public class GDXImGuiBasedUIDemo
 
    public GDXImGuiBasedUIDemo()
    {
-      GDXApplicationCreator.launchGDXApplication(new Lwjgl3ApplicationAdapter()
+      baseUI.launchGDXApplication(new Lwjgl3ApplicationAdapter()
       {
          @Override
          public void create()
@@ -31,7 +29,7 @@ public class GDXImGuiBasedUIDemo
             baseUI.get3DSceneManager().addModelInstance(new ModelInstance(GDXModelPrimitives.createCoordinateFrame(0.3)));
             baseUI.get3DSceneManager().addModelInstance(new BoxesDemoModel().newInstance());
 
-            baseUI.getImGuiPanelManager().splitAdd("Window", ImGuiDir.Right, 0.20);
+            baseUI.getImGuiPanelManager().addWindow("Window", GDXImGuiBasedUIDemo.this::renderPanel);
          }
 
          @Override
@@ -39,25 +37,7 @@ public class GDXImGuiBasedUIDemo
          {
             baseUI.renderBeforeOnScreenUI();
 
-            ImGui.begin("Window");
-            if (ImGui.beginTabBar("main"))
-            {
-               if (ImGui.beginTabItem("Window"))
-               {
-                  ImGui.text("Tab bar detected!");
-                  ImGui.endTabItem();
-               }
-               ImGui.endTabBar();
-            }
-            ImGui.text(StringTools.format3D("Time: {} s", stopwatch.totalElapsed()).get());
-            ImGui.button("I'm a Button!");
-            float[] values = new float[100];
-            for (int i = 0; i < 100; i++)
-            {
-               values[i] = i;
-            }
-            ImGui.plotLines("Histogram", values, 100);
-            ImGui.end();
+            renderPanel();
 
             baseUI.renderEnd();
          }
@@ -67,7 +47,30 @@ public class GDXImGuiBasedUIDemo
          {
             baseUI.dispose();
          }
-      }, getClass());
+      });
+   }
+
+   private void renderPanel()
+   {
+      ImGui.begin("Window");
+      if (ImGui.beginTabBar("main"))
+      {
+         if (ImGui.beginTabItem("Window"))
+         {
+            ImGui.text("Tab bar detected!");
+            ImGui.endTabItem();
+         }
+         ImGui.endTabBar();
+      }
+      ImGui.text(StringTools.format3D("Time: {} s", stopwatch.totalElapsed()).get());
+      ImGui.button("I'm a Button!");
+      float[] values = new float[100];
+      for (int i = 0; i < 100; i++)
+      {
+         values[i] = i;
+      }
+      ImGui.plotLines("Histogram", values, 100);
+      ImGui.end();
    }
 
    public static void main(String[] args)
--- ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDXImGuiVRDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDXImGuiVRDemo.java
@@ -1,7 +1,6 @@
 package us.ihmc.gdx;
 
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
-import imgui.flag.ImGuiDir;
 import imgui.internal.ImGui;
 import us.ihmc.commons.time.Stopwatch;
 import us.ihmc.gdx.tools.BoxesDemoModel;
@@ -31,16 +30,19 @@ public class GDXImGuiVRDemo
             baseUI.get3DSceneManager().addModelInstance(new ModelInstance(GDXModelPrimitives.createCoordinateFrame(0.3)));
             baseUI.get3DSceneManager().addModelInstance(new BoxesDemoModel().newInstance());
 
-            baseUI.getImGuiPanelManager().splitAdd("Window", ImGuiDir.Right, 0.20);
+            baseUI.getImGuiPanelManager().addWindow("Window", this::renderPanel);
          }
 
          @Override
          public void render()
          {
             baseUI.pollVREvents();
-
             baseUI.renderBeforeOnScreenUI();
+            baseUI.renderEnd();
+         }
 
+         private void renderPanel()
+         {
             ImGui.begin("Window");
             if (ImGui.beginTabBar("main"))
             {
@@ -60,8 +62,6 @@ public class GDXImGuiVRDemo
             }
             ImGui.plotLines("Histogram", values, 100);
             ImGui.end();
-
-            baseUI.renderEnd();
          }
 
          @Override
