800d9b96e59819ba22fde642194abff3ac32cd03
Duncan Calvert <dcalvert@ihmc.us>
2021-06-20 22:59:09 -0500

    Fix up ReferenceFrameMissingTools method.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/referenceFrames/ReferenceFrameMissingTools.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/referenceFrames/ReferenceFrameMissingTools.java
@@ -12,6 +12,10 @@ public class ReferenceFrameMissingTools
     * The {@code transformToParent} should describe the pose of the new frame expressed in its parent
     * frame.
     * </p>
+    * <p>
+    *    Note: It <strong>is</strong> necessary to call update on this reference frame.
+    * </p>
+    *
     *
     * @param frameName         the name of the new frame.
     * @param parentFrame       the parent frame of the new reference frame.
@@ -26,7 +30,7 @@ public class ReferenceFrameMissingTools
       boolean isZupFrame = parentFrame.isZupFrame() && transformToParent.isRotation2D();
       boolean isAStationaryFrame = parentFrame.isAStationaryFrame();
 
-      ReferenceFrame referenceFrame = new ReferenceFrame(frameName, parentFrame, transformToParent, isAStationaryFrame, isZupFrame)
+      return new ReferenceFrame(frameName, parentFrame, transformToParent, isAStationaryFrame, isZupFrame)
       {
          @Override
          protected void updateTransformToParent(RigidBodyTransform transformToParentToUpdate)
@@ -34,7 +38,5 @@ public class ReferenceFrameMissingTools
             transformToParentToUpdate.set(transformToParent);
          }
       };
-
-      return referenceFrame;
    }
 }
