6c792b1e09967789cece73fbe1067a6d98ef0007
rgriffin@ihmc.org <rgriffin@ihmc.us>
2021-06-22 12:29:57 -0500

    fixed a not implemented exception

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/ioHandling/LinearMPCTrajectoryHandler.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/ioHandling/LinearMPCTrajectoryHandler.java
@@ -1,7 +1,6 @@
 package us.ihmc.commonWalkingControlModules.modelPredictiveController.ioHandling;
 
 import org.ejml.data.DMatrixRMaj;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.ConstraintType;
 import us.ihmc.commonWalkingControlModules.dynamicPlanning.comPlanning.*;
 import us.ihmc.commonWalkingControlModules.modelPredictiveController.ContactPlaneProvider;
@@ -361,7 +360,7 @@ public class LinearMPCTrajectoryHandler
 
    public void removeCompletedSegments(double timeToCrop)
    {
-      throw new NotImplementedException();
+      throw new RuntimeException("Not implemented.");
       /*
       while (comTrajectory.getCurrentNumberOfSegments() > 0 && comTrajectory.getSegment(0).getTimeInterval().getEndTime() <= timeToCrop)
          comTrajectory.removeSegment(0);
