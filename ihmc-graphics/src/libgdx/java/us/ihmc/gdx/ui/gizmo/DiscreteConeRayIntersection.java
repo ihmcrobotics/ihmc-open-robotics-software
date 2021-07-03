16660398a1cbc06deae8c1c4b52960087e432bcc
Duncan Calvert <dcalvert@ihmc.us>
2021-07-02 20:27:16 -0500

    Make field final.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/ui/gizmo/DiscreteConeRayIntersection.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/ui/gizmo/DiscreteConeRayIntersection.java
@@ -10,7 +10,7 @@ import us.ihmc.euclid.tuple3D.Point3D;
 
 public class DiscreteConeRayIntersection
 {
-   private BoundingSphereIntersection boundingSphereIntersection = new BoundingSphereIntersection();
+   private final BoundingSphereIntersection boundingSphereIntersection = new BoundingSphereIntersection();
    private final Plane3D coneBasePlaneFacingTip = new Plane3D();
    private final Plane3D coneTipPlaneFacingBase = new Plane3D();
    private final Line3D coneBaseTowardsTipAxis = new Line3D();
