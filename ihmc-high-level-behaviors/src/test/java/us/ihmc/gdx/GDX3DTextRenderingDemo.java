b4cc8fc13b36f03609f2f83ec2b04833d5040997
Perry <pmacmurray@ihmc.org>
2021-07-08 14:17:53 -0500

    Implement barebones API for GDXTextRenderer

---
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDX3DTextRenderingDemo.java
@@ -0,0 +1,46 @@
+package us.ihmc.gdx;
+
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.gdx.sceneManager.GDX3DSceneManager;
+import us.ihmc.gdx.sceneManager.GDX3DSceneTools;
+import us.ihmc.gdx.tools.BoxesDemoModel;
+import us.ihmc.gdx.tools.GDXApplicationCreator;
+
+public class GDX3DTextRenderingDemo
+{
+   public GDX3DTextRenderingDemo()
+   {
+      GDX3DSceneManager sceneManager = new GDX3DSceneManager();
+      GDXTextRenderer textRenderer = new GDXTextRenderer();
+      GDXApplicationCreator.launchGDXApplication(new Lwjgl3ApplicationAdapter()
+      {
+         @Override
+         public void create()
+         {
+            sceneManager.create();
+
+            sceneManager.addCoordinateFrame(0.3);
+            sceneManager.addModelInstance(new BoxesDemoModel().newInstance());
+
+            textRenderer.create();
+            sceneManager.addRenderableProvider(textRenderer);
+         }
+
+         @Override
+         public void render()
+         {
+            textRenderer.setTextAtPosition(new Point3D(0.5, 0.5, 0.5), "Text rendering demo (very cool)");
+
+            GDX3DSceneTools.glClearGray();
+            textRenderer.update();
+            sceneManager.setViewportBoundsToWindow();
+            sceneManager.render();
+         }
+      }, "GDX3DDemo", 1100, 800);
+   }
+
+   public static void main(String[] args)
+   {
+      new GDX3DTextRenderingDemo();
+   }
+}
