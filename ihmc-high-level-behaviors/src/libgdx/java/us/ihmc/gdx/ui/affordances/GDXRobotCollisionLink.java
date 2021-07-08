029570d0b47ff8a2ae8448a7ef2df9d42828e3a6
Duncan Calvert <dcalvert@ihmc.us>
2021-07-08 18:48:14 -0500

    Create a visualizer for IK collision meshes.

---
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/affordances/GDXRobotCollisionLink.java
@@ -0,0 +1,108 @@
+package us.ihmc.gdx.ui.affordances;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.graphics.g3d.RenderableProvider;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.matrix.RotationMatrix;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
+import us.ihmc.euclid.shape.primitives.interfaces.*;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.gdx.tools.GDXModelPrimitives;
+import us.ihmc.gdx.tools.GDXTools;
+import us.ihmc.graphicsDescription.appearance.YoAppearance;
+import us.ihmc.log.LogTools;
+import us.ihmc.mecano.frames.MovingReferenceFrame;
+import us.ihmc.robotics.physics.Collidable;
+import us.ihmc.robotics.referenceFrames.ReferenceFrameMissingTools;
+
+public class GDXRobotCollisionLink implements RenderableProvider
+{
+   private static final Color COLOR = GDXTools.toGDX(YoAppearance.DarkGreen());
+   static
+   {
+      COLOR.a = 0.5f;
+   }
+   private final ReferenceFrame shapeFrame;
+   private final ModelInstance modelInstance;
+   private final RigidBodyTransform transformToJoint;
+   private final RigidBodyTransform tempTransform = new RigidBodyTransform();
+   private final ReferenceFrame collisionMeshFrame;
+
+   public GDXRobotCollisionLink(Collidable collidable)
+   {
+      Shape3DReadOnly shape = collidable.getShape();
+      shapeFrame = collidable.getShape().getReferenceFrame();
+      MovingReferenceFrame frameAfterJoint = collidable.getRigidBody().getParentJoint().getFrameAfterJoint();
+      // TODO update every frame
+      transformToJoint = new RigidBodyTransform(shapeFrame.getTransformToDesiredFrame(frameAfterJoint));
+      collisionMeshFrame = ReferenceFrameMissingTools.constructFrameWithChangingTransformToParent("collisionMeshFrame" + collidable.getRigidBody().getName(),
+                                                                                                  frameAfterJoint,
+                                                                                                  transformToJoint);
+
+      modelInstance = GDXModelPrimitives.buildModelInstance(meshBuilder ->
+      {
+         if (shape instanceof Sphere3DReadOnly)
+         {
+            Sphere3DReadOnly sphere = (Sphere3DReadOnly) shape;
+            meshBuilder.addSphere((float) sphere.getRadius(), sphere.getPosition(), COLOR);
+         }
+         else if (shape instanceof Capsule3DReadOnly)
+         {
+            Capsule3DReadOnly capsule = (Capsule3DReadOnly) shape;
+            Quaternion orientation = new Quaternion();
+            EuclidGeometryTools.orientation3DFromZUpToVector3D(capsule.getAxis(), orientation);
+            transformToJoint.appendTranslation(capsule.getPosition());
+            transformToJoint.appendOrientation(orientation);
+            meshBuilder.addCapsule(capsule.getLength(),
+                                   capsule.getRadius(),
+                                   capsule.getRadius(),
+                                   capsule.getRadius(),
+                                   50,
+                                   50,
+                                   COLOR);
+         }
+         else if (shape instanceof Box3DReadOnly)
+         {
+            Box3DReadOnly box = (Box3DReadOnly) shape;
+            transformToJoint.appendTranslation(box.getPosition());
+            transformToJoint.appendOrientation(box.getOrientation());
+            meshBuilder.addBox(box.getSizeX(),
+                               box.getSizeY(),
+                               box.getSizeZ(),
+                               COLOR);
+         }
+         else if (shape instanceof PointShape3DReadOnly)
+         {
+            PointShape3DReadOnly pointShape = (PointShape3DReadOnly) shape;
+            meshBuilder.addSphere((float) 0.01, pointShape, COLOR);
+         }
+         else
+         {
+            LogTools.warn("Shape not handled: {}", shape);
+         }
+      }, collidable.getRigidBody().getName());
+      GDXTools.setTransparency(modelInstance, COLOR.a);
+   }
+
+   public void update()
+   {
+//      tempTransform.set(shapeFrame.getTransformToWorldFrame());
+//      transformToJoint.transform(tempTransform);
+////      shapeFrame.getTransformToWorldFrame().transform(tempTransform);
+//      GDXTools.toGDX(tempTransform, modelInstance.transform);
+      collisionMeshFrame.update();
+      GDXTools.toGDX(collisionMeshFrame.getTransformToWorldFrame(), modelInstance.transform);
+   }
+
+   @Override
+   public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
+   {
+      modelInstance.getRenderables(renderables, pool);
+   }
+}
