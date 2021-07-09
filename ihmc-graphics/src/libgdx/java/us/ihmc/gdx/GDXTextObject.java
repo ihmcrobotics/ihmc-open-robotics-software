dfb3b976113540942ca76cd7bf7797091ad4dea0
Perry <pmacmurray@ihmc.org>
2021-07-09 11:26:36 -0500

    Text rendering paradigm shift

---
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/GDXTextObject.java
@@ -0,0 +1,15 @@
+package us.ihmc.gdx;
+
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.graphics.g3d.RenderableProvider;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+
+public class GDXTextObject implements RenderableProvider
+{
+   @Override
+   public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
+   {
+
+   }
+}
