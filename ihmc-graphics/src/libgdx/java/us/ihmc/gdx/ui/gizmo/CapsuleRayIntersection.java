606ff17158bcadf90f9564fcf4877bbab25c28a5
Duncan Calvert <dcalvert@ihmc.us>
2021-07-12 14:47:44 -0500

    Get capsule intersection working.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/ui/gizmo/CapsuleRayIntersection.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/ui/gizmo/CapsuleRayIntersection.java
@@ -1,7 +1,7 @@
 package us.ihmc.gdx.ui.gizmo;
 
 import us.ihmc.euclid.geometry.interfaces.Line3DReadOnly;
-import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.UnitVector3DReadOnly;
@@ -13,35 +13,33 @@ public class CapsuleRayIntersection
    private final CylinderRayIntersection cylinderIntersection = new CylinderRayIntersection();
 
    private final Vector3D tempVector = new Vector3D();
-   private final Point3D tempPoint = new Point3D();
+   private final RigidBodyTransform tempTransform = new RigidBodyTransform();
 
-   public void setup(double radius, double length, Point3DReadOnly position, UnitVector3DReadOnly axis)
+   public void setup(double radius, double length, Point3DReadOnly position, UnitVector3DReadOnly axis, RigidBodyTransform transformToWorld)
    {
+      tempTransform.set(transformToWorld);
       tempVector.set(axis);
+      transformToWorld.transform(tempVector);
       tempVector.scale(length / 2.0);
-      tempPoint.set(position);
-      tempPoint.add(tempVector);
-      sphereIntersectionPositiveEnd.setup(radius, tempPoint);
+      tempTransform.appendTranslation(tempVector);
+      sphereIntersectionPositiveEnd.setup(radius, tempTransform);
 
-      tempVector.set(axis);
-      tempVector.scale(length / 2.0);
-      tempPoint.set(position);
-      tempPoint.sub(tempVector);
-      sphereIntersectionNegativeEnd.setup(radius, tempPoint);
+      tempVector.scale(-2.0);
+      tempTransform.appendTranslation(tempVector);
+      sphereIntersectionNegativeEnd.setup(radius, tempTransform);
 
-      cylinderIntersection.setup(length, radius);
+      cylinderIntersection.setup(length, radius, transformToWorld);
 
 //      EuclidGeometryTools.distanceFromPoint3DToLineSegment3D() // TODO: Is there a simpler way to do capsule ray intersection?
    }
 
    public boolean intersect(Line3DReadOnly pickRay)
    {
-      boolean intersects = sphereIntersectionPositiveEnd.intersect(pickRay);
-
+      boolean intersects = false;
+      intersects |= sphereIntersectionPositiveEnd.intersect(pickRay);
       intersects |= sphereIntersectionNegativeEnd.intersect(pickRay);
-
       double closestDistance = cylinderIntersection.intersect(pickRay);
-
-      return intersects || Double.isNaN(closestDistance);
+      intersects |= !Double.isNaN(closestDistance);
+      return intersects;
    }
 }
