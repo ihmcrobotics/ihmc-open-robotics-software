56dfb86635d661b1cf9ffcc1a492d4c2870f68c0
Duncan Calvert <dcalvert@ihmc.us>
2021-07-12 16:38:07 -0500

    Create BoxRayIntersection class.

---
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/ui/gizmo/BoxRayIntersection.java
@@ -0,0 +1,17 @@
+package us.ihmc.gdx.ui.gizmo;
+
+import us.ihmc.euclid.geometry.interfaces.Line3DReadOnly;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+
+public class BoxRayIntersection
+{
+   public void setup(RigidBodyTransform transform)
+   {
+
+   }
+
+   public boolean intersect(Line3DReadOnly pickRay)
+   {
+      return false;
+   }
+}
