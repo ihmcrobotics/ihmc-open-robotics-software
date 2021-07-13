e8c339346214630d2ccf79bb4a10829c09ae6405
Perry <pmacmurray@ihmc.org>
2021-07-13 15:53:52 -0500

    Create demo for ImGui 3D rendering

---
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDX3DImGuiPanelsDemo.java
@@ -0,0 +1,54 @@
+package us.ihmc.gdx;
+
+import imgui.ImGui;
+import us.ihmc.gdx.sceneManager.GDX3DSceneManager;
+import us.ihmc.gdx.sceneManager.GDX3DSceneTools;
+import us.ihmc.gdx.tools.BoxesDemoModel;
+import us.ihmc.gdx.tools.GDXApplicationCreator;
+
+public class GDX3DImGuiPanelsDemo
+{
+   public GDX3DImGuiPanelsDemo()
+   {
+      GDX3DSceneManager sceneManager = new GDX3DSceneManager();
+      GDXApplicationCreator.launchGDXApplication(new Lwjgl3ApplicationAdapter()
+      {
+         @Override
+         public void create()
+         {
+            sceneManager.create();
+
+            sceneManager.addCoordinateFrame(0.3);
+            sceneManager.addModelInstance(new BoxesDemoModel().newInstance());
+
+            new GDXImGuiWindow("Test Window")
+            {
+               @Override
+               public void renderImGuiWidgets()
+               {
+                  ImGui.text("this is a test");
+                  ImGui.button("this is a button");
+               }
+            };
+
+            sceneManager.addRenderableProvider(GDXImGuiWindowManager.getInstance());
+         }
+
+         @Override
+         public void render()
+         {
+            GDX3DSceneTools.glClearGray();
+            sceneManager.setViewportBoundsToWindow();
+
+            GDXImGuiWindowManager.getInstance().update();
+
+            sceneManager.render();
+         }
+      }, "GDX3DDemo", 1100, 800);
+   }
+
+   public static void main(String[] args)
+   {
+      new GDX3DImGuiPanelsDemo();
+   }
+}
