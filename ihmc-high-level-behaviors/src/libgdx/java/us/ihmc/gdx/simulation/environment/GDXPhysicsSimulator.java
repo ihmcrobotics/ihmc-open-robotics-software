70248bc80586333af45e02278936e1bdd0538e49
Duncan Calvert <dcalvert@ihmc.us>
2021-07-16 11:43:04 -0500

    Depend on SCS 2 and setup libGDX physics demo.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/GDXPhysicsSimulator.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/GDXPhysicsSimulator.java
@@ -2,16 +2,24 @@ package us.ihmc.gdx.simulation.environment;
 
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.robotics.physics.ExperimentalPhysicsEngine;
+import us.ihmc.scs2.simulation.SimulationSession;
+import us.ihmc.scs2.simulation.physicsEngine.PhysicsEngine;
 import us.ihmc.simulationToolkit.physicsEngine.ExperimentalSimulation;
 import us.ihmc.simulationconstructionset.Robot;
 
 public class GDXPhysicsSimulator
 {
-   private final ExperimentalPhysicsEngine physicsEngine = new ExperimentalPhysicsEngine();
+   private final SimulationSession simulationSession;
+   //   private final ExperimentalPhysicsEngine physicsEngine = new ExperimentalPhysicsEngine();
+//   private final PhysicsEngine physicsEngine = new PhysicsEngine()
 
    public GDXPhysicsSimulator()
    {
-//      ExperimentalSimulation experimentalSimulation = new ExperimentalSimulation(allSimulatedRobotList.toArray(new Robot[0]),
+      simulationSession = new SimulationSession();
+
+//      simulationSession.
+
+      //      ExperimentalSimulation experimentalSimulation = new ExperimentalSimulation(allSimulatedRobotList.toArray(new Robot[0]),
 //                                                                                 simulationConstructionSetParameters.getDataBufferSize());
 //      experimentalSimulation.setGravity(new Vector3D(0.0, 0.0, -Math.abs(gravity.get())));
    }
