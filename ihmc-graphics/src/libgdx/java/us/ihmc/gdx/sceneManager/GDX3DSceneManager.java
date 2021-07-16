7ef5adab376e302be9090299826a5f7cbafc28e0
Perry <pmacmurray@ihmc.org>
2021-07-16 15:20:13 -0500

    Implement ClassicShadowSystem (working, but not properly)

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/sceneManager/GDX3DSceneManager.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/sceneManager/GDX3DSceneManager.java
@@ -4,8 +4,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalShadowLight;
+import com.badlogic.gdx.graphics.g3d.environment.PointLight;
+import com.badlogic.gdx.graphics.g3d.utils.DepthShaderProvider;
 import com.badlogic.gdx.graphics.profiling.GLProfiler;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.tests.g3d.shadows.system.ShadowSystem;
+import com.badlogic.gdx.tests.g3d.shadows.system.classical.ClassicalShadowSystem;
+import com.badlogic.gdx.tests.g3d.shadows.utils.AABBNearFarAnalyzer;
+import com.badlogic.gdx.tests.g3d.shadows.utils.BoundingSphereDirectionalAnalyzer;
+import com.badlogic.gdx.tests.g3d.shadows.utils.FixedShadowMapAllocator;
+import com.badlogic.gdx.tests.g3d.shadows.utils.FrustumLightFilter;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.BufferUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.exception.ExceptionTools;
@@ -15,6 +31,7 @@ import us.ihmc.gdx.tools.GDXModelPrimitives;
 import us.ihmc.gdx.tools.GDXTools;
 import us.ihmc.log.LogTools;
 
+import java.nio.IntBuffer;
 import java.util.HashSet;
 
 /**
@@ -27,6 +44,9 @@ public class GDX3DSceneManager
    private Environment environment;
    private ScreenViewport viewport;
    private ModelBatch modelBatch;
+
+   private ShadowSystem shadowSystem;
+   private Array<ModelBatch> shadowPassBatches = new Array<ModelBatch>();
 
    private int x = 0;
    private int y = 0;
@@ -63,7 +83,23 @@ public class GDX3DSceneManager
       viewport = new ScreenViewport(camera3D);
       viewport.setUnitsPerPixel(1.0f); // TODO: Is this relevant for high DPI displays?
 
-      //Do stuff here
+      environment = new Environment();
+      environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1, 1, 1, 0.2f));
+
+      PointLight light = new PointLight();
+      light.set(1, 1, 1, 0, 0, 20, 100);
+      environment.add(light);
+
+      shadowSystem = new ClassicalShadowSystem(new AABBNearFarAnalyzer(), new FixedShadowMapAllocator(2048, 4),
+                                               new BoundingSphereDirectionalAnalyzer(), new FrustumLightFilter());
+      shadowSystem.init();
+      shadowSystem.addLight(light);
+
+      for (int i = 0; i < shadowSystem.getPassQuantity(); i++) {
+         shadowPassBatches.add(new ModelBatch(shadowSystem.getPassShaderProvider(i)));
+      }
+
+      modelBatch = new ModelBatch(shadowSystem.getShaderProvider());
    }
 
    private void preRender()
@@ -84,7 +120,34 @@ public class GDX3DSceneManager
       if (height < 0)
          height = getCurrentWindowHeight();
 
+      IntBuffer buffer = BufferUtils.newIntBuffer(1);
+      Gdx.gl.glGetIntegerv(GL30.GL_DRAW_FRAMEBUFFER_BINDING, buffer);
+      int bufferID = buffer.get();
+
+      shadowSystem.begin(camera3D, renderables);
+      shadowSystem.update();
+
+      for (int i = 0; i < shadowSystem.getPassQuantity(); i++) {
+         shadowSystem.begin(i);
+         Camera camera;
+         while ((camera = shadowSystem.next()) != null) {
+            ModelBatch passBatch = shadowPassBatches.get(i);
+
+            passBatch.begin(camera);
+            passBatch.render(renderables, environment);
+            passBatch.end();
+         }
+         shadowSystem.end(i);
+      }
+
+      shadowSystem.end();
+
+      Gdx.gl.glBindFramebuffer(GL30.GL_DRAW_FRAMEBUFFER, bufferID); //manually bind framebuffer here even though it shouldn't be necessary because shadows mess it up somehow
+
       viewport.update(width, height);
+      viewport.apply();
+
+      GDX3DSceneTools.glClearGray();
 
       modelBatch.begin(camera3D);
    }
@@ -218,11 +281,6 @@ public class GDX3DSceneManager
       {
          LogTools.error(1, "libGDX is not being used for input!");
       }
-   }
-
-   public ModelBatch getModelBatch()
-   {
-      return modelBatch;
    }
 
    public Environment getEnvironment()
