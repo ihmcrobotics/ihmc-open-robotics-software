27c1e74893e3e10dcc6e0bd0a24d7b692be9b279
Perry <pmacmurray@ihmc.org>
2021-07-22 11:56:17 -0500

    Implementation of much of GDXShadowManager (and related classes)

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/lighting/GDXDirectionalLight.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/lighting/GDXDirectionalLight.java
@@ -1,12 +1,86 @@
 package us.ihmc.gdx.lighting;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g3d.RenderableProvider;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Vector3;
 
 public class GDXDirectionalLight extends GDXLight
 {
+   private FrameBuffer framebuffer;
+   private Texture depthMap;
+
+   protected Vector3 direction;
+
+   public GDXDirectionalLight(Vector3 position, Vector3 direction) {
+      this.position = position;
+      this.direction = direction;
+   }
+
+   @Override
+   protected <T extends RenderableProvider> void render(Iterable<T> renderableProviders)
+   {
+      if (framebuffer == null)
+      {
+         framebuffer = new FrameBuffer(Pixmap.Format.RGBA8888, DEPTHMAP_SIZE, DEPTHMAP_SIZE, true);
+      }
+      framebuffer.begin();
+      Gdx.gl.glClearColor(0, 0, 0, 1);
+      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+      shaderProgram.begin();
+      shaderProgram.setUniformf("u_cameraFar", camera.far);
+      shaderProgram.setUniformf("u_lightPosition", camera.position);
+      shaderProgram.end();
+
+      modelBatch.begin(camera);
+      modelBatch.render(renderableProviders);
+      modelBatch.end();
+
+      //PerryUtilsDONOTCOMMIT.screenshot(framebuffer, "depth");
+      framebuffer.end();
+      depthMap = framebuffer.getColorBufferTexture();
+   }
+
    @Override
    protected void apply(ShaderProgram shader)
    {
+      shader.begin();
+      final int textureNum = 2;
+      depthMap.bind(textureNum);
+      shader.setUniformi("u_depthMap", textureNum);
+      shader.setUniformMatrix("u_lightTrans", camera.combined);
+      shader.setUniformf("u_cameraFar", camera.far);
+      shader.setUniformf("u_lightPosition", camera.position);
+      shader.end();
+   }
 
+   @Override
+   public void init()
+   {
+      super.init();
+
+      this.camera = new PerspectiveCamera(120f, DEPTHMAP_SIZE, DEPTHMAP_SIZE);
+      this.camera.near = 0.1f;
+      this.camera.far = 5f;
+      this.camera.position.set(position);
+      this.camera.lookAt(direction);
+
+      this.camera.update();
+   }
+
+   public Vector3 getDirection()
+   {
+      return direction;
+   }
+
+   public void setDirection(Vector3 direction)
+   {
+      this.direction = direction;
    }
 }
--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/lighting/GDXShadowManager.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/lighting/GDXShadowManager.java
@@ -1,6 +1,42 @@
 package us.ihmc.gdx.lighting;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.RenderableProvider;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.utils.Array;
+
 public class GDXShadowManager
 {
+   private final Array<GDXLight> lights = new Array<>();
+   private final ShaderProgram shader;
 
+   /**
+    * @param shader The ShaderProgram used to create the shader used by the main ModelBatch
+    */
+   public GDXShadowManager(ShaderProgram shader) {
+      this.shader = shader;
+   }
+
+   public void addLight(GDXLight light) {
+      lights.add(light);
+   }
+
+   public <T extends RenderableProvider> void render(Iterable<T> renderableProviders) {
+      for (GDXLight light : lights) {
+         light.render(renderableProviders);
+      }
+
+      for (GDXLight light : lights) {
+         light.apply(shader);
+      }
+   }
+
+   public static String getVertexShader() {
+      return Gdx.files.classpath("us/ihmc/gdx/shadows/scene_v.glsl").readString();
+   }
+
+   public static String getFragmentShader() {
+      return Gdx.files.classpath("us/ihmc/gdx/shadows/scene_f.glsl").readString();
+   }
 }
