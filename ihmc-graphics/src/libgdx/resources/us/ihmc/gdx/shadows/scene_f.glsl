2ed253736e179e77e4520ae64bab6457a9f1a59b
Perry <pmacmurray@ihmc.org>
2021-07-22 11:55:00 -0500

    Update scene vertex/fragment shaders

--- ihmc-graphics/src/libgdx/resources/us/ihmc/gdx/shadows/scene_f.glsl
+++ ihmc-graphics/src/libgdx/resources/us/ihmc/gdx/shadows/scene_f.glsl
@@ -10,29 +10,39 @@ precision mediump float;
 #endif
 
 uniform sampler2D u_diffuseTexture;
-uniform sampler2D u_shadows;
-uniform float u_screenWidth;
-uniform float u_screenHeight;
+uniform sampler2D u_depthMap;
+uniform float u_cameraFar;
+uniform vec3 u_lightPosition;
+
 
 varying vec2 v_texCoords0;
 varying float v_intensity;
+varying vec4 v_positionLightTrans;
+varying vec4 v_position;
 
 void main()
 {
 	vec4 finalColor  = texture2D(u_diffuseTexture, v_texCoords0);
 	finalColor.rgb   = finalColor.rgb*v_intensity;
 
-	// Retrieve the shadow color from shadow map
-	vec2 c= gl_FragCoord.xy;
-	c.x/=u_screenWidth;
-	c.y/=u_screenHeight;
-	vec4 color=texture2D(u_shadows,c);
-	
-	// Apply shadow
-	finalColor.rgb*=(0.4+0.6*color.a);
-	
+	vec3 depth = (v_positionLightTrans.xyz / v_positionLightTrans.w)*0.5+0.5;
+	// Make sure the point is in the field of view of the light
+	// and also that it is not behind it
+	if (v_positionLightTrans.z>=0.0 &&
+			(depth.x >= 0.0) && (depth.x <= 1.0) &&
+			(depth.y >= 0.0) && (depth.y <= 1.0) ) {
+		float lenToLight=length(v_position.xyz-u_lightPosition)/u_cameraFar;
+		float lenDepthMap= texture2D(u_depthMap, depth.xy).a;
+		// If can not be viewed by light > shadows
+		if(lenDepthMap<lenToLight-0.0025){ //subtract a bit to help remove floating point arithmetic artifacts
+			finalColor.rgb*=0.4;
+		}else{
+			finalColor.rgb*=0.4+0.6*(1.0-lenToLight);
+		}
+	}else{
+		finalColor.rgb*=0.4;
+	}
+
 	gl_FragColor     = finalColor;
-	
-	
 }
 
--- ihmc-graphics/src/libgdx/resources/us/ihmc/gdx/shadows/scene_v.glsl
+++ ihmc-graphics/src/libgdx/resources/us/ihmc/gdx/shadows/scene_v.glsl
@@ -17,20 +17,26 @@ attribute vec3 a_normal;
 uniform mat4 u_projViewTrans;
 uniform mat4 u_worldTrans;
 uniform mat3 u_normalMatrix;
+uniform mat4 u_lightTrans;
 
 varying vec2 v_texCoords0;
 varying float v_intensity;
+varying vec4 v_positionLightTrans;
+varying vec4 v_position;
+
+
 
 void main()
-{	
+{
 	// Vertex position after transformation
-    vec4 pos = u_worldTrans * vec4(a_position, 1.0);
-    gl_Position = u_projViewTrans * pos;
+    v_position = u_worldTrans * vec4(a_position, 1.0);
+    v_positionLightTrans = u_lightTrans * v_position;
+    gl_Position =   u_projViewTrans * v_position;
     
     v_texCoords0 = a_texCoord0;
     
     // Just add some basic self shadow
-    vec3 normal = normalize(u_normalMatrix * a_normal);    
+    vec3 normal = normalize(u_normalMatrix * a_normal);
 	v_intensity=1.0;
    	if(normal.y<0.5){
 		if(normal.x>0.5 || normal.x<-0.5)
