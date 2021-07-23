980320c2aa813d1550ecb5da51c7a3cf3aa0d572
Duncan Calvert <dcalvert@ihmc.us>
2021-07-23 00:00:31 -0500

    Fix the poses of the collision meshes for SCS 2. Some of the visual poses are wrong though.

---
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/FrameGDXNodePart.java
@@ -0,0 +1,40 @@
+package us.ihmc.gdx.simulation.environment;
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
+import us.ihmc.gdx.tools.GDXTools;
+import us.ihmc.gdx.ui.gizmo.DynamicGDXModel;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FrameGDXNodePart
+{
+   private static final AtomicInteger INDEX = new AtomicInteger();
+
+   private final DynamicGDXModel model;
+   private final ModelInstance modelInstance;
+   private final ReferenceFrame modelFrame;
+
+   public FrameGDXNodePart(ReferenceFrame referenceFrame, DynamicGDXModel model)
+   {
+      this.model = model;
+      modelInstance = model.getOrCreateModelInstance();
+      modelFrame = ReferenceFrameTools.constructFrameWithUnchangingTransformToParent("modelFrame" + INDEX.getAndIncrement(),
+                                                                                     referenceFrame,
+                                                                                     model.getLocalTransform());
+   }
+
+   public void update()
+   {
+      GDXTools.toGDX(modelFrame.getTransformToRoot(), modelInstance.transform);
+   }
+
+   public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
+   {
+      modelInstance.getRenderables(renderables, pool);
+   }
+}
