738c887642e08c2bd049b5194dea7d40abe978b9
Duncan Calvert <dcalvert@ihmc.us>
2021-07-24 13:51:19 -0500

    :arrow_up: simulation-construction-set-2 alpha-20210724

--- ihmc-high-level-behaviors/build.gradle.kts
+++ ihmc-high-level-behaviors/build.gradle.kts
@@ -36,7 +36,7 @@ libgdxDependencies {
    api("us.ihmc:ihmc-graphics-libgdx:source")
    api("us.ihmc:ihmc-perception-javacv:source")
    api("us.ihmc:ihmc-graphics-libgdx:source")
-   api("us.ihmc:simulation-construction-set-2-simulation:source")
+   api("us.ihmc:simulation-construction-set-2-simulation:alpha-20210724")
 }
 
 testDependencies {
