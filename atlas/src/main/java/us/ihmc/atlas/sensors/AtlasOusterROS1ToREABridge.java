3b235e02152b2fb1f27585d0367e67b1a797a720
Operator Computer <OperatorComputer@unknown>
2021-07-26 15:19:55 -0500

    Outgoing changes on Atlas OCU

--- atlas/src/main/java/us/ihmc/atlas/sensors/AtlasOusterROS1ToREABridge.java
+++ atlas/src/main/java/us/ihmc/atlas/sensors/AtlasOusterROS1ToREABridge.java
@@ -1,2 +1,56 @@
-package us.ihmc.atlas.sensors;public class AtlasOusterROS1ToREABridge {
+package us.ihmc.atlas.sensors;
+
+import controller_msgs.msg.dds.LidarScanMessage;
+import sensor_msgs.PointCloud2;
+import us.ihmc.avatar.networkProcessor.stereoPointCloudPublisher.PointCloudData;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.communication.configuration.NetworkParameters;
+import us.ihmc.communication.ros2.ROS2Helper;
+import us.ihmc.pubsub.DomainFactory;
+import us.ihmc.ros2.ROS2Node;
+import us.ihmc.tools.thread.MissingThreadTools;
+import us.ihmc.tools.thread.ResettableExceptionHandlingExecutorService;
+import us.ihmc.utilities.ros.RosMainNode;
+import us.ihmc.utilities.ros.RosTools;
+import us.ihmc.utilities.ros.subscriber.AbstractRosTopicSubscriber;
+
+public class AtlasOusterROS1ToREABridge
+{
+   public AtlasOusterROS1ToREABridge()
+   {
+      RosMainNode ros1Node = RosTools.createRosNode(NetworkParameters.getROSURI(), "ouster_to_rea");
+      ROS2Node ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "ouster_to_rea");
+      ROS2Helper ros2Helper = new ROS2Helper(ros2Node);
+
+      ResettableExceptionHandlingExecutorService executor = MissingThreadTools.newSingleThreadExecutor("OusterToREABridge", true);
+
+      AbstractRosTopicSubscriber<PointCloud2> ousterSubscriber = new AbstractRosTopicSubscriber<PointCloud2>(PointCloud2._TYPE)
+      {
+         @Override
+         public void onNewMessage(PointCloud2 pointCloud2)
+         {
+            executor.submit(() ->
+            {
+               PointCloudData pointCloudData = new PointCloudData(pointCloud2, 1600000, false);
+               LidarScanMessage lidarScanMessage = pointCloudData.toLidarScanMessage();
+               ros2Helper.publish(ROS2Tools.MULTISENSE_LIDAR_SCAN, lidarScanMessage);
+            });
+         }
+      };
+      ros1Node.attachSubscriber(RosTools.OUSTER_POINT_CLOUD, ousterSubscriber);
+      ros1Node.execute();
+
+      Runtime.getRuntime().addShutdownHook(new Thread(() ->
+      {
+         executor.destroy();
+         ros1Node.shutdown();
+      }, "IHMC-OusterROS1ToREABridgeShutdown"));
+
+//      ThreadTools.join();
+   }
+
+   public static void main(String[] args)
+   {
+      new AtlasOusterROS1ToREABridge();
+   }
 }
