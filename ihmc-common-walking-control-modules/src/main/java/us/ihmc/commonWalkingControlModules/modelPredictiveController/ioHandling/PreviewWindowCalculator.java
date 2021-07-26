d14965b7a8f54f51c7920012d99af0c679a0194d
rgriffin@ihmc.org <rgriffin@ihmc.us>
2021-07-26 16:00:29 -0500

    fixed the orientation drift by having a lower weight in the final angular velocity than the orientation error

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/ioHandling/PreviewWindowCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/ioHandling/PreviewWindowCalculator.java
@@ -4,6 +4,7 @@ import us.ihmc.commonWalkingControlModules.dynamicPlanning.comPlanning.ContactSt
 import us.ihmc.commonWalkingControlModules.modelPredictiveController.ContactPlaneProvider;
 import us.ihmc.commons.lists.RecyclingArrayList;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
+import us.ihmc.log.LogTools;
 import us.ihmc.robotics.time.TimeIntervalBasics;
 import us.ihmc.robotics.time.TimeIntervalReadOnly;
 import us.ihmc.yoVariables.registry.YoRegistry;
