a7084fb08f922b59e61e6639fd5b94c3be145089
Perry <pmacmurray@ihmc.org>
2021-08-04 11:12:00 -0500

    Get color from ROS1 topics

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS1PointCloudVisualizer.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS1PointCloudVisualizer.java
@@ -1,19 +1,25 @@
 package us.ihmc.gdx.ui.graphics.live;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.graphics.g3d.RenderableProvider;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 import imgui.internal.ImGui;
+import org.jboss.netty.buffer.ChannelBuffer;
+import sensor_msgs.CameraInfo;
+import sensor_msgs.Image;
 import sensor_msgs.PointCloud2;
 import us.ihmc.avatar.networkProcessor.stereoPointCloudPublisher.PointCloudData;
 import us.ihmc.commons.lists.RecyclingArrayList;
-import us.ihmc.euclid.exceptions.NotARotationMatrixException;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.Point3D32;
+import us.ihmc.gdx.BufferBasedColorProvider;
 import us.ihmc.gdx.GDXPointCloudRenderer;
 import us.ihmc.gdx.imgui.ImGuiPlot;
 import us.ihmc.gdx.ui.visualizers.ImGuiGDXROS1Visualizer;
@@ -51,7 +57,19 @@ public class GDXROS1PointCloudVisualizer extends ImGuiGDXROS1Visualizer implemen
 
    private AbstractRosTopicSubscriber<PointCloud2> subscriber;
    private long receivedCount = 0;
+
+   //Camera color stuff
    private Color color = Color.WHITE;
+   private String rosVideoTopic = null;
+   private String rosCameraInfoTopic = null;
+
+   private AbstractRosTopicSubscriber<Image> imageSubscriber = null;
+   private AbstractRosTopicSubscriber<CameraInfo> cameraInfoSubscriber = null;
+
+   private volatile Image image;
+   private volatile CameraInfo cameraInfo;
+
+   private GDXPointCloudRenderer.ColorProvider colorProvider = null;
 
    private boolean flipToZUp = false;
 
@@ -93,12 +111,38 @@ public class GDXROS1PointCloudVisualizer extends ImGuiGDXROS1Visualizer implemen
          }
       };
       ros1Node.attachSubscriber(ros1PointCloudTopic, subscriber);
+
+      if (rosVideoTopic != null) {
+         imageSubscriber = new AbstractRosTopicSubscriber<Image>(sensor_msgs.Image._TYPE)
+         {
+            @Override
+            public void onNewMessage(Image image)
+            {
+               GDXROS1PointCloudVisualizer.this.image = image;
+            }
+         };
+         ros1Node.attachSubscriber(rosVideoTopic, imageSubscriber);
+
+         cameraInfoSubscriber = new AbstractRosTopicSubscriber<CameraInfo>(sensor_msgs.CameraInfo._TYPE)
+         {
+            @Override
+            public void onNewMessage(CameraInfo info)
+            {
+               GDXROS1PointCloudVisualizer.this.cameraInfo = info;
+            }
+         };
+         ros1Node.attachSubscriber(rosVideoTopic, imageSubscriber);
+      }
    }
 
    @Override
    public void unsubscribe(RosNodeInterface ros1Node)
    {
       ros1Node.removeSubscriber(subscriber);
+      if (imageSubscriber != null) {
+         ros1Node.removeSubscriber(imageSubscriber);
+         imageSubscriber = null;
+      }
    }
 
    private void queueRenderPointCloud(PointCloud2 message)
@@ -111,6 +155,48 @@ public class GDXROS1PointCloudVisualizer extends ImGuiGDXROS1Visualizer implemen
             {
                boolean hasColors = false;
                PointCloudData pointCloudData = new PointCloudData(message, MAX_POINTS, hasColors);
+
+               if (imageSubscriber != null) {
+                  PointCloudData pointCloudDataForColors = new PointCloudData(message, MAX_POINTS, hasColors);
+
+                  Image image = this.image;
+                  CameraInfo info = this.cameraInfo;
+
+                  //Applies the following transform:
+                  //     [fx'  0  cx' Tx]
+                  // P = [ 0  fy' cy' Ty]
+                  //     [ 0   0   1   0]
+                  //This transforms a 3D point to a 2D pixel coordinate
+                  //https://docs.ros.org/en/noetic/api/sensor_msgs/html/msg/CameraInfo.html#:~:text=%23%C2%A0Projection/camera%C2%A0matrix,a%C2%A0stereo%C2%A0pair%2E
+                  pointCloudDataForColors.applyTransform(new RigidBodyTransform(info.getP()));
+
+                  ChannelBuffer data = image.getData();
+                  int zeroedIndex = 0;
+
+                  Pixmap pixmap = new Pixmap(image.getWidth(), image.getHeight(), Pixmap.Format.RGBA8888);
+                  for (int y = 0; y < image.getHeight(); y++)
+                  {
+                     for (int x = 0; x < image.getWidth(); x++)
+                     {
+                        int r = Byte.toUnsignedInt(data.getByte(zeroedIndex + 0));
+                        int g = Byte.toUnsignedInt(data.getByte(zeroedIndex + 1));
+                        int b = Byte.toUnsignedInt(data.getByte(zeroedIndex + 2));
+                        int a = 255;
+                        zeroedIndex += 3;
+                        int rgb8888 = (r << 24) | (g << 16) | (b << 8) | a;
+                        pixmap.drawPixel(x, y, rgb8888);
+                     }
+                  }
+
+                  BufferBasedColorProvider provider = new BufferBasedColorProvider();
+
+                  for (Point3D point3D : pointCloudDataForColors.getPointCloud()) {
+                     Point2D point = new Point2D(point3D.getX(), point3D.getY());
+                     provider.add(new Color(pixmap.getPixel((int) point.getX(), (int) point.getY())));
+                  }
+
+                  this.colorProvider = provider;
+               }
 
                if (flipToZUp)
                   pointCloudData.flipToZUp();
@@ -175,7 +261,14 @@ public class GDXROS1PointCloudVisualizer extends ImGuiGDXROS1Visualizer implemen
             }
          }
 
-         pointCloudRenderer.setPointsToRender(pointsToRender, color);
+         if (colorProvider != null) {
+            pointCloudRenderer.setPointsToRender(pointsToRender, colorProvider);
+         }
+         else
+         {
+            pointCloudRenderer.setPointsToRender(pointsToRender, color);
+         }
+
          if (!pointsToRender.isEmpty())
          {
             pointCloudRenderer.updateMesh();
@@ -212,8 +305,21 @@ public class GDXROS1PointCloudVisualizer extends ImGuiGDXROS1Visualizer implemen
       this.flipToZUp = flipToZUp;
    }
 
+   /**
+    * Overrides {@link #setColor(Color)} - will get color from camera. Should be called BEFORE subscribing
+    */
+   public void setROSVideoTopic(String videoTopic, String cameraInfoTopic) {
+      this.color = null;
+      this.rosVideoTopic = videoTopic;
+      this.rosCameraInfoTopic = cameraInfoTopic;
+   }
+
+   /**
+    * Overrides {@link #setROSVideoTopic(String, String)} - will always use passed color (not from camera)
+    */
    public void setColor(Color color)
    {
       this.color = color;
+      this.rosVideoTopic = null;
    }
 }
