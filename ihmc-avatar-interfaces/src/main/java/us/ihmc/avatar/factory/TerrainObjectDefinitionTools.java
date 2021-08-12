5ae081d472014c192990fd54f9e1e4072a153688
Sylvain Bertrand <sbertrand@ihmc.us>
2021-08-12 10:33:14 -0500

    Explicitly defined the terrain collision's mask & group

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/factory/TerrainObjectDefinitionTools.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/factory/TerrainObjectDefinitionTools.java
@@ -3,6 +3,7 @@ package us.ihmc.avatar.factory;
 import java.util.List;
 
 import us.ihmc.robotics.physics.Collidable;
+import us.ihmc.robotics.physics.CollidableHelper;
 import us.ihmc.scs2.definition.collision.CollisionShapeDefinition;
 import us.ihmc.scs2.definition.terrain.TerrainObjectDefinition;
 import us.ihmc.scs2.definition.visual.ColorDefinitions;
@@ -13,11 +14,21 @@ import us.ihmc.simulationToolkit.physicsEngine.ExperimentalSimulation;
 
 public class TerrainObjectDefinitionTools
 {
-   public static TerrainObjectDefinition toTerrainObjectDefinition(CommonAvatarEnvironmentInterface environment)
+   public static TerrainObjectDefinition toTerrainObjectDefinition(CommonAvatarEnvironmentInterface environment,
+                                                                   CollidableHelper collidableHelper,
+                                                                   String terrainCollisionMask,
+                                                                   String... interactableCollisionGroups)
+   {
+      return toTerrainObjectDefinition(environment,
+                                       collidableHelper.getCollisionMask(terrainCollisionMask),
+                                       collidableHelper.createCollisionGroup(interactableCollisionGroups));
+   }
+
+   public static TerrainObjectDefinition toTerrainObjectDefinition(CommonAvatarEnvironmentInterface environment, long terrainCollisionMask, long collisionGroup)
    {
       TerrainObjectDefinition output = new TerrainObjectDefinition();
 
-      List<Collidable> collidables = ExperimentalSimulation.toCollidables(-1, -1, environment);
+      List<Collidable> collidables = ExperimentalSimulation.toCollidables(terrainCollisionMask, collisionGroup, environment);
 
       for (Collidable collidable : collidables)
       {
