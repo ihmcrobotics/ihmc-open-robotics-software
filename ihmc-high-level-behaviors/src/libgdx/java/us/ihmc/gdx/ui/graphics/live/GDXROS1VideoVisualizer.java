978eb6f188f920b33f19ae686bc6b18004a63a43
Operator Computer <OperatorComputer@unknown>
2021-08-13 14:45:16 -0500

    L515 color video working in the UI using OpenCV.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS1VideoVisualizer.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS1VideoVisualizer.java
@@ -12,12 +12,17 @@ import us.ihmc.gdx.imgui.ImGuiPlot;
 import us.ihmc.gdx.imgui.ImGuiTools;
 import us.ihmc.gdx.imgui.ImGuiVideoPanel;
 import us.ihmc.gdx.ui.visualizers.ImGuiGDXROS1Visualizer;
+import us.ihmc.perception.CvImage;
+import us.ihmc.perception.ImageEncodings;
 import us.ihmc.utilities.ros.RosNodeInterface;
 import us.ihmc.utilities.ros.RosTools;
 import us.ihmc.utilities.ros.subscriber.AbstractRosTopicSubscriber;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 
 public class GDXROS1VideoVisualizer extends ImGuiGDXROS1Visualizer
 {
@@ -112,99 +117,111 @@ public class GDXROS1VideoVisualizer extends ImGuiGDXROS1Visualizer
             boolean is8BitRGB = compressedImage.getFormat().contains("rgb8");
             boolean isBGR8 = compressedImage.getFormat().contains("bgr8");
 
-            BufferedImage bufferedImage = RosTools.bufferedImageFromRosMessageJpeg(compressedImage);
-            byte[] data = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
-            int width = bufferedImage.getWidth();
-            int height = bufferedImage.getHeight();
-
-            ensureTextureReady(width, height);
-
-            if (is8BitRGB)
+            try
             {
-               int zeroedIndex = 0;
-               for (int y = 0; y < height; y++)
-               {
-                  for (int x = 0; x < width; x++)
-                  {
-//                     int rgbaColor = data[zeroedIndex] << 24 | data[zeroedIndex + 1] << 16 | data[zeroedIndex + 2] << 8 | 255;
-                     int color = bufferedImage.getRGB(x, y);
-                     zeroedIndex += 3;
-                     pixmap.drawPixel(x, y, (color << 8) | 255);
-                  }
-               }
+               CvImage cvImage = CvImage.toCvCopy(compressedImage, ImageEncodings.RGBA8);
+               Buffer buffer = cvImage.image.createBuffer();
+               ensureTextureReady(cvImage.image.cols(), cvImage.image.rows());
+               pixmap.setPixels((ByteBuffer) buffer);
+               //               cvImage.image.asByteBuffer().get(pixmap.getPixels().array());
             }
-            else if (isBGR8)
+            catch (Exception e)
             {
-               for (int y = 0; y < height; y++)
-               {
-                  for (int x = 0; x < width; x++)
-                  {
-                     int i = (y * width + x) * 3;
-                     int b = Byte.toUnsignedInt(data[i + 0]);
-                     int g = Byte.toUnsignedInt(data[i + 1]);
-                     int r = Byte.toUnsignedInt(data[i + 2]);
-                     int a = 255;
-                     int rgb8888 = (r << 24) | (g << 16) | (b << 8) | a;
-                     pixmap.drawPixel(x, y, rgb8888);
-                  }
-               }
+               e.printStackTrace();
             }
-            texture.draw(pixmap, 0, 0);
-         }
-         else if (image != null)
-         {
-            ensureTextureReady(image.getWidth(), image.getHeight());
 
-            boolean is16BitDepth = image.getEncoding().equals("16UC1");
-            boolean is8BitRGB = image.getEncoding().equals("rgb8");
-            if (is8BitRGB)
-            {
-               ChannelBuffer data = image.getData();
-               int zeroedIndex = 0;
-               for (int y = 0; y < image.getHeight(); y++)
-               {
-                  for (int x = 0; x < image.getWidth(); x++)
-                  {
-                     int r = Byte.toUnsignedInt(data.getByte(zeroedIndex + 0));
-                     int g = Byte.toUnsignedInt(data.getByte(zeroedIndex + 1));
-                     int b = Byte.toUnsignedInt(data.getByte(zeroedIndex + 2));
-                     int a = 255;
-                     zeroedIndex += 3;
-                     int rgb8888 = (r << 24) | (g << 16) | (b << 8) | a;
-                     pixmap.drawPixel(x, y, rgb8888);
-                  }
-               }
+            //            BufferedImage bufferedImage = RosTools.bufferedImageFromRosMessageJpeg(compressedImage);
+            //            byte[] data = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
+            //            int width = bufferedImage.getWidth();
+            //            int height = bufferedImage.getHeight();
+            //
+//
+//            if (is8BitRGB)
+//            {
+//               int zeroedIndex = 0;
+//               for (int y = 0; y < height; y++)
+//               {
+//                  for (int x = 0; x < width; x++)
+//                  {
+////                     int rgbaColor = data[zeroedIndex] << 24 | data[zeroedIndex + 1] << 16 | data[zeroedIndex + 2] << 8 | 255;
+//                     int color = bufferedImage.getRGB(x, y);
+//                     zeroedIndex += 3;
+//                     pixmap.drawPixel(x, y, (color << 8) | 255);
+//                  }
+//               }
+//            }
+//            else if (isBGR8)
+//            {
+//               for (int y = 0; y < height; y++)
+//               {
+//                  for (int x = 0; x < width; x++)
+//                  {
+//                     int i = (y * width + x) * 3;
+//                     int b = Byte.toUnsignedInt(data[i + 0]);
+//                     int g = Byte.toUnsignedInt(data[i + 1]);
+//                     int r = Byte.toUnsignedInt(data[i + 2]);
+//                     int a = 255;
+//                     int rgb8888 = (r << 24) | (g << 16) | (b << 8) | a;
+//                     pixmap.drawPixel(x, y, rgb8888);
+//                  }
+//               }
+//            }
+//            texture.draw(pixmap, 0, 0);
+//         }
+//         else if (image != null)
+//         {
+//            ensureTextureReady(image.getWidth(), image.getHeight());
+//
+//            boolean is16BitDepth = image.getEncoding().equals("16UC1");
+//            boolean is8BitRGB = image.getEncoding().equals("rgb8");
+//            if (is8BitRGB)
+//            {
+//               ChannelBuffer data = image.getData();
+//               int zeroedIndex = 0;
+//               for (int y = 0; y < image.getHeight(); y++)
+//               {
+//                  for (int x = 0; x < image.getWidth(); x++)
+//                  {
+//                     int r = Byte.toUnsignedInt(data.getByte(zeroedIndex + 0));
+//                     int g = Byte.toUnsignedInt(data.getByte(zeroedIndex + 1));
+//                     int b = Byte.toUnsignedInt(data.getByte(zeroedIndex + 2));
+//                     int a = 255;
+//                     zeroedIndex += 3;
+//                     int rgb8888 = (r << 24) | (g << 16) | (b << 8) | a;
+//                     pixmap.drawPixel(x, y, rgb8888);
+//                  }
+//               }
+//               texture.draw(pixmap, 0, 0);
+//            }
+//            else if (is16BitDepth)
+//            {
+//               ChannelBuffer data = image.getData();
+//               byte[] array = data.array();
+//               int dataIndex = data.arrayOffset();
+//               for (int y = 0; y < image.getHeight(); y++)
+//               {
+//                  for (int x = 0; x < image.getWidth(); x++)
+//                  {
+//                     int bigByte = array[dataIndex];
+//                     dataIndex++;
+//                     int smallByte = array[dataIndex];
+//                     dataIndex++;
+//
+//                     float value = (float) (bigByte & 0xFF | smallByte << 8);
+//
+//                     if (highestValueSeen < 0 || value > highestValueSeen)
+//                        highestValueSeen = value;
+//                     if (lowestValueSeen < 0 || value < lowestValueSeen)
+//                        lowestValueSeen = value;
+//
+//                     float colorRange = highestValueSeen - lowestValueSeen;
+//                     float grayscale = (value - lowestValueSeen) / colorRange;
+//
+//                     pixmap.drawPixel(x, y, Color.rgba8888(grayscale, grayscale, grayscale, 1.0f));
+//                  }
+//               }
                texture.draw(pixmap, 0, 0);
-            }
-            else if (is16BitDepth)
-            {
-               ChannelBuffer data = image.getData();
-               byte[] array = data.array();
-               int dataIndex = data.arrayOffset();
-               for (int y = 0; y < image.getHeight(); y++)
-               {
-                  for (int x = 0; x < image.getWidth(); x++)
-                  {
-                     int bigByte = array[dataIndex];
-                     dataIndex++;
-                     int smallByte = array[dataIndex];
-                     dataIndex++;
-
-                     float value = (float) (bigByte & 0xFF | smallByte << 8);
-
-                     if (highestValueSeen < 0 || value > highestValueSeen)
-                        highestValueSeen = value;
-                     if (lowestValueSeen < 0 || value < lowestValueSeen)
-                        lowestValueSeen = value;
-
-                     float colorRange = highestValueSeen - lowestValueSeen;
-                     float grayscale = (value - lowestValueSeen) / colorRange;
-
-                     pixmap.drawPixel(x, y, Color.rgba8888(grayscale, grayscale, grayscale, 1.0f));
-                  }
-               }
-               texture.draw(pixmap, 0, 0);
-            }
+//            }
          }
       }
    }
