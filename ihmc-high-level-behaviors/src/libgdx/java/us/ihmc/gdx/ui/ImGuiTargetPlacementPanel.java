e6dd708fb821d641e511de7c0e647961e6bb2847
Duncan Calvert <dcalvert@ihmc.us>
2021-08-22 18:36:17 -0500

    Format only.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/ImGuiTargetPlacementPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/ImGuiTargetPlacementPanel.java
@@ -14,39 +14,41 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+public class ImGuiTargetPlacementPanel
+{
+   private static final String WINDOW_NAME = "Target Placement";
 
+   private ManagedMessager managedMessager = new ManagedMessager();
 
-public class ImGuiTargetPlacementPanel {
+   private boolean targetEnabled = false;
+   private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
-    private static final String WINDOW_NAME = "Target Placement";
+   public ImGuiTargetPlacementPanel(RosNodeInterface ros1Node, ROS2NodeInterface ros2Node)
+   {
+      executor.scheduleAtFixedRate(this::run, 0, 2000, TimeUnit.MILLISECONDS);
+   }
 
-    private ManagedMessager managedMessager = new ManagedMessager();
+   public void run()
+   {
+      if (targetEnabled)
+      {
+         LogTools.info("Running Target Placer");
 
-    private boolean targetEnabled = false;
-    private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+         Pose3D goalPose = new Pose3D(0, 10, 0, 0, 0, 0);
+         managedMessager.submitMessage(BuildingExplorationBehaviorAPI.Goal, goalPose);
+      }
+   }
 
-    public ImGuiTargetPlacementPanel(RosNodeInterface ros1Node, ROS2NodeInterface ros2Node) {
-        executor.scheduleAtFixedRate(this::run, 0, 2000, TimeUnit.MILLISECONDS);
-    }
+   public void render()
+   {
+      if (ImGui.button("Place Target"))
+      {
+         targetEnabled = true;
+      }
+   }
 
-    public void run() {
-        if (targetEnabled) {
-            LogTools.info("Running Target Placer");
-
-            Pose3D goalPose = new Pose3D(0,10,0, 0, 0, 0);
-            managedMessager.submitMessage(BuildingExplorationBehaviorAPI.Goal, goalPose);
-
-        }
-    }
-
-    public void render() {
-        if (ImGui.button("Place Target")) {
-            targetEnabled = true;
-        }
-
-    }
-
-    public String getWindowName() {
-        return WINDOW_NAME;
-    }
+   public String getWindowName()
+   {
+      return WINDOW_NAME;
+   }
 }
