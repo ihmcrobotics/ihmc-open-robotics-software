882b0cab6eba825ab7d1b719597ca817528cd576
BhavyanshM <bhavyanshmishra@gmail.com>
2021-08-25 23:18:32 -0500

    Add the initial files for target-following-behavior.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/GDXPerceptionVisualizerUI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/GDXPerceptionVisualizerUI.java
@@ -32,161 +32,161 @@ import java.net.URISyntaxException;
 
 public class GDXPerceptionVisualizerUI
 {
-    private final PlanarRegionSLAMMapper realsensePlanarRegionSLAM = new PlanarRegionSLAMMapper();
-    private PlanarRegionsList regionsUpdate = new PlanarRegionsList();
-//    private final GDXHighLevelDepthSensorSimulator simulatedDepthSensor;
+   private final PlanarRegionSLAMMapper realsensePlanarRegionSLAM = new PlanarRegionSLAMMapper();
+   private PlanarRegionsList regionsUpdate = new PlanarRegionsList();
+   //    private final GDXHighLevelDepthSensorSimulator simulatedDepthSensor;
 
-    private final GDXImGuiBasedUI baseUI;
-    private final ImGuiMapSenseConfigurationPanel mapsenseConfigurationUI;
-    private final ImGuiTargetPlacementPanel targetPlacementPanel;
-    private final ImGuiGDXGlobalVisualizersPanel globalVisualizersUI;
-    private final GDXEnvironmentBuilderPanel environmentBuilderUI;
+   private final GDXImGuiBasedUI baseUI;
+   private final ImGuiMapSenseConfigurationPanel mapsenseConfigurationUI;
+   private final ImGuiTargetPlacementPanel targetPlacementPanel;
+   private final ImGuiGDXGlobalVisualizersPanel globalVisualizersUI;
+   private final GDXEnvironmentBuilderPanel environmentBuilderUI;
 
-    private final RigidBodyTransform depthSensorTransform = new RigidBodyTransform();
-    private final Matrix4 gdxSensorTransform = new Matrix4();
+   private final RigidBodyTransform depthSensorTransform = new RigidBodyTransform();
+   private final Matrix4 gdxSensorTransform = new Matrix4();
 
-    private final boolean LOW_RESOLUTION_SENSORS = true;
-    private GDXL515SensorObject l515Model;
+   private final boolean LOW_RESOLUTION_SENSORS = true;
+   private GDXL515SensorObject l515Model;
 
-    public GDXPerceptionVisualizerUI()
-    {
-        ROS2Node ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, ROS2Tools.REA_NODE_NAME);
-        RosMainNode ros1Node = new RosMainNode(NetworkParameters.getROSURI(), "PerceptionVisualizer");
+   public GDXPerceptionVisualizerUI()
+   {
+      ROS2Node ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, ROS2Tools.REA_NODE_NAME);
+      RosMainNode ros1Node = new RosMainNode(NetworkParameters.getROSURI(), "PerceptionVisualizer");
 
-        baseUI = new GDXImGuiBasedUI(getClass(), "ihmc-open-robotics-software", "ihmc-high-level-behaviors/src/libgdx/resources", "Perception Visualizer");
-        mapsenseConfigurationUI = new ImGuiMapSenseConfigurationPanel(ros1Node, ros2Node);
-        targetPlacementPanel = new ImGuiTargetPlacementPanel(ros1Node, ros2Node);
+      baseUI = new GDXImGuiBasedUI(getClass(), "ihmc-open-robotics-software", "ihmc-high-level-behaviors/src/libgdx/resources", "Perception Visualizer");
+      mapsenseConfigurationUI = new ImGuiMapSenseConfigurationPanel(ros1Node, ros2Node);
+      targetPlacementPanel = new ImGuiTargetPlacementPanel(ros1Node, ros2Node);
 
-        globalVisualizersUI = new ImGuiGDXGlobalVisualizersPanel();
-        GDXROS1PlanarRegionsVisualizer mapsenseRegionsVisualizer = new GDXROS1PlanarRegionsVisualizer("MapSense Planar Regions",
-                                                                                                      ros2Node,
-                                                                                                      RosTools.MAPSENSE_REGIONS);
-        globalVisualizersUI.addVisualizer(mapsenseRegionsVisualizer);
-        globalVisualizersUI.addVisualizer(new GDXROS1PointCloudVisualizer("Head Ouster", RosTools.OUSTER_POINT_CLOUD));
-        globalVisualizersUI.addVisualizer(new GDXROS1VideoVisualizer("L515 Color Video", RosTools.L515_VIDEO));
-        globalVisualizersUI.addVisualizer(new GDXROS1VideoVisualizer("L515 Depth Video", RosTools.L515_DEPTH));
-        globalVisualizersUI.addVisualizer(new GDXROS1VideoVisualizer("L515 Compressed Video", RosTools.L515_COMPRESSED_VIDEO));
-        globalVisualizersUI.addVisualizer(new GDXROS2PointCloudVisualizer("MultiSense lidar scan", ros2Node, ROS2Tools.MULTISENSE_LIDAR_SCAN));
-        globalVisualizersUI.addVisualizer(new GDXROS2PlanarRegionsVisualizer("Lidar REA planar regions", ros2Node, ROS2Tools.LIDAR_REA_REGIONS));
-        globalVisualizersUI.addVisualizer(new GDXROS1OdometryVisualizer("SLAM Poses", RosTools.SLAM_POSE));
-        globalVisualizersUI.addVisualizer(new GDXROS1OdometryVisualizer("Internal Sensor Pose", "/atlas/sensors/chest_l515/pose"));
+      globalVisualizersUI = new ImGuiGDXGlobalVisualizersPanel();
+      GDXROS1PlanarRegionsVisualizer mapsenseRegionsVisualizer = new GDXROS1PlanarRegionsVisualizer("MapSense Planar Regions",
+                                                                                                    ros2Node,
+                                                                                                    RosTools.MAPSENSE_REGIONS);
+      globalVisualizersUI.addVisualizer(mapsenseRegionsVisualizer);
+      globalVisualizersUI.addVisualizer(new GDXROS1PointCloudVisualizer("Head Ouster", RosTools.OUSTER_POINT_CLOUD));
+      globalVisualizersUI.addVisualizer(new GDXROS1VideoVisualizer("L515 Color Video", RosTools.L515_VIDEO));
+      globalVisualizersUI.addVisualizer(new GDXROS1VideoVisualizer("L515 Depth Video", RosTools.L515_DEPTH));
+      globalVisualizersUI.addVisualizer(new GDXROS1VideoVisualizer("L515 Compressed Video", RosTools.L515_COMPRESSED_VIDEO));
+      globalVisualizersUI.addVisualizer(new GDXROS2PointCloudVisualizer("MultiSense lidar scan", ros2Node, ROS2Tools.MULTISENSE_LIDAR_SCAN));
+      globalVisualizersUI.addVisualizer(new GDXROS2PlanarRegionsVisualizer("Lidar REA planar regions", ros2Node, ROS2Tools.LIDAR_REA_REGIONS));
+      globalVisualizersUI.addVisualizer(new GDXROS1OdometryVisualizer("SLAM Poses", RosTools.SLAM_POSE));
+      globalVisualizersUI.addVisualizer(new GDXROS1OdometryVisualizer("Internal Sensor Pose", "/atlas/sensors/chest_l515/pose"));
 
-        environmentBuilderUI = new GDXEnvironmentBuilderPanel();
+      environmentBuilderUI = new GDXEnvironmentBuilderPanel();
 
-//        simulatedDepthSensor = createSimulatedDepthSensor(ros1Node);
-//        simulatedDepthSensor.setSensorFrameToWorldTransform(depthSensorTransform);
-//        simulatedDepthSensor.setRenderPointCloudDirectly(true);
-//        simulatedDepthSensor.setDebugCoordinateFrame(true);
+      //        simulatedDepthSensor = createSimulatedDepthSensor(ros1Node);
+      //        simulatedDepthSensor.setSensorFrameToWorldTransform(depthSensorTransform);
+      //        simulatedDepthSensor.setRenderPointCloudDirectly(true);
+      //        simulatedDepthSensor.setDebugCoordinateFrame(true);
 
-        baseUI.getImGuiPanelManager().addPanel(globalVisualizersUI);
-        baseUI.getImGuiPanelManager().addPanel(mapsenseRegionsVisualizer.getLoggingPanel());
-//        baseUI.getImGuiDockingSetup().addWindow(simulatedDepthSensor.getWindowName(), simulatedDepthSensor::renderImGuiWindow);
-        baseUI.getImGuiPanelManager().addPanel(mapsenseConfigurationUI.getWindowName(), mapsenseConfigurationUI::render);
-        baseUI.getImGuiPanelManager().addPanel(targetPlacementPanel.getWindowName(), targetPlacementPanel::render);
-        baseUI.getImGuiPanelManager().addPanel(environmentBuilderUI.getWindowName(), environmentBuilderUI::renderImGuiWindow);
+      baseUI.getImGuiPanelManager().addPanel(globalVisualizersUI);
+      baseUI.getImGuiPanelManager().addPanel(mapsenseRegionsVisualizer.getLoggingPanel());
+      //        baseUI.getImGuiDockingSetup().addWindow(simulatedDepthSensor.getWindowName(), simulatedDepthSensor::renderImGuiWindow);
+      baseUI.getImGuiPanelManager().addPanel(mapsenseConfigurationUI.getWindowName(), mapsenseConfigurationUI::render);
+      baseUI.getImGuiPanelManager().addPanel(targetPlacementPanel.getWindowName(), targetPlacementPanel::render);
+      baseUI.getImGuiPanelManager().addPanel(environmentBuilderUI.getWindowName(), environmentBuilderUI::renderImGuiWindow);
 
-        baseUI.launchGDXApplication(new Lwjgl3ApplicationAdapter()
-        {
-            @Override
-            public void create()
-            {
-                baseUI.create();
+      baseUI.launchGDXApplication(new Lwjgl3ApplicationAdapter()
+      {
+         @Override
+         public void create()
+         {
+            baseUI.create();
 
-                baseUI.get3DSceneManager().addRenderableProvider(globalVisualizersUI, GDXSceneLevel.VIRTUAL);
+            baseUI.get3DSceneManager().addRenderableProvider(globalVisualizersUI, GDXSceneLevel.VIRTUAL);
 
-//                simulatedDepthSensor.create();
-//                baseUI.getSceneManager().addRenderableProvider(simulatedDepthSensor, GDXSceneLevel.VIRTUAL);
+            //                simulatedDepthSensor.create();
+            //                baseUI.getSceneManager().addRenderableProvider(simulatedDepthSensor, GDXSceneLevel.VIRTUAL);
 
-                globalVisualizersUI.create();
-                environmentBuilderUI.create(baseUI);
-//                l515Model = new GDXL515SensorObject();
-//                environmentBuilderUI.getModelInput().addInstance(l515Model);
-                baseUI.get3DSceneManager().addRenderableProvider(environmentBuilderUI);
+            globalVisualizersUI.create();
+            environmentBuilderUI.create(baseUI);
+            //                l515Model = new GDXL515SensorObject();
+            //                environmentBuilderUI.getModelInput().addInstance(l515Model);
+            baseUI.get3DSceneManager().addRenderableProvider(environmentBuilderUI);
 
-                ros1Node.execute();
-            }
+            ros1Node.execute();
+         }
 
-            @Override
-            public void render()
-            {
-//                gdxSensorTransform.set(l515Model.getRealisticModelInstance().transform);
-//                GDXTools.toEuclid(gdxSensorTransform, depthSensorTransform);
-//                simulatedDepthSensor.render(baseUI.getSceneManager());
-                globalVisualizersUI.update();
+         @Override
+         public void render()
+         {
+            //                gdxSensorTransform.set(l515Model.getRealisticModelInstance().transform);
+            //                GDXTools.toEuclid(gdxSensorTransform, depthSensorTransform);
+            //                simulatedDepthSensor.render(baseUI.getSceneManager());
+            globalVisualizersUI.update();
 
-                baseUI.renderBeforeOnScreenUI();
-                baseUI.renderEnd();
-            }
+            baseUI.renderBeforeOnScreenUI();
+            baseUI.renderEnd();
+         }
 
-            @Override
-            public void dispose()
-            {
-//                simulatedDepthSensor.dispose();
-                globalVisualizersUI.destroy();
-                baseUI.dispose();
-                ros2Node.destroy();
-                ros1Node.shutdown();
-            }
-        });
-    }
+         @Override
+         public void dispose()
+         {
+            //                simulatedDepthSensor.dispose();
+            globalVisualizersUI.destroy();
+            baseUI.dispose();
+            ros2Node.destroy();
+            ros1Node.shutdown();
+         }
+      });
+   }
 
-    public void regionsCallback(PlanarRegionsListMessage regions)
-    {
-        this.regionsUpdate = PlanarRegionMessageConverter.convertToPlanarRegionsList(regions);
-    }
+   public void regionsCallback(PlanarRegionsListMessage regions)
+   {
+      this.regionsUpdate = PlanarRegionMessageConverter.convertToPlanarRegionsList(regions);
+   }
 
-    public void setupPlanarRegionSLAM(ROS2Node ros2Node)
-    {
-        new IHMCROS2Callback<>(ros2Node, ROS2Tools.MAPSENSE_REGIONS, this::regionsCallback);
-        new PeriodicPlanarRegionPublisher(ros2Node, ROS2Tools.REALSENSE_SLAM_REGIONS, 0.001f, () -> realsensePlanarRegionSLAM.update(regionsUpdate)).start();
-    }
+   public void setupPlanarRegionSLAM(ROS2Node ros2Node)
+   {
+      new IHMCROS2Callback<>(ros2Node, ROS2Tools.MAPSENSE_REGIONS, this::regionsCallback);
+      new PeriodicPlanarRegionPublisher(ros2Node, ROS2Tools.REALSENSE_SLAM_REGIONS, 0.001f, () -> realsensePlanarRegionSLAM.update(regionsUpdate)).start();
+   }
 
-    private GDXHighLevelDepthSensorSimulator createSimulatedDepthSensor(RosNodeInterface ros1Node)
-    {
-        double publishRateHz = 5.0;
-        double verticalFOV = 55.0;
-        int imageWidth = 640;
-        int imageHeight = 480;
-        double fx = 500.0;
-        double fy = 500.0;
-        if (LOW_RESOLUTION_SENSORS)
-        {
-            imageWidth /= 2;
-            imageHeight /= 2;
-            fx /= 2;
-            fy /= 2;
-        }
-        double minRange = 0.105;
-        double maxRange = 5.0;
-        CameraPinholeBrown depthCameraIntrinsics = new CameraPinholeBrown(fx, fy, 0, imageWidth / 2.0, imageHeight / 2.0, imageWidth, imageHeight);
-        ROS2NodeInterface ros2Node = null;
-        ROS2Topic<?> ros2Topic = null;
-        ReferenceFrame sensorFrame = null;
-        ROS2SyncedRobotModel syncedRobot = null;
-        return new GDXHighLevelDepthSensorSimulator("L515",
-                                                    ros1Node,
-                                                    RosTools.MAPSENSE_DEPTH_IMAGE,
-                                                    RosTools.MAPSENSE_DEPTH_CAMERA_INFO,
-                                                    depthCameraIntrinsics,
-                                                    RosTools.L515_VIDEO,
-                                                    RosTools.L515_COLOR_CAMERA_INFO,
-                                                    ros2Node,
-                                                    ros2Topic,
-                                                    null,
-                                                    sensorFrame,
-                                                    syncedRobot::getTimestamp,
-                                                    verticalFOV,
-                                                    imageWidth,
-                                                    imageHeight,
-                                                    minRange,
-                                                    maxRange,
-                                                    publishRateHz,
-                                                    false);
-    }
+   private GDXHighLevelDepthSensorSimulator createSimulatedDepthSensor(RosNodeInterface ros1Node)
+   {
+      double publishRateHz = 5.0;
+      double verticalFOV = 55.0;
+      int imageWidth = 640;
+      int imageHeight = 480;
+      double fx = 500.0;
+      double fy = 500.0;
+      if (LOW_RESOLUTION_SENSORS)
+      {
+         imageWidth /= 2;
+         imageHeight /= 2;
+         fx /= 2;
+         fy /= 2;
+      }
+      double minRange = 0.105;
+      double maxRange = 5.0;
+      CameraPinholeBrown depthCameraIntrinsics = new CameraPinholeBrown(fx, fy, 0, imageWidth / 2.0, imageHeight / 2.0, imageWidth, imageHeight);
+      ROS2NodeInterface ros2Node = null;
+      ROS2Topic<?> ros2Topic = null;
+      ReferenceFrame sensorFrame = null;
+      ROS2SyncedRobotModel syncedRobot = null;
+      return new GDXHighLevelDepthSensorSimulator("L515",
+                                                  ros1Node,
+                                                  RosTools.MAPSENSE_DEPTH_IMAGE,
+                                                  RosTools.MAPSENSE_DEPTH_CAMERA_INFO,
+                                                  depthCameraIntrinsics,
+                                                  RosTools.L515_VIDEO,
+                                                  RosTools.L515_COLOR_CAMERA_INFO,
+                                                  ros2Node,
+                                                  ros2Topic,
+                                                  null,
+                                                  sensorFrame,
+                                                  syncedRobot::getTimestamp,
+                                                  verticalFOV,
+                                                  imageWidth,
+                                                  imageHeight,
+                                                  minRange,
+                                                  maxRange,
+                                                  publishRateHz,
+                                                  false);
+   }
 
-    public static void main(String[] args) throws URISyntaxException
-    {
-        new GDXPerceptionVisualizerUI();
-    }
+   public static void main(String[] args) throws URISyntaxException
+   {
+      new GDXPerceptionVisualizerUI();
+   }
 }
 
---
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/targetFollowing/TargetFollowingBehavior.java
@@ -0,0 +1,92 @@
+package us.ihmc.behaviors.targetFollowing;
+
+import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
+import us.ihmc.behaviors.BehaviorDefinition;
+import us.ihmc.behaviors.BehaviorInterface;
+import us.ihmc.behaviors.lookAndStep.LookAndStepBehavior;
+import us.ihmc.behaviors.tools.BehaviorHelper;
+import us.ihmc.behaviors.tools.behaviorTree.BehaviorTreeNodeStatus;
+import us.ihmc.behaviors.tools.behaviorTree.ResettingNode;
+import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.log.LogTools;
+import us.ihmc.tools.thread.MissingThreadTools;
+import us.ihmc.tools.thread.ResettableExceptionHandlingExecutorService;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static us.ihmc.behaviors.buildingExploration.BuildingExplorationBehaviorAPI.*;
+import static us.ihmc.behaviors.buildingExploration.BuildingExplorationBehaviorTools.NAN_POSE;
+
+public class TargetFollowingBehavior extends ResettingNode implements BehaviorInterface
+{
+   public static final BehaviorDefinition DEFINITION = new BehaviorDefinition("Target Following", TargetFollowingBehavior::new, TargetFollowingBehaviorAPI.API);
+
+   private final BehaviorHelper helper;
+   private final LookAndStepBehavior lookAndStepBehavior;
+   private final TargetFollowingBehaviorParameters parameters;
+   private final ROS2SyncedRobotModel syncedRobot;
+   private final AtomicReference<Pose3D> goal = new AtomicReference<>(NAN_POSE);
+   private final ResettableExceptionHandlingExecutorService executor = MissingThreadTools.newSingleThreadExecutor("CommsRelay", true);
+
+   private String lastTickedThing = "NONE";
+
+   public TargetFollowingBehavior(BehaviorHelper helper)
+   {
+      this.helper = helper;
+      LogTools.info("Constructing");
+      parameters = new TargetFollowingBehaviorParameters();
+      syncedRobot = helper.newSyncedRobot();
+      lookAndStepBehavior = new LookAndStepBehavior(helper);
+      addChild(lookAndStepBehavior);
+      helper.subscribeViaCallback(Parameters, parameters ->
+      {
+         helper.getOrCreateStatusLogger().info("Accepting new building exploration parameters");
+         this.parameters.setAllFromStrings(parameters);
+      });
+      helper.subscribeViaCallback(Goal, newGoal ->
+      {
+         goal.set(newGoal);
+      });
+      helper.subscribeViaCallback(Mode, newValue ->
+      {
+         LogTools.info("Received mode: {}", newValue);
+      });
+      //      helper.getOrCreateControllerStatusTracker().addNotWalkingStateAnymoreCallback(() ->
+      //      {
+      //      });
+   }
+
+   @Override
+   public BehaviorTreeNodeStatus tickInternal()
+   {
+      return null;
+   }
+
+   private BehaviorTreeNodeStatus tickLookAndStep()
+   {
+      if (lookAndStepBehavior.isReset())
+         lookAndStepBehavior.acceptGoal(goal.get());
+      lastTickedThing = "LOOK_AND_STEP";
+      return lookAndStepBehavior.tick();
+   }
+
+   @Override
+   public void reset()
+   {
+
+   }
+
+   @Override
+   public String getName()
+   {
+      return DEFINITION.getName();
+   }
+
+   @Override
+   public void destroy()
+   {
+      lookAndStepBehavior.destroy();
+   }
+}
+
+
