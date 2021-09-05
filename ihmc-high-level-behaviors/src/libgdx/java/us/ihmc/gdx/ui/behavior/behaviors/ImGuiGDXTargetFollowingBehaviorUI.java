fbe175a58f719cb196e9375bf60f94e485c96737
Operator Computer <OperatorComputer@unknown>
2021-09-04 19:14:52 -0500

    Getting person following working.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/behavior/behaviors/ImGuiGDXTargetFollowingBehaviorUI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/behavior/behaviors/ImGuiGDXTargetFollowingBehaviorUI.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 import imgui.internal.ImGui;
+import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
 import us.ihmc.behaviors.targetFollowing.TargetFollowingBehavior;
 import us.ihmc.behaviors.targetFollowing.TargetFollowingBehaviorParameters;
 import us.ihmc.behaviors.tools.BehaviorHelper;
 import us.ihmc.communication.ROS2Tools;
-import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.gdx.imgui.ImGuiLabelMap;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
 import us.ihmc.gdx.ui.ImGuiStoredPropertySetTuner;
@@ -17,7 +19,13 @@ import us.ihmc.gdx.ui.affordances.ImGuiGDXPoseGoalAffordance;
 import us.ihmc.gdx.ui.behavior.registry.ImGuiGDXBehaviorUIDefinition;
 import us.ihmc.gdx.ui.behavior.registry.ImGuiGDXBehaviorUIInterface;
 import us.ihmc.gdx.visualizers.GDXPlanarRegionsGraphic;
+import us.ihmc.humanoidRobotics.frames.HumanoidReferenceFrames;
+import us.ihmc.robotics.kinematics.TimeStampedTransform3D;
 import us.ihmc.tools.thread.PausablePeriodicThread;
+import us.ihmc.utilities.ros.RosTools;
+import us.ihmc.utilities.ros.subscriber.RosPoseStampedSubscriber;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import static us.ihmc.behaviors.buildingExploration.BuildingExplorationBehaviorAPI.*;
 
@@ -27,6 +35,7 @@ public class ImGuiGDXTargetFollowingBehaviorUI extends ImGuiGDXBehaviorUIInterfa
                                                                                                   ImGuiGDXTargetFollowingBehaviorUI::new);
 
    private final BehaviorHelper helper;
+   private final ROS2SyncedRobotModel syncedRobot;
    private TargetFollowingBehaviorParameters parameters;
    private final ImGuiStoredPropertySetTuner parameterTuner = new ImGuiStoredPropertySetTuner("Target Following Parameters");
    private final ImGuiGDXPoseGoalAffordance goalAffordance = new ImGuiGDXPoseGoalAffordance();
@@ -35,12 +44,14 @@ public class ImGuiGDXTargetFollowingBehaviorUI extends ImGuiGDXBehaviorUIInterfa
    private final GDXPlanarRegionsGraphic planarRegionsGraphic = new GDXPlanarRegionsGraphic();
    private String lastTickedThing = "NONE";
    private int pointNumber;
-   private final Pose3D goalPose = new Pose3D();
+   private final FramePose3D goalPose = new FramePose3D();
    private final PausablePeriodicThread periodicThread;
+   private final AtomicReference<TimeStampedTransform3D> latestSemanticTargetPose = new AtomicReference<>();
 
    public ImGuiGDXTargetFollowingBehaviorUI(BehaviorHelper helper)
    {
       this.helper = helper;
+      syncedRobot = helper.newSyncedRobot();
       lookAndStepUI = new ImGuiGDXLookAndStepBehaviorUI(helper);
       addChild(lookAndStepUI);
       helper.subscribeToPlanarRegionsViaCallback(ROS2Tools.MAPSENSE_REGIONS, regions ->
@@ -50,6 +61,17 @@ public class ImGuiGDXTargetFollowingBehaviorUI extends ImGuiGDXBehaviorUIInterfa
             planarRegionsGraphic.generateMeshesAsync(regions);
       });
       helper.subscribeViaCallback(LastTickedThing, lastTickedThing -> this.lastTickedThing = lastTickedThing);
+      helper.getROS1Helper().attachSubscriber(RosTools.SEMANTIC_TARGET_POSE, new RosPoseStampedSubscriber()
+      {
+         @Override
+         protected void newPose(String frameID, TimeStampedTransform3D transform)
+         {
+            synchronized (this)
+            {
+               latestSemanticTargetPose.set(transform);
+            }
+         }
+      });
 
       pointNumber = 0;
       int numberOfPoints = 20;
@@ -65,8 +87,11 @@ public class ImGuiGDXTargetFollowingBehaviorUI extends ImGuiGDXBehaviorUIInterfa
          double x = radius * Math.cos(percentage2PI);
          double y = radius * Math.sin(percentage2PI);
          double yaw = Math.PI / 2.0 + percentage2PI;
-         goalPose.set(x, y, 0.0, yaw, 0.0, 0.0);
-         lookAndStepUI.setGoal(goalPose);
+//         goalPose.set(x, y, 0.0, yaw, 0.0, 0.0);
+         synchronized (this)
+         {
+            lookAndStepUI.setGoal(goalPose);
+         }
 
          ++pointNumber;
       });
@@ -91,6 +116,11 @@ public class ImGuiGDXTargetFollowingBehaviorUI extends ImGuiGDXBehaviorUIInterfa
       // TODO: Need to get the state of the remote node. Is it actively being ticked?
       periodicThread.setRunning(wasTickedRecently(0.5));
 
+      syncedRobot.update();
+      goalPose.setIncludingFrame(syncedRobot.getReferenceFrames().getObjectDetectionCameraFrame(), latestSemanticTargetPose.get().getTransform3D());
+      goalPose.changeFrame(ReferenceFrame.getWorldFrame());
+      goalPose.getPosition().setZ(syncedRobot.getFramePoseReadOnly(HumanoidReferenceFrames::getMidFeetZUpFrame).getZ());
+
       if (areGraphicsEnabled())
       {
          planarRegionsGraphic.update();
@@ -106,7 +136,6 @@ public class ImGuiGDXTargetFollowingBehaviorUI extends ImGuiGDXBehaviorUIInterfa
       ImGui.text(areGraphicsEnabled() ? "Showing graphics." : "Graphics hidden.");
       parameterTuner.renderImGuiWidgets();
    }
-
 
    @Override
    public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
