08d0f00cbb7ca5b88e263fe89e29929081ca43e6
Sylvain Bertrand <sbertrand@ihmc.us>
2021-09-16 18:37:34 -0500

    Updated dependency.

--- ihmc-high-level-behaviors/build.gradle.kts
+++ ihmc-high-level-behaviors/build.gradle.kts
@@ -36,7 +36,7 @@ libgdxDependencies {
    api("us.ihmc:ihmc-graphics-libgdx:source")
    api("us.ihmc:ihmc-perception-javacv:source")
    api("us.ihmc:ihmc-graphics-libgdx:source")
-   api("us.ihmc:simulation-construction-set-2-simulation:alpha-20210724")
+   api("us.ihmc:scs2-simulation:0.0")
 }
 
 testDependencies {
