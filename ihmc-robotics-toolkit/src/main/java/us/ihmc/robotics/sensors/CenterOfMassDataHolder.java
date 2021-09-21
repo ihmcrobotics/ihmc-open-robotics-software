80cbddc59550f08029250bec238fa470b91de4c5
Sylvain Bertrand <sbertrand@ihmc.us>
2021-09-21 12:05:14 -0500

    Added position data in the CoM data holder.

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/sensors/CenterOfMassDataHolder.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/sensors/CenterOfMassDataHolder.java
@@ -1,32 +1,84 @@
 package us.ihmc.robotics.sensors;
 
+import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FixedFramePoint3DBasics;
+import us.ihmc.euclid.referenceFrame.interfaces.FixedFrameVector3DBasics;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.robotics.screwTheory.GenericCRC32;
 
 public class CenterOfMassDataHolder implements CenterOfMassDataHolderReadOnly
 {
-   private final FrameVector3D centerOfMassVelocity = new FrameVector3D();
-   
-   public void setCenterOfMassVelocity(FrameVector3DReadOnly centerOfMassVelocity)
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+   private boolean hasPosition = false;
+   private boolean hasVelocity = false;
+   private final FixedFramePoint3DBasics centerOfMassPosition = new FramePoint3D(worldFrame);
+   private final FixedFrameVector3DBasics centerOfMassVelocity = new FrameVector3D(worldFrame);
+
+   public CenterOfMassDataHolder()
    {
-      this.centerOfMassVelocity.setIncludingFrame(centerOfMassVelocity); 
+      clear();
    }
 
-   public void set(CenterOfMassDataHolder estimatorCenterOfMassDataHolder)
+   public void clear()
    {
-      this.centerOfMassVelocity.setIncludingFrame(estimatorCenterOfMassDataHolder.centerOfMassVelocity);
+      hasPosition = false;
+      hasVelocity = false;
+      centerOfMassPosition.setToNaN();
+      centerOfMassVelocity.setToNaN();
+   }
+
+   public void setCenterOfMassPosition(FramePoint3DReadOnly centerOfMassPosition)
+   {
+      hasPosition = true;
+      this.centerOfMassPosition.setMatchingFrame(centerOfMassPosition);
+   }
+
+   public void setCenterOfMassVelocity(FrameVector3DReadOnly centerOfMassVelocity)
+   {
+      hasVelocity = true;
+      this.centerOfMassVelocity.setMatchingFrame(centerOfMassVelocity);
+   }
+
+   public void set(CenterOfMassDataHolder other)
+   {
+      hasPosition = other.hasPosition;
+      hasVelocity = other.hasVelocity;
+      centerOfMassPosition.set(other.centerOfMassPosition);
+      centerOfMassVelocity.set(other.centerOfMassVelocity);
    }
 
    @Override
-   public void getCenterOfMassVelocity(FrameVector3D centerOfMassVelocityToPack)
+   public boolean hasCenterOfMassPosition()
    {
-      centerOfMassVelocityToPack.setIncludingFrame(centerOfMassVelocity);
+      return hasPosition;
+   }
+
+   @Override
+   public FramePoint3DReadOnly getCenterOfMassPosition()
+   {
+      return centerOfMassPosition;
+   }
+
+   @Override
+   public boolean hasCenterOfMassVelocity()
+   {
+      return hasVelocity;
+   }
+
+   @Override
+   public FrameVector3DReadOnly getCenterOfMassVelocity()
+   {
+      return centerOfMassVelocity;
    }
 
    public void calculateChecksum(GenericCRC32 checksum)
    {
+      checksum.update(hasPosition);
+      checksum.update(hasVelocity);
+      checksum.update(centerOfMassPosition);
       checksum.update(centerOfMassVelocity);
    }
-
 }
