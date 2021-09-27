345680dd165233565e804c15d939d33a73a11650
rgriffin@ihmc.org <rgriffin@ihmc.us>
2021-09-27 08:59:11 -0500

    set up a flag to zero the angular excursion value

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/AngularExcursionCalculator.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/AngularExcursionCalculator.java
@@ -4,24 +4,29 @@ import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.yoVariables.euclid.YoQuaternion;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
 import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoBoolean;
 
 public class AngularExcursionCalculator
 {
    private final WholeBodyAngularVelocityCalculator angularVelocityCalculator;
    private final YoFrameVector3D angularExcursion;
    private final YoFrameVector3D wholeBodyAngularVelocity;
+   private final YoBoolean zeroAngularExcursionFlag;
    private final double dt;
 
-   public AngularExcursionCalculator(ReferenceFrame centerOfMassFrame, RigidBodyBasics rootBody, double dt, YoRegistry registry)
+   public AngularExcursionCalculator(ReferenceFrame centerOfMassFrame, RigidBodyBasics rootBody, double dt, YoRegistry registry,
+                                     YoGraphicsListRegistry graphicsListRegistry)
    {
       this.dt = dt;
 
-      angularVelocityCalculator = new WholeBodyAngularVelocityCalculator(centerOfMassFrame, rootBody.subtreeArray());
+      angularVelocityCalculator = new WholeBodyAngularVelocityCalculator(centerOfMassFrame, graphicsListRegistry, rootBody.subtreeArray());
 
+      zeroAngularExcursionFlag = new YoBoolean("zeroAngularExcursionFlag", registry);
       angularExcursion = new YoFrameVector3D("angularExcursion", centerOfMassFrame, registry);
       wholeBodyAngularVelocity = new YoFrameVector3D("wholeBodyAngularVelocity", centerOfMassFrame, registry);
    }
@@ -39,6 +44,12 @@ public class AngularExcursionCalculator
    public void compute()
    {
       angularVelocityCalculator.compute();
+
+      if (zeroAngularExcursionFlag.getBooleanValue())
+      {
+         setToZero();
+         zeroAngularExcursionFlag.set(false);
+      }
 
       wholeBodyAngularVelocity.set(angularVelocityCalculator.getWholeBodyAngularVelocity());
       angularExcursion.scaleAdd(dt, wholeBodyAngularVelocity, angularExcursion);
