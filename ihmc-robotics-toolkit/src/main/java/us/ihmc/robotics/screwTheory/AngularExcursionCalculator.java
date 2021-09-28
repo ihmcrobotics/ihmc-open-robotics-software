03a7009d363ec3d31c12279520b924ea3f627cd8
Robert <rgriffin@ihmc.us>
2021-09-27 22:14:23 -0500

    set up some more vis stuff

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/AngularExcursionCalculator.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/screwTheory/AngularExcursionCalculator.java
@@ -5,9 +5,11 @@ import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.graphicsDescription.yoGraphics.YoGraphicCoordinateSystem;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.yoVariables.euclid.YoQuaternion;
+import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePose3D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameQuaternion;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameYawPitchRoll;
@@ -22,13 +24,18 @@ public class AngularExcursionCalculator
    private final YoFrameQuaternion angularExcursion;
    private final YoFrameYawPitchRoll angularExcursionRPY;
    private final YoFrameVector3D wholeBodyAngularVelocity;
+   private final YoFrameVector3D wholeBodyAngularMomentum;
    private final YoBoolean zeroAngularExcursionFlag;
    private final double dt;
+
+   private final YoFramePose3D comPose;
+   private final ReferenceFrame centerOfMassFrame;
 
    public AngularExcursionCalculator(ReferenceFrame centerOfMassFrame, RigidBodyBasics rootBody, double dt, YoRegistry registry,
                                      YoGraphicsListRegistry graphicsListRegistry)
    {
       this.dt = dt;
+      this.centerOfMassFrame = centerOfMassFrame;
 
       angularVelocityCalculator = new WholeBodyAngularVelocityCalculator(centerOfMassFrame, graphicsListRegistry, rootBody.subtreeArray());
 
@@ -36,6 +43,11 @@ public class AngularExcursionCalculator
       angularExcursion = new YoFrameQuaternion("angularExcursion", centerOfMassFrame, registry);
       angularExcursionRPY = new YoFrameYawPitchRoll("angularExcursion", centerOfMassFrame, registry);
       wholeBodyAngularVelocity = new YoFrameVector3D("wholeBodyAngularVelocity", centerOfMassFrame, registry);
+      wholeBodyAngularMomentum = new YoFrameVector3D("wholeBodyAngularMomentum", centerOfMassFrame, registry);
+
+      comPose = new YoFramePose3D("comPose", ReferenceFrame.getWorldFrame(), registry);
+      YoGraphicCoordinateSystem comCoordinate = new YoGraphicCoordinateSystem("comCoordinate", comPose, 1.0);
+      graphicsListRegistry.registerYoGraphic("comFrame", comCoordinate);
    }
 
    public void setToZero()
@@ -50,6 +62,7 @@ public class AngularExcursionCalculator
 
    public void compute()
    {
+      comPose.setFromReferenceFrame(centerOfMassFrame);
       angularVelocityCalculator.compute();
 
       if (zeroAngularExcursionFlag.getBooleanValue())
@@ -59,6 +72,7 @@ public class AngularExcursionCalculator
       }
 
       wholeBodyAngularVelocity.set(angularVelocityCalculator.getWholeBodyAngularVelocity());
+      wholeBodyAngularMomentum.set(angularVelocityCalculator.getAngularMomentum());
       axisAngle.setAndScale(dt, wholeBodyAngularVelocity);
       rotation.setRotationVector(axisAngle);
 
