41bf70f02b5657f3ce9bd647f242f7d43407fd49
rgriffin@ihmc.org <rgriffin@ihmc.us>
2021-09-28 18:21:21 -0500

    deleted all the things related to jumping control to move into its own repo

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/orientationControl/VariationalFunction.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.commonWalkingControlModules.orientationControl;
-
-import org.ejml.data.DMatrixRMaj;
-
-public interface VariationalFunction
-{
-   void compute(double timeInState, DMatrixRMaj PToPack, DMatrixRMaj KToPack);
-}
