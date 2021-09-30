c7588ff7753c0f5a1a3324d0deaf436fa589c72a
Sylvain Bertrand <sbertrand@ihmc.us>
2021-09-30 16:31:23 -0500

    Added squarewave function

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/functionGenerator/SinewaveFunctionGenerator.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/functionGenerator/SinewaveFunctionGenerator.java
@@ -1,129 +1,38 @@
 package us.ihmc.robotics.math.functionGenerator;
 
-import us.ihmc.yoVariables.providers.DoubleProvider;
-
-public class SinewaveFunctionGenerator
+public class SinewaveFunctionGenerator extends BaseFunctionGenerator
 {
-   public static final DoubleProvider zeroDoubleProvider = () -> 0.0;
-   private DoubleProvider offset = zeroDoubleProvider;
-   private DoubleProvider amplitude = zeroDoubleProvider;
-   private DoubleProvider frequency = zeroDoubleProvider;
-   private DoubleProvider phase = zeroDoubleProvider;
-
-   private double angle = 0.0;
-
-   private boolean outputDirty = true;
-   private double value = 0.0;
-   private double valueDot = 0.0;
-   private double valueDDot = 0.0;
-
    public SinewaveFunctionGenerator()
    {
    }
 
-   public void resetAngle()
+   @Override
+   protected double computeValue()
    {
-      angle = 0.0;
+      double offset = getOffset();
+      double amplitude = getAmplitude();
+      double angle = getAngle();
+      double phase = getPhase();
+      return offset + amplitude * Math.sin(angle + phase);
    }
 
-   public void integrateAngle(double dt)
+   @Override
+   protected double computeValueDot()
    {
-      setAngle(getAngle() + getAngleDot() * dt);
-   }
-
-   private void markDirty()
-   {
-      outputDirty = true;
-   }
-
-   private void updateOutput()
-   {
-      if (!outputDirty)
-         return;
-      outputDirty = false;
-      value = offset.getValue() + amplitude.getValue() * Math.sin(angle + phase.getValue());
       double angleDot = getAngleDot();
-      valueDot = angleDot * amplitude.getValue() * Math.cos(angle + phase.getValue());
-      valueDDot = -angleDot * angleDot * amplitude.getValue() * Math.sin(angle + phase.getValue());
+      double amplitude = getAmplitude();
+      double angle = getAngle();
+      double phase = getPhase();
+      return angleDot * amplitude * Math.cos(angle + phase);
    }
 
-   public void setAngle(double angle)
+   @Override
+   protected double computeValueDDot()
    {
-      if (angle == this.angle)
-         return;
-      this.angle = angle;
-      markDirty();
-   }
-
-   public void setOffset(double offset)
-   {
-      setOffset(() -> offset);
-   }
-
-   public void setOffset(DoubleProvider offset)
-   {
-      this.offset = offset;
-      markDirty();
-   }
-
-   public void setAmplitude(double amplitude)
-   {
-      setAmplitude(() -> amplitude);
-   }
-
-   public void setAmplitude(DoubleProvider amplitude)
-   {
-      this.amplitude = amplitude;
-      markDirty();
-   }
-
-   public void setFrequency(double frequency)
-   {
-      setFrequency(() -> frequency);
-   }
-
-   public void setFrequency(DoubleProvider frequency)
-   {
-      this.frequency = frequency;
-      markDirty();
-   }
-
-   public void setPhase(double phase)
-   {
-      setPhase(() -> phase);
-   }
-
-   public void setPhase(DoubleProvider phase)
-   {
-      this.phase = phase;
-      markDirty();
-   }
-
-   public double getAngle()
-   {
-      return angle;
-   }
-
-   public double getAngleDot()
-   {
-      return 2.0 * Math.PI * frequency.getValue();
-   }
-
-   public double getValue()
-   {
-      updateOutput();
-      return value;
-   }
-
-   public double getValueDot()
-   {
-      updateOutput();
-      return valueDot;
-   }
-
-   public double getValueDDot()
-   {
-      updateOutput();
-      return valueDDot;
+      double angleDot = getAngleDot();
+      double amplitude = getAmplitude();
+      double angle = getAngle();
+      double phase = getPhase();
+      return -angleDot * angleDot * amplitude * Math.sin(angle + phase);
    }
 }
