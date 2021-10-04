d783074ebca40c5d7e7da5aa0b3562abe7fc7dab
smccrory <smccrory@ihmc.us>
2021-10-03 20:41:01 -0500

    Adding height map ROS message

---
+++ ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/HeightMap.java
@@ -0,0 +1,181 @@
+package controller_msgs.msg.dds;
+
+import us.ihmc.communication.packets.Packet;
+import us.ihmc.euclid.interfaces.Settable;
+import us.ihmc.euclid.interfaces.EpsilonComparable;
+import java.util.function.Supplier;
+import us.ihmc.pubsub.TopicDataType;
+
+/**
+       * This message is part of the IHMC height map module
+       */
+public class HeightMap extends Packet<HeightMap> implements Settable<HeightMap>, EpsilonComparable<HeightMap>
+{
+   /**
+            * Unique ID used to identify this message, should preferably be consecutively increasing.
+            */
+   public long sequence_id_;
+   public double xy_resolution_ = -1.0;
+   public double grid_size_xy_ = -1.0;
+   public us.ihmc.idl.IDLSequence.Integer  x_cells_;
+   public us.ihmc.idl.IDLSequence.Integer  y_cells_;
+   public us.ihmc.idl.IDLSequence.Float  heights_;
+
+   public HeightMap()
+   {
+      x_cells_ = new us.ihmc.idl.IDLSequence.Integer (30000, "type_2");
+
+      y_cells_ = new us.ihmc.idl.IDLSequence.Integer (30000, "type_2");
+
+      heights_ = new us.ihmc.idl.IDLSequence.Float (30000, "type_5");
+
+   }
+
+   public HeightMap(HeightMap other)
+   {
+      this();
+      set(other);
+   }
+
+   public void set(HeightMap other)
+   {
+      sequence_id_ = other.sequence_id_;
+
+      xy_resolution_ = other.xy_resolution_;
+
+      grid_size_xy_ = other.grid_size_xy_;
+
+      x_cells_.set(other.x_cells_);
+      y_cells_.set(other.y_cells_);
+      heights_.set(other.heights_);
+   }
+
+   /**
+            * Unique ID used to identify this message, should preferably be consecutively increasing.
+            */
+   public void setSequenceId(long sequence_id)
+   {
+      sequence_id_ = sequence_id;
+   }
+   /**
+            * Unique ID used to identify this message, should preferably be consecutively increasing.
+            */
+   public long getSequenceId()
+   {
+      return sequence_id_;
+   }
+
+   public void setXyResolution(double xy_resolution)
+   {
+      xy_resolution_ = xy_resolution;
+   }
+   public double getXyResolution()
+   {
+      return xy_resolution_;
+   }
+
+   public void setGridSizeXy(double grid_size_xy)
+   {
+      grid_size_xy_ = grid_size_xy;
+   }
+   public double getGridSizeXy()
+   {
+      return grid_size_xy_;
+   }
+
+
+   public us.ihmc.idl.IDLSequence.Integer  getXCells()
+   {
+      return x_cells_;
+   }
+
+
+   public us.ihmc.idl.IDLSequence.Integer  getYCells()
+   {
+      return y_cells_;
+   }
+
+
+   public us.ihmc.idl.IDLSequence.Float  getHeights()
+   {
+      return heights_;
+   }
+
+
+   public static Supplier<HeightMapPubSubType> getPubSubType()
+   {
+      return HeightMapPubSubType::new;
+   }
+
+   @Override
+   public Supplier<TopicDataType> getPubSubTypePacket()
+   {
+      return HeightMapPubSubType::new;
+   }
+
+   @Override
+   public boolean epsilonEquals(HeightMap other, double epsilon)
+   {
+      if(other == null) return false;
+      if(other == this) return true;
+
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsPrimitive(this.sequence_id_, other.sequence_id_, epsilon)) return false;
+
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsPrimitive(this.xy_resolution_, other.xy_resolution_, epsilon)) return false;
+
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsPrimitive(this.grid_size_xy_, other.grid_size_xy_, epsilon)) return false;
+
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsIntegerSequence(this.x_cells_, other.x_cells_, epsilon)) return false;
+
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsIntegerSequence(this.y_cells_, other.y_cells_, epsilon)) return false;
+
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsFloatSequence(this.heights_, other.heights_, epsilon)) return false;
+
+
+      return true;
+   }
+
+   @Override
+   public boolean equals(Object other)
+   {
+      if(other == null) return false;
+      if(other == this) return true;
+      if(!(other instanceof HeightMap)) return false;
+
+      HeightMap otherMyClass = (HeightMap) other;
+
+      if(this.sequence_id_ != otherMyClass.sequence_id_) return false;
+
+      if(this.xy_resolution_ != otherMyClass.xy_resolution_) return false;
+
+      if(this.grid_size_xy_ != otherMyClass.grid_size_xy_) return false;
+
+      if (!this.x_cells_.equals(otherMyClass.x_cells_)) return false;
+      if (!this.y_cells_.equals(otherMyClass.y_cells_)) return false;
+      if (!this.heights_.equals(otherMyClass.heights_)) return false;
+
+      return true;
+   }
+
+   @Override
+   public java.lang.String toString()
+   {
+      StringBuilder builder = new StringBuilder();
+
+      builder.append("HeightMap {");
+      builder.append("sequence_id=");
+      builder.append(this.sequence_id_);      builder.append(", ");
+      builder.append("xy_resolution=");
+      builder.append(this.xy_resolution_);      builder.append(", ");
+      builder.append("grid_size_xy=");
+      builder.append(this.grid_size_xy_);      builder.append(", ");
+      builder.append("x_cells=");
+      builder.append(this.x_cells_);      builder.append(", ");
+      builder.append("y_cells=");
+      builder.append(this.y_cells_);      builder.append(", ");
+      builder.append("heights=");
+      builder.append(this.heights_);
+      builder.append("}");
+      return builder.toString();
+   }
+}
---
+++ ihmc-sensor-processing/src/main/java/us/ihmc/sensorProcessing/heightMap/HeightMap.java
@@ -0,0 +1,88 @@
+package us.ihmc.sensorProcessing.heightMap;
+
+import gnu.trove.list.array.TIntArrayList;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple3D.Point3D;
+
+public class HeightMap
+{
+   private final double gridResolutionXY;
+   private final int minMaxIndexXY;
+   private final HeightMapCell[][] heightMapCells;
+
+   private final HeightMapParameters parameters = new HeightMapParameters(null);
+   private final Point2D gridCenterXY = new Point2D();
+
+   private final TIntArrayList xCells = new TIntArrayList();
+   private final TIntArrayList yCells = new TIntArrayList();
+
+   public HeightMap(double gridResolutionXY, double gridSizeXY)
+   {
+      this.gridResolutionXY = gridResolutionXY;
+      minMaxIndexXY = toIndex(gridSizeXY, gridResolutionXY, 0);
+
+      int cellsPerAxis = 2 * minMaxIndexXY + 1;
+      heightMapCells = new HeightMapCell[cellsPerAxis][cellsPerAxis];
+   }
+
+   public void initialize(double xCenter, double yCenter)
+   {
+      gridCenterXY.set(xCenter, yCenter);
+   }
+
+   public void update(Point3D[] pointCloud, RigidBodyTransform transform)
+   {
+      for (int i = 0; i < pointCloud.length; i++)
+      {
+         if (pointCloud[i] != null)
+         {
+            Point3D point = new Point3D(pointCloud[i]);
+            point.applyTransform(transform);
+
+            int indexX = toIndex(point.getX() - gridCenterXY.getX(), gridResolutionXY, minMaxIndexXY);
+            int indexY = toIndex(point.getY() - gridCenterXY.getY(), gridResolutionXY, minMaxIndexXY);
+            if (indexX < 0 || indexY < 0 || indexX >= heightMapCells.length || indexY >= heightMapCells.length)
+            {
+               continue;
+            }
+
+            if (heightMapCells[indexX][indexY] == null)
+            {
+               heightMapCells[indexX][indexY] = new HeightMapCell(parameters);
+               xCells.add(indexX);
+               yCells.add(indexY);
+            }
+
+            heightMapCells[indexX][indexY].addPoint(point.getZ());
+         }
+      }
+
+      System.out.println(xCells.size() + " cells");
+   }
+
+   public double getHeightAt(int indexX, int indexY)
+   {
+      return heightMapCells[indexX][indexY].getEstimatedHeight();
+   }
+
+   public TIntArrayList getXCells()
+   {
+      return xCells;
+   }
+
+   public TIntArrayList getYCells()
+   {
+      return yCells;
+   }
+
+   public static int toIndex(double coordinate, double resolution, int offset)
+   {
+      return (int) Math.round(coordinate / resolution) + offset;
+   }
+
+   static double toCoordinate(int index, double resolution)
+   {
+      return index * resolution;
+   }
+}
