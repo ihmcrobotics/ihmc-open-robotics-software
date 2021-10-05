b4ab1d715283fac950c7e4e9a47ac16a8f891e64
Operator Computer <OperatorComputer@unknown>
2021-10-05 17:05:39 -0500

    Trying to fix eye views.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/vr/GDXVRCamera.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/vr/GDXVRCamera.java
@@ -15,6 +15,8 @@ import us.ihmc.robotics.robotSide.RobotSide;
 
 import java.util.function.Supplier;
 
+import static com.badlogic.gdx.math.Matrix4.*;
+
 /**
  * A {@link Camera} implementation for one {@link RobotSide}
  * of a {@link GDXVRContext}. All properties  will be overwritten
@@ -25,11 +27,11 @@ public class GDXVRCamera extends Camera
 {
    private final RobotSide eye;
    private final Supplier<GDXVRDevice> headsetSupplier;
-   private final Matrix4 eyeSpace = new Matrix4();
-   private final Matrix4 invEyeSpace = new Matrix4();
-   private final HmdMatrix44 projectionMat = HmdMatrix44.create();
-   private final HmdMatrix34 eyeMat = HmdMatrix34.create();
-   private final Vector3 tmp = new Vector3();
+   private final Matrix4 eyeToHeadMatrix4 = new Matrix4();
+   private final Matrix4 headToEyeMatrix4 = new Matrix4();
+   private final HmdMatrix44 projectionHmdMatrix44 = HmdMatrix44.create();
+   private final HmdMatrix34 eyeToHeadHmdMatrix34 = HmdMatrix34.create();
+   private final Vector3 target = new Vector3();
 
    private final Vector3D euclidDirection = new Vector3D();
    private final Vector3D euclidUp = new Vector3D();
@@ -51,32 +53,31 @@ public class GDXVRCamera extends Camera
    @Override
    public void update(boolean updateFrustum)
    {
-      // get the projection matrix from the HDM
-      VRSystem.VRSystem_GetProjectionMatrix(eye.ordinal(), near, far, projectionMat);
-      GDXTools.toGDX(projectionMat, projection);
+      VRSystem.VRSystem_GetProjectionMatrix(eye.ordinal(), near, far, projectionHmdMatrix44);
+      GDXTools.toGDX(projectionHmdMatrix44, projection);
 
-      // get the eye space matrix from the HDM
-      VRSystem.VRSystem_GetEyeToHeadTransform(eye.ordinal(), eyeMat);
-      GDXTools.toGDX(eyeMat, eyeSpace);
-      invEyeSpace.set(eyeSpace).inv();
+      VRSystem.VRSystem_GetEyeToHeadTransform(eye.ordinal(), eyeToHeadHmdMatrix34);
+      GDXTools.toGDX(eyeToHeadHmdMatrix34, eyeToHeadMatrix4);
+      headToEyeMatrix4.set(eyeToHeadMatrix4).inv();
 
-      // get the pose matrix from the HDM
-      GDXVRDevice hmd = headsetSupplier.get();
-      hmd.getPose().changeFrame(ReferenceFrame.getWorldFrame());
+      GDXVRDevice headset = headsetSupplier.get();
+      headset.getPose().changeFrame(ReferenceFrame.getWorldFrame());
       euclidDirection.set(Axis3D.Z);
       euclidDirection.negate(); // Z is forward in libGDX, contrary to OpenVR where it's backward
-      hmd.getPose().getOrientation().transform(euclidDirection);
+      headset.getPose().getOrientation().transform(euclidDirection);
       euclidUp.set(Axis3D.Y); // camera is rendered in Y up
-      hmd.getPose().getOrientation().transform(euclidUp);
+      headset.getPose().getOrientation().transform(euclidUp);
 
-      position.set(hmd.getPose().getPosition().getX32(), hmd.getPose().getPosition().getY32(), hmd.getPose().getPosition().getZ32());
+      GDXTools.toGDX(headset.getPose().getPosition(), position);
+//      position.add(eyeToHeadMatrix4.val[M03], eyeToHeadMatrix4.val[M13], eyeToHeadMatrix4.val[M23]);
       direction.set(euclidDirection.getX32(), euclidDirection.getY32(), euclidDirection.getZ32());
       up.set(euclidUp.getX32(), euclidUp.getY32(), euclidUp.getZ32());
 
-      view.setToLookAt(position, tmp.set(position).add(direction), up);
+      target.set(position).add(direction);
+      view.setToLookAt(position, target, up);
 
       combined.set(projection);
-      Matrix4.mul(combined.val, invEyeSpace.val);
+      Matrix4.mul(combined.val, headToEyeMatrix4.val);
       Matrix4.mul(combined.val, view.val);
 
       if (updateFrustum)
