33fc9b24f1274dd81d025d2009cb3670d6dd5d3d
Duncan Calvert <dcalvert@ihmc.us>
2021-10-09 19:27:33 -0500

    Switch to OpenGL 4.1 which is latest supported on MacOS.

--- ihmc-graphics/src/libgdx/resources/us/ihmc/gdx/GDXPointCloudRenderer.glsl
+++ ihmc-graphics/src/libgdx/resources/us/ihmc/gdx/GDXPointCloudRenderer.glsl
@@ -1,5 +1,5 @@
 #type vertex
-#version 450
+#version 410
 
 layout(location = 0) in vec3 a_position;
 layout(location = 1) in vec4 a_color;
@@ -49,7 +49,7 @@ void main()
 }
 
 #type fragment
-#version 450
+#version 410
 
 in vec4 v_color;
 
