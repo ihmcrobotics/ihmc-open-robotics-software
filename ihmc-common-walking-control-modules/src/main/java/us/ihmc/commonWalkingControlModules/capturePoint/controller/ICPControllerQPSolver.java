98467af2804a84e924d3c7637e35af84a985cf9a
nadiaocu <nadiaocu@NADIA-OCU>
2021-10-13 11:21:35 -0500

    Fix bad conditions on finite.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/ICPControllerQPSolver.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/ICPControllerQPSolver.java
@@ -791,7 +791,7 @@ public class ICPControllerQPSolver
     */
    private void addMaximumFeedbackRateConstraint()
    {
-      if (!Double.isFinite(maxFeedbackXMagnitude) && !Double.isFinite(maxFeedbackYMagnitude))
+      if (!Double.isFinite(maximumFeedbackRate))
          return;
 
       ICPControllerQPConstraintCalculator.calculateMaxFeedbackRateConstraint(feedbackRateLimitConstraint,
