ae7dd09950d7388797efd694d0c7c2f1dedcdb3a
VR Station <VRStation@unknown>
2021-10-15 10:58:58 -0500

    Get minimal VR demo working.

--- ihmc-graphics/src/libgdx-test/java/us/ihmc/gdx/GDXVROnlyDemo.java
+++ ihmc-graphics/src/libgdx-test/java/us/ihmc/gdx/GDXVROnlyDemo.java
@@ -8,6 +8,7 @@ import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.system.MemoryUtil;
 import us.ihmc.gdx.sceneManager.GDX3DSceneBasics;
+import us.ihmc.gdx.sceneManager.GDXSceneLevel;
 import us.ihmc.gdx.tools.GDXTools;
 import us.ihmc.gdx.vr.GDXVRContext;
 import us.ihmc.gdx.vr.GDXVRControllerButtons;
@@ -44,6 +45,7 @@ public class GDXVROnlyDemo
       Gdx.gl20 = Gdx.gl30;
       Gdx.files = new Lwjgl3Files();
       Gdx.graphics = new MinimalGDXGraphics();
+      Gdx.app = new MinimalGDXApplication();
 
       sceneBasics = new GDX3DSceneBasics();
       sceneBasics.create();
@@ -52,12 +54,22 @@ public class GDXVROnlyDemo
       vrContext = new GDXVRContext();
       vrContext.initSystem();
       vrContext.setupEyes();
+      sceneBasics.addRenderableProvider(((renderables, pool) ->
+      {
+         for (RobotSide side : RobotSide.values)
+         {
+            vrContext.getController(side, controller -> controller.getModelInstance().getRenderables(renderables, pool));
+            vrContext.getEyes().get(side).getCoordinateFrameInstance().getRenderables(renderables, pool);
+         }
+         vrContext.getBaseStations(baseStation -> baseStation.getModelInstance().getRenderables(renderables, pool));
+         vrContext.getGenericDevices(genericDevice -> genericDevice.getModelInstance().getRenderables(renderables, pool));
+      }), GDXSceneLevel.VIRTUAL);
 
       vrContext.addVRInputProcessor(vrContext ->
       {
          vrContext.getController(RobotSide.RIGHT, controller ->
          {
-            if (controller.isButtonNewlyPressed(GDXVRControllerButtons.A))
+            if (controller.isButtonNewlyPressed(GDXVRControllerButtons.INDEX_A))
             {
                running = false;
             }
