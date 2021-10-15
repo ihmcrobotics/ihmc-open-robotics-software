975d6ce9811a216ca416a6e5d79d513a154b1517
Duncan Calvert <dcalvert@ihmc.org>
2021-10-15 00:16:27 -0500

    Update GDXVROnlyDemo. Need to figure out how to make a libGDX OpenGL context without a window?

--- ihmc-graphics/src/libgdx-test/java/us/ihmc/gdx/GDXVROnlyDemo.java
+++ ihmc-graphics/src/libgdx-test/java/us/ihmc/gdx/GDXVROnlyDemo.java
@@ -1,14 +1,57 @@
 package us.ihmc.gdx;
 
-import us.ihmc.gdx.vr.GDXVRManager;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3NativesLoader;
+import org.lwjgl.opengl.GL;
+import us.ihmc.gdx.sceneManager.GDX3DSceneBasics;
+import us.ihmc.gdx.vr.GDXVRContext;
+import us.ihmc.gdx.vr.GDXVRControllerButtons;
+import us.ihmc.robotics.robotSide.RobotSide;
 
 public class GDXVROnlyDemo
 {
-   private final GDXVRManager vrManager;
+   private final GDXVRContext vrContext;
+   private final GDX3DSceneBasics sceneBasics;
+   private volatile boolean running = true;
 
    public GDXVROnlyDemo()
    {
-      vrManager = new GDXVRManager();
+      Lwjgl3NativesLoader.load();
+//      GL.createCapabilities();
+
+//      Gdx.graphics = new Lwjgl3Graphics(this);
+//      Gdx.gl30 = Gdx.graphics.getGL30();
+//      Gdx.gl20 = Gdx.gl30 != null ? Gdx.gl30 : Gdx.graphics.getGL20();
+//      Gdx.gl = Gdx.gl30 != null ? Gdx.gl30 : Gdx.gl20;
+
+
+      // TODO: Create an OpenGL context without GLFW?
+
+      sceneBasics = new GDX3DSceneBasics();
+      sceneBasics.addCoordinateFrame(1.0);
+
+      vrContext = new GDXVRContext();
+      vrContext.initSystem();
+      vrContext.setupEyes();
+
+      vrContext.addVRInputProcessor(vrContext ->
+      {
+         vrContext.getController(RobotSide.RIGHT, controller ->
+         {
+            if (controller.isButtonNewlyPressed(GDXVRControllerButtons.A))
+            {
+               running = false;
+            }
+         });
+      });
+
+      while (running)
+      {
+         vrContext.waitGetPoses();
+         vrContext.pollEvents();
+         vrContext.renderEyes(sceneBasics);
+      }
    }
 
    public static void main(String[] args)
