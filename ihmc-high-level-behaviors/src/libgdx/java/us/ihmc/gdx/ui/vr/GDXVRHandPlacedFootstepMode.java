dd7d02b12f30ec102bf5aba1bf19f8b77dd6b85c
OperatorComputer <OperatorComputer@ihmc.us>
2021-10-21 17:50:17 -0500

    Comment stuff

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/vr/GDXVRHandPlacedFootstepMode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/vr/GDXVRHandPlacedFootstepMode.java
@@ -1,9 +1,6 @@
 package us.ihmc.gdx.ui.vr;
 
-import org.lwjgl.openvr.VR;
-import org.lwjgl.openvr.VRControllerAxis;
-import org.lwjgl.openvr.VRControllerState;
-import org.lwjgl.openvr.VREvent;
+import org.lwjgl.openvr.*;
 import us.ihmc.gdx.vr.GDXVRContext;
 import us.ihmc.log.LogTools;
 import us.ihmc.robotics.robotSide.RobotSide;
@@ -18,34 +15,34 @@ public class GDXVRHandPlacedFootstepMode
       {
          vrContext.getController(side, controller ->
          {
-            VRControllerState controllerState = controller.getControllerState();
-            VRControllerAxis vrControllerAxis = controllerState.rAxis(2);
-            LogTools.info("axis: {} {}", vrControllerAxis.x(), vrControllerAxis.y());
-//            LogTools.info("button pressed {}", controllerState.ulButtonPressed());
-//            LogTools.info("button touched {}", controllerState.ulButtonTouched());
-
-
-
-
-
-
-            //            controller.isButtonNewlyPressed(GDXVRControllerAxes.SteamVR_Trigger)
-            HashMap<Integer, VREvent> events = vrContext.getDeviceIndexToEventsThisFrameMap().get(controller.getDeviceIndex());
-
-            if (events != null)
-            {
-               VREvent buttonPress = events.get(VR.EVREventType_VREvent_ButtonPress);
-               if (buttonPress != null)
-               {
-                  LogTools.info("{} button press {}", side, buttonPress.data().controller().button());
-               }
-               VREvent dualAnalogPress = events.get(VR.EVREventType_VREvent_DualAnalog_Press);
-               if (dualAnalogPress != null)
-               {
-                  LogTools.info("{} dual analog {}, {}", side, dualAnalogPress.data().dualAnalog().x(),
-                                dualAnalogPress.data().dualAnalog().y());
-               }
-            }
+//            VRControllerState controllerState = controller.getControllerState();
+//            VRControllerAxis vrControllerAxis = controllerState.rAxis(2);
+//            LogTools.info("axis: {} {}", vrControllerAxis.x(), vrControllerAxis.y());
+////            LogTools.info("button pressed {}", controllerState.ulButtonPressed());
+////            LogTools.info("button touched {}", controllerState.ulButtonTouched());
+//
+//
+//
+//
+//
+//
+//            //            controller.isButtonNewlyPressed(GDXVRControllerAxes.SteamVR_Trigger)
+//            HashMap<Integer, VREvent> events = vrContext.getDeviceIndexToEventsThisFrameMap().get(controller.getDeviceIndex());
+//
+//            if (events != null)
+//            {
+//               VREvent buttonPress = events.get(VR.EVREventType_VREvent_ButtonPress);
+//               if (buttonPress != null)
+//               {
+//                  LogTools.info("{} button press {}", side, buttonPress.data().controller().button());
+//               }
+//               VREvent dualAnalogPress = events.get(VR.EVREventType_VREvent_DualAnalog_Press);
+//               if (dualAnalogPress != null)
+//               {
+//                  LogTools.info("{} dual analog {}, {}", side, dualAnalogPress.data().dualAnalog().x(),
+//                                dualAnalogPress.data().dualAnalog().y());
+//               }
+//            }
          });
       }
 
