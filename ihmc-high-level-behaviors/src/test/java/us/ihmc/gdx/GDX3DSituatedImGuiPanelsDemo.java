5b163eff6db043aa2ddc2add766023e8770f1b36
Duncan Calvert <dcalvert@ihmc.org>
2021-11-12 23:35:18 -0600

    Start messing with 3D situated ImGui panels.

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDX3DSituatedImGuiPanelsDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/GDX3DSituatedImGuiPanelsDemo.java
@@ -1,6 +1,10 @@
 package us.ihmc.gdx;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
 import imgui.ImGui;
+import imgui.gl3.ImGuiImplGl3;
+import imgui.glfw.ImGuiImplGlfw;
 import us.ihmc.gdx.sceneManager.GDX3DSceneManager;
 import us.ihmc.gdx.sceneManager.GDX3DSceneTools;
 import us.ihmc.gdx.tools.BoxesDemoModel;
@@ -8,9 +12,13 @@ import us.ihmc.gdx.tools.GDXApplicationCreator;
 
 public class GDX3DSituatedImGuiPanelsDemo
 {
+   private final GDX3DSceneManager sceneManager = new GDX3DSceneManager();
+   private final ImGuiImplGlfw imGuiGlfw = new ImGuiImplGlfw();
+   private final ImGuiImplGl3 imGuiGl3 = new ImGuiImplGl3();
+   private final GDX3DSituatedImGuiPanelManager situatedImGuiPanelManager = new GDX3DSituatedImGuiPanelManager();
+
    public GDX3DSituatedImGuiPanelsDemo()
    {
-      GDX3DSceneManager sceneManager = new GDX3DSceneManager();
       GDXApplicationCreator.launchGDXApplication(new Lwjgl3ApplicationAdapter()
       {
          @Override
@@ -18,20 +26,20 @@ public class GDX3DSituatedImGuiPanelsDemo
          {
             sceneManager.create();
 
+            imGuiGlfw.init(((Lwjgl3Graphics) Gdx.graphics).getWindow().getWindowHandle(), true);
+            imGuiGl3.init();
+
+            situatedImGuiPanelManager.create(imGuiGlfw, imGuiGl3);
+            GDX3DSituatedImGuiPanel panel = new GDX3DSituatedImGuiPanel("Test Panel", () ->
+            {
+               ImGui.text("This is a 3D situated panel.");
+               ImGui.button("Button");
+            });
+            situatedImGuiPanelManager.addPanel(panel);
+            sceneManager.addRenderableProvider(situatedImGuiPanelManager);
+
             sceneManager.addCoordinateFrame(0.3);
             sceneManager.addModelInstance(new BoxesDemoModel().newInstance());
-
-            new GDX3DSituatedImGuiPanel("Test Window")
-            {
-               @Override
-               public void renderImGuiWidgets()
-               {
-                  ImGui.text("this is a test");
-                  ImGui.button("this is a button");
-               }
-            };
-
-            sceneManager.addRenderableProvider(GDX3DSituatedImGuiPanelManager.getInstance());
          }
 
          @Override
@@ -40,9 +48,18 @@ public class GDX3DSituatedImGuiPanelsDemo
             GDX3DSceneTools.glClearGray();
             sceneManager.setViewportBoundsToWindow();
 
-            GDX3DSituatedImGuiPanelManager.getInstance().update();
+            situatedImGuiPanelManager.update();
 
             sceneManager.render();
+         }
+
+         @Override
+         public void dispose()
+         {
+            sceneManager.dispose();
+            situatedImGuiPanelManager.dispose();
+            imGuiGl3.dispose();
+            imGuiGlfw.dispose();
          }
       }, "GDX3DDemo", 1100, 800);
    }
