16e1929235ac29181a2c3da2e3a95f7277320ed4
Duncan Calvert <dcalvert@ihmc.org>
2021-12-06 00:03:16 -0600

    Fix teleportation so headset is where you are trying to go.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/vr/GDXVRHeadset.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/vr/GDXVRHeadset.java
@@ -3,6 +3,11 @@ package us.ihmc.gdx.vr;
 import com.badlogic.gdx.utils.BufferUtils;
 import org.lwjgl.openvr.*;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.yawPitchRoll.YawPitchRoll;
 
 import java.nio.LongBuffer;
 import java.util.function.Consumer;
@@ -10,11 +15,31 @@ import java.util.function.Consumer;
 public class GDXVRHeadset extends GDXVRTrackedDevice
 {
    private final LongBuffer inputSourceHandle = BufferUtils.newLongBuffer(1);
+   private final RigidBodyTransform headsetToWorldTransform = new RigidBodyTransform();
+   private final ReferenceFrame xRightZDownHeadsetFrame;
+   private static final RigidBodyTransformReadOnly headsetXRightZDownToXForwardZUp = new RigidBodyTransform(
+         new YawPitchRoll(          // For this transformation, we start with IHMC ZUp with index forward and thumb up
+                                    Math.toRadians(90.0),  // rotating around thumb, index goes forward to right
+                                    Math.toRadians(135.0),    // no rotation about middle finger
+                                    Math.toRadians(0.0)   // rotating about index finger, thumb goes up to toward you then down
+         ),
+         new Point3D()
+   );
+   private final ReferenceFrame xForwardZUpHeadsetFrame;
 
    public GDXVRHeadset(ReferenceFrame vrPlayAreaYUpZBackFrame)
    {
       super(vrPlayAreaYUpZBackFrame);
       setDeviceIndex(VR.k_unTrackedDeviceIndex_Hmd);
+
+      xRightZDownHeadsetFrame
+            = ReferenceFrameTools.constructFrameWithChangingTransformToParent("xRightZDownHeadsetFrame",
+                                                                              ReferenceFrame.getWorldFrame(),
+                                                                              headsetToWorldTransform);
+      xForwardZUpHeadsetFrame
+            = ReferenceFrameTools.constructFrameWithUnchangingTransformToParent("xForwardZUpHeadsetFrame",
+                                                                                xRightZDownHeadsetFrame,
+                                                                                headsetXRightZDownToXForwardZUp);
    }
 
    public void initSystem()
@@ -27,6 +52,9 @@ public class GDXVRHeadset extends GDXVRTrackedDevice
       setConnected(VRSystem.VRSystem_IsTrackedDeviceConnected(VR.k_unTrackedDeviceIndex_Hmd));
 
       super.update(trackedDevicePoses);
+
+      getPose().get(headsetToWorldTransform);
+      xRightZDownHeadsetFrame.update();
    }
 
    public void runIfConnected(Consumer<GDXVRHeadset> runIfConnected)
@@ -35,5 +63,10 @@ public class GDXVRHeadset extends GDXVRTrackedDevice
       {
          runIfConnected.accept(this);
       }
+   }
+
+   public ReferenceFrame getXForwardZUpHeadsetFrame()
+   {
+      return xForwardZUpHeadsetFrame;
    }
 }
