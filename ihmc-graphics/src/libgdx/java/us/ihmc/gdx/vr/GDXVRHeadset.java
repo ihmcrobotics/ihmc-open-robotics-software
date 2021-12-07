ad7ecbb9363927bd67e9b838c9b9d2189b9e9eba
OperatorComputer <OperatorComputer@ihmc.us>
2021-12-07 13:46:17 -0600

    Fix headset frame.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/vr/GDXVRHeadset.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/vr/GDXVRHeadset.java
@@ -18,10 +18,10 @@ public class GDXVRHeadset extends GDXVRTrackedDevice
    private final RigidBodyTransform headsetToWorldTransform = new RigidBodyTransform();
    private final ReferenceFrame xRightZDownHeadsetFrame;
    private static final RigidBodyTransformReadOnly headsetXRightZDownToXForwardZUp = new RigidBodyTransform(
-         new YawPitchRoll(          // For this transformation, we start with IHMC ZUp with index forward and thumb up
-                                    Math.toRadians(90.0),  // rotating around thumb, index goes forward to right
-                                    Math.toRadians(135.0),    // no rotation about middle finger
-                                    Math.toRadians(0.0)   // rotating about index finger, thumb goes up to toward you then down
+         new YawPitchRoll(         // For this transformation, we start with IHMC ZUp with index forward and thumb up
+            Math.toRadians(90.0),  // rotating around thumb, index goes forward to left
+            Math.toRadians(90.0),  // rotating about middle finger, index goes left to down
+            Math.toRadians(0.0)    // no rotation about index finger
          ),
          new Point3D()
    );
