3525dbe1dab495200cc9aed1923f5459a8b73f5d
Duncan Calvert <dcalvert@ihmc.us>
2021-12-20 20:24:35 -0600

    :arrow_up: ihmc-ci 7.5 JUnit 5.8.2

--- ihmc-high-level-behaviors/build.gradle.kts
+++ ihmc-high-level-behaviors/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
    id("us.ihmc.ihmc-build")
-   id("us.ihmc.ihmc-ci") version "7.4"
+   id("us.ihmc.ihmc-ci") version "7.5"
    id("us.ihmc.ihmc-cd") version "1.21"
    id("us.ihmc.log-tools-plugin") version "0.6.3"
 }
