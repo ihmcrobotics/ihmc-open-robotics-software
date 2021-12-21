ff1f72b747f99bee251e4d04e0fd6e561719905b
Duncan Calvert <dcalvert@ihmc.us>
2021-12-21 00:07:08 -0600

    :arrow_up: commons-math 3.6.1

--- ihmc-parameter-tuner/build.gradle.kts
+++ ihmc-parameter-tuner/build.gradle.kts
@@ -14,7 +14,7 @@ ihmc {
 
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
-   api("org.apache.commons:commons-math3:3.3")
+   api("org.apache.commons:commons-math3:3.6.1")
    api("jakarta.xml.bind:jakarta.xml.bind-api:2.3.2")
    api("org.glassfish.jaxb:jaxb-runtime:2.3.2")
 
