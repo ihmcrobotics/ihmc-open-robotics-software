db2f12b45bcc8d169c2cd48e3426f7c5311380f7
Duncan Calvert <dcalvert@ihmc.us>
2021-12-23 14:06:51 -0600

    Set up base demo GPU planar region extraction.

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/perception/GDXGPUPlanarRegionExtractionDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/perception/GDXGPUPlanarRegionExtractionDemo.java
@@ -1,13 +1,12 @@
 package us.ihmc.gdx.perception;
 
-import com.github.swrirobotics.bags.reader.BagFile;
-import com.github.swrirobotics.bags.reader.BagReader;
-import com.github.swrirobotics.bags.reader.exceptions.BagReaderException;
+import boofcv.struct.calib.CameraPinholeBrown;
 import us.ihmc.gdx.Lwjgl3ApplicationAdapter;
+import us.ihmc.gdx.sceneManager.GDXSceneLevel;
+import us.ihmc.gdx.simulation.environment.GDXEnvironment;
+import us.ihmc.gdx.simulation.sensors.GDXHighLevelDepthSensorSimulator;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
-
-import java.io.File;
-import java.nio.file.Paths;
+import us.ihmc.gdx.ui.gizmo.GDXPose3DGizmo;
 
 public class GDXGPUPlanarRegionExtractionDemo
 {
@@ -15,6 +14,9 @@ public class GDXGPUPlanarRegionExtractionDemo
                                                               "ihmc-open-robotics-software",
                                                               "ihmc-high-level-behaviors/src/test/resources");
 
+   private GDXHighLevelDepthSensorSimulator l515;
+   private final GDXPose3DGizmo l515PoseGizmo = new GDXPose3DGizmo();
+   private GDXEnvironment environmentBuilder;
 
    public GDXGPUPlanarRegionExtractionDemo()
    {
@@ -25,19 +27,68 @@ public class GDXGPUPlanarRegionExtractionDemo
          {
             baseUI.create();
 
-            try
-            {
-               BagFile rosBag = BagReader.readFile(new File("meep"));
-            }
-            catch (BagReaderException e)
-            {
-               e.printStackTrace();
-            }
+            environmentBuilder = new GDXEnvironment(baseUI.get3DSceneManager());
+            environmentBuilder.create(baseUI);
+            baseUI.getImGuiPanelManager().addPanel(environmentBuilder.getPanelName(), environmentBuilder::renderImGuiWidgets);
+            baseUI.get3DSceneManager().addRenderableProvider(environmentBuilder::getRealRenderables, GDXSceneLevel.REAL_ENVIRONMENT);
+            baseUI.get3DSceneManager().addRenderableProvider(environmentBuilder::getVirtualRenderables, GDXSceneLevel.VIRTUAL);
+            environmentBuilder.loadEnvironment("DemoPullDoor.json");
+
+            l515PoseGizmo.create(baseUI.get3DSceneManager().getCamera3D());
+            l515PoseGizmo.setResizeAutomatically(false);
+            baseUI.addImGui3DViewInputProcessor(l515PoseGizmo::process3DViewInput);
+            baseUI.get3DSceneManager().addRenderableProvider(l515PoseGizmo, GDXSceneLevel.VIRTUAL);
+            l515PoseGizmo.getTransformToParent().appendTranslation(0.0, 0.0, 0.5);
+            l515PoseGizmo.getTransformToParent().appendPitchRotation(Math.PI / 6.0);
+
+            double publishRateHz = 5.0;
+            double verticalFOV = 55.0;
+            int imageWidth = 640;
+            int imageHeight = 480;
+            double fx = 500.0;
+            double fy = 500.0;
+            double minRange = 0.105;
+            double maxRange = 5.0;
+            CameraPinholeBrown depthCameraIntrinsics = new CameraPinholeBrown(fx, fy, 0, imageWidth / 2.0, imageHeight / 2.0, imageWidth, imageHeight);
+            l515 = new GDXHighLevelDepthSensorSimulator("Stepping L515",
+                                                        null,
+                                                        null,
+                                                        null,
+                                                        depthCameraIntrinsics,
+                                                        null,
+                                                        null,
+                                                        null,
+                                                        null,
+                                                        null,
+                                                        l515PoseGizmo.getGizmoFrame(),
+                                                        () -> 0L,
+                                                        verticalFOV,
+                                                        imageWidth,
+                                                        imageHeight,
+                                                        minRange,
+                                                        maxRange,
+                                                        publishRateHz,
+                                                        false);
+            baseUI.getImGuiPanelManager().addPanel(l515);
+            l515.setSensorEnabled(true);
+            l515.setPublishPointCloudROS2(false);
+            l515.setRenderPointCloudDirectly(false);
+            l515.setPublishDepthImageROS1(false);
+            l515.setDebugCoordinateFrame(false);
+            l515.setRenderColorVideoDirectly(true);
+            l515.setRenderDepthVideoDirectly(true);
+            l515.setPublishColorImageROS1(false);
+            l515.setPublishColorImageROS2(false);
+            l515.create();
+
+            baseUI.get3DSceneManager().addRenderableProvider(l515, GDXSceneLevel.VIRTUAL);
+
          }
 
          @Override
          public void render()
          {
+            l515.render(baseUI.get3DSceneManager());
             baseUI.renderBeforeOnScreenUI();
             baseUI.renderEnd();
          }
@@ -46,6 +97,8 @@ public class GDXGPUPlanarRegionExtractionDemo
          public void dispose()
          {
             baseUI.dispose();
+            environmentBuilder.destroy();
+            l515.dispose();
          }
       });
    }
