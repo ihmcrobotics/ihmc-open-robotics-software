7716cdb64ff46a6c1d73e7fcec3258daab502310
Duncan Calvert <dcalvert@ihmc.us>
2022-01-02 00:38:52 -0600

    Clean up stuff that didn't work and debug stuff.

--- ihmc-perception/src/test/java/us/ihmc/perception/OpenCLManagerTest.java
+++ ihmc-perception/src/test/java/us/ihmc/perception/OpenCLManagerTest.java
@@ -73,7 +73,6 @@ public class OpenCLManagerTest
       BytePointer hostMemoryPointer = new BytePointer(backingDirectByteBuffer);
       Mat openCVMat = new Mat(imageHeight, imageWidth, cvMatType, hostMemoryPointer);
 
-//      hostMemoryPointer = null;
       _cl_mem image = openCLManager.createImage(OpenCL.CL_MEM_READ_WRITE,
                                                 OpenCL.CL_R,
                                                 OpenCL.CL_FLOAT,
@@ -92,7 +91,7 @@ public class OpenCLManagerTest
       {
          for (int y = 0; y < imageHeight; y++)
          {
-            System.out.println("hostMemoryPointer[" + x + "] : " + openCVMat.ptr(y, x).getFloat());
+            System.out.println("image[" + x + ", " + y + "] : " + openCVMat.ptr(y, x).getFloat());
          }
       }
       openCLManager.destroy();
@@ -106,7 +105,6 @@ public class OpenCLManagerTest
       _cl_platform_id platforms = new _cl_platform_id();
       _cl_device_id devices = new _cl_device_id();
       _cl_context context;
-      _cl_command_queue commandQueue = new _cl_command_queue();
       IntPointer numberOfDevices = new IntPointer(1);
       IntPointer numberOfPlatforms = new IntPointer(3);
       IntPointer returnCode = new IntPointer(1);
@@ -118,43 +116,13 @@ public class OpenCLManagerTest
       int flags = OpenCL.CL_MEM_READ_WRITE;
       int imageChannelOrder = CL_R;
       int imageChannelDataType = CL_UNSIGNED_INT16;
-//      cl_image_format imageFormat = new cl_image_format(new IntPointer(imageChannelOrder, imageChannelDataType));
       cl_image_format imageFormat = new cl_image_format();
       imageFormat.image_channel_order(imageChannelOrder);
       imageFormat.image_channel_data_type(imageChannelDataType);
-      int rowPitch = 0;
-      int imageDepth = 0;
-      int imageArraySize = 0; // image array size
-      int imageSlicePitch = 0;
-      int numberOfMipmapLevels = 0;
-      int numberOfMipmapSamples = 0;
-      int memoryObject = 0;
-//      cl_image_desc imageDescription = new cl_image_desc(new IntPointer(
-//            CL_MEM_OBJECT_IMAGE2D,
-//            imageWidth,
-//            imageHeight,
-//            imageDepth,
-//            imageArraySize,
-//            rowPitch,
-//            imageSlicePitch,
-//            numberOfMipmapLevels,
-//            numberOfMipmapSamples,
-//            memoryObject
-//      ));
       cl_image_desc imageDescription = new cl_image_desc();
       imageDescription.image_type(CL_MEM_OBJECT_IMAGE2D);
       imageDescription.image_width(imageWidth);
       imageDescription.image_height(imageHeight);
-//      imageDescription.position(0).put(new cl_mem);
-//      imageDescription.position(1).put(new IntPointer(new int[] {width}));
-//      imageDescription.position(2).put(new IntPointer(new int[] {height}));
-//      imageDescription.position(3).put(new IntPointer(new int[] {0})); // depth
-//      imageDescription.position(4).put(new IntPointer(new int[] {0})); // arraySize
-//      imageDescription.position(5).put(new IntPointer(new int[] {rowPitch}));
-//      imageDescription.position(6).put(new IntPointer(new int[] {0})); // slicePitch
-//      imageDescription.position(7).put(new IntPointer(new int[] {0})); // number of mipmap levels
-//      imageDescription.position(8).put(new IntPointer(new int[] {0})); // number of samples
-//      imageDescription.position(9).put(new IntPointer(new int[] {0})); // mem_object
       Pointer hostPointer = null;
       _cl_mem image = clCreateImage(context, flags, imageFormat, imageDescription, hostPointer, returnCode);
       System.out.println("code: " + returnCode.get() + " image: " + image);
--- ihmc-perception/src/test/resources/openCL/ManipulateImage.cl
+++ ihmc-perception/src/test/resources/openCL/ManipulateImage.cl
@@ -3,5 +3,8 @@ kernel void manipulateImage(read_write image2d_t image)
    int x = get_global_id(0);
    int y = get_global_id(1);
 
-   write_imagef(image, (int2) (x, y), (float4) (x, 0.0, 0.0, 1.0));
+   if (y == 1)
+      write_imagef(image, (int2) (x, y), (float4) (5.0, 0.0, 0.0, 1.0));
+   else
+      write_imagef(image, (int2) (x, y), (float4) (x, 0.0, 0.0, 1.0));
 }
