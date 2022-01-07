97cc13c3aa1a294d03b37a7a13365eb9b0a7083e
jpratt <jerry.pratt@boardwalkrobotics.com>
2022-01-07 11:01:32 -0600

    Made getFastStepWidth a method so it can be overwritten.
    But default it to what it was.

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/AvatarFlatGroundFastWalkingTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/AvatarFlatGroundFastWalkingTest.java
@@ -65,6 +65,11 @@ public abstract class AvatarFlatGroundFastWalkingTest implements MultiRobotTestI
 
    public abstract double getMaxForwardStepLength();
 
+   public double getFastStepWidth()
+   {
+      return 0.25;
+   }
+
    @Test
    public void testForwardWalking() throws Exception
    {
@@ -78,15 +83,16 @@ public abstract class AvatarFlatGroundFastWalkingTest implements MultiRobotTestI
       FootstepDataListMessage footsteps = forwardSteps(RobotSide.LEFT,
                                                        30,
                                                        trapezoidFunction(0.2, getMaxForwardStepLength(), 0.15, 0.85),
-                                                       0.25,
+                                                       getFastStepWidth(),
                                                        getFastSwingTime(),
                                                        getFastTransferTime(),
                                                        startPose,
                                                        true);
       footsteps.setOffsetFootstepsHeightWithExecutionError(true);
       drcSimulationTestHelper.publishToController(footsteps);
-      assertTrue(drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(1.1
-            * EndToEndTestTools.computeWalkingDuration(footsteps, getRobotModel().getWalkingControllerParameters())));
+
+      boolean success = drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(1.1 * EndToEndTestTools.computeWalkingDuration(footsteps, getRobotModel().getWalkingControllerParameters()));
+      assertTrue(success);
    }
 
    private void setupSim(DRCRobotModel robotModel, boolean useVelocityAndHeadingScript, boolean cheatWithGroundHeightAtForFootstep,
