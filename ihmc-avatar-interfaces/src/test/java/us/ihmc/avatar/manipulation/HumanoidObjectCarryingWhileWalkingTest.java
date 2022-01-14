57ab1c2d911376d678f7193b0ff02e89e91b3def
Sylvain Bertrand <sbertrand@ihmc.org>
2022-01-14 11:55:20 -0600

    End-to-end test of val carrying a pendulum.

---
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/manipulation/HumanoidObjectCarryingWhileWalkingTest.java
@@ -0,0 +1,14 @@
+package us.ihmc.avatar.manipulation;
+
+import org.junit.jupiter.api.Test;
+
+import us.ihmc.avatar.MultiRobotTestInterface;
+
+public abstract class HumanoidObjectCarryingWhileWalkingTest implements MultiRobotTestInterface
+{
+   @Test
+   public void testInPlaceWalking()
+   {
+      
+   }
+}
