23da47f506c25a61384e49993f6154647645aeb1
Duncan Calvert <dcalvert@ihmc.us>
2022-01-16 16:30:39 -0600

    Remove LWJGL OpenCL dependency that's not used.

--- ihmc-high-level-behaviors/build.gradle.kts
+++ ihmc-high-level-behaviors/build.gradle.kts
@@ -31,7 +31,6 @@ javafxDependencies {
 
 libgdxDependencies {
    api(ihmc.sourceSetProject("main"))
-   api("org.lwjgl:lwjgl-opencl:3.2.3")
    api("org.abego.treelayout:org.abego.treelayout.core:1.0.3")
    api("us.ihmc:ihmc-graphics-libgdx:source")
    api("us.ihmc:ihmc-perception-javacv:source")
