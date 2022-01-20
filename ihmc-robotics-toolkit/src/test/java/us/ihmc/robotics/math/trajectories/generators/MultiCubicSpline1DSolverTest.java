c38824393d89bb161bbed165ee80c3246d32d348
Sylvain Bertrand <sbertrand@ihmc.org>
2022-01-20 15:06:14 -0600

    Formatting and added method to compute the velocity at a waypoint.

--- ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/math/trajectories/generators/MultiCubicSpline1DSolverTest.java
+++ ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/math/trajectories/generators/MultiCubicSpline1DSolverTest.java
@@ -1,14 +1,24 @@
 package us.ihmc.robotics.math.trajectories.generators;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static us.ihmc.robotics.math.trajectories.generators.MultiCubicSpline1DSolver.coefficients;
+
+import java.util.Random;
+
 import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.CommonOps_DDRM;
 import org.junit.jupiter.api.Test;
+
+import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.robotics.Assert;
 
 public class MultiCubicSpline1DSolverTest
 {
    private final double permissableError = 1e-3;
+
    @Test
-   public void testNotUsingNativeCommonOps() {
+   public void testNotUsingNativeCommonOps()
+   {
       DMatrixRMaj mat = new DMatrixRMaj(1, 1);
       MultiCubicSpline1DSolver solverUsingNative = new MultiCubicSpline1DSolver(true);
       solverUsingNative.setEndpoints(0, 1, 0, 1);
@@ -25,5 +35,66 @@ public class MultiCubicSpline1DSolverTest
 
       double percentDiff = 100 * Math.abs(costUsingNative - costNotUsingNative) / costNotUsingNative;
       Assert.assertTrue(percentDiff < permissableError);
+   }
+
+   @Test
+   public void testGetWaypointVelocityFromSolution()
+   {
+      Random random = new Random(45435);
+      DMatrixRMaj solution = new DMatrixRMaj(2, 1);
+      MultiCubicSpline1DSolver solver = new MultiCubicSpline1DSolver();
+
+      for (int i = 0; i < 100; i++)
+      {
+
+         int numberOfWaypoints = random.nextInt(10) + 1;
+
+         double startPosition = EuclidCoreRandomTools.nextDouble(random);
+         double endPosition = EuclidCoreRandomTools.nextDouble(random);
+         double[] waypointPositions = random.doubles(numberOfWaypoints, -0.5, 10.0).toArray();
+         double[] waypointTimes = new double[numberOfWaypoints];
+         { // Computing the waypoint times
+            double[] weights = new double[numberOfWaypoints + 1];
+            double sumOfWeights = 0.0;
+            for (int j = 0; j < weights.length; j++)
+            {
+               weights[j] = random.nextDouble();
+               sumOfWeights += weights[j];
+            }
+            for (int j = 0; j < numberOfWaypoints; j++)
+            {
+               waypointTimes[j] = weights[j] / sumOfWeights;
+               if (j > 0)
+                  waypointTimes[j] += waypointTimes[j - 1];
+            }
+         }
+
+         solver.clearWeights();
+         solver.clearWaypoints();
+         solver.setEndpoints(startPosition, 0.0, endPosition, 0.0);
+
+         for (int j = 0; j < numberOfWaypoints; j++)
+         {
+            solver.addWaypoint(waypointPositions[j], waypointTimes[j]);
+         }
+
+         solver.solve(solution);
+
+         int waypointIndex = random.nextInt(numberOfWaypoints);
+         double waypointTime = waypointTimes[waypointIndex];
+         double expectedVelocity = computeExpectedWaypointVelocity(waypointTime, waypointIndex, solution);
+         double actualVelocity = solver.computeWaypointVelocityFromSolution(waypointIndex, solution);
+         assertEquals(expectedVelocity, actualVelocity, 1.0e-10 * Math.max(1.0, Math.abs(expectedVelocity)));
+      }
+   }
+
+   private static double computeExpectedWaypointVelocity(double waypointTime, int waypointIndex, DMatrixRMaj solution)
+   {
+      DMatrixRMaj tempLine = new DMatrixRMaj(1, coefficients);
+      DMatrixRMaj tempCoeffs = new DMatrixRMaj(coefficients, 1);
+      MultiCubicSpline1DSolver.getVelocityConstraintABlock(waypointTime, 0, 0, tempLine);
+      int index = waypointIndex * coefficients;
+      CommonOps_DDRM.extract(solution, index, index + coefficients, 0, 1, tempCoeffs, 0, 0);
+      return CommonOps_DDRM.dot(tempCoeffs, tempLine);
    }
 }
