ee21799beebeb0bb4687002c03b17a6cde7669b8
Duncan Calvert <dcalvert@ihmc.us>
2022-01-20 19:01:39 -0600

    :arrow_up: ihmc-build 0.28.0 Remove Artifactory from build plugin.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
    plugins {
-      id("us.ihmc.ihmc-build") version "0.27.3"
+      id("us.ihmc.ihmc-build") version "0.28.0"
    }
 }
 
@@ -10,7 +10,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.27.3")
+      classpath("us.ihmc:ihmc-build:0.28.0")
    }
 }
 
