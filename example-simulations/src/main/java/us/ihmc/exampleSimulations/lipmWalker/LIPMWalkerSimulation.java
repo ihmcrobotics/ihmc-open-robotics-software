af13de7628d6d67092d3aa9d0d31719a46be9ff2
jerry <jerry@DESKTOP-KQ0MCA3>
2022-01-25 15:38:59 -0600

    Added constructor.

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/lipmWalker/LIPMWalkerSimulation.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/lipmWalker/LIPMWalkerSimulation.java
@@ -2,5 +2,13 @@ package us.ihmc.exampleSimulations.lipmWalker;
 
 public class LIPMWalkerSimulation
 {
-
+   public LIPMWalkerSimulation()
+   {
+      
+   }
+   
+   public static void main(String[] args)
+   {
+      LIPMWalkerSimulation simulation = new LIPMWalkerSimulation();
+   }
 }
