0246e9d0c1b0f8ad9d5479ae7a86e03c5336b363
gabri <gabri@LAPTOP-N0T20MHD>
2022-01-25 16:56:15 -0500

    implemented robot controller

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/lipmWalker/LIPMWalkerController.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/lipmWalker/LIPMWalkerController.java
@@ -1,6 +1,27 @@
 package us.ihmc.exampleSimulations.lipmWalker;
 
-public class LIPMWalkerController
+import us.ihmc.simulationconstructionset.util.RobotController;
+import us.ihmc.yoVariables.registry.YoRegistry;
+
+public class LIPMWalkerController implements RobotController
 {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public YoRegistry getYoRegistry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doControl() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
