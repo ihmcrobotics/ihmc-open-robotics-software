c68e69f712ed9a75d2400a19f38b082ee8bf5ae9
Duncan Calvert <dcalvert@ihmc.us>
2022-02-03 00:47:45 -0600

    Make the floor massive, larger, and a kinematic object so it can collide with multi bodies.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/object/objects/GDXLabFloorObject.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/object/objects/GDXLabFloorObject.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
 import us.ihmc.euclid.shape.primitives.Box3D;
+import us.ihmc.gdx.simulation.environment.GDXBulletPhysicsManager;
 import us.ihmc.gdx.simulation.environment.object.GDXEnvironmentObject;
 import us.ihmc.gdx.simulation.environment.object.GDXEnvironmentObjectFactory;
 import us.ihmc.gdx.tools.GDXModelLoader;
@@ -22,10 +23,24 @@ public class GDXLabFloorObject extends GDXEnvironmentObject
       double sizeX = 0.3;
       double sizeY = 0.3;
       double sizeZ = 0.01;
-      setMass(0.0f);
+      setMass(10000.0f);
       getBoundingSphere().setRadius(0.7);
       Box3D collisionBox = new Box3D(sizeX, sizeY, sizeZ);
       setCollisionGeometryObject(collisionBox);
-      setBtCollisionShape(new btBoxShape(new Vector3(10.0f, 10.0f, 0.0001f)));
+      setBtCollisionShape(new btBoxShape(new Vector3(10.0f, 10.0f, 1.0f)));
+      getBulletCollisionOffset().getTranslation().addZ(-1.0);
+   }
+
+   @Override
+   public void addToBullet(GDXBulletPhysicsManager bulletPhysicsManager)
+   {
+      super.addToBullet(bulletPhysicsManager);
+      bulletPhysicsManager.setKinematicObject(getBtRigidBody(), true); // has to be a massive kinematics object to collide with multi bodies
+   }
+
+   @Override
+   public void setSelected(boolean selected)
+   {
+      setRawIsSelected(selected);
    }
 }
