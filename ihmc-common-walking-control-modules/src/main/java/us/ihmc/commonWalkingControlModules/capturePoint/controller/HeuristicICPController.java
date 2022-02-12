7bd884e5119367de3e2ae87bcc8689d7537dd38c
jerry <jerry@DESKTOP-KQ0MCA3>
2022-02-12 10:34:18 -0600

    Working on Smart projection. Added some YoVariables for debugging.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/HeuristicICPController.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/HeuristicICPController.java
@@ -41,6 +41,14 @@ public class HeuristicICPController implements ICPControllerInterface
    private final String yoNamePrefix = "heuristic";
    private final YoRegistry registry = new YoRegistry("HeuristicICPController");
 
+   private final YoDouble adjustedICP = new YoDouble(yoNamePrefix + "AdjustedICP", registry);
+   private final YoDouble firstIntersection = new YoDouble(yoNamePrefix + "FirstIntersection", registry);
+   private final YoDouble secondIntersection = new YoDouble(yoNamePrefix + "SecondIntersection", registry);
+   private final YoDouble firstPerfect = new YoDouble(yoNamePrefix + "FirstPerfect", registry);
+   private final YoDouble secondPerfect = new YoDouble(yoNamePrefix + "SecondPerfect", registry);
+   private final YoDouble minICPPushDelta = new YoDouble(yoNamePrefix + "MinICPPushDelta", registry);
+   private final YoDouble adjustmentDistance = new YoDouble(yoNamePrefix + "AdjustmentDistance", registry);
+
    private final BooleanProvider useCMPFeedback;
    private final BooleanProvider useAngularMomentum;
 
@@ -113,8 +121,8 @@ public class HeuristicICPController implements ICPControllerInterface
                                  YoRegistry parentRegistry,
                                  YoGraphicsListRegistry yoGraphicsListRegistry)
    {
-      this(walkingControllerParameters, walkingControllerParameters.getICPOptimizationParameters(), bipedSupportPolygons, icpControlPolygons, contactableFeet,
-           controlDT, parentRegistry, yoGraphicsListRegistry);
+      this(walkingControllerParameters, walkingControllerParameters.getICPOptimizationParameters(), bipedSupportPolygons, icpControlPolygons, contactableFeet, controlDT, parentRegistry,
+           yoGraphicsListRegistry);
    }
 
    public HeuristicICPController(WalkingControllerParameters walkingControllerParameters,
@@ -127,6 +135,7 @@ public class HeuristicICPController implements ICPControllerInterface
                                  YoGraphicsListRegistry yoGraphicsListRegistry)
    {
       pureFeedbackThreshError.set(0.2);
+      minICPPushDelta.set(0.03);
 
       this.controlDT = controlDT;
       this.controlDTSquare = controlDT * controlDT;
@@ -219,7 +228,6 @@ public class HeuristicICPController implements ICPControllerInterface
          icpParallelError.set(0.0);
       }
 
-
       icpParallelFeedback.set(icpParallelError.getValue());
       icpParallelFeedback.mul(feedbackGains.getKpParallelToMotion());
 
@@ -229,66 +237,174 @@ public class HeuristicICPController implements ICPControllerInterface
       pureFeedbackControl.set(icpError);
       pureFeedbackControl.scale(feedbackGains.getKpOrthogonalToMotion());
       pureFeedbackMagnitude.set(pureFeedbackControl.length());
-//      pureFeedbackControl.clipToMaxLength(maxLength);
-      
+      //      pureFeedbackControl.clipToMaxLength(maxLength);
+
       pureFeedforwardControl.sub(perfectCMP, desiredICP);
       pureFeedforwardMagnitude.set(pureFeedforwardControl.length());
-      
-//      if (icpErrorMagnitude.getValue() >= pureFeedbackThreshError.getValue())
-         if (Math.abs(icpPerpError.getValue()) >= pureFeedbackThreshError.getValue())
+
+      //      if (icpErrorMagnitude.getValue() >= pureFeedbackThreshError.getValue())
+      if (Math.abs(icpPerpError.getValue()) >= pureFeedbackThreshError.getValue())
       {
          feedbackFeedforwardAlpha.set(1.0);
       }
       else
       {
-//         feedbackFeedforwardAlpha.set(icpErrorMagnitude.getValue()/pureFeedbackThreshError.getValue());
-         double perpErrorAdjusted = Math.abs(icpPerpError.getValue()) - pureFeedbackThreshError.getValue()/2.0;
+         //         feedbackFeedforwardAlpha.set(icpErrorMagnitude.getValue()/pureFeedbackThreshError.getValue());
+         double perpErrorAdjusted = Math.abs(icpPerpError.getValue()) - pureFeedbackThreshError.getValue() / 2.0;
          if (perpErrorAdjusted < 0.0)
             perpErrorAdjusted = 0.0;
-                  
-         feedbackFeedforwardAlpha.set(perpErrorAdjusted/(pureFeedbackThreshError.getValue()/2.0));
+
+         feedbackFeedforwardAlpha.set(perpErrorAdjusted / (pureFeedbackThreshError.getValue() / 2.0));
       }
-      
+
       limitAbsoluteValue(icpParallelFeedback, feedbackGains.getFeedbackPartMaxValueParallelToMotion());
       limitAbsoluteValue(icpPerpFeedback, feedbackGains.getFeedbackPartMaxValueOrthogonalToMotion());
 
-//      unconstrainedFeedback.interpolate(pureFeedforwardControl, pureFeedbackControl, feedbackFeedforwardAlpha.getValue());
-      
-      
+      //      unconstrainedFeedback.interpolate(pureFeedforwardControl, pureFeedbackControl, feedbackFeedforwardAlpha.getValue());
+
       unconstrainedFeedback.set(pureFeedforwardControl);
       unconstrainedFeedback.scale(1.0 - feedbackFeedforwardAlpha.getValue());
       unconstrainedFeedback.add(pureFeedbackControl);
-      
+
       unconstrainedFeedbackCMP.set(currentICP);
       unconstrainedFeedbackCMP.add(unconstrainedFeedback);
 
-//      unconstrainedFeedback.setToZero();
-//      if (!parallelDirection.containsNaN())
-//      {
-//         tempVector.set(parallelDirection);
-//         tempVector.scale(icpParallelFeedback.getValue());
-//         unconstrainedFeedback.add(tempVector);
-//      }
-//
-//      if (!perpDirection.containsNaN())
-//      {
-//         tempVector.set(perpDirection);
-//         tempVector.scale(icpPerpFeedback.getValue());
-//         unconstrainedFeedback.add(tempVector);
-//      }
-//
-//      unconstrainedFeedbackCMP.add(perfectCoP, perfectCMPOffset);
-//      unconstrainedFeedbackCMP.add(icpError);
-//      unconstrainedFeedbackCMP.add(unconstrainedFeedback);
+      //      unconstrainedFeedback.setToZero();
+      //      if (!parallelDirection.containsNaN())
+      //      {
+      //         tempVector.set(parallelDirection);
+      //         tempVector.scale(icpParallelFeedback.getValue());
+      //         unconstrainedFeedback.add(tempVector);
+      //      }
+      //
+      //      if (!perpDirection.containsNaN())
+      //      {
+      //         tempVector.set(perpDirection);
+      //         tempVector.scale(icpPerpFeedback.getValue());
+      //         unconstrainedFeedback.add(tempVector);
+      //      }
+      //
+      //      unconstrainedFeedbackCMP.add(perfectCoP, perfectCMPOffset);
+      //      unconstrainedFeedbackCMP.add(icpError);
+      //      unconstrainedFeedbackCMP.add(unconstrainedFeedback);
 
-      
-      
-      
-      
-      
       unconstrainedFeedbackCoP.set(unconstrainedFeedbackCMP);
       unconstrainedFeedbackCoP.sub(perfectCMPOffset);
 
+      projectTowardsMidpoint();
+
+      feedbackCMP.set(feedbackCoP);
+      feedbackCMP.add(perfectCMPOffset);
+
+      controllerTimer.stopMeasurement();
+   }
+
+   private void projectTowardsMidpoint()
+   {
+      // Project the CoP onto the support polygon, using a projection vector.
+      coPProjection.setToNaN();
+      firstProjectionIntersection.setToNaN();
+      secondProjectionIntersection.setToNaN();
+      closestPointWithProjectionLine.setToNaN();
+      projectionVector.setToNaN();
+      projectionLine.setToNaN();
+
+      // Determine the closest point, intersection point, etc.
+      FrameConvexPolygon2DReadOnly supportPolygonInWorld = bipedSupportPolygons.getSupportPolygonInWorld();
+
+      //Compute the projection vector and projection line:
+      projectionVector.set(currentICP);
+      projectionVector.sub(unconstrainedFeedbackCMP);
+
+      adjustedICP.set(projectionVector.length());
+
+      //TODO: What if projection vector length is small?
+      projectionVector.normalize();
+
+      projectionLine.set(unconstrainedFeedbackCoP, projectionVector);
+
+      supportPolygonInWorld.intersectionWith(projectionLine, firstProjectionIntersection, secondProjectionIntersection);
+
+      if (firstProjectionIntersection.containsNaN() || (secondProjectionIntersection.containsNaN()))
+      {
+         projectWhenProjectionLineDoesNotIntersectFoot(supportPolygonInWorld);
+         return;
+      }
+
+      tempVector.set(firstProjectionIntersection);
+      tempVector.sub(unconstrainedFeedbackCMP);
+      firstIntersection.set(tempVector.dot(projectionVector));
+
+      tempVector.set(secondProjectionIntersection);
+      tempVector.sub(unconstrainedFeedbackCMP);
+      secondIntersection.set(tempVector.dot(projectionVector));
+
+      if (firstIntersection.getValue() > secondIntersection.getValue())
+      {
+         double temp = firstIntersection.getValue();
+         firstIntersection.set(secondIntersection.getValue());
+         secondIntersection.set(temp);
+      }
+
+      firstPerfect.set(firstIntersection.getValue());
+      secondPerfect.set(secondIntersection.getValue());
+
+      //      tempVector.set(perfectCoP);
+      //      tempVector.sub(unconstrainedFeedbackCMP);
+      //      firstPerfect.set(tempVector.dot(projectionVector));
+      //      secondPerfect.set(firstPerfect.getValue());
+      //
+      //      firstPerfect.sub(0.01);
+      //      secondPerfect.add(0.01);
+
+      adjustmentDistance.set(HeuristicICPControllerHelper.computeAdjustmentDistance(adjustedICP.getValue(),
+                                                                                    firstIntersection.getValue(),
+                                                                                    secondIntersection.getValue(),
+                                                                                    firstPerfect.getValue(),
+                                                                                    secondPerfect.getValue(),
+                                                                                    minICPPushDelta.getValue()));
+
+      tempVector.set(projectionVector);
+      tempVector.scale(adjustmentDistance.getValue());
+      feedbackCoP.set(unconstrainedFeedbackCoP);
+      feedbackCoP.add(tempVector);
+   }
+
+   private void projectWhenProjectionLineDoesNotIntersectFoot(FrameConvexPolygon2DReadOnly supportPolygonInWorld)
+   {
+      coPProjection.set(unconstrainedFeedbackCoP);
+      supportPolygonInWorld.orthogonalProjection(coPProjection);
+
+      supportPolygonInWorld.getClosestPointWithRay(projectionLine, closestPointWithProjectionLine);
+
+      tempVector.set(currentICP);
+      tempVector.sub(coPProjection);
+      tempVector.normalize();
+      double copProjectionDot = tempVector.dot(projectionVector);
+
+      tempVector.set(currentICP);
+      tempVector.sub(closestPointWithProjectionLine);
+      tempVector.normalize();
+      double closestPointWithProjectionLineDot = tempVector.dot(projectionVector);
+
+      // TODO: Not sure if this is what we want to be doing here. Might be something smarter.
+      if (copProjectionDot >= closestPointWithProjectionLineDot)
+         feedbackCoP.set(coPProjection);
+      else
+         feedbackCoP.set(closestPointWithProjectionLine);
+
+      //            double coPProjectionDistanceSquared = coPProjection.distanceSquared(unconstrainedFeedbackCoP);
+      //            double closestPointWithProjectionLineDistanceSquared = closestPointWithProjectionLine.distanceSquared(unconstrainedFeedbackCoP);
+
+      //            if (coPProjectionDistanceSquared <= closestPointWithProjectionLineDistanceSquared)
+      //               feedbackCoP.set(coPProjection);
+      //            else
+      //               feedbackCoP.set(closestPointWithProjectionLine);
+
+   }
+
+   private void projectIntoFoot()
+   {
       // Project the CoP onto the support polygon, using a projection vector.
       coPProjection.setToNaN();
       firstProjectionIntersection.setToNaN();
@@ -305,7 +421,7 @@ public class HeuristicICPController implements ICPControllerInterface
          //TODO: Move more inside the foot if there is a large enough distance from the CMP to the ICP, so that 
          // the foot doesn't unnecessarily rotate.
          // Maybe after all the stuff is computed, do this tweak inside to prevent foot rotation if you can.
-         
+
          feedbackCoP.set(unconstrainedFeedbackCoP);
       }
 
@@ -376,11 +492,6 @@ public class HeuristicICPController implements ICPControllerInterface
             //               feedbackCoP.set(closestPointWithProjectionLine);
          }
       }
-
-      feedbackCMP.set(feedbackCoP);
-      feedbackCMP.add(perfectCMPOffset);
-
-      controllerTimer.stopMeasurement();
    }
 
    private void limitAbsoluteValue(YoDouble yoDouble, double maxAbsoluteValue)
@@ -395,11 +506,9 @@ public class HeuristicICPController implements ICPControllerInterface
    {
       ArtifactList artifactList = new ArtifactList(getClass().getSimpleName());
 
-      YoGraphicPosition feedbackCoPViz = new YoGraphicPosition(yoNamePrefix
-            + "FeedbackCoP", this.feedbackCoP, 0.005, YoAppearance.Darkorange(), YoGraphicPosition.GraphicType.BALL_WITH_CROSS);
+      YoGraphicPosition feedbackCoPViz = new YoGraphicPosition(yoNamePrefix + "FeedbackCoP", this.feedbackCoP, 0.005, YoAppearance.Darkorange(), YoGraphicPosition.GraphicType.BALL_WITH_CROSS);
 
-      YoGraphicPosition unconstrainedFeedbackCMPViz = new YoGraphicPosition(yoNamePrefix
-            + "UnconstrainedFeedbackCMP", this.unconstrainedFeedbackCMP, 0.008, Purple(), GraphicType.BALL_WITH_CROSS);
+      YoGraphicPosition unconstrainedFeedbackCMPViz = new YoGraphicPosition(yoNamePrefix + "UnconstrainedFeedbackCMP", this.unconstrainedFeedbackCMP, 0.008, Purple(), GraphicType.BALL_WITH_CROSS);
 
       YoGraphicPosition unconstrainedFeedbackCoPViz = new YoGraphicPosition(yoNamePrefix
             + "UnconstrainedFeedbackCoP", this.unconstrainedFeedbackCoP, 0.004, YoAppearance.Green(), GraphicType.BALL_WITH_ROTATED_CROSS);
@@ -407,22 +516,19 @@ public class HeuristicICPController implements ICPControllerInterface
       //TODO: Figure out a viz that works with the logger.
       YoArtifactLine2d projectionLineViz = new YoArtifactLine2d(yoNamePrefix + "ProjectionLine", this.projectionLine, YoAppearance.Aqua().getAwtColor());
 
-      YoGraphicPosition firstIntersectionViz = new YoGraphicPosition(yoNamePrefix
-            + "FirstIntersection", this.firstProjectionIntersection, 0.004, YoAppearance.Green(), GraphicType.SOLID_BALL);
+      YoGraphicPosition firstIntersectionViz = new YoGraphicPosition(yoNamePrefix + "FirstIntersection", this.firstProjectionIntersection, 0.004, YoAppearance.Green(), GraphicType.SOLID_BALL);
 
-      YoGraphicPosition secondIntersectionViz = new YoGraphicPosition(yoNamePrefix
-            + "SecondIntersection", this.secondProjectionIntersection, 0.004, YoAppearance.Green(), GraphicType.SOLID_BALL);
+      YoGraphicPosition secondIntersectionViz = new YoGraphicPosition(yoNamePrefix + "SecondIntersection", this.secondProjectionIntersection, 0.004, YoAppearance.Green(), GraphicType.SOLID_BALL);
 
       YoGraphicPosition closestPointWithProjectionLineViz = new YoGraphicPosition(yoNamePrefix
             + "ClosestToProjectionLine", this.closestPointWithProjectionLine, 0.003, YoAppearance.Green(), GraphicType.SOLID_BALL);
 
-      YoGraphicPosition copProjectionViz = new YoGraphicPosition(yoNamePrefix
-            + "CoPProjection", this.coPProjection, 0.002, YoAppearance.Green(), GraphicType.SOLID_BALL);
+      YoGraphicPosition copProjectionViz = new YoGraphicPosition(yoNamePrefix + "CoPProjection", this.coPProjection, 0.002, YoAppearance.Green(), GraphicType.SOLID_BALL);
 
       artifactList.add(feedbackCoPViz.createArtifact());
       artifactList.add(unconstrainedFeedbackCMPViz.createArtifact());
       artifactList.add(unconstrainedFeedbackCoPViz.createArtifact());
-//      artifactList.add(projectionLineViz);
+      //      artifactList.add(projectionLineViz);
 
       artifactList.add(firstIntersectionViz.createArtifact());
       artifactList.add(secondIntersectionViz.createArtifact());
