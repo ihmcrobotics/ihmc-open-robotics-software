94a5a418b9e2eb6f6ed83a923bac663d9fe38f30
jerry <jerry@DESKTOP-KQ0MCA3>
2022-02-12 13:21:08 -0600

    More extensive tests and decide to not speed up for now.
    It does slow down too much though, so need to add some deltas from edge.
    Then we can allow speed up a little, but asymetrically perhaps?

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/HeuristicICPControllerHelper.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/HeuristicICPControllerHelper.java
@@ -5,49 +5,59 @@ public class HeuristicICPControllerHelper
 
    public static double computeAdjustmentDistance(double adjustedICP, double firstIntersection, double secondIntersection, double firstPerfect, double secondPerfect, double minICPPushDelta)
    {
-//      double maxTowardMiddle = 0.0;
-      
       double firstBoundary = firstPerfect;
       double secondBoundary = secondPerfect;
-      
+
       if (firstBoundary < firstIntersection)
          firstBoundary = firstIntersection;
-      
+
       if (secondBoundary < firstIntersection)
          secondBoundary = firstIntersection;
-      
+
       if (firstBoundary > secondIntersection)
          firstBoundary = secondIntersection;
-      
+
       if (secondBoundary > secondIntersection)
          secondBoundary = secondIntersection;
-      
+
+      if (secondBoundary < 0.0)
+         return secondBoundary;
+
       double midPointOrPerfectBoundaryIfOutside = 0.5 * (firstIntersection + secondIntersection);
 
       if (midPointOrPerfectBoundaryIfOutside < firstBoundary)
          midPointOrPerfectBoundaryIfOutside = firstBoundary;
       else if (midPointOrPerfectBoundaryIfOutside > secondBoundary)
          midPointOrPerfectBoundaryIfOutside = secondBoundary;
-      
-      
-//      double foo = firstIntersection + maxTowardMiddle;
-//      if ((foo > 0.0) && (midPointOrPerfectBoundaryIfOutside > foo))
-//         midPointOrPerfectBoundaryIfOutside = foo;
-//
-//      foo = secondIntersection - maxTowardMiddle;
-//      if ((foo < 0.0) && (midPointOrPerfectBoundaryIfOutside < foo))
-//         midPointOrPerfectBoundaryIfOutside = foo;
-      
-      
+
+      // Don't shift towards midpoint if it means speeding up!
+      // TODO: Relax this constraint later with some deltas...
+      if (midPointOrPerfectBoundaryIfOutside < 0.0)
+      {
+         midPointOrPerfectBoundaryIfOutside = 0.0;
+      }
+
       double maxAdjustmentBeforeInsideMinICPPush = adjustedICP - minICPPushDelta;
-      
+
       if (midPointOrPerfectBoundaryIfOutside < maxAdjustmentBeforeInsideMinICPPush)
+      {
          return midPointOrPerfectBoundaryIfOutside;
-      
+      }
+
+      else if (maxAdjustmentBeforeInsideMinICPPush < 0.0)
+      {
+         if (firstIntersection > 0.0)
+            return firstIntersection;
+         else if (secondIntersection < 0.0)
+            return secondIntersection;
+         else
+            return 0.0;
+      }
+
       else if (maxAdjustmentBeforeInsideMinICPPush > firstIntersection)
          return maxAdjustmentBeforeInsideMinICPPush;
-      
-      else 
+
+      else
          return firstIntersection;
    }
 
