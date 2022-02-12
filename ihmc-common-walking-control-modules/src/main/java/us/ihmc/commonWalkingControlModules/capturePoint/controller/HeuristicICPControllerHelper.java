f7495c97820db62aeabc1458eecb6b011cf53871
jerry <jerry@DESKTOP-KQ0MCA3>
2022-02-12 15:55:34 -0600

    More test cases. Fixed some bugs. Tune on Atlas sim.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/HeuristicICPControllerHelper.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/HeuristicICPControllerHelper.java
@@ -3,7 +3,13 @@ package us.ihmc.commonWalkingControlModules.capturePoint.controller;
 public class HeuristicICPControllerHelper
 {
 
-   public static double computeAdjustmentDistance(double adjustedICP, double firstIntersection, double secondIntersection, double firstPerfect, double secondPerfect, double minICPPushDelta, double maxProjectionInside)
+   public static double computeAdjustmentDistance(double adjustedICP,
+                                                  double firstIntersection,
+                                                  double secondIntersection,
+                                                  double firstPerfect,
+                                                  double secondPerfect,
+                                                  double minICPPushDelta,
+                                                  double maxProjectionInside)
    {
       double firstBoundary = firstPerfect;
       double secondBoundary = secondPerfect;
@@ -20,9 +26,12 @@ public class HeuristicICPControllerHelper
       if (secondBoundary > secondIntersection)
          secondBoundary = secondIntersection;
 
+      // For now if you are trying to slow down and you cannot because the unconstrained CoP is in front of the
+      // second edge, just project it there.
       if (secondBoundary < 0.0)
          return secondBoundary;
 
+      // Compute the best point to go towards.
       double midPointOrPerfectBoundaryIfOutside = 0.5 * (firstIntersection + secondIntersection);
 
       if (midPointOrPerfectBoundaryIfOutside < firstBoundary)
@@ -41,11 +50,23 @@ public class HeuristicICPControllerHelper
 
       if (midPointOrPerfectBoundaryIfOutside < maxAdjustmentBeforeInsideMinICPPush)
       {
-         return midPointOrPerfectBoundaryIfOutside;
+         // Can push as far as the midpoint. But do not push any further inside than maxProjectionInside.
+
+         //TODO: Check the ordering of these. And make test cases where they matter.
+         if (midPointOrPerfectBoundaryIfOutside <= 0.0)
+            return midPointOrPerfectBoundaryIfOutside;
+         else if ((midPointOrPerfectBoundaryIfOutside > 0.0) && (midPointOrPerfectBoundaryIfOutside < firstIntersection + maxProjectionInside))
+            return midPointOrPerfectBoundaryIfOutside;
+         else if (firstIntersection + maxProjectionInside < 0.0)
+            return 0.0;
+         else
+            return firstIntersection + maxProjectionInside;
       }
 
       else if (maxAdjustmentBeforeInsideMinICPPush < 0.0)
       {
+         //TODO: Check the ordering of these. And make test cases where they matter.
+
          if (firstIntersection > 0.0)
             return firstIntersection;
          else if (secondIntersection < 0.0)
@@ -55,7 +76,18 @@ public class HeuristicICPControllerHelper
       }
 
       else if (maxAdjustmentBeforeInsideMinICPPush > firstIntersection)
-         return maxAdjustmentBeforeInsideMinICPPush;
+      {
+         //TODO: Check the ordering of these. And make test cases where they matter.
+
+         if (maxAdjustmentBeforeInsideMinICPPush <= 0.0)
+            return maxAdjustmentBeforeInsideMinICPPush;
+         else if ((maxAdjustmentBeforeInsideMinICPPush > 0.0) && (maxAdjustmentBeforeInsideMinICPPush < firstIntersection + maxProjectionInside))
+            return maxAdjustmentBeforeInsideMinICPPush;
+         else if (firstIntersection + maxProjectionInside < 0.0)
+            return 0.0;
+         else
+            return firstIntersection + maxProjectionInside;
+      }
 
       else
          return firstIntersection;
