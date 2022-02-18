393500815d1a4b05807e147040ddeae926f09f26
Duncan Calvert <dcalvert@ihmc.us>
2022-02-18 16:46:02 -0600

    :arrow_up: ihmc-jmonkey-engine-toolkit 0.19.8 Fix NaN crashes

--- ihmc-simulation-toolkit/build.gradle.kts
+++ ihmc-simulation-toolkit/build.gradle.kts
@@ -16,7 +16,7 @@ ihmc {
 mainDependencies {
    api("us.ihmc:euclid:0.17.0")
    api("us.ihmc:ihmc-yovariables:0.9.11")
-   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.7")
+   api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.8")
    api("us.ihmc:ihmc-java-toolkit:source")
    api("us.ihmc:ihmc-robotics-toolkit:source")
    api("us.ihmc:ihmc-robot-models:source")
