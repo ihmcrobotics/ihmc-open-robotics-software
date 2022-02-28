380cc9112ede4d098745728279c46f3b9ded05ba
Duncan Calvert <dcalvert@ihmc.us>
2022-02-28 15:48:42 -0600

    Fix up replacing block falling demo.

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/simulation/bullet/libgdx/GDXBulletPhysicsInteractionForcesDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/gdx/simulation/bullet/libgdx/GDXBulletPhysicsInteractionForcesDemo.java
@@ -1,11 +1,15 @@
 package us.ihmc.gdx.simulation.bullet.libgdx;
 
+import imgui.internal.ImGui;
+import imgui.type.ImFloat;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.gdx.Lwjgl3ApplicationAdapter;
+import us.ihmc.gdx.imgui.ImGuiPanel;
 import us.ihmc.gdx.simulation.bullet.GDXBulletTools;
 import us.ihmc.gdx.simulation.environment.GDXEnvironmentBuilder;
 import us.ihmc.gdx.simulation.environment.object.objects.GDXLabFloorObject;
 import us.ihmc.gdx.simulation.environment.object.objects.GDXMediumCinderBlockRoughed;
+import us.ihmc.gdx.tools.GDXTools;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
 
 public class GDXBulletPhysicsInteractionForcesDemo
@@ -15,6 +19,7 @@ public class GDXBulletPhysicsInteractionForcesDemo
                                                               "ihmc-open-robotics-software",
                                                               "ihmc-high-level-behaviors/src/test/resources");
    private final GDXEnvironmentBuilder environmentBuilder = new GDXEnvironmentBuilder(baseUI.get3DSceneManager());
+   private final ImFloat blockTransparency = new ImFloat(1.0f);
 
    public GDXBulletPhysicsInteractionForcesDemo()
    {
@@ -22,6 +27,9 @@ public class GDXBulletPhysicsInteractionForcesDemo
 
       baseUI.launchGDXApplication(new Lwjgl3ApplicationAdapter()
       {
+
+         private GDXMediumCinderBlockRoughed mediumCinderBlockRoughed;
+
          @Override
          public void create()
          {
@@ -35,15 +43,36 @@ public class GDXBulletPhysicsInteractionForcesDemo
             environmentBuilder.addObject(labFloorObject);
             labFloorObject.copyThisTransformToBulletMultiBody();
 
-            GDXMediumCinderBlockRoughed mediumCinderBlockRoughed = new GDXMediumCinderBlockRoughed();
+            recreateAndPlace();
+
+            ImGuiPanel experimentPanel = new ImGuiPanel("Demo", () ->
+            {
+               if (ImGui.button("Replace Block"))
+               {
+                  recreateAndPlace();
+               }
+               if (ImGui.sliderFloat("Block transparency", blockTransparency.getData(), 0.0f, 1.0f))
+               {
+                  GDXTools.setTransparency(mediumCinderBlockRoughed.getRealisticModelInstance(), blockTransparency.get());
+               }
+            });
+            baseUI.getImGuiPanelManager().addPanel(experimentPanel);
+         }
+
+         public void recreateAndPlace()
+         {
+            if (mediumCinderBlockRoughed != null)
+            {
+               environmentBuilder.removeObject(mediumCinderBlockRoughed);
+            }
+
+            mediumCinderBlockRoughed = new GDXMediumCinderBlockRoughed();
             RigidBodyTransform transformToWorld = new RigidBodyTransform();
             transformToWorld.getTranslation().set(0.0, 0.0, 0.5);
             transformToWorld.getRotation().setYawPitchRoll(Math.toRadians(45.0), Math.toRadians(45.0), Math.toRadians(15.0));
             mediumCinderBlockRoughed.setTransformToWorld(transformToWorld);
             environmentBuilder.addObject(mediumCinderBlockRoughed);
             mediumCinderBlockRoughed.copyThisTransformToBulletMultiBody();
-
-
          }
 
          @Override
