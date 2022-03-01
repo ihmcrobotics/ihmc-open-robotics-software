67ec3cec0345f716efc36cbbf3b9815f3b9b47de
James Foster <james.paul.foster@outlook.com>
2022-03-01 14:47:14 -0600

    Cleaned up LIPMWalkerControllerJames into methods, removed redundant commands.

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/lipmWalker/LIPMWalkerControllerJames.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/lipmWalker/LIPMWalkerControllerJames.java
@@ -1,16 +1,9 @@
 package us.ihmc.exampleSimulations.lipmWalker;
 
-import us.ihmc.euclid.tools.EuclidCoreTools;
-import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
-import us.ihmc.log.LogTools;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
-import us.ihmc.robotics.stateMachine.core.State;
-import us.ihmc.robotics.stateMachine.core.StateMachine;
-import us.ihmc.robotics.stateMachine.core.StateTransitionCondition;
-import us.ihmc.robotics.stateMachine.factories.StateMachineFactory;
 import us.ihmc.simulationconstructionset.util.RobotController;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
@@ -31,6 +24,7 @@ public class LIPMWalkerControllerJames implements RobotController
    private final YoDouble kdHip = new YoDouble("kdHip", registry);
 
    private final YoDouble comXVelocity = new YoDouble("comXVelocity", registry);
+   private final YoDouble comZVelocity = new YoDouble("comZVelocity", registry);
    private final YoDouble comXPositionFromFoot = new YoDouble("comXPositionFromFoot", registry);
 
    private final YoDouble q_d_leftKnee = new YoDouble("q_d_leftKnee", registry);
@@ -123,105 +117,123 @@ public class LIPMWalkerControllerJames implements RobotController
    @Override
    public void doControl()
    {
+      // Simulation data.
       RobotSide swingSide = supportSide.getOppositeSide();
       Point3DReadOnly centerOfMassPosition = robot.getCenterOfMassPosition();
       Vector3DReadOnly centerOfMassVelocity = robot.getCenterOfMassVelocity();
-      double mass = robot.getMass();
-
       comXVelocity.set(centerOfMassVelocity.getX());
+      comZVelocity.set(centerOfMassVelocity.getZ());
       comHeight.set(centerOfMassPosition.getZ());
       footForces.get(supportSide).set(robot.getFootZForce(supportSide));
       footForces.get(swingSide).set(robot.getFootZForce(swingSide));
 
-      // If in the first half of step, the desired angles / lengths are computed to satisfy an isoceles triangle with base given by step length and height
-      // given by desired CoM height.
-      if (!isTopOfStrideVelocityRecorded.getValue())
-      {
-         double desiredKneeLength = Math.sqrt((desiredHeight.getValue() * desiredHeight.getValue()) + (0.5 * desiredStepLength.getValue() * 0.5 * desiredStepLength.getValue()));
-         desiredKneeLengths.get(supportSide).set(desiredKneeLength);
-         desiredKneeLengths.get(swingSide).set(desiredKneeLength + footIntoGroundLength.getValue());
-         double desiredHipAngle = Math.atan2(0.5 * desiredStepLength.getValue(), desiredHeight.getValue());
-//         desiredHipAngles.get(supportSide).set(desiredHipAngle);
-         desiredHipAngles.get(supportSide).set(Double.NaN);
-         desiredHipAngles.get(swingSide).set(-desiredHipAngle);
-      }
-
-      // Support Leg
-      double kneeLength = robot.getKneeLength(supportSide);
-      double feedForwardKneeForce = g * mass * kneeLength / centerOfMassPosition.getZ();
-      double feedBackKneeForce = kpKnee.getValue() * (desiredHeight.getValue() - comHeight.getValue()) + kdKnee.getValue() * (0.0 - centerOfMassVelocity.getZ());
-      // NOTE: not happy about this fudge factor on the feedforward -- I can't find a good enough set of knee gains.
-      robot.setKneeForce(supportSide, feedForwardKneeForce + feedBackKneeForce);
-      // TODO: if totalForce < 0, cap below, or make below cap 10% of body force
-
-      double hipAngle = robot.getHipAngle(supportSide) + robot.getBodyPitchAngle();
-      worldHipAngles.get(supportSide).set(hipAngle);
-      double hipVelocity = robot.getHipVelocity(supportSide);
-      double feedBackHipTorque = -kpBodyPitch.getValue() * (desiredBodyPitch.getValue() - robot.getBodyPitchAngle()) - kdBodyPitch.getValue() * (0.0 - robot.getBodyPitchAngularVelocity());
-      robot.setHipTorque(supportSide, feedBackHipTorque);
-
-      // Swing Leg
-      hipAngle = robot.getHipAngle(swingSide) + robot.getBodyPitchAngle();
-      worldHipAngles.get(swingSide).set(hipAngle);
-      hipVelocity = robot.getHipVelocity(swingSide);
-      feedBackHipTorque = kpHip.getValue() * (desiredHipAngles.get(swingSide).getValue() - hipAngle) + kdKnee.getValue() * (0.0 - hipVelocity);
-      robot.setHipTorque(swingSide, feedBackHipTorque);
-
-      kneeLength = robot.getKneeLength(swingSide);
-      double kneeVelocity = robot.getKneeVelocity(swingSide);
-      // If the swing leg is behind the support leg, retract it to half length to prevent scuffing.
-      // NOTE: The > and dependence on angles means this only works for forward walking.
-      if (worldHipAngles.get(swingSide).getValue() > worldHipAngles.get(supportSide).getValue())
-         feedBackKneeForce = kpKnee.getValue() * (0.5 - kneeLength) + kdKnee.getValue() * (0.0 - kneeVelocity);
-      else
-         feedBackKneeForce = kpKnee.getValue() * (desiredKneeLengths.get(swingSide).getValue() - kneeLength) + kdKnee.getValue() * (0.0 - kneeVelocity);
-      robot.setKneeForce(swingSide, feedBackKneeForce);
-
       // When swing leg crosses in front of support leg, record top of stride velocity, and set a new target hip angle and knee length for the swing leg
       // depending on whether we're going slower or faster than we want to.
-      if (robot.getFootPosition(supportSide).getX() - centerOfMassPosition.getX() < 0.0)
+      if (robot.getFootPosition(supportSide).getX() - centerOfMassPosition.getX() > 0.0)
       {
+         // Pre-half step.
+         calculatePreHalfStepSwingLegKinematics(swingSide);
+      }
+      else
+      {
+         // Post-half step.
          if (!isTopOfStrideVelocityRecorded.getValue())
          {
             topOfStrideVelocity.set(comXVelocity.getValue());
-            double newSwingHipAngle;
-//            if (topOfStrideVelocity.getValue() < desiredTopOfStrideVelocity.getValue())
-               newSwingHipAngle = desiredHipAngles.get(swingSide).getValue() + kpStepLength.getValue() * (desiredTopOfStrideVelocity.getValue() - topOfStrideVelocity.getValue());
-//            else
-//               newSwingHipAngle = desiredHipAngles.get(swingSide).getValue() + 0.062 * (desiredTopOfStrideVelocity.getValue() - topOfStrideVelocity.getValue());
-               if (newSwingHipAngle > -0.05)
-                  newSwingHipAngle = -0.05;
-               if (newSwingHipAngle > 1.0)
-                  newSwingHipAngle = 1.0;
-
-               desiredHipAngles.get(swingSide).set(newSwingHipAngle);
-            double newSwingKneeLength = desiredHeight.getValue() / Math.cos(newSwingHipAngle) + footIntoGroundLength.getValue();
-            finalKneeLength.set(newSwingKneeLength);
-
-//            desiredKneeLengths.get(swingSide).set(newSwingKneeLength);
-            isTopOfStrideVelocityRecorded.set(true);
+            calculatePostHalfStepSwingLegKinematics(swingSide);
+            isTopOfStrideVelocityRecorded.set(true);  // locks out this if statement to keep post half step kinematics fixed
          }
       }
-      
-      double percentOver = 0.5 + 0.5 * robot.getHipAngle(supportSide) / (0.2);
-      if (percentOver < 0.5) percentOver = 0.5;
-      if (percentOver > 1.5) percentOver = 1.5;
-      
-      
-      desiredKneeLengths.get(swingSide).set(finalKneeLength.getValue() * percentOver);
 
-      // Support Leg Switching
-      boolean isSwingFootOnGround = robot.getFootPosition(swingSide).getZ() < 0.001 && worldHipAngles.get(swingSide).getValue() < worldHipAngles.get(supportSide).getValue();
-      boolean isSwingAnklePastVertical = isTopOfStrideVelocityRecorded.getValue() && worldHipAngles.get(swingSide).getValue() > 0.0;
-      if ( isSwingFootOnGround)// || isSwingAnklePastVertical )
+      // Control.
+      controlSupportLeg(supportSide);
+      controlSwingLeg(swingSide);
+
+      // Update desired swing knee length to further drive swing leg into the ground as stance lengthens.
+      updateSwingLegGroundDrivingHeuristic(swingSide);
+
+      // Support Leg Switching.
+      boolean isSwingFootOnGround = robot.getFootPosition(swingSide).getZ() < 0.001 &&
+                                    worldHipAngles.get(swingSide).getValue() < worldHipAngles.get(supportSide).getValue();
+      if ( isSwingFootOnGround )
       {
-         // We change swing leg hip angle halfway through the step, for the next step let's assume that the new hip angle we chose was perfect and set BOTH
-         // hip angles to be that by updating the step length as below. We'll end up selecting a new hip angle halfway through the next step anyway.
-         // NOTE: The minus in front of the tan is because hip angles are negative going forward. As tan is an odd function, we need to prefix a minus to
-         // get the desired results.
-//         desiredStepLength.set(2.0 * desiredHeight.getValue() * -Math.tan(desiredHipAngles.get(swingSide).getValue()));
          supportSide = supportSide.getOppositeSide();
          isTopOfStrideVelocityRecorded.set(false);
       }
+   }
+
+   public void controlSwingLeg(RobotSide side)
+   {
+      // Hip -- PD control on desired hip angle.
+      double hipAngle = robot.getHipAngle(side) + robot.getBodyPitchAngle();
+      worldHipAngles.get(side).set(hipAngle);
+      double hipVelocity = robot.getHipVelocity(side);
+      double feedBackHipTorque = kpHip.getValue() * (desiredHipAngles.get(side).getValue() - hipAngle) + kdKnee.getValue() * (0.0 - hipVelocity);
+      robot.setHipTorque(side, feedBackHipTorque);
+
+      // Knee -- PD control on desired knee length.
+      double kneeLength = robot.getKneeLength(side);
+      double kneeVelocity = robot.getKneeVelocity(side);
+      // If the swing leg is behind the support leg, retract it to half length to prevent scuffing.
+      // NOTE: The > and dependence on angles means this only works for forward walking.
+      double feedBackKneeForce;
+      if (worldHipAngles.get(side).getValue() > worldHipAngles.get(supportSide).getValue())
+         feedBackKneeForce = kpKnee.getValue() * (0.5 - kneeLength) + kdKnee.getValue() * (0.0 - kneeVelocity);
+      else
+         feedBackKneeForce = kpKnee.getValue() * (desiredKneeLengths.get(side).getValue() - kneeLength) + kdKnee.getValue() * (0.0 - kneeVelocity);
+      robot.setKneeForce(side, feedBackKneeForce);
+   }
+
+   public void controlSupportLeg(RobotSide side)
+   {
+      // Knee -- PD control around desired CoM height.
+      double mass = robot.getMass();
+      double kneeLength = robot.getKneeLength(side);
+      double feedForwardKneeForce = mass * g * kneeLength / comHeight.getValue();
+      double feedBackKneeForce = kpKnee.getValue() * (desiredHeight.getValue() - comHeight.getValue()) + kdKnee.getValue() * (0.0 - comZVelocity.getValue());
+      robot.setKneeForce(supportSide, feedForwardKneeForce + feedBackKneeForce);
+
+      // Hip -- PD control around desired body pitch (keep upright).
+      double hipAngle = robot.getHipAngle(supportSide) + robot.getBodyPitchAngle();
+      worldHipAngles.get(supportSide).set(hipAngle);
+      double feedBackHipTorque = -kpBodyPitch.getValue() * (desiredBodyPitch.getValue() - robot.getBodyPitchAngle()) - kdBodyPitch.getValue() * (0.0 - robot.getBodyPitchAngularVelocity());
+      robot.setHipTorque(supportSide, feedBackHipTorque);
+   }
+
+   public void calculatePreHalfStepSwingLegKinematics(RobotSide side)
+   {
+      // Hip -- trigonometry based on desired step length and desired CoM height.
+      double desiredHipAngle = Math.atan2(0.5 * desiredStepLength.getValue(), desiredHeight.getValue());
+      desiredHipAngles.get(side).set(-desiredHipAngle);
+
+      // Knee -- pythagoras based on desired step length and desired CoM height.
+      double desiredKneeLength = Math.sqrt((desiredHeight.getValue() * desiredHeight.getValue()) + (0.5 * desiredStepLength.getValue() * 0.5 * desiredStepLength.getValue()));
+      desiredKneeLengths.get(side).set(desiredKneeLength + footIntoGroundLength.getValue());
+   }
+
+   public void calculatePostHalfStepSwingLegKinematics(RobotSide side)
+   {
+      // Hip -- Raibert style proportional heuristic, change swing leg hip angle depending on whether we're going faster
+      // or slower than we want to, as measured at top of stride.
+      double newSwingHipAngle;
+      newSwingHipAngle = desiredHipAngles.get(side).getValue() + kpStepLength.getValue() * (desiredTopOfStrideVelocity.getValue() - topOfStrideVelocity.getValue());
+      if (newSwingHipAngle > -0.05)
+         newSwingHipAngle = -0.05;
+      if (newSwingHipAngle > 1.0)
+         newSwingHipAngle = 1.0;
+      desiredHipAngles.get(side).set(newSwingHipAngle);
+
+      // Knee -- desired length updated based on trig with the new hip angle, plus an extra heuristic on knee length to ensure ground contact.
+      double newSwingKneeLength = desiredHeight.getValue() / Math.cos(newSwingHipAngle) + footIntoGroundLength.getValue();
+      finalKneeLength.set(newSwingKneeLength);
+   }
+
+   public void updateSwingLegGroundDrivingHeuristic(RobotSide side)
+   {
+      double percentOver = 0.5 + 0.5 * robot.getHipAngle(supportSide) / (0.2);
+      if (percentOver < 0.5) percentOver = 0.5;
+      if (percentOver > 1.5) percentOver = 1.5;
+
+      desiredKneeLengths.get(side).set(finalKneeLength.getValue() * percentOver);
    }
 }
