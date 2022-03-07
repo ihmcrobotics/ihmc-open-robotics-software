b2e750a3edf5f2bbe2abf096eb61176047a3f7ea
Robert Griffin <rgriffin@ihmc.us>
2022-03-06 20:56:18 -0600

    gave some more options for visualization of the adjustment

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/trajectories/BlendedPositionTrajectoryGeneratorVisualizer.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/trajectories/BlendedPositionTrajectoryGeneratorVisualizer.java
@@ -1,15 +1,28 @@
 package us.ihmc.robotics.math.trajectories;
 
+import us.ihmc.graphicsDescription.appearance.YoAppearance;
+import us.ihmc.graphicsDescription.yoGraphics.BagOfBalls;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
+import us.ihmc.robotics.math.trajectories.generators.MultipleWaypointsPositionTrajectoryGenerator;
 import us.ihmc.robotics.math.trajectories.interfaces.FixedFramePositionTrajectoryGenerator;
+import us.ihmc.robotics.math.trajectories.trajectorypoints.YoFrameEuclideanTrajectoryPoint;
 import us.ihmc.robotics.math.trajectories.yoVariables.YoGraphicTrajectory3D;
 import us.ihmc.robotics.math.trajectories.yoVariables.YoGraphicTrajectory3D.TrajectoryColorType;
 import us.ihmc.yoVariables.providers.DoubleProvider;
 import us.ihmc.yoVariables.registry.YoRegistry;
 
+import java.awt.*;
+
 public class BlendedPositionTrajectoryGeneratorVisualizer
 {
-   private final YoGraphicTrajectory3D trajectoryViz;
+   private static final boolean useBagOfBalls = true;
+
+   private static final int numberOfBalls = 25;
+   private final DoubleProvider swingDuration;
+   private final BlendedWaypointPositionTrajectoryGenerator trajectory;
+   private final BagOfBalls trajectoryViz;
+   private final BagOfBalls waypointViz;
+   private final YoGraphicTrajectory3D splineViz;
 
    public BlendedPositionTrajectoryGeneratorVisualizer(String namePrefix,
                                                        BlendedPoseTrajectoryGenerator trajectory,
@@ -21,44 +34,81 @@ public class BlendedPositionTrajectoryGeneratorVisualizer
    }
 
    public BlendedPositionTrajectoryGeneratorVisualizer(String namePrefix,
-                                                       FixedFramePositionTrajectoryGenerator trajectory,
+                                                       BlendedWaypointPositionTrajectoryGenerator trajectory,
                                                        DoubleProvider swingDuration,
                                                        YoRegistry registry,
                                                        YoGraphicsListRegistry graphicsListRegistry)
    {
-      trajectoryViz = new YoGraphicTrajectory3D(namePrefix + "BlendedTrajectory",
-                                                null,
-                                                trajectory,
-                                                swingDuration,
-                                                0.01,
-                                                25,
-                                                8,
-                                                registry);
-      graphicsListRegistry.registerYoGraphic(namePrefix + "Trajectory", trajectoryViz);
+      this.trajectory = trajectory;
+      this.swingDuration = swingDuration;
+      if (useBagOfBalls)
+      {
+         trajectoryViz = new BagOfBalls(numberOfBalls, 0.01, namePrefix + "BlendTrajectory", registry, graphicsListRegistry);
+         waypointViz = new BagOfBalls(numberOfBalls, 0.025, namePrefix + "BlendWaypoints", YoAppearance.Green(), registry, graphicsListRegistry);
+         splineViz = null;
+      }
+      else
+      {
+         trajectoryViz = null;
+         waypointViz = null;
+         splineViz = new YoGraphicTrajectory3D(namePrefix + "BlendedTrajectory", null, trajectory, swingDuration, 0.01, 25, 8, registry);
+         graphicsListRegistry.registerYoGraphic(namePrefix + "Trajectory", splineViz);
 
-      trajectoryViz.setColorType(TrajectoryColorType.ACCELERATION_BASED);
+         splineViz.setColorType(TrajectoryColorType.ACCELERATION_BASED);
+      }
    }
 
    public void visualize()
    {
-      if (trajectoryViz == null)
-         return;
+      if (useBagOfBalls)
+      {
+         trajectoryViz.reset();
+         waypointViz.reset();
 
-      trajectoryViz.showGraphic();
-      trajectoryViz.update();
+         MultipleWaypointsPositionTrajectoryGenerator blendTrajecotry = trajectory.getBlendTrajectory();
+         for (int i = 0; i < blendTrajecotry.getCurrentNumberOfWaypoints(); i++)
+         {
+            YoFrameEuclideanTrajectoryPoint blendWaypont = blendTrajecotry.getWaypoint(i);
+            trajectory.compute(blendWaypont.getTime());
+            waypointViz.setBall(trajectory.getPosition());
+         }
+
+         double dt = swingDuration.getValue() / numberOfBalls;
+         double time = 0.0;
+         for (int i = 0; i < numberOfBalls; i++)
+         {
+            trajectory.compute(time);
+            trajectoryViz.setBall(trajectory.getPosition());
+
+            time += dt;
+         }
+      }
+      else
+      {
+         splineViz.showGraphic();
+         splineViz.update();
+      }
    }
 
    public void showVisualization()
    {
-      if (trajectoryViz == null)
-         return;
-      trajectoryViz.showGraphic();
+      if (!useBagOfBalls)
+         splineViz.showGraphic();
    }
 
    public void hideVisualization()
    {
-      if (trajectoryViz == null)
-         return;
-      trajectoryViz.hideGraphic();
+      if (useBagOfBalls)
+      {
+         trajectoryViz.reset();
+         trajectoryViz.hideAll();
+
+         waypointViz.reset();
+         waypointViz.hideAll();
+      }
+      else
+      {
+         splineViz.hideGraphic();
+      }
    }
 }
