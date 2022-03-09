41938c6fa033cb7f6117362445659e45cbeaf80f
Duncan Calvert <dcalvert@ihmc.us>
2022-03-09 12:38:38 -0600

    Make initialization of Atlas collisions more clear.

--- atlas/src/main/java/us/ihmc/atlas/AtlasFallingOnGroundSCS2Bullet.java
+++ atlas/src/main/java/us/ihmc/atlas/AtlasFallingOnGroundSCS2Bullet.java
@@ -25,7 +25,6 @@ import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizer;
 import us.ihmc.scs2.sessionVisualizer.jfx.SessionVisualizerControls;
 import us.ihmc.scs2.simulation.SimulationSession;
 import us.ihmc.scs2.simulation.bullet.physicsEngine.BulletPhysicsEngine;
-import us.ihmc.scs2.simulation.robot.Robot;
 import us.ihmc.simulationConstructionSetTools.util.HumanoidFloatingRootJointRobot;
 import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
 
@@ -41,7 +40,9 @@ public class AtlasFallingOnGroundSCS2Bullet
 
       SimulationSession simulationSession = new SimulationSession(BulletPhysicsEngine::new);
 
-      RobotDefinition robotDefinition = robotModel.createRobotDefinition(null, false);
+      boolean removeCollisions = false;
+      MaterialDefinition materialDefinition = null; // default material
+      RobotDefinition robotDefinition = robotModel.createRobotDefinition(materialDefinition, removeCollisions);
       double initialYaw = 0.3;
       double groundHeight = 0.1;
       RobotInitialSetup<HumanoidFloatingRootJointRobot> robotInitialSetup = robotModel.getDefaultRobotInitialSetup(groundHeight, initialYaw);
@@ -55,11 +56,14 @@ public class AtlasFallingOnGroundSCS2Bullet
 //      initialJointState.setPosition(new Point3D(0.0, 0.0, 1.0));
 //      sixDoFJointDefinition.setInitialJointState(initialJointState);
 
-//      CollidableHelper collidableHelper = new CollidableHelper();
-//      RobotCollisionModel collisionModel = robotModel.getSimulationRobotCollisionModel(collidableHelper, "robot", "terrain");
-//      if (collisionModel != null)
-//         RobotDefinitionTools.addCollisionsToRobotDefinition(collisionModel.getRobotCollidables(robotModel.createFullRobotModel().getElevator()),
-//                                                             robotDefinition);
+      if (removeCollisions)
+      {
+         CollidableHelper collidableHelper = new CollidableHelper();
+         RobotCollisionModel collisionModel = robotModel.getSimulationRobotCollisionModel(collidableHelper, "robot", "terrain");
+         if (collisionModel != null)
+            RobotDefinitionTools.addCollisionsToRobotDefinition(collisionModel.getRobotCollidables(robotModel.createFullRobotModel().getElevator()),
+                                                                robotDefinition);
+      }
 //      robotInitialSetup.initializeRobotDefinition(robotDefinition);
       Set<String> lastSimulatedJoints = robotModel.getJointMap().getLastSimulatedJoints();
       lastSimulatedJoints.forEach(robotDefinition::addSubtreeJointsToIgnore);
