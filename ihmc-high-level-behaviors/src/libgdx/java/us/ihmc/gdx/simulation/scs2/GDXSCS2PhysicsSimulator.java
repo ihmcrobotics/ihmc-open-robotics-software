4d2ccd56364305d3a297ffffc187a786b3484660
Duncan Calvert <dcalvert@ihmc.us>
2022-03-11 14:11:36 -0600

    Implement finer grained SCS 2 session controls with ImGui widgets. Needed to get the buffer size and current index, so made the getter public.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/scs2/GDXSCS2PhysicsSimulator.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/scs2/GDXSCS2PhysicsSimulator.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.utils.Pool;
 import imgui.ImGui;
 import imgui.type.ImBoolean;
 import imgui.type.ImDouble;
+import imgui.type.ImFloat;
+import imgui.type.ImInt;
 import us.ihmc.gdx.imgui.ImGuiPanel;
+import us.ihmc.gdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.gdx.sceneManager.GDXSceneLevel;
 import us.ihmc.gdx.simulation.bullet.GDXBulletPhysicsAsyncDebugger;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
@@ -15,15 +18,20 @@ import us.ihmc.scs2.definition.terrain.TerrainObjectDefinition;
 import us.ihmc.scs2.session.SessionMode;
 import us.ihmc.scs2.simulation.SimulationSession;
 import us.ihmc.scs2.simulation.bullet.physicsEngine.BulletPhysicsEngine;
+import us.ihmc.tools.UnitConversions;
 
 import java.util.ArrayList;
 
 public class GDXSCS2PhysicsSimulator
 {
    private final SimulationSession simulationSession;
-   private final ImBoolean runAtRealtimeRate;
-   private final ImDouble playbackRealtimeRate;
+   private final ImBoolean runAtRealtimeRate = new ImBoolean(true);
+   private final ImDouble playbackRealtimeRate = new ImDouble(1.0);
    private final ImGuiPanel controlPanel = new ImGuiPanel("Physics Simulator", this::renderImGuiWidgets);
+   private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
+   private final ImInt bufferIndex = new ImInt();
+   private final ImInt dtHz = new ImInt(240);
+   private final ImFloat bufferLength = new ImFloat(5.0f);
    private final GDXYoManager yoManager = new GDXYoManager();
    private final ArrayList<GDXSimulatedRobot> robots = new ArrayList<>();
    private final ArrayList<GDXSimulatedTerrainObject> terrainObjects = new ArrayList<>();
@@ -34,12 +42,15 @@ public class GDXSCS2PhysicsSimulator
       simulationSession = new SimulationSession(BulletPhysicsEngine::new);
       BulletPhysicsEngine bulletPhysicsEngine = (BulletPhysicsEngine) simulationSession.getPhysicsEngine();
       bulletPhysicsDebugger = new GDXBulletPhysicsAsyncDebugger(bulletPhysicsEngine.getBulletMultiBodyDynamicsWorld());
-      runAtRealtimeRate = new ImBoolean(simulationSession.getRunAtRealTimeRate());
-      playbackRealtimeRate = new ImDouble(simulationSession.getPlaybackRealTimeRate());
 
       simulationSession.addAfterPhysicsCallback(time ->
       {
          bulletPhysicsDebugger.drawBulletDebugDrawings();
+
+         if (simulationSession.getBuffer().getProperties().getCurrentIndex() == simulationSession.getBuffer().getProperties().getSize() - 1)
+         {
+            simulationSession.setSessionMode(SessionMode.PAUSE);
+         }
       });
    }
 
@@ -71,7 +82,13 @@ public class GDXSCS2PhysicsSimulator
          simulatedTerrainObject.create();
       }
 
+      changeBufferLength();
+      changeDT();
+      simulationSession.submitPlaybackRealTimeRate(playbackRealtimeRate.get());
+      simulationSession.submitRunAtRealTimeRate(runAtRealtimeRate.get());
+
       simulationSession.startSessionThread(); // TODO: Need start/stop controls?
+
 
       baseUI.get3DSceneManager().addRenderableProvider(this::getRealRenderables, GDXSceneLevel.REAL_ENVIRONMENT);
    }
@@ -101,6 +118,10 @@ public class GDXSCS2PhysicsSimulator
 
    private void renderImGuiWidgets()
    {
+      if (ImGui.inputInt("Bullet simulation dt (Hz)", dtHz))
+      {
+         changeDT();
+      }
       if (ImGui.radioButton("Simulate", simulationSession.getActiveMode() == SessionMode.RUNNING))
       {
          simulationSession.setSessionMode(SessionMode.RUNNING);
@@ -115,6 +136,18 @@ public class GDXSCS2PhysicsSimulator
       {
          simulationSession.setSessionMode(SessionMode.PLAYBACK);
       }
+      if (ImGui.inputFloat(labels.get("Buffer length (s)"), bufferLength))
+      {
+         changeBufferLength();
+      }
+      if (ImGui.sliderInt(labels.get("Buffer"), bufferIndex.getData(), 0, simulationSession.getBuffer().getProperties().getSize()))
+      {
+         simulationSession.submitBufferIndexRequest(bufferIndex.get());
+      }
+      else
+      {
+         bufferIndex.set(simulationSession.getBuffer().getProperties().getCurrentIndex());
+      }
       if (ImGui.checkbox("Run at real-time rate", runAtRealtimeRate))
       {
          simulationSession.submitRunAtRealTimeRate(runAtRealtimeRate.get());
@@ -126,6 +159,17 @@ public class GDXSCS2PhysicsSimulator
       }
       ImGui.popItemWidth();
       bulletPhysicsDebugger.renderImGuiWidgets();
+   }
+
+   private void changeBufferLength()
+   {
+      int bufferSizeRequest = (int) (bufferLength.get() / UnitConversions.hertzToSeconds(dtHz.get()));
+      simulationSession.submitBufferSizeRequest(bufferSizeRequest);
+   }
+
+   private void changeDT()
+   {
+      simulationSession.setSessionDTSeconds(UnitConversions.hertzToSeconds(dtHz.get()));
    }
 
    public SimulationSession getSession()
