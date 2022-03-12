fec0ab06f462efae80dad14fedfaccc897ba9e49
Duncan Calvert <dcalvert@ihmc.us>
2022-03-11 18:42:24 -0600

    Add door panel to door.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/object/objects/door/DoorDefinition.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/object/objects/door/DoorDefinition.java
@@ -1,27 +1,55 @@
 package us.ihmc.gdx.simulation.environment.object.objects.door;
 
+import us.ihmc.euclid.Axis3D;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.yawPitchRoll.YawPitchRoll;
+import us.ihmc.scs2.definition.robot.RevoluteJointDefinition;
 import us.ihmc.scs2.definition.robot.RigidBodyDefinition;
 import us.ihmc.scs2.definition.robot.RobotDefinition;
 import us.ihmc.scs2.definition.robot.SixDoFJointDefinition;
+import us.ihmc.scs2.definition.state.OneDoFJointState;
 import us.ihmc.scs2.definition.state.SixDoFJointState;
 
-public class DoorDefinition
+public class DoorDefinition extends RobotDefinition
 {
+   private final SixDoFJointState initialSixDoFState;
+   private final OneDoFJointState initialHingeState;
+
    public DoorDefinition()
    {
-      RobotDefinition robotDefinition = new RobotDefinition("door");
-      RigidBodyDefinition rootBodyDefinition = new RigidBodyDefinition("rootBody");
+      super("door");
+      RigidBodyDefinition rootBodyDefinition = new RigidBodyDefinition("doorRootBody");
 
-      SixDoFJointDefinition rootJointDefinition = new SixDoFJointDefinition("rootJoint");
+      SixDoFJointDefinition rootJointDefinition = new SixDoFJointDefinition("doorRootJoint");
       rootBodyDefinition.addChildJoint(rootJointDefinition);
+      initialSixDoFState = new SixDoFJointState(new YawPitchRoll(), new Point3D());
+      initialSixDoFState.setVelocity(new Vector3D(), new Vector3D());
+      rootJointDefinition.setInitialJointState(initialSixDoFState);
 
+      DoorFrameDefinition doorFrameDefinition = new DoorFrameDefinition();
+      rootJointDefinition.setSuccessor(doorFrameDefinition);
 
+      RevoluteJointDefinition doorHingeJoint = new RevoluteJointDefinition("doorHingeJoint");
+      doorHingeJoint.setAxis(Axis3D.Z);
+      doorFrameDefinition.addChildJoint(doorHingeJoint);
+      doorHingeJoint.getTransformToParent().getTranslation().add(0.0, 0.005, 0.02);
+      initialHingeState = new OneDoFJointState();
+      doorHingeJoint.setInitialJointState(initialHingeState);
 
-      robotDefinition.setRootBodyDefinition(rootBodyDefinition);
+      DoorPanelDefinition doorPanelDefinition = new DoorPanelDefinition();
+      doorHingeJoint.setSuccessor(doorPanelDefinition);
 
-      SixDoFJointState initialRootJointState = new SixDoFJointState(new YawPitchRoll(), new Point3D(0.0, 0.0, 0.0));
-      rootJointDefinition.setInitialJointState(initialRootJointState);
+      setRootBodyDefinition(rootBodyDefinition);
+   }
+
+   public SixDoFJointState getInitialSixDoFState()
+   {
+      return initialSixDoFState;
+   }
+
+   public OneDoFJointState getInitialHingeState()
+   {
+      return initialHingeState;
    }
 }
