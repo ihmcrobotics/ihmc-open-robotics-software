5874fa253df9fb69f9601face14edc1d1a31cf74
Sylvain Bertrand <sbertrand@ihmc.org>
2022-03-15 09:21:27 -0500

    :arrow_up: euclid 0.17.2

--- ihmc-avatar-interfaces/build.gradle.kts
+++ ihmc-avatar-interfaces/build.gradle.kts
@@ -25,8 +25,8 @@ mainDependencies {
    api("com.hierynomus:sshj:0.27.0")
 
    api("us.ihmc:jinput:2.0.6-ihmc2")
-   api("us.ihmc:euclid:0.17.0")
-   api("us.ihmc:euclid-geometry:0.17.0")
+   api("us.ihmc:euclid:0.17.2")
+   api("us.ihmc:euclid-geometry:0.17.2")
    api("us.ihmc:ihmc-yovariables:0.9.11")
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:0.19.8")
    api("us.ihmc:simulation-construction-set:0.21.16")
--- ihmc-common-walking-control-modules/build.gradle.kts
+++ ihmc-common-walking-control-modules/build.gradle.kts
@@ -23,8 +23,8 @@ mainDependencies {
 
    api("us.ihmc:ihmc-realtime:1.4.0")
    api("us.ihmc:ihmc-native-library-loader:1.3.1")
-   api("us.ihmc:euclid:0.17.0")
-   api("us.ihmc:euclid-geometry:0.17.0")
+   api("us.ihmc:euclid:0.17.2")
+   api("us.ihmc:euclid-geometry:0.17.2")
    api("us.ihmc:ihmc-yovariables:0.9.11")
    api("us.ihmc:ihmc-graphics-description:0.19.4")
    api("us.ihmc:ihmc-robot-models:source")
