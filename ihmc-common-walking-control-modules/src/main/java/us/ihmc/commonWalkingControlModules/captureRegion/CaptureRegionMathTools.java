be021cb86fd13709af1a4879375dd10f0770ecfb
rgriffin@ihmc.org <rgriffin@ihmc.us>
2022-03-15 13:20:02 -0500

    refreshed the capture region math tools

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/CaptureRegionMathTools.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/captureRegion/CaptureRegionMathTools.java
@@ -1,6 +1,7 @@
 package us.ihmc.commonWalkingControlModules.captureRegion;
 
 import us.ihmc.commons.MathTools;
+import us.ihmc.euclid.referenceFrame.FrameVector2D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
@@ -9,8 +10,9 @@ import us.ihmc.euclid.transform.RigidBodyTransform;
 
 public class CaptureRegionMathTools
 {
-   private final RigidBodyTransform rotation = new RigidBodyTransform();
-   private final FrameVector3D rotatedFromA = new FrameVector3D();
+   private final FrameVector2D rotatedFromA = new FrameVector2D();
+   private final FrameVector2D normalizedA = new FrameVector2D();
+   private final FrameVector2D normalizedB = new FrameVector2D();
 
    /**
    * Will return a point on a circle around the origin. The point will be in between the given directions and at
@@ -28,15 +30,15 @@ public class CaptureRegionMathTools
       directionA.checkReferenceFrameMatch(centerOfCircle.getReferenceFrame());
       alpha = MathTools.clamp(alpha, 0.0, 1.0);
 
-      double angleBetweenDirections = Math.abs(directionA.angle(directionB));
-      double angleBetweenDirectionsToSetLine = -angleBetweenDirections * alpha;
+      normalizedA.setReferenceFrame(directionA.getReferenceFrame());
+      normalizedB.setReferenceFrame(directionA.getReferenceFrame());
+      rotatedFromA.setReferenceFrame(directionA.getReferenceFrame());
 
-      rotatedFromA.setIncludingFrame(directionA, 0.0);
+      normalizedA.setAndNormalize(directionA);
+      normalizedB.setAndNormalize(directionB);
 
-      rotation.setRotationYawAndZeroTranslation(angleBetweenDirectionsToSetLine);
-      rotatedFromA.applyTransform(rotation);
-
-      rotatedFromA.scale(radius / rotatedFromA.length());
+      rotatedFromA.interpolate(normalizedA, normalizedB, alpha);
+      rotatedFromA.scale(radius);
 
       pointToPack.setIncludingFrame(rotatedFromA);
       pointToPack.add(centerOfCircle);
