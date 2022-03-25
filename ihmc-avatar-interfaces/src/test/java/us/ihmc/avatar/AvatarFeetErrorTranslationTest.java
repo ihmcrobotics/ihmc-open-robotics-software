f7fed2a6323bc38c0d92dae502c6a7d08a37d7d5
Valkyrie <valkyrie@nasa.gov>
2022-03-25 17:07:00 -0500

    Refactored some flat ground walking tests.

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/AvatarFeetErrorTranslationTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/AvatarFeetErrorTranslationTest.java
@@ -1,39 +1,34 @@
 package us.ihmc.avatar;
 
-import controller_msgs.msg.dds.FootstepDataListMessage;
-import controller_msgs.msg.dds.FootstepDataMessage;
+import static us.ihmc.robotics.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import controller_msgs.msg.dds.FootstepDataListMessage;
+import controller_msgs.msg.dds.FootstepDataMessage;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
-import us.ihmc.avatar.testTools.DRCSimulationTestHelper;
+import us.ihmc.avatar.testTools.scs2.SCS2AvatarTestingSimulation;
+import us.ihmc.avatar.testTools.scs2.SCS2AvatarTestingSimulationFactory;
 import us.ihmc.commonWalkingControlModules.controlModules.foot.FootControlModule;
 import us.ihmc.commonWalkingControlModules.controlModules.foot.FootControlModule.ConstraintType;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.WalkingHighLevelHumanoidController;
-import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.walkingController.states.WalkingStateEnum;
-import us.ihmc.commons.PrintTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.log.LogTools;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.robotics.stateMachine.core.StateTransitionCondition;
-import us.ihmc.simulationConstructionSetTools.util.HumanoidFloatingRootJointRobot;
+import us.ihmc.scs2.definition.controller.interfaces.Controller;
 import us.ihmc.simulationConstructionSetTools.util.environments.FlatGroundEnvironment;
-import us.ihmc.simulationToolkit.controllers.FootMotionOnTouchdownSlipper;
-import us.ihmc.simulationconstructionset.SimulationConstructionSet;
-import us.ihmc.simulationconstructionset.util.RobotController;
-import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.simulationconstructionset.util.simulationTesting.SimulationTestingParameters;
 import us.ihmc.tools.MemoryTools;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
 import us.ihmc.yoVariables.variable.YoDouble;
 import us.ihmc.yoVariables.variable.YoEnum;
-
-import java.util.ArrayList;
-
-import static us.ihmc.robotics.Assert.assertTrue;
 
 public abstract class AvatarFeetErrorTranslationTest implements MultiRobotTestInterface
 {
@@ -42,45 +37,45 @@ public abstract class AvatarFeetErrorTranslationTest implements MultiRobotTestIn
    private static final double stepWidth = 0.25;
 
    private SimulationTestingParameters simulationTestingParameters = SimulationTestingParameters.createFromSystemProperties();
-   private DRCSimulationTestHelper drcSimulationTestHelper;
+   private SCS2AvatarTestingSimulation simulationTestHelper;
 
    @Test
-   public void testForwardWalk() throws SimulationExceededMaximumTimeException
+   public void testForwardWalk()
    {
       FlatGroundEnvironment flatGround = new FlatGroundEnvironment();
-      String className = getClass().getSimpleName();
 
-      PrintTools.debug("simulationTestingParameters.getKeepSCSUp " + simulationTestingParameters.getKeepSCSUp());
-      drcSimulationTestHelper = new DRCSimulationTestHelper(simulationTestingParameters, getRobotModel());
-      drcSimulationTestHelper.setTestEnvironment(flatGround);
-      drcSimulationTestHelper.createSimulation(className);
+      LogTools.debug("simulationTestingParameters.getKeepSCSUp " + simulationTestingParameters.getKeepSCSUp());
+      simulationTestHelper = SCS2AvatarTestingSimulationFactory.createDefaultTestSimulation(getRobotModel(), flatGround, simulationTestingParameters);
+      simulationTestHelper.start();
       DRCRobotModel robotModel = getRobotModel();
 
       SideDependentList<SingleSupportStartCondition> singleSupportStartConditions = new SideDependentList<>();
-      SimulationConstructionSet scs = drcSimulationTestHelper.getSimulationConstructionSet();
 
       for (RobotSide robotSide : RobotSide.values)
       {
          String sidePrefix = robotSide.getCamelCaseNameForStartOfExpression();
-         @SuppressWarnings("unchecked") final YoEnum<ConstraintType> footConstraintType = (YoEnum<ConstraintType>) scs
-               .findVariable(sidePrefix + "FootControlModule", sidePrefix + "FootCurrentState");
+         @SuppressWarnings("unchecked")
+         final YoEnum<ConstraintType> footConstraintType = (YoEnum<ConstraintType>) simulationTestHelper.findVariable(sidePrefix + "FootControlModule",
+                                                                                                                      sidePrefix + "FootCurrentState");
          String prefix = robotSide.getSideNameFirstLowerCaseLetter();
-         YoBoolean trustFootSwitch = ((YoBoolean) scs.findVariable(prefix + "_footTrustFootSwitch"));
-         YoBoolean trustStateEstimatorFootSwitch = ((YoBoolean) scs.findVariable(prefix + "_footStateEstimatorTrustFootSwitch"));
+         YoBoolean trustFootSwitch = ((YoBoolean) simulationTestHelper.findVariable(prefix + "_footTrustFootSwitch"));
+         YoBoolean trustStateEstimatorFootSwitch = ((YoBoolean) simulationTestHelper.findVariable(prefix + "_footStateEstimatorTrustFootSwitch"));
          trustFootSwitch.set(false);
          trustStateEstimatorFootSwitch.set(false);
-         YoBoolean controllerDetectedTouchdown = ((YoBoolean) scs.findVariable(prefix + "_footControllerDetectedTouchdown"));
-         YoBoolean controllerStateEstimatorDetectedTouchdown = ((YoBoolean) scs.findVariable(prefix + "_footStateEstimatorControllerDetectedTouchdown"));
-         singleSupportStartConditions.put(robotSide, new SingleSupportStartCondition(footConstraintType, controllerDetectedTouchdown,
-                                                                                     controllerStateEstimatorDetectedTouchdown));
+         YoBoolean controllerDetectedTouchdown = ((YoBoolean) simulationTestHelper.findVariable(prefix + "_footControllerDetectedTouchdown"));
+         YoBoolean controllerStateEstimatorDetectedTouchdown = ((YoBoolean) simulationTestHelper.findVariable(prefix
+               + "_footStateEstimatorControllerDetectedTouchdown"));
+         singleSupportStartConditions.put(robotSide,
+                                          new SingleSupportStartCondition(footConstraintType,
+                                                                          controllerDetectedTouchdown,
+                                                                          controllerStateEstimatorDetectedTouchdown));
       }
 
-      YoDouble time = drcSimulationTestHelper.getRobot().getYoTime();
+      YoDouble time = simulationTestHelper.getSimulationSession().getTime();
       double swing = robotModel.getWalkingControllerParameters().getDefaultSwingTime();
       EarlyTouchdownController earlyTouchdownController = new EarlyTouchdownController(singleSupportStartConditions, swing, time);
       earlyTouchdownController.setFractionToTriggerTouchdown(0.75);
-      drcSimulationTestHelper.getRobot().setController(earlyTouchdownController);
-
+      simulationTestHelper.getRobot().getControllerManager().addController(earlyTouchdownController);
 
       ThreadTools.sleep(1000);
 
@@ -93,7 +88,7 @@ public abstract class AvatarFeetErrorTranslationTest implements MultiRobotTestIn
       footMessage.setOffsetFootstepsHeightWithExecutionError(true);
       ArrayList<Point3D> rootLocations = new ArrayList<>();
 
-      PelvisCheckpointChecker controllerSpy = new PelvisCheckpointChecker(drcSimulationTestHelper);
+      PelvisCheckpointChecker controllerSpy = new PelvisCheckpointChecker(simulationTestHelper);
 
       for (int currentStep = 0; currentStep < numberOfSteps; currentStep++)
       {
@@ -114,11 +109,11 @@ public abstract class AvatarFeetErrorTranslationTest implements MultiRobotTestIn
       int steps = footMessage.getFootstepDataList().size();
 
       controllerSpy.setFootStepCheckPoints(rootLocations, stepLength, stepWidth);
-      drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(1.0);
-      drcSimulationTestHelper.publishToController(footMessage);
+      simulationTestHelper.simulateAndWait(1.0);
+      simulationTestHelper.publishToController(footMessage);
       double simulationTime = initialTransfer + (transfer + swing) * steps + 1.0;
 
-      assertTrue(drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(simulationTime));
+      assertTrue(simulationTestHelper.simulateAndWait(simulationTime));
       controllerSpy.assertCheckpointsReached();
    }
 
@@ -126,7 +121,7 @@ public abstract class AvatarFeetErrorTranslationTest implements MultiRobotTestIn
    {
       Point3D cameraFix = new Point3D(0.0, 0.0, 1.0);
       Point3D cameraPosition = new Point3D(0.0, 10.0, 1.0);
-      drcSimulationTestHelper.setupCameraForUnitTest(cameraFix, cameraPosition);
+      simulationTestHelper.setCamera(cameraFix, cameraPosition);
    }
 
    private class SingleSupportStartCondition implements StateTransitionCondition
@@ -135,7 +130,9 @@ public abstract class AvatarFeetErrorTranslationTest implements MultiRobotTestIn
       private final YoBoolean touchdownDetected;
       private final YoBoolean estimatorTouchdownDetection;
 
-      public SingleSupportStartCondition(YoEnum<FootControlModule.ConstraintType> footConstraintType, YoBoolean touchdownDetected, YoBoolean estimatorTouchdownDetection)
+      public SingleSupportStartCondition(YoEnum<FootControlModule.ConstraintType> footConstraintType,
+                                         YoBoolean touchdownDetected,
+                                         YoBoolean estimatorTouchdownDetection)
       {
          this.footConstraintType = footConstraintType;
          this.touchdownDetected = touchdownDetected;
@@ -170,17 +167,17 @@ public abstract class AvatarFeetErrorTranslationTest implements MultiRobotTestIn
       }
 
       // Do this here in case a test fails. That way the memory will be recycled.
-      if (drcSimulationTestHelper != null)
+      if (simulationTestHelper != null)
       {
-         drcSimulationTestHelper.destroySimulation();
-         drcSimulationTestHelper = null;
+         simulationTestHelper.finishTest();
+         simulationTestHelper = null;
       }
 
       simulationTestingParameters = null;
       MemoryTools.printCurrentMemoryUsageAndReturnUsedMemoryInMB(getClass().getSimpleName() + " after test.");
    }
 
-   private class EarlyTouchdownController implements RobotController
+   private class EarlyTouchdownController implements Controller
    {
       private final YoRegistry registry = new YoRegistry(getClass().getSimpleName());
       private final SideDependentList<SingleSupportStartCondition> startConditions;
