7d4125fbf52d7981dc6c2760eb2ead68b2dae64a
smccrory <smccrory@ihmc.us>
2022-03-28 16:44:08 -0500

    Additional index fixes for height map test and adding random offsets to height

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/bodyPath/BodyPathCollisionDetectorTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/bodyPath/BodyPathCollisionDetectorTest.java
@@ -51,38 +51,39 @@ public class BodyPathCollisionDetectorTest
       collisionDetector.initialize(heightMapData.getGridResolutionXY(), 0.15, 0.3);
 
       heightMapData.reset();
-      heightMapData.setHeightAt(0.0, 0.0, 0.201);
-      collision = collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), 1, 0.0, 0.2);
+      randomHeight = EuclidCoreRandomTools.nextDouble(random, 10.0);
+      heightMapData.setHeightAt(0.0, 0.0, randomHeight + 0.201);
+      collision = collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), 1, randomHeight, 0.2);
       Assertions.assertTrue(collision, "Body path collision detector failed");
 
       Point2D[] collidingPoints = new Point2D[]{new Point2D(0.0, 0.0), new Point2D(0.1, 0.0), new Point2D(0.0, -0.1), new Point2D(0.0, 0.1), new Point2D(0.1, 0.1), new Point2D(-0.1, -0.1), new Point2D(-0.1, 0.0)};
       Point2D[] nonCollidingPoints = new Point2D[]{new Point2D(0.1, 0.1), new Point2D(-0.1, -0.1), new Point2D(0.2, 0.0)};
 
-      for (int yawIndex : new int[]{1, 5})
+      for (int yawIndex : new int[]{2, 10})
       {
          heightMapData.reset();
          for (int i = 0; i < collidingPoints.length; i++)
          {
-            heightMapData.setHeightAt(collidingPoints[i].getX(), collidingPoints[i].getY(), 0.201);
+            heightMapData.setHeightAt(collidingPoints[i].getX(), collidingPoints[i].getY(), randomHeight + 0.201);
          }
-         Assertions.assertTrue(collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), yawIndex, 0.0, 0.2), "Body collision detector failed");
+         Assertions.assertTrue(collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), yawIndex, randomHeight, 0.2), "Body collision detector failed");
 
          heightMapData.reset();
          for (int i = 0; i < nonCollidingPoints.length; i++)
          {
-            heightMapData.setHeightAt(nonCollidingPoints[i].getX(), nonCollidingPoints[i].getY(), 0.201);
+            heightMapData.setHeightAt(nonCollidingPoints[i].getX(), nonCollidingPoints[i].getY(), randomHeight + 0.201);
          }
-         Assertions.assertFalse(collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), yawIndex, 0.0, 0.2), "Body collision detector failed");
+         Assertions.assertFalse(collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), yawIndex, randomHeight, 0.2), "Body collision detector failed");
       }
 
-      for (int yawIndex : new int[]{3, 7})
+      for (int yawIndex : new int[]{6, 14})
       {
          heightMapData.reset();
          for (int i = 0; i < collidingPoints.length; i++)
          {
-            heightMapData.setHeightAt(collidingPoints[i].getY(), -collidingPoints[i].getX(), 0.201);
+            heightMapData.setHeightAt(collidingPoints[i].getY(), -collidingPoints[i].getX(), randomHeight + 0.201);
          }
-         Assertions.assertTrue(collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), yawIndex, 0.0, 0.2), "Body collision detector failed");
+         Assertions.assertTrue(collisionDetector.collisionDetected(heightMapData, new BodyPathLatticePoint(0.0, 0.0), yawIndex, randomHeight, 0.2), "Body collision detector failed");
       }
    }
 
