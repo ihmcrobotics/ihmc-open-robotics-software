5e1c2249f2bb7a33c5622143e28e193cf18ca59a
Robert Griffin <rgriffin@ihmc.us>
2022-04-03 09:28:32 -0500

    shuffled some classes and started working on a dynamic state inspector

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/toeOff/ICPPlanToeOffCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/toeOff/ICPPlanToeOffCalculator.java
@@ -1,35 +1,51 @@
-package us.ihmc.commonWalkingControlModules.controlModules.foot.toeOffCalculator;
+package us.ihmc.commonWalkingControlModules.controlModules.foot.toeOff;
 
-import us.ihmc.euclid.referenceFrame.FrameLineSegment2D;
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.YoContactPoint;
+import us.ihmc.commonWalkingControlModules.bipedSupportPolygons.YoPlaneContactState;
+import us.ihmc.euclid.geometry.LineSegment2D;
 import us.ihmc.euclid.referenceFrame.FramePoint2D;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameLineSegment2DBasics;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
-import us.ihmc.humanoidRobotics.bipedSupportPolygons.ContactableFoot;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
-import us.ihmc.robotics.geometry.*;
+import us.ihmc.robotics.contactable.ContactablePlaneBody;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
-public class SimpleToeOffCalculator implements ToeOffCalculator
+import java.util.List;
+
+public class ICPPlanToeOffCalculator implements ToeOffCalculator
 {
    private final YoRegistry registry = new YoRegistry(getClass().getSimpleName());
-   private static final String namePrefix = "simple";
+   private static final String namePrefix = "icpProj";
 
-   private final SideDependentList<ContactableFoot> feet;
+   private final SideDependentList<List<YoContactPoint>> contactPoints = new SideDependentList<>();
+
+   private final FramePoint3D exitCMP = new FramePoint3D();
+   private final FramePoint2D exitCMP2d = new FramePoint2D();
 
    private final FramePoint2D toeOffContactPoint2d = new FramePoint2D();
-   private final FrameLineSegment2D toeOffContactLine2d = new FrameLineSegment2D();
+   private final LineSegment2D toeOffContactLine2d = new LineSegment2D();
+
+   private final SideDependentList<ReferenceFrame> soleFrames = new SideDependentList<>();
 
    private final YoBoolean hasComputedToeOffContactPoint;
    private final YoBoolean hasComputedToeOffContactLine;
 
-   public SimpleToeOffCalculator(SideDependentList<ContactableFoot> feet, YoRegistry parentRegistry)
+   public ICPPlanToeOffCalculator(SideDependentList<YoPlaneContactState> contactStates, SideDependentList<? extends ContactablePlaneBody> feet,
+         YoRegistry parentRegistry)
    {
-      this.feet = feet;
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         ReferenceFrame soleFrame = feet.get(robotSide).getSoleFrame();
+         soleFrames.put(robotSide, soleFrame);
+
+         contactPoints.put(robotSide, contactStates.get(robotSide).getContactPoints());
+      }
 
       hasComputedToeOffContactPoint = new YoBoolean(namePrefix + "HasComputedToeOffContactPoint", registry);
       hasComputedToeOffContactLine = new YoBoolean(namePrefix + "HasComputedToeOffContactLine", registry);
@@ -40,25 +56,29 @@ public class SimpleToeOffCalculator implements ToeOffCalculator
    @Override
    public ToeOffEnum getEnum()
    {
-      return ToeOffEnum.SIMPLE;
+      return ToeOffEnum.ICP_PLAN;
    }
 
    @Override
    public void clear()
    {
+      exitCMP2d.setToNaN();
       hasComputedToeOffContactPoint.set(false);
-      hasComputedToeOffContactLine.set(false);
    }
 
    @Override
    public void setExitCMP(FramePoint3DReadOnly exitCMP, RobotSide trailingLeg)
    {
+      ReferenceFrame soleFrame = soleFrames.get(trailingLeg);
+      this.exitCMP.setIncludingFrame(exitCMP);
+      this.exitCMP.changeFrame(soleFrame);
+      exitCMP2d.setToZero(soleFrame);
+      exitCMP2d.setIncludingFrame(this.exitCMP);
    }
 
    @Override
    public void computeToeOffContactPoint(FramePoint2DReadOnly desiredCMP, RobotSide trailingLeg)
    {
-      feet.get(trailingLeg).getToeOffContactPoint(toeOffContactPoint2d);
       hasComputedToeOffContactPoint.set(true);
    }
 
@@ -74,7 +94,6 @@ public class SimpleToeOffCalculator implements ToeOffCalculator
    @Override
    public void computeToeOffContactLine(FramePoint2DReadOnly desiredCMP, RobotSide trailingLeg)
    {
-      feet.get(trailingLeg).getToeOffContactLine(toeOffContactLine2d);
       hasComputedToeOffContactLine.set(true);
    }
 
---
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/toeOff/OverhauledToeOffManager.java
@@ -0,0 +1,45 @@
+package us.ihmc.commonWalkingControlModules.controlModules.foot.toeOff;
+
+import us.ihmc.commonWalkingControlModules.configurations.SteppingParameters;
+import us.ihmc.commonWalkingControlModules.configurations.ToeOffParameters;
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.commonWalkingControlModules.controlModules.foot.ToeOffStepPositionInspector;
+import us.ihmc.humanoidRobotics.footstep.Footstep;
+import us.ihmc.mecano.frames.MovingReferenceFrame;
+import us.ihmc.robotics.robotSide.SideDependentList;
+import us.ihmc.yoVariables.registry.YoRegistry;
+
+public class OverhauledToeOffManager
+{
+   private Footstep nextFootstep;
+   private Footstep nextNextFootstep;
+
+   private final ToeOffStepPositionInspector stepPositionInspector;
+   private final LegJointLimitsInspector jointLimitsInspector;
+
+   public OverhauledToeOffManager(WalkingControllerParameters walkingControllerParameters,
+                                  SideDependentList<MovingReferenceFrame> soleZUpFrames,
+                                  YoRegistry parentRegistry)
+   {
+      ToeOffParameters toeOffParameters = walkingControllerParameters.getToeOffParameters();
+      SteppingParameters steppingParameters = walkingControllerParameters.getSteppingParameters();
+
+      double footLength = steppingParameters.getFootBackwardOffset() + steppingParameters.getFootForwardOffset();
+
+      stepPositionInspector = new ToeOffStepPositionInspector(soleZUpFrames,
+                                                              toeOffParameters,
+                                                              steppingParameters.getInPlaceWidth(),
+                                                              footLength,
+                                                              parentRegistry);
+      jointLimitsInspector = new LegJointLimitsInspector(toeOffParameters, parentRegistry);
+   }
+
+
+   public void submitNextFootstep(Footstep nextFootstep, Footstep nextNextFootstep)
+   {
+      this.nextFootstep = nextFootstep;
+      this.nextNextFootstep = nextNextFootstep;
+   }
+
+
+}
