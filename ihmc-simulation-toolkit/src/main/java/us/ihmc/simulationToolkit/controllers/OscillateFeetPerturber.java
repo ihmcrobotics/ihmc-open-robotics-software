7d51028346d9d332311ea1b1d61e1de942f9b7f1
Sylvain Bertrand <sbertrand@ihmc.org>
2022-04-07 19:43:56 -0500

    Refactored DRCObstacleCourseFlatWithErrorsTest.

--- ihmc-simulation-toolkit/src/main/java/us/ihmc/simulationToolkit/controllers/OscillateFeetPerturber.java
+++ ihmc-simulation-toolkit/src/main/java/us/ihmc/simulationToolkit/controllers/OscillateFeetPerturber.java
@@ -1,20 +1,24 @@
 package us.ihmc.simulationToolkit.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.tuple3D.Vector3D;
-import us.ihmc.robotics.robotController.ModularRobotController;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
-import us.ihmc.simulationConstructionSetTools.util.HumanoidFloatingRootJointRobot;
+import us.ihmc.scs2.definition.controller.interfaces.Controller;
+import us.ihmc.scs2.simulation.robot.Robot;
+import us.ihmc.scs2.simulation.robot.trackers.GroundContactPoint;
 import us.ihmc.simulationConstructionSetTools.util.perturbance.GroundContactPointsSlipper;
-import us.ihmc.simulationconstructionset.GroundContactPoint;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameYawPitchRoll;
+import us.ihmc.yoVariables.registry.YoRegistry;
 
-public class OscillateFeetPerturber extends ModularRobotController
+public class OscillateFeetPerturber implements Controller
 {
+   private final YoRegistry registry = new YoRegistry("NoisilyShakeFeetPerturber");
+   private final List<Controller> subControllers = new ArrayList<>();
    private final SideDependentList<GroundContactPointsSlipper> groundContactPointsSlippers;
    private final SideDependentList<List<GroundContactPoint>> groundContactPointsMap = new SideDependentList<List<GroundContactPoint>>();
 
@@ -23,34 +27,33 @@ public class OscillateFeetPerturber extends ModularRobotController
 
    private final SideDependentList<YoFrameVector3D> translationPhases = new SideDependentList<YoFrameVector3D>();
    private final SideDependentList<YoFrameVector3D> rotationPhasesEuler = new SideDependentList<YoFrameVector3D>();
-   
+
    private final SideDependentList<double[]> translationFrequenciesHz = new SideDependentList<double[]>();
    private final SideDependentList<double[]> rotationFrequenciesHzYawPitchRoll = new SideDependentList<double[]>();
 
    private final double deltaT;
-   
-   public OscillateFeetPerturber(HumanoidFloatingRootJointRobot robot, double deltaT)
+
+   public OscillateFeetPerturber(Robot robot, SideDependentList<String> footNames, double deltaT)
    {
-      super("NoisilyShakeFeetPerturber");
       String name = "ShakeFeet";
 
       this.deltaT = deltaT;
-      
+
       for (RobotSide robotSide : RobotSide.values())
       {
-         groundContactPointsMap.put(robotSide, robot.getFootGroundContactPoints(robotSide));
-         
+         groundContactPointsMap.put(robotSide, robot.getRigidBody(footNames.get(robotSide)).getParentJoint().getAuxialiryData().getGroundContactPoints());
+
          YoFrameVector3D translationPhase = new YoFrameVector3D(robotSide.getLowerCaseName() + "TranslationPhase", null, registry);
          YoFrameVector3D rotationPhaseEuler = new YoFrameVector3D(robotSide.getLowerCaseName() + "RotationPhase", null, registry);
-         
+
          double[] translationalFreqHz = new double[3];
          double[] rotationFreqHzYawPitchRoll = new double[3];
-         
-         translationPhases.put(robotSide,  translationPhase);
-         rotationPhasesEuler.put(robotSide,  rotationPhaseEuler);
-         
-         translationFrequenciesHz.put(robotSide,  translationalFreqHz);
-         rotationFrequenciesHzYawPitchRoll.put(robotSide,  rotationFreqHzYawPitchRoll);
+
+         translationPhases.put(robotSide, translationPhase);
+         rotationPhasesEuler.put(robotSide, rotationPhaseEuler);
+
+         translationFrequenciesHz.put(robotSide, translationalFreqHz);
+         rotationFrequenciesHzYawPitchRoll.put(robotSide, rotationFreqHzYawPitchRoll);
       }
 
       translationMagnitude = new YoFrameVector3D(name + "TranslationMagnitude", ReferenceFrame.getWorldFrame(), registry);
@@ -58,11 +61,11 @@ public class OscillateFeetPerturber extends ModularRobotController
 
       GroundContactPointsSlipper leftSlipper = new GroundContactPointsSlipper("left");
       GroundContactPointsSlipper rightSlipper = new GroundContactPointsSlipper("right");
-      
+
       groundContactPointsSlippers = new SideDependentList<GroundContactPointsSlipper>(leftSlipper, rightSlipper);
-      
-      this.addRobotController(leftSlipper);
-      this.addRobotController(rightSlipper);
+
+      subControllers.add(leftSlipper);
+      subControllers.add(rightSlipper);
    }
 
    public void setTranslationMagnitude(double[] translation)
@@ -74,21 +77,21 @@ public class OscillateFeetPerturber extends ModularRobotController
    {
       this.rotationMagnitude.set(rotationYawPitchRoll);
    }
-   
+
    public void setTranslationFrequencyHz(RobotSide robotSide, double[] freqHz)
    {
       translationFrequenciesHz.put(robotSide, freqHz);
    }
-   
+
    public void setRotationFrequencyHzYawPitchRoll(RobotSide robotSide, double[] freqHz)
    {
       rotationFrequenciesHzYawPitchRoll.put(robotSide, freqHz);
    }
-   
+
    @Override
    public void doControl()
    {
-      super.doControl();
+      subControllers.forEach(Controller::doControl);
 
       for (RobotSide robotSide : RobotSide.values())
       {
@@ -104,64 +107,63 @@ public class OscillateFeetPerturber extends ModularRobotController
 
    }
 
-
    private void startSlipping(RobotSide robotSide)
    {
       YoFrameVector3D translationPhase = translationPhases.get(robotSide);
       YoFrameVector3D rotationPhaseEuler = rotationPhasesEuler.get(robotSide);
-      
+
       double[] translationFreqHz = translationFrequenciesHz.get(robotSide);
       double[] rotationFreqHzYawPitchRoll = rotationFrequenciesHzYawPitchRoll.get(robotSide);
-      
+
       GroundContactPointsSlipper groundContactPointsSlipper = groundContactPointsSlippers.get(robotSide);
 
       groundContactPointsSlipper.setGroundContactPoints(groundContactPointsMap.get(robotSide));
       groundContactPointsSlipper.setPercentToSlipPerTick(1.0); //nextSlipPercentSlipPerTick.getDoubleValue());
       groundContactPointsSlipper.setDoSlip(true);
-      
-      translationPhase.setX(translationPhase.getX() + 2.0  * Math.PI * translationFreqHz[0] * deltaT);
-      translationPhase.setY(translationPhase.getY() + 2.0  * Math.PI * translationFreqHz[1] * deltaT);
-      translationPhase.setZ(translationPhase.getZ() + 2.0  * Math.PI * translationFreqHz[2] * deltaT);
 
-      rotationPhaseEuler.setX(rotationPhaseEuler.getX() + 2.0  * Math.PI * rotationFreqHzYawPitchRoll[2] * deltaT);
-      rotationPhaseEuler.setY(rotationPhaseEuler.getY() + 2.0  * Math.PI * rotationFreqHzYawPitchRoll[1] * deltaT);
-      rotationPhaseEuler.setZ(rotationPhaseEuler.getZ() + 2.0  * Math.PI * rotationFreqHzYawPitchRoll[0] * deltaT);
+      translationPhase.setX(translationPhase.getX() + 2.0 * Math.PI * translationFreqHz[0] * deltaT);
+      translationPhase.setY(translationPhase.getY() + 2.0 * Math.PI * translationFreqHz[1] * deltaT);
+      translationPhase.setZ(translationPhase.getZ() + 2.0 * Math.PI * translationFreqHz[2] * deltaT);
 
-      
+      rotationPhaseEuler.setX(rotationPhaseEuler.getX() + 2.0 * Math.PI * rotationFreqHzYawPitchRoll[2] * deltaT);
+      rotationPhaseEuler.setY(rotationPhaseEuler.getY() + 2.0 * Math.PI * rotationFreqHzYawPitchRoll[1] * deltaT);
+      rotationPhaseEuler.setZ(rotationPhaseEuler.getZ() + 2.0 * Math.PI * rotationFreqHzYawPitchRoll[0] * deltaT);
+
       Vector3D nextTranslationToSlip = new Vector3D(translationMagnitude);
       nextTranslationToSlip.setX(nextTranslationToSlip.getX() * (2.0 * Math.PI * translationFreqHz[0] * Math.sin(translationPhase.getX()) * deltaT));
       nextTranslationToSlip.setY(nextTranslationToSlip.getY() * (2.0 * Math.PI * translationFreqHz[1] * Math.sin(translationPhase.getY()) * deltaT));
       nextTranslationToSlip.setZ(nextTranslationToSlip.getZ() * (2.0 * Math.PI * translationFreqHz[2] * Math.sin(translationPhase.getZ()) * deltaT));
-      
+
       Vector3D nextRotationToSlipEulerAngles = new Vector3D();
       rotationMagnitude.getEuler(nextRotationToSlipEulerAngles);
-      nextRotationToSlipEulerAngles.setX(nextRotationToSlipEulerAngles.getX() * (2.0 * Math.PI * rotationFreqHzYawPitchRoll[2] * Math.sin(rotationPhaseEuler.getX()) * deltaT));
-      nextRotationToSlipEulerAngles.setY(nextRotationToSlipEulerAngles.getY() * (2.0 * Math.PI * rotationFreqHzYawPitchRoll[1] * Math.sin(rotationPhaseEuler.getY()) * deltaT));
-      nextRotationToSlipEulerAngles.setZ(nextRotationToSlipEulerAngles.getZ() * (2.0 * Math.PI * rotationFreqHzYawPitchRoll[0] * Math.sin(rotationPhaseEuler.getZ()) * deltaT));
-      
+      nextRotationToSlipEulerAngles.setX(nextRotationToSlipEulerAngles.getX()
+            * (2.0 * Math.PI * rotationFreqHzYawPitchRoll[2] * Math.sin(rotationPhaseEuler.getX()) * deltaT));
+      nextRotationToSlipEulerAngles.setY(nextRotationToSlipEulerAngles.getY()
+            * (2.0 * Math.PI * rotationFreqHzYawPitchRoll[1] * Math.sin(rotationPhaseEuler.getY()) * deltaT));
+      nextRotationToSlipEulerAngles.setZ(nextRotationToSlipEulerAngles.getZ()
+            * (2.0 * Math.PI * rotationFreqHzYawPitchRoll[0] * Math.sin(rotationPhaseEuler.getZ()) * deltaT));
+
       groundContactPointsSlipper.setSlipTranslation(nextTranslationToSlip);
       groundContactPointsSlipper.setSlipRotationEulerAngles(nextRotationToSlipEulerAngles);
    }
 
    private void stopSlipping(RobotSide robotSide)
    {
-       YoFrameVector3D translationPhase = translationPhases.get(robotSide);
-       translationPhase.setToZero();
-       
-       YoFrameVector3D rotationPhaseEuler = rotationPhasesEuler.get(robotSide);
-       rotationPhaseEuler.setToZero();
+      YoFrameVector3D translationPhase = translationPhases.get(robotSide);
+      translationPhase.setToZero();
+
+      YoFrameVector3D rotationPhaseEuler = rotationPhasesEuler.get(robotSide);
+      rotationPhaseEuler.setToZero();
    }
-   
-   
+
    private boolean footTouchedDown(RobotSide robotSide)
    {
       for (GroundContactPoint groundContactPoint : groundContactPointsMap.get(robotSide))
       {
-         if (groundContactPoint.isInContact())
+         if (groundContactPoint.getInContact().getValue())
             return true;
       }
 
       return false;
    }
 }
-
