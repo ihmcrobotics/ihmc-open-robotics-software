d78a5842106fbbfe184fd88c0a5e35838c99b661
brandon <brandon@sager>
2022-04-13 17:11:22 -0500

    added a better example in the L515DepthImageReceiver comments

--- ihmc-perception/src/main/java/us/ihmc/perception/realsense/L515DepthImageReceiver.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/realsense/L515DepthImageReceiver.java
@@ -10,12 +10,12 @@ public interface L515DepthImageReceiver
     * This callback gets called from the L515 update method after we get a depth image
     * @param depthFrame - the raw rs2_frame that can be used with the realsense2 library
     * @param depthData - the depth data as unsigned ints. This is a 1D array that represents a 2D depth image.
-
     * 
-    * When you try to get data from the ShortBuffer, be sure to call:
-    * int val = Short.toUnsignedInt(depthData.get(index));
+    * Example (Note the Explicit call to deal with unsigned numbers. Java doesn't like them):
     * 
     * int index = y * l515.getDepthWidth() + x;
+    * int val = Short.toUnsignedInt(depthData.get(index));
+    * double distance = val * l515.getDepthToMeterConversion();
     */
    public void receivedDepthData(rs2_frame depthFrame, ShortBuffer depthData);
 }
