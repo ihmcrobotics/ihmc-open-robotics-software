a1e248c2075c1027e7361ab55006cd96c3d3d408
Sylvain Bertrand <sbertrand@ihmc.org>
2022-04-13 14:32:26 -0500

    Refactored KinematicsPlanningBehaviorTest.

--- valkyrie/src/test/java/us/ihmc/valkyrie/behaviorTests/ValkyrieKinematicsPlanningBehaviorTest.java
+++ valkyrie/src/test/java/us/ihmc/valkyrie/behaviorTests/ValkyrieKinematicsPlanningBehaviorTest.java
@@ -1,7 +1,5 @@
 package us.ihmc.valkyrie.behaviorTests;
 
-import java.io.IOException;
-
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +7,6 @@ import us.ihmc.avatar.behaviorTests.KinematicsPlanningBehaviorTest;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
-import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
 import us.ihmc.valkyrie.ValkyrieRobotModel;
 
 public class ValkyrieKinematicsPlanningBehaviorTest extends KinematicsPlanningBehaviorTest
@@ -31,7 +28,7 @@ public class ValkyrieKinematicsPlanningBehaviorTest extends KinematicsPlanningBe
    @Tag("humanoid-toolbox")
    @Override
    @Test
-   public void testReachToDoorKnob() throws SimulationExceededMaximumTimeException, IOException
+   public void testReachToDoorKnob()
    {
       super.testReachToDoorKnob();
    }
@@ -39,7 +36,7 @@ public class ValkyrieKinematicsPlanningBehaviorTest extends KinematicsPlanningBe
    @Tag("humanoid-behaviors-slow")
    @Override
    @Test
-   public void testSingleKeyFrameInput() throws SimulationExceededMaximumTimeException, IOException
+   public void testSingleKeyFrameInput()
    {
       super.testSingleKeyFrameInput();
    }
