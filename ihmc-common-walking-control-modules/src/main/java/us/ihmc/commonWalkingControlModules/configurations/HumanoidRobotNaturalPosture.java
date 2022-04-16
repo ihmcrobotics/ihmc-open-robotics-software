11e19dc26cc13aca91eeed88dc529ac0b3023d62
Gabriel Nelson <gabrielmnelson@gmail.com>
2022-04-16 10:21:58 -0400

    Add instance of abstract getNaturalPostureQuaternionrtBase(...)

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/HumanoidRobotNaturalPosture.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/HumanoidRobotNaturalPosture.java
@@ -7,12 +7,18 @@ import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
 
 public interface HumanoidRobotNaturalPosture
 {
+   public abstract void initialize();
+   
+   public abstract Quaternion getNominalStandingPoseQoffset();
+   
    public abstract void setNaturalPostureOffset(QuaternionReadOnly Qoffset);
 
    public abstract void compute(double[] q, QuaternionReadOnly Qbase);
 
    public abstract Quaternion getNaturalPostureQuaternion();
 
+   public abstract Quaternion getNaturalPostureQuaternionrtBase();
+   
    public abstract DMatrixRMaj getNaturalPostureJacobian();
 
 }
