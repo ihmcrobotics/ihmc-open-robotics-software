fc872bf1e382a14cc33a64f013a4b89fc8c87bcd
Duncan Calvert <dcalvert@ihmc.us>
2022-04-16 21:56:18 -0500

    Use KryoMessager to stream RAM and CPU usage.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/missionControl/ImGuiSSHJMissionControlUI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/missionControl/ImGuiSSHJMissionControlUI.java
@@ -7,12 +7,22 @@ import imgui.type.ImInt;
 import imgui.type.ImString;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Signal;
+import org.apache.commons.lang3.tuple.MutablePair;
+import std_msgs.msg.dds.Float64MultiArray;
 import us.ihmc.avatar.ros2.networkTest.SSHJTools;
+import us.ihmc.behaviors.tools.MessagerHelper;
 import us.ihmc.commons.Conversions;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.exception.ExceptionTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.gdx.imgui.ImGuiGlfwWindow;
+import us.ihmc.gdx.ui.tools.ImGuiMessagerManagerWidget;
+import us.ihmc.gdx.ui.yo.ImPlotDoublePlotLine;
+import us.ihmc.gdx.ui.yo.ImPlotPlot;
+import us.ihmc.missionControl.MissionControlService;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ImGuiSSHJMissionControlUI
 {
@@ -21,12 +31,23 @@ public class ImGuiSSHJMissionControlUI
    private final ImInt bufferSize = new ImInt(Conversions.megabytesToBytes(2));
    private final ImString command = new ImString(10000);
    private final ImDouble timeout = new ImDouble(0.0);
+   private final MessagerHelper messagerHelper;
    private int exitStatus = -1;
    private final SSHJInputStream standardOut = new SSHJInputStream();
    private final SSHJInputStream standardError = new SSHJInputStream();
    private Session.Command sshjCommand;
    private final ImGuiConsoleArea consoleArea = new ImGuiConsoleArea();
    private Thread runThread;
+//   private final KryoMessager kryoMessagerClient;
+   private final ImGuiMessagerManagerWidget messagerManagerWidget;
+   private final AtomicReference<MutablePair<Double, Double>> ramUsageSubscription;
+   private final AtomicReference<ArrayList<Double>> cpuUsagesSubscription;
+
+   private ImPlotPlot ramPlot = new ImPlotPlot();
+   private ImPlotDoublePlotLine ramUsagePlotLine = new ImPlotDoublePlotLine("RAM Usage GiB");
+   private ImPlotDoublePlotLine ramTotalPlotLine = new ImPlotDoublePlotLine("RAM Total GiB");
+   private ImPlotPlot cpuPlot = new ImPlotPlot();
+//   private final ROS2Input<Float64MultiArray> ramUsageSubscription;
 
    public ImGuiSSHJMissionControlUI()
    {
@@ -39,10 +60,52 @@ public class ImGuiSSHJMissionControlUI
       standardOut.resize(bufferSize.get());
       standardError.resize(bufferSize.get());
       command.set("sudo journalctl -ef -u mission-control-2");
+
+//      ROS2Node ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "mission_control_ui");
+//      ROS2Helper ros2Helper = new ROS2Helper(ros2Node);
+//      ramUsageSubscription = ros2Helper.subscribe(MissionControlService.RAM_USAGE_TOPIC);
+
+//      kryoMessagerClient = KryoMessager.createClient(MissionControlService.API.create(),
+//                                                     REMOTE_HOSTNAME,
+//                                                     MissionControlService.API.PORT,
+//                                                     "mission_control_ui",
+//                                                     100);
+
+      ramPlot.getPlotLines().add(ramUsagePlotLine);
+      ramPlot.getPlotLines().add(ramTotalPlotLine);
+
+      messagerHelper = new MessagerHelper(MissionControlService.API.create());
+
+      ramUsageSubscription = messagerHelper.subscribeViaReference(MissionControlService.API.RAMUsage, MutablePair.of(0.0, 1.0));
+      cpuUsagesSubscription = messagerHelper.subscribeViaReference(MissionControlService.API.CPUUsages, new ArrayList<>());
+
+      messagerManagerWidget = new ImGuiMessagerManagerWidget(messagerHelper, () -> REMOTE_HOSTNAME, MissionControlService.API.PORT);
+      //      kryoMessagerClient.
    }
 
    private void renderImGuiWidgets()
    {
+      messagerManagerWidget.renderImGuiWidgets();
+
+      MutablePair<Double, Double> ramUsage = ramUsageSubscription.get();
+      double used = ramUsage.getLeft();
+      double total = ramUsage.getRight();
+      ramUsagePlotLine.addValue(used);
+      ramTotalPlotLine.addValue(total);
+      ramPlot.render();
+
+      ArrayList<Double> cpuUsages = cpuUsagesSubscription.get();
+      for (int i = 0; i < cpuUsages.size(); i++)
+      {
+         if (cpuPlot.getPlotLines().size() == i)
+         {
+            cpuPlot.getPlotLines().add(new ImPlotDoublePlotLine("CPU Core " + i + " %"));
+         }
+
+         ((ImPlotDoublePlotLine)cpuPlot.getPlotLines().get(i)).addValue(cpuUsages.get(i));
+      }
+      cpuPlot.render();
+
       int inputTextFlags = ImGuiInputTextFlags.None;
       inputTextFlags |= ImGuiInputTextFlags.CallbackResize;
       ImGui.inputText("Command", command, inputTextFlags);
