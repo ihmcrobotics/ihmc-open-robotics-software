fbe25001df5be9ea61817d0c028f919aa2ae0e2a
Duncan Calvert <dcalvert@ihmc.us>
2022-04-16 19:01:32 -0500

    Add Mission Control 2 service and messing with resource monitoring.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/missionControl/ImGuiSSHJMissionControlUI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/missionControl/ImGuiSSHJMissionControlUI.java
@@ -1,9 +1,6 @@
 package us.ihmc.gdx.ui.missionControl;
 
-import com.sun.management.OperatingSystemMXBean;
 import imgui.ImGui;
-import imgui.extension.texteditor.TextEditor;
-import imgui.extension.texteditor.TextEditorLanguageDefinition;
 import imgui.flag.ImGuiInputTextFlags;
 import imgui.type.ImDouble;
 import imgui.type.ImInt;
@@ -12,15 +9,10 @@ import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Signal;
 import us.ihmc.avatar.ros2.networkTest.SSHJTools;
 import us.ihmc.commons.Conversions;
-import us.ihmc.commons.FormattingTools;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.exception.ExceptionTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.gdx.imgui.ImGuiGlfwWindow;
-import us.ihmc.gdx.imgui.ImGuiTools;
-
-import java.lang.management.ManagementFactory;
-import java.util.HashMap;
 
 public class ImGuiSSHJMissionControlUI
 {
@@ -29,14 +21,12 @@ public class ImGuiSSHJMissionControlUI
    private final ImInt bufferSize = new ImInt(Conversions.megabytesToBytes(2));
    private final ImString command = new ImString(10000);
    private final ImDouble timeout = new ImDouble(0.0);
-   private final OperatingSystemMXBean platformMXBean;
    private int exitStatus = -1;
    private final SSHJInputStream standardOut = new SSHJInputStream();
    private final SSHJInputStream standardError = new SSHJInputStream();
    private Session.Command sshjCommand;
    private final ImGuiConsoleArea consoleArea = new ImGuiConsoleArea();
    private Thread runThread;
-//   private Opera
 
    public ImGuiSSHJMissionControlUI()
    {
@@ -48,28 +38,11 @@ public class ImGuiSSHJMissionControlUI
 
       standardOut.resize(bufferSize.get());
       standardError.resize(bufferSize.get());
-      command.set("sudo journalctl -ef -u sshd");
-
-      platformMXBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+      command.set("sudo journalctl -ef -u mission-control-2");
    }
 
    private void renderImGuiWidgets()
    {
-      ImGui.text("System load average: " + platformMXBean.getSystemLoadAverage());
-      ImGui.text("Process CPU: " + platformMXBean.getProcessCpuLoad());
-      ImGui.text("Process CPU time: " + platformMXBean.getProcessCpuTime());
-      ImGui.text("System CPU: " + platformMXBean.getSystemCpuLoad());
-      ImGui.text("Committed virtual memory size: " + platformMXBean.getCommittedVirtualMemorySize());
-      ImGui.text("Free physical memory size: " + platformMXBean.getFreePhysicalMemorySize());
-      double totalRAMGB = platformMXBean.getTotalPhysicalMemorySize() / 1000000000.0;
-      double committedRAMGB = platformMXBean.getCommittedVirtualMemorySize() / 1000000000.0;
-      double freeRAMGB = platformMXBean.getFreePhysicalMemorySize() / 1000000000.0;
-      double usedRAMGB = totalRAMGB - freeRAMGB;
-      ImGui.text("RAM Usage: " + FormattingTools.getFormattedDecimal1D(committedRAMGB) + " / " + FormattingTools.getFormattedDecimal1D(totalRAMGB));
-      ImGui.text("Free swap space size: " + platformMXBean.getFreeSwapSpaceSize());
-      ImGui.text("Total swap space size: " + platformMXBean.getTotalSwapSpaceSize());
-      ImGui.text("Available processors: " + platformMXBean.getAvailableProcessors());
-
       int inputTextFlags = ImGuiInputTextFlags.None;
       inputTextFlags |= ImGuiInputTextFlags.CallbackResize;
       ImGui.inputText("Command", command, inputTextFlags);
