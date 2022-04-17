0bc9143f911548b6a146879ef47b6c433277cacc
bmishra <bmishra@ihmc.us>
2022-04-17 16:19:34 -0500

    Fix wall yaw. Add rectangular corner projection funciton.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/object/objects/GDXBuildingObject.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/simulation/environment/object/objects/GDXBuildingObject.java
@@ -1,7 +1,9 @@
 package us.ihmc.gdx.simulation.environment.object.objects;
 
 
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
 
 import java.util.ArrayList;
 
@@ -13,6 +15,36 @@ public class GDXBuildingObject
    public GDXBuildingObject()
    {
 
+   }
+
+   public Point3D getClosestRectangularCorner(Point3D lastPickPoint)
+   {
+      Point3D cornerPoint;
+      if(corners.size() > 0)
+      {
+         Point3D corner = corners.get(corners.size() - 1);
+         double angle = EuclidGeometryTools.angleFromFirstToSecondVector2D(corner.getX() - lastPickPoint.getX(),
+                                                                           corner.getY() - lastPickPoint.getY(),
+                                                                           1,
+                                                                           0);
+
+         if( StrictMath.abs(angle - StrictMath.PI / 2.0f) < 0.1f || StrictMath.abs(angle + StrictMath.PI / 2.0f) < 0.1f)
+         {
+            Vector3D direction = new Vector3D(0.0f, 1.0f, 0.0f);
+            cornerPoint = EuclidGeometryTools.orthogonalProjectionOnLine3D(lastPickPoint, corner, direction);
+         }
+         else if( StrictMath.abs(angle) < 0.1f || StrictMath.abs(angle - StrictMath.PI) < 0.1f)
+         {
+            Vector3D direction = new Vector3D(1.0f, 0.0f, 0.0f);
+            cornerPoint = EuclidGeometryTools.orthogonalProjectionOnLine3D(lastPickPoint, corner, direction);
+         }
+         else {
+            cornerPoint = lastPickPoint;
+         }
+
+      }
+      else cornerPoint = lastPickPoint;
+      return cornerPoint;
    }
 
    public Point3D getLastCorner()
