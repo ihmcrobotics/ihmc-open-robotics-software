2bd2051c43fe5977591f774e4d5e5319d91564e4
Duncan Calvert <dcalvert@ihmc.us>
2022-04-17 00:41:02 -0500

    Extract Command and Machine.

---
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/missionControl/ImGuiSSHJMachine.java
@@ -0,0 +1,73 @@
+package us.ihmc.gdx.ui.missionControl;
+
+import imgui.ImGui;
+import org.apache.commons.lang3.tuple.MutablePair;
+import us.ihmc.behaviors.tools.MessagerHelper;
+import us.ihmc.gdx.ui.tools.ImGuiMessagerManagerWidget;
+import us.ihmc.gdx.ui.yo.ImPlotDoublePlotLine;
+import us.ihmc.gdx.ui.yo.ImPlotPlot;
+import us.ihmc.missionControl.MissionControlService;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ImGuiSSHJMachine
+{
+   private final MessagerHelper messagerHelper;
+
+   private final ImGuiMessagerManagerWidget messagerManagerWidget;
+   private final AtomicReference<MutablePair<Double, Double>> ramUsageSubscription;
+   private final AtomicReference<ArrayList<Double>> cpuUsagesSubscription;
+
+   private final ImPlotPlot ramPlot = new ImPlotPlot();
+   private final ImPlotDoublePlotLine ramUsagePlotLine = new ImPlotDoublePlotLine("RAM Usage GiB", 30 * 5, 30.0);
+   private final ImPlotDoublePlotLine ramTotalPlotLine = new ImPlotDoublePlotLine("RAM Total GiB", 30 * 5, 30.0);
+   private final ImPlotPlot cpuPlot = new ImPlotPlot();
+
+   public ImGuiSSHJMachine(String hostname)
+   {
+      ramPlot.getPlotLines().add(ramUsagePlotLine);
+      ramPlot.getPlotLines().add(ramTotalPlotLine);
+
+      messagerHelper = new MessagerHelper(MissionControlService.API.create());
+
+      ramUsageSubscription = messagerHelper.subscribeViaReference(MissionControlService.API.RAMUsage, MutablePair.of(0.0, 1.0));
+      cpuUsagesSubscription = messagerHelper.subscribeViaReference(MissionControlService.API.CPUUsages, new ArrayList<>());
+
+      messagerManagerWidget = new ImGuiMessagerManagerWidget(messagerHelper, () -> hostname, MissionControlService.API.PORT);
+   }
+
+   public void renderImGuiWidgets()
+   {
+
+
+      messagerManagerWidget.renderImGuiWidgets();
+
+      MutablePair<Double, Double> ramUsage = ramUsageSubscription.getAndSet(null);
+      if (ramUsage != null)
+      {
+         double used = ramUsage.getLeft();
+         double total = ramUsage.getRight();
+         ramUsagePlotLine.addValue(used);
+         ramTotalPlotLine.addValue(total);
+      }
+      ramPlot.render(ImGui.getColumnWidth() / 2.0f, 50.0f);
+
+      ImGui.sameLine();
+
+      ArrayList<Double> cpuUsages = cpuUsagesSubscription.getAndSet(null);
+      if (cpuUsages != null)
+      {
+         for (int i = 0; i < cpuUsages.size(); i++)
+         {
+            if (cpuPlot.getPlotLines().size() == i)
+            {
+               cpuPlot.getPlotLines().add(new ImPlotDoublePlotLine("CPU Core " + i + " %", 30 * 5, 30.0));
+            }
+
+            ((ImPlotDoublePlotLine) cpuPlot.getPlotLines().get(i)).addValue(cpuUsages.get(i));
+         }
+      }
+      cpuPlot.render(ImGui.getColumnWidth(), 50.0f);
+   }
+}
