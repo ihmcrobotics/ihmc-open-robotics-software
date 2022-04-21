3bfc101a99b5e956f8b4b910fb50417a1a71bf16
Robert Griffin <rgriffin@ihmc.us>
2022-04-20 22:49:02 -0500

    interpolated towards desired icp, not current icp

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/ICPController.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/ICPController.java
@@ -437,7 +437,7 @@ public class ICPController implements ICPControllerInterface
       else
          feedForwardAlpha.set(0.0);
 
-      feedbackCoP.interpolate(perfectCoP, currentICP, feedForwardAlpha.getDoubleValue());
+      feedbackCoP.interpolate(perfectCoP, desiredICP, feedForwardAlpha.getDoubleValue());
       feedbackCoP.scaleAdd(1.0 - feedbackAlpha.getValue(), feedbackCoPDelta, feedbackCoP);
       feedbackCMP.scaleAdd(1.0 - feedForwardAlpha.getValue(), perfectCMPOffset, feedbackCoP);
       feedbackCMP.scaleAdd(1.0 - feedbackAlpha.getValue(), feedbackCMPDelta, feedbackCMP);
