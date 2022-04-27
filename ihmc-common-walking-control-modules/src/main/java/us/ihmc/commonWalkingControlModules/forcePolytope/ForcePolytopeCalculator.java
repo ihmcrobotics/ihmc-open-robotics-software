24ceca3030fef388fa10ac0db45655cd8586e60d
smccrory <smccrory@ihmc.us>
2022-04-27 17:01:17 -0500

    Trying out qp formulation for force polytope

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/forcePolytope/ForcePolytopeCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/forcePolytope/ForcePolytopeCalculator.java
@@ -3,9 +3,11 @@ package us.ihmc.commonWalkingControlModules.forcePolytope;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
 import us.ihmc.convexOptimization.linearProgram.LinearProgramSolver;
+import us.ihmc.convexOptimization.quadraticProgram.QuadProgSolver;
+import us.ihmc.convexOptimization.quadraticProgram.SimpleEfficientActiveSetQPSolver;
+import us.ihmc.convexOptimization.quadraticProgram.SimpleEfficientActiveSetQPSolverWithInactiveVariables;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
 import us.ihmc.euclid.shape.convexPolytope.ConvexPolytope3D;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Point3D;
@@ -22,7 +24,8 @@ import us.ihmc.robotics.screwTheory.PointJacobian;
 
 public class ForcePolytopeCalculator
 {
-   private static final boolean debug = false;
+   private static final boolean solveWithLP = false;
+   private static final boolean debug = true;
    private static final double gravity = -9.81;
 
    private final RigidBodyBasics rootBody;
@@ -48,10 +51,20 @@ public class ForcePolytopeCalculator
    private final Point3D[] directionsToOptimize;
 
    private final LinearProgramSolver lpSolver = new LinearProgramSolver();
-   private final DMatrixRMaj inequalityA = new DMatrixRMaj(3, 1);
-   private final DMatrixRMaj inequalityB = new DMatrixRMaj(3, 1);
-   private final DMatrixRMaj cost = new DMatrixRMaj(6, 1);
-   private final DMatrixRMaj forceSolution = new DMatrixRMaj(3, 1);
+   private final DMatrixRMaj lpInequalityA = new DMatrixRMaj(3, 1);
+   private final DMatrixRMaj lpInequalityB = new DMatrixRMaj(3, 1);
+   private final DMatrixRMaj lpCost = new DMatrixRMaj(6, 1);
+   private final DMatrixRMaj lpSolution = new DMatrixRMaj(3, 1);
+
+//   private final SimpleEfficientActiveSetQPSolver qpSolver = new SimpleEfficientActiveSetQPSolver();
+   private final DMatrixRMaj qpQuadraticCost = new DMatrixRMaj(0);
+   private final DMatrixRMaj qpLinearCost = new DMatrixRMaj(0);
+   private final DMatrixRMaj qpInequalityA = new DMatrixRMaj(0);
+   private final DMatrixRMaj qpInequalityB = new DMatrixRMaj(0);
+   private final DMatrixRMaj qpSolution = new DMatrixRMaj(0);
+
+   private final DMatrixRMaj identity = new DMatrixRMaj(0);
+   private final DMatrixRMaj jacobianOuterProduct = new DMatrixRMaj(0);
 
    public ForcePolytopeCalculator(RigidBodyBasics base, RigidBodyBasics endEffector)
    {
@@ -79,8 +92,18 @@ public class ForcePolytopeCalculator
       jacobianTranspose.reshape(joints.length, 3);
       calculator.setGravitionalAcceleration(gravity);
       directionsToOptimize = SpiralBasedAlgorithm.generatePointsOnSphere(1.0, 50);
-      inequalityA.reshape(2 * joints.length, 6);
-      inequalityB.reshape(2 * joints.length, 1);
+      lpInequalityA.reshape(2 * joints.length, 6);
+      lpInequalityB.reshape(2 * joints.length, 1);
+
+      int qpSize = 3 + joints.length;
+      qpQuadraticCost.reshape(qpSize, qpSize);
+      qpLinearCost.reshape(qpSize, 1);
+      qpInequalityA.reshape(2 * joints.length, qpSize);
+      qpInequalityB.reshape(2 * joints.length, 1);
+      qpSolution.reshape(qpSize, 1);
+      identity.reshape(joints.length, joints.length);
+      CommonOps_DDRM.setIdentity(identity);
+      jacobianOuterProduct.reshape(3, 3);
    }
 
    public void update()
@@ -113,43 +136,96 @@ public class ForcePolytopeCalculator
          tauUpperLimit.set(i, -gravityCompensationTorques.get(i) + joints[i].getEffortLimitUpper());
       }
 
-      int rows = jacobianTranspose.getNumRows();
-      int cols = jacobianTranspose.getNumCols();
-
-      MatrixTools.setMatrixBlock(inequalityA, 0, 0, jacobianTranspose, 0, 0, rows, cols, 1.0);
-      MatrixTools.setMatrixBlock(inequalityA, 0, cols, jacobianTranspose, 0, 0, rows, cols, -1.0);
-      MatrixTools.setMatrixBlock(inequalityA, rows, 0, jacobianTranspose, 0, 0, rows, cols, -1.0);
-      MatrixTools.setMatrixBlock(inequalityA, rows, cols, jacobianTranspose, 0, 0, rows, cols, 1.0);
-
-      MatrixTools.setMatrixBlock(inequalityB, 0, 0, tauUpperLimit, 0, 0, joints.length, 1, 1.0);
-      MatrixTools.setMatrixBlock(inequalityB, rows, 0, tauLowerLimit, 0, 0, joints.length, 1, -1.0);
-
       forcePolytope.clear();
+
       for (int i = 0; i < directionsToOptimize.length; i++)
       {
          Point3D direction = directionsToOptimize[i];
-         for (int j = 0; j < 3; j++)
-         {
-            cost.set(j, direction.getElement(j));
-            cost.set(3 + j, -direction.getElement(j));
-         }
 
-         if (lpSolver.solve(cost, inequalityA, inequalityB, forceSolution))
+         if (solveWithLP)
          {
+            // setup as linear program
+            int rows = jacobianTranspose.getNumRows();
+            int cols = jacobianTranspose.getNumCols();
+
+            MatrixTools.setMatrixBlock(lpInequalityA, 0, 0, jacobianTranspose, 0, 0, rows, cols, 1.0);
+            MatrixTools.setMatrixBlock(lpInequalityA, 0, cols, jacobianTranspose, 0, 0, rows, cols, -1.0);
+            MatrixTools.setMatrixBlock(lpInequalityA, rows, 0, jacobianTranspose, 0, 0, rows, cols, -1.0);
+            MatrixTools.setMatrixBlock(lpInequalityA, rows, cols, jacobianTranspose, 0, 0, rows, cols, 1.0);
+
+            MatrixTools.setMatrixBlock(lpInequalityB, 0, 0, tauUpperLimit, 0, 0, joints.length, 1, 1.0);
+            MatrixTools.setMatrixBlock(lpInequalityB, rows, 0, tauLowerLimit, 0, 0, joints.length, 1, -1.0);
+
+            for (int j = 0; j < 3; j++)
+            {
+               lpCost.set(j, direction.getElement(j));
+               lpCost.set(3 + j, -direction.getElement(j));
+            }
+
+            if (lpSolver.solve(lpCost, lpInequalityA, lpInequalityB, lpSolution))
+            {
+               Point3D vertex = new Point3D();
+               vertex.set(lpSolution.get(0), lpSolution.get(1), lpSolution.get(2));
+               vertex.sub(lpSolution.get(3), lpSolution.get(4), lpSolution.get(5));
+               forcePolytope.addVertex(vertex);
+            }
+         }
+         else
+         {
+            // setup as quadratic program
+            CommonOps_DDRM.multOuter(jacobianMatrix, jacobianOuterProduct);
+            MatrixTools.setMatrixBlock(qpQuadraticCost, 0, 0, identity, 0, 0, joints.length, joints.length, 1.0);
+            MatrixTools.setMatrixBlock(qpQuadraticCost, 0, joints.length, jacobianTranspose, 0, 0, jacobianTranspose.getNumRows(), jacobianTranspose.getNumCols(), -1.0);
+            MatrixTools.setMatrixBlock(qpQuadraticCost, joints.length, 0, jacobianMatrix, 0, 0, jacobianMatrix.getNumRows(), jacobianMatrix.getNumCols(), -1.0);
+            MatrixTools.setMatrixBlock(qpQuadraticCost, joints.length, joints.length, jacobianOuterProduct, 0, 0, jacobianOuterProduct.getNumRows(), jacobianOuterProduct.getNumCols(), 1.0);
+
+            double quadCostScale = 1.0;
+            CommonOps_DDRM.scale(quadCostScale, qpQuadraticCost);
+
+            direction.get(qpLinearCost);
+            CommonOps_DDRM.scale(-1.0, qpLinearCost);
+
+            MatrixTools.setMatrixBlock(qpInequalityA, 0, 0, identity, 0, 0, identity.getNumRows(), identity.getNumCols(), -1.0);
+            MatrixTools.setMatrixBlock(qpInequalityA, identity.getNumRows(), 0, identity, 0, 0, identity.getNumRows(), identity.getNumCols(), 1.0);
+            MatrixTools.setMatrixBlock(qpInequalityB, 0, 0, tauLowerLimit, 0, 0, tauLowerLimit.getNumRows(), tauLowerLimit.getNumCols(), -1.0);
+            MatrixTools.setMatrixBlock(qpInequalityB, identity.getNumRows(), 0, tauUpperLimit, 0, 0, tauUpperLimit.getNumRows(), tauUpperLimit.getNumCols(), 1.0);
+
+            for (int j = 0; j < 3; j++)
+            {
+               double forceDampingTerm = 1e-6;
+               qpQuadraticCost.add(joints.length + j, joints.length + j, forceDampingTerm);
+            }
+
+//            SimpleEfficientActiveSetQPSolver qpSolver = new SimpleEfficientActiveSetQPSolver();
+            SimpleEfficientActiveSetQPSolverWithInactiveVariables qpSolver = new SimpleEfficientActiveSetQPSolverWithInactiveVariables();
+
+            qpSolver.setUseWarmStart(false);
+            qpSolver.setMaxNumberOfIterations(1000);
+            qpSolver.setConvergenceThreshold(1e-5);
+            qpSolver.clear();
+            qpSolver.resetActiveSet();
+            qpSolver.setQuadraticCostFunction(qpQuadraticCost, qpLinearCost);
+            qpSolver.setLinearInequalityConstraints(qpInequalityA, qpInequalityB);
+            int iterations = qpSolver.solve(qpSolution);
+
+            System.out.println(qpSolution);
+            System.out.println("iterations: " + iterations);
+
             Point3D vertex = new Point3D();
-            vertex.set(forceSolution.get(0), forceSolution.get(1), forceSolution.get(2));
-            vertex.sub(forceSolution.get(3), forceSolution.get(4), forceSolution.get(5));
-            forcePolytope.addVertex(vertex);
+            vertex.set(joints.length, qpSolution);
+
+            if (!vertex.containsNaN())
+               forcePolytope.addVertex(vertex);
          }
       }
 
       if (debug)
       {
          LogTools.info("------- Force Polytope -------------");
-         for (int i = 0; i < forcePolytope.getNumberOfVertices(); i++)
-         {
-            LogTools.info(forcePolytope.getVertex(i));
-         }
+//         for (int i = 0; i < forcePolytope.getNumberOfVertices(); i++)
+//         {
+//            LogTools.info(forcePolytope.getVertex(i));
+//         }
       }
    }
 
