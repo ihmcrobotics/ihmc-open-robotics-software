3389e28c63f6cc70310d420ad3a3b5aee2e26afb
Duncan Calvert <dcalvert@ihmc.us>
2022-04-28 15:39:59 -0500

    Add Atlas left nub and right hand model, support it in DRCRobotModel, and make it the default for some apps.

--- atlas/src/main/java/us/ihmc/atlas/AtlasRobotVersion.java
+++ atlas/src/main/java/us/ihmc/atlas/AtlasRobotVersion.java
@@ -15,12 +15,13 @@ public enum AtlasRobotVersion
    ATLAS_UNPLUGGED_V5_NO_HANDS,
    ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ,
    ATLAS_UNPLUGGED_V5_ROBOTIQ_AND_SRI,
-   ATLAS_UNPLUGGED_V5_TROOPER;
+   ATLAS_UNPLUGGED_V5_TROOPER,
+   ATLAS_UNPLUGGED_V5_LEFT_NUB_RIGHT_ROBOTIQ;
 
    private static String[] resourceDirectories;
    private final SideDependentList<RigidBodyTransform> offsetHandFromAttachmentPlate = new SideDependentList<RigidBodyTransform>();
 
-   public DRCHandType getHandModel()
+   public DRCHandType getHandModel(RobotSide side)
    {
       switch (this)
       {
@@ -29,6 +30,8 @@ public enum AtlasRobotVersion
          return DRCHandType.ROBOTIQ;
       case ATLAS_UNPLUGGED_V5_ROBOTIQ_AND_SRI:
          return DRCHandType.ROBOTIQ_AND_SRI;
+      case ATLAS_UNPLUGGED_V5_LEFT_NUB_RIGHT_ROBOTIQ:
+         return side == RobotSide.RIGHT ? DRCHandType.ROBOTIQ : DRCHandType.NONE;
       case ATLAS_UNPLUGGED_V5_NO_HANDS:
       case ATLAS_UNPLUGGED_V5_NO_FOREARMS:
       default:
@@ -36,9 +39,9 @@ public enum AtlasRobotVersion
       }
    }
 
-   public boolean hasRobotiqHands()
+   public boolean hasRobotiqHands(RobotSide side)
    {
-      return getHandModel() == DRCHandType.ROBOTIQ || getHandModel() == DRCHandType.ROBOTIQ_AND_SRI;
+      return getHandModel(side) == DRCHandType.ROBOTIQ || getHandModel(side) == DRCHandType.ROBOTIQ_AND_SRI;
    }
 
    public String getSdfFile()
@@ -50,6 +53,8 @@ public enum AtlasRobotVersion
       case ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ:
       case ATLAS_UNPLUGGED_V5_ROBOTIQ_AND_SRI:
          return "models/GFE/atlas_unplugged_v5_dual_robotiq.sdf";
+      case ATLAS_UNPLUGGED_V5_LEFT_NUB_RIGHT_ROBOTIQ:
+         return "models/GFE/atlas_unplugged_v5_left_nub_right_robotiq.sdf";
       case ATLAS_UNPLUGGED_V5_NO_FOREARMS:
          return "models/GFE/atlas_unplugged_v5_no_forearms.sdf";
       case ATLAS_UNPLUGGED_V5_TROOPER:
@@ -84,25 +89,23 @@ public enum AtlasRobotVersion
 
    private void createTransforms()
    {
-      double distanceAttachmentPlateHand;
-      
-      switch (this)
-      {
-      case ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ:
-      case ATLAS_UNPLUGGED_V5_ROBOTIQ_AND_SRI:
-         distanceAttachmentPlateHand = 0.12; // On the palm.
-         break;
-      default:
-         distanceAttachmentPlateHand = 0.0;
-         break;
-      }
-
       for (RobotSide robotSide : RobotSide.values)
       {
+         double distanceAttachmentPlateHand;
+
+         if (getHandModel(robotSide) == DRCHandType.ROBOTIQ)
+         {
+            distanceAttachmentPlateHand = 0.12; // On the palm.
+         }
+         else
+         {
+            distanceAttachmentPlateHand = 0.0;
+         }
+
          Point3D centerOfHandToWristTranslation = new Point3D();
          Quaternion centerOfHandToWristOrientation = new Quaternion();
 
-         if (hasRobotiqHands())
+         if (hasRobotiqHands(robotSide))
          {
             centerOfHandToWristTranslation = new Point3D(distanceAttachmentPlateHand, 0.0, 0.0);
             centerOfHandToWristOrientation.appendRollRotation(robotSide.negateIfLeftSide(Math.PI * 0.5));
