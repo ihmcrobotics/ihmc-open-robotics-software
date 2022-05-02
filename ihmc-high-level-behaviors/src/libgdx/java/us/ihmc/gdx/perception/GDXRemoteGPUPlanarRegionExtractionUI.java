b67d53c6cefddbc689916b517eefa9e1dc610e21
Duncan Calvert <dcalvert@ihmc.us>
2022-05-02 17:51:39 -0500

    Progress on the GDXRemoteGPUPlanarRegionExtractionUI.

---
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/perception/GDXRemoteGPUPlanarRegionExtractionUI.java
@@ -0,0 +1,17 @@
+package us.ihmc.gdx.perception;
+
+import us.ihmc.avatar.gpuPlanarRegions.GPUPlanarRegionExtractionParameters;
+import us.ihmc.robotEnvironmentAwareness.geometry.ConcaveHullFactoryParameters;
+import us.ihmc.robotEnvironmentAwareness.planarRegion.PolygonizerParameters;
+
+public class GDXRemoteGPUPlanarRegionExtractionUI
+{
+   private GPUPlanarRegionExtractionParameters parameters = new GPUPlanarRegionExtractionParameters();
+   private PolygonizerParameters polygonizerParameters = new PolygonizerParameters();
+   private ConcaveHullFactoryParameters concaveHullFactoryParameters = new ConcaveHullFactoryParameters();
+
+   public GDXRemoteGPUPlanarRegionExtractionUI()
+   {
+
+   }
+}
