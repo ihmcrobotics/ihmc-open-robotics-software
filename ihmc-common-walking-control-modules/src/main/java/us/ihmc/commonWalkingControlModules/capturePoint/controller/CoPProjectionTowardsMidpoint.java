ad19a4467147b681d9d78829730f37759bb8f9e0
rgriffin@ihmc.org <rgriffin@ihmc.us>
2022-05-06 16:54:17 -0500

    set using the cop projection to false by default

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/CoPProjectionTowardsMidpoint.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/capturePoint/controller/CoPProjectionTowardsMidpoint.java
@@ -66,7 +66,7 @@ public class CoPProjectionTowardsMidpoint implements ICPControllerParameters.Fee
                                                    "When projecting the CoP into the foot, move up to this far from the edge if possible",
                                                    registry,
                                                    0.04);
-      useCoPProjection = new BooleanParameter(yoNamePrefix + "UseCoPProjection", registry, true);
+      useCoPProjection = new BooleanParameter(yoNamePrefix + "UseCoPProjection", registry, false);
 
       dotProductForFootEdgeProjection = new YoDouble(yoNamePrefix + "DotProductForFootEdgeProjection", registry);
       momentumShiftedICPOnProjection = new YoDouble(yoNamePrefix + "momentumShiftedICPOnProjection", registry);
