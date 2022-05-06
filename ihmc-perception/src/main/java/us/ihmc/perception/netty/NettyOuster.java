224c673c28fa5ab6289a7ae36fc90cb5260b6c4c
Perry MacMurray <pmacmurray@ihmc.org>
2022-05-06 15:32:23 -0500

    Very incomplete implementation that binds to port and outputs UDP stream from Ouster to console

--- ihmc-perception/src/main/java/us/ihmc/perception/netty/NettyOuster.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/netty/NettyOuster.java
@@ -1,2 +1,45 @@
-package us.ihmc.perception.netty;public class NettyOuster {
+package us.ihmc.perception.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+public class NettyOuster
+{
+   public static final int PORT = 7502;
+
+   private EventLoopGroup group;
+   private Bootstrap bootstrap;
+   public NettyOuster() {
+      //this constructor does nothing right now
+   }
+
+   public void initialize() {
+      group = new NioEventLoopGroup();
+      bootstrap = new Bootstrap();
+      bootstrap.group(group).channel(NioDatagramChannel.class).handler(new SimpleChannelInboundHandler<DatagramPacket>()
+      {
+         @Override
+         protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg)
+         {
+            ByteBuf data = msg.content().readBytes(msg.content().capacity());
+            for (int i = 0; i < data.capacity(); i++) {
+               System.out.printf("%02x ", data.getByte(i));
+            }
+         }
+      });
+   }
+
+   public void start() {
+      bootstrap.bind(PORT);
+   }
+
+   public void stop() {
+      group.shutdownGracefully();
+   }
 }
