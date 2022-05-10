9fc94921ace074b0ccd7b44d5379946dfed88a77
rgriffin@ihmc.org <rgriffin@ihmc.us>
2022-05-10 18:00:22 -0500

    added some more convenience constructors

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/gpuPlanarRegions/GPUPlanarRegionExtraction.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/gpuPlanarRegions/GPUPlanarRegionExtraction.java
@@ -45,7 +45,10 @@ import static us.ihmc.avatar.gpuPlanarRegions.GPUPlanarRegionExtractionParameter
 
 public class GPUPlanarRegionExtraction
 {
-   private GPUPlanarRegionExtractionParameters parameters;
+   private final GPUPlanarRegionExtractionParameters parameters;
+   private final ConcaveHullFactoryParameters concaveHullFactoryParameters;
+   private final PolygonizerParameters polygonizerParameters;
+
    private BytedecoImage inputFloatDepthImage;
    private BytedecoImage inputScaledFloatDepthImage;
    private BytedecoImage inputU16DepthImage;
@@ -87,17 +90,29 @@ public class GPUPlanarRegionExtraction
    private int patchWidth;
    private int filterPatchImageHeight;
    private int filterPatchImageWidth;
-   private final ConcaveHullFactoryParameters concaveHullFactoryParameters = new ConcaveHullFactoryParameters();
-   private final PolygonizerParameters polygonizerParameters = new PolygonizerParameters();
+
    private final PlanarRegionsList planarRegionsList = new PlanarRegionsList();
    private final GPUPlanarRegionIsland tempIsland = new GPUPlanarRegionIsland();
    private boolean firstRun = true;
+
+   public GPUPlanarRegionExtraction()
+   {
+      this(new GPUPlanarRegionExtractionParameters(), new PolygonizerParameters(), new ConcaveHullFactoryParameters());
+   }
+
+   public GPUPlanarRegionExtraction(GPUPlanarRegionExtractionParameters parameters,
+                                    PolygonizerParameters polygonizerParameters,
+                                    ConcaveHullFactoryParameters concaveHullFactoryParameters)
+   {
+      this.parameters = parameters;
+      this.polygonizerParameters = polygonizerParameters;
+      this.concaveHullFactoryParameters = concaveHullFactoryParameters;
+   }
 
    public void create(int imageWidth, int imageHeight, ByteBuffer sourceDepthByteBufferOfFloats, double fx, double fy, double cx, double cy)
    {
       this.imageWidth = imageWidth;
       this.imageHeight = imageHeight;
-      parameters = new GPUPlanarRegionExtractionParameters();
       parameters.set(focalLengthXPixels, fx);
       parameters.set(focalLengthYPixels, fy);
       parameters.set(principalOffsetXPixels, cx);
