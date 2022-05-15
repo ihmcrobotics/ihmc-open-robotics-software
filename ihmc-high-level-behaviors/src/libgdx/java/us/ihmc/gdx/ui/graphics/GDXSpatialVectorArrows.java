61dac965b2acfd7ae4f0386e5b8bb1107a28db5e
Duncan Calvert <dcalvert@ihmc.us>
2022-05-14 20:41:12 -0500

    Switch wrist wrench arrows to use the YoVariable values, which involved adding YoVariableClients to the UIs.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/GDXSpatialVectorArrows.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/GDXSpatialVectorArrows.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import us.ihmc.behaviors.tools.yo.YoDoubleClientHelper;
+import us.ihmc.behaviors.tools.yo.YoVariableClientHelper;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.gdx.tools.GDXTools;
 
@@ -19,7 +22,27 @@ public class GDXSpatialVectorArrows
    private final RigidBodyTransform tempTransform = new RigidBodyTransform();
    private final FramePoint3D origin = new FramePoint3D();
    private final ReferenceFrame originFrame;
-   private final int indexOfSensor;
+   private YoVariableClientHelper yoVariableClientHelper;
+   private int indexOfSensor = -1;
+   private YoDoubleClientHelper linearXYoVariable;
+   private YoDoubleClientHelper linearYYoVariable;
+   private YoDoubleClientHelper linearZYoVariable;
+   private YoDoubleClientHelper angularXYoVariable;
+   private YoDoubleClientHelper angularYYoVariable;
+   private YoDoubleClientHelper angularZYoVariable;
+   private final Vector3D tempVector = new Vector3D();
+
+   public GDXSpatialVectorArrows(ReferenceFrame originFrame, YoVariableClientHelper yoVariableClientHelper, String variablePrefix)
+   {
+      this.originFrame = originFrame;
+      this.yoVariableClientHelper = yoVariableClientHelper;
+      linearXYoVariable = yoVariableClientHelper.subscribeToYoDouble(variablePrefix + "ForceX");
+      linearYYoVariable = yoVariableClientHelper.subscribeToYoDouble(variablePrefix + "ForceY");
+      linearZYoVariable = yoVariableClientHelper.subscribeToYoDouble(variablePrefix + "ForceZ");
+      angularXYoVariable = yoVariableClientHelper.subscribeToYoDouble(variablePrefix + "TorqueX");
+      angularYYoVariable = yoVariableClientHelper.subscribeToYoDouble(variablePrefix + "TorqueY");
+      angularZYoVariable = yoVariableClientHelper.subscribeToYoDouble(variablePrefix + "TorqueZ");
+   }
 
    public GDXSpatialVectorArrows(ReferenceFrame originFrame, int indexOfSensor)
    {
@@ -34,6 +57,24 @@ public class GDXSpatialVectorArrows
 
       transform(linearPartArrow, linearPart, 0.005);
       transform(angularPartArrow, angularPart, 0.02);
+   }
+
+   public void updateFromYoVariables()
+   {
+      if (yoVariableClientHelper.isConnected())
+      {
+         origin.setToZero(originFrame);
+         origin.changeFrame(ReferenceFrame.getWorldFrame());
+
+         tempVector.set(linearXYoVariable.get(),
+                        linearYYoVariable.get(),
+                        linearZYoVariable.get());
+         transform(linearPartArrow, tempVector, 0.005);
+         tempVector.set(angularXYoVariable.get(),
+                        angularYYoVariable.get(),
+                        angularZYoVariable.get());
+         transform(angularPartArrow, tempVector, 0.005);
+      }
    }
 
    private void transform(GDXArrowGraphic arrowGraphic, Vector3DReadOnly vector, double scalar)
