25d09f02ac18b5625a886debabfdac0985864724
Duncan Calvert <dcalvert@ihmc.us>
2022-05-17 10:29:51 -0500

    Clean up GDXModelBuilder.

--- ihmc-graphics/src/libgdx/java/us/ihmc/gdx/tools/GDXModelBuilder.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/tools/GDXModelBuilder.java
@@ -18,7 +18,6 @@ import us.ihmc.gdx.mesh.GDXMultiColorMeshBuilder;
 
 import java.util.function.Consumer;
 
-// TODO: Rename to GDXModelBuilder?
 public class GDXModelBuilder
 {
    public static ModelInstance createCoordinateFrameInstance(double length)
@@ -87,41 +86,41 @@ public class GDXModelBuilder
    public static Model createCoordinateFrame(double length)
    {
       return buildModel(meshBuilder ->
-                        {
-                           double radius = 0.02 * length;
-                           double coneHeight = 0.10 * length;
-                           double coneRadius = 0.05 * length;
-                           meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), Color.RED);
-                           meshBuilder.addCone(coneHeight, coneRadius, new Point3D(length, 0.0, 0.0), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), Color.RED);
-                           meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0), Color.GREEN);
-                           meshBuilder.addCone(coneHeight,
-                                               coneRadius,
-                                               new Point3D(0.0, length, 0.0),
-                                               new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0),
-                                               Color.GREEN);
-                           meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(), Color.BLUE);
-                           meshBuilder.addCone(coneHeight, coneRadius, new Point3D(0.0, 0.0, length), new AxisAngle(), Color.BLUE);
-                        }, "coordinateFrame");
+      {
+         double radius = 0.02 * length;
+         double coneHeight = 0.10 * length;
+         double coneRadius = 0.05 * length;
+         meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), Color.RED);
+         meshBuilder.addCone(coneHeight, coneRadius, new Point3D(length, 0.0, 0.0), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), Color.RED);
+         meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0), Color.GREEN);
+         meshBuilder.addCone(coneHeight,
+                             coneRadius,
+                             new Point3D(0.0, length, 0.0),
+                             new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0),
+                             Color.GREEN);
+         meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(), Color.BLUE);
+         meshBuilder.addCone(coneHeight, coneRadius, new Point3D(0.0, 0.0, length), new AxisAngle(), Color.BLUE);
+      }, "coordinateFrame");
    }
 
    public static Model createCoordinateFrame(double length, Color color)
    {
       return buildModel(meshBuilder ->
-                        {
-                           double radius = 0.02 * length;
-                           double coneHeight = 0.10 * length;
-                           double coneRadius = 0.05 * length;
-                           meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
-                           meshBuilder.addCone(coneHeight, coneRadius, new Point3D(length, 0.0, 0.0), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), Color.RED);
-                           meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0), color);
-                           meshBuilder.addCone(coneHeight,
-                                               coneRadius,
-                                               new Point3D(0.0, length, 0.0),
-                                               new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0),
-                                               Color.GREEN);
-                           meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(), color);
-                           meshBuilder.addCone(coneHeight, coneRadius, new Point3D(0.0, 0.0, length), new AxisAngle(), Color.BLUE);
-                        }, "coordinateFrame");
+      {
+         double radius = 0.02 * length;
+         double coneHeight = 0.10 * length;
+         double coneRadius = 0.05 * length;
+         meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
+         meshBuilder.addCone(coneHeight, coneRadius, new Point3D(length, 0.0, 0.0), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), Color.RED);
+         meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0), color);
+         meshBuilder.addCone(coneHeight,
+                             coneRadius,
+                             new Point3D(0.0, length, 0.0),
+                             new AxisAngle(1.0, 0.0, 0.0, -Math.PI / 2.0),
+                             Color.GREEN);
+         meshBuilder.addCylinder(length, radius, new Point3D(), new AxisAngle(), color);
+         meshBuilder.addCone(coneHeight, coneRadius, new Point3D(0.0, 0.0, length), new AxisAngle(), Color.BLUE);
+      }, "coordinateFrame");
    }
 
    public static ModelInstance createSphere(float radius, Color color)
@@ -142,64 +141,64 @@ public class GDXModelBuilder
    public static ModelInstance createCylinder(double height, double radius, Color color)
    {
       return buildModelInstance(meshBuilder ->
-                                {
-                                   meshBuilder.addCylinder(height, radius, new Point3D(), color);
-                                }, "cylinder");
+      {
+         meshBuilder.addCylinder(height, radius, new Point3D(), color);
+      }, "cylinder");
    }
 
    public static ModelInstance createArrow(double length, Color color)
    {
       return buildModelInstance(meshBuilder ->
-                                {
-                                   double coneHeight = 0.10 * length;
-                                   double cylinderLength = length - coneHeight;
-                                   double cylinderRadius = cylinderLength / 20.0;
-                                   double coneRadius = 1.5 * cylinderRadius;
-                                   meshBuilder.addCylinder(cylinderLength, cylinderRadius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
-                                   meshBuilder.addCone(coneHeight,
-                                                       coneRadius,
-                                                       new Point3D(cylinderLength, 0.0, 0.0),
-                                                       new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0),
-                                                       color);
-                                }, "arrow");
+      {
+         double coneHeight = 0.10 * length;
+         double cylinderLength = length - coneHeight;
+         double cylinderRadius = cylinderLength / 20.0;
+         double coneRadius = 1.5 * cylinderRadius;
+         meshBuilder.addCylinder(cylinderLength, cylinderRadius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
+         meshBuilder.addCone(coneHeight,
+                             coneRadius,
+                             new Point3D(cylinderLength, 0.0, 0.0),
+                             new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0),
+                             color);
+      }, "arrow");
    }
 
    public static ModelInstance createPose(double radius, Color color)
    {
       return buildModelInstance(meshBuilder ->
-                                {
-                                   double cylinderLength = radius * 6.0;
-                                   double cylinderRadius = cylinderLength / 20.0;
-                                   double coneHeight = 0.10 * cylinderLength;
-                                   double coneRadius = 1.5 * cylinderRadius;
-                                   meshBuilder.addCylinder(cylinderLength, cylinderRadius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
-                                   meshBuilder.addCone(coneHeight,
-                                                       coneRadius,
-                                                       new Point3D(cylinderLength, 0.0, 0.0),
-                                                       new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0),
-                                                       color);
-                                   meshBuilder.addSphere((float) radius, color);
-                                }, "arrow");
+      {
+         double cylinderLength = radius * 6.0;
+         double cylinderRadius = cylinderLength / 20.0;
+         double coneHeight = 0.10 * cylinderLength;
+         double coneRadius = 1.5 * cylinderRadius;
+         meshBuilder.addCylinder(cylinderLength, cylinderRadius, new Point3D(), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0), color);
+         meshBuilder.addCone(coneHeight,
+                             coneRadius,
+                             new Point3D(cylinderLength, 0.0, 0.0),
+                             new AxisAngle(0.0, 1.0, 0.0, Math.PI / 2.0),
+                             color);
+         meshBuilder.addSphere((float) radius, color);
+      }, "arrow");
    }
 
    public static ModelInstance createStairs(double width, double stepHeight, double stepWidth, int numberOfSteps, Color color)
    {
       return buildModelInstance(meshBuilder ->
-                        {
-                           for(int i = 1; i<numberOfSteps + 1; i++)
-                           {
-                              meshBuilder.addBox(stepWidth, width, stepHeight * (float) i, new Point3D(stepWidth * (float) i, 0.0f, (stepHeight * (float) i) / 2.0f), color);
-                           }
+      {
+         for(int i = 1; i<numberOfSteps + 1; i++)
+         {
+            meshBuilder.addBox(stepWidth, width, stepHeight * (float) i, new Point3D(stepWidth * (float) i, 0.0f, (stepHeight * (float) i) / 2.0f), color);
+         }
 
-                           for(int i = 1; i<numberOfSteps + 1; i++)
-                           {
-                              meshBuilder.addCylinder(1.0f, 0.02f, new Point3D(stepWidth * (float) i, -width * 0.45f, (stepHeight * (float) i)), Color.BROWN);
-                              meshBuilder.addCylinder(1.0f, 0.02f, new Point3D(stepWidth * (float) i, width * 0.45f, (stepHeight * (float) i)), Color.BROWN);
-                           }
+         for(int i = 1; i<numberOfSteps + 1; i++)
+         {
+            meshBuilder.addCylinder(1.0f, 0.02f, new Point3D(stepWidth * (float) i, -width * 0.45f, (stepHeight * (float) i)), Color.BROWN);
+            meshBuilder.addCylinder(1.0f, 0.02f, new Point3D(stepWidth * (float) i, width * 0.45f, (stepHeight * (float) i)), Color.BROWN);
+         }
 
-                           meshBuilder.addCylinder(EuclidCoreTools.norm(numberOfSteps * stepWidth, numberOfSteps * stepHeight), 0.03f, new Point3D(0.0f, -width * 0.45f, 1.0f), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 4.0), Color.DARK_GRAY);
-                           meshBuilder.addCylinder(EuclidCoreTools.norm(numberOfSteps * stepWidth, numberOfSteps * stepHeight), 0.03f, new Point3D(0.0f, width * 0.45f, 1.0f), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 4.0), Color.DARK_GRAY);
+         meshBuilder.addCylinder(EuclidCoreTools.norm(numberOfSteps * stepWidth, numberOfSteps * stepHeight), 0.03f, new Point3D(0.0f, -width * 0.45f, 1.0f), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 4.0), Color.DARK_GRAY);
+         meshBuilder.addCylinder(EuclidCoreTools.norm(numberOfSteps * stepWidth, numberOfSteps * stepHeight), 0.03f, new Point3D(0.0f, width * 0.45f, 1.0f), new AxisAngle(0.0, 1.0, 0.0, Math.PI / 4.0), Color.DARK_GRAY);
 
-                        }, "stairs");
+      }, "stairs");
    }
 }
