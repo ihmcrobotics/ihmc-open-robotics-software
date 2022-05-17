14f27e064c20cac4e57f778f566dd2f1f9f3b2a7
Perry MacMurray <pmacmurray@ihmc.org>
2022-05-16 21:04:39 -0500

    Still just printing to console, but maybe correct retreival of data?

--- ihmc-perception/src/main/java/us/ihmc/perception/netty/NettyOuster.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/netty/NettyOuster.java
@@ -11,6 +11,7 @@ import io.netty.channel.socket.nio.NioDatagramChannel;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.tuple3D.Point3D32;
 
+import java.nio.ByteOrder;
 import java.util.HashSet;
 
 /**
@@ -26,6 +27,7 @@ public class NettyOuster
    private EventLoopGroup group;
    private Bootstrap bootstrap;
    private HashSet<Point3D32> points = new HashSet<>();
+   private long time = System.nanoTime();
 
    public NettyOuster()
    {
@@ -42,12 +44,93 @@ public class NettyOuster
          protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg)
          {
             ByteBuf data = msg.content().readBytes(msg.content().capacity());
-            for (int i = 0; i < data.capacity(); i++)
-            {
-               System.out.printf("%02x ", data.getByte(i));
+            data = data.order(ByteOrder.LITTLE_ENDIAN);
+            int i = 0;
+            System.out.print("Timestamp: ");
+            i = printBytes(data, i, 8);
+            System.out.print("Frame ID: ");
+            i = printBytes(data, i, 2);
+            System.out.print("Measurement ID: ");
+            i = printBytes(data, i, 2);
+            System.out.print("Encoder Count: ");
+            i = printBytes(data, i, 4);
+            System.out.println();
+
+            for (int j = 0; j < 64; j++) {
+               System.out.println("Block " + j + ":");
+               System.out.print("Range (mm): ");
+               i = printBytes(data, i, 4);
+               System.out.print("Signal Photons: ");
+               i = printBytes(data, i, 2);
+               i++;
+               System.out.print("Reflectivity: ");
+               i = printBytes(data, i, 1);
+               i+=2;
+               System.out.print("NIR Photons: ");
+               i = printBytes(data, i, 2);
+               System.out.println();
             }
+
+            System.out.println();
+            i = printBytes(data, i, 4);
+
+            System.out.println("\n\n");
+
+            System.out.print("Timestamp: ");
+            i = printBytes(data, i, 8);
+            System.out.print("Frame ID: ");
+            i = printBytes(data, i, 2);
+            System.out.print("Measurement ID: ");
+            i = printBytes(data, i, 2);
+            System.out.print("Encoder Count: ");
+            i = printBytes(data, i, 4);
+            System.out.println();
+
+            for (int j = 0; j < 64; j++) {
+               System.out.println("Block " + j + ":");
+               System.out.print("Range (mm): ");
+               i = printBytes(data, i, 4);
+               System.out.print("Signal Photons: ");
+               i = printBytes(data, i, 2);
+               i++;
+               System.out.print("Reflectivity: ");
+               i = printBytes(data, i, 1);
+               i+=2;
+               System.out.print("NIR Photons: ");
+               i = printBytes(data, i, 2);
+               System.out.println();
+            }
+
+            System.out.println();
+            printBytes(data, i, 4);
+
+            System.out.println("\n\n");
          }
       });
+   }
+
+   private int printBytes(ByteBuf data, int index, int num) {
+      int shift = index % 4;
+      int modIndex = index - (index % 4);
+      long val = data.getUnsignedInt(modIndex);
+      val >>= shift;
+      switch(num) {
+         case 1:
+            val = val & 0xFF;
+            break;
+         case 2:
+            val = val & 0xFFFF;
+            break;
+         case 4:
+            break;
+         case 8:
+            val += data.getUnsignedInt(modIndex + 4) << 32;
+            break;
+      }
+
+      System.out.println(val == 4294967295L ? "FFFF" : val);
+
+      return index + num;
    }
 
    public void start()
