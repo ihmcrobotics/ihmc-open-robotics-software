350014883ee29808e71c8b93b1bfa4bec928dacf
Duncan Calvert <dcalvert@ihmc.us>
2022-05-18 17:11:25 -0500

    Make wrench action forces adjustable and add lifting motion to the box lift.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/behavior/editor/GDXHandWrenchAction.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/behavior/editor/GDXHandWrenchAction.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import controller_msgs.msg.dds.FrameInformation;
 import controller_msgs.msg.dds.HandWrenchTrajectoryMessage;
+import controller_msgs.msg.dds.WrenchTrajectoryPointMessage;
 import imgui.ImGui;
 import imgui.type.ImBoolean;
 import imgui.type.ImDouble;
@@ -15,6 +16,7 @@ import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.gdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.gdx.input.ImGui3DViewInput;
 import us.ihmc.humanoidRobotics.communication.packets.HumanoidMessageTools;
+import us.ihmc.idl.IDLSequence;
 import us.ihmc.robotics.robotSide.RobotSide;
 
 public class GDXHandWrenchAction implements GDXBehaviorAction
@@ -24,6 +26,7 @@ public class GDXHandWrenchAction implements GDXBehaviorAction
    private ROS2ControllerHelper ros2ControllerHelper;
    private final ImBoolean selected = new ImBoolean();
    private final ImDouble trajectoryTime = new ImDouble(1000.0);
+   private final ImDouble force = new ImDouble(20.0);
 
    public void create(ROS2ControllerHelper ros2ControllerHelper)
    {
@@ -58,6 +61,7 @@ public class GDXHandWrenchAction implements GDXBehaviorAction
    {
       ImGui.pushItemWidth(80.0f);
       ImGui.inputDouble(labels.get("Trajectory time"), trajectoryTime);
+      ImGui.inputDouble(labels.get("Force"), force);
       ImGui.popItemWidth();
    }
 
@@ -72,6 +76,7 @@ public class GDXHandWrenchAction implements GDXBehaviorAction
    {
       jsonNode.put("side", side.getLowerCaseName());
       jsonNode.put("trajectoryTime", trajectoryTime.get());
+      jsonNode.put("force", force.get());
    }
 
    @Override
@@ -79,6 +84,7 @@ public class GDXHandWrenchAction implements GDXBehaviorAction
    {
       setSide(RobotSide.getSideFromString(jsonNode.get("side").asText()));
       trajectoryTime.set(jsonNode.get("trajectoryTime").asDouble());
+      force.set(jsonNode.get("force").asDouble());
    }
 
    @Override
@@ -92,18 +98,27 @@ public class GDXHandWrenchAction implements GDXBehaviorAction
    {
       HandWrenchTrajectoryMessage handWrenchTrajectoryMessage = new HandWrenchTrajectoryMessage();
       handWrenchTrajectoryMessage.setRobotSide(side.toByte());
-      double time0 = 0.0;
-      Vector3D torque0 = new Vector3D();
-      double force = 20.0;
-      Vector3D force0 = new Vector3D(0.0, side == RobotSide.RIGHT ? force : -force, 0.0);
-      handWrenchTrajectoryMessage.getWrenchTrajectory().getWrenchTrajectoryPoints().add()
-                                 .set(HumanoidMessageTools.createWrenchTrajectoryPointMessage(time0, torque0, force0));
-      double time1 = trajectoryTime.get();
-      Vector3D torque1 = new Vector3D();
-      Vector3D force1 = new Vector3D(0.0, side == RobotSide.RIGHT ? force : -force, 0.0);
-      handWrenchTrajectoryMessage.getWrenchTrajectory().getWrenchTrajectoryPoints().add()
-                                 .set(HumanoidMessageTools.createWrenchTrajectoryPointMessage(time1, torque1, force1));
+//      double force = 4.2; // For 0.5 kg box
+      double force = this.force.get();
+      if (force > 0.0)
+      {
+         IDLSequence.Object<WrenchTrajectoryPointMessage> wrenchTrajectoryPoints
+               = handWrenchTrajectoryMessage.getWrenchTrajectory().getWrenchTrajectoryPoints();
+
+         double time0 = 0.0;
+         Vector3D torque0 = new Vector3D();
+         Vector3D force0 = new Vector3D(0.0, side == RobotSide.RIGHT ? force : -force, 0.0);
+         wrenchTrajectoryPoints.add().set(HumanoidMessageTools.createWrenchTrajectoryPointMessage(time0, torque0, force0));
+
+         double time1 = trajectoryTime.get();
+         Vector3D torque1 = new Vector3D();
+         Vector3D force1 = new Vector3D(0.0, side == RobotSide.RIGHT ? force : -force, 0.0);
+         wrenchTrajectoryPoints.add().set(HumanoidMessageTools.createWrenchTrajectoryPointMessage(time1, torque1, force1));
+      }
       handWrenchTrajectoryMessage.getWrenchTrajectory().getFrameInformation().setTrajectoryReferenceFrameId(FrameInformation.CHEST_FRAME);
+      handWrenchTrajectoryMessage.getWrenchTrajectory().setUseCustomControlFrame(true);
+      double handCenterOffset = 0.05;
+      handWrenchTrajectoryMessage.getWrenchTrajectory().getControlFramePose().setY(side == RobotSide.RIGHT ? -handCenterOffset : handCenterOffset);
 
       ros2ControllerHelper.publishToController(handWrenchTrajectoryMessage);
    }
