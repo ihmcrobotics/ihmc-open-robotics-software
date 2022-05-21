c74ad506e7a1a6dd472724cbc5dfa5fbe8de7f53
Robert Griffin <rgriffin@ihmc.us>
2022-05-21 10:28:32 -0500

    started working on a gpu implementation of the height map, copied from the ETH approach, using OpenCL

---
+++ ihmc-perception/src/main/java/us/ihmc/perception/gpuHeightMap/GPUHeightMap.java
@@ -0,0 +1,57 @@
+package us.ihmc.perception.gpuHeightMap;
+
+import sensor_msgs.msg.dds.PointCloud;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.tuple3D.Point3D32;
+import us.ihmc.idl.IDLSequence;
+import us.ihmc.perception.OpenCLFloatBuffer;
+import us.ihmc.perception.OpenCLManager;
+
+import java.nio.FloatBuffer;
+
+public class GPUHeightMap
+{
+   private final OpenCLManager openCLManager = new OpenCLManager();
+
+   private final OpenCLFloatBuffer localizationBuffer = new OpenCLFloatBuffer(14);
+   private OpenCLFloatBuffer pointCloudBuffer;
+
+   public GPUHeightMap()
+   {
+      openCLManager.create();
+
+      localizationBuffer.createOpenCLBufferObject(openCLManager);
+   }
+
+   // point cloud is expected to be in sensor frame
+   public void updateMap(PointCloud pointCloud, ReferenceFrame pointCloudFrame)
+   {
+      pointCloudBuffer = new OpenCLFloatBuffer(pointCloud.getPoints().size() * Float.BYTES);
+      pointCloudBuffer.createOpenCLBufferObject(openCLManager);
+
+      packPointCloudIntoFloatBUffer(pointCloud.getPoints(), pointCloudBuffer.getBackingDirectFloatBuffer());
+      populateParametersBuffer((float) 0.0, (float) 0.0, pointCloudFrame);
+   }
+
+   private void packPointCloudIntoFloatBUffer(IDLSequence.Object<Point3D32> points, FloatBuffer floatBufferToPack)
+   {
+      // TODO
+   }
+
+   private void populateParametersBuffer(float centerX, float centerY, ReferenceFrame pointCloudFrame)
+   {
+      int index = 0;
+      localizationBuffer.getBytedecoFloatBufferPointer().put(index++, centerX);
+      localizationBuffer.getBytedecoFloatBufferPointer().put(index++, centerY);
+      for (int i = 0; i < 3; i++)
+      {
+         for (int j = 0; j < 3; j++)
+            localizationBuffer.getBytedecoFloatBufferPointer().put(index++, (float) pointCloudFrame.getTransformToWorldFrame().getRotation().getElement(i, j));
+      }
+      for (int i = 0; i < 3; i++)
+         localizationBuffer.getBytedecoFloatBufferPointer().put(index++, (float) pointCloudFrame.getTransformToWorldFrame().getTranslation().getElement(i));
+
+      localizationBuffer.writeOpenCLBufferObject(openCLManager);
+   }
+
+}
