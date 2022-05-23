585e8e49d8cbb9eff4f480f47dde65df02d2cb25
Duncan Calvert <dcalvert@ihmc.us>
2022-05-23 16:43:30 -0500

    Refactor a bit. Make start stop button. Plot logging hz.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/GDXFFMPEGLoggingDemo.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/GDXFFMPEGLoggingDemo.java
@@ -1,7 +1,9 @@
 package us.ihmc.gdx.logging;
 
 import imgui.ImGui;
+import imgui.type.ImInt;
 import org.bytedeco.ffmpeg.avutil.AVRational;
+import org.bytedeco.ffmpeg.global.avutil;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.Mat;
 import us.ihmc.commons.thread.ThreadTools;
@@ -10,35 +12,41 @@ import us.ihmc.gdx.imgui.ImGuiPanel;
 import us.ihmc.gdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.gdx.perception.GDXCVImagePanel;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
+import us.ihmc.gdx.ui.tools.ImPlotFrequencyPlot;
 import us.ihmc.perception.BytedecoImage;
 import us.ihmc.perception.BytedecoTools;
 import us.ihmc.tools.thread.Activator;
-import us.ihmc.tools.time.FrequencyCalculator;
 
+import java.io.File;
 import java.nio.ByteOrder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
-
-import static org.bytedeco.ffmpeg.global.avutil.av_q2d;
 
 public class GDXFFMPEGLoggingDemo
 {
    public static final int WIDTH = 256;
    public static final int HEIGHT = 256;
-   public static final int FRAMERATE = 30;
    public static final int NICE_COLOR = 0xFFAA6600;
+   private final Activator nativesLoadedActivator = BytedecoTools.loadNativesOnAThread();
    private final GDXImGuiBasedUI baseUI = new GDXImGuiBasedUI(getClass(), "ihmc-open-robotics-software", "ihmc-high-level-behaviors/src/main/resources");
-   private final Activator nativesLoadedActivator;
    private GDXCVImagePanel imagePanel;
-   private final FrequencyCalculator frameReadFrequency = new FrequencyCalculator();
+   private ImPlotFrequencyPlot loggerPutFrequencyPlot;
+   private ImInt framerate = new ImInt(30);
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
+   private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+   private static final String logDirectory = System.getProperty("user.home") + File.separator + ".ihmc" + File.separator + "logs" + File.separator;
+   private String fileName;
 
    private BytedecoImage image;
    private FFMPEGLogger logger;
+   private final Random random = new Random();
+   private final byte[] data = new byte[4];
+   private int index = 0;
+   private volatile boolean logging = false;
 
    public GDXFFMPEGLoggingDemo()
    {
-      nativesLoadedActivator = BytedecoTools.loadNativesOnAThread();
-
       baseUI.launchGDXApplication(new Lwjgl3ApplicationAdapter()
       {
          @Override
@@ -49,47 +57,7 @@ public class GDXFFMPEGLoggingDemo
             ImGuiPanel panel = new ImGuiPanel("Image", this::renderImGuiWidgets);
             baseUI.getImGuiPanelManager().addPanel(panel);
 
-            image = new BytedecoImage(WIDTH, HEIGHT, opencv_core.CV_8UC4);
-            ThreadTools.startAThread(new Runnable()
-            {
-               final Random random = new Random();
-               final byte[] data = new byte[4];
-               int index = 0;
-
-               @Override
-               public void run()
-               {
-                  AVRational msBetweenFrames = new AVRational();
-                  msBetweenFrames.num(1);
-                  msBetweenFrames.den(FRAMERATE);
-
-                  while (true)
-                  {
-                     if (index % 10 == 0)
-                        random.nextBytes(data);
-
-                     if (imagePanel != null)
-                     {
-                        image.getBytedecoOpenCVMat().setTo(new Mat(data));
-                        for (int i = 0; i < 256; i++)
-                        {
-                           boolean d = ((index >> (31 - i / 8)) & 1) == 1;
-
-                           for (int j = 0; j < 8; j++)
-                              image.getBackingDirectByteBuffer().putInt((WIDTH * j + i) * 4, d ? NICE_COLOR : 0xFF000000);
-                        }
-
-                        if (logger != null)
-                           logger.put(image);
-
-                        index++;
-                     }
-
-                     //Using an AVRational helps ensure that we calculate fps the same way the logger does
-                     ThreadTools.sleep((int) (av_q2d(msBetweenFrames) * 1000));
-                  }
-               }
-            }, "ByteDeco Image Updater");
+            updateFileName();
          }
 
          @Override
@@ -97,22 +65,22 @@ public class GDXFFMPEGLoggingDemo
          {
             if (nativesLoadedActivator.poll())
             {
-               if (imagePanel == null)
+               if (nativesLoadedActivator.isNewlyActivated())
                {
+                  loggerPutFrequencyPlot = new ImPlotFrequencyPlot("FFMPEGLogger put (Hz)");
+
+                  image = new BytedecoImage(WIDTH, HEIGHT, opencv_core.CV_8UC4);
                   imagePanel = new GDXCVImagePanel("Sample Image", image);
 
                   baseUI.getImGuiPanelManager().addPanel(imagePanel.getVideoPanel());
                   baseUI.getPerspectiveManager().reloadPerspective();
 
-                  logger = new FFMPEGLogger(WIDTH, HEIGHT);
+                  boolean lossless = true;
+                  logger = new FFMPEGLogger(WIDTH, HEIGHT, lossless, fileName);
                }
 
-               frameReadFrequency.ping();
 
-               if (imagePanel != null)
-               {
-                  imagePanel.draw();
-               }
+               imagePanel.draw();
             }
 
             baseUI.renderBeforeOnScreenUI();
@@ -123,9 +91,65 @@ public class GDXFFMPEGLoggingDemo
          {
             ImGui.text("System native byte order: " + ByteOrder.nativeOrder().toString());
 
-            if (imagePanel != null)
+            ImGui.text("File name: " + fileName);
+            ImGui.text("Resolution: " + WIDTH + " x " + HEIGHT);
+            ImGui.inputInt(labels.get("framerate"), framerate, 1);
+
+            if (nativesLoadedActivator.peek())
             {
-               ImGui.text("Frame read frequency: " + frameReadFrequency.getFrequency());
+               if (!logging)
+               {
+                  if (ImGui.button(labels.get("Start logging")))
+                  {
+                     updateFileName();
+                     logging = true;
+                     ThreadTools.startAThread(this::loggingThread, "FFMPEGLogging");
+                  }
+               }
+               else
+               {
+                  if (ImGui.button(labels.get("Stop logging")))
+                  {
+                     logging = false;
+                  }
+               }
+
+               loggerPutFrequencyPlot.renderImGuiWidgets();
+            }
+         }
+
+         private void updateFileName()
+         {
+            fileName = logDirectory + dateFormat.format(new Date()) + "_FFMPEGLoggingDemo.webm";
+         }
+
+         private void loggingThread()
+         {
+            AVRational msBetweenFrames = new AVRational();
+            msBetweenFrames.num(1);
+            msBetweenFrames.den(framerate.get());
+
+            while (logging)
+            {
+               if (index % 10 == 0)
+                  random.nextBytes(data);
+
+               image.getBytedecoOpenCVMat().setTo(new Mat(data));
+               for (int i = 0; i < 256; i++)
+               {
+                  boolean d = ((index >> (31 - i / 8)) & 1) == 1;
+
+                  for (int j = 0; j < 8; j++)
+                     image.getBackingDirectByteBuffer().putInt((WIDTH * j + i) * 4, d ? NICE_COLOR : 0xFF000000);
+               }
+
+               loggerPutFrequencyPlot.ping();
+               logger.put(image);
+
+               index++;
+
+               // Using an AVRational helps ensure that we calculate fps the same way the logger does
+               ThreadTools.sleep((int) (avutil.av_q2d(msBetweenFrames) * 1000));
             }
          }
 
