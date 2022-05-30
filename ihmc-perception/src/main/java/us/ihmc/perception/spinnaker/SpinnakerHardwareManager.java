0745a78e3ff77883d765ae8d8abf0ea5699874b7
Duncan Calvert <dcalvert@ihmc.us>
2022-05-30 18:13:20 -0500

    Refactor cleanup for Blackfly stuff.

--- ihmc-perception/src/main/java/us/ihmc/perception/spinnaker/SpinnakerHardwareManager.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/spinnaker/SpinnakerHardwareManager.java
@@ -2,36 +2,35 @@ package us.ihmc.perception.spinnaker;
 
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.spinnaker.Spinnaker_C.*;
+import org.bytedeco.spinnaker.global.Spinnaker_C;
 
-import static org.bytedeco.spinnaker.global.Spinnaker_C.*;
 import static us.ihmc.perception.spinnaker.SpinnakerTools.assertNoError;
 
 public class SpinnakerHardwareManager
 {
-   private final spinSystem system;
-   private final spinCameraList cameras;
+   private final spinSystem spinSystem = new spinSystem();
+   private final spinCameraList spinCameraList = new spinCameraList();
 
    public SpinnakerHardwareManager()
    {
-      system = new spinSystem();
-      assertNoError(spinSystemGetInstance(system), "Unable to retrieve Spinnaker system instance!");
-
-      cameras = new spinCameraList();
-      assertNoError(spinCameraListCreateEmpty(cameras), "Unable to create camera list");
-      assertNoError(spinSystemGetCameras(system, cameras), "Unable to retrieve camera list from Spinnaker system");
+      assertNoError(Spinnaker_C.spinSystemGetInstance(spinSystem), "Unable to retrieve Spinnaker system instance!");
+      assertNoError(Spinnaker_C.spinCameraListCreateEmpty(spinCameraList), "Unable to create camera list");
+      assertNoError(Spinnaker_C.spinSystemGetCameras(spinSystem, spinCameraList), "Unable to retrieve camera list from Spinnaker system");
    }
 
-   public BytedecoBlackfly buildBlackfly(String serial)
+   public BytedecoBlackfly buildBlackfly(String serialNumber)
    {
-      return buildBlackfly(serial, "Continuous");
+      return buildBlackfly(serialNumber, "Continuous");
    }
 
-   public BytedecoBlackfly buildBlackfly(String serial, String acqMode) // acqMode = Single/Multi(?)/Continuous. Should be continuous in almost all cases
+   // acquisitionMode = Single/Multi(?)/Continuous. Should be continuous in almost all cases
+   public BytedecoBlackfly buildBlackfly(String serialNumber, String acquisitionMode)
    {
       spinCamera blackflyCamera = new spinCamera();
-      assertNoError(spinCameraListGetBySerial(cameras, new BytePointer(serial), blackflyCamera), "Unable to create spinCamera from serial number!");
+      assertNoError(Spinnaker_C.spinCameraListGetBySerial(spinCameraList, new BytePointer(serialNumber), blackflyCamera),
+                    "Unable to create spinCamera from serial number!");
       // Note: the BytedecoBlackfly class is responsible for releasing the new camera. This is done with the destroy() call
-      return new BytedecoBlackfly(blackflyCamera, acqMode, serial);
+      return new BytedecoBlackfly(blackflyCamera, acquisitionMode, serialNumber);
    }
 
    /**
@@ -39,8 +38,8 @@ public class SpinnakerHardwareManager
     */
    public void destroy()
    {
-      spinCameraListClear(cameras);
-      spinCameraListDestroy(cameras);
-      spinSystemReleaseInstance(system);
+      Spinnaker_C.spinCameraListClear(spinCameraList);
+      Spinnaker_C.spinCameraListDestroy(spinCameraList);
+      Spinnaker_C.spinSystemReleaseInstance(spinSystem);
    }
 }
