4a1d20b0590fc637b94663b6fca0a6eb71d00885
Robert Griffin <rgriffin@ihmc.us>
2022-05-30 09:29:35 -0500

    deleted unused parameters

--- ihmc-perception/src/main/java/us/ihmc/perception/gpuHeightMap/SimpleGPUHeightMapParameters.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/gpuHeightMap/SimpleGPUHeightMapParameters.java
@@ -6,11 +6,7 @@ public class SimpleGPUHeightMapParameters
    public double resolution = 0.025;
    // map's size in m
    public double mapLength = 3.5;
-
-   // points outside this distance are outliers
-   double mahalanobisThreshold = 2.0;
-   // cells with higher traversability are used for drift compensation
-   double traversabilityInlier = 0.9;
+   
    // points higher than this value from the sensor will be filtered out
    double maxHeightRange = 1.0;
    // points with shorter distance will be filtered out
@@ -21,12 +17,4 @@ public class SimpleGPUHeightMapParameters
    double rampedHeightRangeA = 0.3;
    // if z > max(d - rapmed_height_range_b, 0) * ramped_height_range_a + ramped_height_range_c, reject
    double rampedHeightRangeC = 0.2;
-   // point's noise is sensor_noise_factor * z^2 (z is distance from sensor).
-   double sensorNoiseFactor = 0.05;
-   // if point is an outlier, and this value to the cell's variance
-   double outlierVariance = 0.01;
-   // initial variance for each cell.
-   double initialVariance = 1000.0;
-   // max variance for each cell
-   double maxVariance = 100.0;
 }
