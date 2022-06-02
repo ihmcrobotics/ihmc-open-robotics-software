e525426c246ccf52db573f57732267932cfb669d
Perry MacMurray <pmacmurray@ihmc.org>
2022-06-02 14:32:33 -0500

    Use example webm in resources folder (with proper attribution) so if log directory is empty demo still runs

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/GDXFFMPEGPlaybackDemo.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/GDXFFMPEGPlaybackDemo.java
@@ -1,6 +1,7 @@
 package us.ihmc.gdx.logging;
 
 import imgui.ImGui;
+import imgui.extension.imguifiledialog.ImGuiFileDialog;
 import imgui.type.ImInt;
 import org.bytedeco.ffmpeg.ffmpeg;
 import org.bytedeco.opencv.global.opencv_core;
@@ -10,6 +11,8 @@ import us.ihmc.gdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.gdx.perception.GDXCVImagePanel;
 import us.ihmc.gdx.ui.GDXImGuiBasedUI;
 import us.ihmc.perception.BytedecoTools;
+import us.ihmc.tools.io.WorkspaceDirectory;
+import us.ihmc.tools.io.WorkspaceFile;
 import us.ihmc.tools.thread.Activator;
 
 import java.io.File;
@@ -18,7 +21,12 @@ import java.nio.ByteOrder;
 public class GDXFFMPEGPlaybackDemo
 {
    private final Activator nativesLoadedActivator = BytedecoTools.loadNativesOnAThread(opencv_core.class, ffmpeg.class);
-   private final File logDirectory = new File(System.getProperty("user.home") + File.separator + ".ihmc" + File.separator + "logs" + File.separator);
+   private final String logDirectory = System.getProperty("user.home") + File.separator + ".ihmc" + File.separator + "logs" + File.separator;
+
+   //example.webm contains licensing information at attribution.txt in same directory. Used with permission from https://en.wikipedia.org/wiki/File:Schlossbergbahn.webm
+   private final WorkspaceFile exampleVideo = new WorkspaceFile(new WorkspaceDirectory("ihmc-open-robotics-software",
+                                                                                       "ihmc-high-level-behaviors/src/main/resources/us/ihmc/gdx/logging"), "example.webm");
+
    private final GDXImGuiBasedUI baseUI = new GDXImGuiBasedUI(getClass(), "ihmc-open-robotics-software", "ihmc-high-level-behaviors/src/main/resources");
    private GDXCVImagePanel imagePanel;
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
@@ -47,7 +55,7 @@ public class GDXFFMPEGPlaybackDemo
             {
                if (nativesLoadedActivator.isNewlyActivated())
                {
-                  video = new FFMPEGVideoPlaybackManager(logDirectory.listFiles()[0].getAbsolutePath());
+                  video = new FFMPEGVideoPlaybackManager(exampleVideo.getFilePath().toString());
 
                   imagePanel = new GDXCVImagePanel("Playback Video", video.getImage());
 
@@ -67,21 +75,33 @@ public class GDXFFMPEGPlaybackDemo
          private void renderImGuiWidgets()
          {
             ImGui.text("System native byte order: " + ByteOrder.nativeOrder().toString());
+
+            ImGui.separator();
+
+            ImGui.text("TODO put file dialog here");
+
+            ImGui.separator();
+
             if (video != null)
                ImGui.text("Resolution: " + video.getWidth() + " x " + video.getHeight());
 
             if (video != null)
-               ImGui.text("Pos: " + video.getCurrentTimestampInMillis() / 1000 + "s of " + video.getVideoDurationInMillis() / 1000 + 's');
+            {
+               ImGui.progressBar(video.getCurrentTimestampInMillis() / (float) video.getVideoDurationInMillis());
 
-            if (ImGui.button("Play"))
-               video.play();
+               if (ImGui.button("Play"))
+                  video.play();
+               ImGui.sameLine();
+               if (ImGui.button("Pause"))
+                  video.pause();
+               ImGui.sameLine();
+               ImGui.text(video.getCurrentTimestampInMillis() / 1000 + "s of " + video.getVideoDurationInMillis() / 1000 + 's');
 
-            if (ImGui.button("Pause"))
-               video.pause();
-
-            ImGui.inputInt("Go to (s) ", seekLocation);
-            if (ImGui.button("Seek"))
-               video.seek(seekLocation.get() * 1000);
+               ImGui.inputInt("##seekBox", seekLocation);
+               ImGui.sameLine();
+               if (ImGui.button("Seek"))
+                  video.seek(seekLocation.get() * 1000);
+            }
          }
 
          @Override
