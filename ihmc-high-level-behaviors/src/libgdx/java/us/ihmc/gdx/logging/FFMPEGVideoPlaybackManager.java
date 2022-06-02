18b11dd616eff97cd282e03a0c953c84eaa76895
Perry MacMurray <pmacmurray@ihmc.org>
2022-06-02 11:37:05 -0500

    Thread.wait() does not wait for the thread

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/FFMPEGVideoPlaybackManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/FFMPEGVideoPlaybackManager.java
@@ -73,7 +73,7 @@ public class FFMPEGVideoPlaybackManager
 
       try
       {
-         currentPlaybackThread.wait();
+         currentPlaybackThread.join();
       }
       catch (InterruptedException ex)
       {
