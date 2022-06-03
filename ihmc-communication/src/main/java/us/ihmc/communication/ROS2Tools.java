c4fea4539eb70e02ee60e51957a18673b1ab6756
Duncan Calvert <dcalvert@ihmc.us>
2022-06-02 22:20:43 -0500

    Setup external hand force points with control over ROS 2 from behavior UI.

--- ihmc-communication/src/main/java/us/ihmc/communication/ROS2Tools.java
+++ ihmc-communication/src/main/java/us/ihmc/communication/ROS2Tools.java
@@ -157,6 +157,8 @@ public class ROS2Tools
    private static final ROS2Topic<BipedalSupportPlanarRegionParametersMessage> BIPEDAL_SUPPORT_REGION_PARAMETERS
          = BIPED_SUPPORT_REGION_PUBLISHER.withInput().withType(BipedalSupportPlanarRegionParametersMessage.class);
 
+   public static final ROS2Topic<Float64> BOX_MASS = IHMC_ROOT.withSuffix("box_mass").withType(Float64.class);
+
    public static final Function<String, String> NAMED_BY_TYPE = typeName -> typeName;
 
    public static ROS2Topic<BipedalSupportPlanarRegionParametersMessage> getBipedalSupportRegionParametersTopic(String robotName)
