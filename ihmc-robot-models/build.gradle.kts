d9145f230f151dddbafbb9ad099c9ff6bb8d2221
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-07 11:18:25 -0500

    Removed dependency, going to rely on transitive dependencies.

--- ihmc-robot-models/build.gradle.kts
+++ ihmc-robot-models/build.gradle.kts
@@ -17,7 +17,6 @@ mainDependencies {
    api("com.google.guava:guava:18.0")
 
    api("us.ihmc:ihmc-robot-description:0.21.4")
-   api("us.ihmc:ihmc-graphics-description:0.19.4")
    api("us.ihmc:scs2-definition:0.8.3-bullet-alpha-5")
    api("us.ihmc:ihmc-robotics-toolkit:source")
 }
