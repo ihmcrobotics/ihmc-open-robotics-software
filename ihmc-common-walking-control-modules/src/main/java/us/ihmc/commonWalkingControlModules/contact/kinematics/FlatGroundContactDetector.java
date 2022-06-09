6e18af247ac27b7ab7efe1c14c5dd078bef423d3
smccrory <smccrory@ihmc.us>
2022-06-09 17:02:28 -0500

    Stubbing out code to broadcast mcbs

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/contact/kinematics/FlatGroundContactDetector.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/contact/kinematics/FlatGroundContactDetector.java
@@ -1,9 +1,9 @@
 package us.ihmc.commonWalkingControlModules.contact.kinematics;
 
+import controller_msgs.msg.dds.MultiContactBalanceStatus;
 import us.ihmc.commonWalkingControlModules.controllers.Updatable;
 import us.ihmc.communication.controllerAPI.StatusMessageOutputManager;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
-import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.robotics.physics.Collidable;
@@ -31,10 +31,9 @@ public class FlatGroundContactDetector implements Updatable
    protected final List<RigidBodyBasics> contactableRigidBodies = new ArrayList<>();
    protected final List<ContactableShape> allCollidables = new ArrayList<>();
    protected final Map<RigidBodyBasics, List<ContactableShape>> contactableRigidBodyCollidables = new HashMap<>();
+   protected final Map<RigidBodyBasics, List<DetectedContactPoint>> contactPoints = new HashMap<>();
 
-   protected final Map<RigidBodyBasics, List<FramePoint3DReadOnly>> contactPointMap = new HashMap<>();
-   protected final Map<RigidBodyBasics, FrameVector3DReadOnly> contactNormalMap = new HashMap<>(); // down the road consider different normals per rigid body
-   protected final Map<RigidBodyBasics, List<ContactPointVisualization>> contactPointVisualizations = new HashMap<>();
+   private final MultiContactBalanceStatus multiContactBalanceStatus = new MultiContactBalanceStatus();
 
    public FlatGroundContactDetector(RigidBodyBasics rootBody,
                                     RobotCollisionModel collisionModel,
@@ -65,15 +64,15 @@ public class FlatGroundContactDetector implements Updatable
          allCollidables.addAll(rigidBodyCollidables);
          contactableRigidBodyCollidables.put(rigidBody, rigidBodyCollidables);
 
-         List<ContactPointVisualization> contactPointVisualizations = new ArrayList<>();
+         List<DetectedContactPoint> contactPoints = new ArrayList<>();
          for (int j = 0; j < 4 * collidables.size(); j++)
          {
-            ContactPointVisualization contactPointVisualization = new ContactPointVisualization(registry, graphicsListRegistry);
-            contactPointVisualization.setVerticalContactNormal();
-            contactPointVisualizations.add(contactPointVisualization);
+            DetectedContactPoint detectedContactPoint = new DetectedContactPoint(rigidBody.getName() + j, registry, graphicsListRegistry);
+            detectedContactPoint.setVerticalContactNormal();
+            contactPoints.add(detectedContactPoint);
          }
 
-         this.contactPointVisualizations.put(rigidBody, contactPointVisualizations);
+         this.contactPoints.put(rigidBody, contactPoints);
       }
    }
 
@@ -92,7 +91,7 @@ public class FlatGroundContactDetector implements Updatable
       for (int i = 0; i < contactableRigidBodies.size(); i++)
       {
          List<ContactableShape> rigidBodyCollidables = contactableRigidBodyCollidables.get(contactableRigidBodies.get(i));
-         List<ContactPointVisualization> visualization = contactPointVisualizations.get(contactableRigidBodies.get(i));
+         List<DetectedContactPoint> visualization = contactPoints.get(contactableRigidBodies.get(i));
 
          List<FramePoint3DReadOnly> contactPoints = new ArrayList<>();
          for (int j = 0; j < rigidBodyCollidables.size(); j++)
@@ -105,6 +104,20 @@ public class FlatGroundContactDetector implements Updatable
             visualization.get(j).getContactPointPosition().set(contactPoints.get(j));
          }
       }
+
+      if (statusOutputManager != null)
+      {
+         multiContactBalanceStatus.getContactPointsInWorld().clear();
+         multiContactBalanceStatus.getSupportRigidBodyIds().clear();
+         multiContactBalanceStatus.getSurfaceNormalsInWorld().clear();
+
+
+      }
+   }
+
+   public List<DetectedContactPoint> getContactPoints(RigidBodyBasics rigidBody)
+   {
+      return contactPoints.get(rigidBody);
    }
 
    protected void reset()
@@ -112,7 +125,7 @@ public class FlatGroundContactDetector implements Updatable
       for (int i = 0; i < contactableRigidBodies.size(); i++)
       {
          RigidBodyBasics rigidBody = contactableRigidBodies.get(i);
-         List<ContactPointVisualization> visualization = contactPointVisualizations.get(rigidBody);
+         List<DetectedContactPoint> visualization = contactPoints.get(rigidBody);
          for (int j = 0; j < visualization.size(); j++)
          {
             visualization.get(j).hide();
