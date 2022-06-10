c51a13ffdb0f6afc33e88e38ffc7578dfcad9377
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-10 15:31:25 -0500

    :arrow_up: ihmc-javafx-toolkit 17-0.21.1

--- ihmc-robot-data-visualizer/build.gradle.kts
+++ ihmc-robot-data-visualizer/build.gradle.kts
@@ -21,7 +21,7 @@ mainDependencies {
    api("us.ihmc:euclid:0.17.2")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:simulation-construction-set:0.22.1")
-   api("us.ihmc:ihmc-javafx-toolkit:0.21.1")
+   api("us.ihmc:ihmc-javafx-toolkit:17-0.21.1")
    api("us.ihmc:ihmc-graphics-javafx:source")
    api("us.ihmc:ihmc-model-file-loader:source")
    api("us.ihmc:ihmc-communication:source")
