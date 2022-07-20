69a61636d3de8a87968a83b20f9cad48b90e9559
Allen Reed <areed@ihmc.org>
2022-07-20 13:36:15 -0500

    Feet alternate sides automaticaaly - in progress

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/affordances/SingleFootstep.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/affordances/SingleFootstep.java
@@ -8,25 +8,25 @@ import us.ihmc.gdx.sceneManager.GDX3DScene;
 import us.ihmc.gdx.sceneManager.GDXSceneLevel;
 import us.ihmc.gdx.simulation.environment.GDXModelInstance;
 import us.ihmc.gdx.tools.GDXModelLoader;
+import us.ihmc.robotics.robotSide.RobotSide;
 
 public class SingleFootstep
 {
    private GDXModelInstance footstepModelInstance;
 
-   public enum FootstepSide {LEFT, RIGHT, NONE};
-   private FootstepSide footstepSide = FootstepSide.NONE;
+   private RobotSide footstepSide;
    ReferenceFrame referenceFrameFootstep;
 
    public Pose3D footPose;
 
-   public SingleFootstep(GDX3DScene primaryScene, FootstepSide footstepSide)
+   public SingleFootstep(GDX3DScene primaryScene, RobotSide footstepSide)
    {
       this.footstepSide = footstepSide;
-      if (footstepSide.equals(FootstepSide.LEFT))
+      if (footstepSide.equals(RobotSide.LEFT))
       {
          footstepModelInstance = new GDXModelInstance(GDXModelLoader.load("models/footsteps/footstep_left.g3dj"));
       }
-      else if (footstepSide.equals(FootstepSide.RIGHT))
+      else if (footstepSide.equals(RobotSide.RIGHT))
       {
          footstepModelInstance = new GDXModelInstance(GDXModelLoader.load("models/footsteps/footstep_right.g3dj"));
       }
@@ -52,7 +52,7 @@ public class SingleFootstep
 
    }
 
-   public FootstepSide getFootstepSide()
+   public RobotSide getFootstepSide()
    {
       return footstepSide;
    }
