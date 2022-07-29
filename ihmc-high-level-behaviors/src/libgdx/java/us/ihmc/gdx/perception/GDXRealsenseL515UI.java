e9cb67dd3320a1e797617cd53ba2191457141484
Duncan Calvert <dcalvert@ihmc.us>
2022-07-28 22:47:06 -0500

    Put L515 color and depth fusion into OpenCL.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/perception/GDXRealsenseL515UI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/perception/GDXRealsenseL515UI.java
@@ -2,14 +2,18 @@ package us.ihmc.gdx.perception;
 
 import boofcv.struct.calib.CameraPinholeBrown;
 import imgui.ImGui;
+import imgui.flag.ImGuiInputTextFlags;
 import imgui.type.ImFloat;
 import imgui.type.ImInt;
 import org.bytedeco.librealsense2.global.realsense2;
+import org.bytedeco.opencl._cl_kernel;
+import org.bytedeco.opencl._cl_program;
+import org.bytedeco.opencl.global.OpenCL;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.global.opencv_imgproc;
 import org.bytedeco.opencv.opencv_core.Mat;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.gdx.GDXPointCloudRenderer;
 import us.ihmc.gdx.Lwjgl3ApplicationAdapter;
 import us.ihmc.gdx.imgui.ImGuiPanel;
@@ -42,7 +46,8 @@ public class GDXRealsenseL515UI
    private GDXCVImagePanel depthImagePanel;
    private GDXCVImagePanel colorImagePanel;
    private Mat depthU16C1Image;
-   private Mat color8UC3Image;
+   private Mat colorRGBImageMat;
+   private BytedecoImage colorRGBImage;
    private BytedecoImage depth32FC1Image;
    private FrequencyCalculator frameReadFrequency = new FrequencyCalculator();
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
@@ -50,9 +55,19 @@ public class GDXRealsenseL515UI
    private final ImFloat receiverSensitivity = new ImFloat(0.5f);
    private final ImInt digitalGain = new ImInt(realsense2.RS2_DIGITAL_GAIN_LOW);
    private final String[] digitalGains = new String[] { "AUTO", "LOW", "HIGH" };
+   private static final int RGBA8888_WHITE = (255 << 24) | (255 << 16) | (255 << 8) | 255;
    private GDXPointCloudRenderer pointCloudRenderer;
-   private final OpenCLManager openCLManager = new OpenCLManager();
+   private OpenCLManager openCLManager;
    private final FramePoint3D framePoint = new FramePoint3D();
+   private final ImFloat focalLength = new ImFloat(0.00254f); // should be 1.88mm, but tuned it;
+   private final ImFloat cmosWidth = new ImFloat(0.0036894f); // 1/6" format
+   private final ImFloat cmosHeight = new ImFloat(0.0020753f); // 1/6" format
+   private OpenCLFloatBuffer parametersBuffer;
+   private _cl_program openCLProgram;
+   private _cl_kernel createPointCloudKernel;
+   private OpenCLFloatBuffer pointCloudRenderingBuffer;
+   private int numberOfDepthPoints;
+   private boolean firstRun = true;
 
    public GDXRealsenseL515UI()
    {
@@ -88,6 +103,7 @@ public class GDXRealsenseL515UI
                   l515.enableColor(1280, 720, 30);
                   l515.initialize();
 
+                  openCLManager = new OpenCLManager();
                   openCLManager.create();
                   pointCloudRenderer = new GDXPointCloudRenderer();
                }
@@ -105,15 +121,25 @@ public class GDXRealsenseL515UI
                      depthImagePanel = new GDXCVImagePanel("L515 Depth", l515.getDepthWidth(), l515.getDepthHeight());
                      baseUI.getImGuiPanelManager().addPanel(depthImagePanel.getVideoPanel());
 
-                     MutableBytePointer colorFrameData = l515.getColorFrameData();
-                     color8UC3Image = new Mat(l515.getColorHeight(), l515.getColorWidth(), opencv_core.CV_8UC3, colorFrameData);
+//                     colorRGBImageMat = new Mat(l515.getColorHeight(), l515.getColorWidth(), opencv_core.CV_8UC3, l515.getColorFrameData());
+                     colorRGBImage = new BytedecoImage(l515.getColorWidth(), l515.getColorHeight(), opencv_core.CV_8UC3);
                      colorImagePanel = new GDXCVImagePanel("L515 Color", l515.getColorWidth(), l515.getColorHeight());
                      baseUI.getImGuiPanelManager().addPanel(colorImagePanel.getVideoPanel());
 
                      baseUI.getPerspectiveManager().reloadPerspective();
 
-                     pointCloudRenderer.create(l515.getDepthWidth() * l515.getDepthHeight());
+                     numberOfDepthPoints = l515.getDepthWidth() * l515.getDepthHeight();
+                     int pointCloudBytesLength = pointCloudRenderer.getFloatsPerVertex();
+
+                     parametersBuffer = new OpenCLFloatBuffer(23);
+
+                     openCLProgram = openCLManager.loadProgram("RealsenseL515Fusion");
+                     createPointCloudKernel = openCLManager.createKernel(openCLProgram, "createPointCloud");
+
+                     pointCloudRenderer.create(numberOfDepthPoints);
                      baseUI.getPrimaryScene().addRenderableProvider(pointCloudRenderer, GDXSceneLevel.MODEL);
+
+                     pointCloudRenderingBuffer = new OpenCLFloatBuffer(numberOfDepthPoints * 8, pointCloudRenderer.getVertexBuffer());
                   }
 
                   frameReadFrequency.ping();
@@ -121,31 +147,83 @@ public class GDXRealsenseL515UI
 
                   depthImagePanel.drawFloatImage(depth32FC1Image.getBytedecoOpenCVMat());
 
-                  opencv_imgproc.cvtColor(color8UC3Image, colorImagePanel.getBytedecoImage().getBytedecoOpenCVMat(), opencv_imgproc.COLOR_RGB2RGBA);
+                  colorRGBImage.changeAddress(l515.getColorFrameData().address());
+//                  colorRGBImageMat.copyTo(colorRGBImage.getBytedecoOpenCVMat());
+
+//                  opencv_imgproc.cvtColor(color8UC3Image, colorImagePanel.getBytedecoImage().getBytedecoOpenCVMat(), opencv_imgproc.COLOR_RGB2RGBA);
+                  opencv_imgproc.cvtColor(colorRGBImage.getBytedecoOpenCVMat(),
+                                          colorImagePanel.getBytedecoImage().getBytedecoOpenCVMat(),
+                                          opencv_imgproc.COLOR_RGB2RGBA);
                   colorImagePanel.draw();
 
-                  pointCloudRenderer.prepareVertexBufferForAddingPoints();
                   CameraPinholeBrown depthCameraIntrinsics = l515.getDepthCameraIntrinsics();
 
-                  // TODO: Put in OpenCL
-                  for (int x = 0; x < l515.getDepthWidth(); x++)
+                  double cmosSensorDiagonal = 0.004233; // format 1/6"
+                  double cmosWidthLocal = cmosWidth.get();
+                  double cmosHeightLocal = cmosHeight.get();
+                  double halfCMOSWidth = cmosWidthLocal / 2.0;
+                  double halfCMOSHeight = cmosHeightLocal / 2.0;
+
+//                  double focalLength = 0.00188; // 1.88 focal length
+                  double focalLengthLocal = focalLength.get();
+                  double focalPointToSensorEdgeX = 0.0026339;
+                  double focalPointToSensorEdgeY = 0.0021474;
+
+                  double cmosToPixelsX = l515.getColorWidth() / cmosWidthLocal;
+                  double cmosToPixelsY = l515.getColorHeight() / cmosHeightLocal;
+
+                  RigidBodyTransform transformToWorldFrame = cameraPoseGizmo.getGizmoFrame().getTransformToWorldFrame();
+
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(0, focalLength.get());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(1, cmosWidth.get());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(2, cmosHeight.get());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(3, transformToWorldFrame.getTranslation().getX32());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(4, transformToWorldFrame.getTranslation().getY32());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(5, transformToWorldFrame.getTranslation().getZ32());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(6, (float) transformToWorldFrame.getRotation().getM00());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(7, (float) transformToWorldFrame.getRotation().getM01());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(8, (float) transformToWorldFrame.getRotation().getM02());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(9, (float) transformToWorldFrame.getRotation().getM10());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(10, (float) transformToWorldFrame.getRotation().getM11());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(11, (float) transformToWorldFrame.getRotation().getM12());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(12, (float) transformToWorldFrame.getRotation().getM20());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(13, (float) transformToWorldFrame.getRotation().getM21());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(14, (float) transformToWorldFrame.getRotation().getM22());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(15, (float) depthCameraIntrinsics.getCx());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(16, (float) depthCameraIntrinsics.getCy());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(17, (float) depthCameraIntrinsics.getFx());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(18, (float) depthCameraIntrinsics.getFy());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(19, (float) l515.getDepthWidth());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(20, (float) l515.getDepthHeight());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(21, (float) l515.getColorWidth());
+                  parametersBuffer.getBytedecoFloatBufferPointer().put(22, (float) l515.getColorHeight());
+
+                  if (firstRun)
                   {
-                     for (int y = 0; y < l515.getDepthHeight(); y++)
-                     {
-                        float eyeDepth = depth32FC1Image.getFloat(x, y);
-
-                        framePoint.setToZero(cameraPoseGizmo.getGizmoFrame());
-                        framePoint.setX(eyeDepth);
-                        framePoint.setY(-(x - depthCameraIntrinsics.getCx()) / depthCameraIntrinsics.getFx() * eyeDepth);
-                        framePoint.setZ(-(y - depthCameraIntrinsics.getCy()) / depthCameraIntrinsics.getFy() * eyeDepth);
-
-                        framePoint.changeFrame(ReferenceFrame.getWorldFrame());
-
-                        pointCloudRenderer.putVertex(framePoint);
-                     }
+                     firstRun = false;
+                     depth32FC1Image.createOpenCLImage(openCLManager, OpenCL.CL_MEM_READ_ONLY);
+//                     colorRGBImage.createOpenCLImage(openCLManager, OpenCL.CL_MEM_READ_ONLY);
+                     colorImagePanel.getBytedecoImage().createOpenCLImage(openCLManager, OpenCL.CL_MEM_READ_ONLY);
+                     pointCloudRenderingBuffer.createOpenCLBufferObject(openCLManager);
+                     parametersBuffer.createOpenCLBufferObject(openCLManager);
+                  }
+                  else
+                  {
+                     depth32FC1Image.writeOpenCLImage(openCLManager);
+//                     colorRGBImage.writeOpenCLImage(openCLManager);
+                     colorImagePanel.getBytedecoImage().writeOpenCLImage(openCLManager);
+                     parametersBuffer.writeOpenCLBufferObject(openCLManager);
                   }
 
-                  pointCloudRenderer.updateMeshFastest();
+                  openCLManager.setKernelArgument(createPointCloudKernel, 0, depth32FC1Image.getOpenCLImageObject());
+                  openCLManager.setKernelArgument(createPointCloudKernel, 1, colorImagePanel.getBytedecoImage().getOpenCLImageObject());
+                  openCLManager.setKernelArgument(createPointCloudKernel, 2, pointCloudRenderingBuffer.getOpenCLBufferObject());
+                  openCLManager.setKernelArgument(createPointCloudKernel, 3, parametersBuffer.getOpenCLBufferObject());
+                  openCLManager.execute2D(createPointCloudKernel, l515.getDepthWidth(), l515.getDepthHeight());
+                  pointCloudRenderingBuffer.readOpenCLBufferObject(openCLManager);
+                  openCLManager.finish();
+
+                  pointCloudRenderer.updateMeshFastest(numberOfDepthPoints);
                }
             }
 
@@ -162,6 +240,10 @@ public class GDXRealsenseL515UI
                ImGui.text("Depth frame data size: " + l515.getDepthFrameDataSize());
                ImGui.text("Frame read frequency: " + frameReadFrequency.getFrequency());
                ImGui.text("Depth to meters conversion: " + l515.getDepthToMeterConversion());
+
+               volatileInputFloat(labels.get("Focal length"), focalLength, 0.00001f);
+               volatileInputFloat(labels.get("CMOS width"), cmosWidth, 0.00001f);
+               volatileInputFloat(labels.get("CMOS height"), cmosHeight, 0.00001f);
 
                if (ImGui.sliderFloat(labels.get("Laser power"), laserPower.getData(), 0.0f, 100.0f))
                {
@@ -215,9 +297,9 @@ public class GDXRealsenseL515UI
 
                for (int i = 0; i < 5; i++)
                {
-                  printBytes(color8UC3Image.ptr(0, i).get(0),
-                             color8UC3Image.ptr(0, i).get(1),
-                             color8UC3Image.ptr(0, i).get(2));
+                  printBytes(colorRGBImage.getBytedecoOpenCVMat().ptr(0, i).get(0),
+                             colorRGBImage.getBytedecoOpenCVMat().ptr(0, i).get(1),
+                             colorRGBImage.getBytedecoOpenCVMat().ptr(0, i).get(2));
                }
 
                ImGui.text("Color R G B A:");
@@ -231,6 +313,13 @@ public class GDXRealsenseL515UI
                              colorImagePanel.getBytedecoImage().getBytedecoOpenCVMat().ptr(0, i).get(3));
                }
             }
+         }
+
+         private boolean volatileInputFloat(String label, ImFloat imFloat, float step)
+         {
+            int inputTextFlags = ImGuiInputTextFlags.None;
+            inputTextFlags += ImGuiInputTextFlags.EnterReturnsTrue;
+            return ImGui.inputFloat(label, imFloat, step, 0, "%.6f", inputTextFlags);
          }
 
          private void printBytes(byte byte0, byte byte1, byte byte2, byte byte3)
@@ -262,6 +351,7 @@ public class GDXRealsenseL515UI
          public void dispose()
          {
             baseUI.dispose();
+            openCLManager.destroy();
             l515.deleteDevice();
             realSenseHardwareManager.deleteContext();
          }
