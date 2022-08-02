b70152ef71068c9ee9e78b76f2e7c08490adaae8
Duncan Calvert <dcalvert@ihmc.us>
2022-08-02 18:36:20 -0500

    Make some new check pitch and yaw sliders.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/teleoperation/GDXTeleoperationParameters.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/teleoperation/GDXTeleoperationParameters.java
@@ -10,8 +10,13 @@ public class GDXTeleoperationParameters extends StoredPropertySet
    public static final StoredPropertyKeyList keys = new StoredPropertyKeyList();
 
    public static final DoubleStoredPropertyKey trajectoryTime = keys.addDoubleKey("Trajectory time");
+   public static final DoubleStoredPropertyKey chestOrientationVelocity = keys.addDoubleKey("Chest orientation velocity");
    public static final DoubleStoredPropertyKey swingTime = keys.addDoubleKey("Swing time");
    public static final DoubleStoredPropertyKey transferTime = keys.addDoubleKey("Transfer time");
+   public static final DoubleStoredPropertyKey chestMinimumPitch = keys.addDoubleKey("Chest minimum pitch");
+   public static final DoubleStoredPropertyKey chestMaximumPitch = keys.addDoubleKey("Chest maximum pitch");
+   public static final DoubleStoredPropertyKey chestMinimumYaw = keys.addDoubleKey("Chest minimum yaw");
+   public static final DoubleStoredPropertyKey chestMaximumYaw = keys.addDoubleKey("Chest maximum yaw");
 
    public GDXTeleoperationParameters(String robotRepoName, String robotSubsequentPathToResourceFolder, String robotName)
    {
@@ -23,6 +28,11 @@ public class GDXTeleoperationParameters extends StoredPropertySet
       return get(trajectoryTime);
    }
 
+   public double getChestOrientationVelocity()
+   {
+      return get(chestOrientationVelocity);
+   }
+
    public double getSwingTime()
    {
       return get(swingTime);
@@ -31,5 +41,25 @@ public class GDXTeleoperationParameters extends StoredPropertySet
    public double getTransferTime()
    {
       return get(transferTime);
+   }
+
+   public double getChestMinimumPitch()
+   {
+      return get(chestMinimumPitch);
+   }
+
+   public double getChestMaximumPitch()
+   {
+      return get(chestMaximumPitch);
+   }
+
+   public double getChestMinimumYaw()
+   {
+      return get(chestMinimumYaw);
+   }
+
+   public double getChestMaximumYaw()
+   {
+      return get(chestMaximumYaw);
    }
 }
