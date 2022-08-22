e89a07b47da08b1808733b0c77eedd129b9d15a0
Sylvain Bertrand <sbertrand@ihmc.org>
2022-08-22 14:07:00 -0500

    Fixing compile error

--- valkyrie/src/main/java/us/ihmc/valkyrie/simulation/ValkyriePlanarRegionPositionControlSimulation.java
+++ valkyrie/src/main/java/us/ihmc/valkyrie/simulation/ValkyriePlanarRegionPositionControlSimulation.java
@@ -179,7 +179,7 @@ public class ValkyriePlanarRegionPositionControlSimulation
                                                          commandInputManager,
                                                          statusOutputManager,
                                                          controlledJoints,
-                                                         controllerToolbox,
+                                                         controllerToolbox.getYoTime(),
                                                          highLevelControllerParameters,
                                                          highLevelControllerOutput);
          }
