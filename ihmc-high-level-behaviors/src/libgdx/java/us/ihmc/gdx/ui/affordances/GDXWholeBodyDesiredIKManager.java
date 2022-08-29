cf82b98556ead84f07e6d347a23420bc63106f5d
Duncan Calvert <dcalvert@ihmc.org>
2022-08-28 22:53:58 -0500

    Extract GDXArmDesiredIKManager.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/affordances/GDXWholeBodyDesiredIKManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/affordances/GDXWholeBodyDesiredIKManager.java
@@ -10,22 +10,14 @@ import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.communication.packets.MessageTools;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.gdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.gdx.ui.teleoperation.GDXTeleoperationParameters;
 import us.ihmc.humanoidRobotics.communication.packets.HumanoidMessageTools;
 import us.ihmc.log.LogTools;
-import us.ihmc.mecano.multiBodySystem.interfaces.JointBasics;
-import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
-import us.ihmc.mecano.tools.MultiBodySystemTools;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
-import us.ihmc.robotics.kinematics.DdoglegInverseKinematicsCalculator;
-import us.ihmc.robotics.kinematics.InverseKinematicsCalculator;
 import us.ihmc.robotics.partNames.ArmJointName;
-import us.ihmc.robotics.referenceFrames.ModifiableReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
-import us.ihmc.robotics.screwTheory.GeometricJacobian;
 import us.ihmc.tools.thread.MissingThreadTools;
 
 public class GDXWholeBodyDesiredIKManager
@@ -41,22 +33,8 @@ public class GDXWholeBodyDesiredIKManager
    private final ArmJointName[] armJointNames;
    private HandDataType handPoseDataTypeToSend = HandDataType.JOINT_ANGLES;
 
-   // returned as output
-   private final SideDependentList<GeometricJacobian> desiredArmJacobians = new SideDependentList<>();
+   private final SideDependentList<GDXArmDesiredIKManager> armManagers = new SideDependentList<>(GDXArmDesiredIKManager::new);
 
-   // passed to IK solvers
-   private final SideDependentList<GeometricJacobian> workArmJacobians = new SideDependentList<>();
-   private final SideDependentList<GeometricJacobian> actualArmJacobians = new SideDependentList<>();
-
-   private static final int INVERSE_KINEMATICS_CALCULATIONS_PER_UPDATE = 5;
-   private int maxIterations = 500;
-
-   private final SideDependentList<FramePose3D> correctedDesiredHandControlFramePoses = new SideDependentList<>(new FramePose3D(), new FramePose3D());
-   private final SideDependentList<FramePose3D> lastCorrectedDesiredHandControlFramePoses = new SideDependentList<>(new FramePose3D(), new FramePose3D());
-   private final SideDependentList<Boolean> ikFoundASolution = new SideDependentList<>();
-   private final SideDependentList<InverseKinematicsCalculator> inverseKinematicsCalculators = new SideDependentList<>();
-   private final SideDependentList<RigidBodyTransform> controlToWristTransforms = new SideDependentList<>();
-   private final ModifiableReferenceFrame temporaryFrame = new ModifiableReferenceFrame(ReferenceFrame.getWorldFrame());
    private volatile boolean readyToSolve = true;
    private volatile boolean readyToCopySolution = false;
 
@@ -85,38 +63,7 @@ public class GDXWholeBodyDesiredIKManager
 
       for (RobotSide side : RobotSide.values)
       {
-         ikFoundASolution.put(side, false);
-
-         desiredArmJacobians.put(side, new GeometricJacobian(desiredRobot.getChest(),
-                                                             desiredRobot.getHand(side),
-                                                             desiredRobot.getHand(side).getBodyFixedFrame()));
-         actualArmJacobians.put(side, new GeometricJacobian(syncedRobot.getFullRobotModel().getChest(),
-                                                            syncedRobot.getFullRobotModel().getHand(side),
-                                                            syncedRobot.getFullRobotModel().getHand(side).getBodyFixedFrame()));
-         workArmJacobians.put(side, new GeometricJacobian(workingRobot.getChest(),
-                                                          workingRobot.getHand(side),
-                                                          workingRobot.getHand(side).getBodyFixedFrame()));
-
-         double convergeTolerance = 4.0e-6; //1e-12;
-         double parameterChangePenalty = 0.1;
-         double positionCost = 1.0;
-         double orientationCost = 0.2;
-         boolean solveOrientation = true;
-         double toleranceForPositionError = 0.005;
-         double toleranceForOrientationError = 0.02;
-         inverseKinematicsCalculators.put(side, new DdoglegInverseKinematicsCalculator(workArmJacobians.get(side),
-                                                                                       positionCost,
-                                                                                       orientationCost,
-                                                                                       maxIterations,
-                                                                                       solveOrientation,
-                                                                                       convergeTolerance,
-                                                                                       toleranceForPositionError,
-                                                                                       toleranceForOrientationError,
-                                                                                       parameterChangePenalty));
-
-         RigidBodyTransform wristToControlTransform = new RigidBodyTransform(robotModel.getJointMap().getHandControlFrameToWristTransform(side));
-         wristToControlTransform.invert();
-         controlToWristTransforms.put(side, wristToControlTransform);
+         armManagers.get(side).create(robotModel, syncedRobot.getFullRobotModel(), desiredRobot, workingRobot);
       }
    }
 
@@ -126,29 +73,13 @@ public class GDXWholeBodyDesiredIKManager
       boolean desiredHandsChanged = false;
       for (RobotSide side : handInteractables.sides())
       {
-         computeArmJacobians(side);
-
-         FramePose3D correctedDesiredHandControlFramePose = correctedDesiredHandControlFramePoses.get(side);
-         correctedDesiredHandControlFramePose.setToZero(handInteractables.get(side).getControlReferenceFrame());
-         correctedDesiredHandControlFramePose.changeFrame(ReferenceFrame.getWorldFrame());
-         correctedDesiredHandControlFramePose.get(temporaryFrame.getTransformToParent());
-         temporaryFrame.getReferenceFrame().update();
-         correctedDesiredHandControlFramePose.setToZero(temporaryFrame.getReferenceFrame());
-         correctedDesiredHandControlFramePose.set(controlToWristTransforms.get(side));
-         // IDK where these come from; I tuned using JRebel
-         correctedDesiredHandControlFramePose.getTranslation().subZ(.045);
-         correctedDesiredHandControlFramePose.getTranslation().subX(.007);
-         correctedDesiredHandControlFramePose.getTranslation().subY(side.negateIfLeftSide(.0015));
-
-         // TODO: Frame??
-         correctedDesiredHandControlFramePose.changeFrame(desiredRobot.getChest().getBodyFixedFrame());
+         armManagers.get(side).update(handInteractables.get(side), desiredRobot);
 
          // We only want to evaluate this when we are going to take action on it
          // Otherwise, we will not notice the desired changed while the solver was still solving
          if (readyToSolve)
          {
-            desiredHandsChanged |= !lastCorrectedDesiredHandControlFramePoses.get(side).geometricallyEquals(correctedDesiredHandControlFramePose, 0.0001);
-            lastCorrectedDesiredHandControlFramePoses.get(side).setIncludingFrame(correctedDesiredHandControlFramePose);
+            desiredHandsChanged |= armManagers.get(side).getArmDesiredChanged();
          }
       }
 
@@ -158,26 +89,22 @@ public class GDXWholeBodyDesiredIKManager
          readyToSolve = false;
          for (RobotSide side : handInteractables.sides())
          {
-            copyOneDofJoints(actualArmJacobians.get(side).getJointsInOrder(), workArmJacobians.get(side).getJointsInOrder());
+            armManagers.get(side).copyActualToWork();
          }
 
          MissingThreadTools.startAThread("IKSolver", DefaultExceptionHandler.MESSAGE_AND_STACKTRACE, () ->
          {
-            for (RobotSide side : handInteractables.sides())
+            try
             {
-               FramePose3D lastCorrectedDesiredHandControlFramePose = lastCorrectedDesiredHandControlFramePoses.get(side);
-
-               workArmJacobians.get(side).compute();
-               ikFoundASolution.put(side, false);
-               for (int i = 0; i < INVERSE_KINEMATICS_CALCULATIONS_PER_UPDATE && !ikFoundASolution.get(side); i++)
+               for (RobotSide side : handInteractables.sides())
                {
-                  InverseKinematicsCalculator inverseKinematicsCalculator = inverseKinematicsCalculators.get(side);
-                  boolean foundASolution = inverseKinematicsCalculator.solve(lastCorrectedDesiredHandControlFramePose);
-                  ikFoundASolution.put(side, foundASolution);
+                  armManagers.get(side).solve();
                }
             }
-
-            readyToCopySolution = true;
+            finally
+            {
+               readyToCopySolution = true;
+            }
          });
       }
       if (readyToCopySolution)
@@ -185,7 +112,7 @@ public class GDXWholeBodyDesiredIKManager
          readyToCopySolution = false;
          for (RobotSide side : handInteractables.sides())
          {
-            copyOneDofJoints(workArmJacobians.get(side).getJointsInOrder(), desiredArmJacobians.get(side).getJointsInOrder());
+            armManagers.get(side).copyWorkToDesired();
          }
 
          readyToSolve = true;
@@ -238,7 +165,7 @@ public class GDXWholeBodyDesiredIKManager
          }
          else if (handPoseDataTypeToSend == HandDataType.POSE_WORLD || handPoseDataTypeToSend == HandDataType.POSE_CHEST)
          {
-            FramePose3D lastCorrectedDesiredHandControlFramePose = lastCorrectedDesiredHandControlFramePoses.get(robotSide);
+            FramePose3D desiredControlFramePose = armManagers.get(robotSide).getDesiredControlFramePose();
 
             ReferenceFrame frame;
             if (handPoseDataTypeToSend == HandDataType.POSE_WORLD)
@@ -250,18 +177,18 @@ public class GDXWholeBodyDesiredIKManager
                frame = syncedRobot.getReferenceFrames().getChestFrame();
             }
 
-            lastCorrectedDesiredHandControlFramePose.changeFrame(frame);
+            desiredControlFramePose.changeFrame(frame);
 
             LogTools.info("Sending HandTrajectoryMessage");
             HandTrajectoryMessage handTrajectoryMessage = HumanoidMessageTools.createHandTrajectoryMessage(robotSide,
                                                                                                            teleoperationParameters.getTrajectoryTime(),
-                                                                                                           lastCorrectedDesiredHandControlFramePose,
+                                                                                                           desiredControlFramePose,
                                                                                                            frame);
             long dataFrameId = MessageTools.toFrameId(frame);
             handTrajectoryMessage.getSe3Trajectory().getFrameInformation().setDataReferenceFrameId(dataFrameId);
             ros2Helper.publishToController(handTrajectoryMessage);
 
-            lastCorrectedDesiredHandControlFramePose.changeFrame(desiredRobot.getChest().getBodyFixedFrame());
+            desiredControlFramePose.changeFrame(desiredRobot.getChest().getBodyFixedFrame());
          }
       };
       return runnable;
@@ -269,25 +196,9 @@ public class GDXWholeBodyDesiredIKManager
 
    public void setDesiredToCurrent()
    {
-      lastCorrectedDesiredHandControlFramePoses.clear();
-      for (RobotSide robotSide : RobotSide.values)
-         copyOneDofJoints(actualArmJacobians.get(robotSide).getJointsInOrder(), desiredArmJacobians.get(robotSide).getJointsInOrder());
-   }
-
-   private void computeArmJacobians(RobotSide currentlyUpdatingSide)
-   {
-      desiredArmJacobians.get(currentlyUpdatingSide).compute();
-      actualArmJacobians.get(currentlyUpdatingSide).compute();
-   }
-
-   private static void copyOneDofJoints(JointBasics[] inverseDynamicsJoints1, JointBasics[] inverseDynamicsJoints2)
-   {
-      OneDoFJointBasics[] oneDofJoints1 = MultiBodySystemTools.filterJoints(inverseDynamicsJoints1, OneDoFJointBasics.class);
-      OneDoFJointBasics[] oneDofJoints2 = MultiBodySystemTools.filterJoints(inverseDynamicsJoints2, OneDoFJointBasics.class);
-
-      for (int i = 0; i < oneDofJoints1.length; i++)
+      for (RobotSide side : RobotSide.values)
       {
-         oneDofJoints2[i].setQ(oneDofJoints1[i].getQ());
+         armManagers.get(side).setDesiredToCurrent();
       }
    }
 }
