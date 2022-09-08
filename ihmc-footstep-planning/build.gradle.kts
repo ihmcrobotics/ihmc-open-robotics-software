c50dff34bb30dc28c9d73b3599d4ef22eea60ff6
Sylvain Bertrand <sbertrand@ihmc.org>
2022-09-08 17:01:57 -0500

    :arrow_up: ihmc-convex-optimization 0.17.14

--- ihmc-footstep-planning/build.gradle.kts
+++ ihmc-footstep-planning/build.gradle.kts
@@ -18,7 +18,7 @@ mainDependencies {
 
    api("us.ihmc:euclid:0.19.0")
    api("us.ihmc:ihmc-robotics-toolkit:source")
-   api("us.ihmc:ihmc-convex-optimization:0.17.13")
+   api("us.ihmc:ihmc-convex-optimization:0.17.14")
    api("us.ihmc:ihmc-path-planning:source")
    api("us.ihmc:ihmc-humanoid-robotics:source")
    api("us.ihmc:ihmc-common-walking-control-modules:source")
--- ihmc-quadruped/build.gradle.kts
+++ ihmc-quadruped/build.gradle.kts
@@ -80,7 +80,7 @@ communicationTestDependencies {
 roboticsDependencies {
    api(ihmc.sourceSetProject("communication"))
 
-   api("us.ihmc:ihmc-convex-optimization:0.17.13")
+   api("us.ihmc:ihmc-convex-optimization:0.17.14")
    api("us.ihmc:ihmc-state-estimation:source")
    api("us.ihmc:ihmc-simulation-toolkit:source")
    api("us.ihmc:ihmc-system-identification:source")
