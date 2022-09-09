ed007394e630f32a5a31c763f972503c2378ab01
Sylvain Bertrand <sbertrand@ihmc.org>
2022-09-08 19:13:42 -0500

    :arrow_up: scs2 17-0.9.6

--- ihmc-high-level-behaviors/build.gradle.kts
+++ ihmc-high-level-behaviors/build.gradle.kts
@@ -45,7 +45,7 @@ libgdxDependencies {
    api("us.ihmc:ihmc-graphics-libgdx:source")
    api("us.ihmc:ihmc-perception-javacv:source")
    api("us.ihmc:ihmc-graphics-libgdx:source")
-   api("us.ihmc:scs2-simulation:17-0.9.4")
+   api("us.ihmc:scs2-simulation:17-0.9.6")
    api("us.ihmc:mecano-graphviz:17-0.11.3")
    api("com.badlogicgames.gdx-controllers:gdx-controllers-core:2.2.1")
    api("com.badlogicgames.gdx-controllers:gdx-controllers-desktop:2.2.1")
