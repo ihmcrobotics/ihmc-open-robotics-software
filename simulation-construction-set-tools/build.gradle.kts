33b51507fb37a2d5ada5e9df636e0bd9d5f7889c
Sylvain Bertrand <sbertrand@ihmc.org>
2022-09-08 19:23:02 -0500

    :arrow_up: scs 0.22.9

--- simulation-construction-set-tools/build.gradle.kts
+++ simulation-construction-set-tools/build.gradle.kts
@@ -17,7 +17,7 @@ mainDependencies {
    api("us.ihmc:euclid-frame:0.19.0")
    api("us.ihmc:euclid-frame-shape:0.19.0")
    api("us.ihmc:euclid-shape:0.19.0")
-   api("us.ihmc:simulation-construction-set:0.22.8")
+   api("us.ihmc:simulation-construction-set:0.22.9")
    api("us.ihmc:scs2-definition:17-0.9.6")
    api("us.ihmc:scs2-simulation:17-0.9.6")
    api("us.ihmc:scs2-simulation-construction-set:17-0.9.6")
@@ -28,5 +28,5 @@ mainDependencies {
 testDependencies {
    api("us.ihmc:ihmc-robotics-toolkit-test:source")
 
-   api("us.ihmc:simulation-construction-set-test:0.22.8")
+   api("us.ihmc:simulation-construction-set-test:0.22.9")
 }
