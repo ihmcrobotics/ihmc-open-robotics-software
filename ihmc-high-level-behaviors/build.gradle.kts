4fb7a5def6c5720b273151a115bcd15f1aa19370
Sylvain Bertrand <sbertrand@ihmc.org>
2022-09-19 14:33:06 -0500

    :arrow_up: mecano 17-0.11.5

--- ihmc-high-level-behaviors/build.gradle.kts
+++ ihmc-high-level-behaviors/build.gradle.kts
@@ -46,7 +46,7 @@ libgdxDependencies {
    api("us.ihmc:ihmc-perception-javacv:source")
    api("us.ihmc:ihmc-graphics-libgdx:source")
    api("us.ihmc:scs2-simulation:17-0.10.1")
-   api("us.ihmc:mecano-graphviz:17-0.11.4")
+   api("us.ihmc:mecano-graphviz:17-0.11.5")
    api("com.badlogicgames.gdx-controllers:gdx-controllers-core:2.2.1")
    api("com.badlogicgames.gdx-controllers:gdx-controllers-desktop:2.2.1")
    api("com.badlogicgames.gdx:gdx-bullet:1.11.0")
