db6f6edd9a989eaf34635ad13d8657620d20e833
Dexton Anderson <danderson@ihmc.org>
2022-09-21 13:27:07 -0500

    Start javacpp build process

---
+++ ihmc-perception/src/native/cpp/ihmc-slam-wrapper/ihmc_slam_wrapper.java
@@ -0,0 +1,16 @@
+import org.bytedeco.javacpp.*;
+import org.bytedeco.javacpp.annotation.*;
+import org.bytedeco.javacpp.tools.*;
+
+@Properties(value =
+@Platform(
+      include = {"include/factor_graph_external.h"},
+      link = {"libihmc-slam-wrapper"}),
+      target = "FactorGraphExternal")
+public class ihmc_slam_wrapper implements InfoMapper
+{
+   public void map(InfoMap infoMap)
+   {
+
+   }
+}
