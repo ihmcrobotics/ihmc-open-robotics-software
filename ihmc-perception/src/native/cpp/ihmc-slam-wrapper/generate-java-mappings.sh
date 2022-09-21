f0621b46ee1bcdec3433e64038a64693b769f140
Dexton Anderson <danderson@ihmc.org>
2022-09-21 13:27:07 -0500

    Start javacpp build process

---
+++ ihmc-perception/src/native/cpp/ihmc-slam-wrapper/generate-java-mappings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=. ..
+make -j 4
+make install
+
+# Use the latest release on GitHub
+# https://github.com/bytedeco/javacpp/releases
+JAVACPP_VERSION=1.5.7
+
+curl -L https://github.com/bytedeco/javacpp/releases/download/$JAVACPP_VERSION/javacpp-platform-$JAVACPP_VERSION-bin.zip -o javacpp-platform-$JAVACPP_VERSION-bin.zip
+unzip -j javacpp-platform-$JAVACPP_VERSION-bin.zip
+
+# Copy the javacpp InfoMapper into build
+cp ../ihmc_slam_wrapper.java .
+
+java -jar javacpp.jar ihmc_slam_wrapper.java -d ../../../gen
