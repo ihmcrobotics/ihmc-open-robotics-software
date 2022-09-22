494b8367618be5549ac370b606420fe48462aade
Dexton Anderson <danderson@ihmc.org>
2022-09-22 14:44:49 -0500

    Typos, remove jni cpp from classpath resources

--- ihmc-perception/src/slam-wrapper/cpp/generate-java-mappings.sh
+++ ihmc-perception/src/slam-wrapper/cpp/generate-java-mappings.sh
@@ -15,7 +15,7 @@ JAVACPP_VERSION=1.5.7
 # Copy all Java code from the root of slam-wrapper into the build directory
 cp -r ../../java/ .
 
-# Move into the Java directory; javacpp.jar needs to reside here
+# Move into the java directory; javacpp.jar needs to reside here
 cd java
 
 # Download and unzip javacpp into the java source directory
@@ -23,7 +23,7 @@ curl -L https://github.com/bytedeco/javacpp/releases/download/$JAVACPP_VERSION/j
 unzip -j javacpp-platform-$JAVACPP_VERSION-bin.zip
 
 java -jar javacpp.jar us/ihmc/perception/slamWrapper/SlamWrapperInfoMapper.java
-# This will generated the jni shared library and place it into the classpath resources dir
+# This will generate the jni shared library and place it into the classpath resources dir
 java -jar javacpp.jar SlamWrapper.java -d ../../../resources
 
 # Clean old generated Java code
