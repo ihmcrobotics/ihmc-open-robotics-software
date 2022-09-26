fc4352f80b611fb4eee3a71838fbe9a76b736057
BhavyanshM <bhavyanshmishra@gmail.com>
2022-09-26 18:50:53 -0500

    Add an external SLAM test to verify JNI native methods.

--- ihmc-perception/src/slam-wrapper/cpp/external/factor_graph_external.cpp
+++ ihmc-perception/src/slam-wrapper/cpp/external/factor_graph_external.cpp
@@ -2,10 +2,16 @@
 
 void FactorGraphExternal::addPriorPoseFactor(int index, float *pose)
 {
+    using namespace gtsam;
+    Pose3 init_pose(Rot3::Ypr(pose[0], pose[1], pose[2]), Point3(pose[3], pose[4], pose[5]));
+    factorGraphHandler.addPriorPoseFactor(index, init_pose);
 }
 
 void FactorGraphExternal::addOdometryFactor(float *odometry, int poseId)
 {
+    using namespace gtsam;
+    Pose3 odometry_value(Rot3::Ypr(odometry[0], odometry[1], odometry[2]), Point3(odometry[3], odometry[4], odometry[5]));
+    factorGraphHandler.addOdometryFactor(odometry_value, poseId);
 }
 
 void FactorGraphExternal::addOrientedPlaneFactor(float *lmMean, int lmId, int poseIndex)
@@ -14,18 +20,24 @@ void FactorGraphExternal::addOrientedPlaneFactor(float *lmMean, int lmId, int po
 
 void FactorGraphExternal::optimize()
 {
+    factorGraphHandler.optimize();
 }
 
 void FactorGraphExternal::optimizeISAM2(uint8_t numberOfUpdates)
 {
+    factorGraphHandler.optimizeISAM2(numberOfUpdates);
 }
 
 void FactorGraphExternal::clearISAM2()
 {
+    factorGraphHandler.clearISAM2();
 }
 
-void FactorGraphExternal::setPoseInitialValue_Pose3(int index, float *value)
+void FactorGraphExternal::setPoseInitialValue(int index, float *value)
 {
+    using namespace gtsam;
+    Pose3 initial_value(Rot3::Ypr(value[0], value[1], value[2]), Point3(value[3], value[4], value[5]));
+    factorGraphHandler.setPoseInitialValue(index, initial_value);
 }
 
 void FactorGraphExternal::setOrientedPlaneInitialValue(int landmarkId, float *value)
@@ -38,6 +50,11 @@ void FactorGraphExternal::createOdometryNoiseModel(float *odomVariance)
 
 void FactorGraphExternal::createOrientedPlaneNoiseModel(float *lmVariances)
 {
+}
+
+void FactorGraphExternal::printResults()
+{
+    factorGraphHandler.getResults().print();
 }
 
 void FactorGraphExternal::helloWorldTest()
