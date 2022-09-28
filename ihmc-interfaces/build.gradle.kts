009d50a13ba425910671c1d92990241c782f7d74
Duncan Calvert <dcalvert@ihmc.us>
2022-09-28 16:22:32 -0500

    :arrow_up: ihmc-pub-sub 0.18.1, ros2 0.22.2

--- ihmc-interfaces/build.gradle.kts
+++ ihmc-interfaces/build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
       mavenLocal()
       jcenter()
    }
-//   dependencies {
-//      classpath("us.ihmc:ros2-msg-to-pubsub-generator:0.20.8")
-//      classpath("us.ihmc:log-tools:0.6.3") // removes vulnerable log4j versions from plugin classpath; can be removed later
-//   }
+   dependencies {
+      classpath("us.ihmc:ros2-msg-to-pubsub-generator:0.22.2")
+      classpath("us.ihmc:log-tools:0.6.3") // removes vulnerable log4j versions from plugin classpath; can be removed later
+   }
 }
 
 plugins {
@@ -32,8 +32,8 @@ ihmc {
 mainDependencies {
    api("us.ihmc:euclid:0.19.0")
    api("us.ihmc:euclid-geometry:0.19.0")
-   api("us.ihmc:ihmc-pub-sub:0.16.4")
-   api("us.ihmc:ros2-common-interfaces:0.20.8") {
+   api("us.ihmc:ihmc-pub-sub:0.18.1")
+   api("us.ihmc:ros2-common-interfaces:0.22.2") {
       exclude(group = "org.junit.jupiter", module = "junit-jupiter-api")
       exclude(group = "org.junit.jupiter", module = "junit-jupiter-engine")
       exclude(group = "org.junit.platform", module = "junit-platform-commons")
@@ -43,71 +43,71 @@ mainDependencies {
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-ros2-library:0.20.8")
+   api("us.ihmc:ihmc-ros2-library:0.22.2")
 }
 
 generatorDependencies {
    api("us.ihmc:euclid:0.19.0")
    api("us.ihmc:ihmc-commons:0.31.0")
-   api("us.ihmc:ros2-msg-to-pubsub-generator:0.20.8")
+   api("us.ihmc:ros2-msg-to-pubsub-generator:0.22.2")
 }
 
-//val generator = us.ihmc.ros2.rosidl.ROS2InterfaceGenerator()
-//
-//tasks.create("generateMessages") {
-//   doFirst {
-//      delete("src/main/generated-idl")
-//      delete("src/main/generated-java")
-//      delete("src/main/messages/ros1/controller_msgs/msg")
-//      delete("build/tmp/generateMessages")
-//
-//      var foundDependency = false
-//
-//      copy {
-//         for (file in configurations.default.get().files)
-//         {
-//            if (file.name.contains("ros2-common-interfaces"))
-//            {
-//               from(zipTree(file))
-//               foundDependency = true
-//            }
-//            into(file("build/tmp/generateMessages/ros2-common-interfaces"))
-//         }
-//      }
-//
-//      if (!foundDependency)
-//      {
-//         throw GradleException("Could not find ros2-common-interfaces in configurations.default!")
-//      }
-//
-//      generator.addPackageRootToIDLGenerator(file("build/tmp/generateMessages/ros2-common-interfaces/rcl_interfaces").toPath())
-//      generator.addPackageRootToIDLGenerator(file("build/tmp/generateMessages/ros2-common-interfaces/common_interfaces").toPath())
-//      generator.addPackageRootToIDLGenerator(file("src/main/messages/ihmc_interfaces").toPath())
-//      generator.addPackageRootToROS1Generator(file("src/main/messages/ihmc_interfaces").toPath())
-//
-//      generator.addCustomIDLFiles(file("build/tmp/generateMessages/ros2-common-interfaces/").toPath())
-//
-//      generator.generate(file("build/tmp/generateMessages/generated-idl").toPath(),
-//                         file("build/tmp/generateMessages/generated-ros1").toPath(),
-//                         file("build/tmp/generateMessages/generated-java").toPath())
-//
-//      copy {
-//         from("build/tmp/generateMessages/generated-idl/controller_msgs")
-//         into("src/main/generated-idl/controller_msgs")
-//      }
-//
-//      copy {
-//         from("build/tmp/generateMessages/generated-java/controller_msgs")
-//         into("src/main/generated-java/controller_msgs")
-//      }
-//
-//      copy {
-//         from("build/tmp/generateMessages/generated-ros1/controller_msgs")
-//         into("src/main/messages/ros1/controller_msgs")
-//      }
-//
-//      us.ihmc.ros2.rosidl.ROS2InterfaceGenerator.convertDirectoryToUnixEOL(file("src/main/generated-idl").toPath())
-//      us.ihmc.ros2.rosidl.ROS2InterfaceGenerator.convertDirectoryToUnixEOL(file("src/main/generated-java").toPath())
-//      us.ihmc.ros2.rosidl.ROS2InterfaceGenerator.convertDirectoryToUnixEOL(file("src/main/messages/ros1").toPath())
-//   }
-//}
+val generator = us.ihmc.ros2.rosidl.ROS2InterfaceGenerator()
+
+tasks.create("generateMessages") {
+   doFirst {
+      delete("src/main/generated-idl")
+      delete("src/main/generated-java")
+      delete("src/main/messages/ros1/controller_msgs/msg")
+      delete("build/tmp/generateMessages")
+
+      var foundDependency = false
+
+      copy {
+         for (file in configurations.default.get().files)
+         {
+            if (file.name.contains("ros2-common-interfaces"))
+            {
+               from(zipTree(file))
+               foundDependency = true
+            }
+            into(file("build/tmp/generateMessages/ros2-common-interfaces"))
+         }
+      }
+
+      if (!foundDependency)
+      {
+         throw GradleException("Could not find ros2-common-interfaces in configurations.default!")
+      }
+
+      generator.addPackageRootToIDLGenerator(file("build/tmp/generateMessages/ros2-common-interfaces/rcl_interfaces").toPath())
+      generator.addPackageRootToIDLGenerator(file("build/tmp/generateMessages/ros2-common-interfaces/common_interfaces").toPath())
+      generator.addPackageRootToIDLGenerator(file("src/main/messages/ihmc_interfaces").toPath())
+      generator.addPackageRootToROS1Generator(file("src/main/messages/ihmc_interfaces").toPath())
+
+      generator.addCustomIDLFiles(file("build/tmp/generateMessages/ros2-common-interfaces/").toPath())
+
+      generator.generate(file("build/tmp/generateMessages/generated-idl").toPath(),
+                         file("build/tmp/generateMessages/generated-ros1").toPath(),
+                         file("build/tmp/generateMessages/generated-java").toPath())
+
+      copy {
+         from("build/tmp/generateMessages/generated-idl/controller_msgs")
+         into("src/main/generated-idl/controller_msgs")
+      }
+
+      copy {
+         from("build/tmp/generateMessages/generated-java/controller_msgs")
+         into("src/main/generated-java/controller_msgs")
+      }
+
+      copy {
+         from("build/tmp/generateMessages/generated-ros1/controller_msgs")
+         into("src/main/messages/ros1/controller_msgs")
+      }
+
+      us.ihmc.ros2.rosidl.ROS2InterfaceGenerator.convertDirectoryToUnixEOL(file("src/main/generated-idl").toPath())
+      us.ihmc.ros2.rosidl.ROS2InterfaceGenerator.convertDirectoryToUnixEOL(file("src/main/generated-java").toPath())
+      us.ihmc.ros2.rosidl.ROS2InterfaceGenerator.convertDirectoryToUnixEOL(file("src/main/messages/ros1").toPath())
+   }
+}
