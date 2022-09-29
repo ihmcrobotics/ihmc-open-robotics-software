4fbcf7e7a77de857df03327a2b8d9bf480eba09c
lpenco <lpenco@ihmc.org>
2022-09-29 16:58:39 -0500

    editing example and added python plotting to visualize 1D Promps for easy analysis

---
+++ promp/examples/1Dplots_simple_promp.py
@@ -0,0 +1,47 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+
+#----------------SIMPLE PROMP------------------------------------------
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+n_demos=10
+demo = []
+for i in range(1,n_demos+1):
+    demos_pathname = '../build/demo' + str(i) + '.csv'
+    demo.append(np.genfromtxt(demos_pathname,delimiter=',', dtype = float))
+mean = np.genfromtxt('../build/mean.csv',delimiter=',', dtype = float)
+variance = np.genfromtxt('../build/variance.csv',delimiter=',', dtype = float)
+
+mean_handX = np.array([row[0] for row in mean])
+std_handX= np.array([row[0] for row in variance])
+mean_handY = np.array([row[1] for row in mean])
+std_handY = np.array([row[1] for row in variance])
+mean_handZ = np.array([row[2] for row in mean])
+std_handZ = np.array([row[2] for row in variance])
+
+fig = plt.figure()
+colorX ='red'
+colorY ='yellowgreen'
+colorZ ='cornflowerblue'
+democolorX='darkred'
+democolorY='darkgreen'
+democolorZ='midnightblue'
+
+plt.plot(mean_handX,colorX,linewidth=4.0)
+plt.fill_between(np.linspace(0, mean_handX.size, num=mean_handX.size), mean_handX - std_handX, mean_handX + std_handX, color=colorX, alpha=0.2)
+plt.plot(mean_handY,colorY,linewidth=4.0)
+plt.fill_between(np.linspace(0, mean_handY.size, num=mean_handY.size), mean_handY - std_handY, mean_handY + std_handY, color=colorY, alpha=0.2)
+plt.plot(mean_handZ,colorZ,linewidth=4.0)
+plt.fill_between(np.linspace(0, mean_handZ.size, num=mean_handZ.size), mean_handZ - std_handZ, mean_handZ + std_handZ, color=colorZ, alpha=0.2)
+for i in range(0,n_demos):
+    plt.plot(demo[i][:,0],democolorX)
+    plt.plot(demo[i][:,1],democolorY)
+    plt.plot(demo[i][:,2],democolorZ)
+plt.xlabel('#samples')  
+plt.ylabel('right hand [m]')
+plt.grid(True)
+
+
--- promp/src/test/java/us/ihmc/promp/test/SinglePrompExample.java
+++ promp/src/test/java/us/ihmc/promp/test/SinglePrompExample.java
@@ -1,18 +1,20 @@
 package us.ihmc.promp.test;
 
 import org.bytedeco.javacpp.DoublePointer;
-import us.ihmc.promp.ProMP;
-import us.ihmc.promp.SizeTVector;
-import us.ihmc.promp.StringVector;
-import us.ihmc.promp.TrajectoryGroup;
+import us.ihmc.promp.*;
 import us.ihmc.tools.io.WorkspaceDirectory;
 import us.ihmc.tools.io.WorkspaceFile;
 
 import static us.ihmc.promp.global.promp.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // This demo corresponds to promp/examples/single_promp.cpp
 public class SinglePrompExample
@@ -38,6 +40,31 @@ public class SinglePrompExample
       }
    }
 
+//   private static void saveAsCSV(DoublePointer dataMatrix, String fileName)
+//   {
+//      List<String[]> dataLines = new ArrayList<>();
+//      for (int i=0; i<dataMatrix.get(0).size(); i++)
+//      {
+//         String[] stringLine = new String[dataMatrix.get(0).size()];
+//         Arrays.setAll(stringLine, j -> "" + (dataMatrix.get(0)).get(i);
+//         dataLines.add(stringLine);
+//      }
+//      File csvFile = new File(fileName);
+//      try (PrintWriter writer = new PrintWriter(csvFile))
+//      {
+//         dataLines.stream().map(this::convertToCSV).forEach(writer::println);
+//      }
+//      catch (IOException e)
+//      {
+//         e.printStackTrace();
+//      }
+//   }
+//
+//   private String convertToCSV(String[] data)
+//   {
+//      return Stream.of(data).collect(Collectors.joining(","));
+//   }
+
    public static void main(String[] args)
    {
       try
@@ -49,23 +76,25 @@ public class SinglePrompExample
          throw new RuntimeException(e);
       }
 
-      WorkspaceDirectory putasideBoxDemoDir = new WorkspaceDirectory("ihmc-open-robotics-software", "promp/etc/demos/PutasideBox");
-      String putasideBoxDemoDirAbs = putasideBoxDemoDir.getDirectoryPath().toAbsolutePath().toString();
+      WorkspaceDirectory demoDir = new WorkspaceDirectory("ihmc-open-robotics-software", "promp/etc/demos/Reaching1");
+      String demoDirAbs = demoDir.getDirectoryPath().toAbsolutePath().toString();
 
       List<String> fileList = new ArrayList<>();
-
-      fileList.add(putasideBoxDemoDirAbs + "/p1.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p2.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p3.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p4.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p5.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p6.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p7.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p8.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p9.csv");
-      fileList.add(putasideBoxDemoDirAbs + "/p10.csv");
-
-      List<Long> dofs = List.of(0L, 1L, 2L);
+      // The trajectories contained in the Reaching1 folder represent different demonstration of a given task
+      // Several trajectories of different body parts have been recorded
+      // 0: waist Z; 1,2,3: right hand X,Y,Z; 5,6,7: left hand X,Y,Z
+      fileList.add(demoDirAbs + "/pr1.csv");
+      fileList.add(demoDirAbs + "/pr2.csv");
+      fileList.add(demoDirAbs + "/pr3.csv");
+      fileList.add(demoDirAbs + "/pr4.csv");
+      fileList.add(demoDirAbs + "/pr5.csv");
+      fileList.add(demoDirAbs + "/pr6.csv");
+      fileList.add(demoDirAbs + "/pr7.csv");
+      fileList.add(demoDirAbs + "/pr8.csv");
+      fileList.add(demoDirAbs + "/pr9.csv");
+      fileList.add(demoDirAbs + "/pr10.csv");
+      // consider only right hand trajectories
+      List<Long> dofs = List.of(1L, 2L, 3L);
 
       TrajectoryGroup trajectoryGroup = new TrajectoryGroup();
 
@@ -83,33 +112,54 @@ public class SinglePrompExample
       int n_rbf = 20;
 
       ProMP m_promp = new ProMP(trajectoryGroup, n_rbf);
-      EigenMatrixXd vect = m_promp.generate_trajectory();
-      EigenMatrixXd stdTraj = m_promp.gen_traj_std_dev();
-      EigenMatrixXd traj_covariance = m_promp.generate_trajectory_covariance();
+      EigenMatrixXd meanTrajectory = m_promp.generate_trajectory();
+      EigenMatrixXd stdTrajectory = m_promp.gen_traj_std_dev();
+      EigenMatrixXd covarianceTrajectory = m_promp.generate_trajectory_covariance();
 
-      DoublePointer vectData = vect.data();
-      DoublePointer stdTrajData = stdTraj.data();
-      DoublePointer traj_covarianceData = traj_covariance.data();
+      DoublePointer meanTrajectoryData = meanTrajectory.data();
+      DoublePointer stdTrajectoryData = stdTrajectory.data();
+      DoublePointer covarianceTrajectoryData = covarianceTrajectory.data();
 
-      System.out.println("vect");
-      for (int i = 0; i < t_len; i++)
+      TrajectoryVector demoTrajectories = trajectoryGroup.trajectories();
+      List<EigenMatrixXd> hand_demo_trajectory = new ArrayList<>();
+      for (int i=0; i<demoTrajectories.size(); i++)
       {
-         System.out.println(vectData.get(i));
+         hand_demo_trajectory.add((demoTrajectories.get(i)).matrix());
+      }
+
+      //TODO add .csv saving for debug and ProMP analysis
+
+//      saveAsCSV(meanTrajectoryData,"mean.csv");
+//      saveAsCSV(stdTrajectoryData,"variance.csv");
+//      saveAsCSV(covarianceTrajectoryData,"covariance.csv");
+
+      System.out.println("meanTrajectory");
+//      System.out.println(meanTrajectoryData.length());
+//      for (int i = 0; i < t_len; i++)
+      {
+         System.out.println((meanTrajectoryData.get(0)));
+          System.out.println(meanTrajectoryData.get(604));
       }
       System.out.println();
 
-      System.out.println("stdTraj");
-      for (int i = 0; i < t_len; i++)
-      {
-         System.out.println(stdTrajData.get(i));
-      }
-      System.out.println();
-
-      System.out.println("traj_covariance");
-      for (int i = 0; i < t_len; i++)
-      {
-         System.out.println(traj_covarianceData.get(i));
-      }
-      System.out.println();
+//      for (int i = 0; i < t_len; i++)
+//      {
+//         System.out.println(meanTrajectoryData.get(0));
+//      }
+//      System.out.println();
+//
+//      System.out.println("stdTrajectory");
+//      for (int i = 0; i < t_len; i++)
+//      {
+//         System.out.println(stdTrajectoryData.get(i));
+//      }
+//      System.out.println();
+//
+//      System.out.println("covarianceTrajectory");
+//      for (int i = 0; i < t_len; i++)
+//      {
+//         System.out.println(covarianceTrajectoryData.get(i));
+//      }
+//      System.out.println();
    }
 }
