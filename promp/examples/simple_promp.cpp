debd5cc72d95de75afbad62ac66523d81d3a9177
lpenco <lpenco@ihmc.org>
2022-09-29 16:58:39 -0500

    editing example and added python plotting to visualize 1D Promps for easy analysis

--- promp/examples/simple_promp.cpp
+++ promp/examples/simple_promp.cpp
@@ -14,20 +14,20 @@ int main()
 {
 	//vector of strings containing file names of all .csv files (each .csv file contains data from a single demonstration)
 	std::vector<std::string> file_list;
-	std::string file_path = "../etc/demos/PutasideBox/";
-	file_list.push_back(file_path + "p1.csv");
-	file_list.push_back(file_path + "p2.csv");
-	file_list.push_back(file_path + "p3.csv");
-	file_list.push_back(file_path + "p4.csv");
-	file_list.push_back(file_path + "p5.csv");
-	file_list.push_back(file_path + "p6.csv");
-	file_list.push_back(file_path + "p7.csv");
-	file_list.push_back(file_path + "p8.csv");
-	file_list.push_back(file_path + "p9.csv");
-	file_list.push_back(file_path + "p10.csv");
+	std::string file_path = "../etc/demos/Reaching1/";
+	file_list.push_back(file_path + "pr1.csv");
+	file_list.push_back(file_path + "pr2.csv");
+	file_list.push_back(file_path + "pr3.csv");
+	file_list.push_back(file_path + "pr4.csv");
+	file_list.push_back(file_path + "pr5.csv");
+	file_list.push_back(file_path + "pr6.csv");
+	file_list.push_back(file_path + "pr7.csv");
+	file_list.push_back(file_path + "pr8.csv");
+	file_list.push_back(file_path + "pr9.csv");
+	file_list.push_back(file_path + "pr10.csv");
 
 	// data handle object which takes indeces of trajectories in .csv file and also the file list
-	std::vector<size_t> dofs = {0, 1, 2};
+	std::vector<size_t> dofs = {1, 2, 3};
 	promp::TrajectoryGroup trajectory_group;
 	trajectory_group.load_csv_trajectories(file_list, dofs);
 	size_t t_len = trajectory_group.normalize_length();
@@ -35,13 +35,29 @@ int main()
 	/// initialize promp object with number of basis functions and std as arguments.
 	int n_rbf = 20;
 	promp::ProMP m_promp(trajectory_group, n_rbf);
-	Eigen::MatrixXd vect = m_promp.generate_trajectory();
+	Eigen::MatrixXd meanTraj = m_promp.generate_trajectory();
 	Eigen::MatrixXd stdTraj = m_promp.gen_traj_std_dev();
 	auto traj_covariance = m_promp.generate_trajectory_covariance();
-	
+
+	std::vector<promp::Trajectory> demo_trajectories = trajectory_group.trajectories();
+	std::vector<Eigen::MatrixXd> hand_demo_trajectory;
+	for (const auto& demo : demo_trajectories){
+		hand_demo_trajectory.push_back(demo.matrix());
+	}
+
+	for (int i=0; i<hand_demo_trajectory.size(); i++)
 	{
-		std::ofstream out("generated.csv");
-		out << vect.format(csv_format);
+		std::string filename = "demo";
+		filename += std::to_string(i+1);
+		filename += ".csv";
+		std::ofstream out(filename);
+		out << hand_demo_trajectory[i].format(csv_format);
+		out.close();
+	}
+
+	{
+		std::ofstream out("mean.csv");
+		out << meanTraj.format(csv_format);
 		out.close();
 	}
 
@@ -57,8 +73,6 @@ int main()
 			out << cov.format(csv_format) << std::endl;
 		out.close();
 	}
-	
 
-	
 	return 0;
 }
