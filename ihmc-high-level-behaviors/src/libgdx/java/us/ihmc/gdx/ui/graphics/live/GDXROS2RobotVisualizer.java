2e611e21636699c146ded316cdfef299fb2a13dc
Duncan Calvert <dcalvert@ihmc.us>
2022-09-30 11:10:29 -0500

    Revert "Merge pull request #2155 in LIBS/ihmc-open-robotics-software from feature/camera-focus to develop"
    This reverts commit 832dc62d10cb3b2ab5b3e0a3fc3e7328bbc625ef, reversing
    changes made to ae780c8b47ffbed7433f6ddc2052b95be31adaae.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS2RobotVisualizer.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/ui/graphics/live/GDXROS2RobotVisualizer.java
@@ -4,13 +4,9 @@ import imgui.ImGui;
 import imgui.type.ImBoolean;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
-import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.gdx.GDXFocusBasedCamera;
-import us.ihmc.gdx.imgui.ImGuiTools;
 import us.ihmc.gdx.imgui.ImGuiUniqueLabelMap;
-import us.ihmc.gdx.input.ImGui3DViewInput;
-import us.ihmc.gdx.ui.GDXImGuiBasedUI;
 import us.ihmc.gdx.ui.graphics.GDXMultiBodyGraphic;
 import us.ihmc.gdx.ui.visualizers.ImGuiFrequencyPlot;
 import us.ihmc.humanoidRobotics.frames.HumanoidReferenceFrames;
@@ -23,7 +19,6 @@ import java.util.function.Supplier;
 
 public class GDXROS2RobotVisualizer extends GDXMultiBodyGraphic
 {
-   private final GDXImGuiBasedUI baseUI;
    private final ImBoolean trackRobot = new ImBoolean(false);
    private final ImBoolean hideChest = new ImBoolean(false);
    private final Supplier<GDXFocusBasedCamera> cameraForTrackingSupplier;
@@ -37,18 +32,14 @@ public class GDXROS2RobotVisualizer extends GDXMultiBodyGraphic
    private final ImGuiFrequencyPlot frequencyPlot = new ImGuiFrequencyPlot();
    private String chestName;
 
-   public GDXROS2RobotVisualizer(GDXImGuiBasedUI baseUI, DRCRobotModel robotModel, ROS2SyncedRobotModel syncedRobot)
+   public GDXROS2RobotVisualizer(DRCRobotModel robotModel, ROS2SyncedRobotModel syncedRobot)
    {
-      this(baseUI, robotModel, syncedRobot, () -> null);
+      this(robotModel, syncedRobot, () -> null);
    }
 
-   public GDXROS2RobotVisualizer(GDXImGuiBasedUI baseUI,
-                                 DRCRobotModel robotModel,
-                                 ROS2SyncedRobotModel syncedRobot,
-                                 Supplier<GDXFocusBasedCamera> cameraForTrackingSupplier)
+   public GDXROS2RobotVisualizer(DRCRobotModel robotModel, ROS2SyncedRobotModel syncedRobot, Supplier<GDXFocusBasedCamera> cameraForTrackingSupplier)
    {
       super(robotModel.getSimpleRobotName() + " Robot Visualizer (ROS 2)");
-      this.baseUI = baseUI;
       this.robotModel = robotModel;
       this.syncedRobot = syncedRobot;
       this.cameraForTrackingSupplier = cameraForTrackingSupplier;
@@ -63,7 +54,6 @@ public class GDXROS2RobotVisualizer extends GDXMultiBodyGraphic
    public void create()
    {
       super.create();
-      baseUI.getPrimary3DPanel().addImGui3DViewInputProcessor(this::processImGuiInput);
       cameraForTracking = cameraForTrackingSupplier.get();
       RobotDefinition robotDefinition = new RobotDefinition(robotModel.getRobotDefinition());
       MaterialDefinition material = new MaterialDefinition(ColorDefinitions.Black());
@@ -105,29 +95,11 @@ public class GDXROS2RobotVisualizer extends GDXMultiBodyGraphic
       }
    }
 
-   public void processImGuiInput(ImGui3DViewInput input)
-   {
-      boolean ctrlHeld = imgui.internal.ImGui.getIO().getKeyCtrl();
-      boolean isPPressed = input.isWindowHovered() && ImGui.isKeyDown('P');
-      if (ctrlHeld && isPPressed)
-      {
-         teleportCameraToRobotPelvis();
-      }
-   }
-
    @Override
    public void renderImGuiWidgets()
    {
       super.renderImGuiWidgets();
       frequencyPlot.renderImGuiWidgets();
-
-      if (ImGui.button(labels.get("Snap to Robot")))
-      {
-         teleportCameraToRobotPelvis();
-      }
-      ImGuiTools.previousWidgetTooltip("Moves the camera focus point to the robot's current location.\nKeybind: Ctrl + P");
-      ImGui.sameLine();
-
       if (ImGui.checkbox(labels.get("Track robot"), trackRobot))
       {
          if (!trackRobot.get())
@@ -150,11 +122,5 @@ public class GDXROS2RobotVisualizer extends GDXMultiBodyGraphic
    public ImBoolean getHideChest()
    {
       return hideChest;
-   }
-
-   public void teleportCameraToRobotPelvis()
-   {
-      RigidBodyTransform robotTransform = syncedRobot.getReferenceFrames().getPelvisFrame().getTransformToWorldFrame();
-      cameraForTracking.setFocusPointPose(robotTransform);
    }
 }
