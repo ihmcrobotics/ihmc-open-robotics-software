45aefe5698c868343e3a609a5be9679962b5353a
lpenco <lpenco@ihmc.org>
2022-09-30 12:07:53 -0500

    camelCasing, formatting, verbose naming, path getter

--- promp/etc/1Dplots_simple_promp.py
+++ promp/etc/1Dplots_simple_promp.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plotter
 
 
 #----------------SIMPLE PROMP------------------------------------------
@@ -22,7 +22,7 @@ std_handY = np.array([row[1] for row in variance])
 mean_handZ = np.array([row[2] for row in mean])
 std_handZ = np.array([row[2] for row in variance])
 
-fig = plt.figure()
+fig = plotter.figure()
 colorX ='red'
 colorY ='yellowgreen'
 colorZ ='cornflowerblue'
@@ -30,18 +30,18 @@ democolorX='darkred'
 democolorY='darkgreen'
 democolorZ='midnightblue'
 
-plt.plot(mean_handX,colorX,linewidth=4.0)
-plt.fill_between(np.linspace(0, mean_handX.size, num=mean_handX.size), mean_handX - std_handX, mean_handX + std_handX, color=colorX, alpha=0.2)
-plt.plot(mean_handY,colorY,linewidth=4.0)
-plt.fill_between(np.linspace(0, mean_handY.size, num=mean_handY.size), mean_handY - std_handY, mean_handY + std_handY, color=colorY, alpha=0.2)
-plt.plot(mean_handZ,colorZ,linewidth=4.0)
-plt.fill_between(np.linspace(0, mean_handZ.size, num=mean_handZ.size), mean_handZ - std_handZ, mean_handZ + std_handZ, color=colorZ, alpha=0.2)
+plotter.plot(mean_handX,colorX,linewidth=4.0)
+plotter.fill_between(np.linspace(0, mean_handX.size, num=mean_handX.size), mean_handX - std_handX, mean_handX + std_handX, color=colorX, alpha=0.2)
+plotter.plot(mean_handY,colorY,linewidth=4.0)
+plotter.fill_between(np.linspace(0, mean_handY.size, num=mean_handY.size), mean_handY - std_handY, mean_handY + std_handY, color=colorY, alpha=0.2)
+plotter.plot(mean_handZ,colorZ,linewidth=4.0)
+plotter.fill_between(np.linspace(0, mean_handZ.size, num=mean_handZ.size), mean_handZ - std_handZ, mean_handZ + std_handZ, color=colorZ, alpha=0.2)
 for i in range(0,n_demos):
-    plt.plot(demo[i][:,0],democolorX)
-    plt.plot(demo[i][:,1],democolorY)
-    plt.plot(demo[i][:,2],democolorZ)
-plt.xlabel('#samples')  
-plt.ylabel('right hand [m]')
-plt.grid(True)
+    plotter.plot(demo[i][:,0],democolorX)
+    plotter.plot(demo[i][:,1],democolorY)
+    plotter.plot(demo[i][:,2],democolorZ)
+plotter.xlabel('#samples')
+plotter.ylabel('right hand [m]')
+plotter.grid(True)
 
 
