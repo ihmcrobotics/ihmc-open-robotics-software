fdb9c6397157558ac8014b10d9d060e4ba9da39a
BhavyanshM <bhavyanshmishra@gmail.com>
2022-10-03 01:48:23 -0500

    Add the ZED Stereo videocapture for opencv.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/perception/GDXStereoZED2UI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/perception/GDXStereoZED2UI.java
@@ -3,6 +3,7 @@ package us.ihmc.gdx.perception;
 import imgui.ImGui;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
 import us.ihmc.gdx.Lwjgl3ApplicationAdapter;
 import us.ihmc.gdx.imgui.ImGuiPanel;
 import us.ihmc.gdx.imgui.ImGuiUniqueLabelMap;
@@ -26,16 +27,16 @@ public class GDXStereoZED2UI
                                                               "ihmc-open-robotics-software",
                                                               "ihmc-high-level-behaviors/src/main/resources");
    private final Activator nativesLoadedActivator;
-   private GDXInteractableRealsenseD435 d435Interactable;
-   private YoRegistry yoRegistry = new YoRegistry(getClass().getSimpleName());
-   private YoGraphicsListRegistry yoGraphicsListRegistry = new YoGraphicsListRegistry();
-   private RealSenseHardwareManager realSenseHardwareManager;
-   private BytedecoRealsense d435;
    private GDXCVImagePanel colorImagePanelLeft;
    private GDXCVImagePanel colorImagePanelRight;
-   private Mat colorU16C3ImageLeft;
-   private Mat colorU16C3ImageRight;
-   private FrequencyCalculator frameReadFrequency = new FrequencyCalculator();
+   private Mat colorU8C3ImageLeft = new Mat(480, 640, opencv_core.CV_8UC3);
+   private Mat colorU8C3ImageRight = new Mat(480, 640, opencv_core.CV_8UC3);;
+
+   private VideoCapture cap;
+
+   private FrequencyCalculator leftFrameReadFrequency = new FrequencyCalculator();
+   private FrequencyCalculator rightFrameReadFrequency = new FrequencyCalculator();
+
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
 
    public GDXStereoZED2UI()
@@ -52,7 +53,6 @@ public class GDXStereoZED2UI
             ImGuiPanel panel = new ImGuiPanel("D435", this::renderImGuiWidgets);
             baseUI.getImGuiPanelManager().addPanel(panel);
 
-            d435Interactable = new GDXInteractableRealsenseD435(baseUI.getPrimary3DPanel());
          }
 
          @Override
@@ -62,16 +62,11 @@ public class GDXStereoZED2UI
             {
                if (nativesLoadedActivator.isNewlyActivated())
                {
-                  realSenseHardwareManager = new RealSenseHardwareManager(yoRegistry, yoGraphicsListRegistry);
-
-                  d435 = realSenseHardwareManager.createD435("049222073352", 1280, 720, 30);
-//                  d435.enableColor(1920, 1080, 30);
-                  d435.initialize();
+                  cap = new VideoCapture(0);
                }
 
-               if (d435.readFrameData())
+               if (cap.read(colorU16C3ImageLeft))
                {
-                  d435.updateDataBytePointers();
 
                   if (depthImagePanel == null)
                   {
@@ -97,24 +92,14 @@ public class GDXStereoZED2UI
          {
             ImGui.text("System native byte order: " + ByteOrder.nativeOrder().toString());
 
-            if (depthImagePanel != null)
+            if (colorImagePanelLeft != null)
             {
-               ImGui.text("Depth frame data size: " + d435.getDepthFrameDataSize());
-               ImGui.text("Frame read frequency: " + frameReadFrequency.getFrequency());
-               ImGui.text("Depth to meters conversion: " + d435.getDepthToMeterConversion());
+               ImGui.text("Left Frame read frequency: " + leftFrameReadFrequency.getFrequency());
+            }
 
-               ImGui.text("Unsigned 16 Depth:");
-
-               ImGui.text("R G B A:");
-
-               depthImagePanel.getBytedecoImage().rewind();
-               for (int i = 0; i < 5; i++)
-               {
-                  printBytes(depthImagePanel.getBytedecoImage().getBytedecoOpenCVMat().ptr(0, i).get(0),
-                             depthImagePanel.getBytedecoImage().getBytedecoOpenCVMat().ptr(0, i).get(1),
-                             depthImagePanel.getBytedecoImage().getBytedecoOpenCVMat().ptr(0, i).get(2),
-                             depthImagePanel.getBytedecoImage().getBytedecoOpenCVMat().ptr(0, i).get(3));
-               }
+            if (colorImagePanelRight != null)
+            {
+               ImGui.text("Right Frame read frequency: " + rightFrameReadFrequency.getFrequency());
             }
          }
 
@@ -135,8 +120,6 @@ public class GDXStereoZED2UI
          public void dispose()
          {
             baseUI.dispose();
-            d435.deleteDevice();
-            realSenseHardwareManager.deleteContext();
          }
       });
    }
