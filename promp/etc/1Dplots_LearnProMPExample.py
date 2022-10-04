2fc38002cffe669c4758e6f96d7255b553d5f54e
lpenco <lpenco@ihmc.org>
2022-10-04 17:39:45 -0500

    added example conditioning, refactoring py files for plotting. edit promp.cpp ridge factor bug. TODO regenarte promp library

--- promp/etc/1Dplots_LearnProMPExample.py
+++ promp/etc/1Dplots_LearnProMPExample.py
@@ -6,7 +6,7 @@ Created on Fri Sep 30 16:22:57 2022
 @author: luigi
 """
 
-import numpy as np
+import numpy
 import sys
 import matplotlib.pyplot as plotter
 
@@ -19,16 +19,16 @@ n_demos=10
 demo = []
 for i in range(1,n_demos+1):
     demos_pathname = 'demo' + str(i) + '.csv'
-    demo.append(np.genfromtxt(demos_pathname,delimiter=',', dtype = float))
-mean = np.genfromtxt('mean.csv',delimiter=',', dtype = float)
-variance = np.genfromtxt('stdDeviation.csv',delimiter=',', dtype = float)
+    demo.append(numpy.genfromtxt(demos_pathname,delimiter=',', dtype = float))
+mean = numpy.genfromtxt('mean.csv',delimiter=',', dtype = float)
+stdDeviation = numpy.genfromtxt('stdDeviation.csv',delimiter=',', dtype = float)
 
-mean_handX = np.array([row[0] for row in mean])
-std_handX= np.array([row[0] for row in variance])
-mean_handY = np.array([row[1] for row in mean])
-std_handY = np.array([row[1] for row in variance])
-mean_handZ = np.array([row[2] for row in mean])
-std_handZ = np.array([row[2] for row in variance])
+mean_handX = numpy.array([row[0] for row in mean])
+std_handX= numpy.array([row[0] for row in stdDeviation])
+mean_handY = numpy.array([row[1] for row in mean])
+std_handY = numpy.array([row[1] for row in stdDeviation])
+mean_handZ = numpy.array([row[2] for row in mean])
+std_handZ = numpy.array([row[2] for row in stdDeviation])
 
 fig = plotter.figure()
 colorX ='red'
@@ -39,11 +39,11 @@ democolorY='darkgreen'
 democolorZ='midnightblue'
 
 plotter.plot(mean_handX,colorX,linewidth=4.0)
-plotter.fill_between(np.linspace(0, mean_handX.size, num=mean_handX.size), mean_handX - std_handX, mean_handX + std_handX, color=colorX, alpha=0.2)
+plotter.fill_between(numpy.linspace(0, mean_handX.size, num=mean_handX.size), mean_handX - std_handX, mean_handX + std_handX, color=colorX, alpha=0.2)
 plotter.plot(mean_handY,colorY,linewidth=4.0)
-plotter.fill_between(np.linspace(0, mean_handY.size, num=mean_handY.size), mean_handY - std_handY, mean_handY + std_handY, color=colorY, alpha=0.2)
+plotter.fill_between(numpy.linspace(0, mean_handY.size, num=mean_handY.size), mean_handY - std_handY, mean_handY + std_handY, color=colorY, alpha=0.2)
 plotter.plot(mean_handZ,colorZ,linewidth=4.0)
-plotter.fill_between(np.linspace(0, mean_handZ.size, num=mean_handZ.size), mean_handZ - std_handZ, mean_handZ + std_handZ, color=colorZ, alpha=0.2)
+plotter.fill_between(numpy.linspace(0, mean_handZ.size, num=mean_handZ.size), mean_handZ - std_handZ, mean_handZ + std_handZ, color=colorZ, alpha=0.2)
 for i in range(0,n_demos):
     plotter.plot(demo[i][:,0],democolorX)
     plotter.plot(demo[i][:,1],democolorY)
