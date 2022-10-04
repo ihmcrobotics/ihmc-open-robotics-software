5fc7f38c522574b8513701f205f6675a5f2c5a59
Duncan Calvert <dcalvert@ihmc.us>
2022-10-04 16:22:11 -0500

    Progress on generator.

--- ihmc-java-toolkit/src/test/java/us/ihmc/tools/property/StoredPropertySetGeneratorTest.java
+++ ihmc-java-toolkit/src/test/java/us/ihmc/tools/property/StoredPropertySetGeneratorTest.java
@@ -1,24 +1,13 @@
 package us.ihmc.tools.property;
 
-import org.apache.commons.lang3.StringUtils;
-import us.ihmc.tools.io.WorkspaceDirectory;
-
-import java.nio.file.Paths;
-
 public class StoredPropertySetGeneratorTest
 {
    public static void main(String[] args)
    {
-      WorkspaceDirectory javaDirectory = new WorkspaceDirectory("ihmc-open-robotics-software", "ihmc-java-toolkit/src/test/java");
-      WorkspaceDirectory resourcesDirectory = new WorkspaceDirectory("ihmc-open-robotics-software", "ihmc-java-toolkit/src/test/resources");
-
-//      Resou
-      Class<StoredPropertySetGeneratorTest> clazz = StoredPropertySetGeneratorTest.class;
-
-      String iniFileName = StringUtils.uncapitalize(clazz.getSimpleName()) + ".ini";
-      clazz.getResourceAsStream(iniFileName);
-
-      StoredPropertySetJavaGenerator generator = new StoredPropertySetJavaGenerator(clazz, javaDirectory);
+      StoredPropertySetJavaGenerator generator = new StoredPropertySetJavaGenerator(StoredPropertySetGeneratorTest.class,
+                                                                                    "ihmc-open-robotics-software",
+                                                                                    "ihmc-java-toolkit/src/test/resources",
+                                                                                    "ihmc-java-toolkit/src/test/java");
       generator.generate();
    }
 }
