3f3480b2707a1f5909ae686d532762c2ad3b153a
BhavyanshM <bhavyanshmishra@gmail.com>
2022-10-05 16:00:59 -0500

    Add files for Docker environment.

---
+++ docker/nvidia-ros2-java/ros_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Immediately exit on any errors.
+set -e
+
+# Setup the ROS environment.
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+# Takes any command line arguments passed to ros_entrypoint.sh and execs them as a command.
+# The intention is basically "Do everything in this .sh script, then in the same shell
+# run the command the user passes in on the command line".
+# Taken from https://stackoverflow.com/a/39082923/1070333
+exec "$@"
