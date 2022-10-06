e5fb0480e595799abd9f1958c4d06f7dec29013c
Duncan Calvert <dcalvert@ihmc.us>
2022-10-06 13:13:19 -0500

    Start generating ConcaveHullFactoryParameters.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/geometry/ConcaveHullFactoryParameters.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/geometry/ConcaveHullFactoryParameters.java
@@ -1,14 +1,16 @@
 package us.ihmc.robotEnvironmentAwareness.geometry;
 
-import java.util.Scanner;
-
-import us.ihmc.jOctoMap.tools.ScannerTools;
 import us.ihmc.tools.property.*;
 
+/**
+ * This class was auto generated. Do not edit by hand. Edit the cooresponding JSON file
+ * and run the main to regenerate.
+ */
 public class ConcaveHullFactoryParameters extends StoredPropertySet implements ConcaveHullFactoryParametersBasics
 {
-   public static final String PROJECT_NAME = "ihmc-open-robotics-software";
-   public static final String TO_RESOURCE_FOLDER = "robot-environment-awareness/src/main/resources";
+   public static final String DIRECTORY_NAME_TO_ASSUME_PRESENT = "ihmc-open-robotics-software";
+   public static final String SUBSEQUENT_PATH_TO_RESOURCE_FOLDER = "robot-environment-awareness/src/main/resources";
+   public static final String SUBSEQUENT_PATH_TO_JAVA_FOLDER = "robot-environment-awareness/src/main/java";
 
    public static final StoredPropertyKeyList keys = new StoredPropertyKeyList();
 
@@ -23,54 +25,24 @@ public class ConcaveHullFactoryParameters extends StoredPropertySet implements C
       this("");
    }
 
-   public ConcaveHullFactoryParameters(String fileNameSuffix)
+   public ConcaveHullFactoryParameters(String versionSpecifier)
    {
-      super(keys, ConcaveHullFactoryParameters.class, PROJECT_NAME, TO_RESOURCE_FOLDER, fileNameSuffix);
+      super(keys, ConcaveHullFactoryParameters.class, DIRECTORY_NAME_TO_ASSUME_PRESENT, SUBSEQUENT_PATH_TO_RESOURCE_FOLDER, versionSpecifier);
       load();
    }
 
-   public ConcaveHullFactoryParameters(ConcaveHullFactoryParameters other)
+   public ConcaveHullFactoryParameters(StoredPropertySetReadOnly other)
    {
-      super(keys, ConcaveHullFactoryParameters.class, PROJECT_NAME, TO_RESOURCE_FOLDER);
+      super(keys, ConcaveHullFactoryParameters.class, DIRECTORY_NAME_TO_ASSUME_PRESENT, SUBSEQUENT_PATH_TO_RESOURCE_FOLDER, other.getCurrentVersionSuffix());
       set(other);
-   }
-
-   public void set(ConcaveHullFactoryParameters other)
-   {
-      setAll(other.getAll());
-   }
-
-   @Override
-   public String toString()
-   {
-      return "edge length threshold: " + edgeLengthThreshold + ", remove any triangle with two borderedges: " + removeAllTrianglesWithTwoBorderEdges
-            + ", allow splitting concave hull: " + allowSplittingConcaveHull + ", maximum number of iterations: " + maxNumberOfIterations
-            + ", triangulation tolerance: " + triangulationTolerance;
-   }
-
-   public void setFromString(String parametersAsString)
-   {
-      parametersAsString = parametersAsString.replace(",", "");
-      Scanner scanner = new Scanner(parametersAsString);
-      setEdgeLengthThreshold(ScannerTools.readNextDouble(scanner, getEdgeLengthThreshold()));
-      setRemoveAllTrianglesWithTwoBorderEdges(ScannerTools.readNextBoolean(scanner, doRemoveAllTrianglesWithTwoBorderEdges()));
-      setAllowSplittingConcaveHull(ScannerTools.readNextBoolean(scanner, isSplittingConcaveHullAllowed()));
-      setMaxNumberOfIterations(ScannerTools.readNextInt(scanner, getMaxNumberOfIterations()));
-      setTriangulationTolerance(ScannerTools.readNextDouble(scanner, getTriangulationTolerance()));
-      scanner.close();
-   }
-
-   public static ConcaveHullFactoryParameters parse(String parametersAsString)
-   {
-      ConcaveHullFactoryParameters parameters = new ConcaveHullFactoryParameters();
-      parameters.setFromString(parametersAsString);
-      return parameters;
    }
 
    public static void main(String[] args)
    {
-      StoredPropertySet parameters = new StoredPropertySet(keys, ConcaveHullFactoryParameters.class, PROJECT_NAME, TO_RESOURCE_FOLDER);
-      parameters.loadUnsafe();
-      parameters.save();
+      StoredPropertySet parameters = new StoredPropertySet(keys,
+                                                           ConcaveHullFactoryParameters.class,
+                                                           DIRECTORY_NAME_TO_ASSUME_PRESENT,
+                                                           SUBSEQUENT_PATH_TO_RESOURCE_FOLDER);
+      parameters.generateJavaFiles(SUBSEQUENT_PATH_TO_JAVA_FOLDER);
    }
 }
