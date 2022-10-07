4e2e87d3aeb82e12efc4b70183507fcdcc6175bd
Duncan Calvert <dcalvert@ihmc.us>
2022-10-06 23:17:01 -0500

    Add fun extras to StoredPropertySet keys.

--- ihmc-java-toolkit/src/test/java/us/ihmc/tools/property/StoredPropertySetTestParameters.java
+++ ihmc-java-toolkit/src/test/java/us/ihmc/tools/property/StoredPropertySetTestParameters.java
@@ -17,11 +17,46 @@ public class StoredPropertySetTestParameters extends StoredPropertySet implement
    public static final BooleanStoredPropertyKey theFirstBooleanProperty = keys.addBooleanKey("The first boolean property");
    public static final DoubleStoredPropertyKey theFirstDoubleProperty = keys.addDoubleKey("The first double property");
    public static final IntegerStoredPropertyKey theFirstIntegerProperty = keys.addIntegerKey("The first integer property");
+   /**
+    * The first boolean description.
+    */
+   public static final BooleanStoredPropertyKey booleanPropertyWithADescription = keys.addBooleanKey("Boolean property with a description");
+   /**
+    * The first integer description.
+    */
+   public static final DoubleStoredPropertyKey doublePropertyWithADescription = keys.addDoubleKey("Double property with a description");
+   /**
+    * The first integer description.
+    */
+   public static final IntegerStoredPropertyKey integerPropertyWithADescription = keys.addIntegerKey("Integer property with a description");
+   /**
+    * The double property with more stuff.
+    */
+   public static final DoubleStoredPropertyKey doublePropertyWithMoreStuff = keys.addDoubleKey("Double property with more stuff");
+   /**
+    * The integer property with more stuff.
+    */
+   public static final IntegerStoredPropertyKey integerPropertyWithMoreStuff = keys.addIntegerKey("Integer property with more stuff");
+   /**
+    * The integer property with discrete valid values.
+    */
+   public static final IntegerStoredPropertyKey integerPropertyWithDiscreteValidValues = keys.addIntegerKey("Integer property with discrete valid values");
 
    public StoredPropertySetTestParameters()
    {
-      super(keys, StoredPropertySetTestParameters.class, DIRECTORY_NAME_TO_ASSUME_PRESENT, SUBSEQUENT_PATH_TO_RESOURCE_FOLDER);
+      this("");
+   }
+
+   public StoredPropertySetTestParameters(String versionSpecifier)
+   {
+      super(keys, StoredPropertySetTestParameters.class, DIRECTORY_NAME_TO_ASSUME_PRESENT, SUBSEQUENT_PATH_TO_RESOURCE_FOLDER, versionSpecifier);
       load();
+   }
+
+   public StoredPropertySetTestParameters(StoredPropertySetReadOnly other)
+   {
+      super(keys, StoredPropertySetTestParameters.class, DIRECTORY_NAME_TO_ASSUME_PRESENT, SUBSEQUENT_PATH_TO_RESOURCE_FOLDER, other.getCurrentVersionSuffix());
+      set(other);
    }
 
    public static void main(String[] args)
--- ihmc-java-toolkit/src/test/java/us/ihmc/tools/property/StoredPropertySetTestParametersReadOnly.java
+++ ihmc-java-toolkit/src/test/java/us/ihmc/tools/property/StoredPropertySetTestParametersReadOnly.java
@@ -24,4 +24,52 @@ public interface StoredPropertySetTestParametersReadOnly extends StoredPropertyS
    {
       return get(theFirstIntegerProperty);
    }
+
+   /**
+    * The first boolean description.
+    */
+   default boolean getBooleanPropertyWithADescription()
+   {
+      return get(booleanPropertyWithADescription);
+   }
+
+   /**
+    * The first integer description.
+    */
+   default double getDoublePropertyWithADescription()
+   {
+      return get(doublePropertyWithADescription);
+   }
+
+   /**
+    * The first integer description.
+    */
+   default int getIntegerPropertyWithADescription()
+   {
+      return get(integerPropertyWithADescription);
+   }
+
+   /**
+    * The double property with more stuff.
+    */
+   default double getDoublePropertyWithMoreStuff()
+   {
+      return get(doublePropertyWithMoreStuff);
+   }
+
+   /**
+    * The integer property with more stuff.
+    */
+   default int getIntegerPropertyWithMoreStuff()
+   {
+      return get(integerPropertyWithMoreStuff);
+   }
+
+   /**
+    * The integer property with discrete valid values.
+    */
+   default int getIntegerPropertyWithDiscreteValidValues()
+   {
+      return get(integerPropertyWithDiscreteValidValues);
+   }
 }
