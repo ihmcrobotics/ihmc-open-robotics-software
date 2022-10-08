3c1cf9189edd945fe0603847508c34fee40f8b12
Duncan Calvert <dcalvert@ihmc.us>
2022-10-08 18:41:30 -0500

    Add ImIntegerWrapper.

---
+++ ihmc-graphics/src/libgdx/java/us/ihmc/gdx/imgui/ImIntegerWrapper.java
@@ -0,0 +1,32 @@
+package us.ihmc.gdx.imgui;
+
+import imgui.type.ImInt;
+
+import java.util.function.*;
+
+public class ImIntegerWrapper
+{
+   private final ImInt imInt = new ImInt();
+   private final IntSupplier wrappedValueGetter;
+   private final IntConsumer wrappedValueSetter;
+
+   public ImIntegerWrapper(IntSupplier wrappedValueGetter, IntConsumer wrappedValueSetter)
+   {
+      this.wrappedValueGetter = wrappedValueGetter;
+      this.wrappedValueSetter = wrappedValueSetter;
+   }
+
+   public void accessImInt(Consumer<ImInt> imIntConsumer)
+   {
+      // This basic set has no effects to just set it even if the values are the same
+      imInt.set(wrappedValueGetter.getAsInt());
+      imIntConsumer.accept(imInt);
+      // wrappedValueSetter might be hooked to a callback, so let's prevent
+      // that unless necessary
+      int imIntValue = imInt.get();
+      if (imIntValue != wrappedValueGetter.getAsInt())
+      {
+         wrappedValueSetter.accept(imIntValue);
+      }
+   }
+}
