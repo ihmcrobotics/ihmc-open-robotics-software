04cf75a27d0665526822aedc132eccbbd6fc968f
Duncan Calvert <dcalvert@ihmc.us>
2022-10-10 22:03:55 -0500

    Move concave hull and polygonizer bounds to JSON and remove manual code to manage it.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/perception/GDXRemoteGPUPlanarRegionExtractionUI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/perception/GDXRemoteGPUPlanarRegionExtractionUI.java
@@ -1,9 +1,6 @@
 package us.ihmc.gdx.perception;
 
 import imgui.ImGui;
-import imgui.type.ImBoolean;
-import imgui.type.ImFloat;
-import imgui.type.ImInt;
 import us.ihmc.avatar.gpuPlanarRegions.GPUPlanarRegionExtractionComms;
 import us.ihmc.avatar.gpuPlanarRegions.GPUPlanarRegionExtractionParameters;
 import us.ihmc.communication.property.StoredPropertySetMessageTools;
@@ -27,23 +24,15 @@ public class GDXRemoteGPUPlanarRegionExtractionUI
    private GDXROS1VideoVisualizer debugExtractionPanel;
    private final ImGuiPanel panel = new ImGuiPanel("GPU Planar Regions", this::renderImGuiWidgets);
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
-   private final ImFloat edgeLengthThresholdSlider = new ImFloat();
-   private final ImFloat triangulationToleranceSlider = new ImFloat();
-   private final ImInt maxNumberOfIterationsSlider = new ImInt();
-   private final ImBoolean allowSplittingConcaveHullChecked = new ImBoolean();
-   private final ImBoolean removeAllTrianglesWithTwoBorderEdgesChecked = new ImBoolean();
-   private final ImFloat concaveHullThresholdSlider = new ImFloat();
-   private final ImFloat shallowAngleThresholdSlider = new ImFloat();
-   private final ImFloat peakAngleThresholdSlider = new ImFloat();
-   private final ImFloat lengthThresholdSlider = new ImFloat();
-   private final ImFloat depthThresholdSlider = new ImFloat();
-   private final ImInt minNumberOfNodesSlider = new ImInt();
-   private final ImBoolean cutNarrowPassageChecked = new ImBoolean();
    private final StoredPropertySetROS2Input gpuRegionParametersROS2Input;
    private final StoredPropertySetROS2Input polygonizerParametersROS2Input;
    private final StoredPropertySetROS2Input concaveHullFactoryParametersROS2Input;
    private final ImGuiStoredPropertySetTuner gpuRegionParametersTuner;
+   private final ImGuiStoredPropertySetTuner concaveHullFactoryParametersTuner;
+   private final ImGuiStoredPropertySetTuner polygonizerParametersTuner;
    private boolean gpuRegionParametersChangedByImGuiUser = false;
+   private boolean concaveHullFactoryParametersChangedByImGuiUser = false;
+   private boolean polygonizerParametersChangedByImGuiUser = false;
 
    public GDXRemoteGPUPlanarRegionExtractionUI(ROS1Helper ros1Helper, ROS2Helper ros2Helper)
    {
@@ -66,8 +55,10 @@ public class GDXRemoteGPUPlanarRegionExtractionUI
                                                                              concaveHullFactoryParameters);
       gpuRegionParametersTuner = new ImGuiStoredPropertySetTuner(gpuRegionParameters.getTitle());
       gpuRegionParametersTuner.create(gpuRegionParameters, () -> gpuRegionParametersChangedByImGuiUser = true);
-      setConcaveHullFactoryImGuiWidgetsFromParameters();
-      setPolygonizerImGuiWidgetsFromParameters();
+      concaveHullFactoryParametersTuner = new ImGuiStoredPropertySetTuner(concaveHullFactoryParameters.getTitle());
+      concaveHullFactoryParametersTuner.create(concaveHullFactoryParameters, () -> concaveHullFactoryParametersChangedByImGuiUser = true);
+      polygonizerParametersTuner = new ImGuiStoredPropertySetTuner(polygonizerParameters.getTitle());
+      polygonizerParametersTuner.create(polygonizerParameters, () -> polygonizerParametersChangedByImGuiUser = true);
    }
 
    public void update()
@@ -112,10 +103,7 @@ public class GDXRemoteGPUPlanarRegionExtractionUI
 
       ImGui.separator();
 
-      if (polygonizerParametersROS2Input.update())
-      {
-         setPolygonizerImGuiWidgetsFromParameters();
-      }
+      polygonizerParametersROS2Input.update();
 
       ImGui.text("Polygonizer Parameters");
       if (polygonizerParametersROS2Input.getWaitingForUpdate())
@@ -129,10 +117,7 @@ public class GDXRemoteGPUPlanarRegionExtractionUI
 
       ImGui.separator();
 
-      if (concaveHullFactoryParametersROS2Input.update())
-      {
-         setConcaveHullFactoryImGuiWidgetsFromParameters();
-      }
+      concaveHullFactoryParametersROS2Input.update();
 
       ImGui.text("Concave Hull Factory Parameters");
       if (concaveHullFactoryParametersROS2Input.getWaitingForUpdate())
@@ -158,77 +143,25 @@ public class GDXRemoteGPUPlanarRegionExtractionUI
 
    private void renderPolygonizerParameterWidgets()
    {
-      boolean anyChanged = false;
-      anyChanged |= ImGui.sliderFloat("Concave Hull Threshold", concaveHullThresholdSlider.getData(), 0, 1);
-      anyChanged |= ImGui.sliderFloat("Shallow Angle Threshold", shallowAngleThresholdSlider.getData(), 0, 2.0f * (float) Math.PI);
-      anyChanged |= ImGui.sliderFloat("Peak Angle Threshold", peakAngleThresholdSlider.getData(), 0, 2.0f * (float) Math.PI);
-      anyChanged |= ImGui.sliderFloat("Length Threshold", lengthThresholdSlider.getData(), 0, 1);
-      anyChanged |= ImGui.sliderFloat("Depth Threshold", depthThresholdSlider.getData(), 0, 1);
-      anyChanged |= ImGui.sliderInt("Min Number of Nodes", minNumberOfNodesSlider.getData(), 0, 100);
-      anyChanged |= ImGui.checkbox("Cut Narrow Passage", cutNarrowPassageChecked);
+      polygonizerParametersTuner.renderImGuiWidgets();
 
-      if (anyChanged)
+      if (polygonizerParametersChangedByImGuiUser)
       {
-         setPolygonizerParametersFromImGuiWidgets();
+         polygonizerParametersChangedByImGuiUser = false;
          ros2Helper.publish(GPUPlanarRegionExtractionComms.POLYGONIZER_PARAMETERS_INPUT, StoredPropertySetMessageTools.newMessage(polygonizerParameters));
       }
    }
 
    private void renderConcaveHullFactoryParameterWidgets()
    {
-      boolean anyChanged = false;
-      ImGui.sliderFloat("Edge Length Threshold", edgeLengthThresholdSlider.getData(), 0, 0.5f);
-      ImGui.sliderFloat("Triangulation Tolerance", triangulationToleranceSlider.getData(), 0, 0.3f);
-      ImGui.sliderInt("Max Number of Iterations", maxNumberOfIterationsSlider.getData(), 2000, 6000);
-      ImGui.checkbox("Remove Degenerate Triangles", removeAllTrianglesWithTwoBorderEdgesChecked);
-      ImGui.checkbox("Split Concave Hull", allowSplittingConcaveHullChecked);
+      concaveHullFactoryParametersTuner.renderImGuiWidgets();
 
-      if (anyChanged)
+      if (concaveHullFactoryParametersChangedByImGuiUser)
       {
-         setConcaveHullParametersFromImGuiWidgets();
+         concaveHullFactoryParametersChangedByImGuiUser = false;
          ros2Helper.publish(GPUPlanarRegionExtractionComms.CONVEX_HULL_FACTORY_PARAMETERS_INPUT,
                             StoredPropertySetMessageTools.newMessage(concaveHullFactoryParameters));
       }
-   }
-
-   private void setPolygonizerParametersFromImGuiWidgets()
-   {
-      polygonizerParameters.setConcaveHullThreshold(concaveHullThresholdSlider.get());
-      polygonizerParameters.setShallowAngleThreshold(shallowAngleThresholdSlider.get());
-      polygonizerParameters.setPeakAngleThreshold(peakAngleThresholdSlider.get());
-      polygonizerParameters.setLengthThreshold(lengthThresholdSlider.get());
-      polygonizerParameters.setDepthThreshold(depthThresholdSlider.get());
-      polygonizerParameters.setMinNumberOfNodes(minNumberOfNodesSlider.get());
-      polygonizerParameters.setCutNarrowPassage(cutNarrowPassageChecked.get());
-   }
-
-   private void setConcaveHullParametersFromImGuiWidgets()
-   {
-      concaveHullFactoryParameters.setEdgeLengthThreshold(edgeLengthThresholdSlider.get());
-      concaveHullFactoryParameters.setTriangulationTolerance(triangulationToleranceSlider.get());
-      concaveHullFactoryParameters.setMaxNumberOfIterations(maxNumberOfIterationsSlider.get());
-      concaveHullFactoryParameters.setRemoveAllTrianglesWithTwoBorderEdges(removeAllTrianglesWithTwoBorderEdgesChecked.get());
-      concaveHullFactoryParameters.setAllowSplittingConcaveHull(allowSplittingConcaveHullChecked.get());
-   }
-
-   public void setPolygonizerImGuiWidgetsFromParameters()
-   {
-      concaveHullThresholdSlider.set((float) polygonizerParameters.getConcaveHullThreshold());
-      shallowAngleThresholdSlider.set((float) polygonizerParameters.getShallowAngleThreshold());
-      peakAngleThresholdSlider.set((float) polygonizerParameters.getPeakAngleThreshold());
-      lengthThresholdSlider.set((float) polygonizerParameters.getLengthThreshold());
-      depthThresholdSlider.set((float) polygonizerParameters.getDepthThreshold());
-      minNumberOfNodesSlider.set(polygonizerParameters.getMinNumberOfNodes());
-      cutNarrowPassageChecked.set(polygonizerParameters.getCutNarrowPassage());
-   }
-
-   public void setConcaveHullFactoryImGuiWidgetsFromParameters()
-   {
-      edgeLengthThresholdSlider.set((float) concaveHullFactoryParameters.getEdgeLengthThreshold());
-      triangulationToleranceSlider.set((float) concaveHullFactoryParameters.getTriangulationTolerance());
-      maxNumberOfIterationsSlider.set(concaveHullFactoryParameters.getMaxNumberOfIterations());
-      removeAllTrianglesWithTwoBorderEdgesChecked.set(concaveHullFactoryParameters.getRemoveAllTrianglesWithTwoBorderEdges());
-      allowSplittingConcaveHullChecked.set(concaveHullFactoryParameters.getAllowSplittingConcaveHull());
    }
 
    public void destroy()
