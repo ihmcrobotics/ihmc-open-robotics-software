d12564e825410493c72e6dd632703150cc81723c
lpenco <lpenco@ihmc.org>
2022-10-12 15:55:36 -0500

    working on integration promp in VR

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sharedControl/ProMPAssistance.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sharedControl/ProMPAssistance.java
@@ -4,7 +4,6 @@ import us.ihmc.euclid.geometry.interfaces.Pose3DBasics;
 import us.ihmc.euclid.geometry.interfaces.Pose3DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FixedFramePose3DBasics;
 import us.ihmc.promp.*;
-import us.ihmc.promp.presets.PrompInfoMapper;
 import us.ihmc.tools.io.WorkspaceDirectory;
 import us.ihmc.tools.io.WorkspaceFile;
 
@@ -18,21 +17,17 @@ import static us.ihmc.promp.global.promp.EigenMatrixXd;
 
 public class ProMPAssistance
 {
-   private List<ProMP> learnedProMPs = new ArrayList<>();
+   private HashMap<String, ProMP> learnedProMPs = new HashMap<>();
+   String taskName;
+   TrajectoryGroup trainingTrajectories;
 
-   public ProMPAssistance()
+   public ProMPAssistance(String taskName)
    {
-      try
-      {
-         loadLibraries(); //load promp libraries generated from cpp
-      }
-      catch (IOException e)
-      {
-         throw new RuntimeException(e);
-      }
+      this.taskName = taskName;
+      ProMPNativeLibrary.load();
    }
 
-   public void learnTaskFromDemos(String taskName, int numberDemos, HashMap<String, String> bodyPartsGeometry)
+   public void learnTaskFromDemos(int numberDemos, HashMap<String, String> bodyPartsGeometry)
    {
       WorkspaceDirectory demoDir = new WorkspaceDirectory("ihmc-open-robotics-software", "promp/etc/demos");
       String demoDirAbs = demoDir.getDirectoryPath().toAbsolutePath().toString();
@@ -45,7 +40,9 @@ public class ProMPAssistance
          fileListTraining.add(demoTrainingDirAbs + "/pr" + (i + 1) + ".csv");
 
       List<Long> dofs = new ArrayList<>();
-      // 0,1,2,3: left hand quaternion; 4,5,6: left hand X,Y,Z; 7-13: right hand;
+      // This is how the dofs are stored in the csv training files
+      // 0,1,2,3: left hand quaternion; 4,5,6: left hand X,Y,Z;
+      // 7,8,9,10: right hand quaternion; 11,12,13: left hand X,Y,Z;
       for (String bodyPart : bodyPartsGeometry.keySet()) {
          if (bodyPartsGeometry.get(bodyPart).equals("Orientation")){
             dofs.add(0L);
@@ -66,7 +63,7 @@ public class ProMPAssistance
             dofs.add(5L);
             dofs.add(6L);
          }
-         if(bodyPart.equals("RightHand")){ // TODO add other possible candidate body parts
+         if(bodyPart.equals("RightHand")){
             dofs.replaceAll(dof -> dof + 7L);
          }
       }
@@ -81,17 +78,18 @@ public class ProMPAssistance
       trainingTrajectories.load_csv_trajectories(fileListStringVectorTraining, doFsSizeTVector);
       // make all training trajectories have the same length
       int meanLengthTraining = (int) trainingTrajectories.normalize_length();
-      learnedProMPs.add(new ProMP(trainingTrajectories, 20)); // default 20 rbf functions seems to generalize well
+//      learnedProMPs.add(new ProMP(trainingTrajectories, 20)); // default 20 rbf functions seems to generalize well
    }
 
 //   public void updateTaskSpeed(String taskName, List<Pose3DReadOnly> observedFrameTrajectory){
 //      EigenMatrixXd observedTrajectory = toEigenMatrix(observedFrameTrajectory);
+//      TrajectoryVector demoTrajectories = trainingTrajectories.trajectories();
 //      // infer the new speed for the ProMP based on observed (portion of) trajectory
-//      int demo = infer_closest_trajectory(observedTrajectory, demoTestTrajectories);
+//      int demo = infer_closest_trajectory(observedTrajectory, demoTrajectories);
 //      System.out.println("Inferred closest demo to current observation: " + (demo + 1));
-
-//      double inferredSpeed = demoTestTrajectories.get(demo).infer_speed(observedTrajectory, 0.25, 4.0, 30);
-//      int inferredTimesteps = (int) (demoTestTrajectories.get(demo).timesteps() / inferredSpeed);
+//
+//      double inferredSpeed = demoTrajectories.get(demo).infer_speed(observedTrajectory, 0.25, 4.0, 30);
+//      int inferredTimesteps = (int) (demoTrajectories.get(demo).timesteps() / inferredSpeed);
 //      // generate ProMP mean trajectory with new time modulation
 //      PrompInfoMapper.EigenMatrixXd stdDeviationTrajectoryModulated = myProMP.gen_traj_std_dev(inferredTimesteps);
 //      PrompInfoMapper.EigenMatrixXd meanTrajectoryModulated = myProMP.generate_trajectory(inferredTimesteps);
@@ -105,8 +103,8 @@ public class ProMPAssistance
 //      myProMP.update_time_modulation((double) myProMP.get_traj_length() / inferredTimesteps);
 //      System.out.println("New ProMp timestep: " + myProMP.get_traj_length());
 //   }
-
-
+//
+//   /* transform trajectory from list of points to matrix */
 //   private EigenMatrixXd toEigenMatrix(List<Pose3DReadOnly> frameList)
 //   {
 //      EigenMatrixXd matrix = new EigenMatrixXd(frameList.size(),);
@@ -115,25 +113,4 @@ public class ProMPAssistance
 //            matrix.apply(i, j).put(row, col);
 //      return matrix;
 //   }
-
-   private void loadLibraries() throws IOException
-   {
-      // We need to disable javacpp from trying to automatically load libraries.
-      // Otherwise, it will try to load them by name when they aren't in the library path
-      // (LD_LIBRARY_PATH on Linux).
-      //
-      // The approach taken here is to use System.load to load each library by explicit
-      // absolute path on disk.
-      System.setProperty("org.bytedeco.javacpp.loadlibraries", "false");
-
-      List<String> libraryFiles = new ArrayList<>();
-      libraryFiles.add("libpromp.so");
-      libraryFiles.add("libjnipromp.so");
-
-      WorkspaceDirectory resourcesDirectory = new WorkspaceDirectory("ihmc-open-robotics-software", "promp/src/main/resources");
-      for (String libraryFile : libraryFiles)
-      {
-         System.load(new WorkspaceFile(resourcesDirectory, libraryFile).getFilePath().toAbsolutePath().normalize().toString());
-      }
-   }
 }
