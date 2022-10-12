ffec1810d26fa69b45271952b5c0a63c5cc10587
Duncan Calvert <dcalvert@ihmc.us>
2022-10-12 10:34:35 -0500

    Put comment inline with the docker run command.

--- docker/nvidia-ros2/runGUI.sh
+++ docker/nvidia-ros2/runGUI.sh
@@ -6,9 +6,6 @@ set -o xtrace
 
 xhost +local:docker
 
-# readlink -f provides an absolute path from a relative path, since docker
-# will not accept relative paths in a volume command
-# Setting TERM=xterm-256color enables color in the terminal
 if [ ! "$(docker ps -a | grep ' nvidia-ros2$')" ]; then
     echo "nvidia-ros2 not found. Running new container."
     docker run \
@@ -21,8 +18,10 @@ if [ ! "$(docker ps -a | grep ' nvidia-ros2$')" ]; then
         --gpus all \
         --device /dev/dri:/dev/dri \
         --env DISPLAY \
-        --env "TERM=xterm-256color" \
+        --env "TERM=xterm-256color" `# Enable color in the terminal` \
         --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        `# Provides an absolute path from a relative path, since docker` \
+        `# will not accept relative paths in a volume command` \
         --volume $(readlink -f ../../ihmc-interfaces/src/main/messages/ihmc_interfaces):/home/robotlab/colcon_ws/src \
         ihmcrobotics/nvidia-ros2:0.3
 else
