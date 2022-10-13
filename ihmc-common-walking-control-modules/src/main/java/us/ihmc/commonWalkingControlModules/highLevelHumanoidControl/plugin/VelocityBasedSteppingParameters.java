34df6329935f0bd5772b28f4d312d09e6f6817c6
rgriffin@ihmc.org <rgriffin@ihmc.us>
2022-10-13 17:01:23 -0500

    cleaned up the input parameters for the velocity based stepping generator

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/plugin/VelocityBasedSteppingParameters.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/plugin/VelocityBasedSteppingParameters.java
@@ -3,64 +3,13 @@ package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.plugin;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
 
-public class VelocityBasedSteppingParameters
+public interface VelocityBasedSteppingParameters
 {
-   private final YoDouble maxDesiredForwardVelocity;
-   private final YoDouble maxDesiredBackwardVelocity;
-   private final YoDouble maxDesiredLateralVelocity;
+   double getMaxDesiredForwardVelocity();
 
-   private final YoDouble maxDesiredTurningVelocity;
+   double getMaxDesiredBackwardVelocity();
 
-   public VelocityBasedSteppingParameters(String suffix, YoRegistry parentRegistry)
-   {
-      YoRegistry registry = new YoRegistry(getClass().getSimpleName());
+   double getMaxDesiredLateralVelocity();
 
-      maxDesiredForwardVelocity = new YoDouble("maxDesiredForwardVelocity" + suffix, registry);
-      maxDesiredBackwardVelocity = new YoDouble("maxDesiredBackwardVelocity" + suffix, registry);
-      maxDesiredLateralVelocity = new YoDouble("maxDesiredLateralVelocity" + suffix, registry);
-
-      maxDesiredTurningVelocity = new YoDouble("maxDesiredTurningVelocity" + suffix, registry);
-
-      parentRegistry.addChild(registry);
-   }
-
-   public double getMaxDesiredForwardVelocity()
-   {
-      return maxDesiredForwardVelocity.getDoubleValue();
-   }
-
-   public double getMaxDesiredBackwardVelocity()
-   {
-      return maxDesiredBackwardVelocity.getDoubleValue();
-   }
-
-   public double getMaxDesiredLateralVelocity()
-   {
-      return maxDesiredLateralVelocity.getDoubleValue();
-   }
-
-   public double getMaxDesiredTurningVelocity()
-   {
-      return maxDesiredTurningVelocity.getDoubleValue();
-   }
-
-   public void setMaxDesiredForwardVelocity(double maxVelocity)
-   {
-      maxDesiredForwardVelocity.set(maxVelocity);
-   }
-
-   public void setMaxDesiredBackwardVelocity(double maxVelocity)
-   {
-      maxDesiredBackwardVelocity.set(maxVelocity);
-   }
-
-   public void setMaxDesiredLateralVelocity(double maxVelocity)
-   {
-      maxDesiredLateralVelocity.set(maxVelocity);
-   }
-
-   public void setMaxDesiredTurningVelocity(double maxVelocity)
-   {
-      maxDesiredTurningVelocity.set(maxVelocity);
-   }
+   double getMaxDesiredTurningVelocity();
 }
