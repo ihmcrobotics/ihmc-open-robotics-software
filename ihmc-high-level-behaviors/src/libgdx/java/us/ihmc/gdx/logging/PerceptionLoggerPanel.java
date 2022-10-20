39e02ec3b8a1402daa10c762e36acc6443f4f9a5
BhavyanshM <bhavyanshmishra@gmail.com>
2022-10-20 18:04:30 -0500

    Add pointcloud loader.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/PerceptionLoggerPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/gdx/logging/PerceptionLoggerPanel.java
@@ -3,6 +3,8 @@ package us.ihmc.gdx.logging;
 import imgui.ImGui;
 import imgui.type.ImInt;
 import perception_msgs.msg.dds.FusedSensorHeadPointCloudMessage;
+import us.ihmc.commons.lists.RecyclingArrayList;
+import us.ihmc.euclid.tuple3D.Point3D32;
 import us.ihmc.gdx.GDXPointCloudRenderer;
 import us.ihmc.gdx.imgui.ImGuiPanel;
 import us.ihmc.gdx.sceneManager.GDXSceneLevel;
@@ -18,6 +20,7 @@ public class PerceptionLoggerPanel extends ImGuiPanel
    private PerceptionDataLoader loader;
    private GDXPointCloudRenderer pointCloudRenderer;
 
+   private final RecyclingArrayList<Point3D32> points = new RecyclingArrayList<>(200000, Point3D32::new);
    private ArrayList<String> topicNames = new ArrayList<>();
    private ArrayList<Integer> topicObjectCounts = new ArrayList<>();
 
@@ -37,8 +40,8 @@ public class PerceptionLoggerPanel extends ImGuiPanel
 
       pointCloudRenderer = new GDXPointCloudRenderer();
 
-      pointCloudRenderer.create(numberOfDepthPoints);
-      baseUI.getPrimaryScene().addRenderableProvider(pointCloudRenderer, GDXSceneLevel.MODEL);
+      pointCloudRenderer.create(400000);
+//      baseUI.getPrimaryScene().addRenderableProvider(pointCloudRenderer, GDXSceneLevel.MODEL);
 
       loader = new PerceptionDataLoader(PERCEPTION_LOG_FILE);
       topicNames = HDF5Tools.getTopicNames(loader.getHDF5Manager().getFile());
@@ -60,44 +63,44 @@ public class PerceptionLoggerPanel extends ImGuiPanel
       ImGui.sliderInt("Object Index", objectIndex.getData(), 0, 10);
       if(ImGui.button("Load Next"))
       {
-         loader.loadCompressedPointCloud("/os_cloud_node/points", objectIndex.get(), );
+         loader.loadCompressedPointCloud("/os_cloud_node/points", objectIndex.get(), points);
       }
    }
 
    public void loadPointCloud()
    {
-      FusedSensorHeadPointCloudMessage fusedMessage = latestFusedSensorHeadPointCloudMessageReference.getAndSet(null);
-      if (fusedMessage != null)
-      {
-         decompressionInputDirectBuffer.rewind();
-         int numberOfBytes = fusedMessage.getScan().size();
-         decompressionInputDirectBuffer.limit(numberOfBytes);
-         for (int i = 0; i < numberOfBytes; i++)
-         {
-            decompressionInputDirectBuffer.put(fusedMessage.getScan().get(i));
-         }
-         decompressionInputDirectBuffer.flip();
-         decompressedOpenCLIntBuffer.getBackingDirectByteBuffer().rewind();
-         // TODO: Look at using bytedeco LZ4 1.9.X, which is supposed to be 12% faster than 1.8.X
-         lz4Decompressor.decompress(decompressionInputDirectBuffer, decompressedOpenCLIntBuffer.getBackingDirectByteBuffer());
-         decompressedOpenCLIntBuffer.getBackingDirectByteBuffer().rewind();
-
-         latestSegmentIndex = (int) fusedMessage.getSegmentIndex();
-
-         parametersOpenCLFloatBuffer.getBytedecoFloatBufferPointer().put(0, latestSegmentIndex);
-         parametersOpenCLFloatBuffer.getBytedecoFloatBufferPointer().put(1, pointSize.get());
-         parametersOpenCLFloatBuffer.getBytedecoFloatBufferPointer().put(2, pointsPerSegment);
-
-         parametersOpenCLFloatBuffer.writeOpenCLBufferObject(openCLManager);
-         decompressedOpenCLIntBuffer.writeOpenCLBufferObject(openCLManager);
-
-         openCLManager.setKernelArgument(unpackPointCloudKernel, 0, parametersOpenCLFloatBuffer.getOpenCLBufferObject());
-         openCLManager.setKernelArgument(unpackPointCloudKernel, 1, decompressedOpenCLIntBuffer.getOpenCLBufferObject());
-         openCLManager.setKernelArgument(unpackPointCloudKernel, 2, pointCloudVertexBuffer.getOpenCLBufferObject());
-         openCLManager.execute1D(unpackPointCloudKernel, pointsPerSegment);
-         pointCloudVertexBuffer.readOpenCLBufferObject(openCLManager);
-
-         pointCloudRenderer.updateMeshFastest(totalNumberOfPoints);
-      }
+//      FusedSensorHeadPointCloudMessage fusedMessage = latestFusedSensorHeadPointCloudMessageReference.getAndSet(null);
+//      if (fusedMessage != null)
+//      {
+//         decompressionInputDirectBuffer.rewind();
+//         int numberOfBytes = fusedMessage.getScan().size();
+//         decompressionInputDirectBuffer.limit(numberOfBytes);
+//         for (int i = 0; i < numberOfBytes; i++)
+//         {
+//            decompressionInputDirectBuffer.put(fusedMessage.getScan().get(i));
+//         }
+//         decompressionInputDirectBuffer.flip();
+//         decompressedOpenCLIntBuffer.getBackingDirectByteBuffer().rewind();
+//         // TODO: Look at using bytedeco LZ4 1.9.X, which is supposed to be 12% faster than 1.8.X
+//         lz4Decompressor.decompress(decompressionInputDirectBuffer, decompressedOpenCLIntBuffer.getBackingDirectByteBuffer());
+//         decompressedOpenCLIntBuffer.getBackingDirectByteBuffer().rewind();
+//
+//         latestSegmentIndex = (int) fusedMessage.getSegmentIndex();
+//
+//         parametersOpenCLFloatBuffer.getBytedecoFloatBufferPointer().put(0, latestSegmentIndex);
+//         parametersOpenCLFloatBuffer.getBytedecoFloatBufferPointer().put(1, pointSize.get());
+//         parametersOpenCLFloatBuffer.getBytedecoFloatBufferPointer().put(2, pointsPerSegment);
+//
+//         parametersOpenCLFloatBuffer.writeOpenCLBufferObject(openCLManager);
+//         decompressedOpenCLIntBuffer.writeOpenCLBufferObject(openCLManager);
+//
+//         openCLManager.setKernelArgument(unpackPointCloudKernel, 0, parametersOpenCLFloatBuffer.getOpenCLBufferObject());
+//         openCLManager.setKernelArgument(unpackPointCloudKernel, 1, decompressedOpenCLIntBuffer.getOpenCLBufferObject());
+//         openCLManager.setKernelArgument(unpackPointCloudKernel, 2, pointCloudVertexBuffer.getOpenCLBufferObject());
+//         openCLManager.execute1D(unpackPointCloudKernel, pointsPerSegment);
+//         pointCloudVertexBuffer.readOpenCLBufferObject(openCLManager);
+//
+//         pointCloudRenderer.updateMeshFastest(totalNumberOfPoints);
+//      }
    }
 }
