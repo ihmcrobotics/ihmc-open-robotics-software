51c8a153e2d95657ec8a56db1087fe52289945af
Duncan Calvert <dcalvert@ihmc.us>
2022-11-02 16:13:57 -0500

    Allow to log loader panel to show up when there isn't a log loaded and add a button and input to load logs.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/logging/PerceptionLoggerPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/logging/PerceptionLoggerPanel.java
@@ -2,10 +2,13 @@ package us.ihmc.rdx.logging;
 
 import imgui.ImGui;
 import imgui.type.ImInt;
+import imgui.type.ImString;
 import us.ihmc.log.LogTools;
 import us.ihmc.perception.HDF5Tools;
 import us.ihmc.rdx.RDXPointCloudRenderer;
 import us.ihmc.rdx.imgui.ImGuiPanel;
+import us.ihmc.rdx.imgui.ImGuiTools;
+import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 
 import java.nio.FloatBuffer;
 import java.nio.file.Files;
@@ -14,7 +17,6 @@ import java.util.ArrayList;
 
 public class PerceptionLoggerPanel extends ImGuiPanel
 {
-
    private PerceptionDataLogger logger;
    private PerceptionDataLoader loader;
    private RDXPointCloudRenderer pointCloudRenderer;
@@ -22,8 +24,10 @@ public class PerceptionLoggerPanel extends ImGuiPanel
    private ArrayList<String> topicNames = new ArrayList<>();
    private ArrayList<Integer> topicObjectCounts = new ArrayList<>();
 
-   private ImInt topicIndex = new ImInt(0);
-   private ImInt objectIndex = new ImInt(0);
+   private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
+   private final ImString perceptionLogPath = new ImString();
+   private final ImInt topicIndex = new ImInt(0);
+   private final ImInt objectIndex = new ImInt(0);
 
    private String currentTopic;
 
@@ -33,10 +37,12 @@ public class PerceptionLoggerPanel extends ImGuiPanel
    public PerceptionLoggerPanel(String panelName)
    {
       super(panelName);
+      setRenderMethod(this::renderImguiWidgets);
    }
 
    public void loadLog(String perceptionLogFile)
    {
+      perceptionLogPath.set(perceptionLogFile);
       if (Files.exists(Paths.get(perceptionLogFile)))
       {
          loader = new PerceptionDataLoader(perceptionLogFile);
@@ -45,7 +51,6 @@ public class PerceptionLoggerPanel extends ImGuiPanel
          {
             topicObjectCounts.add((int) loader.getHDF5Manager().getCount(topic));
          }
-         setRenderMethod(this::renderImguiWidgets);
       }
       else
       {
@@ -60,21 +65,30 @@ public class PerceptionLoggerPanel extends ImGuiPanel
 
    public void renderImguiWidgets()
    {
-      ImGui.text("Perception Log: " + loader.getFilePath());
-      String[] names = topicNames.toArray(new String[0]);
-      ImGui.combo("Topic Names", topicIndex, names);
-      currentTopic = topicNames.get(topicIndex.get());
-      ImGui.text("Total Files: " + topicObjectCounts.get(topicIndex.get()));
-
-      ImGui.sliderInt("Object Index", objectIndex.getData(), 0, 10);
-      if (ImGui.button("Load Next"))
+      ImGuiTools.inputText(labels.get("Perception Log"), perceptionLogPath);
+      if (ImGui.button(labels.get("Load log")))
       {
-         FloatBuffer pointsBuffer = loader.loadCompressedPointCloud("/os_cloud_node/points", objectIndex.get());
+         loadLog(perceptionLogPath.get());
+      }
 
-         LogTools.info("Float Buffer: {} {} {}", pointsBuffer.get(0), pointsBuffer.get(1), pointsBuffer.get(2));
+      if (loader != null)
+      {
+         ImGui.text("Loaded Log: " + loader.getFilePath());
+         String[] names = topicNames.toArray(new String[0]);
+         ImGui.combo("Topic Names", topicIndex, names);
+         currentTopic = topicNames.get(topicIndex.get());
+         ImGui.text("Total Files: " + topicObjectCounts.get(topicIndex.get()));
 
-         pointCloudRenderer.getVertexBuffer().put(pointsBuffer);
-         pointCloudRenderer.updateMeshFastest();
+         ImGui.sliderInt("Object Index", objectIndex.getData(), 0, 10);
+         if (ImGui.button("Load Next"))
+         {
+            FloatBuffer pointsBuffer = loader.loadCompressedPointCloud("/os_cloud_node/points", objectIndex.get());
+
+            LogTools.info("Float Buffer: {} {} {}", pointsBuffer.get(0), pointsBuffer.get(1), pointsBuffer.get(2));
+
+            pointCloudRenderer.getVertexBuffer().put(pointsBuffer);
+            pointCloudRenderer.updateMeshFastest();
+         }
       }
    }
 }
