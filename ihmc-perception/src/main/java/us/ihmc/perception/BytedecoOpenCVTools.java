3fa5889fe43ec59cc80a7bd0f7714fae493efe2c
BhavyanshM <bhavyanshmishra@gmail.com>
2022-11-07 17:31:23 -0600

    Add changes to log and load jpeg compressed byte arrays.

--- ihmc-perception/src/main/java/us/ihmc/perception/BytedecoOpenCVTools.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/BytedecoOpenCVTools.java
@@ -1,11 +1,15 @@
 package us.ihmc.perception;
 
+import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.IntPointer;
 import org.bytedeco.javacv.Java2DFrameUtils;
 import org.bytedeco.opencv.global.opencv_core;
+import org.bytedeco.opencv.global.opencv_imgcodecs;
 import org.bytedeco.opencv.global.opencv_imgproc;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Size;
+import perception_msgs.msg.dds.VideoPacket;
+import us.ihmc.idl.IDLSequence;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
@@ -92,37 +96,89 @@ public class BytedecoOpenCVTools
       double sigmaX = 4.74;
       double sigmaY = sigmaX;
       int borderType = opencv_core.BORDER_DEFAULT;
-      opencv_imgproc.GaussianBlur(sourceImage,
-                                  destinationImage,
-                                  gaussianKernelSize,
-                                  sigmaX,
-                                  sigmaY,
-                                  borderType);
+      opencv_imgproc.GaussianBlur(sourceImage, destinationImage, gaussianKernelSize, sigmaX, sigmaY, borderType);
    }
 
    public static Mat convertBufferedImageToMat(BufferedImage image)
    {
       return Java2DFrameUtils.toMat(image);
    }
+
    public static String getTypeString(int type)
    {
       int numImgTypes = 35; // 7 base types, with five channel options each (none or C1, ..., C4)
 
-      int enum_ints[] =       {opencv_core.CV_8U,  opencv_core.CV_8UC1,  opencv_core.CV_8UC2,  opencv_core.CV_8UC3,  opencv_core.CV_8UC4,
-                               opencv_core.CV_8S,  opencv_core.CV_8SC1,  opencv_core.CV_8SC2,  opencv_core.CV_8SC3,  opencv_core.CV_8SC4,
-                               opencv_core.CV_16U, opencv_core.CV_16UC1, opencv_core.CV_16UC2, opencv_core.CV_16UC3, opencv_core.CV_16UC4,
-                               opencv_core.CV_16S, opencv_core.CV_16SC1, opencv_core.CV_16SC2, opencv_core.CV_16SC3, opencv_core.CV_16SC4,
-                               opencv_core.CV_32S, opencv_core.CV_32SC1, opencv_core.CV_32SC2, opencv_core.CV_32SC3, opencv_core.CV_32SC4,
-                               opencv_core.CV_32F, opencv_core.CV_32FC1, opencv_core.CV_32FC2, opencv_core.CV_32FC3, opencv_core.CV_32FC4,
-                               opencv_core.CV_64F, opencv_core.CV_64FC1, opencv_core.CV_64FC2, opencv_core.CV_64FC3, opencv_core.CV_64FC4};
+      int enum_ints[] = {opencv_core.CV_8U,
+                         opencv_core.CV_8UC1,
+                         opencv_core.CV_8UC2,
+                         opencv_core.CV_8UC3,
+                         opencv_core.CV_8UC4,
+                         opencv_core.CV_8S,
+                         opencv_core.CV_8SC1,
+                         opencv_core.CV_8SC2,
+                         opencv_core.CV_8SC3,
+                         opencv_core.CV_8SC4,
+                         opencv_core.CV_16U,
+                         opencv_core.CV_16UC1,
+                         opencv_core.CV_16UC2,
+                         opencv_core.CV_16UC3,
+                         opencv_core.CV_16UC4,
+                         opencv_core.CV_16S,
+                         opencv_core.CV_16SC1,
+                         opencv_core.CV_16SC2,
+                         opencv_core.CV_16SC3,
+                         opencv_core.CV_16SC4,
+                         opencv_core.CV_32S,
+                         opencv_core.CV_32SC1,
+                         opencv_core.CV_32SC2,
+                         opencv_core.CV_32SC3,
+                         opencv_core.CV_32SC4,
+                         opencv_core.CV_32F,
+                         opencv_core.CV_32FC1,
+                         opencv_core.CV_32FC2,
+                         opencv_core.CV_32FC3,
+                         opencv_core.CV_32FC4,
+                         opencv_core.CV_64F,
+                         opencv_core.CV_64FC1,
+                         opencv_core.CV_64FC2,
+                         opencv_core.CV_64FC3,
+                         opencv_core.CV_64FC4};
 
-      String enum_strings[] = {"CV_8U",  "CV_8UC1",  "CV_8UC2",  "CV_8UC3",  "CV_8UC4",
-                               "CV_8S",  "CV_8SC1",  "CV_8SC2",  "CV_8SC3",  "CV_8SC4",
-                               "CV_16U", "CV_16UC1", "CV_16UC2", "CV_16UC3", "CV_16UC4",
-                               "CV_16S", "CV_16SC1", "CV_16SC2", "CV_16SC3", "CV_16SC4",
-                               "CV_32S", "CV_32SC1", "CV_32SC2", "CV_32SC3", "CV_32SC4",
-                               "CV_32F", "CV_32FC1", "CV_32FC2", "CV_32FC3", "CV_32FC4",
-                               "CV_64F", "CV_64FC1", "CV_64FC2", "CV_64FC3", "CV_64FC4"};
+      String enum_strings[] = {"CV_8U",
+                               "CV_8UC1",
+                               "CV_8UC2",
+                               "CV_8UC3",
+                               "CV_8UC4",
+                               "CV_8S",
+                               "CV_8SC1",
+                               "CV_8SC2",
+                               "CV_8SC3",
+                               "CV_8SC4",
+                               "CV_16U",
+                               "CV_16UC1",
+                               "CV_16UC2",
+                               "CV_16UC3",
+                               "CV_16UC4",
+                               "CV_16S",
+                               "CV_16SC1",
+                               "CV_16SC2",
+                               "CV_16SC3",
+                               "CV_16SC4",
+                               "CV_32S",
+                               "CV_32SC1",
+                               "CV_32SC2",
+                               "CV_32SC3",
+                               "CV_32SC4",
+                               "CV_32F",
+                               "CV_32FC1",
+                               "CV_32FC2",
+                               "CV_32FC3",
+                               "CV_32FC4",
+                               "CV_64F",
+                               "CV_64FC1",
+                               "CV_64FC2",
+                               "CV_64FC3",
+                               "CV_64FC4"};
 
       for (int i = 0; i < numImgTypes; i++)
       {
@@ -135,13 +191,34 @@ public class BytedecoOpenCVTools
    public static void printMat(Mat image, String prefix)
    {
       System.out.println("Mat: [" + prefix + "]");
-      for(int i = 0; i<image.rows(); i++)
+      for (int i = 0; i < image.rows(); i++)
       {
-         for(int j = 0; j<image.cols(); j++)
+         for (int j = 0; j < image.cols(); j++)
          {
-            System.out.print(image.ptr(i,j).getShort() + "\t");
+            System.out.print(image.ptr(i, j).getShort() + "\t");
          }
          System.out.println();
       }
+   }
+
+   public static Mat decompressVideoPacketJPEG(BytePointer dataPointer, int size)
+   {
+      Mat inputJPEGMat = new Mat();
+      Mat inputYUVI420Mat = new Mat();
+
+      inputJPEGMat.cols(size);
+      inputJPEGMat.data(dataPointer);
+
+      // imdecode takes the longest by far out of all this stuff
+      opencv_imgcodecs.imdecode(inputJPEGMat, opencv_imgcodecs.IMREAD_UNCHANGED, inputYUVI420Mat);
+
+      // YUV I420 has 1.5 times the height of the image
+      int imageWidth = inputYUVI420Mat.cols();
+      int imageHeight = (int) (inputYUVI420Mat.rows() / 1.5f);
+      Mat outputImage = new Mat(imageHeight, imageWidth, CV_8UC3);
+
+      opencv_imgproc.cvtColor(inputYUVI420Mat, outputImage, opencv_imgproc.COLOR_YUV2BGR_I420);
+
+      return outputImage;
    }
 }
