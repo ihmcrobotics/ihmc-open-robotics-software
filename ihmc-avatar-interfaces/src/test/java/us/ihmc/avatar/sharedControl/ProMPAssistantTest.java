cf31cf8664c05e37c8adcea450e6007ab59d2e91
lpenco <lpenco@ihmc.org>
2022-11-09 17:37:48 -0600

    fixed bugs in promps updates, Test promp Assistant gives no errors, added doc in test, todo analyze generated trajectories

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/sharedControl/ProMPAssistantTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/sharedControl/ProMPAssistantTest.java
@@ -9,13 +9,15 @@ public class ProMPAssistantTest
 {
    public static void main(String[] args)
    {
+      // learn ProMPs
+      // Check ProMPAssistant.json if you want to change parameters (e.g, task to learn, body parts to consider in the motion)
       ProMPAssistant proMPAssistant = new ProMPAssistant();
-
+      // use a csv file with the trajectories of the hands of the robot for testing
       WorkspaceDirectory directory = new WorkspaceDirectory("ihmc-open-robotics-software", "promp/etc");
       String directoryAbsolutePath = directory.getDirectoryPath().toAbsolutePath().toString();
       String demoDirectory = directoryAbsolutePath + "/test/PushDoorTest";
       String testFilePath = demoDirectory + "/1.csv";
-
+      // replay that file
       TrajectoryRecordReplay<Double> trajectoryPlayer = new TrajectoryRecordReplay<>(Double.class, testFilePath, 2);
       FramePose3D framePose = new FramePose3D();
       framePose.setFromReferenceFrame(ReferenceFrame.getWorldFrame());
@@ -26,13 +28,19 @@ public class ProMPAssistantTest
          Double[] dataPoint = trajectoryPlayer.play();
 
          // [0,1,2,3] quaternion of body segment; [4,5,6] position of body segment
+         // right hand +7. This is the way files are generated from recordings in VR. Check KinematicsRecordReplay
          framePose.getOrientation().set(dataPoint[7], dataPoint[8], dataPoint[9], dataPoint[10]);
          framePose.getPosition().set(dataPoint[11], dataPoint[12], dataPoint[13]);
 
-         if (proMPAssistant.readyToPack())
+         if (proMPAssistant.readyToPack()){
+            //change frame according to generated ProMP
             proMPAssistant.framePoseToPack(framePose, "rightHand");
+         }
          else
+         {
+            //do not change the frame, just observe it in order to later generate a prediction
             proMPAssistant.processFrameInformation(framePose, "rightHand");
+         }
       }
    }
 }
