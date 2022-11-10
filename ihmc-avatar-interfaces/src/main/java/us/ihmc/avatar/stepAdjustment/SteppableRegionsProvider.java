bb31103124c3a927de7265851dec77563cb04c29
Robert Griffin <rgriffin@ihmc.us>
2022-11-09 22:58:50 -0600

    added more java doc and some class restructuring

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/stepAdjustment/SteppableRegionsProvider.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/stepAdjustment/SteppableRegionsProvider.java
@@ -1,10 +1,13 @@
-package us.ihmc.commonWalkingControlModules.desiredFootStep.footstepGenerator;
+package us.ihmc.avatar.stepAdjustment;
 
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.PlanarRegionsListCommand;
 import us.ihmc.robotics.geometry.PlanarRegion;
 
 import java.util.List;
 
+/**
+ * Class that provides the regions that the robot can step on, if a planar reigons list is provided.
+ */
 public interface SteppableRegionsProvider
 {
    void consume(PlanarRegionsListCommand command);
