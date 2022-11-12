b676505a24a485aca74ee6c9e0c4fee32a57595f
Robert Griffin <rgriffin@ihmc.us>
2022-11-11 21:57:31 -0600

    did some more parameter cleanup

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/YoSteppingEnvironmentalConstraintParameters.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/YoSteppingEnvironmentalConstraintParameters.java
@@ -4,6 +4,7 @@ import us.ihmc.commonWalkingControlModules.capturePoint.stepAdjustment.Constrain
 import us.ihmc.commonWalkingControlModules.capturePoint.stepAdjustment.ConstraintOptimizerParametersReadOnly;
 import us.ihmc.commonWalkingControlModules.capturePoint.stepAdjustment.YoConstraintOptimizerParameters;
 import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoBoolean;
 import us.ihmc.yoVariables.variable.YoDouble;
 
 public class YoSteppingEnvironmentalConstraintParameters extends SteppingEnvironmentalConstraintParameters
@@ -14,6 +15,7 @@ public class YoSteppingEnvironmentalConstraintParameters extends SteppingEnviron
    private final YoDouble maxPlanarRegionNormalAngleForStepping;
    private final YoDouble distanceFromStanceToTrustEnvironment;
    private final YoDouble smallIntersectionAreaToFilter;
+   private final YoBoolean useSimpleSnapping;
 
    public YoSteppingEnvironmentalConstraintParameters(SteppingEnvironmentalConstraintParameters parameters, YoRegistry parentRegistry)
    {
@@ -23,11 +25,13 @@ public class YoSteppingEnvironmentalConstraintParameters extends SteppingEnviron
       maxPlanarRegionNormalAngleForStepping = new YoDouble("maxPlanarRegionNormalAngleForStepping", registry);
       distanceFromStanceToTrustEnvironment = new YoDouble("distanceFromStanceToTrustEnvironment", registry);
       smallIntersectionAreaToFilter = new YoDouble("smallIntersectionAreaToFilter", registry);
+      useSimpleSnapping = new YoBoolean("useSimpleSnapping", registry);
 
       minPlanarRegionAreaForStepping.set(parameters.getMinPlanarRegionAreaForStepping());
       maxPlanarRegionNormalAngleForStepping.set(parameters.getMaxPlanarRegionNormalAngleForStepping());
       distanceFromStanceToTrustEnvironment.set(parameters.getDistanceFromStanceToTrustEnvironment());
       smallIntersectionAreaToFilter.set(parameters.getSmallIntersectionAreaToFilter());
+      useSimpleSnapping.set(parameters.useSimpleSnapping());
 
       parentRegistry.addChild(registry);
    }
@@ -60,5 +64,11 @@ public class YoSteppingEnvironmentalConstraintParameters extends SteppingEnviron
    public double getDistanceFromStanceToTrustEnvironment()
    {
       return distanceFromStanceToTrustEnvironment.getDoubleValue();
+   }
+
+   @Override
+   public boolean useSimpleSnapping()
+   {
+      return useSimpleSnapping.getValue();
    }
 }
