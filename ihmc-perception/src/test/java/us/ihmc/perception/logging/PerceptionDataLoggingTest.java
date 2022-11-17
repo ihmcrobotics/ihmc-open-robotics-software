0abfe06a21d75a7010810175114a3be0b34550ab
BhavyanshM <bhavyanshmishra@gmail.com>
2022-11-17 15:45:02 -0600

    Add initial unit test attempt for PNG depth logging.

--- ihmc-perception/src/test/java/us/ihmc/perception/logging/PerceptionDataLoggingTest.java
+++ ihmc-perception/src/test/java/us/ihmc/perception/logging/PerceptionDataLoggingTest.java
@@ -2,8 +2,15 @@ package us.ihmc.perception.logging;
 
 import org.bytedeco.hdf5.Group;
 import org.bytedeco.hdf5.global.hdf5;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.IntPointer;
+import org.bytedeco.opencv.global.opencv_core;
+import org.bytedeco.opencv.global.opencv_imgcodecs;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Scalar;
 import org.junit.jupiter.api.Test;
 import us.ihmc.log.LogTools;
+import us.ihmc.perception.BytedecoOpenCVTools;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
@@ -116,5 +123,57 @@ public class PerceptionDataLoggingTest
          assertEquals(dataArray[i], outputArray[i]);
       }
       hdf5ManagerReader.getFile().close();
+   }
+
+   @Test
+   public void testCompressedFloatDepthLogging()
+   {
+
+      hdf5ManagerWriter = new HDF5Manager("/home/bmishra/Workspace/Data/Sensor_Logs/hdf5_test.hdf5", hdf5.H5F_ACC_TRUNC());
+
+      Mat depth = new Mat(128, 128, opencv_core.CV_32FC1);
+      depth.put(new Scalar(1.234));
+
+
+      BytePointer compressedDepthPointer = new BytePointer();
+      BytedecoOpenCVTools.compressFloatDepthPNG(depth, compressedDepthPointer);
+
+      byte[] dataArray = new byte[compressedDepthPointer.asBuffer().remaining()];
+      Group writeGroup = hdf5ManagerWriter.getGroup("/test/bytes/");
+      HDF5Tools.storeByteArray(writeGroup, 0, dataArray, dataArray.length);
+
+      writeGroup.close();
+      hdf5ManagerWriter.getFile().close();
+
+//      hdf5ManagerReader = new HDF5Manager("/home/bmishra/Workspace/Data/Sensor_Logs/hdf5_test.hdf5", hdf5.H5F_ACC_RDONLY());
+//      Group readGroup = hdf5ManagerReader.openGroup("/test/bytes/");
+//
+//      byte[] outputArray = HDF5Tools.loadByteArray(readGroup, 0);
+//
+//      BytePointer messageEncodedBytePointer = new BytePointer(outputArray);
+//      messageEncodedBytePointer.limit(outputArray.length);
+//
+//      Mat inputJPEGMat = new Mat(1, outputArray.length, opencv_core.CV_8UC1, messageEncodedBytePointer);
+//      Mat inputYUVI420Mat = new Mat(1, 1, opencv_core.CV_8UC1);
+//      opencv_imgcodecs.imdecode(inputJPEGMat, opencv_imgcodecs.IMREAD_UNCHANGED, inputYUVI420Mat);
+//
+//      Mat finalMat = new Mat((int) (inputYUVI420Mat.rows() / 1.5f), inputYUVI420Mat.cols(), opencv_core.CV_8UC4);
+//
+//      LogTools.info("Height: {}, Width: {}", inputYUVI420Mat.rows(), inputYUVI420Mat.cols());
+//      opencv_imgproc.cvtColor(inputYUVI420Mat, finalMat, opencv_imgproc.COLOR_YUV2RGBA_I420);
+//
+//      Mat finalDepth = new Mat(128, 128, opencv_core.CV_32FC1, finalMat.data());
+//
+//      FloatBufferIndexer indexer = new FloatBufferIndexer(finalDepth.getFloatBuffer());
+//      FloatBufferIndexer indexerActual = new FloatBufferIndexer(depth.getFloatBuffer());
+//
+//      for(int i = 0; i<128; i++)
+//      {
+//         for(int j = 0; j<128; j++)
+//         {
+//            LogTools.info("Depth ({} {}): {}", i, j, indexer.get(i*128 + j));
+//            LogTools.info("Actual ({} {}): {}", i, j, indexerActual.get(i*128 + j));
+//         }
+//      }
    }
 }
