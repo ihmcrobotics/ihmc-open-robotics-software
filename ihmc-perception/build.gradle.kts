e7d5218370be06be78e736f713f5fdcbfd2daa5e
Dexton Anderson <danderson@ihmc.org>
2022-12-08 15:50:05 -0600

    Move line in build.gradle.kts

--- ihmc-perception/build.gradle.kts
+++ ihmc-perception/build.gradle.kts
@@ -25,7 +25,6 @@ mainDependencies {
    // For experimenting with local OpenCV:
    // api(files("/usr/local/share/OpenCV/java/opencv-310.jar"))
 
-   api("us.ihmc:ihmc-native-library-loader:2.0.1")
    api("org.georegression:georegression:0.22")
    api("org.ejml:ejml-core:0.39")
    api("org.ejml:ejml-ddense:0.39")
@@ -40,6 +39,7 @@ mainDependencies {
 
    api("us.ihmc:euclid:0.19.0")
    api("us.ihmc:simulation-construction-set:0.22.10")
+   api("us.ihmc:ihmc-native-library-loader:2.0.1")
    api("us.ihmc:ihmc-humanoid-robotics:source")
    api("us.ihmc:ihmc-communication:source")
    api("us.ihmc:ihmc-ros-tools:source")
