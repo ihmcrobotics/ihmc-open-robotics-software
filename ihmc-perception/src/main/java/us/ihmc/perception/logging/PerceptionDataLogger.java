d7ac87ea1c8696791e84329379f2bec99d9f4bc4
Operator Computer <OperatorComputer@unknown>
2022-12-22 14:25:14 -0600

    Add logger for Ouster depth with Realtime ROS2 node.

--- ihmc-perception/src/main/java/us/ihmc/perception/logging/PerceptionDataLogger.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/logging/PerceptionDataLogger.java
@@ -14,15 +14,21 @@ import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.idl.IDLSequence;
 import us.ihmc.log.LogTools;
 import us.ihmc.pubsub.DomainFactory;
+import us.ihmc.pubsub.common.SampleInfo;
 import us.ihmc.ros2.ROS2Node;
+import us.ihmc.ros2.ROS2QosProfile;
 import us.ihmc.ros2.RealtimeROS2Node;
+import us.ihmc.tools.string.StringTools;
 import us.ihmc.tools.thread.ExecutorServiceTools;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.bytedeco.opencv.global.opencv_highgui.imshow;
 import static org.bytedeco.opencv.global.opencv_highgui.waitKeyEx;
@@ -32,8 +38,6 @@ public class PerceptionDataLogger
    /* TODO:
     *     Remove or fix commented parts of the code.
     *     Improve multi-threading
-    *     Convert buffer sizes to final constant values
-    *
     * */
    private static final int BUFFER_SIZE = 2500000;
 
@@ -45,9 +49,9 @@ public class PerceptionDataLogger
    private final ScheduledExecutorService executorService = ExecutorServiceTools.newScheduledThreadPool(1,
                                                                                                         getClass(),
                                                                                                         ExecutorServiceTools.ExceptionHandling.CATCH_AND_REPORT);
-
    private ROS2Node ros2Node;
    private ROS2Helper ros2Helper;
+   private ROS2Helper realtimeROS2Helper;
    private HDF5Manager hdf5Manager;
    private RealtimeROS2Node realtimeROS2Node;
 
@@ -55,15 +59,25 @@ public class PerceptionDataLogger
    private int depthMapCount = 0;
 
    private HashMap<String, PerceptionLogChannel> channels = new HashMap<>();
+   private HashMap<String, AtomicReference<ImageMessage>> references = new HashMap<>();
 
    public PerceptionDataLogger()
    {
       channels.put(PerceptionLoggerConstants.D435_COLOR_NAME, new PerceptionLogChannel(PerceptionLoggerConstants.D435_COLOR_NAME, 0, 0));
+      references.put(PerceptionLoggerConstants.D435_COLOR_NAME, new AtomicReference<>(null));
+
       channels.put(PerceptionLoggerConstants.D435_DEPTH_NAME, new PerceptionLogChannel(PerceptionLoggerConstants.D435_DEPTH_NAME, 0, 0));
+      references.put(PerceptionLoggerConstants.D435_DEPTH_NAME, new AtomicReference<>(null));
+
       channels.put(PerceptionLoggerConstants.L515_COLOR_NAME, new PerceptionLogChannel(PerceptionLoggerConstants.L515_COLOR_NAME, 0, 0));
+      references.put(PerceptionLoggerConstants.L515_COLOR_NAME, new AtomicReference<>(null));
+
       channels.put(PerceptionLoggerConstants.L515_DEPTH_NAME, new PerceptionLogChannel(PerceptionLoggerConstants.L515_DEPTH_NAME, 0, 0));
-      channels.put(PerceptionLoggerConstants.OUSTER_CLOUD_NAME, new PerceptionLogChannel(PerceptionLoggerConstants.OUSTER_CLOUD_NAME, 0, 0));
+      references.put(PerceptionLoggerConstants.L515_DEPTH_NAME, new AtomicReference<>(null));
+
       channels.put(PerceptionLoggerConstants.OUSTER_DEPTH_NAME, new PerceptionLogChannel(PerceptionLoggerConstants.OUSTER_DEPTH_NAME, 0, 0));
+      references.put(PerceptionLoggerConstants.OUSTER_DEPTH_NAME, new AtomicReference<>(null));
+
       channels.put(PerceptionLoggerConstants.ROBOT_CONFIGURATION_DATA_NAME,
                    new PerceptionLogChannel(PerceptionLoggerConstants.ROBOT_CONFIGURATION_DATA_NAME, 0, 0));
 
@@ -99,52 +113,84 @@ public class PerceptionDataLogger
       // Use both regular and real-time ROS2 nodes to assign callbacks to different message types
       ros2Node = ROS2Tools.createROS2Node(communicationMode.getPubSubImplementation(), "perception_logger_node");
       ros2Helper = new ROS2Helper(ros2Node);
+
       realtimeROS2Node = ROS2Tools.createRealtimeROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "perception_logger_realtime_node");
+      realtimeROS2Helper = new ROS2Helper(realtimeROS2Node);
 
       // Add callback for Robot Configuration Data
-      var robotConfigurationDataSubscription = ros2Helper.subscribe(ROS2Tools.getRobotConfigurationDataTopic(simpleRobotName));
-      robotConfigurationDataSubscription.addCallback(this::logRobotConfigurationData);
-      runnablesToStopLogging.addLast(robotConfigurationDataSubscription::destroy);
+      if(channels.get(PerceptionLoggerConstants.ROBOT_CONFIGURATION_DATA_NAME).isEnabled())
+      {
+         var robotConfigurationDataSubscription = ros2Helper.subscribe(ROS2Tools.getRobotConfigurationDataTopic(simpleRobotName));
+         robotConfigurationDataSubscription.addCallback(this::logRobotConfigurationData);
+         runnablesToStopLogging.addLast(robotConfigurationDataSubscription::destroy);
+      }
 
       // Add callback for D435 Color images
-      var d435VideoSubscription = ros2Helper.subscribe(ROS2Tools.D435_VIDEO);
-      d435VideoSubscription.addCallback(this::logColorD435);
-      runnablesToStopLogging.addLast(d435VideoSubscription::destroy);
-      byteBuffers.put(PerceptionLoggerConstants.D435_COLOR_NAME, new byte[BUFFER_SIZE]);
-      counts.put(PerceptionLoggerConstants.D435_COLOR_NAME, 0);
+      if(channels.get(PerceptionLoggerConstants.D435_COLOR_NAME).isEnabled())
+      {
+         byteBuffers.put(PerceptionLoggerConstants.D435_COLOR_NAME, new byte[BUFFER_SIZE]);
+         counts.put(PerceptionLoggerConstants.D435_COLOR_NAME, 0);
+         var d435VideoSubscription = ros2Helper.subscribe(ROS2Tools.D435_VIDEO);
+         d435VideoSubscription.addCallback(this::logColorD435);
+         runnablesToStopLogging.addLast(d435VideoSubscription::destroy);
+      }
 
       // Add callback for D435 Depth images
-      var d435DepthSubscription = ros2Helper.subscribe(ROS2Tools.D435_DEPTH);
-      d435DepthSubscription.addCallback(this::logDepthD435);
-      runnablesToStopLogging.addLast(d435DepthSubscription::destroy);
-      byteBuffers.put(PerceptionLoggerConstants.D435_DEPTH_NAME, new byte[BUFFER_SIZE]);
-      counts.put(PerceptionLoggerConstants.D435_DEPTH_NAME, 0);
+      if(channels.get(PerceptionLoggerConstants.D435_DEPTH_NAME).isEnabled())
+      {
+         byteBuffers.put(PerceptionLoggerConstants.D435_DEPTH_NAME, new byte[BUFFER_SIZE]);
+         counts.put(PerceptionLoggerConstants.D435_DEPTH_NAME, 0);
+         var d435DepthSubscription = ros2Helper.subscribe(ROS2Tools.D435_DEPTH);
+         d435DepthSubscription.addCallback(this::logDepthD435);
+         runnablesToStopLogging.addLast(d435DepthSubscription::destroy);
+      }
 
       // Add callback for L515 Depth Maps
-      var l515DepthSubscription = ros2Helper.subscribe(ROS2Tools.L515_DEPTH_IMAGE);
-      l515DepthSubscription.addCallback(this::logDepthL515);
-      runnablesToStopLogging.addLast(l515DepthSubscription::destroy);
-      byteBuffers.put(PerceptionLoggerConstants.L515_DEPTH_NAME, new byte[BUFFER_SIZE]);
-      counts.put(PerceptionLoggerConstants.L515_DEPTH_NAME, 0);
+      if(channels.get(PerceptionLoggerConstants.L515_DEPTH_NAME).isEnabled())
+      {
+         byteBuffers.put(PerceptionLoggerConstants.L515_DEPTH_NAME, new byte[BUFFER_SIZE]);
+         counts.put(PerceptionLoggerConstants.L515_DEPTH_NAME, 0);
+         var l515DepthSubscription = ros2Helper.subscribe(ROS2Tools.L515_DEPTH_IMAGE);
+         l515DepthSubscription.addCallback(this::logDepthL515);
+         runnablesToStopLogging.addLast(l515DepthSubscription::destroy);
+      }
 
       // Add callback for L515 Color Images
-      var l515ColorSubscription = ros2Helper.subscribe(ROS2Tools.L515_COLOR_IMAGE);
-      l515ColorSubscription.addCallback(this::logColorL515);
-      runnablesToStopLogging.addLast(l515ColorSubscription::destroy);
-      byteBuffers.put(PerceptionLoggerConstants.L515_COLOR_NAME, new byte[BUFFER_SIZE]);
-      counts.put(PerceptionLoggerConstants.L515_COLOR_NAME, 0);
+      if(channels.get(PerceptionLoggerConstants.L515_COLOR_NAME).isEnabled())
+      {
+         byteBuffers.put(PerceptionLoggerConstants.L515_COLOR_NAME, new byte[BUFFER_SIZE]);
+         counts.put(PerceptionLoggerConstants.L515_COLOR_NAME, 0);
+         var l515ColorSubscription = ros2Helper.subscribe(ROS2Tools.L515_COLOR_IMAGE);
+         l515ColorSubscription.addCallback(this::logColorL515);
+         runnablesToStopLogging.addLast(l515ColorSubscription::destroy);
+      }
 
       // Add callback for D435 Color images
-      var zed2StereoSubscription = ros2Helper.subscribe(ROS2Tools.ZED2_STEREO_COLOR);
-      zed2StereoSubscription.addCallback(this::logColorZED2);
-      runnablesToStopLogging.addLast(zed2StereoSubscription::destroy);
-      byteBuffers.put(PerceptionLoggerConstants.ZED2_COLOR_NAME, new byte[BUFFER_SIZE]);
-      counts.put(PerceptionLoggerConstants.ZED2_COLOR_NAME, 0);
+      if(channels.get(PerceptionLoggerConstants.ZED2_COLOR_NAME).isEnabled())
+      {
+         byteBuffers.put(PerceptionLoggerConstants.ZED2_COLOR_NAME, new byte[BUFFER_SIZE]);
+         counts.put(PerceptionLoggerConstants.ZED2_COLOR_NAME, 0);
+         var zed2StereoSubscription = ros2Helper.subscribe(ROS2Tools.ZED2_STEREO_COLOR);
+         zed2StereoSubscription.addCallback(this::logColorZED2);
+         runnablesToStopLogging.addLast(zed2StereoSubscription::destroy);
+      }
 
       // Add callback for Ouster depth maps
-      var ousterDepthSubscription = ros2Helper.subscribe(ROS2Tools.OUSTER_DEPTH_IMAGE);
-      ousterDepthSubscription.addCallback(this::logDepthOuster);
-      runnablesToStopLogging.addLast(ousterDepthSubscription::destroy);
+      if(channels.get(PerceptionLoggerConstants.OUSTER_DEPTH_NAME).isEnabled())
+      {
+         SampleInfo sampleInfo = new SampleInfo();
+         byteBuffers.put(PerceptionLoggerConstants.OUSTER_DEPTH_NAME, new byte[BUFFER_SIZE]);
+         counts.put(PerceptionLoggerConstants.OUSTER_DEPTH_NAME, 0);
+         ROS2Tools.createCallbackSubscription(realtimeROS2Node, ROS2Tools.OUSTER_DEPTH_IMAGE, ROS2QosProfile.BEST_EFFORT(), (subscriber) ->
+         {
+            LogTools.info("Depth Map Received");
+
+            ImageMessage imageMessage = new ImageMessage();
+            subscriber.takeNextData(imageMessage, sampleInfo);
+            references.get(PerceptionLoggerConstants.OUSTER_DEPTH_NAME).set(imageMessage);
+            logDepthOuster(references.get(PerceptionLoggerConstants.OUSTER_DEPTH_NAME).getAndSet(null));
+         });
+      }
 
       realtimeROS2Node.spin();
 
@@ -196,7 +242,7 @@ public class PerceptionDataLogger
 
    public void logDepthOuster(ImageMessage message)
    {
-      LogTools.info("Depth Map Received: {}", message.data_);
+      LogTools.info("Depth Map Received: {}", message.getAcquisitionTimeSecondsSinceEpoch());
 
       if (channels.get(PerceptionLoggerConstants.OUSTER_DEPTH_NAME).isEnabled())
       {
@@ -329,22 +375,6 @@ public class PerceptionDataLogger
                              });
    }
 
-   public void storePointCloud(String namespace, FusedSensorHeadPointCloudMessage message)
-   {
-      Group group = hdf5Manager.getGroup(namespace);
-
-      executorService.submit(() ->
-                             {
-                                synchronized (this)
-                                {
-                                   LogTools.info("{} Storing Buffer: {}", namespace, pointCloudCount);
-                                   pointCloudCount = (int) hdf5Manager.getCount(namespace);
-                                   HDF5Tools.storeByteArray(group, pointCloudCount, message.getScan().toArray(), message.getScan().size());
-                                   LogTools.info("{} Done Storing Buffer: {}", namespace, pointCloudCount);
-                                }
-                             });
-   }
-
    public void storeFloatArray(String namespace, float[] array)
    {
       executorService.submit(() ->
@@ -429,10 +459,23 @@ public class PerceptionDataLogger
       PerceptionDataLogger logger = new PerceptionDataLogger();
 
       //logger.setChannelEnabled(logger.PerceptionLoggerConstants.ROBOT_CONFIGURATION_DATA_NAME, true);
-      logger.setChannelEnabled(PerceptionLoggerConstants.L515_DEPTH_NAME, true);
+//      logger.setChannelEnabled(PerceptionLoggerConstants.L515_DEPTH_NAME, true);
       //      logger.setChannelEnabled(PerceptionLoggerConstants.L515_COLOR_NAME, true);
 
+      logger.setChannelEnabled(PerceptionLoggerConstants.OUSTER_DEPTH_NAME, true);
+
       logger.startLogging(logDirectory + logFileName, "Nadia");
+
+      // TEST ROS2 node for Ouster Depth
+
+//      RealtimeROS2Node realtimeROS2Node = ROS2Tools.createRealtimeROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, StringTools.titleToSnakeCase("logger_realtime_ros2_node"));
+//
+//      ROS2Tools.createCallbackSubscription(realtimeROS2Node, ROS2Tools.OUSTER_DEPTH_IMAGE, ROS2QosProfile.BEST_EFFORT(), (message) -> {
+//         LogTools.info("Message Received: {}", message);
+//      });
+//
+//      ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+//      executor.scheduleAtFixedRate(realtimeROS2Node::spin, 0, 10, TimeUnit.MILLISECONDS);
    }
 }
 
