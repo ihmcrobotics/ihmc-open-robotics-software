1393a0264ef0275904624da67a1b2b48c606b49c
Duncan Calvert <dcalvert@ihmc.us>
2023-01-13 11:56:03 -0600

    Extract ImGuiDirectory.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/footstepPlanner/RDXFootstepPlannerLogViewer.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/footstepPlanner/RDXFootstepPlannerLogViewer.java
@@ -6,11 +6,9 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 import imgui.ImGui;
 import imgui.type.ImBoolean;
-import imgui.type.ImString;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.behaviors.tools.footstepPlanner.MinimalFootstep;
 import us.ihmc.commons.nio.BasicPathVisitor;
-import us.ihmc.commons.nio.PathTools;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.commons.thread.TypedNotification;
 import us.ihmc.communication.packets.PlanarRegionMessageConverter;
@@ -28,7 +26,6 @@ import us.ihmc.footstepPlanning.log.FootstepPlannerLogger;
 import us.ihmc.footstepPlanning.tools.FootstepPlannerRejectionReasonReport;
 import us.ihmc.log.LogTools;
 import us.ihmc.rdx.imgui.ImGuiPanel;
-import us.ihmc.rdx.imgui.ImGuiTools;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.input.ImGui3DViewInput;
 import us.ihmc.rdx.sceneManager.RDX3DScene;
@@ -40,6 +37,7 @@ import us.ihmc.rdx.ui.RDX3DPanelTooltip;
 import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.rdx.ui.graphics.RDXFootstepGraphic;
 import us.ihmc.rdx.ui.graphics.RDXFootstepPlanGraphic;
+import us.ihmc.rdx.ui.tools.ImGuiDirectory;
 import us.ihmc.rdx.visualizers.RDXPlanarRegionsGraphic;
 import us.ihmc.rdx.visualizers.RDXSphereAndArrowGraphic;
 import us.ihmc.robotics.referenceFrames.ModifiableReferenceFrame;
@@ -47,29 +45,20 @@ import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
 import java.io.File;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 public class RDXFootstepPlannerLogViewer
 {
    private final RDX3DScene scene3D;
    private final RDX3DPanel panel3D;
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
-   private final ImString logFolder = new ImString();
+   private final ImGuiDirectory directory;
    private final TypedNotification<FootstepPlannerLog> logLoadedNotification = new TypedNotification<>();
    private FootstepPlannerLog footstepPlannerLog = null;
    private final SideDependentList<RDXFootstepGraphic> startFootPoses = new SideDependentList<>();
    private final SideDependentList<RDXFootstepGraphic> goalFootPoses = new SideDependentList<>();
    private final RDXSphereAndArrowGraphic goalGraphic;
    private final Pose3D goalPose = new Pose3D();
-   private final Comparator<Path> naturalOrderComparator = Comparator.comparing(path -> path.getFileName().toString());
-   private final SortedSet<Path> sortedLogFolderPaths = new TreeSet<>(naturalOrderComparator.reversed());
-   private boolean indexedLogFolderOnce = false;
    private final RDXPlanarRegionsGraphic planarRegionsGraphic;
    private final RDXFootstepPlanGraphic footstepPlanGraphic;
    private FootstepPlan footstepPlan;
@@ -100,7 +89,11 @@ public class RDXFootstepPlannerLogViewer
       tooltip = new RDX3DPanelTooltip(panel3D);
       panel3D.addImGuiOverlayAddition(this::probeTooltip);
 
-      logFolder.set(FootstepPlannerLogger.defaultLogsDirectory);
+      directory = new ImGuiDirectory(FootstepPlannerLogger.defaultLogsDirectory,
+                                     logName -> footstepPlannerLog != null && footstepPlannerLog.getLogName().equals(logName),
+                                     pathEntry -> pathEntry.type() == BasicPathVisitor.PathType.DIRECTORY
+                                        && pathEntry.path().getFileName().toString().endsWith(FootstepPlannerLogger.FOOTSTEP_PLANNER_LOG_POSTFIX),
+                                     this::logSelectedAction);
 
       goalGraphic = new RDXSphereAndArrowGraphic();
       goalGraphic.create(0.027, 0.027 * 6.0, Color.GREEN);
@@ -161,41 +154,7 @@ public class RDXFootstepPlannerLogViewer
 
    public void renderImGuiWidgets()
    {
-      ImGuiTools.inputText(labels.get("Log folder"), logFolder);
-
-      boolean reindexClicked = ImGui.button(labels.get("Reindex log folder"));
-      if (!indexedLogFolderOnce || reindexClicked)
-      {
-         indexedLogFolderOnce = true;
-         reindexLogFolder();
-      }
-      ImGui.sameLine();
-      ImGui.text("Available logs:");
-
-      ImGui.beginChild(labels.get("Scroll area"), ImGui.getColumnWidth(), 150.0f);
-      for (Path sortedLogFolderPath : sortedLogFolderPaths)
-      {
-         String logName = sortedLogFolderPath.getFileName().toString();
-         if (ImGui.radioButton(labels.get(logName), footstepPlannerLog != null && footstepPlannerLog.getLogName().equals(logName)))
-         {
-            footstepPlannerLog = null;
-            ThreadTools.startAThread(() ->
-            {
-               FootstepPlannerLogLoader logLoader = new FootstepPlannerLogLoader();
-               FootstepPlannerLogLoader.LoadResult loadResult = logLoader.load(new File(logFolder.get() + "/" + logName));
-               if (loadResult == FootstepPlannerLogLoader.LoadResult.LOADED)
-               {
-                  logLoadedNotification.set(logLoader.getLog());
-               }
-               else if (loadResult == FootstepPlannerLogLoader.LoadResult.ERROR)
-               {
-                  LogTools.error("Error loading log");
-               }
-
-            }, "FootstepPlanLogLoading");
-         }
-      }
-      ImGui.endChild();
+      directory.renderImGuiWidgets();
 
       ImGui.checkbox(labels.get("Probe mode"), probeMode);
 
@@ -236,15 +195,23 @@ public class RDXFootstepPlannerLogViewer
       }
    }
 
-   private void reindexLogFolder()
+   private void logSelectedAction(String logName)
    {
-      sortedLogFolderPaths.clear();
-      PathTools.walkFlat(Paths.get(logFolder.get()), (path, type) -> {
-         if (type == BasicPathVisitor.PathType.DIRECTORY
-             && path.getFileName().toString().endsWith(FootstepPlannerLogger.FOOTSTEP_PLANNER_LOG_POSTFIX))
-            sortedLogFolderPaths.add(path);
-         return FileVisitResult.CONTINUE;
-      });
+      footstepPlannerLog = null;
+      ThreadTools.startAThread(() ->
+      {
+         FootstepPlannerLogLoader logLoader = new FootstepPlannerLogLoader();
+         FootstepPlannerLogLoader.LoadResult loadResult = logLoader.load(new File(directory.getDirectoryName() + "/" + logName));
+         if (loadResult == FootstepPlannerLogLoader.LoadResult.LOADED)
+         {
+            logLoadedNotification.set(logLoader.getLog());
+         }
+         else if (loadResult == FootstepPlannerLogLoader.LoadResult.ERROR)
+         {
+            LogTools.error("Error loading log");
+         }
+
+      }, "FootstepPlanLogLoading");
    }
 
    private void probeTooltip()
