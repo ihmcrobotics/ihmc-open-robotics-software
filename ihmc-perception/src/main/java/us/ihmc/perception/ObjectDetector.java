f68496327ee1fe150af9793196d6fd56aeb5c78b
lpenco <lpenco@ihmc.org>
2023-01-18 16:53:18 -0600

    developing aruco real

--- ihmc-perception/src/main/java/us/ihmc/perception/ObjectDetector.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/ObjectDetector.java
@@ -23,71 +23,16 @@ public class ObjectDetector
 {
    private static final ROS2Topic<?> BASE_TOPIC = ROS2Tools.IHMC_ROOT.withModule("perception_manager");
    public static final ROS2Topic<ArUcoMarkerPoses> ARUCO_MARKER_POSES = BASE_TOPIC.withType(ArUcoMarkerPoses.class).withSuffix("aruco_marker_poses");
-   public static final ROS2Topic<DetectedObjectMessage> DETECTED_PULL_DOOR_FRAME
-         = BASE_TOPIC.withType(DetectedObjectMessage.class).withSuffix("detected_pull_door_frame");
-   public static final ROS2Topic<DetectedObjectMessage> DETECTED_PULL_DOOR_PANEL
-         = BASE_TOPIC.withType(DetectedObjectMessage.class).withSuffix("detected_pull_door_panel");
-   public static final ROS2Topic<DetectedObjectMessage> DETECTED_PUSH_DOOR_FRAME
-         = BASE_TOPIC.withType(DetectedObjectMessage.class).withSuffix("detected_push_door_frame");
-   public static final ROS2Topic<DetectedObjectMessage> DETECTED_PUSH_DOOR_PANEL
-         = BASE_TOPIC.withType(DetectedObjectMessage.class).withSuffix("detected_push_door_panel");
-   public static final ROS2Topic<DetectedObjectMessage> DETECTED_BOX = BASE_TOPIC.withType(DetectedObjectMessage.class).withSuffix("detected_box");
-
-   public static final double REAL_MARKER_WIDTH = 0.1680;
-
-   /** This refers to the edges of the black parts with no margin. The margins included will be wider than this. */
-   public static final double DOOR_MARKER_WIDTH = 0.2032;
-
-   public static final long PULL_DOOR_MARKER_ID = 0;
-   public static final RigidBodyTransform PULL_DOOR_FRAME_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(Math.toRadians(180.0), 0.0, Math.toRadians(180.0)),
-         new Point3D(0.0, -0.678702 - 0.005 - 0.006, 1.14141 + 0.02)
-   );
-   public static final RigidBodyTransform PULL_DOOR_PANEL_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(Math.toRadians(180.0), 0.0, Math.toRadians(180.0)),
-         new Point3D(0.0, -0.678702, 1.14141)
-   );
-
-   public static final long PUSH_DOOR_MARKER_ID = 1;
-   public static final RigidBodyTransform PUSH_DOOR_FRAME_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(0.0, 0.0, Math.toRadians(180.0)),
-         new Point3D(0.0, 0.678702 + 0.005 - 0.006, 1.14141 + 0.02)
-   );
-   public static final RigidBodyTransform PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(0.0, 0.0, Math.toRadians(180.0)),
-         new Point3D(0.0, 0.678702, 1.14141)
-   );
-
-   public static final long BOX_MARKER_ID = 2;
-   public static final double BOX_MARKER_WIDTH = 0.210101;
-   public static final RigidBodyTransform BOX_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(Math.toRadians(180.0), Math.toRadians(0.0), Math.toRadians(-90.0)),
-         new Point3D(0.07, 0.15, 0.17)
-   );
 
    private final IHMCROS2Input<ArUcoMarkerPoses> arUcoMarkerPosesSubscription;
    private final HashMap<Long, BiConsumer<Tuple3DReadOnly, QuaternionReadOnly>> markerUpdaters = new HashMap<>();
-   private final DoorPerceptionManager pullDoorManager;
-   private final DoorPerceptionManager pushDoorManager;
    private final ROS2Helper ros2;
-
-   private final ArrayList<DetectedObjectPublisher> detectedObjectPublishers = new ArrayList<>();
 
    public ObjectDetector(ReferenceFrame cameraFrame)
    {
       ros2 = new ROS2Helper(DomainFactory.PubSubImplementation.FAST_RTPS, "perception_manager");
 
       arUcoMarkerPosesSubscription = ros2.subscribe(ARUCO_MARKER_POSES);
-
-      pullDoorManager = new DoorPerceptionManager(PULL_DOOR_MARKER_ID, "Pull", cameraFrame);
-      pullDoorManager.getDoorFrame().setObjectTransformToMarker(transform -> transform.set(PULL_DOOR_FRAME_TRANSFORM_TO_MARKER));
-      pullDoorManager.getDoorPanel().setObjectTransformToMarker(transform -> transform.set(PULL_DOOR_PANEL_TRANSFORM_TO_MARKER));
-      markerUpdaters.put(PULL_DOOR_MARKER_ID, pullDoorManager::updateMarkerTransform);
-
-      pushDoorManager = new DoorPerceptionManager(PUSH_DOOR_MARKER_ID, "Push", cameraFrame);
-      pushDoorManager.getDoorFrame().setObjectTransformToMarker(transform -> transform.set(PUSH_DOOR_FRAME_TRANSFORM_TO_MARKER));
-      pushDoorManager.getDoorPanel().setObjectTransformToMarker(transform -> transform.set(PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER));
-      markerUpdaters.put(PUSH_DOOR_MARKER_ID, pushDoorManager::updateMarkerTransform);
 
       detectedObjectPublishers.add(new DetectedObjectPublisher(ros2,
                                                                DETECTED_PULL_DOOR_FRAME,
@@ -137,15 +82,5 @@ public class ObjectDetector
             detectedObjectPublisher.publish();
          }
       }
-   }
-
-   public DoorPerceptionManager getPushDoorManager()
-   {
-      return pushDoorManager;
-   }
-
-   public DoorPerceptionManager getPullDoorManager()
-   {
-      return pullDoorManager;
    }
 }
