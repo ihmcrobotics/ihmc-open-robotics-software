478fd48889abcb29ad655605d9825ffd1dc5f3ba
Robert Griffin <rgriffin@ihmc.us>
2023-01-22 00:21:11 -0600

    started working on trying to create the steppable regions

---
+++ ihmc-perception/src/main/java/us/ihmc/ihmcPerception/steppableRegions/SteppableRegionsCalculationModule.java
@@ -0,0 +1,142 @@
+package us.ihmc.ihmcPerception.steppableRegions;
+
+import org.bytedeco.opencl._cl_kernel;
+import org.bytedeco.opencl._cl_program;
+import org.bytedeco.opencv.global.opencv_core;
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.perception.BytedecoImage;
+import us.ihmc.perception.BytedecoTools;
+import us.ihmc.perception.OpenCLManager;
+import us.ihmc.perception.opencl.OpenCLFloatParameters;
+import us.ihmc.robotics.geometry.concavePolygon2D.ConcavePolygon2D;
+import us.ihmc.sensorProcessing.heightMap.HeightMapData;
+
+import java.util.List;
+
+public class SteppableRegionsCalculationModule
+{
+   private static final float distanceFromCliffTops = 0.02f;
+   private static final float distanceFromCliffBottoms = 0.05f;
+   private static final int yawDiscretizations = 5;
+   private static final float footWidth = 0.12f;
+   private static final float footLength = 0.22f;
+
+   private static final int defaultCells = 50;
+   private final OpenCLManager openCLManager;
+   private _cl_program steppableRegionsProgram;
+   private _cl_kernel computeSteppabilityKernel;
+   private _cl_kernel computeSteppabilityConnectionsKernel;
+
+   private final SteppableRegionsCalculator regionsCalculator = new SteppableRegionsCalculator();
+
+   private final OpenCLFloatParameters steppableParameters = new OpenCLFloatParameters();
+   private final BytedecoImage heightMapImage = new BytedecoImage(defaultCells, defaultCells, opencv_core.CV_32FC1);
+   private final BytedecoImage steppabilityImage0 = new BytedecoImage(defaultCells, defaultCells, opencv_core.CV_32FC1);
+   private final BytedecoImage steppabilityConnections0 = new BytedecoImage(defaultCells, defaultCells, opencv_core.CV_32FC1);
+
+   private int cellsPerSide;
+
+   public SteppableRegionsCalculationModule()
+   {
+      // Load all the native data on the thread. This effectively just loads all the bytedeco stuff to be used by the planner
+      var nativeLoader = BytedecoTools.loadNativesOnAThread();
+      boolean doneLoading = false;
+
+      openCLManager = new OpenCLManager();
+
+      while (!doneLoading)
+      {
+         if (nativeLoader.poll())
+         {
+            if (nativeLoader.isNewlyActivated())
+            {
+               createOpenCLStuff(defaultCells);
+               doneLoading = true;
+            }
+         }
+      }
+   }
+
+   public List<ConvexPolygon2D> getConvexSteppableRegions()
+   {
+      return convexSteppableRegions;
+   }
+   /**
+    * This creates all the open cl managers, programs, and kernels used in the planner
+    */
+   private void createOpenCLStuff(int cellsPerSide)
+   {
+      this.cellsPerSide = cellsPerSide;
+
+      openCLManager.create();
+
+      steppableRegionsProgram = openCLManager.loadProgram("SteppableRegions");
+      computeSteppabilityKernel = openCLManager.createKernel(steppableRegionsProgram, "computeSteppability");
+      computeSteppabilityConnectionsKernel = openCLManager.createKernel(steppableRegionsProgram, "computeSteppabilityConnections");
+   }
+
+   public void compute(HeightMapData heightMapData)
+   {
+      resize(heightMapData.getCellsPerAxis());
+
+      populateSteppabilityParameters(heightMapData);
+      populateHeightMapImage(heightMapData);
+
+      openCLManager.setKernelArgument(computeSteppabilityKernel, 0, steppableParameters.getOpenCLBufferObject());
+      openCLManager.setKernelArgument(computeSteppabilityKernel, 1, heightMapImage.getOpenCLImageObject());
+
+      openCLManager.execute3D(computeSteppabilityKernel, cellsPerSide, cellsPerSide, yawDiscretizations);
+
+      openCLManager.finish();
+
+      createRegionsFromResults(heightMapData);
+   }
+
+   private void resize(int cellsPerSide)
+   {
+      if (this.cellsPerSide == cellsPerSide)
+         return;
+
+      this.cellsPerSide = cellsPerSide;
+
+      heightMapImage.resize(cellsPerSide, cellsPerSide, openCLManager, null);
+   }
+
+   private void populateSteppabilityParameters(HeightMapData heightMapData)
+   {
+      steppableParameters.setParameter(heightMapData.getCenterIndex());
+      steppableParameters.setParameter((float) heightMapData.getGridResolutionXY());
+      steppableParameters.setParameter(distanceFromCliffTops);
+      steppableParameters.setParameter(distanceFromCliffBottoms);
+      steppableParameters.setParameter(yawDiscretizations);
+      steppableParameters.setParameter(footWidth);
+      steppableParameters.setParameter(footLength);
+
+      steppableParameters.writeOpenCLBufferObject(openCLManager);
+   }
+
+   private void populateHeightMapImage(HeightMapData heightMapData)
+   {
+      for (int xIndex = 0; xIndex < heightMapData.getCellsPerAxis(); xIndex++)
+      {
+         for (int yIndex = 0; yIndex < heightMapData.getCellsPerAxis(); yIndex++)
+         {
+            // the x and y values are switched between the world coordinates and the image coordinates
+            heightMapImage.setFloat(yIndex, xIndex, (float) heightMapData.getHeightAt(xIndex, yIndex));
+         }
+      }
+      heightMapImage.writeOpenCLImage(openCLManager);
+   }
+
+
+   private void createRegionsFromResults(HeightMapData heightMapData)
+   {
+      List<ConvexPolygon2D> regions0 = regionsCalculator.calculate(heightMapData, steppabilityImage0, steppabilityConnections0);
+   }
+
+   public static void main(String[] args)
+   {
+      new SteppableRegionsCalculationModule();
+   }
+
+}
--- ihmc-perception/src/main/java/us/ihmc/ihmcPerception/steppableRegions/SteppableRegionsCalculator.java
+++ ihmc-perception/src/main/java/us/ihmc/ihmcPerception/steppableRegions/SteppableRegionsCalculator.java
@@ -1,120 +1,200 @@
 package us.ihmc.ihmcPerception.steppableRegions;
 
-import org.bytedeco.opencl._cl_kernel;
-import org.bytedeco.opencl._cl_program;
-import org.bytedeco.opencv.global.opencv_core;
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.perception.BytedecoImage;
-import us.ihmc.perception.BytedecoTools;
-import us.ihmc.perception.OpenCLManager;
-import us.ihmc.perception.opencl.OpenCLFloatParameters;
 import us.ihmc.sensorProcessing.heightMap.HeightMapData;
+import us.ihmc.sensorProcessing.heightMap.HeightMapTools;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SteppableRegionsCalculator
 {
-   private static final float distanceFromCliffTops = 0.02f;
-   private static final float distanceFromCliffBottoms = 0.05f;
-   private static final int yawDiscretizations = 5;
-   private static final float footWidth = 0.12f;
-   private static final float footLength = 0.22f;
+   private final List<SteppableCell> unassignedCells = new ArrayList<>();
+   private SteppableCell[][] steppableCellGrid;
+   private final List<SteppableRegionData> steppableRegions = new ArrayList<>();
 
-   private static final int defaultCells = 50;
-   private final OpenCLManager openCLManager;
-   private _cl_program steppableRegionsProgram;
-   private _cl_kernel computeSteppabilityKernel;
-
-   private final OpenCLFloatParameters steppableParameters = new OpenCLFloatParameters();
-   private final BytedecoImage heightMapImage = new BytedecoImage(defaultCells, defaultCells, opencv_core.CV_32FC1);
-
-   private int cellsPerSide;
-
-   public SteppableRegionsCalculator()
+   public List<ConvexPolygon2D> calculate(HeightMapData heightMapData, BytedecoImage steppability, BytedecoImage connections)
    {
-      // Load all the native data on the thread. This effectively just loads all the bytedeco stuff to be used by the planner
-      var nativeLoader = BytedecoTools.loadNativesOnAThread();
-      boolean doneLoading = false;
+      unassignedCells.clear();
+      steppableRegions.clear();
 
-      openCLManager = new OpenCLManager();
+      if (steppability.getImageHeight() != steppability.getImageWidth())
+         throw new RuntimeException("Should be square");
 
-      while (!doneLoading)
+      createAllTheCells(steppability);
+
+      while (!unassignedCells.isEmpty())
+         recursivelyAddNeighbors(unassignedCells.get(0), steppability, connections);
+
+      return steppableRegions.parallelStream()
+                             .map(region -> SteppableRegionsCalculator.convertToConvexRegion(heightMapData, region))
+                             .collect(Collectors.toList());
+   }
+
+   private void createAllTheCells(BytedecoImage steppability)
+   {
+      int cellsPerSide = steppability.getImageHeight();
+      steppableCellGrid = new SteppableCell[cellsPerSide][];
+      for (int x = 0; x < cellsPerSide; x++)
       {
-         if (nativeLoader.poll())
+         steppableCellGrid[x] = new SteppableCell[cellsPerSide];
+         for (int y = 0; y < cellsPerSide; y++)
          {
-            if (nativeLoader.isNewlyActivated())
+            // this cell is steppable
+            if (steppability.getInt(y, x) == 1)
             {
-               createOpenCLStuff(defaultCells);
-               doneLoading = true;
+               SteppableCell cell = new SteppableCell(x, y, cellsPerSide);
+               unassignedCells.add(cell);
+               steppableCellGrid[x][y] = cell;
             }
          }
       }
    }
 
-   /**
-    * This creates all the open cl managers, programs, and kernels used in the planner
-    */
-   private void createOpenCLStuff(int cellsPerSide)
+   private void recursivelyAddNeighbors(SteppableCell cell, BytedecoImage steppability, BytedecoImage connections)
    {
-      this.cellsPerSide = cellsPerSide;
-
-      openCLManager.create();
-
-      steppableRegionsProgram = openCLManager.loadProgram("SteppableRegions");
-      computeSteppabilityKernel = openCLManager.createKernel(steppableRegionsProgram, "computeSteppability");
-   }
-
-   public void compute(HeightMapData heightMapData)
-   {
-      resize(heightMapData.getCellsPerAxis());
-
-      populateSteppabilityParameters(heightMapData);
-      populateHeightMapImage(heightMapData);
-
-      openCLManager.setKernelArgument(computeSteppabilityKernel, 0, steppableParameters.getOpenCLBufferObject());
-      openCLManager.setKernelArgument(computeSteppabilityKernel, 1, heightMapImage.getOpenCLImageObject());
-
-      openCLManager.execute3D(computeSteppabilityKernel, cellsPerSide, cellsPerSide, yawDiscretizations);
-
-      openCLManager.finish();
-   }
-
-   private void resize(int cellsPerSide)
-   {
-      if (this.cellsPerSide == cellsPerSide)
-         return;
-
-      this.cellsPerSide = cellsPerSide;
-
-      heightMapImage.resize(cellsPerSide, cellsPerSide, openCLManager, null);
-   }
-
-   private void populateSteppabilityParameters(HeightMapData heightMapData)
-   {
-      steppableParameters.setParameter(heightMapData.getCenterIndex());
-      steppableParameters.setParameter((float) heightMapData.getGridResolutionXY());
-      steppableParameters.setParameter(distanceFromCliffTops);
-      steppableParameters.setParameter(distanceFromCliffBottoms);
-      steppableParameters.setParameter(yawDiscretizations);
-      steppableParameters.setParameter(footWidth);
-      steppableParameters.setParameter(footLength);
-
-      steppableParameters.writeOpenCLBufferObject(openCLManager);
-   }
-
-   private void populateHeightMapImage(HeightMapData heightMapData)
-   {
-      for (int xIndex = 0; xIndex < heightMapData.getCellsPerAxis(); xIndex++)
+      if (!cell.hasRegion())
       {
-         for (int yIndex = 0; yIndex < heightMapData.getCellsPerAxis(); yIndex++)
+         SteppableRegionData region = new SteppableRegionData();
+         region.addCell(cell);
+
+         steppableRegions.add(region);
+      }
+      unassignedCells.remove(cell);
+
+      int boundaryConnectionsEncodedAsOnes = connections.getInt(cell.x, cell.y);
+
+      int cellsPerSide = steppability.getImageHeight();
+      for (int i = 0; i < 4; i++)
+      {
+         int neighborX = cell.x;
+         int neighborY = cell.y;
+
+         // These are switched from the open cl code, since the coordinates are different
+         if (i == 0)
+            neighborY++;
+         else if (i == 1)
+            neighborX++;
+         else if (i == 2)
+            neighborY--;
+         else
+            neighborX--;
+
+         if (neighborX < 0 || neighborY < 0 || neighborX >= cellsPerSide || neighborY >= cellsPerSide)
+            continue;
+
+         if (isConnected(i, boundaryConnectionsEncodedAsOnes))
          {
-            // the x and y values are switched between the world coordinates and the image coordinates
-            heightMapImage.setFloat(yIndex, xIndex, (float) heightMapData.getHeightAt(xIndex, yIndex));
+            SteppableCell neighbor = steppableCellGrid[neighborX][neighborY];
+            if (unassignedCells.contains(neighbor))
+            {
+               cell.getRegion().addCell(neighbor);
+               recursivelyAddNeighbors(neighbor, steppability, connections);
+            }
+            else
+            {
+               cell.getRegion().mergeRegion(neighbor.getRegion());
+            }
          }
       }
-      heightMapImage.writeOpenCLImage(openCLManager);
    }
 
-   public static void main(String[] args)
+   private boolean isConnected(int edgeNumber, int connectionValue)
    {
-      new SteppableRegionsCalculator();
+      int mask = (1 << edgeNumber);
+      int maskedValue = mask & connectionValue;
+      return maskedValue > 0;
    }
 
+   private static ConvexPolygon2D convertToConvexRegion(HeightMapData heightMapData, SteppableRegionData steppableRegionData)
+   {
+      ConvexPolygon2D polygon = new ConvexPolygon2D();
+      steppableRegionData.memberCells.forEach(cell -> polygon.addVertex(getAsPoint(heightMapData, cell)));
+      polygon.update();
+
+      return polygon;
+   }
+
+   private static Point2D getAsPoint(HeightMapData heightMapData, SteppableCell steppableCell)
+   {
+      double x = HeightMapTools.indexToCoordinate(steppableCell.x,
+                                                  heightMapData.getGridCenter().getX(),
+                                                  heightMapData.getGridResolutionXY(),
+                                                  heightMapData.getCenterIndex());
+      double y = HeightMapTools.indexToCoordinate(steppableCell.y,
+                                                  heightMapData.getGridCenter().getY(),
+                                                  heightMapData.getGridResolutionXY(),
+                                                  heightMapData.getCenterIndex());
+      return new Point2D(x, y);
+   }
+
+   private static class SteppableRegionData
+   {
+      private final HashSet<SteppableCell> memberCells = new HashSet<>();
+
+      public void addCell(SteppableCell cell)
+      {
+         memberCells.add(cell);
+         cell.setRegion(this);
+      }
+
+      public void mergeRegion(SteppableRegionData other)
+      {
+         other.memberCells.forEach(this::addCell);
+      }
+   }
+
+   private static class SteppableCell
+   {
+      private final int x;
+      private final int y;
+
+      private final int hashCode;
+
+      private SteppableRegionData region;
+
+      public SteppableCell(int x, int y, int cellsPerSide)
+      {
+         this.x = x;
+         this.y = y;
+
+         hashCode = x * cellsPerSide + y;
+      }
+
+      public boolean hasRegion()
+      {
+         return getRegion() == null;
+      }
+
+      public SteppableRegionData getRegion()
+      {
+         return region;
+      }
+
+      public void setRegion(SteppableRegionData region)
+      {
+         this.region = region;
+      }
+
+      @Override
+      public int hashCode()
+      {
+         return hashCode;
+      }
+
+      @Override
+      public boolean equals(Object obj)
+      {
+         if (obj instanceof SteppableCell other)
+         {
+            return hashCode == other.hashCode();
+         }
+
+         return false;
+      }
+   }
 }
