6a53ee5c765b8c0a3555cde01fd95da81a3117b3
BhavyanshM <bhavyanshmishra@gmail.com>
2023-01-25 01:33:10 -0600

    Initial attempt at rapid height map.

---
+++ ihmc-graphics/src/libgdx/java/us/ihmc/rdx/RDXHeightMapRenderer.java
@@ -0,0 +1,97 @@
+package us.ihmc.rdx;
+
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import org.lwjgl.opengl.GL41;
+import us.ihmc.rdx.shader.RDXShader;
+import us.ihmc.rdx.shader.RDXUniform;
+
+import java.nio.FloatBuffer;
+
+public class RDXHeightMapRenderer implements RenderableProvider
+{
+   private Renderable renderable;
+
+   public static final int FLOATS_PER_CELL = 8;
+   public static final int BYTES_PER_VERTEX = FLOATS_PER_CELL * Float.BYTES;
+   private final VertexAttributes vertexAttributes = new VertexAttributes(new VertexAttribute(VertexAttributes.Usage.Position,
+                                                                                              3,
+                                                                                              ShaderProgram.POSITION_ATTRIBUTE),
+                                                                          new VertexAttribute(VertexAttributes.Usage.ColorUnpacked,
+                                                                                              4,
+                                                                                              ShaderProgram.COLOR_ATTRIBUTE),
+                                                                          new VertexAttribute(VertexAttributes.Usage.Generic,
+                                                                                              1,
+                                                                                              GL41.GL_FLOAT,
+                                                                                              false,
+                                                                                              "a_size"));
+   private final int floatsPerCell = vertexAttributes.vertexSize / Float.BYTES;
+   private final RDXUniform screenWidthUniform = RDXUniform.createGlobalUniform("u_screenWidth", (shader, inputID, renderable, combinedAttributes) ->
+   {
+      shader.set(inputID, shader.camera.viewportWidth);
+   });
+   private int multiColor = 0;
+   private final RDXUniform multiColorUniform = RDXUniform.createGlobalUniform("u_multiColor", (shader, inputID, renderable, combinedAttributes) ->
+   {
+      shader.set(inputID, multiColor);
+   });
+
+   private int totalCells;
+
+   public void create(int numberOfCells)
+   {
+      GL41.glEnable(GL41.GL_VERTEX_PROGRAM_POINT_SIZE);
+
+      renderable = new Renderable();
+      renderable.meshPart.primitiveType = GL41.GL_POINTS;
+      renderable.meshPart.offset = 0;
+      renderable.material = new Material(ColorAttribute.createDiffuse(Color.WHITE));
+
+      totalCells = numberOfCells;
+      if (renderable.meshPart.mesh != null)
+         renderable.meshPart.mesh.dispose();
+      boolean isStatic = false;
+      int maxIndices = 0;
+      renderable.meshPart.mesh = new Mesh(isStatic, totalCells, maxIndices, vertexAttributes);
+
+      RDXShader shader = new RDXShader(getClass());
+      shader.create();
+      shader.getBaseShader().register(DefaultShader.Inputs.viewTrans, DefaultShader.Setters.viewTrans);
+      shader.getBaseShader().register(DefaultShader.Inputs.projTrans, DefaultShader.Setters.projTrans);
+      shader.registerUniform(screenWidthUniform);
+      shader.registerUniform(multiColorUniform);
+      shader.init(renderable);
+      renderable.shader = shader.getBaseShader();
+   }
+
+   public void update()
+   {
+      FloatBuffer floatBuffer = renderable.meshPart.mesh.getVerticesBuffer();
+      floatBuffer.position(0);
+      floatBuffer.limit(totalCells * floatsPerCell);
+      renderable.meshPart.size = totalCells;
+   }
+
+   public FloatBuffer getVertexBuffer()
+   {
+      return renderable.meshPart.mesh.getVerticesBuffer();
+   }
+
+   @Override
+   public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
+   {
+      if (renderable != null)
+         renderables.add(renderable);
+   }
+
+   public void dispose()
+   {
+      if (renderable.meshPart.mesh != null)
+         renderable.meshPart.mesh.dispose();
+   }
+}
