00b3b44b230a1a0df788a6bf56a39ab8132b7f21
Duncan Calvert <dcalvert@ihmc.us>
2023-01-25 22:46:07 -0600

    Fill out the parameters for the calibrate function. The calibration suite has threading issues now, as expected, need to look through it.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/BlackflyCalibrationSuite.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/BlackflyCalibrationSuite.java
@@ -4,14 +4,17 @@ import imgui.ImGui;
 import imgui.type.ImDouble;
 import imgui.type.ImFloat;
 import imgui.type.ImInt;
+import org.bytedeco.javacv.JavaCV;
 import org.bytedeco.opencv.global.opencv_calib3d;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.global.opencv_imgproc;
 import org.bytedeco.opencv.opencv_core.*;
 import org.bytedeco.opencv.opencv_features2d.SimpleBlobDetector;
+import us.ihmc.commons.Conversions;
 import us.ihmc.commons.lists.RecyclingArrayList;
 import us.ihmc.commons.thread.Notification;
 import us.ihmc.commons.thread.ThreadTools;
+import us.ihmc.log.LogTools;
 import us.ihmc.perception.BytedecoTools;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
 import us.ihmc.rdx.imgui.ImGuiTools;
@@ -20,6 +23,7 @@ import us.ihmc.rdx.logging.HDF5ImageBrowser;
 import us.ihmc.rdx.logging.HDF5ImageLogging;
 import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.rdx.ui.graphics.ImGuiOpenCVSwapVideoPanelData;
+import us.ihmc.tools.UnitConversions;
 import us.ihmc.tools.thread.Activator;
 import us.ihmc.tools.thread.MissingThreadTools;
 import us.ihmc.tools.thread.ResettableExceptionHandlingExecutorService;
@@ -29,6 +33,17 @@ import java.util.function.Consumer;
 public class BlackflyCalibrationSuite
 {
    private static final String BLACKFLY_SERIAL_NUMBER = System.getProperty("blackfly.serial.number", "00000000");
+
+   // https://www.fujifilm.com/us/en/business/optical-devices/machine-vision-lens/fe185-series
+   private static final double FE185C086HA_1_FOCAL_LENGTH = 0.0027;
+   // https://www.flir.com/products/blackfly-usb3/?model=BFLY-U3-23S6C-C&vertical=machine+vision&segment=iis
+   private static final double BFLY_U3_23S6C_CMOS_SENSOR_FORMAT = UnitConversions.inchesToMeters(1.0 / 1.2);
+   private static final double BFLY_U3_23S6C_CMOS_SENSOR_WIDTH = 0.01067;
+   private static final double BFLY_U3_23S6C_CMOS_SENSOR_HEIGHT = 0.00800;
+   private static final double BFLY_U3_23S6C_WIDTH_PIXELS = 1920.0;
+   private static final double BFLY_U3_23S6C_HEIGHT_PIXELS = 1200.0;
+   private static final double FE185C086HA_1_FOCAL_LENGTH_IN_BFLY_U3_23S6C_PIXELS
+         = FE185C086HA_1_FOCAL_LENGTH * BFLY_U3_23S6C_WIDTH_PIXELS / BFLY_U3_23S6C_CMOS_SENSOR_WIDTH;
 
    private final Activator nativesLoadedActivator = BytedecoTools.loadOpenCVNativesOnAThread();
    private final RDXBaseUI baseUI = new RDXBaseUI(getClass(),
@@ -55,10 +70,12 @@ public class BlackflyCalibrationSuite
    private MatVector cornersOrCentersMatVector;
    private SimpleBlobDetector simpleBlobDetector;
    private final ImFloat patternDistanceBetweenPoints = new ImFloat();
-   private final ImDouble fxGuess = new ImDouble();
-   private final ImDouble fyGuess = new ImDouble();
-   private final ImDouble cxGuess = new ImDouble();
-   private final ImDouble cyGuess = new ImDouble();
+   private final ImDouble fxGuess = new ImDouble(FE185C086HA_1_FOCAL_LENGTH_IN_BFLY_U3_23S6C_PIXELS);
+   private final ImDouble fyGuess = new ImDouble(FE185C086HA_1_FOCAL_LENGTH_IN_BFLY_U3_23S6C_PIXELS);
+   private final ImDouble cxGuess = new ImDouble(BFLY_U3_23S6C_WIDTH_PIXELS / 2.0);
+   private final ImDouble cyGuess = new ImDouble(BFLY_U3_23S6C_HEIGHT_PIXELS / 2.0);
+   private final MatVector estimatedRotationVectors = new MatVector();
+   private final MatVector estimatedTranslationVectors = new MatVector();
 
    public BlackflyCalibrationSuite()
    {
@@ -191,7 +208,11 @@ public class BlackflyCalibrationSuite
          }
       }
 
-      ImGuiTools.volatileInputFloat(labels.get("Pattern distance between points"), patternDistanceBetweenPoints);
+      ImGui.inputFloat(labels.get("Pattern distance between points"), patternDistanceBetweenPoints);
+      ImGui.inputDouble(labels.get("Fx Guess (px)"), fxGuess);
+      ImGui.inputDouble(labels.get("Fy Guess (px)"), fyGuess);
+      ImGui.inputDouble(labels.get("Cx Guess (px)"), cxGuess);
+      ImGui.inputDouble(labels.get("Cy Guess (px)"), cyGuess);
 
       if (ImGui.button(labels.get("Calibrate")))
       {
@@ -265,6 +286,7 @@ public class BlackflyCalibrationSuite
       int patternHeight = calibrationPatternDetectionUI.getPatternHeight();
 
       Point3fVectorVector objectPoints = new Point3fVectorVector();
+      MatVector objectPointsMatVector = new MatVector();
       for (int i = 0; i < calibrationSourceImages.size(); i++)
       {
          Point3fVector pointsOnPattern = new Point3fVector();
@@ -276,6 +298,13 @@ public class BlackflyCalibrationSuite
             }
          }
          objectPoints.push_back(pointsOnPattern);
+         objectPointsMatVector.push_back(new Mat(pointsOnPattern)); // TODO: Does this work?
+      }
+
+      MatVector imagePointsMatVector = new MatVector();
+      for (int i = 0; i < imagePoints.size(); i++)
+      {
+         imagePointsMatVector.push_back(new Mat(imagePoints.get(i))); // TODO: Does this work?
       }
 
       Size imageSize = new Size(calibrationSourceImages.get(0).cols(), calibrationSourceImages.get(0).rows());
@@ -283,14 +312,35 @@ public class BlackflyCalibrationSuite
       Mat cameraMatrix = Mat.eye(3, 3, opencv_core.CV_64F).asMat();
       // Using fisheye::CALIB_USE_INTRINSIC_GUESS
       cameraMatrix.ptr(0, 0).putDouble(fxGuess.get());
-      cameraMatrix.ptr(1, 1).putDouble(fxGuess.get());
+      cameraMatrix.ptr(1, 1).putDouble(fyGuess.get());
       cameraMatrix.ptr(0, 2).putDouble(cxGuess.get());
       cameraMatrix.ptr(1, 2).putDouble(cyGuess.get());
 
+      Mat distortionCoefficients = Mat.zeros(4, 1, opencv_core.CV_64F).asMat();
+
+      estimatedRotationVectors.clear();
+      estimatedTranslationVectors.clear();
+
+      // TODO: We may want to fix the principal point and focal length
       int flags = opencv_calib3d.FISHEYE_CALIB_USE_INTRINSIC_GUESS;
 
+      TermCriteria terminationCriteria = new TermCriteria(TermCriteria.COUNT + TermCriteria.EPS, 100, JavaCV.DBL_EPSILON);
+
       // Here we use the cv::fisheye version
-      opencv_calib3d.calibrate(objectPoints, imagePoints, imageSize, cameraMatrix, distortionCoefficients, rvecs, tvecs, flags, criteria);
+      // https://docs.opencv.org/4.6.0/db/d58/group__calib3d__fisheye.html#gad626a78de2b1dae7489e152a5a5a89e1
+      opencv_calib3d.calibrate(objectPointsMatVector,
+                               imagePointsMatVector,
+                               imageSize,
+                               cameraMatrix,
+                               distortionCoefficients,
+                               estimatedRotationVectors,
+                               estimatedTranslationVectors,
+                               flags,
+                               terminationCriteria);
+
+      LogTools.info("Calibration complete!");
+      LogTools.info("# estimated rotation vectors: {}", estimatedRotationVectors.size());
+      LogTools.info("# estimated translation vectors: {}", estimatedTranslationVectors.size());
    }
 
    public static void main(String[] args)
