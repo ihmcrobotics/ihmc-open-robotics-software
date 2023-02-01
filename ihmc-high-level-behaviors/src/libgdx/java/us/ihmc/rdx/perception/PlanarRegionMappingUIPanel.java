f105ed45443891d66f0143406cac1505db0ea22e
Duncan Calvert <dcalvert@ihmc.us>
2023-02-01 15:44:59 -0600

    Remove extra newlines and reorder a field

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/PlanarRegionMappingUIPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/PlanarRegionMappingUIPanel.java
@@ -17,17 +17,13 @@ import us.ihmc.rdx.visualizers.RDXPlanarRegionsGraphic;
 
 public class PlanarRegionMappingUIPanel
 {
-   private boolean captured = false;
-
    private ImGuiStoredPropertySetTuner mappingParametersTuner;
    private PlanarRegionMappingManager mappingManager;
    private ImGuiPanel imGuiPanel;
    private final ImBoolean liveModeEnabled = new ImBoolean();
    private final ImBoolean renderEnabled = new ImBoolean(true);
-
-
-
-
+   private boolean captured = false;
+  
    public PlanarRegionMappingUIPanel(String name, PlanarRegionMappingManager mappingManager)
    {
       imGuiPanel = new ImGuiPanel(name, this::renderImGuiWidgets);
@@ -36,8 +32,6 @@ public class PlanarRegionMappingUIPanel
       mappingParametersTuner = new ImGuiStoredPropertySetTuner(mappingManager.getParameters().getTitle());
       mappingParametersTuner.create(mappingManager.getParameters());
       imGuiPanel.addChild(mappingParametersTuner);
-
-
    }
 
    public void renderImGuiWidgets()
