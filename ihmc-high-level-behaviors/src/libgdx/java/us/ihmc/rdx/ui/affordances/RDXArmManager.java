aff322f20e93cef2e89e27eacb764c8cada8f899
joh <joh@ihmc.org>
2023-02-02 15:34:30 -0600

    take alphaFilteredYoSpatialVector as well to update from filtered wrench

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXArmManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXArmManager.java
@@ -95,8 +95,7 @@ public class RDXArmManager
          armManagers.get(side).update(interactableHands.get(side), desiredRobot);
 
          // wrench expressed in wrist pitch body fixed-frame
-         interactableHands.get(side).updateEstimatedWrench(handWrenchCalculator.getFilteredWrench().get(side).getLinearPart(),
-                                                           handWrenchCalculator.getFilteredWrench().get(side).getAngularPart());
+         interactableHands.get(side).updateEstimatedWrench(handWrenchCalculator.getFilteredWrench().get(side));
 
          // We only want to evaluate this when we are going to take action on it
          // Otherwise, we will not notice the desired changed while the solver was still solving
