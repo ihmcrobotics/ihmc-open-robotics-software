e44bbc773b194b9b6d72ab402c151cbaa0a71e1f
Luigi Penco <lpenco@ihmc.org>
2023-02-03 11:03:45 -0600

    imrpoved significantly performance, added decimator in case too many vertices are added to the mesh

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/ui/graphics/RDXSplineDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/ui/graphics/RDXSplineDemo.java
@@ -3,7 +3,6 @@ package us.ihmc.rdx.ui.graphics;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.log.LogTools;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
 import us.ihmc.rdx.tools.RDXModelBuilder;
 import us.ihmc.rdx.ui.RDXBaseUI;
@@ -11,9 +10,7 @@ import us.ihmc.rdx.visualizers.RDXSplineGraphic;
 
 public class RDXSplineDemo
 {
-   private final RDXBaseUI baseUI = new RDXBaseUI(getClass(),
-                                                  "ihmc-open-robotics-software",
-                                                  "ihmc-high-level-behaviors/src/test/resources");
+   private final RDXBaseUI baseUI = new RDXBaseUI(getClass(), "ihmc-open-robotics-software", "ihmc-high-level-behaviors/src/test/resources");
    private RDXSplineGraphic spline;
    private double startTime;
 
@@ -28,7 +25,7 @@ public class RDXSplineDemo
             baseUI.getPrimaryScene().addModelInstance(new ModelInstance(RDXModelBuilder.createCoordinateFrame(0.3)));
 
             spline = new RDXSplineGraphic();
-            spline.createStart(new Point3D(0.1,0.1,0.1), Color.BLUE);
+            spline.createStart(new Point3D(0.1, 0.1, 0.1), Color.BLUE);
 
             baseUI.getPrimaryScene().addRenderableProvider(spline);
             startTime = System.currentTimeMillis();
@@ -38,15 +35,17 @@ public class RDXSplineDemo
          public void render()
          {
             double time = System.currentTimeMillis();
-            if((time-startTime)/1000<5)
-               spline.createAdditionalPoint(new Point3D(0.1 + Math.sin(5*(time-startTime)/1000),0.1 + (time-startTime)/10000, 0.1 + (time-startTime)/10000), Color.YELLOW);
-            else if ((time-startTime)/1000<6)
+            if ((time - startTime) / 1000 < 5)
+               spline.createAdditionalPoint(new Point3D(0.1 + Math.sin(5 * (time - startTime) / 1000),
+                                                        0.1 + (time - startTime) / 10000,
+                                                        0.1 + (time - startTime) / 10000), Color.YELLOW);
+            else if ((time - startTime) / 1000 < 6)
                spline.createEnd(Color.BLUE);
-            else if ((time-startTime)/1000<6.5)
+            else if ((time - startTime) / 1000 < 6.5)
             {
                spline.clear();
-               spline.createStart(new Point3D(0.1,0.1,0.1), Color.BLUE);
-               startTime =  System.currentTimeMillis();
+               spline.createStart(new Point3D(0.1, 0.1, 0.1), Color.BLUE);
+               startTime = System.currentTimeMillis();
             }
 
             baseUI.renderBeforeOnScreenUI();
