720dc8aab30f44ec5ba743060c5572892d73316d
Duncan Calvert <dcalvert@ihmc.us>
2023-02-03 18:01:34 -0600

    Extract path size for string constant.

--- ihmc-graphics/src/libgdx/java/us/ihmc/rdx/imgui/ImGuiTools.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/rdx/imgui/ImGuiTools.java
@@ -37,6 +37,7 @@ public class ImGuiTools
    public static final float FLOAT_MAX = 3.40282346638528859811704183484516925e+38F / 2.0f;
    /** This is used so a scroll area can end reasonably for long scrolls so stuff below it can be accessed. */
    public static final float REASONABLE_HEIGHT_FOR_A_SCROLL_AREA = 150.0f;
+   public static final int MAX_STRING_SIZE_FOR_PATH = 1024;
 
    private static ImFont consoleFont;
    private static ImFont smallFont;
