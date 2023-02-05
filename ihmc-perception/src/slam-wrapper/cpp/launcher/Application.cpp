9575e14bbd3b6d9a3873f0cea1a5a757960cb32f
BhavyanshM <bhavyanshmishra@gmail.com>
2023-02-05 02:06:07 -0600

    Change C++ indentation to be 4 spaces.

--- ihmc-perception/src/slam-wrapper/cpp/launcher/Application.cpp
+++ ihmc-perception/src/slam-wrapper/cpp/launcher/Application.cpp
@@ -3,45 +3,45 @@
 
 void SLAMTest(FactorGraphHandler& factorGraphHandler)
 {
-   using namespace gtsam;
+    using namespace gtsam;
 
-   Pose3 initPose(Rot3::Ypr(0.0, 0.0, 0.0), Point3(0.0, 0.0, 0.0));
-   factorGraphHandler.addPriorPoseFactor(1, initPose);
+    Pose3 initPose(Rot3::Ypr(0.0, 0.0, 0.0), Point3(0.0, 0.0, 0.0));
+    factorGraphHandler.addPriorPoseFactor(1, initPose);
 
-   Pose3 odometry(Rot3::Ypr(0.0, 0.0, 0.0), Point3(1.0, 0.0, 0.0));
-   factorGraphHandler.addOdometryFactor(odometry, 2);
+    Pose3 odometry(Rot3::Ypr(0.0, 0.0, 0.0), Point3(1.0, 0.0, 0.0));
+    factorGraphHandler.addOdometryFactor(odometry, 2);
 
-   factorGraphHandler.setPoseInitialValue(1, initPose);
-   factorGraphHandler.setPoseInitialValue(2, odometry);
+    factorGraphHandler.setPoseInitialValue(1, initPose);
+    factorGraphHandler.setPoseInitialValue(2, odometry);
 
-   factorGraphHandler.optimizeISAM2(3);
+    factorGraphHandler.optimizeISAM2(3);
 
-   factorGraphHandler.getResults().print();
+    factorGraphHandler.getResults().print();
 }
 
 void ExternalSLAMTest(FactorGraphExternal& factorGraphExternal)
 {
-   using namespace gtsam;
+    using namespace gtsam;
 
-   float initialPoseValue[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-   factorGraphExternal.addPriorPoseFactor(1, initialPoseValue);
+    float initialPoseValue[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    factorGraphExternal.addPriorPoseFactor(1, initialPoseValue);
 
-   float odometryValue[] = {0.0, 0.0, 0.0, 1.0, 0.0, 0.0};
-   factorGraphExternal.addOdometryFactor(2, odometryValue);
+    float odometryValue[] = {0.0, 0.0, 0.0, 1.0, 0.0, 0.0};
+    factorGraphExternal.addOdometryFactor(2, odometryValue);
 
-   factorGraphExternal.setPoseInitialValue(1, initialPoseValue);
-   factorGraphExternal.setPoseInitialValue(2, odometryValue);
+    factorGraphExternal.setPoseInitialValue(1, initialPoseValue);
+    factorGraphExternal.setPoseInitialValue(2, odometryValue);
 
-   factorGraphExternal.optimizeISAM2(3);
+    factorGraphExternal.optimizeISAM2(3);
 
-   factorGraphExternal.printResults();
+    factorGraphExternal.printResults();
 }
 
 int main()
 {
-   FactorGraphExternal factorGraphExternal;
+    FactorGraphExternal factorGraphExternal;
 
-   ExternalSLAMTest(factorGraphExternal);
+    ExternalSLAMTest(factorGraphExternal);
 
-   printf("Welcome To Factor Graphs in Java World!\n");
+    printf("Welcome To Factor Graphs in Java World!\n");
 }
