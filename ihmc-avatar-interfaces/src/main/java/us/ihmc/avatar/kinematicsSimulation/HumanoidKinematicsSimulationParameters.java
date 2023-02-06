a376049632218ab86c87955634ce5c27ad653742
joh <joh@ihmc.org>
2023-02-06 13:35:53 -0600

    testing on sim. Can't make it plan more than 1 steps

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/kinematicsSimulation/HumanoidKinematicsSimulationParameters.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/kinematicsSimulation/HumanoidKinematicsSimulationParameters.java
@@ -15,7 +15,7 @@ public class HumanoidKinematicsSimulationParameters
    private double playbackSpeedMultiplier = 10.0;
    private double dt = UnitConversions.hertzToSeconds(70);
    private boolean runNoFasterThanMaxRealtimeRate = true;
-   private double maxRealtimeRate = 2.0;
+   private double maxRealtimeRate = 1.0;
 
    public double getInitialGroundHeight()
    {
