b2df868a3b1f30eb94d002cfa57bdbc4335da780
Luigi Penco <lpenco@ihmc.org>
2023-02-06 12:46:33 -0600

    added virtual objects from marker

--- ihmc-perception/src/main/java/us/ihmc/perception/objects/ArUcoMarkerObject.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/objects/ArUcoMarkerObject.java
@@ -12,14 +12,22 @@ public class ArUcoMarkerObject
    private final ReferenceFrame markerFrame = ReferenceFrameMissingTools.constructFrameWithChangingTransformToParent(ReferenceFrame.getWorldFrame(),
                                                                                                                      markerToWorld);
    private final RigidBodyTransform objectToMarker = new RigidBodyTransform();
-   // object frame might be different than marker location, for example the door handle is not exactly where the marker is on the door
+   // object frame might be different from marker location, for example the door handle is not exactly where the marker is on the door
    private ReferenceFrame objectFrame;
    private FramePose3D objectPose = new FramePose3D();
+   private final RigidBodyTransform appendixToObject = new RigidBodyTransform();
+   private FramePose3D appendixPose = new FramePose3D();
+   private boolean hasAppendix =  false;
 
    public ArUcoMarkerObject(int id, ObjectInfo arucoInfo)
    {
       objectToMarker.set(arucoInfo.getMarkerYawPitchRoll(id), arucoInfo.getMarkerTranslation(id));
       objectFrame = ReferenceFrameMissingTools.constructFrameWithUnchangingTransformToParent(markerFrame, objectToMarker);
+      if (arucoInfo.hasAppendix(id))
+      {
+         hasAppendix = true;
+         appendixToObject.set(arucoInfo.getAppendixYawPitchRoll(id), arucoInfo.getAppendixTranslation(id));
+      }
    }
 
    public void updateFrame()
@@ -32,6 +40,11 @@ public class ArUcoMarkerObject
    {
       objectPose.set(markerPose);
       objectPose.appendTransform(objectToMarker);
+      if (hasAppendix)
+      {
+         appendixPose.set(objectPose);
+         appendixPose.appendTransform(appendixToObject);
+      }
    }
 
    public RigidBodyTransform getMarkerToWorld()
@@ -52,5 +65,10 @@ public class ArUcoMarkerObject
    public FramePose3D getObjectPose()
    {
       return objectPose;
+   }
+
+   public FramePose3D getAppendixPose()
+   {
+      return appendixPose;
    }
 }
