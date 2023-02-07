4af94b3fc15e42943f49b203f82c8f4323777d56
Luigi Penco <lpenco@ihmc.org>
2023-02-07 11:39:01 -0600

    adding templates json

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/tools/affordanceTemplates/AffordanceTemplateAction.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/tools/affordanceTemplates/AffordanceTemplateAction.java
@@ -1,8 +1,12 @@
 package us.ihmc.behaviors.tools.affordanceTemplates;
 
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+
 public interface AffordanceTemplateAction
 {
-   // load JSON
+   void loadTemplate(String affordance); // load from JSON
+
+   void framePoseToPack(FramePose3D framePose);
 
    // save JSON
 }
