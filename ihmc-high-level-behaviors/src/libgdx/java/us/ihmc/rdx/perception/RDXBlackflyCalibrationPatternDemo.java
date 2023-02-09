290c57d3a27f8aa24db00fe75d0f9d9f0d32b8c2
Duncan Calvert <dcalvert@ihmc.us>
2023-02-09 13:42:18 -0600

    Rename RDXOpenCVSwapVideoPanelData to RDXImagePanelTexture.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXBlackflyCalibrationPatternDemo.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXBlackflyCalibrationPatternDemo.java
@@ -4,7 +4,7 @@ import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.perception.BytedecoTools;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
 import us.ihmc.rdx.ui.RDXBaseUI;
-import us.ihmc.rdx.ui.graphics.RDXOpenCVSwapVideoPanelData;
+import us.ihmc.rdx.ui.graphics.RDXImagePanelTexture;
 import us.ihmc.tools.thread.Activator;
 
 /**
@@ -70,9 +70,9 @@ public class RDXBlackflyCalibrationPatternDemo
             baseUI.renderEnd();
          }
 
-         private void monitorUIThreadPreprocessor(RDXOpenCVSwapVideoPanelData data)
+         private void monitorUIThreadPreprocessor(RDXImagePanelTexture texture)
          {
-            calibrationPatternDetectionUI.drawCornersOrCenters(data.getRGBA8Mat());
+            calibrationPatternDetectionUI.drawCornersOrCenters(texture.getRGBA8Mat());
          }
 
          @Override
--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/RDXImagePanelTexture.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/RDXImagePanelTexture.java
@@ -13,7 +13,7 @@ import us.ihmc.perception.BytedecoImage;
  * two sets of pixmaps, textures, and images where each set shares some
  * native memory, that gets copied to the GPU in the UI update method.
  */
-public class RDXOpenCVSwapVideoPanelData
+public class RDXImagePanelTexture
 {
    private Pixmap pixmap;
    private boolean needNewTexture = false;
