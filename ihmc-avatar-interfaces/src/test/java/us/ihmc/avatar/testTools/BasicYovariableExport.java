40509781539873678f7baea6e6908381dc28a454
Evan <eyu@ihmc.org>
2023-02-17 16:41:48 -0600

    moved yovar exporter to nadia

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/testTools/BasicYovariableExport.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/testTools/BasicYovariableExport.java
@@ -3,35 +3,75 @@ package us.ihmc.avatar.testTools;
 import us.ihmc.scs2.SimulationConstructionSet2;
 import us.ihmc.scs2.session.SessionDataExportRequest;
 import us.ihmc.scs2.sharedMemory.tools.SharedMemoryIOTools;
+import us.ihmc.yoVariables.registry.YoNamespace;
+import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoVariable;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class BasicYovariableExport
 {
    private SimulationConstructionSet2 scs2;
-   private final ArrayList<String> variableNamesToExport = new ArrayList<>();
+   private final ArrayList<String> individualVariableNamesToExport = new ArrayList<>();
+   private final List<String> registryNamesToExport = new ArrayList<>();
+
+   private final List<String> registryVariableNamesToExport = new ArrayList<>();
+   private YoRegistry parentRegistry;
 
    // assume scs2 has been cropped already
    public BasicYovariableExport() // and list of yovariables
    {
-
    }
 
+   // Must be called first
    public void setSCSToExport(SimulationConstructionSet2 scs2)
    {
       this.scs2 = scs2;
+      this.parentRegistry = scs2.getRootRegistry();
    }
 
    public void addYoVariableNameToExport(String name)
    {
-      variableNamesToExport.add(name);
+      individualVariableNamesToExport.add(name);
+   }
+
+   public void addYoRegistryNameToExport(String registryName)
+   {
+      if (!registryNamesToExport.contains(registryName))
+      {
+         registryNamesToExport.add(registryName);
+      }
+   }
+
+   private void compileRegistryYoVariableNames()
+   {
+      registryVariableNamesToExport.clear();
+      for (String registryName : registryNamesToExport)
+      {
+         System.out.println("Exporting: " + registryName);
+         YoRegistry registry = parentRegistry.findRegistry(new YoNamespace(registryName));
+         List<YoVariable> yoList = registry.collectSubtreeVariables();
+         for (YoVariable var : yoList)
+         {
+            if (!registryVariableNamesToExport.contains(var.getName()))
+               registryVariableNamesToExport.add(var.getFullNameString());
+         }
+      }
    }
 
    public void export(File file)
    {
       SessionDataExportRequest request = new SessionDataExportRequest();
-      request.setVariableFilter(v -> variableNamesToExport.contains(v.getName()));
+      compileRegistryYoVariableNames();
+      request.setVariableFilter(v -> (registryVariableNamesToExport.contains(v.getFullNameString()) ||
+                                      individualVariableNamesToExport.contains(v.getName()))
+                              );
+//      request.setRegistryFilter(r -> {
+////         return ((registryNamesToExport.contains(r.getName())) &&
+////          (parentRegistry.getRoot().findRegistry(new YoNamespace(r.getName())) != null));
+//      });
       request.setFile(file);
       request.setExportRobotDefinitions(false);
       request.setExportTerrainObjectDefinitions(false);
