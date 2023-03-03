a289ec29e473eb1a4dc85ec5981e7f5b80362928
Duncan Calvert <dcalvert@ihmc.us>
2023-03-03 14:06:26 -0600

    Revert ":arrow_up: imgui-java 1.86.9"
    This reverts commit d805175880dd5cfdfca6bb0bf8f46e7952ca95de.

--- ihmc-graphics/build.gradle.kts
+++ ihmc-graphics/build.gradle.kts
@@ -67,7 +67,7 @@ libgdxDependencies {
    api("org.lwjgl:lwjgl-assimp:$lwjglVersion:natives-windows-x86")
    api("org.lwjgl:lwjgl-assimp:$lwjglVersion:natives-macos")
 
-   val imguiVersion = "1.86.9"
+   val imguiVersion = "1.86.4"
    api("io.github.spair:imgui-java-binding:$imguiVersion")
    api("io.github.spair:imgui-java-lwjgl3:$imguiVersion")
    api("io.github.spair:imgui-java-natives-linux-ft:$imguiVersion")
