799b616b30f558df4c4954e1752fabf1a6b43a5b
Duncan Calvert <dcalvert@ihmc.us>
2023-03-03 15:02:54 -0600

    Apply tuned Ouster to Fisheye transform.

--- ihmc-perception/src/main/java/us/ihmc/perception/sensorHead/SensorHeadParameters.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/sensorHead/SensorHeadParameters.java
@@ -1,11 +1,8 @@
 package us.ihmc.perception.sensorHead;
 
-import us.ihmc.euclid.matrix.RotationMatrix;
-import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.perception.spinnaker.SpinnakerBlackfly;
-import us.ihmc.robotics.referenceFrames.ReferenceFrameMissingTools;
+import us.ihmc.robotics.EuclidCoreMissingTools;
 
 /**
  * These parameters are specific to the sensor head setup we've designed for Nadia.
@@ -32,25 +29,7 @@ public class SensorHeadParameters
    public static final RigidBodyTransform OUSTER_TO_FISHEYE_TRANSFORM = new RigidBodyTransform();
    static
    {
-      // For the benchtop sensorhead setup
-      FramePose3D ousterPose = new FramePose3D();
-      ousterPose.getPosition().set(0.225, 0.004, 0.459);
-      RotationMatrix rotationMatrix = new RotationMatrix();
-      rotationMatrix.setAndNormalize( 0.779, -0.155,  0.607,
-                                      0.189,  0.982,  0.009,
-                                     -0.598,  0.108,  0.794);
-      ousterPose.getOrientation().set(rotationMatrix);
-      ousterPose.getOrientation().appendPitchRotation(Math.toRadians(-2));
-
-      RigidBodyTransform transformChestToBlackflyFujinon = new RigidBodyTransform();
-      transformChestToBlackflyFujinon.setIdentity();
-      transformChestToBlackflyFujinon.getTranslation().set(0.160, -0.095, 0.419);
-      transformChestToBlackflyFujinon.getRotation().setAndNormalize( 0.986, -0.000, 0.167, 0.000, 1.000, -0.000, -0.167, 0.000, 0.986);
-      ReferenceFrame blackflyFrame
-            = ReferenceFrameMissingTools.constructFrameWithUnchangingTransformToParent(ReferenceFrame.getWorldFrame(),
-                                                                                       transformChestToBlackflyFujinon);
-
-      ousterPose.changeFrame(blackflyFrame);
-      ousterPose.get(OUSTER_TO_FISHEYE_TRANSFORM);
+      OUSTER_TO_FISHEYE_TRANSFORM.getTranslation().set(0.023,  0.078,  0.076);
+      EuclidCoreMissingTools.setYawPitchRollDegrees(OUSTER_TO_FISHEYE_TRANSFORM.getRotation(), 13.007, 24.205, 6.461);
    }
 }
