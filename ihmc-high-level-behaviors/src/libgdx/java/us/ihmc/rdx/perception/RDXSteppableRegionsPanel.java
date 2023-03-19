7f40173db40c622f3ec49d7af01d9b993dce7627
Robert <robert.griffin@boardwalkrobotics.com>
2023-03-19 14:06:04 -0500

    cleaned up the way the different steppability panels were rendered

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXSteppableRegionsPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXSteppableRegionsPanel.java
@@ -2,13 +2,13 @@ package us.ihmc.rdx.perception;
 
 import imgui.internal.ImGui;
 import imgui.type.ImBoolean;
+import imgui.type.ImInt;
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.opencv.opencv_core.Mat;
 import perception_msgs.msg.dds.SteppableRegionDebugImageMessage;
 import perception_msgs.msg.dds.SteppableRegionDebugImagesMessage;
 import us.ihmc.communication.ros2.ROS2Helper;
 import us.ihmc.ihmcPerception.steppableRegions.SteppableRegionsAPI;
-import us.ihmc.ihmcPerception.steppableRegions.SteppableRegionsCalculationModule;
 import us.ihmc.perception.OpenCLManager;
 import us.ihmc.perception.steppableRegions.SteppableRegionCalculatorParameters;
 import us.ihmc.perception.steppableRegions.SteppableRegionCalculatorParametersReadOnly;
@@ -16,8 +16,6 @@ import us.ihmc.rdx.imgui.ImGuiPanel;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.ui.ImGuiRemoteROS2StoredPropertySetGroup;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class RDXSteppableRegionsPanel
@@ -27,13 +25,18 @@ public class RDXSteppableRegionsPanel
 
    private final OpenCLManager openCLManager = new OpenCLManager();
 
+   private final AtomicReference<SteppableRegionDebugImagesMessage> incomingSteppableRegionDebugImagesToRender = new AtomicReference<>();
+
    private final AtomicReference<SteppableRegionDebugImagesMessage> latestSteppableRegionDebugImagesToRender = new AtomicReference<>();
+   private int latestIndexToRender = 0;
+   private boolean shouldUpdateRender = false;
 
    private final ImBoolean drawPatches = new ImBoolean(true);
+   private final ImInt yawIndexToRender = new ImInt(0);
 
    private ImGuiPanel imguiPanel;
-   private final List<RDXMatImagePanel> steppabilityPanels = new ArrayList<>();
-   private final List<RDXMatImagePanel> steppableRegionsPanels = new ArrayList<>();
+   private RDXMatImagePanel steppabilityPanel;
+   private RDXMatImagePanel steppableRegionsPanel;
 
    private final ImGuiRemoteROS2StoredPropertySetGroup remotePropertySets;
    private int cellsPerSide;
@@ -51,35 +54,33 @@ public class RDXSteppableRegionsPanel
       imguiPanel = new ImGuiPanel("Steppable Region Extraction", this::renderImGuiWidgetsPanel);
 
       cellsPerSide = 100;
-      for (int i = 0; i < parameters.getYawDiscretizations(); i++)
-      {
-         RDXMatImagePanel steppabilityPanel = new RDXMatImagePanel("Raw Steppability " + i, cellsPerSide, cellsPerSide, false);
-         RDXMatImagePanel panel = new RDXMatImagePanel("Steppable Regions " + i, cellsPerSide, cellsPerSide, false);
-         steppabilityPanels.add(steppabilityPanel);
-         steppableRegionsPanels.add(panel);
-         imguiPanel.addChild(steppabilityPanel.getImagePanel());
-         imguiPanel.addChild(panel.getImagePanel());
-      }
+      steppabilityPanel = new RDXMatImagePanel("Raw Steppability ", cellsPerSide, cellsPerSide, false);
+      steppableRegionsPanel = new RDXMatImagePanel("Steppable Regions ", cellsPerSide, cellsPerSide, false);
+      imguiPanel.addChild(steppabilityPanel.getImagePanel());
+      imguiPanel.addChild(steppableRegionsPanel.getImagePanel());
    }
 
    volatile boolean needToDraw = false;
 
    public void setLatestSteppableRegionDebugImagesToRender(SteppableRegionDebugImagesMessage steppableRegionDebugImagesToRender)
    {
-      this.latestSteppableRegionDebugImagesToRender.set(steppableRegionDebugImagesToRender);
+      this.incomingSteppableRegionDebugImagesToRender.set(steppableRegionDebugImagesToRender);
    }
 
    public void update()
    {
       if (renderRegions.get())
       {
-         drawDebugRegions();
+         updateValuesToRender();
+
+         if (shouldUpdateRender)
+         {
+            drawDebugRegions();
+         }
 
          if (needToDraw)
          {
             draw2DPanels();
-
-            needToDraw = false;
          }
       }
    }
@@ -89,78 +90,89 @@ public class RDXSteppableRegionsPanel
       if (newSize != cellsPerSide)
       {
          cellsPerSide = newSize;
-         for (int i = 0; i < parameters.getYawDiscretizations(); i++)
-         {
-            steppableRegionsPanels.get(i).resize(newSize, newSize);
-            steppabilityPanels.get(i).resize(newSize, newSize);
-         }
+         steppableRegionsPanel.resize(newSize, newSize);
+         steppabilityPanel.resize(newSize, newSize);
       }
    }
 
+
+   private void updateValuesToRender()
+   {
+      SteppableRegionDebugImagesMessage debugImagesMessage = incomingSteppableRegionDebugImagesToRender.getAndSet(null);
+      if (debugImagesMessage != null)
+      {
+         shouldUpdateRender = true;
+         latestSteppableRegionDebugImagesToRender.set(debugImagesMessage);
+      }
+
+      if (latestIndexToRender != yawIndexToRender.get())
+      {
+         shouldUpdateRender = true;
+         latestIndexToRender = yawIndexToRender.get();
+      }
+   }
    private void drawDebugRegions()
    {
-      SteppableRegionDebugImagesMessage debugImagesMessage = latestSteppableRegionDebugImagesToRender.getAndSet(null);
+      needToDraw = false;
+      SteppableRegionDebugImagesMessage debugImagesMessage = latestSteppableRegionDebugImagesToRender.get();
       if (debugImagesMessage == null)
          return;
 
       resize(debugImagesMessage.getRegionImages().get(0).getImageWidth());
+      int yawToDraw = latestIndexToRender;
 
-      for (int yaw = 0; yaw < parameters.getYawDiscretizations(); yaw++)
+      if (steppableRegionsPanel.getImagePanel().getIsShowing().get() && drawPatches.get())
       {
-         RDXMatImagePanel panel = steppableRegionsPanels.get(yaw);
-         if (panel.getImagePanel().getIsShowing().get() && drawPatches.get())
+         needToDraw = true;
+         Mat image = steppableRegionsPanel.getImage();
+
+         SteppableRegionDebugImageMessage regionImage = debugImagesMessage.getRegionImages().get(yawToDraw);
+
+         if (image.arrayHeight() != regionImage.getImageHeight() || image.arrayWidth() != regionImage.getImageWidth())
+            throw new RuntimeException("Sizes don't match");
+
+         for (int row = 0; row < regionImage.getImageHeight(); row++)
          {
-            Mat image = panel.getImage();
-
-            SteppableRegionDebugImageMessage regionImage = debugImagesMessage.getRegionImages().get(yaw);
-
-            if (image.arrayHeight() != regionImage.getImageHeight() || image.arrayWidth() != regionImage.getImageWidth())
-               throw new RuntimeException("Sizes don't match");
-
-            for (int row = 0; row < regionImage.getImageHeight(); row++)
+            for (int col = 0; col < regionImage.getImageWidth(); col++)
             {
-               for (int col = 0; col < regionImage.getImageWidth(); col++)
+               BytePointer pointer = image.ptr(row, col);
+               for (int j = 0; j < 3; j++)
                {
-                  BytePointer pointer = image.ptr(row, col);
-                  for (int j = 0; j < 3; j++)
-                  {
-                     int index = row * regionImage.getImageWidth() + col;
-                     int start = 3 * index;
-                     pointer.put(j, regionImage.getData().get(start + j));
-                  }
-               }
-            }
-         }
-
-         panel = steppabilityPanels.get(yaw);
-         if (panel.getImagePanel().getIsShowing().get() && drawPatches.get())
-         {
-            Mat image = panel.getImage();
-            SteppableRegionDebugImageMessage steppabilityImage = debugImagesMessage.getSteppabilityImages().get(yaw);
-
-            for (int row = 0; row < steppabilityImage.getImageHeight(); row++)
-            {
-               for (int col = 0; col < steppabilityImage.getImageWidth(); col++)
-               {
-                  BytePointer pointer = image.ptr(row, col);
-                  for (int j = 0; j < 3; j++)
-                  {
-                     int index = row * steppabilityImage.getImageWidth() + col;
-                     int start = 3 * index;
-                     pointer.put(j, steppabilityImage.getData().get(start + j));
-                  }
+                  int index = row * regionImage.getImageWidth() + col;
+                  int start = 3 * index;
+                  pointer.put(j, regionImage.getData().get(start + j));
                }
             }
          }
       }
 
-      needToDraw = true;
+      if (steppabilityPanel.getImagePanel().getIsShowing().get() && drawPatches.get())
+      {
+         needToDraw = true;
+
+         Mat image = steppabilityPanel.getImage();
+         SteppableRegionDebugImageMessage steppabilityImage = debugImagesMessage.getSteppabilityImages().get(yawToDraw);
+
+         for (int row = 0; row < steppabilityImage.getImageHeight(); row++)
+         {
+            for (int col = 0; col < steppabilityImage.getImageWidth(); col++)
+            {
+               BytePointer pointer = image.ptr(row, col);
+               for (int j = 0; j < 3; j++)
+               {
+                  int index = row * steppabilityImage.getImageWidth() + col;
+                  int start = 3 * index;
+                  pointer.put(j, steppabilityImage.getData().get(start + j));
+               }
+            }
+         }
+      }
    }
 
    private void draw2DPanels()
    {
-      steppableRegionsPanels.forEach(RDXMatImagePanel::display);
-      steppabilityPanels.forEach(RDXMatImagePanel::display);
+      steppableRegionsPanel.display();
+      steppabilityPanel.display();
    }
 
    private void renderImGuiWidgetsPanel()
@@ -171,6 +183,8 @@ public class RDXSteppableRegionsPanel
       ImGui.text("Input height map dimensions: " + cellsPerSide + " x " + cellsPerSide);
 
       ImGui.checkbox(labels.get("Render regions"), renderRegions);
+
+      ImGui.sliderInt("Yaw index to render", yawIndexToRender.getData(), 0, parameters.getYawDiscretizations() - 1);
    }
 
    public void renderImGuiWidgets()
