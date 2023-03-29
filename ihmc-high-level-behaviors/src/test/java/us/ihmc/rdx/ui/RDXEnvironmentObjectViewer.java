bdc5f1eb5504a919ce6732cd786e96b8cb42873d
Nadia Operator <ocu@ihmc.org>
2023-03-29 17:01:19 -0500

    Add environment object view for making sure model origins are correct.

---
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/ui/RDXEnvironmentObjectViewer.java
@@ -0,0 +1,46 @@
+package us.ihmc.rdx.ui;
+
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
+import us.ihmc.rdx.sceneManager.RDXSceneLevel;
+import us.ihmc.rdx.tools.RDXModelLoader;
+
+public class RDXEnvironmentObjectViewer
+{
+   private final RDXBaseUI baseUI = new RDXBaseUI();
+
+   public RDXEnvironmentObjectViewer()
+   {
+      baseUI.launchRDXApplication(new Lwjgl3ApplicationAdapter()
+      {
+         @Override
+         public void create()
+         {
+            baseUI.create();
+            baseUI.getPrimaryScene().getSceneLevelsToRender().add(RDXSceneLevel.GROUND_TRUTH);
+
+            Model model = RDXModelLoader.load("environmentObjects/ousterSensor/Ouster.g3dj");
+            baseUI.getPrimaryScene().addModelInstance(new ModelInstance(model));
+         }
+
+         @Override
+         public void render()
+         {
+            baseUI.renderBeforeOnScreenUI();
+            baseUI.renderEnd();
+         }
+
+         @Override
+         public void dispose()
+         {
+            baseUI.dispose();
+         }
+      });
+   }
+
+   public static void main(String[] args)
+   {
+      new RDXEnvironmentObjectViewer();
+   }
+}
