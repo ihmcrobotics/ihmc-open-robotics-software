ee8715fe1d749283d1a47c08b838a0ec162875ed
rgriffin@ihmc.org <rgriffin@ihmc.us>
2023-03-30 11:27:53 -0500

    added an additional function to the cop objective calculator

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/wrenchDistribution/CoPObjectiveCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/wrenchDistribution/CoPObjectiveCalculator.java
@@ -38,4 +38,31 @@ public class CoPObjectiveCalculator
 
       objectiveToPack.zero();
    }
+
+   public void computeTask(DMatrixRMaj wrenchJacobianInPlaneFrame,
+                           FramePoint2DReadOnly desiredCoPInPlaneFrame,
+                           int rhoSize,
+                           DMatrixRMaj jacobianToPack,
+                           DMatrixRMaj objectiveToPack)
+   {
+      fzRow.reshape(1, rhoSize);
+      singleCopRow.reshape(1, rhoSize);
+
+      int fzIndex = 5;
+      CommonOps_DDRM.extractRow(wrenchJacobianInPlaneFrame, fzIndex, fzRow);
+
+      // [x_cop * J_fz + J_ty] * rho == 0
+      int tauYIndex = 1;
+      CommonOps_DDRM.extractRow(wrenchJacobianInPlaneFrame, tauYIndex, singleCopRow);
+      CommonOps_DDRM.add(desiredCoPInPlaneFrame.getX(), fzRow, 1.0, singleCopRow, singleCopRow);
+      CommonOps_DDRM.insert(singleCopRow, jacobianToPack, 0, 0);
+
+      // [y_cop * J_fz - J_tx] * rho == 0
+      int tauXIndex = 0;
+      CommonOps_DDRM.extractRow(wrenchJacobianInPlaneFrame, tauXIndex, singleCopRow);
+      CommonOps_DDRM.add(desiredCoPInPlaneFrame.getY(), fzRow, -1.0, singleCopRow, singleCopRow);
+      CommonOps_DDRM.insert(singleCopRow, jacobianToPack, 1, 0);
+
+      objectiveToPack.zero();
+   }
 }
