7ae24b9dad712a3f696fc0afd11546ed2d5f0954
Luigi Penco <lpenco@ihmc.org>
2023-03-30 18:20:49 -0500

    unused imports

--- ihmc-perception/src/main/java/us/ihmc/perception/objects/DetectedObjectPublisher.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/objects/DetectedObjectPublisher.java
@@ -4,8 +4,6 @@ import perception_msgs.msg.dds.DetectedObjectMessage;
 import us.ihmc.communication.packets.MessageTools;
 import us.ihmc.communication.ros2.ROS2PublishSubscribeAPI;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.transform.RigidBodyTransform;
-import us.ihmc.log.LogTools;
 import us.ihmc.ros2.ROS2Topic;
 
 public class DetectedObjectPublisher
