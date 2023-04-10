9b061b00d22ea6e9e25f74f6bd63ff38b8584f6d
Duncan Calvert <dcalvert@ihmc.us>
2023-04-10 18:09:51 -0500

    Building out more scene object architecture.

---
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/scene/RDXSceneObject.java
@@ -0,0 +1,11 @@
+package us.ihmc.rdx.perception.scene;
+
+public class RDXSceneObject
+{
+   private String modelFileName;
+
+   public RDXSceneObject(String modelFileName)
+   {
+      this.modelFileName = modelFileName;
+   }
+}
