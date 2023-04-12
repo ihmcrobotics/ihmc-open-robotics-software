f2dab63dc6f51f6ed6ee0ade66a8b70e126fa70e
Duncan Calvert <dcalvert@ihmc.us>
2023-04-11 22:04:02 -0500

    Remove pull door with lever handle.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/scene/objects/RDXPullDoorPanelWithLeverHandle.java
+++
@@ -1,9 +0,0 @@
-package us.ihmc.rdx.perception.scene.objects;
-
-public class RDXPullDoorPanelWithLeverHandle extends RDXSceneObject
-{
-   public RDXPullDoorPanelWithLeverHandle()
-   {
-      super("environmentObjects/door/doorLeverHandle/DoorLeverHandle.g3dj", "PullDoorPanelWithLeverHandle");
-   }
-}
