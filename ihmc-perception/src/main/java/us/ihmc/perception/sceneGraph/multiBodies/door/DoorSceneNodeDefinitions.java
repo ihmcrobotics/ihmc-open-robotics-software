f32f6b6067cfd25ab88af116f0deed9b8e20e765
Duncan Calvert <dcalvert@ihmc.us>
2023-04-23 21:29:05 -0500

    Tune up door transforms.

--- ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/multiBodies/door/DoorSceneNodeDefinitions.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/multiBodies/door/DoorSceneNodeDefinitions.java
@@ -13,27 +13,32 @@ public class DoorSceneNodeDefinitions
    public static final double DOOR_ARUCO_MARKER_WIDTH = 0.2032;
 
    public static final int PULL_DOOR_MARKER_ID = 0;
-   public static final RigidBodyTransform PULL_DOOR_FRAME_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(Math.toRadians(180.0), 0.0, Math.toRadians(180.0)),
-         new Point3D(0.0, -0.678702 - 0.005 - 0.006, 1.14141 + 0.02)
-   );
-   public static final RigidBodyTransform PULL_DOOR_PANEL_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(Math.toRadians(180.0), 0.0, Math.toRadians(180.0)),
-         new Point3D(0.0, -0.678702, 1.14141)
-   );
    public static final int PUSH_DOOR_MARKER_ID = 1;
-   public static final RigidBodyTransform PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER = new RigidBodyTransform(
-         new YawPitchRoll(0.0, 0.0, 0.0),
-         new Point3D(-DoorModelParameters.DOOR_PANEL_THICKNESS / 2.0,
-                     -DoorModelParameters.ARUCO_MARKER_PUSH_SIDE_BOTTOM_RIGHT_CORNER_Y_IN_PANEL_FRAME,
-                     -DoorModelParameters.ARUCO_MARKER_PUSH_SIDE_BOTTOM_RIGHT_CORNER_Z_IN_PANEL_FRAME)
-   );
+
+   public static final RigidBodyTransform PULL_DOOR_PANEL_TRANSFORM_TO_MARKER = new RigidBodyTransform();
+   static
+   {
+      EuclidCoreMissingTools.setYawPitchRollDegrees(PULL_DOOR_PANEL_TRANSFORM_TO_MARKER.getRotation(), 180.0, 0.0, 0.0);
+      PULL_DOOR_PANEL_TRANSFORM_TO_MARKER.getTranslation().setX(-DoorModelParameters.DOOR_PANEL_THICKNESS / 2.0);
+      PULL_DOOR_PANEL_TRANSFORM_TO_MARKER.getTranslation().setY(DoorModelParameters.DOOR_PANEL_HINGE_OFFSET
+                                                                + DoorModelParameters.ARUCO_MARKER_PULL_SIDE_BOTTOM_RIGHT_CORNER_Y_IN_PANEL_FRAME);
+      PULL_DOOR_PANEL_TRANSFORM_TO_MARKER.getTranslation().setZ(DoorModelParameters.ARUCO_MARKER_PULL_SIDE_BOTTOM_RIGHT_CORNER_Z_IN_PANEL_FRAME);
+   }
+   /** The frame has the same origin as the panel. */
+   public static final RigidBodyTransform PULL_DOOR_FRAME_TRANSFORM_TO_MARKER = new RigidBodyTransform(PULL_DOOR_PANEL_TRANSFORM_TO_MARKER);
+
+   public static final RigidBodyTransform PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER = new RigidBodyTransform();
+   static
+   {
+      PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER.getTranslation().setX(DoorModelParameters.DOOR_PANEL_THICKNESS / 2.0);
+      PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER.getTranslation().setY(DoorModelParameters.DOOR_PANEL_HINGE_OFFSET
+                                                                + DoorModelParameters.ARUCO_MARKER_PUSH_SIDE_BOTTOM_RIGHT_CORNER_Y_IN_PANEL_FRAME);
+      PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER.getTranslation().setZ(DoorModelParameters.ARUCO_MARKER_PUSH_SIDE_BOTTOM_RIGHT_CORNER_Z_IN_PANEL_FRAME);
+   }
    public static final RigidBodyTransform PUSH_DOOR_FRAME_TRANSFORM_TO_MARKER = new RigidBodyTransform(PUSH_DOOR_PANEL_TRANSFORM_TO_MARKER);
    static
    {
-      PUSH_DOOR_FRAME_TRANSFORM_TO_MARKER.getTranslation().add(0.0,
-                                                               -DoorModelParameters.DOOR_FRAME_HINGE_OFFSET,
-                                                               -DoorModelParameters.DOOR_PANEL_GROUND_GAP_HEIGHT);
+      PUSH_DOOR_FRAME_TRANSFORM_TO_MARKER.getTranslation().addZ(-DoorModelParameters.DOOR_PANEL_GROUND_GAP_HEIGHT);
    }
 
    public static final double DOOR_FRAME_MAXIMUM_DISTANCE_TO_LOCK_IN = 3.0;
