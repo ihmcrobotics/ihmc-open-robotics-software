e64581e8580a8ed876766603c1a41d939df92cad
Duncan Calvert <dcalvert@ihmc.us>
2023-05-03 12:42:27 -0500

    Start working in the new scene graph stuff.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/BehaviorActionSequence.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/BehaviorActionSequence.java
@@ -15,6 +15,7 @@ import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.footstepPlanning.FootstepPlanningModule;
 import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParametersBasics;
 import us.ihmc.log.LogTools;
+import us.ihmc.perception.sceneGraph.PredefinedSceneNodeLibrary;
 import us.ihmc.robotics.referenceFrames.ReferenceFrameLibrary;
 import us.ihmc.ros2.ROS2Topic;
 import us.ihmc.tools.thread.Throttler;
@@ -103,7 +104,7 @@ public class BehaviorActionSequence
    private final WaitDurationActionMessage waitDurationActionMessage = new WaitDurationActionMessage();
    private final WalkActionMessage walkActionMessage = new WalkActionMessage();
 
-   public BehaviorActionSequence(DRCRobotModel robotModel, ROS2ControllerHelper ros2, ReferenceFrameLibrary referenceFrameLibrary)
+   public BehaviorActionSequence(DRCRobotModel robotModel, ROS2ControllerHelper ros2, PredefinedSceneNodeLibrary predefinedSceneNodeLibrary)
    {
       this.robotModel = robotModel;
       this.ros2 = ros2;
