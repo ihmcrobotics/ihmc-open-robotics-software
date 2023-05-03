315ebc406e37ddec2e422b7012ccdb647520716b
Duncan Calvert <dcalvert@ihmc.us>
2023-05-03 12:42:27 -0500

    Start working in the new scene graph stuff.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/BehaviorActionSequenceModule.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/BehaviorActionSequenceModule.java
@@ -5,7 +5,8 @@ import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
 import us.ihmc.avatar.ros2.ROS2ControllerHelper;
 import us.ihmc.commons.Conversions;
 import us.ihmc.commons.thread.ThreadTools;
-import us.ihmc.perception.ArUcoObjectsPerceptionManager;
+import us.ihmc.perception.sceneGraph.PredefinedSceneNodeLibrary;
+import us.ihmc.perception.sceneGraph.ROS2DetectableSceneNodesSubscription;
 import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.tools.thread.Throttler;
 
@@ -15,28 +16,23 @@ import us.ihmc.tools.thread.Throttler;
 public class BehaviorActionSequenceModule
 {
    private final ROS2SyncedRobotModel syncedRobot;
+   private final ROS2DetectableSceneNodesSubscription detectableSceneNodesSubscription;
    private volatile boolean running = true;
    private final Throttler throttler = new Throttler();
    private final double PERIOD = Conversions.hertzToSeconds(30.0);
-   private final ArUcoObjectsPerceptionManager arUcoObjectsPerceptionManager;
    private final BehaviorActionSequence sequence;
 
-   public BehaviorActionSequenceModule(DRCRobotModel robotModel)
+   public BehaviorActionSequenceModule(DRCRobotModel robotModel, PredefinedSceneNodeLibrary predefinedSceneNodeLibrary)
    {
       ROS2ControllerHelper ros2 = new ROS2ControllerHelper(PubSubImplementation.FAST_RTPS, "behavior_action_sequence", robotModel);
 
       syncedRobot = new ROS2SyncedRobotModel(robotModel, ros2.getROS2NodeInterface());
       syncedRobot.initializeToDefaultRobotInitialSetup(0.0, 0.0, 0.0, 0.0);
 
-      arUcoObjectsPerceptionManager = new ArUcoObjectsPerceptionManager(syncedRobot.getReferenceFrames().getObjectDetectionCameraFrame());
 
-      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
-      referenceFrameLibrary.add(arUcoObjectsPerceptionManager.getPullDoorManager().getDoorPanel().getObjectFrame());
-      referenceFrameLibrary.add(arUcoObjectsPerceptionManager.getPullDoorManager().getDoorFrame().getObjectFrame());
-      referenceFrameLibrary.add(arUcoObjectsPerceptionManager.getPushDoorManager().getDoorPanel().getObjectFrame());
-      referenceFrameLibrary.add(arUcoObjectsPerceptionManager.getPushDoorManager().getDoorFrame().getObjectFrame());
+      detectableSceneNodesSubscription = new ROS2DetectableSceneNodesSubscription(predefinedSceneNodeLibrary.getDetectableSceneNodes(), ros2);
 
-      sequence = new BehaviorActionSequence(robotModel, ros2, referenceFrameLibrary);
+      sequence = new BehaviorActionSequence(robotModel, ros2, predefinedSceneNodeLibrary);
 
       Runtime.getRuntime().addShutdownHook(new Thread(this::destroy, "Shutdown"));
       ThreadTools.startAThread(this::actionThread, "ActionThread");
@@ -49,7 +45,7 @@ public class BehaviorActionSequenceModule
          throttler.waitAndRun(PERIOD);
 
          syncedRobot.update();
-         arUcoObjectsPerceptionManager.update();
+         detectableSceneNodesSubscription.update();
          sequence.update();
       }
    }
