048a731643c285202f0dbe9ce6fe894c304639b9
Duncan Calvert <dcalvert@ihmc.us>
2023-05-07 12:52:36 -0500

    Add table to scene and other miscellaneous improvements.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/simulation/scs2/RestartableSCS2SessionRobot.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/simulation/scs2/RestartableSCS2SessionRobot.java
@@ -7,14 +7,9 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
- * Sometimes we want to restart an SCS 2 session with a button
- * and rebuild it, also possibly having changed the robot definition
- * parameters. Therefore, we want to pass in a supplier for each
- * robot definition and a consumer for when the Session gets
- * recreated, so we can pass in the instatiated Robot and setup
- * the controller, in the case that the robot needs one.
+ * See {@link RestartableSCS2SessionRobotInterface}.
  */
-public class RestartableSCS2SessionRobot
+public class RestartableSCS2SessionRobot implements RestartableSCS2SessionRobotInterface
 {
    private final Supplier<RobotDefinition> robotDefinitionSupplier;
    private final Consumer<Robot> setupRobotConsumer;
