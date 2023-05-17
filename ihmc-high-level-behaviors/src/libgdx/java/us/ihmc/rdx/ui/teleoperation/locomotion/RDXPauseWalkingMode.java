bf09e1b9f05616dbfa3ded3a17e13a75cd55a3d1
Nick Kitchel <nkitchel@ihmc.org>
2023-05-17 10:36:34 -0500

    Cleaned walking with space bar. Added RDXPauseWalkingMode to reduce duplicate code. Added some comments

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/locomotion/RDXPauseWalkingMode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/locomotion/RDXPauseWalkingMode.java
@@ -1,19 +1,42 @@
 package us.ihmc.rdx.ui.teleoperation.locomotion;
 
 import controller_msgs.msg.dds.PauseWalkingMessage;
+import imgui.ImGui;
 import us.ihmc.behaviors.tools.CommunicationHelper;
-import us.ihmc.behaviors.tools.interfaces.LogToolsLogger;
 import us.ihmc.log.LogTools;
+import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 
+/**
+ * Manages the PauseWalkingMessage that determines whether the robot is walking.
+ * Buttons are displayed onto the UI that can be clicked to Pause or Continue walking.
+ */
 public class RDXPauseWalkingMode
 {
    private final CommunicationHelper communicationHelper;
    private final PauseWalkingMessage pauseWalkingMessage = new PauseWalkingMessage();
+   private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
+
 
 
    public RDXPauseWalkingMode(CommunicationHelper communicationHelper)
    {
       this.communicationHelper = communicationHelper;
+   }
+
+   public void renderImGuiWidgets()
+   {
+      ImGui.text("Walking Options:");
+      ImGui.sameLine();
+
+      if (ImGui.button(labels.get("Pause")))
+      {
+         setPauseWalking(true);
+      }
+      ImGui.sameLine();
+      if (ImGui.button(labels.get("Continue")))
+      {
+         setPauseWalking(false);
+      }
    }
 
    public void setPauseWalking(boolean pauseWalking)
@@ -25,5 +48,10 @@ public class RDXPauseWalkingMode
          LogTools.info("Paused Walking");
       else
          LogTools.info("Continue Walking");
+   }
+
+   public boolean getPauseWalking()
+   {
+      return pauseWalkingMessage.getPause();
    }
 }
