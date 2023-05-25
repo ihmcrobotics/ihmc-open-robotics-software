235d315827bd93948b4e1ad255f28be54eb0fc56
Duncan Calvert <dcalvert@ihmc.us>
2023-05-24 20:29:42 -0500

    Document behavior tree status

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/tools/behaviorTree/BehaviorTreeStatus.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/tools/behaviorTree/BehaviorTreeStatus.java
@@ -1,11 +1,23 @@
 package us.ihmc.behaviors.tools.behaviorTree;
 
+/**
+ * This node exists for the UI side tree that is updated
+ * via network communication. It is used to show a visualization
+ * of the tree's state and NOT to perform any computation.
+ * All nodes in the UI side tree are instances of this class.
+ */
 public class BehaviorTreeStatus extends BehaviorTreeControlFlowNode
 {
+   /**
+    * Normal use.
+    */
    public BehaviorTreeStatus()
    {
    }
 
+   /**
+    * Deep copy constuctor.
+    */
    public BehaviorTreeStatus(BehaviorTreeNodeBasics node)
    {
       setPreviousStatus(node.getPreviousStatus());
@@ -13,10 +25,8 @@ public class BehaviorTreeStatus extends BehaviorTreeControlFlowNode
       setLastTickInstant(node.getLastTickInstant());
       setType(node.getType());
 
-      if (node instanceof BehaviorTreeControlFlowNodeBasics)
+      if (node instanceof BehaviorTreeControlFlowNodeBasics controlFlowNode)
       {
-         BehaviorTreeControlFlowNodeBasics controlFlowNode = (BehaviorTreeControlFlowNodeBasics) node;
-
          setHasBeenClocked(controlFlowNode.getHasBeenClocked());
 
          for (BehaviorTreeNodeBasics child : controlFlowNode.getChildren())
