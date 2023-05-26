f422405e529164d1b1cc28479ef4b0131e377eca
BhavyanshM <bhavyanshmishra@gmail.com>
2023-05-26 12:00:57 -0500

    Improve terrain perception module.

--- ihmc-perception/src/main/java/us/ihmc/perception/modules/TerrainPerceptionModule.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/modules/TerrainPerceptionModule.java
@@ -11,6 +11,7 @@ import us.ihmc.perception.rapidRegions.RapidPlanarRegionsExtractor;
 import us.ihmc.perception.tools.PerceptionFilterTools;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.geometry.FramePlanarRegionsList;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
 
 public class TerrainPerceptionModule
 {
@@ -21,6 +22,8 @@ public class TerrainPerceptionModule
    private FullHumanoidRobotModel fullRobotModel;
    private CollisionBoxProvider collisionBoxProvider;
    private FramePlanarRegionsList framePlanarRegionsList;
+   private PlanarRegionsList regionsInSensorFrame;
+   private PlanarRegionsList regionsInWorldFrame;
 
    public TerrainPerceptionModule(OpenCLManager openCLManager)
    {
@@ -28,7 +31,7 @@ public class TerrainPerceptionModule
       this.framePlanarRegionsList = new FramePlanarRegionsList();
    }
 
-   public void initializeRapidPlanarRegionsExtractor(int depthHeight, int depthWidth, int fx, int fy, int cx, int cy)
+   public void initializeRapidPlanarRegionsExtractor(int depthHeight, int depthWidth, double fx, double fy, double cx, double cy)
    {
       this.bytedecoDepthImage = new BytedecoImage(depthWidth, depthHeight, opencv_core.CV_16UC1);
       this.bytedecoDepthImage.createOpenCLImage(openCLManager, OpenCL.CL_MEM_READ_WRITE);
@@ -43,15 +46,43 @@ public class TerrainPerceptionModule
       this.collidingScanRegionFilter = PerceptionFilterTools.createHumanoidShinCollisionFilter(fullRobotModel, collisionBoxProvider);
    }
 
-   public void extractFramePlanarRegionsList(BytedecoImage depthImage, ReferenceFrame cameraFrame)
+   public void extractFramePlanarRegionsList(ReferenceFrame cameraFrame)
    {
-      this.rapidPlanarRegionsExtractor.update(depthImage, cameraFrame, this.framePlanarRegionsList);
+      this.rapidPlanarRegionsExtractor.update(bytedecoDepthImage, cameraFrame, this.framePlanarRegionsList);
       this.rapidPlanarRegionsExtractor.setProcessing(false);
+      this.regionsInSensorFrame = this.framePlanarRegionsList.getPlanarRegionsList();
+      this.regionsInWorldFrame = this.regionsInSensorFrame.copy();
+      this.regionsInWorldFrame.applyTransform(cameraFrame.getTransformToWorldFrame());
    }
 
-   public FramePlanarRegionsList getExtractedPlanarRegions()
+   public void filterFramePlanarRegionsList()
+   {
+      PerceptionFilterTools.applyCollisionFilter(framePlanarRegionsList, this.collidingScanRegionFilter);
+   }
+
+   public FramePlanarRegionsList getFramePlanarRegionsResult()
    {
       return this.framePlanarRegionsList;
+   }
+
+   public PlanarRegionsList getRegionsInSensorFrame()
+   {
+      return this.regionsInSensorFrame;
+   }
+
+   public PlanarRegionsList getRegionsInWorldFrame()
+   {
+      return this.regionsInWorldFrame;
+   }
+
+   public BytedecoImage getBytedecoDepthImage()
+   {
+      return this.bytedecoDepthImage;
+   }
+
+   public RapidPlanarRegionsExtractor getRapidRegionsExtractor()
+   {
+      return this.rapidPlanarRegionsExtractor;
    }
 
 }
