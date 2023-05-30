afefd1199e151d06a5abb3a48c8ae75254e36990
BhavyanshM <bhavyanshmishra@gmail.com>
2023-05-30 12:31:15 -0500

    Fix the robot configuration data subscriber hash name issue.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/HumanoidPerceptionModule.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/HumanoidPerceptionModule.java
@@ -1,8 +1,10 @@
 package us.ihmc.perception;
 
+import controller_msgs.msg.dds.RobotConfigurationData;
 import org.bytedeco.opencl.global.OpenCL;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.Mat;
+import us.ihmc.communication.ROS2Tools;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.log.LogTools;
 import us.ihmc.perception.camera.CameraIntrinsics;
@@ -15,51 +17,76 @@ import us.ihmc.perception.tools.PerceptionFilterTools;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.geometry.FramePlanarRegionsList;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
+import us.ihmc.ros2.ROS2Node;
+import us.ihmc.sensorProcessing.communication.producers.RobotConfigurationDataBuffer;
 
 public class HumanoidPerceptionModule
 {
+   private final PlanarRegionMappingHandler mapHandler = new PlanarRegionMappingHandler();
+
    private BytedecoImage bytedecoDepthImage;
    private OpenCLManager openCLManager;
    private RapidPlanarRegionsExtractor rapidPlanarRegionsExtractor;
    private CollidingScanRegionFilter collidingScanRegionFilter;
    private FullHumanoidRobotModel fullRobotModel;
-   private CollisionBoxProvider collisionBoxProvider;
-   private FramePlanarRegionsList framePlanarRegionsList;
    private PlanarRegionsList regionsInSensorFrame;
    private PlanarRegionsList regionsInWorldFrame;
-   private PlanarRegionMappingHandler mapHandler;
+   private CollisionBoxProvider collisionBoxProvider;
+   private FramePlanarRegionsList sensorFrameRegions;
 
-   public HumanoidPerceptionModule(FullHumanoidRobotModel fullRobotModel, CollisionBoxProvider collisionBoxProvider, OpenCLManager openCLManager)
+   private RobotConfigurationDataBuffer robotConfigurationDataBuffer;
+   private RobotConfigurationData robotConfigurationData;
+
+   boolean waitIfNecessary = false; // dangerous if true! need a timeout
+
+   public HumanoidPerceptionModule(OpenCLManager openCLManager)
    {
       this.openCLManager = openCLManager;
-
-      this.mapHandler = new PlanarRegionMappingHandler();
-      this.framePlanarRegionsList = new FramePlanarRegionsList();
-
-      initializeBodyCollisionFilter(fullRobotModel, collisionBoxProvider);
    }
 
-   public void update(Mat depthImage, ReferenceFrame cameraFrame, boolean rapidRegions)
+   public void subscribeToRobotData(String robotName, ROS2Node ros2Node)
    {
-      if (rapidRegions)
+      this.robotConfigurationDataBuffer = new RobotConfigurationDataBuffer();
+      this.robotConfigurationData = new RobotConfigurationData();
+      ROS2Tools.createCallbackSubscriptionTypeNamed(ros2Node,
+                                                    RobotConfigurationData.class,
+                                                    ROS2Tools.getRobotConfigurationDataTopic(robotName),
+                                                    s ->
+                                                    {
+                                                       s.takeNextData(robotConfigurationData, null);
+                                                    });
+   }
+
+   public void update(Mat depthImage, ReferenceFrame cameraFrame, boolean enabled)
+   {
+      if (enabled)
       {
+         if (robotConfigurationData != null && robotConfigurationData.getJointNameHash() != 0)
+         {
+            robotConfigurationDataBuffer.update(robotConfigurationData);
+            long newestTimestamp = robotConfigurationDataBuffer.getNewestTimestamp();
+            long selectedTimestamp = robotConfigurationDataBuffer.updateFullRobotModel(waitIfNecessary, newestTimestamp, this.fullRobotModel, null);
+         }
+
          OpenCVTools.convertFloatToShort(depthImage, bytedecoDepthImage.getBytedecoOpenCVMat(),
                                          1000.0,
                                          0.0);
 
          extractFramePlanarRegionsList(cameraFrame);
+         filterFramePlanarRegionsList();
 
          if (mapHandler.isEnabled())
          {
-            mapHandler.updateMapWithNewRegions(this.framePlanarRegionsList);
+            mapHandler.updateMapWithNewRegions(this.sensorFrameRegions);
          }
       }
    }
 
    public void initializePerspectiveRapidRegionsExtractor(CameraIntrinsics cameraIntrinsics)
    {
-      LogTools.info("Initialize Rapid Regions: {}", cameraIntrinsics);
+      LogTools.info("Initializing Perspective Rapid Regions: {}", cameraIntrinsics);
 
+      this.sensorFrameRegions = new FramePlanarRegionsList();
       this.bytedecoDepthImage = new BytedecoImage(cameraIntrinsics.getWidth(), cameraIntrinsics.getHeight(), opencv_core.CV_16UC1);
       this.bytedecoDepthImage.createOpenCLImage(openCLManager, OpenCL.CL_MEM_READ_WRITE);
       this.rapidPlanarRegionsExtractor = new RapidPlanarRegionsExtractor();
@@ -67,7 +94,7 @@ public class HumanoidPerceptionModule
                                               cameraIntrinsics.getFx(), cameraIntrinsics.getFy(), cameraIntrinsics.getCx(),
                                               cameraIntrinsics.getCy());
 
-      this.rapidPlanarRegionsExtractor.getDebugger().setEnabled(true);
+      this.rapidPlanarRegionsExtractor.getDebugger().setEnabled(false);
    }
 
    public void initializeBodyCollisionFilter(FullHumanoidRobotModel fullRobotModel, CollisionBoxProvider collisionBoxProvider)
@@ -79,22 +106,28 @@ public class HumanoidPerceptionModule
 
    public void extractFramePlanarRegionsList(ReferenceFrame cameraFrame)
    {
-      this.rapidPlanarRegionsExtractor.update(bytedecoDepthImage, cameraFrame, this.framePlanarRegionsList);
+      this.rapidPlanarRegionsExtractor.update(bytedecoDepthImage, cameraFrame, this.sensorFrameRegions);
       this.rapidPlanarRegionsExtractor.setProcessing(false);
 
-      this.regionsInSensorFrame = this.framePlanarRegionsList.getPlanarRegionsList();
+      this.regionsInSensorFrame = this.sensorFrameRegions.getPlanarRegionsList();
       this.regionsInWorldFrame = this.regionsInSensorFrame.copy();
       this.regionsInWorldFrame.applyTransform(cameraFrame.getTransformToWorldFrame());
    }
 
    public void filterFramePlanarRegionsList()
    {
-      PerceptionFilterTools.applyCollisionFilter(framePlanarRegionsList, this.collidingScanRegionFilter);
+      this.fullRobotModel.updateFrames();
+      this.collidingScanRegionFilter.update();
+
+      synchronized (sensorFrameRegions)
+      {
+         PerceptionFilterTools.filterCollidingPlanarRegions(sensorFrameRegions, this.collidingScanRegionFilter);
+      }
    }
 
    public FramePlanarRegionsList getFramePlanarRegionsResult()
    {
-      return this.framePlanarRegionsList;
+      return this.sensorFrameRegions;
    }
 
    public PlanarRegionsList getRegionsInSensorFrame()
