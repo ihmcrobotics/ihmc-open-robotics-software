96d06355e86086e1376c378cd5edb1814bd0cdea
Evan <eyu@ihmc.org>
2023-06-01 13:41:15 -0500

    cleaned up tests. Generic optimizer for lagrange solver

--- ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/optimization/TestALM.java
+++ ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/optimization/TestALM.java
@@ -1,12 +1,10 @@
 package us.ihmc.robotics.optimization;
 
-import gnu.trove.list.array.TDoubleArrayList;
 import org.ejml.data.DMatrixD1;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.mult.VectorVectorMult_DDRM;
+import org.junit.jupiter.api.Test;
 import us.ihmc.commons.MathTools;
-import us.ihmc.robotics.numericalMethods.GradientDescentModule;
-import us.ihmc.robotics.numericalMethods.SingleQueryFunction;
 import us.ihmc.robotics.optimization.constrainedOptimization.AugmentedLagrangeOptimizationProblem;
 
 import static us.ihmc.robotics.Assert.assertTrue;
@@ -23,7 +21,7 @@ public class TestALM
    private double initialPenalty = 1.0;
    private double penaltyIncreaseFactor = 1.5;
 
-   TDoubleArrayList initial = new TDoubleArrayList(new double[] {10.0, 14.5, 16.0});
+   DMatrixD1 initial = new DMatrixRMaj(new double[] {10.0, 14.5, 16.0});
 
    // optimum is (0,0,0...)
    public static double costFunction(DMatrixD1 inputs)
@@ -56,14 +54,10 @@ public class TestALM
       return inputs.get(0) + inputs.get(1) + inputs.get(2) - 6.0;
    }
 
-   public static void convertArrayToMatrix(DMatrixD1 vector, TDoubleArrayList list)
+   @Test
+   public void test()
    {
-      vector.setData(list.toArray());
-      vector.reshape(list.size(), 1);
-   }
-
-   public TestALM()
-   {
+      // Set up the optimization problem
       augmentedLagrange = new AugmentedLagrangeOptimizationProblem(TestALM::costFunction);
       augmentedLagrange.addEqualityConstraint(TestALM::constraint1);
       augmentedLagrange.addInequalityConstraint(TestALM::constraint2);
@@ -71,58 +65,31 @@ public class TestALM
 //      augmentedLagrange.addEqualityConstraint(TestALM::constraint4);
       augmentedLagrange.initialize(initialPenalty, penaltyIncreaseFactor);
 
-      SingleQueryFunction lagrangeCostFunction = new SingleQueryFunction()
-      {
-         @Override
-         public double getQuery(TDoubleArrayList values)
-         {
-            // convert TDoubleArrayList to DMatrixRMaj
-            convertArrayToMatrix(lagrangeInputVector, values);
-            double cost = augmentedLagrange.calculateDualProblemCost(lagrangeInputVector);
-            return cost;
-         }
-      };
+      // Set up the optimizer
+      Optimizer optimizer = new WrappedGradientDescent();
 
       // Repeat lagrange step multiple times
       int iteration = 0;
-      DMatrixD1 optimumMatrixX = new DMatrixRMaj();
-      TDoubleArrayList optimumX = new TDoubleArrayList();
-      TDoubleArrayList optimalSolution = new TDoubleArrayList();
+      DMatrixD1 optimumX = new DMatrixRMaj();
+      optimizer.setCostFunction(augmentedLagrange.getAugmentedCostFunction());
       while (iteration < numLagrangeIterations)
       {
          System.out.println("===== Lagrange Iteration: " + iteration + "==========");
 
          // Run optimization
-         if (iteration > 0)
-         {
-            initial.reset();
-            initial.addAll(optimumX);
-         }
-         GradientDescentModule optimizer = new GradientDescentModule(lagrangeCostFunction, initial);
-         optimizer.run();
+         optimumX = optimizer.optimize(initial);
+         augmentedLagrange.updateLagrangeMultipliers(optimumX);
+         initial.set(optimumX);
 
-         // Update lagrange multipliers
-         optimumX = optimizer.getOptimalInput();
-         convertArrayToMatrix(optimumMatrixX, optimumX);
-         augmentedLagrange.updateLagrangeMultipliers(optimumMatrixX);
          iteration += 1;
 
-         optimalSolution = optimizer.getOptimalInput();
-         for (int i = 0; i < optimalSolution.size(); i++)
-            System.out.println("solution is " + optimalSolution.get(i));
-
-         System.out.println("optimal query is " + optimizer.getOptimalQuery());
+         optimizer.printOutput();
       }
-      assertTrue("x1 arrived on desired value", MathTools.epsilonCompare(optimalSolution.get(0), 6, 1e-3));
-      assertTrue("x2 arrived on desired value", MathTools.epsilonCompare(optimalSolution.get(1), 5, 1e-3));
-      assertTrue("x3 arrived on desired value", MathTools.epsilonCompare(optimalSolution.get(2), 0, 1e-3));
+      assertTrue("x1 arrived on desired value", MathTools.epsilonCompare(optimumX.get(0), 6, 1e-3));
+      assertTrue("x2 arrived on desired value", MathTools.epsilonCompare(optimumX.get(1), 5, 1e-3));
+      assertTrue("x3 arrived on desired value", MathTools.epsilonCompare(optimumX.get(2), 0, 1e-3));
 
    }
 
 
-   public static void main(String[] args)
-   {
-      TestALM alm = new TestALM();
-
-   }
 }
