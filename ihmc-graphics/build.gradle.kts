3d8dfa96f4b4481a2706fe52a429d896993c6e94
Dexton Anderson <danderson@ihmc.org>
2023-06-05 10:46:10 -0500

    Use ihmc release of javacpp libraries

--- ihmc-graphics/build.gradle.kts
+++ ihmc-graphics/build.gradle.kts
@@ -77,17 +77,15 @@ libgdxDependencies {
    val javaFXVersion = "17.0.2"
    api(ihmc.javaFXModule("graphics", javaFXVersion)) // JFX Color
 
-   // https://oss.sonatype.org/content/repositories/snapshots/org/bytedeco/
-   val javaCPPVersion = "1.5.9-SNAPSHOT"
-   api("org.bytedeco:javacpp:$javaCPPVersion")
-   val openblasVersion = "0.3.23-1.5.9-20230404.015544-39"
-   api("org.bytedeco:openblas:$openblasVersion")
-   api("org.bytedeco:openblas:$openblasVersion:linux-x86_64")
-   api("org.bytedeco:openblas:$openblasVersion:windows-x86_64")
-   val opencvVersion = "4.7.0-1.5.9-20230516.151940-290"
-   api("org.bytedeco:opencv:$opencvVersion")
-   api("org.bytedeco:opencv:$opencvVersion:linux-x86_64")
-   api("org.bytedeco:opencv:$opencvVersion:windows-x86_64")
+   api("us.ihmc:javacpp:1.5.9")
+   val openblasVersion = "0.3.23-1.5.9"
+   api("us.ihmc:openblas:$openblasVersion")
+   api("us.ihmc:openblas:$openblasVersion:linux-x86_64")
+   api("us.ihmc:openblas:$openblasVersion:windows-x86_64")
+   val opencvVersion = "4.7.0-1.5.9"
+   api("us.ihmc:opencv:$opencvVersion")
+   api("us.ihmc:opencv:$opencvVersion:linux-x86_64")
+   api("us.ihmc:opencv:$opencvVersion:windows-x86_64")
 }
 
 libgdxTestDependencies {
