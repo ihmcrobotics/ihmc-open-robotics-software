9f28440bf6cb79cdeb529789adcc157dd1cf825c
Duncan Calvert <dcalvert@ihmc.us>
2023-06-07 14:24:14 -0500

    Cleanup frame code for interactable robot links.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXInteractableHand.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXInteractableHand.java
@@ -6,22 +6,19 @@ import com.badlogic.gdx.utils.Pool;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
 import us.ihmc.behaviors.tools.yo.YoVariableClientHelper;
-import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.referenceFrame.interfaces.FixedFrameVector3DBasics;
-import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
-import us.ihmc.mecano.spatial.SpatialVector;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.mecano.spatial.interfaces.SpatialVectorReadOnly;
 import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.rdx.ui.graphics.RDXSpatialVectorArrows;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
-import us.ihmc.robotics.math.filters.AlphaFilteredYoSpatialVector;
 import us.ihmc.robotics.partNames.LimbName;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.robotics.sensors.ForceSensorDefinition;
 import us.ihmc.scs2.definition.robot.RobotDefinition;
 import us.ihmc.sensorProcessing.parameters.HumanoidRobotSensorInformation;
+import us.ihmc.wholeBodyController.HandTransformTools;
 
 /**
  * This class manages being able to click on a humanoid robot's hand in the UI
@@ -53,19 +50,17 @@ public class RDXInteractableHand extends RDXInteractableRobotLink
       this.side = side;
       this.syncedRobot = syncedRobot;
 
-      String robotSidePrefix = (side == RobotSide.LEFT) ? "l_" : "r_";
       RobotDefinition robotDefinition = robotModel.getRobotDefinition();
       FullHumanoidRobotModel fullRobotModel = syncedRobot.getFullRobotModel();
       String modelFileName = RDXInteractableTools.getModelFileName(robotDefinition.getRigidBodyDefinition(robotCollidable.getRigidBodyName()));
 
       ReferenceFrame afterLastWristJointFrame = fullRobotModel.getEndEffectorFrame(side, LimbName.ARM);
-      ReferenceFrame collisionFrame = afterLastWristJointFrame;
-      ReferenceFrame handControlFrame = fullRobotModel.getHandControlFrame(side);
-      ReferenceFrame handGraphicFrame
-            = ReferenceFrameTools.constructFrameWithUnchangingTransformToParent(robotSidePrefix + "graphicFrame",
-                                                                                afterLastWristJointFrame,
-                                                                                robotModel.getUIParameters().getHandGraphicToHandFrameTransform(side));
-      super.create(robotCollidable, handGraphicFrame, collisionFrame, handControlFrame, modelFileName, baseUI.getPrimary3DPanel());
+      ReferenceFrame syncedControlFrame = fullRobotModel.getHandControlFrame(side);
+      RigidBodyTransform graphicToControlFrameTransform = new RigidBodyTransform();
+      RigidBodyTransform linkToControlFrameTransform = new RigidBodyTransform();
+      HandTransformTools.getHandLinkToControlFrameTransform(fullRobotModel, side, linkToControlFrameTransform);
+      HandTransformTools.getHandGraphicToControlFrameTransform(fullRobotModel, robotModel.getUIParameters(), side, graphicToControlFrameTransform);
+      super.create(robotCollidable, syncedControlFrame, graphicToControlFrameTransform, linkToControlFrameTransform, modelFileName, baseUI.getPrimary3DPanel());
 
       HumanoidRobotSensorInformation sensorInformation = robotModel.getSensorInformation();
       SideDependentList<String> wristForceSensorNames = sensorInformation.getWristForceSensorNames();
