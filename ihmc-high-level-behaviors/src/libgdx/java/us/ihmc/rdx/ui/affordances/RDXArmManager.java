9cc670feb98c08036fe0a94a5130232603f20028
Duncan Calvert <dcalvert@ihmc.us>
2023-06-09 14:35:16 -0500

    Move ddogleg solver.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXArmManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXArmManager.java
@@ -6,6 +6,7 @@ import imgui.ImGui;
 import imgui.type.ImBoolean;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
+import us.ihmc.avatar.inverseKinematics.DDogLegArmIKSolver;
 import us.ihmc.avatar.ros2.ROS2ControllerHelper;
 import us.ihmc.behaviors.tools.HandWrenchCalculator;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
@@ -43,7 +44,7 @@ public class RDXArmManager
    private final SideDependentList<double[]> armHomes = new SideDependentList<>();
    private final SideDependentList<double[]> doorAvoidanceArms = new SideDependentList<>();
 
-   private final SideDependentList<RDXDDogLegArmIKSolver> armIKSolvers = new SideDependentList<>();
+   private final SideDependentList<DDogLegArmIKSolver> armIKSolvers = new SideDependentList<>();
 
    private volatile boolean readyToSolve = true;
    private volatile boolean readyToCopySolution = false;
@@ -86,11 +87,11 @@ public class RDXArmManager
       for (RobotSide side : RobotSide.values)
       {
          armIKSolvers.put(side,
-                          new RDXDDogLegArmIKSolver(side,
-                                                   robotModel,
-                                                   syncedRobot.getFullRobotModel(),
-                                                   desiredRobot,
-                                                   interactableHands.get(side).getControlReferenceFrame()));
+                          new DDogLegArmIKSolver(side,
+                                                 robotModel,
+                                                 syncedRobot.getFullRobotModel(),
+                                                 desiredRobot,
+                                                 interactableHands.get(side).getControlReferenceFrame()));
       }
    }
 
