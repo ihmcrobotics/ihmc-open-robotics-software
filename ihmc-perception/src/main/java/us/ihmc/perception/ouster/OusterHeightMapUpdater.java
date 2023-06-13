485488f0d5a0805b1efbf1368a7c31f7fb4f796c
Luigi Penco <lpenco@ihmc.org>
2023-06-13 09:47:36 -0500

    ouster map update

--- ihmc-perception/src/main/java/us/ihmc/perception/ouster/OusterHeightMapUpdater.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/ouster/OusterHeightMapUpdater.java
@@ -46,9 +46,7 @@ public class OusterHeightMapUpdater
 
    private final ROS2StoredPropertySetGroup ros2PropertySetGroup;
 
-   private final ScheduledExecutorService executorService
-         = ExecutorServiceTools.newSingleThreadScheduledExecutor(ThreadTools.createNamedThreadFactory(getClass().getSimpleName()),
-                                                                 ExecutorServiceTools.ExceptionHandling.CATCH_AND_REPORT);
+   private ScheduledExecutorService executorService;
 
    public OusterHeightMapUpdater(ROS2ControllerPublishSubscribeAPI ros2)
    {
@@ -73,6 +71,8 @@ public class OusterHeightMapUpdater
 
    public void start()
    {
+      executorService = ExecutorServiceTools.newSingleThreadScheduledExecutor(ThreadTools.createNamedThreadFactory(getClass().getSimpleName()),
+                                                                              ExecutorServiceTools.ExceptionHandling.CATCH_AND_REPORT);
       executorService.scheduleAtFixedRate(this::update, 0, updateDTMillis, TimeUnit.MILLISECONDS);
    }
 
