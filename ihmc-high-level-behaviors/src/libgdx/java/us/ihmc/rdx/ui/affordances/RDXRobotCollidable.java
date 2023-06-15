f1c8d4630727c40c345800b902e62e1fa662374e
Duncan Calvert <dcalvert@ihmc.us>
2023-06-15 15:18:49 -0500

    Extract MouseCollidable.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXRobotCollidable.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXRobotCollidable.java
@@ -10,18 +10,14 @@ import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.*;
-import us.ihmc.euclid.shape.primitives.interfaces.*;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
-import us.ihmc.euclid.tuple3D.interfaces.UnitVector3DReadOnly;
 import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.rdx.input.ImGui3DViewInput;
 import us.ihmc.rdx.input.ImGui3DViewPickResult;
 import us.ihmc.rdx.tools.RDXModelInstance;
 import us.ihmc.rdx.tools.RDXModelBuilder;
 import us.ihmc.rdx.tools.LibGDXTools;
-import us.ihmc.rdx.ui.gizmo.*;
 import us.ihmc.log.LogTools;
 import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.rdx.vr.RDXVRContext;
@@ -43,6 +39,7 @@ public class RDXRobotCollidable implements RenderableProvider
    private ReferenceFrame linkFrame;
    private final MovingReferenceFrame syncedLinkFrame;
    private final ModifiableReferenceFrame collisionShapeFrame;
+   private final MouseCollidable mouseCollidable;
    private final RDXModelInstance collisionModelInstance;
    private final RDXModelInstance collisionShapeCoordinateFrameGraphic;
    private final FrameShape3DBasics shape;
@@ -50,11 +47,6 @@ public class RDXRobotCollidable implements RenderableProvider
    private final String rigidBodyName;
    private final ImGui3DViewPickResult pickResult = new ImGui3DViewPickResult();
    private final SideDependentList<RDXVRPickResult> vrPickResult = new SideDependentList<>(RDXVRPickResult::new);
-   private SphereRayIntersection sphereRayIntersection;
-   private CapsuleRayIntersection capsuleIntersection;
-   private CylinderRayIntersection cylinderRayIntersection;
-   private EllipsoidRayIntersection ellipsoidRayIntersection;
-   private BoxRayIntersection boxRayIntersection;
    private boolean pickSelected = false;
    private boolean mousePickSelected = false;
    private final SideDependentList<Boolean> vrPickSelected = new SideDependentList<>(false, false);
@@ -89,12 +81,12 @@ public class RDXRobotCollidable implements RenderableProvider
       RigidBodyTransform collisionToLinkFrameTransform = new RigidBodyTransform();
       collisionShapeFrame = new ModifiableReferenceFrame("collisionShapeFrame" + rigidBodyName, linkFrame);
 
+      mouseCollidable = new MouseCollidable(shape);
       collisionModelInstance = new RDXModelInstance(RDXModelBuilder.buildModel(meshBuilder ->
       {
          if (shape instanceof FrameSphere3DReadOnly sphere)
          {
             meshBuilder.addSphere((float) sphere.getRadius(), sphere.getPosition(), color);
-            sphereRayIntersection = new SphereRayIntersection();
          }
          else if (shape instanceof FrameCapsule3DReadOnly capsule)
          {
@@ -109,7 +101,6 @@ public class RDXRobotCollidable implements RenderableProvider
                                    50,
                                    50,
                                    color);
-            capsuleIntersection = new CapsuleRayIntersection();
          }
          else if (shape instanceof FrameBox3DReadOnly box)
          {
@@ -119,7 +110,6 @@ public class RDXRobotCollidable implements RenderableProvider
                                box.getSizeY(),
                                box.getSizeZ(),
                                color);
-            boxRayIntersection = new BoxRayIntersection();
          }
          else if (shape instanceof FramePointShape3DReadOnly pointShape)
          {
@@ -132,7 +122,6 @@ public class RDXRobotCollidable implements RenderableProvider
             EuclidGeometryTools.orientation3DFromZUpToVector3D(cylinder.getAxis(), orientation);
             collisionToLinkFrameTransform.appendOrientation(orientation);
             meshBuilder.addCylinder(cylinder.getLength(), cylinder.getRadius(), new Point3D(0.0, 0.0, -cylinder.getHalfLength()), color);
-            cylinderRayIntersection = new CylinderRayIntersection();
          }
          else if (shape instanceof FrameEllipsoid3DReadOnly ellipsoid)
          {
@@ -143,7 +132,6 @@ public class RDXRobotCollidable implements RenderableProvider
                                      ellipsoid.getRadiusZ(),
                                      new Point3D(),
                                      color);
-            ellipsoidRayIntersection = new EllipsoidRayIntersection();
          }
          else
          {
@@ -207,71 +195,9 @@ public class RDXRobotCollidable implements RenderableProvider
    {
       Line3DReadOnly pickRayInWorld = input.getPickRayInWorld();
       pickResult.reset();
-      if (shape instanceof Sphere3DReadOnly sphere)
-      {
-         sphereRayIntersection.update(sphere.getRadius(), sphere.getPosition(), collisionShapeFrame.getReferenceFrame());
-         if (sphereRayIntersection.intersect(input.getPickRayInWorld()))
-         {
-            pickResult.addPickCollision(input.getPickRayInWorld().getPoint().distance(sphereRayIntersection.getFirstIntersectionToPack()));
-         }
-      }
-      else if (shape instanceof Capsule3DReadOnly capsule)
-      {
-         UnitVector3DReadOnly axis = capsule.getAxis();
-         Point3DReadOnly position = capsule.getPosition();
-         double length = capsule.getLength();
-         double radius = capsule.getRadius();
-         capsuleIntersection.update(radius, length, position, axis, linkFrame);
-         if (capsuleIntersection.intersect(pickRayInWorld))
-         {
-            pickResult.addPickCollision(capsuleIntersection.getDistanceToCollision(input.getPickRayInWorld()));
-         }
-      }
-      else if (shape instanceof Box3DReadOnly box)
-      {
-         if (boxRayIntersection.intersect(box.getSizeX(),
-                                          box.getSizeY(),
-                                          box.getSizeZ(),
-                                          collisionShapeFrame.getReferenceFrame().getTransformToRoot(),
-                                          pickRayInWorld))
-         {
-            pickResult.addPickCollision(boxRayIntersection.getFirstIntersectionToPack().distance(input.getPickRayInWorld().getPoint()));
-         }
-      }
-      else if (shape instanceof PointShape3DReadOnly pointShape)
-      {
-         // We're not colliding with points as they have no volume
-      }
-      else if (shape instanceof Cylinder3DReadOnly cylinder)
-      {
-         cylinderRayIntersection.update(cylinder.getLength(),
-                                        cylinder.getRadius(),
-                                        cylinder.getPosition(),
-                                        cylinder.getAxis(),
-                                        linkFrame);
-         double intersection = cylinderRayIntersection.intersect(input.getPickRayInWorld());
-         if (!Double.isNaN(intersection))
-         {
-            pickResult.addPickCollision(intersection);
-         }
-      }
-      else if (shape instanceof Ellipsoid3DReadOnly ellipsoid)
-      {
-         ellipsoidRayIntersection.update(ellipsoid.getRadiusX(),
-                                         ellipsoid.getRadiusY(),
-                                         ellipsoid.getRadiusZ(),
-                                         ellipsoid.getPosition(),
-                                         ellipsoid.getOrientation(),
-                                         linkFrame);
-         if (ellipsoidRayIntersection.intersect(input.getPickRayInWorld()))
-         {
-            pickResult.addPickCollision(ellipsoidRayIntersection.getFirstIntersectionToPack().distance(input.getPickRayInWorld().getPoint()));
-         }
-      }
-      else
-      {
-         LogTools.warn("Shape not handled: {}", shape);
-      }
+      double collision = mouseCollidable.collide(pickRayInWorld, collisionShapeFrame.getReferenceFrame());
+      if (!Double.isNaN(collision))
+         pickResult.addPickCollision(collision);
 
       if (pickResult.getPickCollisionWasAddedSinceReset())
       {
