5c5620a8139c571bd0b6eae698290c24d6ffaa6b
Tomasz Bialek <tbialek@ihmc.org>
2023-06-16 09:16:07 -0500

    Revert ":arrow_up: ihmc-ci 8.0"
    This reverts commit f7561dbc9b737b75bd69dc823ae7ce9571855312.

--- ihmc-footstep-planning/build.gradle.kts
+++ ihmc-footstep-planning/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
    id("us.ihmc.ihmc-build")
-   id("us.ihmc.ihmc-ci") version "8.3"
+   id("us.ihmc.ihmc-ci") version "7.7"
    id("us.ihmc.ihmc-cd") version "1.24"
    id("us.ihmc.log-tools-plugin") version "0.6.3"
 }
