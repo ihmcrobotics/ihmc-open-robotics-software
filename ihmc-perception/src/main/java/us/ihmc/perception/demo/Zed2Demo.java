a0fc62aef54da61c257aabe82f63d7f6dc6c3c88
Tomasz Bialek <tbialek@ihmc.org>
2023-06-29 07:17:33 -0500

    Started zed 2 demo class

--- ihmc-perception/src/main/java/us/ihmc/perception/demo/Zed2Demo.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/demo/Zed2Demo.java
@@ -1,5 +1,13 @@
 package us.ihmc.perception.demo;
 
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+
 public class Zed2Demo
 {
+   VideoCapture  videoCapture;
+
+   Zed2Demo()
+   {
+
+   }
 }
