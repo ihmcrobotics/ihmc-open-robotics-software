9bc3b4a47219882bf2921a93d06fa551d7a4c9a5
chris <chris@BW23016>
2023-07-14 16:11:08 -0500

    Added a test and some comments. Test is passing.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/virtualModelControl/JointTorqueAndPowerConstraintHandler.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/virtualModelControl/JointTorqueAndPowerConstraintHandler.java
@@ -6,24 +6,29 @@ public class JointTorqueAndPowerConstraintHandler
 {
    private double torqueLimitLower;
    private double torqueLimitUpper;
+   private double torqueLimitFromPowerLower;
+   private double torqueLimitFromPowerUpper;
 
+   /**
+    * Computes the more restrictive constraints on torque given a joint (which has a velocity and may
+    * have effort limits), power limits, and a boolean, which dictates whether the joint effort limits
+    * are considered.
+    */
    public JointTorqueAndPowerConstraintHandler(OneDoFJointBasics joint, double powerLimitLower, double powerLimitUpper, boolean hasTorqueConstraint)
    {
       if (powerLimitLower > powerLimitUpper)
          throw new RuntimeException("powerLimitLower cannot be larger than powerLimitUpper");
-      
+
       double qd = joint.getQd();
-      double torqueLimitFromPowerLower;
-      double torqueLimitFromPowerUpper;
       if (qd < 0)
       {
-         torqueLimitFromPowerLower = powerLimitUpper / qd;
-         torqueLimitFromPowerUpper = powerLimitLower / qd;
+         this.torqueLimitFromPowerLower = powerLimitUpper / qd;
+         this.torqueLimitFromPowerUpper = powerLimitLower / qd;
       }
       else
       {
-         torqueLimitFromPowerLower = powerLimitLower / qd;
-         torqueLimitFromPowerUpper = powerLimitUpper / qd;
+         this.torqueLimitFromPowerLower = powerLimitLower / qd;
+         this.torqueLimitFromPowerUpper = powerLimitUpper / qd;
       }
 
       if (hasTorqueConstraint)
@@ -38,13 +43,37 @@ public class JointTorqueAndPowerConstraintHandler
       }
    }
 
+   /**
+    * Gets the least upper bound on torque
+    */
    public double getTorqueLimitLower()
    {
       return torqueLimitLower;
    }
 
+   /**
+    * Gets the greatest lower bound on torque
+    */
    public double getTorqueLimitUpper()
    {
       return torqueLimitUpper;
+   }
+
+   /**
+    * This method only gets the torque limit relative to the power limit and is intended ONLY for
+    * testing and debugging.
+    */
+   public double getTorqueLimitsFromPowerLower()
+   {
+      return torqueLimitFromPowerLower;
+   }
+
+   /**
+    * This method only gets the torque limit relative to the power limit and is intended ONLY for
+    * testing and debugging.
+    */
+   public double getTorqueLimitsFromPowerUpper()
+   {
+      return torqueLimitFromPowerUpper;
    }
 }
