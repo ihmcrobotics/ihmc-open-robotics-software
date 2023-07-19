f22db4c2ab301490dc05af959696bda74b086e60
Duncan Calvert <dcalvert@ihmc.us>
2023-07-19 14:40:02 -0500

    Disable by default and clean up the code for the wrist wrench display.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDX3DPanelHandWrenchIndicator.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDX3DPanelHandWrenchIndicator.java
@@ -10,11 +10,10 @@ import us.ihmc.robotics.robotSide.SideDependentList;
 
 public class RDX3DPanelHandWrenchIndicator
 {
-   private RDX3DPanel panel;
+   private final RDX3DPanel panel;
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
-   private boolean showAndUpdate = true;
-   private SideDependentList<Double> linearParts = new SideDependentList<>((double) 0, (double) 0);
-   private SideDependentList<Double> angularParts = new SideDependentList<>((double) 0, (double) 0);
+   private final SideDependentList<Double> linearParts = new SideDependentList<>((double) 0, (double) 0);
+   private final SideDependentList<Double> angularParts = new SideDependentList<>((double) 0, (double) 0);
 
    public RDX3DPanelHandWrenchIndicator(RDX3DPanel panel)
    {
@@ -23,43 +22,32 @@ public class RDX3DPanelHandWrenchIndicator
 
    public void renderImGuiOverlay()
    {
-      if (showAndUpdate)
-      {
-         // temporarily set arbitrary width height
-         float panelWidth = 200;
-         float panelHeight = 115;
+      // temporarily set arbitrary width height
+      float panelWidth = 200;
+      float panelHeight = 115;
 
-         ImGui.setNextWindowSize(panelWidth, panelHeight);
-         float startX = panel.getWindowPositionX() + (panel.getWindowSizeX() - panelWidth - 5);
-         float startY = (panel.getWindowPositionY() + 10);
-         ImGui.setNextWindowPos(startX, startY);
-         ImGui.setNextWindowBgAlpha(0.2f);
-         int windowFlags = ImGuiWindowFlags.NoTitleBar; // undecorated
-         ImGui.begin(labels.get("WrenchMagnitudeIndicator"), windowFlags);
-         ImGui.pushFont(ImGuiTools.getMediumFont());
-         for (RobotSide side : RobotSide.values)
-         {
-            ImGui.text(side.getPascalCaseName() + " Linear: " + String.format("%.2f", linearParts.get(side)) + " N");
-            ImGui.text(side.getPascalCaseName() + " Angular: " + String.format("%.2f", angularParts.get(side)) + " Nm");
-            if (side == RobotSide.LEFT)
-               ImGui.separator();
-         }
-         ImGui.popFont();
-         ImGui.end();
+      ImGui.setNextWindowSize(panelWidth, panelHeight);
+      float startX = panel.getWindowPositionX() + (panel.getWindowSizeX() - panelWidth - 5);
+      float startY = (panel.getWindowPositionY() + 10);
+      ImGui.setNextWindowPos(startX, startY);
+      ImGui.setNextWindowBgAlpha(0.2f);
+      int windowFlags = ImGuiWindowFlags.NoTitleBar; // undecorated
+      ImGui.begin(labels.get("WrenchMagnitudeIndicator"), windowFlags);
+      ImGui.pushFont(ImGuiTools.getMediumFont());
+      for (RobotSide side : RobotSide.values)
+      {
+         ImGui.text(side.getPascalCaseName() + " Linear: " + String.format("%.2f", linearParts.get(side)) + " N");
+         ImGui.text(side.getPascalCaseName() + " Angular: " + String.format("%.2f", angularParts.get(side)) + " Nm");
+         if (side == RobotSide.LEFT)
+            ImGui.separator();
       }
+      ImGui.popFont();
+      ImGui.end();
    }
 
    public void update(RobotSide side, double linearWrenchMagnitude, double angularWrenchMagnitude)
    {
-      if (showAndUpdate)
-      {
-         linearParts.set(side, linearWrenchMagnitude);
-         angularParts.set(side, angularWrenchMagnitude);
-      }
-   }
-
-   public void setShowAndUpdate(boolean showAndUpdate)
-   {
-      this.showAndUpdate = showAndUpdate;
+      linearParts.set(side, linearWrenchMagnitude);
+      angularParts.set(side, angularWrenchMagnitude);
    }
 }
