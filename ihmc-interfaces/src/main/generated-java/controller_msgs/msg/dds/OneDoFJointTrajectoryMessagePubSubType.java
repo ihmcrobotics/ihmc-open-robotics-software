2353d533e746506852e6418ce772bc5182ce1964
smccrory <smccrory@ihmc.us>
2023-07-22 00:25:00 -0500

    increasing traj size - setting surface normals for wall leaning script

--- ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/OneDoFJointTrajectoryMessagePubSubType.java
+++ ihmc-interfaces/src/main/generated-java/controller_msgs/msg/dds/OneDoFJointTrajectoryMessagePubSubType.java
@@ -54,7 +54,7 @@ public class OneDoFJointTrajectoryMessagePubSubType implements us.ihmc.pubsub.To
 
       current_alignment += 4 + us.ihmc.idl.CDR.alignment(current_alignment, 4);
 
-      current_alignment += 4 + us.ihmc.idl.CDR.alignment(current_alignment, 4);for(int i0 = 0; i0 < 350; ++i0)
+      current_alignment += 4 + us.ihmc.idl.CDR.alignment(current_alignment, 4);for(int i0 = 0; i0 < 500; ++i0)
       {
           current_alignment += ihmc_common_msgs.msg.dds.TrajectoryPoint1DMessagePubSubType.getMaxCdrSerializedSize(current_alignment);}
       current_alignment += 8 + us.ihmc.idl.CDR.alignment(current_alignment, 8);
@@ -91,7 +91,7 @@ public class OneDoFJointTrajectoryMessagePubSubType implements us.ihmc.pubsub.To
    {
       cdr.write_type_4(data.getSequenceId());
 
-      if(data.getTrajectoryPoints().size() <= 350)
+      if(data.getTrajectoryPoints().size() <= 500)
       cdr.write_type_e(data.getTrajectoryPoints());else
           throw new RuntimeException("trajectory_points field exceeds the maximum length");
 
