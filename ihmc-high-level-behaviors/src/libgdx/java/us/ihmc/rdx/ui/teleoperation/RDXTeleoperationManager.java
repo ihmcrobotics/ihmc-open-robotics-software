9dbae95d27d3f9aa83d6ef3e2fccc9618fd2e1b5
Nick Kitchel <nkitchel@ihmc.org>
2023-07-22 17:00:54 -0500

    Remove checkbox to show additional parameter panels, they can still be clicked in the panels menu

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/RDXTeleoperationManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/RDXTeleoperationManager.java
@@ -419,8 +419,6 @@ public class RDXTeleoperationManager extends ImGuiPanel
 
       trajectoryTimeSlider.renderImGuiWidget();
 
-      ImGui.checkbox(labels.get("Show teleoperation parameter tuner"), teleoperationParametersTuner.getIsShowing());
-
       ImGui.separator();
 
       if (interactablesAvailable)
