c3110b10bfbc780ec5ee3801d184039f215130f6
Ben <bpratt@ihmc.org>
2023-07-24 09:18:03 -0500

    fixes problem where squareUp could continue making more footsteps

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/locomotion/RDXLocomotionManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/locomotion/RDXLocomotionManager.java
@@ -294,14 +294,21 @@ public class RDXLocomotionManager
       }
       ImGui.sameLine();
 
-//      ImGui.beginDisabled(!walkAvailable);
+      //      ImGui.beginDisabled(!walkAvailable);
       if (ImGui.button(labels.get("Walk")))
       { // TODO: Add checker here. Make it harder to walk or give warning if the checker is failing
          interactableFootstepPlan.walkFromSteps();
       }
       ImGuiTools.previousWidgetTooltip("Keybind: Space");
       ImGui.sameLine();
-//      ImGui.endDisabled();
+      //      ImGui.endDisabled();
+
+      if (ImGui.button(labels.get("Square Up")))
+      {
+         deleteAll();
+         manualFootstepPlacement.squareUpFootstep();
+      }
+      ImGui.sameLine();
 
       ImGui.beginDisabled(!pauseAvailable);
       if (ImGui.button(labels.get("Pause")))
