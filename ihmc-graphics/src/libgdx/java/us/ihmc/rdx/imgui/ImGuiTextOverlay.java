c361b1658096477800446c55c91b7ef43f6de7c7
Duncan Calvert <dcalvert@ihmc.us>
2023-08-01 17:30:15 -0500

    Extract constant.

--- ihmc-graphics/src/libgdx/java/us/ihmc/rdx/imgui/ImGuiTextOverlay.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/rdx/imgui/ImGuiTextOverlay.java
@@ -7,18 +7,18 @@ import us.ihmc.rdx.ui.RDX3DPanel;
 public class ImGuiTextOverlay
 {
    private final transient ImVec2 textSize = new ImVec2();
+   private static final int MARGIN = 10;
 
    public void render(String text, float drawMinX, float drawMinY, float textPositionX, float textPositionY)
    {
       ImGui.calcTextSize(textSize, text);
-      int margin = 10;
-      ImGui.setCursorPos(textPositionX + margin, textPositionY + textSize.y + margin);
+      ImGui.setCursorPos(textPositionX + MARGIN, textPositionY + textSize.y + MARGIN);
       float drawStartX = drawMinX + textPositionX;
       float drawStartY = drawMinY + textPositionY;
       ImGui.getWindowDrawList().addRectFilled(drawStartX,
                                               drawStartY,
-                                              drawStartX + textSize.x + margin + margin,
-                                              drawStartY + textSize.y + margin + margin,
+                                              drawStartX + textSize.x + MARGIN + MARGIN,
+                                              drawStartY + textSize.y + MARGIN + MARGIN,
                                               RDX3DPanel.OVERLAY_BACKGROUND_COLOR);
       ImGui.text(text);
    }
