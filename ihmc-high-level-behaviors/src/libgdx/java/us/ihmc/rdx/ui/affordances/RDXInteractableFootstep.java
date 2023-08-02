256bcf6b8c1e7950dcf4970e82b0d256f09f0796
Ben <bpratt@ihmc.org>
2023-08-02 15:45:48 -0500

    Fixes measurements of footstep

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXInteractableFootstep.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXInteractableFootstep.java
@@ -53,9 +53,9 @@ public class RDXInteractableFootstep
    // Intended to reuse text renderables, as they are relatively expensive to create
    private static final Map<String, RDX3DSituatedText> textRenderablesMap = new HashMap<>();
    // Measured footstep.fbx in Blender
-   private static final double FOOTSTEP_GRAPHIC_HEIGHT = 0.033251;
-   private static final double FOOTSTEP_GRAPHIC_LENGTH = 0.266301;
-   private static final double FOOTSTEP_GRAPHIC_WIDTH = 0.128225;
+   private static final double FOOTSTEP_GRAPHIC_HEIGHT = 0.0287;
+   private static final double FOOTSTEP_GRAPHIC_LENGTH = 0.2565;
+   private static final double FOOTSTEP_GRAPHIC_WIDTH = 0.1248;
    private static final double FOOTSTEP_GRAPHIC_SOLE_OFFSET_X = 0.01;
    private static final double FOOTSTEP_GRAPHIC_SOLE_OFFSET_Y = 0.0;
    private static final double FOOTSTEP_GRAPHIC_SOLE_OFFSET_Z = FOOTSTEP_GRAPHIC_HEIGHT / 2.0;
