9b8f65a8d80c6194ecdb123bd3cfef97e82a3eed
Stefan Fasano <sfasano@ihmc.org>
2023-08-08 10:58:08 -0500

    first pass at flushing foostep queue everytime walking is paused with joystick

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/plugin/ComponentBasedFootstepDataMessageGeneratorFactory.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/plugin/ComponentBasedFootstepDataMessageGeneratorFactory.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-import controller_msgs.msg.dds.FootstepStatusMessage;
-import controller_msgs.msg.dds.HighLevelStateChangeStatusMessage;
-import controller_msgs.msg.dds.PauseWalkingMessage;
-import controller_msgs.msg.dds.WalkingStatusMessage;
+import controller_msgs.msg.dds.*;
 import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
 import us.ihmc.commonWalkingControlModules.controllers.Updatable;
 import us.ihmc.commonWalkingControlModules.desiredFootStep.footstepGenerator.*;
@@ -155,7 +152,7 @@ public class ComponentBasedFootstepDataMessageGeneratorFactory implements Humano
       continuousStepGenerator.configureWith(walkingControllerParameters);
       continuousStepGenerator.setStopWalkingMessenger(new StopWalkingMessenger()
       {
-         private final PauseWalkingMessage message = HumanoidMessageTools.createPauseWalkingMessage(true);
+         private final AbortWalkingMessage message = new AbortWalkingMessage();
 
          @Override
          public void submitStopWalkingRequest()
