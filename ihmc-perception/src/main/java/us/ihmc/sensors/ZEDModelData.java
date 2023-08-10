90c126cbfc3070874774d30c7fbcbeb42bc3998f
Nadia Operator <ocu@ihmc.org>
2023-08-10 11:09:43 -0500

    Fixed ZED distance values

--- ihmc-perception/src/main/java/us/ihmc/sensors/ZEDModelData.java
+++ ihmc-perception/src/main/java/us/ihmc/sensors/ZEDModelData.java
@@ -8,12 +8,12 @@ public enum ZEDModelData
     * This has been done as sl_get_camera_model() does not differentiate between 2.2mm and 4mm lens models.
     * Assuming the use of wide angle (2.2mm) models seems safe for now (2023).
     */
-   ZED(0.6, 0.2f, 40.0f),
-   ZED_MINI(0.315, 0.1f, 20.0f),
-   ZED_2(0.6, 0.3f, 40.0f),
-   ZED_2I(0.6, 0.2f, 40.0f),
-   ZED_X(0.6, 0.3f, 20.0f),
-   ZED_X_MINI(0.25, 0.1f, 8.0f);
+   ZED(0.06, 0.2f, 40.0f),
+   ZED_MINI(0.0315, 0.1f, 20.0f),
+   ZED_2(0.06, 0.3f, 40.0f),
+   ZED_2I(0.06, 0.2f, 40.0f),
+   ZED_X(0.06, 0.3f, 20.0f),
+   ZED_X_MINI(0.025, 0.1f, 8.0f);
 
    private final double centerToCameraDistance;
    private final float minimumDepthDistance;
