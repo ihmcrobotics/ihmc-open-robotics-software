800f21e36b9b11efd90e0d0e2d74394ca793f970
Robert Griffin <rgriffin@ihmc.us>
2023-08-18 11:30:08 -0500

    fixed some frame and matrix sizing bugs in the external control state

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/external/WholeBodyConfigurationManager.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/highLevelStates/external/WholeBodyConfigurationManager.java
@@ -12,7 +12,6 @@ import us.ihmc.humanoidRobotics.communication.controllerAPI.command.CrocoddylSol
 import us.ihmc.humanoidRobotics.communication.controllerAPI.command.CrocoddylStateCommand;
 import us.ihmc.mecano.frames.MovingReferenceFrame;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointReadOnly;
-import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
 import us.ihmc.mecano.spatial.Twist;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotModels.FullRobotModel;
@@ -41,8 +40,8 @@ public class WholeBodyConfigurationManager
    private final MultipleWaypointsTrajectoryGenerator[] jointTrajectories;
    private final MultipleWaypointsPoseTrajectoryGenerator basePoseTrajectory;
 
-   private final RecyclingArrayList<DMatrixRMaj> feedForwardTorqueValues = new RecyclingArrayList<>(DMatrixRMaj::new);
-   private final RecyclingArrayList<DMatrixRMaj> feedbackGainMatrix = new RecyclingArrayList<>(DMatrixRMaj::new);
+   private final RecyclingArrayList<DMatrixRMaj> feedForwardTorqueValues = new RecyclingArrayList<>(() -> new DMatrixRMaj(0, 0));
+   private final RecyclingArrayList<DMatrixRMaj> feedbackGainMatrices = new RecyclingArrayList<>(() -> new DMatrixRMaj(0, 0));
 
    private final PoseReferenceFrame desiredPelvisFrame = new PoseReferenceFrame("DesiredPelvisFrame", ReferenceFrame.getWorldFrame());
    private final PoseReferenceFrame tempDesiredPelvisFrame = new PoseReferenceFrame("tempDesiredPelvisFrame", ReferenceFrame.getWorldFrame());
@@ -60,6 +59,8 @@ public class WholeBodyConfigurationManager
    private final YoDouble[] jointRateError;
 
    private final FrameVector3D tempVector = new FrameVector3D();
+   private final FrameVector3D rootLinearVelocity = new FrameVector3D();
+   private final FrameVector3D rootAngularVelocity = new FrameVector3D();
 
    private final DoubleProvider time;
 
@@ -103,10 +104,33 @@ public class WholeBodyConfigurationManager
 
    public void initialize()
    {
-      timeAtWholeBodyCommand.set(Double.NaN);
-      for (MultipleWaypointsTrajectoryGenerator jointTrajectory : jointTrajectories)
-         jointTrajectory.clear();
+      timeAtWholeBodyCommand.set(time.getValue());
+      for (int i = 0; i < jointTrajectories.length; i++)
+      {
+         jointTrajectories[i].clear();
+         jointTrajectories[i].appendWaypoint(0.0, controlledJoints[i].getQ(), 0.0);
+         jointTrajectories[i].initialize();
+      }
+
+      desiredPelvisPose.setToZero(fullRobotModel.getRootBody().getBodyFixedFrame());
+      desiredPelvisPose.changeFrame(ReferenceFrame.getWorldFrame());
+      desiredAngularRate.setToZero(ReferenceFrame.getWorldFrame());
+      desiredLinearRate.setToZero(ReferenceFrame.getWorldFrame());
+
       basePoseTrajectory.clear(ReferenceFrame.getWorldFrame());
+      basePoseTrajectory.appendPoseWaypoint(0.0, desiredPelvisPose, desiredLinearRate, desiredAngularRate);
+      basePoseTrajectory.initialize();
+
+      feedForwardTorqueValues.clear();
+      feedbackGainMatrices.clear();
+
+      DMatrixRMaj feedforwardTorques = feedForwardTorqueValues.add();
+      feedforwardTorques.reshape(controlledJoints.length, 1);
+      feedforwardTorques.zero();
+
+      DMatrixRMaj gainMatrix = feedbackGainMatrices.add();
+      gainMatrix.reshape(controlledJoints.length, 2 * (controlledJoints.length + 6));
+      gainMatrix.zero();
    }
 
    public void doControl()
@@ -122,11 +146,11 @@ public class WholeBodyConfigurationManager
       populateFeedForwardTorqueVector(currentIndex);
 
       // make sure everything is sized properly
-      feedbackTorqueVector.reshape(controlledJoints.length, 0);
-      feedForwardTorqueVector.reshape(controlledJoints.length, 0);
-      totalTorqueVector.reshape(controlledJoints.length, 0);
+      feedbackTorqueVector.reshape(controlledJoints.length, 1);
+      feedForwardTorqueVector.reshape(controlledJoints.length, 1);
+      totalTorqueVector.reshape(controlledJoints.length, 1);
 
-      CommonOps_DDRM.mult(feedbackGainMatrix.get(currentIndex), stateErrorVector, feedbackTorqueVector);
+      CommonOps_DDRM.mult(feedbackGainMatrices.get(currentIndex), stateErrorVector, feedbackTorqueVector);
       CommonOps_DDRM.add(feedForwardTorqueVector, feedbackTorqueVector, totalTorqueVector);
 
       // populate the yo variables
@@ -156,11 +180,19 @@ public class WholeBodyConfigurationManager
       currentPelvisPose.getRotation().getRotationVector(rootOrientationError);
 
       tempVector.setReferenceFrame(ReferenceFrame.getWorldFrame());
-      tempVector.sub(basePoseTrajectory.getVelocity(), rootFrame.getTwistOfFrame().getLinearPart());
+      rootLinearVelocity.setIncludingFrame(rootFrame.getTwistOfFrame().getLinearPart());
+      rootLinearVelocity.changeFrame(ReferenceFrame.getWorldFrame());
+      tempVector.sub(basePoseTrajectory.getVelocity(), rootLinearVelocity);
       rootLinearRateError.setMatchingFrame(tempVector);
 
-      tempVector.sub(basePoseTrajectory.getAngularVelocity(), rootFrame.getTwistOfFrame().getAngularPart());
+      rootLinearVelocity.setIncludingFrame(rootFrame.getTwistOfFrame().getAngularPart());
+      rootLinearVelocity.changeFrame(ReferenceFrame.getWorldFrame());
+      tempVector.sub(basePoseTrajectory.getAngularVelocity(), rootAngularVelocity);
       rootAngularRateError.setMatchingFrame(tempVector);
+
+      // resize the vector
+      stateErrorVector.reshape(2 * (controlledJoints.length + 6), 1);
+      stateErrorVector.zero();
 
       rootTranslationError.get(stateErrorVector);
       rootOrientationError.get(3, stateErrorVector);
@@ -190,7 +222,7 @@ public class WholeBodyConfigurationManager
       for (int jointIndex = 0; jointIndex < jointTrajectories.length; jointIndex++)
          jointTrajectories[jointIndex].clear();
       basePoseTrajectory.clear(ReferenceFrame.getWorldFrame());
-      feedbackGainMatrix.clear();
+      feedbackGainMatrices.clear();
       feedForwardTorqueValues.clear();
 
       double currentTime = 0.0;
@@ -219,7 +251,7 @@ public class WholeBodyConfigurationManager
 
          CrocoddylControlCommand control = command.getControl(knot);
          feedForwardTorqueValues.add().set(control.getControl());
-         feedbackGainMatrix.add().set(control.getFeedbackGainCommand().getGainMatrix());
+         feedbackGainMatrices.add().set(control.getFeedbackGainCommand().getGainMatrix());
 
          currentTime += duration;
       }
