3302089c7a7addeadc102cab94c8911b55d9aee1
smccrory <smccrory@ihmc.us>
2023-08-24 11:07:20 -0500

    :arrow_up: scs2 17-0.16.2

--- ihmc-robotics-toolkit/build.gradle.kts
+++ ihmc-robotics-toolkit/build.gradle.kts
@@ -40,7 +40,7 @@ mainDependencies {
    api("us.ihmc:ihmc-graphics-description:0.20.5")
    api("us.ihmc:simulation-construction-set-utilities:0.24.1")
    api("us.ihmc:ihmc-native-library-loader:2.0.2")
-   api("us.ihmc:scs2-definition:17-0.16.1")
+   api("us.ihmc:scs2-definition:17-0.16.2")
    // NOTE: IHMCRoboticsToolkit should not depend on any other IHMC project(!), especially it should not depend on IHMCJavaToolkit.
 }
 
--- simulation-construction-set-tools/build.gradle.kts
+++ simulation-construction-set-tools/build.gradle.kts
@@ -18,8 +18,8 @@ mainDependencies {
    api("us.ihmc:euclid-frame-shape:0.20.0")
    api("us.ihmc:euclid-shape:0.20.0")
    api("us.ihmc:simulation-construction-set:0.24.1")
-   api("us.ihmc:scs2-definition:17-0.16.1")
-   api("us.ihmc:scs2-simulation-construction-set:17-0.16.1")
+   api("us.ihmc:scs2-definition:17-0.16.2")
+   api("us.ihmc:scs2-simulation-construction-set:17-0.16.2")
    api("us.ihmc:ihmc-parameter-optimization:source")
    api("us.ihmc:ihmc-java-toolkit:source")
 }
