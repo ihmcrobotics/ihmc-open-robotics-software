a927c6e1ee3987c09f789ca541605fb3aec8cab8
Duncan Calvert <dcalvert@ihmc.org>
2023-08-26 16:41:55 -0500

    Dock perception panel behind the global visualizers panel.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/visualizers/RDXGlobalVisualizersPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/visualizers/RDXGlobalVisualizersPanel.java
@@ -3,11 +3,12 @@ package us.ihmc.rdx.ui.visualizers;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
-import imgui.internal.ImGui;
+import imgui.ImGui;
 import us.ihmc.rdx.imgui.RDXPanel;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.sceneManager.RDXRenderableProvider;
 import us.ihmc.rdx.sceneManager.RDXSceneLevel;
+import us.ihmc.tools.Timer;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -19,6 +20,9 @@ public class RDXGlobalVisualizersPanel extends RDXPanel implements RDXRenderable
    private final ArrayList<RDXVisualizer> visualizers = new ArrayList<>();
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
    private boolean created = false;
+   private boolean firstFrame = true;
+   private boolean startedTimer = false;
+   private final Timer focusTimer = new Timer();
 
    public RDXGlobalVisualizersPanel()
    {
@@ -60,6 +64,23 @@ public class RDXGlobalVisualizersPanel extends RDXPanel implements RDXRenderable
 
    public void renderImGuiWidgets()
    {
+      // This window is important, so we make sure it is showing on launch.
+      // Unfortunately there's an ImGui issue where the docked windows show
+      // the alphabetically last window on start instead of the one the user
+      // had open when the configuration was saved.
+      if (firstFrame)
+      {
+         firstFrame = false;
+
+         startedTimer = true;
+         focusTimer.reset();
+      }
+      if (startedTimer && focusTimer.isExpired(1.0))
+      {
+         startedTimer = false;
+         ImGui.setWindowFocus(WINDOW_NAME);
+      }
+
       for (RDXVisualizer visualizer : visualizers)
       {
          visualizer.renderImGuiWidgets();
