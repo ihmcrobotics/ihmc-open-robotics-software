426153a2ec8de0f04ac004ece557285ec2e7b0c0
Duncan Calvert <dcalvert@ihmc.us>
2023-09-04 10:42:37 -0500

    No need to create a registry for archived behaviors.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/BehaviorRegistry.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/BehaviorRegistry.java
@@ -1,10 +1,6 @@
 package us.ihmc.behaviors;
 
-import us.ihmc.behaviors.exploreArea.ExploreAreaBehavior;
-import us.ihmc.behaviors.fancyPoses.FancyPosesBehavior;
 import us.ihmc.behaviors.lookAndStep.LookAndStepBehavior;
-import us.ihmc.behaviors.navigation.NavigationBehavior;
-import us.ihmc.behaviors.patrol.PatrolBehavior;
 import us.ihmc.behaviors.stairs.TraverseStairsBehavior;
 
 import java.util.LinkedHashSet;
@@ -17,17 +13,10 @@ import java.util.LinkedHashSet;
 public class BehaviorRegistry
 {
    public static final BehaviorRegistry DEFAULT_BEHAVIORS = new BehaviorRegistry(LookAndStepBehavior.DEFINITION);
-   public static final BehaviorRegistry ARCHIVED_BEHAVIORS = new BehaviorRegistry(ExploreAreaBehavior.DEFINITION);
    static
    {
       DEFAULT_BEHAVIORS.register(LookAndStepBehavior.DEFINITION);
       DEFAULT_BEHAVIORS.register(TraverseStairsBehavior.DEFINITION);
-
-      ARCHIVED_BEHAVIORS.register(StepInPlaceBehavior.DEFINITION);
-      ARCHIVED_BEHAVIORS.register(PatrolBehavior.DEFINITION);
-      ARCHIVED_BEHAVIORS.register(FancyPosesBehavior.DEFINITION);
-      ARCHIVED_BEHAVIORS.register(ExploreAreaBehavior.DEFINITION);
-      ARCHIVED_BEHAVIORS.register(NavigationBehavior.DEFINITION);
    }
 
    private BehaviorDefinition highestLevelNode;
