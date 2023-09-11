24cf6979d6ebd972b34b6ccfcb2733dc7c5b499c
Dexton Anderson <danderson@ihmc.org>
2023-09-11 16:49:51 -0500

    ZEDImage2CenterPose.py -> zed2_centerpose_node.py

--- ihmc-perception/src/centerpose-ros2/zed2/run_zed2_centerpose_node.sh
+++ ihmc-perception/src/centerpose-ros2/zed2/run_zed2_centerpose_node.sh
@@ -6,10 +6,10 @@ echo "Setting up workspace..."
 export ROS_DOMAIN_ID=80
 echo "Setting ROS_DOMAIN_ID to $ROS_DOMAIN_ID"
 
-cp /root/centerpose-ros2/zed2/ZEDImage2CenterPose.py /root/centerpose-install/src/
+cp /root/centerpose-ros2/zed2/zed2_centerpose_node.py /root/centerpose-install/src/
 cd /root/centerpose-install
 
 # Source the IHMC ROS2 workspace to bring in all the custom IHMC interfaces
 source /root/ihmc_ros2_ws/install/setup.bash
 
-python3 src/ZEDImage2CenterPose.py
+python3 src/zed2_centerpose_node.py
