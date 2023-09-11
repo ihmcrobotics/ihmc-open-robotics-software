00115d8a731f84b54b61e94a90994890e3b3c144
Dexton Anderson <danderson@ihmc.org>
2023-09-11 17:41:00 -0500

    Share ROS_DOMAIN_ID with host in centerpose zed2 node

--- ihmc-perception/src/centerpose-ros2/run_zed2_centerpose_node_docker.sh
+++ ihmc-perception/src/centerpose-ros2/run_zed2_centerpose_node_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bash download_pretrained_models.sh
-docker run -it --network=host --rm --runtime=nvidia --gpus all -v centerpose-cache:/root/.cache --mount type=bind,source=.,target=/root/centerpose-ros2 centerpose-ros2:0.0.1 bash /root/centerpose-ros2/zed2/run_zed2_centerpose_node.sh
+docker run -it --network=host --rm -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID --runtime=nvidia --gpus all -v centerpose-cache:/root/.cache --mount type=bind,source=.,target=/root/centerpose-ros2 centerpose-ros2:0.0.1 bash /root/centerpose-ros2/zed2/run_zed2_centerpose_node.sh
