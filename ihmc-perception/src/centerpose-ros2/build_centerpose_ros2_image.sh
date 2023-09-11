0a78dee81d5e7d079befa65812d82e224cd6839e
Dexton Anderson <danderson@ihmc.org>
2023-09-11 17:30:41 -0500

    Use docker commit to compile centerpose with GPU support

--- ihmc-perception/src/centerpose-ros2/build_centerpose_ros2_image.sh
+++ ihmc-perception/src/centerpose-ros2/build_centerpose_ros2_image.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 docker build -t centerpose-ros2:0.0.1 .
+
+random_id=$(uuidgen | cut -c-8)
+
+# Build centerpose with GPUs
+docker run -it --name "centerpose-build-$random_id" --network=host --runtime=nvidia --gpus all --mount type=bind,source=.,target=/root/centerpose-ros2 centerpose-ros2:0.0.1 bash compile_centerpose.sh
+
+# Commit
+docker commit "centerpose-build-$random_id" centerpose-ros2:0.0.1
