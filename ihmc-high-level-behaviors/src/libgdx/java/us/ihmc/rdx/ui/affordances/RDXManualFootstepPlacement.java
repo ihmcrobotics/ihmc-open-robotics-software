eff27dfa711f8bfbbabb6d08a4b534332cb87bcf
Nick Kitchel <nkitchel@ihmc.org>
2023-09-12 08:33:58 -0500

    Used method to check the previous footstep on the opposite side, make that method public so other classes could use it

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXManualFootstepPlacement.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXManualFootstepPlacement.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.utils.Pool;
 import imgui.flag.ImGuiMouseButton;
 import imgui.ImGui;
 import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
+import us.ihmc.behaviors.tools.walkingController.ControllerStatusTracker;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
@@ -338,20 +339,10 @@ public class RDXManualFootstepPlacement implements RenderableProvider
    private boolean isFootstepBeingPlacedReachable()
    {
       FramePose3D previousFootstepPose = new FramePose3D();
-      // Find the previous footstep of the opposite side of the footstep being placed
-      int i = footstepPlan.getNumberOfFootsteps() - 1;
-      while (i >= 0 && footstepPlan.getFootsteps().get(i).getFootstepSide() == footstepBeingPlaced.getFootstepSide())
-      {
-         --i;
-      }
-      if (i >= 0)
-      {
-         previousFootstepPose.setIncludingFrame(footstepPlan.getFootsteps().get(i).getFootPose());
-      }
-      else
-      {
-         previousFootstepPose.setFromReferenceFrame(syncedRobot.getReferenceFrames().getSoleFrame(currentFootStepSide.getOppositeSide()));
-      }
+
+      previousFootstepPose.set(stepChecker.getPreviousFootstepOnOppositeSide(footstepPlan.getFootsteps(),
+                                                                             footstepPlan.getNumberOfFootsteps(),
+                                                                             footstepBeingPlaced.getFootstepSide()));
 
       boolean isReachable = footstepBeingPlaced.getFootPose().getPositionDistance(previousFootstepPose) < MAX_DISTANCE_MULTIPLIER * footstepPlannerParameters.getMaximumStepReach();
       isReachable &= footstepBeingPlaced.getFootPose().getZ() - previousFootstepPose.getZ() < MAX_DISTANCE_MULTIPLIER * footstepPlannerParameters.getMaxStepZ();
