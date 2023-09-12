99690b8a5084d0b53395d0f4fe1fe4baf8784e0a
Dexton Anderson <danderson@ihmc.org>
2023-09-12 16:40:59 -0500

    Fix centerpose model download urls

--- ihmc-perception/src/centerpose-ros2/download_pretrained_models.sh
+++ ihmc-perception/src/centerpose-ros2/download_pretrained_models.sh
@@ -12,11 +12,11 @@ wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1
 # book
 wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=16NTsoJZIeZpeVFTL_nxF5nhWISZEoEDo' -O models/book_v1_140.pth
 # bottle
-wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PaNVJgRkyQjnNAVxOwQ9Ho3REAsmgtzf' -O models/bottle_v1_sym_12_140.pth
+wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-vauJwkztE47__m90wyYx-5grXuy4ix4' -O models/bottle_v1_sym_12_140.pth
 # camera
-wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PaNVJgRkyQjnNAVxOwQ9Ho3REAsmgtzf' -O models/camera_v1_140.pth
+wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1_CkUdhr_-Xr2RP5NihB2yLSlzS1_KLRT' -O models/camera_v1_140.pth
 # cereal box
-wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PaNVJgRkyQjnNAVxOwQ9Ho3REAsmgtzf' -O models/cereal_box_v1_140.pth
+wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BhHnExf-fAI77XF6IGaIwnsK5r5OT1bF' -O models/cereal_box_v1_140.pth
 # chair
 wget -nc --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PaNVJgRkyQjnNAVxOwQ9Ho3REAsmgtzf' -O models/chair_v1_140.pth
 # cup
