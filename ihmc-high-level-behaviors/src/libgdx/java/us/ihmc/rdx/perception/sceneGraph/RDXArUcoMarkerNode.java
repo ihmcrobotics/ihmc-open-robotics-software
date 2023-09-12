1bc1ef3ca740d56f9b494fecd4452eabb1b84977
Duncan Calvert <dcalvert@ihmc.us>
2023-09-12 14:36:07 -0500

    Break out the UI nodes functionality into their classes.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXArUcoMarkerNode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXArUcoMarkerNode.java
@@ -1,16 +1,29 @@
 package us.ihmc.rdx.perception.sceneGraph;
 
 import us.ihmc.perception.sceneGraph.arUco.ArUcoMarkerNode;
+import us.ihmc.rdx.imgui.ImGuiSliderDoubleWrapper;
 import us.ihmc.rdx.ui.RDX3DPanel;
 
 public class RDXArUcoMarkerNode extends RDXDetectableSceneNode
 {
    private final ArUcoMarkerNode arUcoMarkerNode;
+   private ImGuiSliderDoubleWrapper alphaFilterValueSlider;
 
-   public RDXArUcoMarkerNode(ArUcoMarkerNode arUcoMarkerNode, RDX3DPanel panel3D)
+   public RDXArUcoMarkerNode(ArUcoMarkerNode arUcoMarkerNode)
    {
-      super(arUcoMarkerNode, panel3D);
-
+      super(arUcoMarkerNode);
       this.arUcoMarkerNode = arUcoMarkerNode;
+      alphaFilterValueSlider = new ImGuiSliderDoubleWrapper("Break frequency", "%.2f", 0.2, 5.0,
+                                                            arUcoMarkerNode::getBreakFrequency,
+                                                            arUcoMarkerNode::setBreakFrequency,
+                                                            arUcoMarkerNode::markModifiedByOperator);
+   }
+
+   @Override
+   public void renderImGuiWidgets()
+   {
+      super.renderImGuiWidgets();
+
+      alphaFilterValueSlider.render();
    }
 }
--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXStaticRelativeSceneNode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXStaticRelativeSceneNode.java
@@ -1,16 +1,31 @@
 package us.ihmc.rdx.perception.sceneGraph;
 
+import imgui.ImGui;
 import us.ihmc.perception.sceneGraph.rigidBodies.StaticRelativeSceneNode;
+import us.ihmc.rdx.imgui.ImGuiInputDoubleWrapper;
 import us.ihmc.rdx.ui.RDX3DPanel;
 
 public class RDXStaticRelativeSceneNode extends RDXPredefinedRigidBodySceneNode
 {
    private final StaticRelativeSceneNode staticRelativeSceneNode;
+   private final ImGuiInputDoubleWrapper distanceToDisableTrackingInput;
 
    public RDXStaticRelativeSceneNode(StaticRelativeSceneNode staticRelativeSceneNode, RDX3DPanel panel3D)
    {
       super(staticRelativeSceneNode, panel3D);
-
       this.staticRelativeSceneNode = staticRelativeSceneNode;
+      distanceToDisableTrackingInput = new ImGuiInputDoubleWrapper("Distance to disable tracking", "%.2f", 0.1, 0.5,
+                                                                   staticRelativeSceneNode::getDistanceToDisableTracking,
+                                                                   staticRelativeSceneNode::setDistanceToDisableTracking,
+                                                                   staticRelativeSceneNode::markModifiedByOperator);
+   }
+
+   @Override
+   public void renderImGuiWidgets()
+   {
+      super.renderImGuiWidgets();
+
+      ImGui.text("Current distance: %.2f".formatted(staticRelativeSceneNode.getCurrentDistance()));
+      distanceToDisableTrackingInput.render();
    }
 }
