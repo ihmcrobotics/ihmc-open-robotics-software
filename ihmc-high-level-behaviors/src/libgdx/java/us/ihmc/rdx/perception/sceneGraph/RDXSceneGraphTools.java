f7b4cd477150c110877b742ec0936aa41dc15e7c
Duncan Calvert <dcalvert@ihmc.us>
2023-09-12 18:59:53 -0500

    Implement RDX node creation for all types. Code is compiling!

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXSceneGraphTools.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXSceneGraphTools.java
@@ -1,42 +1,72 @@
 package us.ihmc.rdx.perception.sceneGraph;
 
-import us.ihmc.perception.sceneGraph.DetectableSceneNode;
+import perception_msgs.msg.dds.PredefinedRigidBodySceneNodeMessage;
+import perception_msgs.msg.dds.SceneGraphMessage;
+import us.ihmc.communication.packets.MessageTools;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.perception.sceneGraph.SceneGraph;
 import us.ihmc.perception.sceneGraph.SceneNode;
-import us.ihmc.perception.sceneGraph.arUco.ArUcoMarkerNode;
-import us.ihmc.perception.sceneGraph.rigidBodies.PredefinedRigidBodySceneNode;
-import us.ihmc.perception.sceneGraph.rigidBodies.StaticRelativeSceneNode;
 import us.ihmc.perception.sceneGraph.ros2.ROS2SceneGraphSubscriptionNode;
-import us.ihmc.perception.sceneGraph.ros2.ROS2SceneGraphTools;
 import us.ihmc.rdx.ui.RDX3DPanel;
 
 public class RDXSceneGraphTools
 {
-   public static RDXSceneNode createNodeFromMessage(ROS2SceneGraphSubscriptionNode subscriptionNode, RDX3DPanel panel3D, SceneGraph sceneGraph)
+   public static SceneNode createNodeFromMessage(ROS2SceneGraphSubscriptionNode subscriptionNode, RDX3DPanel panel3D, SceneGraph sceneGraph)
    {
-      SceneNode sceneNode = ROS2SceneGraphTools.createNodeFromMessage(subscriptionNode, sceneGraph);
+      SceneNode sceneNode;
 
-      RDXSceneNode uiSceneNode;
-      if (sceneNode instanceof ArUcoMarkerNode arUcoMarkerNode)
+      byte nodeType = subscriptionNode.getType();
+      long nodeID = subscriptionNode.getSceneNodeMessage().getId();
+      String nodeName = subscriptionNode.getSceneNodeMessage().getNameAsString();
+
+      if (nodeType == SceneGraphMessage.PREDEFINED_RIGID_BODY_NODE_TYPE || nodeType == SceneGraphMessage.STATIC_RELATIVE_NODE_TYPE)
       {
-         uiSceneNode = new RDXArUcoMarkerNode(arUcoMarkerNode);
+         PredefinedRigidBodySceneNodeMessage predefinedRigidBodySceneNodeMessage = subscriptionNode.getPredefinedRigidBodySceneNodeMessage();
+         RigidBodyTransform initialTransformToParent = new RigidBodyTransform();
+         MessageTools.toEuclid(predefinedRigidBodySceneNodeMessage.getInitialTransformToParent(), initialTransformToParent);
+         RigidBodyTransform visualTransformToParent = new RigidBodyTransform();
+         MessageTools.toEuclid(predefinedRigidBodySceneNodeMessage.getVisualTransformToParent(), visualTransformToParent);
+
+         if (nodeType == SceneGraphMessage.STATIC_RELATIVE_NODE_TYPE)
+         {
+            sceneNode = new RDXStaticRelativeSceneNode(nodeID,
+                                                       nodeName,
+                                                       sceneGraph.getIDToNodeMap(),
+                                                       predefinedRigidBodySceneNodeMessage.getInitialParentId(),
+                                                       initialTransformToParent,
+                                                       predefinedRigidBodySceneNodeMessage.getVisualModelFilePathAsString(),
+                                                       visualTransformToParent,
+                                                       subscriptionNode.getStaticRelativeSceneNodeMessage().getDistanceToDisableTracking(),
+                                                       panel3D);
+         }
+         else // PREDEFINED_RIGID_BODY_NODE_TYPE
+         {
+            sceneNode = new RDXPredefinedRigidBodySceneNode(nodeID,
+                                                            nodeName,
+                                                            sceneGraph.getIDToNodeMap(),
+                                                            predefinedRigidBodySceneNodeMessage.getInitialParentId(),
+                                                            initialTransformToParent,
+                                                            predefinedRigidBodySceneNodeMessage.getVisualModelFilePathAsString(),
+                                                            visualTransformToParent,
+                                                            panel3D);
+         }
       }
-      else if (sceneNode instanceof StaticRelativeSceneNode staticRelativeSceneNode)
+      else if (nodeType == SceneGraphMessage.ARUCO_MARKER_NODE_TYPE)
       {
-         uiSceneNode = new RDXStaticRelativeSceneNode(staticRelativeSceneNode, panel3D);
+         sceneNode = new RDXArUcoMarkerNode(nodeID,
+                                            nodeName,
+                                            subscriptionNode.getArUcoMarkerNodeMessage().getMarkerId(),
+                                            subscriptionNode.getArUcoMarkerNodeMessage().getMarkerSize());
       }
-      else if (sceneNode instanceof PredefinedRigidBodySceneNode predefinedRigidBodySceneNode)
+      else if (nodeType == SceneGraphMessage.DETECTABLE_SCENE_NODE_TYPE)
       {
-         uiSceneNode = new RDXPredefinedRigidBodySceneNode(predefinedRigidBodySceneNode, panel3D);
-      }
-      else if (sceneNode instanceof DetectableSceneNode detectableSceneNode)
-      {
-         uiSceneNode = new RDXDetectableSceneNode(detectableSceneNode);
+         sceneNode = new RDXDetectableSceneNode(nodeID, nodeName);
       }
       else
       {
-         uiSceneNode = new RDXSceneNode(sceneNode);
+         sceneNode = new RDXSceneNode(nodeID, nodeName);
       }
-      return uiSceneNode;
+
+      return sceneNode;
    }
 }
