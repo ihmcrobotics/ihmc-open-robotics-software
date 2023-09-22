e528945a836ad350d0a41ed1ddcf340895adee37
Dexton Anderson <danderson@ihmc.org>
2023-09-22 11:19:29 -0500

    ConditionalReferenceFrame implementation/changes

--- ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/referenceFrames/ConditionalReferenceFrameTest.java
+++ ihmc-robotics-toolkit/src/test/java/us/ihmc/robotics/referenceFrames/ConditionalReferenceFrameTest.java
@@ -10,54 +10,90 @@ public class ConditionalReferenceFrameTest
    @Test
    public void testAnNonExistentParentFrame()
    {
+      // Update with a ReferenceFrameLibrary
       ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
       ConditionalReferenceFrame conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", "someParentFrameNameThatDoesNotExist");
       conditionalReferenceFrame.update(referenceFrameLibrary);
-      assertEquals(ConditionalReferenceFrame.NULL_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
+      assertEquals(ConditionalReferenceFrame.INVALID_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
+
+      // Update without a ReferenceFrameLibrary
+      conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", "someParentFrameNameThatDoesNotExist");
+      conditionalReferenceFrame.update();
+      assertEquals(ConditionalReferenceFrame.INVALID_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
    }
 
    @Test
    public void testAnExistentParentFrame()
    {
-      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
       ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
       PoseReferenceFrame frame0 = new PoseReferenceFrame("frame0", worldFrame);
-      referenceFrameLibrary.add(()-> frame0);
+
+      // Update with a ReferenceFrameLibrary
+      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
+      referenceFrameLibrary.add(() -> frame0);
       ConditionalReferenceFrame conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", frame0.getName());
       conditionalReferenceFrame.update(referenceFrameLibrary);
+      assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
+
+      // Update without a ReferenceFrameLibrary
+      conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", frame0.getName());
+      conditionalReferenceFrame.update();
       assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
    }
 
    @Test
    public void testChangingFromAnExistentParentFrameToANonExistentParentFrame()
    {
-      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
       ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
       PoseReferenceFrame frame0 = new PoseReferenceFrame("frame0", worldFrame);
-      referenceFrameLibrary.add(()-> frame0);
+
+      // Update with a ReferenceFrameLibrary
+      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
+      referenceFrameLibrary.add(() -> frame0);
       ConditionalReferenceFrame conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", frame0.getName());
       conditionalReferenceFrame.update(referenceFrameLibrary);
       assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
       conditionalReferenceFrame.setParentFrameName("frame1");
       conditionalReferenceFrame.update(referenceFrameLibrary);
-      assertEquals(ConditionalReferenceFrame.NULL_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
+      assertEquals(ConditionalReferenceFrame.INVALID_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
+
+      // Update without a ReferenceFrameLibrary
+      conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", frame0.getName());
+      conditionalReferenceFrame.update();
+      assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
+      conditionalReferenceFrame.setParentFrameName("frame1");
+      conditionalReferenceFrame.update();
+      assertEquals(ConditionalReferenceFrame.INVALID_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
    }
 
    @Test
    public void testChangingFromAnExistentParentFrameToANonExistentParentFrameAndBack()
    {
-      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
       ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
       PoseReferenceFrame frame0 = new PoseReferenceFrame("frame0", worldFrame);
-      referenceFrameLibrary.add(()-> frame0);
+
+      // Update with a ReferenceFrameLibrary
+      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
+      referenceFrameLibrary.add(() -> frame0);
       ConditionalReferenceFrame conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", frame0.getName());
       conditionalReferenceFrame.update(referenceFrameLibrary);
       assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
       conditionalReferenceFrame.setParentFrameName("frame1");
       conditionalReferenceFrame.update(referenceFrameLibrary);
-      assertEquals(ConditionalReferenceFrame.NULL_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
+      assertEquals(ConditionalReferenceFrame.INVALID_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
       conditionalReferenceFrame.setParentFrameName("frame0");
       conditionalReferenceFrame.update(referenceFrameLibrary);
+      assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
+
+      // Update without a ReferenceFrameLibrary
+      conditionalReferenceFrame = new ConditionalReferenceFrame("testFrame", frame0.getName());
+      conditionalReferenceFrame.update();
+      assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
+      conditionalReferenceFrame.setParentFrameName("frame1");
+      conditionalReferenceFrame.update();
+      assertEquals(ConditionalReferenceFrame.INVALID_FRAME.getName(), conditionalReferenceFrame.get().getParent().getName());
+      conditionalReferenceFrame.setParentFrameName("frame0");
+      conditionalReferenceFrame.update();
       assertEquals(frame0.getName(), conditionalReferenceFrame.get().getParent().getName());
    }
 }
