32f88c2a8996b5aaf764cfba0db22bbb6778f213
Dexton Anderson <danderson@ihmc.org>
2023-09-25 10:58:31 -0500

    ArmJoinAnglesAction always has trajectoryDuration

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/ArmJointAnglesActionData.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/ArmJointAnglesActionData.java
@@ -27,9 +27,9 @@ public class ArmJointAnglesActionData implements BehaviorActionData
       jsonNode.put("description", description);
       jsonNode.put("preset", preset == null ? CUSTOM_ANGLES_NAME : preset.name());
       jsonNode.put("side", side.getLowerCaseName());
+      jsonNode.put("trajectoryDuration", trajectoryDuration);
       if (preset == null)
       {
-         jsonNode.put("trajectoryDuration", trajectoryDuration);
          for (int i = 0; i < NUMBER_OF_JOINTS; i++)
          {
             jsonNode.put("j" + i, jointAngles[i]);
@@ -44,9 +44,9 @@ public class ArmJointAnglesActionData implements BehaviorActionData
       String presetName = jsonNode.get("preset").textValue();
       preset = presetName.equals(CUSTOM_ANGLES_NAME) ? null : PresetArmConfiguration.valueOf(presetName);
       side = RobotSide.getSideFromString(jsonNode.get("side").asText());
+      trajectoryDuration = jsonNode.get("trajectoryDuration").asDouble();
       if (preset == null)
       {
-         trajectoryDuration = jsonNode.get("trajectoryDuration").asDouble();
          for (int i = 0; i < NUMBER_OF_JOINTS; i++)
          {
             jointAngles[i] = jsonNode.get("j" + i).asDouble();
