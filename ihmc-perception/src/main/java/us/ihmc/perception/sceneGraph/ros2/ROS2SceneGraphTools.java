3fe30740cf872e7508545a15342df422a873ae7e
Duncan Calvert <dcalvert@ihmc.us>
2023-09-25 13:44:31 -0500

    Remove ROS2 from type in places that don't need it.

--- ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/ros2/ROS2SceneGraphTools.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/ros2/ROS2SceneGraphTools.java
@@ -5,6 +5,7 @@ import perception_msgs.msg.dds.SceneGraphMessage;
 import us.ihmc.communication.packets.MessageTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.perception.sceneGraph.DetectableSceneNode;
+import us.ihmc.perception.sceneGraph.SceneGraph;
 import us.ihmc.perception.sceneGraph.SceneNode;
 import us.ihmc.perception.sceneGraph.arUco.ArUcoMarkerNode;
 import us.ihmc.perception.sceneGraph.rigidBodies.PredefinedRigidBodySceneNode;
@@ -12,7 +13,7 @@ import us.ihmc.perception.sceneGraph.rigidBodies.StaticRelativeSceneNode;
 
 public class ROS2SceneGraphTools
 {
-   public static SceneNode createNodeFromMessage(ROS2SceneGraphSubscriptionNode subscriptionNode, ROS2SceneGraph sceneGraph)
+   public static SceneNode createNodeFromMessage(ROS2SceneGraphSubscriptionNode subscriptionNode, SceneGraph sceneGraph)
    {
       SceneNode sceneNode;
 
