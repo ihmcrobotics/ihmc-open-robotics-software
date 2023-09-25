bafa16ed3c29e91a3f19f607e8da659b5f943f00
Duncan Calvert <dcalvert@ihmc.us>
2023-09-25 11:21:58 -0500

    Test moving a child by moving it's parent.

--- ihmc-perception/src/test/java/us/ihmc/perception/sceneGraph/SceneGraphTest.java
+++ ihmc-perception/src/test/java/us/ihmc/perception/sceneGraph/SceneGraphTest.java
@@ -2,10 +2,14 @@ package us.ihmc.perception.sceneGraph;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.perception.sceneGraph.modification.SceneGraphNodeAddition;
+import us.ihmc.robotics.EuclidCoreTestMissingTools;
 
 public class SceneGraphTest
 {
+   private static final double EPSILON = 1.0e-15;
+
    @Test
    public void testBasicOperations()
    {
@@ -50,5 +54,47 @@ public class SceneGraphTest
       Assertions.assertEquals(4, sceneGraph.getNamesToNodesMap().size());
       Assertions.assertEquals(4, sceneGraph.getIDToNodeMap().size());
       Assertions.assertEquals(0, sceneGraph.getArUcoMarkerIDToNodeMap().size());
+   }
+
+   @Test
+   public void testMovingNodes()
+   {
+      SceneGraph sceneGraph = new SceneGraph();
+      SceneNode child0 = new SceneNode(sceneGraph.getNextID().getAndIncrement(), "Child0");
+      SceneNode child1 = new SceneNode(sceneGraph.getNextID().getAndIncrement(), "Child1");
+      SceneNode child1child0 = new SceneNode(sceneGraph.getNextID().getAndIncrement(), "Child1Child0");
+      sceneGraph.modifyTree(modificationQueue ->
+      {
+         modificationQueue.accept(new SceneGraphNodeAddition(child0, sceneGraph.getRootNode()));
+         modificationQueue.accept(new SceneGraphNodeAddition(child1, sceneGraph.getRootNode()));
+         modificationQueue.accept(new SceneGraphNodeAddition(child1child0, child1));
+      });
+
+      child1child0.getModifiableNodeFrame().update(transformToParent -> transformToParent.getTranslation().set(5.0, 2.0, 1.0));
+
+      System.out.println(EuclidCoreTestMissingTools.toStringFullPrecision(child1child0.getNodeFrame().getTransformToWorldFrame()));
+      EuclidCoreTestTools.assertEquals(EuclidCoreTestMissingTools.newRigidBodyTransformFromString("""
+                                                            1.000000000000000000 0.000000000000000000 0.000000000000000000 | 5.000000000000000000
+                                                            0.000000000000000000 1.000000000000000000 0.000000000000000000 | 2.000000000000000000
+                                                            0.000000000000000000 0.000000000000000000 1.000000000000000000 | 1.000000000000000000
+                                                            0.000000000000000000 0.000000000000000000 0.000000000000000000 | 1.000000000000000000
+                                                                  """), child1child0.getNodeFrame().getTransformToWorldFrame(), EPSILON);
+
+      child1.getModifiableNodeFrame().update(transformToParent -> transformToParent.getTranslation().set(-2.0, 3.0, 0.5));
+
+      System.out.println(EuclidCoreTestMissingTools.toStringFullPrecision(child1.getNodeFrame().getTransformToWorldFrame()));
+      EuclidCoreTestTools.assertEquals(EuclidCoreTestMissingTools.newRigidBodyTransformFromString(""" 
+                                                            1.000000000000000000 0.000000000000000000 0.000000000000000000 | -2.000000000000000000
+                                                            0.000000000000000000 1.000000000000000000 0.000000000000000000 | 3.000000000000000000
+                                                            0.000000000000000000 0.000000000000000000 1.000000000000000000 | 0.500000000000000000
+                                                            0.000000000000000000 0.000000000000000000 0.000000000000000000 | 1.000000000000000000
+                                                                  """), child1.getNodeFrame().getTransformToWorldFrame(), EPSILON);
+      System.out.println(EuclidCoreTestMissingTools.toStringFullPrecision(child1child0.getNodeFrame().getTransformToWorldFrame()));
+      EuclidCoreTestTools.assertEquals(EuclidCoreTestMissingTools.newRigidBodyTransformFromString(""" 
+                                                            1.000000000000000000 0.000000000000000000 0.000000000000000000 | 3.000000000000000000
+                                                            0.000000000000000000 1.000000000000000000 0.000000000000000000 | 5.000000000000000000
+                                                            0.000000000000000000 0.000000000000000000 1.000000000000000000 | 1.500000000000000000
+                                                            0.000000000000000000 0.000000000000000000 0.000000000000000000 | 1.000000000000000000
+                                                                  """), child1child0.getNodeFrame().getTransformToWorldFrame(), EPSILON);
    }
 }
