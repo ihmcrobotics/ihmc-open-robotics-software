91e68c45474d7f06c4d2e9f4b6c891aff88c9fae
Sylvain Bertrand <sbertrand@ihmc.org>
2023-09-25 17:40:44 -0500

    :arrow_up: scs2 17-0.16.3

--- ihmc-common-walking-control-modules/build.gradle.kts
+++ ihmc-common-walking-control-modules/build.gradle.kts
@@ -47,7 +47,7 @@ testDependencies {
 visualizersDependencies {
    api(ihmc.sourceSetProject("main"))
    api("us.ihmc:simulation-construction-set:0.24.1")
-   api("us.ihmc:scs2-simulation-construction-set:17-0.16.2")
+   api("us.ihmc:scs2-simulation-construction-set:17-0.16.3")
 
    var javaFXVersion = "17.0.2"
    api(ihmc.javaFXModule("base", javaFXVersion))
