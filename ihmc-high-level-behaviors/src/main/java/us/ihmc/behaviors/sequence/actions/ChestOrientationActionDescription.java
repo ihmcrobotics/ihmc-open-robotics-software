d43ba092947f7a8f3116c2d426d0d6dc90424a29
Duncan Calvert <dcalvert@ihmc.us>
2023-09-27 14:42:25 -0500

    Fix up ChestOrientationActionDescription.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/ChestOrientationActionDescription.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/ChestOrientationActionDescription.java
@@ -6,50 +6,24 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import us.ihmc.behaviors.sequence.FrameBasedBehaviorActionDescription;
 import us.ihmc.communication.packets.MessageTools;
 import us.ihmc.euclid.matrix.interfaces.RotationMatrixBasics;
-import us.ihmc.robotics.referenceFrames.ModifiableReferenceFrame;
-import us.ihmc.robotics.referenceFrames.ReferenceFrameLibrary;
 import us.ihmc.tools.io.JSONTools;
 
-public class ChestOrientationActionDescription implements FrameBasedBehaviorActionDescription
+public class ChestOrientationActionDescription extends FrameBasedBehaviorActionDescription
 {
    private String description = "Chest orientation";
    private double trajectoryDuration = 4.0;
-   private ReferenceFrameLibrary referenceFrameLibrary;
-   private final ModifiableReferenceFrame chestInteractableReferenceFrame = new ModifiableReferenceFrame(ReferenceFrame.getWorldFrame());
    private boolean executeWitNextAction = false;
    private boolean holdPoseInWorldLater = false;
-
-   @Override
-   public String getDescription()
-   {
-      return description;
-   }
-
-   @Override
-   public void setDescription(String description)
-   {
-      this.description = description;
-   }
-
-   public double getTrajectoryDuration()
-   {
-      return trajectoryDuration;
-   }
-
-   public void setTrajectoryDuration(double trajectoryDuration)
-   {
-      this.trajectoryDuration = trajectoryDuration;
-   }
 
    @Override
    public void saveToFile(ObjectNode jsonNode)
    {
       jsonNode.put("description", description);
-      jsonNode.put("trajectoryDuration", trajectoryDuration);
       jsonNode.put("parentFrame", getConditionalReferenceFrame().getConditionallyValidParentFrameName());
+      jsonNode.put("trajectoryDuration", trajectoryDuration);
+      JSONTools.toJSON(jsonNode, getTransformToParent());
       jsonNode.put("executeWithNextAction", executeWitNextAction);
       jsonNode.put("holdPoseInWorldLater", holdPoseInWorldLater);
-      JSONTools.toJSON(jsonNode, chestInteractableReferenceFrame.getTransformToParent());
    }
 
    @Override
@@ -58,18 +32,17 @@ public class ChestOrientationActionDescription implements FrameBasedBehaviorActi
       description = jsonNode.get("description").textValue();
       trajectoryDuration = jsonNode.get("trajectoryDuration").asDouble();
       getConditionalReferenceFrame().setParentFrameName(jsonNode.get("parentFrame").textValue());
-      chestInteractableReferenceFrame.changeParentFrame(referenceFrameLibrary.findFrameByNameOrWorld(jsonNode.get("parentFrame").asText()));
-      chestInteractableReferenceFrame.update(transformToParent -> JSONTools.toEuclid(jsonNode, transformToParent));
+      JSONTools.toEuclid(jsonNode, getTransformToParent());
       executeWitNextAction = jsonNode.get("executeWithNextAction").asBoolean();
       holdPoseInWorldLater = jsonNode.get("holdPoseInWorldLater").asBoolean();
    }
 
    public void toMessage(BodyPartPoseActionDescriptionMessage message)
    {
-      message.setTrajectoryDuration(trajectoryDuration);
       message.getParentFrame().resetQuick();
       message.getParentFrame().add(getConditionalReferenceFrame().getConditionallyValidParentFrameName());
       MessageTools.toMessage(getTransformToParent(), message.getTransformToParent());
+      message.setTrajectoryDuration(trajectoryDuration);
       message.setExecuteWithNextAction(executeWitNextAction);
       message.setHoldPoseInWorld(holdPoseInWorldLater);
    }
@@ -78,8 +51,6 @@ public class ChestOrientationActionDescription implements FrameBasedBehaviorActi
    {
       getConditionalReferenceFrame().setParentFrameName(message.getParentFrame().getString(0));
       MessageTools.toEuclid(message.getTransformToParent(), getTransformToParent());
-      chestInteractableReferenceFrame.changeParentFrame(referenceFrameLibrary.findFrameByNameOrWorld(message.getParentFrame().getString(0)));
-      chestInteractableReferenceFrame.update(transformToParent -> MessageTools.toEuclid(message.getTransformToParent(), transformToParent));
       trajectoryDuration = message.getTrajectoryDuration();
       executeWitNextAction = message.getExecuteWithNextAction();
       holdPoseInWorldLater = message.getHoldPoseInWorld();
@@ -138,36 +109,6 @@ public class ChestOrientationActionDescription implements FrameBasedBehaviorActi
       this.holdPoseInWorldLater = holdPoseInWorldLater;
    }
 
-   public ReferenceFrame getParentFrame()
-   {
-      return chestInteractableReferenceFrame.getReferenceFrame().getParent();
-   }
-
-   public ReferenceFrame getReferenceFrame()
-   {
-      return chestInteractableReferenceFrame.getReferenceFrame();
-   }
-
-   public void changeParentFrameWithoutMoving(ReferenceFrame parentFrame)
-   {
-      chestInteractableReferenceFrame.changeParentFrameWithoutMoving(parentFrame);
-   }
-
-   public void changeParentFrame(ReferenceFrame parentFrame)
-   {
-      chestInteractableReferenceFrame.changeParentFrame(parentFrame);
-   }
-
-   public void setTransformToParent(Consumer<RigidBodyTransform> transformToParentConsumer)
-   {
-      chestInteractableReferenceFrame.update(transformToParentConsumer);
-   }
-
-   public RigidBodyTransform getTransformToParent()
-   {
-      return chestInteractableReferenceFrame.getTransformToParent();
-   }
-
    @Override
    public void setDescription(String description)
    {
@@ -178,10 +119,5 @@ public class ChestOrientationActionDescription implements FrameBasedBehaviorActi
    public String getDescription()
    {
       return description;
-   }
-
-   public ReferenceFrameLibrary getReferenceFrameLibrary()
-   {
-      return referenceFrameLibrary;
    }
 }
