ee391b309aeabbbc74847c50970539b3e5f66d34
Duncan Calvert <dcalvert@ihmc.us>
2023-10-02 17:26:31 -0500

    Removing BehaviorActionStateBasics.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/WaitDurationActionState.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/WaitDurationActionState.java
@@ -1,18 +1,10 @@
 package us.ihmc.behaviors.sequence.actions;
 
 import us.ihmc.behaviors.sequence.BehaviorActionState;
-import us.ihmc.behaviors.sequence.BehaviorActionStateBasics;
 
-public class WaitDurationActionState implements BehaviorActionState
+public class WaitDurationActionState extends BehaviorActionState
 {
    private final WaitDurationActionDefinition definition = new WaitDurationActionDefinition();
-   private final BehaviorActionStateBasics stateBasics = new BehaviorActionStateBasics();
-
-   @Override
-   public long getID()
-   {
-      return stateBasics.getID();
-   }
 
    @Override
    public WaitDurationActionDefinition getDefinition()
