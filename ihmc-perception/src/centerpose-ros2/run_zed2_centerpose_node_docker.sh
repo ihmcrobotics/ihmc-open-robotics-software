8d431e8d2fdd3e8226a4685626a253e909f502c4
Dexton Anderson <danderson@ihmc.org>
2023-10-02 16:06:55 -0500

    Remove hard set ros domain id

--- ihmc-perception/src/centerpose-ros2/run_zed2_centerpose_node_docker.sh
+++ ihmc-perception/src/centerpose-ros2/run_zed2_centerpose_node_docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 bash download_pretrained_models.sh
-export ROS_DOMAIN_ID=80
 xhost +si:localuser:root  # allow containers to communicate with X server
 docker run -it --network=host --rm -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --runtime=nvidia --gpus all -v centerpose-cache:/root/.cache --mount type=bind,source=.,target=/root/centerpose-ros2 centerpose-ros2:0.0.1 bash /root/centerpose-ros2/zed2/run_zed2_centerpose_node.sh
 # python3 demo.py --demo /root/centerpose-ros2/zed2/IMG_1132.jpg --arch dlav1_34 --load_model /root/centerpose-ros2/models/cup_mug_v1_140.pth --debug 4
