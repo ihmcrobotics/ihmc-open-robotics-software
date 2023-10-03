d5709e4b5f8984694c127d45a6a658f1d5efb968
Dexton Anderson <danderson@ihmc.org>
2023-10-03 10:56:52 -0500

    Update frames

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/ChestOrientationActionState.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/ChestOrientationActionState.java
@@ -2,11 +2,18 @@ package us.ihmc.behaviors.sequence.actions;
 
 import us.ihmc.behaviors.sequence.BehaviorActionState;
 import us.ihmc.robotics.referenceFrames.DetachableReferenceFrame;
+import us.ihmc.robotics.referenceFrames.ReferenceFrameLibrary;
 
 public class ChestOrientationActionState extends BehaviorActionState
 {
    private final ChestOrientationActionDefinition definition = new ChestOrientationActionDefinition();
    private final DetachableReferenceFrame chestFrame = new DetachableReferenceFrame(definition.getTransformToParent());
+
+   @Override
+   public void update(ReferenceFrameLibrary referenceFrameLibrary)
+   {
+      chestFrame.update(referenceFrameLibrary, definition.getParentFrameName());
+   }
 
    @Override
    public ChestOrientationActionDefinition getDefinition()
--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/WalkActionState.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/WalkActionState.java
@@ -2,11 +2,18 @@ package us.ihmc.behaviors.sequence.actions;
 
 import us.ihmc.behaviors.sequence.BehaviorActionState;
 import us.ihmc.robotics.referenceFrames.DetachableReferenceFrame;
+import us.ihmc.robotics.referenceFrames.ReferenceFrameLibrary;
 
 public class WalkActionState extends BehaviorActionState
 {
    private final WalkActionDefinition definition = new WalkActionDefinition();
    private final DetachableReferenceFrame soleFrame = new DetachableReferenceFrame(definition.getTransformToParent());
+
+   @Override
+   public void update(ReferenceFrameLibrary referenceFrameLibrary)
+   {
+      soleFrame.update(referenceFrameLibrary, definition.getParentFrameName());
+   }
 
    @Override
    public WalkActionDefinition getDefinition()
