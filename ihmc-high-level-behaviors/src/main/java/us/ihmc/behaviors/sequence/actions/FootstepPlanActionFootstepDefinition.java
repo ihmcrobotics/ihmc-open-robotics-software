76bf470a4b1b3333edb2f978322e3d9f8177ca21
Duncan Calvert <dcalvert@ihmc.us>
2023-10-04 18:11:38 -0500

    Redo FootstepPlanFootstep

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/FootstepPlanActionFootstepDefinition.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/FootstepPlanActionFootstepDefinition.java
@@ -3,15 +3,13 @@ package us.ihmc.behaviors.sequence.actions;
 import behavior_msgs.msg.dds.FootstepActionDefinitionMessage;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import us.ihmc.behaviors.sequence.BehaviorActionDefinition;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SidedObject;
 import us.ihmc.tools.io.JSONTools;
 
-public class FootstepActionDefinition implements BehaviorActionDefinition<FootstepActionDefinitionMessage>, SidedObject
+public class FootstepPlanActionFootstepDefinition implements SidedObject
 {
-   private String description = "Footstep";
    private RobotSide side = RobotSide.LEFT;
    private final RigidBodyTransform soleToPlanFrameTransform = new RigidBodyTransform();
 
@@ -31,43 +29,27 @@ public class FootstepActionDefinition implements BehaviorActionDefinition<Footst
       return soleToPlanFrameTransform;
    }
 
-   @Override
    public void saveToFile(ObjectNode jsonNode)
    {
       jsonNode.put("side", side.getLowerCaseName());
       JSONTools.toJSON(jsonNode, soleToPlanFrameTransform);
    }
 
-   @Override
    public void loadFromFile(JsonNode jsonNode)
    {
       side = RobotSide.getSideFromString(jsonNode.get("side").asText());
       JSONTools.toEuclid(jsonNode, soleToPlanFrameTransform);
    }
 
-   @Override
    public void toMessage(FootstepActionDefinitionMessage message)
    {
       message.setRobotSide(side.toByte());
       message.getSolePose().set(soleToPlanFrameTransform);
    }
 
-   @Override
    public void fromMessage(FootstepActionDefinitionMessage message)
    {
       side = RobotSide.fromByte(message.getRobotSide());
       soleToPlanFrameTransform.set(message.getSolePose());
-   }
-
-   @Override
-   public void setDescription(String description)
-   {
-      this.description = description;
-   }
-
-   @Override
-   public String getDescription()
-   {
-      return description;
    }
 }
