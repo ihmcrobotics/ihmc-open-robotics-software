b27347c49f09f66f96d3e59cc0e83f0a38bb220b
Dexton Anderson <danderson@ihmc.org>
2023-10-05 17:03:59 -0500

    Remove RDXSceneNode multi inheritance problem

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXDetectableSceneNode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXDetectableSceneNode.java
@@ -3,38 +3,57 @@ package us.ihmc.rdx.perception.sceneGraph;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import imgui.ImGui;
 import us.ihmc.perception.sceneGraph.DetectableSceneNode;
+import us.ihmc.perception.sceneGraph.SceneGraph;
 import us.ihmc.perception.sceneGraph.modification.SceneGraphModificationQueue;
+import us.ihmc.rdx.imgui.ImGuiEnumPlot;
+import us.ihmc.rdx.imgui.ImGuiTools;
 import us.ihmc.rdx.sceneManager.RDXSceneLevel;
 
 import java.util.Set;
 
-public class RDXDetectableSceneNode extends DetectableSceneNode implements RDXSceneNodeInterface
+public class RDXDetectableSceneNode extends RDXSceneNode
 {
-   private final RDXDetectableSceneNodeBasics detectableSceneNodeBasics;
+   private final DetectableSceneNode detectableSceneNode;
+   private final RDXSceneNode uiSceneNode;
+   private final ImGuiEnumPlot currentlyDetectedPlot;
 
-   public RDXDetectableSceneNode(DetectableSceneNode nodeToCopy)
+   public RDXDetectableSceneNode(DetectableSceneNode detectableSceneNode)
    {
-      super(nodeToCopy.getID(), nodeToCopy.getName());
+      super(detectableSceneNode);
 
-      detectableSceneNodeBasics = new RDXDetectableSceneNodeBasics(this);
+      this.detectableSceneNode = detectableSceneNode;
+      uiSceneNode = new RDXSceneNode(detectableSceneNode);
+      currentlyDetectedPlot = new ImGuiEnumPlot();
    }
 
    @Override
    public void update(SceneGraphModificationQueue modificationQueue)
    {
-      detectableSceneNodeBasics.update();
+      uiSceneNode.update(modificationQueue);
    }
 
    @Override
    public void renderImGuiWidgets()
    {
-      detectableSceneNodeBasics.renderImGuiWidgets();
+      uiSceneNode.renderImGuiWidgets();
+      ImGui.sameLine();
+
+      boolean currentlyDetected = detectableSceneNode.getCurrentlyDetected();
+      currentlyDetectedPlot.setWidgetTextColor(currentlyDetected ? ImGuiTools.GREEN : ImGuiTools.RED);
+      currentlyDetectedPlot.render(currentlyDetected ? 1 : 0, currentlyDetected ? "CURRENTLY DETECTED" : "NOT DETECTED");
+   }
+
+   @Override
+   public void renderRemove(SceneGraphModificationQueue modificationQueue, SceneGraph sceneGraph)
+   {
+      uiSceneNode.renderRemove(modificationQueue, sceneGraph);
    }
 
    @Override
    public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool, Set<RDXSceneLevel> sceneLevels)
    {
-      detectableSceneNodeBasics.getRenderables(renderables, pool, sceneLevels);
+      uiSceneNode.getRenderables(renderables, pool, sceneLevels);
    }
 }
