2877ec99e4f6a03077e19c1fd124fdf3daa9f781
christian <christian.debuys@boardwalkrobotics.com>
2023-10-11 10:54:52 -0500

    Overrides the joint offline privileged configuration default weight instead of replacing it.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/JointPrivilegedConfigurationParameters.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/JointPrivilegedConfigurationParameters.java
@@ -113,6 +113,6 @@ public class JointPrivilegedConfigurationParameters
     */
    public double getDefaultWeight()
    {
-      return 0.5; //default 5.0; //should eventually override this
+      return 5.0; //default 5.0;
    }
 }
