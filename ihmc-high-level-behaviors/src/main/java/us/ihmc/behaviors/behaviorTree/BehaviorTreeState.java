1c8057e5d8288875a6ac1d78daaa56c351fdbfd6
Duncan Calvert <dcalvert@ihmc.us>
2023-10-17 16:46:48 -0500

    Creating the behavior tree CRDT publisher.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/BehaviorTreeState.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/BehaviorTreeState.java
@@ -1,5 +1,7 @@
 package us.ihmc.behaviors.behaviorTree;
 
+import org.apache.commons.lang3.mutable.MutableLong;
+
 /**
  * This is the state related functionality of a behavior tree,
  * which would live on the UI side and the robot side.
@@ -9,10 +11,21 @@ package us.ihmc.behaviors.behaviorTree;
  */
 public class BehaviorTreeState
 {
+   private final MutableLong nextID = new MutableLong(1); // Starts at 1 because root node is created automatically
    private final BehaviorTreeRootNode rootNode = new BehaviorTreeRootNode();
 
    public BehaviorTreeState()
    {
 
+   }
+
+   public BehaviorTreeRootNode getRootNode()
+   {
+      return rootNode;
+   }
+
+   public MutableLong getNextID()
+   {
+      return nextID;
    }
 }
