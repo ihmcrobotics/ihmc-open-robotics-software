7fbcfd9e2ad61a368905f94776e0d692102703a2
Tomasz Bialek <tbialek@ihmc.org>
2023-10-18 17:15:11 -0500

    Blackfly in progress

---
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/colorVision/BlackflyImageRetriever.java
@@ -0,0 +1,18 @@
+/*
+// TODO: finish writing retriever
+package us.ihmc.avatar.colorVision;
+
+import us.ihmc.perception.spinnaker.SpinnakerBlackfly;
+import us.ihmc.perception.spinnaker.SpinnakerBlackflyManager;
+
+public class BlackflyImageRetriever
+{
+   private final SpinnakerBlackfly blackfly;
+
+   public BlackflyImageRetriever(String serialNumber, SpinnakerBlackflyManager blackflyManager)
+   {
+      blackflyManager = new SpinnakerBlackflyManager();
+      blackfly = new
+   }
+}
+*/
