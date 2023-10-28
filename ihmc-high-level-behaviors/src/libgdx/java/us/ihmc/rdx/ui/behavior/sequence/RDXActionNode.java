17a54096d0983b53e609b59de70c0ca72b2c264a
Duncan Calvert <dcalvert@ihmc.us>
2023-10-28 17:13:55 -0500

    Get action nodes rendering in RDX while syncing.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/behavior/sequence/RDXActionNode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/behavior/sequence/RDXActionNode.java
@@ -1,8 +1,5 @@
 package us.ihmc.rdx.ui.behavior.sequence;
 
-import com.badlogic.gdx.graphics.g3d.Renderable;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.Pool;
 import imgui.ImGui;
 import imgui.type.ImBoolean;
 import imgui.type.ImString;
@@ -10,9 +7,7 @@ import us.ihmc.behaviors.sequence.ActionNodeDefinition;
 import us.ihmc.behaviors.sequence.ActionNodeState;
 import us.ihmc.rdx.imgui.ImGuiTools;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
-import us.ihmc.rdx.input.ImGui3DViewInput;
 import us.ihmc.rdx.ui.behavior.tree.RDXBehaviorTreeNode;
-import us.ihmc.rdx.vr.RDXVRContext;
 
 /**
  * The UI representation of a robot behavior action. It provides a base
@@ -32,6 +27,7 @@ public abstract class RDXActionNode<S extends ActionNodeState<D>,
 
    }
 
+   @Override
    public void update()
    {
       super.update();
@@ -39,26 +35,7 @@ public abstract class RDXActionNode<S extends ActionNodeState<D>,
       getState().update();
    }
 
-   public void calculateVRPick(RDXVRContext vrContext)
-   {
-
-   }
-
-   public void processVRInput(RDXVRContext vrContext)
-   {
-
-   }
-
-   public void calculate3DViewPick(ImGui3DViewInput input)
-   {
-
-   }
-
-   public void process3DViewInput(ImGui3DViewInput input)
-   {
-
-   }
-
+   @Override
    public void renderImGuiWidgets()
    {
       if (expanded.get())
@@ -72,11 +49,6 @@ public abstract class RDXActionNode<S extends ActionNodeState<D>,
    }
 
    protected void renderImGuiWidgetsInternal()
-   {
-
-   }
-
-   public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
    {
 
    }
