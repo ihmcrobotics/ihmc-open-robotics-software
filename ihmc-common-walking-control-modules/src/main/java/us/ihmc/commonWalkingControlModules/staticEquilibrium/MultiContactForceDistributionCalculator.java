adea2a07be1c2caac6dc57184c69a4a6fa163e6c
smccrory <smccrory@ihmc.us>
2023-11-08 15:56:59 -0600

    Setting up visualizer for new support region api

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/staticEquilibrium/MultiContactForceDistributionCalculator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/staticEquilibrium/MultiContactForceDistributionCalculator.java
@@ -46,83 +46,84 @@ public class MultiContactForceDistributionCalculator
     */
    public boolean solve(WholeBodyContactState input)
    {
-      clear();
-
-      double mg = 9.81 * input.getRobotMass();
-
-      int rhoSize = WholeBodyContactState.numberOfBasisVectors * input.getNumberOfContactPoints();
-      int decisionVariables = rhoSize;
-
-      rho.reshape(rhoSize, 1);
-      Aeq.reshape(staticEquilibriumConstraints, decisionVariables);
-      beq.reshape(staticEquilibriumConstraints, 1);
-      Ain.reshape(rhoSize + 2 * input.getNumberOfJoints(), rhoSize);
-      bin.reshape(rhoSize + 2 * input.getNumberOfJoints(), 1);
-      quadraticCost.reshape(rhoSize, rhoSize);
-      linearCost.reshape(rhoSize, 1);
-
-      CommonOps_DDRM.setIdentity(quadraticCost);
-      CommonOps_DDRM.fill(beq, 0.0);
-
-      for (int i = 0; i < rhoSize; i++)
-      { // Constraint for rho to be positive
-         Ain.set(i, i, -1.0);
-      }
-
-      DMatrixRMaj constraintUpperBound = input.getConstraintUpperBound();
-      DMatrixRMaj constraintLowerBound = input.getConstraintLowerBound();
-      DMatrixRMaj graspMatrixJacobianTranspose = input.getGraspMatrixJacobianTranspose();
-
-      MatrixTools.setMatrixBlock(Ain, rhoSize, 0, graspMatrixJacobianTranspose, 0, 0, graspMatrixJacobianTranspose.getNumRows(), graspMatrixJacobianTranspose.getNumCols(), 1.0);
-      MatrixTools.setMatrixBlock(bin, rhoSize, 0, constraintUpperBound, 0, 0, constraintUpperBound.getNumRows(), constraintUpperBound.getNumCols(), 1.0);
-
-      MatrixTools.setMatrixBlock(Ain, rhoSize + graspMatrixJacobianTranspose.getNumRows(), 0, graspMatrixJacobianTranspose, 0, 0, graspMatrixJacobianTranspose.getNumRows(), graspMatrixJacobianTranspose.getNumCols(), -1.0);
-      MatrixTools.setMatrixBlock(bin, rhoSize + graspMatrixJacobianTranspose.getNumRows(), 0, constraintLowerBound, 0, 0, constraintLowerBound.getNumRows(), constraintLowerBound.getNumCols(), -1.0);
-
-      for (int contactPointIndex = 0; contactPointIndex < input.getNumberOfContactPoints(); contactPointIndex++)
-      {
-         contactPointPosition.setToZero(input.getContactFrame(contactPointIndex));
-         contactPointPosition.changeFrame(ReferenceFrame.getWorldFrame());
-
-         for (int basisVectorIndex = 0; basisVectorIndex < WholeBodyContactState.numberOfBasisVectors; basisVectorIndex++)
-         {
-            FrameVector3D basisVector = input.getBasisVector(contactPointIndex, basisVectorIndex);
-            int column = basisVectorsPerContactPoint * contactPointIndex + basisVectorIndex;
-
-            Aeq.set(0, column, basisVector.getX());
-            Aeq.set(1, column, basisVector.getY());
-            Aeq.set(2, column, basisVector.getZ());
-
-            // x-component of cross product
-            double xMomentScale = contactPointPosition.getY() * basisVector.getZ() - contactPointPosition.getZ() * basisVector.getY();
-            Aeq.set(3, column, xMomentScale);
-
-            // y-component of cross product
-            double yMomentScale = contactPointPosition.getZ() * basisVector.getX() - contactPointPosition.getX() * basisVector.getZ();
-            Aeq.set(4, column, yMomentScale);
-
-            // z-component of cross product
-            double zMomentScale = contactPointPosition.getX() * basisVector.getY() - contactPointPosition.getY() * basisVector.getX();
-            Aeq.set(5, column, zMomentScale);
-         }
-      }
-
-      FramePoint3DReadOnly centerOfMass = input.getCenterOfMass();
-      beq.set(2, 0, mg);
-      beq.set(3, 0, mg * centerOfMass.getY());
-      beq.set(4, 0, -mg * centerOfMass.getX());
-
-      qpSolver.clear();
-      qpSolver.resetActiveSet();
-
-      qpSolver.setMaxNumberOfIterations(500);
-      qpSolver.setConvergenceThreshold(1e-9);
-      qpSolver.setQuadraticCostFunction(quadraticCost, linearCost, 0.0);
-      qpSolver.setLinearEqualityConstraints(Aeq, beq);
-      qpSolver.setLinearInequalityConstraints(Ain, bin);
-
-      qpSolver.solve(rho);
-      return !MatrixTools.containsNaN(rho);
+//      clear();
+//
+//      double mg = 9.81 * input.getRobotMass();
+//
+//      int rhoSize = WholeBodyContactState.numberOfBasisVectors * input.getNumberOfContactPoints();
+//      int decisionVariables = rhoSize;
+//
+//      rho.reshape(rhoSize, 1);
+//      Aeq.reshape(staticEquilibriumConstraints, decisionVariables);
+//      beq.reshape(staticEquilibriumConstraints, 1);
+//      Ain.reshape(rhoSize + 2 * input.getNumberOfJoints(), rhoSize);
+//      bin.reshape(rhoSize + 2 * input.getNumberOfJoints(), 1);
+//      quadraticCost.reshape(rhoSize, rhoSize);
+//      linearCost.reshape(rhoSize, 1);
+//
+//      CommonOps_DDRM.setIdentity(quadraticCost);
+//      CommonOps_DDRM.fill(beq, 0.0);
+//
+//      for (int i = 0; i < rhoSize; i++)
+//      { // Constraint for rho to be positive
+//         Ain.set(i, i, -1.0);
+//      }
+//
+//      DMatrixRMaj constraintUpperBound = input.getConstraintUpperBound();
+//      DMatrixRMaj constraintLowerBound = input.getConstraintLowerBound();
+//      DMatrixRMaj graspMatrixJacobianTranspose = input.getGraspMatrixJacobianTranspose();
+//
+//      MatrixTools.setMatrixBlock(Ain, rhoSize, 0, graspMatrixJacobianTranspose, 0, 0, graspMatrixJacobianTranspose.getNumRows(), graspMatrixJacobianTranspose.getNumCols(), 1.0);
+//      MatrixTools.setMatrixBlock(bin, rhoSize, 0, constraintUpperBound, 0, 0, constraintUpperBound.getNumRows(), constraintUpperBound.getNumCols(), 1.0);
+//
+//      MatrixTools.setMatrixBlock(Ain, rhoSize + graspMatrixJacobianTranspose.getNumRows(), 0, graspMatrixJacobianTranspose, 0, 0, graspMatrixJacobianTranspose.getNumRows(), graspMatrixJacobianTranspose.getNumCols(), -1.0);
+//      MatrixTools.setMatrixBlock(bin, rhoSize + graspMatrixJacobianTranspose.getNumRows(), 0, constraintLowerBound, 0, 0, constraintLowerBound.getNumRows(), constraintLowerBound.getNumCols(), -1.0);
+//
+//      for (int contactPointIndex = 0; contactPointIndex < input.getNumberOfContactPoints(); contactPointIndex++)
+//      {
+//         contactPointPosition.setToZero(input.getContactFrame(contactPointIndex));
+//         contactPointPosition.changeFrame(ReferenceFrame.getWorldFrame());
+//
+//         for (int basisVectorIndex = 0; basisVectorIndex < WholeBodyContactState.numberOfBasisVectors; basisVectorIndex++)
+//         {
+//            FrameVector3D basisVector = input.getBasisVector(contactPointIndex, basisVectorIndex);
+//            int column = basisVectorsPerContactPoint * contactPointIndex + basisVectorIndex;
+//
+//            Aeq.set(0, column, basisVector.getX());
+//            Aeq.set(1, column, basisVector.getY());
+//            Aeq.set(2, column, basisVector.getZ());
+//
+//            // x-component of cross product
+//            double xMomentScale = contactPointPosition.getY() * basisVector.getZ() - contactPointPosition.getZ() * basisVector.getY();
+//            Aeq.set(3, column, xMomentScale);
+//
+//            // y-component of cross product
+//            double yMomentScale = contactPointPosition.getZ() * basisVector.getX() - contactPointPosition.getX() * basisVector.getZ();
+//            Aeq.set(4, column, yMomentScale);
+//
+//            // z-component of cross product
+//            double zMomentScale = contactPointPosition.getX() * basisVector.getY() - contactPointPosition.getY() * basisVector.getX();
+//            Aeq.set(5, column, zMomentScale);
+//         }
+//      }
+//
+//      FramePoint3DReadOnly centerOfMass = input.getCenterOfMass();
+//      beq.set(2, 0, mg);
+//      beq.set(3, 0, mg * centerOfMass.getY());
+//      beq.set(4, 0, -mg * centerOfMass.getX());
+//
+//      qpSolver.clear();
+//      qpSolver.resetActiveSet();
+//
+//      qpSolver.setMaxNumberOfIterations(500);
+//      qpSolver.setConvergenceThreshold(1e-9);
+//      qpSolver.setQuadraticCostFunction(quadraticCost, linearCost, 0.0);
+//      qpSolver.setLinearEqualityConstraints(Aeq, beq);
+//      qpSolver.setLinearInequalityConstraints(Ain, bin);
+//
+//      qpSolver.solve(rho);
+//      return !MatrixTools.containsNaN(rho);
+      return false;
    }
 
    public DMatrixRMaj getRho()
@@ -144,17 +145,17 @@ public class MultiContactForceDistributionCalculator
    public Vector3D getResolvedForce(int contactPointIndex, WholeBodyContactState input)
    {
       Vector3D resolvedForce = new Vector3D();
-      for (int i = 0; i < WholeBodyContactState.numberOfBasisVectors; i++)
-      {
-         FrameVector3D basisVector = input.getBasisVector(contactPointIndex, i);
-         basisVector.changeFrame(ReferenceFrame.getWorldFrame());
-
-         double rho = this.rho.get(WholeBodyContactState.numberOfBasisVectors * contactPointIndex + i, 0);
-
-         resolvedForce.addX(basisVector.getX() * rho);
-         resolvedForce.addY(basisVector.getY() * rho);
-         resolvedForce.addZ(basisVector.getZ() * rho);
-      }
+//      for (int i = 0; i < WholeBodyContactState.numberOfBasisVectors; i++)
+//      {
+//         FrameVector3D basisVector = input.getBasisVector(contactPointIndex, i);
+//         basisVector.changeFrame(ReferenceFrame.getWorldFrame());
+//
+//         double rho = this.rho.get(WholeBodyContactState.numberOfBasisVectors * contactPointIndex + i, 0);
+//
+//         resolvedForce.addX(basisVector.getX() * rho);
+//         resolvedForce.addY(basisVector.getY() * rho);
+//         resolvedForce.addZ(basisVector.getZ() * rho);
+//      }
 
       return resolvedForce;
    }
