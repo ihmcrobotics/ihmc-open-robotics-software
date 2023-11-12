6657410c2701dfa103723c53ec75fcfc9866b464
Duncan Calvert <dcalvert@ihmc.us>
2023-11-12 13:36:16 -0600

    Render hollow arrows.

---
+++ ihmc-graphics/src/libgdx/java/us/ihmc/rdx/imgui/ImGuiHollowArrowRenderer.java
@@ -0,0 +1,72 @@
+package us.ihmc.rdx.imgui;
+
+import imgui.ImGui;
+import us.ihmc.euclid.tuple2D.Point2D32;
+
+public class ImGuiHollowArrowRenderer
+{
+   private final Point2D32 center = new Point2D32();
+   private final Point2D32 arrowheadTip = new Point2D32();
+   private final Point2D32 arrowheadTop = new Point2D32();
+   private final Point2D32 arrowheadBottom = new Point2D32();
+   private final Point2D32 baseTopLeft = new Point2D32();
+   private final Point2D32 baseTopRight = new Point2D32();
+   private final Point2D32 baseBottomLeft = new Point2D32();
+   private final Point2D32 baseBottomRight = new Point2D32();
+   private float cursorXDesktopFrame;
+   private float cursorYDesktopFrame;
+   private int color;
+
+   public void render(float scale, int color)
+   {
+      this.color = color;
+
+      float fontSize = ImGui.getFontSize();
+
+      scale *= fontSize;
+
+      // Center is base of arrowhead
+      center.set(0.5f * fontSize, 0.5f * fontSize);
+
+      float zero = 0.0f;
+      float arrowheadHalfheight = 0.4f;
+      float arrowheadHalfwidth = 0.6f;
+      float baseWidth = 0.9f;
+      float baseHeight = 0.35f;
+
+      // a, b, c are arrowhead points
+      arrowheadTop.set(+zero, -arrowheadHalfheight);
+      arrowheadTip.set(+arrowheadHalfwidth, +zero);
+      arrowheadBottom.set(+zero, +arrowheadHalfheight);
+
+      baseTopLeft.set(-baseWidth, -baseHeight / 2.0f);
+      baseTopRight.set(+zero, -baseHeight / 2.0f);
+      baseBottomLeft.set(-baseWidth, +baseHeight / 2.0f);
+      baseBottomRight.set(+zero, +baseHeight / 2.0f);
+
+      arrowheadTop.scaleAdd(scale, center);
+      arrowheadTip.scaleAdd(scale, center);
+      arrowheadBottom.scaleAdd(scale, center);
+      baseTopLeft.scaleAdd(scale, center);
+      baseTopRight.scaleAdd(scale, center);
+      baseBottomLeft.scaleAdd(scale, center);
+      baseBottomRight.scaleAdd(scale, center);
+
+      cursorXDesktopFrame = ImGui.getWindowPosX() + ImGui.getCursorPosX() - ImGui.getScrollX();
+      cursorYDesktopFrame = ImGui.getWindowPosY() + ImGui.getCursorPosY() - ImGui.getScrollY();
+      drawLine(arrowheadTop, arrowheadTip);
+      drawLine(arrowheadTip, arrowheadBottom);
+      drawLine(arrowheadTop, baseTopRight);
+      drawLine(arrowheadBottom, baseBottomRight);
+      drawLine(baseTopRight, baseTopLeft);
+      drawLine(baseTopLeft, baseBottomLeft);
+      drawLine(baseBottomLeft, baseBottomRight);
+   }
+
+   private void drawLine(Point2D32 from, Point2D32 to)
+   {
+      ImGui.getWindowDrawList().addLine(cursorXDesktopFrame + from.getX32(), cursorYDesktopFrame + from.getY32(),
+                                        cursorXDesktopFrame + to.getX32(), cursorYDesktopFrame + to.getY32(),
+                                        color);
+   }
+}
