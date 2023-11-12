c3b39d29cc9f59cce7e26cc4f2c6da757210e189
Duncan Calvert <dcalvert@ihmc.us>
2023-11-12 11:56:36 -0600

    Convert arrow renderer to Euclid.

--- ihmc-graphics/src/libgdx/java/us/ihmc/rdx/imgui/ImGuiArrowRenderer.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/rdx/imgui/ImGuiArrowRenderer.java
@@ -1,48 +1,59 @@
 package us.ihmc.rdx.imgui;
 
 import imgui.ImGui;
-import imgui.ImVec2;
 import imgui.flag.ImGuiDir;
+import us.ihmc.euclid.tuple2D.Point2D32;
 
 public class ImGuiArrowRenderer
 {
-   private final ImVec2 center = new ImVec2();
-   private final ImVec2 a = new ImVec2();
-   private final ImVec2 b = new ImVec2();
-   private final ImVec2 c = new ImVec2();
-   private final ImVec2 centerA = new ImVec2();
-   private final ImVec2 centerB = new ImVec2();
-   private final ImVec2 centerC = new ImVec2();
+   private final Point2D32 center = new Point2D32();
+   private final Point2D32 a = new Point2D32();
+   private final Point2D32 b = new Point2D32();
+   private final Point2D32 c = new Point2D32();
+   private final Point2D32 centerA = new Point2D32();
+   private final Point2D32 centerB = new Point2D32();
+   private final Point2D32 centerC = new Point2D32();
 
    /** Taken from imgui_draw.cpp */
    public void renderArrow(int imGuiDirection, float scale, int color)
    {
-      float h = ImGui.getFontSize();
-      float r = h * 0.40f * scale;
-      center.set(h * 0.50f, h * 0.50f * scale);
+      float fontSize = ImGui.getFontSize();
+      float radius = fontSize * 0.40f * scale;
+
+      center.set(0.5f, 0.5f);
+      center.scale(fontSize);
+      center.setY(center.getY32() * scale);
+
+      float vertex0 = 0.000f;
+      float vertex1 = 0.750f;
+      float vertex2 = 0.866f;
 
       if (imGuiDirection == ImGuiDir.Down)
       {
-         a.set(+0.000f * r, +0.750f * r);
-         b.set(-0.866f * r, -0.750f * r);
-         c.set(+0.866f * r, -0.750f * r);
+         a.set(+vertex0, +vertex1);
+         b.set(-vertex2, -vertex1);
+         c.set(+vertex2, -vertex1);
       }
       if (imGuiDirection == ImGuiDir.Right)
       {
-         a.set(+0.750f * r, +0.000f * r);
-         b.set(-0.750f * r, +0.866f * r);
-         c.set(-0.750f * r, -0.866f * r);
+         a.set(+vertex1, +vertex0);
+         b.set(-vertex1, +vertex2);
+         c.set(-vertex1, -vertex2);
       }
 
-      centerA.set(center.x + a.x, center.y + a.y);
-      centerB.set(center.x + b.x, center.y + b.y);
-      centerC.set(center.x + c.x, center.y + c.y);
+      a.scale(radius);
+      b.scale(radius);
+      c.scale(radius);
+
+      centerA.add(center, a);
+      centerB.add(center, b);
+      centerC.add(center, c);
 
       float cursorXDesktopFrame = ImGui.getWindowPosX() + ImGui.getCursorPosX() - ImGui.getScrollX();
       float cursorYDesktopFrame = ImGui.getWindowPosY() + ImGui.getCursorPosY() - ImGui.getScrollY();
-      ImGui.getWindowDrawList().addTriangleFilled(cursorXDesktopFrame + centerA.x, cursorYDesktopFrame + centerA.y,
-                                                  cursorXDesktopFrame + centerB.x, cursorYDesktopFrame + centerB.y,
-                                                  cursorXDesktopFrame + centerC.x, cursorYDesktopFrame + centerC.y,
+      ImGui.getWindowDrawList().addTriangleFilled(cursorXDesktopFrame + centerA.getX32(), cursorYDesktopFrame + centerA.getY32(),
+                                                  cursorXDesktopFrame + centerB.getX32(), cursorYDesktopFrame + centerB.getY32(),
+                                                  cursorXDesktopFrame + centerC.getX32(), cursorYDesktopFrame + centerC.getY32(),
                                                   color);
    }
 }
