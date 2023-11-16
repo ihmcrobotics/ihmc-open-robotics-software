b07bea061f2e881854141a624bed24cf005a334e
Duncan Calvert <dcalvert@ihmc.us>
2023-11-15 19:06:23 -0600

    Document.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/BehaviorTreeNodeLayer.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/BehaviorTreeNodeLayer.java
@@ -6,6 +6,12 @@ import us.ihmc.tools.Destroyable;
  * This is an interface that represents a behavior tree node's
  * State, UI, or Executor layer.
  *
+ * A "Layer" will be RDX, Executor, State, where Definition is always the base layer.
+ *
+ * The layers actually do not extend each other, they are more encapsulations of the
+ * lower layers that add functionality. A node exists through it's state and different
+ * processes may wrap that synchronized state with layers appropriate for acting in that process.
+ *
  * @param <T> This node's type. It will be a State, RDX, or Executor layer type.
  *            This is so we can extend BehaviorTreeNode and provide the appropriate
  *            base type of children.
