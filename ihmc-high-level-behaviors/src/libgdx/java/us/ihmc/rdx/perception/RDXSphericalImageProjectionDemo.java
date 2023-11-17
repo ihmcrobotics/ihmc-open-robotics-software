4d6f9fc7192f14703975e37911a911e7c1832a38
Duncan Calvert <dcalvert@ihmc.us>
2023-11-17 17:33:09 -0600

    Setup static stereo 3D image viewer.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXSphericalImageProjectionDemo.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXSphericalImageProjectionDemo.java
@@ -1,16 +1,23 @@
 package us.ihmc.rdx.perception;
 
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import imgui.ImGui;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
+import us.ihmc.rdx.sceneManager.RDXSceneLevel;
 import us.ihmc.rdx.tools.RDXIconTexture;
 import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
+import java.util.Set;
+
 public class RDXSphericalImageProjectionDemo
 {
    private final RDXBaseUI baseUI = new RDXBaseUI();
    private final SideDependentList<RDXProjectionSphere> projectionSpheres = new SideDependentList<>(RDXProjectionSphere::new);
-   private RDXIconTexture imageTexture;
+   private final SideDependentList<RDXIconTexture> imageTextures = new SideDependentList<>();
 
    public RDXSphericalImageProjectionDemo()
    {
@@ -21,24 +28,39 @@ public class RDXSphericalImageProjectionDemo
          {
             baseUI.create();
 
-            imageTexture = new RDXIconTexture("/images/blackflytest.jpg");
+            for (RobotSide side : RobotSide.values)
+            {
+               imageTextures.put(side, new RDXIconTexture("/images/blackflytest%s.jpg".formatted(side.getLowerCaseName())));
+               projectionSpheres.get(side).create();
+               projectionSpheres.get(side).updateTexture(imageTextures.get(side).getTexture());
+            }
 
-            projectionSpheres.get(RobotSide.LEFT).create();
-            projectionSpheres.get(RobotSide.LEFT).updateTexture(imageTexture.getTexture());
-
-            baseUI.getPrimaryScene().addRenderableProvider(projectionSpheres.get(RobotSide.LEFT)::getRenderables);
+            baseUI.getPrimaryScene().addRenderableProvider(this::getRenderables);
 
             baseUI.getImGuiPanelManager().addPanel("Projection", () ->
             {
+               ImGui.text("Left:");
                projectionSpheres.get(RobotSide.LEFT).renderImGuiWidgets();
+               ImGui.text("Right:");
+               projectionSpheres.get(RobotSide.RIGHT).renderImGuiWidgets();
             });
+         }
+
+         private void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool, Set<RDXSceneLevel> sceneLevels)
+         {
+            if (sceneLevels.contains(RDXSceneLevel.VR_EYE_RIGHT))
+            {
+               baseUI.getPrimaryScene().addRenderableProvider(projectionSpheres.get(RobotSide.RIGHT)::getRenderables);
+            }
+            else
+            {
+               baseUI.getPrimaryScene().addRenderableProvider(projectionSpheres.get(RobotSide.LEFT)::getRenderables);
+            }
          }
 
          @Override
          public void render()
          {
-            projectionSpheres.get(RobotSide.LEFT).updateTexture(imageTexture.getTexture());
-
             baseUI.renderBeforeOnScreenUI();
             baseUI.renderEnd();
          }
