9dbd8a7f1c0533eeed112549412414b10b689669
smccrory <smccrory@ihmc.us>
2023-11-21 11:10:53 -0600

    Setting up custom contact point friction params

--- ihmc-common-walking-control-modules/src/visualizers/java/us/ihmc/commonWalkingControlModules/staticEquilibrium/ContactStateExamples.java
+++ ihmc-common-walking-control-modules/src/visualizers/java/us/ihmc/commonWalkingControlModules/staticEquilibrium/ContactStateExamples.java
@@ -16,6 +16,8 @@ import us.ihmc.robotics.robotSide.SideDependentList;
 
 public class ContactStateExamples
 {
+   private static final double coefficientOfFriction = 0.7;
+
    public static MutableWholeBodyContactState createTriangleFlatGround()
    {
       return createTriangleInput(0.0, 0.0, 0.0);
@@ -52,17 +54,17 @@ public class ContactStateExamples
    public static MutableWholeBodyContactState createFlatSquare()
    {
       MutableWholeBodyContactState input = new MutableWholeBodyContactState();
-      input.addContactPoint(new Point3D(-0.5, -0.5, 0.0), new Vector3D(Axis3D.Z));
-      input.addContactPoint(new Point3D(-0.5, 0.5, 0.0), new Vector3D(Axis3D.Z));
-      input.addContactPoint(new Point3D( 0.5, -0.5, 0.0), new Vector3D(Axis3D.Z));
-      input.addContactPoint(new Point3D( 0.5, 0.5, 0.0), new Vector3D(Axis3D.Z));
+      input.addContactPoint(new Point3D(-0.5, -0.5, 0.0), new Vector3D(Axis3D.Z), coefficientOfFriction);
+      input.addContactPoint(new Point3D(-0.5, 0.5, 0.0), new Vector3D(Axis3D.Z), coefficientOfFriction);
+      input.addContactPoint(new Point3D( 0.5, -0.5, 0.0), new Vector3D(Axis3D.Z), coefficientOfFriction);
+      input.addContactPoint(new Point3D( 0.5, 0.5, 0.0), new Vector3D(Axis3D.Z), coefficientOfFriction);
       return input;
    }
 
    public static MutableWholeBodyContactState createSingleFlatContactPoint(double x, double y)
    {
       MutableWholeBodyContactState input = new MutableWholeBodyContactState();
-      input.addContactPoint(new Point3D(x, y, 0.0), new Vector3D(Axis3D.Z));
+      input.addContactPoint(new Point3D(x, y, 0.0), new Vector3D(Axis3D.Z), coefficientOfFriction);
       return input;
    }
 
@@ -84,7 +86,7 @@ public class ContactStateExamples
          FrameVector3D normal = new FrameVector3D(ReferenceFrame.getWorldFrame(), 0.0, 0.0, 1.0);
          axisAngle.transform(normal);
 
-         input.addContactPoint(contactPoint, normal);
+         input.addContactPoint(contactPoint, normal, coefficientOfFriction);
       }
 
       return input;
@@ -244,6 +246,6 @@ public class ContactStateExamples
       contactPoint.changeFrame(ReferenceFrame.getWorldFrame());
       normal.changeFrame(ReferenceFrame.getWorldFrame());
 
-      input.addContactPoint(contactPoint, normal);
+      input.addContactPoint(contactPoint, normal, coefficientOfFriction);
    }
 }
