90a038d4637073a794d22efb6bcf8e26d2eb1f7f
Duncan Calvert <dcalvert@ihmc.us>
2023-11-29 14:18:54 -0600

    Add door floorboard collisions and cleanup prep.

--- ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/multiBodies/door/DoorFrameDefinition.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/multiBodies/door/DoorFrameDefinition.java
@@ -10,6 +10,8 @@ import us.ihmc.scs2.definition.geometry.ModelFileGeometryDefinition;
 import us.ihmc.scs2.definition.robot.RigidBodyDefinition;
 import us.ihmc.scs2.definition.visual.VisualDefinition;
 
+import static us.ihmc.perception.sceneGraph.multiBodies.door.DoorModelParameters.*;
+
 public class DoorFrameDefinition extends RigidBodyDefinition
 {
    private final boolean ENABLE_SUPPORT_BOARD_COLLISIONS = true;
@@ -18,18 +20,21 @@ public class DoorFrameDefinition extends RigidBodyDefinition
    {
       super("frameBody");
 
-      double sizeX = 0.0889;
-      double sizeY = 0.0889;
-      double sizeZ = 2.159;
+      double pillarSizeX = DOOR_FRAME_PILLAR_SIZE_X;
+      double pillarSizeY = DOOR_FRAME_PILLAR_SIZE_X;
+      double pillarSizeZ = DOOR_FRAME_PILLAR_SIZE_Z;
 
-      setMass(300.0f);
+      // Like a bunch of cinder blocks
+      float heavyMass = 300.0f;
+      setMass(heavyMass);
       double radiusOfGyrationPercent = 0.8;
       setMomentOfInertia(MomentOfInertiaFactory.fromMassAndRadiiOfGyration(getMass(),
-                                                                           radiusOfGyrationPercent * sizeX,
-                                                                           radiusOfGyrationPercent * sizeY,
-                                                                           radiusOfGyrationPercent * sizeZ));
+                                                                           radiusOfGyrationPercent * pillarSizeX,
+                                                                           radiusOfGyrationPercent * pillarSizeY,
+                                                                           radiusOfGyrationPercent * pillarSizeZ));
 
-      Vector3D centerOfMassInModelFrame = new Vector3D(0.0, (0.939887 / 2.0) - 0.006046, 0.1);
+      double lowCoMForStabilityZ = 0.05;
+      Vector3D centerOfMassInModelFrame = new Vector3D(0.0, (DOOR_PANEL_WIDTH / 2.0) - DOOR_FRAME_HINGE_OFFSET, lowCoMForStabilityZ);
       getInertiaPose().getTranslation().set(centerOfMassInModelFrame);
       getInertiaPose().getRotation().setToZero();
 
@@ -39,23 +44,36 @@ public class DoorFrameDefinition extends RigidBodyDefinition
       addVisualDefinition(frameModelVisualDefinition);
 
       Point3D latchSidePostOffset = new Point3D();
-      latchSidePostOffset.add(0.0, -0.006064, 0.0);
-      latchSidePostOffset.add(0.0, -0.0889 / 2.0, 2.159 / 2.0);
-      addCollisionShape(sizeX, sizeY, sizeZ, latchSidePostOffset);
+      latchSidePostOffset.add(0.0, -DOOR_FRAME_HINGE_OFFSET, 0.0);
+      latchSidePostOffset.add(0.0, -DOOR_FRAME_PILLAR_SIZE_X / 2.0, DOOR_FRAME_PILLAR_SIZE_Z / 2.0);
+      addCollisionShape(pillarSizeX, pillarSizeY, pillarSizeZ, latchSidePostOffset);
 
+      double flatboardSizeX = 0.63;
+      double flatboardSizeY = 0.39;
+      double flatboardSizeZ = 0.02;
+      double twoX4Width = 0.0381;
       if (ENABLE_SUPPORT_BOARD_COLLISIONS)
       {
          Point3D hingeSideSupportBoardOffset = new Point3D();
-         hingeSideSupportBoardOffset.add(0.0, -0.006064, 0.0);
-         hingeSideSupportBoardOffset.add(0.0, -0.0889, 0.0);
-         hingeSideSupportBoardOffset.add(0.0, -0.0381 / 2.0, 0.0889 / 2.0f);
-         addCollisionShape(1.016, 0.0381, 0.0889, hingeSideSupportBoardOffset);
+         hingeSideSupportBoardOffset.add(0.0, -DOOR_FRAME_HINGE_OFFSET, 0.0);
+         hingeSideSupportBoardOffset.add(0.0, -DOOR_FRAME_PILLAR_SIZE_X, 0.0);
+         hingeSideSupportBoardOffset.add(0.0, -twoX4Width / 2.0, DOOR_FRAME_PILLAR_SIZE_X / 2.0f);
+         double twoX4Length = 1.016;
+         addCollisionShape(twoX4Length, twoX4Width, DOOR_FRAME_PILLAR_SIZE_X, hingeSideSupportBoardOffset);
+
+         Point3D hingeSideSupportWeightBaseOffset = new Point3D();
+         hingeSideSupportWeightBaseOffset.add(0.0, -DOOR_FRAME_HINGE_OFFSET, 0.0);
+         hingeSideSupportWeightBaseOffset.add(0.0, -DOOR_FRAME_PILLAR_SIZE_X, 0.0);
+         hingeSideSupportWeightBaseOffset.add(0.0, -twoX4Width, 0.0);
+         hingeSideSupportWeightBaseOffset.add(0.0, -flatboardSizeY / 2.0, flatboardSizeZ / 2.0);
+         addCollisionShape(flatboardSizeX, flatboardSizeY, 0.02, hingeSideSupportWeightBaseOffset);
       }
 
       Point3D hingeSidePostOffset = new Point3D();
-      hingeSidePostOffset.add(0.0, 0.93415, 0.0);
-      hingeSidePostOffset.add(0.0, 0.0889 / 2.0, 2.159 / 2.0);
-      addCollisionShape(sizeX, sizeY, sizeZ, hingeSidePostOffset);
+      double toLatchPillarY = 0.93415;
+      hingeSidePostOffset.add(0.0, toLatchPillarY, 0.0);
+      hingeSidePostOffset.add(0.0, DOOR_FRAME_PILLAR_SIZE_X / 2.0, DOOR_FRAME_PILLAR_SIZE_Z / 2.0);
+      addCollisionShape(pillarSizeX, pillarSizeY, pillarSizeZ, hingeSidePostOffset);
 
       if (ENABLE_SUPPORT_BOARD_COLLISIONS)
       {
@@ -63,11 +81,18 @@ public class DoorFrameDefinition extends RigidBodyDefinition
          // short edge 0.372505 from center
          // scootch 0.0680925
          Point3D latchSideSupportBoardOffset = new Point3D();
-         latchSideSupportBoardOffset.add(0.0, 0.93415, 0.0);
-         latchSideSupportBoardOffset.add(0.0, 0.0889, 0.0);
-         latchSideSupportBoardOffset.add(0.0680925, 0.0381 / 2.0, 0.0889 / 2.0f);
-//         addCollisionShape(1.016, 0.0381, 0.0889, latchSideSupportBoardOffset);
-         addCollisionShape(0.881195, 0.0381, 0.0889, latchSideSupportBoardOffset);
+         latchSideSupportBoardOffset.add(0.0, toLatchPillarY, 0.0);
+         latchSideSupportBoardOffset.add(0.0, DOOR_FRAME_PILLAR_SIZE_X, 0.0);
+         latchSideSupportBoardOffset.add(0.0680925, twoX4Width / 2.0, DOOR_FRAME_PILLAR_SIZE_X / 2.0f);
+         double sideFloorBoardLength = 0.881195;
+         addCollisionShape(sideFloorBoardLength, twoX4Width, DOOR_FRAME_PILLAR_SIZE_X, latchSideSupportBoardOffset);
+
+         Point3D hingeSideSupportWeightBaseOffset = new Point3D();
+         hingeSideSupportWeightBaseOffset.add(0.0, toLatchPillarY, 0.0);
+         hingeSideSupportWeightBaseOffset.add(0.0, DOOR_FRAME_PILLAR_SIZE_X, 0.0);
+         hingeSideSupportWeightBaseOffset.add(0.0, twoX4Width, 0.0);
+         hingeSideSupportWeightBaseOffset.add(0.0, flatboardSizeY / 2.0, flatboardSizeZ / 2.0);
+         addCollisionShape(flatboardSizeX, flatboardSizeY, 0.02, hingeSideSupportWeightBaseOffset);
       }
    }
 
