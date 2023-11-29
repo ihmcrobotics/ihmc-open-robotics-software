eb47a61719a99ec4b910edebb16fb4808a348546
Duncan Calvert <dcalvert@ihmc.us>
2023-11-29 10:50:39 -0600

    Make door simulation restartable.

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/simulation/scs2/RDXSCS2DoorDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/simulation/scs2/RDXSCS2DoorDemo.java
@@ -26,124 +26,127 @@ import us.ihmc.scs2.simulation.robot.multiBodySystem.SimSixDoFJoint;
 /**
  * An SCS 2 simulation of a door hinged on a frame with a lever handle.
  */
-public class RDXSCS2DoorDemo
+public class RDXSCS2DoorDemo extends Lwjgl3ApplicationAdapter
 {
    private final RDXBaseUI baseUI = new RDXBaseUI();
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
    private final ImDouble hingeTorque = new ImDouble();
-   private RDXSCS2SimulationSession rdxSimulationSession;
+   private RDXSCS2RestartableSimulationSession rdxSimulationSession;
    private RDXSCS2SixDoFJointGizmo doorRootGizmo;
    private SimRevoluteJoint doorHingeJoint;
+   private BulletRobot bulletRobot;
 
    public RDXSCS2DoorDemo()
    {
-      baseUI.launchRDXApplication(new Lwjgl3ApplicationAdapter()
+      baseUI.launchRDXApplication(this);
+   }
+
+   @Override
+   public void create()
+   {
+      baseUI.create();
+      baseUI.getPrimaryScene().getSceneLevelsToRender().add(RDXSceneLevel.GROUND_TRUTH);
+      baseUI.getPrimary3DPanel().getCamera3D().setCameraFocusPoint(new Point3D(0.7, 0.0, 0.4));
+      baseUI.getPrimary3DPanel().getCamera3D().changeCameraPosition(-3.0, -4.0, 4.0);
+
+      rdxSimulationSession = new RDXSCS2RestartableSimulationSession(baseUI, this::buildSession);
+
+      rdxSimulationSession.getOnSessionStartedRunnables().add(() ->
       {
-         @Override
-         public void create()
+         rdxSimulationSession.getSCS2SimulationSession().getAdditionalImGuiWidgets().add(() ->
          {
-            baseUI.create();
-            baseUI.getPrimaryScene().getSceneLevelsToRender().add(RDXSceneLevel.GROUND_TRUTH);
-            baseUI.getPrimary3DPanel().getCamera3D().setCameraFocusPoint(new Point3D(0.7, 0.0, 0.4));
-            baseUI.getPrimary3DPanel().getCamera3D().changeCameraPosition(-3.0, -4.0, 4.0);
+            doorRootGizmo.renderMoveJointCheckbox();
+            ImGuiTools.sliderDouble(labels.get("Hinge effort"), hingeTorque, -100.0, 100.0);
 
-            rdxSimulationSession = new RDXSCS2SimulationSession();
-            SimulationSession simulationSession = new SimulationSession(BulletPhysicsEngine::new);
-
-            DoorDefinition doorDefinition = new DoorDefinition();
-            doorDefinition.getDoorPanelDefinition().setAddArUcoMarkers(true);
-            doorDefinition.build();
-
-            // Pull door
-//            doorDefinition.getInitialSixDoFState().setConfiguration(new YawPitchRoll(0.0, 0.0, 0.0), new Point3D(1.0, -0.5, 0.01));
-
-            // Push door
-            doorDefinition.getInitialSixDoFState().setConfiguration(new YawPitchRoll(Math.PI, 0.0, 0.0), new Point3D(1.3, 0.5, 0.01));
-            Robot doorRobot = simulationSession.addRobot(doorDefinition);
-            doorDefinition.applyPDController(doorRobot);
-
-            BulletPhysicsEngine bulletPhysicsEngine = (BulletPhysicsEngine) simulationSession.getPhysicsEngine();
-            BulletRobot bulletRobot = (BulletRobot) bulletPhysicsEngine.getBulletRobots().get(0);
-
-
-            doorRootGizmo = new RDXSCS2SixDoFJointGizmo(baseUI.getPrimary3DPanel(),
-                                                        (SimSixDoFJoint) doorRobot.getJoint("doorRootJoint"));
-            doorHingeJoint = (SimRevoluteJoint) doorRobot.getJoint("doorHingeJoint");
-
-            doorRobot.addController(() -> doorHingeJoint.setTau(hingeTorque.get()));
-
-            simulationSession.addTerrainObject(new FlatGroundDefinition());
-
-            rdxSimulationSession.getOnSessionStartedRunnables().add(() ->
+            if (ImGui.button("Make kinematic"))
             {
-               rdxSimulationSession.getAdditionalImGuiWidgets().add(() ->
-               {
-                  doorRootGizmo.renderMoveJointCheckbox();
-                  ImGuiTools.sliderDouble(labels.get("Hinge effort"), hingeTorque, -100.0, 100.0);
-
-                  if (ImGui.button("Make kinematic"))
-                  {
-                     btMultiBody btMultiBody = bulletRobot.getBulletMultiBodyRobot().getBtMultiBody();
-                     btMultiBodyLinkCollider baseCollider = btMultiBody.getBaseCollider();
-                     baseCollider.setCollisionFlags(baseCollider.getCollisionFlags() | btCollisionObject.CF_KINEMATIC_OBJECT);
+               btMultiBody btMultiBody = bulletRobot.getBulletMultiBodyRobot().getBtMultiBody();
+               btMultiBodyLinkCollider baseCollider = btMultiBody.getBaseCollider();
+               baseCollider.setCollisionFlags(baseCollider.getCollisionFlags() | btCollisionObject.CF_KINEMATIC_OBJECT);
 //                     baseCollider.setActivationState(BulletCollision.DISABLE_DEACTIVATION);
-                     for (int i = 0; i < btMultiBody.getNumLinks(); i++)
-                     {
-                        btMultiBodyLinkCollider collider = btMultiBody.getLink(i).m_collider();
-                        if (!collider.isNull())
-                        {
-                           collider.setCollisionFlags(collider.getCollisionFlags() | btCollisionObject.CF_KINEMATIC_OBJECT);
-                           collider.setActivationState(BulletCollision.DISABLE_DEACTIVATION);
-                        }
-                     }
-                  }
-                  if (ImGui.button("Make not kinematic"))
+               for (int i = 0; i < btMultiBody.getNumLinks(); i++)
+               {
+                  btMultiBodyLinkCollider collider = btMultiBody.getLink(i).m_collider();
+                  if (!collider.isNull())
                   {
-                     btMultiBody btMultiBody = bulletRobot.getBulletMultiBodyRobot().getBtMultiBody();
-                     btMultiBodyLinkCollider baseCollider = btMultiBody.getBaseCollider();
-                     baseCollider.setCollisionFlags(baseCollider.getCollisionFlags() & ~btCollisionObject.CF_KINEMATIC_OBJECT);
+                     collider.setCollisionFlags(collider.getCollisionFlags() | btCollisionObject.CF_KINEMATIC_OBJECT);
+                     collider.setActivationState(BulletCollision.DISABLE_DEACTIVATION);
+                  }
+               }
+            }
+            if (ImGui.button("Make not kinematic"))
+            {
+               btMultiBody btMultiBody = bulletRobot.getBulletMultiBodyRobot().getBtMultiBody();
+               btMultiBodyLinkCollider baseCollider = btMultiBody.getBaseCollider();
+               baseCollider.setCollisionFlags(baseCollider.getCollisionFlags() & ~btCollisionObject.CF_KINEMATIC_OBJECT);
 //                     baseCollider.setActivationState(BulletCollision.ACTIVE_TAG);
-                     for (int i = 0; i < btMultiBody.getNumLinks(); i++)
-                     {
-                        btMultiBodyLinkCollider collider = btMultiBody.getLink(i).m_collider();
-                        if (!collider.isNull())
-                        {
-                           collider.setCollisionFlags(collider.getCollisionFlags() & ~btCollisionObject.CF_KINEMATIC_OBJECT);
+               for (int i = 0; i < btMultiBody.getNumLinks(); i++)
+               {
+                  btMultiBodyLinkCollider collider = btMultiBody.getLink(i).m_collider();
+                  if (!collider.isNull())
+                  {
+                     collider.setCollisionFlags(collider.getCollisionFlags() & ~btCollisionObject.CF_KINEMATIC_OBJECT);
 //                           collider.setActivationState(BulletCollision.ACTIVE_TAG);
 //                           collider.activate(true);
-                        }
-                     }
                   }
-               });
+               }
+            }
+         });
 
-               doorRootGizmo.linkVariables(rdxSimulationSession.getYoManager().getRootRegistry(),
-                                           rdxSimulationSession.getYoManager().getLinkedYoVariableFactory());
-            });
-
-            rdxSimulationSession.create(baseUI);
-            rdxSimulationSession.startSession(simulationSession);
-            rdxSimulationSession.getSession().setSessionMode(SessionMode.RUNNING);
-            baseUI.getPrimaryScene().addRenderableProvider(rdxSimulationSession::getRenderables);
-            baseUI.getImGuiPanelManager().addPanel(rdxSimulationSession.getControlPanel());
-         }
-
-         @Override
-         public void render()
-         {
-            rdxSimulationSession.update();
-
-            doorRootGizmo.update();
-
-            baseUI.renderBeforeOnScreenUI();
-            baseUI.renderEnd();
-         }
-
-         @Override
-         public void dispose()
-         {
-            baseUI.dispose();
-         }
+         doorRootGizmo.linkVariables(rdxSimulationSession.getSCS2SimulationSession().getYoManager().getRootRegistry(),
+                                     rdxSimulationSession.getSCS2SimulationSession().getYoManager().getLinkedYoVariableFactory());
       });
+
+      rdxSimulationSession.buildSimulation();
+      rdxSimulationSession.getSCS2SimulationSession().getSession().setSessionMode(SessionMode.RUNNING);
+   }
+
+   @Override
+   public void render()
+   {
+      rdxSimulationSession.update();
+
+      doorRootGizmo.update();
+
+      baseUI.renderBeforeOnScreenUI();
+      baseUI.renderEnd();
+   }
+
+   @Override
+   public void dispose()
+   {
+      baseUI.dispose();
+   }
+
+   private SimulationSession buildSession()
+   {
+      SimulationSession simulationSession = new SimulationSession(BulletPhysicsEngine::new);
+
+      DoorDefinition doorDefinition = new DoorDefinition();
+      doorDefinition.getDoorPanelDefinition().setAddArUcoMarkers(true);
+      doorDefinition.build();
+
+      // Pull door
+      //            doorDefinition.getInitialSixDoFState().setConfiguration(new YawPitchRoll(0.0, 0.0, 0.0), new Point3D(1.0, -0.5, 0.01));
+
+      // Push door
+      doorDefinition.getInitialSixDoFState().setConfiguration(new YawPitchRoll(Math.PI, 0.0, 0.0), new Point3D(1.3, 0.5, 0.01));
+      Robot doorRobot = simulationSession.addRobot(doorDefinition);
+      doorDefinition.applyPDController(doorRobot);
+
+      BulletPhysicsEngine bulletPhysicsEngine = (BulletPhysicsEngine) simulationSession.getPhysicsEngine();
+      bulletRobot = (BulletRobot) bulletPhysicsEngine.getBulletRobots().get(0);
+
+
+      doorRootGizmo = new RDXSCS2SixDoFJointGizmo(baseUI.getPrimary3DPanel(),
+                                                  (SimSixDoFJoint) doorRobot.getJoint("doorRootJoint"));
+      doorHingeJoint = (SimRevoluteJoint) doorRobot.getJoint("doorHingeJoint");
+
+      doorRobot.addController(() -> doorHingeJoint.setTau(hingeTorque.get()));
+
+      simulationSession.addTerrainObject(new FlatGroundDefinition());
+
+      return simulationSession;
    }
 
    public static void main(String[] args)
