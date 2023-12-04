c280569f7b35798f9dcad4e043835583d3692575
Nick Kitchel <nkitchel@ihmc.org>
2023-12-04 09:18:05 -0600

    Name change from ImGuiYoVariableClientUI to RDXYoVariableClient since its not its own UI.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/yo/RDXYoVariableClientPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/yo/RDXYoVariableClientPanel.java
@@ -3,13 +3,13 @@ package us.ihmc.rdx.ui.yo;
 import us.ihmc.behaviors.tools.yo.YoVariableClientHelper;
 import us.ihmc.rdx.imgui.RDXPanel;
 
-public class ImGuiYoVariableClientUI
+public class RDXYoVariableClientPanel
 {
    private final RDXPanel panel;
    private final YoVariableClientHelper yoVariableClientHelper;
    private final ImGuiYoVariableClientManagerWidget managerWidget;
 
-   public ImGuiYoVariableClientUI(String registryName, String hostname, int port)
+   public RDXYoVariableClientPanel(String registryName, String hostname, int port)
    {
       yoVariableClientHelper = new YoVariableClientHelper(registryName);
       managerWidget = new ImGuiYoVariableClientManagerWidget(yoVariableClientHelper, () -> hostname, port);
