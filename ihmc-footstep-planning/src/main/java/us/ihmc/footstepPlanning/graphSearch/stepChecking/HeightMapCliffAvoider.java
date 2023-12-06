db768f4318da3b0ed6386a214df4986764237bd3
Nick Kitchel <nkitchel@ihmc.org>
2023-12-06 09:00:22 -0600

    Fix vertices on foot polygon, raised the walking height by 3 cm

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepChecking/HeightMapCliffAvoider.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepChecking/HeightMapCliffAvoider.java
@@ -4,7 +4,6 @@ import us.ihmc.euclid.Axis3D;
 import us.ihmc.euclid.geometry.ConvexPolygon2D;
 import us.ihmc.euclid.geometry.Plane3D;
 import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DReadOnly;
-import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
@@ -13,10 +12,11 @@ import us.ihmc.footstepPlanning.graphSearch.footstepSnapping.FootstepSnapperRead
 import us.ihmc.footstepPlanning.graphSearch.graph.DiscreteFootstep;
 import us.ihmc.footstepPlanning.graphSearch.graph.DiscreteFootstepTools;
 import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParametersReadOnly;
-import us.ihmc.log.LogTools;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.sensorProcessing.heightMap.HeightMapData;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HeightMapCliffAvoider
 {
@@ -47,17 +47,19 @@ public class HeightMapCliffAvoider
    public void addVertexToFootPolygon(ConvexPolygon2D scaledFootPolygon)
    {
       int numberOfVertices = scaledFootPolygon.getNumberOfVertices();
+      ConvexPolygon2D copyFootPolygon = new ConvexPolygon2D();
+      copyFootPolygon.set(scaledFootPolygon);
 
       for (int i = 0; i < numberOfVertices; i++)
       {
-         Point2DReadOnly start = scaledFootPolygon.getVertex(i);
+         Point2DReadOnly start = copyFootPolygon.getVertex(i);
          Point2DReadOnly end;
 
          // Make sure we do the entire loop which includes the line segment back to the first vertex
          if (i + 1 < numberOfVertices)
-            end = scaledFootPolygon.getVertex(i + 1);
+            end = copyFootPolygon.getVertex(i + 1);
          else
-            end = scaledFootPolygon.getVertex(0);
+            end = copyFootPolygon.getVertex(0);
 
          if (start.distance(end) > 0.1)
          {
