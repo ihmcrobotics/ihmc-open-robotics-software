07ff55c9dcae4099098e3337488525b752bf72dd
Sylvain Bertrand <sbertrand@ihmc.org>
2023-12-11 14:43:37 -0600

    :arrow_up: ihmc-javafx-toolkit 17-0.22.8

--- ihmc-avatar-interfaces/build.gradle.kts
+++ ihmc-avatar-interfaces/build.gradle.kts
@@ -69,7 +69,7 @@ testDependencies {
 visualizersDependencies {
    api(ihmc.sourceSetProject("main"))
 
-   api("us.ihmc:ihmc-javafx-toolkit:17-0.22.7")
+   api("us.ihmc:ihmc-javafx-toolkit:17-0.22.8")
    api("us.ihmc:robot-environment-awareness-application:source")
    api("us.ihmc:ihmc-path-planning-visualizers:source")
    api("us.ihmc:ihmc-footstep-planning-visualizers:source")
