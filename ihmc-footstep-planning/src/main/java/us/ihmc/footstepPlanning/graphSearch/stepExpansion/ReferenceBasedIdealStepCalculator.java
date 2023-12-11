7a70d4f798ed24417c08121afba1e223b3d5ee89
smccrory <smccrory@ihmc.us>
2023-12-11 12:58:50 -0600

    Adding new reference based expansion. Stubbing out functionality for height map traversability metric

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepExpansion/ReferenceBasedIdealStepCalculator.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepExpansion/ReferenceBasedIdealStepCalculator.java
@@ -1,19 +1,15 @@
 package us.ihmc.footstepPlanning.graphSearch.stepExpansion;
 
 import us.ihmc.euclid.referenceFrame.FramePose3D;
-import us.ihmc.euclid.tools.EuclidCoreTools;
 import us.ihmc.footstepPlanning.FootstepPlan;
 import us.ihmc.footstepPlanning.PlannedFootstep;
 import us.ihmc.footstepPlanning.graphSearch.graph.DiscreteFootstep;
 import us.ihmc.footstepPlanning.graphSearch.graph.FootstepGraphNode;
 import us.ihmc.footstepPlanning.graphSearch.parameters.FootstepPlannerParametersBasics;
-import us.ihmc.log.LogTools;
 import us.ihmc.pathPlanning.graph.structure.DirectedGraph;
-import us.ihmc.robotics.geometry.AngleTools;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
 
-import java.text.DecimalFormat;
 import java.util.List;
 
 public class ReferenceBasedIdealStepCalculator implements IdealStepCalculatorInterface
@@ -23,17 +19,17 @@ public class ReferenceBasedIdealStepCalculator implements IdealStepCalculatorInt
    private DirectedGraph<FootstepGraphNode> footstepGraph;
    private DiscreteFootstep nominalIdealStep;
    private final YoBoolean stepSideIncorrect;
-   public static String statusMessage = "Step calculate mode: Nominal";
 
    // This will be used to query what reference alpha to use.
    private final FootstepPlannerParametersBasics footstepPlannerParameters;
-   private static final double DEFAULT_ALPHA = 0.7;
+   private final YoBoolean usingReferenceStep;
 
    public ReferenceBasedIdealStepCalculator(FootstepPlannerParametersBasics footstepPlannerParameters, IdealStepCalculator nominalIdealStepCalculator, YoRegistry registry)
    {
       this.nominalIdealStepCalculator = nominalIdealStepCalculator;
       stepSideIncorrect = new YoBoolean("stepSideIncorrect", registry);
       this.footstepPlannerParameters = footstepPlannerParameters;
+      this.usingReferenceStep = new YoBoolean("usingReferenceStep", registry);
    }
 
    @Override
@@ -44,49 +40,25 @@ public class ReferenceBasedIdealStepCalculator implements IdealStepCalculatorInt
       double referenceAlpha = footstepPlannerParameters.getReferencePlanAlpha();
       if (referenceFootstepPlan == null || referenceFootstepPlan.isEmpty() || referenceAlpha == 0.0)
       {
-         statusMessage = "Step calculate mode: Nominal";
          return nominalIdealStep;
       }
 
-      // TODO check that this indexing is correct. The sides should match if setup correctly
-      List<FootstepGraphNode> pathFromStart = footstepGraph.getPathFromStart(new FootstepGraphNode(startOfSwing, stanceNode));
-
-      int stepIndexInPlan = pathFromStart.size() - 1;
-
-      if (stepIndexInPlan >= referenceFootstepPlan.getNumberOfSteps())
+      PlannedFootstep referenceFootstep = getReferenceStep(new FootstepGraphNode(startOfSwing, stanceNode));
+      if (referenceFootstep == null)
       {
-         statusMessage = "Step calculate mode: Nominal";
          return nominalIdealStep;
       }
-
-      PlannedFootstep referenceFootstep = referenceFootstepPlan.getFootstep(stepIndexInPlan);
 
       // TODO: Indexing should match so that referencedStep's side is equal to stance Node's opposite side.
       // In case this is not true, we will try to find the closest step from either previous or next step in the reference plan.
       stepSideIncorrect.set(referenceFootstep.getRobotSide() != stanceNode.getRobotSide().getOppositeSide());
       if (stepSideIncorrect.getBooleanValue())
       {
-         LogTools.error("Wrong side from reference plan, this should not happen ! ! !");
-         return nominalIdealStep;
+         throw new RuntimeException("Invalid side on reference plan");
       }
 
-      // For any reason if alpha is not a number, set it to default value.
-      if (Double.isNaN(referenceAlpha))
-         referenceAlpha = DEFAULT_ALPHA;
-      // Ensure alpha is within the 0 ~ 1.0 range
-      else if (referenceAlpha < 0)
-         referenceAlpha = 0;
-      else if (referenceAlpha > 1.0)
-         referenceAlpha = 1.0;
-
-      statusMessage = "Step calculate mode: Reference (alpha : " + referenceAlpha + " )";
       FramePose3D referenceFootstepPose = referenceFootstep.getFootstepPose();
-
-      double idealStepX = EuclidCoreTools.interpolate(nominalIdealStep.getX(), referenceFootstepPose.getX(), referenceAlpha);
-      double idealStepY = EuclidCoreTools.interpolate(nominalIdealStep.getY(), referenceFootstepPose.getY(), referenceAlpha);
-      double idealStepYaw = AngleTools.interpolateAngle(nominalIdealStep.getYaw(), referenceFootstepPose.getYaw(), referenceAlpha);
-
-      return new DiscreteFootstep(idealStepX, idealStepY, idealStepYaw, referenceFootstep.getRobotSide());
+      return new DiscreteFootstep(referenceFootstepPose.getX(), referenceFootstepPose.getY(), referenceFootstepPose.getYaw(), referenceFootstep.getRobotSide());
    }
 
    public void setReferenceFootstepPlan(FootstepPlan referenceFootstepPlan)
@@ -102,6 +74,25 @@ public class ReferenceBasedIdealStepCalculator implements IdealStepCalculatorInt
    public void setFootstepGraph(DirectedGraph<FootstepGraphNode> footstepGraph)
    {
       this.footstepGraph = footstepGraph;
+   }
+
+   public boolean isUsingReferenceStep()
+   {
+      return usingReferenceStep.getBooleanValue();
+   }
+
+   public PlannedFootstep getReferenceStep(FootstepGraphNode graphNode)
+   {
+      List<FootstepGraphNode> pathFromStart = footstepGraph.getPathFromStart(graphNode);
+      int stepIndexInPlan = pathFromStart.size() - 1;
+      if (stepIndexInPlan < referenceFootstepPlan.getNumberOfSteps())
+      {
+         return referenceFootstepPlan.getFootstep(stepIndexInPlan);
+      }
+      else
+      {
+         return null;
+      }
    }
 
    public DiscreteFootstep getNominalIdealStep()
