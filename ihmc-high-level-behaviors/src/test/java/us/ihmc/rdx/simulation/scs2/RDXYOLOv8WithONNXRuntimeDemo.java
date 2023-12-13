1842a95fa08d9941ea6ee3cd465ab5bef2049e1e
Geoffrey Clark <gclark@ihmc.org>
2023-12-13 12:37:30 -0600

    added ros message for left zed camera

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/simulation/scs2/RDXYOLOv8WithONNXRuntimeDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/simulation/scs2/RDXYOLOv8WithONNXRuntimeDemo.java
@@ -3,11 +3,24 @@ package us.ihmc.rdx.simulation.scs2;
 import imgui.internal.ImGui;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.global.opencv_imgcodecs;
+import org.bytedeco.opencv.global.opencv_imgproc;
 import org.bytedeco.opencv.opencv_core.Mat;
+import perception_msgs.msg.dds.ImageMessage;
+import us.ihmc.communication.PerceptionAPI;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.log.LogTools;
 import us.ihmc.perception.BytedecoImage;
+import us.ihmc.perception.comms.ImageMessageFormat;
 import us.ihmc.perception.opencv.OpenCVArUcoMarker;
 import us.ihmc.perception.opencv.OpenCVArUcoMarkerDetection;
+import us.ihmc.perception.opencv.OpenCVTools;
+import us.ihmc.perception.tools.NativeMemoryTools;
+import us.ihmc.pubsub.DomainFactory;
+import us.ihmc.pubsub.common.SampleInfo;
+import us.ihmc.pubsub.subscriber.Subscriber;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
 import us.ihmc.rdx.imgui.RDXPanel;
 import us.ihmc.rdx.perception.RDXOpenCVArUcoMarkerDetectionUI;
@@ -17,8 +30,17 @@ import us.ihmc.rdx.simulation.sensors.RDXHighLevelDepthSensorSimulator;
 import us.ihmc.rdx.simulation.sensors.RDXSimulatedSensorFactory;
 import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.rdx.ui.gizmo.RDXPose3DGizmo;
+import us.ihmc.rdx.ui.graphics.RDXPerceptionVisualizerPanel;
+import us.ihmc.rdx.ui.graphics.ros2.RDXROS2CenterposeVisualizer;
+import us.ihmc.rdx.ui.graphics.ros2.RDXROS2ImageMessageVisualizer;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.robotics.time.TimeTools;
+import us.ihmc.ros2.ROS2QosProfile;
+import us.ihmc.ros2.ROS2Topic;
+import us.ihmc.ros2.RealtimeROS2Node;
 import us.ihmc.tools.io.WorkspaceDirectory;
 import us.ihmc.tools.io.WorkspaceFile;
+import us.ihmc.tools.string.StringTools;
 
 import java.util.ArrayList;
 
@@ -30,6 +52,9 @@ public class RDXYOLOv8WithONNXRuntimeDemo
         private RDXHighLevelDepthSensorSimulator cameraSensor;
 //        private OpenCVArUcoMarkerDetection arUcoMarkerDetection;
 //        private RDXOpenCVArUcoMarkerDetectionUI arUcoMarkerDetectionUI;
+
+        private final ImageMessage imageMessage = new ImageMessage();
+        private final SampleInfo sampleInfo = new SampleInfo();
 
 
    public RDXYOLOv8WithONNXRuntimeDemo()
@@ -53,12 +78,22 @@ public class RDXYOLOv8WithONNXRuntimeDemo
                     baseUI.getPrimaryScene().addRenderableProvider(sensorPoseGizmo, RDXSceneLevel.VIRTUAL);
                     sensorPoseGizmo.getTransformToParent().appendTranslation(0.0, 0.0, 1.0);
 
-//                    cameraSensor = RDXSimulatedSensorFactory.createBlackflyFisheyeImageOnlyNoComms(sensorPoseGizmo.getGizmoFrame());
-                    cameraSensor = RDXSimulatedSensorFactory.cre
+                    cameraSensor = RDXSimulatedSensorFactory.createBlackflyFisheyeImageOnlyNoComms(sensorPoseGizmo.getGizmoFrame());
                     cameraSensor.setSensorEnabled(true);
                     cameraSensor.setRenderColorVideoDirectly(true);
                     baseUI.getImGuiPanelManager().addPanel(cameraSensor);
                     baseUI.getPrimaryScene().addRenderableProvider(cameraSensor::getRenderables);
+
+                    String titleBeforeAdditions = "ZED 2 Color Left";
+                    DomainFactory.PubSubImplementation pubSubImplementation = DomainFactory.PubSubImplementation.FAST_RTPS;
+                    ROS2Topic<ImageMessage> topic = PerceptionAPI.ZED2_COLOR_IMAGES.get(RobotSide.LEFT);
+                    RealtimeROS2Node realtimeROS2Node = ROS2Tools.createRealtimeROS2Node(pubSubImplementation, StringTools.titleToSnakeCase(titleBeforeAdditions));
+                    ROS2Tools.createCallbackSubscription(realtimeROS2Node, topic, ROS2QosProfile.BEST_EFFORT(), this::getLeftImage);
+
+//                    RDXROS2ImageMessageVisualizer zedLeftColorVisualizer = new RDXROS2ImageMessageVisualizer("ZED 2 Color Left",
+//                                                                                                                DomainFactory.PubSubImplementation.FAST_RTPS,
+//                                                                                                                PerceptionAPI.ZED2_COLOR_IMAGES.get(RobotSide.LEFT));
+
 
 //                    arUcoMarkerDetection = new OpenCVArUcoMarkerDetection();
 //                    arUcoMarkerDetection.create(cameraSensor.getSensorFrame());
@@ -73,6 +108,12 @@ public class RDXYOLOv8WithONNXRuntimeDemo
 //                    baseUI.getImGuiPanelManager().addPanel(arUcoMarkerDetectionUI.getMainPanel());
 //                    baseUI.getPrimaryScene().addRenderableProvider(arUcoMarkerDetectionUI::getRenderables, RDXSceneLevel.VIRTUAL);
 
+                }
+
+                private void getLeftImage(Subscriber<ImageMessage> subscriber)
+                {
+                        imageMessage.getData().resetQuick();
+                        subscriber.takeNextData(imageMessage, sampleInfo);
                 }
 
                 @Override
