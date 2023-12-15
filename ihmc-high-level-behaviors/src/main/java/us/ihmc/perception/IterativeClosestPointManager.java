4ff86fd301258e276b46416c5641aed8f64501fa
Tomasz Bialek <tbialek@ihmc.org>
2023-12-15 17:50:04 -0600

    Added more comments

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/IterativeClosestPointManager.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/IterativeClosestPointManager.java
@@ -6,13 +6,11 @@ import us.ihmc.commons.lists.RecyclingArrayList;
 import us.ihmc.communication.IHMCROS2Input;
 import us.ihmc.communication.PerceptionAPI;
 import us.ihmc.communication.ros2.ROS2Helper;
-import us.ihmc.euclid.matrix.RotationMatrix;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Point3D32;
 import us.ihmc.perception.opencl.OpenCLManager;
 import us.ihmc.perception.sceneGraph.SceneGraph;
 import us.ihmc.perception.sceneGraph.SceneNode;
-import us.ihmc.perception.sceneGraph.rigidBody.primitive.PrimitiveRigidBodySceneNode;
 import us.ihmc.perception.sceneGraph.rigidBody.primitive.PrimitiveRigidBodyShape;
 import us.ihmc.tools.thread.RestartableThrottledThread;
 
@@ -43,28 +41,27 @@ public class IterativeClosestPointManager
       this.sceneGraph = sceneGraph;
 
       requestMessageSubscription = ros2Helper.subscribe(PerceptionAPI.ICP_REQUEST);
+
+      // Each time a request message is received, we update the corresponding worker accordingly
+      // TODO: Find out whether messages can be skipped, resulting in updates being misseed
       requestMessageSubscription.addCallback(requestMessage ->
       {
-         System.out.println("Reveived message");
          long nodeID = requestMessage.getNodeId();
 
          if (!nodeIDToWorkerMap.containsKey(nodeID))     // ICP Worker requested (add new)
          {
-            System.out.println("Adding worker " + nodeID);
             addWorker(requestMessage);
          }
          else if (!requestMessage.getRunIcp())           // ICP Worker no longer needed (remove)
          {
-            System.out.println("Removing worker " + nodeID);
             nodeIDToWorkerMap.remove(nodeID);
          }
-         else                                            // ICP Worker exists (ensure correct size)
+         else                                            // ICP Worker exists (update it)
          {
-            System.out.println("Updating worker " + nodeID);
             IterativeClosestPointWorker worker = nodeIDToWorkerMap.get(nodeID);
 
+            // Check if size changed
             PrimitiveRigidBodyShape shape = PrimitiveRigidBodyShape.fromByte(requestMessage.getShape());
-
             float requestWidth = requestMessage.getYLength();
             float requestHeight = requestMessage.getZLength();
             float requestDepth = requestMessage.getXLength();
@@ -87,12 +84,14 @@ public class IterativeClosestPointManager
                }
             }
 
+            // Update worker size if changed
             if (sizeChanged)
             {
                int newNumberOfPoints = approximateNumberOfPoints(shape, requestWidth, requestHeight, requestDepth, requestLength, requestRadius);
                worker.changeSize(requestWidth, requestHeight, requestDepth, requestLength, requestRadius, newNumberOfPoints);
             }
 
+            // Update worker
             worker.setTargetPoint(requestMessage.getProvidedPose().getPosition());
             worker.useProvidedTargetPoint(requestMessage.getUseProvidedPose());
             worker.setSegmentSphereRadius(requestMessage.getUseProvidedPose() ? 0.3 : 0.2);
@@ -102,6 +101,11 @@ public class IterativeClosestPointManager
       workerThread = new RestartableThrottledThread("ICPWorkers", ICP_WORK_FREQUENCY, this::runWorkers);
    }
 
+   /**
+    * Sets the environment point cloud from a 16UC1 depth image,
+    * then passes the new point cloud to all the workers
+    * @param depthImage 16UC1 image from which the new point cloud is extracted
+    */
    public void setEnvironmentPointCloud(RawImage depthImage)
    {
       depthImage.get();
@@ -132,6 +136,10 @@ public class IterativeClosestPointManager
       openCLManager.destroy();
    }
 
+   /**
+    * Runs all ICP workers present for two iterations,
+    * updates the associated SceneNodes if ICP tracking is enabled
+    */
    private void runWorkers()
    {
       for (long id : nodeIDToWorkerMap.keySet())
@@ -139,9 +147,9 @@ public class IterativeClosestPointManager
          IterativeClosestPointWorker worker = nodeIDToWorkerMap.get(id);
          worker.runICP(2);
 
+         // If ICP isn't using the provided target pose, it'll update the SceneNode to the ICP worker's centroid
          if (!worker.isUsingTargetPoint())
          {
-            System.out.println("Updating node using ICP");
             SceneNode node = sceneGraph.getIDToNodeMap().get(id);
             RigidBodyTransform centroidToWorldTransform = new RigidBodyTransform(worker.getOrientation(), worker.getCentroid());
             node.getNodeToParentFrameTransform().set(centroidToWorldTransform);
@@ -182,9 +190,9 @@ public class IterativeClosestPointManager
       nodeIDToWorkerMap.putIfAbsent(requestMessage.getNodeId(), worker);
    }
 
+   // FIXME: Maybe just allow the user to select number of points instead of trying to calculate it using the dimensions
    private int approximateNumberOfPoints(PrimitiveRigidBodyShape shape, float width, float height, float depth, float length, float radius)
    {
-      int numberOfPoints;
       switch (shape)
       {
          case BOX ->
