376c09bb1fe1c4bbacfee7f9fa031f1e12065213
Duncan Calvert <dcalvert@ihmc.us>
2023-12-26 22:37:30 -0600

    Document taskspace control states.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/rigidBody/RigidBodyPoseController.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/rigidBody/RigidBodyPoseController.java
@@ -29,6 +29,18 @@ import us.ihmc.yoVariables.variable.YoBoolean;
 import us.ihmc.yoVariables.variable.YoDouble;
 import us.ihmc.yoVariables.variable.YoInteger;
 
+/**
+ * A rigid body control mode for position and orientation. It combines the functionalities from
+ * the {@link RigidBodyPositionControlHelper position} and {@link RigidBodyOrientationControlHelper orientation}
+ * control helpers to provide combined spatial feedback control commands for submission to the whole body
+ * controller core.
+ * <p>
+ * This class also features a hybrid control mode, where the jointspace control commands from
+ * {@link RigidBodyJointControlHelper} are also included. This is useful for more control over, for example,
+ * the arms of the robot by specifying desireds in both taskspace and jointspace, which can help avoid
+ * singularities and instabilities.
+ * </p>
+ */
 public class RigidBodyPoseController extends RigidBodyTaskspaceControlState
 {
    private final SpatialFeedbackControlCommand feedbackControlCommand = new SpatialFeedbackControlCommand();
