c9f08238087aa8d8f0a7d171c141fdbfadd050f2
Duncan Calvert <dcalvert@ihmc.us>
2024-01-01 17:02:54 -0600

    The other part.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/ActionNodeExecutor.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/ActionNodeExecutor.java
@@ -1,6 +1,7 @@
 package us.ihmc.behaviors.sequence;
 
 import us.ihmc.behaviors.behaviorTree.BehaviorTreeNodeExecutor;
+import us.ihmc.commons.thread.Notification;
 
 /**
  * Base template for a robot action, like a hand pose or a walk goal.
@@ -9,6 +10,8 @@ public abstract class ActionNodeExecutor<S extends ActionNodeState<D>,
                                          D extends ActionNodeDefinition>
       extends BehaviorTreeNodeExecutor<S, D>
 {
+   private final Notification executionFailedNotification = new Notification();
+
    public ActionNodeExecutor(S state)
    {
       super(state);
@@ -24,5 +27,10 @@ public abstract class ActionNodeExecutor<S extends ActionNodeState<D>,
    public void updateCurrentlyExecuting()
    {
 
+   }
+
+   public Notification getExecutionFailedNotification()
+   {
+      return executionFailedNotification;
    }
 }
