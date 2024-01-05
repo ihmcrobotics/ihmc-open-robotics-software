29398f4b564d506dac1c4a335ae67f3cad19ee8c
Nick Kitchel <nkitchel@ihmc.org>
2024-01-05 10:38:11 -0600

    Update RDXYoVariableClientPanel to extend RDXPanel and update classes

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/yo/RDXYoVariableClientPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/yo/RDXYoVariableClientPanel.java
@@ -3,17 +3,19 @@ package us.ihmc.rdx.ui.yo;
 import us.ihmc.behaviors.tools.yo.YoVariableClientHelper;
 import us.ihmc.rdx.imgui.RDXPanel;
 
-public class RDXYoVariableClientPanel
+public class RDXYoVariableClientPanel extends  RDXPanel
 {
-   private final RDXPanel panel;
    private final YoVariableClientHelper yoVariableClientHelper;
    private final ImGuiYoVariableClientManagerWidget managerWidget;
 
    public RDXYoVariableClientPanel(String registryName, String hostname, int port)
    {
+      super("YoVariableClient");
       yoVariableClientHelper = new YoVariableClientHelper(registryName);
       managerWidget = new ImGuiYoVariableClientManagerWidget(yoVariableClientHelper, () -> hostname, port);
-      panel = new RDXPanel(registryName + " YoVariable Client", managerWidget::renderImGuiWidgets);
+
+      // The setRenderMethod is done at the bottom because the other variables need to be initialized first
+      setRenderMethod(managerWidget::renderImGuiWidgets);
    }
 
    public YoVariableClientHelper getYoVariableClientHelper()
@@ -29,10 +31,5 @@ public class RDXYoVariableClientPanel
    public void destroy()
    {
       managerWidget.destroy();
-   }
-
-   public RDXPanel getPanel()
-   {
-      return panel;
    }
 }
