f48a1155340ef865f2bef538ae0dfc11d2309323
Duncan Calvert <dcalvert@ihmc.us>
2024-01-21 00:45:41 -0600

    Add heel.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/widgets/ImGuiFootstepsWidget.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/widgets/ImGuiFootstepsWidget.java
@@ -15,7 +15,9 @@ public class ImGuiFootstepsWidget
    private final Point2D32 topRight = new Point2D32();
    private final Point2D32 bottomLeft = new Point2D32();
    private final Point2D32 bottomRight = new Point2D32();
-   private final ImVec2[] polygon = new ImVec2[6];
+   private final Point2D32 heelLeft = new Point2D32();
+   private final Point2D32 heelRight = new Point2D32();
+   private final ImVec2[] polygon = new ImVec2[8];
    private float cursorXDesktopFrame;
    private float cursorYDesktopFrame;
    private int lineColor;
@@ -52,14 +54,18 @@ public class ImGuiFootstepsWidget
       float halfWidth = 0.34f;
       float halfHeight = 0.7f;
       float toeHalfWidth = 0.17f;
-      float toeHeight = 0.22f;
+      float toeHeight = 0.26f;
+      float heelHeight = 0.1f;
+      float heelHalfWidth = 0.26f;
 
       toeLeft.set(-toeHalfWidth, -halfHeight);
       toeRight.set(toeHalfWidth, -halfHeight);
       topLeft.set(-halfWidth, -halfHeight + toeHeight);
       topRight.set(halfWidth, -halfHeight + toeHeight);
-      bottomLeft.set(-halfWidth, halfHeight);
-      bottomRight.set(halfWidth, halfHeight);
+      bottomLeft.set(-halfWidth, halfHeight - heelHeight);
+      bottomRight.set(halfWidth, halfHeight - heelHeight);
+      heelLeft.set(-heelHalfWidth, halfHeight);
+      heelRight.set(heelHalfWidth, halfHeight);
 
       toeLeft.scaleAdd(scale, center);
       toeRight.scaleAdd(scale, center);
@@ -67,6 +73,8 @@ public class ImGuiFootstepsWidget
       topRight.scaleAdd(scale, center);
       bottomLeft.scaleAdd(scale, center);
       bottomRight.scaleAdd(scale, center);
+      heelLeft.scaleAdd(scale, center);
+      heelRight.scaleAdd(scale, center);
 
       float itemWidth = bottomRight.getX32() - bottomLeft.getX32();
       isHovered = ImGuiTools.isItemHovered(itemWidth);
@@ -79,13 +87,15 @@ public class ImGuiFootstepsWidget
       backgroundColor = side == RobotSide.LEFT ? ImGuiTools.DARK_RED : ImGuiTools.DARK_GREEN;
 
       polygon[0].set(cursorXDesktopFrame + bottomRight.getX32(), cursorYDesktopFrame + bottomRight.getY32());
-      polygon[1].set(cursorXDesktopFrame + bottomLeft.getX32(), cursorYDesktopFrame + bottomLeft.getY32());
-      polygon[2].set(cursorXDesktopFrame + topLeft.getX32(), cursorYDesktopFrame + topLeft.getY32());
-      polygon[3].set(cursorXDesktopFrame + toeLeft.getX32(), cursorYDesktopFrame + toeLeft.getY32());
-      polygon[4].set(cursorXDesktopFrame + toeRight.getX32(), cursorYDesktopFrame + toeRight.getY32());
-      polygon[5].set(cursorXDesktopFrame + topRight.getX32(), cursorYDesktopFrame + topRight.getY32());
+      polygon[1].set(cursorXDesktopFrame + heelRight.getX32(), cursorYDesktopFrame + heelRight.getY32());
+      polygon[2].set(cursorXDesktopFrame + heelLeft.getX32(), cursorYDesktopFrame + heelLeft.getY32());
+      polygon[3].set(cursorXDesktopFrame + bottomLeft.getX32(), cursorYDesktopFrame + bottomLeft.getY32());
+      polygon[4].set(cursorXDesktopFrame + topLeft.getX32(), cursorYDesktopFrame + topLeft.getY32());
+      polygon[5].set(cursorXDesktopFrame + toeLeft.getX32(), cursorYDesktopFrame + toeLeft.getY32());
+      polygon[6].set(cursorXDesktopFrame + toeRight.getX32(), cursorYDesktopFrame + toeRight.getY32());
+      polygon[7].set(cursorXDesktopFrame + topRight.getX32(), cursorYDesktopFrame + topRight.getY32());
 
-      ImGui.getWindowDrawList().addConvexPolyFilled(polygon, 6, backgroundColor);
+      ImGui.getWindowDrawList().addConvexPolyFilled(polygon, polygon.length, backgroundColor);
 
       ImGui.setCursorPosX(ImGui.getCursorPosX() + (itemWidth * 1.2f));
 
