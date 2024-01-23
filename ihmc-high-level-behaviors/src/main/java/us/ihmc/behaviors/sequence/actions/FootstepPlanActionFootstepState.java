983b50a61751b4ca60da81fae08ddcb6f5314e1b
Duncan Calvert <dcalvert@ihmc.us>
2024-01-22 21:28:42 -0600

    More work on merging the footstep plan actions.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/FootstepPlanActionFootstepState.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/actions/FootstepPlanActionFootstepState.java
@@ -20,7 +20,7 @@ public class FootstepPlanActionFootstepState
       this.definition = definition;
 
       soleFrame = new CRDTDetachableReferenceFrame(referenceFrameLibrary,
-                                                   footstepPlan.getDefinition().getBasics().getCRDTParentFrameName(),
+                                                   footstepPlan.getDefinition().getCRDTParentFrameName(),
                                                    definition.getSoleToPlanFrameTransform());
    }
 
