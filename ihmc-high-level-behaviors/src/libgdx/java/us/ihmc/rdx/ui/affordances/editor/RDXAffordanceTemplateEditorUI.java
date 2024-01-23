94fef601f7ac8bf505834052c2b9c9e20ff3bf2f
Dhruv Thanki <thankid@udel.edu>
2024-01-23 14:52:39 -0600

    fixes

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/editor/RDXAffordanceTemplateEditorUI.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/editor/RDXAffordanceTemplateEditorUI.java
@@ -11,9 +11,13 @@ import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.log.LogTools;
 import us.ihmc.perception.sceneGraph.SceneGraph;
+import us.ihmc.perception.sceneGraph.modification.SceneGraphNodeAddition;
 import us.ihmc.perception.sceneGraph.multiBodies.door.DoorSceneNodeDefinitions;
 import us.ihmc.perception.sceneGraph.rigidBody.RigidBodySceneObjectDefinitions;
+import us.ihmc.perception.sceneGraph.rigidBody.primitive.PrimitiveRigidBodySceneNode;
+import us.ihmc.perception.sceneGraph.rigidBody.primitive.PrimitiveRigidBodyShape;
 import us.ihmc.rdx.imgui.ImGuiDirectory;
 import us.ihmc.rdx.imgui.ImGuiInputText;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
@@ -80,27 +84,11 @@ public class RDXAffordanceTemplateEditorUI
    private final ImGuiInputText textInput = new ImGuiInputText("(optional) Enter additional description");
    private final ImGuiDirectory fileManagerDirectory;
 
-   public RDXAffordanceTemplateEditorUI(RDXBaseUI baseUI, SceneGraph uiSceneGraph)
+   public RDXAffordanceTemplateEditorUI(RDXBaseUI baseUI)
    {
       panel3D = baseUI.getPrimary3DPanel();
 
-      ReferenceFrameLibrary referenceFrameLibrary = new ReferenceFrameLibrary();
-
-      SceneGraph sceneGraph = new SceneGraph(referenceFrameLibrary);
-      sceneGraph.modifyTree(modificationQueue ->
-                            {
-                               DoorSceneNodeDefinitions.ensureRightPushDoorNodesAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               DoorSceneNodeDefinitions.ensureLeftPushDoorNodesAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureBoxNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureCanOfSoupNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureDebrisNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureShoeNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureLaptopNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureBookNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureCerealNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureMugNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                               RigidBodySceneObjectDefinitions.ensureBikeNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
-                            });
+      SceneGraph sceneGraph = getSceneGraph();
 
       objectBuilder = new RDXInteractableObjectBuilder(baseUI, sceneGraph);
       baseUI.getImGuiPanelManager().addPanel(objectBuilder.getWindowName(), objectBuilder::renderImGuiWidgets);
@@ -169,6 +157,43 @@ public class RDXAffordanceTemplateEditorUI
                                                              && !pathEntry.path().getFileName().toString().contains("Frames")
                                                              && !pathEntry.path().getFileName().toString().contains("Extra"),
                                                 fileManager::setLoadingFile);
+   }
+
+   private static SceneGraph getSceneGraph()
+   {
+      SceneGraph sceneGraph = new SceneGraph(new ReferenceFrameLibrary());
+      sceneGraph.modifyTree(modificationQueue ->
+                            {
+                               DoorSceneNodeDefinitions.ensureRightPushDoorNodesAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               DoorSceneNodeDefinitions.ensureLeftPushDoorNodesAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureBoxNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureCanOfSoupNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureDebrisNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureShoeNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureLaptopNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureBookNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureCerealNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureMugNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               RigidBodySceneObjectDefinitions.ensureBikeNodeAdded(sceneGraph, modificationQueue, sceneGraph.getRootNode());
+                               for (PrimitiveRigidBodyShape primitiveShape : PrimitiveRigidBodyShape.values())
+                               {
+                                  if (primitiveShape != PrimitiveRigidBodyShape.CUSTOM)
+                                  {
+                                     String primitiveShapename =
+                                           Character.toUpperCase(primitiveShape.name().charAt(0)) + primitiveShape.name().substring(1).toLowerCase();
+                                     PrimitiveRigidBodySceneNode primitiveRigidBodySceneNode = new PrimitiveRigidBodySceneNode(sceneGraph.getNextID()
+                                                                                                                                         .getAndIncrement(),
+                                                                                                                               "Resizable" + primitiveShapename,
+                                                                                                                               sceneGraph.getIDToNodeMap(),
+                                                                                                                               sceneGraph.getRootNode().getID(),
+                                                                                                                               new RigidBodyTransform(),
+                                                                                                                               primitiveShape);
+                                     LogTools.info("Adding Resizable " + primitiveShapename + " Node to scene graph.");
+                                     modificationQueue.accept(new SceneGraphNodeAddition(primitiveRigidBodySceneNode, sceneGraph.getRootNode()));
+                                  }
+                               }
+                            });
+      return sceneGraph;
    }
 
    public void update()
