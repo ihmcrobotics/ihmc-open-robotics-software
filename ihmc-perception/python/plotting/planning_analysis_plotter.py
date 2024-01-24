1a96339a075cb5f79748e6545a2cb3f1076c895a
BhavyanshM <bhavyanshmishra@gmail.com>
2024-01-24 17:44:55 -0600

    Add scripts for plotting variances for planner logs.

--- ihmc-perception/python/plotting/planning_analysis_plotter.py
+++ ihmc-perception/python/plotting/planning_analysis_plotter.py
@@ -43,26 +43,32 @@ plt.figure(figsize=(18, 12))
 
 plt.subplot(3, 2, 1)
 plt.plot(total_time)
+plt.fill_between(range(len(total_time)), total_time - np.std(total_time), total_time + np.std(total_time), alpha=0.3, color='red')
 plt.title("Total Time")
 
 plt.subplot(3, 2, 2)
 plt.plot(simulation_time)
+plt.fill_between(range(len(simulation_time)), simulation_time - np.std(simulation_time), simulation_time + np.std(simulation_time), alpha=0.3, color='red')
 plt.title("Simulation Time")
 
 plt.subplot(3, 2, 3)
 plt.plot(expansion_time)
+plt.fill_between(range(len(expansion_time)), expansion_time - np.std(expansion_time), expansion_time + np.std(expansion_time), alpha=0.3, color='red')
 plt.title("Expansion Time")
 
 plt.subplot(3, 2, 4)
 plt.plot(pruning_time)
+plt.fill_between(range(len(pruning_time)), pruning_time - np.std(pruning_time), pruning_time + np.std(pruning_time), alpha=0.3, color='red')
 plt.title("Pruning Time")
 
 plt.subplot(3, 2, 5)
 plt.plot(propagation_time)
+plt.fill_between(range(len(propagation_time)), propagation_time - np.std(propagation_time), propagation_time + np.std(propagation_time), alpha=0.3, color='red')
 plt.title("Propagation Time")
 
 plt.subplot(3, 2, 6)
 plt.plot(search_time)
+plt.fill_between(range(len(search_time)), search_time - np.std(search_time), search_time + np.std(search_time), alpha=0.3, color='red')
 plt.title("Search Time")
 
 # Display the plots
