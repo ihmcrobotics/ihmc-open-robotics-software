daea3a4fe5e8372b2c118a41fbbd8c631c068b61
Frexicane <135539994+Frexicane@users.noreply.github.com>
2024-01-31 14:30:19 -0600

    COMMIT MESSAGE

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkingRobotDefinition.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkingRobotDefinition.java
@@ -12,6 +12,7 @@ import us.ihmc.scs2.definition.visual.ColorDefinition;
 import us.ihmc.scs2.definition.visual.VisualDefinition;
 
 import java.awt.*;
+//this is a test for pushing
 
 public class BWCPlanarWalkingRobotDefinition extends RobotDefinition
 {
