997aeb53799bbbedaff99264123e494790784a4f
Nick Kitchel <nkitchel@ihmc.org>
2024-01-30 19:41:26 -0600

    Create simple robot and get the simulation running in SCS2

---
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkerSimulation.java
@@ -0,0 +1,26 @@
+package us.ihmc.exampleSimulations.planarWalker;
+
+import geometry_msgs.Vector3;
+import us.ihmc.scs2.SimulationConstructionSet2;
+
+public class BWCPlanarWalkerSimulation
+{
+   public BWCPlanarWalkerSimulation()
+   {
+      SimulationConstructionSet2 scs = new SimulationConstructionSet2();
+      scs.getGravity().setToZero();
+
+      BWCPlanarWalkingRobotDefinition robotDefinition = new BWCPlanarWalkingRobotDefinition();
+      scs.addRobot(robotDefinition);
+      // TODO create the ground.
+      // TODO create a robot controller and add it to the robot
+
+      scs.startSimulationThread();
+      scs.simulate();
+   }
+
+   public static void main(String[] args)
+   {
+      new BWCPlanarWalkerSimulation();
+   }
+}
