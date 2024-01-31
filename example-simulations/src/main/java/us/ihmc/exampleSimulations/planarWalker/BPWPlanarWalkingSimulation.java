4d1b86e7f139e4f12bef8abe2689a1eb6c333b24
cshah <cshah@ihmc.org>
2024-01-31 16:49:28 -0600

    Added the simple planar walker code for Bipedal Walking Class

---
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BPWPlanarWalkingSimulation.java
@@ -0,0 +1,27 @@
+package us.ihmc.exampleSimulations.planarWalker;
+
+import us.ihmc.scs2.SimulationConstructionSet2;
+
+public class BPWPlanarWalkingSimulation {
+
+    public BPWPlanarWalkingSimulation()
+    {
+        SimulationConstructionSet2 scs = new SimulationConstructionSet2();
+        scs.getGravity().setToZero();
+
+        // Todo create a robot and add it to scs
+        BPWPlanarWalkingRobotDefinition robotDefinition = new BPWPlanarWalkingRobotDefinition();
+        scs.addRobot(robotDefinition);
+
+
+
+        scs.startSimulationThread();
+        scs.simulate();
+    }
+
+    public static void main(String[] args)
+    {
+        new BPWPlanarWalkingSimulation();
+
+    }
+}
