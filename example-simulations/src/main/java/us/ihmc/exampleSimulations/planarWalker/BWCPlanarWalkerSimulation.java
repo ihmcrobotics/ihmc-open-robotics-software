b1001a0d2ff0d80bd02416fcb1b3860af1bdac09
Robert Griffin <rgriffin@ihmc.us>
2024-02-01 09:26:55 -0600

    added the ground, added ground contacts, added a controller robot, added a controller, and got the controller standing in place

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkerSimulation.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkerSimulation.java
@@ -1,18 +1,28 @@
 package us.ihmc.exampleSimulations.planarWalker;
 
 import us.ihmc.scs2.SimulationConstructionSet2;
+import us.ihmc.scs2.definition.terrain.TerrainObjectDefinition;
+import us.ihmc.scs2.simulation.robot.Robot;
+import us.ihmc.simulationConstructionSetTools.util.environments.CommonAvatarEnvironmentInterface;
 
 public class BWCPlanarWalkerSimulation
 {
    public BWCPlanarWalkerSimulation()
    {
       SimulationConstructionSet2 scs = new SimulationConstructionSet2();
-      scs.getGravity().setToZero();
+//      scs.getGravity().setToZero();
 
       BWCPlanarWalkingRobotDefinition robotDefinition = new BWCPlanarWalkingRobotDefinition();
-      scs.addRobot(robotDefinition);
-      // TODO create the ground.
-      // todo create a robot controller and add it to the robot
+      Robot robot = new Robot(robotDefinition, scs.getInertialFrame());
+      scs.addRobot(robot);
+      scs.addTerrainObject(new SlopeGroundDefinition(0.0));
+
+      // set up the controller robot that has convenience methods for us to do control things with.
+      BWCPlanarWalkingRobot controllerRobot = new BWCPlanarWalkingRobot(robot);
+      // create the robot controller
+      BWCPlanarWalkingController controller = new BWCPlanarWalkingController(controllerRobot);
+      // set the controller to control the robot.
+      robot.addController(controller);
 
       scs.startSimulationThread();
       scs.simulate();
