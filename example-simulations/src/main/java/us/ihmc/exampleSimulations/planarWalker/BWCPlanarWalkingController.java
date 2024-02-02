da1f7cafe18e17dfa2c5ec0eb0bdc721663a7d32
Christian DeBuys <christian.debuys@boardwalkrobotics.com>
2024-02-02 16:20:07 -0600

    Adds a walking controller (more of a standing controller for now). Adds a walking robot. Adds a ground definition. Adds ground contact points in the feet. Refactors into sided lists to shorten everything.

---
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkingController.java
@@ -0,0 +1,72 @@
+package us.ihmc.exampleSimulations.planarWalker;
+
+import us.ihmc.robotics.controllers.PDController;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.robotics.robotSide.SideDependentList;
+import us.ihmc.scs2.definition.controller.interfaces.Controller;
+import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoDouble;
+
+public class BWCPlanarWalkingController implements Controller
+{
+   private final YoRegistry registry = new YoRegistry(getClass().getSimpleName());
+
+   private BWCPlanarWalkingRobot controllerRobot;
+
+   private final SideDependentList<PDController> kneeLengthControllers = new SideDependentList<>();
+   private final SideDependentList<YoDouble> desiredKneeForces = new SideDependentList<>();
+
+   private final YoDouble desiredLegLength = new YoDouble("desiredLegLength", registry);
+
+   public BWCPlanarWalkingController(BWCPlanarWalkingRobot controllerRobot)
+   {
+      this.controllerRobot = controllerRobot;
+
+      // set up our leg length gains for the leg length controller
+      YoDouble legLengthKp = new YoDouble("legLengthKp", registry);
+      YoDouble legLengthKd = new YoDouble("legLengthKd", registry);
+      legLengthKp.set(1500.0);
+      legLengthKd.set(50.0);
+
+      desiredLegLength.set((BWCPlanarWalkingRobotDefinition.shinLength + BWCPlanarWalkingRobotDefinition.thighLength) / 2.0);
+
+      // set up our leg length controllers
+      PDController leftLegLengthController = new PDController(legLengthKp, legLengthKd, "leftLegLengthController", registry);
+      PDController rightLegLengthController = new PDController(legLengthKp, legLengthKd, "rightLegLengthController", registry);
+      YoDouble leftLegDesiredKneeForce = new YoDouble("leftLegDesiredKneeForce", registry);
+      YoDouble rightLegDesiredKneeForce = new YoDouble("rightLegDesiredKneeForce", registry);
+      kneeLengthControllers.put(RobotSide.LEFT, leftLegLengthController);
+      kneeLengthControllers.put(RobotSide.RIGHT, rightLegLengthController);
+      desiredKneeForces.put(RobotSide.LEFT, leftLegDesiredKneeForce);
+      desiredKneeForces.put(RobotSide.RIGHT, rightLegDesiredKneeForce);
+
+      registry.addChild(controllerRobot.getYoRegistry());
+   }
+
+   @Override
+   public YoRegistry getYoRegistry()
+   {
+      return registry;
+   }
+
+   @Override
+   public void doControl()
+   {
+      controllerRobot.update();
+
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         double legLength = controllerRobot.getLegLength(robotSide);
+         double kneeVelocity = -controllerRobot.getKneeJoint(robotSide).getQd();
+
+         double desiredLegLength = this.desiredLegLength.getDoubleValue();
+         double desiredKneeVelocity = 0.0;
+         // compute and record the desired knee force to hold the leg at the desired length
+         double desiredKneeForce = kneeLengthControllers.get(robotSide).compute(legLength, desiredLegLength, kneeVelocity, desiredKneeVelocity);
+         desiredKneeForces.get(robotSide).set(desiredKneeForce);
+
+         // set the desired force to the knee joint to hold the leg at the desired length
+         controllerRobot.getKneeJoint(robotSide).setTau(desiredKneeForce);
+      }
+   }
+}
--- example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkingRobotDefinition.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/planarWalker/BWCPlanarWalkingRobotDefinition.java
@@ -2,6 +2,8 @@ package us.ihmc.exampleSimulations.planarWalker;
 
 import us.ihmc.euclid.Axis3D;
 import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.scs2.definition.geometry.Ellipsoid3DDefinition;
 import us.ihmc.scs2.definition.geometry.GeometryDefinition;
 import us.ihmc.scs2.definition.robot.*;
@@ -23,24 +25,20 @@ public class BWCPlanarWalkingRobotDefinition extends RobotDefinition
    public static final String leftKneeName = "leftKnee";
    public static final String rightKneeName = "rightKnee";
 
-   private static final double torsoLength = 0.5;
-   private static final double thighLength = 0.5;
-   private static final double shinLength = 0.5;
+   public static final double torsoLength = 0.5;
+   public static final double thighLength = 0.5;
+   public static final double shinLength = 0.5;
+
+   private final SideDependentList<String> hipPitchNames = new SideDependentList<>(leftHipPitchName, rightHipPitchName);
+   private final SideDependentList<String> thighNames = new SideDependentList<>(leftThighName, rightThighName);
+   public static final SideDependentList<String> kneeNames = new SideDependentList<>(leftKneeName, rightKneeName);
+   private final SideDependentList<String> shinNames = new SideDependentList<>(leftShinName, rightShinName);
+
+   private final SideDependentList<RevoluteJointDefinition> hipPitchJointDefinitions = new SideDependentList<>();
 
    private final PlanarJointDefinition floatingBaseDefinition;
-   private final RevoluteJointDefinition leftHipPitchJointDefinition;
-   private final RevoluteJointDefinition rightHipPitchJointDefinition;
-
-   private final PrismaticJointDefinition leftKneeJointDefinition;
-   private final PrismaticJointDefinition rightKneeJointDefinition;
 
    private final RigidBodyDefinition torsoBodyDefinition;
-
-   private final RigidBodyDefinition leftUpperLeg;
-   private final RigidBodyDefinition rightUpperLeg;
-
-   private final RigidBodyDefinition leftLowerLeg;
-   private final RigidBodyDefinition rightLowerLeg;
 
    public BWCPlanarWalkingRobotDefinition()
    {
@@ -54,40 +52,32 @@ public class BWCPlanarWalkingRobotDefinition extends RobotDefinition
       torsoBodyDefinition = createTorso();
       floatingBaseDefinition.setSuccessor(torsoBodyDefinition);
 
-      // Create the hip pitch joints and add them to the tree
-      Vector3D leftHipPitchOffsetTorso = new Vector3D(0.0, 0.05, -0.2);
-      leftHipPitchJointDefinition = new RevoluteJointDefinition(leftHipPitchName, leftHipPitchOffsetTorso, Axis3D.Y);
+      for (RobotSide robotSide : RobotSide.values)
+      {
+         // Create the hip pitch joints and add them to the tree
+         Vector3D hipPitchOffsetTorso = new Vector3D(0.0, robotSide.negateIfRightSide(0.5), -torsoLength / 2.0);
+         RevoluteJointDefinition hipPitchJointDefinition = new RevoluteJointDefinition(hipPitchNames.get(robotSide), hipPitchOffsetTorso, Axis3D.Y);
+         torsoBodyDefinition.addChildJoint(hipPitchJointDefinition);
 
-      Vector3D rightHipPitchOffsetTorso = new Vector3D(0.0, -0.05, -0.2);
-      rightHipPitchJointDefinition = new RevoluteJointDefinition(rightHipPitchName, rightHipPitchOffsetTorso, Axis3D.Y);
+         // Create the upper leg links and add them to the tree
+         // FIXME: we probably need to add an offset so the link isn't placed at the origin
+         RigidBodyDefinition thighLink = createThigh(thighNames.get(robotSide));
+         hipPitchJointDefinition.setSuccessor(thighLink);
 
-      torsoBodyDefinition.addChildJoint(leftHipPitchJointDefinition);
-      torsoBodyDefinition.addChildJoint(rightHipPitchJointDefinition);
+         // Create the knee joints and add them to the tree
+         Vector3D kneeOffsetInThigh = new Vector3D(0.0, 0.0, -thighLength / 2.0);
+         PrismaticJointDefinition kneeJointDefinition = new PrismaticJointDefinition(kneeNames.get(robotSide), kneeOffsetInThigh, Axis3D.Z);
+         thighLink.addChildJoint(kneeJointDefinition);
 
-      // Create the upper leg links and add them to the tree
-      // FIXME: we probably need to add an offset so the link isn't placed at the origin
-      leftUpperLeg = createThigh(leftThighName);
-      leftHipPitchJointDefinition.setSuccessor(leftUpperLeg);
+         // Create the lower leg links and add them to the tree
+         // FIXME: we probably need to add an offset so the link isn't placed at the origin
+         RigidBodyDefinition shinLink = createShin(shinNames.get(robotSide));
+         kneeJointDefinition.setSuccessor(shinLink);
 
-      rightUpperLeg = createThigh(rightThighName);
-      rightHipPitchJointDefinition.setSuccessor(rightUpperLeg);
-
-      // Create the knee joints and add them to the tree
-      Vector3D leftKneeOffsetInThigh = new Vector3D(0.0, 0.0, -thighLength/2.0);
-      leftKneeJointDefinition = new PrismaticJointDefinition(leftKneeName, leftKneeOffsetInThigh, Axis3D.Z);
-      leftUpperLeg.addChildJoint(leftKneeJointDefinition);
-
-      Vector3D rightKneeOffsetInThigh = new Vector3D(0.0, 0.0, -thighLength/2.0);
-      rightKneeJointDefinition = new PrismaticJointDefinition(rightKneeName, rightKneeOffsetInThigh, Axis3D.Z);
-      rightUpperLeg.addChildJoint(rightKneeJointDefinition);
-
-      // Create the lower leg links and add them to the tree
-      // FIXME: we probably need to add an offset so the link isn't placed at the origin
-      leftLowerLeg = createShin(leftShinName);
-      leftKneeJointDefinition.setSuccessor(leftLowerLeg);
-
-      rightLowerLeg = createShin(rightShinName);
-      rightKneeJointDefinition.setSuccessor(rightLowerLeg);
+         // Create the contact points for the feet
+         GroundContactPointDefinition footContactPoint = new GroundContactPointDefinition(robotSide.getLowerCaseName() + "_gc_point", new Vector3D(0.0, 0.0, -shinLength / 2.0));
+         kneeJointDefinition.addGroundContactPointDefinition(footContactPoint);
+      }
 
       //TODO: add collisions shapes
    }
