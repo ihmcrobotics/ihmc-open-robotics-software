1bfa0d07f93f22c2d67264b12b5cb65f6fe95211
James Foster <jfoster@ihmc.org>
2024-02-06 10:42:27 -0600

    Remove changes to NadiaSensorReader and JointStateUpdater

--- ihmc-state-estimation/src/main/java/us/ihmc/stateEstimation/humanoid/kinematicsBasedStateEstimation/JointStateUpdater.java
+++ ihmc-state-estimation/src/main/java/us/ihmc/stateEstimation/humanoid/kinematicsBasedStateEstimation/JointStateUpdater.java
@@ -127,7 +127,6 @@ public class JointStateUpdater
 
          double positionSensorData = jointSensorOutput.getPosition();
          double velocitySensorData = jointSensorOutput.getVelocity();
-         double accelerationSensorData = jointSensorOutput.getAcceleration();
          double torqueSensorData = jointSensorOutput.getEffort();
 
          for (int j = 0; j < imuBasedJointStateEstimators.size(); j++)
@@ -150,7 +149,6 @@ public class JointStateUpdater
 
          oneDoFJoint.setQ(positionSensorData);
          oneDoFJoint.setQd(velocitySensorData);
-         oneDoFJoint.setQdd(accelerationSensorData);
          oneDoFJoint.setTau(torqueSensorData);
       }
 
