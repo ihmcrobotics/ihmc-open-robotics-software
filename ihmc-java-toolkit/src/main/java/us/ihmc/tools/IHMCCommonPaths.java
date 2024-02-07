e1cc1f96758fd5121e550e949377756e5f1bf22f
BhavyanshM <bhavyanshmishra@gmail.com>
2024-02-06 18:05:24 -0600

    Get plan logger all set for creating datasets.

--- ihmc-java-toolkit/src/main/java/us/ihmc/tools/IHMCCommonPaths.java
+++ ihmc-java-toolkit/src/main/java/us/ihmc/tools/IHMCCommonPaths.java
@@ -23,6 +23,9 @@ public class IHMCCommonPaths
    public static final String PLANNING_DATASETS_DIRECTORY_NAME = "planning-datasets";
    public static final Path PLANNING_DATASETS_DIRECTORY = LOGS_DIRECTORY.resolve(PLANNING_DATASETS_DIRECTORY_NAME);
 
+   public static final String ELEVATION_DATASETS_DIRECTORY_NAME = "elevation-maps";
+   public static final Path ELEVATION_DATASETS_DIRECTORY = PLANNING_DATASETS_DIRECTORY.resolve(ELEVATION_DATASETS_DIRECTORY_NAME);
+
    public static final String CONTINUOUS_PLANNING_DIRECTORY_NAME = "continuous-planning";
    public static final Path CONTINUOUS_PLANNING_DIRECTORY = LOGS_DIRECTORY.resolve(CONTINUOUS_PLANNING_DIRECTORY_NAME);
 
