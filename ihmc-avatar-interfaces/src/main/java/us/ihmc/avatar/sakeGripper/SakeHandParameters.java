361e059693bc38c1027f1ca3bb025fa9b4482b3b
Duncan Calvert <dcalvert@ihmc.us>
2024-02-15 20:24:38 -0600

    Simulate hand temperature.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sakeGripper/SakeHandParameters.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sakeGripper/SakeHandParameters.java
@@ -8,23 +8,33 @@ public class SakeHandParameters
     * When hand is fully open, fingertips form 210 degrees angle.
     * This corresponds to the normalized value of 1.0.
     */
-   public static double MAX_DESIRED_HAND_OPEN_ANGLE_DEGREES = 210.0;
+   public static final double MAX_DESIRED_HAND_OPEN_ANGLE_DEGREES = 210.0;
    /** Joint angle of a finger is -3 degrees when fully closed */
-   public static double CLOSED_KNUCKLE_JOINT_ANGLE_DEGREES = -3.0;
+   public static final double CLOSED_KNUCKLE_JOINT_ANGLE_DEGREES = -3.0;
    /** Joint angle of a finger is 102 degrees when fully open */
-   public static double OPEN_KNUCKLE_JOINT_ANGLE_DEGREES = 102.0;
+   public static final double OPEN_KNUCKLE_JOINT_ANGLE_DEGREES = 102.0;
    /** Sake hand can produce 29 N of grip force between the fingertips */
-   public static double FINGERTIP_GRIP_FORCE_HARDWARE_LIMIT = 29.0;
+   public static final double FINGERTIP_GRIP_FORCE_HARDWARE_LIMIT = 29.0;
    /** This is a safe amount of force. */
-   public static double FINGERTIP_GRIP_FORCE_SAFE = 8.7;
+   public static final double FINGERTIP_GRIP_FORCE_SAFE = 8.7;
    /** This is a moderate amount of force. */
-   public static double FINGERTIP_GRIP_FORCE_MODERATE_THRESHOLD = 14.5;
+   public static final double FINGERTIP_GRIP_FORCE_MODERATE_THRESHOLD = 14.5;
    /** This is a high amount of force. */
-   public static double FINGERTIP_GRIP_FORCE_HIGH_THRESHOLD = 20.3;
+   public static final double FINGERTIP_GRIP_FORCE_HIGH_THRESHOLD = 20.3;
    /** The stall torque of the dynamixel that drives the gripper. */
-   public static double DYNAMIXEL_MX_64AR_STALL_TORQUE = 6.0;
+   public static final double DYNAMIXEL_MX_64AR_STALL_TORQUE = 6.0;
    /** Rough guess how much knuckle torque corresponding to our limit above. */
-   public static double MAX_KNUCKLE_TORQUE = 4.5;
+   public static final double KNUCKLE_TORQUE_AT_LIMIT = 2.0;
+   /** Normal hand temperature. */
+   public static final double NORMAL_TEMPERATURE_CELCIUS = 30.0;
+   /** Warning hand temperature. */
+   public static final double WARNING_TEMPERATURE_CELCIUS = 40.0;
+   /** Red colored hand temperature. */
+   public static final double ERROR_TEMPERATURE_CELCIUS = 50.0;
+   /** The max temperature before the hand resets. */
+   public static final double TEMPERATURE_LIMIT_CELCIUS = 60.0;
+   /** The temperature at which the Dynamixel will break. */
+   public static final double DYNAMIXEL_FAILURE_TEMPERATURE_CELCIUS = 80.0;
 
    /**
     * @param normalizedHandOpenAngle 0.0 (closed) to 1.0 (open)
@@ -56,12 +66,12 @@ public class SakeHandParameters
 
    public static double denormalizeKnuckleTorque(double knuckleTorque)
    {
-      return knuckleTorque * MAX_KNUCKLE_TORQUE;
+      return knuckleTorque * KNUCKLE_TORQUE_AT_LIMIT;
    }
 
    public static double normalizeKnuckleTorque(double knuckleTorque)
    {
-      return knuckleTorque / MAX_KNUCKLE_TORQUE;
+      return knuckleTorque / KNUCKLE_TORQUE_AT_LIMIT;
    }
 
    public static double knuckleJointAngleToHandOpenAngle(double knuckleJointAngle)
