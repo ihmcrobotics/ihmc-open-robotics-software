ba49024e2b3f0cf9206d6b378b379d4902541d06
smccrory <smccrory@ihmc.us>
2024-02-27 15:09:05 -0600

    Increasing samples for function generator error calculator

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/functionGenerator/FunctionGeneratorErrorCalculator.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/functionGenerator/FunctionGeneratorErrorCalculator.java
@@ -4,6 +4,7 @@ import gnu.trove.list.array.TDoubleArrayList;
 import us.ihmc.euclid.tools.EuclidCoreTools;
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.robotics.math.functionGenerator.YoFunctionGeneratorNew;
+import us.ihmc.yoVariables.providers.DoubleProvider;
 import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoDouble;
 import us.ihmc.yoVariables.variable.YoInteger;
@@ -17,7 +18,7 @@ import java.util.List;
  */
 public class FunctionGeneratorErrorCalculator
 {
-   private static final int SAMPLES_PER_PERIOD = 100;
+   private static final int MAX_SAMPLES = 600;
    private final double controlDT;
    private final YoLong controllerCounter;
 
@@ -31,9 +32,9 @@ public class FunctionGeneratorErrorCalculator
       controllerCounter = new YoLong(namePrefix + "controllerCounter", registry);
    }
 
-   public void addTrajectorySignal(YoFunctionGeneratorNew functionGenerator, OneDoFJointBasics joint)
+   public void addTrajectorySignal(YoFunctionGeneratorNew functionGenerator, DoubleProvider baselineDesiredValue, OneDoFJointBasics joint)
    {
-      trajectorySignals.add(new TrajectorySignal(functionGenerator, joint, registry));
+      trajectorySignals.add(new TrajectorySignal(functionGenerator, joint, baselineDesiredValue, registry));
    }
 
    public void update()
@@ -51,23 +52,25 @@ public class FunctionGeneratorErrorCalculator
       private final YoFunctionGeneratorNew functionGenerator;
       private final OneDoFJointBasics joint;
       private final YoDouble previousFrequency;
-      private final YoInteger controlTicksPerSample;
+      private final YoInteger controlTicksPerPeriod;
       private final YoLong startCount;
       private final YoInteger counter;
+      private final DoubleProvider baselineDesiredValue;
 
       private final YoDouble rmsPositionError;
       private final YoDouble rmsVelocityError;
 
-      private final TDoubleArrayList positionErrorsSq = new TDoubleArrayList(new double[SAMPLES_PER_PERIOD]);
-      private final TDoubleArrayList velocityErrorsSq = new TDoubleArrayList(new double[SAMPLES_PER_PERIOD]);
+      private final TDoubleArrayList positionErrorsSq = new TDoubleArrayList(new double[MAX_SAMPLES]);
+      private final TDoubleArrayList velocityErrorsSq = new TDoubleArrayList(new double[MAX_SAMPLES]);
       private boolean firstTick = true;
 
-      TrajectorySignal(YoFunctionGeneratorNew functionGenerator, OneDoFJointBasics joint, YoRegistry registry)
+      TrajectorySignal(YoFunctionGeneratorNew functionGenerator, OneDoFJointBasics joint, DoubleProvider baselineDesiredValue, YoRegistry registry)
       {
          this.functionGenerator = functionGenerator;
          this.joint = joint;
          this.previousFrequency = new YoDouble("prevFreq" + joint.getName(), registry);
-         this.controlTicksPerSample = new YoInteger("sampleFreq" + joint.getName(), registry);
+         this.controlTicksPerPeriod = new YoInteger("controlTicksPerPeriod" + joint.getName(), registry);
+         this.baselineDesiredValue = baselineDesiredValue;
 
          rmsPositionError = new YoDouble("q_err_rms_" + joint.getName(), registry);
          rmsVelocityError = new YoDouble("qd_err_rms_" + joint.getName(), registry);
@@ -78,32 +81,39 @@ public class FunctionGeneratorErrorCalculator
 
       void update()
       {
+         if (functionGenerator.getMode() == YoFunctionGeneratorMode.OFF)
+         {
+            rmsPositionError.set(0.0);
+            rmsVelocityError.set(0.0);
+            return;
+         }
+
          if (firstTick || !EuclidCoreTools.epsilonEquals(functionGenerator.getFrequency(), previousFrequency.getValue(), 1e-5))
          {
             firstTick = false;
             previousFrequency.set(functionGenerator.getFrequency());
+
             double periodDuration = 1.0 / functionGenerator.getFrequency();
-            double sampleDT = periodDuration / SAMPLES_PER_PERIOD;
-            controlTicksPerSample.set(Math.max((int) (sampleDT / controlDT), 1));
+            controlTicksPerPeriod.set((int) Math.round(periodDuration / controlDT));
+
+            positionErrorsSq.reset();
 
             positionErrorsSq.fill(0.0);
             velocityErrorsSq.fill(0.0);
 
             startCount.set(controllerCounter.getValue());
             counter.set(0);
-            firstTick = false;
          }
 
-         long count = controllerCounter.getValue() - startCount.getValue();
-         if (controlTicksPerSample.getValue() > 0 && count % controlTicksPerSample.getValue() == 0)
+         if (controlTicksPerPeriod.getValue() > 0)
          {
-            positionErrorsSq.set(counter.getValue(), EuclidCoreTools.square(functionGenerator.getValue() - joint.getQ()));
+            positionErrorsSq.set(counter.getValue(), EuclidCoreTools.square(baselineDesiredValue.getValue() + functionGenerator.getValue() - joint.getQ()));
             velocityErrorsSq.set(counter.getValue(), EuclidCoreTools.square(functionGenerator.getValueDot() - joint.getQd()));
 
-            rmsPositionError.set(Math.sqrt(positionErrorsSq.sum() / SAMPLES_PER_PERIOD));
-            rmsVelocityError.set(Math.sqrt(velocityErrorsSq.sum() / SAMPLES_PER_PERIOD));
+            rmsPositionError.set(Math.sqrt(positionErrorsSq.sum() / controlTicksPerPeriod.getValue()));
+            rmsVelocityError.set(Math.sqrt(velocityErrorsSq.sum() / controlTicksPerPeriod.getValue()));
 
-            counter.set((counter.getValue() + 1) % SAMPLES_PER_PERIOD);
+            counter.set((counter.getValue() + 1) % controlTicksPerPeriod.getValue());
          }
       }
    }
