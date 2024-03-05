59b7e51cceca7d805f512873deceb3c2b36e0329
Tomasz Bialek <tbialek@ihmc.org>
2024-03-05 11:27:03 -0600

    Added method to run YOLO every 5 frames

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXYOLOv8OpticalFlowDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXYOLOv8OpticalFlowDemo.java
@@ -2,6 +2,7 @@ package us.ihmc.rdx.perception;
 
 import com.badlogic.gdx.graphics.Color;
 import imgui.ImGui;
+import imgui.type.ImBoolean;
 import imgui.type.ImFloat;
 import imgui.type.ImInt;
 import org.bytedeco.javacpp.BytePointer;
@@ -11,6 +12,7 @@ import org.bytedeco.opencv.global.opencv_imgproc;
 import org.bytedeco.opencv.opencv_core.GpuMat;
 import org.bytedeco.opencv.opencv_core.GpuMatVector;
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Point;
 import org.bytedeco.opencv.opencv_core.Size;
 import us.ihmc.commons.thread.Notification;
 import us.ihmc.commons.thread.ThreadTools;
@@ -27,7 +29,8 @@ import us.ihmc.perception.YOLOv8.YOLOv8DetectableObject;
 import us.ihmc.perception.YOLOv8.YOLOv8DetectionResults;
 import us.ihmc.perception.YOLOv8.YOLOv8ObjectDetector;
 import us.ihmc.perception.opencl.OpenCLManager;
-import us.ihmc.perception.opticalFlow.OpenCVOpticalFlowProcessor;
+import us.ihmc.perception.opticalFlow.OpenCVDenseOpticalFlowProcessor;
+import us.ihmc.perception.tools.PerceptionDebugTools;
 import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
 import us.ihmc.rdx.RDXPointCloudRenderer;
@@ -53,7 +56,7 @@ public class RDXYOLOv8OpticalFlowDemo
    private final ZEDColorDepthImageRetriever imageRetriever;
    private final ZEDColorDepthImagePublisher imagePublisher;
 
-   private OpenCVOpticalFlowProcessor opticalFlowProcessor;
+   private OpenCVDenseOpticalFlowProcessor opticalFlowProcessor;
    private final YOLOv8ObjectDetector objectDetector = new YOLOv8ObjectDetector();
 
    private final OpenCLManager openCLManager = new OpenCLManager();
@@ -65,6 +68,7 @@ public class RDXYOLOv8OpticalFlowDemo
    private final RDXPointCloudRenderer segmentedPointCloudRenderer = new RDXPointCloudRenderer();
    private final Notification runYOLONotification = new Notification();
 
+   private final ImBoolean keepRunningYOLO = new ImBoolean(false);
    private final ImFloat confidenceThreshold = new ImFloat(0.5f);
    private final ImFloat nmsThreshold = new ImFloat(0.1f);
    private final ImFloat maskThreshold = new ImFloat(0.0f);
@@ -76,6 +80,7 @@ public class RDXYOLOv8OpticalFlowDemo
    private boolean done = false;
 
    private GpuMat indexMat;
+   private int erosionKernelRadius = 2;
 
    public RDXYOLOv8OpticalFlowDemo()
    {
@@ -120,18 +125,24 @@ public class RDXYOLOv8OpticalFlowDemo
                                                    colorImage.getOrientation());
 
       if (opticalFlowProcessor == null)
-         opticalFlowProcessor = new OpenCVOpticalFlowProcessor(compressedColorImage);
+         opticalFlowProcessor = new OpenCVDenseOpticalFlowProcessor(compressedColorImage);
 
-      if (runYOLONotification.poll())
+      if (runYOLONotification.poll() || (keepRunningYOLO.get() && colorImage.getSequenceNumber() % 5 == 0))
       {
          opticalFlowProcessor.setFirstImage(compressedColorImage);
          YOLOv8DetectionResults yoloResults = objectDetector.runOnImage(colorImage, confidenceThreshold.get(), nmsThreshold.get());
          if (objectMask != null)
             objectMask.release();
          objectMask = yoloResults.getSegmentationMatrixForObject(OBJECT_TYPE, maskThreshold.get());
+
       }
       else if (objectMask != null)
       {
+         opencv_imgproc.erode(objectMask.getCpuImageMat(),
+                              objectMask.getCpuImageMat(),
+                              opencv_imgproc.getStructuringElement(opencv_imgproc.CV_SHAPE_RECT,
+                                                                   new Size(2 * erosionKernelRadius + 1, 2 * erosionKernelRadius + 1),
+                                                                   new Point(erosionKernelRadius, erosionKernelRadius)));
          opticalFlowProcessor.setSecondImage(compressedColorImage);
          try (GpuMat flow = opticalFlowProcessor.calculateFlow())
          {
@@ -224,6 +235,18 @@ public class RDXYOLOv8OpticalFlowDemo
       opencv_cudaarithm.split(map, splitMap);
 
       opencv_cudawarping.remap(originalMask, newMask, splitMap.get(0), splitMap.get(1), opencv_imgproc.INTER_NEAREST);
+
+      Mat cpuOriginalMask = new Mat();
+      originalMask.download(cpuOriginalMask);
+      PerceptionDebugTools.display("Original Mask", cpuOriginalMask, 1);
+
+      Mat cpuNewMask = new Mat();
+      newMask.download(cpuNewMask);
+      PerceptionDebugTools.display("New Mask", cpuNewMask, 1);
+
+      cpuNewMask.close();
+      cpuOriginalMask.close();
+
       return newMask;
    }
 
@@ -288,6 +311,7 @@ public class RDXYOLOv8OpticalFlowDemo
 
          private void renderSettings()
          {
+            ImGui.checkbox("Keep Running YOLO", keepRunningYOLO);
             if (ImGui.button("Run YOLO"))
             {
                runYOLONotification.set();
