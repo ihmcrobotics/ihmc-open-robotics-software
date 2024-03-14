e09f918c830b5d86233f42eaaf175d3eef4e36ea
Operator Computer <OperatorComputer@unknown>
2024-03-14 17:08:32 -0500

    Change blackfly recv ip

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/colorVision/stereo/DualBlackflyUDPReceiver.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/colorVision/stereo/DualBlackflyUDPReceiver.java
@@ -35,8 +35,8 @@ public class DualBlackflyUDPReceiver
       frequencyCalculators.put(RobotSide.LEFT, new FrequencyCalculator(10));
       frequencyCalculators.put(RobotSide.RIGHT, new FrequencyCalculator(10));
 
-      addresses.put(RobotSide.LEFT, "127.0.0.1");
-      addresses.put(RobotSide.RIGHT, "127.0.0.1");
+      addresses.put(RobotSide.LEFT, "172.16.66.230");
+      addresses.put(RobotSide.RIGHT, "172.16.66.230");
    }
 
    public boolean connected()
