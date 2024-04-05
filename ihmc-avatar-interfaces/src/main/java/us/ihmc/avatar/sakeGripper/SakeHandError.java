8c060b87b1eb8f16507719ee3bc814f9616c76c8
Tomasz Bialek <tbialek@ihmc.org>
2024-04-05 09:22:21 -0500

    Moved Sake error & added interpreter

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sakeGripper/SakeHandError.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/sakeGripper/SakeHandError.java
@@ -1,22 +1,29 @@
 package us.ihmc.avatar.sakeGripper;
 
 /**
- * Errors to be sent over EtherCAT from Arduino. Needs to match Arduino error indices.
+ * Errors to be sent from hand's Dynamixel over EtherCAT.
  */
-public enum SakeHandErrorCode
+public enum SakeHandError
 {
-   // Values based off of Dynamixel Protocol 2.0 Error codes
-   NONE(0b00000000),
-   OVERLOAD_ERROR(0b00100000),
-   ELECTRICAL_SHOCK_ERROR(0b00010000),
-   MOTOR_ENCODER_ERROR(0b00001000),
-   OVER_HEATING_ERROR(0b00000100),
-   INPUT_VOLTAGE_ERROR(0b00000001);
+   // Values based off of Dynamixel Protocol 1.0 Error codes (https://emanual.robotis.com/docs/en/dxl/protocol1/#error)
+   NONE                 (0b00000000),
+   OVERLOAD_ERROR       (0b00100000),
+   CHECKSUM_ERROR       (0b00010000),
+   RANGE_ERROR          (0b00001000),
+   OVER_HEATING_ERROR   (0b00000100),
+   ANGLE_LIMIT_ERROR    (0b00000010),
+   INPUT_VOLTAGE_ERROR  (0b00000001);
 
    public final int errorCode;
 
-   SakeHandErrorCode(int errorCode)
+   SakeHandError(int errorCode)
    {
       this.errorCode = errorCode;
+   }
+
+   @Override
+   public String toString()
+   {
+      return name().replace("_", " ");
    }
 }
