dd833f0de4cc7e13d6e9a097422b117c793e9508
Duncan Calvert <dcalvert@ihmc.us>
2024-04-12 13:06:57 -0500

    Gather subscriber stats and warn about large messages.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/tools/RDXROS2StatsPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/tools/RDXROS2StatsPanel.java
@@ -3,6 +3,7 @@ package us.ihmc.rdx.ui.tools;
 import imgui.ImGui;
 import us.ihmc.pubsub.impl.PubSubStats;
 import us.ihmc.pubsub.impl.PublisherStats;
+import us.ihmc.pubsub.impl.SubscriberStats;
 import us.ihmc.rdx.imgui.RDXPanel;
 import us.ihmc.ros2.ROS2Stats;
 import us.ihmc.tools.time.RateCalculator;
@@ -22,8 +23,8 @@ public class RDXROS2StatsPanel extends RDXPanel
    private void renderImGuiWidgets()
    {
       ImGui.text("Number of nodes created: %d".formatted(ROS2Stats.NUMBER_OF_NODES_CREATED));
-      ImGui.text("Number of publishers created: %d".formatted(PubSubStats.NUMBER_OF_PUBLISHERS_CREATED));
-      ImGui.text("Number of subscribers created: %d".formatted(ROS2Stats.NUMBER_OF_SUBSCRIBERS_CREATED));
+      ImGui.text("Number of publishers created: %d".formatted(PubSubStats.PUBLISHER_STATS.size()));
+      ImGui.text("Number of subscribers created: %d".formatted(PubSubStats.SUBSCRIBER_STATS.size()));
       ImGui.text("Number of matched subscriptions: %d".formatted(PubSubStats.NUMBER_OF_MATCHED_SUBSCRIPTIONS));
       ImGui.text("Number of published messages: %d".formatted(PubSubStats.NUMBER_OF_PUBLISHED_MESSAGES));
       ImGui.text("Publish frequency: %.0f Hz".formatted(publishFrequency.finiteDifference(PubSubStats.NUMBER_OF_PUBLISHED_MESSAGES)));
@@ -31,14 +32,24 @@ public class RDXROS2StatsPanel extends RDXPanel
       ImGui.text("Receive frequency: %.0f Hz".formatted(receiveFrequency.finiteDifference(PubSubStats.NUMBER_OF_RECEIVED_MESSAGES)));
       ImGui.text("Largest message size: %.1f kB".formatted(PubSubStats.LARGEST_MESSAGE_SIZE / 1000.0));
 
+      ImGui.text("Publishers:");
       for (PublisherStats publisherStats : PubSubStats.PUBLISHER_STATS.values())
       {
-         ImGui.text("%s %s %s %d %d %d".formatted(publisherStats.getPublisher().getAttributes().getTopicName(),
-                                                  publisherStats.getPublisher().getAttributes().getTopicDataType().getName(),
+         ImGui.text("%s %s %s %d %d %d".formatted(publisherStats.getPublisher().getAttributes().getHumanReadableTopicName(),
+                                                  publisherStats.getPublisher().getAttributes().getHumanReadableTopicDataTypeName(),
                                                   publisherStats.getPublisher().getAttributes().getReliabilityKind().name(),
                                                   publisherStats.getNumberOfPublishedMessages(),
                                                   publisherStats.getLatestMessageSize(),
                                                   publisherStats.getLargestMessageSize()));
+      }
+
+      ImGui.text("Subscribers:");
+      for (SubscriberStats subscriberStats : PubSubStats.SUBSCRIBER_STATS.values())
+      {
+         ImGui.text("%s %s %s %d".formatted(subscriberStats.getSubscriber().getAttributes().getHumanReadableTopicName(),
+                                            subscriberStats.getSubscriber().getAttributes().getHumanReadableTopicDataTypeName(),
+                                            subscriberStats.getSubscriber().getAttributes().getReliabilityKind().name(),
+                                            subscriberStats.getNumberOfReceivedMessages()));
       }
    }
 }
