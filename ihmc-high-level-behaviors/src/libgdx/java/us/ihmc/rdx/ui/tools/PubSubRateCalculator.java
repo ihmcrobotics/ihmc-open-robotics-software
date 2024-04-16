0791277be954d295b8c35ae7ef85be1554492c3c
Duncan Calvert <dcalvert@ihmc.us>
2024-04-16 15:06:08 -0500

    Fix ROS 2 stats panel.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/tools/PubSubRateCalculator.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/tools/PubSubRateCalculator.java
@@ -6,19 +6,22 @@ public class PubSubRateCalculator
 {
    private double lastTime = Conversions.millisecondsToSeconds(System.currentTimeMillis());
    private long lastValue = 0;
-   private double lastRate;
+   private double lastRate = 0.0;
 
    public double finiteDifference(long currentValue)
    {
+      if (Double.isNaN(lastRate) || Double.isInfinite(lastRate))
+         lastRate = 0.0;
+
       double currentTime = Conversions.millisecondsToSeconds(System.currentTimeMillis());
 
       long change = currentValue - lastValue;
       double dt = currentTime - lastTime;
 
+      double currentRate = change / dt;
+
       lastValue = currentValue;
       lastTime = currentTime;
-
-      double currentRate = change / dt;
 
       double alpha = 0.99;
       currentRate = alpha * lastRate + (1.0 - alpha) * currentRate;
