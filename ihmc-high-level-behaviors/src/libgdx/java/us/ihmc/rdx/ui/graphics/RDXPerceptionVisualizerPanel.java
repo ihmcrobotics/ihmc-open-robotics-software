fb85e13cada335db2ed2aee943f02ccb09792563
Tomasz Bialek <tbialek@ihmc.org>
2024-04-18 10:24:56 -0500

    Moved move overlap checkbox to ZED visualizer

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/RDXPerceptionVisualizerPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/RDXPerceptionVisualizerPanel.java
@@ -4,11 +4,8 @@ import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 import imgui.ImGui;
-import imgui.type.ImBoolean;
 import std_msgs.msg.dds.Empty;
-import us.ihmc.communication.PerceptionAPI;
 import us.ihmc.communication.ROS2Tools;
-import us.ihmc.communication.ros2.ROS2Heartbeat;
 import us.ihmc.pubsub.DomainFactory;
 import us.ihmc.rdx.imgui.RDXPanel;
 import us.ihmc.rdx.sceneManager.RDXRenderableProvider;
@@ -27,17 +24,12 @@ public class RDXPerceptionVisualizerPanel extends RDXPanel implements RDXRendera
    private final LinkedHashMap<RDXVisualizer, RDXVisualizerWithHeartbeat> visualizers = new LinkedHashMap<>();
    private final ROS2Node heartbeatNode;
 
-   private final ImBoolean removePointCloudOverlap = new ImBoolean(true);
-   private final ROS2Heartbeat overlapRemovalHeartbeat;
-
    private boolean created = false;
 
    public RDXPerceptionVisualizerPanel()
    {
       super(WINDOW_NAME);
-      heartbeatNode = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "visualizer_heartbeat_node");
-      overlapRemovalHeartbeat = new ROS2Heartbeat(heartbeatNode, PerceptionAPI.REQUEST_OVERLAP_REMOVAL);
-      overlapRemovalHeartbeat.setAlive(removePointCloudOverlap.get());
+      heartbeatNode = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "visualizer_hearbeat_node");
       setRenderMethod(this::renderImGuiWidgets);
    }
 
@@ -79,13 +71,6 @@ public class RDXPerceptionVisualizerPanel extends RDXPanel implements RDXRendera
 
    public void renderImGuiWidgets()
    {
-      if (ImGui.collapsingHeader("Visualizer Settings"))
-      {
-         if (ImGui.checkbox("Remove point cloud overlap", removePointCloudOverlap))
-            overlapRemovalHeartbeat.setAlive(removePointCloudOverlap.get());
-         ImGui.separator();
-      }
-
       for (RDXVisualizerWithHeartbeat visualizer : visualizers.values())
       {
          visualizer.renderImGuiWidgets();
@@ -111,8 +96,5 @@ public class RDXPerceptionVisualizerPanel extends RDXPanel implements RDXRendera
       {
          visualizer.destroy();
       }
-
-      overlapRemovalHeartbeat.destroy();
-      heartbeatNode.destroy();
    }
 }
