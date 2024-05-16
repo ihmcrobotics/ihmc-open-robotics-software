adda307065780b6bb941d132bb746016af49d171
smccrory <smccrory@ihmc.us>
2024-05-16 13:24:20 -0500

    Adding java-doc

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/multiContact/WholeBodyPostureAdjustmentProvider.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/multiContact/WholeBodyPostureAdjustmentProvider.java
@@ -1,21 +1,53 @@
 package us.ihmc.commonWalkingControlModules.controlModules.multiContact;
 
+/**
+ * Interface for modifying the whole-body posture of the robot.
+ * Setpoints from this class are propagated down to the respective control modules for root joint position/orientation and joint angles.
+ * This can be used as a diagnostic tool or an API for a posture-adjustment control module.
+ */
 public interface WholeBodyPostureAdjustmentProvider
 {
+   /**
+    * Should be called each tick
+    */
    void update();
 
+   /**
+    * Whether the setpoints from this should be consumed
+    */
    boolean isEnabled();
 
+   /**
+    * Desired position offset of a 1-dof joint. Used in RigidBodyJointControlHelper and
+    * affects JOINTSPACE, TASKSPACE (if hybrid mode) and LOAD-BEARING (if jointspace is active)
+    */
    double getDesiredJointPositionOffset(String jointName);
 
+   /**
+    * Desired velocity offset of a 1-dof joint. Used in RigidBodyJointControlHelper and
+    * affects JOINTSPACE, TASKSPACE (if hybrid mode) and LOAD-BEARING (if jointspace is active)
+    */
    double getDesiredJointVelocityOffset(String jointName);
 
+   /**
+    * Desired feed-forward acceleration offset of a 1-dof joint. Used in RigidBodyJointControlHelper and
+    * affects JOINTSPACE, TASKSPACE (if hybrid mode) and LOAD-BEARING (if jointspace is active)
+    */
    double getDesiredJointAccelerationOffset(String jointName);
 
+   /**
+    * Desired vertical position offset of the floating-base (pelvis). Used in CenterOfMassHeightControlState.
+    */
    double getFloatingBasePositionOffsetZ();
 
+   /**
+    * Desired vertical velocity offset of the floating-base (pelvis). Used in CenterOfMassHeightControlState.
+    */
    double getFloatingBaseVelocityOffsetZ();
 
+   /**
+    * Desired vertical feed-forward acceleration offset of the floating-base (pelvis). Used in CenterOfMassHeightControlState.
+    */
    double getFloatingBaseAccelerationOffsetZ();
 
    static WholeBodyPostureAdjustmentProvider createZeroPostureAdjustmentProvider()
