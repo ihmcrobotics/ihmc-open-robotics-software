e82c67aa7559a4d38189ee6fdcf4d0224ab56ea3
Duncan Calvert <dcalvert@ihmc.us>
2024-06-04 14:32:42 -0500

    Set height map parameters for building exploration.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/PerceptionAndAutonomyProcess.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/PerceptionAndAutonomyProcess.java
@@ -734,6 +734,8 @@ public class PerceptionAndAutonomyProcess
       ROS2Node ros2Node = ROS2Tools.createROS2Node(CommunicationMode.INTERPROCESS.getPubSubImplementation(), "perception_autonomy_process");
       ROS2Helper ros2Helper = new ROS2Helper(ros2Node);
 
+      RapidHeightMapExtractor.reloadHeightMapParameters("GPUBuildingExploration"); // FIXME: This should be redesigned.
+
       PerceptionAndAutonomyProcess perceptionAndAutonomyProcess = new PerceptionAndAutonomyProcess(ros2Helper,
                                                                                                    ReferenceFrame::getWorldFrame,
                                                                                                    ReferenceFrame::getWorldFrame,
--- ihmc-perception/src/main/java/us/ihmc/perception/gpuHeightMap/RapidHeightMapExtractor.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/gpuHeightMap/RapidHeightMapExtractor.java
@@ -803,6 +803,11 @@ public class RapidHeightMapExtractor
       return heightMapDataAvailable;
    }
 
+   public static void reloadHeightMapParameters(String versionSuffix)
+   {
+      heightMapParameters = new HeightMapParameters(versionSuffix);
+   }
+
    public static HeightMapParameters getHeightMapParameters()
    {
       return heightMapParameters;
