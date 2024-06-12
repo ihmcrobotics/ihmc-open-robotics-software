2f97060bb9db45432e7bec92b753f51a48dfc591
Dexton Anderson <danderson@ihmc.org>
2024-06-12 17:26:00 -0500

    FrequencyCalculator fixes/optimization

--- ihmc-java-toolkit/src/main/java/us/ihmc/tools/time/FrequencyCalculator.java
+++ ihmc-java-toolkit/src/main/java/us/ihmc/tools/time/FrequencyCalculator.java
@@ -1,59 +1,78 @@
 package us.ihmc.tools.time;
 
-import us.ihmc.commons.time.Stopwatch;
-import us.ihmc.tools.UnitConversions;
+import us.ihmc.commons.thread.ThreadTools;
+import us.ihmc.log.LogTools;
 
-import java.util.ArrayDeque;
+import javax.annotation.Nullable;
+import java.util.UUID;
 
+/**
+ * A rolling frequency calculator with an optional logging thread to print the frequency once per second.
+ * The frequency is updated on each query to the frequency rather than each new event.
+ * Call {@link #ping()} on each new event.
+ * Call {@link #getFrequency()} to get the current frequency.
+ */
 public class FrequencyCalculator
 {
-   private volatile double frequency;
-   private final ArrayDeque<Double> deltas = new ArrayDeque<>();
-   private final Stopwatch stopwatch = new Stopwatch().start();
-   private final int history;
+   private static final double NANOS_IN_A_SECOND = 1_000_000_000.0;
 
-   /**
-    * History of 3.
-    */
+   private long lastPingTimeNanos;
+   private int pingCount;
+   private double frequency;
+   private long elapsedNanos;
+
+   @Nullable
+   private Thread loggingThread;
+   private volatile boolean loggingThreadRunning;
+
+   public FrequencyCalculator(boolean enableLoggingThread)
+   {
+      if (enableLoggingThread)
+      {
+         String threadID = UUID.randomUUID().toString().substring(0, 5);
+
+         loggingThread = new Thread(() ->
+         {
+            loggingThreadRunning = true;
+
+            while (loggingThreadRunning)
+            {
+               LogTools.info("FrequencyCalculator[" + threadID + "] average rate: " + getFrequency());
+
+               ThreadTools.sleep(1000);
+            }
+         }, getClass().getSimpleName() + "-" + threadID);
+
+         loggingThread.start();
+      }
+   }
+
    public FrequencyCalculator()
    {
-      this(3);
+      this(false);
    }
 
-   /**
-    * History if one yields the latest calculation only.
-    * @param history 1 or more.
-    */
-   public FrequencyCalculator(int history)
-   {
-      this.history = history;
-   }
+   public void ping() {
+      long currentTime = System.nanoTime();
 
-   public void ping()
-   {
-      double elapsed = stopwatch.lap();
-      deltas.addLast(elapsed);
-
-      while (deltas.size() > history)
-      {
-         deltas.removeFirst();
-      }
-
-      double totalElapsed = 0.0;
-      for (Double delta : deltas)
-      {
-         totalElapsed += delta;
-      }
-      frequency = UnitConversions.secondsToHertz(totalElapsed / deltas.size());
+      pingCount++;
+      elapsedNanos += currentTime - lastPingTimeNanos;
+      lastPingTimeNanos = currentTime;
    }
 
    public double getFrequency()
    {
+      if (elapsedNanos >= (NANOS_IN_A_SECOND / 2)) {
+         frequency = pingCount / (elapsedNanos / (NANOS_IN_A_SECOND));
+         pingCount = 0;
+         elapsedNanos = 0;
+      }
+
       return frequency;
    }
 
-   public boolean anyPingsYet()
+   public void destroy()
    {
-      return !deltas.isEmpty();
+      loggingThreadRunning = false;
    }
 }
---
+++ ihmc-java-toolkit/src/test/java/us/ihmc/tools/thread/ThrottlerAndFrequencyCounterTest.java
@@ -0,0 +1,93 @@
+package us.ihmc.tools.thread;
+
+import org.junit.jupiter.api.Test;
+import us.ihmc.commons.thread.ThreadTools;
+import us.ihmc.log.LogTools;
+import us.ihmc.robotics.TestTools;
+import us.ihmc.tools.UnitConversions;
+import us.ihmc.tools.time.FrequencyCalculator;
+
+public class ThrottlerAndFrequencyCounterTest
+{
+   private static void testFrequencyCounter(int targetFrequency, double epsilon)
+   {
+      LogTools.info("testFrequencyCounter targetFrequency=" + targetFrequency + " epsilon=" + epsilon);
+
+      long start = System.currentTimeMillis();
+      FrequencyCalculator frequencyCalculator = new FrequencyCalculator(true);
+
+      // Run for 5 seconds
+      while (System.currentTimeMillis() - start < 5000)
+      {
+         frequencyCalculator.ping();
+
+         double sleepTimeSeconds = UnitConversions.hertzToSeconds(targetFrequency);
+         long sleepTimeMillis = (long) (sleepTimeSeconds * 1000);
+
+         ThreadTools.sleep(sleepTimeMillis);
+      }
+
+      frequencyCalculator.destroy();
+
+      TestTools.assertEpsilonEquals(targetFrequency, frequencyCalculator.getFrequency(), epsilon, "Frequency not correct");
+   }
+
+   private static void testThrottlerAndFrequencyCounter(int targetFrequency, double epsilon)
+   {
+      LogTools.info("testThrottlerAndFrequencyCounter (using us.ihmc.tools.thread.Throttler) targetFrequency=" + targetFrequency + " epsilon=" + epsilon);
+
+      Throttler throttler = new Throttler();
+      throttler.setFrequency(targetFrequency);
+
+      long start = System.currentTimeMillis();
+      FrequencyCalculator frequencyCalculator = new FrequencyCalculator(true);
+
+      // Run for 5 seconds
+      while (System.currentTimeMillis() - start < 5000)
+      {
+         frequencyCalculator.ping();
+
+         throttler.waitAndRun();
+      }
+
+      frequencyCalculator.destroy();
+
+      TestTools.assertEpsilonEquals(targetFrequency, frequencyCalculator.getFrequency(), epsilon, "Frequency not correct");
+   }
+
+   @Test
+   public void testFrequencyCounter100Hz()
+   {
+      testFrequencyCounter(100, 5);
+   }
+
+   @Test
+   public void testThrottlerAndFrequencyCounter100Hz()
+   {
+      testThrottlerAndFrequencyCounter(100, 5);
+   }
+
+   @Test
+   public void testFrequencyCounter10Hz()
+   {
+      testFrequencyCounter(10, 0.5);
+   }
+
+   @Test
+   public void testThrottlerAndFrequencyCounter10Hz()
+   {
+      testThrottlerAndFrequencyCounter(10, 0.5);
+   }
+
+   @Test
+   public void testFrequencyCounter1Hz()
+   {
+      testFrequencyCounter(1, 0.5);
+   }
+
+   @Test
+   public void testThrottlerAndFrequencyCounter1Hz()
+   {
+      testThrottlerAndFrequencyCounter(1, 0.5);
+   }
+}
