bd10054766178660f478ff6a10c14141877cd322
Dexton Anderson <danderson@ihmc.org>
2024-06-18 15:34:07 -0500

    Start some building exploration implementation

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/buildingExploration/BuildingExplorationExecutor.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/buildingExploration/BuildingExplorationExecutor.java
@@ -1,31 +1,56 @@
 package us.ihmc.behaviors.buildingExploration;
 
 import us.ihmc.behaviors.behaviorTree.BehaviorTreeNodeExecutor;
+import us.ihmc.behaviors.door.DoorTraversalExecutor;
+import us.ihmc.behaviors.sequence.ActionSequenceExecutor;
 import us.ihmc.communication.crdt.CRDTInfo;
+import us.ihmc.perception.sceneGraph.SceneGraph;
+import us.ihmc.perception.sceneGraph.SceneNode;
+import us.ihmc.perception.sceneGraph.rigidBody.doors.DoorNode;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
 
 public class BuildingExplorationExecutor extends BehaviorTreeNodeExecutor<BuildingExplorationState, BuildingExplorationDefinition>
 {
-   public BuildingExplorationExecutor(long id, CRDTInfo crdtInfo, WorkspaceResourceDirectory saveFileDirectory)
+   private final SceneGraph sceneGraph;
+
+   public BuildingExplorationExecutor(long id, CRDTInfo crdtInfo, WorkspaceResourceDirectory saveFileDirectory, SceneGraph sceneGraph)
    {
       super(new BuildingExplorationState(id, crdtInfo, saveFileDirectory));
+      this.sceneGraph = sceneGraph;
    }
 
-   @Override
-   public void clock()
-   {
-      super.clock();
-   }
-
-   @Override
-   public void tick()
-   {
-      super.tick();
-   }
-
+   // TODO: finish
    @Override
    public void update()
    {
       super.update();
+
+      // Find the next door to traverse
+      if (getState().getNextDoorNode() == null)
+      {
+         for (SceneNode sceneNode : sceneGraph.getSceneNodesByID())
+         {
+            if (sceneNode instanceof DoorNode doorNode)
+            {
+               // TODO: check detection status
+               // boolean detected = doorNode.isDetected();
+               boolean detected = true;
+
+               // If detected and we have not traversed the door
+               if (detected && !getState().getTraversedDoorNodes().contains(doorNode))
+               {
+                  getState().setNextDoorNode(doorNode);
+               }
+            }
+         }
+      }
+
+      for (BehaviorTreeNodeExecutor<?, ?> child : getChildren())
+      {
+         if (child instanceof DoorTraversalExecutor doorTraversalExecutor)
+         {
+            doorTraversalExecutor.getState().setDoorNode(getState().getNextDoorNode());
+         }
+      }
    }
 }
--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/door/DoorTraversalExecutor.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/door/DoorTraversalExecutor.java
@@ -9,10 +9,8 @@ import us.ihmc.communication.crdt.CRDTInfo;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.mecano.multiBodySystem.RevoluteJoint;
 import us.ihmc.perception.sceneGraph.DetectableSceneNode;
-import us.ihmc.perception.sceneGraph.SceneGraph;
 import us.ihmc.perception.sceneGraph.SceneNode;
 import us.ihmc.perception.sceneGraph.rigidBody.StaticRelativeSceneNode;
-import us.ihmc.perception.sceneGraph.rigidBody.doors.DoorSceneNodeDefinitions;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
@@ -23,7 +21,6 @@ public class DoorTraversalExecutor extends BehaviorTreeNodeExecutor<DoorTraversa
    private final DoorTraversalDefinition definition;
    private final ROS2ControllerHelper ros2ControllerHelper;
    private final ROS2SyncedRobotModel syncedRobot;
-   private final SceneGraph sceneGraph;
 
    private final SideDependentList<RevoluteJoint> x1KnuckleJoints = new SideDependentList<>();
    private final SideDependentList<RevoluteJoint> x2KnuckleJoints = new SideDependentList<>();
@@ -38,8 +35,7 @@ public class DoorTraversalExecutor extends BehaviorTreeNodeExecutor<DoorTraversa
                                 CRDTInfo crdtInfo,
                                 WorkspaceResourceDirectory saveFileDirectory,
                                 ROS2ControllerHelper ros2ControllerHelper,
-                                ROS2SyncedRobotModel syncedRobot,
-                                SceneGraph sceneGraph)
+                                ROS2SyncedRobotModel syncedRobot)
    {
       super(new DoorTraversalState(id, crdtInfo, saveFileDirectory));
 
@@ -48,7 +44,6 @@ public class DoorTraversalExecutor extends BehaviorTreeNodeExecutor<DoorTraversa
 
       this.ros2ControllerHelper = ros2ControllerHelper;
       this.syncedRobot = syncedRobot;
-      this.sceneGraph = sceneGraph;
 
       for (RobotSide side : RobotSide.values)
       {
@@ -77,12 +72,13 @@ public class DoorTraversalExecutor extends BehaviorTreeNodeExecutor<DoorTraversa
 
       state.getDoorHingeJointAngle().setValue(Double.NaN);
 
-      // TODO: PUSH_DOOR_FRAME_NAME renamed to RIGHT_DOOR_FRAME?
-      SceneNode pushDoorFrameNode = sceneGraph.getNamesToNodesMap().get(DoorSceneNodeDefinitions.PUSH_DOOR_FRAME_NAME);
-      SceneNode pushDoorPanelNode = sceneGraph.getNamesToNodesMap().get(DoorSceneNodeDefinitions.RIGHT_DOOR_PANEL_NAME);
+      // TODO:
+      SceneNode pushDoorFrameNode = null;
+      SceneNode pushDoorPanelNode = null;
 
-      DetectableSceneNode yoloDoorHandleNode = (DetectableSceneNode) sceneGraph.getNamesToNodesMap().get("YOLO door lever");
-      StaticRelativeSceneNode staticHandleClosedDoor = (StaticRelativeSceneNode) sceneGraph.getNamesToNodesMap().get("doorStaticHandle");
+      // TODO:
+      DetectableSceneNode yoloDoorHandleNode = null;
+      StaticRelativeSceneNode staticHandleClosedDoor = null;
 
       if (pushDoorFrameNode != null && pushDoorPanelNode != null)
       {
@@ -112,21 +108,25 @@ public class DoorTraversalExecutor extends BehaviorTreeNodeExecutor<DoorTraversa
             if (yoloDoorHandleNode != null)
             {
                double openedDoorHandleDistanceFromStart = getDefinition().getOpenedDoorHandleDistanceFromStart().getValue();
-               double distanceHandleFromStart = yoloDoorHandleNode.getNodeToParentFrameTransform().getTranslation()
+               double distanceHandleFromStart = yoloDoorHandleNode.getNodeToParentFrameTransform()
+                                                                  .getTranslation()
                                                                   .differenceNorm(staticHandleClosedDoor.getNodeToParentFrameTransform().getTranslation());
                state.getDoorHandleDistanceFromStart().setValue(distanceHandleFromStart);
                if (state.getDoorHandleDistanceFromStart().getValue() < openedDoorHandleDistanceFromStart)
                {
                   state.getLogger().info("""
-                                         Retrying pull door. Distance door handle from start %.2f / %.2f [m].
-                                         Stopping all trajectories.
-                                         Going back to %s.
-                                         """.formatted(state.getDoorHandleDistanceFromStart().getValue(), openedDoorHandleDistanceFromStart, state.getWaitToOpenRightHandAction().getDefinition().getName()));
+                                               Retrying pull door. Distance door handle from start %.2f / %.2f [m].
+                                               Stopping all trajectories.
+                                               Going back to %s.
+                                               """.formatted(state.getDoorHandleDistanceFromStart().getValue(),
+                                                             openedDoorHandleDistanceFromStart,
+                                                             state.getWaitToOpenRightHandAction().getDefinition().getName()));
                   ros2ControllerHelper.publishToController(stopAllTrajectoryMessage);
                   waitForPullScrewToFinish = true;
                   state.getActionSequence().setExecutionNextIndex(state.getWaitToOpenRightHandAction().getActionIndex());
                }
             }
+            // TODO?
 //            double knuckle1Q = x1KnuckleJoints.get(RobotSide.RIGHT).getQ();
 //            double knuckle2Q = x2KnuckleJoints.get(RobotSide.RIGHT).getQ();
 //            double handOpenAngle = SakeHandParameters.knuckleJointAnglesToHandOpenAngle(knuckle1Q, knuckle2Q);
@@ -135,14 +135,14 @@ public class DoorTraversalExecutor extends BehaviorTreeNodeExecutor<DoorTraversa
 //            if (handOpenAngle < lostGraspDetectionHandOpenAngle)
 //            {
 //               state.getLogger().info("""
-//                                      Retrying pull door. Hand open angle %.2f%s / %.2f%s degrees.
-//                                      Stopping all trajectories.
-//                                      Going back to %s.
-//                                      """.formatted(Math.toDegrees(knuckle1Q),
-//                                                    EuclidCoreMissingTools.DEGREE_SYMBOL,
-//                                                    Math.toDegrees(lostGraspDetectionHandOpenAngle),
-//                                                    EuclidCoreMissingTools.DEGREE_SYMBOL,
-//                                                    state.getWaitToOpenRightHandAction().getDefinition().getName()));
+//                                            Retrying pull door. Hand open angle %.2f%s / %.2f%s degrees.
+//                                            Stopping all trajectories.
+//                                            Going back to %s.
+//                                            """.formatted(Math.toDegrees(knuckle1Q),
+//                                                          EuclidCoreMissingTools.DEGREE_SYMBOL,
+//                                                          Math.toDegrees(lostGraspDetectionHandOpenAngle),
+//                                                          EuclidCoreMissingTools.DEGREE_SYMBOL,
+//                                                          state.getWaitToOpenRightHandAction().getDefinition().getName()));
 //               ros2ControllerHelper.publishToController(stopAllTrajectoryMessage);
 //               waitForPullScrewToFinish = true;
 //               state.getActionSequence().setExecutionNextIndex(state.getWaitToOpenRightHandAction().getActionIndex());
@@ -157,15 +157,18 @@ public class DoorTraversalExecutor extends BehaviorTreeNodeExecutor<DoorTraversa
          {
             if (staticHandleClosedDoor != null)
             {
-               double handToHandleDistance = syncedRobot.getFullRobotModel().getHandControlFrame(RobotSide.RIGHT).
-                     getTransformToDesiredFrame(staticHandleClosedDoor.getNodeFrame()).getTranslation().norm();
+               double handToHandleDistance = syncedRobot.getFullRobotModel()
+                                                        .getHandControlFrame(RobotSide.RIGHT)
+                                                        .getTransformToDesiredFrame(staticHandleClosedDoor.getNodeFrame())
+                                                        .getTranslation()
+                                                        .norm();
                if (handToHandleDistance > 0.19)
                {
                   state.getLogger().info("""
-                                      Retrying reach door handle. Distance hand to door handle %.2f / %.2f [m].
-                                      Stopping all trajectories.
-                                      Going back to %s.
-                                      """.formatted(handToHandleDistance, 0.19, state.getWaitToOpenRightHandAction().getDefinition().getName()));
+                                               Retrying reach door handle. Distance hand to door handle %.2f / %.2f [m].
+                                               Stopping all trajectories.
+                                               Going back to %s.
+                                               """.formatted(handToHandleDistance, 0.19, state.getWaitToOpenRightHandAction().getDefinition().getName()));
                   ros2ControllerHelper.publishToController(stopAllTrajectoryMessage);
                   waitForGraspToFinish = true;
                   state.getActionSequence().setExecutionNextIndex(state.getWaitToOpenRightHandAction().getActionIndex());
