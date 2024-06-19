ec72d765b341ffde4d6c1ff40dd52d67bdaab819
Duncan Calvert <dcalvert@ihmc.us>
2024-06-19 15:14:06 -0500

    Fix planar regions visualizer.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/ros2/RDXROS2FramePlanarRegionsVisualizer.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/ros2/RDXROS2FramePlanarRegionsVisualizer.java
@@ -1,24 +1,31 @@
 package us.ihmc.rdx.ui.graphics.ros2;
 
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import imgui.ImGui;
 import perception_msgs.msg.dds.FramePlanarRegionsListMessage;
 import us.ihmc.communication.packets.PlanarRegionMessageConverter;
+import us.ihmc.rdx.sceneManager.RDXSceneLevel;
 import us.ihmc.rdx.ui.graphics.RDXPlanarRegionsVisualizer;
 import us.ihmc.robotics.geometry.PlanarRegionsList;
-import us.ihmc.ros2.ROS2Callback;
 import us.ihmc.ros2.ROS2NodeInterface;
 import us.ihmc.ros2.ROS2Topic;
+
+import java.util.Set;
 
 public class RDXROS2FramePlanarRegionsVisualizer extends RDXROS2SingleTopicVisualizer<FramePlanarRegionsListMessage>
 {
    private final RDXPlanarRegionsVisualizer planarRegionsVisualizer;
    private final ROS2Topic<FramePlanarRegionsListMessage> topic;
+   private PlanarRegionsList planarRegionsList;
 
    public RDXROS2FramePlanarRegionsVisualizer(String title, ROS2NodeInterface ros2Node, ROS2Topic<FramePlanarRegionsListMessage> topic)
    {
       super(title);
       planarRegionsVisualizer = new RDXPlanarRegionsVisualizer(title);
       this.topic = topic;
-      new ROS2Callback<>(ros2Node, topic, this::acceptMessage);
+      ros2Node.createSubscription2(topic, this::acceptMessage);
    }
 
    private void acceptMessage(FramePlanarRegionsListMessage framePlanarRegionsListMessage)
@@ -27,7 +34,7 @@ public class RDXROS2FramePlanarRegionsVisualizer extends RDXROS2SingleTopicVisua
       {
          planarRegionsVisualizer.getExecutorService().clearQueueAndExecute(() ->
          {
-            PlanarRegionsList planarRegionsList = PlanarRegionMessageConverter.convertToPlanarRegionsListInWorld(framePlanarRegionsListMessage);
+            planarRegionsList = PlanarRegionMessageConverter.convertToPlanarRegionsListInWorld(framePlanarRegionsListMessage);
             planarRegionsVisualizer.setNumberOfPlanarRegions(planarRegionsList.getNumberOfPlanarRegions());
             planarRegionsVisualizer.getPlanarRegionsGraphic().generateMeshes(planarRegionsList);
          });
@@ -39,7 +46,16 @@ public class RDXROS2FramePlanarRegionsVisualizer extends RDXROS2SingleTopicVisua
    @Override
    public void renderImGuiWidgets()
    {
+      ImGui.text("Number of planar regions: %d".formatted(planarRegionsList.getNumberOfPlanarRegions()));
+   }
 
+   @Override
+   public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool, Set<RDXSceneLevel> sceneLevels)
+   {
+      if (isActive())
+      {
+         planarRegionsVisualizer.getRenderables(renderables, pool, sceneLevels);
+      }
    }
 
    @Override
