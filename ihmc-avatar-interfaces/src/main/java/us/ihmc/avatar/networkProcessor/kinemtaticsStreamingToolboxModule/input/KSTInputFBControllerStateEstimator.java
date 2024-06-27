32ea586adb34085e9b1fd3f77c3bfc4093a71ef8
vroy <vroy@10.100.3.21>
2024-06-27 12:37:59 -0500

    First try on the state estimator.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinemtaticsStreamingToolboxModule/input/KSTInputFBControllerStateEstimator.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinemtaticsStreamingToolboxModule/input/KSTInputFBControllerStateEstimator.java
@@ -2,9 +2,20 @@ package us.ihmc.avatar.networkProcessor.kinemtaticsStreamingToolboxModule.input;
 
 import us.ihmc.avatar.networkProcessor.kinemtaticsStreamingToolboxModule.KSTTools;
 import us.ihmc.avatar.networkProcessor.kinemtaticsStreamingToolboxModule.KinematicsStreamingToolboxParameters;
+import us.ihmc.avatar.networkProcessor.kinemtaticsStreamingToolboxModule.output.KSTOutputDataReadOnly;
+import us.ihmc.avatar.networkProcessor.kinemtaticsStreamingToolboxModule.output.YoKinematicsToolboxOutputStatus;
+import us.ihmc.commonWalkingControlModules.controlModules.YoSE3OffsetFrame;
+import us.ihmc.commonWalkingControlModules.controllerCore.data.FBQuaternion3D;
+import us.ihmc.commonWalkingControlModules.controllerCore.data.FBVector6D;
 import us.ihmc.commons.Conversions;
+import us.ihmc.euclid.matrix.Matrix3D;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FixedFrameVector3DBasics;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameOrientation3DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePoint3DReadOnly;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePose2DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.euclid.tools.EuclidCoreTools;
@@ -13,8 +24,15 @@ import us.ihmc.humanoidRobotics.communication.kinematicsStreamingToolboxAPI.Kine
 import us.ihmc.humanoidRobotics.communication.kinematicsToolboxAPI.KinematicsToolboxCenterOfMassCommand;
 import us.ihmc.humanoidRobotics.communication.kinematicsToolboxAPI.KinematicsToolboxRigidBodyCommand;
 import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyReadOnly;
+import us.ihmc.mecano.spatial.SpatialVector;
+import us.ihmc.mecano.spatial.Twist;
 import us.ihmc.mecano.spatial.interfaces.SpatialVectorReadOnly;
 import us.ihmc.mecano.yoVariables.spatial.YoFixedFrameSpatialVector;
+import us.ihmc.robotics.controllers.pidGains.PID3DGains;
+import us.ihmc.robotics.controllers.pidGains.YoPID3DGains;
+import us.ihmc.robotics.controllers.pidGains.YoPIDSE3Gains;
+import us.ihmc.robotics.controllers.pidGains.implementations.YoPDGains;
+import us.ihmc.robotics.screwTheory.SelectionMatrix6D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePoint3D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePose3D;
 import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameQuaternion;
@@ -39,21 +57,62 @@ public class KSTInputFBControllerStateEstimator implements KSTInputStateEstimato
    private final SingleEndEffectorEstimator[] endEffectorEstimatorsArray;
    private final CenterOfMassEstimator centerOfMassEstimator;
 
-   private final YoDouble correctionDuration = new YoDouble("correctionDuration", registry);
-   private final YoDouble rawVelocityAlpha = new YoDouble("rawVelocityAlpha", registry);
-   private final double updateDT;
    /**
     * Period at which the input is updated. This is used to clamp the correction of the estimated pose. Should preferably be filtered.
     */
    private final DoubleProvider inputPeriod;
    private final YoDouble inputVelocityDecayDuration = new YoDouble("inputVelocityDecayDuration", registry);
 
+   private final Quaternion diff = new Quaternion();
+   private final SpatialVector feedbackPosition = new SpatialVector();
+   private final SpatialVector feedbackVelocity = new SpatialVector();
+   private final Twist currentTwist = new Twist();
+
+   private final FrameVector3D linearFeedback = new FrameVector3D();
+   private final FrameVector3D angularFeedback = new FrameVector3D();
+
+   private final FixedFrameVector3DBasics currentRotationVector;
+   private final FramePose2DReadOnly desiredPose;
+
+   protected final YoPDGains gains;
+   private final YoPID3DGains positionGains;
+   private final YoPID3DGains orientationGains;
+
+   private ReferenceFrame controlFrame;
+   private FBVector6D errorVector;
+   private int currentCommandId;
+   private FBQuaternion3D errorOrientation;
+   private ReferenceFrame linearGainsFrame;
+   private ReferenceFrame angularGainsFrame;
+   private final Matrix3D tempGainMatrix = new Matrix3D();
+   private final SelectionMatrix6D selectionMatrix = new SelectionMatrix6D();
+
+   private final YoDouble correctionDuration = new YoDouble("correctionDuration", registry);
+   private final YoDouble rawVelocityAlpha = new YoDouble("rawVelocityAlpha", registry);
+   private final double updateDT;
+
+   @Override
+   public void initialize()
+   {
+      gains.createDerivativeGainUpdater(false);
+      gains.setKp(500.0);
+      gains.setZeta(1.0);
+
+   }
+
    public KSTInputFBControllerStateEstimator(Collection<? extends RigidBodyReadOnly> endEffectors,
                                              KinematicsStreamingToolboxParameters parameters,
                                              double updateDT,
                                              DoubleProvider inputPeriod,
-                                             YoRegistry parentRegistry)
+                                             YoRegistry parentRegistry,
+                                             YoPID3DGains orientationGains)
    {
+
+      this.currentRotationVector = null;
+      this.desiredPose = null;
+      this.gains = null;
+      this.positionGains = null;
+      this.orientationGains = orientationGains;
       this.updateDT = updateDT;
       this.inputPeriod = inputPeriod;
 
@@ -90,48 +149,104 @@ public class KSTInputFBControllerStateEstimator implements KSTInputStateEstimato
                       KinematicsStreamingToolboxInputCommand latestInputCommand,
                       KinematicsStreamingToolboxInputCommand previousRawInputCommand)
    {
-      double minCorrectionDuration = 2.0 * updateDT;
-      if (inputPeriod.getValue() > updateDT)
-         minCorrectionDuration = Math.max(minCorrectionDuration, SAFE_INPUT_PERIOD_TO_CORRECTION_FACTOR * inputPeriod.getValue());
-
-      if (!Double.isFinite(correctionDuration.getValue()) || correctionDuration.getValue() < minCorrectionDuration)
-         correctionDuration.set(minCorrectionDuration);
 
       if (isNewInput)
+
       {
+
          for (int i = 0; i < latestInputCommand.getNumberOfInputs(); i++)
          {
-            KinematicsToolboxRigidBodyCommand input = latestInputCommand.getInput(i);
+            KinematicsToolboxRigidBodyCommand inputDesired = latestInputCommand.getInput(i);
+            KinematicsToolboxRigidBodyCommand inputCurrent = previousRawInputCommand.getInput(i);
 
-            SingleEndEffectorEstimator inputPoseEstimator = inputPoseEstimators.get(input.getEndEffector());
+            ReferenceFrame trajectoryFrame = inputDesired.getDesiredPose().getReferenceFrame();
 
-            if (inputPoseEstimator != null)
-            {
-               inputPoseEstimator.update(time, defaultLinearRateLimitation, defaultAngularRateLimitation, latestInputCommand.getTimestamp(), input);
-            }
-         }
+            inputCurrent.getDesiredPose().setToZero(controlFrame);
+            inputCurrent.getDesiredPose().changeFrame(trajectoryFrame);
+            inputCurrent.getDesiredPose().getOrientation().getRotationVector(currentRotationVector);
 
-         if (latestInputCommand.hasCenterOfMassInput())
-         {
-            KinematicsToolboxCenterOfMassCommand input = latestInputCommand.getCenterOfMassInput();
-            centerOfMassEstimator.update(time, defaultLinearRateLimitation, latestInputCommand.getTimestamp(), input);
-         }
-      }
-      else
-      {
-         for (int i = 0; i < latestInputCommand.getNumberOfInputs(); i++)
-         {
-            KinematicsToolboxRigidBodyCommand input = latestInputCommand.getInput(i);
+            inputDesired.getDesiredPose().setIncludingFrame(desiredPose);
+            inputDesired.getDesiredPose().changeFrame(controlFrame);
 
-            SingleEndEffectorEstimator inputPoseEstimator = inputPoseEstimators.get(input.getEndEffector());
+            //LINEAR PART
 
-            if (inputPoseEstimator != null)
-               inputPoseEstimator.predict(time);
-         }
+            FrameVector3D q_err_linear = new FrameVector3D();
+            q_err_linear.sub(inputDesired.getControlFramePose().getPosition(), inputCurrent.getControlFramePose().getPosition());
 
-         if (latestInputCommand.hasCenterOfMassInput())
-         {
-            centerOfMassEstimator.predict(time);
+            FrameVector3D qd_err_linear = new FrameVector3D();
+            FrameVector3D linearVelocityDesired = new FrameVector3D(inputDesired.getDesiredVelocity().getLinearPart());
+            FrameVector3D linearVelocityCurrent = new FrameVector3D(inputCurrent.getDesiredVelocity().getLinearPart());
+
+            qd_err_linear.sub(linearVelocityDesired, linearVelocityCurrent);
+
+            q_err_linear.scale(gains.getKp());
+            qd_err_linear.scale(gains.getKd());
+
+            linearFeedback.add(q_err_linear, qd_err_linear);
+
+            //ANGULAR PART
+            FrameVector3D q_err_angular = new FrameVector3D();
+            q_err_angular.sub(inputDesired.getControlFramePose().getPosition(), inputCurrent.getControlFramePose().getPosition());
+            q_err_angular.scale(gains.getKp());
+
+            Quaternion angularVelocityDesired = new Quaternion(inputDesired.getDesiredVelocity().getAngularPart());
+            Quaternion angularVelocityCurrent = new Quaternion(inputCurrent.getDesiredVelocity().getAngularPart());
+
+            Quaternion qd_err_angular = new Quaternion();
+            qd_err_angular.difference(angularVelocityCurrent, angularVelocityDesired);
+
+            //log for quaternion
+
+            double x = qd_err_angular.getX();
+            double y = qd_err_angular.getY();
+            double z = qd_err_angular.getZ();
+            double w = qd_err_angular.getS();
+            double norm = qd_err_angular.norm();
+            double vectorNorm = Math.sqrt(x * x + y * y + z * z);
+
+            double theta = Math.acos(w / norm);
+            double scale = theta / vectorNorm;
+
+            FrameVector3D v_err = new FrameVector3D(trajectoryFrame, x * scale, y * scale, z * scale);
+            v_err.scale(gains.getKd());
+
+            angularFeedback.add(q_err_angular, v_err);
+
+            inputDesired.getDesiredPose().getOrientation().normalizeAndLimitToPi();
+            linearFeedback.setIncludingFrame(inputDesired.getDesiredPose().getPosition());
+            inputDesired.getDesiredPose().getRotationVector(angularFeedback);
+
+            selectionMatrix.applyLinearSelection(linearFeedback);
+            selectionMatrix.applyAngularSelection(angularFeedback);
+
+            linearFeedback.clipToMaxNorm(positionGains.getMaximumProportionalError());
+            angularFeedback.clipToMaxNorm(orientationGains.getMaximumProportionalError());
+
+            errorVector.setIncludingFrame(angularFeedback, linearFeedback);
+            errorVector.changeFrame(controlFrame);
+            errorVector.setCommandId(currentCommandId);
+            errorOrientation.setRotationVectorIncludingFrame(errorVector.getAngularPart());
+            errorOrientation.setCommandId(currentCommandId);
+
+            if (linearGainsFrame != null)
+               linearFeedback.changeFrame(linearGainsFrame);
+            else
+               linearFeedback.changeFrame(controlFrame);
+
+            if (angularGainsFrame != null)
+               angularFeedback.changeFrame(angularGainsFrame);
+            else
+               angularFeedback.changeFrame(controlFrame);
+
+            positionGains.getProportionalGainMatrix(tempGainMatrix);
+            tempGainMatrix.transform(linearFeedback);
+
+            orientationGains.getProportionalGainMatrix(tempGainMatrix);
+            tempGainMatrix.transform(angularFeedback);
+
+            linearFeedback.changeFrame(controlFrame);
+            angularFeedback.changeFrame(controlFrame);
+
          }
       }
    }
