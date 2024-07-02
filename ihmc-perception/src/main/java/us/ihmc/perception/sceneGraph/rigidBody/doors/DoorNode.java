1b32622f01154c15b7ca952bbb23e53b50c99fd3
ihmc-rosie <rosie@ihmc.org>
2024-07-02 15:28:58 -0500

    Remove print

--- ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/rigidBody/doors/DoorNode.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/rigidBody/doors/DoorNode.java
@@ -8,7 +8,6 @@ import us.ihmc.euclid.tools.TupleTools;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
-import us.ihmc.log.LogTools;
 import us.ihmc.perception.YOLOv8.YOLOv8DetectionClass;
 import us.ihmc.perception.sceneGraph.SceneNode;
 import us.ihmc.robotics.geometry.PlanarRegion;
@@ -96,7 +95,6 @@ public class DoorNode extends SceneNode
       // Check if the current door planar region is old
       if (System.currentTimeMillis() - doorPlanarRegionUpdateTimeMillis > 2000)
       {
-         LogTools.info("Setting planar region door panel");
          setDoorPlanarRegion(new PlanarRegion());
       }
 
