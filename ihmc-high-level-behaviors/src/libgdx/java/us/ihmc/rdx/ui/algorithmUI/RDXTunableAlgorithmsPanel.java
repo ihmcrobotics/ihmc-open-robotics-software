cf4f24823ed1e8b558a884a8c24fefaa14133f0c
Tomasz Bialek <tbialek@ihmc.org>
2024-07-19 17:05:15 -0500

    Removed adding panels to RDXBaseUI

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/algorithmUI/RDXTunableAlgorithmsPanel.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/algorithmUI/RDXTunableAlgorithmsPanel.java
@@ -1,10 +1,9 @@
 package us.ihmc.rdx.ui.algorithmUI;
 
 import imgui.ImGui;
-import us.ihmc.rdx.imgui.ImGuiTools;
+import imgui.type.ImBoolean;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.imgui.RDXPanel;
-import us.ihmc.rdx.ui.RDXBaseUI;
 
 import java.util.Comparator;
 import java.util.TreeSet;
@@ -13,60 +12,50 @@ public class RDXTunableAlgorithmsPanel extends RDXPanel
 {
    private static final String WINDOW_NAME = "Perception Algorithms";
 
-   private final RDXBaseUI baseUI;
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
+   private final ImBoolean allowMultiplePanels = new ImBoolean(false);
 
    private final TreeSet<RDXTunableAlgorithm> algorithms = new TreeSet<>(Comparator.comparing(RDXTunableAlgorithm::getTitle));
 
-   public RDXTunableAlgorithmsPanel(RDXBaseUI baseUI)
+   public RDXTunableAlgorithmsPanel()
    {
       super(WINDOW_NAME);
 
-      this.baseUI = baseUI;
       setRenderMethod(this::renderImGuiWidgets);
    }
 
    public void addAlgorithmPanel(RDXTunableAlgorithm algorithm)
    {
       algorithms.add(algorithm);
+      addChild(algorithm);
       if (!algorithm.isCreated())
          algorithm.create();
    }
 
    private void renderImGuiWidgets()
    {
+      ImGui.checkbox(labels.get("Allow Multiple Settings Panels"), allowMultiplePanels);
+
       for (RDXTunableAlgorithm algorithm : algorithms)
       {
          if (algorithm.renderMenuEntry())
          {
-            if (algorithm.isShowing())
+            if (algorithm.isShowing() && !allowMultiplePanels.get())
             {
-               showAlgorithmPanel(algorithm);
-            }
-            else
-            {
-               hideAlgorithmPanel(algorithm);
+               hideAllOtherPanels(algorithm);
             }
          }
       }
    }
 
-   private void showAlgorithmPanel(RDXTunableAlgorithm algorithmToShow)
+   private void hideAllOtherPanels(RDXTunableAlgorithm algorithmToShow)
    {
       for (RDXTunableAlgorithm algorithm : algorithms)
       {
          if (algorithm != algorithmToShow && algorithm.getIsShowing().get())
          {
-            hideAlgorithmPanel(algorithm);
+            algorithm.getIsShowing().set(false);
          }
       }
-
-      baseUI.getImGuiPanelManager().queueAddPanel(algorithmToShow);
-   }
-
-   private void hideAlgorithmPanel(RDXTunableAlgorithm algorithm)
-   {
-      baseUI.getImGuiPanelManager().queueRemovePanel(algorithm);
-      algorithm.getIsShowing().set(false);
    }
 }
