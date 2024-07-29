f2be2d52ffd6f4dcc27c0aaaed2f756dd4da183a
Duncan Calvert <dcalvert@ihmc.org>
2024-07-29 15:13:13 -0500

    Introducing GLTFs to RDX (#324)
    * Add .glb and .bin to Git LFS.
    
    * Add ground to demo.
    
    * Cleanup lighting; add upward facing light.
    
    * Load GLBs and GLTFs when possible.
    
    * Add ground to demo.
    
    * Add Couch from Polycam.

--- ihmc-graphics/src/libgdx/java/us/ihmc/rdx/lighting/RDXDirectionalLight.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/rdx/lighting/RDXDirectionalLight.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector3;
 import net.mgsx.gltf.scene3d.lights.DirectionalLightEx;
 import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+import us.ihmc.rdx.sceneManager.RDX3DScene;
 import us.ihmc.rdx.tools.LibGDXTools;
 
 public class RDXDirectionalLight
@@ -12,7 +13,7 @@ public class RDXDirectionalLight
 
    public RDXDirectionalLight()
    {
-      float directionalLightIntensity = 5.0f;
+      float directionalLightIntensity = RDX3DScene.DEFAULT_DIRECTIONAL_LIGHT_INTENSITY;
       Vector3 direction = new Vector3(-1.0f, -4.0f, -2.0f);
       directionalLightEx.set(Color.WHITE, direction, directionalLightIntensity);
    }
