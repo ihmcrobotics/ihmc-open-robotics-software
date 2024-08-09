984169e92fad28d40f4ac7946b9e689319305f76
Tomasz Bialek <tbialek@ihmc.org>
2024-08-09 10:38:19 -0500

    Reset unused changes to develop

--- ihmc-perception/src/main/java/us/ihmc/perception/tools/ImageMessageDecompressionInput.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/tools/ImageMessageDecompressionInput.java
@@ -23,9 +23,9 @@ public class ImageMessageDecompressionInput
 
    public void extract(ImageMessage imageMessage)
    {
-      decompressionInputBytePointer.put(imageMessage.getData().toArray());
-
       int numberOfBytes = imageMessage.getData().size();
+      PerceptionMessageTools.copyToBytePointer(imageMessage.getData(), decompressionInputBytePointer);
+
       decompressionInputMat.cols(numberOfBytes);
       decompressionInputMat.data(decompressionInputBytePointer);
    }
@@ -33,10 +33,5 @@ public class ImageMessageDecompressionInput
    public Mat getInputMat()
    {
       return decompressionInputMat;
-   }
-
-   public BytePointer getInputBytePointer()
-   {
-      return decompressionInputBytePointer;
    }
 }
