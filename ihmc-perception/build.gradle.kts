088cf391c646a9e4ec0fc4d619922908d99732f2
Duncan Calvert <dcalvert@ihmc.org>
2024-08-09 13:51:07 -0500

    :arrow_up: ihmc-native-library-loader 2.0.3

--- ihmc-perception/build.gradle.kts
+++ ihmc-perception/build.gradle.kts
@@ -98,7 +98,7 @@ mainDependencies {
 
    api("us.ihmc:euclid:0.21.0")
    api("us.ihmc:simulation-construction-set:0.25.1")
-   api("us.ihmc:ihmc-native-library-loader:2.0.2")
+   api("us.ihmc:ihmc-native-library-loader:2.0.3")
    api("us.ihmc:ihmc-humanoid-robotics:source")
    api("us.ihmc:ihmc-communication:source")
    api("us.ihmc:ihmc-ros-tools:source")
