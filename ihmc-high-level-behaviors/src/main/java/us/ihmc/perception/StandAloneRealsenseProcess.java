41cca03acc94ed59d60216c932c509a873428314
nkitchel <nkitchel@ihmc.org>
2024-08-15 15:27:46 -0500

    Cleanup classes and add destroy methods

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/StandAloneRealsenseProcess.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/StandAloneRealsenseProcess.java
@@ -3,7 +3,6 @@ package us.ihmc.perception;
 import perception_msgs.msg.dds.ImageMessage;
 import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
 import us.ihmc.commons.thread.ThreadTools;
-import us.ihmc.communication.CommunicationMode;
 import us.ihmc.communication.PerceptionAPI;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.ros2.ROS2DemandGraphNode;
@@ -16,13 +15,14 @@ import us.ihmc.ros2.ROS2Node;
 import us.ihmc.ros2.ROS2Topic;
 import us.ihmc.sensors.RealsenseColorDepthImagePublisher;
 import us.ihmc.sensors.RealsenseColorDepthImageRetriever;
-import us.ihmc.tools.thread.ExecutorServiceTools;
 import us.ihmc.tools.thread.RestartableThread;
 
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.function.Supplier;
 
-public class RealSensePublisher
+/**
+ * This class handles publishing the color and depth of the realsense. Its meant to be a stand alone class that only touches the realsense.
+ */
+public class StandAloneRealsenseProcess
 {
    private static final String REALSENSE_SERIAL_NUMBER = System.getProperty("d455.serial.number", "213522252883");
    private static final ROS2Topic<ImageMessage> REALSENSE_COLOR_TOPIC = PerceptionAPI.D455_COLOR_IMAGE;
@@ -34,11 +34,7 @@ public class RealSensePublisher
    private final ROS2DemandGraphNode realsenseDemandNode;
    private Supplier<ReferenceFrame> realsenseFrameSupplier = ReferenceFrame::getWorldFrame;
 
-   protected final ScheduledExecutorService executorService = ExecutorServiceTools.newScheduledThreadPool(1,
-                                                                                                          getClass(),
-                                                                                                          ExecutorServiceTools.ExceptionHandling.CATCH_AND_REPORT);
-
-   public RealSensePublisher(ROS2Helper ros2Helper, ROS2SyncedRobotModel syncedRobot)
+   public StandAloneRealsenseProcess(ROS2Helper ros2Helper, ROS2SyncedRobotModel syncedRobot)
    {
       if (syncedRobot != null)
       {
@@ -54,11 +50,11 @@ public class RealSensePublisher
 
       realsenseImagePublisher = new RealsenseColorDepthImagePublisher(REALSENSE_DEPTH_TOPIC, REALSENSE_COLOR_TOPIC);
 
-      RestartableThread realsenseProcessAndPublishThread = new RestartableThread("RealsenseProcessAndPublish", this::processAndPublishRealsense);
+      RestartableThread realsenseProcessAndPublishThread = new RestartableThread("RealsenseProcess", this::publishRealSense);
       realsenseProcessAndPublishThread.start();
    }
 
-   private void processAndPublishRealsense()
+   private void publishRealSense()
    {
       if (realsenseDemandNode.isDemanded())
       {
@@ -75,10 +71,19 @@ public class RealSensePublisher
          ThreadTools.sleep(500);
    }
 
+   public void destroy()
+   {
+      realsenseImagePublisher.destroy();
+      realsenseImageRetriever.destroy();
+      realsenseDemandNode.destroy();
+   }
+
    public static void main(String[] args)
    {
       ROS2Node ros2Node = ROS2Tools.createROS2Node(DomainFactory.PubSubImplementation.FAST_RTPS, "nadia_realsense_process");
       ROS2Helper ros2Helper = new ROS2Helper(ros2Node);
-      new RealSensePublisher(ros2Helper, null);
+
+      StandAloneRealsenseProcess standAloneRealsenseProcess = new StandAloneRealsenseProcess(ros2Helper, null);
+      Runtime.getRuntime().addShutdownHook(new Thread(standAloneRealsenseProcess::destroy, "RealSenseProcess"));
    }
 }
