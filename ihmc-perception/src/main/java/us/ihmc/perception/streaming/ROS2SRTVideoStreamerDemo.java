0f5cede8dd108157f74f490d0ac4de502589b3b6
Tomasz Bialek <tbialek@ihmc.org>
2024-09-12 10:39:14 -0500

    WIP - fixing threading

--- ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoStreamerDemo.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoStreamerDemo.java
@@ -3,30 +3,45 @@ package us.ihmc.perception.streaming;
 import org.bytedeco.ffmpeg.global.avutil;
 import us.ihmc.commons.thread.Notification;
 import us.ihmc.communication.PerceptionAPI;
+import us.ihmc.communication.ROS2Tools;
+import us.ihmc.communication.ros2.ROS2Helper;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.perception.RawImage;
 import us.ihmc.perception.realsense.RealsenseConfiguration;
 import us.ihmc.perception.realsense.RealsenseDeviceManager;
+import us.ihmc.pubsub.DomainFactory.PubSubImplementation;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.ros2.ROS2Node;
 import us.ihmc.sensors.RealsenseColorDepthImageRetriever;
+import us.ihmc.sensors.ZEDColorDepthImageRetrieverSVO;
+import us.ihmc.sensors.ZEDColorDepthImageRetrieverSVO.RecordMode;
+import us.ihmc.tools.IHMCCommonPaths;
 
 public class ROS2SRTVideoStreamerDemo
 {
+   private static final String SVO_FILE_NAME = IHMCCommonPaths.PERCEPTION_LOGS_DIRECTORY.resolve("20240715_103234_ZEDRecording_NewONRCourseWalk.svo2")
+                                                                                        .toAbsolutePath()
+                                                                                        .toString();
+
+   private final ROS2Node ros2Node = ROS2Tools.createROS2Node(PubSubImplementation.FAST_RTPS, "srt_video_streamer_demo");
+   private final ROS2Helper ros2Helper = new ROS2Helper(ros2Node);
+
    private final ROS2SRTVideoStreamer streamer = new ROS2SRTVideoStreamer(PerceptionAPI.REALSENSE_COLOR_STREAM);
-   private final RealsenseDeviceManager realsenseManager = new RealsenseDeviceManager();
-   private final RealsenseColorDepthImageRetriever imageRetriever = new RealsenseColorDepthImageRetriever(realsenseManager,
-                                                                                                          "215122253249",
-                                                                                                          RealsenseConfiguration.D455_COLOR_720P_DEPTH_720P_30HZ,
-                                                                                                          ReferenceFrame::getWorldFrame,
-                                                                                                          () -> true);
+   private final ZEDColorDepthImageRetrieverSVO imageRetriever;
+
    private volatile boolean running = true;
    private final Notification doneNotification = new Notification();
+
    private ROS2SRTVideoStreamerDemo()
    {
+      imageRetriever = new ZEDColorDepthImageRetrieverSVO(0, () -> true, () -> true, ros2Helper, RecordMode.PLAYBACK, SVO_FILE_NAME);
+      imageRetriever.start();
+
       Runtime.getRuntime().addShutdownHook(new Thread(this::destroy, getClass().getSimpleName() + "Destruction"));
 
       while (running)
       {
-         RawImage colorImage = imageRetriever.getLatestRawColorImage();
+         RawImage colorImage = imageRetriever.getLatestRawColorImage(RobotSide.LEFT);
          if (!streamer.isInitialized())
             streamer.initialize(colorImage, 20.0, avutil.AV_PIX_FMT_BGR24);
          streamer.sendFrame(colorImage);
@@ -44,6 +59,7 @@ public class ROS2SRTVideoStreamerDemo
 
       streamer.destroy();
       imageRetriever.destroy();
+      ros2Node.destroy();
    }
 
    public static void main(String[] args)
--- ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoSubscriber.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoSubscriber.java
@@ -8,16 +8,20 @@ import us.ihmc.communication.ros2.ROS2IOTopicPair;
 import us.ihmc.communication.ros2.ROS2PublishSubscribeAPI;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.transform.interfaces.RigidBodyTransformReadOnly;
+import us.ihmc.log.LogTools;
 import us.ihmc.perception.camera.CameraIntrinsics;
 import us.ihmc.ros2.ROS2Input;
 import us.ihmc.tools.thread.MissingThreadTools;
 
 import java.net.InetSocketAddress;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static us.ihmc.perception.streaming.StreamingTools.CONNECTION_TIMEOUT;
 import static us.ihmc.perception.streaming.StreamingTools.STATUS_MESSAGE_UUID;
@@ -26,17 +30,27 @@ public class ROS2SRTVideoSubscriber
 {
    private static final double UPDATE_TIMEOUT = 0.5; // half a second
 
+   private final UUID subscriberId = UUID.randomUUID();
+
    private final ROS2PublishSubscribeAPI ros2;
    private final ROS2IOTopicPair<SRTStreamMessage> streamMessageTopicPair;
-   private final ROS2Input<SRTStreamMessage> statusMessageSubscriber;
+   private final ROS2Input<SRTStreamMessage> statusMessageSubscription;
+   private final ROS2Input<SRTStreamMessage> ackMessageSubscription;
+   private final Object ackMessageReceivedNotification = new Object();
 
    private final SRTStreamMessage requestMessage;
 
-   private final ThreadPoolExecutor subscriptionMaintainer = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
-   private Future<?> connectionMaintainer;
-   private volatile boolean connectionDesired = false;
+   private final ExecutorService taskExecutor = Executors.newFixedThreadPool(4);
+   private Future<?> subscriptionMaintenanceTask;
+   private Future<?> connectionRequestTask;
+   private Future<?> subscriptionTask;
+   private Future<?> unsubscriptionTask;
 
-   private final SRTVideoReceiver videoSubscriber;
+   private volatile boolean connectionDesired = false;
+   private final Lock connectionLock = new ReentrantLock();
+   private final Condition connectionDesiredCondition = connectionLock.newCondition();
+
+   private final SRTVideoReceiver videoReceiver;
    private final CameraIntrinsics cameraIntrinsics;
    private float depthDiscretization = 0.0f;
 
@@ -53,132 +67,206 @@ public class ROS2SRTVideoSubscriber
       this.ros2 = ros2;
       this.streamMessageTopicPair = streamMessageTopicPair;
 
-      statusMessageSubscriber = ros2.subscribe(streamMessageTopicPair.getStatusTopic(),
-                                               statusMessage -> STATUS_MESSAGE_UUID.equals(MessageTools.toUUID(statusMessage.getId())));
+      statusMessageSubscription = ros2.subscribe(streamMessageTopicPair.getStatusTopic(),
+                                                 statusMessage -> STATUS_MESSAGE_UUID.equals(MessageTools.toUUID(statusMessage.getId())));
+      ackMessageSubscription = ros2.subscribe(streamMessageTopicPair.getStatusTopic(),
+                                              ackMessage -> subscriberId.equals(MessageTools.toUUID(ackMessage.getId())));
+      ackMessageSubscription.addCallback(this::receiveACKMessage);
 
       requestMessage = new SRTStreamMessage();
       requestMessage.setReceiverAddress(inputAddress.getHostString());
       requestMessage.setReceiverPort(inputAddress.getPort());
 
-      videoSubscriber = new SRTVideoReceiver(inputAddress, outputAVPixelFormat);
+      videoReceiver = new SRTVideoReceiver(inputAddress, outputAVPixelFormat);
       cameraIntrinsics = new CameraIntrinsics();
+
+      subscriptionMaintenanceTask = taskExecutor.submit(this::maintainConnection);
    }
 
-   public synchronized void subscribe()
+   public void subscribe()
    {
-      connectionDesired = true;
-      subscriptionMaintainer.purge();
-      connectionMaintainer = subscriptionMaintainer.submit(this::maintainConnection);
+      cancelSubscriptionTasks();
+      subscriptionTask = taskExecutor.submit(() ->
+      {
+         connectionLock.lock();
+         try
+         {
+            LogTools.warn("Signaling connection desired");
+            connectionDesired = true;
+            connectionDesiredCondition.signal();
+         }
+         finally
+         {
+            connectionLock.unlock();
+         }
+      });
    }
 
    private void maintainConnection()
    {
-      while (connectionDesired)
+      while (true)
       {
-         if (!videoSubscriber.isConnected())
-            sendConnectionRequest();
+         connectionLock.lock();
+         try
+         {
+            while (!connectionDesired)
+               connectionDesiredCondition.await();
 
-         // This is interruptable, unlike ThreadTools.sleep()
-         MissingThreadTools.sleep(0.5);
+            if (!videoReceiver.isConnected())
+            {
+               LogTools.warn("Sending connection request");
+               connectionRequestTask = taskExecutor.submit(this::sendConnectionRequest);
+               connectionRequestTask.get();
+            }
+         }
+         catch (InterruptedException e)
+         {
+            LogTools.error("Maintenance Interrupted");
+            break;
+         }
+         catch (ExecutionException e)
+         {
+            LogTools.error("Request Execution Failed");
+            break;
+         }
+         finally
+         {
+            connectionLock.unlock();
+         }
+
+         MissingThreadTools.sleep(UPDATE_TIMEOUT);
       }
    }
 
    private void sendConnectionRequest()
    {
-      Object messageReceived = new Object();
-
-      // Select UUID and create subscription to that ID
-      UUID messageId = UUID.randomUUID();
-      ROS2Input<SRTStreamMessage> ackMessageSubscription = ros2.subscribe(streamMessageTopicPair.getStatusTopic(),
-                                                                          ackMessage -> messageId.equals(MessageTools.toUUID(ackMessage.getId())));
-      ackMessageSubscription.addCallback(ackMessage ->
-      {
-         // Subscribe via SRT
-         videoSubscriber.waitForConnection(CONNECTION_TIMEOUT);
-
-         // get camera intrinsics from ACK message
-         cameraIntrinsics.setWidth(ackMessage.getImageWidth());
-         cameraIntrinsics.setHeight(ackMessage.getImageHeight());
-         cameraIntrinsics.setFx(ackMessage.getFx());
-         cameraIntrinsics.setFy(ackMessage.getFy());
-         cameraIntrinsics.setCx(ackMessage.getCx());
-         cameraIntrinsics.setCy(ackMessage.getCy());
-         depthDiscretization = ackMessage.getDepthDiscretization();
-
-         synchronized (messageReceived)
-         {
-            messageReceived.notify();
-         }
-      });
-
       // Send a request message
-      MessageTools.toMessage(messageId, requestMessage.getId());
+      MessageTools.toMessage(subscriberId, requestMessage.getId());
       requestMessage.setConnectionWanted(true);
       ros2.publish(streamMessageTopicPair.getCommandTopic(), requestMessage);
 
       // Wait to receive ACK
-      synchronized (messageReceived)
+      synchronized (ackMessageReceivedNotification)
       {
          try
          {
-            messageReceived.wait((long) Conversions.secondsToMilliseconds(CONNECTION_TIMEOUT));
+            LogTools.warn("Waiting to receive ACK");
+            ackMessageReceivedNotification.wait((long) Conversions.secondsToMilliseconds(CONNECTION_TIMEOUT));
+            LogTools.warn("Stopped waiting for ACK");
          }
          catch (InterruptedException ignored)
          {
+            LogTools.error("Send Connection Interrupted");
+            return;
          }
       }
 
-      ackMessageSubscription.destroy();
+      // Subscribe via SRT
+      videoReceiver.waitForConnection(CONNECTION_TIMEOUT);
    }
 
-   public synchronized void update()
+   private void receiveACKMessage(SRTStreamMessage ackMessage)
    {
-      if (!videoSubscriber.isConnected() || !connectionDesired)
-         return;
-
-      Mat newFrame = videoSubscriber.getNextImage(UPDATE_TIMEOUT);
-      if (newFrame != null)
+      synchronized (ackMessageReceivedNotification)
       {
-         newFrame.copyTo(currentFrame);
-         receivedFirstFrame = true;
+         LogTools.warn("Received ACK");
+         ackMessageReceivedNotification.notify();
       }
 
-      if (statusMessageSubscriber.hasReceivedFirstMessage())
+      // get camera intrinsics from ACK message
+      cameraIntrinsics.setWidth(ackMessage.getImageWidth());
+      cameraIntrinsics.setHeight(ackMessage.getImageHeight());
+      cameraIntrinsics.setFx(ackMessage.getFx());
+      cameraIntrinsics.setFy(ackMessage.getFy());
+      cameraIntrinsics.setCx(ackMessage.getCx());
+      cameraIntrinsics.setCy(ackMessage.getCy());
+      depthDiscretization = ackMessage.getDepthDiscretization();
+   }
+
+   public void update()
+   {
+      connectionLock.lock();
+      try
       {
-         SRTStreamMessage statusMessage = statusMessageSubscriber.getLatest();
+         if (!videoReceiver.isConnected() || !connectionDesired)
+            return;
+
+         LogTools.warn("Updating");
+         Mat newFrame = videoReceiver.getNextImage(UPDATE_TIMEOUT);
+         if (newFrame != null)
+         {
+            newFrame.copyTo(currentFrame);
+            receivedFirstFrame = true;
+         }
+      }
+      finally
+      {
+         connectionLock.unlock();
+      }
+
+      if (statusMessageSubscription.hasReceivedFirstMessage())
+      {
+         SRTStreamMessage statusMessage = statusMessageSubscription.getLatest();
          sensorTransformToWorld.set(statusMessage.getOrientation(), statusMessage.getPosition());
       }
    }
 
-   public synchronized void unsubscribe()
+   public void unsubscribe()
    {
-      connectionDesired = false;
-      if (connectionMaintainer != null && !connectionMaintainer.isDone())
-         connectionMaintainer.cancel(false);
+      cancelSubscriptionTasks();
+      unsubscriptionTask = taskExecutor.submit(() ->
+      {
+         try
+         {
+            connectionLock.lockInterruptibly();
 
-      UUID messageId = UUID.randomUUID();
-      MessageTools.toMessage(messageId, requestMessage.getId());
-      requestMessage.setConnectionWanted(false);
-      ros2.publish(streamMessageTopicPair.getCommandTopic(), requestMessage);
-      videoSubscriber.disconnect();
+            LogTools.warn("Unsubscribing");
+            connectionDesired = false;
+
+            UUID messageId = UUID.randomUUID();
+            MessageTools.toMessage(messageId, requestMessage.getId());
+            requestMessage.setConnectionWanted(false);
+            ros2.publish(streamMessageTopicPair.getCommandTopic(), requestMessage);
+            videoReceiver.disconnect();
+         }
+         catch (InterruptedException ignored) {}
+         finally
+         {
+            connectionLock.unlock();
+         }
+      });
+   }
+
+   private void cancelSubscriptionTasks()
+   {
+      if (connectionRequestTask != null)
+         connectionRequestTask.cancel(true);
+      if (subscriptionTask != null)
+         subscriptionTask.cancel(true);
+      if (unsubscriptionTask != null)
+         unsubscriptionTask.cancel(true);
    }
 
    public void destroy()
    {
       unsubscribe();
-      statusMessageSubscriber.destroy();
-      videoSubscriber.destroy();
+
+      taskExecutor.shutdown();
+
+      statusMessageSubscription.destroy();
+      ackMessageSubscription.destroy();
+      videoReceiver.destroy();
       currentFrame.close();
    }
 
    public boolean isConnected()
    {
-      return videoSubscriber.isConnected();
+      return videoReceiver.isConnected();
    }
 
    public boolean hasCameraIntrinsics()
    {
-      return statusMessageSubscriber.hasReceivedFirstMessage();
+      return statusMessageSubscription.hasReceivedFirstMessage();
    }
 
    public boolean hasReceivedFirstFrame()
