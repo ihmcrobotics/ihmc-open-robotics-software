e18bb4a3383d3e4a4abc8957c98157c78905e017
Duncan Calvert <dcalvert@ihmc.us>
2024-09-27 17:04:21 -0500

    Use better method for detecting footsteps and separate SVG from data recording.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/SCS2LogDataProcessor.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/SCS2LogDataProcessor.java
@@ -1,8 +1,9 @@
 package us.ihmc.avatar.logProcessor;
 
-import gnu.trove.list.array.TIntArrayList;
 import org.jfree.svg.SVGGraphics2D;
 import org.jfree.svg.SVGUnits;
+import us.ihmc.commonWalkingControlModules.controlModules.foot.FootControlModule.ConstraintType;
+import us.ihmc.commons.lists.RecyclingArrayList;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.log.LogTools;
 import us.ihmc.scs2.session.log.LogSession;
@@ -13,76 +14,89 @@ import us.ihmc.yoVariables.variable.YoVariable;
 import java.awt.*;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class SCS2LogDataProcessor
 {
    /** Path to the log folder containing robotData.log */
-   private final String LOG_PATH = System.getProperty("log.path");
+   private static final Path LOG_PATH = System.getProperty("log.path") == null ? null : Paths.get(System.getProperty("log.path"));
    /** Square document size in meters. */
-   private final double DOCUMENT_SIZE = Double.parseDouble(System.getProperty("document.size", "15.0"));
+   private static final double DOCUMENT_SIZE = Double.parseDouble(System.getProperty("document.size", "15.0"));
 
-   private final LogSession logSession;
-   private final YoRegistry rootRegistry;
-   private final SVGGraphics2D svgGraphics2D;
+   private LogSession logSession;
+   private YoRegistry rootRegistry;
+   private SVGGraphics2D svgGraphics2D;
    private long tick = 0;
-   private final int numberOfEntries;
+   private int numberOfEntries;
 
    private final Point2D robotStartToDocumentCenter = new Point2D(DOCUMENT_SIZE / 2.0, DOCUMENT_SIZE / 2.0);
 
-   private long lastNumberOfFoosteps = 0;
-   private final YoVariable yoCurrentNumberOfFootsteps;
-   private double lastFootstepTime = Double.NaN;
+   private YoVariable yoCurrentNumberOfFootsteps;
+   private YoVariable leftFootState;
+   private YoVariable rightFootState;
+   private ConstraintType leftLastFootState = ConstraintType.SWING;
+   private ConstraintType rightLastFootState = ConstraintType.SWING;
+   private Double leftFullSupportTime = Double.NaN;
+   private Double rightFullSupportTime = Double.NaN;
+   private boolean newLeftStep = false;
+   private boolean newRightStep = false;
+   private YoVariable leftFootPolygon_0_x ;
+   private YoVariable leftFootPolygon_0_y ;
+   private YoVariable leftFootPolygon_1_x ;
+   private YoVariable leftFootPolygon_1_y ;
+   private YoVariable leftFootPolygon_2_x ;
+   private YoVariable leftFootPolygon_2_y ;
+   private YoVariable leftFootPolygon_3_x ;
+   private YoVariable leftFootPolygon_3_y ;
+   private YoVariable rightFootPolygon_0_x;
+   private YoVariable rightFootPolygon_0_y;
+   private YoVariable rightFootPolygon_1_x;
+   private YoVariable rightFootPolygon_1_y;
+   private YoVariable rightFootPolygon_2_x;
+   private YoVariable rightFootPolygon_2_y;
+   private YoVariable rightFootPolygon_3_x;
+   private YoVariable rightFootPolygon_3_y;
+   private final ArrayList<double[]> leftFootsteps = new ArrayList<>();
+   private final ArrayList<double[]> rightFootsteps = new ArrayList<>();
 
    private final Point2D currentCenterOfMass = new Point2D();
    private final Point2D lastCenterOfMass = new Point2D(Double.NaN, Double.NaN);
-   private final YoVariable yoCenterOfMassX;
-   private final YoVariable yoCenterOfMassY;
+   private YoVariable yoCenterOfMassX;
+   private YoVariable yoCenterOfMassY;
    private final double comPlotProximityToFootsteps = 5.0;
    private final double comPlotResolution = 0.1;
    private double lastCoMPlotTime = Double.NaN;
-   private final TIntArrayList comXs = new TIntArrayList();
-   private final TIntArrayList comYs = new TIntArrayList();
-
-   private final YoVariable leftFootPolygon_0_x ;
-   private final YoVariable leftFootPolygon_0_y ;
-   private final YoVariable leftFootPolygon_1_x ;
-   private final YoVariable leftFootPolygon_1_y ;
-   private final YoVariable leftFootPolygon_2_x ;
-   private final YoVariable leftFootPolygon_2_y ;
-   private final YoVariable leftFootPolygon_3_x ;
-   private final YoVariable leftFootPolygon_3_y ;
-   private final YoVariable rightFootPolygon_0_x;
-   private final YoVariable rightFootPolygon_0_y;
-   private final YoVariable rightFootPolygon_1_x;
-   private final YoVariable rightFootPolygon_1_y;
-   private final YoVariable rightFootPolygon_2_x;
-   private final YoVariable rightFootPolygon_2_y;
-   private final YoVariable rightFootPolygon_3_x;
-   private final YoVariable rightFootPolygon_3_y;
+   private final RecyclingArrayList<Point2D> coms = new RecyclingArrayList<>(Point2D::new);
 
    public SCS2LogDataProcessor()
    {
       try
       {
          if (LOG_PATH == null)
+         {
             LogTools.error("Must pass -Dlog.path=/path/to/log");
+            return;
+         }
 
-         logSession = new LogSession(Paths.get(LOG_PATH).toFile(), null);
+         logSession = new LogSession(LOG_PATH.toFile(), null);
          logSession.addAfterReadCallback(this::afterRead);
          rootRegistry = logSession.getRootRegistry();
 
-         yoCurrentNumberOfFootsteps = rootRegistry.findVariable(
-               "root.main.DRCControllerThread.DRCMomentumBasedController.HumanoidHighLevelControllerManager.HighLevelHumanoidControllerFactory.WalkingMessageHandler.currentNumberOfFootsteps");
-         yoCenterOfMassX = rootRegistry.findVariable(
-               "root.main.DRCControllerThread.DRCMomentumBasedController.HumanoidHighLevelControllerManager.WalkingControllerState.LinearMomentumRateControlModule.centerOfMassX");
-         yoCenterOfMassY = rootRegistry.findVariable(
-               "root.main.DRCControllerThread.DRCMomentumBasedController.HumanoidHighLevelControllerManager.WalkingControllerState.LinearMomentumRateControlModule.centerOfMassY");
+         String highLevelController = "root.main.DRCControllerThread.DRCMomentumBasedController.HumanoidHighLevelControllerManager.";
 
-         String footPolygonPrefix = "root.main.DRCControllerThread.DRCMomentumBasedController.HumanoidHighLevelControllerManager.HighLevelHumanoidControllerToolbox.BipedSupportPolygons.";
+         String momentumRateControl = highLevelController + "WalkingControllerState.LinearMomentumRateControlModule.";
+         yoCenterOfMassX = rootRegistry.findVariable(momentumRateControl + "centerOfMassX");
+         yoCenterOfMassY = rootRegistry.findVariable(momentumRateControl + "centerOfMassY");
+
+         String walkingMessageHandler = highLevelController + "HighLevelHumanoidControllerFactory.WalkingMessageHandler.";
+         yoCurrentNumberOfFootsteps = rootRegistry.findVariable(walkingMessageHandler + "currentNumberOfFootsteps");
+         String feetManager = highLevelController + "HighLevelHumanoidControllerFactory.HighLevelControlManagerFactory.FeetManager.";
+         leftFootState = rootRegistry.findVariable(feetManager + "leftFootControlModule.leftFootCurrentState");
+         rightFootState = rootRegistry.findVariable(feetManager + "rightFootControlModule.rightFootCurrentState");
+         String footPolygonPrefix = highLevelController + "HighLevelHumanoidControllerToolbox.BipedSupportPolygons.";
          leftFootPolygon_0_x  = rootRegistry.findVariable(footPolygonPrefix + "leftFootPolygon_0_x");
          leftFootPolygon_0_y  = rootRegistry.findVariable(footPolygonPrefix + "leftFootPolygon_0_y");
          leftFootPolygon_1_x  = rootRegistry.findVariable(footPolygonPrefix + "leftFootPolygon_1_x");
@@ -106,34 +120,16 @@ public class SCS2LogDataProcessor
          numberOfEntries = logSession.getLogDataReader().getNumberOfEntries();
          LogTools.info("numberOfEntries: %d".formatted(numberOfEntries));
 
-         double documentSizeMillimeters = convertToMillimeters(DOCUMENT_SIZE);
-         svgGraphics2D = new SVGGraphics2D(documentSizeMillimeters, documentSizeMillimeters, SVGUnits.MM);
-
-         svgGraphics2D.setColor(Color.BLACK);
-         svgGraphics2D.setStroke(new BasicStroke(15));
-
          for (int i = 0; i < numberOfEntries; i++)
          {
             logSession.runTick();
          }
 
-         svgGraphics2D.drawPolyline(comXs.toArray(), comYs.toArray(), comXs.size());
-
          MissingThreadTools.sleep(0.1);
 
          logSession.stopSessionThread();
 
-         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmssSSS");
-         Path saveFile = Paths.get(LocalDateTime.now().format(formatter) + "_LogDataDrawing.svg");
-         LogTools.info("Saving to {}", saveFile);
-
-         try (FileWriter writer = new FileWriter(saveFile.toFile()))
-         {
-            String svgDocument = svgGraphics2D.getSVGDocument();
-            // Add viewBox attribute to the SVG element, to make it load correctly in Inkscape
-            svgDocument = svgDocument.replace("<svg", "<svg viewBox=\"0 0 %f %f\"".formatted(documentSizeMillimeters, documentSizeMillimeters));
-            writer.write(svgDocument);
-         }
+         drawSVG();
       }
       catch (IOException e)
       {
@@ -143,49 +139,65 @@ public class SCS2LogDataProcessor
 
    private void afterRead(double currentTime)
    {
-      long currentNumberOfFootsteps = (long) yoCurrentNumberOfFootsteps.getValueAsDouble();
-      if (currentNumberOfFootsteps != lastNumberOfFoosteps)
+      if (leftLastFootState != ConstraintType.FULL && leftFootState.getValueAsString().equals(ConstraintType.FULL.name()))
       {
-         LogTools.info("tick: %d/%d  footstep: %d -> %d".formatted(tick, numberOfEntries, lastNumberOfFoosteps, currentNumberOfFootsteps));
-
-         svgGraphics2D.drawPolygon(new int[] {metersToMMX(leftFootPolygon_0_x.getValueAsDouble()),
-                                              metersToMMX(leftFootPolygon_1_x.getValueAsDouble()),
-                                              metersToMMX(leftFootPolygon_2_x.getValueAsDouble()),
-                                              metersToMMX(leftFootPolygon_3_x.getValueAsDouble())},
-                                   new int[] {metersToMMY(leftFootPolygon_0_y.getValueAsDouble()),
-                                              metersToMMY(leftFootPolygon_1_y.getValueAsDouble()),
-                                              metersToMMY(leftFootPolygon_2_y.getValueAsDouble()),
-                                              metersToMMY(leftFootPolygon_3_y.getValueAsDouble())},
-                                   4);
-
-         svgGraphics2D.drawPolygon(new int[] {metersToMMX(rightFootPolygon_0_x.getValueAsDouble()),
-                                              metersToMMX(rightFootPolygon_1_x.getValueAsDouble()),
-                                              metersToMMX(rightFootPolygon_2_x.getValueAsDouble()),
-                                              metersToMMX(rightFootPolygon_3_x.getValueAsDouble())},
-                                   new int[] {metersToMMY(rightFootPolygon_0_y.getValueAsDouble()),
-                                              metersToMMY(rightFootPolygon_1_y.getValueAsDouble()),
-                                              metersToMMY(rightFootPolygon_2_y.getValueAsDouble()),
-                                              metersToMMY(rightFootPolygon_3_y.getValueAsDouble())},
-                                   4);
-
-         lastFootstepTime = currentTime;
+         LogTools.info("Recording left footstep");
+         newLeftStep = true;
+         leftFullSupportTime = currentTime;
       }
-      lastNumberOfFoosteps = currentNumberOfFootsteps;
+      leftLastFootState = ConstraintType.valueOf(leftFootState.getValueAsString());
 
-      if (!Double.isNaN(lastFootstepTime) && currentTime - lastFootstepTime < comPlotProximityToFootsteps)
+      if (rightLastFootState != ConstraintType.FULL && rightFootState.getValueAsString().equals(ConstraintType.FULL.name()))
+      {
+         LogTools.info("Recording left footstep");
+         newRightStep = true;
+         rightFullSupportTime = currentTime;
+      }
+      rightLastFootState = ConstraintType.valueOf(rightFootState.getValueAsString());
+
+
+      if (newLeftStep && currentTime - leftFullSupportTime > 0.1)
+      {
+         leftFootsteps.add(new double[] {leftFootPolygon_0_x.getValueAsDouble(),
+                                         leftFootPolygon_1_x.getValueAsDouble(),
+                                         leftFootPolygon_2_x.getValueAsDouble(),
+                                         leftFootPolygon_3_x.getValueAsDouble(),
+                                         leftFootPolygon_0_y.getValueAsDouble(),
+                                         leftFootPolygon_1_y.getValueAsDouble(),
+                                         leftFootPolygon_2_y.getValueAsDouble(),
+                                         leftFootPolygon_3_y.getValueAsDouble()});
+         newLeftStep = false;
+      }
+
+      if (newRightStep && currentTime - rightFullSupportTime > 0.1)
+      {
+         rightFootsteps.add(new double[] {rightFootPolygon_0_x.getValueAsDouble(),
+                                          rightFootPolygon_1_x.getValueAsDouble(),
+                                          rightFootPolygon_2_x.getValueAsDouble(),
+                                          rightFootPolygon_3_x.getValueAsDouble(),
+                                          rightFootPolygon_0_y.getValueAsDouble(),
+                                          rightFootPolygon_1_y.getValueAsDouble(),
+                                          rightFootPolygon_2_y.getValueAsDouble(),
+                                          rightFootPolygon_3_y.getValueAsDouble()});
+         newRightStep = false;
+      }
+
+      boolean recentLeftStep = !Double.isNaN(leftFullSupportTime) && currentTime - leftFullSupportTime < comPlotProximityToFootsteps;
+      boolean recentRightStep = !Double.isNaN(rightFullSupportTime) && currentTime - rightFullSupportTime < comPlotProximityToFootsteps;
+
+      if (recentLeftStep || recentRightStep)
       {
          if (Double.isNaN(lastCoMPlotTime) || currentTime - lastCoMPlotTime > comPlotResolution)
          {
             currentCenterOfMass.set(yoCenterOfMassX.getValueAsDouble(), yoCenterOfMassY.getValueAsDouble());
 
-            if (comXs.isEmpty())
+            if (coms.isEmpty())
             {
                robotStartToDocumentCenter.add(-currentCenterOfMass.getX(), currentCenterOfMass.getY());
             }
 
-            LogTools.info("Drawing CoM at {}", currentCenterOfMass);
-            comXs.add(metersToMMX(currentCenterOfMass.getX()));
-            comYs.add(metersToMMY(currentCenterOfMass.getY()));
+            LogTools.info("Extracting CoM at {}", currentCenterOfMass);
+            coms.add().set(currentCenterOfMass);
 
             lastCenterOfMass.set(currentCenterOfMass);
             lastCoMPlotTime = currentTime;
@@ -193,6 +205,70 @@ public class SCS2LogDataProcessor
       }
 
       ++tick;
+   }
+
+   private void drawSVG()
+   {
+      double documentSizeMillimeters = convertToMillimeters(DOCUMENT_SIZE);
+      svgGraphics2D = new SVGGraphics2D(documentSizeMillimeters, documentSizeMillimeters, SVGUnits.MM);
+
+      svgGraphics2D.setColor(Color.BLACK);
+      svgGraphics2D.setStroke(new BasicStroke(15));
+
+      for (double[] footstep : leftFootsteps)
+      {
+         svgGraphics2D.drawPolygon(new int[] {metersToMMX(footstep[0]),
+                                              metersToMMX(footstep[1]),
+                                              metersToMMX(footstep[2]),
+                                              metersToMMX(footstep[3])},
+                                   new int[] {metersToMMY(footstep[4]),
+                                              metersToMMY(footstep[5]),
+                                              metersToMMY(footstep[6]),
+                                              metersToMMY(footstep[7])},
+                                   4);
+      }
+      for (double[] footstep : rightFootsteps)
+      {
+         svgGraphics2D.drawPolygon(new int[] {metersToMMX(footstep[0]),
+                                              metersToMMX(footstep[1]),
+                                              metersToMMX(footstep[2]),
+                                              metersToMMX(footstep[3])},
+                                   new int[] {metersToMMY(footstep[4]),
+                                              metersToMMY(footstep[5]),
+                                              metersToMMY(footstep[6]),
+                                              metersToMMY(footstep[7])},
+                                   4);
+      }
+
+      int[] comXs = new int[coms.size()];
+      int[] comYs = new int[coms.size()];
+      for (int i = 0; i < coms.size(); i++)
+      {
+         comXs[i] = metersToMMX(coms.get(i).getX());
+         comYs[i] = metersToMMY(coms.get(i).getY());
+      }
+      svgGraphics2D.drawPolyline(comXs, comYs, comXs.length);
+
+      Path savePath = Paths.get("%s.svg".formatted(LOG_PATH.getFileName()));
+      int i = 1;
+      while (Files.exists(savePath))
+      {
+         savePath = LOG_PATH.getFileName().resolve("_%d.svg".formatted(i++));
+      }
+
+      LogTools.info("Saving to {}", savePath);
+
+      try (FileWriter writer = new FileWriter(savePath.toFile()))
+      {
+         String svgDocument = svgGraphics2D.getSVGDocument();
+         // Add viewBox attribute to the SVG element, to make it load correctly in Inkscape
+         svgDocument = svgDocument.replace("<svg", "<svg viewBox=\"0 0 %f %f\"".formatted(documentSizeMillimeters, documentSizeMillimeters));
+         writer.write(svgDocument);
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
    }
 
    private int metersToMMX(double x)
