a5a1134bf440e813b9c9305fd313fc590d68fe0f
bpark <bpark@ihmc.org>
2024-10-03 18:02:47 -0500

    Adding the ControllerCoreOutputDataHolder for sharing the result of the wholebody controllercore between the wholebodycontrollercore thread and controllerThread.

---
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/lowLevel/ControllerCoreOutputDataHolder.java
@@ -0,0 +1,208 @@
+package us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel;
+
+import us.ihmc.commonWalkingControlModules.controllerCore.command.ControllerCoreOutputReadOnly;
+import us.ihmc.commonWalkingControlModules.controllerCore.command.DesiredExternalWrenchHolder;
+import us.ihmc.euclid.referenceFrame.FrameVector3D;
+import us.ihmc.euclid.referenceFrame.interfaces.FramePoint2DBasics;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
+import us.ihmc.humanoidRobotics.model.CenterOfPressureDataHolder;
+import us.ihmc.mecano.multiBodySystem.interfaces.RigidBodyBasics;
+import us.ihmc.mecano.spatial.interfaces.WrenchBasics;
+import us.ihmc.mecano.spatial.interfaces.WrenchReadOnly;
+import us.ihmc.sensorProcessing.outputData.JointDesiredOutputListReadOnly;
+
+public class ControllerCoreOutPutDataHolder implements ControllerCoreOutputReadOnly
+{
+   private boolean hasCenterOfPressure = false;
+   private final CenterOfPressureDataHolder centerOfPressureDataHolder = new CenterOfPressureDataHolder();
+
+   private boolean hasDesiredExternalWrench = false;
+   private final DesiredExternalWrenchHolder desiredExternalWrenchHolder = new DesiredExternalWrenchHolder();
+
+   private boolean hasLinearMomentum = false;
+   private final FrameVector3D linearMomentum = new FrameVector3D();
+   private boolean hasAngularMomentum = false;
+   private final FrameVector3D angularMomentum = new FrameVector3D();
+
+   private boolean hasLinearMomentumRate = false;
+   private final FrameVector3D linearMomentumRate = new FrameVector3D();
+   private boolean hasAngularMomentumRate = false;
+   private final FrameVector3D angularMomentumRate = new FrameVector3D();
+
+   private boolean hasRootJointDesiredConfiguration = false;
+   private final RootJointDesiredConfigurationData rootJointDesiredConfigurationData = new RootJointDesiredConfigurationData();
+
+   private boolean hasLowLevelOneDoFJointControllerCoreOutputDataHolder = false;
+   private final LowLevelOneDoFJointDesiredDataHolder lowLevelOneDoFJointControllerCoreOutputDataHolder = new LowLevelOneDoFJointDesiredDataHolder();
+
+   public ControllerCoreOutPutDataHolder()
+   {
+      clear();
+   }
+
+   public void clear()
+   {
+      hasAngularMomentum = false;
+      hasLowLevelOneDoFJointControllerCoreOutputDataHolder = false;
+      hasAngularMomentumRate = false;
+      hasLinearMomentum = false;
+      hasLinearMomentumRate = false;
+      hasCenterOfPressure = false;
+      hasDesiredExternalWrench = false;
+      hasRootJointDesiredConfiguration = false;
+
+      angularMomentum.setToNaN();
+      angularMomentumRate.setToNaN();
+      linearMomentum.setToNaN();
+      linearMomentumRate.setToNaN();
+
+      centerOfPressureDataHolder.clear();
+      desiredExternalWrenchHolder.clear();
+      rootJointDesiredConfigurationData.clear();
+      lowLevelOneDoFJointControllerCoreOutputDataHolder.clear();
+   }
+
+   public void setDesiredCenterOfPressureDataHolder(CenterOfPressureDataHolder centerOfPressureDataHolder)
+   {
+      this.centerOfPressureDataHolder.set(centerOfPressureDataHolder);
+   }
+
+   @Override
+   public void getDesiredCenterOfPressure(FramePoint2DBasics copToPack, RigidBodyBasics rigidBody)
+   {
+      this.centerOfPressureDataHolder.getCenterOfPressure(copToPack, rigidBody);
+   }
+
+   public void setDesiredExternalWrench(WrenchReadOnly desiredExternalWrench, RigidBodyBasics rigidBody)
+   {
+      this.desiredExternalWrenchHolder.setDesiredExternalWrench(desiredExternalWrench, rigidBody);
+   }
+
+   @Override
+   public boolean getDesiredExternalWrench(WrenchBasics desiredExternalWrenchToPack, RigidBodyBasics rigidBody)
+   {
+      return desiredExternalWrenchHolder.getDesiredExternalWrench(desiredExternalWrenchToPack, rigidBody);
+   }
+
+   public void setLinearMomentum(FrameVector3DReadOnly linearMomentum)
+   {
+      this.linearMomentum.setIncludingFrame(linearMomentum);
+   }
+
+   @Override
+   public FrameVector3DReadOnly getLinearMomentum()
+   {
+      return linearMomentum;
+   }
+
+   public void setAngularMomentum(FrameVector3DReadOnly angularMomentum)
+   {
+      this.angularMomentum.setIncludingFrame(angularMomentum);
+   }
+
+   @Override
+   public FrameVector3DReadOnly getAngularMomentum()
+   {
+      return angularMomentum;
+   }
+
+   public void setLinearMomentumRate(FrameVector3DReadOnly linearMomentumRate)
+   {
+      this.linearMomentumRate.setIncludingFrame(linearMomentumRate);
+   }
+
+   @Override
+   public FrameVector3DReadOnly getLinearMomentumRate()
+   {
+      return linearMomentumRate;
+   }
+
+   public void setAngularMomentumRate(FrameVector3DReadOnly angularMomentumRate)
+   {
+      this.angularMomentumRate.setIncludingFrame(angularMomentumRate);
+   }
+
+   @Override
+   public FrameVector3DReadOnly getAngularMomentumRate()
+   {
+      return angularMomentumRate;
+   }
+
+   public void setRootJointDesiredConfigurationData(RootJointDesiredConfigurationDataReadOnly rootJointDesiredConfigurationData)
+   {
+      this.rootJointDesiredConfigurationData.set(rootJointDesiredConfigurationData);
+   }
+
+   @Override
+   public RootJointDesiredConfigurationDataReadOnly getRootJointDesiredConfigurationData()
+   {
+      return rootJointDesiredConfigurationData;
+   }
+
+   public void setLowLevelOneDoFJointControllerCoreOutputDataHolder(JointDesiredOutputListReadOnly lowLevelOneDoFJointControllerCoreOutputDataHolder)
+   {
+      this.lowLevelOneDoFJointControllerCoreOutputDataHolder.overwriteWith(lowLevelOneDoFJointControllerCoreOutputDataHolder);
+   }
+
+   //TODO this will be replace the getLowLevelOneDoFJointDesiredDataHolderPreferered. This one returns lowLevelOneDoFJointDesiredDataHolder
+   //while getLowLevelOneDoFJointDesiredDataHolder returns jointDesiredOutputList.
+   public JointDesiredOutputListReadOnly getLowLevelOneDoFJointControllerCoreDesiredDataHolder()
+   {
+      return lowLevelOneDoFJointControllerCoreOutputDataHolder;
+   }
+   @Override
+   public JointDesiredOutputListReadOnly getLowLevelOneDoFJointDesiredDataHolder()
+   {
+      return null;
+   }
+
+   public void set(ControllerCoreOutPutDataHolder other)
+   {
+      if (this.lowLevelOneDoFJointControllerCoreOutputDataHolder == null)
+         throw new RuntimeException("You used the deprecated constructor set is not supported in that case. ");
+
+      this.centerOfPressureDataHolder.set(other.centerOfPressureDataHolder);
+      this.desiredExternalWrenchHolder.set(other.desiredExternalWrenchHolder);
+      this.linearMomentum.setIncludingFrame(other.linearMomentum);
+      this.linearMomentumRate.setIncludingFrame(other.linearMomentumRate);
+      this.angularMomentum.setIncludingFrame(other.angularMomentum);
+      this.angularMomentumRate.setIncludingFrame(other.angularMomentumRate);
+      this.rootJointDesiredConfigurationData.set(other.rootJointDesiredConfigurationData);
+      this.lowLevelOneDoFJointControllerCoreOutputDataHolder.set(other.lowLevelOneDoFJointControllerCoreOutputDataHolder);
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (lowLevelOneDoFJointControllerCoreOutputDataHolder == null)
+         throw new RuntimeException("You used the deprecated constructor equals is not supported in that case");
+
+      if (obj == this)
+      {
+         return true;
+      }
+
+      else if (obj instanceof ControllerCoreOutPutDataHolder other)
+      {
+         if (!centerOfPressureDataHolder.equals(other.centerOfPressureDataHolder))
+            return false;
+         if (!desiredExternalWrenchHolder.equals(other.desiredExternalWrenchHolder))
+            return false;
+         if (!linearMomentum.equals(other.linearMomentum))
+            return false;
+         if (!angularMomentum.equals(other.angularMomentum))
+            return false;
+         if (!linearMomentumRate.equals(other.linearMomentumRate))
+            return false;
+         if (!angularMomentumRate.equals(other.angularMomentumRate))
+            return false;
+         if (!rootJointDesiredConfigurationData.equals(other.rootJointDesiredConfigurationData))
+            return false;
+         return lowLevelOneDoFJointControllerCoreOutputDataHolder.equals(other.lowLevelOneDoFJointControllerCoreOutputDataHolder);
+      }
+      else
+      {
+         return false;
+      }
+   }
+}
