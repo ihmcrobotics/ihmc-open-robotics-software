2e476ddc88f5e4730d879b06c9b681c272abb6ef
Duncan Calvert <dcalvert@ihmc.org>
2024-10-06 23:12:03 -0500

    Adding joint tracker.

---
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/SCS2LogJointTracker.java
@@ -0,0 +1,18 @@
+package us.ihmc.avatar.logProcessor;
+
+import us.ihmc.yoVariables.variable.YoDouble;
+
+public class SCS2LogJointTracker
+{
+   private final YoDouble jointPosition;
+
+   public SCS2LogJointTracker(YoDouble jointPosition)
+   {
+      this.jointPosition = jointPosition;
+   }
+
+   public void update()
+   {
+
+   }
+}
