f6b6b60b112460a431136f0e237208b99f2e2340
Tomasz Bialek <tbialek@ihmc.org>
2024-10-08 16:01:39 -0500

    Added ability to use multiple subnet masks

--- ihmc-communication/src/main/java/us/ihmc/communication/RTPSCommunicationFactory.java
+++ ihmc-communication/src/main/java/us/ihmc/communication/RTPSCommunicationFactory.java
@@ -1,22 +1,24 @@
 package us.ihmc.communication;
 
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.net.util.SubnetUtils;
+import org.apache.commons.net.util.SubnetUtils.SubnetInfo;
+import us.ihmc.communication.configuration.NetworkParameterKeys;
+import us.ihmc.communication.configuration.NetworkParameters;
+import us.ihmc.log.LogTools;
+
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.SystemUtils;
-import org.apache.commons.net.util.SubnetUtils;
-
-import org.apache.commons.net.util.SubnetUtils.SubnetInfo;
-import us.ihmc.communication.configuration.NetworkParameterKeys;
-import us.ihmc.communication.configuration.NetworkParameters;
-import us.ihmc.log.LogTools;
 
 /**
  * Loads the Domain ID and Subnet Restriction from ~/.ihmc/IHMCNetworkParameters.ini
@@ -27,7 +29,7 @@ public class RTPSCommunicationFactory
    private static final List<InterfaceAddress> MACHINE_INTERFACE_ADDRESSES = findInterfaceAddresses();
 
    private final int defaultDomainID;
-   private final InetAddress defaultAddressRestriction;
+   private final InetAddress[] defaultAddressRestrictions;
 
    /**
     * Creates an RTPSCommunicationFactory. Loads the default RTPS Domain ID from the Network Parameter
@@ -55,66 +57,69 @@ public class RTPSCommunicationFactory
                      """.formatted(NetworkParameters.defaultParameterFile, rtpsDomainID));
       }
 
-      InetAddress foundAddressRestriction = null;
+      Set<InetAddress> foundAddressRestrictions = new HashSet<>();
 
-      String restrictionHost;
+      String restrictionHostString;
       if (MACHINE_INTERFACE_ADDRESSES != null
        && NetworkParameters.hasKey(NetworkParameterKeys.RTPSSubnet)
-       && !(restrictionHost = NetworkParameters.getHost(NetworkParameterKeys.RTPSSubnet)).isEmpty())
+       && !(restrictionHostString = NetworkParameters.getHost(NetworkParameterKeys.RTPSSubnet)).isEmpty())
       {
-         if (SystemUtils.IS_OS_WINDOWS && !restrictionHost.contains("127.0.0.1")) // 127.0.0.1/X might be the only one that works on Windows
+         if (SystemUtils.IS_OS_WINDOWS && !restrictionHostString.contains("127.0.0.1")) // 127.0.0.1/X might be the only one that works on Windows
          {
             LogTools.warn("This feature might not work on Windows! "
                           + "If you are not receiving data, try it using 127.0.0.1/24 or without setting a subnet restriction.");
          }
 
-         LogTools.info("Scanning interfaces for restriction: {}",  restrictionHost);
+         LogTools.info("Scanning interfaces for restriction: {}",  restrictionHostString);
 
-         SubnetInfo restrictionSubnetInfo = new SubnetUtils(restrictionHost).getInfo();
-         LogTools.debug("Restriction subnet info:\n{}", restrictionSubnetInfo);
-         LogTools.info("Restriction address: {}", restrictionSubnetInfo.getAddress());
-         LogTools.info("Restriction netmask: {}", restrictionSubnetInfo.getNetmask());
-
-         for (InterfaceAddress interfaceAddress : MACHINE_INTERFACE_ADDRESSES)
+         String[] restrictionHostList = restrictionHostString.split("\\s*,\\s*");
+         for (String restrictionHost : restrictionHostList)
          {
-            InetAddress address = interfaceAddress.getAddress();
+            SubnetInfo restrictionSubnetInfo = new SubnetUtils(restrictionHost).getInfo();
+            LogTools.debug("Restriction subnet info:\n{}", restrictionSubnetInfo);
+            LogTools.info("Restriction address: {}", restrictionSubnetInfo.getAddress());
+            LogTools.info("Restriction netmask: {}", restrictionSubnetInfo.getNetmask());
 
-            if (address instanceof Inet4Address)
+            for (InterfaceAddress interfaceAddress : MACHINE_INTERFACE_ADDRESSES)
             {
-               short netmaskAsShort = interfaceAddress.getNetworkPrefixLength();
+               InetAddress address = interfaceAddress.getAddress();
 
-               String interfaceHost = address.getHostAddress();
-               SubnetInfo interfaceSubnetInfo = new SubnetUtils(interfaceHost + "/" + netmaskAsShort).getInfo();
-
-               LogTools.debug("Interface Subnet Info: " + interfaceSubnetInfo);
-               LogTools.debug("Interface address: " + interfaceSubnetInfo.getAddress());
-               LogTools.debug("Interface netmask: " + interfaceSubnetInfo.getNetmask());
-
-               boolean inRange;
-               if (SystemUtils.IS_OS_WINDOWS)
+               if (address instanceof Inet4Address)
                {
-                  inRange = interfaceSubnetInfo.isInRange(restrictionSubnetInfo.getAddress()); // This worked on Windows, but not Linux: Doug
-               }
-               else // Linux and others
-               {
-                  // This works on Linux. Does not work on Windows. Not tested on Mac.
-                  inRange = restrictionSubnetInfo.isInRange(interfaceSubnetInfo.getAddress());
-               }
+                  short netmaskAsShort = interfaceAddress.getNetworkPrefixLength();
 
-               if (inRange)
-               {
-                  LogTools.info("Found address in range: {}", address);
-                  foundAddressRestriction = address;
-                  break;
+                  String interfaceHost = address.getHostAddress();
+                  SubnetInfo interfaceSubnetInfo = new SubnetUtils(interfaceHost + "/" + netmaskAsShort).getInfo();
+
+                  LogTools.debug("Interface Subnet Info: " + interfaceSubnetInfo);
+                  LogTools.debug("Interface address: " + interfaceSubnetInfo.getAddress());
+                  LogTools.debug("Interface netmask: " + interfaceSubnetInfo.getNetmask());
+
+                  boolean inRange;
+                  if (SystemUtils.IS_OS_WINDOWS)
+                  {
+                     inRange = interfaceSubnetInfo.isInRange(restrictionSubnetInfo.getAddress()); // This worked on Windows, but not Linux: Doug
+                  }
+                  else // Linux and others
+                  {
+                     // This works on Linux. Does not work on Windows. Not tested on Mac.
+                     inRange = restrictionSubnetInfo.isInRange(interfaceSubnetInfo.getAddress());
+                  }
+
+                  if (inRange)
+                  {
+                     LogTools.info("Found address in range: {}", address);
+                     foundAddressRestrictions.add(address);
+                  }
                }
             }
          }
       }
 
       defaultDomainID = rtpsDomainID;
-      defaultAddressRestriction = foundAddressRestriction;
-      if (defaultAddressRestriction != null)
-         LogTools.info("Setting IP restriction: {}", defaultAddressRestriction.getHostAddress());
+      defaultAddressRestrictions = foundAddressRestrictions.toArray(InetAddress[]::new);
+      if (defaultAddressRestrictions.length > 0)
+         LogTools.info("Setting IP restriction: {}", Arrays.stream(defaultAddressRestrictions).map(InetAddress::getHostAddress).toList());
    }
 
    private static List<InterfaceAddress> findInterfaceAddresses()
@@ -136,9 +141,9 @@ public class RTPSCommunicationFactory
     * 
     * @return
     */
-   public InetAddress getAddressRestriction()
+   public InetAddress[] getAddressRestriction()
    {
-      return defaultAddressRestriction;
+      return defaultAddressRestrictions;
    }
 
    int getDomainId()
