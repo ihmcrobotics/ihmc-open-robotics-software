08b63f6056b6f2eee422b493f4deb99e229b45ce
Robert Griffin <rgriffin@ihmc.us>
2024-10-11 13:59:47 -0500

    moved out the code to its own repo

--- ihmc-operator-interface/src/main/java/us/ihmc/humanoidOperatorInterface/communication/subscribers/DetectedObjectListener.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.humanoidOperatorInterface.communication.subscribers;
-
-import us.ihmc.euclid.transform.RigidBodyTransform;
-
-public interface DetectedObjectListener
-{
-   public abstract void updatePose(RigidBodyTransform pose, int id);
-}
