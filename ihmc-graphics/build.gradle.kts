984d4e26650c1090266b8f134f018d82a1c69fd9
Duncan Calvert <dcalvert@ihmc.org>
2024-10-11 21:42:47 -0500

    Remove sensor-processing from rdx dependencies, moving up the height map renderer.

--- ihmc-graphics/build.gradle.kts
+++ ihmc-graphics/build.gradle.kts
@@ -45,7 +45,8 @@ jmonkeyengineTestDependencies {
 }
 
 libgdxDependencies {
-   api(ihmc.sourceSetProject("main"))
+   api("us.ihmc:ihmc-communication:source")
+   api("us.ihmc:ihmc-robot-models:source")
 
    val gdxVersion = "1.12.1"
    api("com.badlogicgames.gdx:gdx-backend-lwjgl3:$gdxVersion")
