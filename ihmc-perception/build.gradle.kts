8e2164858c0fcc3ebed82e6fd23f01c90f5f1688
Dexton Anderson <danderson@ihmc.org>
2024-10-22 14:41:46 -0500

    :arrow_up: zed-java-api 4.2.0

--- ihmc-perception/build.gradle.kts
+++ ihmc-perception/build.gradle.kts
@@ -70,7 +70,7 @@ mainDependencies {
    api("org.bytedeco:spinnaker:$spinnakerVersion:windows-x86_64") {
       exclude(group = "org.bytedeco", module = "javacpp")
    }
-   api("us.ihmc:zed-java-api:4.1.4") {
+   api("us.ihmc:zed-java-api:4.2.0") {
       exclude(group = "org.bytedeco")
    }
    val hdf5Version = "1.14.1-1.5.9"
