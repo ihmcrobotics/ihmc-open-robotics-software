08eba6cc16fb14b3fcabce1c3c60b85db9f41ff5
Dexton Anderson <danderson@ihmc.org>
2024-10-24 16:12:06 -0500

    Remove onnx

--- ihmc-perception/build.gradle.kts
+++ ihmc-perception/build.gradle.kts
@@ -94,9 +94,6 @@ mainDependencies {
 
    api("us.ihmc:ihmc-common-walking-control-modules:source")
    api("us.ihmc:robot-environment-awareness:source")
-
-   api("com.microsoft.onnxruntime:onnxruntime:1.11.0")
-   api("com.microsoft.onnxruntime:onnxruntime_gpu:1.11.0")
 }
 
 testDependencies {
