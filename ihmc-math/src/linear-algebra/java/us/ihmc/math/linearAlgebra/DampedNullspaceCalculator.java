b2d320406c8a76a16ad0bf3da12b0ef6b21ba73f
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-28 11:31:21 -0500

    moved out ihmc math project to its own repo

--- ihmc-math/src/linear-algebra/java/us/ihmc/math/linearAlgebra/DampedNullspaceCalculator.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.math.linearAlgebra;
-
-import us.ihmc.math.linearAlgebra.NullspaceCalculator;
-
-public interface DampedNullspaceCalculator extends NullspaceCalculator
-{
-   public void setPseudoInverseAlpha(double alpha);
-}
