aae7422ba582aed27f91c3b0522a18d942c89b97
Dexton Anderson <danderson@ihmc.org>
2024-10-31 13:03:45 -0500

    Rename loopbackNode->sharedMemoryNode

--- ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoStreamImageMessageRelay.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoStreamImageMessageRelay.java
@@ -9,14 +9,14 @@ import java.util.Set;
 
 public class ROS2SRTVideoStreamImageMessageRelay
 {
-   private final ROS2Node loopbackNode = ROS2Tools.createSharedMemoryROS2Node("srt_stream_image_message_republisher");
+   private final ROS2Node sharedMemoryNode = ROS2Tools.createSharedMemoryROS2Node("srt_stream_image_message_republisher");
 
    private final Set<ROS2SRTVideoStreamImageMessageRelayWorker> workers = new HashSet<>();
 
    public ROS2SRTVideoStreamImageMessageRelay(Set<ROS2SRTStreamTopicPair> topicsToRelay, ROS2Node ros2Node)
    {
       for (ROS2SRTStreamTopicPair topicPair : topicsToRelay)
-         workers.add(new ROS2SRTVideoStreamImageMessageRelayWorker(loopbackNode, ros2Node, topicPair));
+         workers.add(new ROS2SRTVideoStreamImageMessageRelayWorker(sharedMemoryNode, ros2Node, topicPair));
    }
 
    public void destroy()
@@ -24,6 +24,6 @@ public class ROS2SRTVideoStreamImageMessageRelay
       for (ROS2SRTVideoStreamImageMessageRelayWorker worker : workers)
          worker.destroy();
 
-      loopbackNode.destroy();
+      sharedMemoryNode.destroy();
    }
 }
