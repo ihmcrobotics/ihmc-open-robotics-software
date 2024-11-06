81e29a00771e8348460a61926f33cc297312b1f7
Tomasz Bialek <tbialek@ihmc.org>
2024-11-06 09:10:30 -0600

    Working planar regions

--- ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/ros2/ROS2SceneGraphUpdateThread.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/sceneGraph/ros2/ROS2SceneGraphUpdateThread.java
@@ -1,9 +1,14 @@
 package us.ihmc.perception.sceneGraph.ros2;
 
 import us.ihmc.commons.thread.RepeatingTaskThread;
+import us.ihmc.commons.thread.TypedNotification;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.perception.detections.DetectionManager;
 import us.ihmc.perception.sceneGraph.SceneGraph;
+import us.ihmc.perception.sceneGraph.SceneNode;
+import us.ihmc.perception.sceneGraph.rigidBody.doors.DoorNode;
+import us.ihmc.robotics.geometry.FramePlanarRegionsList;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
 
 import java.util.function.Supplier;
 
@@ -12,14 +17,19 @@ public class ROS2SceneGraphUpdateThread extends RepeatingTaskThread
    private final ROS2SceneGraph sceneGraph;
    private final DetectionManager detectionManager;
    private final Supplier<ReferenceFrame> robotPelvisFrameSupplier;
+   private final TypedNotification<FramePlanarRegionsList> planarRegionsNotification;
 
-   public ROS2SceneGraphUpdateThread(ROS2SceneGraph sceneGraph, DetectionManager detectionManager, Supplier<ReferenceFrame> robotPelvisFrameSupplier)
+   public ROS2SceneGraphUpdateThread(ROS2SceneGraph sceneGraph,
+                                     DetectionManager detectionManager,
+                                     Supplier<ReferenceFrame> robotPelvisFrameSupplier,
+                                     TypedNotification<FramePlanarRegionsList> planarRegionsNotification)
    {
       super(ROS2SceneGraphUpdateThread.class.getSimpleName());
       setFrequencyLimit(SceneGraph.UPDATE_FREQUENCY);
       this.sceneGraph = sceneGraph;
       this.detectionManager = detectionManager;
       this.robotPelvisFrameSupplier = robotPelvisFrameSupplier;
+      this.planarRegionsNotification = planarRegionsNotification;
    }
 
    @Override
@@ -31,7 +41,19 @@ public class ROS2SceneGraphUpdateThread extends RepeatingTaskThread
 
       sceneGraph.updateDetections(detectionManager);
 
-      // TODO: Add planar regions
+      // Update planar regions for door nodes
+      if (planarRegionsNotification.poll())
+      {
+         FramePlanarRegionsList framePlanarRegions = planarRegionsNotification.read();
+         PlanarRegionsList planarRegionsInWorldFrame = framePlanarRegions.getPlanarRegionsList();
+         planarRegionsInWorldFrame.applyTransform(framePlanarRegions.getSensorToWorldFrameTransform());
+
+         for (SceneNode sceneNode : sceneGraph.getSceneNodesByID())
+         {
+            if (sceneNode instanceof DoorNode doorNode)
+               doorNode.getDoorPanel().filterAndSetPlanarRegionFromPlanarRegionsList(planarRegionsInWorldFrame);
+         }
+      }
 
       ReferenceFrame pelvisFrame = robotPelvisFrameSupplier.get();
       sceneGraph.updateOnRobotOnly(pelvisFrame);
