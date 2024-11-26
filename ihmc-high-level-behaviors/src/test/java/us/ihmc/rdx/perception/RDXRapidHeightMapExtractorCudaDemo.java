fc1da2be70b0f47ed8acb1d2dc4ae9d2d15ab575
Duncan Calvert <dcalvert@ihmc.us>
2024-11-26 16:04:25 -0600

    Take a stab at rendering the height map.

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXRapidHeightMapExtractorCudaDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXRapidHeightMapExtractorCudaDemo.java
@@ -1,22 +1,37 @@
 package us.ihmc.rdx.perception;
 
+import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.GpuMat;
+import org.bytedeco.opencv.opencv_core.Mat;
+import perception_msgs.msg.dds.HeightMapMessage;
+import perception_msgs.msg.dds.ImageMessage;
 import us.ihmc.commons.thread.Throttler;
+import us.ihmc.communication.PerceptionAPI;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.log.LogTools;
-import us.ihmc.perception.RawImage;
 import us.ihmc.perception.camera.CameraIntrinsics;
+import us.ihmc.perception.gpuHeightMap.RapidHeightMapExtractor;
 import us.ihmc.perception.gpuHeightMap.RapidHeightMapExtractorCuda;
+import us.ihmc.perception.opencv.OpenCVTools;
+import us.ihmc.perception.tools.NativeMemoryTools;
+import us.ihmc.perception.tools.PerceptionMessageTools;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
+import us.ihmc.rdx.mesh.RDXMultiColorMeshBuilder;
 import us.ihmc.rdx.sceneManager.RDXSceneLevel;
 import us.ihmc.rdx.simulation.environment.RDXEnvironmentBuilder;
 import us.ihmc.rdx.simulation.sensors.RDXSensorSimulator;
 import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.rdx.ui.gizmo.RDXPose3DGizmo;
+import us.ihmc.rdx.ui.graphics.RDXHeightMapGraphicNew;
 import us.ihmc.robotics.referenceFrames.ZUpFrame;
+import us.ihmc.sensorProcessing.heightMap.HeightMapData;
+import us.ihmc.sensorProcessing.heightMap.HeightMapMessageTools;
+
+import java.util.List;
 
 public class RDXRapidHeightMapExtractorCudaDemo
 {
@@ -33,6 +48,15 @@ public class RDXRapidHeightMapExtractorCudaDemo
    private final RigidBodyTransform sensorToSensorZUp = new RigidBodyTransform();
    private final RigidBodyTransform sensorZUpToWorld = new RigidBodyTransform();
    private GpuMat heightMapImage;
+   private final RDXHeightMapGraphicNew heightMapGraphic = new RDXHeightMapGraphicNew();
+   private final BytePointer compressedCroppedHeightMapPointer = new BytePointer();
+   private final ImageMessage croppedHeightMapImageMessage = new ImageMessage();
+   private final FramePose3D cameraPoseForHeightMap = new FramePose3D();
+   private float pixelScalingFactor = 10000.0f;
+   private boolean heightMapMessageGenerated = false;
+   private final RigidBodyTransform zUpToWorldTransform = new RigidBodyTransform();
+   private HeightMapData heightMapData;
+   private final HeightMapMessage heightMapMessage = new HeightMapMessage();
 
    public RDXRapidHeightMapExtractorCudaDemo()
    {
@@ -80,6 +104,8 @@ public class RDXRapidHeightMapExtractorCudaDemo
             extractor = new RapidHeightMapExtractorCuda(leftFootSoleFrame, rightFootSoleFrame);
             extractor.setDepthIntrinsics(intrinsics);
             extractor.create(heightMapImage, 1);
+
+            baseUI.getPrimaryScene().addRenderableProvider(heightMapGraphic, RDXSceneLevel.MODEL);
          }
 
          @Override
@@ -97,6 +123,58 @@ public class RDXRapidHeightMapExtractorCudaDemo
                heightMapImage.upload(sensorSimulator.getDepthImage().getCpuImageMat());
 
                extractor.update(sensorPoseGizmo.getTransformToParent(), sensorToSensorZUp, sensorZUpToWorld);
+
+               heightMapData = RapidHeightMapExtractorCuda.packHeightMapData(extractor, heightMapData);
+
+               Mat croppedHeightMapImage = extractor.getTerrainMapData().getHeightMap();
+
+               cameraPoseForHeightMap.setToZero(sensorPoseGizmo.getGizmoFrame());
+               cameraPoseForHeightMap.changeFrame(ReferenceFrame.getWorldFrame());
+
+//               OpenCVTools.compressImagePNG(croppedHeightMapImage, compressedCroppedHeightMapPointer);
+//               PerceptionMessageTools.packCompressedDepthImage(compressedCroppedHeightMapPointer,
+//                                                               croppedHeightMapImageMessage,
+//                                                               cameraPoseForHeightMap,
+//                                                               sensorSimulator.getDepthImage().getAcquisitionTime(),
+//                                                               sensorSimulator.getDepthImage().getSequenceNumber(),
+//                                                               croppedHeightMapImage.rows(),
+//                                                               croppedHeightMapImage.cols(),
+//                                                               (float) RapidHeightMapExtractor.getHeightMapParameters().getHeightScaleFactor());
+
+               pixelScalingFactor = croppedHeightMapImageMessage.getDepthDiscretization();
+               zUpToWorldTransform.set(croppedHeightMapImageMessage.getOrientation(), croppedHeightMapImageMessage.getPosition());
+
+
+
+
+//               PerceptionMessageTools.convertToHeightMapImage(croppedHeightMapImageMessage,
+//                                                              heightMapImage,
+//                                                              incomingCompressedImageBuffer,
+//                                                              incomingCompressedImageBytePointer,
+//                                                              compressedBytesMat);
+
+                  PerceptionMessageTools.convertToHeightMapData(sensorSimulator.getDepthImage().getCpuImageMat(),
+                                                                heightMapData,
+                                                                croppedHeightMapImageMessage.getPosition(),
+                                                                (float) RapidHeightMapExtractor.getHeightMapParameters()
+                                                                                               .getGlobalWidthInMeters(),
+                                                                (float) RapidHeightMapExtractor.getHeightMapParameters()
+                                                                                               .getGlobalCellSizeInMeters());
+                  HeightMapMessageTools.toMessage(heightMapData, heightMapMessage);
+
+
+//               List<RDXMultiColorMeshBuilder> multiColorMeshBuilders = RDXHeightMapGraphicNew.generateHeightCells(heightsProvided,
+//                                                                                                                  keysProvider,
+//                                                                                                                  numberOfOccupiedCells,
+//                                                                                                                  gridResolutionXY,
+//                                                                                                                  gridSizeXy,
+//                                                                                                                  gridCenterX,
+//                                                                                                                  gridCenterY,
+//                                                                                                                  groundHeight);
+//               heightMapGraphic.setLatestMeshBuilder(multiColorMeshBuilders);
+
+               heightMapGraphic.generateMeshesAsync(heightMapMessage);
+               heightMapGraphic.update();
             }
 
             baseUI.renderBeforeOnScreenUI();
@@ -106,6 +184,7 @@ public class RDXRapidHeightMapExtractorCudaDemo
          @Override
          public void dispose()
          {
+            heightMapGraphic.destroy();
             extractor.destroy();
             environmentBuilder.destroy();
             baseUI.dispose();
