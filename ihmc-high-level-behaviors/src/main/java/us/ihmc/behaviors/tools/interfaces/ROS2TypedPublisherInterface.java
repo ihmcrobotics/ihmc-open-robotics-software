ac576906ead2a3e9f98e97031fe42ed62bc531e2
Duncan Calvert <dcalvert@ihmc.org>
2024-11-29 10:51:35 -0600

    Archive old behavior code (#497)

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/tools/interfaces/ROS2TypedPublisherInterface.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.behaviors.tools.interfaces;
-
-import us.ihmc.ros2.ROS2Topic;
-
-public interface ROS2TypedPublisherInterface
-{
-   <T> void publishROS2(ROS2Topic<T> topic, T message);
-}
