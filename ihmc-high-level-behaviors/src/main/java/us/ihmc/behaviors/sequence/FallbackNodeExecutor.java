1d03d4a65921b3a6abf4368e470149f74c37c4a7
Duncan Calvert <dcalvert@ihmc.us>
2024-12-04 14:35:45 -0600

    Document and build list of action children.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/FallbackNodeExecutor.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/FallbackNodeExecutor.java
@@ -4,10 +4,19 @@ import us.ihmc.behaviors.behaviorTree.BehaviorTreeNodeExecutor;
 import us.ihmc.communication.crdt.CRDTInfo;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * If the first child action of a fallback node succeeds, the rest of the children are skipped.
+ * If it fails, the rest of the children are executed.
+ * Afterwards, the parent sequence proceeds.
+ */
 public class FallbackNodeExecutor extends BehaviorTreeNodeExecutor<FallbackNodeState, FallbackNodeDefinition>
 {
    private final FallbackNodeState state;
    private final FallbackNodeDefinition definition;
+   private final List<ActionNodeExecutor<?, ?>> actionChildren = new ArrayList<>();
 
    public FallbackNodeExecutor(long id, CRDTInfo crdtInfo, WorkspaceResourceDirectory saveFileDirectory)
    {
@@ -21,5 +30,19 @@ public class FallbackNodeExecutor extends BehaviorTreeNodeExecutor<FallbackNodeS
    public void update()
    {
       super.update();
+
+      actionChildren.clear();
+      for (BehaviorTreeNodeExecutor<?, ?> child : getChildren())
+      {
+         if (child instanceof ActionNodeExecutor<?, ?> actionNode)
+         {
+            actionChildren.add(actionNode);
+         }
+      }
+   }
+
+   public List<ActionNodeExecutor<?, ?>> getActionChildren()
+   {
+      return actionChildren;
    }
 }
