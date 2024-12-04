401f427d896ec5fade2bfc96becf847494e2c987
Duncan Calvert <dcalvert@ihmc.org>
2024-12-04 14:17:45 -0600

    Undo accidentally included change.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/FallbackNodeExecutor.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/sequence/FallbackNodeExecutor.java
@@ -1,8 +1,6 @@
 package us.ihmc.behaviors.sequence;
 
 import us.ihmc.behaviors.behaviorTree.BehaviorTreeNodeExecutor;
-import us.ihmc.behaviors.behaviorTree.BehaviorTreeRootNodeExecutor;
-import us.ihmc.behaviors.behaviorTree.BehaviorTreeTools;
 import us.ihmc.communication.crdt.CRDTInfo;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
 
@@ -23,7 +21,5 @@ public class FallbackNodeExecutor extends BehaviorTreeNodeExecutor<FallbackNodeS
    public void update()
    {
       super.update();
-
-      BehaviorTreeRootNodeExecutor rootNode = BehaviorTreeTools.findRootNode(this);
    }
 }
