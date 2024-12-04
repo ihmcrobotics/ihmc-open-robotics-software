e3b638e032b252fa601f97e2dbd09049ae745405
Dexton Anderson <danderson@ihmc.org>
2024-12-04 10:13:10 -0600

    Add optional jpeg compression in srt video relay

--- ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoStreamImageMessageRelayWorker.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/streaming/ROS2SRTVideoStreamImageMessageRelayWorker.java
@@ -1,5 +1,6 @@
 package us.ihmc.perception.streaming;
 
+import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.opencv.opencv_core.Mat;
 import perception_msgs.msg.dds.ImageMessage;
 import us.ihmc.communication.packets.MessageTools;
@@ -7,6 +8,7 @@ import us.ihmc.communication.ros2.ROS2Helper;
 import us.ihmc.communication.ros2.ROS2SRTStreamTopicPair;
 import us.ihmc.perception.CameraModel;
 import us.ihmc.perception.RawImage;
+import us.ihmc.perception.cuda.CUDAJPEGProcessor;
 import us.ihmc.perception.imageMessage.CompressionType;
 import us.ihmc.perception.imageMessage.PixelFormat;
 import us.ihmc.perception.opencv.OpenCVTools;
@@ -14,12 +16,22 @@ import us.ihmc.perception.tools.PerceptionMessageTools;
 import us.ihmc.ros2.ROS2Node;
 import us.ihmc.ros2.ROS2PublisherBasics;
 
+import javax.annotation.Nullable;
 import java.time.Instant;
 
 public class ROS2SRTVideoStreamImageMessageRelayWorker
 {
+   /**
+    * Set to true to enable compression to JPEG (using nvJPEG) before publishing the ImageMessage.
+    * Introduces GPU overhead and more latency, but useful for recording rosbags.
+    */
+   private static final boolean COMPRESS_USING_NVJPEG = true;
+
    private final ROS2PublisherBasics<ImageMessage> publisher;
    private final ROS2SRTVideoSubscriber subscriber;
+
+   @Nullable
+   private CUDAJPEGProcessor cudajpegProcessor;
 
    private final ImageMessage imageMessage;
    private long frameSequenceNumber = 0L;
@@ -30,7 +42,7 @@ public class ROS2SRTVideoStreamImageMessageRelayWorker
 
       imageMessage = new ImageMessage();
       imageMessage.setPixelFormat(outputPixelFormat.toByte());
-      imageMessage.setCompressionType(CompressionType.UNCOMPRESSED.toByte());
+      imageMessage.setCompressionType(COMPRESS_USING_NVJPEG ? CompressionType.NVJPEG.toByte() : CompressionType.UNCOMPRESSED.toByte());
       imageMessage.setCameraModel(CameraModel.PINHOLE.toByte());
 
       // Create publisher and subscriber using two separate nodes as publisher should ideally only publish on loopback.
@@ -38,12 +50,17 @@ public class ROS2SRTVideoStreamImageMessageRelayWorker
       subscriber = new ROS2SRTVideoSubscriber(new ROS2Helper(subscriberNode), streamTopicPair.streamStatusTopic(), outputPixelFormat);
       subscriber.addNewFrameConsumer(this::republishFrameAsImageMessage);
       subscriber.subscribe();
+
+      if (COMPRESS_USING_NVJPEG)
+         cudajpegProcessor = new CUDAJPEGProcessor(90);
    }
 
    public void destroy()
    {
       subscriber.destroy();
       publisher.remove();
+      if (COMPRESS_USING_NVJPEG && cudajpegProcessor != null)
+         cudajpegProcessor.destroy();
    }
 
    private void republishFrameAsImageMessage(RawImage frame)
@@ -51,9 +68,19 @@ public class ROS2SRTVideoStreamImageMessageRelayWorker
       // Set acquisition time as now... this isn't super accurate though
       MessageTools.toMessage(Instant.now(), imageMessage.getAcquisitionTime());
 
-      // Pack the uncompressed data
       Mat frameMat = frame.getCpuImageMat();
-      PerceptionMessageTools.packImageMessageData(imageMessage, frameMat.data().limit(OpenCVTools.dataSize(frameMat)));
+
+      if (COMPRESS_USING_NVJPEG && cudajpegProcessor != null)
+      {
+         BytePointer encodedData = new BytePointer(OpenCVTools.dataSize(frameMat));
+         cudajpegProcessor.encodeBGR(frameMat, encodedData);
+         PerceptionMessageTools.packImageMessageData(imageMessage, encodedData);
+         encodedData.close();
+      }
+      else
+      {
+         PerceptionMessageTools.packImageMessageData(imageMessage, frameMat.data().limit(OpenCVTools.dataSize(frameMat)));
+      }
 
       // Pack the image message meta data
       frame.packImageMessageMetaData(imageMessage);
