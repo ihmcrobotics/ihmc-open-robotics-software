4cfaf0c07f5856b3448a0ce42ad33629a5a407c5
npoddar <npoddar@ihmc.org>
2024-12-06 14:28:48 -0600

    make some reviewed changes 3

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXRapidHeightMapExtractorCudaDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXRapidHeightMapExtractorCudaDemo.java
@@ -7,20 +7,15 @@ import org.bytedeco.opencv.opencv_core.Mat;
 import perception_msgs.msg.dds.HeightMapMessage;
 import perception_msgs.msg.dds.ImageMessage;
 import us.ihmc.commons.thread.Throttler;
-import us.ihmc.communication.PerceptionAPI;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.tools.ReferenceFrameTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
-import us.ihmc.log.LogTools;
 import us.ihmc.perception.camera.CameraIntrinsics;
 import us.ihmc.perception.gpuHeightMap.RapidHeightMapExtractor;
 import us.ihmc.perception.gpuHeightMap.RapidHeightMapExtractorCuda;
-import us.ihmc.perception.opencv.OpenCVTools;
-import us.ihmc.perception.tools.NativeMemoryTools;
 import us.ihmc.perception.tools.PerceptionMessageTools;
 import us.ihmc.rdx.Lwjgl3ApplicationAdapter;
-import us.ihmc.rdx.mesh.RDXMultiColorMeshBuilder;
 import us.ihmc.rdx.sceneManager.RDXSceneLevel;
 import us.ihmc.rdx.simulation.environment.RDXEnvironmentBuilder;
 import us.ihmc.rdx.simulation.sensors.RDXSensorSimulator;
@@ -31,105 +26,97 @@ import us.ihmc.robotics.referenceFrames.ZUpFrame;
 import us.ihmc.sensorProcessing.heightMap.HeightMapData;
 import us.ihmc.sensorProcessing.heightMap.HeightMapMessageTools;
 
-import java.util.List;
+public class RDXRapidHeightMapExtractorCudaDemo {
+    private static final float FOV = 70.0f;
+    private static final float MIN_RANGE = 0.2f;
+    private static final float MAX_RANGE = 20.0f;
 
-public class RDXRapidHeightMapExtractorCudaDemo
-{
-   private static final float FOV = 70.0f;
-   private static final float MIN_RANGE = 0.2f;
-   private static final float MAX_RANGE = 20.0f;
+    private final Throttler throttler = new Throttler().setFrequency(30.0);
+    private final RDXSensorSimulator sensorSimulator;
+    private RDXPose3DGizmo sensorPoseGizmo;
+    private RDXEnvironmentBuilder environmentBuilder;
+    private RapidHeightMapExtractorCuda extractor;
+    private ZUpFrame sensorZUpFrame;
+    private final RigidBodyTransform sensorToSensorZUp = new RigidBodyTransform();
+    private final RigidBodyTransform sensorZUpToWorld = new RigidBodyTransform();
+    private GpuMat heightMapImage;
+    private final RDXHeightMapGraphicNew heightMapGraphic = new RDXHeightMapGraphicNew();
+    private final BytePointer compressedCroppedHeightMapPointer = new BytePointer();
+    private final ImageMessage croppedHeightMapImageMessage = new ImageMessage();
+    private final FramePose3D cameraPoseForHeightMap = new FramePose3D();
+    private float pixelScalingFactor = 10000.0f;
+    private boolean heightMapMessageGenerated = false;
+    private final RigidBodyTransform zUpToWorldTransform = new RigidBodyTransform();
+    private HeightMapData heightMapData;
+    private final HeightMapMessage heightMapMessage = new HeightMapMessage();
 
-   private final Throttler throttler = new Throttler().setFrequency(30.0);
-   private final RDXSensorSimulator sensorSimulator;
-   private RDXPose3DGizmo sensorPoseGizmo;
-   private RDXEnvironmentBuilder environmentBuilder;
-   private RapidHeightMapExtractorCuda extractor;
-   private ZUpFrame sensorZUpFrame;
-   private final RigidBodyTransform sensorToSensorZUp = new RigidBodyTransform();
-   private final RigidBodyTransform sensorZUpToWorld = new RigidBodyTransform();
-   private GpuMat heightMapImage;
-   private final RDXHeightMapGraphicNew heightMapGraphic = new RDXHeightMapGraphicNew();
-   private final BytePointer compressedCroppedHeightMapPointer = new BytePointer();
-   private final ImageMessage croppedHeightMapImageMessage = new ImageMessage();
-   private final FramePose3D cameraPoseForHeightMap = new FramePose3D();
-   private float pixelScalingFactor = 10000.0f;
-   private boolean heightMapMessageGenerated = false;
-   private final RigidBodyTransform zUpToWorldTransform = new RigidBodyTransform();
-   private HeightMapData heightMapData;
-   private final HeightMapMessage heightMapMessage = new HeightMapMessage();
+    public RDXRapidHeightMapExtractorCudaDemo() {
+        sensorSimulator = new RDXSensorSimulator(1280, 720, FOV, MIN_RANGE, MAX_RANGE);
 
-   public RDXRapidHeightMapExtractorCudaDemo()
-   {
-      sensorSimulator = new RDXSensorSimulator(1280, 720, FOV, MIN_RANGE, MAX_RANGE);
+        RDXBaseUI baseUI = new RDXBaseUI();
+        baseUI.launchRDXApplication(new Lwjgl3ApplicationAdapter() {
+            @Override
+            public void create() {
+                baseUI.create(RDXSceneLevel.GROUND_TRUTH, RDXSceneLevel.MODEL, RDXSceneLevel.VIRTUAL);
 
-      RDXBaseUI baseUI = new RDXBaseUI();
-      baseUI.launchRDXApplication(new Lwjgl3ApplicationAdapter()
-      {
-         @Override
-         public void create()
-         {
-            baseUI.create(RDXSceneLevel.GROUND_TRUTH, RDXSceneLevel.MODEL, RDXSceneLevel.VIRTUAL);
+                environmentBuilder = new RDXEnvironmentBuilder(baseUI.getPrimary3DPanel());
+                environmentBuilder.create();
+                environmentBuilder.loadEnvironment("HarderTerrain.json");
+                baseUI.getImGuiPanelManager().addPanel(environmentBuilder);
 
-            environmentBuilder = new RDXEnvironmentBuilder(baseUI.getPrimary3DPanel());
-            environmentBuilder.create();
-            environmentBuilder.loadEnvironment("HarderTerrain.json");
-            baseUI.getImGuiPanelManager().addPanel(environmentBuilder);
+                sensorSimulator.create(baseUI.getPrimaryScene());
+                sensorSimulator.enableColor(true);
+                sensorSimulator.enableDepth(true);
 
-            sensorSimulator.create(baseUI.getPrimaryScene());
-            sensorSimulator.enableColor(true);
-            sensorSimulator.enableDepth(true);
+                sensorPoseGizmo = new RDXPose3DGizmo();
+                sensorPoseGizmo.createAndSetupDefault(baseUI.getPrimary3DPanel());
+                sensorPoseGizmo.getTransformToParent().getTranslation().set(0.3, 0.0, 1.5);
+                sensorPoseGizmo.getTransformToParent().getRotation().setYawPitchRoll(0.0, Math.toRadians(38.0), 0.0);
+                sensorPoseGizmo.update();
 
-            sensorPoseGizmo = new RDXPose3DGizmo();
-            sensorPoseGizmo.createAndSetupDefault(baseUI.getPrimary3DPanel());
-            sensorPoseGizmo.getTransformToParent().getTranslation().set(0.3, 0.0, 1.5);
-            sensorPoseGizmo.getTransformToParent().getRotation().setYawPitchRoll(0.0, Math.toRadians(38.0), 0.0);
-            sensorPoseGizmo.update();
+                RigidBodyTransform leftSoleToGizmo = new RigidBodyTransform();
+                leftSoleToGizmo.getTranslation().set(0.0, 0.1, -0.5);
+                RigidBodyTransform rightSoleToGizmo = new RigidBodyTransform();
+                rightSoleToGizmo.getTranslation().set(0.0, -0.1, -0.5);
+                ReferenceFrame leftFootSoleFrame = ReferenceFrameTools.constructFrameWithUnchangingTransformToParent("leftFootSoleFrame",
+                        sensorPoseGizmo.getGizmoFrame(),
+                        leftSoleToGizmo);
+                ReferenceFrame rightFootSoleFrame = ReferenceFrameTools.constructFrameWithUnchangingTransformToParent("rightFootSoleFrame",
+                        sensorPoseGizmo.getGizmoFrame(),
+                        rightSoleToGizmo);
+                sensorZUpFrame = new ZUpFrame(sensorPoseGizmo.getGizmoFrame(), "sensorZUpFrame");
 
-            RigidBodyTransform leftSoleToGizmo = new RigidBodyTransform();
-            leftSoleToGizmo.getTranslation().set(0.0, 0.1, -0.5);
-            RigidBodyTransform rightSoleToGizmo = new RigidBodyTransform();
-            rightSoleToGizmo.getTranslation().set(0.0, -0.1, -0.5);
-            ReferenceFrame leftFootSoleFrame = ReferenceFrameTools.constructFrameWithUnchangingTransformToParent("leftFootSoleFrame",
-                                                                                                                 sensorPoseGizmo.getGizmoFrame(),
-                                                                                                                 leftSoleToGizmo);
-            ReferenceFrame rightFootSoleFrame = ReferenceFrameTools.constructFrameWithUnchangingTransformToParent("rightFootSoleFrame",
-                                                                                                                  sensorPoseGizmo.getGizmoFrame(),
-                                                                                                                  rightSoleToGizmo);
-            sensorZUpFrame = new ZUpFrame(sensorPoseGizmo.getGizmoFrame(), "sensorZUpFrame");
+                CameraIntrinsics intrinsics = sensorSimulator.getDepthImage().getIntrinsicsCopy();
 
-            CameraIntrinsics intrinsics = sensorSimulator.getDepthImage().getIntrinsicsCopy();
+                heightMapImage = new GpuMat(intrinsics.getWidth(), intrinsics.getHeight(), opencv_core.CV_16UC1);
 
-            heightMapImage = new GpuMat(intrinsics.getWidth(), intrinsics.getHeight(), opencv_core.CV_16UC1);
+                extractor = new RapidHeightMapExtractorCuda(leftFootSoleFrame, rightFootSoleFrame);
+                extractor.setDepthIntrinsics(intrinsics);
+                extractor.create(heightMapImage, 1);
 
-            extractor = new RapidHeightMapExtractorCuda(leftFootSoleFrame, rightFootSoleFrame);
-            extractor.setDepthIntrinsics(intrinsics);
-            extractor.create(heightMapImage, 1);
+                baseUI.getPrimaryScene().addRenderableProvider(heightMapGraphic, RDXSceneLevel.MODEL);
+            }
 
-            baseUI.getPrimaryScene().addRenderableProvider(heightMapGraphic, RDXSceneLevel.MODEL);
-         }
+            @Override
+            public void render() {
+                if (throttler.run()) {
+                    // "Grab" the image
+                    sensorSimulator.render(sensorPoseGizmo.getTransformToParent());
 
-         @Override
-         public void render()
-         {
-            if (throttler.run())
-            {
-               // "Grab" the image
-               sensorSimulator.render(sensorPoseGizmo.getTransformToParent());
+                    sensorZUpFrame.update();
+                    sensorPoseGizmo.getGizmoFrame().getTransformToDesiredFrame(sensorToSensorZUp, sensorZUpFrame);
+                    sensorZUpFrame.getTransformToDesiredFrame(sensorZUpToWorld, ReferenceFrame.getWorldFrame());
 
-               sensorZUpFrame.update();
-               sensorPoseGizmo.getGizmoFrame().getTransformToDesiredFrame(sensorToSensorZUp, sensorZUpFrame);
-               sensorZUpFrame.getTransformToDesiredFrame(sensorZUpToWorld, ReferenceFrame.getWorldFrame());
+                    heightMapImage.upload(sensorSimulator.getDepthImage().getCpuImageMat());
 
-               heightMapImage.upload(sensorSimulator.getDepthImage().getCpuImageMat());
+                    extractor.update(sensorPoseGizmo.getTransformToParent(), sensorToSensorZUp, sensorZUpToWorld);
 
-               extractor.update(sensorPoseGizmo.getTransformToParent(), sensorToSensorZUp, sensorZUpToWorld);
+                    heightMapData = RapidHeightMapExtractorCuda.packHeightMapData(extractor, heightMapData);
 
-               heightMapData = RapidHeightMapExtractorCuda.packHeightMapData(extractor, heightMapData);
+                    Mat croppedHeightMapImage = extractor.getTerrainMapData().getHeightMap();
 
-               Mat croppedHeightMapImage = extractor.getTerrainMapData().getHeightMap();
-
-               cameraPoseForHeightMap.setToZero(sensorPoseGizmo.getGizmoFrame());
-               cameraPoseForHeightMap.changeFrame(ReferenceFrame.getWorldFrame());
+                    cameraPoseForHeightMap.setToZero(sensorPoseGizmo.getGizmoFrame());
+                    cameraPoseForHeightMap.changeFrame(ReferenceFrame.getWorldFrame());
 
 //               OpenCVTools.compressImagePNG(croppedHeightMapImage, compressedCroppedHeightMapPointer);
 //               PerceptionMessageTools.packCompressedDepthImage(compressedCroppedHeightMapPointer,
@@ -141,10 +128,8 @@ public class RDXRapidHeightMapExtractorCudaDemo
 //                                                               croppedHeightMapImage.cols(),
 //                                                               (float) RapidHeightMapExtractor.getHeightMapParameters().getHeightScaleFactor());
 
-               pixelScalingFactor = croppedHeightMapImageMessage.getDepthDiscretization();
-               zUpToWorldTransform.set(croppedHeightMapImageMessage.getOrientation(), croppedHeightMapImageMessage.getPosition());
-
-
+                    pixelScalingFactor = croppedHeightMapImageMessage.getDepthDiscretization();
+                    zUpToWorldTransform.set(croppedHeightMapImageMessage.getOrientation(), croppedHeightMapImageMessage.getPosition());
 
 
 //               PerceptionMessageTools.convertToHeightMapImage(croppedHeightMapImageMessage,
@@ -153,14 +138,14 @@ public class RDXRapidHeightMapExtractorCudaDemo
 //                                                              incomingCompressedImageBytePointer,
 //                                                              compressedBytesMat);
 
-                  PerceptionMessageTools.convertToHeightMapData(sensorSimulator.getDepthImage().getCpuImageMat(),
-                                                                heightMapData,
-                                                                croppedHeightMapImageMessage.getPosition(),
-                                                                (float) RapidHeightMapExtractor.getHeightMapParameters()
-                                                                                               .getGlobalWidthInMeters(),
-                                                                (float) RapidHeightMapExtractor.getHeightMapParameters()
-                                                                                               .getGlobalCellSizeInMeters());
-                  HeightMapMessageTools.toMessage(heightMapData, heightMapMessage);
+                    PerceptionMessageTools.convertToHeightMapData(sensorSimulator.getDepthImage().getCpuImageMat(),
+                            heightMapData,
+                            croppedHeightMapImageMessage.getPosition(),
+                            (float) RapidHeightMapExtractor.getHeightMapParameters()
+                                    .getGlobalWidthInMeters(),
+                            (float) RapidHeightMapExtractor.getHeightMapParameters()
+                                    .getGlobalCellSizeInMeters());
+                    HeightMapMessageTools.toMessage(heightMapData, heightMapMessage);
 
 
 //               List<RDXMultiColorMeshBuilder> multiColorMeshBuilders = RDXHeightMapGraphicNew.generateHeightCells(heightsProvided,
@@ -173,27 +158,25 @@ public class RDXRapidHeightMapExtractorCudaDemo
 //                                                                                                                  groundHeight);
 //               heightMapGraphic.setLatestMeshBuilder(multiColorMeshBuilders);
 
-               heightMapGraphic.generateMeshesAsync(heightMapMessage);
-               heightMapGraphic.update();
+                    heightMapGraphic.generateMeshesAsync(heightMapMessage);
+                    heightMapGraphic.update();
+                }
+
+                baseUI.renderBeforeOnScreenUI();
+                baseUI.renderEnd();
             }
 
-            baseUI.renderBeforeOnScreenUI();
-            baseUI.renderEnd();
-         }
+            @Override
+            public void dispose() {
+                heightMapGraphic.destroy();
+                extractor.destroy();
+                environmentBuilder.destroy();
+                baseUI.dispose();
+            }
+        });
+    }
 
-         @Override
-         public void dispose()
-         {
-            heightMapGraphic.destroy();
-            extractor.destroy();
-            environmentBuilder.destroy();
-            baseUI.dispose();
-         }
-      });
-   }
-
-   public static void main(String[] args)
-   {
-      new RDXRapidHeightMapExtractorCudaDemo();
-   }
+    public static void main(String[] args) {
+        new RDXRapidHeightMapExtractorCudaDemo();
+    }
 }
