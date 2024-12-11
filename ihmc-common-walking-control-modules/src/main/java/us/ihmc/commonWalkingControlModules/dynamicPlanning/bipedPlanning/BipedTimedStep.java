2d0dbe951be151edcdd653739330be538cd780a5
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-12-11 08:47:03 -0600

    added a message for sending biped timed contact lists directly

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/bipedPlanning/BipedTimedStep.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/dynamicPlanning/bipedPlanning/BipedTimedStep.java
@@ -2,6 +2,7 @@ package us.ihmc.commonWalkingControlModules.dynamicPlanning.bipedPlanning;
 
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.humanoidRobotics.communication.controllerAPI.command.BipedTimedStepCommand;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.time.TimeInterval;
 import us.ihmc.robotics.time.TimeIntervalBasics;
@@ -30,6 +31,14 @@ public class BipedTimedStep extends BipedStep implements TimeIntervalProvider
    {
       super(robotSide, goalPose, groundClearance, predictedContactPoints);
       setTimeInterval(timeInterval);
+   }
+
+   public void set(BipedTimedStepCommand command)
+   {
+      setTimeInterval(command.getTimeInterval());
+      setGoalPose(command.getGoalPose());
+      setRobotSide(command.getRobotSide());
+      setSwingHeight(command.getSwingHeight());
    }
 
    @Override
