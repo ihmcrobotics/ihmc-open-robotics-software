cde543f2a64ec8b9bb9014f85ab5ced8ba15ac1a
bpark <bpark@ihmc.org>
2024-12-12 14:55:41 -0600

    Deleting unused dataHolder in HumanoidRobotContextDataFactory.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/barrierScheduler/context/HumanoidRobotContextDataFactory.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/barrierScheduler/context/HumanoidRobotContextDataFactory.java
@@ -1,7 +1,5 @@
 package us.ihmc.commonWalkingControlModules.barrierScheduler.context;
 
-import us.ihmc.commonWalkingControlModules.controllerCore.command.ControllerCoreCommandDataHolder;
-import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.ControllerCoreOutputDataHolder;
 import us.ihmc.commonWalkingControlModules.controllerCore.command.lowLevel.LowLevelOneDoFJointDesiredDataHolder;
 import us.ihmc.humanoidRobotics.model.CenterOfPressureDataHolder;
 import us.ihmc.robotics.sensors.CenterOfMassDataHolder;
@@ -23,11 +21,6 @@ public class HumanoidRobotContextDataFactory
    protected final RequiredFactoryField<RobotMotionStatusHolder> robotMotionStatusHolder = new RequiredFactoryField<>("robotMotionStatusHolder");
    protected final RequiredFactoryField<LowLevelOneDoFJointDesiredDataHolder> jointDesiredOutputList = new RequiredFactoryField<>("jointDesiredOutputList");
    protected final RequiredFactoryField<SensorDataContext> sensorDataContext = new RequiredFactoryField<>("sensorDataContext");
-   protected final RequiredFactoryField<LowLevelOneDoFJointDesiredDataHolder> wbccJointDesiredOutputList = new RequiredFactoryField<>(
-         "wholeBodyControllerCoreJointDesiredOutputList");
-   protected final RequiredFactoryField<ControllerCoreOutputDataHolder> controllerCoreOutputDataHolder = new RequiredFactoryField<>("controllerCoreDataHolder");
-   protected final RequiredFactoryField<ControllerCoreCommandDataHolder> controllerCoreCommandDataHolder = new RequiredFactoryField<>(
-         "controllerCoreCommandDataHolder");
 
    public HumanoidRobotContextData createHumanoidRobotContextData()
    {
@@ -75,20 +68,5 @@ public class HumanoidRobotContextDataFactory
    public void setSensorDataContext(SensorDataContext value)
    {
       sensorDataContext.set(value);
-   }
-
-   public void setWBCCJointDesiredOutputList(LowLevelOneDoFJointDesiredDataHolder value)
-   {
-      wbccJointDesiredOutputList.set(value);
-   }
-
-   public void setControllerCoreOutputDataHolder(ControllerCoreOutputDataHolder value)
-   {
-      controllerCoreOutputDataHolder.set(value);
-   }
-
-   public void setControllerCoreCommandDataHolder(ControllerCoreCommandDataHolder value)
-   {
-      controllerCoreCommandDataHolder.set(value);
    }
 }
