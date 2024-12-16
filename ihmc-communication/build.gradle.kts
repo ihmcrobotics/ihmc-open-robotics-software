7e40fea10088df238bf797f7328d6cccf3480d21
Dexton Anderson <danderson@ihmc.org>
2024-12-16 15:43:13 -0600

    :arrow_up: ihmc-realtime 1.7.0

--- ihmc-communication/build.gradle.kts
+++ ihmc-communication/build.gradle.kts
@@ -11,7 +11,7 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:ihmc-realtime:1.6.0")
+   api("us.ihmc:ihmc-realtime:1.7.0")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:ros2-library:1.1.3")
    api("commons-net:commons-net:3.6")
