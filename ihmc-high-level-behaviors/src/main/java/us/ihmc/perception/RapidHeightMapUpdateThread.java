8c5981b2f61f61c3fe55034a3e2d9fe4293535bd
ihmc-rosie <rosie@ihmc.us>
2024-12-23 14:22:54 -0600

    Pass in ROS2SyncedRobotModel to have direct access to the reference frames (#562)
    Co-authored-by: ihmc-rosie <rosie@ihmc.org>

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/RapidHeightMapUpdateThread.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/RapidHeightMapUpdateThread.java
@@ -1,19 +1,18 @@
 package us.ihmc.perception;
 
-import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
 import us.ihmc.commons.thread.RepeatingTaskThread;
 import us.ihmc.communication.ros2.ROS2PublishSubscribeAPI;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.perception.heightMap.TerrainMapData;
 import us.ihmc.perception.opencl.OpenCLManager;
-import us.ihmc.robotics.referenceFrames.MutableReferenceFrame;
 import us.ihmc.sensorProcessing.heightMap.HeightMapData;
 import us.ihmc.sensors.ImageSensor;
 
 public class RapidHeightMapUpdateThread extends RepeatingTaskThread
 {
    private final ROS2PublishSubscribeAPI ros2;
-   private final DRCRobotModel robotModel;
+   private final ROS2SyncedRobotModel syncedRobotModel;
    private final ReferenceFrame leftFootFrame;
    private final ReferenceFrame rightFootFrame;
    private final OpenCLManager openCLManager;
@@ -22,12 +21,12 @@ public class RapidHeightMapUpdateThread extends RepeatingTaskThread
    private final Object heightMapLock = new Object();
 
    private final ImageSensor imageSensor;
-   private final MutableReferenceFrame sensorFrame;
-   private final MutableReferenceFrame zUpSensorFrame;
+   private final ReferenceFrame sensorFrame;
+   private final ReferenceFrame zUpSensorFrame;
    private final int depthImageKey;
 
    public RapidHeightMapUpdateThread(ROS2PublishSubscribeAPI ros2,
-                                     DRCRobotModel robotModel,
+                                     ROS2SyncedRobotModel syncedRobotModel,
                                      ReferenceFrame leftFootFrame,
                                      ReferenceFrame rightFootFrame,
                                      OpenCLManager openCLManager,
@@ -37,15 +36,15 @@ public class RapidHeightMapUpdateThread extends RepeatingTaskThread
       super(imageSensor.getSensorName() + RapidHeightMapUpdateThread.class.getSimpleName());
 
       this.ros2 = ros2;
-      this.robotModel = robotModel;
+      this.syncedRobotModel = syncedRobotModel;
       this.leftFootFrame = leftFootFrame;
       this.rightFootFrame = rightFootFrame;
       this.openCLManager = openCLManager;
       this.imageSensor = imageSensor;
       this.depthImageKey = depthImageKey;
 
-      sensorFrame = new MutableReferenceFrame();
-      zUpSensorFrame = new MutableReferenceFrame(sensorFrame.getReferenceFrame());
+      sensorFrame = syncedRobotModel.getReferenceFrames().getSteppingCameraFrame();
+      zUpSensorFrame = syncedRobotModel.getReferenceFrames().getSteppingCameraZUpFrame();
    }
 
    @Override
@@ -58,28 +57,23 @@ public class RapidHeightMapUpdateThread extends RepeatingTaskThread
 
          // Initialize
          if (heightMapManager == null)
-            heightMapManager = new RapidHeightMapManager(openCLManager, robotModel, leftFootFrame, rightFootFrame, depthImage.getIntrinsicsCopy(), ros2);
-
-         // Update sensor frames
-         sensorFrame.update(transformToWorld -> transformToWorld.set(depthImage.getPose()));
-         zUpSensorFrame.update(transformToWorld ->
-         {
-            transformToWorld.setRotationToZero();
-            transformToWorld.appendYawRotation(sensorFrame.getTransformToParent().getRotation().getYaw());
-         });
-
+            heightMapManager = new RapidHeightMapManager(openCLManager,
+                                                         syncedRobotModel.getRobotModel(),
+                                                         leftFootFrame,
+                                                         rightFootFrame,
+                                                         depthImage.getIntrinsicsCopy(),
+                                                         ros2);
          // Update height map
          synchronized (heightMapLock)
          {
-            heightMapManager.update(depthImage.getCpuImageMat(),
-                                    depthImage.getAcquisitionTime(),
-                                    sensorFrame.getReferenceFrame(),
-                                    zUpSensorFrame.getReferenceFrame(),
-                                    ros2);
+            heightMapManager.update(depthImage.getCpuImageMat(), depthImage.getAcquisitionTime(), sensorFrame, zUpSensorFrame, ros2);
          }
 
          depthImage.release();
-      } catch (InterruptedException ignored) {}
+      }
+      catch (InterruptedException ignored)
+      {
+      }
    }
 
    public HeightMapData getLatestHeightMapData()
