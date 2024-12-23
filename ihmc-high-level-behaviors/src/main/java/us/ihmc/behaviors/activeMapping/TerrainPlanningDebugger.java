a6b37dc6bc2ab14f7f895b37dc177df872c1e7b3
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2024-12-23 14:53:19 -0600

    When the user places a goal the robot attempts to square up to it (#561)
    * Initial square up when placing a goal. Added a SquareUpTransitionCondition and a JustWaitState
    
    * Tune parameter to increase the distance margin before considering the next goal if one exists

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/activeMapping/TerrainPlanningDebugger.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/activeMapping/TerrainPlanningDebugger.java
@@ -7,7 +7,6 @@ import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.global.opencv_imgproc;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Size;
-import us.ihmc.behaviors.activeMapping.ContinuousPlannerSchedulingTask.PlanningMode;
 import us.ihmc.communication.packets.MessageTools;
 import us.ihmc.euclid.geometry.Pose3D;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
@@ -64,7 +63,6 @@ public class TerrainPlanningDebugger
    private ROS2Publisher<PoseListMessage> monteCarloNodesPublisherForUI;
    private MonteCarloFootstepPlannerRequest request;
    private MonteCarloFootstepPlannerParameters parameters;
-   private PlanningMode planningMode;
 
    private Mat contactHeatMapImage;
 
