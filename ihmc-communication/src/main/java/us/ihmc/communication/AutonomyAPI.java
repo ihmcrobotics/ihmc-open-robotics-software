8f5e1625337b2a946bea88cf23b67729ff89bfdf
Duncan Calvert <dcalvert@ihmc.us>
2025-01-03 13:59:57 -0600

    Add note to switch to RELIABLE.

--- ihmc-communication/src/main/java/us/ihmc/communication/AutonomyAPI.java
+++ ihmc-communication/src/main/java/us/ihmc/communication/AutonomyAPI.java
@@ -4,13 +4,15 @@ import behavior_msgs.msg.dds.AI2RCommandMessage;
 import behavior_msgs.msg.dds.AI2RStatusMessage;
 import behavior_msgs.msg.dds.BehaviorTreeStateMessage;
 import us.ihmc.communication.ros2.ROS2IOTopicPair;
+import us.ihmc.ros2.ROS2QosProfile;
 import us.ihmc.ros2.ROS2Topic;
 
 public final class AutonomyAPI
 {
    public static final String BEAVIOR_TREE_MODULE_NAME = "behavior_tree";
 
-   public static final ROS2Topic<?> BEAVIOR_TREE_MODULE = ROS2Tools.IHMC_ROOT.withModule(BEAVIOR_TREE_MODULE_NAME);
+   public static final ROS2Topic<?> BEAVIOR_TREE_MODULE = ROS2Tools.IHMC_ROOT.withModule(BEAVIOR_TREE_MODULE_NAME)
+                                                                             .withQoS(ROS2QosProfile.BEST_EFFORT()); // TODO: Switch to RELIABLE
    public static final ROS2IOTopicPair<BehaviorTreeStateMessage> BEAVIOR_TREE
          = new ROS2IOTopicPair<>(BEAVIOR_TREE_MODULE.withTypeName(BehaviorTreeStateMessage.class));
 
