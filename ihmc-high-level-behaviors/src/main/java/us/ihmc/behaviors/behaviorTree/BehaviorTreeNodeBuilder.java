7689687d3cca404e2438ae4602dd3fd5d6cdfe95
Duncan Calvert <dcalvert@ihmc.us>
2025-01-09 16:42:07 -0600

    Rename BehaviorTreeNodeBuilder.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/BehaviorTreeNodeBuilder.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/BehaviorTreeNodeBuilder.java
@@ -4,9 +4,9 @@ import us.ihmc.communication.crdt.CRDTInfo;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
 
 /**
- * @param <HLT> The generic type of this node high layer: RDX or Executor
+ * @param <HLT> The generic type of this node: RDX or Executor
  */
-public interface BehaviorTreeNodeHighLayerBuilder<HLT extends BehaviorTreeNodeHighLayer<HLT, ? ,?>>
+public interface BehaviorTreeNodeHighLayerBuilder<HLT extends BehaviorTreeNode<HLT, ? ,?>>
 {
    HLT createNode(Class<?> nodeType, long id, CRDTInfo crdtInfo, WorkspaceResourceDirectory saveFileDirectory);
 }
