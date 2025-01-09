7b5caac6a54dcf1ff1c8023b7998ba43cdd4e522
nkitchel <nkitchel@ihmc.org>
2025-01-09 15:58:18 -0600

    Add exception thrown around the DepthColorizer classes

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXDepthStreamingDemo.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/RDXDepthStreamingDemo.java
@@ -43,7 +43,7 @@ public class RDXDepthStreamingDemo
    private final RDXOpenCVVideoVisualizer receivedDepthVisualizer = new RDXOpenCVVideoVisualizer("Received Colorized Depth", "Received Colorized Depth", false);
    private final RDXRawImagePointCloudVisualizer pointCloudVisualizer = new RDXRawImagePointCloudVisualizer("De-Colorized Point Cloud");
 
-   public RDXDepthStreamingDemo()
+   public RDXDepthStreamingDemo() throws Exception
    {
       zed.useTrackedPose(true);
       zed.run(true);
