d086292743452f39778ebbfe2ab5b1b54adffb46
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2025-02-03 19:22:25 -0600

    Fix the height map parameters for CUDA (#635)

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/RapidHeightMapManager.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/RapidHeightMapManager.java
@@ -127,6 +127,16 @@ public class RapidHeightMapManager
 
       Mat croppedHeightMapImage = rapidHeightMapExtractor.getTerrainMapData().getHeightMap();
 
+      float heightScaleFactor;
+      if (runWithCUDA)
+      {
+         heightScaleFactor = (float) RapidHeightMapExtractorCUDA.getHeightMapParameters().getHeightScaleFactor();
+      }
+      else
+      {
+         heightScaleFactor = (float) RapidHeightMapExtractor.getHeightMapParameters().getHeightScaleFactor();
+      }
+
       OpenCVTools.compressImagePNG(croppedHeightMapImage, compressedCroppedHeightMapPointer);
       PerceptionMessageTools.publishCompressedDepthImage(compressedCroppedHeightMapPointer,
                                                          PerceptionAPI.HEIGHT_MAP_CROPPED,
@@ -137,12 +147,19 @@ public class RapidHeightMapManager
                                                          rapidHeightMapExtractor.getSequenceNumber(),
                                                          croppedHeightMapImage.rows(),
                                                          croppedHeightMapImage.cols(),
-                                                         (float) RapidHeightMapExtractor.getHeightMapParameters().getHeightScaleFactor());
+                                                         heightScaleFactor);
    }
 
    public HeightMapData getLatestHeightMapData()
    {
-      return RapidHeightMapExtractorCUDA.packHeightMapData(rapidHeightMapExtractor);
+      if (runWithCUDA)
+      {
+         return RapidHeightMapExtractorCUDA.packHeightMapData(rapidHeightMapExtractor);
+      }
+      else
+      {
+         return RapidHeightMapExtractor.packHeightMapData(rapidHeightMapExtractor);
+      }
    }
 
    public TerrainMapData getTerrainMapData()
