aa0eb422b64b2698ab7837382873443eb20a94c1
Duncan Calvert <dcalvert@ihmc.org>
2025-02-06 22:30:31 -0600

    Adding count field.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/logic/ConditionNodeState.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/logic/ConditionNodeState.java
@@ -3,19 +3,33 @@ package us.ihmc.behaviors.logic;
 import behavior_msgs.msg.dds.ConditionNodeStateMessage;
 import us.ihmc.behaviors.behaviorTree.BehaviorTreeNodeState;
 import us.ihmc.communication.crdt.CRDTInfo;
+import us.ihmc.communication.crdt.CRDTStatusLong;
+import us.ihmc.communication.ros2.ROS2ActorDesignation;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
 
 public class ConditionNodeState extends BehaviorTreeNodeState<ConditionNodeDefinition>
 {
+   private final CRDTStatusLong count;
+
    public ConditionNodeState(long id, CRDTInfo crdtInfo, WorkspaceResourceDirectory saveFileDirectory)
    {
       super(id, new ConditionNodeDefinition(crdtInfo, saveFileDirectory), crdtInfo);
+
+      count = new CRDTStatusLong(ROS2ActorDesignation.ROBOT, crdtInfo, 0);
    }
 
    @Override
    public void update()
    {
       super.update();
+   }
+
+   @Override
+   public boolean hasStatus()
+   {
+      boolean hasStatus = false;
+      hasStatus |= count.pollHasStatus();
+      return hasStatus;
    }
 
    public void toMessage(ConditionNodeStateMessage message)
