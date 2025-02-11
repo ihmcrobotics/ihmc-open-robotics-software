1da4272bf8ae5dfe6b02ae8a38d044e096250539
Duncan Calvert <dcalvert@ihmc.us>
2025-02-11 17:21:51 -0600

    Add ROS 2 API for enable HPU.

--- ihmc-communication/src/main/java/us/ihmc/communication/controllerAPI/ControllerAPI.java
+++ ihmc-communication/src/main/java/us/ihmc/communication/controllerAPI/ControllerAPI.java
@@ -103,6 +103,9 @@ public final class ControllerAPI
       inputMessageClasses.add(WholeBodyTrajectoryMessage.class);
       inputMessageClasses.add(WholeBodyStreamingMessage.class);
 
+      // Robot startup messages
+      inputMessageClasses.add(EnableHPUCommandMessage.class);
+
       // Statuses supported by bipedal walking controller {@link WalkingControllerState}
       outputMessageClasses.add(CapturabilityBasedStatus.class);
       outputMessageClasses.add(FootstepStatusMessage.class);
@@ -129,6 +132,9 @@ public final class ControllerAPI
       // Statuses supported by multi-contact controller, not in this repo
       outputMessageClasses.add(MultiContactBalanceStatus.class);
       outputMessageClasses.add(MultiContactTrajectoryStatus.class);
+
+      // Robot hardware status messages
+      outputMessageClasses.add(EnableHPUStatusMessage.class);
 
       // Setting the input messages with specific QoS
       inputMessageClassSpecificQoS.put(WholeBodyStreamingMessage.class, ROS2QosProfile.BEST_EFFORT());
