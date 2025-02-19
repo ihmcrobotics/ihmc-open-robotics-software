045ce952bccb02932c7563149816930e9359ab6b
Tomasz Bialek <103042423+TomaszTB@users.noreply.github.com>
2025-02-19 09:18:29 -0600

    [Feature] Version Control YOLO Models (#671)
    * Added existing YOLO models to version control
    
    * Use YOLO models from resoures
    
    * Removed broken YOLO model

--- ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXYOLOv8PipelineDemo.java
+++ ihmc-high-level-behaviors/src/test/java/us/ihmc/rdx/perception/RDXYOLOv8PipelineDemo.java
@@ -104,7 +104,7 @@ public class RDXYOLOv8PipelineDemo
       {
          YOLOv8Model model = new YOLOv8Model(yoloModelDirectory);
 
-         LogTools.info("Loaded YOLOv8 model: " + YOLOv8Tools.getONNXFile(yoloModelDirectory));
+         LogTools.info("Loaded YOLOv8 model: " + model.getName());
          LogTools.info("\t\t\tClasses: " + model.getDetectableObjectCount());
 
          yoloModels.add(model);
