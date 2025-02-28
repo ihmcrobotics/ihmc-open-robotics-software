9a491fedb13e06f0ad1f41e7220783b2003ea4c5
Duncan Calvert <dcalvert@ihmc.us>
2025-02-28 13:33:52 -0600

    Remove vendores kherud llamacpp

--- ihmc-high-level-behaviors/src/main/java/de/kherud/llama/args/PoolingType.java
+++
@@ -1,8 +0,0 @@
-package de.kherud.llama.args;
-
-public enum PoolingType {
-
-	UNSPECIFIED,
-	MEAN,
-	CLS
-}
--- ihmc-high-level-behaviors/src/main/java/de/kherud/llama/args/RopeScalingType.java
+++
@@ -1,8 +0,0 @@
-package de.kherud.llama.args;
-
-public enum RopeScalingType {
-
-	UNSPECIFIED,
-	LINEAR,
-	YARN
-}
