f56a3d447bc1848483b1bdb665e66a5c11a5d0f8
Duncan Calvert <dcalvert@ihmc.us>
2025-02-28 14:54:57 -0600

    Reimplement Llama but having some issues still.

--- ihmc-high-level-behaviors/build.gradle.kts
+++ ihmc-high-level-behaviors/build.gradle.kts
@@ -18,7 +18,7 @@ mainDependencies {
       exclude(group = "org.lwjgl.lwjgl") // exclude lwjgl 2
    }
    api("us.ihmc:promp-java:1.0.1")
-//   api("de.kherud:llama:3.4.1:cuda12-linux-x86-64")
+   api("us.ihmc:llamacpp-javacpp:b4743")
 }
 
 libgdxDependencies {
