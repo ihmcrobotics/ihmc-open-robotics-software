42672613e43875a944c0d6fdb721d564d81f8dd7
ihmc-rosie <rosie@ihmc.org>
2025-02-27 23:12:50 -0600

    added a default block environment

--- simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/planarRegionEnvironments/BlockEnvironment.java
+++ simulation-construction-set-tools/src/main/java/us/ihmc/simulationConstructionSetTools/util/environments/planarRegionEnvironments/BlockEnvironment.java
@@ -4,6 +4,18 @@ import us.ihmc.graphicsDescription.appearance.YoAppearance;
 
 public class BlockEnvironment extends PlanarRegionEnvironmentInterface
 {
+   public BlockEnvironment()
+   {
+      // first ground plane
+      generator.identity();
+      generator.addRectangle(10.0, 10.0);
+
+      generator.translate(0.15, 0.0, 0.0);
+      generator.addCubeReferencedAtBottomMiddle(0.3, 0.3, 0.09);
+
+      addPlanarRegionsToTerrain(YoAppearance.Grey());
+   }
+
    public BlockEnvironment(double blockWidth, double blockLength, double blockHeight)
    {
       // first ground plane
