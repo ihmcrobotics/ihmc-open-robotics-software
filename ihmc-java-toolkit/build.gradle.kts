bf601b03ceecc316844d7596b49535a095bb02f4
Dexton Anderson <danderson@ihmc.org>
2025-03-05 11:34:49 -0600

    :arrow_up: ihmc-native-library-loader 2.0.4

--- ihmc-java-toolkit/build.gradle.kts
+++ ihmc-java-toolkit/build.gradle.kts
@@ -26,7 +26,7 @@ mainDependencies {
    api("us.ihmc:euclid-shape:0.22.3")
    api("us.ihmc:euclid-geometry:0.22.3")
    api("us.ihmc:ihmc-graphics-description:0.26.2")
-   api("us.ihmc:ihmc-native-library-loader:2.0.3")
+   api("us.ihmc:ihmc-native-library-loader:2.0.4")
 }
 
 testDependencies {
