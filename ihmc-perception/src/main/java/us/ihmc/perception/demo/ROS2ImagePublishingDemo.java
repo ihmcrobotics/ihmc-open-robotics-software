33149c10dfc2a3ac8518f63051cdf1b8f9a93049
Tomasz Bialek <tbialek@ihmc.org>
2025-03-10 15:26:41 -0500

    Added method to publish CameraInfo

--- ihmc-perception/src/main/java/us/ihmc/perception/demo/ROS2ImagePublishingDemo.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/demo/ROS2ImagePublishingDemo.java
@@ -1,5 +1,6 @@
 package us.ihmc.perception.demo;
 
+import sensor_msgs.msg.dds.CameraInfo;
 import sensor_msgs.msg.dds.Image;
 import us.ihmc.commons.thread.RepeatingTaskThread;
 import us.ihmc.communication.ros2.ROS2Helper;
@@ -18,16 +19,18 @@ import us.ihmc.tools.IHMCCommonPaths;
 import static us.ihmc.zed.global.zed.SL_DEPTH_MODE_PERFORMANCE;
 
 /**
- * Demo for publishing standard ROS 2 {@link Image} messages.
+ * Demo for publishing standard ROS 2 {@link Image} messages along with {@link CameraInfo} messages.
  * This demo does NOT publish IHMC's custom {@link perception_msgs.msg.dds.ImageMessage}.
  * <p>
- * The demo publishes color and depth images on the following topics:
+ * The demo publishes messages on the following topics:
  * <ul>
- *    <li>/zed/color</li>
- *    <li>/zed/depth</li>
+ *    <li>/zed/color/image_raw</li>
+ *    <li>/zed/color/camera_info</li>
+ *    <li>/zed/depth/image_raw</li>
+ *    <li>/zed/depth/camera_info</li>
  * </ul>
  * <p>
- * To visualize these images, you may use RViz2
+ * To visualize these images, you may use RViz2 from ROS 2
  * (<a href="https://docs.ros.org/en/humble/Installation.html">ROS 2 Humble Installation</a>).
  * <p>
  * With this demo running, launch RViz2. On Linux, open a new terminal and run:
@@ -40,7 +43,19 @@ import static us.ihmc.zed.global.zed.SL_DEPTH_MODE_PERFORMANCE;
  * </pre>
  * In RViz2, add two Image display by clicking the Add button,
  * and in the By topic options selecting the Image displays under
- * the /zed/color topic, and again under the /zed/depth topic.
+ * the /zed/color/image_raw topic, and again under the /zed/depth/image_raw topic.
+ * <p>
+ * To show a point cloud, set the Fixed Frame (under Global Options) to "world".
+ * Then, click Add, select DepthCloud, and click Ok.
+ * Under the DepthCloud options, set the options as such:
+ * <ul>
+ *    <li>Reliability Policy = Reliable</li>
+ *    <li>Depth Map Topic = /zed/depth/image_raw</li>
+ *    <li>Color Image Topic = /zed/color/image_raw</li>
+ * </ul>
+ * <p>
+ * You may notice the orientation of the point cloud is incorrect
+ * as this demo does not publish a tf tree.
  */
 class ROS2ImagePublishingDemo
 {
@@ -48,9 +63,15 @@ class ROS2ImagePublishingDemo
                                                                                    .toAbsolutePath()
                                                                                    .toString();
 
+   private static final int CAMERA_INFO_PUBLISH_SKIP = 5; // Publish camera_info messages every 5 frames
+
    private final ROS2Node ros2Node;
+
    private final ROS2Topic<Image> colorTopic;
+   private final ROS2Topic<CameraInfo> colorCameraInfoTopic;
+
    private final ROS2Topic<Image> depthTopic;
+   private final ROS2Topic<CameraInfo> depthCameraInfoTopic;
 
    private final RawImagePublisher rawImagePublisher;
    private final RepeatingTaskThread publishThread;
@@ -62,11 +83,16 @@ class ROS2ImagePublishingDemo
       // Create a ROS 2 Node
       ros2Node = new ROS2NodeBuilder().build("image_publishing_demo");
 
-      // Create the /zed/color topic. QoS must be reliable for RViz2 to receive the images.
-      colorTopic = new ROS2Topic<>().withModule("zed").withSuffix("color").withQoS(ROS2QosProfile.RELIABLE()).withType(Image.class);
+      // ZED topic used to create color and depth topics. QoS must be reliable for RViz2 to receive the images.
+      ROS2Topic<?> zedTopic = new ROS2Topic<>().withPrefix("zed").withQoS(ROS2QosProfile.RELIABLE());
 
-      // Create the /zed/depth topic. Again, QoS must be reliable.
-      depthTopic = new ROS2Topic<>().withModule("zed").withSuffix("depth").withQoS(ROS2QosProfile.RELIABLE()).withType(Image.class);
+      // Create the /zed/color/image_raw and /zed/color/camera_info topics
+      colorTopic = zedTopic.withModule("color").withSuffix("image_raw").withType(Image.class);
+      colorCameraInfoTopic = zedTopic.withModule("color").withSuffix("camera_info").withType(CameraInfo.class);
+
+      // Create the /zed/depth/image_raw and /zed/depth/camera_info topics
+      depthTopic = zedTopic.withModule("depth").withSuffix("image_raw").withType(Image.class);
+      depthCameraInfoTopic = zedTopic.withModule("depth").withSuffix("camera_info").withType(CameraInfo.class);
 
       // Create a publisher, and the publisher thread
       rawImagePublisher = new RawImagePublisher(ros2Node);
@@ -102,6 +128,14 @@ class ROS2ImagePublishingDemo
          LogTools.info("Publishing images {}", color.getSequenceNumber());
          rawImagePublisher.publishImage(colorTopic, color);
          rawImagePublisher.publishImage(depthTopic, depth);
+
+         // Publish the camera info
+         if (color.getSequenceNumber() % CAMERA_INFO_PUBLISH_SKIP == 0)
+         {
+            LogTools.info("Publishing camera info");
+            rawImagePublisher.publishImage(colorCameraInfoTopic, color);
+            rawImagePublisher.publishImage(depthCameraInfoTopic, depth);
+         }
 
          // Release the images
          color.release();
