4abbb59d1fe0a79de3ba4dc4561927f23df6a4d4
Duncan Calvert <dcalvert@ihmc.org>
2025-03-28 14:14:43 -0500

    Create lerobot dataset class.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/logging/RDXLeRobotDatasetCreator.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/logging/RDXLeRobotDatasetCreator.java
@@ -1,8 +1,27 @@
 package us.ihmc.rdx.logging;
 
+import us.ihmc.rdx.imgui.RDXPanel;
+import us.ihmc.rdx.simulation.scs2.RDXSCS2LogSession;
+
 public class RDXLeRobotDatasetCreator
 {
-   public RDXLeRobotDatasetCreator()
+   private final RDXSCS2LogSession logSession;
+   private final RDXPanel panel;
+
+   public RDXLeRobotDatasetCreator(RDXSCS2LogSession logSession)
    {
+      this.logSession = logSession;
+
+      panel = new RDXPanel("LeRobot Dataset Creator", this::renderImGuiWidgets);
+   }
+
+   private void renderImGuiWidgets()
+   {
+
+   }
+
+   public RDXPanel getPanel()
+   {
+      return panel;
    }
 }
