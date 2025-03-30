200c63e7b58a3d8852ccddc9fbd03be14d2a8bb2
Duncan Calvert <dcalvert@ihmc.org>
2025-03-30 17:22:02 -0500

    Work on LeRobot dataset creator.

---
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/leRobot/LeRobotDatasetTools.java
@@ -0,0 +1,21 @@
+package us.ihmc.avatar.logProcessor.leRobot;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class LeRobotDatasetTools
+{
+   public static List<Path> findLeRobotDatasetSubdirectories(Path startDirectory)
+   {
+      try (var paths = Files.walk(startDirectory))
+      {
+         return paths.filter(Files::isDirectory).filter(directory -> Files.exists(directory.resolve("meta/info.json"))).toList();
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException("Error searching for dataset directories with meta/info.json", e);
+      }
+   }
+}
--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/logging/RDXLeRobotDatasetCreator.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/logging/RDXLeRobotDatasetCreator.java
@@ -2,12 +2,18 @@ package us.ihmc.rdx.logging;
 
 import imgui.ImGui;
 import imgui.type.ImString;
+import us.ihmc.avatar.logProcessor.leRobot.LeRobotDataset;
+import us.ihmc.avatar.logProcessor.leRobot.LeRobotDatasetTools;
+import us.ihmc.commons.exception.DefaultExceptionHandler;
+import us.ihmc.commons.nio.FileTools;
 import us.ihmc.rdx.imgui.ImGuiTools;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.imgui.RDXPanel;
 import us.ihmc.rdx.simulation.scs2.RDXSCS2LogSession;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
 
 public class RDXLeRobotDatasetCreator
 {
@@ -16,35 +22,61 @@ public class RDXLeRobotDatasetCreator
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
    private transient final ImString datasetName = new ImString(512);
    private transient final ImString imTaskName = new ImString(512);
-   private File logDirectory;
+   private List<Path> datasets;
+   private LeRobotDataset dataset;
 
    public RDXLeRobotDatasetCreator(RDXSCS2LogSession logSession)
    {
       this.logSession = logSession;
 
-      panel = new RDXPanel("LeRobot Dataset Creator", this::renderImGuiWidgets);
+      panel = new RDXPanel("LeRobot Dataset Creator", this::renderImGuiWidgets, false, true);
 
-
-
+      refresh();
    }
 
    public void update()
    {
-      if (logSession.getSession().hasSessionStarted())
-      {
-         File newlogDirectory = logSession.getSession().getLogDataReader().getLogDirectory();
 
-         if (newlogDirectory != logDirectory)
-         {
-            logDirectory = newlogDirectory;
-
-
-         }
-      }
    }
 
    private void renderImGuiWidgets()
    {
+      if (ImGui.beginMenuBar())
+      {
+         if (ImGui.beginMenu(labels.get("Dataset")))
+         {
+            if (ImGui.menuItem(labels.get("Refresh")))
+            {
+               refresh();
+            }
+            ImGui.separator();
+            ImGui.text("Datasets:");
+            for (Path datasetPath : datasets)
+            {
+               if (ImGui.radioButton(labels.get(datasetPath.getFileName().toString()), dataset.getDirectory().equals(datasetPath)))
+               {
+                  if (!dataset.getDirectory().equals(datasetPath))
+                     dataset = new LeRobotDataset(datasetPath);
+               }
+            }
+            ImGui.separator();
+
+            ImGui.text("Dataset name:");
+            ImGuiTools.inputText(labels.getHidden("datasetName"), datasetName);
+
+            if (ImGui.menuItem(labels.get("Create Dataset")))
+            {
+               File logDirectory = logSession.getSession().getLogDirectory();
+               dataset = new LeRobotDataset(logDirectory.toPath().resolve(datasetName.get().trim()));
+               dataset.mkdirs();
+               refresh();
+            }
+            ImGui.endMenu();
+         }
+
+         ImGui.endMenuBar();
+      }
+
       if (!logSession.getSession().hasSessionStarted())
       {
          ImGui.text("Session has not started yet.");
@@ -53,20 +85,25 @@ public class RDXLeRobotDatasetCreator
 
       ImGui.text("Log directory: %s".formatted(logSession.getSession().getLogDataReader().getLogDirectory().getName()));
 
-      ImGui.text("Dataset name:");
-      ImGui.sameLine();
-      ImGuiTools.inputText(labels.getHidden("datasetName"), datasetName);
 
-
-
-
-      ImGui.text("Task name:");
-      ImGuiTools.inputTextMultiline(labels.getHidden("taskName"), imTaskName);
-
-      if (ImGui.button(labels.get("Create Dataset")))
+      if (dataset != null)
       {
-         File logDirectory = logSession.getSession().getLogDataReader().getLogDirectory();
+         ImGui.text("Task name:");
+         ImGuiTools.inputTextMultiline(labels.getHidden("taskName"), imTaskName);
+
       }
+   }
+
+   private Path mkdir(Path path, String name)
+   {
+      Path newPath = path.resolve(name);
+      FileTools.ensureDirectoryExists(newPath, DefaultExceptionHandler.PRINT_MESSAGE);
+      return newPath;
+   }
+   
+   private void refresh()
+   {
+      datasets = LeRobotDatasetTools.findLeRobotDatasetSubdirectories(logSession.getSession().getLogDataReader().getLogDirectory().toPath());
    }
 
    public RDXPanel getPanel()
