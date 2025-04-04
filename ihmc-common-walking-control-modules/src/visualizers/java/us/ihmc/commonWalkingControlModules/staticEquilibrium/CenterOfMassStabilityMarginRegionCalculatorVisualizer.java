51bc4d4a09a646e742a9402b757f20f5d5f0c613
Stephen McCrory <smccrory1024@gmail.com>
2025-04-04 08:53:27 -0500

    updating viz for simple com support region class

--- ihmc-common-walking-control-modules/src/visualizers/java/us/ihmc/commonWalkingControlModules/staticEquilibrium/CenterOfMassStabilityMarginRegionCalculatorVisualizer.java
+++ ihmc-common-walking-control-modules/src/visualizers/java/us/ihmc/commonWalkingControlModules/staticEquilibrium/CenterOfMassStabilityMarginRegionCalculatorVisualizer.java
@@ -1,5 +1,6 @@
 package us.ihmc.commonWalkingControlModules.staticEquilibrium;
 
+import org.ejml.data.DMatrixRMaj;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.Axis3D;
 import us.ihmc.euclid.geometry.interfaces.ConvexPolygon2DReadOnly;
@@ -7,15 +8,24 @@ import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
 import us.ihmc.euclid.tuple4D.Quaternion;
+import us.ihmc.graphicsDescription.appearance.YoAppearance;
 import us.ihmc.graphicsDescription.conversion.YoGraphicConversionTools;
 import us.ihmc.graphicsDescription.yoGraphics.YoGraphicsListRegistry;
 import us.ihmc.log.LogTools;
 import us.ihmc.scs2.SimulationConstructionSet2;
 import us.ihmc.scs2.definition.visual.ColorDefinition;
+import us.ihmc.scs2.definition.visual.ColorDefinitions;
+import us.ihmc.scs2.definition.visual.PaintDefinition;
 import us.ihmc.scs2.definition.visual.VisualDefinitionFactory;
 import us.ihmc.scs2.definition.yoGraphic.YoGraphicDefinition;
+import us.ihmc.scs2.definition.yoGraphic.YoGraphicDefinitionFactory;
 import us.ihmc.scs2.simulation.physicsEngine.PhysicsEngineFactory;
+import us.ihmc.yoVariables.euclid.referenceFrame.YoFramePoint3D;
+import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
+import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoDouble;
 
 import java.util.List;
 
@@ -31,19 +41,57 @@ public class CenterOfMassStabilityMarginRegionCalculatorVisualizer
    private void runWithSCS2(WholeBodyContactStateInterface input)
    {
       SimulationConstructionSet2 scs2 = new SimulationConstructionSet2(PhysicsEngineFactory.newDoNothingPhysicsEngineFactory());
-      scs2.start(true, true, false);
 
       YoGraphicsListRegistry graphicsListRegistry = new YoGraphicsListRegistry();
       StabilityMarginRegionCalculator calculator = StabilityMarginRegionCalculator.createForCoMStabilityMargin("", 1.0, scs2.getRootRegistry(), graphicsListRegistry);
+      calculator.setupForStabilityMarginCalculation(FramePoint3D::new);
       calculator.updateContactState(input);
 
       List<YoGraphicDefinition> graphicDefinitions = YoGraphicConversionTools.toYoGraphicDefinitions(graphicsListRegistry);
       scs2.addYoGraphics(graphicDefinitions);
 
-      LogTools.info("----- Support Region -----");
+      YoFramePoint3D optimizedCoM = new YoFramePoint3D("optimizedCoM", ReferenceFrame.getWorldFrame(), scs2.getRootRegistry());
+      scs2.addYoGraphic(YoGraphicDefinitionFactory.newYoGraphicPoint3D("optimizedCoMViz", optimizedCoM, 0.03, ColorDefinitions.Red()));
 
-      calculator.performFullRegionUpdate();
-      scs2.simulateNow(1);
+      YoFramePoint3D[] contactPoints = new YoFramePoint3D[input.getNumberOfContactPoints()];
+      YoFrameVector3D[] contactForces = new YoFrameVector3D[input.getNumberOfContactPoints()];
+      for (int i = 0; i < input.getNumberOfContactPoints(); i++)
+      {
+         contactPoints[i] = new YoFramePoint3D("contactPoint" + i, ReferenceFrame.getWorldFrame(), scs2.getRootRegistry());
+         contactForces[i] = new YoFrameVector3D("contactForce" + i, ReferenceFrame.getWorldFrame(), scs2.getRootRegistry());
+         scs2.addYoGraphic(YoGraphicDefinitionFactory.newYoGraphicArrow3D("contactForceViz" + i, contactPoints[i], contactForces[i], 0.2, ColorDefinitions.Red()));
+         contactPoints[i].setFromReferenceFrame(input.getContactFrame(i));
+      }
+
+      YoDouble[] yoDualSolution = new YoDouble[50];
+      for (int i = 0; i < yoDualSolution.length; i++)
+      {
+         yoDualSolution[i] = new YoDouble("dual" + i, scs2.getRootRegistry());
+      }
+
+      LogTools.info("----- Support Region -----");
+      scs2.start(true, true, false);
+
+      for (int i = 0; i < 18; i++)
+      {
+         calculator.performUpdateForNextVertex(false);
+
+         Point2DReadOnly optimizedStabilityPoint = calculator.getOptimizationModule().getOptimizedStabilityPoint();
+         optimizedCoM.set(optimizedStabilityPoint);
+
+         for (int j = 0; j < input.getNumberOfContactPoints(); j++)
+         {
+            calculator.getOptimizationModule().getResolvedForce(j, contactForces[j]);
+         }
+
+         DMatrixRMaj solverDualSolution = calculator.getSolverDualSolution(i);
+         for (int j = 0; j < Math.min(solverDualSolution.getNumRows(), yoDualSolution.length); j++)
+         {
+            yoDualSolution[j].set(solverDualSolution.get(j));
+         }
+
+         scs2.simulateNow(1);
+      }
 
       ConvexPolygon2DReadOnly supportRegion0 = calculator.getFeasibleRegion();
       for (int i = 0; i < supportRegion0.getNumberOfVertices(); i++)
