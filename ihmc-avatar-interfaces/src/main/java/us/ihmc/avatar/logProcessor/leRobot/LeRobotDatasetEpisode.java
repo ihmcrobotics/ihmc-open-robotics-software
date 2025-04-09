66e05a0a093a5acabd0b9a23d4ef2b5ccb333d0e
Duncan Calvert <dcalvert@ihmc.org>
2025-04-08 21:18:17 -0500

    Fix slowness. Videos playing with VLC but not Firefox. Not sure.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/leRobot/LeRobotDatasetEpisode.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/leRobot/LeRobotDatasetEpisode.java
@@ -106,9 +106,9 @@ public class LeRobotDatasetEpisode
             recorder.setFormat("mp4");
 //            recorder.setVideoCodecName("av1_nvenc");
 //            recorder.setVideoOption("preset", "p7");
-            recorder.setVideoCodecName("libaom-av1");
-            recorder.setVideoOption("preset", "10");
-            recorder.setVideoOption("threads", String.valueOf(Runtime.getRuntime().availableProcessors()));
+//            recorder.setVideoCodecName("libaom-av1");
+//            recorder.setVideoOption("preset", "10");
+//            recorder.setVideoOption("threads", String.valueOf(Runtime.getRuntime().availableProcessors()));
             recorder.setPixelFormat(avutil.AV_PIX_FMT_YUV420P);
             recorder.setFrameRate(LeRobotDataset.ZED_FPS);
             recorder.setVideoBitrate(1352000);
