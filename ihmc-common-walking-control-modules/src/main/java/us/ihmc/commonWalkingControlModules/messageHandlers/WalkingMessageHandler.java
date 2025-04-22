a2e9eec8eeba85580e806341f95eab64cbd1a51c
ihmc-rosie <rosie@ihmc.org>
2025-04-22 17:20:53 -0500

    undo walking message handler

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/messageHandlers/WalkingMessageHandler.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/messageHandlers/WalkingMessageHandler.java
@@ -130,8 +130,6 @@ public class WalkingMessageHandler implements SCS2YoGraphicHolder
    private final YoBoolean usingQFP = new YoBoolean("usingQFP", registry);
    private final YoBoolean isWalkingInPlace = new YoBoolean("isWalkingInPlace", registry);
 
-   private final YoDouble yoTransferDuration = new YoDouble("TransferDuration", registry);
-
    public WalkingMessageHandler(double defaultTransferTime,
                                 double defaultSwingTime,
                                 double defaultInitialTransferTime,
@@ -957,13 +955,11 @@ public class WalkingMessageHandler implements SCS2YoGraphicHolder
       {
          // There are no upcoming steps, we are not walking, and this is an overwrite message:
          if (stepsInQueue == 0 && !isWalking.getBooleanValue())
-//            transferDuration = defaultInitialTransferTime.getDoubleValue();
-            transferDuration =  defaultTransferTime.getDoubleValue();
+            transferDuration = defaultInitialTransferTime.getDoubleValue();
          else
             transferDuration = defaultTransferTime.getDoubleValue();
       }
 
-      yoTransferDuration.set(transferDuration);
       timingToSet.setTimings(swingDuration, transferDuration);
       timingToSet.setTouchdownDuration(footstep.getTouchdownDuration());
       timingToSet.setLiftoffDuration(footstep.getLiftoffDuration());
