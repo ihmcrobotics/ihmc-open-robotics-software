8eca276a0643c6c8d49a812baca42cac19d9bc5b
lpenco <lpenco@ihmc.org>
2025-05-08 16:04:36 -0500

    tomasz clean up cuda file

--- ihmc-perception/src/main/java/us/ihmc/perception/gpuHeightMap/CUDAFootstepOptimizer.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/gpuHeightMap/CUDAFootstepOptimizer.java
@@ -6,6 +6,7 @@ import org.bytedeco.cuda.global.cudart;
 import org.bytedeco.javacpp.DoublePointer;
 import org.bytedeco.javacpp.FloatPointer;
 import org.bytedeco.javacpp.IntPointer;
+import org.bytedeco.javacpp.PointerScope;
 import us.ihmc.euclid.referenceFrame.FramePose3D;
 import us.ihmc.euclid.referenceFrame.interfaces.FramePose3DReadOnly;
 import us.ihmc.euclid.yawPitchRoll.YawPitchRoll;
@@ -82,8 +83,8 @@ public class CUDAFootstepOptimizer implements AutoCloseable
 
       planarityChecker = new PlanarityChecker(footLength, footWidth);
 
-      blockSize = new dim3(512, 1, 1);
-      gridSize = new dim3((searchSpaceDim + blockSize.x() - 1) / blockSize.x(), 1, 1);
+      blockSize = new dim3();
+      gridSize = new dim3();
    }
 
    /**
@@ -102,107 +103,121 @@ public class CUDAFootstepOptimizer implements AutoCloseable
       }
       HeightMapData currentHeightMapData = new HeightMapData(heightMapData);
 
-      FloatPointer cpuCosts = new FloatPointer(searchSpaceDim);
-      FloatPointer gpuCosts = new FloatPointer();
-      CUDATools.mallocAsync(gpuCosts, searchSpaceDim, cudaStream);
+      try (PointerScope ignored = new PointerScope())
+      {
+         FloatPointer gpuCosts = new FloatPointer();
+         CUDATools.mallocAsync(gpuCosts, searchSpaceDim, cudaStream);
 
-      FloatPointer cpuSolutions = new FloatPointer(3L * searchSpaceDim);
-      FloatPointer gpuSolutions = new FloatPointer();
-      CUDATools.mallocAsync(gpuSolutions, 3L * searchSpaceDim, cudaStream);
+         FloatPointer cpuSolutions = new FloatPointer(3L * searchSpaceDim);
+         FloatPointer gpuSolutions = new FloatPointer();
+         CUDATools.mallocAsync(gpuSolutions, 3L * searchSpaceDim, cudaStream);
 
-      FloatPointer cpuInitialPose = new FloatPointer((float) initialPose.getX(), (float) initialPose.getY(), (float) initialPose.getZ(), (float) initialPose.getYaw());
-      FloatPointer gpuInitialPose = new FloatPointer();
-      CUDATools.mallocAsync(gpuInitialPose, 4, cudaStream);
+         FloatPointer cpuInitialPose = new FloatPointer((float) initialPose.getX(),
+                                                        (float) initialPose.getY(),
+                                                        (float) initialPose.getZ(),
+                                                        (float) initialPose.getYaw());
+         FloatPointer gpuInitialPose = new FloatPointer();
+         CUDATools.mallocAsync(gpuInitialPose, 4, cudaStream);
 
-      DoublePointer cpuHeights = new DoublePointer(currentHeightMapData.getHeights());
-      DoublePointer gpuHeights = new DoublePointer();
-      CUDATools.mallocAsync(gpuHeights, currentHeightMapData.getHeights().length, cudaStream);
+         DoublePointer cpuHeights = new DoublePointer(currentHeightMapData.getHeights());
+         DoublePointer gpuHeights = new DoublePointer();
+         CUDATools.mallocAsync(gpuHeights, currentHeightMapData.getHeights().length, cudaStream);
 
-      FloatPointer cpuGridCenter = new FloatPointer((float) currentHeightMapData.getGridCenter().getX(), (float) currentHeightMapData.getGridCenter().getY());
-      FloatPointer gpuGridCenter = new FloatPointer();
-      CUDATools.mallocAsync(gpuGridCenter, 2, cudaStream);
+         FloatPointer cpuGridCenter = new FloatPointer((float) currentHeightMapData.getGridCenter().getX(),
+                                                       (float) currentHeightMapData.getGridCenter().getY());
+         FloatPointer gpuGridCenter = new FloatPointer();
+         CUDATools.mallocAsync(gpuGridCenter, 2, cudaStream);
 
-      // Copying data from CPU to GPU
-      CUDATools.memcpyAsync(gpuCosts, cpuCosts, searchSpaceDim, cudaStream);
-      CUDATools.memcpyAsync(gpuSolutions, cpuSolutions, 3L * searchSpaceDim, cudaStream);
-      CUDATools.memcpyAsync(gpuInitialPose, cpuInitialPose, 4, cudaStream);
-      CUDATools.memcpyAsync(gpuHeights, cpuHeights, currentHeightMapData.getHeights().length, cudaStream);
-      CUDATools.memcpyAsync(gpuGridCenter, cpuGridCenter, 2, cudaStream);
+         // Copying data from CPU to GPU
+         CUDATools.memcpyAsync(gpuInitialPose, cpuInitialPose, 4, cudaStream);
+         CUDATools.memcpyAsync(gpuHeights, cpuHeights, currentHeightMapData.getHeights().length, cudaStream);
+         CUDATools.memcpyAsync(gpuGridCenter, cpuGridCenter, 2, cudaStream);
 
-      // Runs the kernel with the desired grid and block sizes
-      computeKernel.withPointer(gpuHeights)
-                   .withPointer(gpuGridCenter)
-                   .withInt(currentHeightMapData.getCenterIndex())
-                   .withFloat((float) currentHeightMapData.getGridResolutionXY())
-                   .withPointer(gpuInitialPose)
-                   .withFloat(footLength)
-                   .withFloat(footWidth)
-                   .withFloat(searchRadius)
-                   .withFloat(searchYawLimit)
-                   .withInt(stepsXY)
-                   .withInt(stepsYaw)
-                   .withPointer(gpuCosts)
-                   .withPointer(gpuSolutions)
-                   .run(cudaStream, gridSize, blockSize, 0);
+         LogTools.warn("Running compute kernel");
+         // Runs the kernel with the desired grid and block sizes
+         blockSize.x(512);
+         gridSize.x((searchSpaceDim + blockSize.x() - 1) / blockSize.x());
+         computeKernel.withPointer(gpuHeights)
+                      .withPointer(gpuGridCenter)
+                      .withInt(currentHeightMapData.getCenterIndex())
+                      .withFloat((float) currentHeightMapData.getGridResolutionXY())
+                      .withPointer(gpuInitialPose)
+                      .withFloat(footLength)
+                      .withFloat(footWidth)
+                      .withFloat(searchRadius)
+                      .withInt(stepsXY)
+                      .withInt(stepsYaw)
+                      .withPointer(gpuCosts)
+                      .withPointer(gpuSolutions)
+                      .run(cudaStream, gridSize, blockSize, 0);
 
-      CUDATools.memcpyAsync(cpuSolutions, gpuSolutions, 3L * searchSpaceDim, cudaStream);
+         CUDATools.memcpyAsync(cpuSolutions, gpuSolutions, 3L * searchSpaceDim, cudaStream);
 
-      // Now run another kernel to retrieve the best solution among those in cpuSolutions
-      int resultSize = searchSpaceDim / blockSize.x();
-      FloatPointer gpuBestCosts = new FloatPointer();
-      IntPointer gpuBestIndices = new IntPointer();
-      CUDATools.mallocAsync(gpuBestCosts, resultSize, cudaStream);
-      CUDATools.mallocAsync(gpuBestIndices, resultSize, cudaStream);
+         // Now run another kernel to retrieve the best solution among those in cpuSolutions
+         int resultSize = (searchSpaceDim + blockSize.x() - 1) / blockSize.x();
+         FloatPointer gpuBestCosts = new FloatPointer();
+         IntPointer gpuBestIndices = new IntPointer();
+         CUDATools.mallocAsync(gpuBestCosts, resultSize, cudaStream);
+         CUDATools.mallocAsync(gpuBestIndices, resultSize, cudaStream);
 
-      // Run the kernel
-      blockResultKernel.withPointer(gpuCosts)
-                       .withInt(searchSpaceDim)
-                       .withPointer(gpuBestCosts)
-                       .withPointer(gpuBestIndices)
-                       .run(cudaStream, gridSize, blockSize, 0);
+         LogTools.warn("Running block result kernel");
+         // Run the kernel
+         blockSize.x(512);
+         gridSize.x(resultSize);
+         blockResultKernel.withPointer(gpuCosts)
+                          .withInt(searchSpaceDim)
+                          .withPointer(gpuBestCosts)
+                          .withPointer(gpuBestIndices)
+                          .run(cudaStream, gridSize, blockSize, 0);
 
-      FloatPointer gpuGlobalBestCost = new FloatPointer();
-      IntPointer gpuGlobalBestIndex = new IntPointer();
-      CUDATools.mallocAsync(gpuGlobalBestIndex, 1, cudaStream);
-      CUDATools.mallocAsync(gpuGlobalBestCost, 1, cudaStream);
+         FloatPointer gpuGlobalBestCost = new FloatPointer();
+         IntPointer gpuGlobalBestIndex = new IntPointer();
+         CUDATools.mallocAsync(gpuGlobalBestIndex, 1, cudaStream);
+         CUDATools.mallocAsync(gpuGlobalBestCost, 1, cudaStream);
 
-      // Run the kernel
-      globalResultKernel.withPointer(gpuBestCosts)
-                        .withPointer(gpuBestIndices)
-                        .withInt(resultSize)
-                        .withPointer(gpuGlobalBestCost)
-                        .withPointer(gpuGlobalBestIndex)
-                        .run(cudaStream, gridSize, blockSize, 0);
-      IntPointer cpuGlobalBestIndex = new IntPointer(1);
-      FloatPointer cpuGlobalBestCost = new FloatPointer(1);
-      CUDATools.memcpyAsync(cpuGlobalBestIndex, gpuGlobalBestIndex, 1, cudaStream);
-      CUDATools.memcpyAsync(cpuGlobalBestCost, gpuGlobalBestCost, 1, cudaStream);
-      cudart.cudaStreamSynchronize(cudaStream);
+         LogTools.warn("Running global result kernel");
+         // Run the kernel
+         blockSize.x(resultSize);
+         gridSize.x(1);
+         globalResultKernel.withPointer(gpuBestCosts)
+                           .withPointer(gpuBestIndices)
+                           .withInt(resultSize)
+                           .withPointer(gpuGlobalBestCost)
+                           .withPointer(gpuGlobalBestIndex)
+                           .run(cudaStream, gridSize, blockSize, 0);
+         LogTools.warn("Done global result kernel");
+         IntPointer cpuGlobalBestIndex = new IntPointer(1);
+         FloatPointer cpuGlobalBestCost = new FloatPointer(1);
+         CUDATools.memcpyAsync(cpuGlobalBestIndex, gpuGlobalBestIndex, 1, cudaStream);
+         CUDATools.memcpyAsync(cpuGlobalBestCost, gpuGlobalBestCost, 1, cudaStream);
 
-      int startIndex = cpuGlobalBestIndex.get(0);
-      LogTools.info("Grid search finished. Best cost: {}, idx: {}", cpuGlobalBestCost.get(0), startIndex);
-      bestSolution[0] = cpuSolutions.get(startIndex * 3L);
-      bestSolution[1] = cpuSolutions.get(startIndex * 3L + 1);
-      bestSolution[2] = cpuSolutions.get(startIndex * 3L + 2);
+         cudart.cudaStreamSynchronize(cudaStream);
 
-      FramePose3D optimalFootstepPose = new FramePose3D(initialPose);
-      optimalFootstepPose.setX(bestSolution[0]);
-      optimalFootstepPose.setY(bestSolution[1]);
-      optimalFootstepPose.getRotation().set(new YawPitchRoll(bestSolution[2], initialPose.getPitch(), initialPose.getRoll()));
-      optimalFootstepPose.setZ(currentHeightMapData.getHeightAt(bestSolution[0], bestSolution[1]));
+         int startIndex = cpuGlobalBestIndex.get(0);
+         LogTools.info("Grid search finished. Best cost: {}, idx: {}", cpuGlobalBestCost.get(0), startIndex);
+         bestSolution[0] = cpuSolutions.get(startIndex * 3L);
+         bestSolution[1] = cpuSolutions.get(startIndex * 3L + 1);
+         bestSolution[2] = cpuSolutions.get(startIndex * 3L + 2);
 
-      // Release stuff
-      cudaFreeAsync(gpuInitialPose, cudaStream);
-      cudaFreeAsync(gpuHeights, cudaStream);
-      cudaFreeAsync(gpuGridCenter, cudaStream);
-      cudaFreeAsync(gpuCosts, cudaStream);
-      cudaFreeAsync(gpuGlobalBestCost, cudaStream);
-      cudaFreeAsync(gpuGlobalBestIndex, cudaStream);
-      cudaFreeAsync(gpuSolutions, cudaStream);
-      cudaFreeAsync(gpuBestCosts, cudaStream);
-      cudaFreeAsync(gpuBestIndices, cudaStream);
+         FramePose3D optimalFootstepPose = new FramePose3D(initialPose);
+         optimalFootstepPose.setX(bestSolution[0]);
+         optimalFootstepPose.setY(bestSolution[1]);
+         optimalFootstepPose.getRotation().set(new YawPitchRoll(bestSolution[2], initialPose.getPitch(), initialPose.getRoll()));
+         optimalFootstepPose.setZ(currentHeightMapData.getHeightAt(bestSolution[0], bestSolution[1]));
 
-      return optimalFootstepPose;
+         // Release stuff
+         cudaFreeAsync(gpuInitialPose, cudaStream);
+         cudaFreeAsync(gpuHeights, cudaStream);
+         cudaFreeAsync(gpuGridCenter, cudaStream);
+         cudaFreeAsync(gpuCosts, cudaStream);
+         cudaFreeAsync(gpuGlobalBestCost, cudaStream);
+         cudaFreeAsync(gpuGlobalBestIndex, cudaStream);
+         cudaFreeAsync(gpuSolutions, cudaStream);
+         cudaFreeAsync(gpuBestCosts, cudaStream);
+         cudaFreeAsync(gpuBestIndices, cudaStream);
+
+         return optimalFootstepPose;
+      }
    }
 
    @Override
@@ -219,60 +234,69 @@ public class CUDAFootstepOptimizer implements AutoCloseable
 
    public void testResultKernel()
    {
-      int resultSize = searchSpaceDim / blockSize.x();
-      FloatPointer cpuBestCosts = new FloatPointer(resultSize);
-      FloatPointer gpuBestCosts = new FloatPointer();
-      CUDATools.mallocAsync(gpuBestCosts, resultSize, cudaStream);
-
-      IntPointer cpuBestIndices = new IntPointer(resultSize);
-      IntPointer gpuBestIndices = new IntPointer();
-      CUDATools.mallocAsync(gpuBestIndices, resultSize, cudaStream);
-
-      LogTools.info("Running block kernel result");
-      // Run the kernel
-      blockResultKernel.withPointer(gpuCosts)
-                       .withInt(searchSpaceDim)
-                       .withPointer(gpuBestCosts)
-                       .withPointer(gpuBestIndices)
-                       .run(cudaStream, gridSize, blockSize, 0);
-
-      CUDATools.memcpyAsync(cpuBestCosts, gpuBestCosts, resultSize, cudaStream);
-      CUDATools.memcpyAsync(cpuBestIndices, gpuBestIndices, resultSize, cudaStream);
-
-      for (int i=0; i<resultSize; i++)
-         LogTools.info("Best Block Costs: {}, Indices: {}", cpuBestCosts.get(i), cpuBestIndices.get(i));
-
-      searchSpaceDim = resultSize;
-      FloatPointer gpuGlobalBestCost = new FloatPointer();
-      IntPointer gpuGlobalBestIndex = new IntPointer();
-      CUDATools.mallocAsync(gpuGlobalBestIndex, 1, cudaStream);
-      CUDATools.mallocAsync(gpuGlobalBestCost, 1, cudaStream);
-
-      LogTools.info("Running global kernel result");
-      // Run the kernel
-      globalResultKernel.withPointer(gpuBestCosts)
-                        .withPointer(gpuBestIndices)
-                        .withInt(searchSpaceDim)
-                        .withPointer(gpuGlobalBestCost)
-                        .withPointer(gpuGlobalBestIndex)
-                        .run(cudaStream, new dim3(), blockSize, 0);
-
       cpuGlobalBestIndex = new IntPointer(1);
       cpuGlobalBestCost = new FloatPointer(1);
-      CUDATools.memcpyAsync(cpuGlobalBestCost, gpuGlobalBestCost, 1, cudaStream);
-      CUDATools.memcpyAsync(cpuGlobalBestIndex, gpuGlobalBestIndex, 1, cudaStream);
-      cudart.cudaStreamSynchronize(cudaStream);
+      try (PointerScope ignored = new PointerScope())
+      {
+         blockSize.x(512);
 
-      LogTools.info("Best Cost: {}, Idx: {}", cpuGlobalBestCost.get(0), cpuGlobalBestIndex.get(0));
+         int resultSize = (searchSpaceDim + blockSize.x() -1) / blockSize.x();
+         FloatPointer cpuBestCosts = new FloatPointer(resultSize);
+         FloatPointer gpuBestCosts = new FloatPointer();
+         CUDATools.mallocAsync(gpuBestCosts, resultSize, cudaStream);
 
-      // Release stuff
-      blockSize.close();
-      gridSize.close();
-      cudaFreeAsync(gpuCosts, cudaStream);
-      cudaFreeAsync(cpuGlobalBestCost, cudaStream);
-      cudaFreeAsync(gpuGlobalBestIndex, cudaStream);
-      cudaFreeAsync(gpuBestCosts, cudaStream);
-      cudaFreeAsync(gpuBestIndices, cudaStream);
+         IntPointer cpuBestIndices = new IntPointer(resultSize);
+         IntPointer gpuBestIndices = new IntPointer();
+         CUDATools.mallocAsync(gpuBestIndices, resultSize, cudaStream);
+
+         LogTools.info("Running block kernel result");
+         // Run the kernel
+         blockSize.x(512);
+         gridSize.x(resultSize);
+         blockResultKernel.withPointer(gpuCosts)
+                          .withInt(searchSpaceDim)
+                          .withPointer(gpuBestCosts)
+                          .withPointer(gpuBestIndices)
+                          .run(cudaStream, gridSize, blockSize, 0);
+
+         CUDATools.memcpyAsync(cpuBestCosts, gpuBestCosts, resultSize, cudaStream);
+         CUDATools.memcpyAsync(cpuBestIndices, gpuBestIndices, resultSize, cudaStream);
+
+         for (int i = 0; i < resultSize; i++)
+            LogTools.info("Best Block Costs: {}, Indices: {}", cpuBestCosts.get(i), cpuBestIndices.get(i));
+
+         searchSpaceDim = resultSize;
+         FloatPointer gpuGlobalBestCost = new FloatPointer();
+         IntPointer gpuGlobalBestIndex = new IntPointer();
+         CUDATools.mallocAsync(gpuGlobalBestIndex, 1, cudaStream);
+         CUDATools.mallocAsync(gpuGlobalBestCost, 1, cudaStream);
+
+         LogTools.info("Running global kernel result");
+         // Run the kernel
+         blockSize.x(resultSize);
+         gridSize.x(1);
+         globalResultKernel.withPointer(gpuBestCosts)
+                           .withPointer(gpuBestIndices)
+                           .withInt(searchSpaceDim)
+                           .withPointer(gpuGlobalBestCost)
+                           .withPointer(gpuGlobalBestIndex)
+                           .run(cudaStream, new dim3(), blockSize, 0);
+
+         CUDATools.memcpyAsync(cpuGlobalBestCost, gpuGlobalBestCost, 1, cudaStream);
+         CUDATools.memcpyAsync(cpuGlobalBestIndex, gpuGlobalBestIndex, 1, cudaStream);
+         cudart.cudaStreamSynchronize(cudaStream);
+
+         LogTools.info("Best Cost: {}, Idx: {}", cpuGlobalBestCost.get(0), cpuGlobalBestIndex.get(0));
+
+         // Release stuff
+         blockSize.close();
+         gridSize.close();
+         cudaFreeAsync(gpuCosts, cudaStream);
+         cudaFreeAsync(cpuGlobalBestCost, cudaStream);
+         cudaFreeAsync(gpuGlobalBestIndex, cudaStream);
+         cudaFreeAsync(gpuBestCosts, cudaStream);
+         cudaFreeAsync(gpuBestIndices, cudaStream);
+      }
    }
 
    public void setGpuCosts(float[] costs)
