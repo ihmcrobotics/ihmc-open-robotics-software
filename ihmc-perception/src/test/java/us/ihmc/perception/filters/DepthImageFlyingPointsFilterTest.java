1782aa8eccfb0ed2679a16f53111f627fc9d2121
nkitchel <nkitchel@ihmc.org>
2025-05-13 09:40:36 -0500

    Update tests

--- ihmc-perception/src/test/java/us/ihmc/perception/filters/DepthImageFlyingPointsFilterTest.java
+++ ihmc-perception/src/test/java/us/ihmc/perception/filters/DepthImageFlyingPointsFilterTest.java
@@ -5,6 +5,8 @@ import org.bytedeco.javacpp.ShortPointer;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.GpuMat;
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import us.ihmc.perception.camera.CameraIntrinsics;
 
@@ -19,10 +21,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DepthImageFlyingPointsFilterTest
 {
+   private CameraIntrinsics cameraIntrinsics;
+   private DepthImageFlyingPointsFilter flyingPointsFilter;
+
+   @BeforeEach
+   public void setupVariables()
+   {
+      DepthImageFilteringParameters depthImageFilteringParameters = new DepthImageFilteringParameters();
+      cameraIntrinsics = new CameraIntrinsics();
+      flyingPointsFilter = new DepthImageFlyingPointsFilter(depthImageFilteringParameters);
+   }
+
+   @AfterEach
+   public void shutdownTest()
+   {
+      flyingPointsFilter.destroy();
+   }
+
+   /**
+    * This test makes sure that we don't detect flying points on a section that doesn't have any problems
+    */
    @Test
    public void testFlyingPointsDontGetDetected_0()
    {
-
+      // Don't format this grid because it's easier to visualize this way
       short[] depthValues = new short[]
             {
                   1940, 1938, 1936, 1934, 1932, 1932, 1930,
@@ -31,47 +53,36 @@ public class DepthImageFlyingPointsFilterTest
                   1936, 1934, 1932, 1930, 1929, 1927, 1925,
                   1936, 1932, 1930, 1929, 1927, 1925, 1923,
                   1934, 1932, 1930, 1927, 1925, 1923, 1921,
-      };
+            };
 
+      // u is for the columns, v is for the rows (7 x 6)
+      int u = 7;
+      int v = 6;
+      setupCameraIntrinsics(u, v);
 
-      Mat depthMat = new Mat(25, 25, opencv_core.CV_16UC1);
+      // Setup mat to pass into the filter
+      Mat depthMat = new Mat(u, v, opencv_core.CV_16UC1);
       ShortPointer sp = new ShortPointer(depthValues);
       depthMat.data().put(sp);
 
       GpuMat inputDepthImage = new GpuMat();
       inputDepthImage.upload(depthMat);
-      GpuMat outputFilteredDepthImage = new GpuMat(inputDepthImage.size(), inputDepthImage.type());
 
-      DepthImageFilteringParameters depthImageFilteringParameters = new DepthImageFilteringParameters();
-      DepthImageFlyingPointsFilter flyingPointsFilter = new DepthImageFlyingPointsFilter(depthImageFilteringParameters);
-      CameraIntrinsics cameraIntrinsics = new CameraIntrinsics();
-      cameraIntrinsics.setCx(7/640.0);
-      cameraIntrinsics.setCy(6/360.0);
-      cameraIntrinsics.setFx(7/649.4571918977125); // Pixels per meter
-      cameraIntrinsics.setFy(6/649.4571918977125); // Pixels per meter
+      GpuMat outputFilteredDepthImage = new GpuMat(inputDepthImage.size(), inputDepthImage.type());
       flyingPointsFilter.applyFilter(inputDepthImage, outputFilteredDepthImage, cameraIntrinsics);
 
-      Mat result = new Mat();
-      outputFilteredDepthImage.download(result);
+      checkResultFromFilter(outputFilteredDepthImage, depthMat);
+   }
 
-      for (int i = 0; i < result.rows(); i++)
-      {
-         for (int j = 0; j < result.cols(); j++)
-         {
-            assertEquals(depthMat.row(i).col(j).data().getShort(),
-                         result.row(i).col(j).data().getShort(),
-                         "The filtered data does not match the expected data!" + "Input Data: " + depthMat.row(i).col(j).data().getShort()
-                         + "\n Filtered Data: " + result.row(i).col(j).data().getShort() + ".\n" + "Row: " + i + " and Column: " + j + "!");
-         }
-      }
-
-      flyingPointsFilter.destroy();
-   }      @Test
+   /**
+    * This test makes sure that we don't detect flying points on a section that doesn't have any problems
+    */
+   @Test
    public void testFlyingPointsDontGetDetected_1()
    {
-
+      // Don't format this grid because it's easier to visualize this way
       short[] depthValues = new short[]
-            {
+         {
             1953, 1953, 1955, 1955, 1955, 1953, 1953, 1951, 1949, 1948, 1946, 1944, 1942, 1940, 1938, 1936, 1934, 1932, 1932, 1930, 1929, 1929, 1927, 1927, 1927,
             1953, 1953, 1955, 1955, 1955, 1953, 1953, 1951, 1949, 1948, 1946, 1944, 1942, 1940, 1938, 1936, 1934, 1932, 1932, 1930, 1929, 1929, 1927, 1927, 1927,
             1955, 1955, 1955, 1955, 1955, 1953, 1953, 1951, 1949, 1948, 1946, 1944, 1940, 1938, 1936, 1934, 1932, 1930, 1929, 1927, 1927, 1927, 1925, 1925, 1923,
@@ -97,48 +108,34 @@ public class DepthImageFlyingPointsFilterTest
             1917, 1917, 1917, 1915, 1915, 1915, 1915, 1914, 1914, 1914, 1914, 1912, 1912, 1912, 1912, 1912, 1912, 1912, 1912, 1912, 1912, 1910, 1910, 1910, 1908,
             1917, 1917, 1915, 1915, 1915, 1915, 1914, 1914, 1914, 1914, 1912, 1912, 1912, 1912, 1912, 1912, 1912, 1912, 1910, 1910, 1910, 1910, 1908, 1908, 1906,
             1915, 1915, 1915, 1915, 1914, 1914, 1914, 1914, 1914, 1912, 1912, 1912, 1912, 1912, 1910, 1910, 1910, 1910, 1910, 1910, 1910, 1908, 1908, 1906, 1906
-      };
+         };
 
-
-      Mat depthMat = new Mat(25, 25, opencv_core.CV_16UC1);
+      int u = 25;
+      int v = 25;
+      Mat depthMat = new Mat(u, v, opencv_core.CV_16UC1);
       ShortPointer sp = new ShortPointer(depthValues);
       depthMat.data().put(sp);
 
       GpuMat inputDepthImage = new GpuMat();
       inputDepthImage.upload(depthMat);
-      GpuMat outputFilteredDepthImage = new GpuMat(inputDepthImage.size(), inputDepthImage.type());
 
-      DepthImageFilteringParameters depthImageFilteringParameters = new DepthImageFilteringParameters();
-      DepthImageFlyingPointsFilter flyingPointsFilter = new DepthImageFlyingPointsFilter(depthImageFilteringParameters);
-      CameraIntrinsics cameraIntrinsics = new CameraIntrinsics();
-      cameraIntrinsics.setCx(25/640.0);
-      cameraIntrinsics.setCy(25/360.0);
-      cameraIntrinsics.setFx((double) 25 / 720); // Pixels per meter
-      cameraIntrinsics.setFy((double) 25 / 1280); // Pixels per meter
+      setupCameraIntrinsics(u, v);
+
+      GpuMat outputFilteredDepthImage = new GpuMat(inputDepthImage.size(), inputDepthImage.type());
       flyingPointsFilter.applyFilter(inputDepthImage, outputFilteredDepthImage, cameraIntrinsics);
 
-      Mat result = new Mat();
-      outputFilteredDepthImage.download(result);
-
-      for (int i = 0; i < result.rows(); i++)
-      {
-         for (int j = 0; j < result.cols(); j++)
-         {
-            assertEquals(depthMat.row(i).col(j).data().getShort(),
-                         result.row(i).col(j).data().getShort(),
-                         "The filtered data does not match the expected data!" + "Input Data: " + depthMat.row(i).col(j).data().getShort()
-                         + "\n Filtered Data: " + result.row(i).col(j).data().getShort() + ".\n" + "Row: " + i + " and Column: " + j + "!");
-         }
-      }
-
-      flyingPointsFilter.destroy();
+      checkResultFromFilter(outputFilteredDepthImage, depthMat);
    }
 
+   /**
+    * This test makes sure that we don't detect flying points on a section that doesn't have any problems
+    */
    @Test
    public void testFlyingPointsDontGetDetected_2()
    {
-
-      short[] depthValues = new short[] {
+      // Don't format this grid because it's easier to visualize this way
+      short[] depthValues = new short[]
+         {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -159,10 +156,12 @@ public class DepthImageFlyingPointsFilterTest
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-      };
+         };
 
+      int u = 20;
+      int v = 20;
 
-      Mat depthMat = new Mat(20, 20, opencv_core.CV_16UC1);
+      Mat depthMat = new Mat(u, v, opencv_core.CV_16UC1);
       ShortPointer sp = new ShortPointer(depthValues);
       depthMat.data().put(sp);
 
@@ -170,52 +169,28 @@ public class DepthImageFlyingPointsFilterTest
       inputDepthImage.upload(depthMat);
       GpuMat outputFilteredDepthImage = new GpuMat(inputDepthImage.size(), inputDepthImage.type());
 
-      DepthImageFilteringParameters depthImageFilteringParameters = new DepthImageFilteringParameters();
-      DepthImageFlyingPointsFilter flyingPointsFilter = new DepthImageFlyingPointsFilter(depthImageFilteringParameters);
-      CameraIntrinsics cameraIntrinsics = new CameraIntrinsics();
-      cameraIntrinsics.setCx(20/640.0);
-      cameraIntrinsics.setCy(20/360.0);
-      cameraIntrinsics.setFx((double) 20 / 720); // Pixels per meter
-      cameraIntrinsics.setFy((double) 20 / 1280); // Pixels per meter
+      setupCameraIntrinsics(u, v);
       flyingPointsFilter.applyFilter(inputDepthImage, outputFilteredDepthImage, cameraIntrinsics);
 
-      Mat result = new Mat();
-      outputFilteredDepthImage.download(result);
-
-      //      PerceptionDebugTools.printMat("Result", result, 1);
-
-      for (int i = 0; i < result.rows(); i++)
-      {
-         for (int j = 0; j < result.cols(); j++)
-         {
-            assertEquals(depthMat.row(i).col(j).data().getShort(), result.row(i).col(j).data().getShort(), "The filtered data does not match the expected data!"
-                                                                                                           + "Input Data: " + depthMat.row(i).col(j).data().getShort() +
-                                                                                                           "\n Filtered Data: " + result.row(i).col(j).data().getShort());
-         }
-      }
-
-      flyingPointsFilter.destroy();
+      checkResultFromFilter(outputFilteredDepthImage, depthMat);
    }
 
    @Test
    public void testFlyingPoints() throws IOException, URISyntaxException
    {
-
       Path path = Paths.get(Objects.requireNonNull(DepthImageFlyingPointsFilterTest.class.getResource("depthImageAsBytes.raw")).toURI());
       byte[] imageBytes = Files.readAllBytes(path);
       Mat depthMat = new Mat(720, 1280, opencv_core.CV_16UC1, new BytePointer(imageBytes));
-      //      PerceptionDebugTools.printMat("s", depthMat, 1);
 
       GpuMat inputDepthImage = new GpuMat();
       inputDepthImage.upload(depthMat);
       GpuMat outputFilteredDepthImage = new GpuMat(inputDepthImage.size(), inputDepthImage.type());
 
-      DepthImageFlyingPointsFilter flyingPointsFilter = new DepthImageFlyingPointsFilter(new DepthImageFilteringParameters());
       CameraIntrinsics cameraIntrinsics = new CameraIntrinsics();
       cameraIntrinsics.setCx(640.0);
       cameraIntrinsics.setCy(360.0);
-      cameraIntrinsics.setFx(720); // Pixels per meter
-      cameraIntrinsics.setFy(1280); // Pixels per meter
+      cameraIntrinsics.setFx(720);
+      cameraIntrinsics.setFy(1280);
 
       flyingPointsFilter.applyFilter(inputDepthImage, outputFilteredDepthImage, cameraIntrinsics);
 
@@ -237,6 +212,35 @@ public class DepthImageFlyingPointsFilterTest
 
          assertTrue(somethingChanged);
 
-      flyingPointsFilter.destroy();
+   }
+
+   private static void checkResultFromFilter(GpuMat outputFilteredDepthImage, Mat expectedValue)
+   {
+      Mat actualValue = new Mat();
+      outputFilteredDepthImage.download(actualValue);
+
+      for (int i = 0; i < actualValue.rows(); i++)
+      {
+         for (int j = 0; j < actualValue.cols(); j++)
+         {
+            assertEquals(expectedValue.row(i).col(j).data().getShort(),
+                         actualValue.row(i).col(j).data().getShort(),
+                         "The filtered data does not match the expected data!" + "Input Data: " + expectedValue.row(i).col(j).data().getShort()
+                         + "\n Filtered Data: " + actualValue.row(i).col(j).data().getShort() + ".\n" + "Row: " + i + " and Column: " + j + "!");
+         }
+      }
+   }
+
+   /**
+    * These are the camera intrinsics.
+    * The values hardcoded are pulled from the RealSense camera
+    * We scale those values by the current grid size to get an accurate representation for our smaller grid.
+    */
+   private void setupCameraIntrinsics(int uSize, int vSize)
+   {
+      cameraIntrinsics.setCx(uSize / 640.0);
+      cameraIntrinsics.setCy(vSize / 360.0);
+      cameraIntrinsics.setFx((double) uSize / 720);
+      cameraIntrinsics.setFy((double) vSize / 1280);
    }
 }
