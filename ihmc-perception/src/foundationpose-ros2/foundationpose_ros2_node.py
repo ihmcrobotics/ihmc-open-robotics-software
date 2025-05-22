c0c0391f1e748b6fc0a6a5fc0ed8752df4d7bf46
Tomasz Bialek <tbialek@ihmc.org>
2025-05-22 16:11:53 -0500

    Added ability to remove workers + cleanup

--- ihmc-perception/src/foundationpose-ros2/foundationpose_ros2_node.py
+++ ihmc-perception/src/foundationpose-ros2/foundationpose_ros2_node.py
@@ -13,9 +13,11 @@ from cv_bridge import CvBridge
 import trimesh
 import glob
 import threading
+import queue
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from sensor_msgs.msg import Image, CameraInfo
+from std_msgs.msg import String as StringMessage
 from perception_msgs.msg import FoundationPoseRequest, FoundationPoseResult
 from foundationpose_worker import FoundationPoseWorker
 
@@ -23,6 +25,7 @@ COLOR_TOPIC = '/foundation_pose/camera/color/image_raw'
 DEPTH_TOPIC = '/foundation_pose/camera/aligned_depth_to_color/image_raw'
 CAMERA_INFO_TOPIC = '/foundation_pose/camera/color/camera_info'
 REQUEST_TOPIC = '/foundation_pose/request'
+REMOVE_TOPIC = '/foundation_pose/remove'
 RESULT_TOPIC = '/foundation_pose/result'
 
 IMAGE_SCALE = 0.5
@@ -50,6 +53,8 @@ class FoundationPoseROS2Node(Node):
         self.depth = None
         self.camera_k = None
 
+        self.remove_queue = queue.Queue()
+
         self.new_depth_available = threading.Event()
         self.new_color_available = threading.Event()
 
@@ -62,12 +67,15 @@ class FoundationPoseROS2Node(Node):
         # Create subscription to request messages
         self.request_subscription = self.create_subscription(FoundationPoseRequest, REQUEST_TOPIC, self.request_callback, 10)
 
+        # Create subscription to reset messages
+        self.reset_subscription = self.create_subscription(StringMessage, REMOVE_TOPIC, self.remove_callback, 10)
+
         # Create a result publisher
         print("Creating publisher...")
         self.result_publisher = self.create_publisher(FoundationPoseResult, RESULT_TOPIC, 10)
 
         print("Starting the process thread...")
-        self.pose_estimation_thread = threading.Thread(target=self.process)
+        self.pose_estimation_thread = threading.Thread(target=self.process, daemon=True)
         self.pose_estimation_thread.start()
 
 
@@ -129,44 +137,53 @@ class FoundationPoseROS2Node(Node):
         )
 
 
+    def remove_callback(self, remove_target):
+        if remove_target.data in self.workers:
+            self.remove_queue.put(remove_target.data)
+
+
     def process(self):
         print("Starting process thread...")
-        try:
-            while True:
-                # Wait for new images to be available
-                self.new_color_available.wait()
-                self.new_depth_available.wait()
+        while True:
+            # Wait for new images to be available
+            self.new_color_available.wait()
+            self.new_depth_available.wait()
 
-                for object_id, worker in self.workers.items():
-                    pose = worker.update(self.rgb, self.depth)
+            # Remove workers requested on the remove topic
+            while not self.remove_queue.empty():
+                remove_target = self.remove_queue.get()
+                if remove_target in self.workers:
+                    del self.workers[remove_target]
 
-                    # Get the position and rotation (Z forward, Y up)
-                    position = pose[:3, 3]
-                    rotation_matrix = pose[:3, :3]
+            # Update each worker and publish the result pose
+            for object_id, worker in self.workers.items():
+                pose = worker.update(self.rgb, self.depth)
 
-                    # Transform to X forward Z up coordinates
-                    position = IHMC_COORD_ROTATION @ position
-                    rotation_matrix = IHMC_COORD_ROTATION @ rotation_matrix @ IHMC_COORD_ROTATION.T
+                # Get the position and rotation (Z forward, Y up)
+                position = pose[:3, 3]
+                rotation_matrix = pose[:3, :3]
 
-                    quaternion = R.from_matrix(rotation_matrix).as_quat()
+                # Transform to X forward Z up coordinates
+                position = IHMC_COORD_ROTATION @ position
+                rotation_matrix = IHMC_COORD_ROTATION @ rotation_matrix @ IHMC_COORD_ROTATION.T
 
-                    result = FoundationPoseResult()
-                    result.object_id = object_id
-                    result.object_pose.position.x = position[0]
-                    result.object_pose.position.y = position[1]
-                    result.object_pose.position.z = position[2]
+                # Create the result message and publish it
+                result = FoundationPoseResult()
+                result.object_id = object_id
+                result.object_pose.position.x = position[0]
+                result.object_pose.position.y = position[1]
+                result.object_pose.position.z = position[2]
 
-                    result.object_pose.orientation.x = quaternion[0]
-                    result.object_pose.orientation.y = quaternion[1]
-                    result.object_pose.orientation.z = quaternion[2]
-                    result.object_pose.orientation.w = quaternion[3]
+                quaternion = R.from_matrix(rotation_matrix).as_quat()
+                result.object_pose.orientation.x = quaternion[0]
+                result.object_pose.orientation.y = quaternion[1]
+                result.object_pose.orientation.z = quaternion[2]
+                result.object_pose.orientation.w = quaternion[3]
 
-                    self.result_publisher.publish(result)
+                self.result_publisher.publish(result)
 
-                self.new_color_available.clear()
-                self.new_depth_available.clear()
-        except KeyboardInterrupt:
-            print("Process finishing...")
+            self.new_color_available.clear()
+            self.new_depth_available.clear()
 
 
 def main():
