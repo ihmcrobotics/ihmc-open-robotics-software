30512563b2c69ab2e1cb6ce448f460c3bfb5f956
Tomasz Bialek <tbialek@ihmc.org>
2025-05-28 13:19:27 -0500

    Added new YOLO mode, and associated object meshes

--- ihmc-perception/src/main/java/us/ihmc/perception/detections/foundationPose/FoundationPoseTools.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/detections/foundationPose/FoundationPoseTools.java
@@ -8,7 +8,10 @@ import java.util.Set;
 public class FoundationPoseTools
 {
    private static final Map<String, String> YOLO_CLASS_TO_OBJECT_MESH_MAP
-         = Map.of("bottle", "mustard0.obj");
+         = Map.of("bottle", "mustard0.obj",
+                  "door_panel", "DoorPanel.obj",
+                  "charge", "charge.obj",
+                  "traffic_barrier", "traffic_barrier.obj");
 
    public static Map<String, String> getYOLOClassToObjectMeshMap()
    {
