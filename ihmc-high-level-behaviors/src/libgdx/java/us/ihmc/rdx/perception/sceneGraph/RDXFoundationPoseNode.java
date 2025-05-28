d75f25dc28779d4f9f03a3ae9829bd7724887dea
Tomasz Bialek <tbialek@ihmc.org>
2025-05-28 16:01:47 -0500

    Fixed RDXFoundationPoseNode frame graphic

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXFoundationPoseNode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/perception/sceneGraph/RDXFoundationPoseNode.java
@@ -1,8 +1,10 @@
 package us.ihmc.rdx.perception.sceneGraph;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
+import us.ihmc.perception.sceneGraph.SceneGraph;
 import us.ihmc.perception.sceneGraph.foundationPose.FoundationPoseNode;
 import us.ihmc.rdx.sceneManager.RDXSceneLevel;
 import us.ihmc.rdx.ui.graphics.RDXReferenceFrameGraphic;
@@ -11,22 +13,33 @@ import java.util.Set;
 
 public class RDXFoundationPoseNode extends RDXDetectableSceneNode
 {
+   private final FoundationPoseNode foundationPoseNode;
    private final RDXReferenceFrameGraphic poseGraphic;
 
    public RDXFoundationPoseNode(FoundationPoseNode foundationPoseNode)
    {
       super(foundationPoseNode);
 
-      poseGraphic = new RDXReferenceFrameGraphic(0.2);
+      this.foundationPoseNode = foundationPoseNode;
+
+      poseGraphic = new RDXReferenceFrameGraphic(0.2, Color.ORANGE);
+      poseGraphic.setToReferenceFrame(foundationPoseNode.getNodeFrame());
+   }
+
+   @Override
+   public void update(SceneGraph sceneGraph)
+   {
+      super.update(sceneGraph);
       poseGraphic.setToReferenceFrame(foundationPoseNode.getNodeFrame());
    }
 
    @Override
    public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool, Set<RDXSceneLevel> sceneLevels)
    {
-      super.getRenderables(renderables, pool, sceneLevels);
-      poseGraphic.updateFromLastGivenFrame();
-      poseGraphic.getRenderables(renderables, pool);
+      if (sceneLevels.contains(RDXSceneLevel.VIRTUAL) && !super.isGraphicsHidden())
+      {
+         poseGraphic.getRenderables(renderables, pool);
+      }
    }
 
    @Override
