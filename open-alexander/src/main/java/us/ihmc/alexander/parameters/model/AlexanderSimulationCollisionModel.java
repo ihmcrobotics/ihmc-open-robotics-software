a2f536f1eade89945d3648f76b5b281f2471253a
lpenco <lpenco@ihmc.org>
2025-06-10 16:23:53 -0500

    kin and sim collision for alexander

--- open-alexander/src/main/java/us/ihmc/alexander/parameters/model/AlexanderSimulationCollisionModel.java
+++ open-alexander/src/main/java/us/ihmc/alexander/parameters/model/AlexanderSimulationCollisionModel.java
@@ -1,7 +1,9 @@
 package us.ihmc.alexander.parameters.model;
 
 import us.ihmc.avatar.initialSetup.DRCSCSInitialSetup;
+import us.ihmc.euclid.Axis3D;
 import us.ihmc.euclid.referenceFrame.FrameBox3D;
+import us.ihmc.euclid.referenceFrame.FrameCapsule3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.referenceFrame.interfaces.FrameBox3DBasics;
 import us.ihmc.euclid.tuple3D.Point3D;
@@ -69,18 +71,58 @@ public class AlexanderSimulationCollisionModel implements RobotCollisionModel
       collisionMask = helper.getCollisionMask(robotCollisionMask);
       collisionGroup = helper.createCollisionGroup(otherCollisionMasks);
 
+      RigidBodyBasics head = RobotCollisionModel.findRigidBody(jointMap.getHeadName(), multiBodySystem);
+      RigidBodyBasics torso = RobotCollisionModel.findRigidBody(jointMap.getChestName(), multiBodySystem);
+      RigidBodyBasics pelvis = RobotCollisionModel.findRigidBody(jointMap.getPelvisName(), multiBodySystem);
+      // Head ---------------------------------------------------------------------
+      if (head != null)
+      {
+         MovingReferenceFrame headFrame = head.getBodyFixedFrame();
+         FrameCapsule3D headShapeMultisense = new FrameCapsule3D(headFrame, 0.08, 0.15);
+         headShapeMultisense.getPosition().set(0.03, 0.0, 0.03);
+         headShapeMultisense.getAxis().set(Axis3D.Z);
+         collidables.add(new Collidable(head, collisionMask, collisionGroup, headShapeMultisense));
+      }
+      // Torso ---------------------------------------------------------------------
+      if (torso != null)
+      {
+         MovingReferenceFrame torsoFrame = torso.getParentJoint().getFrameAfterJoint();
+         // Capsule along the forward axis covering the bottom part of the "abdomen" and of the chest.
+         FrameCapsule3D torsoShapeBottomCenter = new FrameCapsule3D(torsoFrame, 0.1, 0.17);
+         torsoShapeBottomCenter.getPosition().set(-0.01, 0.0, 0.22);
+         torsoShapeBottomCenter.getAxis().set(Axis3D.Z);
+         collidables.add(new Collidable(torso, collisionMask, collisionGroup, torsoShapeBottomCenter));
+      }
+      // Pelvis ---------------------------------------------------------------------
+      if (pelvis != null)
+      {
+         MovingReferenceFrame pelvisFrame = pelvis.getParentJoint().getFrameAfterJoint();
+         FrameCapsule3D pelvisShape = new FrameCapsule3D(pelvisFrame, 0.05, 0.135);
+         pelvisShape.getAxis().set(Axis3D.Y);
+         pelvisShape.getPosition().set(-0.06, 0.0, -0.02);
+         collidables.add(new Collidable(pelvis, collisionMask, collisionGroup, pelvisShape));
+      }
       for (RobotSide robotSide : RobotSide.values)
       {
+         { // Hand
+            RigidBodyBasics hand = RobotCollisionModel.findRigidBody(jointMap.getHandName(robotSide), multiBodySystem);
+            if (hand != null)
+            {
+               ReferenceFrame handFrame = hand.getParentJoint().getFrameAfterJoint();
+               FrameCapsule3D handShapeKnob = new FrameCapsule3D(handFrame, 0.06, 0.06);
+               handShapeKnob.getPosition().set(0.0, 0.0, 0.0);
+               handShapeKnob.getAxis().set(Axis3D.Z);
+               collidables.add(new Collidable(hand, collisionMask, collisionGroup, handShapeKnob));
+            }
+         }
          { // Foot
             JointBasics ankleRoll = RobotCollisionModel.findJoint(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_ROLL), multiBodySystem);
             MovingReferenceFrame ankleRollFrame = ankleRoll.getFrameAfterJoint();
             RigidBodyBasics foot = ankleRoll.getSuccessor();
-
-            Vector3D footSize = new Vector3D(physicalProperties.getActualFootLength(), physicalProperties.getActualFootWidth(), 0.025);
-            Point3D footPosition = new Point3D(physicalProperties.getActualFootBack(), 0.0, -physicalProperties.getAnkleHeight());
-            // Using an STP box so the sole is slightly rounded allowing for continuous and smooth contact with the ground.
-            FrameBox3DBasics footShape = newBoxWithSTP(ankleRollFrame, footSize);
-            footShape.getPosition().set(footPosition);
+            // Using a STP box so the sole is slightly rounded allowing for continuous and smooth contact with the ground.
+            FrameSTPBox3D footShape = new FrameSTPBox3D(ankleRollFrame, 0.26, 0.14, 0.055);
+            footShape.getPosition().set(0.045, 0.0, -0.05);
+            footShape.setMargins(1.0e-5, 4.0e-4);
             collidables.add(new Collidable(foot, collisionMask, collisionGroup, footShape));
          }
       }
