36ba11cdbb15cbf5203e0b97815c27d43affdb31
Nick Kitchel <nkitchel@ihmc.org>
2025-06-11 12:06:17 -0500

    Removed debugging prints

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/RapidHeightMapThread.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/perception/RapidHeightMapThread.java
@@ -110,9 +110,7 @@ public class RapidHeightMapThread extends RepeatingTaskThread
          // Update height map
          synchronized (heightMapLock)
          {
-            LogTools.info("Start...");
             heightMapManager.updateAndPublishHeightMap(filteredDepthImage, depthIntrinsicsCopy, cameraFrameInWorld, cameraZUpFrameInWorld);
-            LogTools.info("END");
          }
 
          filteredDepthImage.close();
