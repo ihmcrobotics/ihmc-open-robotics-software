5d563c9f5433c95626699731d10be33fb308a02d
Ben Pratt <c.ben.pratt@gmail.com>
2025-06-11 11:43:25 -0500

    A lot of copies and pastes, will change later just need to get in for now

---
+++ ihmc-communication/src/main/java/us/ihmc/communication/AbilityHandAPI.java
@@ -0,0 +1,13 @@
+package us.ihmc.communication;
+
+import controller_msgs.msg.dds.AbilityHandDesiredCommandMessage;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.ros2.ROS2Topic;
+
+public final class AbilityHandAPI
+{
+   public static ROS2Topic<AbilityHandDesiredCommandMessage> getHandAbilityCommandTopic(String robotName, RobotSide side)
+   {
+      return HumanoidControllerAPI.getInputTopic(robotName).withTypeName(AbilityHandDesiredCommandMessage.class).withSuffix(side.getLowerCaseName());
+   }
+}
