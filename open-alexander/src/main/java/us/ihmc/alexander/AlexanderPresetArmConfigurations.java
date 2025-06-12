18fd4e107c7d5eea19fe40725e56d6e4835a6f4f
lpenco <lpenco@ihmc.org>
2025-06-12 15:26:29 -0500

    get rid of nadia references

--- open-alexander/src/main/java/us/ihmc/alexander/AlexanderPresetArmConfigurations.java
+++ open-alexander/src/main/java/us/ihmc/alexander/AlexanderPresetArmConfigurations.java
@@ -5,31 +5,29 @@ import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 
 /**
- * This class contains the arm configurations for Nadia; this allows for the Cycloid arms, the TMotor arms, and the Cycloid arms
- * with the TMotor forearms with the SAKE hands.
+ * This class contains the arm configurations for Alexander
  */
 public class AlexanderPresetArmConfigurations
 {
-   private static final SideDependentList<double[]> STAND_PREP_CYCLOID_4DOF = new SideDependentList<>();
-   private static final SideDependentList<double[]> HOME_CYCLOID_4DOF = new SideDependentList<>();
-   private static final SideDependentList<double[]> TUCKED_UP_ARMS_CYCLOID_4DOF = new SideDependentList<>();
+   private static final SideDependentList<double[]> STAND_PREP_4DOF = new SideDependentList<>();
+   private static final SideDependentList<double[]> HOME_4DOF = new SideDependentList<>();
+   private static final SideDependentList<double[]> TUCKED_UP_ARMS_4DOF = new SideDependentList<>();
 
-   private static final SideDependentList<double[]> STAND_PREP_CYCLOID_7DOF = new SideDependentList<>();
-   private static final SideDependentList<double[]> HOME_CYCLOID_7DOF = new SideDependentList<>();
-   private static final SideDependentList<double[]> TUCKED_UP_ARMS_CYCLOID_7DOF = new SideDependentList<>();
+   private static final SideDependentList<double[]> STAND_PREP_7DOF = new SideDependentList<>();
+   private static final SideDependentList<double[]> HOME_7DOF = new SideDependentList<>();
+   private static final SideDependentList<double[]> TUCKED_UP_ARMS_7DOF = new SideDependentList<>();
 
-   // To tune these values, run NadiaModelViewer as it allows you to tune joint angles with sliders
    static
    {
       for (RobotSide side : RobotSide.values)
       {
-         HOME_CYCLOID_4DOF.put(side, new double[] {0.018, side.negateIfRightSide(0.146), side.negateIfRightSide(-0.202), -0.312});
-         STAND_PREP_CYCLOID_4DOF.put(side, HOME_CYCLOID_4DOF.get(side));
-         TUCKED_UP_ARMS_CYCLOID_4DOF.put(side, new double[] {0.104, side.negateIfRightSide(0.347), side.negateIfRightSide(-0.094), -1.2});
+         HOME_4DOF.put(side, new double[] {0.018, side.negateIfRightSide(0.146), side.negateIfRightSide(-0.202), -0.312});
+         STAND_PREP_4DOF.put(side, HOME_4DOF.get(side));
+         TUCKED_UP_ARMS_4DOF.put(side, new double[] {0.104, side.negateIfRightSide(0.347), side.negateIfRightSide(-0.094), -1.2});
 
-         HOME_CYCLOID_7DOF.put(side, new double[]{0.018, side.negateIfRightSide(0.146), side.negateIfRightSide(-0.202), -0.312, side.negateIfRightSide(0.0), side.negateIfRightSide(0.0), side.negateIfRightSide(0.0)});
-         STAND_PREP_CYCLOID_7DOF.put(side, HOME_CYCLOID_7DOF.get(side));
-         TUCKED_UP_ARMS_CYCLOID_7DOF.put(side, new double[]{0.104, side.negateIfRightSide(0.347), side.negateIfRightSide(-0.094), -1.2, side.negateIfRightSide(0.0), side.negateIfRightSide(0.0), side.negateIfRightSide(0.0)});
+         HOME_7DOF.put(side, new double[]{0.018, side.negateIfRightSide(0.146), side.negateIfRightSide(-0.202), -0.312, side.negateIfRightSide(0.0), side.negateIfRightSide(0.0), side.negateIfRightSide(0.0)});
+         STAND_PREP_7DOF.put(side, HOME_7DOF.get(side));
+         TUCKED_UP_ARMS_7DOF.put(side, new double[]{0.104, side.negateIfRightSide(0.347), side.negateIfRightSide(-0.094), -1.2, side.negateIfRightSide(0.0), side.negateIfRightSide(0.0), side.negateIfRightSide(0.0)});
       }
    }
 
@@ -42,12 +40,12 @@ public class AlexanderPresetArmConfigurations
 
       if (robotVersion.getJointMap().hasCycloidForearm(side))
       {
-         jointAngles = new double[HOME_CYCLOID_7DOF.get(RobotSide.LEFT).length];
+         jointAngles = new double[HOME_7DOF.get(RobotSide.LEFT).length];
          getCycloid7DoFArmConfigurations(side, presetArmConfiguration, jointAngles);
       }
       else
       {
-         jointAngles = new double[HOME_CYCLOID_4DOF.get(RobotSide.LEFT).length];
+         jointAngles = new double[HOME_4DOF.get(RobotSide.LEFT).length];
          getCycloid4DoFArmConfigurations(side, presetArmConfiguration, jointAngles);
       }
 
@@ -58,9 +56,9 @@ public class AlexanderPresetArmConfigurations
    {
       switch (presetArmConfiguration)
       {
-         case STAND_PREP -> System.arraycopy(STAND_PREP_CYCLOID_4DOF.get(side), 0, jointAnglesToPack, 0, STAND_PREP_CYCLOID_4DOF.get(side).length);
-         case HOME -> System.arraycopy(HOME_CYCLOID_4DOF.get(side), 0, jointAnglesToPack, 0, HOME_CYCLOID_4DOF.get(side).length);
-         case TUCKED_UP_ARMS -> System.arraycopy(TUCKED_UP_ARMS_CYCLOID_4DOF.get(side), 0, jointAnglesToPack, 0, TUCKED_UP_ARMS_CYCLOID_4DOF.get(side).length);
+         case STAND_PREP -> System.arraycopy(STAND_PREP_4DOF.get(side), 0, jointAnglesToPack, 0, STAND_PREP_4DOF.get(side).length);
+         case HOME -> System.arraycopy(HOME_4DOF.get(side), 0, jointAnglesToPack, 0, HOME_4DOF.get(side).length);
+         case TUCKED_UP_ARMS -> System.arraycopy(TUCKED_UP_ARMS_4DOF.get(side), 0, jointAnglesToPack, 0, TUCKED_UP_ARMS_4DOF.get(side).length);
       }
    }
 
@@ -68,9 +66,9 @@ public class AlexanderPresetArmConfigurations
    {
       switch (presetArmConfiguration)
       {
-         case STAND_PREP -> System.arraycopy(STAND_PREP_CYCLOID_7DOF.get(side), 0, jointAnglesToPack, 0, STAND_PREP_CYCLOID_7DOF.get(side).length);
-         case HOME -> System.arraycopy(HOME_CYCLOID_7DOF.get(side), 0, jointAnglesToPack, 0, HOME_CYCLOID_7DOF.get(side).length);
-         case TUCKED_UP_ARMS -> System.arraycopy(TUCKED_UP_ARMS_CYCLOID_7DOF.get(side), 0, jointAnglesToPack, 0, TUCKED_UP_ARMS_CYCLOID_7DOF.get(side).length);
+         case STAND_PREP -> System.arraycopy(STAND_PREP_7DOF.get(side), 0, jointAnglesToPack, 0, STAND_PREP_7DOF.get(side).length);
+         case HOME -> System.arraycopy(HOME_7DOF.get(side), 0, jointAnglesToPack, 0, HOME_7DOF.get(side).length);
+         case TUCKED_UP_ARMS -> System.arraycopy(TUCKED_UP_ARMS_7DOF.get(side), 0, jointAnglesToPack, 0, TUCKED_UP_ARMS_7DOF.get(side).length);
       }
    }
 }
