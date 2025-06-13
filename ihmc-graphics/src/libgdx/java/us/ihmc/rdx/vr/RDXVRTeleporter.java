3bd5d293efc2354465ea858f2630acea7dd57e6a
Stephen McCrory <smccrory1024@gmail.com>
2025-06-13 17:06:49 -0500

    Selectively disabling teleportation modes

--- ihmc-graphics/src/libgdx/java/us/ihmc/rdx/vr/RDXVRTeleporter.java
+++ ihmc-graphics/src/libgdx/java/us/ihmc/rdx/vr/RDXVRTeleporter.java
@@ -32,7 +32,9 @@ public class RDXVRTeleporter
    private final RigidBodyTransform xyYawHeadsetToTeleportTransform = new RigidBodyTransform();
    private final Color color = Color.WHITE;
    private double lastTouchpadY = Double.NaN;
-   private boolean enabled = true;
+   private boolean bButtonEnabled = true;
+   private boolean joysticButtonEnabled = true;
+   private boolean touchpadHeightEnabled = true;
 
    private ReferenceFrame linkBeforeCameraReferenceFrame;
    // Reference frame of cameras to be set from syncedRobot (used for teleporting vr viewPoint to robot position)
@@ -67,66 +69,83 @@ public class RDXVRTeleporter
          InputDigitalActionData bButton = controller.getBButtonActionData();
 
          // Only enable teleportation when nothing is pick selected
-         if (enabled && controller.getSelectedPick() == null && !controller.anythingElseBeingDragged(this))
+         if ((bButtonEnabled || joysticButtonEnabled || touchpadHeightEnabled) && controller.getSelectedPick() == null && !controller.anythingElseBeingDragged(this))
          {
             controller.setBButtonText("Teleport");
             InputDigitalActionData joystickButton = controller.getJoystickPressActionData();
 
-            if (bButton.bChanged() && bButton.bState()) // Pressed B button
+            if (bButtonEnabled)
             {
-               preparingToTeleport = true;
-            }
-            else if (preparingToTeleport && bButton.bChanged() && !bButton.bState())
-            {
-               vrContext.teleport(teleportIHMCZUpToIHMCZUpWorld ->
+               if (bButton.bChanged() && bButton.bState()) // Pressed B button
                {
-                  xyYawHeadsetToTeleportTransform.setIdentity();
-                  vrContext.getHeadset().runIfConnected(headset -> // Teleport such that your headset ends up where you're trying to go
-                  {
-                     headset.getXForwardZUpHeadsetFrame().getTransformToDesiredFrame(xyYawHeadsetToTeleportTransform, vrContext.getTeleportFrameIHMCZUp());
-                     xyYawHeadsetToTeleportTransform.getTranslation().setZ(0.0);
-                     xyYawHeadsetToTeleportTransform.getRotation().setYawPitchRoll(xyYawHeadsetToTeleportTransform.getRotation().getYaw(), 0.0, 0.0);
-                  });
-                  teleportIHMCZUpToIHMCZUpWorld.set(xyYawHeadsetToTeleportTransform);
-                  teleportIHMCZUpToIHMCZUpWorld.invert();
-                  proposedTeleportPose.get(tempTransform);
-                  tempTransform.transform(teleportIHMCZUpToIHMCZUpWorld);
-               });
-            }
-
-            // Pressed right joystick button
-            if (linkBeforeCameraReferenceFrame != null && controller.getJoystickIsCentered() && joystickButton.bChanged() && !joystickButton.bState())
-            {
-               snapToVRHome(vrContext);
-            }
-            else if (preparingToTeleport) // Holding B button
-            {
-               proposedTeleportPose.set(pickPlaneYawCalculator.calculate(controller.getPickPoseFrame(), vrContext.getTeleportFrameIHMCZUp()));
-
-               controller.setPickRayColliding(controller.getPickPointPose().getPosition().distance(proposedTeleportPose.getPosition()));
-
-               proposedTeleportPose.get(tempTransform);
-               LibGDXTools.toLibGDX(tempTransform, ring.transform);
-               LibGDXTools.toLibGDX(tempTransform, arrow.transform);
-            }
-
-            if (controller.getTouchpadTouchedActionData().bState())
-            {
-               double y = controller.getTouchpadActionData().y();
-               if (!Double.isNaN(lastTouchpadY))
-               {
-                  double delta = y - lastTouchpadY;
-                  vrContext.teleport(teleportIHMCZUpToIHMCZUpWorld -> teleportIHMCZUpToIHMCZUpWorld.getTranslation().addZ(delta * 0.3));
+                  preparingToTeleport = true;
                }
-               lastTouchpadY = y;
+               else if (preparingToTeleport && bButton.bChanged() && !bButton.bState())
+               {
+                  vrContext.teleport(teleportIHMCZUpToIHMCZUpWorld ->
+                                     {
+                                        xyYawHeadsetToTeleportTransform.setIdentity();
+                                        vrContext.getHeadset().runIfConnected(headset -> // Teleport such that your headset ends up where you're trying to go
+                                                                              {
+                                                                                 headset.getXForwardZUpHeadsetFrame()
+                                                                                        .getTransformToDesiredFrame(xyYawHeadsetToTeleportTransform, vrContext.getTeleportFrameIHMCZUp());
+                                                                                 xyYawHeadsetToTeleportTransform.getTranslation().setZ(0.0);
+                                                                                 xyYawHeadsetToTeleportTransform.getRotation().setYawPitchRoll(xyYawHeadsetToTeleportTransform.getRotation().getYaw(), 0.0, 0.0);
+                                                                              });
+                                        teleportIHMCZUpToIHMCZUpWorld.set(xyYawHeadsetToTeleportTransform);
+                                        teleportIHMCZUpToIHMCZUpWorld.invert();
+                                        proposedTeleportPose.get(tempTransform);
+                                        tempTransform.transform(teleportIHMCZUpToIHMCZUpWorld);
+                                     });
+               }
             }
-            else
+
+            if (joysticButtonEnabled)
             {
-               lastTouchpadY = Double.NaN;
+               // Pressed right joystick button
+               if (linkBeforeCameraReferenceFrame != null && controller.getJoystickIsCentered() && joystickButton.bChanged() && !joystickButton.bState())
+               {
+                  snapToVRHome(vrContext);
+               }
+               else if (preparingToTeleport) // Holding B button
+               {
+                  proposedTeleportPose.set(pickPlaneYawCalculator.calculate(controller.getPickPoseFrame(), vrContext.getTeleportFrameIHMCZUp()));
+
+                  controller.setPickRayColliding(controller.getPickPointPose().getPosition().distance(proposedTeleportPose.getPosition()));
+
+                  proposedTeleportPose.get(tempTransform);
+                  LibGDXTools.toLibGDX(tempTransform, ring.transform);
+                  LibGDXTools.toLibGDX(tempTransform, arrow.transform);
+               }
+            }
+
+            if (touchpadHeightEnabled)
+            {
+               if (controller.getTouchpadTouchedActionData().bState())
+               {
+                  double y = controller.getTouchpadActionData().y();
+                  if (!Double.isNaN(lastTouchpadY))
+                  {
+                     double delta = y - lastTouchpadY;
+                     vrContext.teleport(teleportIHMCZUpToIHMCZUpWorld -> teleportIHMCZUpToIHMCZUpWorld.getTranslation().addZ(delta * 0.3));
+                  }
+                  lastTouchpadY = y;
+               }
+               else
+               {
+                  lastTouchpadY = Double.NaN;
+               }
             }
          }
 
-         if (!bButton.bState())
+         if (bButtonEnabled)
+         {
+            if (!bButton.bState())
+            {
+               preparingToTeleport = false;
+            }
+         }
+         else
          {
             preparingToTeleport = false;
          }
@@ -213,8 +232,18 @@ public class RDXVRTeleporter
       return robotVRHomeReferenceFrame;
    }
 
-   public void setEnabled(boolean enabled)
+   public void setBButtonEnabled(boolean bButtonEnabled)
    {
-      this.enabled = enabled;
+      this.bButtonEnabled = bButtonEnabled;
+   }
+
+   public void setJoysticButtonEnabled(boolean joysticButtonEnabled)
+   {
+      this.joysticButtonEnabled = joysticButtonEnabled;
+   }
+
+   public void setTouchpadHeightEnabled(boolean touchpadHeightEnabled)
+   {
+      this.touchpadHeightEnabled = touchpadHeightEnabled;
    }
 }
