8173ad71869885c12ceeef16cd44c316592caf53
Ben Pratt <c.ben.pratt@gmail.com>
2025-06-16 13:01:40 -0500

    Implemented a few basic functions as well as setting the hand to the right spot at the beginning

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/psyonicAbilityHand/ROS2AbilityHand.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/psyonicAbilityHand/ROS2AbilityHand.java
@@ -9,12 +9,12 @@ public class ROS2AbilityHand implements AbilityHand
 {
    private final RobotSide handSide;
    private AHWrapper wrapperHand;
-   private float indexFingerPosition;
-   private float middleFingerPosition;
-   private float ringFingerPosition;
-   private float pinkyFingerPosition;
-   private float thumbFlexorPosition;
-   private float thumbRotatorPosition;
+   private float indexFingerPosition = 30.0f;
+   private float middleFingerPosition = 30.0f;
+   private float ringFingerPosition = 30.0f;
+   private float pinkyFingerPosition = 30.0f;
+   private float thumbFlexorPosition = 30.0f;
+   private float thumbRotatorPosition = -30.0f;
 
    private final FloatArray6 command = new FloatArray6();
 
@@ -34,6 +34,13 @@ public class ROS2AbilityHand implements AbilityHand
       {
          wrapperHand = new AHWrapper((byte) 0x51, 1000000);
       }
+      // Sets hand to starting position
+      for(int i = 0; i <5; i++)
+      {
+         command.put(i, 30.0f);
+      }
+      command.put(5, -30.0f);
+      wrapperHand.read_write_once(command, abilityhand.POSITION, (byte) 0);
    }
 
    @Override
@@ -45,49 +52,58 @@ public class ROS2AbilityHand implements AbilityHand
       command.put(3, pinkyFingerPosition);
       command.put(4, thumbFlexorPosition);
       command.put(5, thumbRotatorPosition);
+      if(command.get(5) > 0.0f)
+      {
+         command.put(5, -command.get(5));
+      }
       wrapperHand.read_write_once(command, abilityhand.POSITION, (byte) 0);
+//      wrapperHand.write_once(command, abilityhand.POSITION, (byte) 0);
+//      wrapperHand.read_once((byte) 0);
    }
 
    @Override
    public void setIndexFingerPositionCommand(float indexFingerPositionCommand)
    {
-      command.put(0, indexFingerPositionCommand);
       indexFingerPosition = indexFingerPositionCommand;
    }
 
    @Override
    public void setMiddleFingerPositionCommand(float middleFingerPositionCommand)
    {
-      command.put(1, middleFingerPositionCommand);
       middleFingerPosition = middleFingerPositionCommand;
    }
 
    @Override
    public void setRingFingerPositionCommand(float ringFingerPositionCommand)
    {
-      command.put(2, ringFingerPositionCommand);
       ringFingerPosition = ringFingerPositionCommand;
    }
 
    @Override
    public void setPinkyFingerPositionCommand(float pinkyFingerPositionCommand)
    {
-      command.put(3, pinkyFingerPositionCommand);
       pinkyFingerPosition = pinkyFingerPositionCommand;
    }
 
    @Override
    public void setThumbFlexorPositionCommand(float thumbFlexorPositionCommand)
    {
-      command.put(4, thumbFlexorPositionCommand);
       thumbFlexorPosition = thumbFlexorPositionCommand;
    }
 
    @Override
    public void setThumbRotatorPositionCommand(float thumbRotatorPositionCommand)
    {
-      command.put(5, thumbRotatorPositionCommand);
       thumbRotatorPosition = thumbRotatorPositionCommand;
+   }
+   public void setAllFingers(float fingerPosition)
+   {
+      indexFingerPosition = fingerPosition;
+      middleFingerPosition = fingerPosition;
+      ringFingerPosition = fingerPosition;
+      pinkyFingerPosition = fingerPosition;
+      thumbFlexorPosition = fingerPosition;
+      thumbRotatorPosition = -fingerPosition;
    }
 
    @Override
@@ -125,4 +141,10 @@ public class ROS2AbilityHand implements AbilityHand
    {
       return thumbRotatorPosition;
    }
+
+   public float[] getAllFingerPositions()
+   {
+      return new float[]{indexFingerPosition, middleFingerPosition, ringFingerPosition, pinkyFingerPosition, thumbFlexorPosition, thumbRotatorPosition};
+   }
+
 }
