5adc31916df865d7ab83ac50e619328e1cd9115b
LuigiPenco <161648875+LuigiPenco93@users.noreply.github.com>
2025-06-16 17:43:47 -0500

    hand graphic transform fix and tuning (#861)
    * hand graphic transform fix and tuning
    
    * added common yovariables
    
    * new stand prep values for alexander
    
    * revert pelvis  height change
    
    * fixing tests
    
    * fixed push backward test
    
    * diable hand bearing test alexander
    
    * disable hand load bearing test alexander

--- open-alexander/src/main/java/us/ihmc/alexander/parameters/controller/AlexanderStandPrepSetPoints.java
+++ open-alexander/src/main/java/us/ihmc/alexander/parameters/controller/AlexanderStandPrepSetPoints.java
@@ -31,11 +31,11 @@ public class AlexanderStandPrepSetPoints implements WholeBodySetpointParameters
          setPoints.put(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_PITCH), -0.375);
          setPoints.put(jointMap.getLegJointName(robotSide, LegJointName.ANKLE_ROLL), robotSide.negateIfLeftSide(0.05));
 
-         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), 0.0);
-         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), 0.0);
-         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), -0.525);
-         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), 1.0);
-         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_YAW), -0.45);
+         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), robotSide.negateIfRightSide(0.146));
+         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), robotSide.negateIfRightSide(-0.202));
+         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), 0.018);
+         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), -0.312);
+         setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_YAW), 0.0);
          setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.WRIST_ROLL), 0.0);
          setPoints.put(jointMap.getArmJointName(robotSide, ArmJointName.WRIST_YAW), 0.0);
       }
--- open-alexander/src/main/java/us/ihmc/alexander/parameters/controller/OpenAlexanderWalkingControllerParameters.java
+++ open-alexander/src/main/java/us/ihmc/alexander/parameters/controller/OpenAlexanderWalkingControllerParameters.java
@@ -23,6 +23,7 @@ import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.
 import us.ihmc.commonWalkingControlModules.momentumBasedController.optimization.OneDoFJointPrivilegedConfigurationParameters;
 import us.ihmc.commonWalkingControlModules.sensors.footSwitch.WrenchBasedFootSwitchFactory;
 import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.log.LogTools;
 import us.ihmc.robotics.controllers.pidGains.GainCoupling;
 import us.ihmc.robotics.controllers.pidGains.PID3DGains;
 import us.ihmc.robotics.controllers.pidGains.PIDGainsReadOnly;
@@ -568,10 +569,10 @@ public class OpenAlexanderWalkingControllerParameters extends WalkingControllerP
 
       for (RobotSide robotSide : RobotSide.values)
       {
-         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), 0.6);
-         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), robotSide.negateIfRightSide(0.3));
-         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), robotSide.negateIfLeftSide(0.6));
-         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), -1.3);
+         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH), 0.018);
+         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL), robotSide.negateIfRightSide(0.146));
+         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.SHOULDER_YAW), robotSide.negateIfRightSide(-0.202));
+         jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_PITCH), -0.312);
          jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.ELBOW_YAW), 0.0);
          jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.WRIST_ROLL), 0.0);
          jointHomeConfiguration.put(jointMap.getArmJointName(robotSide, ArmJointName.WRIST_YAW), 0.0);
@@ -598,15 +599,15 @@ public class OpenAlexanderWalkingControllerParameters extends WalkingControllerP
 
          if (version.getJointMap().hasCycloidForearm(robotSide))
          { // Cycloid forearm home pose
-            handPoseInChestBodyFrame.getPosition().set(0.304, robotSide.negateIfRightSide(0.397), -0.553);
+            handPoseInChestBodyFrame.getPosition().set(0.111, robotSide.negateIfRightSide(0.338), -0.641);
             handPoseInChestBodyFrame.getOrientation()
-                                    .set(robotSide.negateIfRightSide(0.051), robotSide.negateIfRightSide(-0.446), robotSide.negateIfRightSide(-0.103), 0.887);
+                                    .set(robotSide.negateIfRightSide(0.056), -0.140, robotSide.negateIfRightSide(-0.105), 0.983);
          }
          else
          { // 4-dof cycloid home pose
-            handPoseInChestBodyFrame.getPosition().set(0.323, robotSide.negateIfRightSide(0.396), -0.565);
+            handPoseInChestBodyFrame.getPosition().set(0.125, robotSide.negateIfRightSide(0.342), -0.695);
             handPoseInChestBodyFrame.getOrientation()
-                                    .set(robotSide.negateIfRightSide(0.051), robotSide.negateIfRightSide(0.446), robotSide.negateIfRightSide(-0.103), 0.887);
+                                    .set(robotSide.negateIfRightSide(0.056), -0.140, robotSide.negateIfRightSide(-0.105), 0.983);
          }
 
          bodyHomeConfiguration.put(jointMap.getHandName(robotSide), handPoseInChestBodyFrame);
