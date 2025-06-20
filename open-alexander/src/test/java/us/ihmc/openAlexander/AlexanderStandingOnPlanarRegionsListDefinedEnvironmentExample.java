edfb9decd3a3ae3b9cd1041fa62eb631fc54e6d4
Stefan Fasano <sfasano@ihmc.org>
2025-06-20 15:19:57 -0500

    renamed test package from alexander to openAlexander

---
+++ open-alexander/src/test/java/us/ihmc/openAlexander/AlexanderStandingOnPlanarRegionsListDefinedEnvironmentExample.java
@@ -0,0 +1,28 @@
+package us.ihmc.openAlexander;
+
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.simulationStarter.DRCSimulationStarter;
+import us.ihmc.avatar.simulationStarter.DRCSimulationTools;
+import us.ihmc.openAlexander.OpenAlexanderRobotModel;
+import us.ihmc.openAlexander.OpenAlexanderVersion;
+import us.ihmc.robotics.geometry.PlanarRegionsListGenerator;
+import us.ihmc.simulationConstructionSetTools.util.environments.PlanarRegionsListDefinedEnvironment;
+
+public class AlexanderStandingOnPlanarRegionsListDefinedEnvironmentExample
+{
+   public static void main(String[] args)
+   {
+      OpenAlexanderRobotModel robotModel = new OpenAlexanderRobotModel(OpenAlexanderVersion.V0_FULL_ROBOT, RobotTarget.SCS);
+
+      PlanarRegionsListGenerator generator = new PlanarRegionsListGenerator();
+
+      generator.addRectangle(20, 20);
+
+      PlanarRegionsListDefinedEnvironment environment = new PlanarRegionsListDefinedEnvironment("FlatPlanarRegionsList", generator.getPlanarRegionsList(), 0.005, false);
+
+      DRCSimulationStarter simulationStarter = new DRCSimulationStarter(robotModel, environment);
+      simulationStarter.setRunMultiThreaded(true);
+      simulationStarter.setInitializeEstimatorToActual(true);
+      DRCSimulationTools.startSimulationWithGraphicSelector(simulationStarter, null, null);
+   }
+}
---
+++ open-alexander/src/test/java/us/ihmc/openAlexander/roughTerrainWalking/AlexanderSwingOverPlanarRegionsTest.java
@@ -0,0 +1,33 @@
+package us.ihmc.openAlexander.roughTerrainWalking;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import us.ihmc.openAlexander.OpenAlexanderVersion;
+import us.ihmc.openAlexander.OpenAlexanderRobotModel;
+import us.ihmc.avatar.drcRobot.DRCRobotModel;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.roughTerrainWalking.AvatarSwingOverPlanarRegionsTest;
+import us.ihmc.simulationConstructionSetTools.tools.CITools;
+
+@Tag("humanoid-rough-terrain")
+public class AlexanderSwingOverPlanarRegionsTest extends AvatarSwingOverPlanarRegionsTest
+{
+   @Override
+   @Test
+   public void testSwingOverPlanarRegions()
+   {
+      super.testSwingOverPlanarRegions();
+   }
+
+   @Override
+   public DRCRobotModel getRobotModel()
+   {
+      return new OpenAlexanderRobotModel(OpenAlexanderVersion.V0_FULL_ROBOT, RobotTarget.SCS);
+   }
+
+   @Override
+   public String getSimpleRobotName()
+   {
+      return CITools.getSimpleRobotNameFor(CITools.SimpleRobotNameKeys.ALEXANDER);
+   }
+}
