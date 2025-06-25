a1a6f38d37883b9847200e8dd4af764233761144
bpratt <c.ben.pratt@gmail.com>
2025-06-25 15:19:01 -0500

    Sets up two panels for the different hands

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/RDXHandManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/teleoperation/RDXHandManager.java
@@ -2,6 +2,7 @@ package us.ihmc.rdx.ui.teleoperation;
 
 import imgui.ImGui;
 import us.ihmc.psyonicros2.AbilityHandHardwareCommunication;
+import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.rdx.ui.interactable.RDXAbilityHand;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
@@ -12,13 +13,22 @@ public class RDXHandManager
 
    private final SideDependentList<RDXAbilityHand> hands = new SideDependentList<>();
 
-   public void create()
+   public void create(RDXBaseUI baseUI)
    {
       communication.start();
       for (RobotSide side : RobotSide.values)
       {
-         hands.put(side, new RDXAbilityHand(side, communication));
+         hands.put(side, new RDXAbilityHand(side, communication, baseUI));
+         baseUI.getImGuiPanelManager().addPanel(
+               "Individual Finger Control " + side.toString(), () -> {
+                  ImGui.begin("Individual Finger Control " + side.toString());
+                  hands.get(side).renderIndividualFingerControls();
+                  ImGui.end();
+               }
+         );
       }
+
+
    }
 
    public void update()
