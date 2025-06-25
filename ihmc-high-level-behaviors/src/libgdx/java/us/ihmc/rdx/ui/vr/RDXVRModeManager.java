2c9d01ff35ecded6013c1e9d6daf6f634a20c1d9
bpratt <c.ben.pratt@gmail.com>
2025-06-25 11:02:21 -0500

    Uncomments some stuff

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/vr/RDXVRModeManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/vr/RDXVRModeManager.java
@@ -90,7 +90,7 @@ public class RDXVRModeManager
          if (panel instanceof RDXTeleoperationManager teleoperationPanel)
          {
             interactablesEnabled = teleoperationPanel.getInteractablesEnabled();
-//            handManager = teleoperationPanel.getArmManager().getHandConfigurationManager();
+            handManager = teleoperationPanel.getArmManager().getHandConfigurationManager();
             break;
          }
       }
