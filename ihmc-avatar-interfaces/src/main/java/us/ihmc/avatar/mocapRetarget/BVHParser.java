992c05a2675f4356a49e5f0a5b2f1f2569e9fb14
Gabrielle Walrath <gwalrath@gmail.com>
2025-07-22 17:39:30 -0500

    Setting up mocap retargeting framework

---
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/mocapRetarget/BVHParser.java
@@ -0,0 +1,9 @@
+package us.ihmc.avatar.mocapRetarget;
+
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.commonWalkingControlModules.controllerCore.WholeBodyInverseKinematicsSolver;
+import us.ihmc.communication.packetCommunicator.PacketCommunicator;
+
+public class BVHParser
+{
+}
