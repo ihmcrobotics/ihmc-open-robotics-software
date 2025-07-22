c73127811d3c6951b9e33d0ec067be5b2b7322c5
Duncan Calvert <dcalvert@ihmc.org>
2025-07-22 18:18:34 -0500

    Setup lerobot UI.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/lerobot/RDXLeRobotOperation.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/lerobot/RDXLeRobotOperation.java
@@ -7,9 +7,12 @@ import us.ihmc.commons.thread.Throttler;
 import us.ihmc.commons.thread.TypedNotification;
 import us.ihmc.communication.crdt.CRDTBidirectionalBoolean;
 import us.ihmc.communication.crdt.CRDTInfo;
+import us.ihmc.communication.crdt.CRDTStatusDouble;
+import us.ihmc.communication.crdt.CRDTStatusLong;
 import us.ihmc.communication.crdt.LatestTimestampModifiable;
 import us.ihmc.communication.ros2.ROS2ActorDesignation;
 import us.ihmc.communication.ros2.sync.ROS2PeerClockOffsetEstimator;
+import us.ihmc.rdx.imgui.ImGuiAveragedFrequencyText;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 
 import static us.ihmc.perception.lerobot.LeRobotInferenceUpdateThread.LEROBOT_UI;
@@ -21,7 +24,12 @@ public class RDXLeRobotOperation
    private final TypedNotification<LerobotInferenceOperationMessage> statusSubscription;
    private final Throttler commandThrottler = new Throttler().setFrequency(30.0);
    private final LatestTimestampModifiable latestTimestampModifiable;
+   private final CRDTStatusLong sequenceID;
    private final CRDTBidirectionalBoolean running;
+   private final CRDTBidirectionalBoolean controlRobot;
+   private final CRDTStatusDouble pythonStatusFrequency;
+   private final CRDTStatusLong receivedActions;
+   private final ImGuiAveragedFrequencyText commsFrequencyText = new ImGuiAveragedFrequencyText();
 
    public RDXLeRobotOperation(CommunicationHelper communicationHelper, ROS2PeerClockOffsetEstimator peerClockEstimator)
    {
@@ -30,7 +38,11 @@ public class RDXLeRobotOperation
       statusSubscription = communicationHelper.subscribeViaTypedNotification(LEROBOT_UI.getTopic(ROS2ActorDesignation.OPERATOR.getIncomingQualifier()));
 
       latestTimestampModifiable = new LatestTimestampModifiable(new CRDTInfo(ROS2ActorDesignation.OPERATOR, peerClockEstimator));
+      sequenceID = new CRDTStatusLong(ROS2ActorDesignation.ROBOT, latestTimestampModifiable.getCRDTInfo(), 0L);
       running = new CRDTBidirectionalBoolean(latestTimestampModifiable, false);
+      controlRobot = new CRDTBidirectionalBoolean(latestTimestampModifiable, false);
+      pythonStatusFrequency = new CRDTStatusDouble(ROS2ActorDesignation.ROBOT, latestTimestampModifiable.getCRDTInfo(), 0.0);
+      receivedActions = new CRDTStatusLong(ROS2ActorDesignation.ROBOT, latestTimestampModifiable.getCRDTInfo(), 0L);
       // TODO Implement rest of sync data comms
    }
 
@@ -38,21 +50,28 @@ public class RDXLeRobotOperation
    {
       if (statusSubscription.poll())
       {
+         commsFrequencyText.ping();
          LerobotInferenceOperationMessage status = statusSubscription.read();
          latestTimestampModifiable.fromMessage(status.getLatestTimestampModifiable());
          running.fromMessage(status.getRunning());
       }
 
-      if (ImGui.checkbox(labels.get("Run Lerobot inference"), running.getValue()))
-      {
+      ImGui.text("LeRobot: Comms: %s   Thread: %3d Hz   Actions: %d".formatted(commsFrequencyText.getText(),
+                                                                           (int) pythonStatusFrequency.getValue(),
+                                                                           receivedActions.getValue()));
+      if (ImGui.checkbox(labels.get("Run inference & preview"), running.getValue()))
          running.setValue(!running.getValue());
-      }
+      if (ImGui.checkbox(labels.get("Control robot"), controlRobot.getValue()))
+         controlRobot.setValue(!controlRobot.getValue());
+
+      ImGui.separator();
 
       if (commandThrottler.run())
       {
          LerobotInferenceOperationMessage command = new LerobotInferenceOperationMessage();
          latestTimestampModifiable.toMessage(command.getLatestTimestampModifiable());
          command.setRunning(running.toMessage());
+         command.setControlRobot(controlRobot.toMessage());
          communicationHelper.publish(LEROBOT_UI.getTopic(ROS2ActorDesignation.OPERATOR.getOutgoingQualifier()), command);
       }
    }
