c2bf1312bf578e9c627133dbe9b8985e417f9307
bpratt <c.ben.pratt@gmail.com>
2025-07-28 09:06:25 -0500

    Kinematic Streaming activation from LeRobot area

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXArmManager.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/affordances/RDXArmManager.java
@@ -128,7 +128,7 @@ public class RDXArmManager
 
       handManager = new RDXHandConfigurationManager();
       if (peerClockEstimator != null)
-         leRobotOperation = new RDXLeRobotOperation(communicationHelper.getROS2Node(), peerClockEstimator);
+         leRobotOperation = new RDXLeRobotOperation(communicationHelper.getROS2Node(), peerClockEstimator, syncedRobot);
    }
 
    public void create(RDXBaseUI baseUI)
--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/lerobot/RDXLeRobotOperation.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/lerobot/RDXLeRobotOperation.java
@@ -5,16 +5,25 @@ import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 import imgui.ImGui;
+import imgui.type.ImBoolean;
 import std_msgs.msg.dds.Float32MultiArray;
+import toolbox_msgs.msg.dds.KinematicsStreamingToolboxConfigurationMessage;
+import toolbox_msgs.msg.dds.KinematicsStreamingToolboxInitialConfigurationMessage;
+import toolbox_msgs.msg.dds.ToolboxStateMessage;
+import us.ihmc.avatar.drcRobot.ROS2SyncedRobotModel;
+import us.ihmc.avatar.networkProcessor.kinematicsStreamingToolboxModule.KinematicsStreamingToolboxModule;
+import us.ihmc.avatar.networkProcessor.kinematicsStreamingToolboxModule.KinematicsStreamingToolboxParameters;
 import us.ihmc.commons.thread.Throttler;
 import us.ihmc.commons.thread.TypedNotification;
 import us.ihmc.communication.ROS2Tools;
 import us.ihmc.communication.crdt.CRDTBidirectionalBoolean;
 import us.ihmc.communication.crdt.CRDTInfo;
 import us.ihmc.communication.crdt.LatestTimestampModifiable;
+import us.ihmc.communication.packets.ToolboxState;
 import us.ihmc.communication.ros2.ROS2ActorDesignation;
 import us.ihmc.communication.ros2.sync.ROS2PeerClockOffsetEstimator;
 import us.ihmc.euclid.geometry.Pose3D;
+import us.ihmc.humanoidRobotics.communication.packets.KinematicsToolboxMessageFactory;
 import us.ihmc.idl.IDLSequence;
 import us.ihmc.perception.lerobot.LeRobotInferenceManager;
 import us.ihmc.perception.lerobot.LeRobotInferenceUpdateThread;
@@ -23,6 +32,7 @@ import us.ihmc.rdx.imgui.ImGuiTools;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.ui.RDXBaseUI;
 import us.ihmc.rdx.ui.graphics.RDXReferenceFrameGraphic;
+import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.ros2.ROS2Node;
@@ -47,11 +57,28 @@ public class RDXLeRobotOperation
    private final ROS2Publisher<LerobotInferenceOperationMessage> commandPublisher;
    private final ImGuiAveragedFrequencyText commsFrequencyText = new ImGuiAveragedFrequencyText();
 
+   private final ImBoolean isKSTEnabled = new ImBoolean(false);
+   private final ImBoolean controlArmsOnly = new ImBoolean(false);
+
+   private final ROS2Publisher<KinematicsStreamingToolboxConfigurationMessage> kstConfigPublisher;
+   private final ROS2Publisher<KinematicsStreamingToolboxInitialConfigurationMessage> kstInitialConfigPublisher;
+   private final ROS2Publisher<ToolboxStateMessage> toolboxStatePublisher;
+   private final KinematicsStreamingToolboxParameters kstParameters;
+   private final ROS2SyncedRobotModel syncedRobotModel;
+
    private final SideDependentList<RDXReferenceFrameGraphic> actionHandPoseGraphics = new SideDependentList<>();
    private final Pose3D handPose = new Pose3D();
 
-   public RDXLeRobotOperation(ROS2Node ros2Node, ROS2PeerClockOffsetEstimator peerClockEstimator)
+   public RDXLeRobotOperation(ROS2Node ros2Node, ROS2PeerClockOffsetEstimator peerClockEstimator, ROS2SyncedRobotModel syncedRobot)
    {
+      this.syncedRobotModel = syncedRobot;
+      this.kstParameters = new KinematicsStreamingToolboxParameters();
+      this.kstConfigPublisher = ros2Node.createPublisher(KinematicsStreamingToolboxModule.getInputStreamingConfigurationTopic(syncedRobot.getRobotModel()
+                                                                                                                                              .getSimpleRobotName()));
+      this.kstInitialConfigPublisher = ros2Node.createPublisher(KinematicsStreamingToolboxModule.getInputStreamingInitialConfigurationTopic(syncedRobot.getRobotModel()
+                                                                                                                                                            .getSimpleRobotName()));
+      this.toolboxStatePublisher = ros2Node.createPublisher(KinematicsStreamingToolboxModule.getInputStateTopic(syncedRobot.getRobotModel()
+                                                                                                                                .getSimpleRobotName()));
       latestTimestampModifiable = new LatestTimestampModifiable(new CRDTInfo(ROS2ActorDesignation.OPERATOR, peerClockEstimator));
       running = new CRDTBidirectionalBoolean(latestTimestampModifiable, false);
       controlRobot = new CRDTBidirectionalBoolean(latestTimestampModifiable, false);
@@ -79,16 +106,23 @@ public class RDXLeRobotOperation
          int index = 0;
          for (RobotSide side : RobotSide.values)
          {
-            handPose.getPosition().set(data.get(index++),
-                                       data.get(index++),
-                                       data.get(index++));
-            handPose.getOrientation().set(data.get(index++),
-                                          data.get(index++),
-                                          data.get(index++),
-                                          data.get(index++));
+            handPose.getPosition().set(data.get(index++), data.get(index++), data.get(index++));
+            handPose.getOrientation().set(data.get(index++), data.get(index++), data.get(index++), data.get(index++));
             actionHandPoseGraphics.get(side).setPoseInWorldFrame(handPose);
          }
       }
+
+//      if(receivedActions == 1)
+//      {
+//         isKSTEnabled.set(true);
+//         setKSTEnabled(isKSTEnabled.get());
+//         reinitializeToolboxConfiguration();
+//         controlArmsOnly.set(true);
+//         KinematicsStreamingToolboxConfigurationMessage config = kstParameters.getDefaultConfiguration();
+//         config.setLockPelvis(controlArmsOnly.get());
+//         config.setLockChest(controlArmsOnly.get());
+//         kstConfigPublisher.publish(config);
+//      }
    }
 
    public void renderImGuiWidgets()
@@ -109,6 +143,21 @@ public class RDXLeRobotOperation
          running.setValue(!running.getValue());
       if (ImGui.checkbox(labels.get("Control robot"), controlRobot.getValue()))
          controlRobot.setValue(!controlRobot.getValue());
+      if (ImGui.checkbox(labels.get("Enable Kinematic Streaming"), isKSTEnabled))
+      {
+         setKSTEnabled(isKSTEnabled.get());
+      }
+      if (ImGui.checkbox(labels.get("Control Arms Only"), controlArmsOnly))
+      {
+         KinematicsStreamingToolboxConfigurationMessage config = kstParameters.getDefaultConfiguration();
+         config.setLockPelvis(controlArmsOnly.get());
+         config.setLockChest(controlArmsOnly.get());
+         kstConfigPublisher.publish(config);
+      }
+      if (ImGui.button(labels.get("Reinitialize Toolbox Configuration")))
+      {
+         reinitializeToolboxConfiguration();
+      }
       if (ImGui.isKeyPressed(ImGuiTools.getSpaceKey()))
       {
          controlRobot.setValue(false);
@@ -125,6 +174,45 @@ public class RDXLeRobotOperation
          command.setControlRobot(controlRobot.toMessage());
          commandPublisher.publish(command);
       }
+   }
+
+   private void setKSTEnabled(boolean enabled)
+   {
+      if (enabled)
+      {
+         wakeUpToolbox();
+      }
+      else
+      {
+         sleepToolbox();
+         controlArmsOnly.set(false);
+      }
+      isKSTEnabled.set(enabled);
+   }
+
+   private void wakeUpToolbox()
+   {
+      ToolboxStateMessage msg = new ToolboxStateMessage();
+      msg.setRequestedToolboxState(ToolboxState.WAKE_UP.toByte());
+      toolboxStatePublisher.publish(msg);
+   }
+
+   private void sleepToolbox()
+   {
+      ToolboxStateMessage msg = new ToolboxStateMessage();
+      msg.setRequestedToolboxState(ToolboxState.SLEEP.toByte());
+      toolboxStatePublisher.publish(msg);
+   }
+
+   private void reinitializeToolboxConfiguration()
+   {
+      sleepToolbox();
+      KinematicsStreamingToolboxInitialConfigurationMessage initMsg = KinematicsToolboxMessageFactory.initialConfigurationFromFullRobotModel(syncedRobotModel.getFullRobotModel());
+      kstInitialConfigPublisher.publish(initMsg);
+      ToolboxStateMessage reinitMsg = new ToolboxStateMessage();
+      reinitMsg.setRequestedToolboxState(ToolboxState.REINITIALIZE.toByte());
+      toolboxStatePublisher.publish(reinitMsg);
+      wakeUpToolbox();
    }
 
    private void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool)
