a58a272f86aaff36bcdf33e7cf790e81aabe1555
bpratt <c.ben.pratt@gmail.com>
2025-07-30 13:00:26 -0500

    Adds hand side and serial number to top of gui

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/hands/psyonicAbilityHand/RDXAbilityHand.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/hands/psyonicAbilityHand/RDXAbilityHand.java
@@ -84,7 +84,7 @@ public class RDXAbilityHand implements RDXHandInterface
    public void renderImGuiWidgets()
    {
       ImGui.pushID(serialNumber);
-
+      ImGui.text(getSide().toString() + " serial number: " + serialNumber);
       if (!communication.getAvailableHandSerialNumbers().contains(serialNumber))
          return;
 
