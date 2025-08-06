677cc0e5b8d3697e5696aff9d40c9a85fe667972
ihmc-rosie <rosie@ihmc.org>
2025-08-06 11:16:58 -0500

    Import addition

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/vr/RDXVRWholeBodyKinematicStreaming.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/vr/RDXVRWholeBodyKinematicStreaming.java
@@ -17,6 +17,7 @@ import imgui.ImGui;
 import imgui.type.ImBoolean;
 import net.mgsx.gltf.scene3d.attributes.PBRTextureAttribute;
 import org.lwjgl.openvr.InputDigitalActionData;
+import toolbox_msgs.msg.dds.KinematicsStreamingToolboxConfigurationMessage;
 import toolbox_msgs.msg.dds.KinematicsStreamingToolboxContactConfigurationMessage;
 import toolbox_msgs.msg.dds.KinematicsStreamingToolboxInitialConfigurationMessage;
 import toolbox_msgs.msg.dds.KinematicsStreamingToolboxInputMessage;
