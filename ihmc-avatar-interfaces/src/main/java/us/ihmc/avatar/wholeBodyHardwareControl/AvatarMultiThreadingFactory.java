a9397583c426436c53bffb8969f604e8685e4f9f
Beomyeong <bpark@ihmc.org>
2025-08-14 17:34:38 -0500

    Still adding the quickster to the H1

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/wholeBodyHardwareControl/AvatarMultiThreadingFactory.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/wholeBodyHardwareControl/AvatarMultiThreadingFactory.java
@@ -61,6 +61,7 @@ public class AvatarMultiThreadingFactory
 {
    private static final double GRAVITY = -9.81;
    public static final boolean RUN_AUTO_DIAGNOSTIC = false;
+   public static final boolean CREATE_FAST_WALKING_CONTROLLER = false;
    private static final int ROS2_PRIORITY = 25;
 
    private final YoRegistry rootRegistry;
@@ -291,6 +292,7 @@ public class AvatarMultiThreadingFactory
    /**
     * Create High Level Controller Factory
     */
+   // Adding quicksterWalking parameters
    private HighLevelHumanoidControllerFactory createHighLevelControllerFactory(DRCRobotModel robotModel,
                                                                                RealtimeROS2Node ros2Node,
                                                                                AvatarLowLevelOutputProcessor lowLevelOutputProcessor,
@@ -345,6 +347,12 @@ public class AvatarMultiThreadingFactory
          controllerFactory.useDefaultWalkingControlState();
          controllerFactory.useDefaultDoNothingControlState();
          controllerFactory.useDefaultExitWalkingTransitionControlState(STAND_PREP_STATE);
+         
+         // setup QUICKSTER
+         if (CREATE_FAST_WALKING_CONTROLLER)
+         {
+            
+         }
 
          // setup transitions
          HighLevelControllerName fallbackControllerState = highLevelControllerParameters.getFallbackControllerState();
