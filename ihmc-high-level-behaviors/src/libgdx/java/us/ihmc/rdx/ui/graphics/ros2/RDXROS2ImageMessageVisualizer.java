03ea7c1c8b53676c9223d4d5f20fdc6d388050de
lpenco <lpenco@ihmc.org>
2025-08-15 12:11:27 -0500

    unused import

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/ros2/RDXROS2ImageMessageVisualizer.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/ros2/RDXROS2ImageMessageVisualizer.java
@@ -1,6 +1,5 @@
 package us.ihmc.rdx.ui.graphics.ros2;
 
-import imgui.ImGui;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.jetbrains.annotations.Nullable;
 import perception_msgs.msg.dds.ImageMessage;
