f968ecde3c56bf31a56f54ad842d6651954d422c
Stefan Fasano <sfasano@ihmc.org>
2025-08-18 17:22:44 -0500

    decreased max step length and width to make joystick walking less sensative

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/ContinuousStepGenerator.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/desiredFootStep/footstepGenerator/ContinuousStepGenerator.java
@@ -326,6 +326,7 @@ public class ContinuousStepGenerator implements Updatable, SCS2YoGraphicHolder
 
       double minMaxVelocityX = maxStepLength / stepTime.getValue();
       double minMaxVelocityY = maxStepWidth / stepTime.getValue();
+      double minMaxVelocityTurn = (turnMaxAngleOutward - turnMaxAngleInward) / stepTime.getValue();
 
       if (desiredVelocityProvider.isUnitVelocity())
       {
@@ -339,10 +340,10 @@ public class ContinuousStepGenerator implements Updatable, SCS2YoGraphicHolder
       }
 
       if (desiredTurningVelocityProvider.isUnitVelocity())
-      {
-         double minMaxVelocityTurn = (turnMaxAngleOutward - turnMaxAngleInward) / stepTime.getValue();
+         turningVelocity = MathTools.clamp(turningVelocity, minMaxVelocityTurn);
+      else
          turningVelocity = minMaxVelocityTurn * MathTools.clamp(turningVelocity, 1.0);
-      }
+
 
       this.desiredVelocity.set(desiredVelocityX, desiredVelocityY);
       this.desiredTurningVelocity.set(turningVelocity);
