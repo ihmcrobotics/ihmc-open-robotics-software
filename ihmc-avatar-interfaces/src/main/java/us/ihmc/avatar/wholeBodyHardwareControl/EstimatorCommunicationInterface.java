4b6dee32d98ad831cf0009abb59780bdf689f9f7
Robert <rgriffin@ihmc.org>
2025-08-21 23:15:35 -0500

    worked on the avatar estimator

---
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/wholeBodyHardwareControl/EstimatorCommunicationInterface.java
@@ -0,0 +1,37 @@
+package us.ihmc.avatar.wholeBodyHardwareControl;
+
+import us.ihmc.commonWalkingControlModules.barrierScheduler.context.HumanoidRobotContextData;
+import us.ihmc.commonWalkingControlModules.barrierScheduler.context.HumanoidRobotContextJointData;
+import us.ihmc.sensorProcessing.outputData.JointDesiredOutputListReadOnly;
+import us.ihmc.sensorProcessing.simulatedSensors.SensorDataContext;
+import us.ihmc.yoVariables.listener.YoVariableChangedListener;
+
+/**
+ * General interface for passing measured and desired robot data to and from
+ * a physical robot, either through ROS2 or directly via low-level hardware
+ * device drivers/managers.
+ *
+ * @author Stefan Fasano
+ */
+public interface EstimatorCommunicationInterface
+{
+   void read(SensorDataContext sensorDataContext);
+
+   void write(HumanoidRobotContextJointData sensorDataContext);
+
+   void start();
+
+   void stop();
+
+   void destroy();
+
+   boolean hasReceivedFirstState();
+
+   default void addSoftEStopListener(YoVariableChangedListener listener)
+   {
+   }
+
+   boolean hasRobotFaulted();
+
+   void addFaultListener(YoVariableChangedListener listener);
+}
