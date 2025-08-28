c1c1e6a0464c7d5f076718fe05cf33c09711371e
Nick Kitchel <nkitchel@ihmc.org>
2025-08-28 08:27:35 -0500

    Add another test for speed

--- ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/steppableRegions/TerrainMapToolsTest.java
+++ ihmc-footstep-planning/src/test/java/us/ihmc/footstepPlanning/steppableRegions/TerrainMapToolsTest.java
@@ -3,12 +3,16 @@ package us.ihmc.footstepPlanning.steppableRegions;
 import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Scalar;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TerrainMapToolsTest
 {
+   private final static float MILLISECOND_TOLERANCE = 1.0f;
+   private final int iterations = 1000;
+
    @Test
    public void testConvertToTerrainMapData()
    {
@@ -67,5 +71,52 @@ public class TerrainMapToolsTest
          assertEquals(steppabilityConnectionsMapResult[i], steppabilityConnectionsMap.ptr(x, y).get());
          assertEquals(snappedAreaFractionMapResult[i], snappedAreaFractionMap.ptr(x).get());
       }
+   }
+
+   @Test
+   public void testSpeedConvertToTerrainMapData()
+   {
+      double cellResolution = 0.2;
+      double gridSizeXY = 1.0;
+      int cellsPerAxis = (int) (gridSizeXY / cellResolution);
+
+      TerrainMapData terrainMapData = new TerrainMapData(cellsPerAxis, cellResolution, gridSizeXY);
+
+      Mat heightMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_32FC1, new Scalar(100));
+      Mat terrainCostMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(1));
+      Mat contactMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(2));
+      Mat snapNormalXMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(3));
+      Mat snapNormalYMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(4));
+      Mat snapNormalZMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(5));
+      Mat steppabilityMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(6));
+      Mat steppabilityConnectionsMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(7));
+      Mat snappedAreaFractionMap = new Mat(cellsPerAxis, cellsPerAxis, opencv_core.CV_8UC1, new Scalar(8));
+
+      long startTime = System.nanoTime();
+
+      for (int i = 0; i < iterations; i++)
+      {
+         TerrainMapTools.convertToTerrainMapData(heightMap,
+                                                 terrainCostMap,
+                                                 contactMap,
+                                                 snapNormalXMap,
+                                                 snapNormalYMap,
+                                                 snapNormalZMap,
+                                                 steppabilityMap,
+                                                 steppabilityConnectionsMap,
+                                                 snappedAreaFractionMap,
+                                                 terrainMapData);
+      }
+
+      long endTime = System.nanoTime();
+      double totalTimeMillis = (endTime - startTime) / 1_000_000.0;
+      double averageTimePerIteration = totalTimeMillis / iterations;
+
+      System.out.printf("Average time per pack of Mats -> Terrain Map Data: %.3f ms%n", averageTimePerIteration);
+
+      // This will be machine-dependent, the benchmark for this value came from a laptop with a AMD Ryzen 7 5800H cpu.
+      float expectedTimeTakenToPackHeightMapMessageFromAMat = MILLISECOND_TOLERANCE * 5;
+      Assertions.assertTrue(averageTimePerIteration < expectedTimeTakenToPackHeightMapMessageFromAMat,
+                            "Actual was: " + averageTimePerIteration + ", but the Expected was: " + expectedTimeTakenToPackHeightMapMessageFromAMat);
    }
 }
