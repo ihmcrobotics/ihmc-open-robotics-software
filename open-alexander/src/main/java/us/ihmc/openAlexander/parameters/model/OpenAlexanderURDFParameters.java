00af1ba2ced6c6beeac382638e1fa36d2e8b74c1
Stefan Fasano <sfasano@ihmc.org>
2025-09-11 13:12:06 -0500

    set up way to send urdf and xml robot model info over ros

--- open-alexander/src/main/java/us/ihmc/openAlexander/parameters/model/OpenAlexanderURDFParameters.java
+++ open-alexander/src/main/java/us/ihmc/openAlexander/parameters/model/OpenAlexanderURDFParameters.java
@@ -39,7 +39,7 @@ public class OpenAlexanderURDFParameters implements HumanoidURDFParameterInterfa
 
    public OpenAlexanderURDFParameters(AlexanderVersionInterface alexanderVersion)
    {
-      urdfModelPath = alexanderVersion.getModelPath();
+      urdfModelPath = alexanderVersion.getURDFDescriptionResources();
    }
 
    @Override
