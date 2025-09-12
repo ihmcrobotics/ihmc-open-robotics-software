717d6fee1568673aea8a49bfadfaec7fdd284c3a
Stephen McCrory <smccrory1024@gmail.com>
2025-09-12 16:45:33 -0500

    removing import and tuning

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/steppableRegions/SnapResult.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/steppableRegions/SnapResult.java
@@ -1,7 +1,5 @@
 package us.ihmc.footstepPlanning.steppableRegions;
 
-import us.ihmc.euclid.geometry.Plane3D;
-
 public enum SnapResult
 {
    VALID, SNAP_FAILED, NOT_ENOUGH_AREA, SQUARED_ERROR, TOO_STEEP, CLIFF_TOP;
