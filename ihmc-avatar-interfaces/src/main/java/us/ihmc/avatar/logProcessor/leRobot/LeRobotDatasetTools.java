608243874378e6f89aa622c06a6ea3d18fbe66f0
Duncan Calvert <dcalvert@ihmc.org>
2025-09-17 17:35:39 -0500

    Visuomotor policy: Implement inference, upgrade dataset management. (#896)

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/leRobot/LeRobotDatasetTools.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/leRobot/LeRobotDatasetTools.java
@@ -1,13 +1,24 @@
 package us.ihmc.avatar.logProcessor.leRobot;
 
+import org.apache.commons.lang.StringUtils;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.exception.ExceptionTools;
+import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.robotics.robotSide.SideDependentList;
+import us.ihmc.scs2.definition.robot.JointDefinition;
+import us.ihmc.scs2.definition.robot.RigidBodyDefinition;
+import us.ihmc.scs2.definition.robot.RobotDefinition;
+import us.ihmc.scs2.session.log.LogSession;
+import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoDouble;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Assorted tools.
@@ -31,5 +42,82 @@ public class LeRobotDatasetTools
    public static void appendLine(Path path, String line)
    {
       ExceptionTools.handle(() -> Files.writeString(path, "%s%n".formatted(line.trim()), StandardOpenOption.APPEND), DefaultExceptionHandler.PRINT_MESSAGE);
+   }
+
+   public static String findRegistry(YoRegistry yoRegistry, String path, String childSearchSuffix)
+   {
+      Predicate<YoRegistry> filter = reg -> reg.getName().endsWith(childSearchSuffix);
+      YoRegistry result = yoRegistry.findRegistry(path).getChildren().stream().filter(filter).findFirst().orElse(null);
+      return path + "." + result.getName() + ".";
+   }
+
+   public static SideDependentList<String> getRobotLinkNames(RobotDefinition robotDefinition, String linkNamePart)
+   {
+      SideDependentList<String> handLinkNames = new SideDependentList<>();
+      for (RigidBodyDefinition link : robotDefinition.getAllRigidBodies())
+      {
+         String linkName = link.getName().toLowerCase();
+         if (linkName.contains(linkNamePart))
+            for (RobotSide side : RobotSide.values)
+               if (linkName.contains(side.getLowerCaseName()))
+                  handLinkNames.put(side, link.getName());
+      }
+      return handLinkNames;
+   }
+
+   public static SideDependentList<List<String>> getRobotArmJointNames(RobotDefinition robotDefinition)
+   {
+      SideDependentList<List<String>> armJointNames = new SideDependentList<>();
+      for (RigidBodyDefinition link : robotDefinition.getAllRigidBodies())
+         if (link.getName().toLowerCase().contains("torso")) // found torso link
+            for (JointDefinition joint : link.getChildrenJoints())
+               for (RobotSide side : RobotSide.values)
+                  if (joint.getName().toLowerCase().contains(side.getLowerCaseName())) // 1st arm joint
+                  {
+                     armJointNames.put(side, new ArrayList<>());
+                     recursivelyAddArmJoints(joint, armJointNames.get(side));
+                  }
+      return armJointNames;
+   }
+
+   private static void recursivelyAddArmJoints(JointDefinition joint, List<String> names)
+   {
+      names.add(joint.getName());
+
+      if (joint.getSuccessor().getChildrenJoints().size() == 1) // filter for nub & fingers
+      {
+         JointDefinition childJoint = joint.getSuccessor().getChildrenJoints().get(0);
+         recursivelyAddArmJoints(childJoint, names);
+      }
+   }
+
+   /** Unused for now. Keeping it around in case we need it later. */
+   public static void getJointAngleVariables(RobotDefinition robotDefinition, YoRegistry rootRegistry, LogSession session)
+   {
+      SideDependentList<YoDouble[]> jointAnglesCurrent = new SideDependentList<>();
+      SideDependentList<YoDouble[]> jointAnglesDesired = new SideDependentList<>();
+
+      SideDependentList<List<String>> robotArmJointNames = LeRobotDatasetTools.getRobotArmJointNames(robotDefinition);
+      for (RobotSide side : robotArmJointNames.sides())
+      {
+         String kstModule = LeRobotDatasetTools.findRegistry(rootRegistry, "root.main", "IKStreamingRTThread");
+         String kstController = kstModule + "KinematicsStreamingToolboxController.HumanoidKinematicsToolboxController.";
+         String capitalizedRobotName = StringUtils.capitalize(session.getLogProperties().getModel().getNameAsString());
+         String hwPosition = kstModule + "%sROS2HardwareCommunication.".formatted(capitalizedRobotName);
+         String ikSolver = kstController + "WholeBodyControllerCore.WholeBodyInverseKinematicsSolver.";
+         List<String> armJointNames = robotArmJointNames.get(side);
+         YoDouble[] currentState = new YoDouble[armJointNames.size()];
+         YoDouble[] desiredState = new YoDouble[armJointNames.size()];
+         for (int i = 0; i < armJointNames.size(); i++)
+            if (rootRegistry.findVariable("%sMotorState_Position_%s_%s".formatted(hwPosition,
+                                                                                  armJointNames.get(i),
+                                                                                  capitalizedRobotName)) instanceof YoDouble variable)
+               currentState[i] = variable;
+         for (int i = 0; i < armJointNames.size(); i++)
+            if (rootRegistry.findVariable("%sq_qp_%s".formatted(ikSolver, armJointNames.get(i))) instanceof YoDouble variable)
+               desiredState[i] = variable;
+         jointAnglesCurrent.put(side, currentState);
+         jointAnglesDesired.put(side, desiredState);
+      }
    }
 }
--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/leRobot/LeRobotEpisodeRecord.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logProcessor/leRobot/LeRobotEpisodeRecord.java
@@ -12,17 +12,16 @@ import java.util.List;
  * Part of the {@link LeRobotDataset} generation system from IHMC logs.
  */
 public record LeRobotEpisodeRecord
-(
-    @Alias("observation.state") List<Float> state,
-    List<Float> action, // goal position of arm joints
-    long episodeIndex, // index of the episode for this sample
-    long frameIndex, // index of the frame for this sample in the episode; starts at 0 for each episode
-    float timestamp, // in the episode
-    int ihmcLogPosition, // in the ihmc log
-    @Alias("next.done") boolean nextDone, // true for the last frame of an episode
-    long index, // general index in the whole dataset
-    long taskIndex // probably 0 mostly, since we only train one task at a time
-)
+      (@Alias("observation.state") List<Float> state, List<Float> action, // goal position of arm joints
+       long episodeIndex, // index of the episode for this sample
+       long frameIndex, // index of the frame for this sample in the episode; starts at 0 for each episode
+       float timestamp, // in the episode
+       int logPosition, // tick index in source log
+       String logName, // source log name
+       @Alias("next.done") boolean nextDone, // true for the last frame of an episode
+       long index, // general index in the whole dataset
+       long taskIndex // probably 0 mostly, since we only train one task at a time
+      )
 {
 
 }
