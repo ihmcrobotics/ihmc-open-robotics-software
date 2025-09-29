a374d295a9189dd3fa4c7167845a4bc3324fbdf0
Duncan Calvert <dcalvert@ihmc.org>
2025-09-28 21:33:20 -0500

    Add sequence icon. Work in progress.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/behavior/sequence/RDXActionSequence.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/behavior/sequence/RDXActionSequence.java
@@ -6,11 +6,13 @@ import us.ihmc.behaviors.sequence.ActionSequenceState;
 import us.ihmc.communication.crdt.CRDTInfo;
 import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.ui.behavior.tree.RDXBehaviorTreeNode;
+import us.ihmc.rdx.ui.widgets.ImGuiSequenceIconWidget;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
 
 public class RDXActionSequence extends RDXBehaviorTreeNode<ActionSequenceState, ActionSequenceDefinition>
 {
    private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
+   private final ImGuiSequenceIconWidget sequenceIconWidget = new ImGuiSequenceIconWidget();
 
    public RDXActionSequence(long id, CRDTInfo crdtInfo, WorkspaceResourceDirectory saveFileDirectory)
    {
@@ -21,6 +23,19 @@ public class RDXActionSequence extends RDXBehaviorTreeNode<ActionSequenceState, 
    public void update()
    {
       super.update();
+   }
+
+   @Override
+   public void renderTreeViewIconArea()
+   {
+      super.renderTreeViewIconArea();
+
+      if (sequenceIconWidget.render())
+      {
+         setSpecificWidgetOnRowClicked();
+         setTreeWidgetExpanded(!getTreeWidgetExpanded());
+      }
+      ImGui.sameLine();
    }
 
    @Override
