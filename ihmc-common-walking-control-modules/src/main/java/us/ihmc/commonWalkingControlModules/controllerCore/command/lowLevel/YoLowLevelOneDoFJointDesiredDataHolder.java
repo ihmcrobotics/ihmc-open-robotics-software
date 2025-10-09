058ff81f22fc855e2462d0ed0b800aa30de45e19
James Foster <jfoster@ihmc.org>
2025-10-09 17:02:02 -0500

    Implement isEmpty in a cleaner way thanks to Robert

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/lowLevel/YoLowLevelOneDoFJointDesiredDataHolder.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controllerCore/command/lowLevel/YoLowLevelOneDoFJointDesiredDataHolder.java
@@ -60,17 +60,6 @@ public class YoLowLevelOneDoFJointDesiredDataHolder implements JointDesiredOutpu
    }
 
    @Override
-   public boolean isEmpty()
-   {
-      for (int i = 0; i < lowLevelJointDataList.length; i++)
-      {
-         if (!lowLevelJointDataList[i].isEmpty())
-            return false;
-      }
-      return true;
-   }
-
-   @Override
    public JointDesiredOutputBasics getJointDesiredOutputFromHash(int jointHashCode)
    {
       return lowLevelJointDataMap.get(jointHashCode);
