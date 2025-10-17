24a712e7d0379f8606581f97decd4bbd96b0c6da
Duncan Calvert <dcalvert@ihmc.us>
2025-10-17 14:20:31 -0500

    Checkpoint icon.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/behavior/actions/RDXCheckPointNode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/behavior/actions/RDXCheckPointNode.java
@@ -1,19 +1,29 @@
 package us.ihmc.rdx.ui.behavior.actions;
 
+import imgui.ImGui;
 import us.ihmc.behaviors.sequence.actions.CheckPointNodeDefinition;
 import us.ihmc.behaviors.sequence.actions.CheckPointNodeState;
 import us.ihmc.communication.crdt.CRDTInfo;
-import us.ihmc.rdx.imgui.ImGuiUniqueLabelMap;
 import us.ihmc.rdx.ui.behavior.sequence.RDXLeafNode;
+import us.ihmc.rdx.ui.widgets.ImGuiCheckpointNodeWidget;
 import us.ihmc.tools.io.WorkspaceResourceDirectory;
 
 public class RDXCheckPointNode extends RDXLeafNode<CheckPointNodeState, CheckPointNodeDefinition>
 {
-   private final ImGuiUniqueLabelMap labels = new ImGuiUniqueLabelMap(getClass());
+   private final ImGuiCheckpointNodeWidget checkpointNodeWidget = new ImGuiCheckpointNodeWidget();
 
    public RDXCheckPointNode(long id, CRDTInfo crdtInfo, WorkspaceResourceDirectory saveFileDirectory)
    {
       super(new CheckPointNodeState(id, crdtInfo, saveFileDirectory));
+   }
+
+   @Override
+   public void renderTreeViewIconArea()
+   {
+      super.renderTreeViewIconArea();
+
+      checkpointNodeWidget.render();
+      ImGui.sameLine();
    }
 
    @Override
