2648a0afaada155091770b1d7080132b1fa380ff
nkitchel <nkitchel@ihmc.org>
2025-10-17 13:42:31 -0500

    Started compressing the height map message to reduce networking

--- ihmc-perception/src/main/java/us/ihmc/perception/gpuMapping/HeightMapMessageTools.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/gpuMapping/HeightMapMessageTools.java
@@ -1,11 +1,13 @@
 package us.ihmc.perception.gpuMapping;
 
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.FloatPointer;
 import org.bytedeco.opencv.global.opencv_core;
+import org.bytedeco.opencv.global.opencv_imgcodecs;
 import org.bytedeco.opencv.opencv_core.Mat;
 import perception_msgs.msg.dds.ChunkMessage;
 import perception_msgs.msg.dds.HeightMapMessage;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.idl.IDLSequence.Float;
 
 import java.nio.FloatBuffer;
 
@@ -59,22 +61,23 @@ public class HeightMapMessageTools
       // This is done for speed optimization
       float[] heightsArray = new float[totalCells];
       floatBuffer.get(heightsArray);
-      Float heights = messageToPack.getHeights();
-
-      // No overhead for this loop, it's as fast as possible (according to AI) with the current message
-      for (int i = 0; i < totalCells; ++i)
-      {
-         if (i < heights.size())
-            heights.set(i, heightsArray[i]);
-         else
-            heights.add(heightsArray[i]);
-      }
+//      Float heights = messageToPack.getHeights();
+//
+//      // No overhead for this loop, it's as fast as possible (according to AI) with the current message
+//      for (int i = 0; i < totalCells; ++i)
+//      {
+//         if (i < heights.size())
+//            heights.set(i, heightsArray[i]);
+//         else
+//            heights.add(heightsArray[i]);
+//      }
    }
 
    /**
     * There is no safety for this method, in the sense that we've got an array of floats coming from the message, and we've got data to put the floats into.
     * Its expected that the data matches the size requirements of the message.
     * By not having a safety check (i < size()) we can drastically speed up the computational time of this method.
+    *
     * @param heightMapMessage the message that contains the data to put into the {@link HeightMapData}
     * @return the {@link HeightMapData} that is filled with the floats from the message
     */
@@ -88,8 +91,22 @@ public class HeightMapMessageTools
                                                       heightMapMessage.getGridCenterX(),
                                                       heightMapMessage.getGridCenterY());
 
-      Float heightsFromMessage = heightMapMessage.getHeights();
-      heightsFromMessage.toArray(heightMapData.getHeights());
+      // Decode the PNG compressed height map data
+      Mat compressedMat = new Mat(heightMapMessage.getHeights().getBuffer().array());
+      Mat heightMapMat = new Mat(heightMapMessage.getCellsPerAxis(), 2 * heightMapMessage.getCellsPerAxis(), opencv_core.CV_16UC1);
+      opencv_imgcodecs.imdecode(compressedMat, opencv_imgcodecs.IMREAD_UNCHANGED, heightMapMat);
+
+      // Convert the Mat into HeightMapData object
+      Point3D mapOrigin = new Point3D(heightMapMessage.getGridCenterX(), heightMapMessage.getGridCenterY(), 0.0);
+      HeightMapTools.convertToHeightMapData(heightMapMat,
+                                            heightMapData,
+                                            mapOrigin,
+                                            (float) heightMapMessage.getWidthInMeters(),
+                                            (float) heightMapMessage.getCellSizeInMeters());
+
+      // Close pointers
+      compressedMat.close();
+      heightMapMat.close();
 
       return heightMapData;
    }
@@ -110,11 +127,23 @@ public class HeightMapMessageTools
       messageToPack.setCellSizeInMeters(heightMapData.getCellSize());
       messageToPack.setCellsPerAxis(heightMapData.getCellsPerAxis());
 
-      float[] heightsFromData = heightMapData.getHeights();
+      // Get the heights in native memory as a 16U Mat
+      FloatPointer dataPointer = new FloatPointer(heightMapData.getHeights());
+      Mat heightMapMat16U = new Mat(heightMapData.getCellsPerAxis(), 2 * heightMapData.getCellsPerAxis(), opencv_core.CV_16U, dataPointer);
 
-      messageToPack.getHeights().resetQuick();
-      Float heights = messageToPack.getHeights();
-      heights.add(heightsFromData);
+      // Compress the data using PNG
+      BytePointer compressedData = new BytePointer();
+      opencv_imgcodecs.imencode(".png",  heightMapMat16U, compressedData);
+
+      // Pack the compressed data into the message
+      int compressedDataSize = (int) compressedData.limit();
+      compressedData.get(messageToPack.getHeights().getBuffer().array(), 0, compressedDataSize);
+      messageToPack.getHeights().getBuffer().position(compressedDataSize);
+
+      // Close pointers
+      dataPointer.close();
+      heightMapMat16U.close();
+      compressedData.close();
    }
 
    /**
@@ -126,6 +155,6 @@ public class HeightMapMessageTools
       messageToClear.setGridCenterX(-1.0);
       messageToClear.setGridCenterY(-1.0);
 
-      messageToClear.getHeights().clear();
+      //      messageToClear.getHeights().clear();
    }
 }
