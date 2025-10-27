48d0dcdd81234c03fce30a6ba85531b444fed614
Duncan Calvert <dcalvert@ihmc.us>
2025-10-27 14:36:13 -0500

    Add PersistentDetection.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/scene/BehaviorTreeSceneObjectTracker.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/scene/BehaviorTreeSceneObjectTracker.java
@@ -1,8 +1,14 @@
 package us.ihmc.behaviors.behaviorTree.scene;
 
+import us.ihmc.perception.detections.PersistentDetection;
+
 public class BehaviorTreeSceneObjectTracker
 {
+   private PersistentDetection persistentDetection;
+
    public BehaviorTreeSceneObjectTracker()
    {
+
+      //new PersistentDetection()
    }
 }
