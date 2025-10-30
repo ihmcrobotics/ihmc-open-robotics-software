fe9679a0fa9be68ec0746073876545605168461c
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2025-10-29 20:04:46 -0500

    Got local logging working and tuned the realsense and height map for real world testing (#1063)
    * Settup logging to work on both the high and low level process, fixed a couple of bugs in the starting, and naming of the thing
    
    * This shouldn't be a hard coded value, neesds to be the cell size from the parameters
    
    * Shrink height map to reduce networking lag
    
    * Adjust realsense transform, adjust local logging for testing
    
    * Tested more of the logging onboard. Update is either the server, or the intraprocess logger, not both. Optimized the IntraprocessYoVariableLogger so now the longest part is compressing the data before it gets written to disk
    
    * Fix gradle deploy, remove link in warning prints
    
    * Fix VR finger streaming.
    
    ---------
    
    Co-authored-by: Alexander OCU <rosie@ihmc.org>
    Co-authored-by: VR-OCU <lpenco@ihmc.org>

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/wholeBodyHardwareControl/AvatarMultiThreadingFactory.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/wholeBodyHardwareControl/AvatarMultiThreadingFactory.java
@@ -32,7 +32,8 @@ import us.ihmc.realtime.MonotonicTime;
 import us.ihmc.realtime.PriorityParameters;
 import us.ihmc.robotDataLogger.YoVariableServer;
 import us.ihmc.robotDataLogger.dataBuffers.RegistrySendBufferBuilder;
-import us.ihmc.robotDataVisualizer.logger.JVMStatisticsGenerator;
+import us.ihmc.robotDataVisualizer.logger.localLogging.JVMStatisticsGenerator;
+import us.ihmc.robotDataVisualizer.logger.localLogging.LocalLoggingTools;
 import us.ihmc.robotModels.FullHumanoidRobotModel;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.robotSide.SideDependentList;
@@ -80,9 +81,6 @@ public class AvatarMultiThreadingFactory
 
    // Hardware communication API
    private final HardwareCommunicationInterface hardwareCommunicationInterface;
-
-   // Logger stuff
-   private boolean logLocally = false;
 
    // ROS stuff
    public final String IHMC_ROS_STATE_ESTIMATOR_NODE_NAME;
@@ -263,7 +261,7 @@ public class AvatarMultiThreadingFactory
       // Set up the block to prevent execution whenever there is no new state message.
       threadingManager.get().setBlockingProvider(() -> !hardwareCommunicationInterface.hasNewStateMessage());
 
-      if (logLocally)
+      if (LocalLoggingTools.LOGGING_LOCALLY)
       {
          // Setup logger
          ArrayList<RegistrySendBufferBuilder> builders = new ArrayList<>();
@@ -281,11 +279,8 @@ public class AvatarMultiThreadingFactory
          builders.add(new RegistrySendBufferBuilder(jvmStatisticsGenerator.getYoRegistry(), null));
 
          // Logging locally on the robot
-         IntraprocessYoVariableLogger intraprocessYoVariableLogger = new IntraprocessYoVariableLogger(getClass().getSimpleName(),
-                                                                                                      robotModel.getLogModelProvider(),
-                                                                                                      builders,
-                                                                                                      100000,
-                                                                                                      0.01);
+         IntraprocessYoVariableLogger intraprocessYoVariableLogger = new IntraprocessYoVariableLogger(
+               robotModel.getSimpleRobotName().toLowerCase() + getClass().getSimpleName(), robotModel.getLogModelProvider(), builders, 100000, 0.01);
          intraprocessYoVariableLogger.start();
 
          threadingManager.get()
@@ -596,11 +591,6 @@ public class AvatarMultiThreadingFactory
    public void addSmoothTransitionState(String transitionName, HighLevelControllerName transitionStateEnum, HighLevelControllerName currentControlStateEnum, HighLevelControllerName nextControlStateEnum)
    {
       controllerFactory.addCustomSmoothTransitionControlState(transitionName, transitionStateEnum, currentControlStateEnum, nextControlStateEnum);
-   }
-
-   public void setLogLocally(boolean logLocally)
-   {
-      this.logLocally = logLocally;
    }
 
    public void setHighLevelControllerCallbackForEstimator(Map<HighLevelControllerName, StateEstimatorMode> estimatorModeMap)
