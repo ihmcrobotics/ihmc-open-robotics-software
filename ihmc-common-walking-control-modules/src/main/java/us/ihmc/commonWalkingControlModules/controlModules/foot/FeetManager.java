0f2e95dedcb49ba94f3a315b45fabe577b13130b
Robert <rgriffin@ihmc.us>
2025-11-04 12:48:25 -0600

    did some patches for the height trajectory

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/FeetManager.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/FeetManager.java
@@ -52,7 +52,7 @@ import us.ihmc.yoVariables.registry.YoRegistry;
 public class FeetManager implements SCS2YoGraphicHolder
 {
    private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
-   private static final double extraCoMHeightWithToes = 0.06;
+   private static final double extraCoMHeightWithToes = 0.04;
 
    private final YoRegistry registry = new YoRegistry(getClass().getSimpleName());
 
