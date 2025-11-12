bffcda871ac31f17d1577c7d8773e7f19405fd21
nkitchel <nkitchel@ihmc.org>
2025-11-12 15:07:25 -0600

    Remove commented code ( 1 line )

--- ihmc-perception/src/main/resources/us/ihmc/perception/filters/DepthImageBodyCollisionFilter.cu
+++ ihmc-perception/src/main/resources/us/ihmc/perception/filters/DepthImageBodyCollisionFilter.cu
@@ -117,8 +117,8 @@ __device__ bool rayIntersectsCapsule(float3 rayOrigin,
         sphereCheck = sub(rayOrigin, bottomCenter);
     }
 
-    float raySphereIntersection =  dot(rayDirection, sphereCheck);//rayDirection.x * sphereCheck.x + rayDirection.y * sphereCheck.y + rayDirection.z * sphereCheck.z;
-    float sphereQuadraticConstant = (dot(sphereCheck, sphereCheck)) - radius * radius;
+    float raySphereIntersection =  dot(rayDirection, sphereCheck);
+    float sphereQuadraticConstant = dot(sphereCheck, sphereCheck) - radius * radius;
     float quadraticCoeffDiscriminantSphere = raySphereIntersection * raySphereIntersection - sphereQuadraticConstant;
     if (quadraticCoeffDiscriminantSphere < 0.0f)
         return false;
