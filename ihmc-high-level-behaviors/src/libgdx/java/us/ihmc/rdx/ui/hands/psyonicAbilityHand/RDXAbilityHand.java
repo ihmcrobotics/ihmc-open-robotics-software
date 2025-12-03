5f973dc8914b41f042ecdb9a15cebe53e7991f04
Duncan Calvert <dcalvert@ihmc.us>
2025-12-03 13:50:11 -0600

    Simplify hand control code. Fix UI bug.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/hands/psyonicAbilityHand/RDXAbilityHand.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/hands/psyonicAbilityHand/RDXAbilityHand.java
@@ -139,7 +139,7 @@ public class RDXAbilityHand implements RDXHandInterface
          ImGui.pushItemWidth(sliderWidth);
          scheduleExecuteVelToPos |= ImGui.sliderFloat(labels.getHidden(FINGER_NAMES[i]), desiredPositions[i].getData(), sliderMin, sliderMax,
                                "%s: %.2f%s flexion".formatted(FINGER_NAMES[i], actuatorPosition, EuclidCoreMissingTools.DEGREE_SYMBOL));
-         if (!ImGui.isItemActive() && !executeVelToPos) // Prevent overriding externally submitted positions too
+         if (!ImGui.isItemActive() && !executeVelToPos && latestState != null) // Prevent overriding externally submitted positions too
             desiredPositions[i].set(latestState.getGoalPositions()[i]);
          ImGui.popItemWidth();
          ImGui.sameLine();
