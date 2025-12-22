ec86aa10b7cf6a11ff1b8a0a0cff5a9ea581bfac
ihmc-rosie <rosie@ihmc.org>
2025-12-22 16:37:33 -0600

    Add door pull handle.

--- ihmc-perception/src/main/java/us/ihmc/perception/detections/foundationPose/IsaacROSFoundationPoseObject.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/detections/foundationPose/IsaacROSFoundationPoseObject.java
@@ -18,7 +18,8 @@ public enum IsaacROSFoundationPoseObject
    DOOR_PANEL("Door Panel", "door_panel", "door_panel"),
    DOOR_HANDLE("Door Handle", "door_handle", "door_lever"),
    DOOR_LEVER_WAVY("Door Lever Wavy", "door_lever_wavy", "door_lever"),
-   DOOR_KNOB("Door Knob", "door_knob", "door_knob");
+   DOOR_KNOB("Door Knob", "door_knob", "door_knob"),
+   DOOR_PULL_HANDLE("Door Pull Handle", "door_handle", "door_pull_handle"); // TODO: Add model
 
    public static final IsaacROSFoundationPoseObject[] values = values();
 
