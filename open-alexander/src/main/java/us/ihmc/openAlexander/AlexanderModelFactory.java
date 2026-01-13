8c2ed41962976c93c8f5e6a548107b3ba496a668
Duncan Calvert <dcalvert@ihmc.us>
2026-01-13 14:24:29 -0600

    Remove simplify kinematics = false setting

--- open-alexander/src/main/java/us/ihmc/openAlexander/AlexanderModelFactory.java
+++ open-alexander/src/main/java/us/ihmc/openAlexander/AlexanderModelFactory.java
@@ -79,7 +79,6 @@ public class AlexanderModelFactory
          URDFTools.URDFParserProperties parserProperties = new URDFTools.URDFParserProperties();
          parserProperties.setTransformToZUp(false);
          parserProperties.setHandleImplicitJointDefinitions(false);
-         parserProperties.setSimplifyKinematics(false);
          simulationRobotDefinition = RobotDefinitionLoader.loadURDFModel(urdfParameters.getURDFAsInputStream(),
                                                                          Arrays.asList(urdfParameters.getResourceDirectories()),
                                                                          getClass().getClassLoader(),
