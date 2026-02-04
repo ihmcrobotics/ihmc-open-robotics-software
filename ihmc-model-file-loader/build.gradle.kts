4f9d35447a99cab740e10150c5f7bb26a6f11bfc
Robert <rjgriffin42@gmail.com>
2026-02-04 17:47:09 -0600

    :arrow_up: ihmc-javafx-toolkit 17-0.23.0

--- ihmc-model-file-loader/build.gradle.kts
+++ ihmc-model-file-loader/build.gradle.kts
@@ -17,7 +17,7 @@ ihmc {
 mainDependencies {
 
    api("us.ihmc:ihmc-robot-description:0.25.3")
-   api("us.ihmc:ihmc-javafx-toolkit:17-0.22.12")
+   api("us.ihmc:ihmc-javafx-toolkit:17-0.23.0")
    api("us.ihmc:ihmc-robotics-toolkit:source")
 }
 
