52982b8aa218bc6f083b1f041414b811156a183c
Duncan Calvert <dcalvert@ihmc.us>
2026-03-06 14:43:30 -0600

    Code cleanup.

--- ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/action/actions/AbilityHandActionComms.java
+++ ihmc-high-level-behaviors/src/main/java/us/ihmc/behaviors/behaviorTree/action/actions/AbilityHandActionComms.java
@@ -2,7 +2,6 @@ package us.ihmc.behaviors.behaviorTree.action.actions;
 
 import ihmc_hands_ros2.msg.dds.AbilityHandCommand;
 import ihmc_hands_ros2.msg.dds.AbilityHandState;
-import us.ihmc.commons.thread.Throttler;
 import us.ihmc.commons.thread.TypedNotification;
 import us.ihmc.handsros2.abilityHand.AbilityHandROS2API;
 import us.ihmc.robotics.robotSide.RobotSide;
@@ -12,18 +11,14 @@ import us.ihmc.tools.Timer;
 
 public class AbilityHandActionComms
 {
-   private final RobotSide handSide;
    private final TypedNotification<AbilityHandState> stateNotification = new TypedNotification<>();
    private AbilityHandState latestState = null;
    private final AbilityHandCommand command = new AbilityHandCommand();
    private final ROS2Publisher<AbilityHandCommand> commandPublisher;
-   private final Throttler commandThrottler = new Throttler().setFrequency(30.0);
    private final Timer connectedTimer = new Timer();
 
    public AbilityHandActionComms(RobotSide handSide, ROS2Node ros2Node)
    {
-      this.handSide = handSide;
-
       ros2Node.createSubscription2(AbilityHandROS2API.STATE_TOPICS.get(handSide), stateNotification::set);
       commandPublisher = ros2Node.createPublisher(AbilityHandROS2API.COMMAND_TOPICS.get(handSide));
    }
