9e642f2ae3243bea61c1a81c188b844b6b0d7909
Dexton Anderson <danderson@ihmc.org>
2026-03-17 11:27:46 -0500

    Start jros2 conversion

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logging/IntraprocessYoVariableLogger.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/logging/IntraprocessYoVariableLogger.java
@@ -1,16 +1,15 @@
 package us.ihmc.avatar.logging;
 
+import logger_msgs.msg.dds.Handshake;
+import logger_msgs.msg.dds.HandshakeFileType;
 import us.ihmc.commons.exception.DefaultExceptionHandler;
 import us.ihmc.commons.nio.FileTools;
 import us.ihmc.commons.thread.RepeatingTaskThread;
 import us.ihmc.concurrent.ConcurrentRingBuffer;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.idl.serializers.extra.YAMLSerializer;
+import us.ihmc.idl.serializers.extra.ROS2YAMLSerializer;
 import us.ihmc.log.LogTools;
 import us.ihmc.multicastLogDataProtocol.modelLoaders.LogModelProvider;
-import us.ihmc.robotDataLogger.Handshake;
-import us.ihmc.robotDataLogger.HandshakeFileType;
-import us.ihmc.robotDataLogger.HandshakePubSubType;
 import us.ihmc.robotDataLogger.dataBuffers.RegistrySendBufferBuilder;
 import us.ihmc.robotDataLogger.handshake.YoVariableHandShakeBuilder;
 import us.ihmc.robotDataLogger.jointState.JointHolder;
@@ -123,7 +122,7 @@ public class IntraprocessYoVariableLogger
 
          Handshake handshake = handshakeBuilder.getHandShake();
 
-         YAMLSerializer<Handshake> serializer = new YAMLSerializer<>(new HandshakePubSubType());
+         ROS2YAMLSerializer<Handshake> serializer = new ROS2YAMLSerializer<>(Handshake.class);
          serializer.serialize(createFileInLogFolder(HANDSHAKE_FILENAME), handshake);
 
          LogPropertiesWriter logProperties = new LogPropertiesWriter(createFileInLogFolder(PROPERTY_FILE));
--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/scs2/SCS2LogSessionWithVideo.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/scs2/SCS2LogSessionWithVideo.java
@@ -1,9 +1,9 @@
 package us.ihmc.avatar.scs2;
 
+import logger_msgs.msg.dds.Camera;
+import logger_msgs.msg.dds.CameraType;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import us.ihmc.log.LogTools;
-import us.ihmc.robotDataLogger.Camera;
-import us.ihmc.robotDataLogger.CameraType;
 import us.ihmc.robotDataLogger.logger.LogPropertiesReader;
 import us.ihmc.scs2.session.log.*;
 import us.ihmc.yoVariables.variable.YoLong;
@@ -51,7 +51,7 @@ public class SCS2LogSessionWithVideo extends LogSession
             LogTools.info("Found camera: %s".formatted(camera.getName()));
             try
             {
-               if (camera.getTypeAsString().equals(CameraType.CAPTURE_CARD_MAGEWELL.toString()))
+               if (camera.getTypeAsString().equals("CAPTURE_CARD_MAGEWELL"))
                {
                   MagewellScrubber magewellScrubber = new MagewellScrubber(camera, logDirectory, logProperties.getVideo().getHasTimebase());
                   magewellScrubbers.add(magewellScrubber);
