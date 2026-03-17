d43cebc6308620cfab9727900ea2bd520f19d2c5
Tomasz Bialek <103042423+TomaszTB@users.noreply.github.com>
2026-03-17 10:38:10 -0500

    [Bugfix] YOLO Parameters Cleanup & Bug Fixes (#1292)
    * Cleanup & fix bunch of bugs
    
    * Restore RDXBehaviorTreeScene to develop
    
    ---------
    
    Co-authored-by: ihmc-rosie <rosie@ihmc.org>

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/ros2/yolo/RDXROS2YOLOv8Settings.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/ui/graphics/ros2/yolo/RDXROS2YOLOv8Settings.java
@@ -2,14 +2,11 @@ package us.ihmc.rdx.ui.graphics.ros2.yolo;
 
 import imgui.ImGui;
 import imgui.type.ImInt;
-import perception_msgs.msg.dds.YOLOv8ExecutorParameters;
-import us.ihmc.communication.PerceptionAPI;
 import us.ihmc.communication.crdt.CRDTInfo;
 import us.ihmc.communication.ros2.ROS2ActorDesignation;
 import us.ihmc.communication.ros2.sync.ROS2PeerClockOffsetEstimator;
 import us.ihmc.perception.detections.yolo.SyncedYOLOv8ExecutorParameters;
 import us.ihmc.ros2.ROS2Node;
-import us.ihmc.ros2.ROS2Publisher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,29 +21,16 @@ public class RDXROS2YOLOv8Settings
 
    private final List<RDXROS2YOLOv8ModelSettings> rdxModelSettings = new ArrayList<>();
 
-   private final YOLOv8ExecutorParameters parametersMessage = new YOLOv8ExecutorParameters();
-   private final ROS2Publisher<YOLOv8ExecutorParameters> parametersPublisher;
-
    public RDXROS2YOLOv8Settings(ROS2Node ros2Node, ROS2PeerClockOffsetEstimator ros2ClockOffsetEstimator)
    {
       CRDTInfo crdtInfo = new CRDTInfo(ROS2ActorDesignation.OPERATOR, ros2ClockOffsetEstimator);
-      parameters = new SyncedYOLOv8ExecutorParameters(crdtInfo);
-      parameters.requestSendFullData();
-
-      parametersPublisher = ros2Node.createPublisher(PerceptionAPI.YOLO_PARAMETERS);
-      ros2Node.createSubscription2(PerceptionAPI.YOLO_PARAMETERS, message ->
-      {
-         parameters.fromMessage(message);
-         parameters.confirmReceivedFullData();
-      });
-
-      parameters.toMessage(parametersMessage);
-      parametersPublisher.publish(parametersMessage);
+      parameters = new SyncedYOLOv8ExecutorParameters(ros2Node, crdtInfo);
    }
 
    public void update()
    {
-      parameters.checkModified();
+      parameters.checkModifiedAndUpdate();
+
       if (rdxModelSettings.size() != parameters.getAvailableModels().getSize())
       {
          rdxModelSettings.clear();
@@ -77,12 +61,6 @@ public class RDXROS2YOLOv8Settings
 
       if (selectedModel.get() != 0)
          rdxModelSettings.get(selectedModel.get() - 1).update();
-
-      if (parameters.pollNeedSendFullData() || parameters.getModelParameters().pollNeedSendFullData())
-      {
-         parameters.toMessage(parametersMessage);
-         parametersPublisher.publish(parametersMessage);
-      }
    }
 
    public void renderSettings()
@@ -100,6 +78,6 @@ public class RDXROS2YOLOv8Settings
 
    public void destroy()
    {
-      parametersPublisher.remove();
+      parameters.close();
    }
 }
