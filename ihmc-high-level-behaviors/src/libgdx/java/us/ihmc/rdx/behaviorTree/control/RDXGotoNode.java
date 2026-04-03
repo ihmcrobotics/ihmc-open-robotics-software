f66ab751df6b3e404e6c157296fbd55fbd595d30
Duncan Calvert <dcalvert@ihmc.us>
2026-04-03 14:37:34 -0500

    Goto node shows which node its going to.

--- ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/behaviorTree/control/RDXGotoNode.java
+++ ihmc-high-level-behaviors/src/libgdx/java/us/ihmc/rdx/behaviorTree/control/RDXGotoNode.java
@@ -39,6 +39,8 @@ public class RDXGotoNode extends RDXLeafNode<GotoNodeState, GotoNodeDefinition>
 
       ImGui.sameLine();
       gotoNodeWidget.render();
+      ImGui.sameLine();
+      ImGui.textDisabled("%s".formatted(definition.getNodeToGotoName()));
 
       renderRowEnd();
    }
