4397d02ced774f0594ac26b4229d48f2e89360aa
Stefan <161661671+stefanfasano@users.noreply.github.com>
2026-04-09 16:47:58 -0500

    added some safeties to reset desired velocity commands to 0.0 and standing if new command hasn't come in (#1340)

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/plugin/StepGeneratorCommandInputManager.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/plugin/StepGeneratorCommandInputManager.java
@@ -158,16 +158,18 @@ public class StepGeneratorCommandInputManager implements Updatable
    @Override
    public void update(double time)
    {
-      isOpen = true;
+      boolean validHighLevelControlState = currentController.equals(HighLevelControllerName.WALKING) || currentController.equals(HighLevelControllerName.RL_CONTROL);
+      isOpen = validHighLevelControlState && (heartbeatMonitor.isAlive() || overrideHeartbeat.getBooleanValue());
       commandInputManager.setEnabled(isOpen);
 
-      if (!overrideHeartbeat.getValue() && !heartbeatMonitor.isAlive())
+      if (!isOpen)
       {
+         walk.set(false);
          desiredVelocity.setX(0.0);
          desiredVelocity.setY(0.0);
          turningVelocity.set(0.0);
          areVelocitiesNormalized.set(true);
-         walk.set(false);
+         commandInputManager.clearAllCommands();
       }
       else if (commandInputManager.isNewCommandAvailable(ContinuousStepGeneratorInputCommand.class))
       {
@@ -256,12 +258,6 @@ public class StepGeneratorCommandInputManager implements Updatable
 
       previousWalkingStatus.set(latestWalkingStatus.get());
       previousFootstepStatusReceived.set(latestFootstepStatusReceived.get());
-
-      if (!isOpen)
-      {
-         walk.set(false);
-         commandInputManager.clearAllCommands();
-      }
    }
 
    public boolean isOpen()
