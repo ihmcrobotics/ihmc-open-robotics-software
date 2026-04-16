f464b8cd22f2aa3fca9b98c01d90883a60c46ba3
ArghyaChatterjee <arghyame20buet@gmail.com>
2026-04-15 23:36:41 -0500

    updated the parameters

--- ihmc-perception/src/main/java/us/ihmc/perception/detections/foundationPose/CategoryLevelFoundationPoseResetDistanceLibrary.java
+++ ihmc-perception/src/main/java/us/ihmc/perception/detections/foundationPose/CategoryLevelFoundationPoseResetDistanceLibrary.java
@@ -9,32 +9,32 @@ public class CategoryLevelFoundationPoseResetDistanceLibrary
     * Key format: "<category>/<instance>"
     */
    private static final Map<String, Double> RESET_DISTANCE_MAP = Map.ofEntries(
-         Map.entry("bottle/bottle_1", 0.08),
-         Map.entry("bottle/bottle_2", 0.09),
-         Map.entry("bottle/bottle_3", 0.10),
+         Map.entry("bottle/bottle_1", 0.3),
+         Map.entry("bottle/bottle_2", 0.3),
+         Map.entry("bottle/bottle_3", 0.3),
 
-         Map.entry("charge/charge_1", 0.07),
-         Map.entry("charge/charge_2", 0.07),
+         Map.entry("charge/charge_1", 0.5),
+         Map.entry("charge/charge_2", 0.5),
 
-         Map.entry("door_knob/door_knob_1", 0.04),
+         Map.entry("door_knob/door_knob_1", 0.3),
 
-         Map.entry("door_lever/door_lever_1", 0.05),
-         Map.entry("door_lever/door_lever_2", 0.05),
+         Map.entry("door_lever/door_lever_1", 0.3),
+         Map.entry("door_lever/door_lever_2", 0.3),
 
-         Map.entry("door_panel/door_panel_1", 0.15),
-         Map.entry("door_panel/door_panel_2", 0.15),
+         Map.entry("door_panel/door_panel_1", 0.80),
+         Map.entry("door_panel/door_panel_2", 0.80),
 
-         Map.entry("door_pull_handle/door_pull_handle_1", 0.05),
-         Map.entry("door_push_bar/door_push_bar_1", 0.10),
+         Map.entry("door_pull_handle/door_pull_handle_1", 0.50),
+         Map.entry("door_push_bar/door_push_bar_1", 0.50),
 
-         Map.entry("storage_container/storage_container_1", 0.12),
-         Map.entry("storage_container/storage_container_2", 0.12),
+         Map.entry("storage_container/storage_container_1", 0.50),
+         Map.entry("storage_container/storage_container_2", 0.50),
 
-         Map.entry("traffic_barrier/traffic_barrier_1", 0.20),
+         Map.entry("traffic_barrier/traffic_barrier_1", 0.80),
 
-         Map.entry("trash_can/trash_can_1", 0.18),
-         Map.entry("trash_can/trash_can_2", 0.18),
-         Map.entry("trash_can/trash_can_3", 0.18)
+         Map.entry("trash_can/trash_can_1", 0.8),
+         Map.entry("trash_can/trash_can_2", 0.8),
+         Map.entry("trash_can/trash_can_3", 0.8)
    );
 
    private static final double DEFAULT_RESET_DISTANCE = 0.10;
