0ce7a694fc2846da622013d1cd7a45eb682871f7
Nehar Poddar <161652971+LaughingCommit@users.noreply.github.com>
2026-04-15 22:44:04 -0500

    Feature/policy selection (#1343)
    * New messages for selecting RL policy
    
    * added a rl model selector panel
    
    * removed thread
    
    * renamed command to msg
    
    * made the command hold an int rather than a byte, and added a test
    
    ---------
    
    Co-authored-by: Reese Peterson <rpeterson@ihmc.org>
    Co-authored-by: Robert <rjgriffin42@gmail.com>

---
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/controllerAPI/command/RLModelSelectionCommand.java
@@ -0,0 +1,61 @@
+package us.ihmc.humanoidRobotics.communication.controllerAPI.command;
+
+import controller_msgs.msg.dds.RLModelSelectionMessage;
+import us.ihmc.communication.controllerAPI.command.Command;
+
+public class RLModelSelectionCommand implements Command<RLModelSelectionCommand, RLModelSelectionMessage>
+{
+   private int desiredModel;
+   private boolean executeDesiredModel;
+
+   @Override
+   public void set(RLModelSelectionCommand other)
+   {
+      clear();
+      desiredModel = other.desiredModel;
+      executeDesiredModel = other.executeDesiredModel;
+   }
+
+   @Override
+   public void setFromMessage(RLModelSelectionMessage message)
+   {
+      clear();
+      desiredModel = message.getDesiredModel();
+      executeDesiredModel = message.getExecuteDesiredModel();
+   }
+
+   @Override
+   public void clear()
+   {
+      desiredModel = 0;
+      executeDesiredModel = false;
+   }
+
+   @Override
+   public Class<RLModelSelectionMessage> getMessageClass()
+   {
+      return RLModelSelectionMessage.class;
+   }
+
+   @Override
+   public boolean isCommandValid()
+   {
+      return true;
+   }
+
+   @Override
+   public long getSequenceId()
+   {
+      return 0;
+   }
+
+   public int getDesiredModel()
+   {
+      return desiredModel;
+   }
+
+   public boolean getExecuteDesiredModel()
+   {
+      return executeDesiredModel;
+   }
+}
