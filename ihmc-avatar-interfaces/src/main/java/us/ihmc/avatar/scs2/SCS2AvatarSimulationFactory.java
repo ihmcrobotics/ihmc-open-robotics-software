45926436e8a364b43050666dcfa661a5bd212e1f
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2026-06-25 16:31:55 -0500

    Change default value of showGUI to true

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/scs2/SCS2AvatarSimulationFactory.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/scs2/SCS2AvatarSimulationFactory.java
@@ -158,7 +158,7 @@ public class SCS2AvatarSimulationFactory
    protected final OptionalFactoryField<Boolean> initializeEstimatorToActual = new OptionalFactoryField<>(
          "initializeEstimatorToActual",
          true);
-   protected final OptionalFactoryField<Boolean> showGUI = new OptionalFactoryField<>("showGUI", false);
+   protected final OptionalFactoryField<Boolean> showGUI = new OptionalFactoryField<>("showGUI", true);
    protected final OptionalFactoryField<Boolean> automaticallyStartSimulation = new OptionalFactoryField<>(
          "automaticallyStartSimulation",
          false);
