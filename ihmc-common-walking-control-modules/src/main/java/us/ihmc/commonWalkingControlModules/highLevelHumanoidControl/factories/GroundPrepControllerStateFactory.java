a63948876854b3f8467c3f7a3fd0cdca0a8a7fdd
LuigiPenco <161648875+LuigiPenco93@users.noreply.github.com>
2026-07-06 13:15:14 -0500

    groudn prep (#1389)

---
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/factories/GroundPrepControllerStateFactory.java
@@ -0,0 +1,31 @@
+package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.factories;
+
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.HighLevelControllerFactoryHelper;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.GroundPrepControllerState;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.HighLevelControllerState;
+import us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelControllerName;
+
+public class GroundPrepControllerStateFactory implements HighLevelControllerStateFactory
+{
+   private GroundPrepControllerState groundPrepControllerState;
+
+   @Override
+   public HighLevelControllerState getOrCreateControllerState(HighLevelControllerFactoryHelper controllerFactoryHelper)
+   {
+      if (groundPrepControllerState == null)
+      {
+         groundPrepControllerState = new GroundPrepControllerState(controllerFactoryHelper.getHighLevelHumanoidControllerToolbox().getControlledOneDoFJoints(),
+                                                                   controllerFactoryHelper.getHighLevelControllerParameters(),
+                                                                   controllerFactoryHelper.getLowLevelControllerOutput(),
+                                                                   controllerFactoryHelper.getHighLevelHumanoidControllerToolbox().getYoTime());
+      }
+
+      return groundPrepControllerState;
+   }
+
+   @Override
+   public HighLevelControllerName getStateEnum()
+   {
+      return HighLevelControllerName.GROUND_PREP_STATE;
+   }
+}
