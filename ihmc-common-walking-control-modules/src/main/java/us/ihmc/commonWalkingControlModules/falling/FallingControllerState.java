dbc0b0f9fb30fc85ab38af3e652d03b2bb45f03c
beomyeong <bpark@ihmc.org>
2026-07-14 15:01:10 -0500

    Staged falling behaviors update

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/falling/FallingControllerState.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/falling/FallingControllerState.java
@@ -22,9 +22,14 @@ public class FallingControllerState extends HighLevelControllerState
 {
    private static final HighLevelControllerName controllerState = HighLevelControllerName.FALLING_STATE;
    private final YoDouble fallTransitionDuration;
+   private final YoDouble fallVerticalLoweringDuration;
 
    private final LowLevelOneDoFJointDesiredDataHolder lowLevelOneDoFJointDesiredDataHolder;
    private final YoPolynomial trajectory = new YoPolynomial("fallingTrajectory", 4, registry);
+   private final YoPolynomial loweringTrajectory = new YoPolynomial("fallingLoweringTrajectory", 4, registry);
+   private final YoDouble fallingLoweringAlpha = new YoDouble("fallingLoweringAlpha", registry);
+   private final YoDouble fallingConfigurationAlpha = new YoDouble("fallingConfigurationAlpha", registry);
+   private final YoBoolean enableStagedFallingConfiguration = new YoBoolean("enableStagedFallingConfiguration", registry);
    private final YoBoolean enableFallingDampingMode = new YoBoolean("enableFallingDampingMode", registry);
    private final YoBoolean fallingDampingModeActive = new YoBoolean("fallingDampingModeActive", registry);
    private final YoEnum<FallingTrialConfiguration> fallingTrialConfiguration = new YoEnum<>("fallingTrialConfiguration",
@@ -33,8 +38,10 @@ public class FallingControllerState extends HighLevelControllerState
                                                                                            false);
 
    private final double[] initialJointPositions;
+   private final double[] loweringJointPositions;
    private final double[] capturedJointPositions;
    private final double[] capturedJointVelocities;
+   private final boolean[] verticalLoweringJoints;
    private final WholeBodySetpointParameters fallingSetpoints;
 
    public FallingControllerState(CommandInputManager commandInputManager,
@@ -64,12 +71,21 @@ public class FallingControllerState extends HighLevelControllerState
       super(controllerState, highLevelControllerParameters, controllerToolbox.getControlledOneDoFJoints());
       fallTransitionDuration = new YoDouble("fallTransitionDuration", registry);
       fallTransitionDuration.set(0.5);
+      fallVerticalLoweringDuration = new YoDouble("fallVerticalLoweringDuration", registry);
+      fallVerticalLoweringDuration.set(0.25);
+      enableStagedFallingConfiguration.set(true);
       lowLevelOneDoFJointDesiredDataHolder = new LowLevelOneDoFJointDesiredDataHolder(controlledJoints.length);
       lowLevelOneDoFJointDesiredDataHolder.registerJointsWithEmptyData(controlledJoints);
 
       initialJointPositions = new double[controlledJoints.length];
+      loweringJointPositions = new double[controlledJoints.length];
       capturedJointPositions = new double[controlledJoints.length];
       capturedJointVelocities = new double[controlledJoints.length];
+      verticalLoweringJoints = new boolean[controlledJoints.length];
+      for (int i = 0; i < controlledJoints.length; i++)
+      {
+         verticalLoweringJoints[i] = isVerticalLoweringJoint(controlledJoints[i].getName());
+      }
       this.fallingSetpoints = fallingSetpoints;
       fallingTrialConfiguration.set(FallingTrialConfiguration.DEFAULT);
    }
@@ -77,12 +93,48 @@ public class FallingControllerState extends HighLevelControllerState
    @Override
    public void doAction(double timeInState)
    {
-      double timeInTrajectory = MathTools.clamp(timeInState, 0.0, fallTransitionDuration.getDoubleValue());
-      boolean useDampingMode = enableFallingDampingMode.getBooleanValue() && timeInState >= fallTransitionDuration.getDoubleValue();
+      double totalTransitionDuration = getTotalTransitionDuration();
+      double loweringDuration = getLoweringDuration();
+      double configurationDuration = totalTransitionDuration - loweringDuration;
+      boolean useStagedTrajectory = enableStagedFallingConfiguration.getBooleanValue() && loweringDuration > 1.0e-3 && configurationDuration > 1.0e-3;
+      boolean useDampingMode = enableFallingDampingMode.getBooleanValue() && timeInState >= totalTransitionDuration;
       fallingDampingModeActive.set(useDampingMode);
-      trajectory.compute(timeInTrajectory);
-      double alphaPosition = trajectory.getValue();
-      double alphaVelocity = trajectory.getVelocity();
+
+      double loweringAlphaPosition = 1.0;
+      double loweringAlphaVelocity = 0.0;
+      double configurationAlphaPosition = 1.0;
+      double configurationAlphaVelocity = 0.0;
+
+      if (useStagedTrajectory && timeInState < loweringDuration)
+      {
+         double timeInLowering = MathTools.clamp(timeInState, 0.0, loweringDuration);
+         loweringTrajectory.compute(timeInLowering);
+         loweringAlphaPosition = loweringTrajectory.getValue();
+         loweringAlphaVelocity = loweringTrajectory.getVelocity();
+         configurationAlphaPosition = 0.0;
+         configurationAlphaVelocity = 0.0;
+      }
+      else if (useStagedTrajectory)
+      {
+         double timeInConfiguration = MathTools.clamp(timeInState - loweringDuration, 0.0, configurationDuration);
+         trajectory.compute(timeInConfiguration);
+         loweringAlphaPosition = 1.0;
+         loweringAlphaVelocity = 0.0;
+         configurationAlphaPosition = trajectory.getValue();
+         configurationAlphaVelocity = trajectory.getVelocity();
+      }
+      else
+      {
+         double timeInTrajectory = MathTools.clamp(timeInState, 0.0, totalTransitionDuration);
+         trajectory.compute(timeInTrajectory);
+         loweringAlphaPosition = trajectory.getValue();
+         loweringAlphaVelocity = trajectory.getVelocity();
+         configurationAlphaPosition = loweringAlphaPosition;
+         configurationAlphaVelocity = loweringAlphaVelocity;
+      }
+
+      fallingLoweringAlpha.set(loweringAlphaPosition);
+      fallingConfigurationAlpha.set(configurationAlphaPosition);
 
       for (int i = 0; i < controlledJoints.length; i++)
       {
@@ -97,8 +149,24 @@ public class FallingControllerState extends HighLevelControllerState
          }
          else
          {
-            double desiredPosition = (1.0 - alphaPosition) * initialJointPositions[i] + alphaPosition * capturedJointPositions[i];
-            double desiredVelocity = alphaVelocity * (capturedJointPositions[i] - initialJointPositions[i]);
+            double desiredPosition;
+            double desiredVelocity;
+
+            if (useStagedTrajectory && timeInState < loweringDuration)
+            {
+               desiredPosition = interpolate(initialJointPositions[i], loweringJointPositions[i], loweringAlphaPosition);
+               desiredVelocity = loweringAlphaVelocity * (loweringJointPositions[i] - initialJointPositions[i]);
+            }
+            else if (useStagedTrajectory)
+            {
+               desiredPosition = interpolate(loweringJointPositions[i], capturedJointPositions[i], configurationAlphaPosition);
+               desiredVelocity = configurationAlphaVelocity * (capturedJointPositions[i] - loweringJointPositions[i]);
+            }
+            else
+            {
+               desiredPosition = interpolate(initialJointPositions[i], capturedJointPositions[i], configurationAlphaPosition);
+               desiredVelocity = configurationAlphaVelocity * (capturedJointPositions[i] - initialJointPositions[i]);
+            }
 
             lowLevelJointData.setDesiredPosition(desiredPosition);
             lowLevelJointData.setDesiredVelocity(desiredVelocity);
@@ -111,7 +179,13 @@ public class FallingControllerState extends HighLevelControllerState
    @Override
    public void onEntry()
    {
-      trajectory.setCubic(0.0, fallTransitionDuration.getDoubleValue(), 0.0, 0.0, 1.0, 0.0);
+      double totalTransitionDuration = getTotalTransitionDuration();
+      double loweringDuration = getLoweringDuration();
+      double configurationDuration = totalTransitionDuration - loweringDuration;
+      loweringTrajectory.setCubic(0.0, Math.max(1.0e-3, loweringDuration), 0.0, 0.0, 1.0, 0.0);
+      trajectory.setCubic(0.0, Math.max(1.0e-3, configurationDuration), 0.0, 0.0, 1.0, 0.0);
+      fallingLoweringAlpha.set(0.0);
+      fallingConfigurationAlpha.set(0.0);
       fallingDampingModeActive.set(false);
       FallingTrialConfiguration selectedFallingTrialConfiguration = fallingTrialConfiguration.getEnumValue();
 
@@ -133,6 +207,8 @@ public class FallingControllerState extends HighLevelControllerState
             capturedJointPositions[i] = controlledJoints[i].getQ();
             capturedJointVelocities[i] = controlledJoints[i].getQd();
          }
+
+         loweringJointPositions[i] = verticalLoweringJoints[i] ? capturedJointPositions[i] : initialJointPositions[i];
       }
    }
 
@@ -145,5 +221,31 @@ public class FallingControllerState extends HighLevelControllerState
    public JointDesiredOutputListReadOnly getOutputForLowLevelController()
    {
       return lowLevelOneDoFJointDesiredDataHolder;
+   }
+
+   private double getTotalTransitionDuration()
+   {
+      return Math.max(1.0e-3, fallTransitionDuration.getDoubleValue());
+   }
+
+   private double getLoweringDuration()
+   {
+      return MathTools.clamp(fallVerticalLoweringDuration.getDoubleValue(), 0.0, getTotalTransitionDuration());
+   }
+
+   private static double interpolate(double start, double end, double alpha)
+   {
+      return (1.0 - alpha) * start + alpha * end;
+   }
+
+   private static boolean isVerticalLoweringJoint(String jointName)
+   {
+      String lowerCaseJointName = jointName.toLowerCase();
+      boolean legJoint = lowerCaseJointName.contains("hip") || lowerCaseJointName.contains("knee") || lowerCaseJointName.contains("ankle");
+      boolean pitchJoint = lowerCaseJointName.contains("pitch") || lowerCaseJointName.endsWith("_y") || lowerCaseJointName.endsWith("y");
+      boolean yawOrRollJoint = lowerCaseJointName.contains("yaw") || lowerCaseJointName.contains("roll") || lowerCaseJointName.endsWith("_x")
+                               || lowerCaseJointName.endsWith("_z");
+
+      return legJoint && pitchJoint && !yawOrRollJoint;
    }
 }
